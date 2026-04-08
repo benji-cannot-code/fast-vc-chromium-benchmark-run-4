24203da6d38934a32bbb3cbd4c1d88a92cb64867
Emily Stark <estark@google.com>
2026-04-07 17:31:35 -0700

    Don't set Origin headers on navigation requests from Blink
    This will make it easier to strictly enforce validation on headers
    provided by the renderer on navigation requests. (See
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7617635.)
    
    Sending an Origin header from Blink should be basically vestigial,
    because the Origin header is set when applicable in
    AddAdditionalRequestHeaders() in navigation_request.cc. This CL removes
    calls to set the Origin header on navigation requests and ports the
    relevant unit tests to be browser tests covering the corresponding
    browser-side behavior.
    
    Bug: 491783215
    Change-Id: Ia961c154232b99a52f42c3d3322acb1d39c8975a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722305
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611078}

--- third_party/blink/renderer/core/loader/frame_load_request.cc
+++ third_party/blink/renderer/core/loader/frame_load_request.cc
@@ -49,8 +49,6 @@ static void SetReferrerForRequest(LocalDOMWindow* origin_window,
 
   request.SetReferrerString(referrer.referrer);
   request.SetReferrerPolicy(referrer.referrer_policy);
-  request.SetHTTPOriginToMatchReferrerPolicyIfNeeded(
-      origin_window->GetSecurityOrigin());
 }
 
 void LogDanglingMarkupHistogram(LocalDOMWindow* origin_window,
--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -211,8 +211,6 @@ ResourceRequest FrameLoader::ResourceRequestForReload(
     request.SetReferrerString(referrer.referrer);
     request.SetReferrerPolicy(referrer.referrer_policy);
     request.ClearHTTPOrigin();
-    request.SetHTTPOriginToMatchReferrerPolicyIfNeeded(
-        window->GetSecurityOrigin());
   }
 
   request.SetSkipServiceWorker(frame_load_type ==
