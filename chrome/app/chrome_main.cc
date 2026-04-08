3875210de0701e632d69c2e953791bda65e3c917
Mark Mentovai <mark@chromium.org>
2026-04-08 13:23:49 -0700

    mac: Transform inappropriate exit(69) from OS library into a crash
    Some users have reported Chrome terminating unexpectedly when attempting
    to screen share. Investigations into these reports have shown that OS
    code is calling `exit(69)`, producing abnormal termination but no crash
    report (either via Crashpad or the OS’ ReportCrash) or other visible
    artifacts.
    
    This installs an `atexit` handler to detect the inappropriate exits.
    `atexit` handlers are not provided with the `status` argument passed to
    `exit`, so it’s not possible to look for `exit(69)` directly. The
    inappropriate `exit` calls are believed to come from the OS’ ViewBridge
    framework, so the `atexit` handler performs a backtrace to look for the
    non-libc caller of libc’s `exit`, and treats any call coming from
    ViewBridge as inappropriate. Since OS library code should not normally
    be causing application termination, and since all this does is transform
    one form of termination (`exit`) to another (a crash), this seems
    appropriate.
    
    In other words, this implements *69 for exit(69).
    
    Bug: 474158974, 485292574, 498266556
    Change-Id: Icd81dc5e5847b7ce6a2ca5eb3267e8ac78b598f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738889
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611753}

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_MAC)
 #include "chrome/app/chrome_main_mac.h"
+#include "chrome/common/mac/detect_inappropriate_exit.h"
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
@@ -128,8 +129,9 @@ int ChromeMain(int argc, const char** argv) {
   base::debug::SetDumpWithoutCrashingFunction(&DumpProcessWithoutCrash);
 
   // Verify that chrome_elf and this module (chrome.dll) have the same version.
-  if (install_static::InstallDetails::Get().VersionMismatch())
+  if (install_static::InstallDetails::Get().VersionMismatch()) {
     base::debug::DumpWithoutCrashing();
+  }
 #else
   params.argc = argc;
   params.argv = argv;
@@ -148,6 +150,7 @@ int ChromeMain(int argc, const char** argv) {
 #endif
 
 #if BUILDFLAG(IS_MAC)
+  chrome::InitializeExitSixtyNineDetector();
   SetUpBundleOverrides();
 #endif
 
