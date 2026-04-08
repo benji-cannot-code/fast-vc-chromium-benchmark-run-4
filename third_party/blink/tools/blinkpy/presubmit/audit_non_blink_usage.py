9cb0e15c7f91c3ee09c0354fe39a347488dd9fdc
Colin Blundell <blundell@chromium.org>
2026-04-08 00:03:13 -0700

    [Blink] Pull out WebGL getting RGB/YUV caches into local vars
    This is a slight behavioral change as these caches are created
    on-demand. Pulling them out into local vars means that they will be
    created on every invocation of this method instead of only where they
    are actually used. This is behvariorally fine as the caches do not
    hold any meaningful state by default. Making this slight behavioral
    change in this CL enables a followup CL that moves management of these
    caches to clients to be a functional no-op.
    
    Bug: 343011436
    Change-Id: If27a82620447ee917709b944c55726f049fa22ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736182
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611258}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -1827,6 +1827,7 @@ _CONFIG = [
             'media::PaintCanvasVideoRenderer',
             'media::PIXEL_FORMAT_Y16',
             'media::VideoFrame',
+            'media::VideoFrameSharedImageCache',
             'viz::RasterContextProvider',
             'viz::ReleaseCallback',
             'viz::SinglePlaneFormat',
