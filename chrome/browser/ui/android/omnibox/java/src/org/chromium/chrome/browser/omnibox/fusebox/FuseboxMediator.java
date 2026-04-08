e73bccef468b0f2494add8b09600bea0326f82f8
Sky Malice <skym@chromium.org>
2026-04-08 13:00:10 -0700

    [AIM] Record tool mode for shown and used buttons.
    Bug: 493338804
    Change-Id: I9a72f3ab0b43e60ad42e7c9127d81949f119ceda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694470
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611737}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
@@ -364,6 +364,9 @@ public class FuseboxMediator implements FuseboxAttachmentChangeListener {
     /** Activate AI Mode as the Next Request fulfillment type. */
     @VisibleForTesting
     void activateAiMode(@AiModeActivationSource int activationReason) {
+        if (activationReason == AiModeActivationSource.TOOL_MENU) {
+            FuseboxMetrics.notifyToolButtonSelected(ToolMode.TOOL_MODE_UNSPECIFIED_VALUE);
+        }
         if (trySetRequestType(AutocompleteRequestType.AI_MODE)) {
             FuseboxMetrics.notifyAiModeActivated(activationReason);
         }
@@ -372,16 +375,19 @@ public class FuseboxMediator implements FuseboxAttachmentChangeListener {
     /** Activate image generation as the Next Request fulfillment type. */
     @VisibleForTesting
     void activateImageGeneration() {
+        FuseboxMetrics.notifyToolButtonSelected(ToolMode.TOOL_MODE_IMAGE_GEN_VALUE);
         trySetRequestType(AutocompleteRequestType.IMAGE_GENERATION);
     }
 
     private void onToolDeepSearchClicked() {
         assert OmniboxFeatures.sShowModelPicker.getValue();
+        FuseboxMetrics.notifyToolButtonSelected(ToolMode.TOOL_MODE_DEEP_SEARCH_VALUE);
         trySetRequestType(AutocompleteRequestType.DEEP_SEARCH);
     }
 
     private void onToolCanvasClicked() {
         assert OmniboxFeatures.sShowModelPicker.getValue();
+        FuseboxMetrics.notifyToolButtonSelected(ToolMode.TOOL_MODE_CANVAS_VALUE);
         trySetRequestType(AutocompleteRequestType.CANVAS);
     }
 
@@ -978,7 +984,7 @@ public class FuseboxMediator implements FuseboxAttachmentChangeListener {
 
     private void onDynamicButtonClicked(PopupButtonData data) {
         if (data.type == PopupButtonType.MODEL) {
-            FuseboxMetrics.notifyModelButtonUsed(data.protoId);
+            FuseboxMetrics.notifyModelButtonSelected(data.protoId);
             setModelMode(data.protoId);
         }
     }
