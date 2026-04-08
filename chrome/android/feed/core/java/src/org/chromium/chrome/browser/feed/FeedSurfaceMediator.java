02b45f60290097586e645e73f365ba994e2720b5
Xi Han <hanxi@google.com>
2026-04-08 08:57:58 -0700

    [Memory] Clean up AnimationFinishDetector of FeedSurfaceMediator.
    In this CL, we declare two RecyclerViewAnimationFinishDetector Nullable
    in FeedSurfaceMediator and fixes the wrong place where the detector
    destroyed. Screenshots: http://shortn/_vZeoTS1S8H.
    
    Bug: 487641528, 493844946
    Change-Id: Id52e88d8c71efee6e590461cbc885ac926971d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7739021
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611547}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -209,12 +209,12 @@ public class FeedSurfaceMediator
             ObservableSuppliers.createNonNull(RestoringState.WAITING_TO_RESTORE);
 
     private RecyclerView.@Nullable OnScrollListener mStreamScrollListener;
+    private @Nullable RecyclerViewAnimationFinishDetector mStreamScrollAnimationFinishDetector;
     private final ObserverList<ScrollListener> mScrollListeners = new ObserverList<>();
     private @Nullable ContentChangedListener mStreamContentChangedListener;
     private @Nullable MemoryPressureCallback mMemoryPressureCallback;
     private @Nullable FeedSigninPromo mSigninPromo;
-    private final RecyclerViewAnimationFinishDetector mRecyclerViewAnimationFinishDetector =
-            new RecyclerViewAnimationFinishDetector();
+    private @Nullable RecyclerViewAnimationFinishDetector mRecyclerViewAnimationFinishDetector;
 
     private boolean mFeedEnabled;
     private boolean mTouchEnabled = true;
@@ -289,14 +289,15 @@ public class FeedSurfaceMediator
         mPrefChangeRegistrar.addObserver(Pref.ENABLE_SNIPPETS, this::updateContent);
         mPrefChangeRegistrar.addObserver(Pref.ENABLE_SNIPPETS_BY_DSE, this::updateContent);
 
+        mRecyclerViewAnimationFinishDetector = new RecyclerViewAnimationFinishDetector();
         // This works around the bug that the out-of-screen toolbar is not brought back together
         // with the new tab page view when it slides down. This is because the RecyclerView
         // animation may not finish when content changed event is triggered and thus the new tab
         // page layout view may still be partially off screen.
         mStreamContentChangedListener =
                 contents ->
-                        mRecyclerViewAnimationFinishDetector.runWhenAnimationComplete(
-                                this::onContentsChanged);
+                        assumeNonNull(mRecyclerViewAnimationFinishDetector)
+                                .runWhenAnimationComplete(this::onContentsChanged);
 
         initialize();
     }
@@ -357,6 +358,10 @@ public class FeedSurfaceMediator
         mStreamHolder = null;
         mCurrentStream = null;
         mStreamContentChangedListener = null;
+        if (mRecyclerViewAnimationFinishDetector != null) {
+            mRecyclerViewAnimationFinishDetector.destroy();
+            mRecyclerViewAnimationFinishDetector = null;
+        }
         mRestoreScrollState = null;
     }
 
@@ -475,11 +480,9 @@ public class FeedSurfaceMediator
 
         mSettingUpStreams = false;
 
+        mStreamScrollAnimationFinishDetector = new RecyclerViewAnimationFinishDetector();
         mStreamScrollListener =
                 new RecyclerView.OnScrollListener() {
-                    private final RecyclerViewAnimationFinishDetector mAnimationFinishDetector =
-                            new RecyclerViewAnimationFinishDetector();
-
                     @Override
                     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                         for (ScrollListener listener : mScrollListeners) {
@@ -531,11 +534,12 @@ public class FeedSurfaceMediator
                                                         mCoordinator
                                                                 .getRecyclerView()
                                                                 .setItemAnimator(originalAnimator);
-                                                        mAnimationFinishDetector
+                                                        assumeNonNull(
+                                                                        mStreamScrollAnimationFinishDetector)
                                                                 .runWhenAnimationComplete(null);
                                                     };
-                                            mAnimationFinishDetector.runWhenAnimationComplete(
-                                                    onComplete);
+                                            assumeNonNull(mStreamScrollAnimationFinishDetector)
+                                                    .runWhenAnimationComplete(onComplete);
                                         }
                                     }
                                 };
@@ -711,6 +715,11 @@ public class FeedSurfaceMediator
             mStreamScrollListener = null;
         }
 
+        if (mStreamScrollAnimationFinishDetector != null) {
+            mStreamScrollAnimationFinishDetector.destroy();
+            mStreamScrollAnimationFinishDetector = null;
+        }
+
         if (mMemoryPressureCallback != null) {
             MemoryPressureListener.removeCallback(mMemoryPressureCallback);
             mMemoryPressureCallback = null;
@@ -728,8 +737,6 @@ public class FeedSurfaceMediator
             mStreamHolder = null;
         }
 
-        mRecyclerViewAnimationFinishDetector.destroy();
-        mStreamContentChangedListener = null;
         unbindStream();
 
         mPrefChangeRegistrar.removeObserver(Pref.ARTICLES_LIST_VISIBLE);
