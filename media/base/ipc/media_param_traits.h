0e92f063d5f22d930fcafd2d0488f5ecb77c45c9
Vikram Pasupathy <vpasupathy@chromium.org>
2026-04-07 19:25:28 -0700

    media: Remove legacy IPC traits for EncryptionPattern
    encryption_pattern_mojom_traits.h exists.
    
    Bug: 40621529, 497929481
    Change-Id: I4a125a0627418a384ed6f21d663bc1ca3c9d5197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7732451
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611151}

--- media/base/ipc/media_param_traits.h
+++ media/base/ipc/media_param_traits.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 class AudioParameters;
-class EncryptionPattern;
 }
 
 namespace IPC {
@@ -29,15 +28,6 @@ struct ParamTraits<media::AudioParameters> {
 template <>
 struct ParamTraits<media::AudioParameters::HardwareCapabilities> {
   typedef media::AudioParameters::HardwareCapabilities param_type;
-  static void Write(base::Pickle* m, const param_type& p);
-  static bool Read(const base::Pickle* m,
-                   base::PickleIterator* iter,
-                   param_type* r);
-};
-
-template <>
-struct ParamTraits<media::EncryptionPattern> {
-  typedef media::EncryptionPattern param_type;
   static void Write(base::Pickle* m, const param_type& p);
   static bool Read(const base::Pickle* m,
                    base::PickleIterator* iter,
