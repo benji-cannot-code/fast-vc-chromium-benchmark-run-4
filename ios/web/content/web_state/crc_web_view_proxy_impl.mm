6f942b34782665036cc59977351bd8a3348475f9
Gyuyoung Kim <gyuyoung@igalia.com>
2026-04-08 15:47:16 -0700

    [iOS Blink] Add a dummy setMinimumViewportInset
    https://crrev.com/c/7727318 introduced setMinimumViewportInset method,
    iOS blink port also needs to have it.
    
    Change-Id: I098d0430a3134f9f2a1aab3d5c79e2c5dc4a40ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738542
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611829}

--- ios/web/content/web_state/crc_web_view_proxy_impl.mm
+++ ios/web/content/web_state/crc_web_view_proxy_impl.mm
@@ -128,4 +128,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setObscuredInsets:(UIEdgeInsets)obscuredInsets {
 }
 
+- (void)setMinimumViewportInset:(UIEdgeInsets)minInset
+           maximumViewportInset:(UIEdgeInsets)maxInset {
+}
 @end
