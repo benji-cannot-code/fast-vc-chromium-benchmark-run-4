9f4695251c5b011eba2fee9cb3cb7b7c383a9d98
Jason Hu <hujasonx@google.com>
2026-04-08 14:19:23 -0700

    [context-input] Allow local files to be suggested as auto tab chips
    Enables urls that have the file:// scheme to be suggested.
    
    Bug: b:500705531
    Change-Id: I67cffb9e7fb05097d21ba5485067d7bc0d92f4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738768
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611780}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -987,7 +987,8 @@ bool ContextualTasksUI::CanUpdateSuggestedTabContext(
   }
 
   if (!last_committed_url.is_valid() ||
-      !last_committed_url.SchemeIsHTTPOrHTTPS()) {
+      !(last_committed_url.SchemeIsHTTPOrHTTPS() ||
+        last_committed_url.SchemeIsFile())) {
     return false;
   }
 
