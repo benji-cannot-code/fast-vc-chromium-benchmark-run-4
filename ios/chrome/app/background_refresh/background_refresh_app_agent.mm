b2d8dbf2978c666f5153487fd1930d78753da934
Sylvain Defresne <sdefresne@chromium.org>
2026-04-09 04:51:24 -0700

    [ios] Use base::Time instead of NSDate to manipulate time constants
    The best practise guide recommends using Chromium primitive over the
    system primitives when possible. The API of base::Time is available
    and simpler than NSDate (especially to compute time differences and
    to declare constants), so convert code to use it.
    
    Bug: none
    Change-Id: I569506a0772ef9e00c6b9150e53682bb6f558cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736438
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612103}

--- ios/chrome/app/background_refresh/background_refresh_app_agent.mm
+++ ios/chrome/app/background_refresh/background_refresh_app_agent.mm
@@ -370,14 +370,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // No provider provided a usable refresh interval.
     return;
   }
-  NSTimeInterval delayInSeconds = delay.InSecondsF();
 
   // TODO(crbug.com/354918222): coalesce multiple requests so there's only ever
   // a single scheduled refresh pending.
   BGAppRefreshTaskRequest* request = [[BGAppRefreshTaskRequest alloc]
       initWithIdentifier:kAppBackgroundRefreshTaskIdentifier];
-  request.earliestBeginDate =
-      [NSDate dateWithTimeIntervalSinceNow:delayInSeconds];
+  request.earliestBeginDate = (base::Time::Now() + delay).ToNSDate();
   NSError* error = nil;
   [BGTaskScheduler.sharedScheduler submitTaskRequest:request error:&error];
   BGTaskSchedulerErrorActions action = BGTaskSchedulerErrorActions::kUnknown;
