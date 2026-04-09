57ec54ea7cfe519281974ce121d36ff1e0515b66
Aishwarya Rajesh <aishwaryarj@google.com>
2026-04-09 13:46:44 -0700

    [MW] Re-enable tests in MultiWindowUtilsUnitTest
    Bug: 423920653
    Change-Id: Iaf65e21d028de23bc9b12a0b3928c7ddbd53a324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745745
    Reviewed-by: Ryan Thomas <rathomas@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612488}

--- chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsUnitTest.java
@@ -913,7 +913,6 @@ public class MultiWindowUtilsUnitTest {
 
     @Test
     @Config(sdk = 31)
-    @DisabledTest(message = "https://crbug.com/423920653")
     public void testGetInstanceIdForViewIntent_LessThanMaxInstancesOpen() {
         MultiWindowTestUtils.enableMultiInstance();
         when(mTabModelSelector.getModel(false)).thenReturn(mNormalTabModel);
@@ -937,7 +936,6 @@ public class MultiWindowUtilsUnitTest {
 
     @Test
     @Config(sdk = 31)
-    @DisabledTest(message = "https://crbug.com/423920653")
     public void testGetInstanceIdForViewIntent_MaxInstancesOpen_MaxRunningActivities() {
         MultiWindowTestUtils.enableMultiInstance();
         when(mTabModelSelector.getModel(false)).thenReturn(mNormalTabModel);
