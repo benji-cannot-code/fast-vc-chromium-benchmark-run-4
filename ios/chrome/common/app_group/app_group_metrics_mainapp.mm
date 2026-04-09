6ae39d970f142d632f82c9764b3187f7bcb18909
Sylvain Defresne <sdefresne@chromium.org>
2026-04-09 04:09:39 -0700

    [ios] Use base::Time instead of NSDate to manipulate time constants
    The best practise guide recommends using Chromium primitive over the
    system primitives when possible. The API of base::Time is available
    and simpler than NSDate (especially to compute time differences and
    to declare constants), so convert code to use it.
    
    Bug: none
    Change-Id: Ia08d3392bf7351628c8fd3870a1990f89d343c5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735060
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612089}

--- ios/chrome/common/app_group/app_group_metrics_mainapp.mm
+++ ios/chrome/common/app_group/app_group_metrics_mainapp.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/metrics/histogram_functions.h"
 #import "base/threading/scoped_blocking_call.h"
+#import "base/time/time.h"
 #import "ios/chrome/common/app_group/app_group_constants.h"
 #import "ios/chrome/common/app_group/app_group_metrics.h"
 
@@ -16,7 +17,7 @@ namespace {
 // Delay in seconds before reporting the metrics coming from Open extensions.
 // This delay is needed to be sure the shared user default
 // is correctly synchronized.
-const int kDispatchTimeInSeconds = 2;
+constexpr base::TimeDelta kDispatchTime = base::Seconds(2);
 
 // Maximum number of outcomes reported to UMA to avoid infinite loops.
 const int kMaxNumberOfLogs = 10;
@@ -49,8 +50,7 @@ void EnableMetrics(NSString* client_id,
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     dispatch_after(
-        dispatch_time(DISPATCH_TIME_NOW,
-                      (int64_t)(kDispatchTimeInSeconds * NSEC_PER_SEC)),
+        dispatch_time(DISPATCH_TIME_NOW, kDispatchTime.InNanoseconds()),
         dispatch_get_main_queue(), ^{
           LogOpenExtensionMetrics();
         });
