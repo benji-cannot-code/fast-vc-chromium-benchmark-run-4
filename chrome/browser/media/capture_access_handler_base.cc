5667a9167528da7a4ca9dddee79d24cb186441fc
James Cook <jamescook@chromium.org>
2026-04-09 16:35:27 -0700

    media: Update ENABLE_EXTENSIONS in CaptureAccessHandlerBase
    Now that we have extensions on desktop Android, there's a new build flag
    ENABLE_EXTENSIONS_CORE required for "traditional extensions platforms +
    desktop Android". This file was previously converted, but we missed one
    usage in the includes section.
    
    See bug for details on the history of the extensions build flags.
    
    Bug: 494643383
    Change-Id: Ife1054010319c2ab0f7f8944e2b0e5f710c559e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745095
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612575}

--- chrome/browser/media/capture_access_handler_base.cc
+++ chrome/browser/media/capture_access_handler_base.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/buildflags/buildflags.h"
 #include "ui/gfx/native_ui_types.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/common/extension.h"
 #endif
 
