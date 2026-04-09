fae6224af68d6394e16d036f72a816cf97342f03
Hailey Wang <haileywang@google.com>
2026-04-08 17:16:56 -0700

    [Glic] Persist the completed state until user take action
    Bug: 500437470
    Change-Id: Ic567460f96bede0b1a1400f3a0b0dd41e60fd86f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736416
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611869}

--- chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicToolbarButtonController.java
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicToolbarButtonController.java
@@ -69,6 +69,7 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
     private final ButtonSpec mDoneSpec;
 
     private @ButtonState int mButtonState = ButtonState.DEFAULT;
+    private boolean mPersistDoneState;
 
     /**
      * @param context The Android context.
@@ -231,6 +232,7 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
             ActorTask task = mCurrentActorService.getCurrentActiveTask();
             if (task != null) {
                 @ActorTaskState int state = task.getState();
+                mPersistDoneState = false;
                 switch (state) {
                     case ActorTaskState.WAITING_ON_USER:
                     case ActorTaskState.FAILED:
@@ -238,11 +240,11 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
                         break;
                     case ActorTaskState.FINISHED:
                         mButtonState = ButtonState.DONE;
+                        mPersistDoneState = true;
                         break;
                     case ActorTaskState.ACTING:
                     case ActorTaskState.REFLECTING:
                         mButtonState = ButtonState.WORKING;
-                        // TODO(haileywang): Start the animation of the working button.
                         break;
                     case ActorTaskState.PAUSED_BY_USER:
                     case ActorTaskState.PAUSED_BY_ACTOR:
@@ -255,6 +257,8 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
                     default:
                         throw new AssertionError("Unexpected task state: " + state);
                 }
+            } else if (mPersistDoneState) {
+                mButtonState = ButtonState.DONE;
             }
         }
     }
@@ -287,11 +291,14 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
 
     @Override
     public void onClick(View view) {
+        mPersistDoneState = false;
         mToggleGlicCallback.run();
         Tracker tracker = mTrackerSupplier.get();
         if (tracker != null) {
             tracker.notifyEvent(EventConstants.ADAPTIVE_TOOLBAR_CUSTOMIZATION_GLIC_CLICKED);
         }
+        updateButtonState();
+        notifyObservers(true);
     }
 
     @Override
