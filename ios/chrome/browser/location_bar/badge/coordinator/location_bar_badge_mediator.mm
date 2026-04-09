cf6540cb11bc78827c0fc32679ea479a36c1b65b
Sylvain Defresne <sdefresne@chromium.org>
2026-04-09 05:19:31 -0700

    [ios] Use base::Time instead of NSDate to manipulate time constants
    The best practise guide recommends using Chromium primitive over the
    system primitives when possible. The API of base::Time is available
    and simpler than NSDate (especially to compute time differences and
    to declare constants), so convert code to use it.
    
    Bug: none
    Change-Id: I288b70e3b080d781b6ca5d1af1fbe97d104aa795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735059
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612116}

--- ios/chrome/browser/location_bar/badge/coordinator/location_bar_badge_mediator.mm
+++ ios/chrome/browser/location_bar/badge/coordinator/location_bar_badge_mediator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/raw_ptr.h"
 #import "base/strings/sys_string_conversions.h"
+#import "base/time/time.h"
 #import "base/timer/timer.h"
 #import "components/feature_engagement/public/event_constants.h"
 #import "components/feature_engagement/public/feature_constants.h"
@@ -48,9 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Time to start transition in seconds.
-const int kStartExpandTransitionTimeInSeconds = 2;
+constexpr base::TimeDelta kStartExpandTransitionTime = base::Seconds(2);
 // Time to start the collapse transition in seconds.
-const int kStartCollapseTransitionTimeInSeconds = 5;
+constexpr base::TimeDelta kStartCollapseTransitionTime = base::Seconds(5);
 }  // anonymous namespace
 
 @interface LocationBarBadgeMediator () <ContextualPanelTabHelperObserving,
@@ -374,8 +375,7 @@ const int kStartCollapseTransitionTimeInSeconds = 5;
 - (void)startPromoTimer:(LocationBarBadgeConfiguration*)badgeConfig {
   __weak LocationBarBadgeMediator* weakSelf = self;
   _promoStartTimer = std::make_unique<base::OneShotTimer>();
-  _promoStartTimer->Start(FROM_HERE,
-                          base::Seconds(kStartExpandTransitionTimeInSeconds),
+  _promoStartTimer->Start(FROM_HERE, kStartExpandTransitionTime,
                           base::BindOnce(^{
                             [weakSelf setupAndExpandChip:badgeConfig];
                           }));
@@ -385,8 +385,7 @@ const int kStartCollapseTransitionTimeInSeconds = 5;
 - (void)startEndPromoTimer {
   __weak LocationBarBadgeMediator* weakSelf = self;
   _promoEndTimer = std::make_unique<base::OneShotTimer>();
-  _promoEndTimer->Start(FROM_HERE,
-                        base::Seconds(kStartCollapseTransitionTimeInSeconds),
+  _promoEndTimer->Start(FROM_HERE, kStartCollapseTransitionTime,
                         base::BindOnce(^{
                           [weakSelf cleanupAndTransitionToDefaultBadgeState];
                         }));
@@ -396,8 +395,7 @@ const int kStartCollapseTransitionTimeInSeconds = 5;
 - (void)startIPHTimer:(LocationBarBadgeConfiguration*)badgeConfig {
   __weak LocationBarBadgeMediator* weakSelf = self;
   _promoStartTimer = std::make_unique<base::OneShotTimer>();
-  _promoStartTimer->Start(FROM_HERE,
-                          base::Seconds(kStartExpandTransitionTimeInSeconds),
+  _promoStartTimer->Start(FROM_HERE, kStartExpandTransitionTime,
                           base::BindOnce(^{
                             [weakSelf setupAndShowIPH:badgeConfig];
                           }));
