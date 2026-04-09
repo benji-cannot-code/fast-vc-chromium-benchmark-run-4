47903458a1a0f12a07719c33aab242ca9de42d8d
Zoraiz Naeem <zoraiznaeem@google.com>
2026-04-09 12:30:51 -0700

    Nit: Refactor frame header width to use std::optional
    The width of the DefaultFrameHeader was previously stored as an int,
    with a value of -1 indicating that the width was not explicitly set.
    
    This change refactors width_in_pixels_ to be a std::optional<int>.
    
    Change-Id: I09680b262e0a0609b01b8b97a56d3f9d08003f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7739961
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612428}

--- chromeos/ui/frame/header_view.h
+++ chromeos/ui/frame/header_view.h
@@ -91,7 +91,7 @@ class COMPONENT_EXPORT(CHROMEOS_UI_FRAME) HeaderView
 
   void UpdateCaptionButtons();
 
-  void SetWidthInPixels(int width_in_pixels);
+  void SetWidthInPixels(std::optional<int> width_in_pixels);
 
   void SetHeaderCornerRadius(int radius);
 
