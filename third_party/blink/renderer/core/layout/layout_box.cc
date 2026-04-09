04beff0d9f211955f609a97e676d451654a40c54
Joey Arhar <jarhar@chromium.org>
2026-04-09 16:39:19 -0700

    Fix clipping of customizable select
    There was some layout code manually performing clipping for select
    elements which shouldn't apply for customizable select.
    
    Bug: 379805732
    Change-Id: Ic8d817ab919b274c90a3558f8e84bea055b04898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7718182
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612578}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -479,6 +479,12 @@ void RecalcFragmentScrollableOverflow(RecalcScrollableOverflowResult& result,
   }
 }
 
+bool IsAppearanceAutoMenuList(const LayoutBox& obj) {
+  return obj.IsMenuList() &&
+         obj.StyleRef().EffectiveAppearance() != AppearanceValue::kBase &&
+         obj.StyleRef().EffectiveAppearance() != AppearanceValue::kBaseSelect;
+}
+
 }  // namespace
 
 LayoutBoxRareData::LayoutBoxRareData()
@@ -2423,7 +2429,7 @@ PhysicalRect LayoutBox::OverflowClipRect(
       control_clip.Move(location);
       clip_rect.Intersect(control_clip);
     }
-  } else if (IsMenuList()) [[unlikely]] {
+  } else if (IsAppearanceAutoMenuList(*this)) [[unlikely]] {
     DCHECK(HasControlClip());
     PhysicalRect control_clip = PhysicalContentBoxRect();
     control_clip.Move(location);
@@ -2442,7 +2448,8 @@ PhysicalRect LayoutBox::OverflowClipRectForScrollNode(
 
 bool LayoutBox::HasControlClip() const {
   NOT_DESTROYED();
-  if (IsTextField() || IsMenuList() || IsInputButton()) [[unlikely]] {
+  if (IsTextField() || IsAppearanceAutoMenuList(*this) || IsInputButton())
+      [[unlikely]] {
     return true;
   }
   return false;
