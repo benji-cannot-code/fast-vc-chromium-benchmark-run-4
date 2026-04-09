773fbda8010601e1fc5bd9e8c02ba201819a4e32
Alex Valadkevich <valadkevich@google.com>
2026-04-09 08:49:11 -0700

    [DLP cancellation] Implement user-initiated cancellation for deep scanning downloads
    This change adds the way to call MaybeCancelRequests on the CloudBinaryUploadService for the download items scanning. Like that We do the best effort to cancel them if necessary to free resources, and to report the cancellation.
    
    Bug: 399639311
    Change-Id: I331b1e127f0c176a454bca02de3ec0e2a6fbfac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705273
    Commit-Queue: Alex Valadkevich <valadkevich@google.com>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612273}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_request.cc
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_request.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/observer_list.h"
+#include "base/rand_util.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/thread_pool.h"
@@ -421,6 +422,7 @@ DeepScanningRequest::DeepScanningRequest(
       pre_scan_download_check_result_(pre_scan_download_check_result),
       password_(password.CopyAsOptional()),
       reason_(enterprise_connectors::ContentAnalysisRequest::NORMAL_DOWNLOAD),
+      user_action_id_(base::HexEncode(base::RandBytesAsVector(128))),
       weak_ptr_factory_(this) {
   base::UmaHistogramEnumeration("SBClientDownload.DeepScanType",
                                 DeepScanType::NORMAL);
@@ -444,6 +446,7 @@ DeepScanningRequest::DeepScanningRequest(
       pending_scan_requests_(save_package_files_.size()),
       pre_scan_download_check_result_(pre_scan_download_check_result),
       reason_(enterprise_connectors::ContentAnalysisRequest::SAVE_AS_DOWNLOAD),
+      user_action_id_(base::HexEncode(base::RandBytesAsVector(128))),
       weak_ptr_factory_(this) {
   base::UmaHistogramEnumeration("SBClientDownload.DeepScanType",
                                 DeepScanType::SAVE_PACKAGE);
@@ -842,6 +845,21 @@ void DeepScanningRequest::OnDownloadDestroyed(
   // scan has finished.
   callback_.Reset();
 
+  Profile* profile =
+      Profile::FromBrowserContext(metadata_->GetBrowserContext());
+  enterprise_connectors::BinaryUploadService* upload_service =
+      download_service_->GetBinaryUploadService(profile, analysis_settings_);
+
+  if (upload_service) {
+    auto cancel =
+        std::make_unique<enterprise_connectors::BinaryUploadCancelRequests>(
+            analysis_settings_.cloud_or_local_settings);
+    cancel->set_user_action_id(user_action_id_);
+
+    // We do the best effort to cancel the requests in upload service.
+    upload_service->MaybeCancelRequests(std::move(cancel));
+  }
+
   // `FinishRequest` always clears the `download_observation` and `metadata`,
   // preventing use-after-free issues for `download_item` after it's been
   // destroyed.
@@ -870,7 +888,7 @@ std::string DeepScanningRequest::tab_title() const {
 }
 
 std::string DeepScanningRequest::user_action_id() const {
-  return "";
+  return user_action_id_;
 }
 
 std::string DeepScanningRequest::email() const {
