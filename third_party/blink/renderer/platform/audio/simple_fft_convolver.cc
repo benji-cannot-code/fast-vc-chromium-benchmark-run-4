17332e0052020ce6ce5149aa3c72c1be77de0afb
Michael Wilson <mjwilson@chromium.org>
2026-04-09 15:16:36 -0700

    [WebAudio] Take input as const ref instead of const ref to unique_ptr
    This should cause no functional change.
    
    Const ref to unique_ptr is considered an anti-pattern, and taking input
    by ref is recommended by the style guide since it cannot be null.
    
    Change-Id: I1dc1f62a6ad7e1ac974708c295448cc1034c1c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738126
    Reviewed-by: Mahesh Kannan <kmaheshb@google.com>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612535}

--- third_party/blink/renderer/platform/audio/simple_fft_convolver.cc
+++ third_party/blink/renderer/platform/audio/simple_fft_convolver.cc
@@ -12,8 +12,8 @@ namespace blink {
 
 SimpleFFTConvolver::SimpleFFTConvolver(
     unsigned input_block_size,
-    const std::unique_ptr<AudioFloatArray>& convolution_kernel)
-    : convolution_kernel_size_(convolution_kernel->size()),
+    const AudioFloatArray& convolution_kernel)
+    : convolution_kernel_size_(convolution_kernel.size()),
       fft_kernel_(2 * input_block_size),
       frame_(2 * input_block_size),
       input_buffer_(2 *
@@ -24,7 +24,7 @@ SimpleFFTConvolver::SimpleFFTConvolver(
   // Do padded FFT to get frequency-domain version of the convolution kernel.
   // This FFT and caching is done once in here so that it does not have to be
   // done repeatedly in |Process|.
-  fft_kernel_.DoPaddedFFT(convolution_kernel->as_span());
+  fft_kernel_.DoPaddedFFT(convolution_kernel.as_span());
 }
 
 void SimpleFFTConvolver::Process(base::span<const float> source,
--- third_party/blink/renderer/platform/audio/simple_fft_convolver.h
+++ third_party/blink/renderer/platform/audio/simple_fft_convolver.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_AUDIO_SIMPLE_FFT_CONVOLVER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_AUDIO_SIMPLE_FFT_CONVOLVER_H_
 
-#include <memory>
-
 #include "third_party/blink/renderer/platform/audio/audio_array.h"
 #include "third_party/blink/renderer/platform/audio/fft_frame.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -24,9 +22,8 @@ class SimpleFFTConvolver final {
   USING_FAST_MALLOC(SimpleFFTConvolver);
 
  public:
-  SimpleFFTConvolver(
-      unsigned input_block_size,
-      const std::unique_ptr<AudioFloatArray>& convolution_kernel);
+  SimpleFFTConvolver(unsigned input_block_size,
+                     const AudioFloatArray& convolution_kernel);
   SimpleFFTConvolver(const SimpleFFTConvolver&) = delete;
   SimpleFFTConvolver& operator=(const SimpleFFTConvolver&) = delete;
 
