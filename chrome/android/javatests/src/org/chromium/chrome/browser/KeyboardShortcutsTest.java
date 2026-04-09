5a08c578dccb05d5260a5fa16aa553e64e568dcd
Ryo Hashimoto <hashimoto@google.com>
2026-04-08 18:55:31 -0700

    android: Disable KeyboardShortcutsTest#testCloseTab_singlePinnedTab_firstAttempt_timeout
    This test is flaky
    
    BUG=490369117
    
    Change-Id: Ie84ff1a2c276a96549a4fa38bb04b3b97244cbde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7741339
    Commit-Queue: ben chin <lungc@chromium.org>
    Reviewed-by: ben chin <lungc@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Owners-Override: Ryo Hashimoto <hashimoto@chromium.org>
    Auto-Submit: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611929}

--- chrome/android/javatests/src/org/chromium/chrome/browser/KeyboardShortcutsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/KeyboardShortcutsTest.java
@@ -38,6 +38,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.ui.KeyboardUtils;
@@ -219,6 +220,7 @@ public class KeyboardShortcutsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "Flaky - crbug.com/490369117")
     public void testCloseTab_singlePinnedTab_firstAttempt_timeout() {
         // Setup the first closure attempt of a pinned tab.
         setUpTabModelSelector(List.of(mTab));
