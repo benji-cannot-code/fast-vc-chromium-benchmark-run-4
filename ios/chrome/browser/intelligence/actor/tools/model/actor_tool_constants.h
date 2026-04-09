a4803bae29d4cfe58394ed57419f7452fb631ac9
Kirubel <kaklilu@chromium.org>
2026-04-09 12:06:11 -0700

    Make ScrollTool support omitting the `target` field.
    The desktop tool handles this case by targeting the root element
    of the page. We use document.scrollingElement to get this on iOS.
    
    We mirror the kRootElementDomNodeId constant from Desktop, since
    it's also an invalid DOM Node ID on iOS.
    
    Bug: b:472289079
    Change-Id: I628edbfcabc81913fa9b2290f1e759996a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727456
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612412}

---
+++ ios/chrome/browser/intelligence/actor/tools/model/actor_tool_constants.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_TOOLS_MODEL_ACTOR_TOOL_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_TOOLS_MODEL_ACTOR_TOOL_CONSTANTS_H_
+
+namespace actor {
+
+// Mirrored from the Desktop analogue at chrome/common/actor/actor_constants.h.
+//
+// 0 is not a valid DOM Node ID so it is used to indicate targeting the
+// root/viewport. See the code that assigns DOM node ids at
+// ios/chrome/browser/intelligence/proto_wrappers/resources/dom_node_ids.ts.
+inline constexpr int kRootElementDomNodeId = 0;
+
+}  // namespace actor
+
+#endif  // IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_TOOLS_MODEL_ACTOR_TOOL_CONSTANTS_H_
