249a5691acf5084d2423ad768e71638ec35e4f9b
Andreas Haas <ahaas@chromium.org>
2026-04-08 22:17:13 -0700

    Add runtime type checking for CallbackHolder in gin.
    Introduce a unique kSignatureId for each function signature. Store a
    pointer to this ID in CallbackHolderBase and check it in
    DispatchToCallbackImpl to ensure the correct CallbackHolder type is
    being cast to.
    
    Bug: 499960178
    Change-Id: I4f8b200f6d104c447d0ac00dcb2294e56b524b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736695
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611979}

--- gin/function_template.cc
+++ gin/function_template.cc
@@ -9,9 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "gin/public/gin_embedders.h"
 
-namespace gin {
-
-namespace internal {
+namespace gin::internal {
 
 CallbackHolderBase::DisposeObserver::DisposeObserver(
     gin::PerIsolateData* per_isolate_data,
@@ -31,8 +29,10 @@ void CallbackHolderBase::DisposeObserver::OnDisposed() {
   delete &holder_.get();
 }
 
-CallbackHolderBase::CallbackHolderBase(v8::Isolate* isolate)
-    : v8_ref_(
+CallbackHolderBase::CallbackHolderBase(v8::Isolate* isolate,
+                                       uintptr_t type_identifier)
+    : type_identifier_(type_identifier),
+      v8_ref_(
           isolate,
           v8::External::New(isolate, this, kGinInternalCallbackHolderBaseTag)),
       dispose_observer_(PerIsolateData::From(isolate), this) {
@@ -85,6 +85,4 @@ void ThrowConversionError(Arguments* args,
   }
 }
 
-}  // namespace internal
-
-}  // namespace gin
+}  // namespace gin::internal
