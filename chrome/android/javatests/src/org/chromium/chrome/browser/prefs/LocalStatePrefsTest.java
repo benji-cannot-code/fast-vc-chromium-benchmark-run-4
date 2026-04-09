984bf6ca3eb317480a2dea6aa811efe93445446a
Jenna Himawan <jhimawan@google.com>
2026-04-08 17:52:11 -0700

    [XplatSyncedSetup] Add observation of local state prefs
    To ensure omnibox position is correctly written and read for
    cross-device setting import user journeys.
    
    Bug: 475543024
    Change-Id: I579e7d50c2d97b5efecea5056fb4e45d1de45371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738996
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1611904}

--- chrome/android/javatests/src/org/chromium/chrome/browser/prefs/LocalStatePrefsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/prefs/LocalStatePrefsTest.java
@@ -23,6 +23,8 @@ import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.components.prefs.PrefService;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /** Tests for {@link LocalStatePrefs}. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
@@ -62,5 +64,38 @@ public class LocalStatePrefsTest {
                             .that(prefService.getString(TEST_PREF_KEY))
                             .isEqualTo(existingValue);
                 });
+    }
+
+    @Test
+    @SmallTest
+    public void testObserver() {
+        AtomicBoolean called = new AtomicBoolean(false);
+        Runnable observer = () -> called.set(true);
+
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    LocalStatePrefs.setNativePrefsLoadedForTesting(false);
+                    LocalStatePrefs.addObserver(observer);
+                });
+
+        assertWithMessage("Observer should not be called yet").that(called.get()).isFalse();
+
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    LocalStatePrefs.setNativePrefsLoadedForTesting(true);
+                });
+
+        assertWithMessage("Observer should be called").that(called.get()).isTrue();
+
+        called.set(false);
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    LocalStatePrefs.removeObserver(observer);
+                    LocalStatePrefs.setNativePrefsLoadedForTesting(false);
+                    LocalStatePrefs.setNativePrefsLoadedForTesting(true);
+                });
+        assertWithMessage("Observer should not be called after removal")
+                .that(called.get())
+                .isFalse();
     }
 }
