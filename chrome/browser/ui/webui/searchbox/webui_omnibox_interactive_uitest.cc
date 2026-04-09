a23e96422b0f2f64b407245af4f69e6fb016bc2b
Devi Varaprasad Jonnala <jonnalad@google.com>
2026-04-09 10:21:17 -0700

    Revert "[ntp][omnibox] Update WaitForGoogleSearch to check multiple parameters"
    This reverts commit 205f588ad0463f25ad479b46d1f42b68af0e87af.
    
    Reason for revert: Older version of WaitForGoogleSearch was used in recently landed CL
    
    Original change's description:
    > [ntp][omnibox] Update WaitForGoogleSearch to check multiple parameters
    >
    > The `WaitForGoogleSearch` helper now accepts a map of expected query
    > parameters. This allows tests to verify not just the 'q' parameter but
    > also other parameters like 'oq', 'utm' in the Google Search URL. The
    > existing calls to WaitForGoogleSearch have been updated to use the new
    > format. This also solves the issue when the URL doesn't have 'q' as its
    > first parameter.
    >
    > Bug: b:499325541
    > Change-Id: I5509794114f5656db9dae055160e82e144891ec8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7740968
    > Reviewed-by: Marlon Facey <mfacey@chromium.org>
    > Reviewed-by: Sho Mizoe <shomizoe@google.com>
    > Commit-Queue: Devi Varaprasad Jonnala <jonnalad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1612306}
    
    Bug: b:499325541
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I42841eb6de84388248ee335c126dfa3ca7629680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7744865
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Devi Varaprasad Jonnala <jonnalad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612335}

--- chrome/browser/ui/webui/searchbox/webui_omnibox_interactive_uitest.cc
+++ chrome/browser/ui/webui/searchbox/webui_omnibox_interactive_uitest.cc
@@ -224,7 +224,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxWebUiInteractiveTest,
       WaitForVerbatimMatch(kClassicPopupWebView, kClassicMatchText, "@gemini"),
       // Clicking the top match should navigate to a Google search results page.
       InSameContext(ClickElement(kClassicPopupWebView, kClassicMatch)),
-      WaitForGoogleSearch(kNewTab, {{"q", "@gemini"}, {"oq", "@gemini"}}));
+      WaitForGoogleSearch(kNewTab, "%40gemini&oq=%40gemini"));
 }
 
 // TODO(crbug.com/496926191): Interactive tests involving verbatim matches are
@@ -266,7 +266,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxWebUiInteractiveTest,
 //                  .SetMustRemainVisible(false))),
 //          Else(SendKeyPress(kOmniboxElementId, ui::VKEY_RETURN))),
 //       // Ensure google search occurs.
-//       WaitForGoogleSearch(kNewTab, {{"q", "a"}}));
+//       WaitForGoogleSearch(kNewTab, "a"));
 // }
 // Ensures that the entrypoint is not shown in the popup whenever the AIM popup
 // feature is disabled.
@@ -513,7 +513,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxAimWebUiInteractiveTest,
       // Trigger a search.
       InputAimPopupText("foo"),
       InSameContext(ClickElement(kAimPopupWebView, kAimSubmit)),
-      WaitForGoogleSearch(kNewTab, {{"q", "foo"}}),
+      WaitForGoogleSearch(kNewTab, "foo"),
       // Verify tab has focus and not the location bar.
       CheckJsResult(kNewTab, "() => document.hasFocus()", true),
       CheckViewProperty(kOmniboxElementId, &views::View::HasFocus, false));
@@ -589,7 +589,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxAimWebUiInteractiveTest, QueryWithTabContext) {
       InSameContext(ClickElement(kAimPopupWebView, kAimSubmit)),
 
       // 7. Verify navigation to Google Search with the query.
-      WaitForGoogleSearch(kNewTab, {{"q", "foo"}}));
+      WaitForGoogleSearch(kNewTab, "foo"));
 }
 
 IN_PROC_BROWSER_TEST_F(OmniboxAimWebUiInteractiveTest, TextTransfersOnDismiss) {
@@ -687,5 +687,5 @@ IN_PROC_BROWSER_TEST_P(OmniboxAimSearchFulfillmentTest,
                         SendKeyPress(kOmniboxElementId, ui::VKEY_RETURN))
                   : InSameContext(ClickElement(kAimPopupWebView, kAimSubmit)))),
       // Ensure tab navigates to a Google search results page.
-      WaitForGoogleSearch(kNewTab, {{"q", query}}));
+      WaitForGoogleSearch(kNewTab, query));
 }
