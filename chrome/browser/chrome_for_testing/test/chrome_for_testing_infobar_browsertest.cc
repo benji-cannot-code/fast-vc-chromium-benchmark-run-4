ec1509765fe4b58f93c5708fedce31e4697610a4
Peter Kvitek <kvitekp@chromium.org>
2026-04-10 13:22:42 -0700

    [cft] Move Chrome for Testing infobar tests to a subdir
    This CL relocates existing Chrome for Testing infobar tests to the test
    subdirectory where all other CfT browser tests live.
    
    It also removes direct reference to CfT infobar tests from /c/t/BUILD.gn
    so that the main test build file refers only to
    chrome_for_testing:browser_tests target.
    
    Change-Id: I32e40218c47d6fdab0b54dd3d3240183ead659d7
    Bypass-Check-License: Moving existing browser test files.
    Cq-Include-Trybots: luci.chromium.try:linux-rel-cft,mac-rel-cft,win-rel-cft
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747071
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613062}

--- chrome/browser/chrome_for_testing/test/chrome_for_testing_infobar_browsertest.cc
+++ chrome/browser/chrome_for_testing/test/chrome_for_testing_infobar_browsertest.cc
@@ -21,8 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace infobars {
+using infobars::ContentInfoBarManager;
+using infobars::InfoBarDelegate;
 
+namespace chrome_for_testing {
 namespace {
 
 // TODO (crbug.com/469533286): fix ChromeForTestingInfoBarTest for win-cft
@@ -185,5 +187,4 @@ IN_PROC_BROWSER_TEST_F(ChromeForTestingInfoBarDisabledTest,
 }
 
 }  // namespace
-
-}  // namespace infobars
+}  // namespace chrome_for_testing
