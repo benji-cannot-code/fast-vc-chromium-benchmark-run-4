8df66908eb9b03b887ea8993e74ad0e0f360ff94
Madhav Pruthi <madhavpruthi@google.com>
2026-04-10 13:52:23 -0700

    [Clank IA] Update StatusView visibility logic in StatusMediator when permissions icons are supposed to be shown
    Demo: http://screencast/cast/NTQyMDM0MjgzODI5NjU3NnxjMzZhNTBhNS01YQ
    
    Change-Id: Ie9794d0f931fe159482a7760b259a081eaa57b28
    Bug: 497894910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727795
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613078}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
@@ -446,6 +446,8 @@ public class StatusMediator
         mModel.set(StatusProperties.STATUS_ICON_RESOURCE, icon);
         mModel.set(StatusProperties.STATUS_ICON_DESCRIPTION_RES, icon.getContentDescriptionRes());
         mModel.set(StatusProperties.STATUS_CLICK_LISTENER, this::onClickOpenPageInfo);
+
+        updateStatusViewVisibility();
     }
 
     /** Update selection of icon presented on the location bar. */
@@ -489,9 +491,8 @@ public class StatusMediator
         } else if (mPermissionStatusHandler.isClapperQuietIconShowing()) {
             return;
         } else if (mSecurityIconRes != 0) {
-            if (isPageInfoMovedToAppMenu()
-                    && mPageSecurityLevel == ConnectionSecurityLevel.SECURE
-                    && !mShowStatusIconForSecureOrigins) {
+            if (mPageSecurityLevel == ConnectionSecurityLevel.SECURE
+                    && (isPageInfoMovedToAppMenu() || !mShowStatusIconForSecureOrigins)) {
                 mIsSecurityViewShown = false;
             } else {
                 mIsSecurityViewShown = true;
@@ -517,6 +518,8 @@ public class StatusMediator
                 StatusProperties.STATUS_ACCESSIBILITY_DOUBLE_TAP_DESCRIPTION_RES,
                 doubleTapDescriptionRes);
         mModel.set(StatusProperties.STATUS_CLICK_LISTENER, clickListener);
+
+        updateStatusViewVisibility();
     }
 
     void onFuseboxStateChanged(int state) {
@@ -683,6 +686,8 @@ public class StatusMediator
     public void resetCustomIconsStatus() {
         mPermissionStatusHandler.reset(/* shouldDismissNativePrompt= */ true);
         resetEmbeddedIconHandlers();
+
+        updateLocationBarIcon(IconTransitionType.CROSSFADE);
     }
 
     private void openPageInfo(Tab tab) {
@@ -797,7 +802,7 @@ public class StatusMediator
     private void applyStatusIconAndTooltipProperties(
             boolean showIcon, boolean verboseStatusTextVisible) {
         mModel.set(StatusProperties.SHOW_STATUS_ICON, showIcon);
-        if (showIcon && !isHubSearch()) {
+        if ((showIcon || verboseStatusTextVisible) && !isHubSearch()) {
             Drawable background;
             if (mLocationBarDataProvider.isIncognitoBranded()) {
                 background =
@@ -849,8 +854,14 @@ public class StatusMediator
                 mUrlHasFocus
                         || isHubSearch()
                         || mShowStatusIconForSecureOrigins
-                        || mPageSecurityLevel != ConnectionSecurityLevel.SECURE
+                        || mIsSecurityViewShown
+                        || hasStatusIconResource()
+                        || shouldShowVerboseStatusText()
                         || mIsStoreIconShowing);
+    }
+
+    private boolean hasStatusIconResource() {
+        return mModel.get(StatusProperties.STATUS_ICON_RESOURCE) != null;
     }
 
     private void onClickOpenPageInfo(View view) {
