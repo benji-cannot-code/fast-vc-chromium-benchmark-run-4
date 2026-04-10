5dfa20cb92a7080e2c2ba04786d81bc5f378cfaa
Grace Cham <hscham@google.com>
2026-04-10 07:22:45 -0700

    [Media Capture] refine onTabContentUpdated triggers
    On logging, onUrlUpdated and onDidFinishNavigationInPrimaryMainFrame
    often comes in succession, triggering too many thumbnail fetching calls.
    OTOH, with only onUrlUpdated (sent on navigation started), the captured
    thumbnail may be of a page not fully loaded.
    Add onPageLoadFinished to catch that.
    
    The internal side will add some debouncing logic so that if navigation
    start and page load finish comes in quick succession it will only
    trigger thumbnail capture once.
    
    Bug: 495616328
    Test: manual: new UI thumbnail updated with acceptable rate (to users)
    Change-Id: I2954892d162101289a0cd519840212916a05876d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725301
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612846}

--- chrome/android/java/src/org/chromium/chrome/browser/media/MediaCapturePickerTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/MediaCapturePickerTabObserver.java
@@ -15,7 +15,6 @@ import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.browser.ui.native_page.NativePage;
-import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.url.GURL;
 
@@ -96,8 +95,7 @@ public class MediaCapturePickerTabObserver implements AllTabObserver.Observer {
                 }
 
                 @Override
-                public void onDidFinishNavigationInPrimaryMainFrame(
-                        Tab tab, NavigationHandle navigation) {
+                public void onPageLoadFinished(Tab tab, GURL url) {
                     maybeUpdatePickableTab(tab, mObserverDelegate::onTabContentUpdated);
                 }
             };
