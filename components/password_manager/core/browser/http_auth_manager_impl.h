f275ce78223c6dad62981a95c77c3fc43ec600c7
Viktor Semeniuk <vsemeniuk@google.com>
2026-04-10 07:48:08 -0700

    Verify origin before filling the form during http auth
    This CL adds verification for origin before performing filling password
    after biometric authentication. Also if SetObserverAndDeliverCredentials
    is invoked any ongoing authentication is canceled.
    
    Fixed: 497831111
    Change-Id: I72e03e523c6641f9fe1c58b4b0a8f24c63d91a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748326
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612861}

--- components/password_manager/core/browser/http_auth_manager_impl.h
+++ components/password_manager/core/browser/http_auth_manager_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
 #include "components/password_manager/core/browser/http_auth_manager.h"
 #include "components/password_manager/core/browser/http_auth_observer.h"
+#include "url/origin.h"
 
 namespace device_reauth {
 class DeviceAuthenticator;
@@ -69,6 +70,7 @@ class HttpAuthManagerImpl : public HttpAuthManager {
 
   void OnReauthCompleted(const std::u16string& username,
                          const std::u16string& password,
+                         const url::Origin& credential_origin,
                          base::OnceClosure on_filling_complete,
                          bool auth_result);
 
@@ -84,6 +86,9 @@ class HttpAuthManagerImpl : public HttpAuthManager {
   // When set to true, the password form has been dismissed and |form_manager_|
   // will be cleared on next navigation.
   bool form_dismissed_ = false;
+
+  // The origin of the observed form.
+  url::Origin observed_origin_;
 
   // The authenticator used to trigger a biometric re-auth before filling.
   std::unique_ptr<device_reauth::DeviceAuthenticator> authenticator_;
