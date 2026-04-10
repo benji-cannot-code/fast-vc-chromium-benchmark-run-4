7cec5b1ed81f6bfe1650a41488fc15d51904c955
S. Ganesh <ganesh@chromium.org>
2026-04-10 13:21:23 -0700

    updater: implement high DPI scaling for the UI
    This CL optimizes the updater UI to resolve previous Windows scaling
    issues. The interface now renders at native resolution, providing a much
    sharper and more professional appearance.
    
    Bug: 409590312
    Change-Id: I44c483256ea9f55add16f01a245c69cf6c02f768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747030
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613061}

--- chrome/updater/win/ui/owner_draw_controls.h
+++ chrome/updater/win/ui/owner_draw_controls.h
@@ -150,6 +150,7 @@ class OwnerDrawTitleBarWindow : public CWindowImpl<OwnerDrawTitleBarWindow> {
     MESSAGE_HANDLER(WM_LBUTTONDOWN, OnLButtonDown)
     MESSAGE_HANDLER(WM_LBUTTONUP, OnLButtonUp)
     MESSAGE_HANDLER(WM_ERASEBKGND, OnEraseBkgnd)
+    MESSAGE_HANDLER(WM_SIZE, OnSize)
     COMMAND_ID_HANDLER(kButtonClose, OnClose)
     COMMAND_ID_HANDLER(kButtonMaximize, OnMaximize)
     COMMAND_ID_HANDLER(kButtonMinimize, OnMinimize)
@@ -199,6 +200,7 @@ class OwnerDrawTitleBarWindow : public CWindowImpl<OwnerDrawTitleBarWindow> {
                        WPARAM wparam,
                        LPARAM lparam,
                        BOOL& handled);  // NOLINT
+  LRESULT OnSize(UINT, WPARAM, LPARAM, BOOL& handled);  // NOLINT
   LRESULT OnClose(WORD notify_code,
                   WORD id,
                   HWND hwnd_ctrl,
@@ -230,7 +232,7 @@ class OwnerDrawTitleBar {
                                HWND title_bar_spacer_hwnd,
                                COLORREF bk_color);
 
-  void RecalcLayout();
+  void RecalcLayout(HWND parent_hwnd, HWND title_bar_spacer_hwnd);
 
   BEGIN_MSG_MAP(OwnerDrawTitleBar)
   END_MSG_MAP()
