1dbd336ba628ef1dbbb9f3346b6557b4122cdb72
Ernesto Izquierdo Clua <eic@google.com>
2026-04-10 06:15:14 -0700

    [ios] Introduce DataProtectionSceneAgent skeleton.
    This CL introduces the foundational boilerplate for the
    DataProtectionSceneAgent. This agent will be responsible for
    managing enterprise-specific screenshot protection at the scene level.
    
    This is part 1 of a multi-part feature. Subsequent CLs will
    incrementally add the actual protection logic, including observing
    preference changes, rules services, Tab Grid visibility, and
    active WebState changes.
    
    Bug: 485585995
    Change-Id: I0bea153afaa019f72cb20d95f5e5e41a7672d59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7742229
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612803}

---
+++ ios/chrome/browser/enterprise/data_protection/model/data_protection_scene_agent.mm
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/data_protection/model/data_protection_scene_agent.h"
+
+@implementation DataProtectionSceneAgent
+
+#pragma mark - ObservingSceneAgent
+
+- (void)setSceneState:(SceneState*)sceneState {
+  [super setSceneState:sceneState];
+}
+
+@end
