725d3cd670ed4536aa5933a4b886ea850c6ff7e9
Steven Luong <stluong@chromium.org>
2026-04-10 16:47:55 -0700

    [TabAlert] Stop notifying intermediate state on audible state change
    A tab can have the kAudioPlaying and kAudioMuting alert active. When the
    tab stops playing audio, the TabAlertController currently fires two
    alert notifications that the alert to show has changed: the first goes
    from kAudioMuting to kAudioPlaying then fires again when we go from
    kAudioPlaying to no alerts being active.
    
    This CL updates the TabAlertController so that only a single notification
    is triggered when recently audible state changes so clients won't receive the
    intermediate step. Instead, clients will receive a notification to go
    from the kAudioMuting alert to no active alerts.
    
    Bug: none
    Change-Id: I3f3e83ebe842b371448674eb1758bcb565d1ad7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749864
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: Steven Luong <stluong@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613174}

--- chrome/browser/ui/tabs/alert/tab_alert_controller.h
+++ chrome/browser/ui/tabs/alert/tab_alert_controller.h
@@ -106,6 +106,8 @@ class TabAlertController : public tabs::ContentsObservingTabFeature,
   void OnIsContentDisplayedInHeadsetChanged(bool state) override;
 
  private:
+  class ScopedAlertNotifier;
+
   void OnGlicSharingStateChange(bool is_sharing);
   void OnGlicAccessingStateChange(bool is_accessing);
 
