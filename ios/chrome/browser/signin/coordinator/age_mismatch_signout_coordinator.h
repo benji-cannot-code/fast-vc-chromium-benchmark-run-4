8de09ee5a11dfcb3093176aad4109ed8db50bcc2
Jérôme Lebel <jlebel@chromium.org>
2026-04-10 05:41:42 -0700

    [iOS] Move sign-in command out of AgeMismatchSignoutCoordinator
    AgeMismatchSignoutCoordinator should not send the sign-in command.
    This change will help future changes to use this coordinator in other
    places like in the sign-in bottom sheet or AuthenticationFlow.
    
    In following patch, ageMismatchSignoutCoordinatorWantsToBeStopped will
    be replaced by ageMismatchSignoutCoordinatorWantsToSignOut.
    
    Bug: 484261211
    Change-Id: I1f7ebde78759ca07ca38eb7f2d6de9aca18bec61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735055
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612791}

--- ios/chrome/browser/signin/coordinator/age_mismatch_signout_coordinator.h
+++ ios/chrome/browser/signin/coordinator/age_mismatch_signout_coordinator.h
@@ -19,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)ageMismatchSignoutCoordinatorWantsToBeStopped:
     (AgeMismatchSignoutCoordinator*)coordinator;
 
+// Called when the user wants to sign in. The view controller is not dismissed
+// when this method is called.
+- (void)ageMismatchSignoutCoordinatorWantsToSignIn:
+    (AgeMismatchSignoutCoordinator*)coordinator;
+
 @end
 
 // Coordinator for the Age Mismatch Prompt.
