46595e9d1a0667d1c541423f942dda0ddc9fed9a
Jérôme Lebel <jlebel@chromium.org>
2026-04-10 08:51:55 -0700

    [iOS] Move learn more into age_mismatch_signout/age_mismatch_learn_more
    Move:
    ios/chrome/browser/authentication/age_mismatch_learn_more/
    to:
    ios/chrome/browser/authentication/age_mismatch_signout/age_mismatch_learn_more/
    
    Change-Id: I032df08bcdd46e5728d94020053c30aa3fc5331a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748608
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612900}

--- ios/chrome/browser/authentication/age_mismatch_signout/age_mismatch_learn_more/ui/age_mismatch_learn_more_view_controller.mm
+++ ios/chrome/browser/authentication/age_mismatch_signout/age_mismatch_learn_more/ui/age_mismatch_learn_more_view_controller.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/authentication/age_mismatch_learn_more/ui/age_mismatch_learn_more_view_controller.h"
+#import "ios/chrome/browser/authentication/age_mismatch_signout/age_mismatch_learn_more/ui/age_mismatch_learn_more_view_controller.h"
 
 #import <WebKit/WebKit.h>
 
--- ios/chrome/browser/authentication/age_mismatch_signout/coordinator/age_mismatch_signout_coordinator.mm
+++ ios/chrome/browser/authentication/age_mismatch_signout/coordinator/age_mismatch_signout_coordinator.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/age_mismatch_signout/coordinator/age_mismatch_signout_coordinator.h"
 
 #import "base/not_fatal_until.h"
-#import "ios/chrome/browser/authentication/age_mismatch_learn_more/coordinator/age_mismatch_learn_more_coordinator.h"
+#import "ios/chrome/browser/authentication/age_mismatch_signout/age_mismatch_learn_more/coordinator/age_mismatch_learn_more_coordinator.h"
 #import "ios/chrome/browser/authentication/age_mismatch_signout/coordinator/age_mismatch_signout_mediator.h"
 #import "ios/chrome/browser/authentication/age_mismatch_signout/ui/age_mismatch_signout_view_controller.h"
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_constants.h"
