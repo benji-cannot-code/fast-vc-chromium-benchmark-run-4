767d62551ee547e4dd4ea54e775cbcfe7dd67961
Kurt Catti-Schmidt <kschmi@microsoft.com>
2026-04-09 21:19:13 -0700

    [CSS Modules] Add origin_trial_feature_name
    The OT gates have been approved via Chromestatus[1], so this change
    prepares the feature for an OT.
    
    [1] https://chromestatus.com/feature/4790543041298432
    
    Bug: 448174611
    Change-Id: Ia99532932d4545b7a860ac310c30ee171085c02a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7739305
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1612599}

--- third_party/blink/renderer/core/dom/shadow_root.cc
+++ third_party/blink/renderer/core/dom/shadow_root.cc
@@ -382,7 +382,8 @@ class PendingModuleEntry final : public SingleModuleClient {
 
 HeapVector<Member<CSSStyleSheet>> ShadowRoot::ResolveAdoptedStyleSheets(
     const AtomicString& shadowrootadoptedstylesheets_attribute_value) {
-  CHECK(RuntimeEnabledFeatures::ShadowRootAdoptedStyleSheetEnabled());
+  CHECK(RuntimeEnabledFeatures::ShadowRootAdoptedStyleSheetEnabled(
+      GetDocument().GetExecutionContext()));
 
   // Early exit if `domWindow` isn't available. This won't work in contexts such
   // as `Document.parseHTMLUnsafe`. This is probably fine, as adopted
@@ -470,7 +471,8 @@ HeapVector<Member<CSSStyleSheet>> ShadowRoot::ResolveAdoptedStyleSheets(
 
 void ShadowRoot::ProcessAdoptedStylesheetAttribute(
     AtomicString value) {
-  CHECK(RuntimeEnabledFeatures::ShadowRootAdoptedStyleSheetEnabled());
+  CHECK(RuntimeEnabledFeatures::ShadowRootAdoptedStyleSheetEnabled(
+      GetDocument().GetExecutionContext()));
   if (!value.empty()) {
     AppendAdoptedStyleSheets(ResolveAdoptedStyleSheets(value));
   }
