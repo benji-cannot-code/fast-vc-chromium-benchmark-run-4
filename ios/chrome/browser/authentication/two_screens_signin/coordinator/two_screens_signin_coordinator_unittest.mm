4355e07e3e87742fa698749164253ce2b80d4158
Arthur Milchior <arthurmilchior@google.com>
2026-04-10 01:58:36 -0700

    [iOS]Move two screen signin out of ui_bundled
    Bypass-Check-License: Moved files
    Bug: None
    Change-Id: Ibf9da6eaef4309afd7f3f89e984499d77d09e9c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7147343
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612697}

--- ios/chrome/browser/authentication/two_screens_signin/coordinator/two_screens_signin_coordinator_unittest.mm
+++ ios/chrome/browser/authentication/two_screens_signin/coordinator/two_screens_signin_coordinator_unittest.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/authentication/ui_bundled/signin/two_screens_signin/two_screens_signin_coordinator.h"
+#import "ios/chrome/browser/authentication/two_screens_signin/coordinator/two_screens_signin_coordinator.h"
 
 #import <UIKit/UIKit.h>
 
