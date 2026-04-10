7abef53a7bca06a062c78f34fed3bd4aab4adc85
Morten Stenshorne <mstensho@chromium.org>
2026-04-10 01:12:58 -0700

    Include what you need in assorted layout/inline/ headers.
    Change-Id: Icd5e94d03d7ab0289273fd0dcb3d5b4e1e90cec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7741723
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612682}

--- third_party/blink/renderer/core/layout/inline/line_widths.cc
+++ third_party/blink/renderer/core/layout/inline/line_widths.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/inline/fit_text_scale.h"
 #include "third_party/blink/renderer/core/layout/inline/inline_box_state.h"
+#include "third_party/blink/renderer/core/layout/inline/inline_break_token.h"
 #include "third_party/blink/renderer/core/layout/inline/inline_node.h"
 #include "third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h"
 
