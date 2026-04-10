649f23f6bb30cd41ca5f2b7a0df720bdd6d87fcb
Sophie Chang <sophiechang@chromium.org>
2026-04-10 11:13:40 -0700

    Do not show cue if any feature promo is showing
    Bug: 500927915
    Change-Id: Icf8c7c78377feffb264ad29c88c6deaea3ac7d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7740482
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612988}

--- chrome/browser/ui/views/user_education/impl/browser_user_education_interface_impl.h
+++ chrome/browser/ui/views/user_education/impl/browser_user_education_interface_impl.h
@@ -32,6 +32,7 @@ class BrowserUserEducationInterfaceImpl : public BrowserUserEducationInterface {
   // BrowserUserEducationInterface:
   bool IsFeaturePromoQueued(const base::Feature& iph_feature) const override;
   bool IsFeaturePromoActive(const base::Feature& iph_feature) const override;
+  bool IsAnyFeaturePromoActive() const override;
   bool HasFeaturePromoBeenDismissed(
       const base::Feature& iph_feature) const override;
   bool MaybeShowFeaturePromo(
