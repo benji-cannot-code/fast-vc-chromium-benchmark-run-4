b68ba3b7eef9ee97628d4e93eca1dca13ea72099
Fiaz Muhammad <mfiaz@google.com>
2026-04-10 07:10:36 -0700

    [TabBottomSheet] Dynamic Resizing for Tab Bottom Sheet
    - Completely flag-guard resizing logic.
    - When enabled, allow for resizing the WebView container.
    - Lock the minimum height for the bottom sheet to the default
            height ratio (normally 70%).
    
    Bug: 486916366
    Change-Id: I2788fb88a5930f80f85cc26650d3d4a1ab7381bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745671
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612832}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
@@ -14,6 +14,7 @@ import android.view.View;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.SheetState;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.StateChangeReason;
@@ -328,7 +329,20 @@ public class TabBottomSheetCoordinator {
                     mBottomSheetController.collapseSheet(/* animate= */ true);
                     mExpectingLayoutChange = false;
                 }
-                updateResizingStateWithFixedHeight();
+                if (!ChromeFeatureList.sTabBottomSheetResizeWebview.getValue()) {
+                    updateResizingStateWithFixedHeight();
+                }
+            }
+
+            @Override
+            public void onSheetOffsetChanged(float heightFraction, float offsetPx) {
+                if (ChromeFeatureList.sTabBottomSheetResizeWebview.getValue()) {
+                    mMediator.updateResizingState(
+                            getDefaultHeightRatio(),
+                            heightFraction,
+                            (int) offsetPx,
+                            mBottomSheetController.getMaxOffset());
+                }
             }
         };
     }
