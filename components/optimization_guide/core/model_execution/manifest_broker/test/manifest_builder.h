234ad7ec872d7924b880557bee4aeac434a1c37d
Steven Holte <holte@google.com>
2026-04-10 10:28:47 -0700

    Add ManifestBrokerState tests
    Bug: 489510258
    Change-Id: I62bdc910e450872b8f6da75e9a66757c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745673
    Auto-Submit: Steven Holte <holte@chromium.org>
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Reviewed-by: Jingyun Liu <jingyun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612955}

--- components/optimization_guide/core/model_execution/manifest_broker/test/manifest_builder.h
+++ components/optimization_guide/core/model_execution/manifest_broker/test/manifest_builder.h
@@ -111,6 +111,9 @@ class ManifestComponentDirectory {
   explicit ManifestComponentDirectory(const proto::Manifest& manifest);
   ~ManifestComponentDirectory();
 
+  // Replaces the manifest in the directory.
+  ManifestComponentDirectory& Add(const proto::Manifest& manifest);
+  // Adds a new solution config to the directory, overwriting existing ones.
   ManifestComponentDirectory& Add(const std::string& filename,
                                   const proto::SolutionConfig& config);
 
