ddc5ff685ae1a7cfda395a2a1996f89e1e4cfda6
Mariam Ali <alimariam@google.com>
2026-04-10 07:49:52 -0700

    [a11y][ffr] Implement WebUI layout.
    This change adds basic styling, buttons, and placeholder content.
    Screenshot: http://screen/9RnbDk6Ybs5bbXY
    
    Bug: b:488321731
    Change-Id: Ie51913df85a6050a4b23defc27ac3fcd6929ae1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748745
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612863}

--- chrome/browser/resources/accessibility_annotator_info/accessibility_annotator_info.ts
+++ chrome/browser/resources/accessibility_annotator_info/accessibility_annotator_info.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
 import {getCss} from './accessibility_annotator_info.css.js';
@@ -19,6 +21,16 @@ export class AccessibilityAnnotatorInfoElement extends CrLitElement {
 
   override render() {
     return getHtml.bind(this)();
+  }
+
+  protected onManageSettingsClick_() {
+    // Handle the manage settings click event here.
+    // TODO(crbug.com/488321731): Connect this with handler via proxy.
+  }
+
+  protected onGotItClick_() {
+    // Handle the got it click event here.
+    // TODO(crbug.com/488321731): Connect this with handler via proxy.
   }
 }
 
