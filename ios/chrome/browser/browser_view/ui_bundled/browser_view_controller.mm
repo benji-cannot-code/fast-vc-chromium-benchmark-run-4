f7fc9d7f5ccc96a1afedb9262c21e5dd191fec30
Scott Yoder <scottyoder@google.com>
2026-04-10 04:39:06 -0700

    [ios] Clear pointer to FullscreenBrowserAgent
    This change clears the BVC's pointer to the FullscreenBrowserAgent to
    avoid it dangling on shutdown.
    
    Change-Id: I1e091781cd764a7bef729b7876504f2b64db54be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745505
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612766}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -807,6 +807,7 @@ const CGFloat kTopDynamicIslandInset = 24;
   _tabUsageRecorderBrowserAgent = nullptr;
   _snapshotBrowserAgent = nullptr;
   _fullscreenBrowserAgentObserverBridge = nullptr;
+  _fullscreenBrowserAgent = nullptr;
 }
 
 #pragma mark - UIAccessibilityAction
