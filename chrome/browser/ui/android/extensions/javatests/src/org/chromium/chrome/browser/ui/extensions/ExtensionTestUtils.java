dbd881d7b07a07f243b04142eb571524dd3a0a0d
Masa Fujita <massan@google.com>
2026-04-10 11:58:05 -0700

    extensions: Add instrumentation tests for drag reordering action buttons
    This patch adds instrumentation test for drag reordering, making sure
    that reordering properly modifies the `ToolbarActionsModel`.
    
    Bug: 459079173
    Change-Id: I1e016295d0e5c8381bac5f6ff9fd2a0fec9af63b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747645
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613017}

--- chrome/browser/ui/android/extensions/javatests/src/org/chromium/chrome/browser/ui/extensions/ExtensionTestUtils.java
+++ chrome/browser/ui/android/extensions/javatests/src/org/chromium/chrome/browser/ui/extensions/ExtensionTestUtils.java
@@ -116,6 +116,17 @@ public class ExtensionTestUtils {
     }
 
     /**
+     * Returns the list of pinned actions.
+     *
+     * @param profile The profile that the extension belongs to.
+     * @return The list of IDs of pinned actions.
+     */
+    public static String[] getPinnedActionIds(Profile profile) {
+        return ThreadUtils.runOnUiThreadBlocking(
+                () -> ExtensionTestUtilsJni.get().getPinnedActionIds(profile));
+    }
+
+    /**
      * Returns the number of active RenderFrameHosts for the given extension.
      *
      * @param profile The profile the extension belongs to.
@@ -312,6 +323,9 @@ public class ExtensionTestUtils {
                 @JniType("Profile*") Profile profile,
                 @JniType("std::string") String extensionId,
                 boolean visible);
+
+        @JniType("std::vector<std::string>")
+        String[] getPinnedActionIds(@JniType("Profile*") Profile profile);
 
         int getRenderFrameHostCount(
                 @JniType("Profile*") Profile profile, @JniType("std::string") String extensionId);
