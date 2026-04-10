64ad750f97bc685dd182a84da862920998f207a0
Richard Chui <richui@google.com>
2026-04-10 16:42:30 -0700

    [ALPC Extensions]: Record missing "SupervisedUsers.ExtensionEnablement" metric
    Port over the final missing families UMA metric for Clank on AL. This brings parity to the ChromeOS flow for recording the missing `kFailedToEnable`. The other values should all be recorded normally through the extension installation flow.
    
    Bug: 462774360
    Change-Id: I89a71fb670956407d930c3ceda10eda763fe6149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749761
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Richard Chui <richui@chromium.org>
    Commit-Queue: Richard Chui <richui@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613172}

--- chrome/browser/extensions/api/webstore_private/webstore_private_apitest.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_apitest.cc
@@ -926,6 +926,16 @@ IN_PROC_BROWSER_TEST_F(
       SupervisedUserExtensionsMetricsRecorder::kAskParentDialogHistogramName,
       SupervisedUserExtensionsMetricsRecorder::AskParentDialogState::kApproved,
       1);
+
+  // Verify the Enablement metrics.
+  EXPECT_EQ(
+      1,
+      user_action_tester.GetActionCount(
+          SupervisedUserExtensionsMetricsRecorder::kFailedToEnableActionName));
+  histogram_tester.ExpectBucketCount(
+      SupervisedUserExtensionsMetricsRecorder::kEnablementHistogramName,
+      SupervisedUserExtensionsMetricsRecorder::EnablementState::kFailedToEnable,
+      1);
 }
 
 // Tests that the parent approval install dialog is shown when the parent
@@ -994,6 +1004,16 @@ IN_PROC_BROWSER_TEST_F(SupervisedUserExtensionWebstorePrivateApiTestAndroid,
           kExtensionInstallDialogHistogramName,
       SupervisedUserExtensionsMetricsRecorder::ExtensionInstallDialogState::
           kChildCanceled,
+      1);
+
+  // Verify the Enablement metrics.
+  EXPECT_EQ(
+      1,
+      user_action_tester.GetActionCount(
+          SupervisedUserExtensionsMetricsRecorder::kFailedToEnableActionName));
+  histogram_tester.ExpectBucketCount(
+      SupervisedUserExtensionsMetricsRecorder::kEnablementHistogramName,
+      SupervisedUserExtensionsMetricsRecorder::EnablementState::kFailedToEnable,
       1);
 }
 
