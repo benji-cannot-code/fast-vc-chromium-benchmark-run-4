3a5ce76d6ae89e15c1fe747cd8977a10792f1dc9
Colin Blundell <blundell@chromium.org>
2026-04-10 00:06:50 -0700

    [Blink] Move up IsValid() call in DoExternalDrawAndSnapshot()
    This method is here [1]. It is nonrecoverable, i.e. once it goes to
    false on a given instance it will stay false on that instance. Also,
    invoking the draw callback on the instance's recording canvas cannot
    cause it to flip from true to false. Hence, we can move this
    short-circuit above the invocation of the draw callback.
    
    This has the benefit of ensuring that every draw callback here is
    immediately followed by a FlushCanvas(), which will help us reason about
    decoupling this class from holding the recorder as an ivar down the
    line.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=990-996;drc=15be50893f87a32a6a12adea5051b701414e932f?q=DoExternalDrawAndSn&ss=chromium
    
    Bug: 352263194
    Change-Id: I55e0b3d4b8b69ca551f12fac323c302645d1617a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7743365
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612659}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1070,12 +1070,12 @@ CanvasNon2DResourceProviderSharedImage::DoExternalDrawAndSnapshot(
     ImageOrientation orientation,
     bool is_overwrite) {
   cached_snapshot_.reset();
-  draw_callback(recorder_->getRecordingCanvas());
 
   if (!IsValid()) {
     return nullptr;
   }
 
+  draw_callback(recorder_->getRecordingCanvas());
   FlushCanvas(is_overwrite);
   return Snapshot(orientation);
 }
