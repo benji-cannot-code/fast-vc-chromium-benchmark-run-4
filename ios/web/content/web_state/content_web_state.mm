11a3851ae166a3181c12244b960812a48f8ed428
Gyuyoung Kim <gyuyoung@igalia.com>
2026-04-10 17:42:40 -0700

    [iOS Blink] Add dummy FooSetCustomOpenPanelSupported to ContentWebState
    https://crrev.com/c/7736028 implemented IsCustomOpenPanelSupported and
    SetCustomOpenPanelSupported method in WebState.h, but it missed adding
    them to ContentWebState. This CL adds them to ContentWebState to fix the
    build break on the iOS Blink port.
    
    Bug: 500706378
    Change-Id: I687370f4e6cb99feaa789638fb7f107962625672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747286
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613199}

--- ios/web/content/web_state/content_web_state.mm
+++ ios/web/content/web_state/content_web_state.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "base/functional/callback_helpers.h"
+#import "base/notimplemented.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
 #import "components/embedder_support/ios/delegate/color_chooser/color_chooser_ios.h"
@@ -568,6 +569,15 @@ id<CRWFindInteraction> ContentWebState::GetFindInteraction() {
 
 id ContentWebState::GetActivityItem() {
   return nil;
+}
+
+bool ContentWebState::IsCustomOpenPanelSupported() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void ContentWebState::SetCustomOpenPanelSupported(bool supports) {
+  NOTIMPLEMENTED();
 }
 
 UIColor* ContentWebState::GetThemeColor() {
