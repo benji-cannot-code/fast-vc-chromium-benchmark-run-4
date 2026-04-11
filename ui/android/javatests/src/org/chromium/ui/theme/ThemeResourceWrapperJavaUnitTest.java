12ccfa59466a051155e8fcfa19158d12a3425462
Wenyu Fu <wenyufu@chromium.org>
2026-04-10 17:00:29 -0700

    [Gardener] Disable ThemeResourceWrapperJavaUnitTest
    Bug: 499024053
    Change-Id: I7be88b0de2082fe0e62587e40a1e779264afb526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748946
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613178}

--- ui/android/javatests/src/org/chromium/ui/theme/ThemeResourceWrapperJavaUnitTest.java
+++ ui/android/javatests/src/org/chromium/ui/theme/ThemeResourceWrapperJavaUnitTest.java
@@ -29,13 +29,13 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.ui.R;
-import org.chromium.ui.base.DeviceFormFactor;
 
 /** Java test show case how to use the wrapper. */
 @Batch(Batch.PER_CLASS)
 @RunWith(BaseJUnit4ClassRunner.class)
+@DisabledTest(message = "crbug.com/499024053")
 public class ThemeResourceWrapperJavaUnitTest {
 
     @Rule
@@ -56,7 +56,6 @@ public class ThemeResourceWrapperJavaUnitTest {
 
     @Test
     @SmallTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // https://crbug.com/499024053
     public void setThemeForOriginalActivity() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -75,7 +74,6 @@ public class ThemeResourceWrapperJavaUnitTest {
 
     @Test
     @SmallTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // https://crbug.com/499024053
     public void setThemeNoOpWithThemeResourceWrapper() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -101,7 +99,6 @@ public class ThemeResourceWrapperJavaUnitTest {
 
     @Test
     @SmallTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // https://crbug.com/499024053
     public void themeWrapperNoImpactOnOtherAttribute() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
