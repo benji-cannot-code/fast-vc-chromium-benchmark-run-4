5be1f4fb7baa645e781ea50e4843c207ca0f5f23
Calder Kitagawa <ckitagawa@chromium.org>
2026-04-13 09:14:29 -0700

    Revert "AppBannerManagerAndroid remove ScopedJavaGlobalWeakRef"
    This reverts commit db815f58caa934760fe8f3b900172f25df7fb0d4.
    
    Reason for revert: https://crbug.com/491972940
    
    Prospective revert out of an abundance of caution. That said,
    there is a longstanding CHECK failure that also requires further
    investigation.
    
    https://source.chromium.org/chromium/chromium/src/+/main:components/webapps/browser/android/app_banner_manager_android.cc;drc=0a39e93d5a632021d620eca8115ba583b8fc6b49;l=210
    
    Original change's description:
    > AppBannerManagerAndroid remove ScopedJavaGlobalWeakRef
    >
    > The C++ AppBannerManagerAndroid no longer holds a weak reference to the
    > Java object. Instead, it retrieves the Java object from a static map.
    >
    > Bug: 484619649
    > Change-Id: Iedec43f074d47440c51e548f07a5835c1a0b2164
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639530
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Auto-Submit: Dan Polanco <polardz@google.com>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Commit-Queue: Dan Polanco <polardz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1603797}
    
    Bug: 484619649
    Change-Id: Ia6daacec9b35d873bc6687c883fbcc937c737e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747771
    Reviewed-by: Dan Polanco <polardz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613776}

--- chrome/browser/banners/android/ambient_badge_manager_browsertest.cc
+++ chrome/browser/banners/android/ambient_badge_manager_browsertest.cc
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "url/gurl.h"
 
-using ::base::android::AttachCurrentThread;
-using ::base::android::ScopedJavaGlobalRef;
-using ::base::android::ScopedJavaLocalRef;
 using ::base::test::RunOnceCallback;
 using testing::_;
 
@@ -122,17 +119,11 @@ class TestAppBannerManager : public AppBannerManagerAndroid {
     ambient_badge_test_->WaitForState(target_badge_state_,
                                       std::move(on_badge_done_));
 
-    JNIEnv* env = AttachCurrentThread();
-    ScopedJavaLocalRef<jobject> native_java_app_data =
-        GetNativeJavaAppData(env);
-    auto global_native_java_app_data =
-        ScopedJavaGlobalRef<jobject>(env, native_java_app_data);
-
-    WebappInstallSource install_source = InstallableMetrics::GetInstallSource(
-        &GetWebContents(), InstallTrigger::AMBIENT_BADGE);
     std::unique_ptr<AddToHomescreenParams> a2hs_params =
         AppBannerManagerAndroid::CreateAddToHomescreenParams(
-            install_config, global_native_java_app_data, install_source);
+            install_config, native_java_app_data_for_testing(),
+            InstallableMetrics::GetInstallSource(
+                &GetWebContents(), InstallTrigger::AMBIENT_BADGE));
 
     ambient_badge_test_->MaybeShow(
         install_config.validated_url, install_config.GetWebOrNativeAppName(),
@@ -142,7 +133,7 @@ class TestAppBannerManager : public AppBannerManagerAndroid {
                        GetAndroidWeakPtr(), install_config),
         // Create the params, then pass them to MaybeShow.
         base::BindOnce(&AppBannerManagerAndroid::CreateAddToHomescreenParams,
-                       install_config, global_native_java_app_data)
+                       install_config, native_java_app_data_for_testing())
             .Then(base::BindOnce(
                 &PwaBottomSheetController::MaybeShow,
                 app_banner_manager()->web_contents(),
