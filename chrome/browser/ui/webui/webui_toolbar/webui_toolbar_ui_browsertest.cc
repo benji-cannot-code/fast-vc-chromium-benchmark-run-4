6ef3a8151a586711f85345f922d57dd399520b59
Matt Menke <mmenke@chromium.org>
2026-04-13 08:54:40 -0700

    WebUIToolbarWebView: Avoid unnecessary clone of state.
    WebUIToolbarWebView::PushNavigationState was cloning state, and then
    passing it down to ToolbarUIService, which would clone it again for
    each observer.
    
    Because of the second Clone(), the first one was unnecessary, so switch
    to passing it by const ref instead of cloning it.
    
    Bug: None
    Change-Id: I4d8d23c8f5465ac080b50fb50a9e1f0a07b09bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745184
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613759}

--- chrome/browser/ui/webui/webui_toolbar/webui_toolbar_ui_browsertest.cc
+++ chrome/browser/ui/webui/webui_toolbar/webui_toolbar_ui_browsertest.cc
@@ -258,7 +258,7 @@ IN_PROC_BROWSER_TEST_F(WebUIToolbarUIBrowserTest, SetReloadButtonState) {
               &toolbar_ui_api::mojom::ReloadControlState::is_navigation_loading,
               true))))))
       .Times(1);
-  ui()->OnNavigationControlsStateChanged(std::move(state));
+  ui()->OnNavigationControlsStateChanged(*state);
   connection.mock_observer().FlushForTesting();
 }
 
