cb370c8b43f7498d8b0d5114c5b8f0d76dc146e3
Federica Germinario <fedegermi@google.com>
2026-04-13 04:22:17 -0700

    [ios] Implement execute for kManagePasswords NSUserActivity
    Add implementation to handle opening manage passwords from Siri
    Shortcuts in TaskRequestForUserActivity.
    
    Tests will be added in a follow-up CL.
    
    Bug: 492115056
    Change-Id: I8515f20e39aff3bfcc28948831bf02ad315304b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7743366
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613621}

--- ios/chrome/app/task_request_user_activity.mm
+++ ios/chrome/app/task_request_user_activity.mm
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/bookmarks_commands.h"
 #import "ios/chrome/browser/shared/public/commands/browser_coordinator_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/shared/public/commands/settings_commands.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_params.h"
 #import "ios/chrome/common/intents/AddBookmarkToChromeIntent.h"
 #import "ios/chrome/common/intents/AddReadingListItemToChromeIntent.h"
@@ -268,6 +269,15 @@ void OpenBookmarksWithBrowser(base::WeakPtr<Browser> weak_browser) {
   }
 }
 
+// Navigates to the password search UI.
+void OpenPasswordSearchWithBrowser(base::WeakPtr<Browser> weak_browser) {
+  if (Browser* browser = weak_browser.get()) {
+    id<SettingsCommands> handler =
+        HandlerForProtocol(browser->GetCommandDispatcher(), SettingsCommands);
+    [handler showPasswordSearchPage];
+  }
+}
+
 // Adds bookmarks to Chrome.
 void AddBookmarkToChromeWithIntent(INIntent* intent,
                                    base::WeakPtr<Browser> weak_browser) {
@@ -453,7 +463,9 @@ std::vector<GURL> GetURLsFromOpenInChromeIntent(INIntent* intent) {
       // TODO(crbug.com/492115056): Add implementation.
       break;
     case UserActivityType::kManagePasswords:
-      // TODO(crbug.com/492115056): Add implementation.
+      completion = base::CallbackToBlock(base::BindRepeating(
+          &OpenPasswordSearchWithBrowser, browser->AsWeakPtr()));
+      webpageGURLs.push_back(GURL(kChromeUINewTabURL));
       break;
     case UserActivityType::kManageSettings:
       // TODO(crbug.com/492115056): Add implementation.
