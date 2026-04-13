6980b338d4088f6de21be2a11a8fb92a8d7bfb52
Boris Sazonov <bsazonov@chromium.org>
2026-04-13 07:30:13 -0700

    [Signin] Move more scopes from gaia_constants
    No behavior change.
    
    Bug: None
    Change-Id: I470eba729cde2a8c393daa8437142a38c5efb85c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748825
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Monica Salama <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613694}

--- components/signin/public/base/oauth_consumer_registry.cc
+++ components/signin/public/base/oauth_consumer_registry.cc
@@ -70,6 +70,9 @@ constexpr char kClientChannelOAuth2Scope[] =
 // OAuth2 scope for Cloud Search query API.
 constexpr char kCloudSearchQueryOAuth2Scope[] =
     "https://www.googleapis.com/auth/cloud_search.query";
+// OAuth2 scope for read-write access to contacts.
+constexpr char kContactsOAuth2Scope[] =
+    "https://www.googleapis.com/auth/contacts";
 constexpr char kCryptAuthOAuth2Scope[] =
     "https://www.googleapis.com/auth/cryptauth";
 // OAuth2 scope for Discovery Engine suggestion API.
@@ -84,6 +87,9 @@ constexpr char kDriveAppsOAuth2Scope[] =
 // OAuth2 scope for access to readonly Drive Apps.
 constexpr char kDriveAppsReadonlyOAuth2Scope[] =
     "https://www.googleapis.com/auth/drive.apps.readonly";
+// OAuth 2 scope for readonly access to Drive.
+constexpr char kDriveReadOnlyOAuth2Scope[] =
+    "https://www.googleapis.com/auth/drive.readonly";
 // OAuth2 scope for access for DriveFS to access flags.
 constexpr char kExperimentsAndConfigsOAuth2Scope[] =
     "https://www.googleapis.com/auth/experimentsandconfigs";
@@ -109,6 +115,8 @@ constexpr char kKidManagementPrivilegedOAuth2Scope[] =
 // OAuth2 scope for access to Google Family Link Supervision Setup.
 constexpr char kKidsSupervisionSetupChildOAuth2Scope[] =
     "https://www.googleapis.com/auth/kids.supervision.setup.child";
+// OAuth2 scope for Lens.
+constexpr char kLensOAuth2Scope[] = "https://www.googleapis.com/auth/lens";
 // OAuth2 scope for app license check.
 constexpr char kLicenseCheckOAuth2Scope[] =
     "https://www.googleapis.com/auth/applicense.bytebot";
@@ -368,7 +376,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
       return OAuthConsumer(
           /*name=*/kProjectorTokenFetcherName,
           /*scopes=*/{GaiaConstants::kDriveOAuth2Scope,
-                      GaiaConstants::kDriveReadOnlyOAuth2Scope});
+                      kDriveReadOnlyOAuth2Scope});
     case OAuthConsumerId::kAddSupervision:
       return OAuthConsumer(
           /*name=*/kAddSupervisionName,
@@ -388,7 +396,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kLauncherItemSuggest:
       return OAuthConsumer(
           /*name=*/kLauncherItemSuggestName,
-          /*scopes=*/{GaiaConstants::kDriveReadOnlyOAuth2Scope});
+          /*scopes=*/{kDriveReadOnlyOAuth2Scope});
     case OAuthConsumerId::kMarketingBackendConnector:
       return OAuthConsumer(
           /*name=*/kMarketingBackendConnectorName,
@@ -431,7 +439,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kNtpDriveService:
       return OAuthConsumer(
           /*name=*/kNtpDriveServiceName,
-          /*scopes=*/{GaiaConstants::kDriveReadOnlyOAuth2Scope});
+          /*scopes=*/{kDriveReadOnlyOAuth2Scope});
     case OAuthConsumerId::kForceSigninVerifier:
       return OAuthConsumer(
           /*name=*/kForceSigninVerifierName,
@@ -481,7 +489,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kComposeboxQueryController:
       return OAuthConsumer(
           /*name=*/kComposeboxQueryControllerName,
-          /*scopes=*/{GaiaConstants::kLensOAuth2Scope});
+          /*scopes=*/{kLensOAuth2Scope});
     case OAuthConsumerId::kDocumentSuggestionsService:
       return OAuthConsumer(
           /*name=*/kDocumentSuggestionsServiceName,
@@ -514,7 +522,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kLensOverlayQueryController:
       return OAuthConsumer(
           /*name=*/kLensOverlayQueryControllerName,
-          /*scopes=*/{GaiaConstants::kLensOAuth2Scope});
+          /*scopes=*/{kLensOAuth2Scope});
     case OAuthConsumerId::kTrustedVaultFrontend:
       return OAuthConsumer(
           /*name=*/kTrustedVaultFrontendName,
@@ -675,7 +683,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
           /*scopes=*/{contextual_tasks::ShouldUseSearchResultsScope()
                           ? kSearchResultsOAuth2Scope
                           : GaiaConstants::kChromeSyncOAuth2Scope,
-                      kClearCutOAuth2Scope, GaiaConstants::kLensOAuth2Scope});
+                      kClearCutOAuth2Scope, kLensOAuth2Scope});
     case OAuthConsumerId::kEnterprisePlusAddress:
       return GetOAuthConsumerForEnterprisePlusAddress();
     case OAuthConsumerId::kGlicUserStatus:
@@ -687,7 +695,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kAshDriveIntegration:
       return OAuthConsumer(
           /*name=*/kAshDriveIntegrationName,
-          /*scopes=*/{GaiaConstants::kDriveReadOnlyOAuth2Scope});
+          /*scopes=*/{kDriveReadOnlyOAuth2Scope});
     case OAuthConsumerId::kAshBocaClassroomPageHandler:
       return OAuthConsumer(
           /*name=*/kAshClassroomPageHandlerName,
@@ -700,7 +708,7 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kAshScannerKeyedService:
       return OAuthConsumer(
           /*name=*/kAshScannerKeyedServiceName,
-          /*scopes=*/{GaiaConstants::kContactsOAuth2Scope});
+          /*scopes=*/{kContactsOAuth2Scope});
     case OAuthConsumerId::kAshAutotestPrivateApi:
       // This consumer id should be converted using
       // GetOAuthConsumerForDynamicScopes().
