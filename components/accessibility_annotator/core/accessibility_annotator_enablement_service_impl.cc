873c6b1b5a00f00697b6c2c2c57eccd45648164e
Norge Vizcay <vizcay@google.com>
2026-04-13 08:49:37 -0700

    Remove database storage requirement for enablement
    This CL removes the kAccessibilityAnnotatorDatabaseStorage feature from
    the required features list in AccessibilityAnnotatorEnablementService.
    This feature is no longer a prerequisite for the service to be enabled.
    
    Bug: b:494149753
    Change-Id: I64b44a6676a6dffa93e2952d711fad3f1a3c85ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7755559
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613752}

--- components/accessibility_annotator/core/accessibility_annotator_enablement_service_impl.cc
+++ components/accessibility_annotator/core/accessibility_annotator_enablement_service_impl.cc
@@ -38,7 +38,6 @@ void MaybeOutputReason(std::string* out, std::string_view message) {
   const base::Feature* const kRequiredFeatures[] = {
       &features::kAccessibilityAnnotator,
       &features::kAccessibilityAnnotatorFirstRun,
-      &features::kAccessibilityAnnotatorDatabaseStorage,
   };
 
   for (const base::Feature* feature : kRequiredFeatures) {
