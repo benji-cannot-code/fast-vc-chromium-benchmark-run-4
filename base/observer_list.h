aeac3e47e19f1f4d95f241ecc76c9b3000d9f287
Sylvain Defresne <sdefresne@chromium.org>
2026-04-13 03:57:53 -0700

    Reland "Assert AddObserver(...) is called on the correct sequence"
    This is a reland of commit cf7e006bdabcef4ecdaec12f806024a8ae3a4301
    without any changes.
    
    The original CL was reverted by gardener who though it caused failures
    on some tests, but the failures were unrelated (according to their own
    words). It looks like the failure are due to some timeout of the tests
    (either flakiness or due to another change).
    
    See comment on https://crrev.com/c/7738134
    
    Original change's description:
    > [base] Assert AddObserver(...) is called on the correct sequence
    >
    > ObserverList is temporarily sequence-bound during iteration in order
    > to detect one sequence modifying the list while another is iterating.
    >
    > The protection was not present in AddObserver(...) even though adding
    > an item into an std::vector<...> can invalidate the pointers (as the
    > memory needs to be reallocated).
    >
    > Add a DCHECK_CALLED_ON_VALID_SEQUENCE(...) call when AddObserver(...)
    > is called while the list is being iterated in order to detect those
    > case too.
    >
    > Bug: 498769567
    > Change-Id: I821f73214949e6ccbbc4ed4f3df81b740ded5ec4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7725744
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1611373}
    
    Bug: 498769567
    Change-Id: I3cc42384c1569e717d18f62f1e769acbbd396093
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7743060
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613614}

--- base/observer_list.h
+++ base/observer_list.h
@@ -372,6 +372,9 @@ class ObserverList {
       DUMP_WILL_BE_NOTREACHED() << "Observers can only be added once!";
       return;
     }
+    if (!live_iterators_.empty()) {
+      DCHECK_CALLED_ON_VALID_SEQUENCE(iteration_sequence_checker_);
+    }
     ++observers_count_;
     observers_.emplace_back(ObserverStorageType(observer));
   }
