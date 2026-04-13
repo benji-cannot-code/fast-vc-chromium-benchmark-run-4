0e739613342a21ec9ff2a817d72c3719e4383e7c
Ilya Nikolaevskiy <ilnik@chromium.org>
2026-04-13 05:29:22 -0700

    Protect against data races in VideoCaptureDeviceWin
    Mark thread shared members with GUARDED_BY to prevent similar mistakes in the future.
    
    The locks are grabbed 2 times in AllocateAndStart and Deallocate now
    because we can't hold them throughout the whole function body due to deadlocks risk.The DirectShow methods may be dependent on the thread running `|SinkFilterObserver::SinkFilterObserver` callbacks, which also grabs the locks.
    
    The performance impact is neglegible because these extra locks happen only 2 times per stream.
    
    Fixed: 501581035
    Change-Id: Ic022556826eb9105489949966a9c2cd691edc8c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7754900
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613639}

--- media/capture/video/win/video_capture_device_win.h
+++ media/capture/video/win/video_capture_device_win.h
@@ -131,8 +131,14 @@ class VideoCaptureDeviceWin : public VideoCaptureDevice,
                      HRESULT hr);
 
   const VideoCaptureDeviceDescriptor device_descriptor_;
-  InternalState state_;
-  std::unique_ptr<VideoCaptureDevice::Client> client_;
+
+  // Used to guard between race checking capture state between the thread used
+  // in |thread_checker_| and a thread used in
+  // |SinkFilterObserver::SinkFilterObserver| callbacks.
+  base::Lock lock_;
+
+  InternalState state_ GUARDED_BY(lock_);
+  std::unique_ptr<VideoCaptureDevice::Client> client_ GUARDED_BY(lock_);
 
   Microsoft::WRL::ComPtr<IBaseFilter> capture_filter_;
 
@@ -159,14 +165,9 @@ class VideoCaptureDeviceWin : public VideoCaptureDevice,
 
   base::TimeTicks first_ref_time_;
 
-  base::queue<TakePhotoCallback> take_photo_callbacks_;
+  base::queue<TakePhotoCallback> take_photo_callbacks_ GUARDED_BY(lock_);
 
   base::ThreadChecker thread_checker_;
-
-  // Used to guard between race checking capture state between the thread used
-  // in |thread_checker_| and a thread used in
-  // |SinkFilterObserver::SinkFilterObserver| callbacks.
-  base::Lock lock_;
 
   bool enable_get_photo_state_;
 
