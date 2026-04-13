2a16b7f3d96e6d63405bae1818a550a079dd8f2f
Andreu Botella <abotella@igalia.com>
2026-04-12 23:11:14 -0700

    [line-clamp] Don't associate line-break ellipses with non-atomic inlines
    In https://crrev.com/c/6802120, to fix a repaint issue, line-clamp
    ellipses added as part of line-breaking (with the
    `CSSLineClampLineBreakingEllipsis` runtime flag) were associated with
    a relevant `LayoutObject`.
    
    This involved filtering the `InlineItem`s before the clamp point with
    the `CanUseItemForNeedsPaint` function added to
    `logical_line_builder.cc`. Notably, this function did not filter out
    open tags, meaning that the ellipsis could be associated with
    non-atomic inline box layout objects, which is not something that
    could ever happen for the ellipses generated by `LineTruncator`.
    
    As it turns out, having an inline box start immediately before a
    block-in-inline causes the line-breaking ellipsis to be associated
    with the inline box. If that inline box happens to have an outline,
    then `PhysicalBoxFragment::AddOutlineRectsForInlineBox` will try to
    find all fragment items associated with the box, including the
    ellipsis, leading to wrong assumptions since the ellipsis isn't a
    wrong item.
    
    A solution could be to explicitly filter out inline boxes with an
    outline, but since `LineTruncator` doesn't associate any ellipses with
    any non-atomic inline items, we chose to do the case for line-breaking
    ellipses.
    
    As a related note, this fix would also have solved bug 489494018,
    which was fixed in https://crrev.com/c/7649894 by allowing associating
    ellipses with line break items. Since both changes are separate, and
    both bring the behavior closer to `LineTruncator`'s, it makes sense to
    not revert that one.
    
    Bug: 40336192, 499496973
    Change-Id: Iacb8cb5298e199198aba0545fdcd3cd64c5f4da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726309
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Andreu Botella <abotella@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1613505}

--- third_party/blink/renderer/core/layout/inline/logical_line_builder.cc
+++ third_party/blink/renderer/core/layout/inline/logical_line_builder.cc
@@ -35,6 +35,7 @@ bool CanUseItemForNeedsPaint(const InlineItem& item) {
     case InlineItem::kFloating:
     case InlineItem::kOutOfFlowPositioned:
     case InlineItem::kListMarker:
+    case InlineItem::kOpenTag:
     case InlineItem::kBidiControl:
     case InlineItem::kOpenRubyColumn:
     case InlineItem::kCloseRubyColumn:
@@ -49,7 +50,6 @@ bool CanUseItemForNeedsPaint(const InlineItem& item) {
       break;
 
     case InlineItem::kAtomicInline:
-    case InlineItem::kOpenTag:
     case InlineItem::kInitialLetterBox:
       break;
   }
