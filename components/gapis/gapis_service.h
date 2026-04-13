0d0a299c33f629fd99d451f713e2526180a2abf5
Rushan Suleymanov <rushans@google.com>
2026-04-13 14:46:56 -0700

    Use CDM helper to sign challenge
    Signed challenge is used to obtain an app token from the server.
    
    Bug: b:485217840
    Change-Id: Iee9bfde80172ce88fbc280b1df817f7ad2c419fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7684640
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613989}

--- components/gapis/gapis_service.h
+++ components/gapis/gapis_service.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_GAPIS_GAPIS_SERVICE_H_
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/memory/raw_ref.h"
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/public/identity_manager/access_token_info.h"
 #include "components/version_info/channel.h"
+#include "content/public/browser/cdm_helper.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "url/gurl.h"
 
@@ -42,7 +44,23 @@ class GapisService : public KeyedService {
   void FetchAppTokenIfNeeded();
 
  private:
-  void OnAccessTokenFetched(GoogleServiceAuthError error,
+  enum class CdmInitializationState {
+    kNotInitialized,
+    kInitializedSuccessfully,
+    kFailedToInitialize,
+  };
+
+  void InitializeCdm();
+  void OnCdmInitialized(content::CdmHelper::InitializeResult result);
+
+  void FetchChallenge();
+  void OnChallengeFetched(const std::string& challenge_init_data_hex);
+  void SignChallenge(const std::string& challenge);
+  void OnChallengeSigned(const std::string& signed_challenge,
+                         content::CdmHelper::SignChallengeResult result);
+
+  void OnAccessTokenFetched(const std::string& challenge_init_data_hex,
+                            GoogleServiceAuthError error,
                             signin::AccessTokenInfo access_token_info);
 
   void OnAppTokenFetched(const std::string& app_token);
@@ -50,6 +68,10 @@ class GapisService : public KeyedService {
   const raw_ref<signin::IdentityManager> identity_manager_;
   const scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   const GURL service_url_;
+
+  std::unique_ptr<content::CdmHelper> cdm_helper_;
+  CdmInitializationState cdm_initialization_state_ =
+      CdmInitializationState::kNotInitialized;
 
   // Pending request for an access token. Non-null iff there is a request
   // ongoing.
