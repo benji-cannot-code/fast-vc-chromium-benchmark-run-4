0037fd69a051bfd2eb2f96e5e613d29c19f38a7d
Fredrik Söderquist <fs@opera.com>
2026-04-13 09:42:46 -0700

    Drop unused WindRule from PolygonShape constructor
    Change-Id: I10713f304201f1287a9ba4017860019c744912f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7752706
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613794}

--- third_party/blink/renderer/core/layout/shapes/shape.cc
+++ third_party/blink/renderer/core/layout/shapes/shape.cc
@@ -229,8 +229,7 @@ std::unique_ptr<Shape> Shape::CreateShape(const BasicShape* basic_shape,
                            FloatValueForLength(values.at(i + 1), box_height));
         vertices[i / 2] = converter.ToLogical(vertex);
       }
-      shape = std::make_unique<PolygonShape>(std::move(vertices),
-                                             polygon->GetWindRule());
+      shape = std::make_unique<PolygonShape>(std::move(vertices));
       break;
     }
 
