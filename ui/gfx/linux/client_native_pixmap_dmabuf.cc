c04ba8e18b7b5dd2428495cb47a834fe54984a83
Ted Meyer <tmathmeyer@chromium.org>
2026-04-13 13:02:31 -0700

    Add a crash key for MMAP ENOMEM terminations
    We see a few crashes at this spot, and have some direct user reports
    pointing here as well. I suspect that we're trying to allocate a very
    large image as a result of some kind of bad calculation in the video
    stack somewhere, but it might also just be a generic mmap failure.
    Recording the actual size requested will help determine if it's the
    former or the latter.
    
    Bug: 500318522
    Change-Id: I1694819e1d73d337d9843e62a86c826e13509ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749948
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613920}

--- ui/gfx/linux/client_native_pixmap_dmabuf.cc
+++ ui/gfx/linux/client_native_pixmap_dmabuf.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/debug/crash_logging.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/numerics/safe_conversions.h"
@@ -85,6 +86,8 @@ ClientNativePixmapDmaBuf::MapPlane(const NativePixmapPlane& plane) {
   if (data == MAP_FAILED) {
     logging::SystemErrorCode mmap_error = logging::GetLastSystemErrorCode();
     if (mmap_error == ENOMEM) {
+      SCOPED_CRASH_KEY_NUMBER("gfx", "mmap_size_plane_size", plane.size);
+      SCOPED_CRASH_KEY_NUMBER("gfx", "mmap_size_plane_offset", plane.offset);
       base::TerminateBecauseOutOfMemory(size_to_map);
     }
     LOG(ERROR) << "Failed to mmap dmabuf: "
