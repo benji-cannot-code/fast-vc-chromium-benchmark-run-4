5e29f5b61954877fd14c89939587207c306c7894
Colin Blundell <blundell@chromium.org>
2026-04-13 02:02:46 -0700

    [Blink] Mark WebGL's DoExternalDrawAndProduceResource() as overwrite
    The draw is scaled to the size of the CRP [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc;l=567-568?q=webgl_rendering_context_base.cc&ss=chromium
    
    Bug: 352263194
    Change-Id: I1007ff3a8aaac0129e37378984fb184bd3f99b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748046
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613559}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -2207,7 +2207,7 @@ WebGLRenderingContextBase::CopyRenderingResultsFromDrawingBufferToResource(
           [&image, dest_rect](cc::PaintCanvas& canvas) {
             DrawImageToCanvas(image.get(), canvas, dest_rect);
           },
-          /*is_overwrite=*/false);
+          /*is_overwrite=*/true);
       copy_succeeded = true;
     }
   }
