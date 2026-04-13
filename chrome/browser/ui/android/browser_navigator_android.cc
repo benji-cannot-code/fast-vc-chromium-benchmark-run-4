fc1a721061a0d9fc864d73eea1cb338635b621ff
Jack Thiesen <jthiesen@chromium.org>
2026-04-13 09:02:17 -0700

    Android's Navigate() Supports IGNORE_ACTION Disposition
    If WindowOpenDisposition::IGNORE_ACTION is provided to Android's
    Navigate() implementation, nullptr is returned. Testing is included.
    
    Some notes:
     1. params->source_contents is set to the active tab if it is null
     2. params->contents_to_insert is correctly ignored
    
    Bug: 501692851
    Change-Id: I5f113f0a0dcc9b159bda021514779e876c7fd280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748677
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613765}

--- chrome/browser/ui/android/browser_navigator_android.cc
+++ chrome/browser/ui/android/browser_navigator_android.cc
@@ -27,6 +27,7 @@ namespace {
 // TODO(crbug.com/477944342): Expand the scenarios where we support it.
 bool SupportsContentsToInsert(NavigateParams* params) {
   switch (params->disposition) {
+    case WindowOpenDisposition::IGNORE_ACTION:
     case WindowOpenDisposition::NEW_BACKGROUND_TAB:
     case WindowOpenDisposition::NEW_FOREGROUND_TAB:
       return true;
@@ -88,6 +89,8 @@ void GetOrCreateBrowserWindowForDisposition(
     case WindowOpenDisposition::NEW_BACKGROUND_TAB:
       [[fallthrough]];
     case WindowOpenDisposition::NEW_FOREGROUND_TAB:
+      [[fallthrough]];
+    case WindowOpenDisposition::IGNORE_ACTION:
       [[fallthrough]];
     case WindowOpenDisposition::CURRENT_TAB: {
       base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
@@ -229,6 +232,16 @@ raw_ptr<tabs::TabInterface> GetOrCreateTabForDisposition(
       params->source_contents = active_tab->GetContents();
       return active_tab;
     }
+    case WindowOpenDisposition::IGNORE_ACTION: {
+      if (!params->source_contents) {
+        raw_ptr<tabs::TabInterface> active_tab = tab_model->GetActiveTab();
+        if (active_tab) {
+          params->source_contents = active_tab->GetContents();
+        }
+      }
+      params->browser = nullptr;
+      return nullptr;
+    }
     default:
       NOTIMPLEMENTED();
       return nullptr;
@@ -241,6 +254,7 @@ base::WeakPtr<content::NavigationHandle> GetTabAndPerformNavigation(
 
   tabs::TabInterface* tab = GetOrCreateTabForDisposition(params);
   if (!tab || !tab->GetContents()) {
+    // WindowOpenDisposition::IGNORE_ACTION exits here.
     return nullptr;
   }
 
@@ -281,6 +295,7 @@ base::WeakPtr<content::NavigationHandle> Navigate(NavigateParams* params) {
   }
   // Only handles dispositions that do not create new windows.
   if (params->disposition != WindowOpenDisposition::CURRENT_TAB &&
+      params->disposition != WindowOpenDisposition::IGNORE_ACTION &&
       params->disposition != WindowOpenDisposition::NEW_BACKGROUND_TAB &&
       params->disposition != WindowOpenDisposition::NEW_FOREGROUND_TAB) {
     return nullptr;
