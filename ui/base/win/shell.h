4819f2a50c625bd0a2838f62647a8fe834e3613c
Shunya Shishido <sisidovski@chromium.org>
2026-04-12 19:04:37 -0700

    Revert "Add regression test for path resolution from ShellExecute"
    This reverts commit 92b45f7cab0b07fb383c9d52366a4786fa4ff321.
    
    Reason for revert: ShellTest.OpenFileWithSpaceInExtension/AutomaticallyParsed fails on Windows.
    
    Original change's description:
    > Add regression test for path resolution from ShellExecute
    >
    > In crrev.com/c/7514332 stricter path resolution was added to make sure
    > legacy filename resolution did not result in the incorrect target being
    > picked when calling ShellExecute.
    >
    > This CL adds a regression test for this behavior, to verify both the old
    > and new codepaths behave as expected.
    >
    > In the old codepath, the test EXE is executed, but in the new codepath
    > the shellexecute launches the .txt file as expected.
    >
    > BUG=476862276
    >
    > Change-Id: Iec580286dcd559d1d76acccebeb2cca657a23f37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7722299
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1613106}
    
    Bug: 476862276, 501987421
    Change-Id: Ic1cdff4ef9cdb51ee89cb69c726f1ec88e915578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7753879
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Owners-Override: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613470}

--- ui/base/win/shell.h
+++ ui/base/win/shell.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/component_export.h"
-#include "base/feature_list.h"
 #include "base/win/windows_types.h"
 
 namespace base {
@@ -17,9 +16,6 @@ class FilePath;
 }
 
 namespace ui::win {
-
-COMPONENT_EXPORT(UI_BASE)
-BASE_DECLARE_FEATURE(kManuallyParsePathForShellExecute);
 
 // Open the folder at |full_path| via the Windows shell. It is an error if
 // |full_path| does not refer to a folder.
