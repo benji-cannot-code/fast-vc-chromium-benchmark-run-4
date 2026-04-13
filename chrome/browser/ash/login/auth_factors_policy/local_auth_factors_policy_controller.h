fe4058edc2747869a7c76236e6d2999b2ab63d7b
Ivan Šandrk <isandrk@chromium.org>
2026-04-13 05:41:34 -0700

    LPP: Add prefs() helper to LocalAuthFactorsPolicyController
    Introduces a private `prefs()` helper method to simplify accessing
    the `PrefService`. This reduces `pref_change_registrar_.prefs()->`
    boilerplate and cleans up the class ahead of upcoming changes that
    will interact more heavily with local state preferences (e.g., checking
    and updating verified complexity).
    
    This is a non-functional refactoring change.
    
    Bug: 445625494
    Change-Id: I7aaee03f2296fd9f0a398d77a4cdc79a6328ef21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747951
    Reviewed-by: Fahad Mansoor <fahadmansoor@google.com>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Auto-Submit: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613645}

--- chrome/browser/ash/login/auth_factors_policy/local_auth_factors_policy_controller.h
+++ chrome/browser/ash/login/auth_factors_policy/local_auth_factors_policy_controller.h
@@ -38,6 +38,8 @@ class LocalAuthFactorsPolicyController : public KeyedService {
       base::RepeatingClosure on_pref_processed);
 
  private:
+  PrefService& prefs();
+
   void OnAllowedAuthFactorsPrefUpdated();
   void OnGetAuthFactorsConfiguration(
       base::ScopedClosureRunner pref_processed_runner,
