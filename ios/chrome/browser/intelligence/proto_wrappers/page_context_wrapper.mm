0ad14f474b2a5cae18736576966ff11169ece54a
Jean-Francois Le <jfle@google.com>
2026-04-13 06:57:02 -0700

    [iOS][apcv2] Defer base::DictValue destruction to background thread
    Since the APC extraction can be very big, destroying the base::DictValue
    containing all that information takes about 12ms for the test page.
    
    Before/After screenshot:
    https://screenshot.googleplex.com/9JiQNzx4NyrzFzq
    
    Test Page:
    https://en.wikipedia.org/wiki/List_of_Falcon_9_and_Falcon_Heavy_launches
    
    Bug: 501460481
    Change-Id: I79c3d0b1b366a8446d1d3b04498856d5dea2eef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749844
    Commit-Queue: Jean-François Le <jfle@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613675}

--- ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/task/bind_post_task.h"
 #import "base/task/sequenced_task_runner.h"
 #import "base/task/task_traits.h"
+#import "base/task/thread_pool.h"
 #import "base/time/time.h"
 #import "base/timer/timer.h"
 #import "base/token.h"
@@ -557,6 +558,15 @@ result.links = linksArray;
                              securityOrigin:securityOrigin
                             localFrameToken:frameId];
       barrier.Run();
+
+      // Defer destruction of the base::DictValue to a background thread to
+      // prevent blocking the main thread. The object is self-contained and
+      // thread-safe.
+      if (value) {
+        base::ThreadPool::PostTask(
+            FROM_HERE, {base::TaskPriority::BEST_EFFORT},
+            base::BindOnce([](base::Value v) {}, std::move(*value)));
+      }
     };
 
     extractorFeature->ExtractPageContextJSON(
