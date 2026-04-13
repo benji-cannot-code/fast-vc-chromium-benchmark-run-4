5d9ec36b4ef62594f568e0c3a8d79c05584f1894
Radu Nitescu <radunitescu@google.com>
2026-04-13 09:42:15 -0700

    [iOS] Consider `IsComposeboxAIMDisabled` when enabling the plus button
    In this CL:
     - The new killswitch `IsComposeboxAIMDisabled` is taken into account when determining `IsPlusButtonInFakeboxEnabled`
     - All dependencies are moved inside `IsPlusButtonInFakeboxEnabled` to simplify the check in NTP
    
    Change-Id: Idb1afd7d69ae301da02c5eb1cdd760bfe4761d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7754685
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613793}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1184,7 +1184,11 @@ bool IsSyncedGroupColorEnabled() {
 // Enables the plus button in NTP fakebox.
 BASE_FEATURE(kPlusButtonInFakebox, base::FEATURE_DISABLED_BY_DEFAULT);
 
-// Returns true if the plus button in NTP fakebox is enabled
+// Returns true if the plus button in NTP fakebox is enabled.
 bool IsPlusButtonInFakeboxEnabled() {
+  if (IsComposeboxAIMDisabled() || !IsComposeboxIOSEnabled()) {
+    return false;
+  }
+
   return base::FeatureList::IsEnabled(kPlusButtonInFakebox);
 }
