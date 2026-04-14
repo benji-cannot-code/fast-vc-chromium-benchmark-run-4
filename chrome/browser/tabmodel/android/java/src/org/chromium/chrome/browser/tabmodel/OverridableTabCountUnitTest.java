1c5a5fd97170aa8defa06d7cd25c1c522c4aa333
Dan Polanco <polardz@google.com>
2026-04-14 08:06:14 -0700

    Rename to OverridableTabCount and move out of toolbar package
    This CL:
    -Renames CustomTabCount to OverridableTabCount.
    -Moves relevant files to the tabmodel package.
    
    Bypass-Check-License: Moving file.
    Change-Id: I2aa18ab5fa2ed4692ac4e7199caf9f61e2c58083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759982
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614458}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/OverridableTabCountUnitTest.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/OverridableTabCountUnitTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.toolbar;
+package org.chromium.chrome.browser.tabmodel;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -22,77 +22,76 @@ import org.chromium.base.supplier.ObservableSuppliers;
 import org.chromium.base.supplier.SettableMonotonicObservableSupplier;
 import org.chromium.base.supplier.SettableNonNullObservableSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
-/** Unit tests for {@link CustomTabCount}. */
+/** Unit tests for {@link OverridableTabCount}. */
 @RunWith(BaseRobolectricTestRunner.class)
-public class CustomTabCountUnitTest {
+public class OverridableTabCountUnitTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
     @Mock private TabModelSelector mTabModelSelector;
     private final SettableMonotonicObservableSupplier<TabModelSelector> mTabModelSelectorSupplier =
             ObservableSuppliers.createMonotonic();
     private final SettableNonNullObservableSupplier<Integer> mTabModelSelectorTabCountSupplier =
             ObservableSuppliers.createNonNull(0);
-    private CustomTabCount mCustomTabCount;
+    private OverridableTabCount mOverridableTabCount;
 
     @Before
     public void setUp() {
         when(mTabModelSelector.getCurrentModelTabCountSupplier())
                 .thenReturn(mTabModelSelectorTabCountSupplier);
-        mCustomTabCount = new CustomTabCount(mTabModelSelectorSupplier);
+        mOverridableTabCount = new OverridableTabCount(mTabModelSelectorSupplier);
         mTabModelSelectorSupplier.set(mTabModelSelector);
     }
 
     @Test
     public void testTabCountSupplier() {
         mTabModelSelectorTabCountSupplier.set(1);
-        assertEquals(1, mCustomTabCount.get());
-        assertFalse(mCustomTabCount.hasTokensForTesting());
+        assertEquals(1, mOverridableTabCount.get());
+        assertFalse(mOverridableTabCount.hasTokensForTesting());
 
         mTabModelSelectorTabCountSupplier.set(10);
-        assertEquals(10, mCustomTabCount.get());
-        assertFalse(mCustomTabCount.hasTokensForTesting());
+        assertEquals(10, mOverridableTabCount.get());
+        assertFalse(mOverridableTabCount.hasTokensForTesting());
 
         mTabModelSelectorTabCountSupplier.set(6);
-        assertEquals(6, mCustomTabCount.get());
-        assertFalse(mCustomTabCount.hasTokensForTesting());
+        assertEquals(6, mOverridableTabCount.get());
+        assertFalse(mOverridableTabCount.hasTokensForTesting());
     }
 
     @Test
-    public void testCustomTabCount() {
-        int token = mCustomTabCount.setCount(4);
-        assertEquals(4, mCustomTabCount.get());
-        assertTrue(mCustomTabCount.hasTokensForTesting());
+    public void testOverridableTabCount() {
+        int token = mOverridableTabCount.setCount(4);
+        assertEquals(4, mOverridableTabCount.get());
+        assertTrue(mOverridableTabCount.hasTokensForTesting());
 
         mTabModelSelectorTabCountSupplier.set(10);
-        assertEquals(4, mCustomTabCount.get());
-        assertTrue(mCustomTabCount.hasTokensForTesting());
+        assertEquals(4, mOverridableTabCount.get());
+        assertTrue(mOverridableTabCount.hasTokensForTesting());
 
-        mCustomTabCount.releaseCount(token);
-        assertEquals(10, mCustomTabCount.get());
-        assertFalse(mCustomTabCount.hasTokensForTesting());
+        mOverridableTabCount.releaseCount(token);
+        assertEquals(10, mOverridableTabCount.get());
+        assertFalse(mOverridableTabCount.hasTokensForTesting());
     }
 
     @Test
     public void testMultipleTokens() {
-        int token1 = mCustomTabCount.setCount(4);
-        assertEquals(4, mCustomTabCount.get());
-        assertTrue(mCustomTabCount.hasTokensForTesting());
+        int token1 = mOverridableTabCount.setCount(4);
+        assertEquals(4, mOverridableTabCount.get());
+        assertTrue(mOverridableTabCount.hasTokensForTesting());
 
-        int token2 = mCustomTabCount.setCount(5);
-        assertEquals(5, mCustomTabCount.get());
-        assertTrue(mCustomTabCount.hasTokensForTesting());
+        int token2 = mOverridableTabCount.setCount(5);
+        assertEquals(5, mOverridableTabCount.get());
+        assertTrue(mOverridableTabCount.hasTokensForTesting());
 
         mTabModelSelectorTabCountSupplier.set(10);
-        assertEquals(5, mCustomTabCount.get());
-        assertTrue(mCustomTabCount.hasTokensForTesting());
+        assertEquals(5, mOverridableTabCount.get());
+        assertTrue(mOverridableTabCount.hasTokensForTesting());
 
-        mCustomTabCount.releaseCount(token1);
-        assertEquals(5, mCustomTabCount.get());
-        assertTrue(mCustomTabCount.hasTokensForTesting());
+        mOverridableTabCount.releaseCount(token1);
+        assertEquals(5, mOverridableTabCount.get());
+        assertTrue(mOverridableTabCount.hasTokensForTesting());
 
-        mCustomTabCount.releaseCount(token2);
-        assertEquals(10, mCustomTabCount.get());
-        assertFalse(mCustomTabCount.hasTokensForTesting());
+        mOverridableTabCount.releaseCount(token2);
+        assertEquals(10, mOverridableTabCount.get());
+        assertFalse(mOverridableTabCount.hasTokensForTesting());
     }
 }
