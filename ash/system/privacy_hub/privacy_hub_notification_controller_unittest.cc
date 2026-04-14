066cca076b211968637082d4e952309b23c3c087
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-13 23:07:08 -0700

    remove PrivacyHubNotificationControllerTest raw_ptr
    Bug: 40212619
    Change-Id: I35a668cac1713f4db53585d281c6ab803a8d9da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7750377
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614239}

--- ash/system/privacy_hub/privacy_hub_notification_controller_unittest.cc
+++ ash/system/privacy_hub/privacy_hub_notification_controller_unittest.cc
@@ -56,13 +56,6 @@ class PrivacyHubNotificationControllerTest : public AshTestBase {
 
   ~PrivacyHubNotificationControllerTest() override = default;
 
-  // AshTestBase:
-  void SetUp() override {
-    AshTestBase::SetUp();
-    controller_ = PrivacyHubNotificationController::Get();
-  }
-  void TearDown() override { AshTestBase::TearDown(); }
-
  protected:
   const message_center::Notification* GetCombinedNotification() const {
     return GetNotification(
@@ -91,7 +84,7 @@ class PrivacyHubNotificationControllerTest : public AshTestBase {
       FakeCrasAudioClient::Get()->SetActiveInputStreamsWithPermission(
           {{"CRAS_CLIENT_TYPE_CHROME", 1}});
     } else {
-      controller_->ShowSoftwareSwitchNotification(sensor);
+      controller()->ShowSoftwareSwitchNotification(sensor);
     }
   }
 
@@ -102,7 +95,7 @@ class PrivacyHubNotificationControllerTest : public AshTestBase {
       FakeCrasAudioClient::Get()->SetActiveInputStreamsWithPermission(
           {{"CRAS_CLIENT_TYPE_CHROME", 0}});
     } else {
-      controller_->RemoveSoftwareSwitchNotification(sensor);
+      controller()->RemoveSoftwareSwitchNotification(sensor);
     }
   }
 
@@ -135,7 +128,10 @@ class PrivacyHubNotificationControllerTest : public AshTestBase {
     return nullptr;
   }
 
-  raw_ptr<PrivacyHubNotificationController, DanglingUntriaged> controller_;
+  PrivacyHubNotificationController* controller() {
+    return PrivacyHubNotificationController::Get();
+  }
+
   const base::HistogramTester histogram_tester_;
   base::test::ScopedFeatureList scoped_feature_list_;
   MockNewWindowDelegate new_window_delegate_;
