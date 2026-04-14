d1114d09ac3e3dc87357090e8fc99ee3dbe33b8b
Sasha Kunitskaya <saskekun@google.com>
2026-04-14 06:47:39 -0700

    [FRE Refresh] Init Sign In Celebration UI
    This CL adds the placeholder WebUI classes for the new 'Sign In
    Celebration' screen. The actual implementation of the new UI will be
    done in future CL(s).
    
    Bug: 500274413
    Change-Id: I0899457fd31d9cb07cd97303dd5610015bd824bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749020
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Commit-Queue: Sasha Kunitskaya <saskekun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614406}

---
+++ chrome/browser/resources/intro/sign_in_celebration/app.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import {getHtml} from './app.html.js';
+
+export class SignInCelebrationAppElement extends CrLitElement {
+  static get is() {
+    return 'sign-in-celebration-app';
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'sign-in-celebration-app': SignInCelebrationAppElement;
+  }
+}
+
+customElements.define(
+    SignInCelebrationAppElement.is, SignInCelebrationAppElement);
