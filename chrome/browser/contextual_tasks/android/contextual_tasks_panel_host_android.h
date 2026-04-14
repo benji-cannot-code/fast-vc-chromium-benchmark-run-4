ba759b261bbb93543b48ec5e4a23a95a795c0ce0
Shakti Sahu <shaktisahu@chromium.org>
2026-04-14 08:25:17 -0700

    Introduce WebContentsDelegate for Android
    This CL adds missing WebContentsDelegate for the WebContents when shown
    as the bottom sheet. This is required for providing custom actions
    for the bottom sheet such as link click leading to a new tab.
    
    Bug: b:501104897
    Change-Id: Ibbdeb3f39684958ee00607f2da4cc26508e6ee43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7752956
    Reviewed-by: Khanh Nguyen <khanhtmn@google.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614471}

--- chrome/browser/contextual_tasks/android/contextual_tasks_panel_host_android.h
+++ chrome/browser/contextual_tasks/android/contextual_tasks_panel_host_android.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "chrome/browser/context_sharing/tab_bottom_sheet/android/tab_bottom_sheet_bridge.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_panel_host.h"
+#include "content/public/browser/web_contents_delegate.h"
 
 class BrowserWindowInterface;
 class TabAndroid;
@@ -24,7 +25,8 @@ namespace contextual_tasks {
 // Android implementation of ContextualTasksPanelHost using a bottom sheet.
 class ContextualTasksPanelHostAndroid
     : public ContextualTasksPanelHost,
-      public context_sharing::TabBottomSheetBridge::Observer {
+      public context_sharing::TabBottomSheetBridge::Observer,
+      public content::WebContentsDelegate {
  public:
   explicit ContextualTasksPanelHostAndroid(
       BrowserWindowInterface* browser_window);
@@ -46,6 +48,13 @@ class ContextualTasksPanelHostAndroid
   void OnClosed() override;
   void OnSuppressed() override;
   void OnOpened(bool is_expanded) override;
+
+  // content::WebContentsDelegate:
+  content::WebContents* OpenURLFromTab(
+      content::WebContents* source,
+      const content::OpenURLParams& params,
+      base::OnceCallback<void(content::NavigationHandle&)>
+          navigation_handle_callback) override;
 
  private:
   // Helper method to get the bridge, creating it if necessary. This is because
