601c8d3836ab3a8a13d8ad5c84fccfedb857d076
Mitsuru Oshima <oshima@chromium.org>
2026-04-14 15:25:47 -0700

    Cleanup obsolete vector icons and dependencies, includes
    in chromeos/ash/experiences/arc/vector_icons
    
    Bug: 492325954
    
    Change-Id: If9326c06d70d0ad0e50213b53aebc8b539026d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738138
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614747}

--- chromeos/ash/experiences/arc/compat_mode/arc_splash_screen_dialog_view.cc
+++ chromeos/ash/experiences/arc/compat_mode/arc_splash_screen_dialog_view.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "chromeos/ash/experiences/arc/compat_mode/overlay_dialog.h"
 #include "chromeos/ash/experiences/arc/compat_mode/style/arc_color_provider.h"
-#include "chromeos/ash/experiences/arc/vector_icons/vector_icons.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/ui/frame/caption_buttons/frame_center_button.h"
 #include "chromeos/ui/frame/default_frame_header.h"
