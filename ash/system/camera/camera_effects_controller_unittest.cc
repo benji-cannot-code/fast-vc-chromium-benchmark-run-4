c8f04411031470241363192eefffb86b4b48f6c9
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-13 23:31:33 -0700

    fix CameraEffectsControllerTest dangling pointer
    Bug: 40212619
    Change-Id: I9ba097d268213760681a6dda5a94db651a8e86c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7478929
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614260}

--- ash/system/camera/camera_effects_controller_unittest.cc
+++ ash/system/camera/camera_effects_controller_unittest.cc
@@ -111,6 +111,7 @@ class CameraEffectsControllerTest : public NoSessionAshTestBase {
   }
 
   void TearDown() override {
+    camera_effects_controller_ = nullptr;
     NoSessionAshTestBase::TearDown();
     controller_.reset();
   }
@@ -238,8 +239,7 @@ class CameraEffectsControllerTest : public NoSessionAshTestBase {
   base::FilePath camera_background_img_dir_;
   base::FilePath camera_background_run_dir_;
 
-  raw_ptr<CameraEffectsController, DanglingUntriaged>
-      camera_effects_controller_ = nullptr;
+  raw_ptr<CameraEffectsController> camera_effects_controller_ = nullptr;
 
   std::unique_ptr<FakeVideoConferenceTrayController> controller_;
   base::test::ScopedFeatureList scoped_feature_list_;
