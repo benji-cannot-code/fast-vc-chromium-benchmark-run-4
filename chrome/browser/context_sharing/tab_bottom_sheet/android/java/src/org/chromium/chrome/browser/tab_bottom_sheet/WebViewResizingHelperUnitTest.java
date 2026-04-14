072cb568e13a1677cee46d8dedb4a3a0e11aa1a0
Fiaz Muhammad <mfiaz@google.com>
2026-04-14 14:38:53 -0700

    [TabBottomSheet] Disable resizing web views until settled
    Disable resizing of the ThinWebView until the tab bottom sheet has
    settled.
    
    Bug: 486916366
    Change-Id: I77de88b664cbdfd32d09cadb99751d2d011c9557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7758299
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614717}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/WebViewResizingHelperUnitTest.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/WebViewResizingHelperUnitTest.java
@@ -9,7 +9,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import androidx.test.core.app.ApplicationProvider;
@@ -60,6 +62,9 @@ public class WebViewResizingHelperUnitTest {
         FrameLayout container = (FrameLayout) mHelper.getResizingContainer();
         assertEquals(1, container.getChildCount());
         assertEquals(mView, container.getChildAt(0));
+
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) mView.getLayoutParams();
+        assertEquals(Gravity.BOTTOM, layoutParams.gravity);
     }
 
     @Test
@@ -69,6 +74,19 @@ public class WebViewResizingHelperUnitTest {
 
         FrameLayout container = (FrameLayout) mHelper.getResizingContainer();
         assertEquals(0, container.getChildCount());
+    }
+
+    @Test
+    public void testSetIsResizing() {
+        mHelper.setThinWebView(mMockThinWebView);
+        mView.layout(0, 0, 100, 200);
+
+        mHelper.setIsResizing(true);
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) mView.getLayoutParams();
+        assertEquals(200, layoutParams.height);
+
+        mHelper.setIsResizing(false);
+        assertEquals(ViewGroup.LayoutParams.MATCH_PARENT, layoutParams.height);
     }
 
     @Test
