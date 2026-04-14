fd01420c05a96133d7daf02e312493ff85d6af6f
Pierre-Laurent Coirier <plcoirier@google.com>
2026-04-14 08:45:33 -0700

    [Encrypt Bookmarks] Add flag to chrome://flags
    Bug: 435317726
    Change-Id: I10f43c740ed32516f57188c60579cbd5f79b1800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7742045
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Pierre-Laurent Coirier <plcoirier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614500}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/autofill/core/common/autofill_switches.h"
 #include "components/autofill/core/common/autofill_util.h"
+#include "components/bookmarks/common/bookmark_features.h"
 #include "components/browser_sync/browser_sync_switches.h"
 #include "components/browser_ui/accessibility/android/features.h"
 #include "components/browsing_data/core/features.h"
@@ -4545,6 +4546,17 @@ const FeatureEntry::FeatureParam kWalletApiPrivatePassesUrl[] = {
 const FeatureEntry::FeatureVariation
     kWalletApiPrivatePassesEnabledVariations[] = {
         {"Debug", kWalletApiPrivatePassesUrl, nullptr}};
+
+const FeatureEntry::FeatureParam kBookmarksEncryptionStage1[] = {
+    {"stage", "write_both_read_only_clear"}};
+const FeatureEntry::FeatureParam kBookmarksEncryptionStage2[] = {
+    {"stage", "write_both_read_prefer_encrypted"}};
+const FeatureEntry::FeatureParam kBookmarksEncryptionStage3[] = {
+    {"stage", "write_only_encrypted_read_prefer_encrypted"}};
+const FeatureEntry::FeatureVariation kBookmarksEncryptionVariations[] = {
+    {"Stage1", kBookmarksEncryptionStage1, nullptr},
+    {"Stage2", kBookmarksEncryptionStage2, nullptr},
+    {"Stage3", kBookmarksEncryptionStage3, nullptr}};
 
 // RECORDING USER METRICS FOR FLAGS:
 // -----------------------------------------------------------------------------
@@ -12936,6 +12948,12 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kSyncSearchEnginesAndroidDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(syncer::kSyncSearchEnginesAndroid)},
 #endif
+
+    {"bookmarks-encryption", flag_descriptions::kBookmarksEncryptionName,
+     flag_descriptions::kBookmarksEncryptionDescription, kOsAll,
+     FEATURE_WITH_PARAMS_VALUE_TYPE(bookmarks::kEncryptBookmarks,
+                                    kBookmarksEncryptionVariations,
+                                    "BookmarksEncryption")},
 
     // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
