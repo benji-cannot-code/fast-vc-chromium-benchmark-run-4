ab449ca57ccb1183cec8c5f4b5c648fa73c41b76
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-13 23:41:02 -0700

    fix WorkspaceLayoutManagerBackdropTest and WorkspaceLayoutManagerKeyboardTest dangling pointers
    Bug: 40212619
    Change-Id: Ia2aa47f113552b44c10886fcf4a660db5e1612dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7517937
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614267}

--- ash/wm/workspace/workspace_layout_manager_unittest.cc
+++ ash/wm/workspace/workspace_layout_manager_unittest.cc
@@ -1694,6 +1694,11 @@ class WorkspaceLayoutManagerBackdropTest : public AshTestBase {
         desks_util::GetActiveDeskContainerId());
   }
 
+  void TearDown() override {
+    default_container_ = nullptr;
+    AshTestBase::TearDown();
+  }
+
   // Turn tablet mode on / off.
   void SetTabletModeEnabled(bool enabled) {
     Shell::Get()->tablet_mode_controller()->SetEnabledForTest(enabled);
@@ -1747,7 +1752,7 @@ class WorkspaceLayoutManagerBackdropTest : public AshTestBase {
 
  private:
   // The default container.
-  raw_ptr<aura::Window, DanglingUntriaged> default_container_;
+  raw_ptr<aura::Window> default_container_;
 };
 
 constexpr std::optional<Sound> kNoSoundKey = std::nullopt;
@@ -2151,6 +2156,11 @@ class WorkspaceLayoutManagerKeyboardTest : public AshTestBase {
     layout_manager_ = GetWorkspaceLayoutManager(active_desk_container);
   }
 
+  void TearDown() override {
+    layout_manager_ = nullptr;
+    AshTestBase::TearDown();
+  }
+
   void ShowKeyboard() {
     layout_manager_->OnKeyboardDisplacingBoundsChanged(keyboard_bounds_);
 
@@ -2197,7 +2207,7 @@ class WorkspaceLayoutManagerKeyboardTest : public AshTestBase {
  private:
   gfx::Insets restore_work_area_insets_;
   gfx::Rect keyboard_bounds_;
-  raw_ptr<WorkspaceLayoutManager, DanglingUntriaged> layout_manager_;
+  raw_ptr<WorkspaceLayoutManager> layout_manager_;
 };
 
 // Tests that when a child window gains focus the top level window containing it
