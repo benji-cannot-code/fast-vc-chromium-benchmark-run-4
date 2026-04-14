4dbe19b1c6cc9541cb530c72d679d61627e41adf
Magdalena Skarbinska <mskarbinska@google.com>
2026-04-14 01:12:23 -0700

    iwa: Keep IWA installer as .swbn handler even when unmanaged install is disabled
    When IWA installation is disabled by admin policy, trying to install an
    IWA from the file manager gives a poor user experience - an incorrect
    "Filetype unknown" error is shown.
    
    This CL changes the "Install" file handler to always be available. The
    installer will then open and show a screen explaining that installation
    is disabled by the administrator, providing clearer user feedback.
    
    Change-Id: I38c00e304e191f8842eb89aafe98e09e06d59e27
    Bug: 498941895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735426
    Reviewed-by: Josh Simmons <simmonsjosh@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614297}

--- chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view_controller_unittest.cc
+++ chrome/browser/ui/views/web_apps/isolated_web_apps/isolated_web_app_installer_view_controller_unittest.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/browser/web_applications/web_app_ui_manager.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
@@ -564,6 +565,33 @@ TEST_F(IsolatedWebAppInstallerViewControllerTest,
 }
 
 #if BUILDFLAG(IS_CHROMEOS)
+
+TEST_F(IsolatedWebAppInstallerViewControllerTest,
+       TransitionsToDisabledStepWhenUnmanagedInstallDisabled) {
+  base::test::ScopedFeatureList disable_feature;
+  disable_feature.InitAndDisableFeature(
+      features::kIsolatedWebAppUnmanagedInstall);
+
+  profile()->GetPrefs()->SetBoolean(
+      prefs::kIsolatedWebAppUserInstallationEnabled, false);
+
+  base::FilePath bundle_path = CreateBundlePath("test_bundle.swbn");
+  IsolatedWebAppUrlInfo url_info = CreateAndWriteTestBundle(bundle_path, "1.0");
+  MockIconAndPageState(url_info);
+
+  IsolatedWebAppInstallerModel model{IwaSourceBundleProdMode(bundle_path)};
+  IsolatedWebAppInstallerViewController controller(profile(), fake_provider(),
+                                                   &model);
+  testing::StrictMock<MockView> view;
+  controller.SetViewForTesting(&view);
+
+  EXPECT_CALL(view, ShowDisabledScreen());
+
+  controller.Start(base::DoNothing(), base::DoNothing());
+
+  TestIsolatedWebAppInstallerModelObserver(&model).WaitForStepChange(
+      Step::kDisabled);
+}
 
 TEST_F(IsolatedWebAppInstallerViewControllerTest,
        ChangingPrefToFalseDisablesInstaller) {
