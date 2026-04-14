b1ef060dfc68ff2ed88cec2272961d2863e47cf1
Alex Chen <alxchn@google.com>
2026-04-14 09:39:56 -0700

    Add ENTERPRISE_WATERMARK ifdefs to allow building without watermarking
    Separating by preprocessor allows reusing NavigationController and
    NavigationObserver on Android, where watermarking is not yet supported.
    
    Bug: b:428695892
    Change-Id: I6957e09482883965676a0a4a06a2b7092a17cc82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747350
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Alex Chen <alxchn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614545}

--- chrome/browser/enterprise/data_protection/data_protection_navigation_controller.cc
+++ chrome/browser/enterprise/data_protection/data_protection_navigation_controller.cc
@@ -17,14 +17,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "components/enterprise/connectors/core/reporting_constants.h"
 #include "components/enterprise/data_protection/utils.h"
-#include "components/enterprise/watermarking/content/watermark_text_container.h"
-#include "components/enterprise/watermarking/watermark.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+#include "components/enterprise/watermarking/content/watermark_text_container.h"
+#include "components/enterprise/watermarking/watermark.h"
+#endif  // BUILDFLAG(ENTERPRISE_WATERMARK)
 
 namespace enterprise_data_protection {
 
@@ -55,11 +57,13 @@ DataProtectionNavigationController::DataProtectionNavigationController(
 DataProtectionNavigationController::~DataProtectionNavigationController() =
     default;
 
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
 base::CallbackListSubscription
 DataProtectionNavigationController::RegisterWatermarkStringUpdatedCallback(
     WatermarkStringUpdatedCallback callback) {
   return watermark_string_updated_callbacks_.Add(std::move(callback));
 }
+#endif  // BUILDFLAG(ENTERPRISE_WATERMARK)
 
 #if BUILDFLAG(ENTERPRISE_SCREENSHOT_PROTECTION)
 base::CallbackListSubscription
@@ -77,8 +81,10 @@ void DataProtectionNavigationController::ApplyDataProtectionSettings(
     return;
   }
 
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
   watermark_text_ = settings.watermark_text;
   watermark_string_updated_callbacks_.Notify(watermark_text_);
+#endif  // BUILDFLAG(ENTERPRISE_WATERMARK)
 
 #if BUILDFLAG(ENTERPRISE_SCREENSHOT_PROTECTION)
   screenshot_allowed_ = settings.allow_screenshots;
@@ -113,11 +119,13 @@ void DataProtectionNavigationController::
   // Note that steps #5 and #6 are racy but the final outcome is correct
   // regardless of the order in which they execute.
 
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
   if (clear_watermark_text_on_page_load_) {
     watermark_text_ = std::string();
     clear_watermark_text_on_page_load_ = false;
     watermark_string_updated_callbacks_.Notify(watermark_text_);
   }
+#endif  // BUILDFLAG(ENTERPRISE_WATERMARK)
 
 #if BUILDFLAG(ENTERPRISE_SCREENSHOT_PROTECTION)
   if (clear_screenshot_protection_on_page_load_) {
@@ -190,6 +198,7 @@ void DataProtectionNavigationController::
   }
 #endif  // BUILDFLAG(ENTERPRISE_SCREENSHOT_PROTECTION)
 
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
   // Regardless of whether watermark text is empty, attach it as web contents
   // user data so that other browser process code can draw watermarks outside
   // of the context of a navigation (ex. when printing).
@@ -218,6 +227,7 @@ void DataProtectionNavigationController::
     watermark_text_ = settings.watermark_text;
     watermark_string_updated_callbacks_.Notify(watermark_text_);
   }
+#endif  // BUILDFLAG(ENTERPRISE_WATERMARK)
 
   if (!on_delay_apply_data_protection_settings_if_empty_called_for_testing_
            .is_null()) {
