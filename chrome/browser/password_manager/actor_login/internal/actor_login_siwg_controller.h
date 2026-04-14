91fea783fca4cfd9d704b836056aab41c8ceee47
Oleksandr Tara <otara@google.com>
2026-04-14 00:36:38 -0700

    [Actor Login M2] Add MQLS for federated logins.
    Uses the existing ActorLoginQualityLogger to log some
    federation-specific metrics in the actor login journey.
    ActorLoginQualityLogger is passed as a WeakPtr because it's attached to
    the tool, which is not guaranteed to outlive the ActorLoginDelegate (and
    transitively the federated fetcher and the SiwG controller).
    
    We also want to track if the button click by the BD infrastructure was
    successful. For this I'm adding a method to inform the controller about
    the button click status.
    
    Fixed: 486087863
    Change-Id: Ia6551822548a558b734c4d8b345b3f862e140c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747405
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614293}

--- chrome/browser/password_manager/actor_login/internal/actor_login_siwg_controller.h
+++ chrome/browser/password_manager/actor_login/internal/actor_login_siwg_controller.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
-#include "base/time/time.h"
 #include "chrome/browser/password_manager/actor_login/internal/actor_login_metrics_helper.h"
 #include "chrome/browser/password_manager/actor_login/internal/siwg_button_finder.h"
 #include "chrome/common/actor.mojom-forward.h"
@@ -19,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/mojom/autofill_types.mojom-forward.h"
 #include "components/optimization_guide/content/browser/page_content_proto_provider.h"
 #include "components/password_manager/core/browser/actor_login/actor_login_permission_service.h"
+#include "components/password_manager/core/browser/actor_login/actor_login_quality_logger_interface.h"
 #include "components/password_manager/core/browser/actor_login/actor_login_types.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -61,7 +61,9 @@ class ActorLoginSiwgController : public content::WebContentsObserver {
       bool should_store_permission,
       ActorLoginPermissionService& permission_service,
       LoginStatusResultOrErrorReply on_finished_callback,
-      base::WeakPtr<ActionSequenceDelegate> action_sequence_delegate);
+      base::WeakPtr<ActionSequenceDelegate> action_sequence_delegate,
+      base::WeakPtr<ActorLoginQualityLoggerInterface> mqls_logger,
+      base::TimeTicks attempt_login_tool_start_time);
   ActorLoginSiwgController(
       content::WebContents* web_contents,
       const Credential& credential,
@@ -69,7 +71,9 @@ class ActorLoginSiwgController : public content::WebContentsObserver {
       bool should_store_permission,
       ActorLoginPermissionService& permission_service,
       LoginStatusResultOrErrorReply on_finished_callback,
-      base::WeakPtr<ActionSequenceDelegate> action_sequence_delegate);
+      base::WeakPtr<ActionSequenceDelegate> action_sequence_delegate,
+      base::WeakPtr<ActorLoginQualityLoggerInterface> mqls_logger,
+      base::TimeTicks attempt_login_tool_start_time);
   ~ActorLoginSiwgController() override;
 
   // Not copyable or movable.
@@ -87,6 +91,10 @@ class ActorLoginSiwgController : public content::WebContentsObserver {
   // Starts the detection process for SiwG buttons on the current page and
   // clicks the first one found.
   void ClickSiwgButton();
+
+  // Informs the controller about the success of a button click triggered by
+  // the actor framework.
+  void OnButtonClickCompleted(bool success);
 
  private:
   void OnPageContentReceived(
@@ -114,6 +122,8 @@ class ActorLoginSiwgController : public content::WebContentsObserver {
       std::unique_ptr<ActorLoginMetricsHelper> metrics_helper,
       content::webid::FederatedLoginResult result);
 
+  void LogFederatedLoginResult(content::webid::FederatedLoginResult result);
+
   GetPageContentProvider get_page_content_provider_;
   std::unique_ptr<SiwgButtonFinder> siwg_finder_;
   // Invoked once the actions taken by this class to advance the login are
@@ -133,6 +143,13 @@ class ActorLoginSiwgController : public content::WebContentsObserver {
   // SiwG button was found. Keeps the remote alive for the duration of the click
   // action.
   mojo::AssociatedRemote<chrome::mojom::ChromeRenderFrame> chrome_render_frame_;
+
+  // Details of the current federated login attempt.
+  optimization_guide::proto::ActorLoginQuality_AttemptLoginDetails
+      federated_attempt_login_details_;
+
+  base::WeakPtr<ActorLoginQualityLoggerInterface> mqls_logger_;
+  base::TimeTicks attempt_login_tool_start_time_;
 
   base::WeakPtrFactory<ActorLoginSiwgController> weak_ptr_factory_{this};
 };
