fbc2c83ad6a3407788f5b4fa2e8d9b73142282c5
Yao Xiao <yaoxia@chromium.org>
2026-04-14 10:33:59 -0700

    Connect WebPreferences to WebSettings for global ad highlighting
    In a previous CL (https://crrev.com/c/7723798), we added a
    "Highlight ads" checkbox to chrome://subresource-filter-internals. Its
    state was synced to profile preferences and WebPreferences. This CL
    completes the end-to-end integration by connecting WebPreferences to
    WebSettings (in WebView::ApplyWebPreferences), enabling global ad
    highlighting.
    
    To support this, Blink's ad highlighting flag is split into two
    distinct settings:
    1. `HighlightAds`: A global setting driven by the internals page
       (reusing the existing name).
    2. `InspectorHighlightAds`: A setting driven by DevTools.
    
    Rendering now triggers ad highlighting if either flag is enabled.
    
    Bug: 498512077
    Change-Id: Ic88886ddeace5dabcaac599e81225c33a9a3e105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726770
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614591}

--- third_party/blink/renderer/core/exported/web_settings_impl.cc
+++ third_party/blink/renderer/core/exported/web_settings_impl.cc
@@ -522,6 +522,10 @@ void WebSettingsImpl::SetHighlightAds(bool enabled) {
   settings_->SetHighlightAds(enabled);
 }
 
+void WebSettingsImpl::SetInspectorHighlightAds(bool enabled) {
+  settings_->SetInspectorHighlightAds(enabled);
+}
+
 void WebSettingsImpl::SetHyperlinkAuditingEnabled(bool enabled) {
   settings_->SetHyperlinkAuditingEnabled(enabled);
 }
--- third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_overlay_agent.cc
@@ -542,7 +542,8 @@ protocol::Response InspectorOverlayAgent::disable() {
 
 protocol::Response InspectorOverlayAgent::setShowAdHighlights(bool show) {
   show_ad_highlights_.Set(show);
-  frame_impl_->ViewImpl()->GetPage()->GetSettings().SetHighlightAds(show);
+  frame_impl_->ViewImpl()->GetPage()->GetSettings().SetInspectorHighlightAds(
+      show);
   return protocol::Response::Success();
 }
 
