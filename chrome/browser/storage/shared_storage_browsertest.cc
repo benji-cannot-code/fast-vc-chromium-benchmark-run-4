a6add0eda28bd43c7e3b66d06334a23d81f70f2c
James Cook <jamescook@chromium.org>
2026-04-13 23:36:46 -0700

    storage: Update ENABLE_EXTENSIONS in shared_storage_browsertest.cc
    Now that we have extensions on desktop Android, there's a new build flag
    ENABLE_EXTENSIONS_CORE required for "traditional extensions platforms +
    desktop Android". Switch to that, so we get similar extensions testing
    on desktop Android as we do on Win, Mac, Linux and Chrome OS.
    
    See bug for details on the history of the extensions build flags.
    
    Bug: 494643383
    Change-Id: I1afe788ead398fd4376dffed9bfdc8d7c9dce69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759069
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614263}

--- chrome/browser/storage/shared_storage_browsertest.cc
+++ chrome/browser/storage/shared_storage_browsertest.cc
@@ -83,7 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #endif
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "extensions/browser/extension_registrar.h"
 #include "extensions/test/test_extension_dir.h"
@@ -4780,7 +4780,7 @@ class SharedStorageChromeNoParamsBrowserTest
   base::test::ScopedFeatureList attestation_feature_;
 };
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 namespace {
 
 constexpr char kBackgroundJSTemplate[] = R"(
@@ -4879,8 +4879,6 @@ class SharedStorageExtensionBrowserTest
   content::WebContents* GetActiveWebContents() {
     return chrome_test_utils::GetActiveWebContents(this);
   }
-
-  Profile* GetProfile() { return browser()->profile(); }
 
   const extensions::Extension* InstallExtension(
       std::optional<std::string> value_to_set) {
@@ -5397,6 +5395,6 @@ IN_PROC_BROWSER_TEST_F(
                         ->GetPrimaryMainFrame()
                         ->GetStoragePartition()));
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
 }  // namespace storage
