142ee17281a4553df34c1376f7e0f0bdef681098
Evan Liu <evliu@google.com>
2026-04-13 17:44:17 -0700

    Fix potential Use-After-Free data race in ChromeSpeechRecognitionClient
    This CL fixes a cross-thread data race in ChromeSpeechRecognitionClient
    where `on_ready_callback_` was being concurrently modified on the media
    thread (via SetOnReadyCallback) and executed on the main thread
    (via OnRecognizerBound) without synchronization.
    
    To fix this, `on_ready_callback_` is now guarded by the existing
    `is_recognizer_bound_lock_`. The callback is moved into a local
    variable while holding the lock and then executed outside the lock to
    prevent deadlocks.
    
    Fixed: 501546443
    Change-Id: I39a1bcc70d6a71edff5f88bd1d3155fe76563341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759475
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614101}

--- chrome/renderer/media/chrome_speech_recognition_client.h
+++ chrome/renderer/media/chrome_speech_recognition_client.h
@@ -106,8 +106,6 @@ class ChromeSpeechRecognitionClient
 
   ChromeSpeechRecognitionClient::InitializeCallback initialize_callback_;
 
-  media::SpeechRecognitionClient::OnReadyCallback on_ready_callback_;
-
   base::RepeatingClosure reset_callback_;
 
   // Sends audio to the speech recognition thread on the renderer thread.
@@ -136,6 +134,9 @@ class ChromeSpeechRecognitionClient
   // Protects `is_recognizer_bound_` when it's accessed from the main and
   // rendering threads concurrently.
   mutable base::Lock is_recognizer_bound_lock_;
+
+  media::SpeechRecognitionClient::OnReadyCallback on_ready_callback_
+      GUARDED_BY(is_recognizer_bound_lock_);
 
   // A flag indicating whether the speech recognition service supports
   // multichannel audio.
