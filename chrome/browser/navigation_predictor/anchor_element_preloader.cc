e26ef62a993339219358e4c0c282cdfb72fe88eb
Mike West <mkwst@chromium.org>
2026-04-14 03:05:56 -0700

    [AnchorElementPreloader] Select the correct NAK.
    In `AnchorElementPreloader::MaybePreconnect`, we're currently creating a
    NAK based on the site of the target document. We can do better than
    that by selecting the NAK associated with the initiating
    RenderFrameHost.
    
    Bug: 496093673
    Change-Id: I3ee2558641c772c2e15115707ed9f91e8f8856b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735711
    Auto-Submit: Mike West <mkwst@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614346}

--- chrome/browser/navigation_predictor/anchor_element_preloader.cc
+++ chrome/browser/navigation_predictor/anchor_element_preloader.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/navigation_predictor/anchor_element_preloader.h"
+
 #include "base/functional/callback.h"
 #include "base/metrics/histogram_functions.h"
 #include "chrome/browser/predictors/loading_predictor.h"
@@ -14,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/preloading.h"
 #include "content/public/browser/preloading_data.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+#include "net/base/isolation_info.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "third_party/blink/public/common/features.h"
@@ -113,9 +116,9 @@ void AnchorElementPreloader::MaybePreconnect(const GURL& target) {
   attempt->SetTriggeringOutcome(
       content::PreloadingTriggeringOutcome::kTriggeredButOutcomeUnknown);
 
-  net::SchemefulSite schemeful_site(target);
   auto network_anonymization_key =
-      net::NetworkAnonymizationKey::CreateSameSite(std::move(schemeful_site));
+      render_frame_host_->GetIsolationInfoForSubresources()
+          .network_anonymization_key();
   loading_predictor->PreconnectURLIfAllowed(target, /*allow_credentials=*/true,
                                             network_anonymization_key);
 }
