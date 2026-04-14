750281456f9cff93b28ff7ef321cd3fa701352ec
Yaron Friedman <yfriedman@google.com>
2026-04-14 11:51:31 -0700

    Revert "Speculative fix for white page on load."
    This reverts commit 16d3c6db45045cba14433c6630bb00d964fdafca.
    Reason: We still get repros, better to not do this
    
    Bug: 461315951
    
    Change-Id: I2b4eb83c4c1353a730e6ba324611ef35dc690ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762101
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614634}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -452,7 +452,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kSubmenusInAppMenu,
     &kSubmenusTabContextMenuLffTabStrip,
     &kSuppressToolbarCapturesAtGestureEnd,
-    &kSurfaceDestroyedRecreatesManager,
     &kTabBottomSheet,
     &kTabClosureMethodRefactor,
     &kTabModelInitFixes,
@@ -813,7 +812,6 @@ BASE_FEATURE(kStartSurfaceReturnTime, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kSubmenusInAppMenu, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSubmenusTabContextMenuLffTabStrip, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kSuppressToolbarCapturesAtGestureEnd, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kSurfaceDestroyedRecreatesManager, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kTabBottomSheet, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kTabClosureMethodRefactor, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kTabModelInitFixes, base::FEATURE_ENABLED_BY_DEFAULT);
