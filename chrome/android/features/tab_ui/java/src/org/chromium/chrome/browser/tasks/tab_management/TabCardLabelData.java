843080007bf8e9d9debdced45d12cb8cfb6f1407
Dan Polanco <polardz@google.com>
2026-04-14 07:33:06 -0700

    Move TextResolver to components/browser_ui/util
    Bypass-Check-License: Moving file.
    Change-Id: I2dbdf689fb1e72e9e8c10d927c28bca0d600201a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759396
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614434}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabCardLabelData.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabCardLabelData.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.components.browser_ui.util.TextResolver;
 import org.chromium.components.browser_ui.widget.async_image.AsyncImageView;
 
 /** Data for the {@link TabCardLabelView}. */
