b0322718d263c54dd3cb6a42eeab8283f6789ede
Christopher Grant <cjgrant@chromium.org>
2026-04-14 05:24:50 -0700

    NTP Promos: Fix reading of promo type feature param
    The "setup list" type option was removed from code.  However, if a user
    explicitly set their promo type to setup list, new code cannot parse
    that option. We can revert the removal of the enum value, but that
    means restoring and handling an obsolete option in switch statements.
    Instead, just ignore the feature param entirely - if promos are on, they
    are "simple" type.  In beta, this is netting a handful of "dump without
    crashing" instances, so it should be fixed and merged to M148.
    
    A follow-up CL will simplify the promo type getter function to a
    simple boolean return value.
    
    Bug: 501310002
    Change-Id: I9243ff9d85f1a8056ebf452702bc30a9d0139d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7756039
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614381}

--- components/user_education/common/user_education_features.cc
+++ components/user_education/common/user_education_features.cc
@@ -183,7 +183,7 @@ BASE_FEATURE_ENUM_PARAM(NtpBrowserPromoType,
 
 NtpBrowserPromoType GetNtpBrowserPromoType() {
   if (base::FeatureList::IsEnabled(kEnableNtpBrowserPromos)) {
-    return kNtpBrowserPromoType.Get();
+    return NtpBrowserPromoType::kSimple;
   }
   return NtpBrowserPromoType::kNone;
 }
