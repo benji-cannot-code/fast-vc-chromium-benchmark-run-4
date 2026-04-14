271e3b6071db850d8bce4156239b360461bd9a43
James Cook <jamescook@chromium.org>
2026-04-13 20:12:17 -0700

    extensions: Port ServiceWorkerTrackingBrowserTest to desktop Android
    As well as ServiceWorkerStopTrackingBrowserTest and similar tests in
    service_worker_tracking_browsertest.cc
    
    We skipped porting this file when we initially brought up
    chrome/browser/extensions. The tests pass with little modification.
    
    Bug: 469417243
    Change-Id: I0f6b3e1a2936d7b47d14b2a990d4dd2a92640261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759312
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614180}

--- chrome/browser/extensions/service_worker_tracking_browsertest.cc
+++ chrome/browser/extensions/service_worker_tracking_browsertest.cc
@@ -18,9 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/service_worker_context.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/result_codes.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -316,8 +315,8 @@ class ServiceWorkerIdTrackingBrowserTest
 
     // Navigate somewhere to trigger the start of the worker to handle the
     // webNavigation.onBeforeRequest event.
-    ASSERT_TRUE(ui_test_utils::NavigateToURL(
-        browser(),
+    ASSERT_TRUE(NavigateToURL(
+        GetActiveWebContents(),
         embedded_test_server()->GetURL("example.com", "/simple.html")));
 
     // Wait for the new worker instance to be added to `WorkerIdSet` (registered
@@ -836,8 +835,9 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerRendererTrackingBrowserTest,
 
   // To indirectly confirm that the extension without a worker loaded in the
   // renderer we navigate to a page and wait for the content script to run.
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("/extensions/test_file.html")));
+  ASSERT_TRUE(NavigateToURL(
+      GetActiveWebContents(),
+      embedded_test_server()->GetURL("/extensions/test_file.html")));
   {
     SCOPED_TRACE("waiting for extension without worker to load");
     ASSERT_TRUE(extension_without_worker_loaded.WaitUntilSatisfied());
