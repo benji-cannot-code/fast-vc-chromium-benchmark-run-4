b4e72a7e5e9f02d38e181c232d928d7cf6f0a200
Calder Kitagawa <ckitagawa@chromium.org>
2026-04-14 12:54:56 -0700

    [IA] Disable bottom bar on NTP
    Fixed: 502538483
    Change-Id: If382fb140b75fbfb28c626d4f0270a61252a8319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762190
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614679}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -2409,7 +2409,8 @@ public class ToolbarManager
         BottomBarContainerCoordinator bottomBarContainerCoordinator =
                 new BottomBarContainerCoordinator(
                         bottomAppBarContainer.findViewById(R.id.bottom_container_slot),
-                        mBottomControlsStacker::requestLayerUpdate);
+                        mBottomControlsStacker::requestLayerUpdate,
+                        mCurrentTabSupplier);
         bottomBarContainerOneshotSupplier.set(bottomBarContainerCoordinator);
 
         if (mBottomBarHostManager != null) {
