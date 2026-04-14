677e55cb5d83bd1b3a1acea01eba9bb7c3b32018
Egor Pasko <pasko@chromium.org>
2026-04-14 04:54:46 -0700

    net: Record UMA histogram time for OnHandshakeIOComplete
    Bug: 501050526
    Change-Id: Idaeb1151580915f53109794fef0517d512d23380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736031
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614374}

--- net/socket/ssl_client_socket_impl.cc
+++ net/socket/ssl_client_socket_impl.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_view_util.h"
 #include "base/synchronization/lock.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -1239,6 +1240,10 @@ void SSLClientSocketImpl::DoConnectCallback(int rv) {
 }
 
 void SSLClientSocketImpl::OnHandshakeIOComplete(int result) {
+  std::optional<base::ElapsedTimer> timer;
+  if (base::ShouldRecordSubsampledMetric(0.001)) {
+    timer.emplace();
+  }
   int rv = DoHandshakeLoop(result);
   if (rv != ERR_IO_PENDING) {
     if (in_confirm_handshake_) {
@@ -1248,6 +1253,11 @@ void SSLClientSocketImpl::OnHandshakeIOComplete(int result) {
       LogConnectEndEvent(rv);
     }
     DoConnectCallback(rv);
+  }
+  if (timer) {
+    base::UmaHistogramTimes(
+        "Net.SSLClientSocketImpl.OnHandshakeIOCompleteDuration",
+        timer->Elapsed());
   }
 }
 
