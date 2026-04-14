ac67f3a635354378babab97235560b6922a772f7
Jose Arturo Barrera <jabgonzalez@google.com>
2026-04-14 12:18:52 -0700

    spanification: automatically spanify .../thread_pool/pooled_single_thread_task_runner_manager.h etc.
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    gemini-run/batch-run-1761696331/group_3
    
    Bug: 439964610
    Change-Id: I19779ef444e069adc8d322bc8ec2b897389a8aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7710185
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: José Arturo Barrera <jabgonzalez@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614647}

--- base/task/thread_pool/pooled_single_thread_task_runner_manager.h
+++ base/task/thread_pool/pooled_single_thread_task_runner_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_TASK_THREAD_POOL_POOLED_SINGLE_THREAD_TASK_RUNNER_MANAGER_H_
 #define BASE_TASK_THREAD_POOL_POOLED_SINGLE_THREAD_TASK_RUNNER_MANAGER_H_
 
+#include <array>
 #include <memory>
 #include <string>
 #include <vector>
@@ -126,7 +127,7 @@ class BASE_EXPORT PooledSingleThreadTaskRunnerManager final {
   template <typename DelegateType>
   WorkerThread*& GetSharedWorkerThreadForTraits(
       const TaskTraits& traits,
-      ThreadType originating_thread_type);
+      ThreadType originating_thread_type) EXCLUSIVE_LOCKS_REQUIRED(lock_);
 
   void UnregisterWorkerThread(WorkerThread* worker);
 
@@ -150,12 +151,13 @@ class BASE_EXPORT PooledSingleThreadTaskRunnerManager final {
   // CONTINUE_ON_SHUTDOWN to avoid being in a situation where a
   // CONTINUE_ON_SHUTDOWN task effectively blocks shutdown by preventing a
   // BLOCK_SHUTDOWN task to be scheduled. https://crbug.com/829786
-  WorkerThread* shared_worker_threads_[ENVIRONMENT_COUNT]
-                                      [CONTINUE_ON_SHUTDOWN_COUNT] GUARDED_BY(
-                                          lock_) = {};
+  std::array<std::array<WorkerThread*, CONTINUE_ON_SHUTDOWN_COUNT>,
+             ENVIRONMENT_COUNT>
+      shared_worker_threads_ GUARDED_BY(lock_) = {};
 #if BUILDFLAG(IS_WIN)
-  WorkerThread* shared_com_worker_threads_
-      [ENVIRONMENT_COUNT][CONTINUE_ON_SHUTDOWN_COUNT] GUARDED_BY(lock_) = {};
+  std::array<std::array<WorkerThread*, CONTINUE_ON_SHUTDOWN_COUNT>,
+             ENVIRONMENT_COUNT>
+      shared_com_worker_threads_ GUARDED_BY(lock_) = {};
 #endif  // BUILDFLAG(IS_WIN)
 
   // Set to true when Start() is called.
