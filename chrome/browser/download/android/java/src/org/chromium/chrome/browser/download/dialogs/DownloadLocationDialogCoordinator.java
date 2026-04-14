581671927757c5fa9c6bb4cd86dd96aae1025103
Gazal <agazal@google.com>
2026-04-14 07:36:33 -0700

    [histogram-cleanup] Remove expired histogram: MobileDownload.Location.Dialog.DirectoryType
    This histogram expired on 2022-02-16.
    
    Also removing the following expired histograms from the same file:
    - Mobile.ContextMenu.LensWebImageProcessTime
    - Mobile.ContextMenu.LensWebUploadStatus
    - Mobile.ContextMenu.LensWebUploadTime
    - MobileDownload.DownloadLaterPromptStatus
    - MobileDownload.Location.Download.DirectoryType
    - MobileFre.CctTos.LoadingDuration
    - MobileFre.TosFragment.SpinnerVisibleDuration
    
    Bug: 501943318
    Change-Id: I2ffbee168d5343b4952a3a6e65f1882a61f8abd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7752918
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614437}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLocationDialogCoordinator.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLocationDialogCoordinator.java
@@ -13,7 +13,6 @@ import android.content.res.Resources;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -349,11 +348,6 @@ public class DownloadLocationDialogCoordinator implements ModalDialogProperties.
         // Update native with new path.
         DownloadDialogBridge.setDownloadAndSaveFileDefaultDirectory(
                 mProfile, directoryOption.location);
-
-        RecordHistogram.recordEnumeratedHistogram(
-                "MobileDownload.Location.Dialog.DirectoryType",
-                directoryOption.type,
-                DirectoryOption.DownloadLocationDirectoryType.NUM_ENTRIES);
 
         File file = new File(directoryOption.location, fileName);
 
