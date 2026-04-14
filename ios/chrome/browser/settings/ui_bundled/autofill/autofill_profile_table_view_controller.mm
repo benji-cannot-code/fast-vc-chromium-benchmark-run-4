f9551a55283cfe23d02e329c3a07ea702c7888b5
Alexis Hétu <sugoi@chromium.org>
2026-04-14 07:07:40 -0700

    [iOS] Fix voice over for toggles in settings
    A few toggles in settings were not getting the proper state read by
    VoiceOver. This was caused by missing "reconfigureCellsForItems"
    calls, which this CL adds to fix the issue.
    
    Bug: 502203759
    Change-Id: I999f5e4ed887e02e227838208317a47e8fe4ec30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7756969
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614420}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
@@ -1127,6 +1127,7 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
       base::apple::ObjCCastStrict<TableViewSwitchItem>(
           [model itemAtIndexPath:switchPath]);
   switchItem.on = on;
+  [self reconfigureCellsForItems:@[ switchItem ]];
 }
 
 // Sets switchItem's enabled status to `enabled` and reconfigures the
