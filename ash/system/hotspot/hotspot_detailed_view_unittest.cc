1ca5abb1e57dbc56a98ebb877d4ff6282f0fcd2d
hiroki oshima <hiroki.oshima@gmail.com>
2026-04-13 23:06:51 -0700

    fix HotspotDetailedViewTest dangling pointer
    Bug: 40212619
    Change-Id: Ibf2390da9d5738b42ac264c8c2840e9c2879cae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7750350
    Reviewed-by: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614238}

--- ash/system/hotspot/hotspot_detailed_view_unittest.cc
+++ ash/system/hotspot/hotspot_detailed_view_unittest.cc
@@ -76,6 +76,7 @@ class HotspotDetailedViewTest : public AshTestBase {
 
   void TearDown() override {
     normal_duration_.reset();
+    hotspot_detailed_view_ = nullptr;
     widget_.reset();
 
     AshTestBase::TearDown();
@@ -158,8 +159,7 @@ class HotspotDetailedViewTest : public AshTestBase {
   std::unique_ptr<views::Widget> widget_;
   FakeHotspotDetailedViewDelegate hotspot_detailed_view_delegate_;
   FakeDetailedViewDelegate detailed_view_delegate_;
-  raw_ptr<HotspotDetailedView, DanglingUntriaged> hotspot_detailed_view_ =
-      nullptr;
+  raw_ptr<HotspotDetailedView> hotspot_detailed_view_ = nullptr;
   std::optional<gfx::ScopedAnimationDurationScaleMode> normal_duration_;
 };
 
