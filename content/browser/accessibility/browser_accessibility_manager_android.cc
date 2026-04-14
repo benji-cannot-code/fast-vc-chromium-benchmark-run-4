a520905d71bdd7ef29d7e738e6cebb4c627eaac1
Ramin Halavati <rhalavati@chromium.org>
2026-04-13 21:36:29 -0700

    a11y-android: Check node has parent when updating selection for Android.
    In `BAMA::ConvertChromeSelectionPositionToAndroid`, make sure that the
    target node has a parent before returning it as the selection anchor.
    
    AX-Relnotes: n/a
    Bug: 501317424, 443078007
    Change-Id: Ic5c4234476c706ec0f9d911260b2c04e4641fc17
    Cq-Include-Trybots: luci.chromium.try:android-16-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7753148
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614217}

--- content/browser/accessibility/browser_accessibility_manager_android.cc
+++ content/browser/accessibility/browser_accessibility_manager_android.cc
@@ -326,6 +326,7 @@ void BrowserAccessibilityManagerAndroid::FireDocumentSelectionChangedEvent(
   }
 
   // Send event to the focus node.
+  CHECK(selection->focus_object);
   wcax->HandleTextSelectionChanged(selection->focus_object->GetUniqueId());
 }
 
@@ -1039,9 +1040,16 @@ BrowserAccessibilityManagerAndroid::ConvertChromeSelectionPositionToAndroid(
   if (at_end_of_anchor) {
     offset++;
   }
-  return std::make_pair(static_cast<BrowserAccessibilityAndroid*>(
-                            GetFromAXNode(target_node->GetUnignoredParent())),
-                        offset);
+
+  BrowserAccessibilityAndroid* parent_node =
+      static_cast<BrowserAccessibilityAndroid*>(
+          GetFromAXNode(target_node->GetUnignoredParent()));
+  // TODO(crbug.com/498376490): Find a test case that triggers this behavior.
+  if (!parent_node) {
+    return std::nullopt;
+  }
+
+  return std::make_pair(parent_node, offset);
 }
 
 ui::BrowserAccessibility::AXPosition
