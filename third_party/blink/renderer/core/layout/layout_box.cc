46dd64574c676ae32f1304de81e4388b2a8a5a19
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-04-14 06:40:42 -0700

    [layout] Don't manually invalidate layout on position change.
    This logic was originally added in:
    https://chromium.googlesource.com/chromium/src/+/e086feb9f5505f31b555c9d9f0b8084a0d30c424
    
    We reinsert the layout object on out-of-flow change, so this logic would
    only be triggered on fixed->absolute & absolute->fixed.
    
    We don't need to manually invalidate the parent now. The only
    information that gets set by the parent is the static-position
    information which is the same for fixedpos & abspos.
    
    There should be no behaviour change.
    
    Bug: 469120960
    Change-Id: I19634cf33161280f669cd0f494f532bdf40710b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7751034
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614403}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -603,8 +603,6 @@ void LayoutBox::StyleWillChange(StyleDifference diff,
 
         if (old_style->GetPosition() == EPosition::kStatic) {
           SetShouldDoFullPaintInvalidation();
-        } else if (new_style.HasOutOfFlowPosition()) {
-          Parent()->SetChildNeedsLayout();
         }
       }
     }
