693842ec1dcde5bbe9e1ff499347eb672f2c344d
Arthur Milchior <arthurmilchior@google.com>
2026-04-14 05:03:35 -0700

    [iOS]Refactor Google Service’s setting into modular layered directories
    Moves all google_services_settings_* files into their respective layered
    architecture directories (coordinator, ui, test) and adds appropriate
    DEPS files.
    
    This CL also remove a file that was introduced to facilitate
    ios_internal migration and is not needed anymore.
    
    This CL is part of the chain:
    * crrev.com/c/7746505
    * crrev.com/i/9187956
    + crrev.com/c/7746644
    
    Change done by antigravity
    
    Bypass-Check-License: Moved files
    Change-Id: I220439b89808043c1b2ee8cd06fa5d99445fddc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7746644
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614377}

--- ios/chrome/browser/settings/ui_bundled/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/sync/sync_encryption_passphrase_table_view_controller_egtest.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/test/signin_matchers.h"
 #import "ios/chrome/browser/metrics/model/metrics_app_interface.h"
 #import "ios/chrome/browser/settings/google_services/manage_sync/public/manage_sync_settings_constants.h"
-#import "ios/chrome/browser/settings/ui_bundled/google_services/google_services_settings_constants.h"
+#import "ios/chrome/browser/settings/google_services/public/google_services_settings_constants.h"
 #import "ios/chrome/browser/settings/ui_bundled/settings_table_view_controller_constants.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/grit/ios_strings.h"
