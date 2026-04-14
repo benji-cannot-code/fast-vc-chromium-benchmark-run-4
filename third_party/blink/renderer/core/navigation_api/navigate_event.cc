f71ab011635552aafa819d5070415d92126c4400
Noam Rosenthal <nrosenthal@chromium.org>
2026-04-14 11:32:14 -0700

    Scroll to top on navigation.scroll() to a non-existent fragment
    See spec: https://html.spec.whatwg.org/multipage/nav-history-apis.html#process-scroll-behavior
    "If document's indicated part is null, then scroll to the beginning of
    the document given document."
    
    Bug: 479874917
    Change-Id: I33c5d1bf3a66d6b2761032eb06825c51e089ae07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7743305
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614620}

--- third_party/blink/renderer/core/navigation_api/navigate_event.cc
+++ third_party/blink/renderer/core/navigation_api/navigate_event.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/navigation_api/navigate_event.h"
 
 #include "base/functional/bind.h"
+#include "cc/trees/scroll_source_type.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom-shared.h"
 #include "third_party/blink/public/mojom/frame/frame.mojom-blink.h"
@@ -44,8 +45,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/navigation_api/navigation_destination.h"
 #include "third_party/blink/renderer/core/navigation_api/navigation_precommit_controller.h"
 #include "third_party/blink/renderer/core/navigation_api/navigation_type_util.h"
+#include "third_party/blink/renderer/core/page/scrolling/fragment_anchor.h"
 #include "third_party/blink/renderer/core/scheduler/task_attribution_top_level_override_scope.h"
 #include "third_party/blink/renderer/core/scheduler/task_attribution_util.h"
+#include "third_party/blink/renderer/core/scroll/scrollable_area.h"
 #include "third_party/blink/renderer/core/timing/dom_window_performance.h"
 #include "third_party/blink/renderer/core/timing/responsiveness_metrics.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
@@ -746,6 +749,14 @@ void NavigateEvent::ProcessScrollBehavior() {
   auto scroll_behavior = has_ua_visual_transition_
                              ? mojom::blink::ScrollBehavior::kInstant
                              : mojom::blink::ScrollBehavior::kAuto;
+
+  if (!FragmentAnchor::TryCreate(dispatch_params_->url,
+                                 *DomWindow()->GetFrame(), false)) {
+    DomWindow()->GetFrame()->View()->GetScrollableArea()->SetScrollOffset(
+        ScrollOffset(0, 0), mojom::blink::ScrollType::kProgrammatic,
+        cc::ScrollSourceType::kNone, mojom::blink::ScrollBehavior::kAuto);
+  }
+
   // Use mojom::blink::ScrollRestorationType::kAuto unconditionally here
   // because we are certain that we want to actually scroll if we reach this
   // point. Using mojom::blink::ScrollRestorationType::kManual would block the
