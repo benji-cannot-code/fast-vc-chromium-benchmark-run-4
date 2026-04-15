d962459effb6edf9d55861bea202fc20debf1aa1
Michael Wu <mklw@google.com>
2026-04-15 11:54:30 -0700

    [Tab Strip Glic Button] Make spark icon monochrome when Glic shows
    https://screencast.googleplex.com/cast/NjM3Njc1MzA2Nzk4Mjg0OHwyYTE2MDRiYS02Mg
    
    Bug: 493250907
    Change-Id: Id6ee5758792a7d2746952ed6f1da734a917d9995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762341
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615314}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -266,6 +266,7 @@ public class StripLayoutHelperManager
     private int mOrientation;
     private final Runnable mGlicClickHandler;
     private @Nullable TintedCompositorTextButton mGlicButton;
+    private boolean mIsGlicUIVisible;
     private @Nullable TintedCompositorButton mModelSelectorButton;
     private final Context mContext;
     private float mStripTransitionScrimOpacity;
@@ -565,6 +566,7 @@ public class StripLayoutHelperManager
         if (mGlicKeyedService != null) {
             mGlicUIObserver =
                     isOpened -> {
+                        mIsGlicUIVisible = isOpened;
                         if (mGlicButton != null && mRenderHost != null) {
                             mGlicButton.setPressed(isOpened);
                             mRenderHost.requestRender();
@@ -840,8 +842,14 @@ public class StripLayoutHelperManager
 
         updateGlicButtonOpacity();
 
+        mGlicButton.setTint(SemanticColorUtils.getDefaultIconColor(context));
+
         mGlicButton.setAccessibilityDescription(
                 context.getString(R.string.glic_tab_strip_button_tooltip));
+    }
+
+    public boolean isGlicUIVisible() {
+        return mIsGlicUIVisible;
     }
 
     private void createModelSelectorButton(
--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayerTest.java
@@ -641,6 +641,7 @@ public class TabStripSceneLayerTest {
                         anyBoolean(),
                         anyBoolean(),
                         anyInt(),
+                        anyBoolean(),
                         anyInt(),
                         anyFloat(),
                         eq(true),
