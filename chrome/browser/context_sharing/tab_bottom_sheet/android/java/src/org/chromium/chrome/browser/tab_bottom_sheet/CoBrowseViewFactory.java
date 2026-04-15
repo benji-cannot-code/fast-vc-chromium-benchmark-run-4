dcbc6de3d1d4bcdad0f1e1171312a17a0d82b6c7
Shuhei Takahashi <nya@chromium.org>
2026-04-14 18:43:11 -0700

    Revert "Remove toolbar from TabBottomSheet"
    This reverts commit 92945917f1f9fb1c81042c82e3e68cefe2ef899b.
    
    Reason for revert: Suspected to break glic-related tests on Android (crbug.com/502723965)
    
    Original change's description:
    > Remove toolbar from TabBottomSheet
    >
    > This CL removes the toolbar from TabBottomSheet as it was never
    > used by any client.
    >
    > Bug: None
    > Change-Id: I62f28e0ec8e0cc4cdec60720884c28eae849a321
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762768
    > Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    > Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    > Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1614662}
    
    Bug: 502723965
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Change-Id: I36237c1854680a551a99af48339ebbb11266390d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761365
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614882}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/CoBrowseViewFactory.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/CoBrowseViewFactory.java
@@ -77,10 +77,14 @@ public class CoBrowseViewFactory {
      * Builds the co-browse views.
      *
      * @param webContents The {@link WebContents} to be displayed in the thin web view.
+     * @param showToolbar Whether to show the toolbar.
      * @param showFusebox Whether to show the fusebox.
      * @return The {@link CoBrowseViews} instance.
      */
-    CoBrowseViews buildCoBrowseViews(@Nullable WebContents webContents, boolean showFusebox) {
+    CoBrowseViews buildCoBrowseViews(
+            @Nullable WebContents webContents, boolean showToolbar, boolean showFusebox) {
+        TabBottomSheetToolbar toolbar =
+                showToolbar ? new TabBottomSheetSimpleToolbar(mActivity) : null;
         TabBottomSheetWebUi webUi =
                 new TabBottomSheetWebUi(mActivity, mWindowAndroid, mContextMenuPopulatorFactory);
         ContextualTasksFusebox fusebox =
@@ -98,7 +102,7 @@ public class CoBrowseViewFactory {
 
         webUi.setWebContents(webContents);
 
-        return new CoBrowseViews(mActivity, webUi, fusebox);
+        return new CoBrowseViews(mActivity, toolbar, webUi, fusebox);
     }
 
     @CalledByNative
@@ -106,11 +110,12 @@ public class CoBrowseViewFactory {
     public static @Nullable CoBrowseViews buildCoBrowseViews(
             @JniType("ui::WindowAndroid*") WindowAndroid windowAndroid,
             @Nullable @JniType("content::WebContents*") WebContents webContents,
+            boolean showToolbar,
             boolean showFusebox) {
         CoBrowseViewFactory factory = TabBottomSheetUtils.getFactoryFromWindow(windowAndroid);
         if (factory == null) {
             return null;
         }
-        return factory.buildCoBrowseViews(webContents, showFusebox);
+        return factory.buildCoBrowseViews(webContents, showToolbar, showFusebox);
     }
 }
--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/CoBrowseViews.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/CoBrowseViews.java
@@ -31,6 +31,7 @@ import org.chromium.content_public.browser.WebContents;
  */
 @NullMarked
 public class CoBrowseViews {
+    private final @Nullable TabBottomSheetToolbar mToolbar;
     private final @Nullable TabBottomSheetWebUi mWebUi;
     private final @Nullable ContextualTasksFusebox mFusebox;
     private final View mView;
@@ -40,13 +41,16 @@ public class CoBrowseViews {
      * Constructor for CoBrowseViews.
      *
      * @param context The context for the view.
+     * @param toolbar The toolbar for the view.
      * @param webUi The web UI for the view.
      * @param fusebox The fusebox for the view.
      */
     public CoBrowseViews(
             Context context,
+            @Nullable TabBottomSheetToolbar toolbar,
             @Nullable TabBottomSheetWebUi webUi,
             @Nullable ContextualTasksFusebox fusebox) {
+        mToolbar = toolbar;
         mWebUi = webUi;
         mFusebox = fusebox;
         mView = buildView(context);
@@ -57,6 +61,11 @@ public class CoBrowseViews {
         TabBottomSheetWebUiContainer webUiContainer =
                 assertNonNull(mView.findViewById(R.id.web_ui_container));
         webUiContainer.setTouchHandler(touchHandler);
+    }
+
+    /** Returns whether the toolbar is present. */
+    public boolean hasToolbar() {
+        return mToolbar != null;
     }
 
     /** Returns the view for the co-browse content. */
@@ -73,9 +82,13 @@ public class CoBrowseViews {
     @CalledByNative
     @VisibleForTesting
     void destroy() {
+        ViewGroup toolbarContainer = mView.findViewById(R.id.toolbar_container);
         ViewGroup webUiContainer = mView.findViewById(R.id.web_ui_container);
         ViewGroup fuseboxContainer = mView.findViewById(R.id.fusebox_container);
         ViewGroup peekContainer = mView.findViewById(R.id.actor_control_container);
+        if (mToolbar != null) {
+            toolbarContainer.removeAllViews();
+        }
         if (mWebUi != null) {
             webUiContainer.removeAllViews();
             mWebUi.destroy();
@@ -138,6 +151,9 @@ public class CoBrowseViews {
     }
 
     int getToolbarHeight() {
+        if (mToolbar != null) {
+            return mToolbar.getToolbarView().getHeight();
+        }
         return 0;
     }
 
@@ -157,10 +173,16 @@ public class CoBrowseViews {
 
     private View buildView(Context context) {
         View view = LayoutInflater.from(context).inflate(R.layout.tab_bottom_sheet, null);
+        ViewGroup toolbarContainer = view.findViewById(R.id.toolbar_container);
         ViewGroup webUiContainer = view.findViewById(R.id.web_ui_container);
         ViewGroup fuseboxContainer = view.findViewById(R.id.fusebox_container);
         ViewGroup peekContainer = view.findViewById(R.id.actor_control_container);
 
+        if (mToolbar != null) {
+            View toolbarView = mToolbar.getToolbarView();
+            detachFromParent(toolbarView);
+            toolbarContainer.addView(toolbarView);
+        }
         if (mWebUi != null) {
             View webUiView = mWebUi.getWebUiView();
             detachFromParent(webUiView);
