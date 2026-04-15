489e1a34a9e19a66d40f245a2a36a2112572f24b
dpapad <dpapad@chromium.org>
2026-04-14 17:06:13 -0700

    History: Extract 'change-query' CustomEvent to a dedicated type.
    Also declaring it with the HTMLElementEventMap so that TS can
    automatically infer it whenever used with eventToPromise().
    
    Bug: 494464740
    Change-Id: Ia2cdb20017c4010a6324f3c35a3a9581696664e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7746492
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614782}

--- chrome/browser/resources/history/query_manager.ts
+++ chrome/browser/resources/history/query_manager.ts
@@ -26,6 +26,19 @@ export function convertDateToQueryValue(date: Date) {
   return `${fullYear}-${twoDigits(month)}-${twoDigits(day)}`;
 }
 
+export type ChangeQueryEvent = CustomEvent<{
+  search: string | null,
+  after: string | null,
+  includeUserVisits?: boolean,
+  includeActorVisits?: boolean,
+}>;
+
+declare global {
+  interface HTMLElementEventMap {
+    'change-query': ChangeQueryEvent;
+  }
+}
+
 export class HistoryQueryManagerElement extends CrLitElement {
   static get is() {
     return 'history-query-manager';
@@ -119,12 +132,7 @@ export class HistoryQueryManagerElement extends CrLitElement {
     promise.then((result) => this.onQueryResult_(result.results), () => {});
   }
 
-  private onChangeQuery_(e: CustomEvent<{
-    search: string,
-    after: string,
-    includeUserVisits?: boolean,
-    includeActorVisits?: boolean,
-  }>) {
+  private onChangeQuery_(e: ChangeQueryEvent) {
     const changes = e.detail;
     let needsUpdate = false;
 
