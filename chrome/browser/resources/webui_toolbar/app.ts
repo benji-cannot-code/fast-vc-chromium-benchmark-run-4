b5328f699c37a87be4b25426167fa1770b73a6e5
Matt Menke <mmenke@chromium.org>
2026-04-15 10:21:59 -0700

    Add double-click "debouncing" to the WebUI reload button.
    In particular, whenever we're showing the reload icon and it's clicked,
    keep displaying the reload icon for the double-click interval, ignoring
    any clicks during that period.
    
    We do not apply the double-click delay while clicking the stop icon,
    since the old code effectively doesn't seem do that. The old code is
    not very well documented, and code calling into it to inform it of state
    changes makes it difficult to know exactly what it's expected to be
    doing, so this change is aimed more at mimicking the old behavior,
    rather than exactly matching it.
    
    This CL puts all the logic into the TypeScript side of the reload
    button, passing it the system double-click delay and the number of
    times ChangeMode(force=true) has been called, so the debouncing logic
    can be overridden by that boolean, which is used to indicate when the
    focused tab changes.
    
    This does not add the behavior of disabling the stop button for 1.3
    seconds after loading completes, if the mouse is hovering over it, and
    the stop button was not pressed, which will be done in another CL.
    
    Bug: 498170189
    Change-Id: Icbce8c14f4dffa76d9d4b5f03595438e674a8bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727664
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615261}

--- chrome/browser/resources/webui_toolbar/app.ts
+++ chrome/browser/resources/webui_toolbar/app.ts
@@ -90,9 +90,14 @@ export class ToolbarAppElement extends CrLitElement {
       loadTimeData.getBoolean('enablePinnedToolbarActions');
   protected accessor navigationControlsState_: NavigationControlsState = {
     reloadControlState: {
+      // While this will be overwritten anyways, this matches the default value
+      // on some platforms.
+      doubleClickInterval: {microseconds: BigInt(500 * 1000)},
+
       canShowMenu: false,
       isNavigationLoading: false,
       isContextMenuVisible: false,
+      resetStateCount: 0,
     },
     splitTabsControlState: {
       isCurrentTabSplit: false,
