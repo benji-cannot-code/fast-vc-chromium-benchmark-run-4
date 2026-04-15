b6f2b86277c9abe25a19a1c43f222eba0c2754a1
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-04-14 21:03:14 -0700

    [layout] Remove a LayoutObject from its parent first when destroying.
    Previously we'd remove the LayoutObject deep within WillBeDestroyed.
    There is no (good) reason to do this, instead we can just remove first,
    then proceed with destruction.
    
    Change-Id: I7f30f0ce241613d998cc10bd50944371e78382b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7729067
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614929}

--- third_party/blink/renderer/core/layout/layout_text.cc
+++ third_party/blink/renderer/core/layout/layout_text.cc
@@ -274,7 +274,6 @@ void LayoutText::WillBeDestroyed() {
 
   // We skip invoking LayoutObject::WillBeDestroyed as all of the logic (except
   // for removing from the tree) doesn't apply to LayoutText.
-  Remove();
 
 #if DCHECK_IS_ON()
   if (IsInLayoutNGInlineFormattingContext())
