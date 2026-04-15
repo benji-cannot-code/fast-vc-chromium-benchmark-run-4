116312f0e53c0d5aeeb258f674ce4507b2aded18
Bryant Chandler <bryantchandler@google.com>
2026-04-15 10:32:48 -0700

    [glic] Null check instance helper on tab creation
    This avoids a crash, but it means that daisy chaining won't happen in
    this case. I've added a TODO to track figuring out why tab features
    sometimes aren't fully initialized after the tab is created.
    
    Bug: b:501276046
    Change-Id: I7f71d3c9458d80872c3ae2f1708db6806a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7763406
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615278}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/glic/public/glic_side_panel_coordinator.h"
+#include "chrome/browser/glic/service/glic_instance_helper.h"
 #include "chrome/browser/glic/service/glic_ui_embedder.h"
 #include "chrome/browser/glic/service/glic_ui_types.h"
 #include "chrome/browser/glic/suggestions/contextual_cueing_features.h"
@@ -611,7 +612,9 @@ tabs::TabInterface* GlicInstanceImpl::CreateTab(
     return nullptr;
   }
 
-  if (!created_tab) {
+  // TODO(b/501276046): Figure out how to ensure that instance helper is
+  // initialized when we get to this point.
+  if (!created_tab || !GlicInstanceHelper::From(created_tab)) {
     instance_metrics_.OnDaisyChain(DaisyChainSource::kGlicContents,
                                    /*success=*/false, nullptr, source_tab);
     return nullptr;
