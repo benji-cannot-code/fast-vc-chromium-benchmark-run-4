f50eff944f7903c9da10e66af1dbd3a27f73d009
Henrique Nakashima <hnakashima@chromium.org>
2026-04-15 09:25:49 -0700

    [Android] Mark status and offline indicators as @Nullable
    This is part of adding @NullMarked to TabbedRootUiCoordinator.
    
    Bug: 389129271
    Change-Id: I2bf9967851695222bb696006af5c7752ede6fa12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759754
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615231}

--- chrome/android/java/src/org/chromium/chrome/browser/offlinepages/indicator/OfflineIndicatorInProductHelpController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/indicator/OfflineIndicatorInProductHelpController.java
@@ -45,7 +45,6 @@ public class OfflineIndicatorInProductHelpController
         mAppMenuHandler = appMenuHandler;
         mUserEducationHelper = new UserEducationHelper(mActivity, profile, mHandler);
 
-        assert coordinator != null;
         mCoordinator = coordinator;
         mCoordinator.addObserver(this);
     }
