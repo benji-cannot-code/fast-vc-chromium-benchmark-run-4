7ad0256c001d3157e6baef491280ad5a69eb6c41
Kent Tamura <tkent@chromium.org>
2026-04-15 01:28:25 -0700

    Reland "Remove blink::WebString::FromUTF8()"
    This is a reland of commit 64bea51a34e9a0f68769c2275e939d986f918495
    
    Differences from the original commit:
    Update `FromUTF8()` in the following files too:
    * chromecast/renderer/activity_filtering_websocket_handshake_throttle.cc
    * components/translate/content/renderer/translate_agent.cc
    * third_party/blink/renderer/core/script_tools/model_context_test.cc
    
    Original change's description:
    > Remove blink::WebString::FromUTF8()
    >
    > We prefer FromUtf8().
    > This CL has no behavior changes.
    >
    > Bug: 492927412
    > Change-Id: I3976e85012e2189dd4377a7cd49148771970d158
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7760402
    > Commit-Queue: Fredrik Söderquist <fs@opera.com>
    > Owners-Override: Kent Tamura <tkent@chromium.org>
    > Auto-Submit: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Cr-Commit-Position: refs/heads/main@{#1614316}
    
    Bug: 492927412
    Change-Id: I36fa1420e10759ad581bf8d2ccf7552831e10205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7760406
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Owners-Override: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614993}

--- chromecast/renderer/activity_filtering_websocket_handshake_throttle.cc
+++ chromecast/renderer/activity_filtering_websocket_handshake_throttle.cc
@@ -32,7 +32,7 @@ void ActivityFilteringWebSocketHandshakeThrottle::ThrottleHandshake(
   }
 
   std::move(completion_callback)
-      .Run(blink::WebString::FromUTF8(base::StringPrintf(
+      .Run(blink::WebString::FromUtf8(base::StringPrintf(
           "WebSocket connection to %s is blocked", gurl.spec().c_str())));
 }
 
