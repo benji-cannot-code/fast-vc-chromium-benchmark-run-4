92e73c7480183705afefbd490a76fe695590d6e5
jingjinggu <jingjinggu@microsoft.com>
2026-04-14 18:57:10 -0700

    [FRE] Clean up sSupportSkippingTos dead code
    The sSupportSkippingTos boolean was hardcoded to true and its
    @VisibleForTesting setter had zero callers across the entire
    codebase. This removes the dead boolean, the unused setter, the
    always-true guard in the constructor, and the now-unnecessary
    @Nullable on mSkipTosDialogPolicyListener.
    
    Bug: 40156897
    Change-Id: I2677c36b38aab3ef2042edc9163dd48d02b04c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7752787
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: jingjing gu <jingjinggu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1614891}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/LightweightFirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/LightweightFirstRunActivity.java
@@ -17,7 +17,6 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.StringRes;
-import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.IntentUtils;
 import org.chromium.base.ThreadUtils;
@@ -40,11 +39,7 @@ import org.chromium.ui.widget.LoadingView;
 @NullMarked
 public class LightweightFirstRunActivity extends FirstRunActivityBase
         implements LoadingView.Observer {
-    // TODO(crbug.com/40156897) Clean this boolean when releasing this feature, and remove
-    // @Nullable from members below.
-    private static boolean sSupportSkippingTos = true;
-
-    private @Nullable SkipTosDialogPolicyListener mSkipTosDialogPolicyListener;
+    private final SkipTosDialogPolicyListener mSkipTosDialogPolicyListener;
 
     private TextView mTosAndPrivacyTextView;
     private Button mOkButton;
@@ -67,14 +62,12 @@ public class LightweightFirstRunActivity extends FirstRunActivityBase
     public LightweightFirstRunActivity() {
         super();
 
-        if (sSupportSkippingTos) {
-            mSkipTosDialogPolicyListener =
-                    new SkipTosDialogPolicyListener(
-                            getPolicyLoadListener(), EnterpriseInfo.getInstance(), null);
-            // We can ignore the result from #onAvailable here, as views are not created at this
-            // point.
-            mSkipTosDialogPolicyListener.onAvailable((ignored) -> onPolicyLoadListenerAvailable());
-        }
+        mSkipTosDialogPolicyListener =
+                new SkipTosDialogPolicyListener(
+                        getPolicyLoadListener(), EnterpriseInfo.getInstance(), null);
+        // We can ignore the result from #onAvailable here, as views are not created at this
+        // point.
+        mSkipTosDialogPolicyListener.onAvailable((ignored) -> onPolicyLoadListenerAvailable());
     }
 
     @Override
@@ -157,16 +150,14 @@ public class LightweightFirstRunActivity extends FirstRunActivityBase
 
         mViewCreated = true;
 
-        if (mSkipTosDialogPolicyListener != null) {
-            // Check if we need to setup logic for policy loading.
-            if (mSkipTosDialogPolicyListener.get() == null) {
-                mLoadingView.addObserver(this);
-                mLoadingView.showLoadingUi();
-                setTosComponentVisibility(false);
-            } else if (mSkipTosDialogPolicyListener.get()) {
-                setTosComponentVisibility(false);
-                skipTosByPolicy();
-            }
+        // Check if we need to setup logic for policy loading.
+        if (mSkipTosDialogPolicyListener.get() == null) {
+            mLoadingView.addObserver(this);
+            mLoadingView.showLoadingUi();
+            setTosComponentVisibility(false);
+        } else if (mSkipTosDialogPolicyListener.get()) {
+            setTosComponentVisibility(false);
+            skipTosByPolicy();
         }
     }
 
@@ -187,7 +178,7 @@ public class LightweightFirstRunActivity extends FirstRunActivityBase
 
     @Override
     public void onHideLoadingUiComplete() {
-        assert mSkipTosDialogPolicyListener != null && mSkipTosDialogPolicyListener.get() != null;
+        assert mSkipTosDialogPolicyListener.get() != null;
         if (mSkipTosDialogPolicyListener.get()) {
             skipTosByPolicy();
         } else {
@@ -219,7 +210,7 @@ public class LightweightFirstRunActivity extends FirstRunActivityBase
 
         mLoadingView.destroy();
 
-        if (mSkipTosDialogPolicyListener != null) mSkipTosDialogPolicyListener.destroy();
+        mSkipTosDialogPolicyListener.destroy();
 
         if (mHandler != null && mExitFreRunnable != null) {
             mHandler.removeCallbacks(mExitFreRunnable);
@@ -283,10 +274,5 @@ public class LightweightFirstRunActivity extends FirstRunActivityBase
     public void showInfoPage(@StringRes int url) {
         CustomTabActivity.showInfoPage(
                 this, LocalizationUtils.substituteLocalePlaceholder(getString(url)));
-    }
-
-    @VisibleForTesting
-    public static void setSupportSkippingTos(boolean isSupported) {
-        sSupportSkippingTos = isSupported;
     }
 }
