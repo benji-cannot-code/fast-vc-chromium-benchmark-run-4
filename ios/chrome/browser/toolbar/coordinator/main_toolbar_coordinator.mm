2862ba40ed72fbdbf92d2d2d297f7a76289876ff
Gauthier Ambard <gambard@chromium.org>
2026-04-15 06:10:47 -0700

    [iOS] Stop TabGroup indicator coordinator on toolbar stop
    It was forgotten on crrev.com/c/7754582.
    
    Bug: 497685511
    Change-Id: I4fc6eb498f80e4d7191247062cc67d0ef6e9cb6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762010
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615086}

--- ios/chrome/browser/toolbar/coordinator/main_toolbar_coordinator.mm
+++ ios/chrome/browser/toolbar/coordinator/main_toolbar_coordinator.mm
@@ -312,6 +312,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _topLocationBarCoordinator = nil;
     _topToolbarViewController = nil;
 
+    [_tabGroupIndicatorCoordinator stop];
+
     [_bottomToolbarMediator disconnect];
     _bottomToolbarMediator = nil;
     [_bottomLocationBarCoordinator stop];
