e013d189216eea2243baa7a07b8b12f2a389447f
Josef Raska <josefraska@google.com>
2026-04-15 02:06:34 -0700

    [HoT][Android] Add Autofill Travel search provider into registry
    Bug: 482994258
    Change-Id: Idfa250d635872762846f1c4e0554dc1491bd3378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747928
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Josef Raska <josefraska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615011}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SearchIndexProviderRegistry.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SearchIndexProviderRegistry.java
@@ -115,6 +115,8 @@ public final class SearchIndexProviderRegistry {
                             .SEARCH_INDEX_DATA_PROVIDER,
                     org.chromium.chrome.browser.autofill.settings.AutofillProfilesFragment
                             .SEARCH_INDEX_DATA_PROVIDER,
+                    org.chromium.chrome.browser.autofill.settings.AutofillTravelFragment
+                            .SEARCH_INDEX_DATA_PROVIDER,
                     org.chromium.chrome.browser.autofill.settings.HomeOfTransactionsFragment
                             .SEARCH_INDEX_DATA_PROVIDER,
                     org.chromium.chrome.browser.browsing_data.ClearBrowsingDataFragment
