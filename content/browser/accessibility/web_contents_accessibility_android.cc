a0b4d414b3ab3bd4be6a63a9f7dd2665d0303867
Ramin Halavati <rhalavati@chromium.org>
2026-04-15 10:20:29 -0700

    a11y-android: Update movement at granularity initialization.
    To reduce sending an unnecessary focus change event, do not update the
    accessibility focus when extended selection is changed.
    Instead, when focus is changed, if an extended selection exists and it
    ends on the focused node, use its end offset to initialize the internal
    state for movement at granularity actions.
    
    AX-Relnotes: n/a
    Bug: 500229303, 500946895, 443078007
    Change-Id: I0d59e36dbf2a86a907a796e7ecad5d09cb9b4999
    Cq-Include-Trybots: luci.chromium.try:android-16-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761882
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615258}

--- content/browser/accessibility/web_contents_accessibility_android.cc
+++ content/browser/accessibility/web_contents_accessibility_android.cc
@@ -1071,20 +1071,6 @@ void WebContentsAccessibilityAndroid::HandleTextSelectionChanged(
                                                                unique_id);
 }
 
-void WebContentsAccessibilityAndroid::HandleExtendedSelectionChanged(
-    int32_t unique_id,
-    int32_t focus_unique_id,
-    int32_t focus_offset) {
-  JNIEnv* env = AttachCurrentThread();
-  ScopedJavaLocalRef<jobject> obj = GetJavaObject(env);
-  if (obj.is_null()) {
-    return;
-  }
-
-  Java_WebContentsAccessibilityImpl_handleExtendedSelectionChange(
-      env, obj, unique_id, focus_unique_id, focus_offset);
-}
-
 void WebContentsAccessibilityAndroid::HandleEditableTextChanged(
     int32_t unique_id,
     int32_t subType) {
@@ -1372,6 +1358,16 @@ bool WebContentsAccessibilityAndroid::IsFocused(JNIEnv* env,
   }
 
   return node->IsFocused();
+}
+
+bool WebContentsAccessibilityAndroid::IsTextSelectable(JNIEnv* env,
+                                                       int32_t unique_id) {
+  BrowserAccessibilityAndroid* node = GetAXFromUniqueID(unique_id);
+  if (!node) {
+    return false;
+  }
+
+  return node->IsTextSelectable();
 }
 
 int32_t WebContentsAccessibilityAndroid::GetEditableTextSelectionStart(
