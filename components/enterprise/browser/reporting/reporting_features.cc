83db47c9f3422ef0bdf481b7ef1c92f44c6629dd
Jan Zarzycki <janzarzycki@google.com>
2026-04-15 07:33:20 -0700

    [SaaS report] Enable SaasUsageReporting by default.
    This change flips the default state of the kSaasUsageReporting feature
    flag from disabled to enabled as the feature is being launched. The
    field trial testing config for SaasUsageReporting is also removed.
    
    Bug: b:499009517
    Change-Id: I85bd89a079b1e970b0cf709e9a2d6e5138a6e0d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761456
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Jan Zarzycki <janzarzycki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615151}

--- components/enterprise/browser/reporting/reporting_features.cc
+++ components/enterprise/browser/reporting/reporting_features.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_reporting {
 
-BASE_FEATURE(kSaasUsageReporting, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kSaasUsageReporting, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kBrowserLaunchMetadataReporting,
              base::FEATURE_DISABLED_BY_DEFAULT);
