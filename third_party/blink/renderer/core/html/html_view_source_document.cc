19c546fa8126c18c414b4c3e77aace9b79628102
Noam Rosenthal <nrosenthal@chromium.org>
2026-04-15 08:47:16 -0700

    Support processing instructions in view-source
    Bug: 481087638
    
    Change-Id: Iac3fe588d1d052cdf81686fe5bffb42b17fc0233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762134
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615206}

--- third_party/blink/renderer/core/html/html_view_source_document.cc
+++ third_party/blink/renderer/core/html/html_view_source_document.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/input_type_names.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
 
 namespace blink {
@@ -192,7 +193,7 @@ void HTMLViewSourceDocument::AddSource(
       ProcessTagToken(source, token, attributes_ranges, token_start);
       break;
     case HTMLToken::kProcessingInstruction:
-      // TODO(nrosenthal): implement processing instructions in view-source.
+      ProcessProcessingInstructionToken(source, token);
       break;
     case HTMLToken::kComment:
       ProcessCommentToken(source, token);
@@ -287,6 +288,14 @@ void HTMLViewSourceDocument::ProcessCommentToken(const String& source,
 void HTMLViewSourceDocument::ProcessCharacterToken(const String& source,
                                                    HTMLToken&) {
   AddText(source, g_empty_atom);
+}
+
+void HTMLViewSourceDocument::ProcessProcessingInstructionToken(
+    const String& source,
+    HTMLToken&) {
+  CHECK(RuntimeEnabledFeatures::HTMLProcessingInstructionEnabled());
+  AddText(source, class_processing_instruction_);
+  current_ = td_;
 }
 
 Element* HTMLViewSourceDocument::AddSpanWithClassName(
