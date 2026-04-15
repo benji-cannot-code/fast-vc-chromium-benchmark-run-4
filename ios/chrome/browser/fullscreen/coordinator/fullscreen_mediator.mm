56770dc9136213ba24e082b4ea9e619858b8bf3f
Scott Yoder <scottyoder@google.com>
2026-04-15 08:24:24 -0700

    [ios] Exit Fullscreen when WebState changes
    This change exits Fullscreen when the WebState changes, in the
    refactored Fullscreen. This matches the behavior of the old
    implementation.
    
    Change-Id: Ic7f87a9a795f01c8601ecc5cc13cf479d1b7a8ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759711
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615188}

--- ios/chrome/browser/fullscreen/coordinator/fullscreen_mediator.mm
+++ ios/chrome/browser/fullscreen/coordinator/fullscreen_mediator.mm
@@ -159,6 +159,9 @@ const CGFloat kFullscreenSnapThreshold = 10.0;
         WebViewProxyTabHelper::FromWebState(_webState);
     tabHelper->AddObserver(_webViewProxyObserver.get());
     self.scrollViewProxy = tabHelper->GetWebViewProxy().scrollViewProxy;
+    _browserAgent->ExitFullscreen(
+        PassKey(), FullscreenModeTransitionTrigger::kForcedByCode,
+        /*animated=*/false);
   } else {
     self.scrollViewProxy = nil;
   }
