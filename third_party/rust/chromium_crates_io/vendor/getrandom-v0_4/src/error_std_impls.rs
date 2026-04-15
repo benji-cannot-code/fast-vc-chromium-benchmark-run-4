ec7978bac5ab6941d4b754237062a397ea3b20c1
alphonsea@google.com <alphonsea@google.com>
2026-04-15 13:02:30 -0700

    Add getrandom dependency for libafl crate usage
    Due to the size of crrev.com/c/7564051, this CL works to split all of
    the unsafe dependencies into separate CLs for individual review. The end
    goal is to have all unsafe transitive dependencies for libafl imported
    and reviewed prior to importing the remaining dependencies (including
    libafl). We have approval for importing libafl by Chrome TSCs/ATLs (see
    https://groups.google.com/a/google.com/g/chrome-tsc-discuss/c/jvyNkbHOLhM).
    
    The getrandom crate has also been marked as `group = test` and
    `allow_first_party_usage = false`.
    
    Bug: 495772162
    
    Change-Id: I795c0f3a738b472a935f096a97d2e9627f43f7c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7697952
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Antonio Alphonse <alphonsea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615353}

---
+++ third_party/rust/chromium_crates_io/vendor/getrandom-v0_4/src/error_std_impls.rs
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+extern crate std;
+
+use crate::Error;
+use std::io;
+
+impl From<Error> for io::Error {
+    fn from(err: Error) -> Self {
+        match err.raw_os_error() {
+            Some(errno) => io::Error::from_raw_os_error(errno),
+            None => io::Error::other(err),
+        }
+    }
+}
---
+++ third_party/rust/chromium_crates_io/vendor/getrandom-v0_4/tests/sys_rng.rs
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! Tests for `SysRng`
+#![cfg(feature = "sys_rng")]
+
+use getrandom::SysRng;
+use getrandom::rand_core::TryRng;
+
+#[test]
+fn test_sys_rng() {
+    let x = SysRng.try_next_u64().unwrap();
+    let y = SysRng.try_next_u64().unwrap();
+    assert!(x != 0);
+    assert!(x != y);
+}
+
+#[test]
+fn test_construction() {
+    assert!(SysRng.try_next_u64().unwrap() != 0);
+}
