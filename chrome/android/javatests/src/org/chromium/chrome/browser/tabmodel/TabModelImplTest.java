e6963714e7901bb327edbb9fcafb37182ed404cb
Nafis Abedin <nafisabedin@google.com>
2026-04-15 13:51:25 -0700

    [Gardener] Disable flaky TabModelImplTest
    Bug: 503008051
    Change-Id: I4af0db642d08d5d66c6e2ccc6e4fc088bb614eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759385
    Commit-Queue: Dan Polanco <polardz@google.com>
    Owners-Override: Nafis Abedin <nafisabedin@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Auto-Submit: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615391}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelImplTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelImplTest.java
@@ -48,6 +48,7 @@ import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.RequiresRestart;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -1997,6 +1998,7 @@ public class TabModelImplTest {
 
     @Test
     @SmallTest
+    @Restriction(DeviceFormFactor.PHONE_OR_TABLET) // crbug.com/503008051
     public void testPinTabInGroup_ActionListener_Reject() {
         TabGroupModelFilter filter = mPage.getTabGroupModelFilter();
         createTabGroup(1, filter); // Group with 1 tab.
