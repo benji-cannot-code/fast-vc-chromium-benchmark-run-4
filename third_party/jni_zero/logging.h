a84357926d104e15ac2bf32dd9d5795704226236
Martin Kong <martinkong@google.com>
2026-04-15 07:05:10 -0700

    Make JNI Zero's IMMEDIATE_CRASH call Chromium's IMMEDIATE_CRASH
    This CL makes it so that if we are building with chromium,
    then JNI_ZERO_IMMEDIATE_CRASH should call Chromium's
    IMMEDIATE_CRASH instead of using __builtin_trap().
    
    Bug: 496183738
    Change-Id: Ia811e527f135cb594b62897e023873eda397eb00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761650
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615134}

--- third_party/jni_zero/logging.h
+++ third_party/jni_zero/logging.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/jni_zero/jni_export.h"
 
+#if defined(JNI_ZERO_BUILD_WITH_CHROMIUM)
+#include "base/immediate_crash.h"
+#endif
+
 #if defined(NDEBUG) && !defined(DCHECK_ALWAYS_ON)
 #define JNI_ZERO_DCHECK_IS_ON() false
 #else
@@ -65,11 +69,15 @@ JNI_ZERO_COMPONENT_BUILD_EXPORT void LogMessage(LogLev,
                                                 ...)
     __attribute__((__format__(__printf__, 4, 5)));
 
+#if defined(JNI_ZERO_BUILD_WITH_CHROMIUM)
+#define JNI_ZERO_IMMEDIATE_CRASH() base::ImmediateCrash()
+#else
 #define JNI_ZERO_IMMEDIATE_CRASH() \
   do {                             \
     __builtin_trap();              \
     __builtin_unreachable();       \
   } while (0)
+#endif
 #define JNI_ZERO_XLOG(level, fmt, ...)                                         \
   ::jni_zero::LogMessage(level, ::jni_zero::Basename(__FILE__), __LINE__, fmt, \
                          ##__VA_ARGS__)
