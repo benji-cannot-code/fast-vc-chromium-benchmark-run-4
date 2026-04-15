3c61d538698a1647f0aad8c31974ce3338755d1c
Emily Shack <emshack@chromium.org>
2026-04-14 17:13:19 -0700

    [GlowUp] Remove unused Views icons
    These icons are not referenced anywhere in the codebase, remove them
    prior to a planned mass icon update
    
    Bug: 498731414
    Change-Id: I0766ea77f1eadf40d5f1ea596355bdd69b59c7b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7737965
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Sandeep Vijayasekar <sandv@google.com>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Sandeep Vijayasekar <sandv@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614793}

--- components/permissions/request_type.cc
+++ components/permissions/request_type.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_ANDROID)
 #include "components/resources/android/theme_resources.h"
 #else
-#include "components/permissions/vector_icons/vector_icons.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/gfx/vector_icon_types.h"
 #endif  // BUILDFLAG(IS_ANDROID)
