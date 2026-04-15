f2909f4bb31d7d4d8d552621bab3052b14f80d55
Zekun Jiang <zekunjiang@google.com>
2026-04-14 17:45:55 -0700

    [OptGuide] Migrate model provider consumers in chrome/browser/browsing_topics to use optimization guide global state.
    This CL is part of a large-scale refactor to consolidate `OptimizationGuideModelProvider` users and migrate them to use the API provided in `OptimizationGuideGlobalState`. The goal of this effort is to have the model provider be a globally managed asset instead of a per-profile one like `OptimizationGuideKeyedService`.
    
    This CL was uploaded by git cl split.
    
    R=yaoxia@chromium.org
    
    Bug: 481734987
    Change-Id: Id85421a14a3fdc10beae3b82097dd31eac5bb2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7746465
    Auto-Submit: Zekun Jiang <zekunjiang@google.com>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614834}

--- chrome/browser/browsing_topics/browsing_topics_service_factory.cc
+++ chrome/browser/browsing_topics/browsing_topics_service_factory.cc
@@ -9,8 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/history/history_service_factory.h"
-#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
-#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
+#include "chrome/browser/optimization_guide/model_execution/optimization_guide_global_state.h"
+#include "chrome/browser/optimization_guide/optimization_guide_global_state_holder_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_global_state_holder_keyed_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/browsing_topics/annotator.h"
@@ -57,7 +58,8 @@ BrowsingTopicsServiceFactory::BrowsingTopicsServiceFactory()
               .Build()) {
   DependsOn(PrivacySandboxSettingsFactory::GetInstance());
   DependsOn(HistoryServiceFactory::GetInstance());
-  DependsOn(OptimizationGuideKeyedServiceFactory::GetInstance());
+  DependsOn(
+      OptimizationGuideGlobalStateHolderKeyedServiceFactory::GetInstance());
 }
 
 BrowsingTopicsServiceFactory::~BrowsingTopicsServiceFactory() = default;
@@ -87,8 +89,13 @@ BrowsingTopicsServiceFactory::BuildServiceInstanceForBrowserContext(
   if (!site_data_manager)
     return nullptr;
 
-  OptimizationGuideKeyedService* opt_guide_service =
-      OptimizationGuideKeyedServiceFactory::GetForProfile(profile);
+  auto* global_state_holder =
+      OptimizationGuideGlobalStateHolderKeyedServiceFactory::GetForProfile(
+          profile);
+  auto* opt_guide_service =
+      global_state_holder
+          ? &global_state_holder->GetGlobalState().prediction_manager()
+          : nullptr;
   if (!opt_guide_service) {
     return nullptr;
   }
