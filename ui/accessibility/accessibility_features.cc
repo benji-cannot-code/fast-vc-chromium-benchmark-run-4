6c5f1a3f03124b368c5202c30c10c9cb655ae755
andresmolina <andresmolina@google.com>
2026-04-15 08:31:52 -0700

    [Read Anything] Add feature flag for Improved Read Aloud.
    This CL sets up the `kImprovedReadAloud` feature flag in accessibility
    features and exposes it to the Reading Mode WebUI via Gin bindings as
    `isImprovedReadAloudEnabled`.
    
    This is the first step to regulate the activation of Improved Read Aloud
    features in follow-up CLs.
    
    Test: ReadAnythingAppControllerTest.IsImprovedReadAloudEnabled
    Bug: 494305993
    
    Change-Id: I7d3baa6a3b2f5e824156fb217b0c0cdc052b3b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7760078
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Andres Molina <andresmolina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615194}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -361,6 +361,11 @@ bool IsMainNodeAnnotationsEnabled() {
   return base::FeatureList::IsEnabled(::features::kMainNodeAnnotations);
 }
 
+BASE_FEATURE(kImprovedReadAloud, base::FEATURE_DISABLED_BY_DEFAULT);
+bool IsImprovedReadAloudEnabled() {
+  return base::FeatureList::IsEnabled(::features::kImprovedReadAloud);
+}
+
 BASE_FEATURE(kReadAnythingMenuShuffleExperiment,
              base::FEATURE_DISABLED_BY_DEFAULT);
 bool IsReadAnythingMenuShuffleExperimentEnabled() {
