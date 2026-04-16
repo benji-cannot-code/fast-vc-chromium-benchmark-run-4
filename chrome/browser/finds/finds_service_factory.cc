405ab3ee7607a425e0fbb9568804891123123ecc
Evan Luo <evanluo@google.com>
2026-04-16 07:38:04 -0700

    [finds] Flag guard instantiation of finds service in service factory
    Return a nullptr and do not create the finds service class if the
    kChromeFinds feature flag is not enabled.
    
    Bug: 495433572
    Change-Id: I4619bff2789640aaea499f5d468516b04e1abb19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738002
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615842}

--- chrome/browser/finds/finds_service_factory.cc
+++ chrome/browser/finds/finds_service_factory.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/finds/finds_service_factory.h"
 
+#include "base/feature_list.h"
+#include "chrome/browser/finds/core/finds_features.h"
 #include "chrome/browser/finds/core/finds_service.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/notifications/scheduler/notification_schedule_service_factory.h"
@@ -48,6 +50,9 @@ FindsServiceFactory::~FindsServiceFactory() = default;
 std::unique_ptr<KeyedService>
 FindsServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
+  if (!base::FeatureList::IsEnabled(finds::features::kChromeFinds)) {
+    return nullptr;
+  }
   Profile* profile = Profile::FromBrowserContext(context);
   OptimizationGuideKeyedService* opt_guide_service =
       OptimizationGuideKeyedServiceFactory::GetForProfile(profile);
