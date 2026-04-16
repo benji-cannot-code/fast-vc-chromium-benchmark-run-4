a2b5e2093db177224535bd4745db498442af63b7
Mike West <mkwst@chromium.org>
2026-04-16 02:28:26 -0700

    [Navigation API] Fix cross-origin leak of sourceElement.
    This CL prevents a cross-origin initiator's Element from being exposed
    via NavigateEvent.sourceElement during same-document navigations.
    
    Bug: 502410911
    Change-Id: I756dbd4876afc0db7617f18772e0055962804c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762284
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615710}

--- third_party/blink/renderer/core/navigation_api/navigation_api.cc
+++ third_party/blink/renderer/core/navigation_api/navigation_api.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "third_party/blink/public/mojom/frame/frame.mojom-blink.h"
 #include "third_party/blink/public/web/web_frame_load_type.h"
+#include "third_party/blink/renderer/bindings/core/v8/binding_security.h"
 #include "third_party/blink/renderer/bindings/core/v8/capture_source_location.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
@@ -754,6 +755,11 @@ NavigationApi::DispatchResult NavigationApi::DispatchNavigateEvent(
     CHECK(!upcoming_non_traverse_api_method_tracker_);
     CHECK(upcoming_traverse_api_method_trackers_.empty());
     return DispatchResult::kContinue;
+  }
+
+  if (params->source_element && !BindingSecurity::ShouldAllowAccessTo(
+                                    window_, params->source_element.Get())) {
+    params->source_element = nullptr;
   }
 
   LocalFrame* frame = window_->GetFrame();
