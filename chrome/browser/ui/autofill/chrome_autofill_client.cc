eeca192eeba94f936d2d932eef0548166722e589
Lijin Shen <lazzzis@google.com>
2026-04-16 10:29:37 -0700

    Clean up cc save infobar on Android
    This CL is part of efforts to clean up infobar system on Android in
    favor of message ui system.
    
    This infobar has been replaced by message and bottom sheet on Android.
    
    Verified that this infobar has not been recorded by Infobar.Shown on
    recent chrome stable releases.
    
    Note that AutofillSaveCardInfoBarDelegateMobile is extended by iOS
    counterpart and therefore is not removed in this CL.
    
    Bug: 500405285
    Change-Id: I496fff5c24bd5768e34fe011530e031ea177d0ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736286
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615989}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -185,7 +185,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_snackbar_type.h"
 #include "chrome/browser/ui/autofill/payments/offer_notification_controller_android.h"
 #include "components/autofill/core/browser/payments/autofill_save_card_infobar_delegate_mobile.h"
-#include "components/autofill/core/browser/payments/autofill_save_card_infobar_mobile.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/infobar.h"
 #include "components/messages/android/messages_feature.h"
