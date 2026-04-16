fecafcb3604b9f78d24d62312159d06364e9367c
Alison Gale <agale@chromium.org>
2026-04-16 11:46:19 -0700

    Move window finder into dragging subfolder
    Bug: 475568167
    Change-Id: I21eb567e5c5b69bd83be735d2d81b271ea75274b
    Bypass-Check-License: files moved
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762340
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Vince Lugli <lugli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616028}

--- chrome/browser/ui/views/tabs/dragging/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/dragging/tab_drag_controller_interactive_uitest.cc
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/dragging/tab_drag_controller_interactive_test_mixin.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
-#include "chrome/browser/ui/views/tabs/window_finder.h"
+#include "chrome/browser/ui/views/tabs/dragging/window_finder.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/browser/web_applications/model/display_override.h"
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
--- chrome/browser/ui/views/tabs/dragging/window_finder.h
+++ chrome/browser/ui/views/tabs/dragging/window_finder.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_TABS_WINDOW_FINDER_H_
-#define CHROME_BROWSER_UI_VIEWS_TABS_WINDOW_FINDER_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_TABS_DRAGGING_WINDOW_FINDER_H_
+#define CHROME_BROWSER_UI_VIEWS_TABS_DRAGGING_WINDOW_FINDER_H_
 
 #include <set>
 
@@ -33,4 +33,4 @@ class WindowFinder {
       const std::set<gfx::NativeWindow>& ignore);
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_TABS_WINDOW_FINDER_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_TABS_DRAGGING_WINDOW_FINDER_H_
