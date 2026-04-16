b55842ef7c11f038cb73e1b022aab3a078d11520
Abhay Prakash <abhayprakash@google.com>
2026-04-16 09:38:39 -0700

    Call Tflite model for scoring tab relevance.
    - Will do batch processing in a follow up CL. Wanted to integrate model executor first.
    
    Bug: 462793437
    Change-Id: I9f04771c52a902a1d6bbb36a0e412b633eb4b0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698916
    Commit-Queue: Abhay Prakash <abhayprakash@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615936}

--- chrome/browser/resources/contextual_tasks/internals/app.html.ts
+++ chrome/browser/resources/contextual_tasks/internals/app.html.ts
@@ -25,6 +25,7 @@ export function getHtml(this: ContextualTasksInternalsAppElement) {
             <option value="kEmbeddingsMatch">Embeddings Match</option>
             <option value="kMultiSignalScoring">Multi Signal Scoring</option>
             <option value="kStaticSignalsOnly">Static Signals Scoring</option>
+            <option value="kStaticSignalsMlModel">Static Signals ML Model</option>
           </select>
         </div>
         <div class="container">
