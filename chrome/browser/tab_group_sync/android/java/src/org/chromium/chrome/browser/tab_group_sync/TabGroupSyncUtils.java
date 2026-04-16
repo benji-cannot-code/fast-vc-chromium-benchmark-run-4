020bc45f26aaffbca97bbd1b3bad3cfec369ab18
Shakti Sahu <shaktisahu@chromium.org>
2026-04-16 10:02:31 -0700

    Sanitize recent activity / message URLs before reopening
    Before opening a URL from recent activity or instant message,
    this CL checks for sanity of the URL. This fixes a security
    vulnerability.
    
    Bug: b:502458903
    Change-Id: Ic3f046659b241bf70925207330acfd2a1f7a5bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7768872
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615961}

--- chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncUtils.java
+++ chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncUtils.java
@@ -98,7 +98,7 @@ public final class TabGroupSyncUtils {
     }
 
     @VisibleForTesting
-    static boolean isNtpOrAboutBlankUrl(GURL url) {
+    public static boolean isNtpOrAboutBlankUrl(GURL url) {
         String urlString = url.getValidSpecOrEmpty();
         return UrlUtilities.isNtpUrl(url)
                 || TextUtils.equals(urlString, ContentUrlConstants.ABOUT_BLANK_DISPLAY_URL)
