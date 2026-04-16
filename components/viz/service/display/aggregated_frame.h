75723437867e1d7e81f271b9de8cc6a67e674636
Ryan Kalla <ryankalla@google.com>
2026-04-16 08:21:16 -0700

    Add TrackedElementRects to AggregatedFrame
    This collects the TrackedElementRects from all frames during surface
    aggregation and adds them to the aggregated frame. The tracked elements'
    bounds are transformed to the coordinate space of the root render pass.
    
    See here for more context:
    https://docs.google.com/document/d/1admTWDMlns478TvaScUqwllvNdVIAA_xNJy2L9aZE8A
    
    Bug: 441532128
    Change-Id: I51c15dc6dccbfeb208ebf3479047c7c2e06a33b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705892
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Ryan Kalla <ryankalla@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615883}

--- components/viz/service/display/aggregated_frame.h
+++ components/viz/service/display/aggregated_frame.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "components/viz/common/quads/aggregated_render_pass.h"
+#include "components/viz/common/surfaces/tracked_element_rects.h"
 #include "components/viz/service/viz_service_export.h"
 #include "ui/gfx/delegated_ink_metadata.h"
 #include "ui/gfx/display_color_spaces.h"
@@ -63,6 +64,11 @@ class VIZ_SERVICE_EXPORT AggregatedFrame {
   std::unique_ptr<gfx::DelegatedInkMetadata> delegated_ink_metadata;
 
   AggregatedRenderPassList render_pass_list;
+
+  // The combined tracked element rects, aggregated from the individual
+  // compositor frames and transformed to the coordinate space of the root
+  // render pass.
+  TrackedElementRects tracked_element_rects;
 };
 
 }  // namespace viz
