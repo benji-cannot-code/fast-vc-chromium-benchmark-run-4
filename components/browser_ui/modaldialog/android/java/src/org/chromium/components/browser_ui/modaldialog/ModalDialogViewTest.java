e111022a2aab39ea82cb6777753a07f233f0ee74
Jack Thiesen <jthiesen@chromium.org>
2026-04-16 15:06:28 -0700

    Re-enable ModalDialogViewTest#testButtonGroupIsScrollable
    Include-Ci-Only-Tests: true
    Fixed: 329163841
    Change-Id: Idea03f6808848c361878c916f82b6e684427dd08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765558
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616133}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogViewTest.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/ModalDialogViewTest.java
@@ -70,7 +70,6 @@ import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.browser_ui.modaldialog.test.R;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
@@ -729,7 +728,6 @@ public class ModalDialogViewTest {
     @Test
     @MediumTest
     @Feature({"ModalDialog"})
-    @DisabledTest(message = "crbug.com/329163841")
     public void testButtonGroupIsScrollable() throws InterruptedException {
         ModalDialogProperties.ModalDialogButtonSpec[] buttonSpecList =
                 new ModalDialogButtonSpec[20];
