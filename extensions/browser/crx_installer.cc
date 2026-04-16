a68ff679818a5d671b95c1d5043b93fdaa092861
Justin Lulejian <jlulejian@chromium.org>
2026-04-16 09:38:20 -0700

    [Extensions] Track extension update failures due to corruption.
    Currently, extension unpacking failures are tracked by
    `Extensions.SandboxUnpackFailureReason2`, but this does not distinguish
    between new installations and updates. We want to specifically track
    failures during extension updates to isolate issues caused by file
    corruption or invalid hashes.
    
    After this commit, two new histograms are introduced to help with this:
    
    1) `Extensions.SandboxUnpackSuccessForUpdates` (boolean) will be
    recorded when an extension update completes. It will be true on success
    and false on `SANDBOXED_UNPACKER_FAILURE`. This will help with
    determining how often failures occur.
    
    2) `Extensions.SandboxUnpackFailureReasonForUpdates` (enum) will be
    recorded when an extension update encounters
    `SANDBOXED_UNPACKER_FAILURE`. This will help record the reason for the
    failure -- file corruption, or something else.
    
    A browser test is added verify the histogram's behavior.
    
    Bug: 416484593
    Change-Id: I05fd080e53f8f1883ce4fdb01c6b83406bfb5f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761647
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615935}

--- extensions/browser/crx_installer.cc
+++ extensions/browser/crx_installer.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extensions_browser_client.h"
 #include "extensions/browser/forced_extensions/install_stage_tracker.h"
 #include "extensions/browser/install/crx_install_error.h"
+#include "extensions/browser/install/sandboxed_unpacker_failure_reason.h"
 #include "extensions/browser/install_approval.h"
 #include "extensions/browser/install_flag.h"
 #include "extensions/browser/install_stage.h"
@@ -319,6 +320,8 @@ void CrxInstaller::UpdateExtensionFromUnpackedCrx(
   if (!browser_context_) {
     return;
   }
+
+  is_update_ = true;
 
   const Extension* extension = ExtensionRegistry::Get(browser_context_)
                                    ->GetInstalledExtension(extension_id);
@@ -1168,6 +1171,17 @@ void CrxInstaller::NotifyCrxInstallComplete(
                                                extension()->GetType());
   }
 
+  if (is_update_) {
+    if (success) {
+      base::UmaHistogramBoolean("Extensions.SandboxUnpackSuccessForUpdates",
+                                true);
+    } else if (error->type() ==
+               CrxInstallErrorType::SANDBOXED_UNPACKER_FAILURE) {
+      base::UmaHistogramBoolean("Extensions.SandboxUnpackSuccessForUpdates",
+                                false);
+    }
+  }
+
   if (!success && (!expected_id_.empty() || extension())) {
     switch (error->type()) {
       case CrxInstallErrorType::DECLINED:
@@ -1176,10 +1190,17 @@ void CrxInstaller::NotifyCrxInstallComplete(
             InstallStageTracker::FailureReason::CRX_INSTALL_ERROR_DECLINED,
             error->detail());
         break;
-      case CrxInstallErrorType::SANDBOXED_UNPACKER_FAILURE:
+      case CrxInstallErrorType::SANDBOXED_UNPACKER_FAILURE: {
         install_stage_tracker->ReportSandboxedUnpackerFailureReason(
             extension_id, error.value());
+        if (is_update_) {
+          base::UmaHistogramEnumeration(
+              "Extensions.SandboxUnpackFailureReasonForUpdates",
+              error->sandbox_failure_detail(),
+              SandboxedUnpackerFailureReason::NUM_FAILURE_REASONS);
+        }
         break;
+      }
       case CrxInstallErrorType::OTHER:
         install_stage_tracker->ReportCrxInstallError(
             extension_id,
