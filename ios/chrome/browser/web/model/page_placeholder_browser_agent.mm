ba9eeb654bcbfcf2ef1676f59cf5dff1d0373e21
Briana McClure <bmcclure@google.com>
2026-04-16 09:04:28 -0700

    [iOS] Fix blank snapshot when creating empty tab group
    Previously, when creating a tab group from the new tab switcher overflow
    menu on an empty tab grid, the tab preview would remain blank. This
    change updates ShouldInstallPagePlaceholder to check for the visible_url
    that is used when loading a NTP so that the tab preview correctly loads.
    
    Before:
    https://drive.google.com/file/d/18uSAiyWL-WZNVQRzIxWOPSG1lht7BhG9/view?usp=drive_link&resourcekey=0-M2X-evhYVibHF4cdtEVkmg
    
    After: http://screencast/cast/NDk3MjQwNjkwODEyNTE4NHw5ODA2NTYyYS1jMA
    Fixed: 500676501
    Change-Id: Ie06de79d3257342baac104fb6b2c588a6e55340a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761832
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615917}

--- ios/chrome/browser/web/model/page_placeholder_browser_agent.mm
+++ ios/chrome/browser/web/model/page_placeholder_browser_agent.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check_op.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
+#import "ios/chrome/browser/shared/model/url/url_util.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web/model/page_placeholder_tab_helper.h"
 #import "ios/web/public/web_state.h"
@@ -19,7 +20,7 @@ namespace {
 // when it is realized.
 bool ShouldInstallPagePlaceholder(web::WebState* web_state) {
   const GURL& visible_url = web_state->GetVisibleURL();
-  return visible_url.is_valid() && visible_url != kChromeUINewTabURL;
+  return visible_url.is_valid() && !IsUrlNtp(visible_url);
 }
 
 }  // namespace
