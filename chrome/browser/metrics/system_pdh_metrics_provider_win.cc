d3669288671d91e243d9bc165df55a66e3c58c95
Sean Maher <spvm@chromium.org>
2026-04-16 16:20:48 -0700

    Append .FirstSample suffix to first PDH metric recording
    Modify the per-process recording in SystemPdhMetricsProvider to append a
    ".FirstSample" suffix to the histogram name for the first valid sample
    recorded.
    
    Bug: 465731177
    Change-Id: Ib29b1f06079ec01511c21400ba51e388a21f8937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766329
    Commit-Queue: Sean Maher <spvm@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616183}

--- chrome/browser/metrics/system_pdh_metrics_provider_win.cc
+++ chrome/browser/metrics/system_pdh_metrics_provider_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/process/process_handle.h"
 #include "base/rand_util.h"
+#include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions_win.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
@@ -346,6 +347,10 @@ void SystemPdhMetricsProvider::PdhQueryHandler::ProcessCounter::Record() {
         "Windows.Experimental.Pdh.ProcessV2.");
     auto histogram_name =
         base::StrCat({kPrefix, uma_name_, ".", process_type_suffix_});
+    if (first_emission_) {
+      first_emission_ = false;
+      base::StrAppend(&histogram_name, {".FirstSample"});
+    }
     switch (format_) {
       case PDH_FMT_DOUBLE:
         base::UmaHistogramPercentage(
