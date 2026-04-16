5732bea5d7e1283f36dfba0675c42f83f71e6f86
Henrique Nakashima <hnakashima@chromium.org>
2026-04-16 15:34:54 -0700

    [Android] Restore @NullMarked to ChromeTabCreator
    It was accidentally removed.
    
    Bug: 389129271
    Change-Id: I781e72f6ef6be37c370de20b09232c111f5eebd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766563
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616157}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java
@@ -17,6 +17,7 @@ import org.chromium.base.SysUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.TimingMetric;
 import org.chromium.base.supplier.OneshotSupplier;
+import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.ServiceTabLauncher;
@@ -55,6 +56,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 /** This class creates various kinds of new tabs and adds them to the right {@link TabModel}. */
+@NullMarked
 public class ChromeTabCreator implements TabCreator, NeedsTabModel, NeedsTabModelOrderController {
     /**
      * The application ID used for tabs opened from an application that does not specify an app ID
@@ -535,6 +537,7 @@ public class ChromeTabCreator implements TabCreator, NeedsTabModel, NeedsTabMode
             futureAddTabToModel.thenAccept(
                     addTabToModel -> {
                         if (addTabToModel) {
+                            assumeNonNull(mTabModel);
                             mTabModel.addTab(tab, position, type, creationState);
                         }
                     });
