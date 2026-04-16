b62d83d6adbe3b8a2bc9ce941dcd645198dd839e
Kirubel <kaklilu@chromium.org>
2026-04-16 09:53:16 -0700

    Implement SelectToolJavaScriptFeature.
    This class has a Select method which calls the functions in the
    select_tool.ts based on the targeting approach used in the proto.
    
    Bug: b:472289606
    Change-Id: I7600efaa230b5190238831f3bf18e9c96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7767312
    Reviewed-by: Andrew Liu <liu@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615951}

--- ios/chrome/browser/web/model/chrome_web_client.mm
+++ ios/chrome/browser/web/model/chrome_web_client.mm
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/actor/tools/model/action_target_java_script_feature.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/click_tool_java_script_feature.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/scroll_tool_java_script_feature.h"
+#import "ios/chrome/browser/intelligence/actor/tools/model/select_tool_java_script_feature.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/type_tool_java_script_feature.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/chrome/browser/intelligence/proto_wrappers/page_context_extractor_java_script_feature.h"
@@ -440,6 +441,7 @@ std::vector<web::JavaScriptFeature*> ChromeWebClient::GetJavaScriptFeatures(
     features.push_back(actor::ActionTargetJavaScriptFeature::GetInstance());
     features.push_back(actor::ClickToolJavaScriptFeature::GetInstance());
     features.push_back(actor::ScrollToolJavaScriptFeature::GetInstance());
+    features.push_back(actor::SelectToolJavaScriptFeature::GetInstance());
     features.push_back(actor::TypeToolJavaScriptFeature::GetInstance());
   }
 
