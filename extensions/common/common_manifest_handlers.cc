8e75e79f82d3649270c1aeeec7fb311b0e87f556
Anton Bershanskyi <bershanskyi@gmail.com>
2026-04-16 11:20:46 -0700

    [Extensions] Create extensions::{VersionNameHandler,VersionNameInfo}
    Move Extension class attribute version_name_ into a dedicated
    ManifestHandler class and update Extension::version_name() to use the
    new store. A follow-up CL will migrate call sites of
    Extension::version_name() to VersionNameInfo::GetVersionName().
    
    Bug: 324534603
    Change-Id: I0f0ccea27898c70e491a473cae3280520553e47a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7717842
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1616011}

--- extensions/common/common_manifest_handlers.cc
+++ extensions/common/common_manifest_handlers.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest_handlers/sandboxed_page_info.h"
 #include "extensions/common/manifest_handlers/shared_module_info.h"
 #include "extensions/common/manifest_handlers/trial_tokens_handler.h"
+#include "extensions/common/manifest_handlers/version_name_info.h"
 #include "extensions/common/manifest_handlers/web_accessible_resources_info.h"
 #include "extensions/common/manifest_handlers/webview_info.h"
 #include "extensions/common/manifest_url_handlers.h"
@@ -100,6 +101,7 @@ void RegisterCommonManifestHandlers(ManifestHandlerRegistry* registry) {
   registry->RegisterHandler(std::make_unique<UpdateURLHandler>());
   registry->RegisterHandler(std::make_unique<URLOverridesHandler>());
   registry->RegisterHandler(std::make_unique<UsbPrinterManifestHandler>());
+  registry->RegisterHandler(std::make_unique<VersionNameHandler>());
   registry->RegisterHandler(std::make_unique<WebAccessibleResourcesHandler>());
   registry->RegisterHandler(std::make_unique<WebviewHandler>());
 }
