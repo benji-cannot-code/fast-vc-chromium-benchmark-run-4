1b9043331652d51793a65e44acc07754b5dbc51d
Xi Han <hanxi@google.com>
2026-04-16 07:35:10 -0700

    [Memory] Fix listener issues in ViewBinders.
    In this CL, we fix the leak of existing listener when property key is
    reset. We use a tab to cache a reference of the existing listener,
    and remove it when null is set to clean up the listener.
    
    Bug: 487641528, 493844946
    Change-Id: I906d3d851f576559e7f0b744fe21600240ba82cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766489
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615837}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxViewBinder.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ntp.search;
 
 import android.os.Build;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
@@ -70,8 +71,17 @@ class SearchBoxViewBinder
             searchBoxTextView.setOnDragListener(
                     model.get(SearchBoxProperties.SEARCH_BOX_DRAG_CALLBACK));
         } else if (SearchBoxProperties.SEARCH_BOX_TEXT_WATCHER == propertyKey) {
-            searchBoxTextView.addTextChangedListener(
-                    model.get(SearchBoxProperties.SEARCH_BOX_TEXT_WATCHER));
+            // Sets the search box text watcher. Previously added text watcher will be removed.
+            TextWatcher oldWatcher =
+                    (TextWatcher) searchBoxTextView.getTag(R.id.ntp_search_box_text_watcher_tag);
+            if (oldWatcher != null) {
+                searchBoxTextView.removeTextChangedListener(oldWatcher);
+            }
+            TextWatcher newWatcher = model.get(SearchBoxProperties.SEARCH_BOX_TEXT_WATCHER);
+            if (newWatcher != null) {
+                searchBoxTextView.addTextChangedListener(newWatcher);
+            }
+            searchBoxTextView.setTag(R.id.ntp_search_box_text_watcher_tag, newWatcher);
         } else if (SearchBoxProperties.SEARCH_TEXT == propertyKey) {
             searchBoxTextView.setText(model.get(SearchBoxProperties.SEARCH_TEXT));
         } else if (SearchBoxProperties.SEARCH_HINT_VISIBILITY == propertyKey) {
