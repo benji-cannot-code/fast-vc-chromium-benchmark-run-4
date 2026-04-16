68733132e22d4f8d49b9d9a15e4a90b4eaefb616
Alex Turner <alexmt@chromium.org>
2026-04-16 12:30:51 -0700

    Revert "Introduce C++ interface for PageContextEligibilityApi"
    This reverts commit 34132671af65a16f7909af2ee3352b9827ee973a.
    
    Reason for revert: Cause of crashes due to missing DISABLE_CFI_DLSYM
    annotations
    
    Original change's description:
    > Introduce C++ interface for PageContextEligibilityApi
    >
    > Introduces a pure virtual C++ interface class,
    > `PageContextEligibilityApi`, to replace the existing struct of function
    > pointers approach. This should allow for more idiomatic C++ usage,
    > including passing C++ objects, etc. This should improve readability and
    > maintainablity.
    >
    > This uses a wrapper to maintain backward compatibility with the legacy
    > approach (until the internal library is updated in crrev.com/i/9177846).
    >
    > Note that the API getter function is still wrapped in `extern "C"`, but
    > we are free to use C++ functions in the actual library class.
    >
    > Bug: b:490161242
    > Change-Id: I5e3032321bede48468ebeea6c9e9e3f19b364ab8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7703657
    > Auto-Submit: Alex Turner <alexmt@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Alex Turner <alexmt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1614668}
    
    Bug: 503261341
    Change-Id: Ib0addd071020e94c8c7663f522002777859eb315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766096
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616049}

--- components/optimization_guide/content/browser/page_context_eligibility_api.h
+++ components/optimization_guide/content/browser/page_context_eligibility_api.h
@@ -8,10 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <span>
 #include <string>
 #include <string_view>
 #include <vector>
+
+extern "C" {
 
 namespace optimization_guide {
 
@@ -42,43 +43,6 @@ struct FrameMetadata {
   std::vector<MetaTag> meta_tags;
 };
 
-// New C++ Interface. This allows us to pass C++ objects rather than requiring
-// everything to be C ABI compatible. When adding a new method to the library,
-// you can define a default implementation with `NOTIMPLEMENTED()` here first.
-class PageContextEligibilityApi {
- public:
-  virtual ~PageContextEligibilityApi() = default;
-
-  virtual bool IsPageContextEligible(
-      const std::string& host,
-      const std::string& path,
-      const std::vector<FrameMetadata>& frame_metadata) const = 0;
-
-  virtual bool IsPageContextEligibleWithAccount(
-      const std::string& host,
-      const std::string& path,
-      const std::string& account,
-      const std::vector<FrameMetadata>& frame_metadata) const = 0;
-
-  virtual bool ShouldReextractPageContext(
-      const std::string& host,
-      const std::string& path,
-      const std::vector<std::string>& updated_meta_tags) const = 0;
-
-  virtual std::span<const std::string_view> GetMetaTagNamesAffectingEligibility(
-      const std::string& host,
-      const std::string& path,
-      const std::vector<FrameMetadata>& frame_metadata) const = 0;
-};
-
-using PageContextEligibilityApiGetter = const PageContextEligibilityApi* (*)();
-
-}  // namespace optimization_guide
-
-extern "C" {
-
-namespace optimization_guide {
-
 // Represents an span of string views. Used instead of a std::span to avoid ABI
 // issues.
 struct StringViewSpan {
@@ -86,7 +50,7 @@ struct StringViewSpan {
   size_t size;
 };
 
-// Legacy C ABI interface for PageContextEligibility.
+// Table of C API functions defined within the library.
 struct PageContextEligibilityAPI {
   // Whether the page is context eligible.
   bool (*IsPageContextEligible)(
