2cc06ffd6d7e7cbccf0c8725ffc269df9ae7e53b
Zac Moreland <morez@google.com>
2026-04-16 07:22:09 -0700

    Implement UI handling in QuickDelete for disabled browsing history.
    This will enable the QuickDelete menu to set an expectation and
    display a reasoning when browsing history itself will not be deleted
    as part of the quick delete due to being disabled by enterprise policy
    
    Screenshot of UI when policy set to disabled:
    https://screenshot.googleplex.com/AVmcYrXtwa9Adsg
    
    BUG: 467702883
    Change-Id: Ifa9e51303552b4ed6237bc7abdf887364278bd95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689542
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Zac Moreland <morez@google.com>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1615827}

--- chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteProperties.java
+++ chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteProperties.java
@@ -27,6 +27,8 @@ class QuickDeleteProperties {
             new PropertyModel.WritableBooleanPropertyKey();
     public static final PropertyModel.WritableBooleanPropertyKey IS_DOMAIN_VISITED_DATA_PENDING =
             new PropertyModel.WritableBooleanPropertyKey();
+    public static final PropertyModel.WritableBooleanPropertyKey IS_HISTORY_DELETION_ALLOWED =
+            new PropertyModel.WritableBooleanPropertyKey();
 
     public static final PropertyModel.ReadableObjectPropertyKey<Context> CONTEXT =
             new PropertyModel.ReadableObjectPropertyKey<>();
@@ -40,6 +42,7 @@ class QuickDeleteProperties {
         IS_SIGNED_IN,
         IS_SYNCING_HISTORY,
         IS_DOMAIN_VISITED_DATA_PENDING,
+        IS_HISTORY_DELETION_ALLOWED,
         CONTEXT,
         HAS_MULTI_WINDOWS
     };
