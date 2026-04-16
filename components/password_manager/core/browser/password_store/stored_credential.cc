c7013f8d3153b0933852cd554092fbbd47936394
Vasilii Sukhanov <vasilii@chromium.org>
2026-04-16 08:10:04 -0700

    Introduce StoredCredential with converters to/from PasswordForm.
    go/divide-and-conquer-the-passwordform
    
    Bug: 501020786
    Change-Id: I16dee133f6c1ba153d2f777ff592e59e18dae9e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7743364
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615875}

---
+++ components/password_manager/core/browser/password_store/stored_credential.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/password_store/stored_credential.h"
+
+namespace password_manager {
+
+StoredCredential::StoredCredential() = default;
+
+StoredCredential::StoredCredential(StoredCredential&&) = default;
+
+StoredCredential& StoredCredential::operator=(StoredCredential&&) = default;
+
+StoredCredential::~StoredCredential() = default;
+
+}  // namespace password_manager
