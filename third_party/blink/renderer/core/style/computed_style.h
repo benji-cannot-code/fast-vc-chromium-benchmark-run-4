4d1873e907100b83cf87f323e507ede0cf7365bd
David Grogan <dgrogan@chromium.org>
2026-04-16 07:51:34 -0700

    [autosizer] Condense SpecifiedLineHeight, LineHeight, LineHeightInternal
    We don't need these to be separate after removing the autosizer.
    
    Change-Id: I515fc2dc17c26f90a13321cc99fac0616e5ee622
    Bug: 391990606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7763192
    Auto-Submit: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615855}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -823,9 +823,6 @@ class ComputedStyle final : public ComputedStyleBase {
 
   // Inherited properties.
 
-  // line-height
-  CORE_EXPORT Length LineHeight() const;
-
   // List style properties.
 
   // list-style-type
@@ -848,7 +845,7 @@ class ComputedStyle final : public ComputedStyleBase {
   LineLogicalSide GetTextEmphasisLineLogicalSide() const;
 
   CORE_EXPORT FontSizeStyle GetFontSizeStyle() const {
-    return FontSizeStyle(GetFont(), LineHeightInternal(), EffectiveZoom());
+    return FontSizeStyle(GetFont(), LineHeight(), EffectiveZoom());
   }
 
   // Font properties.
@@ -1313,7 +1310,6 @@ class ComputedStyle final : public ComputedStyleBase {
                      TextOffsetMap* offset_map = nullptr) const;
 
   // Line-height utility functions.
-  const Length& SpecifiedLineHeight() const { return LineHeightInternal(); }
   static float ComputedLineHeight(const Length& line_height, const Font&);
   float ComputedLineHeight() const;
   CORE_EXPORT LayoutUnit ComputedLineHeightAsFixed() const;
@@ -3305,7 +3301,7 @@ class ComputedStyleBuilder final : public ComputedStyleBuilderBase {
   void UpdateFontOrientation();
 
   FontSizeStyle GetFontSizeStyle() const {
-    return FontSizeStyle(GetFont(), LineHeightInternal(), EffectiveZoom());
+    return FontSizeStyle(GetFont(), LineHeight(), EffectiveZoom());
   }
 
   // grid-template-*
@@ -3326,10 +3322,8 @@ class ComputedStyleBuilder final : public ComputedStyleBuilderBase {
 
   // line-height
   bool HasInitialLineHeight() const {
-    return LineHeightInternal() ==
-           ComputedStyleInitialValues::InitialLineHeight();
+    return LineHeight() == ComputedStyleInitialValues::InitialLineHeight();
   }
-  const Length& LineHeight() const { return LineHeightInternal(); }
 
   // margin-*
   void SetMarginTop(const Length& v) {
