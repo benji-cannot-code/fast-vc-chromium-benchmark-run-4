caeca75c973ad2cf844687ca07b67a5079a8aa72
Anders Hartvoll Ruud <andruud@chromium.org>
2026-04-16 05:22:13 -0700

    Make StyleRuleBase::Clone() always clone
    Before CL:6943852, we had separate paths for deep-copying the rule
    tree (for e.g. StyleSheetContents copies) and for "re-nesting",
    which copied parts of the rule tree affected by a parent selector
    change. The unification accidentally left the kNestedDeclarations
    branch returning "this" in some cases, which is not correct
    as it could cause multiple StyleSheetContents objects to hold
    the same rule. The kApplyMixin and kContents branches have
    the same problem.
    
    Changed `DowncastTraits<StyleRuleGroup>` to include mixin/result
    rules. (Additional rules should ultimately be listed, see
    Issue 502048295). Otherwise test failures would be masked.
    
    Fixed: 501810226
    Bug: 502048295
    Change-Id: I17f1e5cda0981f5cc29e38bd7d65ec060f0b3e7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7755161
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615762}

--- third_party/blink/renderer/core/css/style_rule.h
+++ third_party/blink/renderer/core/css/style_rule.h
@@ -865,7 +865,7 @@ struct DowncastTraits<StyleRuleGroup> {
            rule.IsContainerRule() || rule.IsLayerBlockRule() ||
            rule.IsScopeRule() || rule.IsStartingStyleRule() ||
            rule.IsFunctionRule() || rule.IsPageRule() ||
-           rule.IsNavigationRule();
+           rule.IsNavigationRule() || rule.IsMixinRule() || rule.IsResultRule();
   }
 };
 
