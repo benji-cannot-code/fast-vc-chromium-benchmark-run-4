c0309898441b28530f91663e9f1cc1f1fb7a5a69
Emily Shack <emshack@chromium.org>
2026-04-16 11:25:44 -0700

    [ComboButton] Enable combo button on ToT
    Pre-req for 100% Stable launch
    
    Bug: 503351191
    Change-Id: I2a399c88581ed83d897b4eeae7e5b7d0f4c23bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7769076
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616014}

--- chrome/browser/ui/tabs/features.cc
+++ chrome/browser/ui/tabs/features.cc
@@ -70,7 +70,7 @@ BASE_FEATURE_PARAM(double,
 
 BASE_FEATURE(kTabSelectionByPointer, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kHorizontalTabStripComboButton, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kHorizontalTabStripComboButton, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(bool,
                    kHorizontalTabStripComboButtonShowStartOnly,
                    &kHorizontalTabStripComboButton,
