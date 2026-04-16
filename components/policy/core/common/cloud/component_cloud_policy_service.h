4113270a2f49d2c2c5010e4dc9cd1833fce1edd1
Zac Moreland <morez@google.com>
2026-04-16 11:45:00 -0700

    Reland "Enable building cloud policy components for extensions on Android."
    This is a reland of commit 420a8d9cea19540a11830c358bbc2b2e80e61687
    
    Revert / reland explanation:
    - Original CL passed manual tests and CQ, but unit tests for extension-enabled desktop must not have been included in CQ and were certainly not included in APK build for manual test, so broken unit tests made it through where they broke LUCI tree and it was reverted.
    - Created reland CL and reproduced broken unit test build locally
    - Updated some build gates in unit tests to include extension-enabled android, to prevent the build failures as well as include test coverage for the newly enabled feature on extension-enabled android.
    - Confirmed locally that the extension-enabled android unit tests build successfully now.
    
    Original change's description:
    > Enable building cloud policy components for extensions on Android.
    >
    > This change modifies build conditions to include
    > `ComponentCloudPolicyService`, `ResourceCache`, and related protos and
    > tests on Android when the `enable_extensions_core` buildflag is true.
    > Previously, these components were only built on non-Android and non-iOS
    > platforms. This will allow for extension managed storage / custom policy
    > to be set for extensions on android if extensions are enabled.
    >
    > BUG: 492160614
    > Change-Id: I96180359fd8b492670dd14e6ccda9d1c7b99046c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7700833
    > Reviewed-by: Anqing Zhao <anqing@chromium.org>
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Zac Moreland <morez@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1615218}
    
    Bug: 492160614
    Change-Id: I9bd89fa1a1df7001e5918c289c2f9cbb7493ffd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7767269
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Zac Moreland <morez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616027}

--- components/policy/core/common/cloud/component_cloud_policy_service.h
+++ components/policy/core/common/cloud/component_cloud_policy_service.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/schema_registry.h"
 #include "components/policy/core/common/values_util.h"
 #include "components/policy/policy_export.h"
+#include "extensions/buildflags/buildflags.h"
 
 namespace base {
 class SequencedTaskRunner;
@@ -96,7 +97,7 @@ class POLICY_EXPORT ComponentCloudPolicyService
       SchemaRegistry* schema_registry,
       CloudPolicyCore* core,
       CloudPolicyClient* client,
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_EXTENSIONS_CORE)) && !BUILDFLAG(IS_IOS)
       std::unique_ptr<ResourceCache> cache,
 #endif
       scoped_refptr<base::SequencedTaskRunner> backend_task_runner);
@@ -152,7 +153,7 @@ class POLICY_EXPORT ComponentCloudPolicyService
   }
 
  private:
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_EXTENSIONS_CORE)) && !BUILDFLAG(IS_IOS)
   class Backend;
 
   void UpdateFromSuperiorStore();
--- components/policy/core/common/cloud/test/policy_builder.h
+++ components/policy/core/common/cloud/test/policy_builder.h
@@ -21,7 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/keypair.h"
 #include "google_apis/gaia/gaia_id.h"
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#include "extensions/buildflags/buildflags.h"
+#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_EXTENSIONS_CORE)) && !BUILDFLAG(IS_IOS)
 #include "components/policy/proto/chrome_extension_policy.pb.h"
 #endif
 
@@ -223,7 +224,7 @@ using UserPolicyBuilder =
 using ExtensionInstallPoliciesBuilder =
     TypedPolicyBuilder<enterprise_management::ExtensionInstallPolicies>;
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#if (!BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_EXTENSIONS_CORE)) && !BUILDFLAG(IS_IOS)
 using ComponentCloudPolicyBuilder =
     TypedPolicyBuilder<enterprise_management::ExternalPolicyData>;
 #endif
