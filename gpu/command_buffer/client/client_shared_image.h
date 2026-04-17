d2e807cc3d1ec4ad7a0d850432cc3600bbe9ef66
Saifuddin Hitawala <hitawala@chromium.org>
2026-04-17 07:21:40 -0700

    [gpu] Move SharedImageInfo and SharedImageMetadata to common header
    This moves the definition of SharedImageInfo and SharedImageMetadata
    from client-side headers to a new common header:
    gpu/command_buffer/common/shared_image_info.h.
    
    This is the first step towards encapsulating CreateSharedImage
    parameters in the GPU service layer backing factories, as it
    avoids a dependency from service to client.
    
    Bug: 40263319
    Change-Id: Idd3167ff6824efa7caa63026a156e81c57452e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765617
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616530}

--- gpu/command_buffer/client/client_shared_image.h
+++ gpu/command_buffer/client/client_shared_image.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/gpu_command_buffer_client_export.h"
 #include "gpu/command_buffer/client/internal/mappable_buffer.h"
 #include "gpu/command_buffer/common/mailbox.h"
+#include "gpu/command_buffer/common/shared_image_info.h"
 #include "gpu/command_buffer/common/shared_image_trace_utils.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/common/sync_token.h"
@@ -82,15 +83,6 @@ class WebGPUTextureScopedAccess;
 class WebGPUBufferScopedAccess;
 
 struct ExportedSharedImage;
-
-struct SharedImageMetadata {
-  viz::SharedImageFormat format;
-  gfx::Size size;
-  gfx::ColorSpace color_space;
-  GrSurfaceOrigin surface_origin;
-  SkAlphaType alpha_type;
-  SharedImageUsageSet usage;
-};
 
 class GPU_COMMAND_BUFFER_CLIENT_EXPORT SharedImageExportResult {
  public:
---
+++ gpu/command_buffer/common/shared_image_info.h
@@ -0,0 +1,61 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GPU_COMMAND_BUFFER_COMMON_SHARED_IMAGE_INFO_H_
+#define GPU_COMMAND_BUFFER_COMMON_SHARED_IMAGE_INFO_H_
+
+#include <string>
+#include <string_view>
+
+#include "components/viz/common/resources/shared_image_format.h"
+#include "gpu/command_buffer/common/shared_image_usage.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
+#include "third_party/skia/include/gpu/ganesh/GrTypes.h"
+#include "ui/gfx/color_space.h"
+#include "ui/gfx/geometry/size.h"
+
+namespace gpu {
+
+struct SharedImageMetadata {
+  viz::SharedImageFormat format;
+  gfx::Size size;
+  gfx::ColorSpace color_space;
+  GrSurfaceOrigin surface_origin;
+  SkAlphaType alpha_type;
+  SharedImageUsageSet usage;
+};
+
+struct SharedImageInfo {
+  SharedImageInfo(const viz::SharedImageFormat& format,
+                  gfx::Size size,
+                  const gfx::ColorSpace& color_space,
+                  GrSurfaceOrigin surface_origin,
+                  SkAlphaType alpha_type,
+                  SharedImageUsageSet usage,
+                  std::string_view debug_label)
+      : meta(format, size, color_space, surface_origin, alpha_type, usage),
+        debug_label(debug_label) {}
+  SharedImageInfo(const viz::SharedImageFormat& format,
+                  gfx::Size size,
+                  const gfx::ColorSpace& color_space,
+                  SharedImageUsageSet usage,
+                  std::string_view debug_label)
+      : meta(format,
+             size,
+             color_space,
+             kTopLeft_GrSurfaceOrigin,
+             kPremul_SkAlphaType,
+             usage),
+        debug_label(debug_label) {}
+  SharedImageInfo(const SharedImageMetadata& meta, std::string_view debug_label)
+      : meta(meta), debug_label(debug_label) {}
+
+  SharedImageMetadata meta;
+  std::string debug_label;
+};
+
+}  // namespace gpu
+
+#endif  // GPU_COMMAND_BUFFER_COMMON_SHARED_IMAGE_INFO_H_
