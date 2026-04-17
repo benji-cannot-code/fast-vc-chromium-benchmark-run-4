c89bc5b876f0571bc34a213ab1f6cffd41dd6e7c
Gazal <agazal@google.com>
2026-04-16 19:17:29 -0700

    [histogram-cleanup] Remove expired histogram: Android.WebView.DarkMode.ForceDarkMode
    This histogram expired on 2023-06-01.
    
    Bug: 40903690
    Change-Id: I21a949b1ca28cbab2913f14001ef3a9535c3de00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765994
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616279}

--- android_webview/java/src/org/chromium/android_webview/AwWebContentsMetricsRecorder.java
+++ android_webview/java/src/org/chromium/android_webview/AwWebContentsMetricsRecorder.java
@@ -53,16 +53,10 @@ public class AwWebContentsMetricsRecorder extends WebContentsObserver {
         int nightMode = DarkModeHelper.getNightMode(context);
         int lightTheme = DarkModeHelper.getLightTheme(context);
         boolean isForceDarkApplied = awSettings.isForceDarkApplied();
-        int forceDarkMode = awSettings.getForceDarkMode();
         int forceDarkBehavior = awSettings.getForceDarkBehavior();
         int textLuminance = DarkModeHelper.getPrimaryTextLuminace(context);
         recordDarkModeMetrics(
-                nightMode,
-                lightTheme,
-                isForceDarkApplied,
-                forceDarkMode,
-                forceDarkBehavior,
-                textLuminance);
+                nightMode, lightTheme, isForceDarkApplied, forceDarkBehavior, textLuminance);
     }
 
     @VisibleForTesting
@@ -70,17 +64,12 @@ public class AwWebContentsMetricsRecorder extends WebContentsObserver {
             int nightMode,
             int lightTheme,
             boolean isForceDarkApplied,
-            int forceDarkMode,
             int forceDarkBehavior,
             int textLuminance) {
         RecordHistogram.recordEnumeratedHistogram(
                 "Android.WebView.DarkMode.ForceDarkBehavior",
                 forceDarkBehavior,
                 AwSettings.FORCE_DARK_STRATEGY_COUNT);
-        RecordHistogram.recordEnumeratedHistogram(
-                "Android.WebView.DarkMode.ForceDarkMode",
-                forceDarkMode,
-                AwSettings.FORCE_DARK_MODES_COUNT);
         RecordHistogram.recordBooleanHistogram(
                 "Android.WebView.DarkMode.InDarkMode", isForceDarkApplied);
         // Refer to WebViewInDarkModeVsLightTheme in enums.xml.
@@ -124,7 +113,6 @@ public class AwWebContentsMetricsRecorder extends WebContentsObserver {
         int value =
                 DarkModeHelper.getNightMode(context) * AwSettings.FORCE_DARK_MODES_COUNT
                         + forceDarkMode;
-        System.out.println("recordForce value " + value);
         RecordHistogram.recordEnumeratedHistogram(
                 "Android.WebView.ForceDarkMode",
                 value,
