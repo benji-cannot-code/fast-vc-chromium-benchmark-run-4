45a5edbc72c9d672c1879a27e474b7899189edcc
Salvador Guerrero <salg@google.com>
2026-04-17 16:08:59 -0700

    [Glic Android] implemented opening settings from glic sheet
    This CL adds separate glic_settings_util implementations for Android and
    desktop. And adds a JNI bridge to navigate to the glic settings
    activity.
    
    Bug: b:500389587
    Change-Id: I16f43f52f429fbad958ff1d3ded5ec5aa0eab2c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770288
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616937}

---
+++ chrome/browser/glic/android/glic_settings_navigation_android.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/android/glic_settings_navigation_android.h"
+
+#include "base/android/jni_android.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
+#include "chrome/browser/glic/android/jni_headers/GlicSettingsNavigation_jni.h"
+
+namespace glic {
+
+void ShowGlicSettings() {
+  Java_GlicSettingsNavigation_showGlicSettings(
+      base::android::AttachCurrentThread());
+}
+
+}  // namespace glic
---
+++ chrome/browser/glic/glic_settings_util_android.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/glic_settings_util.h"
+
+#include "base/notimplemented.h"
+#include "chrome/browser/glic/android/glic_settings_navigation_android.h"
+
+namespace glic {
+
+void OpenGlicSettingsPage(Profile* profile) {
+  ShowGlicSettings();
+}
+
+void OpenGlicOsToggleSetting(Profile* profile) {
+  ShowGlicSettings();
+}
+
+void OpenGlicKeyboardShortcutSetting(Profile* profile) {
+  ShowGlicSettings();
+}
+
+void OpenPasswordManagerSettingsPage(Profile* profile) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace glic
