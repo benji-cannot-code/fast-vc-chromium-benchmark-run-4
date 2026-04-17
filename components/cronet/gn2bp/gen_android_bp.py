2e128adca7bd92ce7675775b3e0ea05f9d805816
Mohannad Farrag <aymanm@google.com>
2026-04-17 04:02:34 -0700

    Add -fdiagnostics-show-inlining-chain to GN2BP denylist
    '-fdiagnostics-show-inlining-chain' is not recognized by Clang in
    AOSP and is breaking the import pipeline.
    
    Bug: 503241836
    Change-Id: I621efcf71cf165312e57641e2333b81f12c42d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7768511
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616449}

--- components/cronet/gn2bp/gen_android_bp.py
+++ components/cronet/gn2bp/gen_android_bp.py
@@ -2744,6 +2744,7 @@ def _is_cflag_allowed(cflag):
         # applied.
         '-mllvm -enable-ml-inliner=',
         '-mllvm -ml-inliner-model-selector=',
+        '-fdiagnostics-show-inlining-chain',
     ])
 
 
