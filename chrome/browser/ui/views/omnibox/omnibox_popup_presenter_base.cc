bb7431b1bc27db6bc197472819693dbc3f923168
Aviv Kiss <avivkiss@google.com>
2026-04-16 19:16:27 -0700

    [next] Adjust flicker fix to resolve content cutoff.
    Prior to this adjustment cases where the size of the popup did not
    change between presentation (disabled suggestions) could result in the
    popup content being cut off.
    
    This change also adds metrics for how often the fallback path is used.
    
    Bug: 482087136
    Change-Id: If122973a048cc4af798dc23fed55cbe5a54521d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770968
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616278}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.cc
@@ -62,8 +62,6 @@ void OmniboxPopupPresenterBase::Show() {
     auto show_request_time = base::TimeTicks::Now();
     if (ShouldDeferUntilVisualStateReady()) {
       is_deferred_ = true;
-      content_height_ = 0;
-      SynchronizePopupBounds();
 
       // Call WasShown to mark the WebContents as visible so that a frame will
       // eventually be produced that triggers the OnVisualStateReady callback.
@@ -73,7 +71,8 @@ void OmniboxPopupPresenterBase::Show() {
           ->GetPrimaryMainFrame()
           ->InsertVisualStateCallback(
               base::BindOnce(&OmniboxPopupPresenterBase::OnVisualStateReady,
-                             weak_factory_.GetWeakPtr(), show_request_time));
+                             weak_factory_.GetWeakPtr(), show_request_time,
+                             /*from_fallback=*/false));
 
       // Add a backup timer in case the visual state callback is never called.
       // The visual state callback should always be called, but this fallback
@@ -83,6 +82,7 @@ void OmniboxPopupPresenterBase::Show() {
           FROM_HERE,
           base::BindOnce(&OmniboxPopupPresenterBase::OnVisualStateReady,
                          weak_factory_.GetWeakPtr(), show_request_time,
+                         /*from_fallback=*/true,
                          /*success=*/false),
           base::Milliseconds(
               omnibox::kOmniboxAimDeferShowUntilVisualStateReadyTimeoutMs
@@ -95,10 +95,16 @@ void OmniboxPopupPresenterBase::Show() {
 
 void OmniboxPopupPresenterBase::OnVisualStateReady(
     base::TimeTicks show_request_time,
+    bool from_fallback,
     bool success) {
   if (!is_deferred_) {
     return;
   }
+
+  base::UmaHistogramBoolean(
+      base::StrCat(
+          {GetPopupMetricPrefix(), ".DeferredShowVisualStateReadyFromTimeout"}),
+      from_fallback);
 
   is_deferred_ = false;
   // Fall back to showing the widget even if success == false
