1b33bbd9b47549a2019fae345bbce7ea664f5ea1
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-04-17 09:59:06 -0700

    Use same instance of ExportedCanvasResource in CRDispatcher
    There is no reason to create new one for each of the use-cases.
    Flow change:
    As we now have only one instance, we will have only one destructor call.
    And so there will be only one CanvasResource::DropRefOnOwningThread call
    
    This is no-op, because on all calls except last one CanvasResource will
    have more than one ref and it will early out here [1]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;drc=036d07d8c68f4b0322530fbe1f0061f2d8c84366;l=450
    
    Change-Id: I43f99bea8bceb5ce8e5d3174d7969e88f47b366b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7769728
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616647}

--- third_party/blink/renderer/platform/graphics/exported_canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/exported_canvas_resource.h
@@ -32,6 +32,9 @@ class PLATFORM_EXPORT ExportedCanvasResource
   void Transfer();
   void EndDisplayCompositorAccess(gpu::SharedImageExportResult export_result,
                                   bool is_lost);
+  bool PrepareTransferableResource(
+      viz::TransferableResource* transferable_resource,
+      bool needs_verified_synctoken);
 
   CanvasResource* GetResourceForTesting() const { return resource_.get(); }
 
