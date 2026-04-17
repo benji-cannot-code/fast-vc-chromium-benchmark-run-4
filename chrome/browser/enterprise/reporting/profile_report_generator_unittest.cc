bdc08560620dbeedb84f5f44f304bee530b18ec3
Miyoung shin <myid.shin@igalia.com>
2026-04-16 21:51:25 -0700

    Move key names used by prefs::kCloudExtensionRequestIds to //extensions
    This patch prepares WebstoreInstaller and WebstorePrivateApi for
    migration to //extensions by moving pref key names used by the
    prefs::kCloudExtensionRequestIds preference.
    
    Bug: 358567092
    Change-Id: I5657d8c0b9ffaaee551939a4c21b3802952e6fd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761088
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616332}

--- chrome/browser/enterprise/reporting/profile_report_generator_unittest.cc
+++ chrome/browser/enterprise/reporting/profile_report_generator_unittest.cc
@@ -43,9 +43,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
-#include "chrome/common/extensions/extension_constants.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/pref_names.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/extension_builder.h"
 #endif
 
