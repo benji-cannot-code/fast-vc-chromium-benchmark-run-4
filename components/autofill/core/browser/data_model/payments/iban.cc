d08cdd4a2756a1f555f32f25b1ad0fe46c6d4e43
Rebecca <qihuizhao@google.com>
2026-04-17 10:17:36 -0700

    Launch IBAN for Ireland
    Bug: 503331193
    Change-Id: I443ab4d5c94b7fb9121810f6a1a09fdcb2e8bd9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766527
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616658}

--- components/autofill/core/browser/data_model/payments/iban.cc
+++ components/autofill/core/browser/data_model/payments/iban.cc
@@ -226,6 +226,8 @@ Iban::IbanSupportedCountry Iban::GetIbanSupportedCountry(
     return IbanSupportedCountry::kHR;
   } else if (country_code == "HU") {
     return IbanSupportedCountry::kHU;
+  } else if (country_code == "IE") {
+    return IbanSupportedCountry::kIE;
   } else if (country_code == "IL") {
     return IbanSupportedCountry::kIL;
   } else if (country_code == "IQ") {
@@ -392,6 +394,8 @@ size_t Iban::GetLengthOfIbanCountry(IbanSupportedCountry supported_country) {
       return 21;
     case IbanSupportedCountry::kHU:
       return 28;
+    case IbanSupportedCountry::kIE:
+      return 22;
     case IbanSupportedCountry::kIL:
       return 23;
     case IbanSupportedCountry::kIQ:
--- components/autofill/core/browser/data_model/payments/iban.h
+++ components/autofill/core/browser/data_model/payments/iban.h
@@ -48,6 +48,7 @@ class Iban {
 
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
+  // Next ID: 80
   enum class IbanSupportedCountry {
     kUnsupported = 0,
     kAD = 1,   // Andorra
@@ -82,6 +83,7 @@ class Iban {
     kGT = 30,  // Guatemala
     kHR = 31,  // Croatia
     kHU = 32,  // Hungary
+    kIE = 79,  // Ireland
     kIL = 33,  // Israel
     kIQ = 34,  // Iraq
     kIS = 35,  // Iceland
@@ -128,7 +130,7 @@ class Iban {
     kVA = 76,  // Vatican City
     kVG = 77,  // Virgin Islands, British
     kXK = 78,  // Kosovo
-    kMaxValue = kXK,
+    kMaxValue = kIE,
   };
 
   // Creates an IBAN with `kUnknown` record type.
