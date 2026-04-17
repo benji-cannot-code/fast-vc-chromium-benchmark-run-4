3c46d74610b6814cc8573ef09a8267b75799028d
Dan Polanco <polardz@google.com>
2026-04-17 06:49:07 -0700

    [Actions] Introduce TabSwitcherActionButtonBinder
    Bug: 502622272
    Change-Id: I28f308c0cddc21bcb6a209a883a2125c8634977a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766392
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616510}

---
+++ chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabSwitcherButtonView.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab_ui;
+
+import org.chromium.build.annotations.NullMarked;
+
+/**
+ * Interface for the Tab Switcher button view, designed to be used with the Actions framework. This
+ * allows the ActionButtonBinder to push state without knowing the concrete view implementation.
+ *
+ * <p>Note: This interface should be implemented by a {@link View} in order to be compatible with
+ * the Actions framework.
+ */
+@NullMarked
+public interface TabSwitcherButtonView {
+    /** Sets the tab count with the proper drawable incognito state. */
+    void setTabCount(int tabCount, boolean isIncognito);
+
+    /** Sets whether the notification dot is visible on the button. */
+    void setNotificationDotVisible(boolean showDot);
+
+    /** Force the ripple to end so the transition looks correct when showing the tab switcher. */
+    void endRippleAnimation();
+}
