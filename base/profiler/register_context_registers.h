dfef09a0961cfb4ffd713efcaf6b74ce0ec746f1
Justin Novosad <junov@chromium.org>
2026-04-17 06:56:41 -0700

    Fix RISCV build of register_contex_registers.h
    This change fixes a build breakage cause by
    commit f71e47fc0edf5b2bae628835c5d9def62169b65d
    
    The offending CL accidentally removed UNSAFE_TODO macro invocations
    that were necessary to suppress compiler errors on RISCV
    
    Bug: b/503665835
    Change-Id: I7d29ffcf651c4adff1e58205d4183b0781cf1d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771895
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Auto-Submit: Justin Novosad <junov@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616515}

--- base/profiler/register_context_registers.h
+++ base/profiler/register_context_registers.h
@@ -159,7 +159,7 @@ inline uintptr_t RegisterContextFramePointer(mcontext_t* context) {
 #elif defined(ARCH_CPU_X86_64)
   return AsUintPtr(&context->gregs[REG_RBP]);
 #else
-  return *(reinterpret_cast<uintptr_t*>(context) + 1);
+  return *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 1));
 #endif
 }
 
@@ -175,7 +175,7 @@ inline void SetRegisterContextFramePointer(mcontext_t* context, uintptr_t val) {
 #elif defined(ARCH_CPU_X86_64)
   AsUintPtr(&context->gregs[REG_RBP]) = val;
 #else
-  *(reinterpret_cast<uintptr_t*>(context) + 1) = val;
+  *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 1)) = val;
 #endif
 }
 
@@ -189,7 +189,7 @@ inline uintptr_t RegisterContextInstructionPointer(mcontext_t* context) {
 #elif defined(ARCH_CPU_X86_64)
   return AsUintPtr(&context->gregs[REG_RIP]);
 #else
-  return *(reinterpret_cast<uintptr_t*>(context) + 2);
+  return *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 2));
 #endif
 }
 
@@ -204,7 +204,7 @@ inline void SetRegisterContextInstructionPointer(mcontext_t* context,
 #elif defined(ARCH_CPU_X86_64)
   AsUintPtr(&context->gregs[REG_RIP]) = val;
 #else
-  *(reinterpret_cast<uintptr_t*>(context) + 2) = val;
+  *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 2)) = val;
 #endif
 }
 
