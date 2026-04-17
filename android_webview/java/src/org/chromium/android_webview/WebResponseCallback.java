7d7607e764b87343368266ce247aaf2099854f5e
Ben Wiser <bewise@chromium.org>
2026-04-17 15:31:56 -0700

    [android_webview] Clean up expired shouldInterceptRequestAsync histogram
    The histogram
    Android.WebView.ShouldInterceptRequest.Async.CallbackLeakedWithoutResponse
    has expired and the feature it was intended for
    (shouldInterceptRequestAsync API) was never launched, so this code is
    unused.
    
    This CL removes the histogram definition and its usage in
    WebResponseCallback.java.
    
    Looks like the expiration warning went to my chromium account which was
    still alive while I wasn't working on Chromium so this histogram has
    already full expired.
    
    Bug: 478863780
    Change-Id: I9e145bbc272c63a8772225239cd5f096e2a9dcc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771573
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616910}

--- android_webview/java/src/org/chromium/android_webview/WebResponseCallback.java
+++ android_webview/java/src/org/chromium/android_webview/WebResponseCallback.java
@@ -9,7 +9,6 @@ import org.jni_zero.JNINamespace;
 
 import org.chromium.base.JniOnceCallback;
 import org.chromium.base.Log;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.embedder_support.util.WebResourceResponseInfo;
@@ -87,9 +86,6 @@ public final class WebResponseCallback {
     protected void finalize() throws Throwable {
         try {
             boolean intercepted = mInterceptCalled.get();
-            RecordHistogram.recordBooleanHistogram(
-                    "Android.WebView.ShouldInterceptRequest.Async.CallbackLeakedWithoutResponse",
-                    !intercepted);
             if (!intercepted) {
                 Log.e(
                         TAG,
