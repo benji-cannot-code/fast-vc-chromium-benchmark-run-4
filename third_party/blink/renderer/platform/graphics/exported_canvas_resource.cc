925263bdcaf446a549083c7bbab8121a5d78952e
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-04-17 07:28:42 -0700

    Simplify ExternalCanvasResource destruction
    DropRefOnOwningThread doesn't really do anything, we can replace it
    with DoNothingWithBoundArgs
    
    Change-Id: Idbfc529246dcb6cf3af1a23bdf3d970a69786cce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7767317
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616541}

--- third_party/blink/renderer/platform/graphics/exported_canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/exported_canvas_resource.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/exported_canvas_resource.h"
 
+#include "base/functional/callback_helpers.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 
@@ -26,13 +27,6 @@ ExportedCanvasResource::~ExportedCanvasResource() {
 }
 
 // static
-void ExportedCanvasResource::DropRefOnOwningThread(
-    scoped_refptr<ExportedCanvasResource>&& exported_resource) {
-  CHECK(exported_resource);
-  CHECK(exported_resource->HasOneRef());
-}
-
-// static
 void ExportedCanvasResource::OnPlaceholderReleasedResource(
     scoped_refptr<ExportedCanvasResource>&& exported_resource) {
   if (!exported_resource) {
@@ -45,10 +39,7 @@ void ExportedCanvasResource::OnPlaceholderReleasedResource(
     auto& owning_thread_task_runner =
         exported_resource->resource_->owning_thread_task_runner_;
     owning_thread_task_runner->PostTask(
-        FROM_HERE,
-        base::BindOnce(&DropRefOnOwningThread, std::move(exported_resource)));
-  } else {
-    DropRefOnOwningThread(std::move(exported_resource));
+        FROM_HERE, base::DoNothingWithBoundArgs(std::move(exported_resource)));
   }
 }
 
--- third_party/blink/renderer/platform/graphics/exported_canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/exported_canvas_resource.h
@@ -33,9 +33,6 @@ class PLATFORM_EXPORT ExportedCanvasResource
   CanvasResource* GetResourceForTesting() const { return resource_.get(); }
 
  private:
-  static void DropRefOnOwningThread(
-      scoped_refptr<ExportedCanvasResource>&& exported_resource);
-
   scoped_refptr<CanvasResource> resource_;
 };
 
