4b8e55389d1be1ed6941e86ea2e87284025dfded
Eriko Kurimoto <elkurin@google.com>
2026-04-16 23:54:38 -0700

    WaaP: Fix pixel test failures by adding URL waiter
    This CL fixes ManagedUserProfileNoticeUIWindowPixelTest.InvokeUi_default
    tests with Initial WebUI feature enabled.
    
    The test was capturing the screenshot before the correct page was fully
    loaded, so we call `WaitForPickerUrl` to ensure the expected URL is
    ready.
    
    Bug: 477426026
    Change-Id: I04dc2811dbb90f2c795c91dafc3441271f9a041f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766573
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616362}

--- chrome/browser/ui/views/profiles/managed_user_profile_notice_ui_browsertest.cc
+++ chrome/browser/ui/views/profiles/managed_user_profile_notice_ui_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
+#include "chrome/browser/ui/profiles/profile_ui_test_utils.h"
 #include "chrome/browser/ui/test/test_browser_ui.h"
 #include "chrome/browser/ui/views/profiles/profile_management_step_controller.h"
 #include "chrome/browser/ui/views/profiles/profile_picker_view_test_utils.h"
@@ -185,6 +186,12 @@ class ManagedUserProfileNoticeUIWindowPixelTest
             GetParam().is_ui_refresh_enabled, account_info));
     profile_picker_view_->views::View::AddObserver(this);
     profile_picker_view_->ShowAndWait(GetParam().pixel_test_param.window_size);
+    if (ProfilePicker::GetWebViewForTesting()) {
+      profiles::testing::WaitForPickerUrl(
+          GetParam().is_ui_refresh_enabled
+              ? GURL(chrome::kChromeUIManagedUserProfileNoticeRefreshURL)
+              : GURL(chrome::kChromeUIManagedUserProfileNoticeUrl));
+    }
   }
 
   bool VerifyUi() override {
