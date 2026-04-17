e18dd1c38602c4ca177eebf7feb6308e8b400239
Jérôme Lebel <jlebel@chromium.org>
2026-04-17 06:57:27 -0700

    [iOS] ExtendedAccountInfo notification can happen at anytime
    -[<IdentityManagerObserverBridgeDelegate> onExtendedAccountInfoUpdated:]
    can be called anytime, even when `_areExternalPrivacyContextsBeingBuilt`
    is NO.
    This patch removes:
    CHECK(!_areExternalPrivacyContextsBeingBuilt, base::NotFatalUntil::M153);
    
    And changes:
    CHECK(_handledIdentities.contains(GaiaId(info.gaia)),
              base::NotFatalUntil::M153);
    To:
    CHECK(_handledIdentities.contains(GaiaId(info.gaia)),
              base::NotFatalUntil::M155);
    
    This is not clear if this can be an issue.
    
    Fixed: 501357518
    Change-Id: I9bf11bc29ef4e492bc76d9f5160663943b5f7732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772650
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616516}

--- ios/chrome/browser/authentication/age_mismatch_signout/coordinator/signin_account_capabilities_scene_agent.mm
+++ ios/chrome/browser/authentication/age_mismatch_signout/coordinator/signin_account_capabilities_scene_agent.mm
@@ -172,9 +172,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (info.capabilities.can_sign_in_to_chrome() == signin::Tribool::kFalse) {
     // Capabilities are only available after the External Privacy Contexts have
     // been built.
-    CHECK(!_areExternalPrivacyContextsBeingBuilt, base::NotFatalUntil::M153);
     CHECK(_handledIdentities.contains(GaiaId(info.gaia)),
-          base::NotFatalUntil::M153);
+          base::NotFatalUntil::M155);
     [self handleAgeMismatchSignout];
   }
 }
