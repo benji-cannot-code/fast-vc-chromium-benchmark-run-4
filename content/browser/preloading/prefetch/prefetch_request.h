56a4b486b24a8e653e26e6535c9971d234b7186f
Henrique Nakashima <hnakashima@chromium.org>
2026-04-17 15:11:18 -0700

    Revert "[prerender] rename embedder_histogram_suffix"
    This reverts commit 6219816327003520c0b1dda6ddbeaa292d3d00ea.
    
    Reason for revert: Broke PrerenderBrowserTest + others in Android
    (crbug.com/503825661)
    
    Original change's description:
    > [prerender] rename embedder_histogram_suffix
    >
    > We'll allow speculation rules to annotate histogram as well. This CL
    > renames the embedder_histogram_suffix to histogram_suffix, in prepare
    > for the following changes.
    >
    > No behavior change.
    >
    > NO_IFTTT=Does not change enum
    >
    > Bug: 428500219
    > Change-Id: I72355b20ca0ae6260725f97effe44267a74091f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7768169
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1616559}
    
    Bug: 428500219
    Change-Id: I87dabdc23f9013892a67b31726c92cc229c7c9bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770824
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1616893}

--- content/browser/preloading/prefetch/prefetch_request.h
+++ content/browser/preloading/prefetch/prefetch_request.h
@@ -90,7 +90,7 @@ class CONTENT_EXPORT PrefetchRendererInitiatorInfo final {
 class CONTENT_EXPORT PrefetchBrowserInitiatorInfo final {
  public:
   PrefetchBrowserInitiatorInfo(
-      const std::string& histogram_suffix,
+      const std::string& embedder_histogram_suffix,
       std::unique_ptr<PrefetchRequestStatusListener> request_status_listener);
   ~PrefetchBrowserInitiatorInfo();
 
@@ -100,7 +100,9 @@ class CONTENT_EXPORT PrefetchBrowserInitiatorInfo final {
       delete;
   PrefetchBrowserInitiatorInfo(PrefetchBrowserInitiatorInfo&&);
 
-  const std::string& histogram_suffix() const { return histogram_suffix_; }
+  const std::string& embedder_histogram_suffix() const {
+    return embedder_histogram_suffix_;
+  }
   PrefetchRequestStatusListener* request_status_listener() const {
     return request_status_listener_.get();
   }
@@ -108,7 +110,7 @@ class CONTENT_EXPORT PrefetchBrowserInitiatorInfo final {
  private:
   // The suffix string of embedder triggers used for generating histogram
   // recorded per trigger.
-  std::string histogram_suffix_;
+  std::string embedder_histogram_suffix_;
 
   // Listener of prefetch request. Currently used for WebView initiated
   // prefetch.
@@ -170,7 +172,7 @@ class CONTENT_EXPORT PrefetchRequest final {
       WebContents& referring_web_contents,
       const GURL& url,
       const PrefetchType& prefetch_type,
-      const std::string& histogram_suffix,
+      const std::string& embedder_histogram_suffix,
       const blink::mojom::Referrer& referrer,
       const std::optional<url::Origin>& referring_origin,
       std::optional<net::HttpNoVarySearchData> no_vary_search_hint,
@@ -189,7 +191,7 @@ class CONTENT_EXPORT PrefetchRequest final {
       BrowserContext* browser_context,
       const GURL& url,
       const PrefetchType& prefetch_type,
-      const std::string& histogram_suffix,
+      const std::string& embedder_histogram_suffix,
       const blink::mojom::Referrer& referrer,
       bool javascript_enabled,
       const std::optional<url::Origin>& referring_origin,
@@ -212,7 +214,7 @@ class CONTENT_EXPORT PrefetchRequest final {
       base::WeakPtr<BrowserContext> browser_context,
       const GURL& url,
       const PrefetchType& prefetch_type,
-      const std::string& histogram_suffix,
+      const std::string& embedder_histogram_suffix,
       const blink::mojom::Referrer& referrer,
       bool javascript_enabled,
       const std::optional<url::Origin>& referring_origin,
