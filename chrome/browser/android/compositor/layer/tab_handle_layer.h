9ce521c00b4355ef434c8a5e96c707706c6121bc
Michael Wu <mklw@google.com>
2026-04-17 10:44:33 -0700

    [Sharing Indicators] Implement spinning animation for dynamic indicator
    Previously we treated TabIndicatorStatus kStatic and kDynamic the same.
    This change adds a spinning animation for when the status is kDynamic to
    indicate a currently actuating task.
    
    Demo -
    https://screencast.googleplex.com/cast/NDk1MTE5OTQzMzM1OTM2MHwyNjE2ZDVjZS0xNA
    
    Bug: 499059097
    Change-Id: Ia383ef904f54fc7b0de3f3762f4cf1eedb2c57b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749955
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616687}

--- chrome/browser/android/compositor/layer/tab_handle_layer.h
+++ chrome/browser/android/compositor/layer/tab_handle_layer.h
@@ -56,6 +56,9 @@ class TabHandleLayer : public Layer {
                      float media_indicator_internal_padding,
                      float title_to_media_indicator_spacing,
                      float media_indicator_opacity,
+                     ui::Resource* tab_indicator_overlay_resource,
+                     float target_rotation,
+                     float tab_indicator_overlay_width,
                      float toolbar_width,
                      float x,
                      float y,
@@ -106,6 +109,7 @@ class TabHandleLayer : public Layer {
   scoped_refptr<cc::slim::UIResourceLayer> start_divider_;
   scoped_refptr<cc::slim::UIResourceLayer> end_divider_;
   scoped_refptr<cc::slim::UIResourceLayer> media_indicator_layer_;
+  scoped_refptr<cc::slim::UIResourceLayer> tab_indicator_overlay_layer_;
   scoped_refptr<cc::slim::NinePatchLayer> decoration_tab_;
   scoped_refptr<cc::slim::NinePatchLayer> tab_outline_;
   scoped_refptr<cc::slim::Layer> title_layer_;
@@ -115,6 +119,8 @@ class TabHandleLayer : public Layer {
   scoped_refptr<cc::slim::NinePatchLayer> keyboard_focus_ring_;
 
   float opacity_;
+  float tab_indicator_overlay_rotation_ = 0.f;
+  std::unique_ptr<gfx::Transform> transform_;
   bool foreground_ = false;
   bool is_pinned_ = false;
 };
