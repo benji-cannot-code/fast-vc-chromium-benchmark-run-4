887fdb9d49c993f1208833df44d9a8ab263b2534
Andrew Grieve <agrieve@chromium.org>
2026-04-17 07:04:51 -0700

    JNI Zero: Fix parsing of nested classes in generate_jar_jni()
    And use field accessors in meminfo_dump_provider.cc
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Change-Id: I28b588234347d5c2bc5b5b89c0bbf01092d80891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765913
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616521}

--- base/uuid.h
+++ base/uuid.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_ROBOLECTRIC)
 #include "base/android/jni_string.h"
-#include "base/uuid_jni/UUID_jni.h"
+#include "base/system_jni/UUID_jni.h"
 #include "third_party/jni_zero/jni_zero.h"
 #endif
 
