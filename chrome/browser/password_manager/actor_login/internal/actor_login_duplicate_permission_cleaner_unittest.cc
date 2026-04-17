0d5afab7ce5e12089d96239e857c0b8a159891de
Ioana Pandele <ioanap@chromium.org>
2026-04-17 06:10:18 -0700

    [ActorLoginM2] Add metrics for conflicting permissions deletions
    Bug: b:503668461
    Change-Id: Ie49dfdeb6e5741d5c9ba61f231f8b8bf670310bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772748
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616486}

--- chrome/browser/password_manager/actor_login/internal/actor_login_duplicate_permission_cleaner_unittest.cc
+++ chrome/browser/password_manager/actor_login/internal/actor_login_duplicate_permission_cleaner_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/run_loop.h"
 #include "base/test/gmock_callback_support.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
@@ -106,6 +107,7 @@ class ActorLoginDuplicatePermissionCleanerTest : public testing::Test {
 // the one corresponding to the matching federated credentials are preserved.
 TEST_F(ActorLoginDuplicatePermissionCleanerTest,
        KeepsExcludedPasswordPermissionAndItsFederatedMatchIfPasswordSaved) {
+  base::HistogramTester histogram_tester;
   const GURL kUrl("https://example.com/login");
   const url::Origin kOrigin = url::Origin::Create(kUrl);
   const std::u16string kExcludeUser = u"user1@gmail.com";
@@ -184,6 +186,16 @@ TEST_F(ActorLoginDuplicatePermissionCleanerTest,
                       Not(kExcludeUser)),
                 Field(&password_manager::PasswordForm::actor_login_approved,
                       false))));
+
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.ActorLogin.DuplicatePermissionCleaner.Invocations", true,
+      1);
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.ActorLogin.DuplicatePermissionCleaner.PasswordsDeleted",
+      1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.ActorLogin.DuplicatePermissionCleaner.FederatedDeleted",
+      1, 1);
 }
 
 // Tests that when a new permission is saved for a federated credential
