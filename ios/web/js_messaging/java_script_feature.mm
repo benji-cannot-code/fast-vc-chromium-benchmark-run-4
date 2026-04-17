e3accbc27bebcf04085faf226d1bbe7f0cf24f96
Mike Dougherty <michaeldo@chromium.org>
2026-04-16 19:55:35 -0700

    [iOS] Check OriginFilter before calling JS functions
    JavaScriptFeature instances already restrict injected scripts and filter
    received messages based upon the origin filter.
    
    However, there was no logic to prevent calling a function on a domain
    which doesn't match the origin filter. Blocking this within
    JavaScriptFeature prevents a programmer error from accidentally sending
    function params to an unexpected domain.
    
    Fixed: 503418479
    
    Change-Id: I6190f6c1f8fd4f7dae12fbf9c8a05de37a5033b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7769315
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616295}

--- ios/web/js_messaging/java_script_feature.mm
+++ ios/web/js_messaging/java_script_feature.mm
@@ -279,6 +279,20 @@ bool JavaScriptFeature::GetFeatureRepliesToMessages() const {
   return false;
 }
 
+bool JavaScriptFeature::ShouldAllowCallingFunctionInOrigin(
+    const url::Origin& origin) {
+  if (origin_filter_ == OriginFilter::kPublic) {
+    return YES;
+  }
+
+  for (url::Origin& filter_origin : origin_filter_origins_) {
+    if (origin.IsSameOriginWith(filter_origin)) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
 bool JavaScriptFeature::ShouldHandleMessageFromOrigin(
     const url::Origin& origin) {
   if (origin_filter_ == OriginFilter::kPublic) {
@@ -319,6 +333,10 @@ bool JavaScriptFeature::CallJavaScriptFunction(
   // TODO(crbug.com/40254930): Call the ContentJavascriptFeatureManager instead.
   return false;
 #else
+  if (!ShouldAllowCallingFunctionInOrigin(web_frame->GetSecurityOrigin())) {
+    return false;
+  }
+
   JavaScriptFeatureManager* feature_manager =
       JavaScriptFeatureManager::FromBrowserState(web_frame->GetBrowserState());
   DCHECK(feature_manager);
@@ -331,12 +349,12 @@ bool JavaScriptFeature::CallJavaScriptFunction(
   if (!content_world) {
     return false;
   }
-#endif
+#endif  // BUILDFLAG(ENABLE_IOS_JAVASCRIPT_FLAGS)
   DCHECK(content_world);
 
   return web_frame->GetWebFrameInternal()->CallJavaScriptFunctionInContentWorld(
       function_name, parameters, content_world);
-#endif
+#endif  // BUILDFLAG(USE_BLINK)
 }
 
 bool JavaScriptFeature::CallJavaScriptFunction(
@@ -351,6 +369,10 @@ bool JavaScriptFeature::CallJavaScriptFunction(
   // TODO(crbug.com/40254930): Call the ContentJavascriptFeatureManager instead.
   return false;
 #else
+  if (!ShouldAllowCallingFunctionInOrigin(web_frame->GetSecurityOrigin())) {
+    return false;
+  }
+
   JavaScriptFeatureManager* feature_manager =
       JavaScriptFeatureManager::FromBrowserState(web_frame->GetBrowserState());
   DCHECK(feature_manager);
@@ -363,12 +385,12 @@ bool JavaScriptFeature::CallJavaScriptFunction(
   if (!content_world) {
     return false;
   }
-#endif
+#endif  // BUILDFLAG(ENABLE_IOS_JAVASCRIPT_FLAGS)
   DCHECK(content_world);
 
   return web_frame->GetWebFrameInternal()->CallJavaScriptFunctionInContentWorld(
       function_name, parameters, content_world, std::move(callback), timeout);
-#endif
+#endif  // BUILDFLAG(USE_BLINK)
 }
 
 bool JavaScriptFeature::ExecuteJavaScript(
@@ -381,6 +403,10 @@ bool JavaScriptFeature::ExecuteJavaScript(
   // TODO(crbug.com/40254930): Call the ContentJavascriptFeatureManager instead.
   return false;
 #else
+  if (!ShouldAllowCallingFunctionInOrigin(web_frame->GetSecurityOrigin())) {
+    return false;
+  }
+
   JavaScriptFeatureManager* feature_manager =
       JavaScriptFeatureManager::FromBrowserState(web_frame->GetBrowserState());
   DCHECK(feature_manager);
@@ -393,12 +419,12 @@ bool JavaScriptFeature::ExecuteJavaScript(
   if (!content_world) {
     return false;
   }
-#endif
+#endif  // BUILDFLAG(ENABLE_IOS_JAVASCRIPT_FLAGS)
   DCHECK(content_world);
 
   return web_frame->GetWebFrameInternal()->ExecuteJavaScriptInContentWorld(
       script, content_world, std::move(callback));
-#endif
+#endif  // BUILDFLAG(USE_BLINK)
 }
 
 }  // namespace web
