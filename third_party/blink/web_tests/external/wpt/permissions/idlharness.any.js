cf4762c96ae4f36ba0fdf529deb8e44f33b1b5f4
Antonio Sartori <antoniosartori@chromium.org>
2026-04-17 04:00:51 -0700

    [permissions] Make the ApproximateGeolocationPermissionAPI experimental
    Bug: 494089503
    Change-Id: Ibc45f7e4b37e0788c8fa30d21b386fad99f14e5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705836
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616447}

--- third_party/blink/web_tests/external/wpt/permissions/idlharness.any.js
+++ third_party/blink/web_tests/external/wpt/permissions/idlharness.any.js
@@ -11,7 +11,7 @@ idl_test(
   ['html', 'dom'],
   async idl_array => {
     try {
-      self.permissionStatus = await navigator.permissions.query({ name: "geolocation" });
+      self.permissionStatus = await navigator.permissions.query({ name: "notifications" });
     } catch (e) {}
 
     if (self.GLOBAL.isWorker()) {
