35687b9bd5717605bc8be5c6588684b259a503c1
Colin Blundell <blundell@chromium.org>
2026-04-17 00:26:07 -0700

    [Blink] Fork CRPSI::ShouldReplaceTargetBuffer()
    Bug: 352263194
    Change-Id: Icbf3698cea94f1c25bd2c6d0c28867be84282500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7767455
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616374}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -448,8 +448,6 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
   virtual void EndWriteAccess() = 0;
 
   virtual scoped_refptr<CanvasResourceSharedImage> NewOrRecycledResource() = 0;
-  bool ShouldReplaceTargetBuffer(
-      PaintImage::ContentId content_id = PaintImage::kInvalidContentId);
 
   bool resource_recycling_enabled_ = true;
   const bool is_accelerated_;
@@ -608,6 +606,8 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
   ScopedRasterTimer CreateScopedRasterTimerForCanvas2D() override;
 
  private:
+  bool ShouldReplaceTargetBuffer(
+      PaintImage::ContentId content_id = PaintImage::kInvalidContentId);
   bool IsResourceUsable(CanvasResourceSharedImage* resource);
 
   CanvasImageProvider* GetOrCreateCanvasImageProvider();
@@ -769,6 +769,8 @@ class PLATFORM_EXPORT CanvasNon2DResourceProviderSharedImage
   void EndExternalWrite(const gpu::SyncToken& external_write_sync_token);
 
  private:
+  bool ShouldReplaceTargetBuffer(
+      PaintImage::ContentId content_id = PaintImage::kInvalidContentId);
   void FlushRecording(cc::PaintRecord last_recording);
 
   std::unique_ptr<gpu::RasterScopedAccess> WillDrawInternal();
