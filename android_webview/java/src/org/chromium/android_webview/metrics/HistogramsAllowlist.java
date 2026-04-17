ede059cb2edaf1de599074905dc4bc625f0d0829
Jingping Sun <jings@microsoft.com>
2026-04-17 06:46:22 -0700

    [Unchecked] Fix unchecked warnings in android_webview
    Eliminates all remaining unchecked generics warnings in android_webview
    code (plus EmbeddedComponentLoader, AndroidPaymentAppFinder,
    AccountSelectionJUnitTestBase).
    
    Root-cause fixes:
    * Diamond operator for WeakReference / ArrayList / AtomicReference /
      HashSet / HashMap call sites.
    * AwMediaIntegrityServiceFactory parameterized as
      InterfaceFactory<WebViewMediaIntegrityService>.
    * AwBrowserProcess.transmitMinidumps refactored from a Map[] generic
      array to ArrayList<Map<String, String>>, eliminating the unchecked
      generic array creation.
    
    Suppressions only where the API cannot be typed:
    * AIDL signatures: List<byte[]>, List<Map<String, String>>, and
      Map<String, Boolean> are all rejected by the AIDL compiler (verified
      with the android_sdk aidl binary).
    * Android FilterResults.values is typed Object by the framework.
    * AndroidX support-library boundary passes values as raw Object.
    * Reflection Field.get, Bundle.getSerializable.
    * PropertyKey -> WritableObjectPropertyKey<PropertyModel> cast.
    
    This is part of the effort to enable -Xlint:unchecked globally.
    
    Bug: 358366667
    Change-Id: I056be38ae999f47620ec42a462a9f10a4f1a250f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772008
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616509}

--- android_webview/java/src/org/chromium/android_webview/metrics/HistogramsAllowlist.java
+++ android_webview/java/src/org/chromium/android_webview/metrics/HistogramsAllowlist.java
@@ -53,7 +53,7 @@ public class HistogramsAllowlist {
                     "Power.ForegroundBatteryDrain.30SecondsAvg2",
                     // histograms_allowlist_check END_PARSING
                 };
-        Set<Long> hashes = new HashSet();
+        Set<Long> hashes = new HashSet<>();
         for (String histogram : histogramsAllowlist) {
             hashes.add(AwMetricsUtils.hashHistogramName(histogram));
         }
--- android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewBuilderAdapter.java
+++ android_webview/support_library/java/src/org/chromium/support_lib_glue/SupportLibWebViewBuilderAdapter.java
@@ -72,6 +72,7 @@ class SupportLibWebViewBuilderAdapter implements WebViewBuilderBoundaryInterface
         private @Nullable String mProfileName;
 
         @Override
+        @SuppressWarnings("unchecked") // Values cross the AndroidX boundary as raw Object.
         public void accept(@ConfigField Integer key, Object value) {
             try {
                 switch (key) {
