31beddb1b29799c9f185f72a6f1d217d4e6f051e
Jenna Himawan <jhimawan@google.com>
2026-04-17 07:32:03 -0700

    [Icon migration] Update child account icon pt2
    There was an additional usage of the child account icon that I missed.
    
    Bug: 502609340
    Change-Id: I4a59ed03c699e7f0b8ebd2158826941d9bcc8d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7769986
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1616546}

--- components/browser_ui/settings/android/java/src/org/chromium/components/browser_ui/settings/ManagedPreferencesUtils.java
+++ components/browser_ui/settings/android/java/src/org/chromium/components/browser_ui/settings/ManagedPreferencesUtils.java
@@ -106,9 +106,11 @@ public class ManagedPreferencesUtils {
         return R.drawable.ic_business_small;
     }
 
-    /** @return The resource ID for the Managed by Custodian icon. */
+    /**
+     * @return The resource ID for the Managed by Custodian icon.
+     */
     public static @DrawableRes int getManagedByCustodianIconId() {
-        return R.drawable.ic_account_child_grey600_36dp;
+        return R.drawable.ic_account_child_36dp;
     }
 
     /**
