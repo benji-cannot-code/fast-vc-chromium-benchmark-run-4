71bc2490e3cb6f04403f566e760f239daceb0963
Mike Dougherty <michaeldo@chromium.org>
2026-04-17 19:46:59 -0700

    Add public crash_keys API
    Expose `clearCrashKey` and `setCrashKey` publicly outside of web to
    allow for usage in //ios/chrome and //components.
    
    Change-Id: I97ca12ba099350559e664808d27b383a5f4aef79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7768123
    Reviewed-by: Alexandra Pereira <alexandrasp@google.com>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616995}

---
+++ ios/web/public/js_messaging/resources/crash_keys.ts
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Re-export public functions to make them available outside //ios/web.
+export { clearCrashKey, setCrashKey } from '//ios/web/js_features/crash_keys/resources/crash_keys.js';
