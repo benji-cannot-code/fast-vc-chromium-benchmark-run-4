0648e000cac8bf8a649cf99b8ce14f3ef07b42f5
James Cook <jamescook@chromium.org>
2026-04-18 09:49:20 -0700

    extensions: Port mutation_observers_apitest.cc to desktop Android
    We skipped this test when we brought up chrome/browser/extensions on
    desktop Android. It passes without modification.
    
    Bug: 469417243
    Change-Id: I780e147af18919684d8d1b0c716ea90a6143b761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7774407
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617107}

--- chrome/browser/extensions/mutation_observers_apitest.cc
+++ chrome/browser/extensions/mutation_observers_apitest.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
