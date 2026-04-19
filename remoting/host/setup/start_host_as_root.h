182c4b46a88906da797d44f26f07c71a3322f102
Yuwei Huang <yuweih@chromium.org>
2026-04-19 12:55:36 -0700

    [crd linux][multi-process] Start host implementation
    * Add a --gdm-managed flag to the start-host binary to allow for
      starting the GDM-managed multi-process host.
    * Implement DaemonControllerDelegateLinuxMultiProcess::SetConfigAndStart
      for starting the multi-process host.
    * Just initialize CommandLine once in StartHostMain, so that we can
      check the switches immediately.
    
    Note that the change will break the host migration logic in
    start_host_as_root. I'm going to introduce a new migrate_host binary
    for that purpose.
    
    Bug: 502664397
    Change-Id: I73512ad2a8b615b7051c4148546ba0c9d8b7e52f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7774254
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617239}

--- remoting/host/setup/start_host_as_root.h
+++ remoting/host/setup/start_host_as_root.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-int StartHostAsRoot(int argc, char** argv);
+int StartHostAsRoot();
 
 }  // namespace remoting
 
