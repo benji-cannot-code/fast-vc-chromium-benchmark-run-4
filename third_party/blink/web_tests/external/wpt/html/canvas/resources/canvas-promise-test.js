e1d0abe473c99aeb2f33b0d7b52d025dd3b1a7e4
Jean-Philippe Gravel <jpgravel@chromium.org>
2026-04-19 15:09:29 -0700

    Add missing removeChild in placeholder canvasPromiseTest
    The canvas was left in the DOM after each individual test finished,
    meaning that these canvases could not be garbage collected and
    accumulated until all tests were done.
    
    Change-Id: I73dcf3c8b52a2b18b5ce80f11ba3fb3ab97bc1cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770788
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617257}

--- third_party/blink/web_tests/external/wpt/html/canvas/resources/canvas-promise-test.js
+++ third_party/blink/web_tests/external/wpt/html/canvas/resources/canvas-promise-test.js
@@ -135,6 +135,7 @@ function canvasPromiseTest(
       document.body.appendChild(placeholder);
       await testBody(placeholder.transferControlToOffscreen(),
                       {test, canvasType: CanvasTestType.PLACEHOLDER});
+      document.body.removeChild(placeholder);
     }, 'PlaceholderCanvas: ' + description);
   }
 }
