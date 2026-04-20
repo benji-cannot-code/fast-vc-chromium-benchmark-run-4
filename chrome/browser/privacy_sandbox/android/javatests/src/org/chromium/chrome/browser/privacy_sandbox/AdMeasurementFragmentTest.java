fa50db9c050207752b6f83edaca4b2761d689bfa
Jacob Stanley <jacobstanley@google.com>
2026-04-20 11:43:33 -0700

    Deprecate PrivacySandboxPromptEnabled policy
    The Privacy Sandbox Notice is being deprecated, so the policy is not
    necessary anymore.
    
    Bug: 474716334
    Change-Id: I7cf859ac5545cfc5b84173b51702d1b545a70b58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667323
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617657}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/AdMeasurementFragmentTest.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/AdMeasurementFragmentTest.java
@@ -197,10 +197,7 @@ public final class AdMeasurementFragmentTest {
 
     @Test
     @SmallTest
-    @Policies.Add({
-        @Policies.Item(key = "PrivacySandboxAdMeasurementEnabled", string = "false"),
-        @Policies.Item(key = "PrivacySandboxPromptEnabled", string = "false")
-    })
+    @Policies.Add({@Policies.Item(key = "PrivacySandboxAdMeasurementEnabled", string = "false")})
     public void testAdMeasurementManaged() {
         startAdMeasuremenSettings();
 
