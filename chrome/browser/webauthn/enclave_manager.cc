f0b5ed03ee637ef261ee7c3fb3381b1869f82632
Nina Satragno <nsatragno@chromium.org>
2026-04-20 11:51:44 -0700

    [webauthn] Abort enclave requests if context dies
    Have enclave authenticator transactions return a unique pointer to an
    `EnclaveTransaction` object. When this object is destroyed, the
    websockets client is also destroyed, which should clear the socket from
    the pool. Before this change, the request would only be cleaned up if it
    failed or completed successfully, running even after the user cancels
    out of the WebAuthn dialog or signs out.
    
    Written with Gemini CLI.
    
    Fixed: 487208466
    Change-Id: I3fc26722bcf21ceb6994d9382b7554e691bfab81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7773139
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617668}

--- chrome/browser/webauthn/enclave_manager.cc
+++ chrome/browser/webauthn/enclave_manager.cc
@@ -2050,7 +2050,7 @@ class EnclaveManager::StateMachine {
 
     state_ = State::kRegisteringWithEnclave;
     std::string token = std::move(std::get_if<AccessToken>(&event)->value());
-    enclave::Transact(
+    pending_transaction_ = enclave::Transact(
         manager_->network_context_factory_, enclave::GetEnclaveIdentity(),
         std::move(token),
         /*reauthentication_token=*/std::nullopt,
@@ -2109,7 +2109,7 @@ class EnclaveManager::StateMachine {
 
     state_ = State::kWrappingSecrets;
     std::string token = std::move(std::get_if<AccessToken>(&event)->value());
-    enclave::Transact(
+    pending_transaction_ = enclave::Transact(
         manager_->network_context_factory_, enclave::GetEnclaveIdentity(),
         std::move(token),
         /*reauthentication_token=*/std::nullopt,
@@ -2369,7 +2369,7 @@ class EnclaveManager::StateMachine {
 
   void SendPINAndSecretWrappingRequest(std::string token) {
     state_ = State::kWrappingPINAndSecret;
-    enclave::Transact(
+    pending_transaction_ = enclave::Transact(
         manager_->network_context_factory_, enclave::GetEnclaveIdentity(),
         std::move(token),
         /*reauthentication_token=*/std::nullopt,
@@ -2388,7 +2388,7 @@ class EnclaveManager::StateMachine {
     state_ = State::kSettingPIN;
     std::vector<uint8_t> wrapped_secret =
         GetCurrentWrappedSecretForUser(user_).second;
-    enclave::Transact(
+    pending_transaction_ = enclave::Transact(
         manager_->network_context_factory_, enclave::GetEnclaveIdentity(),
         std::move(token), std::move(rapt_),
         BuildPINAndSecurityDomainSecretWrappingEnclaveRequest(
@@ -2402,7 +2402,7 @@ class EnclaveManager::StateMachine {
 
   void SendPINRenewalRequest(std::string token) {
     state_ = State::kRenewingPIN;
-    enclave::Transact(
+    pending_transaction_ = enclave::Transact(
         manager_->network_context_factory_, enclave::GetEnclaveIdentity(),
         std::move(token), std::nullopt,
         BuildPINRenewalRequest(
@@ -2705,13 +2705,13 @@ class EnclaveManager::StateMachine {
 
     state_ = State::kUnregistering;
     std::string token = std::move(std::get_if<AccessToken>(&event)->value());
-    enclave::Transact(manager_->network_context_factory_,
-                      enclave::GetEnclaveIdentity(), std::move(token),
-                      /*reauthentication_token=*/std::nullopt,
-                      BuildUnregisterMessage(user_->device_id()),
-                      enclave::SigningCallback(),
-                      base::BindOnce(&StateMachine::OnEnclaveResponse,
-                                     weak_ptr_factory_.GetWeakPtr()));
+    pending_transaction_ = enclave::Transact(
+        manager_->network_context_factory_, enclave::GetEnclaveIdentity(),
+        std::move(token),
+        /*reauthentication_token=*/std::nullopt,
+        BuildUnregisterMessage(user_->device_id()), enclave::SigningCallback(),
+        base::BindOnce(&StateMachine::OnEnclaveResponse,
+                       weak_ptr_factory_.GetWeakPtr()));
   }
 
   void DoUnregistering(Event event) {
@@ -3063,6 +3063,8 @@ class EnclaveManager::StateMachine {
   std::optional<trusted_vault::MemberKeysSource> member_keys_source_;
   // When uploading a PIN, this contains the pending `WrappedPIN`.
   std::unique_ptr<EnclaveLocalState::WrappedPIN> wrapped_pin_proto_;
+
+  std::unique_ptr<device::enclave::EnclaveTransaction> pending_transaction_;
 
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<StateMachine> weak_ptr_factory_{this};
--- device/fido/enclave/enclave_authenticator.cc
+++ device/fido/enclave/enclave_authenticator.cc
@@ -230,17 +230,18 @@ void EnclaveAuthenticator::MakeCredential(CtapMakeCredentialRequest request,
 
   RecordEvent(Event::kMakeCredential);
 
-  Transact(network_context_factory_, GetEnclaveIdentity(),
-           std::move(ui_request_->access_token),
-           /*reauthentication_token=*/std::nullopt,
-           BuildMakeCredentialCommand(
-               std::move(pending_make_credential_request_->options.json),
-               std::move(ui_request_->claimed_pin),
-               std::move(ui_request_->wrapped_secret),
-               std::move(ui_request_->secret)),
-           std::move(ui_request_->signing_callback),
-           base::BindOnce(&EnclaveAuthenticator::ProcessMakeCredentialResponse,
-                          weak_factory_.GetWeakPtr()));
+  pending_transaction_ = Transact(
+      network_context_factory_, GetEnclaveIdentity(),
+      std::move(ui_request_->access_token),
+      /*reauthentication_token=*/std::nullopt,
+      BuildMakeCredentialCommand(
+          std::move(pending_make_credential_request_->options.json),
+          std::move(ui_request_->claimed_pin),
+          std::move(ui_request_->wrapped_secret),
+          std::move(ui_request_->secret)),
+      std::move(ui_request_->signing_callback),
+      base::BindOnce(&EnclaveAuthenticator::ProcessMakeCredentialResponse,
+                     weak_factory_.GetWeakPtr()));
 }
 
 void EnclaveAuthenticator::DispatchMakeCredentialWithNewUVKey(
@@ -258,13 +259,13 @@ void EnclaveAuthenticator::DispatchMakeCredentialWithNewUVKey(
       std::move(ui_request_->claimed_pin),
       std::move(ui_request_->wrapped_secret), std::move(ui_request_->secret)));
 
-  Transact(network_context_factory_, GetEnclaveIdentity(),
-           std::move(ui_request_->access_token),
-           /*reauthentication_token=*/std::nullopt,
-           cbor::Value(std::move(requests)),
-           std::move(ui_request_->signing_callback),
-           base::BindOnce(&EnclaveAuthenticator::ProcessMakeCredentialResponse,
-                          weak_factory_.GetWeakPtr()));
+  pending_transaction_ = Transact(
+      network_context_factory_, GetEnclaveIdentity(),
+      std::move(ui_request_->access_token),
+      /*reauthentication_token=*/std::nullopt, cbor::Value(std::move(requests)),
+      std::move(ui_request_->signing_callback),
+      base::BindOnce(&EnclaveAuthenticator::ProcessMakeCredentialResponse,
+                     weak_factory_.GetWeakPtr()));
 }
 
 void EnclaveAuthenticator::GetAssertion(CtapGetAssertionRequest request,
@@ -338,19 +339,20 @@ void EnclaveAuthenticator::DispatchGetAssertion() {
 
   RecordEvent(Event::kGetAssertion);
 
-  Transact(network_context_factory_, GetEnclaveIdentity(),
-           std::move(ui_request_->access_token),
-           /*reauthentication_token=*/std::nullopt,
-           BuildGetAssertionCommand(
-               *ui_request_->entity,
-               std::move(pending_get_assertion_request_->options.json),
-               pending_get_assertion_request_->request.client_data_json,
-               std::move(ui_request_->claimed_pin),
-               std::move(ui_request_->wrapped_secret),
-               std::move(ui_request_->secret)),
-           std::move(ui_request_->signing_callback),
-           base::BindOnce(&EnclaveAuthenticator::ProcessGetAssertionResponse,
-                          weak_factory_.GetWeakPtr()));
+  pending_transaction_ = Transact(
+      network_context_factory_, GetEnclaveIdentity(),
+      std::move(ui_request_->access_token),
+      /*reauthentication_token=*/std::nullopt,
+      BuildGetAssertionCommand(
+          *ui_request_->entity,
+          std::move(pending_get_assertion_request_->options.json),
+          pending_get_assertion_request_->request.client_data_json,
+          std::move(ui_request_->claimed_pin),
+          std::move(ui_request_->wrapped_secret),
+          std::move(ui_request_->secret)),
+      std::move(ui_request_->signing_callback),
+      base::BindOnce(&EnclaveAuthenticator::ProcessGetAssertionResponse,
+                     weak_factory_.GetWeakPtr()));
 }
 
 void EnclaveAuthenticator::DispatchGetAssertionWithNewUVKey(
@@ -370,13 +372,13 @@ void EnclaveAuthenticator::DispatchGetAssertionWithNewUVKey(
       std::move(ui_request_->claimed_pin),
       std::move(ui_request_->wrapped_secret), std::move(ui_request_->secret)));
 
-  Transact(network_context_factory_, GetEnclaveIdentity(),
-           std::move(ui_request_->access_token),
-           /*reauthentication_token=*/std::nullopt,
-           cbor::Value(std::move(requests)),
-           std::move(ui_request_->signing_callback),
-           base::BindOnce(&EnclaveAuthenticator::ProcessGetAssertionResponse,
-                          weak_factory_.GetWeakPtr()));
+  pending_transaction_ = Transact(
+      network_context_factory_, GetEnclaveIdentity(),
+      std::move(ui_request_->access_token),
+      /*reauthentication_token=*/std::nullopt, cbor::Value(std::move(requests)),
+      std::move(ui_request_->signing_callback),
+      base::BindOnce(&EnclaveAuthenticator::ProcessGetAssertionResponse,
+                     weak_factory_.GetWeakPtr()));
 }
 
 void EnclaveAuthenticator::ProcessMakeCredentialResponse(
