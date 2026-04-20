b9c4eaa82fc45f7105ac0ec8d8d5cdb3d482c9d6
Steinar H. Gunderson <sesse@chromium.org>
2026-04-20 09:53:14 -0700

    For static QualifiedNames, precompute the Bloom filter.
    If the name is known in advance, e.g. xml_names::kLangAttr, there's no
    point in dynamically trying to call LocalNameUpper() and meticulously
    putting together the bits of a Bloom filter; we can just store those 32
    bits directly along the QualifiedName. This reduces the code size for
    calling FastHasAttribute() and related code against these known names.
    (The APK size bot claims a small increase for unclear reasons, but a
    large reduction on arm64, and we also wee a significant reduction just
    building the binaries locally on x86-64 Linux.)
    
    It would be even better to just have a compile-time constant
    instead of loading it from RAM, but we don't have that kind of ambition
    in this patch; it would require constexpr rapidhash, at the very least.
    
    Note that HTMLQualifiedName (and also SVGQualifiedName etc.), which is
    mostly a simple alias to increase type safety, now inherits from
    HTMLQualifiedNameWithHash, since there's no point in having a variant
    of these without the hash.
    
    We currently hard-code an assumption that Element::TinyBloomFilter
    is uint32_t, in order to not pull element.h into qualified_name.h.
    
    Certain benchmarks look unfairly good with this presumably since
    we move the work of creating the uppercased LocalName string to
    the process initialization instead of the actual parse time.
    It _is_ faster to do it once on a static buffer like we do here,
    but it seems unlikely to actually speed up e.g. CSS parsing by 10%,
    like the Search microbenchmark would suggest. To counteract this,
    we run the style perftest with --gtest_repeat=10 so that also the
    old version gets to do most of its runs using a warmed-up string
    pool; yet, we get a small benefit in parsing.
    
    Style perftest (Zen 3, LTO but no PGO):
    
      Parse (µs)             Before     After    Perf      95% CI (BCa)
      =================== ========= ========= ======= =================
      ECommerce                 999       993   +0.6%  [ -0.0%,  +1.4%]
      Encyclopedia             5405      5372   +0.6%  [ +0.2%,  +1.1%]
      Extension                 635       645   -1.5%  [ -2.4%,  -0.6%]
      News                     5919      5867   +0.9%  [ +0.4%,  +1.4%]
      Search                   3760      3697   +1.7%  [ +0.8%,  +3.1%]
      Social1                 10712     10651   +0.6%  [ +0.1%,  +1.0%]
      Social2                   472       465   +1.4%  [ +0.6%,  +2.2%]
      Sports                  41381     41007   +0.9%  [ +0.3%,  +1.6%]
      Video                   24636     24398   +1.0%  [ -1.1%,  +3.1%]
      Geometric mean                            +0.7%  [ +0.3%,  +1.1%]
    
    Speedometer3 is a bit up and down, but more up than down and those
    that are down are just barely statistically significant
    (M1 Pinpoint, lower is better, significant results at 99% CI only):
    
      Charts-observable-plot                      [ +0.1%,  +0.8%]
      TodoMVC-WebComponents                       [ +0.1%,  +0.8%]
      NewsSite-Next                               [ +0.0%,  +0.3%]
      TodoMVC-JavaScript-ES5                      [ -2.3%,  -0.4%]
      TodoMVC-JavaScript-ES6-Webpack-Complex-DOM  [ -1.4%,  -0.6%]
    
    Change-Id: Icb83bef991945175aa13645eb1603f3b98397546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766594
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617562}

--- third_party/blink/renderer/core/dom/qualified_name.cc
+++ third_party/blink/renderer/core/dom/qualified_name.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/qualified_name.h"
 
+#include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/mathml_names.h"
 #include "third_party/blink/renderer/core/svg_names.h"
@@ -151,11 +152,31 @@ void QualifiedName::CreateStatic(void* target_address, StringImpl* name) {
       QualifiedName(g_null_atom, AtomicString(name), g_null_atom, true);
 }
 
+void QualifiedNameWithHash::CreateStatic(void* target_address,
+                                         StringImpl* name,
+                                         const AtomicString& name_namespace) {
+  new (target_address) QualifiedNameWithHash(g_null_atom, AtomicString(name),
+                                             name_namespace, true);
+}
+
+void QualifiedNameWithHash::CreateStatic(void* target_address,
+                                         StringImpl* name) {
+  new (target_address)
+      QualifiedNameWithHash(g_null_atom, AtomicString(name), g_null_atom, true);
+}
+
 std::ostream& operator<<(std::ostream& ostream, const QualifiedName& qname) {
   ostream << "QualifiedName(local=" << qname.LocalName()
           << " ns=" << qname.NamespaceURI() << " prefix=" << qname.Prefix()
           << ")";
   return ostream;
 }
+
+QualifiedNameWithHash::QualifiedNameWithHash(const AtomicString& prefix,
+                                             const AtomicString& local_name,
+                                             const AtomicString& namespace_uri,
+                                             bool is_static)
+    : QualifiedName(prefix, local_name, namespace_uri, is_static),
+      bloom_filter(Element::FilterForAttribute(*this)) {}
 
 }  // namespace blink
