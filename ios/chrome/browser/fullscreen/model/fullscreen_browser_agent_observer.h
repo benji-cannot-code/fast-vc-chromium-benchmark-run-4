ed73213ce6f932146a885dcb5e3ad4b4f4b2371c
Scott Yoder <scottyoder@google.com>
2026-04-20 11:25:36 -0700

    [ios] Add FullscreenDidTransition to FullscreenBrowserAgentObserver
    This CL adds a FullscreenDidTransition callback to
    FullscreenBrowserAgentObserver to notify observers when a
    fullscreen transition completes. This is similar to the legacy
    FullscreenController's "FullscreenDidAnimate" callback.
    
    Bug: 500669310
    Change-Id: Icb4c1159b8df632c06ef259736f22143d26ee62e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7778707
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617640}

--- ios/chrome/browser/fullscreen/model/fullscreen_browser_agent_observer.h
+++ ios/chrome/browser/fullscreen/model/fullscreen_browser_agent_observer.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class FullscreenBrowserAgent;
 
+// Enum representing the type of fullscreen transition.
+enum class FullscreenTransition { kEnterFullscreen, kExitFullscreen };
+
 // Observer interface for the FullscreenBrowserAgent.
 class FullscreenBrowserAgentObserver : public base::CheckedObserver {
  public:
@@ -24,6 +27,10 @@ class FullscreenBrowserAgentObserver : public base::CheckedObserver {
 
   // Called after the obscured inset range updates.
   virtual void DidUpdateObscuredInsetRange(FullscreenBrowserAgent* agent) {}
+
+  // Called when the fullscreen transition completes.
+  virtual void FullscreenDidTransition(FullscreenBrowserAgent* agent,
+                                       FullscreenTransition transition) {}
 };
 
 #endif  // IOS_CHROME_BROWSER_FULLSCREEN_MODEL_FULLSCREEN_BROWSER_AGENT_OBSERVER_H_
--- ios/chrome/browser/fullscreen/model/fullscreen_browser_agent_observer_bridge.h
+++ ios/chrome/browser/fullscreen/model/fullscreen_browser_agent_observer_bridge.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Called after the obscured inset range updates.
 - (void)fullscreenDidUpdateObscuredInsetRange:(FullscreenBrowserAgent*)agent;
 
+// Called when the fullscreen transition completes.
+- (void)fullscreen:(FullscreenBrowserAgent*)agent
+     didTransition:(FullscreenTransition)transition;
+
 @end
 
 // Bridge class that listens for `FullscreenBrowserAgent` notifications and
@@ -47,6 +51,8 @@ class FullscreenBrowserAgentObserverBridge
   void DidUpdateState(FullscreenBrowserAgent* agent) override;
   void WillUpdateObscuredInsetRange(FullscreenBrowserAgent* agent) override;
   void DidUpdateObscuredInsetRange(FullscreenBrowserAgent* agent) override;
+  void FullscreenDidTransition(FullscreenBrowserAgent* agent,
+                               FullscreenTransition transition) override;
 
   __weak id<FullscreenBrowserAgentObserving> observer_;
   base::ScopedObservation<FullscreenBrowserAgent,
