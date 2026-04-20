e65bb07d1836c6cc6baace583005b9eab7cc663e
Hailey Wang <haileywang@google.com>
2026-04-20 13:40:26 -0700

    [Glic] Include actor visits in Android history queries
    Bug: 485266564
    Change-Id: I9094f26bdda161f62e6f327fc21135d6d7ef49ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7775074
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617752}

--- chrome/browser/android/history/browsing_history_bridge.cc
+++ chrome/browser/android/history/browsing_history_bridge.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sync_service_factory.h"
+#include "chrome/common/chrome_features.h"
 #include "components/history/core/browser/browsing_history_service.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/url_formatter/url_formatter.h"
@@ -63,6 +64,7 @@ void BrowsingHistoryBridge::QueryHistory(
   options.policy_for_404_visits = history::VisitQuery404sPolicy::kExclude404s;
   options.duplicate_policy = history::QueryOptions::REMOVE_DUPLICATES_PER_DAY;
   options.host_only = j_host_only;
+  options.include_actor_visits = base::FeatureList::IsEnabled(features::kGlic);
   if (j_app_id) {
     options.app_id = base::android::ConvertJavaStringToUTF8(j_app_id);
   }
