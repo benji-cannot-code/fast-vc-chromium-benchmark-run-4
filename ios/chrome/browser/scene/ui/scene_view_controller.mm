c02e991b5c4594547104c6ebec82238c1a416b12
Scott Yoder <scottyoder@google.com>
2026-04-20 05:29:48 -0700

    [ios][ChromeNextIa] Fix resizing of appContentView
    When the appContentView initially is created, the scene view's frame is
    CGRectZero. That results in the appContentView's frame also being
    CGRectZero, and autoresize cannot scale from zero to match the parent
    view. Since we are updating appContentContainerView's frame, we can also
    manually update appContentView. This fixes the issue where the bottom
    omnibox does not appear because it is behind the AppBar.
    
    Change-Id: Ic20ffaa0a41542b6a96af961600d12c04fc7d7e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771899
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617426}

--- ios/chrome/browser/scene/ui/scene_view_controller.mm
+++ ios/chrome/browser/scene/ui/scene_view_controller.mm
@@ -487,6 +487,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   CGRect contentFrame = UIEdgeInsetsInsetRect(frame, insets);
   _appContentContainerView.frame = contentFrame;
+  _appContentView.frame = _appContentContainerView.bounds;
 }
 
 // Applies manual frames to views. This is the fallback layout path when
