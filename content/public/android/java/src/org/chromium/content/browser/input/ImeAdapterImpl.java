0b56f1c05519a6aa67d5f0bc1ecc316fc09f7ff4
Chuong Ho <hdchuong@google.com>
2026-04-20 14:57:13 -0700

    autocorrect: decrement underline clearance counter by key down events
    This CL ensures that the AutocorrectManager gets notified when key down
    events is emitted, so it will count towards decrementing the counter
    that triggers clearing the autocorrect underline. An important case is
    when users press Enter key continuously.
    
    Bug: b:504433975
    Change-Id: I4394bcca5f9fbf678550c528c98f7ebda5c98e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777049
    Commit-Queue: Chuong Ho-Dac <hdchuong@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617785}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
@@ -1278,7 +1278,7 @@ public class ImeAdapterImpl
                                 lastKeyDownEvent.getUnicodeChar());
 
                 if (mAutocorrectManager != null) {
-                    mAutocorrectManager.onCommitText();
+                    mAutocorrectManager.onCommitTextOrSendKeyEvent();
                 }
                 return true;
             }
@@ -1309,7 +1309,7 @@ public class ImeAdapterImpl
             // must be committed before the span can be applied to it.
             if (mAutocorrectManager != null) {
                 mAutocorrectManager.maybeAppendAutocorrectUnderlineSpan();
-                mAutocorrectManager.onCommitText();
+                mAutocorrectManager.onCommitTextOrSendKeyEvent();
             }
 
         } else {
@@ -1362,6 +1362,10 @@ public class ImeAdapterImpl
 
         for (ImeEventObserver observer : mEventObservers) observer.onBeforeSendKeyEvent(event);
         onImeEvent();
+
+        if (action == KeyEvent.ACTION_DOWN && mAutocorrectManager != null) {
+            mAutocorrectManager.onCommitTextOrSendKeyEvent();
+        }
 
         return ImeAdapterImplJni.get()
                 .sendKeyEvent(
