fd06a801a1327b19727ca9a22d74c9ee381c0e8f
Yuwei Huang <yuweih@chromium.org>
2026-04-20 11:34:31 -0700

    remoting: Remove legacy datagram sockets
    Identify and remove dead code from legacy Chromotocol components,
    specifically focusing on P2PDatagramSocket and its fake implementation
    which are no longer used since the decommissioning of PseudoTCP.
    
    Deleted files:
    - remoting/protocol/p2p_datagram_socket.h
    - remoting/protocol/fake_datagram_socket.cc/h
    
    Updated files:
    - remoting/protocol/connection_tester.cc: Removed redundant include.
    - remoting/protocol/BUILD.gn: Removed deleted files from the build.
    
    Bug: 375470501
    Change-Id: If03b825d86c46f8ca1486fbf149133483191fa59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777243
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617653}

--- remoting/protocol/connection_tester.cc
+++ remoting/protocol/connection_tester.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/proto/video.pb.h"
 #include "remoting/protocol/message_pipe.h"
 #include "remoting/protocol/message_serialization.h"
-#include "remoting/protocol/p2p_datagram_socket.h"
 #include "remoting/protocol/p2p_stream_socket.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
