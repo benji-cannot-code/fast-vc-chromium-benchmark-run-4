3dab77a567496314fbd03cabe4f575a5508d2b6c
Liza Bipin <mlbipin@google.com>
2026-04-20 05:50:44 -0700

    [iOS] Add kNoAccountsWebSignin to chrome://flags
    Bug: 503328901
    Change-Id: Iec8d2ef34d6a420492a6fe4d771e903465d2842f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777428
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617437}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -2845,6 +2845,9 @@ constexpr auto kFeatureEntries = std::to_array<flags_ui::FeatureEntry>({
      flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillUpstreamEnforceStrikeDelay)},
+    {"no-accounts-web-signin", flag_descriptions::kNoAccountWebSigninName,
+     flag_descriptions::kNoAccountWebSigninDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(switches::kNoAccountWebSignin)},
 });
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -746,6 +746,9 @@ extern const char kNativeFindInPageDescription[];
 extern const char kNewTabPageFieldTrialName[];
 extern const char kNewTabPageFieldTrialDescription[];
 
+extern const char kNoAccountWebSigninName[];
+extern const char kNoAccountWebSigninDescription[];
+
 extern const char kNonModalSignInPromoName[];
 extern const char kNonModalSignInPromoDescription[];
 
