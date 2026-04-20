85a2750d59605589cf68ac480050ed258a2263fc
Muhammad Salmaan <musalmaan@chromium.org>
2026-04-20 12:22:14 -0700

    [Infobar internals] Add developer tools infobar
    Added developer tools infobar to internals.
    
    Bug: 502951773
    Change-Id: I88294c0bbdf75fbe56ded4460c5870f0fcd9bec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762487
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617690}

--- chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.cc
+++ chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.cc
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
 #include "base/no_destructor.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/devtools/devtools_infobar_delegate.h"
 #include "chrome/browser/global_features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
@@ -90,11 +92,22 @@ void InfoBarInternalsHandler::GetInfoBars(GetInfoBarsCallback callback) {
       "prevent it to shown and then trigger a show request."));
 #endif
 
+  infobar_list.emplace_back(InfoBarEntry::New(
+      /*type=*/InfoBarType::kDevTools, /*name=*/"DevTools",
+      /*description=*/
+      "The DevTools infobar is used to confirm that the user wants to "
+      "allow DevTools to be used. This trigger shows the infobar."));
+
   std::move(callback).Run(std::move(infobar_list));
 }
 
 bool InfoBarInternalsHandler::TriggerInfoBarInternal(InfoBarType type) {
   switch (type) {
+    case InfoBarType::kDevTools: {
+      DevToolsInfoBarDelegate::Create(u"DevTools Infobar test",
+                                      base::DoNothing());
+      return true;
+    }
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
     case InfoBarType::kDefaultBrowser: {
       BrowserWindowInterface* const bwi =
