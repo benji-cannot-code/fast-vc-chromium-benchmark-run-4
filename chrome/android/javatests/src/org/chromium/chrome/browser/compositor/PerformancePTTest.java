07887273672c63079512a368acb390f2ee8042d2
Anna Tsvirchkova <atsvirchkova@google.com>
2026-04-20 05:43:05 -0700

    [Gardener] Disable flaky PerformancePTTest
    The test is flaky on the majority of bots.
    
    Bug: 494044397
    Change-Id: Ie69b84e4343e1665a376c92fd54cda21f5371d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7775929
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617435}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/PerformancePTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/PerformancePTTest.java
@@ -23,7 +23,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.TabStateBrowserControlsVisibilityDelegate;
@@ -33,7 +33,6 @@ import org.chromium.chrome.test.transit.ChromeTransitTestRules;
 import org.chromium.chrome.test.transit.page.WebPageStation;
 import org.chromium.chrome.test.transit.testhtmls.TopBottomLinksPageStation;
 import org.chromium.components.feature_engagement.Tracker;
-import org.chromium.ui.base.DeviceFormFactor;
 
 @RunWith(ChromeJUnit4ClassRunner.class)
 @Batch(Batch.PER_CLASS)
@@ -73,7 +72,7 @@ public class PerformancePTTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/482900746
+    @DisabledTest(message = "crbug.com/482900746, crbug.com/494044397")
     @CommandLineFlags.Add({ChromeSwitches.DISABLE_MINIMUM_SHOW_DURATION})
     public void zeroCompositorFramesWhileScrollingBrowserControls() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
