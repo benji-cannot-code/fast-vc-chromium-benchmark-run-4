d9cfc276e50128e2b460bcc0e5b29ccd38b48dee
Leo Zhao <leozhao@google.com>
2026-04-20 11:23:42 -0700

    [iOS][Forms AI] Fix wallet logo and tint for infobar icons
    It turns out that when UseIconBackgroundTint returns true, Wallet logo
    can not be rendered at all, leaving the UI with a default blue square
    icon. This CL fixs it by returning false for UseIconBackgroundTint.
    Previously icons for entities are tinted with primary color for text.
    This CL changes that to blue 600 to match the design. This CL also fixes
    a DCHECK when branded asset is not used.
    
    Screenshots:
    https://screenshot.googleplex.com/zuaKr4gwBbhfcAq
    https://screenshot.googleplex.com/5JuciPRmjhisUfC
    
    Bug: 503332459, 503328317
    Change-Id: Id36e0216d0afce53cbfc8e344dc5f8b570d01211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770849
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617637}

--- ios/chrome/browser/autofill/model/autofill_ai_save_entity_infobar_delegate_ios.h
+++ ios/chrome/browser/autofill/model/autofill_ai_save_entity_infobar_delegate_ios.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/browser/foundations/autofill_client.h"
 #import "components/infobars/core/confirm_infobar_delegate.h"
 #import "ios/chrome/browser/autofill/autofill_ai/public/save_entity_params.h"
+#import "ui/base/models/image_model.h"
 
 namespace autofill {
 
@@ -41,6 +42,7 @@ class AutofillAiSaveEntityInfoBarDelegateIOS : public ConfirmInfoBarDelegate {
   std::u16string GetTitleText() const override;
   std::u16string GetMessageText() const override;
   std::u16string GetButtonLabel(InfoBarButton button) const override;
+  bool UseIconBackgroundTint() const override;
   bool Accept() override;
   bool Cancel() override;
   void InfoBarDismissed() override;
@@ -53,6 +55,7 @@ class AutofillAiSaveEntityInfoBarDelegateIOS : public ConfirmInfoBarDelegate {
 
   SaveEntityParams params_;
   base::OnceClosure accept_callback_;
+  ui::ImageModel icon_;
 };
 
 }  // namespace autofill
