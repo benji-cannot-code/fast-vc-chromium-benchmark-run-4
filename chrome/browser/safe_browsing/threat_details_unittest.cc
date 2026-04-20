88e1731c60defd07ae0f3c63e01771f4df8ea21a
Antonio Sartori <antoniosartori@chromium.org>
2026-04-20 10:40:56 -0700

    Avoid continuously reparsing NTP url
    This CL is a refactoring which switches to using the GURL version of
    ChromeUINewTabURL and/or ChromeUINewPageURL in chrome/browser/safe_browsing, avoiding
    parsing GURL again and again.
    
    This CL was uploaded by git cl split.
    
    R=thefrog@chromium.org
    
    Bug: b:503370195
    Change-Id: Iede42e8aa20ef37a18ed931291731e794d3b5be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7776801
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617601}

--- chrome/browser/safe_browsing/threat_details_unittest.cc
+++ chrome/browser/safe_browsing/threat_details_unittest.cc
@@ -200,7 +200,7 @@ class MockSafeBrowsingUIManager : public SafeBrowsingUIManager {
       : SafeBrowsingUIManager(
             std::make_unique<ChromeSafeBrowsingUIManagerDelegate>(),
             std::make_unique<ChromeSafeBrowsingBlockingPageFactory>(),
-            GURL(chrome::kChromeUINewTabURL)),
+            chrome::ChromeUINewTabURLAsGURL()),
         report_sent_(false) {}
   MOCK_METHOD2(AttachThreatDetailsAndLaunchSurvey,
                void(content::BrowserContext* browser_context,
