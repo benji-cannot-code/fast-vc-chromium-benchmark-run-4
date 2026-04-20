ae61733016b4cd96a59d287a86481ac3508b31d3
Kristi Saney <kristislee@google.com>
2026-04-20 14:12:04 -0700

    [RM Line Focus] Add new style and movement strategies for line focus.
    These are basic classes for now but more logic will be moved over a few
    changes. This is part of a broader effort to refactor line focus to have
    better separation of concerns and be more readable.
    
    Bug: 503090130
    Change-Id: I8ac7d86d8dcb9720f255f0dabf0c23b8d9d7e507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7774110
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617764}

---
+++ chrome/browser/resources/side_panel/read_anything/content/line_focus_move_mode.ts
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {LineFocusMovement} from '../content/read_anything_types.js';
+
+// Base class for line focus movement strategies.
+export abstract class LineFocusMoveMode {
+  // Returns the movement type of this movement strategy.
+  abstract getMovement(): LineFocusMovement;
+}
+
+// Movement strategy where the focus element stays centered in the view,
+// scrolling the view when needed.
+export class LineFocusStaticMoveMode extends LineFocusMoveMode {
+  getMovement(): LineFocusMovement {
+    return LineFocusMovement.STATIC;
+  }
+}
+
+// Movement strategy where the focus element follows the mouse cursor.
+export class LineFocusCursorMoveMode extends LineFocusMoveMode {
+  getMovement(): LineFocusMovement {
+    return LineFocusMovement.CURSOR;
+  }
+}
+
+// Movement strategy for when line focus is disabled.
+export class LineFocusNoneMoveMode extends LineFocusMoveMode {
+  constructor(private movement_: LineFocusMovement) {
+    super();
+  }
+
+  getMovement(): LineFocusMovement {
+    return this.movement_;
+  }
+}
