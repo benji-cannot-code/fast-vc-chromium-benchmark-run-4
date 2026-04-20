877bf78f059d3144c914f20a325434dc58fa2dd3
Joemer Ramos <joemerramos@google.com>
2026-04-20 08:52:16 -0700

    [iOS] Fix Ask Gemini Chip 1s Impression
    It was found that the Reader Mode chip was overriding the Ask Gemini
    Chip causing the Ask Gemini Chip to only show for 1s before
    disappearing. This was only seen in non-US countries since the Reader
    Mode chip isn't available yet on US. To fix this issue, we ensure that
    Gemini IPHs and chips prevent Contextual Panel chips and IPHs in
    LocationBarBadgeMediator. We also add this check in ReaderModeModel to
    ensure that we're not adding/ignoring a large entrypoint impression.
    
    To make this implementation work, we now identify an Ask Gemini Chip
    impression as the badge showing opposed to waiting until the badge has
    expanded into a chip and then collapsed to count the impression.
    
    UI Video:
    https://drive.google.com/file/d/1Sdf6Tyst7ZJBLCUIt3xF-31sd9PzxLoX/view?usp=drive_link&resourcekey=0-zFBGFUNzFl73KpIw9RP12A
    
    Fixed: 485527161
    Change-Id: I4dac56ef762447cbc9a7c69148edcc7bfbb47f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770823
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617519}

--- ios/chrome/browser/location_bar/badge/coordinator/location_bar_badge_mediator.mm
+++ ios/chrome/browser/location_bar/badge/coordinator/location_bar_badge_mediator.mm
@@ -208,6 +208,8 @@ constexpr base::TimeDelta kStartCollapseTransitionTime = base::Seconds(5);
     _promoStartTimer = nil;
     _promoEndTimer = nil;
     [self.consumer hideBadge];
+    [self ensureFETFeatureIsDismissed];
+    [self preventContextualPanelEntryPoint:NO];
   }
 }
 
@@ -361,17 +363,28 @@ constexpr base::TimeDelta kStartCollapseTransitionTime = base::Seconds(5);
 
 - (void)handleBadgeContainerCollapse:(LocationBarBadgeType)badgeType {
   switch (badgeType) {
-    case LocationBarBadgeType::kGeminiContextualCueChip:
-      if (!IsAskGeminiChipIgnoreCriteria()) {
-        _tracker->Dismissed(feature_engagement::kIPHiOSGeminiContextualCueChip);
-      }
+    case LocationBarBadgeType::kGeminiContextualCueChip: {
+      [self ensureFETFeatureIsDismissed];
+      [self preventContextualPanelEntryPoint:NO];
       break;
+    }
     default:
       break;
   }
 }
 
 #pragma mark - Private
+
+// Sets the suppression flag for the contextual panel entrypoint.
+- (void)preventContextualPanelEntryPoint:(BOOL)prevent {
+  if (!_activeWebState) {
+    return;
+  }
+  BwgTabHelper* tabHelper = BwgTabHelper::FromWebState(_activeWebState);
+  if (tabHelper) {
+    tabHelper->SetPreventContextualPanelEntryPoint(prevent);
+  }
+}
 
 // Dismisses the Feature Engagement Tracker feature. Safe to call
 // multiple times as a cleanup function since Dismissed() only clears active
@@ -562,6 +575,7 @@ constexpr base::TimeDelta kStartCollapseTransitionTime = base::Seconds(5);
   switch (badgeType) {
     case LocationBarBadgeType::kGeminiContextualCueChip:
       if ([self shouldShowGeminiContextualBadge]) {
+        [self preventContextualPanelEntryPoint:YES];
         return YES;
       }
       return NO;
@@ -592,8 +606,6 @@ constexpr base::TimeDelta kStartCollapseTransitionTime = base::Seconds(5);
           contextualPanelTabHelper->GetFirstCachedConfig().get();
       return [self canShowLargeEntrypointWithConfig:config];
     }
-    case LocationBarBadgeType::kGeminiContextualCueChip:
-      return [self shouldShowGeminiContextualChip];
     case LocationBarBadgeType::kNone:
       return NO;
     default:
@@ -699,18 +711,7 @@ constexpr base::TimeDelta kStartCollapseTransitionTime = base::Seconds(5);
     return YES;
   }
 
-  return isPageEligible && isConsentEligible && eligibleTimeWindow &&
-         _tracker->WouldTriggerHelpUI(
-             feature_engagement::kIPHiOSGeminiContextualCueChip);
-}
-
-// Whether to show Gemini contextual chip.
-- (BOOL)shouldShowGeminiContextualChip {
-  if (IsAskGeminiChipIgnoreCriteria()) {
-    return YES;
-  }
-
-  if (_isFETPromoShowing) {
+  if (!(isPageEligible && isConsentEligible && eligibleTimeWindow)) {
     return NO;
   }
 
