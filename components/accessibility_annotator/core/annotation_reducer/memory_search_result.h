fc7c3faf67b23a364941cf1022788f420dd9ef6a
Muyao Xu <muyaoxu@google.com>
2026-04-20 13:30:57 -0700

    Revert "Use IbanAccessManager before revealing Iban values for filling"
    This reverts commit c277678dfc604806f9c84040b5cd1b7d9f75c1f6.
    
    Reason for revert: cauing msan builder failures. See crbug.com/504698172
    
    Original change's description:
    > Use IbanAccessManager before revealing Iban values for filling
    >
    > Previously, revealing obfuscated IBANs in AtMemory search results did
    > not require any authentication steps.
    >
    > This CL:
    > 1. Removes the reveal_callback in favor of storing the identifiers of
    > objects the filled value is coming from, and accessing them (or their
    > managers) to get the unmasked values.
    > 2. Uses the identifier and IbanAccessManager in order to respect the
    > authentication before filling (both local and server ibans).
    >
    > Bug: 497066073
    > Change-Id: Ia30a4bbf298f4af0ed8d07f11c4fffeb1c622282
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766174
    > Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    > Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    > Commit-Queue: Karol Sygiet <sygiet@google.com>
    > Reviewed-by: Ireneusz Szulc <ireneuszszulc@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1617512}
    
    Bug: 497066073
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I07a3b2f81f5637e0e9cc4b81d444ad1a50be80b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7774652
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Auto-Submit: Muyao Xu <muyaoxu@google.com>
    Owners-Override: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617736}

--- components/accessibility_annotator/core/annotation_reducer/memory_search_result.h
+++ components/accessibility_annotator/core/annotation_reducer/memory_search_result.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_ANNOTATION_REDUCER_MEMORY_SEARCH_RESULT_H_
 #define COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_ANNOTATION_REDUCER_MEMORY_SEARCH_RESULT_H_
 
-#include <memory>
 #include <optional>
 #include <string>
 #include <variant>
@@ -102,9 +101,8 @@ struct MemorySearchResult {
   // Whether the value is obfuscated.
   bool is_obfuscated = false;
 
-  // The identifier of the entry (e.g. IBAN Guid or InstrumentId). If
-  // `EntryType` does not support identifiers, it will be unset (monostate).
-  std::variant<std::monostate, std::string, int64_t> identifier;
+  // Callback to reveal the real, unobfuscated, value.
+  base::RepeatingCallback<std::u16string()> reveal_callback;
 };
 
 enum class MemorySearchStatus {
