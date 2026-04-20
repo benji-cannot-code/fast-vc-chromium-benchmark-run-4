9fe2906a7edeac589d4ccc03cee2f81dcea26f53
Federica Germinario <fedegermi@google.com>
2026-04-20 07:01:46 -0700

    [ios] Remove some unused pref names
    Bug: none
    Change-Id: I508d624b89c5038db032749f4738e23aefd37dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7776211
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617461}

--- ios/chrome/browser/shared/model/prefs/pref_names.h
+++ ios/chrome/browser/shared/model/prefs/pref_names.h
@@ -73,9 +73,6 @@ inline constexpr char kLegacyProfileMap[] = "profile.legacy_profiles.map";
 // A boolean recording whether the legacy profiles have been marked as such.
 inline constexpr char kLegacyProfileHidden[] = "profile.legacy_profiles.hidden";
 
-inline constexpr char kClearBrowsingDataHistoryNoticeShownTimes[] =
-    "browser.clear_data.history_notice_shown_times";
-
 // A dictionary mapping content notification enrollment eligibilities. This is
 // stored in Profile prefs.
 inline constexpr char kContentNotificationsEnrollmentEligibility[] =
@@ -711,11 +708,6 @@ inline constexpr char kUserAgentWasChanged[] = "UserAgentWasChanged";
 // were logged.
 inline constexpr char kLastApplicationStorageMetricsLogTime[] =
     "LastApplicationStorageMetricsLogTime";
-
-// Count the number of times the Search Engine Choice Screen was skipped
-// because the application was started via an external Intent.
-inline constexpr char kChoiceScreenSkippedCount[] =
-    "ios.search_engine_choice_screen.skip_count";
 
 // Prefs indicating whether Home surface modules are enabled.
 inline constexpr char kHomeCustomizationMostVisitedEnabled[] =
