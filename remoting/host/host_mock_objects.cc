c6af23ac4c532b28efbea0127995ff36429d2bb6
Yuwei Huang <yuweih@chromium.org>
2026-04-20 16:49:41 -0700

    remoting: Remove legacy audio pipeline components
    Identify and remove dead code from legacy Chromotocol audio components
    that are no longer used in the WebRTC-based architecture.
    
    Deleted files:
    - remoting/protocol/audio_pump.cc/h
    - remoting/protocol/audio_pump_unittest.cc
    - remoting/protocol/audio_reader.cc/h
    - remoting/protocol/audio_writer.cc/h
    - remoting/protocol/audio_decode_scheduler.cc/h
    - remoting/protocol/audio_decode_scheduler_unittest.cc
    - remoting/codec/audio_decoder.cc/h
    - remoting/codec/audio_decoder_opus.cc/h
    - remoting/codec/audio_encoder.h
    - remoting/codec/audio_encoder_opus.cc/h
    - remoting/codec/audio_encoder_opus_unittest.cc
    
    Updated files to remove references and dependencies:
    - remoting/protocol/BUILD.gn
    - remoting/codec/BUILD.gn
    - remoting/codec/DEPS
    - remoting/codec/GEMINI.md
    - remoting/host/host_mock_objects.cc
    
    Bug: 375470501
    Change-Id: I73a44afeb0f66446c9022dbd4a7929637491ccb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780367
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617863}

--- remoting/host/host_mock_objects.cc
+++ remoting/host/host_mock_objects.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "net/base/ip_endpoint.h"
 #include "remoting/base/auto_thread_task_runner.h"
-#include "remoting/codec/audio_encoder.h"
 #include "remoting/codec/video_encoder.h"
 #include "remoting/host/input_injector.h"
 #include "remoting/proto/event.pb.h"
