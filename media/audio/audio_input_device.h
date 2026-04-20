4e0cbb6b570c9c3afd2102049d976752b92999f5
Palak Agarwal <agpalak@chromium.org>
2026-04-20 04:38:04 -0700

    Cleanup obsolete AudioInputStreamHandle and MojoAudioInputStream
    This CL removes several classes that were part of the legacy audio input
    path: AudioInputStreamHandle, MojoAudioInputStream, and
    AudioInputDelegate, along with their associated unit tests.
    
    The architecture diagram in audio_input_device.h is also updated to
    reflect the current implementation.
    
    Bug: 498221242
    Change-Id: Ie1d6099632f1767ab8860699fccbe7b16b3df74c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766172
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617403}

--- media/audio/audio_input_device.h
+++ media/audio/audio_input_device.h
@@ -8,15 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Relationship of classes:
 //
-//  AudioInputController                 AudioInputDevice
+//  audio::InputController                 AudioInputDevice
 //           ^                                  ^
 //           |                                  |
 //           v                  IPC             v
-// MojoAudioInputStream    <----------->  AudioInputIPC
-//           ^                            (MojoAudioInputIPC)
-//           |
-//           v
-// AudioInputDeviceManager
+// audio::mojom::AudioInputStream    <----------->  AudioInputIPC
+//                                       (MojoAudioInputIPC)
 //
 // Transportation of audio samples from the browser to the render process
 // is done by using shared memory in combination with a SyncSocket.
