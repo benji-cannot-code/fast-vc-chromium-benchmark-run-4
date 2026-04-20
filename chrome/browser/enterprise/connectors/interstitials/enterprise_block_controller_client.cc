deaf8f697189f78dcbeb0c72db6e85eefc64ec6a
Antonio Sartori <antoniosartori@chromium.org>
2026-04-20 13:11:56 -0700

    Avoid continuously reparsing NTP url
    This CL is a refactoring which switches to using the GURL version of
    ChromeUINewTabURL and/or ChromeUINewPageURL in chrome/browser/enterprise/connectors/interstitials, avoiding
    parsing GURL again and again.
    
    This CL was uploaded by git cl split.
    
    R=seblalancette@chromium.org
    
    Bug: 503370195
    Change-Id: I07d981915acabc003d21b59e4bb8f1ca75bed1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7776798
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617730}

--- chrome/browser/enterprise/connectors/interstitials/enterprise_block_controller_client.cc
+++ chrome/browser/enterprise/connectors/interstitials/enterprise_block_controller_client.cc
@@ -37,7 +37,7 @@ EnterpriseBlockControllerClient::EnterpriseBlockControllerClient(
           Profile::FromBrowserContext(web_contents->GetBrowserContext())
               ->GetPrefs(),
           g_browser_process->GetApplicationLocale(),
-          GURL(chrome::kChromeUINewTabURL),
+          chrome::ChromeUINewTabURLAsGURL(),
           /*settings_page_helper=*/nullptr),
       request_url_(request_url) {}
 
--- chrome/browser/enterprise/connectors/interstitials/enterprise_warn_controller_client.cc
+++ chrome/browser/enterprise/connectors/interstitials/enterprise_warn_controller_client.cc
@@ -37,7 +37,7 @@ EnterpriseWarnControllerClient::EnterpriseWarnControllerClient(
           Profile::FromBrowserContext(web_contents->GetBrowserContext())
               ->GetPrefs(),
           g_browser_process->GetApplicationLocale(),
-          GURL(chrome::kChromeUINewTabURL),
+          chrome::ChromeUINewTabURLAsGURL(),
           /*settings_page_helper=*/nullptr),
       request_url_(request_url) {}
 
