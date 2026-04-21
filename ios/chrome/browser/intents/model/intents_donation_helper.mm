b143f04a1cb30194040ed083202af3456e6ee747
Federica Germinario <fedegermi@google.com>
2026-04-21 07:27:45 -0700

    [ios] Remove duplicate "Search 'search phrase' in Chrome" intent
    It seems SiriKit does not require anymore that the intent parameter is
    non-empty. Moreover, it seems this is creating a wrong behavior on iOS
    26 (an additional shortcut is added with an empty search phrase).
    
    Removing that code does not create regressions on old iOS versions and
    fixes the issue on iOS26.
    
    Moreover, with old code, running "Search in Chrome" with an empty
    'search phrase' was a no-op, only NTP was opened. On the other hand by
    removing this line the expected behavior is restored (NTP is opened and
    omnibox is focused).
    
    Fixed: 498177995
    Change-Id: If7b410b6c94408678ecc167133bd7730a2e3693d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7716480
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618166}

--- ios/chrome/browser/intents/model/intents_donation_helper.mm
+++ ios/chrome/browser/intents/model/intents_donation_helper.mm
@@ -45,12 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     case IntentType::kSearchInChrome: {
       SearchInChromeIntent* searchInChromeIntent =
           [[SearchInChromeIntent alloc] init];
-
-      // SiriKit requires the intent parameter to be set to a non-empty
-      // string in order to accept the intent donation. Set it to a single
-      // space, to be later trimmed by the intent handler, which will result
-      // in the shortcut being treated as if no search phrase was supplied.
-      searchInChromeIntent.searchPhrase = @" ";
       searchInChromeIntent.suggestedInvocationPhrase = l10n_util::GetNSString(
           IDS_IOS_INTENTS_SEARCH_IN_CHROME_INVOCATION_PHRASE);
       INInteraction* interaction =
