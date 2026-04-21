06f26c94fddacc6e4ffc658328d8e5e75ece7795
Andrew Paseltiner <apaseltiner@chromium.org>
2026-04-21 09:10:02 -0700

    js_injection: Fix Use-After-Free in JsBinding
    JsBinding is an Oilpan-managed object but used a raw Mojo receiver. This
    allowed IPC messages to be dispatched to a logically dead object during
    the lazy sweeping phase. Synchronous JS execution during the IPC handler
    could then trigger a nested GC that frees the object while it is still
    on the stack.
    
    This CL fixes the issue by adding a pre-finalizer to JsBinding that
    explicitly resets the Mojo receiver during the GC's atomic pause.
    
    Fixed: 503889643
    Change-Id: I3468fce74c5c8c3fa24d33ee1b35398df2e08b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780011
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618238}

--- components/js_injection/renderer/js_binding.h
+++ components/js_injection/renderer/js_binding.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "third_party/blink/public/common/messaging/string_message_codec.h"
 #include "v8/include/cppgc/persistent.h"
+#include "v8/include/cppgc/prefinalizer.h"
 #include "v8/include/v8.h"
 
 namespace v8 {
@@ -39,6 +40,8 @@ class JsCommunication;
 // to the page. JsBinding is owned by v8.
 class JsBinding final : public gin::Wrappable<JsBinding>,
                         public mojom::BrowserToJsMessaging {
+  CPPGC_USING_PRE_FINALIZER(JsBinding, Dispose);
+
  public:
   static constexpr gin::WrapperInfo kWrapperInfo = {{gin::kEmbedderNativeGin},
                                                     gin::kJsBinding};
@@ -74,6 +77,8 @@ class JsBinding final : public gin::Wrappable<JsBinding>,
       mojo::PendingAssociatedReceiver<mojom::BrowserToJsMessaging> receiver);
 
  private:
+  void Dispose();
+
   // gin::WrappableBase implementation.
   gin::ObjectTemplateBuilder GetObjectTemplateBuilder(
       v8::Isolate* isolate) override;
