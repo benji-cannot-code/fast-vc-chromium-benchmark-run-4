4e37c5a3f2465ec9e54bd9ca14ed07d8860873a1
Alexis Wu <alexiswu@google.com>
2026-04-21 15:56:13 -0700

    Add Glic Enabling JNI
    Bug: 481386779
    Change-Id: I1cda2bd254f13093a562e6c059f7abaa1f02b4c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765743
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Alexis Wu <alexiswu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618518}

---
+++ chrome/browser/glic/android/glic_enabling_android.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "chrome/browser/glic/public/glic_enabling.h"
+
+#include "chrome/browser/glic/android/jni_headers/GlicEnabling_jni.h"
+#include "chrome/browser/profiles/profile.h"
+namespace glic {
+bool JNI_GlicEnabling_IsEnabledByFlags(JNIEnv* env) {
+  return GlicEnabling::IsEnabledByFlags();
+}
+bool JNI_GlicEnabling_IsProfileEligible(JNIEnv* env, Profile* profile) {
+  return GlicEnabling::IsProfileEligible(profile);
+}
+bool JNI_GlicEnabling_IsEnabledForProfile(JNIEnv* env, Profile* profile) {
+  return GlicEnabling::IsEnabledForProfile(profile);
+}
+bool JNI_GlicEnabling_ShouldShowSettingsPage(JNIEnv* env, Profile* profile) {
+  return GlicEnabling::ShouldShowSettingsPage(profile);
+}
+bool JNI_GlicEnabling_IsReadyForProfile(JNIEnv* env, Profile* profile) {
+  return GlicEnabling::IsReadyForProfile(profile);
+}
+}  // namespace glic
+DEFINE_JNI(GlicEnabling)
