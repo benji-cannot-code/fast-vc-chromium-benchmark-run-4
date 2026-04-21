62eb6926bfc8f822ab279adce8840e10144841b5
Yuwei Huang <yuweih@chromium.org>
2026-04-21 16:36:17 -0700

    [remoting] Add boilerplate migrate_host binary for Linux
    This CL adds a boilerplate 'migrate_host' binary to the Chrome Remote
    Desktop host on Linux. This binary will be used to handle migration
    between the single-process host and the multi-process (GDM-managed)
    host.
    
    Changes included:
    - Created a new 'migrate_host' directory with boilerplate
      implementation.
    - Added 'migrate_host' to the Linux build targets and installer.
    - Integrated 'migrate_host' into the .deb package build process.
    - Updated documentation in architecture.md to include instructions for
      adding new binaries, specifically emphasizing the need to clarify
      target platforms and update Linux-specific build targets.
    
    Bug: 502664397
    Change-Id: I8940830fe6683abacf1028a56ee3f3000b22332a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783662
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618541}

---
+++ remoting/host/linux/migrate_host_main.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/linux/migrate_host_main.h"
+
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/message_loop/message_pump_type.h"
+#include "base/task/single_thread_task_executor.h"
+#include "remoting/base/logging.h"
+#include "remoting/host/base/host_exit_codes.h"
+
+namespace remoting {
+
+int MigrateHostMain(int argc, char** argv) {
+  base::AtExitManager exit_manager;
+  base::SingleThreadTaskExecutor io_task_executor(base::MessagePumpType::IO);
+
+  base::CommandLine::Init(argc, argv);
+  InitHostLogging();
+
+  HOST_LOG << "migrate_host binary started.";
+
+  // TODO: crbug.com/502664397 - Implement host migration logic.
+
+  return kSuccessExitCode;
+}
+
+}  // namespace remoting
---
+++ remoting/host/linux/migrate_host_main.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_LINUX_MIGRATE_HOST_MAIN_H_
+#define REMOTING_HOST_LINUX_MIGRATE_HOST_MAIN_H_
+
+#include "remoting/base/remoting_export.h"
+
+namespace remoting {
+
+// The migrate_host entry point exported from remoting_core.so.
+REMOTING_EXPORT int MigrateHostMain(int argc, char** argv);
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_LINUX_MIGRATE_HOST_MAIN_H_
