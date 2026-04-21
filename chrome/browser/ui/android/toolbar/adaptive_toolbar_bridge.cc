04312e09bc64ed0733a6a3ca601ff1cd01b98fce
Calder Kitagawa <ckitagawa@chromium.org>
2026-04-21 10:28:47 -0700

    [IA] Move optional/adaptive code to new build target
    When moved to LocationBar this will create a cyclic dependency between
    omnibox and toolbar. Break this by moving the optional/adaptive button
    code to a separate build target.
    
    Bug: 491511644
    Change-Id: I4531d28de4507d5590f0ba447e6b66cc2a9766a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771432
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618292}

--- chrome/browser/ui/android/toolbar/adaptive_toolbar_bridge.cc
+++ chrome/browser/ui/android/toolbar/adaptive_toolbar_bridge.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/public/segmentation_platform_service.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/browser/ui/android/toolbar/jni_headers/AdaptiveToolbarBridge_jni.h"
+#include "chrome/browser/ui/android/toolbar/adaptive_jni_headers/AdaptiveToolbarBridge_jni.h"
 
 using base::android::AttachCurrentThread;
 using base::android::JavaRef;
