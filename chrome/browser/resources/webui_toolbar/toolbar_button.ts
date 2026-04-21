f31f5f98b9c94fd5ac31f525aed6874c7954d64d
Matt Menke <mmenke@chromium.org>
2026-04-21 12:48:26 -0700

    Webium: Make PressHandler not try to cancel a timer that's not running.
    PressHandler never clears the ID of the timer it starts, so can
    potentially call clearTimeout() with the ID of a no-longer-running
    timer. Since setTimeout() doesn't guarantee IDs won't be reused, this
    could, in theory, cancel a random newly started timer.
    
    In practice, I believe we just keep on incrementing IDs used for new
    timers, so if we cancel a timer with the old ID likely isn't going to
    cause issues, since we'd need to create 2 billion before they wrap
    around, but just to be safe, we should avoid cancelling a timer that's
    not running.
    
    This CL introduces a TimerHelper class that wraps the timer ID and
    setTimeout() / clearTimeout() calls, and migrates PressHandler and
    ReloadButton to both use it.
    
    This may be a bit more complexity than it's worth, but ReloadButton
    will need another timer as well, and possible we'll run into other
    UI-related consumers that need the same usage pattern: only one timer
    running at a time, with the occasional need to cancel it in response
    to new user input or other events.
    
    Bug: 503755158
    Change-Id: Iefcb3d38f2b73da7b4c3103b3f4341a11876aaa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772836
    Reviewed-by: Youseff Bourouphel <ybourouphael@google.com>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618381}

--- chrome/browser/resources/webui_toolbar/toolbar_button.ts
+++ chrome/browser/resources/webui_toolbar/toolbar_button.ts
@@ -7,6 +7,7 @@ import {MenuSourceType} from '//resources/mojo/ui/base/mojom/menu_source_type.mo
 import {isMac} from 'chrome://resources/js/platform.js';
 
 import {ClickDispositionFlag} from './browser_proxy.js';
+import {TimerHelper} from './timer_helper.js';
 
 export const BUTTON_LEFT = 0;
 export const BUTTON_MIDDLE = 1;
@@ -67,7 +68,7 @@ export class PressHandler {
   private static readonly NO_ACTIVE_POINTER_ID = -1;
 
   private isLongPressed_: boolean = false;
-  private longPressTimer_: number = 0;
+  private longPressTimer_: TimerHelper = new TimerHelper();
   private onLongPress_: (source: MenuSourceType) => void;
   private onShortPress_: (e: MouseEvent) => void;
   // Whether to treat Mac Ctrl+LeftClick as a context menu trigger (long press)
@@ -101,7 +102,7 @@ export class PressHandler {
     // Detect downward drag.
     if (e.clientY - this.dragState_.initialY > PressHandler.DRAG_THRESHOLD_PX) {
       this.isLongPressed_ = true;
-      clearTimeout(this.longPressTimer_);
+      this.longPressTimer_.clearTimeout();
       this.onLongPress_(getContextMenuSourceType(e));
       this.resetDragState_();
     }
@@ -156,7 +157,7 @@ export class PressHandler {
     }
 
     this.isLongPressed_ = false;
-    clearTimeout(this.longPressTimer_);
+    this.longPressTimer_.clearTimeout();
 
     if (skipLongPress) {
       return;
@@ -175,7 +176,7 @@ export class PressHandler {
       target.addEventListener('pointermove', this.onPointermove_);
     }
 
-    this.longPressTimer_ = setTimeout(() => {
+    this.longPressTimer_.setTimeout(() => {
       // When the long press is triggered and handled, mark `isLongPressed_`
       // as true, so that it won't be treated as a normal click.
       this.isLongPressed_ = true;
@@ -204,12 +205,12 @@ export class PressHandler {
     // skip the rest.
     if (this.isLongPressed_ || isMacCtrlClick) {
       this.isLongPressed_ = false;
-      clearTimeout(this.longPressTimer_);
+      this.longPressTimer_.clearTimeout();
       this.resetDragState_();
       return;
     }
 
-    clearTimeout(this.longPressTimer_);
+    this.longPressTimer_.clearTimeout();
     const shouldCancel = this.shouldCancelClick_(e);
     this.resetDragState_();
 
@@ -228,7 +229,7 @@ export class PressHandler {
       return;
     }
     this.resetDragState_();
-    clearTimeout(this.longPressTimer_);
+    this.longPressTimer_.clearTimeout();
   };
 
   onContextmenu = (e: PointerEvent) => {
