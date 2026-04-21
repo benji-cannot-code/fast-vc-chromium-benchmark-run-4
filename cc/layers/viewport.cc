7b67e41e0a5e572c7c5edddfa0083331e985317b
Anand Ravi <anandrv@google.com>
2026-04-21 13:25:03 -0700

    [cc] Relax hide only once constraint on browser controls snap animation
    Currently, when browser controls show/hide through a snap animation,
    they can only hide once per scroll to prevent the animation from lagging
    too far behind the user's finger. The toolbar not hiding is noticeable
    when the user fling scrolls.
    
    Relax the constraint by allowing the toolbar to hide during inertial
    scrolls since that is also an indication that the user has lifted their
    finger from the screen and thus is unlikely to trigger a show animation
    before the hide animation finishes.
    
    Bug: 481743766
    Change-Id: I0d4b700e94fb23802199dceaf5fff03d6798ed31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7734986
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618401}

--- cc/layers/viewport.cc
+++ cc/layers/viewport.cc
@@ -43,7 +43,8 @@ Viewport::ScrollResult Viewport::ScrollBy(const gfx::Vector2dF& physical_delta,
                                           const gfx::Point& viewport_point,
                                           bool is_direct_manipulation,
                                           bool affect_browser_controls,
-                                          bool scroll_outer_viewport) {
+                                          bool scroll_outer_viewport,
+                                          bool is_inertial) {
   if (!OuterScrollNode())
     return ScrollResult();
 
@@ -51,7 +52,7 @@ Viewport::ScrollResult Viewport::ScrollBy(const gfx::Vector2dF& physical_delta,
 
   if (affect_browser_controls &&
       ShouldBrowserControlsConsumeScroll(physical_delta))
-    scroll_node_delta -= ScrollBrowserControls(physical_delta);
+    scroll_node_delta -= ScrollBrowserControls(physical_delta, is_inertial);
 
   gfx::Vector2dF pending_scroll_node_delta = scroll_node_delta;
 
@@ -121,7 +122,9 @@ void Viewport::SnapIfNeeded() {
   gfx::Vector2dF delta = snap.position - current_position;
   delta.Scale(host_impl_->active_tree()->page_scale_factor_for_scroll());
 
-  ScrollBy(delta, gfx::Point(), false, false, true);
+  ScrollBy(delta, /*viewport_point=*/gfx::Point(),
+           /*is_direct_manipulation=*/false, /*affect_browser_controls=*/false,
+           /*scroll_outer_viewport=*/true, /*is_inertial=*/false);
 }
 
 gfx::Vector2dF Viewport::ComputeClampedDelta(
@@ -330,9 +333,10 @@ bool Viewport::ShouldScroll(const ScrollNode& scroll_node) const {
          scroll_node.scrolls_outer_viewport;
 }
 
-gfx::Vector2dF Viewport::ScrollBrowserControls(const gfx::Vector2dF& delta) {
+gfx::Vector2dF Viewport::ScrollBrowserControls(const gfx::Vector2dF& delta,
+                                               bool is_inertial) {
   gfx::Vector2dF excess_delta =
-      host_impl_->browser_controls_manager()->ScrollBy(delta);
+      host_impl_->browser_controls_manager()->ScrollBy(delta, is_inertial);
 
   return delta - excess_delta;
 }
