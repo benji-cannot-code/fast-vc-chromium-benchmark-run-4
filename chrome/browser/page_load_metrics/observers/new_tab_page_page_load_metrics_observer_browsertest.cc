78ec0ff44c8fc8ab9a20854bcd23d81a72177c14
Antonio Sartori <antoniosartori@chromium.org>
2026-04-21 07:21:47 -0700

    Avoid continuously reparsing NTP url
    This CL is a refactoring which switches to using the GURL version of
    ChromeUINewTabURL and/or ChromeUINewPageURL in chrome/browser/page_load_metrics/observers, avoiding
    parsing GURL again and again.
    
    This CL was uploaded by git cl split.
    
    R=toyoshim@chromium.org
    
    Bug: 503370195
    Change-Id: I9b827cdcc3143c968046291a5d6d3a18b32cb9ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7776799
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618156}

--- chrome/browser/page_load_metrics/observers/new_tab_page_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/new_tab_page_page_load_metrics_observer_browsertest.cc
@@ -42,7 +42,7 @@ IN_PROC_BROWSER_TEST_F(NewTabPagePageLoadMetricsBrowserTest,
 
   // Navigate to New Tab Page.
   ASSERT_TRUE(content::NavigateToURL(GetActiveWebContents(),
-                                     GURL(chrome::kChromeUINewTabPageURL)));
+                                     chrome::ChromeUINewTabPageURLAsGURL()));
   metrics_waiter.Wait();
 
   // LCP is only collected at the end of page lifecycle. Navigate to flush.
