72e31c2d63e569c5c8c7c1e6ec78be2197a5d95b
Alex Valadkevich <valadkevich@google.com>
2026-04-21 05:24:11 -0700

    [DLP cancellation] Add UMA metrics for cancelled file scans
    This change introduces two new UMA histograms to track file scans that
    are cancelled before completion:
    -   `Enterprise.{AccessPoint}.Cancelled.BatchSize`: Records the number of files in the batch when a scan is cancelled.
    -   `Enterprise.{AccessPoint}.Cancelled.Duration`: Records the time elapsed from the start of the scan until it is cancelled.
    
    Bug: 399639311
    Change-Id: Ie09e640237badcd8d1b8ef586dbf7c8d9e8a0337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7775117
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Alex Valadkevich <valadkevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618078}

--- chrome/browser/enterprise/connectors/analysis/files_request_handler_unittest.cc
+++ chrome/browser/enterprise/connectors/analysis/files_request_handler_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "build/build_config.h"
@@ -368,6 +369,7 @@ class FilesRequestHandlerTest : public BaseTest {
         {safe_browsing::kEnhancedFieldsForSecOps,
          enterprise_connectors::kEnableCancelUploadOnContentAnalysis},
         {});
+    histogram_tester_ = std::make_unique<base::HistogramTester>();
   }
 
   void FakeFileUploadCallback(
@@ -444,6 +446,7 @@ class FilesRequestHandlerTest : public BaseTest {
   bool upload_performed_ = false;
 
  protected:
+  std::unique_ptr<base::HistogramTester> histogram_tester_;
   base::WeakPtrFactory<FilesRequestHandlerTest> weak_ptr_factory_{this};
 };
 
@@ -954,6 +957,11 @@ TEST_F(FilesRequestHandlerTest, DestructorReportsCancelled) {
   fake_files_request_handler_.reset();
 
   run_loop.Run();
+
+  histogram_tester_->ExpectUniqueSample(
+      "Enterprise.OnFileAttach.Cancelled.BatchSize", 2, 1);
+  histogram_tester_->ExpectTotalCount(
+      "Enterprise.OnFileAttach.Cancelled.Duration", 1);
 }
 
 TEST_F(FilesRequestHandlerTest, DestructorReportsCancelled_FileDeleted) {
@@ -1024,6 +1032,11 @@ TEST_F(FilesRequestHandlerTest, DestructorReportsCancelled_FileDeleted) {
   fake_files_request_handler_.reset();
 
   run_loop.Run();
+
+  histogram_tester_->ExpectUniqueSample(
+      "Enterprise.OnFileAttach.Cancelled.BatchSize", 2, 1);
+  histogram_tester_->ExpectTotalCount(
+      "Enterprise.OnFileAttach.Cancelled.Duration", 1);
 }
 
 TEST_F(FilesRequestHandlerTest, NoDelay) {
