530e529af430f19760334ee505c79f53ec833827
Radu Nitescu <radunitescu@google.com>
2026-04-21 05:19:34 -0700

    [iOS][AIM] Add attachment view to the bottom sheet picker
    https://screenshot.googleplex.com/4HTd6xNGGFyNx7i
    
    Change-Id: Idd0a5fe170c53f982a24a0b526609df21cb18a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779870
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618070}

---
+++ ios/chrome/browser/composebox/menu/ui/composebox_menu_attachment_view.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_COMPOSEBOX_MENU_UI_COMPOSEBOX_MENU_ATTACHMENT_VIEW_H_
+#define IOS_CHROME_BROWSER_COMPOSEBOX_MENU_UI_COMPOSEBOX_MENU_ATTACHMENT_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// Represents an individual attachment method in the attachment carrousel.
+@interface ComposeboxMenuAttachmentView : UIButton
+
+// The image that is displayed in the middle of this attachment view.
+@property(nonatomic, strong) UIImage* image;
+
+// The title of the attachment view.
+@property(nonatomic, copy) NSString* title;
+
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_COMPOSEBOX_MENU_UI_COMPOSEBOX_MENU_ATTACHMENT_VIEW_H_
