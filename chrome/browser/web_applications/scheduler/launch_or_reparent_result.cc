584e835d0befd35d7db29819dd4dffd98ab8e530
dmurph@chromium.org <dmurph@chromium.org>
2026-04-20 17:35:49 -0700

    [PWA] Add LaunchOrReparentWebContentsIntoAppCommand
    This CL adds the new command to reparent or launch a web app,
    which is part of decoupling installation and launch.
    
    Bug: 492656527
    Change-Id: I7f9a888dd65f4dd74e52f12c30d3ce0579c9a43e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779764
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Adriana Ixba <aixba@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617888}

---
+++ chrome/browser/web_applications/scheduler/launch_or_reparent_result.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/scheduler/launch_or_reparent_result.h"
+
+#include <ostream>
+
+namespace web_app {
+
+std::ostream& operator<<(std::ostream& os, LaunchOrReparentResult result) {
+  switch (result) {
+    case LaunchOrReparentResult::kReparented:
+      return os << "Reparented";
+    case LaunchOrReparentResult::kLaunched:
+      return os << "Launched";
+    case LaunchOrReparentResult::kWebContentsGone:
+      return os << "WebContentsGone";
+    case LaunchOrReparentResult::kAppNotInstalledAsDedicatedWindow:
+      return os << "AppNotInstalledAsDedicatedWindow";
+    case LaunchOrReparentResult::kShutdown:
+      return os << "Shutdown";
+  }
+}
+
+}  // namespace web_app
