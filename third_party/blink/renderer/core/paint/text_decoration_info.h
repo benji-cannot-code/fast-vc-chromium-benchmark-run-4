7cbd124ac99f5783b88414897de371ba8c41ff3b
Kent Tamura <tkent@chromium.org>
2026-04-21 02:08:09 -0700

    Add ResolvedDecoration::effective_zoom
    This removes `decorating_box_style_` dependency from some methods.
    
    Bug: 501752810
    Change-Id: I6f439f6148b6ca931af28d6b622e4e004f52036d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747288
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618049}

--- third_party/blink/renderer/core/paint/text_decoration_info.h
+++ third_party/blink/renderer/core/paint/text_decoration_info.h
@@ -58,6 +58,7 @@ struct ResolvedDecoration {
   float ascent = 0.f;
   float computed_font_size = 0.f;
   float resolved_thickness = 0.f;
+  float effective_zoom = 1.0f;
   // This field is available only if a decorating box is applied and `lines`
   // has underline.
   LayoutUnit offset_from_decorating_box;
@@ -66,8 +67,6 @@ struct ResolvedDecoration {
   bool has_underline = false;
   bool has_overline = false;
   bool is_flipped_underline_and_overline = false;
-
-  // TODO(crbug.com/501752810): Move more fields from TextDecorationInfo.
 
   bool HasUnderline() const { return has_underline; }
   bool HasOverline() const { return has_overline; }
