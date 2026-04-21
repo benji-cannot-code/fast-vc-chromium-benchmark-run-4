b90da9df62f747eaf803862e86ba32cfaf671e49
Scott Yoder <scottyoder@google.com>
2026-04-21 05:34:12 -0700

    [ios] Migrate Lens Overlay Snapshot to FullscreenRefactoring
    This CL migrates the Lens Overlay Snapshot fullscreen implementation to
    the new FullscreenRefactoring architecture, behind the
    FullscreenRefactoring flag.
    
    Bug: 500669310
    Change-Id: I8d9c72a13e260d17045fac34b498c1c80eafbd07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7775517
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618095}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_snapshot_controller.h
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_snapshot_controller.h
@@ -13,19 +13,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/task/sequenced_task_runner.h"
+#import "ios/chrome/browser/fullscreen/model/fullscreen_browser_agent_observer.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller_observer.h"
 #import "ios/chrome/browser/lens_overlay/model/lens_overlay_snapshot_controller_delegate.h"
 
 class SnapshotTabHelper;
 class FullscreenController;
+class FullscreenBrowserAgent;
+@protocol FullscreenCommands;
 enum class FullscreenAnimatorStyle : short;
 
 // Manages the flow of capturing the snapshot of a given base window.
 class LensOverlaySnapshotController final
-    : public FullscreenControllerObserver {
+    : public FullscreenControllerObserver,
+      public FullscreenBrowserAgentObserver {
  public:
   LensOverlaySnapshotController(SnapshotTabHelper* snapshot_tab_helper,
                                 FullscreenController* fullscreen_controller,
+                                FullscreenBrowserAgent* fullscreen_agent,
+                                id<FullscreenCommands> fullscreen_handler,
                                 UIWindow* window,
                                 bool is_bottom_omnibox);
 
@@ -72,6 +78,10 @@ class LensOverlaySnapshotController final
   void FullscreenDidAnimate(FullscreenController* controller,
                             FullscreenAnimatorStyle style) override;
 
+  // FullscreenBrowserAgentObserver:
+  void FullscreenDidTransition(FullscreenBrowserAgent* agent,
+                               FullscreenTransition transition) override;
+
   // Cover the viewport with static image of the base window.
   void ShowStaticSnapshotOfBaseWindowIfNeeded();
 
@@ -104,6 +114,8 @@ class LensOverlaySnapshotController final
 
   raw_ptr<SnapshotTabHelper, DanglingUntriaged> snapshot_tab_helper_ = nullptr;
   raw_ptr<FullscreenController> fullscreen_controller_ = nullptr;
+  raw_ptr<FullscreenBrowserAgent> fullscreen_agent_ = nullptr;
+  __weak id<FullscreenCommands> fullscreen_handler_ = nil;
   base::WeakPtr<LensOverlaySnapshotControllerDelegate> delegate_ = nullptr;
 
   base::CancelableTaskTracker task_tracker_;
