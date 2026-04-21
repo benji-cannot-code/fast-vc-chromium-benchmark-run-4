2b754b5a918f4d41adc4d74ace659444c3e50005
Matt Stark <msta@google.com>
2026-04-20 20:30:14 -0700

    Implement "unified modules"
    Unified modules attempt to calculate the transitive dependency graph of
    libc++ and dynamically generate a modulemap based on it.
    
    The theory says this should significantly improve remote execution performance, and the benchmarks line up with it.
    
    To compile the all_modules target:
    local execution: 11.1 seconds before, 10.8 seconds after
    remote fetch (100% cache hit rate): 41 seconds before, 19 seconds after
    remote execution: 90 seconds before, 37 seconds after
    
    This also has the very convenient advantage of being able to skip
    generating the `libcxx_headers` and `custom_headers` targets, as we no
    longer need them to be in the same directory.
    
    Bug: b:495559142
    Change-Id: I9cf7eb4af8dc642fde09f48e4760de766a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7695635
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Matt Stark <msta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1617963}

---
+++ build/modules/unified/testdata/libc++/subdir/missing.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BUILD_MODULES_UNIFIED_TESTDATA_LIBC___SUBDIR_MISSING_H_
+#define BUILD_MODULES_UNIFIED_TESTDATA_LIBC___SUBDIR_MISSING_H_
+
+#include <subdir/bits/missing.h>
+
+#endif  // BUILD_MODULES_UNIFIED_TESTDATA_LIBC___SUBDIR_MISSING_H_
