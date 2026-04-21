b948f44b32566aee1427a5ee8da9894e7ca447ac
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-04-21 05:53:26 -0700

    [cleanup] Remove individual margin accessors.
    Most accessors of the individual margin accessors use multiple of them,
    to convert them all to "MarginOutests".
    
    This patch is in preparation for accessing the margins directly off the
    fragments instead.
    
    There should be no behaviour change.
    
    Change-Id: Ifb183fc57e381a759f4a88ab06a16dedcf5c1dd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7478009
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618127}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -6522,7 +6522,7 @@ const CSSValue* MarginBottom::CSSValueFromComputedStyleInternal(
     CSSValuePhase value_phase) const {
   if (const LayoutBox* box = DynamicTo<LayoutBox>(layout_object)) {
     if (!style.MarginBottom().IsFixed()) {
-      return ZoomAdjustedPixelValue(box->MarginBottom(), style);
+      return ZoomAdjustedPixelValue(box->MarginOutsets().bottom, style);
     }
   }
   return ComputedStyleUtils::ZoomAdjustedPixelValueForLength(
@@ -6579,7 +6579,7 @@ const CSSValue* MarginLeft::CSSValueFromComputedStyleInternal(
     CSSValuePhase value_phase) const {
   if (const LayoutBox* box = DynamicTo<LayoutBox>(layout_object)) {
     if (!style.MarginLeft().IsFixed()) {
-      return ZoomAdjustedPixelValue(box->MarginLeft(), style);
+      return ZoomAdjustedPixelValue(box->MarginOutsets().left, style);
     }
   }
   return ComputedStyleUtils::ZoomAdjustedPixelValueForLength(style.MarginLeft(),
@@ -6608,7 +6608,7 @@ const CSSValue* MarginRight::CSSValueFromComputedStyleInternal(
     CSSValuePhase value_phase) const {
   if (const LayoutBox* box = DynamicTo<LayoutBox>(layout_object)) {
     if (!style.MarginRight().IsFixed()) {
-      return ZoomAdjustedPixelValue(box->MarginRight(), style);
+      return ZoomAdjustedPixelValue(box->MarginOutsets().right, style);
     }
   }
   return ComputedStyleUtils::ZoomAdjustedPixelValueForLength(
@@ -6637,7 +6637,7 @@ const CSSValue* MarginTop::CSSValueFromComputedStyleInternal(
     CSSValuePhase value_phase) const {
   if (const LayoutBox* box = DynamicTo<LayoutBox>(layout_object)) {
     if (!style.MarginTop().IsFixed()) {
-      return ZoomAdjustedPixelValue(box->MarginTop(), style);
+      return ZoomAdjustedPixelValue(box->MarginOutsets().top, style);
     }
   }
   return ComputedStyleUtils::ZoomAdjustedPixelValueForLength(style.MarginTop(),
