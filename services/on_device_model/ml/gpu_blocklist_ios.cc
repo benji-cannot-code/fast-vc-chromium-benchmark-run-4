65bdbb296f2f51a2242fa8219ac428e9c5ee0aa3
Nathan Memmott <memmott@chromium.org>
2026-04-21 15:59:03 -0700

    ODMS: Fix QueryDeviceInfo in gpu_blocklist_ios.cc
    This was recently updated in https://crrev.com/c/7735006 to take
    ChromeML instead of ChromeMLAPI.
    
    Bug: 489761125
    Change-Id: Iec2a72d4b617045e85a7f7b2ae48102050d87358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780249
    Reviewed-by: Jingyun Liu <jingyun@google.com>
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Auto-Submit: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618519}

--- services/on_device_model/ml/gpu_blocklist_ios.cc
+++ services/on_device_model/ml/gpu_blocklist_ios.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ml {
 
 COMPONENT_EXPORT(ON_DEVICE_MODEL_ML)
-DeviceInfo QueryDeviceInfo(const ChromeMLAPI& api, bool log_histogram) {
+DeviceInfo QueryDeviceInfo(const ChromeML& api, bool log_histogram) {
   DeviceInfo query_device_info;
   query_device_info.gpu_blocked_reason = GpuBlockedReason::kNotBlocked;
   return query_device_info;
