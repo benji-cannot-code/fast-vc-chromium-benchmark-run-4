06575e109774c35d37a84396503d091cabca7009
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2026-04-21 15:39:51 -0700

    Import wpt@86e05bfda87c0146be40384d09c0aa946713f1a4
    https://github.com/web-platform-tests/wpt/compare/3c6a8f59a...86e05bfda
    
    Using wpt-import in Chromium 0570c31c9ddb3e4bd48f95915371418b14878c4b.
    With Chromium commits locally applied on WPT:
      3b1c1a8d4c "Allow WebTransport to enter BackForwardCache"
      06ca230cae "Include url and eval hashes in CSP violation reports [...]
      e91545ca0f "Two-phase view transition"
      a9f55c6e86 "Fix WPT version of scroll-timeilne-snapshotting test"
      0bd7eed851 "[align] Disable anchor-center on align- [...]
      34d625da61 "[css-shapes] Implement polygon() round modifier"
      eacb0481a6 "Fix scroll-timeline-inactive.html"
      ecd37c5df7 "webrtc wpt: update insertable streams metadata tests"
      092175216a "Propagate attr() taint ranges when resolving shorthands"
      7ed6b2f1c8 "Implement new/revised popover=hint behavior"
      2cf839a799 "[CC Clip Paths] Invalidate correct element after [...]
      1165ff98d3 "Fix a bug in new popover=hint stacking behavior, and [...]
      6c745acc7f "Activation across navigation: add a WPT for cross- [...]
      43550d9e23 "Make view-timeline-inset viewport-size invariant"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/fullscreen
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-arm64-blink-rel
    Change-Id: I233d6553395e56eaaf02217e9556d93f940ae900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783352
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618504}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/network/before_request_sent/before_request_sent.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/network/before_request_sent/before_request_sent.py
@@ -3,6 +3,7 @@ import asyncio
 from urllib.parse import urlencode
 
 import pytest
+import pytest_asyncio
 
 from webdriver.bidi.modules.script import ContextTarget
 
@@ -21,6 +22,11 @@ from .. import (
 )
 
 pytestmark = pytest.mark.asyncio
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def delete_cookies(bidi_session):
+    await bidi_session.storage.delete_cookies()
 
 
 async def test_subscribe_status(bidi_session, subscribe_events, top_context, wait_for_event, wait_for_future_safe, url, fetch):
@@ -244,7 +250,6 @@ async def test_request_cookies(
                 "httpOnly": False,
                 "name": "foo",
                 "path": "/",
-                "sameSite": "default",
                 "secure": False,
                 "size": 6,
                 "value": {"type": "string", "value": "bar"},
@@ -275,7 +280,6 @@ async def test_request_cookies(
                 "httpOnly": False,
                 "name": "foo",
                 "path": "/",
-                "sameSite": "default",
                 "secure": False,
                 "size": 6,
                 "value": {"type": "string", "value": "bar"},
@@ -284,7 +288,6 @@ async def test_request_cookies(
                 "httpOnly": False,
                 "name": "fuu",
                 "path": "/",
-                "sameSite": "default",
                 "secure": False,
                 "size": 6,
                 "value": {"type": "string", "value": "baz"},
@@ -297,8 +300,6 @@ async def test_request_cookies(
         events[1],
         expected_event={"request": expected_request, "redirectCount": 0},
     )
-
-    await bidi_session.storage.delete_cookies()
 
 
 async def test_request_cookie_same_name_different_host(
@@ -352,7 +353,6 @@ async def test_request_cookie_same_name_different_host(
                 "httpOnly": False,
                 "name": "foo",
                 "path": "/webdriver/tests/bidi/network/support",
-                "sameSite": "default",
                 "secure": False,
                 "size": 6,
                 "value": {"type": "string", "value": "bar"},
@@ -365,8 +365,6 @@ async def test_request_cookie_same_name_different_host(
         events[0],
         expected_event={"request": expected_request, "redirectCount": 0},
     )
-
-    await bidi_session.storage.delete_cookies()
 
 
 async def test_request_timing_info(
