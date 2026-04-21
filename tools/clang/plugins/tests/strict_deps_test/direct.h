1a3342d5fbab7dc8bfe314763133dce1ea1dc80b
Matt Stark <msta@google.com>
2026-04-20 23:36:33 -0700

    Create a test to validate strict_deps.
    This test will later be extended with a strict deps plugin that gives
    hints to solve these errors.
    
    Design: https://docs.google.com/document/d/1IUs1VcswbE15n9qQsUAhyixPeHt68GdWhqkuQPE4by8/edit?resourcekey=0-r3yEOZ7dgxBhSAn_XjGIfA&tab=t.0
    
    Bug: 500845363
    Link: https://chromium-review.googlesource.com/id/I39139c6fb1a27804018f90baf575586f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7775774
    Commit-Queue: Matt Stark <msta@google.com>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618004}

---
+++ tools/clang/plugins/tests/strict_deps_test/direct.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_CLANG_PLUGINS_TESTS_STRICT_DEPS_TEST_DIRECT_H_
+#define TOOLS_CLANG_PLUGINS_TESTS_STRICT_DEPS_TEST_DIRECT_H_
+
+#include "impl_dep.h"           // OK (it should only verify the main module).
+#include "undeclared_header.h"  // OK (it should only verify the main module)
+
+#endif  // TOOLS_CLANG_PLUGINS_TESTS_STRICT_DEPS_TEST_DIRECT_H_
---
+++ tools/clang/plugins/tests/strict_deps_test/indirect.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_CLANG_PLUGINS_TESTS_STRICT_DEPS_TEST_INDIRECT_H_
+#define TOOLS_CLANG_PLUGINS_TESTS_STRICT_DEPS_TEST_INDIRECT_H_
+
+#endif  // TOOLS_CLANG_PLUGINS_TESTS_STRICT_DEPS_TEST_INDIRECT_H_
