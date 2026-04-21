3e9ca43997d30c9e5cad8c64abf573d619b9f760
Ruoxi He <ruoxih@google.com>
2026-04-21 14:51:28 -0700

    [Composebox] Define CSS variables for composebox(realbox) submit button styling.
    Adds variables to customize the dimensions, border-radius, icon
    size, and background color of the composebox submit button.
    
    Manual Testing: http://screenshot/8vTgpkTCaMvUpvP
    
    Fixed: b:500758370
    Change-Id: Ib8c65ab5bff832b171310583d4ba255ce3428d8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7760154
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Ruoxi He <ruoxih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618460}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -271,6 +271,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageThreadsRailIconButton) \
   E_CPONLY(kColorNewTabPageChipBackground) \
   E_CPONLY(kColorNewTabPageChipForeground) \
+  E_CPONLY(kColorNewTabPageComposeboxSubmitButtonBackground) \
   E_CPONLY(kColorComposeboxBackground) \
   E_CPONLY(kColorComposeboxFileChipSpinner) \
   E_CPONLY(kColorComposeboxFont) \
