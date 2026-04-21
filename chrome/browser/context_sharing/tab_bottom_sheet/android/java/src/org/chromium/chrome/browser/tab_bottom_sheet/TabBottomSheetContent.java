e98778091161de2f886e3a0edfae557d4faf3f52
Wenyu Fu <wenyufu@chromium.org>
2026-04-21 16:24:52 -0700

    [TabBottomSheet] Fix Actor Peek View background and scrolling
    Fixes UI regressions in the Actor Peek View introduced in a recent
    refactor.
    
    1. Background Color: The peek view background was defaulting to white in
    dark mode due to layout XML setting. This change removes the background
    from XML and applies the dynamic background color in the java
    constructor of TabBottomSheetContent. NullUtil.assertNonNull is used as
    requested.
    
    2. Scrolling: The expanded content was set to View.GONE in peek state,
    removing it from layout hierarchy and causing BottomSheetController to
    miscalculate scrollable height. This change switches it (and the peek
    container in expanded state) to View.INVISIBLE to preserve layout
    height.
    
    Preview: https://screencast.googleplex.com/cast/NTI0NDI2OTM0NTM3NDIwOHw4YzFjMDI0MC1hNw
    
    Bug: 505049486
    Change-Id: I93afc5fada2a53d1abe566ee623ccfdcd3add21b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783665
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618538}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetContent.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetContent.java
@@ -13,6 +13,7 @@ import androidx.annotation.StringRes;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.build.NullUtil;
 import org.chromium.chrome.browser.context_sharing.R;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
@@ -55,6 +56,10 @@ public class TabBottomSheetContent implements BottomSheetContent {
                 mContentView
                         .getResources()
                         .getDimensionPixelSize(R.dimen.tab_bottom_sheet_peek_height_total);
+
+        View view = mContentView.findViewById(R.id.actor_control_container);
+        View peekContainer = NullUtil.assertNonNull(view);
+        peekContainer.setBackgroundColor(mBackgroundColor);
     }
 
     @Override
