27236fa826d13c6b963e97b90a857fdd312c9a2f
Jeffrey Yu <yuje@chromium.org>
2026-04-20 19:36:00 -0700

    Use std::u16string_view in TokenizedString to avoid copies.
    Replaces usages of std::u16string copies with std::u16string_view where
    possible, such as when getting the string from BreakIterator and when
    taking substrings of the main text.
    
    Bug: 501076278
    Change-Id: I60aaf63979a0d3f7d0604c66b5c020aabcf58d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771228
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617941}

--- chromeos/ash/components/string_matching/tokenized_string.cc
+++ chromeos/ash/components/string_matching/tokenized_string.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "base/i18n/break_iterator.h"
@@ -83,13 +84,13 @@ void TokenizedString::TokenizeWords() {
     // 1. Whitespace chars only: generate a token from |text_| in the range of
     //    [start, end). Also reset |start| and |end| for next token.
     // 2. A punctuation: do nothing and Advance.
-    const std::u16string word(break_iter.GetString());
+    const std::u16string_view word = break_iter.GetString();
     const bool only_whitechars =
         base::ContainsOnlyChars(word, base::kWhitespaceUTF16);
     if (only_whitechars) {
       if (end - start > 1) {
-        tokens_.emplace_back(
-            base::i18n::ToLower(text_.substr(start, end - start)));
+        tokens_.emplace_back(base::i18n::ToLower(
+            std::u16string_view(text_).substr(start, end - start)));
         mappings_.emplace_back(start, end);
       }
       start = break_iter.pos();
@@ -99,7 +100,8 @@ void TokenizedString::TokenizeWords() {
 
   // Generate the last token.
   if (end - start >= 1) {
-    tokens_.emplace_back(base::i18n::ToLower(text_.substr(start, end - start)));
+    tokens_.emplace_back(base::i18n::ToLower(
+        std::u16string_view(text_).substr(start, end - start)));
     mappings_.emplace_back(start, end);
   }
 }
