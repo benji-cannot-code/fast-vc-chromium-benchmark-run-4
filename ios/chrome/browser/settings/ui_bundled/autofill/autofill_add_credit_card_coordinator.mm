80a62a97b4417ae97d4bbcc5f084d3bffe8cde96
Sourav <sinhasourav@google.com>
2026-04-22 12:44:19 -0700

    [iOS] Fix race condition and missing cleanup in Payment Methods
    This CL addresses a bug where the cancel button or UI could become
    unresponsive when adding a payment method, specifically when using
    multi-touch or rapid tapping while closing the view.
    
    Before: https://drive.google.com/file/d/1RFoJePeHqhH1WDA2J7Lhl-wjWNr7oleG/view?usp=sharing
    After: https://drive.google.com/file/d/1bwzMnVJhTL84rZQIRpnR_A6UWCn-3Crz/view?usp=sharing
    Bug: 484415286
    Change-Id: Ica5068bc9286ed432a116d354ecd7c855888f7a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780723
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619019}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_add_credit_card_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_add_credit_card_coordinator.mm
@@ -128,6 +128,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self showActionSheetAlert];
 }
 
+- (void)presentationControllerDidDismiss:
+    (UIPresentationController*)presentationController {
+  [self.delegate autofillAddCreditCardCoordinatorWantsToBeStopped:self];
+}
+
 #pragma mark - Helper Methods
 
 - (void)showActionSheetAlert {
