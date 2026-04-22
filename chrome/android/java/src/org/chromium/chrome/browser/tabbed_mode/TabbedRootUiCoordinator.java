008de1d992ae64d9598ee97521ca1c51f66f3f64
Shakti Sahu <shaktisahu@chromium.org>
2026-04-21 22:09:31 -0700

    Refactor ContextualTasksFuseboxManager to an Interface
    This CL refactors the ContextualTasksFuseboxManager from a
    concrete class into an interface-implementation pattern
    in order to resolve circular dependencies and improve
    testability of the fusebox management logic.
    
    Key changes:
     - Converted ContextualTasksFuseboxManager into a public
     interface located in the fusebox subdirectory, the
     implementation ContextualTasksFuseboxManagerImpl
     exists in the android directory that contains the
     concrete logic and JNI plumbing.
     - Added JUnit tests for the ContextualTasksFuseboxManagerImpl.
    
    Bug: b:493281303
    Change-Id: I4f188d82f17d43786678efe3d9ddf8c57c6e1750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7776472
    Reviewed-by: Kevin Shah <kevinshah@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618659}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -85,6 +85,7 @@ import org.chromium.chrome.browser.compositor.overlays.strip.StripLayoutHelperMa
 import org.chromium.chrome.browser.contextmenu.ChromeContextMenuPopulator;
 import org.chromium.chrome.browser.contextmenu.ChromeContextMenuPopulatorFactory;
 import org.chromium.chrome.browser.contextual_tasks.ContextualTasksBridge;
+import org.chromium.chrome.browser.contextual_tasks.ContextualTasksFuseboxManagerImpl;
 import org.chromium.chrome.browser.contextual_tasks.fusebox.ContextualTasksFusebox.ContextualTasksFuseboxConfig;
 import org.chromium.chrome.browser.contextual_tasks.fusebox.ContextualTasksFuseboxManager;
 import org.chromium.chrome.browser.crash.ChromePureJavaExceptionReporter;
@@ -1303,7 +1304,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
 
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.CONTEXTUAL_TASKS)) {
             mContextualTasksFuseboxManager =
-                    new ContextualTasksFuseboxManager(
+                    new ContextualTasksFuseboxManagerImpl(
                             mActivity,
                             () -> {
                                 ViewStub stub =
