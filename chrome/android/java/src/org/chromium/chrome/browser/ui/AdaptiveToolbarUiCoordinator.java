3f016ee3a8692c6b750529f960247b918f57e3f9
Hailey Wang <haileywang@google.com>
2026-04-21 18:45:35 -0700

    [Glic] Observe when Chrome resume on MTB
    When returning from PiP mode, the pinned adaptive toolbar button stays
    hidden because lifecycle resume processes are not observed to update the
    stale hidden limitations. Adding the observation here.
    
    Bug: 500440962
    Change-Id: Ifa52e736de8cb508e1b3172d0d005b9775ed0a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7769313
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618606}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/AdaptiveToolbarUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/AdaptiveToolbarUiCoordinator.java
@@ -9,6 +9,7 @@ import static org.chromium.build.NullUtil.assertNonNull;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 
 import androidx.appcompat.content.res.AppCompatResources;
 
@@ -134,7 +135,8 @@ public class AdaptiveToolbarUiCoordinator {
             DeviceLockActivityLauncher deviceLockActivityLauncher,
             Supplier<@Nullable Tracker> trackerSupplier,
             Supplier<ScrimManager> scrimSupplier,
-            Supplier<@Nullable ReaderModeIphController> readerModeIphControllerSupplier) {
+            Supplier<@Nullable ReaderModeIphController> readerModeIphControllerSupplier,
+            View toolbarContainer) {
         if (!toolbarBehavior.shouldInitialize()) return;
 
         mBottomSheetController = bottomSheetController;
@@ -207,7 +209,8 @@ public class AdaptiveToolbarUiCoordinator {
                         profileSupplier,
                         new AdaptiveButtonActionMenuCoordinator(toolbarBehavior.canShowSettings()),
                         toolbarBehavior,
-                        windowAndroid);
+                        windowAndroid,
+                        toolbarContainer);
 
         DiscountsButtonController discountsButtonController =
                 new DiscountsButtonController(
--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -1919,7 +1919,8 @@ public class RootUiCoordinator
                     assertNonNull(mDeviceLockActivityLauncherSupplier.get()),
                     trackerSupplier,
                     mScrimManagerSupplier.asNonNull(),
-                    mReaderModeIphControllerSupplier);
+                    mReaderModeIphControllerSupplier,
+                    mToolbarContainer);
 
             var omniboxActionDelegate =
                     new OmniboxActionDelegateImpl(
