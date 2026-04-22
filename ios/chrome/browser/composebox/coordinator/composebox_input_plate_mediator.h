04004bac8b4a81810b34036df7bbcbafbdc80682
christianxu <christianxu@chromium.org>
2026-04-22 02:46:13 -0700

    [iOS][Composebox] Add input state manager
    Add an object to manager the composebox input state. This CL moves input
    state model ownership and input state observation to the class.
    Following CLs will move the rest of input state handling to that class.
    
    This CL also fixes a bug in the preselection logic during searchbox
    config reloads. Previously, the mediator validated the preselected
    tool/model against its cached `_inputState`, which had not yet been
    updated with the new configuration. The new
    `ComposeboxInputStateManager` correctly validates the preselection
    against the newly initialized state, ensuring that only valid tools and
    models are preserved.
    
    Bug: 502851687
    Change-Id: Iac7e2baa43c5c262e45d859973a71339fd0cab06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777852
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618742}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.h
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/contextual_search/internal/ios/composebox_context_upload_observer_bridge.h"
 #import "components/contextual_tasks/public/query_contextualizer.h"
+#import "ios/chrome/browser/composebox/coordinator/composebox_input_state_manager.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_mode_holder.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_omnibox_client_delegate.h"
 #import "ios/chrome/browser/composebox/public/composebox_entrypoint.h"
@@ -61,6 +62,7 @@ class ContextualSearchSessionHandle;
                 ComposeboxModeObserver,
                 TabPickerSelectionDelegate,
                 TextFieldViewContainingHeightDelegate,
+                ComposeboxInputStateManagerDelegate,
                 VoiceSearchDelegate>
 
 // The composebox input plate consumer.
