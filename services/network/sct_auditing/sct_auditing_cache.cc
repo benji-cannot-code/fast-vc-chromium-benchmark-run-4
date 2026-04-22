1e7fbada5c474db6a91340187d61b265e9ceacb6
Elly <ellyjones@chromium.org>
2026-04-22 16:42:30 -0700

    services/network: stop writing into net::HashValue instances
    Instead, construct them directly from a computed hash value. This is
    part of making net::HashValue instances immutable, and in making
    net::HashValue support actually changing hash types. See the linked bug
    for more details.
    
    AI-Model: antigravity
    Bug: 406566438
    Change-Id: Id62d89ed990eac35f06ced24d28b3843b9c336c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782726
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619190}

--- services/network/sct_auditing/sct_auditing_cache.cc
+++ services/network/sct_auditing/sct_auditing_cache.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/time/time.h"
 #include "components/version_info/version_info.h"
+#include "crypto/hash.h"
 #include "crypto/secure_hash.h"
 #include "crypto/sha2.h"
 #include "net/base/hash_value.h"
@@ -27,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/proto/sct_audit_report.pb.h"
 #include "services/network/sct_auditing/sct_auditing_handler.h"
 #include "third_party/boringssl/src/include/openssl/pool.h"
-#include "third_party/boringssl/src/include/openssl/sha.h"
 
 namespace network {
 
@@ -103,8 +103,7 @@ SCTAuditingCache::MaybeGenerateReportEntry(
   // SCTs is used as the cache key to deduplicate reports with the same SCTs.
   // Constructing the report in parallel with computing the hash avoids
   // encoding the SCTs multiple times and avoids extra copies.
-  SHA256_CTX ctx;
-  SHA256_Init(&ctx);
+  crypto::hash::Hasher hasher(crypto::hash::kSha256);
   for (const auto& sct : signed_certificate_timestamps) {
     auto* sct_source_and_status = tls_report->add_included_sct();
     // TODO(crbug.com/40692154): Update the proto to remove the status entirely
@@ -116,15 +115,15 @@ SCTAuditingCache::MaybeGenerateReportEntry(
     net::ct::EncodeSignedCertificateTimestamp(
         sct.sct, sct_source_and_status->mutable_serialized_sct());
 
-    SHA256_Update(&ctx, sct_source_and_status->serialized_sct().data(),
-                  sct_source_and_status->serialized_sct().size());
+    hasher.Update(sct_source_and_status->serialized_sct());
   }
   // Don't handle reports if there were no valid SCTs.
   if (tls_report->included_sct().empty())
     return std::nullopt;
 
-  net::HashValue cache_key(net::HASH_VALUE_SHA256);
-  SHA256_Final(cache_key.span().data(), &ctx);
+  net::SHA256HashValue hash;
+  hasher.Finish(hash);
+  net::HashValue cache_key(net::HASH_VALUE_SHA256, hash);
 
   // Check if the SCTs are already in the cache. This will update the last seen
   // time if they are present in the cache.
