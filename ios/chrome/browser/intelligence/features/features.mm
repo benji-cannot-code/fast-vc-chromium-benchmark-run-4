09b64ac1ca38f8caee4862419c1680c424130c97
Kirubel <kaklilu@chromium.org>
2026-04-21 18:01:27 -0700

    Add missing kDisabledTools feature param for kActorTools feature.
    Change-Id: If3fb511e9d2ce8ac49ceb47272022e706a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780893
    Commit-Queue: Kirubel <kaklilu@chromium.org>
    Reviewed-by: Fikre Mengistu <fikremengistu@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Kirubel <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618586}

--- ios/chrome/browser/intelligence/features/features.mm
+++ ios/chrome/browser/intelligence/features/features.mm
@@ -565,6 +565,12 @@ bool IsGeminiDynamicSettingsEnabled() {
 
 BASE_FEATURE(kActorTools, base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE_PARAM(std::string,
+                   kDisabledTools,
+                   &kActorTools,
+                   "DisabledTools",
+                   "");
+
 bool IsActorEnabled() {
   return base::FeatureList::IsEnabled(kActorTools);
 }
@@ -580,8 +586,7 @@ bool IsToolDisabled(optimization_guide::proto::Action::ActionCase tool) {
     return true;
   }
 
-  std::string disabled_tools =
-      base::GetFieldTrialParamValueByFeature(kActorTools, "DisabledTools");
+  std::string disabled_tools = kDisabledTools.Get();
   if (disabled_tools.empty()) {
     return false;
   }
