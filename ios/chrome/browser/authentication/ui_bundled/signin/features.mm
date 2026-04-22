37a780e46cd0840b6f3757e564149b798100b604
Cheick Cisse <cheickcisse@google.com>
2026-04-22 07:33:49 -0700

    [iOS] Enable Fullscreen Signin Promo Manager Migration by default
    This CL flips the default state of the
    kFullscreenSigninPromoManagerMigration feature flag from disabled to
    enabled.
    
    Change-Id: Ie20c9387cd4e087d5e9a90ed2e079f6be3305da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782052
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618858}

--- ios/chrome/browser/authentication/ui_bundled/signin/features.mm
+++ ios/chrome/browser/authentication/ui_bundled/signin/features.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/ui_bundled/signin/features.h"
 
 BASE_FEATURE(kFullscreenSigninPromoManagerMigration,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsFullscreenSigninPromoManagerMigrationEnabled() {
   return base::FeatureList::IsEnabled(kFullscreenSigninPromoManagerMigration);
