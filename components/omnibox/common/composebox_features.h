44c580b5f886fc89f653ba0b91fb33f98ca9aabb
Ananya Seelam <ananyaseelam@google.com>
2026-04-22 15:43:09 -0700

    [context-input] Add feature for tab flyover
    This CL adds a file for composebox related features and adds a new
    feature for "Add Tabs" flyover feature. This file will contain future
    "+" menu overhaul features as well as other composebox features.
    
    Bug: b:505381339
    Change-Id: I1bf12576d2f7d30eb32c729c2dc1de1e4e9bccaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7786372
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619149}

---
+++ components/omnibox/common/composebox_features.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OMNIBOX_COMMON_COMPOSEBOX_FEATURES_H_
+#define COMPONENTS_OMNIBOX_COMMON_COMPOSEBOX_FEATURES_H_
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace omnibox {
+
+// If enabled, the Composebox Context Menu will show the current/most recent tab
+// in the context menu and below it, an "Add tabs" menu item which, when
+// clicked, shows the users current tab in a flyover.
+BASE_DECLARE_FEATURE(kContextMenuTabFlyover);
+
+}  // namespace omnibox
+
+#endif  // COMPONENTS_OMNIBOX_COMMON_COMPOSEBOX_FEATURES_H_
