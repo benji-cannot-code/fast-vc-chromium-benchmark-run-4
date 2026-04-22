4855bab4937dfcecac95762018233561ee17fb98
Reka Norman <rekanorman@google.com>
2026-04-21 22:16:23 -0700

    Enable VideoConferenceMatcher on android desktop as well
    Currently VideoConferenceMatcher is only enabled on desktop platforms,
    and not enabled on android. Desktop android platforms can benefit from
    it too, so use GetDeviceFormFactor() to determine whether to enable it.
    
    Bug: b:498770079
    Test: Record a trace on android desktop and check the matcher is enabled
    Change-Id: I0c6cc7bdd1a3cc4771d2ff247dfce786df8d88ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7760788
    Auto-Submit: Reka Norman <rekanorman@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Reka Norman <rekanorman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618661}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkMallocPixelRef.h"
 #include "ui/android/window_android.h"
+#include "ui/base/device_form_factor.h"
 #include "ui/display/display.h"
 #include "ui/display/display_transform.h"
 #include "ui/display/screen.h"
@@ -794,6 +795,10 @@ void CompositorImpl::InitializeVizLayerTreeFrameSink(
   if (!is_offscreen_rendering_ &&
       input::InputUtils::IsTransferInputToVizSupported()) {
     root_params->create_input_receiver = true;
+  }
+
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_DESKTOP) {
+    root_params->enable_video_conference_matcher = true;
   }
 
   GetHostFrameSinkManager()->CreateRootCompositorFrameSink(
