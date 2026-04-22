aa79ad0184083f4e74cbece46103ed42f7864202
Alison Gale <agale@chromium.org>
2026-04-22 14:49:35 -0700

    [Tab Search] Fix how domains with numbers are rendered
    Directionality of the hostname text is reversed (direction: rtl) to
    elide from the front. A side effect of this is that neutral characters
    like numbers and dots get reordered. So a domain like 111-google.com
    gets rendered as google.com-111. Wrapping the domain in the <bdi> tag
    will ensure the rtl doesn't propagate to the inner text while allowing
    the elliding from the front.
    
    https://screenshot.googleplex.com/GPV72D3f2RPQJDZ
    
    Bug: 504594425
    Change-Id: I62829d9020bef4f22f5b89d3a50903d5bf33f1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783227
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619106}

--- chrome/browser/resources/tab_search/tab_search_item.ts
+++ chrome/browser/resources/tab_search/tab_search_item.ts
@@ -48,6 +48,7 @@ export interface TabSearchItemElement {
   $: {
     primaryText: HTMLElement,
     secondaryText: HTMLElement,
+    secondaryTextInner: HTMLElement,
   };
 }
 
@@ -249,7 +250,7 @@ export class TabSearchItemElement extends TabSearchItemBase {
     const data = this.data;
     ([
       ['tab.title', this.$.primaryText],
-      ['hostname', this.$.secondaryText],
+      ['hostname', this.$.secondaryTextInner],
       ['tabGroup.title', this.shadowRoot.querySelector('#groupTitle')],
     ] as Array<[string, HTMLElement | null]>)
         .forEach(([path, element]) => {
@@ -263,7 +264,7 @@ export class TabSearchItemElement extends TabSearchItemBase {
     // Show chrome:// if it's a chrome internal url
     const protocol = new URL(normalizeURL(data.tab.url)).protocol;
     if (protocol === 'chrome:') {
-      this.$.secondaryText.prepend(document.createTextNode('chrome://'));
+      this.$.secondaryTextInner.prepend(document.createTextNode('chrome://'));
     }
   }
 
