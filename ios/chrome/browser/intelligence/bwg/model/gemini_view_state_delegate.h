06177a9b36866e3d9af0bd158c46716c4c9b5c2c
Tarek Makkouk <tarekmakkouk@google.com>
2026-04-22 13:07:35 -0700

    [iOS] Add didUpdateProcessingStatus to Gemini session delegate
    Bug: 504596190, 504752862
    Change-Id: If4d4ea2f32fb86700b05afcc6bda6c929a177c4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7786274
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619033}

--- ios/chrome/browser/intelligence/bwg/model/gemini_view_state_delegate.h
+++ ios/chrome/browser/intelligence/bwg/model/gemini_view_state_delegate.h
@@ -19,6 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Switch to `viewState`.
 - (void)switchToViewState:(ios::provider::GeminiViewState)viewState;
 
+// Called when the processing status changes.
+- (void)didUpdateProcessingStatus:
+            (ios::provider::GeminiClientMode)processingStatus
+                        sessionID:(NSString*)sessionID
+                   conversationID:(NSString*)conversationID;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_INTELLIGENCE_BWG_MODEL_GEMINI_VIEW_STATE_DELEGATE_H_
