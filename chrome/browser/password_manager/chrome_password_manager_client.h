b7bafba86cf8036b50bf4f94816e8c544755deca
Anna Tsvirchkova <atsvirchkova@google.com>
2026-04-22 05:44:41 -0700

    [APC] Notify  password manager about nonFedCM logins
    Introduced OnNonFedCmFederatedLogin method in ChromePasswordManagerClient and triggers it in LoginDetectionTabHelper.
    
    Bug: 498593355
    Change-Id: Ic533a847494e1f33f1791d5fc6396ae85a9d832d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772668
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618800}

--- chrome/browser/password_manager/chrome_password_manager_client.h
+++ chrome/browser/password_manager/chrome_password_manager_client.h
@@ -419,6 +419,8 @@ class ChromePasswordManagerClient
     apply_client_side_prediction_override_ = true;
   }
 
+  void OnNonFedCmFederatedLogin();
+
  protected:
   // Callable for tests.
   explicit ChromePasswordManagerClient(content::WebContents* web_contents);
@@ -515,6 +517,8 @@ class ChromePasswordManagerClient
   void PropagatePredictionsToPasswordManager(autofill::AutofillManager& manager,
                                              autofill::FormGlobalId form_id,
                                              FieldTypeSource source);
+
+  void OnNonPasswordLoginDetected();
 
   password_manager::PasswordManager password_manager_;
   password_manager::PasswordFeatureManagerImpl password_feature_manager_;
