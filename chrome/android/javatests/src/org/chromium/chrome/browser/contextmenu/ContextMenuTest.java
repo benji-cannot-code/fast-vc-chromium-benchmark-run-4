4b82b0cdd73ac3cb5dd64cd7bf79d1f32def414d
Grace Cham <hscham@google.com>
2026-04-22 11:25:04 -0700

    Disable ContextMenuTest#test*PageLongPress on tablet and automotive
    The switch to desktop UA at setup is taking a long time but manifests
    as timeout at the openContextMenu step.
    Disable the test on tablet and automotive for now (but keep it on
    desktop builds) until the problem is fixed.
    
    Bug: 500650152, 504680967, 502983881
    Change-Id: Ib5f1aad2d77f0135eab36e2349173f4e0053d2ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7784856
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Owners-Override: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618982}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -1312,7 +1312,10 @@ public class ContextMenuTest {
 
     @Test
     @SmallTest
-    @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
+    @Restriction({
+        DeviceFormFactor.DESKTOP, // Re-enable on tablets once the crbug.com/500650152 is fixed.
+        DeviceRestriction.RESTRICTION_TYPE_NON_AUTO // crbug.com/502983881
+    })
     @EnableFeatures({ChromeFeatureList.CONTEXT_MENU_EMPTY_SPACE})
     @DisableFeatures({UiAndroidFeatures.ANDROID_WINDOW_OCCLUSION})
     public void testSharePageLongPress() throws Exception {
@@ -1394,7 +1397,7 @@ public class ContextMenuTest {
     @Test
     @MediumTest
     @Restriction({
-        DeviceFormFactor.TABLET_OR_DESKTOP,
+        DeviceFormFactor.DESKTOP, // Re-enable on tablets once the crbug.com/500650152 is fixed.
         // Re-enable on automotive once crbug.com/504680967 is fixed
         DeviceRestriction.RESTRICTION_TYPE_NON_AUTO
     })
