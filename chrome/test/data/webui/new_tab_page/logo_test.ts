a795c2cf9430250dbefd5e8c78444f435fc1ed51
Gianni Chen <giannich@chromium.org>
2026-04-21 17:17:48 -0700

    [ntp-doodles] Create Animated Doodles Fieldtrial Config
    Create the fieldtrial configs for Animated Doodles. Since this enables
    the feature for the browser tests, the expected values for the logo_test
    were updated as well.
    
    Bug: https://g-issues.chromium.org/issues/504723179
    Change-Id: Icf257f4df7a8a5275aa23d248d8df2eeeb6b408f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780405
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Gianni Chen <giannich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618564}

--- chrome/test/data/webui/new_tab_page/logo_test.ts
+++ chrome/test/data/webui/new_tab_page/logo_test.ts
@@ -144,6 +144,7 @@ suite('NewTabPageLogoTest', () => {
       const doodle = createImageDoodle();
       assertTrue(!!doodle.image);
       doodle.image.light.backgroundColor.value = 0xff0000ff;
+      loadTimeData.overrideValues({animatedDoodlesEnabled: true});
 
       // Act.
       const logo = await createLogo(
@@ -152,8 +153,8 @@ suite('NewTabPageLogoTest', () => {
             backgroundColor: color ? hexColorToSkColor(color) : undefined,
           }));
       // Assert.
-      assertStyle($$(logo, '#imageDoodle')!, 'padding', '16px 24px');
-      assertStyle($$(logo, '#imageDoodle')!, 'border-radius', '20px');
+      assertStyle($$(logo, '#imageDoodle')!, 'padding', '16px');
+      assertStyle($$(logo, '#imageDoodle')!, 'border-radius', '28px');
       assertStyle(
           $$(logo, '#imageDoodle')!, 'background-color', 'rgb(0, 0, 255)');
     });
