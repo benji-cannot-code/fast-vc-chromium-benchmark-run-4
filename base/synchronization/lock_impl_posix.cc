d0edc5e8a276227f5dc09bb2c2df6ad7eda3d232
Anand Ravi <anandrv@google.com>
2026-04-22 13:25:17 -0700

    [posix] Add user-space spinning to base::Lock
    This change introduces a feature where threads attempting to acquire a
    base::Lock on POSIX systems will first try to spin in user space for a
    configurable number of attempts before blocking in the kernel.
    
    Bug: 502232954
    Change-Id: I10f0aa9e1228139519f523b1cf1842b00119e9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7757702
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619047}

--- base/synchronization/lock_impl_posix.cc
+++ base/synchronization/lock_impl_posix.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/posix/safe_strerror.h"
 #include "base/synchronization/lock.h"
 #include "base/synchronization/lock_metrics_recorder.h"
+#include "base/synchronization/lock_subtle.h"
 #include "base/synchronization/synchronization_buildflags.h"
 #include "base/system/sys_info.h"
 #include "build/build_config.h"
@@ -38,6 +39,11 @@ namespace base {
 
 namespace internal {
 namespace {
+
+#if BUILDFLAG(IS_POSIX)
+// static
+std::atomic<int> g_spin_count{1024};
+#endif  // BUILDFLAG(IS_POSIX)
 
 #if BUILDFLAG(ENABLE_MUTEX_PRIORITY_INHERITANCE) && BUILDFLAG(IS_ANDROID)
 bool IsMutexPriorityInheritanceEnabled() {
@@ -129,10 +135,46 @@ LockImpl::~LockImpl() {
 }
 
 void LockImpl::LockInternal() {
+#if BUILDFLAG(IS_POSIX)
+  if (TrySpin()) {
+    return;
+  }
+#endif  // BUILDFLAG(IS_POSIX)
+
   LockMetricsRecorder::ScopedLockAcquisitionTimer timer;
   int rv = pthread_mutex_lock(&native_handle_);
   DCHECK_EQ(rv, 0) << ". " << SystemErrorCodeToString(rv);
 }
+
+#if BUILDFLAG(IS_POSIX)
+bool LockImpl::TrySpin() {
+  // Logic same as in SpinningMutex::AcquireSpinThenBlock() in partition_alloc.
+  int tries = 0;
+  int backoff = 1;
+  const int spin_count = g_spin_count.load(std::memory_order_relaxed);
+
+  do {
+    if (Try()) [[likely]] {
+      return true;
+    }
+
+    for (int yields = 0; yields < backoff; yields++) {
+      subtle::YieldProcessor();
+    }
+    tries += backoff;
+
+    constexpr int kMaxBackoff = 16;
+    backoff = std::min(kMaxBackoff, backoff << 1);
+  } while (tries < spin_count);
+
+  return false;
+}
+
+// static
+void LockImpl::SetTrySpinCount(int spin_count) {
+  g_spin_count.store(spin_count, std::memory_order_relaxed);
+}
+#endif  // BUILDFLAG(IS_POSIX)
 
 // static
 bool LockImpl::PriorityInheritanceAvailable() {
