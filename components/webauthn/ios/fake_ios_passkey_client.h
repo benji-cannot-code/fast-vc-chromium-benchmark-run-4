912d68999209dbb4e6c707fb98eb08cb07fad6ab
Sourav <sinhasourav@google.com>
2026-04-22 12:54:41 -0700

    [iOSPasskeysM3] Add enterprise policy check for passkeys M3
    This CL implements enterprise policy checks for passkey creation in
    Chrome for iOS. It ensures compliance with the PasswordManagerEnabled,
    PasswordManagerPasskeysEnabled, and SyncTypesListDisabled policies. When
    passkey saving to Google Password Manager is restricted by these
    policies, PasskeyTabHelper now defers passkey creation requests to the
    OS-mediated flow.
    
    Bug: 460487084
    Change-Id: Ib17914a9e6967707e394732892ee653810408381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782548
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619022}

--- components/webauthn/ios/fake_ios_passkey_client.h
+++ components/webauthn/ios/fake_ios_passkey_client.h
@@ -27,8 +27,10 @@ class FakeIOSPasskeyClient : public IOSPasskeyClient {
   void CancelPasskeyRequest(RequestInfo request_info) override;
 
   void AllowPasskeyCreationInfobar(bool allowed) override;
+  bool IsGpmPasskeySavingEnabled() const override;
 
   bool DidShowSuggestionBottomSheet() const;
+  void SetGpmPasskeySavingEnabled(bool enabled);
   bool DidShowCreationBottomSheet() const;
   bool DidFetchKeys() const;
   bool DidShowInterstitial() const;
@@ -40,6 +42,7 @@ class FakeIOSPasskeyClient : public IOSPasskeyClient {
   bool fetch_keys_called_ = false;
   bool show_interstitial_called_ = false;
   bool interstitial_proceeds_ = true;
+  bool gpm_passkey_saving_enabled_ = true;
 };
 
 }  // namespace webauthn
