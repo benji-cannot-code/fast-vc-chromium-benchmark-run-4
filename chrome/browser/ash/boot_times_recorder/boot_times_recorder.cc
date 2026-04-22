6b3195b265130b046f72a4e1fc3c88b6e100d4dd
Hidehiko Abe <hidehiko@chromium.org>
2026-04-22 16:35:45 -0700

    Move kLogoutStartedLast pref to ash/constants/ash_pref_names.h
    From chrome/common/pref_names.h for better OS and browser isolation.
    
    BUG=487139800
    TEST=Tryjob
    
    Change-Id: Iee7a2773356562ed65ebd4611d7f233eed7498f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782704
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619182}

--- chrome/browser/ash/boot_times_recorder/boot_times_recorder.cc
+++ chrome/browser/ash/boot_times_recorder/boot_times_recorder.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "ash/constants/ash_pref_names.h"
 #include "base/check.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
@@ -17,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/time/time.h"
-#include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/metrics/login_event_recorder.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
@@ -49,7 +49,7 @@ const std::string GetTabUrl(RenderWidgetHost* rwh) {
 // Clear saved logout-started metric in Local State. This method is called when
 // logout-state was written to file.
 void ClearLogoutStartedLastPreference(PrefService& local_state) {
-  local_state.ClearPref(prefs::kLogoutStartedLast);
+  local_state.ClearPref(ash::prefs::kLogoutStartedLast);
 }
 
 }  // namespace
@@ -128,7 +128,7 @@ void BootTimesRecorder::AddLoginTimeMarkerWithURL(const char* marker_name,
 
 void BootTimesRecorder::OnChromeProcessStart(PrefService& local_state) {
   const std::string logout_started_last_str =
-      local_state.GetString(prefs::kLogoutStartedLast);
+      local_state.GetString(ash::prefs::kLogoutStartedLast);
   if (logout_started_last_str.empty())
     return;
 
@@ -164,7 +164,7 @@ void BootTimesRecorder::OnLogoutStarted(PrefService* state) {
   const std::string uptime =
       LoginEventRecorder::Stats::GetCurrentStats().SerializeToString();
   if (!uptime.empty())
-    state->SetString(prefs::kLogoutStartedLast, uptime);
+    state->SetString(ash::prefs::kLogoutStartedLast, uptime);
 }
 
 void BootTimesRecorder::RecordCurrentStats(const std::string& name) {
