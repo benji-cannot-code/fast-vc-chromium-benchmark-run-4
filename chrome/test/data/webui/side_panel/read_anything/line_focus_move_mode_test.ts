ac76bca5c5713b0a5a4182a43573c0a01a9c62b2
Kristi Saney <kristislee@google.com>
2026-04-22 16:00:13 -0700

    [RM Line Focus] Move some movement logic to MoveMode
    This is part of a broader effort to refactor line focus to have better
    separation of concerns and be more readable.
    
    Bug: 503090130
    Change-Id: I66c0e5eae0a2cdda01db135fcef33f5cc6daa59e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7778733
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619158}

--- chrome/test/data/webui/side_panel/read_anything/line_focus_move_mode_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/line_focus_move_mode_test.ts
@@ -7,7 +7,7 @@ import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js'
 
 import {LineFocusCursorMoveMode, LineFocusLineStyleMode, LineFocusModel, LineFocusMovement, LineFocusNoneMoveMode, LineFocusStaticMoveMode, LineFocusStyle} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import type {MoveModeDelegate} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
-import {assertEquals, assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
+import {assertEquals, assertFalse, assertLT, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 
 import {FakeReadingMode} from './fake_reading_mode.js';
 
@@ -17,6 +17,17 @@ suite('LineFocusMoveMode', () => {
   let delegate: MoveModeDelegate;
   let sessionEnded: boolean;
 
+  const defaultHeight = 1000;
+
+  function createShortContainer(): HTMLElement {
+    const container = document.createElement('p');
+    container.innerText =
+        'I\'ve heard it said\nThat people come into our lives\nfor a reason.';
+    container.style.whiteSpace = 'pre-line';
+    document.body.appendChild(container);
+    return container;
+  }
+
   setup(() => {
     model = new LineFocusModel();
     styleMode = new LineFocusLineStyleMode(LineFocusStyle.UNDERLINE, model);
@@ -25,6 +36,8 @@ suite('LineFocusMoveMode', () => {
       onSessionEnd() {
         sessionEnded = true;
       },
+      notifyMove() {},
+      notifyScroll() {},
     };
   });
 
@@ -47,20 +60,32 @@ suite('LineFocusMoveMode', () => {
       model.setSessionActive(false);
       let started = false;
       chrome.readingMode.startLineFocusSession = () => started = true;
+      const container = createShortContainer();
 
-      mode.onActivated();
+      mode.onActivated(container, defaultHeight);
 
       assertTrue(started);
       assertTrue(model.isSessionActive());
       assertEquals(styleMode.getStyle(), model.getLastEnabledLineFocusStyle());
     });
 
+    test('onActivated updates positions', () => {
+      const container = createShortContainer();
+
+      mode.onActivated(container, defaultHeight);
+
+      assertEquals(defaultHeight, model.getMaxY());
+      assertLT(model.getMinY(), defaultHeight);
+      assertEquals(3, model.getTextBounds().length);
+    });
+
     test('onActivated does not restart active session', () => {
       model.setSessionActive(true);
       let started = false;
       chrome.readingMode.startLineFocusSession = () => started = true;
+      const container = document.createElement('div');
 
-      mode.onActivated();
+      mode.onActivated(container, 100);
 
       assertFalse(started);
       assertTrue(model.isSessionActive());
@@ -82,20 +107,32 @@ suite('LineFocusMoveMode', () => {
       model.setSessionActive(false);
       let started = false;
       chrome.readingMode.startLineFocusSession = () => started = true;
+      const container = document.createElement('div');
 
-      mode.onActivated();
+      mode.onActivated(container, defaultHeight);
 
       assertTrue(started);
       assertTrue(model.isSessionActive());
       assertEquals(styleMode.getStyle(), model.getLastEnabledLineFocusStyle());
     });
 
+    test('onActivated updates positions', () => {
+      const container = createShortContainer();
+
+      mode.onActivated(container, defaultHeight);
+
+      assertEquals(defaultHeight, model.getMaxY());
+      assertLT(model.getMinY(), defaultHeight);
+      assertEquals(3, model.getTextBounds().length);
+    });
+
     test('onActivated does not restart active session', () => {
       model.setSessionActive(true);
       let started = false;
       chrome.readingMode.startLineFocusSession = () => started = true;
+      const container = document.createElement('div');
 
-      mode.onActivated();
+      mode.onActivated(container, defaultHeight);
 
       assertFalse(started);
       assertTrue(model.isSessionActive());
@@ -122,8 +159,9 @@ suite('LineFocusMoveMode', () => {
       model.setSessionActive(true);
       model.setTop(100);
       model.setWindowHeight(100);
+      const container = document.createElement('div');
 
-      mode.onActivated();
+      mode.onActivated(container, 100);
 
       assertTrue(sessionEnded);
       assertFalse(model.isSessionActive());
@@ -133,8 +171,21 @@ suite('LineFocusMoveMode', () => {
 
     test('onActivated does not end inactive session', () => {
       model.setSessionActive(false);
-      mode.onActivated();
+      const container = document.createElement('div');
+
+      mode.onActivated(container, 100);
+
       assertFalse(sessionEnded);
+    });
+
+    test('onActivated does not update positions', () => {
+      const container = createShortContainer();
+
+      mode.onActivated(container, defaultHeight);
+
+      assertEquals(0, model.getMaxY());
+      assertEquals(0, model.getMinY());
+      assertEquals(0, model.getTextBounds().length);
     });
   });
 });
