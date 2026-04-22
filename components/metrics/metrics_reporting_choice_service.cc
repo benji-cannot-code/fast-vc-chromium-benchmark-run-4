cfd1bb01ef21265eae0d7d9fbbfcf0b124761255
Chirag Arora <heychirag@google.com>
2026-04-22 09:42:04 -0700

    Add boilerplate code for MetricsReportingChoiceService
    This service will help in managing the new three-level metrics consent
    state.
    
    Bug: 496476603
    Change-Id: I4725d4d9fcb6a9e889e82490f71b3f6bd4a7e092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780049
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1618928}

---
+++ components/metrics/metrics_reporting_choice_service.cc
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/metrics/metrics_reporting_choice_service.h"
+
+#include "base/check.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
+
+namespace metrics {
+
+MetricsReportingChoiceService::MetricsReportingChoiceService(
+    PrefService* local_state)
+    : local_state_(local_state) {
+  CHECK(local_state_);
+}
+
+MetricsReportingChoiceService::~MetricsReportingChoiceService() = default;
+
+// static
+void MetricsReportingChoiceService::RegisterPrefs(
+    PrefRegistrySimple* registry) {
+  // TODO(crbug.com/496476603): Register prefs used by this class.
+}
+
+}  // namespace metrics
