08cb1d40c1269cf1ee629df642e405dd6ada93b5
David Yeung <dayeung@chromium.org>
2026-04-22 16:28:26 -0700

    Add tab context menu to webui browser tabstrip
    Implement a native context menu for tabs in the Webium tab strip. This
    change enables users to access tab-specific actions via right click.
    
    Not all actions are implemented yet but this enables further testing to
    be done.
    
    https://screenshot.googleplex.com/6Lg9nBvbMMEXXFy
    
    Bug: 470136275
    Change-Id: Iab119e1666f78be02565835eca4b5ea4e2a43599
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7769233
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619178}

--- chrome/browser/ui/webui_browser/webui_browser_ui.h
+++ chrome/browser/ui/webui_browser/webui_browser_ui.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui_browser/extensions_bar.mojom.h"
 #include "chrome/browser/ui/webui_browser/webui_browser_window.h"
 #include "components/browser_apis/tab_strip/tab_strip_api.mojom.h"
+#include "components/browser_apis/tab_strip/tab_strip_experiment_api.mojom.h"
 #include "components/guest_contents/common/guest_contents.mojom.h"
 #include "components/omnibox/browser/searchbox.mojom.h"
 #include "content/public/browser/web_contents.h"
@@ -76,6 +77,9 @@ class WebUIBrowserUI : public ui::MojoWebUIController,
       mojo::PendingReceiver<guest_contents::mojom::GuestContentsHost> receiver);
   void BindInterface(
       mojo::PendingReceiver<tabs_api::mojom::TabStripService> receiver);
+  void BindInterface(
+      mojo::PendingReceiver<tabs_api::mojom::TabStripExperimentService>
+          receiver);
   void BindInterface(
       mojo::PendingReceiver<tracked_element::mojom::TrackedElementHandler>
           receiver);
