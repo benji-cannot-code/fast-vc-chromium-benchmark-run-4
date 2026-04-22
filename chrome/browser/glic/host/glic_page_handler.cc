f41d33e8a7e51770ff82356d2979f5a78cee8756
Alvin Ji <alvinji@chromium.org>
2026-04-22 16:23:10 -0700

    Revert "Return a reference from instance_metrics()"
    This reverts commit aa2a01a6693a1683b6b983a70a327dc7f0f88d88.
    
    Reason for revert: tree is closed due to compilation failed (https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/74937/overview).
    
    
    Original change's description:
    > Return a reference from instance_metrics()
    >
    > It's never null
    >
    > Bug: b:489122337
    > Link: https://chromium-review.googlesource.com/id/I06ed749fc1c2031948ff4c8e75f53b3c6a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787033
    > Reviewed-by: Nick Birnie <birnie@google.com>
    > Commit-Queue: Dan H <harringtond@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1619166}
    
    Bug: b:489122337
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib0bc8642a4a4f2c9a7f02eeee7dbf558cfd1ce8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787658
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1619171}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -1357,7 +1357,9 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
 
   void RecordSkillsWebClientEvent(
       glic::mojom::SkillsWebClientEvent action) override {
-    host().instance_metrics().RecordSkillsWebClientEvent(action);
+    if (auto* instance_metrics = host().instance_metrics()) {
+      instance_metrics->RecordSkillsWebClientEvent(action);
+    }
   }
 
   void CreateActorTab(int32_t task_id,
@@ -1683,8 +1685,10 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
   // non-profile-scoped metrics are logged entirely from GlicInstanceMetrics.
   void OnResponseStarted() override {
     host().instance_metrics_backwards_compatibility().OnResponseStarted();
-    host().instance_metrics().RecordAttachedContextTabCount(
-        sharing_manager().GetNumPinnedTabs());
+    if (auto* instance_metrics = host().instance_metrics()) {
+      instance_metrics->RecordAttachedContextTabCount(
+          sharing_manager().GetNumPinnedTabs());
+    }
   }
 
   // TODO(crbug.com/450026474): Remove call to GlicMetrics once
@@ -1715,7 +1719,7 @@ class GlicWebClientHandler : public glic::mojom::WebClientHandler,
   }
 
   void OnActionSubmitted(bool is_retry) override {
-    host().instance_metrics().OnActionSubmitted(is_retry);
+    host().instance_metrics()->OnActionSubmitted(is_retry);
   }
 
   void ScrollTo(mojom::ScrollToParamsPtr params,
