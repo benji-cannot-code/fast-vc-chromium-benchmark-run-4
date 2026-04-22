05146d718aa23441cce116d8ecd1d0706bba623d
Vignesh Shenvi <vshenvi@google.com>
2026-04-21 20:50:50 -0700

    Add: JNI API for surfacing the restricted content system dialog
    This change introduces a new JNI API for triggering the corresponding
    intent to surface the system dialog with additional metadata about the
    blocked webview navigation. We also update other JNI APIs to leverage
    the AconfigFlaggedApiDelegate.getInstance() static helper method.
    
    Bug: b:499090696
    Change-Id: I723bf039815958f75c3a23df49f60a086881ec29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779769
    Reviewed-by: Luke Cao <caott@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618643}

--- android_webview/java/src/org/chromium/android_webview/AwContentRestrictionManagerBridge.java
+++ android_webview/java/src/org/chromium/android_webview/AwContentRestrictionManagerBridge.java
@@ -18,7 +18,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.JniOnceCallback;
 import org.chromium.base.Log;
 import org.chromium.base.Promise;
-import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 
@@ -46,8 +45,7 @@ public class AwContentRestrictionManagerBridge {
         if (!Boolean.TRUE.equals(ManifestMetadataUtil.getContentRestrictionAppOptInPreference())) {
             return false;
         }
-        AconfigFlaggedApiDelegate delegate =
-                ServiceLoaderUtil.maybeCreate(AconfigFlaggedApiDelegate.class);
+        @Nullable AconfigFlaggedApiDelegate delegate = AconfigFlaggedApiDelegate.getInstance();
         if (delegate == null) {
             Log.w(TAG, "Unable to retrieve the AconfigFlaggedApiDelegate instance.");
             return false;
@@ -60,13 +58,12 @@ public class AwContentRestrictionManagerBridge {
             @JniType("std::string") String url,
             @JniType("std::string") String mimeType,
             JniOnceCallback<Boolean> callback) {
-        Uri uri = parseUrl(url);
+        @Nullable Uri uri = parseUrl(url);
         if (uri == null) {
             callback.onResult(false);
             return;
         }
-        AconfigFlaggedApiDelegate delegate =
-                ServiceLoaderUtil.maybeCreate(AconfigFlaggedApiDelegate.class);
+        @Nullable AconfigFlaggedApiDelegate delegate = AconfigFlaggedApiDelegate.getInstance();
         if (delegate == null) {
             Log.w(TAG, "Unable to retrieve the AconfigFlaggedApiDelegate instance.");
             callback.onResult(false);
@@ -91,5 +88,19 @@ public class AwContentRestrictionManagerBridge {
                     }
                     callback.onResult(false);
                 });
+    }
+
+    @CalledByNative
+    public static boolean sendShowRestrictedContentIntent(@JniType("std::string") String url) {
+        @Nullable Uri uri = parseUrl(url);
+        if (uri == null) {
+            return false;
+        }
+        @Nullable AconfigFlaggedApiDelegate delegate = AconfigFlaggedApiDelegate.getInstance();
+        if (delegate == null) {
+            Log.w(TAG, "Unable to retrieve the AconfigFlaggedApiDelegate instance.");
+            return false;
+        }
+        return delegate.sendShowRestrictedContentIntent(uri);
     }
 }
