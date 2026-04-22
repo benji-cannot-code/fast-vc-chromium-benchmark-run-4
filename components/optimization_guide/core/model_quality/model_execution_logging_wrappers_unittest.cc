258828dbcaa86a01f2db811cb797c558d7190d63
Oleh Lamzin <lamzin@google.com>
2026-04-22 06:35:08 -0700

    private_ai: Redirect FormsAI traffic to PAIC
    Redirect FormsAI traffic to Private AI Compute behind a feature flag.
    
    Bug: b:483985178
    Change-Id: I02498c5deb633afce0d9fd91fc04f8ec26c18be0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7720886
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1618827}

--- components/optimization_guide/core/model_quality/model_execution_logging_wrappers_unittest.cc
+++ components/optimization_guide/core/model_quality/model_execution_logging_wrappers_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/functional/callback_helpers.h"
 #include "base/test/bind.h"
 #include "base/test/protobuf_matchers.h"
 #include "base/types/expected.h"
@@ -52,6 +53,7 @@ TEST_F(ModelExecutionLoggingWrappersTest, ExecuteModelWithLogging) {
                     const google::protobuf::MessageLite& request_metadata,
                     const ModelExecutionOptions& options,
                     OptimizationGuideModelExecutionResultCallback callback) {
+        EXPECT_EQ(options.service_type, ModelExecutionServiceType::kDefault);
         std::move(callback).Run(OptimizationGuideModelExecutionResult(
                                     AnyWrapProto(response),
                                     std::make_unique<proto::ModelExecutionInfo>(
@@ -89,6 +91,7 @@ TEST_F(ModelExecutionLoggingWrappersTest, ExecuteModelWithLogging_Error) {
                    const google::protobuf::MessageLite& request_metadata,
                    const ModelExecutionOptions& options,
                    OptimizationGuideModelExecutionResultCallback callback) {
+        EXPECT_EQ(options.service_type, ModelExecutionServiceType::kDefault);
         std::move(callback).Run(
             OptimizationGuideModelExecutionResult(
                 base::unexpected(OptimizationGuideModelExecutionError::
@@ -121,6 +124,26 @@ TEST_F(ModelExecutionLoggingWrappersTest, ExecuteModelWithLogging_Error) {
   ExecuteModelWithLogging(model_executor(),
                           ModelBasedCapabilityKey::kTabOrganization, request,
                           std::nullopt, std::move(callback));
+}
+
+TEST_F(ModelExecutionLoggingWrappersTest,
+       ExecuteModelWithLogging_CustomServiceType) {
+  EXPECT_CALL(*model_executor(),
+              ExecuteModel(ModelBasedCapabilityKey::kTabOrganization, _, _,
+                           An<OptimizationGuideModelExecutionResultCallback>()))
+      .WillOnce([](ModelBasedCapabilityKey feature,
+                   const google::protobuf::MessageLite& request_metadata,
+                   const ModelExecutionOptions& options,
+                   OptimizationGuideModelExecutionResultCallback callback) {
+        EXPECT_EQ(options.service_type, ModelExecutionServiceType::kPrivateAi);
+        std::move(callback).Run(OptimizationGuideModelExecutionResult(),
+                                /*log_entry=*/nullptr);
+      });
+
+  proto::TabOrganizationRequest request;
+  ExecuteModelWithLogging<proto::TabOrganizationLoggingData>(
+      model_executor(), ModelBasedCapabilityKey::kTabOrganization, request,
+      std::nullopt, base::DoNothing(), ModelExecutionServiceType::kPrivateAi);
 }
 
 }  // namespace
