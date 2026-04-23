3b6f33c83ac6eb2ecee8caeb7f0ca50f5eedd8ed
Peter Kvitek <kvitekp@chromium.org>
2026-04-23 10:48:19 -0700

    [cft] Add required components configuration options
    This CL adds requiredComponents[Dir|UpdateTimeout] options handling in
    the JSON configuration file specified by --chrome-for-testing-config
    command line switch. The actual Required Components option will be
    landed in a follow up CL.
    
    Change-Id: Ic17883ac2d9adc9c326a78664f109bb1380e0528
    Cq-Include-Trybots: luci.chrome.try:linux-cft,mac-cft,win-cft
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7763894
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619627}

--- chrome/browser/chrome_for_testing/config.h
+++ chrome/browser/chrome_for_testing/config.h
@@ -6,7 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROME_FOR_TESTING_CONFIG_H_
 #define CHROME_BROWSER_CHROME_FOR_TESTING_CONFIG_H_
 
+#include <string>
+
+#include "base/containers/flat_map.h"
+#include "base/files/file_path.h"
+#include "base/time/time.h"
+
 class PrefService;
+
+namespace base {
+class ListValue;
+}
 
 namespace chrome_for_testing {
 
@@ -15,11 +25,17 @@ namespace chrome_for_testing {
 // missing or contains invalid JSON.
 bool LoadConfig(PrefService* pref_service);
 
-// Convenience functions to check Chrome for Testing prefs returned by
+// Convenience functions to retrieve Chrome for Testing prefs returned by
 // |g_browser_process->local_state()|.
 bool IsEnableUserEducationUI();
 bool IsEnableSearchEngineChoiceDialog();
 bool IsEnableVirtualClipboard();
+const base::ListValue& GetRequiredComponentsList();
+base::FilePath GetRequiredComponentsDir();
+base::TimeDelta GetRequiredComponentsUpdateTimeout();
+
+// Returns required component names and, optionally, their versions.
+base::flat_map<std::string, std::string> GetRequiredComponentsMap();
 
 }  // namespace chrome_for_testing
 
--- chrome/browser/chrome_for_testing/prefs.cc
+++ chrome/browser/chrome_for_testing/prefs.cc
@@ -5,14 +5,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chrome_for_testing/prefs.h"
 
+#include "base/files/file_path.h"
+#include "base/time/time.h"
+#include "base/values.h"
 #include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
 
 namespace chrome_for_testing {
+
+namespace {
+
+// All registered components are typically updated in less than 3 seconds, so
+// set the default timeout anticipating slower networks.
+constexpr base::TimeDelta kDefaultRequiredComponentsUpdateTimeout =
+    base::Seconds(15);
+
+}  // namespace
 
 void RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kEnableUserEducationUI, false);
   registry->RegisterBooleanPref(prefs::kEnableSearchEngineChoiceDialog, false);
   registry->RegisterBooleanPref(prefs::kEnableVirtualClipboard, false);
+  registry->RegisterListPref(prefs::kRequiredComponents, base::ListValue());
+  registry->RegisterFilePathPref(prefs::kRequiredComponentsDir,
+                                 base::FilePath());
+  registry->RegisterTimeDeltaPref(prefs::kRequiredComponentsUpdateTimeout,
+                                  kDefaultRequiredComponentsUpdateTimeout);
+}
+
+void ClearPrefs(PrefService* pref_service) {
+  pref_service->ClearPrefsWithPrefixSilently("chrome_for_testing");
 }
 
 }  // namespace chrome_for_testing
