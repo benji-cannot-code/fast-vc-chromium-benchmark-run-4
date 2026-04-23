21de819cf2ebca70b343a2f6cc69ff2bfaaf10ba
Peter Conn <peconn@chromium.org>
2026-04-23 09:50:25 -0700

    Add Preconnect.Event metrics.
    Bug: 444385271
    Change-Id: I8f215667bc00510ae333afc5ca2f7cb29cee2e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790892
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619590}

--- android_webview/browser/aw_preconnector.cc
+++ android_webview/browser/aw_preconnector.cc
@@ -26,6 +26,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+//
+// LINT.IfChange(AwPreconnectEvent)
+enum class AwPreconnectEvent {
+  kPreconnectCalled = 0,
+  kSessionClosed = 1,
+  kMaxValue = kSessionClosed,
+};
+// LINT.ThenChange(tools/metrics/histograms/enums.xml:AndroidWebViewPreconnectEvent)
+
 inline constexpr net::NetworkTrafficAnnotationTag
     kWebViewPreconnectTrafficAnnotation =
         net::DefineNetworkTrafficAnnotation("webview_preconnect",
@@ -107,6 +118,9 @@ bool AwPreconnector::Preconnect(JNIEnv* env, const GURL& url) {
 
   TRACE_EVENT1("android_webview", "Preconnect::Begin", "url", url);
 
+  base::UmaHistogramEnumeration("Android.WebView.Preconnect.Event",
+                                AwPreconnectEvent::kPreconnectCalled);
+
   return true;
 }
 
@@ -148,6 +162,9 @@ void AwPreconnector::OnSessionClosed() {
 
   base::UmaHistogramMediumTimes("Android.WebView.Preconnect.ConnectionDuration",
                                 duration);
+
+  base::UmaHistogramEnumeration("Android.WebView.Preconnect.Event",
+                                AwPreconnectEvent::kSessionClosed);
 
   TRACE_EVENT2("android_webview", "Preconnect::OnSessionClosed", "url",
                context.url, "duration", duration);
--- android_webview/javatests/src/org/chromium/android_webview/test/AwPreconnectorTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwPreconnectorTest.java
@@ -24,6 +24,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.url.GURL;
 
 import java.io.IOException;
@@ -60,12 +61,16 @@ public class AwPreconnectorTest extends AwParameterizedTest {
         AwBrowserContext profile = mTestRule.getProfileSync("Default", /* createIfNeeded= */ true);
         String url = "http://localhost:" + mServerThread.getPort();
 
+        var histogramWatcher =
+                HistogramWatcher.newSingleRecordWatcher("Android.WebView.Preconnect.Event", 0);
+
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     profile.getPreconnector().preconnect(new GURL(url));
                 });
 
         mServerThread.waitForConnection();
+        histogramWatcher.assertExpected();
     }
 
     @Test
