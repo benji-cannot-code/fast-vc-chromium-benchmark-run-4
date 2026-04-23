d603efe4d09c95b35f8ffdf3624c1010e254d641
Dan Harrington <harringtond@chromium.org>
2026-04-23 10:25:56 -0700

    Return a reference from instance_metrics()
    It's never null
    
    Bug: b:489122337
    Link: https://chromium-review.googlesource.com/id/Ia0cef45375aa9f77d6cbc440605a32246a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790111
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619620}

--- chrome/browser/glic/host/glic_annotation_manager.cc
+++ chrome/browser/glic/host/glic_annotation_manager.cc
@@ -157,9 +157,7 @@ void GlicAnnotationManager::ScrollTo(
   }
   annotation_task_.reset();
 
-  if (auto* metrics = host->instance_metrics()) {
-    metrics->OnGlicScrollAttempt();
-  }
+  host->instance_metrics().OnGlicScrollAttempt();
 
   mojom::WebClientHandler::ScrollToCallback wrapped_callback =
       base::BindOnce(&RunScrollToCallback, std::move(callback));
@@ -425,7 +423,7 @@ void GlicAnnotationManager::AnnotationTask::SetState(State new_state) {
     case State::kFailed: {
       bool success = new_state == State::kActive;
       if (host_) {
-        host_->instance_metrics()->OnGlicScrollComplete(success);
+        host_->instance_metrics().OnGlicScrollComplete(success);
       }
       break;
     }
