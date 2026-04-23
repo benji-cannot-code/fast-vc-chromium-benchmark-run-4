98cfbf5ae701efe3dd516baf48383515bbfebf5c
Ewann Pelle <ewannpv@chromium.org>
2026-04-23 05:42:42 -0700

    [iOS][Assistant] Fix appContentView frame
    This CL fixes a layout issue in `SceneViewController.mm` on iPad when
    the side panel is active. We now use `bounds` instead of `frame` for
    `_appContentView` to avoid a double shift of the origin.
    
    Bug: 494503434
    Change-Id: Ia6cba2b8baa29d2ea965ed41926561bb97d793b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789497
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619448}

--- ios/chrome/browser/scene/ui/scene_view_controller.mm
+++ ios/chrome/browser/scene/ui/scene_view_controller.mm
@@ -567,6 +567,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   CGRect contentFrame = UIEdgeInsetsInsetRect(frame, insets);
   _appContentContainerView.frame = contentFrame;
+  if (self.layoutState.containedLayoutActive && !IsChromeNextIaEnabled()) {
+    // When the Assistant side panel is active, use bounds to avoid a double
+    // shift of the origin.
+    // However, when IsChromeNextIaEnabled() is true, the view hierarchy is
+    // inverted (when fullscreen refactor is disabled). In that mode, we must
+    // use frame to avoid misplacing the view at the left edge.
+    _appContentView.frame = _appContentContainerView.bounds;
+    return;
+  }
   _appContentView.frame = _appContentContainerView.frame;
 }
 
