01d756797f13b1267be637073f50d9a586cfcaca
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2026-04-23 10:16:57 -0700

    [views-ax] Fire OnReadonlyChanged for kEnabledChanged on Linux
    On ATK, the enabled and read-only states are exposed through different properties and therefore the provider is expected to fire the state changed event for both events. They are correlated: a disabled
    control is implicitly read-only. AXPlatformNodeAuraLinux already fires
    OnReadonlyChanged alongside OnEnabledChanged for web content, but
    we were missing it for views. This CL fixes it.
    
    Update the navigation-api auralinux expectation to include the new
    READ-ONLY state-change events on the Stop button.
    
    Bug: 40672441
    Include-Ci-Only-Tests: true
    Change-Id: Id8dfdc11d6beee9cd0f64d33c75f501c66f3cb74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787911
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619614}

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -4149,6 +4149,7 @@ void AXPlatformNodeAuraLinux::NotifyAccessibilityEvent(
       break;
     case ax::mojom::Event::kEnabledChanged:
       OnEnabledChanged();
+      OnReadonlyChanged();
       break;
     case ax::mojom::Event::kTextChanged:
       OnNameChanged();
