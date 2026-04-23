d0c812fa44175c30e06981c01c42e72bdb9bd8ed
Joe Mason <joenotcharles@google.com>
2026-04-23 07:19:21 -0700

    Refactor heap profiler Supervisor initialization for clarity
    A confusing number of methods are named "Start" with different meanings,
    but the method that actually sets `started_` isn't! Renames some methods
    so that only the one that actually triggers profiling is named "Start":
    
    * StartProfilingOnMemoryInfraThread ->
      RegisterProfilerOnMemoryInfraThread (also updates the doc comments to
      call this Stage 1 of initialization, since it kicks off the other 2
      stages)
    * StartServiceOnIOThread -> LaunchServiceOnIOThread
    * FinishInitializationOnUIThread -> StartClientConnectionOnUIThread
    
    Also, instead of passing the parameters that are needed for each stage
    to the previous stages, passes continuation callbacks with the
    parameters bound to them. That will make it easier for a followup to
    change the initialization flow by modifying the callbacks instead of
    having to plumb state through multiple functions.
    
    Change-Id: Iefdc2ab0f018318cd546106e1342fc095e9b8de7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785323
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619497}

--- components/heap_profiling/multi_process/supervisor.cc
+++ components/heap_profiling/multi_process/supervisor.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/no_destructor.h"
+#include "base/task/bind_post_task.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "components/heap_profiling/multi_process/client_connection_manager.h"
@@ -73,29 +74,36 @@ void Supervisor::Start(Mode mode,
                        base::OnceClosure closure) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   DCHECK(!started_);
+  // Unretained is safe because `this` is a leaked global singleton.
+  auto ui_thread_callback = base::BindPostTask(
+      content::GetUIThreadTaskRunner({}),
+      base::BindOnce(&Supervisor::StartClientConnectionOnUIhread,
+                     base::Unretained(this), mode, std::move(closure)));
+  auto io_thread_callback = base::BindPostTask(
+      content::GetIOThreadTaskRunner({}),
+      base::BindOnce(&Supervisor::LaunchServiceOnIOThread,
+                     base::Unretained(this), stack_mode, sampling_rate,
+                     std::move(ui_thread_callback)));
   base::trace_event::MemoryDumpManager::GetInstance()
       ->GetDumpThreadTaskRunner()
       ->PostTask(FROM_HERE,
-                 base::BindOnce(&Supervisor::StartProfilingOnMemoryInfraThread,
-                                base::Unretained(this), mode, stack_mode,
-                                sampling_rate, std::move(closure)));
+                 base::BindOnce(
+                     &Supervisor::RegisterProfilerOnMemoryInfraThread,
+                     base::Unretained(this), std::move(io_thread_callback)));
 }
 
-void Supervisor::StartProfilingOnMemoryInfraThread(Mode mode,
-                                                   mojom::StackMode stack_mode,
-                                                   uint32_t sampling_rate,
-                                                   base::OnceClosure closure) {
+void Supervisor::RegisterProfilerOnMemoryInfraThread(
+    base::OnceCallback<void(
+        mojo::PendingReceiver<memory_instrumentation::mojom::HeapProfiler>,
+        mojo::PendingRemote<memory_instrumentation::mojom::HeapProfilerHelper>)>
+        continue_on_io_thread) {
   mojo::PendingRemote<memory_instrumentation::mojom::HeapProfilerHelper> helper;
   mojo::PendingRemote<memory_instrumentation::mojom::HeapProfiler> profiler;
   auto profiler_receiver = profiler.InitWithNewPipeAndPassReceiver();
   content::GetMemoryInstrumentationRegistry()->RegisterHeapProfiler(
       std::move(profiler), helper.InitWithNewPipeAndPassReceiver());
-  content::GetIOThreadTaskRunner({})->PostTask(
-      FROM_HERE,
-      base::BindOnce(&Supervisor::StartServiceOnIOThread,
-                     base::Unretained(this), std::move(profiler_receiver),
-                     std::move(helper), mode, stack_mode, sampling_rate,
-                     std::move(closure)));
+  std::move(continue_on_io_thread)
+      .Run(std::move(profiler_receiver), std::move(helper));
 }
 
 Mode Supervisor::GetMode() {
@@ -186,14 +194,13 @@ void Supervisor::RequestTraceWithHeapDump(TraceFinishedCallback callback,
   DCHECK(result);
 }
 
-void Supervisor::StartServiceOnIOThread(
-    mojo::PendingReceiver<memory_instrumentation::mojom::HeapProfiler> receiver,
-    mojo::PendingRemote<memory_instrumentation::mojom::HeapProfilerHelper>
-        remote_helper,
-    Mode mode,
+void Supervisor::LaunchServiceOnIOThread(
     mojom::StackMode stack_mode,
     uint32_t sampling_rate,
-    base::OnceClosure closure) {
+    base::OnceCallback<void(base::WeakPtr<Controller>)> continue_on_ui_thread,
+    mojo::PendingReceiver<memory_instrumentation::mojom::HeapProfiler> receiver,
+    mojo::PendingRemote<memory_instrumentation::mojom::HeapProfilerHelper>
+        remote_helper) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::IO));
 
   mojo::PendingRemote<mojom::ProfilingService> service =
@@ -201,15 +208,10 @@ void Supervisor::StartServiceOnIOThread(
 
   controller_ = std::make_unique<Controller>(std::move(service), stack_mode,
                                              sampling_rate);
-  base::WeakPtr<Controller> controller_weak_ptr = controller_->GetWeakPtr();
-
-  content::GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(&Supervisor::FinishInitializationOnUIhread,
-                                base::Unretained(this), mode,
-                                std::move(closure), controller_weak_ptr));
+  std::move(continue_on_ui_thread).Run(controller_->GetWeakPtr());
 }
 
-void Supervisor::FinishInitializationOnUIhread(
+void Supervisor::StartClientConnectionOnUIhread(
     Mode mode,
     base::OnceClosure closure,
     base::WeakPtr<Controller> controller_weak_ptr) {
