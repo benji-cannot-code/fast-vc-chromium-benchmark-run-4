da4e5acb76c5246fab960a602846e247c9f6997e
Victor Shu <victorshu@google.com>
2026-04-23 09:24:07 -0700

    [AA] IngestedContentAnnotation Conversion
    - Converts ContentAnnotationData into IngestedContentAnnotation
    - Unit tests
    
    Bug: b:502225870
    Change-Id: Ibf504dde4f72fdb88112027f0e3a25c197a004b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759209
    Commit-Queue: Victor Shu <victorshu@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619570}

---
+++ components/accessibility_annotator/core/annotation_reducer/ingested_content_annotation_utils.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_ANNOTATION_REDUCER_INGESTED_CONTENT_ANNOTATION_UTILS_H_
+#define COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_ANNOTATION_REDUCER_INGESTED_CONTENT_ANNOTATION_UTILS_H_
+
+#include "components/accessibility_annotator/core/annotation_reducer/ingested_content_annotation.h"
+#include "components/accessibility_annotator/core/storage/accessibility_annotator_backend.h"
+
+namespace accessibility_annotator {
+
+// Converts a `ContentAnnotationData` to an `IngestedContentAnnotation`.
+IngestedContentAnnotation ConvertIngestedContentAnnotation(
+    const AccessibilityAnnotatorBackend::ContentAnnotationsData&
+        content_annotation_data);
+
+}  // namespace accessibility_annotator
+
+#endif  // COMPONENTS_ACCESSIBILITY_ANNOTATOR_CORE_ANNOTATION_REDUCER_INGESTED_CONTENT_ANNOTATION_UTILS_H_
