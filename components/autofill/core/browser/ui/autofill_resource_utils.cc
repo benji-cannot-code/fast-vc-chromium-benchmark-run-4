260cf19c87b78163e74d57d5a181efa66e45eb79
Timofey Chudakov <tchudakov@google.com>
2026-04-23 04:06:35 -0700

    Reland "[Autofill] Add new suggestion icons."
    This reverts commit 1aad18411f6ba49eb7c67a86d1e820cca51d649e.
    
    Reason for revert: The build works now
    
    Original change's description:
    > Revert "[Autofill] Add new suggestion icons."
    >
    > This reverts commit d7b023cf3cdfe3f13af2d92083f7979a52239b2a.
    >
    > Reason for revert: breaks the tree
    >
    > Original change's description:
    > > [Autofill] Add new suggestion icons.
    > >
    > > Spark: screen/4zAbTPLKhQYK2p6
    > >
    > > Bug: 501040307
    > > Change-Id: I31693dc85c913861be8d5992ede432f6a02ae870
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7743123
    > > Reviewed-by: Norge Vizcay <vizcay@google.com>
    > > Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1619389}
    >
    > Bug: 501040307
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I33c3f7d491f86d09114aacbc651a634a02e219b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789810
    > Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Norge Vizcay <vizcay@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1619398}
    
    Bug: 501040307
    Change-Id: Ic53a6682926f8aa9694116f24a3fda9d7a3df04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789496
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619425}

--- components/autofill/core/browser/ui/autofill_resource_utils.cc
+++ components/autofill/core/browser/ui/autofill_resource_utils.cc
@@ -65,6 +65,7 @@ constexpr auto kOldDataResources = base::MakeFixedFlatMap<Suggestion::Icon,
     {Suggestion::Icon::kPersonCheck, IDR_ANDROID_AUTOFILL_PERSON_CHECK},
     {Suggestion::Icon::kVehicle, IDR_ANDROID_AUTOFILL_VEHICLE},
     {Suggestion::Icon::kPassport, IDR_ANDROID_AUTOFILL_PASSPORT},
+    {Suggestion::Icon::kSpark, IDR_ANDROID_AUTOFILL_SPARK},
 #endif  // BUILDFLAG(IS_ANDROID)
 });
 
@@ -99,6 +100,7 @@ constexpr auto kDataResources = base::MakeFixedFlatMap<Suggestion::Icon, int>({
     {Suggestion::Icon::kPersonCheck, IDR_ANDROID_AUTOFILL_PERSON_CHECK},
     {Suggestion::Icon::kVehicle, IDR_ANDROID_AUTOFILL_VEHICLE},
     {Suggestion::Icon::kPassport, IDR_ANDROID_AUTOFILL_PASSPORT},
+    {Suggestion::Icon::kSpark, IDR_ANDROID_AUTOFILL_SPARK},
 #endif  // BUILDFLAG(IS_ANDROID)
 });
 
