dd7e6d9fe664bcae2f20b62618044fc61178c721
Fiaz Muhammad <mfiaz@google.com>
2026-04-23 07:10:14 -0700

    [TabBottomSheet] Support bottom toolbar when resizing is enabled
    Bug: 486916366
    Change-Id: I5799f5f108a6f83199a788bd9a99556d73b29a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785621
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619492}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
@@ -228,7 +228,7 @@ public class TabBottomSheetCoordinator {
                         if (mSheetEventsCallback == null) {
                             return;
                         }
-                        setToFixedHeight();
+                        setToFixedHeightOrFallback();
 
                         boolean isSheetHeightSufficient =
                                 mMediator.isSheetHeightSufficient(
@@ -364,10 +364,14 @@ public class TabBottomSheetCoordinator {
                     mExpectingLayoutChange = false;
                 }
                 if (ChromeFeatureList.sTabBottomSheetResizeWebview.getValue()) {
-                    if (mInitialContainerSizeChanged) setToFlexibleHeight();
+                    if (mInitialContainerSizeChanged) {
+                        setToFlexibleHeight();
+                    } else {
+                        setToFixedHeightOrFallback();
+                    }
                     mInitialContainerSizeChanged = true;
                 } else {
-                    setToFixedHeight();
+                    setToFixedHeightOrFallback();
                 }
             }
 
@@ -422,9 +426,26 @@ public class TabBottomSheetCoordinator {
         mMediator.setToFlexibleHeight();
     }
 
-    private void setToFixedHeight() {
+    private void setToFixedHeightOrFallback() {
         if (mWindowAndroid.getWindow() == null) return;
-        mMediator.setToFixedHeight((int) (getVisibleViewportHeight() * getDefaultHeightRatio()));
+        @Px int fixedHeight = (int) (getVisibleViewportHeight() * getDefaultHeightRatio());
+
+        // The bottom sheet controller max offset calculations may not be correct without the
+        // sheet content being set to a fixed height first.
+        mMediator.setToFixedHeight(fixedHeight);
+
+        int newFixedHeight = Math.min(fixedHeight, mBottomSheetController.getMaxOffset());
+        if (newFixedHeight != fixedHeight) {
+            fixedHeight = newFixedHeight;
+            mMediator.setToFixedHeight(fixedHeight);
+        }
+
+        // In the case the bottom sheet is unable to set to our desired fixed height, fallback to
+        // use of flexible heights.
+        if (ChromeFeatureList.sTabBottomSheetResizeWebview.getValue()
+                && mBottomSheetController.getContainerHeight() != fixedHeight) {
+            setToFlexibleHeight();
+        }
     }
 
     private int getVisibleViewportHeight() {
