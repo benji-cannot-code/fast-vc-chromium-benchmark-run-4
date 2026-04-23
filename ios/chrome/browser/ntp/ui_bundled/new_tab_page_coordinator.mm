3f57a4f9c74523a7c82a5424722a352e812f307d
Radu Nitescu <radunitescu@google.com>
2026-04-23 07:30:37 -0700

    [iOS] Integrate ComposeboxMenu in NTP coordinator
    Change-Id: I16c665bf5a7e109b30dba2abaa5e4a3ae7ce2d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7784692
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619501}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/authentication/ui_bundled/signin/signin_utils.h"
 #import "ios/chrome/browser/bubble/ui_bundled/bubble_view_controller_presenter.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_availability.h"
+#import "ios/chrome/browser/composebox/menu/coordinator/composebox_menu_coordinator.h"
 #import "ios/chrome/browser/content_suggestions/coordinator/content_suggestions_coordinator.h"
 #import "ios/chrome/browser/content_suggestions/coordinator/content_suggestions_delegate.h"
 #import "ios/chrome/browser/content_suggestions/coordinator/content_suggestions_mediator.h"
@@ -303,6 +304,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   SearchEngineLogoMediator* _searchEngineLogoMediator;
   // The Safari data import used by the content suggestions.
   SafariDataImportExportCoordinator* _safariDataImportExportCoordinator;
+  // The coordinator showing the multimodal composebox menu.
+  ComposeboxMenuCoordinator* _composeboxMenuCoordinator;
 }
 
 // Synthesize NewTabPageConfiguring properties.
@@ -1970,15 +1973,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)openMultimodalActionsMenu {
   [self.NTPMetricsRecorder recordPlusButtonTapped];
   [self dismissCustomizationMenu];
-  if (!IsComposeboxAIMDisabled() &&
-      _aimEligibilityService->IsFuseboxEligible() &&
-      MaybeShowComposebox(self.browser, ComposeboxEntrypoint::kNTPPlusButton)) {
-    return;
-  }
 
   // Fallback to opening AIM if eligibility changed in the meantime and the NTP
   // was not reloaded since.
-  [self openAIMWeb];
+  if (IsComposeboxAIMDisabled() ||
+      !_aimEligibilityService->IsFuseboxEligible()) {
+    [self openAIMWeb];
+  }
+
+  if (IsComposeboxPlusButtonBottomSheet()) {
+    [_composeboxMenuCoordinator stop];
+    // TODO(crbug.com/505386922): Add entrypoint and delegate to cleanup the
+    // coordinator on dismissal.
+    _composeboxMenuCoordinator = [[ComposeboxMenuCoordinator alloc]
+        initWithBaseViewController:self.viewController
+                           browser:self.browser];
+    [_composeboxMenuCoordinator start];
+  } else if (MaybeShowComposebox(self.browser,
+                                 ComposeboxEntrypoint::kNTPPlusButton)) {
+    return;
+  } else {
+    [self openAIMWeb];
+  }
 }
 
 #pragma mark - TabGridStateObserver
