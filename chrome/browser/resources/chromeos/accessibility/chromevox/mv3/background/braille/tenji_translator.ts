28db49c7fc9901f73b522c93b84e01a61a75ebae
Orko Garai <orko@igalia.com>
2026-04-22 18:30:06 -0700

    [ChromeVox] Add tests for BrailleTranslatorManager
    Add ChromeVoxBrailleTranslatorManagerTest browser_tests coverage for
    TenjiTranslator vs LibLouis.Translator selection in
    BrailleTranslatorManager.
    
    Design doc:
    https://docs.google.com/document/d/1R-IvKTwH0Sjim0KJLaH12IKH_gwigUXJ5HM_VQBeOcQ/edit?usp=sharing
    
    R=dtseng
    
    Bug: 500394286
    Change-Id: Iab5aa88bd6b386a9190a5e5e2163eba19a5aad4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783603
    Commit-Queue: Orko Garai <orko@igalia.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619253}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/background/braille/tenji_translator.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/background/braille/tenji_translator.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {TestImportManager} from '/common/testing/test_import_manager.js';
+
 import {BackTranslateCallback, BrailleTranslator, TranslateCallback} from './braille_translator.js';
 
 export class TenjiTranslator implements BrailleTranslator {
@@ -25,3 +27,5 @@ export class TenjiTranslator implements BrailleTranslator {
     callback(null);
   }
 }
+
+TestImportManager.exportForTesting(TenjiTranslator);
