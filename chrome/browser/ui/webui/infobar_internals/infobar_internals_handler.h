f06d03aa17a8f193ca2a6f036281b32a654d6111
Muhammad Salmaan <musalmaan@chromium.org>
2026-04-23 14:46:21 -0700

    [infobar-internals] Add extensions infobar
    Added the extensions infobar to infobar internals.
    
    Bug: 505785449
    Change-Id: I7e9ee848287a1fcd3151b1958ea045210597e68e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789429
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619776}

--- chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.h
+++ chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_INFOBAR_INTERNALS_INFOBAR_INTERNALS_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_INFOBAR_INTERNALS_INFOBAR_INTERNALS_HANDLER_H_
 
+#include <vector>
+
+#include "base/callback_list.h"
 #include "chrome/browser/ui/webui/infobar_internals/infobar_internals.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 
@@ -32,6 +35,10 @@ class InfoBarInternalsHandler final
   bool TriggerInfoBarInternal(infobar_internals::mojom::InfoBarType type);
 
   mojo::Receiver<infobar_internals::mojom::PageHandler> receiver_;
+
+  // Stores the subscriptions to the ExtensionDevToolsInfoBarDelegate to keep
+  // them alive.
+  std::vector<base::CallbackListSubscription> subscriptions_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_INFOBAR_INTERNALS_INFOBAR_INTERNALS_HANDLER_H_
