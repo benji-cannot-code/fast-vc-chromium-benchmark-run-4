9cf457df5cc41f43a0a544f00d55f4fdfb00edf9
Daniel Rubery <drubery@chromium.org>
2026-04-23 11:32:43 -0700

    [bedrock] Remove Browser* from current_tab_desktop_media_list_unittest
    This fixture only uses a Browser as a holder for manually-created
    WebContents. Use a vector instead.
    
    Bug: 505068940
    Link: https://chromium-review.googlesource.com/id/Ia5f4e5a5a57fe1b5088ebfd16e4d89a86a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782886
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619644}

--- chrome/browser/media/webrtc/current_tab_desktop_media_list_unittest.cc
+++ chrome/browser/media/webrtc/current_tab_desktop_media_list_unittest.cc
@@ -17,12 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/desktop_media_list.h"
 #include "chrome/browser/media/webrtc/tab_desktop_media_list_mock_observer.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/fake_profile_manager.h"
-#include "chrome/test/base/test_browser_window.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_switches.h"
@@ -81,9 +80,6 @@ class CurrentTabDesktopMediaListTest : public testing::Test {
     profile_ = g_browser_process->profile_manager()
                    ->GetLastUsedProfileAllowedByPolicy();
 
-    Browser::CreateParams profile_params(profile_, true);
-    browser_ = CreateBrowserWithTestWindowForParams(profile_params);
-
     // Seed some tabs.
     for (int i = 0; i < 10; ++i) {  // Arbitrary value. kMainTab must be lower.
       CreateWebContents();
@@ -95,17 +91,7 @@ class CurrentTabDesktopMediaListTest : public testing::Test {
   void TearDown() override {
     list_.reset();
 
-    // TODO(crbug.com/40571733): Tearing down the TabStripModel should just
-    // delete all its owned WebContents. Then |manually_added_web_contents_|
-    // won't be necessary.
-    TabStripModel* tab_strip_model = browser_->tab_strip_model();
-    for (WebContents* contents : all_web_contents_) {
-      tab_strip_model->DetachAndDeleteWebContentsAt(
-          tab_strip_model->GetIndexOfWebContents(contents));
-    }
     all_web_contents_.clear();
-
-    browser_.reset();
     TestingBrowserProcess::GetGlobal()->SetProfileManager(nullptr);
     rvh_test_enabler_.reset();
   }
@@ -130,16 +116,13 @@ class CurrentTabDesktopMediaListTest : public testing::Test {
       entry = web_contents->GetController().GetLastCommittedEntry();
     }
 
-    all_web_contents_.push_back(web_contents.get());
-    browser_->tab_strip_model()->AppendWebContents(std::move(web_contents),
-                                                   true);
+    all_web_contents_.push_back(std::move(web_contents));
   }
 
   void RemoveWebContents(WebContents* web_contents) {
-    TabStripModel* tab_strip_model = browser_->tab_strip_model();
-    tab_strip_model->DetachAndDeleteWebContentsAt(
-        tab_strip_model->GetIndexOfWebContents(web_contents));
-    std::erase(all_web_contents_, web_contents);
+    std::erase_if(all_web_contents_, [web_contents](const auto& wc) {
+      return wc.get() == web_contents;
+    });
   }
 
   void Wait() {
@@ -156,12 +139,11 @@ class CurrentTabDesktopMediaListTest : public testing::Test {
 
   std::unique_ptr<content::RenderViewHostTestEnabler> rvh_test_enabler_;
   raw_ptr<Profile, DanglingUntriaged> profile_;
-  std::unique_ptr<Browser> browser_;
 
   StrictMock<DesktopMediaListMockObserver> observer_;
   std::unique_ptr<CurrentTabDesktopMediaList> list_;
 
-  std::vector<raw_ptr<WebContents, VectorExperimental>> all_web_contents_;
+  std::vector<std::unique_ptr<content::WebContents>> all_web_contents_;
 
   content::BrowserTaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
@@ -184,7 +166,7 @@ TEST_F(CurrentTabDesktopMediaListTest, UpdateSourcesListCalledWithCurrentTab) {
   EXPECT_CALL(observer_, OnSourceThumbnailChanged(0))
       .Times(1)
       .WillOnce(QuitMessageLoop(run_loop_.get()));
-  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab]);
+  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab].get());
   run_loop_->Run();
 }
 
@@ -196,7 +178,7 @@ TEST_F(CurrentTabDesktopMediaListTest,
   EXPECT_CALL(observer_, OnSourceThumbnailChanged(0))
       .Times(1)
       .WillOnce(QuitMessageLoop(run_loop_.get()));
-  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab]);
+  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab].get());
   run_loop_->Run();
 
   // Test focus.
@@ -212,12 +194,12 @@ TEST_F(CurrentTabDesktopMediaListTest,
   EXPECT_CALL(observer_, OnSourceThumbnailChanged(0))
       .Times(1)
       .WillOnce(QuitMessageLoop(run_loop_.get()));
-  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab]);
+  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab].get());
   run_loop_->Run();
 
   // Test focus.
   EXPECT_CALL(observer_, OnSourceRemoved(_)).Times(0);  // Not called.
-  RemoveWebContents(all_web_contents_[kMainTab + 1]);
+  RemoveWebContents(all_web_contents_[kMainTab + 1].get());
 }
 
 TEST_F(CurrentTabDesktopMediaListTest, OnSourceThumbnailCalledIfNewThumbnail) {
@@ -227,7 +209,7 @@ TEST_F(CurrentTabDesktopMediaListTest, OnSourceThumbnailCalledIfNewThumbnail) {
   EXPECT_CALL(observer_, OnSourceThumbnailChanged(0))
       .Times(1)
       .WillOnce(QuitMessageLoop(run_loop_.get()));
-  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab]);
+  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab].get());
   Wait();
 
   // Test focus.
@@ -247,7 +229,7 @@ TEST_F(CurrentTabDesktopMediaListTest,
   EXPECT_CALL(observer_, OnSourceThumbnailChanged(0))
       .Times(1)
       .WillOnce(QuitMessageLoop(run_loop_.get()));
-  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab]);
+  list_ = CreateCurrentTabDesktopMediaList(all_web_contents_[kMainTab].get());
   Wait();
 
   // Test focus.
@@ -257,7 +239,7 @@ TEST_F(CurrentTabDesktopMediaListTest,
 
 TEST_F(CurrentTabDesktopMediaListTest, CallingRefreshAfterTabFreedIsSafe) {
   constexpr size_t kMainTab = 3;
-  WebContents* const web_contents = all_web_contents_[kMainTab];
+  WebContents* const web_contents = all_web_contents_[kMainTab].get();
 
   // Setup.
   EXPECT_CALL(observer_, OnSourceAdded(0)).Times(1);
