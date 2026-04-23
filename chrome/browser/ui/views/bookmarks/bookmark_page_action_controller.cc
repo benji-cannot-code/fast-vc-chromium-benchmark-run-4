e914d2cfd0b9ab8dd844f646c53c1cfc1892944b
Antonio Sartori <antoniosartori@chromium.org>
2026-04-23 12:10:06 -0700

    Avoid continuously reparsing NTP url
    This CL is a refactoring which switches to using the GURL version of
    ChromeUINewTabURL and/or ChromeUINewPageURL in chrome/browser/ui/views/bookmarks, avoiding
    parsing GURL again and again.
    
    This CL was uploaded by git cl split.
    
    R=dljames@chromium.org
    
    Bug: b:503370195
    Change-Id: I65424e6749aaea2b7edb9074cfc25aa005b0da20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790671
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619673}

--- chrome/browser/ui/views/bookmarks/bookmark_page_action_controller.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_page_action_controller.cc
@@ -32,8 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool IsNTPUrl(const GURL& url) {
-  return url.spec() == chrome::kChromeUINewTabURL ||
-         url.spec() == chrome::kChromeUINewTabPageURL;
+  return url == chrome::ChromeUINewTabURLAsGURL() ||
+         url == chrome::ChromeUINewTabPageURLAsGURL();
 }
 
 }  // namespace
