84415d1e87547a5dcc47b5c780b986cdc1832c1b
Jason Hu <hujasonx@google.com>
2026-04-23 00:03:25 -0700

    [context-input] Prevent sending URLs from NTP realbox composebox
    Checks the select match type for the url, matching how the omnibox edit
    model works. If the query text begins with a URL, it will not be
    submitted (and the query text will be cleared). If the query text begins
    with non-URL content but contains a URL later, it will be sent as
    normal, matching the behaviour in the omnibox
    
    Change-Id: I7e07d2a5e7146fdb3742b17897ea2449c4e75452
    Bug: b:501169860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785125
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619353}

--- chrome/browser/resources/new_tab_page/ntp_searchbox.ts
+++ chrome/browser/resources/new_tab_page/ntp_searchbox.ts
@@ -540,7 +540,13 @@ export class NtpSearchboxElement extends NtpSearchboxElementBase implements
     chrome.histograms.recordBoolean(
         'ContextualSearch.AiModeButtonClick.NtpRealbox', true);
 
-    if (!this.composeboxEnabled || this.$.input.inputElement.value.trim()) {
+    const isSearch = this.selectedMatch?.isSearchType ?? true;
+    if (!isSearch) {
+      this.setInputText('');
+    }
+    const queryText = isSearch ? this.$.input.inputElement.value.trim() : '';
+
+    if (!this.composeboxEnabled || queryText) {
       const histogramName =
           'ContextualSearch.UserAction.SubmitQueryV2.NewTabPage';
       // LINT.IfChange(ContextualSearchContextState)
@@ -554,8 +560,7 @@ export class NtpSearchboxElement extends NtpSearchboxElementBase implements
 
       this.pageHandler().notifySessionStarted();
       this.pageHandler().submitQuery(
-          this.$.input.inputElement.value.trim(), e.detail.button,
-          false, /* altKey */
+          queryText, e.detail.button, false, /* altKey */
           e.detail.ctrlKey, e.detail.metaKey, e.detail.shiftKey);
     } else {
       this.openComposebox_();
