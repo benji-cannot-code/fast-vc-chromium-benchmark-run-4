ec240145fa7c518a4c2d92b5421071a3e7faf4f9
Antonio Sartori <antoniosartori@chromium.org>
2026-04-23 06:13:32 -0700

    Avoid continuously reparsing NTP url
    This CL is a refactoring which switches to using the GURL version of
    ChromeUINewTabURL and/or ChromeUINewPageURL in chrome/browser/ui/extensions, avoiding
    parsing GURL again and again.
    
    This CL was uploaded by git cl split.
    
    R=massan@google.com
    
    Bug: b:503370195
    Change-Id: I66d3e7ae029959cefae828cb630e114e6b811057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789372
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Masa Fujita <massan@google.com>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619464}

--- chrome/browser/ui/extensions/settings_overridden_params_providers_unittest.cc
+++ chrome/browser/ui/extensions/settings_overridden_params_providers_unittest.cc
@@ -57,7 +57,7 @@ class SettingsOverriddenParamsProvidersUnitTest
 
     registrar()->AddExtension(extension);
     EXPECT_EQ(extension, ExtensionUrlOverrides::GetExtensionControllingURL(
-                             GURL(chrome::kChromeUINewTabURL), profile()));
+                             chrome::ChromeUINewTabURLAsGURL(), profile()));
 
     return extension.get();
   }
