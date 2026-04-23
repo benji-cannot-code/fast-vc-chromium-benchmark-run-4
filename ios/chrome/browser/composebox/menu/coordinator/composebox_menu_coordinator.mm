c8e9fe7b97fbcbd9f9b7662f4436d7fce494a2c6
Radu Nitescu <radunitescu@google.com>
2026-04-23 09:57:20 -0700

    [iOS] Fix landscape composebox menu bottom sheet
    Before, the landscape presentation would be undismissable and presented
    fullscreen.
    
    Change-Id: Idda2e0550dc75a1a76d5e721d2903b08c11e8f36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790217
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619596}

--- ios/chrome/browser/composebox/menu/coordinator/composebox_menu_coordinator.mm
+++ ios/chrome/browser/composebox/menu/coordinator/composebox_menu_coordinator.mm
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   _viewController.sheetPresentationController.prefersGrabberVisible = YES;
   _viewController.sheetPresentationController.delegate = self;
+  _viewController.sheetPresentationController
+      .prefersEdgeAttachedInCompactHeight = YES;
 
   [self.baseViewController presentViewController:_viewController
                                         animated:YES
