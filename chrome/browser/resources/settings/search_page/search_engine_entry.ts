94fba6d6a6a3f2a87ffcf81182ef75be259aab49
Amelie Schneider <amelies@google.com>
2026-04-23 08:03:30 -0700

    [Search] Close context menu after clicking "Manage extension"
    Bug: 502831774
    Change-Id: I8c350529a341fec79757137358b401aa4fa59fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789394
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619523}

--- chrome/browser/resources/settings/search_page/search_engine_entry.ts
+++ chrome/browser/resources/settings/search_page/search_engine_entry.ts
@@ -206,6 +206,7 @@ export class SettingsSearchEngineEntryElement extends
 
   private onManageClick_() {
     assert(this.engine.extension);
+    this.closePopupMenu_();
     this.extensionBrowserProxy_.manageExtension(this.engine.extension.id);
   }
 
