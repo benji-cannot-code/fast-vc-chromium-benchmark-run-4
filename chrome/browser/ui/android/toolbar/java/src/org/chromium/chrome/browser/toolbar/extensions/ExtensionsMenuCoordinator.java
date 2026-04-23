65f63903908bd9a5a65b076966ef6f25fe69e6ff
Masa Fujita <massan@google.com>
2026-04-23 08:22:40 -0700

    extensions: Mitigate visual flash when opening the menu
    Opening the extensions menu resulted in a visual flash. This was caused
    by a race condition where the menu rendered stale data before the UI
    thread could clear it, combined with the default RecyclerView insertion
    animation.
    
    Bug: 499304598
    Change-Id: If2e9e3213def34b145a71a99af4ec33f806453fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783069
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619538}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
@@ -154,7 +154,8 @@ public class ExtensionsMenuCoordinator
                         return MenuBuilderHelper.getRectProvider(mExtensionsMenuButton);
                     }
                 },
-                /* overrideOnClickListener= */ false);
+                /* overrideOnClickListener= */ true);
+
         // Menu mediator is created when menu is triggered.
         mExtensionsMenuButton.setOnClickListener(
                 (view) -> {
@@ -172,6 +173,7 @@ public class ExtensionsMenuCoordinator
                     public void onPopupMenuDismissed() {
                         mMenuButtonPinningDelegate.requestLayoutWithViewUtils();
                         destroyMediator();
+                        mExtensionModels.clear();
                     }
                 });
 
@@ -213,9 +215,6 @@ public class ExtensionsMenuCoordinator
         if (mMediator != null) {
             return;
         }
-
-        // Clear old data before repopulating.
-        mExtensionModels.clear();
 
         // Ensure we start on the main page.
         mMainPageModel.set(
@@ -402,6 +401,8 @@ public class ExtensionsMenuCoordinator
 
         extensionRecyclerView.setAdapter(extensionsAdapter);
         extensionRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        extensionRecyclerView.setItemAnimator(null);
     }
 
     private void updateButtonState() {
