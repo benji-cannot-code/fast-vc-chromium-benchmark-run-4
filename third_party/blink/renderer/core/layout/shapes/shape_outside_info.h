127f34122a099cf29aac16645bf1c5155c0ce2bd
Fredrik Söderquist <fs@opera.com>
2026-04-23 04:26:58 -0700

    Merge ShapeOutsideInfo::{Block,Inline}StartOffset()
    Computing both is marginally more expensive[*] than computing one, and
    the callers often (or always) need both.
    
    [*] And it's only used by interfaces used by DevTools.
    
    Change-Id: I96438d5f6c5ebeab2dfb9408819aa781aab9e210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785162
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619428}

--- third_party/blink/renderer/core/layout/shapes/shape_outside_info.h
+++ third_party/blink/renderer/core/layout/shapes/shape_outside_info.h
@@ -89,8 +89,7 @@ class ShapeOutsideInfo final : public GarbageCollected<ShapeOutsideInfo> {
                                              WritingMode,
                                              float margin) const;
 
-  LayoutUnit BlockStartOffset() const;
-  LayoutUnit InlineStartOffset() const;
+  LogicalOffset LogicalStartOffset() const;
 
   using InfoMap =
       HeapHashMap<WeakMember<const LayoutBox>, Member<ShapeOutsideInfo>>;
