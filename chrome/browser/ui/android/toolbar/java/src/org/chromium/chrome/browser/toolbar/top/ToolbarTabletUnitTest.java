9c6062cb73397f9a5b5ec2bbeaad49612bf9c127
Sophie Crowley <crowleso@google.com>
2026-04-23 13:22:06 -0700

    [Signin][Android] Prioritise showing Signin button over Adaptive button
    With plans to always show the Signin button / profile disc on LFF
    devices rather than just on the NTP, the priority of the Signin button
    vs Adaptive button is now more relevant (previously generally only one
    or the other would appear so order did not matter). Signin button should
    take precedence if there is only space for one of the two.
    
    Note: We may need to further increase the Signin Button priority in
    future pending UI decisions, but at the very least for now it should be
    one rank up.
    
    Bug: 504927558
    Change-Id: Ic61299ddda9e863001e613e1c064f7d1dcbde433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7767791
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Sophie Crowley <crowleso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619736}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
@@ -923,7 +923,7 @@ public final class ToolbarTabletUnitTest {
                         padding + 7 * buttonWidth + locationBarMidWidth, EXACTLY),
                 UNSPECIFIED);
         assertToolbarComponentsReceivedWidth(
-                Set.of(PADDING, MENU, TAB_SWITCHER, BACK, INCOGNITO_INDICATOR, ADAPTIVE_BUTTON));
+                Set.of(PADDING, MENU, TAB_SWITCHER, BACK, INCOGNITO_INDICATOR, SIGNIN_BUTTON));
 
         mToolbarTablet.onMeasure(
                 MeasureSpec.makeMeasureSpec(
@@ -1150,7 +1150,7 @@ public final class ToolbarTabletUnitTest {
                         padding + 7 * buttonWidth + locationBarMidWidth, EXACTLY),
                 UNSPECIFIED);
         assertToolbarComponentsReceivedWidth(
-                Set.of(PADDING, MENU, TAB_SWITCHER, BACK, INCOGNITO_INDICATOR, ADAPTIVE_BUTTON));
+                Set.of(PADDING, MENU, TAB_SWITCHER, BACK, INCOGNITO_INDICATOR, SIGNIN_BUTTON));
 
         mToolbarTablet.onMeasure(
                 MeasureSpec.makeMeasureSpec(
