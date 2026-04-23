126d86c38b66bf5b9d6eab3c8163dac9f383a39e
xiaobincui <xiaobincui@microsoft.com>
2026-04-22 20:09:36 -0700

    [ios blink] Persist session cookies
    Fix initialization of SQLitePersistentCookieStore.
    
    Validation:
    1. Sign in any website that supports staying signed in.
    2. Restart browser app and revisit the same website.
    
    Before: need to sign in again.
    After: stay signed in.
    
    Bug: 40254930
    Change-Id: Ide6e14a5545b3c9abfcba90d1fb661624770bb8b
    Bypass-Check-License: This CL is created in the last year.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7104384
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Xiaobin Cui <xiaobincui@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1619296}

---
+++ ios/web/content/common/constants.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/web/content/common/constants.h"
+
+#define FPL FILE_PATH_LITERAL
+
+namespace web {
+
+const base::FilePath::CharType kCookieFilename[] = FPL("Cookies");
+const base::FilePath::CharType kNetworkDataDirname[] = FPL("Network");
+
+}  // namespace web
