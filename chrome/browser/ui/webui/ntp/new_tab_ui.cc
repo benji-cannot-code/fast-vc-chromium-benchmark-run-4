5518dd84f524117d6389ac21cf093eb70398130c
Antonio Sartori <antoniosartori@chromium.org>
2026-04-23 06:15:30 -0700

    Avoid continuously reparsing NTP url
    This CL is a refactoring which switches to using the GURL version of
    ChromeUINewTabURL and/or ChromeUINewPageURL in chrome/browser/ui/webui/ntp, avoiding
    parsing GURL again and again.
    
    This CL was uploaded by git cl split.
    
    R=mahmadi@chromium.org
    
    Bug: b:503370195
    Change-Id: I3a15b1479611604b95620c00835e46a9ee861819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790056
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619465}

--- chrome/browser/ui/webui/ntp/new_tab_ui.cc
+++ chrome/browser/ui/webui/ntp/new_tab_ui.cc
@@ -110,7 +110,7 @@ NewTabUI::~NewTabUI() = default;
 // static
 bool NewTabUI::IsNewTab(const GURL& url) {
   return url.DeprecatedGetOriginAsURL() ==
-         GURL(chrome::kChromeUINewTabURL).DeprecatedGetOriginAsURL();
+         chrome::ChromeUINewTabURLAsGURL().DeprecatedGetOriginAsURL();
 }
 
 // static
