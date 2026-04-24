357cdb3be68c13446fb8cf801c43493596e2497e
James Cook <jamescook@chromium.org>
2026-04-23 17:34:16 -0700

    extensions: Port ExtensionActionIconFactoryTest to desktop Android
    We missed this one during initial bring-up on desktop Android. The test
    passes without modification. Do a little cleanup while we're here.
    
    Bug: 469417243
    Change-Id: Ie6c73e306b8f0997abfe989da601c8d1bccced3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791196
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619862}

--- chrome/browser/extensions/extension_action_icon_factory_unittest.cc
+++ chrome/browser/extensions/extension_action_icon_factory_unittest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_action.h"
 #include "extensions/browser/extension_action_manager.h"
 #include "extensions/browser/extension_registrar.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/image_util.h"
 #include "extensions/grit/extensions_browser_resources.h"
@@ -44,10 +45,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user_manager_impl.h"
 #endif
 
-using extensions::mojom::ManifestLocation;
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 namespace {
+
+using ::extensions::mojom::ManifestLocation;
 
 bool ImageRepsAreEqual(const gfx::ImageSkiaRep& image_rep1,
                        const gfx::ImageSkiaRep& image_rep2) {
@@ -92,7 +95,7 @@ class ExtensionActionIconFactoryTest
     : public testing::Test,
       public ExtensionActionIconFactory::Observer {
  public:
-  ExtensionActionIconFactoryTest() : quit_in_icon_updated_(false) {}
+  ExtensionActionIconFactoryTest() = default;
 
   ExtensionActionIconFactoryTest(const ExtensionActionIconFactoryTest&) =
       delete;
@@ -172,7 +175,7 @@ class ExtensionActionIconFactoryTest
 
  private:
   content::BrowserTaskEnvironment task_environment_;
-  bool quit_in_icon_updated_;
+  bool quit_in_icon_updated_ = false;
   std::unique_ptr<TestingProfile> profile_;
   base::RunLoop loop_;
 
@@ -193,7 +196,7 @@ TEST_F(ExtensionActionIconFactoryTest, NoIcons) {
   // manifest and does not call |SetIcon| by default.
   scoped_refptr<Extension> extension(
       CreateExtension("browser_action/no_icon", ManifestLocation::kUnpacked));
-  ASSERT_TRUE(extension.get() != nullptr);
+  ASSERT_TRUE(extension);
   ExtensionAction* action = GetExtensionAction(*extension);
   ASSERT_TRUE(action);
   ASSERT_FALSE(action->default_icon());
@@ -251,7 +254,7 @@ TEST_F(ExtensionActionIconFactoryTest, AfterSetIcon) {
   // icon resource).
   scoped_refptr<Extension> extension(
       CreateExtension("browser_action/no_icon", ManifestLocation::kUnpacked));
-  ASSERT_TRUE(extension.get() != nullptr);
+  ASSERT_TRUE(extension);
   ExtensionAction* action = GetExtensionAction(*extension);
   ASSERT_TRUE(action);
   ASSERT_FALSE(action->default_icon());
@@ -288,7 +291,7 @@ TEST_F(ExtensionActionIconFactoryTest, DefaultIcon) {
   // icon resource).
   scoped_refptr<Extension> extension(
       CreateExtension("browser_action/no_icon", ManifestLocation::kUnpacked));
-  ASSERT_TRUE(extension.get() != nullptr);
+  ASSERT_TRUE(extension);
   ExtensionAction* action = GetExtensionAction(*extension);
   ASSERT_TRUE(action);
   ASSERT_FALSE(action->default_icon());
