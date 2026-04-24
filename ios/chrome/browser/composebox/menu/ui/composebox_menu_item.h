115c0a15bb38de171c592a8166f57db3798e1f54
Ameur Hosni <ameurhosni@google.com>
2026-04-24 04:37:43 -0700

    [IOS][AIM] Use a collection view instead of a table view
    screenshot: https://screenshot.googleplex.com/3ZE2Ng4MbsjJhDs
    
    Change-Id: I3b5d3399bf5a3a47b2dc3b9ddf37511c376d799f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789900
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620083}

--- ios/chrome/browser/composebox/menu/ui/composebox_menu_item.h
+++ ios/chrome/browser/composebox/menu/ui/composebox_menu_item.h
@@ -16,6 +16,10 @@ enum class ComposeboxMenuItemType {
   kCanvas,
   kModelAuto,
   kModelThinking,
+  kAttachmentTabs,
+  kAttachmentCamera,
+  kAttachmentGallery,
+  kAttachmentFiles,
 };
 
 // Represents a menu item in the Composebox menu.
