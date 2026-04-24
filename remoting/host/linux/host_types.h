5c716b6870d9ab8a660ed4b6830009323564d4c7
Yuwei Huang <yuweih@chromium.org>
2026-04-24 09:19:24 -0700

    [crd host][linux] Refactor start-host
    * Update DaemonController to take a HostType instead of the DelegateType
      enum
    * Update start-host to read the host type from the --host-type switch,
      find the HostType, then set it on DaemonController
    * Update start-host to print the supported host types
    * Unified handling of default host type
    * Moved some initialization code before creating the DaemonController,
      since the GetState() call requires a task runner.
    
    Bug: 502664397
    Change-Id: I9bcb2ad602f21b33d5caa3e6abffc9f74138a40b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790686
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620250}

--- remoting/host/linux/host_types.h
+++ remoting/host/linux/host_types.h
@@ -19,6 +19,9 @@ class HostType {
   static const base::flat_map<std::string_view, const HostType*>&
   GetHostTypes();
 
+  // Returns the default host type.
+  static const HostType& GetDefaultHostType();
+
   // Finds a host type by its name. Returns nullptr if not found.
   static const HostType* Find(std::string_view name);
 
@@ -26,6 +29,9 @@ class HostType {
   static void PrintHostTypeHelp();
 
   virtual ~HostType() = default;
+
+  // The name of the host type.
+  virtual std::string_view name() const = 0;
 
   // Human-readable description of the host type.
   virtual std::string_view description() const = 0;
