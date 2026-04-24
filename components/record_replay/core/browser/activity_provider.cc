c82d27b7de7dea911bed49edf9d83aa8e2395317
Adem Derinel <derinel@google.com>
2026-04-24 04:48:51 -0700

    RecordReplay: Remove ActivityHardcodedProvider
    The ActivityHardcodedProvider was a placeholder that always returned no
    activity. It is no longer needed and has been removed from the build and
    source files.
    
    Change-Id: I79a47beac6388383d59dabc702b5cc851d996c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7792664
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620087}

--- components/record_replay/core/browser/activity_provider.cc
+++ components/record_replay/core/browser/activity_provider.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/record_replay/core/browser/activity_provider.h"
 
 #include "base/command_line.h"
-#include "components/record_replay/core/browser/activity_hardcoded_provider.h"
 #include "components/record_replay/core/browser/file_activity_provider.h"
 #include "components/record_replay/core/common/record_replay_switches.h"
 
@@ -24,7 +23,6 @@ ActivityProvider::CreateProviders() {
     providers.push_back(std::make_unique<FileActivityProvider>(file_path));
   }
 
-  providers.push_back(std::make_unique<ActivityHardcodedProvider>());
   return providers;
 }
 
