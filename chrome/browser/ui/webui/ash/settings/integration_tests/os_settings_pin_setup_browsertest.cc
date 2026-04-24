4876bb3e910e73f89a6c4d5abfb8c5ec3f4af5f0
Fahad Mansoor <fahadmansoor@google.com>
2026-04-24 06:36:34 -0700

    LPP: Fix pin test setup with flags enabled
    - PinSetupScreen test was failing due to the fact it
    was specifically written for flag disabled behavior
    - OSSettingsPinSetupTest was failing where Pin was disabled
    as AllowedLocalAuthFactors policy was not disabled
    
    Bug: 498872357
    Test: browser_tests *OSSettingsPinSetupTest* *OSSettingsPinSetupTest*
    Change-Id: I8d4840774856513fe6ad7199717bb1a4637cc248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785891
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620135}

--- chrome/browser/ui/webui/ash/settings/integration_tests/os_settings_pin_setup_browsertest.cc
+++ chrome/browser/ui/webui/ash/settings/integration_tests/os_settings_pin_setup_browsertest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/core/common/policy_types.h"
+#include "components/policy/policy_constants.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
 
@@ -171,6 +172,11 @@ class OSSettingsPinSetupTest : public OSSettingsLockScreenBrowserTestBase,
     policies.Set("WebAuthnFactors", policy::POLICY_LEVEL_MANDATORY,
                  policy::POLICY_SCOPE_USER, policy::POLICY_SOURCE_CLOUD,
                  policy_value.Clone(),
+                 /*external_data_fetcher=*/nullptr);
+
+    policies.Set(policy::key::kAllowedLocalAuthFactors,
+                 policy::POLICY_LEVEL_MANDATORY, policy::POLICY_SCOPE_USER,
+                 policy::POLICY_SOURCE_CLOUD, policy_value.Clone(),
                  /*external_data_fetcher=*/nullptr);
 
     provider_.UpdateChromePolicy(policies);
