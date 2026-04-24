76c5868068b79b64e0596216371f714d33e9737f
James Lee <ljjlee@google.com>
2026-04-24 07:59:21 -0700

    Disable ServiceWorkerBackgroundSyncTest.Sync on Android desktop
    The test was recently added in https://crrev.com/c/7791447, but does not
    pass.
    
    Bug: 506096527
    Change-Id: I871c75b8d370f614954ee741e3a17a81776a4e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793190
    Owners-Override: James Lee <ljjlee@google.com>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620188}

--- chrome/browser/extensions/service_worker_apitest.cc
+++ chrome/browser/extensions/service_worker_apitest.cc
@@ -1668,6 +1668,8 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerBasedBackgroundTest, VerifyNoApiBindings) {
   EXPECT_TRUE(worker_listener.WaitUntilSatisfied());
 }
 
+// TODO(crbug.com/506096527): Port to desktop Android.
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 IN_PROC_BROWSER_TEST_F(ServiceWorkerBackgroundSyncTest, Sync) {
   const Extension* extension =
       LoadExtension(test_data_dir_.AppendASCII("service_worker/sync"));
@@ -1690,6 +1692,7 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerBackgroundSyncTest, Sync) {
   content::background_sync_test_util::SetOnline(web_contents, true);
   EXPECT_TRUE(sync_listener.WaitUntilSatisfied());
 }
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 IN_PROC_BROWSER_TEST_F(ServiceWorkerTest,
                        FetchFromContentScriptShouldNotGoToServiceWorkerOfPage) {
