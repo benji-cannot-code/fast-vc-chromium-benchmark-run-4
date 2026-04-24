ca4201d13e906452438f8a07b2da9806685b3d3d
Ernesto Izquierdo Clua <eic@google.com>
2026-04-24 08:01:59 -0700

    [ios] Create Objective-C observation bridge for DataProtectionTabHelper
    Adds an Objective-C bridge for DataProtectionTabHelper to allow
    Objective-C clients to observe data protection changes. A subsequent CL
    will use this bridge so DataProtectionSceneAgent can monitor and apply
    screenshot protection to the scene's window.
    
    Bug: 485585995
    Change-Id: I2b10fb167fae96768baa15d42e2aa6d1e00c0b49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790052
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Yixi Wu <yixiwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620190}

---
+++ ios/chrome/browser/enterprise/data_protection/model/data_protection_tab_helper_observer_bridge.mm
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/data_protection/model/data_protection_tab_helper_observer_bridge.h"
+
+DataProtectionTabHelperObserverBridge::DataProtectionTabHelperObserverBridge(
+    id<DataProtectionTabHelperObserving> observer,
+    DataProtectionTabHelper* tab_helper)
+    : observer_(observer) {
+  if (tab_helper) {
+    scoped_observation_.Observe(tab_helper);
+  }
+}
+
+DataProtectionTabHelperObserverBridge::
+    ~DataProtectionTabHelperObserverBridge() = default;
+
+void DataProtectionTabHelperObserverBridge::ScreenshotProtectionDidChange(
+    web::WebState* web_state,
+    bool screenshot_protection_enabled) {
+  if ([observer_ respondsToSelector:@selector
+                 (screenshotProtectionDidChangeForWebState:isProtected:)]) {
+    [observer_
+        screenshotProtectionDidChangeForWebState:web_state
+                                     isProtected:screenshot_protection_enabled];
+  }
+}
+
+void DataProtectionTabHelperObserverBridge::DataProtectionTabHelperDestroyed(
+    DataProtectionTabHelper* helper) {
+  scoped_observation_.Reset();
+}
