f2910817da959a89f87d0062898c4199b6c62414
Masa Fujita <massan@google.com>
2026-04-23 22:26:02 -0700

    Fix missing pressed state highlight on ListMenuButton
    When a `ListMenuButton` is clicked to open the menu, in some cases (e.g.
    extension context menus) the UI logic tries to clear the button's
    pressed state. Since we know that the state set in `setIsPressed()`
    should be the final state of the button, we can simply defer the call to
    `setPressed()` to ensure that button correctly maintains its active
    state.
    
    Bug: 505737201
    Change-Id: I0e8820ea5de697d3e7b86768aa8f330d9a3e5d3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791355
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619968}

--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuButton.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuButton.java
@@ -7,6 +7,8 @@ package org.chromium.ui.listmenu;
 
 import android.content.Context;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -27,6 +29,7 @@ import org.chromium.ui.widget.ChromeImageButton;
 @NullMarked
 public class ListMenuButton extends ChromeImageButton {
     private final ListMenuHost mListMenuHost;
+    private final Handler mHandler;
     private boolean mIsActive;
 
     private boolean mIsAttachedToWindowForTesting;
@@ -40,6 +43,7 @@ public class ListMenuButton extends ChromeImageButton {
     public ListMenuButton(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mListMenuHost = new ListMenuHost(this, attrs, this::setIsPressed);
+        mHandler = new Handler(Looper.getMainLooper());
     }
 
     /**
@@ -101,7 +105,13 @@ public class ListMenuButton extends ChromeImageButton {
     /** Store the active state to set 'pressed' style to the button when the menu is open. */
     public void setIsPressed(boolean active) {
         mIsActive = active;
-        setPressed(active);
+        mHandler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        setPressed(active);
+                    }
+                });
     }
 
     @Override
