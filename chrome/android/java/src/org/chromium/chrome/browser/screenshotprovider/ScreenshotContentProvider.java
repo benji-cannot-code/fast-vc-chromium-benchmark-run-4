564bf64470864611c1ef32025793070be473c826
Ivana Drašković <tivi@google.com>
2026-04-24 07:51:34 -0700

    Add a stub ScreenshotContentProvider.
    This change introduces a new ContentProvider,
    `ScreenshotContentProvider`, and its implementation,
    `ScreenshotContentProviderImpl`.
    
    The provider is declared in the AndroidManifest and added to the build.
    Currently, all methods in `ScreenshotContentProviderImpl` are stubs,
    returning null, zero, or throwing an exception. Unit tests are included
    to verify the stub behavior. The provider is set up using
    `SplitCompatContentProvider`.
    
    Bug: 493156887
    Change-Id: I97edee95db8fd83934725f8a26b70f397fe3f389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789558
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Ivana Drašković <tivi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620179}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/screenshotprovider/ScreenshotContentProvider.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.screenshotprovider;
+
+import org.chromium.build.annotations.IdentifierNameString;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.base.SplitCompatContentProvider;
+
+/** See {@link ScreenshotContentProviderImpl}. */
+@NullMarked
+public class ScreenshotContentProvider extends SplitCompatContentProvider {
+    @SuppressWarnings("FieldCanBeFinal") // @IdentifierNameString requires non-final
+    private static @IdentifierNameString String sImplClassName =
+            "org.chromium.chrome.browser.screenshotprovider.ScreenshotContentProviderImpl";
+
+    public ScreenshotContentProvider() {
+        super(sImplClassName);
+    }
+}
