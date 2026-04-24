f445c8b6e38725bf6c7a667619cbf3c8927aac51
Daniel Rubery <drubery@chromium.org>
2026-04-24 14:15:37 -0700

    [bedrock] Remove Browser* from cpu_health_tracker_unittest
    This test only uses the browser for holding web contents. We can replace
    that with a list of WebContents and deriving from
    ChromeRenderViewHostTestHarness.
    
    Bug: 506004823
    Link: https://chromium-review.googlesource.com/id/I3c86e7ade679dad165b22631828cb1ac6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791454
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620464}

--- chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h
+++ chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h
@@ -90,7 +90,7 @@ class PerformanceDetectionManager {
  private:
   friend class ::ChromeBrowserMainExtraPartsPerformanceManager;
   friend class PerformanceDetectionManagerTest;
-  friend class CpuHealthTrackerBrowserTest;
+  friend class CpuHealthTrackerActionabilityTest;
 
   PerformanceDetectionManager();
 
