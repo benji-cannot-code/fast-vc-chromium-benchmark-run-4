1d3ee647f9b49e6483ffedd2f30e01207fceb759
Emily Shack <emshack@chromium.org>
2026-04-23 18:13:38 -0700

    [GlowUp] Add rounded icons feature flag
    Bug: 498353249
    Change-Id: I20b095efa44b052702093e6b0b40a8bb135ccff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789815
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: Dominic Austria <dominicaustria@google.com>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619895}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4420,6 +4420,9 @@ inline constexpr char kToolbarGlowUpName[] = "Toolbar Glow Up";
 inline constexpr char kToolbarGlowUpDescription[] =
     "Enables the Toolbar Glow Up feature.";
 
+inline constexpr char kRoundedIconsName[] = "Rounded Icons";
+inline constexpr char kRoundedIconsDescription[] = "Enables rounded icons.";
+
 inline constexpr char kToolbarPhoneAnimationRefactorName[] =
     "Toolbar Phone Animation Refactor";
 inline constexpr char kToolbarPhoneAnimationRefactorDescription[] =
