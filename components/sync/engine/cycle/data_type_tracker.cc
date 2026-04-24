76f5176b53cd4e8498920c10cae9fb5a853d3484
Xinyi Ji <xinyiji@google.com>
2026-04-24 06:26:48 -0700

    [NTP Customization Sync] Add scaffold for THEMES_ANDROID data type
    This CL adds the initial scaffolding for the THEMES_ANDROID data type.
    
    - DataType enumeration entry.
    
    - TODOs for mapping to selectable types.
    
    - Histogram entries.
    
    Bug: 488439751
    Change-Id: Ic2435cd72f957978e0b676e0c5d751c46fe83a11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782613
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620128}

--- components/sync/engine/cycle/data_type_tracker.cc
+++ components/sync/engine/cycle/data_type_tracker.cc
@@ -120,6 +120,7 @@ base::TimeDelta GetDefaultLocalChangeNudgeDelay(DataType data_type) {
     case SKILL:
     case GEMINI_THREAD:
     case ACCESSIBILITY_ANNOTATION:
+    case THEMES_ANDROID:
       return kMediumLocalChangeNudgeDelay;
     case UNSPECIFIED:
       NOTREACHED();
@@ -198,6 +199,7 @@ bool CanGetCommitsFromExtensions(DataType data_type) {
     case SKILL:
     case GEMINI_THREAD:
     case ACCESSIBILITY_ANNOTATION:
+    case THEMES_ANDROID:
       return false;
     case UNSPECIFIED:
       NOTREACHED();
