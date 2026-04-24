af0bd0e657678a35e47ff0516ec86f156501233c
Daniel Rubery <drubery@chromium.org>
2026-04-24 14:52:29 -0700

    [bedrock] Remove Browser* from notification_platform_bridge_linux_unittest
    This test only uses BrowserWithTestWindowTest for its profile manager
    and task environment. This CL converts to a unit test by adding those to
    the test fixture.
    
    Bug: 506125891
    Link: https://chromium-review.googlesource.com/id/I769bf16cc80bf1afa1a048b94c883fbd6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791697
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620491}

--- chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/i18n/number_formatting.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/nix/xdg_util.h"
@@ -24,8 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/browser/notifications/notification_test_util.h"
 #include "chrome/common/notifications/notification_operation.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
-#include "components/dbus/thread_linux/dbus_thread_linux.h"
+#include "chrome/test/base/testing_browser_process.h"
+#include "chrome/test/base/testing_profile.h"
+#include "chrome/test/base/testing_profile_manager.h"
+#include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_utils.h"
 #include "dbus/mock_bus.h"
 #include "dbus/object_proxy.h"
@@ -415,17 +418,22 @@ class FakeNotificationProxy : public dbus::ObjectProxy {
 
 }  // namespace
 
-class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
+class NotificationPlatformBridgeLinuxTest : public testing::Test {
  public:
-  NotificationPlatformBridgeLinuxTest() = default;
+  NotificationPlatformBridgeLinuxTest()
+      : profile_manager_(TestingBrowserProcess::GetGlobal()) {}
   NotificationPlatformBridgeLinuxTest(
       const NotificationPlatformBridgeLinuxTest&) = delete;
   NotificationPlatformBridgeLinuxTest& operator=(
       const NotificationPlatformBridgeLinuxTest&) = delete;
   ~NotificationPlatformBridgeLinuxTest() override = default;
 
+  Profile* profile() { return profile_; }
+
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
+    testing::Test::SetUp();
+    ASSERT_TRUE(profile_manager_.SetUp());
+    profile_ = profile_manager_.CreateTestingProfile("Default");
     display_service_tester_ =
         std::make_unique<NotificationDisplayServiceTester>(profile());
     display_service_tester_->SetProcessNotificationOperationDelegate(
@@ -447,7 +455,6 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
     fake_dbus_proxy_.reset();
     fake_notification_proxy_ = nullptr;
     mock_bus_ = nullptr;
-    BrowserWithTestWindowTest::TearDown();
   }
 
   void HandleOperation(NotificationOperation operation,
@@ -534,6 +541,7 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
     EXPECT_EQ(test_params_.expect_init_success, success);
   }
 
+  content::BrowserTaskEnvironment task_environment_;
   scoped_refptr<dbus::MockBus> mock_bus_;
   scoped_refptr<FakeDBusProxy> fake_dbus_proxy_;
   scoped_refptr<FakeNotificationProxy> fake_notification_proxy_;
@@ -546,6 +554,8 @@ class NotificationPlatformBridgeLinuxTest : public BrowserWithTestWindowTest {
   std::optional<std::u16string> last_reply_;
   TestParams test_params_;
   bool ready_called_ = false;
+  TestingProfileManager profile_manager_;
+  raw_ptr<TestingProfile> profile_ = nullptr;
 };
 
 TEST_F(NotificationPlatformBridgeLinuxTest, SetUpAndTearDown) {
