8965c8b22c653bd502b5128dcbe471bc4088572c
Ken Buchanan <kenrb@chromium.org>
2026-04-24 16:06:24 -0700

    [WebAuthn] Add unit tests for AmbientSigninController
    Add basic unit tests for AmbientSigninController.
    
    These only test the Page Action cases. The bubble code will probably
    be removed in future as UX requirements become better defined.
    
    Written using Gemini CLI.
    
    Bug: 358119268
    Change-Id: If3326ebc58670a4df0763c22c4487d66ee12999e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790003
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620536}

--- chrome/browser/ui/webauthn/ambient/ambient_signin_controller.h
+++ chrome/browser/ui/webauthn/ambient/ambient_signin_controller.h
@@ -79,6 +79,9 @@ class AmbientSigninController
   base::OnceClosure GetSignInCallback();
   void OnBubbleViewDestroyed();
 
+  void SetPageActionControllerForTesting(
+      page_actions::PageActionController* controller);
+
   base::WeakPtr<AmbientSigninController> GetWeakPtr();
 
  private:
@@ -120,6 +123,9 @@ class AmbientSigninController
   // Set when `Show()` is called. Retains the UI type until `Show()` is called
   // again.
   UiType ui_type_ = UiType::kNone;
+
+  raw_ptr<page_actions::PageActionController>
+      page_action_controller_test_override_ = nullptr;
 
   base::WeakPtrFactory<AmbientSigninController> weak_ptr_factory_{this};
 };
