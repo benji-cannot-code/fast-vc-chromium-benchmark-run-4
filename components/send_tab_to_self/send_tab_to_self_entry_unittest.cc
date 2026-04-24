ffb463cf53e83c4cb2ba33387022dd904b24e5d3
Michael Tatarski <mtatarski@google.com>
2026-04-24 09:58:25 -0700

    [STTS] Extract duplicated test matcher to test_matchers.h
    This CL addresses a TODO to introduce a shared header for test matchers
    in components/send_tab_to_self.
    
    The `MatchesFormField` matcher was duplicated in:
    - outgoing_tab_form_field_extractor_unittest.cc
    - send_tab_to_self_entry_unittest.cc
    
    Bug: 485145029
    Change-Id: I15c4319ef1e2bfe8e7d395909a1816d4bf88dfdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7792760
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620286}

--- components/send_tab_to_self/send_tab_to_self_entry_unittest.cc
+++ components/send_tab_to_self/send_tab_to_self_entry_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/send_tab_to_self/features.h"
 #include "components/send_tab_to_self/page_context.h"
 #include "components/send_tab_to_self/proto/send_tab_to_self.pb.h"
+#include "components/send_tab_to_self/test_matchers.h"
 #include "components/sessions/core/serialized_navigation_entry.h"
 #include "components/sessions/core/serialized_navigation_entry_test_helper.h"
 #include "components/sessions/core/session_constants.h"
@@ -37,49 +38,6 @@ using ::testing::IsEmpty;
 using ::testing::Not;
 using ::testing::Pointee;
 using ::testing::Property;
-
-MATCHER_P2(MatchesAutofillSignature, form_signature, field_signature, "") {
-  return testing::ExplainMatchResult(
-             Field("form_signature",
-                   &PageContext::FormFieldAutofillSignature::form_signature,
-                   form_signature),
-             arg, result_listener) &&
-         testing::ExplainMatchResult(
-             Field("field_signature",
-                   &PageContext::FormFieldAutofillSignature::field_signature,
-                   field_signature),
-             arg, result_listener);
-}
-
-MATCHER_P5(MatchesFormField,
-           id_attribute,
-           name_attribute,
-           form_control_type,
-           value,
-           autofill_signature,
-           "") {
-  return testing::ExplainMatchResult(
-             Field("id_attribute", &PageContext::FormField::id_attribute,
-                   id_attribute),
-             arg, result_listener) &&
-         testing::ExplainMatchResult(
-             Field("name_attribute", &PageContext::FormField::name_attribute,
-                   name_attribute),
-             arg, result_listener) &&
-         testing::ExplainMatchResult(
-             Field("form_control_type",
-                   &PageContext::FormField::form_control_type,
-                   form_control_type),
-             arg, result_listener) &&
-         testing::ExplainMatchResult(
-             Field("value", &PageContext::FormField::value, value), arg,
-             result_listener) &&
-         testing::ExplainMatchResult(
-             Field("autofill_signature",
-                   &PageContext::FormField::autofill_signature,
-                   autofill_signature),
-             arg, result_listener);
-}
 
 MATCHER_P(MatchesPageContext, fields_matcher, "") {
   return testing::ExplainMatchResult(
