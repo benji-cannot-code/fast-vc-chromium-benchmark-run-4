4de581e4442aab1b33fb6ea2af346cf994210e4e
Lusa Zhan <lusazhan@chromium.org>
2026-04-24 07:04:37 -0700

    fix include in form_mcp_schema
    Bug: 485810761
    Change-Id: Ib804927f37739dce0c8651fcec037c0cee6e18c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790692
    Commit-Queue: Lusa Zhan <lusazhan@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620150}

--- third_party/blink/renderer/core/html/forms/form_mcp_schema.cc
+++ third_party/blink/renderer/core/html/forms/form_mcp_schema.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/files/file_path.h"
-#include "form_mcp_schema.h"
 #include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-blink.h"
 #include "third_party/blink/public/mojom/forms/form_control_type.mojom-blink-forward.h"
 #include "third_party/blink/public/platform/file_path_conversion.h"
@@ -22,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/custom/custom_element.h"
 #include "third_party/blink/renderer/core/html/custom/element_internals.h"
 #include "third_party/blink/renderer/core/html/forms/base_text_input_type.h"
+#include "third_party/blink/renderer/core/html/forms/form_mcp_schema.h"
 #include "third_party/blink/renderer/core/html/forms/html_field_set_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
