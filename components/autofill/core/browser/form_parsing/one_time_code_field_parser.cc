1b6ad5478ee55d9274e0a4aa24d379bfb3b12da5
Matthias Koerber <koerber@google.com>
2026-04-24 00:18:18 -0700

    [OTP Regex] Implement OneTimeCodeFieldParser
    This CL implements the OneTimeCodeFieldParser class and includes a new
    unit test suite to verify correct classification.
    
    This CL is not yet integrating the parser into the heuristic pass.
    
    Bug: 493119015, 505628560
    Change-Id: Ieaab11354ec89b21daec07c506439466a15c9402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779871
    Auto-Submit: Matthias Körber <koerber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1619993}

---
+++ components/autofill/core/browser/form_parsing/one_time_code_field_parser.cc
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/form_parsing/one_time_code_field_parser.h"
+
+#include "base/feature_list.h"
+
+#include "components/autofill/core/browser/form_parsing/autofill_scanner.h"
+#include "components/autofill/core/common/autofill_features.h"
+
+namespace autofill {
+
+// static
+std::unique_ptr<FormFieldParser> OneTimeCodeFieldParser::Parse(
+    ParsingContext& context,
+    AutofillScanner& scanner) {
+  if (!base::FeatureList::IsEnabled(
+          features::kAutofillEnableOneTimeCodeHeuristics)) {
+    return nullptr;
+  }
+
+  std::optional<FieldAndMatchInfo> match;
+  if (ParseField(context, scanner, "ONE_TIME_CODE", &match)) {
+    return std::make_unique<OneTimeCodeFieldParser>(std::move(*match));
+  }
+  return nullptr;
+}
+
+OneTimeCodeFieldParser::OneTimeCodeFieldParser(FieldAndMatchInfo match)
+    : match_(std::move(match)) {}
+
+void OneTimeCodeFieldParser::AddClassifications(
+    FieldCandidatesMap& field_candidates) const {
+  AddClassification(match_, ONE_TIME_CODE, kBaseOneTimeCodeParserScore,
+                    field_candidates);
+}
+
+}  // namespace autofill
