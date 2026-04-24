177c03deec2672952456ff30ed523bb45f32cd37
Michael Tatarski <mtatarski@google.com>
2026-04-24 08:13:23 -0700

    [STTS] Adopt strong signature types for forms and fields
    This CL adopts `autofill::FormSignature` and `autofill::FieldSignature`
    for type safety in `components/send_tab_to_self`, addressing a TODO.
    
    It also moves the `Signature` struct from `ReceivedTabFormsFiller` to
    `PageContext` as `PageContext::AutofillSignature` as suggested in the
    TODO, making it available for wider use if needed.
    
    Bug: 485145029
    Change-Id: Iea080fcb1e52bfb176d74f92e5c5293fb268d626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791843
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620200}

--- components/send_tab_to_self/page_context.h
+++ components/send_tab_to_self/page_context.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "components/autofill/core/common/signatures.h"
+
 namespace shared_highlighting {
 class TextFragment;
 }  // namespace shared_highlighting
@@ -70,6 +72,14 @@ struct ScrollPosition {
 };
 
 struct PageContext {
+  // Represents a combination of form and field signatures for identification.
+  struct FormFieldAutofillSignature {
+    autofill::FormSignature form_signature;
+    autofill::FieldSignature field_signature;
+
+    auto operator<=>(const FormFieldAutofillSignature& other) const = default;
+  };
+
   // Represents a single form field and its value.
   struct FormField {
     FormField();
@@ -83,8 +93,7 @@ struct PageContext {
     std::u16string name_attribute;
     std::string form_control_type;
     std::u16string value;
-    uint64_t form_signature = 0;
-    uint32_t field_signature = 0;
+    FormFieldAutofillSignature autofill_signature;
 
     bool operator==(const FormField& other) const;
   };
--- components/send_tab_to_self/send_tab_to_self_entry_unittest.cc
+++ components/send_tab_to_self/send_tab_to_self_entry_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/gtest_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/simple_test_tick_clock.h"
+#include "components/autofill/core/common/signatures.h"
 #include "components/send_tab_to_self/features.h"
 #include "components/send_tab_to_self/page_context.h"
 #include "components/send_tab_to_self/proto/send_tab_to_self.pb.h"
@@ -37,13 +38,25 @@ using ::testing::Not;
 using ::testing::Pointee;
 using ::testing::Property;
 
-MATCHER_P6(MatchesFormField,
+MATCHER_P2(MatchesAutofillSignature, form_signature, field_signature, "") {
+  return testing::ExplainMatchResult(
+             Field("form_signature",
+                   &PageContext::FormFieldAutofillSignature::form_signature,
+                   form_signature),
+             arg, result_listener) &&
+         testing::ExplainMatchResult(
+             Field("field_signature",
+                   &PageContext::FormFieldAutofillSignature::field_signature,
+                   field_signature),
+             arg, result_listener);
+}
+
+MATCHER_P5(MatchesFormField,
            id_attribute,
            name_attribute,
            form_control_type,
            value,
-           form_signature,
-           field_signature,
+           autofill_signature,
            "") {
   return testing::ExplainMatchResult(
              Field("id_attribute", &PageContext::FormField::id_attribute,
@@ -62,12 +75,9 @@ MATCHER_P6(MatchesFormField,
              Field("value", &PageContext::FormField::value, value), arg,
              result_listener) &&
          testing::ExplainMatchResult(
-             Field("form_signature", &PageContext::FormField::form_signature,
-                   form_signature),
-             arg, result_listener) &&
-         testing::ExplainMatchResult(
-             Field("field_signature", &PageContext::FormField::field_signature,
-                   field_signature),
+             Field("autofill_signature",
+                   &PageContext::FormField::autofill_signature,
+                   autofill_signature),
              arg, result_listener);
 }
 
@@ -292,8 +302,8 @@ TEST(SendTabToSelfEntry, MarkAsOpened) {
 TEST(SendTabToSelfEntry, PageContextRoundTrip) {
   PageContext context;
   PageContext::FormField field = MakeFormField(u"id1", u"value1");
-  field.form_signature = 12345;
-  field.field_signature = 6789;
+  field.autofill_signature.form_signature = autofill::FormSignature(12345u);
+  field.autofill_signature.field_signature = autofill::FieldSignature(6789u);
 
   context.form_field_info.fields.push_back(std::move(field));
 
@@ -310,8 +320,10 @@ TEST(SendTabToSelfEntry, PageContextRoundTrip) {
       restored,
       Pointee(MatchesEntry(
           _, _, _, _, _,
-          MatchesPageContext(ElementsAre(
-              MatchesFormField(u"id1", _, _, u"value1", 12345u, 6789u))),
+          MatchesPageContext(ElementsAre(MatchesFormField(
+              Eq(u"id1"), _, _, Eq(u"value1"),
+              MatchesAutofillSignature(autofill::FormSignature(12345u),
+                                       autofill::FieldSignature(6789u))))),
           MatchesNavigationHistory(IsEmpty(), testing::Eq(std::nullopt)))));
 }
 
