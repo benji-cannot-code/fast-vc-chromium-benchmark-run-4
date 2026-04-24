040fc9d9fb3fd541e38d189a3d66192b10061b96
Bruno Braga <brunobraga@google.com>
2026-04-24 04:40:42 -0700

    [Autofill AI - M4] Fix consent logging string on the settings page
    This CL also improves the tests, specifically:
    
    1. Makes testAutofillAiEntities_opensEditorOnAddClick more realistic
    but not testing the snackbar on a local entity.
    2. Specifically check the string ids used. This guarantees that if
    the "Done" string is removed from the xml, this test breaks.
    
    Bug: 467563385
    Change-Id: I15f287c42d79ad3d90ff039c22d32fdddaf6f076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785692
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620086}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
@@ -43,6 +43,7 @@ import org.chromium.chrome.browser.autofill.editors.address.AddressEditorCoordin
 import org.chromium.chrome.browser.autofill.editors.address.EditorDialogView;
 import org.chromium.chrome.browser.autofill.editors.autofill_ai.EntityEditorCoordinator;
 import org.chromium.chrome.browser.autofill.editors.common.EditorObserverForTest;
+import org.chromium.chrome.browser.autofill.editors.common.EditorViewBase;
 import org.chromium.chrome.browser.autofill.options.AutofillOptionsFragment;
 import org.chromium.chrome.browser.autofill.options.AutofillOptionsFragment.AutofillOptionsReferrer;
 import org.chromium.chrome.browser.customtabs.CustomTabActivity;
@@ -611,7 +612,8 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
 
     public static void setObserverForTest(EditorObserverForTest observerForTest) {
         sObserverForTest = observerForTest;
-        EditorDialogView.setEditorObserverForTest(sObserverForTest);
+        EditorDialogView.setEditorObserverForTest(sObserverForTest); // IN-TEST
+        EditorViewBase.setEditorObserverForTest(sObserverForTest); // IN-TEST
         ResettersForTesting.register(() -> sObserverForTest = null);
     }
 
--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragmentTest.java
@@ -30,7 +30,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -1450,7 +1450,7 @@ public class AutofillProfilesFragmentTest {
     @Test
     @MediumTest
     @Feature({"Preferences"})
-    public void testAutofillAiEntities_opensEditorOnAddClick() throws Exception {
+    public void testAutofillAiEntities_opensEditorOnAddClickForLocalEntity() throws Exception {
         EntityType vehicleType = TestUtils.getVehicleEntityType();
 
         LinkedHashMap<EntityType, List<EntityInstanceWithLabels>> instancesMap =
@@ -1477,7 +1477,59 @@ public class AutofillProfilesFragmentTest {
                             return category.findPreference("Vehicle" + " Add");
                         });
         assertNotNull(addVehicle);
+        int callCount = rule.mClickUpdate.getCallCount();
         ThreadUtils.runOnUiThreadBlocking(addVehicle::performClick);
+        rule.mClickUpdate.waitForCallback(callCount);
+
+        onView(withText("Add Vehicle")).check(matches(isDisplayed()));
+
+        // Click the "Done" button.
+        onView(withText("Done")).perform(click());
+        verify(sEntityDataManager)
+                .addOrUpdateEntityInstance(
+                        any(),
+                        eq(R.string.autofill_ai_save_or_update_local_entity_source_notice),
+                        eq(R.string.done),
+                        any());
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"Preferences"})
+    public void testAutofillAiEntities_opensEditorOnAddClickForWalletEntity() throws Exception {
+        EntityType vehicleType =
+                TestUtils.getVehicleEntityType(
+                        /* isReadOnly= */ false,
+                        /* isEnabled= */ true,
+                        /* isEligibleForWalletStorage= */ true);
+
+        LinkedHashMap<EntityType, List<EntityInstanceWithLabels>> instancesMap =
+                new LinkedHashMap<>();
+        instancesMap.put(vehicleType, Collections.emptyList());
+
+        when(sEntityDataManager.getInstancesToList()).thenReturn(instancesMap);
+        when(sEntityDataManager.isEligibleToAutofillAi()).thenReturn(true);
+        when(sEntityDataManager.getAutofillAiOptInStatus()).thenReturn(true);
+        when(sEntityDataManager.canEnableOrDisableAutofillAi()).thenReturn(true);
+        EntityDataManagerFactory.setInstanceForTesting(sEntityDataManager);
+
+        // Trigger a rebuild of the profile list to pick up the new mock entities.
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> sSettingsActivityTestRule.getFragment().onPersonalDataChanged());
+
+        Preference addVehicle =
+                ThreadUtils.runOnUiThreadBlocking(
+                        () -> {
+                            PreferenceCategory category =
+                                    sSettingsActivityTestRule
+                                            .getFragment()
+                                            .findPreference("Vehicle");
+                            return category.findPreference("Vehicle" + " Add");
+                        });
+        assertNotNull(addVehicle);
+        int callCount = rule.mClickUpdate.getCallCount();
+        ThreadUtils.runOnUiThreadBlocking(addVehicle::performClick);
+        rule.mClickUpdate.waitForCallback(callCount);
 
         onView(withText("Add Vehicle")).check(matches(isDisplayed()));
 
@@ -1487,7 +1539,10 @@ public class AutofillProfilesFragmentTest {
         ArgumentCaptor<Runnable> localSaveFallbackCaptor = ArgumentCaptor.forClass(Runnable.class);
         verify(sEntityDataManager)
                 .addOrUpdateEntityInstance(
-                        any(), anyInt(), anyInt(), localSaveFallbackCaptor.capture());
+                        any(),
+                        eq(R.string.autofill_ai_save_or_update_entity_in_wallet_source_notice),
+                        eq(R.string.done),
+                        localSaveFallbackCaptor.capture());
 
         ThreadUtils.runOnUiThreadBlocking(() -> localSaveFallbackCaptor.getValue().run());
 
@@ -1595,7 +1650,9 @@ public class AutofillProfilesFragmentTest {
                             return category.findPreference("Vehicle" + " Add");
                         });
         assertNotNull(addVehicle);
+        int callCount = rule.mClickUpdate.getCallCount();
         ThreadUtils.runOnUiThreadBlocking(addVehicle::performClick);
+        rule.mClickUpdate.waitForCallback(callCount);
 
         onView(withText("Add Vehicle")).check(matches(isDisplayed()));
 
@@ -1641,7 +1698,9 @@ public class AutofillProfilesFragmentTest {
                             return category.findPreference("Vehicle" + " Add");
                         });
         assertNotNull(addVehicle);
+        int callCount = rule.mClickUpdate.getCallCount();
         ThreadUtils.runOnUiThreadBlocking(addVehicle::performClick);
+        rule.mClickUpdate.waitForCallback(callCount);
 
         onView(withText("Add Vehicle")).check(matches(isDisplayed()));
 
