868ef4b42b05f3abfc22c31108d93775d8b1500c
Leo Zhao <leozhao@google.com>
2026-04-24 07:37:25 -0700

    [iOS][Forms AI] Make three feature flags for Autofill AI configurable
    This CL makes three feature flags that currently are supplied by using
    extra flags available in chrome://flags. We recently discovered that for
    jumping directly to a private pass in Google Wallet web by tapping Edit
    button in the app, kAutofillAiWalletPrivatePassesDeepLink has to be
    enabled. As part of the work to make that available to chrome://flags,
    two other flags that were missing are added too to facilitate developing
    and testing private passes.
    
    Fix: 505357712
    Change-Id: Idb6c1adf87b128df2fd215c0a9b9475e7b4b90cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790632
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620170}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -75,6 +75,9 @@ extern const char kAssistantSidePanelDescription[];
 extern const char kAutofillAcrossIframesName[];
 extern const char kAutofillAcrossIframesDescription[];
 
+extern const char kAutofillAiAvailableByDefaultName[];
+extern const char kAutofillAiAvailableByDefaultDescription[];
+
 extern const char kAutofillAiCreateEntityDataManagerName[];
 extern const char kAutofillAiCreateEntityDataManagerDescription[];
 
@@ -92,6 +95,9 @@ extern const char kAutofillAiValuablesIPHDescription[];
 
 extern const char kAutofillAiWalletFlightReservationName[];
 extern const char kAutofillAiWalletFlightReservationDescription[];
+
+extern const char kAutofillAiWalletPrivatePassesDeepLinkName[];
+extern const char kAutofillAiWalletPrivatePassesDeepLinkDescription[];
 
 extern const char kAutofillAiWalletPrivatePassesName[];
 extern const char kAutofillAiWalletPrivatePassesDescription[];
@@ -1114,6 +1120,9 @@ extern const char kViewCertificateInformationDescription[];
 
 extern const char kWaitThresholdMillisecondsForCapabilitiesApiName[];
 extern const char kWaitThresholdMillisecondsForCapabilitiesApiDescription[];
+
+extern const char kWalletApiPrivatePassesEnabledName[];
+extern const char kWalletApiPrivatePassesEnabledDescription[];
 
 extern const char kWalletServiceUseSandboxName[];
 extern const char kWalletServiceUseSandboxDescription[];
