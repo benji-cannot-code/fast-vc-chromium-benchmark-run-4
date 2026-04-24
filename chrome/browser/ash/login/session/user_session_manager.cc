442131c27c3f15b81b2b401cd51e94faad54ad1c
Mahmoud Rashad <mmrashad@google.com>
2026-04-24 10:33:22 -0700

    Add IsEstimateNewSignInUsersWithFinchAvailablePopulationEnabled
    This CL introduces a new UMA histogram to measure the population size of
    new users signing into their accounts on ChromeOS who have Finch
    configs.
    
    Fixed: 506110398
    Change-Id: I36f8a6e8eb4ad6e096b112aff0965b6c885545c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789962
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620313}

--- chrome/browser/ash/login/session/user_session_manager.cc
+++ chrome/browser/ash/login/session/user_session_manager.cc
@@ -183,6 +183,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/tribool.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
 #include "components/supervised_user/core/browser/child_account_service.h"
+#include "components/sync/base/features.h"
 #include "components/trusted_vault/trusted_vault_client.h"
 #include "components/trusted_vault/trusted_vault_service.h"
 #include "components/user_manager/common_types.h"
@@ -1603,6 +1604,13 @@ void UserSessionManager::InitProfilePreferences(
         accounts_mutator->SeedAccountInfo(gaia_id, user->GetDisplayEmail());
 
     // 3. Set it as the Primary Account.
+    if (is_new_profile) {
+      base::UmaHistogramBoolean(
+          "Signin.ChromeOS."
+          "IsEstimateNewSignInUsersWithFinchAvailablePopulationEnabled",
+          base::FeatureList::IsEnabled(
+              syncer::kEstimateNewSignInUsersWithFinchAvailablePopulation));
+    }
     const signin::PrimaryAccountMutator::PrimaryAccountError
         set_account_result =
             identity_manager->GetPrimaryAccountMutator()->SetPrimaryAccount(
