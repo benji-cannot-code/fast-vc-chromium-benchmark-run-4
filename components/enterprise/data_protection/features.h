4fe6da7d44a7e07d7fd67e49dae10b903a2b7cf6
Nasser Al-shawwa <alshawwa@chromium.org>
2026-04-24 14:23:23 -0700

    Add EnableDeepScanVerdictCacheSize feature flag
    Bug: 498626434
    Change-Id: I06c81a25e213b7c9074eccdc7ebf3ef970bc4eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793467
    Reviewed-by: Shanthanu (Shanth) Bhardwaj <xanth@google.com>
    Commit-Queue: Shanthanu (Shanth) Bhardwaj <xanth@google.com>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620468}

---
+++ components/enterprise/data_protection/features.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_DATA_PROTECTION_FEATURES_H_
+#define COMPONENTS_ENTERPRISE_DATA_PROTECTION_FEATURES_H_
+#include "base/feature_list.h"
+
+namespace enterprise_data_protection {
+
+BASE_DECLARE_FEATURE(kEnableDeepScanVerdictCacheSize);
+
+}  // namespace enterprise_data_protection
+
+#endif  // COMPONENTS_ENTERPRISE_DATA_PROTECTION_FEATURES_H_
