2a74f65d67839c92f1162a0d20cf0105432cef63
jingjinggu <jingjinggu@microsoft.com>
2026-04-24 07:56:18 -0700

    [customtabs] Clean up launched CCTMultipleParallelRequests feature flag
    The CCTMultipleParallelRequests flag has been enabled by default.
    Remove the flag and its dead code guard in maybeStartParallelRequest()
    where the flag-off check was always false.
    
    Bug: 505947416
    Change-Id: I0c51ca6ead25039a9c4881bb47cf3c266e51f3d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790520
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: jingjing gu <jingjinggu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1620184}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/DetachedResourceRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/DetachedResourceRequestTest.java
@@ -328,8 +328,7 @@ public class DetachedResourceRequestTest {
     @Test
     @SmallTest
     @EnableFeatures({
-        ChromeFeatureList.CCT_REPORT_PARALLEL_REQUEST_STATUS,
-        ChromeFeatureList.CCT_MULTIPLE_PARALLEL_REQUESTS
+        ChromeFeatureList.CCT_REPORT_PARALLEL_REQUEST_STATUS
     })
     public void testMultipleParallelRequestCompletionSuccessCallbacks() throws Exception {
         var histogram =
