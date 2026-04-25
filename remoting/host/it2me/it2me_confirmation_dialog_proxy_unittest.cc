e9721588569688e16d47adbe521ba2061d7b4c7f
Joe Downing <joedow@google.com>
2026-04-24 18:20:34 -0700

    remoting: Modernize MOCK_METHOD macros
    This change converts the old MOCK_METHODN() and MOCK_CONST_METHODN()
    macros to the new MOCK_METHOD() style across the //remoting directory.
    This aligns with modern GoogleTest/GoogleMock standards.
    
    Bug: None
    Change-Id: If71e8a8ef1b03849a3cc88b607d377e98a5d73e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794371
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620597}

--- remoting/host/it2me/it2me_confirmation_dialog_proxy_unittest.cc
+++ remoting/host/it2me/it2me_confirmation_dialog_proxy_unittest.cc
@@ -39,7 +39,7 @@ class StubIt2MeConfirmationDialog : public It2MeConfirmationDialog {
     std::move(callback_).Run(result);
   }
 
-  MOCK_METHOD0(OnShow, void());
+  MOCK_METHOD(void, OnShow, ());
 
   // It2MeConfirmationDialog implementation.
   void Show(const std::string& remote_user_email,
@@ -63,7 +63,7 @@ class ResultCallbackTarget {
       scoped_refptr<base::SingleThreadTaskRunner> task_runner)
       : task_runner_(task_runner) {}
 
-  MOCK_METHOD1(OnDialogResult, void(It2MeConfirmationDialog::Result));
+  MOCK_METHOD(void, OnDialogResult, (It2MeConfirmationDialog::Result));
 
   It2MeConfirmationDialog::ResultCallback MakeCallback() {
     return base::BindOnce(&ResultCallbackTarget::HandleDialogResult,
