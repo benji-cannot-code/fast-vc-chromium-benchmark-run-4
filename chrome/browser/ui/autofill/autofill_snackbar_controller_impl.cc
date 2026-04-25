81ad1d4d5275c752f5a366a76feb9c5de6cb6009
Norge Vizcay <vizcay@google.com>
2026-04-25 16:29:47 -0700

    [Autofill] Remove PlusAddressEmailOverride logic
    This CL cleans up the remaining references to PlusAddressEmailOverride
    in the snackbar controller (Android), iOS specific files, and
    histograms.
    
    Specifically:
    - Removed kPlusAddressEmailOverride from AutofillSnackbarType.
    - Removed handling in AutofillSnackbarControllerImpl.
    - Removed associated test in
      autofill_snackbar_controller_impl_unittest.cc.
    - Removed showPlusAddressEmailOverrideNotification from AutofillAgent
      and cwv_autofill_controller.
    - Removed the method from AutofillClientIOSBridge protocol.
    - Removed the variant from histograms.xml.
    - Removed unused strings from plus_addresses_strings.grd.
    - Renumbered enum values in AutofillSnackbarType to close the gap.
    
    Bug: 503268023
    Change-Id: I3d3ae71a10e679ae296f5dd608010026c689b41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789840
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620738}

--- chrome/browser/ui/autofill/autofill_snackbar_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_snackbar_controller_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/keyboard_accessory/android/manual_filling_controller_impl.h"
 #include "chrome/browser/ui/autofill/autofill_snackbar_type.h"
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
-#include "components/plus_addresses/core/browser/grit/plus_addresses_strings.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -117,9 +116,6 @@ std::u16string AutofillSnackbarControllerImpl::GetMessageText() const {
           base::FeatureList::IsEnabled(features::kAutofillEnableWalletBranding)
               ? IDS_AUTOFILL_SAVE_SERVER_IBAN_TO_WALLET_SUCCESS_SNACKBAR_MESSAGE_TEXT
               : IDS_AUTOFILL_SAVE_SERVER_IBAN_SUCCESS_SNACKBAR_MESSAGE_TEXT);
-    case AutofillSnackbarType::kPlusAddressEmailOverride:
-      return l10n_util::GetStringUTF16(
-          IDS_PLUS_ADDRESS_SNACKBAR_UNDO_EMAIL_SWAP_DESCRIPTION_TEXT_ANDROID);
     case AutofillSnackbarType::kCardInfoRetrieval:
       return l10n_util::GetStringUTF16(
           IDS_AUTOFILL_CARD_INFO_RETRIEVAL_SNACKBAR_MESSAGE_TEXT);
@@ -156,9 +152,6 @@ std::u16string AutofillSnackbarControllerImpl::GetActionButtonText() const {
     case AutofillSnackbarType::kSaveServerIbanSuccess:
       return l10n_util::GetStringUTF16(
           IDS_AUTOFILL_SAVE_SERVER_IBAN_SUCCESS_SNACKBAR_BUTTON_TEXT);
-    case AutofillSnackbarType::kPlusAddressEmailOverride:
-      return l10n_util::GetStringUTF16(
-          IDS_PLUS_ADDRESS_SNACKBAR_UNDO_EMAIL_SWAP_ACTION_TEXT_ANDROID);
     case AutofillSnackbarType::kAutofillAiSaveToWalletFailure:
       return l10n_util::GetStringUTF16(
           IDS_AUTOFILL_AI_SAVE_OR_UPDATE_ENTITY_FAILED_WALLET_SAVE_DIALOG_CONFIRMATION_BUTTON_LABEL);
@@ -198,8 +191,6 @@ std::string AutofillSnackbarControllerImpl::GetSnackbarTypeForLogging() {
       return "VirtualCardEnrollSuccess";
     case AutofillSnackbarType::kSaveServerIbanSuccess:
       return "SaveServerIbanSuccess";
-    case AutofillSnackbarType::kPlusAddressEmailOverride:
-      return "PlusAddressEmailOverride";
     case AutofillSnackbarType::kCardInfoRetrieval:
       return "CardInfoRetrievalEnrolled";
     case AutofillSnackbarType::kBnpl:
