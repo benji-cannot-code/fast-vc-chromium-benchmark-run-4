045e5ebf1fd8b9124606f166ab26c4e1db9a8305
Danilo Tedeschi <danft@google.com>
2026-04-24 17:58:11 -0700

    Create initial LanguageCode class
    Initial implementation, this what I am doing:
    
    - Support bcp47 codes with in the format: lang_tag{'-'script_tag}{'-'region_tag}.
    
    - |lang_tag| <= 3
    - |script_tag| = 4
    - |region_tag| <= 3
    
    LanguageCode constructor at constexpr time. We will also provide a list
    of constexpr functions for the canonical languages in the following
    
    format: language_codes::ENGLISH() (not in this CL)
    Change-Id: Ice6f51924b1ef49a9fac928ffc1d57dc20adb37e
    Bug: 506198296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748938
    Reviewed-by: Manish Goregaokar <manishearth@google.com>
    Commit-Queue: Danilo Françoso Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620585}

---
+++ base/i18n/language_code.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/i18n/language_code.h"
+
+#include "base/strings/string_util.h"
+
+namespace base {
+
+std::string LanguageCode::ToLegacyICUFormat() const {
+  std::string code(ToString());
+  base::ReplaceSubstringsAfterOffset(&code, 0, "-", "_");
+  return code;
+}
+
+std::string_view LanguageCode::ToString() const {
+  return std::string_view(code_.data(), length_);
+}
+
+}  // namespace base
