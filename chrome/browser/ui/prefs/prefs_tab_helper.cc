c5b81a0cd5d6d58ea842ab5ab57887481e491c6d
Elly <ellyjones@chromium.org>
2026-04-27 13:55:12 -0700

    prefs: fix UNSAFE_TODO in prefs_tab_helper.cc
    Bug: None
    Change-Id: Ie51bfc662af176eca9e8bd1b448a7078ef33a1fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796664
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621298}

--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <set>
 #include <string>
+#include <string_view>
 
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
@@ -231,10 +232,12 @@ UScriptCode GetScriptOfFontPref(const char* pref_name) {
   static const size_t kScriptNameLength = 4;
 
   size_t len = strlen(pref_name);
-  DCHECK_GT(len, kScriptNameLength);
-  const char* scriptName = &UNSAFE_TODO(pref_name[len - kScriptNameLength]);
-  int32_t code = u_getPropertyValueEnum(UCHAR_SCRIPT, scriptName);
-  DCHECK(code >= 0 && code < USCRIPT_CODE_LIMIT);
+  CHECK_GE(len, kScriptNameLength);
+  std::string_view pref_name_view(pref_name);
+  const char* script_name =
+      pref_name_view.substr(len - kScriptNameLength).data();
+  int32_t code = u_getPropertyValueEnum(UCHAR_SCRIPT, script_name);
+  CHECK(code >= 0 && code < USCRIPT_CODE_LIMIT);
   return static_cast<UScriptCode>(code);
 }
 
