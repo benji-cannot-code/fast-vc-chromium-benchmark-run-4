dd09b77334b6851a6d3315d8decbabab9dd1742c
Rune Lillesveen <futhark@chromium.org>
2026-04-27 08:25:01 -0700

    Implement CSSContainerRule.conditions
    Currently, parsing only supports a single query, hence the API will
    currently hold a single CSSContainerCondition.
    
    The plan is to ship this API together with comma separated @container
    queries behind the same runtime flag.
    
    Bug: 41491726
    Change-Id: Icb47b6077ad88d4869b8ed9d14908586057acef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796871
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621103}

--- third_party/blink/renderer/core/css/css_container_rule.cc
+++ third_party/blink/renderer/core/css/css_container_rule.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_container_rule.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/v8_css_container_condition.h"
 #include "third_party/blink/renderer/core/css/css_markup.h"
 #include "third_party/blink/renderer/core/css/css_style_sheet.h"
 #include "third_party/blink/renderer/core/css/media_query_exp.h"
@@ -44,6 +45,7 @@ void CSSContainerRule::SetConditionText(
   CSSStyleSheet::RuleMutationScope mutation_scope(this);
   To<StyleRuleContainer>(group_rule_.Get())
       ->SetConditionText(execution_context, parent_contents, value);
+  conditions_ = nullptr;
 }
 
 String CSSContainerRule::containerName() const {
@@ -64,6 +66,28 @@ String CSSContainerRule::containerQuery() const {
 
 const ContainerQuery& CSSContainerRule::ContainerQuery() const {
   return To<StyleRuleContainer>(group_rule_.Get())->GetContainerQuery();
+}
+
+const FrozenArray<CSSContainerCondition>& CSSContainerRule::conditions() {
+  if (!conditions_) {
+    FrozenArray<CSSContainerCondition>::VectorType condition_array;
+    CSSContainerCondition* condition = CSSContainerCondition::Create();
+    condition->setName(ContainerQuery().Selector().Name());
+    if (const ConditionalExpNode* query = ContainerQuery().Query()) {
+      condition->setQuery(query->Serialize());
+    } else {
+      condition->setQuery(g_empty_string);
+    }
+    condition_array.push_back(condition);
+    conditions_ = MakeGarbageCollected<FrozenArray<CSSContainerCondition>>(
+        std::move(condition_array));
+  }
+  return *conditions_;
+}
+
+void CSSContainerRule::Trace(Visitor* visitor) const {
+  CSSConditionRule::Trace(visitor);
+  visitor->Trace(conditions_);
 }
 
 }  // namespace blink
