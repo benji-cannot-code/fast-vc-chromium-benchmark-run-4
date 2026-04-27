10d94a85768f9b115628192a217597aea8ec1b46
Wan-Teh Chang <wtc@google.com>
2026-04-27 13:08:09 -0700

    Roll src/third_party/dav1d/libdav1d/ aa4504729..c0f2fe313 (3 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/aa4504729c82..c0f2fe3135e2
    
    $ git log aa4504729..c0f2fe313 --date=short --no-merges --format='%ad %ae %s'
    2026-04-16 gramner build: Update meson version requirement to 0.54.0
    2026-04-15 Arpad.Panyik AArch64: Optimize ipred_h_8bpc_neon
    2026-04-15 Arpad.Panyik AArch64: Optimize ipred_v_8bpc_neon
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I266048431247291172386b3237e58e1981a25e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797590
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621258}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-29-gaa450472"
+#define DAV1D_VERSION "1.5.3-32-gc0f2fe31"
