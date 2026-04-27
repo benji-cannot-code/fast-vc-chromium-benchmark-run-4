262bac4ed62fb1befd0184dcbaf8980f1f155b10
Philip Rogers <pdr@chromium.org>
2026-04-27 07:16:58 -0700

    [html-in-canvas] Do not paint cross-origin SVG patterns
    This patch plumbs PaintFlags through SVG resource painting to ensure
    cross-origin images within SVG patterns are blocked when drawing with
    privacy-preserving painting.
    
    Fixed: 501597365
    Change-Id: I8bd52f1f64a074ca6bcb8031a1e9948c42d7517c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794467
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621069}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/paint/paint_flags.h"
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_resource_container.h"
+#include "third_party/blink/renderer/core/paint/paint_flags.h"
 
 namespace blink {
 
@@ -37,7 +38,8 @@ class LayoutSVGResourcePaintServer : public LayoutSVGResourceContainer {
                            const gfx::RectF& reference_box,
                            const AffineTransform* additional_transform,
                            const AutoDarkMode& auto_dark_mode,
-                           cc::PaintFlags&) = 0;
+                           cc::PaintFlags&,
+                           PaintFlags paint_flags) = 0;
 
  protected:
   void StyleDidChange(StyleDifference,
