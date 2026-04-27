905d7e469b75b4dc2913c90e71ce76aadf230118
Sky Malice <skym@chromium.org>
2026-04-27 08:57:37 -0700

    Use own package name for resources in toolbar/extensions/
    Bug: None
    Change-Id: I60e38aa38762cb93ab162f9394543762d28cb075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793646
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621126}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsToolbarTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsToolbarTest.java
@@ -54,7 +54,6 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.ImportantFormFactors;
-import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.profiles.Profile;
