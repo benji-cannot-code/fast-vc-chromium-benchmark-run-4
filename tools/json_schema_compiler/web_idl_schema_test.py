cbce9cc5a74dc3e9a38a5f04bbf208f41119409f
Solomon Kinard <solomonkinard@chromium.org>
2026-04-27 14:09:20 -0700

    Extensions: CodeHealth: Spell check random word(s)
    Bug: 415304336
    Change-Id: I2df4c4d58cc78577532596d762c5b7e6c7fd0578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7795188
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621309}

--- tools/json_schema_compiler/web_idl_schema_test.py
+++ tools/json_schema_compiler/web_idl_schema_test.py
@@ -677,7 +677,7 @@ class WebIdlSchemaTest(unittest.TestCase):
 
     self.assertEqual('nested.example', schema['namespace'])
     self.assertEqual(
-        'Schema to test nested namespacee names types in IDL (API names with a'
+        'Schema to test nested namespace names types in IDL (API names with a'
         ' dot in them). API should end up on `<Browser>.nested.example`.',
         schema['description'],
     )
@@ -691,7 +691,7 @@ class WebIdlSchemaTest(unittest.TestCase):
 
     self.assertEqual('multi.nested.example', schema['namespace'])
     self.assertEqual(
-        'Schema to test triple nested namespacee names types in IDL (API names'
+        'Schema to test triple nested namespace names types in IDL (API names'
         ' with 2 dots in them). API should end up on `<Browser>.multi.nested.'
         'example`.',
         schema['description'],
@@ -702,7 +702,7 @@ class WebIdlSchemaTest(unittest.TestCase):
   def testMissingBrowserInterfaceError(self):
     expected_error_regex = (
         r'.* File\(test\/web_idl\/missing_browser_interface.idl\): Schema must'
-        r' contain either a paritial Browser interface \(for APIs\) or a'
+        r' contain either a partial Browser interface \(for APIs\) or a'
         r' partial ExtensionManifest dictionary \(for manifest stubs\)\.')
     self.assertRaisesRegex(
         SchemaCompilerError,
