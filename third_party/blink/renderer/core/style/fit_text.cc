a433aa4c75bc8ea3a6095fcfd54ffe67fa8b3ca6
Kent Tamura <tkent@chromium.org>
2026-04-27 03:08:25 -0700

    text-fit: Rename runtime flags to CssTextFit
    Renames `CssFitWidthText` to `CssTextFit` and `CssFitWidthTextReshaping`
    to `CssTextFitReshaping` for consistency with the `text-fit` CSS
    property.
    
    Bug: 417306102
    Change-Id: I7d6c048ddcf7a2dc12c1713a63f132ecd0498849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790539
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620977}

--- third_party/blink/renderer/core/style/fit_text.cc
+++ third_party/blink/renderer/core/style/fit_text.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 FitTextMethod FitText::Method() const {
-  return RuntimeEnabledFeatures::CssFitWidthTextReshapingEnabled()
+  return RuntimeEnabledFeatures::CssTextFitReshapingEnabled()
              ? FitTextMethod::kFontSize
              : FitTextMethod::kScale;
 }
