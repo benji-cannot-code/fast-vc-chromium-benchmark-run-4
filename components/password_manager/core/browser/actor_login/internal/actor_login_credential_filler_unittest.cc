8624ac6a7da944bac17bc2e6ba07f516dec91a44
Ioana Pandele <ioanap@chromium.org>
2026-04-27 11:36:17 -0700

    [ActorLogin] Remove kActorLoginFieldVisibilityCheck flag
    Bug:465073627
    
    Change-Id: Iee07245bc0797e0bfcd3c22ccf8069f63cc5b75f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780672
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621230}

--- components/password_manager/core/browser/actor_login/internal/actor_login_credential_filler_unittest.cc
+++ components/password_manager/core/browser/actor_login/internal/actor_login_credential_filler_unittest.cc
@@ -1371,11 +1371,6 @@ TEST_P(ActorLoginCredentialFillerTest, StoresPermissionWhenFillingAllFields) {
 }
 
 TEST_P(ActorLoginCredentialFillerTest, FillOnlyUsernameInAllEligibleFields) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitWithFeatures(
-      /*enabled_features=*/{},
-      /*disabled_features=*/{
-          password_manager::features::kActorLoginFieldVisibilityCheck});
   const url::Origin origin = url::Origin::Create(GURL(kLoginUrl));
   const Credential credential =
       CreateTestCredential(kTestUsername, origin.GetURL(), origin);
@@ -1460,14 +1455,14 @@ TEST_P(ActorLoginCredentialFillerTest, FillOnlyUsernameInAllEligibleFields) {
           ActorLoginQuality_AttemptLoginDetails_AttemptLoginOutcome_SUCCESS);
   expected_details.set_attempt_login_time_ms(attempt_login_time);
 
-  // The async check is not executed, so there are only details
-  // about the form data.
-  *expected_details.add_parsed_form_details()->mutable_form_data() =
-      CreateExpectedFormData(*parsed_form);
-  *expected_details.add_parsed_form_details()->mutable_form_data() =
-      CreateExpectedFormData(*username_only_parsed_form);
-  *expected_details.add_parsed_form_details()->mutable_form_data() =
-      CreateExpectedFormData(*password_only_parsed_form);
+  *expected_details.add_parsed_form_details() = CreateExpectedLoginFormDetails(
+      *parsed_form, /*is_username_visible=*/true, /*is_password_visible=*/true);
+  *expected_details.add_parsed_form_details() = CreateExpectedLoginFormDetails(
+      *username_only_parsed_form, /*is_username_visible=*/true,
+      /*is_password_visible=*/false);
+  *expected_details.add_parsed_form_details() = CreateExpectedLoginFormDetails(
+      *password_only_parsed_form, /*is_username_visible=*/false,
+      /*is_password_visible=*/true);
 
   FillingFormResult* form_result1 = expected_details.add_filling_form_result();
   *form_result1->mutable_form_data() = CreateExpectedFormData(*parsed_form);
@@ -1793,9 +1788,6 @@ TEST_P(ActorLoginCredentialFillerTest, RequestsReauthBeforeFillingAllFields) {
 
 TEST_P(ActorLoginCredentialFillerTest,
        FillAllFields_OnlyUsernamesVisible_AsyncVisibilityCheck) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      password_manager::features::kActorLoginFieldVisibilityCheck);
   const url::Origin origin = url::Origin::Create(GURL(kLoginUrl));
   const Credential credential =
       CreateTestCredential(kTestUsername, origin.GetURL(), origin);
@@ -1911,10 +1903,6 @@ TEST_P(ActorLoginCredentialFillerTest,
 
 TEST_P(ActorLoginCredentialFillerTest,
        FillAllFields_OnlyPasswordsVisible_AsyncVisibilityCheck) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      password_manager::features::kActorLoginFieldVisibilityCheck);
-
   const url::Origin origin = url::Origin::Create(GURL(kLoginUrl));
   const Credential credential =
       CreateTestCredential(kTestUsername, origin.GetURL(), origin);
@@ -2028,9 +2016,6 @@ TEST_P(ActorLoginCredentialFillerTest,
 
 TEST_P(ActorLoginCredentialFillerTest,
        UsernameAndPasswordFieldAreNotVisible_AsyncCheck) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      password_manager::features::kActorLoginFieldVisibilityCheck);
   const url::Origin origin = url::Origin::Create(GURL(kLoginUrl));
   const Credential credential =
       CreateTestCredential(kTestUsername, origin.GetURL(), origin);
