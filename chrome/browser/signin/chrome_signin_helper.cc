39f099cc7ed8dbd74fe83e415653f895575dcab6
Liza Bipin <mlbipin@google.com>
2026-04-27 06:50:12 -0700

    [Signin][Android] Add metrics to record web signin flow
    Bug: 496835772
    Change-Id: I896bb8af2b2b84b22f7e41783f12aafe761ae5c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779731
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621050}

--- chrome/browser/signin/chrome_signin_helper.cc
+++ chrome/browser/signin/chrome_signin_helper.cc
@@ -382,6 +382,9 @@ void ProcessMirrorHeader(
       base::FeatureList::IsEnabled(switches::kSupportWebSigninAddSession)) {
     if (!target_account_info.has_value()) {
       // Target account is not on the device.
+      base::UmaHistogramEnumeration(
+          "Signin.ProcessMirrorHeaders.Event",
+          signin::MirrorHeaderEvent::kAccountNotOnDevice);
       SigninBridgeFactory::GetForProfile(profile)->StartAddAccountFlow(
           TabAndroid::FromWebContents(web_contents),
           manage_accounts_params.email, continue_url);
@@ -392,6 +395,9 @@ void ProcessMirrorHeader(
     // error.
     if (identity_manager->HasAccountWithRefreshTokenInPersistentErrorState(
             target_account_info->account_id)) {
+      base::UmaHistogramEnumeration(
+          "Signin.ProcessMirrorHeaders.Event",
+          signin::MirrorHeaderEvent::kAccountInPersistentError);
       SigninBridgeFactory::GetForProfile(profile)->StartUpdateCredentialsFlow(
           TabAndroid::FromWebContents(web_contents), continue_url,
           target_account_info->account_id);
@@ -400,6 +406,9 @@ void ProcessMirrorHeader(
 
     // If the account is available on the device but is not in error state
     // then we wait for cookies.
+    base::UmaHistogramEnumeration(
+        "Signin.ProcessMirrorHeaders.Event",
+        signin::MirrorHeaderEvent::kAccountRecentlyAdded);
     SigninBridgeFactory::GetForProfile(profile)->WaitForCookiesAndRedirect(
         TabAndroid::FromWebContents(web_contents), continue_url,
         target_account_info->account_id);
