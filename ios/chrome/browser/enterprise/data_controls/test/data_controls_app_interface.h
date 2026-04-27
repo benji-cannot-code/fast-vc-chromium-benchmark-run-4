01c3a9379e14dd8382e6850c6a9fb8c74455a433
Ana Ercegovac <anaercegovac@google.com>
2026-04-27 07:13:08 -0700

    Integrate Data Controls policy checks into "Search With" on iOS
    This change adds synchronous and asynchronous checks for Data Controls
    policies when using the "Search With" feature. The synchronous check
    (`IsSearchWithAllowed`) is used to determine if the "Search with Google"
    menu item should be shown at all. The asynchronous check
    (`ShouldAllowSearchWith`) is called when the user taps the menu item,
    potentially displaying a warning dialog based on the policy verdict. The
    "Search With" action is treated as a "Copy to OS Clipboard" restriction
    for policy evaluation.
    
    Bug: 442804069
    DD: go/cep-se-dc-support-for-context-menu-search-dd
    Change-Id: I74d2a8bdb265c8ced2476dcca51e401ee2387cb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772776
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Ana Ercegovac <anaercegovac@google.com>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621067}

--- ios/chrome/browser/enterprise/data_controls/test/data_controls_app_interface.h
+++ ios/chrome/browser/enterprise/data_controls/test/data_controls_app_interface.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Sets a data controls policy that warns on copy actions.
 + (void)setWarnCopyRule;
 
+// Sets a data controls policy that reports any copy action.
++ (void)setReportCopyRule;
+
 // Clears all data controls policies.
 + (void)clearDataControlRules;
 
