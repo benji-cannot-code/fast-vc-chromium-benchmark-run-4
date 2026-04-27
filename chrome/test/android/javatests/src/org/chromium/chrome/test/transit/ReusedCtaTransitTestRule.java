427297878ee156ba6d83f6477c1ac1e33eedf895
Henrique Nakashima <hnakashima@chromium.org>
2026-04-27 16:01:24 -0700

    [Android] Fix state leaks in Public Transit and ReusedCta rule
    Clear the static sActivity in ReusedCtaTransitTestRule, preventing
    LeakCanary from detecting it as a leak.
    
    Bug: 476446891
    Change-Id: I7dfc9dfb9f1e7e0711a9bb1de59651c2e1230a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7795682
    Reviewed-by: Sam Maier <smaier@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621384}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/ReusedCtaTransitTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/ReusedCtaTransitTestRule.java
@@ -16,6 +16,7 @@ import org.junit.runners.model.Statement;
 
 import org.chromium.base.test.transit.BatchedPublicTransitRule;
 import org.chromium.base.test.transit.Station;
+import org.chromium.base.test.transit.TrafficControl;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -31,6 +32,10 @@ public class ReusedCtaTransitTestRule<HostStationT extends Station<? extends Chr
         extends BaseCtaTransitTestRule implements TestRule {
     @SuppressLint("StaticFieldLeak")
     private static @Nullable ChromeTabbedActivity sActivity;
+
+    static {
+        TrafficControl.addHopOffListener(ReusedCtaTransitTestRule::clearActivity);
+    }
 
     private final Function<ChromeTabbedActivityTestRule, HostStationT> mEntryPointFunction;
     private final BatchedPublicTransitRule<HostStationT> mBatchedRule;
@@ -66,5 +71,9 @@ public class ReusedCtaTransitTestRule<HostStationT extends Station<? extends Chr
             mActivityTestRule.setActivity(sActivity);
         }
         return station;
+    }
+
+    static void clearActivity() {
+        sActivity = null;
     }
 }
