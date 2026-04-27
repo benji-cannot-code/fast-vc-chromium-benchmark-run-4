e7550319a10c6ab3db6a85c97e8470a250211d6e
trang mai <trangm@google.com>
2026-04-27 02:06:36 -0700

    [OTP] Add Gmail OTP fetch latency metric
    Bug: 479082581
    Change-Id: I900a7c7491e2c21e0199f7be39032a6ee01192a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790163
    Auto-Submit: Trang Mai <trangm@google.com>
    Commit-Queue: Trang Mai <trangm@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620947}

--- components/one_time_tokens/core/browser/email_one_time_token_fetcher.cc
+++ components/one_time_tokens/core/browser/email_one_time_token_fetcher.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/one_time_tokens/core/browser/email_one_time_token_fetcher.h"
 
 #include "base/base64url.h"
+#include "base/metrics/histogram_functions.h"
 #include "components/one_time_tokens/core/browser/fetch_email_one_time_token_request.pb.h"
 #include "components/one_time_tokens/core/browser/fetch_email_one_time_token_response.pb.h"
 #include "components/one_time_tokens/core/browser/one_time_token.h"
@@ -137,12 +138,15 @@ void EmailOneTimeTokenFetcher::StartOneTimeTokenServiceCall(
       url_loader_factory_.get(),
       base::BindOnce(
           &EmailOneTimeTokenFetcher::OnResponseBytesFromOneTimeTokenService,
-          weakptr_factory_.GetWeakPtr()),
+          weakptr_factory_.GetWeakPtr(), base::TimeTicks::Now()),
       network::SimpleURLLoader::kMaxBoundedStringDownloadSize);
 }
 
 void EmailOneTimeTokenFetcher::OnResponseBytesFromOneTimeTokenService(
+    base::TimeTicks network_request_start_time,
     std::optional<std::string> response_body) {
+  base::UmaHistogramTimes("Autofill.OneTimeTokens.Backend.Gmail.NetworkLatency",
+                          base::TimeTicks::Now() - network_request_start_time);
   if (response_body.has_value()) {
     auto result = ExtractOneTimeTokenValueFromResponse(*response_body);
     simple_url_loader_.reset();
--- components/one_time_tokens/core/browser/email_one_time_token_fetcher_unittest.cc
+++ components/one_time_tokens/core/browser/email_one_time_token_fetcher_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64url.h"
 #include "base/functional/callback.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "components/one_time_tokens/core/browser/fetch_email_one_time_token_request.pb.h"
@@ -92,6 +93,7 @@ class EmailOneTimeTokenFetcherTest : public testing::Test {
   }
 
   base::test::TaskEnvironment task_environment_;
+  base::HistogramTester histogram_tester_;
   std::unique_ptr<network::TestURLLoaderFactory> test_url_loader_factory_;
   std::unique_ptr<signin::IdentityTestEnvironment> identity_test_env_;
 };
@@ -170,6 +172,8 @@ TEST_F(EmailOneTimeTokenFetcherTest, NetworkError) {
   ASSERT_FALSE(result.has_value());
   EXPECT_EQ(result.error(),
             OneTimeTokenRetrievalError::kGmailOtpBackendNetworkError);
+  histogram_tester_.ExpectTotalCount(
+      "Autofill.OneTimeTokens.Backend.Gmail.NetworkLatency", 1);
 }
 
 // Tests that an error is returned when the response proto is invalid.
