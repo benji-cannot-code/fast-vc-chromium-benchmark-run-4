cfc887109eed7389ce1425a0d2f177413a23e908
Daniel Rubery <drubery@chromium.org>
2026-04-27 14:05:11 -0700

    [bedrock] Migrate new_tab_page_feature_promo_helper_unittest to browser test
    To minimize the amount of unit test infrastructure reliant on Browser*,
    this CL gets rid of a dependency on BrowserWithTestWindowTest and
    replaces it with a full browser test.
    
    Fixed: 506855840
    Bypass-Check-License: Moved files
    Link: https://chromium-review.googlesource.com/id/I8db7597666b7a58f58df76544aefa0846a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791858
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621305}

--- chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper_browsertest.cc
+++ chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper_browsertest.cc
@@ -8,26 +8,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "build/build_config.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/views/user_education/browser_user_education_service.h"
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
-#include "chrome/test/base/test_browser_window.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/user_education/mock_browser_user_education_interface.h"
 #include "components/feature_engagement/public/event_constants.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/feature_engagement/test/scoped_iph_feature_list.h"
 #include "components/user_education/common/feature_promo/feature_promo_controller.h"
 #include "components/user_education/test/mock_feature_promo_controller.h"
+#include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/base/unowned_user_data/user_data_factory.h"
 
-class NewTabPageFeaturePromoHelperTest : public BrowserWithTestWindowTest {
+class NewTabPageFeaturePromoHelperTest : public InProcessBrowserTest {
  protected:
-  NewTabPageFeaturePromoHelperTest() = default;
+  NewTabPageFeaturePromoHelperTest() {
+    iph_feature_list_.InitAndEnableFeatures(
+        {feature_engagement::kIPHDesktopCustomizeChromeExperimentFeature});
+  }
 
-  void SetUp() override {
+  void SetUpInProcessBrowserTestFixture() override {
     // Override the factory before the browser window is created.
     user_ed_override_ =
         BrowserWindowFeatures::GetUserDataFactoryForTesting()
@@ -36,13 +41,12 @@ class NewTabPageFeaturePromoHelperTest : public BrowserWithTestWindowTest {
                   return std::make_unique<MockBrowserUserEducationInterface>(
                       &window);
                 }));
+    InProcessBrowserTest::SetUpInProcessBrowserTestFixture();
+  }
 
-    BrowserWithTestWindowTest::SetUp();
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
 
-    iph_feature_list_.InitAndEnableFeatures(
-        {feature_engagement::kIPHDesktopCustomizeChromeExperimentFeature});
-
-    AddTab(browser(), GURL("chrome://newtab"));
     tab_ = browser()->tab_strip_model()->GetActiveWebContents();
 
     helper_ = std::make_unique<NewTabPageFeaturePromoHelper>();
@@ -71,7 +75,8 @@ class NewTabPageFeaturePromoHelperTest : public BrowserWithTestWindowTest {
 
 // In CFT mode, there are often no User Ed controllers.
 #if !BUILDFLAG(CHROME_FOR_TESTING)
-TEST_F(NewTabPageFeaturePromoHelperTest, RecordFeatureUsage_CustomizeChrome) {
+IN_PROC_BROWSER_TEST_F(NewTabPageFeaturePromoHelperTest,
+                       RecordFeatureUsage_CustomizeChrome) {
   // By default let through all calls to endpromo.
   EXPECT_CALL(*user_education(),
               NotifyFeaturePromoFeatureUsed(testing::_, testing::_))
@@ -89,8 +94,8 @@ TEST_F(NewTabPageFeaturePromoHelperTest, RecordFeatureUsage_CustomizeChrome) {
 }
 #endif  // !BUILDFLAG(CHROME_FOR_TESTING)
 
-TEST_F(NewTabPageFeaturePromoHelperTest,
-       MaybeShowFeaturePromo_CustomizeChrome) {
+IN_PROC_BROWSER_TEST_F(NewTabPageFeaturePromoHelperTest,
+                       MaybeShowFeaturePromo_CustomizeChrome) {
   EXPECT_CALL(
       *user_education(),
       MaybeShowFeaturePromo(user_education::test::MatchFeaturePromoParams(
@@ -101,8 +106,8 @@ TEST_F(NewTabPageFeaturePromoHelperTest,
       feature_engagement::kIPHDesktopCustomizeChromeExperimentFeature, tab());
 }
 
-TEST_F(NewTabPageFeaturePromoHelperTest,
-       MaybeShowFeaturePromo_NonGoogle_CustomizeChrome) {
+IN_PROC_BROWSER_TEST_F(NewTabPageFeaturePromoHelperTest,
+                       MaybeShowFeaturePromo_NonGoogle_CustomizeChrome) {
   EXPECT_CALL(*user_education(), MaybeShowFeaturePromo).Times(0);
   helper()->SetDefaultSearchProviderIsGoogleForTesting(false);
   helper()->MaybeShowFeaturePromo(
