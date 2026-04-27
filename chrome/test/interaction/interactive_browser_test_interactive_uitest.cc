2e57193d259aa3b9a1143ed9d2f5626d61a5235e
Dana Fried <dfried@chromium.org>
2026-04-27 08:07:04 -0700

    [Test Automation] Allow for pausing tests for UI inspection
    This adds a new verb, "MaybeEnterInteractiveMode()", that can be used
    alongside other verbs like "Log()" and "DebugDumpElements()" to debug
    interactive tests in Kombucha.
    
    This step is normally a no-op, but if "--test-launcher-interactive" is
    passed on the command line, it will pause the test until the user
    sends a specific, configurable accelerator combination to that surface
    (or does something that hides the target).
    
    This is similar to how pixel tests will pause when screenshotting if
    "--test-launcher-interactive" is specified, except that it allows for
    the test to potentially proceed because the window is not required to
    be closed to break out of interactive mode.
    
    The reason this is in InteractiveViewsTest is because it requires a
    views::EventMonitor to catch the accelerator to break out of the step.
    If we did not care to allow a keypress to continue the test it could
    probably be all the way down in InteractiveTest.
    
    Bug: 505741472
    Change-Id: Ifae2812707aa50856459ebb8ec7136df3e8b30a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793373
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621089}

--- chrome/test/interaction/interactive_browser_test_interactive_uitest.cc
+++ chrome/test/interaction/interactive_browser_test_interactive_uitest.cc
@@ -86,8 +86,6 @@ class InteractiveBrowserTestUiTest : public InteractiveBrowserTest {
   }
 
   void TearDownOnMainThread() override {
-    browser()->profile()->GetPrefs()->ClearPref(
-        prefs::kTabSearchPinnedToTabstrip);
     EXPECT_TRUE(embedded_test_server()->ShutdownAndWaitUntilComplete());
     InteractiveBrowserTest::TearDownOnMainThread();
   }
@@ -513,6 +511,11 @@ IN_PROC_BROWSER_TEST_F(InteractiveBrowserTestUiTest, SendKeyPress) {
       SendKeyPress(kOmniboxElementId, ui::VKEY_A),
       SendKeyPress(kOmniboxElementId, ui::VKEY_B, ui::EF_SHIFT_DOWN),
       CheckViewProperty(kOmniboxElementId, &OmniboxViewViews::GetText, u"aB"));
+}
+
+IN_PROC_BROWSER_TEST_F(InteractiveBrowserTestUiTest,
+                       MaybeEnterInteractiveMode) {
+  RunTestSequence(MaybeEnterInteractiveMode(kBrowserViewElementId));
 }
 
 // Simple bubble containing a WebView. Allows us to simulate swapping out one
