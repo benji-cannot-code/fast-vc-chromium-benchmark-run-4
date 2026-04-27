e62a7340a3c97adbd2ff8435b40ba4d8c7f4eaf9
Gaston Rodriguez <gastonr@microsoft.com>
2026-04-27 15:59:43 -0700

    Clean up unused variables from web_theme_engine_default.cc
    These global variables are only used when Fluent scrollbars are disabled
    on Windows. Non-Fluent scrollbars have been disabled, and Fluent
    scrollbars are always enabled in Windows, making these variables
    obsolete.
    This CL cleans up all the code surrounding them.
    
    Bug: 398193016
    Change-Id: I4c62cbfcc0bb31f7140f14545d62bf154eff6669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7746955
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621381}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -3577,7 +3577,6 @@ void WebViewImpl::UpdateRendererPreferences(
     observer.OnRendererPreferencesUpdated(preferences);
   }
 
-  WebThemeEngineHelper::DidUpdateRendererPreferences(preferences);
   UpdateFontRenderingFromRendererPrefs();
 
   blink::SetCaretBlinkInterval(
