48f1edef5203477787a56a06f9aa9295c876a87e
Jeffrey Yu <yuje@chromium.org>
2026-04-27 14:36:45 -0700

    Change string to string_view to avoid unnecessary copies in hash_util
    - `.substr()` on a string_view returns another string_view instead of
    creating a new string.
    - Using `base::SplitStringPiece` instead of `base::SplitString` avoids creating substring copies by returning string_view of the substrings instead.
    
    Bug: 501076278
    Change-Id: I08356c368ba09712b7a0a2f829b97bbed70c4d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783710
    Commit-Queue: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621328}

--- chromeos/version/version_loader.h
+++ chromeos/version/version_loader.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
+#include <string_view>
 
 #include "base/component_export.h"
 
@@ -44,7 +45,7 @@ COMPONENT_EXPORT(CHROMEOS_VERSION) std::string GetFirmware();
 
 // Extracts the firmware from the file.
 COMPONENT_EXPORT(CHROMEOS_VERSION)
-std::string ParseFirmware(const std::string& contents);
+std::string ParseFirmware(std::string_view contents);
 
 // Returns true if |new_version| is older than |current_version|.
 // Version numbers should be dot separated. The sections are compared as
@@ -52,8 +53,7 @@ std::string ParseFirmware(const std::string& contents);
 // precedence. If one version is prefix of another, the shorter one is
 // considered older. (See test for examples.)
 COMPONENT_EXPORT(CHROMEOS_VERSION)
-bool IsRollback(const std::string& current_version,
-                const std::string& new_version);
+bool IsRollback(std::string_view current_version, std::string_view new_version);
 
 }  // namespace version_loader
 }  // namespace chromeos
