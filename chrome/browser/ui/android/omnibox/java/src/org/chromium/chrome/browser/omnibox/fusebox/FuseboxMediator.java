41f6ab27cdda49d36c5b3b75c5417849fbbf3216
Tomasz Wiszkowski <ender@google.com>
2026-04-28 13:35:44 -0700

    Reapply "Clearly differentiate Desktop platform from Desktop exp."
    This reverts commit 0be907ea35a792914da887e7d00ebcfca2d787a8.
    This relands commit https://crrev.com/c/7797685
    
    A subsequent change disables two tests to allow time to figure out why
    these tests make one obscure Desktop bot unhappy.
    
    Change-Id: I5a9fe498af4d4399b3e143a97c9379d6370a4bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7799968
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622006}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
@@ -1068,7 +1068,7 @@ public class FuseboxMediator implements FuseboxAttachmentChangeListener {
     }
 
     private @FuseboxLayoutMode int getFuseboxLayoutMode() {
-        return OmniboxFeatures.isDesktopMode(mContext)
+        return OmniboxFeatures.hasDesktopExperience(mContext)
                 ? FuseboxLayoutMode.POPOVER
                 : FuseboxLayoutMode.SEPARATED;
     }
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionProcessorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/editurl/EditUrlSuggestionProcessorUnitTest.java
@@ -206,7 +206,7 @@ public final class EditUrlSuggestionProcessorUnitTest {
     @Test
     public void doesProcessSuggestion_acceptMatchingWhatYouTypedWhenDesktopModeDisabled() {
         // URL_WHAT_YOU_TYPED
-        OmniboxFeatures.setIsDesktopModeForTesting(false);
+        OmniboxFeatures.setHasDesktopExperienceForTesting(false);
         assertTrue(mProcessor.doesProcessSuggestion(mMatch, 0));
 
         // SEARCH_WHAT_YOU_TYPED
@@ -269,7 +269,7 @@ public final class EditUrlSuggestionProcessorUnitTest {
 
     @Test
     public void doesProcessSuggestion_rejectMatchWhenDesktopModeEnabled() {
-        OmniboxFeatures.setIsDesktopModeForTesting(true);
+        OmniboxFeatures.setHasDesktopExperienceForTesting(true);
         assertFalse(mProcessor.doesProcessSuggestion(mMatch, 0));
         verifyNoMoreInteractions(mSuggestionHost, mShareDelegate, mClipboardManager);
     }
