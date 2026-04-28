a0529d1b43e23517952e58f884e763907fe30963
Cameron Higgins <cameronhiggins@google.com>
2026-04-28 11:13:43 -0700

    macOS 26 - Disable `PerformanceControlsMetricsTest` cases with timeout.
    Temporarily disables tests that use `task_environment()->FastForwardBy` for long time skips as it causes failures on MacOS 26.
    
    Bug: 434660312, 481036378
    Link: https://chromium-review.googlesource.com/id/Idf649fb75af0ba17c641122585545f576a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800256
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621915}

--- chrome/browser/ui/performance_controls/performance_controls_metrics_unittest.cc
+++ chrome/browser/ui/performance_controls/performance_controls_metrics_unittest.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_renderer_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace {
 using performance_manager::user_tuning::PerformanceDetectionManager;
 }
@@ -45,6 +49,13 @@ class PerformanceControlsMetricsTest
 };
 
 TEST_F(PerformanceControlsMetricsTest, DailyMetricsResets) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   base::HistogramTester histogram_tester;
 
   const std::string message_count_histogram_name =
@@ -145,6 +156,13 @@ class PerformanceControlsNotificationTest
 
 TEST_F(PerformanceControlsNotificationTest,
        RecordsNotificationAcceptPercentage) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   base::HistogramTester histogram_tester;
   const std::string message_count_histogram_name =
       "PerformanceControls.Intervention.DailyAcceptancePercentage";
