ac898d1cfc08ab08c3afaf6541bb81540e11e169
manukh <manukh@chromium.org>
2026-04-28 16:09:52 -0700

    [omnibox][webui] Skip in OmniboxRowGroupedViewBrowserTest
    OmniboxRowGroupedViewBrowserTest tests view popup behavior. It crashes
    when view popup isn't instantiated, which is the case when
    `kWebUIOmniboxPopup` is enabled. Since it's view-specific, it's correct
    to skip the test altogether when not running view code.
    
    Effected test:
    OmniboxPopupSuggestionGroupHeadersTest.ShowSuggestionGroupHeadersByPageContext
    
    Bug: 452061489
    Change-Id: I8203f1e3705e5d3ee27f1f9c3dce085609f3f331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801284
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622094}

--- chrome/browser/ui/views/omnibox/omnibox_row_grouped_view_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_row_grouped_view_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/omnibox/omnibox_edit_model.h"
+#include "chrome/browser/ui/omnibox/omnibox_next_features.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_view_views_test.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_result_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_row_view.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/suggestion_group_util.h"
 #include "components/omnibox/common/omnibox_feature_configs.h"
 #include "content/public/test/browser_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/animation/slide_animation.h"
 
 class OmniboxRowGroupedViewBrowserTest : public OmniboxPopupViewViewsTest {
@@ -27,6 +29,14 @@ class OmniboxRowGroupedViewBrowserTest : public OmniboxPopupViewViewsTest {
     feature_list_.InitAndEnableFeature(
         omnibox_feature_configs::ContextualSearch::
             kLoadingSuggestionsAnimation);
+  }
+
+  void SetUp() override {
+    if (base::FeatureList::IsEnabled(omnibox::internal::kWebUIOmniboxPopup)) {
+      GTEST_SKIP() << "Views popup code shouldn't run when kWebUIOmniboxPopup "
+                      "is enabled.";
+    }
+    OmniboxPopupViewViewsTest::SetUp();
   }
 
   void SetUpOnMainThread() override {
