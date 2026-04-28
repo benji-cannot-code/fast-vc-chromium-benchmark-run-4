1e83825f532be177cac9436abad57a5fd47e8221
Jacob Stanley <jacobstanley@google.com>
2026-04-28 11:22:43 -0700

    [Privacy Sandbox] Clean up PrivacySandboxAdsApiUxEnhancements feature
    Bug: b:438953903
    Change-Id: I4de25aeed9103615955ff100744993ee0b840fe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800674
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621923}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -91,9 +91,6 @@ BASE_FEATURE(kPrivateStateTokensDevUI, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kPrivacySandboxAdTopicsContentParity,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kPrivacySandboxAdsApiUxEnhancements,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kPrivacySandboxNoticeFramework, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivacySandboxAdPrivacyUxDeprecation,
