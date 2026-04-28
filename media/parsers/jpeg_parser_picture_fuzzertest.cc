7fd4d89a522cf7c442746853b5914273eb31eafe
Eugene Zemtsov <eugene@chromium.org>
2026-04-28 14:31:14 -0700

    media: Remove legacy C++ JPEG parser and the feature flag
    The Rust JPEG parser is now the only implementation.
    
    This removes the legacy C++ parser, the `kUseRustJpegParser` feature
    flag, and the `parse_jpeg_wrapper` files by inlining the Rust bindings
    directly into `jpeg_parser.cc`. It also removes differential fuzzing
    logic, unused `operator==` implementations from `JpegParseResult` and
    its sub-structs, and their corresponding robustness tests.
    
    Change-Id: I8cca7aa345bd2706292bdb8592b68b82d37264e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7798242
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622040}

--- media/parsers/jpeg_parser_picture_fuzzertest.cc
+++ media/parsers/jpeg_parser_picture_fuzzertest.cc
@@ -6,11 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
-#include "base/check_op.h"
 #include "base/containers/span.h"
 #include "base/logging.h"
 #include "media/parsers/jpeg_parser.h"
-#include "media/parsers/parse_jpeg_wrapper.h"
 #include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 struct Environment {
@@ -21,17 +19,7 @@ Environment* env = new Environment();
 
 // Entry point for LibFuzzer.
 DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(const base::span<const uint8_t> data) {
-  media::JpegParseResult cpp_result;
-  const bool cpp_success = media::ParseJpegPictureLegacy(data, &cpp_result);
-
-  media::JpegParseResult rust_result;
-  const bool rust_success = media::ParseJpegPictureRust(data, &rust_result);
-
-  CHECK_EQ(cpp_success, rust_success)
-      << "Implementations disagree on validity!";
-  if (cpp_success) {
-    CHECK_EQ(cpp_result, rust_result);
-  }
-
+  media::JpegParseResult result;
+  media::ParseJpegPicture(data, &result);
   return 0;
 }
