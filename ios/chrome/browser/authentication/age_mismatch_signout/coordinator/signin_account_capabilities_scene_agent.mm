c7e950f57bd08f5b6dd4778c80f2078e2124b526
Jérôme Lebel <jlebel@chromium.org>
2026-04-28 07:03:25 -0700

    [iOS] Cleanup in SigninAccountCapabilitiesSceneAgent
    Update
    -[SigninAccountCapabilitiesSceneAgent isUIAvailableToShowIOSPrompt] to
    use `self.isSignoutInProgress` instead of
    `_isAgeMismatchSignoutInProgress || _ageMismatchSignoutCoordinator`.
    
    Change-Id: I8faca7c3469be6163fbd25b117b82c09e680018d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793102
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621734}

--- ios/chrome/browser/authentication/age_mismatch_signout/coordinator/signin_account_capabilities_scene_agent.mm
+++ ios/chrome/browser/authentication/age_mismatch_signout/coordinator/signin_account_capabilities_scene_agent.mm
@@ -304,7 +304,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return NO;
   }
 
-  if (_isAgeMismatchSignoutInProgress || _ageMismatchSignoutCoordinator) {
+  if (self.isSignoutInProgress) {
     return NO;
   }
 
