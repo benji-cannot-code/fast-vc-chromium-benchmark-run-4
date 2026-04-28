25292a693a0ced971500ddcb5ed8310d4092eb58
Alison Gale <agale@chromium.org>
2026-04-27 19:04:09 -0700

    [Vertical Tabs] Expand EOH heuristic log to avoid false triggering
    This adds two new configurations: a min time before EOH will trigger and
    a distance from the inside edge of the tab strip before it will trigger.
    Both can be set to 0 to achieve the old behavior (updated flags to
    reflect that).
    
    Bug: 507056866
    Change-Id: I4227a645039bab6c3046859fb4a5c46741f77b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797699
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621489}

--- chrome/browser/ui/tabs/features.h
+++ chrome/browser/ui/tabs/features.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_TABS_FEATURES_H_
 #define CHROME_BROWSER_UI_TABS_FEATURES_H_
 
+#include "base/feature.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/time/time.h"
 
 namespace tabs {
 
@@ -49,6 +51,10 @@ BASE_DECLARE_FEATURE_PARAM(base::TimeDelta,
 // to determine EOH state.
 BASE_DECLARE_FEATURE_PARAM(bool,
                            kVerticalTabsExpandOnHoverUseVelocityHeuristic);
+// When using the velocity heuristic, this is the minimum time before EOH can be
+// triggered.
+BASE_DECLARE_FEATURE_PARAM(base::TimeDelta,
+                           kVerticalTabsExpandOnHoverVelocityHeuristicDelay);
 // This in the minimum number of samples needed to calculate the heuristic.
 BASE_DECLARE_FEATURE_PARAM(
     int,
@@ -62,6 +68,16 @@ BASE_DECLARE_FEATURE_PARAM(base::TimeDelta,
 BASE_DECLARE_FEATURE_PARAM(
     double,
     kVerticalTabsExpandOnHoverVelocityHeuristicThreshold);
+// Minimum distance from the inside edge of the vertical tab strip before EOH
+// can be triggered.
+BASE_DECLARE_FEATURE_PARAM(
+    int,
+    kVerticalTabsExpandOnHoverVelocityHeuristicDistanceFromEdge);
+// When distance from edge is set, only evaluate that parameter until this delay
+// is reached.
+BASE_DECLARE_FEATURE_PARAM(
+    base::TimeDelta,
+    kVerticalTabsExpandOnHoverVelocityHeuristicEdgeDelay);
 
 BASE_DECLARE_FEATURE(kTabSelectionByPointer);
 
