d9d087cb284ae67c80a866752d8939f72e7644a0
Greg Thompson <grt@chromium.org>
2026-04-27 23:34:28 -0700

    Revert "Add custom text to the watermark customization page."
    This reverts commit a4a0ab42c6abd0cd52ffd8d5e25902ef49f41146.
    
    Reason for revert: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/57878/overview: DEATH tests are not supported in browser tests.
    
    Original change's description:
    > Add custom text to the watermark customization page.
    >
    > Bug: 504979162
    > Change-Id: I3deb784ac80ded8971ae5b50aaef73b8fc8894d0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7792673
    > Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1621438}
    
    Bug: 504979162
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3b27707d202124d2ad582eddbc8a5a3d74708353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7792834
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621565}

--- chrome/browser/ui/webui/watermark/watermark_page_handler.cc
+++ chrome/browser/ui/webui/watermark/watermark_page_handler.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/watermark/watermark_page_handler.h"
 
-#include "base/strings/string_util.h"
 #include "base/types/to_address.h"
 #include "chrome/browser/enterprise/data_protection/data_protection_ui_controller.h"
 #include "chrome/browser/enterprise/watermark/settings.h"
@@ -24,8 +23,8 @@ WatermarkPageHandler::WatermarkPageHandler(
 
 WatermarkPageHandler::~WatermarkPageHandler() = default;
 
-void WatermarkPageHandler::SetWatermarkSettings(
-    watermark::mojom::WatermarkSettingsPtr settings) {
+void WatermarkPageHandler::SetWatermarkStyle(
+    watermark::mojom::WatermarkStylePtr style) {
   auto* bwi =
       webui::GetBrowserWindowInterface(base::to_address(host_contents_));
   // The Watermark WebUI loads only in browser-associated contexts.
@@ -35,17 +34,15 @@ void WatermarkPageHandler::SetWatermarkSettings(
       enterprise_data_protection::DataProtectionUIController::From(bwi);
   CHECK(data_protection_ui_controller);
 
-  CHECK(base::IsStringUTF8(settings->watermark_text));
-
   data_protection_ui_controller->ApplyWatermarkSettings(
-      settings->watermark_text,
+      "Watermark Test Page",
       SkColorSetA(
           enterprise_watermark::kBaseFillRGB,
-          enterprise_watermark::PercentageToSkAlpha(settings->fill_opacity)),
+          enterprise_watermark::PercentageToSkAlpha(style->fill_opacity)),
       SkColorSetA(
           enterprise_watermark::kBaseOutlineRGB,
-          enterprise_watermark::PercentageToSkAlpha(settings->outline_opacity)),
-      settings->font_size);
+          enterprise_watermark::PercentageToSkAlpha(style->outline_opacity)),
+      style->font_size);
 }
 
 void WatermarkPageHandler::ShowNotificationToast() {
