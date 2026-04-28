c36f965edf31c4bca57b8cb19d7a424e78698fd6
Sky Malice <skym@chromium.org>
2026-04-28 11:46:17 -0700

    [AIM] Create NtpSearchBox abstraction around fakebox.
    Bug: 507131334
    Change-Id: Ic8942a30c468876b134331bd3a12c960946eef83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797776
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621939}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
@@ -53,7 +53,8 @@ import org.chromium.chrome.browser.magic_stack.ModuleDelegateHost;
 import org.chromium.chrome.browser.magic_stack.ModuleRegistry;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
 import org.chromium.chrome.browser.ntp.NewTabPage.OnSearchBoxScrollListener;
-import org.chromium.chrome.browser.ntp.search.SearchBoxCoordinator;
+import org.chromium.chrome.browser.ntp.search.NtpSearchBox;
+import org.chromium.chrome.browser.ntp.search.NtpSearchBoxFactory;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationConfigManager;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinator;
 import org.chromium.chrome.browser.ntp_customization.NtpCustomizationCoordinatorFactory;
@@ -134,7 +135,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
     private final boolean mEnableLogs;
 
     private @Nullable LogoCoordinator mLogoCoordinator;
-    private @Nullable SearchBoxCoordinator mSearchBoxCoordinator;
+    private @Nullable NtpSearchBox mSearchBoxCoordinator;
     private @Nullable MostVisitedTilesCoordinator mMostVisitedTilesCoordinator;
     private @Nullable OnSearchBoxScrollListener mSearchBoxScrollListener;
     private @Nullable UiConfig mUiConfig;
@@ -332,7 +333,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         }
 
         mSearchBoxCoordinator =
-                new SearchBoxCoordinator(
+                NtpSearchBoxFactory.createSearchBox(
                         mActivity,
                         mNewTabPageLayout,
                         mIsTablet,
