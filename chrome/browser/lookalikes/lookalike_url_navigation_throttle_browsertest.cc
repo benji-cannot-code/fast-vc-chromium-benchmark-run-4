d69371b9fe5ef695b5e45dbb25837a3e5b59d908
manukh <manukh@chromium.org>
2026-04-28 16:57:26 -0700

    [omnibox][webui] Disable in LookalikeUrlNavigationThrottleBrowserTest
    Disables `kWebUIOmniboxPopup` & `kWebUIOmniboxAimPopup`
    
    There were 7 failing tests in the suite:
    - ComboSquatting_EngagedSites_ShouldRecordMetricsWithoutUI
    - EditDistance_EngagedDomain_Match
    - Idn_SiteEngagement_Match_Incognito
    - Idn_SiteEngagementAndTopDomain_Match
    - Idn_TopDomain_EngagedSite_NoMatch
    - IncognitoDoesNotAffectMainProfile
    - Interstitial_Dismiss
    
    I'm unsure why these 2 features are breaking these tests but not the
    other similar tests in the same suite.
    
    FWIW, there are some already disabled tests in the suite.
    
    I mostly looked into Interstitial_Dismiss, which tries to navigate to
    `sité1.com` and expects an interstitial to display with cause
    `kMatchSiteEngagement`, and then verifies it can be dismissed. But for
    some reason, the interstitial was not displaying.
    
    The 2 features create additional web contents, and these tests do
    manipulate web contents. But I verified the tests were navigating and
    looking at the correct web contents, not the new omnibox web contents.
    
    Other similar tests like Idn_TopDomain_Match that navigates to
    `googlé.com` and expects interstitial to show with cause
    `kMatchSkeletonTop500` do continue to pass.
    
    I've also been able to get both `kMatchSiteEngagement` & to
    `kMatchSkeletonTop500` trigger in canary.
    
    I spent a day looking into this with no luck, so leaving it as a todo to
    update the tests.
    
    Bug: 452061489, 507481593
    Change-Id: Ie873beb9d08721d8b344e6e0b5a791f6f8c0f4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801283
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622123}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/simple_test_clock.h"
 #include "build/build_config.h"
 #include "chrome/browser/history/history_service_factory.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
+#include "chrome/browser/ui/omnibox/omnibox_next_features.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -247,6 +249,15 @@ class LookalikeUrlNavigationThrottleBrowserTest : public InProcessBrowserTest {
       : https_server_(std::make_unique<net::EmbeddedTestServer>(
             net::EmbeddedTestServer::TYPE_HTTPS)) {}
 
+  void SetUp() override {
+    // TODO(b:507481593): Some tests are failing when enabling these features.
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features*/ {},
+        /*disabled_features*/ {omnibox::internal::kWebUIOmniboxPopup,
+                               omnibox::internal::kWebUIOmniboxAimPopup});
+    InProcessBrowserTest::SetUp();
+  }
+
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
     test_ukm_recorder_ = std::make_unique<ukm::TestAutoSetUkmRecorder>();
@@ -421,7 +432,6 @@ class LookalikeUrlNavigationThrottleBrowserTest : public InProcessBrowserTest {
       base::HistogramTester* histograms,
       const GURL& navigated_url,
       NavigationSuggestionEvent expected_event) {
-
     history::HistoryService* const history_service =
         HistoryServiceFactory::GetForProfile(
             browser->profile(), ServiceAccessType::EXPLICIT_ACCESS);
@@ -469,6 +479,7 @@ class LookalikeUrlNavigationThrottleBrowserTest : public InProcessBrowserTest {
   // existing tests run with the prewarm feature enabled.
   test::ScopedPrewarmFeatureList prewarm_feature_list_{
       test::ScopedPrewarmFeatureList::PrewarmState::kDisabled};
+  base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<ukm::TestAutoSetUkmRecorder> test_ukm_recorder_;
   std::unique_ptr<LookalikeTestHelper> test_helper_;
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
