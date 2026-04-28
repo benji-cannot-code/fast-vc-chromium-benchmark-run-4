05bb07b62e4337b548acf70e396d689d3ea45848
Sean Maher <spvm@chromium.org>
2026-04-28 11:07:09 -0700

    pdh: fix crashbug due to double-observing renderer hosts
    Instead, track the observed renderers via the
    base::ScopedMultiSourceObservation.
    
    This is landed to fix a crash where AllHostsIterator() is returning
    hosts that will be a part of an OnRenderProcessHostCreated()
    notification in the future.
    
    Bug: 504040905
    Change-Id: I69e50e47068bf8f990f879b466f7065ffdd0ce3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790813
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Sean Maher <spvm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621910}

--- chrome/browser/metrics/system_pdh_metrics_provider_win.cc
+++ chrome/browser/metrics/system_pdh_metrics_provider_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/process/process_handle.h"
 #include "base/rand_util.h"
+#include "base/scoped_multi_source_observation.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions_win.h"
 #include "base/strings/sys_string_conversions.h"
@@ -107,8 +108,11 @@ class SystemPdhMetricsProvider::ProcessMetricsObserver
     // Record 1/`downsampling_factor_` currently active renderers.
     for (auto it = content::RenderProcessHost::AllHostsIterator();
          !it.IsAtEnd(); it.Advance()) {
-      it.GetCurrentValue()->AddObserver(this);
-      ProbabilisticallyListenToRenderer(it.GetCurrentValue());
+      content::RenderProcessHost* host = it.GetCurrentValue();
+      host_observations_.AddObservation(host);
+      if (host->IsReady()) {
+        ProbabilisticallyListenToRenderer(host);
+      }
     }
 
     // Record the GPU, and Record 1/`downsampling_factor_` currently active
@@ -120,10 +124,6 @@ class SystemPdhMetricsProvider::ProcessMetricsObserver
 
   ~ProcessMetricsObserver() override {
     content::BrowserChildProcessObserver::Remove(this);
-    for (auto it = content::RenderProcessHost::AllHostsIterator();
-         !it.IsAtEnd(); it.Advance()) {
-      it.GetCurrentValue()->RemoveObserver(this);
-    }
   }
 
   // Depending on the type of the child process, listen to the given process's
@@ -177,7 +177,9 @@ class SystemPdhMetricsProvider::ProcessMetricsObserver
   }
 
   void OnRenderProcessHostCreated(content::RenderProcessHost* host) override {
-    host->AddObserver(this);
+    if (!host_observations_.IsObservingSource(host)) {
+      host_observations_.AddObservation(host);
+    }
   }
 
   void RenderProcessReady(content::RenderProcessHost* host) override {
@@ -194,7 +196,7 @@ class SystemPdhMetricsProvider::ProcessMetricsObserver
   }
 
   void RenderProcessHostDestroyed(content::RenderProcessHost* host) override {
-    host->RemoveObserver(this);
+    host_observations_.RemoveObservation(host);
     query_handler_
         ->AsyncCall(&SystemPdhMetricsProvider::PdhQueryHandler::
                         StopListeningToProcessPdhMetrics)
@@ -252,6 +254,10 @@ class SystemPdhMetricsProvider::ProcessMetricsObserver
 
   const raw_ref<base::SequenceBound<SystemPdhMetricsProvider::PdhQueryHandler>>
       query_handler_;
+
+  base::ScopedMultiSourceObservation<content::RenderProcessHost,
+                                     content::RenderProcessHostObserver>
+      host_observations_{this};
 };
 
 SystemPdhMetricsProvider::SystemPdhMetricsProvider() = default;
