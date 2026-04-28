90b609026a1925ed2133425e7795829e8aeeb95f
Taiyo Mizuhashi <taiyo@chromium.org>
2026-04-28 01:33:35 -0700

    Remove stale debug code in AwPrefetchTest
    Bug: n/a
    Change-Id: Id32ecf3bfdbb58253ab912faa2723200d2119f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791212
    Auto-Submit: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621599}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwPrefetchTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwPrefetchTest.java
@@ -29,7 +29,6 @@ import org.chromium.android_webview.AwNoVarySearchData;
 import org.chromium.android_webview.AwPrefetchCallback;
 import org.chromium.android_webview.AwPrefetchManager;
 import org.chromium.android_webview.AwPrefetchParameters;
-import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
@@ -1023,7 +1022,6 @@ public class AwPrefetchTest extends AwParameterizedTest {
 
         @Override
         public void onStatusUpdated(int statusCode, @Nullable Bundle extras) {
-            Log.e("Sayed", "Status code is" + statusCode);
             mOnStatusUpdatedHelper.notifyCalled(statusCode, extras);
         }
 
