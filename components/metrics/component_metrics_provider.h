b4318023ae134ffee59f3d994b956ffbf79b0a0f
Jeffrey Yu <yuje@chromium.org>
2026-04-27 19:24:25 -0700

    Use std::string_view in ComponentMetricsProvider
    `.substr()` on a string_view returns another string_view instead of
    creating a new string.
    
    Bug: 501076278
    Change-Id: I48fcc51a807f3a61409dbefffdb76530fc53cade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794006
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621497}

--- components/metrics/component_metrics_provider.h
+++ components/metrics/component_metrics_provider.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_METRICS_COMPONENT_METRICS_PROVIDER_H_
 #define COMPONENTS_METRICS_COMPONENT_METRICS_PROVIDER_H_
 
+#include <string_view>
 #include <vector>
 
 #include "components/metrics/metrics_provider.h"
@@ -46,9 +47,9 @@ class ComponentMetricsProvider : public MetricsProvider {
       SystemProfileProto* system_profile_proto) override;
 
   static SystemProfileProto_ComponentId CrxIdToComponentId(
-      const std::string& app_id);
+      std::string_view app_id);
 
-  static uint32_t HashCohortId(const std::string& cohort_id);
+  static uint32_t HashCohortId(std::string_view cohort_id);
 
  private:
   std::unique_ptr<ComponentMetricsProviderDelegate> components_info_delegate_;
