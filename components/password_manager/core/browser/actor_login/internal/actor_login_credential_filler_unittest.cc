1128b7ca97b71b5a7f950fecde50617841bb635b
Ioana Pandele <ioanap@chromium.org>
2026-04-28 01:15:46 -0700

    [ActorLogin] Remove ActorLoginPemissionsUseStrongAffiliations flag
    Bug: 465073627
    Change-Id: Ic3d24c6b075c6d28d0e385a42cb18e69fc96d826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7784253
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621590}

--- components/password_manager/core/browser/actor_login/internal/actor_login_credential_filler_unittest.cc
+++ components/password_manager/core/browser/actor_login/internal/actor_login_credential_filler_unittest.cc
@@ -2283,9 +2283,6 @@ TEST_P(ActorLoginCredentialFillerTest, AffiliatedOrigin_FillSuccess) {
 
 TEST_P(ActorLoginCredentialFillerTest,
        UsesChosenAffiliatedCredentialOverExactMatch) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      password_manager::features::kActorLoginPermissionsUseStrongAffiliations);
 
   // The origin where the credential is being filled.
   url::Origin current_origin = url::Origin::Create(GURL("https://example.com"));
@@ -2350,11 +2347,8 @@ TEST_P(ActorLoginCredentialFillerTest,
 TEST_P(ActorLoginCredentialFillerTest,
        UsesChosenAffiliatedCredentialOverExactMatch_InIframe) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitWithFeatures(
-      /*enabled_features=*/
-      {password_manager::features::kActorLoginSameSiteIframeSupport,
-       password_manager::features::kActorLoginPermissionsUseStrongAffiliations},
-      /*disabled_features=*/{});
+  feature_list.InitAndEnableFeature(
+      password_manager::features::kActorLoginSameSiteIframeSupport);
 
   // The origin where the credential is being filled.
   url::Origin current_origin = url::Origin::Create(GURL("https://example.com"));
