a287718b9408ef2877b51a8c16d61d9837357e32
Nicolas Dossou-Gbete <dgn@chromium.org>
2026-04-28 01:21:02 -0700

    rcaps: Clean up RestrictLegacySearchEnginePromoOnFormFactors
    The feature has shipped in Chrome M144
    
    Bug: 444652415
    Change-Id: Ia0bd927351028e36f78ef4d72ac1abaef3fad7f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796977
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621593}

--- components/regional_capabilities/regional_capabilities_switches.cc
+++ components/regional_capabilities/regional_capabilities_switches.cc
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 #if BUILDFLAG(IS_ANDROID)
-BASE_FEATURE(kRestrictLegacySearchEnginePromoOnFormFactors,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kResolveRegionalCapabilitiesFromDevice,
              base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
