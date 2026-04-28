dc75104c7f63be788a467539ca024f773128b7d7
Chris Lu <thegreenfrog@google.com>
2026-04-28 09:21:06 -0700

    [ios] Update AppBar button title truncation for landscape mode
    This change updates buttonTitleWithFullTitle:truncatedTitle: to use the view's height when the content is rotated.
    
    Bug: 503232107
    Change-Id: Id9781ae06bed8c049aa193af1ebf531d7e620466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796942
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621821}

--- ios/chrome/browser/app_bar/ui/app_bar_view_controller.mm
+++ ios/chrome/browser/app_bar/ui/app_bar_view_controller.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/app_bar/ui/app_bar_view_controller.h"
 
+#import <CoreGraphics/CoreGraphics.h>
+
 #import <optional>
 
 #import "base/metrics/user_metrics.h"
@@ -153,6 +155,8 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
   CGFloat _buttonsTitleAlpha;
   // Background view for the IPH.
   AppBarIPHBackgroundView* _IPHBackgroundView;
+  // Whether the App Bar content is rotated.
+  BOOL _isRotated;
 }
 
 #pragma mark - Accessors & Mutators
@@ -175,12 +179,14 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
 - (void)updateForAngle:(CGFloat)angle {
   [self loadViewIfNeeded];
 
+  _isRotated = (angle != 0);
+
   CGAffineTransform transform = CGAffineTransformMakeRotation(angle);
   _assistantButton.transform = transform;
   _openNewTabButton.transform = transform;
   _tabGridButton.transform = transform;
 
-  [self updateStackViewConstraintsForPortrait:(angle == 0)];
+  [self updateStackViewConstraintsForPortrait:!_isRotated];
 }
 
 - (void)toggleSpotlightView:(BOOL)shouldShow {
@@ -518,10 +524,16 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
     NSFontAttributeName : AssistantButtonFontSize(self.traitCollection)
   }];
 
-  CGFloat availableWidthForButton =
-      (self.view.bounds.size.width - 2 * kStackViewHorizontalMargin -
-       2 * kStackViewSpacing) /
-      3.0;
+  CGFloat availableWidthForButton;
+  if (_isRotated) {
+    availableWidthForButton = self.view.bounds.size.height;
+  } else {
+    availableWidthForButton =
+        (self.view.bounds.size.width - 2 * kStackViewHorizontalMargin -
+         2 * kStackViewSpacing) /
+        3.0;
+  }
+
   CGFloat availableWidthForTitle =
       availableWidthForButton - 2 * kButtonHorizontalPadding;
 
