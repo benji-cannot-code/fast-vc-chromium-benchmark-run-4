058dd0c867c89d82de9e5f5c1a2fe9983db385ac
Louis Romero <lpromero@google.com>
2026-04-28 07:35:09 -0700

    Implement JavaScript message passing for AI Mode assistant sheet
    This CL introduces the `AimCobrowseJavaScriptFeature` to enable native
    iOS code to send messages to the AI Mode SRP loaded within the special
    WebState. This replaces the previous method of refining queries by
    reloading the page with new URL parameters.
    
    Key changes include:
    -   New `AimCobrowseJavaScriptFeature` to inject and call JavaScript on
        the Google Search origin.
    -   `aim_cobrowse.ts` script to handle incoming messages on the web
        side.
    -   Updated `AssistantAIMMediator` to use
        `AimCobrowseJavaScriptFeature::PostMessage` for sending
        `ClientToAimMessage` protos.
    -   Added `kGoogleSearch` to `web::OriginFilter`.
    -   New unit tests for the mediator and JavaScript feature.
    
    This allows for more flexible communication with the AIM SRP.
    
    Context: go/aim-cobrowse-native-ui-aim-srp-communication-dd
    Bug: 484306645, 485526631
    Change-Id: I1db73e1f162e1e971de36e447b986985144e946b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7660844
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621752}

---
+++ ios/chrome/browser/cobrowse/model/aim_cobrowse.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+
+/**
+ * @fileoverview API for the AIM Cobrowse feature.
+ */
+
+/**
+ * Posts a message to the window.
+ * @param {string} base64Message The base64 encoded message.
+ */
+function postMessage(base64Message: string): void {
+  const binary = atob(base64Message);
+  const queryMsg = Uint8Array.from(binary, c => c.charCodeAt(0));
+  window.postMessage(queryMsg, window.location.origin);
+}
+
+const aimCobrowseApi = new CrWebApi('aimCobrowse');
+
+aimCobrowseApi.addFunction('postMessage', postMessage);
+
+gCrWeb.registerApi(aimCobrowseApi);
