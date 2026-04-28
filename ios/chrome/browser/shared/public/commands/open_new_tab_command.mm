e4f3f46de9335f84da34ffa8015025d793c2044b
Michael Tatarski <mtatarski@google.com>
2026-04-28 12:55:50 -0700

    [STTS] Add Send Tab to Self signal to iOS navigation stack
    This CL adds the infrastructure to carry the Send Tab to Self (STTS)
    signal (entry GUID) through the navigation stack without relying on URL
    text fragments. This is preparatory work for form fields propagation
    through STTS on iOS.
    
    - Adds `sendTabToSelfEntryGUID` to `OpenNewTabCommand` and `UrlLoadParams`.
    - Introduces `FromOpenNewTabCommand` in `UrlLoadParams` to simplify conversion logic in `scene_coordinator.mm`.
    
    Bug: 485145029
    Change-Id: I071c1e5c9da4aa2aea7cf2de6d52714496d8d079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796794
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621981}

--- ios/chrome/browser/shared/public/commands/open_new_tab_command.mm
+++ ios/chrome/browser/shared/public/commands/open_new_tab_command.mm
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize originPoint = _originPoint;
 @synthesize extraHeaders = _extraHeaders;
 @synthesize fromChrome = _fromChrome;
+@synthesize fromSendTabToSelf = _fromSendTabToSelf;
+@synthesize sendTabToSelfEntryGUID = _sendTabToSelfEntryGUID;
 @synthesize appendTo = _appendTo;
 @synthesize userInitiated = _userInitiated;
 @synthesize shouldFocusOmnibox = _shouldFocusOmnibox;
--- ios/chrome/browser/url_loading/model/url_loading_params.mm
+++ ios/chrome/browser/url_loading/model/url_loading_params.mm
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/url_loading/model/url_loading_params.h"
 
+#import "base/strings/sys_string_conversions.h"
+#import "ios/web/public/navigation/referrer.h"
+#import "url/gurl.h"
+
 UrlLoadParams UrlLoadParams::InCurrentTab(
     const web::NavigationManager::WebLoadParams& web_params) {
   UrlLoadParams params = UrlLoadParams();
@@ -55,6 +59,28 @@ UrlLoadParams UrlLoadParams::InNewTab(const GURL& url, int insertion_index) {
   params.web_params = web::NavigationManager::WebLoadParams(url);
   params.append_to = OpenPosition::kSpecifiedIndex;
   params.insertion_index = insertion_index;
+  return params;
+}
+
+UrlLoadParams UrlLoadParams::FromOpenNewTabCommand(OpenNewTabCommand* command) {
+  UrlLoadParams params =
+      UrlLoadParams::InNewTab(command.URL, command.virtualURL);
+  if (command.textFragment.length) {
+    params.web_params.internal_scroll_to_text_fragment =
+        base::SysNSStringToUTF8(command.textFragment);
+  }
+  params.SetInBackground(command.inBackground);
+  params.web_params.referrer = command.referrer;
+  params.web_params.extra_headers = [command.extraHeaders copy];
+  params.in_incognito = command.inIncognito;
+  params.append_to = command.appendTo;
+  params.origin_point = command.originPoint;
+  params.from_chrome = command.fromChrome;
+  params.send_tab_to_self_entry_guid =
+      base::SysNSStringToUTF8(command.sendTabToSelfEntryGUID);
+  params.user_initiated = command.userInitiated;
+  params.should_focus_omnibox = command.shouldFocusOmnibox;
+  params.inherit_opener = !command.inBackground;
   return params;
 }
 
