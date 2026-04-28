ce7e60b8baa4422e6bf772e62e75944cb1b64744
Bruno Braga <brunobraga@google.com>
2026-04-28 05:01:12 -0700

    Autofill private API handles ADM/PayDM not yet loaded
    Because loading edm is an async task, by the time profiles are fetched
    it should be expected that the loaded is not done yet. Therefore,
    instead of returning an error, an empty list is returned instead.
    
    Bug: 498417595
    Change-Id: I385512c83f8b675f39c1c71c93d6c313e743158b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7798859
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621678}

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
@@ -260,8 +260,7 @@ ExtensionFunction::ResponseAction AutofillPrivateGetAccountInfoFunction::Run() {
   }
 
   if (!adm->has_initial_load_finished()) {
-    return RespondNow(Error(base::StrCat(
-        {"Get account info - ", kErrorAddressDataManagerLoadingUnfinished})));
+    return RespondNow(NoArguments());
   }
 
   std::optional<api::autofill_private::AccountInfo> account_info =
@@ -447,8 +446,8 @@ ExtensionFunction::ResponseAction AutofillPrivateGetAddressListFunction::Run() {
         {"Get address list - ", kErrorAddressDataManagerUnavailable})));
   }
   if (!adm->has_initial_load_finished()) {
-    return RespondNow(Error(base::StrCat(
-        {"Get address list - ", kErrorAddressDataManagerLoadingUnfinished})));
+    return RespondNow(ArgumentList(
+        api::autofill_private::GetAddressList::Results::Create({})));
   }
   autofill_util::AddressEntryList address_list =
       autofill_util::GenerateAddressList(*adm);
@@ -641,9 +640,8 @@ AutofillPrivateGetCreditCardListFunction::Run() {
         {"Get credit card list - ", kErrorPaymentsDataManagerUnavailable})));
   }
   if (!paydm->is_payments_data_loaded()) {
-    return RespondNow(
-        Error(base::StrCat({"Get credit card list - ",
-                            kErrorPaymentsDataManagerLoadingUnfinished})));
+    return RespondNow(ArgumentList(
+        api::autofill_private::GetCreditCardList::Results::Create({})));
   }
   autofill_util::CreditCardEntryList credit_card_list =
       autofill_util::GenerateCreditCardList(*paydm);
@@ -766,8 +764,8 @@ ExtensionFunction::ResponseAction AutofillPrivateGetIbanListFunction::Run() {
         {"Get iban list - ", kErrorPaymentsDataManagerUnavailable})));
   }
   if (!paydm->is_payments_data_loaded()) {
-    return RespondNow(Error(base::StrCat(
-        {"Get iban list - ", kErrorPaymentsDataManagerLoadingUnfinished})));
+    return RespondNow(
+        ArgumentList(api::autofill_private::GetIbanList::Results::Create({})));
   }
   autofill_util::IbanEntryList iban_list =
       autofill_util::GenerateIbanList(*paydm);
@@ -879,9 +877,8 @@ AutofillPrivateGetPayOverTimeIssuerListFunction::Run() {
                             kErrorPaymentsDataManagerUnavailable})));
   }
   if (!paydm->is_payments_data_loaded()) {
-    return RespondNow(
-        Error(base::StrCat({"Get pay over time issuer list - ",
-                            kErrorPaymentsDataManagerLoadingUnfinished})));
+    return RespondNow(ArgumentList(
+        api::autofill_private::GetPayOverTimeIssuerList::Results::Create({})));
   }
   autofill_util::PayOverTimeIssuerEntryList pay_over_time_issuer_list =
       autofill_util::GeneratePayOverTimeIssuerList(*paydm);
