3e5c16fa59f967a51e8a3dfe5eaacd8562136b8d
Friedrich Horschig <friedrichh@chromium.org>
2026-04-28 07:07:22 -0700

    [Record Replay] Implement feature parameter annotation seeding
    This CL implements the ability to seed the Record Replay capabilities
    database with annotations provided directly via a feature parameter.
    This allows pre-populating the database with the command line.
    
    Feature Parameter: RecordReplayAnnotationSeed
    
    Feature: RecordReplayBase
    Bug: 496187924
    Change-Id: I0c6cd46ecc92780905af54bc32539199ba7eafd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790341
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621736}

---
+++ components/record_replay/core/browser/parsing_utils.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/record_replay/core/browser/parsing_utils.h"
+
+#include "base/json/json_reader.h"
+#include "base/values.h"
+
+namespace record_replay {
+
+std::vector<base::Value> ParseJSONListOfDicts(std::string_view json_string) {
+  std::vector<base::Value> dicts;
+  auto value = base::JSONReader::Read(json_string,
+                                      base::JSONParserOptions::JSON_PARSE_RFC);
+  if (!value || !value->is_list()) {
+    return dicts;
+  }
+
+  for (const auto& item : value->GetList()) {
+    if (item.is_dict()) {
+      dicts.push_back(item.Clone());
+    }
+  }
+  return dicts;
+}
+
+}  // namespace record_replay
---
+++ components/record_replay/core/browser/parsing_utils.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_RECORD_REPLAY_CORE_BROWSER_PARSING_UTILS_H_
+#define COMPONENTS_RECORD_REPLAY_CORE_BROWSER_PARSING_UTILS_H_
+
+#include <string_view>
+#include <vector>
+
+#include "base/values.h"
+
+namespace record_replay {
+
+// Parses a JSON string expecting a list of dictionaries.
+// Returns a vector of dictionaries on success, or an empty vector on failure.
+std::vector<base::Value> ParseJSONListOfDicts(std::string_view json_string);
+
+}  // namespace record_replay
+
+#endif  // COMPONENTS_RECORD_REPLAY_CORE_BROWSER_PARSING_UTILS_H_
