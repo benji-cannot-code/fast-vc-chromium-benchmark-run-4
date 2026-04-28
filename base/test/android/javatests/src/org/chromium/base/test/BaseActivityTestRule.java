dd9ef1cd6aeb0bcff43b368c425f6dbae687cea7
Henrique Nakashima <hnakashima@chromium.org>
2026-04-28 16:33:05 -0700

    [Android] Fix @ClassRule leak in BaseActivityTestRule
    The file BaseActivityTestRule retains a reference to the activity when
    used as a @ClassRule and another rule disables activity finishing (like
    BlankCTATabInitialStateRule calling setFinishActivity(false)). This
    happens because after() skips finishing the activity if mFinishActivity
    is false.
    
    The solution is to detect when the rule is used as a @ClassRule in
    apply() (by checking if description.getMethodName() is null) and force
    the activity to be finished in after(), ignoring mFinishActivity.
    
    This solves the leak in tests like SiteSettingsTest which use this
    combination of rules. BlankCTATabInitialStateRule is used usually in
    combination with a @ClassRule PermissionTestRule (which derives from
    BaseActivityTestRule), so this fixes the leak in these cases.
    
    Ideally we would migrate these tests from BlankCTATabInitialStateRule
    to AutoResetCtaTransitTestRule, and make PermissionTestRule not a
    BaseActivityTestRule anymore, which also would avoid the problem.
    
    Bug: 507078523
    Change-Id: Ibcf62555b21fb322bd37c374a4e613776c3a26bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802054
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622105}

--- base/test/android/javatests/src/org/chromium/base/test/BaseActivityTestRule.java
+++ base/test/android/javatests/src/org/chromium/base/test/BaseActivityTestRule.java
@@ -28,6 +28,8 @@ import com.google.android.apps.common.testing.accessibility.framework.checks.Tou
 
 import org.junit.Assert;
 import org.junit.rules.ExternalResource;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
@@ -59,6 +61,7 @@ public class BaseActivityTestRule<T extends Activity> extends ExternalResource {
     private final Class<T> mActivityClass;
     private boolean mFinishActivity = true;
     private T mActivity;
+    private boolean mIsClassRule;
 
     /**
      * @param activityClass The Class of the Activity the TestRule will use.
@@ -113,12 +116,18 @@ public class BaseActivityTestRule<T extends Activity> extends ExternalResource {
     }
 
     @Override
+    public Statement apply(Statement base, Description description) {
+        mIsClassRule = (description.getMethodName() == null);
+        return super.apply(base, description);
+    }
+
+    @Override
     @CallSuper
     protected void after() {
         try {
             ensureSoftKeyboardIsHidden();
         } finally {
-            if (mFinishActivity && mActivity != null) {
+            if ((mIsClassRule || mFinishActivity) && mActivity != null) {
                 finishActivity();
             }
         }
