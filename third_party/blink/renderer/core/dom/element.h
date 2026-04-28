34e1e29f1e5e341a283372056219e394bb87cd2a
Kent Tamura <tkent@chromium.org>
2026-04-28 09:03:33 -0700

    Fix Autofill preview length leak via textarea.scrollTop
    When Autofill previews a saved value into a <textarea>, the secret
    string is rendered via a UA-shadow placeholder <div> whose laid-out
    block-size scales with the secret's character count. That <div> is
    included in the textarea's PaintLayerScrollableArea::overflow_rect_.
    
    While HTMLTextAreaElement::scrollWidth()/scrollHeight() were overridden
    to mask this, Element::scrollTop was not. This allowed page scripts to
    recover the glyph count of the previewed value.
    
    This CL makes Element::scrollTop() virtual and overrides it in
    HTMLTextAreaElement to return 0 when a suggested value is set.
    
    BUG: 506163510
    Change-Id: I38373b6f6a76e3d8b4721c5612af4763ebd7856f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794708
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621812}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -567,7 +567,7 @@ class CORE_EXPORT Element : public ContainerNode {
   int clientHeight();
   double currentCSSZoom();
   double scrollLeft();
-  double scrollTop();
+  virtual double scrollTop();
   void setScrollLeft(double);
   void setScrollTop(double);
   virtual int scrollWidth();
