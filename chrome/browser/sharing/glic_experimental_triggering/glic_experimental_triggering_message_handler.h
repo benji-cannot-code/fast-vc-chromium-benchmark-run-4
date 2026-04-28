a4a8c25420a6e273934c7c7bde443a220deca120
Roman Arora <romanarora@chromium.org>
2026-04-28 16:52:39 -0700

    Reland "[experimental-triggering] Stop local browser task capability"
    This is a reland of commit 5dcc571dff5161b670de118ec0fd8247cdafd6bd
    Reason:
    Conflicted with new changes in https://chromium-review.git.corp.google.com/c/chromium/src/+/7763464
    that landed on same day.
    
    Original change's description:
    > [experimental-triggering] Stop local browser task capability
    >
    > Adds logic to handle incoming stop actuation requests.
    >
    > Bug: b:495921045
    > Change-Id: I31b8c8911528d07910061bb8e758897356aa5286
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794122
    > Auto-Submit: Roman Arora <romanarora@chromium.org>
    > Commit-Queue: Roman Arora <romanarora@chromium.org>
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1622011}
    
    Bug: b:495921045
    Change-Id: I27589a25c1eb93237e2f1e24bf870791e8129172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800702
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622119}

--- chrome/browser/sharing/glic_experimental_triggering/glic_experimental_triggering_message_handler.h
+++ chrome/browser/sharing/glic_experimental_triggering/glic_experimental_triggering_message_handler.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace glic {
 class GlicInstance;
+class GlicKeyedService;
 }  // namespace glic
 
 class Profile;
@@ -51,6 +52,12 @@ class GlicExperimentalTriggeringMessageHandler : public SharingMessageHandler {
       components_sharing_message::ServerChannelConfiguration server_channel,
       std::optional<int64_t> last_seen_sequence_number,
       glic::GlicInstance* instance);
+
+  void ProcessStopActionRequest(
+      components_sharing_message::SharingMessage message,
+      tabs::TabInterface* active_tab,
+      glic::GlicKeyedService* glic_service,
+      DoneCallback done_callback);
 
   const raw_ptr<Profile> profile_;
 
