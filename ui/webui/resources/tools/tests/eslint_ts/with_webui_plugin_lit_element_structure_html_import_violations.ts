209b6d4f425fb85ac10dd8ffd3e76e1961eba91d
rbpotter <rbpotter@chromium.org>
2026-04-28 11:01:46 -0700

    WebUI: Add check for html imports in CrLitElement subclass def. files
    This ensures templates are properly placed in .html.ts template files
    for such classes, so that they will get proper template validation.
    
    Bug: 506193591
    Change-Id: Ie0d75b9c566ce22a33c90ae21bdb878fec6762b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794465
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1621907}

---
+++ ui/webui/resources/tools/tests/eslint_ts/with_webui_plugin_lit_element_structure_html_import_violations.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement, html} from '//resources/lit/v3_0/lit.rollup.js';
+
+export class TestHtmlImportErrorElement extends CrLitElement {
+  static get is() {
+    return 'test-html-import-error';
+  }
+
+  override render() {
+    return html`<div></div>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'test-html-import-error': TestHtmlImportErrorElement;
+  }
+}
+
+customElements.define(
+    TestHtmlImportErrorElement.is, TestHtmlImportErrorElement);
