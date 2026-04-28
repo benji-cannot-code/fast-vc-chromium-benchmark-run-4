b9f504c84d72441e6acca1ec224651d15a3b155a
Isabelle Goralsky <igoralsky@google.com>
2026-04-28 15:34:06 -0700

    Add asynchronous debug UI data fetching for Accessibility Annotator.
    This change introduces `GetAnnotationsForDebugUI` in
    `AccessibilityAnnotatorBackend`, which asynchronously fetches content
    annotations for the debug UI. When the
    `kAccessibilityAnnotatorDatabaseStorage` feature is enabled, this method
    reads from the database. Otherwise, it falls back to the in-memory
    cache. In a follow-up CL, the debug UI frontend will be updated to use
    this new asynchronous method and the original `GetDebugUICacheData` will
    be removed.
    
    Bug: b:496386554
    Change-Id: I5c11d6972168ff2cad43587708cc448a1f741778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787270
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622077}

--- components/accessibility_annotator/core/storage/test_accessibility_annotator_backend.h
+++ components/accessibility_annotator/core/storage/test_accessibility_annotator_backend.h
@@ -60,6 +60,10 @@ class TestAccessibilityAnnotatorBackend : public AccessibilityAnnotatorBackend {
   MOCK_METHOD(void, ClearContentAnnotationsCache, (), (override));
   MOCK_METHOD(base::Value, GetDebugUICacheData, (), (const, override));
   MOCK_METHOD(void,
+              GetAnnotationsForDebugUI,
+              (base::OnceCallback<void(base::Value)>),
+              (override));
+  MOCK_METHOD(void,
               AddContentAnnotation,
               (history::VisitID,
                ContentAnnotationsData,
