701642ed32773f71fa380c6b9edb4cda93cabcf5
Jeffrey Yu <yuje@chromium.org>
2026-04-29 14:18:49 -0700

    Refactor AutocompleteMatch to use string_view and avoid string copies.
    -  Correctly captures the result of url_formatter::FormatUrl by value.
    -  Uses std::u16string_view for substrings to avoid creating new string objects.
    -  Update ClassificationsFromString to accept std::string_view.
    -  Use erase() for in-place modification of inline_autocompletion.
    -  Use base::StrCat and base::StrAppend to avoid repeated concatenation
    
    Bug: 501076278
    Change-Id: Ie741b59c37e7366600dc5cbf8e48ffe94825a20d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797391
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Jeffrey Yu <yuje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622726}

--- components/omnibox/browser/autocomplete_match.h
+++ components/omnibox/browser/autocomplete_match.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <ranges>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -396,7 +397,7 @@ struct AutocompleteMatch {
   static std::string ClassificationsToString(
       const ACMatchClassifications& classifications);
   static ACMatchClassifications ClassificationsFromString(
-      const std::string& serialized_classifications);
+      std::string_view serialized_classifications);
 
   // Adds a classification to the end of |classifications| iff its style is
   // different from the last existing classification.  |offset| must be larger
