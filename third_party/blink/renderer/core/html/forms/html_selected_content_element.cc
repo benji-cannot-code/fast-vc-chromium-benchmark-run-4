b51462acaeb37b73d9a7a91690c064edeb768b10
Joey Arhar <jarhar@chromium.org>
2026-04-28 22:39:28 -0700

    Don't update selectedcontent in option insertion/removal steps
    Modifying the DOM by updating selectedcontent elements during insertion
    or removal steps is bad for security reasons. We already removed the
    selectedcontent element removal steps which was one way this can happen,
    but the option's InsertedInto and RemovedFrom methods may change which
    option is selected, which will synchronously update the selectedcontent
    element.
    
    This patch fixes this by moving the selectedcontent updating to the
    post-insertion steps for insertion, and by using a microtask to update
    on removal if needed.
    
    This matches the behavior in the spec PR: https://github.com/whatwg/html/pull/12263
    
    Bug: 458113204
    Change-Id: I42bb94c6eace93445cfbc816529e42ca8a561b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745871
    Reviewed-by: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622241}

--- third_party/blink/renderer/core/html/forms/html_selected_content_element.cc
+++ third_party/blink/renderer/core/html/forms/html_selected_content_element.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/html/forms/html_selected_content_element.h"
 
+#include "third_party/blink/renderer/core/dom/events/event_dispatch_forbidden_scope.h"
 #include "third_party/blink/renderer/core/html/forms/html_option_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/html_collection.h"
+#include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 
 namespace blink {
 
@@ -18,6 +20,13 @@ void HTMLSelectedContentElement::CloneContentsFromOptionElement(
     const HTMLOptionElement* option) {
   if (disabled_) {
     return;
+  }
+
+  if (RuntimeEnabledFeatures::SelectedcontentSpecEnabled()) {
+    DCHECK(!ScriptForbiddenScope::IsScriptForbidden());
+#if DCHECK_IS_ON()
+    DCHECK(!EventDispatchForbiddenScope::IsEventDispatchForbidden());
+#endif
   }
 
   VectorOf<Node> nodes;
