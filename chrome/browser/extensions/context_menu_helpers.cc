2555e05d5e1505131e106a433f1752f1c73600cf
Miguel Ruiz <migueloruiz@google.com>
2026-04-29 10:05:38 -0700

    [Extensions] Tab Context Menu Support for Extensions
    Chrome Extension API Modification:
    https://docs.google.com/document/d/1WU2YUnnQitMcRsOU7FPGN6N0SviJysPIb3l7YhonSmo
    
    This CL implements support for the feature tracked in
    crbug.com/40246822, drawing design and implementation structures from
    previous patches:
    - https://chromium-review.googlesource.com/c/chromium/src/+/7524428
    - https://chromium-review.googlesource.com/c/chromium/src/+/6488651
    
    Enables extensions to contribute custom context menu items to the
    browser's tab context menu, gated behind the kExtensionTabContextMenu
    feature flag. Requested in crbug.com/40246822
    
    - Flags & Schema: Added `kExtensionTabContextMenu` feature flag
    and "tab" context to context_menus.json API schema.
    - Core Enablers: Mapped `MenuItem::TAB` in `menu_manager.h`
    and `context_menu_helpers.cc` correctly.
    - Model Integration: Updated `TabMenuModel` overrides
    (`IsVisibleAt`, `IsEnabledAt`, `IsItemCheckedAt`, `ActivatedAt`) to
    delegate directly to `ContextMenuMatcher`, relying on its native
    separator logic instead of manual layout appending.
    
    ## Automated Tests
    - Unit Tests: Added a comprehensive `ExtensionItems` suite to
    `tab_menu_model_unittest.cc` confirming offset sizing using baseline
    models.
    - Browser Tests: Added
    `ExtensionContextMenuApiTest.ContextMenusTabContext` to
    `context_menu_apitest.cc` to verify that extensions can add items to the
    tab context menu and that they are correctly displayed and enabled.
    
    ## Manual Testings
    
    Use a simple extension to inject a context menu that logs when is tapped
    https://screenshot.googleplex.com/7WUPUWixgWvsGKt
    
    Bug: 40246822
    Bug: 498631043
    Change-Id: I0bee790e4ee18a4f2b29cdf71d0ea0de7eef5ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689933
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Miguel Ruiz <migueloruiz@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622541}

--- chrome/browser/extensions/context_menu_helpers.cc
+++ chrome/browser/extensions/context_menu_helpers.cc
@@ -154,6 +154,10 @@ MenuItem::ContextList GetContexts(
         // Not available for <webview>.
         contexts.Add(MenuItem::ACTION);
         break;
+      case api::context_menus::ContextType::kTab:
+        // Not available for <webview>.
+        contexts.Add(MenuItem::TAB);
+        break;
       case api::context_menus::ContextType::kNone:
         NOTREACHED();
     }
---
+++ chrome/test/data/extensions/api_test/context_menus/tab_context/background.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  async function createTabContextMenu() {
+    const eventPromise = chrome.test.listenOnce(chrome.contextMenus.onClicked);
+
+    await chrome.contextMenus.create({
+      title: "Test Tab Item",
+      contexts: ["tab"],
+      id: "test_item"
+    });
+
+    // Send a message to the C++ to indicate the item has been created.
+    // The C++ side will verify its presence in the menu and click on it.
+    chrome.test.sendMessage("created");
+
+    // Which should trigger our listener.
+    const args = await eventPromise;
+    chrome.test.assertEq(2, args.length);
+
+    const info = args[0];
+    chrome.test.assertEq("test_item", info.menuItemId);
+    chrome.test.assertEq(false, info.editable);
+    chrome.test.assertEq("about:blank", info.pageUrl);
+
+    const tab = args[1];
+    chrome.test.assertTrue(Number.isInteger(tab.id));
+    chrome.test.assertTrue(Number.isInteger(tab.windowId));
+
+    chrome.test.succeed();
+  },
+]);
