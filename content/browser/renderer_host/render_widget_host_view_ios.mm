f32a26d0eb578e14c1a3192fb8b901a9642e0966
Gyuyoung Kim <gyuyoung@igalia.com>
2026-04-29 08:34:27 -0700

    [iOS/tvOS Blink] Update UpdateCALayerTree() override
    https://crrev.com/c/7790009 removed the const keyword in the argument,
    so RenderWidgetHostView[IOS|tvOS]::UpdateCALayerTree should apply the
    change as well.
    
    Bug: 498266556
    Change-Id: I3a6de26112953c60502a6299265b02c048cc42d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802814
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622475}

--- content/browser/renderer_host/render_widget_host_view_ios.mm
+++ content/browser/renderer_host/render_widget_host_view_ios.mm
@@ -471,7 +471,7 @@ void RenderWidgetHostViewIOS::OnSynchronizedDisplayPropertiesChanged(
 }
 
 void RenderWidgetHostViewIOS::UpdateCALayerTree(
-    const gfx::CALayerParams& ca_layer_params) {}
+    gfx::CALayerParams ca_layer_params) {}
 
 void RenderWidgetHostViewIOS::OnOldViewDidNavigatePreCommit() {
   CHECK(browser_compositor_) << "Shouldn't be called during destruction!";
