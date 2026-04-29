3a4e5897be26a7ebba318710fab3410bb16cbf5e
Sophie Chang <sophiechang@chromium.org>
2026-04-28 19:32:26 -0700

    Do not show cue if an infobar is visible
    Bug: 500927915
    Change-Id: If22113cf1f0c3f70e04a1ab7854739cd84bbccde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801860
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622185}

--- chrome/browser/contextual_cueing/contextual_cueing_controller.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_controller.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/side_panel/side_panel_ui_provider.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "components/google/core/common/google_util.h"
+#include "components/infobars/content/content_infobar_manager.h"
 #include "components/optimization_guide/core/optimization_guide_common.mojom.h"
 #include "components/optimization_guide/core/optimization_guide_util.h"
 #include "components/optimization_guide/proto/features/contextual_cueing.pb.h"
@@ -431,6 +432,13 @@ bool ContextualCueingController::IsAllowedToShowCue() {
     return false;
   }
 #endif
+
+  auto* infobar_manager = infobars::ContentInfoBarManager::FromWebContents(
+      tab_list_interface_->GetActiveTab()->GetContents());
+  if (infobar_manager && !infobar_manager->infobars().empty()) {
+    RecordContextualCueingDecision(ContextualCueingDecision::kInfobarVisible);
+    return false;
+  }
 
   if (auto* side_panel_ui =
           SidePanelUIProvider::From(browser_window_interface_);
