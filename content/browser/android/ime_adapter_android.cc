4399706e9547c029a65f4455aec6ea7bb7a3a71f
Chuong Ho <hdchuong@google.com>
2026-04-29 07:03:08 -0700

    autocorrect: add v2 flag in ime adapter android
    Bug: b:506971833
    
    Change-Id: I997058a0e97d0af9f13a158b53290c81d2064b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800779
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Auto-Submit: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622415}

--- content/browser/android/ime_adapter_android.cc
+++ content/browser/android/ime_adapter_android.cc
@@ -690,7 +690,9 @@ void ImeAdapterAndroid::PerformSpellCheck(JNIEnv* env) {
 void ImeAdapterAndroid::AppendAutocorrectUnderlineSpan(JNIEnv* env,
                                                        int32_t start,
                                                        int32_t end) {
-  if (!base::FeatureList::IsEnabled(features::kAndroidPkAutocorrectUnderline)) {
+  if (!base::FeatureList::IsEnabled(features::kAndroidPkAutocorrectUnderline) &&
+      !base::FeatureList::IsEnabled(
+          features::kAndroidPkAutocorrectUnderlineV2)) {
     return;
   }
   blink::mojom::FrameWidgetInputHandler* input_handler =
@@ -711,7 +713,9 @@ void ImeAdapterAndroid::AppendAutocorrectUnderlineSpan(JNIEnv* env,
 }
 
 void ImeAdapterAndroid::ClearAllAutocorrectUnderlineSpans(JNIEnv* env) {
-  if (!base::FeatureList::IsEnabled(features::kAndroidPkAutocorrectUnderline)) {
+  if (!base::FeatureList::IsEnabled(features::kAndroidPkAutocorrectUnderline) &&
+      !base::FeatureList::IsEnabled(
+          features::kAndroidPkAutocorrectUnderlineV2)) {
     return;
   }
   blink::mojom::FrameWidgetInputHandler* input_handler =
