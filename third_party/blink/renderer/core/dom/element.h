5aacb52a8135e1ebb30a4b3030b5a833e1610653
Kent Tamura <tkent@chromium.org>
2026-04-29 08:49:16 -0700

    Fix Autofill preview length leak via textarea.scrollLeft
    This CL makes Element::scrollLeft() virtual and overrides it in
    HTMLTextAreaElement to return 0 when a suggested value is set.
    
    BUG: 506163510
    Change-Id: I940be7ce0624daa88daafb76a209ead7d901f904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802477
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622482}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -566,7 +566,7 @@ class CORE_EXPORT Element : public ContainerNode {
   int clientWidth();
   int clientHeight();
   double currentCSSZoom();
-  double scrollLeft();
+  virtual double scrollLeft();
   virtual double scrollTop();
   void setScrollLeft(double);
   void setScrollTop(double);
