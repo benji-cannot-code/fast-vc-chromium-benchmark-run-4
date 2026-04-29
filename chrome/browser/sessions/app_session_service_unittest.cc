f032c900745f89897d77c77913e39876772921c8
Daniel Rubery <drubery@chromium.org>
2026-04-29 14:40:53 -0700

    [bedrock] Remove Browser* from app_session_service_unittest
    This test only used BrowserWithTestWindowTest for its dependency on
    BrowserTaskEnvironment and Profile. We can easily add those to the
    unit test fixture.
    
    Fixed: 507743687
    Link: https://chromium-review.googlesource.com/id/Iea4b03ba42173209e191548e6899d8f26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802575
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622743}

--- chrome/browser/sessions/app_session_service_unittest.cc
+++ chrome/browser/sessions/app_session_service_unittest.cc
@@ -13,12 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/buildflags.h"
 #include "chrome/browser/sessions/app_session_service_test_helper.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/sessions/content/content_serialized_navigation_builder.h"
 #include "components/sessions/content/content_test_helper.h"
 #include "components/sessions/core/serialized_navigation_entry_test_helper.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/mojom/window_show_state.mojom.h"
 
@@ -37,7 +38,7 @@ using sessions::SerializedNavigationEntryTestHelper;
 
 // Actual app restoration testing will be in
 // app_session_service_browsertests.cc
-class AppSessionServiceTest : public BrowserWithTestWindowTest {
+class AppSessionServiceTest : public testing::Test {
  public:
   AppSessionServiceTest() : window_bounds_(0, 1, 2, 3) {}
 
@@ -45,10 +46,7 @@ class AppSessionServiceTest : public BrowserWithTestWindowTest {
   // SetUp() opens 1 normal window and 1 app window to each of the respective
   // [app]SessionService classes.
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
-
-    app_session_service_ =
-        std::make_unique<AppSessionService>(browser()->profile());
+    app_session_service_ = std::make_unique<AppSessionService>(&profile_);
     app_helper_.SetService(app_session_service_.get());
 
     app_service()->SetWindowType(app_window_id, Browser::TYPE_APP);
@@ -64,8 +62,6 @@ class AppSessionServiceTest : public BrowserWithTestWindowTest {
 
   void TearDown() override {
     DestroyAppSessionService();
-
-    BrowserWithTestWindowTest::TearDown();
   }
 
   void DestroyAppSessionService() {
@@ -92,8 +88,7 @@ class AppSessionServiceTest : public BrowserWithTestWindowTest {
       SessionID* active_window_id) {
     DestroyAppSessionService();
 
-    app_session_service_ =
-        std::make_unique<AppSessionService>(browser()->profile());
+    app_session_service_ = std::make_unique<AppSessionService>(&profile_);
     app_helper_.SetService(app_session_service_.get());
 
     SessionID* non_null_active_window_id = active_window_id;
@@ -147,6 +142,9 @@ class AppSessionServiceTest : public BrowserWithTestWindowTest {
   const SessionID app_window_id = SessionID::NewUnique();
   const SessionID app_tab_id = SessionID::NewUnique();
   SerializedNavigationEntry app_nav;
+
+  content::BrowserTaskEnvironment task_environment_;
+  TestingProfile profile_;
 
   std::unique_ptr<AppSessionService> app_session_service_;
 
