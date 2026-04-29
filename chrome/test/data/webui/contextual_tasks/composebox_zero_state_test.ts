cf5970db0bc253252bbd346a8f80d7ff805178d6
Jilin Yang <jilinyang@google.com>
2026-04-28 17:25:50 -0700

    [composebox]: Replace lens-only flags with element properties
    This is the 2nd part of b:496905627
    
    Move 'composeboxShowTypedSuggestWithContext' and
    'composeboxNoFlickerSuggestionsFix' from loadTimeData reads in
    ComposeboxEmbedderMixin to accessor properties on <cr-composebox>.
    The Lens side panel (the only embedder that sets these flags to true)
    opts in via bare boolean attributes on its Polymer host tag.
    
    No behavior change.
    
    Fixed: b:506163915
    Change-Id: Id1e5d958a189a4b6353f17d5597624e18c7c954f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793089
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Jilin Yang <jilinyang@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622136}

--- chrome/test/data/webui/contextual_tasks/composebox_zero_state_test.ts
+++ chrome/test/data/webui/contextual_tasks/composebox_zero_state_test.ts
@@ -443,7 +443,6 @@ suite('ContextualTasksComposeboxZeroStateTest', () => {
 
   test('SuggestionsHiddenWhenDropdownNotShown', async () => {
     loadTimeData.overrideValues({
-      composeboxShowTypedSuggestWithContext: false,
       enableNativeZeroStateSuggestions: true,
     });
 
@@ -515,7 +514,7 @@ suite('ContextualTasksComposeboxZeroStateTest', () => {
     await composebox.updateComplete;
 
     // show-dropdown should be false because we have a file and
-    // composeboxShowTypedSuggestWithContext is false.
+    // showTypedSuggestWithContext is false.
     assertFalse(
         composebox.hasAttribute('show-dropdown'),
         'Dropdown should hide when typing with' +
--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -104,10 +104,12 @@ export const ComposeboxEmbedderMixin =
             },
             dropdownNeeded: {type: Boolean},
             closeOnEscape: {type: Boolean},
+            composeboxNoFlickerSuggestionsFix: {type: Boolean},
             showFileCarousel: {
               reflect: true,
               type: Boolean,
             },
+            showTypedSuggestWithContext: {type: Boolean},
             usePecApi: {
               type: Boolean,
               reflect: true,
@@ -156,8 +158,6 @@ export const ComposeboxEmbedderMixin =
             loadTimeData.getString('composeboxAttachmentFileTypes').split(',');
         imageFileTypes: string[] =
             loadTimeData.getString('composeboxImageFileTypes').split(',');
-        showTypedSuggestWithContext: boolean = getLoadTimeBoolean(
-            'composeboxShowTypedSuggestWithContext', /*defaultValue=*/ false);
         queryZpsOnLoad: boolean =
             getLoadTimeBoolean('queryZpsOnLoad', /*defaultValue=*/ true);
         clearAllInputsWhenSubmittingQuery: boolean = getLoadTimeBoolean(
@@ -166,6 +166,7 @@ export const ComposeboxEmbedderMixin =
 
         accessor canSubmitFilesAndInput: boolean = true;
         accessor closeOnEscape: boolean = true;
+        accessor composeboxNoFlickerSuggestionsFix: boolean = false;
         accessor contextMenuEnabled: boolean =
             loadTimeData.getBoolean('composeboxShowContextMenu');
         accessor errorMessage: string = '';
@@ -187,6 +188,7 @@ export const ComposeboxEmbedderMixin =
             loadTimeData.getBoolean('composeboxShowZps');
         accessor dropdownNeeded: boolean = true;
         accessor showFileCarousel: boolean = false;
+        accessor showTypedSuggestWithContext: boolean = false;
         accessor usePecApi: boolean = getLoadTimeBoolean(
             'contextualMenuUsePecApi', /*defaultValue=*/ false);
         showZps: boolean = loadTimeData.getBoolean('composeboxShowZps');
@@ -198,8 +200,6 @@ export const ComposeboxEmbedderMixin =
         accessor tabSuggestions: TabInfo[] = [];
         accessor transcript: string = '';
         accessor uploadButtonDisabled: boolean = false;
-        composeboxNoFlickerSuggestionsFix: boolean = getLoadTimeBoolean(
-            'composeboxNoFlickerSuggestionsFix', /*defaultValue=*/ false);
         showTypedSuggest: boolean =
             loadTimeData.getBoolean('composeboxShowTypedSuggest');
         lastQueriedInput: string = '';
