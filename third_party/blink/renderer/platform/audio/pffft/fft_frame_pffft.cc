b143f1d06d5826e635b16a7ad187e511bcbcc07f
Michael Wilson <mjwilson@chromium.org>
2026-04-29 03:58:40 -0700

    [WebAudio] Remove fft_frame_stub.cc and pffft build switch
    This should cause no functional change.
    
    fft_frame_stub.cc would not compile as of 2019 when the function
    signature for Initialize() changed. This is a strong indication it is
    not used in any downstream configurations. Remove it completely.
    
    Also remove the pffft build switch, and use the platform directly to
    determine if pffft should be used instead. The only platform that
    doesn't use pffft by default is mac, and there are no active experiments
    using pffft on mac to my knowledge.
    
    Change-Id: I02edd124651feb0c042aba064b38c7e1b696b8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7803076
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622336}

--- third_party/blink/renderer/platform/audio/pffft/fft_frame_pffft.cc
+++ third_party/blink/renderer/platform/audio/pffft/fft_frame_pffft.cc
@@ -3,7 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(WTF_USE_WEBAUDIO_PFFFT)
+#include "build/build_config.h"
+
+#if !BUILDFLAG(IS_MAC)
 
 #include "base/compiler_specific.h"
 #include "base/synchronization/lock.h"
@@ -251,4 +253,4 @@ void FFTFrame::DoInverseFFT(float* data) {
 
 }  // namespace blink
 
-#endif  // #if defined(WTF_USE_WEBAUDIO_PFFFT)
+#endif  // !BUILDFLAG(IS_MAC)
