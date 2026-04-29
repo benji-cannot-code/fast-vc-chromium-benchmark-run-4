dd423e7cbf11f456d6e20e3d637f4edcbfc3a29e
Joe Mason <joenotcharles@google.com>
2026-04-29 15:44:31 -0700

    Remove kTextInputClientUseNestedLoop feature
    Also removes explicit strong self refs that were added to keep objects
    on the stack alive during the nested RunLoop, but keeps a few other
    lifetime safety changes that were added for the experiment for
    future-proofing. The current impl is safe without them, but they don't
    hurt anything.
    
    As a cleanup, removes TextInputClientMacTestBase from the unit test,
    since it only has one subclass now.
    
    Bug: 475270375,503787240
    Change-Id: Id7b31fe4a4fd0356e44cd866e2c9d6bf68c5180f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7803816
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622787}

--- content/browser/renderer_host/text_input_client_mac.h
+++ content/browser/renderer_host/text_input_client_mac.h
@@ -12,11 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
-#include "base/run_loop.h"
 #include "base/synchronization/condition_variable.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_checker.h"
-#include "base/time/time.h"
 #include "base/types/token_type.h"
 #include "content/common/content_export.h"
 #include "ui/base/mojom/attributed_string.mojom-forward.h"
@@ -149,13 +147,12 @@ class CONTENT_EXPORT TextInputClientMac {
   TextInputClientMac();
   ~TextInputClientMac();
 
-  // Implementations of the public sync methods that take a WeakPtr and a copy
-  // of the argument, because EnterNestedLoop could invalidate pointers and
-  // references.
+  // Implementations of the public sync methods that take a WeakPtr, in case the
+  // the RenderFrameHost is deleted while waiting for the response.
   uint32_t GetCharacterIndexAtPoint(base::WeakPtr<RenderFrameHostImpl> rfhi,
-                                    gfx::Point point);
+                                    const gfx::Point& point);
   gfx::Rect GetFirstRectForRange(base::WeakPtr<RenderFrameHostImpl> rfhi,
-                                 gfx::Range range);
+                                 const gfx::Range& range);
 
   // The critical sections that the Condition guards are in Get*() methods.
   // These methods lock the internal condition for use before the asynchronous
@@ -169,10 +166,6 @@ class CONTENT_EXPORT TextInputClientMac {
   void AfterRequest() VALID_CONTEXT_REQUIRED(thread_checker_)
       UNLOCK_FUNCTION(lock_);
 
-  void EnterNestedLoop(base::TimeDelta timeout)
-      VALID_CONTEXT_REQUIRED(thread_checker_) EXCLUSIVE_LOCKS_REQUIRED(lock_);
-  void OnNestedLoopTimeout();
-
   THREAD_CHECKER(thread_checker_);
 
   std::optional<uint32_t> character_index_ GUARDED_BY(lock_);
@@ -180,11 +173,6 @@ class CONTENT_EXPORT TextInputClientMac {
   std::optional<RequestToken> current_request_ GUARDED_BY(lock_);
 
   base::Lock lock_;
-
-  // If kTextInputClientUseNestedLoop is enabled, sync functions are
-  // implemented with `nested_loop_`. Otherwise they're implemented with
-  // `condition_`.
-  std::optional<base::RunLoop> nested_loop_ GUARDED_BY(lock_);
   base::ConditionVariable condition_;
 
   std::unique_ptr<AsyncRequestDelegate> async_request_delegate_
