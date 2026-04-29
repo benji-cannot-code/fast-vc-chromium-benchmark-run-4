4d75ad67c245cdcf21c18d01be84bee22278eb54
Yuheng Huang <yuhengh@chromium.org>
2026-04-28 18:17:40 -0700

    Start glicPrivate.invoke with ntp
    When glicPrivate.invoke is called with new tab, start with NTP instead
    of about:blank. Also make foreground and background new tab to a
    FeatureParam.
    
    Bug: b/507114327
    Change-Id: I1103d05c5563acf18da2289137a54b6e8086920c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7798050
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622151}

--- chrome/browser/extensions/api/glic_private/glic_private_api.cc
+++ chrome/browser/extensions/api/glic_private/glic_private_api.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/webui_url_constants.h"
 #include "components/endpoint_fetcher/endpoint_fetcher.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/navigation_handle.h"
@@ -360,9 +361,13 @@ void GlicPrivateInvokeFunction::OnPromptRetrieved(
 
   if (in_new_tab) {
     // Navigate to a new tab.
-    NavigateParams navigate_params(profile, GURL("about:blank"),
+    NavigateParams navigate_params(profile, chrome::ChromeUINewTabURLAsGURL(),
                                    ui::PAGE_TRANSITION_LINK);
-    navigate_params.disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
+    bool open_in_foreground =
+        extensions_features::kGlicOpenNewTabInForegroundParam.Get();
+    navigate_params.disposition =
+        open_in_foreground ? WindowOpenDisposition::NEW_FOREGROUND_TAB
+                           : WindowOpenDisposition::NEW_BACKGROUND_TAB;
     base::WeakPtr<content::NavigationHandle> navigation_handle =
         Navigate(&navigate_params);
     if (navigation_handle) {
