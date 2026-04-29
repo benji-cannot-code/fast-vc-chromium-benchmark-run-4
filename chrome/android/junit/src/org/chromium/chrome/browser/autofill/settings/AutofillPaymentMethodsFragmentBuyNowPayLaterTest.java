d739f050e10885b203a02feda0ca3b491b9eb730
viplavkadam <viplavkadam@google.com>
2026-04-29 12:56:39 -0700

    [BNPL] Clean up the presubmit warning on BNPL settings test
    Since @NonNull is the default, adding it explicitly adds noise.
    
    Bug: 504733246
    Change-Id: Icaf9b52e0d7ac64f7fe86d803de4bec41345da40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780588
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622665}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AutofillPaymentMethodsFragmentBuyNowPayLaterTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AutofillPaymentMethodsFragmentBuyNowPayLaterTest.java
@@ -20,7 +20,6 @@ import androidx.fragment.app.FragmentFactory;
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.preference.Preference;
 
-import org.jspecify.annotations.NonNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -114,10 +113,8 @@ public class AutofillPaymentMethodsFragmentBuyNowPayLaterTest {
                         Bundle.EMPTY,
                         R.style.Theme_Chromium_Settings,
                         new FragmentFactory() {
-                            @NonNull
                             @Override
-                            public Fragment instantiate(
-                                    @NonNull ClassLoader classLoader, @NonNull String className) {
+                            public Fragment instantiate(ClassLoader classLoader, String className) {
                                 Fragment fragment = super.instantiate(classLoader, className);
                                 if (fragment instanceof AutofillPaymentMethodsFragment) {
                                     ((AutofillPaymentMethodsFragment) fragment)
