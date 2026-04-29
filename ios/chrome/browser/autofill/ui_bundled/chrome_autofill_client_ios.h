16ed795056de9667f6d48bd257bf9dc579e2ea72
Norge Vizcay <vizcay@google.com>
2026-04-29 05:30:01 -0700

    Remove unused plus_address traces from ChromeAutofillClientIOS
    Remove unused plus_address imports and related build dependencies from
    ChromeAutofillClientIOS since they are no longer needed. This is a dead
    code cleanup.
    
    Bug: 503268023
    Change-Id: Ief7c3d32d593e588cfc26c27b5569e8b51ee18bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800396
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622374}

--- ios/chrome/browser/autofill/ui_bundled/chrome_autofill_client_ios.h
+++ ios/chrome/browser/autofill/ui_bundled/chrome_autofill_client_ios.h
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/browser/autofill_client_ios.h"
 #import "components/autofill/ios/browser/autofill_client_ios_bridge.h"
 #import "components/infobars/core/infobar_manager.h"
-#import "components/plus_addresses/core/browser/plus_address_types.h"
 #import "components/prefs/pref_service.h"
 #import "components/strike_database/strike_database.h"
 #import "components/sync/service/sync_service.h"
