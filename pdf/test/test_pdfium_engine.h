0e571609cebadbda6a672ad07893b937a68861d7
Lei Zhang <thestig@chromium.org>
2026-04-29 12:33:44 -0700

    [PDF Ink Signatures] Backfill test for PdfInkModuleClientImpl::AddFont()
    Add a test case to PdfViewWebPluginInkTest to ensure
    PdfViewWebPlugin::PdfInkModuleClientImpl::AddFont() behaves correctly.
    
    Change-Id: Ia4ffd78c85c0c0ea5909a51c3578a06c0a59d888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802640
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622644}

--- pdf/test/test_pdfium_engine.h
+++ pdf/test/test_pdfium_engine.h
@@ -115,6 +115,8 @@ class TestPDFiumEngine : public PDFiumEngine {
               (override));
 
 #if BUILDFLAG(ENABLE_PDF_INK2)
+  MOCK_METHOD(void, AddFont, (FontId, base::span<const uint8_t>), (override));
+
   MOCK_METHOD(gfx::Size, GetThumbnailSize, (int, float), (override));
 
   MOCK_METHOD(void,
