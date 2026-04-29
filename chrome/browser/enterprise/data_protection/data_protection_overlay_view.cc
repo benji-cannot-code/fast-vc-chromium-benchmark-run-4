a31e8ef51d22a85f9baa77daf68558b4fe730a3b
Nasser Al-shawwa <alshawwa@chromium.org>
2026-04-29 11:52:22 -0700

    Create data_protection_overlay_view source set
    Bug: 507477742
    Change-Id: I2527d78e0115c498e48434c9f69d5e050d2a9a33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802074
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622624}

--- chrome/browser/enterprise/data_protection/data_protection_overlay_view.cc
+++ chrome/browser/enterprise/data_protection/data_protection_overlay_view.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/paint/paint_recorder.h"
 #include "chrome/browser/enterprise/watermark/settings.h"
 #include "components/enterprise/watermarking/watermark.h"
-#include "components/prefs/pref_service.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
