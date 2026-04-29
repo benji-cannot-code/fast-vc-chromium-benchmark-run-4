c29ece7654e805acc07936550c1437e64b30ba8b
Gianmarco Picarella <picarella@google.com>
2026-04-29 07:38:41 -0700

    [finch] Move Autofill profile deduplication to a background thread
    This change introduces a feature flag
    `AutofillEnableDeduplicationOnBackgroundThread`. When enabled, the
    process of deduplicating Autofill profiles is executed on a background
    thread pool to prevent blocking the UI thread.
    
    The profiles and their pending actions are passed to the background
    thread and then returned to the UI thread. The database updates are
    applied from the UI thread after the deduplication completes.
    
    We believe profiles deduplication is safe to be moved to a background
    thread because of the following points.
    
    * Profiles DB mutations that were once done during deduplication are
    now enqueued from the UI thread after deduplication is completed
    (AddressDataManager is not thread-safe).
    
    * The ICU library is thread-safe as stated in the documentation [1].
    
    * Updates to the alternative state map, which may happen when a
    new profile is added, are protected behind a mutex [2].
    
    * Access to rules for region via AddressRewriter::Cache is protected
    behind a mutex [3].
    
    * Based on my own research, PhoneNumberUtil (which includes
    libphonenumber for some operations) seems to be thread-safe for what
    concern the function calls required for deduplication. I checked the
    code paths originating from
    AutofillProfileComparator::MergePhoneNumbers(..) and
    AutofillProfileComparator::HaveMergeablePhoneNumbers(..) and I could not
    find signs of thread-unsafe code.
    
    * The background thread should use TaskShutdownBehavior::SKIP_ON_SHUTDOWN
    to prevent crashes when the process is closing. Some parts of
    deduplication access global "Singleton" objects. During shutdown, these
    Singletons might be destroyed while the deduplication is running,
    leading to a use-after-free crash.
    
    * Building and running the unit tests with ThreadSanitizer (Tsan) showed
    no data race warnings.
    
    [1]:https://unicode-org.github.io/icu/userguide/dev/sync/#overview
    [2]:https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/geo/alternative_state_name_map.cc;l=76;drc=424bb91010b5b2d4b57eb032e8e83c77a95efcd6
    [3]:https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/geo/address_rewriter.cc;l=92;drc=424bb91010b5b2d4b57eb032e8e83c77a95efcd6
    
    Bug: 496889243
    Change-Id: Ia5bdbdece91d0ff426eee963d8a38c341b71134d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779875
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Gianmarco Picarella <picarella@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622443}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -191,6 +191,8 @@ COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE_PARAM(int,
                            kAutofillEnableCacheForRegexMatchingCacheSizeParam);
 COMPONENT_EXPORT(AUTOFILL)
+BASE_DECLARE_FEATURE(kAutofillEnableDeduplicationOnBackgroundThread);
+COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableExpirationDateImprovements);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableGermanTransliteration);
