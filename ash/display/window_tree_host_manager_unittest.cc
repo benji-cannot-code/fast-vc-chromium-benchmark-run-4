28a465695f74344af59c7db7412965abceb6601c
Di Wu <diwux@google.com>
2026-04-28 22:52:02 -0700

    Ash: Migrate display root test off RunUntilIdle
    This changelist is part of an ongoing series to modernize Ash tests by
    migrating them away from RunUntilIdle() and toward targeted waits for
    the specific async signal or state transition each test depends on.
    
    Replace the idle drain in the window tree host manager test with a wait
    for the tracked secondary root window to disappear.
    
    Bug: b:506794349
    Test: CQ
    Change-Id: I59585655cd09904194da731d2f6a9b405a7c453c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794709
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622249}

--- ash/display/window_tree_host_manager_unittest.cc
+++ ash/display/window_tree_host_manager_unittest.cc
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/wm_event.h"
 #include "base/command_line.h"
 #include "base/memory/raw_ptr.h"
-#include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/run_until.h"
 #include "ui/aura/client/cursor_shape_client.h"
 #include "ui/aura/client/focus_change_observer.h"
 #include "ui/aura/client/focus_client.h"
@@ -1224,7 +1224,9 @@ TEST_F(WindowTreeHostManagerTest, SwapPrimaryById) {
 
   // Deleting 2nd display should move the primary to original primary display.
   UpdateDisplay("300x200");
-  base::RunLoop().RunUntilIdle();  // RootWindow is deleted in a posted task.
+  // The secondary root window is deleted in a posted task.
+  ASSERT_TRUE(
+      base::test::RunUntil([&] { return !tracker.Contains(secondary_root); }));
   EXPECT_EQ(1, display::Screen::Get()->GetNumDisplays());
   EXPECT_EQ(primary_display.id(),
             display::Screen::Get()->GetPrimaryDisplay().id());
