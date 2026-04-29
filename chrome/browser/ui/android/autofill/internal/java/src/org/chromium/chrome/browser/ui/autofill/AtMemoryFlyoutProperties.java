d41901ad6a40fe441206f518014719408db40aee
Atalyk Akash <atalyk@google.com>
2026-04-29 04:20:27 -0700

    [Clank] Add skeleton for AtMemoryFlyout.
    This change introduces the basic structure for a new UI component called
    AtMemoryFlyout. It includes the Coordinator, Mediator, View, ViewBinder,
    and Content classes, along with a test for the Mediator. The flyout is
    designed to be shown using the BottomSheetController. The current
    implementation uses a placeholder view and minimal logic.
    
    Bug: 505251096
    Change-Id: I449a0f8b9ca505cd1109414c5f34454616259f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793193
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Atalyk Akash <atalyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1622345}

---
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryFlyoutProperties.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.autofill;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+
+/** Properties defined here reflect the visible state of the {@link AtMemoryFlyoutContent}. */
+@NullMarked
+class AtMemoryFlyoutProperties {
+    // TODO(crbug.com/505257277): Add properties for the flyout.
+    static final PropertyKey[] ALL_KEYS = {};
+
+    private AtMemoryFlyoutProperties() {}
+}
---
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryFlyoutViewBinder.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.autofill;
+
+import android.view.View;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Binds properties for the AtMemoryFlyout. */
+@NullMarked
+class AtMemoryFlyoutViewBinder {
+    private AtMemoryFlyoutViewBinder() {}
+
+    static void bind(PropertyModel model, View view, PropertyKey propertyKey) {
+        // TODO(crbug.com/505257277): Handle property updates when properties are added.
+        assert false : "Unhandled property: " + propertyKey;
+    }
+}
