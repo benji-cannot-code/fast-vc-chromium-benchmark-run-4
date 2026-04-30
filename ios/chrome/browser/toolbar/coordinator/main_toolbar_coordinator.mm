5e5d48e29188e1e41b0002d7a128424e5766def7
Nicolas MacBeth <nicolasmacbeth@google.com>
2026-04-30 08:21:40 -0700

    Revert "[iOS][AppBar] Hide Toolbar on Landscape NTP"
    This reverts commit 9dbd16c0cefec16d44a657fd6040dc891027422a.
    
    Reason for revert: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/484591/overview static initializers check is failing
    
    Original change's description:
    > [iOS][AppBar] Hide Toolbar on Landscape NTP
    >
    > This change hides the toolbar on the non-incognito NTP. The toolbar
    > fades into view as the NTP fakebox is scrolled out of view. This affects
    > iPhone in landscape orientation. A follow-up change will remove fakebox
    > pinning from the non-incognito portrait NTP and add this functionality
    > for revealing the toolbar in split toolbar mode.
    >
    > Screencast: https://screencast.googleplex.com/cast/NjU1NDY3NTQxMDEwODQxNnw2NmQzZTE3Ny0zMw
    >
    > Fixed: 498602138
    > Change-Id: I193901346734df8c6281294d6e5189dc57950aeb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790792
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Eric Ekey <ericekey@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1623151}
    
    Bug: 498602138
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I0d3958257173f5ab663545b1a3df4bab86f7547b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808284
    Owners-Override: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623217}

--- ios/chrome/browser/toolbar/coordinator/main_toolbar_coordinator.mm
+++ ios/chrome/browser/toolbar/coordinator/main_toolbar_coordinator.mm
@@ -520,11 +520,6 @@ constexpr CGFloat kBannerPromoVerticalSpacing = 8;
 
 - (CGFloat)collapsedPrimaryToolbarHeight {
   if (IsChromeNextIaEnabled()) {
-    if (self.primaryToolbarViewController.view.hidden) {
-      // TODO(crbug.com/40279063): Find out why primary toolbar height cannot be
-      // zero. This is a temporary fix for the pdf bug.
-      return 1.0;
-    }
     if ([self isOmniboxInBottomPosition]) {
       // TODO(crbug.com/40279063): Find out why primary toolbar height cannot be
       // zero. This is a temporary fix for the pdf bug.
@@ -547,11 +542,6 @@ constexpr CGFloat kBannerPromoVerticalSpacing = 8;
 
 - (CGFloat)expandedPrimaryToolbarHeight {
   if (IsChromeNextIaEnabled()) {
-    if (self.primaryToolbarViewController.view.hidden) {
-      // TODO(crbug.com/40279063): Find out why primary toolbar height cannot be
-      // zero. This is a temporary fix for the pdf bug.
-      return 1.0;
-    }
     BOOL isOmniboxInBottomPosition = [self isOmniboxInBottomPosition];
     CGFloat height = 0;
     if (_tabGroupIndicatorCoordinator.viewVisible) {
@@ -660,12 +650,10 @@ constexpr CGFloat kBannerPromoVerticalSpacing = 8;
 #pragma mark - NewTabPageControllerDelegate
 
 - (void)setScrollProgressForTabletOmnibox:(CGFloat)progress {
-  if (IsChromeNextIaEnabled()) {
-    [_topToolbarViewController setNTPScrollProgress:progress];
-    [_bottomToolbarViewController setNTPScrollProgress:progress];
+  if (IsChromeNextIaEnabled() && CanShowTabStrip(self.traitEnvironment)) {
+    [_topToolbarViewController setScrollProgressForTabletOmnibox:progress];
     return;
   }
-
   for (id<NewTabPageControllerDelegate> coordinator in self.coordinators) {
     [coordinator setScrollProgressForTabletOmnibox:progress];
   }
