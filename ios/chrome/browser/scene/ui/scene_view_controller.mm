7c9caa2603433a96bcbedc508a7eed1880d6f199
Ewann Pelle <ewannpv@chromium.org>
2026-04-30 06:36:19 -0700

    [iOS][Assistant] Fix side panel layout conflicts
    This CL fixes constraint conflicts in SceneViewController that caused
    the app content to be crushed or overlapping when the Assistant side
    panel was active on iPad.The fill constraints added for Chrome Next are
    now deactivated when the side panel is active.
    
    Also adds an EarlGrey test to verify that the side panel and app content
    are displayed side-by-side without overlapping.
    
    Bug: 494503434
    Change-Id: I9748869d50096abe9221ae4a4601da8bd2043aa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806347
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623122}

--- ios/chrome/browser/scene/ui/scene_view_controller.mm
+++ ios/chrome/browser/scene/ui/scene_view_controller.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/bubble/ui_bundled/bubble_view_controller_presenter.h"
 #import "ios/chrome/browser/scene/ui/app_container_view.h"
 #import "ios/chrome/browser/scene/ui/scene_mutator.h"
+#import "ios/chrome/browser/scene/ui/scene_ui_constants.h"
 #import "ios/chrome/browser/scene/ui/scene_view.h"
 #import "ios/chrome/browser/scene/ui/scene_view_controller_delegate.h"
 #import "ios/chrome/browser/scene/ui/scene_view_delegate.h"
@@ -41,6 +42,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   UIViewController* _appBar;
   // The assistant container view controller.
   AssistantContainerViewController* _assistantContainerViewController;
+
+  // Constraints making app content fill the screen for Chrome Next IA.
+  NSArray<NSLayoutConstraint*>* _chromeNextIaFillConstraints;
 
   // Container for App Content view to handle shadow for Side Panel floating
   // card effect.
@@ -90,6 +94,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _appContentContainerView = [[UIView alloc] init];
   _appContentView = [[AppContainerView alloc] init];
   _appContentView.clipsToBounds = YES;
+  _appContentView.accessibilityIdentifier = kAppContentAccessibilityIdentifier;
 
   if (IsFullscreenRefactoringEnabled()) {
     _appContentContainerView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -110,7 +115,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (IsFullscreenRefactoringEnabled()) {
     AddSameConstraints(_appContentView, _appContentContainerView);
     if (IsChromeNextIaEnabled()) {
-      AddSameConstraints(_appContentContainerView, view);
+      _chromeNextIaFillConstraints = @[
+        [_appContentContainerView.leadingAnchor
+            constraintEqualToAnchor:view.leadingAnchor],
+        [_appContentContainerView.trailingAnchor
+            constraintEqualToAnchor:view.trailingAnchor],
+        [_appContentContainerView.topAnchor
+            constraintEqualToAnchor:view.topAnchor],
+        [_appContentContainerView.bottomAnchor
+            constraintEqualToAnchor:view.bottomAnchor],
+      ];
+      [NSLayoutConstraint activateConstraints:_chromeNextIaFillConstraints];
     }
   }
 
@@ -415,6 +430,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self setupDefaultConstraints];
     _activeAssistantConstraints = _baseAssistantConstraints;
     [NSLayoutConstraint activateConstraints:_activeAssistantConstraints];
+    if (IsChromeNextIaEnabled()) {
+      [NSLayoutConstraint activateConstraints:_chromeNextIaFillConstraints];
+    }
     return;
   }
 
@@ -429,10 +447,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         AssistantPresentationContext::kPanel;
     _activeAssistantConstraints = _assistantPanelConstraints;
     [self updateAssistantTopConstraints:self.layoutState.containedLayoutActive];
+    if (IsChromeNextIaEnabled()) {
+      [NSLayoutConstraint deactivateConstraints:_chromeNextIaFillConstraints];
+    }
   } else {
     _assistantContainerViewController.presentationContext =
         AssistantPresentationContext::kSheet;
     _activeAssistantConstraints = _assistantSheetConstraints;
+    if (IsChromeNextIaEnabled()) {
+      [NSLayoutConstraint activateConstraints:_chromeNextIaFillConstraints];
+    }
   }
 
   [NSLayoutConstraint activateConstraints:_activeAssistantConstraints];
@@ -635,7 +659,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSLayoutConstraint* proportionalWidth = [assistantView.widthAnchor
       constraintEqualToAnchor:view.widthAnchor
                    multiplier:kAssistantSidePanelWidthMultiplier];
-  proportionalWidth.priority = UILayoutPriorityDefaultHigh;
+  proportionalWidth.priority = UILayoutPriorityRequired - 1;
 
   _assistantPanelConstraints = @[
     _assistantLeadingConstraint,
@@ -703,7 +727,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ];
 
   _assistantSheetConstraints = sheetConstraints;
-  if (IsFullscreenRefactoringEnabled()) {
+  if (IsFullscreenRefactoringEnabled() && !IsChromeNextIaEnabled()) {
     _assistantSheetConstraints =
         [sheetConstraints arrayByAddingObjectsFromArray:@[
           [_appContentContainerView.leadingAnchor
