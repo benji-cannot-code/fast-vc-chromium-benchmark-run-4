de01195773e51e7a5e8a9f91f95dff3bce6f13f4
Edgar Aguilar <eledgar@google.com>
2026-04-30 01:39:19 -0700

    [Fuzzing] Fix ICU deps and initialization.
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how the JVM initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes.
    
    Updating fuzzers in components/password_manager/core/browser/import
    
    This CL was uploaded by git cl split.
    
    Bug: 500525162
    Change-Id: Ic21e3b077eb10f3a4a6a1e8258d6ceeb820bd301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797921
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623011}

--- components/password_manager/core/browser/import/password_csv_reader_fuzzer.cc
+++ components/password_manager/core/browser/import/password_csv_reader_fuzzer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
+#include "base/no_destructor.h"
 #include "components/password_manager/core/browser/import/csv_password_sequence.h"
 
 namespace password_manager {
@@ -34,9 +35,8 @@ struct IcuEnvironment {
   base::AtExitManager at_exit_manager;
 };
 
-IcuEnvironment* env = new IcuEnvironment();
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static const base::NoDestructor<IcuEnvironment> env;
   CSVPasswordSequence seq(
       std::string(reinterpret_cast<const char*>(data), size));
   CHECK(IsValid(seq.result()))
