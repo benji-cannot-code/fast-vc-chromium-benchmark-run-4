35ec63e04391fddeecb6ea6a6b350401d474a783
Alison Gale <agale@chromium.org>
2026-04-30 08:34:56 -0700

    [Vertical Tabs] Rename kKeepExpanded lock name
    This option actually preserves the current state. Since I'm adding a new
    option that forces the panel to stay expanded this rename will make
    things more clear. No functionality changes.
    
    Bug: 506215219
    Change-Id: Id4efbc9b6d0f9ab34b1ec2695109bd1460203f28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805804
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623229}

--- chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.cc
+++ chrome/browser/ui/views/tabs/shared/tab_strip_combo_button.cc
@@ -349,7 +349,7 @@ void TabStripComboButton::ShowContextMenuForViewImpl(
   CHECK(browser_view);
   CHECK(browser_view->tab_strip_view());
   expand_on_hover_lock_ = browser_view->tab_strip_view()->GetExpandOnHoverLock(
-      ExpandOnHoverLockType::kKeepExpanded);
+      ExpandOnHoverLockType::kKeepCurrentState);
 
   menu_runner_->RunMenuAt(GetWidget(), nullptr,
                           source->GetAnchorBoundsInScreen(),
@@ -395,7 +395,7 @@ void TabStripComboButton::OnBubbleInitializing() {
   CHECK(browser_view);
   CHECK(browser_view->tab_strip_view());
   expand_on_hover_lock_ = browser_view->tab_strip_view()->GetExpandOnHoverLock(
-      ExpandOnHoverLockType::kKeepExpanded);
+      ExpandOnHoverLockType::kKeepCurrentState);
 
   OnTabSearchBubbleShown();
 
--- chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_group_view.cc
@@ -546,7 +546,7 @@ VerticalTabGroupView::AcquireExpandOnHoverLock() {
   CHECK(browser_view);
   CHECK(browser_view->tab_strip_view());
   return browser_view->tab_strip_view()->GetExpandOnHoverLock(
-      ExpandOnHoverLockType::kKeepExpanded);
+      ExpandOnHoverLockType::kKeepCurrentState);
 }
 
 void VerticalTabGroupView::ShiftGroupUp() {
