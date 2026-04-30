a6c40808cf35c4a2fec9770dec19d6dddac275af
Daniel Clark <daniec@microsoft.com>
2026-04-30 07:36:25 -0700

    Fold away redundant check in HTMLButtonElement::DefaultEventHandler
    There's an if-check in HTMLButtonElement::DefaultEventHandler that is
    identical to the one for the prior block. Fold the statement into the
    prior one to remove the extra check.
    
    Bug: 406566432
    Change-Id: I7123821a77bcd7405fde289f7db7ace271e6cd06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802470
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1623172}

--- third_party/blink/renderer/core/html/forms/html_button_element.cc
+++ third_party/blink/renderer/core/html/forms/html_button_element.cc
@@ -261,9 +261,7 @@ void HTMLButtonElement::DefaultEventHandler(Event& event) {
         return;
       }
     }
-  }
 
-  if (event.type() == event_type_names::kDOMActivate) {
     if (HandleCommandForActivation()) {
       return;
     }
