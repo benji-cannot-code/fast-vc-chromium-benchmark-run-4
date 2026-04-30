fff01caa6cad14104a8ba0fd566e2e056be31cd9
Chirag Arora <heychirag@google.com>
2026-04-30 01:48:06 -0700

    Hide "Help improve Chrome ..." toggle on Desktop and link on ChromeOS
    This change hides the usage and crash reporting setting in the
    personalization options page when the metrics consent restructure
    is enabled and the user has been migrated to the new consent settings.
    
    The implementation varies by platform:
    - On Desktop, the #metricsReportingControl toggle is hidden.
    - On ChromeOS, the #metricsReportingLink link row is hidden.
    
    Tests were added to both metrics_reporting_test.ts (for Desktop) and
    personalization_options_test.ts (for ChromeOS) to verify the behavior
    on each platform.
    
    Bug: b:496476603
    Change-Id: Ie23879c2ef4d8cc0760d3d81cce9059095fed2a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790348
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623012}

--- chrome/browser/resources/settings/privacy_page/personalization_options.ts
+++ chrome/browser/resources/settings/privacy_page/personalization_options.ts
@@ -30,7 +30,6 @@ import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
 // </if>
 // </if>
 
-import type {CrLinkRowElement} from '//resources/cr_elements/cr_link_row/cr_link_row.js';
 import type {CrToastElement} from '//resources/cr_elements/cr_toast/cr_toast.js';
 import {WebUiListenerMixin} from '//resources/cr_elements/web_ui_listener_mixin.js';
 import {assert} from '//resources/js/assert.js';
@@ -56,8 +55,6 @@ export interface SettingsPersonalizationOptionsElement {
   $: {
     toast: CrToastElement,
     signinAllowedToggle: SettingsToggleButtonElement,
-    metricsReportingControl: SettingsToggleButtonElement,
-    metricsReportingLink: CrLinkRowElement,
     urlCollectionToggle: SettingsToggleButtonElement,
     chromeSigninUserChoiceSelection: HTMLSelectElement,
     chromeSigninUserChoiceToast: CrToastElement,
@@ -120,6 +117,12 @@ export class SettingsPersonalizationOptionsElement extends
 
       showSignoutDialog_: Boolean,
 
+      shouldUseMetricsConsentRestructure_: {
+        type: Boolean,
+        value: () =>
+            loadTimeData.getBoolean('shouldUseMetricsConsentRestructure'),
+      },
+
       syncFirstSetupInProgress_: {
         type: Boolean,
         value: false,
@@ -160,6 +163,7 @@ export class SettingsPersonalizationOptionsElement extends
 
   declare private showSignoutDialog_: boolean;
   declare private syncFirstSetupInProgress_: boolean;
+  declare private shouldUseMetricsConsentRestructure_: boolean;
 
   // <if expr="not is_chromeos">
   declare private signinAvailable_: boolean;
@@ -238,7 +242,9 @@ export class SettingsPersonalizationOptionsElement extends
 
   // <if expr="_google_chrome and not is_chromeos">
   private onMetricsReportingChange_() {
-    const enabled = this.$.metricsReportingControl.checked;
+    const control = this.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+        '#metricsReportingControl');
+    const enabled = control?.checked ?? false;
     this.browserProxy_.setMetricsReportingEnabled(enabled);
   }
 
