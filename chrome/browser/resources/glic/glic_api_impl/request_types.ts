bab4764a50471444bef8156dd977f0866cfb58a5
Anthony Cui <cuianthony@google.com>
2026-04-30 13:18:34 -0700

    [GlicZoom] Add glic api for notifying web app of zoom changes
    There isn't enough space in the glic floaty UI to effectively present the experience at a higher zoom level. The current approach to address this is to scale the size of the floaty with the zoom level as it changes. This requires a new glic API, so the web app can be notified of zoom level changes that occur in the WebUI. This CL implements the new API with a subscription method so that the web app can choose to subscribe only when in floating mode and we can reduce the number of messages that need to be sent by the host.
    
    Verified locally using test client.
    
    Bug: b:505874967, b:459431081
    Change-Id: Ic55a9c4cbebc58e4290c93b737cca21b6dc76b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802195
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623391}

--- chrome/browser/resources/glic/glic_api_impl/request_types.ts
+++ chrome/browser/resources/glic/glic_api_impl/request_types.ts
@@ -672,6 +672,12 @@ export declare type HostRequestTypes = ValidateRequestMap<{
     },
     backgroundAllowed: true,
   },
+  glicBrowserSubscribeToZoomLevel: {
+    backgroundAllowed: true,
+  },
+  glicBrowserUnsubscribeFromZoomLevel: {
+    backgroundAllowed: true,
+  },
 }>;
 
 // Types of requests to the GlicWebClient.
@@ -938,6 +944,12 @@ export declare type WebClientRequestTypes = ValidateRequestMap<{
     },
     backgroundAllowed: true,
   },
+  glicWebClientNotifyZoomLevelChanged: {
+    request: {
+      zoomFactor: number,
+    },
+    backgroundAllowed: true,
+  },
 }>;
 
 // Each host request needs to be added to either UnreportedRequests or
@@ -1056,6 +1068,8 @@ const RECORDED_REQUEST_IDS = {
   SubscribeToTabFavicon: 94,
   ShowBrowseSkillsUi: 95,
   OnExperimentalTriggeringUpdate: 96,
+  SubscribeToZoomLevel: 97,
+  UnsubscribeFromZoomLevel: 98,
 } as const satisfies HostRequestEnumNamesType;
 // LINT.ThenChange(
 //  //tools/metrics/histograms/metadata/glic/histograms.xml:ApiRequestType,
