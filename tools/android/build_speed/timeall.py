6f903ff62d020e766016904e4af3f78705e91236
Peter Wen <wnwen@chromium.org>
2026-04-30 10:25:48 -0700

    android: Add al_cc benchmark for Desktop
    Add a compile benchmark for Android Desktop to benchmark.py. This
    benchmark tracks incremental compilation speed of a commonly used C++
    file (compositor_view.cc).
    
    Also add the benchmark to timeall.py.
    
    Bug: 507810250
    Change-Id: Icf24b2ed4261ff5efc37870b09fb80472306cfd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801177
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623301}

--- tools/android/build_speed/timeall.py
+++ tools/android/build_speed/timeall.py
@@ -77,6 +77,8 @@ def _run_benchmark(options: _Options):
     elif 'chrome_junit_' in options.benchmark:
         target = "chrome_junit_tests"
     else:
+        # Note: chrome_apk is preferred over chrome_public_apk for future
+        # notice.
         target = "chrome_apk"
     cmd = [
         "tools/android/build_speed/benchmark.py",
@@ -120,6 +122,7 @@ def run(debug: bool):
         "chrome_nosig",
         "base_sig",
         "cta_test_sig",
+        "al_cc",
     ]
 
     if debug:
