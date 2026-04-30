1d173cf36514c76cf1d58b391cb99e598b52ee8f
Christoph Schwering <schwering@google.com>
2026-04-30 03:48:44 -0700

    [Autofill] Condition 'autofill' use counter on OT
    Some scripts want to observe non-standard 'autofill' event. These
    observers skew the numbers of the proposed 'autofill' standard [1].
    
    This CL limits the use counter to sites where the 'autofill' standard's
    origin trial is enabled.
    
    [1] https://chromestatus.com/metrics/feature/timeline/popularity/5806
    
    Bug: 466333215
    Change-Id: Ie4ff0e7b2329744598e5f7c7221c9ca32375ace1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806669
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623065}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -3418,7 +3418,8 @@ void Document::AddedEventListener(
     const AtomicString& event_type,
     RegisteredEventListener& registered_listener) {
   ContainerNode::AddedEventListener(event_type, registered_listener);
-  if (event_type == event_type_names::kAutofill) {
+  if (event_type == event_type_names::kAutofill &&
+      RuntimeEnabledFeatures::AutofillEventEnabled(GetExecutionContext())) {
     UseCounter::Count(*this, WebFeature::kAutofillEvent);
   }
 }
