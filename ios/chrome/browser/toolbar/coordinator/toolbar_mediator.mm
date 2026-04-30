f0105288ea7bb30fc3715acfd2d289a70575b713
Aliona DANGLA <alionadangla@google.com>
2026-04-30 00:48:03 -0700

    [iOS] Fix exiting 'Hide Toolbars' mode on Chrome Next
    Updates ToolbarMediator to correctly call `ExitForceFullscreenMode`
    when the user taps the collapsed toolbar while in the "Hide Toolbars"
    state. Previously, it used the scroll-based `ExitFullscreen`, which
    failed to expand the toolbars in the Chrome Next layout.
    
    Bug: 444174990
    Change-Id: I88987d95ef3053131607f2fbb3d67c96ee66c637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7803539
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622990}

--- ios/chrome/browser/toolbar/coordinator/toolbar_mediator.mm
+++ ios/chrome/browser/toolbar/coordinator/toolbar_mediator.mm
@@ -186,7 +186,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   if (_fullscreenController) {
-    _fullscreenController->ExitFullscreen(trigger);
+    if (_fullscreenController->IsForceFullscreenMode()) {
+      _fullscreenController->ExitForceFullscreenMode(trigger);
+    } else {
+      _fullscreenController->ExitFullscreen(trigger);
+    }
   }
 }
 
