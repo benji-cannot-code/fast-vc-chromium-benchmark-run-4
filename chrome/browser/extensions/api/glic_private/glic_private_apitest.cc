48d12659ddc7cadef4633f3a20386066ce01c5f5
Eva Su <evasu@chromium.org>
2026-04-30 16:47:26 -0700

    [Extensions] Disable a flaky GlicPrivateApiFullyEnabledTest.
    GlicPrivateApiFullyEnabledTest.InvokeInNewTab is extremely flaky on all
    builds after crrev.com/c/7803637. Disabling the test while we
    investigate.
    
    Change-Id: I0c3cdb8476756a86d624f34a2cb84086c38441e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807557
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623565}

--- chrome/browser/extensions/api/glic_private/glic_private_apitest.cc
+++ chrome/browser/extensions/api/glic_private/glic_private_apitest.cc
@@ -163,7 +163,8 @@ IN_PROC_BROWSER_TEST_F(GlicPrivateApiFullyEnabledTest, Invoke) {
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(GlicPrivateApiFullyEnabledTest, InvokeInNewTab) {
+IN_PROC_BROWSER_TEST_F(GlicPrivateApiFullyEnabledTest,
+                       DISABLED_InvokeInNewTab) {
   SimpleFeature::ScopedThreadUnsafeAllowlistForTest allowlist(
       kGlicPrivateTestExtensionId);
 
