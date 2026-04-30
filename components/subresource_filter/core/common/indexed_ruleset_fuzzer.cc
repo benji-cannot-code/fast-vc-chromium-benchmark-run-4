3957197d34f7af3c0e8b40d3758bef92fb191003
Edgar Aguilar <eledgar@google.com>
2026-04-30 13:05:46 -0700

    [Fuzzing] Fix ICU deps and initialization.
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how the JVM initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes.
    
    Updating fuzzers in components/subresource_filter
    
    This CL was uploaded by git cl split.
    
    Bug: 500525162
    Change-Id: Ib5802466decf0ce7893b527f4a92f4f219f4c029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796450
    Auto-Submit: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Charles Harrison <csharrison@chromium.org>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Edgar Aguilar <eledgar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623370}

--- components/subresource_filter/core/common/indexed_ruleset_fuzzer.cc
+++ components/subresource_filter/core/common/indexed_ruleset_fuzzer.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
+#include "base/no_destructor.h"
 #include "components/subresource_filter/core/common/constants.h"
 #include "components/subresource_filter/core/common/first_party_origin.h"
 #include "components/subresource_filter/core/common/unindexed_ruleset.h"
@@ -27,9 +28,8 @@ struct TestCase {
   base::AtExitManager at_exit_manager;
 };
 
-TestCase* test_case = new TestCase();
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static const base::NoDestructor<TestCase> test_case;
   FuzzedDataProvider fuzzed_data(data, size);
 
   // Split the input into two sections, the URL to check, and the ruleset
