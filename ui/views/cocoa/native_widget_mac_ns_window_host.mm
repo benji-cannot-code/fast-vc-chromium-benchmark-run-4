b0f94d536b587e245da5d86be41ab34e0171c63a
Maggie Chen <magchen@chromium.org>
2026-04-30 10:52:10 -0700

    [CADisplayLink] Plumb the refresh rate change to ExternalBeginFrameSourceMac
    Enabling CADisplayLinkInBrowser has resulted in a regression of
    StartupTime metrics. To mitigate this latency, we switch to a new
    proposal of initializing with CADisplayLinkInGpu and subsequently
    transitioning to CADisplayLinkInBrowser upon the occurrence of the
    initial power event or a change in the refresh rate.
    
    Due to GPU sandbox restrictions, the
    NSApplicationDidChangeScreenParametersNotification API does not work
    in the GPU process. To address this, the Browser process will now
    request the notification and transmit the event to the GPU process
    through IPC.
    
    This CL:
    1. Adds RefreshRateChangedOnSameDisplay() to viz and ui/compositor to
       notify the GPU process when the refresh rate of the current display
       changes.
    2. Implements the propagation of this notification from the Browser
       process to the GPU process via IPC (DisplayPrivate mojom).
    3. Updates ui/display to track and notify NativeWidgetMacNSWindowHost
       and RenderWidgetHostViewMac about refresh rate changes
       on macOS.
    
    Bug: 345275139
    Change-Id: Ia9f9e98b934e189916ea9cfd139b28e5aefab575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802371
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623327}

--- ui/views/cocoa/native_widget_mac_ns_window_host.mm
+++ ui/views/cocoa/native_widget_mac_ns_window_host.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
+#include "base/numerics/ranges.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/time/time.h"
@@ -714,7 +715,8 @@ void NativeWidgetMacNSWindowHost::UpdateCompositorProperties() {
           content_bounds_in_screen_.size(), display_.device_scale_factor()));
   compositor_->UpdateSurface(content_bounds_in_pixels,
                              display_.device_scale_factor(),
-                             display_.GetColorSpaces(), display_.id());
+                             display_.GetColorSpaces(), display_.id(),
+                             /*refresh_rate_changed_on_same_display=*/false);
 }
 
 void NativeWidgetMacNSWindowHost::DestroyCompositor() {
@@ -1485,6 +1487,17 @@ void NativeWidgetMacNSWindowHost::OnVisibleOnAllWorkspacesChanged(
 
 void NativeWidgetMacNSWindowHost::OnWindowDisplayChanged(
     const display::Display& new_display) {
+  // Refresh rate change caused by display change is not handled here. It will
+  // be handled in compositor_->SetVSyncDisplayID().
+  bool refresh_rate_changed_on_same_display = false;
+  bool same_frequency = base::IsApproximatelyEqual(
+      display_.display_frequency(), new_display.display_frequency(),
+      display::Display::kRefreshRateEpsilon);
+  if (display_.display_frequency() != 0 && display_.id() == new_display.id() &&
+      !same_frequency) {
+    refresh_rate_changed_on_same_display = true;
+  }
+
   display_ = new_display;
   if (!compositor_) {
     return;
@@ -1496,7 +1509,8 @@ void NativeWidgetMacNSWindowHost::OnWindowDisplayChanged(
           content_bounds_in_screen_.size(), display_.device_scale_factor()));
   compositor_->UpdateSurface(content_bounds_in_pixels,
                              display_.device_scale_factor(),
-                             display_.GetColorSpaces(), display_.id());
+                             display_.GetColorSpaces(), display_.id(),
+                             refresh_rate_changed_on_same_display);
 }
 
 void NativeWidgetMacNSWindowHost::OnWindowWillClose() {
