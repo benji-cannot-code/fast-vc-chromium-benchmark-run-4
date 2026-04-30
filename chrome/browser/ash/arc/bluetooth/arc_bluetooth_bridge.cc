0712171590f91e7b8a9250a3c21f675a03b84600
Jeffrey Yu <yuje@chromium.org>
2026-04-30 15:42:39 -0700

    Update ConvertGattIdentifierToId to use std::string_view.
    Uses std::string_view to avoid creating string copies.
    
    Bug: 501076278
    Change-Id: I47dc278124d96f82791c9dd2ae91529676ea4548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804666
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623541}

--- chrome/browser/ash/arc/bluetooth/arc_bluetooth_bridge.cc
+++ chrome/browser/ash/arc/bluetooth/arc_bluetooth_bridge.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iomanip>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "ash/constants/ash_pref_names.h"
@@ -153,8 +154,7 @@ arc::mojom::BluetoothGattStatus ConvertGattErrorCodeToStatus(
 // Example of identifier: /org/bluez/hci0/dev_E0_CF_65_8C_86_1A/service001a
 // Convert the last 4 characters of |identifier| to an
 // int, by interpreting them as hexadecimal digits.
-std::optional<uint16_t> ConvertGattIdentifierToId(
-    const std::string& identifier) {
+std::optional<uint16_t> ConvertGattIdentifierToId(std::string_view identifier) {
   uint32_t result;
   if (identifier.size() < 4 ||
       !base::HexStringToUInt(identifier.substr(identifier.size() - 4), &result))
