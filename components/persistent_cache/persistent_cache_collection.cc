17964fe7b0848f2944dc41406aeee071344cebab
Greg Thompson <grt@chromium.org>
2026-04-30 11:09:44 -0700

    [persistent_cache] Add a feature to enable WAL-mode for the code cache
    The new kPersistentCacheCollectionWalMode feature turns on WAL-mode for
    caches in a PersistentCacheCollection. The V8 code cache is the only
    consumer.
    
    Bug: 374930286,486665177
    Change-Id: I1022816f0170fb3c72918e296077cc256c6f49fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790499
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623349}

--- components/persistent_cache/persistent_cache_collection.cc
+++ components/persistent_cache/persistent_cache_collection.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/fixed_flat_map.h"
 #include "base/containers/fixed_flat_set.h"
 #include "base/containers/map_util.h"
+#include "base/feature.h"
+#include "base/feature_list.h"
 #include "base/strings/string_util.h"
 #include "base/types/expected.h"
 #include "base/types/expected_macros.h"
@@ -25,6 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/persistent_cache/transaction_error.h"
 
 namespace persistent_cache {
+
+namespace {
+
+// Enables WAL-mode for databases created in a PersistentCacheCollection.
+BASE_FEATURE(kPersistentCacheCollectionWalMode,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace
 
 PersistentCacheCollection::PersistentCacheCollection(
     base::FilePath top_directory,
@@ -233,8 +243,10 @@ PersistentCache* PersistentCacheCollection::GetOrCreateCache(
 
   ASSIGN_OR_RETURN(
       auto backend,
-      backend_storage_.MakeBackend(base_name, /*single_connection=*/false,
-                                   /*journal_mode_wal=*/false),
+      backend_storage_.MakeBackend(
+          base_name, /*single_connection=*/false,
+          /*journal_mode_wal=*/
+          base::FeatureList::IsEnabled(kPersistentCacheCollectionWalMode)),
       [this, &base_name](TransactionError error) {
         DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
