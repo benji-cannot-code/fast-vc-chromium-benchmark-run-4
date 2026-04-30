9c583ef0662af598f66fafefa184a8ca1955148a
Emily Stark <estark@google.com>
2026-04-30 13:51:31 -0700

    Add flag for putting "Secure connections" in bundled settings
    This CL just creates a feature flag for moving "Secure connections"
    (Ask-Before-HTTP) in bundled security settings. The functionality and
    UI will be in a follow-up CL.
    
    Bug: b:505069102, 464331450
    Change-Id: Ic417cd145cba0a8e6d0ff84aa12aabe75298ab6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804879
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623447}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -59,6 +59,9 @@ constexpr base::FeatureParam<int>
 
 BASE_FEATURE(kBundledSecuritySettings, base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kBundledSecuritySettingsAskBeforeHttp,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kBundledSecuritySettingsSecureDnsV2,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
@@ -458,6 +461,7 @@ base::ListValue GetFeatureStatusList() {
       // keep-sorted start
       &kAutoRevokeSuspiciousNotification,
       &kBundledSecuritySettings,
+      &kBundledSecuritySettingsAskBeforeHttp,
       &kClientSideDetectionClipboardCopyApi,
       &kClientSideDetectionForcedLlamaRedirectChainKillswitch,
       &kClientSideDetectionImageEmbeddingMatch,
