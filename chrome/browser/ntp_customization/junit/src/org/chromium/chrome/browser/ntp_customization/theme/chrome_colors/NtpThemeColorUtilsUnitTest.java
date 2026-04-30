c1f4fad44ac56cb7515f01b3c7d8e32add7d6faa
Xi Han <hanxi@google.com>
2026-04-30 14:08:40 -0700

    [ThemeSync] Change the default color of the color drawable.
    In this CL, we allow to create a NtpThemeColorInfo for the default
    type. Besides, update the default color for chrome icon images.
    Screenshot: http://shortn/_KnETEMY7de.
    
    Bug: 488439751
    Change-Id: I533b5f6cce644d726a6cc00286a0cbfd684aa3b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808286
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623476}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/chrome_colors/NtpThemeColorUtilsUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/chrome_colors/NtpThemeColorUtilsUnitTest.java
@@ -150,6 +150,18 @@ public class NtpThemeColorUtilsUnitTest {
     }
 
     @Test
+    public void testCreateColoredCircle_default() {
+        LayerDrawable drawable =
+                NtpThemeColorUtils.createColoredCircle(
+                        mContext,
+                        /* topColor= */ null,
+                        /* bottomLeftColor= */ null,
+                        /* bottomRightColor= */ null);
+        assertNotNull(drawable);
+        assertEquals(3, drawable.getNumberOfLayers());
+    }
+
+    @Test
     public void testGetNtpThemePrimaryColorFromPreBuiltColors() {
         @ColorRes
         int aquaPrimaryColorResId =
