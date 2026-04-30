8ce4053423f0b72630a1d44bc522f1eadff2cb0f
Michael Wu <mklw@google.com>
2026-04-30 15:27:03 -0700

    [Tab Strip Glic Button] Decouple active UI highlight from touch state
    Introduces an independent mIsHighlighted state in TintedCompositorButton
    to explicitly track when the Glic UI panel is open. This prevents
    standard touch gestures (which toggle isPressed) from accidentally
    resetting or flickering the button's active background highlight.
    
    Bug: 493251114
    Change-Id: I28e9277d233af4425ec2615914e16c176e020949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801561
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623529}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTrailingButtonsCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutTrailingButtonsCoordinator.java
@@ -346,7 +346,7 @@ public class StripLayoutTrailingButtonsCoordinator {
 
         mIsGlicUiVisible = isOpened;
         if (mGlicButton != null) {
-            mGlicButton.setPressed(isOpened);
+            mGlicButton.setHighlighted(isOpened);
             mRenderHost.requestRender();
         }
     }
