67d46be9a02b2e8d42705eba208faad1e363ba16
Dan H <harringtond@chromium.org>
2026-04-30 14:41:24 -0700

    Revert "[Subresource Filter] Add StyleRule support to indexed ruleset"
    This reverts commit bf7a908a6f06d9f552e2d997c523d755fa222688.
    
    Reason for revert: This broke CI bot: https://ci.chromium.org/ui/p/chromium/builders/ci/Deterministic%20Linux/62706/overview
    
    Original change's description:
    > [Subresource Filter] Add StyleRule support to indexed ruleset
    >
    > Adds support for indexing style rules. Using the style rules (matching)
    > will be in the next CL.
    >
    > Because matching isn't yet supported, the testing is necessarily weak
    > sauce in this CL, but rest assured they will be in the followup.
    >
    > Outline of changes:
    >
    > 1. Adds a ruleset_id to each ruleset. This is a random value. This will
    > be needed by downstream subresource_filter clients to know when the
    > ruleset has changed. RulesetService has an injectable method to generate
    > these ids (for testing).
    >
    > 2. The StyleRuleIndex: Basically, store all of the selectors in a list,
    > and the index into that list via maps from domains, class attributes, or
    > id attributes. The idea is that we're only supporting global rules with
    > anchors (classes or ids) and we'll support most rules matched to a
    > domain.
    >
    > 3. StyleRuleIndex has a bloom filter to filter out classes/ids that
    > aren't in the maps. The nice thing about this filter is no extra hashing
    > is required since blink Strings already have a hash member, we use that
    > 24 bit hash here as well. Hence the need for rapidhash here. There will
    > be tests in followup that ensure that blink strings correctly work with
    > the bloom filter so that we don't have to worry about the
    > implementations diverging without us noticing.
    >
    > Bug: 500722400
    > Change-Id: Idb0b942673f3d9aea1d45af749e8f97da2358eea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793587
    > Reviewed-by: Charles Harrison <csharrison@chromium.org>
    > Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1623346}
    
    Bug: 500722400
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib17458c4c391bc2648f7c0360eda38bd7af5940f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806103
    Auto-Submit: Dan H <harringtond@chromium.org>
    Owners-Override: Dan H <harringtond@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1623503}

--- components/subresource_filter/core/common/test_ruleset_creator.cc
+++ components/subresource_filter/core/common/test_ruleset_creator.cc
@@ -52,7 +52,7 @@ std::vector<uint8_t> SerializeUnindexedRulesetWithMultipleRules(
 
 std::vector<uint8_t> SerializeIndexedRulesetWithMultipleRules(
     const std::vector<proto::UrlRule>& rules) {
-  RulesetIndexer indexer(0);
+  RulesetIndexer indexer;
   for (const auto& rule : rules) {
     EXPECT_TRUE(indexer.AddUrlRule(rule));
   }
