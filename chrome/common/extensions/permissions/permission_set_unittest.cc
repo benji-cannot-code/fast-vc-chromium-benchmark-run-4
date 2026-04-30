1f0b0e5ae05a53f7b87ceb8f490b9129a18821de
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-04-30 09:43:22 -0700

    [Extensions] Remove webstorePrivate hosted app entry and permission
    The Chrome Web Store used to be implemented via a number of special
    traits with a component hosted app. Now, it's much more direct and
    uses a special origin, chromewebstore.google.com.
    
    We still need the hosted app for a bit of special handling (e.g. a
    launcher icon on ChromeOS), but it effectively shouldn't be used on
    any supported versions of Chrome.
    
    This CL removes the allowlist entry in permission_features.json for
    the hosted app being able to use webstorePrivate. Since that was the
    only consumer of the webstorePrivate API permission, it also removes
    that in its entirety.
    
    The end result is that the hosted app no longer has access to the
    webstorePrivate API. This shouldn't have any effective impact on users,
    since all supported versions redirect to the webstore domain.
    
    Bug: 328494022
    Change-Id: I9b0bc34412864a51ed2ddee7d3ff2dba9b9dafc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804871
    Reviewed-by: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623283}

--- chrome/common/extensions/permissions/permission_set_unittest.cc
+++ chrome/common/extensions/permissions/permission_set_unittest.cc
@@ -894,7 +894,6 @@ TEST(PermissionsTest, PermissionMessages) {
   skip.insert(APIPermissionID::kWebrtcDesktopCapturePrivate);
   skip.insert(APIPermissionID::kWebrtcLoggingPrivate);
   skip.insert(APIPermissionID::kWebrtcLoggingPrivateAudioDebug);
-  skip.insert(APIPermissionID::kWebstorePrivate);
   skip.insert(APIPermissionID::kWmDesksPrivate);
   skip.insert(APIPermissionID::kSystemLog);
   skip.insert(APIPermissionID::kOdfsConfigPrivate);
