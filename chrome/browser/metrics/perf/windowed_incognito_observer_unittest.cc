88638cb589925a605a13e6341b2690d7fc9ff612
Daniel Rubery <drubery@chromium.org>
2026-04-30 15:17:48 -0700

    Add ScopedGlobalFeaturesOverride and use it in tests
    Link: https://chromium-review.googlesource.com/id/I31c06160c032e49c4e772149094787186a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804745
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623525}

--- chrome/browser/metrics/perf/windowed_incognito_observer_unittest.cc
+++ chrome/browser/metrics/perf/windowed_incognito_observer_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/global_features.h"
+#include "chrome/browser/global_features_test_support.h"
 #include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 #include "chrome/browser/ui/browser_window/test/fake_global_browser_collection.h"
 #include "chrome/browser/ui/browser_window/test/mock_browser_window_interface.h"
@@ -69,15 +70,10 @@ std::unique_ptr<GlobalFeatures> CreateGlobalFeatures() {
 
 class WindowedIncognitoMonitorTest : public testing::Test {
  public:
-  WindowedIncognitoMonitorTest() {
-    GlobalFeatures::ReplaceGlobalFeaturesForTesting(
-        base::BindRepeating(&CreateGlobalFeatures));
-  }
+  WindowedIncognitoMonitorTest()
+      : features_override_(base::BindRepeating(&CreateGlobalFeatures)) {}
 
-  ~WindowedIncognitoMonitorTest() override {
-    GlobalFeatures::ReplaceGlobalFeaturesForTesting(
-        GlobalFeatures::GlobalFeaturesFactory());
-  }
+  ~WindowedIncognitoMonitorTest() override = default;
 
   WindowedIncognitoMonitorTest(const WindowedIncognitoMonitorTest&) = delete;
   WindowedIncognitoMonitorTest& operator=(const WindowedIncognitoMonitorTest&) =
@@ -143,6 +139,9 @@ class WindowedIncognitoMonitorTest : public testing::Test {
   static size_t next_browser_id;
 
   std::unique_ptr<TestWindowedIncognitoMonitor> incognito_monitor_;
+
+ private:
+  test::ScopedGlobalFeaturesOverride features_override_;
 };
 
 size_t WindowedIncognitoMonitorTest::next_browser_id = 1;
