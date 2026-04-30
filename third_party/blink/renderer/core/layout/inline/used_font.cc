30412e3d517e3988f1d02cb0634d17e4b37b53d1
Kent Tamura <tkent@chromium.org>
2026-04-30 15:41:47 -0700

    text-fit: Add branch prediction hints and inline FloatAscent for performance
    These changes aim to improve the performance of ink overflow computation.
    
    * Add `[[likely]]` and `[[unlikely]]` attributes
    
    * Move UsedFont::FloatAscent() definition to the header for ease
      of inlining
    
    Bug: 417306102
    Change-Id: I21a80e7487a0a881004c2f3ec7187f22050cac21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806045
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623540}

--- third_party/blink/renderer/core/layout/inline/used_font.cc
+++ third_party/blink/renderer/core/layout/inline/used_font.cc
@@ -7,11 +7,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-float UsedFont::FloatAscent() const {
-  if (const auto* font_data = PrimaryFont()) {
-    return font_data->GetFontMetrics().FloatAscent() * text_fit_scaling_factor_;
-  }
-  return 0.0f;
-}
-
 }  // namespace blink
