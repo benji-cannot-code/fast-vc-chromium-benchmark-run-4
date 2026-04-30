487a227c7fc1a9b491801027a0eab1e61a288899
Kenichi Ishibashi <bashi@chromium.org>
2026-04-29 21:25:31 -0700

    [network] Plumb Platform LNA permission error to browser process
    This CL implements the network service side of the plumbing for the
    Local Network Protection (LNP) permission prompt.
    
    When the //net stack returns ERR_LOCAL_NETWORK_PERMISSION_MISSING,
    URLRequest::Delegate::OnPlatformLocalNetworkAccessPermissionRequired is
    triggered. URLLoader handles this callback by forwarding it to its
    network observer (StoragePartitionImpl in the browser process) via Mojo.
    
    Depending on the user's response, URLLoader calls either
    SetPlatformLocalNetworkAccessGranted() or
    CancelPlatformLocalNetworkAccessRequest() on the URLRequest to resume or
    fail the connection.
    
    Bug: 444472891
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Change-Id: I1c3f4aa3756eb1e53cbecc58ee09c38016121a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7672857
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622940}

--- services/network/test/test_url_loader_network_observer.h
+++ services/network/test/test_url_loader_network_observer.h
@@ -27,6 +27,10 @@ class TestURLLoaderNetworkObserver
     ignore_certificate_errors_ = ignore_certificate_errors;
   }
 
+  void set_platform_local_network_permission_response(bool granted) {
+    platform_local_network_permission_response_ = granted;
+  }
+
   void FlushReceivers();
 
   // mojom::URLLoaderNetworkServiceObserver overrides:
@@ -53,6 +57,8 @@ class TestURLLoaderNetworkObserver
       mojom::TransportType type,
       network::mojom::IPAddressSpace ip_address_space,
       OnLocalNetworkAccessPermissionRequiredCallback callback) override;
+  void OnPlatformLocalNetworkPermissionRequired(
+      OnPlatformLocalNetworkPermissionRequiredCallback callback) override;
   void OnClearSiteData(
       const GURL& url,
       const std::string& header_value,
@@ -88,6 +94,7 @@ class TestURLLoaderNetworkObserver
  private:
   mojo::ReceiverSet<mojom::URLLoaderNetworkServiceObserver> receivers_;
   bool ignore_certificate_errors_ = false;
+  bool platform_local_network_permission_response_ = false;
 };
 
 }  // namespace network
