207f250db89b31e2d427ba852fe8f9d28bac39fc
Ian Vollick <vollick@chromium.org>
2026-04-30 06:26:52 -0700

    Minor cleanup to GlicInvokeHandler and related classes.
    No intended visible change to real users.
    
    GlicInstance now has a RegisterWillBeDestroyed, and the callback is
    called before destruction starts. GlicInvokeOptions on_success and
    on_error are always invoked asynchronously. Tight coupling between
    GlicInstanceCoordinatorImpl instance destruction and GlicInvokeHandler
    was removed. Removed a brief dangling ptr in
    GlicInstanceCoordinatorImpl::RemoveInstance for last_active_instance_.
    Updated GlicInvokeHandler to explicitly listen for instance and tab
    destruction. This avoids implicitly dependencies between ordering and
    lifetime of other classes. GlicWindowControllerImpl supports
    RegisterWillBeDestroyed for completeness, but this will be removed in a
    subsequent CL as it should never fire.
    
    Change-Id: I807cf5bc3cf32eb0151c5e0a486d133ce8471bd2
    Bug: 494227577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7683887
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623118}

--- chrome/browser/glic/service/glic_invoke_handler.h
+++ chrome/browser/glic/service/glic_invoke_handler.h
@@ -20,13 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/public/glic_instance.h"
 #include "chrome/browser/glic/public/glic_invoke_options.h"
 #include "chrome/browser/glic/public/glic_passkeys.h"
+#include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/web_contents_observer.h"
 
 class Profile;
-
-namespace tabs {
-class TabInterface;
-}
 
 namespace glic {
 
@@ -50,6 +47,9 @@ class GlicInvokeHandler {
   static ResolvedTarget ResolveTargetSurface(Profile* profile,
                                              const Target& target);
 
+  // `tab` must be non-nullptr.
+  // `completion_callback` should be called exactly once and results in
+  // destruction of `this`.
   GlicInvokeHandler(
       GlicInstanceImpl& instance,
       ResolvedTarget resolved_target,
@@ -64,26 +64,30 @@ class GlicInvokeHandler {
   // Kicks off the invocation process.
   void Invoke();
 
-  // Ends the invocation process with the given error.
-  // May delete this.
-  void OnError(GlicInvokeError error);
 
  private:
   mojom::InvokeOptionsPtr CreateMojoOptions();
-  bool RequiresAutoSubmitIncompatibleFre() const;
-  bool RequiresOverrideIncompatibleFre() const;
 
-  // May delete this.
+  // Deletes `this`. Exactly one of these methods will be called.
   void OnSuccess();
-  void OnTabClosed(tabs::TabInterface* tab);
+  void OnError(GlicInvokeError error);
 
+  void OnTabWillDetach(tabs::TabInterface* tab,
+                       tabs::TabInterface::DetachReason reason);
+  void OnInstanceWillBeDestroyed(GlicInstance* instance);
+
+  // This class listens to destruction of both GlicInstanceImpl and TabInterface
+  // and is guaranteed to destroy itself first.
   const base::raw_ref<GlicInstanceImpl> instance_;
   raw_ptr<tabs::TabInterface> tab_;
   GlicInvokeOptions options_;
   std::optional<InvokeWithAutoSubmitPasskey> auto_submit_passkey_;
+
+  // Calling this synchronously destroys `this`.
   CompletionCallback completion_callback_;
 
   bool should_wait_for_load_ = false;
+  base::CallbackListSubscription instance_destruction_subscription_;
   base::CallbackListSubscription tab_destruction_subscription_;
   base::OneShotTimer timeout_timer_;
 
