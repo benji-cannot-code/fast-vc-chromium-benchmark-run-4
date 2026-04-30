d6f68877c33a14fba2b5b2e8cb5077a4f11a7af9
Jan Zarzycki <janzarzycki@google.com>
2026-04-30 14:52:45 -0700

    [SaaS report] Record Gemini in Chrome usage for SaaS reporting.
    This change integrates Glic with the enterprise SaaS usage reporting
    feature. SaaS usage event is recorded once a Glic instance is shown to
    the user. The reporting is performed only once per Glic instance.
    
    Bug: b:498699507
    Change-Id: Ibab54479bf4c8a5b113b6f22b327410935028632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800008
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Jan Zarzycki <janzarzycki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623509}

--- chrome/browser/glic/service/metrics/glic_instance_metrics.h
+++ chrome/browser/glic/service/metrics/glic_instance_metrics.h
@@ -35,6 +35,10 @@ namespace tabs {
 class TabInterface;
 }
 
+namespace enterprise_reporting {
+class SaasUsageReportingController;
+}
+
 namespace base {
 class TimeTicks;
 class TimeDelta;
@@ -159,6 +163,8 @@ class GlicInstanceMetrics : public GlicInstanceMetricsBackwardsCompatibility {
   GlicInstanceMetrics(
       const metrics::ProfileMetricsService* profile_metrics_service,
       GlicSharingManager* sharing_manager,
+      enterprise_reporting::SaasUsageReportingController*
+          saas_usage_reporting_controller,
       PrefService* pref_service = nullptr);
   ~GlicInstanceMetrics() override;
 
@@ -405,9 +411,12 @@ class GlicInstanceMetrics : public GlicInstanceMetricsBackwardsCompatibility {
   base::CallbackListSubscription tab_pinning_status_subscription_;
   const raw_ref<const metrics::ProfileMetricsService> profile_metrics_service_;
   raw_ptr<GlicSharingManager> sharing_manager_ = nullptr;
+  raw_ptr<enterprise_reporting::SaasUsageReportingController>
+      saas_usage_reporting_controller_ = nullptr;
   raw_ptr<PrefService> pref_service_ = nullptr;
 
   bool first_side_panel_close_recorded_ = false;
+  bool saas_usage_recorded_ = false;
 
   // The following variables are used for recording scroll related metrics.
   //
