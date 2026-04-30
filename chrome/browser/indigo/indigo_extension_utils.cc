a31be11877ac318bd94c3f308557b550fb3a1a0c
Baran Erfani <baranerf@google.com>
2026-04-30 11:05:44 -0700

    Add indigo text overlay initial icon
    This change adds a placeholder icon and implements the basic animation
    for icon entry.
    
    Follow up CL: Once the icon stabilizes, it will transition into a lottie
    animation.
    
    Demo: http://screencast/cast/NTI3NTg4MDMwNDY3Mjc2OHwyYjdiMDJlZS0zNA
    Bug: 486887445
    Change-Id: Ifafd076dfea7716902608b9ab67a97365632531e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802916
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623345}

--- chrome/browser/indigo/indigo_extension_utils.cc
+++ chrome/browser/indigo/indigo_extension_utils.cc
@@ -7,11 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <iterator>
 #include <string>
+#include <vector>
 
+#include "base/containers/span.h"
 #include "base/i18n/rtl.h"
+#include "base/no_destructor.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/global_features.h"
+#include "chrome/browser/indigo/resources/grit/indigo_browser_resources.h"
+#include "chrome/browser/indigo/resources/grit/indigo_browser_resources_map.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/indigo_resources.h"
 #include "chrome/grit/indigo_resources_map.h"
@@ -47,7 +52,20 @@ base::DictValue GetStrings() {
 }
 
 base::span<const webui::ResourcePath> GetResources() {
-  return base::span<const webui::ResourcePath>(kIndigoResources);
+  static const base::NoDestructor<std::vector<webui::ResourcePath>>
+      kAllResources([] {
+        std::vector<webui::ResourcePath> resources;
+        resources.reserve(std::size(kIndigoResources) +
+                          std::size(kIndigoBrowserResources));
+        for (const auto& resource : kIndigoResources) {
+          resources.push_back(resource);
+        }
+        for (const auto& resource : kIndigoBrowserResources) {
+          resources.push_back(resource);
+        }
+        return resources;
+      }());
+  return *kAllResources;
 }
 
 }  // namespace indigo_extension_utils
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -171,6 +171,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "messages": [2540],
     "includes": [2600],
   },
+  "chrome/browser/indigo/resources/browser_resources.grd": {
+    "includes": [2640],
+  },
   # END chrome/browser section.
 
   # START chrome/ WebUI resources section
