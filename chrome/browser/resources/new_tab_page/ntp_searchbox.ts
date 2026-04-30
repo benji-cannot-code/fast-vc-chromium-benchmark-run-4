a65a72723debfb0d2977c5dda18028aa19a0f3f6
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-04-30 13:13:50 -0700

    [composebox] Add OnDriveUploadClicked to contextual_searchbox handler.
    This CL adds the OnDriveUploadClicked() method to the PageHandler
    interface in searchbox.mojom. This allows the contextual action menu to
    trigger drive upload functionality from the frontend. The method is
    currently a no-op in the C++ layer (ContextualSearchboxHandler).
    Frontend events are wired up in both contextual_tasks and cr-components
    layers to handle the event and record metrics.
    
    Bug: b:493561714
    Change-Id: Ifddc1a866818b7b25d870113e1513b74ffc461e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785057
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623383}

--- chrome/browser/resources/new_tab_page/ntp_searchbox.ts
+++ chrome/browser/resources/new_tab_page/ntp_searchbox.ts
@@ -530,6 +530,10 @@ export class NtpSearchboxElement extends NtpSearchboxElementBase implements
     this.onVoiceSearchClick_();
   }
 
+  protected onOpenDriveUpload_() {
+    this.pageHandler().onDriveUploadClicked();
+  }
+
   protected onContextMenuEntrypointClick_() {
     this.pageHandler().activateMetricsFunnel('PlusButton');
   }
