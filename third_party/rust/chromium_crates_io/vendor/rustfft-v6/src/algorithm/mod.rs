a2c27d6ac82abe25cfc96acb99a75b4e2cc2affe
Jordan Bayles <jophba@chromium.org>
2026-04-30 13:54:26 -0700

    [Symphonia] Bring in rustfft for Symphonia, webaudio
    This patch imports rustfft, for potential enabling in Symphonia as well
    as in Web audio.
    
    Bug: 485680374
    Change-Id: Ibdcc81ca8c912444d1aeec968bcb285ee60c0d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685455
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623453}

---
+++ third_party/rust/chromium_crates_io/vendor/rustfft-v6/src/algorithm/mod.rs
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+mod bluesteins_algorithm;
+mod dft;
+mod good_thomas_algorithm;
+mod mixed_radix;
+mod raders_algorithm;
+mod radix3;
+mod radix4;
+mod radixn;
+
+/// Hardcoded size-specfic FFT algorithms
+pub mod butterflies;
+
+pub use self::bluesteins_algorithm::BluesteinsAlgorithm;
+pub use self::dft::Dft;
+pub use self::good_thomas_algorithm::{GoodThomasAlgorithm, GoodThomasAlgorithmSmall};
+pub use self::mixed_radix::{MixedRadix, MixedRadixSmall};
+pub use self::raders_algorithm::RadersAlgorithm;
+pub use self::radix3::Radix3;
+pub use self::radix4::Radix4;
+pub(crate) use self::radixn::RadixN;
---
+++ third_party/rust/chromium_crates_io/vendor/rustfft-v6/src/neon/mod.rs
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#[macro_use]
+mod neon_common;
+#[macro_use]
+mod neon_vector;
+
+#[macro_use]
+pub mod neon_butterflies;
+pub mod neon_prime_butterflies;
+pub mod neon_radix4;
+
+mod neon_utils;
+
+pub mod neon_planner;
+
+use std::arch::aarch64::{float32x4_t, float64x2_t};
+
+use crate::FftNum;
+use neon_vector::NeonVector;
+
+pub trait NeonNum: FftNum {
+    type VectorType: NeonVector<ScalarType = Self>;
+}
+
+impl NeonNum for f32 {
+    type VectorType = float32x4_t;
+}
+impl NeonNum for f64 {
+    type VectorType = float64x2_t;
+}
