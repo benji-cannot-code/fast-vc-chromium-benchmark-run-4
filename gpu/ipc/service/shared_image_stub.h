2480d2a6cedd63dde13d88e8886903b0a7c0716b
Saifuddin Hitawala <hitawala@chromium.org>
2026-04-30 07:48:14 -0700

    [gpu] Refactor SharedImageStub
    Remove SharedImageStub::DestroySharedImage as it is no longer used.
    Also remove GetSharedImageDestructionCallback as it is also not used.
    
    Also reorder creation and update methods so that they are close to
    each other and easier for the reader. Also make them private as they
    are not used outside SharedImageStub.
    
    Follow-up from [1] where I got to look at SIStub closely.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7800260
    
    Bug: 40263319
    Change-Id: I18e06b113600c623e35f35a1be3d1ddf8c63caa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800584
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623184}

--- gpu/ipc/service/shared_image_stub.h
+++ gpu/ipc/service/shared_image_stub.h
@@ -42,9 +42,6 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub {
  public:
   ~SharedImageStub();
 
-  using SharedImageDestructionCallback =
-      base::OnceCallback<void(const gpu::SyncToken&)>;
-
   static std::unique_ptr<SharedImageStub> Create(GpuChannel* channel,
                                                  int32_t route_id);
 
@@ -62,18 +59,6 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub {
   }
   const scoped_refptr<gpu::GpuChannelSharedImageInterface>&
   shared_image_interface();
-
-  SharedImageDestructionCallback GetSharedImageDestructionCallback(
-      const Mailbox& mailbox);
-
-  bool CreateSharedImage(
-      const Mailbox& mailbox,
-      const SharedImageInfo& info,
-      gfx::GpuMemoryBufferHandle handle,
-      std::optional<SharedImagePoolId> pool_id = std::nullopt);
-
-  bool UpdateSharedImage(const Mailbox& mailbox,
-                         gfx::GpuFenceHandle in_fence_handle);
 
 #if BUILDFLAG(IS_WIN)
   void CopyToGpuMemoryBufferAsync(const Mailbox& mailbox,
@@ -102,8 +87,17 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub {
       mojom::CreateSharedImageWithDataParamsPtr params);
   void OnCreateSharedImageWithBuffer(
       mojom::CreateSharedImageWithBufferParamsPtr params);
+  bool CreateSharedImage(
+      const Mailbox& mailbox,
+      const SharedImageInfo& info,
+      gfx::GpuMemoryBufferHandle handle,
+      std::optional<SharedImagePoolId> pool_id = std::nullopt);
+
   void OnUpdateSharedImage(const Mailbox& mailbox,
                            gfx::GpuFenceHandle in_fence_handle);
+  bool UpdateSharedImage(const Mailbox& mailbox,
+                         gfx::GpuFenceHandle in_fence_handle);
+
   void OnAddReference(const Mailbox& mailbox);
 
   void OnDestroySharedImage(const Mailbox& mailbox);
@@ -125,9 +119,6 @@ class GPU_IPC_SERVICE_EXPORT SharedImageStub {
 
   ContextResult Initialize();
   void OnError();
-
-  // Wait on the sync token if any and destroy the shared image.
-  void DestroySharedImage(const Mailbox& mailbox, const SyncToken& sync_token);
 
   std::string GetLabel(const std::string& debug_label) const;
 
