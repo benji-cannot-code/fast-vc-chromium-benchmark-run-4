a6c149d852e84904ecba53ce8bed8224602e75a2
Shweta Bindal <shwetabindal@microsoft.com>
2026-04-29 20:27:27 -0700

    [Editing] Move Position-based word navigation overloads
    Moves word navigation functions (StartOfWordPosition, EndOfWordPosition,
    NextWordPosition, PreviousWordPosition, MiddleOfWordPosition,
    IsWordBreak, IsWordBoundary) from visible_units_word.cc to
    position_units_word.cc, with declarations in position_units.h.
    
    Bug: 499921559
    Change-Id: I60ecdceff21d655fd9563bc5b600e1f2d7040a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7720024
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Shweta Bindal <shwetabindal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1622929}

--- third_party/blink/renderer/core/editing/visible_units_word_test.cc
+++ third_party/blink/renderer/core/editing/visible_units_word_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/visible_units.h"
 
+#include "third_party/blink/renderer/core/editing/position_units.h"
 #include "third_party/blink/renderer/core/editing/selection_template.h"
 #include "third_party/blink/renderer/core/editing/testing/editing_test_base.h"
 #include "third_party/blink/renderer/core/editing/visible_position.h"
