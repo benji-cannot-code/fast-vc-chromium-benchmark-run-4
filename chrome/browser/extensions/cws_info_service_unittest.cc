27e058fd585327a5f05fcbee884cfa4112013258
Cameron Higgins <cameronhiggins@google.com>
2026-04-30 13:29:28 -0700

    macOS 26 - Disable `CWSInfoServiceTest` cases with timeout.
    Temporarily disables tests that use `task_environment()->FastForwardBy` for long periods as it causes failures on MacOS 26.
    
    Bug: 434660312, 481036378
    Link: https://chromium-review.googlesource.com/id/If637298da85a4c8bd5aeb7a4cfaab63e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806786
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623409}

--- chrome/browser/extensions/cws_info_service_unittest.cc
+++ chrome/browser/extensions/cws_info_service_unittest.cc
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
@@ -237,6 +241,14 @@ TEST_F(CWSInfoServiceTest, IgnoresNonCWSExtensions) {
 }
 
 TEST_F(CWSInfoServiceTest, HandlesNetworkErrorAndBadServerResponse) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
+
   base::HistogramTester histogram_tester;
   scoped_refptr<const Extension> test1 =
       AddExtension("test1", /* updates_from_cws= */ true);
@@ -381,6 +393,13 @@ TEST_F(CWSInfoServiceTest, HandlesMultipleRequestsPerInfoCheck) {
 }
 
 TEST_F(CWSInfoServiceTest, SchedulesStartupAndPeriodicInfoChecks) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   // Add an extension to cause queries to CWS.
   scoped_refptr<const Extension> test1 =
       AddExtension("test1", /*updates_from_cws=*/true);
@@ -440,6 +459,13 @@ TEST_F(CWSInfoServiceTest, SchedulesStartupAndPeriodicInfoChecks) {
 // If there are no new extensions installed, CWS Info is only
 // requested after a fetch interval has elapsed.
 TEST_F(CWSInfoServiceTest, UpdatesExistingInfoAtUpdateIntervals) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   // Add an extension to cause queries to CWS.
   scoped_refptr<const Extension> test1 =
       AddExtension("test1", /*updates_from_cws=*/true);
