d513ab9b69b710e6726aeaa9e0565f6e6684ec46
Koji Ishii <kojii@chromium.org>
2026-04-30 03:59:02 -0700

    Remove a leftover for `kGenericFontSettingCache`
    This is a follow up to crrev.com/c/5964277.
    
    This patch has no behavior changes.
    
    Bug: 327545434
    Change-Id: I7ce8521eeb022498e970e56d47422f604014853c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796915
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623069}

--- third_party/blink/renderer/platform/fonts/generic_font_family_settings.h
+++ third_party/blink/renderer/platform/fonts/generic_font_family_settings.h
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PLATFORM_EXPORT BASE_DECLARE_FEATURE(kGenericFontSettingCache);
-
 class PLATFORM_EXPORT GenericFontFamilySettings {
   DISALLOW_NEW();
 
