c8bfde89db80a73e6ccf7f0f8e0517c45a1993c1
Mark Schillaci <mschillaci@google.com>
2026-04-30 13:31:49 -0700

    [histogram-cleanup] Remove expired histogram: Media.MediaDevices.GetAllScreensMedia.Latency
    This histogram expired on 2023-05-01.
    
    Bug: 503367825
    Change-Id: I12773a1f9488c06d70a1c5112461dd187e93349d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765804
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623413}

--- third_party/blink/renderer/bindings/core/v8/script_promise_resolver_with_tracker.h
+++ third_party/blink/renderer/bindings/core/v8/script_promise_resolver_with_tracker.h
@@ -103,6 +103,8 @@ class CORE_EXPORT ScriptPromiseResolverWithTracker
     latency_suffix_ = std::move(latency_suffix);
   }
 
+  void SuppressLatencyRecording() { should_record_latency_ = false; }
+
   void RecordAndThrowDOMException(ExceptionState& exception_state,
                                   DOMExceptionCode exception_code,
                                   const String& message,
@@ -142,8 +144,9 @@ class CORE_EXPORT ScriptPromiseResolverWithTracker
   }
 
   void RecordLatency() {
-    if (is_latency_recorded_)
+    if (is_latency_recorded_ || !should_record_latency_) {
       return;
+    }
 
     is_latency_recorded_ = true;
     const base::TimeDelta elapsed = base::TimeTicks::Now() - start_time_;
@@ -174,6 +177,7 @@ class CORE_EXPORT ScriptPromiseResolverWithTracker
   String latency_suffix_ = "Latency";  // Mutable through SetLatencySuffix().
   bool is_latency_recorded_ = false;
   bool is_result_recorded_ = false;
+  bool should_record_latency_ = true;
 };
 
 }  // namespace blink
