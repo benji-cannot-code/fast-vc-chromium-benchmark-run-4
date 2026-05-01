ee51a6c885e9a743f0bfedd8c95aa38099b8a865
Alex Turner <alexmt@chromium.org>
2026-05-01 12:35:20 -0700

    Add latency and trigger source metrics to PageContentExtractionService
    This CL adds metrics to `AnnotatedPageContentRequest` to monitor
    automatic extraction latency and triggers.
    
    Bug: b:490161242
    Change-Id: Iada233599dac92ea6662a3eee4c534fb6c314141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762270
    Reviewed-by: Aaron Leventhal <aleventhal@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624005}

--- components/page_content_annotations/content/page_content_extraction_service.cc
+++ components/page_content_annotations/content/page_content_extraction_service.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
 #include "components/feature_engagement/public/feature_constants.h"
@@ -244,7 +245,7 @@ void PageContentExtractionService::OnVisibilityChanged(
   }
 
   std::optional<ExtractedPageContentResult> extracted_result =
-      request->GetCachedContentAndEligibility();
+      request->GetCachedContentAndEligibility(/*log_metrics=*/false);
   if (extracted_result) {
     page_content_cache_handler_->OnVisibilityChanged(
         tab_id, ToWebStateWrapper(web_contents),
