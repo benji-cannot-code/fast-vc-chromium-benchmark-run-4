a5dc9c952fc68a4d78a0446511cf11daf396a3b2
Eric Ekey <ericekey@google.com>
2026-05-01 06:53:43 -0700

    [iOS][AppBar] Add Touch Down State Opacity to Toolbar Buttons
    Opacity of button images is lowered during the touch down state on
    toolbar buttons.
    
    Screencap: https://screenshot.googleplex.com/7wQSRcEn9yQ9iSz
    
    Fixed: 502843434
    Change-Id: I585751f00bd90c76568fb573139828192a85d689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808166
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623802}

--- ios/chrome/browser/toolbar/ui/buttons/toolbar_button.h
+++ ios/chrome/browser/toolbar/ui/buttons/toolbar_button.h
@@ -8,12 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#include "ios/chrome/browser/toolbar/ui/buttons/toolbar_button_visibility.h"
+#import "ios/chrome/browser/toolbar/ui/buttons/toolbar_button_visibility.h"
+#import "ios/chrome/common/ui/elements/highlight_button.h"
 
 using ToolbarButtonImageLoader = UIImage* (^)(void);
 
 // Button displayed in the toolbar.
-@interface ToolbarButton : UIButton
+@interface ToolbarButton : HighlightButton
 
 // The visibility mask for this button.
 @property(nonatomic, assign) ToolbarButtonVisibility visibilityMask;
