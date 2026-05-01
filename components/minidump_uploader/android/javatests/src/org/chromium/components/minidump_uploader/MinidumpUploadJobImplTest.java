8fd002ea654e0e3f9500ecd7b737df8afa0b6262
Sean Maher <spvm@chromium.org>
2026-05-01 12:35:46 -0700

    minidump_uploader: remove google URL on non-branded builds
    Prevents users accidentally uploading reports to google servers, given
    that there have been cases where third-parties are uploading non-chrome
    crash reports to Google servers.
    
    Bug: 507913898
    Change-Id: I5daecaab677194d0ec320fa21d69bb92f2408472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800267
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Sean Maher <spvm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624006}

--- components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/MinidumpUploadJobImplTest.java
+++ components/minidump_uploader/android/javatests/src/org/chromium/components/minidump_uploader/MinidumpUploadJobImplTest.java
@@ -7,7 +7,9 @@ package org.chromium.components.minidump_uploader;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -36,7 +38,24 @@ public class MinidumpUploadJobImplTest {
 
     private static final String BOUNDARY = "TESTBOUNDARY";
 
-    /** Test to ensure the minidump uploading mechanism allows the expected number of upload retries. */
+    private static final String CRASH_URL = "https://clients2.google.com/cr/report";
+
+    private String mOriginalCrashUrl;
+
+    @Before
+    public void setUp() {
+        mOriginalCrashUrl = MinidumpUploader.sCrashUrlString;
+        MinidumpUploader.setCrashUrlStringForTesting(CRASH_URL);
+    }
+
+    @After
+    public void tearDown() {
+        MinidumpUploader.setCrashUrlStringForTesting(mOriginalCrashUrl);
+    }
+
+    /**
+     * Test to ensure the minidump uploading mechanism allows the expected number of upload retries.
+     */
     @Test
     public void testRetryCountRespected() throws IOException {
         final CrashReportingPermissionManager permManager =
@@ -118,22 +137,20 @@ public class MinidumpUploadJobImplTest {
                 new MinidumpUploadCallableCreator() {
                     @Override
                     public MinidumpUploadCallable createCallable(File minidumpFile, File logfile) {
+                        MinidumpUploader uploader =
+                                new MinidumpUploader(new FailingHttpUrlConnectionFactory());
                         return new MinidumpUploadCallable(
-                                minidumpFile,
-                                logfile,
-                                new MinidumpUploader(new FailingHttpUrlConnectionFactory()),
-                                permManager);
+                                minidumpFile, logfile, uploader, permManager);
                     }
                 });
         callables.add(
                 new MinidumpUploadCallableCreator() {
                     @Override
                     public MinidumpUploadCallable createCallable(File minidumpFile, File logfile) {
+                        MinidumpUploader uploader =
+                                new MinidumpUploader(new TestHttpURLConnectionFactory());
                         return new MinidumpUploadCallable(
-                                minidumpFile,
-                                logfile,
-                                new MinidumpUploader(new TestHttpURLConnectionFactory()),
-                                permManager);
+                                minidumpFile, logfile, uploader, permManager);
                     }
                 });
         MinidumpUploadJob minidumpUploadJob =
@@ -322,10 +339,12 @@ public class MinidumpUploadJobImplTest {
                             mCancelReturnValue = cancelUploads();
                         }
                     };
+            MinidumpUploader uploader =
+                    new MinidumpUploader(new FakeHttpUrlConnectionFactory(mSuccessfulUpload, hook));
             return new MinidumpUploadCallable(
                     minidumpFile,
                     logfile,
-                    new MinidumpUploader(new FakeHttpUrlConnectionFactory(mSuccessfulUpload, hook)),
+                    uploader,
                     mDelegate.createCrashReportingPermissionManager());
         }
     }
