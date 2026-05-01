ec3dbf942c6ff28525846e564ffae8d7e61c672f
Luc Nguyen <lucnguyen@google.com>
2026-05-01 07:12:20 -0700

    Sync external protos to add warning to not modify BUILD.gn manually
    Context:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7795284
    
    Also update various references to LaCrOS since it's been removed from
    the proto.
    
    Change-Id: I70865d26e2b4d214d897ebbaff72d92a98a92e20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807793
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623813}

--- components/metrics/debug/metrics_internals_utils.cc
+++ components/metrics/debug/metrics_internals_utils.cc
@@ -58,8 +58,6 @@ std::string PlatformToString(variations::Study::Platform platform) {
       return "Fuchsia";
     case variations::Study::PLATFORM_ANDROID_WEBLAYER:
       return "WebLayer";
-    case variations::Study::PLATFORM_CHROMEOS_LACROS:
-      return "ChromeOS Lacros";
   }
   NOTREACHED();
 }
