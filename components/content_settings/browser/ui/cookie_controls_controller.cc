622614180d509bedca28752a2e8c43e87191b09f
Fiona Macintosh <fmacintosh@google.com>
2026-05-01 06:57:15 -0700

    Feature gate user bypass animation logic
    Bug: b:382043551
    Change-Id: I746ed156e03007a07c1387f650770b194943e474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808164
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623806}

--- components/content_settings/browser/ui/cookie_controls_controller.cc
+++ components/content_settings/browser/ui/cookie_controls_controller.cc
@@ -376,6 +376,9 @@ void CookieControlsController::UpdatePageReloadStatus(
   }
   SetStateChangedViaBypass(false);
   recent_reloads_count_ = recent_reloads_count;
+  if (base::FeatureList::IsEnabled(features::kUserBypassUxSimplification)) {
+    return;
+  }
 
   if (recent_reloads_count_ >= features::kUserBypassUIReloadCount.Get()) {
     for (auto& observer : observers_) {
@@ -399,6 +402,9 @@ void CookieControlsController::OnPageFinishedLoading() {
   // Ensure the bubble is closed before subsequent calls are made to update the
   // UI.
   OnBubbleCloseTriggered();
+  if (base::FeatureList::IsEnabled(features::kUserBypassUxSimplification)) {
+    return;
+  }
   for (auto& observer : observers_) {
     observer.OnFinishedPageReloadWithChangedSettings();
   }
