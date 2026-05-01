a7b5d099bfda89cdf4cca1ee482731d0b40736f7
Andrew Verge <averge@chromium.org>
2026-04-30 18:47:10 -0700

    Remove window.fence.notifyEvent and associated code.
    This API was added as part of the Fenced Storage Read feature,
    which is no longer launching. This CL cleans up the implementation
    in Blink and content/, as well as associated WPTs and histograms.
    It also removes the FencedFrameViewportObserver, which was only
    necessary due to the existence of notifyEvent.
    
    OBSOLETE_HISTOGRAMS=fence.notifyEvent API is being removed, so features related to its use no longer need to be tracked.
    
    Bug: 499191497
    Change-Id: I337de9e8b902a1927a99798d69acfdc8d990674d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804730
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Andrew Verge <averge@chromium.org>
    Reviewed-by: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623661}

--- content/public/test/fake_remote_frame.cc
+++ content/public/test/fake_remote_frame.cc
@@ -111,6 +111,4 @@ void FakeRemoteFrame::CreateRemoteChildren(
     std::vector<blink::mojom::CreateRemoteChildParamsPtr> params,
     const std::optional<base::UnguessableToken>& navigation_metrics_token) {}
 
-void FakeRemoteFrame::ForwardFencedFrameEventToEmbedder(
-    const std::string& event_type) {}
 }  // namespace content
