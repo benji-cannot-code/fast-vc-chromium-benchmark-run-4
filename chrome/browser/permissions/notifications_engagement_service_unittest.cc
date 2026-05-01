036d4bccf2a723510aeed87e92346df6a15fe279
Cameron Higgins <cameronhiggins@google.com>
2026-05-01 12:33:26 -0700

    macOS 26 - Disable `EraseStaleEntries` with timeout.
    Temporarily disables tests that use `task_environment()->FastForwardBy` for 1 day+ as it causes failures on MacOS 26.
    
    Bug: 434660312, 481036378
    Link: https://chromium-review.googlesource.com/id/I628c013e9723ec17be643ee93394a6f96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807935
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1624002}

--- chrome/browser/permissions/notifications_engagement_service_unittest.cc
+++ chrome/browser/permissions/notifications_engagement_service_unittest.cc
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace permissions {
 constexpr char kEngagementKey[] = "click_count";
 constexpr char kDisplayedKey[] = "display_count";
@@ -339,6 +343,13 @@ TEST_F(NotificationsEngagementServiceTest,
 }
 
 TEST_F(NotificationsEngagementServiceTest, EraseStaleEntries) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   GURL url("https://www.google.com/");
 
   // Test that only entries older than 30 days are deleted.
