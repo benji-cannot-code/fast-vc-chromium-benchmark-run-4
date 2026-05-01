eee7a1cbee068bc226e51ba79d77bcdfdd9643fa
Peilin Wang <peilinwang@google.com>
2026-05-01 12:43:03 -0700

    [Animated PB] FeatureParam and implementation for jump to end without fade
    More context in this CL that implements the version with fade:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7805469.
    
    This CL implements the version without fade.
    
    Bug: 508276690
    Change-Id: I892c7a10f19e907812f00ce1886068b92fcba527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807950
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1624013}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1509,6 +1509,9 @@ public abstract class ChromeFeatureList {
                     ANDROID_ANIMATED_PROGRESS_BAR_IN_BROWSER,
                     "jump_to_completion_with_fade",
                     false);
+    public static final BooleanCachedFeatureParam sAndroidApbJumpToCompletionNoFade =
+            newBooleanCachedFeatureParam(
+                    ANDROID_ANIMATED_PROGRESS_BAR_IN_BROWSER, "jump_to_completion_no_fade", false);
     public static final BooleanCachedFeatureParam sAndroidThemeModuleForceDependencies =
             newBooleanCachedFeatureParam(
                     ANDROID_THEME_MODULE, "force_theme_module_dependencies", false);
@@ -1879,6 +1882,7 @@ public abstract class ChromeFeatureList {
             List.of(
                     // keep-sorted start
                     sAndroidAnimatedProgressBarFpsCap,
+                    sAndroidApbJumpToCompletionNoFade,
                     sAndroidApbJumpToCompletionWithFade,
                     sAndroidAppIntegrationModuleForceCardShow,
                     sAndroidAppIntegrationModuleShowThirdPartyCard,
