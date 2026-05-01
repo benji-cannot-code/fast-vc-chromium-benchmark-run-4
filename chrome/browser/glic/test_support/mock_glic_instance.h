47aebcf56019a333348d94607918bd9175e46425
Marke Hallowell <wry@chromium.org>
2026-05-01 14:08:30 -0700

    Expose CancelTask on the public GlicInstance api.
    Bug: b:508286072
    Link: https://chromium-review.googlesource.com/id/I2fa6400329dac93a2f35f4d8b8b795386a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809514
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624063}

--- chrome/browser/glic/test_support/mock_glic_instance.h
+++ chrome/browser/glic/test_support/mock_glic_instance.h
@@ -55,6 +55,7 @@ class MockGlicInstance : public GlicInstance {
               OnPolylinePointsChanged,
               (const std::vector<int>&),
               (override));
+  MOCK_METHOD(void, CancelTask, (), (override));
   MOCK_METHOD(void, BindTabForTesting, (tabs::TabInterface*), (override));
   MOCK_METHOD(Host&, host, (), (override));
 
