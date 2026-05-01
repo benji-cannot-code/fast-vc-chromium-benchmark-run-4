b5600935bc05db0e4fce2e8d6d7dec1ea96ea207
James Cook <jamescook@chromium.org>
2026-05-01 10:54:34 -0700

    Port ExtensionTelemetryServiceVerdictHandlerTest to desktop Android
    We missed this one during initial bring-up of desktop Android. The test
    passes without modification.
    
    Bug: 469417243
    Change-Id: Ie3a4c05616836a0966c774416d1a3ff1df64db5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809068
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623927}

--- chrome/browser/extensions/extension_telemetry_service_verdict_handler_unittest.cc
+++ chrome/browser/extensions/extension_telemetry_service_verdict_handler_unittest.cc
@@ -10,17 +10,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service_test_base.h"
 #include "chrome/browser/profiles/profile.h"
 #include "extensions/browser/blocklist_extension_prefs.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/test/extension_state_tester.h"
 
-namespace extensions {
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
+namespace extensions {
 namespace {
 
 // Extension ids used during testing.
 constexpr char kTestExtensionId[] = "behllobkkfkfnphdnhnkndlbkcpglgmj";
 constexpr char kUninstalledExtensionId[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-
-}  // namespace
 
 // Test suite to test Extension Telemetry service verdict handler.
 class ExtensionTelemetryServiceVerdictHandlerTest
@@ -31,6 +31,13 @@ class ExtensionTelemetryServiceVerdictHandlerTest
     // extension error controller on the first run.
     ExtensionPrefs::SetRunAlertsInFirstRunForTest();
   }
+
+  ExtensionTelemetryServiceVerdictHandlerTest(
+      const ExtensionTelemetryServiceVerdictHandlerTest&) = delete;
+  ExtensionTelemetryServiceVerdictHandlerTest& operator=(
+      const ExtensionTelemetryServiceVerdictHandlerTest&) = delete;
+
+  ~ExtensionTelemetryServiceVerdictHandlerTest() override = default;
 };
 
 TEST_F(ExtensionTelemetryServiceVerdictHandlerTest, HandlesMalwareExtension) {
@@ -187,4 +194,5 @@ TEST_F(ExtensionTelemetryServiceVerdictHandlerTest,
   service()->PerformActionBasedOnExtensionTelemetryServiceVerdicts(state_map);
 }
 
+}  // namespace
 }  // namespace extensions
