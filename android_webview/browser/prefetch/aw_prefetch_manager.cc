43a5a077bad7e2b18ba040d6c02b86550766235e
Taiyo Mizuhashi <taiyo@chromium.org>
2026-05-01 02:15:25 -0700

    OMTPrefetch: Fix AwPrefetchTest PrePrefetch tests falling back to normal Prefetch
    No behavior change in non-testing code.
    
    After crrev.com/c/7734235 and crrev.com/c/7741080, PrePrefetch succeeds
    only if `PrePrefetchService` has a pre-calculated header cache that can
    be used for an upcoming PrePrefetch. Currently, this is constructed from
    a hint provided from `AwPrefetchManager` during `PrePrefetchService`
    initialization
    
    Currently, Java-side e2e tests do not give any hints to
    `PrePrefetchService`, meaning that PrePrefetch always fails and falls
    back to a normal Prefetch. This makes them meaningless.
    
    This CL fixes this issue by setting an appropriate pref to provide those
    hints to `PrePrefetchService`. This CL introduces new test-only files to
    use `TestingPrefServiceSimple`, which is a test-only target.
    
    Bug: 452406598, 452389538
    Change-Id: I05356c1fd8164ed2534bbb497ccf4b615be469ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790485
    Reviewed-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623762}

--- android_webview/browser/prefetch/aw_prefetch_manager.cc
+++ android_webview/browser/prefetch/aw_prefetch_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/check_is_test.h"
+#include "base/no_destructor.h"
 #include "base/notimplemented.h"
 #include "base/trace_event/trace_event.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -533,7 +534,6 @@ PrefService* AwPrefetchManager::GetPrefService() {
       base::FeatureList::IsEnabled(features::kWebViewPrefetchOffTheMainThread));
 
   if (g_pref_service_for_testing) {
-    CHECK_IS_TEST();
     return g_pref_service_for_testing;
   }
   auto* aw_browser_context =
