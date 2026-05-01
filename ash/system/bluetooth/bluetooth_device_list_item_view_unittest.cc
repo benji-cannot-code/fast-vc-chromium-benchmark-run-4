0b1ef1339c1870e89f2e992a4de95840432fda68
Di Wu <diwux@google.com>
2026-04-30 17:55:32 -0700

    Ash: Migrate BluetoothDeviceListItemViewTest off RunUntilIdle
    This changelist is part of an ongoing series to modernize Ash tests by
    migrating them away from RunUntilIdle() and toward targeted waits for
    the specific async signal or state transition each test depends on.
    
    The Bluetooth list item fixture builds its widget and contents view
    synchronously. Remove the setup idle drain that is not tied to any later
    assertion in the fixture.
    
    Bug: b:506794349
    Test: CQ
    Change-Id: I94f496ad8c275e893b170262ac285d75374ede7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805725
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623600}

--- ash/system/bluetooth/bluetooth_device_list_item_view_unittest.cc
+++ ash/system/bluetooth/bluetooth_device_list_item_view_unittest.cc
@@ -123,8 +123,6 @@ class BluetoothDeviceListItemViewTest : public AshTestBase {
     widget_ = CreateFramelessTestWidget();
     widget_->SetFullscreen(true);
     widget_->SetContentsView(bluetooth_device_list_item.release());
-
-    base::RunLoop().RunUntilIdle();
   }
 
   void TearDown() override {
