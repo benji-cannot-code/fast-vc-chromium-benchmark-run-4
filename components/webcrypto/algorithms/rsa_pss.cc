d7d937b6bb7c8157af2cdbbdff212deaa443961e
Hubert Chao <hchao@chromium.org>
2026-05-01 08:14:41 -0700

    [WebCrypto] add supports() method
    Add the supports() method from
    https://wicg.github.io/webcrypto-modern-algos/#SubtleCrypto-method-supports.
    
    In the cases where the algorithm's operation takes in no parameters (and
    is not deriveBits/deriveKey), the support checks are done entirely in
    third_party/blink/renderer/modules/crypto/subtle_crypto.cc. If the
    operation is deriveBits/deriveKey, or algorithm has parameters for the
    call (e.g. for decrypt, if its one of the algorithms with parameters
    mentioned in
    https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/decrypt),
    then we call into the algorithm implementation to check if the
    parameters need to be checked.
    
    Change-Id: I5778cf6fcff03d9fe454ed7e8d581141f41febf3
    Bug: 450627017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738892
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623839}

--- components/webcrypto/algorithms/rsa_pss.cc
+++ components/webcrypto/algorithms/rsa_pss.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/numerics/safe_math.h"
 #include "components/webcrypto/algorithms/rsa.h"
 #include "components/webcrypto/algorithms/rsa_sign.h"
 #include "components/webcrypto/status.h"
@@ -53,6 +54,20 @@ class RsaPssImplementation : public RsaHashedAlgorithm {
                 bool* signature_match) const override {
     return RsaVerify(key, algorithm.RsaPssParams()->SaltLengthBytes(),
                      signature, data, signature_match);
+  }
+
+  bool Supports(blink::WebCryptoOperation op,
+                const blink::WebCryptoAlgorithm& algorithm,
+                std::optional<unsigned int> length_bits) const override {
+    if ((op == blink::kWebCryptoOperationSign) ||
+        (op == blink::kWebCryptoOperationVerify)) {
+      // There are salt restrictions, but realistically they need to be
+      // calculated against the size of the key, which isn't part of the
+      // algorithm.
+      return true;
+    } else {
+      return RsaHashedAlgorithm::Supports(op, algorithm, length_bits);
+    }
   }
 };
 
