ae3d54b48baad30675e497fad85a14610b3e428f
Alexis Hétu <sugoi@chromium.org>
2026-05-01 12:24:35 -0700

    [iOS] Allow Suggestion Refresh during Biometric Authentication
    Sometimes, retrieveSuggestionsForForm can be called during the
    Biometric Authentication phase, which causes the app to temporarily
    go into the inactive state. This was causing suggestions to fail to
    refresh while Reauthentication was happening.
    
    This CL changes the condition in IsSuggestionRefreshAllowed from
    "== UIApplicationStateActive" to "!= UIApplicationStateBackground",
    which matches the comment above this condition and allows
    suggestion refresh while the app is inactive, but not while it is
    backgrounded, as expected.
    
    Bug: 505431201
    Change-Id: I9937d1c17643ac611ae74e91413a1d47257c5125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809348
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623991}

--- ios/chrome/browser/autofill/form_input_accessory/coordinator/form_input_accessory_mediator.mm
+++ ios/chrome/browser/autofill/form_input_accessory/coordinator/form_input_accessory_mediator.mm
@@ -91,8 +91,8 @@ bool IsSuggestionRefreshAllowed() {
   // enabled.
   return base::FeatureList::IsEnabled(
              kThrottleFormInputAccessorySuggestionRefresh)
-             ? UIApplication.sharedApplication.applicationState ==
-                   UIApplicationStateActive
+             ? UIApplication.sharedApplication.applicationState !=
+                   UIApplicationStateBackground
              : true;
 }
 
