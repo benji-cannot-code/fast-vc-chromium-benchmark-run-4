12b806a27f545367d9a2de2bd54b1c427804932e
Cameron Higgins <cameronhiggins@google.com>
2026-05-01 07:47:48 -0700

    macOS 26 - Disable `AnchoredMessageTrigger` with timeout.
    Temporarily disables tests that use `task_environment()->FastForwardBy` for 1 day+ as it causes failures on MacOS 26.
    
    Bug: 434660312, 481036378
    Link: https://chromium-review.googlesource.com/id/Icc332578f29db66ccdd763f7551f01d26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807126
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623826}

--- chrome/browser/indigo/indigo_service_unittest.cc
+++ chrome/browser/indigo/indigo_service_unittest.cc
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace indigo {
 
 class IndigoServiceTest : public testing::Test {
@@ -151,6 +155,13 @@ TEST_F(IndigoServiceTest, PolicyChangeTriggersUpdate) {
 }
 
 TEST_F(IndigoServiceTest, AnchoredMessageTrigger) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   CreateService();
 
   EXPECT_TRUE(service_->CanShowAnchoredMessage());
