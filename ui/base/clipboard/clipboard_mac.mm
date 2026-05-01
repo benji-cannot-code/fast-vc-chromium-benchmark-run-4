6c9dcf11ceba5cf4461264133271cd192bcf960a
Avi Drissman <avi@chromium.org>
2026-05-01 12:45:40 -0700

    Remove some unsafe code from clipboard_mac
    Bug: 40285824
    Link: https://chromium-review.googlesource.com/id/Ib96b3b90ed14f4c27aaa6f8881319c946a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810454
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624016}

--- ui/base/clipboard/clipboard_mac.mm
+++ ui/base/clipboard/clipboard_mac.mm
@@ -3,11 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/callback_list.h"
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
+#include "ui/base/clipboard/clipboard_mac.h"
 
 #import <Cocoa/Cocoa.h>
 #include <stdint.h>
@@ -18,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/apple/foundation_util.h"
 #include "base/apple/scoped_cftyperef.h"
+#include "base/callback_list.h"
 #include "base/containers/span.h"
+#include "base/containers/to_vector.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
@@ -40,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/clipboard/clipboard_constants.h"
 #include "ui/base/clipboard/clipboard_format_type.h"
-#include "ui/base/clipboard/clipboard_mac.h"
 #include "ui/base/clipboard/clipboard_metrics.h"
 #include "ui/base/clipboard/clipboard_monitor.h"
 #include "ui/base/clipboard/clipboard_util_mac.h"
@@ -112,8 +109,7 @@ std::vector<uint8_t> EncodeGfxImageToPng(gfx::Image image) {
   }
 
   scoped_refptr<base::RefCountedMemory> mem = image.As1xPNGBytes();
-  std::vector<uint8_t> image_data(mem->data(), mem->data() + mem->size());
-  return image_data;
+  return base::ToVector(*mem);
 }
 
 }  // namespace
