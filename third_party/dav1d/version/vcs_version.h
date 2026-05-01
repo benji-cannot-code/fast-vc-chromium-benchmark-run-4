b818164356164951f717bb055ef7b24663b84ce0
Wan-Teh Chang <wtc@google.com>
2026-05-01 15:36:43 -0700

    Roll src/third_party/dav1d/libdav1d/ f995e1fbf..5cfc38326 (2 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/f995e1fbf937..5cfc3832687e
    
    $ git log f995e1fbf..5cfc38326 --date=short --no-merges --format='%ad %ae %s'
    2026-04-29 martin arm: mc: Optimize prep_neon for the w4/w8 cases
    2026-04-29 martin arm: mc: Fix a comment typo
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ie6e625c1ed524ff8ff20f6b17515532bca160ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809859
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624125}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-33-gf995e1fb"
+#define DAV1D_VERSION "1.5.3-35-g5cfc3832"
