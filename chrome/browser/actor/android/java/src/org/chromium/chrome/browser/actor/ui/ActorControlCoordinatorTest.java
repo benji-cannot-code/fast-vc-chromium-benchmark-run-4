8b00eaa69680aa3258adc7ca5c004cce996581e7
Linda Wang <lindaywang@google.com>
2026-05-01 10:42:49 -0700

    [PeekView] Modifies the behavior of the "View" button.
    Pressing the "View" button will now also take the user to the tab that
    was being acted upon last.
    
    Bug: 507568604
    Change-Id: I24c99deeb18c67f3ccd105617c86a3480bdf13c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802200
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Linda Wang <lindaywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623922}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ui/ActorControlCoordinatorTest.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ui/ActorControlCoordinatorTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.actor.ui;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -45,6 +46,9 @@ import org.chromium.chrome.browser.tab_bottom_sheet.TabBottomSheetManager;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.widget.ChromeImageButton;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /** Tests for {@link ActorControlCoordinator}. */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
@@ -53,6 +57,7 @@ public class ActorControlCoordinatorTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     private static final int TASK_ID = 123;
+    private static final int TAB_ID = 456;
     private static final String CONVERSATION_ID_1 = "conversation_1";
     private static final String CONVERSATION_ID_2 = "conversation_2";
     private static final String TASK_TITLE = "Test Task Title";
@@ -66,6 +71,7 @@ public class ActorControlCoordinatorTest {
     @Mock private GlicInstanceHelper.Natives mGlicInstanceHelperNatives;
     @Mock private Tab mTab;
     @Mock private ActorTask mActorTask;
+    @Mock private ActorControlCoordinator.TabSelectionDelegate mTabSelectionDelegate;
 
     private Activity mActivity;
     private ActorControlCoordinator mCoordinator;
@@ -88,7 +94,11 @@ public class ActorControlCoordinatorTest {
 
         mCoordinator =
                 new ActorControlCoordinator(
-                        mActivity, mTabBottomSheetManager, mProfileSupplier, mTabSupplier);
+                        mActivity,
+                        mTabBottomSheetManager,
+                        mProfileSupplier,
+                        mTabSupplier,
+                        mTabSelectionDelegate);
 
         mModel = mCoordinator.getModelForTesting();
         mMediator = mCoordinator.getMediatorForTesting();
@@ -513,6 +523,43 @@ public class ActorControlCoordinatorTest {
         assertEquals(CONVERSATION_TITLE_1, mModel.get(ActorControlProperties.TASK_TITLE));
         assertEquals(
                 PeekViewUiState.DEFAULT, mModel.get(ActorControlProperties.PEEK_VIEW_UI_STATE));
+    }
+
+    @Test
+    public void testOnActorControlClick_noActiveTask_waitingState_triggersCallback() {
+        setUpProfileSupplier();
+        expectValidActorTask();
+
+        Set<Integer> tabIds = new HashSet<>();
+        tabIds.add(TAB_ID);
+        when(mActorTask.getLastActedTabs()).thenReturn(tabIds);
+
+        mCoordinator.onTaskStateChanged(TASK_ID, ActorTaskState.ACTING);
+
+        when(mActorKeyedService.getCurrentActiveTask()).thenReturn(null);
+        mCoordinator.onTaskStateChanged(TASK_ID, ActorTaskState.FINISHED);
+
+        assertEquals(
+                PeekViewUiState.WAITING, mModel.get(ActorControlProperties.PEEK_VIEW_UI_STATE));
+        performActorControlClick();
+
+        verify(mTabSelectionDelegate).switchToTab(TAB_ID);
+    }
+
+    @Test
+    public void testOnActorControlClick_noTabs_doesNotTriggerCallback() {
+        setUpProfileSupplier();
+        expectValidActorTask();
+
+        when(mActorTask.getLastActedTabs()).thenReturn(new HashSet<>());
+        when(mActorTask.getTabs()).thenReturn(new HashSet<>());
+        mCoordinator.onTaskStateChanged(TASK_ID, ActorTaskState.ACTING);
+
+        when(mActorKeyedService.getCurrentActiveTask()).thenReturn(null);
+        mCoordinator.onTaskStateChanged(TASK_ID, ActorTaskState.FINISHED);
+        performActorControlClick();
+
+        verify(mTabSelectionDelegate, never()).switchToTab(anyInt());
     }
 
     @Test
