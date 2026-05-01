a1aba93a459be94c209957836a15f966c959968a
Scott Haseley <shaseley@google.com>
2026-05-01 15:34:26 -0700

    Revert "Implement the edit prompt implementation for cues 3-dot menu"
    This reverts commit eb7d320468bb3ed36c9bd4e8fbe59f21472454b2.
    
    Reason for revert: The unit test added in this CL is causing compile
    errors, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Mac%20Builder%20(dbg)/334693/overview
    
    Original change's description:
    > Implement the edit prompt implementation for cues 3-dot menu
    >
    > Bug: 507138035
    > Change-Id: I09672ef513e8e7fe8d40227b5e42b66b5d5d5e01
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808669
    > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Ian Wells <iwells@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1624115}
    
    Bug: 507138035
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I04ab05e3299f8364aa077c512f4a0484cc1602f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810136
    Auto-Submit: Scott Haseley <shaseley@google.com>
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Owners-Override: Scott Haseley <shaseley@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1624121}

--- chrome/browser/contextual_cueing/test_cue_target.h
+++ chrome/browser/contextual_cueing/test_cue_target.h
@@ -14,7 +14,6 @@ class TestCueTarget : public CueTarget {
  public:
   bool eligible = true;
   CueActionData click_data = std::monostate();
-  CueActionData edit_prompt_data = std::monostate();
 
   TestCueTarget();
   ~TestCueTarget() override;
@@ -24,7 +23,6 @@ class TestCueTarget : public CueTarget {
   // CueTarget:
   bool IsEligible() const override;
   void OnClick(CueActionData data) override;
-  void OnEditPrompt(CueActionData data) override;
   ui::ImageModel GetAnchoredMessageIcon() const override;
   ui::ImageModel GetOmniboxChipIcon() const override;
   CueActionData CueActionDataFromResponse(
