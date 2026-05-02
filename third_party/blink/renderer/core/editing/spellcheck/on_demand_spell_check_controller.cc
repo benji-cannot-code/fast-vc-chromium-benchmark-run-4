88c986b93f439dc88af7a3a13daabaccedb1a92a
Chuong Ho <hdchuong@google.com>
2026-05-02 03:19:51 -0700

    spellcheck: respect element and global settings
    On-demand spell check requests should respect element and global
    settings.
    
    Bug: 497976316
    Change-Id: Iff4a0a43a6e728c4d0d6f4bc66dba7eb9b5f0f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806945
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624284}

--- third_party/blink/renderer/core/editing/spellcheck/on_demand_spell_check_controller.cc
+++ third_party/blink/renderer/core/editing/spellcheck/on_demand_spell_check_controller.cc
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/iterators/text_iterator.h"
 #include "third_party/blink/renderer/core/editing/position_with_affinity.h"
 #include "third_party/blink/renderer/core/editing/spellcheck/spell_check_requester.h"
+#include "third_party/blink/renderer/core/editing/spellcheck/spell_checker.h"
 #include "third_party/blink/renderer/core/editing/visible_units.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
@@ -55,6 +57,12 @@ void OnDemandSpellCheckController::ElementRemoved(const Element& element) {
 
 void OnDemandSpellCheckController::RequestFullChecking(
     const ContainerNode* container_node) {
+  if (!IsSpellCheckingEnabled() ||
+      !SpellChecker::IsSpellCheckingEnabledAt(Position(container_node, 0))) {
+    ClearProgress();
+    return;
+  }
+
   // Some IMEs' functionalities (e.g. Gboard's Add to Personal Dictionary) rely
   // on PerformFullContentSpellCheck to perform a full-content spell check. In
   // such case, the spell check request cache could have become stale by the
@@ -103,6 +111,13 @@ void OnDemandSpellCheckController::ClearProgress() {
 
 void OnDemandSpellCheckController::ContextDestroyed() {
   ClearProgress();
+}
+
+bool OnDemandSpellCheckController::IsSpellCheckingEnabled() const {
+  if (!GetExecutionContext()) {
+    return false;
+  }
+  return window_->GetSpellChecker().IsSpellCheckingEnabled();
 }
 
 void OnDemandSpellCheckController::RequestCheckingForRemainingRange() {
