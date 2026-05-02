e288c3f53a98a18b7810af325e690b1cfc596329
Dominic Austria <dominicaustria@google.com>
2026-05-02 00:59:54 -0700

    [Glow Up] Fade to the glass background
    When in vertical tab strip with glass frame enabled, we fade to the
    opaque background on collapse, and fade back to the glass background in
    uncollapsed mode. This is to play nicely with expand on hover.
    
    Screencast: https://screencast.googleplex.com/cast/NjQzMTk1OTYwNDUyNzEwNHw3YjFiMTM2MC03NA
    
    Bug: 505037067
    Change-Id: I437bd3621772d439befd5ec1e1ca8d934aed4610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790084
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1624266}

--- chrome/browser/ui/views/frame/custom_corners_background.cc
+++ chrome/browser/ui/views/frame/custom_corners_background.cc
@@ -125,6 +125,10 @@ void CustomCornersBackground::Paint(gfx::Canvas* canvas,
     return;
   }
 
+  if (alpha_ < 1.0f) {
+    canvas->SaveLayerAlpha(static_cast<uint8_t>(255 * alpha_));
+  }
+
   if (view->layer()) {
     CHECK(!view->layer()->fills_bounds_opaquely());
   }
@@ -288,6 +292,10 @@ void CustomCornersBackground::Paint(gfx::Canvas* canvas,
     if (!stroke_path.isEmpty()) {
       canvas->DrawPath(stroke_path.detach(), stroke_flags);
     }
+  }
+
+  if (alpha_ < 1.0f) {
+    canvas->Restore();
   }
 }
 
