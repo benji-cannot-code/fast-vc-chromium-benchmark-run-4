ff61f84c1347b9dbb7b119a005901a251c66bba0
Dan Murphy <dmurph@chromium.org>
2026-05-01 17:24:40 -0700

    [PWA] Robust WebContents manifest waiting in tests
    Replaced flaky ad-hoc heuristics for waiting for page loads and
    manifests in browser tests with a robust WebContentsWaiter utility.
    
    This utility uses a new event created on the WebAppTabHelper designed
    to signify if all web app system activity is scheduled for a given
    WebContents page.
    
    Test: FetchManifestAndUpdateCommandMigrationTest.*
    Test: FetchManifestAndUpdateCommandTest.*
    Test: NavigationCapturingBrowserNavigatorBrowserTest.*
    Test: WebAppBlockedMigrationInfoBarDelegateBrowserTest.*
    Test: SingleClientWebAppsSyncGeneratedIconFixSyncTest.*
    Test: WebAppIntegration.WAI_InstallMenuStandalone_SwitchProfileClientsClient2_InstallLocallyStandalone_LaunchFromPlatformShortcutStandalone/kSyncTheFeature
    Change-Id: Iaed859d4aa8e2a744bcc18ebc3a99592a69f448b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782642
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624185}

--- chrome/browser/sync/test/integration/web_apps/single_client_web_apps_sync_generated_icon_fix_sync_test.cc
+++ chrome/browser/sync/test/integration/web_apps/single_client_web_apps_sync_generated_icon_fix_sync_test.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/generated_icon_fix_util.h"
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/test/web_app_page_waiter.h"
 #include "chrome/browser/web_applications/test/web_app_test_observers.h"
 #include "chrome/browser/web_applications/web_app_command_scheduler.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
@@ -111,8 +112,10 @@ class SingleClientWebAppsSyncGeneratedIconFixSyncTest
     Browser* app_browser =
         LaunchWebAppBrowserAndWait(GetProfile(/*index=*/0), app_id);
     CHECK(app_browser);
-    test::WaitForLoadCompleteAndMaybeManifestSeen(
-        *app_browser->tab_strip_model()->GetActiveWebContents());
+    EXPECT_TRUE(test::WebAppPageWaiter(
+                    app_browser->tab_strip_model()->GetActiveWebContents())
+                    .ExpectManifest()
+                    .WaitAndFlushCommands());
     provider(0).command_manager().AwaitAllCommandsCompleteForTesting();
   }
 
