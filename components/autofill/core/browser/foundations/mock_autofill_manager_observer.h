86c4ee212727c29541005d36731de3d9184e7351
Christoph Schwering <schwering@google.com>
2026-05-03 08:39:06 -0700

    [Autofill] Make BAM::DidShowSuggestions() non-virtual
    Bug: 40100455
    Change-Id: I2f6fbae740156045300a96bd8ae48f678eb4821f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807631
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1624430}

--- components/autofill/core/browser/foundations/mock_autofill_manager_observer.h
+++ components/autofill/core/browser/foundations/mock_autofill_manager_observer.h
@@ -137,6 +137,10 @@ class MockAutofillManagerObserver : public AutofillManager::Observer {
               (override));
 
   MOCK_METHOD(void,
+              OnSuggestionsShown,
+              (AutofillManager&, base::span<const Suggestion>),
+              (override));
+  MOCK_METHOD(void,
               OnSuggestionsHidden,
               (AutofillManager&, SuggestionHidingReason),
               (override));
