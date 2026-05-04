202661b9166d0a7fe0827a4c7d3d28a3306912b2
James Cook <jamescook@chromium.org>
2026-05-04 12:07:16 -0700

    extensions: Add static_assert to ExtensionFrameHostBrowserTest
    I forgot to assert ENABLE_EXTENSIONS_CORE when I ported it to desktop
    Android.
    
    Bug: none
    Change-Id: Idcfdd58a3da971c1dd3b0c074096e887a7e373ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814252
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624856}

--- chrome/browser/extensions/extension_frame_host_browsertest.cc
+++ chrome/browser/extensions/extension_frame_host_browsertest.cc
@@ -23,11 +23,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_web_contents_observer.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/test_extensions_browser_client.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/test/result_catcher.h"
 #include "testing/gtest/include/gtest/gtest-spi.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
