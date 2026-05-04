0b28d9b35b9d13f99a8a2c4f6f43018c308f3de9
Nicola Tommasi <tommasin@chromium.org>
2026-05-04 02:54:00 -0700

    [gic] Cleanup TrustFirstOnboarding Toast IPH
    As we ended up not launching it, this CL cleanups
    GlicTrustFirstOnboardingShortcutToastPromoFeature and its related code.
    
    Bug: b:503811335
    Change-Id: I799eb9d1ae7c69bfd54ea190aa0a72b9aed71930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797023
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624547}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -262,7 +262,6 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHFocusHelpBubbleScreenReaderPromoFeature,
     &kIPHGlicPromoFeature,
     &kIPHGlicTrustFirstOnboardingShortcutSnoozePromoFeature,
-    &kIPHGlicTrustFirstOnboardingShortcutToastPromoFeature,
     &kIPHGlicTryItFeature,
     &kIPHGMCCastStartStopFeature,
     &kIPHGMCLocalMediaCastingFeature,
