ae58ab5c2941567c3e592978a4e4434907e1c61e
Jeremy Roman <jbroman@chromium.org>
2026-05-04 09:11:23 -0700

    Reland: Convert Indigo toolbar from a widget to a subview of the ContentsContainerView
    This makes it less dependent on the structure of the widget hierarchy
    which seems to be different on macOS. Since this view is currently
    intended to be fully clipped by the contents anyway, we should be able
    to get by without a native widget.
    
    Instead, it exposes the desired offset at which it wants to be rendered
    (though the details of this may change in the future), adds the toolbar
    view a child of the ContentsContainerView, and tracks it. One
    consequence of this design is that there can at present only be one
    Indigo toolbar per ContentsContainerView.
    
    This proved a bit finicky to test as view layout is somewhat lazier than
    widget positioning, but seems to work well.
    
    Bug: b:493707092,b:504741803
    Change-Id: Icd31444b15fbcf5bc939ced313c64194b1951ad2
    Cq-Include-Trybots: luci.chromium.try:mac12-tests,mac13-tests,mac14-tests
    Include-Ci-Only-Tests: chromium.mac:Mac12 Tests|browser_tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805865
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624731}

--- chrome/browser/indigo/indigo_page_action_controller.cc
+++ chrome/browser/indigo/indigo_page_action_controller.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/page_actions/page_action_controller.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/grit/branded_strings.h"
 #include "components/optimization_guide/core/hints/optimization_guide_decider.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/views/view.h"
 
 namespace indigo {
 
@@ -128,7 +130,13 @@ void IndigoPageActionController::InvokeAction() {
     if (!toolbar_) {
       toolbar_ = std::make_unique<IndigoToolbar>(this);
     }
-    toolbar_->Show(web_contents->GetNativeView());
+    auto* browser_view = BrowserView::GetBrowserViewForBrowser(
+        tab().GetBrowserWindowInterface());
+    if (browser_view) {
+      auto* contents_container =
+          browser_view->GetContentsContainerViewFor(tab().GetContents());
+      toolbar_->Show(contents_container->indigo_overlay_view());
+    }
     return;
   }
 
@@ -142,7 +150,17 @@ void IndigoPageActionController::ShowToolbarInside(const gfx::Rect& rect) {
 
   if (!toolbar_) {
     toolbar_ = std::make_unique<IndigoToolbar>(this);
-    toolbar_->ShowInside(web_contents->GetNativeView(), rect);
+  }
+  auto* browser_view =
+      BrowserView::GetBrowserViewForBrowser(tab().GetBrowserWindowInterface());
+  if (browser_view) {
+    auto* contents_container =
+        browser_view->GetContentsContainerViewFor(tab().GetContents());
+    auto* contents_webview = contents_container->contents_view();
+    gfx::Rect container_rect = views::View::ConvertRectToTarget(
+        contents_webview, contents_container, rect);
+    toolbar_->ShowInside(contents_container->indigo_overlay_view(),
+                         container_rect);
   }
 }
 
