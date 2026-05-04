e43f1fe717e68b850371befe146a8d1642c7b330
Edgar Aguilar <eledgar@google.com>
2026-05-04 09:38:37 -0700

    [Fuzzing] Fix ICU deps and initialization.
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how the JVM initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes.
    
    Updating fuzzers in services/device/usb
    
    This CL was uploaded by git cl split.
    
    R=titouan@chromium.org
    
    Bug: 500525162
    Change-Id: Ibb624d60ddf68a3743ea95b7bd76ed7ddaa80180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809426
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624754}

--- services/device/usb/webusb_descriptors_fuzzer.cc
+++ services/device/usb/webusb_descriptors_fuzzer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/functional/bind.h"
 #include "base/i18n/icu_util.h"
+#include "base/no_destructor.h"
 #include "services/device/usb/fake_usb_device_handle.h"
 #include "services/device/usb/webusb_descriptors.h"
 
@@ -19,13 +20,12 @@ struct TestCase {
   base::AtExitManager at_exit_manager;
 };
 
-TestCase* test_case = new TestCase();
-
 namespace device {
 
 void Done(const GURL& landing_page) {}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static const base::NoDestructor<TestCase> test_case;
   scoped_refptr<UsbDeviceHandle> device_handle =
       new FakeUsbDeviceHandle(data, size);
   ReadWebUsbDescriptors(device_handle, base::BindOnce(&Done));
