1269df54b74089c1a1f604d692a72be3fe011c06
Justin Donnelly <jdonnelly@chromium.org>
2026-05-04 11:18:06 -0700

    [omnibox] Parameterize the DocumentProvider debounce delay.
    Replace the hardcoded 300ms debounce delay for DocumentProvider requests
    with a base::FeatureParam. This enables running experiments to test the
    effect of longer debounce periods on overall request volume. The
    parameter is cached in omnibox_feature_configs to prevent repeating
    Finch parameter lookups on every keystroke.
    
    Bug: b:469963098
    Test: components_unittests --gtest_filter=DocumentProviderTest.*
    Change-Id: Ia5e09bed3597e3528ce8db5a06e47ee25ed3a13f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812797
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624830}

--- components/omnibox/common/omnibox_feature_configs.cc
+++ components/omnibox/common/omnibox_feature_configs.cc
@@ -380,6 +380,10 @@ DocumentProvider::DocumentProvider() {
       base::FeatureParam<int>(&omnibox::kDocumentProvider,
                               "DocumentProviderMinQueryLength", 4)
           .Get();
+  debounce_delay_ms =
+      base::FeatureParam<int>(&omnibox::kDocumentProvider,
+                              "DocumentProviderDebounceDelayMs", 300)
+          .Get();
   scope_backoff_to_profile =
       base::FeatureParam<bool>(&omnibox::kDocumentProvider,
                                "DocumentProviderScopeBackoffToProfile", false)
