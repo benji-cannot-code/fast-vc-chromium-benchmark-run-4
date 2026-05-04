7c6fdb96f4b3d41c90c2f900728eca233ec5c5da
Madhav Pruthi <madhavpruthi@google.com>
2026-05-04 13:51:48 -0700

    [Clank IA] Add SiteControlsIphController to notify users of site controls menu migration
    Change-Id: Ie04ddcca6434910a56093d7e8dc2dc62ea88ddce
    Bug: 508636622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809499
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624913}

--- components/feature_engagement/public/feature_list.h
+++ components/feature_engagement/public/feature_list.h
@@ -181,6 +181,7 @@ DEFINE_VARIATION_PARAM(kIPHShoppingListMenuItemFeature,
                        "IPH_ShoppingListMenuItem");
 DEFINE_VARIATION_PARAM(kIPHShoppingListSaveFlowFeature,
                        "IPH_ShoppingListSaveFlow");
+DEFINE_VARIATION_PARAM(kIPHSiteControlsFeature, "IPH_SiteControls");
 DEFINE_VARIATION_PARAM(kIPHTabGroupCreationDialogSyncTextFeature,
                        "IPH_TabGroupCreationDialogSyncText");
 DEFINE_VARIATION_PARAM(kIPHTabGroupsDragAndDropFeature,
@@ -680,6 +681,7 @@ inline constexpr flags_ui::FeatureEntry::FeatureVariation
         VARIATION_ENTRY(kIPHSharingHubWebnotesStylizeFeature),
         VARIATION_ENTRY(kIPHShoppingListMenuItemFeature),
         VARIATION_ENTRY(kIPHShoppingListSaveFlowFeature),
+        VARIATION_ENTRY(kIPHSiteControlsFeature),
         VARIATION_ENTRY(kIPHTabGroupCreationDialogSyncTextFeature),
         VARIATION_ENTRY(kIPHTabGroupsDragAndDropFeature),
         VARIATION_ENTRY(kIPHTabGroupShareNoticeFeature),
