913069ef7f90e32071294b624591dd1cbc5b1c06
Bryant Chandler <bryantchandler@google.com>
2026-05-04 08:52:01 -0700

    [glic] Avoid using GlicInstance::host() in tests
    This change updates several Glic tests to avoid accessing the Host
    object via the public GlicInstance::host() method, preparing for
    revmoing it from the public interface.
    
    - In glic_metrics_browsertest.cc, use GlicInstanceTracker to get the
      Host directly.
    - In glic_profile_manager_interactive_uitest.cc, use
      GlicInstance::GetPanelSize() instead of accessing the host's
      webui_contents().
    - In render_view_context_menu_interactive_uitest.cc, use
      GetGlicInstanceImpl() and check IsWebClientConnected().
    
    Bug: b:489122337
    Change-Id: I1414bb98fe6a5532eed8a522018c97c06a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809396
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624709}

--- chrome/browser/renderer_context_menu/render_view_context_menu_interactive_uitest.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_interactive_uitest.cc
@@ -170,9 +170,8 @@ IN_PROC_BROWSER_TEST_F(ContextMenuUiTest,
   std::unique_ptr<content::WebContentsViewDelegate> view_delegate =
       CreateWebContentsViewDelegate(web_contents);
   view_delegate->ShowContextMenu(*subframe, params);
-  ASSERT_TRUE(base::test::RunUntil([&]() {
-    return view_delegate->IsContextMenuShowingForTesting();
-  }));
+  ASSERT_TRUE(base::test::RunUntil(
+      [&]() { return view_delegate->IsContextMenuShowingForTesting(); }));
 
   // Simulate using the context menu to "Open link in new tab".
   content::WebContents* new_web_contents = nullptr;
@@ -502,10 +501,8 @@ class GlicInteractiveContextMenuTestBase
     return Steps(
         PollUntil(
             [this]() {
-              if (glic::GlicInstance* instance =
-                      glic_service()->GetInstanceForActiveTab(browser())) {
-                return instance->host().GetPrimaryWebUiState() ==
-                       glic::mojom::WebUiState::kReady;
+              if (auto* instance = GetGlicInstanceImpl()) {
+                return instance->host().IsWebClientConnected();
               }
               return false;
             },
@@ -527,10 +524,8 @@ class GlicInteractiveContextMenuTestBase
         // multi- instance, so we will poll.
         PollUntil(
             [this]() {
-              if (glic::GlicInstance* instance =
-                      glic_service()->GetInstanceForActiveTab(browser())) {
-                return instance->host().GetPrimaryWebUiState() ==
-                       glic::mojom::WebUiState::kReady;
+              if (auto* instance = GetGlicInstanceImpl()) {
+                return instance->host().IsWebClientConnected();
               }
               return false;
             },
