1c3109c9cec11b03cb9f196878fca4fa3832dfa5
Christopher Cameron <ccameron@chromium.org>
2026-05-04 14:08:38 -0700

    Enable kMP4TimedMetadataTrack by default
    The spec is finalized and we have tests with real streams, so this is
    ready to enable.
    
    This path only supports AGTM metadata, which is disabled by the kAgtm
    flag, so this change will have no functional effect.
    
    Bug: 395659818
    Change-Id: I058bcad7b329e5b7c85f639401042190d87db49b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810462
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624924}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -1709,7 +1709,7 @@ bool IsChromeWideEchoCancellationEnabled() {
 #endif
 }
 
-BASE_FEATURE(kMP4TimedMetadataTrack, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kMP4TimedMetadataTrack, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kWebRtcAudioNeuralResidualEchoEstimation,
              base::FEATURE_DISABLED_BY_DEFAULT);
