ff89b8edc17c7704bfceb6331053575c299c8a0e
Rob Pitkin <robpitkin@google.com>
2026-05-04 16:48:53 -0700

    Fix flakiness in GamepadServiceTest and re-enable test
    GamepadServiceTest.ConnectAndDisconnectWhileInactiveTest was flaky
    because the test environment was not resetting properly between tests,
    causing state leakage.
    
    This CL adds explicit cleanup at the end of the test by calling
    `SetPadsConnected(0)` and waiting for the polling thread to settle,
    ensuring gamepads are disconnected before test exit.
    
    This CL also re-enables the test.
    
    Bug: 40834016
    Change-Id: Ie21242897a6dc53b55982f6848cd54d113e8bd4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814163
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Rob Pitkin <robpitkin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625019}

--- device/gamepad/gamepad_service_unittest.cc
+++ device/gamepad/gamepad_service_unittest.cc
@@ -339,8 +339,7 @@ TEST_F(GamepadServiceTest, ConnectWhileInactiveTest) {
   }
 }
 
-// https://crbug.com/1405460: Flaky on Android.
-TEST_F(GamepadServiceTest, DISABLED_ConnectAndDisconnectWhileInactiveTest) {
+TEST_F(GamepadServiceTest, ConnectAndDisconnectWhileInactiveTest) {
   // Create two active consumers.
   auto* consumer1 = CreateConsumer();
   auto* consumer2 = CreateConsumer();
@@ -375,6 +374,7 @@ TEST_F(GamepadServiceTest, DISABLED_ConnectAndDisconnectWhileInactiveTest) {
     SetPadsConnected(/*connected_count=*/0);
     loop.Run();
   }
+  WaitForData();
 
   // Mark the second consumer inactive.
   EXPECT_TRUE(service()->ConsumerBecameInactive(consumer2));
@@ -406,6 +406,10 @@ TEST_F(GamepadServiceTest, DISABLED_ConnectAndDisconnectWhileInactiveTest) {
   // because the connected gamepads were disconnected while the consumer was
   // still inactive.
   EXPECT_TRUE(service()->ConsumerBecameActive(consumer2));
+  WaitForData();
+
+  // Cleanup: disconnect gamepads before exiting.
+  SetPadsConnected(0);
   WaitForData();
 }
 
