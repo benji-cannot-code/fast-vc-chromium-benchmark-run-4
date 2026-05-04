d5cf481587858ab99579240b83037dc35e520aab
Aldo Culquicondor <acondor@chromium.org>
2026-05-04 11:34:24 -0700

    Use embedder's visibility and focus for priority when Webview in WebUI
    This replicates https://crrev.com/c/7748246 for desktop builds.
    
    The only difference is that it makes sure that the webview is embedded
    in a WebUI in order to inherit its visibility and focus.
    
    Change-Id: Ie133633ff9a8720795276c818f8a45b2505704cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804628
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624840}

--- chrome/browser/performance_manager/policies/process_rank_policy_android.h
+++ chrome/browser/performance_manager/policies/process_rank_policy_android.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_manager::policies {
 
-#if BUILDFLAG(ENABLE_GUEST_VIEW) && !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
 class WebViewUpdater;
 #endif
 
@@ -80,6 +80,8 @@ class ProcessRankPolicyAndroid
       std::optional<blink::mojom::PermissionStatus> previous_status) override;
   void OnHadFormInteractionChanged(const PageNode* page_node) override;
   void OnHadUserEditsChanged(const PageNode* page_node) override;
+  void OnEmbedderFrameNodeChanged(const PageNode* page_node,
+                                  const FrameNode* previous_embedder) override;
   // TODO(crbug.com/410444953):
   // `DiscardEligibilityPolicy::IsPageOptedOutOfDiscarding()` depends on
   // `PageNode::GetMainFrameUrl()` and
@@ -110,7 +112,7 @@ class ProcessRankPolicyAndroid
       const PageNode* page_node) override;
 
  private:
-#if BUILDFLAG(ENABLE_GUEST_VIEW) && !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
   friend class WebViewUpdater;
 #endif
 
