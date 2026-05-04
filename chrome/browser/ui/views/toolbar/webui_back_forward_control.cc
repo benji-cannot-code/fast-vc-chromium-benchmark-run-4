ef14fe7ecfaf5dd09986943605185ef0c5ceb764
Paul Jensen <pauljensen@chromium.org>
2026-05-04 16:42:41 -0700

    Avoid WebUIToolbarWebView friending sub-controls
    The friend relationship was dangerous as it allowed for unrestricted
    access. This change transitions it to a safer and more controlled
    delegate interface. This is a non-functional refactoring.
    
    Change-Id: Ic4665636183752de71f1e44e1971f61ad09e84f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807372
    Reviewed-by: Youseff Bourouphel <ybourouphael@google.com>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625017}

--- chrome/browser/ui/views/toolbar/webui_back_forward_control.cc
+++ chrome/browser/ui/views/toolbar/webui_back_forward_control.cc
@@ -16,15 +16,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 
 WebUIBackForwardControl::WebUIBackForwardControl(
-    WebUIToolbarWebView* webui_toolbar_web_view,
+    WebUIToolbarControlDelegate* delegate,
     BackForwardButton::Direction direction)
-    : webui_toolbar_web_view_(webui_toolbar_web_view),
+    : delegate_(delegate),
       direction_(direction),
-      menu_model_(
-          webui_toolbar_web_view->browser_->GetBrowserForMigrationOnly(),
-          direction == BackForwardButton::Direction::kBack
-              ? BackForwardMenuModel::ModelType::kBackward
-              : BackForwardMenuModel::ModelType::kForward) {}
+      menu_model_(delegate->GetBrowser()->GetBrowserForMigrationOnly(),
+                  direction == BackForwardButton::Direction::kBack
+                      ? BackForwardMenuModel::ModelType::kBackward
+                      : BackForwardMenuModel::ModelType::kForward) {}
 
 WebUIBackForwardControl::~WebUIBackForwardControl() = default;
 
@@ -37,19 +36,19 @@ void WebUIBackForwardControl::HandleContextMenu(
   menu_runner_.reset();
 
   menu_model_adapter_ = std::make_unique<views::MenuModelAdapter>(
-      &menu_model_,
-      base::BindRepeating(&WebUIToolbarWebView::OnBackForwardStateChanged,
-                          base::Unretained(webui_toolbar_web_view_)));
+      &menu_model_, base::BindRepeating(
+                        &WebUIToolbarControlDelegate::OnBackForwardStateChanged,
+                        base::Unretained(delegate_)));
   std::unique_ptr<views::MenuItemView> root = menu_model_adapter_->CreateMenu();
   root->SetSubmenuId(direction_ == BackForwardButton::Direction::kBack
                          ? kToolbarBackButtonMenuElementId
                          : kToolbarForwardButtonMenuElementId);
   menu_runner_ = std::make_unique<views::MenuRunner>(
       std::move(root), views::MenuRunner::HAS_MNEMONICS);
-  menu_runner_->RunMenuAt(webui_toolbar_web_view_->GetWidget(), nullptr,
-                          screen_rect, views::MenuAnchorPosition::kTopLeft,
-                          source);
-  webui_toolbar_web_view_->OnBackForwardStateChanged();
+  menu_runner_->RunMenuAt(widget, nullptr, screen_rect,
+                          views::MenuAnchorPosition::kTopLeft, source);
+
+  delegate_->OnBackForwardStateChanged();
 }
 
 void WebUIBackForwardControl::SetEnabled(bool enabled) {
@@ -57,7 +56,7 @@ void WebUIBackForwardControl::SetEnabled(bool enabled) {
     return;
   }
   enabled_ = enabled;
-  webui_toolbar_web_view_->OnBackForwardStateChanged();
+  delegate_->OnBackForwardStateChanged();
 }
 
 void WebUIBackForwardControl::SetIsPinned(bool is_pinned) {
@@ -65,7 +64,7 @@ void WebUIBackForwardControl::SetIsPinned(bool is_pinned) {
     return;
   }
   is_pinned_ = is_pinned;
-  webui_toolbar_web_view_->OnBackForwardStateChanged();
+  delegate_->OnBackForwardStateChanged();
 }
 
 bool WebUIBackForwardControl::IsPinned() const {
