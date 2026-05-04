641448f3681076bafaab7b97f117c98327ac774f
Edgar Aguilar <eledgar@google.com>
2026-05-04 09:01:13 -0700

    [Fuzzing] Fix ICU deps and initialization.
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how the JVM initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes.
    
    Updating fuzzers in content/test/fuzzer
    
    This CL was uploaded by git cl split.
    
    R=titouan@chromium.org
    
    Bug: 500525162
    Change-Id: Id8909e32735915cd3954be019d4dea437b445db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809330
    Auto-Submit: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624723}

--- content/test/fuzzer/origin_trial_token_fuzzer.cc
+++ content/test/fuzzer/origin_trial_token_fuzzer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
+#include "base/no_destructor.h"
 #include "url/gurl.h"
 
 struct TestCase {
@@ -21,10 +22,9 @@ struct TestCase {
   base::AtExitManager at_exit_manager;
 };
 
-TestCase* test_case = new TestCase();
-
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static const base::NoDestructor<TestCase> test_case;
   std::string str = std::string(reinterpret_cast<const char*>(data), size);
   // Generates version 0,1,2,3 or 255.
   uint8_t version = (std::hash<std::string>()(str) % 5) - 1;
--- content/test/fuzzer/signed_exchange_envelope_fuzzer.cc
+++ content/test/fuzzer/signed_exchange_envelope_fuzzer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/at_exit.h"
 #include "base/containers/span.h"
 #include "base/i18n/icu_util.h"
+#include "base/no_destructor.h"
 #include "content/browser/web_package/signed_exchange_prologue.h"  // nogncheck
 #include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
@@ -24,14 +25,13 @@ struct IcuEnvironment {
   base::AtExitManager at_exit_manager;
 };
 
-IcuEnvironment* env = new IcuEnvironment();
-
 std::vector<uint8_t> CopyIntoSeparateBufferToSurfaceOutOfBoundAccess(
     base::span<const uint8_t> data) {
   return std::vector<uint8_t>(std::from_range, data);
 }
 
 DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(base::span<const uint8_t> data) {
+  static const base::NoDestructor<IcuEnvironment> env;
   if (data.size() < BeforeFallbackUrl::kEncodedSizeInBytes) {
     return 0;
   }
