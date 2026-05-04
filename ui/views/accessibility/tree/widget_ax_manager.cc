436e272ddd3e314b299d30bd5af2aee93c3614b0
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2026-05-03 22:08:40 -0700

    [views-ax] Delay WidgetAXManager enablement until widget creation
    WidgetAXManager can be initialized from Widget::InitAccessibility() before Widget::OnNativeWidgetCreated() installs the FocusManager. If AXMode is already enabled at that point, Enable() starts focus observation too early and misses subsequent focus changes.
    
    Defer full enablement until WidgetObserver::OnWidgetCreated() when the
    native widget has been created. Keep the immediate path for managers
    attached to already-initialized widgets.
    
    Bug: 40672441
    Change-Id: Idcf76f1dcb22c1d6a94976c6706c4442cff5a275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809793
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624490}

--- ui/views/accessibility/tree/widget_ax_manager.cc
+++ ui/views/accessibility/tree/widget_ax_manager.cc
@@ -117,6 +117,10 @@ WidgetAXManager::WidgetAXManager(Widget* widget)
       << "WidgetAXManager should only be created when the "
          "accessibility tree feature is enabled.";
 
+  if (widget_) {
+    widget_created_ = widget_->IsNativeWidgetInitialized();
+    widget_observation_.Observe(widget_);
+  }
   ui::AXPlatform::GetInstance().AddModeObserver(this);
 }
 
@@ -129,7 +133,7 @@ void WidgetAXManager::Init() {
   CHECK(widget_->GetRootView());
   if (ui::AXPlatform::GetInstance().GetMode().has_mode(
           ui::AXMode::kNativeAPIs)) {
-    Enable();
+    EnableWhenWidgetCreated();
   } else {
     if (widget_->is_top_level()) {
       InitAXTreeManager();
@@ -218,8 +222,22 @@ void WidgetAXManager::RemoveObserver(WidgetAXManagerObserver* observer) {
 
 void WidgetAXManager::OnAXModeAdded(ui::AXMode mode) {
   if (mode.has_mode(ui::AXMode::kNativeAPIs)) {
+    EnableWhenWidgetCreated();
+  }
+}
+
+void WidgetAXManager::OnWidgetCreated(Widget* widget) {
+  CHECK_EQ(widget_, widget);
+  widget_created_ = true;
+  if (enable_on_widget_created_) {
+    enable_on_widget_created_ = false;
     Enable();
   }
+}
+
+void WidgetAXManager::OnWidgetDestroyed(Widget* widget) {
+  DCHECK_EQ(widget_, widget);
+  widget_observation_.Reset();
 }
 
 gfx::NativeViewAccessible WidgetAXManager::GetNativeViewAccessibleForId(
@@ -465,6 +483,24 @@ void WidgetAXManager::InitAXTreeManager() {
 
   ax_tree_manager_.reset(
       ui::BrowserAccessibilityManager::Create(update, *this, this));
+}
+
+void WidgetAXManager::EnableWhenWidgetCreated() {
+  if (!widget_) {
+    return;
+  }
+
+  if (!widget_created_) {
+    // WidgetAXManager can be attached after OnWidgetCreated() has already fired.
+    widget_created_ = widget_->IsNativeWidgetInitialized();
+  }
+
+  if (widget_created_) {
+    Enable();
+    return;
+  }
+
+  enable_on_widget_created_ = true;
 }
 
 void WidgetAXManager::Enable() {
