5bd40c24d481c4928832c04493de4ae94300afb2
Zoraiz Naeem <zoraiznaeem@google.com>
2026-05-03 23:39:15 -0700

    [views] Improve ScopedPaintLock to completely defer paint scheduling
    This CL improves the behavior of `views::ScopedPaintLock` to ensure it
    reliably defers all paint scheduling within a locked view's subtree
    until the lock is released.
    
    This CL updates `View::SchedulePaintInRectImpl` to check for an active
    paint lock before calling `OnDidSchedulePaint`. This short-circuits the
    scheduling pipeline entirely for the locked subtree and prevents paint
    invalidations from unnecessarily propagating to the parent while locked.
    
    Bug: b:507930653
    Change-Id: I9a18aee11939843d0f013708f658aadef0155dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809307
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624500}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -3080,13 +3080,13 @@ void View::UnlockPaint() {
 }
 
 void View::SchedulePaintInRectImpl(const gfx::Rect& rect) {
-  OnDidSchedulePaint(rect);
-  if (!visible_) {
+  if (IsPaintLocked()) {
+    paint_pending_while_locked_ = true;
     return;
   }
 
-  if (IsPaintLocked()) {
-    paint_pending_while_locked_ = true;
+  OnDidSchedulePaint(rect);
+  if (!visible_) {
     return;
   }
 
