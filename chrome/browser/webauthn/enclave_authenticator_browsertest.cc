dff455a99087e638beaca7113dee791903f2cb92
Nina Satragno <nsatragno@chromium.org>
2026-05-04 14:56:04 -0700

    [webauthn] Remove GPM incognito interstitial
    Google Password Manager passkeys currently show an incognito
    interstitial before letting the user create a passkey. This change
    removes this interstitial, treating GPM passkey creation the same for
    incognito and non-incognito.
    
    Written with the help of Gemini.
    
    Fixed: 424448497
    Change-Id: If63ae21f0f86f65411b57e8ad4081d7cd2194648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7765328
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624952}

--- chrome/browser/webauthn/enclave_authenticator_browsertest.cc
+++ chrome/browser/webauthn/enclave_authenticator_browsertest.cc
@@ -3100,16 +3100,12 @@ IN_PROC_BROWSER_TEST_F(EnclaveAuthenticatorBrowserTest,
   delegate_observer()->WaitForUI();
 
   model_observer()->SetStepToObserve(
-      AuthenticatorRequestDialogModel::Step::kGPMConfirmOffTheRecordCreate);
+      AuthenticatorRequestDialogModel::Step::kGPMTrustThisComputerCreation);
   model_observer()->WaitForStep();
   EXPECT_EQ(request_delegate()
                 ->enclave_controller_for_testing()
                 ->account_state_for_testing(),
             GPMEnclaveController::AccountState::kRecoverable);
-  model_observer()->SetStepToObserve(
-      AuthenticatorRequestDialogModel::Step::kGPMTrustThisComputerCreation);
-  dialog_model()->OnGPMConfirmOffTheRecordCreate();
-  model_observer()->WaitForStep();
 
   model_observer()->SetStepToObserve(
       AuthenticatorRequestDialogModel::Step::kGPMRecoverSecurityDomain);
@@ -3133,11 +3129,7 @@ IN_PROC_BROWSER_TEST_F(EnclaveAuthenticatorBrowserTest,
   delegate_observer()->WaitForUI();
 
   model_observer()->SetStepToObserve(
-      AuthenticatorRequestDialogModel::Step::kGPMConfirmOffTheRecordCreate);
-  model_observer()->WaitForStep();
-  model_observer()->SetStepToObserve(
       AuthenticatorRequestDialogModel::Step::kGPMCreatePasskey);
-  dialog_model()->OnGPMConfirmOffTheRecordCreate();
   model_observer()->WaitForStep();
 
   model_observer()->SetStepToObserve(
@@ -3713,12 +3705,6 @@ IN_PROC_BROWSER_TEST_P(EnclaveAuthenticatorIncognitoBrowserTest,
   // Finally, if the user manually chooses the enclave, it should be the default
   // again. Attempting to bootstrap should be enough.
   dialog_model()->OnGPMCreationSelected();
-  if (GetParam()) {
-    EXPECT_EQ(
-        dialog_model()->step(),
-        AuthenticatorRequestDialogModel::Step::kGPMConfirmOffTheRecordCreate);
-    dialog_model()->OnGPMConfirmOffTheRecordCreate();
-  }
   EXPECT_EQ(
       dialog_model()->step(),
       AuthenticatorRequestDialogModel::Step::kGPMTrustThisComputerCreation);
