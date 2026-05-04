b5edb498245b33b998c2e5022bae85d6aada4fa1
Nicolas MacBeth <nicolasmacbeth@google.com>
2026-05-04 11:42:02 -0700

    [iOS] Fix ActorService PageContext extraction to include APC + snapshot
    Also added a small optimization to PageContextWrapper to not cleanup
    FindInPage text if none was highlighted to begin with. Simplifies tests.
    
    Fixed: 509493413
    Change-Id: I159725aa20eb12ccee1bb0920ddcfd265f75c373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7811000
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1624844}

--- ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.mm
@@ -1452,6 +1452,10 @@ result.links = linksArray;
 
 // Stop the highlighting of text.
 - (void)stopTextHighlighting {
+  if (!_textToHighlight) {
+    return;
+  }
+
   web::WebState* webState = _webState.get();
   if (!webState) {
     return;
