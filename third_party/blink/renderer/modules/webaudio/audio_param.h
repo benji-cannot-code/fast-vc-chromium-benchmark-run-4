5cd36327f4218819e9d47a1a4affa920cf4091b5
Hongchan Choi <hongchan@google.com>
2026-05-04 11:02:34 -0700

    [WebAudio] Clear summing_bus_ memory in AudioParamHandler after use
    AudioParamHandler's summing_bus_ can retain a reference to an external
    buffer that may be garbage collected before the handler itself. This
    happens because the reference is not cleared after its use in
    CalculateFinalValues.
    
    This CL fixes the issue by explicitly resetting the summing_bus_
    channel memory to nullptr at the end of CalculateFinalValues.
    
    It also exports AudioParam and AudioParamHandler for testing and
    adds both a unit test and a Web Platform Test to verify the fix.
    
    Two tests are added:
    - t/b/r/m/webaudio/audio_param_handler_test.cc,
    - t/b/web_tests/webaudio/internals/audioparam-gc.https.html
    
    Bug: 507500101
    Test: t/b/r/m/webaudio/audio_param_handler_test.cc
    Change-Id: Ib4cf5fe59a7638c46c2a7a28741728828312a5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809918
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624822}

--- third_party/blink/renderer/modules/webaudio/audio_param.h
+++ third_party/blink/renderer/modules/webaudio/audio_param.h
@@ -48,7 +48,8 @@ namespace blink {
 class V8AutomationRate;
 
 // AudioParam class represents web-exposed AudioParam interface.
-class AudioParam final : public ScriptWrappable, public InspectorHelperMixin {
+class MODULES_EXPORT AudioParam final : public ScriptWrappable,
+                                        public InspectorHelperMixin {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
