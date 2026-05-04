86099af81fe68c8cb84647a1ab7b5a73870f7e10
Hira Mahmood <hiramahmood@google.com>
2026-05-04 07:54:36 -0700

    [iOS] Support overlapping keyboard insets in FullscreenBrowserAgent
    This CL adds support for keyboard insets in FullscreenBrowserAgent by
    treating the keyboard as an overlapping element. Previously, the
    keyboard was not accounted for at in the agent, which made it impossible
    to scroll to the bottom of a webpage when the keyboard was visible. Now,
    it takes the maximum of the stacked toolbars and the keyboard height.
    
    It also adds keyboard notification handling to FullscreenMediator and
    extracts a shared frame conversion helper in `uikit_ui_util`
    (refactoring Toolbar view controllers to use it).
    
    Before:
    https://screencast.googleplex.com/cast/NDU1MjMwNTE1MTA0OTcyOHw2ODM1OWJiNi1kZQ
    After:
    https://screencast.googleplex.com/cast/NTg1MTgwMjA5OTUxNTM5MnxhNGE2MGRmZC1kZg
    
    Fixed: 501137468
    Change-Id: Idc3364b56eb7bbf66dbb5de452f7a9edbe2a8620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807727
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624668}

--- ios/chrome/browser/fullscreen/model/fullscreen_browser_agent.mm
+++ ios/chrome/browser/fullscreen/model/fullscreen_browser_agent.mm
@@ -134,6 +134,12 @@ void FullscreenBrowserAgent::NotifyObserversOfUpdatedState(
   for (auto& observer : observers_) {
     observer.WillUpdateState(this);
   }
+
+  // Apply keyboard height as overlapping.
+  if (keyboard_obscured_inset_ > 0) {
+    insets_.bottom = std::max(insets_.bottom, keyboard_obscured_inset_);
+  }
+
   updating_insets_ = false;
 
   if (!UIEdgeInsetsEqualToEdgeInsets(old_insets, insets_)) {
@@ -197,6 +203,13 @@ void FullscreenBrowserAgent::InvalidateInsetRange() {
   for (auto& observer : observers_) {
     observer.WillUpdateObscuredInsetRange(this);
   }
+
+  // Apply keyboard height as overlapping.
+  if (keyboard_obscured_inset_ > 0) {
+    min_insets_.bottom = std::max(min_insets_.bottom, keyboard_obscured_inset_);
+    max_insets_.bottom = std::max(max_insets_.bottom, keyboard_obscured_inset_);
+  }
+
   updating_obscured_insets_ = false;
 
   for (auto& observer : observers_) {
@@ -237,4 +250,12 @@ void FullscreenBrowserAgent::AddObscuredInset(UIRectEdge edge, CGFloat amount) {
   } else if (edge == UIRectEdgeRight) {
     insets_.right += amount;
   }
+}
+
+void FullscreenBrowserAgent::SetKeyboardObscuredInset(CGFloat inset) {
+  if (keyboard_obscured_inset_ == inset) {
+    return;
+  }
+  keyboard_obscured_inset_ = inset;
+  InvalidateInsetRange();
 }
--- ios/chrome/browser/toolbar/legacy/ui_bundled/secondary_toolbar_view_controller.mm
+++ ios/chrome/browser/toolbar/legacy/ui_bundled/secondary_toolbar_view_controller.mm
@@ -242,7 +242,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       visibleKeyboardHeight = [self inputAccessoryHeightInWindow];
     } else {
       visibleKeyboardHeight =
-          [self keyboardHeightInWindowFromNotification:notification];
+          VisibleKeyboardHeightFromNotification(notification, self.view.window);
       // If the Find navigator is visible and the toolbar is constrained to the
       // keyboard, then add room for the collapsed toolbar to be visible above
       // the keyboard.
@@ -273,22 +273,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [inputAccessory convertRect:inputAccessory.layer.presentationLayer.frame
                            toView:self.view.window];
   return self.view.window.frame.size.height - rectInWindowIA.origin.y;
-}
-
-// Returns the user visible height of the keyboard.
-- (CGFloat)keyboardHeightInWindowFromNotification:
-    (NSNotification*)notification {
-  NSDictionary* userInfo = notification.userInfo;
-  // Part of the keyboard might be hidden. Keep only the visible area.
-  CGRect keyboardFrame = [userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
-  id<UICoordinateSpace> fromCoordinateSpace =
-      ((UIScreen*)notification.object).coordinateSpace;
-  id<UICoordinateSpace> toCoordinateSpace = self.view.window;
-  CGRect keyboardFrameInWindow =
-      [fromCoordinateSpace convertRect:keyboardFrame
-                     toCoordinateSpace:toCoordinateSpace];
-  return CGRectIntersection(keyboardFrameInWindow, self.view.window.bounds)
-      .size.height;
 }
 
 // The minimum height of this toolbar.
