40f49fb359666a722917792522986d548d44bb3e
Kent Tamura <tkent@chromium.org>
2026-05-04 02:37:40 -0700

    Rename `CR` and `LF` in CharacterIterator methods
    Google C++ style guide prefers `Cr` and `Lf`.
    This CL has no behavior changes.
    
    Bug: 492927412
    Change-Id: I55ddb887ef97c8d6f288cba186f8bfe7e325af15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805632
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624541}

--- third_party/blink/renderer/platform/text/text_break_iterator_icu.cc
+++ third_party/blink/renderer/platform/text/text_break_iterator_icu.cc
@@ -914,7 +914,7 @@ bool CharacterBreakIterator::IsBreak(int offset) const {
   if (!is_8bit_) {
     return iterator_->isBoundary(offset);
   }
-  return !IsLFAfterCR(offset);
+  return !IsLfAfterCr(offset);
 }
 
 int CharacterBreakIterator::Preceding(int offset) const {
@@ -924,7 +924,7 @@ int CharacterBreakIterator::Preceding(int offset) const {
   if (offset <= 0) {
     return kTextBreakDone;
   }
-  if (IsLFAfterCR(offset)) {
+  if (IsLfAfterCr(offset)) {
     return offset - 2;
   }
   return offset - 1;
