77b2875098c75f0654916faf0a33cec95398a717
Peter Kvitek <kvitekp@chromium.org>
2026-05-04 15:43:17 -0700

    [cft] Ensure WidevineCdm hint file directory creation
    The Widevine CDM hint file is stored in the WidevineCdm subdirectory of
    the user data directory, which is normally created during component
    registration. However, Chrome for Testing can install components in an
    alternate directory, so this subdirectory may not exist and must be
    created explicitly.
    
    Bug: 505821564
    Change-Id: I10fa5f9d453806977357e296aee6e414c49e2e29
    Cq-Include-Trybots: luci.chromium.try:linux-rel-cft,mac-rel-cft,win-rel-cft
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7813203
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624988}

--- chrome/common/media/component_widevine_cdm_hint_file_linux.cc
+++ chrome/common/media/component_widevine_cdm_hint_file_linux.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/values.h"
 #include "base/version.h"
+#include "build/branding_buildflags.h"
 #include "chrome/common/chrome_paths.h"
 
 namespace {
@@ -69,6 +70,18 @@ bool UpdateWidevineCdmHintFile(const base::FilePath& cdm_base_path,
   base::FilePath hint_file_path;
   CHECK(base::PathService::Get(chrome::FILE_COMPONENT_WIDEVINE_CDM_HINT,
                                &hint_file_path));
+
+#if BUILDFLAG(CHROME_FOR_TESTING)
+  // Chrome for Testing may store components in a directory other than the user
+  // data directory. In this case, the WidevineCdm subdirectory will not be
+  // created in user data directory during components registration, causing the
+  // hint file write to fail.
+  if (!base::CreateDirectory(hint_file_path.DirName())) {
+    DLOG(ERROR) << "Could not create the directory for the CDM hint file: "
+                << hint_file_path.DirName();
+    return false;
+  }
+#endif
 
   base::DictValue dict;
   dict.Set(kPath, cdm_base_path.value());
