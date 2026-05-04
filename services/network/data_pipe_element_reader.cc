fa62040bc14b3efbf096eb240641e7ee97bc8054
Matt Menke <mmenke@chromium.org>
2026-05-04 09:30:31 -0700

    DataPipeElementReader: Adjust size of data requested over pipe.
    The advertised size of the data was not being taken into consideration.
    
    Also check passed in net::Error when receiving the size.
    
    Bug: 497278395
    Change-Id: I2b3578689b02ea698b213fa86fd6b9f6bafd8cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807517
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624748}

--- services/network/data_pipe_element_reader.cc
+++ services/network/data_pipe_element_reader.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/data_pipe_element_reader.h"
 
+#include <algorithm>
+
 #include "base/check_op.h"
 #include "base/containers/span.h"
 #include "base/functional/bind.h"
@@ -88,8 +90,12 @@ int DataPipeElementReader::Read(net::IOBuffer* buf,
 }
 
 void DataPipeElementReader::ReadCallback(int32_t status, uint64_t size) {
-  if (status == net::OK)
+  if (status == net::OK) {
     size_ = size;
+  } else if (status > 0 || status == net::ERR_IO_PENDING) {
+    mojo::ReportBadMessage("Only net::Errors allowed.");
+    status = net::ERR_INVALID_ARGUMENT;
+  }
   if (init_callback_)
     std::move(init_callback_).Run(status);
 }
@@ -120,12 +126,15 @@ int DataPipeElementReader::ReadInternal(net::IOBuffer* buf, int buf_length) {
   if (BytesRemaining() == 0)
     return net::OK;
 
-  size_t num_bytes = base::checked_cast<size_t>(buf_length);
+  // Don't try to read more bytes than the advertised size of the element.
+  size_t max_bytes = std::min(base::checked_cast<size_t>(buf_length),
+                              base::checked_cast<size_t>(BytesRemaining()));
+  size_t bytes_received;
   MojoResult rv = data_pipe_->ReadData(MOJO_READ_DATA_FLAG_NONE,
-                                       buf->first(num_bytes), num_bytes);
+                                       buf->first(max_bytes), bytes_received);
   if (rv == MOJO_RESULT_OK) {
-    bytes_read_ += num_bytes;
-    return base::checked_cast<int>(num_bytes);
+    bytes_read_ += bytes_received;
+    return base::checked_cast<int>(bytes_received);
   }
 
   if (rv == MOJO_RESULT_SHOULD_WAIT) {
