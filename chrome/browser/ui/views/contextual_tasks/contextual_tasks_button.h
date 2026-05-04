0a22da3915d447342bd3b6a25d43a10b9627772b
Roman Arora <romanarora@chromium.org>
2026-05-04 09:53:45 -0700

    [contextual-tasks] Button styling tweaks
    Changes:
    - Inset size depends on displayed shape variant.
    - Logo size inside button depends on the displayed shape variant.
    - Adds highlight path generator that accounts for shape variant.
    
    Bug: b:498227671
    Change-Id: I883a513852a030b3588511a1e745c28b5a8df8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807483
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624771}

--- chrome/browser/ui/views/contextual_tasks/contextual_tasks_button.h
+++ chrome/browser/ui/views/contextual_tasks/contextual_tasks_button.h
@@ -32,6 +32,7 @@ class ContextualTasksButton
   ~ContextualTasksButton() override;
 
   float GetCornerRadiusFor(ToolbarButton::Edge edge) const override;
+  bool ShouldApplyCircularBackgroundShadow() const;
   // contextual_tasks::ContextualTasksPanelController::Observer:
   void OnSurfaceStateChanged(
       contextual_tasks::ContextualTasksPanelHost::SurfaceState state,
@@ -53,7 +54,6 @@ class ContextualTasksButton
   void OnSidePanelAlignmentChanged();
   void OnShouldUpdateVisibility(bool should_show);
   void OnEligibilityChange(bool is_eligible);
-  bool ShouldApplyCircularBackgroundShadow() const;
   void MaybeUpdateVisibility();
 
   BooleanPrefMember pin_state_;
