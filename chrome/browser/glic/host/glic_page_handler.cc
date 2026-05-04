14037c63697a1a03a3632bfefa437f763459db5c
Salvador Guerrero <salg@google.com>
2026-05-04 16:50:46 -0700

    [Glic Android] implemented internal feedback uploading
    This CL adds an Android implementation to
    GlicPageHandler::SendResponseFeedback, this method is used to generate a
    feedback report that contains an actor trace. This is only used for
    debugging purposes.
    
    CL's binary size increase is caused by desktop code/protos from
    chrome/browser/feedback being added to the Android build.
    
    Binary-Size: Size increase is unavoidable (see above).
    Bug: b:504754155
    Change-Id: I5409073dfc692df7c79ab3febb7cf256dea6e1f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7813481
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625020}

--- chrome/browser/glic/host/glic_page_handler.cc
+++ chrome/browser/glic/host/glic_page_handler.cc
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/background/glic/glic_launcher_configuration.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
+#include "chrome/browser/feedback/feedback_uploader_chrome.h"
+#include "chrome/browser/feedback/feedback_uploader_factory_chrome.h"
 #include "chrome/browser/glic/actor/glic_actor_policy_checker.h"
 #include "chrome/browser/glic/common/future_browser_features.h"
 #include "chrome/browser/glic/common/glic_navigation.h"
@@ -142,8 +144,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/feedback/feedback_uploader_chrome.h"
-#include "chrome/browser/feedback/feedback_uploader_factory_chrome.h"
 #include "chrome/browser/feedback/system_logs/chrome_system_logs_fetcher.h"
 #include "chrome/browser/glic/glic_hotkey.h"
 #include "chrome/browser/glic/host/context/glic_focused_browser_manager.h"
@@ -529,8 +529,6 @@ class JournalHandler {
 
  private:
   void SendResponseFeedback(const std::string& reason) {
-// NEEDS_ANDROID_IMPL: FeedbackUploaderFactoryChrome
-#if !BUILDFLAG(IS_ANDROID)
     base::WeakPtr<feedback::FeedbackUploader> uploader =
         feedback::FeedbackUploaderFactoryChrome::GetForBrowserContext(
             actor_keyed_service_->GetProfile())
@@ -560,6 +558,11 @@ class JournalHandler {
               .email);
     }
 
+// NEEDS_ANDROID_IMPL: ChromeSystemLogsFetcher
+#if BUILDFLAG(IS_ANDROID)
+    feedback_data->CompressSystemInfo();
+    feedback_data->OnFeedbackPageDataComplete();
+#else
     system_logs::BuildChromeSystemLogsFetcher(
         actor_keyed_service_->GetProfile(), /*scrub_data=*/false)
         ->Fetch(base::BindOnce(
