e87e929a98c4f2ad0a9c7ef8dbc90a56b4a5bcb6
Egor Pasko <pasko@chromium.org>
2026-05-04 06:36:56 -0700

    net: Record UMA histogram time for SocketPosix::DoRead
    We noticed that Net.SSLClientSocketImpl.OnHandshakeIOCompleteDuration
    often takes more than 1ms to run. On the surface it is not necessarily a
    large delay because various events on the system can make it move out of
    CPU. In this case optimizing OnHandshakeIOCompleteDuration would not be
    actionable.
    
    Introduce Net.SocketPosix.DoReadDuration as a baseline for comparison.
    We assume that DoRead() meets the network thread responsiveness
    requirements. In other words, if DoRead() is slow, we blame the system
    for being slow. This is not a 100% correct way to route blame, but it
    seems like a compact and intuitive one.
    
    With this baseline, if a network thread task like
    OnHandshakeIOCompleteDuration is generally more than 2x slower than
    DoRead(), it probably justifies some effort into optimizing the
    task/function. An optimization could be moving some work to other
    threads, microoptimization or something in between.
    
    This should not be treated as a strong guidance. Optimizing some things
    is notoriously hard. At this point we are just collecting potentially
    actionable causes of bad responsiveness.
    
    Bug: 501050526, 508179342
    Change-Id: I133cb3563e7490f93792096d9bbea8cc526121cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7803260
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Charles Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624619}

--- net/socket/socket_posix.cc
+++ net/socket/socket_posix.cc
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/rand_util.h"
 #include "base/task/current_thread.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
@@ -512,6 +514,9 @@ void SocketPosix::ConnectCompleted() {
 }
 
 int SocketPosix::DoRead(IOBuffer* buf, int buf_len) {
+  SCOPED_UMA_HISTOGRAM_TIMER_MICROS_SUBSAMPLED(
+      "Net.SocketPosix.DoReadDuration",
+      base::ShouldRecordSubsampledMetric(0.001));
   int rv = HANDLE_EINTR(read(socket_fd_, buf->data(), buf_len));
   return rv >= 0 ? rv : MapSystemError(errno);
 }
