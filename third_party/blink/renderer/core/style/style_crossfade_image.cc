abd0d7375ceedf3ab43f6b8405e7243b224f0c82
Fredrik Söderquist <fs@opera.com>
2026-05-04 05:36:07 -0700

    Remove CheckShapeImageOrigin()
    This helper is essentially dead since the images for shape-outside is
    fetched as "CORS anonymous", and thus the image would be in error if it
    was not CORS same-origin. Keep the CORS same-origin check as a CHECK.
    
    This allows for clean up StyleImage::IsCorsSameOrigin() by removing the
    String argument version.
    
    Change-Id: I62f1bc076889fdb79c34ff8c4901bf86098b318f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800013
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1624591}

--- third_party/blink/renderer/core/style/style_crossfade_image.cc
+++ third_party/blink/renderer/core/style/style_crossfade_image.cc
@@ -90,9 +90,9 @@ bool StyleCrossfadeImage::ErrorOccurred() const {
   });
 }
 
-bool StyleCrossfadeImage::IsCorsSameOrigin(String& failing_url) const {
-  return std::all_of(images_.begin(), images_.end(), [&](StyleImage* image) {
-    return !image || image->IsCorsSameOrigin(failing_url);
+bool StyleCrossfadeImage::IsCorsSameOrigin() const {
+  return std::all_of(images_.begin(), images_.end(), [](StyleImage* image) {
+    return !image || image->IsCorsSameOrigin();
   });
 }
 
