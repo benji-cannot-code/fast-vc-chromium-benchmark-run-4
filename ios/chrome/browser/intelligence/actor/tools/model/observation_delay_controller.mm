dfe0d13b897fa34c65ca16920190f29b8c2e2d8d
Kirubel <kaklilu@chromium.org>
2026-05-04 16:06:24 -0700

    Make ObservationDelayController a WebStateObserver.
    The ActorEngine is updated to provide the targeted webstate
    to the ODC. A navigation to a new document will stop the controller from
    waiting, and callers can continue with the tool result.
    
    Also introduce the kWaitForLoadCompletion state, which will happens
    after the page stability check. Desktop currently routes through a FedCM
    state first, but since that's not yet available on iOS, we can go from
    kWaitForPageStability to kWaitForLoadCompletion.
    
    This follows the example set by desktop, see code reference here:
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/actor/tools/observation_delay_controller.h;l=39;drc=3892619a43c005bf72202ad8a620d63784df8a43 (note, a recent refactor has split some of this logic across chrome/ and components).
    
    Bug: b:498991756
    Link: https://chromium-review.googlesource.com/id/Iab5adc501a60dc054c4b9ffd9f79192e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810075
    Commit-Queue: Kirubel <kaklilu@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624997}

--- ios/chrome/browser/intelligence/actor/tools/model/observation_delay_controller.mm
+++ ios/chrome/browser/intelligence/actor/tools/model/observation_delay_controller.mm
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/actor/public/actor_types.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
 #import "ios/web/public/js_messaging/web_frame.h"
+#import "ios/web/public/navigation/navigation_context.h"
+#import "ios/web/public/web_state.h"
 #import "url/gurl.h"
 
 namespace actor {
@@ -31,12 +33,20 @@ ObservationDelayController::ObservationDelayController(
   }
 }
 
-ObservationDelayController::~ObservationDelayController() = default;
+ObservationDelayController::~ObservationDelayController() {
+  web_state_observation_.Reset();
+  web_state_ = nullptr;
+}
 
-void ObservationDelayController::Wait(base::WeakPtr<web::WebFrame> web_frame,
+void ObservationDelayController::Wait(base::WeakPtr<web::WebState> web_state,
+                                      base::WeakPtr<web::WebFrame> web_frame,
                                       ReadyCallback callback) {
   if (journal_) {
     journal_->Log(GURL(), task_id_, "ObservationDelay: Wait", {});
+  }
+  if (web_state) {
+    web_state_ = web_state;
+    web_state_observation_.Observe(web_state.get());
   }
   ready_callback_ = std::move(callback);
   // Schedule a kDidTimeout state transition to happen later.
@@ -44,6 +54,38 @@ void ObservationDelayController::Wait(base::WeakPtr<web::WebFrame> web_frame,
       .Run();
   // Immediately transition to start waiting for page stability.
   PostMoveToStateClosure(State::kWaitForPageStability).Run();
+}
+
+void ObservationDelayController::DidStartNavigation(
+    web::WebState* web_state,
+    web::NavigationContext* navigation_context) {
+  CHECK_EQ(web_state_.get(), web_state);
+  // This is only called for main frame navigations so we don't check it here.
+  if (!navigation_context || navigation_context->IsSameDocument()) {
+    return;
+  }
+  if (state_ == State::kInitial) {
+    return;
+  }
+  // TODO(crbug.com/498991756) - Count how many navigations occur and keep
+  // observing if its beyond a given threshold, see https://crrev.com/c/7239787.
+  MoveToState(State::kPageNavigated);
+}
+
+void ObservationDelayController::DidStopLoading(web::WebState* web_state) {
+  CHECK_EQ(web_state_.get(), web_state);
+  if (state_ != State::kWaitForLoadCompletion) {
+    return;
+  }
+  // TODO(crbug.com/498991756) - Wait for the visual state to be settled.
+  MoveToState(State::kDone);
+}
+
+void ObservationDelayController::WebStateDestroyed(web::WebState* web_state) {
+  CHECK_EQ(web_state_.get(), web_state);
+  web_state_observation_.Reset();
+  web_state_ = nullptr;
+  MoveToState(State::kDone);
 }
 
 void ObservationDelayController::MoveToState(State state) {
@@ -58,13 +100,28 @@ void ObservationDelayController::MoveToState(State state) {
   }
 
   state_ = state;
+  state_history_.push_back(state_);
+  if (state_change_testing_callback_) {
+    state_change_testing_callback_.Run(state_);
+  }
   switch (state_) {
     case State::kInitial:
       NOTREACHED();
     case State::kWaitForPageStability:
       // TODO(crbug.com/498991756): We'll introduce and use a
       // PageStabilityJavaScriptFeature in a followup. For now, go to kDone.
+      PostMoveToStateClosure(State::kWaitForLoadCompletion).Run();
+      break;
+    case State::kWaitForLoadCompletion:
+      if (web_state_ && web_state_->IsLoading()) {
+        // The state transition will happen in DidStopLoading().
+        break;
+      }
       PostMoveToStateClosure(State::kDone).Run();
+      break;
+    case State::kPageNavigated:
+      result_ = Result::kPageNavigated;
+      MoveToState(State::kDone);
       break;
     case State::kDidTimeout:
       MoveToState(State::kDone);
@@ -73,6 +130,8 @@ void ObservationDelayController::MoveToState(State state) {
       // The state machine is never entered until Wait is called so a callback
       // must be provided.
       CHECK(ready_callback_);
+      web_state_observation_.Reset();
+      web_state_ = nullptr;
       // Post a task to run the callback so that it's not tied to the lifetime
       // of this class.
       base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
@@ -97,18 +156,40 @@ base::OnceClosure ObservationDelayController::PostMoveToStateClosure(
 
 void ObservationDelayController::CheckStateTransition(State old_state,
                                                       State new_state) {
+  // Note: the transitions listed as "async transitions" below can occur at any
+  // point in the state machine and should be tolerated even when unexpected.
   static const base::NoDestructor<base::StateTransitions<State>> transitions(
       base::StateTransitions<State>({
           // clang-format off
           {State::kInitial,
-              {State::kWaitForPageStability}},
+              {State::kWaitForPageStability,
+               /* async transitions */
+               State::kDidTimeout,
+               State::kPageNavigated}},
           {State::kWaitForPageStability,
-              {State::kDidTimeout, State::kDone}},
+              {State::kWaitForLoadCompletion,
+               State::kDone,
+               /* async transitions */
+               State::kDidTimeout,
+               State::kPageNavigated}},
+          {State::kWaitForLoadCompletion,
+              {State::kDone,
+               /* async transitions */
+               State::kDidTimeout,
+               State::kPageNavigated}},
+          {State::kPageNavigated,
+              {State::kDone,
+               /* async transitions */
+               State::kDidTimeout}},
           {State::kDidTimeout,
-              {State::kDone}}
+              {State::kDone,
+               /* async transitions */
+               State::kPageNavigated}}
           // clang-format on
       }));
-  CHECK_STATE_TRANSITION(transitions, old_state, new_state);
+  CHECK((transitions)->IsTransitionValid((old_state), (new_state)),
+        base::NotFatalUntil::M160)
+      << "Invalid transition: " << old_state << " -> " << new_state;
 }
 
 std::ostream& operator<<(std::ostream& o,
@@ -122,6 +203,10 @@ std::string_view ObservationDelayController::StateToString(State state) {
       return "Initial";
     case State::kWaitForPageStability:
       return "WaitForPageStability";
+    case State::kWaitForLoadCompletion:
+      return "WaitForLoadCompletion";
+    case State::kPageNavigated:
+      return "PageNavigated";
     case State::kDidTimeout:
       return "DidTimeout";
     case State::kDone:
