b45dff04b652e93a464efebff788d7d2534a948b
Adithya Srinivasan <adithyas@chromium.org>
2026-05-05 16:14:09 -0700

    indigo: Handle speculative rfh case when binding OnboardingDialogHost
    Bug: b:491559009
    Change-Id: I714e2e4d6217f513420405d8c1331417e9c8a2f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817201
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625799}

--- chrome/browser/indigo/onboarding/indigo_onboarding_dialog_browsertest.cc
+++ chrome/browser/indigo/onboarding/indigo_onboarding_dialog_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -41,6 +42,12 @@ class IndigoOnboardingDialogBrowserTest : public InteractiveBrowserTest {
  public:
   IndigoOnboardingDialogBrowserTest() {
     feature_list_.InitAndEnableFeature(features::kIndigo);
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    InteractiveBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitchASCII(switches::kIsolateOrigins,
+                                    "http://isolated.com");
   }
 
  protected:
@@ -219,6 +226,35 @@ IN_PROC_BROWSER_TEST_F(IndigoOnboardingDialogBrowserTest,
                   Check([&]() { return WasDialogClosed(); }), Check([&]() {
                     return last_result_.acknowledge_chrome_disclaimer;
                   }));
+}
+
+// Tests that acknowledging the disclaimer works after the onboarding dialog
+// navigates to an isolated origin (i.e. a RenderFrameHost swap occurs during
+// the initial navigation).
+IN_PROC_BROWSER_TEST_F(IndigoOnboardingDialogBrowserTest, CrossRfhNavigation) {
+  GURL onboarding_url =
+      embedded_test_server()->GetURL("isolated.com", "/empty.html");
+  tabs::TabInterface* tab = browser()->GetActiveTabInterface();
+
+  RunTestSequence(
+      Do([&]() { OpenDialog(*tab, onboarding_url); }),
+      WaitForShow(IndigoOnboardingDialog::kWebViewId),
+      InstrumentNonTabWebView(kDialogWebContentsId,
+                              IndigoOnboardingDialog::kWebViewId),
+      WaitForWebContentsReady(kDialogWebContentsId, onboarding_url),
+      CheckJsResult(kDialogWebContentsId,
+                    "() => typeof window.chromeOnboarding", "object"),
+      ExecuteJs(kDialogWebContentsId,
+                R"js(
+                  () => {
+                    window.chromeOnboarding.acknowledgeChromeDisclaimer();
+                    window.close();
+                  }
+                )js",
+                ExecuteJsMode::kFireAndForget),
+      WaitForHide(IndigoOnboardingDialog::kWebViewId),
+      Check([&]() { return WasDialogClosed(); }),
+      Check([&]() { return last_result_.acknowledge_chrome_disclaimer; }));
 }
 
 }  // namespace
