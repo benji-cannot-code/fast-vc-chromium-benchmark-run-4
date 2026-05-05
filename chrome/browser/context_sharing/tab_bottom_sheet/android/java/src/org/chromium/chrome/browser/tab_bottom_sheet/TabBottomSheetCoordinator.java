8d82b0445bf98036bbf49ffd7c3d1ab68a542b36
Hitarth Kothari <hitarthkothari@google.com>
2026-05-05 07:05:51 -0700

    [TabBottomSheet] Ensure only one sheet observer per coordinator
    Bug: 469429565
    Change-Id: I34eabb69c4ca5eaa30700b00a5696eb72a3ba8c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814640
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625392}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
@@ -56,20 +56,6 @@ public class TabBottomSheetCoordinator {
         void onBottomSheetOpened(boolean isExpanded);
     }
 
-    private final ComponentCallbacks mComponentsCallbacks =
-            new ComponentCallbacks() {
-                @Override
-                public void onConfigurationChanged(Configuration configuration) {
-                    Log.i(TAG, "onConfigurationChanged: isShowing = " + mIsShowingTabBottomSheet);
-                    if (mIsShowingTabBottomSheet) {
-                        mExpectingLayoutChange = true;
-                    }
-                }
-
-                @Override
-                public void onLowMemory() {}
-            };
-
     private final GestureDetector mGestureDetector;
     private final GestureDetector.SimpleOnGestureListener mGestureListener =
             new GestureDetector.SimpleOnGestureListener() {
@@ -151,6 +137,7 @@ public class TabBottomSheetCoordinator {
     private @Nullable SheetEventsCallback mSheetEventsCallback;
     private @Nullable TabBottomSheetContent mSheetContent;
     private @Nullable BottomSheetObserver mSheetObserver;
+    private @Nullable ComponentCallbacks mComponentsCallbacks;
     private @Nullable PropertyModelChangeProcessor mViewBinder;
     private @Nullable View mContentView;
 
@@ -247,10 +234,14 @@ public class TabBottomSheetCoordinator {
                         }
                     });
 
-            mSheetObserver = buildBottomSheetObserver();
-            mBottomSheetController.addObserver(mSheetObserver);
-
-            mContext.registerComponentCallbacks(mComponentsCallbacks);
+            if (mSheetObserver == null) {
+                mSheetObserver = buildBottomSheetObserver();
+                mBottomSheetController.addObserver(mSheetObserver);
+            }
+            if (mComponentsCallbacks == null) {
+                mComponentsCallbacks = buildComponentsCallback();
+                mContext.registerComponentCallbacks(mComponentsCallbacks);
+            }
 
             mIsShowingTabBottomSheet = true;
             return true;
@@ -322,8 +313,10 @@ public class TabBottomSheetCoordinator {
             mBottomSheetController.removeObserver(mSheetObserver);
             mSheetObserver = null;
         }
-
-        mContext.unregisterComponentCallbacks(mComponentsCallbacks);
+        if (mComponentsCallbacks != null) {
+            mContext.unregisterComponentCallbacks(mComponentsCallbacks);
+            mComponentsCallbacks = null;
+        }
         stopObservingCompositorViewInteractions();
 
         if (mSheetContent != null) {
@@ -340,6 +333,21 @@ public class TabBottomSheetCoordinator {
     }
 
     // Observer methods.
+    private ComponentCallbacks buildComponentsCallback() {
+        return new ComponentCallbacks() {
+            @Override
+            public void onConfigurationChanged(Configuration configuration) {
+                Log.i(TAG, "onConfigurationChanged: isShowing = " + mIsShowingTabBottomSheet);
+                if (mIsShowingTabBottomSheet) {
+                    mExpectingLayoutChange = true;
+                }
+            }
+
+            @Override
+            public void onLowMemory() {}
+        };
+    }
+
     private BottomSheetObserver buildBottomSheetObserver() {
         return new EmptyBottomSheetObserver() {
             @Override
