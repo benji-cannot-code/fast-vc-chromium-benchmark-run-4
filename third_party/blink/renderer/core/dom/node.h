1b58b26cb5c62fb071bab2ffe929e6bdef267d36
David Baron <dbaron@chromium.org>
2026-05-04 18:34:08 -0700

    Use data rather than repeated code to store order of pseudos.
    This uses a std::array to replace repeated code to store the order of
    the pseudo-elements that are generated as children of an element.
    
    This reduces the maintenance burden when adding new pseudos, since fewer
    changes will be required.
    
    This makes the following behavior changes:
    
     * It adds kPseudoIdOverscrollAreaParent to the Node::PseudoAware*
       and LayoutTreeBuilderTraversal::{LastChild,FirstChild} methods; it
       was previously handled only by the
       LayoutTreeBuilderTraversal::{NextSibling,PreviousSibling} methods.
    
     * It adds handling of kPseudoIdViewTransition in
       LayoutTreeBuilderTraversal::LastChild and
       LayoutTreeBuilderTraversal::FirstChild, with the same special
       behavior that was in LayoutTreeBuilderTraversal::NextSibling;
       this makes the 4 LayoutTreeBuilderTraversal methods consistent.
    
     * It fixes the order in LayoutTreeBuilderTraversal::LastChild and
       FirstChild so that kPseudoIdColumn comes in the same order relative
       to other pseudos as it is in the other 6 methods (it moves relative
       to kPseudoIdScrollButton* and kPseudoIdScrollMarker and
       kPseudoIdOverscrollAreaParent).
    
     * It removes the hardcoded assumption in
       LayoutTreeBuilderTraversal::NextSibling and PreviousSibling that an
       element cannot have both a kPseudoIdScrollMarkerGroupAfter and a
       kPseudoIdViewTransition pseudo.
    
    Bug: 453705247
    Change-Id: I81ecd700d1ec68b32df472ebb439889456bec3ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808045
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625110}

--- third_party/blink/renderer/core/dom/node.h
+++ third_party/blink/renderer/core/dom/node.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_DOM_NODE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_NODE_H_
 
+#include <array>
 #include <climits>
 #include <concepts>
 
@@ -317,6 +318,27 @@ class CORE_EXPORT Node : public EventTarget {
   Node* PseudoAwarePreviousSibling() const;
   Node* PseudoAwareFirstChild() const;
   Node* PseudoAwareLastChild() const;
+  static constexpr std::array kElementChildPseudoOrder{
+      kPseudoIdScrollMarkerGroupBefore,
+      kPseudoIdMarker,
+      kPseudoIdColumn,  // special case: contains multiple items
+      kPseudoIdScrollMarker,
+      kPseudoIdScrollButtonBlockStart,
+      kPseudoIdScrollButtonInlineStart,
+      kPseudoIdScrollButtonInlineEnd,
+      kPseudoIdScrollButtonBlockEnd,
+      kPseudoIdOverscrollAreaParent,
+      kPseudoIdCheckMark,
+      kPseudoIdBefore,
+      kPseudoIdNone,  // special case: this means the regular children
+      kPseudoIdPickerIcon,
+      kPseudoIdExpandIcon,
+      kPseudoIdAfter,
+      kPseudoIdInterestButton,
+      kPseudoIdScrollMarkerGroupAfter,
+      kPseudoIdViewTransition,  // layout traversals special case this when it
+                                // is a child of the document
+  };
 
   const KURL& baseURI() const;
 
