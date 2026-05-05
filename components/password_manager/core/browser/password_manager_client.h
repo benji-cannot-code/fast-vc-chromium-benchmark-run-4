1cb0ea5e29d1bf1d15dc5eb5cc109781e1740181
Ioana Treib <ioanap@chromium.org>
2026-05-05 06:38:05 -0700

    [ActorLogin] Reject credential management request on Android ...
    if an actor task is active on the tab.
    
    The code is also now compiled on iOS, although the check for an actor
    task being active there returns false all the time.
    
    Fixed: 509458999
    Change-Id: If6a7caf39a80edf45f04fd50e3601ba706e5cffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816955
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625375}

--- components/password_manager/core/browser/password_manager_client.h
+++ components/password_manager/core/browser/password_manager_client.h
@@ -579,6 +579,8 @@ class PasswordManagerClient {
 
   virtual UndoPasswordChangeController* GetUndoPasswordChangeController();
 
+  // TODO(crbug.com/509852350): Figure out if this is needed on iOS and
+  //  implement it.
   virtual bool IsActorTaskActive();
 };
 
