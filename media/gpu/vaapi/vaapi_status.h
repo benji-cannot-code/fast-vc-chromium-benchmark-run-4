8ecfdb3a831288b8d1aa87763de282a9e95bd9d3
Ted Meyer <tmathmeyer@chromium.org>
2026-05-04 19:57:21 -0700

    Better error handling for VaapiVideoDecoder
    This change mainly passes the FramePool initialization error up, which
    was probably the most confusing and unactionable error when debugging
    how vaapi was failing to play. Now it will render the error:
    `FailedToCreateImageProcessor`, which is a both a slight improvement,
    but also will let the `ImageProcessorFactory::CreateWithInputCandidates`
    function to itself render a better error in later CLs.
    
    Bug: 40139291
    Change-Id: Ifb01f78047d2d37a42654edc6e205ab023052500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793537
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625160}

--- media/gpu/vaapi/vaapi_status.h
+++ media/gpu/vaapi/vaapi_status.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 enum class VaapiStatusCode : StatusCodeType {
-  kOk = 0,
   kBadContext = 1,
   kNoBuffer = 2,
   kNoBufferHandle = 3,
