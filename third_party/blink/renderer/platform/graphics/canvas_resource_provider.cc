a16173c2920ac0da26f11a1fcfa7391b7e189eb6
Colin Blundell <blundell@chromium.org>
2026-05-04 23:56:43 -0700

    [Blink] Move CRPSI::context_provider_wrapper_ into subclasses
    Next step on decoupling CanvasNon2DResourceProviderSI from the
    CanvasResourceProvider class hierarchy.
    
    Bug: 352263194
    Change-Id: Icd1972a1c81bcae721cccbe71c7408ce39c8143d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7813438
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625240}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -243,9 +243,7 @@ CanvasResourceProviderSharedImage::CanvasResourceProviderSharedImage(
     viz::SharedImageFormat format,
     SkAlphaType alpha_type,
     const gfx::ColorSpace& color_space,
-    base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper,
     bool is_accelerated,
-    gpu::SharedImageUsageSet shared_image_usage_flags,
     Delegate* delegate)
     : CanvasResourceProvider(kSharedImage,
                              size,
@@ -253,12 +251,7 @@ CanvasResourceProviderSharedImage::CanvasResourceProviderSharedImage(
                              alpha_type,
                              color_space,
                              delegate),
-      is_accelerated_(is_accelerated),
-      context_provider_wrapper_(std::move(context_provider_wrapper)) {
-  if (context_provider_wrapper_) {
-    context_provider_wrapper_->AddObserver(this);
-  }
-}
+      is_accelerated_(is_accelerated) {}
 
 CanvasResourceProviderSharedImage::CanvasResourceProviderSharedImage(
     gfx::Size size,
@@ -292,16 +285,7 @@ CanvasResourceProviderSharedImage::~CanvasResourceProviderSharedImage() {
     return;
   }
 
-  if (context_provider_wrapper_) {
-    context_provider_wrapper_->RemoveObserver(this);
-  }
-
   GetFlushForImageListener()->RemoveObserver(this);
-
-  // Last chance for outstanding GPU timers to record metrics.
-  if (RasterInterface()) {
-    CheckGpuTimers(RasterInterface());
-  }
 }
 
 base::WeakPtr<CanvasResourceProviderSharedImage>
@@ -2117,13 +2101,13 @@ Canvas2DResourceProviderSharedImage::Canvas2DResourceProviderSharedImage(
                                         format,
                                         alpha_type,
                                         color_space,
-                                        context_provider_wrapper,
                                         is_accelerated,
-                                        shared_image_usage_flags,
-                                        delegate) {
+                                        delegate),
+      context_provider_wrapper_(std::move(context_provider_wrapper)) {
   recorder_for_canvas_2d_ =
       std::make_unique<MemoryManagedPaintRecorder>(Size(), this);
   if (context_provider_wrapper_) {
+    context_provider_wrapper_->AddObserver(this);
     raster_context_provider_ = base::WrapRefCounted(
         context_provider_wrapper_->ContextProvider().RasterContextProvider());
     // Graphite can handle a large buffer size.
@@ -2231,9 +2215,18 @@ Canvas2DResourceProviderSharedImage::Canvas2DResourceProviderSharedImage(
 }
 
 Canvas2DResourceProviderSharedImage::~Canvas2DResourceProviderSharedImage() {
+  if (context_provider_wrapper_) {
+    context_provider_wrapper_->RemoveObserver(this);
+  }
   if (raster_context_provider_) {
     raster_context_provider_->RemoveObserver(this);
   }
+
+  // Last chance for outstanding GPU timers to record metrics.
+  if (RasterInterface()) {
+    CheckGpuTimers(RasterInterface());
+  }
+
   UMA_HISTOGRAM_EXACT_LINEAR("Blink.Canvas.MaximumInflightResources",
                              max_inflight_resources_, 20);
 }
@@ -2271,14 +2264,14 @@ CanvasNon2DResourceProviderSharedImage::CanvasNon2DResourceProviderSharedImage(
                                         format,
                                         alpha_type,
                                         color_space,
-                                        context_provider_wrapper,
                                         is_accelerated,
-                                        shared_image_usage_flags,
                                         delegate),
       recorder_for_external_draws_(
           std::make_unique<MemoryManagedPaintRecorder>(Size(),
-                                                       /*client=*/nullptr)) {
+                                                       /*client=*/nullptr)),
+      context_provider_wrapper_(std::move(context_provider_wrapper)) {
   if (context_provider_wrapper_) {
+    context_provider_wrapper_->AddObserver(this);
     raster_context_provider_ = base::WrapRefCounted(
         context_provider_wrapper_->ContextProvider().RasterContextProvider());
     // Graphite can handle a large buffer size.
@@ -2386,9 +2379,18 @@ CanvasNon2DResourceProviderSharedImage::CanvasNon2DResourceProviderSharedImage(
 
 CanvasNon2DResourceProviderSharedImage::
     ~CanvasNon2DResourceProviderSharedImage() {
+  if (context_provider_wrapper_) {
+    context_provider_wrapper_->RemoveObserver(this);
+  }
   if (raster_context_provider_) {
     raster_context_provider_->RemoveObserver(this);
   }
+
+  // Last chance for outstanding GPU timers to record metrics.
+  if (RasterInterface()) {
+    CheckGpuTimers(RasterInterface());
+  }
+
   UMA_HISTOGRAM_EXACT_LINEAR("Blink.Canvas.MaximumInflightResources",
                              max_inflight_resources_, 20);
 }
--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -383,7 +383,6 @@ class PLATFORM_EXPORT Canvas2DResourceProviderBitmap
 // * Layers may be overlay candidates.
 class PLATFORM_EXPORT CanvasResourceProviderSharedImage
     : public CanvasResourceProvider,
-      public WebGraphicsContext3DProviderWrapper::DestructionObserver,
       public BitmapGpuChannelLostObserver,
       public CanvasResourceSharedImage::Client,
       public FlushForImageObserver {
@@ -393,9 +392,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
       viz::SharedImageFormat,
       SkAlphaType,
       const gfx::ColorSpace&,
-      base::WeakPtr<WebGraphicsContext3DProviderWrapper>,
       bool is_accelerated,
-      gpu::SharedImageUsageSet shared_image_usage_flags,
       Delegate*);
   CanvasResourceProviderSharedImage(gfx::Size,
                                     viz::SharedImageFormat,
@@ -430,9 +427,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
     return static_cast<CanvasResourceSharedImage*>(resource_.get());
   }
   virtual base::WeakPtr<WebGraphicsContext3DProviderWrapper>
-  ContextProviderWrapper() const {
-    return context_provider_wrapper_;
-  }
+  ContextProviderWrapper() const = 0;
   virtual gpu::raster::RasterInterface* RasterInterface() const;
   virtual void EnsureWriteAccess() = 0;
   virtual void EndWriteAccess() = 0;
@@ -446,7 +441,6 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
   // The resource that is currently being used by this provider.
   scoped_refptr<CanvasResourceSharedImage> resource_;
 
-  base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper_;
   bool current_resource_has_write_access_ = false;
   bool is_software_ = false;
   bool is_cleared_ = false;
@@ -482,6 +476,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
 // * by Canvas2D.
 class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
     : public CanvasResourceProviderSharedImage,
+      public WebGraphicsContext3DProviderWrapper::DestructionObserver,
       public viz::ContextLostObserver {
  public:
   // The returned instance will have been cleared at creation.
@@ -591,6 +586,11 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
   ScopedRasterTimer CreateScopedRasterTimerForCanvas2D() override;
 
  private:
+  base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
+      const override {
+    return context_provider_wrapper_;
+  }
+
   // viz::ContextLostObserver implementation.
   void OnContextLost() override {
     CanvasResourceProviderSharedImage::OnContextLost();
@@ -616,6 +616,8 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
       cc::PaintImage::kInvalidContentId;
   scoped_refptr<StaticBitmapImage> cached_snapshot_;
 
+  base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper_;
+
   // `raster_context_provider_` holds a reference on the shared
   // `RasterContextProvider`, to keep it alive until it notifies us after the
   // GPU context is lost. Without this, instances of this class would not get
@@ -632,6 +634,7 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
 // * by non-Canvas2D clients.
 class PLATFORM_EXPORT CanvasNon2DResourceProviderSharedImage
     : public CanvasResourceProviderSharedImage,
+      public WebGraphicsContext3DProviderWrapper::DestructionObserver,
       public viz::ContextLostObserver {
  public:
   static std::unique_ptr<CanvasNon2DResourceProviderSharedImage> Create(
@@ -776,6 +779,11 @@ class PLATFORM_EXPORT CanvasNon2DResourceProviderSharedImage
   void EndExternalWrite(const gpu::SyncToken& external_write_sync_token);
 
  private:
+  base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
+      const override {
+    return context_provider_wrapper_;
+  }
+
   // viz::ContextLostObserver implementation.
   void OnContextLost() override {
     CanvasResourceProviderSharedImage::OnContextLost();
@@ -798,6 +806,8 @@ class PLATFORM_EXPORT CanvasNon2DResourceProviderSharedImage
   cc::PaintImage::ContentId cached_content_id_ =
       cc::PaintImage::kInvalidContentId;
   scoped_refptr<StaticBitmapImage> cached_snapshot_;
+
+  base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper_;
 
   // `raster_context_provider_` holds a reference on the shared
   // `RasterContextProvider`, to keep it alive until it notifies us after the
