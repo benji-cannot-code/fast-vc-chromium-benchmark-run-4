877514651839bb18115375064bd4ae214b483cf1
Gauthier Ambard <gambard@chromium.org>
2026-05-05 06:56:59 -0700

    Revert "[iOS][WebAuthn] Validate rpId against origin for tracking events."
    This reverts commit dd6f528083826089cb41e4643d164b7867723964.
    
    Reason for revert: The previous CL got reverted, I think this one needs to be reverted too. Sorry if it is not the case!
    
    Original change's description:
    > [iOS][WebAuthn] Validate rpId against origin for tracking events.
    >
    > Updates WebAuthn tracking events (logGetResolved, logCreateResolved)
    > to validate the provided rpId against the sender origin using
    > OriginIsAllowedToClaimRelyingPartyId. This prevents malicious frames
    > from spoofing arbitrary RP IDs in metrics events.
    >
    > Bug: 500588580
    > Change-Id: If4ac22c520836cdbb5ab75c24d93718d218ad701
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807251
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1625351}
    
    Bug: 500588580
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I657677a499ed173b4917b24452cb5836a27b3bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816860
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1625380}

--- components/webauthn/ios/passkey_java_script_feature.mm
+++ components/webauthn/ios/passkey_java_script_feature.mm
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/js_messaging/script_message.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/web_state.h"
-#import "url/origin.h"
 
 namespace webauthn {
 
@@ -263,9 +262,8 @@ void PasskeyJavaScriptFeature::ScriptMessageReceived(
   const base::DictValue& dict = body->GetDict();
 
   std::optional<PasskeyScriptEvent> event = ParsePasskeyScriptEvent(
-      dict, message.security_origin(),
-      [passkey_tab_helper](const std::string& rp_id,
-                           const std::string& credential_id) {
+      dict, [passkey_tab_helper](const std::string& rp_id,
+                                 const std::string& credential_id) {
         return passkey_tab_helper->HasCredential(rp_id, credential_id);
       });
 
