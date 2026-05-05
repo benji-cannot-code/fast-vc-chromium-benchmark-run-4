5d444f54de0c62ec0132c32e113a4774ad2d4f04
Abhijith Nair <abhijithnair@chromium.org>
2026-05-05 04:14:02 -0700

    Make certain static methods not trigger startup
    This CL adds a feature flag to stop certain static methods triggering
    and waiting for WebView startup. Several static API methods are updated
    to use this behaviour.
    
    Bug: 437338203
    Change-Id: I7f80a9be443e374e94948b8580fd28e1ce56053f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7799938
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625317}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -72,6 +72,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewRendererKeepAlive,
     &features::kWebViewSkipInterceptsForPrefetch,
     &features::kWebViewStartupTasksYieldToNative,
+    &features::kWebViewStaticMethodsNotTriggerStartup,
     &features::kWebViewTestFeature,
     &features::kWebViewUseInitialNetworkStateAtStartup,
     &features::kWebViewUseMetricsUploadServiceOnlySdkRuntime,
