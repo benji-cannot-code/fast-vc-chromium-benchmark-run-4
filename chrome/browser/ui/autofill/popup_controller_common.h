5fa6bd3985db846474024fdd50efc7d4ddb406ae
Piotr Kotynia <piotrkotynia@google.com>
2026-05-05 03:29:25 -0700

    [Autofill] Remove container_view from PopupControllerCommon
    Removes the redundant `container_view` field from
    `PopupControllerCommon`. Since the controllers already receive a
    `WebContents` pointer, they can fetch the `container_view` directly when
    needed. This simplifies the struct and reduces data duplication.
    
    Bug: 40100455
    Change-Id: I3bb3df194eb78d05f66e82d88ad557cf841d53ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806998
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625307}

--- chrome/browser/ui/autofill/popup_controller_common.h
+++ chrome/browser/ui/autofill/popup_controller_common.h
@@ -20,7 +20,6 @@ struct PopupControllerCommon {
   PopupControllerCommon(
       gfx::RectF element_bounds,
       base::i18n::TextDirection text_direction,
-      gfx::NativeView container_view,
       PopupAnchorType anchor_type = PopupAnchorType::kField,
       bool show_tabbed_popup = false,
       bool prefer_prev_arrow_side_on_suggestions_update = false);
@@ -37,9 +36,6 @@ struct PopupControllerCommon {
 
   // The direction of the <input>.
   base::i18n::TextDirection text_direction;
-
-  // Weak reference
-  gfx::NativeView container_view;
 
   // The type of the element to anchor the popup on.
   PopupAnchorType anchor_type;
