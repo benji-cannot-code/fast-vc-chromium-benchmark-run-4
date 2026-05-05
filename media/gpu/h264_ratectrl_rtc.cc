88224edae852c7f291f3c57ecab717da1cbadef6
James Zern <jzern@chromium.org>
2026-05-05 12:06:20 -0700

    gpu,av1*encoder*delegate: check UpdateRateControl return
    Further calls to the API should not be made until a successful call to
    `UpdateRateControl` is made.
    
    This change also modifies the `MediaFoundationVideoEncodeAccelerator`
    implementation and its delegates for consistency.
    
    Bug: 501657371
    Change-Id: I8baf217a658a2e5b93cc0381a038482456f483bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7783655
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625628}

--- media/gpu/h264_ratectrl_rtc.cc
+++ media/gpu/h264_ratectrl_rtc.cc
@@ -119,7 +119,7 @@ std::unique_ptr<H264RateCtrlRTC> H264RateCtrlRTC::Create(
   return rate_ctrl;
 }
 
-void H264RateCtrlRTC::UpdateRateControl(
+bool H264RateCtrlRTC::UpdateRateControl(
     const H264RateControlConfigRTC& config) {
   CheckRateControlConfig(config);
 
@@ -129,6 +129,7 @@ void H264RateCtrlRTC::UpdateRateControl(
   // New settings are applied on ComputeQP() method call.
   new_config_ = config;
   config_changed_ = true;
+  return true;
 }
 
 H264RateCtrlRTC::FrameDropDecision H264RateCtrlRTC::ComputeQP(
