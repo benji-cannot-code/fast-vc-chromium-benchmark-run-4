b1bb3b3a21d385c69c4fc7bb1c1127daa0cb45f9
Alexis Hétu <sugoi@chromium.org>
2026-05-05 05:47:37 -0700

    [iOS] Add security_origin to ScriptMessage
    This change adds the `security_origin()` getter and a corresponding
    `url::Origin` parameter to `web::ScriptMessage`'s constructor.
    
    The security origin is obtained from `WKSecurityOrigin` using the
    `web::OriginWithWKSecurityOrigin` utility inside
    `JavaScriptContentWorld`'s `GetMessage` helper.
    
    All affected constructor references across unittests and fuzzer
    support utilities have been updated accordingly.
    
    Bug: 500588580
    Change-Id: I988eefe0aae168f9deefce62a03a69ce800c827a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814561
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625345}

--- ios/chrome/browser/reader_mode/model/reader_mode_java_script_feature_unittest.mm
+++ ios/chrome/browser/reader_mode/model/reader_mode_java_script_feature_unittest.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/web_task_environment.h"
 #import "services/metrics/public/cpp/ukm_builders.h"
 #import "testing/platform_test.h"
+#import "url/origin.h"
 
 using IOS_ReaderMode_Heuristic_Result =
     ukm::builders::IOS_ReaderMode_Heuristic_Result;
@@ -41,14 +42,14 @@ class ReaderModeJavaScriptFeatureTest : public PlatformTest {
     return web::ScriptMessage(ValidDerivedFeatures(),
                               /*is_user_interacting=*/true,
                               /*is_main_frame=*/true,
-                              /*request_url=*/url);
+                              /*request_url=*/url, url::Origin::Create(url));
   }
 
   web::ScriptMessage ScriptMessageForInvalidUrl() {
     return web::ScriptMessage(ValidDerivedFeatures(),
                               /*is_user_interacting=*/true,
                               /*is_main_frame=*/true,
-                              /*request_url=*/std::nullopt);
+                              /*request_url=*/std::nullopt, url::Origin());
   }
 
   web::FakeWebState* web_state() { return &web_state_; }
@@ -139,7 +140,8 @@ TEST_F(ReaderModeJavaScriptFeatureTest, MalformedResponseNotDict) {
   web::ScriptMessage script_message(std::move(invalid_body),
                                     /*is_user_interacting=*/true,
                                     /*is_main_frame=*/true,
-                                    /*request_url=*/valid_url());
+                                    /*request_url=*/valid_url(),
+                                    url::Origin::Create(valid_url()));
   ReaderModeJavaScriptFeature::GetInstance()->ScriptMessageReceived(
       web_state(), script_message);
   // Test heuristic result histogram.
@@ -160,7 +162,8 @@ TEST_F(ReaderModeJavaScriptFeatureTest, MalformedResponseMissingFeatures) {
   web::ScriptMessage script_message(std::move(invalid_body),
                                     /*is_user_interacting=*/true,
                                     /*is_main_frame=*/true,
-                                    /*request_url=*/valid_url());
+                                    /*request_url=*/valid_url(),
+                                    url::Origin::Create(valid_url()));
   ReaderModeJavaScriptFeature::GetInstance()->ScriptMessageReceived(
       web_state(), script_message);
   // Test heuristic result histogram.
--- ios/web/js_features/clipboard/clipboard_java_script_feature_unittest.mm
+++ ios/web/js_features/clipboard/clipboard_java_script_feature_unittest.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/web_test_with_web_state.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
+#import "url/origin.h"
 
 using base::test::ios::kWaitForJSCompletionTimeout;
 using base::test::ios::WaitUntilConditionOrTimeout;
@@ -106,7 +107,8 @@ class ClipboardJavaScriptFeatureTest : public WebTestWithWebState {
 
     web::ScriptMessage message(std::make_unique<base::Value>(std::move(body)),
                                /*is_user_interacting=*/true,
-                               /*is_main_frame=*/true, GURL::EmptyGURL());
+                               /*is_main_frame=*/true, GURL::EmptyGURL(),
+                               url::Origin());
     feature_->ScriptMessageReceived(web_state(), message);
   }
 
