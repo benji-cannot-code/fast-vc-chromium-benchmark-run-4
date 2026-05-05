f0076e6d7590678b81e124c726a5296a9c52f333
Riley Tatum <rtatum@google.com>
2026-05-05 16:54:31 -0700

    [next] Set feature params to launching values
    Bug: 505856800
    Change-Id: I9cacfed1107042b0be1cfbbc6bc38c7e35ef4024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7815005
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625827}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -319,7 +319,7 @@ const base::FeatureParam<bool> kNtpNextClientSensitivityCheckParam(
 const base::FeatureParam<bool> kNtpNextShowStaticRecentTabChipParam(
     &ntp_features::kNtpNextFeatures,
     "NtpNextShowStaticRecentTabChipParam",
-    true);
+    false);
 const base::FeatureParam<bool> kNtpNextShowDismissalUIParam(
     &ntp_features::kNtpNextFeatures,
     "NtpNextShowDismissalUIParam",
