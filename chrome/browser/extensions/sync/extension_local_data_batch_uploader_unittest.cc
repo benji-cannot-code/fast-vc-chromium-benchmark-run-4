83906cfe11ad84327a755a0141527f00dcdfcc49
James Cook <jamescook@chromium.org>
2026-05-04 17:18:24 -0700

    extensions: Clean up BUILD.gn for an extensions sync unit test
    Unit tests for files in //chrome/browser/extensions were recently moved
    from //chrome/test/BUILD.gn to //chrome/browser/extensions/BUILD.gn.
    Move the reference to extension_local_data_batch_uploader_unittest.cc
    to the new location.
    
    Also add a static assert to make it clear this file is Win/Mac/Linux
    only.
    
    Bug: 469417243
    Change-Id: I59da1314aecd9fba1691051f9955456e46276772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814781
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625035}

--- chrome/browser/extensions/sync/extension_local_data_batch_uploader_unittest.cc
+++ chrome/browser/extensions/sync/extension_local_data_batch_uploader_unittest.cc
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/url_constants.h"
 
+// Batch upload is only available on Windows, Mac and Linux.
+static_assert(BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX));
+
 namespace extensions {
 
 class ExtensionLocalDataBatchUploaderTest
