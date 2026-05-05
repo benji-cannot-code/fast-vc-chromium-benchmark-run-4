fa1d4379896c88ffb3febd7dff10117753e10098
Piotr Kotynia <piotrkotynia@google.com>
2026-05-05 15:17:03 -0700

    [Clank] Introduce builder pattern for TextBubble
    This CL introduces builder pattern and deprecates the existing public
    constructors in TextBubble.java to encourage the use of the Builder
    pattern.
    
    Bug: 505624919
    Change-Id: Ica22b33450f8b355a1271bf2d7afe8f944fe4a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789371
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625750}

--- chrome/browser/user_education/java/src/org/chromium/chrome/browser/user_education/UserEducationHelper.java
+++ chrome/browser/user_education/java/src/org/chromium/chrome/browser/user_education/UserEducationHelper.java
@@ -162,6 +162,7 @@ public class UserEducationHelper {
             assert !contentString.isEmpty();
             assert !accessibilityString.isEmpty();
 
+            // TODO(crbug.com/505624919): Replace with Builder pattern.
             mTextBubble =
                     new TextBubble(
                             mActivity,
