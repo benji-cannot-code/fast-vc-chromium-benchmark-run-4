e7bb75163fd3adf57e3c402b9ecc374ef4cac163
Dale Curtis <dalecurtis@chromium.org>
2026-05-04 22:30:03 -0700

    [ImageDecoder] Mark global ready, completed promises as handled
    The spec was recently updated to avoid spamming the global unhandled
    rejection handler.
    
    I tried adding a test for this, but it seems we weren't firing the
    rejection handler already or it is only fired on a long enough delay
    that it's hard to test.
    
    R=eugene
    
    Fixed: 509627727
    Change-Id: I79a5ea6d93976b398737f7b5ee878e64e0dd9f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814766
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625194}

--- third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
+++ third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
@@ -160,6 +160,9 @@ ImageDecoderExternal::ImageDecoderExternal(ScriptState* script_state,
     return;
   }
 
+  // Do not report unhandled rejections of the completed promise.
+  completed_property_->MarkAsHandled();
+
   UseCounter::Count(GetExecutionContext(), WebFeature::kWebCodecs);
 
   // |data| is a required field.
