8acfc350407ff3dcf841be3ad0635065c22500c3
Rafał Godlewski <rgod@google.com>
2026-05-05 14:25:06 -0700

    [iOS][Sync] Move todo to a separate bug
    Trying to close the whole feature bug.
    
    Bug: 509894544,408165259
    Change-Id: I518406c6bcfe0382d07754fc7432cfa3055b9718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817296
    Auto-Submit: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625718}

--- ios/chrome/browser/overlays/ui_bundled/infobar_banner/sync_error/sync_error_infobar_banner_overlay_mediator.mm
+++ ios/chrome/browser/overlays/ui_bundled/infobar_banner/sync_error/sync_error_infobar_banner_overlay_mediator.mm
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [consumer setButtonText:base::SysUTF16ToNSString(delegate->GetButtonLabel(
                               SyncErrorInfoBarDelegate::BUTTON_OK))];
 
-  // TODO(crbug.com/408165259): Use a dedicated icon in case when
+  // TODO(crbug.com/509894544): Use a dedicated icon in case when
   // `delegate->DisplayPasswordErrorIcon()` is true.
   [consumer setIconImage:DefaultSymbolTemplateWithPointSize(
                              kSyncErrorSymbol, kInfobarSymbolPointSize)];
