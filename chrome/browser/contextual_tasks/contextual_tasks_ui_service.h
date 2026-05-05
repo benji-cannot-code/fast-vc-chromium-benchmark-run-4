347a40af00ed49e4e58b4e138f2032c27539a841
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-05-05 16:18:49 -0700

    [composebox] Refactor signed-in checks to use ContextualTasksUiService.
    This change updates ComposeBoxQueryControllerBridge and
    ContextualSearchboxHandler to use ContextualTasksUiService's methods for
    determining if a user is signed in with a primary account and if a given
    URL is associated with that account.
    
    Bug: b:507460460
    Change-Id: Ic5b1983e7c51df4cd0324e86fdff559c86070549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806716
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625806}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.h
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.h
@@ -316,6 +316,9 @@ class ContextualTasksUiService : public KeyedService {
   virtual void ShowUndoSnackbar(
       BrowserWindowInterface* browser_window_interface);
 
+  // Returns whether the provided URL is for the primary account in Chrome.
+  virtual bool IsUrlForPrimaryAccount(const GURL& url);
+
   base::WeakPtr<ContextualTasksUiService> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
@@ -329,9 +332,6 @@ class ContextualTasksUiService : public KeyedService {
                                     tabs::TabInterface* tab,
                                     bool is_from_embedded_page,
                                     bool is_to_new_tab);
-
-  // Returns whether the provided URL is for the primary account in Chrome.
-  virtual bool IsUrlForPrimaryAccount(const GURL& url);
 
   // Used primarily for debugging - loads a URL in the specified WebContents.
   virtual void LoadUrlInWebContents(const GURL& url,
