28c0b56d7b40b2c48d6652d614373a4f746aeaf3
Eugene Zemtsov <eugene@chromium.org>
2026-05-05 12:49:37 -0700

    [gpu] Add Dawn support for rendering multi-planar AHBs
    This change enables rendering multi-planar AHardwareBuffers via
    Graphite/Dawn by leveraging the `OpaqueYCbCrAndroidForExternalTexture`
    WebGPU feature on Vulkan.
    
    When an image format prefers an external sampler, the underlying
    AHardwareBuffer is represented in Dawn as an `OpaqueYCbCrAndroid`
    texture.
    
    Currently Cr and Cb planes get mixed up in rendering this will be
    addressed in future CLs by plumbing VulkanYCbCrInfo.
    
    Bug: 417807138
    Change-Id: I9f98b8f2c98d7294fd69c0cc836812a83c990370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766775
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625655}

--- gpu/command_buffer/service/shared_image/skia_graphite_dawn_image_representation.cc
+++ gpu/command_buffer/service/shared_image/skia_graphite_dawn_image_representation.cc
@@ -98,7 +98,7 @@ SkiaGraphiteDawnImageRepresentation::CreateBackendTextureHolders(
       SupportsMultiplanarRendering(context_state_.get());
   const bool supports_multiplanar_copy =
       SupportsMultiplanarCopy(context_state_.get());
-  if (format().is_multi_plane()) {
+  if (format().is_multi_plane() && !format().PrefersExternalSampler()) {
     CHECK(format() == viz::MultiPlaneFormat::kP010 ||
           format() == viz::MultiPlaneFormat::kP210 ||
           format() == viz::MultiPlaneFormat::kP410 ||
@@ -149,6 +149,11 @@ SkiaGraphiteDawnImageRepresentation::BeginWriteAccess(
     const gfx::Rect& update_rect) {
   CHECK_EQ(mode_, RepresentationAccessMode::kNone);
   CHECK(!dawn_scoped_access_);
+
+  if (format().PrefersExternalSampler()) {
+    return {};
+  }
+
   dawn_scoped_access_ = dawn_representation_->BeginScopedAccess(
       supported_tex_usages_, AllowUnclearedAccess::kYes, update_rect);
   if (!dawn_scoped_access_) {
@@ -196,6 +201,10 @@ std::vector<scoped_refptr<GraphiteTextureHolder>>
 SkiaGraphiteDawnImageRepresentation::BeginWriteAccess() {
   CHECK_EQ(mode_, RepresentationAccessMode::kNone);
   CHECK(!dawn_scoped_access_);
+
+  if (format().PrefersExternalSampler()) {
+    return {};
+  }
 
   dawn_scoped_access_ = dawn_representation_->BeginScopedAccess(
       supported_tex_usages_, AllowUnclearedAccess::kYes);
