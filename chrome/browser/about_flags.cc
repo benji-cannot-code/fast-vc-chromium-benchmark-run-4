b12e421629ef2ac7bef467599a74ad97cd7a801d
Lynne Jiang <lyjiang@google.com>
2026-05-04 18:00:04 -0700

    Add Gemma 4 for Built-in AI flag
    Bug: 505417720
    Change-Id: Iaeb5cf38b3efabd64006a6d18965059e58339cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808999
    Reviewed-by: Jingyun Liu <jingyun@google.com>
    Commit-Queue: Lynne Jiang <lyjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625072}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4660,6 +4660,12 @@ const FeatureEntry::Choice kAIClassifierChoices[] = {
     {flags_ui::kGenericExperimentChoiceEnabled, switches::kEnableFeatures,
      "AIClassifierAPI,OnDeviceModelLitertLmBackend"}};
 
+const FeatureEntry::Choice kGemma4Choices[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {flags_ui::kGenericExperimentChoiceEnabled, switches::kEnableFeatures,
+     "OptimizationGuideManifestBroker,OnDeviceModelLitertLmBackend"
+     "AIApiFoundationalModel:model_version/v4"}};
+
 const FeatureEntry::FeatureParam kWalletApiPrivatePassesUrl[] = {
     {"wallet_pass_save_url", "https://wallet1ppasses.pa.googleapis.com"}};
 
@@ -10244,6 +10250,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kClassifierAPIForTinyModelDescription, kOsDesktop,
      MULTI_VALUE_TYPE(kAIClassifierChoices),
      flag_descriptions::kAIAPIsForGeminiNanoLinks},
+
+    {"gemma4-for-built-in-ai", flag_descriptions::kGemma4ForBuiltInAIName,
+     flag_descriptions::kGemma4ForBuiltInAIDescription, kOsDesktop,
+     MULTI_VALUE_TYPE(kGemma4Choices),
+     flag_descriptions::kGemma4ForBuiltInAILinks},
 
     {"summarizer-api-performance-preference",
      flag_descriptions::kSummarizerAPIWithPerformancePreferenceName,
