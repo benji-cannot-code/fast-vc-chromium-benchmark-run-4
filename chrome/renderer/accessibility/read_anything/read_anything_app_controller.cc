c866b635821caa83c0b0a905a09f39f1f5486394
Lauren Winston <lwinston@google.com>
2026-05-04 19:03:04 -0700

    Log the number of words distilled, regardless of distillation method.
    Fixed: 508688040
    Change-Id: I547653184b585e37cffb36879636fbd0382da242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809862
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Shari Trewin <trewin@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625128}

--- chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
@@ -2248,6 +2248,9 @@ void ReadAnythingAppController::OnDistilled(int word_count) {
         "Accessibility.ReadAnything.WordsDistilledByReadability", word_count, 1,
         kMaxWordsConsumed, kWordsConsumedBuckets);
   }
+  base::UmaHistogramCustomCounts(
+      "Accessibility.ReadAnything.WordsDistilledOnNewPage", word_count, 1,
+      kMaxWordsConsumed, kWordsConsumedBuckets);
 }
 
 void ReadAnythingAppController::UpdateWordsSeen(int words_seen) {
