3f2ee4bc5ec5205df6d7e089b538cf766aa1a426
Hira Mahmood <hiramahmood@google.com>
2026-05-05 06:19:31 -0700

    [x-plat promos] Update ESB promo anchor
    This CL updates the anchor for the ESB promo from the 3-dot menu to the
    avatar profile button.
    
    Fixed: 509612548
    Change-Id: I37601efcec432759168637ab0d1c03d28dfddc37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814196
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625365}

--- chrome/browser/ui/views/user_education/browser_user_education_service.cc
+++ chrome/browser/ui/views/user_education/browser_user_education_service.cc
@@ -1701,7 +1701,7 @@ void MaybeRegisterChromeFeaturePromos(
     registry.RegisterFeature(std::move(
         FeaturePromoSpecification::CreateForCustomUi(
             feature_engagement::kIPHiOSEnhancedBrowsingDesktopFeature,
-            kToolbarAppMenuButtonElementId,
+            kToolbarAvatarButtonElementId,
             user_education::CreateCustomHelpBubbleViewFactoryCallback(
                 base::BindRepeating(
                     &IOSPromoBubbleView::Create,
