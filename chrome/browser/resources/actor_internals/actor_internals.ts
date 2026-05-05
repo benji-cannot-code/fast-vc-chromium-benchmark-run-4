8735fe9b38d789686cd3a7ceef723a293650c8a6
rosiey <rosiey@google.com>
2026-05-05 13:30:17 -0700

    [actor-internals] Add 'Clear Table' button to event log viewer
    Adds a 'Clear Table' button to the chrome://actor-internals/ page to
    allow users to clear all existing rows from the table. This clears the
    table contents and resets the internal state of ActorEventLog to prevent
    desynchronization issues.
    
    Test Results:
    
    http: //shortn/_4CJR5HtttV
    http: //shortn/_p4JlttTggu
    Bug: b:503736331
    Change-Id: I66442dc353353cd5f62b99e438ea90bfa510f71f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801564
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625687}

--- chrome/browser/resources/actor_internals/actor_internals.ts
+++ chrome/browser/resources/actor_internals/actor_internals.ts
@@ -28,6 +28,7 @@ class ActorEventLog {
   private spanState =
       new Map<number, {collapsed: boolean, parentIds: number[]}>();
   private indentationByTrack = new Map<string, number>();
+  private objectUrls: string[] = [];
 
   constructor() {
     this.table = getRequiredElement<HTMLTableElement>('actor-events-table');
@@ -136,6 +137,7 @@ class ActorEventLog {
       const byteArray = new Uint8Array(entry.screenshot);
       const blob = new Blob([byteArray], { type: 'image/jpeg' });
       const blobUrl = URL.createObjectURL(blob);
+      this.objectUrls.push(blobUrl);
 
       const img = document.createElement('img');
       img.src = blobUrl; // Use the Blob URL for the small image
@@ -246,8 +248,9 @@ class ActorEventLog {
         return;
       }
     }
-    // If we reached the top or table is empty, append after header
-    rows[0]!.after(row);
+    // If we reached the top or table is empty, insert after thead
+    const thead = this.table.querySelector('thead')!;
+    thead.after(row);
   }
 
   /**
@@ -360,7 +363,27 @@ class ActorEventLog {
       }
     }
   }
+
+  /**
+   * Removes all rows from the table and resets internal state to prevent
+   * desynchronization.
+   */
+  clearTable() {
+    this.table.replaceChildren(this.table.querySelector('thead')!);
+    this.activeSpans = [];
+    this.spanState.clear();
+    this.indentationByTrack.clear();
+    this.spanIdCounter = 0;
+
+    // Revoke object URLs to prevent memory leaks
+    this.objectUrls.forEach(url => URL.revokeObjectURL(url));
+    this.objectUrls = [];
+
+    // Clear filter input
+    this.clearFilter();
+  }
 }
+
 
 
 function startLogging() {
@@ -389,4 +412,5 @@ window.onload = function() {
   getRequiredElement('stop-logging').onclick = stopLogging;
   getRequiredElement('filter-by-task-id').onclick = () => log.filterByTaskId();
   getRequiredElement('clear-filter').onclick = () => log.clearFilter();
+  getRequiredElement('clear-table').onclick = () => log.clearTable();
 };
