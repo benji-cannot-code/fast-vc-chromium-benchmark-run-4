5ab95a1277f3d7c5cde8903e5dd7891ad7022e81
Bryan Oltman <bryanoltman@google.com>
2026-05-05 12:10:23 -0700

    Fix over-release handling in IOSurfaceMachPortTraits::Retain
    Retain() now correctly returns MACH_PORT_NULL if mach_port_mod_refs
    fails, preventing the adoption of unreferenced port names.
    
    Fixed: 502104354
    Change-Id: If9b731f548e354ca101df659152ac5b6aacf9fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7815394
    Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625633}

--- ui/gfx/mac/io_surface.cc
+++ ui/gfx/mac/io_surface.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/apple/mach_logging.h"
+#include "base/apple/scoped_mach_port.h"
 #include "base/bits.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
@@ -78,11 +79,7 @@ namespace internal {
 
 // static
 mach_port_t IOSurfaceMachPortTraits::Retain(mach_port_t port) {
-  kern_return_t kr =
-      mach_port_mod_refs(mach_task_self(), port, MACH_PORT_RIGHT_SEND, 1);
-  MACH_LOG_IF(ERROR, kr != KERN_SUCCESS, kr)
-      << "IOSurfaceMachPortTraits::Retain mach_port_mod_refs";
-  return port;
+  return base::apple::RetainMachSendRight(port).release();
 }
 
 // static
