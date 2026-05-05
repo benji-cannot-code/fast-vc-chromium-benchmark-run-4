7dfc6aa88e8e2c156ee26671f12e82afd6f8645b
Edgar Aguilar <eledgar@google.com>
2026-05-05 09:01:49 -0700

    Update first_party_sets fuzzers for android
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how th jvm initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes
    
    Bug: 500525162
    Change-Id: I2865c728e5e6a4776fca0ffdb52cc8bf8f9d3aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796448
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625475}

--- chrome/browser/ash/smb_client/smb_url_fuzzer.cc
+++ chrome/browser/ash/smb_client/smb_url_fuzzer.cc
@@ -3,13 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ash/smb_client/smb_url.h"
+
 #include <stddef.h>
 #include <stdint.h>
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
 #include "base/logging.h"
-#include "chrome/browser/ash/smb_client/smb_url.h"
+#include "base/no_destructor.h"
 
 // This is a workaround for https://crbug.com/40546867.
 struct IcuEnvironment {
@@ -21,10 +23,9 @@ struct IcuEnvironment {
   base::AtExitManager at_exit_manager;
 };
 
-IcuEnvironment* env = new IcuEnvironment();
-
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static base::NoDestructor<IcuEnvironment> env;
   std::string input(reinterpret_cast<const char*>(data), size);
 
   ash::smb_client::SmbUrl url(input);
--- chrome/browser/first_party_sets/test/first_party_sets_overrides_policy_handler_fuzzer.cc
+++ chrome/browser/first_party_sets/test/first_party_sets_overrides_policy_handler_fuzzer.cc
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/first_party_sets/first_party_sets_overrides_policy_handler.h"
 
 #include <stdlib.h>
+
 #include <iostream>
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
 #include "base/json/json_reader.h"
+#include "base/no_destructor.h"
 #include "components/policy/core/browser/policy_error_map.h"
 #include "components/policy/core/common/chrome_schema.h"
 #include "components/policy/core/common/policy_map.h"
@@ -28,9 +30,8 @@ struct IcuEnvironment {
   base::AtExitManager at_exit_manager;
 };
 
-IcuEnvironment* test_case = new IcuEnvironment();
-
 DEFINE_PROTO_FUZZER(const json_proto::JsonValue& json_value) {
+  static base::NoDestructor<IcuEnvironment> test_case;
   json_proto::JsonProtoConverter converter;
   std::string native_input = converter.Convert(json_value);
   FirstPartySetsOverridesPolicyHandler fps_handler(
