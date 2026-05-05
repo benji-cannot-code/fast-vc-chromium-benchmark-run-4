03923923feb78def98a787a0cb81b17b379439b2
Kay Lin <kaiyilin@google.com>
2026-05-04 22:35:06 -0700

    Site Search: Differentiate Tab/Space activation when deleting chip
    When exiting keyword mode via <backspace>, we need to restore a trailing
    space to the keyword ifhit's entered via space. This CL introduces this
    dynamic restore logic by expanding SiteSearchData to track the
    activation source.
    
    Bug: 509418050
    Change-Id: I8836b78ff2da5fdff7bb674a3b92fcb52086627c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7811435
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625202}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -1212,7 +1212,10 @@ class AutocompleteMediator
 
             if (templateUrl == null) return false;
             SiteSearchData data =
-                    new SiteSearchData(templateUrl.getKeyword(), templateUrl.getShortName());
+                    new SiteSearchData(
+                            templateUrl.getKeyword(),
+                            templateUrl.getShortName(),
+                            /* enteredViaSpace= */ true);
             onKeywordModeEntered(data);
             return true;
         }
