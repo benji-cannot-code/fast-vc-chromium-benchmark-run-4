8fc4dbb1933d3d3c8744034e7d2f4298eaa88ebc
Sebsg <sebsg@chromium.org>
2026-05-05 06:17:15 -0700

    ios: Fix flaky EG tests in ios/chrome/browser/metrics/model
    This CL resolves flakiness in EarlGrey tests on the iOS simulator.
    
    - Tab Usage Recorder: Set Cache-Control headers in test server and remove flaky removeBrowsingCache calls.
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: None
    Change-Id: I3bc72a2bb5cabf2bc5228f620436b4c50aa25937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7811802
    Commit-Queue: Sebastien S-G <sebsg@chromium.org>
    Auto-Submit: Sebastien S-G <sebsg@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625363}

--- ios/chrome/browser/metrics/model/tab_usage_recorder_egtest.mm
+++ ios/chrome/browser/metrics/model/tab_usage_recorder_egtest.mm
@@ -157,7 +157,6 @@ void SwitchToNormalMode() {
 
   chrome_test_util::GREYAssertErrorNil(
       [MetricsAppInterface setupHistogramTester]);
-  [ChromeEarlGrey removeBrowsingCache];
 
   _slowResponseDelay = base::TimeDelta();
 
@@ -179,6 +178,8 @@ void SwitchToNormalMode() {
           response->set_code(net::HTTP_OK);
           response->set_content_type("text/html");
           response->set_content("Slow Page");
+          response->AddCustomHeader("Cache-Control",
+                                    "no-cache, no-store, must-revalidate");
           return response;
         }
         auto it = responses->find(request.relative_url);
@@ -188,6 +189,8 @@ void SwitchToNormalMode() {
           response->set_code(net::HTTP_OK);
           response->set_content_type("text/html");
           response->set_content(it->second);
+          response->AddCustomHeader("Cache-Control",
+                                    "no-cache, no-store, must-revalidate");
           return response;
         }
         return nullptr;
@@ -464,7 +467,6 @@ void SwitchToNormalMode() {
 
   [ChromeEarlGrey openNewIncognitoTab];
   [ChromeEarlGrey evictOtherBrowserTabs];
-  [ChromeEarlGrey removeBrowsingCache];
 
   SwitchToNormalMode();
   // TODO(crbug.com/41271925): EarlGrey synchronize on some animations when a
@@ -506,7 +508,6 @@ void SwitchToNormalMode() {
   [ChromeEarlGrey openNewIncognitoTab];
   [ChromeEarlGrey evictOtherBrowserTabs];
 
-  [ChromeEarlGrey removeBrowsingCache];
 
   SwitchToNormalMode();
   // Wait for the page starting to load. It is possible that the page finish
@@ -547,7 +548,7 @@ void SwitchToNormalMode() {
   [ChromeEarlGrey openNewIncognitoTab];
   [ChromeEarlGrey evictOtherBrowserTabs];
 
-  [ChromeEarlGrey removeBrowsingCache];
+
   SwitchToNormalMode();
   // Wait for the page starting to load. It is possible that the page finish
   // loading before this test. In that case the wait will timeout. Ignore the
