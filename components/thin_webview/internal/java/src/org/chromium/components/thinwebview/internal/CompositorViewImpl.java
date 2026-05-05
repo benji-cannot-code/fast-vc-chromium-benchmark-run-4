daa7c0ff7cde678e4a28036aecfab136b1962a48
Fiaz Muhammad <mfiaz@google.com>
2026-05-05 12:26:52 -0700

    [CompositorView] Fix Surface Leak
    - Release the surface when the SurfaceTexture is destroyed.
    - Prevents an existing resource leak.
    
    Change-Id: I1a24e74c38838da578d509fc6392864a02f5c865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817956
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625641}

--- components/thin_webview/internal/java/src/org/chromium/components/thinwebview/internal/CompositorViewImpl.java
+++ components/thin_webview/internal/java/src/org/chromium/components/thinwebview/internal/CompositorViewImpl.java
@@ -19,6 +19,7 @@ import org.jni_zero.JNINamespace;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.thinwebview.CompositorView;
 import org.chromium.components.thinwebview.ThinWebViewConstraints;
 import org.chromium.ui.base.WindowAndroid;
@@ -155,6 +156,8 @@ public class CompositorViewImpl implements CompositorView {
         TextureView textureView = new TextureView(mContext);
         textureView.setSurfaceTextureListener(
                 new TextureView.SurfaceTextureListener() {
+                    private @Nullable Surface mSurface;
+
                     @Override
                     public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {}
 
@@ -162,6 +165,7 @@ public class CompositorViewImpl implements CompositorView {
                     public void onSurfaceTextureSizeChanged(
                             SurfaceTexture surfaceTexture, int width, int height) {
                         if (mNativeCompositorViewImpl == 0) return;
+                        assert textureView.getSurfaceTexture() == surfaceTexture;
                         CompositorViewImplJni.get()
                                 .surfaceChanged(
                                         mNativeCompositorViewImpl,
@@ -169,11 +173,16 @@ public class CompositorViewImpl implements CompositorView {
                                         width,
                                         height,
                                         false,
-                                        new Surface(surfaceTexture));
+                                        getSurface());
                     }
 
                     @Override
                     public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                        assert textureView.getSurfaceTexture() == surfaceTexture;
+                        if (mSurface != null) {
+                            mSurface.release();
+                            mSurface = null;
+                        }
                         if (mNativeCompositorViewImpl == 0) return false;
                         CompositorViewImplJni.get().surfaceDestroyed(mNativeCompositorViewImpl);
                         return false;
@@ -183,6 +192,7 @@ public class CompositorViewImpl implements CompositorView {
                     public void onSurfaceTextureAvailable(
                             SurfaceTexture surfaceTexture, int width, int height) {
                         if (mNativeCompositorViewImpl == 0) return;
+                        assert textureView.getSurfaceTexture() == surfaceTexture;
                         CompositorViewImplJni.get().surfaceCreated(mNativeCompositorViewImpl);
                         CompositorViewImplJni.get()
                                 .surfaceChanged(
@@ -191,7 +201,14 @@ public class CompositorViewImpl implements CompositorView {
                                         width,
                                         height,
                                         false,
-                                        new Surface(surfaceTexture));
+                                        getSurface());
+                    }
+
+                    private Surface getSurface() {
+                        if (mSurface == null) {
+                            mSurface = new Surface(textureView.getSurfaceTexture());
+                        }
+                        return mSurface;
                     }
                 });
         return textureView;
