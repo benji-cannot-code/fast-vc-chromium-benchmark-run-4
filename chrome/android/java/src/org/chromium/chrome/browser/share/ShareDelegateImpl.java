21b1b705546a2e0791d17662d4670520f2df695a
Jenna Himawan <jhimawan@google.com>
2026-05-04 22:37:30 -0700

    Copy the full link when sharing in Chrome for Android
    Demo: https://screencast.googleplex.com/cast/NTg4NzA2NjM5ODI2MTI0OHxlNWE2M2JiNi02Yw
    
    Bug: 503578717
    Change-Id: I76666a0c61b6c26d4abc422bf4859e5a038d52bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808037
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625206}

--- chrome/android/java/src/org/chromium/chrome/browser/share/ShareDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/share/ShareDelegateImpl.java
@@ -19,6 +19,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
 import org.chromium.base.DeviceInfo;
+import org.chromium.base.FeatureList;
 import org.chromium.base.ResettersForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.supplier.MonotonicObservableSupplier;
@@ -28,6 +29,7 @@ import org.chromium.chrome.browser.data_sharing.DataSharingTabManager;
 import org.chromium.chrome.browser.device_lock.DeviceLockActivityLauncherImpl;
 import org.chromium.chrome.browser.enterprise.util.DataProtectionBridge;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.history_clusters.HistoryClustersTabHelper;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.offlinepages.OfflinePageUtils;
@@ -370,6 +372,10 @@ public class ShareDelegateImpl implements ShareDelegate {
     @VisibleForTesting
     static String getUrlToShare(GURL visibleUrl, @Nullable GURL canonicalUrl) {
         if (PdfUtils.isDownloadedPdf(visibleUrl.getSpec())) return "";
+        if (FeatureList.isNativeInitialized()
+                && ChromeFeatureList.isEnabled(ChromeFeatureList.ANDROID_SHARE_FULL_LINK)) {
+            return visibleUrl.getSpec();
+        }
         if (canonicalUrl == null || canonicalUrl.isEmpty()) {
             return visibleUrl.getSpec();
         }
