d69512faf9a2c47af730ebacb0d72b00a08de6c4
Maya Jelonkiewicz <mjel@google.com>
2026-05-06 05:28:26 -0700

    Remove launched feature AndroidAudioDeviceListener
    Also removes the associated flag.
    
    Bug: 468998638
    Bug: 509844760
    Change-Id: Ic02baba2b12d7bf0d9fe6b9d7a8b2b03f3536b7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816735
    Commit-Queue: Maya Jelonkiewicz <mjel@google.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626072}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11712,10 +11712,6 @@ const FeatureEntry kFeatureEntries[] = {
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_ANDROID)
-    {"android-audio-device-listener",
-     flag_descriptions::kAndroidAudioDeviceListenerName,
-     flag_descriptions::kAndroidAudioDeviceListenerDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(features::kAndroidAudioDeviceListener)},
     {"android-block-misspelling-suggestion-span-in-composition-mode",
      flag_descriptions::
          kAndroidBlockMisspellingSuggestionSpanInCompositionModeName,
