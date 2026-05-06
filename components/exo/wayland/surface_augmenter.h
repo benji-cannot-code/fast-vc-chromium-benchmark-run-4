a6bb28f7ee9ba9d5f5440796d9779e8bc4322703
Andrew Paseltiner <apaseltiner@chromium.org>
2026-05-06 05:32:56 -0700

    exo: fix heap OOB read in surface_augmenter
    This adds size validation to wl_array arguments in the surface_augmenter
    extension to prevent reading past the end of the provided data.
    
    Fixed: 502761571
    Change-Id: Ie99f5f939e56ba8e3048bdbbb39f43cf118a49ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817199
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626079}

--- components/exo/wayland/surface_augmenter.h
+++ components/exo/wayland/surface_augmenter.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace exo::wayland {
 
-// version: 12
+// version: 13
 constexpr uint32_t kSurfaceAugmenterVersion =
-    AUGMENTED_SURFACE_SET_FRAME_TRACE_ID_SINCE_VERSION + 1;
+    AUGMENTED_SURFACE_SET_FRAME_TRACE_ID_SINCE_VERSION + 2;
 
 void bind_surface_augmenter(wl_client* client,
                             void* data,
