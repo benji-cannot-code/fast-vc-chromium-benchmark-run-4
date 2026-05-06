97b15277aa9429357ebd326c3995bd587db3021b
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-05-05 18:33:48 -0700

    [composebox] Remove AddDriveContext method from SearchboxHandler.
    This change removes the AddDriveContext Mojo method and its
    implementations from ContextualTasksComposeboxHandler,
    ContextualSearchboxHandler, and the base SearchboxHandler interface. The
    corresponding test proxies are also updated.
    
    Bug: b:493561714
    Change-Id: I17c18016de6327a47cc57e208c1eb35331cdad85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808329
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625895}

--- chrome/test/data/webui/cr_components/searchbox/test_searchbox_browser_proxy.ts
+++ chrome/test/data/webui/cr_components/searchbox/test_searchbox_browser_proxy.ts
@@ -46,7 +46,6 @@ class FakePageHandler extends TestBrowserProxy implements PageHandlerInterface {
       'notifySessionAbandoned',
       'addFileContext',
       'addTabContext',
-      'addDriveContext',
       'onDriveUploadClicked',
       'deleteContext',
       'clearFiles',
@@ -201,11 +200,6 @@ class FakePageHandler extends TestBrowserProxy implements PageHandlerInterface {
 
   addFileContext(fileInfo: SelectedFileInfo, fileBytes: BigBuffer) {
     this.methodCalled('addFileContext', {fileInfo, fileBytes});
-    return Promise.resolve('');
-  }
-
-  addDriveContext(driveId: string, resourceKey: string, mimeType: string) {
-    this.methodCalled('addDriveContext', {driveId, resourceKey, mimeType});
     return Promise.resolve('');
   }
 
