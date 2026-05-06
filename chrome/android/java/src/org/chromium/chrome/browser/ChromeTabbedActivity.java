1a38479d317cbcc5f1d02a193c8f2c535943ec17
Brandon Fong <bjfong@google.com>
2026-05-05 18:09:34 -0700

    [tips] Remove the magic stack opt in promo for tips backend portion
    Remove the backend portion that suggests the tips opt in promo through
    segmentation for magic stack first.
    
    Bug: 508677848
    Change-Id: I2d781058fc67bc0f929ea262c88e2ed552d9f42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7818943
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625877}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1595,10 +1595,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
 
             initiateArchivedTabsAutoDeletePromoManager();
 
-            if (ChromeFeatureList.sAndroidTipsNotifications.isEnabled()) {
-                TipsUtils.registerTipsNotificationsModuleEnabledSettingsPref();
-            }
-
             if (ChromeFeatureList.isEnabled(ChromeFeatureList.CHROME_FINDS)) {
                 Profile profile = getProfileProviderSupplier().get().getOriginalProfile();
                 FindsService findsService = FindsService.getForProfile(profile);
--- components/segmentation_platform/embedder/home_modules/home_modules_card_registry_android_unittest.cc
+++ components/segmentation_platform/embedder/home_modules/home_modules_card_registry_android_unittest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/home_modules/tab_group_promo.h"
 #include "components/segmentation_platform/embedder/home_modules/tab_group_sync_promo.h"
 #include "components/segmentation_platform/embedder/home_modules/test_utils.h"
-#include "components/segmentation_platform/embedder/home_modules/tips_notifications_promo.h"
 #include "components/segmentation_platform/public/constants.h"
 #include "components/segmentation_platform/public/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -234,43 +233,6 @@ TEST_F(HomeModulesCardRegistryAndroidTest, TestHistorySyncPromoCardDisabled) {
   ExpectCardNotRegistered(
       registry_.get(), kHistorySyncPromo,
       {kHistorySyncPromoShownCount, "educational_tip_shown_count"});
-}
-
-// Tests that the Registry registers the TipsNotificationsPromo card when its
-// feature is enabled.
-TEST_F(HomeModulesCardRegistryAndroidTest,
-       TestTipsNotificationsPromoCardEnabled) {
-  feature_list_.InitWithFeatures({features::kAndroidTipsNotifications}, {});
-  registry_ = HomeModulesCardRegistry::Create(&profile_pref_service_,
-                                              &local_state_pref_service_);
-
-  EXPECT_GE(registry_->all_cards_input_size(), 3u);
-  ExpectCardRegistered(
-      registry_.get(), kTipsNotificationsPromo,
-      {"tips_notifications_promo_shown_count", "is_eligible_to_tips_opt_in",
-       "educational_tip_shown_count"});
-}
-
-// Tests that the Registry won't register the TipsNotificationsPromo card when
-// it is disabled because of user's impression history.
-TEST_F(HomeModulesCardRegistryAndroidTest,
-       TestTipsNotificationsPromoCardDisabled) {
-  feature_list_.InitWithFeatures({features::kAndroidTipsNotifications}, {});
-
-  // Simulate showing the card across enough sessions to reach its max limit.
-  for (int i = 0; i < kSingleEphemeralCardMaxImpressions; ++i) {
-    auto card =
-        std::make_unique<TipsNotificationsPromo>(&profile_pref_service_);
-    card->OnShow(&profile_pref_service_, &local_state_pref_service_);
-  }
-
-  registry_ = HomeModulesCardRegistry::Create(&profile_pref_service_,
-                                              &local_state_pref_service_);
-
-  ExpectCardNotRegistered(
-      registry_.get(), kTipsNotificationsPromo,
-      {"tips_notifications_promo_shown_count", "is_eligible_to_tips_opt_in",
-       "educational_tip_shown_count"});
 }
 
 }  // namespace segmentation_platform::home_modules
