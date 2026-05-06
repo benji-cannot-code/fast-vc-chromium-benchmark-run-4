bca55200ed9be2c506431f06656a91a2b735e0f7
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-05-05 17:34:52 -0700

    [composebox] Update OnDriveUploadClicked to return selected Drive files.
    This change modifies the OnDriveUploadClicked mojo method to be
    asynchronous and return an array of DriveFileData objects, representing
    the files selected by the user from a Drive picker. Mock data is
    currently returned from the handler. We perform error handling for the
    Drive files in the contextual_searchbox_handler and return the error, if
    any, back to the WebUI.
    
    Bug: b:508691598,b:508691750
    Change-Id: Id8daae79a1661dd821ccd879c8a9560e5c5d6196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797552
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625849}

--- ui/webui/resources/cr_components/composebox/common.ts
+++ ui/webui/resources/cr_components/composebox/common.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {ComposeboxContextAddedMethod} from '//resources/cr_components/search/constants.js';
 import {assertNotReachedCase} from '//resources/js/assert.js';
 import {loadTimeData} from '//resources/js/load_time_data.js';
+import type {DriveUploadError} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 import type {UnguessableToken} from '//resources/mojo/mojo/public/mojom/base/unguessable_token.mojom-webui.js';
 import type {Url} from '//resources/mojo/url/mojom/url.mojom-webui.js';
 
@@ -152,12 +153,20 @@ export class ComposeboxFile {
 export interface ComposeboxState {
   text: string;
   files: ContextualUpload[];
+  error?: DriveUploadError;
   mode: ToolMode;
   model: ModelMode;
 }
 
 export interface FileUpload {
   file: File;
+}
+
+export interface DriveUpload {
+  token: UnguessableToken;
+  mimeType: string;
+  fileName: string;
+  thumbnailUrl: string|null;
 }
 
 export enum TabUploadOrigin {
@@ -176,7 +185,7 @@ export interface TabUpload {
   origin: TabUploadOrigin;
 }
 
-export type ContextualUpload = TabUpload|FileUpload;
+export type ContextualUpload = TabUpload|FileUpload|DriveUpload;
 
 export enum GlifAnimationState {
   INELIGIBLE = 'ineligible',
