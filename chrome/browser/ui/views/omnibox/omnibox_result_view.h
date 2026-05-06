7038eaa7246d4303a17b345b07cb56e96fd9017a
Marlon Facey <mfacey@google.com>
2026-05-06 16:22:39 -0700

    [OB Simplification] Update Non-WebUI on hover show secondary text
    1. Implements showing secondary text on hover in the Views omnibox by
    setting the visibility of the description and separator view to true and
    setting the width to 0 in the result view layout.
    2. Show secondary text on focus in the WebUI omnibox.
    
    Non-WebUI: http://screencast/cast/NTE1MjEyNjkyODk0NTE1MnxmNmM1ZTE1NS0yZA
    WebUI: http://screencast/cast/NjIwNzY1MDU0MTg2MjkxMnwwOWYzYTQzMS1mZQ
    
    Bug: b:500437601
    Change-Id: I4bf43d54a4a95c1c59f1ad354b9915297f558632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817898
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626541}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.h
@@ -104,6 +104,7 @@ class OmniboxResultView : public views::View {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(OmniboxPopupViewViewsTest, DeleteSuggestion);
+  FRIEND_TEST_ALL_PREFIXES(OmniboxResultViewTest, ContextualSecondaryText);
 
   void OpenIphLink();
 
@@ -114,6 +115,10 @@ class OmniboxResultView : public views::View {
   void UpdateHoverState();
 
   void UpdateDividerLineVisibility();
+
+  // Sets the visibility of the secondary text (description) based on the
+  // current state. Only applies to contextual suggestions.
+  void UpdateSecondaryTextVisibility();
 
   // Sets the visibility of the |thumbs_up_button_| and |thumbs_down_button_|
   // based on the current state.
