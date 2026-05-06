44e7725ca2618c231f2de23b6a99ce7d9fde4d3a
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2026-05-06 13:47:37 -0700

    Fix crash in ContextualCueingHelper during WebContents destruction
    This change adds an override for WebContentsDestroyed() in
    ContextualCueingHelper to invalidate its weak pointers. This ensures
    that callbacks, such as the one from ContextualCueingPageData, do not
    run after the WebContentsImpl destructor body has finished but before
    the ContextualCueingHelper is destroyed. This prevents access to
    destroyed WebContents members during the subsequent destruction of the
    frame tree.
    
    **This fix was generated by the Crash Autofix pipeline for the crash http://crash/8de3e3887af913b6. Please see go/crash-autofix for more details.**
    
    Bug: 510350777
    Change-Id: Ia6e02939e0deb11ab4d3223ea6e556c7944bf482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7823082
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Sana Akbani <sanaakbani@google.com>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626437}

--- chrome/browser/glic/suggestions/contextual_cueing_helper.h
+++ chrome/browser/glic/suggestions/contextual_cueing_helper.h
@@ -48,6 +48,7 @@ class ContextualCueingHelper
   void PrimaryMainDocumentElementAvailable() override;
   void OnFirstContentfulPaintInPrimaryMainFrame() override;
   void DocumentOnLoadCompletedInPrimaryMainFrame() override;
+  void WebContentsDestroyed() override;
 
   // Returns when the last primary main frame navigation was committed if the
   // navigation was a same document navigation.
