a5212254ecba1952b002367a2a68e51e3fd9f4cf
Hitarth Kothari <hitarthkothari@google.com>
2026-05-06 08:30:44 -0700

    Reland "[TabBottomSheet] Ensure only one sheet observer per coordinator"
    This reverts commit bf2da4f14343055840eb45a64cde77a4f3e0b6a3.
    
    Reason for revert: Not the cause
    
    Original change's description:
    > Revert "[TabBottomSheet] Ensure only one sheet observer per coordinator"
    >
    > This reverts commit dcaaa5e1b95326801cce50db6c0b168418c82581.
    >
    > Reason for revert: Suspecting this change is causing TabBottomSheetManagerTest#testDetachNativeInterfaceDelegate to fail on ci / android-arm64-tests: https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-tests/26260/test-results?q=ExactID%3A%3A%2F%2Fchrome%2Fandroid%5C%3Achrome_public_test_apk%21junit%3Aorg.chromium.chrome.browser.tab_bottom_sheet%3ATabBottomSheetManagerTest%23testDetachNativeInterfaceDelegate+VHash%3A38aa266d3739cbbf
    >
    > Original change's description:
    > > [TabBottomSheet] Ensure only one sheet observer per coordinator
    > >
    > > Bug: 469429565
    > > Change-Id: I34eabb69c4ca5eaa30700b00a5696eb72a3ba8c5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814640
    > > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > > Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    > > Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1625392}
    >
    > Bug: 469429565
    > Change-Id: I94550dcd4189fbe9a67139ce8b7d2a10613ac587
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7820159
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Luchen Peng <luchenpeng@google.com>
    > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > Commit-Queue: Luchen Peng <luchenpeng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1626114}
    
    Bug: 469429565
    Change-Id: Ibe06d1fa60bff5515b6a173f2d9f629d2f99dd99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7821919
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1626206}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
@@ -59,20 +59,6 @@ public class TabBottomSheetCoordinator {
         void onBottomSheetOpened(boolean isExpanded);
     }
 
-    private final ComponentCallbacks mComponentsCallbacks =
-            new ComponentCallbacks() {
-                @Override
-                public void onConfigurationChanged(Configuration configuration) {
-                    Log.i(TAG, "onConfigurationChanged: isShowing = " + mIsShowingTabBottomSheet);
-                    if (mIsShowingTabBottomSheet) {
-                        mExpectingLayoutChange = true;
-                    }
-                }
-
-                @Override
-                public void onLowMemory() {}
-            };
-
     private final GestureDetector mGestureDetector;
     private final GestureDetector.SimpleOnGestureListener mGestureListener =
             new GestureDetector.SimpleOnGestureListener() {
@@ -154,6 +140,7 @@ public class TabBottomSheetCoordinator {
     private @Nullable SheetEventsCallback mSheetEventsCallback;
     private @Nullable TabBottomSheetContent mSheetContent;
     private @Nullable BottomSheetObserver mSheetObserver;
+    private @Nullable ComponentCallbacks mComponentsCallbacks;
     private @Nullable PropertyModelChangeProcessor mViewBinder;
     private @Nullable View mContentView;
 
@@ -251,10 +238,14 @@ public class TabBottomSheetCoordinator {
                         }
                     });
 
-            mSheetObserver = buildBottomSheetObserver();
-            mBottomSheetController.addObserver(mSheetObserver);
-
-            mContext.registerComponentCallbacks(mComponentsCallbacks);
+            if (mSheetObserver == null) {
+                mSheetObserver = buildBottomSheetObserver();
+                mBottomSheetController.addObserver(mSheetObserver);
+            }
+            if (mComponentsCallbacks == null) {
+                mComponentsCallbacks = buildComponentsCallback();
+                mContext.registerComponentCallbacks(mComponentsCallbacks);
+            }
 
             if (mKeyboardVisibilityListener == null) {
                 mKeyboardVisibilityListener = buildKeyboardVisibilityListener();
@@ -333,8 +324,10 @@ public class TabBottomSheetCoordinator {
             mBottomSheetController.removeObserver(mSheetObserver);
             mSheetObserver = null;
         }
-
-        mContext.unregisterComponentCallbacks(mComponentsCallbacks);
+        if (mComponentsCallbacks != null) {
+            mContext.unregisterComponentCallbacks(mComponentsCallbacks);
+            mComponentsCallbacks = null;
+        }
         stopObservingCompositorViewInteractions();
 
         if (mKeyboardVisibilityListener != null) {
@@ -358,6 +351,21 @@ public class TabBottomSheetCoordinator {
     }
 
     // Observer methods.
+    private ComponentCallbacks buildComponentsCallback() {
+        return new ComponentCallbacks() {
+            @Override
+            public void onConfigurationChanged(Configuration configuration) {
+                Log.i(TAG, "onConfigurationChanged: isShowing = " + mIsShowingTabBottomSheet);
+                if (mIsShowingTabBottomSheet) {
+                    mExpectingLayoutChange = true;
+                }
+            }
+
+            @Override
+            public void onLowMemory() {}
+        };
+    }
+
     private BottomSheetObserver buildBottomSheetObserver() {
         return new EmptyBottomSheetObserver() {
             @Override
