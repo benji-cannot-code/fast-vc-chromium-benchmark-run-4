a725ca4ce56cbf566b61391e359d0a3a012d3e28
Peter Conn <peconn@chromium.org>
2026-05-06 06:05:11 -0700

    Reset observer when we stop net logs.
    If AwNetLogsConnection::stopNetLogs() is called, we stop the observer,
    but don't null out the pointer. This means that when the destructor is
    called, we attempt to stop the observer again, which isn't supported.
    
    Fixed: 508265342
    Change-Id: I1588ed30fde6499f0c19c4dd2b3896670e53c6b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817482
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626104}

--- android_webview/browser/aw_net_logs_connection.cc
+++ android_webview/browser/aw_net_logs_connection.cc
@@ -50,6 +50,7 @@ void AwNetLogsConnection::stopNetLogs() {
     return;
   }
   aw_net_log_observer_->StopObserving(nullptr, base::OnceClosure());
+  aw_net_log_observer_.reset();
 }
 
 AwNetLogsConnection* GetInstance() {
