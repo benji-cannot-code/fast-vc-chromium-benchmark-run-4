ef0f9933c37df3ad46df672acda786a84e5b85d2
Radu Nitescu <radunitescu@google.com>
2026-05-06 07:48:54 -0700

    [iOS] Move composebox metrics recorded to the shared location
    Bypass-Check-License: Mechanical move
    Change-Id: I16bf5391fc189faf986c8a01f946acb9d2ff6a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816844
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626177}

--- ios/chrome/browser/composebox/shared/metrics/composebox_metrics_recorder_unittest.mm
+++ ios/chrome/browser/composebox/shared/metrics/composebox_metrics_recorder_unittest.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/composebox/ui/composebox_metrics_recorder.h"
+#import "ios/chrome/browser/composebox/shared/metrics/composebox_metrics_recorder.h"
 
 #import "base/test/metrics/histogram_tester.h"
 #import "testing/gtest/include/gtest/gtest.h"
