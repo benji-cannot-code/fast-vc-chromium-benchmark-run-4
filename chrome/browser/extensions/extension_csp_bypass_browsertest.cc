ccabbe8c10d0700875d75de1339cb3ca8e76a298
James Cook <jamescook@chromium.org>
2026-05-05 18:39:06 -0700

    extensions: Port ExtensionCSPBypassTest to desktop Android
    We missed this one during initial bring-up of desktop Android. The test
    passes with minimal modifications. Also port the test extensions to
    manifest v3 while we're here.
    
    Bug: 469417243
    Change-Id: I04ff777dc9b9438c1357a93d8bb433f1028826b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7818749
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625902}

--- chrome/browser/extensions/extension_csp_bypass_browsertest.cc
+++ chrome/browser/extensions/extension_csp_bypass_browsertest.cc
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
@@ -55,11 +53,15 @@ class ExtensionCSPBypassTest : public ExtensionBrowserTest {
         base::DictValue()
             .Set("name", unique_name)
             .Set("version", "1")
-            .Set("manifest_version", 2)
-            .Set("web_accessible_resources", base::ListValue().Append("*"));
+            .Set("manifest_version", 3)
+            .Set("web_accessible_resources",
+                 base::ListValue().Append(
+                     base::DictValue()
+                         .Set("resources", base::ListValue().Append("*"))
+                         .Set("matches", base::ListValue().Append("*://*/*"))));
 
     if (all_urls_permission) {
-      manifest.Set("permissions", base::ListValue().Append("<all_urls>"));
+      manifest.Set("host_permissions", base::ListValue().Append("<all_urls>"));
     }
     if (is_component) {
       // LoadExtensionAsComponent requires the manifest to contain a key.
@@ -134,8 +136,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionCSPBypassTest, LoadWebAccessibleScript) {
   EXPECT_TRUE(CanLoadScript(ext_without_permission));
 
   // chrome-extension:-URLs can never bypass CSP in WebUI.
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), GURL(chrome::kChromeUIExtensionsURL)));
+  ASSERT_TRUE(NavigateToURL(GetActiveWebContents(),
+                            GURL(chrome::kChromeUIExtensionsURL)));
 
   EXPECT_FALSE(CanLoadScript(component_ext_with_permission));
   EXPECT_FALSE(CanLoadScript(component_ext_without_permission));
@@ -189,9 +191,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionCSPBypassTest, FrameAncestors) {
   std::string manifest = R"(
     {
       "name": "CSP frame-ancestors",
-      "manifest_version": 2,
+      "manifest_version": 3,
       "version": "0.1",
-      "browser_action": {
+      "action": {
        "default_popup": "popup.html"
       }
     }
