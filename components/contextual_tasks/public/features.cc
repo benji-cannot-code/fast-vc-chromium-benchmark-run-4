9efee68751396c9596f88023138ad7b7baa6a549
Jason Hu <hujasonx@google.com>
2026-05-06 01:39:04 -0700

    [context-input] Disable APC comparison for recontextualization
    APC comparing was over-triggering and not intuitive to users - this
    change makes APC comparisons for checking for recontextualization need
    dependent on a (default-false) flag, meaning they use viewport change
    checks only.
    
    Bug: b:505346672
    Change-Id: I3690815a08f8cedbbffb5f6a1daf2f8c9cf37d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814763
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625966}

--- components/contextual_tasks/public/features.cc
+++ components/contextual_tasks/public/features.cc
@@ -124,6 +124,11 @@ BASE_FEATURE(kContextualTasksCustomNlmUi, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kContextualTasksBackButtonExpandsSidePanel,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables the use of APC comparison for webpages in the recontextualization
+// flow.
+BASE_FEATURE(kContextualTasksWebpageApcComparison,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool GetIsContextualTasksUpdateModeOnNavigationEnabled() {
   return base::FeatureList::IsEnabled(kContextualTasksUpdateModelOnNavigation);
 }
@@ -576,6 +581,10 @@ ExpandButtonOption GetExpandButtonOption() {
 
 bool IsRoundedClipPathEnabled() {
   return base::FeatureList::IsEnabled(kContextualTasksRoundedClipPath);
+}
+
+bool GetIsWebpageApcComparisonEnabled() {
+  return base::FeatureList::IsEnabled(kContextualTasksWebpageApcComparison);
 }
 
 namespace flag_descriptions {
--- components/contextual_tasks/public/features.h
+++ components/contextual_tasks/public/features.h
@@ -92,6 +92,10 @@ BASE_DECLARE_FEATURE(kContextualTasksBackButtonExpandsSidePanel);
 // Enables lazy fetching of cluster info for multimodal queries.
 BASE_DECLARE_FEATURE(kContextualTasksLazyFetchClusterInfo);
 
+// Enables the use of APC comparison for webpages in the recontextualization
+// flow.
+BASE_DECLARE_FEATURE(kContextualTasksWebpageApcComparison);
+
 bool GetIsContextualTasksUpdateModeOnNavigationEnabled();
 
 bool GetIsContextualTasksPdfCitationsEnabled();
@@ -309,6 +313,9 @@ extern bool IsRoundedClipPathEnabled();
 
 // Returns whether the pin button in toolbar is enabled.
 extern bool IsContextualTasksPinButtonInToolbarEnabled();
+
+// Returns whether the webpage APC comparison is enabled.
+extern bool GetIsWebpageApcComparisonEnabled();
 
 namespace flag_descriptions {
 
