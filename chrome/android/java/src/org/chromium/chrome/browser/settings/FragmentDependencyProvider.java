dfc1dbf7156f1500657cf41278e38c6f8c268161
Josef Raska <josefraska@google.com>
2026-05-06 08:08:02 -0700

    [HoT][Signin] Sign-in promo to Autofill and Passwords.
    Signed on device, not in Chrome:
    https://screenshot.googleplex.com/87ain6QrF29snAy.png
    
    No account on device:
    https://screenshot.googleplex.com/tq6n6bH7XmWgNjP.png
    
    See go/hot-android-mvp-dd for details. Sign-in only supported. No-sync
    promo - product decision for HoT MVP. Seamless sign-in to be implemented
    later.
    
    SignInPromoPreference was added as we are inside a Preference screen. We
    don't want to show the preference in search - removed from index.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED Change covered by
    HomeOfTransactionsFragmentTest - tooling shows 0% - clearly incorrect.
    
    Bug: 482994749
    Change-Id: Ie06448b034a5091370d7723e8e2f12654ab8a560
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797262
    Commit-Queue: Josef Raska <josefraska@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626188}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/FragmentDependencyProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/FragmentDependencyProvider.java
@@ -252,7 +252,12 @@ public class FragmentDependencyProvider extends FragmentManager.FragmentLifecycl
         }
         if (fragment instanceof HomeOfTransactionsFragment) {
             ((HomeOfTransactionsFragment) fragment)
-                    .setModalDialogManagerSupplier(mModalDialogManagerSupplier);
+                    .setDependencies(
+                            mModalDialogManagerSupplier,
+                            mWindowAndroidSupplier,
+                            mActivityResultTracker,
+                            mBottomSheetControllerSupplier,
+                            mSnackbarManagerSupplier);
         }
     }
 }
