18645d65abbc69808edccdd5c1dee8de79b33603
Mike Wasserman <msw@chromium.org>
2026-05-05 18:02:26 -0700

    Remove chrome://flags#translation-api-streaming-by-sentence
    This flag expired, and is enabled by default without known issues.
    The underlying base::Feature can be removed in a follow up change.
    
    Fixed: 509859940
    Change-Id: I3e8b39437f537e57065097c2709c39f97f26608c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7818650
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Isaac Ahouma <iahouma@google.com>
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Isaac Ahouma <iahouma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625872}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -139,7 +139,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/common/composebox_features.h"
 #include "components/omnibox/common/omnibox_feature_configs.h"
 #include "components/omnibox/common/omnibox_features.h"
-#include "components/on_device_translation/buildflags/buildflags.h"
 #include "components/open_from_clipboard/clipboard_recent_content_features.h"
 #include "components/optimization_guide/core/model_execution/model_execution_features.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
@@ -364,10 +363,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/suggestions/contextual_cueing_features.h"  // nogncheck
 #endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) ||
         // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_ANDROID)
-
-#if BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
-#include "components/on_device_translation/features.h"
-#endif  // BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
 
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 #include "components/enterprise/platform_auth/platform_auth_features.h"
@@ -10496,14 +10491,6 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAll,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillEnableCardInfoRuntimeRetrieval)},
-
-#if BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
-    {"translation-api-streaming-by-sentence",
-     flag_descriptions::kTranslationAPIStreamingBySentenceName,
-     flag_descriptions::kTranslationAPIStreamingBySentenceDescription,
-     kOsDesktop,
-     FEATURE_VALUE_TYPE(on_device_translation::kTranslateStreamingBySentence)},
-#endif  // BUILDFLAG(ENABLE_ON_DEVICE_TRANSLATION)
 
     {"password-form-grouped-affiliations",
      flag_descriptions::kPasswordFormGroupedAffiliationsName,
