c5a78243c8f926c4fffe32bd0070cdf970e16045
Matt Reynolds <mattreynolds@google.com>
2026-05-06 16:00:07 -0700

    battery: Asynchronously check battery status on macOS
    base::features::kReducePPMs controls several fixes to
    egregious performance problems in order to measure their impact,
    including a fix for battery status change events to check the
    battery state asynchronously. This CL removes the flag guarding
    so battery state is always checked asynchronously on macOS. This
    fixes a bug where synchronously accessing the battery state on
    macOS could block the UI thread.
    
    Bug: 429980688
    Change-Id: Ibaf5afd1aa8b3d6693bf62bb37de7eab5855beb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814760
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626525}

--- services/device/battery/battery_status_manager_mac.cc
+++ services/device/battery/battery_status_manager_mac.cc
@@ -10,13 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <IOKit/ps/IOPowerSources.h>
 
 #include <memory>
-#include <optional>
 #include <vector>
 
 #include "base/apple/foundation_util.h"
 #include "base/apple/scoped_cftyperef.h"
-#include "base/feature_list.h"
-#include "base/features.h"
 #include "base/functional/bind.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -114,16 +111,12 @@ void FetchBatteryStatus(CFDictionaryRef description,
   }
 }
 
-std::vector<mojom::BatteryStatus> GetInternalBatteriesStates(bool may_block) {
+std::vector<mojom::BatteryStatus> GetInternalBatteriesStates() {
   std::vector<mojom::BatteryStatus> internal_sources;
 
-  // This function is known to block but cannot always be tagged as such right
-  // now because it might run on the UI thread. When running on the ThreadPool
-  // though it should be appropriately tagged.
-  std::optional<base::ScopedBlockingCall> scoped_blocking_call;
-  if (may_block) {
-    scoped_blocking_call.emplace(FROM_HERE, base::BlockingType::MAY_BLOCK);
-  }
+  // IOPSCopyPowerSourcesInfo is known to block.
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
 
   base::apple::ScopedCFTypeRef<CFTypeRef> info(IOPSCopyPowerSourcesInfo());
   base::apple::ScopedCFTypeRef<CFArrayRef> power_sources_list(
@@ -173,12 +166,8 @@ void HandleNewBatteryStatus(const BatteryCallback& callback,
 void OnBatteryStatusChangedAsync(const BatteryCallback& callback) {
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock()},
-      base::BindOnce(&GetInternalBatteriesStates, true),
+      base::BindOnce(&GetInternalBatteriesStates),
       base::BindOnce(&HandleNewBatteryStatus, callback));
-}
-
-void OnBatteryStatusChanged(const BatteryCallback& callback) {
-  HandleNewBatteryStatus(callback, GetInternalBatteriesStates(false));
 }
 
 class BatteryStatusObserver {
@@ -221,11 +210,7 @@ class BatteryStatusObserver {
 
  private:
   static void CallOnBatteryStatusChanged(void* callback) {
-    if (base::FeatureList::IsEnabled(base::features::kReducePPMs)) {
-      OnBatteryStatusChangedAsync(*static_cast<BatteryCallback*>(callback));
-    } else {
-      OnBatteryStatusChanged(*static_cast<BatteryCallback*>(callback));
-    }
+    OnBatteryStatusChangedAsync(*static_cast<BatteryCallback*>(callback));
   }
 
   BatteryCallback callback_;
