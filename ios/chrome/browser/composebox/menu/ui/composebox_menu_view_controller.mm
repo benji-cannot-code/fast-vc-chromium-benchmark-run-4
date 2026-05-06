fd049c7164fa5aebc1e12c6a1c1c6d87ba64371f
christianxu <christianxu@chromium.org>
2026-05-06 05:58:00 -0700

    [iOS][ComposeboxMenu] Use AlignValueToPixel for attachment width
    - Use AlignValueToPixel on division result to avoid fractional layout issues.
    
    Bug: None
    Change-Id: I832cb1b4461cffca8b72d65f2c06cc684b706906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7818057
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626095}

--- ios/chrome/browser/composebox/menu/ui/composebox_menu_view_controller.mm
+++ ios/chrome/browser/composebox/menu/ui/composebox_menu_view_controller.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
@@ -333,7 +334,8 @@ UIImage* IconForModel(ComposeboxModelOption option) {
     CGFloat availableWidth = containerWidth - kAttachmentSectionInsets.leading -
                              kAttachmentSectionInsets.trailing;
     CGFloat totalSpacing = (itemsCount - 1) * kAttachmentItemSpacing;
-    CGFloat itemWidth = (availableWidth - totalSpacing) / itemsCount;
+    CGFloat itemWidth =
+        AlignValueToPixel((availableWidth - totalSpacing) / itemsCount);
 
     NSCollectionLayoutSize* itemSize = [NSCollectionLayoutSize
         sizeWithWidthDimension:[NSCollectionLayoutDimension
