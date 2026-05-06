7bf96a6f959b0dea28563236aad6a3efedc4b34c
Chirag Arora <heychirag@google.com>
2026-05-06 10:27:50 -0700

    overload ChangeMetricsReportingState to support MetricsReportingLevel
    This commit overloads `ChangeMetricsReportingState` to support setting
    `MetricsReportingLevel`. This overloaded method will be called from
    sites where `ChangeMetricsReportingState` is currently being called if
    `MetricsReportingChoiceService::ShouldUseMetricsConsentRestructure` is
    true.
    
    Bug: b:496476603
    Change-Id: Ia392df8369f36a5cbd704d340032321300b00ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7813599
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626293}

--- components/metrics/metrics_reporting_choice_service.h
+++ components/metrics/metrics_reporting_choice_service.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_METRICS_METRICS_REPORTING_CHOICE_SERVICE_H_
 #define COMPONENTS_METRICS_METRICS_REPORTING_CHOICE_SERVICE_H_
 
+#include "components/metrics/metrics_reporting_level.h"
+
 class PrefRegistrySimple;
 class PrefService;
 
@@ -38,6 +40,10 @@ class MetricsReportingChoiceService {
   // Returns true if the metrics consent restructure feature is enabled.
   static bool IsMetricsConsentRestructureFeatureEnabled(
       const PrefService* local_state);
+
+  // Sets the metrics reporting level to |level|.
+  static void SetMetricsReportingLevel(PrefService* local_state,
+                                       MetricsReportingLevel level);
 
   // Returns true if the metrics consent restructure should be used. This is
   // different from IsMetricsConsentRestructureFeatureEnabled() in that it also
