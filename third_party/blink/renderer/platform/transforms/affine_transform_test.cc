5a66cdd1e315bc08b18dd64d3269199165e68a2e
Xiang Ji <jxiang@google.com>
2026-05-06 07:20:23 -0700

    Fix unsafe buffer usage in AffineTransformTest
    Replaced unsafe String::Format with std::stringstream to fix unsafe
    buffer error and string not guaranteed to be null-terminated error.
    
    Initial patchset generated by headless gemini-cli using:
    //agents/prompts/projects/spanification/run.py
    
    Bug: 435317390
    Change-Id: Iff12548e01c2b6c702b867cabdd66f2e9bf5a7f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812245
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626159}

--- third_party/blink/renderer/platform/transforms/affine_transform_test.cc
+++ third_party/blink/renderer/platform/transforms/affine_transform_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/transforms/affine_transform.h"
 
 #include <array>
+#include <sstream>
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -157,8 +158,9 @@ TEST(AffineTransformTest, ValidRangedMatrix) {
     };
 
     auto test = [&](const AffineTransform& m) {
-      SCOPED_TRACE(UNSAFE_TODO(String::Format(
-          "m: %s factor: %lg", m.ToString().Utf8().data(), factor)));
+      std::stringstream ss;
+      ss << "m: " << m << " factor: " << factor;
+      SCOPED_TRACE(ss.str());
       auto p = m.MapPoint(gfx::PointF(factor, factor));
       EXPECT_TRUE(is_valid_point(p)) << p.ToString();
       auto r = m.MapRect(gfx::RectF(factor, factor, factor, factor));
