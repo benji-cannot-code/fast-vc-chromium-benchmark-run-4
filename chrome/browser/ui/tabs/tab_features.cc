f04931481fcf3fe506dd6a4cdc43754ce04d3460
Quinton Lucyk <qlucyk@chromium.org>
2026-05-06 08:22:23 -0700

    Add boilerplate for search promotion manager and observer.
    Setting up the infrastructure that will be filled out in subsequent
    CLs to support promoting search related features and functionalities
    that enhance the user experience.
    
    Bug: 467255671
    Change-Id: Ie53212fa25b84cb74a48d6dee6bbd12d8b3de959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808624
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Quinton Lucyk <qlucyk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626201}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -109,6 +109,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/wallet/chrome_walletable_pass_client.h"
 #include "components/record_replay/core/common/record_replay_features.h"
 #endif
+
+#if BUILDFLAG(IS_WIN)
+#include "chrome/browser/ui/search_promotion/search_promotion_navigation_observer.h"
+#include "components/feature_engagement/public/feature_constants.h"
+#endif
 #include "chrome/browser/glic/browser_ui/glic_tab_indicator_helper.h"
 #include "chrome/browser/glic/glic_selection_observer.h"
 #include "chrome/browser/glic/public/features.h"
@@ -500,6 +505,15 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
     contextual_tasks_tab_visit_tracker_ =
         std::make_unique<contextual_tasks::ContextualTasksTabVisitTracker>(
             tab.GetContents());
+  }
+#endif
+
+#if BUILDFLAG(IS_WIN)
+  if (base::FeatureList::IsEnabled(
+          feature_engagement::kIPHSearchPromotionFeature)) {
+    search_promotion_navigation_observer_ =
+        GetUserDataFactory().CreateInstance<SearchPromotionNavigationObserver>(
+            tab, tab);
   }
 #endif
 
