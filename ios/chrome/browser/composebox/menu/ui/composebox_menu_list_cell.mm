9e39b09fd764c94b4b9a8ffa6352ad49ae7f5b1d
christianxu <christianxu@chromium.org>
2026-05-06 06:25:34 -0700

    [iOS][ComposeboxMenu] Add custom list cell to fix layout loop
    When scrolling UICollectionViewListCell returns fractional different
    height for the preferredLayoutAttributesFittingAttributes, this causes
    the collection view to loop during layout passes unable to settle on a
    specific height and crashes the app.
    
    This CL wraps UICollectionViewListCell with ComposeboxMenuListCell and
    ceils the returned preferredLayoutAttributesFittingAttributes to break
    the loop.
    
    Fixed: 510228106
    Change-Id: Ifbb9d9f848a9ed59c788b39b4eb87ba86920c9e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817483
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626118}

---
+++ ios/chrome/browser/composebox/menu/ui/composebox_menu_list_cell.mm
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/composebox/menu/ui/composebox_menu_list_cell.h"
+
+@implementation ComposeboxMenuListCell
+
+- (UICollectionViewLayoutAttributes*)preferredLayoutAttributesFittingAttributes:
+    (UICollectionViewLayoutAttributes*)layoutAttributes {
+  UICollectionViewLayoutAttributes* attributes =
+      [super preferredLayoutAttributesFittingAttributes:layoutAttributes];
+  CGRect frame = attributes.frame;
+  // Round up to nearest point to break fractional rounding loops that can
+  // cause recursive layout crashes.
+  frame.size.height = ceil(frame.size.height);
+  attributes.frame = frame;
+  return attributes;
+}
+
+@end
