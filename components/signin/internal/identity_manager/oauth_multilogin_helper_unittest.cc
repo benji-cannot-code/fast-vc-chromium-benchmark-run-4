b581159c8b9b604acce87c7cfb7bd97236e54b75
Ali Hijazi <ahijazi@chromium.org>
2026-05-06 02:30:04 -0700

    [DBSC] Add partition suffix to SessionCreationError metrics
    This CL adds `{PartitionSuffix}` token variants to the
    `Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError`
    histogram.
    
    Bug: 506123093
    Change-Id: Idaa7732ddf28a35d88705a9ad102f688b40b24b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814095
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626002}

--- components/signin/internal/identity_manager/oauth_multilogin_helper_unittest.cc
+++ components/signin/internal/identity_manager/oauth_multilogin_helper_unittest.cc
@@ -1617,6 +1617,10 @@ TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledTest,
       "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError",
       net::device_bound_sessions::SessionError::ErrorType::kSuccess,
       /*expected_bucket_count=*/1);
+  histogram_tester.ExpectUniqueSample(
+      "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError.Test",
+      net::device_bound_sessions::SessionError::ErrorType::kSuccess,
+      /*expected_bucket_count=*/1);
 }
 
 TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledTest,
@@ -1912,6 +1916,16 @@ TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledTest,
           base::Bucket(net::device_bound_sessions::SessionError::ErrorType::
                            kInvalidSessionId,
                        1)));
+  EXPECT_THAT(
+      histogram_tester.GetAllSamples(
+          "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError."
+          "Test"),
+      UnorderedElementsAre(
+          base::Bucket(
+              net::device_bound_sessions::SessionError::ErrorType::kSuccess, 1),
+          base::Bucket(net::device_bound_sessions::SessionError::ErrorType::
+                           kInvalidSessionId,
+                       1)));
 }
 
 TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledTest,
@@ -1963,6 +1977,9 @@ TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledTest,
       /*expected_bucket_count=*/1);
   histogram_tester.ExpectTotalCount(
       "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError",
+      /*expected_count=*/0);
+  histogram_tester.ExpectTotalCount(
+      "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError.Test",
       /*expected_count=*/0);
 }
 
@@ -2041,6 +2058,9 @@ TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledTest,
   histogram_tester.ExpectTotalCount(
       "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError",
       /*expected_count=*/0);
+  histogram_tester.ExpectTotalCount(
+      "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError.Test",
+      /*expected_count=*/0);
 }
 
 TEST_F(
@@ -2094,6 +2114,9 @@ TEST_F(
       /*expected_count=*/0);
   histogram_tester.ExpectTotalCount(
       "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError",
+      /*expected_count=*/0);
+  histogram_tester.ExpectTotalCount(
+      "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError.Test",
       /*expected_count=*/0);
 }
 
@@ -2157,6 +2180,10 @@ TEST_F(OAuthMultiloginHelperStandardBoundSessionsEnabledPrototypeDisabledTest,
       "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError",
       net::device_bound_sessions::SessionError::ErrorType::kSuccess,
       /*expected_bucket_count=*/1);
+  histogram_tester.ExpectUniqueSample(
+      "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError.Test",
+      net::device_bound_sessions::SessionError::ErrorType::kSuccess,
+      /*expected_bucket_count=*/1);
 }
 
 TEST_F(
@@ -2206,6 +2233,9 @@ TEST_F(
       /*expected_count=*/0);
   histogram_tester.ExpectTotalCount(
       "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError",
+      /*expected_count=*/0);
+  histogram_tester.ExpectTotalCount(
+      "Signin.DeviceBoundSessions.OAuthMultilogin.SessionCreationError.Test",
       /*expected_count=*/0);
 }
 
