7eb9b0f9b6ae0488e09a0ac309841e4b4eb14044
Andrea Orru <andreaorru@chromium.org>
2026-05-06 13:39:26 -0700

    [Extensions] Remove expired SW unregistration retry histogram
    Remove expired WorkerRegistrationRetryForUnregistrationAttemptsResult
    histogram. This was used to track retries when registration failed due
    to pending unregistration.
    
    Since the histogram is removed, add OnWorkerRegistrationDelayed to
    TestObserver to allow the test to verify that the registration was
    indeed delayed, without relying on the histogram.
    
    Fixed: 495424334
    Change-Id: Ie6dacdb7cdc588af6e916e51b339f6d6ebdc3417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816272
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626430}

--- chrome/browser/extensions/service_worker_registration_apitest.cc
+++ chrome/browser/extensions/service_worker_registration_apitest.cc
@@ -103,6 +103,8 @@ class ExtensionRegistrationAndUnregistrationWaiter
     WaitForWorkerUnregistrationAttemptCompleted();
   }
 
+  bool registration_delayed() const { return registration_delayed_; }
+
  private:
   void WaitForWorkerUnregistrationAttemptCompleted() {
     SCOPED_TRACE("Waiting for worker unregistration attempt to complete");
@@ -121,9 +123,16 @@ class ExtensionRegistrationAndUnregistrationWaiter
     }
   }
 
+  void OnWorkerRegistrationDelayed(const ExtensionId& extension_id) override {
+    if (extension_id == expected_extension_id_) {
+      registration_delayed_ = true;
+    }
+  }
+
   const ExtensionId expected_extension_id_;
   base::RunLoop registration_attempt_runloop;
   base::RunLoop unregistration_attempt_runloop;
+  bool registration_delayed_ = false;
 };
 
 }  // namespace
@@ -922,10 +931,7 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerRegistrationApiTest, ExtensionReinstall) {
               GetServiceWorkerRegistrationState(*reinstalled_extension));
   }
 
-  CheckBooleanHistogramCounts(
-      "Extensions.ServiceWorkerBackground."
-      "WorkerRegistrationRetryForUnregistrationAttemptsResult",
-      /*true_count=*/1, /*false_count=*/0, histogram_tester);
+  EXPECT_TRUE(registration_waiter2.registration_delayed());
 }
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
