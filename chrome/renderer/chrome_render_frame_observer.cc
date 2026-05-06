bf8097a66f6f4c3c62c52afaf0d83753daca95cd
Philip Pfaffe <pfaffe@chromium.org>
2026-05-06 11:02:41 -0700

    Report cross-document tool responses as toolResponded events
    This will work for actor-origin tool calls or when using
    modelContextTesting's explicit cross document result getter. It will not
    work automatically for the WebMCP.invokeTool command.
    
    Bug: 485810761
    Change-Id: Ie14ae544343aaa93b2d64d7af3336e49f1f2fc6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748304
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626329}

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -670,8 +670,10 @@ void ChromeRenderFrameObserver::StartActorJournal(
 }
 
 void ChromeRenderFrameObserver::GetCrossDocumentScriptToolResult(
+    const base::UnguessableToken& invocation_id,
     GetCrossDocumentScriptToolResultCallback callback) {
   render_frame()->GetWebFrame()->GetDocument().GetCrossDocumentScriptToolResult(
+      invocation_id,
       base::BindOnce(
           [](GetCrossDocumentScriptToolResultCallback cb,
              blink::WebString result) { std::move(cb).Run(result.Utf8()); },
--- third_party/blink/renderer/core/script_tools/model_context_testing.cc
+++ third_party/blink/renderer/core/script_tools/model_context_testing.cc
@@ -124,6 +124,7 @@ ScriptPromise<IDLString> ModelContextTesting::getCrossDocumentScriptToolResult(
   };
 
   model_context_->GetCrossDocumentScriptToolResult(
+      base::UnguessableToken::Create(),
       blink::BindOnce(callback, WrapPersistent(resolver)));
 
   return promise;
