2b847f6c92f8a0d45576d982baed13ae7f2b21f1
James Cook <jamescook@chromium.org>
2026-05-07 14:22:18 -0700

    extensions: Fix chrome.tabGroups.move() timeouts on desktop Android
    On Android, moving a tab group between windows is an asynchronous
    operation, as it needs to persist some data to disk before moving the
    tabs. TabGroupsMoveFunction handles this by returning RespondLater()
    and observing for an OnTabGroupCreated() event after the move.
    
    TabGroupsMoveFunction is a ref-counted object, but wasn't
    incrementing its ref-count in this situation. Thus the object would
    be deleted after RespondLater(), it would not observe the tab group
    created notification, and the JS API function would never return.
    
    The existing browser test for moving tab groups between windows didn't
    catch this because it created a TabGroupsMoveFunction object on the
    stack, so there were no ref-counts to maintain.
    
    Added a JS API test so ref-counts will be exercised like production.
    
    Note there is still a problem with the chrome.tabGroups.onRemoved
    JS notification, but that will be addressed later.
    
    Bug: 509581460
    Change-Id: I47614347b2e269d1a04b49d425387646bf4e69e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7821693
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627233}

---
+++ chrome/test/data/extensions/api_test/tab_groups/move_to_window/background.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  async function moveGroup() {
+    // Create other tabs in window 1 to add to a group.
+    await chrome.tabs.create({url: 'about:blank'});
+    await chrome.tabs.create({url: 'about:blank'});
+
+    // Get the tabs in window 1. There should now be three of them.
+    const tabs = await chrome.tabs.query({currentWindow: true});
+    chrome.test.assertEq(3, tabs.length);
+
+    // Create a tab group in window 1 with two tabs.
+    const groupId = await chrome.tabs.group({tabIds: [tabs[1].id, tabs[2].id]});
+
+    // Create a new window.
+    const window2 = await chrome.windows.create({url: 'about:blank'});
+
+    // Move the tab group to window 2 and wait for it to process.
+    await chrome.tabGroups.move(groupId, {windowId: window2.id, index: 0});
+
+    // Two tabs moved, so window 2 now has a total of 3 tabs.
+    const tabs2 = await chrome.tabs.query({windowId: window2.id});
+    chrome.test.assertEq(3, tabs2.length);
+
+    chrome.test.succeed();
+  },
+]);
