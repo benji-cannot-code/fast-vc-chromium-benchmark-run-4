9635d692f24f62005021b0c6dd2a5b941fd92898
Christian Dullweber <dullweber@chromium.org>
2026-05-07 10:42:17 -0700

    Gardener: Avoid timeout in PrefsInternalsTest.TestPrefsAreServed
    This test is sometimes timing out on force-renderer-accessibility
    bots. Avoid rendering the whole prefs page on these bots.
    
    Local runtime improved from 16s to 2s with
    --force-renderer-accessibility enabled.
    
    Bug: 417174864
    Change-Id: Iebc97915bf271893ca00fa1241b845755260a19f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827753
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627052}

--- chrome/browser/ui/webui/prefs_internals_browsertest.cc
+++ chrome/browser/ui/webui/prefs_internals_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "base/strings/stringprintf.h"
 #include "base/uuid.h"
 #include "chrome/browser/profiles/profile.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "ui/accessibility/accessibility_switches.h"
 #include "url/gurl.h"
 
 using PrefsInternalsTest = InProcessBrowserTest;
@@ -31,6 +33,13 @@ IN_PROC_BROWSER_TEST_F(PrefsInternalsTest, TestPrefsAreServed) {
 
   // First, check that navigation succeeds.
   GURL kUrl(content::GetWebUIURL(chrome::kChromeUIPrefsInternalsHost));
+  // We request only the specific pref we set to avoid loading the entire list
+  // of prefs on the accessibility bot, which can be very large and cause
+  // timeouts (see crbug.com/417174864).
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kForceRendererAccessibility)) {
+    kUrl = kUrl.Resolve(prefs::kHomePage);
+  }
   ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), kUrl));
   content::WebContents* web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
