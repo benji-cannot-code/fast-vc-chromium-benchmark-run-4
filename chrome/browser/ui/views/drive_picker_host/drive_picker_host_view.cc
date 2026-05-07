c03b12d31f87bf8ba526e9e3012c281676fe3996
Juliet Lévesque <julietlevesque@google.com>
2026-05-07 13:08:53 -0700

    [aim-drive] Show 3P Drive Picker in unstrusted WebUI
    Retrieve oathtoken, appId, and apiKey in the trusted WebUI to pass to
    the Untrusted WebUI to show the 3p Drive Picker. Follow up CLs will
    sanitize the returned data, and return the results on the resultHandler.
    
    Also add styling changes to make sure the drive picker appears as a
    floating dialog over the browser scrim. Allow lists the WebUI to make
    required network requests to communicate with the Drive Picker 3P API.
    
    Bug: b:497937568
    Change-Id: Ie7e8c907161ca8584ab96e7e94081ba26de8924e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805206
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627177}

--- chrome/browser/ui/views/drive_picker_host/drive_picker_host_view.cc
+++ chrome/browser/ui/views/drive_picker_host/drive_picker_host_view.cc
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/compositor/layer.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/view_utils.h"
-#include "ui/views/widget/widget.h"
 
 namespace {
 constexpr int kDefaultWidth = 800;
@@ -27,10 +27,18 @@ DrivePickerHostView::DrivePickerHostView(Profile* profile) {
   // before the WebUI content determines the final layout.
   SetPreferredSize(gfx::Size(kDefaultWidth, kDefaultHeight));
 
+  // Set the view to paint to a layer so that the view can be transparent over
+  // the web contents. This allows the web contents to appear like a floating
+  // dialog over the browser window.
+  SetPaintToLayer();
+  layer()->SetFillsBoundsOpaquely(false);
+  SetBackground(nullptr);
+  SetBorder(nullptr);
   SetLayoutManager(std::make_unique<views::FillLayout>());
   views::WebView* web_view =
       AddChildView(std::make_unique<views::WebView>(profile));
   view_tracker_.SetView(web_view);
+  web_view->SetBackground(nullptr);
 
   // Since the WebView was just created with a valid profile, GetWebContents()
   // is guaranteed to return a valid pointer. We use a CHECK here to assert
--- components/signin/public/base/oauth_consumer_registry.cc
+++ components/signin/public/base/oauth_consumer_registry.cc
@@ -328,6 +328,7 @@ constexpr char kActorLoginPermissionServiceName[] =
     "actor_login_permission_service";
 constexpr char kGapisServiceName[] = "gapis_service";
 constexpr char kOneTimeTokenServiceName[] = "one_time_token_service";
+constexpr char kDrivePickerHostName[] = "drive_picker_host";
 constexpr char kMultistepFilterName[] = "multistep_filter";
 }  // namespace
 
@@ -777,6 +778,10 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
       return OAuthConsumer(
           /*name=*/kSecureGatewayServiceName,
           /*scopes=*/{GaiaConstants::kSecureGatewayOAuth2Scope});
+    case OAuthConsumerId::kDrivePickerHost:
+      return OAuthConsumer(
+          /*name=*/kDrivePickerHostName,
+          /*scopes=*/{kDriveReadOnlyOAuth2Scope});
   }
 }
 
