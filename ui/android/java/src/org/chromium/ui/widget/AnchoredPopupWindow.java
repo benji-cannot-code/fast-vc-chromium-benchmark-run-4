01fd51e08074b182a1d3a785f8c1b15a20a4a5f9
Masa Fujita <massan@google.com>
2026-05-06 20:44:18 -0700

    AnchoredPopupWindow: Expose touchability
    This CL allows the users of AnchoredPopupWindow to set the touchability
    of the PopupWindow. We will use this in extension action hover cards.
    
    Bug: 423463764
    Change-Id: I54df55ffda95162eda732233e2b406b1295b64ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808827
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626696}

--- ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
+++ ui/android/java/src/org/chromium/ui/widget/AnchoredPopupWindow.java
@@ -287,6 +287,8 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
         private @StyleRes int mAnimationStyleId;
         private boolean mAnimateFromAnchor;
         private boolean mFocusable;
+        private boolean mTouchable;
+        private boolean mIsTouchableSet;
         private float mElevation;
         private boolean mTouchModal;
         private boolean mOutsideTouchable;
@@ -500,6 +502,15 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
         }
 
         /**
+         * @param touchable True if the popup is touchable, false otherwise.
+         */
+        public Builder setTouchable(boolean touchable) {
+            mTouchable = touchable;
+            mIsTouchableSet = true;
+            return this;
+        }
+
+        /**
          * @param elevation The elevation of the popup.
          */
         public Builder setElevation(float elevation) {
@@ -576,6 +587,9 @@ public class AnchoredPopupWindow implements OnTouchListener, RectProvider.Observ
         }
         setAnimateFromAnchor(builder.mAnimateFromAnchor);
         setFocusable(builder.mFocusable);
+        if (builder.mIsTouchableSet) {
+            mPopupWindow.setTouchable(builder.mTouchable);
+        }
         setElevation(builder.mElevation);
         setTouchModal(builder.mTouchModal);
         if (builder.mIsOutsideTouchableSet) {
