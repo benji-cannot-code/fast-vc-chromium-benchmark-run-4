ec61aa665678dea5c14905a49d8269b60a000a3b
Jonathan Lee <jonathanjlee@google.com>
2026-05-06 17:00:30 -0700

    Import wpt@05637f455968b011be7566e7e0a14e49ae00fe63
    https://github.com/web-platform-tests/wpt/compare/1903b4ef7...05637f455
    
    Using wpt-import in Chromium 2caa6ee5efe2a97f58eaa112e3d329b4ddf82c50.
    With Chromium commits locally applied on WPT:
      a9f55c6e86 "Fix WPT version of scroll-timeilne-snapshotting test"
      34d625da61 "[css-shapes] Implement polygon() round modifier"
      ecd37c5df7 "webrtc wpt: update insertable streams metadata tests"
      7ed6b2f1c8 "Implement new/revised popover=hint behavior"
      2cf839a799 "[CC Clip Paths] Invalidate correct element after [...]
      1165ff98d3 "Fix a bug in new popover=hint stacking behavior, and [...]
      43550d9e23 "Make view-timeline-inset viewport-size invariant"
      469d0dca8a "Fix importNode fallback registry not propagating to [...]
      2f850c71c3 "Fix insertAdjacentHTML and outerHTML using wrong [...]
      712f6f0b77 "Change two more popover=hint behaviors"
      9caf1b71c1 "Fix test timeline on Firefox"
      ebb761783b "Take fallback for unknown <attr-unit>s"
      e055df5001 "Disallow <url> type in attr() in the tests"
      2532f26fe2 "Update calculation for scroll timeline boundary alignment"
      62a64a8b3a "webrtc wpt: assert that stats trackIdentifier [...]
      4fa27907b0 "Add uneven longhand attributes parsing for view [...]
      c8ca97247e "Fix custom element registry preservation during tree [...]
      2d57121bac "Track whether a LanguageModel has initial context [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    antoniosartori@chromium.org, arthursonzogni@chromium.org:
      external/wpt/content-security-policy
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/editing
    foolip@chromium.org:
      external/wpt/fullscreen
    futhark@chromium.org, andruud@chromium.org:
      external/wpt/css/css-conditional
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    hongchan@chromium.org, mjwilson@chromium.org:
      external/wpt/webaudio
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    japhet@chromium.org:
      external/wpt/fetch
    kojii@chromium.org:
      external/wpt/css/css-writing-modes
    mstensho@chromium.org:
      external/wpt/css/printing
    ricea@chromium.org:
      external/wpt/streams
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Change-Id: Ifb6d851eefe58f7dc27284d86334144a307c1d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817089
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626573}

--- third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/text-module/ignore-extension.any.js
+++ third_party/blink/web_tests/external/wpt/html/semantics/scripting-1/the-script-element/text-module/ignore-extension.any.js
@@ -4,6 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 for (const name of ["file", "file.js", "file.json", "file.txt"]) {
   promise_test(async t => {
     const result = await import(`./${name}`, { with: { type: "text" } });
-    assert_equals(result, "text file\n");
+    assert_equals(result.default, "text file\n");
   }, `Extension: ${name}`);
 }
--- third_party/blink/web_tests/external/wpt/workers/constructors/Worker/expected-self-properties.worker.js
+++ third_party/blink/web_tests/external/wpt/workers/constructors/Worker/expected-self-properties.worker.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 importScripts("/resources/testharness.js");
 
-var expected = ['XMLHttpRequest', 'WebSocket', 'EventSource', 'MessageChannel', 'Worker', 'SharedWorker'];
+var expected = ['XMLHttpRequest', 'WebSocket', 'EventSource', 'MessageChannel', 'Worker'];
 for (var i = 0; i < expected.length; ++i) {
   var property = expected[i];
   test(function() {
