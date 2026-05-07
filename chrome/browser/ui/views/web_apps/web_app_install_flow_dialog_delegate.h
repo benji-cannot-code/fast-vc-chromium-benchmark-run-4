facfd5c55d28027f2229fd48bf5a9dec3af89180
lukedeng <lukedeng@chromium.org>
2026-05-07 13:35:12 -0700

    Decouple WebAppInstallFlowDialogDelegate from WebAppInstallDialogDelegate
    This CL breaks the inheritance between WebAppInstallFlowDialogDelegate
    and WebAppInstallDialogDelegate, having it inherit directly from
    WebAppModalDialogDelegate instead.
    
    To achieve this decoupling, the necessary underlying logic (such as
    OnAccept, OnCancel, OnClose, and metrics/IPH tracking) has been copied
    over from the former parent class. Optimized location of metric
    recording. While this introduces some intentional code duplication in
    the short term, it removes the tight coupling between the dialog
    components and simplifies the web app install flow architecture going
    forward. WebAppInstallDialogDelegate will planned to be clean up after
    new feature launched.
    
    Bug: 510063779
    Change-Id: I4d0ec5da9eb4050ff3d85142e1de0e19f7162455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7822765
    Commit-Queue: Luke Deng <lukedeng@google.com>
    Reviewed-by: Adriana Ixba <aixba@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627199}

--- chrome/browser/ui/views/web_apps/web_app_install_flow_dialog_delegate.h
+++ chrome/browser/ui/views/web_apps/web_app_install_flow_dialog_delegate.h
@@ -9,15 +9,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iosfwd>
 #include <memory>
 #include <optional>
+#include <string>
+#include <variant>
 
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/page_action/page_action_controller.h"
 #include "chrome/browser/ui/views/web_apps/web_app_install_dialog_delegate.h"
+#include "chrome/browser/ui/views/web_apps/web_app_modal_dialog_delegate.h"
 #include "chrome/browser/ui/web_applications/web_app_dialogs.h"
 #include "chrome/browser/web_applications/web_app_install_params.h"
 #include "ui/base/identifier/unique_identifier.h"
+#include "ui/views/controls/button/button.h"
 
-namespace content {
-class WebContents;
+class PrefService;
+
+namespace feature_engagement {
+class Tracker;
 }
 
 namespace webapps {
@@ -44,11 +52,12 @@ enum class InstallOsType { kMac, kWin, kCros, kOther };
 inline constexpr int kLargeImageSize = 80;
 std::ostream& operator<<(std::ostream& os, InstallOsType type);
 
-class WebAppInstallFlowDialogDelegate : public WebAppInstallDialogDelegate {
+class WebAppInstallFlowDialogDelegate : public WebAppModalDialogDelegate {
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kInstallDialogFlowViewId);
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kLearnMoreButtonId);
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kCancelButtonId);
+  DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kInstallButton);
 
   WebAppInstallFlowDialogDelegate(
       content::WebContents* web_contents,
@@ -86,12 +95,27 @@ class WebAppInstallFlowDialogDelegate : public WebAppInstallDialogDelegate {
 
   bool AdvanceToNextStepOrClose();
 
-  void OnAccept() override;
+  void OnAccept();
+  void OnCancel();
+  void OnClose();
+  void OnDestroyed();
+
+  void OnTextFieldChangedMaybeUpdateButton(
+      const std::u16string& text_field_contents);
+
   void OnProgress(std::optional<double> percent);
 
   base::WeakPtr<WebAppInstallFlowDialogDelegate> AsWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
+
+  // views::WidgetObserver overrides:
+  void OnWidgetBoundsChanged(views::Widget* widget,
+                             const gfx::Rect& new_bounds) override;
+  // WebAppModalDialogDelegate overrides:
+  void CloseDialogAsIgnored() override;
+
+  InstallDialogType dialog_type() const { return dialog_type_; }
 
  protected:
   InstallDialogStep current_step_ = InstallDialogStep::kInstallDialog;
@@ -105,10 +129,28 @@ class WebAppInstallFlowDialogDelegate : public WebAppInstallDialogDelegate {
   void UpdateDialogTitleAndHeader(InstallDialogStep step);
   void UpdateProgressAndMaybeAdvance();
   void OnInstallResult(bool success, base::OnceClosure reparent_closure);
-  void OnAcceptCallback(bool success,
-                        std::unique_ptr<WebAppInstallInfo> web_app_info);
 
+  void MeasureIphOnDialogClose();
+  void MeasureAcceptUserActionsForInstallDialog();
+  void MeasureCancelUserActionsForInstallDialog();
+
+  static bool IsWidgetCurrentSizeSmallerThanPreferredSize(
+      views::Widget* widget);
+
+  std::unique_ptr<WebAppInstallInfo> install_info_;
+  std::unique_ptr<webapps::MlInstallOperationTracker> install_tracker_;
   WebAppInstallationAcceptanceCallback callback_;
+  PwaInProductHelpState iph_state_;
+  raw_ptr<PrefService> prefs_;
+  raw_ptr<feature_engagement::Tracker> tracker_;
+  InstallDialogType dialog_type_;
+  std::u16string text_field_contents_;
+  bool received_user_response_ = false;
+
+  const std::optional<std::variant<views::Button::ScopedAnchorHighlight,
+                                   page_actions::ScopedPageActionActivity>>
+      page_action_highlight_;
+
   std::unique_ptr<ProgressDelay> progress_delay_;
   bool install_success_ = false;
   std::optional<double> timer_percentage_ = 0.0;
