ce188e54ded98bda9b7b3bd6f8b63ec72613ba71
Ben Chin <lungc@chromium.org>
2026-05-06 20:00:48 -0700

    Plumb SetDrawPaused to pause frame submission
    This CL introduces a `SetDrawPaused` method to the Android `Compositor`
    interface. This capability allows the browser process to temporarily
    suspend Viz frame submission during critical OS-level window resizing
    transactions.
    
    Internally, this is implemented in `CompositorImpl` by mapping the
    paused state to `display_private_->SetDisplayVisible()`. When the draw
    state is paused (marked as not visible), the display pipeline defers
    actual DrawAndSwap operations.
    
    This CL also updates `MockCompositor` to provide a mock implementation
    for the new pure virtual method.
    
    Bug: 504855598, 404074325
    Change-Id: Ic13299afe7d167984aac53f0fb2b15dc6c173ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7815594
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: ben chin <lungc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626681}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -453,6 +453,16 @@ void CompositorImpl::SetNeedsComposite() {
   host_->SetNeedsAnimate();
 }
 
+void CompositorImpl::SetDrawPaused(bool paused) {
+  if (draw_paused_ == paused) {
+    return;
+  }
+  draw_paused_ = paused;
+  if (display_private_) {
+    display_private_->SetDisplayVisible(!draw_paused_);
+  }
+}
+
 void CompositorImpl::MaybeCompositeNow() {
   host_->MaybeCompositeNow();
 }
@@ -806,7 +816,7 @@ void CompositorImpl::InitializeVizLayerTreeFrameSink(
 
   display_private_->SetSwapCompletionCallbackEnabled(
       enable_swap_completion_callbacks_);
-  display_private_->SetDisplayVisible(true);
+  display_private_->SetDisplayVisible(!draw_paused_);
   display_private_->Resize(size_);
   display_private_->SetDisplayColorSpaces(display_color_spaces_);
   display_private_->SetSupportedRefreshRates(
--- content/browser/renderer_host/compositor_impl_android.h
+++ content/browser/renderer_host/compositor_impl_android.h
@@ -128,6 +128,7 @@ class CONTENT_EXPORT CompositorImpl : public Compositor,
   const gfx::Size& GetWindowBounds() override;
   void SetRequiresAlphaChannel(bool flag) override;
   void SetNeedsComposite() override;
+  void SetDrawPaused(bool paused) override;
   base::WeakPtr<ui::UIResourceProvider> GetUIResourceProvider() override;
   ui::ResourceManager& GetResourceManager() override;
   void CacheBackBufferForCurrentSurface() override;
@@ -248,6 +249,8 @@ class CONTENT_EXPORT CompositorImpl : public Compositor,
 
   // Whether we need to update animations on the next composite.
   bool needs_animate_;
+
+  bool draw_paused_ = false;
 
   // The number of SubmitFrame calls that have not returned and ACK'd from
   // the GPU thread.
