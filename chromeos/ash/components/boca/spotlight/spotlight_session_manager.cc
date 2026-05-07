748e77dcb08679a472619fdd0473a45784cf2886
aprilzhou@google.com <aprilzhou@google.com>
2026-05-06 17:08:37 -0700

    Boca Spotlight: Terminate CRD session upon INACTIVE state
    When a Spotlight session state goes INACTIVE, the notification bubble is
    removed but the WebRTC connection continues to stream screen and audio.
    This CL ensures that the CRD session is actively terminated when the
    state changes to INACTIVE.
    
    Bug: b:504227152
    Change-Id: I5ae045485ffc017a787a87dc7be298c476fdbb6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808754
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626583}

--- chromeos/ash/components/boca/spotlight/spotlight_session_manager.cc
+++ chromeos/ash/components/boca/spotlight/spotlight_session_manager.cc
@@ -131,6 +131,7 @@ void SpotlightSessionManager::OnConsumerActivityUpdated(
         request_in_progress_ = false;
         notification_handler_->StopSpotlightCountdown();
         spotlight_crd_manager_->HidePersistentNotification();
+        spotlight_crd_manager_->OnSessionEnded();
         break;
       default:
         break;
