a406b5522c099454fd3b41741a9bf2c19321765d
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-05-07 10:49:49 -0700

    Remove unsafe buffer pragma from GL bindings autogen
    This CL modifies ui/gl/generate_bindings.py to stop emitting the
    `#pragma allow_unsafe_buffers` directive in the generated GL and EGL
    binding files.
    
    To avoid unsafe buffer usage warnings in the generated mock files that
    previously relied on `strcmp`, the generator has been updated to emit
    comparisons using `std::string_view` instantiated once outside the loop.
    
    
    Bug: 508160130
    Change-Id: I959b1b481d1ca576091ad0f05e86925029bb7020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805767
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627062}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -2325,11 +2325,6 @@ LICENSE_AND_HEADER = """\
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 """
 
 GLVersion = namedtuple('GLVersion', 'is_es major_version minor_version')
@@ -3046,7 +3041,7 @@ def GenerateMockBindingsSource(file, functions, set_name):
   file.write(LICENSE_AND_HEADER +
 """
 
-#include <string.h>
+#include <string_view>
 
 #include "base/notreached.h"
 #include "ui/gl/%s_mock.h"
@@ -3101,9 +3096,10 @@ namespace gl {
   file.write('GLFunctionPointerType GL_BINDING_CALL ' +
              'Mock%sInterface::GetGLProcAddress(const char* name) {\n' % (
                  set_name.upper(),))
+  file.write('  std::string_view name_view(name);\n')
   for key in sorted_function_names:
     name = uniquely_named_functions[key]['name']
-    file.write('  if (strcmp(name, "%s") == 0)\n' % name)
+    file.write('  if (name_view == "%s")\n' % name)
     file.write(
         '    return reinterpret_cast<GLFunctionPointerType>(Mock_%s);\n' %
             name)
--- ui/gl/gl_stub_autogen_gl.h
+++ ui/gl/gl_stub_autogen_gl.h
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef UI_GL_GL_STUB_AUTOGEN_GL_H_
 #define UI_GL_GL_STUB_AUTOGEN_GL_H_
 
