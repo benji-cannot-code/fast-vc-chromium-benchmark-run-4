6eb6eb16fc2238cad298bfd6c5449bb68474dd59
Jinsuk Kim <jinsukkim@chromium.org>
2026-05-07 15:08:05 -0700

    [Settings Search] Adjust search UI width after animation is done
    Animation added in crrev.com/c/7801152 caused the final actual width
    to be determined later than before. The old state was used to adjust
    the width, therefore cause the regression in the search bar UI. This
    CL ensures the width is updated with the latest state after the
    animation is over.
    
    Bug: 510619398
    Change-Id: I4f546c313e2acd82ea511f8334b7a3919264b881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831030
    Reviewed-by: Moe Adel <adelm@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627270}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -585,6 +585,7 @@ public class SettingsSearchCoordinator
                                     @Override
                                     public void onTransitionEnd(Transition transition) {
                                         searchBox.setOnClickListener(v -> onClickSearchBox(v));
+                                        updateSearchUiWidth();
                                     }
                                 });
         var parentView = (ViewGroup) mActivity.findViewById(R.id.settings_activity);
