ae86bd1401d802753d43d895845c796a054b02cb
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-05-07 12:19:52 -0700

    Move preamble from PrepareFrame to DispatchFrame in CRDispatcher
    As part of pushing ExportedCanvasResource further.
    
    Change-Id: Ib8131611e0e1f753b6a711b64387fd479b6f2202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816301
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627129}

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
@@ -121,7 +121,7 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
   using ExportedResourceMap =
       HashMap<viz::ResourceId, scoped_refptr<ExportedCanvasResource>>;
 
-  bool PrepareFrame(scoped_refptr<CanvasResource>&&,
+  void PrepareFrame(scoped_refptr<ExportedCanvasResource>&&,
                     const gfx::Rect& damage_rect,
                     bool is_opaque,
                     viz::CompositorFrame* frame);
