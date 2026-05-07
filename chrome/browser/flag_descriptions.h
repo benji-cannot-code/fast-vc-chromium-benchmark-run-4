a688a2a3fbc2dcb4e6ab3dc5fb8b7747051b14c1
maciek swiech <drmasquatch@google.com>
2026-05-07 10:02:26 -0700

    Clarify crostini container flag.
    In classic crostini, a user could choose what version of Debian should
    be installed in the penguin container. In baguette, there is just the
    rootfs (which currently is Debian Trixie).
    
    Bug: None
    Change-Id: Ie5be1f87244d78d9397453935d8589dead281b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816546
    Auto-Submit: maciek swiech <drmasquatch@google.com>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627022}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -6997,7 +6997,8 @@ inline constexpr char kDisableBruschettaInstallChecksDescription[] =
 inline constexpr char kCrostiniContainerInstallName[] =
     "Debian version for new Crostini containers";
 inline constexpr char kCrostiniContainerInstallDescription[] =
-    "New Crostini containers will use this Debian version";
+    "New Crostini containers will use this Debian version (does not affect "
+    "baguette installs)";
 
 inline constexpr char kCrostiniGpuSupportName[] = "Crostini GPU Support";
 inline constexpr char kCrostiniGpuSupportDescription[] =
