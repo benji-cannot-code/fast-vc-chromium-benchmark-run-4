a225ab71c066425e8fe6f899728466bd30d3b2b2
Colin Blundell <blundell@chromium.org>
2026-05-07 00:01:35 -0700

    [Blink] Move CRPSI::CreateSkSurface() and dependencies to subclasses
    Continue decoupling CanvasNon2DResourceProviderSI from the class
    hierarchy here.
    
    Bug: 352263194
    Change-Id: I24dd1311a3a31beea56868e298be3a75ed9af287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7822861
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626753}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -406,9 +406,7 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
 
   bool IsAccelerated() const override = 0;
   virtual bool IsSoftware() const = 0;
-  bool IsGpuContextLost() const override;
 
-  sk_sp<SkSurface> CreateSkSurface() const override;
   void OnFlushForImage(cc::PaintImage::ContentId content_id) override = 0;
 
   // Indicates that the compositing path is single buffered, meaning that
@@ -426,7 +424,6 @@ class PLATFORM_EXPORT CanvasResourceProviderSharedImage
   }
   virtual base::WeakPtr<WebGraphicsContext3DProviderWrapper>
   ContextProviderWrapper() const = 0;
-  virtual gpu::raster::RasterInterface* RasterInterface() const;
   virtual void EnsureWriteAccess() = 0;
   virtual void EndWriteAccess() = 0;
 
@@ -517,6 +514,7 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
 
   bool IsAccelerated() const override { return is_accelerated_; }
   bool IsSoftware() const override { return is_software_; }
+  bool IsGpuContextLost() const override;
 
   // WebGraphicsContext3DProviderWrapper::DestructionObserver implementation.
   void OnContextDestroyed() override;
@@ -596,6 +594,9 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
   // by this provider.
   void WillDrawUnaccelerated();
   void DisableLineDrawingAsPathsIfNecessaryForCanvas2D() override;
+
+  sk_sp<SkSurface> CreateSkSurface() const override;
+  gpu::raster::RasterInterface* RasterInterface() const;
 
   // If this instance is single-buffered or |resource_recycling_enabled_| is
   // false, |image_pool_| will not recycle resources.
@@ -696,6 +697,7 @@ class PLATFORM_EXPORT CanvasNon2DResourceProviderSharedImage
 
   bool IsAccelerated() const override { return is_accelerated_; }
   bool IsSoftware() const override { return is_software_; }
+  bool IsGpuContextLost() const override;
 
   // WebGraphicsContext3DProviderWrapper::DestructionObserver implementation.
   void OnContextDestroyed() override;
@@ -777,6 +779,9 @@ class PLATFORM_EXPORT CanvasNon2DResourceProviderSharedImage
   // write have completed. Ensures that the next read of this resource (whether
   // via raster or the compositor) waits on this token.
   void EndExternalWrite(const gpu::SyncToken& external_write_sync_token);
+
+  sk_sp<SkSurface> CreateSkSurface() const override;
+  gpu::raster::RasterInterface* RasterInterface() const;
 
  private:
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
