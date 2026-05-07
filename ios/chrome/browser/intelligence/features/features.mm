f739c95b43277c9d9384ad030969b80ed58f172c
Nicolas MacBeth <nicolasmacbeth@google.com>
2026-05-07 14:30:59 -0700

    [iOS] Enable ImageRemix by default on ToT (with the correct params)
    Bug: 455905288
    Change-Id: I677d6b4c60d2e7747501bcb0ec4cad3ed6719a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828808
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627241}

--- ios/chrome/browser/intelligence/features/features.mm
+++ ios/chrome/browser/intelligence/features/features.mm
@@ -429,7 +429,7 @@ bool IsGeminiUpdatedEligibilityEnabled() {
   return base::FeatureList::IsEnabled(kGeminiUpdatedEligibility);
 }
 
-BASE_FEATURE(kGeminiImageRemixTool, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kGeminiImageRemixTool, base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsGeminiImageRemixToolEnabled() {
   if (!IsPageActionMenuEnabled()) {
@@ -445,7 +445,7 @@ bool IsGeminiImageRemixToolShowFRERowEnabled() {
     return false;
   }
   return base::GetFieldTrialParamByFeatureAsBool(
-      kGeminiImageRemixTool, kGeminiImageRemixToolShowFRERow, false);
+      kGeminiImageRemixTool, kGeminiImageRemixToolShowFRERow, true);
 }
 
 const char kGeminiImageRemixToolShowAboveSearchImage[] = "ShowAboveSearchImage";
@@ -475,7 +475,7 @@ bool IsGeminiImageRemixToolRemovePageContextEnabled() {
     return false;
   }
   return base::GetFieldTrialParamByFeatureAsBool(
-      kGeminiImageRemixTool, kGeminiImageRemixToolRemovePageContext, false);
+      kGeminiImageRemixTool, kGeminiImageRemixToolRemovePageContext, true);
 }
 
 BASE_FEATURE(kGeminiEligibilityAblation, base::FEATURE_DISABLED_BY_DEFAULT);
