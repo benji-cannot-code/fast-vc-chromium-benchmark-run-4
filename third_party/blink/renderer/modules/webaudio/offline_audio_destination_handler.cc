14b0a8d37e5deb28fb7bf1356469373de87dd242
Nidhi Jaju <nidhijaju@chromium.org>
2026-05-06 21:33:57 -0700

    Revert "[WebAudio] Fix thread safety in OfflineAudioDestinationHandler"
    This reverts commit 678cd5af5aef27ba4fc44bd3bf3523915e54d73b.
    
    Reason for revert: Consistently causing failures on linux-blink-msan-rel
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/linux-blink-msan-rel/4871/test-results
    
    Original change's description:
    > [WebAudio] Fix thread safety in OfflineAudioDestinationHandler
    >
    > This CL resolves a threading issue in offline rendering that caused
    > flaky timeouts in `offlineaudiocontext-promise-basic.html`:
    >
    > - OfflineAudioDestinationHandler::FinishOfflineRendering incorrectly
    >   accessed GetWeakPtr() on a background thread. Since the factory
    >   is bound to the main thread, this caused the posted main-thread
    >   completion task to occasionally be dropped, leaving the promise
    >   permanently unresolved.
    > - Fix this by removing GetWeakPtr() and weak_factory_ from the
    >   handler, and posting the main-thread tasks using
    >   WrapRefCounted(this) instead of GetWeakPtr().
    > - Add the OfflineRenderingThreadSafetyAndNoLeak C++ unit test
    >   to verify thread safety and assert no memory leaks.
    >
    > Bug: 459235322
    > Test: t/b/r/m/webaudio/offline_audio_context_test.cc
    > Change-Id: Ibb8cbe2eabf02dd956b07c43c22d43a6fc9540fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814343
    > Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    > Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1626453}
    
    Bug: 459235322
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Idd9b1561d2f3428f39b3430d72e9ec341617fd59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7825829
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1626708}

--- third_party/blink/renderer/modules/webaudio/offline_audio_destination_handler.cc
+++ third_party/blink/renderer/modules/webaudio/offline_audio_destination_handler.cc
@@ -221,8 +221,7 @@ void OfflineAudioDestinationHandler::SuspendOfflineRendering() {
   PostCrossThreadTask(
       *main_thread_task_runner_, FROM_HERE,
       CrossThreadBindOnce(&OfflineAudioDestinationHandler::NotifySuspend,
-                          WrapRefCounted(this),
-                          Context()->CurrentSampleFrame()));
+                          GetWeakPtr(), Context()->CurrentSampleFrame()));
 }
 
 void OfflineAudioDestinationHandler::FinishOfflineRendering() {
@@ -232,7 +231,7 @@ void OfflineAudioDestinationHandler::FinishOfflineRendering() {
   PostCrossThreadTask(
       *main_thread_task_runner_, FROM_HERE,
       CrossThreadBindOnce(&OfflineAudioDestinationHandler::NotifyComplete,
-                          WrapRefCounted(this)));
+                          GetWeakPtr()));
 }
 
 void OfflineAudioDestinationHandler::NotifySuspend(size_t frame) {
