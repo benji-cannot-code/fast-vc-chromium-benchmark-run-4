c2c85b53c65c95c7cc234791c9f2d4cf17db06f2
Helmut Januschka <helmut@januschka.com>
2026-05-07 08:36:07 -0700

    Fix IDN email value attribute validation
    This converts value attributes for email inputs to their internal punycode form without forcing early view updates.
    
    Bug: 429490608
    Change-Id: I0edbfa9ed943a875841c3f091c447fc20eeea0de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807599
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626949}

--- third_party/blink/renderer/core/html/forms/input_type.cc
+++ third_party/blink/renderer/core/html/forms/input_type.cc
@@ -870,6 +870,10 @@ String InputType::VisibleValue() const {
   return GetElement().Value();
 }
 
+String InputType::ConvertFromVisibleValue(const String& visible_value) const {
+  return SanitizeValue(visible_value);
+}
+
 String InputType::SanitizeValue(const String& proposed_value) const {
   return proposed_value;
 }
