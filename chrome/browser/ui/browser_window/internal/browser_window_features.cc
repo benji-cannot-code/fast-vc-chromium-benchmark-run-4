eb5aebcb783c8a10335137a8e7356f5475909166
Thomas Lukaszewicz <tluk@chromium.org>
2026-05-07 11:57:51 -0700

    [bedrock] Introduce BrowserFocusController::Delegate
    This new interface defines a strategy for a given BrowserWindow
    implementation (aka BrowserView or WebUIBrowserWindow). Note
    currently the latter has no implementation as the webui Browser
    is still a WIP.
    
    The existing implementation intended to support BrowserView has
    been encapsulated in BrowserFocusControllerDelegateViews.
    
    This CL also composes both the BrowserFocusController and
    BrowserFocusControllerDelegateViews with specific dependencies
    passed into their constructors by BrowserWindowFeatures.
    
    Bug: 502745808
    Change-Id: I6188f940dd283cddcd730265c187a3f970fd1dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7819439
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627114}

--- chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/find_bar/find_bar.h"
 #include "chrome/browser/ui/find_bar/find_bar_controller.h"
 #include "chrome/browser/ui/focus/browser_focus_controller.h"
+#include "chrome/browser/ui/focus/browser_focus_controller_delegate_views.h"
 #include "chrome/browser/ui/fullscreen/browser_window_fullscreen_controller.h"
 #include "chrome/browser/ui/lens/lens_overlay_entry_point_controller.h"
 #include "chrome/browser/ui/omnibox/ai_mode_page_action_controller.h"
@@ -420,9 +421,6 @@ void BrowserWindowFeatures::Init(BrowserWindowInterface* browser) {
       GetUserDataFactory().CreateInstance<TranslateBubbleController>(
           *browser, browser, browser_actions_->root_action_item());
 
-  browser_focus_controller_ =
-      std::make_unique<BrowserFocusController>(*browser);
-
   cookie_controls_controller_ =
       std::make_unique<content_settings::CookieControlsController>(
           CookieSettingsFactory::GetForProfile(profile),
@@ -572,11 +570,25 @@ void BrowserWindowFeatures::Init(BrowserWindowInterface* browser) {
 }
 
 void BrowserWindowFeatures::InitPostWindowConstruction(Browser* browser) {
+  Profile* const profile = browser_->GetProfile();
+  BrowserView* const browser_view =
+      BrowserView::GetBrowserViewForBrowser(browser);
+
   desktop_browser_window_capabilities_ =
       GetUserDataFactory().CreateInstance<DesktopBrowserWindowCapabilities>(
           *browser, browser_window_modal_dialog_delegate_.get(),
           unload_controller_.get(), browser->window(),
           browser->GetUnownedUserDataHost());
+
+  browser_focus_controller_ =
+      GetUserDataFactory().CreateInstance<BrowserFocusController>(
+          *browser, browser->GetWindow(), browser->GetUnownedUserDataHost());
+  if (browser_view) {
+    browser_focus_controller_->SetDelegate(
+        std::make_unique<BrowserFocusControllerDelegateViews>(
+            profile, browser_elements_.get(),
+            ToolbarButtonProvider::From(browser)));
+  }
 
   if (WebUIBrowserWindow* webui_browser_window =
           WebUIBrowserWindow::FromBrowser(browser)) {
@@ -597,9 +609,6 @@ void BrowserWindowFeatures::InitPostWindowConstruction(Browser* browser) {
   }
 #endif
 
-  Profile* const profile = browser_->GetProfile();
-  BrowserView* const browser_view =
-      BrowserView::GetBrowserViewForBrowser(browser);
   if (browser_view) {
     // Initialize fullscreen control host after exclusive access manager is
     // ready.
@@ -1118,6 +1127,7 @@ void BrowserWindowFeatures::TearDownPreBrowserWindowDestruction() {
 
   data_protection_ui_controller_.reset();
 
+  browser_focus_controller_.reset();
   desktop_browser_window_capabilities_.reset();
   signin_view_controller_->TearDownPreBrowserWindowDestruction();
 
