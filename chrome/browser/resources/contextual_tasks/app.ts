fbe5b998897b992496cda59f3e4af2f989765592
Christian Dullweber <dullweber@chromium.org>
2026-05-07 00:18:21 -0700

    Revert "[nextbox] Double load animation fix"
    This reverts commit ca64a75bfef4ea9e12a7171ef83fecacd619c5bf.
    
    Reason for revert: Suspected to break ContextualTasksBrowserTest.Composebox_ZeroState test
    
    Original change's description:
    > [nextbox] Double load animation fix
    >
    > Screencast before: https://screencast.googleplex.com/cast/NTY3ODM1MTA4Njc4MDQxNnxmM2QwM2ExNC03OQ
    > Screencast now: https://screencast.googleplex.com/cast/NDkwNDEzMzIwMjAxODMwNHwyOTQxNzUxOS03ZA
    >
    > Bug: 510500579
    > Change-Id: I0d6eddbc14d79d19425c7b584af9087edf462bd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7823278
    > Reviewed-by: Gianni Chen <giannich@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Omar Flores <omarflores@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1626714}
    
    Bug: 510500579, 510600760
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 510500579
    Change-Id: Ief9233b74b134d87df3271e3f7a0cb1a857e3d35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7825792
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626765}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -344,9 +344,6 @@ export class ContextualTasksAppElement extends CrLitElement {
   // even if the load is aborted and the frame therefore never changes.
   private lastThreadFrameLoadStartEvent_: chrome.webviewTag.LoadStartEvent|
       LoadEvent|null = null;
-  // Tracks whether the frame is loading for the very first time to prevent
-  // double animations.
-  private isInitialFrameLoad_: boolean = true;
 
   private updateThemeFromUrl(url: URL) {
     const csParam = url.searchParams.get('cs');
@@ -828,9 +825,7 @@ export class ContextualTasksAppElement extends CrLitElement {
 
     if (isAiPage && isZeroState) {
       this.isZeroState_ = true;
-      if (!this.isInitialFrameLoad_) {
-        this.playZeroStateAnimations_();
-      }
+      this.playZeroStateAnimations_();
     }
 
     if (!wasZeroState && isZeroState) {
@@ -858,8 +853,6 @@ export class ContextualTasksAppElement extends CrLitElement {
       }
       this.isInBasicMode_ = true;
     }
-
-    this.isInitialFrameLoad_ = false;
 
     if (this.onLoadStartFinishedCallbackForTesting_) {
       this.onLoadStartFinishedCallbackForTesting_();
