5d49b806862cac55eda7b609c6508ccd93248a95
Pulkit Agarwal <pulkitaga@google.com>
2026-05-07 15:32:43 -0700

    Add histogram for the interaction behavior with the cue (both total and segmented by CUJ)
    Bug: b:506280693
    Change-Id: Ic9bd1748b9c106af628155a36451211f5976aecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809504
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Pulkit Agarwal <pulkitaga@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627289}

--- chrome/browser/contextual_cueing/contextual_cueing_metrics.h
+++ chrome/browser/contextual_cueing/contextual_cueing_metrics.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CONTEXTUAL_CUEING_CONTEXTUAL_CUEING_METRICS_H_
 #define CHROME_BROWSER_CONTEXTUAL_CUEING_CONTEXTUAL_CUEING_METRICS_H_
 
+#include <string>
+
 namespace contextual_cueing {
 
 enum class ContextualCueingInteraction;
 
 void RecordContextualCueingInteraction(
-    ContextualCueingInteraction contextual_cueing_interaction);
+    ContextualCueingInteraction contextual_cueing_interaction,
+    const std::string& cuj);
 
 }  // namespace contextual_cueing
 
