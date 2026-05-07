7e63b4dcb37ab6033664f354fbea187cfcdf9dee
aprilzhou@google.com <aprilzhou@google.com>
2026-05-07 11:51:14 -0700

    [Boca] Fix Missing Authorization & DoS in Boca StartSpotlight
    Add missing authorization checks to verify user is producer and active session before starting CRD client in StartSpotlight. Fix double-callback logic issue when feature is disabled. Add null pointer check for remoting_client_manager_ in BocaSessionManager::StartCrdClient.
    
    Bug: 501891891
    
    Change-Id: Ic07b558860f6b4aa407e3dac114d98a1ac383a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810110
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627106}

--- chromeos/ash/components/boca/boca_session_manager_unittest.cc
+++ chromeos/ash/components/boca/boca_session_manager_unittest.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/common/request_sender.h"
 #include "google_apis/gaia/gaia_id.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using ::testing::_;
@@ -2604,6 +2605,22 @@ TEST_F(BocaSessionManagerStudentHeartbeatNoPollingTest,
       std::make_unique<::boca::Session>(session_1), /*dispatch_event=*/true);
 
   task_environment()->FastForwardBy(kDefaultStudentHeartbeatInterval);
+}
+
+TEST_F(BocaSessionManagerTest, StartCrdClientFailsWithNullManager) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      ash::features::kBocaSpotlightRobotRequester);
+
+  base::test::TestFuture<CrdConnectionState> future;
+  boca_session_manager()->StartCrdClient(
+      "connection_code",
+      /*done_callback=*/base::DoNothing(),
+      /*frame_received_callback=*/base::DoNothing(),
+      /*crd_state_callback=*/base::BindLambdaForTesting(
+          [&](CrdConnectionState state) { future.SetValue(state); }));
+
+  EXPECT_EQ(CrdConnectionState::kFailed, future.Get());
 }
 
 class BocaSessionManagerConsumerTest : public BocaSessionManagerTest {
