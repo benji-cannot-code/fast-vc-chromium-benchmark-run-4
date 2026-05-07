0a08e98d0989c95a537a9023cf6c4167619a7f82
James Cook <jamescook@chromium.org>
2026-05-06 18:37:33 -0700

    extensions: Add static_assert to ExtensionCSPBypassTest
    I ported it to desktop Android but forgot to add the
    static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE)).
    
    Bug: none
    Change-Id: Id900dc1fcebf4473a1f2292b856a22c629d56735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7823559
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626652}

--- chrome/browser/extensions/extension_csp_bypass_browsertest.cc
+++ chrome/browser/extensions/extension_csp_bypass_browsertest.cc
@@ -13,8 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/test/test_extension_dir.h"
 #include "net/dns/mock_host_resolver.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
