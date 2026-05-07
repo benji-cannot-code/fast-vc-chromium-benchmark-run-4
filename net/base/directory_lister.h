5531ded1eaee4e59d6d729888a6b356f8a5ae30d
Ho Cheung <hocheung@chromium.org>
2026-05-06 21:31:54 -0700

    [net] Clean up unnecessary header file
    And use the correct header file.
    
    Bug: 40175832
    Change-Id: I82b57b5a84345d471031313272c501fa88fd2dac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7819615
    Auto-Submit: Aaron Teo <hocheung@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626705}

--- net/base/directory_lister.h
+++ net/base/directory_lister.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_BASE_DIRECTORY_LISTER_H_
 #define NET_BASE_DIRECTORY_LISTER_H_
 
+#include <atomic>
 #include <memory>
 #include <vector>
 
-#include "base/atomicops.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
