b85a51d6c47ee656d024b0d4e60265c5b1b886ed
Paulina Gacek <paulinagacek@google.com>
2026-05-07 10:47:39 -0700

    awc: Add unit tests for AdditionalWindowingControls
    This CL introduces unit testing for the AdditionalWindowingControls API
    (maximize, minimize, restore, and setResizable) and fixes a potential
    null pointer dereference crash.
    
    Bug: 507741091
    Change-Id: I13b627e5f5868b29bd8d532856d93b822df0bda4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805774
    Reviewed-by: Patryk Chodur <pchodur@google.com>
    Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627059}

--- third_party/blink/renderer/modules/awc/additional_windowing_controls.h
+++ third_party/blink/renderer/modules/awc/additional_windowing_controls.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_AWC_ADDITIONAL_WINDOWING_CONTROLS_H_
 
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
 namespace blink {
@@ -16,7 +17,7 @@ class LocalDOMWindow;
 class ScriptState;
 
 // Complements LocalDOMWindow with additional windowing controls.
-class AdditionalWindowingControls {
+class MODULES_EXPORT AdditionalWindowingControls {
   STATIC_ONLY(AdditionalWindowingControls);
 
  public:
