43ef7dbf186649861914ba00b6bc5b5eef6efc62
Gianmarco Picarella <picarella@google.com>
2026-05-07 01:08:28 -0700

    Deprecate unused field types NAME_LAST_PREFIX and NAME_LAST_CORE
    Deprecated and removed the field types NAME_LAST_PREFIX and
    NAME_LAST_CORE from the client codebase. These field types are no longer
    used and have no associated client-side logic. This change aligns with
    the decision made during the feature sync.
    
    OBSOLETE_HISTOGRAM=Removed as the considered field types are deprecated.
    
    Bug: 507312647
    Change-Id: Id640fdf1fc3971f5fe2ea29b5f759efab5f14b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817334
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Gianmarco Picarella <picarella@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626776}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -112,8 +112,6 @@ static constexpr auto kTypeNameToFieldType =
          {"ADDRESS_HOME_HOUSE_NUMBER", ADDRESS_HOME_HOUSE_NUMBER},
          {"ADDRESS_HOME_SUBPREMISE", ADDRESS_HOME_SUBPREMISE},
          {"ADDRESS_HOME_OTHER_SUBUNIT", ADDRESS_HOME_OTHER_SUBUNIT},
-         {"NAME_LAST_PREFIX", NAME_LAST_PREFIX},
-         {"NAME_LAST_CORE", NAME_LAST_CORE},
          {"NAME_LAST_FIRST", NAME_LAST_FIRST},
          {"NAME_LAST_CONJUNCTION", NAME_LAST_CONJUNCTION},
          {"NAME_LAST_SECOND", NAME_LAST_SECOND},
@@ -196,8 +194,6 @@ bool IsFillableFieldType(FieldType field_type) {
     case NAME_FIRST:
     case NAME_MIDDLE:
     case NAME_LAST:
-    case NAME_LAST_CORE:
-    case NAME_LAST_PREFIX:
     case NAME_LAST_FIRST:
     case NAME_LAST_CONJUNCTION:
     case NAME_LAST_SECOND:
@@ -474,10 +470,6 @@ std::string_view FieldTypeToDeveloperRepresentationString(FieldType type) {
       return "Middle name";
     case NAME_LAST:
       return "Last name";
-    case NAME_LAST_PREFIX:
-      return "Last name prefix";
-    case NAME_LAST_CORE:
-      return "Last name core";
     case NAME_LAST_FIRST:
       return "First last name";
     case NAME_LAST_CONJUNCTION:
