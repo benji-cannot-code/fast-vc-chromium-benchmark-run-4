25206de52c44f36d984aafef67db1db1ca4dcf69
Tsuyoshi Horo <horo@chromium.org>
2026-05-06 18:41:10 -0700

    Remove origin parameter from FileSystemManager::Open
    This is step 2 of 2 for enforcing the browser-authoritative origin in
    FileSystemManager::Open.
    
    With the kEnforceFileSystemManagerOpenOrigin feature flag from step 1
    (https://crrev.com/c/7717899) handling the transition, this CL removes
    the origin parameter from the Mojo IPC interface and all associated
    renderer calls.
    
    Bug: 497254383
    Change-Id: I41f29d395d7b0812b13b0535aad095a7622c3302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7720200
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626655}

--- content/browser/file_system/file_system_manager_impl.cc
+++ content/browser/file_system/file_system_manager_impl.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/file_system/browser_file_system_helper.h"
-#include "content/common/features.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/child_process_id.h"
@@ -215,16 +214,9 @@ void FileSystemManagerImpl::BindReceiver(
   receivers_.Add(this, std::move(receiver), storage_key);
 }
 
-void FileSystemManagerImpl::Open(const url::Origin& origin,
-                                 blink::mojom::FileSystemType file_system_type,
+void FileSystemManagerImpl::Open(blink::mojom::FileSystemType file_system_type,
                                  OpenCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-
-  url::Origin origin_to_check = origin;
-  if (base::FeatureList::IsEnabled(
-          features::kEnforceFileSystemManagerOpenOrigin)) {
-    origin_to_check = receivers_.current_context().origin();
-  }
 
   // Run the access check on the UI thread using a duplicated
   // ChildProcessSecurityPolicy::Handle, ensuring the SecurityState exists when
@@ -235,7 +227,7 @@ void FileSystemManagerImpl::Open(const url::Origin& origin,
           &ChildProcessSecurityPolicyImpl::Handle::CanAccessDataForOrigin,
           std::make_unique<ChildProcessSecurityPolicyImpl::Handle>(
               security_policy_handle_.Duplicate()),
-          origin_to_check),
+          receivers_.current_context().origin()),
       base::BindOnce(&FileSystemManagerImpl::ContinueOpen,
                      weak_factory_.GetWeakPtr(), file_system_type,
                      receivers_.GetBadMessageCallback(), std::move(callback),
--- third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
+++ third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
@@ -23,7 +23,6 @@ namespace blink {
 class Blob;
 class ExecutionContext;
 class KURL;
-class SecurityOrigin;
 
 // Sends messages via mojo to the blink::mojom::FileSystemManager service
 // running in the browser process. It is owned by ExecutionContext, and
@@ -44,11 +43,9 @@ class FileSystemDispatcher : public GarbageCollected<FileSystemDispatcher>,
 
   mojom::blink::FileSystemManager& GetFileSystemManager();
 
-  void OpenFileSystem(const SecurityOrigin* origin,
-                      mojom::blink::FileSystemType type,
+  void OpenFileSystem(mojom::blink::FileSystemType type,
                       std::unique_ptr<FileSystemCallbacks> callbacks);
-  void OpenFileSystemSync(const SecurityOrigin* origin,
-                          mojom::blink::FileSystemType type,
+  void OpenFileSystemSync(mojom::blink::FileSystemType type,
                           std::unique_ptr<FileSystemCallbacks> callbacks);
 
   void ResolveURL(const KURL& filesystem_url,
