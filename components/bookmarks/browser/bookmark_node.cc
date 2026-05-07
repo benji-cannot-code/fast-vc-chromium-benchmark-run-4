159e232956571db9b44f55b1707ac6922072f2f8
Kelvin Jiang <kelvinjiang@chromium.org>
2026-05-06 21:44:27 -0700

    [Bookmarks] Desktop android folder visibility follows desktop
    Treat desktop android as another desktop OS for bookmarks folder
    visibility (and other things?) logic, i.e.
    - on desktop, the mobile folder is hidden when empty
    - on mobile, the bookmarks bar and other bookmarks is hidden when empty
    
    there was a "hack" somewhere in Java which forced visibility for some
    folders. Adding the intended logic in the C++ end allowed the hack to
    be removed.
    
    This also allows the rest of the bookmarks API tests for extension to be
    enabled again on desktop Android now that the intended behavior is
    defined. This completes bookmarks API porting to desktop Android.
    
    Bug: 509209039, 414844449
    Change-Id: Iaca3a7e170c0f1d4055e73401cdd5faa0882380d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789287
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626716}

--- components/bookmarks/browser/bookmark_node.cc
+++ components/bookmarks/browser/bookmark_node.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/uuid.h"
-#include "build/build_config.h"
 #include "components/bookmarks/browser/bookmark_uuids.h"
 #include "components/bookmarks/common/bookmark_features.h"
 #include "components/strings/grit/components_strings.h"
@@ -154,12 +153,10 @@ BookmarkPermanentNode::CreateManagedBookmarks(int64_t id) {
 }
 
 // static
-bool BookmarkPermanentNode::IsTypeVisibleWhenEmpty(Type type) {
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-  bool is_desktop = false;
-#else
-  bool is_desktop = true;
-#endif
+bool BookmarkPermanentNode::IsTypeVisibleWhenEmpty(
+    Type type,
+    BookmarkFormFactor form_factor) {
+  bool is_desktop = form_factor == BookmarkFormFactor::kDesktop;
 
   switch (type) {
     case BookmarkNode::URL:
@@ -174,7 +171,7 @@ bool BookmarkPermanentNode::IsTypeVisibleWhenEmpty(Type type) {
                                kAllBookmarksBaselineFolderVisibility);
     case BookmarkNode::MOBILE:
       // Either MOBILE or OTHER_NODE is visible when empty, but never both.
-      return !IsTypeVisibleWhenEmpty(BookmarkNode::OTHER_NODE);
+      return !IsTypeVisibleWhenEmpty(BookmarkNode::OTHER_NODE, form_factor);
   }
   NOTREACHED();
 }
--- components/bookmarks/browser/bookmark_node.h
+++ components/bookmarks/browser/bookmark_node.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
 #include "base/uuid.h"
+#include "components/bookmarks/browser/bookmark_form_factor.h"
 #include "components/bookmarks/browser/titled_url_node.h"
 #include "ui/base/models/tree_node_model.h"
 #include "ui/gfx/image/image.h"
@@ -253,7 +254,7 @@ class BookmarkPermanentNode : public BookmarkNode {
 
   // Returns whether the permanent node of type `type` should be visible even
   // when it is empty (i.e. no children).
-  static bool IsTypeVisibleWhenEmpty(Type type);
+  static bool IsTypeVisibleWhenEmpty(Type type, BookmarkFormFactor form_factor);
 
   bool IsVisible() const override;
 
