545d44fbc2696646e5e2c91dd26248d8bcc0e87e
Gayane Petrosyan <gayane@google.com>
2026-05-07 14:35:44 -0700

    [iOS] Rename backend promo service to match internal naming
    - Renamed backend_promo_service directories to backend_promo
    - Renamed chromium_backend_promo_service.mm to chromium_backend_promo.mm
    
    Bug: 503366680
    Change-Id: I85c1a139925d7cd9a3f943ab4e4790fd65ee4943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7823420
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627246}

--- ios/chrome/browser/providers/backend_promo/chromium_backend_promo.mm
+++ ios/chrome/browser/providers/backend_promo/chromium_backend_promo.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/backend_promo_service/model/backend_promo_service.h"
+#import "ios/chrome/browser/backend_promo/model/backend_promo_service.h"
 
 class ChromiumBackendPromoService final : public BackendPromoService {
  public:
