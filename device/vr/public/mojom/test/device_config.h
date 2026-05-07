d49ed38cba376ce2c90d9e26cef36f6a8923c204
Alexander Cooper <alcooper@chromium.org>
2026-05-07 12:32:41 -0700

    Create default values for device config
    We are racily seeing an error where we get an invalid transform that
    causes a DumpWithoutCrashing because the matrix is full of NaN values
    when attempting to decompose. It is speculated that this is due to some
    racy teardown behavior leaving the DeviceConfig in a bad state during
    session shutdown. As an easy fix, attempt to initialize the DeviceConfig
    to some reasonable defaults.
    
    Bug: 462787840
    Change-Id: If55d014826e5132325cbd411bc08d7a9cae28dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7824835
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627140}

--- device/vr/public/mojom/test/device_config.h
+++ device/vr/public/mojom/test/device_config.h
@@ -11,11 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace device {
 
 struct DeviceConfig {
-  float interpupillary_distance;
-  std::array<float, 4>
-      viewport_left;  // raw projection left {left, right, top, bottom}
-  std::array<float, 4>
-      viewport_right;  // raw projection right {left, right, top, bottom}
+  float interpupillary_distance = 0.0f;
+  // Both viewports are in the form of {left, right, top, bottom} FOVs.
+  std::array<float, 4> viewport_left = {0.0f, 0.0f, 0.0f, 0.0f};
+  std::array<float, 4> viewport_right = {0.0f, 0.0f, 0.0f, 0.0f};
 };
 
 }  // namespace device
