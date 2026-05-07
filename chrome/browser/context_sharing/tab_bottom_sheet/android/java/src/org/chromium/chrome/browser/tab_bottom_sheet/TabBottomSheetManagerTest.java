e9e4b8d1c2533c85e6dca74dcef738eb5ff3d63e
Linyu He <linyuh@google.com>
2026-05-06 17:10:52 -0700

    Gardening: Disable a flaky test in TabBottomSheetManagerTest
    testDetachNativeInterfaceDelegate() started being flaky on May 6, 2026:
    
    https://ci.chromium.org/ui/test/chrome/%3A%2F%2Fchrome%2Fandroid%5C%3Achrome_public_test_apk!junit%3Aorg.chromium.chrome.browser.tab_bottom_sheet%3ATabBottomSheetManagerTest%23testDetachNativeInterfaceDelegate?q=VHASH%3A38aa266d3739cbbf
    
    Bug: 510449718
    Change-Id: I3b8d28fed9e5e6137a183cb4ccf82c08a5ed69c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7822114
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Commit-Queue: Linyu He <linyuh@google.com>
    Auto-Submit: Linyu He <linyuh@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Owners-Override: Linyu He <linyuh@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1626585}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetManagerTest.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetManagerTest.java
@@ -36,6 +36,7 @@ import org.chromium.base.supplier.SettableNullableObservableSupplier;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -501,6 +502,7 @@ public class TabBottomSheetManagerTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/510449718")
     public void testDetachNativeInterfaceDelegate() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
