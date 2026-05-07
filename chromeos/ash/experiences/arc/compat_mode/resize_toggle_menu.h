b8324cfb3e5882c4d386143b3655ae20e19342eb
Christian Dullweber <dullweber@chromium.org>
2026-05-07 00:33:15 -0700

    Revert "Introduce unique_ptr based CreateBubble for bubble widgets"
    This reverts commit 1a3139ec46750e0b9e0f8acdf7e311d04dc11864.
    
    Reason for revert: Suspected to break ResizeToggleMenuTest.TestIsBubbleShown on Linux Asan
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/69328/overview
    
    Original change's description:
    > Introduce unique_ptr based CreateBubble for bubble widgets
    >
    > Refactor bubble creation logic to provide a safer and more modern API.
    > This change introduces a new CreateBubble static method that returns a
    > std::unique_ptr<Widget> and defaults to CLIENT_OWNS_WIDGET ownership.
    > This approach helps prevent memory leaks and clarifies widget lifecycle
    > management.
    >
    > Key modifications include:
    > * Adding CreateBubbleInternal to consolidate shared initialization
    >     logic, including anchor view setup and observer attachment.
    > * Supporting an optional on_close callback in the new creation flow,
    >     allowing for synchronous handling of widget closure.
    > * Marking existing CreateBubbleDeprecated methods with warnings to
    >     discourage their continued use in favor of the unique_ptr variant.
    >
    > Bug: 509983464
    > Change-Id: I96d244f6e40aee9f05effdab16b6fac79dba30ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793292
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1626428}
    
    Bug: 509983464, 510617577
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I562b11427e093e81b8dc2e0d0c0a9f355a41260a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7826350
    Owners-Override: Christian Dullweber <dullweber@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626769}

--- chromeos/ash/experiences/arc/compat_mode/resize_toggle_menu.h
+++ chromeos/ash/experiences/arc/compat_mode/resize_toggle_menu.h
@@ -119,6 +119,7 @@ class ResizeToggleMenu : public views::WidgetObserver,
 
   base::CancelableOnceClosure auto_close_closure_;
 
+  std::unique_ptr<views::BubbleDialogDelegate> bubble_delegate_;
   std::unique_ptr<views::Widget> bubble_widget_;
 
   // Store only for testing.
