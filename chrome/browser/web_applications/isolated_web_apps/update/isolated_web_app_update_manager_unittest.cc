398e6e739c1d6f73b9fb45036143f6e4d8f16f67
Cameron Higgins <cameronhiggins@google.com>
2026-05-08 12:23:44 -0700

    macOS 26 - Disable `IsolatedWebAppUpdateManagerUpdateMockTimeTest`  case with timeout.
    Temporarily disables tests that use `task_environment()->FastForwardBy` for long periods of time as it causes failures/flakiness on MacOS 26.
    
    Bug: 434660312, 481036378
    Link: https://chromium-review.googlesource.com/id/I384571d2d75a0c942c37d6eb30602fe66a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828703
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Cameron Higgins <cameronhiggins@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627830}

--- chrome/browser/web_applications/isolated_web_apps/update/isolated_web_app_update_manager_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/update/isolated_web_app_update_manager_unittest.cc
@@ -87,6 +87,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/origin.h"
 
+#if BUILDFLAG(IS_MAC)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace web_app {
 namespace {
 
@@ -547,6 +551,13 @@ TEST_F(IsolatedWebAppUpdateManagerUpdateMockTimeTest,
 
 TEST_F(IsolatedWebAppUpdateManagerUpdateMockTimeTest,
        DiscoversAndPreparesUpdateOfPolicyInstalledAppsToPinnedVersion) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   InitialIwaBundleForceInstall(CreateIwa1Bundle(kInitialIwaVersion));
 
   // Pins the IWA to v2.0.0.
@@ -618,6 +629,13 @@ TEST_F(IsolatedWebAppUpdateManagerUpdateMockTimeTest,
 
 TEST_F(IsolatedWebAppUpdateManagerUpdateMockTimeTest,
        DoesNotDiscoverUpdateOfPolicyInstalledAppsWhenPinnedToIncorrectVersion) {
+#if BUILDFLAG(IS_MAC)
+  // TODO(crbug.com/434660312): Re-enable on macOS 26 once issues with
+  // unexpected test timeout failures are resolved.
+  if (base::mac::MacOSMajorVersion() == 26) {
+    GTEST_SKIP() << "Disabled on macOS Tahoe.";
+  }
+#endif
   InitialIwaBundleForceInstall(CreateIwa1Bundle(kInitialIwaVersion));
 
   // Pin IWA to a version that is higher than the latest version.
@@ -774,17 +792,16 @@ TEST_F(IsolatedWebAppUpdateManagerUpdateMockTimeTest, KeyRotationUpdateRetry) {
     return future.Take();
   };
 
-  ASSERT_THAT(
-      capture_discovery_task_result([&] {
-        // Rotate the signing key from ed25519 to ecdsaP256. This will
-        // trigger an unsuccessful update.
-        data_provider_.Update([&](auto& update) {
-          update.AddToKeyRotations(
-              GetIwa1WebBundleId(),
-              test::GetDefaultEcdsaP256KeyPair().public_key.bytes());
-        });
-      }),
-      ErrorIs(_));
+  ASSERT_THAT(capture_discovery_task_result([&] {
+                // Rotate the signing key from ed25519 to ecdsaP256. This will
+                // trigger an unsuccessful update.
+                data_provider_.Update([&](auto& update) {
+                  update.AddToKeyRotations(
+                      GetIwa1WebBundleId(),
+                      test::GetDefaultEcdsaP256KeyPair().public_key.bytes());
+                });
+              }),
+              ErrorIs(_));
 
   ASSERT_THAT(capture_discovery_task_result([&] {
                 // Fast forward by a minute -- this will trigger the first
