f7e727193579037de25f3627f434d9971ef274e7
Marwan Tammam <quarz@chromium.org>
2026-05-08 11:33:43 -0700

    Reland "Add ExtensionUserActivionService for recording transient user gestures"
    This reverts commit 6fd46c3689a635a47bee8d6ae553f4e6d7e3cf71.
    
    Reason for revert: Fixed failing unit tests
    
    Original change's description:
    > Revert "Add ExtensionUserActivionService for recording transient user gestures"
    >
    > This reverts commit 67fe37bd32388a788eb0106319c34168ed2b9e8e.
    >
    > Reason for revert: Causing test failures of
    > ExtensionUserActivationServiceTest.TransientActivation
    > ExtensionUserActivationServiceTest.TransientActivation_MultipleExtensions
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/59572/overview
    >
    > Original change's description:
    > > Add ExtensionUserActivionService for recording transient user gestures
    > >
    > > A transient gesture for an extension is activated when an extension function is called or an event is dispatched.
    > >
    > > Bug: b:494251632
    > > Change-Id: I54de6dfb045c23d6b03b523849a8c81a20da2467
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789733
    > > Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    > > Auto-Submit: Marwan Tammam <quarz@chromium.org>
    > > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1627344}
    >
    > Bug: b:494251632
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I162ba76951ebbf89449cdbafb055a92f104557d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831986
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1627449}
    
    Bug: b:494251632
    Change-Id: I68e36dfebecdeac9899a025547719f2a2167f0fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7832408
    Auto-Submit: Marwan Tammam <quarz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Marwan Tammam <quarz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627805}

--- extensions/browser/event_router_factory.cc
+++ extensions/browser/event_router_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_prefs_factory.h"
 #include "extensions/browser/extension_registry_factory.h"
+#include "extensions/browser/extension_user_activation_service_factory.h"
 #include "extensions/browser/extensions_browser_client.h"
 #include "extensions/browser/process_manager_factory.h"
 
@@ -36,6 +37,7 @@ EventRouterFactory::EventRouterFactory()
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(ExtensionRegistryFactory::GetInstance());
   DependsOn(ExtensionPrefsFactory::GetInstance());
+  DependsOn(ExtensionUserActivationServiceFactory::GetInstance());
   DependsOn(ProcessManagerFactory::GetInstance());
 }
 
