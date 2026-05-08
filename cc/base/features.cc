491213787072991eb48fe0c7d318313fc4387169
Benoît Lizé <lizeb@chromium.org>
2026-05-08 15:16:34 -0700

    [cc] Add a feature to prioritize exact resource size reuse
    When cc::ResourcePool looks for a resource to re-use, it proceeds in
    strictly MRU ordering, and thus allows resources that are up to 2x
    oversized (in area). This wastes memory in the case where there is for
    instance a layer that is downsizing, as the too-large resource will
    likely be used all the time.
    
    This CL adds a feature to prefer exact size reuse, but still preserve
    MRU ordering, within exact and inexact size matches.
    
    Bug: 505766103
    Change-Id: I0fe0c2e057175146fef700042f04457992047e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7832935
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627956}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -264,4 +264,7 @@ BASE_FEATURE(kBrowserControlsScrollSnapAnimation,
 BASE_FEATURE(kSelectionEdgeVisibilityUsesFullEdge,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kResourcePoolPreferExactSizeReuse,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
