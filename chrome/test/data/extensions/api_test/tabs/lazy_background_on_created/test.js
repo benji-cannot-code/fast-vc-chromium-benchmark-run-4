d248fa966efe906cadf90d173b5b9a317d51281f
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-05-08 16:36:04 -0700

    [Desktop Android] Update TODOs in Tabs.OnCreated tests
    Navigate() doesn't atomically create an active tab on desktop android,
    which results in the onCreated event not being fired with an active
    tab. Update TODOs in the related tests to point to a specific bug for
    this scenario.
    
    Bug: 430344931, 511254270
    Change-Id: I4142cc458f61b0d79174a90f31cc30735ab088b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833056
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628012}

--- chrome/test/data/extensions/api_test/tabs/lazy_background_on_created/test.js
+++ chrome/test/data/extensions/api_test/tabs/lazy_background_on_created/test.js
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 chrome.test.runTests([
   async function testCreateWithActiveTrue() {
-    // TODO(https://crbug.com/430344931): `NavigateParams::tabstrip_add_types`
-    // isn't supported on android builds yet, so we can't create active tabs.
+    // TODO(https://crbug.com/511254270): `NavigateParams::tabstrip_add_types`
+    // with ADD_ACTIVE isn't fully supported on desktop android yet.
     if ((await chrome.runtime.getPlatformInfo()).os === 'android') {
       chrome.test.succeed('skipped');
       return;
