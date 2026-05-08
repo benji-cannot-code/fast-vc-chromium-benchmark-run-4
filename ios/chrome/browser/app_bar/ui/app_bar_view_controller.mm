383e14f16d0ffc1d4a0cdc5725eb9245b32fa48c
Chris Lu <thegreenfrog@google.com>
2026-05-08 08:47:57 -0700

    [iOS] Temporarily show AppBar button titles in context menu previews
    When in fullscreen mode, the buttons' title texts are hidden by setting
    their alpha to 0.0. The button configuration still reserves space for
    the title text, which causes the custom preview to display an empty
    space at the bottom.
    
    This CL temporarily overrides the title alpha to 1.0 for the button currently while I it being previewed, and animates the alpha back to its
    original value during the dismissal of the context menu.
    
    Video: https://screencast.googleplex.com/cast/NDcwODk4Mzg0NjczMTc3NnxjMjllNjNhMy00ZA
    
    Fixed: 509462492
    Change-Id: Id17e04769eb26064494af23cf2e01b836e095611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827982
    Reviewed-by: Eric Ekey <ericekey@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627678}

--- ios/chrome/browser/app_bar/ui/app_bar_view_controller.mm
+++ ios/chrome/browser/app_bar/ui/app_bar_view_controller.mm
@@ -173,6 +173,8 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
   // Spacers to for button layout in landscape.
   UIView* _leadingSpacer;
   UIView* _trailingSpacer;
+  // The button currently being previewed by a context menu.
+  __weak UIButton* _previewedButton;
 }
 
 - (void)dealloc {
@@ -691,9 +693,11 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
 
 // Updates the title configuration for buttons.
 - (void)updateButtonTitleConfiguration:(UIButtonConfiguration*)config
-                        highlightAlpha:(CGFloat)highlightAlpha {
+                        highlightAlpha:(CGFloat)highlightAlpha
+                                button:(UIButton*)button {
   // Text fades on highlight/disabled AND scroll.
-  CGFloat textAlpha = highlightAlpha * _buttonsTitleAlpha;
+  CGFloat targetAlpha = (button == _previewedButton) ? 1.0 : _buttonsTitleAlpha;
+  CGFloat textAlpha = highlightAlpha * targetAlpha;
 
   config.titleTextAttributesTransformer =
       ^NSDictionary<NSAttributedStringKey, id>*(
@@ -724,7 +728,9 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
     return [baseColor colorWithAlphaComponent:activeAlpha];
   };
 
-  [self updateButtonTitleConfiguration:config highlightAlpha:activeAlpha];
+  [self updateButtonTitleConfiguration:config
+                        highlightAlpha:activeAlpha
+                                button:button];
 
   button.configuration = config;
 }
@@ -741,7 +747,9 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
 
   CGFloat highlightAlpha = ButtonHighlightAlpha(button);
 
-  [self updateButtonTitleConfiguration:config highlightAlpha:highlightAlpha];
+  [self updateButtonTitleConfiguration:config
+                        highlightAlpha:highlightAlpha
+                                button:button];
 
   UIColor* symbolColor = ButtonsForegroundColor();
   UIColor* baseLabelColor =
@@ -1043,6 +1051,12 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
     return nil;
   }
 
+  if ([view isKindOfClass:[UIButton class]]) {
+    _previewedButton = (UIButton*)view;
+    [_previewedButton setNeedsUpdateConfiguration];
+    [_previewedButton layoutIfNeeded];
+  }
+
   return [UIContextMenuConfiguration
       configurationWithIdentifier:nil
                   previewProvider:nil
@@ -1083,6 +1097,21 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
     return [[UITargetedPreview alloc] initWithView:view parameters:parameters];
   }
   return nil;
+}
+
+- (void)contextMenuInteraction:(UIContextMenuInteraction*)interaction
+       willEndForConfiguration:(UIContextMenuConfiguration*)configuration
+                      animator:(id<UIContextMenuInteractionAnimating>)animator {
+  if (interaction.view == _previewedButton) {
+    __weak __typeof(self) weakSelf = self;
+    [animator addAnimations:^{
+      __strong __typeof(weakSelf) strongSelf = weakSelf;
+      if (strongSelf) {
+        strongSelf->_previewedButton = nil;
+        [interaction.view setNeedsUpdateConfiguration];
+      }
+    }];
+  }
 }
 
 @end
