35a106732c99214722bdb6a2999b1d6fbcb496fa
dresslerd <dresslerd@google.com>
2026-05-08 10:25:02 -0700

    [OTP][Otp4Bd] Register OneTimeTokenServiceFactory early on.
    Else it crashes when used later on.
    That also explains the crash in the test.
    
    Bug: b:500265255
    Change-Id: I9b27c50876f4f724a56d093fd5c90dd26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827573
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Bekzhan Kassenov <bkassenov@google.com>
    Commit-Queue: Daniel Dressler <dresslerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627741}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/autofill_optimization_guide_decider_factory.h"
 #include "chrome/browser/autofill/merchant_promo_code_manager_factory.h"
 #include "chrome/browser/autofill/ml_log_router_factory.h"
+#include "chrome/browser/autofill/one_time_token_service_factory.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/autofill/valuables_data_manager_factory.h"
 #include "chrome/browser/autofill/wallet_pass_access_manager_factory.h"
@@ -771,6 +772,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   autofill::AutofillOptimizationGuideDeciderFactory::GetInstance();
   autofill::MerchantPromoCodeManagerFactory::GetInstance();
   autofill::MlLogRouterFactory::GetInstance();
+  autofill::OneTimeTokenServiceFactory::GetInstance();
   autofill::PersonalDataManagerFactory::GetInstance();
   autofill::ValuablesDataManagerFactory::GetInstance();
   autofill::WalletPassAccessManagerFactory::GetInstance();
