1f0c9d36e779eeccbb5b76c9af7ed3e94d9199c8
Brian Begnoche <bcb@chromium.org>
2026-05-08 10:36:36 -0700

    Add URL scheme and method checks to PhishingImageEmbedder.
    This change modifies PhishingImageEmbedder to only perform image
    embedding for URLs with HTTP or HTTPS schemes and for GET requests. If
    the URL does not meet these criteria, the embedding process is aborted,
    and the failure callback is run. A browsertest is added to verify that
    image embedding fails for unsupported schemes.
    
    These checks are identical to the checks that are already performed in
    PhishingClassifier::BeginClassification.
    
    Bug: 508204412
    
    Change-Id: I4f200fc201505e5daa89ca1ccb8278abd12562b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801366
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Brian Begnoche <bcb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627753}

--- components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier.cc
+++ components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/buildflags.h"
 #include "components/safe_browsing/content/common/visual_utils.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/phishing_classifier_delegate.h"
+#include "components/safe_browsing/content/renderer/phishing_classifier/phishing_dom_utils.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/phishing_visual_feature_extractor.h"
 #include "components/safe_browsing/content/renderer/phishing_classifier/scorer.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -83,18 +84,16 @@ void PhishingClassifier::BeginClassification(DoneCallback done_callback) {
 
   blink::WebLocalFrame* frame = render_frame_->GetWebFrame();
 
-  // Check whether the URL is one that we should classify.
-  // Currently, we only classify http/https URLs that are GET requests.
-  GURL url(frame->GetDocument().Url());
-  if (!url.SchemeIsHTTPOrHTTPS()) {
-    RunFailureCallback(Result::kInvalidURLFormatRequest);
-    return;
-  }
-
-  blink::WebDocumentLoader* document_loader = frame->GetDocumentLoader();
-  if (!document_loader || document_loader->HttpMethod().Ascii() != "GET") {
-    RunFailureCallback(Result::kInvalidDocumentLoader);
-    return;
+  PhishingProcessStatus status = CanPerformPhishingDetection(frame);
+  switch (status) {
+    case PhishingProcessStatus::kInvalidUrlFormat:
+      RunFailureCallback(PhishingClassifier::Result::kInvalidURLFormatRequest);
+      return;
+    case PhishingProcessStatus::kInvalidDomLoader:
+      RunFailureCallback(PhishingClassifier::Result::kInvalidDocumentLoader);
+      return;
+    case PhishingProcessStatus::kValid:
+      break;
   }
 
   // For consistency, we always want to invoke the DoneCallback
---
+++ components/safe_browsing/content/renderer/phishing_classifier/phishing_dom_utils.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/safe_browsing/content/renderer/phishing_classifier/phishing_dom_utils.h"
+
+#include "third_party/blink/public/web/web_document.h"
+#include "third_party/blink/public/web/web_document_loader.h"
+#include "third_party/blink/public/web/web_local_frame.h"
+#include "url/gurl.h"
+
+namespace safe_browsing {
+
+PhishingProcessStatus CanPerformPhishingDetection(blink::WebLocalFrame* frame) {
+  if (!frame) {
+    return PhishingProcessStatus::kInvalidDomLoader;
+  }
+  GURL url(frame->GetDocument().Url());
+  if (!url.SchemeIsHTTPOrHTTPS()) {
+    return PhishingProcessStatus::kInvalidUrlFormat;
+  }
+
+  blink::WebDocumentLoader* document_loader = frame->GetDocumentLoader();
+  if (!document_loader || document_loader->HttpMethod().Ascii() != "GET") {
+    return PhishingProcessStatus::kInvalidDomLoader;
+  }
+
+  return PhishingProcessStatus::kValid;
+}
+
+}  // namespace safe_browsing
