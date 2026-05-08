3de392ab603e413ae3ceaaa6bfe300d24410e7e1
Ryo Hashimoto <hashimoto@google.com>
2026-05-08 00:04:06 -0700

    android: Disable WebContentsAccessibilityTest#testPerformAction_setExtendedSelection_atEndOfEmptyTextAnchor
    This test has been flaky.
    
    BUG=511014514
    
    Change-Id: I8fc0378b9a923474132d612d857e8f411b1ef91c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831993
    Owners-Override: Ryo Hashimoto <hashimoto@chromium.org>
    Auto-Submit: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Masa Fujita <massan@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627496}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTest.java
@@ -3166,6 +3166,7 @@ public class WebContentsAccessibilityTest {
     /** Test extended selection with a leaf node at the end of root to trigger at_end_of_anchor. */
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/511014514")
     public void testPerformAction_setExtendedSelection_atEndOfEmptyTextAnchor() throws Throwable {
         setupTestWithHTML(
                 """
