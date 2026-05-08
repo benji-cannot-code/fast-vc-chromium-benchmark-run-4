2570004c8d912ff406ebeee1bf5bddcebe466a48
Sangwhan Moon <sxm@google.com>
2026-05-08 12:10:15 -0700

    Spoof Android Desktop User Agent as ChromeOS instead of Linux
    Adds the `kAndroidDesktopUASpoofAsChromeOS` feature to report Android
    devices as ChromeOS instead of Linux.
    
    Details in go/white-lie-to-fix-another-white-lie
    
    Bug: 507566377
    Change-Id: I8ea53c89ec9fa59bbacf015d54b8d113564f96f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802707
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sangwhan Moon <sxm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627820}

--- chrome/test/data/webui/js/icon_test.ts
+++ chrome/test/data/webui/js/icon_test.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {getFavicon, getFaviconForPageURL, getFaviconUrl, getFileIconUrl} from 'chrome://resources/js/icon.js';
 import type {GetFaviconUrlParams} from 'chrome://resources/js/icon.js';
-import {isAndroid, isChromeOS, isLinux, isMac, isWindows} from 'chrome://resources/js/platform.js';
+import {isAndroid, isIOS} from 'chrome://resources/js/platform.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 
 suite('IconTest', function() {
@@ -93,9 +93,7 @@ suite('IconTest', function() {
           encodeURIComponent(url) + '&allowGoogleServerFallback=0") ' +
           `${window.devicePixelRatio}x)`;
 
-      // Android simulator returns true for isLinux, so also check isAndroid.
-      const isDesktop =
-          (isMac || isChromeOS || isWindows || isLinux) && !isAndroid;
+      const isDesktop = !isIOS && !isAndroid;
       return isDesktop ? expectedDesktop : expectedOther;
     }
 
@@ -131,9 +129,7 @@ suite('IconTest', function() {
         encodeURIComponent('http://foo.com/foo.ico') + '") ' +
         `${window.devicePixelRatio}x)`;
 
-    // Android simulator returns true for isLinux, so also check isAndroid.
-    const isDesktop =
-        (isMac || isChromeOS || isWindows || isLinux) && !isAndroid;
+    const isDesktop = !isIOS && !isAndroid;
     const expected = isDesktop ? expectedDesktop : expectedOther;
     assertEquals(expected, getFavicon(url));
   });
