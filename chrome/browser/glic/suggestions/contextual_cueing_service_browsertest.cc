bc818b81eb3ba769900d73f38925cfa327e8c2a9
Christian Dullweber <dullweber@chromium.org>
2026-05-08 03:48:19 -0700

    PrivateAi: Enable backend by default
    Enable the PrivateAi backend by default and use the Google Chrome API
    key if no other key is set. Individual features are still guarded by
    their specific Feature flag.
    
    Bug: 488268379
    Change-Id: I48db69a36b6e2916aad4811591d9eb92f86d24b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816400
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627555}

--- chrome/browser/glic/suggestions/contextual_cueing_service_browsertest.cc
+++ chrome/browser/glic/suggestions/contextual_cueing_service_browsertest.cc
@@ -62,7 +62,7 @@ class ContextualCueingServiceBrowserTestZSSFlag
             {kGlicZeroStateSuggestions,
              {{"ZSSAllowContextualSuggestionsForSearchResultsPages", "false"}}},
         },
-        {});
+        {{kZeroStateSuggestionsUsePrivateAi}});
     // Initialize `scoped_prewarm_feature_list_` after the
     // `scoped_feature_list_` that will be removed in the parent class's
     // destructor, so that these instances are destroyed in the reversed order.
@@ -353,7 +353,7 @@ class ContextualCueingServiceBrowserTestAllowZSSForSrp
         {{kGlicZeroStateSuggestions,
           {{"ZSSAllowContextualSuggestionsForSearchResultsPages", "true"}}},
          {features::kGlic, {}}},
-        {});
+        {{kZeroStateSuggestionsUsePrivateAi}});
   }
 
   void SetUpOnMainThread() override {
--- components/private_ai/features.cc
+++ components/private_ai/features.cc
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/private_ai/features.h"
 
+#include "base/feature.h"
 #include "base/feature_list.h"
 
 namespace private_ai {
 
-BASE_FEATURE(kPrivateAi, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPrivateAi, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivateAiServerAttestation, base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -21,7 +22,7 @@ const base::FeatureParam<std::string> kPrivateAiApiKey{
 const base::FeatureParam<std::string> kPrivateAiUrl{
     &kPrivateAi, /*name=*/"url",
     /*default_value=*/
-    "privatearatea-pa.googleapis.com/ws/"
+    "privatearatea.pa.googleapis.com/ws/"
     "mdi.privatearatea.PrivateArateaService.StartNoiseSession"};
 
 const base::FeatureParam<std::string> kPrivateAiProxyServerUrl{
