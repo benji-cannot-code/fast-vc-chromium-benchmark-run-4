f71d3b7bc6bd8c4c6e76ce7224d9d43081701767
Abhijith Nair <abhijithnair@chromium.org>
2026-05-08 13:13:20 -0700

    Remove unnecessary Chromium initialization from setBackgroundColor.
    Based on UMA data from the experiment, a large fraction of startups
    happen in setBackgroundColor in the experiment arms.
    
    Bug: 509906038
    Change-Id: I3f9d575fc0626831db0b50a1a0082f7eef99b3dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827746
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627875}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
@@ -4170,7 +4170,6 @@ class WebViewChromium
 
     @Override
     public void setBackgroundColor(final int color) {
-        mAwInit.triggerAndWaitForChromiumStarted(CallSite.WEBVIEW_INSTANCE_SET_BACKGROUND_COLOR);
         if (checkNeedsPost()) {
             mFactory.addTask(
                     new Runnable() {
