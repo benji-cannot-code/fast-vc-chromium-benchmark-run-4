4955aa2ae4e657e01c2bef9166bc50f9269a0cf9
Stephen McGruer <smcgruer@chromium.org>
2026-05-08 10:38:32 -0700

    [WebPayments] Add UKM metric for WindowSizeCheckRejectionReason
    Register a new UKM metric 'WindowSizeCheckRejectionReason' under the
    'PaymentRequest.CheckoutEvents' event in ukm.xml, which mirrors the
    UMA histogram PaymentRequest.WindowSizeCheckRejectionReason.
    
    UKM approval: b/509838997
    
    WindowSizeCheckRejectionUkm,
    PaymentRequestWindowSizeTest.*,
    PaymentHandlerWindowSizeTest.*
    
    Bug: 421927614
    Test: JourneyLoggerTest.RecordJourneyStatsHistograms_
    Change-Id: Ib41a169a1f13bafe8b4c4eeb9a890f05c6158b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817894
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627754}

--- chrome/browser/ui/views/payments/payment_request_dialog_view.cc
+++ chrome/browser/ui/views/payments/payment_request_dialog_view.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/payments/content/payment_request.h"
 #include "components/payments/core/error_strings.h"
 #include "components/payments/core/features.h"
+#include "components/payments/core/journey_logger.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/browser_thread.h"
@@ -134,9 +135,8 @@ bool PaymentRequestDialogView::ShouldShowCloseButton() const {
 
 void PaymentRequestDialogView::ShowDialog() {
   if (!DialogFitsInBrowserWindow()) {
-    base::UmaHistogramEnumeration(
-        "PaymentRequest.WindowSizeCheckRejectionReason",
-        WindowSizeCheckRejectionReason::kRejectedAtShow);
+    request_->SetWindowSizeCheckRejectionReason(
+        JourneyLogger::WindowSizeCheckRejectionReason::kRejectedAtShow);
 
     // To avoid tearing down the PaymentRequest class in the middle of showing
     // the dialog, we post this call asynchronously.
@@ -227,9 +227,9 @@ void PaymentRequestDialogView::ShowPaymentHandlerScreen(
   // Once we have resized the dialog, re-check that it still fits in the
   // available window space.
   if (!DialogFitsInBrowserWindow()) {
-    base::UmaHistogramEnumeration(
-        "PaymentRequest.WindowSizeCheckRejectionReason",
-        WindowSizeCheckRejectionReason::kRejectedAtPaymentHandlerTransition);
+    request_->SetWindowSizeCheckRejectionReason(
+        JourneyLogger::WindowSizeCheckRejectionReason::
+            kRejectedAtPaymentHandlerTransition);
     std::move(callback).Run(false, 0, 0);
     request_->OnInternalError(errors::kBrowserWindowTooSmall);
     return;
@@ -757,9 +757,8 @@ void PaymentRequestDialogView::ResizeDialogWindow() {
 void PaymentRequestDialogView::CheckIfDialogFitsInBrowserWindow() {
   last_check_for_too_small_window_time_ = base::TimeTicks::Now();
   if (!DialogFitsInBrowserWindow()) {
-    base::UmaHistogramEnumeration(
-        "PaymentRequest.WindowSizeCheckRejectionReason",
-        WindowSizeCheckRejectionReason::kRejectedAtResize);
+    request_->SetWindowSizeCheckRejectionReason(
+        JourneyLogger::WindowSizeCheckRejectionReason::kRejectedAtResize);
     request_->OnInternalError(errors::kBrowserWindowTooSmall);
   }
 }
