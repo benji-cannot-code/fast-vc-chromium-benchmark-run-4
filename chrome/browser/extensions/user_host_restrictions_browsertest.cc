7bc11f60d0788eb4a840c4ea0ae62af1a370042b
James Cook <jamescook@chromium.org>
2026-05-07 17:45:25 -0700

    extensions: Port UserHostRestrictionsBrowserTest to desktop Android
    We missed this one during initial bring-up of desktop Android. The test
    passes with minimal modifications.
    
    Bug: 469417243
    Change-Id: I8bd4578080c83a5df66e0628bdfd9c3798f2e3b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830225
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627381}

--- chrome/browser/extensions/user_host_restrictions_browsertest.cc
+++ chrome/browser/extensions/user_host_restrictions_browsertest.cc
@@ -8,13 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/background_script_executor.h"
 #include "extensions/browser/permissions/scripting_permissions_modifier.h"
 #include "extensions/browser/permissions_manager.h"
 #include "extensions/browser/script_executor.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_features.h"
 #include "extensions/common/mojom/api_permission_id.mojom.h"
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/test/test_extension_dir.h"
 #include "net/dns/mock_host_resolver.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -41,7 +43,7 @@ class UserHostRestrictionsBrowserTest
   ~UserHostRestrictionsBrowserTest() override = default;
 
   void SetUpOnMainThread() override {
-    ExtensionBrowserTest::SetUpOnMainThread();
+    ExtensionApiTest::SetUpOnMainThread();
 
     host_resolver()->AddRule("*", "127.0.0.1");
   }
