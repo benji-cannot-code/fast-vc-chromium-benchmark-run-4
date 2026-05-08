428c791566e6c6b451b7eec53f8507f465e6224a
Lijin Shen <lazzzis@google.com>
2026-05-08 13:53:46 -0700

    Remove right edge gesture forward experiment
    Remove feature flag and BackPressResult.IGNORED
    
    Bug: 510924996
    Change-Id: I09d06db585460419e3854989675a946bc42a8e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828770
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627898}

--- chrome/browser/back_press/android/java/src/org/chromium/chrome/browser/back_press/BackPressManager.java
+++ chrome/browser/back_press/android/java/src/org/chromium/chrome/browser/back_press/BackPressManager.java
@@ -104,7 +104,6 @@ public class BackPressManager implements Destroyable, BackPressHandlerRegistry {
                     case BackPressResult.SUCCESS:
                         return true;
                     case BackPressResult.UNKNOWN:
-                    case BackPressResult.IGNORED:
                         return null;
                 }
             } else {
@@ -146,7 +145,7 @@ public class BackPressManager implements Destroyable, BackPressHandlerRegistry {
                     mLastCalledHandlerType = index;
                     if (result == BackPressResult.FAILURE) {
                         BackPressManager.this.handleBackPress();
-                    } else if (result != BackPressResult.IGNORED) {
+                    } else {
                         record(index);
                     }
                 } else {
@@ -470,7 +469,7 @@ public class BackPressManager implements Destroyable, BackPressHandlerRegistry {
                 if (res == BackPressResult.FAILURE) {
                     failed = true;
                     recordFailure(i);
-                } else if (res != BackPressResult.IGNORED) {
+                } else {
                     record(i);
                     return;
                 }
--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -600,8 +600,6 @@ public abstract class ChromeFeatureList {
     public static final String RENAME_JOURNEYS = "RenameJourneys";
     public static final String REPORT_NOTIFICATION_CONTENT_DETECTION_DATA =
             "ReportNotificationContentDetectionData";
-    public static final String RIGHT_EDGE_GOES_FORWARD_GESTURE_NAV =
-            "RightEdgeGoesForwardGestureNav";
     public static final String ROBUST_WINDOW_MANAGEMENT_EXPERIMENTAL =
             "RobustWindowManagementExperimental";
     public static final String SAFETY_HUB = "SafetyHub";
@@ -1116,8 +1114,6 @@ public abstract class ChromeFeatureList {
                     REPORT_NOTIFICATION_CONTENT_DETECTION_DATA,
                     /* defaultValue= */ false,
                     /* defaultValueInTests= */ true);
-    public static final CachedFlag sRightEdgeGoesForwardGestureNav =
-            newCachedFlag(RIGHT_EDGE_GOES_FORWARD_GESTURE_NAV, false);
     public static final CachedFlag sSafetyHubWeakAndReusedPasswords =
             newCachedFlag(SAFETY_HUB_WEAK_AND_REUSED_PASSWORDS, false);
     public static final CachedFlag sSearchInCCT =
@@ -1373,7 +1369,6 @@ public abstract class ChromeFeatureList {
                     sPriceChangeModule,
                     sProtectRecentlyVisibleTab,
                     sReportNotificationContentDetectionData,
-                    sRightEdgeGoesForwardGestureNav,
                     sSafetyHubWeakAndReusedPasswords,
                     sSearchInCCT,
                     sSearchInCCTAlternateTapHandling,
