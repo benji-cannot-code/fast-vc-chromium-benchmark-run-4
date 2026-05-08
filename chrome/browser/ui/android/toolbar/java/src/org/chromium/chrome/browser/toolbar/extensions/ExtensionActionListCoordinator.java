62f1c9bbae48f6b8e012345afb45515c65e92189
Masa Fujita <massan@google.com>
2026-05-08 12:41:56 -0700

    extensions: Dismiss popups on new dialogs
    We monitor ModalDialogManager and dismiss extension action popups when
    new dialogs are added. This does not prohibit the user from opening the
    popup on top of dialogs. We also do not dismiss context menus because
    they aren't supposed to be ephemeral.
    
    Bug: 449801118
    Change-Id: I3e62988e6c6985a12ffb948cbf8f1855b080c01e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7826209
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627848}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionActionListCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionActionListCoordinator.java
@@ -33,6 +33,7 @@ import org.chromium.content_public.browser.selection.SelectionDropdownMenuDelega
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.listmenu.ListMenuButton;
+import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -64,7 +65,8 @@ public class ExtensionActionListCoordinator implements Destroyable {
             ViewGroup rootView,
             @Nullable ContextMenuPopulatorFactory contextMenuPopulatorFactory,
             @Nullable SelectionDropdownMenuDelegate selectionDropdownMenuDelegate,
-            TabModelSelector tabModelSelector) {
+            TabModelSelector tabModelSelector,
+            ModalDialogManager modalDialogManager) {
         mContext = context;
         mContainer = container;
 
@@ -81,7 +83,8 @@ public class ExtensionActionListCoordinator implements Destroyable {
                         extensionsToolbarBridge,
                         contextMenuPopulatorFactory,
                         selectionDropdownMenuDelegate,
-                        tabModelSelector);
+                        tabModelSelector,
+                        modalDialogManager);
 
         ExtensionsToolbarDragTouchHandler dragTouchHandler =
                 new ExtensionsToolbarDragTouchHandler(context, mModels);
