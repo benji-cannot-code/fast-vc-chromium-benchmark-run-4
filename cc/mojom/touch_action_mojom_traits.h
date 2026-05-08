943e29fe426dcfef519fbe6e6e4a1f66c77a3bc0
Tom Sepez <tsepez@google.com>
2026-05-08 16:51:59 -0700

    Convert old cc/ipc traits to mojom
    Gemini-generated patch cleanup.
    
    Bug: 40621529
    Change-Id: I930412c679ab477d45d6a799c6c2f83462f33ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7821767
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628022}

---
+++ cc/mojom/touch_action_mojom_traits.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CC_MOJOM_TOUCH_ACTION_MOJOM_TRAITS_H_
+#define CC_MOJOM_TOUCH_ACTION_MOJOM_TRAITS_H_
+
+#include <stdint.h>
+
+#include "cc/input/touch_action.h"
+#include "cc/mojom/touch_action.mojom-shared.h"
+#include "mojo/public/cpp/bindings/struct_traits.h"
+
+namespace mojo {
+
+template <>
+struct StructTraits<cc::mojom::TouchActionDataView, cc::TouchAction> {
+  static uint32_t value(const cc::TouchAction& input) {
+    return static_cast<uint32_t>(input);
+  }
+
+  static bool Read(cc::mojom::TouchActionDataView data, cc::TouchAction* out) {
+    uint32_t value = data.value();
+    if (value > static_cast<uint32_t>(cc::TouchAction::kMax)) {
+      return false;
+    }
+    *out = static_cast<cc::TouchAction>(value);
+    return true;
+  }
+};
+
+}  // namespace mojo
+
+#endif  // CC_MOJOM_TOUCH_ACTION_MOJOM_TRAITS_H_
