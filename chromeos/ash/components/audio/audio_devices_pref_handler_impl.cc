39cdc33ad88b7c2d69194e2925c9291cf68d1901
Hung-Hsien Chen <hunghsienchen@google.com>
2026-05-08 08:20:51 -0700

    Listen to Krisp NC pref state change and send to CRAS
    The Krisp NC pref state will reflect the Admin Console Policy change.
    Listen to the pref change in audio_devices_pref_handler_impl, and then
    notify cras_audio_handler, and finally communicate the state back to
    CRAS through D-Bus call in cras_audio_client.
    
    For CrasAudioHandler::InitializeAudioAfterCrasServiceAvailable(), in case CRAS restarts, we used to call RefreshVoiceIsolationState(), now changed to calling OnVoiceIsolationPrefChanged() to refresh all voice isolation related states.
    
    Bug=b:463837927
    Test=autoninja -C out/Default chromeos_unittests && out/Default/chromeos_unittests --gtest_filter="*KrispNoiseCancellation*"
    
    Change-Id: I318ae81a5da68ea897cb28f93e0cdf8eacc32462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7813983
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Hung-Hsien Chen <hunghsienchen@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627663}

--- chromeos/ash/components/audio/audio_devices_pref_handler_impl.cc
+++ chromeos/ash/components/audio/audio_devices_pref_handler_impl.cc
@@ -459,6 +459,16 @@ void AudioDevicesPrefHandlerImpl::SetVoiceIsolationPreferredEffect(
                            static_cast<int>(effect));
 }
 
+bool AudioDevicesPrefHandlerImpl::GetKrispNoiseCancellationState() {
+  return local_state_->GetBoolean(prefs::kInputKrispNoiseCancellationEnabled);
+}
+
+void AudioDevicesPrefHandlerImpl::SetKrispNoiseCancellationState(
+    bool krisp_noise_cancellation_state) {
+  local_state_->SetBoolean(prefs::kInputKrispNoiseCancellationEnabled,
+                           krisp_noise_cancellation_state);
+}
+
 bool AudioDevicesPrefHandlerImpl::GetNoiseCancellationState() {
   return local_state_->GetBoolean(prefs::kInputVoiceIsolationEnabled);
 }
@@ -540,6 +550,8 @@ void AudioDevicesPrefHandlerImpl::InitializePrefObservers() {
   pref_change_registrar_.Add(prefs::kInputVoiceIsolationEnabled,
                              callbackVoiceIsolation);
   pref_change_registrar_.Add(prefs::kInputVoiceIsolationPreferredEffect,
+                             callbackVoiceIsolation);
+  pref_change_registrar_.Add(prefs::kInputKrispNoiseCancellationEnabled,
                              callbackVoiceIsolation);
 }
 
--- chromeos/ash/components/audio/cras_audio_handler.cc
+++ chromeos/ash/components/audio/cras_audio_handler.cc
@@ -644,6 +644,17 @@ void CrasAudioHandler::RecordVoiceIsolationPreferredEffectChange(
       kVoiceIsolationPreferredEffectChangeHistogramName, preferred_effect);
 }
 
+bool CrasAudioHandler::GetKrispNoiseCancellationState() const {
+  return audio_pref_handler_->GetKrispNoiseCancellationState();
+}
+
+void CrasAudioHandler::RefreshKrispNoiseCancellationState() {
+  // Refresh should only update the state in CRAS and leave the preference
+  // as-is.
+  CrasAudioClient::Get()->SetKrispNoiseCancellationEnabled(
+      GetKrispNoiseCancellationState());
+}
+
 bool CrasAudioHandler::IsNoiseCancellationSupportedForDevice(
     uint64_t device_id) {
   if (!noise_cancellation_supported()) {
@@ -1735,6 +1746,7 @@ void CrasAudioHandler::OnAudioPolicyPrefChanged() {
 void CrasAudioHandler::OnVoiceIsolationPrefChanged() {
   RefreshVoiceIsolationState();
   RefreshVoiceIsolationPreferredEffect();
+  RefreshKrispNoiseCancellationState();
 }
 
 const AudioDevice* CrasAudioHandler::GetDeviceFromId(uint64_t device_id) const {
@@ -1904,7 +1916,7 @@ void CrasAudioHandler::InitializeAudioAfterCrasServiceAvailable(
   }
 
   // Refreshes voice isolation state in CRAS, in case CRAS restarts.
-  RefreshVoiceIsolationState();
+  OnVoiceIsolationPrefChanged();
 
   // Sets speak-on-mute detection enabled based on local variable, it re-applies
   // the previous state if CRAS restarts.
