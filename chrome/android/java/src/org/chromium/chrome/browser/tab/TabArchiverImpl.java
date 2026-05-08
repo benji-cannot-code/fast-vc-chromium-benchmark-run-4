5398b55549ecbce004a35e10e460a493dddfe4bb
Brandon Wylie <wylieb@google.com>
2026-05-08 16:06:51 -0700

    [declutter] Run autodelete pass even if nothing was archived
    Bug: 511256104
    Change-Id: I7647e648cff5490ac44b6a825e9b90c2f66ddecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833044
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627995}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabArchiverImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabArchiverImpl.java
@@ -136,6 +136,8 @@ public class TabArchiverImpl implements TabArchiver {
 
         if (tabsToArchive.size() > 0) {
             archiveAndRemoveTabs(model, tabsToArchive);
+        } else {
+            broadcastPersistedTabDataCreated();
         }
 
         if (tabsToClose.size() > 0) {
