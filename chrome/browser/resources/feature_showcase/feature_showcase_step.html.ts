e1a63df26cda8633c5c8cea8292e6f1111c715bf
Kacper Bajkiewicz <kbajkiewicz@google.com>
2026-05-08 10:24:18 -0700

    [FRE Revamp] Add common FeatureShowcaseStepElement.
    Views in feature showcase share very similar layout which can be
    extracted into a common element.
    
    This component is displayed in side-by-side composition (one side being
    illustration (or theme picker for theme showcase) and second one feature
    description along with buttons to proceed). Text, background
    (illustration / theme picker), and buttons are slottable which makes it
    customizable by the client.
    
    Buttons container layout is handled by FeatureShowcaseElement (as shown
    on the screencast), client only defines number of buttons / its class /
    and callback. For now, we invert the layout based on OS (as for default
    browser view) which might be changed in the future (TBD if other
    features need it).
    
    Steps to be shown are parsed from an URL and its serving from C++
    handler will be done in future CLs.
    
    View from this CL can be tested via navigating to:
    chrome://feature-showcase/?steps=example.
    
    Screencast: https://screenshot.googleplex.com/4BguucxyrMCC99F
    
    steps will be investigated and fixed later (if possible).
    
    Note: Tests will be added in later CLs. Small flickering in transition between
    Bug: 500274411
    Change-Id: Ibc79321830277cd68ccb5c03bfdf8c6e853c3916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7786149
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Commit-Queue: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Auto-Submit: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627740}

---
+++ chrome/browser/resources/feature_showcase/feature_showcase_step.html.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {FeatureShowcaseStepElement} from './feature_showcase_step.js';
+
+export function getHtml(this: FeatureShowcaseStepElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div class="content-child" id="showcase-illustration">
+  <slot name="illustration"></slot>
+</div>
+
+<div class="content-child" id="showcase-description">
+<!-- TODO(crbug.com/500272103): Substitute placeholder with real stepper. -->
+  <div id="stepper-placeholder">
+    <slot name="header-icon"></slot>
+  </div>
+  <div id="showcase-text">
+    <h1 class="title"><slot name="title"></slot></h1>
+    <p class="subtitle"><slot name="description"></slot></p>
+    <div id="button-container">
+      <slot name="button"></slot>
+    </div>
+  </div>
+</div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
---
+++ chrome/browser/resources/feature_showcase/feature_showcase_step.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {getCss} from './feature_showcase_step.css.js';
+import {getHtml} from './feature_showcase_step.html.js';
+
+export class FeatureShowcaseStepElement extends CrLitElement {
+  static get is() {
+    return 'feature-showcase-step';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'feature-showcase-step': FeatureShowcaseStepElement;
+  }
+}
+
+customElements.define(
+    FeatureShowcaseStepElement.is, FeatureShowcaseStepElement);
