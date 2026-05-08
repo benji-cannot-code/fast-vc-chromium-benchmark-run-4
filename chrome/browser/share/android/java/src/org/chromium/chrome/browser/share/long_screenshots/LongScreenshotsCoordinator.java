51edeaf5f4ccd8fbe01a01b32b3a54419b7518d3
Jenna Himawan <jhimawan@google.com>
2026-05-07 17:06:09 -0700

    Refactor: Rename LongScreenshotErrorUtils to LongScreenshotsUtils
    Rename LongScreenshotErrorUtils to LongScreenshotsUtils to make it more generic and support future utility functions for long screenshots. Define public BitmapGeneratorStatus IntDef in LongScreenshotsUtils and remove the duplicate definition in ScrollCaptureCallbackDelegate. Update all references and tests accordingly.
    
    TAG=agy
    
    CONV=cf75a003-5cb6-4719-ad07-6205a6bfd0b1
    
    Bug: 505046957
    Change-Id: Idd3d9418510fd25054cf12c515b431e23c2a5cbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7824353
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627354}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/long_screenshots/LongScreenshotsCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/long_screenshots/LongScreenshotsCoordinator.java
@@ -121,7 +121,7 @@ public class LongScreenshotsCoordinator extends ScreenshotCoordinator {
                 () -> {
                     mScreenshot = mMediator.getScreenshot();
                     if (mScreenshot == null) {
-                        LongScreenshotErrorUtils.showErrorMessage(mActivity);
+                        LongScreenshotsUtils.showErrorMessage(mActivity);
                     } else {
                         super.handleScreenshot();
                     }
