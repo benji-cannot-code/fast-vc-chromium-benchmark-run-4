1e4bfa17feaf3411bc92e8da0577c17331d80eb7
Patrick Monette <pmonette@chromium.org>
2026-05-08 15:00:39 -0700

    base: Migrate DiscardableSharedMemory tests to TaskEnvironment MOCK_TIME
    Replace the custom TestDiscardableSharedMemory subclass and manual
    timestamp manipulation with base::test::TaskEnvironment MOCK_TIME.
    This allows for the removal of the virtual Now() method in
    DiscardableSharedMemory and DiscardableSharedMemoryManager.
    
    Also includes fixes for several tests to avoid unintended timestamp
    collisions when using MOCK_TIME.
    
    Change-Id: I614f76a2e50f450d2f05765eed797438fb4e0f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780402
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627945}

--- base/memory/discardable_shared_memory.cc
+++ base/memory/discardable_shared_memory.cc
@@ -342,7 +342,7 @@ void DiscardableSharedMemory::Unlock(size_t offset, size_t length) {
     return;
   }
 
-  Time current_time = Now();
+  Time current_time = Time::Now();
   DCHECK(!current_time.is_null());
 
   SharedState old_state(SharedState::LOCKED, Time());
@@ -556,10 +556,6 @@ void DiscardableSharedMemory::UnlockPages(
     }
   }
 #endif
-}
-
-Time DiscardableSharedMemory::Now() const {
-  return Time::Now();
 }
 
 #if BUILDFLAG(IS_ANDROID)
