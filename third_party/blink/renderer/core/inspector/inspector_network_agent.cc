102489ef904ba23e573ab5bd3544afec68854d81
Alex Rudenko <alexrudenko@chromium.org>
2026-05-07 23:11:20 -0700

    Rename DevTools token to DevTools throttling token
    Similar to https://crrev.com/c/7800333, this change renames the
    `devtools_token_` member and related getter/setter methods in
    `WebURLRequest`, `ResourceRequest`, and `InspectorNetworkAgent` to
    `devtools_throttling_token_` to better reflect its purpose of being used
    for network throttling in DevTools.
    
    Bug: 509502553
    Change-Id: If8ed1a119aaad66c5025bbf19c43f4e177924d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7826969
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627483}

--- third_party/blink/renderer/core/inspector/inspector_network_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_network_agent.cc
@@ -1513,7 +1513,7 @@ void InspectorNetworkAgent::SetDevToolsIds(
   if (initiator_info.name == fetch_initiator_type_names::kInternal) {
     return;
   }
-  request.SetDevToolsToken(devtools_token_);
+  request.SetDevToolsThrottlingToken(devtools_throttling_token_);
 
   // The loader parameter is for generating a browser generated ID for a browser
   // initiated request. We pass it null here because we are reporting a renderer
@@ -2025,8 +2025,8 @@ String InspectorNetworkAgent::GetProtocolAsString(
 }
 
 void InspectorNetworkAgent::WillCreateP2PSocketUdp(
-    std::optional<base::UnguessableToken>* devtools_token) {
-  *devtools_token = devtools_token_;
+    std::optional<base::UnguessableToken>* devtools_throttling_token) {
+  *devtools_throttling_token = devtools_throttling_token_;
 }
 
 void InspectorNetworkAgent::WillCreateWebSocket(
@@ -2034,8 +2034,8 @@ void InspectorNetworkAgent::WillCreateWebSocket(
     uint64_t identifier,
     const KURL& request_url,
     const String&,
-    std::optional<base::UnguessableToken>* devtools_token) {
-  *devtools_token = devtools_token_;
+    std::optional<base::UnguessableToken>* devtools_throttling_token) {
+  *devtools_throttling_token = devtools_throttling_token_;
   std::unique_ptr<v8_inspector::protocol::Runtime::API::StackTrace>
       current_stack_trace =
           CaptureSourceLocation(execution_context)->BuildInspectorObject();
@@ -2870,7 +2870,7 @@ InspectorNetworkAgent::InspectorNetworkAgent(
       resources_data_(MakeGarbageCollected<NetworkResourcesData>(
           kDefaultTotalBufferSize,
           kDefaultResourceBufferSize)),
-      devtools_token_(
+      devtools_throttling_token_(
           worker_or_worklet_global_scope_
               ? worker_or_worklet_global_scope_->GetParentDevToolsToken()
               : inspected_frames->Root()->GetDevToolsFrameToken()),
