4b34d2895974baf32bcb5b763feeb8f5f5cd568e
Andrew Grieve <agrieve@chromium.org>
2026-05-08 12:09:22 -0700

    JNI Zero: Fix typo "convertable" -> "convertible"
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Change-Id: Ic9ab7d09b95a72af283e1593bec9834210a48041
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829499
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@google.com>
    Commit-Queue: Sam Maier <smaier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627819}

--- third_party/jni_zero/java_refs.h
+++ third_party/jni_zero/java_refs.h
@@ -42,7 +42,7 @@ concept IsJobject =
 // The "jobject" type is basically the "any" type, so we use this concept to
 // allow implicit casts from jobject -> subclass.
 template <typename T, typename U>
-concept IsConvertableJObject =
+concept IsConvertibleJObject =
     std::is_convertible_v<U, T> || std::same_as<U, jobject>;
 
 template <typename T>
@@ -333,7 +333,7 @@ class JavaRef : public JavaRef<jobject> {
   // Create a JavaRef that is not automatically released. Used for JNI
   // parameters (which should not be released).
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   static JavaRef<T> CreateLeaky(JNIEnv* env, U obj) {
     return JavaRef<T>(env, obj);
   }
@@ -395,7 +395,7 @@ class JavaRef<internal::_JObjectArray<T>*> : public JavaRef<jobjectArray> {
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<JArray<T>, U>
+    requires internal::IsConvertibleJObject<JArray<T>, U>
   static JavaRef<JArray<T>> CreateLeaky(JNIEnv* env, U obj) {
     return JavaRef<internal::_JObjectArray<T>*>(env, obj);
   }
@@ -408,7 +408,7 @@ class JavaRef<internal::_JObjectArray<T>*> : public JavaRef<jobjectArray> {
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Set(JNIEnv* env, int32_t index, const JavaRef<U>& value) const {
     env->SetObjectArrayElement(this->obj(), index, value.obj());
   }
@@ -443,7 +443,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
   // This should only be used by JNI helper functions, or in cases where code
   // must call JNIEnv methods directly.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   static ScopedJavaLocalRef Adopt(JNIEnv* env, U obj) {
     return ScopedJavaLocalRef(env, obj);
   }
@@ -459,7 +459,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
 
   // Copy conversion constructor.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaLocalRef(const ScopedJavaLocalRef<U>& other) : env_(other.env_) {
     JavaRef<jobject>::SetNewLocalRef(env_, other.obj());
   }
@@ -472,20 +472,20 @@ class ScopedJavaLocalRef : public JavaRef<T> {
 
   // Move conversion constructor.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaLocalRef(ScopedJavaLocalRef<U>&& other) : env_(other.env_) {
     JavaRef<jobject>::steal(std::move(other));
   }
 
   // Constructor for other JavaRef types.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   explicit ScopedJavaLocalRef(const JavaRef<U>& other) {
     Reset(other);
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaLocalRef(JNIEnv* env, const JavaRef<U>& other) {
     Reset(other);
   }
@@ -506,7 +506,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
 
   // Copy conversion assignment.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaLocalRef& operator=(const ScopedJavaLocalRef<U>& other) {
     Reset(other);
     return *this;
@@ -514,7 +514,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
 
   // Move assignment.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaLocalRef& operator=(ScopedJavaLocalRef<U>&& other) {
     env_ = other.env_;
     Reset();
@@ -524,7 +524,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
 
   // Assignment for other JavaRef types.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaLocalRef& operator=(const JavaRef<U>& other) {
     Reset(other);
     return *this;
@@ -533,7 +533,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
   void Reset() { JavaRef<jobject>::ResetLocalRef(env_); }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Reset(const ScopedJavaLocalRef<U>& other) {
     // We can copy over env_ here as |other| instance must be from the same
     // thread as |this| local ref. (See class comment for multi-threading
@@ -542,7 +542,7 @@ class ScopedJavaLocalRef : public JavaRef<T> {
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Reset(const JavaRef<U>& other) {
     // If |env_| was not yet set (is still null) it will be attached to the
     // current thread in SetNewLocalRef().
@@ -606,7 +606,7 @@ class ScopedJavaGlobalRef : public JavaRef<T> {
 
   // Copy conversion constructor.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaGlobalRef(const ScopedJavaGlobalRef<U>& other) {
     Reset(other);
   }
@@ -619,20 +619,20 @@ class ScopedJavaGlobalRef : public JavaRef<T> {
 
   // Move conversion constructor.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaGlobalRef(ScopedJavaGlobalRef<U>&& other) {
     JavaRef<T>::steal(std::move(other));
   }
 
   // Conversion constructor for other JavaRef types.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   explicit ScopedJavaGlobalRef(const JavaRef<U>& other) {
     Reset(other);
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaGlobalRef(JNIEnv* env, const JavaRef<U>& other) {
     JavaRef<T>::SetNewGlobalRef(env, other.obj());
   }
@@ -653,7 +653,7 @@ class ScopedJavaGlobalRef : public JavaRef<T> {
 
   // Copy conversion assignment.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaGlobalRef& operator=(const ScopedJavaGlobalRef<U>& other) {
     Reset(other);
     return *this;
@@ -661,7 +661,7 @@ class ScopedJavaGlobalRef : public JavaRef<T> {
 
   // Move assignment.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaGlobalRef& operator=(ScopedJavaGlobalRef<U>&& other) {
     Reset();
     JavaRef<T>::steal(std::move(other));
@@ -670,7 +670,7 @@ class ScopedJavaGlobalRef : public JavaRef<T> {
 
   // Assignment for other JavaRef types.
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   ScopedJavaGlobalRef& operator=(const JavaRef<U>& other) {
     Reset(other);
     return *this;
@@ -679,19 +679,19 @@ class ScopedJavaGlobalRef : public JavaRef<T> {
   void Reset() { JavaRef<T>::ResetGlobalRef(); }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Reset(const ScopedJavaGlobalRef<U>& other) {
     Reset(nullptr, other);
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Reset(const JavaRef<U>& other) {
     Reset(nullptr, other);
   }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Reset(JNIEnv* env, const JavaRef<U>& other) {
     JavaRef<T>::SetNewGlobalRef(env, other.obj());
   }
@@ -765,7 +765,7 @@ class JNI_ZERO_COMPONENT_BUILD_EXPORT LeakedJavaGlobalRef : public JavaRef<T> {
   void Reset() { JavaRef<T>::ResetGlobalRef(); }
 
   template <typename U>
-    requires internal::IsConvertableJObject<T, U>
+    requires internal::IsConvertibleJObject<T, U>
   void Reset(JNIEnv* env, const JavaRef<U>& j_object) {
     Reset();
     JavaRef<T>::SetNewGlobalRef(env, j_object.obj());
