268943c1bb8678ce3c7b3e193d15e39507229700
Patrick Noland <pnoland@google.com>
2026-05-08 09:13:29 -0700

    Implement LocationBar reparenting into the suggestions container
    This is accomplished by:
    * Checking FuseboxLayoutMode on url focus edge, reparenting to the
      suggestions container on focus and back to the toolbar when losing
      focus. It's assumed that the layout mode won't change after startup.
    * To reparent to the suggestions container, the LocationBar is removed
      from its holder and added as the first child of the suggestions
      container. The suggestions "dropdown" (recycler view) is added as the
      third child of the LocationBar with appropriate constraints to place
      it below the UrlBar.
    * To reparent back, the Dropdown and LocationBarLayout are removed and
      the LocationBarLayout is placed back into its holder.
    * Since we need the SuggestionsContainer and Dropdown immediately to do
      this, they're inflated synchronously if not yet present on url focus.
    * FuseboxLayoutMode needs to be calculated sooner than it currently is
      so that we can check its up to date value before FuseboxMediator is
      initialized.
    * Since the SuggestionsContainer can no longer cast a shadow on the
      LocationBar, we restore the z translation logic, which puts the
      container above the toolbar and casts a shadow.
    
    This does not completely accomplish the popover spec on desktop devices;
    the constraints are still wrong for some fusebox buttons and there are
    some large shadows where there shouldn't be, among other issues.
    
    Bug: 507111601
    Change-Id: Ia1dfeffc6483b882b3dabb96a19408b57dea7dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817500
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627694}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarCoordinator.java
@@ -421,12 +421,13 @@ public class UrlBarCoordinator
      *     process has completed.
      */
     public void finishReparenting(boolean postReparentingFocus) {
-        mMediator.finishReparenting();
         if (postReparentingFocus) {
             mUrlBar.requestFocus();
         } else {
             mUrlBar.clearFocus();
         }
+        mMediator.finishReparenting();
+        mHasFocus = postReparentingFocus;
     }
 
     /**
