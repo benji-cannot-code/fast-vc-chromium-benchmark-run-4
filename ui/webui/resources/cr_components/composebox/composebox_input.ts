842f0acb0811294a63f4b1b143b20b8f51976c4f
Nihar Majmudar <niharm@google.com>
2026-05-08 14:19:13 -0700

    [composebox] Don't show smart compose on backspace.
    g.com doesn't do this so neither should chrome.
    
    Change-Id: I9b7192611f8fb5801b0f75b50dc0fe384c303a15
    Bug: 510436431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829148
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627915}

--- ui/webui/resources/cr_components/composebox/composebox_input.ts
+++ ui/webui/resources/cr_components/composebox/composebox_input.ts
@@ -48,6 +48,7 @@ export class ComposeboxInputElement extends I18nMixinLit
       submitEnabled: {type: Boolean, reflect: true},
       entrypointName: {type: String, reflect: true},
       cancelButtonTitle: {type: String},
+      isBackspacing_: {type: Boolean},
     };
   }
 
@@ -61,6 +62,7 @@ export class ComposeboxInputElement extends I18nMixinLit
   accessor submitEnabled: boolean = false;
   accessor entrypointName: string = '';
   accessor cancelButtonTitle: string = '';
+  accessor isBackspacing_: boolean = false;
 
   private caretResizeObserver_: ResizeObserver|null = null;
   private smartComposeResizeObserver_: ResizeObserver|null = null;
@@ -108,8 +110,9 @@ export class ComposeboxInputElement extends I18nMixinLit
       }
     }
 
-    if (changedProperties.has('smartComposeInlineHint')) {
-      if (this.smartComposeInlineHint) {
+    if (changedProperties.has('smartComposeInlineHint') ||
+        changedProperties.has('isBackspacing_')) {
+      if (this.showSmartComposeInlineHint_()) {
         const smartCompose =
             this.shadowRoot.querySelector<HTMLElement>('#smartCompose');
         if (smartCompose) {
@@ -213,6 +216,14 @@ export class ComposeboxInputElement extends I18nMixinLit
     this.dispatchEvent(new CustomEvent('input-click', {detail: e}));
   }
 
+  protected onInputKeydown_(e: KeyboardEvent) {
+    if (e.key === 'Backspace') {
+      this.isBackspacing_ = true;
+    } else if (e.key.length === 1 || e.key === 'Enter') {
+      this.isBackspacing_ = false;
+    }
+  }
+
   protected onInputKeyup_(e: KeyboardEvent) {
     if (!this.disableCaretColorAnimation) {
       this.updateCaret_();
@@ -235,6 +246,10 @@ export class ComposeboxInputElement extends I18nMixinLit
 
   protected onCancelClick_(e: Event) {
     this.dispatchEvent(new CustomEvent('cancel-click', {detail: e}));
+  }
+
+  protected showSmartComposeInlineHint_(): boolean {
+    return !!this.smartComposeInlineHint && !this.isBackspacing_;
   }
 
   // Recalculate the caret only when #inputWrapper's width changes.
