67867822cd79fdddbe03b006087370a32954280f
Yue Liu <yueliu1@microsoft.com>
2026-05-08 08:12:23 -0700

    [Glic] Remove leftover debug logging and unused stack_trace includes
    Three stray debug artifacts from recent Glic CLs:
    
    * GlicActorTaskIconManager::OnTaskStateUpdated logged
      "ShouldShowBubble!" via LOG(WARNING) on every bubble-eligible task
      state update.
    * ActorTaskListBubbleController included <base/debug/stack_trace.h> with
      no remaining users in the file.
    * GlicSidePanelCoordinatorImpl included <base/debug/stack_trace.h> with
      no remaining users in the file.
    
    All three are clearly accidental debug leftovers. Drop them.
    
    Change-Id: Id4d9cdafb5505a966f5dbda3a24851d7cdc82cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7832425
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627654}

--- chrome/browser/glic/widget/glic_side_panel_coordinator_impl.cc
+++ chrome/browser/glic/widget/glic_side_panel_coordinator_impl.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/glic/public/widget/glic_side_panel_coordinator_impl.h"
 
-#include "base/debug/stack_trace.h"
 #include "base/functional/callback.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
