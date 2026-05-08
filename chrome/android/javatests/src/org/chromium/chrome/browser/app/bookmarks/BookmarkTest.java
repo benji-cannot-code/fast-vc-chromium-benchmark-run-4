9577f59784817c44fc52e6495159f5376c2b71ea
Kelvin Jiang <kelvinjiang@chromium.org>
2026-05-08 14:06:13 -0700

    Reland "[Bookmarks] Desktop android folder visibility follows desktop"
    This is a reland of commit 85f5cc3b48e0cf1dc4b3d6c3aca443a914f056ab
    
    Some tests in Bookmarktest.java and AccountBookmarkTest.java failed on
    non dry-run and CQ+2 tryjobs because those tryjobs ran the tests on an
    actual desktop emulator?
    
    Original change's description:
    > [Bookmarks] Desktop android folder visibility follows desktop
    >
    > Treat desktop android as another desktop OS for bookmarks folder
    > visibility (and other things?) logic, i.e.
    > - on desktop, the mobile folder is hidden when empty
    > - on mobile, the bookmarks bar and other bookmarks is hidden when empty
    >
    > there was a "hack" somewhere in Java which forced visibility for some
    > folders. Adding the intended logic in the C++ end allowed the hack to
    > be removed.
    >
    > This also allows the rest of the bookmarks API tests for extension to be
    > enabled again on desktop Android now that the intended behavior is
    > defined. This completes bookmarks API porting to desktop Android.
    >
    > Bug: 509209039, 414844449
    > Change-Id: Iaca3a7e170c0f1d4055e73401cdd5faa0882380d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789287
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Brandon Wylie <wylieb@google.com>
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1626716}
    
    Bug: 509209039, 414844449
    Change-Id: Ib915013a9c553cb42732987f528e275ee802747e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827496
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627905}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/bookmarks/BookmarkTest.java
@@ -74,6 +74,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
 import org.chromium.base.CallbackUtils;
+import org.chromium.base.DeviceInfo;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.supplier.MonotonicObservableSupplier;
@@ -1436,7 +1437,13 @@ public class BookmarkTest {
         // NOTE: Hide promos to ensure top level-folders will fit the viewport.
         SigninPromoCoordinator.disablePromoForTesting();
         openBookmarkManager();
-        onViewWaiting(allOf(withText("Mobile bookmarks"), isDisplayed()));
+
+        // An empty mobile bookmarks folder is hidden on desktop devices.
+        if (DeviceInfo.isDesktop()) {
+            onView(withText("Mobile bookmarks")).check(doesNotExist());
+        } else {
+            onViewWaiting(allOf(withText("Mobile bookmarks"), isDisplayed()));
+        }
         onViewWaiting(allOf(withText("Reading list"), isDisplayed()));
         onView(withText("Bookmarks bar"))
                 .check(
@@ -1470,7 +1477,12 @@ public class BookmarkTest {
         runOnUiThreadBlocking(getTestingDelegate()::simulateSignInForTesting);
 
         final List<String> expectedTopLevelFolders =
-                new ArrayList<>(List.of("Mobile bookmarks", "Other bookmarks", "Reading list"));
+                new ArrayList<>(List.of("Other bookmarks", "Reading list"));
+
+        // An empty mobile bookmarks folder is hidden on desktop devices.
+        if (!DeviceInfo.isDesktop()) {
+            expectedTopLevelFolders.add(0, "Mobile bookmarks");
+        }
 
         if (BookmarkBarUtils.isDeviceBookmarkBarCompatible(mActivityTestRule.getActivity())) {
             expectedTopLevelFolders.add(1, "Bookmarks bar");
