2c0b6db913b193e3ef45c0ba3d3538073d1ef4da
Ian Vollick <vollick@chromium.org>
2026-05-08 17:54:24 -0700

    Use GlicEnabling to gate the creation of GlicSelectionObserver
    This spot wasn't updated in crrev.com/c/7790916.
    
    Bug: b:488019937
    Change-Id: I263f29d59d2d9a609898307ca3f7744b012a23f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812734
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628044}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -371,7 +371,7 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
           GetUserDataFactory().CreateInstance<glic::SelectionOverlayController>(
               tab, &tab, profile->GetPrefs());
 
-      if (base::FeatureList::IsEnabled(features::kGlicSelectionPrompt)) {
+      if (glic::GlicEnabling::IsSelectionPromptEnabledForProfile(profile)) {
         glic_selection_observer_ =
             std::make_unique<glic::GlicSelectionObserver>(tab.GetContents());
       }
