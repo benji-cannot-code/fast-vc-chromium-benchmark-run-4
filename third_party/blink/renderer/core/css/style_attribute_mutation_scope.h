5851632ad238533b1a99c013a2d3c785db1b912f
Kevin Babbitt <kbabbitt@microsoft.com>
2026-05-11 09:37:13 -0700

    Fire style attribute change event on change to attributeStyleMap
    - Refactor StyleAttributeMutationScope to hold an Element as context
      rather than an AbstractPropertySetCSSStyleDeclaration.
    - Wrap mutations effected from InlineStylePropertyMap in a
      StyleAttributeMutationScope.
    - In InlineStylePropertyMap, call SetProperty() directly rather than
      delegating to Element::SetInlineStyleProperty() to avoid duplicate
      mutation records.
    
    Fixed: 40710238
    Change-Id: I2287d68a21d5e126aa7c449260b2da8df9fa599d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7823966
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1628636}

--- third_party/blink/renderer/core/css/style_attribute_mutation_scope.h
+++ third_party/blink/renderer/core/css/style_attribute_mutation_scope.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class AbstractPropertySetCSSStyleDeclaration;
+class Element;
 class MutationObserverInterestGroup;
 class MutationRecord;
 
@@ -38,7 +39,8 @@ class StyleAttributeMutationScope {
   STACK_ALLOCATED();
 
  public:
-  StyleAttributeMutationScope(AbstractPropertySetCSSStyleDeclaration*);
+  explicit StyleAttributeMutationScope(AbstractPropertySetCSSStyleDeclaration*);
+  explicit StyleAttributeMutationScope(Element*);
   StyleAttributeMutationScope(const StyleAttributeMutationScope&) = delete;
   StyleAttributeMutationScope& operator=(const StyleAttributeMutationScope&) =
       delete;
@@ -50,8 +52,10 @@ class StyleAttributeMutationScope {
   void DidInvalidateStyleAttr() { should_notify_inspector_ = true; }
 
  private:
+  void Initialize(Element*);
+
   static unsigned scope_count_;
-  static AbstractPropertySetCSSStyleDeclaration* current_decl_;
+  static Element* current_element_;
   static bool should_notify_inspector_;
   static bool should_deliver_;
 
