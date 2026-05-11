80025bfd7bf2a5524bab04dbd050f7b6315a7811
Kamila Hasanbega <hkamila@google.com>
2026-05-11 09:38:52 -0700

    [History] Clean up pre M2 codepath
    BrowsingHistoryActorIntegrationM2 is fully launched.
    
    Bug: 467988947
    Change-Id: I3419c43fbb15d8349598aeebd226516d09c2d909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828682
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Auto-Submit: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628637}

--- components/history/core/browser/browsing_history_service.cc
+++ components/history/core/browser/browsing_history_service.cc
@@ -630,11 +630,9 @@ void BrowsingHistoryService::MergeDuplicateResults(
     }
 
 #if !BUILDFLAG(IS_IOS)
-    auto& current_day_entries =
-        history::IsBrowsingHistoryActorIntegrationM2Enabled() &&
-                entry.is_actor_visit
-            ? actor_current_day_entries
-            : non_actor_current_day_entries;
+    auto& current_day_entries = entry.is_actor_visit
+                                    ? actor_current_day_entries
+                                    : non_actor_current_day_entries;
 #else
     auto& current_day_entries = non_actor_current_day_entries;
 #endif
@@ -725,11 +723,9 @@ BrowsingHistoryService::GroupSimilarVisits(QueryHistoryState* state) {
     }
 
 #if !BUILDFLAG(IS_IOS)
-    auto& current_day_entries =
-        history::IsBrowsingHistoryActorIntegrationM2Enabled() &&
-                entry.is_actor_visit
-            ? actor_current_day_entries
-            : non_actor_current_day_entries;
+    auto& current_day_entries = entry.is_actor_visit
+                                    ? actor_current_day_entries
+                                    : non_actor_current_day_entries;
 #else
     auto& current_day_entries = non_actor_current_day_entries;
 #endif
--- components/history/core/browser/history_service.cc
+++ components/history/core/browser/history_service.cc
@@ -563,7 +563,6 @@ void HistoryService::AddPage(const GURL& url,
   bool consider_for_ntp_most_visited = true;
 #if !BUILDFLAG(IS_IOS)
   consider_for_ntp_most_visited =
-      !history::IsBrowsingHistoryActorIntegrationM2Enabled() ||
       visit_source != VisitSource::SOURCE_ACTOR;
 #endif
 
@@ -583,7 +582,6 @@ void HistoryService::AddPage(const GURL& url,
   bool consider_for_ntp_most_visited = true;
 #if !BUILDFLAG(IS_IOS)
   consider_for_ntp_most_visited =
-      !history::IsBrowsingHistoryActorIntegrationM2Enabled() ||
       visit_source != VisitSource::SOURCE_ACTOR;
 #endif
 
