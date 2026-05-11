2d18a9fb91e54175aa28350f4b1386fa471f9c0b
Ameur Hosni <ameurhosni@google.com>
2026-05-11 06:33:31 -0700

    [IOS][AIM] Fix image attachment in standalone state
    In standalone state, _preselection is nil which leads to the passed
    images to be nil.
    
    Bug: 511982482
    Change-Id: I6b56ca34c87a7b362bd4611fd470e1e5653d0f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830211
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628526}

--- ios/chrome/browser/composebox/menu/coordinator/composebox_menu_mediator.mm
+++ ios/chrome/browser/composebox/menu/coordinator/composebox_menu_mediator.mm
@@ -44,7 +44,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)processImageItems:(NSArray<ComposeboxPickerImageResult*>*)imageItems {
   NSMutableArray<ComposeboxPickerImageResult*>* updatedImageResults =
-      [_preselection.images mutableCopy];
+      [[NSMutableArray alloc] init];
+
+  if (_preselection.images) {
+    updatedImageResults = [_preselection.images mutableCopy];
+  }
+
   [updatedImageResults addObjectsFromArray:imageItems];
 
   ComposeboxAttachmentSelection* selection =
