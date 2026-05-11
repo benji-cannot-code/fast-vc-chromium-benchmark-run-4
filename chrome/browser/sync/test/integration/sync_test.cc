035e75e1f6dfe2e9b2f0bd75d27c76cf53449ce4
Kay Lin <kaiyilin@google.com>
2026-05-10 22:10:34 -0700

    Site Search: Bring up search engine sync test
    This CL enables single-client search engine sync test on Clank. To
    support running the tests on Clank, this CL:
    - Enable the test only on LFF devices since the sync feature is limited
      to LFF.
    - Disable verification against the verifier profile on Android since the
      verifier is not supported; we query the fake server instead.
    - Disable tests relying on kSigninAllowedOnNextStartup as the pref is
      used for desktop-only DICE sign in.
    
    Bug: 502050495
    Change-Id: Idd809dbd72d76e39426f35c111af7ff0784a1daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827089
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628363}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -1411,6 +1411,18 @@ syncer::DataTypeSet AllowedTypesInStandaloneTransportMode() {
     allowed_types.Put(syncer::CONTEXTUAL_TASK);
   }
 
+  if (base::FeatureList::IsEnabled(
+          syncer::kSeparateLocalAndAccountSearchEngines) &&
+      // Support for transport mode for search engines is implemented alongside
+      // that of preferences.
+      base::FeatureList::IsEnabled(switches::kEnablePreferencesAccountStorage)
+#if BUILDFLAG(IS_ANDROID)
+      && base::FeatureList::IsEnabled(syncer::kSyncSearchEnginesAndroidLFF)
+#endif
+  ) {
+    allowed_types.Put(syncer::SEARCH_ENGINES);
+  }
+
 #if BUILDFLAG(IS_ANDROID)
   if (base::FeatureList::IsEnabled(syncer::kWebApkBackupAndRestoreBackend)) {
     allowed_types.Put(syncer::WEB_APKS);
@@ -1418,15 +1430,6 @@ syncer::DataTypeSet AllowedTypesInStandaloneTransportMode() {
 #else   // BUILDFLAG(IS_ANDROID)
   if (base::FeatureList::IsEnabled(syncer::kSeparateLocalAndAccountThemes)) {
     allowed_types.Put(syncer::THEMES);
-  }
-
-  if (base::FeatureList::IsEnabled(
-          syncer::kSeparateLocalAndAccountSearchEngines) &&
-      // Support for transport mode for search engines is implemented alongside
-      // that of preferences.
-      base::FeatureList::IsEnabled(
-          switches::kEnablePreferencesAccountStorage)) {
-    allowed_types.Put(syncer::SEARCH_ENGINES);
   }
 
   // These types are excluded on Android as they run outside Chrome.
