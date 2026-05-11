88b898591df200a51cbf9bc0ff6fed0abf3d86d5
Alex Moshchuk <alexmos@chromium.org>
2026-05-11 14:53:14 -0700

    Add chrome://flags entry for Rust-based ChildProcessSecurityPolicy.
    This introduces a chrome://flags entry for enabling Rust-based CPSP,
    with two possible modes when enabled: Rust-only and Rust and C++
    instances of CPSP running in parallel. This is similar to what was
    done for sandboxed frames which also support multiple modes (see
    https://crrev.com/c/3703318 and https://crrev.com/c/3727308).
    
    As one consequence of this, the Rust CPSP features are moved into
    content/public.
    
    Change-Id: I0486bc696b70dd7670b33d8e1183650dbc29461d
    Bug: 482216433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7698472
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Liam Brady <lbrady@google.com>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628832}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4618,6 +4618,19 @@ const FeatureEntry::FeatureVariation kSendTabToSelfGestureVariations[] = {
 };
 #endif  // BUILDFLAG(IS_ANDROID)
 
+// The choices for Rust-based ChildProcessSecurityPolicy feature.
+const FeatureEntry::FeatureParam kChildProcessSecurityPolicyRustRustOnly[] = {
+    {features::kChildProcessSecurityPolicyRustPolicyName,
+     features::kChildProcessSecurityPolicyRustPolicyRustOnly}};
+const FeatureEntry::FeatureParam kChildProcessSecurityPolicyRustRustAndCpp[] = {
+    {features::kChildProcessSecurityPolicyRustPolicyName,
+     features::kChildProcessSecurityPolicyRustPolicyRustAndCpp}};
+const FeatureEntry::FeatureVariation
+    kChildProcessSecurityPolicyRustVariations[] = {
+        {"Rust only", kChildProcessSecurityPolicyRustRustOnly, nullptr},
+        {"Rust and C++", kChildProcessSecurityPolicyRustRustAndCpp, nullptr},
+};
+
 // RECORDING USER METRICS FOR FLAGS:
 // -----------------------------------------------------------------------------
 // The first line of the entry is the internal name.
@@ -13004,6 +13017,13 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableCancelUploadOnContentAnalysisDescription, kOsAll,
      FEATURE_VALUE_TYPE(
          enterprise_connectors::kEnableCancelUploadOnContentAnalysis)},
+
+    {"child-process-security-policy-rust",
+     flag_descriptions::kChildProcessSecurityPolicyRustName,
+     flag_descriptions::kChildProcessSecurityPolicyRustDescription, kOsAll,
+     FEATURE_WITH_PARAMS_VALUE_TYPE(features::kChildProcessSecurityPolicyRust,
+                                    kChildProcessSecurityPolicyRustVariations,
+                                    "ChildProcessSecurityPolicyRust")},
 
     // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
