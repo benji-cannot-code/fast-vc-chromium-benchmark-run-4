1f0933a975e57b657763963dfeafd8248d94ef16
Alexander Cooper <alcooper@chromium.org>
2026-05-11 12:54:13 -0700

    Reland "Remove src_overrides for cardboard"
    This is a reland of commit 3459adc8118a110da049e3111d64bbd57e59e568
    
    Original change's description:
    > Remove src_overrides for cardboard
    >
    > The JNI system was patched to intercept `FindClass` calls to become
    > split-aware, which was the reason for the first src_overrides patch
    > that was being kept.
    > Cardboard has been rolled to a version that added upstream support for
    > edge-to-edge activities, so the patch here in the downstream can now
    > be removed.
    >
    > Fixed: 493763068
    > Change-Id: Ia7c6ae1468a376021795f3e44fded4b675c425e7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7804675
    > Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Commit-Queue: Brandon Jones <bajones@chromium.org>
    > Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1622822}
    
    Change-Id: Ica4cbe20d7c7dade7df4887deb635e6eef34575a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829162
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628754}

--- base/android/jni_android.h
+++ base/android/jni_android.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/compiler_specific.h"
 #include "base/debug/debugging_buildflags.h"
+#include "build/robolectric_buildflags.h"
 #include "third_party/jni_zero/jni_zero.h"
 
 namespace base {
@@ -114,7 +115,11 @@ BASE_EXPORT std::string GetJavaExceptionInfo(
 // This returns a string representation of the java stack trace.
 BASE_EXPORT std::string GetJavaStackTraceIfPresent();
 
-void HookJniFindClass(JNIEnv* env);
+BASE_EXPORT void HookJniFindClass(JNIEnv* env);
+BASE_EXPORT void UnhookJniFindClassForTesting(JNIEnv* env);
+#if !BUILDFLAG(IS_ROBOLECTRIC)
+BASE_EXPORT const JNINativeInterface* GetOriginalJniFunctionsForTesting();
+#endif
 
 using MethodID = jni_zero::MethodID;
 }  // namespace android
