1c5be3528a1887822b2fdd9729c1134edf4fc994
Henrique Nakashima <hnakashima@chromium.org>
2026-05-11 13:17:40 -0700

    [Android] Reset mocks after activities are finished to avoid leaks
    Mockito mocks retain arguments passed to them, causing leaks if the
    mocks are retained by long-lived objects (e.g., static singletons).
    Resetting mocks after every test method is not enough. This misses
    invocations triggered by asynchronous activity destruction that happened
    *after* the test method finished.
    
    This CL introduces a deferred reset approach: 1. Mocks declared as
    fields in the test instance are collected after each test method. 2. All
    collected mocks are aggressively reset in onAfterTestClass, *after*
    ActivityFinisher.finishAll() has run.
    
    This ensures that any late cleanup calls (like removeObserver) recorded
    by Mockito after the test has finished are cleared before LeakCanary
    runs, resolving leaks that were previously unaddressed.
    
    Bug: 507078523
    Change-Id: I5f5618c0de17966eda5563a937de9701a3700000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7822763
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Sam Maier <smaier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628773}

--- base/test/android/javatests/src/org/chromium/base/test/util/LeakCanaryChecker.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/LeakCanaryChecker.java
@@ -8,6 +8,8 @@ package org.chromium.base.test.util;
 import leakcanary.LeakAssertions;
 import leakcanary.LeakCanary;
 
+import org.junit.runners.model.FrameworkMethod;
+
 import shark.AndroidReferenceMatchers;
 import shark.ReferenceMatcher;
 import shark.ReferenceMatcherKt;
@@ -19,6 +21,7 @@ import shark.ReferencePattern.StaticFieldPattern;
 import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.test.BaseJUnit4ClassRunner.AfterCleanupCheck;
+import org.chromium.base.test.BaseJUnit4ClassRunner.ClassCleanupHook;
 import org.chromium.build.annotations.ServiceImpl;
 
 import java.lang.annotation.ElementType;
@@ -32,12 +35,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.ServiceLoader;
 
-@ServiceImpl(AfterCleanupCheck.class)
-public class LeakCanaryChecker implements AfterCleanupCheck {
+public class LeakCanaryChecker {
     private static final String TAG = "LeakCanaryChecker";
 
-    @Override
-    public void onAfterTestClass(Class<?> testClass) {
+    /**
+     * @return True if LeakCanary should run for this test class.
+     */
+    public static boolean isEnabled(Class<?> testClass) {
         boolean enabledByAnnotation = testClass.getAnnotation(EnableLeakChecks.class) != null;
         boolean enabledByFlag = CommandLine.getInstance().hasSwitch("enable-leak-checks");
         boolean disabledByAnnotation = testClass.getAnnotation(DisableLeakCheck.class) != null;
@@ -48,10 +52,36 @@ public class LeakCanaryChecker implements AfterCleanupCheck {
                             + testClass.getName());
         }
 
-        if (enabledByAnnotation || enabledByFlag) {
-            if (disabledByAnnotation) {
-                Log.w(TAG, "Leak check skipped by @DisableLeakCheck");
-            } else {
+        if ((enabledByAnnotation || enabledByFlag) && disabledByAnnotation) {
+            Log.w(TAG, "Leak check skipped by @DisableLeakCheck");
+        }
+
+        return (enabledByAnnotation || enabledByFlag) && !disabledByAnnotation;
+    }
+
+    @ServiceImpl(ClassCleanupHook.class)
+    public static class MockitoResetHook implements ClassCleanupHook {
+        @Override
+        public void onAfterTest(FrameworkMethod method, Object test) {
+            if (isEnabled(test.getClass())) {
+                MockitoResetter.addMocks(test);
+            }
+        }
+
+        @Override
+        public void onAfterTestClass(Class<?> testClass) {
+            if (isEnabled(testClass)) {
+                MockitoResetter.resetRecordedMocks();
+                MockitoResetter.clearOngoingStubbing();
+            }
+        }
+    }
+
+    @ServiceImpl(AfterCleanupCheck.class)
+    public static class LeakCheckHook implements AfterCleanupCheck {
+        @Override
+        public void onAfterTestClass(Class<?> testClass) {
+            if (isEnabled(testClass)) {
                 Log.i(TAG, "Running LeakCanary assertion");
                 checkLeaks();
             }
