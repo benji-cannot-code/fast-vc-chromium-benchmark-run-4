eaebb61eb6377ba4160e400f3fa9677282cf8e3e
Steve Becker <stevebe@microsoft.com>
2026-05-11 13:09:07 -0700

    [DomStorage] Clean up expired histograms and use histogram functions
    Removes histograms that expired over a year ago. Converts infrequently
    emitted histograms from macros to function.
    
    Bug: 41473263
    Change-Id: Icb143b01f7e18d94b12d8614770019c8c38481a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806731
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1628765}

--- components/services/storage/dom_storage/storage_area_impl.cc
+++ components/services/storage/dom_storage/storage_area_impl.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/process_memory_dump.h"
@@ -694,7 +693,7 @@ base::TimeDelta StorageAreaImpl::ComputeCommitDelay() const {
                         data_rate_limiter_.ComputeDelayNeeded(elapsed_time)));
   // TODO(mek): Rename histogram to match class name, or eliminate histogram
   // entirely.
-  UMA_HISTOGRAM_LONG_TIMES("LevelDBWrapper.CommitDelay", delay);
+  base::UmaHistogramLongTimes("LevelDBWrapper.CommitDelay", delay);
   return delay;
 }
 
--- third_party/blink/renderer/modules/storage/storage_area.cc
+++ third_party/blink/renderer/modules/storage/storage_area.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/task_type.h"
