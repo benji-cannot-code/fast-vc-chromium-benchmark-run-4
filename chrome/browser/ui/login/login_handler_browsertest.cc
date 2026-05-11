f2109bf0fd734a65ffb3d5bd875c8fd6ac83f739
James Cook <jamescook@chromium.org>
2026-05-11 15:59:31 -0700

    ui: Fix unneeded BUILDFLAG guard in login_handler_browsertest.cc
    This file only compiles on platforms that support extensions, and
    already #includes extensions files without guards, so remove the
    unneeded guard around the last include.
    
    This is part of the effort to clean up ENABLE_EXTENSIONS usage in
    //chrome.
    
    Bug: 494643383
    Change-Id: Ifba20f1d309b5fa300f8968a37ee514dd45c13e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7836694
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628876}

--- chrome/browser/ui/login/login_handler_browsertest.cc
+++ chrome/browser/ui/login/login_handler_browsertest.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/process_manager.h"
+#include "extensions/common/extension_features.h"
 #include "net/base/auth.h"
 #include "net/base/host_port_pair.h"
 #include "net/dns/mock_host_resolver.h"
@@ -72,9 +73,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "url/gurl.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-#include "extensions/common/extension_features.h"
-#endif
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS));
 
 using content::NavigationController;
 using content::OpenURLParams;
