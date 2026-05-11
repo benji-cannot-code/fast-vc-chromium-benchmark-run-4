5164080751ce21511029f2a7a2ea5e8b499c79df
Marc Treib <treib@chromium.org>
2026-05-11 08:47:13 -0700

    TabsFromOtherDevices side panel: Ensure screenshots get updated
    The side panel loads screenshots from a URLDataSource, based on the
    tab's ID and session tag. That means a tab's screenshot URL doesn't
    change when the tab (and its screenshot) is updated, and so screenshots
    in the side panel weren't consistently updated.
    
    This CL fixes the issue by appending the tab's timestamp to the
    screenshot URL, to bypass the browser's cache.
    
    Bug: 488242420
    Change-Id: Ib5bc89c85e8470a50232ae0e9d3e80f36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829792
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628598}

--- chrome/browser/resources/side_panel/tabs_from_other_devices/app.ts
+++ chrome/browser/resources/side_panel/tabs_from_other_devices/app.ts
@@ -157,7 +157,8 @@ export class TabsFromOtherDevicesAppElement extends
       ...tab,
       sessionTag,
       screenshotUrl: this.showScreenshots_ ?
-          `chrome://synced-screenshot/${sessionTag}/${tab.sessionId}` :
+          `chrome://synced-screenshot/${sessionTag}/${tab.sessionId}` +
+              `?${tab.timestamp}` :
           undefined,
     };
   }
