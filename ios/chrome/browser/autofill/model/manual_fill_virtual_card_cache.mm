41224c2eed4fab634191d18206917ac267aedc63
YiwenQian <yiwenqian@google.com>
2026-05-11 14:22:50 -0700

    [VCN iOS] Fix manual fallback reshow for virtual cards after direct fill
    Fixes the issue where the manual fallback UI only shows the masked FPAN
    after a user fills a virtual card directly from the keyboard accessory
    suggestions. To fix this, this CL:
    - Moves `ManualFillVirtualCardCache` to `autofill/model` for non-UI access.
    - Sets the unmasking origin in `FormSuggestionController` during direct fill to prevent subsequent cache misses.
    
    Bug: 504215814, 502225011
    Change-Id: I8abb85c1f0abf6425878cb23827615c5c7006f69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828103
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628807}

--- ios/chrome/browser/autofill/model/manual_fill_virtual_card_cache.mm
+++ ios/chrome/browser/autofill/model/manual_fill_virtual_card_cache.mm
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#import "ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_virtual_card_cache.h"
+#import "ios/chrome/browser/autofill/model/manual_fill_virtual_card_cache.h"
 
 #import "base/check.h"
 #import "ios/web/public/navigation/navigation_context.h"
--- ios/chrome/browser/autofill/model/manual_fill_virtual_card_cache_unittest.mm
+++ ios/chrome/browser/autofill/model/manual_fill_virtual_card_cache_unittest.mm
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#import "ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_virtual_card_cache.h"
+#import "ios/chrome/browser/autofill/model/manual_fill_virtual_card_cache.h"
 
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/core/browser/data_model/payments/credit_card.h"
