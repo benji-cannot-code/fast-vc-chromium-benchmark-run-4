eb4e917ae0c316da31d4b9a2756324d7a99d0534
Keigo Oka <oka@chromium.org>
2026-05-10 23:06:06 -0700

    Disable ExtensionsToolbarTest.testNewDialogDismissesExtensionsMenu on Desktop.
    This test is currently failing and is being temporarily disabled.
    
    Bug: 511895956
    Change-Id: Iaa8a17b04915870706d5f70ba661a6143a43a8fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7835967
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628374}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsToolbarTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsToolbarTest.java
@@ -53,6 +53,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.ImportantFormFactors;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -1104,6 +1105,8 @@ public class ExtensionsToolbarTest {
 
     @Test
     @LargeTest
+    // TODO(crbug.com/511895956): Re-enable this test.
+    @DisableIf.Device(DeviceFormFactor.DESKTOP)
     public void testNewDialogDismissesExtensionsMenu() throws IOException {
         loadBasicExtension("extension1", "Test Extension", "Test Action");
 
