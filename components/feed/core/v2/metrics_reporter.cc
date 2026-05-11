9dd1e24d210b29e8ea5218fc0563de643e58d3ba
Yanling <yanlinghu@google.com>
2026-05-11 08:27:42 -0700

    [Feed] Part11 of remove web feed feature: clean up deprecated Web Feed and Supervised Feed histograms
    This CL removes the remaining Web Feed and Supervised Feed specific
    histograms from histograms.xml, as these features have been deprecated.
    Specifically, the Supervised Web Feed was deprecated long ago:
    https://b.corp.google.com/issues/385899804
    
    Removed Histograms (full removal of the histogram definition):
    - ContentSuggestions.Feed.SupervisedFeed.Opened
    - ContentSuggestions.Feed.WebFeed.Opened
    - ContentSuggestions.Feed.SupervisedFeed.Shown
    - ContentSuggestions.Feed.WebFeed.Shown
    
    Removed Variants (removed from parameterized histograms using the
    FeedType token): ContentSuggestions.Feed.SupervisedFeed and
    ContentSuggestions.Feed.WebFeed were removed as variants for the
    following histograms:
    - ContentSuggestions.{FeedType}.LoadStreamStatus.BackgroundRefresh
    - ContentSuggestions.{FeedType}.LoadStreamStatus.Initial
    - ContentSuggestions.{FeedType}.LoadStreamStatus.InitialFromStore
    - ContentSuggestions.{FeedType}.LoadStepLatency{FeedLoadLatencyStep}
    - ContentSuggestions.{FeedType}.DisplayStatusOnOpen
    - ContentSuggestions.{FeedType}.Network.ResponseStatus{Type}
    
    OBSOLETE_HISTOGRAMS=Removed as part of the web feed feature deprecation.
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size;luci.chrome.try:android-internal-rel;luci.chrome.try:android-internal-dbg
    Bug: 407797637
    Change-Id: I5fc030df3171f96aaaf37f5840a0d1bc214cdb37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812954
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628587}

--- components/feed/core/v2/metrics_reporter.cc
+++ components/feed/core/v2/metrics_reporter.cc
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace feed {
 namespace {
-StreamKind kStreamKinds[] = {StreamKind::kForYou, StreamKind::kFollowing};
+StreamKind kStreamKinds[] = {StreamKind::kForYou};
 
 using feed::FeedEngagementType;
 using feed::FeedUserActionType;
@@ -80,8 +80,8 @@ std::string_view HistogramReplacement(const StreamType& stream_type) {
   switch (stream_type.GetKind()) {
     case StreamKind::kForYou:
       return "Feed.";
+      // TODO(crbug.com/407797637): Remove kFollowing from StreamKind
     case StreamKind::kFollowing:
-      return "Feed.WebFeed.";
     case StreamKind::kUnknown:
       DCHECK(false) << "unknown feed kind";
       return "Feed.";
@@ -109,9 +109,7 @@ void ReportContentSuggestionsOpened(const StreamType& stream_type,
                                     index_in_stream, kMaxSuggestionsTotal);
       break;
     case StreamKind::kFollowing:
-      base::UmaHistogramExactLinear("ContentSuggestions.Feed.WebFeed.Opened",
-                                    index_in_stream, kMaxSuggestionsTotal);
-      break;
+      // TODO(crbug.com/407797637): Remove kFollowing from StreamKind
     case StreamKind::kUnknown:
       DCHECK(false) << "unknown feed kind";
       break;
@@ -457,9 +455,7 @@ void MetricsReporter::ContentSliceViewed(const StreamType& stream_type,
                                     index_in_stream, kMaxSuggestionsTotal);
       break;
     case StreamKind::kFollowing:
-      base::UmaHistogramExactLinear("ContentSuggestions.Feed.WebFeed.Shown",
-                                    index_in_stream, kMaxSuggestionsTotal);
-      break;
+      // TODO(crbug.com/407797637): Remove kFollowing from StreamKind
     case StreamKind::kUnknown:
       DCHECK(false) << "unknown feed kind";
       break;
@@ -1050,10 +1046,9 @@ MetricsReporter::StreamStats& MetricsReporter::ForStream(
     case StreamKind::kForYou:
       return for_you_stats_;
     case StreamKind::kFollowing:
-      return web_feed_stats_;
     case StreamKind::kUnknown:
       DCHECK(false) << "unknown feed kind";
-      return web_feed_stats_;
+      return for_you_stats_;
   }
 }
 
