3376109c2d7288c2955053cbb1590c0523b9f501
Eva Su <evasu@chromium.org>
2026-05-11 13:28:11 -0700

    [Desktop Android] Fix toolbar icon scaling on extended displays
    This CL addresses an issue where toolbar icons become abnormally large
    when interacting with an extended display on Desktop Android.
    
    Screen recording:
    https://drive.google.com/file/d/1mO-xNugE4iszlkCmftpIs0qP0sFrC-gF/view
    
    Potential causes for this were:
    - `ExtensionActionIconUtil` was using
    `DisplayAndroid.getNonMultiDisplay(context)` to calculate scaling
    factors, which can return incorrect metrics in multi-display setups.
    - Configuration changes (like density changes when moving between
    displays) were not triggering a re-layout or re-read of dimensions for
    some toolbar components.
    
    Bug: 510263631
    Change-Id: Ibd2255ff5516f686426da34e6fcf0fa59ccd6175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829767
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628779}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionAccessControlButtonMediator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionAccessControlButtonMediator.java
@@ -162,7 +162,11 @@ class ExtensionAccessControlButtonMediator implements Destroyable {
 
             Bitmap iconBitmap =
                     ExtensionActionIconUtil.getIcon(
-                            mContext, mExtensionsToolbarBridge, extensionId, webContents);
+                            mContext,
+                            webContents.getTopLevelNativeWindow(),
+                            mExtensionsToolbarBridge,
+                            extensionId,
+                            webContents);
             if (iconBitmap != null) {
                 iconDrawable = new BitmapDrawable(mContext.getResources(), iconBitmap);
             }
