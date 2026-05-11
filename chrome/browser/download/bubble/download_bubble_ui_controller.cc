60cdabc36008949dad3f89a5e65ceb3956f46252
Tiago Vignatti <tvignatti@igalia.com>
2026-05-11 11:45:54 -0700

    Remove dead UI includes in download/bubble
    The .cc files don't use the symbols those headers declare; replace
    the transitive include via browser_window.h with an explicit
    browser_window_interface.h in download_bubble_ui_controller.cc.
    
    Bug: 353332589
    Change-Id: I21c0c0ec613f62b6239d06d1b5a2d96dae9b1ef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830045
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1628713}

--- chrome/browser/download/bubble/download_bubble_ui_controller.cc
+++ chrome/browser/download/bubble/download_bubble_ui_controller.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/offline_items_collection/offline_content_aggregator_factory.h"
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/browser_window/public/profile_browser_collection.h"
 #include "chrome/browser/ui/hats/trust_safety_sentiment_service.h"
--- chrome/browser/download/bubble/download_bubble_utils.cc
+++ chrome/browser/download/bubble/download_bubble_utils.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/download/download_ui_model.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/browser_window/public/profile_browser_collection.h"
 #include "chrome/browser/ui/download/download_item_mode.h"
