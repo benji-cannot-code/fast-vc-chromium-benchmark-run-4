0c17120b698a5ca9bdd585797a077b4e826f1019
Liza Burakova <liza@chromium.org>
2026-05-12 13:47:55 -0700

    [rustmojo] Add support for typemapping
    This CL adds support for mapping Mojom structs to pre-existing Rust
    types by bundling Into/From trait implementations directly into a single
    generated bindings crate.
    
    Unlike C++, there are two conversions that happen for typemapping.
    - A converstion into the Rust type generated from the Mojo bindings
    - Next, a concversion to the user type
    
    To make this portable across machines, we added functionality to the
    generator to compute the relative path of the generated file in the gen
    directory to the traits file in the source tree.
    
    Bug: 493197810
    Change-Id: Iddabd17dfce6c9741cef685e2e65d7718190192d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797819
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629544}

---
+++ mojo/public/rust/bindings/test/test_traits.rs
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use super::TypemappedStruct;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct MyCustomStruct {
+    pub value: i32,
+}
+
+impl From<MyCustomStruct> for TypemappedStruct {
+    fn from(custom: MyCustomStruct) -> Self {
+        Self { a: custom.value }
+    }
+}
+
+impl TryFrom<TypemappedStruct> for MyCustomStruct {
+    type Error = anyhow::Error;
+    fn try_from(mojo: TypemappedStruct) -> Result<Self, Self::Error> {
+        Ok(Self { value: mojo.a })
+    }
+}
