5760a0c060123c01a262303fb7dcd8f43245a008
Martin Kong <martinkong@google.com>
2026-05-12 07:13:22 -0700

    JNI Zero: Remove the compatibility shim
    We have migrated all google3 code to not use the deprecated
    JNI Zero APIs, so we can now remove the compatibility shim.
    
    Bug: 481689330
    Change-Id: Ic89cd7a2f54af519c384eb39c6ace31a24e463f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831062
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629300}

--- third_party/jni_zero/java_refs.h
+++ third_party/jni_zero/java_refs.h
@@ -20,10 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/jni_zero/jni_export.h"
 #include "third_party/jni_zero/logging.h"
 
-#if !defined(JNI_ZERO_ENABLE_COMPAT_API)
-#define JNI_ZERO_ENABLE_COMPAT_API 0
-#endif
-
 // Forward declaration of template class that contains @CalledByNative methods.
 // Must live in a custom / unique namespace to ensure it doesn't collide with
 // namespaces used from @JniType strings.
@@ -261,9 +257,7 @@ class JNI_ZERO_COMPONENT_BUILD_EXPORT JavaRef<jobject> {
     return FromJniType<To>(env, *this);
   }
 
-#if !JNI_ZERO_ENABLE_COMPAT_API
  protected:
-#endif
 // Takes ownership of the |obj| reference passed; requires it to be a local
 // reference type.
 #if JNI_ZERO_DCHECK_IS_ON()
@@ -271,10 +265,6 @@ class JNI_ZERO_COMPONENT_BUILD_EXPORT JavaRef<jobject> {
   JavaRef(JNIEnv* env, jobject obj);
 #else
   JavaRef(JNIEnv* env, jobject obj) : obj_(obj) {}
-#endif
-
-#if JNI_ZERO_ENABLE_COMPAT_API
- protected:
 #endif
 
   // Used for move semantics. obj_ must have been released first if non-null.
@@ -375,10 +365,7 @@ class JavaRef : public JavaRef<jobject> {
         env, static_cast<JArray<ElementType>>(this->obj()));
   }
 
-#if !JNI_ZERO_ENABLE_COMPAT_API
  protected:
-#endif
-
   JavaRef(JNIEnv* env, jobject obj) : JavaRef<jobject>(env, obj) {}
 };
 
@@ -567,14 +554,9 @@ class ScopedJavaLocalRef : public JavaRef<T> {
     return std::move(*reinterpret_cast<ScopedJavaLocalRef<To>*>(this));
   }
 
-#if !JNI_ZERO_ENABLE_COMPAT_API
  private:
-#endif
   ScopedJavaLocalRef(JNIEnv* env, jobject obj)
       : JavaRef<T>(env, obj), env_(env) {}
-#if JNI_ZERO_ENABLE_COMPAT_API
- private:
-#endif
 
   // This class is only good for use on the thread it was created on so
   // it's safe to cache the non-threadsafe JNIEnv* inside this object.
@@ -814,11 +796,6 @@ inline ScopedJavaLocalRef<T> JavaRef<internal::_JObjectArray<T>*>::Get(
   jobject obj = env->GetObjectArrayElement(this->obj(), index);
   return jni_zero::AdoptRef(env, static_cast<T>(obj));
 }
-
-#if JNI_ZERO_ENABLE_COMPAT_API
-template <typename T>
-using JavaParamRef = JavaRef<T>;
-#endif
 
 // Identity overload for FromJniType when the destination type is already a
 // JavaRef.
