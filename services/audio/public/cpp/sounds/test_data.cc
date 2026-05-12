c49f1c33fd4ff5080eab95207c300f53d1396d7f
Vidhan Jain <vidhanj@google.com>
2026-05-12 07:32:55 -0700

    Revert "audio: Add looping support to `audio::SoundsManager`"
    This reverts commit 2312ad6665c8d80494d07744576c392171634c65.
    
    Reason for revert: Causing test failures in build
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20TSan%20Tests/100895/overview
    
    Original change's description:
    > audio: Add looping support to `audio::SoundsManager`
    >
    > This CL adds a looping support to `audio::SoundsManager`. It also
    > extends `audio::TestObserver` adding an 'on render' callback, needed to
    > properly test the looping functionality.
    >
    > Bug: 510295495
    > Change-Id: I55bad0f68bca02dcc2f2862a3a740ee952aa4587
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7822759
    > Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1629113}
    
    Bug: 510295495
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I29ae6122491e4da3933bb5a6cccaee510b8a666f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7840229
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629315}

--- services/audio/public/cpp/sounds/test_data.cc
+++ services/audio/public/cpp/sounds/test_data.cc
@@ -5,18 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/audio/public/cpp/sounds/test_data.h"
 
-#include <utility>
-
 #include "base/task/single_thread_task_runner.h"
 #include "media/base/audio_bus.h"
 
 namespace audio {
 
-TestObserver::TestObserver(base::RepeatingClosure quit,
-                           base::RepeatingClosure render)
+TestObserver::TestObserver(const base::RepeatingClosure& quit)
     : task_runner_(base::SingleThreadTaskRunner::GetCurrentDefault()),
-      quit_(std::move(quit)),
-      render_(std::move(render)),
+      quit_(quit),
       num_play_requests_(0),
       num_stop_requests_(0) {}
 
@@ -32,20 +28,18 @@ void TestObserver::Initialize(
 void TestObserver::OnPlay() {
   ++num_play_requests_;
   is_playing = true;
-  task_runner_->PostTask(FROM_HERE, base::BindOnce(&TestObserver::Render,
-                                                   weak_factory_.GetWeakPtr()));
+  task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&TestObserver::Render, base::Unretained(this)));
 }
 
 void TestObserver::Render() {
   if (!is_playing) {
     return;
   }
-  render_.Run();
   if (callback_->Render(base::Seconds(0), base::TimeTicks::Now(), {},
                         bus_.get())) {
-    task_runner_->PostTask(
-        FROM_HERE,
-        base::BindOnce(&TestObserver::Render, weak_factory_.GetWeakPtr()));
+    task_runner_->PostTask(FROM_HERE, base::BindOnce(&TestObserver::Render,
+                                                     base::Unretained(this)));
   }
 }
 
