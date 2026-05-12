7dce38fca2aa5dbe3863ee799a4ad33d2233f87b
Brandon Wylie <wylieb@google.com>
2026-05-11 19:23:34 -0700

    [aim] Add fling to dismiss in a custom scroll view
    Doing so in a custom scroll view makes it so the fling gesture
    can be detected prior to the touch events being consumed by the
    views/subviews.
    
    Bug: 510461222
    Change-Id: I23247601dc0c475cece7c44dc9f38e33bd368bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837278
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628973}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxPopup.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxPopup.java
@@ -12,7 +12,6 @@ import android.view.ViewStub;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 import android.widget.PopupWindow.OnDismissListener;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.core.graphics.Insets;
@@ -44,7 +43,7 @@ class FuseboxPopup {
 
     /* package */ final AnchoredPopupWindow mPopupWindow;
     /* package */ final ViewGroup mViewGroup;
-    /* package */ final ScrollView mScrollView;
+    /* package */ final FuseboxScrollView mScrollView;
     /* package */ final View mAddCurrentTab;
     /* package */ final View mTabButton;
     /* package */ final View mClipboardButton;
@@ -107,6 +106,10 @@ class FuseboxPopup {
                 });
         mScrollView = contentView.findViewById(R.id.fusebox_scroll_view);
         mInitialScrollPaddingBottom = mScrollView.getPaddingBottom();
+
+        if (isBottomSheet) {
+            mScrollView.setOnSwipeDownListener(this::dismiss);
+        }
 
         ViewStub stub = contentView.findViewById(R.id.fusebox_attachments_stub);
         stub.setLayoutResource(
@@ -175,6 +178,7 @@ class FuseboxPopup {
         if (mInsetObserver != null) {
             mInsetObserver.removeObserver(mWindowInsetObserver);
         }
+        mScrollView.setOnSwipeDownListener(null);
     }
 
     /** Show the popup window. */
