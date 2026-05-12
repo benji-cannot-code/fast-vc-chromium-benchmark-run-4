44253530ab31cc9d8f85ad4fe4642f28a9a557a9
Stefano Duo <stefanoduo@google.com>
2026-05-12 03:36:51 -0700

    Guard AndroidProxyOptions usage behind an SDK version check
    We should only be calling into android.net.http.ProxyOptions if the
    device is up to date.
    
    Change-Id: I3ff066505a4e011802e89dcfa70ac897e7a00286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828237
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629201}

--- components/cronet/android/java/src/org/chromium/net/AndroidProxyOptions.java
+++ components/cronet/android/java/src/org/chromium/net/AndroidProxyOptions.java
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.net;
 
 import android.net.http.HttpEngine;
+import android.os.Build;
+import android.os.ext.SdkExtensions;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +20,12 @@ public final class AndroidProxyOptions {
     public static void apply(
             @NonNull HttpEngine.Builder backend,
             @Nullable org.chromium.net.ProxyOptions proxyOptions) {
+        if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+                && SdkExtensions.getExtensionVersion(Build.VERSION_CODES.S) >= 22)) {
+            throw new UnsupportedOperationException(
+                    "This Cronet implementation does not support ProxyOptions");
+        }
+
         if (proxyOptions == null) {
             backend.setProxyOptions(null);
             return;
