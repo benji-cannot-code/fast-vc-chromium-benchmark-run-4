a125439ac04006722faf6b2b3be0b3cbeec6ab1f
Nate Fischer <ntfschr@google.com>
2026-05-12 05:47:11 -0700

    Fix thread safety of AwSettings.setWebauthnSupport
    AwSettings.setWebauthnSupport was not thread-safe because it called
    WebauthnModeProvider.setWebauthnModeForWebContents which ultimately
    made a JNI call that accessed WebContents on a non-UI thread.
    
    This CL fixes the issue by wrapping the WebauthnModeProvider call in
    mEventHandler.runOnUiThreadBlockingAndLocked, which ensures it runs
    on the UI thread.
    
    This also adds a test testSetWebauthnSupportFromNonUIThread to verify
    that both setWebauthnSupport/getWebauthnSupport can be called from a
    non-UI thread without crashing.
    
    Fixed: 512242132
    Test: run_webview_instrumentation_test_apk -f AwSettingsTest.*
    Change-Id: If1c37e5f84b935b82087232a8569d7c2c8e979e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7839758
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629251}

--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -2399,8 +2399,13 @@ public class AwSettings {
             if (mWebauthnMode != support) {
                 mWebauthnMode = support;
                 mEventHandler.updateWebkitPreferencesLocked();
-                WebauthnModeProvider.getInstance()
-                        .setWebauthnModeForWebContents(mWebContents, support);
+                mEventHandler.runOnUiThreadBlockingAndLocked(
+                        () -> {
+                            if (mWebContents != null) {
+                                WebauthnModeProvider.getInstance()
+                                        .setWebauthnModeForWebContents(mWebContents, support);
+                            }
+                        });
             }
         }
     }
