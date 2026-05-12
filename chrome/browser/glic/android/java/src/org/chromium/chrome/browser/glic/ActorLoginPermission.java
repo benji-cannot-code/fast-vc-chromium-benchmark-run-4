0de0c51c4d6806ad45e2fc22295cc3a529f15076
Lijin Shen <lazzzis@google.com>
2026-05-12 13:38:56 -0700

    [Actor] Add settings ui of glic login permission management
    Ux mock:
    https://docs.google.com/presentation/d/13yIIm6GQVTgkwgnSoWkUJdL4QGmD3J2cWG4LxDBPTfA/edit?slide=id.g3d165097925_0_0&resourcekey=0-xTN2w8VHviGrusQcL6MTgQ#slide=id.g3d165097925_0_0
    
    screenshot:
    
    * https://screenshot.googleplex.com/C47mUitLqC7Lktw
    * https://screenshot.googleplex.com/a4GpVpM7fViJYRQ
    
    Note that these screenshots use mock data. I will refine the UI once
    real data is available.
    
    Bug: 500353055
    Change-Id: If00e58da7c99a5ad315986e8b4db1f8b27f37a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802258
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629538}

--- chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/ActorLoginPermission.java
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/ActorLoginPermission.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.glic;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.jni_zero.CalledByNative;
 
 import org.chromium.build.annotations.NullMarked;
@@ -20,7 +22,8 @@ public class ActorLoginPermission {
     private final GURL mFaviconUrl;
 
     @CalledByNative
-    private ActorLoginPermission(
+    @VisibleForTesting
+    ActorLoginPermission(
             String siteOrAppName, GURL url, String signonRealm, String username, GURL faviconUrl) {
         mSiteOrAppName = siteOrAppName;
         mUrl = url;
