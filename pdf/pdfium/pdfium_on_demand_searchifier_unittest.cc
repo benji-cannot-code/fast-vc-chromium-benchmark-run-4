468dae289419d56c49b64ff66f1712d615049194
Lei Zhang <thestig@chromium.org>
2026-05-12 12:20:01 -0700

    [PDF] Add a return value to PDFiumPage::Unload()
    Return whether Unload() actually did something. Use it in existing tests
    to make sure unloading worked, and add test cases to show its
    interactions with unload preventers.
    
    Change-Id: I788f57af805d0c1ffa2cb0cb61abf098732794e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827494
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629498}

--- pdf/pdfium/pdfium_on_demand_searchifier_unittest.cc
+++ pdf/pdfium/pdfium_on_demand_searchifier_unittest.cc
@@ -328,7 +328,7 @@ TEST_P(PDFiumOnDemandSearchifierTest, PageWithImagesNoRecognizableText) {
   EXPECT_TRUE(GetPageText(page).empty());
 
   // Unload the page where Searchify did not add any text.
-  page.Unload();
+  ASSERT_TRUE(page.Unload());
 
   // Get the text from the page, which reloads the page.
   EXPECT_EQ(GetPageText(page), "");
@@ -396,7 +396,7 @@ TEST_P(PDFiumOnDemandSearchifierTest, AddedTextPreservedAfterUnload) {
   EXPECT_EQ(GetPageText(GetPDFiumPage(*engine, 3)), "OCR Text 3");
 
   for (int page = 0; page < kPageCount; page++) {
-    GetPDFiumPage(*engine, page).Unload();
+    ASSERT_TRUE(GetPDFiumPage(*engine, page).Unload());
   }
   EXPECT_EQ(GetPageText(GetPDFiumPage(*engine, 0)), "OCR Text 0");
   EXPECT_EQ(GetPageText(GetPDFiumPage(*engine, 1)), "OCR Text 1");
@@ -441,7 +441,7 @@ TEST_P(PDFiumOnDemandSearchifierTest, MultipleImagesWithUnload) {
   ASSERT_EQ(GetPageText(page), "");
 
   // Unloading the page, doesn't result in canceling the task in `searchifier`.
-  page.Unload();
+  ASSERT_TRUE(page.Unload());
   ASSERT_TRUE(searchifier->IsPageScheduled(0));
 
   // Let `searchifier` finish.
@@ -465,7 +465,7 @@ TEST_P(PDFiumOnDemandSearchifierTest, MultiplePagesWithUnload) {
   }
 
   PDFiumPage& page0 = GetPDFiumPage(*engine, 0);
-  page0.Unload();
+  ASSERT_TRUE(page0.Unload());
 
   PDFiumOnDemandSearchifier* searchifier = engine->GetSearchifierForTesting();
   ASSERT_TRUE(searchifier);
@@ -514,7 +514,7 @@ TEST_P(PDFiumOnDemandSearchifierTest, MultiplePagesWithUnload) {
   EXPECT_TRUE(page3_info.value().is_searchified);
 
   // Unload a Searchified page.
-  page3.Unload();
+  ASSERT_TRUE(page3.Unload());
 
   // Get the text from the page, which reloads the page. It still has the
   // Searchified text because OCR finished and the text has been committed into
@@ -729,7 +729,7 @@ TEST_P(PDFiumOnDemandSearchifierTest, UpdateWithUnloadedPage) {
   // Load page 0 to schedule it for searchify.
   EXPECT_TRUE(page0.GetPage());
 
-  page1.Unload();
+  ASSERT_TRUE(page1.Unload());
 
   StartSearchify(/*empty_results=*/false);
   PDFiumOnDemandSearchifier* searchifier = engine->GetSearchifierForTesting();
