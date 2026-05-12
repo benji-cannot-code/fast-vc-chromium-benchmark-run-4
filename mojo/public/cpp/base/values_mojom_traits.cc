84cd3c8edcc8acc64ec2c6f35a2baf6bdf62ff90
Dylan Jew <dylanj@google.com>
2026-05-12 09:50:31 -0700

    Handle NaN/Infinite double values in base::Value's StructTraits
    The digital_identity_request_in_process_fuzzer is passing in NaN and
    Infinite values and this is triggering a CHECK failure in base/values.cc
    
    https://clusterfuzz.com/testcase-detail/5246040097619968
    
    This CL adds a check for these values and returns false to trigger
    VALIDATION_ERROR_DESERIALIZATION_FAILED instead.
    
    fuzzing output after the CL:
    https://paste.googleplex.com/6078083431006208
    
    Bug: 437363545
    Link: https://chromium-review.googlesource.com/id/I4b7f8233d0342834c7d3ea1fc58c5bac6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827735
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dylan Jew <dylanj@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629407}

--- mojo/public/cpp/base/values_mojom_traits.cc
+++ mojo/public/cpp/base/values_mojom_traits.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/base/values_mojom_traits.h"
 
+#include <cmath>
 #include <memory>
 #include <utility>
 
@@ -73,6 +74,9 @@ bool UnionTraits<mojo_base::mojom::ValueDataView, base::Value>::Read(
       return true;
     }
     case mojo_base::mojom::ValueDataView::Tag::kDoubleValue: {
+      if (!std::isfinite(data.double_value())) {
+        return false;
+      }
       *value_out = base::Value(data.double_value());
       return true;
     }
