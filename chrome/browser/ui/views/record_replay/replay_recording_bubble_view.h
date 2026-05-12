906ec08b691c72093d9ba86790f9bbee583cb817
Paul Jensen <pauljensen@chromium.org>
2026-05-11 19:16:28 -0700

    Make Save/ReplayRecordingBubbleView compatible with BubbleAnchor
    Previously they were assuming the anchor was a View and using its
    ColorProvider.  This changes makes them compatible with WebUI
    page action icon anchoring, and uses the WebContent's
    ColorProvider instead of the anchor's, which should be the same.
    
    Also, this uncovered the hidden prerequisite that
    BubbleDiaglogDelegateView::SetMainImage() only worked with Images.
    This change added a comment and CHECK to this effect.
    
    Bug: 501451284
    Change-Id: I1366bd04abafecbd09b9d59cac5ecbb769b91c68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7834832
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628962}

--- chrome/browser/ui/views/record_replay/replay_recording_bubble_view.h
+++ chrome/browser/ui/views/record_replay/replay_recording_bubble_view.h
@@ -17,7 +17,6 @@ class WebContents;
 }
 
 namespace views {
-class View;
 class Widget;
 }  // namespace views
 
@@ -34,12 +33,12 @@ class ReplayRecordingBubbleView : public LocationBarBubbleDelegateView {
   // `recording_name` is the name of the recording to show.
   // `on_replay_started` is called when the user confirms replay.
   static std::unique_ptr<views::Widget> Show(
-      views::View* anchor_view,
+      views::BubbleAnchor anchor,
       content::WebContents* web_contents,
       const std::u16string& recording_name,
       base::OnceClosure on_replay_started);
 
-  ReplayRecordingBubbleView(views::View* anchor_view,
+  ReplayRecordingBubbleView(views::BubbleAnchor anchor,
                             content::WebContents* web_contents,
                             const std::u16string& recording_name,
                             base::OnceClosure on_replay_started);
