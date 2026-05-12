0e4c2edc2c85cd3bc638c37da726fe77ee2c9a06
Vincent Chiang <vincentchiang@chromium.org>
2026-05-11 19:18:27 -0700

    Allow PIP window to move to another display
    Wrap PipWindowResizer with DragWindowResizer to reuse the logic for
    multi display dragging. Add a method in DragWindowResizer to forward
    pinch actions to pip window. This feature is limited to mouse drag.
    
    Bug: b:508366548
    Change-Id: I86a25085fab2741695940a9f19e18bb5cc9d7843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830526
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628965}

--- ash/wm/drag_window_resizer.h
+++ ash/wm/drag_window_resizer.h
@@ -38,6 +38,7 @@ class ASH_EXPORT DragWindowResizer : public WindowResizer {
   void CompleteDrag() override;
   void RevertDrag() override;
   void FlingOrSwipe(ui::GestureEvent* event) override;
+  void Pinch(const gfx::PointF& location, float scale) override;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(DragWindowResizerTest, DragWindowController);
