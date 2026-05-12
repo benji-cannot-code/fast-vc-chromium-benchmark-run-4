8acf0343ac34489b91cdce0a20c3d6c3f490fff1
Fiona Macintosh <fmacintosh@google.com>
2026-05-12 08:57:17 -0700

    Remove send feedback link from third-party cookies Clank page info
    Feedback is no longer being looked at or used, and link has already been
    removed on desktop
    
    Bug: b:382043551
    Change-Id: I1397eb6b78705557536b9a2dc251c9c9fe658bb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838258
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629342}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesSettings.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoCookiesSettings.java
@@ -56,7 +56,6 @@ public class PageInfoCookiesSettings extends BaseSiteSettingsFragment {
     private TextMessagePreference mCookieSummary;
     private Runnable mOnClearCallback;
     private Runnable mOnCookieSettingsLinkClicked;
-    private Callback<Activity> mOnFeedbackClicked;
     private @Nullable Dialog mConfirmationDialog;
     private boolean mDeleteDisabled;
     private boolean mDataUsed;
@@ -135,7 +134,6 @@ public class PageInfoCookiesSettings extends BaseSiteSettingsFragment {
         mDaysUntilExpirationForTesting = params.daysUntilExpirationForTesting;
         mDeleteDisabled = params.disableCookieDeletion;
         mOnClearCallback = params.onClearCallback;
-        mOnFeedbackClicked = params.onFeedbackLinkClicked;
         mHostName = params.hostName;
 
         // Initialize UI elements that are based on params.
@@ -251,23 +249,8 @@ public class PageInfoCookiesSettings extends BaseSiteSettingsFragment {
             }
         }
         mThirdPartyCookiesTitle.setTitle(title);
-
-        int resId =
-                expiration == 0
-                        ? R.string
-                                .page_info_cookies_tracking_protection_permanent_allowed_description
-                        : R.string.page_info_cookies_send_feedback_description;
-        mThirdPartyCookiesSummary.setSummary(
-                SpanApplier.applySpans(
-                        getString(resId),
-                        new SpanApplier.SpanInfo(
-                                "<link>",
-                                "</link>",
-                                new ChromeClickableSpan(
-                                        getContext(),
-                                        (view) -> {
-                                            mOnFeedbackClicked.onResult(this.getActivity());
-                                        }))));
+        int resId = R.string.page_info_tracking_protection_cookies_permanent_allowed_description;
+        mThirdPartyCookiesSummary.setSummary(getString(resId));
     }
 
     /**
