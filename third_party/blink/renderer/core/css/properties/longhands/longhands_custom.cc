fee4e4af1181c1e3fea23b15b60a8b7794caa6b2
Suyeon Ji <suyeon@chromium.org>
2026-05-12 09:09:01 -0700

    Revert "Fix border-image reset logic in border shorthand"
    This reverts commit 2b0511e730015539b271ad956f207caf91d46804.
    
    Reason for revert: memory regression
    https://issues.chromium.org/issues/512150818
    
    Original change's description:
    > Fix border-image reset logic in border shorthand
    >
    > According to the spec[1][2], the border shorthand should reset
    > border-image to its initial value. However, Chromium currently sets
    > border-image longhands to the "initial" keyword instead of their actual
    > initial values. This causes the serialization of border-image to return
    > "initial" instead of "none".
    >
    > To fix this, the border shorthand now expands border-image longhands to
    > their respective initial values during parsing. Additionally,
    > border-image serialization now returns only the border-image-source
    > value when all other longhands are at their initial values.
    >
    > [1] https://drafts.csswg.org/css-backgrounds/#propdef-border
    > [2] https://drafts.csswg.org/css-backgrounds/#border-image
    >
    > Bug: 497112477
    > Change-Id: Ic0cc1e0cae4497e6bfa4fe8a97b99e363303ab32
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7707435
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Commit-Queue: Suyeon Ji <suyeon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1628515}
    
    Bug: 497112477
    Change-Id: I13b25b16db2df8a265a376cfda5733efde29fa2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7839065
    Commit-Queue: Suyeon Ji <suyeon@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629361}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -1489,10 +1489,7 @@ const CSSValue* BorderImageRepeat::CSSValueFromComputedStyleInternal(
 }
 
 const CSSValue* BorderImageRepeat::InitialValue() const {
-  return MakeGarbageCollected<CSSValuePair>(
-      CSSIdentifierValue::Create(CSSValueID::kStretch),
-      CSSIdentifierValue::Create(CSSValueID::kStretch),
-      CSSValuePair::kDropIdenticalValues);
+  return CSSIdentifierValue::Create(CSSValueID::kStretch);
 }
 
 const CSSValue* BorderImageSlice::ParseSingleValue(
