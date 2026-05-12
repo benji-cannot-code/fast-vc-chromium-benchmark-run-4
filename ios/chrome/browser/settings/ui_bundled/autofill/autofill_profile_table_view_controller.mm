10cd9b4a57b73e787f7b224b8c6b1635be1da0cb
Alexis Hétu <sugoi@chromium.org>
2026-05-12 10:14:28 -0700

    [iOS] Grey out and disable Wallet entities in Settings edit mode
    Server-side Google Wallet entities displayed in Autofill AI settings
    are already not editable/deletable during table view editing.
    However, they currently visually match local entities, which can be
    misleading.
    
    This CL makes these Google Wallet entities visually distinct and non-
    interactive when editing mode is active:
    - Setting their alpha/opacity to 50% to grey them out.
    - Disabling their user interaction to prevent tapping or selection.
    - Reconfiguring cells dynamically on editing state transitions.
    
    I also consolidated the identical checks across standard delegate
    and data source methods (indent, edit style, edit permission) by
    introducing a new `isServerWalletItemAtIndexPath:` helper function.
    
    Bug: 512140600
    Change-Id: I167a87f816ff667391e1156acefa04bac914b03c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837771
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629424}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
@@ -839,6 +839,13 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
                     itemType:ItemTypeAutofillAddressSwitch];
   [self setWalletPromoButtonItemEnabled:!self.tableView.editing];
   [self updatedToolbarForEditState];
+
+  for (NSIndexPath* indexPath in self.tableView.indexPathsForVisibleRows) {
+    UITableViewCell* cell = [self.tableView cellForRowAtIndexPath:indexPath];
+    if (cell) {
+      [self updateOpacityAndInteractionForCell:cell atIndexPath:indexPath];
+    }
+  }
 }
 
 // Override.
@@ -877,25 +884,14 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
 
 - (UITableViewCellEditingStyle)tableView:(UITableView*)tableView
            editingStyleForRowAtIndexPath:(NSIndexPath*)indexPath {
-  TableViewItem* item = [self.tableViewModel itemAtIndexPath:indexPath];
-  if ([item isKindOfClass:[AutofillAIEntityItem class]]) {
-    AutofillAIEntityItem* aiItem =
-        base::apple::ObjCCastStrict<AutofillAIEntityItem>(item);
-    return aiItem.isServerWalletItem ? UITableViewCellEditingStyleNone
-                                     : UITableViewCellEditingStyleDelete;
-  }
-  return UITableViewCellEditingStyleDelete;
+  return [self isServerWalletItemAtIndexPath:indexPath]
+             ? UITableViewCellEditingStyleNone
+             : UITableViewCellEditingStyleDelete;
 }
 
 - (BOOL)tableView:(UITableView*)tableView
     shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath*)indexPath {
-  TableViewItem* item = [self.tableViewModel itemAtIndexPath:indexPath];
-  if ([item isKindOfClass:[AutofillAIEntityItem class]]) {
-    AutofillAIEntityItem* aiItem =
-        base::apple::ObjCCastStrict<AutofillAIEntityItem>(item);
-    return !aiItem.isServerWalletItem;
-  }
-  return YES;
+  return ![self isServerWalletItemAtIndexPath:indexPath];
 }
 
 - (CGFloat)tableView:(UITableView*)tableView
@@ -1057,18 +1053,13 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
 
 - (BOOL)tableView:(UITableView*)tableView
     canEditRowAtIndexPath:(NSIndexPath*)indexPath {
-  if (_settingsAreDismissed) {
+  if (_settingsAreDismissed || [self isServerWalletItemAtIndexPath:indexPath]) {
     return NO;
   }
 
   TableViewItem* item = [self.tableViewModel itemAtIndexPath:indexPath];
-  if ([item isKindOfClass:[AutofillAIEntityItem class]]) {
-    AutofillAIEntityItem* aiItem =
-        base::apple::ObjCCastStrict<AutofillAIEntityItem>(item);
-    return !aiItem.isServerWalletItem;
-  }
-
-  return [item isKindOfClass:[AutofillProfileItem class]];
+  return [item isKindOfClass:[AutofillProfileItem class]] ||
+         [item isKindOfClass:[AutofillAIEntityItem class]];
 }
 
 - (void)tableView:(UITableView*)tableView
@@ -1089,6 +1080,8 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
   selectedBackgroundView.backgroundColor =
       [UIColor colorNamed:kTertiaryBackgroundColor];
   cell.selectedBackgroundView = selectedBackgroundView;
+
+  [self updateOpacityAndInteractionForCell:cell atIndexPath:indexPath];
 
   return cell;
 }
@@ -1327,6 +1320,29 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
 }
 
 #pragma mark - Private
+
+// Returns YES if the item at the given `indexPath` represents a server-side
+// Wallet entity.
+- (BOOL)isServerWalletItemAtIndexPath:(NSIndexPath*)indexPath {
+  TableViewItem* item = [self.tableViewModel itemAtIndexPath:indexPath];
+  if ([item isKindOfClass:[AutofillAIEntityItem class]]) {
+    AutofillAIEntityItem* aiItem =
+        base::apple::ObjCCastStrict<AutofillAIEntityItem>(item);
+    return aiItem.isServerWalletItem;
+  }
+  return NO;
+}
+
+// Updates the opacity and interaction state of the given `cell` based on
+// whether it represents a server wallet entity and the table's editing state.
+- (void)updateOpacityAndInteractionForCell:(UITableViewCell*)cell
+                               atIndexPath:(NSIndexPath*)indexPath {
+  BOOL shouldDisable =
+      [self isServerWalletItemAtIndexPath:indexPath] && self.tableView.editing;
+
+  cell.contentView.alpha = shouldDisable ? 0.5 : 1.0;
+  cell.userInteractionEnabled = !shouldDisable;
+}
 
 - (void)dismissDeletionSheet {
   [_deletionSheetCoordinator stop];
