4636d507536fc7fb3ac8e6b0908e12b9d8faf70a
Michael Wu <mklw@google.com>
2026-05-12 00:39:34 -0700

    Remove TabStripEmptySpaceContextMenuAndroid feature flag
    Flag expired in M150, feature fully launched.
    
    Bug: 509859385
    Change-Id: I6016c134a93614386d2246e038e306eafb2736d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7836296
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629108}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -9407,12 +9407,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kTabStripDensityChangeAndroidDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kTabStripDensityChangeAndroid)},
 
-    {"tab-strip-empty-space-context-menu-android",
-     flag_descriptions::kTabStripEmptySpaceContextMenuAndroidName,
-     flag_descriptions::kTabStripEmptySpaceContextMenuAndroidDescription,
-     kOsAndroid,
-     FEATURE_VALUE_TYPE(
-         chrome::android::kTabStripEmptySpaceContextMenuAndroid)},
 
     {"tab-switcher-group-suggestions-android",
      flag_descriptions::kTabSwitcherGroupSuggestionsAndroidName,
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -6074,10 +6074,6 @@ inline constexpr char kTabStripDensityChangeAndroidDescription[] =
     "mouse, touchpad, etc.) is connected, including reducing minimum tab "
     "width and button touch target to better support click-first interactions.";
 
-inline constexpr char kTabStripEmptySpaceContextMenuAndroidName[] =
-    "Tab Strip Empty Space Context Menu";
-inline constexpr char kTabStripEmptySpaceContextMenuAndroidDescription[] =
-    "Enables the context menu on the empty space of the tab strip.";
 
 inline constexpr char kThreeDotMenuBackButtonName[] =
     "Three Dot Menu Back Button";
