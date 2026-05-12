ad1338309e294211dcdf25c2d0f7b32d12654d73
Gayane Petrosyan <gayane@google.com>
2026-05-12 06:56:26 -0700

    [iOS][gnp] Create backend promo service factory
    Boilerplate code that adds profile keyed service factory for backend
    promo service.
    
    Bug: 503366680
    Change-Id: I351d2f231529495c2bbf40978bdb13684b73c26f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7832769
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629286}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/model/ios_wallet_pass_access_manager_factory.h"
 #import "ios/chrome/browser/autofill/model/personal_data_manager_factory.h"
 #import "ios/chrome/browser/autofill/model/strike_database_factory.h"
+#import "ios/chrome/browser/backend_promo/model/backend_promo_service_factory.h"
 #import "ios/chrome/browser/bookmarks/model/account_bookmark_sync_service_factory.h"
 #import "ios/chrome/browser/bookmarks/model/bookmark_model_factory.h"
 #import "ios/chrome/browser/bookmarks/model/bookmark_undo_service_factory.h"
@@ -290,6 +291,7 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   AcceptLanguagesServiceFactory::GetInstance();
   AppStoreBundleServiceFactory::GetInstance();
   AuthenticationServiceFactory::GetInstance();
+  BackendPromoServiceFactory::GetInstance();
   BackgroundDownloadServiceFactory::GetInstance();
   BookmarkModelMetricsServiceFactory::GetInstance();
   BreadcrumbManagerKeyedServiceFactory::GetInstance();
---
+++ ios/chrome/test/providers/backend_promo/test_backend_promo.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/backend_promo/model/backend_promo_service.h"
+
+// Test BackendPromoService implementation.
+class TestBackendPromoService final : public BackendPromoService {
+ public:
+  TestBackendPromoService() = default;
+  ~TestBackendPromoService() override = default;
+};
+
+namespace ios::provider {
+
+std::unique_ptr<BackendPromoService> CreateBackendPromoService() {
+  return std::make_unique<TestBackendPromoService>();
+}
+
+}  // namespace ios::provider
