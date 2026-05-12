49fe20d0cabecfe0d25e88dd1762a2f73e716ce4
Benoît Lizé <lizeb@chromium.org>
2026-05-12 01:31:58 -0700

    [cc] Add metrics for peak ResourcePool memory usage
    There are multiple scenarios where peak memory usage coming from tiles
    can reach a very high value, but in a transient fashion. There is also
    the question of what should be the memory limits. This CL adds metrics
    to help understand the current status in the field, as well as allowing
    to see the impact of efforts to reduce it.
    
    However, this is a not a complete solution: in particular, the metrics
    are per-TileManager, and a given tab (and/or process) can host many,
    most of which small (for instance, for OOPIFs), which needs to be taken
    into accoung when looking at quantiles of the metric.
    
    Bug: 505766103
    Change-Id: Ide49544a8f6074e488e6a13d96c57922a25dd60a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833471
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629139}

--- cc/resources/resource_pool.cc
+++ cc/resources/resource_pool.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/format_macros.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
@@ -211,6 +212,12 @@ ResourcePool::~ResourcePool() {
   base::trace_event::MemoryDumpManager::GetInstance()->UnregisterDumpProvider(
       this);
 
+  UMA_HISTOGRAM_MEMORY_MEDIUM_MB(
+      "Compositing.ResourcePool.PeakMemoryUsage",
+      peak_total_memory_usage_bytes_ / (1024 * 1024));
+  UMA_HISTOGRAM_COUNTS_10000("Compositing.ResourcePool.PeakResourceCount",
+                             peak_total_resource_count_);
+
   DCHECK_EQ(0u, in_use_resources_.size());
 
   while (!busy_resources_.empty()) {
@@ -302,6 +309,8 @@ ResourcePool::PoolResource* ResourcePool::CreateResource(
   // No backing, the memory_usage() should be 0.
   DCHECK_EQ(pool_resource->memory_usage(), 0u);
   ++total_resource_count_;
+  peak_total_resource_count_ =
+      std::max(peak_total_resource_count_, total_resource_count_);
 
   PoolResource* resource = pool_resource.get();
   in_use_resources_[resource->unique_id()] = std::move(pool_resource);
@@ -420,6 +429,8 @@ ResourcePool::TryAcquireResourceForPartialRaster(
 void ResourcePool::OnBackingAllocated(PoolResource* resource) {
   size_t size = resource->memory_usage();
   total_memory_usage_bytes_ += size;
+  peak_total_memory_usage_bytes_ =
+      std::max(peak_total_memory_usage_bytes_, total_memory_usage_bytes_);
   if (resource->state() == PoolResource::kUnused)
     unused_memory_usage_bytes_ += size;
   UpdateTracingCounters();
