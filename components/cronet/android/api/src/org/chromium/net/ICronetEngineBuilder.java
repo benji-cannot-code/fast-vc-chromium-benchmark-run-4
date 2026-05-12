0b15534c5da5c39379a2da793161d40e3061898c
Stefano Duo <stefanoduo@google.com>
2026-05-12 03:37:05 -0700

    Correctly mark AndroidProxyOptions#apply args as non-null
    This drops the arguments in favor of relying on @NullMarked which
    assumes objects to be non-null unless explicitly marked with @Nullable.
    
    Change-Id: Id2e04f173f46317533ed52c0e053934b12da2757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828058
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629202}

--- components/cronet/android/api/src/org/chromium/net/ICronetEngineBuilder.java
+++ components/cronet/android/api/src/org/chromium/net/ICronetEngineBuilder.java
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.net;
 
-import androidx.annotation.Nullable;
-
 import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
@@ -76,7 +74,7 @@ public abstract class ICronetEngineBuilder {
     // Proxy.HttpConnectCallback to fail later on (due to ABI mismatch), this has been renamed to
     // setProxyOptionsV2. This way, callers will always get an UnsupportedOperationException at
     // CronetEngine.Builder#setProxyOptions time, if the implementation being used is too old.
-    public ICronetEngineBuilder setProxyOptionsV2(@Nullable ProxyOptions proxyOptions) {
+    public ICronetEngineBuilder setProxyOptionsV2(ProxyOptions proxyOptions) {
         // API layer last resort: prevents calling setProxyOptionsV2 on an implementation that does
         // not know about it.
         throw new UnsupportedOperationException(
