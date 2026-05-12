e499d567017a6e2b0db682c955a122cb99786a16
Lei Zhang <thestig@chromium.org>
2026-05-12 13:42:59 -0700

    [PDF] Improve some verification code
    - Rephrase comments to better explain what is going on.
    - Remove comments that are no longer needed in static_assert().
    - Change an assumption in a comment to an actual CHECK_GE() call.
    
    Change-Id: I38dfbaffb5ea115196d9453cbb1a718ea62d637e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831465
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629540}

--- pdf/pdfium/pdfium_print.cc
+++ pdf/pdfium/pdfium_print.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/check_op.h"
 #include "base/compiler_specific.h"
 #include "build/build_config.h"
 #include "pdf/flatten_pdf_result.h"
@@ -43,9 +44,10 @@ namespace chrome_pdf {
 
 namespace {
 
-// UI should have done parameter sanity check, when execution
-// reaches here, `pages_per_sheet` should be a positive integer.
+// Assumes the Print Preview UI passed in a positive integer. So this function
+// does not need to handle invalid values like negative values or zero.
 bool ShouldDoNup(int pages_per_sheet) {
+  CHECK_GE(pages_per_sheet, 1);
   return pages_per_sheet > 1;
 }
 
