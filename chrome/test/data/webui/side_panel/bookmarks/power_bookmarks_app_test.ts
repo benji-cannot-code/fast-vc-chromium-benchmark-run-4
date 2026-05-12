91b56a9dac19064c88c998431e294478f5392039
rbpotter <rbpotter@chromium.org>
2026-05-12 09:53:37 -0700

    Side panel bookmarks: Refactor power-bookmarks-list
    Breaking up power-bookmarks-list and creating:
    power-bookmarks-list-header, to hold the header HTML. Also moving the
    sort menu from power-bookmarks-app to this element, and making it
    fully responsible for handling setting the current sorting state.
    
    power-bookmarks-add-folder-button, to reduce repeated HTML and hold
    the large amount of styles for the add folder button.
    
    metrics helper file, to simplify code in power-bookmarks-list and
    power-bookmarks-app
    
    This is to reduce the size of power-bookmarks-list and
    power-bookmarks-app, such that these elements are safer to migrate
    to Lit.
    
    Bug: 493823435
    Change-Id: I55dd398967436df5d0dfc79ef38c858648c35f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830633
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629411}

--- chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_app_test.ts
+++ chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_app_test.ts
@@ -737,9 +737,11 @@ suite('General', () => {
     });
 
     test('SetsExpandedDescription', async () => {
-      const viewButton: HTMLElement =
-          powerBookmarksApp.$.bookmarksList.shadowRoot!.querySelector(
-              '#viewButton')!;
+      const header =
+          powerBookmarksApp.$.bookmarksList.shadowRoot!
+              .querySelector<HTMLElement>('power-bookmarks-list-header')!;
+      const viewButton =
+          header.shadowRoot!.querySelector<HTMLElement>('#viewButton')!;
       viewButton.click();
       await microtasksFinished();
 
@@ -753,8 +755,11 @@ suite('General', () => {
     });
 
     test('SetsExpandedSearchResultDescription', async () => {
-      const viewButton = powerBookmarksApp.$.bookmarksList.shadowRoot!
-                             .querySelector<HTMLElement>('#viewButton')!;
+      const header =
+          powerBookmarksApp.$.bookmarksList.shadowRoot!
+              .querySelector<HTMLElement>('power-bookmarks-list-header')!;
+      const viewButton =
+          header.shadowRoot!.querySelector<HTMLElement>('#viewButton')!;
       viewButton.click();
 
       await performSearch('child bookmark');
@@ -835,9 +840,11 @@ suite('General', () => {
     });
 
     test('ShowsFolderImages', () => {
-      const viewButton: HTMLElement =
-          powerBookmarksApp.$.bookmarksList.shadowRoot!.querySelector(
-              '#viewButton')!;
+      const header =
+          powerBookmarksApp.$.bookmarksList.shadowRoot!
+              .querySelector<HTMLElement>('power-bookmarks-list-header')!;
+      const viewButton =
+          header.shadowRoot!.querySelector<HTMLElement>('#viewButton')!;
       viewButton.click();
 
       flush();
@@ -853,9 +860,11 @@ suite('General', () => {
     });
 
     test('DeletesSelectedBookmarks', async () => {
-      const editButton: HTMLElement =
-          powerBookmarksApp.$.bookmarksList.shadowRoot!.querySelector(
-              '#editButton')!;
+      const header =
+          powerBookmarksApp.$.bookmarksList.shadowRoot!
+              .querySelector<HTMLElement>('power-bookmarks-list-header')!;
+      const editButton =
+          header.shadowRoot!.querySelector<HTMLElement>('#editButton')!;
       editButton.click();
 
       flush();
@@ -927,9 +936,11 @@ suite('General', () => {
 
       await flushTasks();
 
-      const editButton: HTMLElement =
-          powerBookmarksApp.$.bookmarksList.shadowRoot!.querySelector(
-              '#editButton')!;
+      const header =
+          powerBookmarksApp.$.bookmarksList.shadowRoot!
+              .querySelector<HTMLElement>('power-bookmarks-list-header')!;
+      const editButton =
+          header.shadowRoot!.querySelector<HTMLElement>('#editButton')!;
       editButton.click();
 
       flush();
@@ -1037,7 +1048,9 @@ suite('General', () => {
     test('TogglesSectionVisibilityAndEmptyStates', async () => {
       const search = powerBookmarksApp.$.searchField;
       const labels = powerBookmarksApp.$.labels;
-      const heading = powerBookmarksApp.$.bookmarksList.$.heading;
+      const heading =
+          powerBookmarksApp.$.bookmarksList.shadowRoot!.querySelector(
+              'power-bookmarks-list-header')!;
       const folderEmptyState =
           powerBookmarksApp.$.bookmarksList.$.folderEmptyState;
       const bookmarksList = powerBookmarksApp.$.bookmarksList.$.bookmarks;
