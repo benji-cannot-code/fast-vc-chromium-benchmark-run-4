575b992e41628a394994d167304dec9192fd8195
Juan Mojica <juanmojica@google.com>
2026-05-13 15:04:53 -0700

    [lens] Disable LensSearchSidePanelNewFeedback and clean up flag
    Set LensSearchSidePanelNewFeedback feature to DISABLED_BY_DEFAULT and
    remove its corresponding chrome://flags entry and field trial testing
    configuration.
    
    Bug: b:499292292
    Change-Id: If6965002a8c855e8263054bdee1d853dfbb883bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7845016
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630253}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2935,11 +2935,6 @@ inline constexpr char kLensSearchReinvocationAffordanceDescription[] =
     "Enables the Lens button in the AIM Searchbox for reinvocation of "
     "selection overlay.";
 
-inline constexpr char kLensSearchSidePanelNewFeedbackName[] =
-    "Lens side panel new feedback";
-inline constexpr char kLensSearchSidePanelNewFeedbackDescription[] =
-    "Enables a new feedback entry point in the Lens side panel.";
-
 
 inline constexpr char kLensVideoCitationsName[] = "Lens video citations";
 inline constexpr char kLensVideoCitationsDescription[] =
