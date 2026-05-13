f7230baaa340a8834a016f54502ead79ec48843a
Phil Yan <philyan@chromium.org>
2026-05-13 11:14:06 -0700

    [Media] Re-enable AutoPictureInPictureTabHelperTest on Desktop Freeform
    Re-enable testHideAutoPip and testBackToTabPostHideTimeRecorded in
    AutoPictureInPictureTabHelperTest on DESKTOP_FREEFORM.
    
    Extensive local verification (30 runs) using a true Desktop Freeform
    AVD and a desktop-configured build shows that these tests are now 100%
    stable (31/31 successful runs for all tests in the class).
    
    Bug: 511288544
    Change-Id: Ie55fbcfb1bf074935834c001cbc451bc957e102f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843952
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630099}

--- chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
+++ chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
@@ -179,7 +179,6 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511288544
     public void testHideAutoPip() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         Tab originalTab = mPage.getTab();
@@ -544,7 +543,6 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511288544
     public void testBackToTabPostHideTimeRecorded() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         Tab originalTab = mPage.getTab();
