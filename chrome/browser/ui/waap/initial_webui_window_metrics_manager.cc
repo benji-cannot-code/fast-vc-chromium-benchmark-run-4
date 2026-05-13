9714a8f2ae204102d8fd49ea637046f5fd97adb7
Mingyu Lei <leimy@chromium.org>
2026-05-12 17:19:28 -0700

    Reland "Webium product: skip metrics for session restored minimized window"
    This is a reland of commit be815acfd06675b195e8274153793c1482b5fc0c
    
    The test which involves minimize a window seems only working on Windows
    for now, added comments and verified the behavior manually on macOS and
    Linux.
    
    Original change's description:
    > Webium product: skip metrics for session restored minimized window
    >
    > The WebUI duration metrics that rely on subtracting of two time ticks
    > might fail in the case where the browser window is created as minimized.
    > Since the OS will optimize it so that the paint only happens after the
    > user activate the minimized window, the duration recorded in this case
    > might be larger than the time it actually takes.
    >
    > This CL fix that by ignoring the window if it's initially minmized.
    >
    > More details can be found here in the doc (googler only):
    > https://docs.google.com/document/d/1E5MzYBTA3N6ou4FcziALJmDMR2Eq9rHQQIaS9LFNwHU/edit?usp=sharing&resourcekey=0-DmPJK6uNXj20RxjDSRhlwQ
    >
    > Bug: 507317176
    > Change-Id: Ib32be1f9d09fd002d0e61a28e4c15a028601f91f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831810
    > Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    > Commit-Queue: Mingyu Lei <leimy@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1629022}
    
    Bug: 507317176
    Change-Id: I4df6605cd0d103e14aa4c9343330624aa40f232b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7839023
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629649}

--- chrome/browser/ui/waap/initial_webui_window_metrics_manager.cc
+++ chrome/browser/ui/waap/initial_webui_window_metrics_manager.cc
@@ -28,6 +28,7 @@ DEFINE_USER_DATA(InitialWebUIWindowMetricsManager);
 InitialWebUIWindowMetricsManager::InitialWebUIWindowMetricsManager(
     BrowserWindowInterface* browser)
     : waap_service_(WaapUIMetricsService::Get(browser->GetProfile())),
+      browser_(browser),
       scoped_data_holder_(browser->GetUnownedUserDataHost(), *this),
       was_created_with_existing_windows_(
           ProfileBrowserCollection::GetForProfile(browser->GetProfile())
@@ -51,6 +52,11 @@ void InitialWebUIWindowMetricsManager::SetWindowCreationInfo(
 
 void InitialWebUIWindowMetricsManager::OnBrowserWindowShowRequested(
     base::TimeTicks time) {
+  if (browser_ && browser_->GetWindow() &&
+      browser_->GetWindow()->IsMinimized()) {
+    should_skip_latency_metrics_ = true;
+    return;
+  }
   if (!window_show_first_requested_time_.has_value()) {
     window_show_first_requested_time_ = time;
   }
@@ -58,6 +64,9 @@ void InitialWebUIWindowMetricsManager::OnBrowserWindowShowRequested(
 
 void InitialWebUIWindowMetricsManager::OnBrowserWindowFirstPresentation(
     base::TimeTicks timestamp) {
+  if (should_skip_latency_metrics_) {
+    return;
+  }
   // Ensures only one startup window is recorded per browser process.
   bool& is_startup_first_paint_recorded = g_is_startup_first_paint_recorded;
   if (!waap_service_) {
@@ -111,6 +120,9 @@ void InitialWebUIWindowMetricsManager::OnReloadButtonCreated() {
 
 void InitialWebUIWindowMetricsManager::OnReloadButtonFirstPaint(
     base::TimeTicks timestamp) {
+  if (should_skip_latency_metrics_) {
+    return;
+  }
   // Ensures only one startup reload button is recorded per browser process.
   bool& is_startup_first_paint_recorded =
       g_is_startup_reload_first_paint_recorded;
@@ -137,6 +149,9 @@ void InitialWebUIWindowMetricsManager::OnReloadButtonFirstPaint(
 
 void InitialWebUIWindowMetricsManager::OnReloadButtonFirstContentfulPaint(
     base::TimeTicks timestamp) {
+  if (should_skip_latency_metrics_) {
+    return;
+  }
   // Ensures only one startup reload button is recorded per browser process.
   bool& is_startup_first_contentful_paint_recorded =
       g_is_startup_reload_first_contentful_paint_recorded;
@@ -162,6 +177,9 @@ void InitialWebUIWindowMetricsManager::
     OnReloadButtonRendererProcessCreatedAndLaunched(
         base::TimeTicks created_timestamp,
         base::TimeTicks launched_timestamp) {
+  if (should_skip_latency_metrics_) {
+    return;
+  }
   // Ensures only one startup process launch is recorded per browser process.
   bool& is_startup_process_recorded = g_is_startup_process_recorded;
   if (!waap_service_) {
