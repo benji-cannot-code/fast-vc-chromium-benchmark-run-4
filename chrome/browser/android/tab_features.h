8a384bc339549691f55e855bd414add24394bd93
Thomas Lukaszewicz <tluk@chromium.org>
2026-05-12 18:35:11 -0700

    [Android] Enable WebUI embedding context APIs for tabs and bottom sheets
    This CL enables the WebUI embedding context APIs on android by setting
    up the appropriate associations for both browser tabs and bottom
    sheets/side panels.
    
    Bug: 511014565
    Change-Id: Iacccbe974fc55537d6d94066b6607fb4d2463dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7839407
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629688}

--- chrome/browser/android/tab_features.h
+++ chrome/browser/android/tab_features.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_list.h"
 #include "chrome/browser/ui/side_panel/side_panel_registry.h"
 #include "chrome/common/buildflags.h"
 #include "ui/base/unowned_user_data/user_data_factory.h"
@@ -89,6 +90,9 @@ class TabFeatures {
   std::unique_ptr<glic::GlicSidePanelCoordinator> glic_side_panel_coordinator_;
   std::unique_ptr<actor::ui::ActorUiTabControllerInterface>
       actor_ui_tab_controller_;
+
+  // Holds the WebUI embedding context subscription.
+  base::CallbackListSubscription tab_subscription_;
 };
 
 }  // namespace tabs
