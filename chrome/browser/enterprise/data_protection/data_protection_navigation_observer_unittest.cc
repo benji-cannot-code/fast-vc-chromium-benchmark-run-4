0450f90a4fceb2db50f741428c93054373c794c5
Nancy Xiao <nancylanxiao@google.com>
2026-05-13 14:14:55 -0700

    Clean up json-based test coverage from misc enterprise tests (2/n)
    Bug: b:478929452
    Change-Id: If0e6d987d2094a81f245af285c4d9469c0b5e63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842754
    Commit-Queue: Lina Ismail <ismaill@google.com>
    Auto-Submit: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Lina Ismail <ismaill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630211}

--- chrome/browser/enterprise/data_protection/data_protection_navigation_observer_unittest.cc
+++ chrome/browser/enterprise/data_protection/data_protection_navigation_observer_unittest.cc
@@ -292,12 +292,7 @@ TEST_F(DataProtectionNavigationObserverTest, MatchedAuditRuleHasEvent) {
   base::RunLoop run_loop;
   validator.SetDoneClosure(run_loop.QuitClosure());
 
-  if (base::FeatureList::IsEnabled(
-          policy::kUploadRealtimeReportingEventsUsingProto)) {
-    validator.ExpectProtoBasedUrlFilteringInterstitialEvent(expected_event);
-  } else {
-    validator.ExpectURLFilteringInterstitialEvent(expected_event);
-  }
+  validator.ExpectProtoBasedUrlFilteringInterstitialEvent(expected_event);
 
   lookup_service_.SetShouldHaveMatchedRule(true);
   lookup_service_.SetWatermarkTextForURL(GURL("https://example.com/"),
@@ -980,12 +975,7 @@ TEST_P(OrderedDataProtectionNavigationObserverTest, TestWatermarkTextUpdated) {
   enterprise_connectors::test::EventReportValidator validator(client_.get());
   base::RunLoop run_loop;
   validator.SetDoneClosure(run_loop.QuitClosure());
-  if (base::FeatureList::IsEnabled(
-          policy::kUploadRealtimeReportingEventsUsingProto)) {
-    validator.ExpectProtoBasedUrlFilteringInterstitialEvent(expected_event);
-  } else {
-    validator.ExpectURLFilteringInterstitialEvent(expected_event);
-  }
+  validator.ExpectProtoBasedUrlFilteringInterstitialEvent(expected_event);
 
   base::test::TestFuture<const UrlSettings&> future;
   FakeDataProtectionNavigationController controller(
