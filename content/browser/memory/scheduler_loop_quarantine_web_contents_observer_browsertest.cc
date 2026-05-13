2bb0a7048d97b2bad52aae7b5e8928a7df3d3e41
mikt <mikt@google.com>
2026-05-13 02:44:08 -0700

    Launch PartitionAllocWithAdvancedChecks (MiracleObject) on Browser UI
    Enabling the feature by default. Fixing some unit test failures.
    
    Bug: 351974425
    Change-Id: I63d7522907fe459da035c8e0ddf2c5b27425551a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761746
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629854}

--- content/browser/memory/scheduler_loop_quarantine_web_contents_observer_browsertest.cc
+++ content/browser/memory/scheduler_loop_quarantine_web_contents_observer_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/escape.h"
 #include "base/strings/string_util.h"
 #include "base/test/scoped_feature_list.h"
+#include "content/common/features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -89,16 +90,15 @@ class SchedulerLoopQuarantineWebContentsObserverBrowserTest
           }},
          { base::features::kPartitionAllocWithAdvancedChecks,
            {} }},
-        // Disable preloading `kPrewarm` because it causes the browser to load a
-        // webpage before each test starts (during SetUp) which breaks our
-        // tests.
-        {});
+        // Disable the task observer because the test body is not considered
+        // to be inside a "task".
+        // TODO(https://crbug.com/351974425): Convert the test to run inside a
+        // task.
+        {features::
+             kPartitionAllocSchedulerLoopQuarantineTaskObserverForBrowserUIThread});
 #else
     feature_list_.InitWithFeatures(
         {},
-        // Disable preloading `kPrewarm` because it causes the browser to load a
-        // webpage before each test starts (during SetUp) which breaks our
-        // tests.
         {});
 #endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
     // Ensure even when USE_PARTITION_ALLOC_AS_MALLOC is false we use the
