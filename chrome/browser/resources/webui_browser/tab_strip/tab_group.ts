d7e22747c3ddef2c76904660c4b626253d7d7f3e
David Yeung <dayeung@chromium.org>
2026-05-13 08:06:34 -0700

    [webium tabstrip] Add colors to TabGroup
    Define theme colors and foreground text colors for tab groups within the
    webui browser tab strip. This supports a dynamic color mapping based on
    individual group metadata.
    
    The current TabStripAPI does not reference a direct color but uses a
    color id. It is up to the client to choose which color they'd like to
    use.
    
    Bug: 511296274
    Change-Id: I49cda3eda1231fb476a3216a4a3667664962524d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843075
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629973}

--- chrome/browser/resources/webui_browser/tab_strip/tab_group.ts
+++ chrome/browser/resources/webui_browser/tab_strip/tab_group.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {TabGroupVisualData} from '/tab_strip_api/tab_strip_api_data_model.mojom-webui.js';
 
@@ -10,6 +11,18 @@ import {Color} from '../tab_group_types.mojom-webui.js';
 
 import {getCss} from './tab_group.css.js';
 import {getHtml} from './tab_group.html.js';
+
+const colorMap = new Map<Color, string>([
+  [Color.kGrey, 'grey'],
+  [Color.kBlue, 'blue'],
+  [Color.kRed, 'red'],
+  [Color.kYellow, 'yellow'],
+  [Color.kGreen, 'green'],
+  [Color.kPink, 'pink'],
+  [Color.kPurple, 'purple'],
+  [Color.kCyan, 'cyan'],
+  [Color.kOrange, 'orange'],
+]);
 
 export class TabGroupElement extends CrLitElement {
   static get is() {
@@ -37,6 +50,27 @@ export class TabGroupElement extends CrLitElement {
     color: Color.kGrey,
     isCollapsed: false,
   };
+
+  protected get colorName_(): string {
+    const color = colorMap.get(this.groupData.color);
+    if (color === undefined) {
+      throw Error('Undefined color id');
+    }
+    return color;
+  }
+
+  override updated(changedProperties: PropertyValues<this>) {
+    super.updated(changedProperties);
+
+    if (changedProperties.has('groupData')) {
+      const colorName = this.colorName_;
+      this.style.setProperty(
+          '--tab-group-color', `var(--tab-group-color-${colorName})`);
+      this.style.setProperty(
+          '--tab-group-foreground-color',
+          `var(--foreground-color-on-${colorName}-background)`);
+    }
+  }
 }
 
 declare global {
