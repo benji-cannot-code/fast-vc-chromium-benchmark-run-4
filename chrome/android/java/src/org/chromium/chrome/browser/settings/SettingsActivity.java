43ba12c6bd8899e8c534fc7d9c0624dc4003216c
Hailey Wang <haileywang@google.com>
2026-05-12 18:11:57 -0700

    [Glic] Show task in progress snackbar based on finch param
    Task in progress snackbar will be default turned off via finch param as
    we are testing with bottom bar.
    
    Bug: 511220914
    Change-Id: Icb3a49d0a098eded85faa2d6ccb86b135dd01e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7836697
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629681}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -816,7 +816,9 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
-        GlicHelper.maybeShowGlicTaskInProgressSnackbar(this, mProfile, this);
+        if (ChromeFeatureList.sGlicShowTaskInProgressSnackbar.getValue()) {
+            GlicHelper.maybeShowGlicTaskInProgressSnackbar(this, mProfile, this);
+        }
     }
 
     @Override
