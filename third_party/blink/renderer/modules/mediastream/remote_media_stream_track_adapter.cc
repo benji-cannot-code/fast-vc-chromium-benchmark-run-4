4c361cc0b9f350a0570a559590fcc8c9e9b8be75
Philipp Hancke <philipp.hancke@googlemail.com>
2026-05-13 03:47:40 -0700

    webrtc: set remote MediaStreamTrack label to "remote " + <kind>
    as specified by
    https://w3c.github.io/webrtc-pc/#rtcrtpreceiver-interface
    
    Prompt: "Can you help me find where the remote MediaStreamTrack's
    'label' attribute (whose value is its 'id') is being emitted by the
    C++ code? ... Can you change so that it returns the string 'remote '
    with the tracks 'kind'?"
    
    Drive-by: fix `true /*remote*/` to `/*remote=*/true` to match the
    convention used elsewhere in the file's neighborhood.
    
    Guarded by kWebRtcRemoteTrackLabel killswitch.
    
    Bug: chromium:40684245
    Change-Id: I61272607a86580057b9fb49adac76034b3ec2ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829244
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#1629872}

--- third_party/blink/renderer/modules/mediastream/remote_media_stream_track_adapter.cc
+++ third_party/blink/renderer/modules/mediastream/remote_media_stream_track_adapter.cc
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+BASE_FEATURE(kWebRtcRemoteTrackLabel, base::FEATURE_ENABLED_BY_DEFAULT);
+
 RemoteVideoTrackAdapter::RemoteVideoTrackAdapter(
     const scoped_refptr<base::SingleThreadTaskRunner>& main_thread,
     webrtc::VideoTrackInterface* webrtc_track,
