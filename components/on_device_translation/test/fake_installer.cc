b5e22942880e45eed4a50002ca643249f4cbed81
Paula Gómez <paugomez@google.com>
2026-05-12 20:36:14 -0700

    Reland "[on-device-translation] Migrate tests to use FakeInstaller"
    This relands commit 85cacd39c111acba3d2b1fd1c6d829d8c40eaf2b.
    
    Reason for revert: Failed Mac12 Tests because a hardcoded .so file in
    fake installer. Using base::GetNativeLibraryName solved the issue
    
    Original change's description:
    > [on-device-translation] Migrate tests to use FakeInstaller
    >
    > Migrate 10 tests in on_device_translation_browsertest.cc to use
    > FakeOnDeviceTranslationInstaller instead of MockComponentManager to get
    > rid of on-device-translation's ComponentManager. This change also adds
    > SetupFakeInstaller and WriteMockLanguagePackFiles helper functions in
    > test_util to simplify test setup.
    >
    > Bug: 481370174
    > Change-Id: Ida9a9323f82ecf586b25cb2a0fdb8344dccbdb64
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745064
    > Reviewed-by: Nathan Memmott <memmott@chromium.org>
    > Commit-Queue: Paula Gómez <paugomez@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1626284}
    
    Bug: 481370174
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Id8b5301c8ece6088b0d8ea024d69a600d5b0b27d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829997
    Commit-Queue: Paula Gómez <paugomez@google.com>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629746}

--- components/on_device_translation/test/fake_installer.cc
+++ components/on_device_translation/test/fake_installer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/on_device_translation/test/fake_installer.h"
 
+#include "base/native_library.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/test/gmock_callback_support.h"
@@ -31,7 +32,8 @@ FakeOnDeviceTranslationInstaller::InstalledLanguagePacks() const {
   return installed_lang_packs_;
 }
 base::FilePath FakeOnDeviceTranslationInstaller::GetLibraryPath() const {
-  return fake_install_dir_.AppendASCII("fake_installation.so");
+  return fake_install_dir_.AppendASCII(
+      base::GetNativeLibraryName("fake_installation"));
 }
 
 base::FilePath FakeOnDeviceTranslationInstaller::GetLanguagePackPath(
