111be9a6abc6af079cbf184740b3085682efd6c0
Olov Brändström <brandstrom@chromium.org>
2026-05-13 03:52:53 -0700

    Add SendMessageToNativeLog to GetApplicationAudioCaptureIdForProcess.
    Add `SendMessageToNativeLog` logs to
    `GetApplicationAudioCaptureIdForProcess` in
    `content/browser/media/capture/desktop_capture_util_mac.h`.
    
    Update test to have a content::BrowserTaskEnvironment to avoid DCHECK
    failure when calling `SendMessageToNativeLog`.
    
    Test: --enable-logging=stderr --v=1 2>&1 | grep AudioCaptureId
    Bug: 507803904
    Change-Id: I2042838ca957199e08a567fa97c90355abeaf074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837560
    Commit-Queue: Olov Brändström <brandstrom@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629874}

--- content/browser/media/capture/desktop_capture_util_mac_unittest.mm
+++ content/browser/media/capture/desktop_capture_util_mac_unittest.mm
@@ -206,7 +206,8 @@ struct BundleIdTestParams {
 };
 
 class DesktopCaptureUtilBrowserTest
-    : public testing::TestWithParam<BundleIdTestParams> {};
+    : public DesktopCaptureUtilTest,
+      public testing::WithParamInterface<BundleIdTestParams> {};
 
 TEST_P(DesktopCaptureUtilBrowserTest, TruncatesCorrectly) {
   const BundleIdTestParams& params = GetParam();
@@ -253,8 +254,9 @@ struct PwaTestParams {
   std::string expected_id;
 };
 
-class DesktopCaptureUtilPwaTest : public testing::TestWithParam<PwaTestParams> {
-};
+class DesktopCaptureUtilPwaTest
+    : public DesktopCaptureUtilTest,
+      public testing::WithParamInterface<PwaTestParams> {};
 
 TEST_P(DesktopCaptureUtilPwaTest, ResolvesToBrowser) {
   const PwaTestParams& params = GetParam();
