6bafc616326fd2fad34a1bd119606203397946b2
Sam Goto <goto@chromium.org>
2026-05-13 15:17:56 -0700

    [evp] Rename nonce -> challenge
    This CL renames the <input> attribute from "nonce" to "challenge", as
    it seems a bit more appropriate (in that it describes its purpose
    more than what it is).
    
    Bug: 512561617
    Change-Id: I006e50b4344e906d7957e859311aa0e1e4c9373b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843237
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630264}

--- components/autofill/content/browser/email_verifier_delegate_unittest.cc
+++ components/autofill/content/browser/email_verifier_delegate_unittest.cc
@@ -102,7 +102,7 @@ class EmailVerifierDelegateTest : public content::RenderViewHostTestHarness {
              {
                  {.label = u"Email",
                   .name = u"email",
-                  .nonce = u"test_nonce",
+                  .challenge = u"test_nonce",
                   .value = u"Triggering field (filled)",
                   .form_control_type = FormControlType::kInputEmail},
              },
@@ -239,7 +239,7 @@ TEST_F(EmailVerifierDelegateTest, NotEmailField) {
       test::GetFormData({.fields = {
                              {.label = u"Email",
                               .name = u"email",
-                              .nonce = u"test_nonce",
+                              .challenge = u"test_nonce",
                               .value = u"Triggering field (filled)"},
                          }});
 
