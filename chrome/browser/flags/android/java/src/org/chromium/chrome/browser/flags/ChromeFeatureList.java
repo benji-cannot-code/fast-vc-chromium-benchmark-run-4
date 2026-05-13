9577e5115df331556db3732b2125883d448fd84e
Anand Ravi <anandrv@google.com>
2026-05-12 19:32:06 -0700

    Add GMSCoreBindServiceOptimization to field trial testing config
    Bug: 493947077
    Change-Id: Ieab9c3d7fba8a0ad9cd50b7176671e4e1f8c45c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842253
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629721}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1008,7 +1008,8 @@ public abstract class ChromeFeatureList {
                     /* defaultValueInTests= */ true);
     public static final CachedFlag sGlic = newCachedFlag(GLIC, false);
     public static final CachedFlag sGmscoreBindServiceOptimization =
-            newCachedFlag(GMSCORE_BIND_SERVICE_OPTIMIZATION, false);
+            newCachedFlag(
+                    GMSCORE_BIND_SERVICE_OPTIMIZATION, false, /* defaultValueInTests= */ true);
     public static final CachedFlag sGridTabSwitcherSurfaceColorUpdate =
             newCachedFlag(
                     GRID_TAB_SWITCHER_SURFACE_COLOR_UPDATE,
