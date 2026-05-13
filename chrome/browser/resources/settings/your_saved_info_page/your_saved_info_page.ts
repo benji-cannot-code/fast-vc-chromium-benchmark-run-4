34c1510eed4e4d34bb79fa2812088dad81466faa
dpapad <dpapad@chromium.org>
2026-05-13 15:21:52 -0700

    Settings: Fix runtime error caused by your_save_info_page_index.ts
    Regressed at crrev.com/c/7715731. Added test that would have caught the
    regression.
    
    Fixed: 506789468
    Change-Id: I0a6673d85756aee71ba4d094bbb61414e16a5273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846334
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630268}

--- chrome/browser/resources/settings/your_saved_info_page/your_saved_info_page.ts
+++ chrome/browser/resources/settings/your_saved_info_page/your_saved_info_page.ts
@@ -469,7 +469,9 @@ export class SettingsYourSavedInfoPageElement extends
     }
     const control =
         this.shadowRoot!.querySelector<HTMLElement>(`#${triggerId}`);
-    assert(control);
+    assert(
+        control,
+        `Failed to find associated control for child '${childViewId}'`);
     return control;
   }
 
