c4c92e5c6a77488581bf7d6a2780c678e03e4e6d
Vasilii Sukhanov <vasilii@chromium.org>
2026-05-13 06:04:44 -0700

    Disable WebUIUrlNoConsoleErrorsTest.NoConsoleErrors/chrome___signin_internals on Linux.
    Bug: 512775747
    Change-Id: Id11a31272d2dd4002b76f19f8df90c32e3e70f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842854
    Auto-Submit: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629911}

--- chrome/browser/ui/webui/webui_url_browsertest.cc
+++ chrome/browser/ui/webui/webui_url_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/webui/web_ui_all_urls_browser_test.h"
 #include "chrome/browser/ui/webui/webui_urls_for_test.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -76,6 +77,11 @@ static const char* const kConsoleErrorUrls[] = {
     "chrome://smb-credentials-dialog",
 #else
     "chrome://signin-email-confirmation",
+#if BUILDFLAG(IS_LINUX)
+    // TODO(crbug.com/512775747): Fix crash error when navigating to
+    // signin-internals.
+    "chrome://signin-internals",
+#endif
 #endif
 };
 
