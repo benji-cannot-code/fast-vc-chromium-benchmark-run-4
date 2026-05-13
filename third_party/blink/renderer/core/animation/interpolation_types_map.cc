8f1892fd78b4f38b946a78670f9566c5329d880a
Yanling Wang <yanlingwang@microsoft.com>
2026-05-13 14:13:55 -0700

    [Masonry] Support animation for flow-tolerance
    This CL adds animation support for flow-tolerance. It animates "as
    length" - length/percentage values interpolate smoothly, while keywords
    (normal, infinite) use discrete interpolation.
    
    Spec: https://www.w3.org/TR/css-grid-3/#placement-tolerance
    
    Animation demo: https://imgur.com/a/W80FHTe
    
    Bug: 343257585
    Change-Id: Ib865e0f25479d3fd63539ad00b46c1ccfa5aa9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828102
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Celeste Pan <celestepan@microsoft.com>
    Commit-Queue: Yanling Wang <yanlingwang@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1630210}

--- third_party/blink/renderer/core/animation/interpolation_types_map.cc
+++ third_party/blink/renderer/core/animation/interpolation_types_map.cc
@@ -148,6 +148,7 @@ const InterpolationTypes* InterpolationTypesMap::Get(
       case CSSPropertyID::kCx:
       case CSSPropertyID::kCy:
       case CSSPropertyID::kFlexBasis:
+      case CSSPropertyID::kFlowTolerance:
       case CSSPropertyID::kHeight:
       case CSSPropertyID::kLeft:
       case CSSPropertyID::kLetterSpacing:
