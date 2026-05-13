fa66f45d7c496ebcc8f53549f7bca6ace8da3268
Hitarth Kothari <hitarthkothari@google.com>
2026-05-13 07:02:12 -0700

    [TabBottomSheet] Collapse sheet when TabModalDialog is shown
    Bug: 491514698
    Change-Id: I61df60e65f67d868047792cb12055ac1b4660ef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7840953
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629939}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
@@ -34,6 +34,9 @@ import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.KeyboardVisibilityDelegate.KeyboardVisibilityListener;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.modaldialog.ModalDialogManager;
+import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogManagerObserver;
+import org.chromium.ui.modaldialog.ModalDialogManager.ModalDialogType;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
@@ -149,6 +152,8 @@ public class TabBottomSheetCoordinator {
     private boolean mInitialContainerSizeChanged;
     private boolean mCanNotBeSuppressed;
     private @Nullable KeyboardVisibilityListener mKeyboardVisibilityListener;
+    private @Nullable ModalDialogManager mObservedModalDialogManager;
+    private @Nullable ModalDialogManagerObserver mModalDialogManagerObserver;
 
     /**
      * @param context The context to use for creating views.
@@ -256,6 +261,15 @@ public class TabBottomSheetCoordinator {
                         .addKeyboardVisibilityListener(mKeyboardVisibilityListener);
             }
 
+            if (mModalDialogManagerObserver == null) {
+                ModalDialogManager modalDialogManager = mWindowAndroid.getModalDialogManager();
+                if (modalDialogManager != null) {
+                    mModalDialogManagerObserver = buildModalDialogManagerObserver();
+                    modalDialogManager.addObserver(mModalDialogManagerObserver);
+                    mObservedModalDialogManager = modalDialogManager;
+                }
+            }
+
             mIsShowingTabBottomSheet = true;
             return true;
         } else {
@@ -342,6 +356,14 @@ public class TabBottomSheetCoordinator {
                     .removeKeyboardVisibilityListener(mKeyboardVisibilityListener);
             mKeyboardVisibilityListener = null;
         }
+
+        if (mObservedModalDialogManager != null) {
+            if (mModalDialogManagerObserver != null) {
+                mObservedModalDialogManager.removeObserver(mModalDialogManagerObserver);
+            }
+            mObservedModalDialogManager = null;
+        }
+        mModalDialogManagerObserver = null;
 
         if (mSheetContent != null) {
             mSheetContent.destroy();
@@ -454,6 +476,18 @@ public class TabBottomSheetCoordinator {
             @Override
             public void onInsetAnimationEnd() {
                 mCoBrowseViews.setIgnoreClearFocus(/* ignoreClearFocus= */ false);
+            }
+        };
+    }
+
+    private ModalDialogManagerObserver buildModalDialogManagerObserver() {
+        return new ModalDialogManagerObserver() {
+            @Override
+            public void onDialogShown(View dialogView) {
+                if (mObservedModalDialogManager != null
+                        && mObservedModalDialogManager.getCurrentType() == ModalDialogType.TAB) {
+                    collapseSheet();
+                }
             }
         };
     }
