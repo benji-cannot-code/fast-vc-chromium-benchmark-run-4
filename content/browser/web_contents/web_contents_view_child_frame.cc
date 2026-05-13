f9b5afe9881e582a86f7ad25e7d229f67623680a
Keren Zhu <kerenzhu@chromium.org>
2026-05-13 15:26:06 -0700

    surface-embed: Allow focusing in WebContentsChildFrame
    WebContentsViewChildFrame is used by both GuestContents and
    SurfaceEmbed. There are CHECKs in the focus path that allows only
    GuestContents. They are triggered when a new tab is opened by clicking
    links on the New Tab Page. This CL removes these CHECKs so that a
    SurfaceEmbed WebContents can be focused. The GuestView WebContents
    should not use this path, so early exit for them by testing if IsGuest()
    is true.
    
    Fixed: 509055766
    Change-Id: Ie23884b7e27f9895980a6efbc5b9131bce035fd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810703
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630272}

--- content/browser/web_contents/web_contents_view_child_frame.cc
+++ content/browser/web_contents/web_contents_view_child_frame.cc
@@ -118,8 +118,10 @@ gfx::Rect WebContentsViewChildFrame::GetContainerBounds() const {
 }
 
 void WebContentsViewChildFrame::SetInitialFocus() {
-  // This should only be reachable in Webium, not other uses.
-  CHECK(base::FeatureList::IsEnabled(features::kAttachUnownedInnerWebContents));
+  // Ignore focus requests for GuestView WebContents.
+  if (web_contents_->IsGuest()) {
+    return;
+  }
 
   if (web_contents_->FocusLocationBarByDefault()) {
     web_contents_->SetFocusToLocationBar();
@@ -201,7 +203,8 @@ void WebContentsViewChildFrame::RestoreFocus() {
 }
 
 void WebContentsViewChildFrame::Focus() {
-  if (!base::FeatureList::IsEnabled(features::kAttachUnownedInnerWebContents)) {
+  // Ignore focus requests for GuestView WebContents.
+  if (web_contents_->IsGuest()) {
     return;
   }
 
