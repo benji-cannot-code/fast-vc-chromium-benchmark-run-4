2a4e065c4442c1747e496845de7d155722ca6fb9
Sophie Chang <sophiechang@chromium.org>
2026-05-13 02:55:15 -0700

    Update CHECK so that contextual cueing can use private AI
    Bug: 504481354
    Change-Id: I73ae541e1a55069f84fd57ca3412f53081325dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843099
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629857}

--- components/optimization_guide/core/model_execution/model_execution_manager.cc
+++ components/optimization_guide/core/model_execution/model_execution_manager.cc
@@ -119,6 +119,16 @@ size_t GetMaxParallelFeatureExecutions(ModelBasedCapabilityKey feature) {
   }
 }
 
+bool IsEligibleForPrivateAI(ModelBasedCapabilityKey feature) {
+  switch (feature) {
+    case ModelBasedCapabilityKey::kZeroStateSuggestions:
+    case ModelBasedCapabilityKey::kContextualCueing:
+      return true;
+    default:
+      return false;
+  }
+}
+
 }  // namespace
 
 using ModelExecutionError =
@@ -220,10 +230,8 @@ void ModelExecutionManager::ExecuteModel(
     fetchers_for_feature.erase(fetchers_for_feature.begin());
   }
   FetcherId fetcher_id = next_model_execution_fetcher_id++;
-  // Currently only ZSS is supported by PrivateAI. Update or remove this CHECK
-  // when other features are supported too.
   CHECK(service_type != ModelExecutionServiceType::kPrivateAi ||
-        feature == ModelBasedCapabilityKey::kZeroStateSuggestions)
+        IsEligibleForPrivateAI(feature))
       << feature;
   base::TimeTicks start_time = base::TimeTicks::Now();
   auto fetcher = CreateModelExecutionFetcher(service_type);
