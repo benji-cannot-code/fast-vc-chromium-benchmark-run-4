2ad03edd6f40107344929a25ce24ac84173aaf16
Sam Goto <goto@chromium.org>
2026-05-12 19:19:53 -0700

    [evp] Remove the EmailVerificationEvent class
    Change-Id: Ifb087d1fb779040338bd333dd0f7c49e05055601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814355
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629716}

--- third_party/blink/renderer/core/exported/web_form_control_element.cc
+++ third_party/blink/renderer/core/exported/web_form_control_element.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/input/focus_type.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
-#include "third_party/blink/renderer/core/events/email_verified_event.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element_with_state.h"
