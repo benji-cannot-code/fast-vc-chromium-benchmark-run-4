65382894d6bf76a0e7906f4121a4652185afcb3f
Brandon Wylie <wylieb@google.com>
2026-05-12 17:24:55 -0700

    [aim] Respond to orientation changes for popup pos/size
    Bug: 512200923
    Change-Id: If3e659c7331ed1fbd70bf2fc0ff7cd57b17d60a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7836363
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629652}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxCoordinator.java
@@ -194,7 +194,6 @@ public class FuseboxCoordinator implements TemplateUrlServiceObserver {
         mBottomSheetRectProvider = new BottomSheetRectProvider(activity, mParent);
         var dynamicRectProvider =
                 new DynamicRectProvider(viewRectProvider, mBottomSheetRectProvider);
-
         mViewportRectProvider = new ViewportRectProvider(mContext);
         var contextButton = mParent.findViewById(R.id.location_bar_attachments_add);
 
@@ -492,6 +491,7 @@ public class FuseboxCoordinator implements TemplateUrlServiceObserver {
         @Override
         public void onConfigurationChanged(Configuration configuration) {
             updateRect();
+            notifyRectChanged();
         }
 
         private void updateRect() {
