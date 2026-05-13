a5b7d80e9cc6a6311a7d20cb0a65cabf2f209e8d
Fahad Mansoor <fahadmansoor@google.com>
2026-05-13 12:31:32 -0700

    LPP: Fix double-click and skip RemoveLocalAuthFactors for PIN users
    - Prevent multiple clicks on the Done button in the
    RemoveLocalAuthFactorsScreen as that causes the crash in the bug
    - Skip the screen for managed users with a PIN as a secondary auth
    factor which prevent the screen from being shown unnecessarily for
    existing users with a PIN
    
    Bug: 512009378
    Test: browser_test *RemoveLocalAuthFactors*
    Change-Id: I7e0360e2a34071aa12c1d55f88fad8f363752ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7840578
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Fahad Mansoor <fahadmansoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630131}

--- chrome/browser/resources/chromeos/login/screens/osauth/remove_local_auth_factors.ts
+++ chrome/browser/resources/chromeos/login/screens/osauth/remove_local_auth_factors.ts
@@ -34,6 +34,7 @@ enum UserAction {
 enum RemoveLocalAuthFactorsScreenState {
   SUCCESS = 'success',
   PROGRESS = 'progress',
+  DONE = 'done',
 }
 
 class RemoveLocalAuthFactorsScreen extends
@@ -49,7 +50,7 @@ class RemoveLocalAuthFactorsScreen extends
   static get properties(): PolymerElementProperties {
     return {
       // User email address.
-      email: {type: String, value: ''}
+      email: {type: String, value: ''},
     };
   }
 
@@ -82,10 +83,19 @@ class RemoveLocalAuthFactorsScreen extends
     this.initializeLoginScreen('RemoveLocalAuthFactorsScreen');
   }
 
+  private isSuccessStep(uiStep: RemoveLocalAuthFactorsScreenState) {
+    return uiStep === RemoveLocalAuthFactorsScreenState.SUCCESS;
+  }
+
   /**
-   * Next button click handler.
+   * Done button click handler, Done button is only shown when the screen is in
+   * SUCCESS state.
    */
   private onDoneClicked(): void {
+    if (this.uiStep !== RemoveLocalAuthFactorsScreenState.SUCCESS) {
+      return;
+    }
+    this.setUIStep(RemoveLocalAuthFactorsScreenState.DONE);
     this.userActed(UserAction.DONE);
   }
 
