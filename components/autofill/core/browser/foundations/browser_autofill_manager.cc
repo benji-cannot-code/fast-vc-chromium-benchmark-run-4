91de0af82cde1572c9ca61697db78cc49c2b4afa
Karol Sygiet <sygiet@google.com>
2026-05-13 04:23:54 -0700

    Reland: Centralize AutofillAI authentication and server-side fetching logic.
    The original CL got reverted due to failures on ios-simulator-full-configs bot. These were fixed by addressing a discrepancy between the device authenticator implementations, where the production IOSDeviceAuthenticator had a CHECK ensuring that there's an authentication before it is cancelled (which isn't present on other platforms).
    
    
    > Centralize AutofillAI authentication and server-side fetching logic.
    > This CL moves the orchestration logic for AutofillAI filling from
    > AutofillExternalDelegate to a new AutofillAiAccessManager class.
    >
    > This refactoring simplifies AutofillExternalDelegate and encapsulates
    > the complex requirements for filling in a single place.
    >
    > A followup CL will utilize the new helpers when using AutofillAi's
    > EntityTypes in a new suggestion payload.
    
    Bug: 492978632
    Change-Id: Ice323f49f05bd83e8b9ccc82a4f70c338ee06cd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842132
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629879}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/filling/addresses/field_filling_address_util.h"
+#include "components/autofill/core/browser/filling/autofill_ai/autofill_ai_access_manager.h"
 #include "components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util.h"
 #include "components/autofill/core/browser/filling/field_filling_skip_reason.h"
 #include "components/autofill/core/browser/filling/filling_product.h"
@@ -815,6 +816,8 @@ BrowserAutofillManager::MetricsState::~MetricsState() {
 
 BrowserAutofillManager::BrowserAutofillManager(AutofillDriver* driver)
     : AutofillManager(driver),
+      autofill_ai_access_manager_(
+          std::make_unique<AutofillAiAccessManager>(this)),
       otp_manager_(
           new OtpManagerImpl(*this, client().GetOneTimeTokenService())),
       at_memory_manager_(std::make_unique<AtMemoryManager>(this)),
@@ -850,6 +853,10 @@ BrowserAutofillManager::GetCreditCardAccessManager() const {
 
 AtMemoryManager& BrowserAutofillManager::GetAtMemoryManager() {
   return *at_memory_manager_;
+}
+
+AutofillAiAccessManager& BrowserAutofillManager::GetAutofillAiAccessManager() {
+  return *autofill_ai_access_manager_;
 }
 
 void BrowserAutofillManager::TriggerAtMemorySuggestions(
@@ -2691,6 +2698,7 @@ void BrowserAutofillManager::Reset() {
   bnpl_manager_.reset();
 
   credit_card_access_manager_.reset();
+  autofill_ai_access_manager_->Reset();
   // Forget stored data (e.g. active subscriptions and pending callbacks) after
   // a navigation.
   otp_manager_ = std::make_unique<OtpManagerImpl>(
