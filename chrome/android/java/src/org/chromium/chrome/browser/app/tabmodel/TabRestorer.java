536f41152a1892f117c03190628f41a793d1a636
Fiaz Muhammad <mfiaz@google.com>
2026-05-13 11:00:41 -0700

    [Persist] Support reparenting during restoration
    Avoids memory leaks as a result of unused TabStates when reparenting.
    
    Bug: 505027629
    Change-Id: Id7f3b874c90d1291a428a14beeff0eefb39bdd6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841756
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630089}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabRestorer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/TabRestorer.java
@@ -192,7 +192,7 @@ class TabRestorer {
 
         int tabCount = mTabModelSelector.getModel(mIncognito).getCount();
         Tab tab =
-                resolveTab(
+                maybeRestoreTab(
                         tabState,
                         loadedTabState.tabId,
                         tabCount,
@@ -436,7 +436,8 @@ class TabRestorer {
         mTabIdsToIgnore.add(tabId);
         assert mCachedRestoredActiveTabId == null || tabId != mCachedRestoredActiveTabId;
 
-        Tab tab = resolveTab(loadedTabState.tabState, tabId, index, isActive, mIsFromRecreating);
+        Tab tab =
+                maybeRestoreTab(loadedTabState.tabState, tabId, index, isActive, mIsFromRecreating);
         if (tab == null) {
             destroyLoadedTabState(loadedTabState);
             return;
@@ -486,21 +487,25 @@ class TabRestorer {
         }
     }
 
-    private @Nullable Tab resolveTab(
+    /**
+     * Restores a tab from the given {@link TabState}. Returns null if the WebContentsState, if
+     * present, was not used to create the tab.
+     */
+    private @Nullable Tab maybeRestoreTab(
             TabState tabState, int tabId, int index, boolean isActiveTab, boolean isRecreating) {
+        boolean isReparenting = mTabCreator.isReparenting(tabId);
+        if (isReparenting) {
+            createTabFromState(tabState, tabId, index);
+            // Reparenting will not use the TabState to create the tab.
+            return null;
+        }
+
         if (!isActiveTab) {
-            if (isRecreating) {
-                // If activity is recreating, we restore non-active NTPs.
-                boolean isNtp = tabState.url != null && UrlUtilities.isNtpUrl(tabState.url);
-                if (isNtp) {
-                    if (tabState.contentsState != null) {
-                        return createFrozenTab(tabState, tabId, index);
-                    } else {
-                        return createTabWithoutContentsState(assumeNonNull(tabState.url), index);
-                    }
-                }
-            }
-            if (shouldSkipTab(tabState)) {
+            // If activity is recreating, we restore non-active NTPs.
+            boolean isNtp = tabState.url != null && UrlUtilities.isNtpUrl(tabState.url);
+            if (isRecreating && isNtp) {
+                return createTabFromState(tabState, tabId, index);
+            } else if (shouldSkipTab(tabState)) {
                 mRestoreFilteredTabCount++;
                 return null;
             }
@@ -521,6 +526,14 @@ class TabRestorer {
             mDelegate.onActiveTabRestored(mIncognito);
         }
         return tab;
+    }
+
+    private @Nullable Tab createTabFromState(TabState tabState, int tabId, int index) {
+        if (tabState.contentsState != null) {
+            return createFrozenTab(tabState, tabId, index);
+        } else {
+            return createTabWithoutContentsState(assumeNonNull(tabState.url), index);
+        }
     }
 
     /** Creates a Tab from the given URL when tabState.contentsState is null. */
