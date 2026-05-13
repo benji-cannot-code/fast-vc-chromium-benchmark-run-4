2f360d78e5027a9f61e9512d348d68e2595aaf67
Andrew Moylan <amoylan@google.com>
2026-05-13 09:28:53 -0700

    Revert "Clean up expired enable-chromeos-soda-conch flag"
    This reverts commit a8a3177f70b19ca19c508e8fcffb7aa163bbaeaf.
    
    Reason for revert: b/512571805
    
    Original change's description:
    > Clean up expired enable-chromeos-soda-conch flag
    >
    > The underlying feature kCrosSodaConchLanguages was enabled by default in
    > M130.
    >
    > Fixed: b:500174836
    > Change-Id: Ibba274b522b49e53754f0964e6ac1bf95242cac2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828562
    > Commit-Queue: Andrew Moylan <amoylan@google.com>
    > Reviewed-by: Jennifer Ling <hsuanling@google.com>
    > Reviewed-by: Evan Liu <evliu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1629155}
    
    Bug: b:500174836
    Change-Id: If804f423161d5a703a94b3146a9377bf48f9a8f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7845374
    Reviewed-by: Jennifer Ling <hsuanling@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Auto-Submit: Andrew Moylan <amoylan@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630024}

--- components/soda/soda_features.h
+++ components/soda/soda_features.h
@@ -13,6 +13,8 @@ namespace speech {
 #if BUILDFLAG(IS_CHROMEOS)
 COMPONENT_EXPORT(SODA_INSTALLER)
 BASE_DECLARE_FEATURE(kFeatureManagementCrosSodaConchLanguages);
+COMPONENT_EXPORT(SODA_INSTALLER)
+BASE_DECLARE_FEATURE(kCrosSodaConchLanguages);
 #endif
 }  // namespace speech
 
