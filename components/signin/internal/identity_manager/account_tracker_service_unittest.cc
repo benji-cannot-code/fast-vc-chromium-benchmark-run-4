567018cc809613ac05f6486aa74ab19fbb4ee361
Tanmoy Mollik <triploblastic@google.com>
2026-05-13 00:37:29 -0700

    [google_apis] Use factory method to create GoogleServiceAuthError
    Bug: 487926694
    Change-Id: I6cde5992bce480e96956d2298678126c54f2b8fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7840239
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629813}

--- components/signin/internal/identity_manager/account_tracker_service_unittest.cc
+++ components/signin/internal/identity_manager/account_tracker_service_unittest.cc
@@ -324,8 +324,8 @@ class AccountTrackerServiceTest : public testing::Test {
   void SimulateIssueAccessTokenPersistentError(AccountKey account_key) {
     fake_oauth2_token_service_.IssueErrorForAllPendingRequestsForAccount(
         AccountKeyToAccountId(account_key),
-        GoogleServiceAuthError(
-            GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS));
+        GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
+            GoogleServiceAuthError::InvalidGaiaCredentialsReason::UNKNOWN));
   }
 
   std::string GenerateValidTokenInfoResponse(AccountKey account_key) {
