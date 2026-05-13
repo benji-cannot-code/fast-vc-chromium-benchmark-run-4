d2a1cde8481040349ff370aa197164fd43d06730
Marc Treib <treib@chromium.org>
2026-05-13 09:50:21 -0700

    ScopedUserPrefUpdate: Avoid dangling pointer
    Before this CL, ScopedUserPrefUpdateBase::Notify() called
    ReportUserPrefValueChanged() *before* resetting the internal `value_`
    pointer to null. This meant that, if the pref got cleared as part of the
    notification, `value_` would (very briefly) be dangling. This wasn't a
    big problem in practice, but could trip up the DanglingPtr detector.
    This CL fixes this by swapping the two lines, and also adds a unit test
    for this case.
    
    Bug: none
    Change-Id: Ia681598fdf91a6af11503de4514b48cca8eda060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843618
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630037}

--- components/prefs/scoped_user_pref_update_unittest.cc
+++ components/prefs/scoped_user_pref_update_unittest.cc
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/prefs/scoped_user_pref_update.h"
+
 #include "components/prefs/mock_pref_change_callback.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_registry_simple.h"
-#include "components/prefs/scoped_user_pref_update.h"
 #include "components/prefs/testing_pref_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -138,4 +139,19 @@ TEST_F(ScopedUserPrefUpdateTest, ScopedListPrefUpdateWithDefaults) {
   ScopedListPrefUpdate update(&prefs_, pref_name);
   update->Append("thirdvalue");
   EXPECT_EQ(3u, prefs_.GetList(pref_name).size());
+}
+
+TEST_F(ScopedUserPrefUpdateTest, ScopedDictPrefUpdateClearsPrefOnNotify) {
+  // Set up a callback to clear the pref in the pref-changed notification. This
+  // should not result in a dangling pointer within ScopedUserPrefUpdate.
+  EXPECT_CALL(dict_observer_, OnPreferenceChanged(std::string(kDictPref)))
+      .WillOnce([this](const std::string& pref_name) {
+        prefs_.ClearPref(pref_name);
+      })
+      .WillRepeatedly(testing::Return());
+
+  {
+    ScopedDictPrefUpdate update(&prefs_, kDictPref);
+    update->Set(kKey, kValue);
+  }
 }
