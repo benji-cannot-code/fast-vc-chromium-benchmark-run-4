714aa4a6a807a68c1771fb2a53ef6808cac5483f
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-05-13 16:23:45 -0700

    [Extensions] Include Manifest::TYPE_USER_SCRIPT in mv2 checker
    Extensions of TYPE_USER_SCRIPT are largely treated as "extensions"
    throughout Chrome -- they have access to extension APIs, are considered
    extensions for feature availability, are shown in the
    chrome://extensions page as extensions, etc.
    
    Include them in the MV2DeprecationImpactChecker to ensure MV2 user
    scripts are also disabled.
    
    Very few* users have any extensions installed of TYPE_USER_SCRIPT, so
    this shouldn't affect significant amounts of users, and any that are
    affected are using deprecated extensions (thus, disabling them is
    working as intended).
    
    Note: This is not to be confused with *user script managers*, a
    class of extension that leverages the browser.userScripts API.
    
    *Internal stats only, sorry.
    
    Bug: 498864176
    Change-Id: I7310fa1f1411290dac935fd6d899bd72daafdcc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846377
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630304}

--- chrome/browser/extensions/extension_management.cc
+++ chrome/browser/extensions/extension_management.cc
@@ -376,10 +376,12 @@ bool ExtensionManagement::IsAllowedManifestVersion(
           extensions_features::kExtensionsManifestV3Only) ||
       manifest_version >= 3;
 
-  // Manifest version policy only supports normal extensions and Chrome OS login
-  // screen extension.
+  // Manifest version policy only supports normal extensions, Chrome OS login
+  // screen extensions, and user scripts (which are largely treated as
+  // extensions).
   if (manifest_type != Manifest::Type::TYPE_EXTENSION &&
-      manifest_type != Manifest::Type::TYPE_LOGIN_SCREEN_EXTENSION) {
+      manifest_type != Manifest::Type::TYPE_LOGIN_SCREEN_EXTENSION &&
+      manifest_type != Manifest::Type::TYPE_USER_SCRIPT) {
     return enabled_by_default;
   }
   switch (global_settings_->manifest_v2_setting) {
@@ -412,7 +414,8 @@ bool ExtensionManagement::IsExemptFromMV2DeprecationByPolicy(
     return false;
   }
   if (manifest_type != Manifest::Type::TYPE_EXTENSION &&
-      manifest_type != Manifest::Type::TYPE_LOGIN_SCREEN_EXTENSION) {
+      manifest_type != Manifest::Type::TYPE_LOGIN_SCREEN_EXTENSION &&
+      manifest_type != Manifest::Type::TYPE_USER_SCRIPT) {
     return false;
   }
 
