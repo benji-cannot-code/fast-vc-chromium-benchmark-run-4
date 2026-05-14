b4fe97c59b41675a18da854a5eb08106f2e42bd7
Chris Thompson <cthomp@chromium.org>
2026-05-13 20:36:52 -0700

    Run HttpsUpgradesBrowserTest suite with new dialog UI
    This enables the HttpsFirstDialogUi feature in HttpsUpgradesBrowserTest,
    while adding a new variation to maintain coverage for the old
    interstitial UI.
    
    This is done by making some of the helper code (e.g.,
    IsShowingHttpsFirstModeIntersitital(), ProceedThroughInterstitial(),
    DontProceedThroughInterstitial()) agnostic to whether the warning is
    using the Dialog UI or the interstitial UI. Test methods are added to
    AskBeforeHttpDialogController to support these test helpers.
    
    This also updates PageInfoBubbleViewBrowserTest and
    CaptivePortalBrowserTest to use the interstitial helpers, and merges the
    PageInfoBubbleViewHttpsUpgradesBrowserTest tests into the main
    PageInfoBubbleViewBrowserTest suite.
    
    Bug: 510847674
    Change-Id: I69bbf7b42fb168297e802803b6fa90e1f1ee336a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7832996
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630429}

--- chrome/browser/ssl/ask_before_http_dialog_controller.h
+++ chrome/browser/ssl/ask_before_http_dialog_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
+#include "components/security_interstitials/core/https_only_mode_metrics.h"
 #include "components/security_interstitials/core/metrics_helper.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -65,6 +66,13 @@ class AskBeforeHttpDialogController : public content::WebContentsObserver {
   // Closes the dialog programmatically when not triggered by user interaction.
   void CloseDialog();
 
+  // Test support:
+  void ProceedForTesting();
+  void CancelForTesting();
+  security_interstitials::https_only_mode::InterstitialReason
+  GetInterstitialReasonForTesting() const;
+  void ClickLearnMoreForTesting();
+
 #if !BUILDFLAG(IS_ANDROID)
   // Closes the dialog widget, if one is open.
   void CloseDialogWidget(views::Widget::ClosedReason reason);
@@ -85,6 +93,8 @@ class AskBeforeHttpDialogController : public content::WebContentsObserver {
   GURL request_url_;
   bool is_suspended_ = false;
   std::unique_ptr<security_interstitials::MetricsHelper> metrics_helper_;
+  security_interstitials::https_only_mode::InterstitialReason warning_reason_ =
+      security_interstitials::https_only_mode::InterstitialReason::kUnknown;
 
 #if !BUILDFLAG(IS_ANDROID)
   // Pointer to the widget that contains the current open dialog, if any.
