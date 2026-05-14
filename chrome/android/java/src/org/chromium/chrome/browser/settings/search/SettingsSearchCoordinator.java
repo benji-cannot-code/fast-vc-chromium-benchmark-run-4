4ea944a888e7744bd52f2a64da3e9a46a09a9bbf
Jinsuk Kim <jinsukkim@chromium.org>
2026-05-14 11:41:32 -0700

    [Multi-column Settings] Always reset breacrumb when exiting browse state
    The title breadcrumb should be reset regardless of the column mode,
    as opposed to doing it only in multi-column mode. This fixes a bug
    when the state is exited in single-column mode after the device is
    rotated with the breadcrumb state modified in multi-column mode.
    
    Fidxed: 513072372
    Change-Id: I6b008bda86853867d8077f5220e8c3c2ef716630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849402
    Commit-Queue: Moe Adel <adelm@google.com>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630703}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/search/SettingsSearchCoordinator.java
@@ -818,7 +818,7 @@ public class SettingsSearchCoordinator
 
         setFragmentState(FS_SETTINGS);
         mBackActionCallback.setEnabled(false);
-        if (mUseMultiColumn) mUpdateFirstVisibleTitle.onResult(0);
+        mUpdateFirstVisibleTitle.onResult(0);
 
         updateHelpMenuVisibility();
         adjustTalkbackTraversalOrder(searchBox);
