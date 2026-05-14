7acd26126b2591dab75a3348322c4e28ad63f4e3
Josh Karlin <jkarlin@chromium.org>
2026-05-14 16:58:58 -0700

    [SubresourceFilter] Support matching of style rules
    Add StyleRuleMatcher to support lookups of style rules, in a similar
    fashion to url rules.
    
    Bug: 500722400
    Change-Id: Ibac45b9dc7b4ea8a1ae8efbe44b306448f968db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829097
    Reviewed-by: Charles Harrison <csharrison@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630917}

--- components/subresource_filter/core/common/indexed_ruleset.cc
+++ components/subresource_filter/core/common/indexed_ruleset.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/trace_event/trace_event.h"
 #include "components/subresource_filter/core/common/first_party_origin.h"
+#include "components/subresource_filter/core/common/style_rule_matcher.h"
 #include "components/url_pattern_index/url_pattern_index.h"
 #include "third_party/rapidhash/rapidhash.h"
 #include "url/gurl.h"
@@ -170,7 +171,8 @@ IndexedRulesetMatcher::IndexedRulesetMatcher(base::span<const uint8_t> buffer)
     : root_(flat::GetIndexedRuleset(buffer.data())),
       blocklist_(root_->blocklist_index()),
       allowlist_(root_->allowlist_index()),
-      deactivation_(root_->deactivation_index()) {}
+      deactivation_(root_->deactivation_index()),
+      style_rule_matcher_(root_->style_rule_index()) {}
 
 IndexedRulesetMatcher::~IndexedRulesetMatcher() = default;
 
@@ -251,6 +253,34 @@ const url_pattern_index::flat::UrlRule* IndexedRulesetMatcher::MatchedUrlRule(
   }
   auto* allowlist_rule = find_match(allowlist_);
   return allowlist_rule ? allowlist_rule : blocklist_rule;
+}
+
+bool IndexedRulesetMatcher::MaybeHasStyleRule(uint32_t hash) const {
+  return style_rule_matcher_.MaybeHasStyleRule(hash);
+}
+
+void IndexedRulesetMatcher::GetDomainSelectors(
+    const url::Origin& document_origin,
+    std::vector<std::string_view>& out_selectors) const {
+  style_rule_matcher_.GetDomainSelectors(document_origin, out_selectors);
+}
+
+void IndexedRulesetMatcher::GetSelectorsByClass(
+    const url::Origin& document_origin,
+    std::string_view class_name,
+    uint32_t hash,
+    std::vector<std::string_view>& out_selectors) const {
+  style_rule_matcher_.GetSelectorsByClass(document_origin, class_name, hash,
+                                          out_selectors);
+}
+
+void IndexedRulesetMatcher::GetSelectorsById(
+    const url::Origin& document_origin,
+    std::string_view id_name,
+    uint32_t hash,
+    std::vector<std::string_view>& out_selectors) const {
+  style_rule_matcher_.GetSelectorsById(document_origin, id_name, hash,
+                                       out_selectors);
 }
 
 }  // namespace subresource_filter
