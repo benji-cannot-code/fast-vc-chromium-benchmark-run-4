5b9788a5478cb4d257e2bb4fcb6bc85f9a4f4f8f
Tom Anderson <thomasanderson@chromium.org>
2026-05-14 10:33:44 -0700

    [X11] Fix Use-After-Free in GeometryCache::GetBoundsPx()
    Synchronous X11 dispatching in GetBoundsPx() can trigger a callback
    chain that destroys the GeometryCache instance. This CL adds WeakPtr
    checks after each synchronous call to protect against this.
    
    Fixed: 513160681
    Test: GeometryCacheTest.DestroyInCallback
    Change-Id: I6fbce5def4a521eee2f4e04a8a28311c15471b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849103
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630691}

--- ui/gfx/x/geometry_cache.cc
+++ ui/gfx/x/geometry_cache.cc
@@ -35,12 +35,19 @@ GeometryCache::GeometryCache(Connection* connection,
 GeometryCache::~GeometryCache() = default;
 
 gfx::Rect GeometryCache::GetBoundsPx() {
+  auto weak_this = weak_ptr_factory_.GetWeakPtr();
   if (!have_parent_) {
     parent_future_.DispatchNow();
+    if (!weak_this) {
+      return {};
+    }
   }
   CHECK(have_parent_);
   if (!have_geometry_) {
     geometry_future_.DispatchNow();
+    if (!weak_this) {
+      return {};
+    }
   }
   CHECK(have_geometry_);
 
@@ -48,6 +55,9 @@ gfx::Rect GeometryCache::GetBoundsPx() {
     return geometry_;
   }
   auto parent_bounds = parent_->GetBoundsPx();
+  if (!weak_this) {
+    return {};
+  }
   gfx::Vector2d offset(parent_bounds.x(), parent_bounds.y());
   return geometry_ + offset;
 }
