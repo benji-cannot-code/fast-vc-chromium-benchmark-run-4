6c4753f75f5d59c109fd3adc2787be7ce97e9c24
Dmytro Dmytriiev <dmytriievd@google.com>
2026-05-14 07:28:08 -0700

    [iOS] Enable screenshot protection feature flag by default
    Finch experimentation is skipped for this feature because it is
    controlled by Enterprise policies and remains a no-op for non-Enterprise
    browsers and accounts. Additionally, the feature's behavior cannot be
    enabled through Finch alone.
    
    Design document: go/bling-enterprise-screenshot-protection
    
    Fixed: 503425573
    Change-Id: I4f294394daa5591c48f149aaf3e36c4d28a24112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7845761
    Reviewed-by: Xu Li <mxlg@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Dmytro Dmytriiev <dmytriievd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630613}

--- ios/chrome/browser/enterprise/data_protection/public/features.mm
+++ ios/chrome/browser/enterprise/data_protection/public/features.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/enterprise/data_protection/public/features.h"
 
-BASE_FEATURE(kEnableScreenshotProtectionIOS, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableScreenshotProtectionIOS, base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsEnableScreenshotProtectionIOSEnabled() {
   return base::FeatureList::IsEnabled(kEnableScreenshotProtectionIOS);
