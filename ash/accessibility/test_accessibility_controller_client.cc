70738b32288b23003dd30764c44c6b04e6586703
Kyle Williams <kdgwill@google.com>
2026-05-13 20:50:09 -0700

    Revert "[cfm-a11y]: ash side toggle for ChromeVox"
    This reverts commit 7e52c030e9aea6c624e48cb71de41ba28486d6fe.
    
    Reason for revert: Variety of accessibility issues are occurring
    * crbug.com/361500636 - Two-finger long press does not enable a11y
    * crbug.com/362429272 - Two-finger long press stateful issue
    
    AX-Relnotes: n/a.
    
    Bug: b:469308759
    Test: manually by CFM team
    
    Change-Id: I9a2d63fe45908ee2d5cd2cef57c2176eb1c923c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7827738
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630451}

--- ash/accessibility/test_accessibility_controller_client.cc
+++ ash/accessibility/test_accessibility_controller_client.cc
@@ -55,6 +55,10 @@ bool TestAccessibilityControllerClient::ToggleDictation() {
 
 void TestAccessibilityControllerClient::SilenceSpokenFeedback() {}
 
+void TestAccessibilityControllerClient::OnTwoFingerTouchStart() {}
+
+void TestAccessibilityControllerClient::OnTwoFingerTouchStop() {}
+
 bool TestAccessibilityControllerClient::ShouldToggleSpokenFeedbackViaTouch()
     const {
   return true;
