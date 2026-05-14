853f65e0b578622033dafa1de3044f31a7ccfd34
Calder Kitagawa <ckitagawa@chromium.org>
2026-05-14 12:52:03 -0700

    [IA] Fix bottom controls color on GTS
    On GTS the BottomControlsStacker color was leaking through. This was
    because the state was being updated after updating the color rather than
    before.
    
    Bug: 508639901
    Change-Id: I879b7a0ed4f7d48b0e7e95307a0e3764fe6b0606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850703
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630718}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedNavigationBarColorController.java
@@ -310,8 +310,8 @@ class TabbedNavigationBarColorController
                     @Override
                     public void onStartedShowing(@LayoutType int layoutType) {
                         if (layoutType == LayoutType.TAB_SWITCHER) {
-                            updateNavigationBarColor();
                             enableOverviewMode();
+                            updateNavigationBarColor();
                         } else if (layoutType == LayoutType.TOOLBAR_SWIPE
                                 && ChromeFeatureList.sNavBarColorAnimation.isEnabled()
                                 && isBottomChinEnabled()) {
@@ -327,8 +327,8 @@ class TabbedNavigationBarColorController
                     @Override
                     public void onStartedHiding(@LayoutType int layoutType) {
                         if (layoutType != LayoutType.TAB_SWITCHER) return;
-                        updateNavigationBarColor();
                         disableOverviewMode();
+                        updateNavigationBarColor();
                     }
 
                     @Override
