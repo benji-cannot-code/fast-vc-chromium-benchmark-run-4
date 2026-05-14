d3ba76aa6e30b6396fcd734938faca00f105bd16
Aishwarya Rajesh <aishwaryarj@google.com>
2026-05-13 23:43:21 -0700

    [MWCR] Track and persist window bounds
    Bug: 505171896
    Change-Id: I8a709392c4d599485b0cb4f8c6e05a3c3750a15b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794423
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630515}

--- chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskIntegrationTest.java
+++ chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskIntegrationTest.java
@@ -1473,7 +1473,7 @@ public class ChromeAndroidTaskIntegrationTest {
         public void onFeatureRemoved() {}
 
         @Override
-        public void onTaskBoundsChanged(Rect newBoundsInDp) {
+        public void onTaskBoundsChanged(int displayId, Rect newBoundsInDp, Rect newBoundsInPx) {
             mTimesOnTaskBoundsChanged++;
         }
 
