26f3d8bfe78137c9f5fa7aebe7dbbe27e124c0d7
Jinsuk Kim <jinsukkim@chromium.org>
2026-05-14 09:53:46 -0700

    [Multicolumn][SIngle Activity] Fix the soft keyboard issue with payment
    Another take on soft keyboard issue in Payment/add card settings.
    
    Bug: 505762141
    Change-Id: I29eedf8d281b1ab66172d94be5e3a82166baa821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842804
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630675}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalCardEditor.java
@@ -49,6 +49,7 @@ import org.chromium.chrome.browser.settings.SettingsActivity;
 import org.chromium.components.autofill.AutofillProfile;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
 import org.chromium.components.browser_ui.styles.SemanticColorUtils;
+import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.accessibility.AccessibilityState;
 import org.chromium.ui.text.EmptyTextWatcher;
 
@@ -152,6 +153,14 @@ public class AutofillLocalCardEditor extends AutofillCreditCardEditor
                         if (mNumberLabel.getError() != null) {
                             validateCardNumberAndUpdateError(removeSpaces(s.toString()));
                         }
+                    }
+                });
+        // Handles the issue of keyboard not appearing with #settings-single-activity flag.
+        // See crbug.com/505762141
+        mNumberText.setOnFocusChangeListener(
+                (view, hasFocus) -> {
+                    if (hasFocus) {
+                        KeyboardVisibilityDelegate.getInstance().showKeyboard(mNumberText);
                     }
                 });
 
