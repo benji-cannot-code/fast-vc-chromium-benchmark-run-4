3683676f1da773b185646b6d3fab71140f360a6f
Sky Malice <skym@chromium.org>
2026-05-14 09:25:08 -0700

    [AIM] Trim search term in ExtractSearchTermsFromURL.
    Bug: 512902149
    Change-Id: I933ac76414d7e339d323d50da633b25deb0f5fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846496
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630666}

--- components/search_engines/template_url_unittest.cc
+++ components/search_engines/template_url_unittest.cc
@@ -1695,6 +1695,10 @@ TEST_F(TemplateURLTest, ExtractSearchTermsFromURL) {
   EXPECT_EQ(u"something", result);
 
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
+      GURL("http://google.com/?q=something%20"), search_terms_data_, &result));
+  EXPECT_EQ(u"something", result);
+
+  EXPECT_TRUE(url.ExtractSearchTermsFromURL(
       GURL("http://google.com/?q=something"), search_terms_data_, &result));
   EXPECT_EQ(u"something", result);
 
