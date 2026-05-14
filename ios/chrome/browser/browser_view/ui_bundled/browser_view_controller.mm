b9409d6768aa8ffd6e603052f96169d2d9314983
Hira Mahmood <hiramahmood@google.com>
2026-05-14 09:29:18 -0700

    [iOS] Fix fullscreen related startup crash in BrowserViewController
    This CL fixes a crash on startup when ChromeNextIA is disabled and
    FullscreenRefactor is enabled. Setting the omnibox position triggered an
    early fullscreen update, forcing the view to load before
    MainToolbarCoordinator created the toolbars.
    
    To fix this, a [self isViewLoaded] check is added to prevent the
    premature view load.
    
    Fixed: 510780274
    Change-Id: I337161fcb36f0351e207662f4ff049cc521194eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829141
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630670}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -2284,9 +2284,11 @@ bool IsFullscreenNextIAEnabled() {
     if (self.secondaryToolbarHeightConstraint.constant != height) {
       self.secondaryToolbarHeightConstraint.constant = height;
       // Force a layout to cause the frame to be recalculated.
-      UIView* view = self.view;
-      [view setNeedsLayout];
-      [view layoutIfNeeded];
+      if ([self isViewLoaded]) {
+        UIView* view = self.view;
+        [view setNeedsLayout];
+        [view layoutIfNeeded];
+      }
     }
   }
 }
