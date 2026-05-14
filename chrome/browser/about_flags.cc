a40c5ea72b6c9934a61f4a6b2586a3a6405e81fd
Christopher Grant <cjgrant@chromium.org>
2026-05-14 15:19:52 -0700

    SE Dialog: Expose rate-limiting feature param in chrome://flags
    The feature param was added in http://crrev.com/c/7844587.
    Expose it via the flags UI for easier testing.
    
    Change-Id: Iec91b86600c4e13c9c74633a93b0287e701e1ba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848738
    Auto-Submit: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630803}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4521,10 +4521,15 @@ const FeatureEntry::FeatureVariation
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 const FeatureEntry::FeatureParam kSearchEngineExplicitChoiceDialogEscapable[] =
     {{"escapable", "true"}};
+const FeatureEntry::FeatureParam
+    kSearchEngineExplicitChoiceDialogShowOncePerSession[] = {
+        {"unlimited_shows", "false"}};
 
 const FeatureEntry::FeatureVariation
     kSearchEngineExplicitChoiceDialogVariations[] = {
         {"Escapable", kSearchEngineExplicitChoiceDialogEscapable, nullptr},
+        {"Show Once Per Session",
+         kSearchEngineExplicitChoiceDialogShowOncePerSession, nullptr},
 };
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 
