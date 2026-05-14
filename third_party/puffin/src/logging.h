a8c69134d1bf27a674e10d9a47e3e363115c8896
Noah Rose Ledesma <noahrose@google.com>
2026-05-14 16:56:46 -0700

    Enable clang-format for puffin
    Allow clang-format to operate on puffin sources and run the formatter.
    
    Change-Id: I872ca9e0a84cf13c042cc1c76eaf46076a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850554
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630914}

--- third_party/puffin/src/logging.h
+++ third_party/puffin/src/logging.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/logging.h"
 
-#define TEST_AND_RETURN_FALSE(_x)    \
-  do {                               \
-    if (!(_x)) {                     \
-      VLOG(1) << #_x " failed.";     \
-      return false;                  \
-    }                                \
+#define TEST_AND_RETURN_FALSE(_x) \
+  do {                            \
+    if (!(_x)) {                  \
+      VLOG(1) << #_x " failed.";  \
+      return false;               \
+    }                             \
   } while (0)
 
 #define TEST_AND_RETURN_VALUE(_x, _v) \
