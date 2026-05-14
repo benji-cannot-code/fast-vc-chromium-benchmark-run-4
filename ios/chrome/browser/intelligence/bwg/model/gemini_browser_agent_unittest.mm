909a73a9ebe0f57f5b6b138a2004b80159c3ffef
adamta <adamta@google.com>
2026-05-14 11:21:33 -0700

    [iOS] Rewrite Gemini web state eligibility
    Combines `IsGeminiAvailableForWebState` and `IsUrlEligibleForGemini`
    into a single function to represent whether Gemini can be shown on a web
    state.
    
    This is complemented by the existing `CanExtractPageContentForWebState` to handle all eligibility cases. The HTTP/HTTPS check is already there,
    so is removed from this condition.
    
    Users are put into the following buckets:
    - Default: Gemini available on all pages that can extract page context, except NTP. With `IsGeminiFloatyAllPagesEnabled` enabled, Gemini is also
    available on pages that can't extract page context.
    - Copresence: Available on all pages except NTP and AIM. Can be eligible on SRP depending on a flag parameter.
    - NextIA: Available for all web states.
    
    Fixed: 507418315, 507470074
    Change-Id: I8bca39a62b280c231714920df3864cb2f2c980cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838379
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630702}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent_unittest.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent_unittest.mm
@@ -99,6 +99,7 @@ class GeminiBrowserAgentTest : public PlatformTest {
         std::make_unique<web::FakeWebState>();
     web_state_ = web_state.get();
     web_state->SetBrowserState(profile_);
+    web_state->SetCurrentURL(GURL("chrome://newtab/"));
     GeminiTabHelper::CreateForWebState(web_state.get());
     WebViewProxyTabHelper::CreateForWebState(web_state.get());
     gemini_tab_helper_ = GeminiTabHelper::FromWebState(web_state.get());
@@ -779,7 +780,7 @@ TEST_F(GeminiBrowserAgentTest, TestOnGeminiAvailabilityChanged) {
   // Navigate to an ineligible site.
   web_state_ = static_cast<web::FakeWebState*>(
       browser_->GetWebStateList()->GetActiveWebState());
-  web_state_->SetCurrentURL(GURL("chrome://settings"));
+  web_state_->SetCurrentURL(GURL("chrome://newtab/"));
 
   // Manually trigger page context updated to simulate navigation finishing.
   gemini_browser_agent_->OnPageContextUpdated(web_state_);
