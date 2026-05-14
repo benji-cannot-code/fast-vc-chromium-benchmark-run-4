cd3cd43bee1c73c3cf425f2419337662045fa300
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-05-13 20:55:13 -0700

    Rename PrefetchHandleImpl::PrefetchContainerObserver
    To avoid confusion.
    
    Bug: 480271813, 400761083
    Change-Id: Ib3b416ced3937ba3ab6093f2d11bcd9d8c6c2307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7823801
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630459}

--- content/browser/preloading/prefetch/prefetch_handle_impl.cc
+++ content/browser/preloading/prefetch/prefetch_handle_impl.cc
@@ -11,19 +11,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class PrefetchHandleImpl::PrefetchContainerObserver final
-    : public content::PrefetchContainerObserver {
+class PrefetchHandleImpl::PrefetchContainerObserverForCallback final
+    : public PrefetchContainerObserver {
  public:
-  PrefetchContainerObserver();
-  ~PrefetchContainerObserver() override;
+  PrefetchContainerObserverForCallback() = default;
+  ~PrefetchContainerObserverForCallback() override = default;
 
   // Not movable nor copyable.
-  PrefetchContainerObserver(PrefetchContainerObserver&& other) = delete;
-  PrefetchContainerObserver& operator=(PrefetchContainerObserver&& other) =
-      delete;
-  PrefetchContainerObserver(const PrefetchContainerObserver&) = delete;
-  PrefetchContainerObserver& operator=(const PrefetchContainerObserver&) =
-      delete;
+  PrefetchContainerObserverForCallback(
+      PrefetchContainerObserverForCallback&& other) = delete;
+  PrefetchContainerObserverForCallback& operator=(
+      PrefetchContainerObserverForCallback&& other) = delete;
+  PrefetchContainerObserverForCallback(
+      const PrefetchContainerObserverForCallback&) = delete;
+  PrefetchContainerObserverForCallback& operator=(
+      const PrefetchContainerObserverForCallback&) = delete;
 
   void SetOnPrefetchHeadReceivedCallback(
       base::RepeatingCallback<void(const network::mojom::URLResponseHead&)>
@@ -34,7 +36,7 @@ class PrefetchHandleImpl::PrefetchContainerObserver final
                const std::optional<int>& response_code)>
           on_prefetch_completed_or_failed);
 
-  // Implements `content::PrefetchContainerObserver`.
+  // Implements `PrefetchContainerObserver`.
   void OnWillBeDestroyed(const PrefetchContainer& prefetch_container) override;
   void OnGotInitialEligibility(
       const PrefetchContainer& prefetch_container) override;
@@ -51,20 +53,14 @@ class PrefetchHandleImpl::PrefetchContainerObserver final
       on_prefetch_completed_or_failed_;
 };
 
-PrefetchHandleImpl::PrefetchContainerObserver::PrefetchContainerObserver() =
-    default;
-
-PrefetchHandleImpl::PrefetchContainerObserver::~PrefetchContainerObserver() =
-    default;
-
-void PrefetchHandleImpl::PrefetchContainerObserver::
+void PrefetchHandleImpl::PrefetchContainerObserverForCallback::
     SetOnPrefetchHeadReceivedCallback(
         base::RepeatingCallback<void(const network::mojom::URLResponseHead&)>
             on_prefetch_head_received) {
   on_prefetch_head_received_ = std::move(on_prefetch_head_received);
 }
 
-void PrefetchHandleImpl::PrefetchContainerObserver::
+void PrefetchHandleImpl::PrefetchContainerObserverForCallback::
     SetOnPrefetchCompletedOrFailedCallback(
         base::RepeatingCallback<
             void(const network::URLLoaderCompletionStatus& completion_status,
@@ -73,13 +69,13 @@ void PrefetchHandleImpl::PrefetchContainerObserver::
   on_prefetch_completed_or_failed_ = std::move(on_prefetch_completed_or_failed);
 }
 
-void PrefetchHandleImpl::PrefetchContainerObserver::OnWillBeDestroyed(
-    const PrefetchContainer& prefetch_container) {}
+void PrefetchHandleImpl::PrefetchContainerObserverForCallback::
+    OnWillBeDestroyed(const PrefetchContainer& prefetch_container) {}
 
-void PrefetchHandleImpl::PrefetchContainerObserver::OnGotInitialEligibility(
-    const PrefetchContainer& prefetch_container) {}
+void PrefetchHandleImpl::PrefetchContainerObserverForCallback::
+    OnGotInitialEligibility(const PrefetchContainer& prefetch_container) {}
 
-void PrefetchHandleImpl::PrefetchContainerObserver::OnDeterminedHead(
+void PrefetchHandleImpl::PrefetchContainerObserverForCallback::OnDeterminedHead(
     const PrefetchContainer& prefetch_container) {
   if (on_prefetch_head_received_) {
     // This condition will be used in a callback provided in the future.
@@ -96,8 +92,8 @@ void PrefetchHandleImpl::PrefetchContainerObserver::OnDeterminedHead(
   }
 }
 
-void PrefetchHandleImpl::PrefetchContainerObserver::OnPrefetchCompletedOrFailed(
-    const PrefetchContainer& prefetch_container) {
+void PrefetchHandleImpl::PrefetchContainerObserverForCallback::
+    OnPrefetchCompletedOrFailed(const PrefetchContainer& prefetch_container) {
   // `IsDecoy()` check is added to preserve the existing behavior.
   // https://crbug.com/400761083
   if (prefetch_container.IsDecoy()) {
@@ -116,7 +112,7 @@ PrefetchHandleImpl::PrefetchHandleImpl(
     : prefetch_service_(std::move(prefetch_service)),
       prefetch_container_(std::move(prefetch_container)),
       prefetch_container_observer_(
-          std::make_unique<PrefetchHandleImpl::PrefetchContainerObserver>()) {
+          std::make_unique<PrefetchContainerObserverForCallback>()) {
   CHECK_CURRENTLY_ON(BrowserThread::UI);
   CHECK(prefetch_service_);
   // Note that `prefetch_container_` can be nullptr.
