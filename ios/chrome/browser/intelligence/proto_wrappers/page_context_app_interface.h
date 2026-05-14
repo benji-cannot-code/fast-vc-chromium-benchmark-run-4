35a186064b0a6863ca4cc00cd04e4b28fe37ed25
Matt Reichhoff <mreichhoff@chromium.org>
2026-05-14 06:55:45 -0700

    Reland "[ios][apcv2] Add egtest that covers large files"
    The prior CL was reverted due to a naming collision in the
    internal repo. The internal class was renamed, which should
    enable this to re-land.
    
    Original change's description:
    > [ios][apcv2] Add egtest that covers large files
    >
    > The linked bug indicated some APCs are too large to handle. An IPC
    > optimization improved that situation. This test suite attempts to add
    > egtests that can recreate the situation on the various device / OS
    > versions in CI, and also informs us that more optimizations may be
    > needed as we add actionable mode.
    >
    > Bug: 507473141
    > Change-Id: Ie5af0c9efe1da2fda7104431eb43cbff84818297
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828645
    > Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    > Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    > Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1629946}
    
    Bug: 507473141
    Change-Id: I05d66ffb7ced1a76d9f823b6bb2a03a73e853ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846532
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630604}

---
+++ ios/chrome/browser/intelligence/proto_wrappers/page_context_app_interface.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INTELLIGENCE_PROTO_WRAPPERS_PAGE_CONTEXT_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_INTELLIGENCE_PROTO_WRAPPERS_PAGE_CONTEXT_APP_INTERFACE_H_
+
+#import <Foundation/Foundation.h>
+
+// App interface to interact with the PageContextWrapper from integration tests.
+@interface PageContextAppInterface : NSObject
+
+// Fetches the latest APC with specific configuration.
++ (NSData*)fetchLatestAPCWithRichExtraction:(BOOL)useRichExtraction
+                             actionableMode:(BOOL)useActionableMode;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_INTELLIGENCE_PROTO_WRAPPERS_PAGE_CONTEXT_APP_INTERFACE_H_
