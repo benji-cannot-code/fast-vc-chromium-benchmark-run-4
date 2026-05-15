6a9dd21c106d3e229b8a7d79e24b287f583e3455
Alexander Cooper <alcooper@chromium.org>
2026-05-15 14:06:08 -0700

    Ensure session end events propagate up from OpenXR Runtime
    It is theoretically possible for a session end event to propagate up
    from the OpenXR runtime in a manner that can be missed, particularly in
    race conditions where other mojom listeners may have been cleaned up,
    thus leaving the OpenXrDevice in an indeterminate state. Tests should
    already exist for this scenario, but it is believed to be mostly due to
    difficult to trigger race conditions.
    
    To ensure the render loop does not hold dangling references to the
    extension or platform helpers during subsequent session requests (such
    as in multi-entry scenarios), this also explicitly destroys the
    OpenXrRenderLoop in OnSessionEnded.
    
    Fixed: 235526581
    Change-Id: I6f5637a9d94b5b00c18b3f4dc6d6a295addfad22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838182
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631550}

--- device/vr/openxr/openxr_render_loop.h
+++ device/vr/openxr/openxr_render_loop.h
@@ -111,7 +111,8 @@ class OpenXrRenderLoop : public XRThread,
   void RequestSession(base::RepeatingCallback<void(mojom::XRVisibilityState)>
                           on_visibility_state_changed,
                       mojom::XRRuntimeSessionOptionsPtr options,
-                      RequestSessionCallback callback);
+                      RequestSessionCallback callback,
+                      base::OnceClosure end_callback);
 
  private:
   void SetVisibilityState(mojom::XRVisibilityState visibility_state);
@@ -127,6 +128,7 @@ class OpenXrRenderLoop : public XRThread,
       base::RepeatingCallback<void(mojom::XRVisibilityState)>
           on_visibility_state_changed,
       mojom::XRRuntimeSessionOptionsPtr options,
+      base::OnceClosure end_callback,
       bool success);
 
   // Will Submit if we have textures submitted from the Overlay (if it is
@@ -205,7 +207,8 @@ class OpenXrRenderLoop : public XRThread,
 
   void StartRuntime(base::RepeatingCallback<void(mojom::XRVisibilityState)>
                         on_visibility_state_changed,
-                    mojom::XRRuntimeSessionOptionsPtr options);
+                    mojom::XRRuntimeSessionOptionsPtr options,
+                    base::OnceClosure end_callback);
   void StopRuntime();
   void OnSessionStart();
   bool HasSessionEnded();
@@ -217,6 +220,7 @@ class OpenXrRenderLoop : public XRThread,
   void OnOpenXrSessionStarted(
       base::RepeatingCallback<void(mojom::XRVisibilityState)>
           on_visibility_state_changed,
+      base::OnceClosure end_callback,
       mojom::XRRuntimeSessionOptionsPtr options,
       XrResult result);
   bool UpdateViews();
@@ -336,6 +340,7 @@ class OpenXrRenderLoop : public XRThread,
       environment_receiver_{this};
 
   RequestSessionCallback request_session_callback_;
+  base::OnceClosure end_callback_;
 
   // This must be the last member
   base::WeakPtrFactory<OpenXrRenderLoop> weak_ptr_factory_{this};
