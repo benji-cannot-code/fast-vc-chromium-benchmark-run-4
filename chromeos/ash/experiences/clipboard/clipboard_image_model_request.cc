d757df76ea0ad0bbf17b868643a26ec708bb6791
Achuith Bhandarkar <achuith@chromium.org>
2026-05-15 13:47:52 -0700

    ash: Use OTR browser context for clipboard
    For ChromeOS Clipboard History HTML Previews, use OTR browser context
    which doesn't have access to user's persistent cookies or HTTP Authentication cache
    
    Bug: 498429876
    Change-Id: Iae34a804dd94fe514185618fb1a8deea4fbae5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846841
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631536}

--- chromeos/ash/experiences/clipboard/clipboard_image_model_request.cc
+++ chromeos/ash/experiences/clipboard/clipboard_image_model_request.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "components/account_id/account_id.h"
 #include "components/viz/common/frame_sinks/copy_output_result.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
@@ -50,7 +51,10 @@ content::BrowserContext* GetBrowserContextByAccountId(
       ash::BrowserContextHelper::Get()->GetBrowserContextByAccountId(
           account_id);
   CHECK(browser_context);
-  return browser_context;
+  // Use OTR browser context to avoid giving access to the user's persistent
+  // cookies or HTTP Authentication cache.
+  return ash::BrowserContextHelper::Get()->GetOTRBrowserContext(
+      browser_context, /*create_if_needed=*/true);
 }
 
 }  // namespace
