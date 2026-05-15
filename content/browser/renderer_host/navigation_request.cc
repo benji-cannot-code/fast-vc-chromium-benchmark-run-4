90bded0364ad379fc429dc43fe51c231461870aa
Rakina Zata Amni <rakina@chromium.org>
2026-05-15 01:24:26 -0700

    Investigate failing initial WebUI -> non-initial-WebUI CHECK
    Changes the CHECK to a DumpWithoutCrashing temporarily.
    
    Bug: 511971445
    Change-Id: I7f9a4c420fc531c4e628c287b6b09bd199df3724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851836
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631144}

--- content/browser/renderer_host/navigation_request.cc
+++ content/browser/renderer_host/navigation_request.cc
@@ -1795,6 +1795,7 @@ NavigationRequest::NavigationRequest(
   CHECK(IsInOutermostMainFrame() || commit_params_->data_url_as_string.empty());
 #endif
   CheckSoftNavigationHeuristicsInvariants();
+  ScopedCrashKeys crash_keys(*this);
 
 #if !BUILDFLAG(IS_ANDROID)
   // It should not be possible to navigate away from the initial WebUI page,
@@ -1806,7 +1807,14 @@ NavigationRequest::NavigationRequest(
               ->GetSiteInstance()
               ->GetSecurityPrincipal()
               .GetDeprecatedSiteURL());
-  CHECK(!current_rfh_is_initial_webui || IsInitialWebUINavigation());
+  if (current_rfh_is_initial_webui && !IsInitialWebUINavigation()) {
+    // TODO(crbug.com/511971445): Turn this into a CHECK again once
+    // investigation on what can cause this is finished.
+    base::debug::DumpWithoutCrashing();
+    // We should not allow navigating away from initial WebUI. Mark this
+    // request for cancellation in BeginNavigation().
+    should_cancel_on_leaving_initial_webui_ = true;
+  }
   if (IsInitialWebUINavigation()) {
     // Initial WebUI navigations must satisfy all these conditions
     // - Is browser initiated
@@ -1833,7 +1841,6 @@ NavigationRequest::NavigationRequest(
   }
 #endif
 
-  ScopedCrashKeys crash_keys(*this);
 
   ComputeDownloadPolicy();
 
@@ -2487,6 +2494,17 @@ void NavigationRequest::BeginNavigation() {
   CHECK(!HasRenderFrameHost());
   ScopedCrashKeys crash_keys(*this);
   UpdateNavigationHandleTimingsOnBeginNavigation();
+
+  // Cancel the navigation if it is an invalid attempt to navigate away from
+  // an initial WebUI page.
+  if (should_cancel_on_leaving_initial_webui_) {
+    StartNavigation();
+    OnRequestFailedInternal(
+        network::URLLoaderCompletionStatus(net::ERR_ABORTED),
+        /*skip_throttles=*/false, /*error_page_content=*/std::nullopt,
+        /*collapse_frame=*/false);
+    return;
+  }
 
   if (begin_navigation_callback_for_testing_) {
     std::move(begin_navigation_callback_for_testing_).Run();
