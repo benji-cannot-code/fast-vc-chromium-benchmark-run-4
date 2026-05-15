4bb2e4058706c60e295bcc559e5d626dc1222d71
Syed AbuTalib <lowkey@google.com>
2026-05-14 17:30:29 -0700

    Convert target_output_layout to ChannelLayoutConfig and update mojo
    There was a case where we guess a `DISCRETE`'s ChannelCount in
    `AudioRenderImpl`. This change converts this `hw_layout` into a
    `ChannelLayoutConfig`, which has stricter requirements in relation to
    `DISCRETE`.
    
    In order to update `target_output_channel_layout_`, misc. files such as
    the mojo files had to be updated as well.
    
    Bug: 483461268
    Change-Id: I893d96ca6d33cc695cfb2fe742c311e1f8381045
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791708
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630945}

---
+++ media/mojo/mojom/channel_layout_mojom_traits.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_MOJO_MOJOM_CHANNEL_LAYOUT_MOJOM_TRAITS_H_
+#define MEDIA_MOJO_MOJOM_CHANNEL_LAYOUT_MOJOM_TRAITS_H_
+
+#include "media/base/channel_layout.h"
+#include "media/mojo/mojom/channel_layout.mojom-shared.h"
+#include "mojo/public/cpp/base/empty_mojom_traits.h"
+
+namespace mojo {
+
+template <>
+struct UnionTraits<media::mojom::ChannelLayoutConfigDataView,
+                   media::ChannelLayoutConfig> {
+  static media::mojom::ChannelLayoutConfigDataView::Tag GetTag(
+      const media::ChannelLayoutConfig& input);
+
+  static media::mojom::ChannelLayout predefined_layout(
+      const media::ChannelLayoutConfig& input);
+  static uint32_t discrete_channels(const media::ChannelLayoutConfig& input);
+
+  static bool Read(media::mojom::ChannelLayoutConfigDataView input,
+                   media::ChannelLayoutConfig* output);
+};
+
+}  // namespace mojo
+
+#endif  // MEDIA_MOJO_MOJOM_CHANNEL_LAYOUT_MOJOM_TRAITS_H_
