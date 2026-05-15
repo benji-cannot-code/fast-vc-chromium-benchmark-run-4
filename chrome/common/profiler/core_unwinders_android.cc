1878bf9c7102cbc98d045173584b43256ea40e9f
Andrew Grieve <agrieve@chromium.org>
2026-05-15 09:55:58 -0700

    Android: Fix #ifdefs that rely on kStartOfText
    In service of use_mold=true for arm32
    
    Bug: 40208899
    Change-Id: I83fb53fd9ae0d8345dc50828f70d1a9d0de57d1f
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831101
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631365}

--- chrome/common/profiler/core_unwinders_android.cc
+++ chrome/common/profiler/core_unwinders_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <vector>
 
+#include "base/android/library_loader/anchor_functions_buildflags.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/feature_list.h"
@@ -26,7 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sampling_profiler/process_type.h"
 #include "components/version_info/channel.h"
 
-#if defined(ARCH_CPU_ARMEL) && BUILDFLAG(ENABLE_ARM_CFI_TABLE)
+#if defined(ARCH_CPU_ARMEL) && BUILDFLAG(ENABLE_ARM_CFI_TABLE) && \
+    BUILDFLAG(SUPPORTS_CODE_ORDERING)
 #define ARM32_UNWINDING_SUPPORTED 1
 #else
 #define ARM32_UNWINDING_SUPPORTED 0
