fdbea63ac2792ad2582407923ba360ac28ad62a2
Hidehiko Abe <hidehiko@chromium.org>
2026-05-14 21:33:59 -0700

    Remove SearchSuggestEnabled from file_manager.
    It is not used.
    
    BUG=487139800
    TEST=Tryjob
    
    Change-Id: Ic249704a10fc90c339e4e7436218b0ae228f72eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848935
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Josh Simmons <simmonsjosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631059}

--- chrome/browser/ash/extensions/file_manager/event_router.cc
+++ chrome/browser/ash/extensions/file_manager/event_router.cc
@@ -64,7 +64,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/api/file_manager_private.h"
 #include "chrome/common/extensions/extension_constants.h"
-#include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/disks/disk.h"
 #include "chromeos/ash/components/drivefs/drivefs_host.h"
 #include "chromeos/ash/components/login/login_state/login_state.h"
@@ -728,7 +727,6 @@ void EventRouter::ObserveEvents() {
     pref_change_registrar_->Add(drive::prefs::kDisableDriveOverCellular, cb);
     pref_change_registrar_->Add(drive::prefs::kDisableDrive, cb);
     pref_change_registrar_->Add(ash::prefs::kFilesAppTrashEnabled, cb);
-    pref_change_registrar_->Add(prefs::kSearchSuggestEnabled, cb);
     pref_change_registrar_->Add(ash::prefs::kUse24HourClock, cb);
     pref_change_registrar_->Add(arc::prefs::kArcEnabled, cb);
     pref_change_registrar_->Add(arc::prefs::kArcHasAccessToRemovableMedia, cb);
--- ui/file_manager/file_manager/definitions/file_manager_private.d.ts
+++ ui/file_manager/file_manager/definitions/file_manager_private.d.ts
@@ -550,7 +550,6 @@ declare global {
       export interface Preferences {
         driveEnabled: boolean;
         driveSyncEnabledOnMeteredNetwork: boolean;
-        searchSuggestEnabled: boolean;
         use24hourClock: boolean;
         timezone: string;
         arcEnabled: boolean;
