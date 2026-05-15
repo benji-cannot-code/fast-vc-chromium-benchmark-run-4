8f684a80348737948494e7bc00fa5c030075c0bd
Charles Hager <clhager@google.com>
2026-05-15 13:24:54 -0700

    Animate web contents for side panel
    This change animates the web contents to account for the side panel
    transition, calling #updateWebContentsSize() on each update of the side
    panel transition. This is done using a Transition that is essentially
    just a wrapper for ValueAnimator, which allows for an animator to update
    the web contents in sync with the rest of the side panel TransitionSet.
    
    Bug: 505118476
    Change-Id: Ia386bfdefeee9d210a338e9f811996316e7deb05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837458
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Linyu He <linyuh@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631511}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/CompositorViewHolderUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/CompositorViewHolderUnitTest.java
@@ -1384,7 +1384,8 @@ public class CompositorViewHolderUnitTest {
     @Test
     @EnableFeatures(ChromeFeatureList.ENABLE_ANDROID_SIDE_PANEL)
     public void testSetSideUiStateProviderSupplier() {
-        when(mSideUiStateProvider.measureSideUiSpecs()).thenReturn(SideUiSpecs.EMPTY_SIDE_UI_SPECS);
+        when(mSideUiStateProvider.getCurrentSideUiSpecs())
+                .thenReturn(SideUiSpecs.EMPTY_SIDE_UI_SPECS);
         mSideUiStateProviderSupplier.set(mSideUiStateProvider);
         runCurrentTasks();
 
@@ -1407,7 +1408,7 @@ public class CompositorViewHolderUnitTest {
         int startContainerWidth = 100;
         int endContainerWidth = 200;
         SideUiSpecs currentSideUiSpecs = new SideUiSpecs(startContainerWidth, endContainerWidth);
-        when(mSideUiStateProvider.measureSideUiSpecs()).thenReturn(currentSideUiSpecs);
+        when(mSideUiStateProvider.getCurrentSideUiSpecs()).thenReturn(currentSideUiSpecs);
         mSideUiStateProviderSupplier.set(mSideUiStateProvider);
         runCurrentTasks();
 
@@ -1441,7 +1442,7 @@ public class CompositorViewHolderUnitTest {
         int startContainerWidth = 50;
         int endContainerWidth = 150;
         SideUiSpecs currentSideUiSpecs = new SideUiSpecs(startContainerWidth, endContainerWidth);
-        when(mSideUiStateProvider.measureSideUiSpecs()).thenReturn(currentSideUiSpecs);
+        when(mSideUiStateProvider.getCurrentSideUiSpecs()).thenReturn(currentSideUiSpecs);
         mSideUiStateProviderSupplier.set(mSideUiStateProvider);
         runCurrentTasks();
 
@@ -1484,7 +1485,7 @@ public class CompositorViewHolderUnitTest {
 
         // Arbitrary Side UI width.
         SideUiSpecs currentSideUiSpecs = new SideUiSpecs(SIDE_UI_START_WIDTH, SIDE_UI_END_WIDTH);
-        when(mSideUiStateProvider.measureSideUiSpecs()).thenReturn(currentSideUiSpecs);
+        when(mSideUiStateProvider.getCurrentSideUiSpecs()).thenReturn(currentSideUiSpecs);
         mSideUiStateProviderSupplier.set(mSideUiStateProvider);
         runCurrentTasks();
         mCompositorViewHolder.onSideUiSpecsChanged(currentSideUiSpecs);
