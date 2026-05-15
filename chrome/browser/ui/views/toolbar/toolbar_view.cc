6feef83800e576640c1321dc0af1cdf2c7820706
Robert Liao <robliao@chromium.org>
2026-05-15 07:14:33 -0700

    Report When the Home Button Overflows in the Toolbar
    Fixed: 513276108
    Change-Id: Ib24d8db4f82eb32e7ccba61ca5a0f07f9bd2942e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851164
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631255}

--- chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/i18n/number_formatting.h"
 #include "base/i18n/rtl.h"
-#include "base/metrics/histogram_macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/notimplemented.h"
@@ -1437,6 +1437,14 @@ void ToolbarView::ChildPreferredSizeChanged(views::View* child) {
   InvalidateLayout();
   if (size() != GetPreferredSize()) {
     PreferredSizeChanged();
+  }
+}
+
+void ToolbarView::ChildVisibilityChanged(views::View* child) {
+  if (child == home_) {
+    if (!home_->GetVisible() && show_home_button_.GetValue()) {
+      base::UmaHistogramBoolean("Toolbar.Overflow.HomeButton", true);
+    }
   }
 }
 
