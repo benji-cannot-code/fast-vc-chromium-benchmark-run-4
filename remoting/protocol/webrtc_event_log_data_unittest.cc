dc3173f796e902b365451b669d462e1ad6e61f9f
Lambros Lambrou <lambroslambrou@chromium.org>
2026-05-15 09:17:24 -0700

    Synchronize WebrtcEventLogData with mutex locking
    Resolve potential threading race conditions and data corruption in
    WebrtcEventLogData by adding base::Lock synchronization.
    
    Changes include:
    - Added base::Lock to protect internal circular_deque sections_.
    - Implemented MAGI-reviewed zero-allocation buffer recycling in CreateNewSection().
    - Added CHECK_GT validation to SetMaxSectionSizeForTest and SetMaxSectionsForTest.
    - Eliminated base::checked_cast<int> to prevent DoS integer overflow crashes.
    - Added multithreaded concurrency stress test.
    
    TAG=agy
    CONV=057084ee-cea7-42d5-a8ca-4575cf9a75e3
    
    Bug: 513231432
    Change-Id: I64bab172d521e704f75289cd3af0eb30ddc60123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848427
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631341}

--- remoting/protocol/webrtc_event_log_data_unittest.cc
+++ remoting/protocol/webrtc_event_log_data_unittest.cc
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/protocol/webrtc_event_log_data.h"
 
+#include "base/barrier_closure.h"
+#include "base/functional/bind.h"
+#include "base/run_loop.h"
+#include "base/task/thread_pool.h"
+#include "base/test/task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace remoting::protocol {
@@ -66,6 +71,34 @@ TEST(WebrtcEventLogDataTest, StoreThenClear_IsEmpty) {
   auto data = event_log.TakeLogData();
 
   EXPECT_TRUE(data.empty());
+}
+
+TEST(WebrtcEventLogDataTest, MultiThreadedAccess) {
+  base::test::TaskEnvironment task_environment;
+  WebrtcEventLogData event_log;
+  event_log.SetMaxSectionSizeForTest(100);
+
+  constexpr int kNumTasks = 10;
+  base::RunLoop run_loop;
+  auto barrier_closure =
+      base::BarrierClosure(kNumTasks, run_loop.QuitClosure());
+
+  for (int i = 0; i < kNumTasks; ++i) {
+    base::ThreadPool::PostTask(FROM_HERE, base::BindOnce(
+                                              [](WebrtcEventLogData* log,
+                                                 base::RepeatingClosure done) {
+                                                for (int j = 0; j < 100; ++j) {
+                                                  log->Write("test");
+                                                  if (j % 10 == 0) {
+                                                    log->TakeLogData();
+                                                  }
+                                                }
+                                                done.Run();
+                                              },
+                                              &event_log, barrier_closure));
+  }
+
+  run_loop.Run();
 }
 
 }  // namespace remoting::protocol
