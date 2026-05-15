4a71056e149240a711568f169686f0c6dc15a7b4
Eshwar Stalin <estalin@chromium.org>
2026-05-15 11:34:39 -0700

    [VerticalTabs] Performance optimizations
    - Fixed an issue where each model update during drag is triggering an expensive toolbar view layout. This was because it was checking for selection changed instead of active tab change event
    
    - Updating visibility of child views directly instead of invalidating the layout. This helps optimize cases where there were no change in child visibility, thereby avoiding layout in those cases
    
    Bug: 501743908
    Change-Id: I63ded317dbda0945ff955b9b07e049cad184a5a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851403
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631431}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -269,7 +269,7 @@ void VerticalTabView::UpdateHovered(bool hovered) {
   }
 
   UpdateColors();
-  InvalidateLayout();
+  UpdateChildVisibilities();
 }
 
 bool VerticalTabView::IsHoverAnimationActive() const {
@@ -770,6 +770,12 @@ bool VerticalTabView::IsChildVisible(const views::View* child_view,
   NOTREACHED() << "Unknown tab child view";
 }
 
+void VerticalTabView::UpdateChildVisibilities() {
+  for (const auto& child : tab_children_configs_) {
+    child.view->SetVisible(IsChildVisible(child.view, width()));
+  }
+}
+
 views::ProposedLayout VerticalTabView::CalculateProposedLayout(
     const views::SizeBounds& size_bounds) const {
   const int width = size_bounds.width().value_or(
@@ -976,7 +982,7 @@ void VerticalTabView::OnTabStateChanged() {
   UpdateTabData(tab);
 
   UpdateColors();
-  InvalidateLayout();
+  UpdateChildVisibilities();
 }
 
 void VerticalTabView::OnTabDataChanged(TabChangeType change_type,
