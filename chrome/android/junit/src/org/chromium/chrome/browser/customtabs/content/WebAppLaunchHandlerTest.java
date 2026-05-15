fb75bdb61646a8b0b915817db6b7e64a47ef9204
Nate Chapin <japhet@chromium.org>
2026-05-15 16:59:27 -0700

    Remove grantUriPermission() permission call in WebAppLaunchHandler.launchNewIntent()
    The package should already have the permissions it needs.
    
    Fixed: 496012495
    Change-Id: I8a757f148d8823be348b2a530f8373e999129775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779798
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631646}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/WebAppLaunchHandlerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/content/WebAppLaunchHandlerTest.java
@@ -14,6 +14,7 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -22,6 +23,7 @@ import static org.robolectric.Shadows.shadowOf;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Looper;
 
@@ -373,9 +375,33 @@ public class WebAppLaunchHandlerTest {
         mExpectedFileList = new String[] {CONTENT_URI};
         doTestNavigateNewNewIntent(
                 LaunchHandlerClientMode.NAVIGATE_NEW, /* expectedStartActivityTimes= */ 1);
-        verify(mActivityMock, times(1))
+        verify(mActivityMock, never())
                 .grantUriPermission(
                         eq(TEST_PACKAGE_NAME), eq(mFileHandlingData.uris.get(0)), anyInt());
+    }
+
+    @Test
+    public void navigateNewStartNewTask_fileData_chromePrivateData() {
+        doTestNavigateNewInitialIntent(LaunchHandlerClientMode.NAVIGATE_NEW);
+
+        final Uri pwCsv =
+                Uri.parse(
+                        "content://com.android.chrome.FileProvider/passwords/"
+                                + "Chrome%20Passwords.csv");
+
+        when(mActivityMock.checkCallingUriPermission(eq(pwCsv), anyInt()))
+                .thenReturn(PackageManager.PERMISSION_DENIED);
+
+        mFileHandlingData = new FileHandlingData(Arrays.asList(Uri.parse(CONTENT_URI), pwCsv));
+        mExpectedFileList = new String[] {CONTENT_URI};
+        doTestNavigateNewNewIntent(
+                LaunchHandlerClientMode.NAVIGATE_NEW, /* expectedStartActivityTimes= */ 1);
+        verify(mActivityMock, never())
+                .grantUriPermission(
+                        eq(TEST_PACKAGE_NAME), eq(mFileHandlingData.uris.get(0)), anyInt());
+        verify(mActivityMock, never())
+                .grantUriPermission(
+                        eq(TEST_PACKAGE_NAME), eq(mFileHandlingData.uris.get(1)), anyInt());
     }
 
     @Test
@@ -407,7 +433,7 @@ public class WebAppLaunchHandlerTest {
         mExpectedFileList = new String[] {CONTENT_URI};
         doTestNavigateNewNewIntent(
                 LaunchHandlerClientMode.NAVIGATE_EXISTING, /* expectedStartActivityTimes= */ 1);
-        verify(mActivityMock, times(1))
+        verify(mActivityMock, never())
                 .grantUriPermission(
                         eq(TEST_PACKAGE_NAME), eq(mFileHandlingData.uris.get(0)), anyInt());
     }
