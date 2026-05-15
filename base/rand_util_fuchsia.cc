66fb552efd114cf05acc6173b7fb22c7c37bd41d
Erik Chen <erikchen@chromium.org>
2026-05-15 14:04:13 -0700

    Speculative revert to fix early startup crash on windows.
    This CL reverts:
    crrev.com/c/7807632
    crrev.com/c/7808465
    crrev.com/c/7796985
    
    As the most likely culprit for an M149 startup crash on windows.
    
    Bug: 513491819
    Change-Id: If8e5ad656db26d8139da592ede804d09e9b20d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853085
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631549}

--- base/rand_util_fuchsia.cc
+++ base/rand_util_fuchsia.cc
@@ -15,11 +15,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+namespace internal {
 
+namespace {
+
+// The BoringSSl helpers are duplicated in rand_util_posix.cc and
+// rand_util_win.cc.
+std::atomic<bool> g_use_boringssl;
+
+BASE_FEATURE(kUseBoringSSLForRandBytes, FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace
+
+void ConfigureBoringSSLBackedRandBytesFieldTrial() {
+  g_use_boringssl.store(FeatureList::IsEnabled(kUseBoringSSLForRandBytes),
+                        std::memory_order_relaxed);
+}
+
+bool UseBoringSSLForRandBytes() {
+  return g_use_boringssl.load(std::memory_order_relaxed);
+}
+
+}  // namespace internal
 
 void RandBytes(span<uint8_t> output) {
-  // BoringSSL's RAND_bytes always returns 1. Any error aborts the program.
-  (void)RAND_bytes(output.data(), output.size());
+  if (internal::UseBoringSSLForRandBytes()) {
+    // BoringSSL's RAND_bytes always returns 1. Any error aborts the program.
+    (void)RAND_bytes(output.data(), output.size());
+    return;
+  }
+
+  zx_cprng_draw(output.data(), output.size());
 }
 
 namespace internal {
