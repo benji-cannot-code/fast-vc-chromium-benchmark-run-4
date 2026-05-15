ad5459696cee8aeeee23e97ebe2ccab82e30ca5a
Daniel Rubery <drubery@chromium.org>
2026-05-15 10:59:00 -0700

    [bedrock] Remove Browser* from sharing_service_operation_unittest.cc
    This test doesn't actually use the Browser dependency.
    
    Fixed: 513491818
    Link: https://chromium-review.googlesource.com/id/Ib83856de37548a7bb6f3fed046f282846a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7835102
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631414}

--- chrome/browser/webshare/mac/sharing_service_operation_unittest.cc
+++ chrome/browser/webshare/mac/sharing_service_operation_unittest.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/webshare/store_file_task.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/public/browser/site_instance.h"
