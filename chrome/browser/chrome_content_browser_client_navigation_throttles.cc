af99aa2a537f87c926fbc0f842fccdc4513815b5
Aldo Culquicondor <acondor@chromium.org>
2026-05-15 13:01:23 -0700

    SlimWebview: Restore navigation throttle for allowed origins
    It was removed in https://crrev.com/c/7776058
    
    Just checkingt when creating a URLLoader would leave the origin of a
    redirect unchecked.
    
    Fixed: 513266160
    Change-Id: Ic8d086ae396ba937923931e77c7190c674d12490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852855
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631489}

--- chrome/browser/chrome_content_browser_client_navigation_throttles.cc
+++ chrome/browser/chrome_content_browser_client_navigation_throttles.cc
@@ -132,6 +132,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_GUEST_VIEW)
 #if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/browser/guest_view/web_view/web_view_guest.h"
+#else  // !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+#include "components/guest_view/browser/slim_web_view/slim_web_view_navigation_throttle.h"  // nogncheck
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #endif  // BUILDFLAG(ENABLE_GUEST_VIEW)
 
@@ -395,6 +397,12 @@ void CreateAndAddChromeThrottlesForNavigation(
 
 #if BUILDFLAG(ENABLE_GUEST_VIEW)
   extensions::WebViewGuest::MaybeCreateAndAddNavigationThrottle(registry);
+#endif  // BUILDFLAG(ENABLE_GUEST_VIEW)
+
+#else  // !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+
+#if BUILDFLAG(ENABLE_GUEST_VIEW)
+  guest_view::MaybeCreateAndAddSlimWebViewNavigationThrottle(registry);
 #endif  // BUILDFLAG(ENABLE_GUEST_VIEW)
 
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
---
+++ components/guest_view/browser/slim_web_view/slim_web_view_navigation_throttle.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_GUEST_VIEW_BROWSER_SLIM_WEB_VIEW_SLIM_WEB_VIEW_NAVIGATION_THROTTLE_H_
+#define COMPONENTS_GUEST_VIEW_BROWSER_SLIM_WEB_VIEW_SLIM_WEB_VIEW_NAVIGATION_THROTTLE_H_
+
+namespace content {
+class NavigationThrottleRegistry;
+}  // namespace content
+
+namespace guest_view {
+
+// Enforces an optional allowlist for top-level navigations in SlimWebView.
+// If the guest was created with an "allowedOrigins" allowlist, any top-level
+// HTTP(S) navigation that does not match the allowlist will be canceled.
+void MaybeCreateAndAddSlimWebViewNavigationThrottle(
+    content::NavigationThrottleRegistry& registry);
+
+}  // namespace guest_view
+
+#endif  // COMPONENTS_GUEST_VIEW_BROWSER_SLIM_WEB_VIEW_SLIM_WEB_VIEW_NAVIGATION_THROTTLE_H_
