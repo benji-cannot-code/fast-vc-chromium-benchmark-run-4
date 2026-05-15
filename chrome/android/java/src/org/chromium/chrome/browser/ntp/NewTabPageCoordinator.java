cf4666a950b2ffff584f6dae1eaab291202a1eac
Xi Han <hanxi@google.com>
2026-05-15 11:44:47 -0700

    [Composeplate] Disable AIM button on android desktop.
    In this CL, we add a new API to check whether the AIM button should be
    shown on NTP, also update the visibility of AIM/Incognito buttons
    using this new API. The new API should NOT be used to check if the
    feature AIM is enabled. Video: http://shortn/_knPCdIsofF
    
    Bug: 513009998, 513545739
    Change-Id: I742e0a09befc1e310054f301ed2fa4ed51e8a78f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851676
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631434}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
@@ -188,9 +188,9 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
     private @Nullable FeedSurfaceScrollDelegate mScrollDelegate;
     private @Nullable Callback<Logo> mOnLogoAvailableCallback;
 
-    // mIsComposeplateEnabled is null before checking whether to initialize composeplate view in
+    // mCanShowComposeplateButton is null before checking whether to initialize composeplate view in
     // NewTabPageCoordinator#initialize().
-    private @Nullable Boolean mIsComposeplateEnabled;
+    private @Nullable Boolean mCanShowComposeplateButton;
     private boolean mIsComposeplatePolicyEnabled;
     private boolean mIsComposeplateViewInitialized;
     private @Nullable Supplier<GURL> mComposeplateUrlSupplier;
@@ -361,7 +361,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         initializeSearchBoxTextView();
 
         initializeComposeplateFlags(mProfile);
-        if (assumeNonNull(mIsComposeplateEnabled)) {
+        if (assumeNonNull(mCanShowComposeplateButton)) {
             initializeComposeplate();
         }
 
@@ -395,7 +395,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         Resources resources = mActivity.getResources();
         int searchBoxHeight =
                 NtpCustomizationUtils.getSearchBoxHeight(
-                        resources, assumeNonNull(mIsComposeplateEnabled));
+                        resources, assumeNonNull(mCanShowComposeplateButton));
         if (mNtpSearchBox != null) {
             mNtpSearchBox.setHeight(searchBoxHeight);
         }
@@ -489,9 +489,9 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
     }
 
     private void initializeComposeplateFlags(Profile profile) {
-        mIsComposeplateEnabled = ComposeplateUtils.isComposeplateEnabled(profile);
+        mCanShowComposeplateButton = ComposeplateUtils.canShowComposeplateButtonOnNtp(profile);
         mIsComposeplatePolicyEnabled =
-                mIsComposeplateEnabled && ComposeplateUtils.isEnabledByPolicy(profile);
+                mCanShowComposeplateButton && ComposeplateUtils.isEnabledByPolicy(profile);
     }
 
     private void initializeComposeplate() {
@@ -750,21 +750,21 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
 
         // Skips if the flag hasn't been initialized since the initialization of the following
         // components will be called again in #initialize().
-        if (mIsComposeplateEnabled != null) {
-            // when mSearchProviderIsGoogle is changed, mIsComposeplateEnabled might be changed too,
-            // recalculate its value.
+        if (mCanShowComposeplateButton != null) {
+            // When mSearchProviderIsGoogle is changed, mCanShowComposeplateButton might be changed
+            // too, recalculate its value.
             if (isSearchProviderIsGoogleChanged) {
-                boolean previousIsComposeplateEnabled = mIsComposeplateEnabled;
+                boolean previousCanShowComposeplateButton = mCanShowComposeplateButton;
                 initializeComposeplateFlags(mProfile);
-                if (!previousIsComposeplateEnabled
-                        && mIsComposeplateEnabled
+                if (!previousCanShowComposeplateButton
+                        && mCanShowComposeplateButton
                         && mComposeplateCoordinator == null) {
                     // If the composeplate view is enabled while mComposeplateCoordinator hasn't
                     // been initialized yet, initialize it now.
                     initializeComposeplate();
                 }
 
-                if (previousIsComposeplateEnabled != mIsComposeplateEnabled) {
+                if (previousCanShowComposeplateButton != mCanShowComposeplateButton) {
                     // When the flag value is changed, the height of search box might be changed.
                     setSearchBoxHeightBoundsVerticalInset();
                     // Updates the composeplate view's visibility.
@@ -975,7 +975,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         // Skips now if the composeplate flag hasn't been initialized. This prevents logging the
         // impression metrics incorrectly due to the status of whether to show the composeplate
         // button hasn't been initialized.
-        if (mIsComposeplateEnabled == null) return;
+        if (mCanShowComposeplateButton == null) return;
 
         mNtpSearchBox.setVoiceSearchButtonVisibility(shouldShowVoiceSearchButton);
         mNtpSearchBox.setLensButtonVisibility(shouldShowLensButton);
@@ -984,7 +984,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         // visibility.
         if (mComposeplateCoordinator != null) {
             shouldShowComposeplateButton =
-                    mIsComposeplateEnabled
+                    mCanShowComposeplateButton
                             && mSearchProviderIsGoogle
                             && IncognitoUtils.isIncognitoModeEnabled(mProfile);
             mComposeplateCoordinator.setVisibility(
@@ -1421,7 +1421,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         }
 
         // If composeplate view flags haven't been initialized yet, returns now.
-        if (mIsComposeplateEnabled == null) {
+        if (mCanShowComposeplateButton == null) {
             return;
         }
 
