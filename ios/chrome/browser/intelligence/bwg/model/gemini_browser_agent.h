78eb002d1f5debd9b042e852bff12a33555987a6
Tarek Makkouk <tarekmakkouk@google.com>
2026-05-15 09:11:12 -0700

    [iOS] Implement mic auth, Gemini Live consent and seen intro delegates
    Bug: 512082728
    Change-Id: Ie107c0ad6054874eb0da8544d5e836e82ab9871b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848635
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631331}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.h
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.h
@@ -50,6 +50,7 @@ class ScopedFullscreenDisabler;
 @class GeminiPageStateChangeHandler;
 @class GeminiSessionHandler;
 @class GeminiCameraHandler;
+@class GeminiConsentProviderHandler;
 @class GeminiPageContext;
 @class GeminiViewStateChangeHandler;
 @class GeminiScrollObserver;
@@ -331,6 +332,10 @@ class GeminiBrowserAgent : public BrowserUserData<GeminiBrowserAgent>,
 
   // Handler for Gemini camera.
   __strong GeminiCameraHandler* gemini_camera_handler_ = nullptr;
+
+  // Handler for Gemini consent provider.
+  __strong GeminiConsentProviderHandler* gemini_consent_provider_handler_ =
+      nullptr;
 
   // Handler for Gemini suggestion chips.
   __strong GeminiSuggestionHandler* gemini_suggestion_handler_ = nullptr;
