06e4aca5be07bd70457a0c886353305fe283d4a2
Gazal <agazal@google.com>
2026-05-15 09:50:41 -0700

    [Vertical Tabs] Rename GridCardOnClickListenerProvider methods to be generic
    To support custom row click and group expansion interactions in Vertical
    Tabs without introducing hardcoded conditionals inside the Tab Switcher
    codebase, we rename openTabGridDialog to onTabGroupClicked.
    
    This provides semantic purity across all implementing dialogs and
    mediators while keeping click delegation fully decoupled.
    
    Bug: 513386904
    Change-Id: I4c94053e2f61ed4fd604069f647bf47666572fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850519
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631359}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinator.java
@@ -231,13 +231,13 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
             new GridCardOnClickListenerProvider() {
                 @Nullable
                 @Override
-                public TabActionListener openTabGridDialog(Tab tab) {
+                public TabActionListener onTabGroupClicked(Tab tab) {
                     return null;
                 }
 
                 @Nullable
                 @Override
-                public TabActionListener openTabGridDialog(String syncId) {
+                public TabActionListener onTabGroupClicked(String syncId) {
                     return new TabActionListener() {
                         @Override
                         public void run(
