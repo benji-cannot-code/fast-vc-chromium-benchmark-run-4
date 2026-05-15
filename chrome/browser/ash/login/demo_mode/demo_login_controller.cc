e4862fb1b37b265c805785f1347d5cbf2a3d4518
Jun Ishiguro <junis@google.com>
2026-05-14 20:52:40 -0700

    login: Pass SharedURLLoaderFactory to DemoLoginController
    Bug: b:404133029
    Change-Id: I1f42834ce14d63874ec28ffb1a4047d443ab6bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782986
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631042}

--- chrome/browser/ash/login/demo_mode/demo_login_controller.cc
+++ chrome/browser/ash/login/demo_mode/demo_login_controller.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/uuid.h"
 #include "base/values.h"
 #include "chrome/browser/ash/login/demo_mode/demo_mode_dimensions.h"
-#include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/webui/ash/login/online_login_utils.h"
 #include "chromeos/ash/components/demo_mode/utils/demo_session_utils.h"
@@ -173,10 +172,6 @@ constexpr net::NetworkTrafficAnnotationTag kCleanUpTrafficAnnotation =
               "Not implemented."
           })");
 
-scoped_refptr<network::SharedURLLoaderFactory> GetUrlLoaderFactory() {
-  return g_browser_process->shared_url_loader_factory();
-}
-
 GURL GetDemoModeServerBaseUrl() {
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   return GURL(
@@ -253,6 +248,7 @@ std::unique_ptr<network::SimpleURLLoader> CreateDemoAccountURLLoader(
 // Send demo account related http requests to server. i.e. setup request,
 // cleanup request.
 void SendDemoAccountRequest(
+    network::mojom::URLLoaderFactory& url_loader_factory,
     const base::DictValue& post_data,
     network::SimpleURLLoader* url_loader,
     base::OnceCallback<void(std::optional<std::string> response_body)>
@@ -265,7 +261,7 @@ void SendDemoAccountRequest(
   CHECK(base::JSONWriter::Write(post_data, &request_string));
   url_loader->AttachStringForUpload(request_string, kContentTypeJSON);
   url_loader->SetTimeoutDuration(kDemoAccountRequestTimeout);
-  url_loader->DownloadToString(GetUrlLoaderFactory().get(), std::move(callback),
+  url_loader->DownloadToString(&url_loader_factory, std::move(callback),
                                kMaxResponseSize);
 }
 
@@ -374,11 +370,14 @@ base::DictValue GetDeviceInfo(PrefService& local_state) {
 
 DemoLoginController::DemoLoginController(
     PrefService* local_state,
+    scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory,
     policy::DeviceCloudPolicyManagerAsh* device_cloud_policy_manager_ash,
     base::RepeatingClosure configure_auto_login_callback)
     : local_state_(CHECK_DEREF(local_state)),
+      shared_url_loader_factory_(std::move(shared_url_loader_factory)),
       device_cloud_policy_manager_ash_(device_cloud_policy_manager_ash),
       configure_auto_login_callback_(std::move(configure_auto_login_callback)) {
+  CHECK(shared_url_loader_factory_);
   if (!device_cloud_policy_manager_ash_) {
     CHECK_IS_TEST();
     state_ = State::kReadyForLoginWithDemoAccount;
@@ -484,7 +483,7 @@ void DemoLoginController::SendSetupDemoAccountRequest() {
                                  kSetupAccountTrafficAnnotation);
 
   SendDemoAccountRequest(
-      post_data, url_loader_.get(),
+      *shared_url_loader_factory_.get(), post_data, url_loader_.get(),
       base::BindOnce(&DemoLoginController::OnSetupDemoAccountComplete,
                      weak_ptr_factory_.GetWeakPtr(), sign_in_scoped_device_id));
 }
@@ -664,7 +663,7 @@ void DemoLoginController::MaybeCleanupPreviousDemoAccount() {
                                  kCleanUpTrafficAnnotation);
 
   SendDemoAccountRequest(
-      post_data, url_loader_.get(),
+      *shared_url_loader_factory_.get(), post_data, url_loader_.get(),
       base::BindOnce(&DemoLoginController::OnCleanUpDemoAccountComplete,
                      weak_ptr_factory_.GetWeakPtr()));
 }
--- chrome/browser/ash/login/demo_mode/demo_login_controller.h
+++ chrome/browser/ash/login/demo_mode/demo_login_controller.h
@@ -13,12 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/policy/core/device_cloud_policy_manager_ash.h"
 #include "components/policy/core/common/cloud/cloud_policy_manager.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 
 class PrefService;
+
+namespace network {
+class SharedURLLoaderFactory;
+}  // namespace network
 
 namespace ash {
 
@@ -55,10 +60,12 @@ class DemoLoginController
   using ResultCode = DemoSessionMetricsRecorder::DemoAccountRequestResultCode;
 
   // `local_state` must be non-null and must outlive `this`.
+  // `shared_url_loader_factory` must be non-null.
   // `device_cloud_policy_manager_ash` may be null in unit tests, and must
   // outlive `this` if non-null
   DemoLoginController(
       PrefService* local_state,
+      scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory,
       policy::DeviceCloudPolicyManagerAsh* device_cloud_policy_manager_ash,
       base::RepeatingClosure auto_login_mgs_callback);
   DemoLoginController(const DemoLoginController&) = delete;
@@ -124,6 +131,8 @@ class DemoLoginController
   void OnPolicyManagerConnectionTimeOut();
 
   const raw_ref<PrefService> local_state_;
+  const scoped_refptr<network::SharedURLLoaderFactory>
+      shared_url_loader_factory_;
   const raw_ptr<policy::DeviceCloudPolicyManagerAsh>
       device_cloud_policy_manager_ash_;
 
