1e557ab1a4f8417213f0c6efb3867b7ecb915f11
Shunya Shishido <sisidovski@chromium.org>
2026-05-14 22:10:26 -0700

    PerformanceObserver: Store policy on commit
    This CL updates `DeclarativePerformanceObserver` to store the parsed
    policy details in its member variables when a navigation commits.
    
    - Added member variables to store the reporting endpoint and enabled
      entry types.
    - Populated these members in `OnCommit`.
    - Added a unit test to verify that policy details are correctly stored.
    
    Bug: 505208781
    Change-Id: I4cb74f1d66867cb2ffb2e7f41327f64225e61b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828859
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631071}

--- components/page_load_metrics/browser/observers/declarative_performance_observer.h
+++ components/page_load_metrics/browser/observers/declarative_performance_observer.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PAGE_LOAD_METRICS_BROWSER_OBSERVERS_DECLARATIVE_PERFORMANCE_OBSERVER_H_
 #define COMPONENTS_PAGE_LOAD_METRICS_BROWSER_OBSERVERS_DECLARATIVE_PERFORMANCE_OBSERVER_H_
 
+#include "base/containers/flat_set.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
+#include "services/network/public/mojom/declarative_performance_observer.mojom.h"
 
 namespace page_load_metrics {
 
@@ -31,6 +33,18 @@ class DeclarativePerformanceObserver : public PageLoadMetricsObserver {
   ObservePolicy OnPrerenderStart(content::NavigationHandle* navigation_handle,
                                  const GURL& currently_committed_url) override;
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle) override;
+
+  const std::string& reporting_endpoint_for_testing() const {
+    return reporting_endpoint_;
+  }
+  const base::flat_set<network::mojom::PerformanceEntryType>&
+  enabled_types_for_testing() const {
+    return enabled_types_;
+  }
+
+ private:
+  std::string reporting_endpoint_;
+  base::flat_set<network::mojom::PerformanceEntryType> enabled_types_;
 };
 
 }  // namespace page_load_metrics
