42f5363b31b81ce72f07984cf27e4d36381b18d3
Caroline Rising <corising@chromium.org>
2026-05-15 13:08:26 -0700

    Remove unused chrome labs responsive logic from ToolbarController.
    The chrome labs responsive overflow logic is now handled by the pinned
    toolbar actions container.
    
    Change-Id: Id82be84c2fb2fe3ef969fff73cfcf2cce5593e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851765
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631498}

--- chrome/browser/ui/views/toolbar/toolbar_controller.cc
+++ chrome/browser/ui/views/toolbar/toolbar_controller.cc
@@ -293,13 +293,6 @@ ToolbarController::GetDefaultResponsiveElements(Browser* browser) {
                kToolbarBatterySaverBubbleElementId),
            /*is_section_end=*/false),
        ToolbarController::ResponsiveElementInfo(
-           ToolbarController::ElementIdInfo(kToolbarChromeLabsButtonElementId,
-                                            IDS_OVERFLOW_MENU_ITEM_TEXT_LABS,
-                                            &kScienceOldIcon,
-                                            kToolbarChromeLabsButtonElementId,
-                                            kToolbarChromeLabsBubbleElementId),
-           /*is_section_end=*/false),
-       ToolbarController::ResponsiveElementInfo(
            ToolbarController::ElementIdInfo(
                kToolbarMediaButtonElementId,
                IDS_OVERFLOW_MENU_ITEM_TEXT_MEDIA_CONTROLS,
@@ -320,10 +313,9 @@ ToolbarController::GetDefaultResponsiveElements(Browser* browser) {
 std::vector<ui::ElementIdentifier>
 ToolbarController::GetDefaultOverflowOrder() {
   return std::vector<ui::ElementIdentifier>(
-      {kToolbarHomeButtonElementId, kToolbarChromeLabsButtonElementId,
-       kToolbarMediaButtonElementId, kToolbarForwardButtonElementId,
-       kToolbarBatterySaverButtonElementId, kToolbarAvatarButtonElementId,
-       kToolbarSplitTabsToolbarButtonElementId,
+      {kToolbarHomeButtonElementId, kToolbarMediaButtonElementId,
+       kToolbarForwardButtonElementId, kToolbarBatterySaverButtonElementId,
+       kToolbarAvatarButtonElementId, kToolbarSplitTabsToolbarButtonElementId,
        ContextualTasksButton::kContextualTasksToolbarButton});
 }
 
@@ -336,7 +328,6 @@ std::string ToolbarController::GetActionNameFromElementIdentifier(
       identifier_to_action_name_map(
           {{kToolbarAvatarButtonElementId, "AvatarButton"},
            {kToolbarBatterySaverButtonElementId, "BatterySaverButton"},
-           {kToolbarChromeLabsButtonElementId, "ChromeLabsButton"},
            {kExtensionsMenuButtonElementId, "ExtensionsMenuButton"},
            {kToolbarForwardButtonElementId, "ForwardButton"},
            {kToolbarHomeButtonElementId, "HomeButton"},
