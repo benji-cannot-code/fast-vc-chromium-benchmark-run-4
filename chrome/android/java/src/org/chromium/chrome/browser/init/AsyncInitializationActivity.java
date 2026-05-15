70656c1d06932b91f96360dc7ce2e2145cb998c2
Kameron Lutes <kalutes@google.com>
2026-05-15 16:27:32 -0700

    [Android] Refactor self occlusion tracking to track the entire lifetime of windows
    Currently, WindowZOrderTracker untracks windows when activities stop.
    Removing stopped windows from the Z-order list causes Z-order history to
    be lost upon device lock and unlock.
    
    This CL refactors WindowZOrderTracker and WindowOcclusionTracker to
    track the entire lifetime of window instances (from onCreate to
    onDestroy) rather than tying tracking to activity start/stop cycles.
    ActivityWindowAndroid instances are stored directly in mZOrder, and
    WindowOcclusionTracker checks window.getActivityState() directly to
    determine whether a window is active for occlusion calculations. Newly
    created windows are added to mZOrder upon creation and remain tracked
    until explicitly untracked upon activity destruction.
    
    Bug: 510609140
    Test: unit tests
    Change-Id: I9af56a03b2912dd1681863db3f5626dfca7c450d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828907
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Auto-Submit: Kameron Lutes <kalutes@google.com>
    Commit-Queue: Kameron Lutes <kalutes@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631627}

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -157,6 +157,9 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
         // mLifecycleDispatcher.dispatchOnDestroy() because objects subscribing to
         // mLifecycleDispatcher's onDestroy events may have references to ActivityWindowAndroid.
         if (mWindowAndroid != null) {
+            if (isSelfOcclusionTrackingEnabled()) {
+                WindowOcclusionTracker.getInstance().untrack(mWindowAndroid);
+            }
             mWindowAndroid.destroy();
             mWindowAndroid = null;
         }
@@ -450,6 +453,9 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
         mSavedInstanceState = savedInstanceState;
 
         mWindowAndroid = createWindowAndroid();
+        if (isSelfOcclusionTrackingEnabled()) {
+            WindowOcclusionTracker.getInstance().track(mWindowAndroid);
+        }
         mIntentRequestTracker.restoreInstanceState(getSavedInstanceState());
         mProfileProviderSupplier = createProfileProvider();
         if (getSupportedProfileType() == SupportedProfileType.OFF_THE_RECORD) {
@@ -653,10 +659,6 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
                             + getClass().getName()
                             + " is trying to start.");
         }
-
-        if (isSelfOcclusionTrackingEnabled()) {
-            WindowOcclusionTracker.getInstance().track(mWindowAndroid);
-        }
     }
 
     @EnsuresNonNullIf("mWindowAndroid")
@@ -701,10 +703,6 @@ public abstract class AsyncInitializationActivity extends ChromeBaseAppCompatAct
     public void onStop() {
         super.onStop();
         mNativeInitializationController.onStop();
-
-        if (isSelfOcclusionTrackingEnabled()) {
-            WindowOcclusionTracker.getInstance().untrack(mWindowAndroid);
-        }
     }
 
     @CallSuper
