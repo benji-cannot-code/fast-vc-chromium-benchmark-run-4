7050aad053d765018c33f39fee120ddb62473081
Wan-Teh Chang <wtc@google.com>
2026-05-15 16:42:51 -0700

    Roll src/third_party/dav1d/libdav1d/ 5cfc38326..1718ff9ad (11 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/5cfc3832687e..1718ff9aded9
    
    $ git log 5cfc38326..1718ff9ad --date=short --no-merges --format='%ad %ae %s'
    2026-03-19 ns.afsan riscv64/ipred_h: Implement ipred_h in RISC-V asm
    2026-05-15 martin aarch64: Fix a name mismatch in a macro error message
    2026-03-18 ns.afsan riscv64/ipred_v: Remove redundent vxrm set instr
    2026-05-10 ns.afsan riscv64/cdef: Fix up code style
    2026-04-24 Arpad.Panyik AArch64: Optimize ipred_smooth_v_8bpc_neon
    2026-04-24 Arpad.Panyik AArch64: Optimize ipred_smooth_h_8bpc_neon
    2026-05-01 martin arm: Fix up code style slightly
    2026-05-05 martin ci: Update the main CI image
    2026-05-06 martin examples: Treat SDL2 headers as system headers
    2026-05-03 martin ci: Add testing on macOS on Apple Silicon too
    2026-05-03 martin checkasm: Readd a dependency on threads
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I772557c01b4d5e8130ad49aa0934caa0992aebd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853143
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631635}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-35-g5cfc3832"
+#define DAV1D_VERSION "1.5.3-46-g1718ff9a"
