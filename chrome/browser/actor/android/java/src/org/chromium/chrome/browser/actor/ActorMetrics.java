2a749db3501d9bb595297bea8b441e7e4fb442f3
Bhuvana Betini <bbetini@google.com>
2026-05-14 18:03:45 -0700

    [Actor] Adds Metrics for ExitReason and Duration for PiP
    Bug: 505134671
    Change-Id: I937793055729057189a273116980e190c1be232b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838132
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630962}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorMetrics.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorMetrics.java
@@ -17,6 +17,8 @@ import java.lang.annotation.RetentionPolicy;
 @NullMarked
 public class ActorMetrics {
 
+    // LINT.IfChange(ActorPipStatus)
+
     @IntDef({ActorPipStatus.ENTERED, ActorPipStatus.EXITED})
     @Retention(RetentionPolicy.SOURCE)
     public @interface ActorPipStatus {
@@ -24,6 +26,27 @@ public class ActorMetrics {
         int EXITED = 1;
         int NUM_ENTRIES = 2;
     }
+
+    // LINT.ThenChange(//chrome/browser/actor/actor_metrics.cc:ActorPipStatus)
+
+    // LINT.IfChange(ActorPipExitReason)
+
+    @IntDef({
+        ActorPipExitReason.CLOSE,
+        ActorPipExitReason.EXPAND,
+        ActorPipExitReason.COMPLETED,
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ActorPipExitReason {
+        int CLOSE = 0;
+        int EXPAND = 1;
+        int COMPLETED = 2;
+        int NUM_ENTRIES = 3;
+    }
+
+    // LINT.ThenChange(//chrome/browser/actor/actor_metrics.cc:ActorPipExitReason)
+
+    // LINT.IfChange(ActorPipUserInteraction)
 
     @IntDef({
         ActorPipUserInteraction.PAUSE,
@@ -38,6 +61,8 @@ public class ActorMetrics {
         int NUM_ENTRIES = 3;
     }
 
+    // LINT.ThenChange(//chrome/browser/actor/actor_metrics.cc:ActorPipUserInteraction)
+
     @IntDef({
         ActorPauseResumeSource.PIP,
     })
@@ -50,6 +75,17 @@ public class ActorMetrics {
     public static void recordPipStatus(@ActorPipStatus int status) {
         RecordHistogram.recordEnumeratedHistogram(
                 "Actor.Pip.Status", status, ActorPipStatus.NUM_ENTRIES);
+    }
+
+    /** Records the PiP exit reason. */
+    public static void recordPipExitReason(@ActorPipExitReason int reason) {
+        RecordHistogram.recordEnumeratedHistogram(
+                "Actor.Pip.ExitReason", reason, ActorPipExitReason.NUM_ENTRIES);
+    }
+
+    /** Records the PiP duration. */
+    public static void recordPipDuration(long durationMs) {
+        RecordHistogram.recordLongTimesHistogram("Actor.Pip.Duration", durationMs);
     }
 
     /** Records an interaction with the PiP window. */
