5aa2c67c6d016a35426956a5ac151e4201b81540
Mike West <mkwst@chromium.org>
2026-05-15 08:19:13 -0700

    Revert "Verify transient user activation in UpdateUserGestureCarryoverInfo"
    This reverts commit 09bdb6eb33866ba0bd8caba35400059d4f4f2a5b.
    
    Reason for revert: WebView. :( https://issues.chromium.org/issues/513155689
    
    Original change's description:
    > Verify transient user activation in UpdateUserGestureCarryoverInfo
    >
    > UpdateUserGestureCarryoverInfo is an Android-only Mojo message that
    > starts a 15-second window in which navigations are treated as
    > user-initiated.
    >
    > This CL adds a browser-side check to verify that a transient user
    > activation exists before trusting the renderer's request. If no
    > activation is found, the renderer is terminated with a bad message.
    >
    > Bug: 511729510
    > Change-Id: Iae26c9cd50b8d2aeae5a3f9b9e5eab4fa8d303fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830736
    > Commit-Queue: Mike West <mkwst@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1629769}
    
    Bug: 511729510, 513155689
    Bug: 511729510
    Change-Id: Icdbd1bca08a48d0ea743e0dd40802d0e11bad420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849380
    Commit-Queue: Mike West <mkwst@chromium.org>
    Owners-Override: Mike West <mkwst@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1631289}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -8690,11 +8690,6 @@ void RenderFrameHostImpl::UpdateUserGestureCarryoverInfo() {
   if (!IsActive()) {
     return;
   }
-  if (!HasTransientUserActivation()) {
-    bad_message::ReceivedBadMessage(
-        GetProcess(), bad_message::RFH_NO_TRANSIENT_USER_ACTIVATION);
-    return;
-  }
   delegate_->UpdateUserGestureCarryoverInfo();
 }
 #endif
