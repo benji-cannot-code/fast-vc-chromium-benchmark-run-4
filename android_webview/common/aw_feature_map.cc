34a7762649f269dfd572ecbc4334a779b392b23f
Joanne de Abreu <jdeabreu@google.com>
2026-05-15 05:52:15 -0700

    Expose WebViewNavigate feature to Java
    This change exposes the kWebViewNavigate Feature to Java. The feature was previously added, but not exposed to the WebView Java code where it is used.
    
    Bug: 513505521
    Change-Id: I3fb12f33a351f1bd88dfd13cd1d8889cae98e81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852058
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631218}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -60,6 +60,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewLazyFetchHandWritingIcon,
     &features::kWebViewMixedContentAutoupgrades,
     &features::kWebViewMoveWorkToProviderInit,
+    &features::kWebViewNavigate,
     &features::kWebViewOptInToGmsBindServiceOptimization,
     &features::kWebViewPassNullFaviconToOnPageStarted,
     &features::kWebViewPersistHttpServerProperties,
