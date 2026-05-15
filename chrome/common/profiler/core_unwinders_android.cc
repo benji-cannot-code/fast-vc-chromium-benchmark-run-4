50b56a2fdce3266875b713a8aec03834d646e01e
Dibyajyoti Pal <dibyapal@chromium.org>
2026-05-15 10:31:21 -0700

    Revert "Android: Fix #ifdefs that rely on kStartOfText"
    This reverts commit 63c1cca7212b0d254de9b831693a4fd146b892ff.
    
    Reason for revert: Broke android-arm64-archive-rel bots, making
    tree red. Please see build with failed compilation [1] for more
    info.
    
    [1] https://ci.chromium.org/ui/p/chromium/builders/ci/android-arm64-archive-rel/29543/overview
    
    Original change's description:
    > Android: Fix #ifdefs that rely on kStartOfText
    >
    > In service of use_mold=true for arm32
    >
    > Bug: 40208899
    > Change-Id: I83fb53fd9ae0d8345dc50828f70d1a9d0de57d1f
    > Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831101
    > Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1631365}
    
    Bug: 40208899
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8efc5b47029187bc21b07ea07df5daa468214e0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851468
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631387}

--- chrome/common/profiler/core_unwinders_android.cc
+++ chrome/common/profiler/core_unwinders_android.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <vector>
 
-#include "base/android/library_loader/anchor_functions_buildflags.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/feature_list.h"
@@ -27,8 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sampling_profiler/process_type.h"
 #include "components/version_info/channel.h"
 
-#if defined(ARCH_CPU_ARMEL) && BUILDFLAG(ENABLE_ARM_CFI_TABLE) && \
-    BUILDFLAG(SUPPORTS_CODE_ORDERING)
+#if defined(ARCH_CPU_ARMEL) && BUILDFLAG(ENABLE_ARM_CFI_TABLE)
 #define ARM32_UNWINDING_SUPPORTED 1
 #else
 #define ARM32_UNWINDING_SUPPORTED 0
