4a44d6f8c6b1f32a8951c02146c406659fca557e
Ananya Seelam <ananyaseelam@google.com>
2026-05-14 18:21:23 -0700

    [context-input] Add killswitch for gating tab chips to tab coins change
    This flag will only gate removing existing tab chips in the composebox,
    and adding tab favicon coins next to the "+" button in the
    composebox.
    
    Fixed: b:513234673
    Change-Id: I9b4cfccc814e32410b85583873f4760ebc16128f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849780
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Wenhu Cheng <wenhuc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630972}

--- components/omnibox/common/composebox_features.cc
+++ components/omnibox/common/composebox_features.cc
@@ -8,5 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace omnibox {
 
 BASE_FEATURE(kContextManagementInComposebox, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kTabFaviconChipsToCoins, base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace omnibox
