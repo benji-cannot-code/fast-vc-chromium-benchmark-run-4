0aacef1e16ece1f129a1ab39b77423d088fddae3
jatinmittal <jatinmittal@microsoft.com>
2026-05-14 23:10:02 -0700

    Migrate EmptyBody500CommitsErrorPage to ExpectationHandler
    Replaces ControllableHttpResponse with ExpectationHandler in
    NavigationControllerBrowserTestNoServer.EmptyBody500CommitsErrorPage.
    
    The test only serves a fixed static HTML response and does not require
    dynamic or delayed response handling. This simplifies and improves test
    reliability while preserving existing behavior.
    
    Bug: 511876973
    Change-Id: I56289c82274468076dcc9167c12bbb1a8d89ec83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849453
    Reviewed-by: Prashant Nevase <pnevase@microsoft.com>
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Commit-Queue: Prashant Nevase <pnevase@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1631099}

--- content/browser/renderer_host/navigation_controller_impl_browsertest.cc
+++ content/browser/renderer_host/navigation_controller_impl_browsertest.cc
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/controllable_http_response.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/expectation_handler.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/url_request/url_request_failed_job.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -17106,8 +17107,9 @@ IN_PROC_BROWSER_TEST_P(NavigationControllerBrowserTest,
 // result in an error page.
 IN_PROC_BROWSER_TEST_P(NavigationControllerBrowserTestNoServer,
                        EmptyBody500CommitsErrorPage) {
-  net::test_server::ControllableHttpResponse response(embedded_test_server(),
-                                                      "/title1.html");
+  net::test_server::ExpectationHandler handler(embedded_test_server());
+  handler.OnRequest("/title1.html")
+      .RespondWith(net::HTTP_INTERNAL_SERVER_ERROR);
   ASSERT_TRUE(embedded_test_server()->Start());
   NavigationControllerImpl& controller =
       static_cast<NavigationControllerImpl&>(contents()->GetController());
@@ -17116,9 +17118,6 @@ IN_PROC_BROWSER_TEST_P(NavigationControllerBrowserTestNoServer,
   GURL url(embedded_test_server()->GetURL("/title1.html"));
   TestNavigationObserver observer(contents());
   shell()->LoadURL(url);
-  response.WaitForRequest();
-  response.Send(net::HTTP_INTERNAL_SERVER_ERROR);
-  response.Done();
   observer.Wait();
 
   // The navigation fails and commits a 500 error page.
