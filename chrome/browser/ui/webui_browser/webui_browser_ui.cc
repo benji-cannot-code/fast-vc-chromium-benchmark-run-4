bb8c28c5713062c5d97f4cbf46cd6d97b3dd8fdb
Keren Zhu <kerenzhu@chromium.org>
2026-05-15 14:20:34 -0700

    Fix crash on realbox query submission in WebUI Browser
    Top-chrome WebContents was not registered in the embedding context,
    causing ContextualSearchboxHandler::OpenUrl to dereference a null active
    tab.
    
    Fixed by registering BrowserWindowInterface in WebUIBrowserUI, and added
    a browser test to verify the fix.
    
    Fixed: 513278994
    Change-Id: Ib802d856f7087df6e683d7fc2521b22afa5af3d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851623
    Reviewed-by: Ryan Akiyama <ryanakiyama@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631560}

--- chrome/browser/ui/webui_browser/webui_browser_ui.cc
+++ chrome/browser/ui/webui_browser/webui_browser_ui.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui_browser/webui_browser.h"
 #include "chrome/browser/ui/webui_browser/webui_browser_page_handler.h"
 #include "chrome/browser/ui/webui_browser/webui_browser_side_panel_ui.h"
+#include "chrome/browser/ui/webui_browser/webui_browser_window.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
@@ -93,6 +94,8 @@ WebUIBrowserUI::WebUIBrowserUI(content::WebUI* web_ui)
       WebUIBrowserWindow::FromWebShellWebContents(web_ui->GetWebContents());
   Profile* profile = Profile::FromWebUI(web_ui);
   browser_ = webui_browser_window->browser();
+
+  webui::SetBrowserWindowInterface(web_ui->GetWebContents(), browser_);
 
   // Set up the chrome://webui-browser source.
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
