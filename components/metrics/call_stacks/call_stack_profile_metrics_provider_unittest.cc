8d286e39dd6775e84ada2d653eea7d2669972eda
Chris Fredrickson <cfredric@chromium.org>
2026-05-15 13:31:19 -0700

    Regenerate metrics_proto files and fix build
    This refreshes the exported files from google3 (after cl/916116649) and
    fixes up a few places where the build broke as a result of the path
    changes.
    
    Bug: b:495471385
    Change-Id: Ie8b130015f1a442bfb743a7ea291d7aa33f53fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849767
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631518}

--- components/metrics/call_stacks/call_stack_profile_metrics_provider_unittest.cc
+++ components/metrics/call_stacks/call_stack_profile_metrics_provider_unittest.cc
@@ -9,11 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_feature_list.h"
 #include "components/metrics/public/mojom/call_stack_profile_collector.mojom.h"
-#include "execution_context.pb.h"
 #include "mojo/public/cpp/base/proto_wrapper.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/metrics_proto/chrome_user_metrics_extension.pb.h"
+#include "third_party/metrics_proto/execution_context.pb.h"
 
 namespace metrics {
 
