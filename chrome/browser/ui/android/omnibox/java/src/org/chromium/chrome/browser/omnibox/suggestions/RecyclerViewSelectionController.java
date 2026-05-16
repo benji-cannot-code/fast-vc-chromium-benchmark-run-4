cca7d95a7571c47e0c2aa54b0193e1ec1f440562
Tomasz Wiszkowski <ender@google.com>
2026-05-15 17:34:56 -0700

    Reland "Dont preselect suggestions in zero prefix context."
    This reverts commit e3afeffd9abc975e9a00341623c65d0f63c5475d.
    
    Reason for revert: Fixed a call refactored to another class.
    
    Original change's description:
    > Revert "Dont preselect suggestions in zero prefix context."
    >
    > This reverts commit 221bf928ae6f2c0eb4c6f2455b8ce97c32895be9.
    >
    > Reason for revert: Causes build failures on Android ARM64 and closes
    the tree. Please see failed build for more information [1].
    >
    > [1]
    https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20arm64%20Builder%20All%20Targets%20%28dbg%29/74036/overview
    >
    > Original change's description:
    > > Dont preselect suggestions in zero prefix context.
    > >
    > > This change differentiates zero- and non-zero-prefix context
    > > suggestion preselection on desktop devices. In zero-prefix context
    > > no suggestionst are pre-selected; in prefixed context the first
    > > suggestion is pre-selected
    > >
    > > Skipping most of the CQ because prior patchset already passed these
    > > tests. Fixed tests impacting android-desktop-x64 and re-running the
    > > suite just on that bot.
    > >
    > > Change-Id: I99fd4cbea65bed2315c9c399186989ae04b210e1
    > > No-Try: true
    > > Reviewed-on:
    https://chromium-review.googlesource.com/c/chromium/src/+/7850706
    > > Reviewed-by: Charles Cai <charlesyc@google.com>
    > > Reviewed-by: Sky Malice <skym@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1631516}
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I728b8de70eaf76cb38082af40968d10ee86fb396
    > Reviewed-on:
    https://chromium-review.googlesource.com/c/chromium/src/+/7853626
    > Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com
    <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Dibyajyoti Pal <dibyapal@chromium.org>
    > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1631604}
    
    Change-Id: Idb7a96adf1b9b26b353b0110443c2e46b76a22d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853917
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631672}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/RecyclerViewSelectionController.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/RecyclerViewSelectionController.java
@@ -31,7 +31,7 @@ public class RecyclerViewSelectionController extends SelectionController
 
     @Override
     protected int getItemCount() {
-        return mLayoutManager.getItemCount();
+        return mLayoutManager == null ? 0 : mLayoutManager.getItemCount();
     }
 
     @Override
