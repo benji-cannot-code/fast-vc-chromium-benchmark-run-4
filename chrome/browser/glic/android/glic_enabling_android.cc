f9772e8f66d6cc081bd1dd573d8c41d4e5d3bbf0
Alexis Wu <alexiswu@google.com>
2026-05-15 17:48:06 -0700

    [GlicEnterprise] Enterprise UI in Glic Settings
    Screenshot1: https://screenshot.googleplex.com/Gkmcqn4Y64ofifL
    Screenshot2: https://screenshot.googleplex.com/3KHBiDNCnkhXBRL
    Screenshot3: https://screenshot.googleplex.com/BTBd7hWc23rA3Mx
    
    Fixed: b:482388543
    Change-Id: I484dd6e760df54602ef9a9f76c70f7c1e3f0a60d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782662
    Commit-Queue: Alexis Wu <alexiswu@google.com>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631680}

--- chrome/browser/glic/android/glic_enabling_android.cc
+++ chrome/browser/glic/android/glic_enabling_android.cc
@@ -4,8 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chrome/browser/glic/public/glic_enabling.h"
 
+#include "chrome/browser/enterprise/browser_management/browser_management_service.h"
+#include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/glic/android/jni_headers/GlicEnabling_jni.h"
 #include "chrome/browser/profiles/profile.h"
+
 namespace glic {
 bool JNI_GlicEnabling_IsEnabledByFlags(JNIEnv* env) {
   return GlicEnabling::IsEnabledByGlobalCriteria();
@@ -22,6 +25,17 @@ bool JNI_GlicEnabling_ShouldShowSettingsPage(JNIEnv* env, Profile* profile) {
 bool JNI_GlicEnabling_IsReadyForProfile(JNIEnv* env, Profile* profile) {
   return GlicEnabling::IsReadyForProfile(profile);
 }
+
+bool JNI_GlicEnabling_IsDisabledByPolicy(JNIEnv* env, Profile* profile) {
+  return GlicEnabling::EnablementForProfile(profile).DisallowedByAdmin();
+}
+
+bool JNI_GlicEnabling_IsProfileManaged(JNIEnv* env, Profile* profile) {
+  policy::ManagementService* management_service =
+      policy::ManagementServiceFactory::GetForProfile(profile);
+  return management_service && management_service->IsManaged();
+}
+
 void JNI_GlicEnabling_SetBypassEnablementChecksForTesting(JNIEnv* env,
                                                           bool bypass) {
   GlicEnabling::SetBypassEnablementChecksForTesting(bypass);
