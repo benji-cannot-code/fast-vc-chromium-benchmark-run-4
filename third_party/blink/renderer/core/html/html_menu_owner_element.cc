4a329b751e01a9517f448ff77f2d418473a9da66
Joey Arhar <jarhar@chromium.org>
2026-05-15 22:41:36 -0700

    Move MenuItemList to shared template class
    This deduplicates the code shared between OptionList and MenuItemList.
    
    Fixed: 422940462
    Change-Id: I8e0349ad8dcb0fba6a78b271d80a30788f413fd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810239
    Reviewed-by: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631729}

--- third_party/blink/renderer/core/html/html_menu_owner_element.cc
+++ third_party/blink/renderer/core/html/html_menu_owner_element.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/popover_data.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
+#include "third_party/blink/renderer/core/html/html_hr_element.h"
+#include "third_party/blink/renderer/core/html/html_menu_bar_element.h"
 #include "third_party/blink/renderer/core/html/html_menu_item_element.h"
-#include "third_party/blink/renderer/core/html/menu_item_list.h"
+#include "third_party/blink/renderer/core/html/html_menu_list_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 
@@ -30,6 +32,13 @@ bool HTMLMenuOwnerElement::IsValidBuiltinCommand(HTMLElement& invoker,
 
 MenuItemList HTMLMenuOwnerElement::ItemList() const {
   return MenuItemList(*this);
+}
+
+bool HTMLMenuOwnerElement::ShouldIgnoreDescendantsForElementTraversals(
+    Element* element) const {
+  // TODO: fieldset owner can be a menulist.
+  return IsA<HTMLMenuBarElement>(element) ||
+         IsA<HTMLMenuListElement>(element) || IsA<HTMLHRElement>(element);
 }
 
 void HTMLMenuOwnerElement::DefaultEventHandler(Event& event) {
--- third_party/blink/renderer/core/html/html_menu_owner_element.h
+++ third_party/blink/renderer/core/html/html_menu_owner_element.h
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_MENU_OWNER_ELEMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_HTML_MENU_OWNER_ELEMENT_H_
 
+#include "third_party/blink/renderer/core/html/forms/option_list.h"
 #include "third_party/blink/renderer/core/html/forms/type_ahead.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 
 namespace blink {
-
-class MenuItemList;
 
 class CORE_EXPORT HTMLMenuOwnerElement : public HTMLElement,
                                          public TypeAheadDataSource {
@@ -21,6 +20,8 @@ class CORE_EXPORT HTMLMenuOwnerElement : public HTMLElement,
                              CommandEventType command) override;
   // This returns an iterable list of menuitems whose owner is this.
   MenuItemList ItemList() const;
+
+  bool ShouldIgnoreDescendantsForElementTraversals(Element* element) const;
 
   void DefaultEventHandler(Event&) override;
 
