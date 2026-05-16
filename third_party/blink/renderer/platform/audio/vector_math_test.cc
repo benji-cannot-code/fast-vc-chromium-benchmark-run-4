f2bb785cd6f9cc3ced9d325b04f1ecb8e74ef216
Michael Wilson <mjwilson@chromium.org>
2026-05-16 10:11:26 -0700

    [WebAudio] Spanify vector_math
    This should cause no functional change.
    
    Bug: 375449662
    Change-Id: Id94fd6ef014a79a3705922746f45e09f7213839f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7704969
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Mahesh Kannan <kmaheshb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631761}

--- third_party/blink/renderer/platform/audio/vector_math_test.cc
+++ third_party/blink/renderer/platform/audio/vector_math_test.cc
@@ -299,9 +299,9 @@ TEST_F(VectorMathTest, Conv) {
       for (auto& dest : GetSecondaryVectors(
                GetDestination(1u), source.memory_layout(), frames_to_process)) {
         AudioFloatArray prepared_filter;
-        PrepareFilterForConv(reversed_filter.as_span(), &prepared_filter);
+        PrepareFilterForConv(reversed_filter.as_span(), prepared_filter);
         Conv(source.as_span(), reversed_filter.as_span(), dest.as_span(),
-             frames_to_process, &prepared_filter);
+             frames_to_process, prepared_filter);
         for (size_t i = 0u; i < frames_to_process; ++i) {
           EXPECT_NEAR(expected_dest[i], dest[i],
                       1e-3 * std::abs(expected_dest[i]));
@@ -319,7 +319,8 @@ TEST_F(VectorMathTest, Vadd) {
         expected_dest[i] = source1[i] + source2[i];
       }
       for (auto& dest : GetSecondaryVectors(GetDestination(1u), source1)) {
-        Vadd(source1.p(), source2.p(), dest.p(), source1.size());
+        Vadd(source1.as_span(), source2.as_span(), dest.as_span(),
+             source1.size());
         EXPECT_EQ(expected_dest, dest);
       }
     }
@@ -334,7 +335,8 @@ TEST_F(VectorMathTest, Vsub) {
         expected_dest[i] = source1[i] - source2[i];
       }
       for (auto& dest : GetSecondaryVectors(GetDestination(1u), source1)) {
-        Vsub(source1.p(), source2.p(), dest.p(), source1.size());
+        Vsub(source1.as_span(), source2.as_span(), dest.as_span(),
+             source1.size());
         EXPECT_EQ(expected_dest, dest);
       }
     }
@@ -368,8 +370,7 @@ TEST_F(VectorMathTest, Vmaxmgv) {
     for (const auto& source : GetPrimaryVectors(source_base)) {
       const float expected_max =
           std::accumulate(source.begin(), source.end(), 0.0f, maxmg);
-      float max;
-      Vmaxmgv(source.p(), &max, source.size());
+      float max = Vmaxmgv(source.as_span(), source.size());
       EXPECT_EQ(expected_max, max) << testing::PrintToString(source);
     }
   }
@@ -383,7 +384,8 @@ TEST_F(VectorMathTest, Vmul) {
         expected_dest[i] = source1[i] * source2[i];
       }
       for (auto& dest : GetSecondaryVectors(GetDestination(1u), source1)) {
-        Vmul(source1.p(), source2.p(), dest.p(), source1.size());
+        Vmul(source1.as_span(), source2.as_span(), dest.as_span(),
+             source1.size());
         EXPECT_EQ(expected_dest, dest);
       }
     }
@@ -400,7 +402,7 @@ TEST_F(VectorMathTest, Vsma) {
     }
     for (auto& dest : GetSecondaryVectors(GetDestination(1u), source)) {
       std::ranges::copy(dest_source, dest.begin());
-      Vsma(source.p(), scale, dest.p(), source.size());
+      Vsma(source.as_span(), scale, dest.as_span(), source.size());
       // Different optimizations may use different precisions for intermediate
       // results which may result in different rounding errors thus let's
       // expect only mostly equal floats.
@@ -430,7 +432,7 @@ TEST_F(VectorMathTest, Vsmul) {
       expected_dest[i] = scale * source[i];
     }
     for (auto& dest : GetSecondaryVectors(GetDestination(1u), source)) {
-      Vsmul(source.p(), scale, dest.p(), source.size());
+      Vsmul(source.as_span(), scale, dest.as_span(), source.size());
       EXPECT_EQ(expected_dest, dest);
     }
   }
@@ -444,7 +446,7 @@ TEST_F(VectorMathTest, Vsadd) {
       expected_dest[i] = addend + source[i];
     }
     for (auto& dest : GetSecondaryVectors(GetDestination(1u), source)) {
-      Vsadd(source.p(), addend, dest.p(), source.size());
+      Vsadd(source.as_span(), addend, dest.as_span(), source.size());
       EXPECT_EQ(expected_dest, dest);
     }
   }
@@ -457,8 +459,7 @@ TEST_F(VectorMathTest, Vsvesq) {
     for (const auto& source : GetPrimaryVectors(source_base)) {
       const float expected_sum =
           std::accumulate(source.begin(), source.end(), 0.0f, sqsum);
-      float sum;
-      Vsvesq(source.p(), &sum, source.size());
+      float sum = Vsvesq(source.as_span(), source.size());
       if (std::isfinite(expected_sum)) {
         // Optimized paths in Vsvesq use parallel partial sums which may result
         // in different rounding errors than the non-partial sum algorithm used
@@ -508,8 +509,8 @@ TEST_F(VectorMathTest, Zvmul) {
     }
     for (auto& dest_real : GetSecondaryVectors(GetDestination(2u), real1)) {
       TestVector<float> dest_imag(GetDestination(3u), real1);
-      Zvmul(real1.p(), imag1.p(), real2.p(), imag2.p(), dest_real.p(),
-            dest_imag.p(), real1.size());
+      Zvmul(real1.as_span(), imag1.as_span(), real2.as_span(), imag2.as_span(),
+            dest_real.as_span(), dest_imag.as_span(), real1.size());
       // Different optimizations may use different precisions for intermediate
       // results which may result in different rounding errors thus let's
       // expect only mostly equal floats.
