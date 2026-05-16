0326b05088c10ea29eef6d7bba74f56fc52829c2
rbpotter <rbpotter@chromium.org>
2026-05-15 17:53:57 -0700

    Reland "WebUI Lit template formatter"
    This is a reland of commit 7e24ece0f65760e2d71a80ddbecb94642055e5aa
    
    Using clang-format from depot_tools instead of buildtools/ directly
    to avoid issues with platform-specific paths.
    
    Original change's description:
    > WebUI Lit template formatter
    >
    > Checking in a node based template formatter for CrLitElement subclass
    > template files (.html.ts). The formatter performs the following steps:
    > (1) Extracts HTML template
    > (2) Substitutes Lit ${} expressions for appropriate placeholders
    > (3) Preprocesses the new HTML
    > (4) Parses with parse5
    > (5) Formats indentation
    > (6) Re-serializes the template, wrapping attributes as needed and
    >     reversing transformations done in (2) and (3)
    > (7) Clang-format on non-template portion of file
    > (8) Place template back in the file.
    >
    > To run:
    > node ui/webui/resources/tools/lit_template_formatter/format.js \
    >     <path/to/desired/file.html.ts>
    >
    > Not connecting formatter to any other tools yet; for now this is
    > checked in as an optional aid for developers and for further
    > refinement/experimentation.
    >
    > Bug: 512911634
    > Change-Id: If59a0b88f3ef96ec76b7ff917d4a60020f14a834
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7744807
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1630829}
    
    Bug: 512911634
    Change-Id: Ib97981e434c990de1ec849165f69a3753ddbdd22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850421
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631682}

---
+++ ui/webui/resources/tools/tests/lit_template_formatter/test_whitespace_sensitive_siblings.html.ts
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+export function getHtml(this: DummyTestElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div class="wrapper">
+  <span>Name: </span><span>${this.name}</span>
+</div>
+<!--_html_template_end_-->
+`;
+  // clang-format on
+}
