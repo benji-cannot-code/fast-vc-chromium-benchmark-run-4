61d7cbe9f6892ee55000bd42503399e54e3f76cc
Tiago Vignatti <tvignatti@igalia.com>
2026-05-18 12:31:54 -0700

    Remove dead UI includes in views/relaunch_notification
    Drops chrome/browser/ui/browser.h from platform_impl_desktop.cc (no
    Browser usage), platform_util.h from recommended_bubble_view.cc (the
    IS_MAC gate had no callers), and replaces toolbar_view.h with
    toolbar_button_provider.h to point at the header that actually owns
    ToolbarButtonProvider.
    
    Bug: 353332589
    Change-Id: I81b93a67ae063478145fd0570c40fb9d2b6e5d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841856
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632338}

--- chrome/browser/ui/views/relaunch_notification/relaunch_notification_controller_platform_impl_desktop.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_notification_controller_platform_impl_desktop.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
--- chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view.cc
@@ -19,8 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/views/toolbar/app_menu_control.h"
-#include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/vector_icons/vector_icons.h"
@@ -42,10 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/widget/widget.h"
-
-#if BUILDFLAG(IS_MAC)
-#include "chrome/browser/platform_util.h"
-#endif  // BUILDFLAG(IS_MAC)
 
 // static
 views::Widget* RelaunchRecommendedBubbleView::ShowBubble(
