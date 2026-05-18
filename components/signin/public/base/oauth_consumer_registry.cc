da1092b9a092fc3fae45d4066fa7ff301424e888
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-05-18 11:35:10 -0700

    [composebox] Add Drive read-only scope to ComposeboxQueryController.
    The ComposeboxQueryController now requests the Drive read-only OAuth2
    scope.
    
    Bug: b:513292715
    Change-Id: Id1e6af7250deff0a3ca943490663752cf22b1b40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848327
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632303}

--- components/signin/public/base/oauth_consumer_registry.cc
+++ components/signin/public/base/oauth_consumer_registry.cc
@@ -499,7 +499,8 @@ OAuthConsumer OAuthConsumerRegistry::GetOAuthConsumerFromId(
     case OAuthConsumerId::kComposeboxQueryController:
       return OAuthConsumer(
           /*name=*/kComposeboxQueryControllerName,
-          /*scopes=*/{GaiaConstants::kLensOAuth2Scope});
+          /*scopes=*/{GaiaConstants::kLensOAuth2Scope,
+                      kDriveReadOnlyOAuth2Scope});
     case OAuthConsumerId::kDocumentSuggestionsService:
       return OAuthConsumer(
           /*name=*/kDocumentSuggestionsServiceName,
