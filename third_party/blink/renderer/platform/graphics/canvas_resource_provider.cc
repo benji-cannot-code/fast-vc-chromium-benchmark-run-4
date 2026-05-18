2c9a3e17cef332567c143c9e16e4b1733f0ae463
Colin Blundell <blundell@chromium.org>
2026-05-18 01:35:43 -0700

    Blink: Have CNon2DRPSI duplicate MemoryManagedPaintRecorder::Client impl
    Prepare for decoupling this class from inheriting from
    CanvasResourceProvider by duplicating CanvasResourceProvider's
    implementation of MemoryManagedPaintRecorder::Client methods [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1792?q=CanvasResourceProvider::Record&ss=chromium
    
    Bug: 352263194
    Change-Id: I9a26eb2d104cf15f720b86ad05c132ae8bc86d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841737
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632016}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -2460,6 +2460,8 @@ size_t CanvasNon2DResourceProviderSharedImage::GetSize() const {
   return info.computeByteSize(info.minRowBytes());
 }
 
+void CanvasNon2DResourceProviderSharedImage::RecordingCleared() {}
+
 gpu::raster::RasterInterface*
 CanvasNon2DResourceProviderSharedImage::RasterInterface() const {
   if (!ContextProviderWrapper()) {
