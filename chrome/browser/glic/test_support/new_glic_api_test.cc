b36e82d82c8aabc224ec12edf63b337ec4767349
Dan H <harringtond@chromium.org>
2026-05-18 09:27:05 -0700

    Reland "Reland: Migrate some glic api tests to new fixture"
    This reverts commit 75b3e054c118aabb2952263ae5c77752e3d9e309.
    
    Reason for revert: Disabling the test instead
    
    Original change's description:
    > Revert "Reland: Migrate some glic api tests to new fixture"
    >
    > This reverts commit 3c4f4062cab1dcc43d3bb79c3f26e17889456d98.
    >
    > Reason for revert: NewGlicApiTest.testCloseAndOpenWhileOpening/Default is constantly failing on some Android bots https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Ftest%5C%3Aandroid_browsertests!gtest%3A%3ANewGlicApiTest%23testCloseAndOpenWhileOpening%2FDefault
    >
    > Original change's description:
    > > Reland: Migrate some glic api tests to new fixture
    > >
    > > This reverts commit af49a14e372e8687d791f56eba2a2d9c9f6abefa, and is
    > > a reland of https://chromium-review.googlesource.com/c/chromium/src/+/7827929
    > >
    > > Patch 0 is the pristine revert.
    > > The fix is defining SLOW_BINARY, which was missing
    > >
    > > Bug: b:477918640, 513445511
    > > Link: https://chromium-review.googlesource.com/id/I69f66e1845cf007b8fd9fadc824e66e16a6a6964
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848550
    > > Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    > > Commit-Queue: Dan H <harringtond@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1631400}
    >
    > Bug: b:477918640, 513445511
    > Change-Id: I7f01fea089c420b97b5d49ed7a35c79d3dfc8a1e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851840
    > Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Ryo Hashimoto <hashimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1631968}
    
    Bug: b:477918640, 513445511
    Change-Id: I24d7e979ad674b8e0d7f36a4d238be6e1cc9624c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856501
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632213}

--- chrome/browser/glic/test_support/new_glic_api_test.cc
+++ chrome/browser/glic/test_support/new_glic_api_test.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/glic/test_support/new_glic_api_test.h"
 
+#include "base/types/expected_macros.h"
+
 namespace glic::internal {
 namespace {
 base::expected<tabs::TabHandle, std::string> ParseTabId(
@@ -24,6 +26,15 @@ base::expected<tabs::TabHandle, std::string> ParseTabId(
   }
   return base::ok(handle);
 }
+
+base::expected<tabs::TabHandle, std::string> ParseOptionalTabId(
+    const base::DictValue& command) {
+  if (!command.FindString("tabId")) {
+    return base::ok(tabs::TabHandle());
+  }
+  return ParseTabId(command);
+}
+
 }  // namespace
 
 base::expected<Command, std::string> DeserializeCommand(
@@ -50,7 +61,7 @@ base::expected<Command, std::string> DeserializeCommand(
     if (!url) {
       return base::unexpected("Missing url");
     }
-    ASSIGN_OR_RETURN(tabs::TabHandle tab_handle, ParseTabId(dict));
+    ASSIGN_OR_RETURN(tabs::TabHandle tab_handle, ParseOptionalTabId(dict));
     return NavigateTabCommand{tab_handle, *url};
   }
   return base::unexpected(base::StrCat({"Unknown command: ", *command_field}));
