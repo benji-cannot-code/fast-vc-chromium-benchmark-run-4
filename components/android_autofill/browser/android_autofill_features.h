d9a18c9fec4274bd9835f76129d69f93247f76f6
Adem Derinel <derinel@google.com>
2026-05-17 20:10:25 -0700

    [Android] Cleanup AutofillVirtualViewStructureAndroidPasskeyLongPress
    This change removes the feature flag
    AutofillVirtualViewStructureAndroidPasskeyLongPress, which was enabled
    by default since M140.
    
    Change-Id: Ie5ddc308e1d4e978f31d08a955a6e1b29eea32c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7834156
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631950}

--- components/android_autofill/browser/android_autofill_features.h
+++ components/android_autofill/browser/android_autofill_features.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::features {
 
-BASE_DECLARE_FEATURE(kAutofillVirtualViewStructureAndroidPasskeyLongPress);
-
 BASE_DECLARE_FEATURE(kAndroidAutofillLazyFrameworkWrapper);
 
 BASE_DECLARE_FEATURE(kAndroidAutofillImprovedVisibilityDetection);
