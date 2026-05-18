01278bdc7e820720240849f34241cf4136dd0c53
Vasilii Sukhanov <vasilii@chromium.org>
2026-05-18 09:02:06 -0700

    Change PasswordAutofillManager::PreviewSuggestion to mask the plain text password on preview.
    Fixed: 503877666
    Change-Id: I429025e9e07326498b815160d065ff747360b029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849368
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632193}

--- components/password_manager/core/browser/password_autofill_manager_unittest.cc
+++ components/password_manager/core/browser/password_autofill_manager_unittest.cc
@@ -462,7 +462,8 @@ TEST_F(PasswordAutofillManagerTest, PreviewSuggestion) {
   InitializePasswordAutofillManager(&client, nullptr);
 
   EXPECT_CALL(*client.mock_driver(),
-              PreviewSuggestion(test_username_, test_password_));
+              PreviewSuggestion(test_username_,
+                                std::u16string(test_password_.length(), '*')));
   EXPECT_TRUE(
       password_autofill_manager_->PreviewSuggestionForTest(test_username_));
   testing::Mock::VerifyAndClearExpectations(client.mock_driver());
@@ -847,7 +848,8 @@ TEST_F(PasswordAutofillManagerTest, PreviewAndFillEmptyUsernameSuggestion) {
 
   // Check that preview of the empty username works.
   EXPECT_CALL(*client.mock_driver(),
-              PreviewSuggestion(std::u16string(), test_password_));
+              PreviewSuggestion(std::u16string(),
+                                std::u16string(test_password_.length(), '*')));
   const Suggestion suggestion = autofill::test::CreateAutofillSuggestion(
       autofill::SuggestionType::kPasswordEntry, no_username_string);
   password_autofill_manager_->DidSelectSuggestion(suggestion);
@@ -2300,8 +2302,10 @@ TEST_F(PasswordAutofillManagerTest,
   const Suggestion suggestion = autofill::test::CreateAutofillSuggestion(
       autofill::SuggestionType::kBackupPasswordEntry, test_username_, payload);
 
-  EXPECT_CALL(*client.mock_driver(),
-              PreviewSuggestion(test_username_, backup_password_));
+  EXPECT_CALL(
+      *client.mock_driver(),
+      PreviewSuggestion(test_username_,
+                        std::u16string(backup_password_.length(), '*')));
   password_autofill_manager_->DidSelectSuggestion(suggestion);
   testing::Mock::VerifyAndClearExpectations(client.mock_driver());
 
