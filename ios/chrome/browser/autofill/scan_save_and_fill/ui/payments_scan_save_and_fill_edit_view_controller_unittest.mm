f290fe13e18bfa24e3f3f540a68127b846d46775
YiwenQian <yiwenqian@google.com>
2026-05-18 12:27:44 -0700

    [ScanCard IOS] Implement loading spinner and success checkmark for Save button
    Bug: 484102792
    Change-Id: I09124f74f202eaf459150aac13db36b58c706a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7818857
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632337}

--- ios/chrome/browser/autofill/scan_save_and_fill/ui/payments_scan_save_and_fill_edit_view_controller_unittest.mm
+++ ios/chrome/browser/autofill/scan_save_and_fill/ui/payments_scan_save_and_fill_edit_view_controller_unittest.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_edit_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_edit_item_delegate.h"
+#import "ios/chrome/common/ui/util/chrome_button.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
@@ -366,6 +367,19 @@ TEST_F(PaymentsScanSaveAndFillEditViewControllerTest, TestDidTapSave) {
   EXPECT_EQ(u"My Card", fake_mutator_.savedDetails.nickname.value());
 }
 
+// Tests the behavior of showLoadingStateWithAccessibilityLabel.
+TEST_F(PaymentsScanSaveAndFillEditViewControllerTest, TestShowLoadingState) {
+  CreateController();
+
+  [view_controller_ showLoadingStateWithAccessibilityLabel:@"Loading"];
+
+  UIButton* saveButton = [view_controller_ valueForKey:@"_saveButton"];
+  EXPECT_FALSE(saveButton.enabled);
+  EXPECT_NSEQ(nil, [saveButton valueForKey:@"title"]);
+  EXPECT_TRUE(saveButton.configuration.showsActivityIndicator);
+  EXPECT_NSEQ(@"Loading", saveButton.accessibilityLabel);
+}
+
 // Tests the behavior of showConfirmationState.
 TEST_F(PaymentsScanSaveAndFillEditViewControllerTest,
        TestShowConfirmationState) {
@@ -376,10 +390,12 @@ TEST_F(PaymentsScanSaveAndFillEditViewControllerTest,
   UIButton* saveButton = [view_controller_ valueForKey:@"_saveButton"];
   EXPECT_FALSE(saveButton.enabled);
   EXPECT_NSEQ(nil, [saveButton valueForKey:@"title"]);
+  EXPECT_FALSE(saveButton.configuration.showsActivityIndicator);
 
-  // 3 corresponds to PrimaryButtonImageCustom
+  // PrimaryButtonImageCheckmark corresponds to confirmation state
   NSNumber* imageValue = [saveButton valueForKey:@"primaryButtonImage"];
-  EXPECT_EQ(3, [imageValue intValue]);
+  EXPECT_EQ(static_cast<int>(PrimaryButtonImageCheckmark),
+            [imageValue intValue]);
 }
 
 // Tests if the expiration date properly validates.
