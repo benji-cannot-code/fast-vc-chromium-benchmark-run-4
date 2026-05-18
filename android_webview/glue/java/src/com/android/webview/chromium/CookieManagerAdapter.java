d7a827ad26e57f99b01509a0d6e57cef5701d713
Torne (Richard Coles) <torne@google.com>
2026-05-18 03:43:01 -0700

    webview: do all the cookie URL fixups together.
    Instead of parsing the URL with WebAddressParser in the glue layer and
    then having a separate special case to handle the app passing a domain
    with a leading dot instead of the URL in AwCookieManager, do both steps
    together in AwCookieManager to make it easier to experiment with
    different fixup logic in future.
    
    This also splits the API of AwCookieManager to have dedicated methods
    that do or do not apply fixups; currently all non-test usage calls the
    version that applies fixups.
    
    This is not intended to change any behavior for apps - the changes to
    test code are just to reflect the leading-dot handling now being applied
    by a different method.
    
    Bug: 513274139
    Change-Id: Ia09c9d2ec8bd69c56e14484207c11cd784a4d8ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848636
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632061}

--- android_webview/glue/java/src/com/android/webview/chromium/CookieManagerAdapter.java
+++ android_webview/glue/java/src/com/android/webview/chromium/CookieManagerAdapter.java
@@ -13,7 +13,6 @@ import com.android.webview.chromium.WebViewChromium.ApiCall;
 import com.android.webview.chromium.WebViewChromium.ApiCallUserAction;
 
 import org.chromium.android_webview.AwCookieManager;
-import org.chromium.android_webview.WebAddressParser;
 import org.chromium.base.Log;
 import org.chromium.base.TraceEvent;
 
@@ -96,7 +95,7 @@ public class CookieManagerAdapter extends CookieManager {
                 WebViewChromium.recordWebViewApiCall(
                         ApiCall.COOKIE_MANAGER_SET_COOKIE,
                         ApiCallUserAction.COOKIE_MANAGER_SET_COOKIE);
-                mChromeCookieManager.setCookie(fixupUrl(url), value);
+                mChromeCookieManager.setCookieWithUrlFixup(url, value);
             }
         } catch (URISyntaxException e) {
             Log.e(TAG, "Not setting cookie due to error parsing URL: %s", url, e);
@@ -116,8 +115,8 @@ public class CookieManagerAdapter extends CookieManager {
                 WebViewChromium.recordWebViewApiCall(
                         ApiCall.COOKIE_MANAGER_SET_COOKIE,
                         ApiCallUserAction.COOKIE_MANAGER_SET_COOKIE);
-                mChromeCookieManager.setCookie(
-                        fixupUrl(url), value, CallbackConverter.fromValueCallback(callback));
+                mChromeCookieManager.setCookieWithUrlFixup(
+                        url, value, CallbackConverter.fromValueCallback(callback));
             }
         } catch (URISyntaxException e) {
             Log.e(TAG, "Not setting cookie due to error parsing URL: %s", url, e);
@@ -132,7 +131,7 @@ public class CookieManagerAdapter extends CookieManager {
                 WebViewChromium.recordWebViewApiCall(
                         ApiCall.COOKIE_MANAGER_GET_COOKIE,
                         ApiCallUserAction.COOKIE_MANAGER_GET_COOKIE);
-                return mChromeCookieManager.getCookie(fixupUrl(url));
+                return mChromeCookieManager.getCookieWithUrlFixup(url);
             }
         } catch (URISyntaxException e) {
             Log.e(TAG, "Unable to get cookies due to error parsing URL: %s", url, e);
@@ -252,16 +251,5 @@ public class CookieManagerAdapter extends CookieManager {
                     ApiCallUserAction.COOKIE_MANAGER_SET_ACCEPT_FILE_SCHEME_COOKIES);
             mChromeCookieManager.setAcceptFileSchemeCookies(accept);
         }
-    }
-
-    private static String fixupUrl(String url) throws URISyntaxException {
-        // WebAddressParser is a copy of the  private API WebAddress in the android framework and a
-        // "quirk" of the Classic WebView implementation that allowed embedders to be relaxed about
-        // what URLs they passed into the CookieManager, so we do the same normalisation before
-        // entering the chromium stack.
-        //
-        // The implementation of WebAddressParser isn't ideal, we should remove its usage and
-        // replace it with UrlFormatter or similar URL parser.
-        return new WebAddressParser(url).toString();
     }
 }
