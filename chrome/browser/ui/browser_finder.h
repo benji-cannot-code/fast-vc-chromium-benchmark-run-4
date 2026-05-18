da03eef8b8ec51fc03cf727e003e362738193868
Yu He <yhe@microsoft.com>
2026-05-17 22:10:11 -0700

    [bedrock] Remove legacy chrome::Find* and GetTotalBrowserCount helpers
    Deletes five free functions from chrome/browser/ui/browser_finder.{h,cc}
    that have been superseded by ProfileBrowserCollection and
    GlobalBrowserCollection:
    
      - chrome::FindTabbedBrowser
      - chrome::FindBrowserWithProfile
      - chrome::FindLastActiveWithProfile
      - chrome::FindLastActive
      - chrome::GetTotalBrowserCount
    
    Remaining callsites are migrated to the new APIs:
    
      - chrome::GetTotalBrowserCount()
          -> GlobalBrowserCollection::GetInstance()->GetSize()
      - chrome::FindBrowserWithProfile(profile)
          -> ProfileBrowserCollection::GetForProfile(profile)
                 ->GetLastActiveBrowser()
            (with a null check on GetForProfile())
    
    The browser_window_features() pointer accessor is also replaced with the
    BrowserWindowInterface::GetFeatures() reference accessor at the migrated
    callsite.
    
    Also drops the FindTabbedBrowser browser test, the now-unused
    BrowserWindowInterface forward declaration and
    ui/display/types/display_constants.h include from the header, and
    several dead includes from the touched .cc files (<algorithm>,
    tab_contents_iterator.h, navigation_controller.h, browser_view.h,
    browser_finder.h).
    
    No behavior change.
    
    Bug: 494010890
    Change-Id: I694da726a144e110b75ff9033f948a16037efe7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850417
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Yu He <yhe@microsoft.com>
    Auto-Submit: Yu He <yhe@microsoft.com>
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1631967}

--- chrome/browser/ui/browser_finder.h
+++ chrome/browser/ui/browser_finder.h
@@ -8,10 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include "ui/display/types/display_constants.h"
-
 class Browser;
-class BrowserWindowInterface;
 class Profile;
 
 namespace content {
@@ -32,8 +29,8 @@ class WebContents;
 // There are a few functions that have valid use cases. For example, we can
 // imagine a hypothetical feature of the task manager (which is not conceptually
 // associated with any given browser window) which wants to display the number
-// of browser windows to the user. This would be a valid use case for
-// GetTotalBrowserCount().
+// of incognito browser windows to the user. This would be a valid use case for
+// GetIncognitoBrowserCount().
 //
 // More colloquial explanation:
 // There are two problems with FindBrowser(*) methods.
@@ -68,30 +65,6 @@ class WebContents;
 
 namespace chrome {
 
-// If you want to find the last active tabbed browser and create a new browser
-// if there are no tabbed browsers, use ScopedTabbedBrowserDisplayer.
-
-// Returns the last active tabbed browser with a profile matching `profile`.
-//
-// If `match_original_profiles` is true, matching is done based on the original
-// profile (e.g. profile->GetOriginalProfile() ==
-// browser->profile()->GetOriginalProfile()). This has the effect of matching
-// against both non-incognito and incognito profiles. If
-// `match_original_profiles` is false, only an exact match may be returned. If
-// `display_id` is not equal to `display::kInvalidDisplayId`, only the browsers
-// in the corresponding display may be returned. Browsers that have closed and
-// are pending deletion are not returned.
-// WARNING: Do not use this method. See comment at top of file.
-Browser* FindTabbedBrowser(const Profile* profile,
-                           bool match_original_profiles,
-                           int64_t display_id = display::kInvalidDisplayId);
-
-// Returns an existing browser window with the provided profile. Searches in the
-// order of last activation. Only browsers that have been active can be
-// returned. Returns nullptr if no such browser currently exists.
-// WARNING: Do not use this method. See comment at top of file.
-Browser* FindBrowserWithProfile(const Profile* profile);
-
 // Returns the browser containing the specified `web_contents` as a tab in that
 // browser. Returns nullptr if no such browser currently exists. `web_contents`
 // must not be nullptr.
@@ -107,32 +80,6 @@ Browser* FindBrowserWithProfile(const Profile* profile);
 // to be found via this method.
 // WARNING: Do not use this method. See comment at top of file.
 Browser* FindBrowserWithTab(const content::WebContents* web_contents);
-
-// Returns the browser owned by `profile` whose window was most recently active.
-// Returns nullptr if no such browser currently exists.
-//
-// WARNING: This returns nullptr until a browser becomes active. If during
-// startup a browser does not become active (perhaps the user launches Chrome,
-// then clicks on another app before the first browser window appears) then this
-// returns nullptr.
-//
-// WARNING #2: This will always return nullptr in unit tests run on the bots.
-BrowserWindowInterface* FindLastActiveWithProfile(Profile* profile);
-
-// Returns the browser whose window was most recently active. Returns nullptr if
-// no such browser currently exists.
-//
-// WARNING: This returns nullptr until a browser becomes active. If during
-// startup a browser does not become active (perhaps the user launches Chrome,
-// then clicks on another app before the first browser window appears) then this
-// returns nullptr.
-//
-// WARNING #2: This will always return nullptr in unit tests run on the bots.
-BrowserWindowInterface* FindLastActive();
-
-// Returns the number of browsers across all profiles. This does not include
-// pending delete browsers.
-size_t GetTotalBrowserCount();
 
 // Returns the number of browsers with the Profile `profile`.
 // Note that:
