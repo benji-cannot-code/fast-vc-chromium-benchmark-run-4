81d47c3e6d984976eb13fe7ece1bdd5c22be5d24
Jason Leo <cgqaq@chromium.org>
2026-05-18 09:56:45 -0700

    [css-color] Render 'none' in light-dark() image as transparent
    Per the CSSWG resolution[1], a selected none arm inside a light-dark()
    image value computes to image(transparent), which then resolves to
    image(rgba(0, 0, 0, 0)) per the standard computed-value rules for
    <color>[2].
    
    [1]: https://github.com/w3c/csswg-drafts/issues/13866#issuecomment-4390052390
    [2]: https://drafts.csswg.org/css-color-4/#transparent-color
    
    Bug: 491829958
    Change-Id: I2df4e7ab066652078a0ed1ee8608f5af9c2683c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806112
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Jason Leo <cgqaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632236}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/css/css_animations.h"
 #include "third_party/blink/renderer/core/css/css_crossfade_value.h"
 #include "third_party/blink/renderer/core/css/css_gradient_value.h"
+#include "third_party/blink/renderer/core/css/css_identifier_value.h"
 #include "third_party/blink/renderer/core/css/css_image_set_value.h"
 #include "third_party/blink/renderer/core/css/css_light_dark_value_pair.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
@@ -75,6 +76,22 @@ Element* ComputeStyledElement(const StyleRequest& style_request,
         style_request.pseudo_id, style_request.pseudo_argument);
   }
   return styled_element;
+}
+
+// image(transparent) is used to substitute 'none' inside light-dark() image
+// values per CSS Color 5.
+// https://drafts.csswg.org/css-color-5/#valdef-light-dark-none
+cssvalue::CSSColorImageValue& TransparentImage() {
+  DEFINE_STATIC_LOCAL(
+      Persistent<cssvalue::CSSColorImageValue>, image,
+      (MakeGarbageCollected<cssvalue::CSSColorImageValue>(
+          CSSIdentifierValue::Create(CSSValueID::kTransparent))));
+  return *image;
+}
+
+bool IsNoneValue(const CSSValue& value) {
+  const auto* ident = DynamicTo<CSSIdentifierValue>(value);
+  return ident && ident->GetValueID() == CSSValueID::kNone;
 }
 
 }  // namespace
@@ -427,7 +444,14 @@ const CSSValue& StyleResolverState::ResolveLightDarkPair(
 
 const CSSValue& StyleResolverState::ResolveGradients(
     const CSSValue& value) const {
+  const bool was_light_dark_pair = IsA<CSSLightDarkValuePair>(value);
   const CSSValue& resolved_value = ResolveLightDarkPair(value);
+  // Per CSS Color 5: 'none' inside a light-dark() image value computes to
+  // image(transparent).
+  // https://drafts.csswg.org/css-color-5/#valdef-light-dark-none
+  if (was_light_dark_pair && IsNoneValue(resolved_value)) {
+    return TransparentImage();
+  }
   if (const auto* gradient_value =
           DynamicTo<cssvalue::CSSGradientValue>(resolved_value)) {
     return gradient_value->ResolveValuesIfNeeded(*this);
@@ -444,7 +468,11 @@ const CSSValue& StyleResolverState::ResolveGradients(
 }
 
 CSSValue& StyleResolverState::ResolveGradients(CSSValue& value) const {
+  const bool was_light_dark_pair = IsA<CSSLightDarkValuePair>(value);
   CSSValue& resolved_value = const_cast<CSSValue&>(ResolveLightDarkPair(value));
+  if (was_light_dark_pair && IsNoneValue(resolved_value)) {
+    return TransparentImage();
+  }
   if (auto* gradient_value =
           DynamicTo<cssvalue::CSSGradientValue>(resolved_value)) {
     return gradient_value->ResolveValuesIfNeeded(*this);
