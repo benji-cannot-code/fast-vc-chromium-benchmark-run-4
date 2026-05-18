657dd9d9f49f38edb5aa2b698b524cc66a8c2aee
ginnnnnnny <ginnyhuang@chromium.org>
2026-05-18 10:52:45 -0700

    [Actor Login] Move ActorLoginMetricsHelper to //components
    This change moves ActorLoginMetricsHelper from //chrome/browser to
    //components/password_manager/core/browser/actor_login/internal. This
    allows the metrics helper to be reused across platforms (including iOS)
    to record UMA and UKM metrics for automated actor login flows.
    
    Bug: 472291829
    Change-Id: I53c65ae20621a4b108973dc9e493de422e09cf45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848157
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632274}

--- components/password_manager/core/browser/actor_login/internal/actor_login_metrics_helper.cc
+++ components/password_manager/core/browser/actor_login/internal/actor_login_metrics_helper.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/password_manager/actor_login/internal/actor_login_metrics_helper.h"
+#include "components/password_manager/core/browser/actor_login/internal/actor_login_metrics_helper.h"
 
 #include "base/metrics/histogram_functions.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
