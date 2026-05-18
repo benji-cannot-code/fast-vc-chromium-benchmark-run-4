4af056efe0fdddd33ac80d1c365eb21d3c635d09
Shu Yang <shuyng@google.com>
2026-05-18 12:37:55 -0700

    Enable AndroidOpenIncognitoAsWindow by default
    This CL enables feature AndroidOpenIncognitoAsWindow by default on
    Android.
    
    This feature improves the multitasking and private browsing experience
    on large-screen Android form factors (Tablets and Foldables) by
    introducing a windowed Incognito mode, separate from regular mode.
    Runtime checks restrict windowed incognito mode strictly to devices
    with a minimum screen diagonal of 8.0 inches running Android S_V2 or
    higher.
    
    This CL also cleans up the redundant desktop feature override in
    chrome_browser_field_trials.cc and removes the test configuration
    from fieldtrial_testing_config.json.
    
    Bug: 445475304
    Change-Id: I4df5fafb35b5591b7e1b2c42ef62ac1bf7a29ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7854107
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Auto-Submit: Shu Yang <shuyng@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632341}

--- components/feed/feed_feature_list.cc
+++ components/feed/feed_feature_list.cc
@@ -58,6 +58,6 @@ BASE_FEATURE(kFeedStreaming, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFeedAudioOverviews, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAndroidOpenIncognitoAsWindow, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAndroidOpenIncognitoAsWindow, base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace feed
