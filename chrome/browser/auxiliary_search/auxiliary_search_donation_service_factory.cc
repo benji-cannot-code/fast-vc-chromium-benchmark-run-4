bbbbedf4f64d80110e81bb38f9864aa3c0aa7ba0
Michael Cui <mlcui@google.com>
2026-05-17 22:51:33 -0700

    Add `AuxiliarySearchDonationServiceBridge` for C++ -> Java interop
    This initial CL only implements the (trivial) construction of Java-side
    AppSearch `WebPage`s, logic to convert C++-side `HistoryData`s to the
    aforementioned Java-side `WebPage`s, and a stub "donate history" method
    which currently does nothing.
    
    A follow-up CL (https://crrev.com/c/7625841) will actually implement the
    "donate history" method, along with the necessary set-up that is
    required for it.
    
    Bug: b:432359106
    Change-Id: I6a6a210019aeb13da26f70dee42eee7030432b1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7625840
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631975}

--- chrome/browser/auxiliary_search/auxiliary_search_donation_service_factory.cc
+++ chrome/browser/auxiliary_search/auxiliary_search_donation_service_factory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/auxiliary_search/auxiliary_search_donation_service.h"
+#include "chrome/browser/auxiliary_search/auxiliary_search_donation_service_bridge.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/page_content_annotations/page_content_annotations_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -62,9 +63,7 @@ AuxiliarySearchDonationServiceFactory::BuildServiceInstanceForBrowserContext(
       visited_url_ranking::VisitedURLRankingServiceFactory::GetForProfile(
           profile),
       profile->GetPrefs(),
-      // TODO: https://crbug.com/432359106 - Use AuxiliarySearchDonor to donate
-      // the entries.
-      /*donate_callback=*/base::DoNothing());
+      AuxiliarySearchDonationServiceBridge::CreateDonationCallback());
 }
 
 bool AuxiliarySearchDonationServiceFactory::ServiceIsCreatedWithBrowserContext()
