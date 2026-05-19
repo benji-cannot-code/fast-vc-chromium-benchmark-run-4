cddefaf5c14cdf752e2237b59f43db61f989f273
Justin Cohen <justincohen@google.com>
2026-05-18 19:08:19 -0700

    cocoa: Fix TouchBarNotificationBridge heap-use-after-free
    Reorder checks in OnBrowserClosed to avoid use-after-free when
    resetting owner_.browser. Also clean up observer registrations
    on destruction.
    
    Bug: 496012368
    Change-Id: I4eb69e69eda25d54b4d83fa92274178cd2daa77c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856402
    Commit-Queue: Justin Cohen <justincohen@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632569}

--- chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
+++ chrome/browser/ui/cocoa/touchbar/browser_window_default_touch_bar.mm
@@ -164,6 +164,9 @@ class TouchBarNotificationBridge : public CommandObserver,
 
   ~TouchBarNotificationBridge() override {
     UpdateWebContents(nullptr);
+    if (browser_) {
+      browser_->command_controller()->RemoveCommandObserver(this);
+    }
   }
 
   void UpdateTouchBar() { [[owner_ controller] invalidateTouchBar]; }
@@ -195,11 +198,12 @@ class TouchBarNotificationBridge : public CommandObserver,
 
   // BrowserCollectionObserver:
   void OnBrowserClosed(BrowserWindowInterface* browser) override {
+    if (browser == browser_) {
+      browser_->command_controller()->RemoveCommandObserver(this);
+      browser_ = nullptr;
+    }
     if (browser == owner_.browser) {
       owner_.browser = nullptr;
-    }
-    if (browser == browser_) {
-      browser_ = nullptr;
     }
   }
 
