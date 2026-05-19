76a0820f6f4cc5832eb07f561b348874f23db9b7
Peter Beverloo <peter@chromium.org>
2026-05-19 03:16:54 -0700

    Launch the probe stylus writing in background feature
    Offloading probing the StylusWritingController to a background thread
    launched on Android WebView in January with positive results. The Chrome
    on Android experiment ended a week ago with no statistically sensitive
    changes (or smoke suggesting we should investigate more), so let's
    enable this by default as it's neutral optimising for code simplicity.
    
    Bug: b:446845510
    Change-Id: Icccc84252dbc09daad56b0cb5ddcc0d883d53091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852485
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632748}

--- components/stylus_handwriting/android/stylus_handwriting_feature_map.cc
+++ components/stylus_handwriting/android/stylus_handwriting_feature_map.cc
@@ -16,7 +16,6 @@ namespace {
 
 const base::Feature* const kFeaturesExposedToJava[] = {
     &kCacheStylusSettings,
-    &kProbeStylusWritingInBackground,
 };
 
 // static
@@ -36,8 +35,6 @@ static int64_t JNI_StylusHandwritingFeatureMap_GetNativeMap(JNIEnv* env) {
 
 // Cache Stylus related settings
 BASE_FEATURE(kCacheStylusSettings, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kProbeStylusWritingInBackground,
-             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace stylus_handwriting::android
 
