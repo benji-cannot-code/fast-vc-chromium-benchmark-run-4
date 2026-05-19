4d550b6b47771f41d2ee653798a63483c57f619d
James Zern <jzern@chromium.org>
2026-05-19 16:52:25 -0700

    Roll src/third_party/libaom/source/libaom/ 5e4b07dc9..13cbb78a1 (4 commits)
    https://aomedia.googlesource.com/aom.git/+log/5e4b07dc9799..13cbb78a1a9e
    
    $ git log 5e4b07dc9..13cbb78a1 --date=short --no-merges --format='%ad %ae %s'
    2026-05-18 jzern validate_img,validate_hbd_input: fix crash w/monochrome
    2026-05-18 wtc Use a fixed copyright year in generated files
    2026-05-14 diksha.singh Refactor av1_interp_cubic_rate_dist()
    2026-05-14 jdorfman [convolve] Optimize av1_dist_wtd_convolve_2d_avx2
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    
    Bug: 307414544, 513603812
    Change-Id: If0eff04806438388a93bcb5f6ba877ecfca488a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859978
    Commit-Queue: James Zern <jzern@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633200}

--- third_party/libaom/source/config/config/aom_version.h
+++ third_party/libaom/source/config/config/aom_version.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (c) 2026, Alliance for Open Media. All rights reserved.
+ * Copyright (c) 2016, Alliance for Open Media. All rights reserved.
  *
  * This source code is subject to the terms of the BSD 2 Clause License and
  * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 14
 #define VERSION_PATCH 0
-#define VERSION_EXTRA "23-g5e4b07dc97"
+#define VERSION_EXTRA "27-g13cbb78a1a"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "3.14.0-23-g5e4b07dc97"
-#define VERSION_STRING " 3.14.0-23-g5e4b07dc97"
+#define VERSION_STRING_NOSP "3.14.0-27-g13cbb78a1a"
+#define VERSION_STRING " 3.14.0-27-g13cbb78a1a"
 #endif  // AOM_VERSION_H_
