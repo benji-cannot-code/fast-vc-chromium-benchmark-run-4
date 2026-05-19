664f575ef319dbed64fc88f5df4c5c07c36fc5ed
James Cook <jamescook@chromium.org>
2026-05-19 15:38:30 -0700

    extensions: Add platform assert to ExtensionAILanguageModelBrowserTest
    While the code compiles on Android, the underlying web platform feature
    (the AI Prompt API) is not supported yet on Android, so we shouldn't
    try to compile this test on Android right now.
    
    Bug: 469417243
    Change-Id: I18b784c95c52a17bb4952bc37b92cf10ef078891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7855893
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633162}

--- chrome/browser/extensions/ai_language_model_browsertest.cc
+++ chrome/browser/extensions/ai_language_model_browsertest.cc
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features_generated.h"
 
+// The AI Prompt API is not yet available on Android. Track status here:
+// https://developer.chrome.com/docs/ai/prompt-api
+static_assert(!BUILDFLAG(IS_ANDROID));
+
 namespace extensions {
 
 namespace {
