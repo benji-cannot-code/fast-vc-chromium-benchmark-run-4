17d70eab4938bdf66442b8fe83263200bf0dcae3
Joanne de Abreu <jdeabreu@google.com>
2026-05-19 16:43:18 -0700

    Add tests for prefetch/prerender and savestate
    This adds tests to check the behaviour of save state, and in particular
    whether headers are saved or not, when requests were made using prefetch
    or prerender.
    
    This also updates the save and restore state test for WebView#navigate
    to check the restore state directly instead of relying on triggering
    another request and checking the test server.
    
    Bug: 408128748
    Test: AwPrerfetchTest#testPrefetchAndSaveState
    Test: AwPrerenderTest#testPrerenderAndSaveState
    Test: NavigateApiTest#extraHeaders_saveRestoreState
    Change-Id: I051bdf4bb8fa27f6f74423fc1680b2511e10fd6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797212
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633193}

---
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/NavigationControllerUtil.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content_public.browser.test.util;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.content_public.browser.WebContents;
+
+@JNINamespace("content")
+public class NavigationControllerUtil {
+    public static NavigationEntrySimple[] getNavigationHistorySimple(WebContents webContents) {
+        return NavigationControllerUtilJni.get().getNavigationHistorySimple(webContents);
+    }
+
+    @NativeMethods
+    interface Natives {
+        NavigationEntrySimple[] getNavigationHistorySimple(WebContents webContents);
+    }
+}
