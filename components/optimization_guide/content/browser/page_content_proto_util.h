bf9fa5f59228a38435a238ed6f201596a3368531
Ryan Kalla <ryankalla@google.com>
2026-05-19 14:36:39 -0700

    Add screenshot iframe info to the APC
    CopyFromSurfaceResult now includes TrackedElementRects which contain the
    coordinates of iframes in the screenshot. The tracked iframe element
    coordinates are calculated by the compositor and have been translated
    into the coordinate space of the CopyFromSurfaceResult bitmap.
    
    In this CL we update the APC by adding entries to the ScreenshotInfo >
    IframeInfo list based on the TrackedElementRects. This new compositor
    based geometry will be used for iframe redaction on the server.
    
    This also updates the AIPageContentTrackedElements blink feature to be
    called AIPageContentTrackedElementsIframe so that we can control the
    different tracking features individually.
    
    See here for more context:
    https://docs.google.com/document/d/1admTWDMlns478TvaScUqwllvNdVIAA_xNJy2L9aZE8A
    
    Bug: 441532128
    Change-Id: Ic7b912bcbbc19dea1c82a9515d4a1f7087461e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7800464
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Ryan Kalla <ryankalla@google.com>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633122}

--- components/optimization_guide/content/browser/page_content_proto_util.h
+++ components/optimization_guide/content/browser/page_content_proto_util.h
@@ -19,11 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/common_types.pb.h"
 #include "components/optimization_guide/proto/features/model_prototyping.pb.h"
 #include "content/public/browser/global_routing_id.h"
+#include "third_party/blink/public/common/tokens/tokens.h"
 #include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom-forward.h"
 #include "ui/gfx/geometry/point.h"
 #include "url/origin.h"
 
 namespace content {
+class RenderFrameHost;
 class WebContents;
 }  // namespace content
 
@@ -139,6 +141,12 @@ std::optional<optimization_guide::TargetNodeInfo> FindNodeWithID(
 content::RenderFrameHost* GetRenderFrameForDocumentIdentifier(
     content::WebContents& web_contents,
     std::string_view target_document_token);
+
+// Returns the RenderFrameHost for the given renderer process id and frame
+// token, or nullptr if the render frame host is not found.
+content::RenderFrameHost* GetRenderFrameHostForToken(
+    int renderer_process_id,
+    blink::FrameToken frame_token);
 
 // Returns the URL to use for frame metadata given the Document's
 // `committed_url` and `committed_origin`. The `committed_url` may not be a
