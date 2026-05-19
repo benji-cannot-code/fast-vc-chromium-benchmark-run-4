ab373381cb9b0d12243c8a797f2e6b6b5409b6e9
Dale Curtis <dalecurtis@chromium.org>
2026-05-18 17:19:54 -0700

    Stop using delayed discard in MediaFoundationAudioDecoder
    There's no reason for this buffering code to live in the discard
    helper anymore now that Vorbis doesn't need it. It's usage here
    was questionable anyways since we don't know that we'll always
    be delayed by one packet in this API.
    
    R=tguilbert
    
    Bug: 498560799
    Change-Id: Icc1815f2bec4ff92b97593eb28461c24361ee1fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7855175
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632515}

--- media/filters/win/media_foundation_audio_decoder.h
+++ media/filters/win/media_foundation_audio_decoder.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/circular_deque.h"
 #include "base/functional/callback.h"
 #include "base/sequence_checker.h"
 #include "media/base/audio_buffer.h"
@@ -100,7 +101,7 @@ class MEDIA_EXPORT MediaFoundationAudioDecoder : public AudioDecoder {
   // Callback that delivers output frames.
   OutputCB output_cb_;
 
-  AudioDiscardHelper::TimeInfo current_buffer_time_info_;
+  base::circular_deque<AudioDiscardHelper::TimeInfo> time_info_;
   std::unique_ptr<AudioDiscardHelper> discard_helper_;
 
   // Pool which helps avoid thrashing memory when returning audio buffers.
