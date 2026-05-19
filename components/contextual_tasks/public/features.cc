5798d23fabba50de06c1533ab1e22e64c6bb5b8f
Abhay Prakash <abhayprakash@google.com>
2026-05-19 00:44:32 -0700

    [STS] Add a flag for de-duping.
    Bug: 499063476
    Change-Id: I3b06e9856f7612728ec1359c5cf3d020836b12cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853382
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Abhay Prakash <abhayprakash@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632676}

--- components/contextual_tasks/public/features.cc
+++ components/contextual_tasks/public/features.cc
@@ -175,6 +175,11 @@ const base::FeatureParam<bool> kOnlyUseTitlesForSimilarity(
     "ContextualTasksContextOnlyUseTitles",
     false);
 
+const base::FeatureParam<bool> kDeduplicateRelevantTabsByUrl(
+    &kContextualTasksContext,
+    "ContextualTasksContextDeduplicateByUrl",
+    false);
+
 const base::FeatureParam<double> kTabSelectionScoreThreshold{
     &kContextualTasksContext,
     "ContextualTasksContextTabSelectionScoreThreshold", 0.4};
