8afc31f6b21a3e56087b8c7cac86fb7175d99363
Helmut Januschka <helmut@januschka.com>
2026-05-19 09:52:47 -0700

    Support closest-corner/farthest-corner for circle()/ellipse()
    Bug: 361617757
    Change-Id: I8d2dce599f2f1cc3960dc178cc73b63d569d3429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7767079
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1632955}

--- third_party/blink/renderer/core/css/basic_shape_functions.cc
+++ third_party/blink/renderer/core/css/basic_shape_functions.cc
@@ -419,6 +419,10 @@ static CSSValue* BasicShapeRadiusToCSSValue(const ComputedStyle& style,
       return CSSIdentifierValue::Create(CSSValueID::kClosestSide);
     case BasicShapeRadius::kFarthestSide:
       return CSSIdentifierValue::Create(CSSValueID::kFarthestSide);
+    case BasicShapeRadius::kClosestCorner:
+      return CSSIdentifierValue::Create(CSSValueID::kClosestCorner);
+    case BasicShapeRadius::kFarthestCorner:
+      return CSSIdentifierValue::Create(CSSValueID::kFarthestCorner);
   }
 
   NOTREACHED();
@@ -647,6 +651,10 @@ static BasicShapeRadius CssValueToBasicShapeRadius(
         return BasicShapeRadius(BasicShapeRadius::kClosestSide);
       case CSSValueID::kFarthestSide:
         return BasicShapeRadius(BasicShapeRadius::kFarthestSide);
+      case CSSValueID::kClosestCorner:
+        return BasicShapeRadius(BasicShapeRadius::kClosestCorner);
+      case CSSValueID::kFarthestCorner:
+        return BasicShapeRadius(BasicShapeRadius::kFarthestCorner);
       default:
         NOTREACHED();
     }
