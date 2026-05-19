fc54734439ea3157c52d817aecfe128df834ae6c
Viktor Semeniuk <vsemeniuk@google.com>
2026-05-19 00:31:53 -0700

    Extract change password form filling capabilities into a new class
    This change extracts change password form filling from
    ChangePasswordFormFillingSubmissionHelper into a new class. This class
    will be reused for password change from checkup.
    
    This is pure refactoring, there are no behavioral changes.
    
    Bug: 512717933
    Change-Id: Ie34e34ab066f912f1f91586a4fe31b64152150e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852567
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632668}

--- chrome/browser/password_manager/password_change/change_password_form_filling_submission_helper_unittest.cc
+++ chrome/browser/password_manager/password_change/change_password_form_filling_submission_helper_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/factories/password_manager_settings_service_factory.h"
 #include "chrome/browser/password_manager/factories/profile_password_store_factory.h"
 #include "chrome/browser/password_manager/password_change/annotated_page_content_capturer.h"
+#include "chrome/browser/password_manager/password_change/change_password_form_filler.h"
 #include "chrome/browser/password_manager/password_change/change_password_form_waiter.h"
 #include "chrome/browser/password_manager/password_change/fake_annotated_page_content_capturer.h"
 #include "chrome/browser/password_manager/password_change/model_quality_logs_uploader.h"
@@ -531,7 +532,7 @@ TEST_F(ChangePasswordFormFillingSubmissionHelperTest,
 
   CompleteFormFilling(form_manager, verifier.get(), std::nullopt);
 
-  ASSERT_TRUE(verifier->form_waiter());
+  ASSERT_TRUE(verifier->form_filler()->form_waiter());
 
   task_environment()->AdvanceClock(base::Seconds(10));
 
@@ -555,7 +556,7 @@ TEST_F(ChangePasswordFormFillingSubmissionHelperTest, ProvisionallySaveFailed) {
   CompleteFormFilling(form_manager, verifier.get(),
                       CreateEmptyTestPasswordFormData());
 
-  EXPECT_TRUE(verifier->form_waiter());
+  EXPECT_TRUE(verifier->form_filler()->form_waiter());
 
   autofill::FormData new_form_data =
       CreateTestPasswordFormData("", "", 101, 102);
@@ -570,7 +571,7 @@ TEST_F(ChangePasswordFormFillingSubmissionHelperTest, ProvisionallySaveFailed) {
   EXPECT_CALL(driver(), FillChangePasswordForm)
       .WillOnce(base::test::RunOnceCallback<5>(filled_form));
   static_cast<password_manager::PasswordFormManagerObserver*>(
-      verifier->form_waiter())
+      verifier->form_filler()->form_waiter())
       ->OnPasswordFormParsed(new_form_manager);
 
   EXPECT_TRUE(
@@ -685,7 +686,7 @@ TEST_F(ChangePasswordFormFillingSubmissionHelperTest,
       QualityStatus::
           PasswordChangeQuality_StepQuality_SubmissionStatus_FORM_FILLING_FAILED);
 
-  EXPECT_TRUE(verifier->form_waiter());
+  EXPECT_TRUE(verifier->form_filler()->form_waiter());
   autofill::FormData new_form_data =
       CreateTestPasswordFormData("", "", 101, 102);
   new_form_data.set_renderer_id(autofill::test::MakeFormRendererId());
@@ -698,7 +699,7 @@ TEST_F(ChangePasswordFormFillingSubmissionHelperTest,
   EXPECT_CALL(driver(), FillChangePasswordForm)
       .WillOnce(base::test::RunOnceCallback<5>(filled_form));
   static_cast<password_manager::PasswordFormManagerObserver*>(
-      verifier->form_waiter())
+      verifier->form_filler()->form_waiter())
       ->OnPasswordFormParsed(new_form_manager);
 
   EXPECT_TRUE(
@@ -739,13 +740,13 @@ TEST_F(ChangePasswordFormFillingSubmissionHelperTest,
   CompleteFormFilling(form_manager, verifier.get(), std::nullopt);
 
   // A form waiter should be created.
-  ASSERT_TRUE(verifier->form_waiter());
+  ASSERT_TRUE(verifier->form_filler()->form_waiter());
 
   // If the same form is parsed again, it should be ignored.
   // No new filling attempt should be made.
   EXPECT_CALL(driver(), FillChangePasswordForm).Times(0);
   static_cast<password_manager::PasswordFormManagerObserver*>(
-      verifier->form_waiter())
+      verifier->form_filler()->form_waiter())
       ->OnPasswordFormParsed(form_manager);
 
   // To ensure no async tasks are pending that would call
