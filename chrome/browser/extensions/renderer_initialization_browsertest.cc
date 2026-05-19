cd81d80a8fc30492f8160729006fb376a99771f5
James Cook <jamescook@chromium.org>
2026-05-19 16:32:47 -0700

    extensions: Add platform assert to RendererInitializationTest
    By convention we add ENABLE_EXTENSIONS_CORE to tests that work on
    desktop Android. I forgot to add this when I ported the test.
    
    Bug: 469417243
    Change-Id: Id5f3ef9411d95ceb5e953e44b84b1f024678c4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859988
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633184}

--- chrome/browser/extensions/renderer_initialization_browsertest.cc
+++ chrome/browser/extensions/renderer_initialization_browsertest.cc
@@ -9,12 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/extension_registrar.h"
+#include "extensions/buildflags/buildflags.h"
 #include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/themes/theme_service.h"
 #endif
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 namespace {
