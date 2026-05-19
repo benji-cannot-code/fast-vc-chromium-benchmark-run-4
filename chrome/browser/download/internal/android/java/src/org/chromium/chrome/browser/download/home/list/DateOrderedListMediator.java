840fc57c7d06de437635153eb9ac19377eab1df9
Nancy Xiao <nancylanxiao@google.com>
2026-05-19 07:22:46 -0700

    Add blocked sensitive offline item filter
    We should show the blocked sensitive download item in the download home UI as per UX deck: http://docs/presentation/d/1wZPvidIrVvSZSYjNj3JsMqLIKou9Yk3xiazk4Vk9Qsc?slide=id.g2b5cf487a3a_0_12&resourcekey=0-MY9_UZ-l5pJayFZ1MGqr-w#slide=id.g2b5cf487a3a_0_12
    
    Screencast: https://screencast.googleplex.com/cast/NTM1NDg0MDcyODI3Mjg5NnxhZWViYTlkMS02Ng
    
    Bug: b:466101570, b:428696481
    Change-Id: Id45b00884dd0f82c621a44b81c5b9ee0608f6d5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852777
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632841}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListMediator.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListMediator.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.download.home.DownloadManagerUiConfig;
 import org.chromium.chrome.browser.download.home.FaviconProvider;
 import org.chromium.chrome.browser.download.home.JustNowProvider;
 import org.chromium.chrome.browser.download.home.OfflineItemSource;
+import org.chromium.chrome.browser.download.home.filter.BlockedSensitiveOfflineItemFilter;
 import org.chromium.chrome.browser.download.home.filter.DangerousOfflineItemFilter;
 import org.chromium.chrome.browser.download.home.filter.DeleteUndoOfflineItemFilter;
 import org.chromium.chrome.browser.download.home.filter.Filters.FilterType;
@@ -133,6 +134,7 @@ class DateOrderedListMediator implements BackPressHandler {
     private final SelectionDelegate<ListItem> mSelectionDelegate;
     private final DownloadManagerUiConfig mUiConfig;
 
+    private final BlockedSensitiveOfflineItemFilter mBlockedSensitiveFilter;
     private final DangerousOfflineItemFilter mDangerousFilter;
     private final OffTheRecordOfflineItemFilter mOffTheRecordFilter;
     private final InvalidStateOfflineItemFilter mInvalidStateFilter;
@@ -203,11 +205,12 @@ class DateOrderedListMediator implements BackPressHandler {
         // Build a chain from the data source to the model.  The chain will look like:
         // [OfflineContentProvider] ->
         //     [OfflineItemSource] ->
-        //         [DangerousOfflineItemFilter] ->
-        //             [OffTheRecordOfflineItemFilter] ->
-        //                 [InvalidStateOfflineItemFilter] ->
-        //                     [DeleteUndoOfflineItemFilter] ->
-        //                         [SearchOfflineItemFilter] ->
+        //         [BlockedSensitiveOfflineItemFilter] ->
+        //             [DangerousOfflineItemFilter] ->
+        //                 [OffTheRecordOfflineItemFilter] ->
+        //                     [InvalidStateOfflineItemFilter] ->
+        //                         [DeleteUndoOfflineItemFilter] ->
+        //                             [SearchOfflineItemFilter] ->
         //                             [TypeOfflineItemFilter] ->
         //                                 [DateOrderedListMutator] ->
         //                                     [ListItemModel]
@@ -225,7 +228,8 @@ class DateOrderedListMediator implements BackPressHandler {
         mUiConfig = config;
 
         mSource = new OfflineItemSource(mProvider);
-        mDangerousFilter = new DangerousOfflineItemFilter(config, mSource);
+        mBlockedSensitiveFilter = new BlockedSensitiveOfflineItemFilter(config, mSource);
+        mDangerousFilter = new DangerousOfflineItemFilter(config, mBlockedSensitiveFilter);
         mOffTheRecordFilter =
                 new OffTheRecordOfflineItemFilter(
                         OtrProfileId.isOffTheRecord(config.otrProfileId), mDangerousFilter);
