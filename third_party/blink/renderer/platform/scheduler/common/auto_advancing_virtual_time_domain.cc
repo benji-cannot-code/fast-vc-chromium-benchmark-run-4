f6fb6c2c8596dd28786e02a91e028efb2d5f2ae6
Etienne Pierre-doray <etiennep@chromium.org>
2026-05-19 14:23:26 -0700

    [task] Fix AutoAdvancingVirtualTimeDomain lifetime
    To honor TimeDomain contract, AutoAdvancingVirtualTimeDomain
    is detached from its observer instead of destroyed in
    ThreadSchedulerBase::Shutdown.
    
    We then rely on ~ThreadSchedulerBase to clear virtual_time_domain_,
    at which point it's safe to do so because it happens after
    all queues are shutdown, synchronized by OperationsController.
    
    Bug: 513544566
    Change-Id: Ib3747306708e0f94f623d74938f4cb9d05e82b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7857479
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633105}

--- third_party/blink/renderer/platform/scheduler/common/auto_advancing_virtual_time_domain.cc
+++ third_party/blink/renderer/platform/scheduler/common/auto_advancing_virtual_time_domain.cc
@@ -33,7 +33,16 @@ AutoAdvancingVirtualTimeDomain::AutoAdvancingVirtualTimeDomain(
 }
 
 AutoAdvancingVirtualTimeDomain::~AutoAdvancingVirtualTimeDomain() {
-  helper_->RemoveTaskObserver(this);
+  if (helper_) {
+    helper_->RemoveTaskObserver(this);
+  }
+}
+
+void AutoAdvancingVirtualTimeDomain::ShutDown() {
+  if (helper_) {
+    helper_->RemoveTaskObserver(this);
+    helper_ = nullptr;
+  }
 }
 
 base::TimeTicks AutoAdvancingVirtualTimeDomain::NowTicks() const {
