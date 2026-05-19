9166420c29bc22658cbc7e56e26c3d332874950c
Luke Halliwell <halliwell@google.com>
2026-05-18 19:45:20 -0700

    chromecast: Clean up IS_CAST_AUDIO_ONLY in browser layer
    Following the permanent deprecation of audio-only builds, this CL
    removes audio-only preprocessor branches and un-nests standard video
    branches across chromecast/browser/.
    
    Bug: 41489655
    Test: cast_shell_browsertests
    Change-Id: I8e738eaefd42d7c2a3a92002f680c696530d6a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842927
    Commit-Queue: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632581}

--- chromecast/browser/cast_display_configurator.cc
+++ chromecast/browser/cast_display_configurator.cc
@@ -36,9 +36,6 @@ constexpr char kCastGraphicsHeight[] = "cast-graphics-height";
 constexpr char kCastGraphicsWidth[] = "cast-graphics-width";
 
 gfx::Size GetDefaultScreenResolution() {
-#if BUILDFLAG(IS_CAST_AUDIO_ONLY)
-  return gfx::Size(1, 1);
-#else
   const base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
   if (!chromecast::IsFeatureEnabled(kTripleBuffer720) &&
       GraphicsPropertiesShlib::IsSupported(GraphicsPropertiesShlib::k1080p,
@@ -47,7 +44,6 @@ gfx::Size GetDefaultScreenResolution() {
   }
 
   return gfx::Size(1280, 720);
-#endif
 }
 
 // Helper to return the screen resolution (device pixels)
@@ -99,7 +95,7 @@ gfx::Rect GetScreenBounds(const gfx::Size& size_in_pixels,
 
 CastDisplayConfigurator::CastDisplayConfigurator(CastScreen* screen)
     : delegate_(
-#if BUILDFLAG(IS_OZONE) && !BUILDFLAG(IS_CAST_AUDIO_ONLY)
+#if BUILDFLAG(IS_OZONE)
           ui::OzonePlatform::GetInstance()->CreateNativeDisplayDelegate()
 #else
           nullptr
