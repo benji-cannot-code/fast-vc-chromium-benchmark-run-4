33ee53683f8747d309f08b97cb081efae9cb16da
Jingping Sun <jings@microsoft.com>
2026-05-19 06:25:13 -0700

    [TabModel] Remove deprecated TabGroupModelFilter accessors
    Deletes three @Deprecated TabGroupModelFilter accessors from
    TabModelSelector and the backing supplier chain that fed them. Renames
    resetTabGroupModelFilterListForTesting to resetTabModelListForTesting.
    
    Bug: 493519189
    Change-Id: I51693b4c55c4838a4789647892382a5e8bbf9f31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851657
    Commit-Queue: Jingping Sun <jings@microsoft.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632810}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/tabmodel/MockTabModelSelector.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/tabmodel/MockTabModelSelector.java
@@ -68,7 +68,7 @@ public class MockTabModelSelector extends TabModelSelectorBase {
     public void initializeTabModels(
             TabModelInternal normalModel, IncognitoTabModelInternal incognitoModel) {
         destroy();
-        resetTabGroupModelFilterListForTesting();
+        resetTabModelListForTesting();
         initialize(
                 TabModelHolderFactory.createTabModelHolderForTesting(normalModel),
                 TabModelHolderFactory.createIncognitoTabModelHolderForTesting(incognitoModel));
