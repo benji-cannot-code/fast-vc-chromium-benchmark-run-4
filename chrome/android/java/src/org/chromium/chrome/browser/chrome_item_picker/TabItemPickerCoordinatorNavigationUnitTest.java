9313e5f989e6b153ab464241f7f2b14f49fa7244
Calder Kitagawa <ckitagawa@chromium.org>
2026-05-19 13:45:48 -0700

    [Tab Context] Create separate LoadIfNeededService
    Move the state out of TabItemPickerCoordinator.
    
    Bug: 513358184
    Change-Id: If431276993e3618178a94f890f1e22f6e8d6076d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853580
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633095}

--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinatorNavigationUnitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinatorNavigationUnitTest.java
@@ -48,6 +48,7 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
+import org.chromium.chrome.browser.tab.utilities.LoadIfNeededService;
 import org.chromium.chrome.browser.tab_ui.TabContentManager;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
@@ -87,6 +88,7 @@ public class TabItemPickerCoordinatorNavigationUnitTest {
 
     @Before
     public void setUp() {
+        LoadIfNeededService.getInstance().clearForTesting();
         OneshotSupplierImpl<Profile> profileSupplierImpl = new OneshotSupplierImpl<>();
         ViewGroup rootView = Mockito.mock(ViewGroup.class);
         ViewGroup containerView = Mockito.mock(ViewGroup.class);
@@ -392,7 +394,7 @@ public class TabItemPickerCoordinatorNavigationUnitTest {
         mNavigationProvider.onSelectionStateChange(selection);
         mNavigationProvider.onSelectionStateChange(selection);
 
-        verify(tab, times(2)).loadIfNeeded(anyBoolean());
+        verify(tab, times(1)).loadIfNeeded(anyBoolean());
     }
 
     @Test
@@ -455,7 +457,7 @@ public class TabItemPickerCoordinatorNavigationUnitTest {
         mNavigationProvider.onSelectionStateChange(selection);
         mNavigationProvider.onSelectionStateChange(selection);
 
-        verify(tab, times(2)).loadIfNeeded(anyBoolean());
+        verify(tab, times(1)).loadIfNeeded(anyBoolean());
         // cacheTabThumbnailWithCallback is not called yet because the tab is still loading.
         verify(mTabContentManager, never())
                 .cacheTabThumbnailWithCallback(any(), anyBoolean(), any());
