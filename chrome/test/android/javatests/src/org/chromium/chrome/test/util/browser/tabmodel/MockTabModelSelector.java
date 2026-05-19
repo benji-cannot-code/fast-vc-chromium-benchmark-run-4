969d1ac459d51b9c8cf409a6c0442c3a7c311abe
Peter Pakkenberg <pbirk@chromium.org>
2026-05-19 08:54:09 -0700

    Revert "[TabModel] Remove deprecated TabGroupModelFilter accessors"
    This reverts commit c0a50ed14aab40bdc79e129dd9db0bcd8682ea97.
    
    Reason for revert: compile failure https://crbug.com/514648028
    
    Original change's description:
    > [TabModel] Remove deprecated TabGroupModelFilter accessors
    >
    > Deletes three @Deprecated TabGroupModelFilter accessors from
    > TabModelSelector and the backing supplier chain that fed them. Renames
    > resetTabGroupModelFilterListForTesting to resetTabModelListForTesting.
    >
    > Bug: 493519189
    > Change-Id: I51693b4c55c4838a4789647892382a5e8bbf9f31
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851657
    > Commit-Queue: Jingping Sun <jings@microsoft.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1632810}
    
    Bug: 514648028
    Bug: 493519189
    Change-Id: I545e1ee835566c4873490c0412ffa650ccc688f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859561
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Jingping Sun <jings@microsoft.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632860}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/tabmodel/MockTabModelSelector.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/tabmodel/MockTabModelSelector.java
@@ -68,7 +68,7 @@ public class MockTabModelSelector extends TabModelSelectorBase {
     public void initializeTabModels(
             TabModelInternal normalModel, IncognitoTabModelInternal incognitoModel) {
         destroy();
-        resetTabModelListForTesting();
+        resetTabGroupModelFilterListForTesting();
         initialize(
                 TabModelHolderFactory.createTabModelHolderForTesting(normalModel),
                 TabModelHolderFactory.createIncognitoTabModelHolderForTesting(incognitoModel));
