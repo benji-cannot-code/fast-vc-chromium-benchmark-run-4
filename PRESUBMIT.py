ea65a9ae8e11632e1116e23bbe328304016c3717
Sam Maier <smaier@google.com>
2026-05-19 13:32:14 -0700

    Reland "Use R8's resource shrinker"
    This reverts commit 8be28b90b97d525daee0891c76d9075e1aa5bffc.
    
    Reason for reland: tested chrome_bundle install and
    chrome_test_apk --list-tests
    
    Original change's description:
    > Revert "Use R8's resource shrinker"
    >
    > This reverts commit bc1ff55d24abcb3af7ad21c9cacf134ad88a2ce2.
    >
    > Reason for revert: Failing to list tests in some configs
    > https://luci-milo.appspot.com/ui/p/chrome/builders/ci/android-x64-rel-ready/47402/overview
    >
    > Original change's description:
    > > Use R8's resource shrinker
    > >
    > > This deletes our custom unused resources code, and uses an extra output
    > > from R8 (shrunk.proto.ap_) to feed as input to optimize_resources.
    > >
    > > This also removes any seperately-applicable resource shrinking
    > > (shortening names, stripping names, stripping unused) and just has a
    > > single global gn arg (optimize_resources) which does all
    > > optimizations.
    > >
    > > Based on some quick local benchmarks, it seems this adds ~10s to
    > > !is_java_debug (versus having it off completely), so I think it's fine
    > > to keep on !is_java_debug vs is_official_build.
    > >
    > > Bug: 362524381
    > > Link: https://chromium-review.googlesource.com/id/I3984a4ddfbc665191fec7819ff897e666a6a6964
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614749
    > > Owners-Override: Sam Maier <smaier@google.com>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Commit-Queue: Sam Maier <smaier@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1630915}
    >
    > Bug: 362524381
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I2f53d05e8915fe4ef9465a2f96432121bc328a6e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850882
    > Owners-Override: Sam Maier <smaier@google.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Sam Maier <smaier@google.com>
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1631241}
    
    Bug: 362524381
    Link: https://chromium-review.googlesource.com/id/I34886a0dc3274f202dc434d66f41103d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7854136
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633088}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1854,10 +1854,11 @@ _BANNED_CPP_FUNCTIONS: Sequence[BanRule] = (
     ),
     BanRule(
         r'/\bperfetto::Track::Global',
-        ('Creating new global tracks is discouraged and should be reserved ',
-        'for high level, user visible state. Consider using scoped tracks ',
-        'instead, see ',
-        'https://chromium.googlesource.com/chromium/src.git/+/main/docs/trace_events.md#named-tracks',
+        (
+            'Creating new global tracks is discouraged and should be reserved ',
+            'for high level, user visible state. Consider using scoped tracks ',
+            'instead, see ',
+            'https://chromium.googlesource.com/chromium/src.git/+/main/docs/trace_events.md#named-tracks',
         ),
         False,
         (
@@ -1867,9 +1868,10 @@ _BANNED_CPP_FUNCTIONS: Sequence[BanRule] = (
     ),
     BanRule(
         r'/\bperfetto::Track::FromPointer',
-        ('Creating tracks from pointer is discouraged because it risks aliasing when the address ',
-         'is reused. Consider using NamedTrack instead, see ',
-        'https://chromium.googlesource.com/chromium/src.git/+/main/docs/trace_events.md#named-tracks',
+        (
+            'Creating tracks from pointer is discouraged because it risks aliasing when the address ',
+            'is reused. Consider using NamedTrack instead, see ',
+            'https://chromium.googlesource.com/chromium/src.git/+/main/docs/trace_events.md#named-tracks',
         ),
         False,
         (),
@@ -2580,7 +2582,6 @@ _GENERIC_PYDEPS_FILES = [
     'build/android/gyp/trace_event_bytecode_rewriter.pydeps',
     'build/android/gyp/tracereferences.pydeps',
     'build/android/gyp/turbine.pydeps',
-    'build/android/gyp/unused_resources.pydeps',
     'build/android/gyp/write_build_config.pydeps',
     'build/android/gyp/write_native_libraries_java.pydeps',
     'build/android/gyp/zip.pydeps',
@@ -2820,7 +2821,7 @@ def CheckNoProductionCodeUsingTestOnlyFunctionsJava(input_api, output_api):
     # exclusion_re misses the closing ''''} when it wraps to the next line, support an on demand
     # multi-line check as a last step.
     multi_line_exclusion_re = input_api.re.compile(
-            r'(%s)[^;]+\{' % name_pattern, input_api.re.DOTALL)
+        r'(%s)[^;]+\{' % name_pattern, input_api.re.DOTALL)
 
     problems = []
     sources = lambda x: input_api.FilterSourceFile(
@@ -2847,7 +2848,8 @@ def CheckNoProductionCodeUsingTestOnlyFunctionsJava(input_api, output_api):
 
                 if cached_file_lines is None:
                     cached_file_lines = input_api.ReadFile(f).splitlines()
-                full_text_from_line = '\n'.join(cached_file_lines[line_number - 1:])
+                full_text_from_line = '\n'.join(cached_file_lines[line_number -
+                                                                  1:])
                 match = multi_line_exclusion_re.search(full_text_from_line)
                 if not match or match.start() >= len(line):
                     problems.append('%s:%d\n    %s' %
