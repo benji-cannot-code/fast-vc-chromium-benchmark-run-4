8d655ef382fc4dfe0600ecd5e3b306bc6fc637dd
S Ganesh <ganesh@chromium.org>
2026-05-19 10:59:14 -0700

    Revert "[Vertical Tabs] Add chrome://flags for two toolbar resizing flags"
    This reverts commit 8ecf2a219e315bfedc22aabdfe2c69b3dde7b11b.
    
    Reason for revert: crrev.com/c/7858089 added a duplicated base::Feature. Ty to victorvianna!
    
    
    Original change's description:
    > [Vertical Tabs] Add chrome://flags for two toolbar resizing flags
    >
    > Bug: 512851186,510345999
    > Change-Id: Ic9f9e442dc2ebb76660ba204b57660385b9ee8ed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858089
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Commit-Queue: Alison Gale <agale@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1632980}
    
    Bug: 512851186,510345999
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I290a8e0c142b37e7f557dbbb6e33d3a313178071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859660
    Owners-Override: S Ganesh <ganesh@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1632994}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -519,8 +519,6 @@ BASE_FEATURE_PARAM(bool,
 
 BASE_FEATURE(kOmniboxResizingPrioritization, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kToolbarProfileChipResizing, base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kToolbarAppMenuLabelResizing, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kToolbarProfileChipResizing, base::FEATURE_DISABLED_BY_DEFAULT);
