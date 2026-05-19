41b96da093a1ed71fca2b18283f9711c197aad91
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-05-19 11:10:17 -0700

    [Reland] Introduce unique_ptr based CreateBubble for bubble widgets
    Initial CL: https://crrev.com/c/7793292
    
    Issue: The CL get reverted
    because a leak happen in ash_components_unittests. The CreateBubble(..)
    introduced by this CL was taking a unique_ptr and then dropping the
    ownership which was causing a leak. This CL, update CreateBubble(..) to
    take a raw pointer. Therefore, the caller is responsible to manage the
    lifetime of the delegate provided.
    
    Refactor bubble creation logic to provide a safer and more modern API.
    This change introduces a new CreateBubble static method that returns a
    std::unique_ptr<Widget> and defaults to CLIENT_OWNS_WIDGET ownership.
    This approach helps prevent memory leaks and clarifies widget lifecycle
    management.
    
    Key modifications include:
    * Adding CreateBubbleInternal to consolidate shared initialization
        logic, including anchor view setup and observer attachment.
    * Supporting an optional on_close callback in the new creation flow,
        allowing for synchronous handling of widget closure.
    * Marking existing CreateBubbleDeprecated methods with warnings to
        discourage their continued use in favor of the unique_ptr variant.
    
    Bug: 510617577
    Change-Id: Ibf545e9c366ca66d9c85ab6aba9e5dfb82d48ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838679
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633010}

--- chrome/test/interaction/interactive_browser_test_interactive_uitest.cc
+++ chrome/test/interaction/interactive_browser_test_interactive_uitest.cc
@@ -533,8 +533,7 @@ class WebBubbleView : public views::BubbleDialogDelegateView {
     auto bubble_ptr = base::WrapUnique(
         new WebBubbleView(browser_view->toolbar(), browser->profile(), url));
     auto* const bubble = bubble_ptr.get();
-    views::BubbleDialogDelegateView::CreateBubble(std::move(bubble_ptr))
-        ->Show();
+    views::BubbleDialogDelegateView::CreateBubble(bubble_ptr.release())->Show();
     return bubble;
   }
 
@@ -745,10 +744,8 @@ class InteractiveBrowserTestHoverUiTest : public InteractiveBrowserTestUiTest {
     CHECK(anchor_view);
     auto bubble_view = std::make_unique<HoverDetectionBubbleView>(anchor_view);
     bubble_view_ = bubble_view.get();
-    bubble_widget_ =
-        base::WrapUnique(views::BubbleDialogDelegate::CreateBubbleDeprecated(
-            std::move(bubble_view),
-            views::Widget::InitParams::CLIENT_OWNS_WIDGET));
+    bubble_widget_ = views::BubbleDialogDelegate::CreateBubble(
+        std::move(bubble_view).release());
     bubble_widget_->Show();
     bubble_view_->SizeToContents();
   }
--- chromeos/ash/experiences/arc/compat_mode/resize_toggle_menu.h
+++ chromeos/ash/experiences/arc/compat_mode/resize_toggle_menu.h
@@ -78,7 +78,6 @@ class ResizeToggleMenu : public views::WidgetObserver,
   ~ResizeToggleMenu() override;
 
   // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override;
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
 
@@ -92,6 +91,8 @@ class ResizeToggleMenu : public views::WidgetObserver,
 
  private:
   friend class ResizeToggleMenuTest;
+
+  void OnBubbleWidgetClosing(views::Widget::ClosedReason reason);
 
   void UpdateSelectedButton();
 
@@ -112,8 +113,8 @@ class ResizeToggleMenu : public views::WidgetObserver,
 
   raw_ptr<ArcResizeLockPrefDelegate> pref_delegate_;
 
-  base::ScopedMultiSourceObservation<views::Widget, views::WidgetObserver>
-      widget_observations_{this};
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      widget_observation_{this};
   base::ScopedObservation<aura::Window, aura::WindowObserver>
       window_observation_{this};
 
