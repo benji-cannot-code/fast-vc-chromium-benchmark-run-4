ee97d81a95c73cbdc1f962809558ed4b11529a78
Li XB <sweeper1203@gmail.com>
2026-05-19 09:30:24 -0700

    [Windows] Get subject name with CertGetNameStringW
    Change-Id: I575650e082d6fc083408058d4e87119cdd3ce959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7839558
    Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632918}

--- components/device_signals/core/system_signals/win/win_platform_delegate.cc
+++ components/device_signals/core/system_signals/win/win_platform_delegate.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/strings/string_util.h"
 #include "base/strings/string_util_win.h"
+#include "base/strings/utf_string_conversions.h"
 #include "components/device_signals/core/common/common_types.h"
 #include "components/device_signals/core/common/platform_utils.h"
 #include "crypto/scoped_capi_types.h"
@@ -78,16 +79,16 @@ std::pair<std::optional<std::string>, std::optional<std::string>> GetSPKIHash(
 
     // Get the subject. First ask how long the name is, including null
     // terminator.
-    size_t length = CertGetNameStringA(
+    size_t length = CertGetNameStringW(
         cert_context, CERT_NAME_SIMPLE_DISPLAY_TYPE, /*dwFlags=*/0,
         /*pvTypePara=*/nullptr, /*pszNameString=*/nullptr, /*cchNameString=*/0);
     if (length > 0) {
-      std::vector<char> subject(length);
-      CertGetNameStringA(
+      std::vector<wchar_t> subject(length);
+      CertGetNameStringW(
           cert_context, CERT_NAME_SIMPLE_DISPLAY_TYPE, /*dwFlags=*/0,
           /*pvTypePara=*/nullptr, /*pszNameString=*/subject.data(),
           /*cchNameString=*/subject.size());
-      ret.second = subject.data();
+      ret.second = base::WideToUTF8(subject.data());
     }
   }
 
