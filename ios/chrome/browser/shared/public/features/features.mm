b956ad1826f41737cd451015ea950f57a813fab6
Jérôme Lebel <jlebel@chromium.org>
2026-05-19 05:45:17 -0700

    [iOS] Remove deprecated function IsDisableU18FeedbackIosEnabled()
    - crrev.com/c/7844993
    - crrev.com/i/9292260
    > crrev.com/c/7841738
    
    Bug: 512382367
    Change-Id: Iaf7627a042ce49611c9625ed74383cf44ed75235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841738
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632791}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1181,10 +1181,6 @@ bool IsDisableFeedbackForIneligibleUsersEnabled() {
   return base::FeatureList::IsEnabled(kDisableFeedbackForIneligibleUsers);
 }
 
-bool IsDisableU18FeedbackIosEnabled() {
-  return IsDisableFeedbackForIneligibleUsersEnabled();
-}
-
 BASE_FEATURE(kFullscreenRefactoring, base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsFullscreenRefactoringEnabled() {
