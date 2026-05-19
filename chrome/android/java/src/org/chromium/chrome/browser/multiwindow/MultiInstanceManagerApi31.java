420b10d624cc2429ed4e3852d75af903ddf51acb
Aishwarya Rajesh <aishwaryarj@google.com>
2026-05-18 18:38:21 -0700

    [OSWP] Refrain from reusing an existing instance id in default id allocation logic for a newly created activity
    When Chrome starts a new ChromeTabbedActivity on Desktop Android or when
    the kOnStartupWindowPolicy feature flag is enabled, it will now refrain
    from reusing an existing task-unmapped instance id during the default id
    allocation logic, that was originally added to support opening the last
    accessed window on startup.
    
    This ensures a clean, independent window startup experience, adhering
    to requirements for the new default on-startup behavior.
    
    The update does not impact id allocation behavior for new activities
    that aim to intentionally revive an existing instance in a new or
    existing task.
    
    Bug: 482419446
    Change-Id: Idac25736fe6f0edebdca2a837d61fea9e0742f56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852745
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632555}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31.java
@@ -26,6 +26,7 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ApplicationStatus.ActivityStateListener;
 import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.DeviceInfo;
 import org.chromium.base.Log;
 import org.chromium.base.TimeUtils;
 import org.chromium.base.metrics.RecordHistogram;
@@ -44,6 +45,7 @@ import org.chromium.chrome.browser.RecentlyClosedEntriesManagerTrackerFactory;
 import org.chromium.chrome.browser.app.tabmodel.TabModelOrchestrator;
 import org.chromium.chrome.browser.app.tabwindow.TabWindowManagerSingleton;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.multiwindow.MultiInstanceState.MultiInstanceStateObserver;
@@ -439,6 +441,17 @@ class MultiInstanceManagerApi31 extends MultiInstanceManagerImpl
             if (ChromeMultiInstancePersistentStore.readMarkedForDeletion(i)) {
                 continue;
             }
+
+            boolean instanceExists = ChromeMultiInstancePersistentStore.hasInstance(i);
+            if (instanceExists
+                    && (DeviceInfo.isDesktop()
+                            && ChromeFeatureList.sOnStartupWindowPolicy.isEnabled())) {
+                // This supports updated default id allocation / startup behavior where a newly
+                // created activity will refrain from using existing instance state and will be
+                // created as a brand-new window instead.
+                continue;
+            }
+
             if (id == INVALID_WINDOW_ID
                     || ChromeMultiInstancePersistentStore.readLastAccessedTime(i)
                             > ChromeMultiInstancePersistentStore.readLastAccessedTime(id)) {
@@ -459,7 +472,7 @@ class MultiInstanceManagerApi31 extends MultiInstanceManagerImpl
                     continue;
                 }
                 id = i;
-                newInstanceIdAllocated = !ChromeMultiInstancePersistentStore.hasInstance(i);
+                newInstanceIdAllocated = !instanceExists;
                 allocationType =
                         newInstanceIdAllocated
                                 ? InstanceAllocationType.NEW_INSTANCE_NEW_TASK
