f5652999ae81ef047cc7164d5fcc49aaafabe841
Antonio Sartori <antoniosartori@chromium.org>
2026-05-19 00:13:55 -0700

    [permissions] Prevent fullscreen while prompt is shown
    Instead of just exiting fullscreen if a permission prompt is triggered
    in fullscreen, let's prevent fullescreen altogether while a permission
    prompt is shown.
    
    Change-Id: Ie989050d4959af8a6dd084f5f70e7ee3117ebbcb
    Bug: 514068972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849423
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632664}

--- chrome/browser/ui/views/permissions/permission_prompt_bubble.cc
+++ chrome/browser/ui/views/permissions/permission_prompt_bubble.cc
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
-#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
-#include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
-#include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.h"
 #include "chrome/browser/ui/views/permissions/permission_prompt_bubble_view_factory.h"
@@ -41,19 +38,12 @@ PermissionPromptBubble::~PermissionPromptBubble() {
 }
 
 void PermissionPromptBubble::ShowBubble() {
-  FullscreenController* fullscreen_controller = browser()
-                                                    ->GetFeatures()
-                                                    .exclusive_access_manager()
-                                                    ->fullscreen_controller();
-  CHECK(fullscreen_controller);
-  if (fullscreen_controller->IsTabFullscreen()) {
     auto blocker =
         web_contents()->ForSecurityDropFullscreen(display::kInvalidDisplayId);
     if (!blocker) {
       return;
     }
     fullscreen_blocker_ = std::move(*blocker);
-  }
 
   raw_ptr<PermissionPromptBubbleBaseView> prompt_bubble =
       CreatePermissionPromptBubbleView(browser(), delegate()->GetWeakPtr(),
