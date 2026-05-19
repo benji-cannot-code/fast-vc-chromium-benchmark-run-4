fedac256cb281be5dc515bae491091904541c74a
Min Qin <qinmin@chromium.org>
2026-05-19 13:14:05 -0700

    Post a task to inform Controller about download Updates to avoid reentrance
    Bug: 504191496
    Change-Id: I346fdca8c8069d12de9b40778df4f96abde3dfe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7860905
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633075}

--- components/download/content/internal/download_driver_impl_unittest.cc
+++ components/download/content/internal/download_driver_impl_unittest.cc
@@ -156,11 +156,13 @@ TEST_F(DownloadDriverImplTest, DownloadItemUpdateEvents) {
       .RetiresOnSaturation();
   static_cast<AllDownloadEventNotifier::Observer*>(driver_.get())
       ->OnDownloadUpdated(&coordinator_, &fake_item);
+  task_runner_->RunUntilIdle();
 
   // Nothing happens for cancelled state.
   fake_item.SetState(DownloadState::CANCELLED);
   static_cast<AllDownloadEventNotifier::Observer*>(driver_.get())
       ->OnDownloadUpdated(&coordinator_, &fake_item);
+  task_runner_->RunUntilIdle();
 
   fake_item.SetReceivedBytes(1024);
   fake_item.SetState(DownloadState::COMPLETE);
@@ -171,6 +173,7 @@ TEST_F(DownloadDriverImplTest, DownloadItemUpdateEvents) {
       .RetiresOnSaturation();
   static_cast<AllDownloadEventNotifier::Observer*>(driver_.get())
       ->OnDownloadUpdated(&coordinator_, &fake_item);
+  task_runner_->RunUntilIdle();
 
   fake_item.SetState(DownloadState::INTERRUPTED);
   fake_item.SetLastReason(
@@ -182,6 +185,7 @@ TEST_F(DownloadDriverImplTest, DownloadItemUpdateEvents) {
       .RetiresOnSaturation();
   static_cast<AllDownloadEventNotifier::Observer*>(driver_.get())
       ->OnDownloadUpdated(&coordinator_, &fake_item);
+  task_runner_->RunUntilIdle();
 }
 
 TEST_F(DownloadDriverImplTest, TestGetActiveDownloadsCall) {
