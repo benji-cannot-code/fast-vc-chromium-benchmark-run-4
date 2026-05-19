3e1159381ede8caf63fd91a915b6e194417db183
Alison Gale <agale@chromium.org>
2026-05-19 09:45:34 -0700

    [Vertical Tabs] Resize profile avatar as window shrinks
    Bug: 510345999
    Change-Id: I403b9d803f6805a86ce3f70bd010f5ee8f5f4a73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830625
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632943}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -372,6 +372,8 @@ BASE_DECLARE_FEATURE(kOmniboxResizingPrioritization);
 
 BASE_DECLARE_FEATURE(kToolbarAppMenuLabelResizing);
 
+BASE_DECLARE_FEATURE(kToolbarProfileChipResizing);
+
 // Whether or not OSCryptAsyncAvailabilityInfoBarDelegate is enabled.
 BASE_DECLARE_FEATURE(kOSCryptAsyncAvailabilityInfoBar);
 
