8692a910398f8e33fd002b920c41d1514f8e93e4
Maggie Chen <magchen@chromium.org>
2026-05-19 09:25:49 -0700

    Fix gpu_angle_passthrough_fuzzer: CHECK failure
    gpu_angle_passthrough_fuzzer: CHECK failure: handle && handle->task_runner_ in single_thread_task_runner.cc
    
    This CL adds a base::SingleThreadTaskExecutor to ensure the task_runner
    is available.
    
    Bug: 513273428
    Change-Id: I12f7030ec2773eb467c841dc46e3f64e5ae6b054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858122
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632911}

--- gpu/command_buffer/tests/fuzzer_main.cc
+++ gpu/command_buffer/tests/fuzzer_main.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/string_util.h"
+#include "base/task/single_thread_task_executor.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/constants.h"
 #include "gpu/command_buffer/common/context_creation_attribs.h"
@@ -302,6 +303,11 @@ class CommandBufferSetup {
  public:
   CommandBufferSetup()
       : at_exit_manager_(),
+#if BUILDFLAG(IS_MAC)
+        task_executor_(base::MessagePumpType::NS_RUNLOOP),
+#else
+        task_executor_(base::MessagePumpType::DEFAULT),
+#endif
         gpu_preferences_(GetGpuPreferences()),
         share_group_(new gl::GLShareGroup),
         translator_cache_(gpu_preferences_) {
@@ -625,6 +631,7 @@ class CommandBufferSetup {
   }
 
   base::AtExitManager at_exit_manager_;
+  base::SingleThreadTaskExecutor task_executor_;
 
   GpuPreferences gpu_preferences_;
 
