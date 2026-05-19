1afa476ef7e4fbd67c6d79a0e675fb0b41821ad6
Sophie Chang <sophiechang@chromium.org>
2026-05-18 22:58:30 -0700

    Disable the ContextualTasksContext feature in the NewTabPageBrowserTest
    Bug: 514448528
    Change-Id: Icdb7ccafc80ef8d19cc400c9c4362de57d9ce527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858299
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Abhay Prakash <abhayprakash@google.com>
    Commit-Queue: Abhay Prakash <abhayprakash@google.com>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632635}

--- chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
+++ chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "components/contextual_tasks/public/features.h"
 #include "components/history_clusters/core/features.h"
 #include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/search/ntp_features.h"
@@ -22,7 +23,8 @@ class NewTabPageBrowserTest : public WebUIMochaBrowserTest {
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/{},
         /*disabled_features=*/{omnibox::kAimServerEligibilityEnabled,
-                               ntp_realbox::kNtpRealboxNext});
+                               ntp_realbox::kNtpRealboxNext,
+                               contextual_tasks::kContextualTasksContext});
   }
 
  private:
