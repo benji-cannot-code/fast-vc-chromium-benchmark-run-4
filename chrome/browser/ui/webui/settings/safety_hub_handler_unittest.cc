2c8f4395b03828f1cd9e14a17ff6195a13df9e68
Bhaskar Sharma <bhaskarsharma@google.com>
2026-05-19 05:59:15 -0700

    Create new ContentSettingsType for adding SubApps
    This CL adds a new content setting called SUB_APPS_WITHOUT_PROMPTS that
    controls access to the subApps.add API in Isolated Web Apps.
    
    Bug: 512071943
    Change-Id: I4f66e96f3010d8177033e1362458e3bc965a565a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829450
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Bhaskar Sharma <bhaskarsharma@google.com>
    Reviewed-by: Vlad Krot <vkrot@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632800}

--- chrome/browser/ui/webui/settings/safety_hub_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/safety_hub_handler_unittest.cc
@@ -1051,6 +1051,7 @@ TEST_F(SafetyHubHandlerTest, RevokeAllContentSettingTypes) {
           ContentSettingsType::FILE_SYSTEM_ACCESS_EXTENDED_PERMISSION,
           ContentSettingsType::POINTER_LOCK,
           ContentSettingsType::LOCAL_NETWORK_ACCESS,
+          ContentSettingsType::SUB_APPS_WITHOUT_PROMPTS,
           // clang-format on
       });
 
