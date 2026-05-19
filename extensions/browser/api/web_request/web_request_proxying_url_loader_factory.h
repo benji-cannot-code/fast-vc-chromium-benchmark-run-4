66bd12ea9dc87098a34c620b5cb49cf5bc221a98
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-05-18 22:34:53 -0700

    Use HttpRequestHeadersUpdateParams in extension's FollowRedirectParams
    Follow-up of https://crrev.com/c/7835601 that introduced
    `network::HttpRequestHeadersUpdateParams`.
    
    This CL shouldn't change the behavior.
    
    Bug: 511306597, 434292502
    Change-Id: Ia8a775daddc2ed271cc4448c49ebfddd3289dc30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7835810
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632624}

--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.h
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/ssl_info.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
+#include "services/network/public/cpp/http_request_headers_update_params.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
@@ -281,9 +282,7 @@ class WebRequestProxyingURLLoaderFactory
       FollowRedirectParams(const FollowRedirectParams&) = delete;
       FollowRedirectParams& operator=(const FollowRedirectParams&) = delete;
       ~FollowRedirectParams();
-      std::vector<std::string> removed_headers;
-      net::HttpRequestHeaders modified_headers;
-      net::HttpRequestHeaders modified_cors_exempt_headers;
+      network::HttpRequestHeadersUpdateParams headers_update_params;
       std::optional<GURL> new_url;
     };
     std::unique_ptr<FollowRedirectParams> pending_follow_redirect_params_;
