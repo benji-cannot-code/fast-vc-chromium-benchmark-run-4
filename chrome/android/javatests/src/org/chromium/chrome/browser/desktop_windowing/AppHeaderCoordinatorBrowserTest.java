58036b40eebdf2ae920ce9196bf87810b721f00d
Henrique Nakashima <hnakashima@chromium.org>
2026-05-19 14:29:45 -0700

    [Android] Force soft keyboard NOT to appear in Android Desktop
    Depends on:
    https://chromium-review.git.corp.google.com/c/catapult/+/7642936
    
    Fix: Update SoftKeyboardCondition to not expect soft keyboard to be
    forced to open in Desktop to match new logic
    
    Include-Ci-Only-Tests: chromium.android:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Bug: 488115473
    Change-Id: I1ac535294817373740b7168e30f6db8b538c7ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848208
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633116}

--- chrome/android/javatests/src/org/chromium/chrome/browser/desktop_windowing/AppHeaderCoordinatorBrowserTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/desktop_windowing/AppHeaderCoordinatorBrowserTest.java
@@ -495,6 +495,7 @@ public class AppHeaderCoordinatorBrowserTest {
 
     @Test
     @MediumTest
+    @Restriction(DeviceFormFactor.ONLY_TABLET)
     public void testKeyboardInDesktopWindow_RootViewNotPaddedOnOmniboxFocus() {
         ChromeTabbedActivity activity = mActivityTestRule.getActivity();
         triggerDesktopWindowingModeChange(activity, true);
--- chrome/android/javatests/src/org/chromium/chrome/test/memory_leaks/PublicTransitLeakTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/memory_leaks/PublicTransitLeakTest.java
@@ -126,6 +126,7 @@ public class PublicTransitLeakTest {
 
     @Test
     @LargeTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // crbug.com/488115473
     public void omniboxTest() {
         var page = mChromeTabbedActivityTestRule.startOnBlankPage();
         var omnibox = page.openOmnibox();
