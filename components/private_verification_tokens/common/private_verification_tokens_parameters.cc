20ca3d5e27956033804ed7c01e38c7887615f3dc
Aykut Bulut <aykutb@chromium.org>
2026-05-19 07:27:27 -0700

    [PVT] Add parameters class
    This CL introduces `PrivateVerificationTokensParameters` to store
    protocol parameters such as minimum and maximum batch sizes. It
    also adds a `GetParametersForVersion` function to retrieve these
    parameters based on the protocol version.
    
    Bug: 502590509
    Change-Id: I4742deca0b7158a2dbe932537dda47620bd847f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853609
    Reviewed-by: Anusha Muley <anushamuley@google.com>
    Commit-Queue: Anusha Muley <anushamuley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1632848}

---
+++ components/private_verification_tokens/common/private_verification_tokens_parameters.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/private_verification_tokens/common/private_verification_tokens_parameters.h"
+
+#include <cstdint>
+#include <optional>
+
+namespace private_verification_tokens {
+
+std::optional<PrivateVerificationTokensParameters> GetParametersForVersion(
+    uint32_t version) {
+  if (version == 1) {
+    return PrivateVerificationTokensParameters{
+        .min_batch_size = 2,
+        .max_batch_size = 20,
+    };
+  }
+  return std::nullopt;
+}
+
+}  // namespace private_verification_tokens
---
+++ components/private_verification_tokens/common/private_verification_tokens_parameters.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PRIVATE_VERIFICATION_TOKENS_COMMON_PRIVATE_VERIFICATION_TOKENS_PARAMETERS_H_
+#define COMPONENTS_PRIVATE_VERIFICATION_TOKENS_COMMON_PRIVATE_VERIFICATION_TOKENS_PARAMETERS_H_
+
+#include <cstdint>
+#include <optional>
+
+namespace private_verification_tokens {
+
+// Holds all PVT parameters.
+struct PrivateVerificationTokensParameters {
+  // Minimum acceptable batch size.
+  int min_batch_size = 0;
+  // Maximum acceptable batch size.
+  int max_batch_size = 0;
+};
+
+// Returns the parameters for a given version, or nullopt if the version is not
+// supported.
+std::optional<PrivateVerificationTokensParameters> GetParametersForVersion(
+    uint32_t version);
+
+}  // namespace private_verification_tokens
+
+#endif  // COMPONENTS_PRIVATE_VERIFICATION_TOKENS_COMMON_PRIVATE_VERIFICATION_TOKENS_PARAMETERS_H_
