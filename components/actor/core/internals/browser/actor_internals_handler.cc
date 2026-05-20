57b739cb362d41868ead585f784d2eb995744d22
Kirubel <kaklilu@chromium.org>
2026-05-20 13:08:40 -0700

    [actor] Move chrome://actor-internals resources to components
    This moves the actor-internals WebUI resources and core logic to
    components to support cross-platform usage, specifically for iOS.
    
    Key changes:
    - Moves WebUI resources from chrome/browser/resources/actor_internals
      to components/actor/core/internals/resources.
    - Moves actor_internals.mojom to components/actor/public/mojom.
    - Introduces ActorInternalsHandler in components/actor/core/internals
    to handle mojom communication.
    - Refactors the desktop ActorInternalsUIHandler to implement the
    new shared Delegate interface.
    
    Bug: b:491878014
    Bypass-Check-License: moving code
    Change-Id: Ie39180fd9e302d97c58a8a0cb74bc5936a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851967
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Kirubel <kaklilu@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633786}

---
+++ components/actor/core/internals/browser/actor_internals_handler.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/actor/core/internals/browser/actor_internals_handler.h"
+
+#include <utility>
+
+namespace actor_internals {
+
+ActorInternalsHandler::ActorInternalsHandler(
+    mojo::PendingRemote<mojom::Page> page,
+    mojo::PendingReceiver<mojom::PageHandler> receiver,
+    Delegate* delegate)
+    : remote_(std::move(page)),
+      receiver_(this, std::move(receiver)),
+      delegate_(delegate) {
+  CHECK(delegate_);
+}
+
+ActorInternalsHandler::~ActorInternalsHandler() = default;
+
+void ActorInternalsHandler::OnJournalEntryAdded(mojom::JournalEntryPtr entry) {
+  remote_->JournalEntryAdded(std::move(entry));
+}
+
+void ActorInternalsHandler::StartLogging() {
+  delegate_->StartLogging();
+}
+
+void ActorInternalsHandler::StopLogging() {
+  delegate_->StopLogging();
+}
+
+}  // namespace actor_internals
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -205,7 +205,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     "META": {"sizes": {"includes": [10],}},
     "includes": [2747],
   },
-  "<(SHARED_INTERMEDIATE_DIR)/chrome/browser/resources/actor_internals/resources.grd": {
+  "<(SHARED_INTERMEDIATE_DIR)/components/actor/core/internals/resources/resources.grd": {
     "META": {"sizes": {"includes": [10],}},
     "includes": [2750],
   },
