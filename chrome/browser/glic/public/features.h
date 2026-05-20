bb390ebbcc40651d70f53d2b3bc6fc482c8612f7
Bryant Chandler <bryantchandler@google.com>
2026-05-20 15:11:23 -0700

    [glic] Remove GlicTabContentsObserver and kGlicDaisyChainViaCoordinator
    The coordinator-based daisy-chaining via GlicTabObserver has launched
    and is now enabled by default. This CL removes the obsolete fallback
    path (GlicTabContentsObserver) and cleans up the
    kGlicDaisyChainViaCoordinator feature flag.
    
    Bug: b:479828899
    Change-Id: Ic14c6668fe879ebb483183e4e32b23bf6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7862358
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633858}

--- chrome/browser/glic/public/features.h
+++ chrome/browser/glic/public/features.h
@@ -17,7 +17,6 @@ namespace features {
 BASE_DECLARE_FEATURE(kGlicTabRestoration);
 BASE_DECLARE_FEATURE(kGlicAndroidSidePanel);
 
-BASE_DECLARE_FEATURE(kGlicDaisyChainViaCoordinator);
 BASE_DECLARE_FEATURE(kGlicClearTurnIdOnPanelWillOpen);
 BASE_DECLARE_FEATURE(kGlicChromeStatusIcon);
 extern const base::FeatureParam<int> kGlicChromeStatusIconSizePx;
