ad15d3a7d765f153cc48c52c96769d08baf4ee4f
Tobiasz Ciepliński <tobson@google.com>
2026-05-20 02:57:30 -0700

    Add `SupportsSyncingEntityMetadata()` function
    Not all entities stored on the server support syncing entity metadata.
    This CL adds a specialized function to determine whether an entity
    supports it.
    
    This is a followup to the discussion in https://crrev.com/c/7813597.
    
    Bug: b:501037715
    Change-Id: I2e8b92069049427fbff055e2a8d25be7453fbf50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863765
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Tobiasz Ciepliński <tobson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633467}

--- components/autofill/core/browser/webdata/autofill_webdata_backend_impl.cc
+++ components/autofill/core/browser/webdata/autofill_webdata_backend_impl.cc
@@ -166,6 +166,16 @@ void ReportResult(Result result) {
       "WebDatabase.AutofillWebDataBackendImpl.OperationResult", result);
 }
 
+bool SupportsSyncingEntityMetadata(const EntityInstance& entity_instance) {
+  switch (entity_instance.record_type()) {
+    case EntityInstance::RecordType::kServerWallet:
+      return true;
+    case EntityInstance::RecordType::kLocal:
+    case EntityInstance::RecordType::kPersonalContext:
+      return false;
+  }
+}
+
 }  // namespace
 
 AutofillWebDataBackendImpl::AutofillWebDataBackendImpl(
@@ -357,7 +367,7 @@ void AutofillWebDataBackendImpl::NotifyOnEntityInstanceChanged(
   }
 
   // Notify about potential server metadata changes.
-  if (change.data_model().IsServerInstance()) {
+  if (SupportsSyncingEntityMetadata(change.data_model())) {
     EntityInstanceMetadataChange::Type metadata_change_type = [&] {
       switch (change.type()) {
         case EntityInstanceChange::ADD:
@@ -626,7 +636,7 @@ WebDatabase::State AutofillWebDataBackendImpl::UpdateEntityMetadata(
     return WebDatabase::COMMIT_NOT_NEEDED;
   }
 
-  if (entity.IsServerInstance()) {
+  if (SupportsSyncingEntityMetadata(entity)) {
     EntityInstanceMetadataChange change(EntityInstanceMetadataChange::UPDATE,
                                         entity.guid(), entity.metadata());
     NotifyOnServerEntityMetadataChanged(change);
