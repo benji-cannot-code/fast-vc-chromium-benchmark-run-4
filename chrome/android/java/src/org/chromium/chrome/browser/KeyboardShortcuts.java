8654c4e83bdd92f018ba19cf46596182794b6990
Fuhsin Liao <fuhsinliao@google.com>
2026-05-19 22:11:44 -0700

    Modify F10 keyboard shortcut
    Previously, the F10 keyboard shortcut opens the context menu, this cl
    makes it focus on the rightmost item (currently context menu) instead.
    
    NO_IFTTT= Doesn't apply
    
    Bug: 440244876
    Change-Id: I6e09486d300498c84f4f34ea4b6a2d582209b7e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7840658
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Auto-Submit: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633345}

--- chrome/android/java/src/org/chromium/chrome/browser/KeyboardShortcuts.java
+++ chrome/android/java/src/org/chromium/chrome/browser/KeyboardShortcuts.java
@@ -15,6 +15,7 @@ import android.content.res.Resources;
 import android.view.KeyEvent;
 import android.view.KeyboardShortcutGroup;
 import android.view.KeyboardShortcutInfo;
+import android.view.View;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.StringRes;
@@ -137,6 +138,7 @@ public class KeyboardShortcuts {
         KeyboardShortcutsSemanticMeaning.TOGGLE_MULTISELECT,
         KeyboardShortcutsSemanticMeaning.ZOOM_IN_LEGACY,
         KeyboardShortcutsSemanticMeaning.ZOOM_OUT_LEGACY,
+        KeyboardShortcutsSemanticMeaning.FOCUS_APP_MENU_BUTTON,
         KeyboardShortcutsSemanticMeaning.MAX_VALUE
     })
     @Retention(RetentionPolicy.SOURCE)
@@ -244,8 +246,11 @@ public class KeyboardShortcuts {
         int ZOOM_IN_LEGACY = 63;
         int ZOOM_OUT_LEGACY = 64;
 
+        // App menu button keyboard shortcut.
+        int FOCUS_APP_MENU_BUTTON = 65;
+
         // Max value.
-        int MAX_VALUE = 65;
+        int MAX_VALUE = 66;
     }
 
     // LINT.ThenChange(//tools/metrics/histograms/metadata/accessibility/enums.xml:KeyboardShortcutsSemanticMeaning, //tools/metrics/histograms/metadata/accessibility/histograms.xml:KeyboardShortcutsSemanticMeaning)
@@ -584,7 +589,6 @@ public class KeyboardShortcuts {
                 R.string.keyboard_shortcut_chrome_feature_group_header,
                 new KeyCombo[] {
                     new KeyCombo(KeyEvent.KEYCODE_F, KeyEvent.META_ALT_ON),
-                    new KeyCombo(KeyEvent.KEYCODE_F10, NO_MODIFIER),
                     new KeyCombo(KeyEvent.KEYCODE_BUTTON_Y, NO_MODIFIER)
                 });
         new KeyboardShortcutDefinition(
@@ -649,6 +653,9 @@ public class KeyboardShortcuts {
         new KeyboardShortcutDefinition(
                 KeyboardShortcutsSemanticMeaning.FOCUSED_TAB_STRIP_ITEM_REORDER_RIGHT,
                 new KeyCombo(KeyEvent.KEYCODE_DPAD_RIGHT, KeyEvent.META_CTRL_ON));
+        new KeyboardShortcutDefinition(
+                KeyboardShortcutsSemanticMeaning.FOCUS_APP_MENU_BUTTON,
+                new KeyCombo(KeyEvent.KEYCODE_F10, NO_MODIFIER));
 
         // Bookmark shortcuts.
         new KeyboardShortcutDefinition(
@@ -1281,8 +1288,14 @@ public class KeyboardShortcuts {
                     // TODO(crbug.com/360423850): Don't allow F6 to be overridden by websites.
                     return menuOrKeyboardActionController.onMenuOrKeyboardAction(
                             R.id.switch_keyboard_focus_row, /* fromMenu= */ false);
-                case KeyboardShortcutsSemanticMeaning
-                        .FOCUSED_TAB_STRIP_ITEM_OPEN_CONTEXT_MENU:
+                case KeyboardShortcutsSemanticMeaning.FOCUS_APP_MENU_BUTTON:
+                    View menuButtonView = toolbarManager.getMenuButtonView();
+                    if (menuButtonView != null) {
+                        menuButtonView.requestFocus();
+                        return true;
+                    }
+                    return false;
+                case KeyboardShortcutsSemanticMeaning.FOCUSED_TAB_STRIP_ITEM_OPEN_CONTEXT_MENU:
                     return menuOrKeyboardActionController.onMenuOrKeyboardAction(
                             R.id.open_tab_strip_context_menu, /* fromMenu= */ false);
                 case KeyboardShortcutsSemanticMeaning.FOCUSED_TAB_STRIP_ITEM_REORDER_LEFT:
