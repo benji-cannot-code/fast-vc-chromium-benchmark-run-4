680b3314304526e159863df7f1b6b5fe4e7ccb4b
James Leung <jamesleung@google.com>
2026-05-19 19:47:10 -0700

    [context-input] Add shared tabs submenu to omnibox context menu
    Move recent and shared tabs into a dedicated submenu within the omnibox
    context menu to improve organization and prevent the main menu from
    becoming cluttered.
    
    * Introduce `IDC_OMNIBOX_CONTEXT_SHARED_TABS_SUBMENU` as the command ID
      for the new submenu.
    * Update `OmniboxContextMenuController` to conditionally
      populate separate `SimpleMenuModel` for shared tabs.
      Otherwise, use regular menu (new section).
    * Draw submenu recursively
    * For the submenu button, add boilerplate checks/updates:
      add visibility, enablement, translation checks to handle
      submenu button updating/rendering.
    * For tabs, add icon draw and on favicon update
    * Sets menu and submenu widths
    * Add mock tab placeholders when recent tabs are empty to ensure
      consistent menu rendering.
    
    Will add checkmarks for multiselecting tabs later in follow up CL.
    
    http://screencast/cast/NDgxNDU5OTc0MzM0MDU0NHw3ZDZjZGQ0Yy1mOQ
    
    Bug: b:504707571
    Change-Id: I544ddf146e4cab199b1bd4282f3ac9d34e5df42c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841488
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633303}

--- chrome/app/chrome_command_ids.h
+++ chrome/app/chrome_command_ids.h
@@ -627,6 +627,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IDC_OMNIBOX_CONTEXT_SET_MODEL_THINKING                54016
 #define IDC_OMNIBOX_CONTEXT_SET_MODEL_REGULAR                 54017
 #define IDC_OMNIBOX_CONTEXT_SET_MODEL_PRO_NO_GEN_UI           54018
+#define IDC_OMNIBOX_CONTEXT_SHARED_TABS_SUBMENU               54019
 
 // NOTE: The last valid command value is 57343 (0xDFFF)
 // See http://msdn.microsoft.com/en-us/library/t2zechd4(VS.71).aspx
