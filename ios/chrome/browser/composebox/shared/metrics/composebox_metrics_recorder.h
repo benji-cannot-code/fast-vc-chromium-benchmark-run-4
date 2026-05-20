57a802ad56dc0c152acd26084e54bcefce1f5b04
Ameur Hosni <ameurhosni@google.com>
2026-05-20 02:42:12 -0700

    [IOS][AIM] Use the new shared metrics for tools/models
    Change-Id: I5656f66d068dda2c94208af3173311133c8c9b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852565
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633457}

--- ios/chrome/browser/composebox/shared/metrics/composebox_metrics_recorder.h
+++ ios/chrome/browser/composebox/shared/metrics/composebox_metrics_recorder.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <vector>
 
+#import "ios/chrome/browser/composebox/public/composebox_mode.h"
+#import "ios/chrome/browser/composebox/public/composebox_model_option.h"
+
 // LINT.IfChange(AiModeActivationSource)
 enum class AiModeActivationSource {
   kToolMenu = 0,
@@ -141,6 +144,12 @@ class ContextualSearchMetricsRecorder;
 
 // Records whether the user edited the text before entering AI Mode.
 - (void)recordTextEditedBeforeAiMode:(BOOL)edited;
+
+// Records the tool explicitly selected in the menu.
+- (void)recordToolSelected:(ComposeboxMode)tool;
+
+// Records the model explicitly selected in the menu.
+- (void)recordModelSelected:(ComposeboxModelOption)model;
 
 @end
 
