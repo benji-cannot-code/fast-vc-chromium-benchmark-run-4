ef90e729085febd36c235c5259a5da946da8f8cf
Marwan Tammam <quarz@chromium.org>
2026-05-20 08:52:39 -0700

    Add extension UKM for Tabs and DNR APIs redirecting search from DSE
    Bug: b:494251632
    Change-Id: Icb6d3492a62463fe79ac9cfce9739ae948aa257a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816468
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Marwan Tammam <quarz@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633610}

---
+++ extensions/browser/api/constants.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_BROWSER_API_CONSTANTS_H_
+#define EXTENSIONS_BROWSER_API_CONSTANTS_H_
+
+namespace extensions {
+
+// Represents the extension API that performed a search redirect.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+// LINT.IfChange(ExtensionSearchRedirectedByApi)
+enum class ExtensionSearchRedirectedByApi {
+  kDeclarativeNetRequest = 0,
+  kTabsUpdate = 1,
+  kMaxValue = kTabsUpdate,
+};
+// LINT.ThenChange(//tools/metrics/histograms/enums.xml:ExtensionSearchRedirectedByApi)
+
+}  // namespace extensions
+
+#endif  // EXTENSIONS_BROWSER_API_CONSTANTS_H_
