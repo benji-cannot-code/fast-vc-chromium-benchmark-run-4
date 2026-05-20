00191237a9f1ef9c418dfcb03e2d52e64939be16
rbpotter <rbpotter@chromium.org>
2026-05-20 14:04:29 -0700

    Side Panel Bookmarks: Migrate app to Lit
    Bug: 493823435
    Change-Id: Ia1401666f87d9a09d3ceba8beb299fb29c5269ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853555
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633822}

--- chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_app_test.ts
+++ chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_app_test.ts
@@ -45,7 +45,7 @@ suite('General', () => {
   let metrics: MetricsTracker;
 
   function getAddTabButton(): CrButtonElement {
-    return powerBookmarksApp.shadowRoot!.querySelector<CrButtonElement>(
+    return powerBookmarksApp.shadowRoot.querySelector<CrButtonElement>(
         '#addCurrentTabButton')!;
   }
 
@@ -71,7 +71,7 @@ suite('General', () => {
 
   async function performSearch(query: string) {
     const searchField =
-        powerBookmarksApp.shadowRoot!.querySelector('cr-toolbar-search-field')!;
+        powerBookmarksApp.shadowRoot.querySelector('cr-toolbar-search-field')!;
     const searchChanged = eventToPromise('search-changed', searchField);
     const metricsLogged = eventToPromise(
         'bookmark-count-recorded', powerBookmarksApp.$.bookmarksList);
@@ -865,7 +865,7 @@ suite('General', () => {
       flush();
 
       const deleteButton: HTMLButtonElement =
-          powerBookmarksApp.shadowRoot!.querySelector('#deleteButton')!;
+          powerBookmarksApp.shadowRoot.querySelector('#deleteButton')!;
       assertFalse(deleteButton.disabled);
       deleteButton.click();
 
@@ -940,7 +940,7 @@ suite('General', () => {
       flush();
 
       const deleteButton: HTMLButtonElement =
-          powerBookmarksApp.shadowRoot!.querySelector('#deleteButton')!;
+          powerBookmarksApp.shadowRoot.querySelector('#deleteButton')!;
       assertFalse(deleteButton.disabled);
       deleteButton.click();
 
@@ -1069,8 +1069,8 @@ suite('General', () => {
       assertFalse(isHidden(footer));
 
       // A search with no results.
-      const searchField = powerBookmarksApp.shadowRoot!.querySelector(
-          'cr-toolbar-search-field');
+      const searchField =
+          powerBookmarksApp.shadowRoot.querySelector('cr-toolbar-search-field');
       assertTrue(!!searchField);
       searchField.$.searchInput.value = 'abcdef';
       searchField.onSearchTermSearch();
