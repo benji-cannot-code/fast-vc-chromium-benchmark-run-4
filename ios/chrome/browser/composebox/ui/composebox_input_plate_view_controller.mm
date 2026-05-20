20dbdc875f250917fbfb5506c740a3877d644ced
Ameur Hosni <ameurhosni@google.com>
2026-05-20 09:15:05 -0700

    [IOS][AIM] Fix legacy menu tools/models shown metrics
    This CL correctly captures the tools/models visibility relying on the
    UI input state instead.
    
    Bug: 515013682
    Change-Id: Ib9c8ba94b1d0be280cddc77eba5db79cf0866d3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7864111
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633633}

--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
@@ -679,7 +679,8 @@ UIImage* SendButtonImage(BOOL highlighted, ComposeboxTheme* theme) {
     visibleButtons.push_back(FuseboxAttachmentButtonType::kFiles);
   }
   [self.delegate composeboxViewController:self
-      didOpenPlusMenuWithVisibleInternalButtons:visibleButtons];
+      didOpenPlusMenuWithVisibleInternalButtons:visibleButtons
+                                   uiInputState:_state];
 }
 
 - (void)micButtonTapped {
