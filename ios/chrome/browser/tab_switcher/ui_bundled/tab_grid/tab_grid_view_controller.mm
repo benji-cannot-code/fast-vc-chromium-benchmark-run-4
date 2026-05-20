1d40688904868ffd9a7ae1749e1ee66639669899
Briana McClure <bmcclure@google.com>
2026-05-20 07:15:42 -0700

    [iOS] Fix pinned tabs drag and drop landscape regression
    Previously, users could not drag pinned tabs in landscape mode. This
    change constrains the view to the safe area so users can perform the
    drag and drop action.
    
    Screen Recording:
    http://screencast/cast/NDY0OTIwMzgwNzU1MTQ4OHw1ZTJiNGRlZi0wNQ
    
    Fixed: 513189404
    Change-Id: I6283d23437360f8cd0e30cc1ec32f52cc9f7471f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859916
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633574}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_view_controller.mm
@@ -2144,7 +2144,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
           constraintEqualToAnchor:self.view.widthAnchor
                        multiplier:kPinnedViewMaxWidthInPercent],
       [pinnedView.bottomAnchor
-          constraintEqualToAnchor:self.view.bottomAnchor
+          constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor
                          constant:-kPinnedViewBottomPadding],
     ]];
   }
