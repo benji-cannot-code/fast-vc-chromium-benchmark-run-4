652c1690215b4c5e22078701dfe30cd6960c70dc
Viktor Semeniuk <vsemeniuk@google.com>
2026-05-20 04:40:23 -0700

    Perform only filling as part of password change from checkup
    Form submission and verification will be handled by GlicService
    
    Bug: 512717933
    Change-Id: Ifeaf787b00fac6b8b2d730cd3a05ef58d2b6469d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7857672
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633506}

--- chrome/browser/password_manager/password_change/password_change_from_checkup_delegate.cc
+++ chrome/browser/password_manager/password_change/password_change_from_checkup_delegate.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/password_manager/actor_login/password_change_from_checkup_actor_login_service.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/password_manager/password_change/change_password_form_waiter.h"
-#include "chrome/browser/password_manager/password_change/model_quality_logs_uploader.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/actor_webui.mojom.h"
 #include "chrome/grit/browser_resources.h"
@@ -355,23 +354,23 @@ void PasswordChangeFromCheckupDelegate::OnChangePasswordFormManagerFound(
     logger->LogMessage(Logger::STRING_PASSWORD_CHANGE_FROM_CHECKUP_FORM_FOUND);
   }
 
-  submission_helper_ =
-      std::make_unique<ChangePasswordFormFillingSubmissionHelper>(
-          actuation_web_contents_.get(),
-          ChromePasswordManagerClient::FromWebContents(
-              actuation_web_contents_.get()),
-          base::BindOnce(
-              &PasswordChangeFromCheckupDelegate::OnChangePasswordFormSubmitted,
-              weak_ptr_factory_.GetWeakPtr()));
+  form_filler_ = std::make_unique<ChangePasswordFormFiller>(
+      actuation_web_contents_.get(),
+      ChromePasswordManagerClient::FromWebContents(
+          actuation_web_contents_.get()),
+      /*logs_uploader=*/nullptr);
 
-  submission_helper_->FillChangePasswordForm(
-      form_manager, username_, current_password_, generated_password_);
+  form_filler_->FillForm(
+      form_manager, username_, current_password_, generated_password_,
+      base::BindOnce(
+          &PasswordChangeFromCheckupDelegate::OnChangePasswordFormFilled,
+          weak_ptr_factory_.GetWeakPtr()));
 }
 
-void PasswordChangeFromCheckupDelegate::OnChangePasswordFormSubmitted(
-    ChangePasswordFormFillingSubmissionHelper::SubmissionResult result) {
-  submission_helper_.reset();
-  // If the form submission failed, do not trigger a verification task.
+void PasswordChangeFromCheckupDelegate::OnChangePasswordFormFilled(
+    ChangePasswordFormFiller::FillingResult result) {
+  form_filler_.reset();
+
   if (!result.has_value()) {
     return;
   }
@@ -382,8 +381,7 @@ void PasswordChangeFromCheckupDelegate::OnChangePasswordFormSubmitted(
   }
 
   if (auto logger = GetLoggerIfAvailable(client_)) {
-    logger->LogMessage(
-        Logger::STRING_PASSWORD_CHANGE_FROM_CHECKUP_FORM_SUBMISSION);
+    logger->LogMessage(Logger::STRING_PASSWORD_CHANGE_FROM_CHECKUP_FORM_FILLED);
   }
 
   glic::GlicKeyedService* glic_service = GetGlicService();
--- chrome/browser/password_manager/password_change/password_change_from_checkup_delegate.h
+++ chrome/browser/password_manager/password_change/password_change_from_checkup_delegate.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/tools/tool_delegate.h"
-#include "chrome/browser/password_manager/password_change/change_password_form_filling_submission_helper.h"
+#include "chrome/browser/password_manager/password_change/change_password_form_filler.h"
 #include "url/gurl.h"
 
 class ChangePasswordFormWaiter;
@@ -66,8 +66,8 @@ class PasswordChangeFromCheckupDelegate {
 
   void OnChangePasswordFormManagerFound(
       password_manager::PasswordFormManager* form_manager);
-  void OnChangePasswordFormSubmitted(
-      ChangePasswordFormFillingSubmissionHelper::SubmissionResult result);
+  void OnChangePasswordFormFilled(
+      ChangePasswordFormFiller::FillingResult result);
 
   void OnVerificationTaskStateChanged(actor::ActorTask& task);
   void OnVerificationTimeout();
@@ -89,7 +89,7 @@ class PasswordChangeFromCheckupDelegate {
 
   base::CallbackListSubscription actor_task_state_subscription_;
 
-  std::unique_ptr<ChangePasswordFormFillingSubmissionHelper> submission_helper_;
+  std::unique_ptr<ChangePasswordFormFiller> form_filler_;
   std::unique_ptr<ChangePasswordFormWaiter> form_waiter_;
 
   std::optional<actor::ActorTask::State> find_form_task_state_ = std::nullopt;
