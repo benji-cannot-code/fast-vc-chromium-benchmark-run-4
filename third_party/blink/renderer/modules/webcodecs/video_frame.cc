35635c4b1b27a011ba4c720af1d21925d661db1e
Colin Blundell <blundell@chromium.org>
2026-05-20 01:40:55 -0700

    [Blink] Remove unneeded arg from CreateUnacceleratedImageFromVideoFrame
    Following elimination of killswitches, this param is now unused.
    
    Bug: 352263194
    Change-Id: I22e857bf0a426d61442ab4e9b1c09f77b9c144b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852569
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633422}

--- third_party/blink/renderer/modules/webcodecs/video_frame.cc
+++ third_party/blink/renderer/modules/webcodecs/video_frame.cc
@@ -1559,8 +1559,7 @@ scoped_refptr<StaticBitmapImage> VideoFrame::CreateImageFromVideoFrame(
     auto* snapshot_provider_bitmap =
         static_cast<CanvasNon2DSnapshotProviderBitmap*>(snapshot_provider);
     return CreateUnacceleratedImageFromVideoFrame(
-        frame, snapshot_provider_bitmap->Info(),
-        /*cached_draw_surface=*/nullptr);
+        frame, snapshot_provider_bitmap->Info());
   }
 
   return CreateAcceleratedImageFromVideoFrame(
--- third_party/blink/renderer/platform/graphics/video_frame_image_util_test.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_image_util_test.cc
@@ -132,8 +132,7 @@ class VideoFrameImageUtilTest
           prefer_tagged_orientation);
     } else {
       return CreateUnacceleratedImageFromVideoFrame(
-          std::move(frame), info, /*cached_draw_surface=*/nullptr,
-          video_renderer, prefer_tagged_orientation);
+          std::move(frame), info, video_renderer, prefer_tagged_orientation);
     }
   }
 
