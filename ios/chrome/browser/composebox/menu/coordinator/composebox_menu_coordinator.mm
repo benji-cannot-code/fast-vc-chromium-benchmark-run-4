c43a8dd32350118317fcbafb3d44c8aa9425ee89
Ameur Hosni <ameurhosni@google.com>
2026-05-20 05:38:16 -0700

    [IOS][AIM] Initialize new metrics recorder for standalone
    For Standalone the metrics recorder was only initialized on init.
    This CL fixes an issue where the recorder is reset on stop but not
    re-initialized when the coordinator is restarted.
    
    Fixed: 514948873
    Change-Id: I9ec04194deefd97321d84ceeef9208a94075e640
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7864380
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633527}

--- ios/chrome/browser/composebox/menu/coordinator/composebox_menu_coordinator.mm
+++ ios/chrome/browser/composebox/menu/coordinator/composebox_menu_coordinator.mm
@@ -94,13 +94,12 @@ CGFloat const kSheetTopPadding = 40.0f;
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
                                    browser:(Browser*)browser
                                 entrypoint:(ComposeboxEntrypoint)entrypoint {
-  return
-      [self initWithBaseViewController:viewController
-                               browser:browser
-                preselectedAttachments:nil
-                            inputState:nil
-                       metricsRecorder:[[ComposeboxMetricsRecorder alloc] init]
-                            entrypoint:entrypoint];
+  return [self initWithBaseViewController:viewController
+                                  browser:browser
+                   preselectedAttachments:nil
+                               inputState:nil
+                          metricsRecorder:nil
+                               entrypoint:entrypoint];
 }
 
 - (void)start {
@@ -119,8 +118,12 @@ CGFloat const kSheetTopPadding = 40.0f;
         std::move(configParams),
         contextual_search::ContextualSearchSource::kNewTabPage,
         lens::LensOverlayInvocationSource::kNtpContextualQuery);
-    _metricsRecorder.contextualSearchMetricsRecorder =
-        _sessionHandle->GetMetricsRecorder();
+
+    _metricsRecorder = [[ComposeboxMetricsRecorder alloc] init];
+    if (_sessionHandle) {
+      _metricsRecorder.contextualSearchMetricsRecorder =
+          _sessionHandle->GetMetricsRecorder();
+    }
 
     ComposeboxModeHolder* modeHolder = [[ComposeboxModeHolder alloc] init];
 
