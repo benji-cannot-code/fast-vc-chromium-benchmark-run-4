2ce5b903be4d267fa8847378cbfa4cc852e3af8d
Long Pan <panlong@google.com>
2026-05-19 22:52:04 -0700

    [Flag Cleanup] Remove stale feature GeoLanguage
    This flag was added around 2017 and is disabled by default.
    We found no active usages in components/translate or components/language
    other than the initialization in chrome_browser_main.cc, which this
    CL removes.
    
    Change-Id: I3fc62144cffe05c90e7b2ac46eb230349c89294a
    
    TEST=presubmit
    
    Bug: 356235642
    Change-Id: I3fc62144cffe05c90e7b2ac46eb230349c89294a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858970
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Long Pan <panlong@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633365}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1859,9 +1859,8 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
   // Post-profile init ---------------------------------------------------------
 
   TranslateService::Initialize();
-  if (base::FeatureList::IsEnabled(features::kGeoLanguage) ||
-      language::GetOverrideLanguageModel() ==
-          language::OverrideLanguageModel::GEO) {
+  if (language::GetOverrideLanguageModel() ==
+      language::OverrideLanguageModel::GEO) {
     language::GeoLanguageProvider::GetInstance()->StartUp(
         browser_process_->local_state());
   }
