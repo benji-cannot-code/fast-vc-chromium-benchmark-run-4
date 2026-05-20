df0defbfa8bd7c4d847514e07c4e3b378f707725
Bryan Oltman <bryanoltman@google.com>
2026-05-20 12:17:09 -0700

    [macOS] Deny distributed notifications in the renderer sandbox
    Harden the renderer sandbox by denying the ability to post distributed
    notifications and looking up the distributed notification service.
    
    This change includes a feature, kMacSandboxDistributedNotifications
    (disabled by default), that can be used as a killswitch if needed.
    
    Fixed: 513454805
    Change-Id: I82a9c46a2f9e921be7f5b286fdbbb9c2bcc6b643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858672
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633749}

--- content/renderer/sandbox_mac_v2_unittest.mm
+++ content/renderer/sandbox_mac_v2_unittest.mm
@@ -76,6 +76,9 @@ void SetParametersForTest(SandboxSerializer* serializer,
 
   CHECK(serializer->SetParameter(sandbox::policy::kParamExecutablePath,
                                  executable_path.value()));
+
+  CHECK(serializer->SetBooleanParameter(
+      sandbox::policy::kParamEnableDistributedNotifications, false));
 }
 
 }  // namespace
