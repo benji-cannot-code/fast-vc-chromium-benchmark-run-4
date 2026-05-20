5a8f2e8de99df6a28da2fc0ce540e52cf2568180
Yiu Liu <yueliu1@microsoft.com>
2026-05-19 21:26:55 -0700

    [BrowserView] Remove unused kColorTabstripLoadingProgress{Foreground,Background}
    The last consumers of these color IDs were removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/7850836,
    leaving the definitions in chrome_color_id.h and the corresponding mixer
    entries in chrome_color_mixer.cc with no remaining references. Drop
    them.
    
    Change-Id: Iac5e3803607d8534096408fbcd5c88ce9d2bc622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856221
    Commit-Queue: Yue Liu <yueliu1@microsoft.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Chang Liu <changliu1@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1633337}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -996,8 +996,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorTabGroupTabStripFrameInactiveOrange) \
   E_CPONLY(kColorTabStrokeFrameActive) \
   E_CPONLY(kColorTabStrokeFrameInactive) \
-  E_CPONLY(kColorTabstripLoadingProgressBackground) \
-  E_CPONLY(kColorTabstripLoadingProgressForeground) \
   E_CPONLY(kColorTabstripScrollContainerShadow) \
   E_CPONLY(kColorTabThrobber) \
   E_CPONLY(kColorTabThrobberPreconnect) \
