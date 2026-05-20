e539060222c418a3745ef466b7d9cb8ca73949e6
Hitarth Kothari <hitarthkothari@google.com>
2026-05-20 05:53:57 -0700

    [TabStripContextMenus] Condense list menu tests, and relocate them under behaviour tests
    Bug: 512805815
    Change-Id: Idce7ccf78127f5c2bdd783c9f05b08d12d04da33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7860846
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633532}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabContextMenuCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabContextMenuCoordinator.java
@@ -576,7 +576,7 @@ public class TabContextMenuCoordinator extends TabStripReorderingHelper<AnchorIn
         itemList.add(createCloseItem(isIncognito));
         itemList.add(createCloseAllTabsItem(isIncognito));
         if (ChromeFeatureList.sAndroidContextMenuNewActions.isEnabled()) {
-            if (tabs.size() > 1) {
+            if (getTabModel().getCount() > 1) {
                 itemList.add(createCloseOtherTabsItem(isIncognito));
             }
             if (canCloseTabsToTheRight(anchorInfo)) {
@@ -609,7 +609,7 @@ public class TabContextMenuCoordinator extends TabStripReorderingHelper<AnchorIn
         }
         itemList.add(createCloseItem(isIncognito));
         if (ChromeFeatureList.sAndroidContextMenuNewActions.isEnabled()) {
-            if (tabs.size() > anchorInfo.getAllTabIds().size()) {
+            if (getTabModel().getCount() > anchorInfo.getAllTabIds().size()) {
                 itemList.add(createCloseOtherTabsItem(isIncognito));
             }
             if (canCloseTabsToTheRight(anchorInfo)) {
