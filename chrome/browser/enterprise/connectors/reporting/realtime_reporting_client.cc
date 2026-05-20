e53369eb0df50d5dcd517b1196cd077d896bfc40
Qiutan Wu <qiutanw@google.com>
2026-05-20 06:12:40 -0700

    Project import generated by Copybara.
    The auto sync pipeline is broken (b/514706187), so I manually triggered
    this sync. components/enterprise/connectors/core/reporting_utils.cc and
    chrome/browser/enterprise/connectors/reporting/realtime_reporting_client.cc
    also have to be updated or the new event can't be handled in the switch
    statements.
    
    PiperOrigin-RevId: 908177399
    Change-Id: Ief75c0148ef053e0dd60857b3fbfbe6ceab2425c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7860596
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Qiutan Wu <qiutanw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633546}

--- chrome/browser/enterprise/connectors/reporting/realtime_reporting_client.cc
+++ chrome/browser/enterprise/connectors/reporting/realtime_reporting_client.cc
@@ -267,6 +267,7 @@ void AddCrowdstrikeSignalsToEvent(
     case Event::kPrototypeRawEvent:
     case Event::kTelomereEvent:
     case Event::kSaasUsageReportEvent:
+    case Event::kBrowserLaunchEvent:
       break;
   }
 }
