f502ad3aa64aad3e1ce02fa886033d799454a576
Christian Dullweber <dullweber@chromium.org>
2026-05-20 07:41:10 -0700

    PrivateAi: Enforce HTTPS for proxy URLs and enable CT policy
    Ensure that we can only connect to https proxies and enable certificate
    transparency.
    
    Bug: 513212892
    Change-Id: Ic671b3f608335ff84632aa2753cf35e856b26906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7857393
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633587}

--- components/private_ai/client.cc
+++ components/private_ai/client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/private_ai/phosphor/token_manager.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "url/gurl.h"
+#include "url/url_constants.h"
 
 namespace private_ai {
 
@@ -45,6 +46,11 @@ std::unique_ptr<Client> Client::Create(
     GURL proxy_url(proxy_url_string);
     if (!proxy_url.SchemeIsHTTPOrHTTPS()) {
       proxy_url = GURL(base::StrCat({"https://", proxy_url_string}));
+    }
+    if (proxy_url.SchemeIs(url::kHttpScheme)) {
+      GURL::Replacements replacements;
+      replacements.SetSchemeStr(url::kHttpsScheme);
+      proxy_url = proxy_url.ReplaceComponents(replacements);
     }
     connection_factory->EnableProxy(proxy_url);
   }
