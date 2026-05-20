913ce52abd2fccd828655ee31e559d588f62b4e7
David Awogbemila <awogbemila@chromium.org>
2026-05-20 08:58:14 -0700

    Parse scroll-axis-lock property
    This implements the parsing of the scroll-axis-lock[1] property which
    takes 'auto' and 'none' as keywords.
    
    [1]
    https://github.com/w3c/csswg-drafts/issues/13207#issuecomment-3808193000
    
    Bug: 479472367
    Change-Id: Ie520339487027dafd4ff8754d93731a54ed55fc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841432
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: David A <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633619}

--- third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
@@ -1356,6 +1356,8 @@ bool CSSParserFastPaths::IsValidKeywordPropertyAndValue(
              value_id == CSSValueID::kInternalTextareaAuto ||
              (RuntimeEnabledFeatures::CSSResizeAutoEnabled() &&
               value_id == CSSValueID::kAuto);
+    case CSSPropertyID::kScrollAxisLock:
+      return value_id == CSSValueID::kAuto || value_id == CSSValueID::kNone;
     case CSSPropertyID::kScrollTargetGroup:
       return value_id == CSSValueID::kNone || value_id == CSSValueID::kAuto;
     case CSSPropertyID::kScrollBehavior:
@@ -1801,6 +1803,7 @@ CSSBitset CSSParserFastPaths::handled_by_keyword_fast_paths_properties_{{
     CSSPropertyID::kResize,
     CSSPropertyID::kRowRuleBreak,
     CSSPropertyID::kRowRuleVisibilityItems,
+    CSSPropertyID::kScrollAxisLock,
     CSSPropertyID::kScrollTargetGroup,
     CSSPropertyID::kScrollBehavior,
     CSSPropertyID::kOverscrollBehaviorInline,
