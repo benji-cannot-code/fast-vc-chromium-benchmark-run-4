126f9373cc0c48cb9183c0e8a2fa84f4e0123029
trang mai <trangm@google.com>
2026-05-20 01:17:19 -0700

    [OTP] Add OneTimeTokenRetrievalError::kSmsOtpUnknown
    Align the enum label with the strict prefixing convention used for OTP
    error codes. This disambiguates with the soon-to-be-added
    kGmailOtpUnknown when they are reported or logged.
    OneTimeTokenRetrievalError::kUnknown is still left in
    OneTimeTokenRetrievalError for unknown errors in the generic backend.
    
    Bug: 511987510
    Change-Id: I294b8259bc325b02bd4f01a1f169b86fef2c5af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859849
    Commit-Queue: Bekzhan Kassenov <bkassenov@google.com>
    Reviewed-by: Bekzhan Kassenov <bkassenov@google.com>
    Auto-Submit: Trang Mai <trangm@google.com>
    Commit-Queue: Trang Mai <trangm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633412}

--- components/one_time_tokens/android/backend/sms/sms_otp_to_one_time_token_retrieval_error_converter_unittest.cc
+++ components/one_time_tokens/android/backend/sms/sms_otp_to_one_time_token_retrieval_error_converter_unittest.cc
@@ -29,7 +29,7 @@ TEST(SmsOtpToOnetimeTokenRetrievalErrorConverterTest, Smoke) {
                 SmsOtpRetrievalApiError::kGmscoreVersionNotSupported),
             OneTimeTokenRetrievalError::kSmsOtpGmscoreVersionNotSupported);
   EXPECT_EQ(ConvertSmsOtpRetrievalApiError(SmsOtpRetrievalApiError::kUnknown),
-            OneTimeTokenRetrievalError::kUnknown);
+            OneTimeTokenRetrievalError::kSmsOtpUnknown);
 }
 
 }  // namespace one_time_tokens
