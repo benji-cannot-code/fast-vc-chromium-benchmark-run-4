60ce74961b7bb31ffc1c1fd6f591a04a42dc0a59
rbpotter <rbpotter@chromium.org>
2026-05-20 10:38:59 -0700

    [PDF Ink Signatures] De-flake and enable some more text box tests
    Re-enabling keyboard related textbox tests, with a few updates to
    prevent flakiness:
    (1) Keyboard tests leverage the already-added 1px tolerances that were
        set up to re-enable the basic tests.
    (2) Fixing an issue where the text box did not record deletion as an
        edit to a textbox, leading to a pure deletion not being correctly
        committed in the manager.
    (3) Making "existing annotation" setup more accurate by properly
        initializing and then re-activating an annotation, instead of
        faking it in initializeBox.
    (4) Adding a ink-text-box-focused-for-test event that is fired by the
        textbox when calling focus() on itself, to avoid potentially
        flaky waiting on real blur/focus events.
    
    Bug: 440552067
    Change-Id: Ib5aebaa608dbbd0449f843ae4782406f16763dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858346
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633690}

--- chrome/browser/resources/pdf/elements/ink_text_box.ts
+++ chrome/browser/resources/pdf/elements/ink_text_box.ts
@@ -437,6 +437,7 @@ export class InkTextBoxElement extends InkTextBoxElementBase {
     const target = e.composedPath()[0];
     if (target === this.$.textbox) {
       this.focus();
+      this.fire('ink-text-box-focused-for-test');
     } else {
       this.commitTextAnnotation();
     }
@@ -455,6 +456,7 @@ export class InkTextBoxElement extends InkTextBoxElementBase {
     // Backspace/Delete key not in the textbox deletes the annotation.
     if (e.key === 'Backspace' || e.key === 'Delete') {
       this.textValue_ = '';
+      this.textBoxEdited_();
       this.commitTextAnnotation();
       return;
     }
