5e52977cc89671456b35145bdd1286d638f73e83
Stacy Gaikovaia <gaiko@google.com>
2026-05-21 12:31:37 -0700

    [blink] Use EarlyBeginMainFrame during ViewTransitions
    This changelist triggers an early BeginMainFrame if the current
    ViewTransitions is cross-document. This eliminates idle time where the
    renderer would have otherwise been waiting for the next BeginMainFrame
    to trigger the paintops cycle and results in a quicker transition from
    kInitial to kCaptureTagDiscovery stages.
    
    R=jonross@chromium.org
    
    Bug: 496610055
    Change-Id: I1a845b182033b5026149bebb6a725ceba692add8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848945
    Commit-Queue: Stacy Gaikovaia <gaiko@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Stacy Gaikovaia <gaiko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634462}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -1586,6 +1586,10 @@ void WebFrameWidgetImpl::SetBackgroundColor(SkColor color) {
       SkColor4f::FromColor(color));
 }
 
+void WebFrameWidgetImpl::SendEarlyFinalBeginMainFrame() {
+  widget_base_->LayerTreeHost()->RequestImmediateBeginMainFrame();
+}
+
 void WebFrameWidgetImpl::SetOverscrollBehavior(
     const cc::OverscrollBehavior& overscroll_behavior) {
   if (!View()->does_composite())
