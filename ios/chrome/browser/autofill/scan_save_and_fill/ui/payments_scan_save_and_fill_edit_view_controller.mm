abd2510c6bf469a021629a3c5c47781dc3e0c6a8
YiwenQian <yiwenqian@google.com>
2026-05-21 13:08:40 -0700

    [ScanCard IOS] Fix background color mismatch for save button
    Bug: 484102792
    Change-Id: I8a2801668fab5bf648bb798a4c22c89b4c06e2e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858519
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634486}

--- ios/chrome/browser/autofill/scan_save_and_fill/ui/payments_scan_save_and_fill_edit_view_controller.mm
+++ ios/chrome/browser/autofill/scan_save_and_fill/ui/payments_scan_save_and_fill_edit_view_controller.mm
@@ -142,7 +142,8 @@ NSString* const kDateSeparator = @"/";
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  self.view.backgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
+  self.view.backgroundColor =
+      [UIColor colorNamed:kGroupedPrimaryBackgroundColor];
 
   // Set title and cancel button.
   self.title = l10n_util::GetNSString(IDS_IOS_AUTOFILL_SAVE_CARD);
@@ -153,6 +154,8 @@ NSString* const kDateSeparator = @"/";
 
   _tableView = [[UITableView alloc] initWithFrame:CGRectZero
                                             style:ChromeTableViewStyle()];
+  _tableView.backgroundColor =
+      [UIColor colorNamed:kGroupedPrimaryBackgroundColor];
   _tableView.delegate = self;
   _tableView.tableHeaderView = [self createHeaderView];
   _tableView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -553,7 +556,7 @@ NSString* const kDateSeparator = @"/";
   _bottomContainerView.layoutMarginsRelativeArrangement = YES;
   _bottomContainerView.translatesAutoresizingMaskIntoConstraints = NO;
   _bottomContainerView.backgroundColor =
-      [UIColor colorNamed:kSecondaryBackgroundColor];
+      [UIColor colorNamed:kGroupedPrimaryBackgroundColor];
 
   [self.view addSubview:_tableView];
   [self.view addSubview:_bottomContainerView];
