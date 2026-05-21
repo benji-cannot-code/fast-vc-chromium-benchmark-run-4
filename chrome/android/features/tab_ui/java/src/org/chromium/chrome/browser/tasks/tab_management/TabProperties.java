215e4fb42efa62060dcac6d456f3c44b5b3dadb0
Gazal <agazal@google.com>
2026-05-21 16:49:48 -0700

    [Vertical Tabs] Support inline pinned tabs
    Introduce UiType.PINNED_TAB to support pinned tabs in the vertical
    tab list.
    
    Update VerticalTabListCoordinator to use a GridLayoutManager with a
    custom SpanSizeLookup. Pinned tabs take up 1 column, while regular
    tabs span the full width of the grid.
    
    Screenshot: https://screenshot.googleplex.com/3DBWJw5TH8UhVtz.png
    
    Bug: 515214828
    Change-Id: Ia2f7203d02e0c81075ed74c96f61f3e91ec33117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870169
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634604}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabProperties.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabProperties.java
@@ -49,6 +49,7 @@ public class TabProperties {
         UiType.TAB_GROUP_SUGGESTION_MESSAGE,
         UiType.IPH_MESSAGE,
         UiType.COLLABORATION_ACTIVITY_MESSAGE,
+        UiType.PINNED_TAB
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface UiType {
@@ -64,6 +65,7 @@ public class TabProperties {
         int TAB_GROUP_SUGGESTION_MESSAGE = 7;
         int IPH_MESSAGE = 8;
         int COLLABORATION_ACTIVITY_MESSAGE = 9;
+        int PINNED_TAB = 10;
     }
 
     /** IDs for possible tab action states. */
@@ -302,6 +304,7 @@ public class TabProperties {
                 TAB_LONG_CLICK_LISTENER,
                 TAB_CONTEXT_CLICK_LISTENER,
                 TAB_ACTION_BUTTON_DATA,
-                TAB_GROUP_CARD_COLOR
+                TAB_GROUP_CARD_COLOR,
+                IS_PINNED
             };
 }
