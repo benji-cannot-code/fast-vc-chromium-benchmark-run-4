07cc56cabfb389a9910386e8067e11dbbf8100d7
Jonathan Ross <jonross@chromium.org>
2026-05-21 06:57:06 -0700

    Convert std::set<base::WeakPtr> to std::vector in Telemetry Extension
    We should not use `std::set<base::WeakPtr>` due to invalidation of the
    WeakPtr, as well as reuse of WeakPtr addresses. See
    base/memory/weak_ptr.h for more details. Since we cannot build a safe
    less-operator for sets, we need an alternative lookup.
    
    Here we switch to using `std::vector<base::WeakPtr>`, while we must
    perform a linear scan, we can safely compare `base::WeakPtr.get()` to a
    pointer.
    
    Since each `RoutineControlProxy` is created immediately before insertion
    we do not need extra validation to prevent duplication.
    
    Change-Id: Id38dbd22e6e692710f1ffda44e5b9e440789d812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849815
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Byron Lee <byronlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634236}

--- chromeos/ash/components/telemetry_extension/routines/telemetry_diagnostic_routine_service_ash.cc
+++ chromeos/ash/components/telemetry_extension/routines/telemetry_diagnostic_routine_service_ash.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <utility>
+#include <vector>
 
 #include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
@@ -103,7 +104,7 @@ void TelemetryDiagnosticsRoutineServiceAsh::CreateRoutine(
   auto routine_control = RoutineControlProxy::Create(
       std::move(routine_receiver), std::move(cros_healthd_remote),
       std::move(control_delete_cb));
-  routine_controls_and_observers_.insert(std::move(routine_control));
+  routine_controls_and_observers_.push_back(std::move(routine_control));
 
   // Setup the RoutineObserver.
   mojo::PendingRemote<healthd::RoutineObserver> cros_healthd_observer;
@@ -116,7 +117,7 @@ void TelemetryDiagnosticsRoutineServiceAsh::CreateRoutine(
     auto routine_observer = RoutineObserverProxy::Create(
         cros_healthd_observer.InitWithNewPipeAndPassReceiver(),
         std::move(observer), std::move(observer_delete_cb));
-    routine_controls_and_observers_.insert(std::move(routine_observer));
+    routine_controls_and_observers_.push_back(std::move(routine_observer));
   }
 
   // Register the two objects with cros_healthd.
@@ -129,7 +130,10 @@ void TelemetryDiagnosticsRoutineServiceAsh::CreateRoutine(
 
 void TelemetryDiagnosticsRoutineServiceAsh::OnConnectionClosed(
     base::WeakPtr<SelfOwnedMojoProxyInterface> closed_connection) {
-  routine_controls_and_observers_.erase(closed_connection);
+  std::erase_if(routine_controls_and_observers_,
+                [&closed_connection](const auto& ptr) {
+                  return ptr.get() == closed_connection.get();
+                });
 }
 
 }  // namespace ash
