69c18345fca484e1a8575c60800bff255255d35b
Paulina Gacek <paulinagacek@google.com>
2026-05-21 04:29:55 -0700

    Reland "iwa: Support dynamic capture handle updates for PWAs/IWAs"
    This is a reland of commit 3221e4a93a9cf7f3b75419fd3fb38e1ed6e533c9.
    
    Reason for reland: This CL was reverted solely because its dependency
    (the "pull" phase CL) was reverted due to a test failure. No code
    changes were needed in this specific CL.
    
    Original change's description:
    > iwa: Support dynamic capture handle updates for PWAs/IWAs
    >
    > Currently, the Capture Handle API only supports dynamic updates for tab
    > captures.
    >
    > This CL implements the "Push" phase of Capture Handle support for
    > standalone web applications. It allows TYPE_WINDOW IDs to pass through
    > the capture pipeline, and updates CaptureHandleManager to resolve native
    > window IDs back to their underlying WebContents via gfx::NativeWindow
    > mapping.
    >
    > Design doc: go/capture-handle-for-pwas-iwas
    >
    > Bug: 494331663
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7715838
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Tove Petersson <tovep@chromium.org>
    > Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1628635}
    
    Bug: 494331663
    Change-Id: Iba70b6359b5ce257072bc5fedfe6be5d017a9994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842214
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634174}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/audio_debug_recordings_handler.h"
 #include "chrome/browser/media/webrtc/capture_policy_utils.h"
 #include "chrome/browser/media/webrtc/chrome_screen_enumerator.h"
+#include "chrome/browser/media/webrtc/desktop_capture_devices_util.h"
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/media/webrtc/media_device_salt_service_factory.h"
 #include "chrome/browser/media/webrtc/rtc_diagnostic_logging_utils.h"
@@ -2733,6 +2734,12 @@ bool ChromeContentBrowserClient::IsMultiCaptureAllowed(
 #else
   return false;
 #endif  // BUILDFLAG(IS_CHROMEOS)
+}
+
+content::WebContents*
+ChromeContentBrowserClient::GetWebContentsFromWindowIfCaptureHandleAllowed(
+    gfx::NativeWindow window) {
+  return ::GetWebContentsFromWindowIfCaptureHandleAllowed(window);
 }
 
 bool ChromeContentBrowserClient::IsFileAccessAllowed(
