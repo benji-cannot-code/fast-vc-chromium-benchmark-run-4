919587ed07bf361d4424f03d3ec8fdf7ca1dc059
Luis Antunes <luisantunes@google.com>
2026-05-20 17:29:30 -0700

    [Commerce] Add InStockNotificationManager skeleton to TabFeatures
    This change creates a new directory at
    chrome/browser/commerce/in_stock_notification with the skeleton for the
    notification feature. Inside the directory, it adds a new
    InStockNotificationManager class and BUILD.gn file. This manager is
    attached to TabFeatures as UnownedUserData, and its instantiation is
    guarded by the commerce::kInStockNotification feature flag. The manager
    uses ScopedUnownedUserData to tie its memory lifetime to the
    TabInterface. Finally, the core chrome/browser/ui/tabs BUILD.gn was
    updated with the new chrome/browser/commerce/in_stock_notification
    dependency.
    
    Bug: 508183131
    Change-Id: Iffdf8bd897547d662c9a2ea08aa52b3335c78d0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842599
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Luis Antunes <luisantunes@google.com>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633925}

--- chrome/browser/ui/tabs/tab_features.cc
+++ chrome/browser/ui/tabs/tab_features.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/actor/ui/actor_ui_tab_controller.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/browsing_topics/browsing_topics_service_factory.h"
+#include "chrome/browser/commerce/in_stock_notification/in_stock_notification_manager.h"
 #include "chrome/browser/commerce/shopping_service_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_service_factory.h"
@@ -132,6 +133,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/browsing_topics/browsing_topics_service.h"
+#include "components/commerce/core/commerce_feature_list.h"
 #include "components/favicon/content/content_favicon_driver.h"
 #include "components/image_fetcher/core/image_fetcher_service.h"
 #include "components/passage_embeddings/core/passage_embeddings_features.h"
@@ -350,6 +352,13 @@ void TabFeatures::Init(TabInterface& tab, Profile* profile) {
               .CreateInstance<CollaborationMessagingPageActionController>(
                   tab, tab, *page_action_controller_,
                   *collaboration_messaging_tab_data_);
+    }
+
+    if (base::FeatureList::IsEnabled(commerce::kInStockNotification) &&
+        !profile->IsIncognitoProfile()) {
+      in_stock_notification_manager_ =
+          GetUserDataFactory()
+              .CreateInstance<commerce::InStockNotificationManager>(tab, &tab);
     }
 
     if (glic::GlicEnabling::IsProfileEligible(profile)) {
