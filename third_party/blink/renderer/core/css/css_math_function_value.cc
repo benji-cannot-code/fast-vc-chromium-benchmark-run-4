8bcbd4d518472c3c735450315bee69df961415b7
Fredrik Söderquist <fs@opera.com>
2026-05-21 05:21:45 -0700

    Move NaN censoring into CSSMathFunctionValue::ClampToPermittedRange
    A NaN would not be modified by ClampToPermittedRange(), so any later
    censoring would produce an incorrect result for the kPositiveInteger
    value range.
    
    Perform the censoring as a first step in ClampToPermittedRange(),
    before applying the value range.
    
    Remove a few of the censoring operations in callers since they will be
    redundant.
    
    Add the censoring operation as CensorNaNToZero() to
    CSSValueClampingUtils, and also convert CSSValueClampingUtils to use it.
    
    Fixed: 509964638
    Change-Id: I33786a5a5b8c887e15247f4107aa2182ab1053aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7862886
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1634196}

--- third_party/blink/renderer/core/css/css_math_function_value.cc
+++ third_party/blink/renderer/core/css/css_math_function_value.cc
@@ -93,7 +93,7 @@ int CSSMathFunctionValue::ComputeInteger(
   // percentages.
   DCHECK_EQ(kCalcNumber, expression_->Category());
   DCHECK(!expression_->HasPercentage());
-  return ClampToWithNaNTo0<int>(
+  return ClampTo<int>(
       ClampToPermittedRange(expression_->ComputeNumber(length_resolver)));
 }
 
@@ -111,7 +111,7 @@ double CSSMathFunctionValue::ComputeNumber(
   if (expression_->Category() == kCalcPercent) {
     value /= 100.0;
   }
-  return std::isnan(value) ? 0.0 : CSSValueClampingUtils::ClampDouble(value);
+  return CSSValueClampingUtils::ClampDouble(value);
 }
 
 double CSSMathFunctionValue::ComputePercentage(
@@ -132,8 +132,7 @@ double CSSMathFunctionValue::ComputeValueInCanonicalUnit(
   std::optional<double> optional_value =
       expression_->ComputeValueInCanonicalUnit(length_resolver);
   DCHECK(optional_value.has_value());
-  double value = ClampToPermittedRange(optional_value.value());
-  return std::isnan(value) ? 0.0 : value;
+  return ClampToPermittedRange(optional_value.value());
 }
 
 std::optional<double> CSSMathFunctionValue::GetValueIfKnown() const {
@@ -190,6 +189,7 @@ bool CSSMathFunctionValue::Equals(const CSSMathFunctionValue& other) const {
 }
 
 double CSSMathFunctionValue::ClampToPermittedRange(double value) const {
+  value = CSSValueClampingUtils::CensorNaNToZero(value);
   switch (PermittedValueRange()) {
     case CSSPrimitiveValue::ValueRange::kInteger:
       return RoundHalfTowardsPositiveInfinity(value);
--- third_party/blink/renderer/core/css/css_value_clamping_utils.cc
+++ third_party/blink/renderer/core/css/css_value_clamping_utils.cc
@@ -9,11 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 double CSSValueClampingUtils::ClampDouble(double value) {
-  // https://www.w3.org/TR/css-values-4/#top-level-calculation
-  if (std::isnan(value)) {
-    value = 0;
-  }
-  return ClampTo<double>(value);
+  return ClampTo<double>(CensorNaNToZero(value));
 }
 
 double CSSValueClampingUtils::ClampLength(double value) {
@@ -21,11 +17,7 @@ double CSSValueClampingUtils::ClampLength(double value) {
 }
 
 float CSSValueClampingUtils::ClampLength(float value) {
-  // https://www.w3.org/TR/css-values-4/#top-level-calculation
-  if (std::isnan(value)) {
-    value = 0;
-  }
-  return ClampTo<float>(value);
+  return ClampTo<float>(CensorNaNToZero(value));
 }
 
 double CSSValueClampingUtils::ClampTime(double value) {
@@ -33,10 +25,6 @@ double CSSValueClampingUtils::ClampTime(double value) {
 }
 
 double CSSValueClampingUtils::ClampAngle(double value) {
-  // https://www.w3.org/TR/css-values-4/#top-level-calculation
-  if (std::isnan(value)) {
-    value = 0;
-  }
   // The value must be multiple of 360deg.
   // Reference:  https://drafts.csswg.org/css-values/#numeric-types
   //
@@ -46,7 +34,7 @@ double CSSValueClampingUtils::ClampAngle(double value) {
   //
   // The details: https://bit.ly/349gXjq
   static constexpr double kApproxDoubleInfinityAngle = 2867080569122160;
-  return ClampTo<double>(value, -kApproxDoubleInfinityAngle,
+  return ClampTo<double>(CensorNaNToZero(value), -kApproxDoubleInfinityAngle,
                          kApproxDoubleInfinityAngle);
 }
 
