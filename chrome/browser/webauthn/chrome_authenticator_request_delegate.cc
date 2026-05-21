f1b0f5bd19cd085b039eb94e19ff6d3c4cf865e0
Adem Derinel <derinel@google.com>
2026-05-21 08:04:17 -0700

    Remove unused cablev2_devices files
    These files weren't used in production
    
    Change-Id: I6a871da1d298eed469f66311efdd143143c4296f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866968
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634283}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webauthn/user_actions.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_controller.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
-#include "chrome/browser/webauthn/cablev2_devices.h"
 #include "chrome/browser/webauthn/enclave_manager.h"
 #include "chrome/browser/webauthn/gpm_enclave_controller.h"
 #include "chrome/browser/webauthn/immediate_request_rate_limiter_factory.h"
