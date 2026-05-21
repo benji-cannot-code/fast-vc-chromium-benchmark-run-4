fb5493c1963683eb18c6a6291c0f2897d323c7f2
Christian Dullweber <dullweber@chromium.org>
2026-05-21 05:05:22 -0700

    PageInfo: Restrict side panel to google-associated domains
    Restrict pageinfo side panel navigations to Google associated domains.
    Avoid navigating through an open redirect to a non-Google domain.
    
    Bug: 513866949
    Change-Id: Iab2ada6a8d392c8d3ec729dbd1fd433deaf599d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865408
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634189}

--- chrome/browser/page_info/web_view_side_panel_throttle.cc
+++ chrome/browser/page_info/web_view_side_panel_throttle.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/page_info/web_view_side_panel_throttle.h"
 
+#include "components/google/core/common/google_util.h"
 #include "components/navigation_interception/intercept_navigation_throttle.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
@@ -26,7 +27,8 @@ bool CanNavigateInPanel(content::NavigationHandle* handle,
   // Server redirects of the observed_url URL are allowed to stay in the
   // SidePanel.
   const GURL& original_url = handle->GetRedirectChain().front();
-  if (handle->WasServerRedirect() && original_url == observed_url) {
+  if (handle->WasServerRedirect() && original_url == observed_url &&
+      google_util::IsGoogleAssociatedDomainUrl(next_url)) {
     return true;
   }
   // Same URL navigations are allowed to stay in the SidePanel.
