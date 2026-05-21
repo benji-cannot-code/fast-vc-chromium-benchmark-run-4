8fe4f3cdb0a1b8f4f467fb13ff6293d4c507bcd2
Solomon Kinard <solomonkinard@chromium.org>
2026-05-21 15:50:49 -0700

    Extensions: CodeHealth: Spell check random word(s) & update arg comments
    * Function argument comments from the style guide:
    https://google.github.io/styleguide/cppguide.html#Function_Argument_Comments
    
    Bug: 415304336
    Change-Id: I19816e5e5679828e1a1df074fe6a9262cfc66209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7862278
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634570}

--- extensions/browser/api/web_request/web_request_api.cc
+++ extensions/browser/api/web_request/web_request_api.cc
@@ -248,7 +248,7 @@ void WebRequestAPI::Proxy::HandleAuthRequest(
   // Default implementation cancels the request.
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), std::nullopt,
-                                false /* should_cancel */));
+                                /*should_cancel=*/false));
 }
 
 WebRequestAPI::ProxySet::ProxySet() {
@@ -322,7 +322,7 @@ void WebRequestAPI::ProxySet::MaybeProxyAuthRequest(
     // their auth credentials.
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE, base::BindOnce(std::move(callback), std::nullopt,
-                                  false /* should_cancel */));
+                                  /*should_cancel=*/false));
     return;
   }
 
