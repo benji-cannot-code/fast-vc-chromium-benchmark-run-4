115c8d7319ca2380302b6f7cf69badf7e17f491a
Youssef Bourouphael <ybourouphael@google.com>
2026-05-21 08:01:27 -0700

    Refactor: Move WebUI test utils to a common file
    Moved `SetUpWebUI` and `GetWebUIToolbarWebView` from
    webui_toolbar_web_view_browsertest.cc to webui_test_utils.cc.
    
    Bug: 513510081
    Change-Id: I2759aa793517e5a44b1721927e58b3d8441b236e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7864688
    Auto-Submit: Youseff Bourouphel <ybourouphael@google.com>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634280}

--- chrome/browser/ui/views/toolbar/webui_toolbar_web_view_uitest.cc
+++ chrome/browser/ui/views/toolbar/webui_toolbar_web_view_uitest.cc
@@ -40,11 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-WebUIToolbarWebView* GetWebUIToolbarWebView(Browser* browser) {
-  return BrowserView::GetBrowserViewForBrowser(browser)
-      ->toolbar_button_provider()
-      ->GetWebUIToolbarViewForTesting();
-}
 
 std::string GetPinnedButtonJS(
     toolbar_ui_api::mojom::PinnedToolbarAction action) {
