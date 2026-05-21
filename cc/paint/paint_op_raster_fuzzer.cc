7a048cc91516b8f45c87b87eec7c0509cef97597
Florin Malita <fmalita@chromium.org>
2026-05-21 05:23:37 -0700

    [paint_op_raster_fuzzer] Add option to dump SKP
    To facilitate extraction of Skia draw ops, introduce a
    --dump-skp=<skp_file> command line switch.  When dumping to SKP,
    paint ops are executed on a recording canvas.
    
    Change-Id: Iec81b37b394d7419b4c4d2bc5a93de141e90b0f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863164
    Reviewed-by: Kaylee Lubick <kjlubick@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634198}

--- cc/paint/paint_op_raster_fuzzer.cc
+++ cc/paint/paint_op_raster_fuzzer.cc
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <cstdint>
+#include <optional>
 
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/process/memory.h"
 #include "base/test/test_discardable_memory_allocator.h"
@@ -20,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/transfer_cache_test_helper.h"
 #include "gpu/command_buffer/common/buffer.h"
 #include "gpu/command_buffer/service/service_font_manager.h"
+#include "testing/libfuzzer/libfuzzer_exports.h"
+#include "third_party/skia/include/core/SkPictureRecorder.h"
+#include "third_party/skia/include/core/SkStream.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GpuTypes.h"
 #include "third_party/skia/include/gpu/ganesh/GrDirectContext.h"
@@ -30,9 +35,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 struct Environment {
   Environment() {
-    // Disable noisy logging as per "libFuzzer in Chrome" documentation:
-    // testing/libfuzzer/getting_started.md#Disable-noisy-error-message-logging.
-    logging::SetMinLogLevel(logging::LOGGING_FATAL);
+    static constexpr char kDumpSKPSwitch[] = "dump-skp";
+    base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+    if (cl->HasSwitch(kDumpSKPSwitch)) {
+      dump_skp = cl->GetSwitchValuePath(kDumpSKPSwitch);
+    } else {
+      // Disable noisy logging as per "libFuzzer in Chrome" documentation:
+      // testing/libfuzzer/getting_started.md#Disable-noisy-error-message-logging.
+      logging::SetMinLogLevel(logging::LOGGING_FATAL);
+    }
 
     base::EnableTerminationOnOutOfMemory();
     base::DiscardableMemoryAllocator::SetInstance(
@@ -41,8 +52,11 @@ struct Environment {
 
   ~Environment() { base::DiscardableMemoryAllocator::SetInstance(nullptr); }
 
+  const std::optional<base::FilePath>& DumpSKP() const { return dump_skp; }
+
  private:
   base::TestDiscardableMemoryAllocator discardable_memory_allocator;
+  std::optional<base::FilePath> dump_skp;
 };
 
 class FontSupport : public gpu::ServiceFontManager::Client {
@@ -75,18 +89,10 @@ class FontSupport : public gpu::ServiceFontManager::Client {
   base::flat_map<uint32_t, scoped_refptr<gpu::Buffer>> buffers_;
 };
 
-void Raster(GrDirectContext* gr_context,
+void Raster(SkCanvas* canvas,
             SkStrikeClient* strike_client,
             cc::ServicePaintCache* paint_cache,
             base::span<const uint8_t> input) {
-  const size_t kRasterDimension = 32;
-
-  SkImageInfo image_info = SkImageInfo::MakeN32(
-      kRasterDimension, kRasterDimension, kOpaque_SkAlphaType);
-  sk_sp<SkSurface> surface =
-      SkSurfaces::RenderTarget(gr_context, skgpu::Budgeted::kYes, image_info);
-  SkCanvas* canvas = surface->getCanvas();
-
   cc::PlaybackParams params(nullptr, canvas->getLocalToDevice());
   cc::TransferCacheTestHelper transfer_cache_helper;
   std::vector<uint8_t> scratch_buffer;
@@ -119,6 +125,42 @@ void Raster(GrDirectContext* gr_context,
   }
 }
 
+void Raster(GrDirectContext* gr_context,
+            SkStrikeClient* strike_client,
+            cc::ServicePaintCache* paint_cache,
+            base::span<const uint8_t> input) {
+  const size_t kRasterDimension = 32;
+
+  SkImageInfo image_info = SkImageInfo::MakeN32(
+      kRasterDimension, kRasterDimension, kOpaque_SkAlphaType);
+  sk_sp<SkSurface> surface =
+      SkSurfaces::RenderTarget(gr_context, skgpu::Budgeted::kYes, image_info);
+
+  Raster(surface->getCanvas(), strike_client, paint_cache, input);
+}
+
+bool DumpSKP(SkStrikeClient* strike_client,
+             cc::ServicePaintCache* paint_cache,
+             base::span<const uint8_t> input,
+             const Environment& env) {
+  if (!env.DumpSKP()) {
+    return false;
+  }
+
+  SkFILEWStream wstream(env.DumpSKP()->AsUTF8Unsafe().c_str());
+  if (!wstream.isValid()) {
+    LOG(ERROR) << "Invalid --dump-skp output path.";
+    return true;
+  }
+
+  SkPictureRecorder recorder;
+  SkCanvas* canvas = recorder.beginRecording(32, 32);
+  Raster(canvas, strike_client, paint_cache, input);
+  recorder.finishRecordingAsPicture()->serialize(&wstream);
+
+  return true;
+}
+
 // Deserialize an arbitrary number of cc::PaintOps and raster them
 // using gpu raster into an SkCanvas.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
@@ -126,8 +168,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
-  [[maybe_unused]] static Environment* env = new Environment();
-  base::CommandLine::Init(0, nullptr);
+  static Environment env;
 
   // SAFETY: required from fuzzer.
   base::span<const uint8_t> data_span = UNSAFE_BUFFERS(base::span(data, size));
@@ -156,6 +197,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
                               &locked_handles);
   }
 
+  if (DumpSKP(font_manager->strike_client(), &paint_cache,
+              data_span.subspan(bytes_for_fonts), env)) {
+    return 0;
+  }
+
   GrMockOptions options_no_support;
   options_no_support.fShaderDerivativeSupport = false;
   auto gr_context_no_support = GrDirectContext::MakeMock(&options_no_support);
@@ -177,5 +223,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
   font_manager->Unlock(locked_handles);
   font_manager->Destroy();
+  return 0;
+}
+
+extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
+  base::CommandLine::Init(*argc, *argv);
   return 0;
 }
