ead85faba6eea15d6a419678312d2a3a05c98ef2
Andrzej Fiedukowicz <afie@google.com>
2026-05-21 04:48:34 -0700

    Extract common logic of client_id backup to a separate file
    Change-Id: I9cbbc9853c0d402b7819511f98fa78404bc79f41
    Bug: 171023419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7664504
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Andrzej Fiedukowicz <afie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634178}

--- chrome/browser/metrics/perf/profile_provider_unittest_main.cc
+++ chrome/browser/metrics/perf/profile_provider_unittest_main.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_path_override.h"
 #include "base/test/test_suite.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/perf/metric_provider.h"
 #include "chrome/browser/metrics/perf/perf_events_collector.h"
 #include "chrome/browser/metrics/perf/profile_provider_chromeos.h"
+#include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/ash/components/dbus/dbus_thread_manager.h"
 #include "components/variations/variations_associated_data.h"
@@ -273,6 +275,7 @@ class ProfileProviderRealCollectionTest : public testing::Test {
 
   std::unique_ptr<aura::Env> aura_env_;
   std::unique_ptr<TestProfileProvider> profile_provider_;
+  base::ScopedPathOverride override_user_data_dir_{chrome::DIR_USER_DATA};
 };
 
 // Flaky on chromeos: crbug.com/1184119
