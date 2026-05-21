15eecd0af90672f4246792fa9b6972d03e67ea87
Longsheng Yin <longsheng@google.com>
2026-05-21 14:30:02 -0700

    Fix InflationException in FacilitatedPaymentsPaymentMethodsViewBridgeTest
    This CL fixes an InflationException in FacilitatedPaymentsPaymentMethodsViewBridgeTest by explicitly setting the theme of the application context to Theme.BrowserUI.DayNight in the setUp method.
    
    The exception occurred because the layout pix_account_linking_prompt.xml uses `@macro/default_text_color_link` in https://chromium-review.git.corp.google.com/c/chromium/src/+/7849976, which requires an AppCompat theme (or descendant) to resolve its style attributes. In the Robolectric test environment, the default context does not have an AppCompat theme applied, leading to the crash during inflation. https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5724027415953408
    
    Change-Id: I9011162fb6f3cb6c7002d90aaab86d52e621ca8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865968
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Reviewed-by: Siddharth Shah <siashah@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634528}

--- chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsViewBridgeTest.java
+++ chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsViewBridgeTest.java
@@ -121,6 +121,7 @@ public class FacilitatedPaymentsPaymentMethodsViewBridgeTest {
         ProfileManager.setLastUsedProfileForTesting(mProfile);
         AutofillImageFetcherFactory.setInstanceForTesting(mAutofillImageFetcher);
         mApplicationContext = ApplicationProvider.getApplicationContext();
+        mApplicationContext.setTheme(R.style.Theme_BrowserUI_DayNight);
         mWindow = new WindowAndroid(mApplicationContext, /* occlusionTrackingAllowed= */ false);
         BottomSheetControllerFactory.attach(mWindow, mBottomSheetController);
         mViewBridge =
