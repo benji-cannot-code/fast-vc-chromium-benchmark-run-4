e9f84cbcfbff0f7b9002501b7648b215ce861562
Calder Kitagawa <ckitagawa@chromium.org>
2026-05-20 17:17:31 -0700

    [Tab] Tab Rehydration on Android
    On Android non-foreground tabs may not have a WebContents on cold start.
    This meant many operations in GLIC did not work on tabs that weren't
    active in the current session.
    
    This CL remedies this by switching to TabInterface and either special
    casing Android (or leveraging non-WebContents dependent x-platform APIs)
    to ensure these capabilities work on Android.
    
    Fixed: 475267465
    Change-Id: Ibbe2d765bac9b7e967e1648e7ce0ddb6fa24bb33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7854475
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633906}

--- chrome/browser/glic/host/context/glic_pin_candidate_comparator.h
+++ chrome/browser/glic/host/context/glic_pin_candidate_comparator.h
@@ -13,11 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/optional_ref.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
-namespace content {
+namespace tabs {
 
-class WebContents;
+class TabInterface;
 
-}  // namespace content
+}  // namespace tabs
 
 namespace glic {
 
@@ -40,7 +40,7 @@ class GlicPinCandidateComparator {
   GlicPinCandidateComparator& operator=(const GlicPinCandidateComparator&) =
       delete;
 
-  bool operator()(content::WebContents* a, content::WebContents* b);
+  bool operator()(tabs::TabInterface* a, tabs::TabInterface* b);
 
  private:
   const SearchResult GetSearchResults(const std::u16string& title);
