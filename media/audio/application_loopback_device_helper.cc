ef6dc20c156fb009ad19a4c1edbdceb89ece32e6
Olov Brändström <brandstrom@chromium.org>
2026-05-21 05:06:35 -0700

    Remove unused include in application_loopback_device_helper.cc
    Change-Id: I206cbe8de4da9db9a9369a98a15daf4f32272d01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867531
    Commit-Queue: Olov Brändström <brandstrom@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634191}

--- media/audio/application_loopback_device_helper.cc
+++ media/audio/application_loopback_device_helper.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/audio/application_loopback_device_helper.h"
 
-#include "base/notreached.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
