6715f32ce238bae66464f48a963fe6bec1e3f128
Dana Fried <dfried@chromium.org>
2026-05-20 18:31:36 -0700

    [User Education] Harden HelpBubbleEventRelay
    It's possible that during a callback the relay object could be destroyed
    so add a WeakPtrFactory and only make the post-callback update if the
    pointer is still valid.
    
    Fixed: 513135965
    Change-Id: Ib93687ed39532f91996ea5f352f46f6ec8aa6445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865821
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633983}

--- components/user_education/views/help_bubble_event_relay.h
+++ components/user_education/views/help_bubble_event_relay.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/events/event.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/menu/menu_item_view.h"
@@ -60,6 +61,8 @@ class HelpBubbleEventRelay {
   raw_ptr<HelpBubbleView> help_bubble_ = nullptr;
   raw_ptr<views::Button> hovered_button_ = nullptr;
   bool sent_click_ = false;
+
+  base::WeakPtrFactory<HelpBubbleEventRelay> weak_ptr_factory_{this};
 };
 
 namespace internal {
