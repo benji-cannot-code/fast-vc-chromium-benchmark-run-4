10903a92a4f94a83a67fa0b62ded3b7eba3c2720
Brandon Fong <bjfong@google.com>
2026-05-21 12:44:02 -0700

    [finds] Clear pending notifications when history is cleared
    Clear any pending finds notifications when history is cleared at any
    specified interval.
    
    Bug: 512908938
    Change-Id: I35cfdc725820e5f62d4cefaa912df5fc0b41d4af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858034
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634469}

--- chrome/browser/finds/core/finds_service.h
+++ chrome/browser/finds/core/finds_service.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/supports_user_data.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/finds/core/finds_metrics.h"
+#include "components/history/core/browser/history_service_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/core/model_execution/remote_model_executor.h"
 #include "components/optimization_guide/proto/features/finds.pb.h"
@@ -47,7 +48,8 @@ namespace finds {
 // inference.
 class FindsService : public KeyedService,
                      public base::SupportsUserData,
-                     public syncer::SyncServiceObserver {
+                     public syncer::SyncServiceObserver,
+                     public history::HistoryServiceObserver {
  public:
   class Observer : public base::CheckedObserver {
    public:
@@ -128,6 +130,10 @@ class FindsService : public KeyedService,
   void OnStateChanged(syncer::SyncService* sync) override;
   void OnSyncShutdown(syncer::SyncService* sync) override;
 
+  // history::HistoryServiceObserver:
+  void OnHistoryDeletions(history::HistoryService* history_service,
+                          const history::DeletionInfo& deletion_info) override;
+
   void CheckFindsNotificationsEnabledAndMaybeExecute();
   void OnHistoryQueryComplete(base::OnceCallback<void(Result)> callback,
                               history::QueryResults results);
@@ -160,6 +166,9 @@ class FindsService : public KeyedService,
   PrefChangeRegistrar pref_change_registrar_;
   base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
       sync_observation_{this};
+  base::ScopedObservation<history::HistoryService,
+                          history::HistoryServiceObserver>
+      history_observation_{this};
 
   base::WeakPtrFactory<FindsService> weak_ptr_factory_{this};
 };
