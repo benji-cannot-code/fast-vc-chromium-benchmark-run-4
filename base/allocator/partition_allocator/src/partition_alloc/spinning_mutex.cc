638c77cd1ca536a74a581759c9ae04009c7d8ecf
Fiona Verzivolli <fionav@google.com>
2026-05-21 07:30:38 -0700

    Clean up launch for PartitionAllocLockTuneSpin
    This change hardcodes the `spin_count` for PartitionAlloc's
    SpinningMutex on Android ARM64 to 2048, since 2048 was found to be
    optimal due to the PartitionAllocLockTuneSpin Finch experiment.
    
    Bug: 501373446
    Change-Id: I55a84c6bf56f9afec4826ef4da9b94d77d9e70f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859570
    Reviewed-by: Anand Ravi <anandrv@google.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Fiona Verzivolli <fionav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634257}

--- base/allocator/partition_allocator/src/partition_alloc/spinning_mutex.cc
+++ base/allocator/partition_allocator/src/partition_alloc/spinning_mutex.cc
@@ -87,14 +87,6 @@ class ScopedLockAcquisitionTimer {
 }  // namespace
 
 // static
-std::atomic<int> SpinningMutex::s_spin_count{SpinningMutex::kSpinCount};
-
-// static
-void SpinningMutex::SetSpinCount(int spin_count) {
-  s_spin_count.store(spin_count, std::memory_order_relaxed);
-}
-
-// static
 void SpinningMutex::SetLockMetricsRecorder(
     LockMetricsRecorderInterface* recorder) {
   auto* old_recorder =
@@ -120,7 +112,6 @@ void SpinningMutex::Reinit() {
 void SpinningMutex::AcquireSpinThenBlock() {
   int tries = 0;
   int backoff = 1;
-  const int spin_count = s_spin_count.load(std::memory_order_relaxed);
 
   do {
     if (Try()) [[likely]] {
@@ -146,7 +137,7 @@ void SpinningMutex::AcquireSpinThenBlock() {
     }
     constexpr int kMaxBackoff = 16;
     backoff = std::min(kMaxBackoff, backoff << 1);
-  } while (tries < spin_count);
+  } while (tries < kSpinCount);
 
   ScopedLockAcquisitionTimer timer;
   LockSlow();
