83782001480bf1cf1dc8d4d46db9316f157724f7
keybo <keybo@google.com>
2026-05-21 08:04:29 -0700

    ash: Clean up stale base::Feature QuickAppAccessTestUI
    BUG=356234782
    
    Change-Id: I329867ba513e8cd6822c6afba9762cb2058eaea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866701
    Auto-Submit: Keybo Qian <keybo@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634284}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -496,10 +496,6 @@ void AppListControllerImpl::OnSessionStateChanged(
     return;
   }
 
-  if (base::FeatureList::IsEnabled(features::kQuickAppAccessTestUI)) {
-    SetHomeButtonQuickApp(kOsSettingsAppId);
-  }
-
   if (in_clamshell)
     return;
 
