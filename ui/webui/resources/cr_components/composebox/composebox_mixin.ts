d9f7e70ef8788ac37e9e4b6fe7ccd355316a9cff
James Leung <jamesleung@google.com>
2026-05-21 12:25:07 -0700

    Fix crash in voice search in co-browse
    The crash occurred because SpeechRecognition.start() was called when
    recognition was already started. This could happen if the voice search
    button was clicked twice or if start() was called while in a running
    state.
    
    This CL adds guards in:
    
    1. onVoiceSearchButtonClick_ in composebox.ts to ignore clicks when
    already in voice search mode.
    2. start() in composebox_voice_search.ts to only proceed if state is
    UNINITIALIZED or ERROR_RECEIVED.
    
    Also added tests to verify these guards.
    Change-Id: I73243980a7147946a14a72d10fcc5296a4622528
    
    Change-Id: Id3bf9853eb191d6175a1f939f76a117efdc3bcf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747696
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634458}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -1151,6 +1151,9 @@ export const ComposeboxEmbedderMixin =
         }
 
         onVoiceSearchButtonClick() {
+          if (this.inVoiceSearchMode) {
+            return;
+          }
           this.inVoiceSearchMode = true;
           this.hasVoiceSearchError = false;
           this.animationState = GlowAnimationState.LISTENING;
