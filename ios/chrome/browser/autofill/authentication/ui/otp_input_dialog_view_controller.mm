b9a1db3fdf68f26c2edf47063b41a64c36d24194
James Sun <jamessun@google.com>
2026-05-21 14:38:47 -0700

    [Cleanup] Fix protocol pragma mark in OtpInputDialog
    Bug: 514722427
    Change-Id: I06c99292c5f047f78b18e19ce7ca7688d801d2e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869870
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634543}

--- ios/chrome/browser/autofill/authentication/ui/otp_input_dialog_view_controller.mm
+++ ios/chrome/browser/autofill/authentication/ui/otp_input_dialog_view_controller.mm
@@ -140,7 +140,7 @@ constexpr base::TimeDelta kNewCodeLinkCooldownTime = base::Seconds(5);
       kNewCodeLinkCooldownTime);
 }
 
-#pragma mark - PaymentsSuggestionBottomSheetConsumer
+#pragma mark - OtpInputDialogConsumer
 
 - (void)setContent:(OtpInputDialogContent*)content {
   // Content should not be updated once initialized.
