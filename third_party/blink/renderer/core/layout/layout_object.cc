74d6e5f8a748480b510a82ba6b3f478f33faf4a9
Koji Ishii <kojii@chromium.org>
2026-05-20 18:14:37 -0700

    Fix OOF not to `SetNeedsCollectInlines` across IFC
    This patch fixes `MarkParentForSpannerOrOutOfFlowPositionedChange`
    to call `SetChildNeedsCollectInlines` instead of
    `SetNeedsCollectInlines`, which is the correct function to
    call for the parent of the dirtied objects.
    
    Calling `SetNeedsCollectInlines` for the parent can set the
    `NeedsCollectInlines` flag across the IFC boundary, which is
    not allowed for atomic inlines.
    
    Fixed: 514295228, 403617975
    Change-Id: Ib5a3eee227ff3b9eb259dca7e7bf382ed79e6960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859770
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633955}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -1654,7 +1654,11 @@ void LayoutObject::MarkParentForSpannerOrOutOfFlowPositionedChange() {
   //
   // Note that this isn't necessary if we're dealing with a column spanner here,
   // but in order to keep things simple, we'll make no difference.
-  object->SetNeedsCollectInlines();
+  if (RuntimeEnabledFeatures::LayoutOOFCollectInlinesFixEnabled()) {
+    object->SetChildNeedsCollectInlines();
+  } else {
+    object->SetNeedsCollectInlines();
+  }
 
   const LayoutBlock* containing_block = ContainingBlock();
   while (object != containing_block) {
