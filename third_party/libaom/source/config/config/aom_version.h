3a3c37451e8015d30e83bdf8c04a7a3c1f6fb85e
Marco Paniconi <marpan@google.com>
2026-05-21 12:16:22 -0700

    Roll src/third_party/libaom/source/libaom/ 2ba2565bd..6f75fec9d (3 commits)
    https://aomedia.googlesource.com/aom.git/+log/2ba2565bdae6..6f75fec9dc36
    
    $ git log 2ba2565bd..6f75fec9d --date=short --no-merges --format='%ad %ae %s'
    2026-05-13 ranjit.tulabandu Avoid unintended RD evaluation of second MV
    2026-05-20 satheesh.kumar Extend sf `disable_extensive_joint_motion_search` to speed 0
    2026-05-19 marpan rtc: Use crop_width/height for source_last_TL0 buffer
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@google.com
    
    Bug: 307414544, 514559073
    Change-Id: I3b4257e039d62dc39927557a61e5f381d3167de8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867687
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Commit-Queue: Marco Paniconi <marpan@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634453}

--- third_party/libaom/source/config/config/aom_version.h
+++ third_party/libaom/source/config/config/aom_version.h
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 14
 #define VERSION_PATCH 0
-#define VERSION_EXTRA "31-g2ba2565bda"
+#define VERSION_EXTRA "34-g6f75fec9dc"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "3.14.0-31-g2ba2565bda"
-#define VERSION_STRING " 3.14.0-31-g2ba2565bda"
+#define VERSION_STRING_NOSP "3.14.0-34-g6f75fec9dc"
+#define VERSION_STRING " 3.14.0-34-g6f75fec9dc"
 #endif  // AOM_VERSION_H_
