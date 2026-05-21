636a760a40ba908dba30f649a3e1b8259a7637ed
Dominic Austria <dominicaustria@google.com>
2026-05-20 23:36:47 -0700

    [Glow Up] Animate Omnibox Page Info icon
    Add an animation to the page info icon in the omnibox when the bubble
    shows and hides.
    
    Screencast: https://screencast.googleplex.com/cast/NjcwNjU0MjcwMjIzMTU1MnxmYjk5NmVjMS0zYg
    Bug: 507859532
    Change-Id: I90b42002ee3491a8e435b26878845c85c8d81917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863220
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634074}

--- chrome/browser/ui/views/location_bar/location_icon_view.h
+++ chrome/browser/ui/views/location_bar/location_icon_view.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/icon_label_bubble_view.h"
 #include "components/security_state/core/security_state.h"
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/lottie/animation.h"
 
 namespace content {
 class WebContents;
@@ -121,12 +122,18 @@ class LocationIconView : public IconLabelBubbleView {
   // - the current page has a special scheme (chrome://, extension, file://).
   bool GetShowText() const;
 
+  // For animating the page info icon when the bubble shows and closes.
+  void MaybeAnimateIcon(bool show);
+
   const views::InkDrop* get_ink_drop_for_testing();
 
  protected:
   // IconLabelBubbleView:
   bool IsTriggerableEvent(const ui::Event& event) override;
   void UpdateBorder() override;
+
+  // gfx::AnimationDelegate:
+  void AnimationProgressed(const gfx::Animation* animation) override;
 
  private:
   friend class ToolbarViewTest;
@@ -163,6 +170,10 @@ class LocationIconView : public IconLabelBubbleView {
   // Set the rounded rect background with the given color.
   void SetBackgroundColor(SkColor color);
 
+  // Sets the icon to a frame of the |page_info_open_animation_|
+  // corresponding to the given |value|.
+  void SetAnimatedPageInfoIcon(float value);
+
   // The security level when the location icon was last updated. Used to decide
   // whether to animate security level transitions.
   security_state::SecurityLevel last_update_security_level_ =
@@ -176,6 +187,9 @@ class LocationIconView : public IconLabelBubbleView {
   bool was_editing_or_empty_ = false;
 
   raw_ptr<Delegate, DanglingUntriaged> delegate_;
+
+  std::unique_ptr<gfx::SlideAnimation> slide_animation_;
+  std::unique_ptr<lottie::Animation> page_info_open_animation_;
 
   // Used to scope the lifetime of asynchronous icon fetch callbacks to the
   // lifetime of the object. Weak pointers issued by this factory are
