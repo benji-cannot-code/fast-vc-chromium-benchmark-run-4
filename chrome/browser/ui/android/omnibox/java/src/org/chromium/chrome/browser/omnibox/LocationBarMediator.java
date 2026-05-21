fdb8679ad927e4a0eeb81133916a468b4686218f
Sky Malice <skym@chromium.org>
2026-05-21 12:16:01 -0700

    [AIM] Remove explicit fusebox esc key handling.
    The BackPressManager integration handles this instead now.
    
    Bug: 514351040
    Change-Id: Ie1a72924ee6ac7f5a2e82c02b796656e78637642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869211
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634452}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -2268,10 +2268,6 @@ class LocationBarMediator
         if (mCurrentInput == null) return false;
         if (mAutocompleteCoordinator == null) return false;
 
-        if (mFuseboxCoordinator.handleHidePopup()) {
-            return true;
-        }
-
         if (mAutocompleteCoordinator.isServingSuggestions()) {
             // First ESC keypress should close the suggestions list.
             mAutocompleteCoordinator.stopAutocomplete();
