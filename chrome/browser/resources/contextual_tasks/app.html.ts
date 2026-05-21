538dc460e9265666692c33d0a382d5106b1721c5
Juan Mojica <juanmojica@google.com>
2026-05-21 11:48:05 -0700

    [lens-convergence] Hide composebox and new task button if AIM ineligible
    This change plumbs the user's AIM eligibility status from the backend
    `AimEligibilityService` to the Contextual Tasks WebUI via load time
    data. Using this new `isAimEligible` flag, the UI now correctly hides
    the composebox in the main app element and the "new thread" button in
    the top toolbar for ineligible users. This ensures that users cannot
    attempt to interact with AI features they are not permitted to use.
    
    Screenshot: http://screen/3fXzgjp8LG9Qsrg
    Bug: b:510807887, b:510807786
    Change-Id: I0a9034f264740cafdcc3f96bb37abf56d4605fa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851359
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634434}

--- chrome/browser/resources/contextual_tasks/app.html.ts
+++ chrome/browser/resources/contextual_tasks/app.html.ts
@@ -18,6 +18,7 @@ export function getHtml(this: ContextualTasksAppElement) {
           .title="${this.threadTitle_}"
           .darkMode="${this.darkMode_}"
           .isAiPage="${this.isAiPage_}"
+          .isAimEligible="${this.isAimEligible_}"
           .enableOpenInNewTabButton="${this.isAiPage_ && !this.isErrorPageVisible_}"
           @new-thread-click="${this.onNewThreadClick_}">
       </top-toolbar>
