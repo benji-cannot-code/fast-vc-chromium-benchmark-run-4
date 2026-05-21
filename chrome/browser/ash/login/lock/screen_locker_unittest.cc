59d09498b9ce186c3344e1fbb46937dfb19146c1
Jun Ishiguro <junis@google.com>
2026-05-21 00:15:59 -0700

    Explicitly create ash::quick_unlock::PinBackend singleton object
    This is a follow-up of https://crrev.com/c/7720241.
    
    This CL adds the Initialize() method and call it on process startup,
    specifically on ChromeBrowserMainPartsAsh::PreProfileInit().
    
    Bug: b:498416395
    Change-Id: I6068eb30a9591dbe0366567a0a0f78ff96a3834c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777887
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634089}

--- chrome/browser/ash/login/lock/screen_locker_unittest.cc
+++ chrome/browser/ash/login/lock/screen_locker_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ash/certificate_provider/certificate_provider_service.h"
 #include "chrome/browser/ash/certificate_provider/certificate_provider_service_factory.h"
+#include "chrome/browser/ash/login/quick_unlock/pin_backend.h"
 #include "chrome/browser/ash/login/session/user_session_manager.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
@@ -105,6 +106,8 @@ class ScreenLockerUnitTest : public testing::Test {
             ->platform_part()
             ->browser_policy_connector_ash());
 
+    quick_unlock::PinBackend::Initialize();
+
     testing_profile_manager_ = std::make_unique<TestingProfileManager>(
         TestingBrowserProcess::GetGlobal());
     ASSERT_TRUE(testing_profile_manager_->SetUp());
@@ -164,6 +167,10 @@ class ScreenLockerUnitTest : public testing::Test {
     BiodClient::Shutdown();
     ConciergeClient::Shutdown();
     SystemSaltGetter::Shutdown();
+
+    // TODO(crbug.com/498416395): We should refactor PinBackend and
+    // shutdown/destroy it in the reverse order of initialization.
+    quick_unlock::PinBackend::Shutdown();
   }
 
   void CreateSessionForUser(bool is_public_account) {
