fdb8f6dd57e7faabe76c6fb41127383c51e966bb
Joel Tan-Aristy <jtanaristy@google.com>
2026-05-21 09:34:08 -0700

    [CustomContextMenu] Updates context menu header to show page title
    Adds text view, properties, updates view binder and coordinator to
    support showing the page title in CCT context menus.
    
    Before: https://screenshot.googleplex.com/8PARedgPMZe8KFr
    
    After: https://screenshot.googleplex.com/98XD7ft58dFP3Ya
    
    Bug: 514771424
    Change-Id: I15d8947a21c5118a7ee8871f8f39c525ebde2385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7861932
    Auto-Submit: Joel Tan-Aristy <jtanaristy@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Moe Adel <adelm@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634348}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHeaderProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHeaderProperties.java
@@ -21,6 +21,8 @@ class ContextMenuHeaderProperties {
     /** Invalid value for OVERRIDE_*_PIXEL resources */
     static final int INVALID_OVERRIDE = -1;
 
+    public static final WritableObjectPropertyKey<CharSequence> PAGE_TITLE =
+            new WritableObjectPropertyKey<>();
     public static final WritableIntPropertyKey TITLE_MAX_LINES = new WritableIntPropertyKey();
     public static final WritableObjectPropertyKey<CharSequence> URL =
             new WritableObjectPropertyKey<>();
@@ -68,6 +70,7 @@ class ContextMenuHeaderProperties {
             new PropertyModel.WritableBooleanPropertyKey();
 
     public static final PropertyKey[] ALL_KEYS = {
+        PAGE_TITLE,
         TITLE,
         TITLE_MAX_LINES,
         URL,
