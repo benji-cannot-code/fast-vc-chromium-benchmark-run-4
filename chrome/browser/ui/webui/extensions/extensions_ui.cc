1a1decf55a348ba27c73cd89fa2dd18357564e9d
Shuhei Takahashi <nya@chromium.org>
2026-05-20 22:04:12 -0700

    Revert^2 "[GlowUp] Use themed colors in Extensions"
    This reverts commit 0d035c1f1aada0bf1a1fc2df6b75b0209c667384.
    
    Reason for revert:
    - crrev.com/c/7836288 Original patch broke chrome://extensions on debug builds
    - crrev.com/c/7865590 Revert unexpectedly broke the build and closed the tree
    - crrev.com/c/7864937 Revert for the revert
    - This patch reattempts the revert
    
    Original change's description:
    > Reland "[GlowUp] Use themed colors in Extensions"
    >
    > This reverts commit a1053d17997e53104ed18e1eda61e5e51998d632.
    >
    > Reason for revert: The revert broke the android-desktop build
    > (though it passed the same builder in CQ):
    >
    > ERROR at //ui/webui/resources/cr_components/color_change_listener/BUILD.gn:9:1: Assertion failed.
    > assert((!is_android && !is_ios) || enable_webui_ntp)
    > ^-----
    > See //chrome/browser/resources/management/BUILD.gn:30:5: which caused the file to be included.
    >     "//ui/webui/resources/cr_components/color_change_listener:build_ts",
    >     ^------------------------------------------------------------------
    >
    > https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/77998/overview
    >
    > Original change's description:
    > > Revert "[GlowUp] Use themed colors in Extensions"
    > >
    > > This reverts commit 50404b499cb02f98d7d54f25d7cf146eb967607d.
    > >
    > > Reason for revert: Causes chrome://extensions to be blank on
    > > desktop Android in debug builds.
    > >
    > > Bug: 515059987
    > >
    > > Original change's description:
    > > > [GlowUp] Use themed colors in Extensions
    > > >
    > > > Fixed: b:510403909, 513571197
    > > > Change-Id: I040520a9e730003df827006acb15f594d1ebbe40
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7836288
    > > > Reviewed-by: Will Harris <wfh@chromium.org>
    > > > Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    > > > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > > > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1632308}
    > >
    > > Bug: b:510403909, 513571197
    > > Change-Id: Ic1d38fe50a05416967b5fe5a5527fe48d61b71c2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865590
    > > Commit-Queue: James Cook <jamescook@chromium.org>
    > > Reviewed-by: Will Harris <wfh@chromium.org>
    > > Owners-Override: Jack Thiesen <jthiesen@chromium.org>
    > > Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    > > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > > Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1633982}
    >
    > Bug: 515059987
    > Bug: b:510403909, 513571197
    > Change-Id: I723c1d8c0b134c374b91e9cfcba366856331c43e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7864937
    > Owners-Override: ben chin <lungc@chromium.org>
    > Commit-Queue: ben chin <lungc@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: ben chin <lungc@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1633994}
    
    Bug: 515059987
    Bug: b:510403909, 513571197
    Test: Local build passes for desktop android
    Change-Id: I95695098d90f5c2669df477a6c4d0945c2c84706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866741
    Auto-Submit: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: ben chin <lungc@chromium.org>
    Reviewed-by: ben chin <lungc@chromium.org>
    Owners-Override: ben chin <lungc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634051}

--- chrome/browser/ui/webui/extensions/extensions_ui.cc
+++ chrome/browser/ui/webui/extensions/extensions_ui.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/permissions_url_constants.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/favicon_source.h"
 #include "chrome/browser/ui/webui/managed_ui_handler.h"
 #include "chrome/browser/ui/webui/metrics_handler.h"
@@ -515,11 +514,6 @@ content::WebUIDataSource* CreateAndAddExtensionsSource(Profile* profile,
           l10n_util::GetStringUTF16(IDS_SHORT_PRODUCT_OS_NAME)));
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
-  source->AddString("webuiRefresh2026",
-                    base::FeatureList::IsEnabled(features::kWebuiRefresh2026)
-                        ? "webui-refresh-2026"
-                        : "");
-
   return source;
 }
 
@@ -541,8 +535,7 @@ ExtensionsUIConfig::CreateWebUIController(content::WebUI* web_ui,
   return std::make_unique<ExtensionsUI>(web_ui);
 }
 
-ExtensionsUI::ExtensionsUI(content::WebUI* web_ui)
-    : ui::MojoWebUIController(web_ui, /*enable_chrome_send=*/true) {
+ExtensionsUI::ExtensionsUI(content::WebUI* web_ui) : WebUIController(web_ui) {
   Profile* profile = Profile::FromWebUI(web_ui);
   content::WebUIDataSource* source = nullptr;
 
