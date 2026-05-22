3b7a7ca97ca5c0c7b72adb8f0911f868f38225d0
Eric Ekey <ericekey@google.com>
2026-05-22 08:41:54 -0700

    [iOS][AppBar] Fade NTP Header on Landscape Scroll
    This change fades the NTP header alpha when the fakebox is being
    scrolled out of view in landscape orientation. This change also fixes an
    issue with the header alpha being stuck at 0 when the device was rotated
    while away from the NTP.
    
    Bug: 498602138
    Change-Id: I8c7651c15387f488e93631038bdc647d3acbed3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869899
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634979}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_view_controller.mm
@@ -1441,9 +1441,7 @@ const CGFloat kBackgroundImageAnimationDuration = 0.2;
 
   if (self.shouldAnimateHeader) {
     BOOL animateScrollAnimation =
-        IsChromeNextIaEnabled()
-            ? (IsSplitToolbarMode(self) || CanShowTabStrip(self))
-            : !self.disableScrollAnimation;
+        IsChromeNextIaEnabled() ? YES : !self.disableScrollAnimation;
     UIEdgeInsets insets = self.collectionView.safeAreaInsets;
     [self.headerViewController
         updateFakeOmniboxForOffset:[self adjustedOffset].y
