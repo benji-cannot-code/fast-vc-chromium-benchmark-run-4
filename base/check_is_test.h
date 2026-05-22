f4e7392c641af23b35342a8103204998aa7942af
Cliff Smolinsky <Cliff.Smolinsky@microsoft.com>
2026-05-22 14:16:04 -0700

    Remove comment about ForTesting methods from check_is_test.h
    check_is_test.h recommends not to use it inside ForTesting() methods
    because there is a presubmit check, however the presubmit check is only
    a warning and is not an effective way to prevent issues. CHECK_IS_TEST()
    inside ForTesting() methods is the best way to block them from use in
    production code.
    
    This change updates the language to clarify that the "should not" is a
    recommendation for trivial accessors, not the codebase as a whole.
    
    Change-Id: Idfa72f1f93f8064ae79de74b1cb777b050f0733a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856324
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Cliff Smolinsky <cliffsmo@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1635215}

--- base/check_is_test.h
+++ base/check_is_test.h
@@ -35,9 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //     return;
 //   }
 //
-// `CHECK_IS_TEST` should not be used within functions named `*ForTesting`,
-// `*ForTests`, etc. because there is a presubmit check which warns against
-// calling such functions in production code.
+// `CHECK_IS_TEST` should not be used within trivial accessors such as
+// `member_name_for_testing()`.
+//
+// There is a presubmit check which warns against calling `*ForTesting` or
+// `*ForTests` functions in production code, however `CHECK_IS_TEST` may be used
+// in those functions to ensure correctness at runtime.
 //
 // `CHECK_IS_TEST` is thread safe.
 //
