6ec32ece1a16d907efd4ee0aede740e4d1e84ca8
Michael Wilson <mjwilson@chromium.org>
2026-05-22 12:37:38 -0700

    Add additional traces for convolver-node perf test
    The current traces do not give enough information about FFT performance
    so we add additional traces to be used for performance evaluation.
    
    Change-Id: I4560b5f346604aa5ff818fe5b887f81495c11298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870793
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635149}

--- third_party/blink/renderer/modules/webaudio/convolver_handler.cc
+++ third_party/blink/renderer/modules/webaudio/convolver_handler.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/audio/reverb.h"
 #include "third_party/blink/renderer/platform/bindings/exception_messages.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/strcat.h"
 
@@ -70,6 +71,8 @@ ConvolverHandler::~ConvolverHandler() {
 }
 
 void ConvolverHandler::Process(uint32_t frames_to_process) {
+  TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("webaudio.audionode"),
+               "ConvolverHandler::Process");
   AudioBus* output_bus = Output(0).Bus();
   DCHECK(output_bus);
   DCHECK(Context()->IsAudioThread());
--- third_party/blink/renderer/platform/audio/fft_convolver.cc
+++ third_party/blink/renderer/platform/audio/fft_convolver.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/audio/fft_convolver.h"
 
 #include "third_party/blink/renderer/platform/audio/vector_math.h"
+#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 
 namespace blink {
 
@@ -82,10 +83,14 @@ void FFTConvolver::Process(const FFTFrame* fft_kernel,
 
     // Check if it's time to perform the next FFT
     if (read_write_index_ == half_size) {
-      // The input buffer is now filled (get frequency-domain version)
-      frame_.DoFFT(input_buffer_span);
-      frame_.Multiply(*fft_kernel);
-      frame_.DoInverseFFT(output_buffer_span);
+      {
+        TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("webaudio.audionode"),
+                     "FFTConvolver::ExecuteFFT");
+        // The input buffer is now filled (get frequency-domain version)
+        frame_.DoFFT(input_buffer_span);
+        frame_.Multiply(*fft_kernel);
+        frame_.DoInverseFFT(output_buffer_span);
+      }
 
       // Overlap-add 1st half from previous time
       vector_math::Vadd(output_buffer_.as_span(),
