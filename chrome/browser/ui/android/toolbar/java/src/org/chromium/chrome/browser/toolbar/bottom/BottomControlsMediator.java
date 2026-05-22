368aa1f8bab44af5647f1be70f6528509ec0665b
Calder Kitagawa <ckitagawa@chromium.org>
2026-05-22 13:36:53 -0700

    [IA] Remove drop shadow from Bottom Controls
    Gated on BottomBarConfigUtils.isBottomBarEnabled() is enabled.
    
    Fixed: 515409548
    Change-Id: I7d1ffb18ae6c58d1045c23f0fe15dba66a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871128
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635188}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/bottom/BottomControlsMediator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/bottom/BottomControlsMediator.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.toolbar.bottom;
 
+import android.content.Context;
+
 import androidx.annotation.ColorInt;
 
 import org.chromium.base.CallbackController;
@@ -27,6 +29,7 @@ import org.chromium.chrome.browser.layouts.LayoutStateProvider.LayoutStateObserv
 import org.chromium.chrome.browser.layouts.LayoutType;
 import org.chromium.chrome.browser.overlay_panel.PanelState;
 import org.chromium.chrome.browser.tab.TabObscuringHandler;
+import org.chromium.chrome.browser.ui.bottombar.BottomBarConfigUtils;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeController;
 import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.base.WindowAndroid;
@@ -256,6 +259,16 @@ class BottomControlsMediator
         updateAndroidViewVisibility();
     }
 
+    private boolean shouldShowShadow() {
+        if (mWindowAndroid.getContext() != null) {
+            Context context = mWindowAndroid.getContext().get();
+            if (context != null && BottomBarConfigUtils.isBottomBarEnabled(context)) {
+                return false;
+            }
+        }
+        return mBottomControlsStacker.isTopmostVisibleLayer(mLayerType);
+    }
+
     /**
      * The composited view is the composited version of the Android View. It is used to be able to
      * scroll the bottom controls off-screen synchronously. Since the bottom controls live below the
@@ -267,9 +280,7 @@ class BottomControlsMediator
         final boolean isCompositedViewVisible = isCompositedViewVisible();
         mModel.set(BottomControlsProperties.COMPOSITED_VIEW_VISIBLE, isCompositedViewVisible);
         mBottomControlsStacker.requestLayerUpdate(false);
-        mModel.set(
-                BottomControlsProperties.SHOW_SHADOW,
-                mBottomControlsStacker.isTopmostVisibleLayer(mLayerType));
+        mModel.set(BottomControlsProperties.SHOW_SHADOW, shouldShowShadow());
     }
 
     private int getAndroidViewHeight() {
@@ -346,16 +357,14 @@ class BottomControlsMediator
     @Override
     public void onBrowserControlsOffsetUpdate(int layerYOffset) {
         setYOffset(layerYOffset);
-        mModel.set(
-                BottomControlsProperties.SHOW_SHADOW,
-                mBottomControlsStacker.isTopmostVisibleLayer(mLayerType));
+        mModel.set(BottomControlsProperties.SHOW_SHADOW, shouldShowShadow());
     }
 
     @Override
     public int updateOffsetTag(BrowserControlsOffsetTagsInfo offsetTagsInfo) {
         mModel.set(
                 BottomControlsProperties.OFFSET_TAG, offsetTagsInfo.getBottomControlsOffsetTag());
-        return mModel.get(BottomControlsProperties.SHOW_SHADOW) ? mBottomControlsShadowHeight : 0;
+        return shouldShowShadow() ? mBottomControlsShadowHeight : 0;
     }
 
     @Override
