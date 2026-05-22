1151cc5a5d2031037d498a4c810b00e78023eb8b
Martín Gómez <martinglopez@google.com>
2026-05-22 11:13:56 -0700

    [Reading Mode] Add IPH for changing presentation state
    Incorporate a new In Product Help (IPH) bubble to show users how to
    change the presentation state from immersive mode to side panel mode.
    
    Screenshot for the new IPH bubble:
    https://docs.google.com/presentation/d/1Dq_bTsYjzopPrgVTsmM7GRtlMl2f3A-7mLVPfRXrowc/edit?usp=sharing&resourcekey=0-L1QYHtj6oD0yK-s1AUaRZA
    
    Bug: 489481172
    Change-Id: I32099bdbd7096a024f55c378492c2a75400008ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858058
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Martín Gómez <martinglopez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635091}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -296,6 +296,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHReadingListInSidePanelFeature,
     &kIPHReadingModeKeyboardShortcutFeature,
     &kIPHReadingModePageActionLabelFeature,
+    &kIPHReadingModePresentationModeFeature,
     &kIPHReadingModeSidePanelFeature,
     &kIPHShoppingCollectionFeature,
     &kIPHSideBySidePinnableFeature,
