7c0e8fb7d53e973ec09324fdb6c7c11d2451cb15
Jeffrey Yu <yuje@chromium.org>
2026-05-22 09:19:45 -0700

    Replace `SplitString` with `SplitStringPiece`.
    Since the created string_view objects don't need to
    outlive their parent string, this helps avoid
    unnecessary string copies.
    
    Bug: 501076278
    Change-Id: I3bf1dc9b094beaf4cc81009d4f067f95bb5aadd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7803401
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635009}

--- ui/base/accelerators/command.cc
+++ ui/base/accelerators/command.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/accelerators/command.h"
 
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "base/functional/bind.h"
@@ -226,7 +227,7 @@ ui::Accelerator Command::ParseImpl(std::string_view accelerator,
   // is not allowed.
   const size_t max_token_size =
       allow_ctrl_alt ? kMaxTokenSize : kMaxTokenSize - 1;
-  std::vector<std::string> tokens = base::SplitString(
+  std::vector<std::string_view> tokens = base::SplitStringPiece(
       accelerator, "+", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
   if (tokens.empty() || tokens.size() > max_token_size) {
     std::move(error_callback).Run(ui::AcceleratorParseError::kMalformedInput);
@@ -236,7 +237,7 @@ ui::Accelerator Command::ParseImpl(std::string_view accelerator,
   // Now, parse it into an accelerator.
   int modifiers = ui::EF_NONE;
   ui::KeyboardCode key = ui::VKEY_UNKNOWN;
-  for (const std::string& token : tokens) {
+  for (std::string_view token : tokens) {
     if (token == ui::kKeyCtrl) {
       modifiers |= ui::EF_CONTROL_DOWN;
     } else if (token == ui::kKeyCommand) {
