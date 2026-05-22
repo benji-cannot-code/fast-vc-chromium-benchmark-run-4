4ca039857f2c1d1187518d31e8ca83bfaf755ed4
Kamil Jarosz <kjarosz@google.com>
2026-05-22 14:22:50 -0700

    protobuf: Add :io_gzip_stream target
    gzip_stream.cc provides compression and decompression capabilities for
    protobuf streams. As it depends on zlib, it's being extracted as a
    separate target instead of being added to protobuf_lite.
    
    In order to use it in a separate target, symbol exports had to be
    removed.
    
    Fix: b:513136433
    Change-Id: Ia7c7cd7a269b3112b40fd54834673476deb55e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865410
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635221}

--- third_party/protobuf/src/google/protobuf/io/gzip_stream.h
+++ third_party/protobuf/src/google/protobuf/io/gzip_stream.h
@@ -29,6 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Must be included last.
 #include "google/protobuf/port_def.inc"
 
+#undef PROTOBUF_EXPORT
+#define PROTOBUF_EXPORT
+
 namespace google {
 namespace protobuf {
 namespace io {
