e094665f174e7654243eb37c0b68b4283623bf47
Jonathan Ross <jonross@chromium.org>
2026-05-22 08:36:58 -0700

    Revert "Fix use-after-free crash in GlicInstanceImpl::Show on Android"
    This reverts commit 0ef812f88a043105683d1cdf1696225d22f4947a.
    
    Reason for revert: Broke the CQ https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20arm64%20Builder%20All%20Targets%20%28dbg%29/74585/overview
    
    Original change's description:
    > Fix use-after-free crash in GlicInstanceImpl::Show on Android
    >
    > When a bottom sheet fails to show synchronously on Android, the active
    > embedder is immediately closed and destroyed. Attempting to call Focus()
    > on the dangling embedder pointer resulted in a SIGSEGV crash.
    >
    > - Check IsActiveEmbedder(new_key) and retrieve a fresh pointer via
    >   GetActiveEmbedder() in GlicInstanceImpl::Show before calling Focus().
    > - Add SuppressBottomSheetForTesting test plumbing to
    >   TabBottomSheetManagerImpl, TabBottomSheetNativeInterface,
    >   TabBottomSheetBridge, and GlicSidePanelCoordinatorAndroid.
    > - Add ShowFailsWhenSuppressedCausesCrash regression test in
    >   GlicAndroidPeekBrowserTest.
    >
    > Bug: 512567837
    > Link: https://chromium-review.googlesource.com/id/I12545b1246c2147f77734622b540dd466a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848470
    > Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    > Commit-Queue: Dan H <harringtond@chromium.org>
    > Reviewed-by: Hailey Wang <haileywang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1634962}
    
    Bug: 512567837
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8c0366e4a053cc2bb4291b70a2810b7dd3cfedba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871430
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634971}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetManagerImpl.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetManagerImpl.java
@@ -175,8 +175,7 @@ public class TabBottomSheetManagerImpl implements TabBottomSheetManager {
             mNativeInterfaceDelegate = nativeInterfaceDelegate;
             return true;
         }
-        if (!mSuppressBottomSheetForTesting
-                && mTabBottomSheetCoordinator.tryToShowBottomSheet(animate, startsExpanded)) {
+        if (mTabBottomSheetCoordinator.tryToShowBottomSheet(animate, startsExpanded)) {
             // Successfully showed bottom sheet.
             mNativeInterfaceDelegate = nativeInterfaceDelegate;
             return true;
@@ -342,12 +341,6 @@ public class TabBottomSheetManagerImpl implements TabBottomSheetManager {
 
     public @Nullable NativeInterfaceDelegate getNativeInterfaceDelegateForTesting() {
         return mNativeInterfaceDelegate;
-    }
-
-    private boolean mSuppressBottomSheetForTesting;
-
-    public void suppressBottomSheetForTesting(boolean suppress) {
-        mSuppressBottomSheetForTesting = suppress;
     }
 
     public void setReadAloudActivePlaybackTabSupplierForTesting(
