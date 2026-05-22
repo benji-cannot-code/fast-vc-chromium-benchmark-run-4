bf9e9c00637c485a935def90d1a6838b723cbf6e
Bryant Chandler <bryantchandler@google.com>
2026-05-22 15:09:15 -0700

    [glic] Globally block all In-Product Help (IPH) features in Glic tests
    Glic browser tests can trigger various In-Product Help (IPH) features
    unexpectedly (e.g., adaptive top toolbar customizations on Android),
    causing test flakiness.
    
    This CL introduces ScopedIphFeatureList in GlicBrowserTestMixin.
    
    Bug: 515611513, 515357372
    Change-Id: I10f418e1ea31c8e9b7373349f01df7916a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870762
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635245}

--- chrome/browser/glic/test_support/glic_browser_test.h
+++ chrome/browser/glic/test_support/glic_browser_test.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/platform_browser_test.h"
+#include "components/feature_engagement/test/scoped_iph_feature_list.h"
 #include "components/tabs/public/tab_interface.h"
 #include "content/public/test/browser_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -148,6 +149,10 @@ class GlicBrowserTestMixin : public T {
     glic_test_environment_.SetGlicPagePath(
         "/glic/browser_tests/minimal_client.html");
     scoped_feature_list_.InitWithFeaturesAndParameters(enabled_features, {});
+    // Globally block all In-Product Help (IPH) triggers in all Glic browser
+    // tests to avoid flakiness caused by unexpected IPH popups (e.g., adaptive
+    // top toolbar customization cues on Android), which can make glic hide.
+    scoped_iph_feature_list_.InitWithNoFeaturesAllowed();
   }
   ~GlicBrowserTestMixin() override = default;
 
@@ -477,6 +482,7 @@ class GlicBrowserTestMixin : public T {
 #endif
   GlicTestEnvironment glic_test_environment_;
   base::test::ScopedFeatureList scoped_feature_list_;
+  feature_engagement::test::ScopedIphFeatureList scoped_iph_feature_list_;
 #if defined(USE_MOCK_ACTIVATION_CONTROLLER)
   std::unique_ptr<views::test::MockActivationController> activation_controller_;
 #endif
