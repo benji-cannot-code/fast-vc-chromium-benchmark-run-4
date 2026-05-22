2181411110ef7e3fc004fe14fcd47b1985b27ae9
Jihad Hanna <jihadghanna@google.com>
2026-05-22 03:40:19 -0700

    Remove unused variable in tests
    Bug: 40100455
    Change-Id: I37098de4d7731345d44c3b41e6d1f7041e9a3ad7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871029
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634845}

--- components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager_unittest.cc
@@ -5566,7 +5566,6 @@ TEST_F(BrowserAutofillManagerTest,
   // upload.
   FormsSeen({form});
 
-  std::vector<std::u16string> expected_values = {u"Elvis", u"Aaron", u"A"};
   // These fields should all match.
   std::vector<FieldTypeSet> expected_types = {
       {NAME_FIRST}, {NAME_MIDDLE}, {NAME_MIDDLE_INITIAL}};
