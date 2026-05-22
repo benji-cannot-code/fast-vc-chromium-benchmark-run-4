2e223818637ec52dc0ca34847fa3b45b6a29ab86
Joshua Hood <jdh@chromium.org>
2026-05-22 07:44:53 -0700

    [SafeBrowsing][iOS] Fix WebUI diagnostics events and macro
    This CL fixes the Safe Browsing diagnostics WebUI page on iOS by:
    1. Registering the observer handler instance with the global event
       singleton on creation (enabling event updates to show up).
    2. Adding SetDefaultResource for the datasource resource mapping.
    3. Fixing a dormant preprocessor bug in the CRSBLOG macro by
       importing web_ui_ios_info_singleton.h and resolving HasListener()
       on the singleton instance.
    4. Adding a unit test verifying CRSBLOG's lazy evaluation, aligning
       iOS test coverage with the desktop version and closing a testing gap.
    
    Bug: 40704516
    Change-Id: Ic6264701a63d204f663a5ddf8a6a1c964241f79d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869284
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Commit-Queue: jdh <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634943}

--- components/safe_browsing/ios/browser/web_ui/safe_browsing_ui.h
+++ components/safe_browsing/ios/browser/web_ui/safe_browsing_ui.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <sstream>
 
 #import "components/safe_browsing/core/browser/web_ui/cr_safe_browsing_log.h"
+#import "components/safe_browsing/ios/browser/web_ui/web_ui_ios_info_singleton.h"
 #import "ios/web/public/webui/web_ui_ios_controller.h"
 
 namespace safe_browsing {
@@ -29,10 +30,10 @@ class CrSBIOSLogMessage : public CrSBLogMessage {
   ~CrSBIOSLogMessage() override;
 };
 
-#define CRSBLOG                                            \
-  (!::safe_browsing::WebUIIOSInfoSingleton::HasListener()) \
-      ? static_cast<void>(0)                               \
-      : ::safe_browsing::CrSBLogVoidify() &                \
+#define CRSBLOG                                                           \
+  (!::safe_browsing::WebUIIOSInfoSingleton::GetInstance()->HasListener()) \
+      ? static_cast<void>(0)                                              \
+      : ::safe_browsing::CrSBLogVoidify() &                               \
             ::safe_browsing::CrSBIOSLogMessage().stream()
 
 }  // namespace safe_browsing
