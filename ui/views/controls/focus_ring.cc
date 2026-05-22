26384f771b3780db0b964f979eab8ec54af2867a
Jack Geng <jackgeng@google.com>
2026-05-22 12:17:15 -0700

    views: Temporarily schedule paint in FocusRing::Refresh()
    Currently, some focus rings (see the linked bugs) fail to repaint
    correctly when regaining focus during keyboard traversal.
    
    This workaround is needed because the old codepath (with native focus)
    is messing up the repainting during the migration. We have validated
    that once the deprecated painting logic is completely removed across the
    codebase, the new FocusRing::Refresh() works exactly as expected.
    However, while we are in this migration where both systems coexist, this
    explicit SchedulePaint() call guarantees that focus rings redraw
    reliably.
    
    This workaround will be removed once the focus ring refresh migration is
    fully complete.
    
    Bug: b:507553991, b:514929515, b:515607858
    Doc: http://go/optimize-refreshlayer
    Change-Id: I33d1c1c77e6ffcfe8e788e37a6313da3b0e23a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870334
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Jack Geng <jackgeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635131}

--- ui/views/controls/focus_ring.cc
+++ ui/views/controls/focus_ring.cc
@@ -364,6 +364,9 @@ SkPath FocusRing::GetPath() const {
 
 void FocusRing::Refresh() {
   RefreshLayer(ShouldPaint());
+  // TODO(b:507553991): Remove this once all components are migrated to
+  // FocusRing::Refresh().
+  SchedulePaint();
 }
 
 void FocusRing::RefreshLayer(bool should_paint) {
