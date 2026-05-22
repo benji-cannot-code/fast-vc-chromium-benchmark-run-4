11b6582fc3a73ee05bda742984fe69165c37760b
David Yeung <dayeung@chromium.org>
2026-05-22 14:04:48 -0700

    Update the history swipe gesture arrow icons
    Update resources to match GM3 100% rond icons. While the contents are
    the same, the images are much different from what is available in the
    GM3 icon library.
    
    PNGs are 128x128 or 256x256 (100% or 200%), transparent background, and
    white stroke color.
    
    Light:
    https://screenshot.googleplex.com/BfQKoqy6QkHdSSV
    https://screenshot.googleplex.com/9JRjenj7kBkE6pF
    
    Dark:
    https://screenshot.googleplex.com/8BuSKioktMdhQUA
    
    Bug: 515519114
    Change-Id: I3c11985368a48e15c4a04fbe94ccf8b4495f5b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871523
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635209}

--- chrome/browser/ui/cocoa/history_overlay_controller.mm
+++ chrome/browser/ui/cocoa/history_overlay_controller.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "chrome/grit/theme_resources.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/image/image.h"
 
 // Constants ///////////////////////////////////////////////////////////////////
@@ -110,10 +111,17 @@ const CGFloat kShieldHeightCompletionAdjust = 10;
 }
 
 - (void)loadView {
+  int resourceId = 0;
+  if (features::IsRoundedIconsEnabled()) {
+    resourceId = (_mode == kHistoryOverlayModeBack) ? IDR_ARROW_BACKWARD
+                                                    : IDR_ARROW_FORWARD;
+  } else {
+    resourceId =
+        (_mode == kHistoryOverlayModeBack) ? IDR_SWIPE_BACK : IDR_SWIPE_FORWARD;
+  }
+
   const gfx::Image& image =
-      ui::ResourceBundle::GetSharedInstance().GetNativeImageNamed(
-          _mode == kHistoryOverlayModeBack ? IDR_SWIPE_BACK
-                                           : IDR_SWIPE_FORWARD);
+      ui::ResourceBundle::GetSharedInstance().GetNativeImageNamed(resourceId);
   _contentView = [[HistoryOverlayView alloc] initWithMode:_mode
                                                     image:image.ToNSImage()];
   self.view = _contentView;
