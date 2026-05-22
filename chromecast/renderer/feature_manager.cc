7e74898681b9f65833471019128a9c9deab5d60e
Luke Halliwell <halliwell@google.com>
2026-05-21 19:11:52 -0700

    chromecast: Remove enable_assistant build flag
    Following team review of definitive build configs, the legacy
    `enable_assistant` GN arg is permanently obsolete. This CL removes
    the flag across GN files and deletes the exclusive proxy implementation
    `assistant_bindings.h/.cc` under chromecast/renderer/.
    
    Bug: 41489655
    Test: cast_shell_unittests
    Change-Id: Ic508d534b7e4d16fba4be320a5852dba2bd6ac2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7844239
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634674}

--- chromecast/renderer/feature_manager.cc
+++ chromecast/renderer/feature_manager.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chromecast/base/cast_features.h"
 #include "chromecast/common/feature_constants.h"
-#include "chromecast/renderer/assistant_bindings.h"
 #include "chromecast/renderer/cast_demo_bindings.h"
 #include "chromecast/renderer/cast_window_manager_bindings.h"
 #include "chromecast/renderer/settings_ui_bindings.h"
@@ -115,12 +114,6 @@ void FeatureManager::ConfigureFeaturesInternal() {
       new shell::CastWindowManagerBindings(render_frame(), this));
   if (FeatureEnabled(feature::kEnableDemoStandaloneMode)) {
     v8_bindings_.insert(new shell::CastDemoBindings(render_frame()));
-  }
-
-  if (FeatureEnabled(feature::kEnableAssistantMessagePipe)) {
-    auto& feature = GetFeature(feature::kEnableAssistantMessagePipe);
-    v8_bindings_.insert(
-        new shell::AssistantBindings(render_frame(), feature->config));
   }
 }
 
