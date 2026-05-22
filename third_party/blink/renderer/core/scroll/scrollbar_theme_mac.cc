7c096e11509dad6fe293975c6ca21332af501e1b
Steve Kobes <skobes@chromium.org>
2026-05-22 08:40:06 -0700

    Darken Mac colored non-overlay scrollbar thumb on hover
    NativeThemeBase knows how to calculate a darker version of the color
    specified in the scrollbar-color style, which is used for hover effects
    on Windows and Linux. This change brings the same behavior to Mac when
    the user has chosen non-overlay scrollbars in OS settings.
    
    Bug: 507490546
    Change-Id: I068bbd1a272601e22391a32121e37cd1b9d1203d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812894
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634976}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_mac.cc
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_mac.cc
@@ -314,8 +314,8 @@ void ScrollbarThemeMac::PaintThumb(const PaintInfo& paint_info,
           : WebThemeEngine::Part::kPartScrollbarVerticalThumb;
   mojom::blink::ColorScheme color_scheme = scrollbar.UsedColorScheme();
   WebThemeEngineHelper::GetNativeThemeEngine()->Paint(
-      context.Canvas(), thumb_part, WebThemeEngine::State::kStateNormal, bounds,
-      &params, scrollbar.InForcedColorsMode(), color_scheme,
+      context.Canvas(), thumb_part, scrollbar.GetStateForPart(kThumbPart),
+      bounds, &params, scrollbar.InForcedColorsMode(), color_scheme,
       scrollbar.GetPreferredContrast(),
       scrollbar.GetColorProvider(color_scheme));
 }
