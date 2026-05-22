77200d601acb5ee7ad74be08d10feab201279d39
Rafał Godlewski <rgod@google.com>
2026-05-22 16:12:09 -0700

    [iOS] Remove obsolete todo
    It is quite infeasible to add an EG test for credential import, as it
    requires to handle a flow in a different app first.
    
    It could possibly be faked / mocked somehow, by faking the app being
    launched with a specific user activity and then faking credential import
    manager returning specific credentials, but that's a bit of an overkill.
    
    The flow is sufficiently unit tested after the credentials are received
    
    Bug: 458733320
    Change-Id: Ie3a108da26984e64f5aa307195c368312494e9a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865630
    Reviewed-by: Sourav Uttam Sinha <sinhasourav@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635293}

--- ios/chrome/browser/credential_exchange/coordinator/credential_import_coordinator.mm
+++ ios/chrome/browser/credential_exchange/coordinator/credential_import_coordinator.mm
@@ -461,7 +461,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Starts reauthCoordinator. Once started, it observes scene state changes and
 // requires authentication when the scene is backgrounded and then foregrounded
 // while credential import is opened.
-// TODO(crbug.com/458733320): Explore EG test feasibility.
 - (void)startReauthCoordinator {
   _reauthCoordinator = [[LocalReauthenticationCoordinator alloc]
       initWithBaseNavigationController:_navigationController
