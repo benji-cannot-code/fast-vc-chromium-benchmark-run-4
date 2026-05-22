099fb697c1f6b82e46d106557f520a9d33f2d21a
Josef Raska <josefraska@google.com>
2026-05-22 06:34:59 -0700

    [HoT][Android] Make Autofill AI entries always visible when enabled
    One of the goals HoT has is visibility of what user can autofill. If
    something is not available for whatever reason we should at least show
    that the user can potentially have it.
    
    The screens will display with all UI disabled. Screenshot:
    https://screenshot.googleplex.com/6TUff9bDEbyauFr.png Aligning this with
    HoT Desktop implementation.
    
    Bug: 515285646
    Change-Id: I2556d5c6e6ff2c39fe5d5bb63306726ef5db7010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868315
    Commit-Queue: Josef Raska <josefraska@google.com>
    Reviewed-by: Stanislav Mikheyev <mikhe@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634899}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/HomeOfTransactionsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/HomeOfTransactionsFragment.java
@@ -22,8 +22,6 @@ import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.autofill.autofill_ai.EntityDataManager;
-import org.chromium.chrome.browser.autofill.autofill_ai.EntityDataManagerFactory;
 import org.chromium.chrome.browser.autofill.options.AutofillOptionsFragment;
 import org.chromium.chrome.browser.autofill.options.AutofillOptionsFragment.AutofillOptionsReferrer;
 import org.chromium.chrome.browser.device_lock.DeviceLockActivityLauncherImpl;
@@ -109,13 +107,13 @@ public class HomeOfTransactionsFragment extends ChromeBaseSettingsFragment {
                                         getActivity(), /* addToBackStack= */ true));
 
         Preference identityDocsPref = findPreference(PREF_AUTOFILL_IDENTITY_DOCS);
-        identityDocsPref.setVisible(shouldShowIdentityDocs(getProfile()));
+        identityDocsPref.setVisible(shouldShowIdentityDocs());
         identityDocsPref.setOnPreferenceClickListener(
                 preference ->
                         SettingsNavigationHelper.showAutofillIdentityDocsSettings(getActivity()));
 
         Preference travelPref = findPreference(PREF_AUTOFILL_TRAVEL);
-        travelPref.setVisible(shouldShowTravel(getProfile()));
+        travelPref.setVisible(shouldShowTravel());
         travelPref.setOnPreferenceClickListener(
                 preference -> SettingsNavigationHelper.showAutofillTravelSettings(getActivity()));
 
@@ -209,19 +207,17 @@ public class HomeOfTransactionsFragment extends ChromeBaseSettingsFragment {
         return AnimationType.PROPERTY;
     }
 
-    private static boolean shouldShowIdentityDocs(Profile profile) {
-        return shouldShowAutofillAiSettings(profile);
+    private static boolean shouldShowIdentityDocs() {
+        return shouldShowAutofillAiSettings();
     }
 
-    private static boolean shouldShowTravel(Profile profile) {
-        return shouldShowAutofillAiSettings(profile);
+    private static boolean shouldShowTravel() {
+        return shouldShowAutofillAiSettings();
     }
 
-    private static boolean shouldShowAutofillAiSettings(Profile profile) {
-        EntityDataManager entityDataManager = EntityDataManagerFactory.getForProfile(profile);
-
-        // Feature flags are checked as part of the EntityDataManager checks.
-        return entityDataManager != null && entityDataManager.canListEntityInstancesInSettings();
+    private static boolean shouldShowAutofillAiSettings() {
+        return ChromeFeatureList.isEnabled(YOUR_SAVED_INFO_SETTINGS_PAGE_ANDROID)
+                && ChromeFeatureList.isEnabled(ChromeFeatureList.AUTOFILL_AI_WITH_DATA_SCHEMA);
     }
 
     private ManagedPreferenceDelegate createManagedPreferenceDelegate() {
@@ -270,10 +266,10 @@ public class HomeOfTransactionsFragment extends ChromeBaseSettingsFragment {
                         indexData.removeEntry(getUniqueId(PREF_AUTOFILL_IDENTITY_DOCS));
                         indexData.removeEntry(getUniqueId(PREF_AUTOFILL_TRAVEL));
                     } else {
-                        if (!shouldShowIdentityDocs(profile)) {
+                        if (!shouldShowIdentityDocs()) {
                             indexData.removeEntry(getUniqueId(PREF_AUTOFILL_IDENTITY_DOCS));
                         }
-                        if (!shouldShowTravel(profile)) {
+                        if (!shouldShowTravel()) {
                             indexData.removeEntry(getUniqueId(PREF_AUTOFILL_TRAVEL));
                         }
                     }
