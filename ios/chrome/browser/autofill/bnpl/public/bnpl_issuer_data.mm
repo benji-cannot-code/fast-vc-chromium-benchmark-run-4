871a7e381ed62922db77bb95cd7f08d35a603147
James Sun <jamessun@google.com>
2026-05-22 07:57:03 -0700

    [BNPL] Define BNPL issuer data model skeleton on iOS
    This CL creates the BnplIssuerData data model class on iOS  and it lays
    out the interface skeleton for upcoming two-tab bottom sheet creation.
    
    The implementation of the function will be added shortly after.
    
    Bug: 512481934
    Change-Id: I76788f6520d12aa8d77d6c8f124fe8ab86203fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842392
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634950}

---
+++ ios/chrome/browser/autofill/bnpl/public/bnpl_issuer_data.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/autofill/bnpl/public/bnpl_issuer_data.h"
+
+@implementation BnplIssuerData
+
+- (instancetype)initWithBnplIssuer:(const autofill::BnplIssuer&)bnplIssuer
+                              icon:(UIImage*)icon {
+  if ((self = [super init])) {
+    // TODO(crbug.com/469521271): Implement full property assignments from
+    // bnplIssuer.
+    _issuerId = bnplIssuer.issuer_id();
+    _icon = icon;
+  }
+  return self;
+}
+
+@end
