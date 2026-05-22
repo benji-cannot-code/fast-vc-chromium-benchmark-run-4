361e0426217891c4824908a0b801f09f3a349417
Kent Tamura <tkent@chromium.org>
2026-05-22 01:24:41 -0700

    Rename kFieldTypeRFC822Zone to kFieldTypeRfc822Zone
    Google C++ style guide prefers "Rfc".
    
    Bug: 492927412
    Change-Id: I34e680b5141e92fa49ca1a8664a4725ea87263b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867481
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1634793}

--- third_party/blink/renderer/platform/text/date_time_format_test.cc
+++ third_party/blink/renderer/platform/text/date_time_format_test.cc
@@ -286,7 +286,7 @@ TEST_F(DateTimeFormatTest, SingleUpperCaseCharacter) {
   EXPECT_EQ(DateTimeFormat::kFieldTypeWeekOfYear, Single('w'));
   EXPECT_EQ(DateTimeFormat::kFieldTypeZoneIso8601Z, Single('X'));
   EXPECT_EQ(DateTimeFormat::kFieldTypeYearOfWeekOfYear, Single('Y'));
-  EXPECT_EQ(DateTimeFormat::kFieldTypeRFC822Zone, Single('Z'));
+  EXPECT_EQ(DateTimeFormat::kFieldTypeRfc822Zone, Single('Z'));
 }
 
 TEST_F(DateTimeFormatTest, SingleUpperCaseInvalid) {
