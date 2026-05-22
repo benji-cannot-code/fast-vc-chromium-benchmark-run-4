52645b2871e352ba1e8dd6c856bae90b157c2abf
Daniel Vogelheim <vogelheim@chromium.org>
2026-05-22 08:17:05 -0700

    [Sanitizer] Ensure StreamingSanitizer also sanitizes redundant body.
    For Document.parseHTML / Document.parseHTMLUnsafe, the parser starts
    with a near-empty document, equivalent to `<html><head/><body/>`.
    There's special logic to merge newly-parsed `<head>` and `<body>`
    elements together, to ensure the resulting document is well-formed.
    This fixup logic doesn't follow the normal element insertion, and for `StreamingSanitizer` it didn't call sanitize at all. This CL ensures
    `sanitizer_->Sanitize` is called after each such merge.
    
    Fix: 513162081
    Change-Id: I2c5102ea3f50cbb25dafeafb6f6741764bf22b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856801
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634966}

--- third_party/blink/renderer/core/html/parser/html_construction_site.cc
+++ third_party/blink/renderer/core/html/parser/html_construction_site.cc
@@ -598,6 +598,10 @@ void HTMLConstructionSite::MergeAttributesFromTokenIntoElement(
       element->setAttribute(token_attribute.GetName(), token_attribute.Value());
   }
 
+  if (sanitizer_) {
+    sanitizer_->Sanitize(element);
+  }
+
   element->HideNonce();
 }
 
