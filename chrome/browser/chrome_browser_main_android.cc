91d71be182c1f807fdfcc489e5cddd1031cab178
Troy Wang <troywang@google.com>
2026-05-22 13:32:51 -0700

    Initialize AndroidAtomsLogger on browser startup.
    Bug:b:483013446
    
    Change-Id: I83286497fa0933bafb7c056deacb6b80e4b4cbb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7790639
    Auto-Submit: Troy Wang <troywang@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Eric Lok <lokeric@google.com>
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635184}

--- chrome/browser/chrome_browser_main_android.cc
+++ chrome/browser/chrome_browser_main_android.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/current_thread.h"
 #include "base/task/thread_pool.h"
 #include "base/trace_event/trace_event.h"
+#include "chrome/browser/android/metrics/android_atoms_logger.h"
 #include "chrome/browser/android/mojo/chrome_interface_registrar_android.h"
 #include "chrome/browser/android/preferences/clipboard_android.h"
 #include "chrome/browser/android/seccomp_support_detector.h"
@@ -101,6 +102,12 @@ int ChromeBrowserMainPartsAndroid::PreEarlyInitialization() {
 
 void ChromeBrowserMainPartsAndroid::PostBrowserStart() {
   ChromeBrowserMainParts::PostBrowserStart();
+
+  // Initializes the logger that forwards allowed UMA histograms to Android
+  // Statsd (Westworld) as atoms.
+  // TODO: crbug.com/512252292 - Add link to the readme doc for more background
+  // and context after it's created.
+  chrome::android::westworld::AndroidAtomsLogger::Initialize();
 
   base::ThreadPool::PostDelayedTask(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
