7933f93903b7bdbe3737c0011285f92ca6e7bca7
Liza Burakova <liza@chromium.org>
2026-05-22 15:22:21 -0700

    Roll minijinja: 2.19.0 => 2.20.0 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * minijinja: 2.19.0 => 2.20.0; https://docs.rs/crate/minijinja/2.20.0
    
    Bug: None
    Change-Id: Idecca5f1f81969e3672601c058357e1245c430f1
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871125
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Liza Burakova <liza@chromium.org>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635253}

--- third_party/rust/chromium_crates_io/vendor/minijinja-v2/src/tests.rs
+++ third_party/rust/chromium_crates_io/vendor/minijinja-v2/src/tests.rs
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! MiniJinja comes with some built-in test functions that are listed below. To
 //! create a custom test write a function that takes at least a value argument
 //! that returns a boolean result, then register it with
-//! [`add_filter`](crate::Environment::add_test).
+//! [`add_test`](crate::Environment::add_test).
 //!
 //! # Using Tests
 //!
