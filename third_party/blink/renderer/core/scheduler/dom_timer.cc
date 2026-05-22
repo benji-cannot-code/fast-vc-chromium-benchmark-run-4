5d301a347cc3e6e82169de48dce99b7d08f693a9
Josh Karlin <jkarlin@chromium.org>
2026-05-22 13:12:35 -0700

    [AdTracker] Capture string based setTimeout targets
    When I removed async-task support for setTimeout and relied on the top
    of stack instead, I missed the fact that setTimeout could have a string
    script target as well which does not have a known ad-script id
    associated with it. So use async scanning in cases where setTimeout's
    target is a string.
    
    Change-Id: Ib1bad0e950e1a59fb1ea748253e7f62940c87984
    Bug: 515416891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868096
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635169}

--- third_party/blink/renderer/core/scheduler/dom_timer.cc
+++ third_party/blink/renderer/core/scheduler/dom_timer.cc
@@ -178,8 +178,9 @@ int DOMTimer::setTimeout(ScriptState* script_state,
   }
   auto* action = MakeGarbageCollected<ScheduledAction>(script_state, context,
                                                        handler, arguments);
-  return MakeGarbageCollected<DOMTimer>(context, action,
-                                        base::Milliseconds(timeout), true)
+  return MakeGarbageCollected<DOMTimer>(
+             context, action, base::Milliseconds(timeout), true,
+             probe::AsyncTaskContext::ScanForAds::kFalse)
       ->timeout_id_;
 }
 
@@ -215,8 +216,9 @@ int DOMTimer::setTimeout(ScriptState* script_state,
   }
   auto* action =
       MakeGarbageCollected<ScheduledAction>(script_state, context, handler);
-  return MakeGarbageCollected<DOMTimer>(context, action,
-                                        base::Milliseconds(timeout), true)
+  return MakeGarbageCollected<DOMTimer>(
+             context, action, base::Milliseconds(timeout), true,
+             probe::AsyncTaskContext::ScanForAds::kTrue)
       ->timeout_id_;
 }
 
@@ -230,8 +232,9 @@ int DOMTimer::setInterval(ScriptState* script_state,
   }
   auto* action = MakeGarbageCollected<ScheduledAction>(script_state, context,
                                                        handler, arguments);
-  return MakeGarbageCollected<DOMTimer>(context, action,
-                                        base::Milliseconds(timeout), false)
+  return MakeGarbageCollected<DOMTimer>(
+             context, action, base::Milliseconds(timeout), false,
+             probe::AsyncTaskContext::ScanForAds::kFalse)
       ->timeout_id_;
 }
 
@@ -262,8 +265,9 @@ int DOMTimer::setInterval(ScriptState* script_state,
   }
   auto* action =
       MakeGarbageCollected<ScheduledAction>(script_state, context, handler);
-  return MakeGarbageCollected<DOMTimer>(context, action,
-                                        base::Milliseconds(timeout), false)
+  return MakeGarbageCollected<DOMTimer>(
+             context, action, base::Milliseconds(timeout), false,
+             probe::AsyncTaskContext::ScanForAds::kTrue)
       ->timeout_id_;
 }
 
@@ -289,7 +293,8 @@ void DOMTimer::RemoveByID(ExecutionContext& context, int timeout_id) {
 DOMTimer::DOMTimer(ExecutionContext& context,
                    ScheduledAction* action,
                    base::TimeDelta timeout,
-                   bool single_shot)
+                   bool single_shot,
+                   probe::AsyncTaskContext::ScanForAds scan_for_ads)
     : ExecutionContextLifecycleObserver(&context),
       TimerBase(nullptr),
       timeout_id_(DOMTimerCoordinator::From(context).Install(this)),
@@ -350,7 +355,7 @@ DOMTimer::DOMTimer(ExecutionContext& context,
       "TimerInstall", inspector_timer_install_event::Data, &context,
       timeout_id_, timeout, single_shot);
   const char* name = single_shot ? "setTimeout" : "setInterval";
-  async_task_context_.Schedule(&context, name);
+  async_task_context_.Schedule(&context, name, scan_for_ads);
   probe::BreakableLocation(&context, name);
 }
 
