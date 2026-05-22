c959d74203076165a443cb9f2d8ff148ad179698
Josef Raska <josefraska@google.com>
2026-05-22 07:27:03 -0700

    [HoT][Android] Wallet promotion to respect kWalletDataSharingPromotion
    Add JNI method to use `AutofillAiAction::kWalletDataSharingPromotion` to
    check if to show the promo card as it doesn't make sense to show it:
    - If the Autofill AI is disabled by enterprise policy
    - If the address toggle is disabled
    - If the toggles for Wallet are disabled.
    
    We don't provide entity type to the permission check as any entity type
    qualifying is enough to show the promotion.
    
    Screenshot: https://screenshot.googleplex.com/7tUbsH7UrZNoPmN.png
    Low-Coverage-Reason: TRIVIAL_CHANGE for entity_data_manager_android
    Bug: 515285646
    Change-Id: I3ac904e9c8b6c2060921f93778b67d6ee24e7f56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868393
    Commit-Queue: Josef Raska <josefraska@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1634933}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/autofill_ai/EntityDataManager.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/autofill_ai/EntityDataManager.java
@@ -225,6 +225,13 @@ public class EntityDataManager implements Destroyable {
                 .canListEntityInstancesInSettings(mNativeEntityDataManagerAndroid);
     }
 
+    /** Returns whether the user can see the Wallet data sharing promotion. */
+    public boolean canShowWalletDataSharingPromotion() {
+        ThreadUtils.assertOnUiThread();
+        return EntityDataManagerJni.get()
+                .canShowWalletDataSharingPromotion(mNativeEntityDataManagerAndroid);
+    }
+
     /** Returns the opt-in status for Autofill AI. */
     public boolean getAutofillAiOptInStatus() {
         ThreadUtils.assertOnUiThread();
@@ -291,6 +298,8 @@ public class EntityDataManager implements Destroyable {
                 long nativeEntityDataManagerAndroid, @EntityTypeName int entityType);
 
         boolean canListEntityInstancesInSettings(long nativeEntityDataManagerAndroid);
+
+        boolean canShowWalletDataSharingPromotion(long nativeEntityDataManagerAndroid);
 
         boolean getAutofillAiOptInStatus(long nativeEntityDataManagerAndroid);
 
