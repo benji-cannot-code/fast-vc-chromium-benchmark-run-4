d2408cc63fba9e027b58399d359cbc9c8e1a97c9
Eitan Goldberger <eitang@google.com>
2026-05-22 11:48:54 -0700

    Introduce a custom render context menu type for Reading Mode.
    Restricts the context menu to only include options supported by Reading
    Mode.
    
    We use an allow-list to include specific features as opposed to a
    block-list, because then we'd need to block every new feature that gets
    introduced, which we're not necessarily aware of.
    
    Bug: 510857479
    Change-Id: I5bb619c1f655ffea50645151ad6947d856b0ab1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848158
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Auto-Submit: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635112}

--- chrome/browser/renderer_context_menu/context_menu_content_type_factory.cc
+++ chrome/browser/renderer_context_menu/context_menu_content_type_factory.cc
@@ -8,9 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "build/build_config.h"
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/renderer_context_menu/context_menu_content_type_read_anything.h"
+#endif
+#include "chrome/common/webui_url_constants.h"
 #include "components/renderer_context_menu/context_menu_content_type.h"
 #include "content/public/browser/context_menu_params.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/url_constants.h"
 #include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -76,6 +82,14 @@ std::unique_ptr<ContextMenuContentType>
 ContextMenuContentTypeFactory::CreateInternal(
     content::RenderFrameHost* render_frame_host,
     const content::ContextMenuParams& params) {
+#if !BUILDFLAG(IS_ANDROID)
+  if (params.page_url.SchemeIs(content::kChromeUIUntrustedScheme) &&
+      params.page_url.host() ==
+          chrome::kChromeUIUntrustedReadAnythingSidePanelHost) {
+    return std::make_unique<ContextMenuContentTypeReadAnything>(params);
+  }
+#endif
+
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   if (IsRunningInForcedAppMode()) {
     return base::WrapUnique(new ContextMenuContentTypeAppMode(params));
