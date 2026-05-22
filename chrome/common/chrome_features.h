91a9b8f694a38e0900853d695dd199bdbc554860
rosiey <rosiey@google.com>
2026-05-22 09:23:42 -0700

    [glic]: Gate experimental task notification suppression behind new flag
    This introduces the kGlicExperimentalTriggeringSuppressDoneNotification
    feature flag to control the suppression of task completion/failure
    notifications for experimental triggering tasks.
    
    Gating this logic behind a dedicated, default-on flag allows the privacy
    teams to remotely toggle the suppression behavior at any time via Finch.
    
    Bug: b:513585661
    Change-Id: I6ce15007ec1874102fb035d97496eba16f3bf792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865341
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635012}

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -160,6 +160,8 @@ extern const base::FeatureParam<double> kGlicActorApcComparisonSamplingRate;
 COMPONENT_EXPORT(CHROME_FEATURES)
 BASE_DECLARE_FEATURE(kGlicExperimentalTriggering);
 COMPONENT_EXPORT(CHROME_FEATURES)
+BASE_DECLARE_FEATURE(kGlicExperimentalTriggeringSuppressDoneNotification);
+COMPONENT_EXPORT(CHROME_FEATURES)
 BASE_DECLARE_FEATURE(kGlicExperimentalTriggeringOptInBypass);
 COMPONENT_EXPORT(CHROME_FEATURES)
 BASE_DECLARE_FEATURE(kGlicExperimentalTriggeringOpenWindowIfNone);
