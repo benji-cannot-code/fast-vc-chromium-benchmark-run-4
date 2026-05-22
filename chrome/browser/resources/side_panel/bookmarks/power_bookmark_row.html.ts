81d967ad373357dcf73abd423054d47e4c89ff4a
rbpotter <rbpotter@chromium.org>
2026-05-22 15:05:40 -0700

    [Side Panel Bookmarks] Bind draggable attribute as string
    The draggable attribute is a string ("true"|"false"|"auto") so should
    not use a boolean binding style or be directly bound to a boolean
    property.
    
    crrev.com/c/7705076 switched this from a property binding because
    draggable is not declared as a reactive property, but incorrectly used
    a boolean attribute binding instead of a standard string attribute
    binding.
    
    Fixed: 515674767
    Change-Id: Ie8593b280ae5dbbb9794852cf4221443080f6e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870118
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635239}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.html.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.html.ts
@@ -44,7 +44,7 @@ ${this.shouldExpand_() ? html`
           .renamingId="${this.renamingId}"
           .imageUrls="${this.imageUrls}"
           .shoppingCollectionFolderId="${this.shoppingCollectionFolderId}"
-          ?draggable="${this.canDrag}"
+          draggable="${String(this.canDrag)}"
           ?can-drag="${this.canDrag}"
           ?has-active-drag="${this.hasActiveDrag}"
           .activeFolderPath="${this.activeFolderPath}"
