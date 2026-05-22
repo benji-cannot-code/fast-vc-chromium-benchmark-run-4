fc040cf8e6fe861a049d216487317267a6ab66ee
Anunoy Ghosh <anunoy@chromium.org>
2026-05-22 10:42:31 -0700

    Include Extension Allowlist unit tests in desktop_android build
    This CL moves extension_allowlist_unittest.cc out of the desktop-only
    enable_extensions block and into the core unit_tests sources list. This
    allows the ExtensionAllowlist tests to be compiled and run on the
    desktop_android platform, which compiles the core extensions system
    (enable_extensions_core = true).
    
    BUG=469417243
    
    TAG=agy CONV=ad139268-e291-4b73-b00b-4c6f7096396a
    
    Change-Id: Ie1a278a440ef9cbb07191c6187004bff3bda0a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868141
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Anunoy Ghosh <anunoy@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635072}

--- chrome/browser/extensions/extension_allowlist_unittest.cc
+++ chrome/browser/extensions/extension_allowlist_unittest.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_id.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
 namespace extensions {
 
 namespace {
