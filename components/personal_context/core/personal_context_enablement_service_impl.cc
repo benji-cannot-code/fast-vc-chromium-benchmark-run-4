5ee212611562c7d258998149d1bc7bd19e044eb4
Aisulu R <aisulu@google.com>
2026-05-22 11:16:18 -0700

    [a11y] Align Notice/OptIn constants with new terminology & existing code
    This CL renames following enum values and a pref:
    1) kDisabledPendingInfo -> kDisabledShouldShowNotice
    2) kDisabledPendingSetup -> kDisabledNeedsOptIn
    3) kShouldShowPersonalContextFirstRunInfo ->
    kPersonalContextInAutofillNoticeShouldBeShown
    
    Context:
    - PS#1: Initial simple renames were requested during the migration from
      accessibility_annotator (b:511173039).
    - PS#2: Further semantic refinement was requested to align with existing
      patterns in the codebase (b:514971689).
    
    Bug: b:510321522, b:511173039, b:514971689
    Change-Id: I02524af3c631b0f5acb245c42e57d6d0ee9e9dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865906
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635092}

--- components/personal_context/core/personal_context_enablement_service_impl.cc
+++ components/personal_context/core/personal_context_enablement_service_impl.cc
@@ -179,9 +179,10 @@ const base::flat_set<int32_t>& GetPersonalContextEligibleTiers() {
     return kDisabledNotEligible;
   }
 
-  if (pref_service->GetBoolean(prefs::kShouldShowPersonalContextFirstRunInfo)) {
-    MaybeOutputReason(debug_message, "Info not yet acknowledged.");
-    return kDisabledPendingInfo;
+  if (pref_service->GetBoolean(
+          prefs::kPersonalContextInAutofillNoticeShouldBeShown)) {
+    MaybeOutputReason(debug_message, "Notice not yet acknowledged.");
+    return kDisabledShouldShowNotice;
   }
 
   return kEnabled;
@@ -213,7 +214,7 @@ PersonalContextEnablementServiceImpl::PersonalContextEnablementServiceImpl(
   if (pref_service_) {
     pref_registrar_.Init(pref_service_);
     pref_registrar_.Add(
-        prefs::kShouldShowPersonalContextFirstRunInfo,
+        prefs::kPersonalContextInAutofillNoticeShouldBeShown,
         base::BindRepeating(
             &PersonalContextEnablementServiceImpl::UpdateEnablementState,
             base::Unretained(this)));
@@ -268,7 +269,7 @@ PersonalContextEnablementServiceImpl::ComputeEnablementState() {
 
   if (!SatisfiesOptInRequirements(account_settings_service_.get())) {
     return personal_context::features::IsPersonalContextFirstRunOptInEnabled()
-               ? kDisabledPendingSetup
+               ? kDisabledNeedsOptIn
                : kDisabledNotEligible;
   }
 
@@ -290,7 +291,8 @@ void PersonalContextEnablementServiceImpl::OnPrimaryAccountChanged(
   if (event_details.GetEventTypeFor(signin::ConsentLevel::kSignin) ==
       signin::PrimaryAccountChangeEvent::Type::kCleared) {
     if (pref_service_) {
-      pref_service_->ClearPref(prefs::kShouldShowPersonalContextFirstRunInfo);
+      pref_service_->ClearPref(
+          prefs::kPersonalContextInAutofillNoticeShouldBeShown);
     }
   }
   UpdateEnablementState();
--- components/personal_context/core/personal_context_prefs.cc
+++ components/personal_context/core/personal_context_prefs.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace personal_context::prefs {
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry) {
-  registry->RegisterBooleanPref(kShouldShowPersonalContextFirstRunInfo, true);
+  registry->RegisterBooleanPref(kPersonalContextInAutofillNoticeShouldBeShown,
+                                true);
 }
 
 }  // namespace personal_context::prefs
