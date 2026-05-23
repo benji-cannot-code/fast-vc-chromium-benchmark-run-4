2fee04609ec3567ccc1be6064f89b0caf5b0b8e1
Matt Reynolds <mattreynolds@chromium.org>
2026-05-22 17:21:58 -0700

    Revert "Reland "Run tasks posted before shutdown before shutdown process start.""
    This reverts commit 3f244ea094903b2fdf4e7534d8eeda8ada5bcde3.
    
    Reason for revert: [gardener] suspected for failures in:
    HomeButtonWithQuickAppAccess.AppWithNoIconThenLoaded
    HomeButtonWithQuickAppAccess.IconUpdatesOnNewQuickAppSet
    
    Original change's description:
    > Reland "Run tasks posted before shutdown before shutdown process start."
    >
    > This reverts commit 7e0390c5734cd9f5fdd7d26152195ebff04741e8.
    >
    > Reason for revert: fixed tests.
    >
    > Original change's description:
    > > Revert "Run tasks posted before shutdown before shutdown process start."
    > >
    > > This reverts commit 01c70173d55db257a42b9caaa226c7c3c9c5a4c1.
    > >
    > > Reason for revert: This CL likely revealed preexisting bugs in:
    > >
    > > * HomeButtonWithQuickAppAccess.AppWithNoIconThenLoaded
    > > * HomeButtonWithQuickAppAccess.IconUpdatesOnNewQuickAppSet
    > > * ImmersiveFullscreenControllerTest.Focus
    > >
    > > and caused them to fail. To de-risk and de-noise, it's better to revert.
    > >
    > > Original change's description:
    > > > Run tasks posted before shutdown before shutdown process start.
    > > >
    > > > This can catch errors that can be missed if we try to execute tasks
    > > > after shutdown starts.
    > > >
    > > > This actually found one minor issue in
    > > > ExtendedDesktopTest.KeyEventsOnLockScreen (missing accessibility name),
    > > > which is fixed.
    > > >
    > > > Bug: None
    > > > Change-Id: I4986974c4f68a09a1dc07fe862e39016888ea4e8
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7864910
    > > > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > > > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1633917}
    > >
    > > Bug: 515293773,515346791
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: Iee96f8f59f4d0e7dbe2bd5d613c7a8ca5ab96b69
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869448
    > > Owners-Override: Samuel Huang <huangs@chromium.org>
    > > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > > Auto-Submit: Samuel Huang <huangs@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1634306}
    >
    > Bug: 515293773,515346791
    > Change-Id: I70f54033aead305d924eb24870619e282ba52ca0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869489
    > Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1635046}
    
    Bug: 515293773,515346791
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I23fddba6f94a4411a6345c0b53f1ed3d3fe1a132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872468
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Owners-Override: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635322}

--- ash/app_list/model/app_list_test_model.cc
+++ ash/app_list/model/app_list_test_model.cc
@@ -81,9 +81,7 @@ AppListTestModel::AppListTestModel()
 AppListTestModel::~AppListTestModel() = default;
 
 AppListItem* AppListTestModel::AddItem(AppListItem* item) {
-  AppListModel::AddItem(base::WrapUnique(item));
-  SetItemName(item, item->name());
-  return item;
+  return AppListModel::AddItem(base::WrapUnique(item));
 }
 
 void AppListTestModel::RequestPositionUpdate(
