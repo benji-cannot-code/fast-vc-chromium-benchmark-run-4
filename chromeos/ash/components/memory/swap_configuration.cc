54d90482770aed0774d3c1806bd3300e124c53b2
Joe Mason <joenotcharles@google.com>
2026-05-22 20:31:56 -0700

    Migrate SysInfo::AmountOfPhysicalMemory callers to AmountOfTotalPhysicalMemory in chromeos
    Convert callers of base::SysInfo::AmountOfPhysicalMemory to use
    AmountOfTotalPhysicalMemory.
    
    This CL was uploaded by git cl split.
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    R=achuith@chromium.org
    
    TAG=agy
    CONV=0f63b45d-cf24-4852-82b7-233c5d9bb873
    
    Bug: 448661443
    Change-Id: I2dc5d78a9c66da646abc56d11c433f500c31c5fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872199
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635365}

--- chromeos/ash/components/memory/swap_configuration.cc
+++ chromeos/ash/components/memory/swap_configuration.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/memory/swap_configuration.h"
 
+#include "base/byte_size.h"
 #include "base/component_export.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -66,9 +67,9 @@ void ConfigureResourcedPressureThreshold(bool arc_enabled) {
   const int kRatioToBps = 10000;
   // limit_bps = (limit_mb / total_mb) * ratio_to_bps, rearrange the
   // multiplication to avoid floating point arithmetic.
-  int protected_threshold_limit_bps =
-      static_cast<uint32_t>(kProtectedThresholdLimitMb * kRatioToBps /
-                            base::SysInfo::AmountOfPhysicalMemory().InMiB());
+  int protected_threshold_limit_bps = static_cast<uint32_t>(
+      kProtectedThresholdLimitMb * kRatioToBps /
+      base::SysInfo::AmountOfTotalPhysicalMemory().InMiB());
   if (arc_enabled) {
     experiment_enabled =
         base::FeatureList::IsEnabled(kCrOSMemoryPressureSignalStudyArc);
