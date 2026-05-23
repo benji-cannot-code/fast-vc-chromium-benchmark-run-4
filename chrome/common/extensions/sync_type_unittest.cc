ae5563164746a07cee7bc570622f519d693ee55d
James Cook <jamescook@chromium.org>
2026-05-22 20:10:33 -0700

    extensions: Port ExtensionSyncTypeTest to desktop Android
    It passes without modifications.
    
    Bug: 469417243
    Change-Id: I2b96ab63fd5320e3d29f982e05509f0f4b36e6a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871687
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635358}

--- chrome/common/extensions/sync_type_unittest.cc
+++ chrome/common/extensions/sync_type_unittest.cc
@@ -9,12 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/common/extensions/sync_helper.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/app_display_info.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
