764794a7a38d2c4f482779c1c814677e1b94cc3d
James Cook <jamescook@chromium.org>
2026-05-22 17:25:59 -0700

    extensions: Port BackgroundHeaderTest to desktop Android
    It passes without modifications.
    
    Bug: 469417243
    Change-Id: I506f67d3f81ba826db673483db5faa73f3c3eaa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871980
    Commit-Queue: James Cook <jamescook@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635324}

--- chrome/browser/extensions/background_header_browsertest.cc
+++ chrome/browser/extensions/background_header_browsertest.cc
@@ -12,10 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/process_manager.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/test_extension_dir.h"
 #include "net/dns/mock_host_resolver.h"
 #include "url/gurl.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
