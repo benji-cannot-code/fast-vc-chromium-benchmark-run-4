99458e580fe10e12e116fdee72df13e287b91740
Etienne Pierre-doray <etiennep@chromium.org>
2026-05-24 06:27:52 -0700

    [tracing] Migrate async track events to more appropriate alternatives in media/cast
    This CL changes async events on custom tracks (TRACE_EVENT_BEGIN/_END)
    to more appropriate alternatives: either on-thread events connected with
    flows, on synchronous events. In this case, the _END event was actually
    missing outside the if condition. This CL fixed the missing _END.
    
    There's a cost to using async events:
    - async events take a lot of vertical space in tracing UI
    - async events looses context of where the start and end happen
    - custom track are tricky to use, and  Track::FromPointer is problematic because it can alias when pointers are reused.
    https://chromium.googlesource.com/chromium/src.git/+/HEAD/docs/trace_events.md#asynchronous-events
    
    As a general rule of thumb, synchronous events are more appropriate when
    a slice is well nested in a function.
    
    This CL was uploaded by git cl split.
    
    Bug: 432427382
    Change-Id: Ia42f299c096461049db5f306c638511b71a6b59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869856
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635519}

--- media/cast/encoding/audio_encoder.cc
+++ media/cast/encoding/audio_encoder.cc
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/cast/constants.h"
 #include "third_party/openscreen/src/cast/streaming/public/encoded_frame.h"
 #include "third_party/opus/src/include/opus.h"
-#include "third_party/perfetto/include/perfetto/tracing/track.h"
+#include "third_party/perfetto/include/perfetto/tracing/track_event_args.h"
 
 #if BUILDFLAG(IS_APPLE)
 #include <AudioToolbox/AudioToolbox.h>
@@ -175,7 +175,6 @@ class AudioEncoder::ImplBase
       audio_frame->capture_end_time = frame_capture_time_;
 
       TRACE_EVENT_BEGIN("cast.stream", "Audio Encode",
-                        perfetto::Track::FromPointer(audio_frame.get()),
                         "frame_id", frame_id_.lower_32_bits(), "rtp_timestamp",
                         frame_rtp_timestamp_.lower_32_bits());
 
@@ -185,9 +184,8 @@ class AudioEncoder::ImplBase
         // by the signal duration.
         audio_frame->encoder_utilization =
             (base::TimeTicks::Now() - start_time) / frame_duration_;
-        TRACE_EVENT_END(
-            "cast.stream", perfetto::Track::FromPointer(audio_frame.get()),
-            "encoder_utilization", audio_frame->encoder_utilization);
+        TRACE_EVENT_END("cast.stream", "encoder_utilization",
+                        audio_frame->encoder_utilization);
 
         audio_frame->encode_completion_time = cast_environment_->NowTicks();
         cast_environment_->PostTask(
@@ -195,6 +193,8 @@ class AudioEncoder::ImplBase
             base::BindOnce(callback_, std::move(audio_frame),
                            samples_dropped_from_buffer_));
         samples_dropped_from_buffer_ = 0;
+      } else {
+        TRACE_EVENT_END("cast.stream");
       }
 
       // Reset the internal buffer, frame ID, and timestamps for the next frame.
