ead44c1dcdf6ea14a63aaa27a42bf7e31b768e84
Chirag Arora <heychirag@google.com>
2026-05-25 03:36:31 -0700

    observe changes to kMetricsReportingLevel to Enable/Disable UKM and DWA
    This CL adds support for observing changes to kMetricsReportingLevel to
    Enable/Disable UKM and DWA Service in ChromeMetricsServiceClient. It
    also adds unit tests to verify the behavior.
    
    Bug: b:496476396
    Change-Id: I726d1fd178e2256c075c8b4cff006d8722eb35dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837561
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635663}

--- components/metrics_services_manager/metrics_services_manager.h
+++ components/metrics_services_manager/metrics_services_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_list.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/field_trial.h"
 #include "base/threading/thread_checker.h"
@@ -177,12 +178,19 @@ class MetricsServicesManager {
   // Called when loading state changed.
   void LoadingStateChanged(bool is_loading);
 
+  // Called when the metrics reporting level has changed.
+  void OnMetricsReportingLevelChanged();
+
   // Used by |GetOnRendererUnresponsiveCb| to construct the callback that will
   // be run by |MetricsServicesWebContentsObserver|.
   void OnRendererUnresponsive();
 
   // The client passed in from the embedder.
   const std::unique_ptr<MetricsServicesManagerClient> client_;
+
+  // Subscription for receiving callbacks when the metrics reporting level
+  // changes.
+  base::CallbackListSubscription metrics_reporting_choice_service_subscription_;
 
   // Ensures that all functions are called from the same thread.
   base::ThreadChecker thread_checker_;
