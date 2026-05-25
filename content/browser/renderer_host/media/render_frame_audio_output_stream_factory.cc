12a782c99786731c8f75f356a846bc3828f779d9
Guido Urdaneta <guidou@chromium.org>
2026-05-25 09:14:59 -0700

    [Audio] Fix race in AudioOutputAuthorizationHandler
    Ensure access to the `hashed_device_id_for_global_media_controls_` field
    always occurs on the IO thread.
    
    Fixed: 497632232
    Change-Id: I7720576858dc857299a5e603da4ab267587a3f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865624
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635745}

--- content/browser/renderer_host/media/render_frame_audio_output_stream_factory.cc
+++ content/browser/renderer_host/media/render_frame_audio_output_stream_factory.cc
@@ -216,8 +216,17 @@ RenderFrameAudioOutputStreamFactory::~RenderFrameAudioOutputStreamFactory() {
 
 void RenderFrameAudioOutputStreamFactory::
     SetAuthorizedDeviceIdForGlobalMediaControls(std::string hashed_device_id) {
-  core_->SetAuthorizedDeviceIdForGlobalMediaControls(
-      std::move(hashed_device_id));
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  // base::Unretained(core_.get()) is safe here because |core_| is owned by
+  // RenderFrameAudioOutputStreamFactory and the
+  // RenderFrameAudioOutputStreamFactory destructor posts a task to delete
+  // |core_| on the IO thread. Since both tasks are posted to IO thread, the
+  // task posted here will always execute before the destruction task.
+  GetIOThreadTaskRunner({})->PostTask(
+      FROM_HERE,
+      base::BindOnce(&Core::SetAuthorizedDeviceIdForGlobalMediaControls,
+                     base::Unretained(core_.get()),
+                     std::move(hashed_device_id)));
 }
 
 size_t
@@ -266,6 +275,7 @@ void RenderFrameAudioOutputStreamFactory::Core::Init(
 
 void RenderFrameAudioOutputStreamFactory::Core::
     SetAuthorizedDeviceIdForGlobalMediaControls(std::string hashed_device_id) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
   authorization_handler_.SetAuthorizedDeviceIdForGlobalMediaControls(
       std::move(hashed_device_id));
 }
