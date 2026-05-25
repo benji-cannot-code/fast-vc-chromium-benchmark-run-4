6a190b88f785447ef86ae1d016a6e1f714b28fd1
Minseong Kim <jja08111@gmail.com>
2026-05-25 00:12:26 -0700

    [ruby-overhang] Support overhang for full-width open/close/middle dot
    This CL implements support for ruby overhang of full-wdth opening,
    closing, and middle dot punctuation. Previously, `ruby-overhang: spaces`
    only considered adjacent spaces.
    
    The implementation introduces logic to determine if Han kerning can be
    used for ruby overhang. It calculates the available overhang amount
    based on the character type, typically the half for opening/closing
    puncuation and quarter for middle dots. And this also supports font
    features like 'halt' and 'vhal'.
    
    The `subset.sh` script is updated to expand the character range for WPT.
    - FE00-FE0F: For variation selectors.
    - FF3B, FF3D: Full-width left and right square brackets.
    
    Spec: https://drafts.csswg.org/css-ruby-1/#ruby-overhang
    Bug: 499042741, 366873207
    Change-Id: I38c8a8f03b3f5eacfea36e305b1fd56758fe8a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7809475
    Commit-Queue: Minseong Kim <jja08111@gmail.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635620}

--- third_party/blink/renderer/platform/text/character.h
+++ third_party/blink/renderer/platform/text/character.h
@@ -142,6 +142,10 @@ class PLATFORM_EXPORT Character {
   }
   static bool MayNeedEastAsianSpacing(UChar32);
 
+  static bool MaybeHanKerningMiddle(UChar32 ch) {
+    return MaybeHanKerningMiddleSlow(ch);
+  }
+
   // Collapsible white space characters defined in CSS:
   // https://drafts.csswg.org/css-text-3/#collapsible-white-space
   static bool IsCollapsibleSpace(UChar c) {
@@ -278,6 +282,7 @@ class PLATFORM_EXPORT Character {
   static bool IsHangulSlow(UChar32);
   static bool MaybeHanKerningOpenSlow(UChar32);
   static bool MaybeHanKerningCloseSlow(UChar32);
+  static bool MaybeHanKerningMiddleSlow(UChar32);
   static void ApplyPatternAndFreezeIfEmpty(icu::UnicodeSet* unicodeSet,
                                            const char* pattern);
 };
