0fa5550096c706ebd6438d88c18aade520d0946f
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-05-25 02:35:04 -0700

    [chrome://policy] Port MachineLevelUserCloudStatusProvider to Mojo
    Implement GetStatusMojo in MachineLevelUserCloudPolicyStatusProvider and
    its base class, PolicyStatusProvider, to support migrating the
    chrome://policy WebUI from the legacy base::DictValue approach to Mojo.
    
    This change introduces:
    * PopulateStatusFromCore and supporting helper methods in
      PolicyStatusProvider to map policy data to policy::mojom::StatusPtr
      objects.
    * An implementation of GetStatusMojo in
      MachineLevelUserCloudPolicyStatusProvider to process machine-level
      cloud policy status.
    * Logic to populate fields including enrollment tokens, device IDs,
      refresh intervals, and the last report timestamps into the Mojo
      structures.
    
    Bug: 40897784
    Change-Id: Ide8293b5bf1ba0030fc07935c4eca299db1e2d55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848478
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635649}

--- components/policy/core/browser/webui/machine_level_user_cloud_policy_status_provider.h
+++ components/policy/core/browser/webui/machine_level_user_cloud_policy_status_provider.h
@@ -46,6 +46,7 @@ class POLICY_EXPORT MachineLevelUserCloudPolicyStatusProvider
 
   // PolicyStatusProvider implementation.
   base::DictValue GetStatus() override;
+  policy::mojom::StatusPtr GetStatusMojo() override;
 
   // CloudPolicyStore::Observer implementation.
   void OnStoreLoaded(CloudPolicyStore* store) override;
