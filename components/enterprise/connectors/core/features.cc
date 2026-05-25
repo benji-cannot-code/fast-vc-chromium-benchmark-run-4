8ab2f70540c2bbcad3062e1320903eceb5bd4db2
Dominique Fauteux-Chapleau <domfc@chromium.org>
2026-05-25 06:57:21 -0700

    Add kEnableAuditOnlyNetworkRequestConnector feature
    Bug: 493906224
    Change-Id: I5586dabe7cce3ff78099228a4aafc4a2dc53478e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871140
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Lina Ismail <ismaill@google.com>
    Commit-Queue: Lina Ismail <ismaill@google.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635701}

--- components/enterprise/connectors/core/features.cc
+++ components/enterprise/connectors/core/features.cc
@@ -52,4 +52,7 @@ BASE_FEATURE(kEnableDownloadEnterpriseScanOnClank,
 BASE_FEATURE(kEnableCancelUploadOnContentAnalysis,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableAuditOnlyNetworkRequestConnector,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace enterprise_connectors
