b7f63749e3e03d5f8d718a8474d21083fc23ce5c
Alexis Hétu <sugoi@chromium.org>
2026-05-25 14:28:05 -0700

    Disable settings accessibility test on iPhone device on iOS < 26
    This test is flaky when running on a physical iPhone device under
    iOS versions below 26. This CL disables it on this configuration.
    
    Bug: 455701421
    Change-Id: I1eb1ecfa2342e894ce7860dbfbe80ab51b2f88ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872570
    Auto-Submit: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Owners-Override: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635834}

--- ios/chrome/browser/settings/ui_bundled/settings_accessibility_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_accessibility_egtest.mm
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2019 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#import <TargetConditionals.h>
 
 #import "base/ios/ios_util.h"
 #import "components/strings/grit/components_strings.h"
@@ -216,6 +217,14 @@ id<GREYMatcher> BandwidthSettingsButton() {
 // Verifies the UI elements are accessible on the Bandwidth Preload Webpages
 // Settings page.
 - (void)testAccessibilityOnBandwidthPreloadWebpagesSettingsPage {
+#if !TARGET_OS_SIMULATOR
+  // TODO(crbug.com/455701421): Re-enable this flaky test on iPhone device on
+  // iOS below 26.
+  if (![ChromeEarlGrey isIPadIdiom] && !base::ios::IsRunningOnIOS26OrLater()) {
+    EARL_GREY_TEST_DISABLED(@"Flaky on iPhone device on iOS below 26.");
+  }
+#endif
+
   [ChromeEarlGreyUI openSettingsMenu];
   [ChromeEarlGreyUI tapSettingsMenuButton:BandwidthSettingsButton()];
   [[EarlGrey selectElementWithMatcher:BandwidthPreloadWebpagesButton()]
