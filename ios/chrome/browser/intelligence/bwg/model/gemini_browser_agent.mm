5d3ef8bc3bd63b6bd554fe790b2101dc3f124e6e
Tarek Makkouk <tarekmakkouk@google.com>
2026-05-25 12:53:27 -0700

    [iOS] Add Gemini Live IPH string
    Bug: 514692680
    Change-Id: I4e5259cd954ae566171ae94501e8cdba52d9b29e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872567
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635811}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -1325,6 +1325,7 @@ GeminiConfiguration* GeminiBrowserAgent::CreateGeminiConfiguration(
     config.shouldShowGeminiLiveIPH = NO;
     config.shouldShowGeminiLiveNewBadge = NO;
   }
+  config.geminiLiveIPHText = l10n_util::GetNSString(IDS_IOS_GEMINI_LIVE_IPH);
 
   config.geminiLocationPermissionState =
       ios::provider::GeminiLocationPermissionState::kUnknown;
