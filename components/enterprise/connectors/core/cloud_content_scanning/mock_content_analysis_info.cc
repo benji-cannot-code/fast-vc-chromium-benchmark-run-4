b35b2261dc593f8eb4b25efbfbefa6483c5b32c3
Nancy Xiao <nancylanxiao@google.com>
2026-05-25 09:09:47 -0700

    Set is_mobile to true on android and ios platforms
    This change also move MockContentAnalysisInfoBase to a stand-alone file
    so that it could be reused in deep_scanning_utils_unittest.
    
    Bug: b:513251019
    Change-Id: I888f33a99b88d23070a2446174744dd7b85d0ec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869045
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635742}

---
+++ components/enterprise/connectors/core/cloud_content_scanning/mock_content_analysis_info.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/connectors/core/cloud_content_scanning/mock_content_analysis_info.h"
+
+namespace enterprise_connectors {
+
+MockContentAnalysisInfoBase::MockContentAnalysisInfoBase() = default;
+MockContentAnalysisInfoBase::~MockContentAnalysisInfoBase() = default;
+
+}  // namespace enterprise_connectors
