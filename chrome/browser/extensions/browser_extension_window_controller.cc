28a0748f76bc4eb9c878ce534a1b4c0a13ad1025
Kun Wang <kunwang@microsoft.com>
2026-05-25 11:44:48 -0700

    [bedrock] Move Miscellaneous Commands & Action Helpers logic into feature controller
    Migrate Browser::CanSaveContents(), and
    Browser::ToggleFullscreenModeWithExtension() off the Browser class,
    calling the underlying APIs directly at each call site instead.
    
    CanSaveContents(): Replaced with chrome::CanSavePage() at the call site
    in RenderViewContextMenu. ToggleFullscreenModeWithExtension(): Callers
    now go through GetExclusiveAccessManager()->fullscreen_controller()
    directly.
    
    Bug: 496674143
    Change-Id: Iab72f7f6591e764858abb792f41dc84eec2d712e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866722
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635798}

--- chrome/browser/extensions/browser_extension_window_controller.cc
+++ chrome/browser/extensions/browser_extension_window_controller.cc
@@ -39,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"  // nogncheck
+#include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"  // nogncheck
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #endif
@@ -138,7 +140,9 @@ void BrowserExtensionWindowController::SetFullscreenMode(
   NOTIMPLEMENTED();
 #else
   if (window()->IsFullscreen() != is_fullscreen) {
-    GetBrowser()->ToggleFullscreenModeWithExtension(extension_url);
+    browser_->GetExclusiveAccessManager()
+        ->fullscreen_controller()
+        ->ToggleBrowserFullscreenModeWithExtension(extension_url);
   }
 #endif
 }
