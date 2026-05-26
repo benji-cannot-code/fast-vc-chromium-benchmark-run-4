de55fb5d39d9fa9c80da29efbe5448adfdce6ea2
Joe Mason <joenotcharles@google.com>
2026-05-26 12:01:18 -0700

    Migrate SysInfo::AmountOfPhysicalMemory callers to AmountOfTotalPhysicalMemory in ui
    Convert callers of base::SysInfo::AmountOfPhysicalMemory to use
    AmountOfTotalPhysicalMemory. In cases where the caller expects a
    ByteCount, convert the result using AsDeprecatedByteCount() to minimize
    unrelated changes.
    
    This CL was uploaded by git cl split.
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    R=zmo@chromium.org
    
    TAG=agy
    CONV=0f63b45d-cf24-4852-82b7-233c5d9bb873
    
    Bug: 448661443
    Change-Id: Ic74e3abe3ee95cedc24d3749b341597c39c35d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872200
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636364}

--- ui/gl/gl_features.cc
+++ ui/gl/gl_features.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/gl_features.h"
 
+#include "base/byte_size.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/strings/string_split.h"
@@ -375,7 +376,7 @@ BASE_FEATURE(kAndroidLimitRgb565DisplayToApi32,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool PreferRGB565ResourcesForDisplay() {
-  return base::SysInfo::AmountOfPhysicalMemory().InMiB() <= 512 &&
+  return base::SysInfo::AmountOfTotalPhysicalMemory().InMiB() <= 512 &&
          (base::android::android_info::sdk_int() <=
               base::android::android_info::SDK_VERSION_Sv2 ||
           !base::FeatureList::IsEnabled(kAndroidLimitRgb565DisplayToApi32));
