98e32e4f62646fa3762647289426e69c7572b0a8
Xi Han <hanxi@google.com>
2026-05-26 08:33:58 -0700

    [Theme] Fix iph showing logic.
    This is a follow up of https://crrev.com/c/7870069, and fixes a wrong
    feature parameter used in that CL.
    
    Bug: 515008133
    Change-Id: Ic9559d430f095e4da234be55dd2d41042e567f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871637
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636213}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/NtpCustomizationPromoManager.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/NtpCustomizationPromoManager.java
@@ -111,7 +111,7 @@ public class NtpCustomizationPromoManager {
             return false;
         }
 
-        if (ChromeFeatureList.sNewTabPageCustomizationV2ForceShowTipBottomSheet.getValue()) {
+        if (ChromeFeatureList.sNewTabPageCustomizationV2ShowTipBottomSheet.getValue()) {
             if (!NtpCustomizationUtils.isThemeTipBottomSheetShownFromSharedPreference()) {
                 return false;
             }
