a75fb6b63e88beeef9a8cf3313157c047d22268f
Ernest Nguyen Hung <ernn@google.com>
2026-05-25 23:18:59 -0700

    [Profiles] Refactor `ProfilePickerToolbar` to be always visible
    It updates the `ProfilePickerToolbar` to be always visible to facilitate
    button(s) that are meant to be visible on every screen of one (or more)
    of the Profile Picker flows.
    
    It also refactors the way the `ProfilePickerToolbar` is constructed
    utilizing the builder pattern, simplifying the api.
    
    Bug: 515028732
    Change-Id: Ice9106b19749281fc9f030f2657a8459cbb9eae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868190
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635945}

--- chrome/browser/ui/views/profiles/profile_picker_sign_in_provider.cc
+++ chrome/browser/ui/views/profiles/profile_picker_sign_in_provider.cc
@@ -133,7 +133,7 @@ void ProfilePickerSignInProvider::SwitchToSignIn(
     host_->ShowScreen(
         contents(), GURL(),
         base::BindOnce(std::move(switch_finished_callback.value()), true));
-    host_->SetNativeToolbarVisible(true);
+    host_->SetNativeToolbarSigninButtonsVisible(true);
     return;
   }
 
@@ -177,7 +177,7 @@ void ProfilePickerSignInProvider::NavigateBack() {
   // Do not load any url because the desired screen is still loaded in the
   // picker contents.
   host_->ShowScreenInPickerContents(GURL(), base::OnceClosure());
-  host_->SetNativeToolbarVisible(false);
+  host_->SetNativeToolbarSigninButtonsVisible(false);
 }
 
 bool ProfilePickerSignInProvider::HandleContextMenu(
@@ -327,10 +327,11 @@ void ProfilePickerSignInProvider::OnProfileInitialized(
       contents(), host_->GetPreferredBackgroundColor());
 
   base::OnceClosure navigation_finished_closure =
-      base::BindOnce(&ProfilePickerWebContentsHost::SetNativeToolbarVisible,
-                     // Unretained is enough as the callback is called by the
-                     // host itself.
-                     base::Unretained(host_), /*visible=*/true)
+      base::BindOnce(
+          &ProfilePickerWebContentsHost::SetNativeToolbarSigninButtonsVisible,
+          // Unretained is enough as the callback is called by the
+          // host itself.
+          base::Unretained(host_), /*visible=*/true)
           .Then(base::BindOnce(std::move(switch_finished_callback.value()),
                                true));
   host_->ShowScreen(contents(), BuildSigninURL(),
@@ -351,7 +352,7 @@ bool ProfilePickerSignInProvider::IsInitialized() const {
 void ProfilePickerSignInProvider::FinishFlow(
     const CoreAccountInfo& account_info) {
   DCHECK(IsInitialized());
-  host_->SetNativeToolbarVisible(false);
+  host_->SetNativeToolbarSigninButtonsVisible(false);
   ResetWebContentsDelegates();
   std::move(callback_).Run(profile_.get(), account_info, std::move(contents_),
                            StepSwitchFinishedCallback());
