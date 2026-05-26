f6598ea3bf2c743819d92f2ac1198bef9807cda2
Norge Vizcay <vizcay@google.com>
2026-05-26 07:59:22 -0700

    Personal Context: Restrict eligibility to en-US locale
    This change updates PersonalContextEnablementService to only allow the
    "en-US" locale.
    
    Bug: b:494149753
    Change-Id: I0f093aafe15df2710646814028a4232f65de40ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871071
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636185}

--- components/personal_context/core/personal_context_enablement_service_impl.h
+++ components/personal_context/core/personal_context_enablement_service_impl.h
@@ -33,7 +33,8 @@ class PersonalContextEnablementServiceImpl
       subscription_eligibility::SubscriptionEligibilityService*
           subscription_eligibility_service,
       PrefService* pref_service,
-      GeoIpCountryCode country_code);
+      GeoIpCountryCode country_code,
+      std::string locale);
   PersonalContextEnablementServiceImpl(
       const PersonalContextEnablementServiceImpl&) = delete;
   PersonalContextEnablementServiceImpl& operator=(
@@ -73,6 +74,7 @@ class PersonalContextEnablementServiceImpl
       subscription_eligibility_service_;
   const raw_ptr<PrefService> pref_service_;
   const GeoIpCountryCode country_code_;
+  const std::string locale_;
   base::ObserverList<PersonalContextEnablementService::Observer> observers_;
   base::ScopedObservation<signin::IdentityManager,
                           signin::IdentityManager::Observer>
