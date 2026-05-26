44f8c16f99742bdc1788d744bf00d5875edcbb37
Yoshisto Yanagisawa <yyanagisawa@chromium.org>
2026-05-26 02:19:38 -0700

    Update on RestrictBackgroundFetchFromServiceWorkerEnabled
    This addresses the reviewer's request on the policy description and
    default value.
    
    See: crrev.com/c/7869857.
    Bug: 40062121
    Change-Id: I1c58ca4855255dde6f13003b3ecdeb9b77231f4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867400
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Owners-Override: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636000}

--- chrome/browser/policy/test/restrict_background_fetch_policy_browsertest.cc
+++ chrome/browser/policy/test/restrict_background_fetch_policy_browsertest.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/policy/core/common/policy_pref_names.h"
 #include "components/policy/policy_constants.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
@@ -91,6 +93,14 @@ IN_PROC_BROWSER_TEST_P(RestrictBackgroundFetchPolicyBrowserTest,
   ASSERT_EQ("ok - service worker registered",
             EvalJs(chrome_test_utils::GetActiveWebContents(this),
                    "RegisterServiceWorker()"));
+
+  // Verify that the preference reflects the expected value.
+  // When Policy::kDefault is used, the preference should be true because
+  // the policy defaults to true in the YAML definition.
+  EXPECT_EQ(
+      GetParam() != Policy::kFalse,
+      chrome_test_utils::GetProfile(this)->GetPrefs()->GetBoolean(
+          policy_prefs::kRestrictBackgroundFetchFromServiceWorkerEnabled));
 
   if (GetParam() == Policy::kFalse) {
     // If policy is set to false, the restriction is bypassed/allowed.
