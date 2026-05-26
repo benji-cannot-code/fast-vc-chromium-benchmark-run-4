edafb4008b252a0b48aa5a3beca788acab9c50dc
Vasili Svirski <vsvirski@google.com>
2026-05-26 11:07:39 -0700

    Add kEnableIOSDeviceTrustConnector feature flag
    This flag will gate the iOS Device Trust Connector logic.
    
    The DeviceTrustTabHelper attaches the DeviceTrustPolicyDecider, which
    implements the core DTC logic for intercepting navigations and handling
    device attestation challenges.
    
    Bug: 498474351
    Change-Id: Ice133166819e5501bc7de746bd9a417b07788295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7781085
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Vasili Svirski <vsvirski@google.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636320}

---
+++ ios/chrome/browser/enterprise/connectors/device_trust/features.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_FEATURES_H_
+#define IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace enterprise_connectors::features {
+
+// Enables the Device Trust Connector flow on iOS.
+BASE_DECLARE_FEATURE(kEnableIOSDeviceTrustConnector);
+
+}  // namespace enterprise_connectors::features
+
+#endif  // IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_FEATURES_H_
---
+++ ios/chrome/browser/enterprise/connectors/device_trust/features.mm
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/connectors/device_trust/features.h"
+
+namespace enterprise_connectors::features {
+
+BASE_FEATURE(kEnableIOSDeviceTrustConnector, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace enterprise_connectors::features
