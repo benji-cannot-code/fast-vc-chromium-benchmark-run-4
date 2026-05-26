e3b33c3a466e19e9a0fef4fd059c78d10721d0a4
Liza Bipin <mlbipin@google.com>
2026-05-26 09:01:26 -0700

    [Signin][Android] Improve web sign-in dialog testability and a11y
    The loading dialog will not be prematurely destroyed if initialization
    methods are called multiple times.
    
    The text can now be focused on by a screenreader but not by a keyboard.
    The cancel button can be focused on by both the screenreader and
    keyboard (or other input source).
    
    Video of updated talkback in bug.
    
    Bug: 512995680
    Change-Id: I5980ff4252f89fcd39479caa68490bff0b24288c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865532
    Reviewed-by: Lucia Giorgi <quacksort@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636234}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/WebSigninRedirectCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/WebSigninRedirectCoordinator.java
@@ -78,6 +78,13 @@ public class WebSigninRedirectCoordinator {
      */
     public void initializeWebSigninAndRedirect(
             Tab tab, String email, GURL continueUrl, GURL initialTabURL) {
+        // If we are forcing the dialog to be shown (e.g. for testing), don't destroy it if
+        // initialize is called again.
+        if (SigninFeatureMap.isEnabled(SigninFeatures.FORCE_SHOW_WEB_SIGNIN_LOADING_DIALOG)
+                && mDialogState == DialogState.SHOWN) {
+            return;
+        }
+
         mInitializeStartTime = SystemClock.elapsedRealtime();
         mTab = tab;
         mContinueUrl = continueUrl;
@@ -101,6 +108,13 @@ public class WebSigninRedirectCoordinator {
      */
     public void initializeWebSigninAndRedirect(
             Tab tab, CoreAccountId accountId, GURL continueUrl, GURL initialTabURL) {
+        // If we are forcing the dialog to be shown (e.g. for testing), don't destroy it if
+        // initialize is called again.
+        if (SigninFeatureMap.isEnabled(SigninFeatures.FORCE_SHOW_WEB_SIGNIN_LOADING_DIALOG)
+                && mDialogState == DialogState.SHOWN) {
+            return;
+        }
+
         mInitializeStartTime = SystemClock.elapsedRealtime();
         mTab = tab;
         mContinueUrl = continueUrl;
@@ -257,6 +271,10 @@ public class WebSigninRedirectCoordinator {
                 "Signin.ProcessMirrorHeaders.LoadingDuration"
                         + getWebSigninTrackerResultString(result),
                 SystemClock.elapsedRealtime() - mInitializeStartTime);
+
+        if (SigninFeatureMap.isEnabled(SigninFeatures.FORCE_SHOW_WEB_SIGNIN_LOADING_DIALOG)) {
+            return;
+        }
 
         destroy();
 
