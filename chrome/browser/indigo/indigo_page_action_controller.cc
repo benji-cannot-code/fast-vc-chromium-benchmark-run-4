0c4d3377462728f1d8bc652b630e4e6c2a4a58cf
Rodney Ding <rodneyding@google.com>
2026-05-25 20:36:55 -0700

    [Indigo] close onboarding dialog on tab navigation
    Bug: b:508219600
    Change-Id: Ie612003fb3b59ec51c7ef63f2e323bcc888855fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868282
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635912}

--- chrome/browser/indigo/indigo_page_action_controller.cc
+++ chrome/browser/indigo/indigo_page_action_controller.cc
@@ -315,7 +315,9 @@ void IndigoPageActionController::DidFinishNavigation(
 
   HideToolbar();
 
-  // TODO: b/508219600 Consider closing the onboarding dialog if navigates away.
+  if (onboarding_dialog_) {
+    onboarding_dialog_->Close();
+  }
 
   invoke_weak_ptr_factory_.InvalidateWeakPtrs();
 
