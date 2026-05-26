ba1abb6574421298e29abf09b8e87be7df625179
Louis Romero <lpromero@google.com>
2026-05-26 03:00:54 -0700

    Add web-to-native handshake for AimCobrowse
    This CL implements the web-to-native handshake between Chrome on iOS
    and the AIM Cobrowse page to establish a secure connection:
    - Modifies AimCobrowseJavaScriptFeature to support the new
      web-to-native API by exposing it in the page content world, and
      implements script message receiving, decoding, and verification.
    - Implements AssistantAimTabHelper to manage the handshake status
      (received vs reset) and forward incoming messages to registered
      callbacks.
    - Extends AssistantAIMMediator to poll and initiate the handshake ping
      when active on an allowed AIM Search URL, stopping the timer when
      a successful handshake is received, and storing the server-side
      capabilities.
    
    Context: go/aim-cobrowse-native-ui-aim-srp-communication-dd
    
    Fixed: 496179019
    Change-Id: Ib3cb2ae7801aab2f8ac58ade1e509a715593926f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828217
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636026}

--- ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/model/bottom_sheet/autofill_bottom_sheet_tab_helper.h"
 #import "ios/chrome/browser/autofill/model/form_suggestion_tab_helper.h"
 #import "ios/chrome/browser/browser_content/model/edit_menu_tab_helper.h"
+#import "ios/chrome/browser/cobrowse/model/assistant_aim_tab_helper.h"
 #import "ios/chrome/browser/cobrowse/model/cobrowse_tab_helper.h"
 #import "ios/chrome/browser/collaboration/model/data_sharing_tab_helper.h"
 #import "ios/chrome/browser/commerce/model/price_alert_util.h"
@@ -367,6 +368,7 @@ void AttachTabHelpers(web::WebState* web_state, TabHelperFilter filter_flags) {
     attacher.CreateWhen<CobrowseTabHelper>(
         attacher.IsNotInTabHelperFilter(),
         ios::TemplateURLServiceFactory::GetForProfile(profile));
+    attacher.CreateWhen<AssistantAimTabHelper>(attacher.IsForAssistantAim());
   }
 
   if (IsComposeboxIOSEnabled()) {
