d435f04e678f25922f7a4f27e2a6e9f98728e97d
Basia Zimirska <basiaz@google.com>
2026-05-26 15:10:30 -0700

    [Glic ContextMenu]Don't show 'Ask gemini' when 'Create image' is there.
    This change hides the general Glic context menu entry point ("Ask Gemini") when "Create Image with Gemini" option is
    available for an image target. This prevents both options from showing
    at the same time.
    
    TAG=agy
    CONV=005bb62a-570f-4b6d-ac7b-ac89f4c5c96f
    
    Bug: 516829938
    Change-Id: I4c5522b6d0bf7e8535c2a641dcbb40b50fe34928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876683
    Commit-Queue: Darryl James <dljames@chromium.org>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636514}

--- chrome/browser/glic/glic_context_menu_browsertest.cc
+++ chrome/browser/glic/glic_context_menu_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/mojom/context_menu/context_menu.mojom.h"
 
 namespace glic {
 
@@ -37,8 +38,9 @@ class GlicContextMenuBrowserTestBase : public GlicBrowserTest {
 class GlicContextMenuBrowserTest : public GlicContextMenuBrowserTestBase {
  public:
   GlicContextMenuBrowserTest() {
-    feature_list_.InitWithFeatures(
-        {features::kGlic, features::kGlicContextMenu}, {});
+    feature_list_.InitWithFeatures({features::kGlic, features::kGlicContextMenu,
+                                    features::kGlicShareImage},
+                                   {});
   }
 
  private:
@@ -66,6 +68,25 @@ IN_PROC_BROWSER_TEST_F(GlicContextMenuBrowserTest, GlicItemPresentForLink) {
   menu->Init();
 
   EXPECT_TRUE(menu->IsItemPresent(IDC_CONTENT_CONTEXT_GLIC));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicContextMenuBrowserTest, GlicItemAbsentForImage) {
+  glic::GlicEnabling::SetBypassEnablementChecksForTesting(true);
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GetSimpleTestUrl()));
+
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  content::ContextMenuParams params;
+  params.page_url = web_contents->GetVisibleURL();
+  params.has_image_contents = true;
+  params.media_type = blink::mojom::ContextMenuDataMediaType::kImage;
+
+  auto menu = std::make_unique<TestRenderViewContextMenu>(
+      *web_contents->GetPrimaryMainFrame(), params);
+  menu->Init();
+
+  EXPECT_FALSE(menu->IsItemPresent(IDC_CONTENT_CONTEXT_GLIC));
+  glic::GlicEnabling::SetBypassEnablementChecksForTesting(false);
 }
 
 IN_PROC_BROWSER_TEST_F(GlicContextMenuBrowserTest, GlicInvokeStandard) {
