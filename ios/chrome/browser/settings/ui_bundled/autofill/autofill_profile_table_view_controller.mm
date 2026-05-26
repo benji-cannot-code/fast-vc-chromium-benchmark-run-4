89cc49cbaed0493d468bace70694e1977853c6ef
Norge Vizcay <vizcay@google.com>
2026-05-26 07:40:55 -0700

    [iOS] Deprecate plus_address settings UI in Autofill settings.
    This CL removes the plus_address section and related logic from the
    AutofillProfileTableViewController in iOS. This aligns the UI with the
    removal of plus_address strings from the iOS repack and follows the
    deprecation of this feature in the settings page.
    
    Bug: 503268023
    Change-Id: I94f2e2883c078dfbb46c5ea0d818e2d9d4e62346
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871300
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636174}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_table_view_controller.mm
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/browser/personal_data_manager_observer_bridge.h"
 #import "components/autofill/ios/common/features.h"
 #import "components/password_manager/core/common/password_manager_features.h"
-#import "components/plus_addresses/core/browser/grit/plus_addresses_strings.h"
-#import "components/plus_addresses/core/common/features.h"
 #import "components/prefs/ios/pref_observer_bridge.h"
 #import "components/prefs/pref_change_registrar.h"
 #import "components/prefs/pref_service.h"
@@ -107,9 +105,6 @@ static constexpr autofill::DenseSet<autofill::EntityTypeName> kTravel = {
     autofill::EntityTypeName::kRedressNumber,
     autofill::EntityTypeName::kVehicle};
 
-// Plus Address Section header height.
-const CGFloat kPlusAddressSectionHeaderHeight = 24;
-
 // TODO(crbug.com/480934103): Update this URL.
 constexpr std::string_view kWalletUrlString =
     "https://wallet.google.com/wallet/settings/managepassesdata";
@@ -120,7 +115,6 @@ const CGFloat kEntityIconPointSize = 20;
 typedef NS_ENUM(NSInteger, SectionIdentifier) {
   SectionIdentifierSwitches = kSectionIdentifierEnumZero,
   SectionIdentifierProfiles,
-  SectionIdentifierPlusAddress,
   SectionIdentifierEnhancedAutofill,
   SectionIdentifierVerificationSwitch,
   SectionIdentifierWalletPromo,
@@ -135,8 +129,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeAddress,
   ItemTypeHeader,
   ItemTypeFooter,
-  ItemTypePlusAddress,
-  ItemTypePlusAddressFooter,
   ItemTypeEnhancedAutofill,
   ItemTypeEnhancedAutofillManaged,
   ItemTypeVerificationSwitch,
@@ -362,17 +354,6 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
   [model setFooter:[self addressSwitchFooter]
       forSectionWithIdentifier:SectionIdentifierSwitches];
 
-  if (base::FeatureList::IsEnabled(
-          plus_addresses::features::kPlusAddressesEnabled) &&
-      _userEmail) {
-    [model addSectionWithIdentifier:SectionIdentifierPlusAddress];
-    [model addItem:[self plusAddressItem]
-        toSectionWithIdentifier:SectionIdentifierPlusAddress];
-
-    [model setFooter:[self plusAddressFooter]
-        forSectionWithIdentifier:SectionIdentifierPlusAddress];
-  }
-
   bool isEnhancedAutofillEnabled = base::FeatureList::IsEnabled(
       autofill::features::kAutofillAiWithDataSchema);
   if (isEnhancedAutofillEnabled) {
@@ -493,24 +474,6 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
   switchItem.on = [self isAutofillProfileEnabled];
   switchItem.accessibilityIdentifier = kAutofillAddressSwitchViewId;
   return switchItem;
-}
-
-- (TableViewItem*)plusAddressItem {
-  TableViewDetailTextItem* plusAddressItem =
-      [[TableViewDetailTextItem alloc] initWithType:ItemTypePlusAddress];
-
-  plusAddressItem.text =
-      l10n_util::GetNSString(IDS_PLUS_ADDRESS_SETTINGS_LABEL);
-  plusAddressItem.accessorySymbol =
-      TableViewDetailTextCellAccessorySymbolExternalLink;
-  return plusAddressItem;
-}
-
-- (TableViewHeaderFooterItem*)plusAddressFooter {
-  TableViewLinkHeaderFooterItem* footer = [[TableViewLinkHeaderFooterItem alloc]
-      initWithType:ItemTypePlusAddressFooter];
-  footer.text = l10n_util::GetNSString(IDS_PLUS_ADDRESS_SETTINGS_SUBLABEL);
-  return footer;
 }
 
 - (TableViewItem*)enhancedAutofillItem {
@@ -894,34 +857,6 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
   return ![self isServerWalletItemAtIndexPath:indexPath];
 }
 
-- (CGFloat)tableView:(UITableView*)tableView
-    heightForHeaderInSection:(NSInteger)section {
-  NSInteger sectionIdentifier =
-      [self.tableViewModel sectionIdentifierForSectionIndex:section];
-
-  if (base::FeatureList::IsEnabled(
-          plus_addresses::features::kPlusAddressesEnabled) &&
-      sectionIdentifier == SectionIdentifierPlusAddress) {
-    return kPlusAddressSectionHeaderHeight;
-  }
-
-  return [super tableView:tableView heightForHeaderInSection:section];
-}
-
-- (CGFloat)tableView:(UITableView*)tableView
-    heightForFooterInSection:(NSInteger)section {
-  NSInteger sectionIdentifier =
-      [self.tableViewModel sectionIdentifierForSectionIndex:section];
-
-  if (base::FeatureList::IsEnabled(
-          plus_addresses::features::kPlusAddressesEnabled) &&
-      sectionIdentifier == SectionIdentifierPlusAddress) {
-    return kTableViewHeaderFooterViewHeight;
-  }
-
-  return [super tableView:tableView heightForFooterInSection:section];
-}
-
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated {
   [super setEditing:editing animated:animated];
   if (_settingsAreDismissed) {
@@ -949,15 +884,6 @@ ItemType ItemTypeForEntitySectionHeader(SectionIdentifier section_identifier) {
   NSInteger itemType = [self.tableViewModel itemTypeForIndexPath:indexPath];
 
   switch (itemType) {
-    case ItemTypePlusAddress: {
-      base::RecordAction(
-          base::UserMetricsAction("Settings.ManageOptionOnSettingsSelected"));
-      OpenNewTabCommand* command = [OpenNewTabCommand
-          commandWithURLFromChrome:
-              GURL(plus_addresses::features::kPlusAddressManagementUrl.Get())];
-      [self.sceneHandler closePresentedViewsAndOpenURL:command];
-      return;
-    }
     case ItemTypeEnhancedAutofill: {
       if (![self canModifyEnhancedAutofill]) {
         [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
