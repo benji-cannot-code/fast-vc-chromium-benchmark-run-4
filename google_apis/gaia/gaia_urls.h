a124f6170872c6062b6675aa5500203fa99f35df
Alex Ilin <alexilin@chromium.org>
2026-05-26 04:39:06 -0700

    [DBSC] Add a new UpgradeToken URL to GaiaUrls
    The new URL is hosted on the same origin as IssueToken.
    
    Bug: 514242898, b:485549157
    Change-Id: I09188c45c9b95b626aa7734a89f274f1eab792cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867996
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636059}

--- google_apis/gaia/gaia_urls.h
+++ google_apis/gaia/gaia_urls.h
@@ -64,6 +64,7 @@ class COMPONENT_EXPORT(GOOGLE_APIS) GaiaUrls {
   const GURL& mtls_oauth2_token_url() const;
   const GURL& oauth2_issue_token_url() const;
   const GURL& mtls_oauth2_issue_token_url() const;
+  const GURL& oauth2_upgrade_token_url() const;
   const GURL& oauth2_token_info_url() const;
   const GURL& oauth2_revoke_url() const;
   const GURL& reauth_api_url() const;
@@ -138,6 +139,7 @@ class COMPONENT_EXPORT(GOOGLE_APIS) GaiaUrls {
   GURL mtls_oauth2_token_url_;
   GURL oauth2_issue_token_url_;
   GURL mtls_oauth2_issue_token_url_;
+  GURL oauth2_upgrade_token_url_;
   GURL oauth2_token_info_url_;
   GURL oauth2_revoke_url_;
 
--- google_apis/gaia/gaia_urls_unittest.cc
+++ google_apis/gaia/gaia_urls_unittest.cc
@@ -136,6 +136,8 @@ TEST_F(GaiaUrlsTest, InitializeDefault_AllUrls) {
             "https://oauthaccountmanager.googleapis.com/v1/issuetoken");
   EXPECT_EQ(gaia_urls()->mtls_oauth2_issue_token_url().spec(),
             "https://oauthaccountmanager.mtls.googleapis.com/v1/issuetoken");
+  EXPECT_EQ(gaia_urls()->oauth2_upgrade_token_url().spec(),
+            "https://oauthaccountmanager.googleapis.com/v1/upgradetoken");
   EXPECT_EQ(gaia_urls()->oauth2_token_info_url().spec(),
             "https://www.googleapis.com/oauth2/v2/tokeninfo");
   EXPECT_EQ(gaia_urls()->oauth2_revoke_url().spec(),
@@ -206,6 +208,8 @@ TEST_F(GaiaUrlsTest, InitializeDefault_URLSwitches) {
             "https://test-googleapis.com/oauth2/v4/token");
   EXPECT_EQ(gaia_urls()->oauth2_issue_token_url().spec(),
             "https://test-oauthaccountmanager.com/v1/issuetoken");
+  EXPECT_EQ(gaia_urls()->oauth2_upgrade_token_url().spec(),
+            "https://test-oauthaccountmanager.com/v1/upgradetoken");
   EXPECT_EQ(gaia_urls()->oauth2_token_info_url().spec(),
             "https://test-googleapis.com/oauth2/v2/tokeninfo");
   EXPECT_EQ(gaia_urls()->oauth2_revoke_url().spec(),
@@ -329,6 +333,8 @@ TEST_F(GaiaUrlsTest, InitializeFromConfig_AllUrls) {
             "https://oauthaccountmanager.exampleapis.com/v1/issuetoken");
   EXPECT_EQ(gaia_urls()->mtls_oauth2_issue_token_url().spec(),
             "https://mtls.oauthaccountmanager.exampleapis.com/v1/issuetoken");
+  EXPECT_EQ(gaia_urls()->oauth2_upgrade_token_url().spec(),
+            "https://oauthaccountmanager.exampleapis.com/v1/upgradetoken");
   EXPECT_EQ(gaia_urls()->oauth2_token_info_url().spec(),
             "https://www.exampleapis.com/oauth2/v2/tokeninfo");
   EXPECT_EQ(gaia_urls()->oauth2_revoke_url().spec(),
@@ -405,6 +411,8 @@ TEST_F(GaiaUrlsTest, InitializeFromConfig_AllBaseUrls) {
             "https://oauthaccountmanager.exampleapis.com/v1/issuetoken");
   EXPECT_EQ(gaia_urls()->mtls_oauth2_issue_token_url().spec(),
             "https://mtls.oauthaccountmanager.exampleapis.com/v1/issuetoken");
+  EXPECT_EQ(gaia_urls()->oauth2_upgrade_token_url().spec(),
+            "https://oauthaccountmanager.exampleapis.com/v1/upgradetoken");
   EXPECT_EQ(gaia_urls()->oauth2_token_info_url().spec(),
             "https://www.exampleapis.com/oauth2/v2/tokeninfo");
   EXPECT_EQ(gaia_urls()->oauth2_revoke_url().spec(),
