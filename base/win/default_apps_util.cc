7a0eb4673019a5e21f84cf1d8fb0d5dfd15f9fca
Jesse McKenna <jessemckenna@google.com>
2026-05-26 12:32:40 -0700

    pdf-default: ensure pop-up opens in foreground
    This change passes along "set foreground window" privilege to the
    Windows Settings pop-up opened by
    `LaunchDefaultAppForFileExtensionSettings()` (via the
    `IOpenWIthLauncher` COM interface).
    
    Since the COM interface is out-of-process (`CLSCTX_LOCAL_SERVER`), the
    pop-up may not have foreground privilege of its own without this. This
    can cause the pop-up to show up minimized in the taskbar. With this
    change, it will always show up in the foreground.
    
    Bug: 493422838
    Change-Id: I5ffae44367051cb1d3612740a4de6308a43f17b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841783
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636401}

--- base/win/default_apps_util.cc
+++ base/win/default_apps_util.cc
@@ -149,6 +149,7 @@ bool LaunchDefaultAppForFileExtensionSettings(
     RecordOpenWithLauncherResult(OpenWithLauncherResult::kComError);
     return false;
   }
+  ::CoAllowSetForegroundWindow(open_with_launcher.Get(), nullptr);
 
   // Open "select a default app for `file_extension` files" dialog.
   // `kOpenWithFlags` is a working `flags` argument discovered by observation.
