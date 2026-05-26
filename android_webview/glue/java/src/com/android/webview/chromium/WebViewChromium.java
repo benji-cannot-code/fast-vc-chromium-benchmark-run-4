4d2451efdec93dfdaa862e17a53cabdc4c2e4e0c
Abhijith Nair <abhijithnair@chromium.org>
2026-05-26 03:44:54 -0700

    Replace triggerAndWaitForChromiumStarted with checks in some WebView APIs.
    Some WebView APIs like overlayHorizontalScrollbar,
    overlayVerticalScrollbar, and getCertificate no longer trigger and wait
    for Chromium initialization. Instead, they check if Chromium is already
    initialized or has started and return a default value (null or false) if
    not.
    
    Bug: 509906038
    Change-Id: Iad5bb2a63e69c783b4ccce92521ccbb9dd2b8f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829903
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636043}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
@@ -1551,8 +1551,9 @@ class WebViewChromium
     @Override
     public boolean overlayHorizontalScrollbar() {
         forbidBuilderConfiguration();
-        mAwInit.triggerAndWaitForChromiumStarted(
-                CallSite.WEBVIEW_INSTANCE_OVERLAY_HORIZONTAL_SCROLLBAR);
+        if (!mAwInit.isChromiumInitStarted()) {
+            return true;
+        }
         if (checkNeedsPost()) {
             boolean ret =
                     mFactory.runOnUiThreadBlocking(
@@ -1576,8 +1577,9 @@ class WebViewChromium
     @Override
     public boolean overlayVerticalScrollbar() {
         forbidBuilderConfiguration();
-        mAwInit.triggerAndWaitForChromiumStarted(
-                CallSite.WEBVIEW_INSTANCE_OVERLAY_VERTICAL_SCROLLBAR);
+        if (!mAwInit.isChromiumInitialized()) {
+            return false;
+        }
         if (checkNeedsPost()) {
             boolean ret =
                     mFactory.runOnUiThreadBlocking(
@@ -1608,7 +1610,9 @@ class WebViewChromium
     @Override
     public SslCertificate getCertificate() {
         forbidBuilderConfiguration();
-        mAwInit.triggerAndWaitForChromiumStarted(CallSite.WEBVIEW_INSTANCE_GET_CERTIFICATE);
+        if (!mAwInit.isChromiumInitialized()) {
+            return null;
+        }
         if (checkNeedsPost()) {
             SslCertificate ret =
                     mFactory.runOnUiThreadBlocking(
