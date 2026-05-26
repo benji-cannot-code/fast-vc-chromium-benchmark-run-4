b77a2c12bf642bb274d87cdae64255720af00990
YiwenQian <yiwenqian@google.com>
2026-05-25 18:47:28 -0700

    [ScanCard IOS] Fix landscape crash and missing icon on scan card bottom sheet
    Bug: 484102792
    Change-Id: I95e81db231ac0bcaec324b5755a313faa78acfae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872796
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635883}

--- ios/chrome/browser/autofill/scan_save_and_fill/coordinator/payments_scan_save_and_fill_offer_bottom_sheet_coordinator.mm
+++ ios/chrome/browser/autofill/scan_save_and_fill/coordinator/payments_scan_save_and_fill_offer_bottom_sheet_coordinator.mm
@@ -93,7 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   presentationController.prefersEdgeAttachedInCompactHeight = YES;
   presentationController.detents = @[
     [_viewController preferredHeightDetent],
-    [UISheetPresentationControllerDetent mediumDetent]
+    [UISheetPresentationControllerDetent largeDetent]
   ];
 
   [self.baseViewController presentViewController:_viewController
