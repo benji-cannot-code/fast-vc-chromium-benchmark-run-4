1eab695e3b894da65d3b32878c2807c14ecce7e3
Jun Ishiguro <junis@google.com>
2026-05-25 20:08:57 -0700

    Reland "Explicitly create ash::quick_unlock::PinBackend singleton object"
    This is a reland of commit 3e882bea3d780a84e7334b77e056c955bd369249
    
    Diff from the original change:
    The original CL caused the LSan bot to detect a memory leak because it
    resets the g_instance variable with nullptr. This reland CL added an
    ANNOTATE_LEAKING_OBJECT_PTR annotation with a TODO comment.
    
    Original change's description:
    > Explicitly create ash::quick_unlock::PinBackend singleton object
    >
    > This is a follow-up of https://crrev.com/c/7720241.
    >
    > This CL adds the Initialize() method and call it on process startup,
    > specifically on ChromeBrowserMainPartsAsh::PreProfileInit().
    >
    > Bug: b:498416395
    > Change-Id: I6068eb30a9591dbe0366567a0a0f78ff96a3834c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777887
    > Commit-Queue: Jun Ishiguro <junis@google.com>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1634089}
    
    Bug: b:498416395
    Change-Id: Icc3427e28282076417fb59d4b6487c955dea685d
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_chromeos_asan_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866374
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635903}

--- chrome/browser/ash/login/quick_unlock/pin_backend.h
+++ chrome/browser/ash/login/quick_unlock/pin_backend.h
@@ -32,6 +32,11 @@ class PinBackend : public ash::auth::PinBackendDelegate {
   using AvailabilityCallback =
       base::OnceCallback<void(bool, std::optional<base::Time>)>;
 
+  // Creates the singleton object.
+  // TODO(crbug.com/498416395): Use std::unique_ptr<PinBackend> for memory
+  // management, and remove this.
+  static void Initialize();
+
   // Fetch the PinBackend instance.
   static PinBackend* GetInstance();
 
@@ -47,9 +52,6 @@ class PinBackend : public ash::auth::PinBackendDelegate {
   static std::string ComputeSecret(const std::string& pin,
                                    const std::string& salt,
                                    Key::KeyType key_type);
-
-  // Use GetInstance().
-  PinBackend();
 
   PinBackend(const PinBackend&) = delete;
   PinBackend& operator=(const PinBackend&) = delete;
@@ -105,9 +107,6 @@ class PinBackend : public ash::auth::PinBackendDelegate {
                        Purpose purpose,
                        AuthOperationCallback result);
 
-  // Resets any cached state for testing purposes.
-  static void ResetForTesting();
-
   // Interface for the lock/login screen to access the user's PIN length.
   // Ensures that the UI is always consistent with the pref values without the
   // need for individual observers.
@@ -155,6 +154,9 @@ class PinBackend : public ash::auth::PinBackendDelegate {
     std::optional<bool> is_supported_;
     std::unique_ptr<PinStorageCryptohome> cryptohome_backend_;
   };
+
+  // Use Initialize().
+  PinBackend();
 
   // Called when we know if the cryptohome supports PIN.
   void OnIsCryptohomeBackendSupported(bool is_supported);
