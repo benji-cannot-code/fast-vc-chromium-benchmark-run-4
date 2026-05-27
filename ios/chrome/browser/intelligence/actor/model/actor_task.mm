92fbd5ef4da432ea0d57e7ad3d626db30621aab9
Guillaume Saulnier-Comte <gsaul@google.com>
2026-05-27 14:31:48 -0700

    [iOS][Agents]: Improve actor task snackbar UX
    This CL improves the snackbar UX displayed during the execution of
    Gemini actor tasks on iOS.
    
    Specifically, it:
    
    - Updates the snackbar style by adding a Gemini logo, and presenting
    task, tool and state updates that have not yet been shown.
    
    - Transitions the actor task to the kReflecting state when a tool
    execution completes.
    
    Bug: 515106323
    Change-Id: Ibf4b1758f1e0585fd98bd810b9b29af5c7036070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871958
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Auto-Submit: Guillaume Saulnier-Comte <gsaul@google.com>
    Commit-Queue: Guillaume Saulnier-Comte <gsaul@google.com>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637239}

--- ios/chrome/browser/intelligence/actor/model/actor_task.mm
+++ ios/chrome/browser/intelligence/actor/model/actor_task.mm
@@ -184,6 +184,7 @@ void ActorTask::OnActCompleted(ActCallback callback,
     return;
   }
 
+  SetState(ActorTaskState::kReflecting);
   std::move(callback).Run(std::move(results));
 }
 
@@ -226,6 +227,7 @@ void ActorTask::OnWebStateFinishedLoading(web::WebState* web_state) {
   // Stop the timeout and execute the deferred callback since no more observed
   // WebStates are still loading.
   load_timeout_timer_.Stop();
+  SetState(ActorTaskState::kReflecting);
   if (deferred_act_callback_) {
     std::move(deferred_act_callback_).Run();
   }
@@ -234,6 +236,7 @@ void ActorTask::OnWebStateFinishedLoading(web::WebState* web_state) {
 void ActorTask::OnPageLoadedTimeout() {
   scoped_web_state_observations_.RemoveAllObservations();
 
+  SetState(ActorTaskState::kReflecting);
   if (deferred_act_callback_) {
     std::move(deferred_act_callback_).Run();
   }
