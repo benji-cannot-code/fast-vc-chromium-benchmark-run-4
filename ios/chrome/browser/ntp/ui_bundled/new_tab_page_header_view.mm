984b83729b7cce26b38fe23a4a402c697688bbeb
Eric Ekey <ericekey@google.com>
2026-05-27 07:36:15 -0700

    [iOS] Fix DSE Logo Disappearing
    When changing the default search engine, the search engine logo in the fakebox would often disappear. This is because the PlaceholderService observer would set newly retrieved DSE logos on a stale leading view before that view was replaced in the fakebox's hierarchy. This change fixes that by saving the new image to an ivar in the NewTabPageHeaderView and creating the leading view using that ivar.
    
    Change-Id: Ibcf020fc0f53889b4269740a8d5edbd882b8f781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7877473
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636940}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -258,6 +258,8 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
   UIStackView* _buttonStack;
   // Default search engine logo view.
   UIImageView* _logoView;
+  // Default search engine logo image.
+  UIImage* _dseLogo;
 
   // Constraints to update the `toolbarView`'s postion according to the
   // `tabGroupIndicatorView`'s visibility.
@@ -663,6 +665,7 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
   } else {
     _logoView = [[UIImageView alloc] init];
     _logoView.contentMode = UIViewContentModeScaleAspectFit;
+    _logoView.image = _dseLogo;
     leadingView = _logoView;
     leadingViewYOffset = 1.0;
   }
@@ -695,6 +698,7 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
 }
 
 - (void)setDefaultSearchEngineLogo:(UIImage*)logo {
+  _dseLogo = logo;
   _logoView.image = logo;
 }
 
