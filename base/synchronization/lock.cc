ceafd1b8e8ddf2fb2fcb21bdcecaf9dfd3767beb
Kalvin Lee <kdlee@chromium.org>
2026-05-27 05:56:00 -0700

    lock: Remove `UNSAFE_TODO()` gated by `DCHECK`
    This CL is motivated by pprof incidentally pointing at
    `GetTrackedLocksHeldByCurrentThread()` as a hot spot in
    `blink_perf.parser` when Checked Span is enabled. It's not a real
    concern WRT Checked Span: the memory safety team cares more about
    optimizing release builds, and this function only exists when
    `DCHECK_IS_ON()`.
    
    The `UNSAFE_TODO()` is snappy but calling `subspan()` is more idiomatic.
    
    Change-Id: Icb7bcea1251c071ea544a056b3c0820fe2028b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867883
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636888}

--- base/synchronization/lock.cc
+++ base/synchronization/lock.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 
-#include "base/compiler_specific.h"
 #include "base/feature_list.h"
 #include "base/features.h"
 
@@ -148,9 +147,8 @@ void Lock::RemoveFromLocksHeldOnCurrentThread() {
 namespace subtle {
 
 span<const uintptr_t> GetTrackedLocksHeldByCurrentThread() {
-  return UNSAFE_TODO(
-      span<const uintptr_t>(g_tracked_locks_held_by_thread.begin(),
-                            g_num_tracked_locks_held_by_thread));
+  return span(g_tracked_locks_held_by_thread)
+      .first(g_num_tracked_locks_held_by_thread);
 }
 
 }  // namespace subtle
