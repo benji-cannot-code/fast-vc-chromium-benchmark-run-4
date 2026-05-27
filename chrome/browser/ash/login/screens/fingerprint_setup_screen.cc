b610ded2c33522ac6af5c16d97589aa1c56effed
yoshiki iguchi <yoshiki@google.com>
2026-05-26 19:37:50 -0700

    ASH Refactoring: Move IDS_OOBE_FINGERPINT_SETUP_SCREEN_* to ash/
    Move all 31 IDS_OOBE_FINGERPINT_SETUP_SCREEN_* strings (OOBE fingerprint setup screen strings containing a typo in their name) from chrome/app/chromeos_strings.grdp to ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific browser features.
    
    Update includes, and BUILD.gn accordingly.
    
    Remove redundant chrome/grit/generated_resources.h include from quick_unlock_utils.cc, fingerprint_setup_screen.cc, and fingerprint_setup_screen_handler.cc as they no longer use any strings from it.
    
    Remove redundant //chrome/app:generated_resources dependency from quick_unlock target in BUILD.gn.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: I3a945b15a58a665c108ce770be58c8da461b4d89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870538
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636659}

--- chrome/browser/ash/login/screens/fingerprint_setup_screen.cc
+++ chrome/browser/ash/login/screens/fingerprint_setup_screen.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/screens/fingerprint_setup_screen.h"
 
 #include "ash/constants/ash_pref_names.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/metrics/histogram_functions.h"
 #include "chrome/browser/ash/auth/legacy_fingerprint_engine.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
@@ -15,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/ash/login/fingerprint_setup_screen_handler.h"
-#include "chrome/grit/generated_resources.h"
 #include "chromeos/ash/components/dbus/userdataauth/userdataauth_client.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/device_service.h"
