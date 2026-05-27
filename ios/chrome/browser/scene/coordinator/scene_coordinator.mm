384f7973091f610853c7b29d976714713470c779
Ewann Pellé <ewannpv@chromium.org>
2026-05-27 02:36:31 -0700

    [iOS] Parent AssistantContainerCoordinator to SceneViewController
    Parent to SceneViewController when the scene view controller is enabled
    to prevent stale baseViewController references and layout crashes.
    
    Fixed: 515711786
    Change-Id: Icb25c5e19686874c84c05b7116ff6ca2287f66a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7873945
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636793}

--- ios/chrome/browser/scene/coordinator/scene_coordinator.mm
+++ ios/chrome/browser/scene/coordinator/scene_coordinator.mm
@@ -312,7 +312,7 @@ void OnListFamilyMembersResponse(
   }
 
   if (IsAssistantContainerEnabled()) {
-    UIViewController* baseViewController = IsAssistantSidePanelEnabled()
+    UIViewController* baseViewController = IsUseSceneViewControllerEnabled()
                                                ? _viewController
                                                : self.activeViewController;
     _assistantContainerCoordinator = [[AssistantContainerCoordinator alloc]
