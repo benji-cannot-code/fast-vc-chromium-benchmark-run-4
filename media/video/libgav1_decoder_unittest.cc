d17e6407caa92001e35ac4c14387957821aebaa4
Erik Språng <sprang@chromium.org>
2026-05-27 04:37:27 -0700

    Add smoke test for libgav1 decoder target.
    That target is currently not used directly, but is indirectly via
    libwebrtc. The new smoke test is just intended to catch any compiler
    problems early.
    
    Bug: webrtc:496266459
    Change-Id: I44147d9f629be716599ebdb7fae8dc841f386699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870898
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Erik Språng <sprang@chromium.org>
    Commit-Queue: Erik Språng <sprang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636849}

---
+++ media/video/libgav1_decoder_unittest.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/libgav1/src/src/gav1/decoder.h"
+
+namespace {
+
+TEST(Libgav1DecoderTest, SmokeTest) {
+  libgav1::Decoder decoder;
+  libgav1::DecoderSettings settings;
+  EXPECT_EQ(decoder.Init(&settings), libgav1::kStatusOk);
+}
+
+}  // namespace
