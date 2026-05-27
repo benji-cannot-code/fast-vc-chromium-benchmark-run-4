7d063e24a6c04f43a2acb2208df33198dd63dc92
Sophie Chang <sophiechang@chromium.org>
2026-05-27 14:42:12 -0700

    Put default on setting and IPH behind a separate flag
    Bug: 517184633
    Change-Id: Ie75855669fb7b8a6c414f44c51f5edb1483225b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880066
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637245}

--- chrome/browser/ui/webui/searchbox/contextual_searchbox_handler_unittest.cc
+++ chrome/browser/ui/webui/searchbox/contextual_searchbox_handler_unittest.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_service.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_service_factory.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_service_factory.h"
+#include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/tab_list/mock_tab_list_interface.h"
 #include "chrome/browser/tab_list/tab_list_interface.h"
@@ -54,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/contextual_tasks/public/features.h"
 #include "components/contextual_tasks/public/mock_contextual_tasks_service.h"
 #include "components/contextual_tasks/public/prefs.h"
+#include "components/feature_engagement/test/mock_tracker.h"
 #include "components/lens/lens_overlay_invocation_source.h"
 #include "components/lens/proto/server/lens_overlay_response.pb.h"
 #include "components/omnibox/browser/autocomplete_input.h"
@@ -1061,6 +1063,27 @@ class SmartTabSharingTest : public ContextualSearchboxHandlerTestHarness {
  public:
   ~SmartTabSharingTest() override = default;
 
+  TestingProfile::TestingFactories GetTestingFactories() const override {
+    auto factories =
+        ContextualSearchboxHandlerTestHarness::GetTestingFactories();
+    factories.emplace_back(
+        feature_engagement::TrackerFactory::GetInstance(),
+        base::BindRepeating([](content::BrowserContext* context)
+                                -> std::unique_ptr<KeyedService> {
+          auto tracker = std::make_unique<
+              testing::NiceMock<feature_engagement::test::MockTracker>>();
+          ON_CALL(*tracker, IsInFeatureTestMode)
+              .WillByDefault(testing::Return(true));
+          return tracker;
+        }));
+    return factories;
+  }
+
+  feature_engagement::test::MockTracker* mock_tracker() {
+    return static_cast<feature_engagement::test::MockTracker*>(
+        feature_engagement::TrackerFactory::GetForBrowserContext(profile()));
+  }
+
   void SetUp() override {
     ContextualSearchboxHandlerTestHarness::SetUp();
 
@@ -1251,6 +1274,60 @@ TEST_F(SmartTabSharingTest, SetSmartTabSharingActive_FeatureDisabled) {
 
   handler().SetSmartTabSharingActive(true);
   EXPECT_FALSE(handler().IsSmartTabSharingActive());
+}
+
+TEST_F(SmartTabSharingTest, SetSmartTabSharingActive_AvailabilityDisabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(
+      contextual_tasks::
+          kContextualTasksContextSmartTabSharingDefaultOnAvailability);
+
+  EXPECT_CALL(*mock_tracker(), NotifyEvent("smart_tab_sharing_activated"))
+      .Times(1);
+  EXPECT_CALL(*mock_tracker(), ShouldTriggerHelpUI(testing::_)).Times(0);
+
+  profile()->GetPrefs()->SetBoolean(
+      contextual_tasks::kContextualTasksShareOpenTabsEveryThread, false);
+
+  handler().SetSmartTabSharingActive(true);
+}
+
+TEST_F(SmartTabSharingTest,
+       SetSmartTabSharingActive_AvailabilityEnabled_RequestPromo) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(
+      contextual_tasks::
+          kContextualTasksContextSmartTabSharingDefaultOnAvailability);
+
+  EXPECT_CALL(*mock_tracker(), NotifyEvent("smart_tab_sharing_activated"))
+      .Times(1);
+  EXPECT_CALL(*mock_tracker(),
+              ShouldTriggerHelpUI(testing::Ref(
+                  feature_engagement::kIPHSmartTabSharingDefaultOnFeature)))
+      .Times(1)
+      .WillOnce(testing::Return(false));
+
+  profile()->GetPrefs()->SetBoolean(
+      contextual_tasks::kContextualTasksShareOpenTabsEveryThread, false);
+
+  handler().SetSmartTabSharingActive(true);
+}
+
+TEST_F(SmartTabSharingTest,
+       SetSmartTabSharingActive_AvailabilityEnabled_StsAlreadyDefaultOn) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(
+      contextual_tasks::
+          kContextualTasksContextSmartTabSharingDefaultOnAvailability);
+
+  EXPECT_CALL(*mock_tracker(), NotifyEvent("smart_tab_sharing_activated"))
+      .Times(1);
+  EXPECT_CALL(*mock_tracker(), ShouldTriggerHelpUI(testing::_)).Times(0);
+
+  profile()->GetPrefs()->SetBoolean(
+      contextual_tasks::kContextualTasksShareOpenTabsEveryThread, true);
+
+  handler().SetSmartTabSharingActive(true);
 }
 
 TEST_F(SmartTabSharingTest, GetSmartTabSharingActive) {
