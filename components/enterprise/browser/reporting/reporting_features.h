bbf60472319d92155f47b5bd3362519eec6112f3
Michał Kaczmarczyk <mickaczmarczyk@google.com>
2026-05-27 07:08:33 -0700

    Add a feature flag for enabling iOS signal sharing.
    This change introduces the `kIOSSignalSharingEnabled` feature, which is
    currently disabled by default, to gate the functionality of sharing
    device signals on iOS.
    
    Bug: 421927804
    Change-Id: I099b800360eccda6a6141a6ddf893e74df0fb28b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871066
    Commit-Queue: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636926}

--- components/enterprise/browser/reporting/reporting_features.h
+++ components/enterprise/browser/reporting/reporting_features.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_ENTERPRISE_BROWSER_REPORTING_REPORTING_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 
 namespace enterprise_reporting {
 
@@ -24,6 +25,11 @@ BASE_DECLARE_FEATURE(kBrowserLaunchMetadataReporting);
 // Controls whether the browser on Android should use its package name as the
 // executable path identifier in CBCM reports.
 BASE_DECLARE_FEATURE(kCbcmAndroidPackageNameIdentifier);
+
+#if BUILDFLAG(IS_IOS)
+// Controls whether enterprise signal sharing is enabled on iOS.
+BASE_DECLARE_FEATURE(kIOSSignalSharingEnabled);
+#endif  // BUILDFLAG(IS_IOS)
 
 }  // namespace enterprise_reporting
 
