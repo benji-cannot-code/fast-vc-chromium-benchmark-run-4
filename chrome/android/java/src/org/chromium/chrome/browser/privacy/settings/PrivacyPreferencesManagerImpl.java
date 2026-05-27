10bc8125d8a93be131c608e3a55f483fa057c9e6
Chirag Arora <heychirag@google.com>
2026-05-27 01:46:12 -0700

    [Android] add new preferences for storing MetricsReportingLevel info
    This commit adds PRIVACY_METRICS_REPORTING_LEVEL and
    PRIVACY_METRICS_REPORTING_DISABLED_BY_POLICY preferences to store
    MetricsReportingLevel related info.
    
    Bug: b:483043192
    Change-Id: I636f165a364b0b187445412a02a7a62319536c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849915
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636759}

--- chrome/android/java/src/org/chromium/chrome/browser/privacy/settings/PrivacyPreferencesManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/privacy/settings/PrivacyPreferencesManagerImpl.java
@@ -72,6 +72,10 @@ public class PrivacyPreferencesManagerImpl implements PrivacyPreferencesManager 
 
         mNativeInitialized = true;
 
+        mPrefs.writeBoolean(
+                ChromePreferenceKeys.PRIVACY_SHOULD_USE_METRICS_CHOICE_RESTRUCTURE,
+                shouldUseMetricsChoiceRestructure());
+
         createPolicyServiceObserver();
     }
 
