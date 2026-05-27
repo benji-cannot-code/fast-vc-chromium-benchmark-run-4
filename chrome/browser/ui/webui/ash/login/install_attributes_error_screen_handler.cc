d75dc466ce4449de7f971f6fd82d4fc42c8750fd
yoshiki iguchi <yoshiki@google.com>
2026-05-26 18:36:47 -0700

    ASH Refactoring: Move IDS_RESET_SCREEN_* to ash/
    Move all 19 IDS_RESET_SCREEN_* strings (device factory reset screen strings, split in two blocks) from chrome/app/chromeos_strings.grdp to ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific browser features.
    
    Update includes accordingly.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: If93f5902b4985762515c17ed440575b9794468d3
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866800
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636629}

--- chrome/browser/ui/webui/ash/login/install_attributes_error_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/install_attributes_error_screen_handler.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/login/install_attributes_error_screen_handler.h"
 
 #include "ash/constants/ash_switches.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/values.h"
--- chrome/browser/ui/webui/ash/login/local_state_error_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/local_state_error_screen_handler.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/login/local_state_error_screen_handler.h"
 
+#include "ash/strings/grit/ash_strings.h"
 #include "chrome/browser/ash/login/oobe_screen.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
