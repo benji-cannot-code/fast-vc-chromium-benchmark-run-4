dc83d59f84f4c494af8571d4d33dd00706350095
Chris Thompson <cthomp@chromium.org>
2026-05-27 13:12:03 -0700

    [ABH] Disable fallback for typed schemeless navigations on timeout
    This CL introduces a new feature flag
    `kHttpsUpgradesTypedSchemelessNavigationNoTimeoutFallback`.
    
    When enabled, main frame navigations initiated from the Omnibox (typed)
    that do not explicitly specify a scheme (schemeless URLs like
    `example.com` which default to HTTP) are upgraded to HTTPS but do not
    fallback to HTTP if the upgraded HTTPS navigation fails specifically due
    to a timeout (net::ERR_TIMED_OUT). Other failures (like cert errors or
    connection resets) will still fallback to HTTP (showing the ABH warning
    if enabled).
    
    New UMA metrics are added to track these typed schemeless navigations,
    collected in addition to the existing HTTPS-Upgrades metrics.
    
    To facilitate testing, the flag is also added to `chrome://flags`.
    
    Bug: 515265983
    Change-Id: I4d404ab517f953b8a07ce4a1f3a532a886415622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865506
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637182}

--- chrome/browser/ssl/https_upgrades_navigation_throttle.cc
+++ chrome/browser/ssl/https_upgrades_navigation_throttle.cc
@@ -278,6 +278,10 @@ HttpsUpgradesNavigationThrottle::WillRedirectRequest() {
     if (timer_started) {
       RecordHttpsFirstModeNavigation(Event::kUpgradeAttempted,
                                      interstitial_state_);
+      if (tab_helper->is_typed_schemeless_upgrade()) {
+        RecordHttpsFirstModeNavigation(Event::kTypedSchemelessUpgradeAttempted,
+                                       interstitial_state_);
+      }
     }
   }
 
@@ -292,6 +296,10 @@ HttpsUpgradesNavigationThrottle::WillProcessResponse() {
   if (tab_helper->is_navigation_upgraded()) {
     RecordHttpsFirstModeNavigation(Event::kUpgradeSucceeded,
                                    interstitial_state_);
+    if (tab_helper->is_typed_schemeless_upgrade()) {
+      RecordHttpsFirstModeNavigation(Event::kTypedSchemelessUpgradeSucceeded,
+                                     interstitial_state_);
+    }
     tab_helper->set_is_navigation_upgraded(false);
   }
 
