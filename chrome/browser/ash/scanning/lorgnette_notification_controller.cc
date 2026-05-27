37cb7bbe467dce9da8832b05efdbf818da1ec1c7
yoshiki iguchi <yoshiki@google.com>
2026-05-26 19:46:57 -0700

    ASH Refactoring: Move IDS_SCANNING_DLC_NOTIFICATION_* to ash/
    Move all 5 IDS_SCANNING_DLC_NOTIFICATION_* strings (scanner software DLC download notifications) from chrome/app/chromeos_strings.grdp to ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific browser features.
    
    Update includes, and BUILD.gn accordingly.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: I7ddd8dffff7f6992ea58b9e2c7f94db83eef2882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870286
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636661}

--- chrome/browser/ash/scanning/lorgnette_notification_controller.cc
+++ chrome/browser/ash/scanning/lorgnette_notification_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
