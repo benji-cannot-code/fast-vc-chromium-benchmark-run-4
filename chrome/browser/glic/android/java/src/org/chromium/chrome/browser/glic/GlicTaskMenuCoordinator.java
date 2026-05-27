18361df77f82f9c89f1fea824508d32f4afb128d
Michael Wu <mklw@google.com>
2026-05-27 12:50:32 -0700

    Glic task menu should show exit icon on hover when Side Panel is enabled
    Also sets the task menu to always be 360dp.
    https://screencast.googleplex.com/cast/NTk3MTQ2OTE4Mjk1OTYxNnw5MDJkMjg2Yy1kOA
    
    Bug: 496671322
    Change-Id: Ic0af41e95641177841254bfa8b2b3c7bccfb86aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831639
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637165}

--- chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicTaskMenuCoordinator.java
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicTaskMenuCoordinator.java
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.glic;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.widget.PopupWindow.OnDismissListener;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.build.annotations.NullMarked;
@@ -28,6 +30,7 @@ import org.chromium.ui.listmenu.ListMenu;
 import org.chromium.ui.listmenu.ListMenuItemProperties;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.util.AttrUtils;
 import org.chromium.ui.widget.AnchoredPopupWindow;
 import org.chromium.ui.widget.RectProvider;
 import org.chromium.ui.widget.ViewRectProvider;
@@ -136,12 +139,15 @@ public class GlicTaskMenuCoordinator {
         // Add gap to the right of the menu so it is not at the right edge of the screen.
         int endOffsetPx =
                 mContext.getResources().getDimensionPixelSize(R.dimen.glic_task_menu_end_offset);
-        int lateralPadding = contentView.getPaddingLeft() + contentView.getPaddingRight();
-        int widthPx = listMenu.getMaxItemWidth() + lateralPadding;
+        int maxWidthPx = AttrUtils.getDimensionPixelSize(mContext, R.attr.glicTaskMenuMaxWidth);
+        int widthPx;
 
-        int maxWidthPx =
-                mContext.getResources().getDimensionPixelSize(R.dimen.glic_task_menu_max_width);
-        widthPx = Math.min(widthPx, maxWidthPx);
+        if (AndroidSidePanelEnabledFn.isEnabled()) {
+            widthPx = maxWidthPx;
+        } else {
+            int lateralPadding = contentView.getPaddingLeft() + contentView.getPaddingRight();
+            widthPx = Math.min(listMenu.getMaxItemWidth() + lateralPadding, maxWidthPx);
+        }
 
         mMenuWindow =
                 new AnchoredPopupWindow.Builder(
@@ -173,7 +179,7 @@ public class GlicTaskMenuCoordinator {
     ModelList buildModelList(List<ActorTask> tasks) {
         ModelList modelList = new ModelList();
         int endIconWidthPx =
-                mContext.getResources().getDimensionPixelSize(R.dimen.glic_menu_dot_width);
+                AttrUtils.getDimensionPixelSize(mContext, R.attr.glicTaskMenuEndIconWidth);
 
         // TODO(crbug.com/498721993): Listen to the task and update menu item when needed.
         for (ActorTask task : tasks) {
@@ -189,11 +195,12 @@ public class GlicTaskMenuCoordinator {
                                         dismiss();
                                     });
 
-            if (GlicButtonStateController.mapTaskStateToButtonState(task.getState())
-                    == GlicButtonStateController.ButtonState.NEEDS_REVIEW) {
-                builder.withStartIconRes(R.drawable.ic_hourglass_empty_24dp)
-                        .withEndIconRes(R.drawable.glic_menu_dot)
-                        .withEndIconWidth(endIconWidthPx);
+            boolean needsReview =
+                    GlicButtonStateController.mapTaskStateToButtonState(task.getState())
+                            == GlicButtonStateController.ButtonState.NEEDS_REVIEW;
+
+            if (needsReview) {
+                builder.withStartIconRes(R.drawable.ic_hourglass_empty_24dp);
             } else {
                 builder.withStartIconRes(R.drawable.ic_arrow_selector_spark_24dp);
             }
@@ -201,6 +208,9 @@ public class GlicTaskMenuCoordinator {
             if (AndroidSidePanelEnabledFn.isEnabled()) {
                 builder.withSubtitle(getTaskSubtitle(mContext, task));
             }
+
+            int endIconRes = getEndIconRes(needsReview, AndroidSidePanelEnabledFn.isEnabled());
+            builder.withEndIconWidth(endIconWidthPx).withEndIconRes(endIconRes);
 
             modelList.add(builder.build());
         }
@@ -267,6 +277,16 @@ public class GlicTaskMenuCoordinator {
                 }
             }
         }
+    }
+
+    @DrawableRes
+    private static int getEndIconRes(boolean needsReview, boolean sidePanelEnabled) {
+        if (sidePanelEnabled) {
+            return needsReview
+                    ? R.drawable.glic_menu_end_icon_needs_review
+                    : R.drawable.glic_menu_end_icon_standard;
+        }
+        return needsReview ? R.drawable.glic_menu_dot : Resources.ID_NULL;
     }
 
     private boolean shouldShowAskGemini() {
