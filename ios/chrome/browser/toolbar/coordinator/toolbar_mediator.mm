ae3df46b04d6c10ad9a7c6d6b4034bf5c6fcbee2
Eric Ekey <ericekey@google.com>
2026-05-27 05:57:13 -0700

    [iOS][AppBar] Fix Forward Button Flickering During Navigation
    This change fixes an issue with the forward button briefly appearing
    then being animated away when navigating to a webpage from the NTP. The
    state of the toolbar is now preloaded before the toolbar appears.
    
    Fixed: 516824892
    Change-Id: Ic1364dba9d2e89fe41a07e70bdb7938058ea6d6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876057
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636890}

--- ios/chrome/browser/toolbar/coordinator/toolbar_mediator.mm
+++ ios/chrome/browser/toolbar/coordinator/toolbar_mediator.mm
@@ -162,10 +162,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!webState) {
     return;
   }
-  [self.consumer setCanGoBack:self.navigationBrowserAgent->CanGoBack(webState)];
-  [self.consumer
-      setCanGoForward:self.navigationBrowserAgent->CanGoForward(webState)
-             animated:animated];
+  [self updateConsumerNavigationButtons:webState animated:animated];
 
   const GURL visibleURL = webState->GetVisibleURL();
   [self.consumer setShareEnabled:!visibleURL.is_empty()];
@@ -336,7 +333,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)webStateDidStartLoading:(web::WebState*)webState {
-  [self updateConsumerWithWebState:webState animated:YES];
+  [self updateConsumerWithWebState:webState animated:NO];
 }
 
 - (void)webStateDidStopLoading:(web::WebState*)webState {
@@ -495,6 +492,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         .keyboardVisible;
   }
   return NO;
+}
+
+// Updates the consumer navigation arrows (forward, back) states for the given
+// `webState`.
+- (void)updateConsumerNavigationButtons:(web::WebState*)webState
+                               animated:(BOOL)animated {
+  if (!webState) {
+    return;
+  }
+  const GURL lastCommittedURL = webState->GetLastCommittedURL();
+  BOOL isLastCommittedUrlNtp =
+      IsUrlNtp(lastCommittedURL) || lastCommittedURL.is_empty();
+  BOOL isToolbarTransitioningToVisible =
+      isLastCommittedUrlNtp && !IsUrlNtp(webState->GetVisibleURL());
+
+  BOOL canGoForward = self.navigationBrowserAgent->CanGoForward(webState);
+  if (isToolbarTransitioningToVisible) {
+    // Navigation buttons will be preloaded before the toolbar appears.
+    animated = NO;
+    if (webState->GetNavigationManager()->GetPendingItemIndex() == -1) {
+      // The Web State is mid-navigation from the NTP to a webpage. Prevents the
+      // forward button from appearing during the navigation if it will not be
+      // present after the navigation.
+      canGoForward = NO;
+    }
+  }
+
+  [self.consumer setCanGoBack:self.navigationBrowserAgent->CanGoBack(webState)];
+  [self.consumer setCanGoForward:canGoForward animated:animated];
 }
 
 // Updates the consumer tab state.
