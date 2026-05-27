b1937bd581423c0399b8249fe0d3df6aa2c92b0e
Ohad Vano <ohad.vano@island.io>
2026-05-27 03:40:33 -0700

    Fixed a crash with invalid PWA URLs
    Fixed a crash in the browser when admin policy pushes an invalid PWA
    URL, e.g., corp-app://example.com/App Valid URL should have HTTP or
    HTTPS scheme
    
    Bug: 512150513
    Change-Id: Ica0761d0fbe0da41d78cffa7fd302aa6d4ce2858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837918
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Michael Maltsev <michaelm@island.io>
    Cr-Commit-Position: refs/heads/main@{#1636827}

--- chrome/browser/web_applications/policy/web_app_policy_manager.cc
+++ chrome/browser/web_applications/policy/web_app_policy_manager.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/webapps/browser/install_result_code.h"
+#include "components/webapps/browser/web_app_url_config.h"
 #include "components/webapps/common/web_app_id.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -649,7 +650,7 @@ WebAppPolicyManager::ParseInstallPolicyEntry(const base::DictValue& entry) {
          (*default_launch_container == kDefaultLaunchContainerWindowValue) ||
          (*default_launch_container == kDefaultLaunchContainerTabValue));
 
-  if (!install_gurl.is_valid()) {
+  if (!webapps::IsUrlEligibleForWebApp(install_gurl)) {
     LOG(WARNING) << "Policy-installed web app has invalid URL " << *install_url;
     return std::nullopt;
   }
