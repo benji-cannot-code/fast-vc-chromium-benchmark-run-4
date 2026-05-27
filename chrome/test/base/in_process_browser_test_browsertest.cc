2d23602613e7737dd2b2badd87571141acd2e239
Zekun Jiang <zekunjiang@google.com>
2026-05-27 12:20:33 -0700

    [Code Health] Fix UNSAFE_TODOs in chrome/test/base.
    Also fixes a bug where row paddings may be ignored in save_desktop_snapshot.cc.
    
    Bug: 506870317, 516816912
    Change-Id: I69f5aa0f16ea648a6b8723138235c4c12237e256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871330
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637141}

--- chrome/test/base/in_process_browser_test_browsertest.cc
+++ chrome/test/base/in_process_browser_test_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include <array>
+#include <string_view>
 
 #include "base/compiler_specific.h"
 #include "base/path_service.h"
@@ -79,10 +80,10 @@ class LoadFailObserver : public content::WebContentsObserver {
 
 class InProcessBrowserTestP
     : public InProcessBrowserTest,
-      public ::testing::WithParamInterface<const char*> {};
+      public ::testing::WithParamInterface<std::string_view> {};
 
 IN_PROC_BROWSER_TEST_P(InProcessBrowserTestP, TestP) {
-  UNSAFE_TODO(EXPECT_EQ(0, strcmp("foo", GetParam())));
+  EXPECT_EQ(GetParam(), "foo");
 }
 
 INSTANTIATE_TEST_SUITE_P(IPBTP,
