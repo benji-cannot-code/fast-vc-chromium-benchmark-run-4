8570f0b278b0326a4b250775353730cc55fd9fc4
yoshiki iguchi <yoshiki@google.com>
2026-05-26 23:50:19 -0700

    ASH Refactoring: Move IDS_SKIP_ENROLLMENT_DIALOG_* to ash/
    Move all 6 IDS_SKIP_ENROLLMENT_DIALOG_* strings (skip enterprise/school enrollment confirmation dialog strings) from chrome/app/chromeos_strings.grdp to ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific browser features.
    
    Update includes accordingly.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: I332f5a8c29d17df8f93a8b53e69a49f28ced06d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866552
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636729}

--- chrome/browser/ui/webui/ash/login/enrollment_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/enrollment_screen_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/constants/ash_features.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
 #include "base/json/json_reader.h"
