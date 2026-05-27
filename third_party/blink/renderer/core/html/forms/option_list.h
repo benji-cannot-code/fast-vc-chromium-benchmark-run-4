1c4eddb190acae36b09f2e96ee812f2498bf8c17
Joey Arhar <jarhar@chromium.org>
2026-05-27 14:16:14 -0700

    Deduplicate PageUp/PageDown handling for options and menuitems
    This patch has no behavior changes, it just moves PageUp and PageDown
    handling for menuitems and option elements into a shared template class
    which can be used for both.
    
    Bug: 422940462
    Change-Id: I12365cd5bebf9d536a5ad527588440f143a60e54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830279
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637228}

--- third_party/blink/renderer/core/html/forms/option_list.h
+++ third_party/blink/renderer/core/html/forms/option_list.h
@@ -16,6 +16,7 @@ class HTMLSelectElement;
 class HTMLOptionElement;
 class HTMLMenuOwnerElement;
 class HTMLMenuItemElement;
+struct FocusParams;
 
 template <typename OwnerType, typename ItemType>
 class ElementListIterator final {
@@ -135,6 +136,11 @@ class ElementList final {
   ItemType* PreviousFocusableElement(ItemType& element,
                                      bool inclusive = false,
                                      bool wrap = false);
+
+  enum class PageKey { kDown, kUp };
+  void HandlePageUpDown(ItemType& element,
+                        PageKey page_key,
+                        FocusParams& focus_params);
 
  private:
   ItemType* FindElement(ItemType& element,
