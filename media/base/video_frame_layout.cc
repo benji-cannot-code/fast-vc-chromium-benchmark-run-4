1a57cf346e4fc3d41dfe942936459c7c9503ace9
Hirokazu Honda <hiroh@chromium.org>
2026-05-26 19:47:36 -0700

    media: Support out-of-order planes in FitsInContiguousBufferOfSize
    VideoFrameLayout::FitsInContiguousBufferOfSize() previously assumed
    planes were defined in increasing offset order. This caused it to
    incorrectly return false for layouts with out-of-order planes, even
    if they did not overlap and fit within the buffer.
    
    Fix this by sorting plane indices by offset before performing the
    overlap check.
    
    Bug: 515041691, 511762404
    Test: media_unittests
    Change-Id: I2dfc77c78dcd5ef636103617159c20cd7589803a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874137
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636662}

--- media/base/video_frame_layout.cc
+++ media/base/video_frame_layout.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/video_frame_layout.h"
 
+#include <algorithm>
+#include <array>
 #include <numeric>
 #include <sstream>
 
@@ -207,15 +209,31 @@ bool VideoFrameLayout::FitsInContiguousBufferOfSize(size_t data_size) const {
   if (planes_.size() != VideoFrame::NumPlanes(format_)) {
     return false;
   }
+
+  const size_t num_planes = planes_.size();
+  CHECK_LE(num_planes, VideoFrame::kMaxPlanes);
+
+  // The planes in `planes_` are not guaranteed to be ordered by offset.
+  // We sort the plane indices by offset so that we can check for overlaps in
+  // the offset order.
+  std::array<size_t, VideoFrame::kMaxPlanes> sorted_plane_indices;
+  std::iota(sorted_plane_indices.begin(),
+            sorted_plane_indices.begin() + num_planes, 0);
+  std::sort(sorted_plane_indices.begin(),
+            sorted_plane_indices.begin() + num_planes,
+            [this](size_t a, size_t b) {
+              return planes_[a].offset < planes_[b].offset;
+            });
+
   size_t previous_plane_end = 0;
-  for (size_t plane_idx = 0; plane_idx < planes_.size(); ++plane_idx) {
+  for (size_t i = 0; i < num_planes; ++i) {
+    size_t plane_idx = sorted_plane_indices[i];
     const auto& plane = planes_[plane_idx];
     if (plane.offset > data_size || plane.size > data_size) {
       return false;
     }
 
-    // Check that planes do not overlap and are in order. This must be
-    // satisfied because the buffer given in this function is contiguous.
+    // Check that planes do not overlap.
     if (plane.offset < previous_plane_end) {
       return false;
     }
