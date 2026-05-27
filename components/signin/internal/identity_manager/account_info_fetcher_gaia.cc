691e1ef7890cb6c6e89229756b54c0a11956c1d9
Etienne Pierre-doray <etiennep@chromium.org>
2026-05-27 05:30:58 -0700

    [tracing] Migrate async track events to more appropriate alternatives in components/signin
    This CL changes async events on custom tracks (TRACE_EVENT_BEGIN/_END)
    to more appropriate alternatives: either on-thread events connected with
    flows, on synchronous events.
    
    There's a tradeof to using async events:
    - async events make tracking duration easier both in the UI and in queries
    - async events take a lot of vertical space in tracing UI
    - async events looses context of where the start and end happen
    - custom track are tricky to use, and  Track::FromPointer is problematic because it can alias when pointers are reused.
    https://chromium.googlesource.com/chromium/src.git/+/HEAD/docs/trace_events.md#asynchronous-events
    
    As a general rule of thumb:
    - synchronous events are more appropriate when a slice is well nested in a function
    - connected flows are more appropriate when the start and end don't wrap a clear operation
      (e.g. scheduling delay is better represented as flows)
    
    This CL was uploaded by git cl split.
    
    Bug: 432427382
    Change-Id: Ic563b9fe4f5acdaa4697ed30788a79900afc4d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868666
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636875}

--- components/signin/internal/identity_manager/account_info_fetcher_gaia.cc
+++ components/signin/internal/identity_manager/account_info_fetcher_gaia.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
-#include "third_party/perfetto/include/perfetto/tracing/track.h"
+#include "third_party/perfetto/include/perfetto/tracing/track_event_args.h"
 
 AccountInfoFetcherGaia::AccountInfoFetcherGaia(
     ProfileOAuth2TokenService& token_service,
@@ -25,16 +25,16 @@ AccountInfoFetcherGaia::AccountInfoFetcherGaia(
       url_loader_factory_(std::move(url_loader_factory)),
       account_id_(account_id),
       callback_(std::move(callback)) {
-  TRACE_EVENT_BEGIN("AccountFetcherService", "AccountIdFetcher",
-                    perfetto::Track::FromPointer(this), "account_id",
-                    account_id.ToString());
+  TRACE_EVENT_INSTANT("AccountFetcherService", "AccountIdFetcher",
+                      perfetto::Flow::FromPointer(this), "account_id",
+                      account_id.ToString());
 
   Start();
 }
 
 AccountInfoFetcherGaia::~AccountInfoFetcherGaia() {
-  TRACE_EVENT_END("AccountFetcherService",
-                  /* AccountIdFetcher */ perfetto::Track::FromPointer(this));
+  TRACE_EVENT_INSTANT("AccountFetcherService", "~AccountInfoFetcherGaia",
+                      perfetto::TerminatingFlow::FromPointer(this));
 }
 
 void AccountInfoFetcherGaia::Start() {
@@ -49,7 +49,7 @@ void AccountInfoFetcherGaia::OnGetTokenSuccess(
     const OAuth2AccessTokenManager::Request* request,
     const OAuth2AccessTokenConsumer::TokenResponse& token_response) {
   TRACE_EVENT_INSTANT("AccountFetcherService", "OnGetTokenSuccess",
-                      perfetto::Track::FromPointer(this));
+                      perfetto::Flow::FromPointer(this));
   DCHECK_EQ(request, login_token_request_.get());
 
   gaia_oauth_client_ =
@@ -63,7 +63,7 @@ void AccountInfoFetcherGaia::OnGetTokenFailure(
     const OAuth2AccessTokenManager::Request* request,
     const GoogleServiceAuthError& error) {
   TRACE_EVENT_INSTANT("AccountFetcherService", "OnGetTokenFailure",
-                      perfetto::Track::FromPointer(this),
+                      perfetto::Flow::FromPointer(this),
                       "google_service_auth_error", error.ToString());
   LOG(ERROR) << "OnGetTokenFailure: " << error.ToString();
   DCHECK_EQ(request, login_token_request_.get());
@@ -73,21 +73,21 @@ void AccountInfoFetcherGaia::OnGetTokenFailure(
 void AccountInfoFetcherGaia::OnGetUserInfoResponse(
     const base::DictValue& user_info) {
   TRACE_EVENT_INSTANT("AccountFetcherService", "OnGetUserInfoResponse",
-                      perfetto::Track::FromPointer(this), "account_id",
+                      perfetto::Flow::FromPointer(this), "account_id",
                       account_id_.ToString());
   std::move(callback_).Run(signin::AccountInfoFromUserInfo(user_info));
 }
 
 void AccountInfoFetcherGaia::OnOAuthError() {
   TRACE_EVENT_INSTANT("AccountFetcherService", "OnOAuthError",
-                      perfetto::Track::FromPointer(this));
+                      perfetto::Flow::FromPointer(this));
   LOG(ERROR) << "OnOAuthError";
   std::move(callback_).Run(std::nullopt);
 }
 
 void AccountInfoFetcherGaia::OnNetworkError(int response_code) {
   TRACE_EVENT_INSTANT("AccountFetcherService", "OnNetworkError",
-                      perfetto::Track::FromPointer(this), "response_code",
+                      perfetto::Flow::FromPointer(this), "response_code",
                       response_code);
   LOG(ERROR) << "OnNetworkError " << response_code;
   std::move(callback_).Run(std::nullopt);
