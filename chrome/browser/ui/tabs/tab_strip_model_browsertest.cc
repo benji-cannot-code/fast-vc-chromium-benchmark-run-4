76685b284edd613a2c06e558fee8fc147fd8f7de
Claudio DeSouza <cdesouza@chromium.org>
2026-05-26 18:07:43 -0700

    Using `EXPECT_CHECK_DEATH` in `TabStripModelBrowserTest`
    This CL replaces the use of `EXPECT_DEATH_IF_SUPPORTED` in a
    `TabStripModelBrowserTest` test with `EXPECT_CHECK_DEATH` to avoid
    failures for this test when building with `dcheck_always_on=true`, as in
    that build mode, `CHECK` messages are stripped from the binary, so this
    expactation would fail.
    
    Bug: 501991031
    Change-Id: I5e923368a2b94a727bff69a584e4e9974e1aecc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876933
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636617}

--- chrome/browser/ui/tabs/tab_strip_model_browsertest.cc
+++ chrome/browser/ui/tabs/tab_strip_model_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 #include "base/json/json_reader.h"
 #include "base/scoped_observation.h"
+#include "base/test/gtest_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
@@ -521,8 +522,7 @@ IN_PROC_BROWSER_TEST_F(TabStripModelBrowserTest,
   TabStripModelCloseWebContentsOnChangeObserver close_tab_observer;
   tab_strip_model->AddObserver(&close_tab_observer);
 
-  EXPECT_DEATH_IF_SUPPORTED(tab_strip_model->MoveWebContentsAt(0, 1, false),
-                            "Check failed");
+  EXPECT_CHECK_DEATH(tab_strip_model->MoveWebContentsAt(0, 1, false));
   tab_strip_model->RemoveObserver(&close_tab_observer);
 }
 
