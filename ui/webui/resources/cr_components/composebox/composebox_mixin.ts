cdd4c39706a8c8fa4c7eefe7b83fcb408897dfda
Ryan Akiyama <ryanakiyama@chromium.org>
2026-05-27 15:24:38 -0700

    [composebox] Migrate `submitButtonIconType` to `composebox_mixin.ts`
    NO_IFTTT=Property moved to the mixin
    
    Bug: b:508286650
    Change-Id: Ie7a94f43f7a4700654592ca598fa1309cde976fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876346
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Auto-Submit: Ryan Akiyama <ryanakiyama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637278}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -36,6 +36,11 @@ export enum VoiceSearchAction {
   QUERY_SUBMITTED = 1,
 }
 
+export enum SubmitButtonIconType {
+  FORWARD = 'forward',
+  UPWARD = 'upward',
+}
+
 const PERMISSION_PROMPT_CSS_CLASS = 'embedded-permission-prompt-showing';
 
 type Constructor<T> = new (...args: any[]) => T;
@@ -74,6 +79,7 @@ export const ComposeboxEmbedderMixin =
             smartTabSharingActive: {type: Boolean},
             shouldShowGhostFiles: {type: Boolean},
             showMenuOnClick: {type: Boolean},
+            submitButtonIconType: {type: String},
             isCanvasQuerySubmitted: {type: Boolean},
             canSubmitFilesAndInput: {
               type: Boolean,
@@ -219,6 +225,8 @@ export const ComposeboxEmbedderMixin =
             loadTimeData.getString('searchboxComposePlaceholder');
         accessor inputState: InputState|null = null;
         accessor inToolMode: boolean = false;
+        accessor submitButtonIconType: SubmitButtonIconType =
+            SubmitButtonIconType.UPWARD;
         // Indicates if voice search overlay is open. Does not indicate if it
         // is 'listening'. This is because there might be an error scrim showing
         // (see `hasVoiceSearchError`), making voice search not 'listening'.
@@ -2306,6 +2314,7 @@ export interface ComposeboxEmbedderMixinInterface extends
   smartComposeStats: SmartComposeStats;
   state: ComposeboxState|null;
   submitEnabled: boolean;
+  submitButtonIconType: SubmitButtonIconType;
   tabSuggestions: TabInfo[];
   transcript: string;
   uploadButtonDisabled: boolean;
