59782048b62826d8efdf38e140256fbf14780e6a
Xuehui Chen <xuehuichen@google.com>
2026-05-27 10:47:00 -0700

    [SplitCompatService][2/4] Migrate Chrome split classes to use base impl
    This CL migrates several Chrome subclass services in dynamic feature
    splits to inherit from the new SplitCompatService in the based module.
    
    Bug: b:507013097
    Change-Id: Ibfac233366659416e4a53e626e7b56337ea4d5db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868992
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Commit-Queue: Xuehui Chen <xuehuichen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637079}

--- chrome/android/java/src/org/chromium/chrome/browser/app/bluetooth/BluetoothNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/bluetooth/BluetoothNotificationService.java
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.app.bluetooth;
 
+import org.chromium.base.SplitCompatService;
 import org.chromium.build.annotations.IdentifierNameString;
 import org.chromium.build.annotations.NullMarked;
-import org.chromium.chrome.browser.base.SplitCompatService;
 
 /** See {@link BluetoothNotificationServiceImpl}. */
 @NullMarked
