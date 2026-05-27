0b7528a0bc6954ccb7e16b9be4a78f40dd953263
yoshiki iguchi <yoshiki@google.com>
2026-05-26 20:56:48 -0700

    ASH Refactoring: Move IDS_ECHE_APP_* to ash/
    Move all 6 IDS_ECHE_APP_* strings (Eche App notifications and name strings) from chrome/app/chromeos_strings.grdp to ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific browser features.
    
    Update includes, and BUILD.gn accordingly.
    
    Remove redundant chrome/grit/generated_resources.h include from eche_app_notification_controller.cc, eche_app_info.cc, and eche_app_integration_browsertest.cc as they no longer use any strings from it.
    
    Remove redundant //chrome/app:generated_resources and //chrome/app:generated_resources_grit dependencies from eche_app target in BUILD.gn. Remove +chrome/grit from DEPS in eche_app as it is no longer required.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: I91262db174c9865eef815970e60e620d9f3a045d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870680
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636686}

--- chrome/browser/ash/system_web_apps/apps/eche_app_integration_browsertest.cc
+++ chrome/browser/ash/system_web_apps/apps/eche_app_integration_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/shell.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/webui/eche_app_ui/url_constants.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ash/system_web_apps/system_web_app_manager.h"
@@ -14,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
-#include "chrome/grit/generated_resources.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
