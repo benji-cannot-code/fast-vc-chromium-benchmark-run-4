199bdfddfa5d56301dab1d713f6438a008fdb462
Bruno Braga <brunobraga@google.com>
2026-05-27 04:39:04 -0700

    Reland "[Autofill AI - M5] Bring in shopping settings: part 1 (Infrastructure)"
    This reverts commit c07f344ff3551381fbcbb616ea4ad8e016ed1e01.
    
    Reason for revert: Fixed flaky test
    
    Original change's description:
    > Revert "[Autofill AI - M5] Bring in shopping settings: part 1 (Infrastructure)"
    >
    > This reverts commit 62cddf1cdcdd5cae15fb7e84d086236f0f9e71cc.
    >
    > Reason for revert: Caused org.chromium.chrome.browser.autofill.settings.HomeOfTransactionsFragmentTest#testClickAutofillSettingsLaunchesAutofillOptions to start failing
    > Sample failing test run https://ci.chromium.org/ui/p/chromium/builders/ci/android-13-x64-rel/19024/overview
    >
    > Original change's description:
    > > [Autofill AI - M5] Bring in shopping settings: part 1 (Infrastructure)
    > >
    > > This CL is part of a 3-CL stack to introduce the Autofill AI Shopping
    > > settings subpage:
    > >
    > > 1. [This CL] Feature Flags, and Resources.
    > > 2. Core settings page fragment implementation
    > >    (autofill-ai-shopping-page)
    > > 3. Entry point hook up and integration/unit tests
    > >    (autofill-ai-shopping-entry-and-tests)
    > >
    > > This first part lays the infrastructure groundwork, including:
    > > - Declaring and exposing the kAutofillAiWalletShopping feature flag
    > >   to Java.
    > > - Registering the settings page enum in histrograms.
    > > - Defining the preference element in
    > >   home_of_transactions_preferences.xml.
    > > - Defining new strings in autofill_strings.grdp.
    > > - Adding test utilities for order and shipment types in
    > >   TestUtils.java.
    > >
    > > Bug: 482994257
    > > Change-Id: I01da73eb01e3893ae2af33bb63370562a94ed848
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856804
    > > Reviewed-by: Abe Boujane <boujane@google.com>
    > > Commit-Queue: Bruno Braga <brunobraga@google.com>
    > > Reviewed-by: Josef Raska <josefraska@google.com>
    > > Reviewed-by: Norge Vizcay <vizcay@google.com>
    > > Auto-Submit: Bruno Braga <brunobraga@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1634981}
    >
    > Bug: 482994257
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I5852aa710cb9762b24970098d3e925f37f672d25
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871314
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Peter Kotwicz <pkotwicz@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Peter Kotwicz <pkotwicz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1635330}
    
    Bug: 482994257
    Change-Id: Ie344cfe5335cd1f17831d62fc35391992bc9e0b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871078
    Reviewed-by: Josef Raska <josefraska@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636851}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -122,6 +122,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillAiShowDialogInSettingsWhenUpstreamingFails,
     &autofill::features::kAutofillAiShowWalletDisabledBanner,
     &autofill::features::kAutofillAiWalletPrivatePassesDeepLink,
+    &autofill::features::kAutofillAiWalletShopping,
     &autofill::features::kAutofillAiWithDataSchema,
     &autofill::features::kAutofillAndroidDesktopKeyboardAccessoryRevamp,
     &autofill::features::kAutofillAndroidDesktopSuppressAccessoryOnEmpty,
