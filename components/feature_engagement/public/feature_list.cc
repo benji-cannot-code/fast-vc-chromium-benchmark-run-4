1ac13f1c288c9dd988dbbbd10d7aed82e8acf4cf
Gayane Petrosyan <gayane@google.com>
2026-05-27 06:14:10 -0700

    [iOS][gnp] Create FET config for backend promos.
    Create FET config for backend promos that will be part of iOS fullscreen
    promos group (promos 2 days apart, no more than 3 promos a week) but
    will not have 1 promo per 30 days as other ios fullscreen promos do.
    Frequency of backend promos will be controlled only from the backend.
    
    Bug: 503367730
    Change-Id: Ie8fc555641990000104e990880f45fc98cf81dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871959
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636899}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -165,6 +165,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHHomeCustomizationMenuFeature,
     &kIPHiOSActiveDaysTrackingFeature,
     &kIPHiOSAIHubNewBadge,
+    &kIPHiOSBackendPromoFeature,
     &kIPHiOSContextualPanelPriceInsightsFeature,
     &kIPHiOSContextualPanelSampleModelFeature,
     &kIPHiOSDefaultBrowserBadgeEligibilityFeature,
