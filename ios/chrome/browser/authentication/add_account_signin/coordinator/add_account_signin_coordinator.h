5511f13699417340ebb42dd2f6841c6d3e2052ad
Jérôme Lebel <jlebel@chromium.org>
2026-05-27 03:24:37 -0700

    [iOS] Add comments in reauth and addaccount coordintor
    Comments added in:
      AddAccountSigninCoordinator
      SigninReauthCoordinator
    
    This patch clarifies the behavior when the user changes the email
    address when the auth dialog is opened with preset email address.
    
    Change-Id: I74f9ab7fce887335a27be9815792dae9dd5ca446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866080
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636822}

--- ios/chrome/browser/authentication/add_account_signin/coordinator/add_account_signin_coordinator.h
+++ ios/chrome/browser/authentication/add_account_signin/coordinator/add_account_signin_coordinator.h
@@ -22,11 +22,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NS_UNAVAILABLE;
 
 // Designated initializer.
+// `contextStyle` is used to customize content on screens.
 // `viewController` presents the sign-in.
 // `accessPoint` is the view where the sign-in button was displayed.
 // `promoAction` is promo button used to trigger the sign-in.
 // `signinIntent` is the sign-in flow that will be triggered.
-// `contextStyle` is used to customize content on screens.
+// `prefilledEmail` preset the email in the add account dialog. The user can
+//     change this email. If the user changes the email and finishes the flow
+//     successfully, the coordinator flow is successful and the sign-in callback
+//     returns SigninCoordinatorResultSuccess.
 - (instancetype)
     initWithBaseViewController:(UIViewController*)viewController
                        browser:(Browser*)browser
@@ -34,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    accessPoint:(signin_metrics::AccessPoint)accessPoint
                    promoAction:(signin_metrics::PromoAction)promoAction
                   signinIntent:(AddAccountSigninIntent)signinIntent
-                prefilledEmail:(NSString*)email
+                prefilledEmail:(NSString*)prefilledEmail
           continuationProvider:
               (const ChangeProfileContinuationProvider&)continuationProvider
     NS_DESIGNATED_INITIALIZER;
