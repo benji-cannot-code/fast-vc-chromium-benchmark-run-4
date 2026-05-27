bc6da5d2f5096aa83577f359d6aa30d63db3d5c9
Peter Kang <peterkang@google.com>
2026-05-27 09:36:10 -0700

    [Omnibox] Cleanup obsolete downward refine drawable resources.
    OmniboxSuggestionsDropdown component was showing arrow pointing
    downwards when mToolbar was on bottom but mToolbar should always be on
    top when suggestions are being displayed after removal of
    AndroidBottomToolbar flag.
    
    Bug: 515137657
    Change-Id: Ifae0b14c8e96aa1ffd39a30a01658b146ab4dc3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869517
    Commit-Queue: Peter Kang <peterkang@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637024}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewProcessor.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewProcessor.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions.base;
 
-import static org.chromium.build.NullUtil.assumeNonNull;
-
 import android.content.Context;
 import android.graphics.Typeface;
 import android.text.Spannable;
@@ -20,7 +18,6 @@ import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.metrics.TimingMetric;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider.ControlsPosition;
 import org.chromium.chrome.browser.omnibox.MatchClassificationStyle;
 import org.chromium.chrome.browser.omnibox.OmniboxMetrics;
 import org.chromium.chrome.browser.omnibox.R;
@@ -187,13 +184,7 @@ public abstract class BaseSuggestionViewProcessor implements SuggestionProcessor
                         mContext,
                         R.string.accessibility_omnibox_btn_refine,
                         suggestion.getFillIntoEdit());
-        @ControlsPosition Integer toolbarPosition = mUiContext.toolbarPositionSupplier.get();
-        assumeNonNull(toolbarPosition);
-        @DrawableRes
-        int icon =
-                toolbarPosition == ControlsPosition.BOTTOM
-                        ? R.drawable.btn_suggestion_refine_down
-                        : R.drawable.btn_suggestion_refine_up;
+        @DrawableRes int icon = R.drawable.btn_suggestion_refine_up;
 
         Runnable action =
                 () -> {
