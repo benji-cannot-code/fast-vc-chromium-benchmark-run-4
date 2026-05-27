3f7d2c5c1b1d258101163923a3aa5dfd81e90f4a
Etienne Pierre-doray <etiennep@chromium.org>
2026-05-27 05:00:00 -0700

    [tracing] Migrate async track events to more appropriate alternatives in content/renderer
    This CL changes async events on custom tracks (TRACE_EVENT_BEGIN/_END)
    to more appropriate alternatives: either on-thread events connected with
    flows, on synchronous events.
    
    There's a tradeof to using async events:
    - async events make tracking duration easier both in the UI and in queries
    - async events take a lot of vertical space in tracing UI
    - async events looses context of where the start and end happen
    - custom track are tricky to use, and  Track::FromPointer is problematic
      because it can alias when pointers are reused.
     (TerminatingFlow::FromPointer works around this by letting perfetto
      know the pointer is "freed" and could be reused)
    
    https://chromium.googlesource.com/chromium/src.git/+/HEAD/docs/trace_events.md#asynchronous-events
    
    As a general rule of thumb:
    - synchronous events are more appropriate when a slice is well nested in
      a function
    - connected flows are more appropriate when the start and end don't wrap
      a clear operation (e.g. scheduling delay is better represented as
      flows)
    
    This CL was uploaded by git cl split.
    
    Bug: 432427382
    Change-Id: Id21368e7d5a34d00cf4f659980dd464b5c0b904d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871750
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636859}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -166,6 +166,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_v8_features.h"
 #include "third_party/blink/public/web/web_view.h"
 #include "third_party/perfetto/include/perfetto/tracing/track.h"
+#include "third_party/perfetto/include/perfetto/tracing/track_event_args.h"
 #include "third_party/skia/include/core/SkFontMgr.h"
 #include "third_party/skia/include/core/SkGraphics.h"
 #include "ui/base/ui_base_switches.h"
@@ -1359,19 +1360,21 @@ scoped_refptr<gpu::GpuChannelHost> RenderThreadImpl::EstablishGpuChannelSync() {
 
 void RenderThreadImpl::EstablishGpuChannel(
     EstablishGpuChannelCallback callback) {
-  TRACE_EVENT_BEGIN("gpu", "RenderThreadImpl::EstablishGpuChannel",
-                    perfetto::Track::FromPointer(this));
+  TRACE_EVENT_INSTANT("gpu", "RenderThreadImpl::EstablishGpuChannel",
+                      perfetto::Flow::FromPointer(this));
   gpu_->EstablishGpuChannel(base::BindOnce(
       [](EstablishGpuChannelCallback callback, RenderThreadImpl* thread,
+         perfetto::TerminatingFlow flow,
          scoped_refptr<gpu::GpuChannelHost> host) {
-        TRACE_EVENT_END("gpu", perfetto::Track::FromPointer(thread));
+        TRACE_EVENT_INSTANT(
+            "gpu", "RenderThreadImpl::EstablishGpuChannel callback", flow);
         OnPotentialNewGpuChannelHost(host.get());
         std::move(callback).Run(std::move(host));
       },
       // The GPU process can crash; in that case, run the callback with no host
       // to signal the compositor to wait and try again.
       mojo::WrapCallbackWithDefaultInvokeIfNotRun(std::move(callback), nullptr),
-      this));
+      this, perfetto::TerminatingFlow::FromPointer(this)));
 }
 
 blink::AssociatedInterfaceRegistry*
