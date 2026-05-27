211db9cf29411ed6191f022c26d7c7e5e52baf92
Shunya Shishido <sisidovski@chromium.org>
2026-05-26 20:34:25 -0700

    Migrate DeclarativePerformanceObserver to content/browser/network
    This CL migrates the DeclarativePerformanceObserver implementation from
    components/page_load_metrics to content/browser/network. This
    refactoring allows us to add end-to-end web platform tests. WPTs should
    be available to be run on content_shell, which doesn't include
    components/page_load_metrics.
    
    Key changes:
    - Moved DeclarativePerformanceObserver implementation and tests to
      content/browser/network.
    - Instantiated DeclarativePerformanceObserver in WebContentsImpl::Init.
    - Removed the observer from PageLoadMetricsEmbedder registration in
      chrome/browser/page_load_metrics.
    - Added an internal tentative Web Platform Test (wpt_internal) to verify
      the basic report format for 'visibility-state'.
    
    Change-Id: I8d1581d3ed1d5c09ff44e889264a27d197f7b19a
    Bug: 505208781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863483
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636678}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/browser/features.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/observers/ad_metrics/ads_page_load_metrics_observer.h"
-#include "components/page_load_metrics/browser/observers/declarative_performance_observer.h"
 #include "components/page_load_metrics/browser/observers/paid_content_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/preload_serving_metrics_page_load_metrics_observer.h"
 #include "components/page_load_metrics/browser/observers/third_party_metrics_observer.h"
@@ -259,8 +258,6 @@ void PageLoadMetricsEmbedder::RegisterObservers(
     tracker->AddObserver(std::make_unique<ThirdPartyMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<PaidContentPageLoadMetricsObserver>());
-    tracker->AddObserver(
-        std::make_unique<page_load_metrics::DeclarativePerformanceObserver>());
 
     std::unique_ptr<page_load_metrics::PageLoadMetricsObserver> ukm_observer =
         UkmPageLoadMetricsObserver::CreateIfNeeded();
