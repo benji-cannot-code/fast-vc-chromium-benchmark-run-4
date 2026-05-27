782b86c2bb77caad8da9e4b2daa70f0c41773c5d
Alison Gale <agale@chromium.org>
2026-05-27 10:34:46 -0700

    [Vertical Tabs] Update EOH field trial and default param values
    Keeping these in sync with the current dogfood to reduce risks of
    launching the wrong thing later.
    
    Change-Id: I765884052a343411c77455cc9e660f9b188caffc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867303
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637066}

--- chrome/browser/ui/views/frame/vertical_tab_strip_region_view_browsertest.cc
+++ chrome/browser/ui/views/frame/vertical_tab_strip_region_view_browsertest.cc
@@ -1158,15 +1158,11 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripRegionViewTest,
   ASSERT_TRUE(base::test::RunUntil(
       [&]() { return state_controller()->IsCollapsed(); }));
 
-  ASSERT_FALSE(browser()->profile()->GetPrefs()->GetBoolean(
+  ASSERT_TRUE(browser()->profile()->GetPrefs()->GetBoolean(
       prefs::kVerticalTabsExpandOnHoverEnabled));
   ASSERT_FALSE(region_view()->is_expanded_on_hover());
 
   region_view()->GetFocusManager()->SetFocusedView(region_view());
-  EXPECT_FALSE(region_view()->is_expanded_on_hover());
-
-  browser()->profile()->GetPrefs()->SetBoolean(
-      prefs::kVerticalTabsExpandOnHoverEnabled, true);
   ASSERT_TRUE(base::test::RunUntil(
       [&]() { return region_view()->is_expanded_on_hover(); }));
   ASSERT_TRUE(base::test::RunUntil([&]() { return !IsAnimatingSize(); }));
@@ -1181,6 +1177,9 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripRegionViewTest,
 }
 
 IN_PROC_BROWSER_TEST_F(VerticalTabStripRegionViewTest, ModeChanged) {
+  browser()->profile()->GetPrefs()->SetBoolean(
+      prefs::kVerticalTabsExpandOnHoverEnabled, false);
+
   // Fully collapse the tabstrip.
   state_controller()->RequestCollapse(true);
   ASSERT_TRUE(base::test::RunUntil(
