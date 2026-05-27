a9faab234ca3f126db1cb65f02805f6ae11f4bf7
Ryan Sultanem <rsult@google.com>
2026-05-27 15:46:55 -0700

    [SyncPreview] Support generic parsing all sync data type statistics
    AccountPreviewData now contains a map of all possible data types (that
    the server can return) to counts.
    The response read from the server follows the proper format, and is
    generic to the available data types in the response.
    
    Bug: 510760810
    Change-Id: I978e1f0ff70921af41d8af05fe47b385e1b72798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878684
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637294}

--- components/signin/core/browser/account_preview_data.h
+++ components/signin/core/browser/account_preview_data.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/values.h"
+#include "components/sync/base/data_type.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 namespace signin {
 
@@ -27,10 +29,7 @@ struct AccountPreviewData {
   AccountPreviewData& operator=(AccountPreviewData&&) noexcept;
   ~AccountPreviewData();
 
-  // Example statistics data: counts of different items.
-  int password_count = 0;
-  int bookmark_count = 0;
-  int history_count = 0;
+  absl::flat_hash_map<syncer::DataType, size_t> counts;
 
   // Example preview data: urls for which the account has saved data.
   std::vector<std::string> password_domains;
