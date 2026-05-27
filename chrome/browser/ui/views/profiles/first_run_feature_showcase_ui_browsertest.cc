443cd5f34daa59575b0565d4d685ebfe2d60d3e0
Kacper Bajkiewicz <kbajkiewicz@google.com>
2026-05-27 03:25:45 -0700

    Reland "[FRE Revamp] Consider steps eligibility for building feature showcase URL."
    This is a reland of commit a18a564538e49b3a418f652b9e294f6c620480fc
    
    Flaky test was disabled and will be re-enabled once cmd line switch lines.
    
    Original change's description:
    > [FRE Revamp] Consider steps eligibility for building feature showcase URL.
    >
    > Add FeatureShowcaseEligibilityChecker interface and it's first implementation
    > on the example step. After this CL, feature showcase in the FRE flow
    > would display only example step and proceed to opening the browser on
    > its closure.
    >
    > FeatureShowcaseStepController redirects the flow to EvaluateEligibleSteps, waits for all eligibility checkers to finish and runs OnEligibilityDetermined with eligible steps. Maximum of 3 feature showcase steps are allowed.
    >
    > When no steps are eligible, feature showcase is skipped.
    >
    > Bug: 507795442
    > Change-Id: Ic9bec0ea3fa91cba47025a2b3a79934e9789bb69
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859384
    > Commit-Queue: Kacper Bajkiewicz <kbajkiewicz@google.com>
    > Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    > Auto-Submit: Kacper Bajkiewicz <kbajkiewicz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1636063}
    
    Bug: 507795442
    Change-Id: Ieff458622ba741cdfbc2cc5c1bbc8d138973724f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878664
    Auto-Submit: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1636823}

--- chrome/browser/ui/views/profiles/first_run_feature_showcase_ui_browsertest.cc
+++ chrome/browser/ui/views/profiles/first_run_feature_showcase_ui_browsertest.cc
@@ -125,7 +125,8 @@ class FirstRunFeatureShowcasePixelTest
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-IN_PROC_BROWSER_TEST_P(FirstRunFeatureShowcasePixelTest, InvokeUi_default) {
+IN_PROC_BROWSER_TEST_P(FirstRunFeatureShowcasePixelTest,
+                       DISABLED_InvokeUi_default) {
   ShowAndVerifyUi();
 }
 
