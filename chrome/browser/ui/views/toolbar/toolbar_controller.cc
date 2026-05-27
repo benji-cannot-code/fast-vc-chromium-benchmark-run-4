9942a49715fddddbdc8cbdf0241b9f89de7dfee3
Alison Gale <agale@chromium.org>
2026-05-27 14:13:40 -0700

    [Vertical Tabs] Add separator after the top section of overflow items
    This is below navigation and above the icons like split view and other
    pinned stuff
    
    Bug: 516841298
    Change-Id: I60cb03680bb39258843ac8d7ed4fcbaace0df3c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874772
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637225}

--- chrome/browser/ui/views/toolbar/toolbar_controller.cc
+++ chrome/browser/ui/views/toolbar/toolbar_controller.cc
@@ -251,7 +251,7 @@ ToolbarController::GetDefaultResponsiveElements(Browser* browser) {
               &(features::IsRoundedIconsEnabled() ? kSplitSceneIcon
                                                   : kSplitSceneOldIcon),
               kToolbarSplitTabsToolbarButtonElementId},
-          /*is_section_end=*/false),
+          /*is_section_end=*/true),
       ToolbarController::ResponsiveElementInfo(
           ToolbarController::ElementIdInfo{
               ContextualTasksButton::kContextualTasksToolbarButton,
