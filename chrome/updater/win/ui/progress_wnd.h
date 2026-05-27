99f2b57402c9412402ccf8095b129088629ec6cb
S Ganesh <ganesh@chromium.org>
2026-05-27 09:52:49 -0700

    updater: Implement rounded corners, frameless UI
    This change removes the frame from the progress window and implements
    rounded corners.
    
    The rounded corners are disabled in High Contrast Mode to ensure
    standard high-contrast borders are visible.
    
    Bug: 409590312
    No-Try: true
    Change-Id: I7e253eb058af9dcb72ffedfa742c5dd5c651a268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869978
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637033}

--- chrome/updater/win/ui/progress_wnd.h
+++ chrome/updater/win/ui/progress_wnd.h
@@ -60,6 +60,7 @@ class ProgressWnd : public CompleteWnd, public AppInstallProgress {
 
   CR_BEGIN_MSG_MAP_EX(ProgressWnd)
     CR_MESSAGE_HANDLER_EX(WM_INITDIALOG, OnInitDialog)
+    CR_MESSAGE_HANDLER_EX(WM_SIZE, OnSize)
     CR_MESSAGE_HANDLER_EX(WM_ERASEBKGND, OnEraseBkgnd)
     CR_MESSAGE_HANDLER_EX(WM_SYSCOLORCHANGE, OnSysColorChange)
     CR_MESSAGE_HANDLER_EX(WM_SETTINGCHANGE, OnSettingChange)
@@ -129,6 +130,7 @@ class ProgressWnd : public CompleteWnd, public AppInstallProgress {
   void OnComplete(const ObserverCompletionInfo& observer_info) override;
 
   LRESULT OnInitDialog(UINT msg, WPARAM wparam, LPARAM lparam);
+  LRESULT OnSize(UINT msg, WPARAM wparam, LPARAM lparam);
   void OnClickedButton(UINT notify_code, int id, HWND wnd_ctl);
   LRESULT OnEraseBkgnd(UINT msg, WPARAM wparam, LPARAM lparam);
   LRESULT OnSysColorChange(UINT msg, WPARAM wparam, LPARAM lparam);
@@ -144,6 +146,7 @@ class ProgressWnd : public CompleteWnd, public AppInstallProgress {
   HRESULT SetMarqueeMode(bool is_marquee);
 
   void HandleCancelRequest();
+  void UpdateWindowRgn();
 
   void DeterminePostInstallUrls(const ObserverCompletionInfo& info);
 
