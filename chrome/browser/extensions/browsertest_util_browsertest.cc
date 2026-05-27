94f96356592508aeda8c2aa6f1d087a8c78341bf
James Cook <jamescook@chromium.org>
2026-05-27 14:16:03 -0700

    extensions: Port ExtensionBrowsertestUtilTest to desktop Android
    It passes without modifications.
    
    Bug: 469417243
    Change-Id: Iae3214257287ceb12445035a72dff2af8f33077c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7877139
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637227}

--- chrome/browser/extensions/browsertest_util_browsertest.cc
+++ chrome/browser/extensions/browsertest_util_browsertest.cc
@@ -11,11 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/test/result_catcher.h"
 #include "testing/gtest/include/gtest/gtest-spi.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 namespace browsertest_util {
