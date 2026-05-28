f933c82b59b670bb864cde2d449acd95c3fbb8b0
Ewann Pellé <ewannpv@chromium.org>
2026-05-28 00:37:15 -0700

    [iOS] Re-enable Chrome Next in SmokeTestCase
    Refactors `ToolsMenuButton()` matcher in `chrome_matchers_app_interface.mm` to dynamically support both standard toolbar and NTP App Bar tools menu buttons under Chrome Next using a `grey_anyOf` matcher block.
    
    Bug: 511992708
    Change-Id: Ie325e312f4fb2f43175e82832c8c03d758f19cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876873
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637520}

--- ios/chrome/test/earl_grey/smoke_egtest.mm
+++ ios/chrome/test/earl_grey/smoke_egtest.mm
@@ -20,15 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation SmokeTestCase
 
-- (AppLaunchConfiguration)appConfigurationForTestCase {
-  AppLaunchConfiguration config = [super appConfigurationForTestCase];
-  // TODO(crbug.com/514608938): Fix test for Chrome Next.
-  if ([self isRunningTest:@selector(testTapToolsMenu)]) {
-    config.features_disabled.push_back(kChromeNextIa);
-  }
-  return config;
-}
-
 // Tests that the tools menu is tappable.
 - (void)testTapToolsMenu {
   [[EarlGrey selectElementWithMatcher:chrome_test_util::ToolsMenuButton()]
--- ios/chrome/test/earl_grey2/smoke_egtest.mm
+++ ios/chrome/test/earl_grey2/smoke_egtest.mm
@@ -31,16 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation SmokeTestCase
 
-- (AppLaunchConfiguration)appConfigurationForTestCase {
-  AppLaunchConfiguration config = [super appConfigurationForTestCase];
-  // TODO(crbug.com/514608938): Fix test for Chrome Next.
-  if ([self isRunningTest:@selector(testAppResourcesArePresent)] ||
-      [self isRunningTest:@selector(testTapToolsMenu)]) {
-    config.features_disabled.push_back(kChromeNextIa);
-  }
-  return config;
-}
-
 // Tests that a tab can be opened.
 - (void)testOpenTab {
   // Open tools menu.
@@ -125,8 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Tests that string resources are loaded into the ResourceBundle and available
 // for use in tests.
 - (void)testAppResourcesArePresent {
-  NSString* settingsLabel = l10n_util::GetNSString(IDS_IOS_TOOLBAR_SETTINGS);
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(settingsLabel)]
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::ToolsMenuButton()]
       assertWithMatcher:grey_sufficientlyVisible()];
 }
 
