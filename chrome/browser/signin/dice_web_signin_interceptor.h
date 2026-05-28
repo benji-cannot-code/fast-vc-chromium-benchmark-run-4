9c1bf7ec31e564aaffa625d93c0653d7431229ac
Monica Salama <msalama@chromium.org>
2026-05-28 06:41:45 -0700

    [Connected Accounts] Add terminal signaling to DiceSigninSession
    This CL introduces a terminal state completion signal in
    DiceSigninSession to notify delegates when the multi-account token
    exchange session has finished.
    
    It adds `OnDiceSigninSessionComplete(secondary_accounts)` to the
    `ProcessDiceHeaderDelegate` interface and implements it in
    `ProcessDiceHeaderDelegateImpl` to forward the signal to
    `DiceWebSigninInterceptor`.
    
    The completion signal will be handled by the interceptor in the
    follow-up CL (https://crrev.com/c/7867766/) to wait for fetches.
    
    Bug: 475436293
    
    Change-Id: I57a6208a230de1e2e0f15ffd07f82b37e76c1e2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870906
    Commit-Queue: Monica Salama <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637662}

--- chrome/browser/signin/dice_web_signin_interceptor.h
+++ chrome/browser/signin/dice_web_signin_interceptor.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "base/cancelable_callback.h"
 #include "base/functional/callback_forward.h"
@@ -115,6 +116,10 @@ class DiceWebSigninInterceptor : public KeyedService,
                                        signin_metrics::AccessPoint access_point,
                                        bool is_new_account,
                                        bool is_sync_signin);
+
+  void OnDiceSigninSessionComplete(
+      const CoreAccountId& initiator_account_id,
+      std::vector<CoreAccountId> secondary_accounts);
 
   // Called after the new profile was created during a signin interception.
   // The token has been moved to the new profile, but the account is not yet in
