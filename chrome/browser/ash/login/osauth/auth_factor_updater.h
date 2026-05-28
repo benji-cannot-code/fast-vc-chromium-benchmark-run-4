5005479ab3cad6319d9345f59e894bffeacddd55
Jun Ishiguro <junis@google.com>
2026-05-27 20:27:52 -0700

    Reland "login: Pass local state PrefService to PinSaltStorageImpl"
    This is a reland of commit fe045873dbc8dbe05a4d53d3ba449bb8609a659c
    
    Original change's description
    > login: Pass local state PrefService to PinSaltStorageImpl
    >
    > Bug: b:404133029
    > Change-Id: I53fb5b664b7053ca493b999e064e3c2199227284
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789310
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Jun Ishiguro <junis@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1634161}
    
    Bug: b:404133029
    Change-Id: I98ca43e5c3962f2ce5fc9b86cb1fecc2c78aac1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7875403
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637444}

--- chrome/browser/ash/login/osauth/auth_factor_updater.h
+++ chrome/browser/ash/login/osauth/auth_factor_updater.h
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ash/login/osauth/auth_factor_migrator.h"
 #include "chrome/browser/ash/login/osauth/auth_policy_enforcer.h"
 #include "chromeos/ash/components/login/auth/public/auth_callbacks.h"
 #include "chromeos/ash/components/login/auth/public/user_context.h"
+
+class PrefService;
 
 namespace ash {
 
@@ -23,9 +26,10 @@ class UserContext;
 // `AuthFactorMigrator` and applies policies by running `AuthPolicyEnforcer`.
 class AuthFactorUpdater {
  public:
-  AuthFactorUpdater(AuthPolicyConnector* connector,
-                    UserDataAuthClient* user_data_auth,
-                    PrefService* local_state);
+  // `local_state` must be non-null and must outlive `this`.
+  AuthFactorUpdater(PrefService* local_state,
+                    AuthPolicyConnector* connector,
+                    UserDataAuthClient* user_data_auth);
   ~AuthFactorUpdater();
 
   AuthFactorUpdater(const AuthFactorUpdater&) = delete;
@@ -40,9 +44,9 @@ class AuthFactorUpdater {
                      std::unique_ptr<UserContext> context,
                      std::optional<AuthenticationError> error);
 
+  const raw_ref<PrefService> local_state_;
   raw_ptr<AuthPolicyConnector> connector_;
   raw_ptr<UserDataAuthClient> user_data_auth_;
-  raw_ptr<PrefService> local_state_;
   std::unique_ptr<AuthFactorMigrator> auth_factor_migrator_;
   std::unique_ptr<AuthPolicyEnforcer> auth_policy_enforcer_;
   // Must be the last member.
--- chrome/browser/ash/login/quick_unlock/pin_salt_storage.h
+++ chrome/browser/ash/login/quick_unlock/pin_salt_storage.h
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/memory/raw_ref.h"
+
 class AccountId;
+class PrefService;
 
 namespace ash {
 namespace quick_unlock {
@@ -24,7 +27,8 @@ class PinSaltStorage {
 
 class PinSaltStorageImpl : public PinSaltStorage {
  public:
-  PinSaltStorageImpl();
+  // `local_state` must be non-null and must outlive `this`.
+  explicit PinSaltStorageImpl(PrefService* local_state);
   PinSaltStorageImpl(const PinSaltStorageImpl&) = delete;
   PinSaltStorageImpl& operator=(const PinSaltStorageImpl&) = delete;
   ~PinSaltStorageImpl() override;
@@ -32,6 +36,9 @@ class PinSaltStorageImpl : public PinSaltStorage {
   // PinSaltStorage overrides:
   std::string GetSalt(const AccountId& account_id) const override;
   void WriteSalt(const AccountId& account_id, const std::string& salt) override;
+
+ private:
+  const raw_ref<PrefService> local_state_;
 };
 
 }  // namespace quick_unlock
