329c98a5b66c303a58bb5f70307047b84efe4991
Andrea Gomez <andreaxg@google.com>
2026-05-28 16:54:51 -0700

    Scope Glic application hotkey delegate to the profile
    Scopes the hotkey registration to the specific profile associated with
    the Glic instance.
    
    Bug: 515124988
    Change-Id: I1204c2a4af976bcc443b120cef3070b12cbd9a67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7862006
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638061}

--- chrome/browser/glic/common/application_hotkey_delegate.h
+++ chrome/browser/glic/common/application_hotkey_delegate.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GLIC_COMMON_APPLICATION_HOTKEY_DELEGATE_H_
 #define CHROME_BROWSER_GLIC_COMMON_APPLICATION_HOTKEY_DELEGATE_H_
 
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/glic/common/local_hotkey_manager.h"
 #include "ui/base/accelerators/accelerator.h"
+
+class Profile;
 
 namespace glic {
 
@@ -20,13 +23,16 @@ namespace glic {
 class ApplicationScopedRegistrationDelegate
     : public LocalHotkeyManager::RegistrationDelegate {
  public:
-  ApplicationScopedRegistrationDelegate();
+  explicit ApplicationScopedRegistrationDelegate(Profile* profile);
   ~ApplicationScopedRegistrationDelegate() override;
 
   std::unique_ptr<LocalHotkeyManager::ScopedHotkeyRegistration>
   CreateScopedHotkeyRegistration(
       ui::Accelerator accelerator,
       base::WeakPtr<ui::AcceleratorTarget> target) override;
+
+ private:
+  raw_ptr<Profile> profile_;
 };
 
 }  // namespace glic
