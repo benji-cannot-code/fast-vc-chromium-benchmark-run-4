2a50b2a6a4cdb1e7ff652efee6d65b8f822016c6
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-05-28 07:22:55 -0700

    Replace hardcoded default browser URL with constant
    Bug: 454597786
    Change-Id: I2b912bfebacc063617f7c73ceb55a8b1bb6abfe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882118
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637689}

--- chrome/browser/default_browser/default_browser_features.cc
+++ chrome/browser/default_browser/default_browser_features.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "chrome/common/webui_url_constants.h"
 #include "url/gurl.h"
 
 namespace default_browser {
@@ -44,9 +45,7 @@ DefaultBrowserSetterType GetDefaultBrowserSetterType() {
 
 GURL GetDefaultBrowserVisualGuideURL() {
   if (!base::FeatureList::IsEnabled(kDefaultBrowserSetterSelection)) {
-    // TODO(https://crbugs.com/454597786): Replace this with the const webui
-    // url.
-    GURL("chrome://default-browser/");
+    GURL(kChromeUIDefaultBrowserVisualGuidedSetterURL);
   }
 
   return GURL(kDefaultBrowserVisualGuideUrlParam.Get());
