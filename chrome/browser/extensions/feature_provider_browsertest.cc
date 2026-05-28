76537543fad652b6dcd3fd9398bb3ec1df2b9a12
James Cook <jamescook@chromium.org>
2026-05-27 20:12:52 -0700

    extensions: Re-enable FeatureProviderBrowserTest on desktop Android
    It was removed from desktop Android (possibly accidentally) in some
    agent-generated build file dependency refactoring:
    https://chromium-review.googlesource.com/c/chromium/src/+/7858342
    
    The test still passes and gn check doesn't show any problems with
    circular dependencies.
    
    Bug: 469417243
    Change-Id: I1bd538215ca7a7311c666880ae8e3a4ab89045cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880690
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637430}

--- chrome/browser/extensions/feature_provider_browsertest.cc
+++ chrome/browser/extensions/feature_provider_browsertest.cc
@@ -11,9 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_test_util.h"
 #include "chrome/test/base/platform_browser_test.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extensions_client.h"
 #include "extensions/common/features/complex_feature.h"
 #include "extensions/test/test_extensions_client.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 namespace {
