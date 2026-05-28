630f37ad775a645b813e4b7d0a6984d76ea2efdb
Ewann Pellé <ewannpv@chromium.org>
2026-05-28 04:09:07 -0700

    [iOS] Re-enable Chrome Next in WebShareTestCase
    Bug: 514608938
    Change-Id: I458e559fdb04f198bad6bf074b7323aa45b515fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883058
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637596}

--- ios/chrome/browser/web/model/web_share_egtest.mm
+++ ios/chrome/browser/web/model/web_share_egtest.mm
@@ -97,7 +97,6 @@ void TapWebShareButton() {
 
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config = [super appConfigurationForTestCase];
-  config.features_disabled.push_back(kChromeNextIa);
   return config;
 }
 
