99a00a43440185a8e96e3762e49c4e47a6087217
Matt Reynolds <mattreynolds@google.com>
2026-05-27 17:54:20 -0700

    contacts: Avoid handling user actions without an active picker
    Introduces a flag mFlowActive which is set to true when the contacts
    picker dialog is shown and set to false when the flow is complete,
    either because it was canceled or a selection was returned.
    
    If a user action occurs when there is no active contact picker flow,
    ignore it and avoid calling any callbacks.
    
    Bug: 510582730
    Change-Id: I3828c60f1335c0e9f0fcd6b91ef8f3cc491691f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876117
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637360}

--- content/public/android/java/src/org/chromium/content_public/browser/ContactsDialogHost.java
+++ content/public/android/java/src/org/chromium/content_public/browser/ContactsDialogHost.java
@@ -29,6 +29,7 @@ public class ContactsDialogHost implements ContactsPickerListener {
 
     private long mNativeContactsProviderAndroid;
     private final WebContents mWebContents;
+    private boolean mFlowActive = true;
 
     private static @Nullable ContactsPermissionProvider sContactsPermissionProvider;
 
@@ -70,9 +71,11 @@ public class ContactsDialogHost implements ContactsPickerListener {
             boolean includeAddresses,
             boolean includeIcons,
             String formattedOrigin) {
+        mFlowActive = true;
         if (sContactsPermissionProvider == null) {
             Log.e(TAG, "Permission provider not set");
             ContactsDialogHostJni.get().endWithPermissionDenied(mNativeContactsProviderAndroid);
+            mFlowActive = false;
         } else {
             ContactsPickerListener listener = this;
 
@@ -101,9 +104,10 @@ public class ContactsDialogHost implements ContactsPickerListener {
 
                         @Override
                         public void onDenied() {
-                            if (isDestroyed()) {
+                            if (isDestroyed() || !mFlowActive) {
                                 return;
                             }
+                            mFlowActive = false;
                             ContactsDialogHostJni.get()
                                     .endWithPermissionDenied(mNativeContactsProviderAndroid);
                         }
@@ -118,16 +122,18 @@ public class ContactsDialogHost implements ContactsPickerListener {
             int percentageShared,
             int propertiesSiteRequested,
             int propertiesUserRejected) {
-        if (mNativeContactsProviderAndroid == 0) return;
+        if (mNativeContactsProviderAndroid == 0 || !mFlowActive) return;
 
         switch (action) {
             case ContactsPickerAction.CANCEL:
+                mFlowActive = false;
                 ContactsDialogHostJni.get()
                         .endContactsList(
                                 mNativeContactsProviderAndroid, 0, propertiesSiteRequested);
                 break;
 
             case ContactsPickerAction.CONTACTS_SELECTED:
+                mFlowActive = false;
                 assumeNonNull(contacts);
                 for (Contact contact : contacts) {
                     ContactsDialogHostJni.get()
