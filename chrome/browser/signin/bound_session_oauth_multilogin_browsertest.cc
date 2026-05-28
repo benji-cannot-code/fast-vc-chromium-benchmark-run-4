a00798f311d6e952d11c09f754b28f4b6cb6b23b
Dan Harrington <harringtond@chromium.org>
2026-05-27 17:20:22 -0700

    Add GlicCookieSyncOnTokenChange to fieldtrial_testing_config.json
    Bug: b:496202766
    Link: https://chromium-review.googlesource.com/id/I62338edc2d555957fa9080f2e01a39af6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871261
    Auto-Submit: Dan H <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637346}

--- chrome/browser/signin/bound_session_oauth_multilogin_browsertest.cc
+++ chrome/browser/signin/bound_session_oauth_multilogin_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/glic/host/glic_cookie_synchronizer.h"
+#include "chrome/browser/glic/public/features.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_cookie_refresh_service.h"
@@ -181,7 +182,12 @@ class BoundSessionOAuthMultiloginBaseTest
       const std::vector<base::test::FeatureRef>& disabled_features) {
     std::vector<base::test::FeatureRef> all_enabled_features = enabled_features;
     all_enabled_features.push_back(switches::kEnableChromeRefreshTokenBinding);
-    feature_list_.InitWithFeatures(all_enabled_features, disabled_features);
+    std::vector<base::test::FeatureRef> all_disabled_features =
+        disabled_features;
+    // Disable automatic syncing of cookies to the glic partition, which breaks
+    // some assertions in this test suite.
+    all_disabled_features.push_back(features::kGlicCookieSyncOnTokenChange);
+    feature_list_.InitWithFeatures(all_enabled_features, all_disabled_features);
   }
 
   ~BoundSessionOAuthMultiloginBaseTest() override = default;
