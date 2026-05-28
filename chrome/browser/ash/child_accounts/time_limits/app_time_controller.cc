838b8eda80eda7f1009ad1a2e1962d8bf4814e49
yoshiki iguchi <yoshiki@google.com>
2026-05-27 20:45:20 -0700

    ASH Refactoring: Move IDS_APP_TIME_LIMIT_* to ash/
    Move the following IDS_APP_TIME_LIMIT_* strings from
    chrome/app/chromeos_strings.grdp
    to ash/ash_strings.grd as they are only used in ash
    (chrome/browser/ash/child_accounts/time_limits/app_time_controller.cc):
    
    - IDS_APP_TIME_LIMIT_APP_BLOCKED_NOTIFICATION_MESSAGE
    - IDS_APP_TIME_LIMIT_APP_AVAILABLE_NOTIFICATION_MESSAGE
    - IDS_APP_TIME_LIMIT_APP_WILL_PAUSE_SYSTEM_NOTIFICATION_TITLE
    - IDS_APP_TIME_LIMIT_APP_WILL_PAUSE_SYSTEM_NOTIFICATION_MESSAGE
    - IDS_APP_TIME_LIMIT_APP_TIME_LIMIT_SET_SYSTEM_NOTIFICATION_TITLE
    - IDS_APP_TIME_LIMIT_APP_TIME_LIMIT_SET_SYSTEM_NOTIFICATION_MESSAGE
    - IDS_APP_TIME_LIMIT_APP_TIME_LIMIT_REMOVED_SYSTEM_NOTIFICATION_MESSAGE
    
    Update app_time_controller.cc includes, and BUILD.gn/DEPS accordingly.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: I11f776135b6e7c9fb212a187290ca03e14cb3529
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870423
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637450}

--- chrome/browser/ash/child_accounts/time_limits/app_time_controller.cc
+++ chrome/browser/ash/child_accounts/time_limits/app_time_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_pref_names.h"
 #include "ash/constants/notifier_catalogs.h"
 #include "ash/public/cpp/notification_utils.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
