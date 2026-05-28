395bb48d5586c0cb12af821d1e055c93dc4f4393
Ivana Žužić <izuzic@google.com>
2026-05-28 11:56:57 -0700

    [Gardener] Wait for regular activity to be destroyed in LauncherShortcutTest
    Add a call to waitForActivityState to ensure the regular activity is
    fully destroyed after calling finishAndRemoveTask, preventing potential
    test flakiness.
    
    Bug: 517535599
    Change-Id: I0180f382a64794f25ecc6d06c3c9e4746564c43d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881244
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Ivana Žužić <izuzic@google.com>
    Owners-Override: Ivana Žužić <izuzic@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637853}

--- chrome/android/javatests/src/org/chromium/chrome/browser/LauncherShortcutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/LauncherShortcutTest.java
@@ -131,6 +131,7 @@ public class LauncherShortcutTest {
                 () -> {
                     regularActivity.finishAndRemoveTask();
                 });
+        ApplicationTestUtils.waitForActivityState(regularActivity, Stage.DESTROYED);
 
         TabModelSelector incognitoSelector = incognitoActivity.getTabModelSelector();
         int initialTabCount =
