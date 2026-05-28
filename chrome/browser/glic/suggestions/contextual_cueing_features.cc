9596b74a716341a6d316849db0dd2c6d0211f144
Christian Dullweber <dullweber@chromium.org>
2026-05-28 09:39:44 -0700

    PrivateAi: Delay prewarm until long hover
    Add a delay until prewarm is triggered to avoid false positives from
    just moving the mouse over the icon.
    
    Bug: 515675850
    Change-Id: I1abf62fc70197a21f04df195c952d311777cc424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876389
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637759}

--- chrome/browser/glic/suggestions/contextual_cueing_features.cc
+++ chrome/browser/glic/suggestions/contextual_cueing_features.cc
@@ -157,4 +157,10 @@ const base::FeatureParam<int> kMaxPinnedPagesForTriggeringSuggestions(
     "ZSSMaxPinnedPagesForTriggeringSuggestions",
     10);
 
+const base::FeatureParam<base::TimeDelta>
+    kZeroStateSuggestionsPrivateAiPrewarmDelay(
+        &kZeroStateSuggestionsUsePrivateAi,
+        "ZSSPrivateAiPrewarmDelay",
+        base::Milliseconds(150));
+
 }  // namespace glic
--- chrome/browser/ui/views/glic/glic_button.h
+++ chrome/browser/ui/views/glic/glic_button.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
+#include "base/timer/timer.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/background/glic/glic_launcher_configuration.h"
@@ -348,9 +349,15 @@ class GlicButton : public GlicBaseShim<T>,
   void StateChanged(views::Button::ButtonState old_state) override {
     T::StateChanged(old_state);
 
-    if (old_state == views::Button::ButtonState::STATE_NORMAL &&
-        this->GetState() == views::Button::ButtonState::STATE_HOVERED) {
-      EstablishPrivateAiConnection(profile_);
+    if (this->GetState() == views::Button::ButtonState::STATE_HOVERED) {
+      if (old_state == views::Button::ButtonState::STATE_NORMAL) {
+        prewarm_timer_.Start(FROM_HERE,
+                             kZeroStateSuggestionsPrivateAiPrewarmDelay.Get(),
+                             base::BindOnce(&GlicButton::OnPrewarmTimerFired,
+                                            weak_ptr_factory_.GetWeakPtr()));
+      }
+    } else if (this->GetState() != views::Button::ButtonState::STATE_PRESSED) {
+      prewarm_timer_.Stop();
     }
 
     UpdateTextAndBackgroundColors();
@@ -450,6 +457,10 @@ class GlicButton : public GlicBaseShim<T>,
   }
 
   bool GetLabelEnabledForTesting() const { return this->label()->GetEnabled(); }
+
+  bool IsPrewarmTimerRunningForTesting() const {
+    return prewarm_timer_.IsRunning();
+  }
 
   // Updates the background painter to match the current border insets.
   void RefreshBackground() { UpdateColors(); }
@@ -639,6 +650,8 @@ class GlicButton : public GlicBaseShim<T>,
     // Setting label text seems to clear the margin. Set it again.
     SetLabelMargins();
   }
+
+  void OnPrewarmTimerFired() { EstablishPrivateAiConnection(profile_); }
 
   void NotifyClick(const ui::Event& event) override {
     if (base::FeatureList::IsEnabled(features::kGlicButtonPressedState)) {
@@ -882,6 +895,8 @@ class GlicButton : public GlicBaseShim<T>,
   // Window active and inactive subscriptions for changing the hover color.
   base::CallbackListSubscription window_did_become_active_subscription_;
   base::CallbackListSubscription window_did_become_inactive_subscription_;
+
+  base::OneShotTimer prewarm_timer_;
 
   base::WeakPtrFactory<GlicButton> weak_ptr_factory_{this};
 };
