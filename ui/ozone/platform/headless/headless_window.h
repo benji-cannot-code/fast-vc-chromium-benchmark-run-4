538501914287186b357cf5d4dabd26488e3907d1
Peter Kvitek <kvitekp@chromium.org>
2026-05-28 12:14:28 -0700

    [headless][linux] Safeguard headless window code against potential UAF
    ui::HeadlessWindow synchronously callsbacks delegate methods during state-changing operations. If an observer synchronously closes the widget during these callbacks, the associated ui::HeadlessWindow instance is deleted while its methods are still executing on the stack.
    
    This CL adds weak ptr checks to avoid ui::HeadlessWindow code execution after a delegate callback that invalidates 'this'.
    
    Bug: 516674532
    Change-Id: I788ba9b19c733c7af17010744bd66b25783ddf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7879467
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637863}

--- ui/ozone/platform/headless/headless_window.h
+++ ui/ozone/platform/headless/headless_window.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/image/image_skia.h"
@@ -74,6 +75,10 @@ class HeadlessWindow : public PlatformWindow {
  protected:
   PlatformWindowDelegate* delegate() { return delegate_; }
 
+  base::WeakPtr<HeadlessWindow> GetWeakPtr() {
+    return weak_ptr_factory_.GetWeakPtr();
+  }
+
  private:
   enum class ActivationState {
     kUnknown,
@@ -91,6 +96,8 @@ class HeadlessWindow : public PlatformWindow {
   std::optional<gfx::Rect> restored_bounds_;
   PlatformWindowState window_state_ = PlatformWindowState::kUnknown;
   ActivationState activation_state_ = ActivationState::kUnknown;
+
+  base::WeakPtrFactory<HeadlessWindow> weak_ptr_factory_{this};
 };
 
 }  // namespace ui
