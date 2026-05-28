e41105a099f719bac05c3462571f3f8fac125755
Eric Ekey <ericekey@google.com>
2026-05-28 12:46:35 -0700

    [iOS][AppBar] Fix Horizontal Scroll Indicator Visible on NTP
    When the Discover feed is hidden, the NTP utilizes a native
    emptyCollectionView which defaults to having horizontal scroll
    indicators enabled. With ChromeNextIa, when the discover feed is
    disabled horizontal scroll overflow may be triggered and cause a
    horizontal scroll indicator to be shown in landscape orientation on the
    NTP. This change ensures it is hidden.
    
    Fixed: 517458031
    Change-Id: I2fc4046ec4bae6fe443715b69357397c5a817767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883219
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637888}

--- ios/chrome/browser/ntp/ui_bundled/feed_wrapper_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/feed_wrapper_view_controller.mm
@@ -87,6 +87,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[UICollectionView alloc] initWithFrame:CGRectZero
                          collectionViewLayout:layout];
   [emptyCollectionView setShowsVerticalScrollIndicator:NO];
+  if (IsChromeNextIaEnabled()) {
+    [emptyCollectionView setShowsHorizontalScrollIndicator:NO];
+  }
   [self.view addSubview:emptyCollectionView];
   self.contentCollectionView = emptyCollectionView;
   self.contentCollectionView.backgroundColor = [UIColor clearColor];
