65b989c01f75235086dad8eda68c74fd975d0abc
Philip Rogers <pdr@chromium.org>
2026-05-28 08:05:06 -0700

    Use an infinite rect when mapping unbuilt filters
    https://crrev.com/1636239 and https://crrev.com/1636071 introduced a
    CHECK that paint properties are clean when mapping rects for filters,
    because filters are built as part of updating paint properties. This
    CHECK hits in scenarios where a filter references a later object which
    has not yet built property trees, which is an instance of a known
    issue (https://crbug.com/40578621). This patch removes the newly-added
    CHECK and instead uses an infinite rect in this scenario for the
    worst-case filter bounds, to prevent any optimizations based on those
    bounds.
    
    Fixed: 517036885
    Bug: 40578621
    Change-Id: I157cbb7a58f523786cfb1d32ca46aa6d316d4649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881704
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637707}

--- third_party/blink/renderer/core/layout/svg/svg_layout_support.cc
+++ third_party/blink/renderer/core/layout/svg/svg_layout_support.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/core/svg/svg_length_functions.h"
+#include "third_party/blink/renderer/platform/geometry/infinite_int_rect.h"
 #include "third_party/blink/renderer/platform/geometry/stroke_data.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/clear_collection_scope.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
@@ -103,9 +104,14 @@ gfx::RectF SVGLayoutSupport::LocalVisualRect(const LayoutObject& object) {
 
 gfx::RectF SVGLayoutSupport::ApplyFiltersToRect(const LayoutObject& object,
                                                 const gfx::RectF& rect) {
-  CHECK(!object.NeedsPaintPropertyUpdate());
   if (!object.StyleRef().HasFilter()) {
     return rect;
+  }
+  if (object.NeedsPaintPropertyUpdate()) {
+    // TODO(crbug.com/40578621): This can happen when we calculate visual
+    // overflow for an object that has not yet built paint properties. Assume
+    // the worst-case and return an infinite rect.
+    return gfx::RectF(InfiniteIntRect());
   }
   gfx::RectF float_rect = rect;
   const gfx::RectF filter_reference_box =
