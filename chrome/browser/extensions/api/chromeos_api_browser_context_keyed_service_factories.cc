8bc02df1b5dc082964f247b7eb1b167b97326eed
dpapad <dpapad@chromium.org>
2026-05-28 05:09:01 -0700

    CrOS: Delete obsolete "Contact Center Insights" component extension.
    Bug: b:269766156
    Change-Id: Ie09b16d3eb5cfeae2bbc54cb8c3af3039e661e12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876074
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637624}

--- chrome/browser/extensions/api/chromeos_api_browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/api/chromeos_api_browser_context_keyed_service_factories.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/extensions/install_limiter_factory.h"
 #include "chrome/browser/ash/extensions/speech/speech_recognition_private_manager_factory.h"
 #include "chrome/browser/ash/extensions/users_private/users_private_delegate_factory.h"
-#include "chrome/browser/chromeos/extensions/contact_center_insights/contact_center_insights_extension_manager_factory.h"
 #include "chrome/browser/chromeos/extensions/desk_api/desk_api_extension_manager_factory.h"
 #include "chrome/browser/chromeos/extensions/login_screen/login_state/session_state_changed_event_dispatcher.h"
 #include "chrome/browser/chromeos/extensions/smart_card_provider_private/smart_card_provider_private_api.h"
@@ -19,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos_extensions {
 
 void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
-  chromeos::ContactCenterInsightsExtensionManagerFactory::GetInstance();
   chromeos::DeskApiExtensionManagerFactory::GetInstance();
   chromeos::ExtensionPlatformKeysServiceFactory::GetInstance();
   chromeos::VpnServiceFactory::GetInstance();
--- chrome/browser/resources/chromeos/contact_center_insights/background.js
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// We have graduated all of the metrics we used to collect using this background
-// service worker.
-// TODO (b/269766156): Delete this file and update the manifest once it is
-// decoupled from grit.
