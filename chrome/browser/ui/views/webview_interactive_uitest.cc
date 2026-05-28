29d8fa1f9ebb1f5237566ca774a392530082161c
Keren Zhu <kerenzhu@chromium.org>
2026-05-27 20:17:44 -0700

    win: Fix WebViewInteractiveUiTest timeouts.
    Chrome's window can by covered by windows owned by another process. The
    WM_MOUSEMOVE will go that that process, not chrome. We've seen HWNDs in
    bots owned by text_input_host.exe or explorer.exe. They do not accept
    SetWindowPos or PostMessage from Chrome.
    
    This is causing timeout in
    WebViewInteractiveUiTest.MouseMoveEventDelivered which sends and waits
    for mouse events.
    
    This CL posts WM_MOUSEMOVE directly to Chrome's HWND and monitors its
    delivery via a SetWindowSubclass message observer in the above known
    cases.
    
    TEST=WebViewInteractiveUiTest.MouseMoveEventDelivered
    
    Bug: 510815056
    Change-Id: I57dbb20eb84b69a7f1e6a20448daa803066ce251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838021
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637438}

--- chrome/browser/ui/views/webview_interactive_uitest.cc
+++ chrome/browser/ui/views/webview_interactive_uitest.cc
@@ -31,8 +31,6 @@ class WebViewInteractiveUiTest : public InProcessBrowserTest {
   }
 };
 
-// TODO(crbug.com/503006729): this test times out on Windows.
-#if !BUILDFLAG(IS_WIN)
 IN_PROC_BROWSER_TEST_F(WebViewInteractiveUiTest, MouseMoveEventDelivered) {
   // Create a widget with a WebView.
   TestingProfile testing_profile;
@@ -97,6 +95,5 @@ IN_PROC_BROWSER_TEST_F(WebViewInteractiveUiTest, MouseMoveEventDelivered) {
   EXPECT_NEAR(expected_x, mouse_x, 2);
   EXPECT_NEAR(expected_y, mouse_y, 2);
 }
-#endif  // !BUILDFLAG(IS_WIN)
 
 }  // namespace views
