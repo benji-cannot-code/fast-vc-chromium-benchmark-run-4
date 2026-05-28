00dd760c045668a494e496bb5c0ef1b4cbcc10d4
Dominik Röttsches <drott@chromium.org>
2026-05-28 14:14:04 -0700

    Vendor HarfRust and build HarfRust shaping backend in HarfBuzz
    Third-party review approved in:
    https://groups.google.com/a/google.com/g/chrome-atls-discuss/c/CWb6UNPdrW0/m/fQHEidz-CQAJ
    
    We add the HarfRust Rust implementation of HarfBuzz to develop
    towards pixel-perfect matching of the existing shaping behavior
    and to evaluate performance compared to the exist C++ shaper
    library, with the aim to eventually migrate over. More details
    in the third-party review above.
    
    Bug: 427409139
    Binary-Size: Rust version added for performance and compat evaluation.
    Fuchsia-Binary-Size: See commit message.
    Change-Id: I0e73998d23073d79822ef359837945b7445e2252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866312
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637948}

---
+++ third_party/rust/chromium_crates_io/vendor/harfrust-v0_8/src/hb/ot/gsub/mod.rs
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! OpenType GSUB lookups.
+
+mod alternate;
+mod ligature;
+mod multiple;
+mod reverse_chain;
+mod single;
