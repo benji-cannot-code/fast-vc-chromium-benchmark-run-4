b6607e2a7d3f3a2fccf9a96c2dcc5c4304ce05e6
Alison Gale <agale@chromium.org>
2026-05-28 08:24:46 -0700

    [Vertical Tabs] Adjust glic button padding for VT
    The is the padding to the left of the icon and to the right of the text.
    Still working on the padding between glic and actor icons when both are
    present.
    
    Bug: 510345555
    Change-Id: I59d77299aae01e54c5c2e4548a183085472ecbe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880869
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637712}

--- chrome/browser/ui/views/toolbar/toolbar_glic_button.cc
+++ chrome/browser/ui/views/toolbar/toolbar_glic_button.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 constexpr int kCloseButtonSize = 16;
 constexpr int kSpaceBetweenButtons = 2;
+constexpr int kLabelToolbarButtonRightMargin = 6;
+constexpr int kIconToolbarButtonLeftMargin = 3;
 }  // namespace
 
 namespace glic {
@@ -49,7 +51,7 @@ ToolbarGlicButton::ToolbarGlicButton(
   auto* image_view = static_cast<views::ImageView*>(image_container_view());
   image_view->SetImageSize({kToolbarGlicIconSize, kToolbarGlicIconSize});
   image_view->SetProperty(views::kMarginsKey,
-                          gfx::Insets().set_left(kIconLeftMargin));
+                          gfx::Insets().set_left(kIconToolbarButtonLeftMargin));
 
   SetLabelMargins();
 
@@ -152,6 +154,16 @@ void ToolbarGlicButton::UpdateStyle(bool should_match_toolbar) {
 void ToolbarGlicButton::SetCloseButtonFocusBehavior(
     views::View::FocusBehavior focus_behavior) {}
 
+void ToolbarGlicButton::SetLabelMargins() {
+  const bool close_button_visible =
+      close_button() && close_button()->GetVisible();
+  this->label()->SetProperty(
+      views::kMarginsKey,
+      gfx::Insets().set_right(close_button_visible
+                                  ? kLabelWithCloseButtonRightMargin
+                                  : kLabelToolbarButtonRightMargin));
+}
+
 void ToolbarGlicButton::AddCloseButton(PressedCallback pressed_callback) {
   auto close_button =
       std::make_unique<views::LabelButton>(std::move(pressed_callback));
@@ -242,8 +254,9 @@ void ToolbarGlicButton::OnBoundsChanged(const gfx::Rect& previous_bounds) {
   } else {
     if (!label()->GetVisible()) {
       label()->SetVisible(true);
-      image_view->SetProperty(views::kMarginsKey,
-                              gfx::Insets().set_left(kIconLeftMargin));
+      image_view->SetProperty(
+          views::kMarginsKey,
+          gfx::Insets().set_left(kIconToolbarButtonLeftMargin));
       box_layout->set_main_axis_alignment(
           views::BoxLayout::MainAxisAlignment::kCenter);
     }
