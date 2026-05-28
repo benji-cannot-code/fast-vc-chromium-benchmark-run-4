530742e3b5f2b2379c0a68e9ec5aaf3d43cb9020
Edgar Aguilar <eledgar@google.com>
2026-05-28 14:06:34 -0700

    [Fuzzing] Fix CommandLine init in protocol_serializer fuzzer
    Avoid crashing due to double initialization of CommandLine on Android, where it might already be initialized by the test runner.
    
    Check if CommandLine is already initialized before calling Init.
    
    Bug: 516909782
    
    R=titouan@chromium.org
    
    Test: Built and executed successfully on an Android emulator.
    Change-Id: I28e772e998b9ccb51f2c9c2e06438e3c6da696d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876075
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Edgar Aguilar <eledgar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637942}

--- components/update_client/protocol_serializer_fuzzer.cc
+++ components/update_client/protocol_serializer_fuzzer.cc
@@ -23,7 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/protocol_handler.h"
 
 struct Environment {
-  Environment() { CHECK(base::CommandLine::Init(0, nullptr)); }
+  Environment() {
+    // Avoid crashing if CommandLine is already initialized, which can happen on
+    // Android.
+    if (!base::CommandLine::InitializedForCurrentProcess()) {
+      CHECK(base::CommandLine::Init(0, nullptr));
+    }
+  }
 };
 
 namespace update_client {
