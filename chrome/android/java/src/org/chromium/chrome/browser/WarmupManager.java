52134950bd4a23c466344d182ef069eafd0c91bd
Andrew Grieve <agrieve@chromium.org>
2026-05-27 20:29:06 -0700

    Refactor AsyncLayoutInflater to not use a dedicated thread
    - Forks AndroidX's AsyncViewInflater to have not use a forever thread
    - Adds JavaDoc about the gotchas of async view inflation
    - Removes androidx_asynclayoutinflater from androidx deps
    
    Bug: 465145691, 40937701
    Change-Id: I8d7d102b64f55b485f05d04ac7a3eded18eb86f1
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869110
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637445}

--- chrome/android/java/src/org/chromium/chrome/browser/WarmupManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/WarmupManager.java
@@ -20,7 +20,6 @@ import android.util.ArraySet;
 import android.util.DisplayMetrics;
 import android.view.ContextThemeWrapper;
 import android.view.InflateException;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
@@ -28,7 +27,6 @@ import android.widget.FrameLayout;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
-import androidx.asynclayoutinflater.appcompat.AsyncAppCompatFactory;
 import androidx.core.content.res.ResourcesCompat;
 
 import org.jni_zero.JniType;
@@ -60,7 +58,7 @@ import org.chromium.chrome.browser.tab.TabSelectionType;
 import org.chromium.chrome.browser.toolbar.ControlContainer;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.WebContents;
-import org.chromium.ui.LayoutInflaterUtils;
+import org.chromium.ui.AsyncLayoutInflater;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.display.DisplayUtil;
 import org.chromium.url.GURL;
@@ -414,14 +412,13 @@ public class WarmupManager {
             CctContextWrapper context, int toolbarContainerId, int toolbarId) {
         try (TraceEvent e = TraceEvent.scoped("WarmupManager.inflateViewHierarchy")) {
             FrameLayout contentHolder = new FrameLayout(context);
-            var layoutInflater = LayoutInflater.from(context);
-            layoutInflater.setFactory2(new AsyncAppCompatFactory());
             ViewGroup mainView =
                     (ViewGroup)
-                            LayoutInflaterUtils.inflate(
-                                    layoutInflater,
-                                    MainLayoutSwitcher.getMainLayoutRes(),
-                                    contentHolder);
+                            new AsyncLayoutInflater(context)
+                                    .inflateSync(
+                                            MainLayoutSwitcher.getMainLayoutRes(),
+                                            contentHolder,
+                                            /* attachToRoot= */ true);
             if (toolbarContainerId != ActivityUtils.NO_RESOURCE_ID) {
                 ViewStub stub = mainView.findViewById(R.id.control_container_stub);
                 stub.setLayoutResource(toolbarContainerId);
