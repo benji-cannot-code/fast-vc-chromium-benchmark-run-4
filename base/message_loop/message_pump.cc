830c2defa903c468144b38e07d1f32efc92bf7e6
Gabriel Charette <gab@chromium.org>
2026-05-27 18:54:56 -0700

    Launch PumpPeekMessageWithObserver by default client-side.
    Bug: 488393677
    Change-Id: Icebe92f0941a4bad2604dbef52cd48141b764380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869357
    Reviewed-by: Sean Maher <spvm@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Sean Maher <spvm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637408}

--- base/message_loop/message_pump.cc
+++ base/message_loop/message_pump.cc
@@ -212,9 +212,6 @@ std::unique_ptr<MessagePump> MessagePump::Create(MessagePumpType type) {
 // static
 void MessagePump::InitializeFeatures() {
   ResetAlignWakeUpsState();
-#if BUILDFLAG(IS_WIN)
-  MessagePumpWin::InitializeFeatures();
-#endif
 }
 
 // static
