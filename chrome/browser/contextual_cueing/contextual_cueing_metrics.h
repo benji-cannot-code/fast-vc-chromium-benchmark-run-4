6d0088afb111902f4b1fdc9da699cb15404a315f
Benedict Wong <benedictwong@google.com>
2026-05-28 13:58:39 -0700

    Fix metrics for show/hide to use PageActionObserver methods
    Expand ContextualCueingPageActionObserver to override all 6 PageActionObserver methods, mapping each to the correct CueFormFactor (Icon, Chip, AnchoredMessage) and recording UMA metrics. Register the new Shown/Hidden UMA histograms.
    
    Plumb shown duration tracking cleanly by moving the exposure timer start (cue_shown_time_) from ShowCue() into the OnCueFormFactorShown observer callback, specifically when the cued form factor is kAnchoredMessage. This guarantees we track the exact exposure time and bypasses any queuing delay/latency from the page actions controller.
    
    Bug: 516040145
    Change-Id: Ia5f1b8e1ce79524de7c55c158f05b7d0c0297cae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7879301
    Commit-Queue: Benedict Wong <benedictwong@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637936}

--- chrome/browser/contextual_cueing/contextual_cueing_metrics.h
+++ chrome/browser/contextual_cueing/contextual_cueing_metrics.h
@@ -15,6 +15,7 @@ namespace contextual_cueing {
 
 enum class ContextualCueingInteraction;
 enum class ContextualCueingDecision;
+enum class CueFormFactor;
 
 // Counts of tabs received from the contextual cue server and whether they are
 // still relevant, or why they aren't.
@@ -38,6 +39,9 @@ void RecordContextualCueingInteraction(
 void RecordContextualCueingDecision(
     ukm::SourceId source_id,
     ContextualCueingDecision contextual_cueing_decision);
+
+void RecordCueFormFactorShown(CueFormFactor form_factor);
+void RecordCueFormFactorHidden(CueFormFactor form_factor);
 
 }  // namespace contextual_cueing
 
