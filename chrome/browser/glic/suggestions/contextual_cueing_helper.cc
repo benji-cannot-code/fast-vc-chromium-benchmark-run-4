aafaa53aab92339bc0c7fbdef841006ee2c09b30
Sana Akbani <sanaakbani@google.com>
2026-05-28 16:40:04 -0700

    [glic auto-open pdf] Bypass ContextualCueingV2 check for auto-open eligible cues.
    Bug: b:517207439
    Change-Id: Iee11ee6ea4aac3907d7238c628ce8194503f8c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7879495
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638048}

--- chrome/browser/glic/suggestions/contextual_cueing_helper.cc
+++ chrome/browser/glic/suggestions/contextual_cueing_helper.cc
@@ -397,12 +397,6 @@ bool ContextualCueingHelper::IsBrowserBlockingNudges(
   }
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-  if (base::FeatureList::IsEnabled(::contextual_cueing::kContextualCueingV2)) {
-    recorder->set_nudge_decision(
-        NudgeDecision::kNudgeNotShownContextualCueingV2);
-    return true;
-  }
-
   return false;
 }
 
@@ -444,6 +438,12 @@ void ContextualCueingHelper::OnCueingDecision(
         return;
       }
     }
+  }
+
+  if (base::FeatureList::IsEnabled(::contextual_cueing::kContextualCueingV2)) {
+    decision_recorder->set_nudge_decision(
+        NudgeDecision::kNudgeNotShownContextualCueingV2);
+    return;
   }
 
   if (can_show_decision != NudgeDecision::kSuccess) {
