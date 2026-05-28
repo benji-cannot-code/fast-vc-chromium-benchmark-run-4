02bce83f7b5cfa563efa1d47f17d7cf7a7c638f1
Dibyajyoti Pal <dibyapal@chromium.org>
2026-05-28 09:57:53 -0700

    Reland "[PWA][1/N] Defer Launch Params enqueueing until navigation commits"
    This reverts commit 4ac8ec3d7abb0021a2076afb8e38bc72c9184eb0.
    
    Reason for reland: The revert was incorrect. The failure stack trace [1] shows that the failing test was actually skipped because the test
    only runs on ChromeOS. This seems to be a problem with the win-rel
    bot [2], affecting a lot of parameterized tests on Windows which are skipped.
    
    [1] https://paste.googleplex.com/6128880384606208
    [2] https://b.corp.google.com/issues/441134574
    
    Original change's description:
    > Revert "[PWA][1/N] Defer Launch Params enqueueing until navigation commits"
    >
    > This reverts commit 8dadc97e28e35d9271a92c307d7e9164565e1e41.
    >
    > Reason for revert: This seems to be crashing NavCaptureParameterizedBrowserTest.VerifyNavCapture. First failure can be found here: https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/109792/overview
    >
    > Original change's description:
    > > [PWA][1/N] Defer Launch Params enqueueing until navigation commits
    > >
    > > This CL refactors `WebAppLaunchNavigationHandleUserData` to directly
    > > hold a `webapps::LaunchParams` object instead of just an app ID, passed
    > > to it via the `NavigateParams`.
    > >
    > > When an app launch triggers a navigation, the launch parameters are
    > > attached as user data on the `NavigationHandle`. Upon completion, the
    > > `WebAppUiManager` verifies the navigation status and dispatches the
    > > launch parameters to the tab helper's launch queue if the navigation
    > > successfully committed and is in-scope. Non-navigating launches (such as
    > > focus-existing actions) enqueue launch parameters directly. The
    > > `WebAppUiManager` hop is needed to access `c/b/ui/` logic. This allows
    > > launch params to be enqueued as soon as the navigation commits. The
    > > `LaunchQueue` implementation has also been simplified to support this.
    > >
    > > There is still some code like extensions which enqueue launch params
    > > before navigation commits, which will be modified in follow-up patches.
    > > The changes in this CL are strictly needed to ensure all tests pass.
    > >
    > > Bug: 390252819
    > > Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|browser_tests
    > > Change-Id: I4a7798fe6588b63724f9f59452a0021637977274
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869543
    > > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1637109}
    >
    > Bug: 390252819
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I8f0c11c9724ed718e53743caaf6574fafd21baa2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876659
    > Commit-Queue: Eva Su <evasu@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Eva Su <evasu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1637268}
    
    Bug: 390252819
    Change-Id: If9f0ea22ef043ab4ceb7b5f0991692abea52118b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880705
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637772}

--- chrome/browser/ui/web_applications/web_app_ui_manager_impl.h
+++ chrome/browser/ui/web_applications/web_app_ui_manager_impl.h
@@ -196,6 +196,9 @@ class WebAppUiManagerImpl : public BrowserCollectionObserver,
   void MaybeRemoveWebAppBlockedMigrationInfoBar(
       content::WebContents* web_contents) override;
 
+  void NotifyDidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override;
+
   void MaybeShowIPHPromoForAppsLaunchedViaLinkCapturing(
       Browser* browser,
       Profile* profile,
--- chrome/browser/web_applications/web_app_tab_helper.h
+++ chrome/browser/web_applications/web_app_tab_helper.h
@@ -29,6 +29,7 @@ class WebContents;
 
 namespace webapps {
 class LaunchQueue;
+struct LaunchParams;
 }
 
 namespace web_app {
@@ -159,10 +160,13 @@ class WebAppTabHelper : public content::WebContentsUserData<WebAppTabHelper>,
   bool is_in_app_window() const { return window_app_id_.has_value(); }
 
   webapps::LaunchQueue& EnsureLaunchQueue();
+  void EnqueueLaunchParams(webapps::LaunchParams launch_params);
 
   // content::WebContentsObserver:
 
   void ReadyToCommitNavigation(
+      content::NavigationHandle* navigation_handle) override;
+  void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
   void PrimaryPageChanged(content::Page& page) override;
   void DidFinishLoad(content::RenderFrameHost* render_frame_host,
