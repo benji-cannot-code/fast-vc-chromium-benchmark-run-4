02366813b2d2a999a6a3f4c690d2b4c50c15ce14
Briana McClure <bmcclure@google.com>
2026-05-28 08:30:32 -0700

    [iOS] Fix testNewTabByLongPressTabGridViewWithNTPLocation for IA
    This change updates testNewTabByLongPressTabGridViewWithNTPLocation to
    open an Incognito tab before accessing the tab grid view long press
    menu. This is necessary because with IA enabled, the tab grid view menu
    only presents the "New Tab" item for the opposite tab grid.
    
    Bug: 514608938
    Change-Id: Ie21d7fb2c8afe13ed2332d41e8a18a074a07d4bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7877851
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637720}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_egtest.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_egtest.mm
@@ -99,13 +99,6 @@ enum class QuickActionsVisibility {
 
 @implementation NewTabPageTestCase
 
-- (AppLaunchConfiguration)appConfigurationForTestCase {
-  AppLaunchConfiguration config = [super appConfigurationForTestCase];
-  // TODO(crbug.com/514608938): Fix test for Chrome Next.
-  config.features_disabled.push_back(kChromeNextIa);
-  return config;
-}
-
 - (void)tearDownHelper {
   [self releaseHistogramTester];
   policy_test_utils::ClearPolicies();
@@ -402,6 +395,10 @@ enum class QuickActionsVisibility {
   // Set the policy's NTP Location value at runtime.
   [self setNTPPolicyValue:expectedURL.spec()];
 
+  // Open a new incognito tab to expose the "New Tab" item in the long press
+  // menu.
+  [ChromeEarlGrey openNewIncognitoTab];
+
   // Open tab via the UI.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::ShowTabsButton()]
       performAction:grey_longPress()];
@@ -415,6 +412,8 @@ enum class QuickActionsVisibility {
       performAction:grey_tap()];
 
   [self validateNTPURL:expectedURL];
+
+  [ChromeEarlGrey closeAllIncognitoTabs];
 }
 
 // Verifies opening a new tab from the tab grid view by tapping on the New Tab
