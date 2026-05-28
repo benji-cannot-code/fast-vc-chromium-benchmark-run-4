a091df727092e21faef72fe3b6ee2235a0316f6e
Peter Kang <peterkang@google.com>
2026-05-28 10:13:23 -0700

    [Flags] Remove obsolete AndroidBottomToolbarV2 feature flag definitions
    Bug: 515137657
    Change-Id: Iac3dbcc9a0f34746b19c7c3a49e37701096080ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880146
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Peter Kang <peterkang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1637786}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -222,7 +222,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidAppRatingPrompt,
     &kAndroidBookmarkBar,
     &kAndroidBottomBar,
-    &kAndroidBottomToolbarV2,
     &kAndroidContextMenuNewActions,
     &kAndroidDeviceSignalsDisclaimer,
     &kAndroidElegantTextHeight,
@@ -573,7 +572,6 @@ BASE_FEATURE(kAndroidAppRatingPrompt, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidAtomsLogging, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidBookmarkBar, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidBottomBar, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kAndroidBottomToolbarV2, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAndroidContextMenuNewActions, base::FEATURE_DISABLED_BY_DEFAULT);
 // Enables additional text shown during profile creation for managed users,
 // informing them about device signal collection for security purposes.
