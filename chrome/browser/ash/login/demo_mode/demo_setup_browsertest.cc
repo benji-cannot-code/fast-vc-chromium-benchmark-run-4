149d0ec98be59393dd32e2ed2a1449d4baaf152b
yoshiki iguchi <yoshiki@google.com>
2026-05-27 20:13:48 -0700

    ASH Refactoring: Move IDS_DEMO_* to ash/
    Move all IDS_DEMO_* strings (including setup flow errors and session
    ToS notification strings) from chrome/app/chromeos_strings.grdp to
    ash/ash_strings.grd as they are only used in ash and related ChromeOS-
    specific browser features.
    
    Update includes, and BUILD.gn accordingly.
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: Iaa48d354a8ef955b8d2c6a398ddb25a970b54de0
    Skip-Translation-Screenshots-Check: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867522
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637431}

--- chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/ash/login/demo_mode/demo_setup_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/login_accelerators.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "base/check_deref.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
