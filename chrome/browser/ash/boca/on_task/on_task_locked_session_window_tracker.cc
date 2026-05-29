2ddd6104335ea72b19ba367d9d56708e01bf7e45
Vignesh Shenvi <vshenvi@google.com>
2026-05-28 17:52:35 -0700

    [ChromeOS] Support FedCM oauth requests in Class Tools
    This change updates the OnTask window tracker to also track FedCM oauth
    flows. So far, we have been tracking conventional oauth flows that are
    easier to detect at the start of a navigation. FedCM oauth flows are
    triggered using JS APIs and cannot be tracked by the navigation
    throttle.
    
    Bug: b:512886718
    Change-Id: I6d451ceedbbdf7bf14c2d00a5a9ba9f831644524
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7855173
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638119}

--- chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker.cc
+++ chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker.cc
@@ -46,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_properties.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/page.h"
+#include "content/public/browser/webid/identity_credential_source.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tracker.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -240,6 +242,7 @@ void LockedSessionWindowTracker::CleanupWindowTracker() {
   can_open_new_popup_ = true;
   oauth_in_progress_ = false;
   authorized_oauth_browser_ = nullptr;
+  identity_credential_source_for_testing_ = nullptr;
 
   for (auto& observer : observers_) {
     observer.OnWindowTrackerCleanedup();
@@ -288,17 +291,27 @@ void LockedSessionWindowTracker::OnTabChangedAt(tabs::TabInterface* tab,
   }
 }
 
+ash::OnTaskPodController* LockedSessionWindowTracker::on_task_pod_controller() {
+  if (!on_task_pod_controller_) {
+    return nullptr;
+  }
+  return on_task_pod_controller_.get();
+}
+
 void LockedSessionWindowTracker::SetNotificationManagerForTesting(
     std::unique_ptr<ash::boca::OnTaskNotificationsManager>
         notifications_manager) {
   notifications_manager_ = std::move(notifications_manager);
 }
 
-ash::OnTaskPodController* LockedSessionWindowTracker::on_task_pod_controller() {
-  if (!on_task_pod_controller_) {
-    return nullptr;
-  }
-  return on_task_pod_controller_.get();
+void LockedSessionWindowTracker::SetIdentityCredentialSourceForTesting(
+    content::webid::IdentityCredentialSource* source) {
+  identity_credential_source_for_testing_ = source;
+}
+
+void LockedSessionWindowTracker::TriggerFedCmFederatedLoginCompletionForTesting(
+    bool success) {
+  OnFedCmFederatedLogin(success);
 }
 
 void LockedSessionWindowTracker::OnTabStripModelChanged(
@@ -462,14 +475,50 @@ void LockedSessionWindowTracker::DidFinishNavigation(
   if (!browser || !browser_) {
     return;
   }
-  if (browser != browser_) {
-    EnsureMaybeCloseBrowserTaskPosted(browser);
-  } else {
+  if (browser == browser_) {
     content::WebContents* const tab = navigation_handle->GetWebContents();
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE,
         base::BindOnce(&LockedSessionWindowTracker::MaybeCloseWebContents,
                        weak_pointer_factory_.GetWeakPtr(), tab->GetWeakPtr()));
+  }
+}
+
+void LockedSessionWindowTracker::DidFinishLoad(
+    content::RenderFrameHost* render_frame_host,
+    const GURL& validated_url) {
+  if (!render_frame_host->IsInPrimaryMainFrame()) {
+    return;
+  }
+  ash::BrowserDelegate* const browser =
+      ash::BrowserController::GetInstance()->GetBrowserForTab(
+          content::WebContents::FromRenderFrameHost(render_frame_host));
+  if (!browser || !browser_) {
+    return;
+  }
+  if (browser != browser_) {
+    if (browser->GetType() == ash::BrowserType::kAppPopup) {
+      // Verify if there are pending FedCM oauth requests for tracking purposes.
+      content::webid::IdentityCredentialSource* const source =
+          GetIdentityCredentialSource(render_frame_host->GetPage());
+      if (source && source->HasPendingRequest()) {
+        set_oauth_in_progress(true, browser);
+      }
+    }
+    EnsureMaybeCloseBrowserTaskPosted(browser);
+  }
+}
+
+void LockedSessionWindowTracker::OnFedCmFederatedLogin(bool success) {
+  set_oauth_in_progress(false, nullptr);
+  if (web_contents()) {
+    ash::BrowserDelegate* const browser =
+        ash::BrowserController::GetInstance()->GetBrowserForTab(web_contents());
+    if (browser && browser != browser_) {
+      // Attempt to close the oauth popup window now that the oauth flow has
+      // completed.
+      EnsureMaybeCloseBrowserTaskPosted(browser);
+    }
   }
 }
 
@@ -484,4 +533,12 @@ void LockedSessionWindowTracker::EnsureMaybeCloseBrowserTaskPosted(
   pending_close_tasks_.emplace(browser, std::move(task));
   base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE, pending_close_tasks_[browser]->callback());
+}
+
+content::webid::IdentityCredentialSource*
+LockedSessionWindowTracker::GetIdentityCredentialSource(content::Page& page) {
+  if (identity_credential_source_for_testing_) {
+    return identity_credential_source_for_testing_.get();
+  }
+  return content::webid::IdentityCredentialSource::FromPage(page);
 }
--- chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker.h
+++ chrome/browser/ash/boca/on_task/on_task_locked_session_window_tracker.h
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 class BrowserWindowInterface;
 
+namespace content::webid {
+class IdentityCredentialSource;
+}
+
 namespace ash {
 class BrowserDelegate;
 class OnTaskPodController;
@@ -98,13 +102,17 @@ class LockedSessionWindowTracker : public KeyedService,
   bool oauth_in_progress() { return oauth_in_progress_; }
   void set_oauth_in_progress(bool in_progress, ash::BrowserDelegate* browser);
 
-  void SetNotificationManagerForTesting(
-      std::unique_ptr<ash::boca::OnTaskNotificationsManager>
-          notification_manager);
-
   ash::OnTaskPodController* on_task_pod_controller();
   OnTaskBlocklist* on_task_blocklist();
   Browser* browser();
+
+  // Test helpers:
+  void SetNotificationManagerForTesting(
+      std::unique_ptr<ash::boca::OnTaskNotificationsManager>
+          notification_manager);
+  void SetIdentityCredentialSourceForTesting(
+      content::webid::IdentityCredentialSource* source);
+  void TriggerFedCmFederatedLoginCompletionForTesting(bool success);
 
  private:
   // TabStripModelObserver Impl
@@ -126,6 +134,9 @@ class LockedSessionWindowTracker : public KeyedService,
   // content::WebContentsObserver Impl
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
+  void DidFinishLoad(content::RenderFrameHost* render_frame_host,
+                     const GURL& validated_url) override;
+  void OnFedCmFederatedLogin(bool success) override;
 
   // Callback for browser closed events.
   void OnBrowserDidClose(BrowserWindowInterface* browser_window_interface);
@@ -133,6 +144,9 @@ class LockedSessionWindowTracker : public KeyedService,
   void MaybeCloseWebContents(base::WeakPtr<content::WebContents> weak_tab_ptr);
   void MaybeCloseBrowser(ash::BrowserDelegate* browser);
   void EnsureMaybeCloseBrowserTaskPosted(ash::BrowserDelegate* browser);
+
+  content::webid::IdentityCredentialSource* GetIdentityCredentialSource(
+      content::Page& page);
 
   void CleanupWindowTracker();
 
@@ -145,6 +159,8 @@ class LockedSessionWindowTracker : public KeyedService,
   std::unique_ptr<ash::OnTaskPodController> on_task_pod_controller_;
   raw_ptr<ash::BrowserDelegate> browser_ = nullptr;
   raw_ptr<ash::BrowserDelegate> authorized_oauth_browser_ = nullptr;
+  raw_ptr<content::webid::IdentityCredentialSource>
+      identity_credential_source_for_testing_ = nullptr;
   base::ScopedObservation<ash::BrowserController,
                           ash::BrowserController::Observer>
       browser_controller_observation_{this};
