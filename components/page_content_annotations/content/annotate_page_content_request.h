c70fd0f17fffdbfe73ed05b7b7ae5e7fee93c9d9
Xiaochen Zhou <xiaochenzh@chromium.org>
2026-05-29 05:16:05 -0700

    PageContentExtractionService: Wait for PDF document load complete for page count and text extraction request
    Feature `PageContentExtractionUsingPageSettledMonitor`:
    
    - When enabled, PageSettledMonitor triggers extraction once page is
    settled. crrev.com/c/7864029 enables PageSettledMonitor to wait for PDF
    document load complete. However, PageSettledMonitor also notifies page
    settled when the wait times out.
    
    - When disabled, PageSettledMonitor is not used. Extraction triggers
    with a default 3-seconds delay after the page stops loading. Page stop
    loading does not imply PDF document load complete.
    
    When PDF document load is not complete, the page count and text
    extraction should not be done. This is currently handled by the check in
    PageContextFetcher.
    
    PageContextFetcher should only be responsible for fetching the page
    context. It's client's job to manage page stability and extraction
    timing.
    
    The CL changes AnnotatedPageContentRequest to register PDF request as a
    callback invoked only when PDF document load completes. Added metrics to
    record PDF document status.
    
    Bug: 514315323, 487632737
    Change-Id: I08759589ae8ef1a2d5931f2a68c52fc2c814e63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7857491
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638404}

--- components/page_content_annotations/content/annotate_page_content_request.h
+++ components/page_content_annotations/content/annotate_page_content_request.h
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/buildflags.h"
 #include "third_party/blink/public/mojom/content_extraction/ai_page_content.mojom.h"
 
+namespace base {
+class ScopedClosureRunner;
+}
+
 namespace optimization_guide {
 class PageContextEligibility;
 
@@ -199,11 +203,11 @@ class AnnotatedPageContentRequest
   bool IsPageReadyForExtraction() const;
 
 #if BUILDFLAG(ENABLE_PDF)
-  void RequestPdfPageCount();
-  void RequestPdfText(TriggerSource trigger_source);
+  void RequestPdf(TriggerSource trigger_source);
 
-  // Invoked when pdf document is loaded, so that the metadata can be queried.
-  void OnPdfDocumentLoadComplete();
+  void RequestPdfPageCount(base::ScopedClosureRunner metrics_recorder);
+  void RequestPdfText(TriggerSource trigger_source,
+                      base::ScopedClosureRunner metrics_recorder);
 #endif  // BUILDFLAG(ENABLE_PDF)
 
   void OnPageContextEligibilityAPILoaded(
