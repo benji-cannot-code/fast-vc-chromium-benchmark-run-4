530d282b39b980a39b29f46b9a61b63600f46057
yoshiki iguchi <yoshiki@google.com>
2026-05-29 04:42:44 -0700

    ASH Refactoring: Move IDS_TERMS_OF_SERVICE_SCREEN_* to ash/
    Move all 7 IDS_TERMS_OF_SERVICE_SCREEN_* strings from chromeos_strings.grdp to
    ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific
    browser features.
    
    Update includes accordingly.
    
    - Moves 6 corresponding translation screenshot files.
    - Removes 3 obsolete includes.
    - Removes 1 obsolete DEPS rules.
    
    Moved string IDs:
      IDS_TERMS_OF_SERVICE_SCREEN_ACCEPT_BUTTON
      IDS_TERMS_OF_SERVICE_SCREEN_BACK_BUTTON
      IDS_TERMS_OF_SERVICE_SCREEN_ERROR
      IDS_TERMS_OF_SERVICE_SCREEN_HEADING
      IDS_TERMS_OF_SERVICE_SCREEN_LOADING
      IDS_TERMS_OF_SERVICE_SCREEN_RETRY_BUTTON
      IDS_TERMS_OF_SERVICE_SCREEN_TRY_AGAIN
    
    Bug: 487122295
    Test: CQ passes
    Skip-Translation-Screenshots-Check: True
    Change-Id: I0abb59a529bcca5b1ba3f9e868599c71d1d5317b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867523
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638392}

--- chrome/browser/ui/webui/ash/login/terms_of_service_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/terms_of_service_screen_handler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "ash/strings/grit/ash_strings.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/strings/string_split.h"
@@ -17,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/login/oobe_ui.h"
 #include "chrome/grit/branded_strings.h"
-#include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user.h"
