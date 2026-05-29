7485b09fc23af4788a242645c9dc45d847be5826
Sky Malice <skym@chromium.org>
2026-05-29 12:49:42 -0700

    [AIM] Emit histograms for upload changes to debug failure.
    NO_IFTTT=Only added Java enum generation, no functional changes to the enum.
    
    Bug: 514708137
    Change-Id: I11449332f199de8e9f5e33f1b159b67cc5d36a91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7861405
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638675}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/ComposeboxQueryControllerBridge.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/ComposeboxQueryControllerBridge.java
@@ -20,6 +20,7 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.components.contextual_search.ContextUploadErrorType;
 import org.chromium.components.contextual_search.ContextUploadStatus;
 import org.chromium.components.contextual_search.InputState;
 import org.chromium.components.omnibox.OmniboxFeatures;
@@ -47,8 +48,12 @@ public class ComposeboxQueryControllerBridge {
         /**
          * @param token Unique string identifier for the context.
          * @param status The status of the context's upload.
+         * @param errorType The error type if the upload failed.
          */
-        void onContextUploadStatusChanged(String token, @ContextUploadStatus int status);
+        void onContextUploadStatusChanged(
+                String token,
+                @ContextUploadStatus int status,
+                @ContextUploadErrorType int errorType);
     }
 
     private long mNativeInstance;
@@ -108,9 +113,13 @@ public class ComposeboxQueryControllerBridge {
     }
 
     @CalledByNative
-    void onContextUploadStatusChanged(String token, @ContextUploadStatus int contextUploadStatus) {
+    void onContextUploadStatusChanged(
+            String token,
+            @ContextUploadStatus int contextUploadStatus,
+            @ContextUploadErrorType int errorType) {
         if (mContextUploadObserver != null) {
-            mContextUploadObserver.onContextUploadStatusChanged(token, contextUploadStatus);
+            mContextUploadObserver.onContextUploadStatusChanged(
+                    token, contextUploadStatus, errorType);
         }
     }
 
