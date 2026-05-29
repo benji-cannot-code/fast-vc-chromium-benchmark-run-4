36b0eabee23b7719135e9e5e77f256d5a39f869c
Thomas Guilbert <tguilbert@chromium.org>
2026-05-29 10:37:00 -0700

    [CodeHealth] Spanify Android interleaving
    This CL replaces uses of the deprecated `ToInterleaved()` with its
    spanified counterpart.
    
    Bug: 373960632
    Change-Id: Ief1828eb2e2609fbe80f354c2dc215af4ac36c87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883434
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Reviewed-by: Phil Yan <philyan@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638576}

--- media/audio/android/audio_track_output_stream.cc
+++ media/audio/android/audio_track_output_stream.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 
 #include "base/android/jni_bytebuffer.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -187,13 +188,13 @@ ScopedJavaLocalRef<jobject> AudioTrackOutputStream::OnMoreData(
 
   callback_->OnMoreData(delay, tick_clock_->NowTicks(), {}, audio_bus_.get());
 
-  int16_t* native_bus = reinterpret_cast<int16_t*>(native_buffer.data());
-  audio_bus_->ToInterleaved<SignedInt16SampleTypeTraits>(audio_bus_->frames(),
-                                                         native_bus);
+  const size_t total_bytes =
+      audio_bus_->frames() * audio_bus_->channels() * sizeof(int16_t);
+  base::span<uint8_t> target_buffer = native_buffer.first(total_bytes);
+  audio_bus_->ToInterleavedBytes<SignedInt16SampleTypeTraits>(target_buffer);
 
   return Java_AudioTrackOutputStream_createAudioBufferInfo(
-      env, j_audio_output_stream_, audio_bus_->frames(),
-      sizeof(*native_bus) * audio_bus_->channels() * audio_bus_->frames());
+      env, j_audio_output_stream_, audio_bus_->frames(), total_bytes);
 }
 
 void AudioTrackOutputStream::OnError(JNIEnv* env) {
