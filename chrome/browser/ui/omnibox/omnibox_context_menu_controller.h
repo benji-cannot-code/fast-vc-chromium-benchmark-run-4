616b4c23c382b2c493812bd98647c84d0a750671
James Leung <jamesleung@google.com>
2026-05-29 12:37:13 -0700

    [tab_management] Sort tabs so selected ones are first in omnibox
    * This is only for omnibox context menu tab menu (in views).
    * Since tab checkmarks are rendered after tab order is decided,
    store checkmark attribute in tab structure (used locally so not many
    places to update the struct usage in). This way, checkmark attribute can
    be used when tabs are sorted, which occurs before checkmarks are
    rendered.
    * Tabs are sorted by checked (selected) tabs, then recent tabs.
      Tiebreakers are sorted by recent tabs, and then by last
      active timestamp.
    * Pulls 'is_checked' from the tab struct attribute instead now
      that that attribute exists.
    * Added tests (add, remove, ensure ties are broken for checked
      tabs by recent tabs, and for unchecked tabs, by recent tabs).
    
    Demo: screencast/cast/NTk4MDU5NDc4MDUwNDA2NHxlYjE1NzIzZi01Ng
    Bug: b:513617671
    Change-Id: Ib4441750b02e016e72909c2cf621b18d1bd632b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883788
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638666}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller.h
@@ -88,6 +88,7 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
     GURL url;
     base::TimeTicks last_active;
     bool is_active_tab = false;
+    bool is_checked = false;
   };
 
   OmniboxContextMenuController(const OmniboxContextMenuController&) = delete;
@@ -124,6 +125,7 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
   static OmniboxController* GetOmniboxController(
       content::WebContents* web_contents);
   static OmniboxPopupUI* GetOmniboxPopupUI(content::WebContents* web_contents);
+  int GetMaxTabSuggestions() const;
 
  private:
   friend class TabSimpleMenuModel;
@@ -198,8 +200,6 @@ class OmniboxContextMenuController : public ui::SimpleMenuModel::Delegate {
   void UpdateSearchboxContextToolMode(omnibox::ToolMode tool_mode);
 
   bool IsContentSharingEnabled() const;
-
-  int GetMaxTabSuggestions() const;
 
   omnibox::ContextType CommandIdToEnum(int command_id) const;
 
