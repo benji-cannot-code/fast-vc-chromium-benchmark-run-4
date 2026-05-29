b40dca8cb9c9d3209390e4685807f91e480a8a1d
Olivier Gallant <ogallant@google.com>
2026-05-29 11:30:56 -0700

    [iOS] Added new zero_state_suggestions_service
    This new service is now responsible for calling the
    modelLedSuggestionsService and parsing the result. In following CLs,
    this service will be responsible for generating the ZSS suggestions
    with static and model-led suggestions.This CL is mainly only moving code
    from gemini_tab_helper to zero_state_suggestions_service.
    
    Bug: 504710882
    Change-Id: Ib7b81cf1c53f14b1c2e4dd7112ea65a7a1a55fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872218
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Commit-Queue: Olivier Gallant <ogallant@google.com>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638612}

--- ios/chrome/browser/intelligence/bwg/model/gemini_suggestion_handler_unittest.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_suggestion_handler_unittest.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/task_environment.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_tab_helper.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
+#import "ios/chrome/browser/intelligence/zero_state_suggestions/zero_state_suggestions_service.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
@@ -39,9 +40,14 @@ class GeminiSuggestionHandlerTest : public PlatformTest {
   // access.
   void SetSuggestions(GeminiTabHelper* tab_helper,
                       const std::vector<std::string>& suggestions) {
-    ASSERT_TRUE(tab_helper->model_led_suggestions_);
-    tab_helper->model_led_suggestions_->can_apply = true;
-    tab_helper->model_led_suggestions_->suggestions = suggestions;
+    ASSERT_TRUE(tab_helper->zero_state_suggestions_service_);
+    tab_helper->zero_state_suggestions_service_->SetCanApply(true);
+    tab_helper->zero_state_suggestions_service_->suggestions_ = suggestions;
+    if (tab_helper->zero_state_suggestions_service_->web_state_) {
+      tab_helper->zero_state_suggestions_service_->suggestions_url_ =
+          tab_helper->zero_state_suggestions_service_->web_state_
+              ->GetVisibleURL();
+    }
   }
 
   // Helper to set current URL in GeminiTabHelper using the friend class access.
