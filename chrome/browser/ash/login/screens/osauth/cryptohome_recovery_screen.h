3b5cc76ed0eeecae32ea45a149fec30447256524
Jun Ishiguro <junis@google.com>
2026-05-29 01:12:12 -0700

    login: Pass local state PrefService to CryptohomeRecoveryScreen
    This is Part 2 of a multi-CL refactoring to remove the dependency on the
    global g_browser_process from ash::RecordReauthReason.
    
    This CL:
    1. Updates the CryptohomeRecoveryScreen constructor to accept and store
       a reference to PrefService (local state).
    2. Updates CryptohomeRecoveryScreen::OnGetAuthFactorsConfiguration to
       use the injected PrefService when calling RecordReauthReason,
       removing the dependency on g_browser_process.
    3. Updates WizardController::CreateScreens to pass the local state
       preference service to the CryptohomeRecoveryScreen constructor.
    
    TAG=agy
    
    Bug: b:404133029
    Change-Id: Ie732cb8313833b97bbd0cf3f8ddbf10e34535f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867052
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638299}

--- chrome/browser/ash/login/screens/osauth/cryptohome_recovery_screen.h
+++ chrome/browser/ash/login/screens/osauth/cryptohome_recovery_screen.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
@@ -20,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/login/auth/public/authentication_error.h"
 #include "chromeos/ash/components/login/auth/public/user_context.h"
 #include "chromeos/ash/components/login/auth/recovery/cryptohome_recovery_performer.h"
+
+class PrefService;
 
 namespace network {
 class SharedURLLoaderFactory;
@@ -43,8 +46,10 @@ class CryptohomeRecoveryScreen : public BaseScreen {
   static std::string GetResultString(Result result);
   using ScreenExitCallback = base::RepeatingCallback<void(Result result)>;
 
+  // `local_state` must be non-null and must outlive `this`.
   // `shared_url_loader_factory` must be non-null.
   CryptohomeRecoveryScreen(
+      PrefService* local_state,
       scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory,
       base::WeakPtr<CryptohomeRecoveryScreenView> view,
       const ScreenExitCallback& exit_callback);
@@ -81,6 +86,7 @@ class CryptohomeRecoveryScreen : public BaseScreen {
   void OnRefreshFactorsConfiguration(std::unique_ptr<UserContext> user_context,
                                      std::optional<AuthenticationError> error);
 
+  const raw_ref<PrefService> local_state_;
   const scoped_refptr<network::SharedURLLoaderFactory>
       shared_url_loader_factory_;
 
