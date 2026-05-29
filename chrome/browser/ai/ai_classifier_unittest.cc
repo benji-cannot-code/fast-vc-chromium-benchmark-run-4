ec780ef79dbafa5f172103f512eebb2199fa5ff7
Jingyun Liu <jingyun@google.com>
2026-05-29 12:36:48 -0700

    Guard AIClassifier API methods with a feature flag in browser
    BUG: 517381770
    Change-Id: I324ba669ded582ba722cbe7de795acb43958abc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883638
    Auto-Submit: Jingyun Liu <jingyun@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638665}

--- chrome/browser/ai/ai_classifier_unittest.cc
+++ chrome/browser/ai/ai_classifier_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/on_device_model/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features_generated.h"
 #include "third_party/blink/public/mojom/ai/ai_classifier.mojom.h"
 #include "third_party/blink/public/mojom/ai/ai_manager.mojom.h"
 #include "third_party/blink/public/mojom/ai/model_streaming_responder.mojom.h"
@@ -95,6 +96,12 @@ blink::mojom::AIClassifierCreateOptionsPtr GetDefaultOptions() {
 }
 
 class AIClassifierTest : public AITestUtils::AITestBase {
+ public:
+  AIClassifierTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        blink::features::kAIClassifierAPI);
+  }
+
  protected:
   optimization_guide::proto::OnDeviceModelExecutionFeatureConfig CreateConfig()
       override {
@@ -145,6 +152,9 @@ class AIClassifierTest : public AITestUtils::AITestBase {
     auto result = classifier_client.result().Take();
     EXPECT_TRUE(result.has_value());
   }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(AIClassifierTest, Classify) {
