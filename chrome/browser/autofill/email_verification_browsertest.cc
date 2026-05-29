8cc142300a24287aa4fbcf96c5b47153807ba436
Yi Gu <yigu@chromium.org>
2026-05-29 06:45:52 -0700

    [EVP] Only trigger strike after user explicitly declines the permission
    This CL replaces a boolean parameter with an enum to better capture the
    user intention about the permission UI, and uses it to properly trigger
    strike.
    
    Fixed: 517518337
    Change-Id: I388eb623dff61bd6ee794e53b7cb432897f8a40f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883998
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638451}

--- chrome/browser/autofill/email_verification_browsertest.cc
+++ chrome/browser/autofill/email_verification_browsertest.cc
@@ -93,7 +93,8 @@ class EmailVerificationBrowserTest : public InProcessBrowserTest {
                 (const gfx::RectF&,
                  const net::SchemefulSite&,
                  const std::u16string&,
-                 base::OnceCallback<void(bool)>),
+                 base::OnceCallback<void(
+                     AutofillClient::EmailVerificationPermissionUiResult)>),
                 (override));
 
     EmailVerifierDelegate& email_verifier_delegate() {
@@ -243,8 +244,11 @@ IN_PROC_BROWSER_TEST_F(EmailVerificationBrowserTest, FullFlowRendererStorage) {
   EXPECT_CALL(*mock_client, ShowEmailVerificationPopup)
       .WillOnce([&](const gfx::RectF&, const net::SchemefulSite&,
                     const std::u16string&,
-                    base::OnceCallback<void(bool)> callback) {
-        std::move(callback).Run(true);
+                    base::OnceCallback<void(
+                        AutofillClient::EmailVerificationPermissionUiResult)>
+                        callback) {
+        std::move(callback).Run(
+            AutofillClient::EmailVerificationPermissionUiResult::kAccepted);
         popup_run_loop.Quit();
       });
   EXPECT_CALL(*mock_client, ShowEmailVerifiedToast);
@@ -330,8 +334,11 @@ IN_PROC_BROWSER_TEST_F(EmailVerificationBrowserTest,
   EXPECT_CALL(*mock_client, ShowEmailVerificationPopup)
       .WillOnce([&](const gfx::RectF&, const net::SchemefulSite&,
                     const std::u16string&,
-                    base::OnceCallback<void(bool)> callback) {
-        std::move(callback).Run(true);
+                    base::OnceCallback<void(
+                        AutofillClient::EmailVerificationPermissionUiResult)>
+                        callback) {
+        std::move(callback).Run(
+            AutofillClient::EmailVerificationPermissionUiResult::kAccepted);
         popup_run_loop1.Quit();
       });
 
@@ -359,8 +366,11 @@ IN_PROC_BROWSER_TEST_F(EmailVerificationBrowserTest,
   EXPECT_CALL(*mock_client, ShowEmailVerificationPopup)
       .WillOnce([&](const gfx::RectF&, const net::SchemefulSite&,
                     const std::u16string&,
-                    base::OnceCallback<void(bool)> callback) {
-        std::move(callback).Run(true);
+                    base::OnceCallback<void(
+                        AutofillClient::EmailVerificationPermissionUiResult)>
+                        callback) {
+        std::move(callback).Run(
+            AutofillClient::EmailVerificationPermissionUiResult::kAccepted);
         popup_run_loop2.Quit();
       });
 
@@ -452,8 +462,11 @@ IN_PROC_BROWSER_TEST_F(EmailVerificationBrowserTest, FullFlowAutocomplete) {
   EXPECT_CALL(*mock_client, ShowEmailVerificationPopup)
       .WillOnce([&](const gfx::RectF&, const net::SchemefulSite&,
                     const std::u16string&,
-                    base::OnceCallback<void(bool)> callback) {
-        std::move(callback).Run(true);
+                    base::OnceCallback<void(
+                        AutofillClient::EmailVerificationPermissionUiResult)>
+                        callback) {
+        std::move(callback).Run(
+            AutofillClient::EmailVerificationPermissionUiResult::kAccepted);
         popup_run_loop.Quit();
       });
   EXPECT_CALL(*mock_client, ShowEmailVerifiedToast);
