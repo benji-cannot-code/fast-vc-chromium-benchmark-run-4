dd7e139b6ea7a4eca199e7b77660ccf9ec02e7fa
Fred Mello <fredmello@google.com>
2026-05-29 12:01:40 -0700

    Use Map for JNI boundary instead of parallel arrays
    Bug: 517494744
    Change-Id: Ic0edfad1e40120b317b3a1b8a01dfae43e59bb96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883538
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fred Mello <fredmello@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638642}

--- chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
+++ chrome/browser/android/omnibox/composebox_query_controller_bridge.cc
@@ -60,6 +60,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/android/gurl_android.h"
 #include "url/gurl.h"
 
+#include "third_party/jni_zero/default_conversions.h"
+
+namespace jni_zero {
+template <>
+inline omnibox::InputType FromJniType<omnibox::InputType>(
+    JNIEnv* env,
+    const JavaRef<jobject>& val) {
+  return static_cast<omnibox::InputType>(FromJavaInteger(env, val));
+}
+
+template <>
+inline ScopedJavaLocalRef<jobject> ToJniType<omnibox::InputType>(
+    JNIEnv* env,
+    const omnibox::InputType& val) {
+  return ToJavaInteger(env, std::to_underlying(val));
+}
+}  // namespace jni_zero
+
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/ui/android/omnibox/jni_headers/ComposeboxQueryControllerBridge_jni.h"
 #include "chrome/browser/ui/android/omnibox/jni_headers/SuggestedTabInfo_jni.h"
@@ -579,13 +597,6 @@ void ComposeboxQueryControllerBridge::OnInputStateChanged(
     const contextual_search::InputState& state) {
   JNIEnv* env = base::android::AttachCurrentThread();
 
-  std::vector<omnibox::InputType> max_inputs_by_types_keys;
-  std::vector<int> max_inputs_by_types_values;
-  for (const auto& [key, value] : state.max_inputs_by_type) {
-    max_inputs_by_types_keys.emplace_back(key);
-    max_inputs_by_types_values.emplace_back(value);
-  }
-
   std::vector<std::vector<uint8_t>> tool_configs;
   for (const auto& config : state.tool_configs) {
     std::string serialized;
@@ -628,7 +639,7 @@ void ComposeboxQueryControllerBridge::OnInputStateChanged(
       contextual_search::Java_InputState_Constructor(
           env, state.hint_text, state.allowed_input_types,
           state.disabled_input_types, state.max_total_inputs,
-          max_inputs_by_types_keys, max_inputs_by_types_values,
+          state.max_inputs_by_type,
           base::android::ToJavaArrayOfByteArray(env, input_type_configs),
           state.active_tool, state.allowed_tools, state.disabled_tools,
           state.image_gen_upload_active,
