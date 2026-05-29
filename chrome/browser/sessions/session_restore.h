f6d4fcf126ebae7bb16ae61bbd39841291ce2ddb
dljames <dljames@chromium.org>
2026-05-28 17:39:39 -0700

    [Sessions] Remove more usages of the new keyword where applicable
    Replace usages of the new keyword with std::unique_ptr/make_unique where
    appropriate.
    
    Replaces a locally static variable `active_session_restorers` with
    base::NoDestructor to manage lifetimes easier.
    
    Bug: None
    Change-Id: I39167dc71d3cc78a365daadda080e0ad555812eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882493
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638102}

--- chrome/browser/sessions/session_restore.h
+++ chrome/browser/sessions/session_restore.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_list.h"
 #include "base/gtest_prod_util.h"
+#include "base/no_destructor.h"
 #include "base/observer_list.h"
 #include "chrome/browser/sessions/session_restore_observer.h"
 #include "components/sessions/core/session_types.h"
@@ -154,30 +155,16 @@ class SessionRestore {
 
   // Accessor for |*on_session_restored_callbacks_|. Creates a new object the
   // first time so that it always returns a valid object.
-  static CallbackList* on_session_restored_callbacks() {
-    if (!on_session_restored_callbacks_)
-      on_session_restored_callbacks_ = new CallbackList();
-    return on_session_restored_callbacks_;
-  }
+  static CallbackList* on_session_restored_callbacks();
 
   // Accessor for the observer list. Create the list the first time to always
   // return a valid reference.
-  static SessionRestoreObserverList* observers() {
-    if (!observers_)
-      observers_ = new SessionRestoreObserverList();
-    return observers_;
-  }
-
-  // Contains all registered callbacks for session restore notifications.
-  static CallbackList* on_session_restored_callbacks_;
+  static SessionRestoreObserverList* observers();
 
   // Notify SessionRestoreObservers session restore started. If there are
   // multiple concurrent session restores, observers get notified only once in
   // the first session restore.
   static void NotifySessionRestoreStartedLoadingTabs();
-
-  // Contains all registered observers for session restore events.
-  static SessionRestoreObserverList* observers_;
 
   // Whether session restore started or not.
   static bool session_restore_started_;
