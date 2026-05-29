5c1da9f7adf11b6d34c868bceb24615c9b1d3b36
Fred Mello <fredmello@google.com>
2026-05-29 13:42:05 -0700

    Shorten UrlFocusChangeListener Javadoc reference in AutocompleteMediator
    Bug: 517950180
    
    TAG=agy CONV=aa85cb7f-789d-416f-917c-6c680c9cb53c
    
    Change-Id: If44c0252ebdd7d96640b9af31ea016c2d9dff1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883682
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Fred Mello <fredmello@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638704}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -39,6 +39,7 @@ import org.chromium.chrome.browser.omnibox.LocationBarDataProvider;
 import org.chromium.chrome.browser.omnibox.OmniboxMetrics;
 import org.chromium.chrome.browser.omnibox.R;
 import org.chromium.chrome.browser.omnibox.UrlBarEditingTextStateProvider;
+import org.chromium.chrome.browser.omnibox.UrlFocusChangeListener;
 import org.chromium.chrome.browser.omnibox.fusebox.ComposeboxQueryControllerBridge;
 import org.chromium.chrome.browser.omnibox.fusebox.FuseboxAttachmentModelList;
 import org.chromium.chrome.browser.omnibox.fusebox.FuseboxAttachmentModelList.FuseboxAttachmentChangeListener;
@@ -678,8 +679,7 @@ class AutocompleteMediator
     }
 
     /**
-     * @see
-     *     org.chromium.chrome.browser.omnibox.UrlFocusChangeListener#onUrlAnimationFinished(boolean)
+     * @see UrlFocusChangeListener#onUrlAnimationFinished(boolean)
      */
     void onUrlAnimationFinished() {
         if (!isInInputSession()) {
