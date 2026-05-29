e923099771e6ac031f4c48ad5f3c4a6755b86122
Mason Freed <masonf@chromium.org>
2026-05-28 17:55:28 -0700

    Unbounded Element: Add cc feature setting [7b.5/N]
    ...and check it in a few places.
    
    See go/unbounded-element for the design doc.
    
    Bug: 508672616
    Change-Id: I44892d23b7122913f2022842a6171125eff198dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852526
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638121}

--- third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.cc
+++ third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.cc
@@ -584,6 +584,9 @@ cc::LayerTreeSettings GenerateLayerTreeSettings(
   settings.enable_backface_visibility_interop =
       RuntimeEnabledFeatures::BackfaceVisibilityInteropEnabled();
 
+  settings.enable_unbounded_element =
+      RuntimeEnabledFeatures::UnboundedElementEnabled();
+
   settings.disable_frame_rate_limit =
       cmd.HasSwitch(::switches::kDisableFrameRateLimit);
 
