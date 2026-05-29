e0262aa7cd64a9a7c3e74cbdeb68cc906df88611
Georg Neis <neis@chromium.org>
2026-05-29 07:39:39 -0700

    ash: Remove dead Lacros leftovers from SyncConsentScreen
    OBSOLETE_HISTOGRAMS=Lacros is sunsetted.
    
    Bug: b:354842935
    Change-Id: Icb9768604541a876c821e0610df421b0c0d7af99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7877744
    Auto-Submit: Georg Neis <neis@chromium.org>
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638469}

--- chrome/browser/resources/chromeos/login/screens/common/sync_consent.ts
+++ chrome/browser/resources/chromeos/login/screens/common/sync_consent.ts
@@ -43,26 +43,9 @@ enum SyncUiSteps {
   LOADING = 'loading',
 }
 
-
-/**
- * Available user actions.
- */
-enum UserAction {
-  CONTINUE = 'continue',
-  SYNC_EVERYTHING = 'sync-everything',
-  SYNC_CUSTOM = 'sync-custom',
-}
+const USER_ACTION_CONTINUE = 'continue';
 
 
-/**
- *  A set of flags of sync options for ChromeOS OOBE.
- */
-interface OsSyncItems {
-  osApps: boolean;
-  osPreferences: boolean;
-  osWifiConfigurations: boolean;
-  osWallpaper: boolean;
-}
 
 const SyncConsentScreenElementBase =
     LoginScreenMixin(MultiStepMixin(OobeI18nMixin(PolymerElement)));
@@ -78,14 +61,6 @@ export class SyncConsentScreen extends SyncConsentScreenElementBase {
 
   static get properties(): PolymerElementProperties {
     return {
-      /**
-       * OS Sync options status.
-       */
-      osSyncItemsStatus: {
-        type: Object,
-        notify: true,
-      },
-
       /**
        * Indicates whether user is minor mode user (e.g. under age of 18).
        */
@@ -118,7 +93,6 @@ export class SyncConsentScreen extends SyncConsentScreenElementBase {
     };
   }
 
-  osSyncItemsStatus: OsSyncItems;
   private isMinorMode: boolean;
   private optInButtonTextKey: string;
   private consentDescription: string[];
@@ -129,12 +103,6 @@ export class SyncConsentScreen extends SyncConsentScreenElementBase {
     super();
 
     this.isMinorMode = false;
-    this.osSyncItemsStatus = {
-      osApps: true,
-      osPreferences: true,
-      osWifiConfigurations: true,
-      osWallpaper: true,
-    };
   }
 
   override get EXTERNAL_API(): string[] {
@@ -162,16 +130,6 @@ export class SyncConsentScreen extends SyncConsentScreenElementBase {
     this.i18nUpdateLocale();
   }
 
-
-  /**
-   * Wallpaper sync is a special case; its implementation relies upon
-   * OS Settings to be synced. Thus, the wallpaper label and toggle are
-   * only enabled when the Settings sync toggle is on.
-   */
-  private onSettingsSyncedChanged(): void {
-    this.set(
-        'osSyncItemsStatus.osWallpaper', this.osSyncItemsStatus.osPreferences);
-  }
 
   /**
    * Reacts to changes in loadTimeData.
@@ -208,7 +166,7 @@ export class SyncConsentScreen extends SyncConsentScreenElementBase {
       checkedBox = reviewSettingsBox.checked;
     }
     this.userActed([
-      UserAction.CONTINUE,
+      USER_ACTION_CONTINUE,
       optedIn,
       checkedBox,
       this.getConsentDescription(),
