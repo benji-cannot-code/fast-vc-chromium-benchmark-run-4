3cff5d944b237ed4555d2917c87fbb75e4ff0537
Federica Germinario <fedegermi@google.com>
2026-05-29 05:25:19 -0700

    [ios] Fix flaky failures in TaskUpdaterSceneAgentTest test class
    Tests in `TaskUpdaterSceneAgentTest` were sometimes failing on bots
    because they depended on the real
    `UIApplication.sharedApplication.applicationState` being `Active`.
    
    In `task_updater_scene_agent.mm`, `canUpdateToUIReady` returns `NO` if
    the application state is not `Active`. In unit test environments, this
    state is not guaranteed to be `Active` and can cause tests to fail by
    staying in the `ProfileLoaded` stage instead of transitioning to
    `UIReady`.
    
    This CL adds a partial mock for `[UIApplication sharedApplication]` in
    `SetUp` and stubs `applicationState` to return
    `UIApplicationStateActive`. It also adds proper clean up in `TearDown`
    to stop mocking.
    
    Bug: None
    Change-Id: I74d40536c7c89d2dde99aa995d6a42893886e1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882145
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638410}

--- ios/chrome/browser/shared/coordinator/scene/task_updater_scene_agent_unittest.mm
+++ ios/chrome/browser/shared/coordinator/scene/task_updater_scene_agent_unittest.mm
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
+#import "third_party/ocmock/OCMock/OCMock.h"
+#import "third_party/ocmock/gtest_support.h"
 
 // Fake SceneState to set sceneSessionID.
 @interface TaskUpdaterFakeSceneState : SceneState
@@ -105,6 +107,15 @@ class TaskUpdaterSceneAgentTest : public PlatformTest {
 
     agent_ = [[TaskUpdaterSceneAgent alloc] init];
     [scene_state_ addAgent:agent_];
+
+    mock_application_ = OCMPartialMock([UIApplication sharedApplication]);
+    OCMStub([mock_application_ applicationState])
+        .andReturn(UIApplicationStateActive);
+  }
+
+  void TearDown() override {
+    [(OCMockObject*)mock_application_ stopMocking];
+    PlatformTest::TearDown();
   }
 
   AuthenticationService* auth_service() {
@@ -125,6 +136,7 @@ class TaskUpdaterSceneAgentTest : public PlatformTest {
   ProfileState* profile_state_;
   TaskUpdaterFakeSceneState* scene_state_;
   TaskUpdaterSceneAgent* agent_;
+  id mock_application_;
 };
 
 // Tests that TaskExecutionProfileLoaded is sent when profile is loaded.
