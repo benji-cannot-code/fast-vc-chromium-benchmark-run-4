83043bfcfec40d4b73fa5984c1bedb0ebda4fbb8
Koji Ishii <kojii@chromium.org>
2026-05-28 23:05:08 -0700

    [line-breaker] Take `box-decoration-break` into account for available width
    This patch fixes the effective available width to take
    `box-decoration-break` into account.
    
    Fixed: 40756909
    Change-Id: I74c762ae8e49283ec804e2c8b0c6d437eeed8ddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883118
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638249}

--- third_party/blink/renderer/core/layout/inline/line_breaker.h
+++ third_party/blink/renderer/core/layout/inline/line_breaker.h
@@ -315,6 +315,7 @@ class CORE_EXPORT LineBreaker {
            (parent_breaker_ && !auto_wrap_);
   }
   void UpdateAvailableWidth();
+  void UpdateAvailableWidthFromBaseAvailableWidth();
 
   // True if the current line is hyphenated.
   bool HasHyphen() const { return hyphen_index_.has_value(); }
@@ -351,6 +352,8 @@ class CORE_EXPORT LineBreaker {
   LayoutUnit position_;
   LayoutUnit applied_text_indent_;
   LayoutUnit available_width_;
+  // Available width without `box-decoration-break`.
+  LayoutUnit base_available_width_;
   LineLayoutOpportunity line_opportunity_;
 
   InlineNode node_;
