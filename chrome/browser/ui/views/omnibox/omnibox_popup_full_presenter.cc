8f8cfc1ebbaa9f2cddd3fa45bca3a63773d641f6
Marlon Facey <mfacey@google.com>
2026-05-29 12:22:10 -0700

    [WebUI Omnibox] Fix focus out behavior
    This CL fixes the mouse click out behavior by explicitly focusing on
    the main web content and set caret visibility to be false. It also
    fixes escape behavior by explicitly setting caret visibility to be true
    and disabling the KillFocus call from the WebUI, which resets the
    caret visibility.
    
    Cast: http://screencast/cast/NTg3NDE0MzI0NDMyMDc2OHwzZjYxOTRmNi1kNQ
    
    Bug: b:504668974
    Change-Id: I400b10620a0793265f6f5b55af0ec33c7dd58010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868485
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638654}

--- chrome/browser/ui/views/omnibox/omnibox_popup_full_presenter.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_full_presenter.cc
@@ -130,6 +130,10 @@ void OmniboxPopupFullPresenter::OnWidgetActivationChanged(views::Widget* widget,
         return;
       }
     }
+
+    controller()->client()->FocusWebContents();
+    controller()->edit_model()->SetCaretVisibility(false);
+
     controller()->popup_state_manager()->SetPopupState(
         OmniboxPopupState::kNone);
   }
