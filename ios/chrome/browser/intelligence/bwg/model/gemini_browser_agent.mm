ff5f73e425e1880a447430d71e4d8352ada4b190
Scott Yoder <scottyoder@google.com>
2026-05-29 10:43:01 -0700

    [ios] Fix Gemini testFloatyHiddenDuringLensOverlay
    This change fixes testFloatyHiddenDuringLensOverlay when
    FullscreenRefactoring is enabled. The code that handles the new
    Fullscreen callbacks need to guard the logic in the same way as the
    legacy Fullscreen callbacks, in order to inadvertently show Floaty when
    the NTP or TabGrid is presented.
    
    Change-Id: I2ab5a8f3ff2ce6943c66a2bc52a07c81fb431845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883247
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638579}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -1330,12 +1330,18 @@ void GeminiBrowserAgent::FullscreenViewportInsetRangeChanged(
 #pragma mark - FullscreenBrowserAgentObserver
 
 void GeminiBrowserAgent::WillUpdateState(FullscreenBrowserAgent* agent) {
+  if (!is_floaty_invoked_ || is_floaty_temporarily_hidden_) {
+    return;
+  }
   ios::provider::UpdateOverlayOffsetWithOpacity(GetFloatyOffset(),
                                                 GetFloatyProgress());
 }
 
 void GeminiBrowserAgent::DidUpdateObscuredInsetRange(
     FullscreenBrowserAgent* agent) {
+  if (!is_floaty_invoked_ || is_floaty_temporarily_hidden_) {
+    return;
+  }
   ios::provider::UpdateOverlayOffsetWithOpacity(GetFloatyOffset(),
                                                 GetFloatyProgress());
 }
