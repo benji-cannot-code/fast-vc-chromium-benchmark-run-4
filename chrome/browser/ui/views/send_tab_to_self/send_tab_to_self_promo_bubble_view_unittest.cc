481484dbcfc065785c16c2cdc50beabb3d61bd11
Michael Tatarski <mtatarski@google.com>
2026-05-29 07:13:15 -0700

    [STTS] Split SendTabToSelfPromoBubbleView into two views
    This CL is a purely mechanical, structural refactoring. It splits the
    monolithic SendTabToSelfPromoBubbleView into two separate, decoupled
    classes:
    
    1. SendTabToSelfNoTargetDeviceBubbleView (shown when signed in but no
    targets exist).
    
    2. SendTabToSelfSignInPromoBubbleView (shown when signed out, offering
    sign-in).
    
    Bug: 488252159
    Change-Id: I9c990d230432da0ded0c4ae7c892092792a27132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7877279
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638460}

--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_promo_bubble_view_unittest.cc
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_promo_bubble_view_unittest.cc
@@ -62,11 +62,20 @@ class SendTabToSelfPromoBubbleViewTest : public ChromeViewsTestBase {
                                std::move(controller));
   }
 
-  void CreateBubble(SendTabToSelfPromoBubbleView::PromoType promo_type) {
-    bubble_ = new SendTabToSelfPromoBubbleView(
+  void CreateSignInPromoBubble(bool is_account_aware) {
+    auto* bubble = new SendTabToSelfSignInPromoBubbleView(
         views::BubbleAnchor(anchor_widget_->GetContentsView()),
-        web_contents_.get(), promo_type);
-    views::BubbleDialogDelegateView::CreateBubble(bubble_);
+        web_contents_.get(), is_account_aware);
+    views::BubbleDialogDelegateView::CreateBubble(bubble);
+    bubble_ = bubble;
+  }
+
+  void CreateNoTargetDeviceBubble() {
+    auto* bubble = new SendTabToSelfNoTargetDeviceBubbleView(
+        views::BubbleAnchor(anchor_widget_->GetContentsView()),
+        web_contents_.get());
+    views::BubbleDialogDelegateView::CreateBubble(bubble);
+    bubble_ = bubble;
   }
 
   void TearDown() override {
@@ -84,7 +93,7 @@ class SendTabToSelfPromoBubbleViewTest : public ChromeViewsTestBase {
   content::RenderViewHostTestEnabler test_render_host_factories_;
   std::unique_ptr<content::WebContents> web_contents_;
   std::unique_ptr<views::Widget> anchor_widget_;
-  raw_ptr<SendTabToSelfPromoBubbleView> bubble_ = nullptr;
+  raw_ptr<SendTabToSelfBubbleView> bubble_ = nullptr;
   // Owned by WebContents.
   raw_ptr<StubSendTabToSelfBubbleController> controller_;
 };
@@ -93,7 +102,7 @@ TEST_F(SendTabToSelfPromoBubbleViewTest,
        InitLayout_EnhancedUiEnabled_LoadsModernizedDesign) {
   feature_list_.InitAndEnableFeature(kSendTabToSelfEnhancedDesktopUI);
 
-  CreateBubble(SendTabToSelfPromoBubbleView::PromoType::kSignInPromo);
+  CreateSignInPromoBubble(/*is_account_aware=*/false);
 
   // Title should match modernized title strictly.
   EXPECT_EQ(
@@ -114,7 +123,7 @@ TEST_F(SendTabToSelfPromoBubbleViewTest,
        InitLayout_EnhancedUiDisabled_LoadsLegacyDesign) {
   feature_list_.InitAndDisableFeature(kSendTabToSelfEnhancedDesktopUI);
 
-  CreateBubble(SendTabToSelfPromoBubbleView::PromoType::kSignInPromo);
+  CreateSignInPromoBubble(/*is_account_aware=*/false);
 
   // Title should match legacy title strictly.
   EXPECT_EQ(bubble_->GetWindowTitle(),
@@ -134,7 +143,7 @@ TEST_F(SendTabToSelfPromoBubbleViewTest,
        InitLayout_NoTargetDevices_LoadsDeviceActivityDesign) {
   feature_list_.InitAndEnableFeature(kSendTabToSelfEnhancedDesktopUI);
 
-  CreateBubble(SendTabToSelfPromoBubbleView::PromoType::kNoTargetDevice);
+  CreateNoTargetDeviceBubble();
 
   // Ok button should not be visible.
   EXPECT_FALSE(bubble_->GetOkButton());
