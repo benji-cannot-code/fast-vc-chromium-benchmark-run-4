8445d2a9ef62c56927667d53cb464cc3ea0b0a58
Brian Lefler <bcl@google.com>
2026-05-29 05:58:40 -0700

    [MaliciousApkDownloadCheck] Show Generic Warning for Poten. Unwanted
    POTENTIALLY_UNWANTED will no longer show the malicious UX and instead
    fallback to the old generic warning.
    
    Screenshots/Screencast: https://drive.google.com/drive/folders/1wSRNcZyA_NvQ7rCupZK3Yp55wZ5wDIny?resourcekey=0-V9b7VxbjYjk1Ewy35nFPdg&usp=sharing
    
    Test: out/Default/unit_tests
    Bug: 397407934
    Change-Id: I3cec595a675c7c568845493127ada2bb0fefd2fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881326
    Reviewed-by: Yaw Frempong <yawfrempong@google.com>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Brian Lefler <bcl@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638422}

--- chrome/browser/download/android/download_controller.cc
+++ chrome/browser/download/android/download_controller.cc
@@ -456,7 +456,9 @@ void DownloadController::ShowDangerousDownloadWarning(DownloadUIModel& model) {
   // For generic filetype warnings, fall back to the DangerousDownloadDialog.
   // TODO(crbug.com/397407934): Consider implementing matching UX for
   // DOWNLOAD_DANGER_TYPE_DANGEROUS_FILE to replace DangerousDownloadDialog.
-  if (item->GetDangerType() == download::DOWNLOAD_DANGER_TYPE_DANGEROUS_FILE) {
+  if (item->GetDangerType() == download::DOWNLOAD_DANGER_TYPE_DANGEROUS_FILE ||
+      item->GetDangerType() ==
+          download::DOWNLOAD_DANGER_TYPE_POTENTIALLY_UNWANTED) {
     OnDangerousDownload(item);
   }
 }
