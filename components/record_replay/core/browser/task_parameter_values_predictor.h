1e6881aaaac68e5ddbadff930596a339dac32a91
Rafal Godlewski <rgod@google.com>
2026-05-29 06:17:37 -0700

    [RecordReplay] Add noop impl of Task Parameter Value Predictor
    For now it always returns std::nullopt, in a follow-up CL it will return
    a value based on the historical observations.
    
    Bug: 517857125
    Change-Id: I108625e0e843a2a49ccf2961b343adf135d5d919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883402
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638431}

---
+++ components/record_replay/core/browser/task_parameter_values_predictor.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_RECORD_REPLAY_CORE_BROWSER_TASK_PARAMETER_VALUES_PREDICTOR_H_
+#define COMPONENTS_RECORD_REPLAY_CORE_BROWSER_TASK_PARAMETER_VALUES_PREDICTOR_H_
+
+#include <optional>
+#include <vector>
+
+#include "base/functional/callback.h"
+
+namespace record_replay {
+
+class TaskDefinition;
+class TaskParameter;
+
+// Predicts parameter values for a provided task definition.
+class TaskParameterValuesPredictor {
+ public:
+  TaskParameterValuesPredictor();
+  TaskParameterValuesPredictor(const TaskParameterValuesPredictor&) = delete;
+  TaskParameterValuesPredictor& operator=(const TaskParameterValuesPredictor&) =
+      delete;
+  ~TaskParameterValuesPredictor();
+
+  // Predicts the values for a given `task_definition`.
+  void Predict(
+      const TaskDefinition& task_definition,
+      base::OnceCallback<void(std::optional<std::vector<TaskParameter>>)>
+          completion_callback);
+};
+
+}  // namespace record_replay
+
+#endif  // COMPONENTS_RECORD_REPLAY_CORE_BROWSER_TASK_PARAMETER_VALUES_PREDICTOR_H_
