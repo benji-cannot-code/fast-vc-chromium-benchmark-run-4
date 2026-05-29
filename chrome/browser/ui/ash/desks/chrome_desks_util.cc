d87249b219f6422f7fe0f99c1253b55b1e780a52
Georg Neis <neis@chromium.org>
2026-05-28 18:21:22 -0700

    ash: Fix CHECK failure in saved desk template restore
    Fix a bug in desk template conversion's ToSyncProto that could lead to
    desk data where the tab group information was inconsistent (out of
    bounds) w.r.t. the tabs.
    
    Because such data already exists in the wild, and in case there are
    other code paths that can lead to inconsistent data, also add a simple
    work-around to the consuming code.
    
    Bug: 508666018
    Change-Id: I635c72c7eff67fc5bae6a681e77409a768b3466f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870494
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638147}

--- chrome/browser/ui/ash/desks/chrome_desks_util.cc
+++ chrome/browser/ui/ash/desks/chrome_desks_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/ash/desks/chrome_desks_util.h"
 
+#include "base/logging.h"
 #include "chrome/browser/ash/browser_delegate/browser_delegate.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "components/tab_groups/tab_group_id.h"
@@ -34,8 +35,16 @@ std::vector<tab_groups::TabGroupInfo> ConvertTabGroupsToTabGroupInfos(
 void AttachTabGroupsToBrowserInstance(
     const std::vector<tab_groups::TabGroupInfo>& tab_groups,
     ash::BrowserDelegate* browser) {
+  const size_t tab_count = browser->GetWebContentsCount();
   for (const tab_groups::TabGroupInfo& tab_group : tab_groups) {
-    browser->CreateTabGroup(tab_group);
+    if (tab_group.tab_range.IsValid() && !tab_group.tab_range.is_empty() &&
+        !tab_group.tab_range.is_reversed() &&
+        tab_group.tab_range.end() <= tab_count) {
+      browser->CreateTabGroup(tab_group);
+    } else {
+      LOG(WARNING) << "Skipping tab group restoration: invalid range "
+                   << tab_group.tab_range.ToString();
+    }
   }
 }
 
