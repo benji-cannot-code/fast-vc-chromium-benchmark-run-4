3577a88033a2b5bcfaee0daba22c0e08b9ebd5de
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-05-29 16:16:36 -0700

    [Default Browser] Implement WebUI frontend for Visual Guided Setter
    See: go/default-browser-visual-guided-setter
    
    Bug: 454597786
    Change-Id: I382f19f10bdab02ad5b3073a7f4b4bafdf3839d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7877119
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638802}

---
+++ chrome/browser/resources/default_browser/visual_guided_setter/visual_guided_setter.ts
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
+
+function initialize() {
+  // TODO(https://crbug.com/454597786): Setup logic for the native OS
+  // integration (Mojo, ResizeObserver, etc.) will be injected here in the
+  // subsequent CL in this chain.
+  ColorChangeUpdater.forDocument().start();
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
--- chrome/browser/ui/webui/default_browser/visual_guided_setter_ui.cc
+++ chrome/browser/ui/webui/default_browser/visual_guided_setter_ui.cc
@@ -7,13 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/visual_guided_setter_resources.h"
 #include "chrome/grit/visual_guided_setter_resources_map.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/webui/webui_util.h"
 
 VisualGuidedSetterUI::VisualGuidedSetterUI(content::WebUI* web_ui)
@@ -21,6 +24,22 @@ VisualGuidedSetterUI::VisualGuidedSetterUI(content::WebUI* web_ui)
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       web_ui->GetWebContents()->GetBrowserContext(),
       chrome::kChromeUIDefaultBrowserVisualGuidedSetterHost);
+
+  source->AddLocalizedString(
+      "title", IDS_DEFAULT_BROWSER_VISUAL_GUIDED_SETTER_PAGE_TITLE);
+  source->AddLocalizedString(
+      "headerTitle", IDS_DEFAULT_BROWSER_VISUAL_GUIDED_SETTER_HEADER_TITLE);
+  source->AddLocalizedString(
+      "contentTitle", IDS_DEFAULT_BROWSER_VISUAL_GUIDED_SETTER_CONTENT_TITLE);
+  source->AddLocalizedString(
+      "contentDescription",
+      IDS_DEFAULT_BROWSER_VISUAL_GUIDED_SETTER_CONTENT_DESCRIPTION);
+  source->AddLocalizedString(
+      "instructionStep1",
+      IDS_DEFAULT_BROWSER_VISUAL_GUIDED_SETTER_INSTRUCTION_STEP_1);
+
+  source->AddResourcePath("chrome_logo.svg", IDR_PRODUCT_LOGO_SVG);
+
   webui::SetupWebUIDataSource(
       source, base::span(kVisualGuidedSetterResources),
       IDR_VISUAL_GUIDED_SETTER_VISUAL_GUIDED_SETTER_HTML);
