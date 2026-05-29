7871a932eabd8243189b65eefcf8f380cd16576a
yoshiki iguchi <yoshiki@google.com>
2026-05-29 04:05:30 -0700

    ASH Refactoring: Move IDS_SECURITY_* to ash/
    Move all 13 IDS_SECURITY_* strings from chromeos_strings.grdp to
    ash/ash_strings.grd as they are only used in ash and related ChromeOS-specific
    browser features.
    
    Update includes accordingly.
    
    - Moves 13 corresponding translation screenshot files.
    - Removes 3 obsolete includes.
    - Removes 1 obsolete DEPS rules.
    
    Moved string IDs:
      IDS_SECURITY_CURTAIN_DESCRIPTION
      IDS_SECURITY_CURTAIN_TITLE
      IDS_SECURITY_TOKEN_SESSION_IMMEDIATE_LOCK_NOTIFICATION_BODY
      IDS_SECURITY_TOKEN_SESSION_IMMEDIATE_LOGOUT_NOTIFICATION_BODY
      IDS_SECURITY_TOKEN_SESSION_LOCK_MESSAGE_TITLE
      IDS_SECURITY_TOKEN_SESSION_LOCK_NOTIFICATION_BODY
      IDS_SECURITY_TOKEN_SESSION_LOCK_NOTIFICATION_BUTTON_TITLE
      IDS_SECURITY_TOKEN_SESSION_LOCK_NOTIFICATION_TITLE
      IDS_SECURITY_TOKEN_SESSION_LOGOUT_MESSAGE_BODY
      IDS_SECURITY_TOKEN_SESSION_LOGOUT_MESSAGE_TITLE
      IDS_SECURITY_TOKEN_SESSION_LOGOUT_NOTIFICATION_BODY
      IDS_SECURITY_TOKEN_SESSION_LOGOUT_NOTIFICATION_BUTTON_TITLE
      IDS_SECURITY_TOKEN_SESSION_LOGOUT_NOTIFICATION_TITLE
    
    Bug: 487122295
    Test: CQ passes
    Change-Id: Ia7fd1c2387854d8d205541da5cac48e462ac01f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7870682
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638384}

--- chrome/browser/ui/webui/ash/curtain_ui/remote_maintenance_curtain_ui.cc
+++ chrome/browser/ui/webui/ash/curtain_ui/remote_maintenance_curtain_ui.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/curtain_ui/remote_maintenance_curtain_ui.h"
 
 #include "ash/constants/webui_url_constants.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/login/oobe_ui.h"
-#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/remote_maintenance_curtain_resources.h"
 #include "chrome/grit/remote_maintenance_curtain_resources_map.h"
 #include "content/public/browser/web_ui_data_source.h"
