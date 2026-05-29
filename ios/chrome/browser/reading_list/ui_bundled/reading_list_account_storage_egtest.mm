38aa641238fd87c078dfb42b603c5e5c1f3685ba
Ewann Pellé <ewannpv@chromium.org>
2026-05-28 23:56:17 -0700

    [iOS] Re-enable Chrome Next in ReadingListAccountStorageTestCase
    Bug: 514608938
    Change-Id: Idfba629d5d14519193a7a6e908b2299c6cce9d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883142
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638257}

--- ios/chrome/browser/reading_list/ui_bundled/reading_list_account_storage_egtest.mm
+++ ios/chrome/browser/reading_list/ui_bundled/reading_list_account_storage_egtest.mm
@@ -100,10 +100,6 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
 
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config = [super appConfigurationForTestCase];
-  // TODO(crbug.com/514608938): Fix test for Chrome Next.
-  if ([self isRunningTest:@selector(testSignInPromoInIncognito)]) {
-    config.features_disabled.push_back(kChromeNextIa);
-  }
   return config;
 }
 
