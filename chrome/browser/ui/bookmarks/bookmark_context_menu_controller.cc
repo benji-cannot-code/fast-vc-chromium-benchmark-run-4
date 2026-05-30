d1ed02f6f7dc58f5f75e3cf2ac2395a71be5af9c
Jennifer Serrano <jennserrano@google.com>
2026-05-29 18:04:50 -0700

    Create bookmark bar submenu
    A simplified version of the submenu is created to add translations. The
    menu still needs logic to keep the menu open and have the check items
    behavior like radio items.
    
    Bug: 503036610
    Change-Id: I85886a61b016455f823a29bdbf092de7f1ba9140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885387
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638877}

--- chrome/browser/ui/bookmarks/bookmark_context_menu_controller.cc
+++ chrome/browser/ui/bookmarks/bookmark_context_menu_controller.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/policy_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/saved_tab_groups/public/features.h"
+#include "components/search/ntp_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/tab_groups/tab_group_visual_data.h"
 #include "components/undo/bookmark_undo_service.h"
@@ -178,6 +179,7 @@ BookmarkContextMenuController::BookmarkContextMenuController(
   CheckSelectionIsValid(selection);
   CHECK(new_nodes_parent_);
   menu_model_ = std::make_unique<ui::SimpleMenuModel>(this);
+  submenu_model_ = std::make_unique<ui::SimpleMenuModel>(this);
   bookmark_service_->bookmark_model()->AddObserver(this);
 
   BuildMenu();
@@ -307,7 +309,20 @@ void BookmarkContextMenuController::BuildMenu() {
   }
   AddCheckboxItem(IDC_BOOKMARK_BAR_SHOW_MANAGED_BOOKMARKS,
                   IDS_BOOKMARK_BAR_SHOW_MANAGED_BOOKMARKS_DEFAULT_NAME);
-  AddCheckboxItem(IDC_BOOKMARK_BAR_ALWAYS_SHOW, IDS_SHOW_BOOKMARK_BAR);
+
+  if (base::FeatureList::IsEnabled(
+          ntp_features::kNtpSimplificationBookmarkBar)) {
+    submenu_model_->AddCheckItemWithStringId(
+        IDC_BOOKMARK_BAR_ALWAYS_SHOW, IDS_BOOKMARK_BAR_SUBMENU_ALWAYS_HIDE);
+    submenu_model_->AddCheckItemWithStringId(
+        IDC_BOOKMARK_BAR_ALWAYS_SHOW, IDS_BOOKMARK_BAR_SUBMENU_ALWAYS_SHOW);
+    submenu_model_->AddCheckItemWithStringId(
+        IDC_BOOKMARK_BAR_ALWAYS_SHOW, IDS_BOOKMARK_BAR_SUBMENU_ONLY_ON_NTP);
+    menu_model_->AddSubMenuWithStringId(
+        IDC_SHOW_BOOKMARK_BAR, IDS_SHOW_BOOKMARK_BAR, submenu_model_.get());
+  } else {
+    AddCheckboxItem(IDC_BOOKMARK_BAR_ALWAYS_SHOW, IDS_SHOW_BOOKMARK_BAR);
+  }
 }
 
 void BookmarkContextMenuController::AddItem(int id, const std::u16string str) {
