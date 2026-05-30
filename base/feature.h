f663c6c8d0fdb21e3bf33f03ae32497d930aec60
Julia Hansbrough <flowerhack@google.com>
2026-05-29 17:46:05 -0700

    [window.rust] Provide initial Rust API for base::Feature.
    Chromium developers frequently use Features so that Finch can roll out
    field trials to subsets of users.
    
    But, right now, there's no ergonomic way to set those Features from Rust
    itself.
    
    This CL offers an FFI wrapper around //base/feature and unit tests for
    the same.
    
    Follow-on CLs will provide idiomatic APIs for other necessary elements
    (FeatureParam, FieldTrial, etc).
    
    Bug: 507165212
    
    Change-Id: I4fb315c8ad5b45070537355792c020d21d0f0115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787991
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638859}

--- base/feature.h
+++ base/feature.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 class FeatureList;
+class FeatureListTest;
 
 // Recommended macros for declaring and defining features and parameters:
 //
@@ -213,6 +214,7 @@ struct BASE_EXPORT LOGICALLY_CONST Feature {
 
  private:
   friend class FeatureList;
+  friend class FeatureListTest;
 
   // A packed value where the first 8 bits represent the `OverrideState` of this
   // feature, the next 8 bits are reserved for flags (e.g. logging), and the
