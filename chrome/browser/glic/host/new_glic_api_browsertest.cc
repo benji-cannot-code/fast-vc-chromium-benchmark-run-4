b1b1b0832754d90e16823464c341941dd280a8c9
Trevor Perrier <perrier@google.com>
2026-05-29 19:57:24 -0700

    [glic] Add feature flag to disable WebUi loader
    Adds the kGlicNoWebUiLoader feature flag. When enabled this skips
    showing a Chrome WebUi loader and immediately shows the guest webview.
    
    Also adds HostCapability.kNoWebUiLoader and includes this in the
    capabilities when the flag is enabled.
    
    Fix: 515818441
    Change-Id: I6b5673a131fa51aa06a52faf38f868a5a6e5ee00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878116
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Auto-Submit: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638927}

--- chrome/browser/glic/host/new_glic_api_browsertest.cc
+++ chrome/browser/glic/host/new_glic_api_browsertest.cc
@@ -138,6 +138,7 @@ namespace {
 std::vector<std::string> GetTestSuiteNames() {
   std::vector<std::string> names = {
       "NewGlicApiTest",
+      "NewGlicApiTestForNoWebUiLoader",
       "NewGlicApiTestWithFastTimeout",
       "NewGlicApiTestWithWebContentsWarming",
       "NewGlicApiTestWithPixelOutput",
@@ -170,6 +171,7 @@ struct TestParams {
   bool trust_first_onboarding_arm1 = false;
   bool trust_first_onboarding_arm2 = false;
   bool auto_open_pdf = false;
+  bool enable_no_web_ui_loader = false;
 };
 
 class WithTestParams : public testing::WithParamInterface<TestParams> {
@@ -190,6 +192,9 @@ class WithTestParams : public testing::WithParamInterface<TestParams> {
     }
     if (info.param.auto_open_pdf) {
       result.push_back("AutoOpenPdf");
+    }
+    if (info.param.enable_no_web_ui_loader) {
+      result.push_back("EnableNoWebUiLoader");
     }
     if (result.empty()) {
       return "Default";
@@ -369,6 +374,42 @@ class NewGlicApiTestWithDefaultTabContextDisabled : public NewGlicApiTest {
 IN_PROC_BROWSER_TEST_P(NewGlicApiTestWithDefaultTabContextDisabled,
                        testDefaultTabContextApiIsUndefinedWhenFeatureDisabled) {
   ASSERT_OK(OpenGlicForActiveTab());
+  ExecuteJsTest();
+}
+
+class NewGlicApiTestForNoWebUiLoader : public NewGlicApiTest {
+ public:
+  NewGlicApiTestForNoWebUiLoader() {
+    if (GetParam().enable_no_web_ui_loader) {
+      feature_list_.InitWithFeatures({features::kGlicNoWebUiLoader}, {});
+    } else {
+      feature_list_.InitWithFeatures({}, {features::kGlicNoWebUiLoader});
+    }
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_P(NewGlicApiTestForNoWebUiLoader, testNoWebUiLoader) {
+  ToggleGlicForActiveTab(/*prevent_close=*/true);
+  auto* instance = GetOnlyGlicInstance();
+  ASSERT_TRUE(instance);
+
+  WebUIStateListener listener(&instance->host());
+
+  ASSERT_OK(WaitForGlicOpen());
+
+  auto* web_contents = instance->host().webui_contents();
+  ASSERT_TRUE(web_contents);
+  ASSERT_TRUE(content::WaitForLoadStop(web_contents));
+
+  // Wait for kReady state to ensure loading is complete.
+  ASSERT_TRUE(WaitForWebUiState(mojom::WebUiState::kReady).has_value());
+
+  bool expect_loading = !GetParam().enable_no_web_ui_loader;
+  EXPECT_EQ(expect_loading, listener.SawState(mojom::WebUiState::kShowLoading));
+
   ExecuteJsTest();
 }
 
@@ -1634,6 +1675,13 @@ INSTANTIATE_TEST_SUITE_P(,
                          DefaultTestParamSet(),
                          &WithTestParams::PrintTestVariant);
 
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    NewGlicApiTestForNoWebUiLoader,
+    testing::Values(TestParams{.enable_no_web_ui_loader = false},
+                    TestParams{.enable_no_web_ui_loader = true}),
+    &WithTestParams::PrintTestVariant);
+
 INSTANTIATE_TEST_SUITE_P(,
                          NewGlicApiTestWithWebActuationSettingDisabled,
                          DefaultTestParamSet(),
@@ -1677,6 +1725,7 @@ GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(
     NewGlicApiTestWithWebActuationSettingEnabled);
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(
     NewGlicApiTestWithProcessCounterAbuseVerdictDisabled);
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(NewGlicApiTestForNoWebUiLoader);
 #if !BUILDFLAG(IS_ANDROID)
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(NewGlicApiTestWithSkills);
 #endif
--- chrome/browser/glic/public/features.cc
+++ chrome/browser/glic/public/features.cc
@@ -192,4 +192,5 @@ BASE_FEATURE(kGlicSetWebContentsVisibilityWhenToggling,
 
 BASE_FEATURE(kGlicProcessCounterAbuseVerdict,
              base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kGlicNoWebUiLoader, base::FEATURE_DISABLED_BY_DEFAULT);
 }  // namespace features
