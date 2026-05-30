2e9db48b70fc43a309cfba38f7f5b065f5eb41ee
Adam Yao <adamyao@google.com>
2026-05-30 12:40:44 -0700

    Setup FPOP Service Component Interface and Proto
    This CL introduces the initial structure and public interface for the
    Footprints One Platform (Fpop) service within the contextual_search
    component.
    
    Bug: 517991968
    Change-Id: I8d499fe0dd0d129dc8d379953138a00c556a9237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817168
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Adam Yao <adamyao@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Auto-Submit: Adam Yao <adamyao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639038}

---
+++ components/contextual_search/footprints/public/constants.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CONTEXTUAL_SEARCH_FOOTPRINTS_PUBLIC_CONSTANTS_H_
+#define COMPONENTS_CONTEXTUAL_SEARCH_FOOTPRINTS_PUBLIC_CONSTANTS_H_
+
+#include <stdint.h>
+
+namespace contextual_search {
+
+// Setting ID for CONTEXTUAL_SEARCH_DRIVE_DISCLAIMER_ACCEPTED.
+inline constexpr int32_t kContextualSearchDriveDisclaimerAccepted = 274;
+
+}  // namespace contextual_search
+
+#endif  // COMPONENTS_CONTEXTUAL_SEARCH_FOOTPRINTS_PUBLIC_CONSTANTS_H_
