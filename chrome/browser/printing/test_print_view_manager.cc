97a49e13ddf2cf22dae229d43d9afdad55f1f6b8
Lei Zhang <thestig@chromium.org>
2026-05-29 19:09:30 -0700

    Print Preview: Move code from PrintViewManagerBase to PrintViewManager
    Ideally, PrintViewManager is PrintViewManagerBase plus Print Preview
    bits, but Print Preview code managed to sneak into PrintViewManagerBase
    over time. Move some of that code, mainly GetPrintPreviewParams() and
    related bits, to PrintViewManager to better align with the way the code
    should be organized. This refactoring is a follow-up to
    crrev.com/1635224, which added some Print Preview code to
    PrintViewManagerBase for the ease of code review and cherry-picking.
    
    Change-Id: I6a7920b700167d92f1d28c6bafa28669ec8a0182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7884451
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638911}

--- chrome/browser/printing/test_print_view_manager.cc
+++ chrome/browser/printing/test_print_view_manager.cc
@@ -159,7 +159,7 @@ void TestPrintViewManager::PrintPreviewAllowedForTesting() {
 
 void TestPrintViewManager::GetPrintPreviewParams(
     GetPrintPreviewParamsCallback callback) {
-  PrintViewManagerBase::GetPrintPreviewParams(
+  PrintViewManager::GetPrintPreviewParams(
       base::BindOnce(&OnDidGetPrintPreviewParams, std::ref(snooped_params_),
                      std::move(callback)));
 }
