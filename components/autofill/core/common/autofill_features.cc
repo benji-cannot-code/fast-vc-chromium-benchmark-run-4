bdd9b3c49728c35d971a808cd9d4fb47ca47f853
Henry Tran <hhtran@google.com>
2026-05-29 19:37:37 -0700

    Add kAutofillAiWalletPassBranding2026 flag
    Bug: 514203233
    Change-Id: I7f72958e5b5035cad15d43e12a7c30d45154bc27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863500
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Auto-Submit: Henry Tran <hhtran@google.com>
    Commit-Queue: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638921}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -340,6 +340,10 @@ BASE_FEATURE(kAutofillAiWalletFlightReservation,
              IS_AUTOFILL_AI_PLATFORM ? base::FEATURE_ENABLED_BY_DEFAULT
                                      : base::FEATURE_DISABLED_BY_DEFAULT);
 
+// Enables the 2026 Autofill AI Wallet Pass Branding Updates.
+BASE_FEATURE(kAutofillAiWalletPassBranding2026,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // If enabled, AutofillAi supports private passes entities from Google Wallet.
 BASE_FEATURE(kAutofillAiWalletPrivatePasses, base::FEATURE_DISABLED_BY_DEFAULT);
 
@@ -1075,7 +1079,6 @@ BASE_FEATURE(kManageTravelPerceptionSurvey, base::FEATURE_DISABLED_BY_DEFAULT);
 // settings page.
 BASE_FEATURE(kManageYourSavedInfoPerceptionSurvey,
              base::FEATURE_DISABLED_BY_DEFAULT);
-
 
 // If enabled, an entry point to AtMemory will be shown at the bottom of the
 // Autocomplete dialogs.
