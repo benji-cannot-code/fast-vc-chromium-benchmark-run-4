29cc00be5146e32d6013eaf6f669e60c6045ef77
Duong Dac <ddac@chromium.org>
2026-05-30 08:54:31 -0700

    Add kEnforceMustFetchAppleAgeRangeInChrome capability for iOS
    Bug: 517512727
    Change-Id: I880f4334b60f1c6960a36f4aa087a40200e3b791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882401
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639015}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -475,6 +475,11 @@ const base::FeatureParam<base::TimeDelta>
 #endif
 
 #if BUILDFLAG(IS_IOS)
+BASE_FEATURE(kEnforceMustFetchAppleAgeRangeInChromeCapability,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
+
+#if BUILDFLAG(IS_IOS)
 BASE_FEATURE(kEnforceMustSkipAppleAgeRangeInChromeCapability,
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
