9110c194b6b44e556014897a8a2fa245c9c9fda2
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-05-31 12:51:53 -0700

    Make PrefetchHandle callbacks async behind kill switch
    To avoid uncontrolled-by-prefetch logic synchronously called from
    `PrefetchContainerObserver`.
    
    In non-test code, this makes the calls to the following methods async,
    which aren't expected not performance-critical:
    - `SearchPreloadService::OnSuggestPrefetchCompletedOrFailed()`
    - `SearchPreloadService::OnPrefetchHeadReceived()`
    
    In `PrefetchHandleImpl::PrefetchContainerObserver::OnDeterminedHead()`
    - `on_prefetch_head_received_` is called also on `kCompleted`, because
      `PrefetchContainer` can (at least theoretically) transition during
      async tasks. This is no-op with `kPrefetchAsyncPrefetchHandleCallback`
      disabled, because when called synchronously it's never `kCompleted`.
    - When a `PrefetchContainer` transitions from `kDeterminedHead` to
      `kFailed`, `on_prefetch_head_received_` isn't called if
      `kPrefetchAsyncPrefetchHandleCallback` is enabled and the transition
      occurs during the async task, while it's always called before this
      CL or if `kPrefetchAsyncPrefetchHandleCallback` is disabled.
      This behavior change should be quite rare and anyway looks
      consistent.
    
    Bug: 480271813, 400761083
    Change-Id: I811b194d293b32f84f2d9c745e6e749bed3e6644
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7536557
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639084}

--- content/browser/preloading/prefetch/prefetch_handle_impl.h
+++ content/browser/preloading/prefetch/prefetch_handle_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "content/browser/preloading/prefetch/prefetch_container_async_observer.h"
 #include "content/public/browser/prefetch_handle.h"
 
 namespace content {
@@ -46,10 +47,26 @@ class PrefetchHandleImpl final : public PrefetchHandle {
  private:
   class PrefetchContainerObserverForCallback;
 
+  // The `content::PrefetchContainerObserver` to be registered to
+  // `PrefetchContainer`.
+  PrefetchContainerObserver& GetObserver() const;
+  // The underlying `PrefetchHandleImpl::PrefetchContainerObserver`, for setting
+  // the callbacks.
+  PrefetchContainerObserverForCallback& GetUnderlyingObserver() const;
+
   base::WeakPtr<PrefetchService> prefetch_service_;
   base::WeakPtr<PrefetchContainer> prefetch_container_;
+
+  using AsyncObserverForCallback =
+      PrefetchContainerAsyncObserver<PrefetchContainerObserverForCallback>;
+
+  // Exactly one of these two is non-null, depending on
+  // `features::kPrefetchAsyncPrefetchHandleCallback`.
+  // TODO(crbug.com/480271813): Cleanup this once the feature is settled.
   std::unique_ptr<PrefetchContainerObserverForCallback>
       prefetch_container_observer_;
+  std::unique_ptr<AsyncObserverForCallback> prefetch_container_async_observer_;
+
   std::optional<PrefetchStatus> prefetch_status_on_release_started_prefetch_;
 };
 
