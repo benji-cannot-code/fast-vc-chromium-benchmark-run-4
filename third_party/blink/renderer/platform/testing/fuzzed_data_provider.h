b07c9d57a654a6da40a33de0c59129c64376e271
Kalvin Lee <kdlee@chromium.org>
2026-06-01 15:15:07 -0700

    text_codec_fuzzer: Use `reinterpret_span()`
    Use the centrally blessed `reinterpret_span()` to mint a view of
    `UChar`, removing an `UNSAFE_BUFFERS()` in the process.
    
    Bug: 516384462
    Change-Id: I08609847b24d218357594077dbb8a191b65cfb44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878925
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639719}

--- third_party/blink/renderer/platform/testing/fuzzed_data_provider.h
+++ third_party/blink/renderer/platform/testing/fuzzed_data_provider.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fuzzer/FuzzedDataProvider.h>
 
+#include <vector>
+
 #include "base/compiler_specific.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -28,6 +30,12 @@ class FuzzedDataProvider {
 
   // Returns a String containing all remaining bytes of the input data.
   std::string ConsumeRemainingBytes();
+
+  // Generic version of `ConsumeRemainingBytes()` above.
+  template <typename T>
+  std::vector<T> ConsumeRemainingBytesAs() {
+    return provider_.ConsumeRemainingBytes<T>();
+  }
 
   // Returns a bool, or false when no data remains.
   bool ConsumeBool() { return provider_.ConsumeBool(); }
