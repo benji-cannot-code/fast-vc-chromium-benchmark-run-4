0c32b7a64fba7a3740e1294739a37296b3e787be
Kamil Jarosz <kjarosz@google.com>
2026-06-01 04:30:45 -0700

    private_insights: Add basic private_insights component and service
    Introduce a new `private_insights` component under
    `components/metrics/private_metrics/private_insights`.
    
    In the future, this will be likely moved into its own component.
    
    Bug: b:518646894
    Change-Id: I443bfe6c3eb47823a2c77256c415ea0d3d0e90e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885344
    Commit-Queue: Matt Dembski <dembski@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639290}

---
+++ components/metrics/private_metrics/private_insights/private_insights_service.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/metrics/private_metrics/private_insights/private_insights_service.h"
+
+namespace private_insights {
+
+PrivateInsightsService::PrivateInsightsService() = default;
+PrivateInsightsService::~PrivateInsightsService() = default;
+
+}  // namespace private_insights
---
+++ components/metrics/private_metrics/private_insights/private_insights_service.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_PRIVATE_INSIGHTS_SERVICE_H_
+#define COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_PRIVATE_INSIGHTS_SERVICE_H_
+
+#include "base/component_export.h"
+#include "base/files/file_path.h"
+#include "base/memory/scoped_refptr.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace private_insights {
+
+class COMPONENT_EXPORT(PRIVATE_INSIGHTS) PrivateInsightsService
+    : public KeyedService {
+ public:
+  PrivateInsightsService();
+  ~PrivateInsightsService() override;
+
+  PrivateInsightsService(const PrivateInsightsService&) = delete;
+  PrivateInsightsService& operator=(const PrivateInsightsService&) = delete;
+};
+
+}  // namespace private_insights
+
+#endif  // COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_PRIVATE_INSIGHTS_SERVICE_H_
