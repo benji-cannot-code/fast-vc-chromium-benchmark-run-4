2fc6afc1ea9648816e7b243981ef01ec5f39d0a4
Jack Thiesen <jthiesen@chromium.org>
2026-06-01 16:12:23 -0700

    Re-enable ExtensionWindowControllerBridgeIntegrationTests on FREEFORM Form Factor
    Include-Ci-Only-Tests: chrome.android.desktop:android-desktop-16-x64-rel-emu-tests|chrome_public_test_apk
    Bug: 511289518
    Change-Id: I53dbf1376542f56dc4389be311d49a26ac283aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891407
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639756}

--- chrome/browser/ui/android/extensions/windowing/internal/java/src/org/chromium/chrome/browser/ui/extensions/windowing/ExtensionWindowControllerBridgeIntegrationTest.java
+++ chrome/browser/ui/android/extensions/windowing/internal/java/src/org/chromium/chrome/browser/ui/extensions/windowing/ExtensionWindowControllerBridgeIntegrationTest.java
@@ -23,7 +23,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
@@ -151,7 +150,6 @@ public class ExtensionWindowControllerBridgeIntegrationTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP /* Test needs "new window" in app menu. */)
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289158
     public void startChromeTabbedActivity_openNewWindow_notifyExtensionInternalsOfWindowCreation() {
         // Arrange:
         // (1) Launch ChromeTabbedActivity (the first window).
@@ -189,7 +187,6 @@ public class ExtensionWindowControllerBridgeIntegrationTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP /* Test needs "new window" in app menu. */)
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289158
     public void
             openIncognitoWindow_destroyIncognitoTabModel_notifyExtensionInternalsOfWindowDestruction() {
         // Arrange:
@@ -367,7 +364,6 @@ public class ExtensionWindowControllerBridgeIntegrationTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP /* Test needs "new window" in app menu. */)
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289158
     public void startChromeTabbedActivity_triggerTaskFocusChange_notifyExtensionWindowController() {
         // Arrange:
         // (1) Launch ChromeTabbedActivity (the first window).
