065f85e4a13fd80b2905c98d488be169d80990a2
Charles Hager <clhager@google.com>
2026-06-01 15:08:14 -0700

    [Side Panel] End transitions early if configuration is changed
    When a configuration change occurs while a side UI transition is in
    progress, force the transition to end before requesting a static update.
    This avoids layout issues caused by resizing during transitions, such as
    the configuration change requesting a static update with outdated
    values.
    
    Bug: 513302000
    Change-Id: I420cd6b9a2fc9de1476c2380eaaf03584f154ff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7884031
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639709}

--- chrome/browser/ui/side_ui/internal/android/java/src/org/chromium/chrome/browser/ui/side_ui/SideUiCoordinatorImpl.java
+++ chrome/browser/ui/side_ui/internal/android/java/src/org/chromium/chrome/browser/ui/side_ui/SideUiCoordinatorImpl.java
@@ -221,7 +221,12 @@ final class SideUiCoordinatorImpl implements SideUiCoordinator, ConfigurationCha
         assert mSideUiContainers.size() == 1;
         var sideUiContainer = mSideUiContainers.get(0);
 
-        // 1. Get the current SideUiSpecs.
+        // 1. End any existing transitions still in progress. This needs to be done before checking
+        // the current specs, since specs aren't fully updated until after all transitions have
+        // finished.
+        TransitionManager.endTransitions(getRootView());
+
+        // 2. Get the current SideUiSpecs.
         SideUiSpecs currentSideUiSpecs = getCurrentSideUiSpecs();
         @AnchorSide int currentAnchorSide = sideUiContainer.getAnchorSide();
         @Px
@@ -230,7 +235,7 @@ final class SideUiCoordinatorImpl implements SideUiCoordinator, ConfigurationCha
                         ? currentSideUiSpecs.getWidth(AnchorSide.LEFT)
                         : currentSideUiSpecs.getWidth(AnchorSide.RIGHT);
 
-        // 2. Check if we need to close/re-open side UI.
+        // 3. Check if we need to close/re-open side UI.
         //
         // Currently there is only one SideUiContainer, so we only need to check if there is enough
         // space for that container.
@@ -246,7 +251,7 @@ final class SideUiCoordinatorImpl implements SideUiCoordinator, ConfigurationCha
         @Px int minSideUiWidth = ViewUtils.dpToPx(mParentActivity, sideUiContainer.getMinWidthDp());
         boolean canShowSideUi = (windowWidth - minWebContentsWidth - minSideUiWidth >= 0);
 
-        // 2.1. Check if we need to close side UI.
+        // 4.1. Check if we need to close side UI.
         if (currentSideUiWidth != 0 && !canShowSideUi) {
             // Don't simply close the side UI by calling requestUpdateContainerInternal() with a
             // zero width. SideUiContainer may need to save states so that it can restore its UI
@@ -257,7 +262,7 @@ final class SideUiCoordinatorImpl implements SideUiCoordinator, ConfigurationCha
             return;
         }
 
-        // 2.2 Check if we need to re-open side UI.
+        // 4.2 Check if we need to re-open side UI.
         if (currentSideUiWidth == 0 && canShowSideUi) {
             // Similarly, we shouldn't call requestUpdateContainerInternal() here.
             // SideUiContainer needs to check whether there actually exists side UI to restore,
@@ -266,7 +271,7 @@ final class SideUiCoordinatorImpl implements SideUiCoordinator, ConfigurationCha
             return;
         }
 
-        // 3. At this point, we don't need to close or re-open side UI, so we'll just resize side
+        // 5. At this point, we don't need to close or re-open side UI, so we'll just resize side
         // UI by requesting a UI update with the current specs. This will re-calculate the
         // SideUiSpecs, and if the re-calculated SideUiSpecs is different, the UI will be updated.
         requestUpdateContainerInternal(
