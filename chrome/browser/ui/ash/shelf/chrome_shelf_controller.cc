98e8e943957e613e4ff089539b209a5133608e50
yoshiki iguchi <yoshiki@google.com>
2026-05-31 22:27:36 -0700

    ASH Refactoring: Clean up 4 includes and dependencies in C/B/ui/ui/ash
    This CL removes:
    - 3 unnecessary includes from 3 .cc files
    - 1 unnecessary deps from 1 DEPS files
    
    Bug: 487122295
    Test: Ran `autoninja -C out_amd64-generic/Release chrome`
    Test: CQ passed
    Change-Id: Iabd672421b1aa9cd0e12551f00bb37dfe793d292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882777
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639172}

--- chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
-#include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "chromeos/ash/components/browser_context_helper/annotated_account_id.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
