2632c3ced99326749a691d67098dd2f7cd0803b3
Brandon Wylie <wylieb@google.com>
2026-06-01 10:37:48 -0700

    [aim] Use #isFuseboxEligible for ntp plus viz
    Bug: 517779563
    Change-Id: I429fef76dbf2ef4478c5e71fd9b44039dcb5f8a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883772
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639505}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediatorUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediatorUnitTest.java
@@ -56,6 +56,8 @@ import org.chromium.chrome.browser.lens.LensController;
 import org.chromium.chrome.browser.lens.LensIntentParams;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.ntp.NewTabPageManager;
+import org.chromium.chrome.browser.omnibox.fusebox.ComposeboxQueryControllerBridge;
+import org.chromium.chrome.browser.omnibox.fusebox.ComposeboxQueryControllerBridgeJni;
 import org.chromium.chrome.browser.omnibox.status.StatusProperties.StatusIconResource;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
@@ -88,6 +90,7 @@ public class SearchBoxMediatorUnitTest {
     @Mock private Profile mProfile;
     @Mock private ComposeplateUtils.Natives mComposeplateUtilsJni;
     @Mock private TemplateUrlService mTemplateUrlService;
+    @Mock private ComposeboxQueryControllerBridge.Natives mComposeboxBridgeJni;
     @Captor private ArgumentCaptor<TemplateUrlServiceObserver> mTemplateUrlServiceObserverCaptor;
 
     private Context mContext;
@@ -113,6 +116,8 @@ public class SearchBoxMediatorUnitTest {
 
         ComposeplateUtilsJni.setInstanceForTesting(mComposeplateUtilsJni);
         when(mComposeplateUtilsJni.isAimEntrypointEligible(any())).thenReturn(false);
+        ComposeboxQueryControllerBridgeJni.setInstanceForTesting(mComposeboxBridgeJni);
+        when(mComposeboxBridgeJni.isFuseboxEligibleForProfile(any())).thenReturn(true);
 
         mMediator =
                 new SearchBoxMediator(
@@ -183,11 +188,20 @@ public class SearchBoxMediatorUnitTest {
     @Test
     @EnableFeatures(OmniboxFeatureList.OMNIBOX_MULTIMODAL_INPUT + ":show_ntp_plus_button/true")
     public void testUpdateStartIcon_AllConditionsMet() {
-        when(mComposeplateUtilsJni.isAimEntrypointEligible(any())).thenReturn(true);
         verify(mTemplateUrlService).addObserver(mTemplateUrlServiceObserverCaptor.capture());
         mTemplateUrlServiceObserverCaptor.getValue().onTemplateURLServiceChanged();
 
         assertTrue(mPropertyModel.get(SearchBoxProperties.PLUS_BUTTON_VISIBILITY));
+    }
+
+    @Test
+    @EnableFeatures(OmniboxFeatureList.OMNIBOX_MULTIMODAL_INPUT + ":show_ntp_plus_button/true")
+    public void testUpdateStartIcon_PolicyDisabled() {
+        when(mComposeboxBridgeJni.isFuseboxEligibleForProfile(any())).thenReturn(false);
+        verify(mTemplateUrlService).addObserver(mTemplateUrlServiceObserverCaptor.capture());
+        mTemplateUrlServiceObserverCaptor.getValue().onTemplateURLServiceChanged();
+
+        assertFalse(mPropertyModel.get(SearchBoxProperties.PLUS_BUTTON_VISIBILITY));
     }
 
     @Test
@@ -200,7 +214,7 @@ public class SearchBoxMediatorUnitTest {
     @Test
     @EnableFeatures(OmniboxFeatureList.OMNIBOX_MULTIMODAL_INPUT)
     public void testUpdateStartIcon_NotEligible() {
-        when(mComposeplateUtilsJni.isAimEntrypointEligible(any())).thenReturn(false);
+        when(mComposeboxBridgeJni.isFuseboxEligibleForProfile(any())).thenReturn(false);
         verify(mTemplateUrlService).addObserver(mTemplateUrlServiceObserverCaptor.capture());
         mTemplateUrlServiceObserverCaptor.getValue().onTemplateURLServiceChanged();
 
@@ -211,7 +225,6 @@ public class SearchBoxMediatorUnitTest {
     @EnableFeatures(OmniboxFeatureList.OMNIBOX_MULTIMODAL_INPUT)
     public void testUpdateStartIcon_NotGoogle() {
         when(mTemplateUrlService.isDefaultSearchEngineGoogle()).thenReturn(false);
-        when(mComposeplateUtilsJni.isAimEntrypointEligible(any())).thenReturn(true);
 
         verify(mTemplateUrlService).addObserver(mTemplateUrlServiceObserverCaptor.capture());
         mTemplateUrlServiceObserverCaptor.getValue().onTemplateURLServiceChanged();
@@ -222,7 +235,6 @@ public class SearchBoxMediatorUnitTest {
     @Test
     @DisableFeatures(OmniboxFeatureList.OMNIBOX_MULTIMODAL_INPUT)
     public void testUpdateStartIcon_FeatureDisabled() {
-        when(mComposeplateUtilsJni.isAimEntrypointEligible(any())).thenReturn(true);
         verify(mTemplateUrlService).addObserver(mTemplateUrlServiceObserverCaptor.capture());
         mTemplateUrlServiceObserverCaptor.getValue().onTemplateURLServiceChanged();
 
