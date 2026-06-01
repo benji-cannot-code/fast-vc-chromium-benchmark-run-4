23edda7481b0c1427fc5ed75c6b1ac0ea27c5d96
Norge Vizcay <vizcay@google.com>
2026-06-01 07:12:49 -0700

    account-settings: Convert kAccountSettingContext to a kill switch
    Renames kAccountSettingContext to kAccountSettingContextKillSwitch and
    enables it by default. This allows the feature to be disabled via
    server-side configuration if needed.
    
    Bug: 518740027
    Change-Id: I47163dd22155e9157f70a35fbce8dc5a7f35bba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7889499
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639361}

--- components/account_settings/account_settings_features.cc
+++ components/account_settings/account_settings_features.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace account_settings::features {
 
-// Feature flag to gate context-related account settings.
-BASE_FEATURE(kAccountSettingContext, base::FEATURE_DISABLED_BY_DEFAULT);
+// Kill switch to gate context-related account settings.
+BASE_FEATURE(kAccountSettingContextKillSwitch,
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace account_settings::features
--- components/account_settings/account_settings_features.h
+++ components/account_settings/account_settings_features.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace account_settings::features {
 
-BASE_DECLARE_FEATURE(kAccountSettingContext);
+BASE_DECLARE_FEATURE(kAccountSettingContextKillSwitch);
 
 }  // namespace account_settings::features
 
