9d8cac020aed353a4f306931c42bea432eeca97d
Kay Lin <kaiyilin@google.com>
2026-06-01 10:19:56 -0700

    Site Search: Coordinator destroy refine
    This CL refines the destroy() methods of site search related components.
    
    Bug: 518087480
    Change-Id: I477c29065cdd644d66503692b040a02b42e93bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882586
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639488}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_search_engine/CustomSearchEngineListCoordinator.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_search_engine/CustomSearchEngineListCoordinator.java
@@ -82,8 +82,8 @@ public class CustomSearchEngineListCoordinator {
 
     public void destroy() {
         mSiteSearchDialogCoordinator.dismiss();
-        mModel.set(SiteSearchProperties.ADAPTER, null);
         mPropertyModelChangeProcessor.destroy();
+        mModel.set(SiteSearchProperties.ADAPTER, null);
         mMediator.destroy();
         mAdapter.destroy();
     }
