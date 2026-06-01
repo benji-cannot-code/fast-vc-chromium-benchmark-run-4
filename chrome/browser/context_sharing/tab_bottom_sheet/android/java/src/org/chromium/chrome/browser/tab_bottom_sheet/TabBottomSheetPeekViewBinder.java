05506c2608103db5c618fcef1b645c3a56d1e16b
Fiaz Muhammad <mfiaz@google.com>
2026-06-01 12:13:00 -0700

    [TabBottomSheet] Modularize public classes to 'public_java'
    Combine PeekView module with TabBottomSheetManager module.
    
    Change-Id: I39097f387a2e4cf4c9f58e5a277d7c49bf63986b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871712
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639574}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetPeekViewBinder.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetPeekViewBinder.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab_bottom_sheet;
 
 import org.chromium.build.annotations.NullMarked;
-import org.chromium.chrome.browser.tab_bottom_sheet.peek_view.TabBottomSheetPeekProperties;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
