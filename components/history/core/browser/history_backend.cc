61081930de2edd5de0af133cbab0a1112ac0687d
Marc Treib <treib@chromium.org>
2026-06-01 03:06:22 -0700

    History: Don't consider IP literals for "typed" promotion
    The history component automatically promotes intranet hosts (like
    "http://myhost") to a "TYPED" transition type, so that the omnibox will
    recognize the known host in the future.
    
    Before this CL, IP literals were also detected as "intranet hosts" and
    so the same promotion logic would (accidentally) apply to them.
    
    This CL fixes this by excluding IP literals from
    IsUntypedIntranetHost().
    
    Fixed: 513741393
    Change-Id: Ia52da2625c3da177fc35e119e3e5b54c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7864242
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639258}

--- components/history/core/browser/history_backend.cc
+++ components/history/core/browser/history_backend.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/client_tag_based_data_type_processor.h"
 #include "components/url_formatter/url_formatter.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "net/base/url_util.h"
 #include "sql/error_delegate_util.h"
 #include "sql/sqlite_result_code.h"
 #include "sql/sqlite_result_code_values.h"
@@ -927,6 +928,15 @@ void HistoryBackend::MarkVisitAsKnownToSync(VisitID visit_id) {
 bool HistoryBackend::IsUntypedIntranetHost(const GURL& url) {
   if (!url.SchemeIs(url::kHttpScheme) && !url.SchemeIs(url::kHttpsScheme) &&
       !url.SchemeIs(url::kFtpScheme)) {
+    return false;
+  }
+
+  // A host is usually considered "intranet" if it has no eTLD suffix (no ".com"
+  // or ".co.uk" etc), i.e. its "registry length" is zero - see
+  // net/base/registry_controlled_domains/registry_controlled_domain.h for more
+  // details. However, IP addresses also don't have an eTLD, but do not
+  // generally belong to an intranet, so they must be separately excluded here.
+  if (url.HostIsIPAddress() && !net::IsLocalhost(url)) {
     return false;
   }
 
