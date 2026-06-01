aece616a4ff42dc0d1e95990f592ac3684bde009
Vidhan Jain <vidhanj@google.com>
2026-06-01 09:16:28 -0700

    [AmbientAutofill] Add converters from Personal Context Entity to EntityInstance
    This CL introduces helper utilities to convert personal context entities
    to Autofill AI's local EntityInstance data model.
    
    Bug: 516721244
    Change-Id: I144f76941fe5c0d783cc022af3834e75cbe5f192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883074
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639448}

---
+++ components/autofill/core/browser/network/autofill_ai/personal_context_conversion_util.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_PERSONAL_CONTEXT_CONVERSION_UTIL_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_PERSONAL_CONTEXT_CONVERSION_UTIL_H_
+
+#include <optional>
+
+namespace personal_context::proto {
+class Entity;
+}  // namespace personal_context::proto
+
+namespace autofill {
+
+class EntityInstance;
+
+// Converts a generic `personal_context::proto::Entity` to an `EntityInstance`.
+std::optional<EntityInstance> PersonalContextEntityToEntityInstance(
+    const personal_context::proto::Entity& entity);
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_PERSONAL_CONTEXT_CONVERSION_UTIL_H_
