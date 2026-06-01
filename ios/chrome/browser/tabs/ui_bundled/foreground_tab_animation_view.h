094d633de419219bd9f3d35ecb2afbe50c223a97
Gauthier Ambard <gambard@chromium.org>
2026-06-01 08:48:48 -0700

    [iOS] Fix new tab animation
    When opening a new tab in landscape with Next, the AppBar covering the
    tab should be taken into consideration.
    This includes the corner radius that should be the one from the AppBar.
    
    Before: https://screencast.googleplex.com/cast/NjQ2ODU5ODk2Mzg5NjMyMHw2Yjc2NTJkOS1kZg
    After: https://screencast.googleplex.com/cast/NjM1NjM4OTIxODc0NjM2OHxiNGZhYWVkNC0zYQ
    
    Fixed: 517471899
    Change-Id: Ie98fb1b582ffc6aeb72ae2974aed8c21aceae5a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890356
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639429}

--- ios/chrome/browser/tabs/ui_bundled/foreground_tab_animation_view.h
+++ ios/chrome/browser/tabs/ui_bundled/foreground_tab_animation_view.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+enum class AppBarPosition;
+
 // View used to contain an animation of a new tab opening in the foreground.
 // A content subview animates to fill the view while the background fades to
 // black.
@@ -23,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // If DeviceCornerRadius should be applied to the contentView, ideally only when
 // the contentView takes the full screen. Default is NO.
 @property(nonatomic, assign) BOOL useDeviceCornerRadius;
+
+// Indicates the position of the App Bar, to apply the correct corner radius.
+@property(nonatomic, assign) AppBarPosition appBarPosition;
 
 // Starts a New Tab animation in `parentView`, from `originPoint` with
 // a `completion` block. The new tab will scale up and move from the direction
