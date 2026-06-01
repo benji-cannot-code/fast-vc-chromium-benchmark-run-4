12786c1da270705d1fee84d3de3aaefaf8d59d6c
James Cook <jamescook@chromium.org>
2026-06-01 16:36:12 -0700

    extensions: Port ContentVerifierHashTest to desktop Android
    The tests pass without modification.
    
    Bug: 469417243
    Change-Id: I99dc832e84b4e3e02411d6fd17bba00e7b53f79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891525
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639771}

--- chrome/browser/extensions/content_verifier_hash_fetch_behavior_browsertest.cc
+++ chrome/browser/extensions/content_verifier_hash_fetch_behavior_browsertest.cc
@@ -25,8 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/test_extension_registry_observer.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/file_util.h"
 #include "services/network/public/cpp/features.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
