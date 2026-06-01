ab6a41b254d14bdda531602f0fb60abb79ddc982
Devon Loehr <dloehr@google.com>
2026-06-01 11:59:50 -0700

    [rustmojo] Allow MessagePipeWatcher to delay the start of processing
    Previously, the `MessagePipeWatcher` class automatically started running
    handlers as soon as it was set up (assuming there were notifications to
    be processed). However, it's possible that a user may not want that,
    for example if their handler uses shared data that needs to be
    initialized before the handler starts running.
    
    This CL adds a parameter that lets them specify whether the watcher
    should start immediately or wait, and adds a function to start the
    process manually in the latter case.
    
    Bug: 493274453
    Change-Id: I6b9d37901aacf3da7ec5d0073f0d63c3090ff4ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883542
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639564}

--- mojo/public/rust/bindings/test/tests.rs
+++ mojo/public/rust/bindings/test/tests.rs
@@ -65,8 +65,8 @@ fn test_watcher_basic() {
         }
     };
 
-    let sender = MessagePipeWatcher::new(sender, sender_msg_handler, None).unwrap();
-    let receiver = MessagePipeWatcher::new(receiver, receiver_msg_handler, None).unwrap();
+    let sender = MessagePipeWatcher::new(sender, sender_msg_handler, None, true).unwrap();
+    let receiver = MessagePipeWatcher::new(receiver, receiver_msg_handler, None, true).unwrap();
 
     // Send some messages through; this should trigger the handler twice
     sender.send_message(RawMojoMessage::new_with_bytes(b"Message 1").unwrap()).unwrap();
@@ -104,6 +104,7 @@ fn test_watcher_disconnect() {
         receiver,
         receiver_msg_handler,
         Some(Box::new(receiver_disconnect_handler)),
+        true,
     )
     .unwrap();
 
@@ -143,9 +144,13 @@ fn test_watcher_disconnect_immediately() {
     };
 
     drop(sender);
-    let receiver =
-        MessagePipeWatcher::new(receiver, |_, _| {}, Some(Box::new(receiver_disconnect_handler)))
-            .unwrap();
+    let receiver = MessagePipeWatcher::new(
+        receiver,
+        |_, _| {},
+        Some(Box::new(receiver_disconnect_handler)),
+        true,
+    )
+    .unwrap();
 
     run_loop.run();
 
