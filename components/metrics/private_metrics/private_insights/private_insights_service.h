f4ab080410f95d8a11162e11e07cce069e4fb75b
Kamil Jarosz <kjarosz@google.com>
2026-06-01 16:58:31 -0700

    private_insights: Add unit tests for PrivateInsightsService
    Introduce unit tests for PrivateInsightsService in a new test suite,
    private_insights_service_unittest.cc.
    
    Specifically, this adds a test to verify that TriggerUpload() does not
    schedule more than one task.
    
    Also, wire up the new unit_tests target in BUILD.gn to the parent
    components/metrics/private_metrics:unit_tests target.
    
    Bug: b:518719944
    Change-Id: I442e2ff8b0f81e77fe2d740b51772eec67b496ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891505
    Commit-Queue: Matt Dembski <dembski@google.com>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639786}

--- components/metrics/private_metrics/private_insights/private_insights_service.h
+++ components/metrics/private_metrics/private_insights/private_insights_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/files/file_path.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
@@ -55,6 +56,9 @@ class COMPONENT_EXPORT(PRIVATE_INSIGHTS) PrivateInsightsService
 
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<PrivateInsightsService> weak_ptr_factory_{this};
+
+  FRIEND_TEST_ALL_PREFIXES(PrivateInsightsServiceTest,
+                           TriggerUploadSkipsPostingTaskWhenAlreadyRunning);
 };
 
 }  // namespace private_insights
