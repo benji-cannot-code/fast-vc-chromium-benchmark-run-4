bc703affa74bde75b7a7aed3c11674fa14a6a14a
Mike Taylor <miketaylr@chromium.org>
2026-06-01 12:40:27 -0700

    [SmartRestart] Re-add SessionCrashed.Bubble histogram
    As we experiment with automatic restarts, it is useful for us to know if
    we see movement in session restore "crashes", for any reasons.
    
    Effectively a revert of
    https://chromium-review.git.corp.google.com/c/chromium/src/+/5050226
    
    Change-Id: Ib13c591013c14a01263cc24035a3401c3a736608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885162
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639592}

--- chrome/browser/ui/views/session_crashed_bubble_view.cc
+++ chrome/browser/ui/views/session_crashed_bubble_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/scoped_observation.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
@@ -61,6 +62,24 @@ namespace {
 
 views::BubbleDialogModelHost* g_instance_for_test = nullptr;
 
+enum SessionCrashedBubbleHistogramValue {
+  SESSION_CRASHED_BUBBLE_SHOWN,
+  SESSION_CRASHED_BUBBLE_ERROR,
+  SESSION_CRASHED_BUBBLE_RESTORED,
+  SESSION_CRASHED_BUBBLE_ALREADY_UMA_OPTIN,
+  SESSION_CRASHED_BUBBLE_UMA_OPTIN,
+  SESSION_CRASHED_BUBBLE_HELP,
+  SESSION_CRASHED_BUBBLE_IGNORED,
+  SESSION_CRASHED_BUBBLE_OPTIN_BAR_SHOWN,
+  SESSION_CRASHED_BUBBLE_STARTUP_PAGES,
+  SESSION_CRASHED_BUBBLE_MAX,
+};
+
+void RecordBubbleHistogramValue(SessionCrashedBubbleHistogramValue value) {
+  UMA_HISTOGRAM_ENUMERATION("SessionCrashed.Bubble", value,
+                            SESSION_CRASHED_BUBBLE_MAX);
+}
+
 bool DoesSupportConsentCheck() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   return true;
@@ -74,6 +93,7 @@ void OpenUmaLink(BrowserWindowInterface* browser, const ui::Event& event) {
       GURL("https://support.google.com/chrome/answer/96817"),
       ui::DispositionFromEventFlags(event.flags(),
                                     WindowOpenDisposition::NEW_FOREGROUND_TAB));
+  RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_HELP);
 }
 
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kUmaConsentCheckboxId);
@@ -102,16 +122,25 @@ class SessionCrashedBubbleDelegate : public ui::DialogModelDelegate {
   }
 
   void OpenStartupPages(BrowserWindowInterface* browser) {
+    ignored_ = false;
     MaybeEnableUma();
     dialog_model()->host()->Close();
 
+    RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_STARTUP_PAGES);
     // Opening tabs has side effects, so it's preferable to do it after the
     // bubble was closed.
     SessionRestore::OpenStartupPagesAfterCrash(
         browser->GetBrowserForMigrationOnly());
   }
 
+  void OnWindowClosing() {
+    if (ignored_) {
+      RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_IGNORED);
+    }
+  }
+
   void RestorePreviousSession(BrowserWindowInterface* browser) {
+    ignored_ = false;
     MaybeEnableUma();
     // The call to Close() deletes this. Grab the lock so that session restore
     // is triggered before the lock is destroyed, otherwise ExitTypeService
@@ -120,6 +149,7 @@ class SessionCrashedBubbleDelegate : public ui::DialogModelDelegate {
         std::move(crashed_lock_);
     dialog_model()->host()->Close();
 
+    RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_RESTORED);
     // Restoring tabs has side effects, so it's preferable to do it after the
     // bubble was closed.
     SessionRestore::RestoreSessionAfterCrash(
@@ -140,10 +170,12 @@ class SessionCrashedBubbleDelegate : public ui::DialogModelDelegate {
       // also has history (and other) syncs enabled?
       change_metrics_reporting_state_callback_.Run(
           metrics::MetricsReportingLevel::kBasic);
+      RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_UMA_OPTIN);
     }
   }
 
  private:
+  bool ignored_ = true;
   std::unique_ptr<ExitTypeService::CrashedLock> crashed_lock_;
   SessionCrashedBubbleView::ChangeMetricsReportingStateCallback
       change_metrics_reporting_state_callback_;
@@ -227,9 +259,11 @@ void SessionCrashedBubbleView::Show(
 
   if (browser && (skip_tab_checking ||
                   browser->GetTabStripModel()->GetActiveWebContents())) {
-    ShowBubble(browser, offer_uma_optin);
+    ShowBubble(browser, uma_opted_in_already, offer_uma_optin);
     return;
   }
+
+  RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_ERROR);
 }
 
 // static
@@ -255,6 +289,7 @@ void SessionCrashedBubbleView::
 
 views::BubbleDialogModelHost* SessionCrashedBubbleView::ShowBubble(
     BrowserWindowInterface* browser,
+    bool uma_opted_in_already,
     bool offer_uma_optin) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser);
   // TODO(webium): WebUI browser does not use BrowserView. Make an WebUI anchor
@@ -276,9 +311,14 @@ views::BubbleDialogModelHost* SessionCrashedBubbleView::ShowBubble(
       .SetTitle(l10n_util::GetStringUTF16(IDS_SESSION_CRASHED_BUBBLE_TITLE))
       .DisableCloseOnDeactivate()
       .SetIsAlertDialog()
+      .SetDialogDestroyingCallback(
+          base::BindOnce(&SessionCrashedBubbleDelegate::OnWindowClosing,
+                         base::Unretained(bubble_delegate)))
       .AddParagraph(ui::DialogModelLabel(IDS_SESSION_CRASHED_VIEW_MESSAGE));
 
   if (offer_uma_optin) {
+    RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_OPTIN_BAR_SHOWN);
+
     dialog_builder.AddCheckbox(
         kUmaConsentCheckboxId,
         ui::DialogModelLabel::CreateWithReplacement(
@@ -315,5 +355,9 @@ views::BubbleDialogModelHost* SessionCrashedBubbleView::ShowBubble(
       std::move(bubble), views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET)
       ->Show();
 
+  RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_SHOWN);
+  if (uma_opted_in_already) {
+    RecordBubbleHistogramValue(SESSION_CRASHED_BUBBLE_ALREADY_UMA_OPTIN);
+  }
   return bubble_ptr;
 }
