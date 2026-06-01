f5140e5a906609e4a7839556aad0b943d6808973
Arthur Eubanks <aeubanks@google.com>
2026-06-01 13:09:18 -0700

    Suppress notes for suppressed warnings
    Clang now emits -Wunsafe-buffer-usage at the end of the function rather than at the end of the TU, which allows it to provide notes for the warnings.
    
    The plugin didn't handle the notes since from a consumer point of view they're not directly associated with the warning. Emitted notes are right after the warning, so we keep track of if the last warning we saw was suppressed and suppress all notes until the next warning/error.
    
    Bug: 517697740
    Change-Id: I1faa923781f0de275d45f3cc6db48266474240c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890716
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639615}

---
+++ tools/clang/plugins/tests/unsafe_buffers_not_clean_dir/skipped_header.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKIPPED_HEADER_H_
+#define SKIPPED_HEADER_H_
+
+template <typename T>
+void unsafe_template_func(T* p) {
+  p[5] = 0;  // This should warn
+}
+
+#endif  // SKIPPED_HEADER_H_
