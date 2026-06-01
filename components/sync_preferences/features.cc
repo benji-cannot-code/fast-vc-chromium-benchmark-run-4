8af61b3e4bb1f9880b5954c69d36a61a09de5a3c
Ankush Singh <ankushkush@google.com>
2026-06-01 04:09:13 -0700

    Enable kAccountScopedPrefs feature flag by default
    This feature flag enables usage of WriteBehavior::kWriteToAccountOnly in
    syncable prefs database. All prefs tagged with kWriteToAccountOnly are
    written only to the account-store and are cleared upon signout.
    
    Bug: 441437179
    Change-Id: Ic28c4757357d393ffdd77e81b9634c6c999932c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7888122
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639283}

--- components/sync_preferences/features.cc
+++ components/sync_preferences/features.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sync_preferences::features {
 
-BASE_FEATURE(kAccountScopedPrefs, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAccountScopedPrefs, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableCrossDevicePrefTracker,
 #if BUILDFLAG(IS_IOS)
