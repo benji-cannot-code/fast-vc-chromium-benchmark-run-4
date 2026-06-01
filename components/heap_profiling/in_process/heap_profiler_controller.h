0d1bcd7e9158372ade88f9d87f7b725c1bad92f2
Joe Mason <joenotcharles@google.com>
2026-06-01 12:43:40 -0700

    Add UMA metric for ratio of actual to requested sampling interval
    Add UMA metrics to heap_profiler/in_process that, at every snapshot, logs the ratio of the requested sampling interval to the actual sampling interval in use as a percentage.
    
    This allows detecting if another user of PoissonAllocationSampler has modified the sampling rate.
    
    The metric is named HeapProfiling.InProcess.SamplingIntervalVariance.
    
    TAG=agy
    CONV=884f5d4a-6d94-4cac-82a7-f9c7c7f86f26
    R=etiennep
    
    Bug: 40915258
    Change-Id: I2cc92b8d84110e698cf288f255e3b9ccd2de1d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876035
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639593}

--- components/heap_profiling/in_process/heap_profiler_controller.h
+++ components/heap_profiling/in_process/heap_profiler_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
+#include "base/byte_size.h"
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
@@ -128,6 +129,7 @@ class HeapProfilerController {
                    scoped_refptr<StoppedFlag> stopped,
                    ProcessType process_type,
                    base::TimeTicks profiler_creation_time,
+                   base::ByteSize expected_sampling_interval,
                    base::OnceClosure on_first_snapshot_callback);
 
     // Creates params for a single snapshot.
@@ -136,6 +138,7 @@ class HeapProfilerController {
                    base::TimeTicks profiler_creation_time,
                    uint32_t process_probability_pct,
                    size_t process_index,
+                   base::ByteSize expected_sampling_interval,
                    base::OnceClosure on_first_snapshot_callback);
 
     ~SnapshotParams();
@@ -168,6 +171,9 @@ class HeapProfilerController {
     uint32_t process_probability_pct = 100;
     size_t process_index = 0;
 
+    // The expected sampling interval in bytes.
+    base::ByteSize expected_sampling_interval;
+
     // A callback to invoke for the first snapshot. Will be null for the
     // following snapshots. For testing.
     base::OnceClosure on_first_snapshot_callback;
@@ -198,10 +204,12 @@ class HeapProfilerController {
       ProcessType process_type,
       base::TimeDelta time_since_profiler_creation,
       uint32_t process_probability_pct,
-      size_t process_index);
+      size_t process_index,
+      base::ByteSize expected_sampling_interval);
 
   const ProcessType process_type_;
   bool profiling_enabled_;
+  base::ByteSize expected_sampling_rate_ GUARDED_BY_CONTEXT(sequence_checker_);
 
   // Group name for the synthetic field trial, or nullopt for none.
   std::optional<std::string> synthetic_field_trial_group_;
