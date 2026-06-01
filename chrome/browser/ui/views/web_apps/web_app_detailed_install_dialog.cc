c65b7bb46464abc73c0a8819db80a1cc5ff0e736
Bryan Oltman <bryanoltman@google.com>
2026-06-01 14:24:27 -0700

    Revert "[webapps] Enforce min height for install & launch"
    This reverts commit 8b67bb7aa19c56d073fb147cf339f9472acb49b4.
    
    Reason for revert: Causing PWA install dialog to immediately close (https://g-issues.chromium.org/issues/518823720)
    
    Original change's description:
    > [webapps] Enforce min height for install & launch
    >
    > To prevent spoofing where app name/origin are hidden in small windows,
    > enforce minimum heights for install and launch dialogs: 320px for new
    > flow simple/diy install dialogs (with banner), 220px for old simple/diy
    > and detailed install dialogs (no banner), and 220px for launch dialogs.
    >
    > If the window is too small, the dialog is immediately ignored/closed. If
    > preferred height is less than minimum, we allow it but block if shrunk
    > at all.
    >
    > TAG=agy
    > CONV=3af44289-688b-4163-9861-9ae75b8b56bb
    >
    > Bug: 407283320
    > Test: Manual verification with local PWA test site.
    > Change-Id: I72f4053cc05e62af3a71f4ff06a987eea1f98b2b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7886718
    > Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    > Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1638854}
    
    Bug: 407283320
    Change-Id: I6e93b69b6b6b7714aca18f92f4e3c753e5de8cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891005
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639674}

--- chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.cc
+++ chrome/browser/ui/views/web_apps/web_app_detailed_install_dialog.cc
@@ -496,8 +496,7 @@ void ShowWebAppDetailedInstallDialog(
   views::Widget* detailed_dialog_widget =
       constrained_window::ShowWebModalDialogViews(dialog.release(),
                                                   web_contents);
-  if (IsWidgetCurrentSizeSmallerThanPreferredSize(
-          detailed_dialog_widget, kMinDetailedInstallDialogHeight)) {
+  if (IsWidgetCurrentSizeSmallerThanPreferredSize(detailed_dialog_widget)) {
     delegate_weak_ptr->CloseDialogAsIgnored();
     return;
   }
