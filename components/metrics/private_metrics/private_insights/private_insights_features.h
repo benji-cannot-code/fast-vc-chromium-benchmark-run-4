2143a65fba120576c30a765f38322cd827f2b226
Kamil Jarosz <kjarosz@google.com>
2026-06-01 05:16:32 -0700

    private_insights: Add feature flags
    Add feature flags to the Private Insights service.
    
    - kPrivateInsightsFeature which enables the service itself.
    
    - kPrivateInsightsPaicErrorReporting which enables error reporting for
      PAIC.
    
    Bug: b:518646894
    Change-Id: I4af5f834f63f04198dfb473c4d68b6a9d81023d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890275
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639306}

---
+++ components/metrics/private_metrics/private_insights/private_insights_features.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_PRIVATE_INSIGHTS_FEATURES_H_
+#define COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_PRIVATE_INSIGHTS_FEATURES_H_
+
+#include "base/component_export.h"
+#include "base/feature_list.h"
+
+namespace private_insights {
+
+// Enables Private Insights.
+COMPONENT_EXPORT(PRIVATE_INSIGHTS)
+BASE_DECLARE_FEATURE(kPrivateInsightsFeature);
+
+// Enables Private AI Compute error reporting over Private Insights.
+COMPONENT_EXPORT(PRIVATE_INSIGHTS)
+BASE_DECLARE_FEATURE(kPrivateInsightsPaicErrorReporting);
+
+}  // namespace private_insights
+
+#endif  // COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_PRIVATE_INSIGHTS_FEATURES_H_
