31cda258e31aa484d17405edbdd515e2b297a1d4
Anuj Kumar <anujbh@google.com>
2026-06-01 16:14:26 -0700

    [ios/web_view] Add IOS_WEB_VIEW_SUPPORTS_MAIN_THREAD_INITIAL_NESTING_LEVEL macro
    This CL adds a new feature flag macro IOS_WEB_VIEW_SUPPORTS_MAIN_THREAD_INITIAL_NESTING_LEVEL
    to ios/web_view/public/cwv_defines.h to indicate support for the
    mainThreadInitialNestingLevel property in CWVEarlyInitFlags.
    
    Bug: None
    Change-Id: I788bad30a05b35bc39e1b4a26b592778c508ebae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891406
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Carlos Acosta <cyacosta@google.com>
    Commit-Queue: Anuj Kumar <anujbh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639759}

--- ios/web_view/public/cwv_defines.h
+++ ios/web_view/public/cwv_defines.h
@@ -145,6 +145,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Supports fetching virtual cards
 #define IOS_WEBVIEW_SUPPORTS_FETCHING_VIRTUAL_CARDS 1
 
+// Supports -[CWVEarlyInitFlags mainThreadInitialNestingLevel].
+#define IOS_WEB_VIEW_SUPPORTS_MAIN_THREAD_INITIAL_NESTING_LEVEL 1
+
 // When enabled, -[CWVGlobalState setDumpWithoutCrashingHandler:] is made available.
 #define CWV_DUMP_WITHOUT_CRASHING_HANDLER_AVAILABLE 1
 
