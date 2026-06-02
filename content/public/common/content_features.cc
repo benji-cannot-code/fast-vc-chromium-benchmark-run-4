291b329e4db48c61e6def692494ef3b580e0eea0
Eriko Kurimoto <elkurin@chromium.org>
2026-06-01 23:51:12 -0700

    WaaP: Enable kInitialWebUIWithoutTranslate by default
    This CL enables kInitialWebUIWithoutTranslate feature flag by default
    when InitialWebUI feature is enabled.
    
    Bug: 512684971
    Change-Id: I0f786d1635d218c24621ed0c77cd1cfe1ed492fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882459
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639963}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -574,7 +574,7 @@ BASE_FEATURE_PARAM(bool,
                    kInitialWebUIWithoutTranslate,
                    &features::kInitialWebUI,
                    "without_translate",
-                   false);
+                   true);
 
 // Whether initial WebUI navigations should synchronously go from navigation
 // start to commit, by doing e.g. in-renderer body loading.
