7e8f1d4856ab5d0d2e1fefaa4e3c626b14a2b0ef
Kamil Jarosz <kjarosz@google.com>
2026-06-02 07:30:24 -0700

    private_insights: Measure background upload time
    Adds a new UMA histogram `PrivateMetrics.PrivateInsights.Upload.Time` to
    measure the duration of `PrivateInsightsService::UploadBlocking()`,
    which runs on a background task runner.
    
    This allows monitoring the performance of the blocking upload step.
    
    Bug: b:518719944
    Change-Id: I9f5a0940ba62e2996e89380ebf0e2ae3b3935e63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895456
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640150}

--- components/metrics/private_metrics/private_insights/private_insights_service.h
+++ components/metrics/private_metrics/private_insights/private_insights_service.h
@@ -22,6 +22,8 @@ inline constexpr char kTriggerUploadOutcomeHistogram[] =
     "PrivateMetrics.PrivateInsights.TriggerUploadOutcome";
 inline constexpr char kUploadPendingTimeHistogram[] =
     "PrivateMetrics.PrivateInsights.Upload.PendingTime";
+inline constexpr char kUploadTimeHistogram[] =
+    "PrivateMetrics.PrivateInsights.Upload.Time";
 
 class COMPONENT_EXPORT(PRIVATE_INSIGHTS) PrivateInsightsService
     : public KeyedService {
