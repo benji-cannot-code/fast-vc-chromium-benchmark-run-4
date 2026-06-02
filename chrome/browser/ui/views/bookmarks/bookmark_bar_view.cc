2be8ed6e7269788f81f71983f03ceab741c061df
Emily Shack <emshack@chromium.org>
2026-06-02 13:08:28 -0700

    [GlowUp] Fix bookmark bar overflow icon size
    Bug: 519190021
    Change-Id: I1f0adb0227e4022b92865e8c5ba51325f0d09e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896856
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640413}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -2220,10 +2220,11 @@ void BookmarkBarView::UpdateAppearanceForTheme() {
 
   const SkColor overflow_color =
       color_provider->GetColor(kColorBookmarkButtonIcon);
+  const int overflow_size = 16;
   ui::ImageModel overflow_button_icon = ui::ImageModel::FromVectorIcon(
       features::IsRoundedIconsEnabled() ? kKeyboardDoubleArrowRightIcon
                                         : kBookmarkbarOverflowRefreshOldIcon,
-      overflow_color);
+      overflow_color, overflow_size);
 
   overflow_button_->SetImageModel(views::Button::STATE_NORMAL,
                                   overflow_button_icon);
