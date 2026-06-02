8bda43680c9b06ea54ae38095aff95e42a598e76
Aisulu R <aisulu@google.com>
2026-06-02 15:42:41 -0700

    [a11y] Rename string IDs {accessibility annotator -> personal context}
    This CL purely renames string IDs. While at it, updates the file name.
    
    For easier review of grdp file (only):
    - https://crrev.com/c/7880734/1: rename .grdp contents
    - https://crrev.com/c/7880734/1..9: rename .grdp file
    
    To confirm this did not break anything, see screenshots.
    
    Screenshot: http://screen/A8nHdhEPKDeLzoo (Desktop - Notice)
    Screenshot: http://screen/4jHQMFPmfYfSsCM (Desktop - Settings)
    Fixed: b:511147704
    Change-Id: Ia6a870e69fef03b965ca3a2ed6975f8afa74388d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880734
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Oleksii Kalinin <kalinino@google.com>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640521}

--- chrome/android/javatests/src/org/chromium/chrome/browser/personal_context/first_run/PersonalContextFirstRunBottomSheetTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/personal_context/first_run/PersonalContextFirstRunBottomSheetTest.java
@@ -114,7 +114,7 @@ public class PersonalContextFirstRunBottomSheetTest {
 
         BottomSheetTestSupport.waitForOpen(mBottomSheetController);
 
-        onView(withText(R.string.accessibility_annotator_info_primary_button)).perform(click());
+        onView(withText(R.string.personal_context_notice_primary_button)).perform(click());
 
         verify(mDelegate).onInfoAcknowledged();
     }
@@ -168,7 +168,7 @@ public class PersonalContextFirstRunBottomSheetTest {
 
         BottomSheetTestSupport.waitForOpen(mBottomSheetController);
 
-        onView(withText(R.string.accessibility_annotator_info_secondary_button)).perform(click());
+        onView(withText(R.string.personal_context_notice_secondary_button)).perform(click());
 
         verify(mDelegate).onManageSettingsClicked();
     }
