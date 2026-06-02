a0a6ac3d24163341051af1fa08c9c6a099d2593a
Sophie Chang <sophiechang@chromium.org>
2026-06-02 13:50:15 -0700

    Reland "Disable click on page action icon if anchored message is visible"
    This reverts commit e9fad93adbda2f68f78ae8093103303c1e70ee2d.
    
    Reason for revert: reland with test fix
    
    Original change's description:
    > Revert "Disable click on page action icon if anchored message is visible"
    >
    > This reverts commit 97b13337daefd2f48d95d0a96c56c117a7270b9c.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5733210945224704
    >
    > Sample build with failed test: https://ci.chromium.org/b/8680087147680116689
    > Affected test(s):
    > [://chrome/test\:browser_tests!gtest::IndigoBrowserTest#ToolbarPositioningScroll](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::IndigoBrowserTest%23ToolbarPositioningScroll?q=VHash%3A9615faa4d856567b)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5733210945224704&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7887085&type=BUG
    >
    > Original change's description:
    > > Disable click on page action icon if anchored message is visible
    > >
    > > Bug: 515594709
    > > Change-Id: I42b7421c76631e5fe265eeda742f74610a1d6ff4
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7887085
    > > Reviewed-by: Dmytro Yeroshkin <yeroshkin@chromium.org>
    > > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1640261}
    > >
    >
    > Bug: 515594709
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ic82b1599ec67850d8796b40eab30d83aa6a374a2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896190
    > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1640360}
    
    Bug: 515594709
    Change-Id: Ie3498818754d52edd05082cbbcba47e1402c3a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897320
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Dmytro Yeroshkin <yeroshkin@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Owners-Override: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640439}

--- chrome/browser/indigo/indigo_browsertest.cc
+++ chrome/browser/indigo/indigo_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/views/indigo/indigo_toolbar.h"
+#include "chrome/browser/ui/views/page_action/anchored_message_view.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "chrome/test/interaction/tracked_element_webcontents.h"
@@ -271,7 +272,10 @@ IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, ToolbarPositioning) {
   RunTestSequence(
       InstrumentTab(kWebContentsId), NavigateWebContents(kWebContentsId, url),
       WaitForShow(kIndigoPageActionIconElementId),
-      PressButton(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
 
       AfterShow(IndigoToolbar::kToolbarElementId,
                 base::BindLambdaForTesting([&](ui::TrackedElement* el) {
@@ -309,7 +313,10 @@ IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, CloseResetsReplacements) {
   RunTestSequence(
       InstrumentTab(kWebContentsId), NavigateWebContents(kWebContentsId, url),
       WaitForShow(kIndigoPageActionIconElementId),
-      PressButton(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
       WaitForShow(IndigoToolbar::kToolbarElementId),
 
       WithElement(
@@ -347,7 +354,10 @@ IN_PROC_BROWSER_TEST_F(IndigoHighDsfBrowserTest, ToolbarPositioning) {
   RunTestSequence(
       InstrumentTab(kWebContentsId), NavigateWebContents(kWebContentsId, url),
       WaitForShow(kIndigoPageActionIconElementId),
-      PressButton(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
 
       AfterShow(IndigoToolbar::kToolbarElementId,
                 base::BindLambdaForTesting([&](ui::TrackedElement* el) {
@@ -385,27 +395,31 @@ IN_PROC_BROWSER_TEST_F(IndigoOnboardingBrowserTest, OnboardingFlow) {
   const GURL main_tab_url = embedded_test_server()->GetURL("/image.html");
   const GURL popup_url = embedded_test_server()->GetURL("/empty.html");
 
-  RunTestSequence(InstrumentTab(kWebContentsId),
-                  NavigateWebContents(kWebContentsId, main_tab_url),
-                  WaitForWebContentsReady(kWebContentsId, main_tab_url),
-                  WaitForShow(kIndigoPageActionIconElementId),
-                  PressButton(kIndigoPageActionIconElementId),
-                  WaitForShow(IndigoOnboardingDialog::kWebViewId),
-                  InstrumentNonTabWebView(kDialogWebContentsId,
-                                          IndigoOnboardingDialog::kWebViewId),
-                  WaitForWebContentsReady(kDialogWebContentsId, popup_url),
-                  ExecuteJs(kDialogWebContentsId,
-                            R"js(
+  RunTestSequence(
+      InstrumentTab(kWebContentsId),
+      NavigateWebContents(kWebContentsId, main_tab_url),
+      WaitForWebContentsReady(kWebContentsId, main_tab_url),
+      WaitForShow(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      WaitForShow(IndigoOnboardingDialog::kWebViewId),
+      InstrumentNonTabWebView(kDialogWebContentsId,
+                              IndigoOnboardingDialog::kWebViewId),
+      WaitForWebContentsReady(kDialogWebContentsId, popup_url),
+      ExecuteJs(kDialogWebContentsId,
+                R"js(
                     () => {
                       window.chromeOnboarding.acknowledgeChromeDisclaimer();
                       window.close();
                     }
                   )js",
-                            ExecuteJsMode::kFireAndForget),
-                  WaitForHide(IndigoOnboardingDialog::kWebViewId), Check([&]() {
-                    return browser()->profile()->GetPrefs()->GetBoolean(
-                        prefs::kIndigoHasOnboarded);
-                  }));
+                ExecuteJsMode::kFireAndForget),
+      WaitForHide(IndigoOnboardingDialog::kWebViewId), Check([&]() {
+        return browser()->profile()->GetPrefs()->GetBoolean(
+            prefs::kIndigoHasOnboarded);
+      }));
 }
 
 IN_PROC_BROWSER_TEST_F(IndigoOnboardingBrowserTest, ClosedOnNavigation) {
@@ -418,7 +432,10 @@ IN_PROC_BROWSER_TEST_F(IndigoOnboardingBrowserTest, ClosedOnNavigation) {
       NavigateWebContents(kWebContentsId, main_tab_url),
       WaitForWebContentsReady(kWebContentsId, main_tab_url),
       WaitForShow(kIndigoPageActionIconElementId),
-      PressButton(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
       WaitForShow(IndigoOnboardingDialog::kWebViewId),
       InstrumentNonTabWebView(kDialogWebContentsId,
                               IndigoOnboardingDialog::kWebViewId),
@@ -438,26 +455,29 @@ IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, TabSwitchPreservesToolbarState) {
   const GURL url = embedded_test_server()->GetURL("/image.html");
   const GURL url2 = embedded_test_server()->GetURL("/empty.html");
 
-  RunTestSequence(InstrumentTab(kWebContentsId),
-                  NavigateWebContents(kWebContentsId, url),
-                  WaitForShow(kIndigoPageActionIconElementId),
-                  PressButton(kIndigoPageActionIconElementId),
-                  WaitForShow(IndigoToolbar::kToolbarElementId),
+  RunTestSequence(
+      InstrumentTab(kWebContentsId), NavigateWebContents(kWebContentsId, url),
+      WaitForShow(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      WaitForShow(IndigoToolbar::kToolbarElementId),
 
-                  // Expand the toolbar
-                  PressButton(IndigoToolbar::kExpandButtonElementId),
-                  WaitForShow(IndigoToolbar::kExpandedContainerElementId),
+      // Expand the toolbar
+      PressButton(IndigoToolbar::kExpandButtonElementId),
+      WaitForShow(IndigoToolbar::kExpandedContainerElementId),
 
-                  // Open a new tab and switch to it
-                  AddInstrumentedTab(kSecondTabId, url2),
-                  WaitForHide(IndigoToolbar::kToolbarElementId),
+      // Open a new tab and switch to it
+      AddInstrumentedTab(kSecondTabId, url2),
+      WaitForHide(IndigoToolbar::kToolbarElementId),
 
-                  // Switch back to the first tab
-                  SelectTab(kTabStripElementId, 0),
-                  WaitForShow(IndigoToolbar::kToolbarElementId),
+      // Switch back to the first tab
+      SelectTab(kTabStripElementId, 0),
+      WaitForShow(IndigoToolbar::kToolbarElementId),
 
-                  // Verify it is still expanded
-                  WaitForShow(IndigoToolbar::kExpandedContainerElementId));
+      // Verify it is still expanded
+      WaitForShow(IndigoToolbar::kExpandedContainerElementId));
 }
 
 IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, ShowToolbarWhileInactiveDeferred) {
@@ -500,7 +520,10 @@ IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, ToolbarPositioningScroll) {
   RunTestSequence(
       InstrumentTab(kWebContentsId), NavigateWebContents(kWebContentsId, url),
       WaitForShow(kIndigoPageActionIconElementId),
-      PressButton(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
 
       AfterShow(IndigoToolbar::kToolbarElementId,
                 base::BindLambdaForTesting([&](ui::TrackedElement* el) {
@@ -551,7 +574,10 @@ IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, ToolbarPositioningTransform) {
   RunTestSequence(
       InstrumentTab(kWebContentsId), NavigateWebContents(kWebContentsId, url),
       WaitForShow(kIndigoPageActionIconElementId),
-      PressButton(kIndigoPageActionIconElementId),
+      WaitForShow(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
+      PressButton(
+          page_actions::AnchoredMessageBubbleView::kAnchoredMessageChipId),
 
       AfterShow(IndigoToolbar::kToolbarElementId,
                 base::BindLambdaForTesting([&](ui::TrackedElement* el) {
--- chrome/browser/ui/views/page_action/page_action_view.cc
+++ chrome/browser/ui/views/page_action/page_action_view.cc
@@ -255,6 +255,10 @@ bool PageActionView::ShouldUpdateInkDropOnClickCanceled() const {
 }
 
 void PageActionView::NotifyClick(const ui::Event& event) {
+  if (IsAnchoredMessageVisible()) {
+    return;
+  }
+
   PageActionTrigger trigger_source;
   if (event.IsMouseEvent()) {
     trigger_source = PageActionTrigger::kMouse;
@@ -371,6 +375,10 @@ bool PageActionView::IsBubbleShowing() const {
 }
 
 bool PageActionView::IsTriggerableEvent(const ui::Event& event) {
+  if (IsAnchoredMessageVisible()) {
+    return false;
+  }
+
   // Returns whether the bubble should be shown given the event. Only trigger an
   // action when action UI isn't already showing (managed at the
   // IconLabelBubbleView level), and if mouse input, when event is a left button
