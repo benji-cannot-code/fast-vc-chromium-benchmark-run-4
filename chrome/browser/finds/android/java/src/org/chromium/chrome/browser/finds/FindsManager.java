8ba684092c6be596b8ea400b333df1fd0161fdf5
Brandon Wylie <wylieb@google.com>
2026-06-02 16:51:28 -0700

    [finds] Streamline chrome flags setup
    Bug: 519257530
    Change-Id: Ibe2a9cf365570de84355de18c563cb45996ec8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896922
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Evan Luo <evanluo@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640560}

--- chrome/browser/finds/android/java/src/org/chromium/chrome/browser/finds/FindsManager.java
+++ chrome/browser/finds/android/java/src/org/chromium/chrome/browser/finds/FindsManager.java
@@ -11,6 +11,7 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.components.user_prefs.UserPrefs;
 
 /**
  * Manages the Finds flow on startup. Listens to {@link FindsService} and shows the UI when criteria
@@ -46,7 +47,10 @@ public class FindsManager implements FindsService.Observer {
         mFindsService.maybeRescheduleNotifications();
 
         if (FindsFeatures.sAlwaysShowOptInPromo.getValue()) {
-            onOptInCriteriaFulfilled();
+            if (!UserPrefs.get(mProfile)
+                    .getBoolean(FindsUtils.FINDS_OPT_IN_PROMO_USER_INTERACTED)) {
+                onOptInCriteriaFulfilled();
+            }
         }
     }
 
