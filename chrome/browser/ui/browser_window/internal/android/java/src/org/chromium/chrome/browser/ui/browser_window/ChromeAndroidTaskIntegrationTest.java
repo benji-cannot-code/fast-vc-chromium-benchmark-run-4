a6a4366490b71dcb5b975c03fa943c259a03f225
Linyu He <linyuh@google.com>
2026-06-02 12:51:21 -0700

    ChromeAndroidTask: Disable individual failed tests on DESKTOP_FREEFORM
    This CL disables individual failed tests on DESKTOP_FREEFORM instead of
    disabling the entire ChromeAndroidTaskIntegrationTest class.
    
    This will quickly enable most tests on DESKTOP_FREEFORM and allow more
    time to fix the failed tests.
    
    Include-Ci-Only-Tests: true
    Bug: 511289000
    Change-Id: Icfd9bdd24b8cbce209d4af56a7f6be6a5230df20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891566
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Auto-Submit: Linyu He <linyuh@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640399}

--- chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskIntegrationTest.java
+++ chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskIntegrationTest.java
@@ -83,7 +83,6 @@ import java.util.concurrent.atomic.AtomicReference;
                 "Tests will be flaky if batched as they create/close windows and change window"
                         + " states in quick succession")
 @NullMarked
-@DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289000
 public class ChromeAndroidTaskIntegrationTest {
 
     @Rule
@@ -381,6 +380,7 @@ public class ChromeAndroidTaskIntegrationTest {
     @MediumTest
     @MinAndroidSdkLevel(Build.VERSION_CODES.R)
     @SuppressLint("NewApi" /* @MinAndroidSdkLevel already specifies the required SDK */)
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289000
     public void getBoundsInDp_returnsCorrectBounds() {
         // Arrange
         mFreshCtaTransitTestRule.startOnBlankPage();
@@ -626,6 +626,7 @@ public class ChromeAndroidTaskIntegrationTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289000
     public void isMaximized_trueByDefault() {
         // Arrange
         mFreshCtaTransitTestRule.startOnBlankPage();
@@ -1012,6 +1013,7 @@ public class ChromeAndroidTaskIntegrationTest {
     @Test
     @MediumTest
     @MinAndroidSdkLevel(VERSION_CODES.R)
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289000
     public void createBrowserWindowSync_createsMaximizedChromeTabbedActivity() {
         // Arrange.
         mFreshCtaTransitTestRule.startOnBlankPage();
@@ -1082,6 +1084,7 @@ public class ChromeAndroidTaskIntegrationTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.DESKTOP_FREEFORM /* test needs freeform windows */)
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511289000
     public void createPendingTask_requestShowInactive_dispatchesShowInactive() {
         // Arrange.
         mFreshCtaTransitTestRule.startOnBlankPage();
