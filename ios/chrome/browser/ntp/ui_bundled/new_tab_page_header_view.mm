27cbffcbbab7e3ae892692a38a32fc80cc45e104
Eric Ekey <ericekey@google.com>
2026-06-02 08:02:20 -0700

    [iOS][AppBar] Fix Fakebox Rotation Bug
    This change fixes a bug where the fakebox resizing due to scrolling on
    the NTP was not reset after rotating to landscape orientation.
    
    Fixed: 518765661
    Change-Id: I461aedfb65a1eed4573b402bab36a580f270fe1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7889856
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640172}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -350,7 +350,7 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
       [weakSelf updateUIOnTraitChange:previousCollection];
     };
     [self registerForTraitChanges:@[
-      UITraitHorizontalSizeClass.class,
+      UITraitHorizontalSizeClass.class, UITraitVerticalSizeClass.class,
       UITraitPreferredContentSizeCategory.class, UITraitUserInterfaceStyle.class
     ]
                       withHandler:handler];
@@ -1537,12 +1537,11 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
                       withNewBadge:_useNewBadgeForCustomizationMenu];
 }
 
-// Creates the Tools menu and adds it to the header view (iPhone only)
+// Creates the Tools menu and adds it to the header view.
 - (void)addToolsMenuIfNeeded {
   CHECK(IsChromeNextIaEnabled());
-
-  // If the App Bar is not available (iPad), the Tools menu should not be added
-  // to the header view.
+  // If the Tools menu button is always visible in the toolbar (iPad), the Tools
+  // menu should not be added to the header view.
   if (CanShowTabStrip(self)) {
     return;
   }
@@ -1935,15 +1934,26 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
 
 // Updates facets of the UI to reflect the change in the collection of UITraits.
 - (void)updateUIOnTraitChange:(UITraitCollection*)previousTraitCollection {
-  if (previousTraitCollection.horizontalSizeClass !=
-          self.traitCollection.horizontalSizeClass ||
+  BOOL horizontalSizeClassChanged =
+      previousTraitCollection.horizontalSizeClass !=
+      self.traitCollection.horizontalSizeClass;
+  BOOL preferredContentSizeCategoryChanged =
       previousTraitCollection.preferredContentSizeCategory !=
-          self.traitCollection.preferredContentSizeCategory) {
+      self.traitCollection.preferredContentSizeCategory;
+  if (horizontalSizeClassChanged || preferredContentSizeCategoryChanged) {
     [self updateFakeboxDisplay];
   }
 
-  if (previousTraitCollection.preferredContentSizeCategory !=
-      self.traitCollection.preferredContentSizeCategory) {
+  if (IsChromeNextIaEnabled()) {
+    if (horizontalSizeClassChanged ||
+        previousTraitCollection.verticalSizeClass !=
+            self.traitCollection.verticalSizeClass) {
+      [self resetSplitToolbarResizing];
+      [self addToolsMenuIfNeeded];
+    }
+  }
+
+  if (preferredContentSizeCategoryChanged) {
     [self updateHintLabelFonts];
   }
 
@@ -1959,11 +1969,6 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
           AccountParticleDiscBadgeBackgroundColor(
               self.traitCollection.userInterfaceStyle);
     }
-  }
-
-  if (IsChromeNextIaEnabled()) {
-    [self resetSplitToolbarResizing];
-    [self addToolsMenuIfNeeded];
   }
 }
 
