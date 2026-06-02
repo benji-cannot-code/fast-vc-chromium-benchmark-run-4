c43f2380333de4d26468f7e7cc28d448f1621cd0
Masa Fujita <massan@google.com>
2026-06-02 12:13:37 -0700

    Reland "nested app menu: Add recent tabs to the app menu"
    This is a reland due to a failing test. We should be calling `updateRecentlyClosedEntries` just in time, instead of every time in `getTabDelegateFactory()`.
    
    Original change's description:
    > Revert "nested app menu: Add recent tabs to the app menu"
    >
    > This reverts commit ddc25bd5133d0fba7e6376eece745f1864cb0920.
    >
    > Reason for revert: culprit for failure:
    > https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Fandroid%5C%3Achrome_public_test_apk!junit%3Aorg.chromium.chrome.browser.multiwindow%3AMultiInstanceManagerApi31Test%23decreaseInstanceLimit_ExcessActive_ExcessTasksFinished?q=VHash%3Af4fa7c1938ac97a5
    >
    > Original change's description:
    > > nested app menu: Add recent tabs to the app menu
    > >
    > > This CL adds recent tabs under the "History" parent item in the app
    > > menu. We make use of the bundle to store the session ID, and use it to
    > > retrieve RecentlyClosedTab in ChromeTabbedActivity. Ideally we would
    > > store the RecentlyClosedTab instance itself, but bundles can only handle
    > > serializable types.
    > >
    > > We will add window / bulk closure restorations in the later CLs.
    > >
    > > This change is guarded behind the flag #submenus-in-app-menu.
    > >
    > > Demo: http://screenshot/Aa3FUqMwQC4okAL
    > >
    > > Bug: 509065810
    > > Change-Id: I12316a95b28cb610f86f8f4c631ece4a735001da
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7884303
    > > Reviewed-by: Jenna Himawan <jhimawan@google.com>
    > > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > > Commit-Queue: Masa Fujita <massan@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1639971}
    >
    > Bug: 509065810
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ib901af994b45fd2e036acc9186787fc45ba5a04b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893569
    > Commit-Queue: James Lee <ljjlee@google.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: James Lee <ljjlee@google.com>
    > Reviewed-by: James Lee <ljjlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1640062}
    
    Bug: 509065810
    Change-Id: I79eaec9282e3d6c09393c92f93746021bd6b9b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894000
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640367}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuHandlerImpl.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuHandlerImpl.java
@@ -615,6 +615,10 @@ class AppMenuHandlerImpl
                 new LayoutViewBuilder<>(standardItemResId),
                 AppMenuItemViewBinder::bindStandardItem);
         adapter.registerType(
+                AppMenuItemType.RECENT_ENTRY,
+                new LayoutViewBuilder<>(standardItemResId),
+                AppMenuItemViewBinder::bindStandardItem);
+        adapter.registerType(
                 AppMenuItemType.EMPTY,
                 new LayoutViewBuilder<>(R.layout.menu_item_empty),
                 AppMenuItemViewBinder::bindStandardItem);
