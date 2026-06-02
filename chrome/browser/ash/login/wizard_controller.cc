08d1b414581ad1c8a336673e46d863c7c9bb65a5
Fahad Mansoor <fahadmansoor@google.com>
2026-06-02 04:41:16 -0700

    LPP: Handle session expiry when finalizing auth
    If a user context has expired show the osauth error screen.
    
    Bug: 508223406
    Change-Id: I6f33eab7ad9fedb0a5fd9c4f290c91846f2118dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885341
    Auto-Submit: Fahad Mansoor <fahadmansoor@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640072}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_deref.h"
 #include "base/check_is_test.h"
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/json/json_string_value_serializer.h"
@@ -2914,6 +2915,15 @@ void WizardController::ObtainContextAndFinalizeAuth() {
 
 void WizardController::FinalizeAuthWithContext(
     std::unique_ptr<UserContext> context) {
+  if (!context) {
+    // Session has expired.
+    LOG(ERROR) << "Session expired before login could proceed.";
+    // Also dump to see in what scenarios would this happen.
+    base::debug::DumpWithoutCrashing(FROM_HERE);
+    session_manager::SessionManager::Get()->RequestSignOut();
+    return;
+  }
+
   auto mount_state = context->GetMountState();
   if (!mount_state.has_value()) {
     // In certain edge cases, such as a Reauthentication that transitions into
