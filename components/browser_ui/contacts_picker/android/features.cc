6de28ef0fed5e77d1508b7d89eda8ae0907365d5
Matt Reynolds <mattreynolds@google.com>
2026-06-02 16:30:00 -0700

    contacts: Default-enable system picker integration on Android
    Integrate with the system contacts picker when supported by the
    operating system.
    
    launch/4453380
    
    Bug: 509653815
    Change-Id: I197d2212c3eac60e6428a4350abbbdfcd1d14ed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897741
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640548}

--- components/browser_ui/contacts_picker/android/features.cc
+++ components/browser_ui/contacts_picker/android/features.cc
@@ -11,6 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browser_ui {
 
 BASE_FEATURE(kContactsPickerSelectAll, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kAndroidSystemContactsPicker, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAndroidSystemContactsPicker, base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace browser_ui
