f00445aa616403b9b0eba815816bfc76496fe823
Steven Luong <stluong@chromium.org>
2026-06-01 18:57:53 -0700

    [GlowUp] Remove icon from bookmark all tabs and name window menu items
    On Mac, the bookmarks all tabs and name window menu items weren't
    supposed to have an icon on the system context menu and was added by
    accident. This CL removes the icon from those menu items on Mac only.
    
    Fixed: 517881937
    Change-Id: I4c1d7a4860c82123a2f3c599126d57511593ec31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891732
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639863}

--- chrome/browser/ui/views/frame/system_menu_model_builder.cc
+++ chrome/browser/ui/views/frame/system_menu_model_builder.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+#if !BUILDFLAG(IS_MAC)
 void AddItemWithIconMaybe(ui::SimpleMenuModel* model,
                           int command_id,
                           int string_id,
@@ -70,6 +71,7 @@ void AddItemWithIconMaybe(ui::SimpleMenuModel* model,
     model->AddItemWithStringId(command_id, string_id);
   }
 }
+#endif  // !BUILDFLAG(IS_MAC)
 
 }  // namespace
 
@@ -147,10 +149,15 @@ void SystemMenuModelBuilder::BuildSystemMenuForBrowserWindow(
                                IDS_GROUP_UNGROUPED_TABS);
   }
 
+#if BUILDFLAG(IS_MAC)
+  model->AddItemWithStringId(IDC_BOOKMARK_ALL_TABS, IDS_BOOKMARK_ALL_TABS);
+  model->AddItemWithStringId(IDC_NAME_WINDOW, IDS_NAME_WINDOW);
+#else
   AddItemWithIconMaybe(model, IDC_BOOKMARK_ALL_TABS, IDS_BOOKMARK_ALL_TABS,
                        kBookmarkAllTabsChromeRefreshOldIcon);
   AddItemWithIconMaybe(model, IDC_NAME_WINDOW, IDS_NAME_WINDOW,
                        kNameWindowOldIcon);
+#endif  // BUILDFLAG(IS_MAC)
 
   if (base::FeatureList::IsEnabled(tabs::kHorizontalTabStripComboButton)) {
     model->AddSeparator(ui::NORMAL_SEPARATOR);
