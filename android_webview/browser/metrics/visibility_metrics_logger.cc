b42d5d95a8e3097b8703eac62a8a386e38f7a14d
Etienne Pierre-doray <etiennep@chromium.org>
2026-06-02 11:18:49 -0700

    [tracing] Avoid perfetto track aliasing in android_webview/browser/metrics
    perfetto::Track::FromPointer is problematic because pointer reuse can
    cause track aliasing in perfetto UI, leading to confusing traces (this
    was true of legacy TRACE_EVENT_ASYNC as well).
    
    To avoid these, we have the following alternatives:
    1- Using GetNextGlobalTraceId() instead of a pointer id to guarantee
       uniqueness. The track will get its name from the first event emitted.
    2- Using NamedTrack to scope the pointer to a specific class
      (an aliased track would always correctly have the same name). This
      is more appropriate when there are several events emitted to the same
      track without a clear name. This has the benefit of giving the track
      a consistent, explicit name.
    3- Replace custom track with "sync" instant events with flows.
      This reduces visual clutter and retain information about where
      events originated from.
    
    This CL applies a migration to of one #1 or #2.
    
    Questions for the reviewer:
    - Design Fit: Is the choice of a dedicated async track (#1/#2)
      appropriate, or would is be suitable to emit "sync" events with
      flows (#3). As a rule of thumb, async tracks are appropriate to
      display a clear async operation (that could be recorded as UMA),
      or high level / user visible state.
    - Naming: (when applicable) Is the track name I chose appropriate
    
    This CL was uploaded by git cl split.
    
    Bug: 432427382
    Change-Id: I73894849f229c75caa97e4349ab93d3c06c26b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871141
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640324}

--- android_webview/browser/metrics/visibility_metrics_logger.cc
+++ android_webview/browser/metrics/visibility_metrics_logger.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/typed_macros.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/url_constants.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 using content::BrowserThread;
 
@@ -257,6 +258,7 @@ bool VisibilityMetricsLogger::VisibilityInfo::IsVisible() const {
 void VisibilityMetricsLogger::ProcessClientUpdate(Client* client,
                                                   const VisibilityInfo& info,
                                                   ClientAction action) {
+  auto track = perfetto::NamedTrack::FromPointer("WebViewVisibility", client);
   VisibilityInfo curr_info = client_visibility_[client];
   bool was_visible = curr_info.IsVisible();
   bool is_visible = info.IsVisible();
@@ -273,28 +275,24 @@ void VisibilityMetricsLogger::ProcessClientUpdate(Client* client,
     // TODO(b/280334022): set the track name explicitly after the Perfetto SDK
     // migration is finished (crbug/1006541).
     if (is_visible) {
-      TRACE_EVENT_BEGIN("android_webview.timeline", "WebViewVisible",
-                        perfetto::Track::FromPointer(client));
+      TRACE_EVENT_BEGIN("android_webview.timeline", "WebViewVisible", track);
     }
   }
 
   // If visibility changes or the client is removed, close the event
   // corresponding to the previous visibility state.
   if (action == ClientAction::kRemoved || was_visible != is_visible) {
-    TRACE_EVENT_END("android_webview.timeline",
-                    perfetto::Track::FromPointer(client));
+    TRACE_EVENT_END("android_webview.timeline", track);
   }
 
   if (!was_visible && is_visible) {
     if (action != ClientAction::kRemoved) {
-      TRACE_EVENT_BEGIN("android_webview.timeline", "WebViewVisible",
-                        perfetto::Track::FromPointer(client));
+      TRACE_EVENT_BEGIN("android_webview.timeline", "WebViewVisible", track);
     }
     ++all_clients_visible_count_;
   } else if (was_visible && !is_visible) {
     if (action != ClientAction::kRemoved) {
-      TRACE_EVENT_BEGIN("android_webview.timeline", "WebViewInvisible",
-                        perfetto::Track::FromPointer(client));
+      TRACE_EVENT_BEGIN("android_webview.timeline", "WebViewInvisible", track);
     }
     --all_clients_visible_count_;
   }
