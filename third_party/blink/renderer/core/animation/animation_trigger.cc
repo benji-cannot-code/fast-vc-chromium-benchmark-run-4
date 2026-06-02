57e99bd121369e0034a20c21146b6bbc811c9364
David Awogbemila <awogbemila@chromium.org>
2026-06-02 15:34:02 -0700

    [animation-trigger] Implement play-once behavior for impl triggers
    play-once[1] should resume an animation that had not finished.
    
    [1]
    https://drafts.csswg.org/animation-triggers-1/#valdef-animation-action-play-once
    
    Bug: 451238244, 390314945
    Change-Id: I68fde292055785b28267a700bf9c027588874321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880369
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: David A <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640513}

--- third_party/blink/renderer/core/animation/animation_trigger.cc
+++ third_party/blink/renderer/core/animation/animation_trigger.cc
@@ -77,9 +77,10 @@ void PerformPlayBackwards(Animation& animation,
 
 void PerformPlayOnce(Animation& animation,
                      V8AnimationPlayState::Enum play_state,
+                     std::optional<base::TimeDelta> event_time,
                      ExceptionState& exception_state) {
   if (play_state != V8AnimationPlayState::Enum::kFinished) {
-    animation.PlayInternal(Animation::AutoRewind::kEnabled, exception_state);
+    PerformPlay(animation, event_time, exception_state);
   }
 }
 
@@ -125,7 +126,7 @@ void AnimationTrigger::PerformBehavior(
       PerformPlayBackwards(animation, play_state, exception_state);
       break;
     case Behavior::kPlayOnce:
-      PerformPlayOnce(animation, play_state, exception_state);
+      PerformPlayOnce(animation, play_state, async_event_time, exception_state);
       break;
     case Behavior::kReset:
       PerformReset(animation, play_state, exception_state);
@@ -185,8 +186,8 @@ bool AnimationTrigger::CanCompositeBehavior(Behavior behavior) {
     case Behavior::kPause:
     case Behavior::kNone:
     case Behavior::kReplay:
-      return true;
     case Behavior::kPlayOnce:
+      return true;
     case Behavior::kPlayForwards:
     case Behavior::kPlayBackwards:
     case Behavior::kReset:
