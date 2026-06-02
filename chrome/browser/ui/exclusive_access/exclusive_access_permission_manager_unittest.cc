f770e1166cc2b5b156e4881a4fea5213e98e1417
Daniel Rubery <drubery@chromium.org>
2026-06-02 08:05:26 -0700

    [bedrock] Remove BrowserWithTestWindowTest from exclusive_access_permission_manager_unittest.cc
    We don't need a full BrowserWithTestWindowTest, just a WebContents.
    ChromeRenderViewHostTestHarness can provide that.
    
    Fixed: 518759198
    Change-Id: Id81bcf085ebf38eed62967b02a1b63cd82425cff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890831
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640174}

--- chrome/browser/ui/exclusive_access/exclusive_access_permission_manager_unittest.cc
+++ chrome/browser/ui/exclusive_access/exclusive_access_permission_manager_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/public/browser/permission_controller.h"
 #include "content/public/browser/permission_result.h"
 #include "content/public/browser/render_frame_host.h"
@@ -20,18 +20,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "third_party/blink/public/mojom/permissions/permission_status.mojom.h"
 
-class ExclusiveAccessPermissionManagerTest : public BrowserWithTestWindowTest {
+class ExclusiveAccessPermissionManagerTest
+    : public ChromeRenderViewHostTestHarness {
  public:
   ExclusiveAccessPermissionManagerTest()
-      : BrowserWithTestWindowTest(
-            base::test::SingleThreadTaskEnvironment::TimeSource::MOCK_TIME),
+      : ChromeRenderViewHostTestHarness(
+            base::test::TaskEnvironment::TimeSource::MOCK_TIME),
         manager_(nullptr) {}
   ~ExclusiveAccessPermissionManagerTest() override = default;
 
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
+    ChromeRenderViewHostTestHarness::SetUp();
     manager_.set_permission_controller_for_test(&permission_controller_);
-    AddTab(browser(), GURL("https://example.com"));
+    NavigateAndCommit(GURL("https://example.com"));
   }
 
  protected:
@@ -83,7 +84,7 @@ class ExclusiveAccessPermissionManagerTest : public BrowserWithTestWindowTest {
   }
 
   content::WebContents* web_contents() {
-    return browser()->tab_strip_model()->GetActiveWebContents();
+    return ChromeRenderViewHostTestHarness::web_contents();
   }
 
   content::MockPermissionController permission_controller_;
@@ -141,7 +142,7 @@ TEST_F(ExclusiveAccessPermissionManagerTest, HandleMultipleRequests) {
 TEST_F(ExclusiveAccessPermissionManagerTest,
        CloseTabBeforeRequestingPermission) {
   QueuePointerLockRequest();
-  browser()->tab_strip_model()->CloseAllTabs();
+  DeleteContents();
 
   EXPECT_CALL(permission_controller_, RequestPermissionsFromCurrentDocument)
       .Times(0);
