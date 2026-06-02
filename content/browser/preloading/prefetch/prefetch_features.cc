55eda6d5446b1d0b4bfdb6ad48c48432914ca878
kenoss <kenoss@chromium.org>
2026-06-01 19:32:17 -0700

    ParallelPrefetch: Enable by default on Desktop
    This CL enables parallel prefetch by default on Desktop with concurrency
    three.
    
    Bug: 499185782
    Change-Id: Ic69989a70dd352f6b286c6e165e1a02c7de8365b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885307
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639885}

--- content/browser/preloading/prefetch/prefetch_features.cc
+++ content/browser/preloading/prefetch/prefetch_features.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prefetch/prefetch_features.h"
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
 
@@ -69,12 +70,21 @@ const base::FeatureParam<size_t>
 
 BASE_FEATURE(kPrefetchCanaryCheckerParams, base::FEATURE_ENABLED_BY_DEFAULT);
 
+#if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kPrefetchMultipleActiveSetSizeLimitForBase,
              base::FEATURE_DISABLED_BY_DEFAULT);
 const base::FeatureParam<size_t>
     kPrefetchMultipleActiveSetSizeLimitForBaseValue{
         &kPrefetchMultipleActiveSetSizeLimitForBase,
         "prefetch_multiple_active_set_size_limit_for_base_value", 2};
+#else
+BASE_FEATURE(kPrefetchMultipleActiveSetSizeLimitForBase,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+const base::FeatureParam<size_t>
+    kPrefetchMultipleActiveSetSizeLimitForBaseValue{
+        &kPrefetchMultipleActiveSetSizeLimitForBase,
+        "prefetch_multiple_active_set_size_limit_for_base_value", 3};
+#endif
 
 BASE_FEATURE(kPrefetchEagerLimit, base::FEATURE_DISABLED_BY_DEFAULT);
 const base::FeatureParam<size_t> kMaxNumberOfEagerPrefetchesPerPage{
