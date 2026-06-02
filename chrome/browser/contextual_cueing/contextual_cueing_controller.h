464193eb94ef136b70937d35a6d85f46248846d8
Ian Wells <iwells@chromium.org>
2026-06-02 13:00:11 -0700

    ContextualCueingV2: show favicons in tab list
    The favicon fetch starts at the same time as the contextual cue request
    and we'll show whatever favicons are available when it's time to show
    the cue, falling back to a default otherwise.
    
    Screen recording (favicons present, but tab list bugs remain):
    https://screencast.googleplex.com/cast/NTAxNjM0OTE4ODI5MjYwOHw4OTBjMmFlYy0yYg
    
    Bug: 507551989
    Change-Id: I0401d11a43dc1f405f063fed8c652225379c7299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890302
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640406}

--- chrome/browser/contextual_cueing/contextual_cueing_controller.h
+++ chrome/browser/contextual_cueing/contextual_cueing_controller.h
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_enums.h"
 #include "chrome/browser/contextual_cueing/cue_target.h"
+#include "components/favicon_base/favicon_types.h"
 #include "components/optimization_guide/proto/features/contextual_cueing.pb.h"
 #include "components/page_content_annotations/core/page_content_annotations_service.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -27,6 +29,10 @@ namespace actions {
 class ActionItem;
 class ActionInvocationContext;
 }  // namespace actions
+
+namespace favicon {
+class FaviconService;
+}  // namespace favicon
 
 namespace optimization_guide {
 class ModelQualityLogEntry;
@@ -96,6 +102,10 @@ class ContextualCueingController
   // Initiates a model execution request to MES for the current window state.
   void InitiateModelExecutionRequest();
 
+  // Retrieves favicon for a specific web contents.
+  void FetchFavicon(tabs::TabInterface* tab,
+                    content::WebContents* web_contents);
+
   // Calculate the amount of time the current cue has been shown, and reset the
   // shown timestamp.
   base::TimeDelta ExtractCueShownDuration();
@@ -139,6 +149,9 @@ class ContextualCueingController
   void OnCueHidden();
   void OnCueFormFactorShown(CueFormFactor form_factor);
   void OnCueFormFactorHidden(CueFormFactor form_factor);
+  void OnFaviconAvailable(tabs::TabHandle handle,
+                          const favicon_base::FaviconImageResult& image_result);
+  void OnShowCueFailed(ContextualCueingDecision decision);
 
   void OnSidePanelShown();
 
@@ -160,10 +173,14 @@ class ContextualCueingController
   raw_ptr<syncer::SyncService> sync_service_;
   raw_ptr<TemplateURLService> template_url_service_;
   raw_ptr<signin::IdentityManager> identity_manager_;
+  raw_ptr<favicon::FaviconService> favicon_service_;
   absl::flat_hash_map<CueTargetType, std::unique_ptr<CueTarget>> cue_targets_;
   base::CallbackListSubscription side_panel_shown_subscription_;
   base::TimeTicks cue_shown_time_;
   base::TimeTicks cue_hidden_time_;
+
+  absl::flat_hash_map<tabs::TabHandle, ui::ImageModel> tab_favicons_;
+  base::CancelableTaskTracker cancelable_task_tracker_;
 
 #if !BUILDFLAG(IS_ANDROID)
   std::unique_ptr<page_actions::PageActionObserver> page_action_observer_;
