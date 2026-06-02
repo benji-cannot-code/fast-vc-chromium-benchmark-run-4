f27eb9f5119fa50137d4b9b1bb17c5b91004dc20
Maria Kazinova <kazinova@google.com>
2026-06-02 07:33:47 -0700

    [Passwords] Ignore password generation requests from BFCached frames
    Messages from frames in the Back/Forward Cache could still trigger
    password generation UI over a new active page.
    
    This CL adds an `IsActive()` check to the relevant Mojo entry points in
    `ChromePasswordManagerClient` to ignore messages from inactive frames.
    It also refactors the checks into a helper method to reduce duplication
    and adds a regression test.
    
    Fixed: 517714728
    Change-Id: Id10122097dc6cba426c7c314d7d9490f42b83757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895297
    Reviewed-by: Oleksandr Tara <otara@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640154}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -231,6 +231,16 @@ url::Origin URLToOrigin(GURL url) {
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
+// Returns true if the frame is active and not prerendering.
+// WARNING: This method will terminate the renderer process if the frame is
+// prerendering.
+bool CheckFrameActiveAndNotPrerendering(content::RenderFrameHost* rfh) {
+  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+    return false;
+  }
+  return rfh->IsActive();
+}
+
 }  // namespace
 
 // static
@@ -1515,7 +1525,7 @@ void ChromePasswordManagerClient::AutomaticGenerationAvailable(
               CPMD_BAD_ORIGIN_AUTOMATIC_GENERATION_STATUS_CHANGED)) {
     return;
   }
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
   password_manager::ContentPasswordManagerDriver* driver =
@@ -1592,7 +1602,7 @@ void ChromePasswordManagerClient::PresaveGeneratedPassword(
           BadMessageReason::CPMD_BAD_ORIGIN_PRESAVE_GENERATED_PASSWORD)) {
     return;
   }
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
 
@@ -1626,7 +1636,7 @@ void ChromePasswordManagerClient::PasswordNoLongerGenerated(
           BadMessageReason::CPMD_BAD_ORIGIN_PASSWORD_NO_LONGER_GENERATED)) {
     return;
   }
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
   PasswordManagerDriver* driver =
@@ -1658,7 +1668,7 @@ void ChromePasswordManagerClient::ShowPasswordEditingPopup(
     const std::u16string& password_value) {
   content::RenderFrameHost* rfh =
       password_generation_driver_receivers_.GetCurrentTargetFrame();
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
   if (!password_manager::bad_message::CheckGeneratedPassword(rfh,
@@ -1697,7 +1707,7 @@ void ChromePasswordManagerClient::ShowPasswordEditingPopup(
 void ChromePasswordManagerClient::PasswordGenerationRejectedByTyping() {
   content::RenderFrameHost* rfh =
       password_generation_driver_receivers_.GetCurrentTargetFrame();
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
   if (popup_controller_) {
@@ -1708,7 +1718,7 @@ void ChromePasswordManagerClient::PasswordGenerationRejectedByTyping() {
 void ChromePasswordManagerClient::FrameWasScrolled() {
   content::RenderFrameHost* rfh =
       password_generation_driver_receivers_.GetCurrentTargetFrame();
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
   if (popup_controller_) {
@@ -1719,7 +1729,7 @@ void ChromePasswordManagerClient::FrameWasScrolled() {
 void ChromePasswordManagerClient::GenerationElementLostFocus() {
   content::RenderFrameHost* rfh =
       password_generation_driver_receivers_.GetCurrentTargetFrame();
-  if (!password_manager::bad_message::CheckFrameNotPrerendering(rfh)) {
+  if (!CheckFrameActiveAndNotPrerendering(rfh)) {
     return;
   }
   // TODO(crbug.com/40629608): Look into removing this since FocusedInputChanged
