613777930cf3b656aa6d086c99ecbdd923ff21e4
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-06-02 08:31:04 -0700

    Inline OffscreenCanvas::SetFrameSinkId
    Function doesn't add value anymore. For call-site in ctor, move setting
    fields to initializer list, they are not used in the ctor body itself.
    
    Change-Id: Ie5ea3175dd87940c44e2fc1ab52844d73cb4111a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885059
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640189}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
@@ -116,7 +116,8 @@ class CORE_EXPORT OffscreenCanvas final
   }
 
   void SetFrameSinkIdForTesting(uint32_t client_id, uint32_t sink_id) {
-    SetFrameSinkId(client_id, sink_id);
+    client_id_ = client_id;
+    sink_id_ = sink_id;
   }
 
   uint32_t ClientId() const { return client_id_; }
@@ -255,10 +256,6 @@ class CORE_EXPORT OffscreenCanvas final
   static ContextFactoryVector& RenderingContextFactories();
   static CanvasRenderingContextFactory* GetRenderingContextFactory(int);
 
-  void SetFrameSinkId(uint32_t client_id, uint32_t sink_id) {
-    client_id_ = client_id;
-    sink_id_ = sink_id;
-  }
   void SetPlaceholderCanvasId(DOMNodeId canvas_id);
 
   Member<CanvasRenderingContext> context_;
