9e4bb84e0139fb388ae7f93d9f34af50184e3bde
Will Harris <wfh@chromium.org>
2026-06-01 17:04:12 -0700

    Revert "Prototype for different ASLR in each child process"
    This reverts commit 09eaeae3f24788e5f119b09a189d411f1d0577a0.
    
    Reason for revert: The field trial is concluded
    
    Original change's description:
    > Prototype for different ASLR in each child process
    >
    > Since Windows 8.1 the loader has been optimized to handle ASLR conflicts
    > more elegantly and will attempt to reuse mappings for the same Section
    > object.
    >
    > Add support to force ASLR for sandboxed child processes.
    >
    > Bug: 514425855
    > Change-Id: I6a17e428a103c1a20cfdc0011a974ed42022d9fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7858511
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1634649}
    
    Bug: 514425855
    Change-Id: I294f985b11109762e317df6f306f2a6d433a2266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7888543
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639790}

--- chrome/browser/chrome_content_browser_client.h
+++ chrome/browser/chrome_content_browser_client.h
@@ -605,8 +605,6 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
   bool ShouldEnableAudioProcessHighPriority() override;
   bool ShouldRestrictCoreSharingOnRenderer() override;
   std::optional<std::wstring> GetWindowsSecurityAttributeName() const override;
-  std::vector<uintptr_t> GetAslrBeaconAddresses(
-      sandbox::mojom::Sandbox sandbox_type) override;
 #endif
   void ExposeInterfacesToRenderer(
       service_manager::BinderRegistry* registry,
