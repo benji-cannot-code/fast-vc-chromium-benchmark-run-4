c4477bbee38239710f206ffd4c43f6c44037d555
mark a. foltz <mfoltz@chromium.org>
2026-06-02 11:47:09 -0700

    [WebMCP] Adds UseCounter for modelContext.getTools().
    This CL introduces the `ModelContextGetTools` UseCounter to measure
    usage of the `getTools()` method on `ModelContext`. It also maps it to
    the `NavigatorModelcontext` WebDX feature and updates the corresponding
    histograms and web tests.
    
    Bug: None
    Change-Id: I0203785d958e5449f0bfd87a414c5183f441fd28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896198
    Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640351}

--- third_party/blink/common/use_counter/webdx_feature_maps.cc
+++ third_party/blink/common/use_counter/webdx_feature_maps.cc
@@ -478,6 +478,8 @@ GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kWriter_WriteStreaming, WebDXFeature::kWriter},
           {WebFeature::kModelContextRegisterTool,
            WebDXFeature::kNavigatorModelcontext},
+          {WebFeature::kModelContextGetTools,
+           WebDXFeature::kNavigatorModelcontext},
           {WebFeature::kModelContextRegisterDeclarativeTool,
            WebDXFeature::kDeclarativeWebmcp},
           {WebFeature::kIdentityDigitalCredentials,
