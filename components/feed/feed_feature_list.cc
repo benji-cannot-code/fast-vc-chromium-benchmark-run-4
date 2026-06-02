11815475d628f7aef72b2ab4ebd1c973390d7a7d
Dibyendu Dey <dibyendudey@google.com>
2026-06-02 09:21:19 -0700

    Add a feature flag for wide screen feed on foldables.
    This change introduces a new feature flag, "WideScreenFeedForFoldables",
    which enables a wide screen single column feed layout in Discover for
    Android foldable devices. When the flag is enabled and the device is
    detected as a foldable, the WIDE_SCREEN_SINGLE_COLUMN_FEED capability is
    added to Feed requests.
    
    Bug: 518792402
    Change-Id: I95d38870142c1eb6a97657630e85621973c4b498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880214
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Dibyendu Dey <dibyendudey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640226}

--- components/feed/feed_feature_list.cc
+++ components/feed/feed_feature_list.cc
@@ -60,4 +60,6 @@ BASE_FEATURE(kFeedAudioOverviews, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidOpenIncognitoAsWindow, base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kWideScreenFeedForFoldables, base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace feed
