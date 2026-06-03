4cca92ff6a57f6e68176a0235e5e14534e2798cd
Sylvain Defresne <sdefresne@chromium.org>
2026-06-03 02:25:55 -0700

    Fix dangling pointer in AuthEventsRecorder scoped observation
    The AuthEventsRecorder observe the session_manager::SessionManager so
    it needs to be destroyed after the source not after. Change the order
    of the member variable to ensure the destruction order is correct.
    
    Bug: 493575740
    Change-Id: I31fd6e866f50345f0853203af421efc948c4cdf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7675317
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640795}

--- chrome/browser/ash/login/demo_mode/demo_login_controller_unittest.cc
+++ chrome/browser/ash/login/demo_mode/demo_login_controller_unittest.cc
@@ -299,9 +299,6 @@ class DemoLoginControllerTest : public testing::Test {
   testing::NiceMock<ash::MockLoginDisplayHost> mock_login_display_host_;
   system::FakeStatisticsProvider statistics_provider_;
 
-  // Required for `user_manager::UserList`:
-  std::unique_ptr<ash::AuthEventsRecorder> auth_events_recorder_;
-
   // Dependencies for `ExistingUserController`:
   FakeSessionManagerClient fake_session_manager_client_;
   ScopedCrosSettingsTestHelper settings_helper_;
@@ -312,6 +309,9 @@ class DemoLoginControllerTest : public testing::Test {
   session_manager::SessionManager session_manager_{
       std::make_unique<session_manager::FakeSessionManagerDelegate>()};
   std::unique_ptr<ExistingUserController> existing_user_controller_;
+
+  // Required for `user_manager::UserList`:
+  std::unique_ptr<ash::AuthEventsRecorder> auth_events_recorder_;
 
   std::unique_ptr<policy::MockCloudPolicyManager> cloud_policy_manager_;
 };
