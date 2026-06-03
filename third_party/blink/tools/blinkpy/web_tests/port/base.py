09c6bea917907abc30cc00f399ba77de6d785b4a
Jonathan Lee <jonathanjlee@google.com>
2026-06-02 17:31:35 -0700

    [web-tests] Remove macOS 12 expectations and support code
    *Note to gardeners*: If this CL introduces a few web test failures on
    macOS, prefer adding TestExpectations to reverting (given the large
    footprint and traffic around other TestExpectations lines).
    
    macOS 12 support is going away. Remove all platform-specific
    expectations and harness support code.
    
    Bug: 474036848, 1026712, 382173821, 710214, 443762762
    Bug: 440374815, 429979543, 457505256, 421653422, 418175808
    Bug: 415407103, 408039416, 406805472, 404571518, 398049421
    Bug: 395921918, 368512272, 366343782, 357909526, 356436618
    Bug: 345936780, 346368803, 346088872, 346088873, 346368829
    Bug: 346368830, 1359409, 1159689, 1171331, 1205184
    Bug: 40197500, 1299948, 1363708, 1450020, 413289778
    Bug: 453916409, 490316323, 40268242, 448806999
    Bug: 431036874, 1231596
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Change-Id: I95d1dde931d5f795fb9b8f2dd49e46e02cbf1160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897310
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640583}

--- third_party/blink/tools/blinkpy/web_tests/port/base.py
+++ third_party/blink/tools/blinkpy/web_tests/port/base.py
@@ -170,8 +170,6 @@ class Port(object):
     # the documentation in docs/testing/web_test_expectations.md when this list
     # changes.
     ALL_SYSTEMS = (
-        ('mac12', 'x86_64'),
-        ('mac12-arm64', 'arm64'),
         ('mac13', 'x86_64'),
         ('mac13-arm64', 'arm64'),
         ('mac14', 'x86_64'),
@@ -190,8 +188,6 @@ class Port(object):
 
     CONFIGURATION_SPECIFIER_MACROS = {
         'mac': [
-            'mac12',
-            'mac12-arm64',
             'mac13',
             'mac13-arm64',
             'mac14',
