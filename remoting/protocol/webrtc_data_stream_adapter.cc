aa5e599c9009de5f48140964d1a0ab743faab4b1
Danil Chapovalov <danilchap@chromium.org>
2026-06-03 11:55:19 -0700

    Add missing include for thread restrictions.
    This change adds the missing include for base::ScopedAllowBaseSyncPrimitivesOutsideBlockingScope
    
    Change-Id: I4cae0e54fa21f44f5562c6cd31a87107553a9bed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893521
    Auto-Submit: Danil Chapovalov <danilchap@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641090}

--- remoting/protocol/webrtc_data_stream_adapter.cc
+++ remoting/protocol/webrtc_data_stream_adapter.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/threading/thread_restrictions.h"
 #include "net/base/net_errors.h"
 #include "remoting/base/compound_buffer.h"
 #include "remoting/base/logging.h"
