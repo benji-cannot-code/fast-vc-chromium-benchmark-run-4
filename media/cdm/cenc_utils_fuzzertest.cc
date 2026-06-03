2612615c60071183f7ddb3bac0d4fa9f9a80d0ca
Kalvin Lee <kdlee@chromium.org>
2026-06-03 08:50:00 -0700

    Busybody: Use `DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN`
    Rather than force an `UNSAFE_TODO()` (or `UNSAFE_BUFFERS()`), use the centrally
    blessed macro for turning the libFuzzer-provided data-and-size pair into a
    byte span.
    
    This CL applies to:
    media/cdm
    
    This CL was uploaded by git cl split.
    
    R=vpasupathy@chromium.org
    
    Change-Id: Ie70cb8f55eb03c0bd5a6b569ab4011bbdf7d1be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895618
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Vikram Pasupathy <vpasupathy@chromium.org>
    Commit-Queue: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640971}

--- media/cdm/cenc_utils_fuzzertest.cc
+++ media/cdm/cenc_utils_fuzzertest.cc
@@ -3,14 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stddef.h>
 #include <stdint.h>
 
-#include <vector>
-
-#include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/logging.h"
 #include "media/cdm/cenc_utils.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 struct Environment {
   Environment() {
@@ -23,8 +21,7 @@ struct Environment {
 Environment* env = new Environment();
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::vector<uint8_t> input(data, UNSAFE_TODO(data + size));
-  media::ValidatePsshInput(input);
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(base::span<const uint8_t> data) {
+  media::ValidatePsshInput(data);
   return 0;
 }
