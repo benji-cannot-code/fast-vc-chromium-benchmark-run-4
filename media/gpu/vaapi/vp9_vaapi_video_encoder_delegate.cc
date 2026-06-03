7ffcc397f8f0987463b466d8949ccbc246b7693f
Hirokazu Honda <hiroh@chromium.org>
2026-06-03 14:33:49 -0700

    media/gpu/vaapi: Reject up-scaling in spatial layer encoding
    VideoEncodeAccelerator supports down-scaling in spatial layer encoding.
    This adds the validation checks to ensure that all spatial layer
    resolutions are less than or equal to the input visible size.
    
    Specifically:
    - VaapiVideoEncodeAccelerator now validates frame size against encode
      size during surface creation for mappable SI encoding.
    - VP9VaapiVideoEncoderDelegate validates spatial layer resolutions
      during initialization.
    - MojoVideoEncodeAcceleratorService rejects initialization if any
      spatial layer is larger than the input visible size.
    
    Bug: 516872067
    Test: video_encode_accelerator_tests
    Test: webrtc.RTCPeerConnection.vp9*
    Change-Id: I02e46207debbfecadecfd2cde7a2956a93b13313
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891570
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641219}

--- media/gpu/vaapi/vp9_vaapi_video_encoder_delegate.cc
+++ media/gpu/vaapi/vp9_vaapi_video_encoder_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <array>
+#include <cstdint>
 #include <numeric>
 
 #include "base/bits.h"
@@ -262,6 +263,13 @@ bool VP9VaapiVideoEncoderDelegate::Initialize(
       return false;
     }
     for (const auto& spatial_layer : config.spatial_layers) {
+      // Only down scaling is supported in spatial layer encoding.
+      if (spatial_layer.width > visible_size_.width() ||
+          spatial_layer.height > visible_size_.height()) {
+        VLOGF(1) << "Spatial layer resolution is larger than visible size";
+        return false;
+      }
+
       spatial_layer_resolutions.emplace_back(
           gfx::Size(spatial_layer.width, spatial_layer.height));
     }
