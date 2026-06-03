4c44e769dd813694b61ab95e54392db637f26a51
Rafał Godlewski <rgod@google.com>
2026-06-03 07:59:50 -0700

    [iOS] Use modern mock syntax
    Bug: None
    Change-Id: I9d0c6c975253d139dfd0f8e0da62e103ad6a40a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894610
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640941}

--- ios/chrome/browser/passwords/model/test/mock_ios_chrome_save_passwords_infobar_delegate.h
+++ ios/chrome/browser/passwords/model/test/mock_ios_chrome_save_passwords_infobar_delegate.h
@@ -8,13 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#include <memory>
-#include <optional>
+#import <memory>
+#import <optional>
 
-#include "components/password_manager/core/browser/password_form.h"
+#import "components/password_manager/core/browser/password_form.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_save_password_infobar_delegate.h"
-#include "testing/gmock/include/gmock/gmock.h"
-#include "url/gurl.h"
+#import "testing/gmock/include/gmock/gmock.h"
+#import "url/gurl.h"
 
 // Mock queue observer.
 class MockIOSChromeSavePasswordInfoBarDelegate
@@ -33,12 +33,15 @@ class MockIOSChromeSavePasswordInfoBarDelegate
       const GURL& url = GURL(),
       std::optional<std::string> account_to_store_password = std::nullopt);
 
-  MOCK_METHOD0(InfoBarDismissed, void());
-  MOCK_METHOD2(UpdateCredentials, void(NSString* username, NSString* password));
-  MOCK_METHOD0(Accept, bool());
-  MOCK_METHOD0(Cancel, bool());
-  MOCK_METHOD1(InfobarPresenting, void(bool automatic));
-  MOCK_METHOD0(InfobarGone, void());
+  MOCK_METHOD(void, InfoBarDismissed, (), (override));
+  MOCK_METHOD(void,
+              UpdateCredentials,
+              (NSString * username, NSString* password),
+              (override));
+  MOCK_METHOD(bool, Accept, (), (override));
+  MOCK_METHOD(bool, Cancel, (), (override));
+  MOCK_METHOD(void, InfobarPresenting, (bool automatic), (override));
+  MOCK_METHOD(void, InfobarGone, (), (override));
 
  private:
   MockIOSChromeSavePasswordInfoBarDelegate(
