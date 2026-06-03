ff3c788a0217cdf3edfeb7b5fd30dba211305efa
Sasha Kunitskaya <saskekun@google.com>
2026-06-03 02:54:44 -0700

    [FRE-Revamp] Add a new button label to the Finish or Continue screen
    This change introduces a new "See what's new" string for a button on the
    First Run Experience finish screen. It also adds character limits to
    several existing FRE strings. The new string is exposed to the UI via
    `intro_ui.cc`. The screenshot is added.
    
    Bug: 516392211
    Change-Id: I56bf1154f4c789e40c733d6ca6e361d0bc546fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881443
    Reviewed-by: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Commit-Queue: Sasha Kunitskaya <saskekun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640812}

--- chrome/browser/resources/intro/finish_or_continue/app.ts
+++ chrome/browser/resources/intro/finish_or_continue/app.ts
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '/strings.m.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import {getCss} from './app.css.js';
@@ -13,7 +15,7 @@ import {getHtml} from './app.html.js';
 export interface FinishOrContinueAppElement {
   $: {
     buttonContainer: HTMLElement,
-    seeMoreTipsButton: HTMLElement,
+    continueEducationButton: HTMLElement,
     startBrowsingButton: HTMLElement,
   };
 }
@@ -29,6 +31,20 @@ export class FinishOrContinueAppElement extends CrLitElement {
 
   override render() {
     return getHtml.bind(this)();
+  }
+
+  private isFeatureShowcaseEligible_: boolean = false;
+
+  constructor() {
+    super();
+    const params = new URLSearchParams(window.location.search);
+    this.isFeatureShowcaseEligible_ = params.get('showcase') === 'true';
+  }
+
+  protected getContinueEducationButtonLabel_(): string {
+    return this.isFeatureShowcaseEligible_
+        ? loadTimeData.getString('seeMoreTipsButtonLabel')
+        : loadTimeData.getString('seeWhatsNewButtonLabel');
   }
 }
 
