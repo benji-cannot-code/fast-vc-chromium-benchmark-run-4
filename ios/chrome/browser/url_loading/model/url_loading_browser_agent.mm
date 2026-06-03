1edae737845debe6392bfa4d70d374af2109535a
Michael Tatarski <mtatarski@google.com>
2026-06-03 16:58:10 -0700

    [STTS] Update and add TODOs for UI cleanup
    This CL updates existing TODOs and adds new ones to track the effort of
    cleaning up Send Tab To Self (STTS) UI code on Android and iOS, and
    migrating business logic to a shared native C++ implementation.
    
    Bug: 519101926
    Fixed: 485145029
    Change-Id: Ia884812dc42c14e1885f0a8f1da3fc3866f800f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893307
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641322}

--- ios/chrome/browser/url_loading/model/url_loading_browser_agent.mm
+++ ios/chrome/browser/url_loading/model/url_loading_browser_agent.mm
@@ -355,7 +355,7 @@ void UrlLoadingBrowserAgent::LoadUrlInTab(const UrlLoadParams& params,
   // Send Tab to Self navigations require a reliable way to be identified later
   // in the navigation lifecycle for handling things like scroll restoration and
   // form filling. We attach this user data to the WebState here.
-  // TODO(crbug.com/485145029): Consider using UrlLoadingObserver to encapsulate
+  // TODO(crbug.com/519101926): Consider using UrlLoadingObserver to encapsulate
   // this.
   if (params.is_from_send_tab_to_self()) {
     SendTabToSelfLoadNavigationUserData::CreateForWebState(
@@ -546,7 +546,7 @@ void UrlLoadingBrowserAgent::LoadUrlInNewTabImpl(
   // in the navigation lifecycle for handling things like scroll restoration and
   // form filling. We attach this user data to the WebState here because it is
   // the exact moment the tab is created for the STTS action.
-  // TODO(crbug.com/485145029): Consider using UrlLoadingObserver to encapsulate
+  // TODO(crbug.com/519101926): Consider using UrlLoadingObserver to encapsulate
   // this.
   if (params.is_from_send_tab_to_self()) {
     SendTabToSelfLoadNavigationUserData::CreateForWebState(
