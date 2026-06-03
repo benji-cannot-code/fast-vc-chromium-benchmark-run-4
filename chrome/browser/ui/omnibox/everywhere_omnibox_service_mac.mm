e5d0760ace803166ca054968926ec9f3498991d5
Aviv Kiss <avivkiss@google.com>
2026-06-02 19:10:52 -0700

    Implement standalone EverywhereOmnibox desktop compose popup
    This CL implements the EverywhereOmnibox feature, providing a global
    system keyboard shortcut (Cmd+Shift+Space on macOS, Ctrl+Shift+Space on
    Win/Linux) to summon a standalone WebUI compose popup centered on screen.
    
    Note: This feature is a prototype behind a flag and is not intended
    to launch any time soon.
    
    Features and architecture:
    - Global accelerator listening with EverywhereOmniboxService.
    - Un-cropped transparent frameless desktop layout.
    - WebUI popup and realbox/composebox components integration.
    - Mac-specific helpers implemented under EverywhereOmniboxServiceMac.
    - Simplified null checks in ContextualSearchboxHandler with safety DCHECKs.
    
    BUG=515862210
    TAG=agy
    CONV=3d627a72-2c0e-4ad4-bf79-527872b706ce
    
    Change-Id: I313a29d9c8c085d9309dee2ed4e95b6216650433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871983
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640647}

---
+++ chrome/browser/ui/omnibox/everywhere_omnibox_service_mac.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/omnibox/everywhere_omnibox_service.h"
+
+#import <Cocoa/Cocoa.h>
+
+#include "ui/gfx/native_ui_types.h"
+#include "ui/views/widget/widget.h"
+
+bool IsAppActiveOnMac() {
+  return [NSApp isActive];
+}
+
+void HideAppOnMac() {
+  [NSApp hide:nil];
+}
+
+void OrderEverywhereOmniboxFrontOnMac(views::Widget* widget) {
+  NSWindow* ns_window = widget->GetNativeWindow().GetNativeNSWindow();
+  [ns_window setLevel:NSFloatingWindowLevel];
+  [ns_window makeKeyWindow];
+  [ns_window orderFrontRegardless];
+}
