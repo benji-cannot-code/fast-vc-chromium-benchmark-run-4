20300c8a56996e1f81209da4435904f62fb7894b
mark a. foltz <mfoltz@chromium.org>
2026-06-03 15:25:38 -0700

    [media-router] Fix several logic and validation issues in DIAL.
    This CL updates the DIAL implementation to:
    
    1. Re-validate the cached application URL host against the current
    device IP address during cache lookup in DeviceDescriptionService.
    
    2. Make DIAL application name lookup for origin allowlisting
    case-insensitive in DialMediaRouteProvider.
    
    3. Explicitly reject dot-segment relative path tokens (".", "..") in
    IsDialAppName validation to prevent incorrect resolution of relative
    URLs.
    
    Includes unit tests for all updated behaviors.
    
    Fixed: 518111542,514460133,513022076
    Change-Id: Ibec34c60d8d2103b3387449735e7f259977cb146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899779
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Auto-Submit: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641256}

--- components/media_router/common/media_source_unittest.cc
+++ components/media_router/common/media_source_unittest.cc
@@ -259,7 +259,8 @@ TEST(MediaSourceTest, IsDialAppName) {
   EXPECT_FALSE(IsDialAppName(""));
   EXPECT_FALSE(IsDialAppName("App Name"));
   EXPECT_FALSE(IsDialAppName("App/Name"));
-  EXPECT_FALSE(IsDialAppName("../Name"));
+  EXPECT_FALSE(IsDialAppName("."));
+  EXPECT_FALSE(IsDialAppName(".."));
   EXPECT_FALSE(IsDialAppName("App\nName"));
 }
 
