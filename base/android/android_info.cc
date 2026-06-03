ee7583fcb2450e3b3b15f9b128daad3c83eec13e
Aaron Moss <aaronmoss@google.com>
2026-06-03 10:27:45 -0700

    Add sdk_int_full accessor to C++
    Motivated by a desire to be able to differentiate between Baklava major
    release and Baklava 1 minor release in C++ code of Chrome for Android,
    but will have ongoing utility for future minor releases.
    
    Change-Id: Iaa80df3081db1f56a791f6ee43fbd8d9b4dd83d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896023
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Aaron Moss <aaronmoss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641042}

--- base/android/android_info.cc
+++ base/android/android_info.cc
@@ -45,6 +45,7 @@ struct IAndroidInfo {
   const std::string manufacturer;
   const std::string model;
   int sdkInt;
+  int sdkIntFull;
   const std::string securityPatch;
   // Available only on android S+. For S-, this method returns empty string.
   const std::string socManufacturer;
@@ -97,6 +98,7 @@ static void JNI_AndroidInfo_FillFields(
     const std::string& socManufacturer,
     const std::string& supportedAbis,
     int32_t sdkInt,
+    int32_t sdkIntFull,
     bool isDebugAndroid,
     const std::string& securityPatch) {
   Set(IAndroidInfo{.abiName = supportedAbis,
@@ -112,6 +114,7 @@ static void JNI_AndroidInfo_FillFields(
                    .manufacturer = manufacturer,
                    .model = model,
                    .sdkInt = sdkInt,
+                   .sdkIntFull = sdkIntFull,
                    .securityPatch = securityPatch,
                    .socManufacturer = socManufacturer,
                    .versionIncremental = versionIncremental});
@@ -151,6 +154,10 @@ const std::string& android_build_fp() {
 
 int sdk_int() {
   return get_android_info().sdkInt;
+}
+
+int sdk_int_full() {
+  return get_android_info().sdkIntFull;
 }
 
 bool is_debug_android() {
