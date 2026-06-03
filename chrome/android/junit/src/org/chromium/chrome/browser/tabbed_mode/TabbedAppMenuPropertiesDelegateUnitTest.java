4dad2a3c6ab5426f3c1f4aaab0d4e2702ccb0cf7
Masa Fujita <massan@google.com>
2026-06-03 12:51:50 -0700

    nested app menu: Add individual recent tabs for windows
    This CL adds individual tabs to window items in the app menu. The tab
    items are currently disabled until we introduce individual tab restore
    for closed windows.
    
    Demo: http://screenshot/BdtVBnR3mTe2hBP
    
    Bug: 509065811
    Change-Id: I98e7b6c2fc05b71c41b73a867d93e543ebb712ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7887955
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641125}

--- chrome/android/junit/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegateUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegateUnitTest.java
@@ -4361,6 +4361,23 @@ public class TabbedAppMenuPropertiesDelegateUnitTest {
         entries.add(closedWindow);
         when(mRecentlyClosedEntriesManager.getRecentlyClosedEntries()).thenReturn(entries);
 
+        RecentlyClosedTab tab1 =
+                new RecentlyClosedTab(
+                        /* sessionId= */ 10,
+                        /* timestamp= */ 0,
+                        "Tab 1 Title",
+                        JUnitTestGURLs.URL_1,
+                        /* tabGroupId= */ null);
+        RecentlyClosedTab tab2 =
+                new RecentlyClosedTab(
+                        /* sessionId= */ 20,
+                        /* timestamp= */ 0,
+                        "Tab 2 Title",
+                        JUnitTestGURLs.URL_2,
+                        /* tabGroupId= */ null);
+        when(mRecentlyClosedEntriesManager.getTabsForClosedWindow(closedWindow))
+                .thenReturn(List.of(tab1, tab2));
+
         List<MenuItem> expectedSubmenu =
                 new ArrayList<>(
                         Arrays.asList(
@@ -4370,7 +4387,10 @@ public class TabbedAppMenuPropertiesDelegateUnitTest {
                                 item(R.id.divider_line_id),
                                 item(
                                         R.id.recent_entry_menu_item,
-                                        item(R.id.recent_entry_window_menu_item))));
+                                        item(R.id.recent_entry_window_menu_item),
+                                        item(R.id.divider_line_id),
+                                        item(R.id.recent_entry_tab_menu_item),
+                                        item(R.id.recent_entry_tab_menu_item))));
 
         List<ListItem> items =
                 findItemById(
@@ -4408,7 +4428,10 @@ public class TabbedAppMenuPropertiesDelegateUnitTest {
                                                 R.string.menu_window_title_with_tab_count,
                                                 "Custom Window",
                                                 tabsText),
-                                        item(restoreText))));
+                                        item(restoreText),
+                                        item(0),
+                                        item("Tab 1 Title"),
+                                        item("Tab 2 Title"))));
 
         assertMenuTitlesAreEqual(items, expectedTitles);
 
