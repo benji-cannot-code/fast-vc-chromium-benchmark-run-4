5ff3c2d3a34279925a91062c05342ce2f8a2dc11
Matthias Koerber <koerber@google.com>
2026-06-03 04:10:21 -0700

    [OneTimeToken] Shift OneTimeTokenService inheritance to KeyedService
    Make OneTimeTokenService inherit directly from KeyedService, and have
    OneTimeTokenServiceImpl inherit only from OneTimeTokenService.
    
    This avoids a downcast-to-unrelated-class bug in
    OneTimeTokenServiceFactory::GetForProfile, where GetServiceForBrowserContext()
    (which returns KeyedService*) was being static_cast'ed to
    OneTimeTokenServiceImpl*. In unit tests, a custom MockOneTimeTokenService
    is registered which does not inherit from OneTimeTokenServiceImpl, leading to
    undefined behavior flagged by UBSan. Casting directly to the interface
    class OneTimeTokenService* is safe because MockOneTimeTokenService is a
    subclass of OneTimeTokenService.
    
    Bug: 502907696
    Change-Id: Ibd82201cd26d67169d96e8307b4ee33b9104ee6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895440
    Reviewed-by: Trang Mai <trangm@google.com>
    Commit-Queue: Trang Mai <trangm@google.com>
    Auto-Submit: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640832}

--- components/one_time_tokens/core/browser/one_time_token_service.h
+++ components/one_time_tokens/core/browser/one_time_token_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "components/one_time_tokens/core/browser/one_time_token.h"
 #include "components/one_time_tokens/core/browser/one_time_token_retrieval_error.h"
 #include "components/one_time_tokens/core/browser/util/expiring_subscription.h"
@@ -28,14 +29,14 @@ enum class OneTimeTokenSource {
 };
 
 // Service to subscribe to `OneTimeToken`s. One instance per profile.
-class OneTimeTokenService {
+class OneTimeTokenService : public KeyedService {
  public:
   using CallbackSignature =
       void(OneTimeTokenSource,
            base::expected<OneTimeToken, OneTimeTokenRetrievalError>);
   using Callback = base::RepeatingCallback<CallbackSignature>;
 
-  virtual ~OneTimeTokenService() = default;
+  ~OneTimeTokenService() override = default;
 
   // Calls `callback` with tokens that were received in the recent past (if any
   // exist). `callback` may be called multiple times for this. This should
