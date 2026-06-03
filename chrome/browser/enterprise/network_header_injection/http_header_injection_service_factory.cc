6c6aa766e60e109772d74b8ce2230b9fcd9cdcf3
Anatoli Hancharou <antoli@google.com>
2026-06-03 09:26:48 -0700

    [Enterprise] Add HttpHeaderInjectionService
    This CL adds the HttpHeaderInjectionService and its associated factory,
    HttpHeaderInjectionServiceFactory.
    The service is responsible for listening to policy preference changes
    and updating the HttpHeaderInjectionMatcher with the new rules.
    
    This is the second part of a chain of CLs for the custom HTTP header
    injection feature.
    
    Bug: 265314050
    Change-Id: Ied6bd6193170606386691757abeaa6cdb1ade53e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7889501
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Anatoli Hancharou <antoli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641004}

---
+++ chrome/browser/enterprise/network_header_injection/http_header_injection_service_factory.cc
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/network_header_injection/http_header_injection_service_factory.h"
+
+#include "chrome/browser/profiles/profile.h"
+#include "components/enterprise/network_header_injection/core/http_header_injection_service.h"
+
+namespace enterprise_custom_headers {
+
+// static
+HttpHeaderInjectionService* HttpHeaderInjectionServiceFactory::GetForProfile(
+    Profile* profile) {
+  return static_cast<HttpHeaderInjectionService*>(
+      GetInstance()->GetServiceForBrowserContext(profile, true));
+}
+
+// static
+HttpHeaderInjectionServiceFactory*
+HttpHeaderInjectionServiceFactory::GetInstance() {
+  static base::NoDestructor<HttpHeaderInjectionServiceFactory> instance;
+  return instance.get();
+}
+
+HttpHeaderInjectionServiceFactory::HttpHeaderInjectionServiceFactory()
+    : ProfileKeyedServiceFactory("HttpHeaderInjectionServiceFactory",
+                                 ProfileSelections::BuildForRegularProfile()) {}
+
+HttpHeaderInjectionServiceFactory::~HttpHeaderInjectionServiceFactory() =
+    default;
+
+std::unique_ptr<KeyedService>
+HttpHeaderInjectionServiceFactory::BuildServiceInstanceForBrowserContext(
+    content::BrowserContext* context) const {
+  Profile* profile = Profile::FromBrowserContext(context);
+  return std::make_unique<HttpHeaderInjectionService>(profile->GetPrefs());
+}
+
+bool HttpHeaderInjectionServiceFactory::ServiceIsCreatedWithBrowserContext()
+    const {
+  return true;
+}
+
+}  // namespace enterprise_custom_headers
--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -471,6 +471,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     BUILDFLAG(IS_WIN)
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_connector_service_factory.h"
 #include "chrome/browser/enterprise/connectors/device_trust/device_trust_service_factory.h"
+#include "chrome/browser/enterprise/network_header_injection/http_header_injection_service_factory.h"  // nogncheck
 #include "chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_observer_factory.h"
 #endif
 
@@ -974,6 +975,10 @@ void ChromeBrowserMainExtraPartsProfiles::
   if (base::FeatureList::IsEnabled(enterprise_connectors::kTelomereReporting)) {
     enterprise_connectors::TelomereEventRouterFactory::GetInstance();
   }
+#endif
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || \
+    BUILDFLAG(IS_WIN)
+  enterprise_custom_headers::HttpHeaderInjectionServiceFactory::GetInstance();
 #endif
 #if BUILDFLAG(ENTERPRISE_WATERMARK)
   enterprise_data_protection::DataProtectionUrlLookupServiceFactory::
