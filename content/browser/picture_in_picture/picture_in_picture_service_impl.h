52170682886f75ab8e8b2a083115563cd393ef1a
Yuki Shiino <yukishiino@chromium.org>
2026-06-02 22:09:22 -0700

    Revert "[ImmersivePlayback] Simplify immersive Picture-in-Picture confirmation flow"
    This reverts commit e472fe98af9472b48586022b7877e1c6579f0340.
    
    Reason for revert: Suspicious about causing test failures of:
    PictureInPictureServiceImplTest.EnterImmersivePlayback_NoFullscreenFails
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/63594/overview
    
    Original change's description:
    > [ImmersivePlayback] Simplify immersive Picture-in-Picture confirmation flow
    >
    > Merges the separate RequestImmersivePlaybackConfirmation Mojo request
    > directly into StartSession on the browser side.
    >
    > - Mojo changes: StartSession now receives a `bool request_immersive` flag.
    > - State Machine: PictureInPictureServiceImpl implements a single-active-request confirmation queue. New/subsequent sessions immediately preempt and cancel any pending confirmation.
    > - Fullscreen bugfix: Prevent exiting fullscreen when entering immersive Picture-in-Picture, as native fullscreen must remain active to display the dialog.
    >
    > Bug: 514069440
    > Fixed: 514069440
    > Change-Id: I694fb6b2d7a411b567ceb215f875e84f66b59dd5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868111
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Commit-Queue: Oleh Desiatyrikov (xWF) <desiatyrikov@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1640570}
    
    Bug: 514069440
    Bug: 514069440
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I9e30f4cf5b5cb374446388987ebd50e8e703be72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893573
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640708}

--- content/browser/picture_in_picture/picture_in_picture_service_impl.h
+++ content/browser/picture_in_picture/picture_in_picture_service_impl.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_PICTURE_IN_PICTURE_PICTURE_IN_PICTURE_SERVICE_IMPL_H_
 #define CONTENT_BROWSER_PICTURE_IN_PICTURE_PICTURE_IN_PICTURE_SERVICE_IMPL_H_
 
-#include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/document_service.h"
 #include "media/mojo/mojom/media_player.mojom.h"
@@ -51,34 +50,13 @@ class CONTENT_EXPORT PictureInPictureServiceImpl final
       bool show_play_pause_button,
       mojo::PendingRemote<blink::mojom::PictureInPictureSessionObserver>,
       const gfx::Rect& source_bounds,
-      bool request_immersive,
+      blink::mojom::ImmersiveOptionsPtr immersive_options,
       StartSessionCallback) final;
+  void RequestImmersivePlaybackConfirmation(
+      RequestImmersivePlaybackConfirmationCallback) final;
 
  private:
   friend class PictureInPictureSession;
-
-  struct PendingSession {
-    PendingSession(
-        uint32_t player_id,
-        mojo::PendingAssociatedRemote<media::mojom::MediaPlayer> player_remote,
-        const viz::SurfaceId& surface_id,
-        const gfx::Size& natural_size,
-        bool show_play_pause_button,
-        mojo::PendingRemote<blink::mojom::PictureInPictureSessionObserver>
-            observer,
-        const gfx::Rect& source_bounds,
-        StartSessionCallback callback);
-    ~PendingSession();
-
-    uint32_t player_id;
-    mojo::PendingAssociatedRemote<media::mojom::MediaPlayer> player_remote;
-    viz::SurfaceId surface_id;
-    gfx::Size natural_size;
-    bool show_play_pause_button;
-    mojo::PendingRemote<blink::mojom::PictureInPictureSessionObserver> observer;
-    gfx::Rect source_bounds;
-    StartSessionCallback callback;
-  };
 
   PictureInPictureServiceImpl(
       RenderFrameHost&,
@@ -86,19 +64,6 @@ class CONTENT_EXPORT PictureInPictureServiceImpl final
   ~PictureInPictureServiceImpl() override;
 
   VideoPictureInPictureWindowControllerImpl& GetController();
-
-  void StartSessionInternal(
-      std::unique_ptr<PendingSession> pending_session,
-      blink::mojom::ImmersiveOptionsPtr immersive_options);
-
-  void StartSessionImmersive(std::unique_ptr<PendingSession> pending_session);
-
-  void OnImmersivePlaybackConfirmation(
-      std::unique_ptr<PendingSession> pending_session,
-      blink::mojom::ImmersivePlaybackConfirmationResultPtr result);
-
-  base::WeakPtrFactory<PictureInPictureServiceImpl>
-      immersive_confirmation_weak_factory_{this};
 };
 
 }  // namespace content
