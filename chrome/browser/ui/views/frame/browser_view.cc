0910cf47080f9e13ffd05009ff1c4f6ce0e1f4a6
Thomas Lukaszewicz <tluk@chromium.org>
2026-06-02 23:34:53 -0700

    [bedrock] Move browser elements init into BrowserWindow impl
    Initializing BrowserElements with the window specific ElementContext
    and any custom window element retrieval callbacks should be the
    responsibility of the BrowserWindow implementation.
    BrowserWindowFeatures should not have deep knowledge of the specifics
    of the different BrowserWindow implementations.
    
    Bug: 496674143
    Change-Id: I81b5636d5b4650938842dc643ec81f8d008d9c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7892107
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640732}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -177,6 +177,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/incognito_clear_browsing_data_dialog_coordinator.h"
 #include "chrome/browser/ui/views/infobars/infobar_container_view.h"
 #include "chrome/browser/ui/views/interaction/browser_elements_views.h"
+#include "chrome/browser/ui/views/interaction/browser_elements_views_impl.h"
 #include "chrome/browser/ui/views/location_bar/intent_chip_button.h"
 #include "chrome/browser/ui/views/location_bar/intent_picker_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
@@ -4840,6 +4841,14 @@ void BrowserView::AddedToWidget() {
   views::ClientView::AddedToWidget();
 
   widget_observation_.Observe(GetWidget());
+
+  auto* browser_elements =
+      BrowserElements::From(browser_)->AsA<BrowserElementsViewsImpl>();
+  browser_elements->Init(this);
+  browser_elements->AddRetrievalCallback(
+      kActiveContentsWebViewRetrievalId,
+      base::BindRepeating(&BrowserView::GetActiveContentsWebView,
+                          base::Unretained(this)));
 
   // Stow a pointer to this object onto the window handle so that we can get at
   // it later when all we have is a native view.
