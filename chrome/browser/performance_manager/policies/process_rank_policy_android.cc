5acf209103870be697509835de4988fb82298b3e
Shin Kawamura <kawasin@google.com>
2026-06-03 09:43:52 -0700

    Reland "PerformanceManager: Enable ProtectRecentlyVisibleTab via DeviceInfo.isDesktop"
    This is a reland of commit cd2bee67333b680be30fe3a9be69d92a72e5750b
    
    This changes ChromeFeatureList.java. `BuildConfig.IS_DESKTOP_ANDROID`
    was a workaround of using `BUILDFLAG(IS_DESKTOP_ANDROID)` in
    chrome/browser/chrome_browser_field_trials.cc
    
    Original change's description:
    > PerformanceManager: Enable ProtectRecentlyVisibleTab via DeviceInfo.isDesktop
    >
    > Previously, the `ProtectRecentlyVisibleTab` feature was enabled on
    > Android Desktop using static feature overrides in
    > `chrome_browser_field_trials.cc`. Relying on field trial overrides for
    > baseline platform enablement is discouraged, as it conflates
    > experimentation configurations with standard platform defaults.
    >
    > This change migrates feature enablement to runtime checks directly
    > within policy evaluation and process initialization paths.
    >
    > TAG=agy
    >
    > Bug: 513050530
    > Change-Id: Ia26b70bbe3817fe071b4416c45fc0b0b5a3b7442
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7855781
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: Shin Kawamura <kawasin@google.com>
    > Reviewed-by: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Eric Lok <lokeric@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1639206}
    
    Bug: 513050530
    Change-Id: I8edb2758ba5cefa00c679c05a62f13ab705206c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893747
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641012}

--- chrome/browser/performance_manager/policies/process_rank_policy_android.cc
+++ chrome/browser/performance_manager/policies/process_rank_policy_android.cc
@@ -135,6 +135,12 @@ bool IsChangeUnfocusedPriorityEnabled() {
              chrome::android::kChangeUnfocusedPriority);
 }
 
+bool IsProtectRecentlyVisibleTabEnabled() {
+  return base::android::device_info::is_desktop() ||
+         base::FeatureList::IsEnabled(
+             chrome::android::kProtectRecentlyVisibleTab);
+}
+
 }  // namespace
 
 ProcessRankPolicyAndroid::ProcessRankPolicyAndroid()
@@ -164,8 +170,7 @@ void ProcessRankPolicyAndroid::OnPassedToGraph(Graph* graph) {
 
 void ProcessRankPolicyAndroid::OnTakenFromGraph(Graph* graph) {
   graph->RemovePageNodeObserver(this);
-  if (base::FeatureList::IsEnabled(
-          chrome::android::kProtectRecentlyVisibleTab)) {
+  if (IsProtectRecentlyVisibleTabEnabled()) {
     visibility_timers_.clear();
   }
 }
@@ -186,8 +191,7 @@ void ProcessRankPolicyAndroid::OnBeforePageNodeRemoved(
     const PageNode* page_node) {
   PageLiveStateDecorator::Data::GetOrCreateForPageNode(page_node)
       ->RemoveObserver(this);
-  if (base::FeatureList::IsEnabled(
-          chrome::android::kProtectRecentlyVisibleTab)) {
+  if (IsProtectRecentlyVisibleTabEnabled()) {
     visibility_timers_.erase(page_node);
   }
 }
@@ -202,8 +206,7 @@ void ProcessRankPolicyAndroid::OnIsFocusedChanged(const PageNode* page_node) {
 }
 
 void ProcessRankPolicyAndroid::OnIsVisibleChanged(const PageNode* page_node) {
-  if (base::FeatureList::IsEnabled(
-          chrome::android::kProtectRecentlyVisibleTab)) {
+  if (IsProtectRecentlyVisibleTabEnabled()) {
     if (page_node->IsVisible()) {
       visibility_timers_.erase(page_node);
     } else {
@@ -400,8 +403,7 @@ content::ChildProcessImportance ProcessRankPolicyAndroid::CalculateRank(
         DiscardEligibilityPolicy::GetFromGraph(GetOwningGraph());
     CHECK(eligibility_policy);
     base::TimeDelta minimum_time_in_background;
-    if (base::FeatureList::IsEnabled(
-            chrome::android::kProtectRecentlyVisibleTab)) {
+    if (IsProtectRecentlyVisibleTabEnabled()) {
       minimum_time_in_background = base::Seconds(
           chrome::android::kProtectRecentlyVisibleTabDuration.Get());
     }
