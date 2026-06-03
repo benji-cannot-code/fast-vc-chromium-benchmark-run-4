f024e06eec6cea356e79abfeb40812e94fba5038
Isabelle Goralsky <igoralsky@google.com>
2026-06-02 18:14:45 -0700

    Introduce sidebar for navigation to Context Hub UI.
    This change introduces a sidebar navigation for Context Hub, allowing
    users to switch between AI Taskbox and Memory Banks content. Basic
    styling is done, but in the future gFonts/icons will be used to align
    with mocks. See bug for more details/screencast.
    
    Bug: b:517998452
    Change-Id: Idc246bd8858cfdb3dd70931b94ff59fd730ac7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883847
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640616}

---
+++ chrome/browser/resources/context_hub/memory_banks/memory_banks.html.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {MemoryBanksElement} from './memory_banks.js';
+
+export function getHtml(this: MemoryBanksElement) {
+  return html`
+    <main id="memory-banks-view">
+        <section>
+            <h1>Memory banks</h1>
+        </section>
+    </main>
+  `;
+}
---
+++ chrome/browser/resources/context_hub/taskbox/ai_taskbox.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {getHtml} from './ai_taskbox.html.js';
+
+export class AiTaskboxElement extends CrLitElement {
+  static get is() {
+    return 'ai-taskbox';
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ai-taskbox': AiTaskboxElement;
+  }
+}
+
+customElements.define(AiTaskboxElement.is, AiTaskboxElement);
