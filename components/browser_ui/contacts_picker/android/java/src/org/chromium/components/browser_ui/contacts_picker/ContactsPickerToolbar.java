fa06b19f38829898bb682972872df9eb6d0b02b0
Matt Reynolds <mattreynolds@google.com>
2026-06-03 13:24:45 -0700

    contacts: Use Close icon to exit contact picker flow
    The new Android contacts picker flow is a two step flow. In the second
    step, the user can tap a button to exit the flow. This CL changes the
    button icon from R.drawable.ic_arrow_back_white_24dp (back arrow) to
    R.drawable.material_ic_close_24dp (close button) when the new flow is
    enabled to better indicate that this button closes the picker flow
    instead of navigating back to the first step.
    
    Bug: 510054742
    Change-Id: I8b7bef7db5ef0846174bb823acc76dc5b81476e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897665
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641147}

--- components/browser_ui/contacts_picker/android/java/src/org/chromium/components/browser_ui/contacts_picker/ContactsPickerToolbar.java
+++ components/browser_ui/contacts_picker/android/java/src/org/chromium/components/browser_ui/contacts_picker/ContactsPickerToolbar.java
@@ -67,6 +67,12 @@ public class ContactsPickerToolbar extends SelectableListToolbar<ContactDetails>
     }
 
     @Override
+    protected void onNavigationBack() {
+        assumeNonNull(mDelegate);
+        mDelegate.onNavigationBackCallback();
+    }
+
+    @Override
     public void initialize(
             SelectionDelegate<ContactDetails> delegate,
             int titleResId,
@@ -102,7 +108,9 @@ public class ContactsPickerToolbar extends SelectableListToolbar<ContactDetails>
             done.setTextAppearance(R.style.TextAppearance_TextMedium_Disabled);
         }
 
-        if (isSystemPickerEnabled || !contactsSelected) {
+        if (isSystemPickerEnabled) {
+            setNavigationButton(NavigationButton.CLOSE);
+        } else if (!contactsSelected) {
             showBackArrow();
         } else {
             setNavigationButton(NavigationButton.SELECTION_BACK);
