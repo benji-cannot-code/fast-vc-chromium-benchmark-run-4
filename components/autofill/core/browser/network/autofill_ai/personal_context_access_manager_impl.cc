1eb691f1b840af1e016227124951bd1e1f67e972
Norge Vizcay <vizcay@google.com>
2026-06-03 08:42:50 -0700

    Drop prefetch callback from PersonalContextAccessManager
    This CL removes the PrefetchAmbientAutofillContextCallback callback from
    the PrefetchAmbientAutofillContext function signature and interfaces.
    
    The callback is not necessary.
    
    Bug: 516721244
    Change-Id: Ibc0a91291a751295478db3b6de1ec5b3641ce71d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895258
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640963}

--- components/autofill/core/browser/network/autofill_ai/personal_context_access_manager_impl.cc
+++ components/autofill/core/browser/network/autofill_ai/personal_context_access_manager_impl.cc
@@ -63,8 +63,7 @@ PersonalContextAccessManagerImpl::PersonalContextAccessManagerImpl(
 PersonalContextAccessManagerImpl::~PersonalContextAccessManagerImpl() = default;
 
 void PersonalContextAccessManagerImpl::PrefetchAmbientAutofillContext(
-    base::span<const EntityType> requested_types,
-    PrefetchAmbientAutofillContextCallback callback) {
+    base::span<const EntityType> requested_types) {
   personal_context::proto::ContextMemoryAmbientAutofillRequest request;
   for (const EntityType& type : requested_types) {
     // TODO(crbug.com/516721244): Only request types with !IsTypeCached.
@@ -77,14 +76,12 @@ void PersonalContextAccessManagerImpl::PrefetchAmbientAutofillContext(
       /*options=*/{},
       base::BindOnce(&PersonalContextAccessManagerImpl::
                          OnPrefetchAmbientAutofillContextComplete,
-                     weak_factory_.GetWeakPtr(), std::move(callback)));
+                     weak_factory_.GetWeakPtr()));
 }
 
 void PersonalContextAccessManagerImpl::OnPrefetchAmbientAutofillContextComplete(
-    PrefetchAmbientAutofillContextCallback callback,
     personal_context::FetchContextResult result) {
   if (!result.response.has_value()) {
-    std::move(callback).Run(base::unexpected(result.response.error()));
     return;
   }
 
@@ -92,7 +89,7 @@ void PersonalContextAccessManagerImpl::OnPrefetchAmbientAutofillContextComplete(
                  personal_context::ContextMemoryError>
       entities = ExtractEntitiesFromResponse(result.response.value().value());
 
-  std::move(callback).Run(std::move(entities));
+  // TODO(crbug.com/516721244): Cache the prefetched entities.
 }
 
 std::optional<EntityInstance> PersonalContextAccessManagerImpl::GetCachedEntity(
