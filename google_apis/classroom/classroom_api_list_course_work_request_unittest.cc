af3878f213d5b24b604e3d938fdb9c29f0c1789e
Ahmed Nasr <anasr@google.com>
2026-06-03 12:44:56 -0700

    Add guided learning to classroom list course work read mask
    Bug: 519623433
    Change-Id: Ie933155c7ceefa18ec04386450c131bc0fd006bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7898153
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641121}

--- google_apis/classroom/classroom_api_list_course_work_request_unittest.cc
+++ google_apis/classroom/classroom_api_list_course_work_request_unittest.cc
@@ -123,8 +123,8 @@ TEST_F(ClassroomApiListCourseWorkRequestTest, ListCourseWorkRequest) {
                    "%2CcreationTime%2CupdateTime%2CdueDate(year%2Cmonth%2Cday)"
                    "%2CdueTime(hours%2Cminutes%2Cseconds%2Cnanos)%2CworkType"
                    "%2Cmaterials(youtubeVideo(title)%2Clink(title)"
-                   "%2Cform(title)%2CdriveFile(driveFile(title))))"
-                   "%2CnextPageToken")))))
+                   "%2Cform(title)%2CguidedLearning(title)"
+                   "%2CdriveFile(driveFile(title))))%2CnextPageToken")))))
       .WillOnce(Return(ByMove(TestRequestHandler::CreateSuccessfulResponse())));
 
   base::test::TestFuture<
@@ -153,8 +153,8 @@ TEST_F(ClassroomApiListCourseWorkRequestTest,
                    "%2CcreationTime%2CupdateTime%2CdueDate(year%2Cmonth%2Cday)"
                    "%2CdueTime(hours%2Cminutes%2Cseconds%2Cnanos)%2CworkType"
                    "%2Cmaterials(youtubeVideo(title)%2Clink(title)"
-                   "%2Cform(title)%2CdriveFile(driveFile(title))))"
-                   "%2CnextPageToken"
+                   "%2Cform(title)%2CguidedLearning(title)"
+                   "%2CdriveFile(driveFile(title))))%2CnextPageToken"
                    "&pageToken=qwerty")))))
       .WillOnce(Return(ByMove(TestRequestHandler::CreateSuccessfulResponse())));
 
@@ -184,8 +184,8 @@ TEST_F(ClassroomApiListCourseWorkRequestTest,
                    "%2CcreationTime%2CupdateTime%2CdueDate(year%2Cmonth%2Cday)"
                    "%2CdueTime(hours%2Cminutes%2Cseconds%2Cnanos)%2CworkType"
                    "%2Cmaterials(youtubeVideo(title)%2Clink(title)"
-                   "%2Cform(title)%2CdriveFile(driveFile(title))))"
-                   "%2CnextPageToken")))))
+                   "%2Cform(title)%2CguidedLearning(title)"
+                   "%2CdriveFile(driveFile(title))))%2CnextPageToken")))))
       .WillOnce(Return(ByMove(TestRequestHandler::CreateFailedResponse())));
 
   base::test::TestFuture<
