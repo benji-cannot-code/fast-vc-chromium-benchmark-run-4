e9e514cecc6fff38f9e4dc15240d29f35d7883ba
Ryan Thomas <rathomas@google.com>
2026-06-03 13:45:38 -0700

    [PDF V2] Add print functionality to the PDF viewer.
    This change adds a print button to the PDF toolbar. Clicking the button triggers the printing flow. To avoid code duplication, the shared printing logic from `ChromeActivity` and `NativePageFactory` has been refactored into a new `PrintHelper` class, which also includes robustness checks for tab initialization.
    
    Verification: http://screencast/cast/NTQwMjU0MDk3MDYwNjU5Mnw5MDRhZGNjMC1iZQ
    Bug: 516844987
    Change-Id: I23e46148098302f947c50623dff97e7cebe22791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880165
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Ryan Thomas <rathomas@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641175}

--- chrome/android/java/src/org/chromium/chrome/browser/native_page/NativePageFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/native_page/NativePageFactory.java
@@ -52,6 +52,7 @@ import org.chromium.chrome.browser.ntp.RecentTabsPage;
 import org.chromium.chrome.browser.pdf.PdfFragmentViewTrackerImpl;
 import org.chromium.chrome.browser.pdf.PdfInfo;
 import org.chromium.chrome.browser.pdf.PdfPage;
+import org.chromium.chrome.browser.printing.PrintHelper;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.tab.Tab;
@@ -670,6 +671,11 @@ public class NativePageFactory {
         public EdgeToEdgePadAdjuster createEdgeToEdgePadAdjuster(View view) {
             return EdgeToEdgeControllerFactory.createForViewAndObserveSupplier(
                     view, mEdgeToEdgeControllerSupplier);
+        }
+
+        @Override
+        public void print() {
+            PrintHelper.printTab(mTab);
         }
     }
 
--- chrome/browser/ui/android/pdf/internal/java/src/org/chromium/chrome/browser/pdf/PdfToolbarActionsDelegate.java
+++ chrome/browser/ui/android/pdf/internal/java/src/org/chromium/chrome/browser/pdf/PdfToolbarActionsDelegate.java
@@ -46,6 +46,9 @@ public interface PdfToolbarActionsDelegate {
     /** Downloads the PDF document. */
     void download();
 
+    /** Triggers printing for the PDF document. */
+    void print();
+
     /** Rotates the PDF document. */
     void rotate();
 }
