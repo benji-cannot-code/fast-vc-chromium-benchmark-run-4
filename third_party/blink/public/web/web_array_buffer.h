5f661b4e36e05c35b17a55610fca29a51f8e9f30
Fredrik Söderquist <fs@opera.com>
2026-06-03 05:49:57 -0700

    Replace WebArrayBuffer::Data()/ByteLength() with ByteSpan()
    This forwards the ByteSpan() accessor from the underlying
    DOMArrayBuffer.
    
    Improve the copy loop in SkiaBenchmarking::Rasterize() a little bit.
    
    Bug: 451652367
    Change-Id: Ie95e1aac9667e3e017feebca953c54020bc49f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893630
    Owners-Override: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1640869}

--- third_party/blink/public/web/web_array_buffer.h
+++ third_party/blink/public/web/web_array_buffer.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ARRAY_BUFFER_H_
 #define THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ARRAY_BUFFER_H_
 
+#include "base/containers/span.h"
 #include "third_party/blink/public/platform/web_common.h"
 #include "third_party/blink/public/platform/web_private_ptr.h"
 
@@ -57,8 +58,7 @@ class BLINK_EXPORT WebArrayBuffer {
   void Assign(const WebArrayBuffer&);
 
   bool IsNull() const { return private_.IsNull(); }
-  void* Data() const;
-  size_t ByteLength() const;
+  base::span<uint8_t> ByteSpan() const;
 
 #if INSIDE_BLINK
   WebArrayBuffer(DOMArrayBuffer*);
