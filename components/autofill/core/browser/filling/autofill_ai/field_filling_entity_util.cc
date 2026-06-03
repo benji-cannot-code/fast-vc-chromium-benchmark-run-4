fe0464b4618c086b0befd2b2ff729e1aee09f419
Jihad Hanna <jihadghanna@google.com>
2026-06-03 00:36:23 -0700

    Skip previewing select elements with obfuscated attributes
    Fixed: 519243927
    Change-Id: I89714ab7e526d984ecebdff482990a7c08cf05ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894642
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640752}

--- components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util.cc
+++ components/autofill/core/browser/filling/autofill_ai/field_filling_entity_util.cc
@@ -287,6 +287,13 @@ FillingValueAndType GetFillingValueAndTypeForEntity(
     return FillingValueAndType(u"", field_type);
   }
 
+  const bool should_obfuscate =
+      action_persistence != mojom::ActionPersistence::kFill &&
+      attribute->type().is_obfuscated();
+  if (should_obfuscate && field.IsSelectElement()) {
+    return FillingValueAndType(u"", field_type);
+  }
+
   if (field.IsSelectElement()) {
     std::optional<SelectOption> select_control_option =
         GetOptionForSelect(*attribute, field, app_locale, address_normalizer);
@@ -298,10 +305,6 @@ FillingValueAndType GetFillingValueAndTypeForEntity(
         field_type);
   } else {
     std::u16string fill_value = GetValueForInput(*attribute, field, app_locale);
-
-    const bool should_obfuscate =
-        action_persistence != mojom::ActionPersistence::kFill &&
-        attribute->type().is_obfuscated();
 
     return FillingValueAndType(should_obfuscate ? GetObfuscatedValue(fill_value)
                                                 : std::move(fill_value),
