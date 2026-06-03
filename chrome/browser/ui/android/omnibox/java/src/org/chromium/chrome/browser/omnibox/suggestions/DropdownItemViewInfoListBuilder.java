4fa6de947d07ed4f954505ddf0c5e1f5a94cc285
Sky Malice <skym@chromium.org>
2026-06-03 10:26:15 -0700

    [AIM] Convert suggestions to use PositionalMode for corner rounding.
    This makes no functional change, but sets suggestions up to add more
    complexity in follow up CLs, ultimately changing when corners are
    rounded.
    
    Bug: 518813849
    Change-Id: I212114c72de9ebb92f7e5d67a739d677a80ec9a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896529
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641039}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
@@ -17,6 +17,7 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider.ControlsPosition;
 import org.chromium.chrome.browser.omnibox.UrlBarEditingTextStateProvider;
 import org.chromium.chrome.browser.omnibox.styles.OmniboxImageSupplier;
+import org.chromium.chrome.browser.omnibox.suggestions.SuggestionCommonProperties.PositionalMode;
 import org.chromium.chrome.browser.omnibox.suggestions.answer.AnswerSuggestionProcessor;
 import org.chromium.chrome.browser.omnibox.suggestions.basic.BasicSuggestionProcessor;
 import org.chromium.chrome.browser.omnibox.suggestions.basic.BasicSuggestionProcessor.BookmarkState;
@@ -266,9 +267,6 @@ class DropdownItemViewInfoListBuilder {
             }
         }
 
-        var roundingStartEdge = SuggestionCommonProperties.BG_TOP_CORNER_ROUNDED;
-        var roundingEndEdge = SuggestionCommonProperties.BG_BOTTOM_CORNER_ROUNDED;
-
         for (int indexInList = 0; indexInList < numGroupMatches; indexInList++) {
             var indexOnList = firstVerticalPosition + indexInList;
             AutocompleteMatch match = groupMatches.get(indexInList);
@@ -276,8 +274,10 @@ class DropdownItemViewInfoListBuilder {
             var model = processor.createModel();
 
             boolean isFirstItem = indexInList == 0;
-            model.set(roundingStartEdge, isFirstItem);
-            model.set(roundingEndEdge, indexInList == numGroupMatches - 1);
+            boolean isLastItem = indexInList == numGroupMatches - 1;
+            model.set(
+                    SuggestionCommonProperties.BG_POSITIONAL_MODE,
+                    getPositionalMode(isFirstItem, isLastItem));
             model.set(SuggestionCommonProperties.SHOW_DIVIDER, indexInList < numGroupMatches - 1);
             model.set(
                     SuggestionCommonProperties.SHOW_GROUP_SEPARATOR,
@@ -441,6 +441,18 @@ class DropdownItemViewInfoListBuilder {
         }
 
         return viewInfoList;
+    }
+
+    private static @PositionalMode int getPositionalMode(boolean isFirstItem, boolean isLastItem) {
+        if (isFirstItem && isLastItem) {
+            return PositionalMode.SINGLE;
+        } else if (isFirstItem) {
+            return PositionalMode.TOP;
+        } else if (isLastItem) {
+            return PositionalMode.BOTTOM;
+        } else {
+            return PositionalMode.MIDDLE;
+        }
     }
 
     /**
