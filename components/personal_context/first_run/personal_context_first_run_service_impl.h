78bc64a48b1d9ffb7fa2ef22c506cac0833b0ae8
Rainhard Findling <rainhard@chromium.org>
2026-06-03 06:44:46 -0700

    Personal Context: setting<>notice<>prefs logic simplification #3
    Clean up FirstRun's MarkPersonalContextInAutofillNoticeAsShown()
    function, as it's no longer needed with the new product requirements.
    
    Simplify MarkPersonalContextInAutofillNoticeAsAcknowledged() as it no
    longer needs to set the kPersonalContextInAutofillSettingsToggleStatus
    pref.
    
    Bug: 519211773
    Change-Id: Ie43a7d00b7260ae765af81be49ef8291fd521b9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893668
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640892}

--- components/personal_context/first_run/personal_context_first_run_service_impl.h
+++ components/personal_context/first_run/personal_context_first_run_service_impl.h
@@ -41,7 +41,6 @@ class PersonalContextFirstRunServiceImpl
       content::WebContents* web_contents,
       FirstRunInvocationSource invocation_source,
       base::OnceCallback<void(FirstRunTriggerResult)> callback) override;
-  void MarkPersonalContextInAutofillNoticeAsShown() override;
   void MarkPersonalContextInAutofillNoticeAsAcknowledged() override;
   bool ShouldShowPersonalContextAutofillNotice() const override;
 
