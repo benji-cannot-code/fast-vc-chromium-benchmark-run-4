b6f77960f5ede4609a770cfea18778611be90fe0
Xi Han <hanxi@google.com>
2026-06-03 15:14:16 -0700

    [ThemeSync] Introduce NtpThemeSyncHistoryCoordinator.
    In this CL:
    1) Add an empty NtpThemeSyncHistoryCoordinator which is responsible
       for showing NTP theme customization history.
    2) Integrate NtpThemeSyncHistoryCoordinator with the NTP customization
       bottom sheet. The "Appearance" item isn't shown if history is shown.
    
    Bug: 488439751
    Change-Id: Id129b74bc3fa2cd56c6ac4efdaa5469f786b4852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896401
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641249}

---
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme_sync/NtpThemeSyncHistoryCoordinator.java
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.ntp_customization.theme_sync;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.ntp_customization.BottomSheetDelegate;
+
+/** Coordinator for the NTP theme sync history. */
+@NullMarked
+public class NtpThemeSyncHistoryCoordinator {
+    public NtpThemeSyncHistoryCoordinator(
+            Context context,
+            ViewGroup parentView,
+            BottomSheetDelegate bottomSheetDelegate,
+            View.OnClickListener moreOptionsClickListener) {}
+
+    /** Prepare data before showing the NTP theme history. */
+    public void prepareToShow() {}
+
+    /** Called to destroy the NtpThemeSyncHistoryCoordinator. */
+    public void destroy() {}
+}
