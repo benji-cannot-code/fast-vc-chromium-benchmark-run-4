10d5df193521bb5fa0713e9be78cad258abc8913
Federica Germinario <fedegermi@google.com>
2026-06-03 09:16:36 -0700

    [iOS] Fix UIApplication mock leak in ScreenshotMetricsRecorderTest
    ScreenshotMetricsRecorderTest partially mocks [UIApplication
    sharedApplication] in SetUp() but did not call stopMocking in
    TearDown(). Because UIApplication is a singleton, this caused the mock
    to leak and keep the application state permanently swizzled for any
    subsequent tests running in the same process. This led to unexpected
    test pollution and failures in unrelated tests (e.g.,
    TaskUpdaterSceneAgentTest).
    
    This CL adds a TearDown() method to explicitly call stopMocking,
    cleaning up the global state and resolving the test pollution.
    
    Change-Id: Ifb9412a9e1cc6a70d3842016b6912f06f236b55a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894927
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640995}

--- ios/chrome/browser/screenshot/model/screenshot_metrics_recorder_unittest.mm
+++ ios/chrome/browser/screenshot/model/screenshot_metrics_recorder_unittest.mm
@@ -23,6 +23,11 @@ class ScreenshotMetricsRecorderTest : public PlatformTest {
   ScreenshotMetricsRecorderTest() {}
   ~ScreenshotMetricsRecorderTest() override {}
 
+  void TearDown() override {
+    [application_partial_mock_ stopMocking];
+    PlatformTest::TearDown();
+  }
+
   void SetUp() override {
     application_partial_mock_ =
         OCMPartialMock([UIApplication sharedApplication]);
