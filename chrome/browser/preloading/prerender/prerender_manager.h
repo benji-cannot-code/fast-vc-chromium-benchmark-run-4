255ef2e5e175043ac56939343f42114f05400e85
Jiacheng Guo <gjc@google.com>
2026-06-02 18:43:05 -0700

    Refactor PrerenderHandle callbacks into a single lifecycle observer
    The CL merges the event callbacks in PrerenderHandle into one single
    PrerenderLifecycleStatus observer.
    This simplifies the API, reduces redundancy, and aligns with standard
    observer patterns.
    
    Bug: 515551195
    Change-Id: Ie363a258f56f245fb6d278ae7bf5dc598f9779a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874068
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640635}

--- chrome/browser/preloading/prerender/prerender_manager.h
+++ chrome/browser/preloading/prerender/prerender_manager.h
@@ -53,6 +53,7 @@ enum class PrerenderPredictionStatus {
 // Chrome manages running prerenders separately, as it prioritizes the latest
 // prerender requests, while the //content prioritizes the earliest requests.
 class PrerenderManager : public content::WebContentsObserver,
+                         public content::PrerenderHandle::Observer,
                          public content::WebContentsUserData<PrerenderManager> {
  public:
   PrerenderManager(const PrerenderManager&) = delete;
@@ -149,6 +150,10 @@ class PrerenderManager : public content::WebContentsObserver,
       content::NavigationHandle& navigation_handle);
 
   void NotifySearchPrewarmFinished(content::PrerenderLifecycleStatus result);
+
+  // content::PrerenderHandle::Observer:
+  void OnLifecycleStateChanged(
+      content::PrerenderLifecycleStatus status) override;
 
   std::unique_ptr<content::PrerenderHandle> search_prewarm_handle_;
   bool is_search_prewarm_ongoing_ = false;
--- content/browser/preloading/prerender/prerender_handle_impl.h
+++ content/browser/preloading/prerender/prerender_handle_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_PRELOADING_PRERENDER_PRERENDER_HANDLE_IMPL_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "base/scoped_observation.h"
 #include "content/browser/preloading/prerender/prerender_host.h"
 #include "content/common/content_export.h"
@@ -38,13 +39,10 @@ class CONTENT_EXPORT PrerenderHandleImpl final
   base::WeakPtr<PrerenderHandle> GetWeakPtr() override;
   void SetPreloadingAttemptFailureReason(
       PreloadingFailureReason reason) override;
-  void AddActivationCallback(base::OnceClosure activation_callback) override;
-  void AddErrorCallback(base::OnceClosure error_callback) override;
+  void AddObserver(PrerenderHandle::Observer* observer) override;
+  void RemoveObserver(PrerenderHandle::Observer* observer) override;
   bool IsValid() const override;
   bool IsWaitingForResponseHeaders() const override;
-  void AddOnResponseHeadersReceivedCallback(
-      base::OnceCallback<void(PrerenderLifecycleStatus result)> callback)
-      override;
 
   // PrerenderHost::Observer:
   void OnActivated() override;
@@ -67,10 +65,7 @@ class CONTENT_EXPORT PrerenderHandleImpl final
   enum class State { kLoading, kReady, kActivated, kCanceled };
   State state_ = State::kLoading;
 
-  std::vector<base::OnceClosure> activation_callbacks_;
-  std::vector<base::OnceClosure> error_callbacks_;
-  std::vector<base::OnceCallback<void(PrerenderLifecycleStatus result)>>
-      on_headers_received_callbacks_;
+  base::ObserverList<PrerenderHandle::Observer> observers_;
 
   base::ScopedObservation<PrerenderHost, PrerenderHandleImpl> obs_{this};
 
