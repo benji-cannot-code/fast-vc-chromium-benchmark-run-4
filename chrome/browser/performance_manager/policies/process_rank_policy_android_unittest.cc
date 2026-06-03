9f7382e867057d56fc4ce37d17533ded92752378
Shin Kawamura <kawasin@google.com>
2026-06-03 16:14:37 -0700

    Update NonFocusedVisiblePage to use GTEST_SKIP for desktop.
    NonFocusedVisiblePage was broken by https://crrev.com/c/7849293.
    ChangeUnfocusedPriority feature is enabled by default on android desktop
    now.
    
    Bug: 518759092
    Change-Id: I24f0dcbd7d186469dc21af891fa6b03813196f6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894715
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641294}

--- chrome/browser/performance_manager/policies/process_rank_policy_android_unittest.cc
+++ chrome/browser/performance_manager/policies/process_rank_policy_android_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/android/android_info.h"
+#include "base/android/device_info.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
@@ -164,12 +165,11 @@ TEST_F(ProcessRankPolicyAndroidTest,
             content::ChildProcessImportance::MODERATE);
 }
 
-#if BUILDFLAG(IS_DESKTOP_ANDROID)
-#define MAYBE_NonFocusedVisiblePage DISABLED_NonFocusedVisiblePage
-#else
-#define MAYBE_NonFocusedVisiblePage NonFocusedVisiblePage
-#endif
-TEST_F(ProcessRankPolicyAndroidTest, MAYBE_NonFocusedVisiblePage) {
+TEST_F(ProcessRankPolicyAndroidTest, NonFocusedVisiblePage) {
+  if (base::android::device_info::is_desktop()) {
+    GTEST_SKIP()
+        << "ChangeUnfocusedPriority feature is always enabled on desktop";
+  }
   scoped_feature_list_.InitAndDisableFeature(
       chrome::android::kChangeUnfocusedPriority);
   graph_->PassToGraph(std::make_unique<ProcessRankPolicyAndroid>());
