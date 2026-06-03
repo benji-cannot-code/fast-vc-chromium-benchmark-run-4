0f16c3af38bd77dee3acaa9d89c626200cb5a282
Olivia Saul <osaul@google.com>
2026-06-03 16:25:25 -0700

    [AF] OAD avoids running checks again after candidate form found
    Fixed: 490214909
    Change-Id: I67f306a6318755026130becabbe75c27be197c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7886938
    Reviewed-by: Fernando Ramirez <ferny@google.com>
    Commit-Queue: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641301}

--- components/autofill/core/browser/ui/payments/omnibox_autofill_delegate.cc
+++ components/autofill/core/browser/ui/payments/omnibox_autofill_delegate.cc
@@ -38,6 +38,11 @@ void OmniboxAutofillDelegate::OnFieldTypesDetermined(
     FormGlobalId form_id,
     AutofillManager::Observer::FieldTypeSource source,
     bool small_forms_were_parsed) {
+  if (candidate_form_found_) {
+    // Candidate already found and awaiting user action asynchronously.
+    return;
+  }
+
   // Only run checks using the outermost AutofillManager to avoid having
   // multiple managers triggering the logic flow at once.
   if (!IsOutermostMainFrameActiveAutofillManager(manager)) {
@@ -128,10 +133,20 @@ void OmniboxAutofillDelegate::OnFieldTypesDetermined(
     }
   }
 
-  // More checks to follow as implementation continues...
-
+  // All checks passed! Log the triggering form and field, start the
+  // IntersectionObserver, and prevent this logic from running again.
   LogOmniboxAutofillShowChipDecisionPart1(
       OmniboxAutofillShowChipDecisionPart1::kSuccess);
+  trigger_form_global_id_ = form_structure->global_id();
+  for (const std::unique_ptr<AutofillField>& field : form_structure->fields()) {
+    if (field->Type().GetCreditCardType() == CREDIT_CARD_NUMBER) {
+      trigger_field_global_id_ = field->global_id();
+      break;
+    }
+  }
+  candidate_form_found_ = true;
+
+  // TODO: crbug.com/490214534 - Initiate GetIntersectionObserverInfo(~).
 }
 
 void OmniboxAutofillDelegate::OnAutofillManagerStateChanged(
@@ -151,6 +166,10 @@ void OmniboxAutofillDelegate::OnAfterFormsSeen(
     AutofillManager& manager,
     base::span<const FormGlobalId> updated_forms,
     base::span<const FormGlobalId> removed_forms) {
+  if (!candidate_form_found_) {
+    // Candidate form has not yet been found, so the chip is not being shown.
+    return;
+  }
   for (const FormGlobalId& id : removed_forms) {
     if (id == trigger_form_global_id_) {
       client_->GetPaymentsAutofillClient()->HideOmniboxAutofillChip();
