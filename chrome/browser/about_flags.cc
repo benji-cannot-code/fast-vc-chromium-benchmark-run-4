30ab0479b7ff36c96d7ec3407b928b8123f1917c
Aviv Kiss <avivkiss@google.com>
2026-06-02 20:27:48 -0700

    Add omnibox-loom entry to about:flags
    This CL adds an entry to chrome://flags for the omnibox-loom
    feature to allow users and developers to easily enable or disable the
    feature flag for testing.
    
    Note: This feature is a prototype behind a flag and is not intended
    to launch any time soon.
    
    BUG=515862210
    TAG=agy
    CONV=3d627a72-2c0e-4ad4-bf79-527872b706ce
    
    Change-Id: I4e0af14a234d9094da8892e5c2a4e47837d47ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872045
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640657}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -9356,6 +9356,9 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_WITH_PARAMS_VALUE_TYPE(omnibox::kDynamicAimSubmit,
                                     kOmniboxDynamicSubmitVariations,
                                     "OmniboxDynamicSubmitVariations")},
+    {"omnibox-loom", flag_descriptions::kOmniboxLoomName,
+     flag_descriptions::kOmniboxLoomDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(omnibox::kEverywhereOmnibox)},
 
     {"webui-omnibox-aim-popup", flag_descriptions::kWebUIOmniboxAimPopupName,
      flag_descriptions::kWebUIOmniboxAimPopupDescription, kOsDesktop,
