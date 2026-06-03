1a5c707feca24d7dcfa75e67485c6546a4517f51
Dominique Fauteux-Chapleau <domfc@chromium.org>
2026-06-03 07:45:33 -0700

    [gardener] Add include for discovery_network_info.h
    This is potentially a source of errors for the
    Windows deterministic builder. For example:
    
    https: //ci.chromium.org/ui/p/chromium/builders/ci/Windows%20deterministic/34553/overview
    Change-Id: Id355105c66063e603c2c87d95b28e2b391035022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895328
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Owners-Override: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640930}

--- chrome/browser/media/router/discovery/discovery_network_list_win.cc
+++ chrome/browser/media/router/discovery/discovery_network_list_win.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/scoped_hstring.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
+#include "chrome/browser/media/router/discovery/discovery_network_info.h"
 #include "chrome/browser/media/router/discovery/discovery_network_list.h"
 
 namespace WinrtConnectivity = ABI::Windows::Networking::Connectivity;
