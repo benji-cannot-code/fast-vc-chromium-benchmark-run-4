6912c6ddfdf20c41af15676ca1b0a0f4e87ba99e
Etienne Pierre-doray <etiennep@chromium.org>
2026-06-03 06:44:12 -0700

    [base] Implement CurrentTaskRunnerInheritsThreadType
    This CL implements a feature to conditionally override the current
    default task runner if lower priority than the default one, to
    avoid unintentional priority escalation.
    
    Bug: 495439030
    Change-Id: I0bf4b171f3273beb802716a42fa876d1b72dfd13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895182
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640891}

--- base/task/single_thread_task_runner.h
+++ base/task/single_thread_task_runner.h
@@ -21,6 +21,7 @@ class MainThreadSchedulerImpl;
 
 namespace base::sequence_manager::internal {
 class CurrentDefaultHandleOverrideForRunOrPostTask;
+class ThreadControllerWithMessagePumpImpl;
 }
 
 namespace base {
@@ -115,6 +116,8 @@ class BASE_EXPORT SingleThreadTaskRunner : public SequencedTaskRunner {
     friend class CurrentHandleOverrideForTesting;
     friend class sequence_manager::internal::
         CurrentDefaultHandleOverrideForRunOrPostTask;
+    friend class sequence_manager::internal::
+        ThreadControllerWithMessagePumpImpl;
     friend class ScopedMockTimeMessageLoopTaskRunner;
     friend class ScopedMockTimeMessageLoopTaskRunnerTest;
     FRIEND_TEST_ALL_PREFIXES(SingleThreadTaskRunnerCurrentDefaultHandleTest,
