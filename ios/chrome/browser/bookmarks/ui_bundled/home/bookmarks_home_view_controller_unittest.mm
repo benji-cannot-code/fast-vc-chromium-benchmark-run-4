5ba3044f97cb246bd495c5190209f731dcc034b3
Arthur Milchior <arthurmilchior@google.com>
2026-06-03 06:51:08 -0700

    [iOS]Remove a fake for test
    This was exposing uselessly internal implementation, and making
    migration from node to id complex.
    
    Bug: 505231370
    Change-Id: I854b76a5387e535925d2fa757713798fa40eb56d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883320
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640896}

--- ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller_unittest.mm
+++ ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller_unittest.mm
@@ -33,22 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                      (const bookmarks::BookmarkNode*)folder;
 @end
 
-// Fake implementation of BookmarksFolderChooserCoordinator for testing.
-@interface FakeBookmarksFolderChooserCoordinator
-    : BookmarksFolderChooserCoordinator
-@property(nonatomic, assign) std::set<raw_ptr<const bookmarks::BookmarkNode>>
-    movedNodesSet;
-@end
-
-@implementation FakeBookmarksFolderChooserCoordinator
-- (const std::set<raw_ptr<const bookmarks::BookmarkNode>>&)movedNodes {
-  return _movedNodesSet;
-}
-- (void)stop {
-  // Do nothing.
-}
-@end
-
 namespace {
 
 using BookmarksHomeViewControllerTest = BookmarkIOSUnitTestSupport;
@@ -289,20 +273,19 @@ TEST_F(BookmarksHomeViewControllerTest,
   std::set<raw_ptr<const bookmarks::BookmarkNode>> selectedNodes;
   selectedNodes.insert(bookmark);
 
-  // Use a fake folder chooser coordinator.
-  FakeBookmarksFolderChooserCoordinator* fakeFolderChooserCoordinator =
-      [[FakeBookmarksFolderChooserCoordinator alloc]
+  BookmarksFolderChooserCoordinator* folderChooserCoordinator =
+      [[BookmarksFolderChooserCoordinator alloc]
           initWithBaseViewController:nil
                              browser:browser_.get()
-                          movedNodes:{}];
-  fakeFolderChooserCoordinator.movedNodesSet = selectedNodes;
-  [controller setFolderChooserCoordinator:fakeFolderChooserCoordinator];
+                          movedNodes:selectedNodes];
+  [folderChooserCoordinator start];
+  [controller setFolderChooserCoordinator:folderChooserCoordinator];
 
   // Call the delegate method with the same parent folder.
   // This should result in MoveBookmarksWithUndoSnackbar returning nil,
   // and showSnackbarMessage: NOT being called.
   [controller
-      bookmarksFolderChooserCoordinatorDidConfirm:fakeFolderChooserCoordinator
+      bookmarksFolderChooserCoordinatorDidConfirm:folderChooserCoordinator
                                withSelectedFolder:mobileNode];
 
   [mockSnackbarHandler verify];
