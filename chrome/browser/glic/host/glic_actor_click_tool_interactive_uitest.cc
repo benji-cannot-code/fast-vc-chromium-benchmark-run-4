e3b7934db09d3c47ae9db7b6838e5494d14263c5
birnie <birnie@google.com>
2026-06-03 13:48:35 -0700

    Disable GlicActorUiTest.ClickActionInWebComponent on ChromeOS
    This test is flaky on ChromeOS builders. Conditionally disable it using
    the MAYBE\_ prefix under BUILDFLAG(IS_CHROMEOS) and add a TODO
    referencing the bug.
    
    Bug: b/497283367
    Change-Id: I5b57120cf559eabdfa7a5ae1b26ec1aa6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894033
    Commit-Queue: Nick Birnie <birnie@google.com>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641178}

--- chrome/browser/glic/host/glic_actor_click_tool_interactive_uitest.cc
+++ chrome/browser/glic/host/glic_actor_click_tool_interactive_uitest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/browser/actor/actor_test_util.h"
 #include "chrome/browser/glic/host/glic_actor_interactive_uitest_common.h"
 #include "components/optimization_guide/proto/features/actions_data.pb.h"
@@ -63,7 +64,13 @@ IN_PROC_BROWSER_TEST_F(GlicActorUiTest, ClickActionWithCoordinatesSucceeds) {
 
 // A click on a button in a web component should work, but a click on another
 // element in the component should not.
-IN_PROC_BROWSER_TEST_F(GlicActorUiTest, ClickActionInWebComponent) {
+// TODO(b/497283367): Flaky on ChromeOS.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_ClickActionInWebComponent DISABLED_ClickActionInWebComponent
+#else
+#define MAYBE_ClickActionInWebComponent ClickActionInWebComponent
+#endif
+IN_PROC_BROWSER_TEST_F(GlicActorUiTest, MAYBE_ClickActionInWebComponent) {
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kNewActorTabId);
   const GURL task_url = embedded_test_server()->GetURL(
       "/actor/page_with_web_component_button.html");
