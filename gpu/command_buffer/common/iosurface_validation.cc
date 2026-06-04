a47db7c53adea2060cb26c37aecbb46753e6fbde
Gyuyoung Kim <gyuyoung@igalia.com>
2026-06-04 09:18:39 -0700

    [iOS Blink] Include IOSurfaceRef.h instead of IOSurface.h in iosurface_validation.cc
    https://crrev.com/c/7882552 added IOSurfaceValidation, which included
    <IOSurface/IOSurface.h>. That header is not available on iOS, so this CL
    switches to <IOSurface/IOSurfaceRef.h>, which is supported on iOS.
    
    Bug: 497531791
    Change-Id: Idbaa3dea9b28e8c7d11055cc904597ad9c2b2c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899038
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641705}

--- gpu/command_buffer/common/iosurface_validation.cc
+++ gpu/command_buffer/common/iosurface_validation.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/iosurface_validation.h"
 
 #include <CoreVideo/CoreVideo.h>
-#include <IOSurface/IOSurface.h>
+#include <IOSurface/IOSurfaceRef.h>
 
 namespace gpu {
 
