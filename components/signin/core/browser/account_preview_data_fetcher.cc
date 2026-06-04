b1ddbdeffa5bdeaa704870324dce855c882f7c95
Ryan Sultanem <rsult@google.com>
2026-06-04 06:58:41 -0700

    [SyncPreview] Allow empty valid result
    Not having any data to return should still be considered as a success.
    When there is no data, the server may entirely omit the
    `dataTypeStatistics` and `entitiesPreviews ` list.
    
    Fixed: 519917780
    Change-Id: I955a91fb3b6edbe352703a989a414a95ef4d9f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902313
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641614}

--- components/signin/core/browser/account_preview_data_fetcher.cc
+++ components/signin/core/browser/account_preview_data_fetcher.cc
@@ -69,7 +69,8 @@ std::optional<AccountPreviewData> ParseStatsResponse(
   const auto& dict = value->GetDict();
   const auto* list = dict.FindList("dataTypeStatistics");
   if (!list) {
-    return std::nullopt;
+    // An empty valid result is still considered a success.
+    return data;
   }
   for (const auto& item : *list) {
     if (!item.is_dict()) {
@@ -117,7 +118,8 @@ std::optional<AccountPreviewData> ParsePreviewsResponse(
   const auto& dict = value->GetDict();
   const auto* list = dict.FindList("entitiesPreviews");
   if (!list) {
-    return std::nullopt;
+    // An empty valid result is still considered a success.
+    return data;
   }
   data->password_domains.clear();
   for (const auto& item : *list) {
