f5ec1e79c7a618f0e1a0a2cc77edde1e59531044
Andres Ricardo Perez <andresrperez@chromium.org>
2026-06-04 13:05:48 -0700

    [Memory Coordinator] Add traits to MemBackendImpl
    Updates MemBackendImpl to explicitly define its memory coordination
    semantics using MemoryConsumerTraits. This provides the global Memory
    Coordinator with accurate information regarding the
    scale, eviction cost, and statefulness of the cache to optimize
    system-wide memory reclamation decisions.
    
    Bug: 489671163
    Change-Id: I588522abbebe9547742c84d0281bac1eda355850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841378
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641881}

--- net/disk_cache/memory/mem_backend_impl.cc
+++ net/disk_cache/memory/mem_backend_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory_coordinator/memory_consumer.h"
 #include "base/memory_coordinator/memory_coordinator_features.h"
+#include "base/memory_coordinator/traits.h"
 #include "base/memory_coordinator/utils.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/system/sys_info.h"
@@ -67,6 +68,16 @@ base::LinkNode<MemEntryImpl>* NextSkippingChildren(
   return node;
 }
 
+constexpr base::MemoryConsumerTraits kMemBackendImplTraits(
+    // Scales with system RAM up to a cap in the tens of MBs.
+    base::MemoryConsumerTraits::EstimatedMemoryUsage::kMedium,
+    // Eviction traverses linked list and erases map entries.
+    base::MemoryConsumerTraits::ReleaseMemoryCost::kRequiresTraversal,
+    // Evicted entries can be re-fetched from network.
+    base::MemoryConsumerTraits::InformationRetention::kLossless,
+    // Eviction runs inline synchronously.
+    base::MemoryConsumerTraits::ExecutionType::kSynchronous);
+
 }  // namespace
 
 MemBackendImpl::MemBackendImpl(net::NetLog* net_log)
@@ -74,7 +85,7 @@ MemBackendImpl::MemBackendImpl(net::NetLog* net_log)
       net_log_(net_log),
       memory_consumer_registration_(
           "MemBackendImpl",
-          std::nullopt,  // TODO(crbug.com/489671163): Add traits.
+          kMemBackendImplTraits,
           this,
           base::AsyncMemoryConsumerRegistration::CheckUnregister::kDisabled,
           base::AsyncMemoryConsumerRegistration::CheckRegistryExists::
