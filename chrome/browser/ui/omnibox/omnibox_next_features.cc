4ff12d8fa0b8c11fb1b1d5f8747eff2c8b3f5dd3
Ryan Akiyama <ryanakiyama@chromium.org>
2026-06-04 08:49:48 -0700

    [omnibox][composebox] Enable Omnibox Composebox fork flag
    Enables the `Omnibox_UseComposeboxFork` flag.
    
    Bug: b:517631958
    Change-Id: Ifee07174f0bfd2f5d8f169a54aa2eb03a8162bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899788
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Ryan Akiyama <ryanakiyama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641674}

--- chrome/browser/ui/omnibox/omnibox_next_features.cc
+++ chrome/browser/ui/omnibox/omnibox_next_features.cc
@@ -359,7 +359,7 @@ const base::FeatureParam<bool> kShowContextMenuHeaders(
 const base::FeatureParam<bool> kUseComposeboxFork(
     &internal::kWebUIOmniboxAimPopup,
     "Omnibox_UseComposeboxFork",
-    false);
+    true);
 const base::FeatureParam<bool> kContextButtonHasBackground{
     &internal::kWebUIOmniboxSimplification,
     "Omnibox_ContextButtonHasBackground", false};
