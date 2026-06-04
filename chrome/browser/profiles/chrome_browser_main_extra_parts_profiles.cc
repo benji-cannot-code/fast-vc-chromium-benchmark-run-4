694dc1b782c48f2cb25873690cdeb500ce9f1760
Brandon Wylie <wylieb@google.com>
2026-06-04 16:10:56 -0700

    [finds] Remove the custom prompt/instructions fields and cleanup code
    Bug: 517631050
    Change-Id: I1388779b40fc69a5ef91699d051b2f5454cb588a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7884716
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642007}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -263,7 +263,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model_factory.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model_factory.h"
 #include "chrome/browser/ui/ui_features.h"
-#include "chrome/browser/ui/webui/chrome_finds_internals/chrome_finds_agent_factory.h"
 #include "chrome/browser/ui/webui/ntp/ntp_resource_cache_factory.h"
 #include "chrome/browser/ui/webui/signin/history_sync_optin_service_factory.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service_factory.h"
@@ -735,7 +734,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   AffiliationServiceFactory::GetInstance();
   actor_login::ActorLoginPermissionCleaningServiceFactory::GetInstance();
   actor_login::ActorLoginPermissionServiceFactory::GetInstance();
-  chrome_finds_internals::ChromeFindsAgentFactory::GetInstance();
   AiDataKeyedServiceFactory::GetInstance();
   AimEligibilityServiceFactory::GetInstance();
 #if BUILDFLAG(IS_ANDROID)
