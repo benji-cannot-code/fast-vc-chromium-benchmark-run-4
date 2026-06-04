5a1a4607480f1dde0631233de39e8b3e05de3363
Chris Lu <thegreenfrog@google.com>
2026-06-04 09:58:49 -0700

    Fix SafeBrowsingTestCase for kChromeNextIA
    Adds logic in -openToolsMenu to dismiss infobars since the tools menu
    is a button on the top left when showing the NTP.
    
    Bug: 514608938
    Change-Id: I240257982e040df80cdfd338d0aaa1502ca8823e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878660
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641744}

--- ios/chrome/test/earl_grey/chrome_earl_grey_ui.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_ui.mm
@@ -144,6 +144,21 @@ const int kMaxNumberOfAttemptsAtTypingTextInOmnibox = 3;
 }
 
 - (void)openToolsMenu {
+  if ([ChromeEarlGrey isChromeNextEnabled]) {
+    // Dismiss infobar banner if present, as it might cover the tools menu
+    // button.
+    NSError* error = nil;
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                            kInfobarBannerViewIdentifier)]
+        assertWithMatcher:grey_sufficientlyVisible()
+                    error:&error];
+    if (!error) {
+      [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                              kInfobarBannerViewIdentifier)]
+          performAction:grey_swipeFastInDirection(kGREYDirectionUp)];
+    }
+  }
+
   // TODO(crbug.com/41271107): Add logic to ensure the app is in the correct
   // state, for example DCHECK if no tabs are displayed.
   if ([ChromeEarlGrey isChromeNextEnabled] && ![ChromeEarlGrey isIPadIdiom] &&
@@ -200,18 +215,6 @@ const int kMaxNumberOfAttemptsAtTypingTextInOmnibox = 3;
 }
 
 - (void)openSettingsMenu {
-  // Dismiss infobar banner if present, as it might cover the tools menu button.
-  NSError* error = nil;
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
-                                          kInfobarBannerViewIdentifier)]
-      assertWithMatcher:grey_sufficientlyVisible()
-                  error:&error];
-  if (!error) {
-    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
-                                            kInfobarBannerViewIdentifier)]
-        performAction:grey_swipeFastInDirection(kGREYDirectionUp)];
-  }
-
   [self openToolsMenu];
   [self tapToolsMenuButton:SettingsDestinationButton()];
 }
