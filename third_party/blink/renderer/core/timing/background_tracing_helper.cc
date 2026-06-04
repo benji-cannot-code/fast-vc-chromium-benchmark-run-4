8d18d034500531f37c9ee99c9167c2e6e025e246
Fredrik Söderquist <fs@opera.com>
2026-06-04 16:47:07 -0700

    Return std::optional<...> from *CharactersTo*Int*
    Convert the following to return a std::optional<*int*> instead of a
    value and an "ok" boolean out parameter:
    
     * CharactersToInt
     * CharactersToUInt
     * HexCharactersToUInt
     * CharactersToInt64
     * CharactersToUInt64
     * HexCharactersToUInt64
    
    Reorder the template arguments to the ToIntegralType() helper to allow
    the character type to be inferred and thus make the callsites simpler.
    
    float version is updated as well for uniformity.
    
    UpdateDescriptorToken: :To{Int,Float} to follow a similar scheme. The
    Change-Id: I087ef1e9cb4f5a6d44155d15b8ef3bcc4b26f54a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899059
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642028}

--- third_party/blink/renderer/core/timing/background_tracing_helper.cc
+++ third_party/blink/renderer/core/timing/background_tracing_helper.cc
@@ -57,7 +57,7 @@ std::optional<uint64_t> ConvertToHashInteger(std::string_view chars) {
     }
   }
   return HexCharactersToUInt64(base::as_byte_span(chars),
-                               NumberParsingOptions(), nullptr);
+                               NumberParsingOptions());
 }
 
 static constexpr char kTriggerPrefix[] = "trigger:";
