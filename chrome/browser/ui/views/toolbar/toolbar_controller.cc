5d03f114d2702c9b57bd051f076c447ee958e833
Matt Menke <mmenke@chromium.org>
2026-06-04 07:38:29 -0700

    Fix user education tests for the WebUIToolbar.
    The tests add dummy buttons to the toolbar until the forward button
    is hidden due to overflow. These tests are explicitly checking the
    Views forward button's visibility. When using a WebUI forward button,
    however, there is no Views forward button. We instead need to check the
    visibility of the WebUI forward button instead.
    
    The approach this CL takes is to expose a method on ToolbarController
    that checks if an element is overflowed, that works for both Views and
    WebUI elements. Method is only exposed for testing.
    
    Note that once the promo logic is updated to support the WebUI toolbar,
    we'll probably have to update how things work again (and potentially
    again when overflow is handled purely by WebUI code, rather than
    sharing overflow logic with the Views path).
    
    Other options considered:
    1) Call into Javascript to repeatedly check if the forward button is
    visible. This potentially races with the message to actually hide the
    views button, so is not viable.
    
    2) Making the views and WebUI forward control share a parent class,
    and then make the existing ToolbarView::forward_button(), which only
    returns the Views class currently (and looks to be unused) return
    that new class, which would have a way to check its visibility.
    This works, but requires a new intermediary class, which seemed a
    bit much.
    
    3) Add a forward button visibility check method check to ToolbarView
    that knows how to call into the right button. This seems a bit too
    specific.
    
    Bug: 508330977, 508257575
    Change-Id: I5912b7baa9876c1bf08d2cc05fd6b08e7852d475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893292
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641639}

--- chrome/browser/ui/views/toolbar/toolbar_controller.cc
+++ chrome/browser/ui/views/toolbar/toolbar_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/no_destructor.h"
+#include "base/notreached.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -599,8 +600,22 @@ views::View* ToolbarController::FindToolbarElementWithId(
   return nullptr;
 }
 
+bool ToolbarController::IsElementOverflowedForTesting(
+    ui::ElementIdentifier id) const {
+  for (const auto& responsive_element : responsive_elements_) {
+    const auto* element_id_info = std::get_if<ToolbarController::ElementIdInfo>(
+        &responsive_element.overflow_id);
+    if (!element_id_info || element_id_info->overflow_identifier != id) {
+      continue;
+    }
+    return IsOverflowed(responsive_element);
+  }
+  // Element cannot overflow, since it is not in `responsive_elements_`
+  NOTREACHED();
+}
+
 std::vector<const ToolbarController::ResponsiveElementInfo*>
-ToolbarController::GetOverflowedElements() {
+ToolbarController::GetOverflowedElements() const {
   std::vector<const ToolbarController::ResponsiveElementInfo*>
       overflowed_buttons;
   if (ToolbarControllerUtil::PreventOverflow()) {
