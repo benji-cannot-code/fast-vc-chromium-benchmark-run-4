c0dfef2868338f73c8dd9225185a6cb0ac619f22
dljames <dljames@chromium.org>
2026-06-04 09:09:36 -0700

    [MenuSimplification] Add Menu Item to Exit Fullscreen Mode
    Updates RenderViewContextMenu to add the missing menu item used to
    exit fullscreen mode. This menu item should be added as the first menu
    item in its own section so uses can easily exit the mode.
    
    Bug: 519737778
    Change-Id: Ibbc4b75a9bb17ae9e00cf317f141ba2cff01551e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900231
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641695}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -2439,6 +2439,8 @@ void RenderViewContextMenu::AppendPluginItems() {
 }
 
 void RenderViewContextMenu::AppendPageItems() {
+  AppendExitFullscreenItem();
+
   if (features::IsMenuSimplificationEnabled() &&
       params_.selection_text.empty() && !params_.is_editable) {
     // Navigation
@@ -2495,8 +2497,6 @@ void RenderViewContextMenu::AppendPageItems() {
     menu_model_.AddSeparator(ui::NORMAL_SEPARATOR);
     return;
   }
-
-  AppendExitFullscreenItem();
 
   const bool use_simplified_text_selection = ShouldUseSimplifiedTextSelection();
 
