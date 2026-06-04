adb5b648a5855fed1e12997a7cb5b2756f3a7769
Daniel Rubery <drubery@chromium.org>
2026-06-04 08:37:20 -0700

    [bedrock] Start of BrowserCommandService
    This CL bundles the various operations from browser_commands.h into a
    BrowserCommands service. Changes include:
    - Adding new declarations for BrowserCommands
    - Remove `browser` arguments from BrowserCommands service
    - Replace implementations in browser_commands.cc with those in the
      service.
    
    Followup CLs will clean up the callers of browser_commands.h, but that
    will need to happen in smaller batches due to the number of files
    involved.
    
    Bug: 514547038
    Change-Id: I8f7795f3d47dab449f5410e4faaae3c96a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865340
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641667}

--- chrome/browser/ui/browser_window/public/browser_window_features.h
+++ chrome/browser/ui/browser_window/public/browser_window_features.h
@@ -168,6 +168,7 @@ class VerticalTabStripStateController;
 
 namespace chrome {
 class BrowserCommandController;
+class BrowserCommands;
 }  // namespace chrome
 
 namespace contextual_cueing {
@@ -538,6 +539,8 @@ class BrowserWindowFeatures {
   std::unique_ptr<WindowFeatureController> window_feature_controller_;
 
   std::unique_ptr<BrowserActions> browser_actions_;
+
+  std::unique_ptr<chrome::BrowserCommands> browser_commands_;
 
   std::unique_ptr<chrome::BrowserCommandController> browser_command_controller_;
 
