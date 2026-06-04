0c7c3dd89083acdd0052c94d629640c758650842
Kirubel <kaklilu@chromium.org>
2026-06-04 13:27:27 -0700

    Remove ParseJavaScriptResult from actor_tool_java_script_feature_util.h
    This was replaced by ParseJavaScriptResultWithResultCode to facilitate
    syncing error codes between TS and C++.
    
    There is no remaining usage and this can be cleaned up:
    https://source.chromium.org/search?q=%22ParseJavaScriptResult%22%20-%22ParseJavaScriptResultWithResultCode%22&sq=&ss=chromium%2Fchromium%2Fsrc:ios%2Fchrome%2Fbrowser%2Fintelligence%2Factor%2Ftools%2F
    
    Bug: b:505037793
    Change-Id: I4f33586411d5566b8e9b7997f64ab5dc6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901808
    Commit-Queue: Kirubel <kaklilu@chromium.org>
    Auto-Submit: Kirubel <kaklilu@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641897}

--- ios/chrome/browser/intelligence/actor/tools/model/actor_tool_java_script_feature_util.h
+++ ios/chrome/browser/intelligence/actor/tools/model/actor_tool_java_script_feature_util.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_TOOLS_MODEL_ACTOR_TOOL_JAVA_SCRIPT_FEATURE_UTIL_H_
 #define IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_TOOLS_MODEL_ACTOR_TOOL_JAVA_SCRIPT_FEATURE_UTIL_H_
 
-#include "base/functional/function_ref.h"
+#import "base/functional/function_ref.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool.h"
 #import "ios/chrome/browser/intelligence/actor/tools/public/actor_tool_types.h"
 
@@ -15,12 +15,6 @@ class Value;
 }  // namespace base
 
 namespace actor {
-
-// Processes a {success: bool, message: string} JS result.
-// TODO(crbug.com/505037793) - remove this once web tools all plumb an error
-// code.
-void ParseJavaScriptResult(ToolExecutionCallback callback,
-                           const base::Value* result);
 
 // Parses a ToolExecutionResult from a JS object that contains a `resultCode`
 // number and `message`. The resultCode is translated to a C++ error code with
