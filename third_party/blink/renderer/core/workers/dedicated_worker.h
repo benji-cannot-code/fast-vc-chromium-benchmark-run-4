c2a77a5a37777b1048848990becf089e8f4e73e6
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-06-04 16:31:32 -0700

    Remove DedicatedWorker::classic_script_loader_
    Always null after https://crrev.com/c/6420185.
    
    Bug: 40572995, 400455021
    Change-Id: Idb10ee25a3a041aecbfdd3e2b9726922adbaadd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872631
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642019}

--- third_party/blink/renderer/core/workers/dedicated_worker.h
+++ third_party/blink/renderer/core/workers/dedicated_worker.h
@@ -48,7 +48,6 @@ class PostMessageOptions;
 class ScriptState;
 class V8UnionTrustedScriptURLOrUSVString;
 class WebContentSettingsClient;
-class WorkerClassicScriptLoader;
 struct GlobalScopeCreationParams;
 
 // Implementation of the Worker interface defined in the WebWorker HTML spec:
@@ -196,14 +195,6 @@ class CORE_EXPORT DedicatedWorker final
   // May return nullptr.
   std::unique_ptr<WebContentSettingsClient> CreateWebContentSettingsClient();
 
-  // Callbacks for |classic_script_loader_|.
-  // TODO(crbug.com/400455021): Investigate whether these can be removed now
-  // that PlzDedicatedWorker has shipped.
-  void OnResponse();
-  void OnFinished(
-      mojo::PendingRemote<mojom::blink::BackForwardCacheControllerHost>
-          back_forward_cache_controller_host);
-
   // Implements EventTarget (via AbstractWorker -> EventTarget).
   const AtomicString& InterfaceName() const final;
 
@@ -217,8 +208,6 @@ class CORE_EXPORT DedicatedWorker final
   Member<const FetchClientSettingsObjectSnapshot>
       outside_fetch_client_settings_object_;
   const Member<DedicatedWorkerMessagingProxy> context_proxy_;
-
-  Member<WorkerClassicScriptLoader> classic_script_loader_;
 
   std::unique_ptr<WebDedicatedWorkerHostFactoryClient> factory_client_;
 
