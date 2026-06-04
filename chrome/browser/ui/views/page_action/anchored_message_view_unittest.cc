20d502f6e3af17a06fa525d5bce817d8acaa4307
Dmytro Yeroshkin <yeroshkin@chromium.org>
2026-06-04 08:08:08 -0700

    [clarity] Make the anchored message buttons focusable
    * Make menu and close buttons focusable for accessibility - this also provides screen reader and focus ring functionality
    * Migrate anchored message unit tests from CreateBubbleDeprecated to CreateBubble
    
    Change-Id: I6204a0b382947db8bedef6fcc404a5aa5152ebf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894030
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Dmytro Yeroshkin <yeroshkin@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641650}

--- chrome/browser/ui/views/page_action/anchored_message_view_unittest.cc
+++ chrome/browser/ui/views/page_action/anchored_message_view_unittest.cc
@@ -104,8 +104,7 @@ TEST_F(AnchoredMessageBubbleViewTest, VisibilityReflectsModelOnCreation) {
       .WillByDefault(Return(AnchoredMessageActionIconType::kClose));
 
   auto view = CreateView();
-  auto* widget = views::BubbleDialogDelegate::CreateBubbleDeprecated(
-      std::move(view), views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
   widget->Show();
 
   RunTestSequence(
@@ -135,8 +134,7 @@ TEST_F(AnchoredMessageBubbleViewTest,
       .WillByDefault(Return(AnchoredMessageActionIconType::kClose));
 
   auto view = CreateView();
-  auto* widget = views::BubbleDialogDelegate::CreateBubbleDeprecated(
-      std::move(view), views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
   widget->Show();
 
   RunTestSequence(
@@ -169,8 +167,7 @@ TEST_F(AnchoredMessageBubbleViewTest,
       .WillByDefault(Return(&menu_model));
 
   auto view = CreateView();
-  auto* widget = views::BubbleDialogDelegate::CreateBubbleDeprecated(
-      std::move(view), views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
   widget->Show();
 
   RunTestSequence(
@@ -194,8 +191,7 @@ TEST_F(AnchoredMessageBubbleViewTest,
       .WillByDefault(Return(AnchoredMessageActionIconType::kMenu));
 
   auto view = CreateView();
-  auto* widget = views::BubbleDialogDelegate::CreateBubbleDeprecated(
-      std::move(view), views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
   widget->Show();
 
   RunTestSequence(
@@ -218,8 +214,7 @@ TEST_F(AnchoredMessageBubbleViewTest, UpdateContentChangesVisibility_ChipOnly) {
   ON_CALL(model_, GetImage()).WillByDefault(ReturnRef(test_image_));
 
   auto view = CreateView();
-  auto* widget = views::BubbleDialogDelegate::CreateBubbleDeprecated(
-      std::move(view), views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
   widget->Show();
 
   RunTestSequence(
@@ -233,6 +228,45 @@ TEST_F(AnchoredMessageBubbleViewTest, UpdateContentChangesVisibility_ChipOnly) {
                 }),
       EnsureNotPresent(AnchoredMessageBubbleView::kAnchoredMessageCloseIconId),
       EnsureNotPresent(AnchoredMessageBubbleView::kAnchoredMessageMenuIconId));
+
+  widget->CloseNow();
+}
+
+TEST_F(AnchoredMessageBubbleViewTest, CloseButtonIsFocusable) {
+  ON_CALL(model_, GetAnchoredMessageText())
+      .WillByDefault(ReturnRef(test_text_));
+  ON_CALL(model_, GetAnchoredMessageActionIconType())
+      .WillByDefault(Return(AnchoredMessageActionIconType::kClose));
+
+  auto view = CreateView();
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
+  widget->Show();
+
+  RunTestSequence(
+      EnsurePresent(AnchoredMessageBubbleView::kAnchoredMessageCloseIconId),
+      CheckView(AnchoredMessageBubbleView::kAnchoredMessageCloseIconId,
+                [](views::View* button) { return button->IsFocusable(); }));
+
+  widget->CloseNow();
+}
+
+TEST_F(AnchoredMessageBubbleViewTest, MenuButtonIsFocusable) {
+  ui::SimpleMenuModel menu_model(nullptr);
+  ON_CALL(model_, GetAnchoredMessageText())
+      .WillByDefault(ReturnRef(test_text_));
+  ON_CALL(model_, GetAnchoredMessageActionIconType())
+      .WillByDefault(Return(AnchoredMessageActionIconType::kMenu));
+  ON_CALL(model_, GetAnchoredMessageMenuModel())
+      .WillByDefault(Return(&menu_model));
+
+  auto view = CreateView();
+  auto widget = views::BubbleDialogDelegate::CreateBubble(view.release());
+  widget->Show();
+
+  RunTestSequence(
+      EnsurePresent(AnchoredMessageBubbleView::kAnchoredMessageMenuIconId),
+      CheckView(AnchoredMessageBubbleView::kAnchoredMessageMenuIconId,
+                [](views::View* button) { return button->IsFocusable(); }));
 
   widget->CloseNow();
 }
