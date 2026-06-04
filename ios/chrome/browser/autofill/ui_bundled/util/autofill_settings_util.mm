e705a2ae54760673f04ae1398216567682497f51
YiwenQian <yiwenqian@google.com>
2026-06-04 12:40:45 -0700

    [ScanCard IOS] Update Strings for scan card flow
    Mock & Implementation:
    https://screenshot.googleplex.com/Ad55CM6AP9o3fri,
    https://screenshot.googleplex.com/4NNiznM4K7bcrTh
    
    Bug: 519324725
    Change-Id: I6db1b873fa1694b46832b5d4c4588c68a584a2c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900597
    Commit-Queue: yiwen qian <yiwenqian@chromium.org>
    Reviewed-by: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641866}

--- ios/chrome/browser/autofill/ui_bundled/util/autofill_settings_util.mm
+++ ios/chrome/browser/autofill/ui_bundled/util/autofill_settings_util.mm
@@ -28,8 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // gone) and manually appends the placeholder back to the accessibility label
   // if it matches one of the essential types.
   if (item.textFieldValue.length > 0) {
-    NSString* optionalPlaceholder = l10n_util::GetNSString(
-        IDS_IOS_AUTOFILL_DIALOG_PLACEHOLDER_CVC_OPTIONAL);
+    NSString* optionalPlaceholder =
+        l10n_util::GetNSString(IDS_IOS_AUTOFILL_DIALOG_PLACEHOLDER_OPTIONAL);
     NSString* monthPlaceholder = l10n_util::GetNSString(
         IDS_IOS_AUTOFILL_DIALOG_PLACEHOLDER_EXPIRY_MONTH);
     NSString* yearPlaceholder = l10n_util::GetNSString(
