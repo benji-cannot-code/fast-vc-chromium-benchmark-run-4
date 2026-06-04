5d685a40c8af834e65ecbc35be9b945ccf2bb381
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-06-04 06:29:19 -0700

    Move placeholder checks to SetPlaceholderCanvasDispatcher
    This is no-op and will allow to move call-sites inside.
    
    Change-Id: I0b4dcbdbaa671addd96f98a084d6b55a9bc6b97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893335
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641606}

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.cc
@@ -111,10 +111,6 @@ void UpdatePlaceholderDispatcher(
     base::WeakPtr<CanvasResourceDispatcher> dispatcher,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     DOMNodeId placeholder_canvas_id) {
-  if (placeholder_canvas_id == OffscreenCanvasPlaceholder::kNoPlaceholderId ||
-      placeholder_canvas_id == kInvalidDOMNodeId) {
-    return;
-  }
   OffscreenCanvasPlaceholder* placeholder_canvas =
       OffscreenCanvasPlaceholder::GetPlaceholderCanvasById(
           placeholder_canvas_id);
@@ -445,6 +441,11 @@ void CanvasResourceDispatcher::SetPlaceholderCanvasDispatcher(
   // was created from a SharedWorker.
   if (!agent_group_scheduler_compositor_task_runner_)
     return;
+
+  if (placeholder_canvas_id == OffscreenCanvasPlaceholder::kNoPlaceholderId ||
+      placeholder_canvas_id == kInvalidDOMNodeId) {
+    return;
+  }
 
   // If the offscreencanvas is in the same thread as the canvas, we will update
   // the canvas resource dispatcher directly. So Offscreen Canvas can behave in
