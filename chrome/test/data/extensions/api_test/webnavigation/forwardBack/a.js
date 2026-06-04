5f565d5c667600e8aeed4697bc4eaf2340466e04
Andrea Orru <andreaorru@chromium.org>
2026-06-04 08:05:04 -0700

    [Extensions] Fix flaky WebNavigationApiTest.ForwardBack
    The test's a.js uses sessionStorage to guard against re-navigating to
    b.html on back-navigation. However, sessionStorage stores all values as
    strings, and the guard used strict inequality against a number:
    
    ```js
    window.sessionStorage['redirected'] !== 1  // ("1" !== 1) is always true
    ```
    
    This meant the guard never worked: every time a.html loaded, it would
    start a new navigation to b.html. The test passed most of the time only
    because chrome.test.succeed() was called before the extra navigation's
    onCommitted event arrived. On slower builds (e.g. ASan), the race was
    lost frequently (>50% flake rate).
    
    Fixed: 512069713
    Change-Id: Ifc517410f7f7f5f2806504184215d5624d406c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899880
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641646}

--- chrome/test/data/extensions/api_test/webnavigation/forwardBack/a.js
+++ chrome/test/data/extensions/api_test/webnavigation/forwardBack/a.js
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 onload = function() {
-  if (window.sessionStorage['redirected'] !== 1) {
-    window.sessionStorage['redirected'] = 1;
+  if (window.sessionStorage['redirected'] !== '1') {
+    window.sessionStorage['redirected'] = '1';
     // Required so this results in a history entry being created.
     window.setTimeout(function() {
       document.location = 'b.html';
