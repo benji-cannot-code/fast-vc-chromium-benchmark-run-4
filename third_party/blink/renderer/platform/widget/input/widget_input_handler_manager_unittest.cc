6a385564e0ab368e1a71f3e0280a6def77cff38c
Jonathan Ross <jonross@chromium.org>
2026-06-04 13:52:31 -0700

    Remove kIgnoreInputWhileHidden feature
    This feature was introduced to ignore input events when the renderer is hidden. However, it was found to break accessibility tools on Windows which may mark the renderer as hidden even when it is active (crbug.com/407265465). The feature was subsequently disabled by default.
    
    We have validated that hidden input is similarly used on other platforms. So
    this feature is not correct.
    
    Change-Id: I07df18f853355d8eb25d2cb9c1aeb5d37c987794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903099
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641916}

--- third_party/blink/renderer/platform/widget/input/widget_input_handler_manager_unittest.cc
+++ third_party/blink/renderer/platform/widget/input/widget_input_handler_manager_unittest.cc
@@ -41,13 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink ::test {
 
-class WidgetInputHandlerManagerTest : public testing::Test,
-                                      public testing::WithParamInterface<bool> {
+class WidgetInputHandlerManagerTest : public testing::Test {
  public:
-  WidgetInputHandlerManagerTest();
+  WidgetInputHandlerManagerTest() = default;
   ~WidgetInputHandlerManagerTest() override = default;
-
-  bool IgnoreInputWhileHidden() const { return GetParam(); }
 
   // Generates and empty touch start, and invokes `DispatchEvent`. Will validate
   // that `HandleInputEventWithLatencyInfo` is called `expected_times_called`.
@@ -80,19 +77,7 @@ class WidgetInputHandlerManagerTest : public testing::Test,
   testing::StrictMock<cc::MockInputHandler> mock_input_handler_;
   raw_ptr<MockInputHandlerProxy> input_handler_proxy_;
   testing::StrictMock<MockInputHandlerProxyClient> mock_client_;
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
-
-WidgetInputHandlerManagerTest::WidgetInputHandlerManagerTest() {
-  if (IgnoreInputWhileHidden()) {
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kIgnoreInputWhileHidden);
-  } else {
-    scoped_feature_list_.InitAndDisableFeature(
-        features::kIgnoreInputWhileHidden);
-  }
-}
 
 void WidgetInputHandlerManagerTest::DispatchTouchEvent(
     int expected_times_called,
@@ -172,62 +157,7 @@ void WidgetInputHandlerManagerTest::SetUp() {
       std::move(unique_input_handler_proxy));
 }
 
-// Tests that while we are hidden, that input is neither dispatched nor
-// consumed. Becoming visible should remove this suppression.
-TEST_P(WidgetInputHandlerManagerTest, InputWhileHidden) {
-  auto manager = widget_input_handler_manager();
-  EXPECT_EQ(
-      manager->suppressing_input_events_state(),
-      static_cast<uint16_t>(WidgetInputHandlerManager::
-                                SuppressingInputEventsBits::kHasNotPainted));
-  manager->OnFirstContentfulPaint();
-  EXPECT_EQ(manager->suppressing_input_events_state(), 0u);
-
-  manager->SetHidden(true);
-  EXPECT_EQ(
-      manager->suppressing_input_events_state(),
-      static_cast<uint16_t>(
-          WidgetInputHandlerManager::SuppressingInputEventsBits::kHidden));
-
-  if (GetParam()) {
-    ExpectNotConsumedDispatchEvent();
-  }
-
-  manager->SetHidden(false);
-  EXPECT_EQ(manager->suppressing_input_events_state(), 0u);
-  DispatchTouchEvent(/*expected_times_called=*/1,
-                     mojom::blink::WidgetInputHandler::DispatchEventCallback());
-}
-
-// Tests that while we are hidden, and attached DevTools sessions witll override
-// the input suppression. Events should be dispatched. Upon the session
-// detaching we should resume neither dispatching nor consuming events.
-TEST_P(WidgetInputHandlerManagerTest, DevToolsSessionOverridesSuppression) {
-  auto manager = widget_input_handler_manager();
-  EXPECT_EQ(
-      manager->suppressing_input_events_state(),
-      static_cast<uint16_t>(WidgetInputHandlerManager::
-                                SuppressingInputEventsBits::kHasNotPainted));
-  manager->OnFirstContentfulPaint();
-  EXPECT_EQ(manager->suppressing_input_events_state(), 0u);
-
-  manager->SetHidden(true);
-  EXPECT_EQ(
-      manager->suppressing_input_events_state(),
-      static_cast<uint16_t>(
-          WidgetInputHandlerManager::SuppressingInputEventsBits::kHidden));
-
-  manager->OnDevToolsSessionConnectionChanged(/*attached=*/true);
-  DispatchTouchEvent(/*expected_times_called=*/1,
-                     mojom::blink::WidgetInputHandler::DispatchEventCallback());
-
-  manager->OnDevToolsSessionConnectionChanged(/*attached=*/false);
-  if (GetParam()) {
-    ExpectNotConsumedDispatchEvent();
-  }
-}
-
-TEST_P(WidgetInputHandlerManagerTest, VizHostRace) {
+TEST_F(WidgetInputHandlerManagerTest, DISABLED_VizHostRace) {
   std::atomic<bool> start_flag{false};
   std::atomic<int> threads_ready{0};
   std::atomic<int> threads_finished{0};
@@ -286,11 +216,4 @@ TEST_P(WidgetInputHandlerManagerTest, VizHostRace) {
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(,
-                         WidgetInputHandlerManagerTest,
-                         testing::Bool(),
-                         [](auto& info) {
-                           return info.param ? "IgnoreInputWhileHidden"
-                                             : "ProcessInputWhileHidden";
-                         });
 }  // namespace blink::test
