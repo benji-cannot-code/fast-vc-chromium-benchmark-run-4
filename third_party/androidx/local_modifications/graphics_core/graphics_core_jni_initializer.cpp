452c34c47147c1f8f5c4c66e1961a1f6e227b5c8
Andrew Grieve <agrieve@chromium.org>
2026-06-04 08:30:43 -0700

    Import the C++ side of androidx.graphics_core
    So that we can build it from source rather than using a .so file from
    the .aar file.
    
    Note that this does not yet link the library.
    
    Bug: 479580929
    Change-Id: I82b69a0104681bd3d43fc239713bedd162488f1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896722
    Commit-Queue: Sam Maier <smaier@google.com>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641662}

---
+++ third_party/androidx/local_modifications/graphics_core/graphics_core_jni_initializer.cpp
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <jni.h>
+
+#include "third_party/androidx/local_modifications/graphics_core/graphics_core_jni_headers/GraphicsCoreJniLoader_jni.h"
+
+jint GraphicsCore_JNI_OnLoad(JNIEnv* env);
+
+static void JNI_GraphicsCoreJniLoader_Init(JNIEnv* env) {
+    GraphicsCore_JNI_OnLoad(env);
+}
+
+DEFINE_JNI_FOR_GraphicsCoreJniLoader()
