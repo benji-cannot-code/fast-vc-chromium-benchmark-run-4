3fe518ddc53aa662e8a88115fee1943effef52e4
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-06-04 10:33:25 -0700

    [Extensions] Remove unused stub API mimeHandlerViewGuestInternal
    mimeHandlerViewGuestInternal has an unused stub api in
    extensions/common/api/mime_handler_view_guest_internal.json, which has
    never had any utility. It also has a feature entry,
    mimeHandlerViewGuestInternal, in _api_features.json
    that's available to all contexts. This is only used for checking in
    MimeHandlerViewGuest if the feature is available.
    
    Remove these:
    * Remove mime_handler_view_guest_internal.json; it's not used for
      anything.
    * Tweak the handling in ExtensionsGuestViewManagerDelegate to
      interpret a null feature name from its delegate as being available
      everywhere (the implicit effect of the current feature).
    
    This CL should have no behavior change.
    
    Bug: None
    Change-Id: Iaec4cab991cdccf738174f6ae5e82c2d92472847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896507
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641769}

--- extensions/browser/guest_view/mime_handler_view/mime_handler_view_constants.h
+++ extensions/browser/guest_view/mime_handler_view/mime_handler_view_constants.h
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace mime_handler_view {
 
-// API namespace.
-extern const char kAPINamespace[];
-
 // Create parameters.
 extern const char kStreamId[];
 
