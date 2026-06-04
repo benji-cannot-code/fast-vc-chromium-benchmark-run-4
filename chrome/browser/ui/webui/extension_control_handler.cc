be5227fa5a6bd13739fe25bef93cace4dd167389
Justin Lulejian <jlulejian@chromium.org>
2026-06-04 07:39:22 -0700

    [Extensions] Prevent crashes when disabling extensions from WebUI.
    Adds checks to ensure the argument is a string and that the provided
    extension ID is a valid CRX ID before attempting to disable the
    extension.
    
    This prevents downstream code involving extension prefs from seeing an
    invalid ID and crashing.
    
    Fixed: 511058790
    Change-Id: Ia78d33b7d21f26100e696f1163984315e811172f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883997
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641640}

--- chrome/browser/ui/webui/extension_control_handler.cc
+++ chrome/browser/ui/webui/extension_control_handler.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/crx_file/id_util.h"
 #include "content/public/browser/web_ui.h"
 #include "extensions/browser/disable_reason.h"
 #include "extensions/browser/extension_registrar.h"
@@ -27,7 +29,21 @@ void ExtensionControlHandler::RegisterMessages() {
 void ExtensionControlHandler::HandleDisableExtension(
     const base::ListValue& args) {
   CHECK_EQ(args.size(), 1u);
+  // Calling base::Value::GetString() on a non-string value can cause a DCHECK.
+  if (!args[0].is_string()) {
+    return;
+  }
+
   const std::string& extension_id = args[0].GetString();
+  // `extension_id` is from the WebUI frontend, so it could be
+  // corrupted/compromised. If so, ignore it because downstream code can assume
+  // valid extension IDs.
+  // TODO(crbug.com/518751548): Investigate whether we should kill the renderer
+  // when this happens here and in other similar places.
+  if (!crx_file::id_util::IdIsValid(extension_id)) {
+    return;
+  }
+
   auto* extension_registrar =
       extensions::ExtensionRegistrar::Get(Profile::FromWebUI(web_ui()));
   extension_registrar->DisableExtension(
