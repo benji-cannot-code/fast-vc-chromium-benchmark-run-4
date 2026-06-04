934bc2ea42b666e3b27febe5175753b0b003777f
Ben Chin <lungc@chromium.org>
2026-06-03 19:48:22 -0700

    force list item decoration redraw on selection
    Selecting the Incognito window row in 'Manage Windows' sometimes didn't
    show the blue highlight. The backgrounds are set dynamically in
    DialogListItemDecoration.onDraw(), but updating `view.setSelected()`
    doesn't automatically tell RecyclerView to redraw its decorations.
    
    Fixed this by calling `invalidateItemDecorations()` when window rows are
    selected or deselected.
    
    Bug: 496257586
    Change-Id: Id041b46495cdbe37a30da4af38a8db8e7bf95ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894116
    Reviewed-by: ben chin <lungc@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: ben chin <lungc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641422}

--- chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherCoordinator.java
+++ chrome/browser/ui/android/multiwindow/java/src/org/chromium/chrome/browser/multiwindow/InstanceSwitcherCoordinator.java
@@ -626,10 +626,12 @@ public class InstanceSwitcherCoordinator {
         // Update the UI models to reflect the new selection state.
         for (ListItem li : getCurrentList()) {
             int id = li.model.get(InstanceSwitcherItemProperties.INSTANCE_ID);
-            li.model.set(InstanceSwitcherItemProperties.IS_SELECTED, mSelectedItems.contains(id));
+            boolean selected = mSelectedItems.contains(id);
+            li.model.set(InstanceSwitcherItemProperties.IS_SELECTED, selected);
         }
 
         updateActionButtons();
+        invalidateItemDecorations();
     }
 
     private void selectAllItems() {
@@ -639,6 +641,7 @@ public class InstanceSwitcherCoordinator {
             li.model.set(InstanceSwitcherItemProperties.IS_SELECTED, true);
         }
         updateActionButtons();
+        invalidateItemDecorations();
     }
 
     private void updateActionButtons() {
@@ -700,6 +703,15 @@ public class InstanceSwitcherCoordinator {
         }
         mSelectedItems.clear();
         updateActionButtons();
+        invalidateItemDecorations();
+    }
+
+    private void invalidateItemDecorations() {
+        if (mIsInactiveListShowing) {
+            mInactiveInstancesList.invalidateItemDecorations();
+        } else {
+            mActiveInstancesList.invalidateItemDecorations();
+        }
     }
 
     void dismissDialog(@DialogDismissalCause int cause) {
