5804951d82dc7140eaa8164dbe19297d9ac34af2
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-06-04 09:54:25 -0700

    [Extensions Metrics] Remove Extensions.StartupPagesOverrides2 histogram
    Remove the histogram Extensions.StartupPagesOverrides2. We're no longer
    actively monitoring it.
    
    Bug: 502205331
    Change-Id: I46c548c9c56c9ef83f65c72f58a33f74084964e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900855
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641741}

--- chrome/browser/extensions/installed_loader.cc
+++ chrome/browser/extensions/installed_loader.cc
@@ -483,7 +483,6 @@ void InstalledLoader::RecordExtensionsMetrics(Profile* profile) {
   int ntp_override_count = 0;
   int homepage_override_count = 0;
   int search_engine_override_count = 0;
-  int startup_pages_override_count = 0;
   int incognito_allowed_count = 0;
   int incognito_not_allowed_count = 0;
   int file_access_allowed_count = 0;
@@ -679,9 +678,6 @@ void InstalledLoader::RecordExtensionsMetrics(Profile* profile) {
     if (settings) {
       if (settings->search_engine) {
         ++search_engine_override_count;
-      }
-      if (!settings->startup_pages.empty()) {
-        ++startup_pages_override_count;
       }
       if (settings->homepage) {
         ++homepage_override_count;
@@ -976,8 +972,6 @@ void InstalledLoader::RecordExtensionsMetrics(Profile* profile) {
                               ntp_override_count);
   base::UmaHistogramCounts100("Extensions.SearchEngineOverrides2",
                               search_engine_override_count);
-  base::UmaHistogramCounts100("Extensions.StartupPagesOverrides2",
-                              startup_pages_override_count);
   base::UmaHistogramCounts100("Extensions.HomepageOverrides2",
                               homepage_override_count);
 
