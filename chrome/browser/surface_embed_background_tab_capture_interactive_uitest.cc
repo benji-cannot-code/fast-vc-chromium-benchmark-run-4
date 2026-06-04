344cf007752639f820bc23d590cecb0d47d11651
Keren Zhu <kerenzhu@chromium.org>
2026-06-04 12:40:32 -0700

    surface-embed: remove GuestContents flag from tab capturing test
    SurfaceEmbedBackgroundTabCaptureInteractiveUiTest incorrectly uses
    GuestContents' feature flag kAttachUnownedInnerWebContents. This CL
    removes it.
    
    Bug: none
    Change-Id: I0b80e07f62f7aa690869af053b418aca7de11ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902261
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641865}

--- chrome/browser/surface_embed_background_tab_capture_interactive_uitest.cc
+++ chrome/browser/surface_embed_background_tab_capture_interactive_uitest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/surface_embed/common/features.h"
-#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 
@@ -25,8 +24,7 @@ class SurfaceEmbedBackgroundTabCaptureInteractiveUiTest
  public:
   SurfaceEmbedBackgroundTabCaptureInteractiveUiTest() {
     feature_list_.InitWithFeatures(
-        {surface_embed::features::kSurfaceEmbed, ::features::kWebium,
-         ::features::kAttachUnownedInnerWebContents},
+        {surface_embed::features::kSurfaceEmbed, ::features::kWebium},
         {});
   }
 
