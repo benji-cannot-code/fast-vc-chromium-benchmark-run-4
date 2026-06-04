b738a17664aa4757eed8c02c9ecf504bf166e607
Robbie Gibson <rkgibson@google.com>
2026-06-04 09:23:57 -0700

    [LevelUp] Populate all 12 Level Up tasks initially
    This CL adds all 12 initial Level Up tasks with whatever data exists
    currently. Where fields are not yet available, they are left blank and
    will be added later.
    
    Bug: b:513246178
    Change-Id: I805b08036f6d3f791debd8bf4b0e8550eaa74f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900375
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641709}

--- ios/chrome/browser/level_up/model/task_types.h
+++ ios/chrome/browser/level_up/model/task_types.h
@@ -12,6 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 enum class TaskType {
   kUnknown = 0,
   kTabGroups = 1,
+  kAutofill = 2,
+  kPinTabs = 3,
+  kGemini = 4,
+  kPaymentMethods = 5,
+  kQuickDelete = 6,
+  kSafeBrowsing = 7,
+  kIncognito = 8,
+  kPasswordCheckup = 9,
+  kLensSearch = 10,
+  kAISearch = 11,
+  kCameraSearch = 12,
 };
 
 // Categories grouping the level-up tasks.
