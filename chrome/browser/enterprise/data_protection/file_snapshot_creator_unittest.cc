2d81e21cebd92b68247c67fa5d8da73b3f305595
Minoru Chikamune <chikamune@chromium.org>
2026-06-03 23:19:45 -0700

    Disable FileSnapshotCreatorTest.MultiChunkFileCreation on ASan ChromeOS
    FileSnapshotCreatorTest.MultiChunkFileCreation is consistently failing
    on ASan ChromeOS bots. This CL disables the test on that platform to
    keep the build green.
    
    Test history:
    https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Ftest%5C%3Aunit_tests!gtest%3A%3AFileSnapshotCreatorTest%23MultiChunkFileCreation
    
    Bug: 519813376
    Bug: 519214863
    Change-Id: Idad797232b392d89b3a5b0f56535403a1a84078c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901751
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Owners-Override: Minoru Chikamune <chikamune@chromium.org>
    Auto-Submit: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641486}

--- chrome/browser/enterprise/data_protection/file_snapshot_creator_unittest.cc
+++ chrome/browser/enterprise/data_protection/file_snapshot_creator_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/test/test_future.h"
+#include "build/build_config.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/browser_task_environment.h"
 #include "storage/browser/file_system/file_system_backend.h"
@@ -130,7 +131,13 @@ TEST_F(FileSnapshotCreatorTest, NonExistentFileErrorHandling) {
   EXPECT_TRUE(snapshot_path.empty());
 }
 
-TEST_F(FileSnapshotCreatorTest, MultiChunkFileCreation) {
+// TODO(crbug.com/519813376): Fails on ASan ChromeOS.
+#if BUILDFLAG(IS_CHROMEOS) && defined(ADDRESS_SANITIZER)
+#define MAYBE_MultiChunkFileCreation DISABLED_MultiChunkFileCreation
+#else
+#define MAYBE_MultiChunkFileCreation MultiChunkFileCreation
+#endif
+TEST_F(FileSnapshotCreatorTest, MAYBE_MultiChunkFileCreation) {
   std::string expected_content("a", FileSnapshotCreator::CHUNK_SIZE + 10);
   storage::FileSystemURL url =
       CreateVirtualFile("multi_chunk_file.txt", expected_content);
