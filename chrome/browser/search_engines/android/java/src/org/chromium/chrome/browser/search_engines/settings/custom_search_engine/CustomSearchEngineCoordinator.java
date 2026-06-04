21a2e5bebcb3dd15f7f54ab3e7152782d0a5e201
Kay Lin <kaiyilin@google.com>
2026-06-03 21:41:58 -0700

    Site Search: Rename CustomSearchEngineList* to CustomSearchEngine
    This CL is only renaming the CustomSearchEngine related component to
    remove the "list" suffix.
    
    Change-Id: I69c462cbd2432c1db7a1d79260778aefc8b4b7b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894045
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641458}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_search_engine/CustomSearchEngineCoordinator.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_search_engine/CustomSearchEngineCoordinator.java
@@ -24,10 +24,10 @@ import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
 
 @NullMarked
-public class CustomSearchEngineListCoordinator {
+public class CustomSearchEngineCoordinator {
     private final ModelList mModelList = new ModelList();
     private final SimpleRecyclerViewAdapter mAdapter;
-    private final CustomSearchEngineListMediator mMediator;
+    private final CustomSearchEngineMediator mMediator;
 
     private final PropertyModel mModel;
     private final PropertyModelChangeProcessor mPropertyModelChangeProcessor;
@@ -36,7 +36,7 @@ public class CustomSearchEngineListCoordinator {
     private final ModalDialogManager mModalDialogManager;
     private final SiteSearchDialogCoordinator mSiteSearchDialogCoordinator;
 
-    public CustomSearchEngineListCoordinator(
+    public CustomSearchEngineCoordinator(
             Context context,
             Profile profile,
             SearchEngineListPreference pref,
@@ -62,7 +62,7 @@ public class CustomSearchEngineListCoordinator {
                 SiteSearchViewBinder::bind);
 
         mMediator =
-                new CustomSearchEngineListMediator(
+                new CustomSearchEngineMediator(
                         context,
                         mModelList,
                         profile,
