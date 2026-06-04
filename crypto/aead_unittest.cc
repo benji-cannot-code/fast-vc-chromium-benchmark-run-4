a86ac807e49049d4d559949807a422d32077322e
Elly <ellyjones@chromium.org>
2026-06-03 17:49:00 -0700

    crypto/aead: add AES-128-GCM
    Required to support components/gcm_driver.
    
    AI-Model: none
    Bug: 427242312
    Change-Id: I7a5acd1b7b66e0dc9d625460a4306300b0ae3946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900060
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Auto-Submit: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641350}

--- crypto/aead_unittest.cc
+++ crypto/aead_unittest.cc
@@ -12,11 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const crypto::Aead::AeadAlgorithm kAllAlgorithms[]{
-    crypto::Aead::AES_128_CTR_HMAC_SHA256,
-    crypto::Aead::AES_256_GCM,
-    crypto::Aead::AES_256_GCM_SIV,
-    crypto::Aead::CHACHA20_POLY1305,
+const crypto::aead::Algorithm kAllAlgorithms[]{
+    crypto::aead::AES_128_CTR_HMAC_SHA256,
+    crypto::aead::AES_128_GCM,
+    crypto::aead::AES_256_GCM,
+    crypto::aead::AES_256_GCM_SIV,
+    crypto::aead::CHACHA20_POLY1305,
 };
 
 class AeadTest : public testing::TestWithParam<crypto::Aead::AeadAlgorithm> {};
