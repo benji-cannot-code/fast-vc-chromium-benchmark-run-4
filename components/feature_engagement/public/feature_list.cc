765943779ce4ae8a046b0e3e3d691e5a1ace062a
Dan Polanco <polardz@google.com>
2026-06-04 14:08:03 -0700

    [IA][BottomBar] Add feature engagement targets for bottom bar dialog
    Bug: 519148302, 517591009
    Change-Id: Ieb9b77b0df2a4db256595ccf6c304278c7dca3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903060
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641930}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -34,6 +34,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHAdaptiveButtonPinGlicToolbarButtonFeature,
     &kIPHAndroidBottomBarGlic,
     &kIPHAndroidBottomBarNewTab,
+    &kIPHAndroidBottomBarPromoDialog,
     &kIPHAndroidTabDeclutter,
     &kIPHAppRatingPromptFeature,
     &kIPHAppSpecificHistory,
