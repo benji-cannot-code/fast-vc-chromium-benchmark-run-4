3b4d811a3a49c6aef1cba6c4af26f5c8abd394e3
Sky Malice <skym@chromium.org>
2026-06-04 11:35:18 -0700

    [AIM] Move request type chip text resolution to mediator.
    This is a counter intuitive change, typically the view is responsible
    for loading things out of the Context/Resource objects, not the
    mediator. However in a follow up change this text will become server
    driven, which must happen in the mediator.
    
    Bug: 519665087
    Change-Id: I90fed0d3b94f4ab4c9a5440d334415a4d3ac3faf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901735
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641827}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxViewBinderUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxViewBinderUnitTest.java
@@ -122,6 +122,7 @@ public class FuseboxViewBinderUnitTest {
         mModel.set(FuseboxProperties.ADD_BUTTON_VISIBLE, true);
         mModel.set(FuseboxProperties.FUSEBOX_STATE, FuseboxState.EXPANDED);
         mModel.set(FuseboxProperties.REQUEST_TYPE, AutocompleteRequestType.SEARCH);
+        mModel.set(FuseboxProperties.REQUEST_TYPE_BUTTON_TEXT, "test label");
         mModel.set(FuseboxProperties.REQUEST_TYPE_BUTTON_VISIBLE, false);
         mModel.set(FuseboxProperties.COLOR_SCHEME, BrandedColorScheme.APP_DEFAULT);
         mModel.set(FuseboxProperties.FUSEBOX_LAYOUT_MODE, FuseboxLayoutMode.TOOLBAR);
@@ -152,6 +153,7 @@ public class FuseboxViewBinderUnitTest {
                 FuseboxProperties.FUSEBOX_STATE,
                 testCase == Variant.COMPACT ? FuseboxState.COMPACT : FuseboxState.EXPANDED);
         mModel.set(FuseboxProperties.REQUEST_TYPE, requestType);
+        mModel.set(FuseboxProperties.REQUEST_TYPE_BUTTON_TEXT, "test label");
         mModel.set(FuseboxProperties.REQUEST_TYPE_BUTTON_VISIBLE, false);
     }
 
