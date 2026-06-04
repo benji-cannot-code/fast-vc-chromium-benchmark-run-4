4a0b26294a29351166b5d1a1066138db73e6757c
Philip Rogers <pdr@chromium.org>
2026-06-04 08:52:46 -0700

    [html-in-canvas] Do not composite non-canvas children
    HTMLCanvasElement::CreateLayoutObject only returns a LayoutHTMLCanvas
    when scripting is enabled. This patch checks for LayoutHTMLCanvas when
    deciding compositing for html-in-canvas.
    
    Bug: 519251256
    Change-Id: I3475a9d62b77f60afffee648a1fb622619012d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900335
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641678}

--- third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_update_tests.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/input/scroll_snap_data.h"
 #include "third_party/blink/renderer/core/animation/animation_clock.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
+#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
@@ -2381,4 +2382,18 @@ TEST_P(PaintPropertyTreeUpdateTest, FixedPositionChangeCompositingReason) {
   UpdateAllLifecyclePhasesForTest();
   EXPECT_FALSE(translation->IsAffectedByOuterViewportBoundsDelta());
 }
+
+TEST_P(PaintPropertyTreeUpdateTest, CanvasScriptsDisabled) {
+  ScopedCanvasDrawElementForTest forced_canvas_draw_element_feature(true);
+  GetDocument().GetSettings()->SetScriptEnabled(false);
+
+  SetBodyInnerHTML(R"HTML(
+    <canvas layoutsubtree style="display: inline;">
+      <div id="target">Hello</div>
+    </canvas>
+  )HTML");
+  UpdateAllLifecyclePhasesForTest();
+  // Pass if no crash.
+}
+
 }  // namespace blink
