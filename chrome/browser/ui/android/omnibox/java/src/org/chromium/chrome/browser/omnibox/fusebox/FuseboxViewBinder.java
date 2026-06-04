ee9d5948bbe763169504771234614d4ddef8e045
Sky Malice <skym@chromium.org>
2026-06-04 11:06:22 -0700

    [AIM] Rename request type props for consistency.
    Also re-ordered some properties to maintain alphabetical oerder.
    
    Bug: 519665087
    Change-Id: I2f81293b2de6027747be599180afb6ee01cd1192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901170
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1641798}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxViewBinder.java
@@ -70,12 +70,6 @@ class FuseboxViewBinder {
             view.attachmentsView.setAdapter(model.get(FuseboxProperties.ADAPTER));
         } else if (propertyKey == FuseboxProperties.ADD_BUTTON_VISIBLE) {
             updateAddButton(model, view);
-        } else if (propertyKey == FuseboxProperties.AUTOCOMPLETE_REQUEST_TYPE) {
-            updateRequestTypeButton(model, view);
-            updateButtonsA11yAnnouncements(model, view);
-        } else if (propertyKey == FuseboxProperties.AUTOCOMPLETE_REQUEST_TYPE_CLICKED) {
-            view.requestType.setOnClickListener(
-                    v -> model.get(FuseboxProperties.AUTOCOMPLETE_REQUEST_TYPE_CLICKED).run());
         } else if (propertyKey == FuseboxProperties.ATTACHMENTS_VISIBLE) {
             boolean visible = model.get(FuseboxProperties.ATTACHMENTS_VISIBLE);
             view.attachmentsView.setVisibility(visible ? View.VISIBLE : View.GONE);
@@ -193,13 +187,6 @@ class FuseboxViewBinder {
                                 ? View.VISIBLE
                                 : View.GONE);
             }
-        } else if (propertyKey == FuseboxProperties.POPUP_RECENT_TABS_HEADER_VISIBLE) {
-            if (view.popup.mRecentTabsHeader != null) {
-                view.popup.mRecentTabsHeader.setVisibility(
-                        model.get(FuseboxProperties.POPUP_RECENT_TABS_HEADER_VISIBLE)
-                                ? View.VISIBLE
-                                : View.GONE);
-            }
         } else if (propertyKey == FuseboxProperties.POPUP_RECENT_TABS_ENABLED) {
             ViewGroup container = view.popup.mRecentTabsContainer;
             if (container != null) {
@@ -207,6 +194,13 @@ class FuseboxViewBinder {
                 for (int i = 0; i < container.getChildCount(); i++) {
                     container.getChildAt(i).setEnabled(enabled);
                 }
+            }
+        } else if (propertyKey == FuseboxProperties.POPUP_RECENT_TABS_HEADER_VISIBLE) {
+            if (view.popup.mRecentTabsHeader != null) {
+                view.popup.mRecentTabsHeader.setVisibility(
+                        model.get(FuseboxProperties.POPUP_RECENT_TABS_HEADER_VISIBLE)
+                                ? View.VISIBLE
+                                : View.GONE);
             }
         } else if (propertyKey == FuseboxProperties.POPUP_STATE) {
             view.popup.setPopupState(model.get(FuseboxProperties.POPUP_STATE));
@@ -224,7 +218,13 @@ class FuseboxViewBinder {
                     model.get(FuseboxProperties.POPUP_TOOL_HEADER_VISIBLE)
                             ? View.VISIBLE
                             : View.GONE);
-        } else if (propertyKey == FuseboxProperties.SHOW_REQUEST_TYPE_BUTTON) {
+        } else if (propertyKey == FuseboxProperties.REQUEST_TYPE) {
+            updateRequestTypeButton(model, view);
+            updateButtonsA11yAnnouncements(model, view);
+        } else if (propertyKey == FuseboxProperties.REQUEST_TYPE_BUTTON_CLICKED) {
+            view.requestType.setOnClickListener(
+                    v -> model.get(FuseboxProperties.REQUEST_TYPE_BUTTON_CLICKED).run());
+        } else if (propertyKey == FuseboxProperties.REQUEST_TYPE_BUTTON_VISIBLE) {
             updateRequestTypeButton(model, view);
         }
     }
@@ -492,7 +492,7 @@ class FuseboxViewBinder {
             PropertyModel model, FuseboxViewHolder view) {
         @StringRes
         int navButtonAccessibilityStringRes = R.string.acc_send_button_search_or_navigate;
-        switch (model.get(FuseboxProperties.AUTOCOMPLETE_REQUEST_TYPE)) {
+        switch (model.get(FuseboxProperties.REQUEST_TYPE)) {
             case AutocompleteRequestType.AI_MODE:
                 navButtonAccessibilityStringRes = R.string.acc_send_button_send_to_ai;
                 break;
@@ -559,13 +559,12 @@ class FuseboxViewBinder {
     }
 
     private static void updateRequestTypeButton(PropertyModel model, FuseboxViewHolder view) {
-        if (!model.get(FuseboxProperties.SHOW_REQUEST_TYPE_BUTTON)) {
+        if (!model.get(FuseboxProperties.REQUEST_TYPE_BUTTON_VISIBLE)) {
             view.requestType.setVisibility(View.GONE);
             return;
         }
 
-        @AutocompleteRequestType
-        int requestType = model.get(FuseboxProperties.AUTOCOMPLETE_REQUEST_TYPE);
+        @AutocompleteRequestType int requestType = model.get(FuseboxProperties.REQUEST_TYPE);
         if (!ToolModeUtils.isAimRequest(requestType)) {
             // The model is in an inconsistent state, wait for the next event.
             return;
