cf1da99a9051d2a73510e29bbe6a519aaef4ce2f
Sky Malice <skym@chromium.org>
2026-06-04 13:28:20 -0700

    [AIM] AI Mode and site search are exclusive.
    * The activation chip should hide when in site search.
    * Site search should not activate when in any ai tool.
    
    Bug: 519654347
    Change-Id: Icb7589120125d3708c35db6d17f195432f0cc965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900594
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641898}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -1205,6 +1205,10 @@ class AutocompleteMediator
             return false;
         }
 
+        if (ToolModeUtils.isAimRequest(mAutocompleteInput.getRequestType())) {
+            return false;
+        }
+
         // If a site search is already active, do not trigger another site search.
         if (mAutocompleteInput.getSiteSearchData() != null) {
             return false;
