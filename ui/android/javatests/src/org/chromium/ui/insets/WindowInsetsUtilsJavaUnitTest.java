79843ba92275b0d061b6a5c69fe1ef40ff0badff
Aishwarya Rajesh <aishwaryarj@google.com>
2026-06-04 10:36:55 -0700

    Enable WindowInsetsUtilsJavaUnitTest PostV bounding rects tests
    This CL updates the PostV WindowInsets test builders to set non-zero
    caption bar insets (`.setInsets(...)`) alongside the bounding rects.
    This allows the platform to successfully retain and return the
    bounding rects inside the built WindowInsets object, enabling us
    to re-enable these tests.
    
    Bug: 362337139
    Change-Id: I6511f7b4a4c0580d5d5c93ce2372f36119dd80dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895148
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641772}

--- ui/android/javatests/src/org/chromium/ui/insets/WindowInsetsUtilsJavaUnitTest.java
+++ ui/android/javatests/src/org/chromium/ui/insets/WindowInsetsUtilsJavaUnitTest.java
@@ -23,7 +23,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MaxAndroidSdkLevel;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.ui.insets.WindowInsetsUtils.UnoccludedRegion;
@@ -105,11 +104,13 @@ public class WindowInsetsUtilsJavaUnitTest {
     @Test
     @SmallTest
     @MinAndroidSdkLevel(VERSION_CODES.VANILLA_ICE_CREAM)
-    @DisabledTest(message = "crbug.com/362337139")
     public void testGetBoundingRects_PostV() {
         var boundingRects = List.of(new Rect(0, 0, 100, 100), new Rect(800, 0, 1600, 100));
         var insets =
                 new WindowInsets.Builder()
+                        .setInsets(
+                                WindowInsetsCompat.Type.captionBar(),
+                                android.graphics.Insets.of(0, 100, 0, 0))
                         .setBoundingRects(WindowInsetsCompat.Type.captionBar(), boundingRects)
                         .build();
         assertEquals(
@@ -122,11 +123,13 @@ public class WindowInsetsUtilsJavaUnitTest {
     @Test
     @SmallTest
     @MinAndroidSdkLevel(VERSION_CODES.VANILLA_ICE_CREAM)
-    @DisabledTest(message = "crbug.com/362337139")
     public void testGetBoundingRects_PostV_correctRects() {
         var boundingRects = List.of(new Rect(50, 0, 100, 100), new Rect(800, 0, 1600, 100));
         var insets =
                 new WindowInsets.Builder()
+                        .setInsets(
+                                WindowInsetsCompat.Type.captionBar(),
+                                android.graphics.Insets.of(0, 100, 0, 0))
                         .setBoundingRects(WindowInsetsCompat.Type.captionBar(), boundingRects)
                         .setFrame(1650, 800)
                         .build();
@@ -139,6 +142,9 @@ public class WindowInsetsUtilsJavaUnitTest {
         boundingRects = List.of(new Rect(400, 0, 100, 100), new Rect(800, 0, 1600, 100));
         insets =
                 new WindowInsets.Builder()
+                        .setInsets(
+                                WindowInsetsCompat.Type.captionBar(),
+                                android.graphics.Insets.of(0, 100, 0, 0))
                         .setBoundingRects(WindowInsetsCompat.Type.captionBar(), boundingRects)
                         .setFrame(2000, 800)
                         .build();
