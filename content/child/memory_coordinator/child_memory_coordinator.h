321e618bb784712b38faacd9632ee891bc47697b
Patrick Monette <pmonette@chromium.org>
2026-06-03 22:38:40 -0700

    [Memory Coordinator] Remove unused file
    This file was a left over from when the MemoryConsumerRegistry
    implementations were refactored into a single implementation that works
    for the browser process and child processes.
    
    Bug: 441951621
    Change-Id: I05f7d7f90aa738dab2fa803ebf39059c98e3cfbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895144
    Reviewed-by: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641475}

--- content/child/memory_coordinator/child_memory_coordinator.h
+++ content/child/memory_coordinator/child_memory_coordinator.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 // ChildMemoryCoordinator is a singleton that owns both the
-// ChildMemoryConsumerRegistry and the MemoryCoordinatorPolicyManager.
+// MemoryConsumerRegistry and the MemoryCoordinatorPolicyManager.
 class CONTENT_EXPORT ChildMemoryCoordinator {
  public:
   static ChildMemoryCoordinator& Get();
