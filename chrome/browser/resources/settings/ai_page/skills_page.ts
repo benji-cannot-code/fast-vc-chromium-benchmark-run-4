6822217849182907c741e72751e6e3e741f5ac37
Howard Chang <howardcha@google.com>
2026-06-05 15:23:02 -0700

    settings: Update content and styling of AI skills subpage
    Update the chrome://settings/ai/skills settings page to align with the
    latest mocks
    
    Demo:https://screencast.googleplex.com/cast/NTgwMzMwODUyOTc0NTkyMHwxYTI3YzkzZC1lZA
    Mock:https://docs.google.com/presentation/d/1oGN04EPgimksCwb2dONcm1cRFaz7QBAoO3CkFBDhLm8/edit?slide=id.g3e3a26647f6_0_1&resourcekey=0-EcJUvNmy9GKRA28xMSafjQ#slide=id.g3e3a26647f6_0_1
    
    Bug: 513332678
    Change-Id: Ibee4ed9d6628c186c8c88fd22895e543078ac325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897706
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Howard Chang <howardcha@google.com>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642647}

--- chrome/browser/resources/settings/ai_page/skills_page.ts
+++ chrome/browser/resources/settings/ai_page/skills_page.ts
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
 import '../controls/settings_toggle_button.js';
+import '../settings_columned_section.css.js';
 import '../settings_page/settings_subpage.js';
 import '../settings_shared.css.js';
+import '../icons.html.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
