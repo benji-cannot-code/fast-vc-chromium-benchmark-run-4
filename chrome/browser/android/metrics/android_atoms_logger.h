c0a98ee681ce01e5c9f99e397fa6f154dad4a9bb
Troy Wang <troywang@google.com>
2026-06-04 17:54:05 -0700

    Use MetricsReportingChoiceService for consent
    Migrate to use MetricsReportingChoiceService to check UMA reporting
    status.
    
    Bug: b:483013446
    Bug: b:505242139
    Change-Id: Iff09c2fed537c188801e7afa6a70aa0ba33f6cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890546
    Auto-Submit: Troy Wang <troywang@google.com>
    Commit-Queue: Eric Lok <lokeric@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Eric Lok <lokeric@google.com>
    Cr-Commit-Position: refs/heads/main@{#1642060}

--- chrome/browser/android/metrics/android_atoms_logger.h
+++ chrome/browser/android/metrics/android_atoms_logger.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/callback_list.h"
 #include "base/containers/span.h"
 #include "base/functional/callback.h"
 #include "base/functional/function_ref.h"
@@ -67,6 +68,7 @@ class AndroidAtomsLogger {
 
   bool metrics_reporting_enabled_ = true;
   PrefChangeRegistrar pref_change_registrar_;
+  base::CallbackListSubscription consent_change_subscription_;
   std::vector<
       std::unique_ptr<base::StatisticsRecorder::ScopedHistogramSampleObserver>>
       observers_;
