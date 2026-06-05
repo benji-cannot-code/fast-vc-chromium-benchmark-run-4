8b365fb157876b2397d0077a2f88a728d4ca6672
Atharv Maan <atharvmaan@chromium.org>
2026-06-05 12:34:47 -0700

    DefaultBrowser: Disable set-as-default integration in dialog manager test
    This test was flakily leaking COM interface references on Windows because pressing 'Open Settings' triggers the real DefaultBrowserWorker setter, which launches an OpenSystemSettingsHelper that registers UIA event listeners (via AutomationController). This helper stays alive waiting on a 2-minute timer, keeping UIA COM event handlers registered and retaining references to the test's widgets, causing ghost AXPlatformNodeWin instances at the end of the test.
    
    This CL fixes this by calling DisableSetAsDefaultForTesting() in the test's SetUp(), similar to other default browser prompt tests.
    
    Bug: 501030880
    Change-Id: I5cd36ae42c9094f2153c0e1a9ce182f6fd167ddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905277
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642521}

--- chrome/browser/ui/startup/default_browser_prompt/default_browser_bubble_dialog_interactive_uitest.cc
+++ chrome/browser/ui/startup/default_browser_prompt/default_browser_bubble_dialog_interactive_uitest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "chrome/browser/default_browser/default_browser_controller.h"
 #include "chrome/browser/default_browser/test_support/fake_default_browser_setter.h"
+#include "chrome/browser/shell_integration.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/startup/default_browser_prompt/default_browser_bubble_dialog.h"
@@ -106,6 +107,11 @@ IN_PROC_BROWSER_TEST_F(DefaultBrowserBubbleDialogInteractiveTest,
 class DefaultBrowserDialogManagerInteractiveTest
     : public InteractiveBrowserTest {
  protected:
+  void SetUp() override {
+    shell_integration::DefaultBrowserWorker::DisableSetAsDefaultForTesting();
+    InteractiveBrowserTest::SetUp();
+  }
+
   void ShowDialogManager() {
     manager_ = std::make_unique<DefaultBrowserBubbleDialogManager>();
     manager_->Show(/*can_pin_to_taskbar=*/false);
@@ -148,14 +154,8 @@ IN_PROC_BROWSER_TEST_F(DefaultBrowserDialogManagerInteractiveTest,
       WaitForHide(default_browser::kBubbleDialogId));
 }
 
-// TODO(crbug.com/501030880): Re-enable the test
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_ShowAndAccept DISABLED_ShowAndAccept
-#else
-#define MAYBE_ShowAndAccept ShowAndAccept
-#endif
 IN_PROC_BROWSER_TEST_F(DefaultBrowserDialogManagerInteractiveTest,
-                       MAYBE_ShowAndAccept) {
+                       ShowAndAccept) {
   RunTestSequence(
       Do([this]() { ShowDialogManager(); }),
       WaitForShow(default_browser::kBubbleDialogOpenSettingsButtonId),
