ea690be0b44a420b28d76011118d345cadd39de2
James Cook <jamescook@chromium.org>
2026-06-05 16:59:21 -0700

    extensions: Port ErrorConsoleUnitTest to desktop Android
    The tests pass without modifications.
    
    Bug: 469417243
    Change-Id: I09564d3a4b45b12fadc5bc25fecf8e25397f1353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7906536
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642678}

--- chrome/browser/extensions/error_console/error_console_unittest.cc
+++ chrome/browser/extensions/error_console/error_console_unittest.cc
@@ -24,19 +24,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_error_test_util.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/unloaded_extension_reason.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/features/feature_channel.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
 namespace extensions {
+namespace {
 
 using error_test_util::CreateNewManifestError;
 using error_test_util::CreateNewRuntimeError;
 
 class ErrorConsoleUnitTest : public testing::Test {
  public:
-  ErrorConsoleUnitTest() : error_console_(nullptr) {}
+  ErrorConsoleUnitTest() = default;
   ~ErrorConsoleUnitTest() override = default;
 
   void SetUp() override {
@@ -49,7 +53,7 @@ class ErrorConsoleUnitTest : public testing::Test {
  protected:
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
-  raw_ptr<ErrorConsole> error_console_;
+  raw_ptr<ErrorConsole> error_console_ = nullptr;
 };
 
 // Test that the error console is enabled/disabled appropriately.
@@ -269,4 +273,5 @@ TEST_F(ErrorConsoleUnitTest, TestDefaultStoringPrefs) {
       unpacked_extension->id()));
 }
 
+}  // namespace
 }  // namespace extensions
