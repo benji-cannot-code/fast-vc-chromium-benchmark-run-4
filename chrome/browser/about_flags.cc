7838a3debebbe30b7b53de70ab67ac14eda55a39
Hao Dong <spdonghao@google.com>
2026-06-05 11:11:22 -0700

    Add feature flag for using p-links in HelpAndFeedbackLauncher.
    Bug: 365755043
    Change-Id: I38811c488c55d499baddfb189990f278e1b77679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903811
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1642469}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7440,6 +7440,10 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAndroid,
      FEATURE_VALUE_TYPE(safe_browsing::kAutoRevokeSuspiciousNotification)},
 
+    {"use-p-link-in-help", flag_descriptions::kUsePLinkInHelpName,
+     flag_descriptions::kUsePLinkInHelpDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kUsePLinkInHelp)},
+
     {"user-feedback-allowed-policy",
      flag_descriptions::kUserFeedbackAllowedPolicyName,
      flag_descriptions::kUserFeedbackAllowedPolicyDescription, kOsAndroid,
