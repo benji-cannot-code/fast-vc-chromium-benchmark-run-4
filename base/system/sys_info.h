cd44ad3313fb5db8eb12029140d92d7da38f0aa7
Joe Mason <joenotcharles@google.com>
2026-06-05 11:07:43 -0700

    Fix IWYU error in sys_info.h
    https://crrev.com/c/7891305 removed the last use of ByteCount in
    sys_info.h but forgot to remove the header.
    
    Bug: 448661443
    Change-Id: I1f53ad4728197b8d449fd917b171308e3fed2e51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894185
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642466}

--- base/system/sys_info.h
+++ base/system/sys_info.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_export.h"
-#include "base/byte_count.h"
 #include "base/byte_size.h"
 #include "base/functional/callback_forward.h"
 #include "base/gtest_prod_util.h"
