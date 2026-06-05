aca3b0085e46959935da7f4cce095a2f1336f3d1
Benjamin Keen <bkeen@google.com>
2026-06-04 19:17:39 -0700

    Enforce normal contrast mode on video picture-in-picture windows
    Video picture-in-picture windows, and therefore their child views (such
    as the Live Caption settings dialog), currently only support dark mode.
    Under High Contrast mode, the system theme overrides colors to high
    contrast, which can render child dialogs illegible.
    
    This CL overrides `GetColorProviderKey()` to intercept the key
    resolution. If High Contrast mode is detected, we bypass it by setting
    contrast mode to `kNormal`.
    
    Here are the samples for the Live Caption dialog:
      * Before: https://screenshot.googleplex.com/6FJcsRFnNfpoUbp
      * After: https://screenshot.googleplex.com/qXrRSkgfLywFhSY
    
    Bug: 508391299
    Change-Id: I7f58777b889a36aac37bd543782f55023e506a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895389
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1642095}

--- chrome/browser/ui/views/overlay/video_overlay_window_views_unittest.cc
+++ chrome/browser/ui/views/overlay/video_overlay_window_views_unittest.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/color/color_provider_key.h"
 #include "ui/compositor/layer.h"
 #include "ui/display/test/test_screen.h"
 #include "ui/events/base_event_utils.h"
@@ -55,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/test/event_generator.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/vector2d.h"
+#include "ui/native_theme/mock_os_settings_provider.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/widget_fade_animator.h"
 #include "ui/views/controls/button/label_button.h"
@@ -1837,4 +1839,15 @@ TEST_F(VideoOverlayWindowWithMuteControlTest, ToggleMuteButton_HitTest) {
   EXPECT_FALSE(mute_button_bounds.IsEmpty());
   EXPECT_TRUE(overlay_window().ControlsHitTestContainsPoint(
       mute_button_bounds.CenterPoint()));
+}
+
+TEST_F(VideoOverlayWindowViewsTest, GetColorProviderKeyBypassesHighContrast) {
+  ui::MockOsSettingsProvider mock_provider;
+  mock_provider.SetPreferredContrast(ui::NativeTheme::PreferredContrast::kMore);
+
+  // The widget's key should have contrast_mode == kNormal.
+  ui::ColorProviderKey key = overlay_window().GetColorProviderKey();
+  EXPECT_EQ(ui::ColorProviderKey::ContrastMode::kNormal, key.contrast_mode);
+  EXPECT_EQ(ui::ColorProviderKey::ColorMode::kDark, key.color_mode);
+  EXPECT_EQ(ui::SystemTheme::kDefault, key.system_theme);
 }
