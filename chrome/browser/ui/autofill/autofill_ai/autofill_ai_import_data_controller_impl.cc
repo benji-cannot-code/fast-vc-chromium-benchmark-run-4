fe70a1489c12b36f5bafbc008f762298267cdc93
Qikai <qikaizhong@microsoft.com>
2026-06-05 03:26:22 -0700

    [bedrock] Remove browser_finder
    Delete the deprecated browser_finder.{h,cc} helper. The last remaining
    callsite of chrome::FindBrowserWithTab in
    Browser::GetBrowserForOpeningWebUi() is replaced with
    GlobalBrowserCollection::GetInstance()->FindBrowserWithTab().
    browser_finder_unittest.cc is also removed (redundant with
    browser_collection_unittest.cc).
    
    To avoid reintroducing a Browser* dependency, also migrate:
     - Browser::GetBrowserForOpeningWebUi() return type and
       Browser::opener_browser_ field type from Browser* to
       BrowserWindowInterface*.
     - Downstream callers in chrome/browser/app_controller_mac.mm and
       chrome/browser/ui/browser_actions.cc to operate on
       BrowserWindowInterface* directly.
    
    Stale BUILD.gn entries and comment references to browser_finder.h /
    chrome::FindBrowserWithTab() are cleaned up. browser_finder_chromeos
    tests are kept for now (tracked separately).
    
    Bug: 494010890
    Change-Id: I91bb6c6d7d8cc5ec72a7ec7cda3a61524f125461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901833
    Commit-Queue: Qikai Zhong <qikaizhong@microsoft.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642242}

--- chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_ai/autofill_ai_import_data_controller_impl.cc
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 
 // TODO(crbug.com/441742849): Refactor this class implementation and possibly
-// others to remove `chrome::FindBrowserWithTab()`.
+// others to remove `FindBrowserWithTab()`.
 namespace autofill {
 
 namespace {
--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -56,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_actions.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
-#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_select_file_dialog_controller.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
