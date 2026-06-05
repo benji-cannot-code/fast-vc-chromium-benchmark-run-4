31d34ea04230721daf738189a63e0deb270a9fa4
Jakob Koblinsky <jkoblinsky@google.com>
2026-06-05 12:09:24 -0700

    [Omnibox] Allow auto-nav for voice AIM queries
    Bug: 509482007
    Change-Id: Ie69a8a9d5323b42003fa65481038b0b01cc617de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7892185
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Jakob Koblinsky <jkoblinsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1642507}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -1616,12 +1616,18 @@ class AutocompleteMediator
         AutocompleteMatch match = mAutocomplete != null ? mAutocomplete.classify(query) : null;
 
         GURL url;
-        if (match == null || match.isSearchSuggestion()) {
+        if (match == null
+                || match.isSearchSuggestion()
+                || ToolModeUtils.isAimRequest(mAutocompleteInput.getRequestType())) {
             url = TemplateUrlServiceFactory.getForProfile(profile).getUrlForVoiceSearchQuery(query);
         } else {
             url = match.getUrl();
         }
 
+        adjustGurlForRequestType(url, query, this::finishLoadUrlFromVoice);
+    }
+
+    private void finishLoadUrlFromVoice(GURL url) {
         mDelegate.loadUrl(
                 new OmniboxLoadUrlParams.Builder(url.getSpec(), PageTransition.TYPED)
                         .setOpenInNewTab(false)
