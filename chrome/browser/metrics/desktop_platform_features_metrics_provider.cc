5b4a8fbcefb8f0e452b164f2924670ad7e922217
Will Harris <wfh@chromium.org>
2026-06-05 07:46:20 -0700

    Add a metric to report whether or not browser is isolated
    A new metric is added, that is logged in every metrics log, that reports
    whether or not the currently running process is running isolated from
    other apps on the system.
    
    Bug: 433545123
    Change-Id: Ib895c5a5e6d720807918777ac3d414aaab071afd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878662
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642329}

--- chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
+++ chrome/browser/metrics/desktop_platform_features_metrics_provider.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -25,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/native_theme/os_settings_provider.h"
+
+#if BUILDFLAG(IS_WIN)
+#include "chrome/browser/win/isolated_browser_support.h"
+#endif  // BUILDFLAG(IS_WIN)
 
 namespace {
 
@@ -96,4 +101,9 @@ void DesktopPlatformFeaturesMetricsProvider::ProvideCurrentSessionData(
         }
         return true;
       });
+
+#if BUILDFLAG(IS_WIN)
+  base::UmaHistogramBoolean("Windows.RunningIsolated",
+                            chrome::IsRunningIsolated());
+#endif  // BUILDFLAG(IS_WIN)
 }
