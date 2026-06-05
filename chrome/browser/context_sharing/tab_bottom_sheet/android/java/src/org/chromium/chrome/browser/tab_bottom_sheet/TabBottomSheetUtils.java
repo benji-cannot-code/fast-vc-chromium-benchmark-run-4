fd095e8b3a96954604fd9ddef015c4209c9632ba
Madhav Pruthi <madhavpruthi@google.com>
2026-06-05 12:51:49 -0700

    [Clank IA] Collapse tab bottom sheet when clicking the toolbar home button
    Change-Id: I55d87ca6fcfccbdc554604481c54650d42c6e2fa
    Bug: 518852349, 519148302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900584
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1642536}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetUtils.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetUtils.java
@@ -7,6 +7,8 @@ package org.chromium.chrome.browser.tab_bottom_sheet;
 
 import android.app.Activity;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.ActivityState;
 import org.chromium.base.UnownedUserDataKey;
 import org.chromium.build.annotations.Contract;
@@ -40,7 +42,9 @@ public final class TabBottomSheetUtils {
      * @param windowAndroid The {@link WindowAndroid} to attach to.
      * @param manager The {@link TabBottomSheetManager} to attach.
      */
-    static void attachManagerToWindow(WindowAndroid windowAndroid, TabBottomSheetManager manager) {
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public static void attachManagerToWindow(
+            WindowAndroid windowAndroid, TabBottomSheetManager manager) {
         MANAGER_KEY.attachToHost(windowAndroid.getUnownedUserDataHost(), manager);
     }
 
@@ -49,7 +53,8 @@ public final class TabBottomSheetUtils {
      *
      * @param windowAndroid The {@link WindowAndroid} to detach from.
      */
-    static void detachManagerFromWindow(WindowAndroid windowAndroid) {
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public static void detachManagerFromWindow(WindowAndroid windowAndroid) {
         MANAGER_KEY.detachFromHost(windowAndroid.getUnownedUserDataHost());
     }
 
