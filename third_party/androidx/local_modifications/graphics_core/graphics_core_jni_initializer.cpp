571efab0aa3864cc5a2d1e0a66432cbb766ca005
Ryan Thomas <rathomas@google.com>
2026-06-05 08:32:21 -0700

    [PDF V2] Replaces PdfViewerFragment with EditablePdfViewerFragment
    Binary-Size: Increases are due to the new EditablePdfViewerFragment dependency.
    Bug: 479583004
    Change-Id: Ibca79e6c25781f31ee2c78f8b54489756464a592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903960
    Commit-Queue: Ryan Thomas <rathomas@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642360}

--- third_party/androidx/local_modifications/graphics_core/graphics_core_jni_initializer.cpp
+++ third_party/androidx/local_modifications/graphics_core/graphics_core_jni_initializer.cpp
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/androidx/local_modifications/graphics_core/graphics_core_jni_headers/GraphicsCoreJniLoader_jni.h"
 
-jint GraphicsCore_JNI_OnLoad(JNIEnv* env);
+extern "C" jint GraphicsCore_JNI_OnLoad(JNIEnv* env);
 
 static void JNI_GraphicsCoreJniLoader_Init(JNIEnv* env) {
     GraphicsCore_JNI_OnLoad(env);
