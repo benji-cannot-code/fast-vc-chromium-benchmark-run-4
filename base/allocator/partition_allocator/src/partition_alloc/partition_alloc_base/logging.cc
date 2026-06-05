22f3cc1187780aca913ac309c06e6d40fef29691
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-06-04 20:24:07 -0700

    partition_alloc: fix outdated circular-dependency header guards
    Restore the safety circular-dependency check by updating it to use the
    current correct header guards.
    
    The old checks used outdated //base guards that were never defined since
    the files moved to partition_alloc, making the check silently dead.
    
    This is a follow-up of CL: https://crrev.com/c/7876007
    
    Bug: 351867706
    Change-Id: I9f825ec87158114c42ddc7436bc3c337e8d3216f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871104
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642115}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/logging.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/logging.cc
@@ -7,13 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "partition_alloc/partition_alloc_base/compiler_specific.h"
 
-// TODO(crbug.com/40158212): After finishing copying //base files to PA library,
-// remove defined(BASE_CHECK_H_) from here.
-#if defined(                                                                                 \
-    BASE_ALLOCATOR_PARTITION_ALLOCATOR_SRC_PARTITION_ALLOC_PARTITION_ALLOC_BASE_CHECK_H_) || \
-    defined(BASE_CHECK_H_) ||                                                                \
-    defined(                                                                                 \
-        BASE_ALLOCATOR_PARTITION_ALLOCATOR_SRC_PARTITION_ALLOC_PARTITION_ALLOC_CHECK_H_)
+#if defined(PARTITION_ALLOC_PARTITION_ALLOC_BASE_CHECK_H_) || \
+    defined(PARTITION_ALLOC_PARTITION_ALLOC_CHECK_H_)
 #error "logging.h should not include check.h"
 #endif
 
