12774062928a2e1aefdb9f9c2e6f2b2047c4b51f
Kevin Graney <kmg@google.com>
2026-06-05 10:15:57 -0700

    Remove Private State Tokens C++ handler and Mojo interface
    This removes the C++ handler, Mojo interface, and associated files for the
    Private State Tokens subpage of Privacy Sandbox Internals, completing the
    cleanup started in the previous CL.
    
    Bug: 520365265
    Change-Id: I762b8f54dd313cb84b161c89d18d9a9a77341c3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903112
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1642422}

--- chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.cc
+++ chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "base/feature_list.h"
-#include "chrome/browser/ui/webui/privacy_sandbox/private_state_tokens/private_state_tokens_handler.h"
 #include "chrome/browser/ui/webui/privacy_sandbox/related_website_sets/related_website_sets_handler.h"
 #include "chrome/browser/ui/webui/sanitized_image/sanitized_image_source.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
@@ -35,7 +34,6 @@ namespace privacy_sandbox_internals {
 using ::privacy_sandbox_internals::mojom::Page;
 using ::privacy_sandbox_internals::mojom::PageHandler;
 #if !BUILDFLAG(IS_ANDROID)
-using ::private_state_tokens::mojom::PrivateStateTokensPageHandler;
 using ::related_website_sets::mojom::RelatedWebsiteSetsPageHandler;
 #endif
 
@@ -72,15 +70,6 @@ void PrivacySandboxInternalsUI::BindInterface(
         related_website_sets::mojom::RelatedWebsiteSetsPageHandler> receiver) {
   if (base::FeatureList::IsEnabled(privacy_sandbox::kRelatedWebsiteSetsDevUI)) {
     related_website_sets_handler_ = std::make_unique<RelatedWebsiteSetsHandler>(
-        web_ui(), std::move(receiver));
-  }
-}
-
-void PrivacySandboxInternalsUI::BindInterface(
-    mojo::PendingReceiver<
-        private_state_tokens::mojom::PrivateStateTokensPageHandler> receiver) {
-  if (base::FeatureList::IsEnabled(privacy_sandbox::kPrivateStateTokensDevUI)) {
-    private_state_tokens_handler_ = std::make_unique<PrivateStateTokensHandler>(
         web_ui(), std::move(receiver));
   }
 }
--- chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.h
+++ chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.h
@@ -12,13 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/webui_config.h"
 #include "content/public/common/url_constants.h"
 #include "ui/webui/mojo_web_ui_controller.h"
-
 #if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/ui/webui/privacy_sandbox/private_state_tokens/private_state_tokens.mojom.h"
 #include "chrome/browser/ui/webui/privacy_sandbox/related_website_sets/related_website_sets.mojom.h"
 #endif
 
-class PrivateStateTokensHandler;
 class RelatedWebsiteSetsHandler;
 
 namespace privacy_sandbox_internals {
@@ -54,16 +51,12 @@ class PrivacySandboxInternalsUI : public ui::MojoWebUIController {
       mojo::PendingReceiver<
           related_website_sets::mojom::RelatedWebsiteSetsPageHandler> receiver);
 
-  void BindInterface(
-      mojo::PendingReceiver<
-          private_state_tokens::mojom::PrivateStateTokensPageHandler> receiver);
 #endif
 
  private:
   std::unique_ptr<PrivacySandboxInternalsHandler> handler_;
 #if !BUILDFLAG(IS_ANDROID)
   std::unique_ptr<RelatedWebsiteSetsHandler> related_website_sets_handler_;
-  std::unique_ptr<PrivateStateTokensHandler> private_state_tokens_handler_;
 #endif
 
   WEB_UI_CONTROLLER_TYPE_DECL();
