4c410082b97f4f823e720647d4173d7fcead091d
Jesse McKenna <jessemckenna@google.com>
2026-06-04 18:33:09 -0700

    etw-stackwalk: add simple stack walk events
    This change adds support for StackWalk Event Tracing for Windows (ETW)
    events to Chrome's ETW consumer.
    
    StackWalk events are not yet enabled, so this CL will not yet change
    Chrome tracing's behavior. This is the first part of a sequence of CLs
    to enable StackWalk events.
    
    This is based on spvm@'s original prototype at
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7598112
    
    Bug: 400769265
    Change-Id: I0aeccd860eaadcc910ac4e2583f32f982dde537b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7794326
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Sean Maher <spvm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642073}

--- components/tracing/common/etw_system_data_source_win.h
+++ components/tracing/common/etw_system_data_source_win.h
@@ -45,6 +45,8 @@ class TRACING_EXPORT EtwSystemDataSource
   void OnSetup(const SetupArgs&) override;
   void OnStart(const StartArgs&) override;
   void OnStop(const StopArgs&) override;
+  void WillClearIncrementalState(
+      const ClearIncrementalStateArgs& args) override;
 
  private:
   std::unique_ptr<perfetto::TraceWriterBase> CreateTraceWriter();
