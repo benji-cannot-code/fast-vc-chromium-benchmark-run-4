86b5a446491c8e9d845da50ef4c331ae5c7d31f6
Jonathan Ross <jonross@chromium.org>
2026-06-05 10:37:26 -0700

    Remove kScrollEventDispatchModeDispatchScrollEventsUntilDeadline
    This mode is no longer wanted. Removing the flag and its specific
    implementation in cc and blink.
    The default mode for WaitForLateScrollEvents is reverted to
    DispatchScrollEventsImmediately.
    
    Change-Id: I4d68ee3271dff52746591b4592249de6a6148b38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904155
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Aman Verma <amanvr@google.com>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642448}

--- cc/input/input_handler.cc
+++ cc/input/input_handler.cc
@@ -65,11 +65,6 @@ InputHandlerClient::ScrollEventDispatchMode GetScrollEventDispatchMode() {
                  kScrollEventDispatchModeUseScrollPredictorForDeadline) {
     return InputHandlerClient::ScrollEventDispatchMode::
         kUseScrollPredictorForDeadline;
-  } else if (mode_name ==
-             ::features::
-                 kScrollEventDispatchModeDispatchScrollEventsUntilDeadline) {
-    return InputHandlerClient::ScrollEventDispatchMode::
-        kDispatchScrollEventsUntilDeadline;
   }
 
   return InputHandlerClient::ScrollEventDispatchMode::kEnqueueScrollEvents;
