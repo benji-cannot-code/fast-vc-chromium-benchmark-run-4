f151c6bd6a124339d6b702ea301c4d465a65fbbb
Darryl James <dljames@chromium.org>
2026-06-05 14:07:14 -0700

    Revert "Migrate ExtensionDevToolsInfoBarDelegate to messages on desktop Android"
    This reverts commit ce0d2ab634b5c5687ef779e58a098ede04917b22.
    
    Reason for revert: Caused a tree closure
    Original change's description:
    > Migrate ExtensionDevToolsInfoBarDelegate to messages on desktop Android
    >
    > Android uses the "messages" UI pattern for warnings and notifications.
    > The "infobar" UI has long been deprecated on Android and the code is
    > being removed from the code base.
    >
    > Switch the extensions debugger API warning "Foo started debugging this
    > browser" to use messages. Add ExtensionDevToolsMessagesDelegate to
    > compliment ExtensionDevToolsInfoBarDelegate on Win/Mac/Linux. Add an
    > android_browsertest to ensure the new UI is triggered and closed
    > properly.
    >
    > Screenshots from manual tests are in the bug.
    >
    > Bug: 518840663
    > Change-Id: Ib454977289f465cbca45cd24ab9f9fba3fd47cd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903357
    > Reviewed-by: Lijin Shen <lazzzis@google.com>
    > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1642553}
    
    Bug: 518840663
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5f3289b6c85c449dd2d9aacf0e88bdded5d4be19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904472
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Darryl James <dljames@chromium.org>
    Owners-Override: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642583}

--- chrome/browser/extensions/api/debugger/extension_dev_tools_infobar_delegate.cc
+++ chrome/browser/extensions/api/debugger/extension_dev_tools_infobar_delegate.cc
@@ -24,9 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/text_constants.h"
 #include "ui/strings/grit/ui_strings.h"
 
-// Android uses messages instead of infobars.
-static_assert(!BUILDFLAG(IS_ANDROID));
-
 namespace extensions {
 
 namespace {
--- chrome/browser/extensions/api/debugger/extension_dev_tools_infobar_delegate.h
+++ chrome/browser/extensions/api/debugger/extension_dev_tools_infobar_delegate.h
@@ -17,8 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_id.h"
 
-// Android uses messages instead of infobars.
-static_assert(!BUILDFLAG(IS_ANDROID));
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 class GlobalConfirmInfoBar;
 
