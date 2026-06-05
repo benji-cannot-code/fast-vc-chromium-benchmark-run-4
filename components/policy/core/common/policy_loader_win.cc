055dc3a4ee4fbbab4b54e21bae0d780284c5f20d
Erik Anderson <Erik.Anderson@microsoft.com>
2026-06-04 21:49:51 -0700

    Remove unneeded shlwapi.h include in policy_loader_win.cc
    The PathIsUNC call was removed in 2017 via
    https://crrev.com/c/756707.
    
    Change-Id: I55cbcd42bc19f2ce61b3c62e6e60530ba63ab479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903748
    Commit-Queue: Anqing Zhao <anqing@chromium.org>
    Auto-Submit: Erik Anderson <Erik.Anderson@microsoft.com>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642142}

--- components/policy/core/common/policy_loader_win.cc
+++ components/policy/core/common/policy_loader_win.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/scoped_thread_priority.h"
 #include "base/values.h"
-#include "base/win/shlwapi.h"  // For PathIsUNC()
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
 #include "components/policy/core/common/async_policy_loader.h"
