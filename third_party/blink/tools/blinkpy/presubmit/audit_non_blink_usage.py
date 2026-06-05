f99af4871882077efc0b0d9cba6d24b930ce08b1
Andres Ricardo Perez <andresrperez@chromium.org>
2026-06-05 13:54:25 -0700

    [Memory Coordinator] Add traits to UniqueFontSelector
    Updates UniqueFontSelector to explicitly define its memory coordination
    semantics using MemoryConsumerTraits. This provides the global Memory
    Coordinator with accurate information regarding the
    scale, eviction cost, and statefulness of the cache to optimize
    system-wide memory reclamation decisions.
    
    Bug: 489671163
    Change-Id: Ifc7d235498efeddd6500a88f214b6071bc0b6bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7844183
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642579}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -124,6 +124,7 @@ _CONFIG = [
             'base::MatchPattern',
             'base::MaxThreadType',
             'base::MemoryConsumer',
+            'base::MemoryConsumerTraits',
             'base::MemoryPressureListener',
             'base::MemoryPressureListenerTag',
             'base::MessagePump',
