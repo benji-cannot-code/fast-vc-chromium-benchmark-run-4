70d35df32a0e113cafb1bdb7da81519e2f451b78
Nidhi Jaju <nidhijaju@chromium.org>
2026-06-04 18:42:20 -0700

    Revert "settings: Add separate sync card for skills entry point"
    This reverts commit 3c90fe0cad56872f526cc220e8ccf44a4132b7ae.
    
    Reason for revert: Causing test failures for SettingsTest.PeoplePageSyncControls
    i.e. https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/58551/test-results
    
    Original change's description:
    > settings: Add separate sync card for skills entry point
    >
    > Add an informational card at the bottom of the Chrome Sync Settings page
    > (chrome://settings/syncSetup) stating that skills are managed separately
    > from Chrome sync, including a button that navigates the user to the
    > Skills settings subpage.
    >
    > Demo: https://screencast.googleplex.com/cast/NTU3Mjk1NTk0NDc3OTc3NnxiMThjMTVjOC05MA
    > Bug: 513332678
    > Change-Id: I6524390514cdddc3b0fac734c934cf7d2ea2a75d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896632
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Howard Chang <howardcha@google.com>
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1641914}
    
    Bug: 513332678
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib201ac793767bc2cc7a655139e1230be4aca82fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901857
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642079}

--- chrome/browser/ui/signin/account_settings_page_pixel_browsertest.cc
+++ chrome/browser/ui/signin/account_settings_page_pixel_browsertest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "components/signin/public/identity_manager/signin_constants.h"
-#include "components/skills/features.h"
 #include "components/sync/base/features.h"
 #include "components/sync/test/test_sync_service.h"
 #include "content/public/test/browser_test.h"
@@ -40,7 +39,7 @@ class AccountSettingsPagePixelBrowserTest : public InteractiveBrowserTest {
     feature_list_.InitWithFeatures(
         /*enabled_features=*/{syncer::kReplaceSyncPromosWithSignInPromos,
                               syncer::kUnoPhase2FollowUp},
-        /*disabled_features=*/{features::kSkillsEnabled});
+        /*disabled_features=*/{});
   }
 
   void SetUpBrowserContextKeyedServices(
