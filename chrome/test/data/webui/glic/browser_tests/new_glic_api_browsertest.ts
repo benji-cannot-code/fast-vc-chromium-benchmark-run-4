13cf30ad97e1ff452bb36d42611a81a1a5544062
Dan Harrington <harringtond@chromium.org>
2026-06-05 16:22:11 -0700

    Fix issue where openPanelInfo.canUserResize was ignored
    Bug: 520306661
    Change-Id: Iec010ad905df89139c75cc8b92073dee6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904469
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642670}

--- chrome/test/data/webui/glic/browser_tests/new_glic_api_browsertest.ts
+++ chrome/test/data/webui/glic/browser_tests/new_glic_api_browsertest.ts
@@ -1030,11 +1030,6 @@ class SkillsApiTests extends ApiTests {
     this.host.showManageSkillsUi();
   }
 
-  async testEnableDragResize() {
-    assertDefined(this.host.enableDragResize);
-    await this.host.enableDragResize(true);
-  }
-
   async testDisableDragResize() {
     assertDefined(this.host.enableDragResize);
     await this.host.enableDragResize(false);
@@ -1157,6 +1152,11 @@ class InitiallyNotResizableTest extends ApiTestFixtureBase {
 
   async testInitiallyNotResizable() {
     await sleep(100);
+  }
+
+  async testEnableDragResize() {
+    assertDefined(this.host.enableDragResize);
+    await this.host.enableDragResize(true);
   }
 }
 
