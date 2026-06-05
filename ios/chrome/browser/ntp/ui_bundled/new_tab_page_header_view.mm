149c71c2d4c82b409bca4296ceb83ada02c3d0c2
Eric Ekey <ericekey@google.com>
2026-06-05 08:41:49 -0700

    [iOS][AppBar] Fix Tools Menu Button Flicker on Rotation
    When rotating from landscape to portrait, the Tools menu button in the
    NTP header would flicker. This is because on size class changes it was
    being removed from the view hierarchy and recreated. This change ensures
    that the button is only removed from the hierarchy when needed. This
    affects iPhone.
    
    Fixed: 520354721
    Change-Id: I2b5d9ca79e73d88b8f509334cb963451d7264a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905496
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1642369}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -427,7 +427,9 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
   [self addSeparatorToSearchField:self.fakeOmniboxContainer];
   [self addCustomizationMenu];
   if (IsChromeNextIaEnabled()) {
-    [self addToolsMenuIfNeeded];
+    if (!CanShowTabStrip(self) && !IsRegularXRegularSizeClass(self)) {
+      [self addToolsMenu];
+    }
   }
 }
 
@@ -1186,8 +1188,10 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
 
 - (void)setToolsMenuButton:(UIButton*)toolsMenuButton {
   CHECK(IsChromeNextIaEnabled());
-  if (_toolsMenuButton) {
+  if (!toolsMenuButton) {
     [_toolsMenuButton removeFromSuperview];
+    _toolsMenuButton = nil;
+    return;
   }
 
   if (IsNTPBackgroundCustomizationEnabled()) {
@@ -1443,11 +1447,12 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
 }
 
 // Creates the Tools menu and adds it to the header view.
-- (void)addToolsMenuIfNeeded {
+- (void)addToolsMenu {
   CHECK(IsChromeNextIaEnabled());
-  // If the Tools menu button is always visible in the toolbar (iPad), the Tools
-  // menu should not be added to the header view.
-  if (CanShowTabStrip(self)) {
+  CHECK(!IsRegularXRegularSizeClass(self));
+  CHECK(!CanShowTabStrip(self));
+  if (self.toolsMenuButton.superview) {
+    // Tools menu button is already in the view hierarchy.
     return;
   }
 
@@ -1831,6 +1836,7 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
   BOOL preferredContentSizeCategoryChanged =
       previousTraitCollection.preferredContentSizeCategory !=
       self.traitCollection.preferredContentSizeCategory;
+
   if (horizontalSizeClassChanged || preferredContentSizeCategoryChanged) {
     [self updateFakeboxDisplay];
   }
@@ -1840,7 +1846,13 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
         previousTraitCollection.verticalSizeClass !=
             self.traitCollection.verticalSizeClass) {
       [self resetSplitToolbarResizing];
-      [self addToolsMenuIfNeeded];
+      if (!CanShowTabStrip(self) && !IsRegularXRegularSizeClass(self)) {
+        [self addToolsMenu];
+      } else {
+        // If the Tools menu button is always visible in the toolbar (iPad), the
+        // Tools menu should not be in the header.
+        self.toolsMenuButton = nil;
+      }
     }
   }
 
