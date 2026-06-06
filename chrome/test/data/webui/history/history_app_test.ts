4f073ea57b61928e550310b5019364cb058aeb3e
Mickey Burks <mickeyburks@chromium.org>
2026-06-06 11:55:56 -0700

    [GlowUp] Fix flaky HistoryAppTests
    These tests append the history-app component multiple times, causing
    connectedCallback to be fired multiple times. This is not how the
    application works in the wild, which is why they fail. The assert is
    failing because connectedCallback has a side effect in this experiment.
    The assertion forces correct usage of the component, so no change is
    needed in history-app. This fix ensures that the body is cleared out
    between multiple `document.body.appendChild` calls.
    
    Change-Id: I85394940f049d053c86bab757868f22c3c822388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905053
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642820}

--- chrome/test/data/webui/history/history_app_test.ts
+++ chrome/test/data/webui/history/history_app_test.ts
@@ -328,6 +328,7 @@ suite('HistoryAppTest', function() {
 
     // Recreate the app with the promo enabled.
     loadTimeData.overrideValues({maybeShowEmbeddingsIph: true});
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
     element = document.createElement('history-app');
     document.body.appendChild(element);
     await microtasksFinished();
