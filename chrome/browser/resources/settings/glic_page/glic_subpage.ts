f79700a5e521f67ccfd4115ed7fd19edeb1e46ca
rosiey <rosiey@google.com>
2026-06-05 18:32:26 -0700

    [glic] Disable experimental triggering when web actuation is disabled
    When a user disables "Actuation on Web", automatically disable the
    experimental triggering setting in the backend.
    
    In the settings WebUI, disable the experimental triggering toggle if
    "Actuation on Web" is disabled or disabled by enterprise policy.
    
    Result: http://shortn/_yZfyvqVOnH
    Bug: b:519247283
    Change-Id: I545c7dc0efd61951d9c44e4e74d545ae244508a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7906319
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642709}

--- chrome/browser/resources/settings/glic_page/glic_subpage.ts
+++ chrome/browser/resources/settings/glic_page/glic_subpage.ts
@@ -788,6 +788,12 @@ export class SettingsGlicSubpageElement extends SettingsGlicSubpageElementBase {
   private onWebActuationToggleVisibilityChanged_(visible: boolean) {
     this.webActuationFeatureEnabled_ = visible;
   }
+
+  private isExperimentalTriggeringDisabled_(
+      webActuationEnabled: boolean,
+      isWebActuationDisabledForEnterprise: boolean): boolean {
+    return !webActuationEnabled || isWebActuationDisabledForEnterprise;
+  }
 }
 
 declare global {
