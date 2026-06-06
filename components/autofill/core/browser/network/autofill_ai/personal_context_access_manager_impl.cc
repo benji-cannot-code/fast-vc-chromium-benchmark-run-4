cf54fca0ccd85d614487a2ff56464474985a3fbe
Norge Vizcay <vizcay@google.com>
2026-06-06 03:18:16 -0700

    Guard prefetch with enablement checks
    Ensure PrefetchAmbientAutofillContext only executes when the
    kAutofillAmbientAutofill feature flag is enabled and the enablement
    state is an enabled state (either kEnabled or kEnabledShouldShowNotice).
    
    Bug: 516721244
    Change-Id: Ie6b4da9c0051ef3420a387c54bc43de5ada8ce56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900633
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642783}

--- components/autofill/core/browser/network/autofill_ai/personal_context_access_manager_impl.cc
+++ components/autofill/core/browser/network/autofill_ai/personal_context_access_manager_impl.cc
@@ -11,11 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_deref.h"
 #include "base/containers/to_vector.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/task/single_thread_task_runner.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_type.h"
 #include "components/autofill/core/browser/network/autofill_ai/personal_context_conversion_util.h"
+#include "components/autofill/core/common/autofill_features.h"
+#include "components/personal_context/core/personal_context_enablement_service.h"
 #include "components/personal_context/core/personal_context_service.h"
 #include "components/personal_context/core/personal_context_types.h"
 #include "components/personal_context/proto/features/ambient_autofill.pb.h"
@@ -49,6 +52,26 @@ ExtractEntitiesFromResponse(const std::string& serialized_response) {
   return entities;
 }
 
+bool IsPrefetchAmbientAutofillContextEnabled(
+    personal_context::PersonalContextEnablementService& enablement_service) {
+  if (!base::FeatureList::IsEnabled(features::kAutofillAmbientAutofill)) {
+    return false;
+  }
+
+  using personal_context::PersonalContextEnablementState;
+
+  switch (enablement_service.GetEnablementState()) {
+    case PersonalContextEnablementState::kDisabledNotEligible:
+    case PersonalContextEnablementState::kDisabledNeedsOptIn:
+    case PersonalContextEnablementState::
+        kDisabledViaPersonalIntelligenceInAutofillToggle:
+      return false;
+    case PersonalContextEnablementState::kEnabled:
+    case PersonalContextEnablementState::kEnabledShouldShowNotice:
+      return true;
+  }
+}
+
 }  // namespace
 
 PersonalContextAccessManagerImpl::PersonalContextAccessManagerImpl(
@@ -63,6 +86,11 @@ PersonalContextAccessManagerImpl::~PersonalContextAccessManagerImpl() = default;
 
 void PersonalContextAccessManagerImpl::PrefetchAmbientAutofillContext(
     base::span<const EntityType> requested_types) {
+  if (!IsPrefetchAmbientAutofillContextEnabled(
+          *personal_context_enablement_service_)) {
+    return;
+  }
+
   std::vector<EntityType> non_cached_requested_types;
   for (const EntityType& type : requested_types) {
     if (!IsTypeCached(type.name())) {
