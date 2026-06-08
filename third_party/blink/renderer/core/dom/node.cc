4f543aa849e82fe19d4ceb29bfcaec54529eae34
Vladimir Levin <vmpstr@chromium.org>
2026-06-08 10:47:32 -0700

    overscroll: Fix a detach crash in shadow tree manipulation
    When we're re-slotting children, and are changing flat tree things
    we should ensure to detach overscroll areas, otherwise that can result
    in a crash.
    
    R=flackr@chromium.org
    
    Change-Id: I7ffe079ae228f435f3ff794f54c6baae579949c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902633
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643316}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -3711,6 +3711,9 @@ void Node::ParentSlotChanged() {
 
 void Node::FlatTreeParentChanged() {
   DCHECK(isConnected());
+  if (Element* element = DynamicTo<Element>(this)) {
+    element->DetachOverscroll();
+  }
   const ComputedStyle* style =
       IsElementNode() ? To<Element>(this)->GetComputedStyle() : nullptr;
   bool detach = false;
@@ -3766,6 +3769,9 @@ void Node::FlatTreeParentChanged() {
 }
 
 void Node::RemovedFromFlatTree() {
+  if (Element* element = DynamicTo<Element>(this)) {
+    element->DetachOverscroll();
+  }
   StyleEngine& engine = GetDocument().GetStyleEngine();
   StyleEngine::DetachLayoutTreeScope detach_scope(engine);
   // This node was previously part of the flat tree, but due to slot re-
