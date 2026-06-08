9d8b3151f490ba3e6906e3fed17f9c1fd190b1b8
Andrii Natiahlyi <natiahlyi@google.com>
2026-06-08 08:38:09 -0700

    [Password Manager] Fix password sharing IPH registration race
    crrev.com/c/7677205 changed syncInfo_ initialization from undefined to
    null. This caused isSyncingPasswords to evaluate to false synchronously,
    skipping the IPH anchor registration during connectedCallback.
    
    Fix this by converting maybeRegisterSharingHelpBubble_ to a Polymer
    complex observer to handle the async sync state resolution.
    
    Fixed: 494149175
    Change-Id: I8375d9bc79e5fc17cea881da68179dd41ab481f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899682
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643227}

--- chrome/test/data/webui/password_manager/password_details_section_test.ts
+++ chrome/test/data/webui/password_manager/password_details_section_test.ts
@@ -424,8 +424,7 @@ suite('PasswordDetailsSectionTest', function() {
       });
 
   // <if expr="_google_chrome">
-  // TODO(crbug.com/494149175): Re-enable after fixing.
-  test.skip(
+  test(
       'Register password sharing IPH for password card', async function() {
         syncProxy.syncInfo = {
           isSyncingPasswords: true,
