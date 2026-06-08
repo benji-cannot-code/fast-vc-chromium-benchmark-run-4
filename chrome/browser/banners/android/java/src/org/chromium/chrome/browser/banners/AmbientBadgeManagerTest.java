59adb108ba37434f8b3d6508f800a15be2729164
Huili Liu <huilliu@microsoft.com>
2026-06-07 19:42:44 -0700

    [Android] fix memory leak of AmbientBadgeManagerTest
    Bug: 512491794
    Change-Id: I9b159eee13e304c5573154e70398d86cf10df722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901458
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: huili liu <huilliu@microsoft.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642972}

--- chrome/browser/banners/android/java/src/org/chromium/chrome/browser/banners/AmbientBadgeManagerTest.java
+++ chrome/browser/banners/android/java/src/org/chromium/chrome/browser/banners/AmbientBadgeManagerTest.java
@@ -27,6 +27,7 @@ import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiSelector;
 
 import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -43,7 +44,6 @@ import org.chromium.base.task.TaskTraits;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableLeakChecks;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -88,10 +88,6 @@ import java.util.Observer;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 // TODO(http://crbug.com/495529795): Enable side panel and fix this test.
 @DisableFeatures({ChromeFeatureList.ENABLE_ANDROID_SIDE_PANEL})
-@DisableLeakChecks({
-    "crbug.com/512491129 (AppBannerManager)",
-    "crbug.com/512491794 (AppBannerManager)"
-})
 public class AmbientBadgeManagerTest {
     @Rule
     public FreshCtaTransitTestRule mTabbedActivityTestRule =
@@ -206,6 +202,15 @@ public class AmbientBadgeManagerTest {
         AppBannerManager.setOverrideSegmentationResultForTesting(true);
         mTestServer = mTabbedActivityTestRule.getTestServer();
         mUiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+    }
+
+    @After
+    public void tearDown() {
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    AppBannerManager.setAppDetailsDelegate(null);
+                });
+        mDetailsDelegate = null;
     }
 
     private AppBannerManager getAppBannerManager(WebContents webContents) {
@@ -376,7 +381,8 @@ public class AmbientBadgeManagerTest {
                         .expectIntRecord(
                                 "Webapp.Install.InstallEvent",
                                 WebappInstallSource.AMBIENT_BADGE_BROWSER_TAB)
-                        .expectIntRecord(INSTALL_PATH_HISTOGRAM_NAME, 1 /* kAmbientInfobar */)
+                        .expectIntRecord(
+                                INSTALL_PATH_HISTOGRAM_NAME, /* value= */ 1) // kAmbientInfobar
                         .build();
 
         triggerInstallWebApp(
@@ -400,7 +406,8 @@ public class AmbientBadgeManagerTest {
                         .expectIntRecord(
                                 "Webapp.Install.InstallEvent",
                                 WebappInstallSource.AMBIENT_BADGE_CUSTOM_TAB)
-                        .expectIntRecord(INSTALL_PATH_HISTOGRAM_NAME, 1 /* kAmbientInfobar */)
+                        .expectIntRecord(
+                                INSTALL_PATH_HISTOGRAM_NAME, /* value= */ 1) // kAmbientInfobar
                         .build();
 
         mCustomTabActivityTestRule.startCustomTabActivityWithIntent(
