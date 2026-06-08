9be9d63993e4eaaaa460fa5198c4c64bc807e1ad
Devon Loehr <dloehr@google.com>
2026-06-08 11:05:41 -0700

    [rustmojo] Unify Remotes and AssociatedRemotes
    This CL refactors the `Remote` class to abstract over whether the remote
    is primary (`Remote`) or associated (`AssociatedRemote`). The two
    provide almost exactly the same API to the user, the only real
    difference is whether they hold a MultiplexRouter directly, or a wrapper
    around it.
    
    The main changes in this CL are:
    1. Adding a generic parameter to the Remote type indicating whether it's
       primary or associated. This is similar to how we handle pending
       endpoints.
    2. Changing the `MessagePipeWatcher` to a `MultiplexRouterHandle`.
       Logically, both associated and primary remotes hold one, but the
       associated version has some wrapper types, so we use a trait object.
    3. Adjusting the rest of the code to accommodate the differences between
       the watcher and router types.
    
    Note that `AssociatedRemote`s don't exist yet; this CL paves the way for
    them.
    
    Bug: 493274453
    Change-Id: Ifae837d207eded13380cdcf193ce37dc2bab7c89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897364
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643330}

--- mojo/public/rust/bindings/interface.rs
+++ mojo/public/rust/bindings/interface.rs
@@ -175,6 +175,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 use crate::message::MojomMessage;
 
+pub use crate::multiplex_router::ResponseSender;
+
 /// This trait abstracts over the parts of individual Mojom `interface`s, such
 /// as `MathService`. This trait is what's used by generic `Remote`s and
 /// `Receiver`s to handle incoming and outgoing messages.
@@ -196,7 +198,11 @@ pub trait MojomInterface {
     /// For use in `Remote`s. Takes a message with a parsed header, examines
     /// it to determine which message type it contains, ensures the callback
     /// has the right type, then invokes it on the parsed message body.
-    fn handle_incoming_response(message: MojomMessage, response_callback: Self::ResponseCallbackTy);
+    fn handle_incoming_response(
+        message: MojomMessage,
+        response_sender: ResponseSender,
+        response_callback: Self::ResponseCallbackTy,
+    );
 
     /// For use in `Receiver`s. Takes a message with a parsed header, examines
     /// it to determine which message type it contains, then invokes the
@@ -228,7 +234,10 @@ pub mod internal {
     // interface traits. A blanket implementation is fine because all the
     // `Remote` code is auto-generated, so there's nothing for a user to get
     // wrong.
-    impl<T> ImplementThisViaMacro for crate::remote::Remote<T> where T: DynMojomInterface + ?Sized {}
+    impl<T, Marker> ImplementThisViaMacro for crate::remote::GenericRemote<T, Marker> where
+        T: DynMojomInterface + ?Sized
+    {
+    }
 }
 
 /// This macro sets up some behind-the-scenes implementations that mojom state
@@ -260,6 +269,7 @@ macro_rules! register_mojom_state_object_impls {
             type ResponseCallbackTy = ();
             fn handle_incoming_response(
                 _message: $crate::message::MojomMessage,
+                _response_sender: $crate::interface::ResponseSender,
                 _response_callback: Self::ResponseCallbackTy,
             ) {
                 panic!("Receivers never get responses")
