c715ee8aa6864294b6149ac7aa7fbfb2619e89b8
Elly <ellyjones@chromium.org>
2026-06-08 14:19:07 -0700

    variations: CHECK() on static public key parse errors
    The public key is hardcoded in the binary, so if it fails to parse,
    something is seriously wrong.
    
    This removes the only code path that could return kInvalidSignature,
    so this change removes that enum value and marks the corresponding
    histogram enum value as obsolete. It also renames the kInvalidSeed
    enum value to kInvalidSeedSignature to reflect its actual meaning.
    
    Bug: None
    Change-Id: I1223346318572db5d4c4eed6354b6157df15e188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903332
    Auto-Submit: Elly <ellyjones@chromium.org>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643458}

--- components/variations/variations_seed_store_unittest.cc
+++ components/variations/variations_seed_store_unittest.cc
@@ -2444,7 +2444,7 @@ INSTANTIATE_TEST_SUITE_P(
                 .signature = TestSeedData().base64_signature,
                 .store_seed_result = StoreSeedResult::kFailedSignature,
                 .verify_signature_result =
-                    VerifySignatureResult::kInvalidSeed}),
+                    VerifySignatureResult::kInvalidSeedSignature}),
         ::testing::Values(kSeedFilesGroup,
                           kControlGroup,
                           kDefaultGroup,
@@ -3120,8 +3120,9 @@ TEST_P(LoadSeedDataAllGroupsTest, VerifySeedSignatureSignatureDoesNotMatch) {
 
   base::HistogramTester histogram_tester;
   ASSERT_FALSE(MakeSeedStoreLoadStoredSeed(seed_store));
-  histogram_tester.ExpectUniqueSample("Variations.LoadSeedSignature",
-                                      VerifySignatureResult::kInvalidSeed, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Variations.LoadSeedSignature",
+      VerifySignatureResult::kInvalidSeedSignature, 1);
 }
 
 TEST_P(LoadSeedDataAllGroupsTest, VerifySeedSignatureSeedDoesNotMatch) {
@@ -3140,8 +3141,9 @@ TEST_P(LoadSeedDataAllGroupsTest, VerifySeedSignatureSeedDoesNotMatch) {
 
   base::HistogramTester histogram_tester;
   ASSERT_FALSE(MakeSeedStoreLoadStoredSeed(seed_store));
-  histogram_tester.ExpectUniqueSample("Variations.LoadSeedSignature",
-                                      VerifySignatureResult::kInvalidSeed, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Variations.LoadSeedSignature",
+      VerifySignatureResult::kInvalidSeedSignature, 1);
 }
 
 class VariationsSeedStoreTestAllGroups
