bf88d459941e5a9b36c4cfbf099db2a0e3bc9cef
Dan Polanco <polardz@google.com>
2026-06-08 12:21:22 -0700

    [IA][BottomBar] Add promo dialog UI resources and assets
    Adds vector illustrations, XML layout definitions for the new bottom bar
    promo dialog.
    
    Bug: 517591009, 519148302
    Change-Id: I1be190f8f66e52707ba260248412377d59def154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902769
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Evan Luo <evanluo@google.com>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643392}

---
+++ chrome/browser/ui/android/bottombar/java/src/org/chromium/chrome/browser/ui/bottombar/BottomBarPromoDialogCoordinator.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.ui.bottombar;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Coordinator for the bottom bar introductory promo dialog. */
+@NullMarked
+public class BottomBarPromoDialogCoordinator {
+    // TODO(crbug.com/517591009): Remove when implementing logic.
+    @SuppressWarnings("unused")
+    private final View mDialogView;
+
+    public BottomBarPromoDialogCoordinator(Context context) {
+        mDialogView =
+                LayoutInflater.from(context).inflate(R.layout.bottom_bar_promo_dialog_view, null);
+    }
+}
