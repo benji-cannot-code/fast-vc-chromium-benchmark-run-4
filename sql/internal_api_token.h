b23edfc2cae6ac02b8e979c0e983309b465904b1
Jean-Philippe Gravel <jpgravel@chromium.org>
2026-06-08 16:28:52 -0700

    Improve WAL file expectations in database_unittest.cc
    These tests were validating WAL checkpoints by making assumptions on
    how SQLite write to the disk, assuming that certain changes always
    results in the files increasing by the size of one page for instance.
    A better approach is to directly query the number of uncheckedpointed
    pages and check that it decreases when checkpointing.
    
    Bug: 496096535
    Change-Id: Ie16d59ac3638977ceca801d24017440e620fb64c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903122
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643520}

--- sql/internal_api_token.h
+++ sql/internal_api_token.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SQL_INTERNAL_API_TOKEN_H_
 
 #include "base/gtest_prod_util.h"
+#include "base/types/expected.h"
 
 namespace base {
 class FilePath;
@@ -14,9 +15,12 @@ class FilePath;
 
 namespace sql {
 
+class Database;
+
 namespace test {
 struct ColumnInfo;
 bool CorruptSizeInHeader(const base::FilePath&);
+base::expected<int, int> GetUncheckpointedFrameCount(const Database& db);
 }  // namespace test
 
 // Restricts access to APIs internal to the //sql package.
@@ -33,6 +37,8 @@ class InternalApiToken {
   friend class Transaction;
   friend struct test::ColumnInfo;
   friend bool test::CorruptSizeInHeader(const base::FilePath&);
+  friend base::expected<int, int> test::GetUncheckpointedFrameCount(
+      const Database& db);
 
   FRIEND_TEST_ALL_PREFIXES(DatabaseDiskFullTest, SqliteFullAbortsTransactions);
   FRIEND_TEST_ALL_PREFIXES(DatabaseDiskFullTest,
--- sql/test/test_helpers.h
+++ sql/test/test_helpers.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/strings/cstring_view.h"
+#include "base/types/expected.h"
 #include "sql/database.h"
 
 // Collection of test-only convenience functions.
@@ -139,6 +140,11 @@ struct ColumnInfo {
   // True if the column is AUTOINCREMENT.
   bool is_auto_incremented;
 };
+
+// Returns the number of frames in `db`'s write-ahead log file that have not yet
+// been checkpointed into the main database file or an extended SQLite error
+// code on failure.
+base::expected<int, int> GetUncheckpointedFrameCount(const Database& db);
 
 }  // namespace sql::test
 
