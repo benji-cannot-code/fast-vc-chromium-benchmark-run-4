167918ffca4312fb95167e6b9a24d0e884a1a457
Troy Wang <troywang@google.com>
2026-06-08 10:34:16 -0700

    Creates a temporary StatisticsRecorder object in unit test.
    By such, logging of UMA histogram is sent to the test instance of
    StatisticsRecorder instead of the prod one.
    
    Bug: 483013446
    
    Change-Id: Ibfff58ea82e5d6bb7004df6d8662f63be9e2c89a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876487
    Reviewed-by: Eric Lok <lokeric@google.com>
    Commit-Queue: Troy Wang <troywang@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Troy Wang <troywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643310}

--- chrome/browser/android/metrics/android_atoms_logger_unittest.cc
+++ chrome/browser/android/metrics/android_atoms_logger_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/fixed_flat_map.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/statistics_recorder.h"
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
@@ -57,6 +58,9 @@ class AndroidAtomsLoggerTest : public testing::Test {
   };
 
   void SetUp() override {
+    statistics_recorder_ =
+        base::StatisticsRecorder::CreateTemporaryForTesting();
+
     TestingPrefServiceSimple* local_state =
         TestingBrowserProcess::GetGlobal()->GetTestingLocalState();
     DCHECK(local_state);
@@ -67,8 +71,11 @@ class AndroidAtomsLoggerTest : public testing::Test {
     }
   }
 
+  void TearDown() override { statistics_recorder_.reset(); }
+
  private:
   base::test::TaskEnvironment task_environment_;
+  std::unique_ptr<base::StatisticsRecorder> statistics_recorder_;
 };
 
 TEST_F(AndroidAtomsLoggerTest, FeatureDisabled_DoesNotInitialize) {
