2bc560884967360b688b3d602da52a4cb89cb279
Kent Tamura <tkent@chromium.org>
2026-06-08 00:00:01 -0700

    Fix crash in text-fit with multi-column layout
    When 'text-fit' is used, the layout engine performs a temporary,
    non-fragmented layout pass to measure the paragraph scaling factor.
    
    This measurement pass was incorrectly allowed to apply side effects to
    the LayoutObject tree.
    
    This patch fixes the issue by wrapping the measurement phase in a
    DisableLayoutSideEffectsScope, ensuring it does not pollute the state of
    the LayoutObject tree.
    
    Bug: 517455256
    Change-Id: I1ecbb31cd14501dd3ca24c49484379a1ca0b98e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7892646
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643008}

--- third_party/blink/renderer/core/layout/block_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/block_layout_algorithm.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/column_spanner_path.h"
 #include "third_party/blink/renderer/core/layout/constraint_space.h"
 #include "third_party/blink/renderer/core/layout/constraint_space_builder.h"
+#include "third_party/blink/renderer/core/layout/disable_layout_side_effects_scope.h"
 #include "third_party/blink/renderer/core/layout/early_break.h"
 #include "third_party/blink/renderer/core/layout/floats_utils.h"
 #include "third_party/blink/renderer/core/layout/fragmentation_utils.h"
@@ -673,6 +674,8 @@ const LayoutResult* BlockLayoutAlgorithm::LayoutInlineChild(
         text_fit.Type() == TextFitType::kShrink &&
         text_fit.Target() == TextFitTarget::kConsistent;
     if (grow_consistent || shrink_consistent) {
+      DisableLayoutSideEffectsScope no_side_effects;
+
       // Compute the paragraph scaling factor with a cloned
       // BlockLayoutAlgorithm.
       // TODO(crbug.com/417306102): This approach is an inefficient because it
