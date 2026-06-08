4ea1cdb3e85a2ae2a20172902a7ffbe02e214dcb
Daniel Angulo <angdaniel@google.com>
2026-06-08 05:09:29 -0700

    [spanify] Handle qualified names in DeclRefExpr to prevent assertion crash
    The spanify tool was crashing with an assertion failure in `GetExprRange`
    when encountering a `DeclRefExpr` with a qualified name
    (e.g., `ns::global_buf`).
    
    The assertion `assert(decl_ref->getBeginLoc() == decl_ref->getEndLoc())`
    assumed that the begin and end locations of a `DeclRefExpr` are always
    the same, which is only true for unqualified names. For qualified names,
    the begin location points to the start of the qualifier, while the end
    location points to the start of the identifier.
    
    This CL fixes the issue by removing the incorrect assertion and updating
    the range calculation to use both the begin location (which includes the
    qualifier) and the end location of the `DeclRefExpr`.
    
    Change-Id: I7c7a656d7af8bb62717ad32d458b2cfbce313291
    Bug: 501143858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903601
    Auto-Submit: Daniel Angulo <angdaniel@google.com>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643104}

---
+++ tools/clang/spanify/tests/chrome/qualified-names-original.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace ns {
+int global_buf[10];
+}
+
+void DeclRefExprWithQualifiedName(int index) {
+  // Expected rewrite:
+  // base::span<int> p = base::span<int>(ns::global_buf)
+  //                         .subspan(base::checked_cast<size_t>(index));
+  int* p = &ns::global_buf[index];
+  p[0] = 0;
+}
