93d46e8341aa5b25ffa3654d2b9cfb3ea3b5a388
James Cook <jamescook@chromium.org>
2026-06-08 12:48:20 -0700

    extensions: Preinstall Docs Offline via new method on Win/Mac/Linux
    This is a followup to https://crrev.com/c/7899711 which introduced a
    hard-coded list of external extensions in order to support Android.
    To align the install behavior on all platforms, switch to the new
    approach on Win/Mac/Linux.
    
    Add tests for preinstalled_apps::Provider::SetPrefs() to check that
    the extension will be installed. Update the build file so that
    preinstalled_apps_unittest.cc is also not compiled on Chrome OS, just
    like preinstalled_apps.h/cc. This makes the test less ifdef-y.
    
    Manually tested on Linux:
    1. Run of Chrome with new user data dir, don't sign in (so extension
       won't sync). Extension is installed
    2. Restart; extension is still there
    3. Add new profile; extension gets installed
    4. Uninstall extension in profile 1; extension gone from profile 1,
       still present in profile 2
    5. Restart, extension still gone from profile 1
    
    Bug: 517655721
    Test: existing unit_tests
    Change-Id: I44f5e2317a071a928e200af7bd6ea0d704ebda09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902086
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643412}

--- chrome/browser/extensions/preinstalled_apps.cc
+++ chrome/browser/extensions/preinstalled_apps.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "base/strings/string_util.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
@@ -166,8 +167,7 @@ Provider::Provider(Profile* profile,
 void Provider::VisitRegisteredExtension() {
   if (!preinstalled_apps_enabled_) {
     // If pre-installed apps aren't enabled for the profile, we short-circuit
-    // the flow to load them from the file (which happens as a result of
-    // VisitRegisteredExtension()), and immediately set empty prefs.
+    // the flow to add them and immediately set empty prefs.
     ExternalProviderImpl::SetPrefs(base::DictValue());
     return;
   }
@@ -178,18 +178,15 @@ void Provider::VisitRegisteredExtension() {
 void Provider::SetPrefs(base::DictValue prefs) {
   DCHECK(preinstalled_apps_enabled_);
 
-  // TODO(crbug.com/517655721): Use this mechanism on Win/Mac/Linux as well.
-#if BUILDFLAG(IS_ANDROID)
   // Load a hard-coded list of external extensions. These are not component
   // extensions; they are installed from the webstore and don't get access to
   // component only APIs. Must be used with ManifestLocation::kInternal to allow
   // CWS to download certain extensions.
-  if (perform_new_installation_) {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  if (perform_new_installation_) {
     AddExtension(extension_misc::kDocsOfflineExtensionId, prefs);
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
   }
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 #if BUILDFLAG(ENABLE_PLATFORM_APPS)
   // First, check if this is for a migration from around 2013. Likely not.
