31fce945a091780abac5ef1a7858aba2ed7c5a82
Fernando Ramirez <ferny@google.com>
2026-06-08 11:12:54 -0700

    [AF][Clank] Expose kAutofillEnableGradientGoogleLogos flag to Java
    Bug: 510887217
    Change-Id: I7b0c17a1428ab1ee2a7673bfa3a4591283ec9c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904811
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643335}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -132,6 +132,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &autofill::features::kAutofillDeepLinkAutofillOptions,
     &autofill::features::kAutofillEnableAiBasedAmountExtraction,
     &autofill::features::kAutofillEnableBuyNowPayLater,
+    &autofill::features::kAutofillEnableGradientGoogleLogos,
     &autofill::features::kAutofillEnableNewCardBenefitsToggleText,
     &autofill::features::kAutofillEnableNewFopDisplayAndroid,
     &autofill::features::kAutofillEnableSecurityTouchEventFilteringAndroid,
