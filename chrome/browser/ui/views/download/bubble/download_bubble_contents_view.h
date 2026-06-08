3dbcc9a915eadcce55e8a97bbdc3047513d5bdc9
Paul Jensen <pauljensen@chromium.org>
2026-06-08 14:31:37 -0700

    Make downloads bubble display async and compatible with WebUI anchor
    A few pinned toolbar action buttons (PTABs) are displayed and then
    immediately used to anchor a bubble. This works successfully with
    Views-based PTABs because they are synchronously shown, but does not
    work with WebUI-based PTABs as they are asynchronously shown (because
    the actual button is rendered in a different process and has to call
    back into the browser process once shown and ready to be used for
    anchoring). This change converts the downloads bubble to use
    a PTAB API to asynchronously get an anchor.
    
    Bug: 493870881
    Change-Id: I3cc50e9e3255cf4d548dde41a8010a3b28b7ed87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7856341
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643463}

--- chrome/browser/ui/views/download/bubble/download_bubble_contents_view.h
+++ chrome/browser/ui/views/download/bubble/download_bubble_contents_view.h
@@ -14,6 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
 
+// Type of downloads to show in the download bubble primary view.
+enum class DownloadBubbleMode {
+  // Shows all recent downloads finished within the last 24 hours.
+  kComplete,
+  // Shows only in-progress and uninteracted downloads.
+  kPartial,
+};
+
 class Browser;
 class DownloadBubbleNavigationHandler;
 class DownloadBubblePrimaryView;
@@ -50,7 +58,7 @@ class DownloadBubbleContentsView : public views::View,
       base::WeakPtr<DownloadBubbleUIController> bubble_controller,
       base::WeakPtr<DownloadBubbleNavigationHandler> navigation_handler,
       // Whether the primary view is the partial view.
-      bool primary_view_is_partial_view,
+      DownloadBubbleMode mode,
       std::unique_ptr<DownloadBubbleContentsViewInfo> info,
       // The owning bubble's delegate.
       views::BubbleDialogDelegate* bubble_delegate);
