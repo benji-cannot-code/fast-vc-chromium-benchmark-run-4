1106fcf0e4303c012dea36a58942b3b8290b2813
Huili Liu <huilliu@microsoft.com>
2026-06-07 22:36:54 -0700

    [Android] fix memory leak of WebApkInitializationTest
    Bug: 512492607
    Change-Id: I657ed5c921ad9b0bf9089e223c06bd5a09d38220
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902458
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: huili liu <huilliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1642993}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkInitializationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkInitializationTest.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 
 import androidx.test.filters.LargeTest;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +22,6 @@ import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableLeakChecks;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.browserservices.ui.SharedActivityCoordinator;
@@ -40,11 +40,11 @@ import java.util.concurrent.TimeoutException;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @DoNotBatch(reason = "Activity initialzation test")
-@DisableLeakChecks("crbug.com/512492607 (ApplicationStatus)")
 public class WebApkInitializationTest {
     @Rule public final WebApkActivityTestRule mActivityRule = new WebApkActivityTestRule();
 
     public ActivityLifecycleDispatcher mActivityLifecycleDispatcher;
+    private CreationObserver mCreationObserver;
 
     class CreationObserver implements ApplicationStatus.ActivityStateListener {
         public CreationObserver() {}
@@ -60,6 +60,21 @@ public class WebApkInitializationTest {
         }
     }
 
+    @After
+    public void tearDown() {
+        // ApplicationStatus#sGeneralActivityStateListeners is process-wide; without unregistering
+        // the (non-static) CreationObserver, it would pin this test instance, the Mockito spy
+        // dispatcher, and transitively the destroyed Activity.
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    if (mCreationObserver != null) {
+                        ApplicationStatus.unregisterActivityStateListener(mCreationObserver);
+                        mCreationObserver = null;
+                    }
+                });
+        mActivityLifecycleDispatcher = null;
+    }
+
     /**
      * Test that {@link WebappActionsNotificationManager}, {@link
      * WebappDisclosureSnackbarController}, {@link WebApkActivityLifecycleUmaTracker} and {@link
@@ -71,7 +86,8 @@ public class WebApkInitializationTest {
     public void testInitialization() throws TimeoutException {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    ApplicationStatus.registerStateListenerForAllActivities(new CreationObserver());
+                    mCreationObserver = new CreationObserver();
+                    ApplicationStatus.registerStateListenerForAllActivities(mCreationObserver);
                 });
         EmbeddedTestServer embeddedTestServer =
                 mActivityRule.getEmbeddedTestServerRule().getServer();
