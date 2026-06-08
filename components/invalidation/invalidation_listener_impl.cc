02ae6f15134f025570d6b414f1d7365adbcdcf27
Artem Sumaneev <asumaneev@google.com>
2026-06-08 07:12:23 -0700

    [invalidations] Improve CHECK failure messages in InvalidationListenerImpl
    Add more informative messages to CHECKs in AddObserver and
    RemoveObserver to provide better context when these assertions fail.
    
    Bug: 520781119
    Change-Id: I87aa035844c3c2eafd17c0af090254d55dd39fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7907722
    Auto-Submit: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643174}

--- components/invalidation/invalidation_listener_impl.cc
+++ components/invalidation/invalidation_listener_impl.cc
@@ -112,9 +112,11 @@ InvalidationListenerImpl::~InvalidationListenerImpl() {
 
 // InvalidationListener overrides.
 void InvalidationListenerImpl::AddObserver(Observer* observer) {
-  const std::string type = observer->GetType();
-  CHECK(!type_to_handler_.contains(type));
-  CHECK(!observers_.HasObserver(observer));
+  const std::string& type = observer->GetType();
+  CHECK(!type_to_handler_.contains(type))
+      << "Observer already exists for type: " << type;
+  CHECK(!observers_.HasObserver(observer))
+      << "Observer already subscribed for type: " << observer;
 
   observers_.AddObserver(observer);
   type_to_handler_[type] = observer;
@@ -133,7 +135,8 @@ bool InvalidationListenerImpl::HasObserver(const Observer* observer) const {
 void InvalidationListenerImpl::RemoveObserver(const Observer* observer) {
   const std::string& type = observer->GetType();
   auto it = type_to_handler_.find(type);
-  CHECK(it != type_to_handler_.end());
+  CHECK(it != type_to_handler_.end())
+      << "Observer not found for type: " << type;
   type_to_handler_.erase(it);
   observers_.RemoveObserver(observer);
 }
