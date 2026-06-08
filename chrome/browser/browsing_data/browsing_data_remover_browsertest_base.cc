34e537cc11dbdda5b5ff6d340aa0a5f886ceec77
Dave Tapuska <dtapuska@chromium.org>
2026-06-08 13:57:08 -0700

    Encode partial content in base64.
    Base64 encode the partial content found in files when a hostname is unexpectedly present. This prevents issues with non-printable characters in the output.
    
    Change-Id: Ic80c8d0496d6205229b1f01b6c67a9b75df0d9cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7907789
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643451}

--- chrome/browser/browsing_data/browsing_data_remover_browsertest_base.cc
+++ chrome/browser/browsing_data/browsing_data_remover_browsertest_base.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/base64.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -375,11 +376,13 @@ void BrowsingDataRemoverBrowserTestBase::CheckUserDirectoryForString(
     size_t pos = content.find(hostname);
     if (pos != std::string::npos) {
       // Print surrounding text of the match.
-      std::string partial_content = content.substr(
-          pos < 30 ? 0 : pos - 30,
-          std::min(content.size() - 1, pos + hostname.size() + 30));
+      size_t start = pos < 30 ? 0 : pos - 30;
+      size_t end = std::min(content.size(), pos + hostname.size() + 30);
+      std::string partial_content_b64 =
+          base::Base64Encode(content.substr(start, end - start));
       ADD_FAILURE() << "Found file content: " << file << "\n"
-                    << "  which had partial_content: " << partial_content;
+                    << "  which had partial_content (base64 encoded): "
+                    << partial_content_b64;
     }
   }
 }
