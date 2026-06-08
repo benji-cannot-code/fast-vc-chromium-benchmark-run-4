4737499bf2e173ccf6dca5bff93db6286aff6745
Mateusz Zbikowski <zbikowski@google.com>
2026-06-08 06:35:36 -0700

    Add ManuallyTriggeredStartupActivity to WebView shell.
    This new activity allows WebView startup to be manually triggered by
    waiting for a user button press before initializing WebView. This is to
    enable more precise capture of memory profiles in automated lab tests.
    "Destroy WebView" menu option was added to allow for inspection of a
    zero WebView state to ascertain no memory leakage. Null checks were
    added throughout as needed.
    
    Bug: 513493415
    Change-Id: I97bfc4751eb76b073043f3753da88283d59a40ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894027
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Mateusz Zbikowski <zbikowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643151}

---
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/ManuallyTriggeredStartupActivity.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.webview_shell;
+
+/**
+ * WebViewBrowserActivity that waits for a button press on startup, and then again
+ * in the fragment before initializing WebView.
+ */
+public class ManuallyTriggeredStartupActivity extends WebViewBrowserActivity {
+    @Override
+    protected boolean shouldDelayStartup() {
+        return true;
+    }
+
+    @Override
+    protected String getBrowserToolbarTitle() {
+        return getResources().getString(R.string.title_activity_manually_triggered_startup);
+    }
+}
