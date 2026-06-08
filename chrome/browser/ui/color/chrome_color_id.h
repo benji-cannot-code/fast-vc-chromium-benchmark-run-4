24daa5d245a5ef95ba97a3b73eb76aef1cd7fed4
Orin Jaworski <orinj@chromium.org>
2026-06-08 13:37:55 -0700

    Create unique color IDs for STS contextual tasks banner promos
    This cleans up the use of miscellaneous sys colors in banner_promo.css.
    
    Fixed: 513636782
    Change-Id: I1ac3fb7621b94a48d0f6c30787fe90b3460fbdfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7853145
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643440}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -121,6 +121,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorComposeDialogTextareaReadonlyForeground) \
   E_CPONLY(kColorComposeDialogTextareaIcon) \
   E_CPONLY(kColorComposeDialogSelectOptionDisabled) \
+  /* Contextual tasks colors. */ \
+  E_CPONLY(kColorContextualTasksBannerPromoBackground) \
+  E_CPONLY(kColorContextualTasksBannerPromoText) \
+  E_CPONLY(kColorContextualTasksBannerPromoIconBackground) \
+  E_CPONLY(kColorContextualTasksBannerPromoIconForeground) \
   /* Desktop media picker view. */ \
   E_CPONLY(kColorDesktopMediaPickerDescriptionLabel) \
   /* Desktop media tab list colors. */ \
