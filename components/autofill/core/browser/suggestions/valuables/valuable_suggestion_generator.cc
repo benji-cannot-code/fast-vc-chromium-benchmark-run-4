e1b7baa5cf485d16aec47907c5ac09bba1e548d6
Jihad Hanna <jihadghanna@google.com>
2026-06-08 07:26:20 -0700

    Cleanup AutofillFixIsAutofilled
    The feature has been enabled by default for a while now. Removing the
    disabled branch will unblock follow-up cleanups and fixes to the filling
    logic.
    
    Bug: 393114125
    Change-Id: Ifd065ae541cce6c35592d1d71332bc9ee48d86ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905083
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643182}

--- components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/valuables/valuable_suggestion_generator.cc
@@ -369,8 +369,7 @@ void LoyaltyCardSuggestionGenerator::GenerateSuggestions(
     base::Extend(suggestions,
                  GetLoyaltyCardsFooterSuggestions(
                      // TODO(crbug.com/393114125): Change to use
-                     // `AutofillField::field_modifiers_`
-                     // after launching `kAutofillFixIsAutofilled`.
+                     // `AutofillField::field_modifiers_`.
                      trigger_field.is_autofilled_according_to_renderer()));
     callback({SuggestionDataSource::kLoyaltyCard, std::move(suggestions)});
     return;
@@ -401,8 +400,7 @@ void LoyaltyCardSuggestionGenerator::GenerateSuggestions(
   base::Extend(suggestions,
                GetLoyaltyCardsFooterSuggestions(
                    // TODO(crbug.com/393114125): Change to use
-                   // `AutofillField::field_modifiers_`
-                   // after launching `kAutofillFixIsAutofilled`.
+                   // `AutofillField::field_modifiers_`.
                    trigger_field.is_autofilled_according_to_renderer()));
   callback({SuggestionDataSource::kLoyaltyCard, std::move(suggestions)});
 }
