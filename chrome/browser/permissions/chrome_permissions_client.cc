abb7469c42a8a315263e138ed70be21a7b73b8a3
Andy Paicu <andypaicu@chromium.org>
2026-06-08 05:32:37 -0700

    Revert "Enable ChromePermissionsClient system permissions functions on Win&CrOS"
    This reverts commit 0473ea84521c2711d9c3b0f744f8ac2582238001.
    
    Reason for revert: AppCapabilityAccessServer::CheckAccess seems to wait internally for cross-thread processing or IPC, and therefore blocks for non-trivial duration of time. See: crbug.com/517569708
    
    Original change's description:
    > Enable ChromePermissionsClient system permissions functions on Win&CrOS
    >
    > We implemented the system_permission_settings functionality for Windows
    > and ChromeOS but we still guard the ChromePermissionsClient functions
    > with a Mac-only check. This CL updates the guard to also allow Windows
    > and ChromeOS.
    >
    > Also a lot of the CrOS tests would now crash since they would try to
    > call into the privacy_hub to get permission status, so I added an
    > `IsReady` function to be checked before trying to get permission status.
    >
    > Bug: 345690715,349317485
    > Change-Id: I69e01a377f7f68582c8410bfab1fa86b7105c884
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7756180
    > Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    > Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1626086}
    
    Bug: 345690715,349317485,517569708
    Bug: 345690715,349317485
    Change-Id: Idedddfff3ae67885d9705ab0ed5fd27cc460b81d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7889558
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643114}

--- chrome/browser/permissions/chrome_permissions_client.cc
+++ chrome/browser/permissions/chrome_permissions_client.cc
@@ -988,7 +988,7 @@ ChromePermissionsClient::CreatePrompt(
 
 bool ChromePermissionsClient::HasDevicePermission(
     ContentSettingsType type) const {
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_MAC)
   return system_permission_settings::IsAllowed(type);
 #else
   return PermissionsClient::HasDevicePermission(type);
@@ -997,7 +997,7 @@ bool ChromePermissionsClient::HasDevicePermission(
 
 bool ChromePermissionsClient::CanRequestDevicePermission(
     ContentSettingsType type) const {
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_MAC)
   return system_permission_settings::CanPrompt(type);
 #else
   return PermissionsClient::CanRequestDevicePermission(type);
