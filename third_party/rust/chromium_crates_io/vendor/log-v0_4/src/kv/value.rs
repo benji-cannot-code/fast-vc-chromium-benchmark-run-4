c9943653bbdd44c8b155b50d647553b1e209ab04
Jon Toohill <jtoohill@google.com>
2026-06-08 08:49:58 -0700

    Roll log: 0.4.30 => 0.4.32 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * log: 0.4.30 => 0.4.32; https://docs.rs/crate/log/0.4.32
    
    Bug: None
    Change-Id: I30cd8d5b6941b1cc681d1be0868a55dd78700e75
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903459
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Auto-Submit: Jon Toohill <jtoohill@google.com>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643238}

--- third_party/rust/chromium_crates_io/vendor/log-v0_4/src/kv/value.rs
+++ third_party/rust/chromium_crates_io/vendor/log-v0_4/src/kv/value.rs
@@ -416,7 +416,7 @@ impl<'v> Value<'v> {
     }
 }
 
-#[cfg(feature = "kv_std")]
+#[cfg(feature = "std")]
 mod std_support {
     use std::borrow::Cow;
     use std::rc::Rc;
@@ -463,6 +463,7 @@ mod std_support {
         }
     }
 
+    #[cfg(feature = "kv_std")]
     impl<'v> Value<'v> {
         /// Try convert this value into a string.
         pub fn to_cow_str(&self) -> Option<Cow<'v, str>> {
--- third_party/rust/chromium_crates_io/vendor/log-v0_4/src/macros.rs
+++ third_party/rust/chromium_crates_io/vendor/log-v0_4/src/macros.rs
@@ -69,7 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// The `logger` argument accepts a value that implements the `Log` trait. The value
 /// will be borrowed within the macro.
 ///
-/// Note that the global level set via Cargo features, or through `set_max_level` will
+/// Note that the global level set via Cargo features or through `set_max_level` will
 /// still apply, even when a custom logger is supplied with the `logger` argument.
 #[macro_export]
 #[clippy::format_args]
@@ -445,12 +445,16 @@ macro_rules! __log_logger {
 #[cfg(feature = "kv")]
 macro_rules! __log_key {
     // key1 = 42
-    ($($args:ident)*) => {
-        $crate::__private_api::stringify!($($args)*)
+    ($args:ident) => {
+        $crate::__private_api::Key::from_str_static($crate::__private_api::stringify!($args))
     };
     // "key1" = 42
-    ($($args:expr)*) => {
-        $($args)*
+    ($args:literal) => {
+        $crate::__private_api::Key::from_str_static($args)
+    };
+    // key = 42, where key is an expression
+    ($args:expr) => {
+        $crate::__private_api::Key::from_str($args)
     };
 }
 
@@ -535,7 +539,7 @@ macro_rules! __log_value_sval {
 #[cfg(not(feature = "kv_sval"))]
 macro_rules! __log_value_sval {
     ($args:expr) => {
-        compile_error!("capturing values as `sval::Value` requites the `kv_sval` feature of `log`")
+        compile_error!("capturing values as `sval::Value` requires the `kv_sval` feature of `log`")
     };
 }
 
@@ -554,7 +558,7 @@ macro_rules! __log_value_serde {
 macro_rules! __log_value_serde {
     ($args:expr) => {
         compile_error!(
-            "capturing values as `serde::Serialize` requites the `kv_serde` feature of `log`"
+            "capturing values as `serde::Serialize` requires the `kv_serde` feature of `log`"
         )
     };
 }
@@ -574,7 +578,7 @@ macro_rules! __log_value_error {
 macro_rules! __log_value_error {
     ($args:expr) => {
         compile_error!(
-            "capturing values as `std::error::Error` requites the `kv_std` feature of `log`"
+            "capturing values as `std::error::Error` requires the `kv_std` feature of `log`"
         )
     };
 }
