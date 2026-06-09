016c7074cbb048287d1ac95946fbf64f9837a933
Jihad Hanna <jihadghanna@google.com>
2026-06-09 04:55:08 -0700

    Relax filling restrictions on dynamic forms
    Autofill used to abort filling if the form changed considerably between
    the time of displaying suggestions and the time of accepting them.
    
    This logic is problematic because a website that randomly adds/removes
    fields on focus events can totally block autofill.
    
    This CL replaces this logic with a best effort approach: All fields that
    can safely be filled are filled, and there's no more dependency on the
    state of the form at the time of displaying suggestions. The passed
    `FormData` parameter has become unused, but will be removed in a
    follow-up because it touches multiple other places.
    
    Fixed: 352757064
    Bug: 40227071
    Change-Id: I321c796f1dbf03cae55d2952b9025fc234e61ede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902425
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643865}

--- components/autofill/core/browser/autofill_field.h
+++ components/autofill/core/browser/autofill_field.h
@@ -492,11 +492,6 @@ class AutofillField : public FormFieldData {
     UpdateFieldData(field_data);
   }
 
-  bool is_autofilled_according_to_renderer(
-      base::PassKey<FormAutofillHistory> pass_key) const {
-    return is_autofilled_according_to_renderer();
-  }
-
 #if defined(UNIT_TEST)
   const std::array<FieldType,
                    static_cast<size_t>(HeuristicSource::kMaxValue) + 1>&
@@ -508,7 +503,6 @@ class AutofillField : public FormFieldData {
  private:
   friend class AutofillFieldTestApi;
 
-  using FormFieldData::is_autofilled_according_to_renderer;
   using FormFieldData::set_is_autofilled_according_to_renderer;
 
   struct PredictionResult {
