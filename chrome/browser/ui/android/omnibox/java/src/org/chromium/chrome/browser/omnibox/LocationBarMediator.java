538c4ab09469fd31530cf78beef115d18815f980
Sky Malice <skym@chromium.org>
2026-06-09 16:11:00 -0700

    [AIM] Do not guard passing fusebox session into search engine hint.
    Bug: 520061213
    Change-Id: Ib7d3170535fcb93512f7a1cbebed70f71fc1574a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914183
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644295}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -2847,13 +2847,9 @@ class LocationBarMediator
                         ? mLocationBarDataProvider.getDefaultRequestType()
                         : mCurrentInput.getRequestType();
 
-        FuseboxSessionState fuseboxSessionState = null;
-        if (OmniboxFeatures.sShowModelPicker.getValue()) {
-            fuseboxSessionState = FuseboxSessionState.from(mLocationBarDataProvider);
-        }
-
         mUrlCoordinator.setUrlBarHintText(
-                mSearchEngineUtils.getOmniboxHintText(requestType, fuseboxSessionState));
+                mSearchEngineUtils.getOmniboxHintText(
+                        requestType, FuseboxSessionState.from(mLocationBarDataProvider)));
     }
 
     /* package */ ToolbarWidthConsumer getBookmarkButtonToolbarWidthConsumer() {
