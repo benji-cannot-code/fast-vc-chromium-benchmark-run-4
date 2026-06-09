f7ad66205137a807fe2c698ac237ccd958d3230a
Takashi Nakayama <tnak@chromium.org>
2026-06-08 20:55:10 -0700

    [CodeCache] Introduce standalone InlineScriptCache
    Previously, InlineScriptCache feature was strictly coupled to
    PersistentCacheForCodeCache feature. This CL decouples these two
    mechanisms, allowing inline script caching to operate standalone without
    the persistent cache resource script cache.
    
    A new `SourceKeyedCacheReader` helper class in `ProxyCodeCacheHost` now
    handles synchronous source-keyed fetch requests like already done in
    `PersistentCodeCacheHost`.
    
    This CL also modifies the relevant browser tests to validate the inline
    script cache both with and without PersistentCacheForCodeCache.
    
    Bug: 514423242
    Change-Id: Ibf8a22ae8cca3b105e45dc611b77795a823c68e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895356
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643671}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2801,9 +2801,8 @@ bool IsMemoryPurgeOnBackgroundingEnabled() {
 }
 
 bool IsInlineScriptCacheEnabled() {
-  // Inline script cache is built on top of PersistentCodeCache.
-  return base::FeatureList::IsEnabled(kInlineScriptCache) &&
-         IsPersistentCacheForCodeCacheEnabled();
+  return net::HttpCache::IsSplitCacheEnabled() &&
+         base::FeatureList::IsEnabled(kInlineScriptCache);
 }
 
 bool IsParkableStringsToDiskEnabled() {
