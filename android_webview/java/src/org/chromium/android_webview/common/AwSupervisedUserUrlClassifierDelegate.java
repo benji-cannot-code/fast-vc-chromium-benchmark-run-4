95a9b6b6a2490c6987c856dba754e95f2cde403c
Chidera Olibie <colibie@google.com>
2026-06-09 08:55:02 -0700

    Only update cached SB/RCB preference if non-null result received
    This CL updates Safe Browsing and Restricted Content Blocking config
    helpers to only update their cached preferences (and log associated
    histograms) when GMS queries return a non-null result (i.e. not on
    timeout or error).
    
    It also updates corresponding histograms.xml to document this behavior,
    and adds test cases to verify it.
    
    TAG=agy
    CONV=151e4036-1bf7-4756-a458-17bb9a74d65f
    
    Bug: 508162988, 517335150
    Change-Id: I03aa75a1043647a696b0fdc479af55184f92fef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899996
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644001}

--- android_webview/java/src/org/chromium/android_webview/common/AwSupervisedUserUrlClassifierDelegate.java
+++ android_webview/java/src/org/chromium/android_webview/common/AwSupervisedUserUrlClassifierDelegate.java
@@ -7,6 +7,7 @@ package org.chromium.android_webview.common;
 
 import org.chromium.base.Callback;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.url.GURL;
 
 /**
@@ -36,5 +37,5 @@ public interface AwSupervisedUserUrlClassifierDelegate {
      * <p>callback.onResult(false) - indicates the user does not require restricted content
      * blocking. callback.onResult(true) - indicates the user requires restricted content blocking.
      */
-    void needsRestrictedContentBlocking(final Callback<Boolean> callback);
+    void needsRestrictedContentBlocking(final Callback<@Nullable Boolean> callback);
 }
