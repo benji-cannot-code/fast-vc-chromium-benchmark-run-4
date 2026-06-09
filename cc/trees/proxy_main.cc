8c9ee3a548be5ce9a2353da235509cd7352c5c48
Thiabaud Engelbrecht <thiabaud@google.com>
2026-06-09 08:56:46 -0700

    [cc] Add MainThreadScroll as a BMFReason
    This CL also groups together all Scroll, Paint and Style invalidation,
    since all are relatively uncommon.
    
    Bug: 467969029, 517658215
    Change-Id: Icb81b5402323223196105fdc6f2e07c19cea6395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7911362
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644003}

--- cc/trees/proxy_main.cc
+++ cc/trees/proxy_main.cc
@@ -166,7 +166,7 @@ void ProxyMain::BeginMainFrame(
       UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
           "Compositing.BeginMainFrame.TimeUs", timer->Elapsed(),
           base::Microseconds(1), base::Seconds(10), 50);
-      UMA_HISTOGRAM_ENUMERATION("Compositing.BeginMainFrame.BMFReason6",
+      UMA_HISTOGRAM_ENUMERATION("Compositing.BeginMainFrame.BMFReason7",
                                 begin_main_frame_reason.to_ulong(),
                                 num_buckets);
       if (reason == CommitEarlyOutReason::kFinishedNoUpdates) {
@@ -174,7 +174,7 @@ void ProxyMain::BeginMainFrame(
             "Compositing.BeginMainFrame.TimeUs.NoUpdate", timer->Elapsed(),
             base::Microseconds(1), base::Seconds(10), 50);
         UMA_HISTOGRAM_ENUMERATION(
-            "Compositing.BeginMainFrame.BMFReason6.NoUpdate",
+            "Compositing.BeginMainFrame.BMFReason7.NoUpdate",
             begin_main_frame_reason.to_ulong(), num_buckets);
       }
     }
