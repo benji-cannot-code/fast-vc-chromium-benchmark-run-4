75b68396155d1dc0e5f86cf867f2dbe8d973e7b8
Sky Malice <skym@chromium.org>
2026-06-09 11:31:22 -0700

    Revert "[Android] fix memory leak of CustomTabActivityTest"
    This reverts commit 11236416a37df0a70a4e3c5ad88a1c1bbba933d1.
    
    Reason for revert: Speculative revert to fix android-16-x64-leakcanary-fyi-rel. I don't understand how this could be the culprit, but it bisect seems to claim it is.
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4854085497913344
    
    Original change's description:
    > [Android] fix memory leak of CustomTabActivityTest
    >
    > In CustomTabTabPersistencePolicy.destroy(), cancel the cleanup task this policy started and clear the static sCleanupTask reference, so the task's captured callback no longer pins the destroyed CustomTabActivity.
    >
    > Bug: 512492806, 512492857
    > Change-Id: I366558095b5c524ae212e79cbdfe8dd1453f9e5c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901434
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: huili liu <huilliu@microsoft.com>
    > Reviewed-by: Ziwen Lin <ziwlin@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1643629}
    
    Bug: 512492806, 512492857
    Change-Id: I9db4107cf09669d574afac69df5793115e5f03f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914263
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1644129}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabTabPersistencePolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabTabPersistencePolicy.java
@@ -288,19 +288,6 @@ public class CustomTabTabPersistencePolicy implements TabPersistencePolicy {
             mTabDataToDeleteCallback = storedTabDataToDeleteCallback;
         }
 
-        /**
-         * Releases the static reference to this task (and the external callback it retains). Only
-         * clears {@link #sCleanupTask} if it still points to this task, to avoid wiping out a newer
-         * task scheduled while this one was still finishing.
-         */
-        private void clearCleanupTask() {
-            synchronized (CLEAN_UP_TASK_LOCK) {
-                if (sCleanupTask == this) {
-                    sCleanupTask = null;
-                }
-            }
-        }
-
         @Override
         protected @Nullable Void doInBackground() {
             if (mDestroyed) return null;
@@ -347,7 +334,6 @@ public class CustomTabTabPersistencePolicy implements TabPersistencePolicy {
             TabPersistenceFileInfo tabDataToDelete = new TabPersistenceFileInfo();
             if (mDestroyed) {
                 mTabDataToDeleteCallback.onResult(tabDataToDelete);
-                clearCleanupTask();
                 return;
             }
 
@@ -356,7 +342,6 @@ public class CustomTabTabPersistencePolicy implements TabPersistencePolicy {
             assumeNonNull(mTabIdsByMetadataFile);
             if (mUnreferencedTabIds.isEmpty() && mDeletableMetadataFiles.isEmpty()) {
                 mTabDataToDeleteCallback.onResult(tabDataToDelete);
-                clearCleanupTask();
                 return;
             }
 
@@ -398,7 +383,9 @@ public class CustomTabTabPersistencePolicy implements TabPersistencePolicy {
 
             mTabDataToDeleteCallback.onResult(tabDataToDelete);
 
-            clearCleanupTask();
+            synchronized (CLEAN_UP_TASK_LOCK) {
+                sCleanupTask = null; // Release static reference to external callback
+            }
         }
 
         private void getTabsFromStateFile(SparseBooleanArray tabIds, File metadataFile) {
@@ -418,7 +405,9 @@ public class CustomTabTabPersistencePolicy implements TabPersistencePolicy {
         @Override
         protected void onCancelled(@Nullable Void result) {
             super.onCancelled(null);
-            clearCleanupTask();
+            synchronized (CLEAN_UP_TASK_LOCK) {
+                sCleanupTask = null;
+            }
         }
     }
 
