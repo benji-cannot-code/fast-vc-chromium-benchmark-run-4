8c443c7a6702705d5ab944270df1d7e8ff95215c
Jan Keitel <jkeitel@google.com>
2026-06-08 22:57:52 -0700

    Avoid dangling pointer in payment_request_browsertest_base
    Bug: 40100455
    Change-Id: I7a56929c83ec8b7aeb20beb4ae53e21a55541276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7908164
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643709}

--- chrome/browser/ui/views/payments/test_chrome_payment_request_delegate.cc
+++ chrome/browser/ui/views/payments/test_chrome_payment_request_delegate.cc
@@ -132,4 +132,9 @@ TestChromePaymentRequestDelegate::GetPaymentUIObserver() const {
   return payment_ui_observer_;
 }
 
+base::WeakPtr<TestChromePaymentRequestDelegate>
+TestChromePaymentRequestDelegate::GetWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
+}
+
 }  // namespace payments
--- chrome/browser/ui/views/payments/test_chrome_payment_request_delegate.h
+++ chrome/browser/ui/views/payments/test_chrome_payment_request_delegate.h
@@ -69,6 +69,8 @@ class TestChromePaymentRequestDelegate : public ChromePaymentRequestDelegate {
     return static_cast<PaymentRequestDialogView*>(shown_dialog_.get());
   }
 
+  base::WeakPtr<TestChromePaymentRequestDelegate> GetWeakPtr();
+
  private:
   // ChromePaymentRequestDelegate:
   void ShowDialog(base::WeakPtr<PaymentRequest> request) override;
@@ -96,6 +98,9 @@ class TestChromePaymentRequestDelegate : public ChromePaymentRequestDelegate {
   std::optional<bool> is_off_the_record_;
   std::optional<bool> is_valid_ssl_;
   std::optional<bool> is_browser_window_active_;
+
+  base::WeakPtrFactory<TestChromePaymentRequestDelegate> weak_ptr_factory_{
+      this};
 };
 
 }  // namespace payments
