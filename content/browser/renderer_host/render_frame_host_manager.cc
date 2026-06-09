93b6fa164afd4f02522efc489847fb020420581e
Michael Thiessen <mthiesse@chromium.org>
2026-06-09 12:54:30 -0700

    Revert "Reland: Remove EnsureRenderFrameHostVisibilityConsistent"
    This reverts commit ab2f1fe49d1e032ab80012f7377c13049fc16253.
    
    Reason for revert: https://issues.chromium.org/issues/521200679
    
    Original change's description:
    > Reland: Remove EnsureRenderFrameHostVisibilityConsistent
    >
    > 2/n making WebContents the sole manager of whether content would like
    > the RenderWidgetHostView to be visible.
    >
    > Per the comment in the code, EnsureRenderFrameHostVisibilityConsistent
    > should no longer be necessary as we unconditionally hide and show when
    > committing a navigation.
    >
    > However, some test infrastructure was initializing the visibility for
    > the TestRenderWidgetHostView incorrectly, and some unit tests that use
    > it were relying on EnsureRenderFrameHostVisibilityConsistent to fix this
    > incorrect initialization.
    >
    > Bug: 505623930
    > Change-Id: I6a25fa6d0a16b22d839f23d55cb069b5e70c6aed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878449
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Basia Zimirska <basiaz@google.com>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1637240}
    
    Bug: 505623930, 521200679
    Change-Id: I6cd9d3b5be7fc8ff55d21fb44da450cb920aa833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915302
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644186}

--- content/browser/renderer_host/render_frame_host_manager.cc
+++ content/browser/renderer_host/render_frame_host_manager.cc
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/common/page_visibility_state.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/common/url_utils.h"
 #include "ipc/constants.mojom.h"
@@ -5158,6 +5159,22 @@ bool RenderFrameHostManager::ReinitializeMainRenderFrame(
   }
 
   CHECK(render_frame_host->IsRenderFrameLive());
+
+  // The RenderWidgetHostView goes away with the render process. Initializing a
+  // RenderFrame means we'll be creating (or reusing, https://crbug.com/419087)
+  // a RenderWidgetHostView. The new RenderWidgetHostView should take its
+  // visibility from the RenderWidgetHostImpl, but this call exists to handle
+  // cases where it did not during a same-process navigation.
+  // TODO(danakj): We now hide the widget unconditionally (treating main frame
+  // and child frames alike) and show in DidFinishNavigation() always, so this
+  // should be able to go away. Try to remove this.
+  // TODO(https://crbug.com/521200679): Removing this breaks keyboard tab
+  // switching while a new tab is loading, despite the tab appearing to become
+  // visible at the correct time.
+  if (render_frame_host == render_frame_host_.get()) {
+    EnsureRenderFrameHostVisibilityConsistent();
+  }
+
   return true;
 }
 
@@ -5979,6 +5996,20 @@ void RenderFrameHostManager::ExecuteRemoteFramesBroadcastMethod(
   render_frame_host_->browsing_context_state()
       ->ExecuteRemoteFramesBroadcastMethod(callback, group_to_skip,
                                            outer_delegate_proxy);
+}
+
+void RenderFrameHostManager::EnsureRenderFrameHostVisibilityConsistent() {
+  RenderWidgetHostView* view = GetRenderWidgetHostView();
+  if (view &&
+      static_cast<RenderWidgetHostImpl*>(view->GetRenderWidgetHost())
+              ->IsHidden() != frame_tree_node_->frame_tree().IsHidden()) {
+    if (frame_tree_node_->frame_tree().IsHidden()) {
+      static_cast<RenderWidgetHostViewBase*>(view)->Hide();
+    } else {
+      static_cast<RenderWidgetHostViewBase*>(view)->ShowWithVisibility(
+          PageVisibilityState::kVisible);
+    }
+  }
 }
 
 void RenderFrameHostManager::EnsureRenderFrameHostPageFocusConsistent() {
--- content/browser/web_contents/web_contents_impl_unittest.cc
+++ content/browser/web_contents/web_contents_impl_unittest.cc
@@ -2042,13 +2042,12 @@ TEST_F(WebContentsImplTest, UpdateWebContentsVisibility) {
       main_test_rfh()->GetRenderViewHost()->GetWidget()->GetView());
   TestWebContentsObserver observer(contents());
 
-  // Test WebContents always start visible.
-  EXPECT_TRUE(view->is_showing());
+  EXPECT_FALSE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
 
-  // WebContents must be made visible once before its visibility can be changed.
+  // WebContents must be made visible once before it can be hidden.
   contents()->UpdateWebContentsVisibility(Visibility::HIDDEN);
-  EXPECT_TRUE(view->is_showing());
+  EXPECT_FALSE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
   EXPECT_EQ(Visibility::VISIBLE, contents()->GetVisibility());
 
@@ -2186,10 +2185,9 @@ void HideOrOccludeWithCapturerTest(WebContentsImpl* contents,
   TestRenderWidgetHostView* view = static_cast<TestRenderWidgetHostView*>(
       contents->GetRenderWidgetHostView());
 
-  // Test WebContents always start visible.
-  EXPECT_TRUE(view->is_showing());
+  EXPECT_FALSE(view->is_showing());
 
-  // WebContents must be made visible once before its visibility can be changed.
+  // WebContents must be made visible once before it can be hidden.
   contents->UpdateWebContentsVisibility(Visibility::VISIBLE);
   EXPECT_TRUE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
@@ -2257,8 +2255,7 @@ TEST_F(WebContentsImplTest, KeepVisibleUntilFirstVisuallyNonEmptyPaint) {
   TestRenderWidgetHostView* view = static_cast<TestRenderWidgetHostView*>(
       contents()->GetRenderWidgetHostView());
 
-  // Test WebContents always start visible.
-  EXPECT_TRUE(view->is_showing());
+  EXPECT_FALSE(view->is_showing());
 
   WebUIConfigMap::GetInstance().AddWebUIConfig(
       std::make_unique<KeepVisibleWebUIConfig>());
@@ -2266,7 +2263,6 @@ TEST_F(WebContentsImplTest, KeepVisibleUntilFirstVisuallyNonEmptyPaint) {
   const GURL kGURL("chrome://keep-visible/");
   NavigationSimulator::NavigateAndCommitFromBrowser(contents(), kGURL);
 
-  // WebContents must be made visible once before its visibility can be changed.
   contents()->UpdateWebContentsVisibility(Visibility::VISIBLE);
   EXPECT_TRUE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
