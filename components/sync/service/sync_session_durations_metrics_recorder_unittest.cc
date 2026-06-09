f647a7db30abb6e1b2f71cf695f63ce518edb5a2
Mihai Sardarescu <msarda@chromium.org>
2026-06-09 09:09:49 -0700

    Record session duration when cookie sign-in status is unknown.
    Add histogram,"Signin.SessionWithCookieSigninStatusUnknown.Duration", to
    log the total duration of sessions during which the Gaia cookie sign-in
    status could not be determined. This helps in understanding whether we
    have UNKNOWN cookie sign-in state for long sessions.
    
    Bug: 510666442
    Change-Id: Iaf38a4b8c8908d4e00d44bb286b8c82a3fe8570f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903369
    Auto-Submit: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644014}

--- components/sync/service/sync_session_durations_metrics_recorder_unittest.cc
+++ components/sync/service/sync_session_durations_metrics_recorder_unittest.cc
@@ -131,11 +131,15 @@ class SyncSessionDurationsMetricsRecorderTest : public testing::Test {
 };
 
 TEST_F(SyncSessionDurationsMetricsRecorderTest, WebSignedOut) {
+  identity_test_env_.SetCookieAccounts({});
+
   base::HistogramTester ht;
   StartAndEndSession(kSessionTime);
 
   ExpectOneSessionWithDuration(ht, {"WithoutAccount"}, kSessionTime);
   ExpectNoSession(ht, {"WithAccount"});
+  ht.ExpectTimeBucketCount(
+      "Signin.SessionWithCookieSigninStatusUnknown.Duration", kSessionTime, 0);
 }
 
 TEST_F(SyncSessionDurationsMetricsRecorderTest, WebSignedIn) {
@@ -147,6 +151,8 @@ TEST_F(SyncSessionDurationsMetricsRecorderTest, WebSignedIn) {
 
   ExpectOneSessionWithDuration(ht, {"WithAccount"}, kSessionTime);
   ExpectNoSession(ht, {"WithoutAccount"});
+  ht.ExpectTotalCount("Signin.SessionWithCookieSigninStatusUnknown.Duration",
+                      0);
 }
 
 TEST_F(SyncSessionDurationsMetricsRecorderTest,
@@ -164,6 +170,8 @@ TEST_F(SyncSessionDurationsMetricsRecorderTest,
 
   ExpectOneSessionWithDuration(ht, {"WithAccount"}, kSessionTime);
   ExpectNoSession(ht, {"WithoutAccount"});
+  ht.ExpectTimeBucketCount(
+      "Signin.SessionWithCookieSigninStatusUnknown.Duration", kSessionTime, 1);
 }
 
 TEST_F(SyncSessionDurationsMetricsRecorderTest,
@@ -181,6 +189,8 @@ TEST_F(SyncSessionDurationsMetricsRecorderTest,
 
   ExpectOneSessionWithDuration(ht, {"WithoutAccount"}, kSessionTime);
   ExpectNoSession(ht, {"WithAccount"});
+  ht.ExpectTimeBucketCount(
+      "Signin.SessionWithCookieSigninStatusUnknown.Duration", kSessionTime, 1);
 }
 
 TEST_F(SyncSessionDurationsMetricsRecorderTest, NotOptedInToSync_SignedOut) {
