2015591dec123d3ae04a646197d043e2656e5aa0
ginnnnnnny <ginnyhuang@chromium.org>
2026-06-09 07:22:32 -0700

    [actor-login] Retrieve PasswordManagerClient via delegate client
    Introduce GetFirstPartyPasswordManagerClient() to the
    ActorLoginDelegateClient interface. This allows ActorLoginDelegateImpl
    to retrieve the first-party PasswordManagerClient directly from the
    delegate client rather than requiring it as a constructor parameter.
    
    This simplifies the creation and initialization of
    ActorLoginDelegateImpl across the codebase, in particular, the Password
    ChangeCheckupActorLoginService that will be updated in a future CL.
    
    Specifically:
    - ActorLoginDelegateClient: Added GetFirstPartyPasswordManagerClient() virtual method.
    - ChromeActorLoginDelegateClient / FakeActorLoginDelegateClient: Implemented the new method.
    - ActorLoginDelegateImpl: Removed the password_manager_client parameter from the constructor and CreateForUserData() factory, retrieving it dynamically instead.
    
    Bug: 472291829
    Change-Id: I36b2d29fd16cd0da91839ae73a9abca6f1599f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895706
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643940}

--- chrome/browser/password_manager/actor_login/internal/fake_actor_login_delegate_client.h
+++ chrome/browser/password_manager/actor_login/internal/fake_actor_login_delegate_client.h
@@ -22,6 +22,7 @@ class ActorLoginWebContentInterface;
 }  // namespace actor_login
 
 namespace password_manager {
+class PasswordManagerClient;
 class PasswordManagerDriver;
 }  // namespace password_manager
 
@@ -35,13 +36,15 @@ class FakeActorLoginDelegateClient : public ActorLoginDelegateClient {
  public:
   FakeActorLoginDelegateClient(Profile* profile,
                                const url::Origin& origin,
-                               password_manager::PasswordManagerDriver* driver);
+                               password_manager::PasswordManagerDriver* driver,
+                               password_manager::PasswordManagerClient* client);
   ~FakeActorLoginDelegateClient() override;
 
   // ActorLoginDelegateClient implementation:
   void SetActorLoginWebContentInterface(
       ActorLoginWebContentInterface* web_interface) override;
   PrefService* GetPrefs() override;
+  password_manager::PasswordManagerClient* GetPasswordManagerClient() override;
   password_manager::PasswordManagerDriver*
   GetPasswordManagerDriverForMainFrame() override;
   ukm::SourceId GetPageUkmSourceIdForMainFrame() override;
@@ -110,6 +113,7 @@ class FakeActorLoginDelegateClient : public ActorLoginDelegateClient {
   bool test_requires_federated_button_click_ = false;
   url::Origin origin_;
   raw_ptr<password_manager::PasswordManagerDriver> driver_ = nullptr;
+  raw_ptr<password_manager::PasswordManagerClient> client_ = nullptr;
   raw_ptr<ActorLoginWebContentInterface> web_interface_ = nullptr;
   raw_ptr<FakeActorLoginSiwgController> siwg_controller_ = nullptr;
   bool is_remove_federated_embedder_login_request_called_ = false;
