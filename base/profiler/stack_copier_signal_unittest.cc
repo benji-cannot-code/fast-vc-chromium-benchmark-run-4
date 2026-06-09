59714b4a52831c66814340cbbc550c1be3d4cf1e
Jose Arturo Barrera <jabgonzalez@google.com>
2026-06-09 16:40:05 -0700

    [spanification] make StackBuffer use base::AlignedHeapArray
    Refactor StackBuffer class in base/profiler/stack_buffer.h to replace current std::unique_ptr usage
    
    Bug: 505042664
    Change-Id: I24c4a04e42e1075c94591f4780a7906746d6d132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7896295
    Commit-Queue: José Arturo Barrera <jabgonzalez@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644320}

--- base/profiler/stack_copier_signal_unittest.cc
+++ base/profiler/stack_copier_signal_unittest.cc
@@ -99,7 +99,7 @@ class TestStackCopierDelegate : public StackCopier::Delegate {
 #endif
 TEST(StackCopierSignalTest, MAYBE_CopyStack) {
   StackBuffer stack_buffer(/* buffer_size = */ 1 << 20);
-  UNSAFE_TODO(memset(stack_buffer.buffer(), 0, stack_buffer.size()));
+  std::ranges::fill(stack_buffer.as_span(), 0);
   uintptr_t stack_top = 0;
   TimeTicks timestamp;
   RegisterContext context;
@@ -111,11 +111,8 @@ TEST(StackCopierSignalTest, MAYBE_CopyStack) {
   StackCopierSignal copier(std::move(thread_delegate));
 
   // Copy the sentinel values onto the stack.
-  uint32_t sentinels[std::size(kStackSentinels)];
-  for (size_t i = 0; i < std::size(kStackSentinels); ++i) {
-    UNSAFE_TODO(sentinels[i]) = kStackSentinels[i];
-  }
-  base::debug::Alias((void*)sentinels);  // Defeat compiler optimizations.
+  std::array<uint32_t, kStackSentinels.size()> sentinels = kStackSentinels;
+  base::debug::Alias(sentinels.data());  // Defeat compiler optimizations.
 
   bool result = copier.CopyStack(&stack_buffer, &stack_top, &timestamp,
                                  &context, &stack_copier_delegate);
@@ -145,7 +142,7 @@ TEST(StackCopierSignalTest, MAYBE_CopyStack) {
 #endif
 TEST(StackCopierSignalTest, MAYBE_CopyStackTimestamp) {
   StackBuffer stack_buffer(/* buffer_size = */ 1 << 20);
-  UNSAFE_TODO(memset(stack_buffer.buffer(), 0, stack_buffer.size()));
+  std::ranges::fill(stack_buffer.as_span(), 0);
   uintptr_t stack_top = 0;
   TimeTicks timestamp;
   RegisterContext context;
@@ -178,7 +175,7 @@ TEST(StackCopierSignalTest, MAYBE_CopyStackTimestamp) {
 #endif
 TEST(StackCopierSignalTest, MAYBE_CopyStackDelegateInvoked) {
   StackBuffer stack_buffer(/* buffer_size = */ 1 << 20);
-  UNSAFE_TODO(memset(stack_buffer.buffer(), 0, stack_buffer.size()));
+  std::ranges::fill(stack_buffer.as_span(), 0);
   uintptr_t stack_top = 0;
   TimeTicks timestamp;
   RegisterContext context;
@@ -210,7 +207,7 @@ TEST(StackCopierSignalTest, MAYBE_CopyStackDelegateInvoked) {
 #endif
 TEST(StackCopierSignalTest, MAYBE_CopyStackFromOtherThread) {
   StackBuffer stack_buffer(/* buffer_size = */ 1 << 20);
-  UNSAFE_TODO(memset(stack_buffer.buffer(), 0, stack_buffer.size()));
+  std::ranges::fill(stack_buffer.as_span(), 0);
   uintptr_t stack_top = 0;
   TimeTicks timestamp;
   RegisterContext context{};
