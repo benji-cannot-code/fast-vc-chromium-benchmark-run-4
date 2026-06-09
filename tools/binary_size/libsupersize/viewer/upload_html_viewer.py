80cb906e03ffba79c920abb9db3d45ff1a0df2c3
Andrew Grieve <agrieve@chromium.org>
2026-06-08 22:53:23 -0700

    SuperSize: Fix service worker not updating right away
    The lack of skipWaiting() meant all supersize tabs needed to be closed
    for it to take effect.
    
    The lack of Cache-Control for sw.js meant new versions would not be
    picked up right away.
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Change-Id: Ifb9ffd9f0bae5e2d024f06fdb3f61e734d1ffbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7911861
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643705}

--- tools/binary_size/libsupersize/viewer/upload_html_viewer.py
+++ tools/binary_size/libsupersize/viewer/upload_html_viewer.py
@@ -57,6 +57,17 @@ def _FirebaseInitProjectDir(project_dir):
       "firebase.json",
       "**/README*",
       "**/.*"
+    ],
+    "headers": [
+      {
+        "source": "/sw.js",
+        "headers": [
+          {
+            "key": "Cache-Control",
+            "value": "no-cache, no-store, must-revalidate"
+          }
+        ]
+      }
     ]
   }
 }
