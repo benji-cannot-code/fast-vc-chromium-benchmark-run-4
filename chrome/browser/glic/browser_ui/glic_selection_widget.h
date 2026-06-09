aaa0e54e0bd12b373d6bab5de9a2ddf6b201bf8d
Sirui Wu <wusirui@google.com>
2026-06-09 06:20:57 -0700

    [Inline] Refactor Glic selection widget ownership and callbacks
    - Refactored selection widget ownership from NATIVE_WIDGET_OWNS_WIDGET
      to CLIENT_OWNS_WIDGET to allow direct management to controller.
    - Replaced the multiple unstructured event closures with a pure virtual
      callback interface: ActionDelegate.
    - Updated tests to cover the new logic.
    
    Bug: 520400815
    Change-Id: I53f64849c935a6e8e5bbbb1422dffa8dd879aa37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905627
    Auto-Submit: Sirui Wu <wusirui@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643900}

--- chrome/browser/glic/browser_ui/glic_selection_widget.h
+++ chrome/browser/glic/browser_ui/glic_selection_widget.h
@@ -9,12 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback.h"
+#include "base/memory/raw_ref.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
-namespace content {
-class WebContents;
-}
 
 namespace views {
 class Widget;
@@ -24,29 +22,27 @@ namespace glic {
 
 class GlicSelectionWidgetDelegate : public views::BubbleDialogDelegate {
  public:
-  // Shows the bubble anchored to `anchor_rect` with `on_click` executed when
-  // the Ask Gemini button is clicked. Parented to `web_contents`'s view.
-  static views::Widget* Show(content::WebContents* web_contents,
-                             const gfx::Rect& anchor_rect,
-                             const std::u16string& selected_text,
-                             bool is_pinned,
-                             base::RepeatingClosure on_ask_gemini,
-                             base::RepeatingClosure on_copy,
-                             base::RepeatingClosure on_copy_link,
-                             base::RepeatingCallback<void(bool)> on_pin_toggled,
-                             base::RepeatingClosure on_dismiss);
+  // Pure virtual interface implemented by the bridge to receive UI events.
+  class ActionDelegate {
+   public:
+    virtual void OnAskGemini() = 0;
+    virtual void OnCopy() = 0;
+    virtual void OnCopyLink() = 0;
+    virtual void OnPinToggled(bool is_pinned) = 0;
+    virtual void OnDismiss() = 0;
 
-  GlicSelectionWidgetDelegate(
-      const gfx::Rect& anchor_rect,
-      const gfx::Rect& window_bounds,
-      const std::u16string& selected_text,
-      bool is_pinned,
-      base::RepeatingClosure on_ask_gemini,
-      base::RepeatingClosure on_copy,
-      base::RepeatingClosure on_copy_link,
-      base::RepeatingCallback<void(bool)> on_pin_toggled,
-      base::RepeatingClosure on_dismiss);
+   protected:
+    virtual ~ActionDelegate() = default;
+  };
+
+  GlicSelectionWidgetDelegate(ActionDelegate& action_delegate,
+                              const gfx::Rect& anchor_rect,
+                              const gfx::Rect& window_bounds,
+                              const std::u16string& selected_text,
+                              bool is_pinned);
   ~GlicSelectionWidgetDelegate() override;
+
+  ActionDelegate& action_delegate() const { return *action_delegate_; }
 
   void TogglePinState();
   void UpdatePosition();
@@ -59,10 +55,10 @@ class GlicSelectionWidgetDelegate : public views::BubbleDialogDelegate {
   void UpdateCopyLinkButton(bool enabled);
 
  private:
+  const raw_ref<ActionDelegate> action_delegate_;
   gfx::Rect original_anchor_rect_;
   gfx::Rect window_bounds_;
   bool is_pinned_;
-  base::RepeatingCallback<void(bool)> on_pin_toggled_callback_;
 };
 
 }  // namespace glic
--- chrome/browser/glic/glic_selection_observer.h
+++ chrome/browser/glic/glic_selection_observer.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_observer.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/link_to_text/link_to_text.mojom.h"
+#include "ui/views/widget/widget.h"
 
 namespace content {
 class Page;
@@ -41,6 +42,7 @@ namespace glic {
 
 enum class GlicNudgeActivity;
 
+class GlicSelectionWidgetDelegate;
 class GlicKeyedService;
 
 class GlicSelectionObserver
@@ -153,7 +155,24 @@ class GlicSelectionObserver
   // updates until the input event completes.
   bool is_selecting_ = false;
 
-  base::WeakPtr<views::Widget> selection_widget_;
+  void OnAskGemini();
+  void OnCopy();
+  void OnCopyLink();
+  void OnPinToggled(bool is_pinned);
+  void OnDismiss();
+
+  // Private bridge implementation of
+  // GlicSelectionWidgetDelegate::ActionDelegate. This is required because
+  // GlicSelectionObserver (in the //chrome/browser/glic) cannot directly
+  // implement the UI-defined ActionDelegate interface to prevent circular
+  // target dependencies in the build configuration.
+  class WidgetActionDelegate;
+
+  void OnWidgetClosed(views::Widget::ClosedReason reason);
+
+  std::unique_ptr<views::Widget> selection_widget_;
+  std::unique_ptr<GlicSelectionWidgetDelegate> widget_delegate_;
+  std::unique_ptr<WidgetActionDelegate> action_delegate_;
 
   mojo::Remote<blink::mojom::TextFragmentReceiver> text_fragment_remote_;
   std::optional<GURL> generated_link_;
