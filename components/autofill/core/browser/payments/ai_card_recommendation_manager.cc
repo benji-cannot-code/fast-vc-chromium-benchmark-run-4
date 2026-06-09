c71382de41580a774390a706ea98d1f3cae46ec5
Haochen Feng <haochenf@google.com>
2026-06-09 14:57:05 -0700

    [AF] Create AI Card Recommendation Skeleton Class
    This CL adds the AiCardRecommendationManager class, owned by
    BrowserAutofillManager. This is for the autofill AI card
    recommendation intern project.
    
    Design: go/autofill-ai-card-recommendations-design
    Fixed: 519722428
    Change-Id: Ieb8294bc8c8379ac7ed353e57b7947e088337e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904543
    Commit-Queue: Haochen Feng <haochenf@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644248}

---
+++ components/autofill/core/browser/payments/ai_card_recommendation_manager.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/payments/ai_card_recommendation_manager.h"
+
+#include "base/check_deref.h"
+#include "components/autofill/core/browser/foundations/browser_autofill_manager.h"
+
+namespace autofill::payments {
+
+AiCardRecommendationManager::AiCardRecommendationManager(
+    BrowserAutofillManager* browser_autofill_manager)
+    : browser_autofill_manager_(CHECK_DEREF(browser_autofill_manager)) {}
+
+AiCardRecommendationManager::~AiCardRecommendationManager() = default;
+
+}  // namespace autofill::payments
