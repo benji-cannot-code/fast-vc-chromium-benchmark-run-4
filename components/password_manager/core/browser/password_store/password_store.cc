460d21b1319a5e77f9f43dc42678900904755367
Etienne Pierre-doray <etiennep@chromium.org>
2026-06-09 15:37:43 -0700

    [tracing] Migrate async track events to more appropriate alternatives in components/password_manager
    This CL changes async events on custom tracks (TRACE_EVENT_BEGIN/_END)
    to more appropriate alternatives: either on-thread events connected with flows,
    on synchronous events.
    
    There's a tradeof to using async events:
    - async events make tracking duration easier both in the UI and in queries
    - async events take a lot of vertical space in tracing UI
    - async events loses context of where the start and end happen
    - custom track are tricky to use, and  Track::FromPointer is problematic because it can alias when pointers are reused.
    https://chromium.googlesource.com/chromium/src.git/+/HEAD/docs/trace_events.md#asynchronous-events
    
    As a general rule of thumb:
    - synchronous events are more appropriate when a slice is well nested in a function
    - connected flows are more appropriate when the start and end don't wrap a clear operation
      (e.g. scheduling delay is better represented as flows)
    
    This CL was uploaded by git cl split.
    
    Bug: 432427382
    Change-Id: I8040dcdf730a088a1e91635c1997d753900e3b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869412
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644283}

--- components/password_manager/core/browser/password_store/password_store.cc
+++ components/password_manager/core/browser/password_store/password_store.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_store/psl_matching_helper.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/sync/model/proxy_data_type_controller_delegate.h"
+#include "third_party/perfetto/include/perfetto/tracing/track_event_args.h"
 
 namespace password_manager {
 
@@ -77,8 +78,8 @@ void PasswordStore::Init() {
   DCHECK(main_task_runner_);
 
   DCHECK(backend_);
-  TRACE_EVENT_BEGIN("passwords", "PasswordStore::InitOnBackgroundSequence",
-                    perfetto::Track::FromPointer(this));
+  TRACE_EVENT_INSTANT("passwords", "PasswordStore::InitOnBackgroundSequence",
+                      perfetto::Flow::FromPointer(this));
   backend_->InitBackend(
       base::BindRepeating(&PasswordStore::NotifyLoginsChangedOnMainSequence,
                           this, LoginsChangedTrigger::ExternalUpdate),
@@ -463,9 +464,8 @@ void PasswordStore::OnInitCompleted(bool success) {
     std::move(post_init_callback_).Run();
   }
 
-  TRACE_EVENT_END("passwords",
-                  /* PasswordStore::InitOnBackgroundSequence */ perfetto::
-                      Track::FromPointer(this));
+  TRACE_EVENT_INSTANT("passwords", "PasswordStore::OnInitCompleted",
+                      perfetto::TerminatingFlow::FromPointer(this));
 }
 
 void PasswordStore::NotifyLoginsChangedOnMainSequence(
@@ -501,8 +501,8 @@ void PasswordStore::NotifyLoginsChangedOnMainSequence(
   base::UmaHistogramEnumeration(
       "PasswordManager.PasswordStore.OnLoginsRetained", logins_changed_trigger);
   if (!changes.has_value()) {
-    TRACE_EVENT_BEGIN("passwords", "LoginsRetrievedForOnLoginsRetained",
-                      perfetto::Track::FromPointer(this));
+    TRACE_EVENT_INSTANT("passwords", "LoginsRetrievedForOnLoginsRetained",
+                        perfetto::Flow::FromPointer(this));
     // If the changes aren't provided, the store propagates the latest logins.
     backend_->GetAllLoginsAsync(base::BindOnce(
         &PasswordStore::NotifyLoginsRetainedOnMainSequence, this));
@@ -545,10 +545,9 @@ void PasswordStore::NotifyLoginsRetainedOnMainSequence(
   }
 
 #if BUILDFLAG(IS_ANDROID)
-  TRACE_EVENT_END(
-      "passwords",
-      /* LoginsRetrievedForOnLoginsRetained */ perfetto::Track::FromPointer(
-          this));
+  TRACE_EVENT_INSTANT("passwords",
+                      "PasswordStore::NotifyLoginsRetainedOnMainSequence",
+                      perfetto::TerminatingFlow::FromPointer(this));
 #endif
 }
 
