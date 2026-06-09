45f67b3a36c24502a1b819b6c1ea4d8faebbb495
Anna Tsvirchkova <atsvirchkova@google.com>
2026-06-09 00:44:52 -0700

    [APC] Display all credentials in Password check if MarkAllCredentialsAsLeaked enabled
    This feature is meant for conveniet testing of the password change from
    Checkup.
    
    Bug: 521322096
    Change-Id: I273442cd2ca02f142b1baa34a343ebc54cd78a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902283
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643752}

--- components/password_manager/core/browser/ui/insecure_credentials_manager.cc
+++ components/password_manager/core/browser/ui/insecure_credentials_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/metrics/field_trial_params.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/thread_pool.h"
+#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/leak_detection/bulk_leak_check.h"
 #include "components/password_manager/core/browser/password_form.h"
 #include "components/password_manager/core/browser/ui/credential_ui_entry.h"
@@ -147,6 +149,15 @@ InsecureCredentialsManager::GetInsecureCredentialEntries() const {
       presenter_->GetSavedCredentials();
 
   for (auto& credential : credentials) {
+    if (base::FeatureList::IsEnabled(
+            password_manager::features::kMarkAllCredentialsAsLeaked)) {
+      credential.password_issues.insert(
+          {password_manager::InsecureType::kLeaked,
+           password_manager::InsecurityMetadata(
+               base::Time(), IsMuted(false),
+               TriggerBackendNotification(false))});
+      continue;
+    }
     if (weak_passwords_.contains(credential.password)) {
       credential.password_issues.insert(
           {password_manager::InsecureType::kWeak,
