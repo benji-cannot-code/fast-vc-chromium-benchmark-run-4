fb98e57409d0252b4271e7c8302309e283ae7dff
Elmehdi Rahmaoui <erahmaoui@google.com>
2026-06-09 06:23:01 -0700

    [iOS][composebox] Add Drive string and associate it to its attachment
    Bug: 521745172
    Change-Id: Ie548dc85e7dba8a0fb34b0f22b96452bc02e4860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7912944
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643903}

--- ios/chrome/browser/composebox/ui/composebox_strings.mm
+++ ios/chrome/browser/composebox/ui/composebox_strings.mm
@@ -118,8 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     case kCamera:
       return l10n_util::GetNSString(IDS_IOS_COMPOSEBOX_CAMERA_ACTION);
     case kDrive:
-      // TODO(crbug.com/515377633): Add string translation.
-      return @"Drive";
+      return l10n_util::GetNSString(IDS_IOS_COMPOSEBOX_DRIVE_ACTION);
   }
 }
 
