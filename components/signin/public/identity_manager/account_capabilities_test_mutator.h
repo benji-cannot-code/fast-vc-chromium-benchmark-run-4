0109805ebd897876c92591e26a0275db204dfb75
Alex Ilin <alexilin@chromium.org>
2026-06-09 08:43:00 -0700

    [Signin] Allow building AccountCapabilitiesTestMutator from AccountInfo*
    The ongoing AccountInfo refactoring will move the `capabilities` class
    member to the private section.
    
    `AccountCapabilitiesTestMutator(account_info.capabilities)` will no
    longer be possible. The currently supported way of updating account
    capabilities in tests via
    `AccountInfo::Builder::UpdateAccountCapabilitiesWith()` is too verbose
    if the goal is to update capabilities of an existing AccountInfo.
    
    Instead, let's allow tests to build a mutator instance from
    AccountInfo*.
    
    Bug: 458409080
    Change-Id: I12320a1a29382300f70b0a92c6d0813817d7ca8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910063
    Commit-Queue: Monica Salama <msalama@chromium.org>
    Reviewed-by: Monica Salama <msalama@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643994}

--- components/signin/public/identity_manager/account_capabilities_test_mutator.h
+++ components/signin/public/identity_manager/account_capabilities_test_mutator.h
@@ -13,11 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/signin/public/identity_manager/account_capabilities.h"
 
+struct AccountInfo;
+
 // Support class that allows callers to modify internal capability state
 // mappings used for tests.
 class AccountCapabilitiesTestMutator {
  public:
   explicit AccountCapabilitiesTestMutator(AccountCapabilities* capabilities);
+  explicit AccountCapabilitiesTestMutator(AccountInfo* account_info);
 
   // Exposes the full list of supported capabilities for tests.
   static base::span<const std::string_view>
