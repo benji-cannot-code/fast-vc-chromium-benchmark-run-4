1db3dee43004f94414d69b43f25787f4694fc5ac
Ali Hijazi <ahijazi@chromium.org>
2026-06-09 03:22:44 -0700

    [Signin] Show mTLS token binding info in WebUI
    Expose the `mtlsTokenBinding` flag for each account on the
    `chrome://signin-internals` page.
    
    This allows developers to inspect whether an account's refresh token
    is bound to an mTLS certificate.
    
    Change-Id: I0a4431f46306c4cbf55dca4ec907d7b4dd948f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900503
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643834}

--- components/signin/core/browser/about_signin_internals.cc
+++ components/signin/core/browser/about_signin_internals.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/hash/hash.h"
 #include "base/i18n/time_formatting.h"
 #include "base/logging.h"
@@ -834,6 +835,11 @@ base::DictValue AboutSigninInternals::SigninStatus::ToValue(
               signin_client->GetPrefs())) {
         entry.Set("isBound", identity_manager->HasAccountWithBoundRefreshToken(
                                  account_info.account_id));
+      }
+      if (base::FeatureList::IsEnabled(switches::kEnableMtlsTokenBinding)) {
+        entry.Set("mtlsTokenBinding",
+                  identity_manager->HasAccountWithRefreshTokenBoundToMtls(
+                      account_info.account_id));
       }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
       account_info_section.Append(std::move(entry));
