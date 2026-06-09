9c617b6e2a1d0a90fafc12b627a10c57476d7ab9
Sana Akbani <sanaakbani@google.com>
2026-06-08 18:01:17 -0700

    Reenable GlicApiTestWithOneTab.testDefaultInvocationSource
    This test is safe to reenable because the default is to test
    multi-instance now. This test only failed with single instance.
    
    Bug: b:486793948
    Change-Id: I6ac03ac79aec9f9af74de31279b4a0642c68dacb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706154
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643576}

--- chrome/browser/glic/host/glic_api_browsertest.cc
+++ chrome/browser/glic/host/glic_api_browsertest.cc
@@ -727,9 +727,7 @@ IN_PROC_BROWSER_TEST_P(GlicApiTestGeminiEnterpriseSettingsPolicyUnset,
   ExecuteJsTest();
 }
 
-// TODO(crbug.com/486793948): Fix and re-enable or remove the test.
-IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab,
-                       DISABLED_testDefaultInvocationSource) {
+IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab, testDefaultInvocationSource) {
   RunTestSequence(CloseGlic(), WaitForGlicClose(),
                   ToggleGlicWindowFromSource(
                       GlicWindowMode::kAttached, kGlicButtonElementId,
