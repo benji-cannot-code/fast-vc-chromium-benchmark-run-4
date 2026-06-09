1fe481322e6fe50e6c953b948241c571e0ce5de0
Shweta Bindal <shwetabindal@microsoft.com>
2026-06-08 23:58:48 -0700

    Change ClipboardItem::getType() rejection to InvalidStateError
    ClipboardItem.getType() should reject with
    InvalidStateError when the clipboard data has changed between read()
    and getType(), or when a null blob is returned from the clipboard
    reader. Chromium was using DataError at these rejection sites in
    clipboard_item.cc. This changes them to InvalidStateError for spec
    compliance.
    
    Bug: 519633341
    Change-Id: Idd2458219b4d2b1b706b644e3f40ca84c1254057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902351
    Commit-Queue: Shweta Bindal <shwetabindal@microsoft.com>
    Reviewed-by: Rohan Raja <roraja@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1643732}

--- third_party/blink/renderer/modules/clipboard/clipboard_item.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_item.cc
@@ -168,7 +168,7 @@ void ClipboardItem::ResolveFormatData(const String& mime_type, Blob* blob) {
                             clipboard_changed);
   if (clipboard_changed) {
     representations_with_resolvers_.at(mime_type)->Reject(
-        MakeGarbageCollected<DOMException>(DOMExceptionCode::kDataError,
+        MakeGarbageCollected<DOMException>(DOMExceptionCode::kInvalidStateError,
                                            "Clipboard data has changed"));
     return;
   }
@@ -176,7 +176,7 @@ void ClipboardItem::ResolveFormatData(const String& mime_type, Blob* blob) {
   base::UmaHistogramBoolean("Blink.Clipboard.LazyRead.NullBlobResolved", !blob);
   if (!blob) {
     representations_with_resolvers_.at(mime_type)->Reject(
-        MakeGarbageCollected<DOMException>(DOMExceptionCode::kDataError,
+        MakeGarbageCollected<DOMException>(DOMExceptionCode::kInvalidStateError,
                                            "Failed to read clipboard data."));
     return;
   }
@@ -224,7 +224,7 @@ ScriptPromise<Blob> ClipboardItem::getType(ScriptState* script_state,
   base::UmaHistogramBoolean("Blink.Clipboard.LazyRead.GetTypeRejected",
                             clipboard_changed);
   if (clipboard_changed) {
-    exception_state.ThrowDOMException(DOMExceptionCode::kDataError,
+    exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                       "Clipboard data has changed");
     return ScriptPromise<Blob>();
   }
