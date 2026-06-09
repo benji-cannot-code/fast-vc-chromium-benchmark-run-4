5b2b8570a4e8917b4259970437840ab67da73fe3
Grace Cham <hscham@google.com>
2026-06-08 19:39:18 -0700

    Support archived tab lookup in TabWindowManager.get*Tab*
    Similar to crrev.com/c/7703458, except that getIncognitoTabCount() does
    not need to look up archived tabs since they are never incognito.
    
    Follow-up of crrev.com/c/7706450/comments/c53a763c_cddec3e4.
    
    Bug: None
    Test: TabWindowManagerImplUnitTest
    Change-Id: Ie9c50c3054cc7706bd318dbf1761f3fc912b181c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901316
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643632}

--- chrome/browser/tabwindow/internal/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowManagerImpl.java
+++ chrome/browser/tabwindow/internal/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowManagerImpl.java
@@ -479,6 +479,11 @@ public class TabWindowManagerImpl implements TabWindowManager {
             }
         }
 
+        if (mArchivedTabModelSelector != null) {
+            TabModel tabModel = mArchivedTabModelSelector.getModelForTabId(tab.getId());
+            if (tabModel != null) return tabModel;
+        }
+
         return null;
     }
 
@@ -521,6 +526,16 @@ public class TabWindowManagerImpl implements TabWindowManager {
                 @Nullable final Tab tab = tabModel.getTabById(tabId);
                 if (tab != null) {
                     return new TabWindowInfo(INVALID_WINDOW_ID, selector, tabModel, tab);
+                }
+            }
+        }
+
+        if (mArchivedTabModelSelector != null) {
+            for (TabModel tabModel : mArchivedTabModelSelector.getModels()) {
+                @Nullable final Tab tab = tabModel.getTabById(tabId);
+                if (tab != null) {
+                    return new TabWindowInfo(
+                            INVALID_WINDOW_ID, mArchivedTabModelSelector, tabModel, tab);
                 }
             }
         }
