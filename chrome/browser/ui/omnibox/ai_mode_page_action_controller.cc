7c178e6b475ba17d577826aafdcd28102ecce7cf
manukh <manukh@chromium.org>
2026-06-09 09:58:43 -0700

    [omnibox][3p-aim] Size icons
    crrev.com/c/7887475 & crrev.com/c/7887742 began showing 3p icons. But 3p
    icons fetched from arbitrary .ico URLs can be of any size. Icons that
    are too large will cause layout issues and `CHECK` crashes.
    
    This CL sizes the icon to `kLocationBarChipIconSize`. The google
    built-icon icon is also sized to `kLocationBarChipIconSize`.
    
    Bug: b:517976330
    Change-Id: I074393a0b7608782585aba7c9e9d8f9b870fe965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7887549
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644062}

--- chrome/browser/ui/omnibox/ai_mode_page_action_controller.cc
+++ chrome/browser/ui/omnibox/ai_mode_page_action_controller.cc
@@ -36,11 +36,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/search_engines/search_engine_type.h"
 #include "components/tabs/public/tab_interface.h"
+#include "skia/ext/image_operations.h"
 #include "third_party/metrics_proto/omnibox_event.pb.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/gfx/image/image.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/view.h"
@@ -56,6 +59,22 @@ page_actions::PageActionController* GetPageActionController(
   }
 
   return tab_interface->GetTabFeatures()->page_action_controller();
+}
+
+// Converts an `Image` of arbitrary size to an `ImageModel` of
+// `kLocationBarChipIconSize` size.
+ui::ImageModel SizedImageModel(const gfx::Image& image) {
+  gfx::Size target_size = {
+      GetLayoutConstant(LayoutConstant::kLocationBarChipIconSize),
+      GetLayoutConstant(LayoutConstant::kLocationBarChipIconSize)};
+  if (image.Size() == target_size) {
+    return ui::ImageModel::FromImage(image);
+  }
+
+  gfx::ImageSkia image_skia = image.AsImageSkia();
+  image_skia = gfx::ImageSkiaOperations::CreateResizedImage(
+      image_skia, skia::ImageOperations::RESIZE_BEST, target_size);
+  return ui::ImageModel::FromImageSkia(image_skia);
 }
 
 }  // namespace
@@ -244,7 +263,7 @@ void AiModePageActionController::SetPageActionVisibility(bool is_visible) {
     // `image` will be empty if not cached. In which case, let
     // `OnFaviconFetchedLocally()` handle visibility and the image.
     if (!image.IsEmpty()) {
-      ShowAndOverrideImage(ui::ImageModel::FromImage(image));
+      ShowAndOverrideImage(SizedImageModel(image));
     }
   }
 }
@@ -276,7 +295,7 @@ void AiModePageActionController::OnFaviconFetchedLocally(
     FetchFaviconFromNetwork(favicon_url);
     return;
   }
-  ShowAndOverrideImage(ui::ImageModel::FromImage(favicon));
+  ShowAndOverrideImage(SizedImageModel(favicon));
 }
 
 void AiModePageActionController::FetchFaviconFromNetwork(
@@ -301,8 +320,8 @@ void AiModePageActionController::OnFaviconFetchedFromNetwork(SkBitmap bitmap) {
     Hide();
     return;
   }
-  gfx::Image image = gfx::Image::CreateFrom1xBitmap(bitmap);
-  ShowAndOverrideImage(ui::ImageModel::FromImage(image));
+  gfx::ImageSkia image_skia = gfx::ImageSkia::CreateFrom1xBitmap(bitmap);
+  ShowAndOverrideImage(SizedImageModel(gfx::Image(image_skia)));
 }
 
 }  // namespace omnibox
