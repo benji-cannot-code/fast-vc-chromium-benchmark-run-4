32519f5a762e75d8c9edc4402999f304ecbcf10a
Javier Fernández García-Boente <jfernandez@igalia.com>
2026-06-09 05:15:01 -0700

    Reland "Extension's handlers honor the AllowInIcongnito setting"
    This is a reland of commit 7b1659736a4a88412f6361f51804b5645e724ca3
    
    The regressions detected by the mac15 bot have been caused by the
    Testing Factory used specifically for the MAC platform; the other
    platforms do not need a custom testing factory because the rely on the
    one instantiated by chrome's keyed services initialization (there is a
    TODO to investigate why this affects only to MAC platforms). The custom
    testing factory needs a non-null PrefService to complete the OTR related
    tests. Additionally, this custom testing factory uses a Testing Delegate
    to avoid the OS registration issues; this pattern is also used in other
    browser tests of this kind.
    
    Original change's description:
    > Extension's handlers honor the AllowInIcongnito setting
    >
    > Extensions with a 'false' value in their AllowInIncognito setting must
    > not have access to the user's profile data when running the browser in
    > incognito mode. As long as the handlers registered by an extension are
    > stored in the user's profile, these handlers' availability should be
    > determined based on the extension's settings.
    >
    > This CL adds a new attribute in the ProtocolHandler class to hold the
    > corresponding extension's AllowInIncognito setting value. Regular
    > handlers have this attribute set to 'true' by default. Additionally, the
    > ProtocolHandlerRegistry instance should know when the browser is running
    > under incognito mode. We have decided to add a new attribute to hold
    > this flag, to be updated whenever the Factory is invoked to get the
    > singleton instance.
    >
    > In the crrev.com/7735521 we have changed the
    > ProtocolHandlerRegistryFactory so that it received a nullptr PrefService
    > to avoid accessing the registered protocol handlers in incognito mode.
    > We had to revert that change in this CL and move the logic to the
    > ProtocolHandlerRegistry itself. The reason is that we want to allow an
    > Extension to access the registered handlers if the allow_in_incognito
    > setting is enabled. However, the changes in this CL still ensure regular
    > (non-extension) handlers can't be accessed using OTR profiles.
    >
    > Finally, in this CL I changed some tests to avoid using GURL on literal
    > URLs so that we can avoid unneeded spec parsing.
    >
    > Bug: 483333979
    > Change-Id: I7f61a43c24410b7d53d9bad91cd503dcb2c73c52
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7688600
    > Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    > Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    > Cr-Commit-Position: refs/heads/main@{#1634784}
    
    Bug: 483333979
    Change-Id: Idafb663e10ff4e963f16924abb33a6aee2a69cb0
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|browser_tests
    Cq-Include-Trybots: luci.chromium.try:mac15-x64-rel-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867247
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1643871}

--- components/custom_handlers/simple_protocol_handler_registry_factory.cc
+++ components/custom_handlers/simple_protocol_handler_registry_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/custom_handlers/protocol_handler_registry.h"
 #include "components/custom_handlers/test_protocol_handler_registry_delegate.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "content/public/browser/browser_context.h"
 
 namespace custom_handlers {
 
@@ -28,7 +29,8 @@ std::unique_ptr<KeyedService> BuildProtocolHandlerRegistryService(
   // We can't ensure the UserPref has been set, so we pass a nullptr
   // PrefService.
   return custom_handlers::ProtocolHandlerRegistry::Create(
-      nullptr, std::make_unique<TestProtocolHandlerRegistryDelegate>());
+      nullptr, std::make_unique<TestProtocolHandlerRegistryDelegate>(),
+      context->IsOffTheRecord());
 }
 
 // static
