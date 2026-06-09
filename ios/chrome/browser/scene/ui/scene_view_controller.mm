013c40cd78aec6e3dc7a9b341c7c3d5d2d0faa7e
Gauthier Ambard <gambard@chromium.org>
2026-06-09 08:27:30 -0700

    [iOS] Replace dispatch_after by post task
    Bug: None
    Change-Id: I84a6161dde4b7b7ee8592b0c24018e9d496a4e20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7912185
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643987}

--- ios/chrome/browser/scene/ui/scene_view_controller.mm
+++ ios/chrome/browser/scene/ui/scene_view_controller.mm
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <QuartzCore/QuartzCore.h>
 
 #import "base/check.h"
+#import "base/functional/bind.h"
+#import "base/location.h"
+#import "base/task/sequenced_task_runner.h"
+#import "base/time/time.h"
 #import "base/trace_event/trace_event.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/app_bar/ui/app_bar_constants.h"
@@ -742,11 +746,11 @@ constexpr NSTimeInterval kIPHTransitionDelay = 0.5;
   [self.appBarHandler hideIPHBackground];
   if (reason == IPHDismissalReasonType::kTappedNext && geminiEligible) {
     __weak __typeof(self) weakSelf = self;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
-                                 (int64_t)(kIPHTransitionDelay * NSEC_PER_SEC)),
-                   dispatch_get_main_queue(), ^{
-                     [weakSelf showSecondIAPromo];
-                   });
+    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+        FROM_HERE, base::BindOnce(^{
+          [weakSelf showSecondIAPromo];
+        }),
+        base::Seconds(kIPHTransitionDelay));
   } else {
     [self.mutator newIAPromoIPHDismissed];
   }
