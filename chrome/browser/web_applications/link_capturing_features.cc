6e436d3f2917acba305c576b7323b866ed2b489e
Dibyajyoti Pal <dibyapal@google.com>
2026-06-09 13:44:32 -0700

    [PWA] Enable the updated app strings on the settings page by default
    This CL also removes the
    `AppManagementPageHandlerTestBase.NavigationCapturingUserChoice` test
    since `AppManagementPageHandlerWithUpdateStringsTest` verifies all
    the use-cases where an app opening in a browser tab can have the
    navigation capturing user UX disabled.
    
    Bug: 520039009
    Change-Id: I4a66769d29dcc9283f294a88cb609d430560997b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914961
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644228}

--- chrome/browser/web_applications/link_capturing_features.cc
+++ chrome/browser/web_applications/link_capturing_features.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace apps::features {
 
-BASE_FEATURE(kUpdateAppStringsOnSettings, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUpdateAppStringsOnSettings, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // TODO(crbug.com/377760841): Remove dead code flag; never enabled.
 BASE_FEATURE(kNavigationCapturingOnExistingFrames,
