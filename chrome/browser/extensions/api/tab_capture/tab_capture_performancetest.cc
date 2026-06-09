5713a15f83575e9ffb7d22feff3709acdd8feabe
James Cook <jamescook@chromium.org>
2026-06-09 11:57:30 -0700

    extensions: Fix exit-time destructors in browser tests
    This is a step towards enabling the compile-time warning globally.
    
    Bug: 40031409
    Change-Id: Id63a6a02061a8852e5bc2536ebfe00b8035d5dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910422
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644146}

--- chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
@@ -53,17 +53,16 @@ constexpr char kMetricCaptureLatencyMs[] = "capture_latency";
 constexpr char kMetricRendererFrameDrawMs[] = "renderer_frame_draw";
 
 constexpr char kEventCapture[] = "Capture";
-constexpr char kEventSuffixFailRate[] = "FailRate";
-constexpr char kEventSuffixLatency[] = "Latency";
+constexpr char kEventCaptureFailRate[] = "CaptureFailRate";
+constexpr char kEventCaptureLatency[] = "CaptureLatency";
 constexpr char kEventCommitAndDrawCompositorFrame[] =
     "WidgetBase::DidCommitAndDrawCompositorFrame";
-const base::flat_map<std::string, std::string> kEventToMetricMap(
-    {{kEventCapture, kMetricCaptureMs},
-     {std::string(kEventCapture) + kEventSuffixFailRate,
-      kMetricCaptureFailRatePercent},
-     {std::string(kEventCapture) + kEventSuffixLatency,
-      kMetricCaptureLatencyMs},
-     {kEventCommitAndDrawCompositorFrame, kMetricRendererFrameDrawMs}});
+constexpr auto kEventToMetricMap =
+    base::MakeFixedFlatMap<std::string_view, std::string_view>(
+        {{kEventCapture, kMetricCaptureMs},
+         {kEventCaptureFailRate, kMetricCaptureFailRatePercent},
+         {kEventCaptureLatency, kMetricCaptureLatencyMs},
+         {kEventCommitAndDrawCompositorFrame, kMetricRendererFrameDrawMs}});
 
 perf_test::PerfResultReporter SetUpTabCaptureReporter(
     const std::string& story) {
@@ -75,7 +74,7 @@ perf_test::PerfResultReporter SetUpTabCaptureReporter(
   return reporter;
 }
 
-std::string GetMetricFromEventName(const std::string& event_name) {
+std::string_view GetMetricFromEventName(const std::string& event_name) {
   auto iter = kEventToMetricMap.find(event_name);
   return iter == kEventToMetricMap.end() ? event_name : iter->second;
 }
@@ -124,6 +123,10 @@ class TabCapturePerformanceTest : public TabCapturePerformanceTestBase,
  public:
   TabCapturePerformanceTest() = default;
   ~TabCapturePerformanceTest() override = default;
+
+  // Member constants to avoid exit-time destructor.
+  const std::string kEventSuffixLatency = "Latency";
+  const std::string kEventSuffixFailRate = "FailRate";
 
   bool HasFlag(TestFlags flag) const {
     return (GetParam() & flag) == flag;
