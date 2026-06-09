e8720f05582ef19b61dd49e1ef8eae6d98c293a6
Thomas Nguyen <tungnh@chromium.org>
2026-06-09 01:26:34 -0700

    [CE]: Remove size update flicker in HTMLGeolocationElement
    Eliminate the visual flickering caused by unnecessary size updates in
    the geolocation element.
    - Moving `UpdateText()` out of `PostTask` to run synchronously during
      `UpdateAppearance()`.
    -`UpdateText()` is called once the shadow root is attached.
    - Adding a defensive check in `UpdateText()` to only update the inner
      text if the new content actually differs from the current text,
      preventing redundant layout passes.
    
    Fixed: 505631016
    Change-Id: I4ab1bff9c51b84822668e7adec543886acdd134b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902282
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643771}

--- third_party/blink/renderer/core/html/html_geolocation_element.h
+++ third_party/blink/renderer/core/html/html_geolocation_element.h
@@ -78,6 +78,7 @@ class CORE_EXPORT HTMLGeolocationElement final
   void OnPermissionStatusChange(mojom::blink::PermissionName,
                                 mojom::blink::PermissionStatus) override;
   void DidFinishLifecycleUpdate(const LocalFrameView&) override;
+  void DidAddUserAgentShadowRoot(ShadowRoot&) override;
 
   void OnActivated();
   void GetCurrentPosition();
