7bcfa2e86e8145c68012776c1866465fdc210805
Isabelle Goralsky <igoralsky@google.com>
2026-06-08 17:29:55 -0700

    Add ContextHubService as a ProfileKeyedService.
    This change introduces a new directory //chrome/browser/context_hub and
    a basic ContextHubService implemented as a ProfileKeyedService. A
    factory is provided to create instances of the service, which are
    currently only available for regular profiles with ContextHub feature
    flag enabled. Unit tests are included for the factory.
    
    Bug: b:521498980
    Change-Id: If640e5be223b0b997137de2ea874cec38bc01e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910741
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643560}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_index/content_index_provider_factory.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/context_hub/context_hub_service_factory.h"
 #include "chrome/browser/contextual_cueing/contextual_cueing_service_factory.h"
 #include "chrome/browser/contextual_search/contextual_search_service_factory.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_context_service_factory.h"
@@ -895,6 +896,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   ContentAnnotatorServiceFactory::GetInstance();
 #endif
 
+  ContextHubServiceFactory::GetInstance();
   contextual_cueing::ContextualCueingServiceFactory::GetInstance();
 
   contextual_tasks::ContextualTasksContextServiceFactory::GetInstance();
