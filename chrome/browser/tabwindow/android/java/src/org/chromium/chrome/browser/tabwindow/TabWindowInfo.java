589fe358e315adbbcbb8f66cec6b4ef57fa0c187
Grace Cham <hscham@google.com>
2026-06-08 18:40:55 -0700

    Add TabWindowInfo.type to differentiate different window types
    Follow-up on crrev.com/c/7703458/comment/0d56d3c8_e36197c0/.
    
    Bug: None
    Test: TabWindowManagerImplUnitTest
    Change-Id: I3130e705d4c5e8d48931156ab95214ef2e7545a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706450
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Grace Cham <hscham@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Grace Cham <hscham@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643604}

--- chrome/browser/tabwindow/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowInfo.java
+++ chrome/browser/tabwindow/android/java/src/org/chromium/chrome/browser/tabwindow/TabWindowInfo.java
@@ -5,10 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabwindow;
 
+import androidx.annotation.IntDef;
+
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabmodel.TabModelType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Holds objects related to a tab and where it lives. CCTs will have an invalid windowId {@link
@@ -16,10 +22,28 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
  */
 @NullMarked
 public class TabWindowInfo {
+    /** The different types of selector instances. */
+    @IntDef({
+        TabWindowType.INVALID,
+        TabWindowType.TABBED,
+        TabWindowType.HEADLESS,
+        TabWindowType.CUSTOM,
+        TabWindowType.ARCHIVED
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface TabWindowType {
+        int INVALID = -1;
+        int TABBED = 0;
+        int HEADLESS = 1;
+        int CUSTOM = 2;
+        int ARCHIVED = 3;
+    }
+
     public final @WindowId int windowId;
     public final TabModelSelector tabModelSelector;
     public final TabModel tabModel;
     public final Tab tab;
+    public final @TabWindowType int type;
 
     public TabWindowInfo(
             @WindowId int windowId, TabModelSelector tabModelSelector, TabModel tabModel, Tab tab) {
@@ -27,5 +51,19 @@ public class TabWindowInfo {
         this.tabModel = tabModel;
         this.tabModelSelector = tabModelSelector;
         this.windowId = windowId;
+        switch (tabModel.getTabModelType()) {
+            case TabModelType.STANDARD:
+                this.type = tab.isCustomTab() ? TabWindowType.CUSTOM : TabWindowType.TABBED;
+                break;
+            case TabModelType.HEADLESS:
+                this.type = TabWindowType.HEADLESS;
+                break;
+            case TabModelType.ARCHIVED:
+                this.type = TabWindowType.ARCHIVED;
+                break;
+            default:
+                this.type = TabWindowType.INVALID;
+                break;
+        }
     }
 }
