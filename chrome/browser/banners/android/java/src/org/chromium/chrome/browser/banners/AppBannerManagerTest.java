78e04c767a0c960f9b297c7736bc66aef3f07888
Huili Liu <huilliu@microsoft.com>
2026-06-08 19:17:03 -0700

    [Android] fix memory leak of AppBannerManagerTest
    Bug: 512492108
    Change-Id: I1a8e582f5b47f199b5640ebf392a9eb0fe6b12c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901817
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: huili liu <huilliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1643622}

--- chrome/browser/banners/android/java/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
+++ chrome/browser/banners/android/java/src/org/chromium/chrome/browser/banners/AppBannerManagerTest.java
@@ -27,6 +27,7 @@ import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiSelector;
 
 import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -44,7 +45,6 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisableLeakChecks;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.ShortcutHelper;
@@ -85,11 +85,6 @@ import org.chromium.ui.widget.ButtonCompat;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 // TODO(http://crbug.com/495529795): Enable side panel and fix this test
 @DisableFeatures({ChromeFeatureList.ENABLE_ANDROID_SIDE_PANEL})
-@DisableLeakChecks({
-    "crbug.com/512492299 (AppBannerManager)",
-    "crbug.com/512492108 (AppBannerManager)",
-    "crbug.com/512492577 (AppBannerManager)"
-})
 public class AppBannerManagerTest {
     @Rule
     public FreshCtaTransitTestRule mTabbedActivityTestRule =
@@ -215,6 +210,15 @@ public class AppBannerManagerTest {
                         .getActivity()
                         .getRootUiCoordinatorForTesting()
                         .getBottomSheetController();
+    }
+
+    @After
+    public void tearDown() {
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    AppBannerManager.setAppDetailsDelegate(null);
+                });
+        mDetailsDelegate = null;
     }
 
     private AppBannerManager getAppBannerManager(WebContents webContents) {
@@ -413,7 +417,8 @@ public class AppBannerManagerTest {
                     Assert.assertEquals(
                             1,
                             RecordHistogram.getHistogramValueCountForTesting(
-                                    "Webapp.Install.InstallEvent", 4 /* API_BROWSER_TAB */));
+                                    "Webapp.Install.InstallEvent",
+                                    /* sample= */ 4)); // API_BROWSER_TAB
 
                     Assert.assertEquals(
                             1,
@@ -448,7 +453,8 @@ public class AppBannerManagerTest {
                     Assert.assertEquals(
                             1,
                             RecordHistogram.getHistogramValueCountForTesting(
-                                    "Webapp.Install.InstallEvent", 5 /* API_CUSTOM_TAB */));
+                                    "Webapp.Install.InstallEvent",
+                                    /* sample= */ 5)); // API_CUSTOM_TAB
 
                     Assert.assertEquals(
                             1,
@@ -752,7 +758,8 @@ public class AppBannerManagerTest {
                     Assert.assertEquals(
                             1,
                             RecordHistogram.getHistogramValueCountForTesting(
-                                    "Webapp.Install.InstallEvent", 4 /* API_BROWSER_TAB */));
+                                    "Webapp.Install.InstallEvent",
+                                    /* sample= */ 4)); // API_BROWSER_TAB
 
                     Assert.assertEquals(
                             1,
