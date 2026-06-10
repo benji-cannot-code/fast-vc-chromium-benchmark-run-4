380a576ec13a7d98c0814d2f4380740b3e2d40d0
Muhammad Salmaan <musalmaan@chromium.org>
2026-06-10 11:55:25 -0700

    [infobar centralization] Add infobar migration flag framework
    Added infobar migration flag framework to migrate different infobars.
    
    Bug: 512837937
    Change-Id: I3cbb59cecaaa7b2a9d8b1b65b7c7789f77e5f91e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7916894
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644765}

--- chrome/browser/infobars/infobar_features.cc
+++ chrome/browser/infobars/infobar_features.cc
@@ -5,8 +5,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/infobars/infobar_features.h"
 
+#include <algorithm>
+#include <vector>
+
+#include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
+
 namespace infobars {
 
+constexpr char kDefaultMigratedInfoBars[] = "";
+
 BASE_FEATURE(kCentralizedInfoBarFramework, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE_PARAM(bool, kEnableAll, &kCentralizedInfoBarFramework, false);
+
+BASE_FEATURE_PARAM(std::string,
+                   kMigrated,
+                   &kCentralizedInfoBarFramework,
+                   kDefaultMigratedInfoBars);
+
+bool IsInfoBarMigrated(std::string_view infobar_id) {
+  if (!base::FeatureList::IsEnabled(kCentralizedInfoBarFramework)) {
+    return false;
+  }
+  if (kEnableAll.Get()) {
+    return true;
+  }
+  std::string migrated_list = kMigrated.Get();
+  std::vector<std::string_view> enabled_infobars = base::SplitStringPiece(
+      migrated_list, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  return std::find(enabled_infobars.begin(), enabled_infobars.end(),
+                   infobar_id) != enabled_infobars.end();
+}
 
 }  // namespace infobars
