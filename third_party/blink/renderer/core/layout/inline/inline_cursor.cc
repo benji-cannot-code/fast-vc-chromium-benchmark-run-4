ffcb830bace03dfc93d5caf7a32fc455cba488d5
Koji Ishii <kojii@chromium.org>
2026-06-10 16:07:09 -0700

    Fix `InlineItemsFor` for first-line of empty items
    http://crrev.com/c/7874376 added a mode where `InlineItemsFor`
    returns all items with `check_layout_object` set to true. In
    this mode, the items isn't empty, and that the caller's check
    of `items.empty()` doesn't cover cases such as `<br>`.
    
    This patch changes `InlineItemsFor` to return empty items even
    when the first line style has different items.
    
    Fixed: 517989170
    Change-Id: Iae54f744948ef3ffe39634d1acd1db1edd45b190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7917312
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644953}

--- third_party/blink/renderer/core/layout/inline/inline_cursor.cc
+++ third_party/blink/renderer/core/layout/inline/inline_cursor.cc
@@ -454,6 +454,10 @@ UBiDiLevel InlineCursorPosition::BidiLevel() const {
 
 std::pair<base::span<const Member<InlineItem>>, bool>
 InlineCursorPosition::InlineItemsFor(const LayoutText& layout_text) const {
+  const auto* const items = layout_text.GetInlineItems();
+  if (!items || items->empty()) [[unlikely]] {
+    return {{}, false};
+  }
   if (UsesFirstLineStyle() &&
       RuntimeEnabledFeatures::FirstLineTextTransformEnabled()) [[unlikely]] {
     if (const LayoutBlockFlow* block_flow =
@@ -465,11 +469,7 @@ InlineCursorPosition::InlineItemsFor(const LayoutText& layout_text) const {
       }
     }
   }
-  const auto* const items = layout_text.GetInlineItems();
-  if (items && !items->empty()) {
-    return {items->Items(), false};
-  }
-  return {{}, false};
+  return {items->Items(), false};
 }
 
 const DisplayItemClient* InlineCursorPosition::GetSelectionDisplayItemClient()
