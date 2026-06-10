31dae42511ed602162457b78057d001fa7e20396
Hitarth Kothari <hitarthkothari@google.com>
2026-06-10 07:08:57 -0700

    [TabBottomSheet] Control omnibox suppression ourselves
    Currently, the bottom sheet automatically suppresses itself when the omnibox is focused, causing requestShowContent to immediately return false.
    
    This CL updates TabBottomSheet to track the omnibox suppression state internally. By managing this state directly, requests to show content made while the omnibox is focused are no longer instantly discarded.
    
    Bug: 507491828
    Change-Id: I43f7e5b16cc9dbb83e9efa84ba9823ce0ebb5502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905655
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644616}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -1897,7 +1897,8 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                             mWindowAndroid,
                             assertNonNull(getBottomSheetController()),
                             mLayoutStateProviderOneShotSupplier,
-                            SupplierUtils.asNonNull(mCompositorViewHolderSupplier).get());
+                            SupplierUtils.asNonNull(mCompositorViewHolderSupplier).get(),
+                            getOmniboxFocusStateSupplier());
             Callback<ReadAloudController> callback =
                     mCallbackController.makeCancelable(
                             (ReadAloudController readAloudController) -> {
