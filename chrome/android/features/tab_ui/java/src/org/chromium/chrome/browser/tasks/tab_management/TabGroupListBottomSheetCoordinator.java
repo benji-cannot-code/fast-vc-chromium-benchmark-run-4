ecb5cecee40775d1ca42301d9ffdd75ceefbc6e3
Fiaz Muhammad <mfiaz@google.com>
2026-06-10 14:24:29 -0700

    Prevent TabGroupListBottomSheetMediator from prematurely clearing
    Fixes a bug where the model list was being cleared when a previous
    sheet was present, resulting in the TabGroupListBottomSheet being
    displayed empty.
    
    Bug: 522221723
    Change-Id: I93c28d1273d67a396c9f7eaa0e35bc1970e2404a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914513
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644887}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinator.java
@@ -184,6 +184,7 @@ public class TabGroupListBottomSheetCoordinator {
 
     /** Permanently cleans up this component. */
     public void destroy() {
+        mMediator.destroy();
         mSimpleRecyclerViewAdapter.destroy();
         mTabListFaviconProvider.destroy();
     }
