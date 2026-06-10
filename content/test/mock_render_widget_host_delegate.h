f90ffe50a259c0e55157190efc5bb7582390ce1b
Aldo Culquicondor <acondor@chromium.org>
2026-06-10 06:20:04 -0700

    Remove histograms Navigation.GestureTransition.IgnoredInputCount
    The histograms are no longer needed. This eliminates the need for
    RenderWidgetHostDelegate::OnInputIgnored.
    
    OBSOLETE_HISTOGRAMS=Patterned histogram Navigation.GestureTransition.IgnoredInputCount.{Reason}.{Position} is no longer needed.
    
    Fixed: 512101071
    Change-Id: Ia133092bfa13ddc27dada4dc8b7c81f1a3a2ee8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910833
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644595}

--- content/test/mock_render_widget_host_delegate.h
+++ content/test/mock_render_widget_host_delegate.h
@@ -75,7 +75,6 @@ class MockRenderWidgetHostDelegate
   VisibleTimeRequestTrigger& GetVisibleTimeRequestTrigger() override;
   gfx::mojom::DelegatedInkPointRenderer* GetDelegatedInkRenderer(
       ui::Compositor* compositor) override;
-  void OnInputIgnored(const blink::WebInputEvent& event) override;
 
   //  RenderWidgetHostInputEventRouter::Delegate
   input::TouchEmulator* GetTouchEmulator(bool create_if_necessary) override;
