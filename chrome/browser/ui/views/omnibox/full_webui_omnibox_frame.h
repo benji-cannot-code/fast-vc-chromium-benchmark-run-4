91e72b4d2e76ff664d2918818f6db708dfa214ff
Paul Adedeji <pauladedeji@google.com>
2026-06-10 14:33:41 -0700

    [omnibox] Refactor `RoundedOmniboxResultsFrame` for full webui popup
    Introduces `FullWebUIOmniboxFrame` as a specialized frame. To support
    this, `CreateResultsFrame` is added as a hook in
    `OmniboxPopupPresenterBase`, allowing subclasses to instantiate custom
    frame types.
    
    Decouples frame-specific logic to support distinct behavior between the
    full WebUI popup and traditional users of the rounded omnibox frame,
    such as handling click events for the full popup [1].
    
    Accordingly, the specific layout logic previously embedded in
    `OmniboxPopupPresenterBase` has been moved to
    `OmniboxPopupFullPresenter`. This cleaner separation removes
    special-case handling from the base class and consolidates the
    frame-specific behavior within the appropriate presenter implementation.
    
    Manually verified and automated verification provided by
    `OmniboxPopupDimensionsTest` suite [2].
    
    [1]https://chromium-review.git.corp.google.com/c/chromium/src/+/7894247/7/chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.cc
    [2]https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ui/views/omnibox/omnibox_popup_view_webui_interactive_uitest.cc
    
    Change-Id: I9dc9020d9bb35f43ebb2b4e0529f8d61cbdeb748
    Bug: b:511199705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7920707
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644891}

---
+++ chrome/browser/ui/views/omnibox/full_webui_omnibox_frame.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_OMNIBOX_FULL_WEBUI_OMNIBOX_FRAME_H_
+#define CHROME_BROWSER_UI_VIEWS_OMNIBOX_FULL_WEBUI_OMNIBOX_FRAME_H_
+
+#include "chrome/browser/ui/views/omnibox/rounded_omnibox_results_frame.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+
+class LocationBar;
+namespace views {
+class View;
+}
+
+class FullWebUIOmniboxFrame : public RoundedOmniboxResultsFrame {
+  METADATA_HEADER(FullWebUIOmniboxFrame, RoundedOmniboxResultsFrame)
+
+ public:
+  FullWebUIOmniboxFrame(views::View* contents,
+                        LocationBar* location_bar,
+                        bool forward_mouse_events);
+  FullWebUIOmniboxFrame(const FullWebUIOmniboxFrame&) = delete;
+  FullWebUIOmniboxFrame& operator=(const FullWebUIOmniboxFrame&) = delete;
+  ~FullWebUIOmniboxFrame() override;
+
+  void SetElevation(int elevation);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_FULL_WEBUI_OMNIBOX_FRAME_H_
--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
@@ -69,7 +69,7 @@ class OmniboxPopupPresenterBase : public content::WebContentsObserver,
   void OnContentHeightChanged(int content_height);
 
   // Synchronize the popup widget's bounds to its anchor (location bar view).
-  void SynchronizePopupBounds();
+  virtual void SynchronizePopupBounds();
 
   // Returns the currently "active" Popup content, whichever one is visible or
   // going to be visible within the popup.
@@ -127,11 +127,18 @@ class OmniboxPopupPresenterBase : public content::WebContentsObserver,
   // Called when the widget has just been destroyed.
   virtual void WidgetDestroyed() {}
 
+  // Hook to create the results frame view.
+  virtual std::unique_ptr<RoundedOmniboxResultsFrame> CreateResultsFrame(
+      std::unique_ptr<views::View> contents,
+      LocationBar* location_bar,
+      bool forward_mouse_events);
+
+  // Returns the frame view of the widget if it exists. CHECKs if no widget
+  // created.
+  RoundedOmniboxResultsFrame* GetResultsFrame() const;
+
   // Returns whether or not the popup should include the location bar cutout.
   virtual bool ShouldShowLocationBarCutout() const;
-
-  // Returns whether or not the popup should trust the content height directly.
-  virtual bool ShouldUseWebContentHeight() const;
 
   // Returns whether the WebUI content view receive focus.
   virtual bool ShouldReceiveFocus() const;
@@ -174,10 +181,6 @@ class OmniboxPopupPresenterBase : public content::WebContentsObserver,
 
   // Remove observation and reset widget, optionally requesting it to close.
   void ReleaseWidget();
-
-  // Returns the frame view of the widget if it exists. CHECKs if no widget
-  // created
-  RoundedOmniboxResultsFrame* GetResultsFrame() const;
 
   // The location bar that owns `this`.
   const raw_ptr<LocationBar> location_bar_;
