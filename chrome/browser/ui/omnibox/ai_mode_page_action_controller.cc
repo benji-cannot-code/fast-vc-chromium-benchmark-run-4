526c0540e0ea2f266a9e55979ac51cfedccf91b4
manukh <manukh@chromium.org>
2026-06-10 12:23:44 -0700

    [omnibox][3p-aim] Hide 3p config if invalid
    Previous CLs already handled hiding the button if the favicon fails to
    fetch. This CL additionally checks its text and navigation URLs are
    formed correctly and hides the button otherwise.
    
    Bug: b:517977413
    Change-Id: I0a079d0a0e7f1ffee02c25f6a44d26fd8e90c808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7897758
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644809}

--- chrome/browser/ui/omnibox/ai_mode_page_action_controller.cc
+++ chrome/browser/ui/omnibox/ai_mode_page_action_controller.cc
@@ -157,6 +157,11 @@ void AiModePageActionController::NotifyOmniboxTriggeredFeatureService(
 bool AiModePageActionController::ShouldShowPageAction(
     Profile* profile,
     LocationBarView& location_bar_view) {
+  const auto& config = ai_mode_button_config::GetCurrentAiModeButtonConfig();
+  if (!config.IsValid()) {
+    return false;
+  }
+
   if (!profile->GetPrefs()->GetBoolean(omnibox::kShowAiModeOmniboxButton)) {
     return false;
   }
