8c74c245e1de3d1cadbabc2dec98c5c79ad35d92
Avi Drissman <avi@chromium.org>
2026-06-10 14:22:57 -0700

    Remove code supporting macOS 12 from base/
    Chromium no longer supports macOS 12.
    
    Bug: 473856754
    Change-Id: I0eab855de50b1d59787cca68035b0bb66a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7920488
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644885}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/mac/mac_util.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/mac/mac_util.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace partition_alloc::internal::base::mac {
 
-// MacOSMajorVersion() returns the major version number (e.g. macOS 12.6.5
-// returns 12) of the macOS currently running. Use for runtime OS version
+// MacOSMajorVersion() returns the major version number (e.g. macOS 14.8.7
+// returns 14) of the macOS currently running. Use for runtime OS version
 // checking. Prefer to use @available in Objective-C files. Note that this does
 // not include any Rapid Security Response (RSR) suffixes (the "(a)" at the end
 // of version numbers.)
