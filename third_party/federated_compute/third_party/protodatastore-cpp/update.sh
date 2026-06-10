168d5a93123684ea313ebb40d799d77a8bb22e29
Kamil Jarosz <kjarosz@google.com>
2026-06-10 04:30:32 -0700

    fcp: Add protodatastore-cpp as a transitive dependency
    This adds protodatastore-cpp as FCP's transitive dependency.
    
    It includes the whole repo as it's a small dependency and FCP uses it
    in its entirety.
    
    BYPASS_LARGE_CHANGE_WARNING: Adding a third party dependency.
    
    Fixed: b:521420094
    Change-Id: I6a9653775e01e166eaba2faecba18e51936cd657
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7909465
    Reviewed-by: Deepak Ravichandran <deepakr@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Rachael Newitt <renewitt@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644561}

---
+++ third_party/federated_compute/third_party/protodatastore-cpp/update.sh
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+shopt -s extglob dotglob
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Get the repo URL and hash from  README.chromium
+PDS_REPO=$(sed -n 's/^URL: \(.*\)/\1/p' "${SCRIPT_DIR}/README.chromium")
+PDS_REV=$(sed -n 's/^Revision: \(.*\)/\1/p' "${SCRIPT_DIR}/README.chromium")
+
+if [[ -z "${PDS_REPO}" || -z "${PDS_REV}" ]]; then
+  echo "Error: Could not extract URL or Revision from README.chromium"
+  exit 1
+fi
+
+echo "Using repo ${PDS_REPO}"
+echo "Using revision ${PDS_REV}"
+
+function check_out() {
+  git init
+  git remote add upstream ${PDS_REPO}
+  git fetch --depth 1 upstream "${PDS_REV}"
+  git checkout FETCH_HEAD 2>/dev/null
+}
+
+pushd "${SCRIPT_DIR}/"
+
+rm -rf .tmp-checkout
+mkdir .tmp-checkout
+pushd .tmp-checkout
+
+check_out
+
+popd # .tmp-checkout
+
+rm -rf src/
+mkdir src/
+cp -r .tmp-checkout/!(.git) src/
+rm -rf .tmp-checkout/
+
+popd # ${SCRIPT_DIR}/
