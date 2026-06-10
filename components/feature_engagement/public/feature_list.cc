d88bd7075e257acb393003b14184ee40f0bf0f02
Bofeng Chen <bofengchen@google.com>
2026-06-10 08:46:07 -0700

    [Bookmarks] Add IPH feature for Bookmark Bar auto-hide toast.
    Introduces a new Feature Engagement IPH,
    `kIPHBookmarkBarSimplifiedFeature`, which is triggered when the bookmark
    bar is auto-hidden due to inactivity. The IPH is a custom action toast
    that allows the user to revert the bookmark bar visibility state to
    "Only Show on NTP".
    
    The IPH shape and arrow is centered at top, and clicking "Undo" button
    updates the visibility state to `kOnlyShowOnNtp`:
    https://screencast.googleplex.com/cast/NTg2NDY1ODM3OTQ3Mjg5NnxmOTA3NWI4Yy04Mw
    
    Fixed: b:520023682
    Change-Id: I098d9da9e15339cef737e55081b9ebb977005701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901349
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Bofeng Chen <bofengchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644675}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -256,6 +256,7 @@ const base::Feature* const kAllFeatures[] = {
     // keep-sorted start case=no
     &kIPHBackNavigationMenuFeature,
     &kIPHBatterySaverModeFeature,
+    &kIPHBookmarkBarSimplifiedFeature,
     &kIPHCompanionSidePanelFeature,
     &kIPHCompanionSidePanelRegionSearchFeature,
     &kIPHComposeMSBBSettingsFeature,
