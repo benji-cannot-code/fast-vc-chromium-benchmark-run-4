00dfe2ead5c306a6ebe2df3ffea1cc9ca02c514c
Etienne Pierre-doray <etiennep@chromium.org>
2026-06-10 13:38:43 -0700

    [tracing] Avoid perfetto track aliasing in third_party/blink/renderer/core/timing
    perfetto::Track::FromPointer is problematic because pointer reuse can
    cause track aliasing in perfetto UI, leading to confusing traces (this
    was true of legacy TRACE_EVENT_ASYNC as well).
    
    To avoid these, we have the following alternatives:
    1- Using GetNextGlobalTraceId() instead of a pointer id to guarantee
      uniqueness. The track will get its name from the first event emitted.
    2- Using NamedTrack to scope the pointer to a specific class
      (an aliased track would always correctly have the same name). This
      is more appropriate when there are several events emitted to the same
      track without a clear name. This has the benefit of giving the track
      a consistent, explicit name.
    3- Replace custom track with "sync" instant events with flows.
      This reduces visual clutter and retain information about where
      events originated from.
    
    This CL applies a migration to of one #1 or #2.
    
    Questions for the reviewer:
    - Design Fit: Is the choice of a dedicated async track (#1/#2)
      appropriate, or would is be suitable to emit "sync" events with
      flows (#3). As a rule of thumb, async tracks are appropriate to
      display a clear async operation (that could be recorded as UMA),
      or high level / user visible state.
    - Naming: (when applicable) Is the track name I chose appropriate
    
    This CL was uploaded by git cl split.
    
    Bug: 432427382
    Change-Id: I406e5949ad17ae608864d10bb8f6a32279c1984d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7873628
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644853}

--- third_party/blink/renderer/core/timing/soft_navigation_context.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_context.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/timing/largest_contentful_paint.h"
 #include "third_party/blink/renderer/core/timing/soft_navigation_heuristics.h"
 #include "third_party/blink/renderer/core/timing/window_performance.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace blink {
 
@@ -31,15 +32,15 @@ SoftNavigationContext::SoftNavigationContext(
       lcp_calculator_(MakeGarbageCollected<LargestContentfulPaintCalculator>(
           DOMWindowPerformance::performance(window),
           this)),
-      initial_event_timing_(initial_event_timing) {
+      initial_event_timing_(initial_event_timing),
+      track_(perfetto::NamedTrack::FromPointer("blink::SoftNavigation", this)) {
   CHECK(initial_event_timing_);
   CHECK(initial_event_timing_->IsInteraction());
 
-  TRACE_EVENT_BEGIN("loading", "SoftNavigation",
-                    perfetto::Track::FromPointer(this), TimeOrigin());
+  TRACE_EVENT_BEGIN("loading", "SoftNavigation", track_, TimeOrigin());
 
-  TRACE_EVENT_INSTANT("loading", "SoftNavigationContextCreated",
-                      perfetto::Track::FromPointer(this), "context", *this);
+  TRACE_EVENT_INSTANT("loading", "SoftNavigationContextCreated", track_,
+                      "context", *this);
 
   GetSoftNavigationHeuristics()->ForEachInteractionEffectsMonitor(
       [&](InteractionEffectsMonitor& monitor) {
@@ -88,9 +89,8 @@ void SoftNavigationContext::AddModifiedNode(Node* node) {
   ++num_modified_dom_nodes_;
   TRACE_EVENT_INSTANT(
       "loading", "SoftNavigationContext::AddedModifiedNodeInAnimationFrame",
-      perfetto::Track::FromPointer(this), "context", this, "nodeId",
-      node->GetDomNodeId(), "nodeDebugName", node->DebugName(),
-      "domModificationsThisAnimationFrame",
+      track_, "context", this, "nodeId", node->GetDomNodeId(), "nodeDebugName",
+      node->DebugName(), "domModificationsThisAnimationFrame",
       num_modified_dom_nodes_ - num_modified_dom_nodes_last_animation_frame_);
 }
 
@@ -113,10 +113,10 @@ bool SoftNavigationContext::AddPaintedArea(PaintTimingRecord* record) {
   painted_area_ += painted_area;
   TRACE_EVENT_INSTANT(
       "loading", "SoftNavigationContext::AttributablePaintInAnimationFrame",
-      perfetto::Track::FromPointer(this), "context", this, "nodeId",
-      node->GetDomNodeId(), "nodeDebugName", node->DebugName(), "rect_x",
-      rect.x(), "rect_y", rect.y(), "rect_width", rect.width(), "rect_height",
-      rect.height(), "paintedAreaThisAnimationFrame",
+      track_, "context", this, "nodeId", node->GetDomNodeId(), "nodeDebugName",
+      node->DebugName(), "rect_x", rect.x(), "rect_y", rect.y(), "rect_width",
+      rect.width(), "rect_height", rect.height(),
+      "paintedAreaThisAnimationFrame",
       painted_area_ - painted_area_last_animation_frame_);
 
   // TODO(crbug.com/434159332): This doesn't currently match hard-FCP semantics
@@ -158,9 +158,9 @@ bool SoftNavigationContext::OnPaintFinished() {
   // if we have an extra loud tracing debug mode.
   if (num_modded_new_nodes || new_painted_area) {
     TRACE_EVENT_INSTANT("loading", "SoftNavigationContext::OnPaintFinished",
-                        perfetto::Track::FromPointer(this), "context", this,
-                        "numModdedNewNodes", num_modded_new_nodes,
-                        "newPaintedArea", new_painted_area);
+                        track_, "context", this, "numModdedNewNodes",
+                        num_modded_new_nodes, "newPaintedArea",
+                        new_painted_area);
   }
 
   if (new_painted_area > 0) {
@@ -236,7 +236,7 @@ void SoftNavigationContext::Trace(Visitor* visitor) const {
 }
 
 void SoftNavigationContext::Shutdown() {
-  TRACE_EVENT_END("loading", perfetto::Track::FromPointer(this));
+  TRACE_EVENT_END("loading", track_);
 
   lcp_calculator_ = nullptr;
   first_image_or_text_ = nullptr;
@@ -259,17 +259,16 @@ void SoftNavigationContext::EmitSoftNavigation() {
     // However, that other event reports all new *potential* soft navs, while
     // this event only reports actually *emitted* soft navs.
     // This is used by DevTools performance profiler to mark the perf timeline.
-    TRACE_EVENT_INSTANT(
-        "scheduler,devtools.timeline,loading", "SoftNavigationStart",
-        perfetto::Track::FromPointer(this), TimeOrigin(), "context", *this,
-        "frame", GetFrameIdForTracing(window_->GetFrame()));
+    TRACE_EVENT_INSTANT("scheduler,devtools.timeline,loading",
+                        "SoftNavigationStart", track_, TimeOrigin(), "context",
+                        *this, "frame",
+                        GetFrameIdForTracing(window_->GetFrame()));
 
     // This trace event reports the when the soft nav heuristics were observerd,
     // and thus when the new navigationId was created, and when the performance
     // timeline is logically "sliced" into soft-nav sub-timelines.
     TRACE_EVENT_INSTANT("scheduler,devtools.timeline,loading",
-                        "SoftNavigationEmitted",
-                        perfetto::Track::FromPointer(this),
+                        "SoftNavigationEmitted", track_,
                         soft_navigation_slicing_time_, "context", *this);
   }
 
--- third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
+++ third_party/blink/renderer/core/timing/soft_navigation_heuristics.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/scheduler/public/task_attribution_info.h"
 #include "third_party/blink/renderer/platform/scheduler/public/task_attribution_tracker.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace blink {
 
@@ -96,7 +97,7 @@ void OnSoftNavigationContextWasExhausted(const SoftNavigationContext& context,
                                          uint64_t required_paint_area) {
   TRACE_EVENT_INSTANT(
       "loading", "SoftNavigationHeuristics::SoftNavigationContextWasExhausted",
-      perfetto::Track::FromPointer(&context), "context", context);
+      context.GetTracingTrack(), "context", context);
 
   // Don't bother to log if the URL was never set.  That means it was just a
   // normal interaction.
@@ -327,8 +328,9 @@ void SoftNavigationHeuristics::SameDocumentNavigationCommitted(
     TRACE_EVENT_INSTANT("loading",
                         "SoftNavigationHeuristics::"
                         "SameDocumentNavigationCommittedWithoutContext",
-                        perfetto::Track::FromPointer(this), "url",
-                        new_url_string);
+                        perfetto::NamedTrack::FromPointer(
+                            "blink::SoftNavigationHeuristics", this),
+                        "url", new_url_string);
     base::UmaHistogramEnumeration(
         kPageLoadInternalSoftNavigationOutcome,
         SoftNavigationOutcome::kNoSoftNavContextDuringUrlChange);
@@ -349,7 +351,7 @@ void SoftNavigationHeuristics::SameDocumentNavigationCommitted(
                         "SoftNavigationHeuristics::"
                         "SameDocumentNavigationCommittedWithoutContextButMerg"
                         "edIntoPreviousContext",
-                        perfetto::Track::FromPointer(context), "context",
+                        context->GetTracingTrack(), "context",
                         *context_for_current_url_, "url", new_url_string);
     base::UmaHistogramEnumeration(
         kPageLoadInternalSoftNavigationOutcome,
@@ -364,7 +366,7 @@ void SoftNavigationHeuristics::SameDocumentNavigationCommitted(
 
   TRACE_EVENT_INSTANT(
       "loading", "SoftNavigationHeuristics::SameDocumentNavigationCommitted",
-      perfetto::Track::FromPointer(context), "context", *context);
+      context->GetTracingTrack(), "context", *context);
 
   MaybeCommitNavigationOrEmitSoftNavigation(context);
 }
