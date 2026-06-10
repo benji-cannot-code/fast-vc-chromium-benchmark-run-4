a569c28838a176078afca7f7bb07409cf3038019
Jean-Philippe Gravel <jpgravel@chromium.org>
2026-06-09 19:26:31 -0700

    Add missing histogram timers in UkmDatabaseBackend
    Most methods in UkmDatabaseBackend have SCOPED_UMA_HISTOGRAM_TIMER
    instrumentation already, but some methods were missing it. Adding it to
    these other methods will give better coverage when we try to evaluate
    the impact of the feature InhibitTransactionFromSegmentationDB which
    disables the global transaction in this class.
    
    Bug: 496096535
    Change-Id: I1307e31fa7a2fb6c6347dea09616873c8963968b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905338
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644416}

--- components/segmentation_platform/internal/database/ukm_database_backend.cc
+++ components/segmentation_platform/internal/database/ukm_database_backend.cc
@@ -248,6 +248,7 @@ void UkmDatabaseBackend::UpdateUrlForUkmSource(ukm::SourceId source_id,
 
 void UkmDatabaseBackend::OnUrlValidated(const GURL& url,
                                         const std::string& profile_id) {
+  SCOPED_UMA_HISTOGRAM_TIMER("SegmentationPlatform.Database.OnUrlValidated");
   if (status_ != Status::INIT_SUCCESS) {
     return;
   }
@@ -361,6 +362,7 @@ UkmDatabaseBackend::RunReadOnlyQueries(UkmDatabase::QueryList queries) {
 
 void UkmDatabaseBackend::CleanupOldEntries(base::Time ukm_time_limit,
                                            base::Time uma_time_limit) {
+  SCOPED_UMA_HISTOGRAM_TIMER("SegmentationPlatform.Database.CleanupOldEntries");
   if (status_ != Status::INIT_SUCCESS) {
     return;
   }
@@ -386,6 +388,7 @@ void UkmDatabaseBackend::CleanupOldEntries(base::Time ukm_time_limit,
 
 void UkmDatabaseBackend::CleanupItems(const std::string& profile_id,
                                       std::vector<CleanupItem> cleanup_items) {
+  SCOPED_UMA_HISTOGRAM_TIMER("SegmentationPlatform.Database.CleanupItems");
   if (status_ != Status::INIT_SUCCESS) {
     return;
   }
@@ -419,6 +422,7 @@ void UkmDatabaseBackend::RollbackTransactionForTesting() {
 }
 
 void UkmDatabaseBackend::DeleteAllUrls() {
+  SCOPED_UMA_HISTOGRAM_TIMER("SegmentationPlatform.Database.DeleteAllUrls");
   CHECK_EQ(status_, Status::INIT_SUCCESS);
 
   std::optional<sql::Transaction> transaction;
