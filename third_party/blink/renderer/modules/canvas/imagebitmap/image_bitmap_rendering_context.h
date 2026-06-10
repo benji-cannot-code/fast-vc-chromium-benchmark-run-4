288b8a3001764d79d7a02059ef3fda1b21b46983
Colin Blundell <blundell@chromium.org>
2026-06-10 00:19:52 -0700

    [Blink] Move ImageLayerBridge::ResourceReleasedGpu() to ImageBitmapRC
    Next step in the elimination of ImageLayerBridge.
    
    Bug: 352263194
    Change-Id: I658f0231faf2d7e6800fe0c925f89e6310f83c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7909755
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644478}

--- third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context.h
+++ third_party/blink/renderer/modules/canvas/imagebitmap/image_bitmap_rendering_context.h
@@ -24,6 +24,10 @@ class Layer;
 class TextureLayer;
 }
 
+namespace gpu {
+struct SyncToken;
+}
+
 namespace blink {
 
 class ExceptionState;
@@ -131,6 +135,9 @@ class MODULES_EXPORT ImageBitmapRenderingContext final
   void ResetInternalBitmapToBlackTransparent(int width, int height);
 
   void SetImageOnImageLayerBridge(scoped_refptr<StaticBitmapImage>);
+  void ResourceReleasedGpu(scoped_refptr<StaticBitmapImage>,
+                           const gpu::SyncToken&,
+                           bool lost_resource);
 
   Member<ImageLayerBridge> image_layer_bridge_;
   scoped_refptr<cc::TextureLayer> layer_;
