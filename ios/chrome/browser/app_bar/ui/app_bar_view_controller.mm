4d4cce5510b0468fa1282912fce1f64cbdfa547d
Chris Lu <thegreenfrog@google.com>
2026-06-10 14:27:35 -0700

    [ios] Cap AppBar button text font
    Fixed: 521340802
    Change-Id: Ibe69c63f822712694a5c9a7c662c1a2ddae6f0a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914222
    Reviewed-by: Eric Ekey <ericekey@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1644889}

--- ios/chrome/browser/app_bar/ui/app_bar_view_controller.mm
+++ ios/chrome/browser/app_bar/ui/app_bar_view_controller.mm
@@ -99,10 +99,18 @@ UIImage* CustomAppBarSymbol(NSString* symbol_name) {
                                        AppBarSymbolConfiguration());
 }
 
-// Returns the font size for the assistant button.
-UIFont* AssistantButtonFontSize(UITraitCollection* traitCollection) {
-  return PreferredFontForTextStyle(UIFontTextStyleCaption2, UIFontWeightMedium,
-                                   std::nullopt);
+// Returns the font size for the buttons.
+UIFont* ButtonFontSize(UITraitCollection* traitCollection) {
+  UIContentSizeCategory category = ContentSizeCategoryWithMaxCategory(
+      traitCollection.preferredContentSizeCategory,
+      UIContentSizeCategoryExtraExtraLarge);
+  UITraitCollection* cappedTraits = [UITraitCollection
+      traitCollectionWithPreferredContentSizeCategory:category];
+  UIFontDescriptor* descriptor = [UIFontDescriptor
+      preferredFontDescriptorWithTextStyle:UIFontTextStyleCaption2
+             compatibleWithTraitCollection:cappedTraits];
+  return [UIFont systemFontOfSize:descriptor.pointSize
+                           weight:UIFontWeightMedium];
 }
 
 // Returns the alpha for the button based on its enabled and highlighted state.
@@ -551,7 +559,7 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
     return fullTitle;
   }
   CGSize size = [fullTitle sizeWithAttributes:@{
-    NSFontAttributeName : AssistantButtonFontSize(self.traitCollection)
+    NSFontAttributeName : ButtonFontSize(self.traitCollection)
   }];
 
   CGFloat availableWidthForButton;
@@ -974,7 +982,7 @@ CGFloat ButtonHighlightAlpha(UIButton* button) {
           NSDictionary<NSAttributedStringKey, id>* textAttributes) {
     NSMutableDictionary* mutableAttributes = [textAttributes mutableCopy];
     mutableAttributes[NSFontAttributeName] =
-        AssistantButtonFontSize(self.traitCollection);
+        ButtonFontSize(self.traitCollection);
     mutableAttributes[NSForegroundColorAttributeName] =
         ButtonsForegroundColor();
     return mutableAttributes;
