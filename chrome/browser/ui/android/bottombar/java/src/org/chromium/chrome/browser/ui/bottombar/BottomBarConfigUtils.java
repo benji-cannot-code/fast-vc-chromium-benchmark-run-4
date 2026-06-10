fa9860619cac627aad09bdd46c6a5abe67229033
Dan Polanco <polardz@google.com>
2026-06-10 11:13:41 -0700

    [IA] Disable flag on automotive devices
    Bug: 519148302
    Fixed: 522022796
    Change-Id: I622843508ec5e4feb61a7340a9fc8afc9a61ace7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914911
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644730}

--- chrome/browser/ui/android/bottombar/java/src/org/chromium/chrome/browser/ui/bottombar/BottomBarConfigUtils.java
+++ chrome/browser/ui/android/bottombar/java/src/org/chromium/chrome/browser/ui/bottombar/BottomBarConfigUtils.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.ui.bottombar;
 
 import android.content.Context;
 
+import org.chromium.base.DeviceInfo;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -21,6 +22,7 @@ public class BottomBarConfigUtils {
     /** Whether the bottom bar is enabled. */
     public static boolean isBottomBarEnabled(Context context) {
         return !DeviceFormFactor.isNonMultiDisplayContextOnTablet(context)
+                && !DeviceInfo.isAutomotive()
                 && ChromeFeatureList.sAndroidBottomBar.isEnabled();
     }
 
