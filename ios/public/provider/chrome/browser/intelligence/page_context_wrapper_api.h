ada525d3b3660e6464781b23d777a51e3f79fa0f
Vincent Boisselle <vincb@google.com>
2026-06-11 11:17:37 -0700

    [ios] Add IsProtectedUrl stub to intelligence provider
    We need to land this CL before landing the downstream changes
    to migrate the providers from bwg to intelligence/.
    
    This CL adds the IsProtectedUrl declaration and stubs to the
    intelligence provider interface. This is the first step of the
    migration to decouple PageContextWrapper from the Gemini SDK.
    
    This CL only adds the interface and stubs. The legacy bwg provider
    and all callers remain unmodified. This allows downstream ios_internal
    to implement the new interface before we switch the callers upstream.
    
    Bug: 522322224, 460380319
    Change-Id: I348cbadfb1fdf73f5a17ead46d354813f547ba26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7919406
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645490}

--- ios/public/provider/chrome/browser/intelligence/page_context_wrapper_api.h
+++ ios/public/provider/chrome/browser/intelligence/page_context_wrapper_api.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios::provider {
 
+// Returns true if a URL is protected.
+bool IsProtectedUrlV2(std::string url);
+
 // TODO(crbug.com/460380319): Remove V2 once the migration is completed.
 // Gets the portion of the PageContext script that checks whether PageContext
 // should be detached from the request.
