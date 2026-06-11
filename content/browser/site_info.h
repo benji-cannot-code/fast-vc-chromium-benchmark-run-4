0038c24975ad6c143b3d8e8f458ad40c3871b435
Jiacheng Guo <gjc@google.com>
2026-06-10 21:22:21 -0700

    Remove redundant OAC check in GetAgentClusterKeyForNonOpaqueOrigin
    The change splits the legacy isolated origin list check in
    GetMatchingProcessIsolatedOrigin to a separate method. In
    GetAgentClusterKeyForNonOpaqueOrigin, checking the OAC list will be
    redundant if we already know the OAC status.
    The CL also adds a holdback experiment flag to measure the
    performance impact.
    
    For more detailed investigation report the correctness of the change
    see: https://docs.google.com/document/d/1HRyBkvwq7CFnowcs_kt6xjPF9jupHgKRGbZIuDrHiuE/edit?usp=sharing&resourcekey=0-5WWj677DqIU_1f9ujgNP6Q
    
    Bug: 519864754
    Change-Id: I7190d795c48016638550d45fa06d1f3b5e03c799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902054
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645093}

--- content/browser/site_info.h
+++ content/browser/site_info.h
@@ -22,6 +22,7 @@ namespace content {
 
 class BrowserContext;
 class IsolationContext;
+class OriginAgentClusterIsolationState;
 class StoragePartitionConfig;
 struct UrlInfo;
 
@@ -439,9 +440,8 @@ class CONTENT_EXPORT SiteInfo : public SecurityPrincipal {
   static AgentClusterKey GetAgentClusterKeyForNonOpaqueOrigin(
       const IsolationContext& isolation_context,
       const UrlInfo& url_info,
-      const url::Origin origin,
-      AgentClusterKey::OACStatus oac_status,
-      bool requires_origin_keyed_process,
+      const url::Origin& origin,
+      const OriginAgentClusterIsolationState& oac_isolation_state,
       bool is_origin_isolated_sandboxed_data_iframe);
   static AgentClusterKey GetAgentClusterKeyForSchemeOnlyOrigin(
       const UrlInfo& url_info,
