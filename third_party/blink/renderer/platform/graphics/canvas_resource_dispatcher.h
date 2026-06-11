5f2df9b0692de46d7ef0eeba3175d7711c904690
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-06-11 08:55:47 -0700

    Clarify task runners names in PlaceholderClient
    The task runners are essentially "PostTask here if you want to talk to
    placeholder" and "PostTaskHere if you want to talk back to canvas".
    
    Name them accordingly, as this is more important detail in this code
    rather than factual correct "agent compositor task runner".
    
    Change-Id: Ife0566a4df138e2383fe911d8dde7d196b07e12e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7913465
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645405}

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
@@ -109,11 +109,11 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
 
   class PlaceholderClient : public OffscreenCanvasPlaceholder::Client {
    public:
-    PlaceholderClient(DOMNodeId placeholder_canvas_id,
-                      scoped_refptr<base::SingleThreadTaskRunner>
-                          agent_group_scheduler_compositor_task_runner,
-                      scoped_refptr<base::SingleThreadTaskRunner> task_runner,
-                      base::RepeatingClosure animation_state_callback);
+    PlaceholderClient(
+        DOMNodeId placeholder_canvas_id,
+        scoped_refptr<base::SingleThreadTaskRunner> placeholder_task_runner,
+        scoped_refptr<base::SingleThreadTaskRunner> canvas_task_runner,
+        base::RepeatingClosure animation_state_callback);
     ~PlaceholderClient() override;
 
     base::WeakPtr<PlaceholderClient> GetWeakPtr() {
@@ -134,9 +134,11 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
 
     const DOMNodeId placeholder_canvas_id_;
 
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-    scoped_refptr<base::SingleThreadTaskRunner>
-        agent_group_scheduler_compositor_task_runner_;
+    // Task runner for the thread where OffscreenCanvas lives.
+    scoped_refptr<base::SingleThreadTaskRunner> canvas_task_runner_;
+    // Task runner for the thread where OffscreenCanvasPlaceholder lives, i.e
+    // main thread.
+    scoped_refptr<base::SingleThreadTaskRunner> placeholder_task_runner_;
 
     OffscreenCanvasPlaceholder::AnimationState animation_state_ =
         OffscreenCanvasPlaceholder::AnimationState::kActive;
