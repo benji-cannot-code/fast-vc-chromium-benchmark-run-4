c8e9e2734f6906e607a02c3bcc5fb161e0b3446c
Norge Vizcay <vizcay@google.com>
2026-06-11 01:21:08 -0700

    Enable kPersonalContext by default
    Enable the main feature flag for the Personal Context service by
    default. The flag is kept around as a kill-switch.
    
    Bug: 518740027
    Change-Id: Iafb064c22dc2fd445e348bd2b8d00582909fc72e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7919328
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645174}

--- components/personal_context/core/personal_context_features.cc
+++ components/personal_context/core/personal_context_features.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace personal_context::features {
 
-BASE_FEATURE(kPersonalContext, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPersonalContext, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPersonalContextFirstRun, base::FEATURE_DISABLED_BY_DEFAULT);
 
--- components/personal_context/core/personal_context_features.h
+++ components/personal_context/core/personal_context_features.h
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace personal_context::features {
 
 // The main feature flag for the Personal Context service. When disabled,
-// all Personal Context features and services are turned off.
+// all Personal Context features and services are turned off. Enabled by default
+// and kept around as a kill-switch.
 BASE_DECLARE_FEATURE(kPersonalContext);
 
 // Controls whether the First Run and onboarding experience for Personal Context
