d5e82a7ffed7b7f92392021975c2192986ea15e6
Joshua Thomas <masnoble@chromium.org>
2026-06-11 07:44:38 -0700

    Add basic navigation gating to CDP-initiated navigations
    There are several browser automation agents that work through CDP to
    actuate chrome on behalf of its user. There is currently no gating
    logic within chrome to reduce the blast radius if such an agent fell
    victim to a prompt injection. This CL adds a basic allow/blocklist
    provided via chrome commandline flags to restrict an agent to only
    work on some predefined list of sites/stay away from certain sites.
    
    This throttle is applied when:
    
    1. Either the allow or blocklist is defined
    2. If there is a devtools session attached to the tab indicating
    the page is under some influence of CDP (this is admittedly a poor
    signal to rely on to detect an agent's presence, but there is
    currently no way to discern between normal CDP sessions and agents)
    
    Bug: b:481769362
    Change-Id: I126ddf4bd01bf671bb61ae08a0cadc629e6ad5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7675311
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Joshua Thomas <masnoble@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645358}

---
+++ chrome/browser/devtools/devtools_navigation_throttle.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DEVTOOLS_DEVTOOLS_NAVIGATION_THROTTLE_H_
+#define CHROME_BROWSER_DEVTOOLS_DEVTOOLS_NAVIGATION_THROTTLE_H_
+
+#include "content/public/browser/navigation_throttle.h"
+
+namespace content {
+class NavigationThrottleRegistry;
+}
+
+// Throttles navigations in tabs under the control of a DevTools Protocol (CDP)
+// session
+class DevToolsNavigationThrottle : public content::NavigationThrottle {
+ public:
+  static void MaybeCreateAndAdd(content::NavigationThrottleRegistry& registry);
+
+  explicit DevToolsNavigationThrottle(
+      content::NavigationThrottleRegistry& registry);
+
+  DevToolsNavigationThrottle(const DevToolsNavigationThrottle&) = delete;
+  DevToolsNavigationThrottle& operator=(const DevToolsNavigationThrottle&) =
+      delete;
+  DevToolsNavigationThrottle(DevToolsNavigationThrottle&&) = delete;
+  DevToolsNavigationThrottle& operator=(DevToolsNavigationThrottle&&) = delete;
+
+  ~DevToolsNavigationThrottle() override;
+
+  // content::NavigationThrottle implementation:
+  content::NavigationThrottle::ThrottleCheckResult WillStartRequest() override;
+  content::NavigationThrottle::ThrottleCheckResult WillRedirectRequest()
+      override;
+  const char* GetNameForLogging() override;
+
+ private:
+  content::NavigationThrottle::ThrottleCheckResult WillStartOrRedirectRequest();
+};
+
+#endif  // CHROME_BROWSER_DEVTOOLS_DEVTOOLS_NAVIGATION_THROTTLE_H_
