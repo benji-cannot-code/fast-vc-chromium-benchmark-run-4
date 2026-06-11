8298e18a8aaa6ea97d421a0e0d4abffd41e0f7d5
Colin Blundell <blundell@chromium.org>
2026-06-10 23:38:54 -0700

    [Blink] Move CRP snapshot-related state into subclasses
    Next step on making CanvasResourceProvider into pure interface.
    
    Bug: 352263194
    Change-Id: I7d86375c01384096836c89431b99b7c382bace51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7918726
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645138}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -250,11 +250,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   std::unique_ptr<CanvasImageProvider> canvas_image_provider_;
 
-  const cc::PaintImage::Id snapshot_paint_image_id_;
-  cc::PaintImage::ContentId snapshot_paint_image_content_id_ =
-      cc::PaintImage::kInvalidContentId;
-  uint32_t snapshot_sk_image_id_ = 0u;
-
   bool always_enable_raster_timers_for_testing_ = false;
 
   bool clear_frame_ = true;
@@ -361,6 +356,10 @@ class PLATFORM_EXPORT Canvas2DResourceProviderBitmap
   raw_ptr<Delegate> delegate_ = nullptr;
   mutable sk_sp<SkSurface> surface_;
   std::unique_ptr<cc::SkiaPaintCanvas> skia_canvas_;
+  const cc::PaintImage::Id snapshot_paint_image_id_;
+  cc::PaintImage::ContentId snapshot_paint_image_content_id_ =
+      cc::PaintImage::kInvalidContentId;
+  uint32_t snapshot_sk_image_id_ = 0u;
 };
 
 // * Subclass of CanvasResourceProvider that is specialized for usage
@@ -617,6 +616,10 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
   raw_ptr<Delegate> delegate_ = nullptr;
   mutable sk_sp<SkSurface> surface_;
   std::unique_ptr<cc::SkiaPaintCanvas> skia_canvas_;
+  const cc::PaintImage::Id snapshot_paint_image_id_;
+  cc::PaintImage::ContentId snapshot_paint_image_content_id_ =
+      cc::PaintImage::kInvalidContentId;
+  uint32_t snapshot_sk_image_id_ = 0u;
 
   base::WeakPtrFactory<Canvas2DResourceProviderSharedImage> weak_ptr_factory_{
       this};
