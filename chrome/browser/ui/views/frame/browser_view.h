a28cb07121c954393bc954322c65237ad1ca27fb
Thomas Lukaszewicz <tluk@chromium.org>
2026-06-11 14:04:06 -0700

    [bedrock] Null-check TabSearchToolbarButtonController
    Currently this is instantiated in InitPostBrowserViewConstruction()
    which happens before BrowserView is added to its widget. However this
    lifecycle hook is being consolidated into InitPostWindowConstruction()
    which happens after window init (which includes setup in
    AddedToWidget()).
    
    This CL null-checks the controller, and updates the window's
    TabSearchBubbleHost when the controller is initialized.
    
    There are no intended behavioral changes.
    
    Bug: 496674143
    Change-Id: Ia177f8c851942310087a1f15eb63f48844c57c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7923127
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645589}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -327,6 +327,9 @@ class BrowserView : public BrowserWindow,
   // Accessor for the BrowserView's TabSearchBubbleHost instance.
   TabSearchBubbleHost* GetTabSearchBubbleHost();
 
+  // Setter for the BrowserView's TabSearchBubbleHost instance.
+  void UpdateTabSearchBubbleHost();
+
   // Accessor for the ExclusiveAccessBubble.
   ExclusiveAccessBubbleViews* GetExclusiveAccessBubble();
 
@@ -853,9 +856,6 @@ class BrowserView : public BrowserWindow,
 
   // Returns the state of the bookmark bar.
   BookmarkBar::State bookmark_bar_state() const;
-
-  // Setter for the BrowserView's TabSearchBubbleHost instance.
-  void UpdateTabSearchBubbleHost();
 
   // Display the current active split view as a series of multiple side-by-side
   // web contents.
