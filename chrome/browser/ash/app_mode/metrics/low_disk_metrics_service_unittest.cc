a34f82eb76bb08deb9b40dacdc70f1e8a9908fdb
Di Wu <diwux@google.com>
2026-06-10 21:09:10 -0700

    Ash: Migrate LowDiskMetricsServiceTest off RunUntilIdle
    This changelist is part of an ongoing series to modernize Ash tests by
    migrating them away from RunUntilIdle() and toward targeted
    synchronization with APIs such as RunUntil(), TestFuture, RunLoop
    callbacks, observer waiters, or exact mock-time advancement.
    
    LowDiskMetricsServiceTest no longer drains the task queue after fake
    low-disk notifications. FakeUserDataAuthClient::NotifyLowDiskSpace()
    synchronously notifies observers, so histogram and local-state checks
    can run immediately after SendLowDiskSpaceEvent().
    
    Bug: b:506794349
    Test: CQ
    Change-Id: I9c894b42a1ba56076c4b338ff5d6760e569aa57c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902626
    Reviewed-by: Ashutosh Singhal <macinashutosh@google.com>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645086}

--- chrome/browser/ash/app_mode/metrics/low_disk_metrics_service_unittest.cc
+++ chrome/browser/ash/app_mode/metrics/low_disk_metrics_service_unittest.cc
@@ -55,7 +55,6 @@ class LowDiskMetricsServiceTest
 
   void SendLowDiskSpaceEvent(uint64_t disk_free_bytes) {
     FakeUserDataAuthClient::Get()->NotifyLowDiskSpace(disk_free_bytes);
-    task_environment_.RunUntilIdle();
   }
 
  private:
