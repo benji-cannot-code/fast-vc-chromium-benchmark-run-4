7f374d0c480a1649b43e055edd578870632fdeb4
Hamza Mohammed <hamzamoh@google.com>
2026-06-11 07:22:09 -0700

    [iOS] Feature cleanup FloatyAllFlags
    This CL should not be connected to any other cl in ios_internal or google3 as it is only used in chromium. Please confirm this during the review.
    
    
    Fixed: 515185189
    Change-Id: Iab7bddd43b150b3c5ac83b89bbeb30a2aa10856e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7916940
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Joe Peplowski <joepeplowski@google.com>
    Commit-Queue: Hamza Mohammed <hamzamoh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645347}

--- ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_mediator_unittest.mm
+++ ios/chrome/browser/intelligence/explain_with_gemini/coordinator/explain_with_gemini_mediator_unittest.mm
@@ -230,7 +230,6 @@ TEST_F(ExplainWithGeminiMediatorTest, TriggerAction_StartsFlow_NoMock) {
     base::test::ScopedFeatureList scoped_feature_list;
     scoped_feature_list.InitWithFeaturesAndParameters(
         {{kPageActionMenu, {}},
-         {kGeminiFloatyAllPages, {}},
          {kExplainGeminiEditMenu, {{"PositionForExplainGeminiEditMenu", "1"}}}},
         {});
 
