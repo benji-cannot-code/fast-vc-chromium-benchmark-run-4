b9a175f38cd103a3f07d1cd0eb6f7fd529b3761b
Peter Kvitek <kvitekp@chromium.org>
2026-06-11 12:02:35 -0700

    [headless] Relocate default headless mode user data directory
    This CL moves the default headless mode temporary user data directory
    created when Chrome in headless mode is ran without explicit
    --user-data-dir specification from shared system temporary directory to
    the location where default normal mode user data directory is located.
    
    Bug: 522307607
    Change-Id: I164d03383821961a0168cab9022ba12fa5dc3f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7920893
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645517}

--- chrome/browser/headless/headless_mode_init.cc
+++ chrome/browser/headless/headless_mode_init.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "chrome/browser/headless/headless_mode_platform.h"
 #include "chrome/browser/headless/headless_mode_switches.h"
+#include "chrome/common/chrome_paths_internal.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/common/content_switches.h"
 
@@ -140,7 +141,18 @@ class HeadlessModeHandleImpl : public HeadlessModeHandle {
       }
     }
 #else   // BUILDFLAG(IS_WIN)
-    if (!user_data_dir_.CreateUniqueTempDir()) {
+    base::FilePath default_user_data_dir;
+    if (!chrome::GetDefaultUserDataDirectory(&default_user_data_dir)) {
+      return base::unexpected("Failed to get default user data directory.");
+    }
+
+    base::FilePath headless_user_data_dir(default_user_data_dir.value() +
+                                          FILE_PATH_LITERAL("-headless"));
+    if (!base::CreateDirectory(headless_user_data_dir)) {
+      return base::unexpected(
+          "Failed to create headless user data directory container.");
+    }
+    if (!user_data_dir_.CreateUniqueTempDirUnderPath(headless_user_data_dir)) {
       return base::unexpected(
           "Failed to create a unique user data directory for headless.");
     }
