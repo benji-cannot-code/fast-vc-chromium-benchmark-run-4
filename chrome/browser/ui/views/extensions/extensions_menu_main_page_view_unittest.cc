2b577c36d793108274a8b9a0d356f8865e97db3d
Eva Su <evasu@chromium.org>
2026-06-11 15:16:08 -0700

    [Extensions] Add input protection to extensions menu "Allow" button
    According to crbug.com/521601450, an extension can call the host access
    request API, triggering a layout reflow that shifts the menu elements.
    This can potentially place the newly appeared 'Allow' button directly
    under the user's cursor during an active click, resulting in an
    unintended permission grant.
    
    This CL adds input protection to the “Allow” button in the menu. This is
    similar to the change we made for the ExtensionsRequestAccessButton in
    crrev.com/c/7876925.
    
    Fixed: 521601450
    Change-Id: Iac4136c5bdf855b5b345dd710f0f8d7498536852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7927809
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645645}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view_unittest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view_unittest.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/vector_icons.h"
 #include "ui/views/view_utils.h"
+#include "ui/views/views_switches.h"
 
 namespace {
 
@@ -194,6 +195,8 @@ ExtensionsMenuMainPageViewUnitTest::menu_entries() {
 
 void ExtensionsMenuMainPageViewUnitTest::SetUp() {
   ExtensionsToolbarUnitTest::SetUp();
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      views::switches::kDisableInputEventActivationProtectionForTesting);
   // Menu needs web contents at construction, so we need to add them to every
   // test.
   web_contents_tester_ = AddWebContentsAndGetTester();
