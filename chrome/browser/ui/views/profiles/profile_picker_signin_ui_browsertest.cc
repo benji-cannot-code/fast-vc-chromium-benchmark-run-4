bf39cc907b90f7bffd9360a2ae18e49d5bbc3eaf
Ernest Nguyen Hung <ernn@google.com>
2026-06-11 04:29:52 -0700

    [Profiles] Add a separator for the toolbar buttons
    It adds a separator between the (right) native toolbar buttons (i.e.
    'Don't sign in' and 'Effects control' buttons). It also updates the
    'Don't sign in' button to not paint the background if the 'Effects
    control' button is visible to meet the latest UX recommendations.
    
    Recordings presenting the change (internal only):
    
    Light mode -
    https://screencast.googleplex.com/cast/NTY3MTE0NTg3NjQ4ODE5Mnw4ZTZlZTdmMy01Ng
    
    Dark mode -
    https://screencast.googleplex.com/cast/NTk0MTEzOTE1NTcxNDA0OHwyMjRiYTI1MC05NA
    
    Bug: 515028732
    Change-Id: Icd1d89cffb9839d96fc68bc167fea8a06d64931d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7909423
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Commit-Queue: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Reviewed-by: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645265}

--- chrome/browser/ui/views/profiles/profile_picker_signin_ui_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_picker_signin_ui_browsertest.cc
@@ -35,7 +35,7 @@ using ::testing::ValuesIn;
 struct ProfilePickerSignInTestParam {
   PixelTestParam pixel_test_param;
   ProfilePicker::EntryPoint entry_point = ProfilePicker::EntryPoint::kOnStartup;
-  std::vector<base::test::FeatureRef> enabled_features;
+  std::vector<base::test::FeatureRefAndParams> enabled_features;
   std::vector<base::test::FeatureRef> disabled_features;
 };
 
@@ -51,44 +51,76 @@ std::vector<ProfilePickerSignInTestParam> GetTestParams() {
       {
           .pixel_test_param = {.test_suffix = "RegularFirstRunRevampEnabled"},
           .entry_point = ProfilePicker::EntryPoint::kFirstRun,
-          .enabled_features = {switches::kFirstRunDesktopRefresh,
-                               switches::kFirstRunDesktopRevamp},
+          .enabled_features = {{switches::kFirstRunDesktopRefresh, {}},
+                               {switches::kFirstRunDesktopRevamp, {}}},
       },
       {
           .pixel_test_param = {.test_suffix = "DarkThemeFirstRunRevampEnabled",
                                .use_dark_theme = true},
           .entry_point = ProfilePicker::EntryPoint::kFirstRun,
-          .enabled_features = {switches::kFirstRunDesktopRefresh,
-                               switches::kFirstRunDesktopRevamp},
+          .enabled_features = {{switches::kFirstRunDesktopRefresh, {}},
+                               {switches::kFirstRunDesktopRevamp, {}}},
       },
       {
           .pixel_test_param = {.test_suffix = "RTLFirstRunRevampEnabled",
                                .use_right_to_left_language = true},
           .entry_point = ProfilePicker::EntryPoint::kFirstRun,
-          .enabled_features = {switches::kFirstRunDesktopRefresh,
-                               switches::kFirstRunDesktopRevamp},
+          .enabled_features = {{switches::kFirstRunDesktopRefresh, {}},
+                               {switches::kFirstRunDesktopRevamp, {}}},
+      },
+      {
+          .pixel_test_param =
+              {.test_suffix = "RegularFirstRunDontSignInOnGaiaPageEnabled"},
+          .entry_point = ProfilePicker::EntryPoint::kFirstRun,
+          .enabled_features =
+              {{switches::kFirstRunDesktopRevamp, {}},
+               {switches::kFirstRunDesktopRefresh,
+                {{switches::kFirstRunDesktopSignInPromoVariation.name,
+                  "dont-sign-in-on-gaia-page"}}}},
+      },
+      {
+          .pixel_test_param =
+              {.test_suffix = "DarkThemeFirstRunDontSignInOnGaiaPageEnabled",
+               .use_dark_theme = true},
+          .entry_point = ProfilePicker::EntryPoint::kFirstRun,
+          .enabled_features =
+              {{switches::kFirstRunDesktopRevamp, {}},
+               {switches::kFirstRunDesktopRefresh,
+                {{switches::kFirstRunDesktopSignInPromoVariation.name,
+                  "dont-sign-in-on-gaia-page"}}}},
+      },
+      {
+          .pixel_test_param = {.test_suffix =
+                                   "RTLFirstRunDontSignInOnGaiaPageEnabled",
+                               .use_right_to_left_language = true},
+          .entry_point = ProfilePicker::EntryPoint::kFirstRun,
+          .enabled_features =
+              {{switches::kFirstRunDesktopRevamp, {}},
+               {switches::kFirstRunDesktopRefresh,
+                {{switches::kFirstRunDesktopSignInPromoVariation.name,
+                  "dont-sign-in-on-gaia-page"}}}},
       },
       {
           .pixel_test_param = {.test_suffix =
                                    "RegularAddNewProfileRevampEnabled"},
           .entry_point = ProfilePicker::EntryPoint::kProfileMenuAddNewProfile,
-          .enabled_features = {switches::kFirstRunDesktopRefresh,
-                               switches::kFirstRunDesktopRevamp},
+          .enabled_features = {{switches::kFirstRunDesktopRefresh, {}},
+                               {switches::kFirstRunDesktopRevamp, {}}},
       },
       {
           .pixel_test_param = {.test_suffix =
                                    "DarkThemeAddNewProfileRevampEnabled",
                                .use_dark_theme = true},
           .entry_point = ProfilePicker::EntryPoint::kProfileMenuAddNewProfile,
-          .enabled_features = {switches::kFirstRunDesktopRefresh,
-                               switches::kFirstRunDesktopRevamp},
+          .enabled_features = {{switches::kFirstRunDesktopRefresh, {}},
+                               {switches::kFirstRunDesktopRevamp, {}}},
       },
       {
           .pixel_test_param = {.test_suffix = "RTLAddNewProfileRevampEnabled",
                                .use_right_to_left_language = true},
           .entry_point = ProfilePicker::EntryPoint::kProfileMenuAddNewProfile,
-          .enabled_features = {switches::kFirstRunDesktopRefresh,
-                               switches::kFirstRunDesktopRevamp},
+          .enabled_features = {{switches::kFirstRunDesktopRefresh, {}},
+                               {switches::kFirstRunDesktopRevamp, {}}},
       }};
 }
 
@@ -156,8 +188,8 @@ class ProfilePickerSigninToolbarUIPixelTest
  public:
   ProfilePickerSigninToolbarUIPixelTest()
       : ProfilesPixelTestBaseT<UiBrowserTest>(GetParam().pixel_test_param) {
-    scoped_feature_list_.InitWithFeatures(GetParam().enabled_features,
-                                          GetParam().disabled_features);
+    scoped_feature_list_.InitWithFeaturesAndParameters(
+        GetParam().enabled_features, GetParam().disabled_features);
   }
 
   void SetUp() override {
