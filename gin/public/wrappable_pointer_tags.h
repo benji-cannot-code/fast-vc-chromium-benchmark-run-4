1492520468e377255f227ef976e8a8eb1a2e1f4f
Andreas Haas <ahaas@chromium.org>
2026-06-11 00:08:41 -0700

    Sort gin::WrappablePointerTag enum values
    The enum values in gin::WrappablePointerTag are now sorted
    alphabetically and enclosed within keep-sorted comments.
    
    Change-Id: I7893bec5ed94733bddd674d1e333ca03257f5a94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915112
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645143}

--- gin/public/wrappable_pointer_tags.h
+++ gin/public/wrappable_pointer_tags.h
@@ -24,6 +24,7 @@ enum WrappablePointerTag : uint16_t {
   // LINT.IfChange(LastGeneratedScriptWrappableTag)
   kFirstPointerTag = 2101,
   // LINT.ThenChange(third_party/blink/renderer/platform/bindings/wrapper_type_info.h)
+  // keep-sorted start case=no
   kAccessibilityControllerBindings,  // content::AccessibilityControllerBindings
   kAPIBindingBridge,                 // extensions::APIBindingBridge
   kAPIBindingJSUtil,                 // extensions::APIBindingJSUtil
@@ -40,10 +41,12 @@ enum WrappablePointerTag : uint16_t {
   kGinJavaBridgeObject,              // content::GinJavaBridgeObject
   kGinPort,                          // extensions::GinPort
   kGpuBenchmarking,                  // content::GpuBenchmarking
+  kIndigoContext,                    // indigo::IndigoContext
+  kIndigoOnboarding,                 // indigo::OnboardingContext
   kJsBinding,                        // js_injection::JsBinding
+  kJSHookInterface,                  // extensions::JSHookInterface
   kJsMessageEvent,                   // android_webview::JsMessageEvent
   kJsSandboxMessagePort,             // android_webview::JsSandboxMessagePort
-  kJSHookInterface,                  // extensions::JSHookInterface
   kLastErrorObject,                  // extensions::LastErrorObject
   kLocalStorageArea,                 // extensions::LocalStorageArea
   kManagedStorageArea,               // extensions::ManagedStorageArea
@@ -77,9 +80,8 @@ enum WrappablePointerTag : uint16_t {
   kTextInputControllerBindings,  // content::TextInputControllerBindings
   kWebAXObjectProxy,             // content::WebAXObjectProxy
   kWrappedExceptionHandler,      // extensions::WrappedExceptionHandler
-  kIndigoContext,                // indigo::IndigoContext
-  kIndigoOnboarding,             // indigo::OnboardingContext
-  kLastPointerTag = kIndigoOnboarding,
+  // keep-sorted end
+  kLastPointerTag,
 };
 
 static_assert(kLastPointerTag <
