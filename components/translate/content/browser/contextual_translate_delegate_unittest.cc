e7016de4b22a8955a970d9673a77262f17048339
Andrew Paseltiner <apaseltiner@chromium.org>
2026-06-11 15:27:00 -0700

    Use JSONReader in ContextualTranslateDelegate
    Since base::JSONReader is now safe for the rule of 2, it can be used
    directly and synchronously in the browser process. This allows for a
    cleaner implementation by inlining the parsing logic and removing the
    need for a separate asynchronous callback.
    
    This change also updates the unit tests to remove the now-unnecessary
    InProcessDataDecoder.
    
    Bug: 459848478
    Change-Id: I4a503c4f706b9f580bb58f4a83ab12920099d1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905439
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645651}

--- components/translate/content/browser/contextual_translate_delegate_unittest.cc
+++ components/translate/content/browser/contextual_translate_delegate_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/translate/core/browser/translate_pref_names.h"
 #include "net/base/url_util.h"
 #include "net/http/http_status_code.h"
-#include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/public/cpp/data_element.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
@@ -102,7 +101,6 @@ class ContextualTranslateDelegateTest : public testing::Test {
   }
 
   base::test::TaskEnvironment task_environment_;
-  data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
   std::unique_ptr<network::TestURLLoaderFactory> test_url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
   std::unique_ptr<ContextualTranslateDelegate> delegate_;
