121df5ca960f4695637afa31eff57e343089c808
Hamza Mohammed <hamzamoh@google.com>
2026-06-11 15:01:10 -0700

    [iOS] Feature cleanup in chromium for Gemini Dynamic Settings.
    It must go in after the ios_internal changes in crrev.com/i/9375883 as well as the google3 changes in https://critique.corp.google.com/cl/929344572/analysis
    
    Fixed: 498579269
    Change-Id: Icb2339d672d0f7f41d98ee2d8858cb9f85ab03c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915104
    Reviewed-by: Joe Peplowski <joepeplowski@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Hamza Mohammed <hamzamoh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645631}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -456,10 +456,6 @@ void GeminiBrowserAgent::OnPrimaryAccountChanged(
 }
 
 void GeminiBrowserAgent::ConfigureGemini() {
-  if (!IsGeminiDynamicSettingsEnabled()) {
-    return;
-  }
-
   AuthenticationService* auth_service =
       AuthenticationServiceFactory::GetForProfile(browser_->GetProfile());
   if (!auth_service || !auth_service->HasPrimaryIdentity()) {
