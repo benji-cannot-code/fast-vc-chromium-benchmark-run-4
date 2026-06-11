c9452bf08f5a067e534cb5be9f6a90ed7674411d
Caitlin Chen <caitc@google.com>
2026-06-11 11:20:26 -0700

    [realbox] Hide Ai Mode button when autocomplete or url suggestion is present
    This change mimics the behavior guarded by
    kNtpRealboxDynamicAiModeButton and kWebUIOmniboxDynamicAiModeButton in
    omnibox. This makes the Ai Mode button hide so it doesn't interfere if
    the user has a url search.
    
    Realbox behavior is guarded by kNtpRealboxDynamicAiModeButton
    
    Bug: 521503358
    Change-Id: I3e692a3fd4b6d0de0579d85ef2ea4d03a860509a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915693
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Caitlin Chen <caitc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645493}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.cc
@@ -661,6 +661,10 @@ content::WebUIDataSource* CreateAndAddNewTabPageUiHtmlSource(
       "searchboxShowComposeEntrypoint",
       (aim_eligible || ntp_composebox::IsNtpComposeboxEnabled(profile)));
 
+  source->AddBoolean(
+      "ntpRealboxDynamicAiModeButton",
+      base::FeatureList::IsEnabled(ntp_realbox::kNtpRealboxDynamicAiModeButton));
+
   if (ntp_realbox::IsNtpRealboxNextEnabled(profile)) {
     if (base::FeatureList::IsEnabled(
             ntp_realbox::kNtpRealboxCyclingPlaceholders)) {
