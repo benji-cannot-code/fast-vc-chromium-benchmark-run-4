a41e1eecd0dcf08a07a9269f5c8442b8a39ffdcd
Huan <huanwu@google.com>
2026-06-11 14:15:07 -0700

    [glic] Fix flaky experimental opt-in offline test
    Tested on linux-oi-rel and linux-chromeos-rel:https://ci.chromium.org/ui/p/chromium/builders/try/linux-chromeos-rel/2901214/overview
    
    Bug: b/517881991
    Change-Id: If354babbfabb5d32b9889b988f47ad26d2a92de6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926568
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Huan Wu <huanwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645601}

--- chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_dialog_browsertest.cc
+++ chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_dialog_browsertest.cc
@@ -846,8 +846,9 @@ IN_PROC_BROWSER_TEST_F(GlicExperimentalOptInTest, NoAccountCookieSyncFails) {
 
   // Click the close button inside the error panel to dismiss the dialog.
   views::test::WidgetDestroyedWaiter waiter(widget);
-  ASSERT_TRUE(ExecJs(dialog_contents,
-                     "setTimeout(() => getCloseButtonError().click(), 0);"));
+  content::ExecuteScriptAsync(
+      dialog_contents,
+      std::string(kCommonJs) + "getCloseButtonError().click();");
   waiter.Wait();
 
   EXPECT_FALSE(opt_in_result.Get());
@@ -943,12 +944,13 @@ IN_PROC_BROWSER_TEST_F(GlicExperimentalOptInOfflineTest,
   EXPECT_TRUE(close_button_visible);
 
   // Click the close button inside the WebUI.
-  ASSERT_TRUE(ExecJs(dialog_contents,
-                     "setTimeout(() => "
-                     "getCloseButtonError().click(), 0);"));
+  views::test::WidgetDestroyedWaiter waiter(widget);
+  content::ExecuteScriptAsync(
+      dialog_contents,
+      std::string(kCommonJs) + "getCloseButtonError().click();");
 
   // The dialog should close and the result should be false (rejected).
-  views::test::WidgetDestroyedWaiter(widget).Wait();
+  waiter.Wait();
   EXPECT_FALSE(opt_in_result.Get());
 }
 
