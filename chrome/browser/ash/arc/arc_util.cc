bc9e34c2fff4a96770b520fbc41573fb2473577f
Jun Ishiguro <junis@google.com>
2026-06-10 22:53:04 -0700

    Revert "arc: Respect ChromeOS location in ARC silent boot-up"
    This reverts commit a4afa38d63d24422a8364cb86571b1165d0360f3.
    
    Reason for revert: Culprit of tast.arc.ManagedProvisioning.vm failure.
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-brya-chrome-tests/12280/overview
    
    Original change's description:
    > arc: Respect ChromeOS location in ARC silent boot-up
    >
    > For ARCVM, "Backup and Restore" and "Location" settings must be
    > selected before the VM spins up. Legacy code used
    > `kArcLocationServicesEnabled` to control the location setting. However,
    > with Privacy Hub, ARC should now respect the ChromeOS-level location
    > preference - `kArcUserGeolocationAccessLevel`.
    >
    > This change updates `ShouldStartArcSilentlyForManagedProfile()` to check
    > the ChromeOS location setting. This prevents ARC from booting up too
    > early during OOBE, which was causing the location switch to "jump" and
    > fail to respect the user's selection.
    >
    > Mocks: go/oobe-arc-toggles-before-after
    >
    > Bug: 465614412
    > Change-Id: I5f757290f317f384f0508231bca39efc61b4e46f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789949
    > Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    > Auto-Submit: Zauri Meshveliani‎ <zauri@chromium.org>
    > Reviewed-by: Osama Fathy <osamafathy@google.com>
    > Commit-Queue: Osama Fathy <osamafathy@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1644694}
    
    Bug: 465614412, b:511161202
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 465614412
    Change-Id: I565451cfb67dd4ca3f84a33e11d8577d0da47845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7924327
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Owners-Override: Jun Ishiguro <junis@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1645124}

--- chrome/browser/ash/arc/arc_util.cc
+++ chrome/browser/ash/arc/arc_util.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/constants/ash_features.h"
-#include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "base/files/file_path.h"
@@ -563,8 +562,7 @@ bool AreArcAllOptInPreferencesIgnorableForProfile(const Profile* profile) {
   if (ash::features::IsCrosPrivacyHubLocationEnabled()) {
     // When PH is enabled, location toggle is no longer ARC specific (applies to
     // entire ChromeOS);
-    return prefs->IsManagedPreference(prefs::kArcBackupRestoreEnabled) &&
-           prefs->IsManagedPreference(ash::prefs::kUserGeolocationAccessLevel);
+    return prefs->IsManagedPreference(prefs::kArcBackupRestoreEnabled);
   } else {
     return prefs->IsManagedPreference(prefs::kArcBackupRestoreEnabled) &&
            prefs->IsManagedPreference(prefs::kArcLocationServiceEnabled);
