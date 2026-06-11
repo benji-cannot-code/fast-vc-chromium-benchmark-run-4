a9ad7deb71a50d8d2c395bb7f58b376749218858
Jason Hu <hujasonx@google.com>
2026-06-10 17:28:32 -0700

    [context-input] Populate AddedInputs for raw-files and drive.
    Populate AddedInputs params from ComposeboxQueryController for raw-file
    types and drive files even when OnlySendAaiForModalityChips is enabled.
    
    Bug: b:520052190
    Change-Id: Ie76647e0f5d4c4df97ffe772de3fad22ac010595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921606
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1644999}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -150,6 +150,9 @@ BASE_FEATURE(kLensSendUrlsInComposeboxes,
 BASE_FEATURE(kLensOnlySendAaiForModalityChips,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+const base::FeatureParam<bool> kLensOnlySendAaiExcludeRawAndDriveFiles{
+    &kLensOnlySendAaiForModalityChips, "exclude_raw_and_drive_files", true};
+
 BASE_FEATURE(kLensRestrictAnnotatedPageContentToSameSiteFramesForNextQueries,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
@@ -1338,6 +1341,10 @@ bool IsLensSendUrlsInComposeboxesEnabled() {
 
 bool IsLensOnlySendAaiForModalityChipsEnabled() {
   return base::FeatureList::IsEnabled(kLensOnlySendAaiForModalityChips);
+}
+
+bool IsLensOnlySendAaiExcludeRawAndDriveFilesEnabled() {
+  return kLensOnlySendAaiExcludeRawAndDriveFiles.Get();
 }
 
 }  // namespace lens::features
