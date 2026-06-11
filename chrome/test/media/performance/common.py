7ce4002121ca2730435b38301ee83973f9963460
Bailey Myers-Morgan <bmyersmorgan@google.com>
2026-06-11 16:48:18 -0700

    [OpenScreen] Update batch scripts to enforce 60fps playback
    Without specifying the external display switch, some lower-performance
    Windows machines were defaulting to the internal display which forced
    30fps video playback. By specifying the switch and a 60fps framerate on
    the capture card, we guarantee 60fps playback on those harnesses.
    
    Change-Id: I6065cfc3d14652c8b8199acca6e57a661915339e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7927847
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645693}

--- chrome/test/media/performance/common.py
+++ chrome/test/media/performance/common.py
@@ -557,6 +557,7 @@ def install_and_setup_chrome(args, chrome_version):
 
         # Create and run the batch script
         batch_script_content = (
+            'DisplaySwitch.exe /external\n'
             f'set PATH=%PATH%;{remote_tmp_dir}/{driver_dir}\n'
             f'cd /d "{remote_tmp_dir}/{driver_dir}"\n'
             f'"{remote_tmp_dir}/{driver_dir}/chromedriver.exe" '
@@ -586,7 +587,7 @@ def install_and_setup_chrome(args, chrome_version):
             args.sender, args.username,
             (f'powershell -Command '
               '"schtasks /create /tn StartChromeDriverTask /tr '
-             f'\'{batch_script_path}\' /sc ONCE /st 23:59 /f"'),
+             f'\'{batch_script_path}\' /sc ONCE /st 23:59 /IT /f"'),
             blocking=True)
         send_ssh_command(args.sender, args.username,
                          'powershell -Command '
