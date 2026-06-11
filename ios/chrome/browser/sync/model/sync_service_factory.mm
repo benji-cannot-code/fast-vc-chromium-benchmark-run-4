c8b423d4dd5b2e5467a6ef8ea804bd47205824bb
Rafał Godlewski <rgod@google.com>
2026-06-11 01:51:59 -0700

    [iOS] Notify password stores of sync service initialized
    This is needed for the password store's GetError() to actually propagate
    the error on iOS, as it's using sync_observation_ which starts observing
    in those calls only [1][2].
    
    This subtly changes whether the save password infobar would be displayed
    on iOS or not, since it's based on the function that uses GetError() to
    decide [3]. Without this change, save password infobar would always
    display on iOS, since the returned error would be `kNoError`.
    
    In the scope of kInFlowTrustedVaultKeyRetrievalIos feature, some of the
    errors will be possible to fix from the save password infobar, and more
    generally, the logic whether the save password infobar should be
    displayed, will be updated.
    
    To prevent any changes to the existing logic, the calls in sync service
    factory will also be flag protected for now.
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:components/password_manager/core/browser/password_store/password_store_built_in_backend.cc;l=228;drc=77010a16c40f098e9a0d9022eed415ad8e491540
    [2]
    https://source.chromium.org/chromium/chromium/src/+/main:components/password_manager/core/browser/password_store/password_store_built_in_backend.cc;l=438;drc=77010a16c40f098e9a0d9022eed415ad8e491540
    [3]
    https://source.chromium.org/chromium/chromium/src/+/main:components/password_manager/core/browser/password_manager_util.cc;l=149;drc=77010a16c40f098e9a0d9022eed415ad8e491540
    
    Bug: 464228247
    Change-Id: I2befd7719c94137bc52c61eda83c8a6b08c7c442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914921
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645202}

--- ios/chrome/browser/sync/model/sync_service_factory.mm
+++ ios/chrome/browser/sync/model/sync_service_factory.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/collaboration/public/collaboration_service.h"
 #import "components/keyed_service/core/service_access_type.h"
 #import "components/network_time/network_time_tracker.h"
+#import "components/password_manager/core/browser/features/password_features.h"
 #import "components/password_manager/core/browser/password_store/password_store_interface.h"
 #import "components/password_manager/core/browser/sharing/password_receiver_service.h"
 #import "components/prefs/pref_service.h"
@@ -246,6 +247,25 @@ std::unique_ptr<KeyedService> BuildSyncService(ProfileIOS* profile) {
   if (GoogleGroupsManager* groups_updater_service =
           GoogleGroupsManagerFactory::GetForProfile(profile)) {
     groups_updater_service->OnSyncServiceInitialized(sync_service.get());
+  }
+
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kInFlowTrustedVaultKeyRetrievalIos)) {
+    scoped_refptr<password_manager::PasswordStoreInterface>
+        profile_password_store =
+            IOSChromeProfilePasswordStoreFactory::GetForProfile(
+                profile, ServiceAccessType::EXPLICIT_ACCESS);
+    if (profile_password_store) {
+      profile_password_store->OnSyncServiceInitialized(sync_service.get());
+    }
+
+    scoped_refptr<password_manager::PasswordStoreInterface>
+        account_password_store =
+            IOSChromeAccountPasswordStoreFactory::GetForProfile(
+                profile, ServiceAccessType::EXPLICIT_ACCESS);
+    if (account_password_store) {
+      account_password_store->OnSyncServiceInitialized(sync_service.get());
+    }
   }
 
   return sync_service;
