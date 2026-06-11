9807c206f31ad93283857d9c6944e38aaba2244a
Etienne Pierre-doray <etiennep@chromium.org>
2026-06-11 04:29:22 -0700

    [tracing] Avoid perfetto track aliasing in chrome/browser/page_load_metrics
    perfetto::Track::FromPointer is problematic because pointer reuse can
    cause track aliasing in perfetto UI, leading to confusing traces (this
    was true of legacy TRACE_EVENT_ASYNC as well).
    
    In this case, I removed the events, since NavigationToStartFirstPreload
    is deprecated.
    
    Bug: 432427382
    Change-Id: Icd683cf364e2f2c8f0b359c1a8d06d95472b1544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7873053
    Auto-Submit: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645264}

--- chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/lcp_critical_path_predictor_page_load_metrics_observer.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/base/url_util.h"
 #include "third_party/blink/public/common/features.h"
-#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace internal {
 
@@ -829,17 +828,6 @@ void LcpCriticalPathPredictorPageLoadMetricsObserver::
         network::mojom::RequestDestination request_destination) {
   if (!lcpp_data_inputs_) {
     lcpp_data_inputs_.emplace();
-  }
-  if (lcpp_data_inputs_->subresource_urls.empty()) {
-    base::UmaHistogramMediumTimes(
-        "Blink.LCPP.NavigationToStartPreload.MainFrame.FirstSubresource.Time",
-        subresource_load_start);
-    const base::TimeTicks navigation_start = GetDelegate().GetNavigationStart();
-    TRACE_EVENT_BEGIN("loading", "NavigationToStartFirstPreload",
-                      perfetto::Track::FromPointer(this), navigation_start,
-                      "url", subresource_url);
-    TRACE_EVENT_END("loading", perfetto::Track::FromPointer(this),
-                    navigation_start + subresource_load_start);
   }
   base::UmaHistogramMediumTimes(
       "Blink.LCPP.NavigationToStartPreload.MainFrame.EachSubresource.Time",
