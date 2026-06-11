90b2e598892ad897982a8eee3d63c7506310a091
Avi Drissman <avi@chromium.org>
2026-06-11 07:35:05 -0700

    Remove code supporting macOS 12 from remote_cocoa
    Chromium no longer supports macOS 12.
    
    Bug: 473856754
    Change-Id: Ia647a6ec67bb1d0031e258a53e84d88d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921149
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645354}

--- components/remote_cocoa/app_shim/native_widget_ns_window_bridge.mm
+++ components/remote_cocoa/app_shim/native_widget_ns_window_bridge.mm
@@ -1854,15 +1854,11 @@ void NativeWidgetNSWindowBridge::SetCanAppearInExistingFullscreenSpaces(
     bool can_appear_in_existing_fullscreen_spaces) {
   NSWindowCollectionBehavior collectionBehavior = window_.collectionBehavior;
   if (can_appear_in_existing_fullscreen_spaces) {
-    if (@available(macOS 13.0, *)) {
-      collectionBehavior &= ~NSWindowCollectionBehaviorPrimary;
-    }
+    collectionBehavior &= ~NSWindowCollectionBehaviorPrimary;
     collectionBehavior |= NSWindowCollectionBehaviorFullScreenAuxiliary;
     collectionBehavior &= ~NSWindowCollectionBehaviorFullScreenPrimary;
   } else {
-    if (@available(macOS 13.0, *)) {
-      collectionBehavior |= NSWindowCollectionBehaviorPrimary;
-    }
+    collectionBehavior |= NSWindowCollectionBehaviorPrimary;
     collectionBehavior |= NSWindowCollectionBehaviorFullScreenPrimary;
     collectionBehavior &= ~NSWindowCollectionBehaviorFullScreenAuxiliary;
   }
