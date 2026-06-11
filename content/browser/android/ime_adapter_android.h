1e1cfa91484860c9721aa01503f7cb0c1878fd05
Akshat Khandelwal <akshatkh@google.com>
2026-06-11 10:39:21 -0700

    Using Cancellation signal to allow users to cancel preview gestures
    Initially, when you stopped a select or delete stylus gesture midway and
    tried to cancel it using scribbles, it would leave the highlight over
    the word(s).
    
    This cl detects a cancellation signal and uses that to delete all
    kPreviewStylusGesture markers to get rid of the highlight over the
    word(s).
    
    Bug: 498290222
    Change-Id: I27c42fbd44a018bd18c4fb26487b38f3479d9157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7734973
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Akshat Khandelwal <akshatkh@google.com>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1645467}

--- content/browser/android/ime_adapter_android.h
+++ content/browser/android/ime_adapter_android.h
@@ -90,6 +90,7 @@ class CONTENT_EXPORT ImeAdapterAndroid : public RenderWidgetHostConnector {
   void HandleStylusWritingGestureAction(JNIEnv*,
                                         const int32_t,
                                         const base::android::JavaRef<jobject>&);
+  void CancelPreviewGesture();
 
   void OnStylusWritingGestureActionCompleted(
       int,
--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapterImpl.java
@@ -427,6 +427,15 @@ public class ImeAdapterImpl
         getStylusWritingImeCallback().handleStylusWritingGestureAction(-1, gestureData);
     }
 
+    /** Signals to Blink to cancel and clear any active handwriting preview spans. */
+    public void cancelPreviewGesture() {
+        if (mNativeImeAdapterAndroid == 0) {
+            Log.e(TAG, "cancelPreviewGesture called after native adapter was destroyed.");
+            return;
+        }
+        ImeAdapterImplJni.get().cancelPreviewGesture(mNativeImeAdapterAndroid);
+    }
+
     void handleGesture(OngoingGesture request) {
         mOngoingGestures.put(request.getId(), request);
         StylusWritingGestureData gestureData = request.getGestureData();
@@ -2229,6 +2238,8 @@ public class ImeAdapterImpl
         // Stylus Writing
         void handleStylusWritingGestureAction(
                 long nativeImeAdapterAndroid, int id, ByteBuffer gestureData);
+
+        void cancelPreviewGesture(long nativeImeAdapterAndroid);
 
         void performSpellCheck(long nativeImeAdapterAndroid);
 
