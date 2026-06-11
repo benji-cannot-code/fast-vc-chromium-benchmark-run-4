d82ef7e532c82ad7e03e0a4f0ff6471bf5c0e25e
Greg Thompson <grt@chromium.org>
2026-06-11 07:13:32 -0700

    [persistent_cache] Fix interpretation of errors upon Bind
    Database::GetErrorCode() unconditionally returns SQLITE_ERROR after a
    failed call to Open(). As such, SqliteBackendImpl must temporarily
    install an error handler and capture an error encountered during a
    failed Open() so that it can report a correct TransactionError.
    
    Bug: 377475540
    Change-Id: Idb2e90967d9a2efeb68cdb98f8ac1f07f92566c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7925893
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645338}

--- components/persistent_cache/sqlite/sqlite_backend_impl.cc
+++ components/persistent_cache/sqlite/sqlite_backend_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <functional>
 #include <memory>
 #include <optional>
 #include <tuple>
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/containers/span.h"
+#include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
@@ -132,8 +134,24 @@ base::expected<void, TransactionError> SqliteBackendImpl::Initialize() {
 }
 
 base::expected<void, int> SqliteBackendImpl::InitializeImpl() {
-  if (!db_->Open(database_path_)) {
-    return base::unexpected(db_->GetErrorCode());
+  // Install an error callback to capture the first error, if any, encountered
+  // while opening a database. Database::GetErrorCode() returns a generic error
+  // after a failed Open(), so it is of no use in this case.
+  std::optional<int> error_code;
+  db_->set_error_callback(base::BindRepeating(
+      [](std::optional<int>& error_code_out, int error,
+         sql::Statement* statement) {
+        if (!error_code_out.has_value()) {
+          error_code_out = error;
+        }
+      },
+      std::ref(error_code)));
+
+  const bool open_success = db_->Open(database_path_);
+  db_->reset_error_callback();
+
+  if (!open_success) {
+    return base::unexpected(error_code.value_or(SQLITE_CANTOPEN));
   }
 
   // Check the user-version (https://sqlite.org/pragma.html#pragma_user_version)
