f1e92a9e224e2388d3d592dd069a150190033f95
Jennifer Serrano <jennserrano@google.com>
2026-06-11 09:08:33 -0700

    Add feature param to easily test the auto-hiding of the bookmark bar.
    Instead of the 15 active day limit to hide the bookmark bar it can be
    hidden after 15 seconds and one page refresh.
    
    Bug: 521974541
    Change-Id: I66c8854d962aed4b42092a9b6c076cafd5648ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7917012
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645414}

--- components/search/ntp_features.h
+++ components/search/ntp_features.h
@@ -88,6 +88,7 @@ BASE_DECLARE_FEATURE(kNtpDoodleMurals);
 BASE_DECLARE_FEATURE(kLightningTakeoverEdition);
 BASE_DECLARE_FEATURE(kNtpShortcutsRedesign);
 BASE_DECLARE_FEATURE(kNtpSimplificationBookmarkBar);
+BASE_DECLARE_FEATURE(kBookmarkBarUpdatesForTesting);
 
 // Parameter for controlling the luminosity difference for NTP elements on light
 // backgrounds.
@@ -336,6 +337,10 @@ int GetMaxMostVisitedTilesInExpandedState();
 
 // Returns the max number of enterprise shortcuts allowed.
 int GetMaxEnterpriseShortcuts();
+
+base::TimeDelta GetBookmarkBarMinStalenessTimeInterval();
+
+int GetBookmarkBarCountThreshold();
 
 }  // namespace ntp_features
 
