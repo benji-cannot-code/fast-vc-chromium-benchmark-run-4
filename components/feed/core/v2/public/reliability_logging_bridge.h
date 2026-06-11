6e09599a950e02830d25baaaf3565c2b90358f9f
Yanling <yanlinghu@google.com>
2026-06-11 16:03:10 -0700

    [Feed] Part18 of remove web feed feature: remove dead web feed JNI and reliability logging methods
    Bug: 407797637
    Change-Id: Iac6acee54c3566ef3801c0936bec2038615f3ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7883586
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645666}

--- components/feed/core/v2/public/reliability_logging_bridge.h
+++ components/feed/core/v2/public/reliability_logging_bridge.h
@@ -33,8 +33,6 @@ class ReliabilityLoggingBridge {
                                    base::TimeTicks timestamp) = 0;
   virtual void LogActionsUploadRequestStart(NetworkRequestId id,
                                             base::TimeTicks timestamp) = 0;
-  virtual void LogWebFeedRequestStart(NetworkRequestId id,
-                                      base::TimeTicks timestamp) = 0;
   virtual void LogRequestSent(NetworkRequestId id,
                               base::TimeTicks timestamp) = 0;
   virtual void LogResponseReceived(
