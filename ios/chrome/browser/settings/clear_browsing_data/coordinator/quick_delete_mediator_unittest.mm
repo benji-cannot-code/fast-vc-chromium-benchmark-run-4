4279291a9a10866b3aff6c0ccd24b8c9e3b7a4eb
Angela Novakovic <novakovica@google.com>
2026-06-11 14:03:07 -0700

    [iOS][PRDBD] Enable PasswordRemovalFromDeleteBrowsingData by default
    This CL turns on the PasswordRemovalFromDeleteBrowsingData feature by
    default (100% stable) and removes the corresponding experiment from
    fieldtrial_testing_config.json.
    
    Fixed: 463396745
    Bug: 463402932
    Change-Id: Id31a690314a3e900533ca704c00be55943e7f945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905453
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Sourav Uttam Sinha <sinhasourav@google.com>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645588}

--- ios/chrome/browser/settings/clear_browsing_data/coordinator/quick_delete_mediator_unittest.mm
+++ ios/chrome/browser/settings/clear_browsing_data/coordinator/quick_delete_mediator_unittest.mm
@@ -301,7 +301,12 @@ TEST_F(QuickDeleteMediatorTest, TestBrowsingHistorySummary) {
   // Trigger the callback for data types not in test. The summary is only
   // dispatches if all counters have returned.
   TriggerUpdateUICallbackForTabsResults(0);
-  TriggerUpdateUICallbackForPasswordsResults(0);
+  // TODO(crbug.com/463402932): Clean up once the
+  // kPasswordRemovalFromDeleteBrowsingData feature flag has been enabled on
+  // stable 100% for a month.
+  if (!IsPasswordRemovalFromDeleteBrowsingDataEnabled()) {
+    TriggerUpdateUICallbackForPasswordsResults(0);
+  }
   TriggerUpdateUICallbackForAutofillResults(0, 0, 0);
 
   // clang-format off
@@ -358,7 +363,12 @@ TEST_F(QuickDeleteMediatorTest, TestTabsSummary) {
   // Trigger the callback for data types not in test. The summary is only
   // dispatches if all counters have returned.
   TriggerUpdateUICallbackForHistoryResults(0);
-  TriggerUpdateUICallbackForPasswordsResults(0);
+  // TODO(crbug.com/463402932): Clean up once the
+  // kPasswordRemovalFromDeleteBrowsingData feature flag has been enabled on
+  // stable 100% for a month.
+  if (!IsPasswordRemovalFromDeleteBrowsingDataEnabled()) {
+    TriggerUpdateUICallbackForPasswordsResults(0);
+  }
   TriggerUpdateUICallbackForAutofillResults(0, 0, 0);
 
   // clang-format off
@@ -481,7 +491,12 @@ TEST_F(QuickDeleteMediatorTest, TestAddressesSummary) {
   // dispatches if all counters have returned.
   TriggerUpdateUICallbackForTabsResults(0);
   TriggerUpdateUICallbackForHistoryResults(0);
-  TriggerUpdateUICallbackForPasswordsResults(0);
+  // TODO(crbug.com/463402932): Clean up once the
+  // kPasswordRemovalFromDeleteBrowsingData feature flag has been enabled on
+  // stable 100% for a month.
+  if (!IsPasswordRemovalFromDeleteBrowsingDataEnabled()) {
+    TriggerUpdateUICallbackForPasswordsResults(0);
+  }
 
   // clang-format off
   const struct TestCase {
@@ -637,6 +652,8 @@ TEST_F(QuickDeleteMediatorTest, TestSuggestionsSummary) {
 
 TEST_F(QuickDeleteMediatorTest,
        TestBrowsingHistorySummaryWithPasswordsUnselected) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(kPasswordRemovalFromDeleteBrowsingData);
   // Select browsing history for deletion, but not passwords.
   prefs()->SetBoolean(browsing_data::prefs::kDeleteBrowsingHistory, true);
 
--- ios/chrome/browser/settings/clear_browsing_data/public/features.mm
+++ ios/chrome/browser/settings/clear_browsing_data/public/features.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/clear_browsing_data/public/features.h"
 
 BASE_FEATURE(kPasswordRemovalFromDeleteBrowsingData,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsPasswordRemovalFromDeleteBrowsingDataEnabled() {
   return base::FeatureList::IsEnabled(kPasswordRemovalFromDeleteBrowsingData);
