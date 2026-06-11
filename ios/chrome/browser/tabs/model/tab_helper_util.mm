7680feeacc772e3b7bf6ee48135816a859cfbc19
ginnnnnnny <ginnyhuang@chromium.org>
2026-06-11 10:24:44 -0700

    [iOS][actor-login] Implement iOS version of ActorLoginDelegateClient
    ...as well as the factories needed to retrieve services that the client
    should return.
    
    Bug: 472291829
    Change-Id: I422de039b07136639e5e2a763994e6fda4a4728b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910442
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645456}

--- ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/keyed_service/core/service_access_type.h"
 #import "components/language/ios/browser/ios_language_detection_tab_helper.h"
 #import "components/omnibox/common/omnibox_features.h"
+#import "components/password_manager/core/browser/features/password_features.h"
 #import "components/safe_browsing/core/common/features.h"
 #import "components/safe_browsing/ios/browser/safe_browsing_url_allow_list.h"
 #import "components/send_tab_to_self/features.h"
@@ -87,6 +88,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/overscroll_actions/model/overscroll_actions_tab_helper.h"
 #import "ios/chrome/browser/page_info/features/features.h"
 #import "ios/chrome/browser/page_info/model/about_this_site_tab_helper.h"
+#import "ios/chrome/browser/passwords/model/actor_login/ios_chrome_actor_login_delegate_client.h"
 #import "ios/chrome/browser/passwords/model/ios_chrome_account_password_store_factory.h"
 #import "ios/chrome/browser/passwords/model/password_controller.h"
 #import "ios/chrome/browser/passwords/model/password_tab_helper.h"
@@ -386,6 +388,9 @@ void AttachTabHelpers(web::WebState* web_state, TabHelperFilter filter_flags) {
   const bool is_actor_tab_helper_enabled =
       IsActorEnabled() && !attacher.IsForPrerender();
   attacher.CreateWhen<ActorTabHelper>(is_actor_tab_helper_enabled);
+  attacher.CreateWhen<IOSChromeActorLoginDelegateClient>(
+      is_actor_tab_helper_enabled &&
+      base::FeatureList::IsEnabled(password_manager::features::kActorLogin));
 
   attacher.Create<WebViewProxyTabHelper>();
 
