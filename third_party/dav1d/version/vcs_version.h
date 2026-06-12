62fadf1deeaa19fd1257f43f5265887d3860c224
Wan-Teh Chang <wtc@google.com>
2026-06-12 11:01:53 -0700

    Roll src/third_party/dav1d/libdav1d/ 62501cc7d..46e901735 (3 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/62501cc7db37..46e901735541
    
    $ git log 62501cc7d..46e901735 --date=short --no-merges --format='%ad %ae %s'
    2026-06-07 code subprojects: Update checkasm to v1.2.0
    2026-06-02 martin ci: Add -Dtrim_dsp=false in a couple of aarch64/arm configurations
    2026-06-07 unlord riscv64/itx: Match stack allocation of 16x16 itx
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ifc088eb2531f020d0c0ad20dcb94ce29515effe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7913910
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646099}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-49-g62501cc7"
+#define DAV1D_VERSION "1.5.3-52-g46e90173"
