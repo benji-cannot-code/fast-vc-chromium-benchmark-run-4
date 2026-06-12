16cd2e76f934867f049d95fabfb7ce25c01fe46d
Jonathan Ross <jonross@chromium.org>
2026-06-12 08:46:36 -0700

    Harden Viz: BeginFrameAck Validation
    Update validation of BeginFrameAck to disconnect from clients providing
    invalid responses.
    
    Bug: 517414414
    Change-Id: I1e1399022b53b4e938b21aaf9884545fc40584e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893523
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Stacy Gaikovaia <gaiko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645994}

--- components/viz/service/frame_sinks/compositor_frame_sink_support.cc
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.cc
@@ -665,7 +665,7 @@ void CompositorFrameSinkSupport::UpdateThreadIdsPostVerification(
   }
 }
 
-void CompositorFrameSinkSupport::DidNotProduceFrame(const BeginFrameAck& ack) {
+bool CompositorFrameSinkSupport::DidNotProduceFrame(const BeginFrameAck& ack) {
   TRACE_EVENT(
       "viz,benchmark,graphics.pipeline", "Graphics.Pipeline",
       perfetto::Flow::Global(ack.trace_id), [&](perfetto::EventContext ctx) {
@@ -677,7 +677,9 @@ void CompositorFrameSinkSupport::DidNotProduceFrame(const BeginFrameAck& ack) {
         frame_sink_id_.WriteIntoTrace(ctx.Wrap(data->set_frame_sink_id()));
         data->set_surface_frame_trace_id(ack.trace_id);
       });
-  DCHECK(ack.frame_id.IsSequenceValid());
+  if (!ack.frame_id.IsSequenceValid()) {
+    return false;
+  }
 
   begin_frame_tracker_.ReceivedAck(ack);
 
@@ -703,6 +705,7 @@ void CompositorFrameSinkSupport::DidNotProduceFrame(const BeginFrameAck& ack) {
     begin_frame_source_->DidFinishFrame(this);
     frame_sink_manager_->DidFinishFrame(frame_sink_id_, last_begin_frame_args_);
   }
+  return true;
 }
 
 void CompositorFrameSinkSupport::SubmitCompositorFrame(
@@ -767,6 +770,10 @@ SubmitResult CompositorFrameSinkSupport::MaybeSubmitCompositorFrame(
     return SubmitResult::INVALID_DISPLAY_TRANSFORM;
   }
 
+  if (!frame.metadata.begin_frame_ack.frame_id.IsSequenceValid()) {
+    return SubmitResult::INVALID_BEGIN_FRAME_ACK;
+  }
+
   CHECK(callback_received_begin_frame_);
   CHECK(callback_received_receive_ack_);
 
@@ -782,7 +789,6 @@ SubmitResult CompositorFrameSinkSupport::MaybeSubmitCompositorFrame(
 
   // Override the has_damage flag (ignoring invalid data from clients).
   frame.metadata.begin_frame_ack.has_damage = true;
-  DCHECK(frame.metadata.begin_frame_ack.frame_id.IsSequenceValid());
 
   if (!ui::LatencyInfo::Verify(
           frame.metadata.latency_info,
@@ -1470,6 +1476,8 @@ const char* CompositorFrameSinkSupport::GetSubmitResultAsString(
       return "Invalid CompositorFrame";
     case SubmitResult::INVALID_DISPLAY_TRANSFORM:
       return "Invalid display transform hint";
+    case SubmitResult::INVALID_BEGIN_FRAME_ACK:
+      return "Invalid BeginFrameAck sequence";
   }
   NOTREACHED();
 }
