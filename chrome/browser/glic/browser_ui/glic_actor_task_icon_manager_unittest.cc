e0f7a9ae0aee894edfd21870faed1dbac2f16303
rosiey <rosiey@google.com>
2026-06-12 14:04:23 -0700

    [glic] Fix start notification not popping out for foreground/background triggered tasks
    This CL fixes the issue where the start notification bubble for
    experimental triggering tasks fails to pop out when started via
    foreground or background triggers.
    
    Result: http://shortn/_5v9pcou9IF
    Bug: b:518779463
    Change-Id: I805f50790a958eabafe2dcfc6ee6a9b8a5474d10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7925313
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Maggie Jennings <mjenn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646231}

--- chrome/browser/glic/browser_ui/glic_actor_task_icon_manager_unittest.cc
+++ chrome/browser/glic/browser_ui/glic_actor_task_icon_manager_unittest.cc
@@ -39,7 +39,7 @@ class MockTaskNudgeStateChangeSubscriber {
 // mock.
 class MockTaskListBubbleChangeSubscriber {
  public:
-  MOCK_METHOD(void, OnStateChanged, ());
+  MOCK_METHOD(void, OnStateChanged, (bool is_start_notification));
 };
 
 class GlicActorTaskIconManagerTest : public testing::Test,
@@ -160,7 +160,7 @@ TEST_F(GlicActorTaskIconManagerTest,
                   /*show_bubble=*/true,
                   ActorTaskNudgeState{
                       .text = ActorTaskNudgeState::Text::kNeedsAttention}));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged());
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(false));
 
   TaskId task_id_1 = actor_service()->CreateTaskForTesting();
   actor_service()->PauseTaskForTesting(task_id_1, /*from_actor=*/true);
@@ -179,7 +179,7 @@ TEST_F(GlicActorTaskIconManagerTest,
                   /*show_bubble=*/true,
                   ActorTaskNudgeState{
                       .text = ActorTaskNudgeState::Text::kNeedsAttention}));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged());
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(false));
   EXPECT_CALL(
       mock_nudge_subscriber_,
       OnStateChanged(
@@ -202,7 +202,7 @@ TEST_F(GlicActorTaskIconManagerTest,
 
 TEST_F(GlicActorTaskIconManagerTest,
        MultipleTasksNeedAttentionNudgeShowsMultipleTasksText) {
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(2);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(false)).Times(2);
 
   TaskId task_id_1 = actor_service()->CreateTaskForTesting();
   TaskId task_id_2 = actor_service()->CreateTaskForTesting();
@@ -338,7 +338,7 @@ TEST_F(GlicActorTaskIconManagerTest, TransientTaskDoesNotShowBubble) {
       OnStateChanged(/*show_bubble=*/true,
                      Field(&ActorTaskNudgeState::text,
                            ActorTaskNudgeState::Text::kNeedsAttention)));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged());
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(false));
   actor_service()->PauseTaskForTesting(task_id, /*from_actor=*/true);
   manager()->UpdateTaskIconComponents(task_id);
 
@@ -347,7 +347,7 @@ TEST_F(GlicActorTaskIconManagerTest, TransientTaskDoesNotShowBubble) {
               OnStateChanged(/*show_bubble=*/false,
                              Field(&ActorTaskNudgeState::text,
                                    ActorTaskNudgeState::Text::kCompleteTasks)));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(0);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(testing::_)).Times(0);
   actor_service()->StopTaskForTesting(
       task_id, actor::ActorTask::StoppedReason::kTaskComplete);
   manager()->UpdateTaskIconComponents(task_id);
@@ -406,7 +406,7 @@ TEST_F(GlicActorTaskIconManagerTest,
               OnStateChanged(/*show_bubble=*/false,
                              Field(&ActorTaskNudgeState::text,
                                    ActorTaskNudgeState::Text::kDefault)));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(0);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(testing::_)).Times(0);
   actor_service()->StopTaskForTesting(
       task_id, actor::ActorTask::StoppedReason::kTaskComplete);
   manager()->UpdateTaskIconComponents(task_id);
@@ -425,7 +425,7 @@ TEST_F(GlicActorTaskIconManagerTest,
               OnStateChanged(/*show_bubble=*/true,
                              Field(&ActorTaskNudgeState::text,
                                    ActorTaskNudgeState::Text::kCompleteTasks)));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(2);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(false)).Times(2);
   actor_service()->StopTaskForTesting(
       task_id, actor::ActorTask::StoppedReason::kTaskComplete);
   manager()->UpdateTaskIconComponents(task_id);
@@ -504,7 +504,7 @@ TEST_F(GlicActorTaskIconManagerTest,
               OnStateChanged(/*show_bubble=*/true,
                              Field(&ActorTaskNudgeState::text,
                                    ActorTaskNudgeState::Text::kDefault)));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(1);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(true)).Times(1);
 
   manager()->UpdateTaskIconComponents(task_id);
 
@@ -516,15 +516,13 @@ TEST_F(GlicActorTaskIconManagerTest,
 
   task->SetState(actor::ActorTask::State::kReflecting);
 
-  EXPECT_CALL(mock_nudge_subscriber_,
-              OnStateChanged(/*show_bubble=*/false,
-                             Field(&ActorTaskNudgeState::text,
-                                   ActorTaskNudgeState::Text::kDefault)));
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(0);
+  EXPECT_CALL(mock_nudge_subscriber_, OnStateChanged(testing::_, testing::_))
+      .Times(0);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(testing::_)).Times(0);
 
   manager()->UpdateTaskIconComponents(task_id);
 
-  EXPECT_FALSE(manager()->actor_task_list_bubble_rows().at(task_id));
+  EXPECT_TRUE(manager()->actor_task_list_bubble_rows().at(task_id));
 
   // Turn 2 starts: state transitions to kActing again.
   testing::Mock::VerifyAndClearExpectations(&mock_nudge_subscriber_);
@@ -534,7 +532,7 @@ TEST_F(GlicActorTaskIconManagerTest,
 
   EXPECT_CALL(mock_nudge_subscriber_, OnStateChanged(testing::_, testing::_))
       .Times(0);
-  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged()).Times(0);
+  EXPECT_CALL(mock_bubble_subscriber_, OnStateChanged(testing::_)).Times(0);
 
   manager()->UpdateTaskIconComponents(task_id);
 }
