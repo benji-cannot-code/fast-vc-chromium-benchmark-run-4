296d3871fb1b6ec6715b30ddf68278cc5ce7b726
Isabelle Goralsky <igoralsky@google.com>
2026-06-12 12:59:36 -0700

    Add a new CMS feature enum for AutoTodos.
    Bug: b:521840844
    Change-Id: I0d8a21dcfd39857b64fe9783dbe1a02f3b7e54af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7929692
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646193}

--- components/personal_context/core/network/personal_context_manager.cc
+++ components/personal_context/core/network/personal_context_manager.cc
@@ -55,6 +55,7 @@ size_t GetMaxParallelFeatureFetchers(proto::ContextMemoryFeature feature) {
   switch (feature) {
     case proto::CONTEXT_MEMORY_FEATURE_AMBIENT_AUTOFILL:
     case proto::CONTEXT_MEMORY_FEATURE_AT_MEMORY:
+    case proto::CONTEXT_MEMORY_FEATURE_AUTO_TODOS:
       return 1;
     default:
       NOTREACHED();
@@ -69,6 +70,7 @@ size_t GetMaxParallelPiiFeatureFetchers(proto::ContextMemoryFeature feature) {
   switch (feature) {
     case proto::CONTEXT_MEMORY_FEATURE_AMBIENT_AUTOFILL:
     case proto::CONTEXT_MEMORY_FEATURE_AT_MEMORY:
+    case proto::CONTEXT_MEMORY_FEATURE_AUTO_TODOS:
       return 1;
     default:
       NOTREACHED();
