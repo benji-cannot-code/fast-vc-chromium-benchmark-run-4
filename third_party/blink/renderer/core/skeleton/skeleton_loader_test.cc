87fe302dbf6589b3422788a4b9776519c6b16cb8
Rune Lillesveen <futhark@chromium.org>
2026-06-12 07:31:57 -0700

    [Skeleton] Source ignored in InsertSkeletonTree()
    The unit tests didn't rely on the actual content of the passed in html,
    but upcoming tests will.
    
    Also create an HTMLDocument instead of using the base class.
    
    Bug: 513276602
    Change-Id: Ib3052557ae107a34eb99ffd3542f2be1492d9f34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928369
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645945}

--- third_party/blink/renderer/core/skeleton/skeleton_loader_test.cc
+++ third_party/blink/renderer/core/skeleton/skeleton_loader_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
+#include "third_party/blink/renderer/core/html/html_document.h"
 #include "third_party/blink/renderer/core/testing/null_execution_context.h"
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_request.h"
@@ -22,10 +23,11 @@ class SkeletonLoaderSimTest : public SimTest {};
 
 class SkeletonLoaderTest : public PageTestBase {
  protected:
-  void InsertSkeletonTree(const String&) {
+  void InsertSkeletonTree(const String& source) {
     ScopedNullExecutionContext execution_context;
-    Document* skeleton_document =
-        Document::CreateForTest(execution_context.GetExecutionContext());
+    HTMLDocument* skeleton_document =
+        HTMLDocument::CreateForTest(execution_context.GetExecutionContext());
+    skeleton_document->SetContent(source);
     SkeletonLoader::Ensure(GetDocument())
         .InsertSkeletonTree(*skeleton_document);
   }
