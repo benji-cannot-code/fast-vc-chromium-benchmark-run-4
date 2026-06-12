ef2c5b69d084e0809359a61c15eada255d80ca4b
Yuki Shiino <yukishiino@chromium.org>
2026-06-12 04:51:04 -0700

    Revert "[dcheck-to-check] Convert DCHECKs in content/browser/renderer_host/ [10/n]"
    This reverts commit 193ca447fc58d701c90a2436735b1637fab9a7f5.
    
    Reason for revert: This patch hit a perf regression.
    https://pinpoint-dot-chromeperf.appspot.com/job/1505e224c90000
    https://issues.chromium.org/issues/522632522
    
    Original change's description:
    > [dcheck-to-check] Convert DCHECKs in content/browser/renderer_host/ [10/n]
    >
    > As part of effort to harden Chromium's safety, turn DCHECKs
    > into CHECKs. The converted CHECKs are not fatal until M152.
    >
    > This patch is generated by the following script.
    > https://crrev.com/c/7849874/10/tools/memory/dcheck/replace_dchecks_with_checks.py
    >
    > Bug: 501112901
    > Change-Id: I60d479cf4ed451aada0edfbd485e1066a6afea9f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7924506
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1645163}
    
    Bug: 501112901, 522632522
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I466790c336976c19f8269fffe3f944b89875f162
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7929187
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645857}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -3158,7 +3158,7 @@ void RenderProcessHostImpl::DisableRefCounts() {
 }
 
 bool RenderProcessHostImpl::AreRefCountsDisabled() {
-  CHECK_CURRENTLY_ON(BrowserThread::UI, base::NotFatalUntil::M152);
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   return are_ref_counts_disabled_;
 }
 
@@ -3215,7 +3215,7 @@ void RenderProcessHostImpl::RemoveRoute(int32_t routing_id) {
                                   ->set_render_process_host_listener_changed();
                 proto->set_routing_id(routing_id);
               });
-  CHECK(listeners_.Lookup(routing_id) != nullptr, base::NotFatalUntil::M152);
+  DCHECK(listeners_.Lookup(routing_id) != nullptr);
   listeners_.Remove(routing_id);
   Cleanup();
 }
@@ -3292,8 +3292,8 @@ void RenderProcessHostImpl::ShutdownForBadMessage(
 
 void RenderProcessHostImpl::UpdateClientPriority(
     RenderProcessHostPriorityClient* client) {
-  CHECK(client, base::NotFatalUntil::M152);
-  CHECK_EQ(1u, priority_clients_.count(client), base::NotFatalUntil::M152);
+  DCHECK(client);
+  DCHECK_EQ(1u, priority_clients_.count(client));
   UpdateProcessPriorityInputs();
 }
 
@@ -4290,7 +4290,7 @@ void RenderProcessHostImpl::OnChannelConnected(int32_t peer_pid) {
 #endif
 
   if (IsReady()) {
-    CHECK(!sent_render_process_ready_, base::NotFatalUntil::M152);
+    DCHECK(!sent_render_process_ready_);
     sent_render_process_ready_ = true;
     // Send RenderProcessReady only if we already received the process handle.
     for (auto& observer : observers_)
@@ -4422,8 +4422,7 @@ bool RenderProcessHostImpl::HasOnlyNonLiveRenderFrameHosts() {
     return false;
 
   // We should never find more than render_frame_host_count_.
-  CHECK_EQ(GetRenderFrameHostCount(), found_rfh_count,
-           base::NotFatalUntil::M152);
+  DCHECK_EQ(GetRenderFrameHostCount(), found_rfh_count);
 
   // We accounted for all the RenderFrameHosts (at least one), and none were
   // live.
@@ -4433,7 +4432,7 @@ bool RenderProcessHostImpl::HasOnlyNonLiveRenderFrameHosts() {
 void RenderProcessHostImpl::Cleanup() {
   TRACE_EVENT("shutdown", "RenderProcessHostImpl::Cleanup",
               ChromeTrackEvent::kRenderProcessHost, *this);
-  CHECK_CURRENTLY_ON(BrowserThread::UI, base::NotFatalUntil::M152);
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   base::UmaHistogramBoolean("BrowserRenderProcessHost.Cleanup.Total", true);
   // Keep the one renderer thread around forever in single process mode.
   if (run_renderer_in_process()) {
@@ -4554,7 +4553,7 @@ void RenderProcessHostImpl::Cleanup() {
   // safe for the process to cleanly exit but not for the RenderProcessHost to
   // be deleted.
   if (has_only_non_live_rfhs) {
-    CHECK(!listeners_.IsEmpty(), base::NotFatalUntil::M152);
+    DCHECK(!listeners_.IsEmpty());
 
     // No need to terminate the renderer if it is already gone.
     if (!IsInitializedAndNotDead())
@@ -4590,7 +4589,7 @@ void RenderProcessHostImpl::Cleanup() {
         base::BindOnce(&WebRtcLog::ClearLogMessageCallback, GetDeprecatedID()));
   }
 
-  CHECK_EQ(0, pending_views_, base::NotFatalUntil::M152);
+  DCHECK_EQ(0, pending_views_);
 
   // If the process associated with this RenderProcessHost is still alive,
   // notify all observers that the process has exited cleanly, even though it
@@ -4654,7 +4653,7 @@ void RenderProcessHostImpl::AddPendingView() {
 }
 
 void RenderProcessHostImpl::RemovePendingView() {
-  CHECK(pending_views_, base::NotFatalUntil::M152);
+  DCHECK(pending_views_);
   --pending_views_;
   if (!pending_views_)
     UpdateProcessPriority();
@@ -4662,8 +4661,7 @@ void RenderProcessHostImpl::RemovePendingView() {
 
 void RenderProcessHostImpl::AddPriorityClient(
     RenderProcessHostPriorityClient* priority_client) {
-  CHECK(!priority_clients_.contains(priority_client),
-        base::NotFatalUntil::M152);
+  DCHECK(!priority_clients_.contains(priority_client));
   priority_clients_.insert(priority_client);
   UpdateProcessPriorityInputs();
 }
