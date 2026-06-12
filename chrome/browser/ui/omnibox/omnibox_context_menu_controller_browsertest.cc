8268f917a852513aa37503f5da81101a060bc6a8
Juliet Lévesque <julietlevesque@google.com>
2026-06-12 12:09:00 -0700

    [aim-drive] Check disclaimer status after drive selection to verify accept state
    This change addresses the edge case where a user has revoked drive
    permissions after seeing the drive option in the plus menu to maintain
    the privacy expectation that the user has accepted consent prior to
    uploading drive files.
    
    Fixed: b:522382344
    Change-Id: I6d62238b41a108943e541f28b2492ba669172a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921947
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Adam Yao <adamyao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646154}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller_browsertest.cc
@@ -311,6 +311,7 @@ class OmniboxContextMenuControllerPecBrowserTest : public InProcessBrowserTest {
            {omnibox::kShowToolsAndModels.name, "true"}}},
          {omnibox::internal::kWebUIOmniboxPopup, {}},
          {omnibox::kAimUsePecApi, {}},
+         {omnibox::kForceDriveDisclaimerAccepted, {}},
          {omnibox::kComposeboxDriveContextMenuOption, {}},
          {omnibox::kContextManagementInComposebox, {}},
          {omnibox::kContextManagementInOmnibox, {}}},
@@ -574,6 +575,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxContextMenuControllerPecBrowserTest,
 
   ASSERT_NE(command_id, -1) << "Drive option not found in menu";
   controller.ExecuteCommand(command_id, 0);
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(OmniboxPopupState::kAim,
             omnibox_controller->popup_state_manager()->popup_state());
@@ -647,6 +649,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxContextMenuControllerPecBrowserTest,
 
   ASSERT_NE(command_id, -1) << "Drive option not found in menu";
   controller.ExecuteCommand(command_id, 0);
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(OmniboxPopupState::kAim,
             omnibox_controller->popup_state_manager()->popup_state());
@@ -704,6 +707,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxContextMenuControllerPecBrowserTest,
 
   ASSERT_NE(command_id, -1) << "Drive option not found in menu";
   controller.ExecuteCommand(command_id, 0);
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(OmniboxPopupState::kClassic,
             omnibox_controller->popup_state_manager()->popup_state());
@@ -759,6 +763,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxContextMenuControllerPecBrowserTest,
 
   ASSERT_NE(command_id, -1) << "Drive option not found in menu";
   controller.ExecuteCommand(command_id, 0);
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(OmniboxPopupState::kAim,
             omnibox_controller->popup_state_manager()->popup_state());
