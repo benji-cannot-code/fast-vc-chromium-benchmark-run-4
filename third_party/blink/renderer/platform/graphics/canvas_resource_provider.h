b0d113fbc8bf3ab43ec05b3b41611b655a373eb9
Colin Blundell <blundell@chromium.org>
2026-06-12 03:27:42 -0700

    [Blink] Move CRP::GetOrCreateSwImageProvider() into subclasses
    Next step on making CanvasResourceProvider into pure interface.
    
    Bug: 352263194
    Change-Id: Ibc63be5465e48578ebb22013312c95083105a740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7924677
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645816}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -221,7 +221,7 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   virtual void RasterRecord(cc::PaintRecord) = 0;
 
-  CanvasImageProvider* GetOrCreateSWCanvasImageProvider();
+  virtual CanvasImageProvider* GetOrCreateSWCanvasImageProvider() = 0;
 
   // Called after the recording was cleared from any draw ops it might have had.
   // Canvas2D-specific, as it is called only when `recorder_` is
@@ -334,6 +334,7 @@ class PLATFORM_EXPORT Canvas2DResourceProviderBitmap
 
   // MemoryManagedPaintRecorder::Client implementation.
   void RecordingCleared() override;
+  CanvasImageProvider* GetOrCreateSWCanvasImageProvider() override;
 
   gfx::Size size_;
   viz::SharedImageFormat format_;
@@ -527,6 +528,7 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
   scoped_refptr<UnacceleratedStaticBitmapImage> UnacceleratedSnapshot(
       ImageOrientation);
   SkSurface* GetSkSurface() const;
+  CanvasImageProvider* GetOrCreateSWCanvasImageProvider() override;
 
  private:
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
