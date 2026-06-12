57891c0b8896cad1b985f784e21911909e0baa9f
Julia Sobiech <jsobiech@google.com>
2026-06-12 03:18:00 -0700

    [iOS] Restore toolbar in Identity Docs and Travel on back navigation
    Forces the bottom toolbar (with the "Add" button) to reappear when
    returning to the Identity Docs or Travel Info settings pages from a
    details page.
    
    Bug: 491417428
    Change-Id: I68e28cc61d7e1b64c0c5725ebe6b971f997f8843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928446
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645801}

--- ios/chrome/browser/settings/autofill/autofill_and_passwords/ui/travel_info_table_view_controller.mm
+++ ios/chrome/browser/settings/autofill/autofill_and_passwords/ui/travel_info_table_view_controller.mm
@@ -154,6 +154,11 @@ enum SectionIdentifier {
 
 #pragma mark - SettingsRootTableViewController
 
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  self.navigationController.toolbarHidden = NO;
+}
+
 - (void)updateUIForEditState {
   [super updateUIForEditState];
   [self updatedToolbarForEditState];
