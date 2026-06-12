0cd00aa40929ef364411430b3fc060dde1e3f50b
Fiaz Muhammad <mfiaz@google.com>
2026-06-12 15:00:18 -0700

    [Utils] Migrate ShareSheetCoordinator to use CommonOnLayoutChangeListeners
    Migrate ShareSheetCoordinator to use CommonOnLayoutChangeListeners.createWidthChangedListener.
    
    Bug: 522368669
    Change-Id: I53323dae7dee50c98ac609ce8a1c9ce7e017050f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7922520
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646260}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetCoordinator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ShareSheetCoordinator.java
@@ -57,6 +57,7 @@ import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.base.WindowAndroid.ActivityStateObserver;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.util.CommonOnLayoutChangeListeners;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,10 +68,7 @@ import java.util.function.Supplier;
 // TODO(crbug.com/40106499): Should be package-protected once modularization is complete.
 @NullMarked
 public class ShareSheetCoordinator
-        implements ActivityStateObserver,
-                ChromeOptionShareCallback,
-                ConfigurationChangedObserver,
-                View.OnLayoutChangeListener {
+        implements ActivityStateObserver, ChromeOptionShareCallback, ConfigurationChangedObserver {
     private final BottomSheetController mBottomSheetController;
     private final Supplier<@Nullable Tab> mTabProvider;
     private final ShareSheetPropertyModelBuilder mPropertyModelBuilder;
@@ -103,6 +101,9 @@ public class ShareSheetCoordinator
     private final ActivityResultTracker mActivityResultTracker;
     private final MonotonicObservableSupplier<ModalDialogManager> mModalDialogManagerSupplier;
     private final SnackbarManager mSnackbarManager;
+
+    private final View.OnLayoutChangeListener mLayoutChangeListener =
+            CommonOnLayoutChangeListeners.createWidthChangedListener(this::updateBottomSheetViews);
 
     /**
      * Constructs a new ShareSheetCoordinator.
@@ -155,11 +156,11 @@ public class ShareSheetCoordinator
                         if (bottomSheet == mBottomSheet) {
                             mBottomSheet
                                     .getContentView()
-                                    .addOnLayoutChangeListener(ShareSheetCoordinator.this);
+                                    .addOnLayoutChangeListener(mLayoutChangeListener);
                         } else {
                             mBottomSheet
                                     .getContentView()
-                                    .removeOnLayoutChangeListener(ShareSheetCoordinator.this);
+                                    .removeOnLayoutChangeListener(mLayoutChangeListener);
                         }
                     }
                 };
@@ -525,21 +526,8 @@ public class ShareSheetCoordinator
         mBottomSheetController.requestShowContent(mBottomSheet, /* animate= */ false);
     }
 
-    // View.OnLayoutChangeListener
-    @Override
-    public void onLayoutChange(
-            View v,
-            int left,
-            int top,
-            int right,
-            int bottom,
-            int oldLeft,
-            int oldTop,
-            int oldRight,
-            int oldBottom) {
-        if ((oldRight - oldLeft) == (right - left)) {
-            return;
-        }
+    private void updateBottomSheetViews() {
+        if (mBottomSheet == null) return;
         mBottomSheet.getFirstPartyView().invalidate();
         ViewUtils.requestLayout(
                 mBottomSheet.getFirstPartyView(),
