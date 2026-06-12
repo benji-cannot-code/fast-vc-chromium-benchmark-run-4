0f94433e3024855e83930a4f3fbb27d63b33cdde
dpapad <dpapad@chromium.org>
2026-06-11 18:12:00 -0700

    Settings: Implement framework-agnostic alternative prefs mechanism.
     - Introduce new class PrefService as an alternative to the global
       'prefs' object.
     - Introduce PrefServiceObserverMixin (Polymer+Lit versions) to be used
       by UI elements that that need to observe prefs changes through the
       new mechanism.
    
    This is in preparation of unblocking Polymer->Lit migration for
    Settings. The current 'prefs' mechanism relies on Polymer's subproperty
    observation and 2-way bindings which have no equivalent in Lit.
    
    PrefService is framework agnostic and can be used with either Polymer or
    Lit. It is also a simpler API to use and an improvement over the global
    'prefs' object.
    
    Bug: 393471368
    Change-Id: I4206e6b7ea934da68fe9da8141af158ebebf2ee9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7898342
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645733}

---
+++ chrome/browser/resources/settings_shared/prefs2/prefs_browser_proxy.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+export class PrefsBrowserProxy implements PrefsBrowserProxy {
+  getAllPrefs() {
+    return chrome.settingsPrivate.getAllPrefs();
+  }
+
+  getPref(name: string) {
+    return chrome.settingsPrivate.getPref(name);
+  }
+
+  setPref(name: string, value: unknown, pageId?: string) {
+    return chrome.settingsPrivate.setPref(name, value, pageId);
+  }
+
+  get onPrefsChanged() {
+    return chrome.settingsPrivate.onPrefsChanged;
+  }
+
+  static getInstance(): PrefsBrowserProxy {
+    return instance || (instance = new PrefsBrowserProxy());
+  }
+
+  static setInstance(obj: PrefsBrowserProxy) {
+    instance = obj;
+  }
+}
+
+let instance: PrefsBrowserProxy|null = null;
---
+++ chrome/test/data/webui/settings/test_prefs_browser_proxy.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {PrefsBrowserProxy} from 'chrome://settings/settings.js';
+import {FakeSettingsPrivate} from 'chrome://webui-test/fake_settings_private.js';
+
+export class TestPrefsBrowserProxy implements PrefsBrowserProxy {
+  fakeApi: FakeSettingsPrivate;
+
+  constructor(initialPrefs: chrome.settingsPrivate.PrefObject[]) {
+    this.fakeApi = new FakeSettingsPrivate(initialPrefs);
+  }
+
+  getAllPrefs() {
+    return this.fakeApi.getAllPrefs();
+  }
+
+  getPref(name: string) {
+    return this.fakeApi.getPref(name);
+  }
+
+  setPref(name: string, value: unknown, pageId?: string) {
+    return this.fakeApi.setPref(name, value, pageId);
+  }
+
+  get onPrefsChanged() {
+    return this.fakeApi.onPrefsChanged as unknown as
+        typeof chrome.settingsPrivate.onPrefsChanged;
+  }
+}
