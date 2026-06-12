dcdad5d85a440e82f74647856746f3c176cd0ea5
Andrea Gomez <andreaxg@google.com>
2026-06-12 16:16:13 -0700

    Move actuation check to OnPromptRetrieved
    Change is behind the kApiGlicAccessFromPromotionPage flag.
    We should only require actuation being enabled if there is a prompt
    request.
    
    Adds tests.
    
    Bug: 516850705
    Change-Id: I6cf13280ae90d66fa03e087b66070ebd034357b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7932320
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646305}

--- chrome/browser/extensions/api/glic_private/glic_private_api.cc
+++ chrome/browser/extensions/api/glic_private/glic_private_api.cc
@@ -419,11 +419,6 @@ ExtensionFunction::ResponseAction GlicPrivateInvokeFunction::Run() {
     return RespondNow(GetPromptResponseValueAndLog(
         extensions::api::glic_private::ErrorCode::kLocalGlicNotReady));
   }
-  if (!profile_state.actuation_allowed) {
-    return RespondNow(
-        GetPromptResponseValueAndLog(extensions::api::glic_private::ErrorCode::
-                                         kLocalGlicActuationNotAllowed));
-  }
   if (extensions_features::kGlicRequireConsentForInvokeParam.Get() &&
       !profile_state.is_enabled_and_consented) {
     return RespondNow(
@@ -487,6 +482,11 @@ ExtensionFunction::ResponseAction GlicPrivateInvokeFunction::Run() {
       return RespondNow(GetPromptResponseValueAndLog(
           extensions::api::glic_private::ErrorCode::kLocalMissingPromptId));
     }
+  }
+  if (!profile_state.actuation_allowed) {
+    return RespondNow(
+        GetPromptResponseValueAndLog(extensions::api::glic_private::ErrorCode::
+                                         kLocalGlicActuationNotAllowed));
   }
 
   GetPromptFromId(
