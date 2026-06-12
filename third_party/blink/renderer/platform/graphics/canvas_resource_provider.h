810a22ba60f4aa484d05bb3b022f9429765da826
Colin Blundell <blundell@chromium.org>
2026-06-12 02:58:09 -0700

    [Blink] Move CRP::Flush() into subclasses
    Next step on making CanvasResourceProvider into pure interface.
    
    Bug: 352263194
    Change-Id: Ie8cc54ad0a1054e3183b9e953776b21eb7a3195b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7920451
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645799}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -156,7 +156,8 @@ class PLATFORM_EXPORT CanvasResourceProvider
   virtual Delegate* GetDelegate() const { return nullptr; }
 
   MemoryManagedPaintCanvas& GetCanvasForTesting();
-  std::optional<cc::PaintRecord> Flush(FlushReason = FlushReason::kOther);
+  virtual std::optional<cc::PaintRecord> Flush(
+      FlushReason = FlushReason::kOther) = 0;
   virtual ScopedRasterTimer CreateScopedRasterTimer();
 
   virtual bool IsAccelerated() const = 0;
@@ -275,6 +276,8 @@ class PLATFORM_EXPORT Canvas2DResourceProviderBitmap
   }
   scoped_refptr<StaticBitmapImage> Snapshot(
       ImageOrientation = ImageOrientationEnum::kDefault) override;
+  std::optional<cc::PaintRecord> Flush(
+      FlushReason = FlushReason::kOther) override;
 
   void RasterRecord(cc::PaintRecord last_recording) override;
   bool WritePixels(const SkImageInfo& orig_info,
@@ -499,6 +502,8 @@ class PLATFORM_EXPORT Canvas2DResourceProviderSharedImage
   }
   scoped_refptr<StaticBitmapImage> Snapshot(
       ImageOrientation = ImageOrientationEnum::kDefault) override;
+  std::optional<cc::PaintRecord> Flush(
+      FlushReason = FlushReason::kOther) override;
   bool WritePixels(const SkImageInfo& orig_info,
                    const void* pixels,
                    size_t row_bytes,
