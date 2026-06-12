45b32429b42b5c1aef61d89378e1b3bb3a00f413
Sky Malice <skym@chromium.org>
2026-06-12 12:35:46 -0700

    [AIM] Stop showing bookmark button on NTP.
    Bug: 522867791
    Change-Id: I00d3b7423f178bb0c495d43e06f4b333687e30bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7932579
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646177}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -2101,6 +2101,10 @@ class LocationBarMediator
 
     @VisibleForTesting
     boolean shouldShowBookmarkButton() {
+        if (UrlUtilities.isNtpUrl(mLocationBarDataProvider.getCurrentGurl())) {
+            return false;
+        }
+
         return mShouldShowButtonsWhenUnfocused && shouldShowPageActionButtons();
     }
 
