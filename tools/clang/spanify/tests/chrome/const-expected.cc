975c553d77be9cee0620642e896671845ae38ad6
Daniel Angulo <angdaniel@google.com>
2026-06-12 04:30:40 -0700

    spanify: Improve const handling
    Improve const qualifier handling by checking if the type location
    already starts with 'const' before attempting to search for it
    lexically, preventing potential misbehavior or double-consting.
    
    Bug: 501143858
    Change-Id: I609a8d45b988a5af8d26a37f677106351e1bcba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833109
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Daniel Angulo <angdaniel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645846}

---
+++ tools/clang/spanify/tests/chrome/const-expected.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "base/containers/span.h"
+
+namespace function_params_and_return {
+
+base::span<int const> get_buf_east();
+
+base::span<int const> get_buf_east() {
+  static std::vector<int> buf;
+  return buf;
+}
+
+void f_east() {
+  base::span<const int> buf = get_buf_east();
+  (void)buf[0];
+}
+
+}  // namespace function_params_and_return
