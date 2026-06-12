0bf878b1f568d24115bfd120cf52914d4c12b9ec
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-06-12 09:46:20 -0700

    Move placeholder_client out of the CanvasResourceDispatcher
    It's really part of the OffscreenCanvas itself, so move it there. No
    functional changes, the life time matches CRD.
    
    In a follow-up we should just create it once when we become thread
    bound.
    
    Change-Id: I0cc255e34753ec4d7e7fb90d6392a953f5348d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926401
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646039}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
@@ -144,7 +144,10 @@ class CORE_EXPORT OffscreenCanvas final
   using CanvasRenderingContextHost::DidDraw;
   bool ShouldAccelerate2dContext() const override;
   CanvasResourceDispatcher* GetOrCreateResourceDispatcher() override;
-  void DiscardResourceDispatcher() override { frame_dispatcher_ = nullptr; }
+  void DiscardResourceDispatcher() override {
+    placeholder_client_ = nullptr;
+    frame_dispatcher_ = nullptr;
+  }
   UkmParameters GetUkmParameters() override;
   bool IsWebGL1Enabled() const override { return true; }
   bool IsWebGL2Enabled() const override { return true; }
@@ -271,6 +274,7 @@ class CORE_EXPORT OffscreenCanvas final
   bool disable_reading_from_canvas_ = false;
 
   std::unique_ptr<CanvasResourceDispatcher> frame_dispatcher_;
+  std::unique_ptr<OffscreenCanvasPlaceholder::Client> placeholder_client_;
 
   // Rect is in a canvas's space (i.e Size() is a full rect and not in a
   // CanvasResource space).
@@ -291,7 +295,6 @@ class CORE_EXPORT OffscreenCanvas final
   // then the following members would remain as initialized zero values.
   uint32_t client_id_ = 0;
   uint32_t sink_id_ = 0;
-
 };
 
 }  // namespace blink
