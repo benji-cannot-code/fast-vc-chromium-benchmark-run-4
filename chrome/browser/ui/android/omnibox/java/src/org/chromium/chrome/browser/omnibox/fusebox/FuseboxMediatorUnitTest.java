a89263d7e3dea97dfed019343c41411606c0fa93
Sky Malice <skym@chromium.org>
2026-06-11 17:20:11 -0700

    [AIM] Only use ALWAYS_VISIBLE_WIDE in AI Mode.
    Bug: 522320191
    Change-Id: I1e546a03ddef6bf0710807729adc751451ce55f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921232
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645711}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediatorUnitTest.java
@@ -511,7 +511,8 @@ public class FuseboxMediatorUnitTest {
     }
 
     @Test
-    public void plusButtonBackground_popoverLayout_isAlwaysVisibleWide() {
+    public void plusButtonBackground_aiModePopover() {
+        mInput.setRequestType(AutocompleteRequestType.AI_MODE);
         mModel.set(FuseboxProperties.FUSEBOX_LAYOUT_MODE, FuseboxLayoutMode.SUGGESTIONS_POPOVER);
         recreateMediator();
 
@@ -521,7 +522,19 @@ public class FuseboxMediatorUnitTest {
     }
 
     @Test
-    public void plusButtonBackground_toolbarLayout_isInteractOnlySmall() {
+    public void plusButtonBackground_imageGenPopover() {
+        mInput.setRequestType(AutocompleteRequestType.IMAGE_GENERATION);
+        mModel.set(FuseboxProperties.FUSEBOX_LAYOUT_MODE, FuseboxLayoutMode.SUGGESTIONS_POPOVER);
+        recreateMediator();
+
+        assertEquals(
+                BackgroundStyle.INTERACT_ONLY_SMALL,
+                mModel.get(FuseboxProperties.PLUS_BUTTON_BACKGROUND_STYLE).intValue());
+    }
+
+    @Test
+    public void plusButtonBackground_aiModeToolbar() {
+        mInput.setRequestType(AutocompleteRequestType.AI_MODE);
         mModel.set(FuseboxProperties.FUSEBOX_LAYOUT_MODE, FuseboxLayoutMode.TOOLBAR);
         recreateMediator();
 
