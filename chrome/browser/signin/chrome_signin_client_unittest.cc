9c85f032576b9a9f95ad5683854c709ba7cbca62
Josef Raska <josefraska@google.com>
2026-06-12 03:35:28 -0700

    [HoT][Android][Signin] Keep user on same screen after sign-in
    https://screencast.googleplex.com/cast/NjM5NDk2Nzk5MDczMDc1MnxjMjIzMTc1Yi1hNg
    
    Update the sign-in promo flow on the Autofill and passwords settings
    screen by:
    
    1. Unwanted Reload: Updates the flow to keep the user smoothly on the
    active setting fragment.
    
    2. Undo Crash: Adds
    `SignoutReason.USER_TAPPED_UNDO_RIGHT_AFTER_SIGN_IN_FROM_AUTOFILL_AND_PASSWORDS`
    to the permitted immediate-reversal list in `SignOutCoordinator` to fix
    an `IllegalArgumentException` crash when tapping "Undo" in the
    completion snackbar.
    
    Bug: 522261090
    Change-Id: I3ae4c5b61116de404dee60d69ec86530e2960584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914727
    Reviewed-by: Sophie Crowley <crowleso@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Commit-Queue: Josef Raska <josefraska@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645821}

--- chrome/browser/signin/chrome_signin_client_unittest.cc
+++ chrome/browser/signin/chrome_signin_client_unittest.cc
@@ -189,6 +189,8 @@ bool IsAlwaysAllowedSignoutSources(
     case signin_metrics::ProfileSignout::
         kUserTappedUndoRightAfterSignInFromRecentTabs:
     case signin_metrics::ProfileSignout::
+        kUserTappedUndoRightAfterSignInFromAutofillAndPasswords:
+    case signin_metrics::ProfileSignout::
         kUserDeclinedHistorySyncAfterDedicatedSignIn:
     case signin_metrics::ProfileSignout::kDeviceLockRemovedOnAutomotive:
     case signin_metrics::ProfileSignout::kRevokeSyncFromSettings:
@@ -315,6 +317,8 @@ const signin_metrics::ProfileSignout kSignoutSources[] = {
     signin_metrics::ProfileSignout::kUserDeclinedEnterpriseManagementDisclaimer,
     signin_metrics::ProfileSignout::kForcedDiceMigration,
     signin_metrics::ProfileSignout::kSignoutFromCanSignInToChromeCapability,
+    signin_metrics::ProfileSignout::
+        kUserTappedUndoRightAfterSignInFromAutofillAndPasswords,
 };
 
 // kNumberOfObsoleteSignoutSources should be updated when a ProfileSignout
