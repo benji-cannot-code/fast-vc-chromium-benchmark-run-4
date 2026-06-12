3b6b6b1b04593de78e8b79cb446e9ef35d0db90d
Andrew Paseltiner <apaseltiner@chromium.org>
2026-06-12 07:53:19 -0700

    Use JSONReader in DigitalAssetLinksHandler
    Since base::JSONReader is now safe for the rule of 2, it can be used
    directly and synchronously in the browser process. This allows for a
    cleaner implementation by inlining the parsing logic and removing the
    need for a separate asynchronous callback and WeakPtrFactory.
    
    This change also updates the unit tests to remove the now-unnecessary
    InProcessDataDecoder.
    
    Bug: 459848478
    Change-Id: I5db1b57cfa8f27af20f4126039434bcd5cffb45e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895001
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645958}

--- components/content_relationship_verification/digital_asset_links_handler.h
+++ components/content_relationship_verification/digital_asset_links_handler.h
@@ -10,10 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <set>
 #include <string>
+#include <vector>
 
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
-#include "services/data_decoder/public/cpp/data_decoder.h"
 
 namespace content {
 class WebContents;
@@ -109,14 +109,6 @@ class DigitalAssetLinksHandler {
       std::map<std::string, std::set<std::string>> target_values,
       RelationshipCheckResultCallback callback,
       std::optional<std::string> response_body);
-
-  // Callback for the DataDecoder.
-  void OnJSONParseResult(
-      std::string relationship,
-      std::optional<std::vector<std::string>> fingerprints,
-      std::map<std::string, std::set<std::string>> target_values,
-      RelationshipCheckResultCallback callback,
-      data_decoder::DataDecoder::ValueOrError result);
 
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
 
--- components/content_relationship_verification/digital_asset_links_handler_unittest.cc
+++ components/content_relationship_verification/digital_asset_links_handler_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_status_code.h"
-#include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/test/test_url_loader_factory.h"
@@ -148,7 +147,6 @@ class DigitalAssetLinksHandlerTest : public ::testing::Test {
   }
 
   content::BrowserTaskEnvironment task_environment_;
-  data_decoder::test::InProcessDataDecoder in_process_data_decoder_;
   network::TestURLLoaderFactory test_url_loader_factory_;
 };
 
