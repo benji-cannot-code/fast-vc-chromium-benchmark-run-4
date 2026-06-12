4bab141789a3100c62b702fda983e4d358df2aab
Dale Curtis <dalecurtis@chromium.org>
2026-06-11 17:09:03 -0700

    Port AudioStreamHandler and SoundsManager tests to FakeOutputStream
    Remove AudioStreamHandler::TestObserver and g_observer_for_testing.
    Port audio_stream_handler_unittest.cc and sounds_manager_unittest.cc to
    use FakeOutputStream instead of TestObserver.
    
    Bug: 512418731
    Change-Id: I833aea8dda5334444c7e76d8aae756134f826958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7914836
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1645704}

--- services/audio/public/cpp/sounds/audio_stream_handler.cc
+++ services/audio/public/cpp/sounds/audio_stream_handler.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/synchronization/lock.h"
 #include "base/task/sequenced_task_runner.h"
-#include "base/task/single_thread_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
@@ -44,8 +43,6 @@ const double kOutputVolumePercent = 0.8;
 
 // Keep alive timeout for audio stream.
 const int kKeepAliveMs = 1500;
-
-AudioStreamHandler::TestObserver* g_observer_for_testing = nullptr;
 
 }  // namespace
 
@@ -73,21 +70,17 @@ class AudioStreamHandler::AudioStreamContainer
     DCHECK(task_runner_->RunsTasksInCurrentSequence());
 
     // Create OutputDevice if it is the first time playing.
-    if (device_ == nullptr) {
+    if (!device_) {
       const media::AudioParameters params(
           media::AudioParameters::AUDIO_PCM_LOW_LATENCY,
           media::ChannelLayoutConfig::Guess(audio_handler_->GetNumChannels()),
           audio_handler_->GetSampleRate(),
           media::AudioHandler::kDefaultFrameCount);
-      if (g_observer_for_testing) {
-        g_observer_for_testing->Initialize(this, params);
-      } else {
-        mojo::PendingRemote<media::mojom::AudioStreamFactory> stream_factory;
-        stream_factory_binder_.Run(
-            stream_factory.InitWithNewPipeAndPassReceiver());
-        device_ = std::make_unique<audio::OutputDevice>(
-            std::move(stream_factory), params, this, std::string());
-      }
+      mojo::PendingRemote<media::mojom::AudioStreamFactory> stream_factory;
+      stream_factory_binder_.Run(
+          stream_factory.InitWithNewPipeAndPassReceiver());
+      device_ = std::make_unique<audio::OutputDevice>(
+          std::move(stream_factory), params, this, std::string());
     }
 
     {
@@ -103,9 +96,7 @@ class AudioStreamHandler::AudioStreamContainer
         }
         return;
       } else {
-        if (!g_observer_for_testing) {
-          device_->SetVolume(kOutputVolumePercent);
-        }
+        device_->SetVolume(kOutputVolumePercent);
       }
 
       if (paused_) {
@@ -116,11 +107,7 @@ class AudioStreamHandler::AudioStreamContainer
     }
 
     started_ = true;
-    if (g_observer_for_testing) {
-      g_observer_for_testing->OnPlay();
-    } else {
-      device_->Play();
-    }
+    device_->Play();
   }
 
   void Stop() {
@@ -128,11 +115,7 @@ class AudioStreamHandler::AudioStreamContainer
 
     if (started_) {
       // Do not hold the |state_lock_| while stopping the output stream.
-      if (g_observer_for_testing) {
-        g_observer_for_testing->OnStop();
-      } else {
-        device_->Pause();
-      }
+      device_->Pause();
     }
 
     started_ = false;
@@ -146,11 +129,7 @@ class AudioStreamHandler::AudioStreamContainer
 
     if (started_) {
       // Do not hold the |state_lock_| while stopping the output stream.
-      if (g_observer_for_testing) {
-        g_observer_for_testing->OnPause();
-      } else {
-        device_->Pause();
-      }
+      device_->Pause();
       paused_ = true;
       started_ = false;
       stop_closure_.Cancel();
@@ -319,11 +298,6 @@ base::TimeDelta AudioStreamHandler::duration() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(IsInitialized());
   return duration_;
-}
-
-// static
-void AudioStreamHandler::SetObserverForTesting(TestObserver* observer) {
-  g_observer_for_testing = observer;
 }
 
 }  // namespace audio
