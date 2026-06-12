fece6d9e89dbb58686951fe2c7cca8973c9bdbd9
Ananya Seelam <ananyaseelam@google.com>
2026-06-12 10:06:09 -0700

    [context-input] Adding feature flag for tooltips in the context menu
    Add feature flag for tooltips feature where tooltips will show upon
    hover of context menu items. This will be populated from PEC API.
    
    Bug: b:515155102
    Change-Id: I5dbe58b4df5fd212e76b07e4321ac09393fb808a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926278
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Wenhu Cheng <wenhuc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646055}

--- components/omnibox/common/composebox_features.cc
+++ components/omnibox/common/composebox_features.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace omnibox {
 
 BASE_FEATURE(kContextManagementInComposebox, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kContextMenuToolTips, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kTabFaviconChipsToCoins, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kContextManagementInOmnibox, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kContextMenuAnimationLimiting, base::FEATURE_DISABLED_BY_DEFAULT);
