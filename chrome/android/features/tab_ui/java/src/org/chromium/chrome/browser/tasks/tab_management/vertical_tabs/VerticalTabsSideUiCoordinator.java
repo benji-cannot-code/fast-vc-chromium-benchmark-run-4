8db684fd0a5a6ddb8861285e147cacf14073ba83
Linyu He <linyuh@google.com>
2026-06-12 06:30:07 -0700

    SideUi: Remove onWindowResized() impl for vertical tabs
    onWindowResized() is only invoked for side panel, and
    https://crbug.com/478338737 will add a new API for all SideUiContainers.
    
    To make the transition to the new API easier, this CL removes the
    onWindowResized() impl for vertical tabs (it's not working anyway).
    
    For more context, please see https://crbug.com/478338737#comment6.
    
    Include-Ci-Only-Tests: true
    Bug: 478338737
    Change-Id: I0ebb1b1bea13fc68569d115800a14f28310f9b9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926792
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1645909}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabsSideUiCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabsSideUiCoordinator.java
@@ -37,6 +37,8 @@ public class VerticalTabsSideUiCoordinator implements SideUiContainer {
     private final @Px int mViewWidth;
 
     // Whether the vertical tab is automatically hidden due to run-time conditions.
+    // TODO(crbug.com/513622986): Handle auto-hide logic when screen size goes below threshold.
+    @SuppressWarnings("UnusedVariable")
     private boolean mIsAutoHidden;
 
     // Whether the vertical tab is set to visible via UI. Remains true even if it is temporarily
@@ -112,16 +114,6 @@ public class VerticalTabsSideUiCoordinator implements SideUiContainer {
 
     @Override
     public void onContainerResized(@Px int containerWidth) {}
-
-    @Override
-    public void onWindowResized(boolean canShowSideUi) {
-        // TODO(crbug.com/513622986): Handle auto-hide logic when screen size goes below threshold.
-        // No-op if currently off or visibility hasn't changed.
-        if (!mManualVisible || (canShowSideUi != mIsAutoHidden)) return;
-
-        mIsAutoHidden = !canShowSideUi;
-        requestShow(canShowSideUi);
-    }
 
     public void destroy() {
         mTabListCoordinator.destroy();
