1502a78fd019766dc3d3527c5df65c6d1d8a99c2
yoshiki iguchi <yoshiki@google.com>
2026-06-12 19:32:48 -0700

    ASH Refactoring: Move 41 login-related strings to ash/ (Part 1)
    Move 41 login-related strings from chrome/app/chromeos_strings.grdp
    to ash/login/resources/login_strings.grd, which is newly created for
    the login-related strings.
    
    Moved strings:
    - IDS_LOGIN_APP_DOWNLOADING_SCREEN_DESCRIPTION
    - IDS_LOGIN_APP_DOWNLOADING_SCREEN_NEXT
    - IDS_LOGIN_APP_DOWNLOADING_SCREEN_TITLE
    - IDS_LOGIN_CATEGORIES_SCREEN_LOADING
    - IDS_LOGIN_CATEGORIES_SCREEN_SKIP
    - IDS_LOGIN_CATEGORIES_SCREEN_SUBTITLE
    - IDS_LOGIN_CATEGORIES_SCREEN_TITLE
    - IDS_LOGIN_FAMILY_LINK_NOTICE_SCREEN_CONTINUE_BUTTON
    - IDS_LOGIN_FAMILY_LINK_NOTICE_SCREEN_EXISTING_ACCOUNT_SUBTITLE
    - IDS_LOGIN_FAMILY_LINK_NOTICE_SCREEN_NEW_ACCOUNT_SUBTITLE
    - IDS_LOGIN_FAMILY_LINK_NOTICE_SCREEN_NOT_ELIGIBLE_SUBTITLE
    - IDS_LOGIN_FAMILY_LINK_NOTICE_SCREEN_NOT_ELIGIBLE_TITLE
    - IDS_LOGIN_FAMILY_LINK_NOTICE_SCREEN_TITLE
    - IDS_LOGIN_PARENTAL_HANDOFF_SCREEN_NEXT_BUTTON
    - IDS_LOGIN_PARENTAL_HANDOFF_SCREEN_SUBTITLE
    - IDS_LOGIN_PARENTAL_HANDOFF_SCREEN_TITLE
    - IDS_LOGIN_PERKS_SCREEN_LOADING
    - IDS_LOGIN_PERSONALIZED_RECOMMEND_APPS_SCREEN_SCREEN_LOADING
    - IDS_LOGIN_PERSONALIZED_RECOMMEND_APPS_SCREEN_SCREEN_SKIP
    - IDS_LOGIN_PERSONALIZED_RECOMMEND_APPS_SCREEN_SCREEN_SUBTITLE
    - IDS_LOGIN_PERSONALIZED_RECOMMEND_APPS_SCREEN_SCREEN_TITLE
    - IDS_LOGIN_RECOMMEND_APPS_INSTALL
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_CONTAINS_ADS
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_DESCRIPTION
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_DESCRIPTION_EXPAND_BUTTON
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_IN_APP_PURCHASES
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_LOADING
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_TITLE
    - IDS_LOGIN_RECOMMEND_APPS_SCREEN_WAS_INSTALLED
    - IDS_LOGIN_RECOMMEND_APPS_SELECT_ALL
    - IDS_LOGIN_RECOMMEND_APPS_SKIP
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_ACCEPT_AND_CONTINUE
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_CHROME_BROWSER_SYNC_DESCRIPTION
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_CHROME_BROWSER_SYNC_NAME_2
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_CHROME_BROWSER_SYNC_NAME_3
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_DECLINE2
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_OS_SYNC_NAME_2
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_REVIEW_SYNC_OPTIONS_LATER
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_SUBTITLE_2
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_TITLE_WITH_DEVICE
    - IDS_LOGIN_SYNC_CONSENT_SCREEN_TURN_ON_SYNC
    
    This CL also moves 38 string image files from
    chrome/app/chromeos_strings_grdp to ash/login/resources/login_strings_grd.
    
    Added *.xtb files are placeholders, as mentioned in the doc:
    https://www.chromium.org/developers/design-documents/ui-localization/
    
    Bug: 487122295
    Test: CQ passes
    Skip-Translation-Screenshots-Check: True
    Change-Id: Ia0426e00750032996946d8ea67b7e339ffd1a812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7917193
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646361}

--- chrome/browser/ui/webui/ash/login/parental_handoff_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/parental_handoff_screen_handler.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/login/parental_handoff_screen_handler.h"
 
+#include "ash/login/resources/grit/ash_login_strings.h"
 #include "chrome/browser/ash/login/screens/parental_handoff_screen.h"
-#include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
 #include "ui/base/l10n/l10n_util.h"
 
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -1604,6 +1604,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   "ash/login/resources/login_resources.grd": {
     "structures": [9140],
   },
+  "ash/login/resources/login_strings.grd": {
+    "messages": [9150],
+  },
   "ash/public/cpp/resources/ash_public_unscaled_resources.grd": {
     "includes": [9160],
     "structures": [9180],
