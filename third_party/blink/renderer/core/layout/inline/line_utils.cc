6eeb0baa3425fb0e73c78938225329e3531af3ee
Kent Tamura <tkent@chromium.org>
2026-06-15 07:06:47 -0700

    editing: Fix caret navigation issues around ruby elements
    This CL fixes a bug where caret navigation (move forward/backward line)
    would incorrectly enter the ruby text (rt) when there is a ruby element.
    NGContainingLineBoxOf() is modified to return the base line instead of
    the ruby annotation line.
    
    Note that this CL doesn't address caret navigation starting inside a
    <ruby>.
    
    Bug: 502468279
    Change-Id: Ifdb2ca35a0f934e9678444357c13569914eaf5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7940388
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646779}

--- third_party/blink/renderer/core/layout/inline/line_utils.cc
+++ third_party/blink/renderer/core/layout/inline/line_utils.cc
@@ -17,6 +17,10 @@ InlineCursor NGContainingLineBoxOf(const PositionWithAffinity& position) {
     return InlineCursor();
   InlineCursor line = caret_position.cursor;
   line.MoveToContainingLine();
+  if (RuntimeEnabledFeatures::EditingLineMovementWithRubyFixEnabled() && line &&
+      line.Current().IsRubyAnnotationLine()) {
+    line.MoveToPreviousLine();
+  }
   return line;
 }
 
