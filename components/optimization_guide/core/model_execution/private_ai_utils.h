ae6e4aab816cb089a3df393e371111f5cb0fbf82
Yerassyl Kutylbek <kutylbeky@google.com>
2026-06-15 03:54:16 -0700

    Move PrivateAiModelExecutionFetcher to components.
    This change relocates the PrivateAiModelExecutionFetcher implementation
    from //chrome/browser/optimization_guide to
    //components/optimization_guide/core/model_execution. This is done to
    allow the fetcher to be used by both Desktop and iOS platforms. The iOS
    delegate will be added in a later CL with private ai support for iOS.
    
    Bug: b:519494980
    Bypass-Check-License: Moved file
    Change-Id: Ia4dd873f3ca4daaef56d5c8e2338063a4280e1ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7907586
    Commit-Queue: Yerassyl Kutylbek <kutylbeky@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646705}

--- components/optimization_guide/core/model_execution/private_ai_utils.h
+++ components/optimization_guide/core/model_execution/private_ai_utils.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PRIVATE_AI_PRIVATE_AI_UTILS_H_
-#define CHROME_BROWSER_PRIVATE_AI_PRIVATE_AI_UTILS_H_
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_PRIVATE_AI_UTILS_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_PRIVATE_AI_UTILS_H_
 
 #include "components/optimization_guide/proto/model_execution.pb.h"
 #include "components/private_ai/proto/private_ai.pb.h"
@@ -22,4 +22,4 @@ void PopulatePaicMessage(
 
 }  // namespace private_ai
 
-#endif  // CHROME_BROWSER_PRIVATE_AI_PRIVATE_AI_UTILS_H_
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_PRIVATE_AI_UTILS_H_
