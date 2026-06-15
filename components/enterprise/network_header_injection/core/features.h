c5684219934b9f57d40e6b4c0e82bebcfe823be2
Anatoli Hancharou <antoli@google.com>
2026-06-15 07:16:18 -0700

    [Enterprise] Add kHttpHeadersInjection feature flag
    Introduces `kHttpHeadersInjection` as a base::Feature flag, disabled by
    default, to control the HTTP Header Injection feature.
    Also guards the `HttpHeaderInjectionServiceFactory` instantiation in
    `ChromeBrowserMainExtraPartsProfiles` with this new flag.
    
    Bug: 265314050
    Change-Id: I8efe7cfe767de0bbcd89e732233220715b492d39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7927992
    Reviewed-by: Alex Valadkevich <valadkevich@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Anatoli Hancharou <antoli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646784}

---
+++ components/enterprise/network_header_injection/core/features.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_NETWORK_HEADER_INJECTION_CORE_FEATURES_H_
+#define COMPONENTS_ENTERPRISE_NETWORK_HEADER_INJECTION_CORE_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace enterprise_custom_headers {
+
+// Controls enabling the HTTP Header Injection feature.
+// This acts as a killswitch.
+BASE_DECLARE_FEATURE(kHttpHeadersInjection);
+
+bool IsHttpHeaderInjectionEnabled();
+
+}  // namespace enterprise_custom_headers
+
+#endif  // COMPONENTS_ENTERPRISE_NETWORK_HEADER_INJECTION_CORE_FEATURES_H_
