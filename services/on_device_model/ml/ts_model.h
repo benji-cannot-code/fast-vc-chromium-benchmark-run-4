915165dbf2ede758be1d3d4946dc34cc434d4973
Jan Keitel <jkeitel@google.com>
2026-06-15 00:02:28 -0700

    [services] Eliminate base::MemoryMappedFile::data() and length() usage
    This CL replaces unsafe usages of base::MemoryMappedFile::data() and
    length() in the //services directory with the buffer-safe bytes()
    method.
    
    TAG=agy
    CONV=7215b49b-7941-43a0-929d-9b66c03bb4ed
    
    Bug: 435317390
    Change-Id: Ief8c64831a5ba8efb87655f3e77904c385d310fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7932846
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646606}

--- services/on_device_model/ml/ts_model.h
+++ services/on_device_model/ml/ts_model.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_ON_DEVICE_MODEL_ML_TS_MODEL_H_
 #define SERVICES_ON_DEVICE_MODEL_ML_TS_MODEL_H_
 
-#include "base/files/memory_mapped_file.h"
 #include "base/memory/raw_ref.h"
 #include "base/threading/sequence_bound.h"
 #include "components/translate/core/language_detection/language_detection_model.h"
