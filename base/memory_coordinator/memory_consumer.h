e06a6f74c230067a0b609a6558a8b9348cae87db
Andres Ricardo Perez <andresrperez@chromium.org>
2026-06-15 13:17:47 -0700

    [Memory Coordinator] Update traits to represent PassiveMemoryConsumers more accurately
    The PassiveMemoryConsumer class represents consumers that query the
    memory_limit() directly, and do not react in any way when
    OnUpdateMemoryLimit() or OnReleaseMemoryPressure() are called.
    
    Therefore, most of the MemoryConsumerTraits don't really make sense for
    them, including the required ones. This CL adds a new trait to store
    whether the consumer is active or passive, and a new constructor that
    only takes a tag to mark it as passive.
    
    A TODO is added to add runtime checks and tests that ensure passive
    memory consumers are registered with passive traits. They will be
    implemented once we migrate the existing passive consumer that already
    have traits.
    
    Bug: 489349561
    Change-Id: I82c336f40e08a9ad3bedfcd78c674398ee7ae567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904256
    Commit-Queue: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647031}

--- base/memory_coordinator/memory_consumer.h
+++ base/memory_coordinator/memory_consumer.h
@@ -101,6 +101,8 @@ class BASE_EXPORT MemoryConsumer : public CheckedObserver {
   MemoryConsumer();
   ~MemoryConsumer() override = default;
 
+  virtual bool IsPassive() const;
+
   // The memory limit, expressed as a percentage.
   int memory_limit() const { return memory_limit_; }
 
@@ -144,6 +146,7 @@ class BASE_EXPORT PassiveMemoryConsumer : public MemoryConsumer {
   // MemoryConsumer:
   void OnReleaseMemory() final {}
   void OnUpdateMemoryLimit() final {}
+  bool IsPassive() const final;
 };
 
 // Similar to ScopedObservation, registers a MemoryConsumer with the global
