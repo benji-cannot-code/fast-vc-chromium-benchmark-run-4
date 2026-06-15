731796d5f0d9df20d3a148576ac75163d1c1b314
Philip Rogers <pdr@chromium.org>
2026-06-15 08:53:30 -0700

    Cleanup NodeRareData includes, forward decls, and comments
    This patch cleans up the NodeRareData and NodeRareDataField includes
    and forward declarations. A comment above `GetFieldIndex` has also been
    updated which referenced `fields_` which no longer exists.
    
    Change-Id: Ia53a715cea492c3a477ae051aac74eaf9c96de79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7943244
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646846}

--- third_party/blink/renderer/core/layout/anchor_position_scroll_data.h
+++ third_party/blink/renderer/core/layout/anchor_position_scroll_data.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/compositor_element_id.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "ui/gfx/geometry/vector2d.h"
 
