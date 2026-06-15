132a3f8723042390e3912399482eb98c0fe200a5
Bryan Oltman <bryanoltman@google.com>
2026-06-15 09:09:48 -0700

    [macOS] Tighten "Confirm Quit" UI key down check to quit shortcut
    Updates the logic to determine whether we show the "Confirm Quit" UI to
    specifically check for the "Quit" shortcut instead of simply key down.
    
    Fixed: 523343652
    Change-Id: I2a215a93caeddc218d0dc44bde4aa1eab2e2e141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7932510
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Bryan Oltman <bryanoltman@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646859}

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_core_service_factory.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/first_run/first_run.h"
+#include "chrome/browser/global_keyboard_shortcuts_mac.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/lifetime/application_lifetime_desktop.h"
 #include "chrome/browser/lifetime/browser_shutdown.h"
@@ -1022,7 +1023,7 @@ class AppControllerProfileObserver : public ProfileAttributesStorage::Observer,
 
   NSEvent* event = [NSApp currentEvent];
   // Run only for keyboard-initiated quits.
-  if (event.type != NSEventTypeKeyDown) {
+  if (!event || CommandForKeyEvent(event).chrome_command != IDC_EXIT) {
     return ConfirmQuitResultNotPrompted;
   }
 
