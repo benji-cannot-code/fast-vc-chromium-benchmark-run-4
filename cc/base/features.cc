f012045ae81b7d4fd994282ebaf724c9d3d1114e
Robert Flack <flackr@chromium.org>
2026-06-15 15:32:25 -0700

    Enable kNewContentForCheckerboardedScrolls PerFrame
    This ensures that when new content is not needed, we don't delay
    producing composited scroll updates, while still waiting for
    a new frame when the current compositor frame would
    checkerboard.
    
    Bug: 40680264
    Change-Id: I6a2abf44ee147fe5b807b9e3fcb6b3df9538587d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7945481
    Reviewed-by: Gaston Rodriguez <gastonr@microsoft.com>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647125}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -115,7 +115,7 @@ constexpr const char kNewContentForCheckerboardedScrollsPerFrame[] =
 const base::FeatureParam<std::string> kNewContentForCheckerboardedScrollsParam(
     &kNewContentForCheckerboardedScrolls,
     "mode",
-    kNewContentForCheckerboardedScrollsPerScroll);
+    kNewContentForCheckerboardedScrollsPerFrame);
 
 BASE_FEATURE(kAllowLCDTextWithFilter, base::FEATURE_ENABLED_BY_DEFAULT);
 
