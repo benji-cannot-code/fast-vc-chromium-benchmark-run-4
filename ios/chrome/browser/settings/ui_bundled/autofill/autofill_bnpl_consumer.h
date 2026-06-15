38654aa247c7824c61f039afb46fb70e7f42572e
James Sun <jamessun@google.com>
2026-06-15 10:35:56 -0700

    [BNPL] Add AutofillBnplTableViewController skeleton for settings subpage
    This CL add the skeleton code for the settings subpage (displaying BNPL
    payment settings), including:
    - AutofillBnplTableViewController: Subpage view controller.
    - AutofillBnplConsumer: Subpage consumer protocol.
    
    The mediator and its coordinator will be added in later CLs.
    
    Bug: 517646489
    Change-Id: I1658ce0dc8b559535c3d8908006d1ccb841bb108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7913207
    Commit-Queue: James Sun <jamessun@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646917}

---
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_bnpl_consumer.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_AUTOFILL_AUTOFILL_BNPL_CONSUMER_H_
+#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_AUTOFILL_AUTOFILL_BNPL_CONSUMER_H_
+
+#import <Foundation/Foundation.h>
+
+// Consumer for AutofillBnplMediator.
+@protocol AutofillBnplConsumer <NSObject>
+@end
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_AUTOFILL_AUTOFILL_BNPL_CONSUMER_H_
