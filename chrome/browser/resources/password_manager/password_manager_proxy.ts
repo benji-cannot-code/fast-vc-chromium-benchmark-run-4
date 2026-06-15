ae2997df438a75984d92af42ac40813a9c0ad3d4
Andrii Natiahlyi <natiahlyi@google.com>
2026-06-15 05:16:45 -0700

    [Password Manager] Migrate Cloud Authenticator methods to Mojo.
    Bug: 432409279
    Change-Id: I1bfa1b6db4d72a4143e2eb629033e1e6fd42f774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7941470
    Reviewed-by: Adem Derinel <derinel@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646734}

--- chrome/browser/resources/password_manager/password_manager_proxy.ts
+++ chrome/browser/resources/password_manager/password_manager_proxy.ts
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * testing. The chrome.passwordsPrivate API is being migrated to use Mojo.
  */
 
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+
 import {PageCallbackRouter, PageHandlerFactory, PageHandlerRemote, PasswordManagerActionableError} from './password_manager.mojom-webui.js';
 import type {ActorLoginPermission} from './password_manager.mojom-webui.js';
 
@@ -792,11 +794,19 @@ export class PasswordManagerImpl implements PasswordManagerProxy {
   }
 
   disconnectCloudAuthenticator() {
-    return chrome.passwordsPrivate.disconnectCloudAuthenticator();
+    if (!loadTimeData.getBoolean('enablePasswordManagerMojoApi')) {
+      return chrome.passwordsPrivate.disconnectCloudAuthenticator();
+    }
+    return this.handler.disconnectCloudAuthenticator().then(
+        result => result.success);
   }
 
   isConnectedToCloudAuthenticator() {
-    return chrome.passwordsPrivate.isConnectedToCloudAuthenticator();
+    if (!loadTimeData.getBoolean('enablePasswordManagerMojoApi')) {
+      return chrome.passwordsPrivate.isConnectedToCloudAuthenticator();
+    }
+    return this.handler.isConnectedToCloudAuthenticator().then(
+        result => result.connected);
   }
 
   deleteAllPasswordManagerData() {
