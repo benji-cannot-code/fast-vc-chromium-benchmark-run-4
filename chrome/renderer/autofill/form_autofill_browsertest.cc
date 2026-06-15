c609a07067098d6065bc1081a782ba7e4210b1d1
Tamino Bauknecht <taminob@chromium.org>
2026-06-15 02:59:52 -0700

    autofill: Remove redundant RunUntilIdle() calls in FormAutofillTest
    Since `RenderViewTest::ExecuteJavaScriptForTests` is already synchronous
    and the asynchronous form extraction is also manually triggered
    synchronously afterwards by `UpdateFormCache`, no `RunLoop` is required.
    There should be no cross-process communication either, so the calls to
    `base::RunLoop::RunUntilIdle()` were removed.
    
    Bug: 41483772
    Change-Id: I87e79b26e2c95c979f522e1be9537c95f57cdc3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7918095
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Tamino Bauknecht <taminob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646679}

--- chrome/renderer/autofill/form_autofill_browsertest.cc
+++ chrome/renderer/autofill/form_autofill_browsertest.cc
@@ -2150,7 +2150,6 @@ TEST_F(FormAutofillTest, OnlyExtractNewForms) {
          newInput.setAttribute('id', 'telephone');
          newInput.value = '12345';
          document.getElementById('testform').appendChild(newInput);)");
-  base::RunLoop().RunUntilIdle();
 
   forms = UpdateFormCache().updated_forms;
   ASSERT_EQ(1U, forms.size());
@@ -2198,7 +2197,6 @@ TEST_F(FormAutofillTest, OnlyExtractNewForms) {
         newForm.appendChild(newLastname);
         newForm.appendChild(newEmail);
         document.body.appendChild(newForm);)");
-  base::RunLoop().RunUntilIdle();
 
   forms = UpdateFormCache().updated_forms;
   ASSERT_EQ(1U, forms.size());
