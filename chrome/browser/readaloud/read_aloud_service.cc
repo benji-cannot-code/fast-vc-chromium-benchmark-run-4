ff485b23dde87dcc7ce03c473c54c575fb5150ae
Jacobo Tapia <jatapiaro@google.com>
2026-06-15 14:19:40 -0700

    [ReadAloud] Introduce skeleton ReadAloudService and GN targets
    This CL introduces the initial skeleton class and GN build targets for
    the native C++ Read Aloud architecture on Android.
    
    As part of the multi-phase migration away from the deprecated Java-based
    Speakr service to an out-of-process C++ architecture, this change sets
    up the foundational ReadAloudService KeyedService stub and connects
    //chrome/browser/readaloud:readaloud to the browser build via
    //chrome/browser/readaloud/android:native.
    
    Subsequent CLs will introduce ReadAloudManagerFactory and wire up
    DOM distillation.
    
    Bug: 521528774
    Change-Id: If6b9e4b3d679998a0332c32656dc38ca5f974991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7909598
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Jacobo Tapia <jatapiaro@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647074}

---
+++ chrome/browser/readaloud/read_aloud_service.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/readaloud/read_aloud_service.h"
+
+#include "chrome/browser/profiles/profile.h"
+
+namespace readaloud {
+
+ReadAloudService::ReadAloudService(Profile* profile) : profile_(profile) {}
+
+ReadAloudService::~ReadAloudService() = default;
+
+}  // namespace readaloud
