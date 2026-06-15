241fd49277500c87a3ddbaa46da2ea006846a182
Dewita Sonya <dsonya@google.com>
2026-06-14 22:33:35 -0700

    [Cast Receiver] Implement StreamingReceiverChannel
    Introduces the StreamingReceiverChannel class to handle/receiving sending input events over a Cast V2 message port.
    
    BUG=501522425
    TEST=autoninja -C out/Default cast_receiver_unittests && out/Default/cast_receiver_unittests
    TAG=agy
    CONV=58e6f4e5-95a8-4c96-81a8-7f1cbc94e5aa
    
    Change-Id: I8ddb0585ea5446e76fd51bf0432a57dcd51ed259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904984
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Commit-Queue: Dewita Sonya <dsonya@google.com>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1646588}

---
+++ components/cast_receiver/browser/streaming_receiver_channel.cc
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cast_receiver/browser/streaming_receiver_channel.h"
+
+#include <string>
+
+#include "base/check.h"
+#include "base/logging.h"
+#include "components/cast_receiver/proto/input_event.pb.h"
+
+namespace cast_receiver {
+
+StreamingReceiverChannel::StreamingReceiverChannel(
+    std::unique_ptr<cast_api_bindings::MessagePort> message_port)
+    : message_port_(std::move(message_port)) {
+  CHECK(message_port_);
+  message_port_->SetReceiver(this);
+}
+
+StreamingReceiverChannel::~StreamingReceiverChannel() {
+  if (message_port_) {
+    message_port_->Close();
+  }
+}
+
+void StreamingReceiverChannel::SendInputEvent(const InputEvent& event) {
+  if (!message_port_ || !message_port_->CanPostMessage()) {
+    LOG(WARNING) << "Cannot send message, port is closed or invalid.";
+    return;
+  }
+
+  std::string serialized;
+  if (!event.SerializeToString(&serialized)) {
+    LOG(ERROR) << "Failed to serialize InputEvent.";
+    return;
+  }
+
+  message_port_->PostMessage(serialized);
+}
+
+bool StreamingReceiverChannel::OnMessage(
+    std::string_view message,
+    std::vector<std::unique_ptr<cast_api_bindings::MessagePort>> ports) {
+  // TODO(b/501522411): Handle messages received from the sender.
+  return true;
+}
+
+void StreamingReceiverChannel::OnPipeError() {
+  LOG(WARNING) << "Streaming Receiver Channel pipe error.";
+}
+
+}  // namespace cast_receiver
