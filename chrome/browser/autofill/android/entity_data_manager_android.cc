221c4747012d721c69f05efb76787a90c485d58b
Oleksii Kalinin <kalinino@google.com>
2026-06-15 04:03:50 -0700

    Implement Personal Context settings toggle in Autofill on Android.
    Connects the JNI methods for Personal Context visibility and enablement
    to the underlying preferences and utility functions. Adds unit tests for
    the new JNI calls in Java.
    
    Fixed: b:517066061
    Change-Id: I22f5aee7abe530da2d1a8efcacaf0eb7657d3af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894236
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Oleksii Kalinin <kalinino@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646711}

--- chrome/browser/autofill/android/entity_data_manager_android.cc
+++ chrome/browser/autofill/android/entity_data_manager_android.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/consent_auditor/consent_auditor.h"
 #include "components/personal_context/core/personal_context_enablement_service.h"
+#include "components/personal_context/core/personal_context_prefs.h"
 #include "components/personal_context/core/personal_context_types.h"
 #include "components/personal_context/core/url_constants.h"
 #include "components/wallet/core/common/wallet_features.h"
@@ -96,13 +97,15 @@ bool EntityDataManagerAndroid::IsPersonalContextPreferenceVisible(JNIEnv* env) {
 }
 
 bool EntityDataManagerAndroid::IsPersonalContextEnabled(JNIEnv* env) {
-  // TODO(b/517066061): Implement pref and read value.
-  return true;
+  return prefs_->GetBoolean(
+      personal_context::prefs::kPersonalContextInAutofillSettingsToggleStatus);
 }
 
 void EntityDataManagerAndroid::SetPersonalContextEnabled(JNIEnv* env,
                                                          bool enabled) {
-  // TODO(b/517066061): Implement pref and write value.
+  prefs_->SetBoolean(
+      personal_context::prefs::kPersonalContextInAutofillSettingsToggleStatus,
+      enabled);
 }
 
 static std::string
