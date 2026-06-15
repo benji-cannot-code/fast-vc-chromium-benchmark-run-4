f4f23efaaa2278ee15a9fae597bb2a7cc4d0a4d1
komal <sambranikar@google.com>
2026-06-14 23:35:30 -0700

    ozone/evdev: Remove expired FastTouchpadClick feature
    The `enable-fast-touchpad-click` flag expired in M140. As the
    experimental behavior is not being retained, this CL removes the flag
    and deletes the associated logic to disable the feature entirely.
    
    Specific changes include:
    - Removing the flag from chrome://flags (about_flags.cc).
    - Cleaning up flag-metadata.json and fieldtrial_testing_config.json.
    - Deleting the kEnableFastTouchpadClick feature definition in features.cc.
    
    BUG=b:427495398
    TEST=ozone_unittests, events_unittests
    
    Change-Id: Ib8ee8f28ab6f500b1318f6e5b63648f50b44af18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874233
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Reviewed-by: Kenneth Albanowski <kenalba@google.com>
    Commit-Queue: Komal Sambranikar (xWF) <sambranikar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646597}

--- ui/events/ozone/features.h
+++ ui/events/ozone/features.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 
 namespace ui {
-COMPONENT_EXPORT(EVENTS_OZONE)
-BASE_DECLARE_FEATURE(kEnableFastTouchpadClick);
 
 COMPONENT_EXPORT(EVENTS_OZONE)
 BASE_DECLARE_FEATURE(kEnableHeatmapPalmDetection);
