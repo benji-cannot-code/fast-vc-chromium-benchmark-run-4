e081471349dde803a7e4774631ac185fad9a033f
Christoph Schwering <schwering@google.com>
2026-06-15 08:12:55 -0700

    [Autofill] Tiny cleanups around PCAM
    Bug: 40100455
    Change-Id: If00d6b4650135f114ec3fea892f36166a0b82aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7940353
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1646818}

--- components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.cc
+++ components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.cc
@@ -223,7 +223,6 @@ void EntityDataManager::OnHistoryDeletions(
   }
 }
 
-
 void EntityDataManager::RecordEntityUsed(const EntityInstance::EntityId& guid,
                                          base::Time use_date) {
   base::optional_ref<EntityInstance> entity = GetMutableEntityInstance(guid);
