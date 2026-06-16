da7eb2a2344ae5d64928578ecbf57a0265edfdc0
Xiaohui Chen <xiaohuic@google.com>
2026-06-16 10:11:08 -0700

    Remove deprecated data migration component.
    This CL removes the deprecated data migration component and its associated
    factory and feature flag as it is no longer used.
    
    BUG=513140774
    TAG=agy
    CONV=f9b51640-b8fa-4591-a0db-0520dd21407b
    
    Change-Id: I455d244c1f6cf6c8284c900fa4fdb9aae5f3998d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7948185
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647681}

--- chrome/browser/ash/keyed_service/browser_context_keyed_service_factories.cc
+++ chrome/browser/ash/keyed_service/browser_context_keyed_service_factories.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/crostini_port_forwarder_factory.h"
 #include "chrome/browser/ash/crostini/crostini_shared_devices_factory.h"
 #include "chrome/browser/ash/crostini/throttle/crostini_throttle_factory.h"
-#include "chrome/browser/ash/data_migration/data_migration_factory.h"
 #include "chrome/browser/ash/early_prefs/early_prefs_export_service_factory.h"
 #include "chrome/browser/ash/eche_app/eche_app_manager_factory.h"
 #include "chrome/browser/ash/extensions/file_manager/event_router_factory.h"
@@ -170,7 +169,6 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   crostini::CrostiniThrottleFactory::GetInstance();
   CupsPrintersManagerFactory::GetInstance();
   CupsPrintJobManagerFactory::GetInstance();
-  data_migration::DataMigrationFactory::GetInstance();
   EarlyPrefsExportServiceFactory::GetInstance();
   eche_app::EcheAppManagerFactory::GetInstance();
   EventBasedStatusReportingServiceFactory::GetInstance();
