1e744a73a406b070ce0d13f6f5b433b0ee93ea1d
Briana McClure <bmcclure@google.com>
2026-06-16 12:54:22 -0700

    [x-plat promos] Create the Wave 2 feature flags
    This change creates the feature flag for the Wave 2 x-platform promos.
    The enums for the feature types will be added and handled in a later
    change.
    
    Fixed: 524186065
    Change-Id: I76db19c5ee1d7da334f540df926ebb5b7fba4597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7940752
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647822}

--- components/desktop_to_mobile_promos/features.cc
+++ components/desktop_to_mobile_promos/features.cc
@@ -18,6 +18,9 @@ BASE_FEATURE(kMobilePromoOnDesktopWithReminder,
 BASE_FEATURE(kMobilePromoOnDesktopWithReminderWave1,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kMobilePromoOnDesktopWithReminderWave2,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kMobilePromoOnDesktopRecordActiveDays,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -25,6 +28,9 @@ BASE_FEATURE(kMobilePromoOnDesktopWithQRCode,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMobilePromoOnDesktopWithQRCodeWave1,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kMobilePromoOnDesktopWithQRCodeWave2,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMobilePromoOnDesktopForcePromoType,
@@ -49,7 +55,10 @@ bool MobilePromoOnDesktopEnabled() {
          (base::FeatureList::IsEnabled(kMobilePromoOnDesktopWithReminder) ||
           base::FeatureList::IsEnabled(kMobilePromoOnDesktopWithQRCode) ||
           base::FeatureList::IsEnabled(kMobilePromoOnDesktopWithQRCodeWave1) ||
-          base::FeatureList::IsEnabled(kMobilePromoOnDesktopWithReminderWave1));
+          base::FeatureList::IsEnabled(
+              kMobilePromoOnDesktopWithReminderWave1) ||
+          base::FeatureList::IsEnabled(kMobilePromoOnDesktopWithQRCodeWave2) ||
+          base::FeatureList::IsEnabled(kMobilePromoOnDesktopWithReminderWave2));
 }
 
 bool IsMobilePromoOnDesktopRecordActiveDaysEnabled() {
@@ -129,6 +138,9 @@ bool IsMobilePromoOnDesktopNotificationsEnabled() {
              kMobilePromoOnDesktopNotificationParam, false) ||
          base::GetFieldTrialParamByFeatureAsBool(
              kMobilePromoOnDesktopWithReminderWave1,
+             kMobilePromoOnDesktopNotificationParam, false) ||
+         base::GetFieldTrialParamByFeatureAsBool(
+             kMobilePromoOnDesktopWithReminderWave2,
              kMobilePromoOnDesktopNotificationParam, false);
 }
 
