c0d96d8346e4ac445be90a1cfbc30542ef9c949e
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-06-16 06:46:20 -0700

    lpm: Add support for protos with imports
    Adds support for generating fuzzable protos for protos with import
    statements by appending _fuzzable to the imported protos.
    
    Also migrates `components/autofill/core/browser/proto` to use
    fuzzable protos.
    
    Bug: 505034799
    Change-Id: Ifab950a17429b858dae771405078d1012fea3e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7930252
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647516}

--- components/password_manager/core/browser/generation/password_generator_proto_fuzzer.cc
+++ components/password_manager/core/browser/generation/password_generator_proto_fuzzer.cc
@@ -4,12 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/autofill/core/browser/proto/password_requirements.pb.h"
+#include "components/autofill/core/browser/proto/password_requirements_fuzzable.pb.h"
 #include "components/password_manager/core/browser/generation/password_generator.h"
 #include "testing/libfuzzer/proto/lpm_interface.h"
 
 namespace autofill {
 
-DEFINE_PROTO_FUZZER(const PasswordRequirementsSpec& spec) {
+DEFINE_PROTO_FUZZER(
+    const fuzzable::autofill::PasswordRequirementsSpec& fuzzable_spec) {
+  PasswordRequirementsSpec spec;
+  spec.ParseFromString(fuzzable_spec.SerializeAsString());
   GeneratePassword(spec);
 }
 
