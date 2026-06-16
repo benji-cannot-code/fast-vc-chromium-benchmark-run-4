467d12aabba5bbdd9c81430644959be42e2dfe88
Andrii Natiahlyi <natiahlyi@google.com>
2026-06-16 04:42:27 -0700

    Migrate UndoRemoveSavedPasswordOrException to Mojo.
    Bug: 432409279
    Change-Id: Ic79e9e31993387746baf39be78c83ac2a8f2ff4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7949799
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647463}

--- chrome/browser/extensions/api/passwords_private/test_passwords_private_delegate.h
+++ chrome/browser/extensions/api/passwords_private/test_passwords_private_delegate.h
@@ -175,6 +175,10 @@ class TestPasswordsPrivateDelegate : public PasswordsPrivateDelegate {
     return disconnect_cloud_authenticator_called_;
   }
 
+  bool get_undo_remove_saved_password_or_exception_called() const {
+    return undo_remove_saved_password_or_exception_called_;
+  }
+
   bool get_delete_all_password_manager_data_called() const {
     return delete_all_password_manager_data_called_;
   }
@@ -258,6 +262,9 @@ class TestPasswordsPrivateDelegate : public PasswordsPrivateDelegate {
 
   // Used to track whether `DisconnectCloudAuthenticator` was called.
   bool disconnect_cloud_authenticator_called_ = false;
+
+  // Used to track whether `UndoRemoveSavedPasswordOrException` was called.
+  bool undo_remove_saved_password_or_exception_called_ = false;
 
   // Used to track whether `DeleteAllPasswordManagerData` was called.
   bool delete_all_password_manager_data_called_ = false;
--- chrome/browser/ui/webui/password_manager/password_manager_ui_handler.h
+++ chrome/browser/ui/webui/password_manager/password_manager_ui_handler.h
@@ -82,6 +82,8 @@ class PasswordManagerUIHandler : public password_manager::mojom::PageHandler {
   void IsConnectedToCloudAuthenticator(
       IsConnectedToCloudAuthenticatorCallback callback) override;
 
+  void UndoRemoveSavedPasswordOrException() override;
+
  private:
   password_manager::SavedPasswordsPresenter* GetSavedPasswordsPresenter();
 
