40b119ade2965b9d6a0634e9ae3296c715edac19
Qingxin Wu <qingxinwu@google.com>
2026-06-16 16:11:44 -0700

    Fix broken icons for WebUI permission chips.
    When rounded icons were introduced, the permission chips icon names were
    changed, which broke the webui permission chip typescript which relied
    on the old icon names. This is just a quick fix to make the icon work,
    with the support for rounded icons as a follow up TODO.
    
    Change-Id: Idec0a6adbfcd0521b737a107a461398cc31b37ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7944263
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647959}

--- chrome/test/data/webui/webui_toolbar/permission_chip_test.ts
+++ chrome/test/data/webui/webui_toolbar/permission_chip_test.ts
@@ -151,7 +151,7 @@ suite('PermissionChipTest', function() {
       accessibilityName: 'Camera',
       tooltip: 'Camera in use',
       isVisible: true,
-      iconName: 'kVideocamChromeRefreshIcon',
+      iconName: 'kVideocamChromeRefreshOldIcon',
       theme: PermissionChipTheme.kNormalVisibility,
       promptStyle: PermissionPromptStyle.kChip,
       userDecision: PermissionAction.kGranted,
