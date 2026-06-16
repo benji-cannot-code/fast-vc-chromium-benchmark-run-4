b6c26669f72c094554c1510fa250b9389cd4caee
Rodney Ding <rodneyding@google.com>
2026-06-16 13:45:18 -0700

    Fix WebUIToolbarWebViewSplitTabsBrowserTest.VerifySplitTabLocations for no_field_trial bot
    - the feature was enabled via fieldtrial_testing_config.json, but fail for no_field_trial bot
    - Explicitly enable the feature to match the updated test expectation.
    
    Include-Ci-Only-Tests: chromium.win:Win10 Tests x64|browser_tests_no_field_trial
    
    Bug: 524698922
    Change-Id: I8fa730cf20f7ac9d4fc4ea1048e943a7a5c9679e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7952366
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Rodney Ding <rodneyding@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Owners-Override: Rodney Ding <rodneyding@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647860}

--- chrome/browser/ui/views/toolbar/webui_toolbar_web_view_browsertest.cc
+++ chrome/browser/ui/views/toolbar/webui_toolbar_web_view_browsertest.cc
@@ -2534,7 +2534,7 @@ class WebUIToolbarWebViewSplitTabsBrowserTest : public InProcessBrowserTest {
         {features::kInitialWebUI, features::kWebUISplitTabsButton,
          features::kSkipIPCChannelPausingForNonGuests,
          features::kWebUIInProcessResourceLoadingV2,
-         features::kInitialWebUISyncNavStartToCommit},
+         features::kInitialWebUISyncNavStartToCommit, features::kRoundedIcons},
         {});
   }
 
