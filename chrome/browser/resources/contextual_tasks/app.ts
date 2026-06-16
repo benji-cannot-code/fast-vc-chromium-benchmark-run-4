af09f36a977077b5cacffd463dba676afd8234f0
Duncan Mercer <mercerd@google.com>
2026-06-16 10:39:56 -0700

    [Contextual Tasks] Fix composebox focus on initial zero state render
    Recent changes made the composebox hidden until the DomContentLoad
    event. This means the focus() call would be missed since the element
    cannot receive focus while hidden. This CL fixes that by using the new
    tryFocus method to try to get focus once the dom content has loaded.
    Additionally, moved other clearInputAndFocus() calls to also use the
    tryFocus method. Finally, added a test to catch this in the future.
    
    Bug: b:521976927
    Change-Id: I74dc4c6f42d47a4bd12c1698fc8d8fb44b093f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7945179
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647706}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -524,7 +524,7 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
         // we are not in zero state anymore, or not in an AIM URL. In
         // both thread/AIM cases for zero state, we clear input.
         if (isZeroState) {
-          this.composebox_?.clearInputAndFocus();
+          this.forceComposeboxFocus();
           // Reset the forced composebox bounds since the zero state position
           if (!this.shouldSetForceComposeboxBounds_()) {
             this.forcedComposeboxBounds_ = null;
@@ -622,9 +622,11 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
     this.eventTracker_.add(window, 'message', (event: MessageEvent) => {
       if (event.data === 'domContentLoaded') {
         this.isDomContentLoaded_ = true;
-        // Play the zero state animations, unhide the composebox and header.
+        // Play the zero state animations, unhide the composebox/header,
+        // and focus the composebox.
         if (this.isZeroState_) {
           this.playZeroStateAnimations_();
+          this.forceComposeboxFocus();
         }
       }
     });
@@ -654,7 +656,6 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
     } else {
       const {url} = await this.browserProxy_.handler.getThreadUrl();
       threadUrl = url;
-      this.composebox_?.clearInputAndFocus();
     }
 
     const threadUrlAsUrl = new URL(threadUrl);
@@ -705,6 +706,7 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
     // it now!
     if (this.isZeroState_ && this.isDomContentLoaded_) {
       this.playZeroStateAnimations_();
+      this.forceComposeboxFocus();
     }
 
     // The thread URL is considered pending (not loaded immediately in the
@@ -1174,6 +1176,14 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
 
     // In all other cases, show the composebox.
     return false;
+  }
+
+  // Helper to focus the composebox, even if it is transitioning from hidden.
+  private forceComposeboxFocus() {
+    this.composebox_?.clearInputAndFocus();
+    this.updateComplete.then(() => {
+      this.composebox_?.tryFocus();
+    });
   }
 
   protected isComposeboxHeaderWrapperHidden_(): boolean {
