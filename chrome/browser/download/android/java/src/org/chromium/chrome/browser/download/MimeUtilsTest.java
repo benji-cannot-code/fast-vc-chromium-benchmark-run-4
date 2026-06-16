chromium/chromium

commit a5d20406b9ba7f5c12279d2e5e1bdb2bc4673f3e

Fix an issue that downloaded image has wrong MIME type
Because server can provide a wrong MIME type in response, Chrome
will recalculate the MIME type during target determination phase.
On desktop, this is done by checking the extension of the target
file. On Android, this is done by calling
GetContentUriMimeType(). However, this method doesn't really check
the file name, it is just returning the previous registered MIME
type. As a result, the wrong MIME type is still used. This CL fixes
the issue by using the display name to calculate the MIME type.

Bug: 500335595
Change-Id: I61d7ebe70bd8cc2d4af04e50e43f0aed00eed416
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770300
Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
Commit-Queue: Min Qin <qinmin@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1617780}

chrome/browser/download/chrome_download_manager_delegate.cc
@@ -287,6 +287,18 @@ bool IsForceSaveToCloud(download::DownloadDangerType danger_type) {
 std::string GetMimeType(const base::FilePath& path) {
 #if BUILDFLAG(IS_ANDROID)
   if (path.IsContentUri()) {
+    // Here we should determine the MIME type from the display name of the
+    // content URI. GetContentUriMimeType() will return the current MIME type
+    // that is registered with the URI. As a result, calling it will not change
+    // the MIME type if it is incorrect.
+    std::u16string display_name;
+    if (base::MaybeGetFileDisplayName(path, &display_name)) {
+      std::string mime_type;
+      if (net::GetMimeTypeFromFile(
+              base::FilePath::FromUTF16Unsafe(display_name), &mime_type)) {
+        return mime_type;
+      }
+    }
     return base::GetContentUriMimeType(path);
   }
 #endif

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 317e14246bfe986603b0e0479241fe90db865b93

Fix regression where non-generic MIME types were overwritten on Android
Commit a5d20406b9ba7 introduced a regression on Android where server-provided
MIME types (e.g., application/x-wifi-config) were unconditionally overwritten
with MIME types guessed from the display name extension using
net::GetMimeTypeFromFile().

This CL updates GetMimeType() on Android to instead use
DownloadUtils::RemapGenericMimeType(), which delegates to Android's
MimeUtils.remapGenericMimeType(). This ensures we only attempt to deduce
a better MIME type from the display name if the currently registered MIME
type is generic (e.g., application/octet-stream).

Bug: 520262198
Change-Id: I6a31b67004bb58dd8c8eb64db173ba8be747c561
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7941029
Reviewed-by: Min Qin <qinmin@chromium.org>
Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1647975}

chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/MimeUtils.java
@@ -38,6 +38,7 @@ public class MimeUtils {
                             "text/plain",
                             "application/octet-stream",
                             "binary/octet-stream",
+                            "binary/data",
                             "octet/stream",
                             "application/download",
                             "application/force-download",

chrome/browser/download/chrome_download_manager_delegate.cc
@@ -291,6 +291,20 @@ bool IsForceSaveToCloud(download::DownloadDangerType danger_type) {
 std::string GetMimeType(const base::FilePath& path) {
 #if BUILDFLAG(IS_ANDROID)
   if (path.IsContentUri()) {
+    if (base::FeatureList::IsEnabled(
+            download::features::kRemapGenericMimeType)) {
+      // Determine the MIME type registered with the content URI. If it is a
+      // generic MIME type (e.g., application/octet-stream), attempt to deduce a
+      // more specific MIME type from the display name extension.
+      std::string mime_type = base::GetContentUriMimeType(path);
+      std::u16string display_name;
+      if (base::MaybeGetFileDisplayName(path, &display_name)) {
+        mime_type = DownloadUtils::RemapGenericMimeType(
+            mime_type, GURL(), base::UTF16ToUTF8(display_name));
+      }
+      return mime_type;
+    }
+
     // Here we should determine the MIME type from the display name of the
     // content URI. GetContentUriMimeType() will return the current MIME type
     // that is registered with the URI. As a result, calling it will not change

components/download/public/common/download_features.cc
@@ -40,6 +40,8 @@ BASE_FEATURE(kShowBlockedSensitiveDownload, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kShowDownloadScanningState, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDownloadsCompactListView, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kRemapGenericMimeType, base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kDownloadNotificationServiceUnifiedAPI,

components/download/public/common/download_features.h
@@ -36,6 +36,10 @@ COMPONENTS_DOWNLOAD_EXPORT BASE_DECLARE_FEATURE(kShowDownloadScanningState);
 // images and videos on the Android Downloads page, in place of the default
 // large preview cards. See crbug.com/505631139.
 COMPONENTS_DOWNLOAD_EXPORT BASE_DECLARE_FEATURE(kDownloadsCompactListView);
+
+// Whether to remap generic MIME types (like application/octet-stream) to more
+// specific MIME types deduced from the filename extension on Android.
+COMPONENTS_DOWNLOAD_EXPORT BASE_DECLARE_FEATURE(kRemapGenericMimeType);
 #endif
 
 // Whether download notification service uses new unified API based on offline

chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/MimeUtilsTest.java
@@ -46,4 +46,23 @@ public class MimeUtilsTest {
         Assert.assertTrue(MimeUtils.canAutoOpenMimeType("application/x-wifi-config"));
         Assert.assertTrue(MimeUtils.canAutoOpenMimeType("application/pkix-cert"));
     }
+
+    /**
+     * Test to make sure {@link MimeUtils#remapGenericMimeType} preserves non-generic MIME types
+     * (such as application/x-wifi-config) regardless of file extension.
+     */
+    @Test
+    @SmallTest
+    @Feature({"Download"})
+    public void testRemapGenericMimeType() {
+        Assert.assertEquals(
+                "image/jpeg",
+                MimeUtils.remapGenericMimeType("application/octet-stream", "http://file.jpg", ""));
+        Assert.assertEquals(
+                "image/jpeg", MimeUtils.remapGenericMimeType("binary/data", "http://file.jpg", ""));
+        Assert.assertEquals(
+                "application/x-wifi-config",
+                MimeUtils.remapGenericMimeType(
+                        "application/x-wifi-config", "http://file.xml", "file.xml"));
+    }
 }