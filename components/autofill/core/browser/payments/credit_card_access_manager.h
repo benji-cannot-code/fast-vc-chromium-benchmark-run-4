530a597794b9b8b1eef8611599b6999b58e25b62
Vinny Persky <vinnypersky@google.com>
2026-06-16 08:55:46 -0700

    Add logging for # of credit card suggestions shown when a BNPL chip is accepted
    To better understand the types of users that use BNPL, we want to log
    the number of credit card suggestions shown when a BNPL suggestion is
    accepted. The number of credit card suggestions shown should not include
    the BNPL suggestion itself.
    
    Bug: 520243556
    Change-Id: Icb388333a471f89cf4a5c69f8bcbd6220372a238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902204
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647613}

--- components/autofill/core/browser/payments/credit_card_access_manager.h
+++ components/autofill/core/browser/payments/credit_card_access_manager.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
 #include "components/autofill/core/browser/foundations/autofill_driver.h"
 #include "components/autofill/core/browser/foundations/browser_autofill_manager.h"
-#include "components/autofill/core/browser/metrics/form_events/credit_card_form_event_logger.h"
 #include "components/autofill/core/browser/payments/credit_card_cvc_authenticator.h"
 #include "components/autofill/core/browser/payments/credit_card_otp_authenticator.h"
 #include "components/autofill/core/browser/payments/credit_card_risk_based_authenticator.h"
@@ -34,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_autofill_client.h"
 #include "components/autofill/core/browser/payments/payments_request_details.h"
 #include "components/autofill/core/browser/payments/payments_window_manager.h"
+#include "components/autofill/core/browser/payments/unmask_auth_flow_type.h"
 #include "components/autofill/core/browser/payments/wait_for_signal_or_timeout.h"
 
 #if !BUILDFLAG(IS_IOS)
@@ -45,30 +45,9 @@ namespace autofill {
 class AutofillClient;
 enum class WebauthnDialogCallbackType;
 
-// Flow type denotes which card unmask authentication method was used.
-// TODO(crbug.com/40216473): Deprecate kCvcThenFido, kCvcFallbackFromFido, and
-// kOtpFallbackFromFido.
-enum class UnmaskAuthFlowType {
-  kNone = 0,
-  // Only CVC prompt was shown.
-  kCvc = 1,
-  // Only WebAuthn prompt was shown.
-  kFido = 2,
-  // CVC authentication was required in addition to WebAuthn.
-  kCvcThenFido = 3,
-  // FIDO authentication failed and fell back to CVC authentication.
-  kCvcFallbackFromFido = 4,
-  // OTP authentication was offered.
-  kOtp = 5,
-  // FIDO authentication failed and fell back to OTP authentication.
-  kOtpFallbackFromFido = 6,
-  // VCN 3DS was the only challenge option returned.
-  kThreeDomainSecure = 7,
-  // VCN 3DS was one of the challenge options returned in the challenge
-  // selection dialog, and user selected the 3DS challenge option.
-  kThreeDomainSecureConsentAlreadyGiven = 8,
-  kMaxValue = kThreeDomainSecureConsentAlreadyGiven,
-};
+namespace autofill_metrics {
+class CreditCardFormEventLogger;
+}
 
 // TODO(crbug.com/40927041): Remove CVC from CachedServerCardInfo.
 struct CachedServerCardInfo {
