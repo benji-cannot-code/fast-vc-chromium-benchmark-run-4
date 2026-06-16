116023c77d21d1ccc9a21fcbb56079c87900549c
Achuith Bhandarkar <achuith@chromium.org>
2026-06-16 12:30:49 -0700

    [content_settings] Fix exit-time destructors in component extensions content settings
    Remove compiler:no_exit_time_destructors from the target and unit_tests
    Changes:
    - Convert global map constants and the testing override optional member to base::NoDestructor helper functions.
    - Remove class static member
    
    Bug: 40031409, 522379416
    Change-Id: I9e02c29d5ec464af1242465713430dc26ad9c35b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7935372
    Reviewed-by: Abdelrahman Eed <abdoeed@google.com>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647804}

--- chrome/browser/chromeos/extensions/component_extension_content_settings/component_extension_content_settings_provider_unittest.cc
+++ chrome/browser/chromeos/extensions/component_extension_content_settings/component_extension_content_settings_provider_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/no_destructor.h"
 #include "base/test/gtest_util.h"
 #include "chrome/browser/chromeos/extensions/component_extension_content_settings/component_extension_content_settings_allowlist.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -30,13 +31,18 @@ namespace extensions {
 namespace {
 inline constexpr char kNotAllowlistedId[] = "not-allowlisted-extension";
 
-static const ComponentExtensionContentSettingsAllowlist::
-    ExtensionsContentSettingsTypes
-        kTestComponentExtensionsContentSettingsTypes = {
-            {extension_misc::kPdfExtensionId,
-             {ContentSettingsType::FILE_SYSTEM_READ_GUARD,
-              ContentSettingsType::FILE_SYSTEM_WRITE_GUARD}},
-            {kNotAllowlistedId, {}}};
+const ComponentExtensionContentSettingsAllowlist::
+    ExtensionsContentSettingsTypes&
+    GetTestComponentExtensionsContentSettingsTypes() {
+  static const base::NoDestructor<ComponentExtensionContentSettingsAllowlist::
+                                      ExtensionsContentSettingsTypes>
+      val({{extension_misc::kPdfExtensionId,
+            {ContentSettingsType::FILE_SYSTEM_READ_GUARD,
+             ContentSettingsType::FILE_SYSTEM_WRITE_GUARD}},
+           {kNotAllowlistedId, {}}});
+  return *val;
+}
+
 }  // namespace
 
 class ComponentExtensionContentSettingsProviderTest
@@ -82,7 +88,7 @@ class ComponentExtensionContentSettingsProviderTest
     ChromeRenderViewHostTestHarness::SetUp();
     ComponentExtensionContentSettingsAllowlist::
         SetComponentExtensionsContentSettingsTypesForTesting(
-            kTestComponentExtensionsContentSettingsTypes);
+            GetTestComponentExtensionsContentSettingsTypes());
   }
 
  private:
