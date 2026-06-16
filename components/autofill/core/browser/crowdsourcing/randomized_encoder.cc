2aaa68614b98b103f634a5ae4ad0b9ad07c9dd3b
Elly <ellyjones@chromium.org>
2026-06-16 16:46:16 -0700

    autofill/crowdsourcing: use crypto/kdf for randomized encoding
    The deprecated crypto/hkdf API, which is hardcoded to be SHA-256 only,
    is going away. The crypto/kdf API is functionally identical, although
    a bit more unwieldy to call here since it only operates on byte spans,
    not strings.
    
    Bug: 430616377
    Change-Id: Iedf7d5c8f063dc3c8b992782bf4fa18b7f1ae25e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7947578
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Auto-Submit: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647981}

--- components/autofill/core/browser/crowdsourcing/randomized_encoder.cc
+++ components/autofill/core/browser/crowdsourcing/randomized_encoder.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
@@ -27,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/autofill/core/common/signatures.h"
 #include "components/prefs/pref_service.h"
-#include "crypto/hkdf.h"
+#include "crypto/kdf.h"
 
 namespace autofill {
 
@@ -84,7 +85,11 @@ std::string GetPseudoRandomBits(std::string_view secret,
   DVLOG(1) << "Generating pseudo-random bits from " << info;
 
   // Generate the pseudo-random bits.
-  return crypto::HkdfSha256(secret, {}, info, encoding_length_in_bytes);
+  std::string result(encoding_length_in_bytes, '\0');
+  crypto::kdf::Hkdf(crypto::hash::kSha256, base::as_byte_span(secret), {},
+                    base::as_byte_span(info),
+                    base::as_writable_byte_span(result));
+  return result;
 }
 
 // Returns the "random" encoding type to use for encoding.
