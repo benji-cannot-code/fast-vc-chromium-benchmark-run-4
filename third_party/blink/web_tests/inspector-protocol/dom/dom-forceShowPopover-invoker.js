952f6aa521c1d0ac23c1ebcc901d713b271b4398
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-06-16 14:56:50 -0700

    [anchor] Remove position-anchor:auto from UA stylesheet.
    We've changed the initial value of position-anchor from none to normal
    in: https://chromium-review.googlesource.com/c/chromium/src/+/7891490
    
    This means that when position-area is set it'll behave as "auto".
    
    This matches other browsers.
    
    Fixed: 524243473
    Change-Id: Icbb453132a834159361d4bcef1119a9fa6f17b28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7947097
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647910}

--- third_party/blink/web_tests/inspector-protocol/dom/dom-forceShowPopover-invoker.js
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-forceShowPopover-invoker.js
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          #btn-popover2 { top: 300px; left: 100px; }
 
          [popover] {
+           position-anchor: auto;
            margin: 0;
            top: anchor(top);
            left: anchor(right);
