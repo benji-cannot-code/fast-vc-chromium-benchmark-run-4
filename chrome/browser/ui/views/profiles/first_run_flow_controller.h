bb8c2b93516a3275c39e95cf2e98e740882426f2
Ernest Nguyen Hung <ernn@google.com>
2026-06-16 04:16:17 -0700

    [FRE] Add welcome back sound effect on sign-in celebration
    It adds a 'Welcome back' sound effect on sign-in celebration for both
    consumer and enterprise users.
    
    Bug: 485150597
    Change-Id: I6604bb019b1c4f7c3e28823af225f7de68e8b561
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7941289
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647456}

--- chrome/browser/ui/views/profiles/first_run_flow_controller.h
+++ chrome/browser/ui/views/profiles/first_run_flow_controller.h
@@ -52,6 +52,7 @@ class FirstRunFlowController : public ProfileManagementFlowControllerImpl {
  public:
   static constexpr audio::SoundsManager::SoundKey kAmbientSoundKey = 0;
   static constexpr audio::SoundsManager::SoundKey kLogoSoundKey = 1;
+  static constexpr audio::SoundsManager::SoundKey kWelcomeBackSoundKey = 2;
 
   // Profile management flow controller that will run the FRE for `profile` in
   // `host`.
@@ -96,6 +97,8 @@ class FirstRunFlowController : public ProfileManagementFlowControllerImpl {
 
  private:
   void HandleIntroSigninChoice(IntroChoice choice);
+
+  void PlaySignInCelebrationSound();
 
   // Run the `finish_flow_callback_` if it's not empty.
   void RunFinishFlowCallback();
