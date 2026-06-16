aa5e4c2723c6abd174f7253fb114ab0d5a3faa47
Muhammad Salmaan <musalmaan@chromium.org>
2026-06-16 10:45:14 -0700

    [Gpointer] Add tests for line selection
    This CL introduces a test suite in line_selection_test.ts for line
    (polyline) selection which validates that drawing gestures with line
    selection enabled correctly translate to polyline selection events and
    verify that static polyline regions are properly styled with the
    has-polyline attribute.
    
    Bug: 519550608
    Change-Id: I26927e8e4b8a0ceb1b1ab31d1bb92e719ea16de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7947938
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647715}

--- chrome/browser/resources/lens/overlay/region_selection.ts
+++ chrome/browser/resources/lens/overlay/region_selection.ts
@@ -701,6 +701,10 @@ export class RegionSelectionElement extends RegionSelectionElementBase {
       height: normalizedHeight,
     };
   }
+
+  setSelectionOverlayRectForTesting(rect: DOMRect) {
+    this.selectionOverlayRect = rect;
+  }
 }
 
 declare global {
