f9cf4fd47ad3df7103fd901157ea4181adb5db92
Isabelle Goralsky <igoralsky@google.com>
2026-06-16 08:13:35 -0700

    Add function for UI to call ContextHubService and generate TODOs.
    Adds GenerateAutoTodos function to the mojom interface. Implements this
    function in ContextHub page handler which calls the ContextHubService to
    generate TODOs.
    
    Bug: b:521840846
    Change-Id: Ie5a4d69250de9df0e3021b99dd3d123f58a13a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7931212
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647572}

--- chrome/browser/ui/webui/context_hub/context_hub_page_handler.h
+++ chrome/browser/ui/webui/context_hub/context_hub_page_handler.h
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_CONTEXT_HUB_CONTEXT_HUB_PAGE_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CONTEXT_HUB_CONTEXT_HUB_PAGE_HANDLER_H_
 
+#include <optional>
+
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/context_hub/context_hub.mojom.h"
+#include "components/personal_context/proto/features/auto_todos.pb.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 
@@ -15,7 +19,7 @@ class Profile;
 
 namespace content {
 class WebContents;
-}
+}  // namespace content
 
 class ContextHubPageHandler : public browser::context_hub::mojom::PageHandler {
  public:
@@ -28,10 +32,18 @@ class ContextHubPageHandler : public browser::context_hub::mojom::PageHandler {
   ContextHubPageHandler(const ContextHubPageHandler&) = delete;
   ContextHubPageHandler& operator=(const ContextHubPageHandler&) = delete;
 
+  // browser::context_hub::mojom::PageHandler:
+  void GenerateAutoTodos(GenerateAutoTodosCallback callback) override;
+
  private:
+  void OnAutoTodosGenerated(
+      GenerateAutoTodosCallback callback,
+      std::optional<personal_context::proto::AutoTodosResponse> result);
+
   mojo::Receiver<browser::context_hub::mojom::PageHandler> receiver_;
   raw_ptr<Profile> profile_;
   raw_ptr<content::WebContents> web_contents_;
+  base::WeakPtrFactory<ContextHubPageHandler> weak_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_CONTEXT_HUB_CONTEXT_HUB_PAGE_HANDLER_H_
