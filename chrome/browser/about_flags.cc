266f8275c45c80a318f913119a8066298c9da879
James Cook <jamescook@chromium.org>
2026-06-15 17:41:42 -0700

    android: Add feature flag SettingsInTab
    In the future, this will allow Android Settings to open in a tab on
    large form factor devices. See bug.
    
    For now, the flag is just for development and does nothing.
    
    Bug: 521895796
    Change-Id: Icc87cac243c6f66a4ff569736a4bf472a4325c58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7944890
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647216}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7333,6 +7333,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kOnStartupWindowPolicyDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kOnStartupWindowPolicy)},
 
+    {"settings-in-tab", flag_descriptions::kSettingsInTabName,
+     flag_descriptions::kSettingsInTabDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kSettingsInTab)},
+
     {"settings-multi-column", flag_descriptions::kSettingsMultiColumnName,
      flag_descriptions::kSettingsMultiColumnDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kSettingsMultiColumn)},
