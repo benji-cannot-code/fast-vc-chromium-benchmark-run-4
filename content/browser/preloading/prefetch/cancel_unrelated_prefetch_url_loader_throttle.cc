2f941cf60996086a5a500fa0704cf6fa27134fd5
kenoss <kenoss@chromium.org>
2026-06-16 03:33:19 -0700

    CancelUnrelatedPrefetch: Cancel only prefetches of the same document
    This patch introduces a feature parameter to control targets of
    cancellation. With a new `NotServableSameDocument` policy, only
    prefeteches that are not servable and share the initiator are cancelled.
    
    Bug: 499176365
    Change-Id: I659e72e975107261cfbf0ecd81ffd418f1fda2ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7894037
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647443}

--- content/browser/preloading/prefetch/cancel_unrelated_prefetch_url_loader_throttle.cc
+++ content/browser/preloading/prefetch/cancel_unrelated_prefetch_url_loader_throttle.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/preloading/prefetch/prefetch_features.h"
 #include "content/browser/preloading/prefetch/prefetch_service.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
+#include "content/browser/renderer_host/navigation_request.h"
+#include "third_party/blink/public/common/tokens/tokens.h"
 
 namespace content {
 
@@ -57,7 +59,15 @@ void CancelUnrelatedPrefetchURLLoaderThrottle::WillStartRequest(
     return;
   }
 
-  prefetch_service->CancelUnrelatedPrefetchForNavigation();
+  NavigationRequest* navigation_request = frame_tree_node->navigation_request();
+  if (!navigation_request) {
+    return;
+  }
+  const std::optional<blink::DocumentToken>& initiator_document_token =
+      navigation_request->GetInitiatorDocumentToken();
+
+  prefetch_service->CancelUnrelatedPrefetchForNavigation(
+      initiator_document_token);
 }
 
 }  // namespace content
