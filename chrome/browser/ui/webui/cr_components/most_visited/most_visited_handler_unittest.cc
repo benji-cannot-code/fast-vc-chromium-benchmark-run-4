19e395fccd3ceda5ab46956523b1ebdf64127f26
Ruoxi He <ruoxih@google.com>
2026-06-16 16:43:50 -0700

    [WebUI NTP on AL] Enable MV Tiles unit tests under enable_webui_ntp
    This CL moves `most_visited_handler_unittest.cc` to the
    `enable_webui_ntp` block in `chrome/test/BUILD.gn`. This enables
    compilation and execution of the MV Tiles tests on Android when WebUI
    NTP is enabled (specifically for desktop Android builds), without
    affecting regular Android builds.
    
    It also wraps the enterprise shortcuts test in
    `most_visited_handler_unittest.cc` with a check for `!BUILDFLAG(IS_ANDROID)`
    since enterprise shortcuts are not supported on Android, which was
    causing the test to crash.
    
    Fixed: b:524679081
    Change-Id: Ie2523cce32f8069f26076c596b7f78e7b7d4d478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7927820
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Ruoxi He <ruoxih@google.com>
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647979}

--- chrome/browser/ui/webui/cr_components/most_visited/most_visited_handler_unittest.cc
+++ chrome/browser/ui/webui/cr_components/most_visited/most_visited_handler_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/gmock_callback_support.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/new_tab_page/ntp_pref_names.h"
 #include "chrome/browser/search_engines/template_url_service_factory_test_util.h"
 #include "chrome/browser/ui/search/ntp_user_data_types.h"
@@ -301,6 +302,9 @@ TEST_P(MostVisitedAutoRemovalTest, DoNotRemoveStaleShortcutsIfFeatureDisabled) {
   EXPECT_TRUE(profile_.GetPrefs()->GetBoolean(ntp_prefs::kNtpShortcutsVisible));
 }
 
+// TODO(b/514161985): Enable this test on Android once enterprise shortcuts are
+// supported.
+#if !BUILDFLAG(IS_ANDROID)
 TEST_P(MostVisitedAutoRemovalTest,
        DoNotRemoveStaleShortcutsIfEnterpriseShortcutsEnabled) {
   InitFeature(true);
@@ -320,6 +324,7 @@ TEST_P(MostVisitedAutoRemovalTest,
 
   EXPECT_TRUE(profile_.GetPrefs()->GetBoolean(ntp_prefs::kNtpShortcutsVisible));
 }
+#endif
 
 TEST_P(MostVisitedAutoRemovalTest, RemoveStaleShortcutsIfReachThreshold) {
   InitFeature(true);
