3b16a1bef4657f6086a3e4265eae486c5f20127b
Mike Wasserman <msw@chromium.org>
2026-06-15 17:49:43 -0700

    Built-In AI APIs: Migrate legacy chrome://flags user state
    Migrate renamed chrome://flags from FlagsStorage.
    Add test coverage. Followup from https://crrev.com/c/7914499
    
    Bug: 524236481
    Change-Id: I1a609909262fa13203e2d607b37eaa9eb59f3c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7929150
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647221}

--- components/webui/flags/flags_state.h
+++ components/webui/flags/flags_state.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "base/command_line.h"
@@ -269,7 +270,7 @@ class FlagsState {
   // b) Is not excluded by |exclude_predicate_|, if it is set (i.e. for which
   //    |exclude_predicate_| returns false).
   bool IsSupportedFeature(const FlagsStorage* storage,
-                          const std::string& name,
+                          std::string_view name,
                           int platform_mask) const;
 
   // Stores the flags in both FlagsStorage and SharedPreferences.
