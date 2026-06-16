b9cd79c276c79ce9121fbc84a1061ecaffb50d92
DianaOu <dianaou@google.com>
2026-06-16 16:43:18 -0700

    Record metrics for pinning and unpinning contextual tasks
    Add user action logging when pinning or unpinning the contextual tasks
    side panel from the WebUI. Specifically, this captures interactions
    originating from both the overflow menu and the top toolbar components.
    
    Additionally, update the metrics XML configurations to register the new
    user action variants and expand the WebUI test suites to verify that the
    corresponding metrics are recorded correctly.
    
    Change-Id: I36522001ef78e688318be8e5f8c6285de3f94066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951281
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647978}

--- chrome/browser/resources/contextual_tasks/top_toolbar.ts
+++ chrome/browser/resources/contextual_tasks/top_toolbar.ts
@@ -173,11 +173,6 @@ export class TopToolbarElement extends TopToolbarElementBase {
 
   protected onPinClick_() {
     this.isPinned = !this.isPinned;
-    if (this.isPinned) {
-      this.browserProxy_.handler.pinSidePanel();
-    } else {
-      this.browserProxy_.handler.unpinSidePanel();
-    }
   }
 
   protected onCloseButtonClick_() {
@@ -195,6 +190,7 @@ export class TopToolbarElement extends TopToolbarElementBase {
   }
 
   protected onOverflowMenuButtonClick_(e: Event) {
+    recordAction('ContextualTasks.WebUI.UserAction.OpenOverflowMenu');
     this.$.overflowMenu.get().showAt(e.target as HTMLElement);
   }
 
