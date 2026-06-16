c3a60dfdf347c18fd1bfa9f8825b58c98fa73421
Marc Treib <treib@chromium.org>
2026-06-15 23:25:22 -0700

    [Android] HistoryAdapter: Fix inconsistent state in search()
    HistoryAdapter::search() calls queryHistory(), but before this CL, did
    not set mIsLoadingItems to true. This meant that canLoadMoreItems()
    might return true, and so queryHistoryContinuation() might get called
    before there was actually anything to continue, causing a null pointer
    crash on the native side.
    
    This CL fixes this by setting mIsLoadingItems to true in search(),
    similar to what startLoadingItems() and loadMoreItems() already did.
    
    Fixed: 524021663
    Change-Id: I43355ebecdb95e767c87e2e1f648c09e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7942011
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647343}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
@@ -217,6 +217,7 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
     public void search(String query) {
         mQueryText = query;
         onSearchStart();
+        mIsLoadingItems = true;
         mClearOnNextQueryComplete = true;
         mHistoryProvider.queryHistory(mQueryText, mAppId);
     }
@@ -272,6 +273,7 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
 
     /**
      * Sets the selectable item mode. Items only selectable if they have a SelectableItemViewHolder.
+     *
      * @param active Whether the selection mode is on or not.
      */
     public void setSelectionActive(boolean active) {
@@ -395,8 +397,7 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
     }
 
     /**
-     * Initialize a more progress button as footer items that will be re-used
-     * during page loading.
+     * Initialize a more progress button as footer items that will be re-used during page loading.
      */
     @Initializer
     void generateFooterItems() {
