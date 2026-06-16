04f7eabd787ac786ccbbf6e643049d073e24b558
Hoch Hochkeppel <mhochk@microsoft.com>
2026-06-16 14:47:16 -0700

    [meta text-scale] Expose text scale factor for Windows
    In Chromium we bake the text-scale into the device-scale-factor on
    Windows, scaling all the UI accordingly. Though not currently applied on
    other platforms, this is the same behavior that would ideally be applied
    on other platforms where there is a system-level text-scale capability
    (e.g. Mac).
    
    This change leaves that behavior as-is, but exposes the text-scale
    information alongside the device-scale information so that it can
    subsequently be used for UI that wants to opt-out of overall scaling to
    specifically only scale the text. This will be used in the renderer to
    allow sites to do this opt-out via the text-scale meta tag.
    
    Bug: 420184705, 430566925
    Change-Id: Icc2571d5cf459ee5e8e6c6b2b4ce9b22a9f177ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7886285
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Hoch Hochkeppel <mhochk@microsoft.com>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647899}

--- ui/display/win/display_info.h
+++ ui/display/win/display_info.h
@@ -23,6 +23,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   DisplayInfo(std::optional<HMONITOR> hmonitor,
               const MONITORINFOEX& monitor_info,
               float device_scale_factor,
+              float text_scale_multiplier,
               int color_depth,
               float sdr_white_level,
               Display::Rotation rotation,
@@ -36,6 +37,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   DisplayInfo(int64_t id,
               const MONITORINFOEX& monitor_info,
               float device_scale_factor,
+              float text_scale_multiplier,
               int color_depth,
               float sdr_white_level,
               Display::Rotation rotation,
@@ -54,6 +56,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   const gfx::Rect& screen_rect() const { return screen_rect_; }
   const gfx::Rect& screen_work_rect() const { return screen_work_rect_; }
   float device_scale_factor() const { return device_scale_factor_; }
+  float text_scale_multiplier() const { return text_scale_multiplier_; }
   int color_depth() const { return color_depth_; }
   float sdr_white_level() const { return sdr_white_level_; }
   Display::Rotation rotation() const { return rotation_; }
@@ -78,6 +81,7 @@ class DISPLAY_EXPORT DisplayInfo final {
   // Used to derive display::Display work areas, and for window placement logic.
   gfx::Rect screen_work_rect_;
   float device_scale_factor_;
+  float text_scale_multiplier_;
   int color_depth_;
   float sdr_white_level_;
   Display::Rotation rotation_;
