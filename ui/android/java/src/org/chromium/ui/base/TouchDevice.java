0c99e74b691d6c67342dc8c4b1123644bd9726dd
Andrew Grieve <agrieve@chromium.org>
2026-06-16 12:19:36 -0700

    Android: Avoid getSystemService() when querying UI Mode
    The UI Mode never changes, and is available on every Context's Resources
    object. Use that instead to save an IPC.
    
    Bug: None
    Change-Id: Iab2d5298366fdde43f2b4a5ec9e7ed5cfdc6baf7
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951322
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1647794}

--- ui/android/java/src/org/chromium/ui/base/TouchDevice.java
+++ ui/android/java/src/org/chromium/ui/base/TouchDevice.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.ui.base;
 
-import android.app.UiModeManager;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Build;
@@ -153,12 +151,8 @@ public class TouchDevice {
         if (DeviceInfo.isDesktop()) { // nocheck
             return true;
         }
-        UiModeManager uiModeManager =
-                (UiModeManager)
-                        ContextUtils.getApplicationContext()
-                                .getSystemService(Context.UI_MODE_SERVICE);
-        return uiModeManager != null
-                && uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_DESK;
+        int uiMode = ContextUtils.getApplicationContext().getResources().getConfiguration().uiMode;
+        return (uiMode & Configuration.UI_MODE_TYPE_MASK) == Configuration.UI_MODE_TYPE_DESK;
     }
 
     private static boolean hasSource(int sources, int inputDeviceSource) {
