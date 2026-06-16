e51db9c4df12420544d868d7a8e36a127fa21a55
Omar Flores <omarflores@google.com>
2026-06-16 06:36:09 -0700

    [ntp] Glyph animation on realbox
    Adding a 150ms delay upon box expansion to address a glow "flash" that
    would occur otherwise as the rendering of the glow clashes with the
    expanding div.
    
    Screencast:
    https://screencast.googleplex.com/cast/NTMyMzg0MzI3ODIwOTAyNHw3Mzk3NGI5OC00NA
    Offline screencast:
    https://screencast.googleplex.com/cast/NTczMDEyNDY2OTM4Njc1MnxkYWE4YTZiYi1lOA
    
    Bug: 488356497
    Change-Id: I3617b497a25d585cbee9b645b6706e159ac68132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7907054
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Omar Flores <omarflores@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647508}

--- chrome/browser/resources/new_tab_page/ntp_searchbox.html.ts
+++ chrome/browser/resources/new_tab_page/ntp_searchbox.html.ts
@@ -25,6 +25,8 @@ export function getHtml(this: NtpSearchboxElement) {
         animation-state="${this.animationState}"
         .isListening="${this.isListening}"
         .darkThemeColorsEnabled="${false}"
+        .energyEffectAnimationEnabled="${this.energyEffectAnimationEnabled}"
+        .entrypointName="${'Realbox'}"
         part="animated-glow">
       </search-animated-glow>
     ` : ''}
