2154b3270ee2994d9d7c7642620cbb9b5f8ae28f
Juan Mojica <juanmojica@google.com>
2026-06-17 12:47:42 -0700

    [contextual-tasks] Fix uninitialized profile on desktop
    Initialize the profile_ member variable in the constructor of
    ContextualTasksUiServiceDelegateDesktop. This was previously ignored,
    leaving profile_ uninitialized. This caused chrome::ShowFeedbackPage
    to fail silently when called from the contextual tasks feedback button.
    
    Bug: b:522408368
    Change-Id: Ie0cea5b64cdf0462607e982354fb371d21ff59df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7955411
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648524}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service_delegate_desktop.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service_delegate_desktop.cc
@@ -17,7 +17,7 @@ namespace contextual_tasks {
 
 ContextualTasksUiServiceDelegateDesktop::
     ContextualTasksUiServiceDelegateDesktop(Profile* profile)
-    : ContextualTasksUiServiceDelegate() {}
+    : ContextualTasksUiServiceDelegate(), profile_(profile) {}
 
 ContextualTasksUiServiceDelegateDesktop::
     ~ContextualTasksUiServiceDelegateDesktop() = default;
