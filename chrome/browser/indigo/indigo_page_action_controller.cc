67444cf538d605d0c3ddecce5ad2f77cec76114d
Rodney Ding <rodneyding@google.com>
2026-06-17 13:06:45 -0700

    [Indigo] Skip side panel trigger when retry
    Change-Id: I69689c2d3ee9234be4e5b4c20cf1c09f3e195c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7945259
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648536}

--- chrome/browser/indigo/indigo_page_action_controller.cc
+++ chrome/browser/indigo/indigo_page_action_controller.cc
@@ -218,18 +218,20 @@ void IndigoPageActionController::InvokeAction(EntryPoint entry_point) {
     return;
   }
 
+  const bool skip_glic_invoke = (entry_point == EntryPoint::kErrorToast);
+
   switch (entry_point) {
     case EntryPoint::kErrorToast:
     case EntryPoint::kAnchoredMessage:
       indigo_service_->GetCombinedEligibility(base::BindOnce(
           &IndigoPageActionController::CheckEligibilityForOnboarding,
-          invoke_weak_ptr_factory_.GetWeakPtr()));
+          invoke_weak_ptr_factory_.GetWeakPtr(), skip_glic_invoke));
       return;
     case EntryPoint::kSuggestionChip:
       if (glic::GlicSidePanelCoordinator::IsShowing(&tab())) {
         indigo_service_->GetCombinedEligibility(base::BindOnce(
             &IndigoPageActionController::CheckEligibilityForOnboarding,
-            invoke_weak_ptr_factory_.GetWeakPtr()));
+            invoke_weak_ptr_factory_.GetWeakPtr(), skip_glic_invoke));
         return;
       }
       ShowAnchoredMessage(
@@ -239,6 +241,7 @@ void IndigoPageActionController::InvokeAction(EntryPoint entry_point) {
 }
 
 void IndigoPageActionController::CheckEligibilityForOnboarding(
+    bool skip_glic_invoke,
     const CombinedEligibility& eligibility) {
   if (eligibility.local_eligibility ==
       LocalEligibility::kRefreshTokenInPersistentErrorState) {
@@ -262,14 +265,15 @@ void IndigoPageActionController::CheckEligibilityForOnboarding(
 
   // Show onboarding if the user is ready to onboard, or if it's forced.
   if (eligibility.ReadyToOnboard() || force_onboarding) {
-    ShowOnboardingDialog(OnboardingDisposition::kDefault);
+    ShowOnboardingDialog(OnboardingDisposition::kDefault, skip_glic_invoke);
     return;
   }
 
-  ContinueInvoke(eligibility);
+  ContinueInvoke(skip_glic_invoke, eligibility);
 }
 
 void IndigoPageActionController::ContinueInvoke(
+    bool skip_glic_invoke,
     const CombinedEligibility& eligibility) {
   content::WebContents* web_contents = tab().GetContents();
   if (!web_contents) {
@@ -285,7 +289,8 @@ void IndigoPageActionController::ContinueInvoke(
     return;
   }
 
-  if (base::FeatureList::IsEnabled(features::kIndigoOpenGlic) &&
+  if (!skip_glic_invoke &&
+      base::FeatureList::IsEnabled(features::kIndigoOpenGlic) &&
       !glic::GlicSidePanelCoordinator::IsShowing(&tab())) {
     Profile* profile =
         Profile::FromBrowserContext(web_contents->GetBrowserContext());
@@ -364,7 +369,8 @@ void IndigoPageActionController::TriggerIndigoAgentWithDelay() {
 }
 
 void IndigoPageActionController::ShowOnboardingDialog(
-    OnboardingDisposition disposition) {
+    OnboardingDisposition disposition,
+    bool skip_glic_invoke) {
   if (onboarding_dialog_) {
     return;
   }
@@ -384,9 +390,9 @@ void IndigoPageActionController::ShowOnboardingDialog(
     base::RecordAction(base::UserMetricsAction("Indigo.Onboarding.Trigger"));
   }
 
-  auto callback =
-      base::BindOnce(&IndigoPageActionController::OnOnboardingDialogClosed,
-                     invoke_weak_ptr_factory_.GetWeakPtr(), disposition);
+  auto callback = base::BindOnce(
+      &IndigoPageActionController::OnOnboardingDialogClosed,
+      invoke_weak_ptr_factory_.GetWeakPtr(), disposition, skip_glic_invoke);
 
   if (onboarding_dialog_factory_for_testing_) {
     onboarding_dialog_ = onboarding_dialog_factory_for_testing_.Run(
@@ -524,7 +530,8 @@ void IndigoPageActionController::OnRegenerate(IndigoToolbar* toolbar) {
 
 void IndigoPageActionController::OnReplaceOriginalPhoto(
     IndigoToolbar* toolbar) {
-  ShowOnboardingDialog(OnboardingDisposition::kReplacePhoto);
+  ShowOnboardingDialog(OnboardingDisposition::kReplacePhoto,
+                       /*skip_glic_invoke=*/true);
 }
 
 void IndigoPageActionController::OnDeleteOriginalPhoto(IndigoToolbar* toolbar) {
@@ -586,6 +593,7 @@ void IndigoPageActionController::UpdateEntryPointsState() {
 
 void IndigoPageActionController::OnOnboardingDialogClosed(
     OnboardingDisposition disposition,
+    bool skip_glic_invoke,
     const OnboardingResult& result) {
   const bool acknowledged = result.acknowledge_chrome_disclaimer;
   onboarding_dialog_.reset();
@@ -614,9 +622,9 @@ void IndigoPageActionController::OnOnboardingDialogClosed(
     if (disposition == OnboardingDisposition::kReplacePhoto) {
       OnRegenerate(toolbar_.get());
     } else {
-      indigo_service_->GetCombinedEligibility(
-          base::BindOnce(&IndigoPageActionController::ContinueInvoke,
-                         invoke_weak_ptr_factory_.GetWeakPtr()));
+      indigo_service_->GetCombinedEligibility(base::BindOnce(
+          &IndigoPageActionController::ContinueInvoke,
+          invoke_weak_ptr_factory_.GetWeakPtr(), skip_glic_invoke));
     }
   }
 }
