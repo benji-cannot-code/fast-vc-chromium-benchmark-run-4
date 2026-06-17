08d35329bf63896b5fa04675e9d2fc7aec16bbfd
Henrique Nakashima <hnakashima@chromium.org>
2026-06-17 16:42:18 -0700

    [Android] Disable more failing Robolectric tests
    Bug: 525086744, 525122514, 525121740, 525121694, 525122375
    Change-Id: I8931ddf1080405cc8761bb003b0ac85afda4ff2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957188
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648676}

--- chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelperTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelperTest.java
@@ -20,6 +20,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -149,6 +150,7 @@ public class MediaSessionTabHelperTest {
         histogramWatcher.assertExpected();
     }
 
+    @DisabledTest(message = "crbug.com/525122375")
     @Test
     @DisableFeatures(MediaFeatureList.NO_PAUSE_MEDIA_ON_HEADPHONE_UNPLUG)
     public void testAudioBecomingNoisy_TimeToResumeMetric() {
@@ -209,6 +211,7 @@ public class MediaSessionTabHelperTest {
         histogramWatcher.assertExpected();
     }
 
+    @DisabledTest(message = "crbug.com/525122375")
     @Test
     public void testAudioBecomingNoisyReceiver_UnregistersWhenNoObservers() {
         AudioBecomingNoisyReceiver receiver = AudioBecomingNoisyReceiver.getInstance();
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionControllerTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionControllerTest.java
@@ -51,6 +51,7 @@ import org.chromium.base.supplier.SettableMonotonicObservableSupplier;
 import org.chromium.base.supplier.SettableNonNullObservableSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.RobolectricUtil;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -99,6 +100,7 @@ import java.util.concurrent.atomic.AtomicReference;
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 @DisableFeatures(ChromeFeatureList.CROSS_DEVICE_PREF_TRACKER_EXTRA_LOGS)
+@DisabledTest(message = "crbug.com/525121694")
 public class ToolbarPositionControllerTest {
 
     @Rule public MockitoRule mMockitoJUnit = MockitoJUnit.rule();
