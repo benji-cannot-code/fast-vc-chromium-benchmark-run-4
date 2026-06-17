2a6fb04053552308e44530f73e5f943719842ab8
Juan Mojica <juanmojica@google.com>
2026-06-16 17:28:39 -0700

    [contextual-tasks] Register contextual tasks private API extension
    This commit introduces the Contextual Tasks component extension, acting
    as a secure bridge between trusted web origins and the internal
    `chrome.contextualTasksPrivate` API.
    
    Specific changes include:
    * Registers the extension ID and manifest in the component allowlist.
    * Updates `ComponentLoader` to load the extension conditionally
      based on the `kApiContextualTasksPrivate` feature flag.
    * Configures the build system and resource generation (GRD/GN)
      to compile and bundle the new extension.
    * Implements the background service worker (`background.ts`) to listen for `getState` and `launchPanelInNewTab` messages from allowed `google.com` origins, forwarding valid requests to the private API.
    
    See bug for design doc and demo.
    
    Bug: b:517599045
    Change-Id: I44a84688fa51bbf57b20b4e1231ad7e815230834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7889984
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648005}

--- chrome/browser/extensions/component_extensions_allowlist/allowlist.cc
+++ chrome/browser/extensions/component_extensions_allowlist/allowlist.cc
@@ -34,6 +34,7 @@ namespace extensions {
 
 bool IsComponentExtensionAllowlisted(const std::string& extension_id) {
   constexpr auto kAllowed = base::MakeFixedFlatSet<std::string_view>({
+      extension_misc::kContextualTasksExtensionId,
       extension_misc::kGlicExtensionId,
       extension_misc::kInAppPaymentsSupportAppId,
       extension_misc::kPdfExtensionId,
@@ -90,6 +91,7 @@ bool IsComponentExtensionAllowlisted(int manifest_resource_id) {
     case IDR_HANGOUT_SERVICES_MANIFEST_V2:
     case IDR_HANGOUT_SERVICES_MANIFEST_V3:
 #endif
+    case IDR_CONTEXTUAL_TASKS_EXTENSION_MANIFEST:
     case IDR_GLIC_EXTENSION_MANIFEST:
     case IDR_NETWORK_SPEECH_SYNTHESIS_MANIFEST:
     case IDR_NETWORK_SPEECH_SYNTHESIS_MANIFEST_MV3:
---
+++ chrome/browser/resources/contextual_tasks/extension/contextual_tasks_private.d.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Definitions for chrome.contextualTasksPrivate API
+ * Generated from: chrome/common/extensions/api/contextual_tasks_private.webidl
+ * run `tools/json_schema_compiler/compiler.py
+ * chrome/common/extensions/api/contextual_tasks_private.webidl -g
+ * ts_definitions` to regenerate.
+ */
+
+
+
+declare namespace chrome {
+  export namespace contextualTasksPrivate {
+
+    export interface ProfileState {
+      isEligible: boolean;
+    }
+
+    export interface LaunchPanelInNewTabDetails {
+      targetUrl: string;
+      aimUrl: string;
+      documentId: string;
+    }
+
+    export function getState(documentId: string): Promise<ProfileState>;
+
+    export function launchPanelInNewTab(details: LaunchPanelInNewTabDetails):
+        Promise<void>;
+
+  }
+}
