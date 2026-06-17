affea5ea10d3404785123ab655740bb875094f21
Sirisha Kavuluru <skavuluru@google.com>
2026-06-16 17:00:28 -0700

    [TabStripTransition] Handle pending height change when app header changes before control container is laid out.
    Context:
    Tab strip height is set via TabStripTransitionCoordinator when (a) app header state changes or (b) control container layout changes. The signals are ignored when control container width or height is 0. As a result, if we get onAppheaderState change with valid values, we do not process this and instead wait for ControlContainer#onLayoutChange.
    
    However, it looks like this onLayoutChange might not get called sometimes and so tab strip height remains 0 even if another onAppHeaderStateChange comes in. From logs, it looks like we get 2 onAppHeaderStateChange events when this issue occurs and so the fix relies on marking the first call as "pending" and processing it in 2nd call. The control container bounds are updated when 2nd call comes in even though we do not get onLayoutChange signal.
    
    Landing this as speculative fix.
    
    Bug: 517186165
    Change-Id: Ifc94a7f35e8a487cf6e747465d2a5bc0bc157660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928410
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Auto-Submit: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1647986}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -9929,6 +9929,12 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kTipsSelfServiceDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(chrome::android::kTipsSelfService)},
 
+    {"tab-strip-height-transition-glitch-fix",
+     flag_descriptions::kTabStripHeightTransitionGlitchFixName,
+     flag_descriptions::kTabStripHeightTransitionGlitchFixDescription,
+     kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kTabStripHeightTransitionGlitchFix)},
+
 #endif  // BUILDFLAG(IS_ANDROID)
 
     {"data-sharing-debug-logs", flag_descriptions::kDataSharingDebugLogsName,
