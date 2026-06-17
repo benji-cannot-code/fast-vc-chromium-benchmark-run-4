0953940e067043e83af540cf9562e795dff80f8f
Hayato Ito <hayato@chromium.org>
2026-06-16 19:35:11 -0700

    Gardening Disable flaky ContextualTasksInteractiveUiTest.* tests
    Disable the following flaky tests on Linux and Chrome OS.
    
    - ContextualTasksInteractiveUiTest.AddAndRemoveImageChipFromComposebox
    - ContextualTasksInteractiveUiTest.AddAndRemovePdfChipFromComposebox
    - ContextualTasksInteractiveUiTest.AddAndRemoveTabFromComposebox
    
    Bug: 524797987
    Change-Id: I3ae100b9eca9c7e09ba687fbe5c73a0a333af952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7952298
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648042}

--- chrome/browser/contextual_tasks/contextual_tasks_interactive_uitest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_interactive_uitest.cc
@@ -878,8 +878,16 @@ class ContextualTasksInteractiveUiTest : public InteractiveBrowserTest {
 };
 
 // TODO(crbug.com/500717050): Parameterize this test suite on the feature flag.
+// TODO(crbug.com/524797987): Re-enable this test.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_AddAndRemovePdfChipFromComposebox \
+  DISABLED_AddAndRemovePdfChipFromComposebox
+#else
+#define MAYBE_AddAndRemovePdfChipFromComposebox \
+  AddAndRemovePdfChipFromComposebox
+#endif
 IN_PROC_BROWSER_TEST_F(ContextualTasksInteractiveUiTest,
-                       AddAndRemovePdfChipFromComposebox) {
+                       MAYBE_AddAndRemovePdfChipFromComposebox) {
   const GURL kInterceptionUrl("https://www.google.com/search?udm=50");
 
   base::FilePath test_data_dir;
@@ -926,8 +934,16 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksInteractiveUiTest,
                   WaitForComposeboxFilesCount(0));
 }
 
+// TODO(crbug.com/524797987): Re-enable this test.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_AddAndRemoveImageChipFromComposebox \
+  DISABLED_AddAndRemoveImageChipFromComposebox
+#else
+#define MAYBE_AddAndRemoveImageChipFromComposebox \
+  AddAndRemoveImageChipFromComposebox
+#endif
 IN_PROC_BROWSER_TEST_F(ContextualTasksInteractiveUiTest,
-                       AddAndRemoveImageChipFromComposebox) {
+                       MAYBE_AddAndRemoveImageChipFromComposebox) {
   const GURL kInterceptionUrl("https://www.google.com/search?udm=50");
 
   base::FilePath test_data_dir;
@@ -964,8 +980,15 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksInteractiveUiTest,
                   WaitForComposeboxFilesCount(0));
 }
 
+// TODO(crbug.com/524797987): Re-enable this test.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_AddAndRemoveTabFromComposebox \
+  DISABLED_AddAndRemoveTabFromComposebox
+#else
+#define MAYBE_AddAndRemoveTabFromComposebox AddAndRemoveTabFromComposebox
+#endif
 IN_PROC_BROWSER_TEST_F(ContextualTasksInteractiveUiTest,
-                       AddAndRemoveTabFromComposebox) {
+                       MAYBE_AddAndRemoveTabFromComposebox) {
   const GURL kInterceptionUrl("https://www.google.com/search?udm=50");
   const GURL kGenericPageUrl = embedded_test_server()->GetURL("/title1.html");
 
