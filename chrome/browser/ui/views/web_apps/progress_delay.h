09aaba32c7c939f6830c798e6a029507a76eae10
Adriana Ixba <aixba@chromium.org>
2026-06-16 17:28:04 -0700

    [PWA_Install_Dialog] Simplify progress delay definition for tests
    This sets the delay_time to 0 by default for tests. This simplifies
    testing the install dialog as we are starting to enable by default for
    tests.
    
    part of splitting of
    https://chromium-review.git.corp.google.com/c/chromium/src/+/7892106
    
    Bug: 518794833
    Change-Id: I7869aab08e37f0e1a63356860156564c58c4b43e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7952055
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Adriana Ixba <aixba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648004}

--- chrome/browser/ui/views/web_apps/progress_delay.h
+++ chrome/browser/ui/views/web_apps/progress_delay.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "base/auto_reset.h"
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -30,8 +29,6 @@ class ProgressDelay {
 
   void DelayComplete();
 
-  static base::AutoReset<std::optional<base::TimeDelta>>
-  SetDurationOverrideForTesting(std::optional<base::TimeDelta> duration);
 
  private:
   void OnTimerTick();
--- chrome/browser/ui/views/web_apps/web_app_install_flow_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_install_flow_browsertest.cc
@@ -58,9 +58,7 @@ namespace {
 
 class WebAppInstallFlowBrowserTest : public WebAppBrowserTestBase {
  public:
-  WebAppInstallFlowBrowserTest()
-      : progress_delay_override_(ProgressDelay::SetDurationOverrideForTesting(
-            base::Milliseconds(0))) {}
+  WebAppInstallFlowBrowserTest() = default;
 
   ~WebAppInstallFlowBrowserTest() override = default;
 
@@ -145,7 +143,6 @@ class WebAppInstallFlowBrowserTest : public WebAppBrowserTestBase {
   }
 
  private:
-  base::AutoReset<std::optional<base::TimeDelta>> progress_delay_override_;
   base::test::ScopedFeatureList feature_list_{features::kWebAppInstallDialog};
 };
 
