bbf17b076a6f40ace88276597d3def8c18a7c401
James Sun <jamessun@google.com>
2026-06-17 12:10:36 -0700

    [BNPL] Add AutofillBnplCoordinator skeleton for settings subpage
    This CL adds the basic coordinator skeleton for the settings subpage,
    including:
    - AutofillBnplCoordinator: Coordinates transitions to/from the subpage.
    - AutofillBnplCoordinatorDelegate: Delegate protocol for coordinator
    parent.
    
    Bug: 517646489
    Change-Id: Id02572984c1bb0ffa40eaba74ba59c6b050d1c97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7913881
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648503}

---
+++ ios/chrome/browser/settings/autofill/bnpl/coordinator/autofill_bnpl_coordinator_delegate.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_BNPL_COORDINATOR_AUTOFILL_BNPL_COORDINATOR_DELEGATE_H_
+#define IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_BNPL_COORDINATOR_AUTOFILL_BNPL_COORDINATOR_DELEGATE_H_
+
+@class AutofillBnplCoordinator;
+
+// Delegate for AutofillBnplCoordinator.
+@protocol AutofillBnplCoordinatorDelegate
+
+// Informs the delegate that the coordinator wants to be stopped.
+- (void)autofillBnplCoordinatorWantsToBeStopped:
+    (AutofillBnplCoordinator*)coordinator;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_BNPL_COORDINATOR_AUTOFILL_BNPL_COORDINATOR_DELEGATE_H_
