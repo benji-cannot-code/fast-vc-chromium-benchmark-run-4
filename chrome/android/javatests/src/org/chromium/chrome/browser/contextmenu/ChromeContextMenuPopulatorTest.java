c26b617c0db1f199df269be075ecf240e803f8ee
Ryo Hashimoto <hashimoto@google.com>
2026-06-16 19:47:16 -0700

    Revert "[context menu] remove page navigation items"
    This reverts commit 5e1ba21b4d5c4e6eab7ab5aa8918ff024a6cf7ef.
    
    Reason for revert: Removed code is needed for b/523029758
    
    Original change's description:
    > [context menu] remove page navigation items
    >
    > Per UX review, the page navigation items have low usage and should be
    > removed to provide a cleaner context menu.
    >
    > Bug: 391719844
    > Test: tools/autotest.py -C out/Default ChromeContextMenuPopulatorTest
    > Test: https://screenshot.googleplex.com/5xgSuT8U2hBKfhr
    > Change-Id: I39588b3558226a19f00704949ae2bbbda17f62ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527871
    > Auto-Submit: Grace Cham <hscham@chromium.org>
    > Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    > Reviewed-by: Sinan Sahin <sinansahin@google.com>
    > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1459283}
    
    Bug: 391719844
    Change-Id: Icb8c545df705fbce752bfad6c4a57987a4c73790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928669
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648046}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulatorTest.java
@@ -2518,6 +2518,9 @@ public class ChromeContextMenuPopulatorTest {
 
         int[][] expected = {
             {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
                 R.id.contextmenu_save_page,
                 R.id.contextmenu_share_page,
                 R.id.contextmenu_print_page,
@@ -2540,7 +2543,14 @@ public class ChromeContextMenuPopulatorTest {
         ContextMenuParams params = getPageParams();
 
         int[][] expected = {
-            {R.id.contextmenu_save_page, R.id.contextmenu_share_page, R.id.contextmenu_print_page},
+            {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
+                R.id.contextmenu_save_page,
+                R.id.contextmenu_share_page,
+                R.id.contextmenu_print_page
+            },
         };
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
@@ -2556,7 +2566,14 @@ public class ChromeContextMenuPopulatorTest {
         ContextMenuParams params = getPageParams();
 
         int[][] expected = {
-            {R.id.contextmenu_save_page, R.id.contextmenu_share_page, R.id.contextmenu_print_page},
+            {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
+                R.id.contextmenu_save_page,
+                R.id.contextmenu_share_page,
+                R.id.contextmenu_print_page
+            },
         };
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
@@ -2575,7 +2592,14 @@ public class ChromeContextMenuPopulatorTest {
         ContextMenuParams params = getPageParams();
 
         int[][] expected = {
-            {R.id.contextmenu_save_page, R.id.contextmenu_share_page, R.id.contextmenu_print_page},
+            {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
+                R.id.contextmenu_save_page,
+                R.id.contextmenu_share_page,
+                R.id.contextmenu_print_page
+            },
         };
 
         initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
@@ -2600,12 +2624,60 @@ public class ChromeContextMenuPopulatorTest {
     @Test
     @SmallTest
     @UiThreadTest
+    public void testPageNavigation() {
+        FirstRunStatus.setFirstRunFlowComplete(true);
+        ContextMenuParams params = getPageParams();
+
+        int[][] expected = {
+            {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
+                R.id.contextmenu_save_page,
+                R.id.contextmenu_share_page,
+                R.id.contextmenu_print_page
+            },
+        };
+
+        // All items are present and enabled.
+        initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
+        checkMenuOptions(expected);
+
+        // Only back is disabled.
+        when(mItemDelegate.canCurrentTabGoBack()).thenReturn(false);
+        List<Integer> expectedDisabled = Arrays.asList(R.id.contextmenu_back);
+        initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
+        checkMenuOptions(expectedDisabled, expected);
+
+        // Both back and forward are disabled.
+        when(mItemDelegate.canCurrentTabGoForward()).thenReturn(false);
+        expectedDisabled = Arrays.asList(R.id.contextmenu_back, R.id.contextmenu_forward);
+        initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
+        checkMenuOptions(expectedDisabled, expected);
+
+        // Only forward is disabled.
+        when(mItemDelegate.canCurrentTabGoBack()).thenReturn(true);
+        expectedDisabled = Arrays.asList(R.id.contextmenu_forward);
+        initializePopulator(ChromeContextMenuPopulator.ContextMenuMode.NORMAL, params);
+        checkMenuOptions(expectedDisabled, expected);
+    }
+
+    @Test
+    @SmallTest
+    @UiThreadTest
     public void testPageWithDevMenu() {
         setAllMandatoryFlowsComplete();
         ContextMenuParams params = getPageParams();
 
         int[][] expected = {
-            {R.id.contextmenu_save_page, R.id.contextmenu_share_page, R.id.contextmenu_print_page},
+            {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
+                R.id.contextmenu_save_page,
+                R.id.contextmenu_share_page,
+                R.id.contextmenu_print_page
+            },
             {R.id.contextmenu_view_page_source, R.id.contextmenu_inspect_element},
         };
 
@@ -2647,7 +2719,12 @@ public class ChromeContextMenuPopulatorTest {
         DownloadUtils.setIsDownloadRestrictedByPolicyForTesting(true);
 
         int[] expectedPage = {
-            R.id.contextmenu_save_page, R.id.contextmenu_share_page, R.id.contextmenu_print_page
+            R.id.contextmenu_back,
+            R.id.contextmenu_forward,
+            R.id.contextmenu_reload,
+            R.id.contextmenu_save_page,
+            R.id.contextmenu_share_page,
+            R.id.contextmenu_print_page
         };
         List<Integer> expectedDisabled = Arrays.asList(R.id.contextmenu_save_page);
 
@@ -2676,7 +2753,13 @@ public class ChromeContextMenuPopulatorTest {
         ContextMenuParams params = getPageParams();
 
         int[][] expected = {
-            {R.id.contextmenu_save_page, R.id.contextmenu_share_page},
+            {
+                R.id.contextmenu_back,
+                R.id.contextmenu_forward,
+                R.id.contextmenu_reload,
+                R.id.contextmenu_save_page,
+                R.id.contextmenu_share_page
+            },
         };
 
         initializePopulator(
