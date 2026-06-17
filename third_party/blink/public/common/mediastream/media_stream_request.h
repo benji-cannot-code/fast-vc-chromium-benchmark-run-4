3912d64f504a8806098488ed727c98091de95dab
Syed AbuTalib <lowkey@google.com>
2026-06-17 13:38:18 -0700

    Pass ChannelLayoutConfig by value in MediaStream
    Given how small the class ChannelLayoutConfig, it is more efficient to
    pass by value instead.
    
    Bug: 483461268
    Change-Id: Ieb4e41ac813ed6b2bc1da898e573192b90fcca13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7954161
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648552}

--- third_party/blink/public/common/mediastream/media_stream_request.h
+++ third_party/blink/public/common/mediastream/media_stream_request.h
@@ -71,7 +71,7 @@ struct BLINK_COMMON_EXPORT MediaStreamDevice {
                     const std::string& id,
                     const std::string& name,
                     int sample_rate,
-                    const media::ChannelLayoutConfig& channel_layout_config,
+                    media::ChannelLayoutConfig channel_layout_config,
                     int frames_per_buffer);
   MediaStreamDevice(const MediaStreamDevice& other);
   ~MediaStreamDevice();
