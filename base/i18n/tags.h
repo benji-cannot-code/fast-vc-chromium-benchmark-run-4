5137485438bbfafb52cde6113a43de2136307442
Danilo Tedeschi <danft@google.com>
2026-06-17 14:03:42 -0700

    Add undefined through language_tag::UNDEFINED()
    Change-Id: I4e8512337489f8c60210a469b12bff35db11c1bd
    Bug: 506198296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957963
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Reviewed-by: Paula Gómez <paugomez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648574}

--- base/i18n/tags.h
+++ base/i18n/tags.h
@@ -26,6 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // `base/i18n/internal/canonical_language_tags.inc`.
 namespace base::language_tags {
 
+// Returns a undefined language tag.
+BASE_I18N_EXPORT const base::LanguageTag& UNDEFINED();
+
 #define IMPL_LANGUAGECODE_TAG_NAME(tag, name) \
   BASE_I18N_EXPORT const base::LanguageTag& name();
 
