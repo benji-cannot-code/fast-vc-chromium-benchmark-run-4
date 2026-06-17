578510c7787433f582507a15831ce767f66fa30b
manukh <manukh@chromium.org>
2026-06-17 10:31:48 -0700

    [omnibox][3p-aim] Remove kAiModeOmniboxEntryPoint feature guards
    The feature launched in m149. There are no active experiments disabling
    the feature or mutating its params.
    
    This CL removed all code sites checking the feature state or its param
    values.
    
    CL written by AI agent, but I reviewed it.
    
    TAG=agy CONV=aed4f1ea-acf7-4052-a94d-0e0d9350ae19
    
    Bug: b:524849472 , b:441102004
    Change-Id: I9f1d35d0913b9fbfe1e5c28532cdfe847b9a6a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951050
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648424}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -6437,10 +6437,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kLensOverlayOmniboxEntryPointDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(lens::features::kLensOverlayOmniboxEntryPoint)},
 
-    {"ai-mode-omnibox-entry-point",
-     flag_descriptions::kAiModeOmniboxEntryPointName,
-     flag_descriptions::kAiModeOmniboxEntryPointDescription, kOsDesktop,
-     FEATURE_VALUE_TYPE(omnibox::kAiModeOmniboxEntryPoint)},
 
     {"hide-aim-omnibox-entrypoint-on-user-input",
      flag_descriptions::kHideAimOmniboxEntrypointOnUserInputName,
