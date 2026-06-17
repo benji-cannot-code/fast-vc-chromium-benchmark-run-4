5cafc112c98fa8e557e7bb474ea780a5a1323647
James Sun <jamessun@google.com>
2026-06-17 12:09:23 -0700

    [BNPL] Move settings subpage VC to new bnpl/ui directory
    This CL moves settings page AutofillBnplTableViewController and
    AutofillBnplConsumer files from icb/settings/ui_bundled/autofill/ to
    the new directory icb/settings/autofill/bnpl/ui/ in order to
    separate coordinator and ui components cleanly.
    
    Bug: 517646489
    Change-Id: Id221ce57a9455313580781e4e07a2bcb6f649242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7953877
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648501}

--- ios/chrome/browser/settings/autofill/bnpl/ui/autofill_bnpl_table_view_controller.h
+++ ios/chrome/browser/settings/autofill/bnpl/ui/autofill_bnpl_table_view_controller.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_AUTOFILL_AUTOFILL_BNPL_TABLE_VIEW_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_AUTOFILL_AUTOFILL_BNPL_TABLE_VIEW_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_BNPL_UI_AUTOFILL_BNPL_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_BNPL_UI_AUTOFILL_BNPL_TABLE_VIEW_CONTROLLER_H_
 
-#import "ios/chrome/browser/settings/ui_bundled/autofill/autofill_bnpl_consumer.h"
+#import "ios/chrome/browser/settings/autofill/bnpl/ui/autofill_bnpl_consumer.h"
 #import "ios/chrome/browser/settings/ui_bundled/settings_root_table_view_controller.h"
 
 @class AutofillBnplTableViewController;
@@ -28,4 +28,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_SETTINGS_UI_BUNDLED_AUTOFILL_AUTOFILL_BNPL_TABLE_VIEW_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_BNPL_UI_AUTOFILL_BNPL_TABLE_VIEW_CONTROLLER_H_
--- ios/chrome/browser/settings/autofill/bnpl/ui/autofill_bnpl_table_view_controller.mm
+++ ios/chrome/browser/settings/autofill/bnpl/ui/autofill_bnpl_table_view_controller.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/settings/ui_bundled/autofill/autofill_bnpl_table_view_controller.h"
+#import "ios/chrome/browser/settings/autofill/bnpl/ui/autofill_bnpl_table_view_controller.h"
 
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/grit/ios_strings.h"
