aa55419daeac0f42e7c20b05b80ad0b54b4b0809
Azamat Myrzabekov <azamatmz@google.com>
2026-06-17 06:33:56 -0700

    [DBD] Add feature flag for removing password from DBD Clank
    Bug: 524163258
    Change-Id: I391765e71c71d2e0730a3e4fedf8643469ab209e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7950937
    Commit-Queue: Azamat Myrzabekov <azamatmz@google.com>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648265}

--- components/browsing_data/core/features.cc
+++ components/browsing_data/core/features.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace browsing_data::features {
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kBrowsingDataModel, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kDbdPasswordRemovalOnAndroid, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
