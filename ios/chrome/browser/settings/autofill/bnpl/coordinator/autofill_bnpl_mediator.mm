4f53fafaba6583e21fbe9c21383e364453f95a2b
James Sun <jamessun@google.com>
2026-06-17 12:09:45 -0700

    [BNPL] Add AutofillBnplMediator skeleton for settings subpage
    This CL adds mediator skeleton code for the settings subpage, including:
    - AutofillBnplMediator: Connects the data store to the subpage.
    
    Bug: 517646489
    Change-Id: I5a6bf38f65c31d24a3266c7b72f079f22edcc761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7912566
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648502}

---
+++ ios/chrome/browser/settings/autofill/bnpl/coordinator/autofill_bnpl_mediator.mm
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/settings/autofill/bnpl/coordinator/autofill_bnpl_mediator.h"
+
+#import "base/check.h"
+#import "components/autofill/core/browser/data_manager/personal_data_manager.h"
+#import "components/prefs/pref_service.h"
+
+@implementation AutofillBnplMediator {
+  raw_ptr<autofill::PersonalDataManager> _personalDataManager;
+  raw_ptr<PrefService> _prefs;
+}
+
+- (instancetype)initWithPersonalDataManager:
+                    (autofill::PersonalDataManager*)personalDataManager
+                                prefService:(PrefService*)prefService {
+  self = [super init];
+  if (self) {
+    CHECK(personalDataManager);
+    CHECK(prefService);
+    _personalDataManager = personalDataManager;
+    _prefs = prefService;
+  }
+  return self;
+}
+
+- (void)disconnect {
+  _personalDataManager = nullptr;
+  _prefs = nullptr;
+}
+
+@end
