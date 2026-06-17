b1c0fc5ae980a680503372f411ae54f474ff0c34
Mitsuru Oshima <oshima@chromium.org>
2026-06-17 00:11:54 -0700

    Ensure WidgetObserver::OnWidgetDestroying is only called once
    This CL adds a check in Widget::HandleWidgetDestroying to make
    sure it's only called once, and before OnWidgetDestroyed. To
    prevent triggering this check, the unconditional call in ~Widget
    is now guarded.
    
    Bug: 524772497
    Change-Id: I639a85d14a4cd0ca2b743d65858568a5410f798a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7953721
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648114}

--- ui/views/widget/widget.h
+++ ui/views/widget/widget.h
@@ -1714,6 +1714,9 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
   // Set to true if the widget is in the process of being destroyed.
   bool is_destroying_ = false;
 
+  // Set to true after HandleWidgetDestroying called.
+  bool widget_destroying_ = false;
+
   // Set to true after OnWidgetDestroyed called.
   bool native_widget_destroyed_ = false;
 
