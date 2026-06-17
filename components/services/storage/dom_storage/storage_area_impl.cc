3a2f6eff617fd44c032107ba37c52afa243e343d
Steve Becker <stevebe@microsoft.com>
2026-06-17 16:17:35 -0700

    [DomStorage] Fix DCHECK during shutdown
    For both `LocalStorageImpl` and `SessionStorageImpl`, when `Shutdown()`
    calls `CancelAllPendingRequests()`, this fails the following `DCHECK`
    when pending requests exist:
    
    > StorageArea::GetAllCallback was destroyed without first either being
    > run or its corresponding binding being closed. It is an error to drop > response callbacks which still correspond to an open interface pipe.
    
    Since both `LocalStorageImpl` and `SessionStorageImpl` call `Shutdown()`
    from the destructor, calling `CancelAllPendingRequests()` is not
    necessary. The destructor will also destruct all storage areas,
    including each storage area's pending requests.
    
    The fix for the DCHECK updates `StorageAreaImpl` by moving the
    declaration of `receivers_` to after `on_load_complete_tasks_`. This
    makes `receivers_` destruct before `on_load_complete_tasks_`, avoiding
    the `DCHECK`.
    
    The change also consolidates the `ShutdownDroppedChanges` histograms to
    record in a single location, the destructor of `StorageAreaImpl`,
    instead of recoding in `Shutdown()` for both `LocalStorageImpl` and
    `SessionStorageImpl`.
    
    This change adds unit test coverage that triggers the `DCHECK` during
    shutdown. This change adds a new helper
    `StorageAreaImpl::SetLoadingStartedCallbackForTesting()`. The helper
    enables the tests to shutdown `LocalStorageImpl` and
    `SessionStorageImpl` while a storage area is loading, exercising the
    DCHECK above. The change also updates an existing test,
    `ShutdownDroppedChanges`, to use the new helper, which enables deletion
    of the now unused `PutValueForTesting()`.
    
    Change-Id: Id59d71a558e376fbfe739c460b8723154ed9a597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7851342
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1648666}

--- components/services/storage/dom_storage/storage_area_impl.cc
+++ components/services/storage/dom_storage/storage_area_impl.cc
@@ -7,15 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/containers/span.h"
 #include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/process_memory_dump.h"
-#include "third_party/leveldatabase/env_chromium.h"
+#include "third_party/abseil-cpp/absl/strings/str_format.h"
 
 namespace storage {
 
@@ -86,9 +84,25 @@ StorageAreaImpl::StorageAreaImpl(
 }
 
 StorageAreaImpl::~StorageAreaImpl() {
-  DCHECK(!has_pending_load_tasks());
-  if (commit_batch_)
+  // For local storage, the map ID is unknown where `database_` must use the
+  // area's storage key to look up the map ID.
+  bool is_session_storage = map_locator_->map_id().has_value();
+
+  // Record data loss, which happens when this storage area destructs before
+  // persisting changes to `database_`.
+  //
+  // TODO(crbug.com/503422295): Monitor this histogram and if dropping changes
+  // is common then handle that here.
+  std::string histogram_name =
+      absl::StrFormat("Storage.%s.ShutdownDroppedChanges",
+                      is_session_storage ? "SessionStorage" : "LocalStorage");
+  base::UmaHistogramBoolean(histogram_name,
+                            has_pending_load_read_write_tasks());
+
+  if (commit_batch_) {
     CommitChanges();
+  }
+
   if (database_) {
     database_->RemoveCommitter(this);
   }
@@ -96,7 +110,11 @@ StorageAreaImpl::~StorageAreaImpl() {
 
 void StorageAreaImpl::InitializeAsEmpty() {
   DCHECK_EQ(map_state_, MapState::UNLOADED);
+
   map_state_ = MapState::LOADING_FROM_DATABASE;
+  if (loading_started_callback_for_testing_) {
+    loading_started_callback_for_testing_.Run();
+  }
   OnMapLoaded(ValueMap());
 }
 
@@ -138,10 +156,6 @@ std::unique_ptr<StorageAreaImpl> StorageAreaImpl::ForkToNewMap(
         AccessMode::ReadWrite));
   }
   return forked_area;
-}
-
-void StorageAreaImpl::CancelAllPendingRequests() {
-  on_load_complete_tasks_.clear();
 }
 
 bool StorageAreaImpl::has_pending_load_read_write_tasks() const {
@@ -226,6 +240,11 @@ void StorageAreaImpl::PurgeMemory() {
 
 void StorageAreaImpl::SetCacheModeForTesting(CacheMode cache_mode) {
   SetCacheMode(cache_mode);
+}
+
+void StorageAreaImpl::SetLoadingStartedCallbackForTesting(
+    base::RepeatingClosure callback) {
+  loading_started_callback_for_testing_ = callback;
 }
 
 void StorageAreaImpl::AddObserver(
@@ -577,6 +596,9 @@ void StorageAreaImpl::LoadMap(OnLoadCompleteTask completion_task) {
   }
 
   map_state_ = MapState::LOADING_FROM_DATABASE;
+  if (loading_started_callback_for_testing_) {
+    loading_started_callback_for_testing_.Run();
+  }
 
   if (!database_) {
     OnMapLoaded(
