8aeb3f8f1df53ace866ca2295274cd3c1ccb665e
Morten Stenshorne <mstensho@chromium.org>
2026-06-17 13:55:59 -0700

    BlockChildIterator: split out IFC logic.
    Untangle inline break token logic and block break token logic. They are
    quite different.
    
    Only use nodes from block break tokens, never from inline break tokens.
    If it's an inline break token, the node to lay out is always the
    one established by the container.
    
    Allows for some stronger typing, and eliminates the need for storing a
    node in inline break tokens (a separate CL is already in progress).
    
    No behavior changes intended.
    
    Change-Id: I309ce80ba50d1de345592a477338a73cf38ac7ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957397
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648566}

--- third_party/blink/renderer/core/layout/block_node.cc
+++ third_party/blink/renderer/core/layout/block_node.cc
@@ -1117,7 +1117,7 @@ MinMaxSizesResult BlockNode::ComputeMinMaxSizes(
   return *result;
 }
 
-LayoutInputNode BlockNode::NextSibling() const {
+BlockNode BlockNode::NextBlockSibling() const {
   LayoutObject* next_sibling = box_->NextSibling();
 
   // We may have some LayoutInline(s) still within the tree (due to treating
