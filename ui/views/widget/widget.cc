42dcd124c6b1806091c705231efb015a043f7d54
Ankush Singh <ankushkush@google.com>
2026-06-17 02:17:34 -0700

    Revert "Ensure WidgetObserver::OnWidgetDestroying is only called once"
    This reverts commit 41b2d8f9dd8212b7c02675c6c0fd8f25b0cec937.
    
    Reason for revert: Causing CHECK failures on mac15-x64-rel-tests
    
    Builder: mac15-x64-rel-tests
    https://ci.chromium.org/p/chromium/builders/ci/mac15-x64-rel-tests
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/mac15-x64-rel-tests/b8678764114055188897
    
    Original change's description:
    > Ensure WidgetObserver::OnWidgetDestroying is only called once
    >
    > This CL adds a check in Widget::HandleWidgetDestroying to make
    > sure it's only called once, and before OnWidgetDestroyed. To
    > prevent triggering this check, the unconditional call in ~Widget
    > is now guarded.
    >
    > Bug: 524772497
    > Change-Id: I639a85d14a4cd0ca2b743d65858568a5410f798a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7953721
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1648114}
    
    Bug: 524772497
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie484b909e3cec5190d7b07a8482347690e94466f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7953058
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648171}

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -302,23 +302,16 @@ Widget::~Widget() {
       native_widget_->ClientDestroyedWidget();
     }
 
-    if (!widget_destroying_) {
-      HandleWidgetDestroying();
-    }
+    HandleWidgetDestroying();
     if (native_widget_) {
       native_widget_->Close();
     }
 
-    if (!native_widget_destroyed_) {
-      HandleWidgetDestroyed();
-    }
+    HandleWidgetDestroyed();
     if (widget_delegate_) {
       widget_delegate_->WidgetDestroying();
     }
   }
-
-  CHECK(widget_destroying_);
-  CHECK(native_widget_destroyed_);
 
   RemoveObserver(&root_view_->GetViewAccessibility());
   // Destroy RootView after the native widget, so in case the WidgetDelegate is
@@ -2844,9 +2837,9 @@ void Widget::HandleShowRequested() {
 }
 
 void Widget::HandleWidgetDestroying() {
-  CHECK(!native_widget_destroyed_);
-  CHECK(!widget_destroying_);
-  widget_destroying_ = true;
+  if (native_widget_destroyed_) {
+    return;
+  }
   ClearFocusManagerFromWidget();
   if (parent_) {
     parent_->OnChildRemoved(this);
@@ -2861,7 +2854,6 @@ void Widget::HandleWidgetDestroying() {
 }
 
 void Widget::HandleWidgetDestroyed() {
-  CHECK(widget_destroying_);
   if (native_widget_destroyed_) {
     return;
   }
