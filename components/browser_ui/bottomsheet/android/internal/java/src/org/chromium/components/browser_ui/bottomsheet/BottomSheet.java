92f1363bc292da4c7e4e5d35a7da16fb96eabf80
Yanting Yu <yyanting@google.com>
2026-06-17 15:25:45 -0700

    [a11y] Set TalkBack auto-focused on bottom sheet when open.
    In this CL, we
    * Revert changes in https://crrev.com/c/7830577. We restore
    the behaviour of setting the accessibility pane title on the sheet
    container, instead of the bottom sheet itself.
    * Move the updateA11yPaneTitle() from setInternalCurrentState() to setSheetState(), which prevents a race condition where TalkBack announces the stale title or incorrectly shifts focus to the tab strip instead of the bottom sheet.
    * Send CONTENT_CHANGE_TYPE_PANE_APPEARED accessibility event when sheet opens.
    
    Demo: http://shortn/_wBkfxlCOaY
    
    Bug: 502655377
    Change-Id: I360bdddb1c0b76ff126164134334d2135124263c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7944304
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Yanting Yu <yyanting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648631}

--- components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheet.java
+++ components/browser_ui/bottomsheet/android/internal/java/src/org/chromium/components/browser_ui/bottomsheet/BottomSheet.java
@@ -18,6 +18,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.FrameLayout;
 
 import androidx.annotation.ColorInt;
@@ -776,6 +777,7 @@ class BottomSheet extends FrameLayout
         mIsSheetOpen = true;
 
         for (BottomSheetObserver o : mObservers) o.onSheetOpened(reason);
+        sendPaneChangeAccessibilityEvent(true);
     }
 
     /**
@@ -802,6 +804,7 @@ class BottomSheet extends FrameLayout
         setFocusable(false);
         setFocusableInTouchMode(false);
         setContentDescription(null);
+        sendPaneChangeAccessibilityEvent(false);
     }
 
     /** Cancels and nulls the height animation if it exists. */
@@ -1184,6 +1187,15 @@ class BottomSheet extends FrameLayout
 
         cancelAnimation();
         mTargetState = state;
+        if (getCurrentSheetContent() != null
+                && (state == SheetState.HALF || state == SheetState.FULL)) {
+            @StringRes
+            int resId =
+                    state == SheetState.FULL
+                            ? getCurrentSheetContent().getSheetFullHeightAccessibilityStringId()
+                            : getCurrentSheetContent().getSheetHalfHeightAccessibilityStringId();
+            updateA11yPaneTitle(getResources().getString(resId));
+        }
 
         if (animate
                 && (state != mCurrentState
@@ -1260,12 +1272,6 @@ class BottomSheet extends FrameLayout
 
         if (mCurrentState == SheetState.HALF || mCurrentState == SheetState.FULL) {
             assumeNonNull(getCurrentSheetContent());
-            @StringRes
-            int resId =
-                    mCurrentState == SheetState.FULL
-                            ? getCurrentSheetContent().getSheetFullHeightAccessibilityStringId()
-                            : getCurrentSheetContent().getSheetHalfHeightAccessibilityStringId();
-            updateA11yPaneTitle(getResources().getString(resId));
 
             // TalkBack will announce the content description if it has changed, so wait to set the
             // content description until after announcing full/half height.
@@ -1839,7 +1845,20 @@ class BottomSheet extends FrameLayout
     }
 
     private void updateA11yPaneTitle(CharSequence msg) {
-        ViewCompat.setAccessibilityPaneTitle(this, msg);
+        // Set the pane title for the container. The bottom sheet view is not always
+        // accessible e.g. when sheet is dismissed.
+        ViewCompat.setAccessibilityPaneTitle(mSheetContainer, msg);
+    }
+
+    private void sendPaneChangeAccessibilityEvent(boolean isShowing) {
+        AccessibilityEvent event =
+                AccessibilityEvent.obtain(AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED);
+        if (isShowing) {
+            event.setContentChangeTypes(AccessibilityEvent.CONTENT_CHANGE_TYPE_PANE_APPEARED);
+        } else {
+            event.setContentChangeTypes(AccessibilityEvent.CONTENT_CHANGE_TYPE_PANE_DISAPPEARED);
+        }
+        AccessibilityState.sendAccessibilityEvent(event);
     }
 
     private void resetCachedKeyboardState() {
