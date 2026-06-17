75b0bdf2a2e091914511663620441f13bf27ebe2
Atharv Maan <atharvmaan@chromium.org>
2026-06-17 13:04:51 -0700

    [PEH] Add PEH Delegated Task Framework
    This CL lays the foundation for the PEH Delegated Task Framework.
    
    Bug: 525018563
    Change-Id: I9d85463ebec96c30f824203de058c8524cb33073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7956995
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648535}

---
+++ chrome/browser/platform_experience/delegated_tasks/delegated_task.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/platform_experience/delegated_tasks/delegated_task.h"
+
+#include "base/time/time.h"
+
+namespace platform_experience {
+
+base::TimeDelta DelegatedTask::GetTimeout() const {
+  return base::Seconds(10);
+}
+
+}  // namespace platform_experience
