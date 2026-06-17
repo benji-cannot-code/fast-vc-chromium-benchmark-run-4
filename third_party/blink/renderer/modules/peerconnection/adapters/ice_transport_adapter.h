205893722f8688a164690d586692986f788026df
Kent Tamura <tkent@chromium.org>
2026-06-17 01:47:57 -0700

    Enable -Wshorten-64-to-32 in modules/peerconnection/
    * adapters/ice_transport_adapter.h
      Disable -Wshorten-64-to-32 for third_party/webrtc/, which is not
      ready for it.
    
    * peer_connection_tracker.cc
      Apply static_cast<> because this narrowing won't cause security/crash
      issues.
    
    Bug: 514751964
    Change-Id: Ib1de61f90d111cf74122bd7948c373876be0760b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7954338
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1648152}

--- third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter.h
+++ third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter.h
@@ -10,7 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
+
+// third_party/webrtc is not ready for -Wshorten-64-to-32.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wshorten-64-to-32"
 #include "third_party/webrtc/p2p/base/p2p_transport_channel.h"
+#pragma clang diagnostic pop
 
 namespace blink {
 
