bbe7d3db7f49c25c7a5f6bba3262f3a24b0d5faf
Justin Novosad <junov@chromium.org>
2026-06-17 06:59:58 -0700

    [ios][tab_strip] Fix failing drag and drop tests on iOS 27
    This CL fixes flakiness and hangs in TabStripGroupTestCase and
    TabStripDragDropTestCase EarlGrey tests on iPad simulators using Chrome
    builds built with the iOS 27 SDK.
    
    Problem:
    The tests used duplicated local XCUI helper implementations of
    DragDropTabStripTabCellInTabStripView and GetElementMatchingIdentifier
    which queried cells using app.windows.firstMatch. On iOS 27, when a
    keyboard is displayed or recently dismissed, the first window in the
    list is the keyboard window (_UIKeyboardInputSceneSecureWindow), which
    contains no tab strip collection view cells. This caused XCUI to wait
    indefinitely for the cells to appear in the keyboard window,
    eventually causing the test to timeout and report that the app was
    hanging.
    
    Fix:
    Replaced the duplicated local helper methods in both
    tab_strip_group_egtest.mm and tab_strip_drag_drop_egtest.mm with the
    shared, robust test utility
    chrome_test_util::LongPressCellAndDragToOffsetOf. This utility queries
    cells relative to the scene-specific window ID (resolved via
    GetWindowAccessibilityIdentifier(window_number)) instead of
    firstMatch, avoiding the keyboard window overlay issue.
    
    Bug: 374897082, 523152429, 523153225
    Change-Id: I0618a50b99311708de61e33909ff0d3c8ea75c15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7950700
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648271}

--- ios/chrome/browser/tab_switcher/tab_strip/test/tab_strip_drag_drop_egtest.mm
+++ ios/chrome/browser/tab_switcher/tab_strip/test/tab_strip_drag_drop_egtest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/chrome/test/earl_grey/chrome_xcui_actions.h"
 #import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 
@@ -36,35 +37,7 @@ id<GREYMatcher> RegularCellAtIndex(unsigned int index) {
       nil);
 }
 
-// Finds the element with the given `identifier` of given `type`.
-XCUIElement* GetElementMatchingIdentifier(XCUIApplication* app,
-                                          NSString* identifier,
-                                          XCUIElementType type) {
-  XCUIElementQuery* query = [[app.windows.firstMatch
-      descendantsMatchingType:type] matchingIdentifier:identifier];
-  return [query elementBoundByIndex:0];
-}
 
-// Drags and drops the cell with the given `src_cell_identifier` to the
-// `dst_cell_identifier` position.
-void DragDropTabStripTabCellInTabStripView(NSString* src_cell_identifier,
-                                           NSString* dst_cell_identifier) {
-  XCUIApplication* app = [[XCUIApplication alloc] init];
-  XCUIElement* src_element = GetElementMatchingIdentifier(
-      app, src_cell_identifier, XCUIElementTypeCell);
-  XCUICoordinate* start_point =
-      [src_element coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.5)];
-
-  XCUIElement* dst_element = GetElementMatchingIdentifier(
-      app, dst_cell_identifier, XCUIElementTypeCell);
-  XCUICoordinate* end_point =
-      [dst_element coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.5)];
-
-  [start_point pressForDuration:1.5
-           thenDragToCoordinate:end_point
-                   withVelocity:XCUIGestureVelocityDefault
-            thenHoldForDuration:1.0];
-}
 
 // Checks that the regular cell matching `tab_title` moved to `tab_index`
 void AssertRegularCellMovedToNewPosition(unsigned int tab_index,
@@ -110,8 +83,10 @@ void AssertRegularCellMovedToNewPosition(unsigned int tab_index,
   // Tab2: About Version
 
   // Move Tab0 to Tab2.
-  DragDropTabStripTabCellInTabStripView(IdentifierForRegularCellAtIndex(0),
-                                        IdentifierForRegularCellAtIndex(2));
+  chrome_test_util::LongPressCellAndDragToOffsetOf(
+      IdentifierForRegularCellAtIndex(0), /*src_window_number=*/0,
+      IdentifierForRegularCellAtIndex(2), /*dst_window_number=*/0,
+      CGVectorMake(0.5, 0.5));
   AssertRegularCellMovedToNewPosition(/*tab_index*/ 2,
                                       /*tab_title*/ @"New Tab");
 
@@ -120,8 +95,10 @@ void AssertRegularCellMovedToNewPosition(unsigned int tab_index,
   // Tab2: New Tab
 
   // Move Tab1 to Tab0.
-  DragDropTabStripTabCellInTabStripView(IdentifierForRegularCellAtIndex(1),
-                                        IdentifierForRegularCellAtIndex(0));
+  chrome_test_util::LongPressCellAndDragToOffsetOf(
+      IdentifierForRegularCellAtIndex(1), /*src_window_number=*/0,
+      IdentifierForRegularCellAtIndex(0), /*dst_window_number=*/0,
+      CGVectorMake(0.5, 0.5));
   AssertRegularCellMovedToNewPosition(/*tab_index*/ 0,
                                       /*tab_title*/ @"About Version");
 }
--- ios/chrome/browser/tab_switcher/tab_strip/test/tab_strip_group_egtest.mm
+++ ios/chrome/browser/tab_switcher/tab_strip/test/tab_strip_group_egtest.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/chrome/test/earl_grey/chrome_xcui_actions.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ui/base/l10n/l10n_util.h"
 
@@ -177,35 +178,7 @@ void AddTabToNewGroup(id<GREYMatcher> tab_cell_matcher,
                       grey_accessibilityID(kCreateTabGroupViewIdentifier)];
 }
 
-// Finds the element with the given `identifier` of given `type`.
-XCUIElement* GetElementMatchingIdentifier(XCUIApplication* app,
-                                          NSString* identifier,
-                                          XCUIElementType type) {
-  XCUIElementQuery* query = [[app.windows.firstMatch
-      descendantsMatchingType:type] matchingIdentifier:identifier];
-  return [query elementBoundByIndex:0];
-}
 
-// Drags and drops the cell with the given `src_cell_identifier` to the
-// `dst_cell_identifier` position.
-void DragDropTabStripTabCellInTabStripView(NSString* src_cell_identifier,
-                                           NSString* dst_cell_identifier) {
-  XCUIApplication* app = [[XCUIApplication alloc] init];
-  XCUIElement* src_element = GetElementMatchingIdentifier(
-      app, src_cell_identifier, XCUIElementTypeCell);
-  XCUICoordinate* start_point =
-      [src_element coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.5)];
-
-  XCUIElement* dst_element = GetElementMatchingIdentifier(
-      app, dst_cell_identifier, XCUIElementTypeCell);
-  XCUICoordinate* end_point =
-      [dst_element coordinateWithNormalizedOffset:CGVectorMake(0.5, 0.5)];
-
-  [start_point pressForDuration:1.5
-           thenDragToCoordinate:end_point
-                   withVelocity:XCUIGestureVelocityDefault
-            thenHoldForDuration:1.0];
-}
 
 // Long-presses on a tab with `title` to make the context menu appear.
 void LongPressTabWithTitle(NSString* title) {
@@ -873,8 +846,10 @@ void LongPressTabWithTitle(NSString* title) {
   // Tab3, currently active: Chrome URLs
 
   // Move Tab2 to Tab3.
-  DragDropTabStripTabCellInTabStripView(IdentifierForRegularCellAtIndex(2),
-                                        IdentifierForRegularCellAtIndex(3));
+  chrome_test_util::LongPressCellAndDragToOffsetOf(
+      IdentifierForRegularCellAtIndex(2), /*src_window_number=*/0,
+      IdentifierForRegularCellAtIndex(3), /*dst_window_number=*/0,
+      CGVectorMake(0.5, 0.5));
 
   [[EarlGrey selectElementWithMatcher:DeleteGroupConfirmationButton()]
       performAction:grey_tap()];
@@ -912,8 +887,10 @@ void LongPressTabWithTitle(NSString* title) {
   // Tab3, currently active: Chrome URLs
 
   // Move Tab2 to Tab3.
-  DragDropTabStripTabCellInTabStripView(IdentifierForRegularCellAtIndex(2),
-                                        IdentifierForRegularCellAtIndex(3));
+  chrome_test_util::LongPressCellAndDragToOffsetOf(
+      IdentifierForRegularCellAtIndex(2), /*src_window_number=*/0,
+      IdentifierForRegularCellAtIndex(3), /*dst_window_number=*/0,
+      CGVectorMake(0.5, 0.5));
 
   [[EarlGrey selectElementWithMatcher:
                  chrome_test_util::ActionSheetItemWithAccessibilityLabelId(
