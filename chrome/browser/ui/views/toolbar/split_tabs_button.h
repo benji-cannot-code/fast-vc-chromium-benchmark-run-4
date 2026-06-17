3863042c9e46d31f37624dc3ba924f083943ec15
Caroline Rising <corising@chromium.org>
2026-06-17 12:56:57 -0700

    Support split tabs button popout if overflowed.
    Since the split tab button shows a menu instead of a bubble we must make
    sure the button is visible in the toolbar before the menu is shown
    (since native menus cannot update bounds). This means we must explicitly
    pop out the button and trigger a layout before the menu is triggered
    instead of letting the toolbar_controller logic handle popping it out.
    
    Bug: 513239707
    Change-Id: I07943151dd758ae170a46feea78a65128a4ca207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7931949
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648529}

--- chrome/browser/ui/views/toolbar/split_tabs_button.h
+++ chrome/browser/ui/views/toolbar/split_tabs_button.h
@@ -20,6 +20,7 @@ class PinnedToolbarButtonStatusIndicator;
 class SplitTabMenuModel;
 
 namespace views {
+class MenuModelAdapter;
 class MenuRunner;
 }  // namespace views
 
@@ -63,6 +64,7 @@ class SplitTabsToolbarButton : public ToolbarButton,
   raw_ptr<Browser> browser_;
   raw_ptr<PinnedToolbarButtonStatusIndicator> status_indicator_;
   std::unique_ptr<SplitTabMenuModel> split_tab_menu_;
+  std::unique_ptr<views::MenuModelAdapter> menu_model_adapter_;
   std::unique_ptr<views::MenuRunner> menu_runner_;
 };
 
