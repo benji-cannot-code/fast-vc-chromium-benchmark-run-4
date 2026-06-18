f3c0c28ee6f7a2eb65280ab1042676daaea0b29e
Wan-Teh Chang <wtc@google.com>
2026-06-18 11:13:31 -0700

    Roll src/third_party/dav1d/libdav1d/ e9c5800ac..14c73c7db (4 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/e9c5800ace88..14c73c7db38e
    
    $ git log e9c5800ac..14c73c7db --date=short --no-merges --format='%ad %ae %s'
    2026-06-17 rsbultje cdef: re-add comments accidentally removed in previous commit
    2026-06-15 GermanAizek cdef: move out of loop constant-calculated expressions
    2026-06-15 GermanAizek refmvs: move out of loop constant-calculated expressions
    2025-11-22 mikolaj ipred_dc/dc_left/dc_top/dc_128 implementation in RISC-V assembly
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I29d6f23b773dd9b43a4ca65f90b96454d4b1396e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7962620
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649187}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-54-ge9c5800a"
+#define DAV1D_VERSION "1.5.3-58-g14c73c7d"
