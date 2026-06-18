8fc6654ab7975d3e055036de4c85e9a70bf571b4
Joe Peplowski <joepeplowski@google.com>
2026-06-18 01:36:36 -0700

    [iOS] Fix cached unrealized tabs not attaching properly
    Unrealized tabs that were also cached were being treated as invalid due
    to the lines:
    
    if (!CanExtractPageContextForWebState(webState)) {
      [self handleAttemptToAttachInvalidTab:itemID];
    }
    
    This would cause CanExtractPageContextForWebState to fail because the
    cached tab was unrealized.
    
    This CL fixes this issue by exiting early if a tab is cached and
    prevents cached tabs from unexpectedly being deselected.
    
    Bug: 525068073
    Change-Id: Ie4e4cf16f2098816a6d989f82b5ddc3d55775419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957680
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648864}

--- ios/chrome/browser/tab_picker/coordinator/tab_picker_mediator.mm
+++ ios/chrome/browser/tab_picker/coordinator/tab_picker_mediator.mm
@@ -146,9 +146,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   // If the tab's APC is cached avoid updating the snapshot.
-  BOOL cached = webState && _validAPCwebStatesIDs.contains(base::NumberToString(
-                                webState->GetUniqueIdentifier().identifier()));
-  if (webState && !webState->IsRealized() && !cached) {
+  if (webState && _validAPCwebStatesIDs.contains(base::NumberToString(
+                      webState->GetUniqueIdentifier().identifier()))) {
+    return;
+  }
+
+  if (webState && !webState->IsRealized()) {
     // If the web state is not realized, force it to realize in order to have
     // the latest content and updated snapshot.
     __weak TabPickerMediator* weakSelf = self;
