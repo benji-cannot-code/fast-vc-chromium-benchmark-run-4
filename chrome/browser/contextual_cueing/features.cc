f01b95af85d0c503489cb94157869dd4ccaab527
Benedict Wong <benedictwong@google.com>
2026-06-18 11:12:42 -0700

    Add kContextualCueingV2MultiSource feature flag and UCB Finch params
    Define the kContextualCueingV2MultiSource feature flag and 5 UCB scorer
    Finch parameters (exploration weight, alpha, beta, gamma, max explore
    impressions) in contextual_cueing/features.h/cc.
    
    This is a pure addition with zero behavioral change, setting up for the
    upcoming V2 multi-source orchestration path.
    
    TAG=agy
    
    Bug: 520159219
    Change-Id: I1b6f5f5177f65349fa3d7079ae8a98314e17d827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7950988
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Benedict Wong <benedictwong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649186}

--- chrome/browser/contextual_cueing/features.cc
+++ chrome/browser/contextual_cueing/features.cc
@@ -10,6 +10,7 @@ namespace contextual_cueing {
 BASE_FEATURE(kContextualCueingV2, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kContextualCueingV2EnforceAgeRestriction,
              base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kContextualCueingV2MultiSource, base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<double> kEduClassifierThreshold(
     &kContextualCueingV2,
@@ -90,6 +91,29 @@ const base::FeatureParam<base::TimeDelta> kClickBackoffTime(
 const base::FeatureParam<bool> kUsePrivateAi(&kContextualCueingV2,
                                              "ContextualCueingV2UsePrivateAi",
                                              true);
+
+// UCB scorer hyperparameters. Defaults are a reasonable starting point;
+// tune via Finch once multi-source experiments are running.
+const base::FeatureParam<double> kUcbExplorationWeight(
+    &kContextualCueingV2MultiSource,
+    "ContextualCueingV2MultiSourceUcbExplorationWeight",
+    1.414);
+const base::FeatureParam<double> kUcbAlpha(
+    &kContextualCueingV2MultiSource,
+    "ContextualCueingV2MultiSourceUcbAlpha",
+    1.0);
+const base::FeatureParam<double> kUcbBeta(
+    &kContextualCueingV2MultiSource,
+    "ContextualCueingV2MultiSourceUcbBeta",
+    0.5);
+const base::FeatureParam<double> kUcbGamma(
+    &kContextualCueingV2MultiSource,
+    "ContextualCueingV2MultiSourceUcbGamma",
+    1.0);
+const base::FeatureParam<int> kUcbMaxExploreImpressions(
+    &kContextualCueingV2MultiSource,
+    "ContextualCueingV2MultiSourceUcbMaxExploreImpressions",
+    50);
 
 const base::FeatureParam<std::string> kHelpCenterArticleLink(
     &kContextualCueingV2,
