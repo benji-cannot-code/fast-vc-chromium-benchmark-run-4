a72546c7230c5d41e9e43e8e41c0f8c050369470
Przemek Perkowski <perkowski@google.com>
2026-06-18 05:22:45 -0700

    Don't show prompt to Ask Gemini if Glic isn't enabled.
    For the profiles that do not support Glic (either by eligibility checks
    or because the feature is not launched) we cannot redirect a user to
    Gemini for complex queries. Let's fallback to `no_data` message instead.
    
    Bug: 510726475
    Change-Id: I633587b34253d41e65d24a00258654ad6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7922993
    Commit-Queue: Przemek Perkowski <perkowski@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Rahim Mammadli <rahimis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648957}

--- components/autofill/core/browser/foundations/autofill_client.cc
+++ components/autofill/core/browser/foundations/autofill_client.cc
@@ -443,4 +443,8 @@ void AutofillClient::OpenGeminiInSidebar(const std::u16string& prompt) {
   NOTIMPLEMENTED();
 }
 
+bool AutofillClient::IsGlicEnabled() const {
+  return false;
+}
+
 }  // namespace autofill
