fb4583f2eea461b6a47c8e8457b2b4b7b1395857
Alexander Cooper <alcooper@chromium.org>
2026-06-18 10:23:45 -0700

    Fix WebXR callback ID wrap-around behavior
    Implement a safe incrementing loop in `XRFrameRequestCallbackCollection`
    to avoid using reserved hash map keys (0 and Max) which could lead to
    corruption when the ID wraps around.
    
    While investigating the proper fix for this, it was noticed that per the
    WebXR spec the callback ID type should be `unsigned long` in the IDL
    instead of `long`. This CL also updates the IDL to use `unsigned long`
    and the corresponding C++ code to use `uint32_t` instead of `int`.
    
    Fixed: 523477987
    Change-Id: I99e329b7ee2d2d7a601aa8f061be5f706625a468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7959900
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649153}

--- third_party/blink/renderer/modules/xr/xr_frame_request_callback_collection.h
+++ third_party/blink/renderer/modules/xr/xr_frame_request_callback_collection.h
@@ -33,7 +33,7 @@ class XRFrameRequestCallbackCollection final
   explicit XRFrameRequestCallbackCollection(ExecutionContext*);
   ~XRFrameRequestCallbackCollection() override = default;
 
-  using CallbackId = int;
+  using CallbackId = uint32_t;
   CallbackId RegisterCallback(V8XRFrameRequestCallback*);
   void CancelCallback(CallbackId);
   void ExecuteCallbacks(XRSession*, double timestamp, XRFrame*);
@@ -49,7 +49,7 @@ class XRFrameRequestCallbackCollection final
   }
 
  private:
-  bool IsValidCallbackId(int id) {
+  bool IsValidCallbackId(CallbackId id) {
     using Traits = HashTraits<CallbackId>;
     return !IsHashTraitsEmptyOrDeletedValue<Traits, CallbackId>(id);
   }
@@ -67,7 +67,7 @@ class XRFrameRequestCallbackCollection final
   CallbackFrameRequestMap current_callback_frame_requests_;
   CallbackAsyncTaskMap current_callback_async_tasks_;
 
-  CallbackId next_callback_id_ = 0;
+  CallbackId previous_callback_id_ = 0;
 
   // Trace IDs need to be unique for any outstanding frames. While we can only
   // have one immersive session at a time, that is not the case for inline
