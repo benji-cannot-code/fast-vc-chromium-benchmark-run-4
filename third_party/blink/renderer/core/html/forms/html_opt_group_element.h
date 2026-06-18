439bf950d052896efd9d1d5c8dc5f49f118a8120
Joey Arhar <jarhar@chromium.org>
2026-06-18 12:35:52 -0700

    Use slot fallback to render optgroup label attr
    Instead of setting display:none on the element which holds the label
    attribute when a legend element is present, we can put the label
    attribute holding element as a child of a new slot for the legend
    element, so it automatically gets rendered when there is no legend
    element.
    
    The legend element is only supposed to be the first element in the
    optgroup as per the content model in the HTML spec, so this patch also
    enforces that by only slotting the legend element in when it is the
    first child element. Allowing or using the legend element inside of
    optgroups was a new feature with customizable select, so compat risk is
    low.
    
    Context: https://github.com/whatwg/html/pull/12201#discussion_r2862689322
    
    There are a11y test expectations because the UA shadowroot is being
    changed, but this should not be visible to ATs because the new nodes are
    marked as invisible and ignored, and only appear in the internal "blink"
    a11y trees as opposed to real platform trees.
    
    Change-Id: I776fee963146abbad2028638479df7d3362c35ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629416
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649235}

--- third_party/blink/renderer/core/html/forms/html_opt_group_element.h
+++ third_party/blink/renderer/core/html/forms/html_opt_group_element.h
@@ -71,6 +71,7 @@ class CORE_EXPORT HTMLOptGroupElement final : public HTMLElement {
   String LabelAttributeText() const;
   void UpdateGroupLabel();
 
+  Member<HTMLSlotElement> legend_slot_;
   Member<HTMLSlotElement> opt_group_slot_;
   Member<HTMLDivElement> label_;
   Member<HTMLSelectElement> owner_select_;
