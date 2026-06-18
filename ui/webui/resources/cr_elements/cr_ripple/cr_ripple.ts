9001d2ee7f73ba58f0fd46abbfd31da8607d10e2
James Leung <jamesleung@google.com>
2026-06-17 17:56:27 -0700

    [voice_search] Fix ripple JS runtime error
    * When in voice search with file carousel, repeatedly stopping/starting voice search causes the error shown in the screenshot in the bug. The browser handles the runtime error as a full crash, closing the browser.
    * The error is incorrect type for variable opacity.
    * This crashes the browser FULLY due to the type not matching for the ripple. (In the screenshot, the other browser running is separate from the browser that crashed).
    * Likely cause: there is race condition in which style is not calculated
      yet, especially if there is latency upon starting/stopping voice search.
    * Fix: allow the type to be 'undefined' as well.
    
    Related to this CL: crrev.com/c/7932806
    
    Bug: b:523312747
    Change-Id: I2f14ef1023236f01a33fe2e4a606a3f80755b2cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7931947
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648724}

--- ui/webui/resources/cr_elements/cr_ripple/cr_ripple.ts
+++ ui/webui/resources/cr_elements/cr_ripple/cr_ripple.ts
@@ -216,8 +216,7 @@ export class CrRippleElement extends CrLitElement {
     }
 
     this.ripples_.forEach(function(ripple) {
-      const opacity =
-          ripple.computedStyleMap().get('opacity') as CSSUnitValue | null;
+      const opacity = (ripple.computedStyleMap().get('opacity') ?? null) as CSSUnitValue | null;
       if (opacity === null) {
         ripple.remove();
         return;
