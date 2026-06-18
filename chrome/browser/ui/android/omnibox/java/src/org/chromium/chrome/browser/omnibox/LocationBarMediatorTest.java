c806f3dc0cf5e0a22ef23654653758de97111b50
Sky Malice <skym@chromium.org>
2026-06-18 13:08:11 -0700

    [AIM] Fix testOnSuggestionsChanged_triggersScrimVisibility
    Fixes testOnSuggestionsChanged_triggersScrimVisibility by clearing mock
    invocations on ScrimHandler immediately after starting the input session
    to prevent verification failures caused by synchronous calls during
    setup.
    
    Bug: 524394236, 524399911, 525121822
    Change-Id: I7d75244f07250ccc8fa78d13e50848eedbb20a9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957750
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649253}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
@@ -73,7 +73,6 @@ import org.chromium.base.supplier.SettableNonNullObservableSupplier;
 import org.chromium.base.supplier.SettableNullableObservableSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.RobolectricUtil;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.build.BuildConfig;
@@ -2965,12 +2964,12 @@ public class LocationBarMediatorTest {
         verify(mLocationBarLayout).setBackButtonVisibility(true);
     }
 
-    @DisabledTest(message = "crbug.com/525121822")
     @Test
     public void testOnSuggestionsChanged_triggersScrimVisibility() {
         mMediator.onFinishNativeInitialization();
         mProfileSupplier.set(mProfile);
         mMediator.beginInput(new AutocompleteInput());
+        clearInvocations(mScrimHandler);
 
         // Show scrim in all contexts if there are any suggestions to show.
         mMediator.onSuggestionsChanged(null, true);
