23292a6b8364b5761b824039bb7ef3c2921a0ae3
Eleanor Lee <eleanorlee@google.com>
2026-06-17 22:45:26 -0700

    [Vertical Tabs] Add context menu to tabs
    After:
    https://screencast.googleplex.com/cast/NTMwNzU2MDExNzY2NTc5MnxlNDQxNDFjYy0xYQ
    
    This includes regular tabs, child tabs and pinned tabs.
    Confirmed that everything works fine on AL.
    
    Bug: 521982129
    Change-Id: I954bb64db998001f36a382142085c415c2f02066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7939090
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1648810}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -2208,7 +2208,8 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                                     mWindowAndroid,
                                     assumeNonNull(mMultiInstanceManager),
                                     assumeNonNull(mSnackbarManagerSupplier.get()),
-                                    getDesktopWindowStateManager()),
+                                    getDesktopWindowStateManager(),
+                                    mShareDelegateSupplier),
                             mIsVerticalTabsActiveSupplier);
             mSideUiCoordinator.registerSideUiContainer(mVerticalTabsSideUiCoordinator);
         }
