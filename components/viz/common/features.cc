cb6a454629e9c22994f7cf4e8bab70dd0ca4907d
Jonathan Ross <jonross@chromium.org>
2026-06-18 12:27:57 -0700

    viz: Remove kThrottleFrameSinksOnInteraction feature
    This feature is being removed due to maintenance complexity and performance
    concerns (specifically the tree crawl in FrameSinkManagerImpl on interaction
    state changes).
    
    BUG=467315115
    TEST=viz_unittests
    
    Change-Id: I7b60692bebfac10bb947dfbaa82ade3ba9143a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951784
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649230}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -195,11 +195,6 @@ BASE_FEATURE(kVSyncAlignedPresentationForScrolling,
 BASE_FEATURE(kVSyncAlignedPresentation, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
-// If enabled, other frame sinks are throttled when a frame sink is handling
-// user interaction.
-BASE_FEATURE(kThrottleFrameSinksOnInteraction,
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kAllowUndamagedNonrootRenderPassToSkip,
 #if BUILDFLAG(IS_MAC)
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -466,11 +461,6 @@ int MaxOverlaysConsidered() {
 
 bool ShouldOnBeginFrameThrottleVideo() {
   return base::FeatureList::IsEnabled(features::kOnBeginFrameThrottleVideo);
-}
-
-bool ShouldThrottleWhenInteractiveFrameSinks() {
-  return base::FeatureList::IsEnabled(
-      features::kThrottleFrameSinksOnInteraction);
 }
 
 bool ShouldAckOnSurfaceActivationWhenInteractive() {
