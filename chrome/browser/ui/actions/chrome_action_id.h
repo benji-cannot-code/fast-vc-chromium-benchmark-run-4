f90246e36326f4f65db0b0ad35bd12303e352773
dljames <dljames@chromium.org>
2026-06-18 11:53:12 -0700

    [CommandController] Add CommandActionUpdater bridge class
    Introduces CommandActionUpdater, a standalone implementation of
    CommandUpdater backed by declarative ActionItems. This class will act as
    the routing bridge when features::kUseActionsForBrowserCommands is
    enabled.
    
    TAG=agy
    CONV=112c32e8-c7a2-4c3f-997e-251b82b89011
    
    Change-Id: I0132a3cc105195b110761a86881093cf1f26669f
    Bug: 525117343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7959001
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649205}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -86,8 +86,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionFilledCardInformation, IDC_FILLED_CARD_INFORMATION) \
   E(kActionSharingHubScreenshot, IDC_SHARING_HUB_SCREENSHOT) \
   E(kActionVirtualCardEnroll, IDC_VIRTUAL_CARD_ENROLL) \
-  E(kActionFollow, IDC_FOLLOW) \
-  E(kActionUnfollow, IDC_UNFOLLOW) \
+  E(kActionFollow) \
+  E(kActionUnfollow) \
   E(kActionAutofillMandatoryReauth, IDC_AUTOFILL_MANDATORY_REAUTH) \
   E(kActionProfileMenuInAppMenu, IDC_PROFILE_MENU_IN_APP_MENU) \
   E(kActionPasswordsAndAutofillMenu, IDC_PASSWORDS_AND_AUTOFILL_MENU) \
@@ -202,7 +202,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionShowFullUrls, IDC_SHOW_FULL_URLS) \
   E(kActionShowGoogleLensShortcut, IDC_SHOW_GOOGLE_LENS_SHORTCUT) \
   E(kActionShowAiModeOmniboxButton, IDC_SHOW_AI_MODE_OMNIBOX_BUTTON) \
-  E(kActionRecordReplay, IDC_RECORD_REPLAY) \
+  E(kActionRecordReplay) \
   E(kActionShowSearchTools, IDC_SHOW_SEARCH_TOOLS) \
   E(kActionCaretBrowsingToggle, IDC_CARET_BROWSING_TOGGLE) \
   E(kActionChromeTips, IDC_CHROME_TIPS) \
@@ -302,10 +302,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     IDC_CONTENT_CONTEXT_SEARCHLENSFORIMAGE) \
   E(kActionContentContextGlicShareImage, \
     IDC_CONTENT_CONTEXT_GLICSHAREIMAGE) \
-  E(kActionContentContextTranslateImageWithWeb, \
-    IDC_CONTENT_CONTEXT_TRANSLATEIMAGEWITHWEB) \
-  E(kActionContentContextTranslateImageWithLens, \
-    IDC_CONTENT_CONTEXT_TRANSLATEIMAGEWITHLENS) \
+  E(kActionContentContextTranslateImageWithWeb) \
+  E(kActionContentContextTranslateImageWithLens) \
   /* Audio/video items. */ \
   E(kActionContentContextSaveAvAs, IDC_CONTENT_CONTEXT_SAVEAVAS) \
   E(kActionContentContextCopyAvLocation, IDC_CONTENT_CONTEXT_COPYAVLOCATION) \
@@ -504,8 +502,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionContentContextAutofillFeedback, \
     IDC_CONTENT_CONTEXT_AUTOFILL_FEEDBACK) \
   /* Autofill context menu commands */ \
-  E(kActionContentContextAutofillImprovedSuggestions, \
-    IDC_CONTENT_CONTEXT_AUTOFILL_PREDICTION_IMPROVEMENTS) \
+  E(kActionContentContextAutofillImprovedSuggestions) \
   E(kActionContentContextAutofillFallbackPasswordsSelectPassword, \
     IDC_CONTENT_CONTEXT_AUTOFILL_FALLBACK_PASSWORDS_SELECT_PASSWORD) \
   E(kActionContentContextAutofillFallbackPasswordsImportPasswords, \
@@ -584,7 +581,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionSidePanelShowLensOverlayResults, IDC_CONTENT_CONTEXT_LENS_OVERLAY) \
   E(kActionSidePanelShowReadAnything, IDC_SHOW_READING_MODE_SIDE_PANEL) \
   E(kActionSidePanelShowReadingList, IDC_READING_LIST_MENU_SHOW_UI) \
-  E(kActionSidePanelShowSearchCompanion, IDC_SHOW_SEARCH_COMPANION) \
+  E(kActionSidePanelShowSearchCompanion) \
   E(kActionSidePanelShowShoppingInsights) \
   E(kActionSidePanelShowSideSearch) \
   E(kActionSidePanelShowMerchantTrust) \
@@ -618,7 +615,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionCopyUrl, IDC_COPY_URL) \
   E(kActionTabGroupsMenu, IDC_SAVED_TAB_GROUPS_MENU) \
   E(kActionTabSearch, IDC_TAB_SEARCH) \
-  E(kActionSplitTab, IDC_SPLIT_TAB) \
+  E(kActionSplitTab, IDC_NEW_SPLIT_TAB) \
   E(kActionFederation) \
   E(kActionGlicContextualCueing) \
   E(kActionShowAiOverlayDialog) \
---
+++ chrome/browser/ui/actions/chrome_action_properties.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ACTIONS_CHROME_ACTION_PROPERTIES_H_
+#define CHROME_BROWSER_UI_ACTIONS_CHROME_ACTION_PROPERTIES_H_
+
+#include "ui/base/class_property.h"
+#include "ui/base/window_open_disposition.h"
+
+DECLARE_UI_CLASS_PROPERTY_TYPE(WindowOpenDisposition)
+
+namespace chrome {
+
+extern const ui::ClassProperty<WindowOpenDisposition>* const kDispositionKey;
+
+}  // namespace chrome
+
+#endif  // CHROME_BROWSER_UI_ACTIONS_CHROME_ACTION_PROPERTIES_H_
