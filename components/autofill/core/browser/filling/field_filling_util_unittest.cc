24f1c1acea88f5aedac60700c1883a9efbdc4bec
Norge Vizcay <vizcay@google.com>
2026-06-18 05:46:41 -0700

    Use length-hiding obfuscation for ambient autofill
    When retrieving an obfuscated value in GetObfuscatedValue(), if the
    kAutofillAiWalletPrivatePasses feature is enabled, a fixed target length
    representation is used to prevent revealing the true length of the
    private value.
    
    This CL updates GetObfuscatedValue() to apply the same length-hiding
    obfuscation logic when the kAutofillAmbientAutofill feature is enabled.
    
    Bug: 503303085
    Change-Id: I23f9c8222b2533c416033ab92b6df1023944a6c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957722
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648966}

--- components/autofill/core/browser/filling/field_filling_util_unittest.cc
+++ components/autofill/core/browser/filling/field_filling_util_unittest.cc
@@ -115,8 +115,10 @@ TEST(GetObfuscatedValue, ObfuscateValueWithPartialLegacy) {
 }
 
 TEST(GetObfuscatedValue, ObfuscateValue) {
-  base::test::ScopedFeatureList feature_list(
-      features::kAutofillAiWalletPrivatePasses);
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures({features::kAutofillAiWalletPrivatePasses,
+                                 features::kAutofillAmbientAutofill},
+                                {});
 
   // 4 dots + up to 4 chars (visible_suffix_length = 4)
   EXPECT_EQ(GetObfuscatedValue(u"123456789", 4),
@@ -132,8 +134,10 @@ TEST(GetObfuscatedValue, ObfuscateValue) {
 }
 
 TEST(GetObfuscatedValue, ObfuscateAll) {
-  base::test::ScopedFeatureList feature_list(
-      features::kAutofillAiWalletPrivatePasses);
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures({features::kAutofillAiWalletPrivatePasses,
+                                 features::kAutofillAmbientAutofill},
+                                {});
 
   // Matches length of UI string (4 dots + 4 visible = 8 dots).
   // visible_suffix_length = 0 means obfuscate all.
