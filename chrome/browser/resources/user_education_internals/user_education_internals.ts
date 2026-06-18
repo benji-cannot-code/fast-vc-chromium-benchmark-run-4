e8e7172d6a689fe583e5e2675037815c5ed2fe1e
Dana Fried <dfried@chromium.org>
2026-06-18 14:21:23 -0700

    [User Education] Show only one section at a time
    This makes it easier to navigate the User Education Internals page.
    Instead of scrolling the page, the menu on the left shows specific
    sections (or all sections, if desired).
    
    Bug: 524982291
    Change-Id: I93890307a2c53f264acfdf7033af00eebd226c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7959118
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649311}

--- chrome/browser/resources/user_education_internals/user_education_internals.ts
+++ chrome/browser/resources/user_education_internals/user_education_internals.ts
@@ -9,6 +9,8 @@ import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import 'chrome://resources/cr_elements/cr_menu_selector/cr_menu_selector.js';
+import 'chrome://resources/cr_elements/cr_page_selector/cr_page_selector.js';
+import 'chrome://resources/cr_elements/cr_tabs/cr_tabs.js';
 import 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
@@ -24,6 +26,7 @@ import type {CrInputElement} from '//resources/cr_elements/cr_input/cr_input.js'
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 import {HelpBubbleMixinLit} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin_lit.js';
 import type {CrMenuSelectorElement} from 'chrome://resources/cr_elements/cr_menu_selector/cr_menu_selector.js';
+import type {CrPageSelectorElement} from 'chrome://resources/cr_elements/cr_page_selector/cr_page_selector.js';
 import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
@@ -39,6 +42,7 @@ export interface UserEducationInternalsElement {
     content: HTMLElement,
     errorMessageToast: CrToastElement,
     menu: CrMenuSelectorElement,
+    selector: CrPageSelectorElement,
     // <if expr="not is_chromeos">
     whatsNewVersionOverride: CrInputElement,
     // </if>
@@ -104,6 +108,7 @@ export class UserEducationInternalsElement extends
       ntpPromoPreferences_: {type: Array},
       sessionData_: {type: Array},
       currentChromeVersion_: {type: Number},
+      selectedTabIndex_: {type: Number},
     };
   }
 
@@ -124,6 +129,7 @@ export class UserEducationInternalsElement extends
       loadTimeData.getInteger('whatsNewVersionToRequest');
   protected accessor currentChromeVersion_: number =
       loadTimeData.getInteger('currentChromeVersion');
+  protected accessor selectedTabIndex_ = -1;
 
   private handler_: UserEducationInternalsPageHandlerInterface;
 
@@ -377,11 +383,14 @@ export class UserEducationInternalsElement extends
   }
 
   protected onSelectorIronActivate_(event: CustomEvent<{selected: string}>) {
-    const url = event.detail.selected;
-    this.$.menu.selected = url;
-    const idx = url.lastIndexOf('#');
-    const el = this.$.content.querySelector(url.substring(idx));
-    el?.scrollIntoView(true);
+    const index = Number(event.detail.selected);
+    this.$.menu.selected = index;
+    if (index >= 0) {
+      this.$.selector.removeAttribute('show-all');
+      this.selectedTabIndex_ = index;
+    } else {
+      this.$.selector.setAttribute('show-all', 'true');
+    }
   }
 
   protected onNarrowChanged_(e: CustomEvent<{value: boolean}>) {
