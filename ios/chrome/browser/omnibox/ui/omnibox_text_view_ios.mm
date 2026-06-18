7f5735057ff35589e1b3402b9f491e5fd0550866
Chris Lu <thegreenfrog@google.com>
2026-06-18 07:32:52 -0700

    [ios] Center clear button inside visual textfield on iPad AI Omnibox
    UX wants the clear button, leading icon, and text inside the visual textfield (pill container) in the iPad new omnibox compact mode to be vertically centered within the white space.
    Currently, they are all center-aligned with the placeholder label's centerY, which is shifted downwards by kVerticalOffset (1.0 pt), causing a minor visual misalignment.
    
    Before:https://screenshot.googleplex.com/9G4ZrjgycVH3Fbc
    After:https://screenshot.googleplex.com/7kCBMMUoJGDXpBq
    
    Fixed: 487532219
    Change-Id: Ib9cc8a9d4367355467755b4db73db0e483494122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957151
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649028}

--- ios/chrome/browser/omnibox/ui/omnibox_text_view_ios.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_text_view_ios.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/material_timing.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/dynamic_type_util.h"
+#import "ios/chrome/common/ui/util/ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/grit/ios_theme_resources.h"
 #import "skia/ext/skia_utils_ios.h"
@@ -1185,7 +1186,9 @@ const CGFloat kVerticalOffset = 1;
   CGFloat verticalPadding =
       MAX(minVerticalInset * 2.0, (minHeight - lineHeight));
   // Distribute padding.
-  CGFloat topPadding = verticalPadding / 2.0 + kVerticalOffset;
+  CGFloat verticalOffset =
+      IsRegularXRegularSizeClass(self.traitCollection) ? 0.0 : kVerticalOffset;
+  CGFloat topPadding = verticalPadding / 2.0 + verticalOffset;
   CGFloat bottomPadding = verticalPadding - topPadding;
   self.textContainerInset = UIEdgeInsetsMake(topPadding, 0, bottomPadding, 0);
   _placeholderTopConstraint.constant = topPadding;
