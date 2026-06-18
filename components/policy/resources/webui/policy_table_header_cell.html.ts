4c6ff5d9d9ef7edea8640dabc8c85f3c6870eca4
Yann Dago <ydago@chromium.org>
2026-06-18 10:34:50 -0700

    [Policy] Migrate policy_table to LitElement.
    This change refactors the policy_table component to use LitElement,
    moving the HTML template to a TypeScript file and the CSS to a separate
    file. The browsertest JavaScript was updated to wait for LitElement's
    update cycle to complete before querying the DOM. The sorting and
    filtering logic is now managed within the LitElement's lifecycle
    methods.
    
    Screenshot: http://screen/B9oXZ5iKdFuJNmH
    Bug: 521988139
    Change-Id: Id4b8cf11b97bb7521d51813efb97f3dc85cc522b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7929549
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649161}

---
+++ components/policy/resources/webui/policy_table_header_cell.html.ts
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import type {PolicyTableHeaderCellElement} from './policy_table_header_cell.js';
+
+export function getHtml(this: PolicyTableHeaderCellElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+    ${this.headerTitle}
+    ${this.noSort ? '' : html`
+      <div class="sort-arrows">
+        <button
+          class="sort-up-arrow"
+          id="${this.field}-sort-up"
+          role="button"
+          aria-hidden="false"
+          title="$i18n{sort} ${this.headerTitle} $i18n{sortAscending}"
+          ?active="${this.sortedColumn === this.field &&
+                     this.sortAscending}"
+          @click="${this.onSortUpClick}">
+        </button>
+        <button
+          class="sort-down-arrow"
+          id="${this.field}-sort-down"
+          role="button"
+          aria-hidden="false"
+          title="$i18n{sort} ${this.headerTitle} $i18n{sortDescending}"
+          ?active="${this.sortedColumn === this.field &&
+                     !this.sortAscending}"
+          @click="${this.onSortDownClick}">
+        </button>
+      </div>
+    `}
+<!--_html_template_end_-->`;
+  // clang-format on
+}
