d3f22efd039938aff8d183ad84abf1505f6361e4
Eva Su <evasu@chromium.org>
2026-06-18 15:00:14 -0700

    [Extensions] Implement default pinning for new extension installations
    This CL implements default pinning of newly installed extensions to the
    toolbar, behind the `kExtensionsPinnedByDefault` feature flag.
    
    When the feature flag is enabled, new extension installations with a
    toolbar action are automatically pinned to the toolbar. However,
    enterprise policy takes precedence. If the policy is `kNotSet`, then the
    pinning behavior will be determined by the feature flag.
    
    Screen recording (when feature flag is enabled):
    http://screencast/cast/NDg1MjE2MDg0Mjk1NjgwMHwwNTQ4MDUzZC00Mw
    
    Adding metrics will be done in another CL.
    
    Bug: 452613365
    Change-Id: Ib2832a5e98dbbdb150047bc849418e9153aef62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7920006
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649335}

--- chrome/browser/extensions/extension_management_internal.cc
+++ chrome/browser/extensions/extension_management_internal.cc
@@ -256,7 +256,7 @@ void IndividualSettings::Reset() {
   policy_blocked_hosts.ClearPatterns();
   policy_allowed_hosts.ClearPatterns();
   blocked_install_message.clear();
-  toolbar_pin = ManagedToolbarPinMode::kDefaultUnpinned;
+  toolbar_pin = ManagedToolbarPinMode::kNotSet;
 }
 
 GlobalSettings::GlobalSettings() = default;
