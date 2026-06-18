af6d39fa5e89814fe1374c51ea861bb198cd1456
Jayden Nikifork <nikifork@google.com>
2026-06-18 10:28:47 -0700

    Return false when CreateTablesIfNecessary fails
    Replaces the `NOTREACHED()` assertion with `return false` in
    `WebAppManifestSectionTable::CreateTablesIfNecessary()`. The
    `WebDatabaseTable` interface already expects `false` to be returned on
    failure.
    
    Bug: 409972634
    Change-Id: I6535cd85905608edd33ca4f5376b89c33613e199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7943644
    Commit-Queue: Jayden Nikifork <nikifork@google.com>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649157}

--- components/payments/content/web_app_manifest_section_table.cc
+++ components/payments/content/web_app_manifest_section_table.cc
@@ -80,15 +80,12 @@ WebDatabaseTable::TypeKey WebAppManifestSectionTable::GetTypeKey() const {
 }
 
 bool WebAppManifestSectionTable::CreateTablesIfNecessary() {
-  if (!db()->Execute("CREATE TABLE IF NOT EXISTS web_app_manifest_section ( "
-                     "expire_date INTEGER NOT NULL DEFAULT 0, "
-                     "id VARCHAR, "
-                     "min_version INTEGER NOT NULL DEFAULT 0, "
-                     "fingerprints BLOB) ")) {
-    NOTREACHED();
-  }
-
-  return true;
+  return db()->Execute(
+      "CREATE TABLE IF NOT EXISTS web_app_manifest_section ( "
+      "expire_date INTEGER NOT NULL DEFAULT 0, "
+      "id VARCHAR, "
+      "min_version INTEGER NOT NULL DEFAULT 0, "
+      "fingerprints BLOB) ");
 }
 
 bool WebAppManifestSectionTable::MigrateToVersion(
