69217d882bea065587763acf13e05ac87794384a
Rafał Godlewski <rgod@google.com>
2026-06-18 13:12:20 -0700

    [iOS] Fix dangling pointers in autofill_profile_edit_mediator.mm
    * Add disconnect to the mediator to ensure raw_ptrs are cleaned up
    when the coordinator stops
    * Switch destruction order in tests, so that profile and personal
    data manager are destroyed last
    
    #codehealth
    
    Change-Id: I339076f6e502b286e3eb65cc1384d82c815ec83d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951497
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649258}

--- ios/chrome/browser/autofill/ui_bundled/address_editor/autofill_edit_profile_coordinator.mm
+++ ios/chrome/browser/autofill/ui_bundled/address_editor/autofill_edit_profile_coordinator.mm
@@ -136,6 +136,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          completion:nil];
   _navigationController.presentationController.delegate = nil;
   _viewController = nil;
+  [_autofillProfileEditMediator disconnect];
   _autofillProfileEditMediator = nil;
 }
 
--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_edit_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_profile_edit_coordinator.mm
@@ -118,6 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)stop {
   _sharedViewController = nil;
   _viewController = nil;
+  [_mediator disconnect];
   _mediator = nil;
 }
 
