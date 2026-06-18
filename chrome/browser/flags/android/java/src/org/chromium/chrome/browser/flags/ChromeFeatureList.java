56639b833cf14e879c4323b990d08950d0010106
Zhe Li <zheliooo@google.com>
2026-06-18 12:03:13 -0700

    [TabFavicon][DisableHostFallback] enable by default
    Bug: 468979189
    Change-Id: I660b429cd8e16735d0eb6bac41b243db00410629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7964044
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649213}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1011,7 +1011,7 @@ public abstract class ChromeFeatureList {
     public static final CachedFlag sFaviconDisableHostFallback =
             newCachedFlag(
                     FAVICON_DISABLE_HOST_FALLBACK,
-                    /* defaultValue= */ false,
+                    /* defaultValue= */ true,
                     /* defaultValueInTests= */ true);
     public static final CachedFlag sFluidResize =
             newCachedFlag(FLUID_RESIZE, /* defaultValue= */ true, /* defaultValueInTests= */ true);
