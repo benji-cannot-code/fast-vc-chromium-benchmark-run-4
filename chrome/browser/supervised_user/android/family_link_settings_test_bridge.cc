a0b466200b3bd2c6586d093e5a2f0fbe3b9d487b
Tomek Jurkiewicz <tju@google.com>
2026-06-18 01:02:02 -0700

    Move BuildSupervisedUserService from test_util to unittest
    The BuildSupervisedUserService template function was only used in
    supervised_user_url_filter_extensions_unittest.cc. This change moves the
    function into the unittest file and makes it specific to the types used
    there, removing it from the general supervised_user_test_util.h.
    
    Resolves issue with transient deps for the classify url tests (should
    include what they use rather than rely on imports of an import).
    
    Bug: 524917704
    Change-Id: Iedbe50f018e749f5e43b88ba69778598e6d78826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7956337
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1648852}

--- chrome/browser/supervised_user/android/family_link_settings_test_bridge.cc
+++ chrome/browser/supervised_user/android/family_link_settings_test_bridge.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/supervised_user/family_link_settings_service_factory.h"
+#include "chrome/browser/supervised_user/supervised_user_service_factory.h"
 #include "chrome/browser/supervised_user/supervised_user_test_util.h"
 #include "components/safe_search_api/url_checker_client.h"
 #include "components/supervised_user/core/browser/family_link_settings_service.h"
 #include "components/supervised_user/core/browser/family_link_url_filter.h"
+#include "components/supervised_user/core/browser/supervised_user_service.h"
 #include "components/supervised_user/core/common/supervised_user_constants.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
