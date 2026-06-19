3c33dc25f782437de2bdf3d950973c3860b80f1b
Anna Sato <annasato@chromium.org>
2026-06-19 00:22:59 -0700

    Disable SessionRestoreAcrossStagesTest.Restore on Linux Wayland
    This test is flaky on linux-wayland-weston-rel-tests.
    https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Ftest%5C%3Abrowser_tests!gtest%3A%3ASessionRestoreAcrossStagesTest%23Restore
    
    Bug: 525638651
    Change-Id: I1ce5d8a6e3acd5e5f65857994fb74fde78d92981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7961461
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Auto-Submit: Anna Sato <annasato@chromium.org>
    Owners-Override: Anna Sato <annasato@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649526}

--- chrome/browser/sessions/encrypted_session_storage_browsertest.cc
+++ chrome/browser/sessions/encrypted_session_storage_browsertest.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/ozone_buildflags.h"
 
 namespace sessions {
 
@@ -752,7 +753,13 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreAcrossStagesTest, PRE_Restore) {
   AssertCommandStorageBackendFilesExist(SessionType::kSessionRestore);
 }
 
-IN_PROC_BROWSER_TEST_F(SessionRestoreAcrossStagesTest, Restore) {
+// TODO(crbug.com/525638651): Re-enable this test on Linux Wayland.
+#if BUILDFLAG(IS_LINUX) && BUILDFLAG(SUPPORTS_OZONE_WAYLAND)
+#define MAYBE_Restore DISABLED_Restore
+#else
+#define MAYBE_Restore Restore
+#endif
+IN_PROC_BROWSER_TEST_F(SessionRestoreAcrossStagesTest, MAYBE_Restore) {
   AssertSessionState();
 }
 
