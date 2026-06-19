cbcbbe7e02612d7fc5e39076a5a112d7ef96d871
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-06-19 08:47:30 -0700

    Remove ConsentLevel::kSync instances from iOS-only code
    Bug: 40066949
    Change-Id: I60250e450d3f499038edaa9b3b048d8525ccfc6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7967781
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649694}

--- components/signin/public/identity_manager/objc/identity_manager_observer_bridge_unittest.mm
+++ components/signin/public/identity_manager/objc/identity_manager_observer_bridge_unittest.mm
@@ -133,7 +133,7 @@ class IdentityManagerObserverBridgeTest : public testing::Test {
 TEST_F(IdentityManagerObserverBridgeTest, TestOnPrimaryAccountChanged) {
   PrimaryAccountChangeEvent::State previous_state;
   PrimaryAccountChangeEvent::State current_state(account_info_,
-                                                 signin::ConsentLevel::kSync);
+                                                 signin::ConsentLevel::kSignin);
   PrimaryAccountChangeEvent event_details(
       previous_state, current_state, signin_metrics::AccessPoint::kStartPage);
   observer_bridge_.get()->OnPrimaryAccountChanged(event_details);
@@ -149,8 +149,8 @@ TEST_F(IdentityManagerObserverBridgeTest, TestOnPrimaryAccountChanged) {
 // Tests IdentityManagerObserverBridge::OnPrimaryAccountChanged(), with clear
 // event.
 TEST_F(IdentityManagerObserverBridgeTest, TestOnPrimaryAccountCleared) {
-  PrimaryAccountChangeEvent::State previous_state(account_info_,
-                                                  signin::ConsentLevel::kSync);
+  PrimaryAccountChangeEvent::State previous_state(
+      account_info_, signin::ConsentLevel::kSignin);
   PrimaryAccountChangeEvent::State current_state;
   PrimaryAccountChangeEvent event_details(
       previous_state, current_state, signin_metrics::ProfileSignout::kTest);
