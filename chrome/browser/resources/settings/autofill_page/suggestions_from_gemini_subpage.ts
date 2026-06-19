9ab4e1440fed37a909975462da3ef39654d03237
Kamila Hasanbega <hkamila@google.com>
2026-06-19 07:49:10 -0700

    Context: Clean up settings row
    The row is currently not needed.
    
    Bug: b:523203352
    Change-Id: I8240821ff526558f075a7546127c6f0e197ef01b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7966020
    Reviewed-by: Aisulu Rakhmetullina <aisulu@google.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649666}

--- chrome/browser/resources/settings/autofill_page/suggestions_from_gemini_subpage.ts
+++ chrome/browser/resources/settings/autofill_page/suggestions_from_gemini_subpage.ts
@@ -33,11 +33,6 @@ export class SettingsSuggestionsFromGeminiSubpageElement extends
     OpenWindowProxyImpl.getInstance().openUrl(
         loadTimeData.getString('personalContextConnectedAppsUrl'));
   }
-
-  private onManageSuggestionsClick_() {
-    // TODO(crbug.com/512204278): Add metrics.
-    // TODO(crbug.com/512202109): Add functionality.
-  }
 }
 
 declare global {
