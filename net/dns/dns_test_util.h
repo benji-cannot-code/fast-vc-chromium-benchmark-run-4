e00b98b48aebcf0af135bc0c14691fa4fe80f53f
Kalvin Lee <kdlee@chromium.org>
2026-06-19 01:21:25 -0700

    Use a byte vector in `net::GrowableIOBuffer`
    This CL is motivated by trying to enable Checked Span (issue 501113358).
    Mysterious racy crashes (see https://crrev.com/c/7953898) in
    `headless_shell_wpt_tests` were eventually traced to `GrowableIOBuffer`.
    
    Rather than minting unsafe (checked) spans, this CL reworks the backing
    storage to use `std::vector`.
    
    Cq-Include-Trybots: luci.chromium.try:android-15-chrome-blink-rel,linux-blink-rel,mac13-blink-rel,mac13.arm64-blink-rel,mac14-blink-rel,mac14.arm64-blink-rel,mac15-blink-rel,mac15.arm64-blink-rel,win10.20h2-blink-rel,win11-arm64-blink-rel,win11-blink-rel
    Bug: 396621713, 501113358
    Change-Id: I8d901bb03b2c86c62a128ccfcebd925cbff1cfb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7961697
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649535}

--- net/dns/dns_test_util.h
+++ net/dns/dns_test_util.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/span.h"
+#include "base/memory/free_deleter.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
