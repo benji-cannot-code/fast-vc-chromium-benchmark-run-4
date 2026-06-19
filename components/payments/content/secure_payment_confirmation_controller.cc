ef308eb7e511a61a42b5ccae4298473df2d3012d
Xuehui Chen <xuehuichen@google.com>
2026-06-19 15:24:44 -0700

    [SPC] Guard SPC Controller destruction during dialog close.
    A `potential` UAF vulnerability exists in the browser process due to
    synchronous reentrancy inside SecurePaymentConfirmationController.
    
    This can happen if platform-specific synchronous activation dispatch
    from native_widget_->Close() that reaches
    ExtensionPopup::OnWidgetDestroying() inside the same call stack. Or
    CloseDialog() itself trigger a controller destruction.
    
    This change adds a guard to the `CloseDialog()` method that ensures the
    call inside SecurePaymentConfirmationController will not continue to
    prevent a memory crash.
    
    Fixed: 522568496
    Change-Id: Ifc0eaaebc42db33bb309e850881722df13cad35d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7959279
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Commit-Queue: Xuehui Chen <xuehuichen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649831}

--- components/payments/content/secure_payment_confirmation_controller.cc
+++ components/payments/content/secure_payment_confirmation_controller.cc
@@ -131,7 +131,15 @@ void SecurePaymentConfirmationController::OnConfirm() {
                                   SecurePaymentRequestOutcome::kAccept);
 
     is_dialog_showing_ = false;
+    // CloseDialog() -> Widget::Close() can potentially synchronously trigger
+    // activation observers that destroy the payment window's WebContents.
+    // This self weak pointer guard can prevent potential UAF if controller is
+    // synchronously deleted inside CloseDialog().
+    auto weak_this = weak_ptr_factory_.GetWeakPtr();
     CloseDialog();
+    if (!weak_this) {
+      return;
+    }
 
     if (!request_) {
       return;
@@ -164,7 +172,13 @@ void SecurePaymentConfirmationController::OnAnotherWay() {
                                 SecurePaymentRequestOutcome::kAnotherWay);
 
   is_dialog_showing_ = false;
+
+  // CloseDialog() can potentially delete `this`. See OnConfirm() above.
+  auto weak_this = weak_ptr_factory_.GetWeakPtr();
   CloseDialog();
+  if (!weak_this) {
+    return;
+  }
 
   if (!request_) {
     return;
@@ -190,7 +204,12 @@ void SecurePaymentConfirmationController::OnCancel() {
   }
 
   is_dialog_showing_ = false;
+  // CloseDialog() can potentially delete `this`. See OnConfirm() above.
+  auto weak_this = weak_ptr_factory_.GetWeakPtr();
   CloseDialog();
+  if (!weak_this) {
+    return;
+  }
 
   if (!request_) {
     return;
@@ -210,7 +229,12 @@ void SecurePaymentConfirmationController::OnOptOut() {
   }
 
   is_dialog_showing_ = false;
+  // CloseDialog() can potentially delete `this`. See OnConfirm() above.
+  auto weak_this = weak_ptr_factory_.GetWeakPtr();
   CloseDialog();
+  if (!weak_this) {
+    return;
+  }
 
   if (!request_) {
     return;
