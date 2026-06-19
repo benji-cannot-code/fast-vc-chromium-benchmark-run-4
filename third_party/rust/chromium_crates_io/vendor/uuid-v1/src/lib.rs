c60726a48718222ab6b9f946fc1e961a645a98c6
Lukasz Anforowicz <lukasza@chromium.org>
2026-06-18 18:17:02 -0700

    Roll uuid: 1.23.2 => 1.23.3 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * uuid: 1.23.2 => 1.23.3; https://docs.rs/crate/uuid/1.23.3
    
    Bug: None
    Change-Id: I8cbc5c363d2b7cd1e9b6a6423fd7a3757cfd6219
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7964309
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649442}

--- third_party/rust/chromium_crates_io/vendor/uuid-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/uuid-v1/src/lib.rs
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.23.2"
+//! version = "1.23.3"
 //! # Lets you generate random UUIDs
 //! features = [
 //!     "v4",
@@ -139,7 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.23.2"
+//! version = "1.23.3"
 //! features = [
 //!     "v4",
 //!     "v7",
@@ -154,7 +154,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.23.2"
+//! version = "1.23.3"
 //! default-features = false
 //! ```
 //!
@@ -212,7 +212,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/uuid/1.23.2"
+    html_root_url = "https://docs.rs/uuid/1.23.3"
 )]
 
 #[cfg(any(feature = "std", test))]
