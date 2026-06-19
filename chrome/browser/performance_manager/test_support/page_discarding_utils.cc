1227729da111b11813c2219277ab88b57726779b
Joe Mason <joenotcharles@google.com>
2026-06-18 19:45:52 -0700

    Add CHECK's to ensure PageType is never set to multiple values for the same page
    Also updates the MakePageNodeDiscardable test helper to only call
    SetType if the type isn't already kTab.
    
    Bug: 525435394
    Change-Id: Id8641eaf2937d6eb4b670ce3933a904d2f9e8370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963751
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649469}

--- chrome/browser/performance_manager/test_support/page_discarding_utils.cc
+++ chrome/browser/performance_manager/test_support/page_discarding_utils.cc
@@ -31,7 +31,10 @@ void MakePageNodeDiscardable(PageNodeImpl* page_node,
                              content::BrowserTaskEnvironment& task_env) {
   page_node->SetIsVisible(false);
   page_node->SetIsAudible(false);
-  page_node->SetType(PageType::kTab);
+  if (page_node->GetType() != PageType::kTab) {
+    ASSERT_EQ(page_node->GetType(), PageType::kUnknown);
+    page_node->SetType(PageType::kTab);
+  }
   const auto kUrl = GURL("https://foo.com");
   page_node->OnMainFrameNavigationCommitted(
       false, base::TimeTicks::Now(), 42, kUrl, "text/html",
@@ -43,14 +46,14 @@ void MakePageNodeDiscardable(PageNodeImpl* page_node,
   task_env.FastForwardBy(base::Minutes(10));
   const auto* eligibility_policy =
       policies::DiscardEligibilityPolicy::GetFromGraph(page_node->graph());
-  CHECK_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::URGENT),
-           CanDiscardResult::kEligible);
-  CHECK_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::PROACTIVE),
-           CanDiscardResult::kEligible);
-  CHECK_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::EXTERNAL),
-           CanDiscardResult::kEligible);
-  CHECK_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::SUGGESTED),
-           CanDiscardResult::kEligible);
+  ASSERT_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::URGENT),
+            CanDiscardResult::kEligible);
+  ASSERT_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::PROACTIVE),
+            CanDiscardResult::kEligible);
+  ASSERT_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::EXTERNAL),
+            CanDiscardResult::kEligible);
+  ASSERT_EQ(eligibility_policy->CanDiscard(page_node, DiscardReason::SUGGESTED),
+            CanDiscardResult::kEligible);
 }
 
 GraphTestHarnessWithDiscardablePage::GraphTestHarnessWithDiscardablePage()
@@ -138,7 +141,7 @@ void GraphTestHarnessWithMockDiscarder::SetUp() {
       std::make_unique<policies::PageDiscardingHelper>();
   page_discarding_helper->SetMockDiscarderForTesting(std::move(mock_discarder));
   graph()->PassToGraph(std::move(page_discarding_helper));
-  DCHECK(policies::PageDiscardingHelper::GetFromGraph(graph()));
+  ASSERT_TRUE(policies::PageDiscardingHelper::GetFromGraph(graph()));
 }
 
 void GraphTestHarnessWithMockDiscarder::TearDown() {
