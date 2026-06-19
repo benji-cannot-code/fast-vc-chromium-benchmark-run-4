bf89250df202a1bf164aa7249b4f41ed592b1931
Ivana Žužić <izuzic@google.com>
2026-06-19 04:33:42 -0700

    Disable flaky Touch-to-fill tests
    This CL disables TouchToFillPaymentMethodViewTest#testSheetScrollabilityDependsOnState and TouchToFillViewTest#testCredentialsAreClickable.
    
    Bug: 516254354
    Change-Id: I1402c8263e6ce83d8a631d44b7d093b193bda771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963897
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1649602}

--- chrome/browser/touch_to_fill/password_manager/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewTest.java
+++ chrome/browser/touch_to_fill/password_manager/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewTest.java
@@ -64,6 +64,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.ScalableTimeout;
@@ -493,6 +494,7 @@ public class TouchToFillViewTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "b/516254354 the test is flaky.")
     public void testCredentialsAreClickable() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
