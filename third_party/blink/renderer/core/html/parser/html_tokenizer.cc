91165acd51a09f6efadf9733485cb5b2c2d5ea99
Noam Rosenthal <nrosenthal@chromium.org>
2026-06-19 02:25:02 -0700

    Allow _ in processing instruction target
    This aligns with latest spec discussions.
    Unlike -, _ is allowed also as the first character.
    
    Bug: 481087638
    Change-Id: I69b67168cbbf19de4d3d8d2702f4095e76a884b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928469
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1649565}

--- third_party/blink/renderer/core/html/parser/html_tokenizer.cc
+++ third_party/blink/renderer/core/html/parser/html_tokenizer.cc
@@ -1111,7 +1111,7 @@ bool HTMLTokenizer::NextTokenImpl(SegmentedString& source) {
 
     HTML_BEGIN_STATE(kProcessingInstructionOpenState) {
       CHECK(RuntimeEnabledFeatures::HTMLProcessingInstructionEnabled());
-      if (IsAsciiAlpha(cc)) {
+      if (cc == '_' || IsAsciiAlpha(cc)) {
         token_.BeginProcessingInstruction();
         HTML_RECONSUME_IN(kProcessingInstructionTargetState);
       } else if (cc == kEndOfFileMarker) {
@@ -1138,7 +1138,7 @@ bool HTMLTokenizer::NextTokenImpl(SegmentedString& source) {
       if (cc == kEndOfFileMarker) {
         ParseError();
         return EmitEndOfFile(source);
-      } else if (cc == '-' || IsAsciiAlphanumeric(cc)) {
+      } else if (cc == '-' || cc == '_' || IsAsciiAlphanumeric(cc)) {
         token_.AppendToProcessingInstructionTarget(ToLowerCaseIfAlpha(cc));
         temporary_buffer_.AddChar(cc);
         HTML_CONSUME(kProcessingInstructionTargetState);
