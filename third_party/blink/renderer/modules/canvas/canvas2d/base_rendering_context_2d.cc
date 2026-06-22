80db832b6b28682eb10898889c8066a004c31360
Colin Blundell <blundell@chromium.org>
2026-06-22 01:54:18 -0700

    [Blink] Move BaseRC2D::IsResourceProviderValid() to subclasses
    Eliminates a usage of GetResourceProvider(), which we are eliminating as
    a step toward eliminating the CanvasResourceProvider interface.
    
    Notes:
    
    * This method is called in BaseRenderingContext2D only here [1], which
      is not called from the BaseRenderingContext2D constructor [2] or
      destructor (implicitly defined).
    * The two subclasses' implementations of GetResourceProvider() are
      different [3][4], which this CL respects.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc;l=745;drc=90941eff28465f834f16704b3543a6fb87054c53?q=IsResourcePRoviderVal&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc;l=129-149;drc=90941eff28465f834f16704b3543a6fb87054c53;bpv=1;bpt=1?q=BaseRenderingContext2D::Base&ss=chromium
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc;l=1207;drc=90941eff28465f834f16704b3543a6fb87054c53
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=313;drc=90941eff28465f834f16704b3543a6fb87054c53
    
    Bug: 352263194
    Change-Id: I7f36ea85d72f565fbc1db0cee7acf8c714adfed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7964422
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650174}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -752,10 +752,6 @@ BaseRenderingContext2D::PaintRenderingResultsToSnapshot(
   return provider->Snapshot();
 }
 
-bool BaseRenderingContext2D::IsResourceProviderValid() {
-  return GetResourceProvider() && GetResourceProvider()->IsValid();
-}
-
 void BaseRenderingContext2D::WillUseCurrentFont() const {
   if (HTMLCanvasElement* canvas = HostAsHTMLCanvasElement();
       canvas != nullptr) {
