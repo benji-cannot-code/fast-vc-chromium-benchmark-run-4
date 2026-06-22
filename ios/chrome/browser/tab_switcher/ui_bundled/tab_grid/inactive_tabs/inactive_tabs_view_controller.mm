1d510e2cc447b9374b1a1c04c27451850ddff4c2
Chris Lu <thegreenfrog@google.com>
2026-06-22 08:52:41 -0700

    [iOS] Fix crash when tapping "Close All" in Inactive Tabs grid
    Initializing the Close All button using `initWithPrimaryAction:` with a
    UIAction causes a private UIKit crash when a popover confirmation dialog
    is presented synchronously from the action handler block.
    
    This CL replaces the UIAction initialization with a standard target
    action.
    
    Fixed: 447777189
    Change-Id: I4111d9d4c940fb85da7d5df19903e1aaf1753a24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7969790
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650344}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/inactive_tabs/inactive_tabs_view_controller.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/inactive_tabs/inactive_tabs_view_controller.mm
@@ -143,16 +143,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [_bottomBar layoutIfNeeded];
   NSString* buttonTitle =
       l10n_util::GetNSString(IDS_IOS_INACTIVE_TABS_CLOSE_ALL_BUTTON);
-  __weak __typeof(self) weakSelf = self;
-  UIAction* closeAllInactiveAction =
-      [UIAction actionWithTitle:buttonTitle
-                          image:nil
-                     identifier:nil
-                        handler:^(UIAction* action) {
-                          [weakSelf didTapCloseAllInactive];
-                        }];
-  _closeAllInactiveButton =
-      [[UIBarButtonItem alloc] initWithPrimaryAction:closeAllInactiveAction];
+  _closeAllInactiveButton = [[UIBarButtonItem alloc]
+      initWithTitle:buttonTitle
+              style:UIBarButtonItemStylePlain
+             target:self
+             action:@selector(didTapCloseAllInactive)];
   _closeAllInactiveButton.accessibilityIdentifier =
       kInactiveTabGridCloseAllButtonIdentifier;
   UIBarButtonItem* flexibleSpace = [[UIBarButtonItem alloc]
