b1866051fa0c177cbb07ee0d32b6396fa1ec8e03
Divyansh Mangal <dmangal@microsoft.com>
2026-06-22 09:51:55 -0700

    [SVG] Remove SvgAnchorElementAttributes and SvgAnchorElementDownloadAttribute flags
    These runtime-enabled features shipped in M142 and M144 respectively.
    Both are stable and can be cleaned up.
    
    Bug: 40589293
    Change-Id: If01dc972ab724746b27ac619934f9eafaac6a7aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7961182
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1650378}

--- content/browser/download/download_browsertest.cc
+++ content/browser/download/download_browsertest.cc
@@ -970,7 +970,7 @@ class DownloadContentTest : public ContentBrowserTest {
  public:
   DownloadContentTest() {
     feature_list_.InitWithFeatures(
-        {blink::features::kSvgAnchorElementDownloadAttribute},
+        {},
         {
             download::features::kAllowDownloadResumptionWithoutStrongValidators,
         });
