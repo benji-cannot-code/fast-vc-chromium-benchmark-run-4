885d65c226242abd845f4e5f1da90a513111955f
Ziad Youssef <ziadyoussef@chromium.org>
2026-06-22 02:49:56 -0700

    Disable disk read ahead after reboot
    I observed that sometimes a reboot can re-enable disk read ahead.
    
    Change-Id: I660290af5a0ef1849fad460e0f7b0797570f0e40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7956612
    Auto-Submit: Ziad Youssef <ziadyoussef@chromium.org>
    Reviewed-by: Mateusz Zbikowski <zbikowski@google.com>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650208}

--- android_webview/tools/pinlist/generate_pinlist.py
+++ android_webview/tools/pinlist/generate_pinlist.py
@@ -191,7 +191,6 @@ def _SetWebViewCommandLineFlags():
 def _InitialSetup(device):
   """Performs initial device setup."""
   _DisableWebViewMemoryPinning(device)
-  _DisableDiskReadAhead(device)
   _SetWebViewCommandLineFlags()
 
   logging.info('Restarting Android framework...')
@@ -213,6 +212,7 @@ def _InitialSetup(device):
   logging.info('Unlocking screen...')
   device.RunShellCommand(['input', 'keyevent', 'KEYCODE_MENU'],
                          check_return=True)
+  _DisableDiskReadAhead(device)
 
 
 def _EnsureLowInitialResidency(device, webview_apk_path, max_retries=10):
