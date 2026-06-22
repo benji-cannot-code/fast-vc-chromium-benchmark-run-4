eecca5d16f76df0992935a604cd2861aa4d254fe
Ankush Singh <ankushkush@google.com>
2026-06-22 11:56:54 -0700

    STTS: Expire tab card labels after 5 days on iOS
    With kSendTabToSelfAutoOpen, tabs received from other devices are
    automatically opened in the background and show a label in the tab
    switcher pane (Googlers-only:
    https://screenshot.googleplex.com/6ZjirsUzrkc6BWz.png). This label
    should get removed upon user interaction, or when it expires (after 5
    days).
    
    This CL introduces the logic to expire the card after the 5 days period.
    
    Bug: 488072250
    Change-Id: I6b073747d1095e7bed194e181281e7836a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7967610
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650471}

--- ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_tab_card_label_data_unittest.mm
+++ ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_tab_card_label_data_unittest.mm
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class SendTabToSelfTabCardLabelDataTest : public PlatformTest {
  protected:
-  base::test::TaskEnvironment task_environment_;
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
 };
 
 // Tests that the label data is successfully attached and is cleared
@@ -53,4 +54,25 @@ TEST_F(SendTabToSelfTabCardLabelDataTest, WebStateDestroyedClearsLabel) {
   // Destroy the WebState. This will trigger WebStateDestroyed, removing the
   // observer and safely destructing the label data.
   web_state.reset();
+}
+
+// Tests that the label data automatically expires after 5 days.
+TEST_F(SendTabToSelfTabCardLabelDataTest, ExpiryClearsLabel) {
+  web::FakeWebState web_state;
+
+  // Attach the label.
+  SendTabToSelfTabCardLabelData::CreateForWebState(&web_state, "remote_device");
+
+  SendTabToSelfTabCardLabelData* label_data =
+      SendTabToSelfTabCardLabelData::FromWebState(&web_state);
+  ASSERT_NE(nullptr, label_data);
+
+  // Fast forward 4 days (less than 5 days). The label should still be valid.
+  task_environment_.FastForwardBy(base::Days(4));
+  EXPECT_NE(nullptr, SendTabToSelfTabCardLabelData::FromWebState(&web_state));
+
+  // Fast forward another 2 days (total 6 days). The label should now be
+  // expired and cleared.
+  task_environment_.FastForwardBy(base::Days(2));
+  EXPECT_EQ(nullptr, SendTabToSelfTabCardLabelData::FromWebState(&web_state));
 }
