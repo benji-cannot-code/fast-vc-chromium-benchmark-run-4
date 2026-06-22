e94f4a5e58c26664c3dddaffedbd1b39ec33db8c
James Sun <jamessun@google.com>
2026-06-22 13:24:57 -0700

    [BNPL on Bling] Add the mediator of ToS
    bug: 521882618
    Change-Id: Id706320767848a216a7656a712627bbfa662fc5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928828
    Commit-Queue: James Sun <jamessun@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650538}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.mm
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.mm
@@ -154,6 +154,7 @@ NSString* const kAirplaneUpRightSymbol = @"airplane.up.right";
 NSString* const kSuitcaseSymbol = @"suitcase";
 NSString* const kCarSymbol = @"car";
 NSString* const kCreditCardSymbol = @"creditcard";
+NSString* const kWalletBifoldSymbol = @"wallet.bifold";
 NSString* const kMicrophoneFillSymbol = @"mic.fill";
 NSString* const kMicrophoneSymbol = @"mic";
 NSString* const kMagnifyingglassSymbol = @"magnifyingglass";
