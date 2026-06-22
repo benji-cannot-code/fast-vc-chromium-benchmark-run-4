920085666f0e5035e6a397106df0ec6e9826f732
Andrew Grieve <agrieve@chromium.org>
2026-06-22 12:45:29 -0700

    Revert "JNI Zero: Remove support for legacy natives"
    This reverts commit 91168a96d0bd044a518fa0eccaab1de1827721a2.
    
    Reason for revert: Missed some legacy natives in webrtc
    
    Failure Link: https://ci.chromium.org/ui/p/webrtc/builders/try/android_arm_dbg/77957/overview
    
    Original change's description:
    > JNI Zero: Remove support for legacy natives
    >
    > Bug: 481689330, 40253109
    > Change-Id: I54eac15b3792adb81332f8db296020955239e312
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957122
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1649740}
    
    Bug: 481689330, 40253109
    Change-Id: Id4e196cdc5b002c8cf1f1f86810151bf1f9431f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7980338
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1650507}

--- third_party/jni_zero/test/java/src/org/jni_zero/SampleUniqueAnnotations.java
+++ third_party/jni_zero/test/java/src/org/jni_zero/SampleUniqueAnnotations.java
@@ -27,4 +27,7 @@ class SampleUniqueAnnotations {
 
     @CalledByNative
     static void useBar1(Bar1.Inner inner) {}
+
+    @NativeClassQualifiedName("Foo::Bar")
+    native void nativeCallWithQualifiedObject(long nativePtr);
 }
