76099bab40d443ec761e983f666107f3094f46be
Ibrahim Kanouche <kanouche@google.com>
2026-06-22 16:56:34 -0700

    [iOS] Add kInfobarBannerRevamp feature flag
    Bug: 517561797
    Change-Id: I6a77ac575f6d332cb19e8d7b0ff5bfc6b5473cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7964734
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650663}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1273,3 +1273,9 @@ BASE_FEATURE(kAiAvatarRingIos, base::FEATURE_DISABLED_BY_DEFAULT);
 bool IsAiAvatarRingIosEnabled() {
   return base::FeatureList::IsEnabled(kAiAvatarRingIos);
 }
+
+BASE_FEATURE(kInfobarBannerRevamp, base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsInfobarBannerRevampEnabled() {
+  return base::FeatureList::IsEnabled(kInfobarBannerRevamp);
+}
