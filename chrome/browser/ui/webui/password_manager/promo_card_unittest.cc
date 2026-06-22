4381f564f7bbf1e5a4e77e5ee81d4d269396946f
Andres Ricardo Perez <andresrperez@chromium.org>
2026-06-22 11:52:00 -0700

    [Gardener] Disable PromoCard tests on ChromeOS
    These are failing on linux-chromeos-chrome.
    
    Bug: 526646990
    Change-Id: Id93eb6e467ba43f90771473bdc0f893e618f279e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7974084
    Auto-Submit: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Commit-Queue: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Owners-Override: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650469}

--- chrome/browser/ui/webui/password_manager/promo_card_unittest.cc
+++ chrome/browser/ui/webui/password_manager/promo_card_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "chrome/browser/affiliations/affiliation_service_factory.h"
 #include "chrome/browser/extensions/api/passwords_private/passwords_private_delegate.h"
 #include "chrome/browser/extensions/api/passwords_private/passwords_private_delegate_factory.h"
@@ -273,7 +274,12 @@ TEST_F(PromoCardCheckupTest, PromoShownWithSavedPasswords) {
   EXPECT_TRUE(promo->ShouldShowPromo());
 }
 
-TEST_F(PromoCardCheckupTest, PromoShownFirstThreeTimes) {
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_PromoShownFirstThreeTimes DISABLED_PromoShownFirstThreeTimes
+#else
+#define MAYBE_PromoShownFirstThreeTimes PromoShownFirstThreeTimes
+#endif
+TEST_F(PromoCardCheckupTest, MAYBE_PromoShownFirstThreeTimes) {
   SavePassword();
 
   ASSERT_THAT(pref_service()->GetList(prefs::kPasswordManagerPromoCardsList),
@@ -295,7 +301,13 @@ TEST_F(PromoCardCheckupTest, PromoShownFirstThreeTimes) {
   EXPECT_TRUE(promo->ShouldShowPromo());
 }
 
-TEST_F(PromoCardCheckupTest, PromoShownIn7DaysAfterDismiss) {
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_PromoShownIn7DaysAfterDismiss \
+  DISABLED_PromoShownIn7DaysAfterDismiss
+#else
+#define MAYBE_PromoShownIn7DaysAfterDismiss PromoShownIn7DaysAfterDismiss
+#endif
+TEST_F(PromoCardCheckupTest, MAYBE_PromoShownIn7DaysAfterDismiss) {
   base::HistogramTester histogram_tester;
 
   SavePassword();
--- chrome/browser/ui/webui/password_manager/promo_cards/move_passwords_promo_unittest.cc
+++ chrome/browser/ui/webui/password_manager/promo_cards/move_passwords_promo_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/api/passwords_private/passwords_private_delegate_factory.h"
 #include "chrome/browser/password_manager/password_manager_test_util.h"
 #include "chrome/browser/sync/sync_service_factory.h"
@@ -134,7 +135,13 @@ TEST_F(PromoCardMovePasswordsTest, PromoShownWithSavedLocalPasswords) {
   EXPECT_TRUE(promo->ShouldShowPromo());
 }
 
-TEST_F(PromoCardMovePasswordsTest, PromoShownIn7DaysAfterDismiss) {
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_PromoShownIn7DaysAfterDismiss \
+  DISABLED_PromoShownIn7DaysAfterDismiss
+#else
+#define MAYBE_PromoShownIn7DaysAfterDismiss PromoShownIn7DaysAfterDismiss
+#endif
+TEST_F(PromoCardMovePasswordsTest, MAYBE_PromoShownIn7DaysAfterDismiss) {
   base::HistogramTester histogram_tester;
   EnableAccountStorage();
   SavePassword();
