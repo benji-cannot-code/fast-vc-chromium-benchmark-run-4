e2ee2026d58b7bed2f201bec91c8f0aadec469a2
Aaron Leventhal <aleventhal@google.com>
2026-06-22 08:09:54 -0700

    [APC] Modal and modeless dialog semantics
    Map modal and modeless dialogs to the new dialog attribute types,
    rather than generic containers.
    
    Bug: 505095203, 515549622
    
    Change-Id: I033e82b3e263300d91a23eadf5a50b7cbb160b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7966078
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@google.com>
    Auto-Submit: Aaron Leventhal <aleventhal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650323}

--- third_party/blink/renderer/modules/content_extraction/ai_page_content_debug_utils.cc
+++ third_party/blink/renderer/modules/content_extraction/ai_page_content_debug_utils.cc
@@ -55,6 +55,10 @@ const char* AttributeTypeToString(
       return "UnorderedList";
     case mojom::blink::AIPageContentAttributeType::kListItem:
       return "ListItem";
+    case mojom::blink::AIPageContentAttributeType::kDialogModal:
+      return "DialogModal";
+    case mojom::blink::AIPageContentAttributeType::kDialogModeless:
+      return "DialogModeless";
   }
   return "Unknown";
 }
