7a80f6422ad827577047712ad60c30d98c63931a
Ibrahim Kanouche <kanouche@google.com>
2026-06-22 07:46:05 -0700

    [iOS][ContextualCues] Add kContextualSuggestionsCues feature flag
    Bug: 517561797
    Change-Id: I5474a10f8a864fd3389c5138b4800348c4ea6723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7969560
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650311}

--- ios/chrome/browser/intelligence/features/features.mm
+++ ios/chrome/browser/intelligence/features/features.mm
@@ -761,6 +761,16 @@ bool IsAppSwitcherAISummarizationEnabled() {
   return base::FeatureList::IsEnabled(kAppSwitcherAISummarization);
 }
 
+BASE_FEATURE(kGeminiContextualSuggestionsCues,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsGeminiContextualSuggestionsCuesEnabled() {
+  if (!IsPageActionMenuEnabled()) {
+    return false;
+  }
+  return base::FeatureList::IsEnabled(kGeminiContextualSuggestionsCues);
+}
+
 #pragma mark - Debugging Features
 
 const char kBWGPromoConsentParams[] = "BWGPromoConsentVariations";
