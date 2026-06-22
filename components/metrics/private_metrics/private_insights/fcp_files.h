8238d7d8cebf8b35d2c3b05c226c53599c242b63
Kamil Jarosz <kjarosz@google.com>
2026-06-22 12:09:35 -0700

    private_insights: Implement FCP's Files interface
    Implement fcp::client::Files interface in FcpFiles using
    base::CreateTemporaryFile to provide cross-platform temporary file
    creation for the Federated Compute Platform (FCP) client.
    
    Bug: b:518646350
    Change-Id: Ic4624416e16065e76937bcf37b0e3098e4254c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7967921
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650477}

---
+++ components/metrics/private_metrics/private_insights/fcp_files.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_FCP_FILES_H_
+#define COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_FCP_FILES_H_
+
+#include <string>
+
+#include "third_party/federated_compute/src/fcp/client/files.h"
+
+namespace private_insights {
+
+class FcpFiles : public fcp::client::Files {
+ public:
+  FcpFiles();
+  ~FcpFiles() override;
+
+  FcpFiles(const FcpFiles&) = delete;
+  FcpFiles& operator=(const FcpFiles&) = delete;
+
+  absl::StatusOr<std::string> CreateTempFile(  // nocheck
+      const std::string& prefix,
+      const std::string& suffix) override;
+};
+
+}  // namespace private_insights
+
+#endif  // COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_FCP_FILES_H_
