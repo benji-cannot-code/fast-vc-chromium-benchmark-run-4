55a52db8112a5768e60fb4eab3f36d3330427537
Diana OuYang <dianaou@google.com>
2026-06-22 12:19:11 -0700

    [Contextual Tasks] Only play zero state animations on DOM loaded
    Before this change, isDomContentLoaded_ was not reset to false when
    top-level navigations began. During onThreadFrameTopLevelNavigation(),
    the page properties (isZeroState/isAiPage) are checked asynchronously.
    Because isInitialFrameLoad_ is false on subsequent navigations, and
    isDomContentLoaded_ remained true from the previous page load,
    playZeroStateAnimations() was called immediately. When the frame
    finished parsing the DOM and sent the domContentLoaded message,
    playZeroStateAnimations() was triggered a second time.
    
    Demo: https://screencast.googleplex.com/cast/NTMwOTQxNTE0MDg4NDQ4MHw1NzcwOWE0MC0zMw
    Bug: b:526622783
    Change-Id: Ib64b86ef827a09d6ca20f8c10c99130e28e9ba3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7980258
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650485}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -999,6 +999,7 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
     this.forcedComposeboxBounds_ = null;
     this.occluders_ = null;
     this.isInputHidden_ = false;
+    this.isDomContentLoaded_ = false;
 
     // Set frame loading to true initially to avoid race conditions.
     this.isFrameLoading = true;
@@ -1020,7 +1021,7 @@ export class ContextualTasksAppElement extends ContextualTasksAppElementBase {
 
     if (isAiPage && isZeroState) {
       this.isZeroState_ = true;
-      if (!this.isInitialFrameLoad_) {
+      if (!this.isInitialFrameLoad_ && this.isDomContentLoaded_) {
         this.playZeroStateAnimations_();
       }
     }
