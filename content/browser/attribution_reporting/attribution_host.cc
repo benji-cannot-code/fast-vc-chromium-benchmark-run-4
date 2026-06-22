3bad7a633126585aa30b24123792babd3b42140c
Kalvin Lee <kdlee@chromium.org>
2026-06-22 06:00:38 -0700

    Use `CurrentTargetFrame()` (split CL)
    Prefer to get a `RenderFrameHost&` rather than a pointer that is (only)
    declared to be non-null.
    
    This CL is scoped to:
    content/browser/attribution_reporting
    
    This CL was uploaded by git cl split.
    
    Bug: 525108538
    Change-Id: I33f2d77065a3339e64a25db2900f978025c8e507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7966579
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650263}

--- content/browser/attribution_reporting/attribution_host.cc
+++ content/browser/attribution_reporting/attribution_host.cc
@@ -368,7 +368,7 @@ void AttributionHost::RegisterDataHost(
     bool is_for_background_requests,
     const std::vector<url::Origin>& reporting_origins) {
   auto suitable_context = AttributionSuitableContext::Create(
-      static_cast<RenderFrameHostImpl*>(receivers_.GetCurrentTargetFrame()));
+      static_cast<RenderFrameHostImpl*>(&receivers_.CurrentTargetFrame()));
   if (!suitable_context.has_value()) {
     return;
   }
@@ -394,7 +394,7 @@ void AttributionHost::NotifyNavigationWithBackgroundRegistrationsWillStart(
     const blink::AttributionSrcToken& attribution_src_token,
     uint32_t expected_registrations) {
   auto suitable_context = AttributionSuitableContext::Create(
-      static_cast<RenderFrameHostImpl*>(receivers_.GetCurrentTargetFrame()));
+      static_cast<RenderFrameHostImpl*>(&receivers_.CurrentTargetFrame()));
   if (!suitable_context.has_value()) {
     return;
   }
@@ -414,7 +414,7 @@ void AttributionHost::RegisterNavigationDataHost(
     mojo::PendingReceiver<attribution_reporting::mojom::DataHost> data_host,
     const blink::AttributionSrcToken& attribution_src_token) {
   auto suitable_context = AttributionSuitableContext::Create(
-      static_cast<RenderFrameHostImpl*>(receivers_.GetCurrentTargetFrame()));
+      static_cast<RenderFrameHostImpl*>(&receivers_.CurrentTargetFrame()));
   if (!suitable_context.has_value()) {
     return;
   }
