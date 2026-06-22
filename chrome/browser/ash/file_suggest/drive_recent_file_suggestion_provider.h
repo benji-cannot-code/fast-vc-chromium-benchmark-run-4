069649dc46add3e9605bd6c87bd5395a66a01cbb
Georg Neis <neis@chromium.org>
2026-06-22 00:04:51 -0700

    ash: Remove FileSuggestionProvider::MaybeUpdateItemSuggestCache
    It has become a no-op. Remove the method from FileSuggestionProvider,
    its implementations, and FileSuggestKeyedService.
    
    Additionally, remove MaybeUpdateCache from ZeroStateDriveProvider along
    with the Drive, Session, and Power observers that existed solely to
    trigger these cache updates, simplifying the provider's dependencies.
    
    Change-Id: Ice014acf65040b3ce9ea985b61d77908ff50d0d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7977338
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650143}

--- chrome/browser/ash/file_suggest/drive_recent_file_suggestion_provider.h
+++ chrome/browser/ash/file_suggest/drive_recent_file_suggestion_provider.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 
 namespace ash {
-class FileSuggestKeyedService;
 
 // A suggestion provider for most recently used drive files.
 class DriveRecentFileSuggestionProvider
@@ -39,8 +38,6 @@ class DriveRecentFileSuggestionProvider
 
   // FileSuggestionProvider:
   void GetSuggestFileData(GetSuggestFileDataCallback callback) override;
-  void MaybeUpdateItemSuggestCache(
-      base::PassKey<FileSuggestKeyedService>) override;
 
   // drive::DriveIntegrationService::Observer:
   void OnDriveIntegrationServiceDestroyed() override;
