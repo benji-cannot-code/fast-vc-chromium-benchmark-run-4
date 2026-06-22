ad9507dc506907168bfe80e4066e5c88be13109b
Chidera Olibie <colibie@google.com>
2026-06-22 07:38:45 -0700

    [wv] Set up memory profiling and callstack collection for renderer process.
    Following up on https://crrev.com/c/7952658 which set up profiling in
    the browser process, this CL sets up heap profiling and sampled call
    stack profile collection for child (renderer) processes in WebView.
    
    Specifically, this CL:
    - Appends the HeapProfilerController command line switch for child processes in AwContentBrowserClient::AppendExtraCommandLineSwitches which turns on profiling.
    - Exposes the SnapshotController Mojo receiver from child processes to the browser process in AwContentClient::ExposeInterfacesToBrowser and binds to the receiver so the bp can trigger snapshots in cp.
    - Registers, binds CallStackProfileCollector interface and sets the parent profile collector for CallStackProfileBuilder to accept profiles from child processes
    
    WANT_LGTM=all
    TAG=agy
    
    Bug: 522385381
    Change-Id: I42f3dc0611db3fb26ff8f0144f13a709073a12ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7956788
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650306}

--- android_webview/renderer/aw_content_renderer_client.cc
+++ android_webview/renderer/aw_content_renderer_client.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/cdm/renderer/key_system_support_update.h"
 #include "components/js_injection/renderer/js_communication.h"
+#include "components/metrics/call_stacks/call_stack_profile_builder.h"
+#include "components/metrics/public/mojom/call_stack_profile_collector.mojom.h"
 #include "components/network_hints/renderer/web_prescient_networking_impl.h"
 #include "components/page_load_metrics/renderer/metrics_render_frame_observer.h"
 #include "components/printing/renderer/print_render_frame_helper.h"
@@ -94,6 +96,15 @@ void AwContentRendererClient::RenderThreadStarted() {
   if (!spellcheck_)
     spellcheck_ = std::make_unique<SpellCheck>(this);
 #endif
+
+  // Set up the profile collector pipe that the renderer process uses to send
+  // profiles to the browser_process.
+  if (base::FeatureList::IsEnabled(features::kWebViewMemoryProfilingClient)) {
+    mojo::PendingRemote<metrics::mojom::CallStackProfileCollector> collector;
+    thread->BindHostReceiver(collector.InitWithNewPipeAndPassReceiver());
+    metrics::CallStackProfileBuilder::SetParentProfileCollectorForChildProcess(
+        std::move(collector));
+  }
 }
 
 void AwContentRendererClient::ExposeInterfacesToBrowser(
