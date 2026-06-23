8030791eacf63db37874edb2df533891642331b7
Tanmoy Mollik <triploblastic@google.com>
2026-06-23 14:26:18 -0700

    [google_apis] Use factory method to create GoogleServiceAuthError
    Bug: 487926694
    Change-Id: I6593543ed2f020df3e29d1b4eb8f36daf586a76d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7947002
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651282}

--- ios/chrome/browser/signin/model/device_accounts_provider_impl.mm
+++ ios/chrome/browser/signin/model/device_accounts_provider_impl.mm
@@ -51,11 +51,9 @@ GoogleServiceAuthError GoogleServiceAuthErrorFromError(
         static_cast<SystemIdentityManagerErrorCode>(error.code);
     switch (error_code) {
       case SystemIdentityManagerErrorCode::kNoAuthenticatedIdentity:
-        return GoogleServiceAuthError(
-            GoogleServiceAuthError::State::ACCOUNT_NOT_FOUND);
+        return GoogleServiceAuthError::CreateAccountNotFound();
       case SystemIdentityManagerErrorCode::kClientIDMismatch:
-        return GoogleServiceAuthError(
-            GoogleServiceAuthError::State::ACCOUNT_NOT_FOUND);
+        return GoogleServiceAuthError::CreateAccountNotFound();
       case SystemIdentityManagerErrorCode::kInvalidTokenIdentity:
         return GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
             GoogleServiceAuthError::InvalidGaiaCredentialsReason::
@@ -71,13 +69,11 @@ GoogleServiceAuthError GoogleServiceAuthErrorFromError(
           GoogleServiceAuthError::InvalidGaiaCredentialsReason::
               CREDENTIALS_REJECTED_BY_SERVER);
     case ios::provider::SigninErrorCategory::kAuthorizationForbiddenError:
-      return GoogleServiceAuthError(
-          GoogleServiceAuthError::SERVICE_UNAVAILABLE);
+      return GoogleServiceAuthError::FromServiceUnavailable("");
     case ios::provider::SigninErrorCategory::kNetworkError:
       return GoogleServiceAuthError::FromConnectionError(net::ERR_FAILED);
     case ios::provider::SigninErrorCategory::kUserCancellationError:
-      return GoogleServiceAuthError(
-          GoogleServiceAuthError::State::REQUEST_CANCELED);
+      return GoogleServiceAuthError::CreateRequestCanceled();
   }
 
   NOTREACHED() << "Unexpected error: "
