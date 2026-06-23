1a12fb0dda83a724c714155db54368fb2ce254eb
Kay Lin <kaiyilin@google.com>
2026-06-22 22:06:24 -0700

    Disable testEditingText_withDesktopModeDisabled on Android Desktop
    Bug: 526811362
    Change-Id: I317eb6a168b566de5311bf21d1f98290dc240fe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7972463
    Owners-Override: Kay Lin <kaiyilin@google.com>
    Auto-Submit: Kay Lin <kaiyilin@google.com>
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Reviewed-by: Fuhsin Liao <fuhsinliao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650770}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarTest.java
@@ -50,6 +50,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -317,6 +318,7 @@ public class LocationBarTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/526811362
     public void testEditingText_withDesktopModeDisabled() {
         OmniboxCapabilities.setHasDesktopExperienceForTesting(false);
         testEditingText(/* expectDesktopMode= */ false);
