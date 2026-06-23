606503ecbf37bfcd41e5a9704d59b9a9cadc7b83
April Kallmeyer <ask@chromium.org>
2026-06-23 15:41:32 -0700

    [PDF Ink Signatures] Approx float in InkTextInfoEq
    Currently the float values are fine-tuned to have exact results. This
    makes extra test diffs if the logic changes order of operations at all.
    
    Bug: 502133184
    Change-Id: Iec9495bdcb18ae62cf485d0fed77d256f2674b6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7988792
    Commit-Queue: April Kallmeyer <ask@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651338}

--- pdf/test/pdf_ink_test_helpers.cc
+++ pdf/test/pdf_ink_test_helpers.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/notreached.h"
+#include "base/numerics/ranges.h"
 #include "base/strings/to_string.h"
 #include "base/values.h"
 #include "pdf/pdf_ink_conversions.h"
@@ -159,9 +160,12 @@ void PrintTo(const InkTextBoxAttributes& info, std::ostream* os) {
 }
 
 bool InkTextInfoEquals(const InkTextInfo& lhs, const InkTextInfo& rhs) {
-  return lhs.font_id == rhs.font_id && lhs.glyphs == rhs.glyphs &&
-         lhs.glyph_positions == rhs.glyph_positions &&
-         lhs.location == rhs.location &&
+  const bool glyph_positions_eq = std::ranges::equal(
+      lhs.glyph_positions, rhs.glyph_positions, [](float lhs, float rhs) {
+        return base::IsApproximatelyEqual(lhs, rhs, 0.01f);
+      });
+  return glyph_positions_eq && lhs.font_id == rhs.font_id &&
+         lhs.glyphs == rhs.glyphs && lhs.location == rhs.location &&
          lhs.is_horizontal == rhs.is_horizontal && lhs.text == rhs.text;
 }
 
