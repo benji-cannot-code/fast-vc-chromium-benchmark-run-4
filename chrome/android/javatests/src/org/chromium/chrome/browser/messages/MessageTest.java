330091902257ca01b9d6fda6fa87aa98f9cbea1f
marco <gmarcoesau@google.com>
2026-06-23 09:37:31 -0700

    [a11y] Re-enable MessageTest
    There was flakiness introduced when checking the visibility of the
    occluded node right after displaying the message and doing so once. Now
    we poll until we've reached the desired visibility.
    
    Bug: 514878860
    Change-Id: If6aa65e2b69ddbc366f076bc9ca5c5f6e1a8d521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7934272
    Commit-Queue: Marco Esau Garcia Martinez <gmarcoesau@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Aeryn Moss <aerynm@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651068}

--- chrome/android/javatests/src/org/chromium/chrome/browser/messages/MessageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/messages/MessageTest.java
@@ -28,7 +28,6 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -106,7 +105,6 @@ public class MessageTest {
     /** Test that the message container occludes the web contents. */
     @Test
     @SmallTest
-    @DisabledTest(message = "b/514878860")
     // TODO(crbug.com/514848255): Failing on other larger form factors.
     @Restriction(DeviceFormFactor.PHONE)
     @Features.EnableFeatures({AccessibilityFeatures.ACCESSIBILITY_HANDLE_OCCLUDING_VIEWS})
@@ -142,7 +140,7 @@ public class MessageTest {
         CriteriaHelper.pollUiThread(
                 () -> wcax.getAccessibilityNodeProvider() != null,
                 "AccessibilityNodeProvider should be initialized",
-                8000L,
+                CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL_LONG,
                 CriteriaHelper.DEFAULT_POLLING_INTERVAL);
 
         CriteriaHelper.pollUiThread(
@@ -152,7 +150,7 @@ public class MessageTest {
                     return buttonNodeId[0] != -1;
                 },
                 "Button should be found",
-                8000L,
+                CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL_LONG,
                 CriteriaHelper.DEFAULT_POLLING_INTERVAL);
 
         ThreadUtils.runOnUiThreadBlocking(
@@ -167,14 +165,15 @@ public class MessageTest {
 
         onView(withId(R.id.message_primary_button)).check(matches(isDisplayed()));
 
-        ThreadUtils.runOnUiThreadBlocking(
+        CriteriaHelper.pollUiThread(
                 () -> {
                     AccessibilityNodeInfoCompat buttonNode =
                             wcax.createAccessibilityNodeInfo(buttonNodeId[0]);
-                    Assert.assertFalse(
-                            "Button node should be mostly occluded, thus not visible",
-                            buttonNode.isVisibleToUser());
-                });
+                    return !buttonNode.isVisibleToUser();
+                },
+                "Button node should be mostly occluded, thus not visible",
+                CriteriaHelper.DEFAULT_MAX_TIME_TO_POLL_LONG,
+                CriteriaHelper.DEFAULT_POLLING_INTERVAL);
     }
 
     /**
