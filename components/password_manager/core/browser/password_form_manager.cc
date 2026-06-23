3d67c06957a80826684ed98c704a7ca378416b59
Maria Kazinova <kazinova@google.com>
2026-06-23 07:55:09 -0700

    [Passwords] Prevent refilling after manual fill in PasswordFormManager
    This CL adds a mechanism to suppress automatic password filling in
    PasswordFormManager if the user has already manually filled the form.
    This prevents subsequent credential fetches from overriding the user's
    manual input. This is done by adding a new member variable
    ignore_filling_after_manual_fill_ to PasswordFormManager to track if a
    manual fill has occurred.
    
    Bug: 496552761
    Change-Id: I2d46fae786a41b8546b2bff6b25f52d17dc65b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849382
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651010}

--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -356,6 +356,7 @@ PasswordFormManager::~PasswordFormManager() {
 }
 
 void PasswordFormManager::OnPasswordFilledManually() {
+  allow_filling_upon_fetching_ = false;
   if (!base::FeatureList::IsEnabled(features::kPasswordDateLastFilled) ||
       !parsed_submitted_form_ ||
       !password_save_manager_->IsEqualToSavedMatch()) {
@@ -976,15 +977,17 @@ void PasswordFormManager::OnFetchCompleted() {
     CreatePendingCredentials();
   }
 
-  if (IsHttpAuth()) {
-    // No server prediction for http auth, so no need to wait.
-    FillHttpAuth();
-  } else if (parser_.server_predictions() ||
-             !wait_for_server_predictions_for_filling_) {
-    ReportTimeBetweenStoreAndServerUMA();
-    FillNow();
-  } else if (!async_predictions_waiter_.IsActive()) {
-    DelayFillForServerSidePredictions();
+  if (allow_filling_upon_fetching_) {
+    if (IsHttpAuth()) {
+      // No server prediction for http auth, so no need to wait.
+      FillHttpAuth();
+    } else if (parser_.server_predictions() ||
+               !wait_for_server_predictions_for_filling_) {
+      ReportTimeBetweenStoreAndServerUMA();
+      FillNow();
+    } else if (!async_predictions_waiter_.IsActive()) {
+      DelayFillForServerSidePredictions();
+    }
   }
 
   if (should_schedule_save_for_later_) {
@@ -1194,6 +1197,10 @@ void PasswordFormManager::Fill() {
 }
 
 void PasswordFormManager::FillNow() {
+  if (!allow_filling_upon_fetching_) {
+    return;
+  }
+
   if (!driver_) {
     return;
   }
@@ -1365,6 +1372,7 @@ void PasswordFormManager::RecordMetricOnReadonly(
 }
 
 void PasswordFormManager::ReportTimeBetweenStoreAndServerUMA() {
+  // TODO(crbug.com/525318107): Prevent multiple records of this metric.
   if (!received_stored_credentials_time_.is_null()) {
     UMA_HISTOGRAM_TIMES("PasswordManager.TimeBetweenStoreAndServer",
                         TimeTicks::Now() - received_stored_credentials_time_);
