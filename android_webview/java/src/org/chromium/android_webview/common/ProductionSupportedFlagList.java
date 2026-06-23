d36b8e8517f64faa5663a3ed9d744d24482043cb
kenoss <kenoss@chromium.org>
2026-06-23 09:17:05 -0700

    CancelUnrelatedPrefetch: Add feature flag for WebView
    Bug: 499176365
    Change-Id: I871979bcc24bc23d01aee43a9c4ca9f98d1fed55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7966900
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Auto-Submit: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651053}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1392,6 +1392,9 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature(
                 AwFeatures.WEBVIEW_DOWNLOAD_FAVICONS,
                 "Determines whether a Favicon will be downloaded upon navigation."),
+        Flag.baseFeature(
+                "PrefetchCancelUnrelatedPrefetch",
+                "Cancels unrelated prefetch when a navigation is started."),
         // Add new commandline switches and features above. The final entry should have a
         // trailing comma for cleaner diffs.
     };
