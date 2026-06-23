5903a39adbb5ce9c235fbd2dc943d2f3ef8b2b63
Thomas Lukaszewicz <tluk@chromium.org>
2026-06-23 13:23:27 -0700

    [permissions] Update PiP check in PermissionPromptBaseView
    There are no intended behavior changes.
    
    The method IsForPictureInPictureWindow() is added to check whether
    the host is a PiP browser or a standaline PiP host.
    
    Bug: 522965795
    Change-Id: I5c076555df1aa30c3e6dc0213bb0e131bb596d01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7976337
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651233}

--- chrome/browser/ui/views/permissions/permission_prompt_base_view.cc
+++ chrome/browser/ui/views/permissions/permission_prompt_base_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/bubble_anchor_util_views.h"
+#include "chrome/browser/ui/views/picture_in_picture/document_pip_host.h"
 #include "chrome/browser/ui/views/title_origin_label.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
@@ -88,10 +89,6 @@ PermissionPromptBaseView::PermissionPromptBaseView(
                                /*autosize=*/true),
       WebContentsObserver(web_contents),
       url_identity_(GetUrlIdentity(web_contents, *delegate)),
-      is_for_picture_in_picture_window_(
-          GetBrowser() &&
-          GetBrowser()->GetType() ==
-              BrowserWindowInterface::Type::TYPE_PICTURE_IN_PICTURE),
       record_browser_always_active_value_(GetBrowser() &&
                                           GetBrowser()->IsActive()) {
   // To prevent permissions being accepted accidentally, and as a security
@@ -251,7 +248,7 @@ std::u16string PermissionPromptBaseView::GetBlockText(
 void PermissionPromptBaseView::StartTrackingPictureInPictureOcclusion() {
   // If we're for a picture-in-picture window, then we are in an always-on-top
   // widget that should be tracked by the PictureInPictureOcclusionTracker.
-  if (is_for_picture_in_picture_window_) {
+  if (IsForPictureInPictureWindow()) {
     PictureInPictureOcclusionTracker* tracker =
         PictureInPictureWindowManager::GetInstance()->GetOcclusionTracker();
     if (tracker) {
@@ -301,6 +298,14 @@ void PermissionPromptBaseView::SetTitleBoldedRanges(
 void PermissionPromptBaseView::DidBecomeInactive(
     BrowserWindowInterface* browser_window_interface) {
   record_browser_always_active_value_ = false;
+}
+
+bool PermissionPromptBaseView::IsForPictureInPictureWindow() const {
+  if (const BrowserWindowInterface* browser = GetBrowser()) {
+    return browser->GetType() ==
+           BrowserWindowInterface::Type::TYPE_PICTURE_IN_PICTURE;
+  }
+  return !!DocumentPipHost::FromChildWebContents(web_contents());
 }
 
 BEGIN_METADATA(PermissionPromptBaseView)
