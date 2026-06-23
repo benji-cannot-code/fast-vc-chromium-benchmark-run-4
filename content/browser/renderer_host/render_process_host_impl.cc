39e577bc726934c8372b6a08e267ea2cf1609ca1
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-06-23 00:50:28 -0700

    [Navigation] Individualize CHECK-exclusion TODOs into separate bugs
    Individualizing the bulk CHECK-exclusion TODOs in core navigation code
    into separate, trackable bugs. Some developers prefer to track
    those using bugganizer bugs.
    
    The original TODOs were grouped under bulk bugs (crbug.com/497761255
    and crbug.com/503784536) after being reverted due to failures in the
    wild, but I didn't take time to create a bug for each manually.
    
    Future bugs are now handled by the infra DCHECK-to-CHECK pipeline.
    
    Bug: None
    Change-Id: I8f40631ce37e1061bc727faa20e9464bbe680352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7978703
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650823}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1708,7 +1708,7 @@ RenderProcessHostImpl::RenderProcessHostImpl(
           perfetto::NamedTrack::FromPointer("RenderProcessHostImpl",
                                             this,
                                             GetChildProcessTracingTrack(id_))) {
-  // TODO(https://crbug.com/497761255): CHECK-exclusion: Convert to CHECK once
+  // TODO(https://crbug.com/526543416): CHECK-exclusion: Convert to CHECK once
   // we are sure this isn't hit.
   DCHECK(!browser_context->ShutdownStarted());
   TRACE_EVENT("shutdown", "RenderProcessHostImpl",
@@ -2262,7 +2262,7 @@ void RenderProcessHostImpl::InitializeSharedMemoryRegionsOnceChannelIsUp() {
         base::AtomicSharedMemory<base::TimeTicks>::Create(
             priority_.is_background() ? base::TimeTicks()
                                       : base::TimeTicks::Now());
-    // TODO(https://crbug.com/497761255): CHECK-exclusion: Convert to CHECK once
+    // TODO(https://crbug.com/526542975): CHECK-exclusion: Convert to CHECK once
     // we are sure this isn't hit.
     DCHECK(last_foreground_time_region_.has_value());
   }
