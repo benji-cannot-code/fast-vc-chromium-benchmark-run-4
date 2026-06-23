2a5b443232a4b861dab808e1ce74f0f64fe341db
Jenna Himawan <jhimawan@google.com>
2026-06-23 09:38:57 -0700

    Make AppMenuAccessibilityDelegate a static class
    Follow-up to crrev.com/c/7945737.
    
    Bug: 469073403
    Change-Id: I977d59dd148c57d5eee3008198514cf7f66d5e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7981677
    Reviewed-by: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651070}

--- chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemViewBinder.java
+++ chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenuItemViewBinder.java
@@ -57,6 +57,9 @@ class AppMenuItemViewBinder {
         R.id.button_wrapper_five
     };
 
+    private static final View.AccessibilityDelegate sAccessibilityDelegate =
+            new AppMenuAccessibilityDelegate();
+
     /* package */ static void bindStandardItem(PropertyModel model, View view, PropertyKey key) {
         if (key == AppMenuItemProperties.MENU_ITEM_ID) {
             int id = model.get(AppMenuItemProperties.MENU_ITEM_ID);
@@ -237,15 +240,7 @@ class AppMenuItemViewBinder {
                     } else {
                         button.setCheckable(false);
                         button.setSelected(isChecked);
-                        button.setAccessibilityDelegate(
-                                new View.AccessibilityDelegate() {
-                                    @Override
-                                    public void onInitializeAccessibilityNodeInfo(
-                                            View host, AccessibilityNodeInfo info) {
-                                        super.onInitializeAccessibilityNodeInfo(host, info);
-                                        info.setSelected(false);
-                                    }
-                                });
+                        button.setAccessibilityDelegate(sAccessibilityDelegate);
                     }
 
                     setupMenuButton(button, iconList.get(i).model, appMenuClickHandler);
@@ -428,5 +423,13 @@ class AppMenuItemViewBinder {
 
         // Menu items may be hidden by command line flags before they get to this point.
         button.setVisibility(View.VISIBLE);
+    }
+
+    private static final class AppMenuAccessibilityDelegate extends View.AccessibilityDelegate {
+        @Override
+        public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfo info) {
+            super.onInitializeAccessibilityNodeInfo(host, info);
+            info.setSelected(false);
+        }
     }
 }
