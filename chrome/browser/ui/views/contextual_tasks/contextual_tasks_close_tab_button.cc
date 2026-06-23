64dafdeee41d06733e133863741485c17a711dfd
Bryan Nguyen <nguyenbryan@google.com>
2026-06-23 12:24:05 -0700

    [contextual-tasks] Add histogram for close tab button.
    The user action was already implemented; this adds a histogram with the
    same name.
    
    Bug: 524504532
    Change-Id: I53d7f26501d5596f4ece3f9abd013d7b8c7f300a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7965686
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651187}

--- chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_tab_button.cc
+++ chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_tab_button.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_tab_button.h"
 
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_button_controller.h"
@@ -67,6 +68,10 @@ void ContextualTasksCloseTabButton::OnButtonPress() {
   base::RecordAction(
       base::UserMetricsAction("ContextualTasks.ToolbarCloseTabButton."
                               "UserAction.CloseTabAndExpandSidePanel"));
+  base::UmaHistogramBoolean(
+      "ContextualTasks.ToolbarCloseTabButton.UserAction."
+      "CloseTabAndExpandSidePanel",
+      true);
 }
 
 void ContextualTasksCloseTabButton::OnShouldUpdateVisibility(bool should_show) {
