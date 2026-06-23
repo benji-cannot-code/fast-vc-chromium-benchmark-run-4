e68a47e87bbf6f85ffa531c261e95be068a9a77a
Devon Loehr <dloehr@google.com>
2026-06-23 07:17:54 -0700

    [rustmojo] Support disconnect notifications for associated interfaces
    This CL adds the ability for `MultiplexRouter`s to send and receive
    control messages, the most notable of which is a disconnect
    notification. These are sent whenever one end of an _associated_ pair
    is dropped. They aren't needed for the primary pair, since dropping
    one of those closes the entire pipe.
    
    These messages are defined in a Mojom file, but that file isn't
    actually used, because the bindings crate is part of the Mojom
    compilation process. To get around that, we redefine the relevant parts
    of the message format manually, and send the messages directly via the
    underlying endpoint.
    
    There are several kinds of control message, but the only ones we support
    in Rust right now are disconnect notifications; the other kinds will be
    silently ignored.
    
    Bug: 517547791
    Change-Id: I72e28b1dfacd9186236299862118b7c8eb9a3c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957243
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1650982}

--- mojo/public/rust/bindings/multiplex_router/mod.rs
+++ mojo/public/rust/bindings/multiplex_router/mod.rs
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! rest of the crate.
 
 mod arc_or_weak;
+mod control_messages;
 mod endpoint_registry;
 mod handle;
 mod response_sender;
