290463ea94e4feafae80b4939b0f6995ba64d96f
Kent Tamura <tkent@chromium.org>
2026-06-22 22:34:47 -0700

    css/selectors: Automate a part of legacy selectors tests (20 to 39)
    Introduce common.js to provide assert_background_color(),
    assert_not_background_color(), LIME, and RED.
    
    Change-Id: Ibfc1a0fecfc25a9fc60280edbacb5b21d9d2feda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7960878
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650778}

---
+++ third_party/blink/web_tests/external/wpt/css/selectors/old-tests/common.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const LIME = 'rgb(0, 255, 0)';
+const RED = 'rgb(255, 0, 0)';
+
+function assert_background_color(elementOrId, colorString, comment) {
+  const element = typeof elementOrId === 'string' ? document.getElementById(elementOrId) : elementOrId;
+  assert_equals(getComputedStyle(element).backgroundColor, colorString, comment);
+}
+
+function assert_not_background_color(elementOrId, colorString, comment) {
+  const element = typeof elementOrId === 'string' ? document.getElementById(elementOrId) : elementOrId;
+  assert_not_equals(getComputedStyle(element).backgroundColor, colorString, comment);
+}
