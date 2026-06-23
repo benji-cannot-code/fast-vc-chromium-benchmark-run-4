70f2045dccfca69fa15c90991ea0d6a8a1b13642
Chirag Arora <heychirag@google.com>
2026-06-23 08:04:18 -0700

    Revert "Update ChromeMetricsServicesManagerClient to use MetricsReportingLevelController"
    This reverts commit 236a72fa00d966e71cdcf84c6c22a928fe61366a.
    
    Reverted CL: crrev.com/c/7842692
    
    This change is being reverted because we have changed the backend
    implementation approach.
    
    Bug: b:492510818
    Change-Id: I2765a39c8802c973402e69c709cf7fd1567e79e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7979211
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651013}

--- chrome/browser/metrics/chrome_metrics_services_manager_client.cc
+++ chrome/browser/metrics/chrome_metrics_services_manager_client.cc
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/installer/util/google_update_settings.h"
 #include "components/metrics/enabled_state_provider.h"
 #include "components/metrics/metrics_pref_names.h"
-#include "components/metrics/metrics_reporting_choice_service.h"
-#include "components/metrics/metrics_reporting_level.h"
 #include "components/metrics/metrics_state_manager.h"
 #include "components/prefs/pref_service.h"
 #include "components/variations/service/variations_service.h"
@@ -60,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/ash/settings/metrics_reporting_level_controller.h"
 #include "chrome/browser/ash/settings/stats_reporting_controller.h"
 #include "components/metrics/structured/recorder.h"
 #endif  // BUILDFLAG(IS_CHROMEOS)
@@ -143,29 +140,10 @@ bool IsClientInSampleImpl(PrefService* local_state) {
 #if BUILDFLAG(IS_CHROMEOS)
 // Callback to update the metrics reporting state when the Chrome OS metrics
 // reporting setting changes.
-// TODO(b/492510818): Remove once migration to metrics reporting level
-// completes.
 void OnCrosMetricsReportingSettingChange(
     metrics::ChangeMetricsReportingStateCalledFrom called_from) {
-  if (metrics::MetricsReportingChoiceService::
-          ShouldUseMetricsConsentRestructure(
-              g_browser_process->local_state())) {
-    return;
-  }
   bool enable_metrics = ash::StatsReportingController::Get()->IsEnabled();
   metrics::ChangeMetricsReportingState(enable_metrics, called_from);
-}
-
-void OnCrosMetricsReportingLevelChange(
-    metrics::ChangeMetricsReportingStateCalledFrom called_from) {
-  if (!metrics::MetricsReportingChoiceService::
-          ShouldUseMetricsConsentRestructure(
-              g_browser_process->local_state())) {
-    return;
-  }
-  metrics::MetricsReportingLevel level =
-      ash::MetricsReportingLevelController::Get()->GetLevel();
-  metrics::ChangeMetricsReportingState(level, called_from);
 }
 #endif
 
@@ -299,28 +277,13 @@ bool ChromeMetricsServicesManagerClient::GetSamplingRatePerMille(int* rate) {
 #if BUILDFLAG(IS_CHROMEOS)
 void ChromeMetricsServicesManagerClient::OnCrosSettingsCreated() {
   // Listen for changes to metrics reporting state.
-  // TODO(b/492510818): Remove once migration to metrics reporting level
-  // completes.
   reporting_setting_subscription_ =
       ash::StatsReportingController::Get()->AddObserver(
           base::BindRepeating(&OnCrosMetricsReportingSettingChange,
                               metrics::ChangeMetricsReportingStateCalledFrom::
                                   kCrosMetricsSettingsChange));
-
-  // Listen for changes to metrics reporting level.
-  reporting_level_setting_subscription_ =
-      ash::MetricsReportingLevelController::Get()->AddObserver(
-          base::BindRepeating(&OnCrosMetricsReportingLevelChange,
-                              metrics::ChangeMetricsReportingStateCalledFrom::
-                                  kCrosMetricsSettingsChange));
-
   // Invoke the callback once initially to set the metrics reporting state.
-  // TODO(b/492510818): Remove once migration to metrics reporting level
-  // completes.
   OnCrosMetricsReportingSettingChange(
-      metrics::ChangeMetricsReportingStateCalledFrom::
-          kCrosMetricsSettingsCreated);
-  OnCrosMetricsReportingLevelChange(
       metrics::ChangeMetricsReportingStateCalledFrom::
           kCrosMetricsSettingsCreated);
 }
