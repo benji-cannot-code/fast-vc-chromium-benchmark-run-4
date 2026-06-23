0baba81361180e47f524a536d3aecfe500a90d01
Joshua Hood <jdh@chromium.org>
2026-06-23 13:51:04 -0700

    [iOS] Detect paste keyboard shortcuts in PhishGuard
    Bug: 517702279
    Change-Id: I11ac62eceda961b4863bc055ed2b154c539beef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921650
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: jdh <jdh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651247}

--- ios/chrome/browser/passwords/model/ios_chrome_password_reuse_detection_manager_client.mm
+++ ios/chrome/browser/passwords/model/ios_chrome_password_reuse_detection_manager_client.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/passwords/model/ios_chrome_password_reuse_detection_manager_client.h"
 
+#import <UIKit/UIKit.h>
+
 #import <memory>
 #import <utility>
 
@@ -163,6 +165,23 @@ void IOSChromePasswordReuseDetectionManagerClient::OnKeyPressed(
 void IOSChromePasswordReuseDetectionManagerClient::OnPaste(
     const std::string text) {
   password_reuse_detection_manager_.OnPaste(base::UTF8ToUTF16(text));
+}
+
+void IOSChromePasswordReuseDetectionManagerClient::OnPasteKeyDetected() {
+  UIPasteboard* const pasteboard = [UIPasteboard generalPasteboard];
+  if ([pasteboard hasStrings]) {
+    NSString* pasted_text = pasteboard.string;
+    if (pasted_text.length > 0) {
+      // Slicing to a generous suffix prevents expensive UTF16 conversions on
+      // huge clipboards while preserving actionable data.
+      if (pasted_text.length > 1000) {
+        pasted_text =
+            [pasted_text substringFromIndex:pasted_text.length - 1000];
+      }
+      password_reuse_detection_manager_.OnPaste(
+          base::SysNSStringToUTF16(pasted_text));
+    }
+  }
 }
 
 web::WebState* IOSChromePasswordReuseDetectionManagerClient::web_state() const {
