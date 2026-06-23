71250d0474c2b0149fefbf266ecead572db18982
Amya Singhal <amyasinghal@google.com>
2026-06-23 07:29:39 -0700

    Voice Typing: implement enterprise policy for voice typing
    This CL adds an enterprise policy for the Voice Typing feature. It
    connects it to a browser-side pref "voice_typing_settings" and adjusts
    enabling status in DictationKeyedService.
    
    Bug: b:524231216
    Change-Id: I0893fc99ddec4aeaddc5cf1375bb233a20ea8c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7962321
    Commit-Queue: Amya Singhal <amyasinghal@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650995}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1776,6 +1776,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   RegisterBrowserUserPrefs(registry);
   registry->RegisterBooleanPref(prefs::kRestrictYouTubeCookiesDeletion, false);
   RegisterGeminiSettingsPrefs(registry);
+  registry->RegisterIntegerPref(prefs::kVoiceTypingSettings, 0);
   RegisterPrefersDefaultScrollbarStylesPrefs(registry);
   RegisterSafetyHubProfilePrefs(registry);
 #if BUILDFLAG(IS_CHROMEOS)
