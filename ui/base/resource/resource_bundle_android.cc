9c212066bff51ab5f549c199cd67676edf8f8f3f
Andrew Grieve <agrieve@chromium.org>
2026-06-22 19:45:28 -0700

    Make android_browsertests use proper locale .pak files
    It was setting setNoAvailableLocalePaks(), and then C++ fallback logic
    was loading .pak files from test data. Since the pak files are already
    in the .apk, just use them as normal.
    
    Bug: 525859933
    Change-Id: I2515059972648dca0c3ee2bc805df064fa8a8658
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7981137
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1650731}

--- ui/base/resource/resource_bundle_android.cc
+++ ui/base/resource/resource_bundle_android.cc
@@ -404,11 +404,6 @@ const std::vector<ResourceBundle::FdAndRegion>& GetLocalePaks() {
   return locale_packs;
 }
 
-void SetNoAvailableLocalePaksForTest() {
-  Java_ResourceBundle_setNoAvailableLocalePaks(
-      base::android::AttachCurrentThread());
-}
-
 void UnloadAndroidLocaleResources() {
   GetLocalePaksGlobal().clear();
 }
