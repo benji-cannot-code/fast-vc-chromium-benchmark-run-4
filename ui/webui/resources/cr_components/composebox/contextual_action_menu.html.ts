81dec5712c2fac1061f8e9a808703844a1c199a8
Ananya Seelam <ananyaseelam@google.com>
2026-06-23 11:21:56 -0700

    [context-input] Disable Add Tabs in WebUI "+" menu when incompatible
    In the composebox context menu, the "Add tabs" label remained enabled
    even when all individual tabs inside it were disabled. This CL ensures
    the label is disabled.
    
    NTP: http://screencast/cast/NTE1NTU4MDc1MzczOTc3NnxkN2VlNDI5Zi1hZg
    Cobrowse: http://screencast/cast/NjY3NTMwODA4OTQ0MjMwNHxlOTBkNWNmMC05OQ
    
    Bug: b:524253069
    Change-Id: I357384a14461db8da2fe79f7d060b902d891cd93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7964031
    Reviewed-by: Wenhu Cheng <wenhuc@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651133}

--- ui/webui/resources/cr_components/composebox/contextual_action_menu.html.ts
+++ ui/webui/resources/cr_components/composebox/contextual_action_menu.html.ts
@@ -34,6 +34,7 @@ export function getHtml(this: ContextualActionMenuElement) {
                   role="menuitem"
                   aria-haspopup="menu"
                   aria-expanded="${this.shareTabsFlyoutOpen}"
+                  ?disabled="${this.isShareTabsTriggerDisabled_()}"
                   @pointerenter="${this.onShareTabsRowPointerenter_}"
                   @pointerleave="${this.onShareTabsRowPointerleave_}"
                   @keydown="${this.onShareTabsRowKeydown_}">
