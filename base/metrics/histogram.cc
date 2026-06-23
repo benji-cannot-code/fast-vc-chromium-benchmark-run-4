2de1ecd9ec08c6c37b17add0b049a5ba50ed1333
Fiona Verzivolli <fionav@google.com>
2026-06-23 13:57:31 -0700

    Bypass Single Sample Optimization in Histograms
    This change introduces `kDisableSingleSampleOptimizationFlag` in HistogramBase. When this flag is used during Histogram creation, it forces the underlying SampleVectorBase instances to immediately allocate and mount their counts storage, effectively disabling the single-sample optimization. This change is part of making LockMetricsRecorder thread safe, and will be used to pre-allocate histograms before we start recording lock acquisitions
    
    Bug: 518786382
    
    Change-Id: I093e37820e4d4dce46d8a3fadd0c9561bbc96947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7898905
    Reviewed-by: Anand Ravi <anandrv@google.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651257}

--- base/metrics/histogram.cc
+++ base/metrics/histogram.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/lock.h"
+#include "base/types/pass_key.h"
 #include "base/values.h"
 #include "build/build_config.h"
 
@@ -292,6 +293,17 @@ HistogramBase* Histogram::Factory::Build() {
     return DummyHistogram::GetInstance();
   }
   return histogram;
+}
+
+// TODO(crbug.com/527037963) - Refactor Histogram so that histograms are created
+// with the correct flags and SampleVectorBase is fully initialized in
+// single-sample-disabled mode, instead of calling SetFlags after creation.
+void Histogram::SetFlags(int32_t flags) {
+  HistogramBase::SetFlags(flags);
+  if (flags & HistogramBase::kDisableSingleSampleOptimizationFlag) {
+    unlogged_samples_->DisableSingleSample(base::PassKey<Histogram>());
+    logged_samples_->DisableSingleSample(base::PassKey<Histogram>());
+  }
 }
 
 HistogramBase* Histogram::FactoryGet(std::string_view name,
