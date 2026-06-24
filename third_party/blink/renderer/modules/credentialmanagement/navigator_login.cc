c7573001e8c8d14af2001719288973d0bb536f15
Nicolás Peña <npm@chromium.org>
2026-06-24 07:18:38 -0700

    [FedCM] Add missing SetIdpSigninStatus calls
    Missed in crrev.com/c/7963348
    
    Bug: 519217823
    Change-Id: I86be0b61fa372c77f99c4be17fde154920393438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7982396
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651698}

--- third_party/blink/renderer/modules/credentialmanagement/navigator_login.cc
+++ third_party/blink/renderer/modules/credentialmanagement/navigator_login.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/credentialmanagement/credential_manager_type_converters.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -48,8 +49,7 @@ ScriptPromise<IDLUndefined> NavigatorLogin::setStatus(
     ScriptState* script_state,
     const V8LoginStatus& v8_status) {
   auto* context = ExecutionContext::From(script_state);
-  auto* request =
-      CredentialManagerProxy::From(script_state)->FederatedAuthRequest();
+  auto* proxy = CredentialManagerProxy::From(script_state);
 
   auto* resolver =
       MakeGarbageCollected<ScriptPromiseResolver<IDLUndefined>>(script_state);
@@ -64,9 +64,15 @@ ScriptPromise<IDLUndefined> NavigatorLogin::setStatus(
       status = mojom::blink::IdpSigninStatus::kSignedOut;
       break;
   }
-  request->SetIdpSigninStatus(
-      context->GetSecurityOrigin(), status, nullptr,
-      BindOnce(&OnSetIdpSigninStatus, WrapPersistent(resolver)));
+  if (RuntimeEnabledFeatures::FedCmMultipleRequestsEnabled(context)) {
+    proxy->FederatedRequestService()->SetIdpSigninStatus(
+        context->GetSecurityOrigin(), status, nullptr,
+        BindOnce(&OnSetIdpSigninStatus, WrapPersistent(resolver)));
+  } else {
+    proxy->FederatedAuthRequest()->SetIdpSigninStatus(
+        context->GetSecurityOrigin(), status, nullptr,
+        BindOnce(&OnSetIdpSigninStatus, WrapPersistent(resolver)));
+  }
   return promise;
 }
 
@@ -75,8 +81,7 @@ ScriptPromise<IDLUndefined> NavigatorLogin::setStatus(
     const V8LoginStatus& v8_status,
     const LoginStatusOptions* options) {
   auto* context = ExecutionContext::From(script_state);
-  auto* request =
-      CredentialManagerProxy::From(script_state)->FederatedAuthRequest();
+  auto* proxy = CredentialManagerProxy::From(script_state);
 
   auto* resolver =
       MakeGarbageCollected<ScriptPromiseResolver<IDLUndefined>>(script_state);
@@ -115,10 +120,17 @@ ScriptPromise<IDLUndefined> NavigatorLogin::setStatus(
     }
   }
 
-  request->SetIdpSigninStatus(
-      context->GetSecurityOrigin(), status,
-      mojo::ConvertTo<blink::mojom::blink::LoginStatusOptionsPtr>(*options),
-      BindOnce(&OnSetIdpSigninStatus, WrapPersistent(resolver)));
+  if (RuntimeEnabledFeatures::FedCmMultipleRequestsEnabled(context)) {
+    proxy->FederatedRequestService()->SetIdpSigninStatus(
+        context->GetSecurityOrigin(), status,
+        mojo::ConvertTo<blink::mojom::blink::LoginStatusOptionsPtr>(*options),
+        BindOnce(&OnSetIdpSigninStatus, WrapPersistent(resolver)));
+  } else {
+    proxy->FederatedAuthRequest()->SetIdpSigninStatus(
+        context->GetSecurityOrigin(), status,
+        mojo::ConvertTo<blink::mojom::blink::LoginStatusOptionsPtr>(*options),
+        BindOnce(&OnSetIdpSigninStatus, WrapPersistent(resolver)));
+  }
   return promise;
 }
 
