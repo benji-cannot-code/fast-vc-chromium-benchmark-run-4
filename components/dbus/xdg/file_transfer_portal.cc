49c0ef8051ecaf85b90952bf031c3d1d2aaf44c1
Tom Anderson <thomasanderson@chromium.org>
2026-06-24 12:11:25 -0700

    Linux: Enable XdgFileTransferPortal by default
    Enable the kXdgFileTransferPortal feature by default to allow Chromium
    to securely send and receive files via copy-paste and drag-and-drop when
    running inside a sandboxed environment (like Flatpak).
    
    Fixed: 40898398
    Change-Id: I41ad30dced655d1143f8001832ab03688fcaaf2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7979978
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651878}

--- components/dbus/xdg/file_transfer_portal.cc
+++ components/dbus/xdg/file_transfer_portal.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace dbus_xdg {
 
-BASE_FEATURE(kXdgFileTransferPortal, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kXdgFileTransferPortal, base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace {
 
