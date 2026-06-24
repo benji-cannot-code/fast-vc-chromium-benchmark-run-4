070318d8341e9d8a149fe8cdb42f5f992b6ddd15
Igor Sheludko <ishell@chromium.org>
2026-06-24 03:51:18 -0700

    [v8] Prepare for making v8::MicrotasksScope stack allocated
    Bug: 515252150
    Change-Id: I6e8b046a42e1eda9d5787bdb3cfdcc67de3a409c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7992437
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651613}

--- third_party/blink/renderer/bindings/core/v8/v8_microtasks_scope.h
+++ third_party/blink/renderer/bindings/core/v8/v8_microtasks_scope.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/scheduler/public/event_loop.h"
+#include "v8/include/cppgc/macros.h"
 #include "v8/include/v8-microtask-queue.h"
 
 namespace blink {
@@ -21,6 +22,8 @@ using MicrotasksScopeMode = v8::MicrotasksScope::Type;
 
 template <MicrotasksScopeMode kMode>
 class V8MicrotasksScope {
+  CPPGC_STACK_ALLOCATED();
+
  public:
   explicit V8MicrotasksScope(ScriptState* script_state)
       : V8MicrotasksScope(ExecutionContext::From(script_state)) {}
