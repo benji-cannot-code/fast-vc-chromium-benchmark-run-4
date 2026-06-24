7e9986000a494ade820a1c38a1a6999a7f907223
Keishi Hattori <keishi@google.com>
2026-06-24 00:03:50 -0700

    [wtf] Add active iterator checks for Vector
    The plan was to use BRP ref count for Vector but it turned out
    that the atomics was a huge performance overhead.
    This CL adds active iterator checks that were added for HeapVector
    to Vector too.
    
    This CL also changes the CL to DumpWithoutCrash instead of crashing on
    detection.
    
    Change-Id: I2d91dc90bb9d084db672b46a79eff9db6fb68b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951919
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651533}

--- third_party/blink/renderer/platform/heap/collection_support/heap_vector.h
+++ third_party/blink/renderer/platform/heap/collection_support/heap_vector.h
@@ -169,7 +169,8 @@ using HeapVector = BasicHeapVector<internal::HeapCollectionType::kDisallowNew,
                                    T,
                                    inlineCapacity>;
 static_assert(IsDisallowNew<HeapVector<int>>);
-#if !BUILDFLAG(ENABLE_HEAP_VECTOR_ACTIVE_ITERATOR_CHECKS)
+#if !BUILDFLAG(ENABLE_HEAP_VECTOR_ACTIVE_ITERATOR_CHECKS) && \
+    !BUILDFLAG(ENABLE_VECTOR_ACTIVE_ITERATOR_CHECKS)
 ASSERT_SIZE(Vector<int>, HeapVector<int>);
 #endif
 
@@ -178,7 +179,8 @@ template <typename T, wtf_size_t inlineCapacity = 0>
 using GCedHeapVector =
     BasicHeapVector<internal::HeapCollectionType::kGCed, T, inlineCapacity>;
 static_assert(!IsDisallowNew<GCedHeapVector<int>>);
-#if !BUILDFLAG(ENABLE_HEAP_VECTOR_ACTIVE_ITERATOR_CHECKS)
+#if !BUILDFLAG(ENABLE_HEAP_VECTOR_ACTIVE_ITERATOR_CHECKS) && \
+    !BUILDFLAG(ENABLE_VECTOR_ACTIVE_ITERATOR_CHECKS)
 ASSERT_SIZE(Vector<int>, GCedHeapVector<int>);
 #endif
 
---
+++ third_party/blink/renderer/platform/wtf/vector.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/wtf/vector.h"
+
+#include <atomic>
+
+#include "base/compiler_specific.h"
+#include "base/debug/dump_without_crashing.h"
+
+namespace blink {
+
+NOINLINE void ReportVectorBackingFreedWhileIteratorsAlive() {
+  // Capture a dump to report the use-after-free hazard of freeing a Vector
+  // backing while iterators are alive. Throttling is applied by
+  // DumpWithoutCrashing internally, but we also use a local relaxed atomic
+  // to avoid lock acquisition and map lookup overhead in a tight loop.
+  static std::atomic<bool> has_dumped{false};
+  if (has_dumped.load(std::memory_order_relaxed)) {
+    return;
+  }
+  if (!has_dumped.exchange(true, std::memory_order_relaxed)) {
+    base::debug::DumpWithoutCrashing();
+  }
+}
+
+}  // namespace blink
