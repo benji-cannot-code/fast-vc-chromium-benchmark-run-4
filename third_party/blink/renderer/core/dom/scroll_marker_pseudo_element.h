b549f09b40e646edf286e39282422cd376aa3232
Daniil Sakhapov <sakhapov@chromium.org>
2026-06-24 07:08:44 -0700

    [carousel] Fix crash due to disconnected selected marker
    ScrollMarkerGroupData::Selected() could return a stale reference to a
    disconnected ScrollMarkerPseudoElement if it was removed from the DOM
    but still referenced as the selected marker. This caused crashes during
    tree traversal or selector matching (e.g., in CheckPseudoClass when
    calling ComparePreorderTreePosition).
    
    Ensure that selected_marker_ references are synchronously cleared during
    lifecycle teardown instead of returning stale references.
    
    Reset selected_marker_ to nullptr inside
    ScrollMarkerGroupData::ClearFocusGroup() when the group is cleared or
    detached from the layout tree.
    
    Override RemovedFrom() in ScrollMarkerPseudoElement to call
    SetScrollMarkerGroup(nullptr) synchronously when removed from the DOM.
    
    Fixed: 507340749
    Change-Id: Iff7c3b5649194aac2bc07392425f6596c1fbfae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7799741
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651695}

--- third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.h
+++ third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.h
@@ -32,6 +32,7 @@ class ScrollMarkerPseudoElement : public PseudoElement {
 
   void AttachLayoutTree(AttachContext&) final;
   void Dispose() final;
+  void RemovedFrom(ContainerNode&) override;
   void Trace(Visitor* v) const final;
 
   void SetHovered(bool hovered) final;
