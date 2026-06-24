7b3624d77d5d862954a09af0d6c70f8d627bb184
Karol Sygiet <sygiet@google.com>
2026-06-24 07:39:04 -0700

    Fix address small form rules for server predictions
    This CL aims to fix the discrepancy between the server side and client
    side implementations of handing the small address form.
    
    Server currently does not suppress suggestions if any of them was for
    non address type, which includes the AutofillAi types.
    Source: go/autofill-server-small-address-forms-handling
    
    Chromium behaviour is modified to remove the explicit bypass for
    AutofillAi types and instead handle them normally.
    
    Bug: 469428128
    Change-Id: I63aa61daef3cf0c6ff37264224e22fc22ba116a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7991261
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Rahim Mammadli <rahimis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651705}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding_unittest.cc
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding_unittest.cc
@@ -4403,10 +4403,11 @@ const ClearSmallAddressFormPredictions_TestCase
                 },
         },
         {
-            // Verifies that AutofillAI classifications are protected but
-            // address classifications of small forms are wiped. This is legacy
-            // behavior. It's unclear if it's ideal.
-            .test_name = "NoWipeOfFormsAIClassifications",
+            // Verifies that if a small form contains an AutofillAI
+            // classification, the form is recognized as a non-address form and
+            // is NOT suppressed. Therefore, standard address predictions (like
+            // EMAIL_ADDRESS) are kept.
+            .test_name = "NoWipeInCaseOfFormsAIClassifications",
             .received_predictions =
                 {
                     {CreateFieldPrediction(
@@ -4419,8 +4420,11 @@ const ClearSmallAddressFormPredictions_TestCase
             .expected_predictions =
                 {
                     {CreateFieldPrediction(
-                        NATIONAL_ID_CARD_NUMBER,
-                        FieldPrediction::SOURCE_AUTOFILL_AI)},
+                         EMAIL_ADDRESS,
+                         FieldPrediction::SOURCE_AUTOFILL_DEFAULT),
+                     CreateFieldPrediction(
+                         NATIONAL_ID_CARD_NUMBER,
+                         FieldPrediction::SOURCE_AUTOFILL_AI)},
                 },
         },
 };
