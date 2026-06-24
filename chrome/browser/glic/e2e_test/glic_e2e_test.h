9fa17f96a41783487a1f7afaf2f67f9fc488065b
Carlos Knippschild <carlosk@chromium.org>
2026-06-24 15:28:17 -0700

    Abort test on WebUI & web client error pages
    Bug: b:525047483
    Change-Id: Ia7b7cb1bc9fa81974f10b3ceeedf07416a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7989054
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651996}

--- chrome/browser/glic/e2e_test/glic_e2e_test.h
+++ chrome/browser/glic/e2e_test/glic_e2e_test.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_GLIC_E2E_TEST_GLIC_E2E_TEST_H_
 #define CHROME_BROWSER_GLIC_E2E_TEST_GLIC_E2E_TEST_H_
 
+#include "base/scoped_observation.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/autofill/captured_sites_test_utils.h"
+#include "chrome/browser/glic/host/host.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/service/glic_instance_coordinator.h"
 #include "chrome/browser/signin/e2e_tests/live_test.h"
@@ -35,7 +37,8 @@ using captured_sites_test_utils::WebPageReplayServerWrapper;
 namespace glic::test {
 
 // Note: Requires --run-live-tests to run any of the tests.
-class GlicE2ETest : public InteractiveBrowserTestMixin<signin::test::LiveTest> {
+class GlicE2ETest : public InteractiveBrowserTestMixin<signin::test::LiveTest>,
+                    public Host::Observer {
  public:
   explicit GlicE2ETest(const std::vector<base::test::FeatureRef>&
                            additional_enabled_features = {},
@@ -59,6 +62,9 @@ class GlicE2ETest : public InteractiveBrowserTestMixin<signin::test::LiveTest> {
   void TearDownOnMainThread() override;
 
   void PreRunTestOnMainThread() override;
+
+  // Host::Observer implementation:
+  void WebUiStateChanged(glic::mojom::WebUiState state) override;
 
   MultiStep WaitForAndInstrumentFre();
 
@@ -99,11 +105,18 @@ class GlicE2ETest : public InteractiveBrowserTestMixin<signin::test::LiveTest> {
     test_account_label_ = test_account_label;
   }
 
+  // Used in tests that expect Glic loading to fail with one of the error states
+  // (error pages) that are monitored by WebUiStateChanged and cause an early
+  // failure.
+  void set_expects_error(bool expects_error) { expects_error_ = expects_error; }
+
  protected:
   // Opt-in flag for using WPR for some requests in real_backend mode.
   bool use_wpr_for_real_backend_ = false;
 
  private:
+  void OnActiveInstanceChanged(GlicInstance* new_instance);
+
   base::test::ScopedFeatureList scoped_feature_list_;
   base::test::ScopedFeatureList exempt_actor_policy_control_feature_list_;
   bool enable_low_bandwidth_tests_ = false;
@@ -114,6 +127,11 @@ class GlicE2ETest : public InteractiveBrowserTestMixin<signin::test::LiveTest> {
            std::unique_ptr<content::TestDevToolsProtocolClient>>
       devtools_clients_;
   std::string test_account_label_;
+
+  bool expects_error_ = false;
+  base::CallbackListSubscription active_instance_subscription_;
+  base::ScopedObservation<Host, Host::Observer> host_observation_{this};
+  base::WeakPtrFactory<GlicE2ETest> weak_ptr_factory_{this};
 };
 
 // Observes Actor task state changes.
