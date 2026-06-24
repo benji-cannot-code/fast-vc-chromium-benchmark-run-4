42339009c463a4853dc3c4e4245ae2866650ab96
Min Qin <qinmin@chromium.org>
2026-06-24 14:13:50 -0700

    Reland "Remove the logic to check for externally removed download on browser"
    This reverts commit b6cc82c23ebaf16c859bde77c4c0b999089f12ab.
    
    Reason for revert: fixed failed test
    
    Original change's description:
    > Revert "Remove the logic to check for externally removed download on browser"
    >
    > This reverts commit 1f455657098c067f3714d295a0a05e2c8b1b262d.
    >
    > Reason for revert: Test org.chromium.chrome.browser.contextmenu.ContextMenuTest#testSaveImageBlockedByPolicy failing on android-automotive-12l-x64-rel-tests. Reverted the CL and the test passed locally: https://ci.chromium.org/ui/inv/u-kaiyilin-2026-06-23-04-34-00-9a3172bb43f016b3/test-results
    >
    > Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-automotive-12l-x64-rel-tests/21272/overview
    >
    > Original change's description:
    > > Remove the logic to check for externally removed download on browser
    > > startup
    > >
    > > There is no need to check for externally removed download on startup.
    > > This is already done in DownloadOfflineContentProvider when download
    > > home is opened. So no need to do it early on browser startup
    > >
    > > Bug: 514438970
    > > Change-Id: Ic83718e1f702c299955a99d3ceb1fd8a1cfa84ea
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7965683
    > > Commit-Queue: Min Qin <qinmin@chromium.org>
    > > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1650514}
    >
    > Bug: 514438970
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I76440e0934c4e7fa0ab308d54dd67d7497258087
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7976782
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Kay Lin <kaiyilin@google.com>
    > Owners-Override: Kay Lin <kaiyilin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1650784}
    
    Bug: 514438970
    Change-Id: Ib9c66fbe5e0017f7fc509574be66b1936e7e74e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7987977
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651975}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestHelper.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestHelper.java
@@ -200,6 +200,7 @@ class DownloadTestHelper {
 
     private String mLastDownloadFilePath;
     private CallbackHelper mHttpDownloadFinished = new CallbackHelper();
+    private final CallbackHelper mAllDownloadsRetrieved = new CallbackHelper();
     private TestDownloadManagerServiceObserver mDownloadManagerServiceObserver;
     private TestDownloadBackendObserver mDownloadBackendObserver;
 
@@ -222,10 +223,16 @@ class DownloadTestHelper {
     }
 
     List<DownloadItem> getAllDownloads() {
+        int callCount = mAllDownloadsRetrieved.getCallCount();
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     DownloadManagerService.getDownloadManagerService().getAllDownloads(null);
                 });
+        try {
+            mAllDownloadsRetrieved.waitForCallback(callCount, 1, 10, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            throw new AssertionError("Timed out waiting for downloads to be retrieved", e);
+        }
         return mAllDownloads;
     }
 
@@ -234,6 +241,7 @@ class DownloadTestHelper {
         @Override
         public void onAllDownloadsRetrieved(final List<DownloadItem> list, ProfileKey profileKey) {
             mAllDownloads = list;
+            mAllDownloadsRetrieved.notifyCalled();
         }
 
         @Override
