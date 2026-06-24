c4007fee7b3302b886dd0045ad6e9778b2c9a232
Henrique Nakashima <hnakashima@chromium.org>
2026-06-24 13:10:58 -0700

    [Android] Convert RecentTabsPageTestUtils to RecentTabsPageStation
    Bug: 527084464
    Change-Id: I9ecb8e691eb77c04928abb6328e2f15953587bd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7982395
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651933}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/RecentTabsPageStation.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/RecentTabsPageStation.java
@@ -5,17 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test.transit.page;
 
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
+import org.chromium.base.test.transit.Element;
 import org.chromium.base.test.transit.ViewElement;
-import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ntp.RecentTabsPage;
+import org.chromium.components.embedder_support.util.UrlConstants;
 
 /** A station representing the Recent Tabs page. */
 public class RecentTabsPageStation extends CtaPageStation {
+    public static final String RECENT_TABS_URL = UrlConstants.RECENT_TABS_URL;
+    public final Element<RecentTabsPage> nativePageElement;
+
     protected RecentTabsPageStation(Config config) {
         super(config);
         declareView(URL_BAR, ViewElement.unscopedOption());
-        declareView(withText(R.string.recently_closed));
+        nativePageElement =
+                declareEnterConditionAsElement(
+                        new NativePageCondition<>(RecentTabsPage.class, loadedTabElement));
     }
 
     public static Builder<RecentTabsPageStation> newBuilder() {
