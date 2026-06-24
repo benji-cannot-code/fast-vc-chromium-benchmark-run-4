612b7fa5bf9d3780b9fbee0c0612a9f0e17f1971
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-06-24 10:49:41 -0700

    Revert "lpm: Fix duplicate proto symbols when using any.proto"
    This reverts commit 190ebfac9fb254c48d09eee1236c09341a26905e.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8678095202558153697
    
    Sample failed build: https://ci.chromium.org/b/8678095202558153697
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7958022&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8678095202558153697&type=BUG
    
    Original change's description:
    > lpm: Fix duplicate proto symbols when using any.proto
    >
    > QUICHE started to use any.proto. LPM also uses any.proto but it uses the
    > full version. Because we can't have both version at the same time,
    > QUICHE tried to use the full version when fuzzing.
    >
    > Unfortunately this didn't work either and broke all LPM fuzzers. This is
    > because the any_full_proto target compiled as a static_library so it was
    > linked into all libraries that used it. Since full protos are registered
    > globally, this caused QUICHE and LPM to register the same proto twice
    > which caused a crash.
    >
    > To fix the issue, we change any_full_proto to be a
    > fuzzable_proto_library essentially compiling as a shared library. This
    > way the protos are only registered once. We also revert
    > https://crrev.com/c/7957615 which fixed LPM fuzzers but broke coverage
    > bots.
    >
    > Also adds a smoke test that runs an LPM fuzzer and verifies it doesn't
    > crash on startup. This ensures changes that break LPM fuzzers are caught
    > by the CQ.
    >
    > LPM fuzzers are broken in a few bots that we don't fuzz in, so this
    > fixes some cases (printing) and disables the fuzzer/test in others.
    >
    > Bug: 524216727, 526664486
    > Change-Id: I62faae99e1146e398d15c785f3c11ac46c9169cb
    > Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,linux-libfuzzer-high-end-asan-rel,chromeos-libfuzzer-asan-rel,mac-libfuzzer-asan-rel,win-libfuzzer-asan-rel,linux-centipede-asan-rel,win_chromium_compile_dbg_ng
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7958022
    > Reviewed-by: Tigran Bantikyan <bantikyan@google.com>
    > Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: David Schinazi <dschinazi@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1651791}
    >
    
    Bug: 524216727, 526664486
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I9478b5dc6b52a15c623205eccf30174ba8f73d1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7990617
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1651833}

--- testing/libfuzzer/tests/fuzz_target.cc
+++ testing/libfuzzer/tests/fuzz_target.cc
@@ -50,7 +50,6 @@ base::CommandLine FuzzTarget::LibfuzzerCommandLine(
       "-artifact_prefix=",
       temp_dir_.GetPath().AppendASCII("crash-").MaybeAsASCII(),
   }));
-  cmd.AppendArg("-rss_limit_mb=0");
   return cmd;
 }
 
