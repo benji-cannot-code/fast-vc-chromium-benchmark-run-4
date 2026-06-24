5571a494b67e09725ce3afe9038114adcead3f6b
Bhaskar Sharma <bhaskarsharma@google.com>
2026-06-24 10:46:16 -0700

    Block chrome.tabs.duplicate for Isolated Web Apps.
    Isolated Web App URLs ('isolated-app:' scheme) are not allowed to be
    opened via chrome.tabs.duplicate.
    Extensions must use chrome.windows.create to open Isolated Web Apps.
    
    Fixed: 523637452
    Change-Id: I47b01d3826dc63d1eb00e35376685dacad52b211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7986105
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Vlad Krot <vkrot@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Bhaskar Sharma <bhaskarsharma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651828}

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -150,6 +150,8 @@ constexpr char kTabsCreateIwaUrlNotAllowedError[] =
 constexpr char kTabsUpdateIwaUrlNotAllowedError[] =
     "Cannot navigate to a URL with the 'isolated-app:' scheme via tabs.update. "
     "Use windows.create instead.";
+constexpr char kCannotDuplicateIwaTabError[] =
+    "The tab of an Isolated Web App cannot be duplicated.";
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
@@ -2372,6 +2374,13 @@ ExtensionFunction::ResponseAction TabsDuplicateFunction::Run() {
       !ExtensionTabUtil::IsTabStripEditable(*browser->GetProfile())) {
     return RespondNow(Error(ExtensionTabUtil::kTabStripNotEditableError));
   }
+
+#if !BUILDFLAG(IS_ANDROID)
+  if (web_contents->GetLastCommittedURL().SchemeIs(
+          webapps::kIsolatedAppScheme)) {
+    return RespondNow(Error(kCannotDuplicateIwaTabError));
+  }
+#endif
 
   TabListInterface* tab_list = TabListInterface::From(browser);
   if (!tab_list) {
