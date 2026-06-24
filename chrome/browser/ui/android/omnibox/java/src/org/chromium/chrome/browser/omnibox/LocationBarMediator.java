e9353e034d914e2bd5568405b30b5700f8cc393f
Sky Malice <skym@chromium.org>
2026-06-24 09:31:57 -0700

    [AIM] Fix mic and nav button alignment on phones.
    NO_IFTTT=Not adding/removing any components
    
    Bug: 524994260
    Change-Id: I8ed2de638c28a68d17b11bfa3e6abb62833b73ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7988837
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651770}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -1391,7 +1391,6 @@ class LocationBarMediator
                 mDropdown.getId(),
                 ConstraintSet.BOTTOM);
         set.connect(R.id.mic_button, ConstraintSet.TOP, mDropdown.getId(), ConstraintSet.BOTTOM);
-        set.connect(R.id.mic_button, ConstraintSet.END, R.id.navigate_button, ConstraintSet.START);
         set.connect(
                 R.id.navigate_button, ConstraintSet.TOP, mDropdown.getId(), ConstraintSet.BOTTOM);
 
