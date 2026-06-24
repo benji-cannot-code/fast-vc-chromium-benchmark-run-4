c23ef49e7513bcb57da0cca60c862f836daf8e97
Samar <samarchehade@google.com>
2026-06-24 04:49:10 -0700

    [Extensions][Al] Implement reauth flow for extensions.
    Bug: 403867715
    Change-Id: Icdae496b67ab5e9aec2c9919c0d65d3cf75537de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7964805
    Reviewed-by: Łukasz Barczyński <lbarczynski@google.com>
    Commit-Queue: Samar Chehade <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651633}

--- chrome/browser/signin/signin_ui_delegate_impl_android.cc
+++ chrome/browser/signin/signin_ui_delegate_impl_android.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/check.h"
-#include "base/notimplemented.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/android/signin_bridge.h"
@@ -76,7 +75,21 @@ void SigninUiDelegateImplAndroid::ShowReauthUI(
     bool enable_sync,
     signin_metrics::AccessPoint access_point,
     signin_metrics::PromoAction promo_action) {
-  NOTIMPLEMENTED() << "Not yet implemented on Android";
+  CHECK(profile);
+  CHECK(profile->IsRegularProfile());
+
+  content::WebContents* web_contents = GetActiveWebContents(profile);
+  if (!web_contents) {
+    // TODO(crbug.com/403867715): Open a new tab in this case.
+    return;
+  }
+  CoreAccountId account_id = IdentityManagerFactory::GetForProfile(profile)
+                                 ->FindExtendedAccountInfoByEmailAddress(email)
+                                 .account_id;
+  CHECK(!account_id.empty());
+  SigninBridgeFactory::GetForProfile(profile)->StartUpdateCredentialsFlow(
+      TabAndroid::FromWebContents(web_contents),
+      /*continue_url=*/web_contents->GetLastCommittedURL(), account_id);
 }
 
 }  // namespace signin_ui_util
