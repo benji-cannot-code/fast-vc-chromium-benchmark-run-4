476cb9a2d33ef95aa9c66d349775d5265aebbabb
Bryant Chandler <bryantchandler@google.com>
2026-06-24 12:34:14 -0700

    [glic] Add a feature param to block cold warming based RAM
    This change adds a new feature param, `glic-warming-min-required-ram-mb`
    under the `kGlicWarming` feature, defaulting to 0. During prepreload
    checks, GlicProfileManager verifies if the device's total physical
    memory is below this limit. If it is, prepreload is blocked with a new
    result `kDeviceLowMemory`.
    
    Browser tests were also added to cover the low memory prewarming
    constraint.
    
    Bug: b:527495843
    Change-Id: Id5c546b71d5ed0600f887c2205d87ed36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7995498
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651894}

--- chrome/browser/glic/glic_profile_manager.cc
+++ chrome/browser/glic/glic_profile_manager.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/glic/glic_profile_manager.h"
 
+#include "base/byte_size.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/memory_coordinator/utils.h"
 #include "base/notimplemented.h"
+#include "base/system/sys_info.h"
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
@@ -322,6 +324,10 @@ void GlicProfileManager::CanPreloadForProfile(Profile* profile,
 
   if (IsUnderMemoryPressure()) {
     return produce_result(GlicPrewarmingChecksResult::kUnderMemoryPressure);
+  }
+  if (base::SysInfo::AmountOfTotalPhysicalMemory() <
+      base::MiBU(features::kGlicWarmingMinRequiredRamMb.Get())) {
+    return produce_result(GlicPrewarmingChecksResult::kDeviceLowMemory);
   }
   if (!g_prewarming_enabled_for_testing_) {
     return produce_result(
