826466ab03a3e579059049d99f5186e4a2c7f205
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-06-23 18:38:23 -0700

    Updates ResizeToggleMenu to use OnWidgetClosing to manage lifetime
    This revert the change to ResizeToggleMenu done by the CL
    https://crrev.com/c/7838679.
    
    Bug: 525302184
    Change-Id: I6653fec6124273c453d88fa1bb36b034614e580d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7975803
    Reviewed-by: Toshiki Kikuchi <toshikikikuchi@chromium.org>
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651432}

--- chromeos/ash/experiences/arc/compat_mode/resize_toggle_menu.h
+++ chromeos/ash/experiences/arc/compat_mode/resize_toggle_menu.h
@@ -78,6 +78,7 @@ class ResizeToggleMenu : public views::WidgetObserver,
   ~ResizeToggleMenu() override;
 
   // views::WidgetObserver:
+  void OnWidgetClosing(views::Widget* widget) override;
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
 
@@ -94,8 +95,6 @@ class ResizeToggleMenu : public views::WidgetObserver,
 
  private:
   friend class ResizeToggleMenuTest;
-
-  void OnBubbleWidgetClosing(views::Widget::ClosedReason reason);
 
   void ApplyResizeCompatMode(ash::ResizeCompatMode mode);
 
@@ -114,8 +113,8 @@ class ResizeToggleMenu : public views::WidgetObserver,
 
   raw_ptr<ArcResizeLockPrefDelegate> pref_delegate_;
 
-  base::ScopedObservation<views::Widget, views::WidgetObserver>
-      widget_observation_{this};
+  base::ScopedMultiSourceObservation<views::Widget, views::WidgetObserver>
+      widget_observations_{this};
   base::ScopedObservation<aura::Window, aura::WindowObserver>
       window_observation_{this};
 
