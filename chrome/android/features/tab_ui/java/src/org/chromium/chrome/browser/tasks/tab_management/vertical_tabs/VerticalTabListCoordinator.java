4e20805e6d5f2f03236d0c9cdd2a76e5fb8c1bfb
Jack Thiesen <jthiesen@chromium.org>
2026-06-23 19:49:14 -0700

    [Vertical Tabs] Implement Mouse Handling
    Screen recording: https://screencast.googleplex.com/cast/NjUzMzc5NjAzMDU3ODY4OHxiMmI4YTFkMi0zOQ
    
    When a left-click occurs over a tab in the VT side panel:
     - On the first frame, the tab clicked is selected
     - While the mouse button is held and the mouse is moved, the tab will
       be dragged by the mouse
     - When the mouse button is released, the drag is ended
    
    This functionality is implemented in a separate TouchListener inside of
    VerticalTabListItemTouchHelperCallback (e.g. not in ItemTouchHelper2).
    
    The touch slop size is decreased for mouse to prevent janky feel.
    
    Bug: 518307037
    Change-Id: I7822eac224c994b3dd4cbb4d5256c30447903503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7960642
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Eleanor Lee <eleanorlee@google.com>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1651452}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabListCoordinator.java
@@ -330,6 +330,10 @@ public class VerticalTabListCoordinator {
 
         ItemTouchHelper2 itemTouchHelper =
                 new ItemTouchHelper2(touchHelperCallback, /* externalLongPressHandler= */ null);
+
+        recyclerView.addOnItemTouchListener(
+                touchHelperCallback.createMouseDragDetector(itemTouchHelper));
+
         itemTouchHelper.attachToRecyclerView(recyclerView);
 
         recyclerView.addOnItemTouchListener(
