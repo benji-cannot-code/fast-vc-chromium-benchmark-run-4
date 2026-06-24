a3b87784e3979b46ac2d5aa6bb0d214a68e57075
James Lee <ljjlee@google.com>
2026-06-24 04:59:17 -0700

    Update kForceGoogleSafeSearch pref when a supervised account signs out
    This ensures that policies like Google SafeSearch, which are updated
    based on the authentication state, are correctly reset when a user signs
    out. A test is added to verify that Google SafeSearch is no longer
    forced after signing out.
    
    Bug: 527036253
    Change-Id: I28e4d5694ac069a9638ab96dc2582d563ba374db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7992835
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1651635}

--- components/supervised_user/core/browser/child_account_service.cc
+++ components/supervised_user/core/browser/child_account_service.cc
@@ -149,6 +149,10 @@ void ChildAccountService::SetSupervisionStatusAndNotifyObservers(
     std::move(callback).Run();
   }
   status_received_callback_list_.clear();
+
+  // It's possible the supervision status change is caused by sign-in /
+  // sign-out event, which would also update the Google auth state.
+  OnAuthStateUpdated();
 }
 
 void ChildAccountService::OnPrimaryAccountChanged(
@@ -204,7 +208,6 @@ void ChildAccountService::OnExtendedAccountInfoUpdated(
 
   SetSupervisionStatusAndNotifyObservers(info.IsChildAccount() ==
                                          signin::Tribool::kTrue);
-  OnAuthStateUpdated();
 }
 
 void ChildAccountService::OnRefreshTokenUpdatedForAccount(
