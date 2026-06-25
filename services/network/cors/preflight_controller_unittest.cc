aad0b178a7fad0df4c541c1967dae39da04c5fc9
Hubert Chao <hchao@chromium.org>
2026-06-25 11:50:13 -0700

    [LNA] remove ClientSecurityState from preflight_controller.[h/cc]
    PreflightController no longer needs the client security state as PNA is
    gone and LNA doesn't do preflights.
    
    Bug: 394636065
    Change-Id: Id1fb6dbd83ddf643f20c8b8587a7eab498c5266a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7974582
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652586}

--- services/network/cors/preflight_controller_unittest.cc
+++ services/network/cors/preflight_controller_unittest.cc
@@ -282,7 +282,6 @@ TEST(PreflightControllerOptionsTest, CheckOptions) {
       NonWildcardRequestHeadersSupport(false),
       /*tainted=*/false, TRAFFIC_ANNOTATION_FOR_TESTS, &url_loader_factory,
       net::IsolationInfo(),
-      /*client_security_state=*/nullptr,
       /*devtools_observer=*/
       base::WeakPtr<mojo::Remote<mojom::DevToolsObserver>>(), net_log, true,
       mojo::PendingRemote<mojom::URLLoaderNetworkServiceObserver>());
@@ -293,7 +292,6 @@ TEST(PreflightControllerOptionsTest, CheckOptions) {
       NonWildcardRequestHeadersSupport(false),
       /*tainted=*/false, TRAFFIC_ANNOTATION_FOR_TESTS, &url_loader_factory,
       net::IsolationInfo(),
-      /*client_security_state=*/nullptr,
       /*devtools_observer=*/
       base::WeakPtr<mojo::Remote<mojom::DevToolsObserver>>(), net_log, true,
       mojo::PendingRemote<mojom::URLLoaderNetworkServiceObserver>());
@@ -516,8 +514,7 @@ class PreflightControllerTest : public testing::Test {
   void PerformPreflightCheck(
       const ResourceRequest& request,
       bool tainted = false,
-      net::IsolationInfo isolation_info = net::IsolationInfo(),
-      mojom::ClientSecurityStatePtr client_security_state = nullptr) {
+      net::IsolationInfo isolation_info = net::IsolationInfo()) {
     DCHECK(preflight_controller_);
     run_loop_ = std::make_unique<base::RunLoop>();
 
@@ -531,8 +528,7 @@ class PreflightControllerTest : public testing::Test {
         0, request, WithTrustedHeaderClient(false),
         non_wildcard_request_headers_support_, tainted,
         TRAFFIC_ANNOTATION_FOR_TESTS, url_loader_factory_remote_.get(),
-        isolation_info, std::move(client_security_state),
-        weak_devtools_observer_factory.GetWeakPtr(),
+        isolation_info, weak_devtools_observer_factory.GetWeakPtr(),
         net::NetLogWithSource::Make(net::NetLog::Get(),
                                     net::NetLogSourceType::URL_REQUEST),
         true, mojo::PendingRemote<mojom::URLLoaderNetworkServiceObserver>());
