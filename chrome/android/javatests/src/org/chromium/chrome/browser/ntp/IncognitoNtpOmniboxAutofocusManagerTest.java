204adb45bada7119c98764c1c52bade72f12e818
Michael Wu <mklw@google.com>
2026-06-24 17:33:50 -0700

    Re-enable Incognito tests on Desktop bots for Desktop-like Incognito
    Bug: 479863847
    Change-Id: I27a301af490cc4739ee0ed6900624393d5f548d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7988401
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652094}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoNtpOmniboxAutofocusManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoNtpOmniboxAutofocusManagerTest.java
@@ -26,7 +26,6 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -60,7 +59,6 @@ import org.chromium.ui.test.util.DeviceRestriction;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Batch(Batch.PER_CLASS)
-@DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/479863847
 public class IncognitoNtpOmniboxAutofocusManagerTest {
     /**
      * The maximum time to wait for omnibox focus and keyboard visibility. On some devices the
