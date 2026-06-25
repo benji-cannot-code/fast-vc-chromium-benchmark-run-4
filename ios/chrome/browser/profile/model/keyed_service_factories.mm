428da8e7a898093a7d9f01e90c25d717341a0996
Yerassyl Kutylbek <kutylbeky@google.com>
2026-06-25 01:42:28 -0700

    Enable Optimization Guide core and implement PrivateAiServiceFactory on iOS.
    This this change sets up the service class factory on iOS, allowing them
    to be integrated into browser features:
     - Removes the !is_ios check in optimization guide core BUILD.gn to
       compile and test PrivateAiModelExecutionFetcher on iOS.
     - Removes strict CHECK(network_context) from PrivateAiService
       constructor and moves the assertion to ConnectionFactoryImpl when
       direct connection is used. This allows instantiating the service on
       iOS with a null network context.
     - Adds PrivateAiServiceFactory to construct and manage
       PrivateAiService instances on iOS using the platform-specific
       drivers.
     - Registers the service with the profile keyed service factories.
    
    Next Steps:
     - CL 5 will hook this up to the Optimization Guide service to enable
       Private AI fetching.
    
    Fixed: b:516683106
    Change-Id: I98e28a7af830860577b977d53ea090e55ba93d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7961521
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Yerassyl Kutylbek <kutylbeky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652235}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -137,6 +137,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_service_factory.h"
 #import "ios/chrome/browser/power_bookmarks/model/power_bookmark_service_factory.h"
 #import "ios/chrome/browser/price_insights/model/price_insights_model_factory.h"
+#import "ios/chrome/browser/private_ai/model/private_ai_service_factory.h"
 #import "ios/chrome/browser/promos_manager/model/promos_manager_factory.h"
 #import "ios/chrome/browser/push_notification/model/provisional_push_notification_service_factory.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_profile_service_factory.h"
@@ -387,6 +388,7 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   PolicyBlocklistServiceFactory::GetInstance();
   PowerBookmarkServiceFactory::GetInstance();
   PriceInsightsModelFactory::GetInstance();
+  PrivateAiServiceFactory::GetInstance();
   PromosManagerFactory::GetInstance();
   ProvisionalPushNotificationServiceFactory::GetInstance();
   PushNotificationProfileServiceFactory::GetInstance();
