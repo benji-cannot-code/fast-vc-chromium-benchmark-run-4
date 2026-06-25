1280d64777b28b325513cc51413d60e4e1e533c4
David Yeung <dayeung@chromium.org>
2026-06-25 14:08:37 -0700

    Support window drag immediately when dragging all tabs
    This CL introduces an abstraction to check if the number of dragged tabs
    matches the total number of tabs currently in the window. When this
    condition is met, bypass the typical tear-off or detachment logic and
    transition directly into a full window move loop on the source window.
    
    This CL also refactors `TabDragSession::HandleMoveWhileAttached` by
    breaking out sub-steps into helper functions.
    
    Bug: 501070793
    Change-Id: I1c716cb0098811de4ae8f4dd199fc2fb7ae85e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8006567
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652708}

--- components/browser_apis/tab_drag/testing/toy_tab_drag_window_adapter.h
+++ components/browser_apis/tab_drag/testing/toy_tab_drag_window_adapter.h
@@ -33,6 +33,7 @@ class ToyTabDragWindowAdapter : public TabDragWindowAdapter {
     return gfx::NativeWindow();
   }
   gfx::Rect GetBoundsInScreen() const override;
+  bool IsDraggingEntireWindow(size_t dragged_tab_count) const override;
   gfx::Point ConvertScreenPointToLocal(
       gfx::NativeView target_view,
       const gfx::Point& screen_point) const override;
@@ -66,6 +67,7 @@ class ToyTabDragWindowAdapter : public TabDragWindowAdapter {
   }
 
   // Toy controls:
+  void set_tab_count(size_t count) { tab_count_ = count; }
   void set_detach_to_new_window_result(
       base::expected<TabDragWindowId, mojo_base::mojom::ErrorPtr> result) {
     detach_to_new_window_result_ = std::move(result);
@@ -96,6 +98,7 @@ class ToyTabDragWindowAdapter : public TabDragWindowAdapter {
   }
 
  private:
+  size_t tab_count_ = 2;
   gfx::Rect bounds_;
   bool has_capture_ = false;
   TabDragWindowId id_;
