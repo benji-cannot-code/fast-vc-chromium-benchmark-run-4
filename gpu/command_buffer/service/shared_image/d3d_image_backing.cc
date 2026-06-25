3ad6748aff7d7c034b609f0e58d3cb451a509396
Kai Ninomiya <kainino@chromium.org>
2026-06-24 19:02:43 -0700

    Validate buffer usages in AssociateMailboxDawnBuffer
    Validate that creating a SharedImage with WEBGPU_SHARED_BUFFER requires:
    - Also having both WEBGPU_READ and WEBGPU_WRITE. There's no
      wgpu::BufferUsage for read-only storage, so if it's going to be used
      as storage at all it needs to be read-write.
    
    Validate that AssociateMailboxDawnBuffer requires:
    - The SI to have both WEBGPU_READ and WEBGPU_WRITE.
    - Usage be one of a small set of usages (it could presumably be
      expanded, but this is what's currently needed).
    
    WebGPUMailboxBufferTest was only running on D3D12 even though there is
    now support for AssociateMailboxDawnBuffer on Metal. Refactored those
    tests to work on Metal (with minor fixes to IOSurfaceImageBacking), and
    added the new tests there.
    
    Fixed: 516509646
    Change-Id: Ifd9ccda302c72a3c4202c2e477de5ee465bcf22a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951049
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652123}

--- gpu/command_buffer/service/shared_image/d3d_image_backing.cc
+++ gpu/command_buffer/service/shared_image/d3d_image_backing.cc
@@ -1836,6 +1836,10 @@ wgpu::Buffer D3DImageBacking::BeginAccessDawnBuffer(
     const wgpu::Device& device,
     wgpu::BackendType backend_type,
     wgpu::BufferUsage usage) {
+  CHECK(this->usage().HasAll(SHARED_IMAGE_USAGE_WEBGPU_SHARED_BUFFER |
+                             SHARED_IMAGE_USAGE_WEBGPU_READ |
+                             SHARED_IMAGE_USAGE_WEBGPU_WRITE));
+
   AutoLock auto_lock(this);
 
   if (!ValidateBeginAccess(true)) {
