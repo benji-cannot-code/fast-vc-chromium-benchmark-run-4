d5ae915a552f860ce97268e7bb4b4730f9716cc7
HuanPo Lin <robertlin@chromium.org>
2026-06-24 20:39:25 -0700

    Add NavigationHandleUserData attachment for Omnibox prerendering
    This CL is a follow-up to https://crrev.com/c/7909261 for
    prerendering parts.
    
    Bug: 517725655
    Change-Id: I7d5e374fe230ff514b4581e1354569f107bc47e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7928614
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652152}

--- chrome/browser/preloading/prerender/prerender_omnibox_interactive_uitest.cc
+++ chrome/browser/preloading/prerender/prerender_omnibox_interactive_uitest.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/search_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/omnibox/browser/base_search_provider.h"
+#include "components/page_load_metrics/browser/navigation_handle_user_data.h"
 #include "components/performance_manager/public/features.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/search_engines/template_url_data.h"
@@ -106,6 +107,27 @@ class AutocompleteActionPredictorObserverImpl
       observation_{this};
 
   base::OnceClosure waiting_;
+};
+
+class TestOmniboxNavigationObserver : public content::WebContentsObserver {
+ public:
+  explicit TestOmniboxNavigationObserver(content::WebContents* web_contents)
+      : content::WebContentsObserver(web_contents) {}
+
+  void DidFinishNavigation(
+      content::NavigationHandle* navigation_handle) override {
+    if (navigation_handle->HasCommitted()) {
+      auto* user_data =
+          page_load_metrics::NavigationHandleUserData::GetForNavigationHandle(
+              *navigation_handle);
+      if (user_data) {
+        navigation_type_ = user_data->navigation_type();
+      }
+    }
+  }
+
+  std::optional<page_load_metrics::NavigationHandleUserData::InitiatorLocation>
+      navigation_type_;
 };
 
 // This is a browser test for Omnibox triggered prerendering. This is
@@ -298,6 +320,9 @@ IN_PROC_BROWSER_TEST_F(PrerenderOmniboxUIBrowserTest,
   WaitForAutocompleteActionPredictorInitialization();
   const GURL kPrerenderingUrl =
       embedded_test_server()->GetURL("/empty.html?prerender");
+
+  TestOmniboxNavigationObserver omnibox_observer(GetActiveWebContents());
+
   GetAutocompleteActionPredictor()->StartPrerendering(kPrerenderingUrl,
                                                       *GetActiveWebContents());
   StartOmniboxNavigationAndWaitForActivation(kPrerenderingUrl);
@@ -306,6 +331,11 @@ IN_PROC_BROWSER_TEST_F(PrerenderOmniboxUIBrowserTest,
                              ui::PAGE_TRANSITION_FROM_ADDRESS_BAR));
   EXPECT_TRUE(IsPrerenderingNavigation());
   EXPECT_EQ(GetActiveWebContents()->GetLastCommittedURL(), kPrerenderingUrl);
+
+  EXPECT_TRUE(omnibox_observer.navigation_type_.has_value());
+  EXPECT_EQ(omnibox_observer.navigation_type_.value(),
+            page_load_metrics::NavigationHandleUserData::InitiatorLocation::
+                kOmniboxDirectUrlInput);
 
   histogram_tester.ExpectUniqueSample(
       internal::kHistogramPrerenderPredictionStatusDirectUrlInput,
