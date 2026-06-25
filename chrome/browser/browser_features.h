87feaf716a96fa0988fed48eda42bd813a73bf96
Will Harris <wfh@chromium.org>
2026-06-24 17:24:04 -0700

    Revert "Add feature for process isolation"
    This reverts commit aad150d0c77ee3ed2f63b724a0a9bc35713f949d.
    
    Reason for revert: win10 bot is failing
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/110424/overview
    
    Original change's description:
    > Add feature for process isolation
    >
    > This adds a feature for the purpose of enabling a field trial for
    > process isolation.
    >
    > The behavior here is non-standard, due to the fact that process
    > isolation requires a restart. Clients will enter the field trial groups
    > and then only respect the isolation state on the next restart.
    >
    > Due to the need to provide a user opt-out during this stage of the
    > rollout, the UI option to disable is shown if process isolation is
    > enabled. Feature switches are filtered at launch of the isolated process
    > so this provides a well-lit path to disable instead of using command
    > line switches.
    >
    > A local state pref is used to save the current field trial configuration
    > and the state is only flipped if the field trial name changes, thus
    > ensuring that even if the client is in an Enabled group, the user still
    > has the UI escape hatch for compatibility, while still allowing
    > iteration to happen during this phase of the rollout.
    >
    > Bug: 433545123
    > Change-Id: I01025dcea41db2749042faf6063c0d4732944773
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7981123
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1651971}
    
    Bug: 433545123
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If576c1445067ff14cc4ec04e3f9bf549f4b5143a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7999020
    Commit-Queue: Will Harris <wfh@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652087}

--- chrome/browser/browser_features.h
+++ chrome/browser/browser_features.h
@@ -95,7 +95,6 @@ BASE_DECLARE_FEATURE(kWebUsbDeviceDetection);
 
 #if BUILDFLAG(IS_WIN)
 BASE_DECLARE_FEATURE(kBrowserDynamicCodeDisabled);
-BASE_DECLARE_FEATURE(kIsolatedProcess);
 
 BASE_DECLARE_FEATURE(kNoPreReadMainDll);
 BASE_DECLARE_FEATURE(kNoPreReadMainDllIfSsd);
--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1625,7 +1625,6 @@ void RegisterLocalState(PrefRegistrySimple* registry) {
   MediaFoundationServiceMonitor::RegisterPrefs(registry);
   os_crypt_async::AppBoundEncryptionProviderWin::RegisterLocalPrefs(registry);
   webnn::RegisterLocalPrefs(registry);
-  registry->RegisterStringPref(prefs::kPreviousIsolationState, std::string());
   registry->RegisterBooleanPref(prefs::kForegroundLaunchOnLogin, false);
   registry->RegisterBooleanPref(prefs::kStartupLaunchInfobarAccepted, false);
   registry->RegisterTimePref(prefs::kStartupLaunchInfobarLastDeclinedTime,
