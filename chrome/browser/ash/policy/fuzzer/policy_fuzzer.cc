070b4696129bcb3008195c8070f650452934ada4
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-06-25 12:45:28 -0700

    lpm: Add support for import_dirs to experimental_fuzzable_protos
    import_dirs is an optional argument of proto_library that allows
    developers to provide extra paths for protoc to look for imports and
    extra include_dirs for the C++ compiler to look for generated pb.h
    files.
    
    This CL also:
     1. Fixes an issue in experimental_fuzzable_protos when the protos are not in the same directory as the rule.
     2. Fixes a bug in proto_library, where we were adding root_gen_dir to paths already in root_gen_dir.
     3. Migrates chrome/browser/ash/policy/fuzzer/BUILD.gn to use experimental_fuzzable_protos.
    
    Bug: 505034799
    Change-Id: I591309fc1b12c6e3eed8abc5f6b280ea5ef94322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951035
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652639}

--- chrome/browser/ash/policy/fuzzer/policy_fuzzer.cc
+++ chrome/browser/ash/policy/fuzzer/policy_fuzzer.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/dbus/ash_dbus_helper.h"
 #include "chrome/browser/ash/policy/core/device_policy_decoder.h"
 #include "chrome/browser/ash/policy/fuzzer/policy_fuzzer.pb.h"
+#include "chrome/browser/ash/policy/fuzzer/policy_fuzzer_fuzzable.pb.h"
 #include "chrome/browser/ash/settings/device_settings_provider.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/policy/configuration_policy_handler_list_factory.h"
@@ -158,7 +159,11 @@ void CheckPolicyToCrosSettingsTranslation(
 
 }  // namespace
 
-DEFINE_PROTO_FUZZER(const PolicyFuzzerProto& proto) {
+DEFINE_PROTO_FUZZER(const fuzzable::policy::PolicyFuzzerProto& fuzzable_proto) {
+  std::string serialized = fuzzable_proto.SerializeAsString();
+  PolicyFuzzerProto proto;
+  CHECK(proto.ParseFromString(serialized));
+
   static Environment env;
   PerInputEnvironment per_input_env;
 
