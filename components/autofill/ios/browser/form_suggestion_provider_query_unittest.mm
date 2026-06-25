c1977627d0cc3266484518bced398af347eaf9db
Christoph Schwering <schwering@google.com>
2026-06-25 06:10:24 -0700

    [Autofill] Rename foo_tests.mm to foo_test.mm
    Bypass-Check-License: Rename.
    Bug: 40100455
    Change-Id: I7ac79733d1f5cd83dfcead8154b0fbdeba4e8c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8004654
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652361}

--- components/autofill/ios/browser/form_suggestion_provider_query_unittest.mm
+++ components/autofill/ios/browser/form_suggestion_provider_query_unittest.mm
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
 
-using autofill::FormRendererId;
 using autofill::FieldRendererId;
+using autofill::FormRendererId;
 
 namespace {
 NSString* const kTestFormName = @"login_form";
