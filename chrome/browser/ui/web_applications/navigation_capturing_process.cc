603a58fa532750baf39340bea8be69cce127ab62
Edman Anjos <edmanp@google.com>
2026-06-25 02:12:44 -0700

    Use CHECK_NE in NavigationCapturingProcess
    The current code triggers a lint error:
    
    ```
    $ git cl lint chrome/browser/ui/web_applications/navigation_capturing_process.cc
    chrome/browser/ui/web_applications/navigation_capturing_process.cc:1947:  (cpplint) Consider using CHECK_NE instead of CHECK(a != b)  [readability/check] [2]
    Total errors found: 1
    ```
    
    Bug: none
    Test: testing/xvfb.py tools/autotest.py -C out_/Default chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
    Change-Id: I447ee06b8368008bf58ad72310496eb286c8fcdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7984605
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652250}

--- chrome/browser/ui/web_applications/navigation_capturing_process.cc
+++ chrome/browser/ui/web_applications/navigation_capturing_process.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/check.h"
 #include "base/check_deref.h"
+#include "base/check_op.h"
 #include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -1945,7 +1947,7 @@ NavigationCapturingProcess::CapturedNavigateExisting(Browser* app_browser,
                                                      int browser_tab) {
   CHECK(first_navigation_app_id_.has_value());
 
-  CHECK(browser_tab != -1);
+  CHECK_NE(browser_tab, -1);
   if (isolated_web_app_navigation_) {
     CHECK(disposition_ == WindowOpenDisposition::NEW_FOREGROUND_TAB ||
           disposition_ == WindowOpenDisposition::CURRENT_TAB);
