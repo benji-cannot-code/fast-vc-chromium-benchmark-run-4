3667ecd73c46f4e7321f5abcec63815c4f2578e6
Ryan Sultanem <rsult@google.com>
2026-06-25 08:08:42 -0700

    [AvatarButton] Make state_maanger_ a stack variable
    Allows to simplify checks and reduce ambiguity for usages.
    
    Change-Id: I3349f4acb2e42c5339126a11042072c0cbcb0199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8003760
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652431}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "base/time/time.h"
+#include "chrome/browser/ui/views/profiles/avatar_toolbar_button_state_manager.h"
 #include "chrome/browser/ui/views/profiles/avatar_toolbar_button_types.h"
 #include "chrome/browser/ui/views/toolbar/avatar_toolbar_button_interface.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
@@ -23,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/events/event.h"
 
-class AvatarToolbarButtonStateManager;
 class BrowserView;
 struct AccountInfo;
 class StateProvider;
@@ -132,7 +132,7 @@ class AvatarToolbarButton : public ToolbarButton,
   void AnimationProgressed(const gfx::Animation* animation) override;
   void AnimationEnded(const gfx::Animation* animation) override;
 
-  std::unique_ptr<AvatarToolbarButtonStateManager> state_manager_;
+  AvatarToolbarButtonStateManager state_manager_;
 
   // Cached icons for the placeholder avatar in forced-colors mode, to avoid
   // recomputing on every ink drop highlight change. Empty when not in
--- chrome/browser/ui/views/toolbar/webui_test_utils.cc
+++ chrome/browser/ui/views/toolbar/webui_test_utils.cc
@@ -269,7 +269,7 @@ AvatarToolbarButtonState AvatarToolbarButtonTestAccessor::GetState() {
   return std::visit(
       absl::Overload{
           [](AvatarToolbarButton* button) -> AvatarToolbarButtonState {
-            return button ? button->state_manager_->GetActiveState()
+            return button ? button->state_manager_.GetActiveState()
                           : AvatarToolbarButtonState::kNormal;
           },
           [this](WebUIAvatarToolbarButton* button) -> AvatarToolbarButtonState {
