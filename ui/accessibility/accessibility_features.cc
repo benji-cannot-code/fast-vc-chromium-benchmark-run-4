c1e2aef777fd701028e9426215bdd848c8f1d45f
Katie Dektar <katydek@google.com>
2026-06-25 15:55:30 -0700

    Enable inverted mouse cursor by default
    Bug: b:40876890
    Change-Id: If3db2863f8ebf06014a95f74fc4a5ae99bf7205d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8006569
    Commit-Queue: Katie D <katie@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652774}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -286,7 +286,7 @@ bool IsAccessibilityFlashScreenFeatureEnabled() {
 }
 
 BASE_FEATURE(kAccessibilityInvertedMouseCursor,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 bool IsAccessibilityInvertedMouseCursorEnabled() {
   return base::FeatureList::IsEnabled(
       ::features::kAccessibilityInvertedMouseCursor);
