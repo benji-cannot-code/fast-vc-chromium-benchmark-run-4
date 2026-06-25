a32b0984a1f4bd5281c850a2dadbbbcd535bdc40
Jan Keitel <jkeitel@google.com>
2026-06-25 08:17:00 -0700

    Return early in AED::DidAcceptSuggestion when UI should remain open
    No functionality change - just enforcing consistency for how to handle
    suggestion UI that should remain open after accepting a suggestion.
    
    Bug: 40100455
    Change-Id: Ide3eaf79e1a456df64f9fe28ab8e6bdc68ba90ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8004450
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652437}

--- components/autofill/core/browser/ui/autofill_external_delegate.cc
+++ components/autofill/core/browser/ui/autofill_external_delegate.cc
@@ -815,9 +815,16 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
     case SuggestionType::kSaveAndFillCreditCardEntry:
     case SuggestionType::kSeePromoCodeDetails:
     case SuggestionType::kScanCreditCard:
-    case SuggestionType::kBnplEntry:
     case SuggestionType::kMaximizeCreditCardBenefitsEntry:
       DidAcceptPaymentsSuggestion(suggestion, metadata);
+      break;
+    case SuggestionType::kBnplEntry:
+      DidAcceptPaymentsSuggestion(suggestion, metadata);
+      if (base::FeatureList::IsEnabled(
+              features::kAutofillEnablePayNowPayLaterTabs)) {
+        // The popup will instead be closed by `BnplManager`.
+        return;
+      }
       break;
     case SuggestionType::kManageAddress:
     case SuggestionType::kManageAutofillAi:
@@ -1021,7 +1028,8 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
     case SuggestionType::kAtMemorySearchAffordance:
       manager_->GetAtMemoryManager().OnSearchSubmitted(
           suggestion.main_text.value);
-      break;
+      // The popup remains open to show search results once the query completes.
+      return;
     case SuggestionType::kTitle:
     case SuggestionType::kSeparator:
     case SuggestionType::kPasswordEntry:
@@ -1046,17 +1054,6 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
     case SuggestionType::kAtMemoryGenericError:
     case SuggestionType::kPersonalContextNotice:
       NOTREACHED();  // Should be handled elsewhere.
-  }
-
-  if (suggestion.type == SuggestionType::kAtMemorySearchAffordance ||
-      (suggestion.type == SuggestionType::kBnplEntry &&
-       base::FeatureList::IsEnabled(
-           features::kAutofillEnablePayNowPayLaterTabs))) {
-    // Return early to prevent the popup from hiding.
-    // For `kBnplEntry`, the popup will instead be closed by `BnplManager`.
-    // For `kAtMemorySearchAffordance`, the popup remains open to show search
-    // results once the query completes.
-    return;
   }
 
   manager_->client().HideSuggestions(SuggestionHidingReason::kAcceptSuggestion,
