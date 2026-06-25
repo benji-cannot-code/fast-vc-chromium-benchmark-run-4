12da54514b894c0e7ad226bd02524f6c2b241b07
Ireneusz Szulc <ireneuszszulc@google.com>
2026-06-25 04:09:07 -0700

    PersonalContext: Fix PopupPersonalContextNoticeView crashes
    This change fixes accessibility startup crash by explicitly setting
    description text as the accessible name on
    PopupPersonalContextNoticeView to satisfy non-empty name assertions in
    RunAccessibilityPaintChecks.
    
    Bug: b:527333203
    Change-Id: I71cb698f3bea10a8b0018eac2ffb15ab6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7992875
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Aisulu Rakhmetullina <aisulu@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652303}

--- chrome/browser/ui/views/autofill/popup/popup_personal_context_notice_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_personal_context_notice_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/styled_label.h"
@@ -52,6 +53,9 @@ PopupPersonalContextNoticeView::PopupPersonalContextNoticeView(
   description_ =
       text_container->AddChildView(std::make_unique<views::StyledLabel>());
   description_->SetText(description_text);
+  // TODO(crbug.com/515651588): Update accessibility names.
+  GetContentView().GetViewAccessibility().SetName(description_text);
+  GetViewAccessibility().SetName(description_text);
 
   // Make the link substring in the description clickable.
   description_->AddStyleRange(
