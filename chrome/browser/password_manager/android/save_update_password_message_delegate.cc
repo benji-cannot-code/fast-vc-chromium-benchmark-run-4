15f84ca1ea157ee42dde41a9b131e7951d01359b
Ivana Žužić <izuzic@google.com>
2026-06-25 10:13:58 -0700

    Replace DCHECKs with CHECKs in SaveUpdatePasswordMessageDelegate
    This change upgrades several DCHECKs to CHECKs with a
    base::NotFatalUntil::M152 annotation in
    SaveUpdatePasswordMessageDelegate to give us time to fix potential
    crashes.
    
    Bug: 483652585
    Change-Id: I54bff873619844791850eb05c0fcef8dff47fb85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8003337
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652521}

--- chrome/browser/password_manager/android/save_update_password_message_delegate.cc
+++ chrome/browser/password_manager/android/save_update_password_message_delegate.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
+#include "base/not_fatal_until.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/android/android_theme_resources.h"
 #include "chrome/browser/android/resource_mapper.h"
@@ -65,7 +66,7 @@ SaveUpdatePasswordMessageDelegate::SaveUpdatePasswordMessageDelegate(
 }
 
 SaveUpdatePasswordMessageDelegate::~SaveUpdatePasswordMessageDelegate() {
-  DCHECK(web_contents_ == nullptr);
+  CHECK(web_contents_ == nullptr, base::NotFatalUntil::M152);
 }
 
 void SaveUpdatePasswordMessageDelegate::DisplaySaveUpdatePasswordPrompt(
@@ -73,8 +74,8 @@ void SaveUpdatePasswordMessageDelegate::DisplaySaveUpdatePasswordPrompt(
     std::unique_ptr<password_manager::PasswordFormManagerForUI> form_to_save,
     bool update_password,
     password_manager::PasswordManagerClient* password_manager_client) {
-  DCHECK_NE(nullptr, web_contents);
-  DCHECK(form_to_save);
+  CHECK_NE(nullptr, web_contents, base::NotFatalUntil::M152);
+  CHECK(form_to_save, base::NotFatalUntil::M152);
 
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
@@ -110,8 +111,8 @@ void SaveUpdatePasswordMessageDelegate::DisplaySaveUpdatePasswordPromptInternal(
     password_manager::PasswordManagerClient* password_manager_client) {
   // Dismiss previous message if it is displayed.
   DismissSaveUpdatePasswordPrompt();
-  DCHECK(message_ == nullptr);
-  DCHECK(password_edit_dialog_ == nullptr);
+  CHECK(message_ == nullptr, base::NotFatalUntil::M152);
+  CHECK(password_edit_dialog_ == nullptr, base::NotFatalUntil::M152);
 
   web_contents_ = web_contents;
   passwords_state_.set_client(password_manager_client);
@@ -441,8 +442,8 @@ bool SaveUpdatePasswordMessageDelegate::IsUsingAccountStorage(
 }
 
 void SaveUpdatePasswordMessageDelegate::ClearState() {
-  DCHECK(message_ == nullptr);
-  DCHECK(password_edit_dialog_ == nullptr);
+  CHECK(message_ == nullptr, base::NotFatalUntil::M152);
+  CHECK(password_edit_dialog_ == nullptr, base::NotFatalUntil::M152);
 
   passwords_state_.OnInactive();
   // web_contents_ is set in DisplaySaveUpdatePasswordPromptInternal().
