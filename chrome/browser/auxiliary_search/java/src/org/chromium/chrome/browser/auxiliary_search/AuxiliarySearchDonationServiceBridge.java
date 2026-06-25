9b6c345dda1e1c0676d79a8b8b3780ecda6f1c19
Michael Cui <mlcui@google.com>
2026-06-24 18:47:10 -0700

    auxiliary_search_donation_service: Add logging on donation errors
    See https://crrev.com/c/7870543/comment/1d3c3af4_d7f59544/ for more
    context.
    
    Bug: b:432359106
    Change-Id: I6a6a2100c659e15386228dee873762afa8ca1a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7974908
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1652118}

--- chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchDonationServiceBridge.java
+++ chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchDonationServiceBridge.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.auxiliary_search;
 import static org.chromium.build.NullUtil.assumeNonNull;
 
 import androidx.annotation.VisibleForTesting;
+import androidx.appsearch.app.AppSearchBatchResult;
 import androidx.appsearch.app.AppSearchSchema;
 import androidx.appsearch.app.AppSearchSession;
 import androidx.appsearch.app.GenericDocument;
@@ -18,6 +19,7 @@ import androidx.appsearch.builtintypes.Account;
 import androidx.appsearch.builtintypes.WebPage;
 import androidx.appsearch.exceptions.AppSearchException;
 
+import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
@@ -25,6 +27,7 @@ import com.google.common.util.concurrent.MoreExecutors;
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JniType;
 
+import org.chromium.base.Log;
 import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -40,6 +43,8 @@ import java.util.concurrent.TimeUnit;
  */
 @NullMarked
 class AuxiliarySearchDonationServiceBridge implements Closeable {
+    @VisibleForTesting static final String TAG = "AuxSearchDonation";
+
     // Differs from `AuxiliarySearchDonor`, which uses the package name as both the database name
     // and namespace.
     @VisibleForTesting static final String DATABASE_NAME = "browsing_data";
@@ -89,7 +94,7 @@ class AuxiliarySearchDonationServiceBridge implements Closeable {
         }
         Account account = fromCoreAccountInfo(coreAccountInfo);
 
-        var unused =
+        ListenableFuture<AppSearchBatchResult<String, Void>> putFuture =
                 Futures.transformAsync(
                         mSessionFuture,
                         session -> {
@@ -115,6 +120,28 @@ class AuxiliarySearchDonationServiceBridge implements Closeable {
                             return session.putAsync(builder.build());
                         },
                         MoreExecutors.directExecutor());
+
+        // WARNING: Do not log URLs here, including document IDs which contain URLs.
+        // See docs/android_logging.md for more information.
+        Futures.addCallback(
+                putFuture,
+                new FutureCallback<AppSearchBatchResult<String, Void>>() {
+                    @Override
+                    public void onSuccess(AppSearchBatchResult<String, Void> result) {
+                        if (!result.isSuccess()) {
+                            Log.w(
+                                    TAG,
+                                    "Failed to donate documents: %d failure(s).",
+                                    result.getFailures().size());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Throwable t) {
+                        Log.w(TAG, "Failed to donate history to AppSearch.", t);
+                    }
+                },
+                MoreExecutors.directExecutor());
     }
 
     @CalledByNative
