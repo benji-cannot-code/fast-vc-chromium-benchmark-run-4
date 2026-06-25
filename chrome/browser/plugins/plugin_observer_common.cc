cc3c5c81c2583cf2fd183d93720c72a1c9ce88b2
Kalvin Lee <kdlee@chromium.org>
2026-06-25 10:55:58 -0700

    Use `CurrentTargetFrame()` (split CL)
    Prefer to get a `RenderFrameHost&` rather than a pointer that is (only)
    declared to be non-null. Additionally, since `CanOpenPdfUrl()` is
    plainly assuming the `RenderFrameHost*` is not null, it's modified to
    take it by reference.
    
    This CL is scoped to `chrome/browser/plugins/`.
    
    Bug: 525108538
    Change-Id: I670612ce9445575491a75b4a490e45705ed62845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8000833
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652550}

--- chrome/browser/plugins/plugin_observer_common.cc
+++ chrome/browser/plugins/plugin_observer_common.cc
@@ -10,16 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/referrer.h"
 
-bool CanOpenPdfUrl(content::RenderFrameHost* render_frame_host,
+bool CanOpenPdfUrl(content::RenderFrameHost& render_frame_host,
                    const GURL& url,
                    const GURL& last_committed_url,
                    content::Referrer* referrer) {
-  if (!render_frame_host->IsActive()) {
+  if (!render_frame_host.IsActive()) {
     return false;
   }
 
   if (!content::ChildProcessSecurityPolicy::GetInstance()->CanRequestURL(
-          render_frame_host->GetProcess()->GetDeprecatedID(), url)) {
+          render_frame_host.GetProcess()->GetDeprecatedID(), url)) {
     return false;
   }
 
