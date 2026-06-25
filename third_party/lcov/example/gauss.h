02b7d1ef33c79a75699dfe425d34dc5854cde63b
Rick Byers <rbyers@chromium.org>
2026-06-25 12:59:47 -0700

    Remove unused third_party/lcov
    The last remaining reference to third_party/lcov was removed in commit
    4414bc62be426 ("checklicenses: delete") in September 2025.
    
    third_party/lcov has no remaining build, test, or script references,
    and lacks an OWNERS file. I'm happy to add it back if there's something using it that I missed, but we need to know what so that we can find OWNERS accountable for maintenance.
    
    Authored with Jetski/Gemini
    
    Bug: 482307291
    Change-Id: Id7c835e057d9447f8b6cd66a1e24d79ac6df6ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7987352
    Commit-Queue: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652650}

--- third_party/lcov/example/gauss.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#ifndef GAUSS_H
-#define GAUSS_H GAUSS_h
-
-extern int gauss_get_sum (int min, int max);
-
-#endif /* GAUSS_H */
