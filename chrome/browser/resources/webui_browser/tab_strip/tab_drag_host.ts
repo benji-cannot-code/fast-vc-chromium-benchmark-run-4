b187b2541cdadd3c0c93a3a1e27025c6f01063ec
David Yeung <dayeung@chromium.org>
2026-06-25 12:10:06 -0700

    Migrate WebUI tab drag-and-drop to Mojo API
    Refactor the WebUI tab strip to use the Mojo-based TabDragService and
    DropTargetInterface instead of local DOM mouse event listeners. This
    aligns the tab dragging experience with the browser's native window and
    cross-process drag-and-drop infrastructure.
    
    Demo
    https://screencast.googleplex.com/cast/NjQzOTI2MjMyMzk5ODcyMHxmZDU2YWIxZS02ZA
    
    Bug: 501070793
    Change-Id: Ib82bc2c060e4f617649618285e8cb9839b5fac8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7987619
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652607}

--- chrome/browser/resources/webui_browser/tab_strip/tab_drag_host.ts
+++ chrome/browser/resources/webui_browser/tab_strip/tab_drag_host.ts
@@ -3,12 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type {TabDragServiceRemote} from '../tab_drag_api.mojom-webui.js';
+
 import type {TabStripItem} from './items.js';
 import type {TabElement} from './tab.js';
 
 export interface TabDragHost {
   shadowRoot: ShadowRoot|null;
   itemsForDrag: TabStripItem[];
+  tabDragService: TabDragServiceRemote;
   getBoundingClientRect(): DOMRect;
   activateTabForDrag(id: string): void;
   getTabElementForDrag(id: string): TabElement|null;
@@ -17,4 +20,6 @@ export interface TabDragHost {
   setTabStripNoDrag(noDrag: boolean): void;
   requestUpdate(): void;
   fire(type: string, detail?: unknown): void;
+  commitDrag(tabId: string, finalIndex: number): void;
+  getDragContainerBounds(): DOMRect;
 }
--- chrome/browser/resources/webui_browser/tab_strip/tab_strip.html.ts
+++ chrome/browser/resources/webui_browser/tab_strip/tab_strip.html.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+import {html, repeat} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import type {TabStripElement} from './tab_strip.js';
 
@@ -11,7 +11,7 @@ export function getHtml(this: TabStripElement) {
   // clang-format off
   return html`<!--_html_template_start_-->
 <div id="tabstrip">
-    ${this.items_.map(item => item.type === 'tab' ? html`
+    ${repeat(this.items_, item => item.id, item => item.type === 'tab' ? html`
           <webui-browser-tab id="${this.tabIdToDomId(item.id)}"
                .tabData="${item.tabData}"
                .dragInProgress="${this.dragInProgress_}"
@@ -20,7 +20,7 @@ export function getHtml(this: TabStripElement) {
           </webui-browser-tab>
         ` : html`
           <webui-browser-tab-group .collectionId="${item.id}"
-              .groupData="${item.groupData}">
+               .groupData="${item.groupData}">
           </webui-browser-tab-group>
         `)}
   <cr-icon-button id="newTabButton" iron-icon="cr:add"
