a5f42448d10b6a7b9e9043e75280bb7a098c40a9
Edman Anjos <edmanp@google.com>
2026-06-26 09:58:55 -0700

    webapps: Extract helper to convert SafeUrlPattern to DictValue
    There are currently two similar implementations of UrlPatternDebugValue
    in display_override.cc and web_app.cc to convert a SafeUrlPattern to a
    DictValue.
    
    This CL extracts them to a shared utility function in a new file.
    
    Fixed: 474605088
    Test: testing/xvfb.py tools/autotest.py -C out_/Default chrome/browser/ui/web_applications/web_app_browsertest.cc chrome/browser/web_applications/web_app_unittest.cc
    Change-Id: Iea2fea45aa8123e37164c6c12648b123bf66a88d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8004690
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653235}

---
+++ chrome/browser/web_applications/model/safe_url_pattern_to_value.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_WEB_APPLICATIONS_MODEL_SAFE_URL_PATTERN_TO_VALUE_H_
+#define CHROME_BROWSER_WEB_APPLICATIONS_MODEL_SAFE_URL_PATTERN_TO_VALUE_H_
+
+#include "base/values.h"
+
+namespace blink {
+struct SafeUrlPattern;
+}
+
+namespace web_app {
+
+// Converts a `SafeUrlPattern` to a `DictValue` for debugging.
+base::DictValue ToValue(const blink::SafeUrlPattern& pattern);
+
+}  // namespace web_app
+
+#endif  // CHROME_BROWSER_WEB_APPLICATIONS_MODEL_SAFE_URL_PATTERN_TO_VALUE_H_
