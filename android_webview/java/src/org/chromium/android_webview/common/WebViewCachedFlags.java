1f38a2daa204fe7561e6fa8f44437a440d8d363d
Barkin Unal <barkinunal@chromium.org>
2026-06-26 09:32:48 -0700

    [AW] Add resetForTesting to WebViewCachedFlags.
    This allows tests to reset the singleton instance of WebViewCachedFlags,
    ensuring each test starts with a clean state.
    
    For context the tests weren't reseted and they are batched, which caused
    test failures on change 7951682.
    
    Bug: 511156405
    Change-Id: Iff7152347ce7d127c73afb12cd0f037048fd21d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010847
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Barkin Unal <barkinunal@chromium.org>
    Reviewed-by: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653214}

--- android_webview/java/src/org/chromium/android_webview/common/WebViewCachedFlags.java
+++ android_webview/java/src/org/chromium/android_webview/common/WebViewCachedFlags.java
@@ -20,6 +20,7 @@ import org.jni_zero.NativeMethods;
 import org.chromium.base.BaseFeatures;
 import org.chromium.base.FeatureList;
 import org.chromium.base.FeatureOverrides;
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -133,6 +134,15 @@ public class WebViewCachedFlags {
      */
     public static void initForTesting(SharedPreferences prefs) {
         initInternal(prefs, false);
+        ResettersForTesting.register(() -> resetForTesting());
+    }
+
+    /** Resets the singleton instance for testing. */
+    @VisibleForTesting
+    public static void resetForTesting() {
+        synchronized (sLock) {
+            sInstance = null;
+        }
     }
 
     private static void initInternal(SharedPreferences prefs, boolean forceDefaults) {
