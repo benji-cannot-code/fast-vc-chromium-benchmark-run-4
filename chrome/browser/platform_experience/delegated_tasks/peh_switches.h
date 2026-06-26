dca0f80b4dffc69f17c373618574c87a3ee9881c
Atharv Maan <atharvmaan@chromium.org>
2026-06-26 09:37:31 -0700

    [PEH] Implement task execution in DelegatedTaskRunner
    This CL adds task execution logic using PehLauncher in
    DelegatedTaskRunner. For now, we will only be executing the task and
    returning. Follow up CLs will focus on adding the capability of waiting
    for process termination and exit code parsing.
    
    Fixed: 525018785
    Change-Id: Ibaa0e6a8e83b4540894732a63c08bc165789b1cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963121
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653219}

---
+++ chrome/browser/platform_experience/delegated_tasks/peh_switches.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PLATFORM_EXPERIENCE_DELEGATED_TASKS_PEH_SWITCHES_H_
+#define CHROME_BROWSER_PLATFORM_EXPERIENCE_DELEGATED_TASKS_PEH_SWITCHES_H_
+
+namespace platform_experience {
+
+inline constexpr const char kDelegatedTasksSwitch[] = "delegated-task";
+
+}
+
+#endif  // CHROME_BROWSER_PLATFORM_EXPERIENCE_DELEGATED_TASKS_PEH_SWITCHES_H_
---
+++ chrome/browser/platform_experience/delegated_tasks/test_support/mock_peh_launcher.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/platform_experience/delegated_tasks/test_support/mock_peh_launcher.h"
+
+namespace platform_experience {
+
+MockPehLauncher::MockPehLauncher() = default;
+
+MockPehLauncher::~MockPehLauncher() = default;
+
+}  // namespace platform_experience
