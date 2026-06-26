70d93e477c99d860a0e98d477d966019760eeb04
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-06-26 16:03:09 -0700

    [Extensions] Check GetLastCommittedURL() in ExtensionActionRunner
    ExtensionActionRunner checks whether user consent is needed for running
    a script on a page. Before this CL, it uses GetVisibleURL() -- at the
    time, this was vaguely intentional, since that corresponds to the URL
    the user sees (and thus, might grant access to). However, this
    introduces potential TOCTOU issues with racing renderers and the site
    the script is going to inject in will be the last committed URL.
    
    Use that instead, and add a regression test.
    
    Bug: 498353463
    Change-Id: I536bd2c2bc5bd7cbd662d4e48443ea377c371c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008626
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653511}

--- chrome/browser/extensions/extension_action_runner.cc
+++ chrome/browser/extensions/extension_action_runner.cc
@@ -258,7 +258,7 @@ ExtensionActionRunner::RequiresUserConsentForScriptInjection(
     return PermissionsData::PageAccess::kAllowed;
   }
 
-  GURL url = web_contents()->GetVisibleURL();
+  GURL url = web_contents()->GetLastCommittedURL();
   int tab_id = sessions::SessionTabHelper::IdForTab(web_contents()).id();
   switch (type) {
     case mojom::InjectionType::kContentScript:
