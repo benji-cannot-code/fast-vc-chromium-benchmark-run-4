aa252899121b2f0e7d6f02276c4c3d2ba3fe9dce
Chris Thompson <cthomp@chromium.org>
2026-06-26 12:40:24 -0700

    [ABH] Enable HttpsFirstDialogUi by default on Android
    Also remove the fieldtrial_testing_config.json entry.
    
    Change-Id: I77e087e524f49a0ddd187787efd9e590c78ef256
    Bug: 351990829
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8011982
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653373}

--- components/security_interstitials/core/features.cc
+++ components/security_interstitials/core/features.cc
@@ -11,14 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace security_interstitials::features {
 
 // Enables a dialog-based UI for HTTPS-First Mode.
-// The flag is currently disabled on Android as the new UI is not implemented
-// there yet. (See crbug.com/469092867 for some previous unintended side
-// effects.)
-#if BUILDFLAG(IS_ANDROID)
-BASE_FEATURE(kHttpsFirstDialogUi, base::FEATURE_DISABLED_BY_DEFAULT);
-#else
 BASE_FEATURE(kHttpsFirstDialogUi, base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 BASE_FEATURE(kInsecureFormNavigationThrottleForPrerender,
              base::FEATURE_ENABLED_BY_DEFAULT);
