aa895b20dcbec0caa8f0cce745d6958e5ba81065
Sayed Elabady <elabadysayed@chromium.org>
2026-06-26 04:59:51 -0700

    aw: Add feature flag to disable AwClassPreloader
    This CL adds a new Finch feature flag kWebViewAwClassPreloader to
    control whether AwClassPreloader preloads classes during startup.
    The feature is enabled by default to preserve existing behavior,
    but can be disabled via Finch if needed.
    
    We are adding this feature to assess the impact of this preloading
    on other metrics.
    
    Test: autoninja -C out/Default system_webview_apk
    
    TAG=agy
    CONV=456849b1-e5f7-46b1-bc3a-95c3c0cefec4
    
    Change-Id: I1033b0e5689a4b766f06443086022e78f36730e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8006746
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653057}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -40,6 +40,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &base::features::kShutdownPreNativeThreadPoolAfterStartup,
     &features::kStartupNonBlockingWebViewConstructor,
     &features::kWebViewAddQuicHints,
+    &features::kWebViewAwClassPreloader,
     &features::kWebViewBackForwardCache,
     &features::kWebViewBackgroundClassPreloading,
     &features::kWebViewBackgroundTracingInit,
--- android_webview/java/src/org/chromium/android_webview/AwClassPreloader.java
+++ android_webview/java/src/org/chromium/android_webview/AwClassPreloader.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview;
 
+import org.chromium.android_webview.common.AwFeatures;
+import org.chromium.android_webview.common.WebViewCachedFlags;
 import org.chromium.base.Log;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
@@ -29,6 +31,10 @@ public final class AwClassPreloader {
 
     /** Preloads a set of classes on a background thread. */
     public static void preloadClasses() {
+        if (!WebViewCachedFlags.get()
+                .isCachedFeatureEnabled(AwFeatures.WEBVIEW_AW_CLASS_PRELOADER)) {
+            return;
+        }
         PostTask.postTask(
                 TaskTraits.BEST_EFFORT,
                 () -> {
