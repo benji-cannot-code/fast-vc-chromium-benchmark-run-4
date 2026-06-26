5f4eb979007045e3a7f08fddb0dc336ce6b699fb
Michal Mocny <mmocny@chromium.org>
2026-06-26 13:44:27 -0700

    [soft-navs] I2S: Enable feature in stable.
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/BfSsaY2b_y8
    Bug: 527074426
    Change-Id: I8030083e431bab140762ce1134c0d6ea6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8012372
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Commit-Queue: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653428}

--- third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
@@ -518,10 +518,6 @@ bool OriginTrialContext::CanEnableTrialFromName(const StringView& trial_name) {
         features::kBackForwardCacheSendNotRestoredReasons);
   }
 
-  if (trial_name == "SoftNavigationHeuristics") {
-    return base::FeatureList::IsEnabled(features::kSoftNavigationDetection);
-  }
-
   if (trial_name == "UserMediaElement") {
     return base::FeatureList::IsEnabled(blink::features::kUserMediaElement);
   }
