7bb814001f88627387768a492685087699b4f771
Aisulu R <aisulu@google.com>
2026-06-26 03:39:53 -0700

    [PersonalContext] Integrate content_view and the existing text_container
    This CL merges existing `text_container` (the main view of the notice
    element) and a newly passed `content_view` (as a part of `PopupRowView`
    that we are now inheriting) that was so far not used.
    
    This is done cause otherwise we'd have an unused view or had to nest
    the existing view into the new one.
    
    No new tests needed as `text_container` is already covered in unittests.
    
    Bug: b:525703986
    Change-Id: I5b9a371a26fb972beeb5854d597535bdec40541a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963783
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653026}

--- chrome/browser/ui/views/autofill/popup/popup_personal_context_notice_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_personal_context_notice_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
+#include "ui/views/view.h"
 
 namespace autofill {
 
@@ -35,13 +36,12 @@ PopupPersonalContextNoticeView::PopupPersonalContextNoticeView(
                    std::move(content_view)),
       controller_(std::move(controller)),
       line_number_(line_number) {
-  SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kVertical));
-
   // Text container (Title + Description)
   // TODO(crbug.com/517520354): Add styling and the title.
-  auto* text_container =
-      AddChildView(views::Builder<views::BoxLayoutView>().Build());
+  views::View& text_container = GetContentView();
+  auto* layout =
+      static_cast<views::BoxLayout*>(text_container.GetLayoutManager());
+  layout->SetOrientation(views::BoxLayout::Orientation::kVertical);
 
   // Description (with link)
   // TODO(crbug.com/517520354): Add styling and strings.
@@ -51,10 +51,10 @@ PopupPersonalContextNoticeView::PopupPersonalContextNoticeView(
   link_offset = description_text.find(link_text);
 
   description_ =
-      text_container->AddChildView(std::make_unique<views::StyledLabel>());
+      text_container.AddChildView(std::make_unique<views::StyledLabel>());
   description_->SetText(description_text);
   // TODO(crbug.com/515651588): Update accessibility names.
-  GetContentView().GetViewAccessibility().SetName(description_text);
+  text_container.GetViewAccessibility().SetName(description_text);
   GetViewAccessibility().SetName(description_text);
 
   // Make the link substring in the description clickable.
