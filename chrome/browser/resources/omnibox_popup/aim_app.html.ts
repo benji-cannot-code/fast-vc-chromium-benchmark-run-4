28c16a11d5f0e4ded70655cf72f5924b100c6c55
Ryan Akiyama <ryanakiyama@chromium.org>
2026-06-26 08:09:58 -0700

    [omnibox][composebox] Update `SubmitButtonIconType` import path
    Bug: b:517196380
    Change-Id: Ib5e19c851c01273c530b2bf28f03df8058111f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007391
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Auto-Submit: Ryan Akiyama <ryanakiyama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653168}

--- chrome/browser/resources/omnibox_popup/aim_app.html.ts
+++ chrome/browser/resources/omnibox_popup/aim_app.html.ts
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(crbug.com/497887993): Remove this when cleaning up the shared composebox
-// component.
-// eslint-disable-next-line no-restricted-imports
-import {SubmitButtonIconType} from '//resources/cr_components/composebox/composebox.js';
+import {SubmitButtonIconType} from '//resources/cr_components/composebox/composebox_mixin.js';
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {OmniboxAimAppElement} from './aim_app.js';
