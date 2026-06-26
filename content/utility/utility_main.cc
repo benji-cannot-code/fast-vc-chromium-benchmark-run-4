8ecc343f189610b7f2b6c722fa410c65c1843cf1
Shiyi Zou <shiyi.zou@intel.com>
2026-06-25 18:19:44 -0700

    support --wait-for-debugger in utility processes
    The existing --utility-startup-dialog uses MessageBox which fails in
    utility processes with win32k lockdown.
    
    This CL adds support for --wait-for-debugger in utility_main.cc and
    extends --wait-for-debugger-children to match against the
    --utility-sub-type value, allowing targeted debugging of specific
    utility processes (e.g., --wait-for-debugger-children=webnn.mojom.WebNNCompilerService).
    
    Bug: 527353142
    Change-Id: I9dac57dcd29977149e506b82c37662dfa9faa639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8002936
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    Commit-Queue: Zou, Shiyi <shiyi.zou@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1652854}

--- content/utility/utility_main.cc
+++ content/utility/utility_main.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/allocator/partition_alloc_support.h"
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/debug/alias.h"
+#include "base/debug/debugger.h"
 #include "base/debug/leak_annotations.h"
 #include "base/functional/bind.h"
 #include "base/immediate_crash.h"
@@ -265,6 +267,10 @@ int UtilityMain(MainFunctionParams parameters) {
   const std::string utility_sub_type =
       parameters.command_line->GetSwitchValueASCII(switches::kUtilitySubType);
   SetUtilityThreadName(utility_sub_type);
+
+  if (parameters.command_line->HasSwitch(switches::kWaitForDebugger)) {
+    base::debug::WaitForDebugger(/*wait_seconds=*/60, /*silent=*/true);
+  }
 
   if (parameters.command_line->HasSwitch(switches::kUtilityStartupDialog)) {
     auto dialog_match = parameters.command_line->GetSwitchValueASCII(
