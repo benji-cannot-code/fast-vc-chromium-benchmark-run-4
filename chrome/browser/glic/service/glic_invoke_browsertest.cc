5505b30f2b7ff61f5c8c6e389c041d9dfd50cb22
Marke Hallowell <wry@chromium.org>
2026-06-26 12:21:28 -0700

    [GlicInvoke] Add test case for profile not enabled.
    Bug: None
    Link: https://chromium-review.googlesource.com/id/I69b582cb915f2ad73ac1c0469ac9327e6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007686
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653350}

--- chrome/browser/glic/service/glic_invoke_browsertest.cc
+++ chrome/browser/glic/service/glic_invoke_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/service/metrics/glic_instance_helper_metrics.h"
 #include "chrome/browser/glic/test_support/glic_browser_test.h"
 #include "chrome/browser/glic/test_support/glic_histogram_tester.h"
+#include "chrome/browser/glic/test_support/glic_test_util.h"
 #include "chrome/browser/tab_list/tab_list_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
@@ -120,6 +121,21 @@ IN_PROC_BROWSER_TEST_F(GlicInvokeBrowserTest, InvokeWithEmptyConversationId) {
   coordinator().Invoke(std::move(options));
 
   EXPECT_EQ(error_future.Get(), GlicInvokeError::kInvalidConversationId);
+}
+
+IN_PROC_BROWSER_TEST_F(GlicInvokeBrowserTest,
+                       InvokeFailsWhenProfileNotEnabled) {
+  ScopedGlicCapability scoped_glic_capability(GetProfile(), false);
+
+  base::test::TestFuture<GlicInvokeError> error_future;
+  GlicInvokeOptions options(mojom::InvocationSource::kOsButton);
+  options.on_error = error_future.GetCallback();
+  options.target.surface = DefaultSurface{
+      GetTabListInterface()->GetActiveTab()->GetBrowserWindowInterface()};
+
+  coordinator().Invoke(std::move(options));
+
+  EXPECT_EQ(error_future.Get(), GlicInvokeError::kProfileNotEnabled);
 }
 
 IN_PROC_BROWSER_TEST_F(GlicInvokeBrowserTest, InvokeWithInvalidInstanceId) {
