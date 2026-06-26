3149457fb06dbd74a8bbe98f5f2c9e7183cccd3e
Ryo Hashimoto <hashimoto@google.com>
2026-06-25 23:28:02 -0700

    policy: Disable CloudPolicyClientTest.PolicyFetchDesktopAndroid and RegistrationDesktopAndroid
    These tests are constantly failing on
    android-automotive-12l-x64-rel-tests
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-automotive-12l-x64-rel-tests/21346/overview
    
    BUG=528019503 BUG=510726518
    
    Change-Id: Iac43e732d37e4eb6db0b77eea52b099795f99719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8009946
    Reviewed-by: Mateusz Zbikowski <zbikowski@google.com>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: ben chin <lungc@chromium.org>
    Commit-Queue: ben chin <lungc@chromium.org>
    Owners-Override: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Masa Fujita <massan@google.com>
    Auto-Submit: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1652925}

--- components/policy/core/common/cloud/cloud_policy_client_unittest.cc
+++ components/policy/core/common/cloud/cloud_policy_client_unittest.cc
@@ -3972,7 +3972,9 @@ INSTANTIATE_TEST_SUITE_P(,
                          ::testing::Values(std::string(), kDeviceDMToken));
 
 #if BUILDFLAG(IS_ANDROID)
-TEST_F(CloudPolicyClientTest, PolicyFetchDesktopAndroid) {
+// Constantly failing on android-automotive-12l-x64-rel-tests.
+// crbug.com/528019503
+TEST_F(CloudPolicyClientTest, DISABLED_PolicyFetchDesktopAndroid) {
   base::android::device_info::set_is_desktop_for_testing(true);
   policy_type_ = dm_protocol::GetChromeUserPolicyType();
   CreateClient();
@@ -4002,7 +4004,9 @@ TEST_F(CloudPolicyClientTest, PolicyFetchDesktopAndroid) {
   base::android::device_info::reset_is_desktop_for_testing();
 }
 
-TEST_F(CloudPolicyClientTest, RegistrationDesktopAndroid) {
+// Constantly failing on android-automotive-12l-x64-rel-tests.
+// crbug.com/528019503
+TEST_F(CloudPolicyClientTest, DISABLED_RegistrationDesktopAndroid) {
   base::android::device_info::set_is_desktop_for_testing(true);
   policy_type_ = dm_protocol::GetChromeUserPolicyType();
   CreateClient();
