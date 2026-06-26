486ab5bbb1213081b102f2c7bede02207ce9c455
Eric Ekey <ericekey@google.com>
2026-06-26 06:33:54 -0700

    [iOS][AppBar] Add NTP Background Preview to Customization Button
    This change adds previews for NTP background images, color themes, and
    un-themed NTPs to the NTP overflow menu action to present the home
    customization menu.
    
    A future change should ensure the preview image is updated if the NTP
    background changes while the preview is visible (SyncThemesIos).
    
    Color Theme: https://screenshot.googleplex.com/LGsEw2iKN2sZeJU
    Background Image: https://screenshot.googleplex.com/866Hip2PT6fatrY
    
    Bug: 527016576
    Change-Id: Ie8e8070e275385ce39af48d1ed5b8ff3d77b6a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8006511
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653106}

--- ios/chrome/browser/popup_menu/overflow_menu/coordinator/overflow_menu_mediator.h
+++ ios/chrome/browser/popup_menu/overflow_menu/coordinator/overflow_menu_mediator.h
@@ -15,20 +15,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace bookmarks {
 class BookmarkModel;
-}
+}  // namespace bookmarks
 namespace feature_engagement {
 class Tracker;
-}
+}  // namespace feature_engagement
 namespace signin {
 class AvatarProvider;
 class IdentityManager;
-}
+}  // namespace signin
 namespace syncer {
 class SyncService;
-}
+}  // namespace syncer
 namespace web {
 class WebState;
-}
+}  // namespace web
 
 @protocol ActivityServiceCommands;
 class AuthenticationService;
@@ -39,9 +39,11 @@ class BrowserPolicyConnectorIOS;
 @protocol FindInPageCommands;
 @protocol GeminiCommands;
 @protocol HelpCommands;
+class HomeBackgroundCustomizationService;
 @class LayoutGuideCenter;
 @protocol LensOverlayCommands;
 @protocol LevelUpCommands;
+class NTPBackgroundImageCacheService;
 @protocol OverflowMenuCustomizationCommands;
 @class OverflowMenuOrderer;
 class OverlayPresenter;
@@ -124,6 +126,14 @@ class WebStateList;
 
 // Readinglist model to know if model has finished loading.
 @property(nonatomic, assign) ReadingListModel* readingListModel;
+
+// Service for NTP background customization.
+@property(nonatomic, assign)
+    HomeBackgroundCustomizationService* backgroundCustomizationService;
+
+// Service for NTP background image cache.
+@property(nonatomic, assign)
+    NTPBackgroundImageCacheService* backgroundImageCacheService;
 
 // Pref service to retrieve profile preference values.
 @property(nonatomic, assign) PrefService* profilePrefs;
