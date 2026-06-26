a88742e3455f1c648c3045e1a9d7d11c168fc3d8
Dibyajyoti Pal <dibyapal@google.com>
2026-06-26 14:26:04 -0700

    [PWA/ChromeOS] Fix intent chip tests to work with v2DefaultOn
    This CL updates the IntentChipButton related browser tests so that they
    work with both the v2DefaultOn and v2DefaultOff configuration, now that
    v2DefaultOn is being launched on ChromeOS. This is done by: 1. Ensuring
    that navigation capturing for apps is turned on manually
       ONLY if the test is running with v2DefaultOff. For all other
       instances, installing the app should start capturing navigations.
    2. Updating the UserActions for the intent picker chip clicking to be
       measured cross platform, instead of just for non-ChromeOS platforms.
       This will fix some tests that are being disabled in
       crrev.com/c/7989361.
    
    Also updated the OWNERs of these files so that the web_applications/
    team owns them going forward to reduce review churn. The original owners
    of the intent picker code no longer work on Chrome, and the primary
    owner of
    https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/apps/intent_helper/OWNERS.
    
    Bug: 521860617
    Change-Id: Ie16a22e17aa0508a6419cf810f8d43177185b834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7998677
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Kunjan Patel <ksukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653455}

--- chrome/browser/ui/intent_picker_tab_helper.cc
+++ chrome/browser/ui/intent_picker_tab_helper.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/bind.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
@@ -391,6 +393,7 @@ void IntentPickerTabHelper::ShowIntentPickerOrLaunchAppImpl(
   // picker is shown via the Web Install API.
   intent_picker_delegate_->RecordIntentPickerIconEvent(
       apps::IntentPickerIconEvent::kIconClicked);
+  base::RecordAction(base::UserMetricsAction("IntentPickerIconClicked"));
 
   if (apps.size() == 1 && !always_show &&
       intent_picker_delegate_->ShouldLaunchAppDirectly(url, apps[0].launch_name,
@@ -444,6 +447,24 @@ void IntentPickerTabHelper::OnIntentPickerClosedMaybeLaunch(
 
   bool should_launch_app =
       (close_reason == apps::IntentPickerCloseReason::OPEN_APP);
+
+  switch (close_reason) {
+    case apps::IntentPickerCloseReason::OPEN_APP:
+      base::RecordAction(
+          base::UserMetricsAction("IntentPickerViewAcceptLaunchApp"));
+      break;
+    case apps::IntentPickerCloseReason::DIALOG_DEACTIVATED:
+      base::RecordAction(base::UserMetricsAction("IntentPickerViewIgnored"));
+      break;
+    case apps::IntentPickerCloseReason::STAY_IN_CHROME:
+      base::RecordAction(
+          base::UserMetricsAction("IntentPickerViewClosedStayInChrome"));
+      break;
+    case apps::IntentPickerCloseReason::ERROR_BEFORE_PICKER:
+    case apps::IntentPickerCloseReason::ERROR_AFTER_PICKER:
+    case apps::IntentPickerCloseReason::PREFERRED_APP_FOUND:
+      break;
+  }
 
   intent_picker_delegate_->RecordOutputMetrics(
       entry_type, close_reason, should_persist, should_launch_app);
