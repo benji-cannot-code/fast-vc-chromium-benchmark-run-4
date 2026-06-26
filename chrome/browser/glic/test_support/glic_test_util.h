7f54c20036f841b10845b6e7922ac1172e177180
Marke Hallowell <wry@chromium.org>
2026-06-26 09:34:09 -0700

    Add a scoped glic capability helper to test util.
    Bug: None
    Link: https://chromium-review.googlesource.com/id/I4322599ad32c1fe6f8a10b9e327fda7d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007939
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653215}

--- chrome/browser/glic/test_support/glic_test_util.h
+++ chrome/browser/glic/test_support/glic_test_util.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/test/test_future.h"
@@ -218,6 +219,18 @@ void SigninWithPrimaryAccount(Profile* profile,
 void SetGlicCapability(Profile* profile, bool enabled);
 void SetGlicCapability(AccountCapabilitiesTestMutator& mutator, bool enabled);
 void SetFRECompletion(Profile* profile, prefs::FreStatus fre_status);
+
+// Helper to temporarily override the Glic capability for a profile during a
+// test, and restore the original capability upon destruction.
+class ScopedGlicCapability {
+ public:
+  ScopedGlicCapability(Profile* profile, bool enabled);
+  ~ScopedGlicCapability();
+
+ private:
+  raw_ptr<Profile> profile_;
+  bool original_enabled_;
+};
 
 void InvalidateAccount(Profile* profile);
 void ReauthAccount(Profile* profile);
