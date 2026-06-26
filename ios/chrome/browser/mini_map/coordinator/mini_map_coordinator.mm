f5ddfe6c686acdc48220f14fb19233711c6589a8
Esteban Brugal <estebanbrugal@google.com>
2026-06-26 10:58:45 -0700

    Configure mini map controller with incognito state.
    Add the `configureIncognito:` method to the `MiniMapController` API
    and update the `MiniMapCoordinator` to pass the current profile's
    off-the-record state. This ensures that the underlying map
    implementation is aware of the user's context and can appropriately
    respect incognito mode when presenting a map view.
    
    Change-Id: I867119b1de684e00c06a42059558a7fa805e1987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7962854
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Esteban Brugal <estebanbrugal@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653280}

--- ios/chrome/browser/mini_map/coordinator/mini_map_coordinator.mm
+++ ios/chrome/browser/mini_map/coordinator/mini_map_coordinator.mm
@@ -203,6 +203,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)configureForURL {
   __weak __typeof(self) weakSelf = self;
   [self.miniMapController configureURL:_URL];
+  [self.miniMapController configureIncognito:self.profile->IsOffTheRecord()];
   [self.miniMapController configureFailureCompletion:^{
     [weakSelf fallbackToOriginalURL];
   }];
