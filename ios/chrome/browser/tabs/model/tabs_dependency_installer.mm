eb1bf7c9d6a47c416710cc62d306b83367d8631c
Louis Romero <lpromero@google.com>
2026-06-26 07:12:13 -0700

    Fix typos in comments in TabsDependencyInstaller.mm.
    Change-Id: I0b77d59832baded1e1766e761bb45e458aaf9c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010992
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653123}

--- ios/chrome/browser/tabs/model/tabs_dependency_installer.mm
+++ ios/chrome/browser/tabs/model/tabs_dependency_installer.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_state_observer.h"
 
 // Helper observing the WebStateList and the unrealized WebStates events and
-// forwaring them to the owning TabsDependencyInstaller instance.
+// forwarding them to the owning TabsDependencyInstaller instance.
 class TabsDependencyInstallationHelper : public WebStateListObserver,
                                          public web::WebStateObserver {
  public:
@@ -43,7 +43,7 @@ class TabsDependencyInstallationHelper : public WebStateListObserver,
  private:
   // Invoked when a WebState is inserted/removed. They handle the fact that
   // the WebState may be unrealized (by observing it) or realized (invokes
-  // the correct method of TabDependencyInstaller).
+  // the correct method of TabsDependencyInstaller).
   void OnWebStateAdded(web::WebState* web_state);
   void OnWebStateRemoved(web::WebState* web_state);
 
