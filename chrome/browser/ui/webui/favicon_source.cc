d823ea9ea14128c9ae12688382148eaca6093cb3
Kevin <kevinshah@google.com>
2026-06-26 10:38:43 -0700

    [Contextual tasks] Enabling WebUI composebox on Android
    This CL ensures we can build and compile contextual tasks composebox for
    Android. Included searchbox favicons resources for Android. Currently
    excluded contextual tasks tests which are available on desktop and
    desktop_android. Added TODOs required for clean up process.
    Exception request for IS_DESKTOP_ANDROID: crbug.com/512583842
    
    Bug: b:525513118
    Binary-Size: Size increase is unavoidable (see above).
    Change-Id: I3b59f1ec0cc45196e9713032436539c5a6564a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7962856
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Kevin Shah <kevinshah@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653269}

--- chrome/browser/ui/webui/favicon_source.cc
+++ chrome/browser/ui/webui/favicon_source.cc
@@ -318,6 +318,7 @@ void FaviconSource::SendDefaultResponse(
 
   int resource_id;
   switch (size_in_dip) {
+#if !BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_DESKTOP_ANDROID)
     case 64:
       resource_id =
           dark_mode ? IDR_DEFAULT_FAVICON_DARK_64 : IDR_DEFAULT_FAVICON_64;
@@ -326,6 +327,7 @@ void FaviconSource::SendDefaultResponse(
       resource_id =
           dark_mode ? IDR_DEFAULT_FAVICON_DARK_32 : IDR_DEFAULT_FAVICON_32;
       break;
+#endif
     default:
       resource_id = dark_mode ? IDR_DEFAULT_FAVICON_DARK : IDR_DEFAULT_FAVICON;
       break;
