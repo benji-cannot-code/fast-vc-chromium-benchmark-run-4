7a18b8651c89ac61fb141e28d4271f41e9b550fa
Andrea Gomez <andreaxg@google.com>
2026-06-26 09:35:54 -0700

    Disable ZSS warming for auto opened sources
    Bug: b:525060692
    Change-Id: I0ac79ae73c94e386fde0e2c7c0dc23f814378e17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957995
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653217}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -127,6 +127,12 @@ BASE_FEATURE(kGlicUnbindOnClose, base::FEATURE_DISABLED_BY_DEFAULT);
 
 namespace {
 
+// Invocation sources for which ZSS warming should be disabled.
+constexpr auto kZssWarmingBlocklist = std::to_array<mojom::InvocationSource>({
+    mojom::InvocationSource::kAutoOpenedForPdf,
+    mojom::InvocationSource::kPromotionPage,
+});
+
 EmbedderKey CreateSidePanelEmbedderKey(tabs::TabInterface* tab) {
   CHECK(tab);
   return EmbedderKey(tab);
@@ -410,7 +416,7 @@ void GlicInstanceImpl::Show(const ShowOptions& options) {
     SetActiveEmbedderAndNotifyVisibilityChange(new_key);
   }
 
-  MaybeWarmZeroStateSuggestions();
+  MaybeWarmZeroStateSuggestions(options.invocation_source);
 
   MaybeShowHostUi(embedder_to_show, options.invocation_source,
                   options.prompt_suggestion, options.fre_override);
@@ -1174,10 +1180,22 @@ void GlicInstanceImpl::MaybeDeactivateEmbedder(EmbedderKey key) {
   }
 }
 
-void GlicInstanceImpl::MaybeWarmZeroStateSuggestions() {
+void GlicInstanceImpl::MaybeWarmZeroStateSuggestions(
+    mojom::InvocationSource invocation_source) {
   if (conversation_id() ||
       !GlicEnabling::IsEnabledAndConsentForProfile(profile_) ||
       !IsZeroStateSuggestionsEnabled()) {
+    return;
+  }
+
+  for (auto blocked_source : kZssWarmingBlocklist) {
+    if (blocked_source == invocation_source) {
+      zss_warming_disabled_ = true;
+      break;
+    }
+  }
+
+  if (zss_warming_disabled_) {
     return;
   }
 
