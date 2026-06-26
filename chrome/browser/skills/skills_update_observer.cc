95fc2e2339c0033a1b8150d42912679a5f3bea49
Bryant Chandler <bryantchandler@google.com>
2026-06-26 15:15:36 -0700

    [glic] Add GlicInstance::UpdateSkillPreviews
    SkillsUpdateObserver was using GlicInstance::host() to update skills.
    
    This change adds UpdateSkillPreviews() directly to GlicInstance, and
    implements it in GlicInstanceImpl to call GlicSkillsManager. This allows
    SkillsUpdateObserver to trigger skill updates without accessing host().
    
    Bug: b:508255934
    Change-Id: Ib97eafb2ae9bb9ac055bd9da098f9a186a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007623
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653487}

--- chrome/browser/skills/skills_update_observer.cc
+++ chrome/browser/skills/skills_update_observer.cc
@@ -114,7 +114,7 @@ void SkillsUpdateObserver::MaybeUpdateContextualSkills() {
   }
   if (glic::GlicInstance* instance =
           glic_keyed_service->GetInstanceForTab(&(*tab_))) {
-    instance->host().skills_manager().UpdateSkillPreviews(&(*tab_));
+    instance->UpdateSkillPreviews(&(*tab_));
   }
 }
 
