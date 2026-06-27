9721e4ef006d9be1e89e07fc747af6badd07d011
Mike Wasserman <msw@chromium.org>
2026-06-26 21:21:44 -0700

    Built-In AI: Update fallback default temperature and top-k
    These are used when server configs do not specify `sampling_params`.
    
    Bug: 521856262
    Change-Id: I2e4ced779f0950f61ae080afcaae9a017c0b48d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7995758
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653605}

--- components/optimization_guide/core/model_execution/manifest_broker/manifest_solution_factory_unittest.cc
+++ components/optimization_guide/core/model_execution/manifest_broker/manifest_solution_factory_unittest.cc
@@ -136,7 +136,7 @@ TEST_F(ManifestSolutionFactoryTest, ExecuteTestFeature) {
       ("Encoder cache weight: 1016"
        "Adapter cache weight: 1017"
        "hello max:1024"
-       "TopK: 3, Temp: 0.800000011920929");
+       "TopK: 64, Temp: 1");
   EXPECT_EQ(*response.value(), expected_response);
 }
 
@@ -227,7 +227,7 @@ TEST_F(ManifestSolutionFactoryTest, ExecuteTestFeatureWithHints) {
        "Encoder cache weight: 1016"
        "Adapter cache weight: 1017"
        "hello max:1024"
-       "TopK: 3, Temp: 0.800000011920929");
+       "TopK: 64, Temp: 1");
   EXPECT_EQ(*response.value(), expected_response);
 }
 
@@ -259,7 +259,7 @@ TEST_F(ManifestSolutionFactoryTest, ExecuteTestFeatureWithAdaptation) {
        "Encoder cache weight: 1016"
        "Adapter cache weight: 1017"
        "hello max:1024"
-       "TopK: 3, Temp: 0.800000011920929");
+       "TopK: 64, Temp: 1");
   EXPECT_EQ(*response.value(), expected_response);
 }
 
@@ -296,7 +296,7 @@ TEST_F(ManifestSolutionFactoryTest, ExecuteTestFeatureWithSafety) {
       ("Encoder cache weight: 1016"
        "Adapter cache weight: 1017"
        "execute:hello max:1024"
-       "TopK: 3, Temp: 0.800000011920929");
+       "TopK: 64, Temp: 1");
   EXPECT_EQ(*response.value(), expected_response);
 }
 
