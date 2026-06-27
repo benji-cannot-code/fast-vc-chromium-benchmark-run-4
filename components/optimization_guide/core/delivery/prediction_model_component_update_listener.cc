436f02ac39c5bcd11bd7648540d3664835359ab3
Zekun Jiang <zekunjiang@google.com>
2026-06-26 19:18:23 -0700

    Make `LoadAndVerifyModelOffThread` a public function.
    Also replaces the '|' notation with backticks for identifiers in comments, for files touched in this CL.
    
    Bug: 515762868
    Change-Id: I8542f2461e5d74b75c3ba4cd3f90561a32eaf65e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7997196
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653578}

--- components/optimization_guide/core/delivery/prediction_model_component_update_listener.cc
+++ components/optimization_guide/core/delivery/prediction_model_component_update_listener.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "components/optimization_guide/core/delivery/model_info.h"
 #include "components/optimization_guide/core/delivery/model_provider_registry.h"
-#include "components/optimization_guide/core/delivery/prediction_model_store.h"
 #include "components/optimization_guide/core/optimization_guide_logger.h"
 
 namespace optimization_guide {
@@ -78,8 +77,7 @@ void PredictionModelComponentUpdateListener::MaybeUpdateModel(
   // Load the model in the background.
   GetTaskRunner(target)->PostTaskAndReplyWithResult(
       FROM_HERE,
-      base::BindOnce(&PredictionModelStore::LoadAndVerifyModelOffThread, target,
-                     install_dir),
+      base::BindOnce(&LoadAndVerifyModelInfoOffThread, target, install_dir),
       base::BindOnce(&PredictionModelComponentUpdateListener::OnModelLoaded,
                      GetWeakPtr(), target, version));
 }
@@ -87,7 +85,7 @@ void PredictionModelComponentUpdateListener::MaybeUpdateModel(
 void PredictionModelComponentUpdateListener::OnModelLoaded(
     proto::OptimizationTarget target,
     const base::Version& version,
-    std::unique_ptr<proto::PredictionModel> prediction_model) {
+    std::unique_ptr<ModelInfo> model_info) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   // Check if this target is still expected and the version matches.
@@ -97,8 +95,6 @@ void PredictionModelComponentUpdateListener::OnModelLoaded(
     return;
   }
 
-  auto model_info =
-      prediction_model ? ModelInfo::Create(*prediction_model) : nullptr;
   if (!model_info) {
     // The model is invalid. We keep it in component_info_map_, since we've
     // still "loaded" it and we don't want to re-do that, but we'll report it to
