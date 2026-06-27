4cc37f37dd1cd3baf2c1cbc3b2022d0a28d4c974
Dan Polanco <polardz@google.com>
2026-06-26 19:31:13 -0700

    [IA][BottomBar][AIM] Add feature engagement targets
    Bug: 528102863
    Change-Id: Icac416c5ba4bee7107c16fdad6f9523742be4988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8009987
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653583}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -33,6 +33,8 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHAdaptiveButtonInTopToolbarCustomizationVoiceSearchFeature,
     &kIPHAdaptiveButtonPinGlicToolbarButtonFeature,
     &kIPHAimActivationHint,
+    &kIPHAndroidBottomBarAim,
+    &kIPHAndroidBottomBarAimPromoDialog,
     &kIPHAndroidBottomBarGlic,
     &kIPHAndroidBottomBarNewTab,
     &kIPHAndroidBottomBarPromoDialog,
