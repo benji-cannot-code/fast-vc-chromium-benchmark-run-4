3aded576b6273a92da6a4cf683f4498f4ddb1c3d
Antonio Sartori <antoniosartori@chromium.org>
2026-06-29 01:13:10 -0700

    Simplify calls to CanBeAutoRevokedAsUnusedPermission
    This CL is just a small refactoring of the function
    CanBeAutoRevokedAsUnusedPermission. That function used to take a
    
    callers constructed that base::Value by serializing a
    PermissioNSetting in the first place. This CL remove that
    back-and-forth conversion.
    
    base: :Value() and parse it into a PermissionSetting, yet all but one
    Change-Id: If0b228298bec8c71f2f1c587428efa664c43859b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8002917
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1653909}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -1710,8 +1710,8 @@ void SiteSettingsHandler::HandleSetOriginPermissions(
     if (base::FeatureList::IsEnabled(
             permissions::features::
                 kSafetyHubUnusedPermissionRevocationForAllSurfaces) &&
-        content_settings::CanBeAutoRevokedAsUnusedPermission(
-            content_type, content_settings::ContentSettingToValue(setting))) {
+        content_settings::CanBeAutoRevokedAsUnusedPermission(content_type,
+                                                             setting)) {
       constraints.set_track_last_visit_for_autoexpiration(true);
     }
 
@@ -1966,8 +1966,8 @@ void SiteSettingsHandler::HandleSetCategoryPermissionForPattern(
   if (base::FeatureList::IsEnabled(
           permissions::features::
               kSafetyHubUnusedPermissionRevocationForAllSurfaces) &&
-      content_settings::CanBeAutoRevokedAsUnusedPermission(
-          content_type, content_settings::ContentSettingToValue(setting))) {
+      content_settings::CanBeAutoRevokedAsUnusedPermission(content_type,
+                                                           setting)) {
     constraints.set_track_last_visit_for_autoexpiration(true);
   }
 
