82a2424dab1c2c2c6011dd893c7fecda343469d7
Henrique Nakashima <hnakashima@chromium.org>
2026-06-29 14:42:34 -0700

    [Android] Use WeakReference to WindowAndroid in TipsUtils delayed task
    Use a WeakReference<WindowAndroid> inside the 1-hour delayed task
    scheduled by TipsUtils to prevent retaining destroyed activity instances
    in memory.
    
    Bug: 527134359
    Change-Id: Id7382d5dd57ed218b2cc56e8109a4a94bd5bc7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8013211
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654375}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/tips/TipsUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/tips/TipsUtils.java
@@ -53,6 +53,7 @@ import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.base.WindowAndroid;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -297,6 +298,7 @@ public class TipsUtils {
 
     private static void maybeScheduleTipsNotification(
             Profile profile, WindowAndroid windowAndroid) {
+        if (windowAndroid.isDestroyed()) return;
         boolean isBottomOmnibox = isBottomOmniboxActive(windowAndroid);
 
         TipsUtils.areTipsNotificationsEnabled(
@@ -314,10 +316,15 @@ public class TipsUtils {
                         // it will be torn down. Note that it is possible that when the notification
                         // is rescheduled, the usage criteria may have changed such that the user is
                         // no longer eligible to receive a notification.
+                        WeakReference<WindowAndroid> windowAndroidRef =
+                                new WeakReference<>(windowAndroid);
                         PostTask.postDelayedTask(
                                 TaskTraits.UI_DEFAULT,
                                 () -> {
-                                    maybeScheduleTipsNotification(profile, windowAndroid);
+                                    WindowAndroid window = windowAndroidRef.get();
+                                    if (window != null && !window.isDestroyed()) {
+                                        maybeScheduleTipsNotification(profile, window);
+                                    }
                                 },
                                 TimeUnit.HOURS.toMillis(1));
                     }
