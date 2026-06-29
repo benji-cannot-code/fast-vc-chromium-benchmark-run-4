7b223c6c19c2a9f97b885f83e31acc6f7ba83a98
Chris Lu <thegreenfrog@google.com>
2026-06-29 11:27:36 -0700

    [ios, prototype] Extract NewTabPageBottomSheetViewController
    This CL separates bottom sheet management out of NewTabPageRedesignViewController into a dedicated child view controller class, NewTabPageBottomSheetViewController.
    
    The new child view controller owns the glassmorphic card view, drag handles, fake location bar, pan gesture recognition, and snapping animations.
    
    Bug: 526677926
    Change-Id: Id9bdc53111f95ecf4a7f1297fa861561d77d3292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8014260
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654217}

---
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_bottom_sheet_view_controller.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_NTP_UI_BUNDLED_NEW_TAB_PAGE_BOTTOM_SHEET_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_NTP_UI_BUNDLED_NEW_TAB_PAGE_BOTTOM_SHEET_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+@class NewTabPageBottomSheetViewController;
+
+// Delegate for events in the bottom sheet view controller.
+@protocol NewTabPageBottomSheetViewControllerDelegate <NSObject>
+
+// Called when the fake location bar in the bottom sheet is tapped.
+- (void)bottomSheetViewControllerDidTapFakeLocationBar:
+    (NewTabPageBottomSheetViewController*)bottomSheetViewController;
+
+@end
+
+// View controller managing the bottom sheet card, gestures, and subviews for
+// the NTP Redesign.
+@interface NewTabPageBottomSheetViewController : UIViewController
+
+// Delegate for bottom sheet actions.
+@property(nonatomic, weak) id<NewTabPageBottomSheetViewControllerDelegate>
+    delegate;
+
+// Clears state and delegates.
+- (void)invalidate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_NTP_UI_BUNDLED_NEW_TAB_PAGE_BOTTOM_SHEET_VIEW_CONTROLLER_H_
