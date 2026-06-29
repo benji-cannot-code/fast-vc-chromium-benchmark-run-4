ca12b2a37fe9f90246fb59549a89470a0c9b7ec5
Briana McClure <bmcclure@google.com>
2026-06-29 09:21:24 -0700

    [iOS] Clean up EG Tests for kTabSwitcherOverflowMenu
    This change updates several tests that were written based on the Tab
    Grid Edit Menu behavior. The tests are updated to expect the new
    Tab Switcher Overflow Menu behavior in preparation to clean up the
    feature flag.
    
    Bug: 505723557
    Fixed: 441313129
    Change-Id: Ifd0971bc1d6cd5be3e5561806951ffec3eddb8f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007168
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654130}

--- ios/chrome/test/earl_grey/chrome_matchers.mm
+++ ios/chrome/test/earl_grey/chrome_matchers.mm
@@ -1051,18 +1051,14 @@ id<GREYMatcher> IncognitoInterstitialCancelButton() {
   return [ChromeMatchersAppInterface incognitoInterstitialCancelButton];
 }
 
-#pragma mark - Tab Grid Edit Mode
+#pragma mark - Tab Grid Overflow Menu
 
-id<GREYMatcher> TabGridEditButton() {
-  return [ChromeMatchersAppInterface tabGridEditButton];
+id<GREYMatcher> TabGridOverflowMenuCloseAllButton() {
+  return [ChromeMatchersAppInterface tabGridOverflowMenuCloseAllButton];
 }
 
-id<GREYMatcher> TabGridEditMenuCloseAllButton() {
-  return [ChromeMatchersAppInterface tabGridEditMenuCloseAllButton];
-}
-
-id<GREYMatcher> TabGridSelectTabsMenuButton() {
-  return [ChromeMatchersAppInterface tabGridSelectTabsMenuButton];
+id<GREYMatcher> TabGridOverflowMenuSelectTabsButton() {
+  return [ChromeMatchersAppInterface tabGridOverflowMenuSelectTabsButton];
 }
 
 id<GREYMatcher> TabGridEditAddToButton() {
