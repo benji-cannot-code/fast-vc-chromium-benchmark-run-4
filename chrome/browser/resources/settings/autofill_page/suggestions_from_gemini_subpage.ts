7377229f983e165e03d8026d4490268c350b791d
Stanislav Mikheyev <mikhe@google.com>
2026-06-29 02:54:02 -0700

    Autofill: add UI for atMemory shortcut setting
    Also remove manageSuggestionsLinkRow, its removal was wrongly
    reverted in one of the previous CLs.
    
    Screenshot: https://screenshot.googleplex.com/3yTBGvgaAUqWXNg
    
    Bug: b:521768751
    Change-Id: I66d52c8e1f86bda4f0e5d4c4a96fce0fdccbdf46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957563
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653948}

--- chrome/browser/resources/settings/autofill_page/suggestions_from_gemini_subpage.ts
+++ chrome/browser/resources/settings/autofill_page/suggestions_from_gemini_subpage.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_components/cr_shortcut_input/cr_shortcut_input.js';
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
 import '../settings_page/settings_subpage.js';
 import '../controls/settings_toggle_button.js';
@@ -30,10 +31,32 @@ export class SettingsSuggestionsFromGeminiSubpageElement extends
     return getTemplate();
   }
 
+  static get properties() {
+    return {
+      atMemoryTrigger_: {
+        type: String,
+        value: '',
+      },
+    };
+  }
+
+  declare private atMemoryTrigger_: string;
+
   private onManageConnectedAppsClick_() {
     // TODO(crbug.com/512204278): Add metrics.
     OpenWindowProxyImpl.getInstance().openUrl(
         loadTimeData.getString('personalContextConnectedAppsUrl'));
+  }
+
+  private onAtMemoryTriggerSettingUpdated_(event: CustomEvent<string>) {
+    // TODO(crbug.com/521768751): update pref instead
+    this.atMemoryTrigger_ = event.detail;
+  }
+
+  private atMemoryTriggerSettingEnabled_() {
+    // TODO(crbug.com/521768751): call MayPerformAtMemoryAction service
+    // MayPerformAtMemoryAction(kAllowCustomizeAtMemoryShortcut,...)
+    return true;
   }
 }
 
