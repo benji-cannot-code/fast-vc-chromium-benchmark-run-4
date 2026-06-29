c89fe8778baf782c6b7327ffc7b192a9ae2329b9
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-06-29 14:20:28 -0700

    pwa: Disable sync integration tests on TSAN
    https://crrev.com/c/8020343 attempted to disable the flaky tests but
    that ended up disabled more than 90 tests. Rather than polluting the
    test with MAYBE_ macros, we change the test suite to skip all tests
    on TSAN.
    
    Tests have been flaky on TSAN for months:
    
    https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Ftest%5C%3Async_integration_tests!gtest%3A%3AWebAppIntegration%23WAI_InstallMenuStandalone_ManifestUpdateAddMigrateToStandaloneStandaloneMigratedSuggested_SwitchProfileClientsClient2_InstallLocallyStandalone_LaunchFromChromeAppsStandalone_ManifestUpdateAddMigrateToStandaloneStandaloneMigratedSuggested_TriggerUpdateDialogAndHandleResponseAcceptUpdate_SwitchProfileClientsClient1%2FkSyncTransportOnly?q=V%3Abuilder%3Dlinux_chromium_tsan_rel_ng+V%3Aos%3DUbuntu-22.04+V%3Atest_suite%3Dsync_integration_tests
    
    Bug: 511805630
    Change-Id: Iddaed2f20a19b291d8ea1bcc4ccccb82fe1a05c8
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8014993
    Reviewed-by: Luke Deng <lukedeng@google.com>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654360}

--- chrome/browser/sync/test/integration/web_apps/two_client_web_apps_integration_test_base.h
+++ chrome/browser/sync/test/integration/web_apps/two_client_web_apps_integration_test_base.h
@@ -20,6 +20,8 @@ class TwoClientWebAppsIntegrationTestBase
   TwoClientWebAppsIntegrationTestBase();
   ~TwoClientWebAppsIntegrationTestBase() override;
 
+  static void SetUpTestSuite();
+
   SyncTest::SetupSyncMode GetSetupSyncMode() const override;
 
   // WebAppIntegrationTestDriver::TestDelegate:
