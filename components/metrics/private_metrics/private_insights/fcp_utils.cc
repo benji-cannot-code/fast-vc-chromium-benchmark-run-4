78e1fa213545c604cb663b89ccfa285878f998e2
Kamil Jarosz <kjarosz@google.com>
2026-06-29 02:11:36 -0700

    private_insights: Introduce CountdownLatch synchronization utility
    Adds `CountdownLatch`, a thread-safe synchronization primitive in
    `fcp_utils.h`.
    
    This utility bridges the synchronous `fcp::client::http::HttpClient`
    interface and Chromium's asynchronous `SharedURLLoaderFactory` execution
    by allowing caller threads to block until a batch of operations completes.
    
    Bug: b:525314527
    Change-Id: I16aef5aa2e87169f5cc61ca523ef072c00ace62b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8011531
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653929}

---
+++ components/metrics/private_metrics/private_insights/fcp_utils.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/metrics/private_metrics/private_insights/fcp_utils.h"
+
+namespace private_insights {
+
+CountdownLatch::CountdownLatch(size_t count)
+    : count_(count),
+      done_event_(base::WaitableEvent::ResetPolicy::MANUAL,
+                  base::WaitableEvent::InitialState::NOT_SIGNALED) {
+  if (count_ == 0) {
+    done_event_.Signal();
+  }
+}
+
+CountdownLatch::~CountdownLatch() = default;
+
+void CountdownLatch::CountDown() {
+  if (--count_ == 0) {
+    done_event_.Signal();
+  }
+}
+
+void CountdownLatch::Wait() {
+  done_event_.Wait();
+}
+
+}  // namespace private_insights
