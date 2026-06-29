6bc0a9d47477961f25c94c4460163b7c3a609f1a
David Bokan <bokan@chromium.org>
2026-06-29 13:12:17 -0700

    Dictation: Add flag to send context async
    Package up all the context kinds into a DictationContext object. This
    object is now optional on the StartStream event. When the flag is off
    StartStream continues to provide context. When enabled, the StartStream
    event elides context and context is provided after StartStream via a new
    ContextUpdate event.
    
    This is provided as a flag since we likely want to use the async
    approach long term but carries more uncertainty in terms of how it will
    be handled on the server so we use a flag for now as we work it out.
    
    Change-Id: I900db4902dc2f83327123baf7feaf3280553f280
    Bug: b:525847081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8012689
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: David Bokan <bokan@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654301}

--- chrome/browser/dictation/listener_stream_provider.h
+++ chrome/browser/dictation/listener_stream_provider.h
@@ -55,7 +55,9 @@ class ListenerStreamProvider : public StreamProvider {
 
  private:
   DictationMultiplexer& GetMultiplexer() const;
-  void OnPageContextCaptured(DictationContext result);
+  void StartStream(std::optional<DictationContext> context);
+  void OnStartContextCaptured(DictationContext result);
+  void OnAsyncContextCaptured(DictationContext result);
 
   // Owns this
   const base::raw_ref<StreamProviderDelegate> delegate_;
