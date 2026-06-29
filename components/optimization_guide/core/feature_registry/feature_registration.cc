cf781870301bbc99e38bb0f320f3a4fca1a3a34e
Sophie Chang <sophiechang@chromium.org>
2026-06-29 09:23:04 -0700

    Onboard AtMemory to MQLS (client-side)
    Note that enterprise policy will be added in a subsequent CL
    
    Bug: 528012688
    Change-Id: Id56ca32052d5022724a517af3233488638f475b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8018666
    Reviewed-by: Rahim Mammadli <rahimis@google.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654131}

--- components/optimization_guide/core/feature_registry/feature_registration.cc
+++ components/optimization_guide/core/feature_registry/feature_registration.cc
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/optimization_guide/proto/model_quality_service.pb.h"
 #include "components/prefs/pref_registry_simple.h"
-#include "enterprise_policy_registry.h"
-#include "mqls_feature_registry.h"
 
 namespace optimization_guide {
 
@@ -98,6 +96,8 @@ BASE_FEATURE(kGeminiAntiscamProtectionMqlsLogging,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFindsMqlsLogging, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAtMemoryMqlsLogging, base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace features
 
@@ -309,6 +309,18 @@ void RegisterContextualCueing() {
   SettingsUiRegistry::GetInstance().Register(std::move(ui_metadata));
 }
 
+void RegisterAtMemory() {
+  const char kAtMemoryName[] = "AtMemory";
+
+  // TODO: b/524157152 - Add enterprise policy.
+
+  auto mqls_metadata = std::make_unique<MqlsFeatureMetadata>(
+      kAtMemoryName, proto::LogAiDataRequest::FeatureCase::kAtMemory,
+      /*enterprise_policy=*/std::nullopt, &features::kAtMemoryMqlsLogging,
+      FeedbackUnspecified());
+  MqlsFeatureRegistry::GetInstance().Register(std::move(mqls_metadata));
+}
+
 }  // anonymous namespace
 
 void RegisterGenAiFeatures(PrefRegistrySimple* pref_registry) {
@@ -332,6 +344,7 @@ void RegisterGenAiFeatures(PrefRegistrySimple* pref_registry) {
     RegisterGeminiAntiscamProtection();
     RegisterFinds();
     RegisterContextualCueing();
+    RegisterAtMemory();
     features_registered = true;
   }
   EnterprisePolicyRegistry::GetInstance().RegisterProfilePrefs(pref_registry);
