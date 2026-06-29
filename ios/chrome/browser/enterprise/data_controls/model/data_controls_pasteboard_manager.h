66e6d57f0d031d7c6aee464a2f3439110272c6bc
Yixi Wu <yixiwu@google.com>
2026-06-29 13:13:46 -0700

    [iOS][Enterprise] Add Pasteboard Manager Observer Interface
    Added a pasteboard observer interface to observe pasteboard content
    change. DataControlsTabHelper will be utilizing this CL in a subsequent
    CL to keep track of Paste Event State.
    
    Bug: 527043149
    Change-Id: I96272f2d8380e7e2b32e11bae3dc0d64efcfcebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7977464
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654304}

--- ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager.h
+++ ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/functional/callback_forward.h"
 #import "base/memory/raw_ptr.h"
 #import "base/no_destructor.h"
+#import "base/observer_list.h"
 #import "base/sequence_checker.h"
 #import "url/gurl.h"
 
@@ -20,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ProfileIOS;
 
 namespace data_controls {
+
+class DataControlsPasteboardManagerObserver;
 
 // Source of the data on the pasteboard, used for evaluating Data Controls
 // policies.
@@ -98,6 +101,10 @@ class DataControlsPasteboardManager {
   // indicates that the items can't remain in the general Pasteboard.
   void RestorePlaceholderToGeneralPasteboardIfNeeded();
 
+  // Add and remove observer for pasteboard content change.
+  void AddObserver(DataControlsPasteboardManagerObserver* observer);
+  void RemoveObserver(DataControlsPasteboardManagerObserver* observer);
+
  private:
   friend class DataControlsPasteboardManagerTest;
   friend class DataControlsTabHelperTest;
@@ -157,6 +164,8 @@ class DataControlsPasteboardManager {
   __strong PasteboardObserver* pasteboard_observer_;
 
   Stage stage_ = Stage::kUnknownSource;
+
+  base::ObserverList<DataControlsPasteboardManagerObserver> observers_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
--- ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager.mm
+++ ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/not_fatal_until.h"
 #import "components/open_from_clipboard/clipboard_async_wrapper_ios.h"
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager_observer.h"
 #import "ios/chrome/browser/enterprise/data_controls/model/pasteboard_observer.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
@@ -158,6 +159,10 @@ void DataControlsPasteboardManager::OnPasteboardChanged(
       stage_ = Stage::kUnknownSource;
       break;
   }
+
+  for (auto& observer : observers_) {
+    observer.OnPasteboardContentChanged();
+  }
 }
 
 void DataControlsPasteboardManager::
@@ -172,6 +177,18 @@ void DataControlsPasteboardManager::
     GetGeneralPasteboard(
         base::BindOnce(&ReplacePasteboardItemsWithPlaceholder));
   }
+}
+
+void DataControlsPasteboardManager::AddObserver(
+    DataControlsPasteboardManagerObserver* observer) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  observers_.AddObserver(observer);
+}
+
+void DataControlsPasteboardManager::RemoveObserver(
+    DataControlsPasteboardManagerObserver* observer) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  observers_.RemoveObserver(observer);
 }
 
 void DataControlsPasteboardManager::ResetForTesting() {
