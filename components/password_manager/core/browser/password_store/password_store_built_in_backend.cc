99cb24e0b151638e51a65ff0abfda1c27cfa5359
Rafał Godlewski <rgod@google.com>
2026-06-29 03:57:00 -0700

    [iOS] Stop observing sync for profile store on iOS
    On iOS, profile store is local only, so the sync errors should not be
    propagated to it.
    
    Bug: 464228247
    Change-Id: I6b161c7491a3e058576d6baa60558de814cf27a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7968999
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653976}

--- components/password_manager/core/browser/password_store/password_store_built_in_backend.cc
+++ components/password_manager/core/browser/password_store/password_store_built_in_backend.cc
@@ -177,6 +177,7 @@ PasswordStoreBuiltInBackend::PasswordStoreBuiltInBackend(
     os_crypt_async::OSCryptAsync* os_crypt_async,
     std::unique_ptr<AffiliatedMatchHelper> affiliated_match_helper)
     : affiliated_match_helper_(std::move(affiliated_match_helper)),
+      is_account_store_(IsAccountStore(login_db->is_account_store())),
       pref_service_(prefs),
       os_crypt_async_(os_crypt_async) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -433,6 +434,14 @@ PasswordStoreBuiltInBackend::CreateSyncControllerDelegate() {
 void PasswordStoreBuiltInBackend::OnSyncServiceInitialized(
     syncer::SyncService* sync_service) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+#if BUILDFLAG(IS_IOS)
+  // On iOS, the profile store is local-only, so no need to observe sync.
+  // TODO(crbug.com/464228247): Expand this to other platforms.
+  if (!is_account_store_.value()) {
+    return;
+  }
+#endif
+
   sync_observation_.Reset();
   if (sync_service) {
     sync_observation_.Observe(sync_service);
@@ -441,6 +450,13 @@ void PasswordStoreBuiltInBackend::OnSyncServiceInitialized(
 
 void PasswordStoreBuiltInBackend::OnStateChanged(syncer::SyncService* sync) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+#if BUILDFLAG(IS_IOS)
+  // On iOS, sync is not observed for the profile store.
+  if (!is_account_store_.value()) {
+    return;
+  }
+#endif
+
   CHECK(sync_observation_.IsObservingSource(sync));
   if (!base::FeatureList::IsEnabled(
           features::kPasswordStorePropagatesActionableErrors)) {
@@ -459,6 +475,13 @@ void PasswordStoreBuiltInBackend::OnStateChanged(syncer::SyncService* sync) {
 
 void PasswordStoreBuiltInBackend::OnSyncShutdown(syncer::SyncService* sync) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+#if BUILDFLAG(IS_IOS)
+  // On iOS, sync is not observed for the profile store.
+  if (!is_account_store_.value()) {
+    return;
+  }
+#endif
+
   CHECK(sync_observation_.IsObservingSource(sync));
   sync_observation_.Reset();
 }
