5d5f06b14839818dee42d9518dab7f88aae77f54
Ernest Nguyen Hung <ernn@google.com>
2026-06-29 06:28:52 -0700

    [FRE] Fix WebUI 'Don't sign in' cta clashing with effects control button
    It fixes clashing the 'Don't sign in' CTA with the native effects
    control button for the `kDontSignInInTheTopCorner` variation.
    
    Screenshots presenting updated UI (internal only):
    
    Light mode - https://screenshot.googleplex.com/4baRuj89YKXPEHg
    
    Dark mode - https://screenshot.googleplex.com/5edxLFbD2XXEekD
    
    Bug: 515028732
    Change-Id: I654cf63838ff94ba184e6358ed179163419f9cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8016103
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Reviewed-by: Kacper Bajkiewicz <kbajkiewicz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654026}

--- chrome/browser/resources/intro/sign_in_promo_refresh.ts
+++ chrome/browser/resources/intro/sign_in_promo_refresh.ts
@@ -76,6 +76,7 @@ export class SignInPromoRefreshElement extends SignInPromoRefreshElementBase {
       anyButtonClicked_: {type: Boolean},
       usePrimaryAndTonalButtonsForPromos_: {type: Boolean},
       shouldDisableAnimations_: {type: Boolean},
+      isFirstRunDesktopRevampEnabled_: {type: Boolean},
       isDarkMode_: {type: Boolean},
     };
   }
@@ -84,6 +85,8 @@ export class SignInPromoRefreshElement extends SignInPromoRefreshElementBase {
   protected accessor managedDeviceDisclaimer_: string = '';
   protected accessor isDeviceManaged_: boolean =
       loadTimeData.getBoolean('isDeviceManaged');
+  protected accessor isFirstRunDesktopRevampEnabled_: boolean =
+      loadTimeData.getBoolean('isFirstRunDesktopRevampEnabled');
   protected accessor usePrimaryAndTonalButtonsForPromos_: boolean =
       loadTimeData.getBoolean('usePrimaryAndTonalButtonsForPromos');
   // Animations are disabled if the feature is disabled (there is no mechanism
