9c4263e306d362b04a9845371cc3beebccac173c
Jenna Himawan <jhimawan@google.com>
2026-06-29 12:30:52 -0700

    [Bookmarks] Fix white back arrow in bookmarks bar folder menu
    After, light mode: https://screenshot.googleplex.com/4wsNv4nATsb8ArX
    After, dark mode: https://screenshot.googleplex.com/4xVgKVtVtqmsxTB
    After, light + incognito: https://screenshot.googleplex.com/3jUwYByGzAWXSQm
    After, dark + incognito: https://screenshot.googleplex.com/8HHMrTbpvBm9dgq
    After, freeform + incognito: https://screenshot.googleplex.com/4xVgKVtVtqmsxTB
    
    Before (for comparison): https://screenshot.googleplex.com/7Sz9LafAqB7Ksfe
    
    We use the default theme-aware icon tint
    (R.color.default_icon_color_tint_list) for the popup menu items. This
    tint resolves based on the popup's context (the activity) instead of
    using the bookmark bar's theming.
    
    TAG=agy
    CONV=b5e0d726-62fe-4283-854d-00fd59256839
    
    Bug: 528372172
    Change-Id: I887c88a729da47427884354a7ea795c102c9da70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8012547
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654275}

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/bar/BookmarkBarCoordinator.java
@@ -847,4 +847,8 @@ public class BookmarkBarCoordinator
     PropertyModel getBookmarkBarSceneLayerModelForTesting() {
         return mBookmarkBarSceneLayerModel;
     }
+
+    BookmarkBarMediator getMediatorForTesting() {
+        return mMediator;
+    }
 }
