eb6f8ef91c71aaa9660bc2d241cf2b85fcca9b53
Alison Gale <agale@google.com>
2026-06-29 09:29:22 -0700

    [Bokmarks] Prevent bookmark reordering via touch
    On ChromeOS the browser freezes when you try to touch drag bookmarks
    into folders in the side panel. This appears to not have been supported
    for a while on ChromeOS and Windows so this change will explicitly
    disable it to avoid getting into a partial drag state.
    
    Change-Id: I32f1a6add38432f70acb1336ff487ab54e1cef1e
    Bug: 516683808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8013135
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654136}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_drag_manager.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_drag_manager.ts
@@ -69,7 +69,6 @@ class DragSession {
   private lastDragOverElements_: HTMLElement[] = [];
   private lastDropTargetBookmark_: BookmarksTreeNode|null = null;
   private lastBookmarkRowElement_: HTMLElement|null = null;
-  private lastPointerWasTouch_ = false;
   private bookmarksApi_: BookmarksApiProxy =
       BookmarksApiProxyImpl.getInstance();
 
@@ -82,8 +81,8 @@ class DragSession {
 
   start(e: DragEvent) {
     chrome.bookmarkManagerPrivate.startDrag(
-        this.dragData_.elements!.map(bookmark => bookmark.id), 0,
-        this.lastPointerWasTouch_, e.clientX, e.clientY);
+        this.dragData_.elements!.map(bookmark => bookmark.id), 0, false,
+        e.clientX, e.clientY);
   }
 
   update(e: DragEvent) {
@@ -223,6 +222,7 @@ export class PowerBookmarksDragManager {
   private delegate_: PowerBookmarksDragDelegate;
   private dragSession_: DragSession|null = null;
   private eventTracker_: EventTracker = new EventTracker();
+  private lastPointerWasTouch_ = false;
 
   constructor(delegate: PowerBookmarksDragDelegate) {
     this.delegate_ = delegate;
@@ -230,6 +230,10 @@ export class PowerBookmarksDragManager {
 
   startObserving() {
     this.eventTracker_.removeAll();
+    this.eventTracker_.add(
+        this.delegate_, 'pointerdown',
+        (e: Event) => this.lastPointerWasTouch_ =
+            (e as PointerEvent).pointerType === 'touch');
     this.eventTracker_.add(
         this.delegate_, 'dragstart',
         (e: Event) => this.onDragStart_(e as DragEvent));
@@ -279,7 +283,8 @@ export class PowerBookmarksDragManager {
 
   private onDragStart_(e: DragEvent) {
     e.preventDefault();
-    if (!loadTimeData.getBoolean('editBookmarksEnabled')) {
+    if (!loadTimeData.getBoolean('editBookmarksEnabled') ||
+        this.lastPointerWasTouch_) {
       return;
     }
 
