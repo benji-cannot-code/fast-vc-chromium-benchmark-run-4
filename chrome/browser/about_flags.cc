1435d3dfb1c69fe858d2753f88d9130d63849f17
Juan Mojica <juanmojica@google.com>
2026-06-29 15:56:58 -0700

    [Contextual Tasks] Add contextual tasks extension flag
    This change adds the api-contextual-tasks-private feature flag to
    chrome://flags. This allows developers to enable and test the
    contextualTasksPrivate API and the contextual tasks component
    extension on desktop platforms.
    
    * Add api-contextual-tasks-private entry to about_flags.cc.
    * Add flag name and description to flag_descriptions.h.
    * Add flag metadata with owners and expiry milestone to
      flag-metadata.json.
    
    Bug: 517599045
    Change-Id: I8b2ec453e546c2e7d11e4c9927a7883bb1ae6e56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007779
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654436}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8898,6 +8898,11 @@ const FeatureEntry kFeatureEntries[] = {
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+    {"api-contextual-tasks-private",
+     flag_descriptions::kApiContextualTasksPrivateName,
+     flag_descriptions::kApiContextualTasksPrivateDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(extensions_features::kApiContextualTasksPrivate)},
+
     {"api-mime-handler", flag_descriptions::kApiMimeHandlerName,
      flag_descriptions::kApiMimeHandlerDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(extensions_features::kApiMimeHandler)},
