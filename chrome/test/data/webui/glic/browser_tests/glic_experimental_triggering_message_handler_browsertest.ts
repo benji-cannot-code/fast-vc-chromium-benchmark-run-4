0b978b688a8293e9994464b1d7bfacfbe1fb01a7
Theodore Olsauskas-Warren <sauski@google.com>
2026-06-29 11:10:36 -0700

    [Glic] Set Experimental Trigger actuation target to target surface
    To allow experimental triggering to always use the tab that was opened
    explicitly for it, the actuation surface target is changed to be
    kTargetSurface.
    
    Bug: b:521882456
    Change-Id: Ib23751a009177e1e450b0b900880a48e1f8a10e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915640
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654204}

--- chrome/test/data/webui/glic/browser_tests/glic_experimental_triggering_message_handler_browsertest.ts
+++ chrome/test/data/webui/glic/browser_tests/glic_experimental_triggering_message_handler_browsertest.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {ExperimentalTriggeringUpdateType} from '/glic/glic_api/glic_api.js';
+import {ActuationTarget, ExperimentalTriggeringUpdateType} from '/glic/glic_api/glic_api.js';
 import type {ExperimentalTriggeringUpdate, Observable2} from '/glic/glic_api/glic_api.js';
 import {Subject} from '/glic/observable.js';
 
@@ -28,8 +28,11 @@ class TriggeringUpdatesTest extends ApiTestFixtureBase {
   }
 
   async testGetExperimentalTriggeringUpdates() {
-    // Push a terminal update to trigger completion.
     await runUntil(() => client.isSubscribed);
+    const invokeOpts = await client.invokeData.waitUntil(v => v !== undefined);
+    assertDefined(invokeOpts);
+    assertEquals(ActuationTarget.TARGET_SURFACE, invokeOpts.actuationTarget);
+    // Push a terminal update to trigger completion.
     client.triggeringUpdatesSubject.next({
       type: ExperimentalTriggeringUpdateType.TERMINAL_COMPLETION,
       data: '',
@@ -99,6 +102,7 @@ class TriggeringUpdatesTest extends ApiTestFixtureBase {
     await runUntil(() => client.isSubscribed);
     const invokeOpts = await client.invokeData.waitUntil(v => v !== undefined);
     assertDefined(invokeOpts);
+    assertEquals(ActuationTarget.TARGET_SURFACE, invokeOpts.actuationTarget);
     assertDefined(invokeOpts.context);
     assertEquals(1, invokeOpts.context!.parts.length);
     const metadata = invokeOpts.context!.parts[0]!.parentConversationMetadata;
