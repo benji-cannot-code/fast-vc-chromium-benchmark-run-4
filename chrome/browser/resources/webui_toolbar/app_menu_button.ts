504b615ca539f2902d34ab2f63f9ed6cdf779683
Youssef Bourouphael <ybourouphael@google.com>
2026-06-29 08:12:59 -0700

    [WebUI] Add Trailing Margin to App Menu Button.
    Connects the trailingMargin state to the inline style of the toolbar
    chip button. This change also includes unit tests to verify the margin
    is applied correctly based on the state.
    
    Fixed: 510825650
    Change-Id: Ia0bc3162d456513c92962f95983bcef4c6878e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7987435
    Commit-Queue: Youseff Bourouphel <ybourouphael@google.com>
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654081}

--- chrome/browser/resources/webui_toolbar/app_menu_button.ts
+++ chrome/browser/resources/webui_toolbar/app_menu_button.ts
@@ -46,7 +46,6 @@ export class AppMenuButtonElement extends AppMenuButtonElementBase {
     accessibilityText: '',
     tooltip: '',
     isContextMenuVisible: false,
-    // TODO(crbug.com/510825650): Connect the trailing margin.
     trailingMargin: 0,
   };
 
