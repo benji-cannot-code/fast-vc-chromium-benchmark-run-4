98797d975e2e2cd5b817f4abfb884aba210d2546
Jakub Kudzia <jkudzia@google.com>
2026-06-29 07:53:49 -0700

    private_insights: Add helper for JSON Serialization of PageInfo
    Implement SerializePageInfoListToJson as a standalone helper function
    in contextual_cue_log_event_helpers to serialize a list of PageInfo
    protos to a JSON array string. This is needed to log recent pages to FCP.
    
    Bug: 483975174
    Change-Id: Ifafc48d1b6b6dc0bac115e00710d2ffb42ccbff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8019545
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Jakub Kudzia <jkudzia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654069}

---
+++ components/metrics/private_metrics/private_insights/contextual_cue_log_event_helpers.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_CONTEXTUAL_CUE_LOG_EVENT_HELPERS_H_
+#define COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_CONTEXTUAL_CUE_LOG_EVENT_HELPERS_H_
+
+#include <string>
+#include <vector>
+
+#include "components/metrics/private_metrics/private_insights/events/contextual_cue_log_event.pb.h"
+
+namespace private_insights {
+
+// Serializes a list of PageInfo protos to a JSON array string.
+// Returns "[]" if serialization fails.
+std::string SerializePageInfoListToJson(
+    const std::vector<events::ContextualCueLogEvent::PageInfo>& list);
+
+}  // namespace private_insights
+
+#endif  // COMPONENTS_METRICS_PRIVATE_METRICS_PRIVATE_INSIGHTS_CONTEXTUAL_CUE_LOG_EVENT_HELPERS_H_
