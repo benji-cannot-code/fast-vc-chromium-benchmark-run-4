b272fe24868d868451353b27fd6a7acb27cabd3f
David Pennington <dpenning@google.com>
2026-06-29 14:51:48 -0700

    Remove unused views includes fromchrome/browser/glic/widget
    Some includes are only used transitively or other refactors have
    removed the need for them.
    
    Bug: 527901286
    Change-Id: I831b32da8e7a8d91289aae1ace48fe0c49d23b0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010661
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654387}

--- chrome/browser/glic/widget/glic_widget.cc
+++ chrome/browser/glic/widget/glic_widget.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/views/chrome_widget_sublevel.h"
 #include "chrome/browser/ui/views/glic/glic_button_interface.h"
-#include "chrome/browser/ui/views/interaction/browser_elements_views.h"
 #include "chrome/common/chrome_features.h"
 #include "ui/base/base_window.h"
 #include "ui/base/hit_test.h"
