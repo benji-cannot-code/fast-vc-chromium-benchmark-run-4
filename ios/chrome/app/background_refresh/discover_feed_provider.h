f13c208922a25834ebdb8bb232ef7d5aa4092ed4
sadhvikch <sadhvikch@google.com>
2026-06-29 02:37:57 -0700

    Implement discover feed background refresh provider.
    * Implements DiscoverFeedBackgroundRefreshProvider to coordinate background refreshes for the Discover feed.
    * This provider integrates with DiscoverFeedService to get refresh intervals, verify if a refresh is due, and handle task execution or cancellation.
    
    go/discover-background-refresh-on-bling
    
    Change-Id: I44da2962b5a9bbc940075063787876a544eaf876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7985209
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sadhvik Chilukoti <sadhvikch@google.com>
    Commit-Queue: Sadhvik Chilukoti <sadhvikch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1653938}

---
+++ ios/chrome/app/background_refresh/discover_feed_provider.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_BACKGROUND_REFRESH_DISCOVER_FEED_PROVIDER_H_
+#define IOS_CHROME_APP_BACKGROUND_REFRESH_DISCOVER_FEED_PROVIDER_H_
+
+#import "ios/chrome/app/background_refresh/app_refresh_provider.h"
+
+// AppRefreshProvider for the Discover Feed.
+@interface DiscoverFeedProvider : AppRefreshProvider
+
+@end
+
+#endif  // IOS_CHROME_APP_BACKGROUND_REFRESH_DISCOVER_FEED_PROVIDER_H_
