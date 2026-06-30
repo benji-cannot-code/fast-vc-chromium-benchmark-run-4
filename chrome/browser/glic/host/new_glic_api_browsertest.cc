c1d8574611e079254e3074c25e14db36c79c6caa
Sophie Chang <sophiechang@chromium.org>
2026-06-30 14:23:48 -0700

    Remove ZeroStateSuggestionsV2 feature flag as it has been fully launched for many milestones
    Bug: 444176634
    Change-Id: Ic0ce8e1329395ce3e9426883c047b9054add475c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8011093
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655042}

--- chrome/browser/glic/host/new_glic_api_browsertest.cc
+++ chrome/browser/glic/host/new_glic_api_browsertest.cc
@@ -637,8 +637,7 @@ class NewGlicApiTestWithContextualCueing : public NewGlicApiTest {
               {features::kGlicMinLoadingTimeMs.name, "40"},
           }},
          {kGlicZeroStateSuggestions, {}},
-         {kContextualCueing, {}},
-         {mojom::features::kZeroStateSuggestionsV2, {}}},
+         {kContextualCueing, {}}},
         /*disabled_features=*/
         {});
   }
--- chrome/browser/glic/suggestions/contextual_cueing_features.cc
+++ chrome/browser/glic/suggestions/contextual_cueing_features.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/field_trial_params.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/glic/host/glic_features.mojom.h"
 #include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/common/chrome_features.h"
 #include "components/variations/service/variations_service.h"
@@ -153,7 +152,7 @@ const base::FeatureParam<base::TimeDelta> kZSSPageContextTimeout(
     base::Seconds(5));
 
 const base::FeatureParam<int> kMaxPinnedPagesForTriggeringSuggestions(
-    &glic::mojom::features::kZeroStateSuggestionsV2,
+    &kGlicZeroStateSuggestions,
     "ZSSMaxPinnedPagesForTriggeringSuggestions",
     10);
 
