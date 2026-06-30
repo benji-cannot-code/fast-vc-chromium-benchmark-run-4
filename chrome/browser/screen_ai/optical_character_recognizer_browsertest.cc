f8124a84dfc3eea876d5f796d825401dd5bb8a5b
Ramin Halavati <rhalavati@chromium.org>
2026-06-30 09:21:08 -0700

    Add missing `Wait` to receive async OCR results in browser tests.
    Waiting for `perform_feature_` was dropped in one of the recent
    refactorings of `PerformOCRWithDisconnectTimerAndAndWait` in OCR browser
    tests. Added it back.
    
    AX-Relnotes: n/a
    Bug: 470431038
    Change-Id: I8812eb3fd0c2cd4b15c95d223c45f644dc575cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010354
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654809}

--- chrome/browser/screen_ai/optical_character_recognizer_browsertest.cc
+++ chrome/browser/screen_ai/optical_character_recognizer_browsertest.cc
@@ -315,6 +315,7 @@ class OpticalCharacterRecognizerTest
     }
 
     ocr()->PerformOCR(*bitmap, perform_future_.GetCallback());
+    ASSERT_TRUE(perform_future_.Wait());
     disconnect_timer_.Stop();
   }
 
