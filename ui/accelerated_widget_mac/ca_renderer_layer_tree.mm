a1cee310615ec8183235fe07e32c23b35596e2ed
Maggie Chen <magchen@chromium.org>
2026-06-30 10:03:32 -0700

    Revert "mac: Fix a crash in ui::CARendererLayerTree::RootLayer::~RootLayer"
    This reverts commit 58b73bfd6afd733d20e6d12107c53e3c7516790a.
    
    Reason for revert: <This cl causes a regression= - [Mac]:Task/Subtask Completion or Deletion Causes Blank Space and Disables Task Actions>
    
    Failure Link: <https://issues.chromium.org/529697104>
    
    Original change's description:
    > mac: Fix a crash in ui::CARendererLayerTree::RootLayer::~RootLayer
    >
    > This CL fixes duplicate CALayer removeFromSuperlayer calls.
    >
    > During fallback matching (e.g. inside CALayerFallBack() loops),
    > unmatched clipping, transform, and content layers have their
    > removeFromSuperlayer called to remove them from the active layer tree.
    > However, their strong pointers (like clipping_ca_layer_ or ca_layer_)
    > were not cleared.
    >
    > This caused duplicate removeFromSuperlayer calls on the same CALayer
    > during old tree destruction (in their respective destructors), which can
    > cause a crash in CA::Layer::remove_from_superlayer() on certain macOS
    > versions.
    >
    > This CL prevents the duplicate calls by setting the strong layer
    > pointers to nil immediately after calling removeFromSuperlayer during
    > fallback matching, as sending removeFromSuperlayer to nil in the
    > destructors is a safe no-op.
    >
    > Bug: 527622652
    > Test: ca_layer_tree_unittest_mac.mm
    > Change-Id: Ic42d38886e011cee8e314d197a514e566f69b30e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7998997
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Auto-Submit: Maggie Chen <magchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1654089}
    
    Bug: 527622652,529697104
    Bug: 527622652
    Change-Id: I69a88d783a57187647c0f9c8ae7550dd15885d08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8026009
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654835}

--- ui/accelerated_widget_mac/ca_renderer_layer_tree.mm
+++ ui/accelerated_widget_mac/ca_renderer_layer_tree.mm
@@ -482,9 +482,6 @@ void CARendererLayerTree::RootLayer::CALayerFallBack() {
             break;
           }
           [old_layer_child->clipping_ca_layer_ removeFromSuperlayer];
-          [old_layer_child->rounded_corner_ca_layer_ removeFromSuperlayer];
-          old_layer_child->clipping_ca_layer_ = nil;
-          old_layer_child->rounded_corner_ca_layer_ = nil;
           ++old_layer_child_it;
         }
       } else {
@@ -524,7 +521,6 @@ void CARendererLayerTree::ClipAndSortingLayer::CALayerFallBack() {
             break;
           }
           [old_layer_child->ca_layer_ removeFromSuperlayer];
-          old_layer_child->ca_layer_ = nil;
           ++old_layer_child_it;
         }
       } else {
@@ -564,9 +560,6 @@ void CARendererLayerTree::TransformLayer::CALayerFallBack() {
             break;
           }
           [old_layer_child->ca_layer_ removeFromSuperlayer];
-          [old_layer_child->update_indicator_layer_ removeFromSuperlayer];
-          old_layer_child->ca_layer_ = nil;
-          old_layer_child->update_indicator_layer_ = nil;
           ++old_layer_child_it;
         }
       } else {
