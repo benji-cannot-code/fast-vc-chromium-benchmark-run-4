f3d194764cbbc02bc7e454215147e0f89e1f2581
Mason Freed <masonf@chromium.org>
2026-06-30 08:22:15 -0700

    Unbounded element: Bypass user activation in privileged contexts
    Bypass the transient user activation requirement for the unbounded
    element API (showUnboundedElement) when called from privileged contexts
    like WebUI (chrome:// or chrome-untrusted://).
    
    Fixed: 529023345
    Change-Id: I132b577543cff1cf241728a1e35d6e3bfbbbca3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8021363
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654766}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -11292,12 +11292,6 @@ void RenderFrameHostImpl::RequestUnboundedSurface(
     }
     return;
   }
-  if (!HasTransientUserActivation()) {
-    local_frame_host_receiver_.ReportBadMessage(
-        "RequestUnboundedSurface should not be called without user "
-        "activation.");
-    return;
-  }
   // If you change the preconditions/permissions for unbounded elements, be sure
   // to update the corresponding Blink-side checks in
   // HTMLElement::showUnboundedElement.
@@ -11305,6 +11299,12 @@ void RenderFrameHostImpl::RequestUnboundedSurface(
   // scheme callers.
   bool is_privileged = GetWebUI() != nullptr ||
                        GetLastCommittedOrigin().scheme() == kChromeUIScheme;
+  if (!is_privileged && !HasTransientUserActivation()) {
+    local_frame_host_receiver_.ReportBadMessage(
+        "RequestUnboundedSurface should not be called without user "
+        "activation.");
+    return;
+  }
   if (!is_privileged && !base::FeatureList::IsEnabled(
                             blink::features::kUnboundedElementOnTheOpenWeb)) {
     local_frame_host_receiver_.ReportBadMessage(
