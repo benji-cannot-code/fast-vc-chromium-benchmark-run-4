077b90482fb9b25c2c47d1fa0d55de98a63b74bb
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-06-30 14:14:31 -0700

    gardener: Disable OmniboxPopupSearchboxTest.HandlesManualBlur
    The test has been flaky since it was introduced in
    https://crrev.com/c/7980271
    
    Also disables SuppressesSelectionChangedDuringComposition which became
    flaky when the above CL landed:
    https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Ftest%5C%3Abrowser_tests!gtest%3A%3AOmniboxPopupTest%23Searchbox__OmniboxPopupSearchboxTest_SuppressesSelectionChangedDuringComposition
    
    Bug: 529516876
    Change-Id: I27b7b7ad3e2fd4ef0e06a122f78eaccb80976a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025479
    Owners-Override: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Paula Gómez <paugomez@google.com>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Auto-Submit: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Paula Gómez <paugomez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655037}

--- chrome/test/data/webui/omnibox_popup/omnibox_popup_searchbox_test.ts
+++ chrome/test/data/webui/omnibox_popup/omnibox_popup_searchbox_test.ts
@@ -197,7 +197,8 @@ suite('OmniboxPopupSearchboxTest', function() {
     assertFalse(searchbox.dropdownIsVisible);
   });
 
-  test('SuppressesSelectionChangedDuringComposition', async () => {
+  // TODO(crbug.com/529516876): Fix and re-enable
+  test.skip('SuppressesSelectionChangedDuringComposition', async () => {
     // Focus the input so it's the active element.
     const input = searchbox.$.input.inputElement;
     input.focus();
@@ -295,7 +296,8 @@ suite('OmniboxPopupSearchboxTest', function() {
     assertFalse(searchbox.$.input === searchbox.shadowRoot.activeElement);
   });
 
-  test('HandlesManualBlur', async () => {
+  // TODO(crbug.com/529516876): Fix and re-enable
+  test.skip('HandlesManualBlur', async () => {
     // Test default sequence number (0) when no state is set.
     const input = searchbox.$.input.inputElement;
     input.focus();
