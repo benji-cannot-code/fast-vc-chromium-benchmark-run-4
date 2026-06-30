8ceee9367c6138112e08f2c2b747ec24a222c108
Alex Ilin <alexilin@chromium.org>
2026-06-30 14:46:46 -0700

    [DBSC] Fix a dangling reference bug in UnexportableKeyServiceProxyImpl
    Callback parameters captured by `std::ref()` trigger dangling reference
    detection on invocation even if a parameter remains unused.
    
    To bypass this problem, this CL binds completion callbacks to a proxy
    class instance weak pointer. This ensures that the completion callback
    is only invoked when the proxy instance is still alive. Per the class
    contract, this also means that the unexportable key service instance is
    also alive.
    
    The callback invocation after the proxy is destroyed has no effect
    anyways because the Mojo pipe gets disconnected after `receiver_` member
    is destroyed.
    
    Fixed: 529685625
    Change-Id: I6496ee73a0a25ba0849a1e853bb3f5983523cace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8019132
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655071}

--- components/unexportable_keys/mojom/unexportable_key_service_proxy_impl.h
+++ components/unexportable_keys/mojom/unexportable_key_service_proxy_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/raw_ref.h"
+#include "base/memory/weak_ptr.h"
 #include "components/unexportable_keys/background_task_priority.h"
 #include "components/unexportable_keys/mojom/unexportable_key_service.mojom.h"
 #include "components/unexportable_keys/unexportable_key_service.h"
@@ -86,12 +87,29 @@ class UnexportableKeyServiceProxyImpl : public mojom::UnexportableKeyService {
   void DeleteAllKeys(DeleteAllKeysCallback result) override;
 
  private:
+  using NewSigningKeyCallback =
+      base::OnceCallback<void(ServiceErrorOr<mojom::NewSigningKeyDataPtr>)>;
+  using NewAttestationKeyCallback =
+      base::OnceCallback<void(ServiceErrorOr<mojom::NewAttestationKeyDataPtr>)>;
+
+  void OnSigningKeyCreated(
+      NewSigningKeyCallback callback,
+      ServiceErrorOr<UnexportableSigningKeyId> error_or_key_id);
+  void OnAttestationKeyCreated(
+      NewAttestationKeyCallback callback,
+      ServiceErrorOr<UnexportableAttestationKeyId> error_or_key_id);
+  void OnGetAllKeysForGarbageCollection(
+      GetAllKeysForGarbageCollectionCallback callback,
+      ServiceErrorOr<std::vector<UnexportableKeyId>> error_or_key_ids);
+
   mojo::Receiver<mojom::UnexportableKeyService> receiver_{this};
   // The underlying UnexportableKeyService instance. Not owned.
   // This pointer must remain valid for the entire lifetime of the
   // UnexportableKeyServiceProxyImpl object.
   const raw_ref<unexportable_keys::UnexportableKeyService>
       unexportable_key_service_;
+
+  base::WeakPtrFactory<UnexportableKeyServiceProxyImpl> weak_ptr_factory_{this};
 };
 
 }  // namespace unexportable_keys
