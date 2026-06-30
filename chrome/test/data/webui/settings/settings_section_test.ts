6fa82557b5cd8d49c290d440dd3b4ed0ef4d3b46
dpapad <dpapad@chromium.org>
2026-06-29 17:01:13 -0700

    Settings Lit: Migrate settings-section.
    Bug: 393471368
    Change-Id: I35acabd0302171a84126299090667a0c80059594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008431
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654477}

--- chrome/test/data/webui/settings/settings_section_test.ts
+++ chrome/test/data/webui/settings/settings_section_test.ts
@@ -5,11 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://settings/settings.js';
 
-import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import type {CrIconButtonElement} from 'chrome://settings/lazy_load.js';
 import type {SettingsSectionElement} from 'chrome://settings/settings.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {eventToPromise} from 'chrome://webui-test/test_util.js';
+import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 suite('SettingsSection', function() {
   let settingsSection: SettingsSectionElement;
@@ -18,19 +17,18 @@ suite('SettingsSection', function() {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     settingsSection = document.createElement('settings-section');
     document.body.appendChild(settingsSection);
-    flush();
   });
 
   test('Send feedback bubble', async function() {
     let feedback: CrIconButtonElement|null =
-        settingsSection.shadowRoot!.querySelector('#feedback');
+        settingsSection.shadowRoot.querySelector('#feedback');
     assertFalse(!!feedback);
 
     settingsSection.showSendFeedbackButton = true;
     const feedbackClicked = eventToPromise('send-feedback', settingsSection);
-    flush();
+    await microtasksFinished();
 
-    feedback = settingsSection.shadowRoot!.querySelector('#feedback');
+    feedback = settingsSection.shadowRoot.querySelector('#feedback');
     assertTrue(!!feedback);
     feedback.click();
     await feedbackClicked;
--- chrome/test/data/webui/settings/system_page_official_test.ts
+++ chrome/test/data/webui/settings/system_page_official_test.ts
@@ -144,7 +144,7 @@ suite('settings system page official', function() {
     assertTrue(!!section);
 
     const feedbackButton =
-        section.shadowRoot!.querySelector<HTMLElement>('#feedback');
+        section.shadowRoot.querySelector<HTMLElement>('#feedback');
     assertTrue(!!feedbackButton);
 
     feedbackButton.click();
