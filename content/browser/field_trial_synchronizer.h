d46985c577488d34f88658614d56516623bbef28
Luc Nguyen <lucnguyen@google.com>
2026-06-30 11:43:25 -0700

    Make PersistentSystemProfile aware of runtime overrides
    Context:
    
    We store system_profiles in PMA files through the
    PersistentSystemProfile class and that is what is used when creating UMA
    logs from PMA files. It contains the latest "full" system profile
    created from that session (i.e. the last time a log was created).
    However, logs aren't created very often, so it's very likely there was
    more field trials activated since the last time a system_profile was
    stored. So if there is a crash before a new system_profile is stored,
    the trials activated since the last log was created would be lost. To
    remedy this, PersistentSystemProfile will "amend" the stored
    system_profile as trials get activated. This CL updates that mechanism
    to also take into account runtime FieldTrial overrides.
    
    Bug: 482449878
    Change-Id: Ifc24fb62cf83e354a0e3644aff200bd5569bd896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7970492
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654917}

--- content/browser/field_trial_synchronizer.h
+++ content/browser/field_trial_synchronizer.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/metrics/field_trial.h"
+#include "base/metrics/runtime_field_trial_overrides.h"
 #include "components/variations/variations_ids_provider.h"
+#include "content/common/content_export.h"
 
 namespace content {
 class RenderProcessHost;
@@ -30,9 +32,10 @@ class RenderProcessHost;
 //
 // This class also registers itself as a VariationsIdsProvider Observer and
 // updates the renderers if the variations header changes.
-class FieldTrialSynchronizer
+class CONTENT_EXPORT FieldTrialSynchronizer
     : public base::FieldTrialList::Observer,
-      public variations::VariationsIdsProvider::Observer {
+      public variations::VariationsIdsProvider::Observer,
+      public base::RuntimeFieldTrialOverrides::Observer {
  public:
   // Creates the global FieldTrialSynchronizer instance for this process. After
   // this is invoked, renderers are notified whenever a field trial group is
@@ -53,6 +56,12 @@ class FieldTrialSynchronizer
 
   // VariationsIdsProvider::Observer methods:
   void VariationIdsHeaderUpdated() override;
+
+  // RuntimeFieldTrialOverrides::Observer methods:
+  void OnRuntimeFieldTrialOverride(
+      const base::RuntimeFieldTrialOverrides::RuntimeOverrideInfo&
+          override_info,
+      std::string_view previous_override_trial_name) override;
 
   // Sends the current variations header to |host|'s renderer.
   static void UpdateRendererVariationsHeader(RenderProcessHost* host);
