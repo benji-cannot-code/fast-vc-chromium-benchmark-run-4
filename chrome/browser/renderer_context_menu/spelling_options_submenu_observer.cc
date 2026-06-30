e03b66af77c7807fdcf82da064d80a8b25b58943
Kunal Daftari <kunaldaftari@google.com>
2026-06-30 11:14:47 -0700

    Revert "[BAM] Delete menu container command IDs and centralize placeholders"
    This reverts commit 26dd5417be41976aab3cd01fff505a6e893bc1f6.
    
    Reason for revert: Causing multiple regressions
    
    Failure Link: https://g-issues.chromium.org/issues/528705330
    
    Original change's description:
    > [BAM] Delete menu container command IDs and centralize placeholders
    >
    > Purge non-actionable wrapper and container command IDs (IDC_EDIT_MENU,
    > IDC_ZOOM_MENU, IDC_PASSWORDS_AND_AUTOFILL_MENU, IDC_FIND_AND_EDIT_MENU,
    > IDC_SAVE_AND_SHARE_MENU, IDC_RECENT_TABS_MENU, IDC_SHARING_HUB_MENU,
    > IDC_PROFILE_MENU_IN_APP_MENU, IDC_READING_LIST_MENU,
    > IDC_EXTENSIONS_SUBMENU, IDC_BOOKMARKS_MENU, IDC_SAVED_TAB_GROUPS_MENU,
    > IDC_HELP_MENU, IDC_MORE_TOOLS_MENU, IDC_SPELLCHECK_MENU,
    > IDC_WRITING_DIRECTION_MENU, IDC_INPUT_METHODS_MENU, IDC_FIND_MENU,
    > IDC_DEVELOPER_MENU, IDC_VIEW_MENU, IDC_FILE_MENU, IDC_CHROME_MENU,
    > IDC_HISTORY_MENU, IDC_TAB_MENU, IDC_PROFILE_MAIN_MENU) from
    > chrome_command_ids.h, chrome_action_id.h, and BrowserCommandController.
    >
    > Centralize all 25 negative container placeholder command IDs in a
    > single, dedicated section at the end of chrome_command_ids.h. Menu
    > models and native macOS/Views UI builders now infer container layout
    > internally using these centralized placeholder IDs, preventing
    > collisions and decoupling menu structure from executable browser
    > actions.
    >
    > BUG=525118424
    > TAG=agy
    > CONV=f7319e29-d4e9-468e-a302-4dfb0081e23f
    > NO_IFTTT=Replaced name of commands, no functional changes are made
    >
    > Change-Id: I08d3bcd7a8fcaba9893ebdd44a3ac0e684a17dc0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7988492
    > Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    > Commit-Queue: Darryl James <dljames@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1653261}
    
    Bug: 525118424
    Change-Id: I79b7f2e320dd1538ed641925cef1864965150087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8026231
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654897}

--- chrome/browser/renderer_context_menu/spelling_options_submenu_observer.cc
+++ chrome/browser/renderer_context_menu/spelling_options_submenu_observer.cc
@@ -96,7 +96,7 @@ void SpellingOptionsSubMenuObserver::InitMenu(
   }
 
   proxy_->AddSubMenu(
-      kSubMenuPlaceholder,
+      IDC_SPELLCHECK_MENU,
       l10n_util::GetStringUTF16(IDS_CONTENT_CONTEXT_SPELLCHECK_MENU),
       &submenu_model_);
 }
@@ -112,7 +112,7 @@ bool SpellingOptionsSubMenuObserver::IsCommandIdSupported(int command_id) {
 
   switch (command_id) {
     case IDC_CHECK_SPELLING_WHILE_TYPING:
-    case kSubMenuPlaceholder:
+    case IDC_SPELLCHECK_MENU:
     case IDC_SPELLCHECK_MULTI_LINGUAL:
       return true;
   }
@@ -165,7 +165,7 @@ bool SpellingOptionsSubMenuObserver::IsCommandIdEnabled(int command_id) {
       return !pref->FindPreference(spellcheck::prefs::kSpellCheckEnable)
                   ->IsManaged();
 
-    case kSubMenuPlaceholder:
+    case IDC_SPELLCHECK_MENU:
       return true;
   }
 
