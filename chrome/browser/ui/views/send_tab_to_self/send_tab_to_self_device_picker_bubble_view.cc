8558ec327988b237a0079e5d917456801074084f
Antonio Gomes <tonikitoo@igalia.com>
2026-06-29 21:48:26 -0700

    [bedrock] Drop stale browser.h/browser_view.h in c/b/ui/views/send_tab_to_self/
    send_tab_to_self_bubble_controller.cc no longer uses Browser or
    BrowserWindow (it operates on a BrowserWindowInterface obtained from
    GlobalBrowserCollection), and
    send_tab_to_self_device_picker_bubble_view.cc does not use BrowserView.
    This CL removes the now-unused browser.h, browser_window.h and
    views/frame/browser_view.h includes.
    
    With those gone,
    //c/b/ui/views/send_tab_to_self:impl no longer includes any
    //c/b/ui:ui headers, so drop its allow_circular_includes_from
    entry (and its TODO) from chrome/browser/ui/BUILD.gn.
    The target remains a normal dep of the ui target.
    
    R=chromium-bedrock-reviews@google.com
    
    Bug: 353332589
    Change-Id: Ib15e54f6e976ecbb9db4a143d5206bf6a61fb5e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8020324
    Commit-Queue: Antonio Gomes Netto <tonikitoo@igalia.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654573}

--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_device_picker_bubble_view.cc
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_device_picker_bubble_view.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/views/send_tab_to_self/manage_account_devices_link_view.h"
 #include "chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_bubble_controller.h"
