39070a883cffe352329331c6123c21cf9b3fb230
Colin Blundell <blundell@chromium.org>
2026-06-30 00:30:47 -0700

    [Blink] Inline C2DBitmapProvider::Snapshot() call in UnacceleratedSBI
    Next step on eliminating this class' usage of Canvas2DBitmapProvider.
    Code being inlined is here [1]. Note that we can hardcode values for
    various params as the C2DBitmapProvider instance has just been
    constructed here. Also note that the default value of orientation to
    both the Snapshot() method and the UnacceleratedSBI::Create() method
    being called here is kDefault.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=316-339;drc=d6cb0bdef0e0ab705ff1c4a2380e3641253de3a5
    
    Bug: 352263194
    Change-Id: I1c3993a92cda01d9c8dbb1302e3c67751241202a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010850
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654611}

--- third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/canvas_2d_bitmap_provider.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
+#include "third_party/blink/renderer/platform/graphics/paint/paint_image.h"
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_wrapper.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/cross_thread_copier_skia.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/skia/include/core/SkImage.h"
+#include "third_party/skia/include/core/SkSurface.h"
 
 namespace blink {
 
@@ -67,7 +69,18 @@ UnacceleratedStaticBitmapImage::CreateFromRaster(
       gfx::ColorSpace::CreateSRGB());
   bitmap_provider->SetAnimatedImageFrameIndexes(animated_image_frame_index_map);
   draw_callback(bitmap_provider->Canvas());
-  return bitmap_provider->Snapshot();
+  sk_sp<SkImage> sk_image =
+      bitmap_provider->GetSkSurface()->makeImageSnapshot();
+  cc::PaintImage paint_image;
+  if (sk_image) {
+    paint_image =
+        PaintImageBuilder::WithDefault()
+            .set_id(cc::PaintImage::GetNextId())
+            .set_image(std::move(sk_image), cc::PaintImage::GetNextContentId())
+            .set_hdr_metadata(bitmap_provider->GetHdrMetadata())
+            .TakePaintImage();
+  }
+  return UnacceleratedStaticBitmapImage::Create(std::move(paint_image));
 }
 
 UnacceleratedStaticBitmapImage::UnacceleratedStaticBitmapImage(
