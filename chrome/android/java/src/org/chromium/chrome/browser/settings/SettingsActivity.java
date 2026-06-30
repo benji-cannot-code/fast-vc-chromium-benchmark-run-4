4e65636a1999e9ca78fb03526a3555be142df402
James Cook <jamescook@chromium.org>
2026-06-30 13:10:36 -0700

    settings: Remove @NonNull from SettingsActivity.java
    The class is already @NullMarked, so objects can be assumed to be non-
    null. This eliminates some presubmit warnings.
    
    Bug: 521895796
    Change-Id: Ie49fd7dafac6164ddebe3848eabdad34b2c07be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8026990
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654988}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -27,7 +27,6 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -428,7 +427,7 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
     }
 
     @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         if (mMultiColumnSettings != null) {
             for (Fragment fragment :
@@ -464,9 +463,9 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
 
                             @Override
                             public void onFragmentViewCreated(
-                                    @NonNull FragmentManager fm,
-                                    @NonNull Fragment f,
-                                    @NonNull View v,
+                                    FragmentManager fm,
+                                    Fragment f,
+                                    View v,
                                     @Nullable Bundle savedFragmentState) {
                                 assert mMultiColumnSettings != null;
 
