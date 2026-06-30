af91e6d1a553f1ed878e6e1bb5ab2efad2c0534e
Sam Maier <smaier@google.com>
2026-06-30 14:18:26 -0700

    Reland "Launch Javaless Renderers"
    This reverts commit 8e4b24fdb1b496a2a0e980eea515db80e6c9ddb5.
    
    Reason for revert: See parent change for most fixes
    
    Webview fixed by always deferring to sForceNativeSandboxedService if
    set, which is only ever set by Webview.
    
    Original change's description:
    > Revert "Launch Javaless Renderers"
    >
    > This reverts commit b1f387ec68ecb5158328d338f80d4833edea0a35.
    >
    > Reason for revert: Suspected to be causing many java crashes on internal android-desktop tests
    >
    > See also b/522703237.
    >
    > Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-17-x64-rel-emu-tests/37/infra
    >
    > Original change's description:
    > > Launch Javaless Renderers
    > >
    > > Turns on the experiment by default, while still leaving one CachedFlag
    > > around to experiment with in the future, as we plan to run a holdback
    > > once Android 17 is generally available.
    > >
    > > Bug: 475852776
    > > Change-Id: I701ac2f2a0119ea541225c38dc7cebaa6a6a6964
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7917046
    > > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > > Commit-Queue: Sam Maier <smaier@google.com>
    > > Owners-Override: Sam Maier <smaier@google.com>
    > > Auto-Submit: Sam Maier <smaier@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1644723}
    >
    > Bug: 475852776, 522703237
    > Bug: 475852776
    > Change-Id: Ideba54dc95fa2666b7640f906043399db839cf7c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926480
    > Commit-Queue: Ben Pastene <bpastene@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Ben Pastene <bpastene@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1645583}
    
    Bug: 475852776, 522703237, 522844912, 522822046, 529318456
    Change-Id: Iadb4f3e3c748c99655e763a48b630dcc8814d96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963240
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Sam Maier <smaier@google.com>
    Owners-Override: Sam Maier <smaier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655040}

--- chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
@@ -70,7 +70,6 @@ public class ChromeCachedFlags {
     static final List<List<CachedFeatureParam<?>>> LISTS_OF_FEATURE_PARAMS_FULL_BROWSER =
             List.of(
                     ChromeFeatureList.sParamsCached,
-                    JavalessRenderersFeatureList.sParamsCached,
                     OmniboxFeatures.getFeatureParamsToCache(),
                     PermissionsAndroidFeatureList.getFeatureParamsToCache(),
                     UiAndroidFeatureList.sParamsCached);
@@ -85,7 +84,6 @@ public class ChromeCachedFlags {
     static final List<List<CachedFeatureParam<?>>> LISTS_OF_FEATURE_PARAMS =
             List.of(
                     ChromeFeatureList.sParamsCached,
-                    JavalessRenderersFeatureList.sParamsCached,
                     OmniboxFeatures.getFeatureParamsToCache(),
                     PermissionsAndroidFeatureList.getFeatureParamsToCache(),
                     UiAndroidFeatureList.sParamsCached);
