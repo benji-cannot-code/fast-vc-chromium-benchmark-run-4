590cd275913022f4e5c6bc4453ee7ccf68cf8859
Julia Hansbrough <flowerhack@google.com>
2026-06-30 14:13:21 -0700

    [rustmojo] Update Rust mojom parser to handle duplicate discriminants in enums.
    A reasonably common pattern in Mojo enums are things like
    
    enum ConnectionType {
      CONNECTION_FOO = 0,
      CONNECTION_BAR = 1,
      CONNECTION_LAST = CONNECTION_BAR,
    }
    
    This works fine when compiled to the C++ bindings.  However, Rust enums
    do not allow duplicate discriminant values, so CONNECTION_LAST can't
    equal 1.
    
    At a high level there's two ways to resolve this:
    
    (1) During preprocessing, track *all* seen values, but only use the
    *first* variant in the actual `pub enum` on the Rust side. Then, for all
    duplicates, add an `impl` block with `pub const CONNECTION_LAST` (or
    similar) onto that enum.
    
    (2) Don't use a native rust enum at all. Instead, make a "wrapper"
    struct that has a `pub const` for each individual value.
    
    I think (1) is preferable (and e.g. it's what
    https://github.com/tokio-rs/prost seems to do), but I'm open to
    alternative suggestions.
    
    Bug: 527642290
    Change-Id: I3fe7502688706b3afe0efff7a6196fa452d77a7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8019896
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655035}

--- mojo/public/rust/mojom_value_parser/test/test_mojomparse.rs
+++ mojo/public/rust/mojom_value_parser/test/test_mojomparse.rs
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 chromium::import! {
     "//mojo/public/rust/mojom_value_parser:mojom_value_parser_core";
     "//mojo/public/rust/mojom_value_parser:parser_unittests_rust";
+    "//mojo/public/rust/mojom_value_parser:dup_enum_unittest_rust";
     "//mojo/public/rust/bindings";
 }
 
@@ -2803,4 +2804,15 @@ fn test_pending_associated_types() {
                 && parsed.rem2.as_ref().unwrap().same_interface_for_testing(&rec_f)
         },
     );
+}
+
+#[gtest(MojomParseTest, DuplicateEnumValues)]
+fn test_duplicate_enum_values() {
+    use dup_enum_unittest_rust::dup_enum_unittest::DupEnum;
+    assert_eq!(DupEnum::kFoo as i32, 0);
+    assert_eq!(DupEnum::kBar as i32, 1);
+    assert_eq!(DupEnum::kLast as i32, 1);
+    assert_eq!(DupEnum::kBar, DupEnum::kLast);
+    assert_eq!(DupEnum::try_from(1).unwrap(), DupEnum::kBar);
+    assert_eq!(DupEnum::try_from(1).unwrap(), DupEnum::kLast);
 }
