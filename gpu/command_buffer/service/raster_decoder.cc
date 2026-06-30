a4b1e8c7352ddaea85340fd02b7fc555a0e08aa9
Saifuddin Hitawala <hitawala@chromium.org>
2026-06-30 12:54:30 -0700

    [gpu] Cleanup kNvidiaWaylandYuvHardwareConversionWorkaround feature
    This feature was launched in M148 which has been on stable for
    quite some time. This change removes the feature and cleans up code.
    
    Bug: 447709687, 498266854, 429585233
    Change-Id: Ieee8e8d7968d5c64f4da2c307210fe71cfde3d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8026790
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654976}

--- gpu/command_buffer/service/raster_decoder.cc
+++ gpu/command_buffer/service/raster_decoder.cc
@@ -1227,9 +1227,7 @@ Capabilities RasterDecoderImpl::GetCapabilities() {
     caps.supports_yuv_readback = true;
   }
 
-  if (base::FeatureList::IsEnabled(
-          features::kNvidiaWaylandYuvHardwareConversionWorkaround) &&
-      feature_info()->workarounds().disable_rgb_to_yuv_conversion) {
+  if (feature_info()->workarounds().disable_rgb_to_yuv_conversion) {
     caps.supports_rgb_to_yuv_conversion = false;
   }
 
