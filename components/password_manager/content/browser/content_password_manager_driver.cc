94017806c25f8491375a76b0be51150416175e6d
Timofey Chudakov <tchudakov@google.com>
2026-06-30 03:10:36 -0700

    [PWM] Remove default parameter from pwm. driver HasValidUrl.
    This CL is a follow up for crrev.com/c/8010689. It removes the
    default parameter from PasswordManagerDriver::HasValidUrl to
    adhere to the style guide.
    
    Bug: n/a
    Change-Id: I5bf0ab721b84091da4e144faeb476f14027b6e7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8017224
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654688}

--- components/password_manager/content/browser/content_password_manager_driver.cc
+++ components/password_manager/content/browser/content_password_manager_driver.cc
@@ -249,7 +249,7 @@ void ContentPasswordManagerDriver::GeneratedPasswordAccepted(
   // operation with generated URL, don't forward anything to password manager.
   // TODO(crbug.com/40191770): Test that PasswordManager doesn't receive url
   // and full_url from renderer.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
 
@@ -506,7 +506,7 @@ void ContentPasswordManagerDriver::PasswordFormsParsed(
 
   // In case we can't obtain a valid URL or a frame isn't allowed to perform an
   // operation with generated URL, don't forward anything to password manager.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
 
@@ -535,7 +535,7 @@ void ContentPasswordManagerDriver::PasswordFormsRendered(
 
   // In case we can't obtain a valid URL or a frame isn't allowed to perform an
   // operation with generated URL, don't forward anything to password manager.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
 
@@ -556,7 +556,7 @@ void ContentPasswordManagerDriver::PasswordFormSubmitted(
 
   // In case we can't obtain a valid URL or a frame isn't allowed to perform an
   // operation with generated URL, don't forward anything to password manager.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
 
@@ -576,7 +576,7 @@ void ContentPasswordManagerDriver::InformAboutUserInput(
   // operation with generated URL, don't forward anything to password manager.
   // TODO(crbug.com/40191770): Test that PasswordManager doesn't receive url
   // and full_url from renderer.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
 
@@ -619,7 +619,7 @@ void ContentPasswordManagerDriver::PasswordFormCleared(
 
   // In case we can't obtain a valid URL or a frame isn't allowed to perform an
   // operation with generated URL, don't forward anything to password manager.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
 
@@ -742,7 +742,7 @@ void ContentPasswordManagerDriver::OnChangePasswordFormFilled(
 
   // In case we can't obtain a valid URL or a frame isn't allowed to perform an
   // operation with generated URL, don't forward anything to password manager.
-  if (!HasValidURL()) {
+  if (!HasValidURL(/*may_kill_renderer=*/true)) {
     return;
   }
   std::move(form_data_callback)
