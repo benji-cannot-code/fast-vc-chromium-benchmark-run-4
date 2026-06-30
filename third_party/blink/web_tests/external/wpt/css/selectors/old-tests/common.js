25951132a4e731dcbee7ccbfb8a7c34e208a4939
Kent Tamura <tkent@chromium.org>
2026-06-30 15:21:03 -0700

    css/selectors: Automate a part of legacy selectors tests (120 to 139)
    Change-Id: I1c7bbb9a3287c068c6cd711e481f137d2914db1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8022122
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655098}

--- third_party/blink/web_tests/external/wpt/css/selectors/old-tests/common.js
+++ third_party/blink/web_tests/external/wpt/css/selectors/old-tests/common.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const LIME = 'rgb(0, 255, 0)';
+const GREEN = 'rgb(0, 128, 0)';
 const RED = 'rgb(255, 0, 0)';
 
 function assert_background_color(elementOrId, colorString, comment) {
