2eb674b7df3c1085e22f0bb8890eb56f98e846de
Mustaq Ahmed <mustaq@google.com>
2026-06-30 12:48:19 -0700

    [CodeHealth] Update event dispatch steps in/around PageAnimator.
    This CL makes a few cleanups to prepare for updates to pre-dispatch
    event filtering to fix the Interop bug:
    - Remove PageRevealEvent from runtime_enabled_features.json5, which
      shipped a while ago.
    - Update event loop step comments in PageAnimator to match HTML spec.
    - Update PageRevealEvent spec link to the HTML Living Standard and
      remove the TODO.
    
    TAG=agy
    CONV=f57d6ee2-dc21-48f0-b1ac-ae29e9c16df4
    
    Bug: 397737222
    Change-Id: Ie7c947ce45439b3bf1e0595758dc9df2205b84bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8020242
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654966}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -10051,7 +10051,6 @@ void Document::ResetAgent(Agent& agent) {
 }
 
 void Document::EnqueuePageRevealEvent() {
-  CHECK(RuntimeEnabledFeatures::PageRevealEventEnabled());
   CHECK(dom_window_);
 
   dom_window_->SetHasBeenRevealed(false);
--- third_party/blink/renderer/core/view_transition/page_reveal_event.cc
+++ third_party/blink/renderer/core/view_transition/page_reveal_event.cc
@@ -11,13 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/view_transition/dom_view_transition.h"
 #include "third_party/blink/renderer/core/view_transition/view_transition_utils.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
 PageRevealEvent::PageRevealEvent()
     : Event(event_type_names::kPagereveal, Bubbles::kNo, Cancelable::kNo) {
-  CHECK(RuntimeEnabledFeatures::PageRevealEventEnabled());
 }
 
 PageRevealEvent::PageRevealEvent(const AtomicString& type,
@@ -25,7 +23,6 @@ PageRevealEvent::PageRevealEvent(const AtomicString& type,
     : Event(type, initializer),
       dom_view_transition_(initializer ? initializer->viewTransition()
                                        : nullptr) {
-  CHECK(RuntimeEnabledFeatures::PageRevealEventEnabled());
 }
 
 PageRevealEvent::~PageRevealEvent() = default;
