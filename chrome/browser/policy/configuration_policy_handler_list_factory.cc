753da19cdd41731e169b9ffbdb4591b8a879d93a
William Liu <liuwilliam@chromium.org>
2026-06-30 11:15:22 -0700

    [indigo] Implement Indigo enterprise policy with browser tests
    Doc: http://docs/document/d/1ZzC-aqI2HR6L5Yvq1M0nVCH4N38V0qqQbtcFGAO9pLg?resourcekey=0-_dSzQj3zep9sul6_yne2ng&tab=t.0
    
    Bug: b:512247450, b:529737129
    
    Change-Id: I9c0875cd23ca07e204c8f3144d09b03d124f5fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8012669
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Auto-Submit: William Liu <liuwilliam@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654898}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -292,6 +292,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/content/common/file_type_policies_prefs.h"
 #endif
 
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/indigo/indigo_prefs.h"
+#endif
+
 namespace policy {
 namespace {
 
@@ -477,6 +481,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
 #if !BUILDFLAG(IS_ANDROID)
   { key::kVoiceTypingSettings,
     prefs::kVoiceTypingSettings,
+    base::Value::Type::INTEGER },
+  { key::kIndigo,
+    indigo::prefs::kIndigoPolicy,
     base::Value::Type::INTEGER },
 #endif
   { key::kAllowDeletingBrowserHistory,
@@ -3663,6 +3670,8 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
 #if !BUILDFLAG(IS_ANDROID)
   gen_ai_default_policies.emplace_back(key::kVoiceTypingSettings,
                                        prefs::kVoiceTypingSettings);
+  gen_ai_default_policies.emplace_back(key::kIndigo,
+                                       indigo::prefs::kIndigoPolicy);
 #endif
   // Default value for SearchContentSharingSettings is 0 if
   // GenAiDefaultSettings value is 0 or 1, or 1 if the latter is 2.
