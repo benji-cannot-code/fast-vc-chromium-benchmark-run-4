3685ab179340bc66e1b83e0c4a917d0fa98aa6bd
Michael Wojcicka <mwoj@google.com>
2026-06-30 12:06:00 -0700

    [Task Manager] Replace Accept() with SetAcceptCallbackWithClose()
    - Accept() is labeled as deprecated, so replace it with the modern
      equivalent, and close out a long standing TODO.
    
    Bug: 529425276
    Change-Id: I2c987c7295dff6873858f80516f31d8d4506a95e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8021526
    Reviewed-by: Elly <ellyjones@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654929}

--- chrome/browser/ui/views/task_manager_view.h
+++ chrome/browser/ui/views/task_manager_view.h
@@ -85,7 +85,6 @@ class TaskManagerView : public TableViewDelegate,
   bool ExecuteWindowsCommand(int command_id) override;
   ui::ImageModel GetWindowIcon() override;
   std::string GetWindowName() const override;
-  bool Accept() override;
   bool IsDialogButtonEnabled(ui::mojom::DialogButton button) const override;
   void WindowClosing() override;
 
@@ -147,6 +146,9 @@ class TaskManagerView : public TableViewDelegate,
 
   // Creates the child controls (header, table, etc).
   void Init();
+
+  // Called when the user presses the "End Task" button.
+  bool OnAccept();
 
   // Initializes the state of the always-on-top setting as the window is shown.
   void InitAlwaysOnTopState();
