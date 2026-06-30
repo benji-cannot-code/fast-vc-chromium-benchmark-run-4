22acb03a21ec661928db965881dbaed3bdf8907a
zijiehe-google-com <zijiehe@google.com>
2026-06-29 17:47:56 -0700

    [fuchsia] Disable ffx daemon start/stop in IsolateDaemon
    Cq-Include-Trybots: luci.chromium.try:fuchsia-arm64-cast-receiver-rel,fuchsia-fyi-x64-dbg-persistent-emulator;luci.chrome.try:fuchsia-fyi-astro-qemu,fuchsia-fyi-sherlock-qemu,fuchsia-webgl-astro-qemu,fuchsia-webgl-sherlock-qemu
    Change-Id: I33761f08a9cabe4c5771a6259cd43a88191c9fe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008815
    Auto-Submit: Zijie He <zijiehe@google.com>
    Reviewed-by: Guocheng Wei <guochengwei@chromium.org>
    Commit-Queue: Guocheng Wei <guochengwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654502}

--- build/fuchsia/test/isolate_daemon.py
+++ build/fuchsia/test/isolate_daemon.py
@@ -2,7 +2,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2023 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Sets up the isolate daemon environment to run test on the bots."""
+"""Sets up the isolated environment for running tests.
+
+Note: Although this file and class are named after "isolate daemon", it now
+runs ffx in daemonless mode. A follow-up cleanup is planned to rename and
+refactor this.
+"""
 
 import os
 import tempfile
@@ -12,14 +17,13 @@ from typing import Optional
 from contextlib import AbstractContextManager
 
 from common import get_ffx_isolate_dir,has_ffx_isolate_dir, \
-                        set_ffx_isolate_dir, is_daemon_running, \
-                        start_ffx_daemon, stop_ffx_daemon
+                        set_ffx_isolate_dir, is_daemon_running
 from ffx_integration import ScopedFfxConfig
 from modification_waiter import ModificationWaiter
 
 
 class IsolateDaemon(AbstractContextManager):
-    """Sets up the environment of an isolate ffx daemon."""
+    """Sets up the environment for ffx (currently running daemonless)."""
 
     class IsolateDir(AbstractContextManager):
         """Sets up the ffx isolate dir to a temporary folder if it's not set."""
@@ -93,16 +97,13 @@ class IsolateDaemon(AbstractContextManager):
 
     # Updating configurations to meet the requirement of isolate.
     def __enter__(self):
-        # This environment variable needs to be set before stopping ffx daemon
+        # This environment variable needs to be set before running ffx commands
         # to avoid sending unnecessary analytics.
         os.environ['FUCHSIA_ANALYTICS_DISABLED'] = '1'
-        stop_ffx_daemon()
         for init in self._inits:
             init.__enter__()
-        start_ffx_daemon()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         for init in self._inits:
             init.__exit__(exc_type, exc_value, traceback)
-        stop_ffx_daemon()
