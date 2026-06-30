5eb5bef2da18e303b60621585de3ba7dd8aebe03
Anton Bershanskyi <bershanskyi@gmail.com>
2026-06-30 11:13:29 -0700

    [Extensions] Remove handling for .user.js files
    Remove special handling for files ending in .user.js which were
    previously assumed to be User Scripts and automatically converted into
    Manifest Version 2 extensions. Since removal of Manifest version 2,
    these extensions failed to install and load producing an appearance of
    broken downloads. (The downloads would be intercepted, substituted with
    extensions, but the extensions would fail to load.)
    This change removes only special handling logic for downloads and
    drag and drops of .user.js files. It leaves handling of manifest key
    "converted_from_user_script" as is.
    
    Bug: None
    Change-Id: I91bc0849fd4fe0396468ab07e9501d6afa5f9c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7941590
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654895}

--- chrome/browser/download/download_target_determiner_unittest.cc
+++ chrome/browser/download/download_target_determiner_unittest.cc
@@ -1736,17 +1736,6 @@ TEST_F(DownloadTargetDeterminerTest, PromptAlways_NonTrustedExtension) {
        FILE_PATH_LITERAL("foo.crx"), DownloadItem::TARGET_DISPOSITION_PROMPT,
 
        EXPECT_CRDOWNLOAD},
-
-      {// 1: Automatic User Script - Shouldn't prompt for user script downloads
-       //    even if "Prompt for download" preference is set.
-       AUTOMATIC, download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS,
-       DownloadFileType::NOT_DANGEROUS, "http://example.com/foo.user.js", "",
-       FILE_PATH_LITERAL(""),
-
-       FILE_PATH_LITERAL("foo.user.js"),
-       DownloadItem::TARGET_DISPOSITION_PROMPT,
-
-       EXPECT_CRDOWNLOAD},
   };
 
   SetPromptForDownload(true);
@@ -1764,17 +1753,6 @@ TEST_F(DownloadTargetDeterminerTest, PromptAlways_TrustedExtension) {
        extensions::Extension::kMimeType, FILE_PATH_LITERAL(""),
 
        FILE_PATH_LITERAL("foo.crx"), DownloadItem::TARGET_DISPOSITION_OVERWRITE,
-
-       EXPECT_CRDOWNLOAD},
-
-      {// 1: Automatic User Script - Shouldn't prompt for user script downloads
-       //    even if "Prompt for download" preference is set.
-       AUTOMATIC, download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS,
-       DownloadFileType::NOT_DANGEROUS, "http://example.com/foo.user.js", "",
-       FILE_PATH_LITERAL(""),
-
-       FILE_PATH_LITERAL("foo.user.js"),
-       DownloadItem::TARGET_DISPOSITION_OVERWRITE,
 
        EXPECT_CRDOWNLOAD},
   };
