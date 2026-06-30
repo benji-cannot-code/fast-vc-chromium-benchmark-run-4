6515ce1bd7949f320c9595f8a7c052e9e29a6451
Ritika Gupta <ritikagup@google.com>
2026-06-30 16:45:23 -0700

    [Background Actuation] Add GlicBackgroundActuation feature flag.
    Add new feature flag for GlicBackgroundActuation in native
    (features::kGlicBackgroundActuation) and expose it to Java
    (ChromeFeatureList.GLIC_BACKGROUND_ACTUATION).
    
    Bug: 529905047
    Change-Id: I413ac688e3f8101c37458d14ea63f64987040953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027852
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655143}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -192,6 +192,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kFluidResize,
     &features::kGenericSensorExtraClasses,
     &features::kGlic,
+    &features::kGlicBackgroundActuation,
     &features::kHttpsFirstBalancedMode,
     &features::kLoadingPredictorLimitPreconnectSocketCount,
     &features::kNetworkServiceInProcess,
