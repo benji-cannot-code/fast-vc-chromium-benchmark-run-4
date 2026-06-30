168b7f1610e27678707b10797ceaa7ccc54f8264
Mason Freed <masonf@chromium.org>
2026-06-29 23:05:59 -0700

    Reject showUnboundedElement with SecurityError if not privileged
    When calling showUnboundedElement on the open web without the
    UnboundedElementOnTheOpenWeb feature flag enabled (but importantly
    *with* the UnboundedElement feature enabled, so the IDL shows up), the
    browser receives the IPC request and terminates the renderer process
    via a BadMessage report.
    
    This patch fixes this by checking the document's security origin on the
    Blink side first. This has to be duplicated between Blink and the
    browser, but I added comments linking the two.
    
    Fixed: 528276490
    Change-Id: I09c8eee45f4295224b2dfa61967b7e8b372c1b04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8013015
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1654589}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -148,6 +148,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 #include "third_party/blink/renderer/platform/text/bidi_paragraph.h"
+#include "third_party/blink/renderer/platform/weborigin/scheme_registry.h"
 #include "third_party/blink/renderer/platform/widget/frame_widget.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
@@ -1576,6 +1577,24 @@ ScriptPromise<IDLUndefined> HTMLElement::showUnboundedElement(
     resolver->Reject(MakeGarbageCollected<DOMException>(
         DOMExceptionCode::kInvalidStateError,
         "The element is not in a document with a valid frame."));
+    return promise;
+  }
+
+  // If you change the preconditions/permissions for unbounded elements, be sure
+  // to update the corresponding browser-side checks in
+  // RenderFrameHostImpl::RequestUnboundedSurface.
+  const SecurityOrigin* security_origin =
+      GetDocument().GetExecutionContext()->GetSecurityOrigin();
+  bool is_privileged =
+      security_origin &&
+      (security_origin->Protocol() == "chrome" ||
+       security_origin->Protocol() == "chrome-untrusted" ||
+       SchemeRegistry::IsWebUIScheme(security_origin->Protocol()));
+  if (!is_privileged &&
+      !RuntimeEnabledFeatures::UnboundedElementOnTheOpenWebEnabled()) {
+    resolver->Reject(MakeGarbageCollected<DOMException>(
+        DOMExceptionCode::kSecurityError,
+        "showUnboundedElement is only supported from privileged contexts."));
     return promise;
   }
 
