08b846fef4a7d049ab341f463f6484a16e7d27f6
Lijin Shen <lazzzis@google.com>
2026-06-30 14:42:10 -0700

    Remove flag TaskGetIdAnrFix
    Fixed: 518763461
    Change-Id: I8035eb32328b13b77755644f9045053d7f8e9695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025877
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655065}

--- chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskImpl.java
+++ chrome/browser/ui/browser_window/internal/android/java/src/org/chromium/chrome/browser/ui/browser_window/ChromeAndroidTaskImpl.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.browser_window;
 
+import static org.chromium.base.ApplicationStatus.getTaskId;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.role.RoleManager;
@@ -34,7 +36,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.TimeUtils;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher.ActivityState;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcherProvider;
@@ -467,15 +468,6 @@ final class ChromeAndroidTaskImpl
                                                     .getDisplay()));
                 }
             };
-
-    // TODO(https://crbug.com/518763461): remove flag once verified
-    private static int getTaskId(Activity activity) {
-        if (ChromeFeatureList.sTaskGetIdAnrFix.isEnabled()) {
-            return ApplicationStatus.getTaskId(activity);
-        } else {
-            return activity.getTaskId();
-        }
-    }
 
     private static Activity getActivity(ActivityWindowAndroid activityWindowAndroid) {
         Activity activity = activityWindowAndroid.getActivity().get();
