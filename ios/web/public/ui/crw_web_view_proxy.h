1ec524dfe5f2ce39ae2b434d251d308d07c8baa7
Quentin Pubert <qpubert@google.com>
2026-06-30 03:08:05 -0700

    [iOS] Add property to ignore `obscuredInsets` in Reader mode
    This CL adds an `ignoreObscuredInsets` BOOL property to the protocol
    `CRWWebViewProxy`, implemented by `CRWWebViewProxyImpl`.
    `CRWWebViewProxyImpl` returns early in `setObscuredInsets:` if
    `ignoreObscuredInsets` is set to `YES`. The property is initialized to
    `NO` by default, ensuring this change does not change the behavior in
    regular web views. It is only set to `YES` in the Reader mode web view
    before it is set to override the web view proxy in
    `WebViewProxyTabHelper`.
    
    This change is necessary to ensure obscured insets are never used in
    Reader mode web views since they are always constrained to the content
    area anyway.
    
    Bug: 525721380
    Change-Id: I356e53a511ff4e6548a14956105f31ce7434c23d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010522
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1654684}

--- ios/web/public/ui/crw_web_view_proxy.h
+++ ios/web/public/ui/crw_web_view_proxy.h
@@ -20,7 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The web view's frame rectangle.
 @property(readonly, assign) CGRect frame;
 
-// Web view's obscured insets.
+// Whether to ignore the value of `obscuredInsets`. If set to `YES` then
+// setting the `obscuredInsets` property will not have any effect.
+@property(nonatomic, assign) BOOL ignoreObscuredInsets;
+
+// Web view's obscured insets. If `ignoreObscuredInsets` is set to `YES` then
+// setting this property will not have any effect.
 @property(nonatomic, assign) UIEdgeInsets obscuredInsets;
 
 // Sets the web view's min and max viewport insets.
