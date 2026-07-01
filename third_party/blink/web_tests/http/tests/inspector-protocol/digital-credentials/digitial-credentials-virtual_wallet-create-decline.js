c3ef3345d4d915bde0225d8d028aadef27e8db44
Tejas Dhagawkar <tdhagawkar@microsoft.com>
2026-07-01 03:10:29 -0700

    [DC] Virtual Wallet Inspector Protocol tests
    This CL adds inspector-protocol web tests for Digital Credentials
    virtual wallet.
    
    Success flow: virtual wallet responds with a token.
    
    Decline flow: virtual wallet declines the request.
    
    Wait/abort flow: virtual wallet waits and the request is aborted with
    AbortController.
    
    Bug: 470685266
    Change-Id: Ib961e0ad69ac9b7d98414d3f1ba49c9cde026f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7913321
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Tejas Dhagawkar <tdhagawkar@microsoft.com>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655387}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/digital-credentials/digitial-credentials-virtual_wallet-create-decline.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      'Check that Digital Credentials API create call can be declined by virtual wallet');
+
+  await page.navigate(
+      'https://devtools.test:8443/inspector-protocol/digital-credentials/resources/digital-credentials-create.html');
+
+  await dp.DigitalCredentials.setVirtualWalletBehavior({
+    behavior: 'decline',
+  });
+
+  const result = await session.evaluateAsyncWithUserGesture(`createDigitalCredential({
+      protocol: 'openid4vci-v1',
+        data: {},
+      })`);
+
+  await dp.DigitalCredentials.setVirtualWalletBehavior({behavior: 'clear'});
+
+  testRunner.log(result);
+  testRunner.completeTest();
+})
