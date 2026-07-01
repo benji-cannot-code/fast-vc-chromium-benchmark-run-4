a56f1025b180ea1ea24b7c5d1f244a7048104560
Stephan Drab <sdrab@google.com>
2026-07-01 03:07:25 -0700

    Add toggle for Gmail OTP codes to contactinfo settings
    - See directional UIR slide: http://screen/4X7p8KDN9yoFw4u
    - See demo screenshot: http://screen/3ooAnNWJZDAcyTD
    - Demo video http://go/scrcast/NDczOTk0MTkzNzQ0NjkxMnw2NDVmM2RlMy0yNQ
    - The learn more link will be done in a followup change
    - Signed out state doesn't show toggle: http://screen/PfhFH9RFntLCorx
    
    Change-Id: Ib374157dbfaaf6dee2910667424619ececd01279
    Bug: b:484907871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7931718
    Reviewed-by: Trang Mai <trangm@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Stephan Drab <sdrab@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655385}

--- chrome/test/data/webui/settings/autofill_fake_data.ts
+++ chrome/test/data/webui/settings/autofill_fake_data.ts
@@ -228,7 +228,7 @@ export class TestAutofillManager extends TestBrowserProxy implements
     AutofillManagerProxy {
   data: {
     addresses: chrome.autofillPrivate.AddressEntry[],
-    accountInfo: chrome.autofillPrivate.AccountInfo,
+    accountInfo?: chrome.autofillPrivate.AccountInfo,
   };
 
   lastCallback:
