7112931d78abf1ad90cdf337364a8aaf7bb8a991
Kalvin Lee <kdlee@chromium.org>
2026-07-01 10:53:46 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    extensions/browser/mime_handler
    
    This CL was uploaded by git cl split.
    
    Bug: 529948677
    Change-Id: I7a5106220755e5e7ada599d4b1194ca7a20b7d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030360
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655619}

--- extensions/browser/mime_handler/mime_handler_registry.cc
+++ extensions/browser/mime_handler/mime_handler_registry.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/containers/span.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
