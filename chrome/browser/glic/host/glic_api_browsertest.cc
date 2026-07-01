1980a0f4b0e36c471e693120bb3ee097c74ab404
Dan Harrington <harringtond@chromium.org>
2026-07-01 10:25:18 -0700

    Migrate testGetPanelStateAttached to new fixture
    Bug: b:477918640
    Change-Id: Ic37cc6100391df665909f44e0314ec2f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7944782
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655603}

--- chrome/browser/glic/host/glic_api_browsertest.cc
+++ chrome/browser/glic/host/glic_api_browsertest.cc
@@ -955,10 +955,6 @@ IN_PROC_BROWSER_TEST_P(GlicApiTestWithDaisyChain,
   ContinueJsTest();
 }
 
-IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab, testGetPanelStateAttached) {
-  ExecuteJsTest();
-}
-
 IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab, testGetPanelStateAttachedHidden) {
   ExecuteJsTest();
 
