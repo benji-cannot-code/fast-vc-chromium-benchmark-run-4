9159edbdd2618b8921fca3d4c79cb08caf3755b0
yoshiki iguchi <yoshiki@google.com>
2026-07-01 02:05:56 -0700

    ASH Refactoring: Move 61 login-related strings to ash/ (Part 8)
    Move 61 login-related strings from chrome/app/chromeos_strings.grdp
    to ash/login/resources/login_strings.grd.
    
    Moved strings:
    - IDS_CONSOLIDATED_CONSENT_ACCEPT_AND_CONTINUE
    - IDS_CONSOLIDATED_CONSENT_ARC_LOCATION_OPT_IN
    - IDS_CONSOLIDATED_CONSENT_ARC_LOCATION_OPT_IN_CHILD
    - IDS_CONSOLIDATED_CONSENT_ARC_LOCATION_OPT_IN_LEARN_MORE
    - IDS_CONSOLIDATED_CONSENT_ARC_LOCATION_OPT_IN_LEARN_MORE_CHILD
    - IDS_CONSOLIDATED_CONSENT_ARC_LOCATION_OPT_IN_TITLE
    - IDS_CONSOLIDATED_CONSENT_ARC_TITLE
    - IDS_CONSOLIDATED_CONSENT_BACKUP_OPT_IN
    - IDS_CONSOLIDATED_CONSENT_BACKUP_OPT_IN_CHILD
    - IDS_CONSOLIDATED_CONSENT_BACKUP_OPT_IN_LEARN_MORE
    - IDS_CONSOLIDATED_CONSENT_BACKUP_OPT_IN_LEARN_MORE_CHILD
    - IDS_CONSOLIDATED_CONSENT_BACKUP_OPT_IN_LEARN_MORE_LINK
    - IDS_CONSOLIDATED_CONSENT_BACKUP_OPT_IN_TITLE
    - IDS_CONSOLIDATED_CONSENT_CROS_EULA_TITLE
    - IDS_CONSOLIDATED_CONSENT_CROS_LOCATION_OPT_IN
    - IDS_CONSOLIDATED_CONSENT_CROS_LOCATION_OPT_IN_CHILD
    - IDS_CONSOLIDATED_CONSENT_CROS_LOCATION_OPT_IN_LEARN_MORE
    - IDS_CONSOLIDATED_CONSENT_CROS_LOCATION_OPT_IN_LEARN_MORE_CHILD
    - IDS_CONSOLIDATED_CONSENT_CROS_LOCATION_OPT_IN_TITLE
    - IDS_CONSOLIDATED_CONSENT_DONE
    - IDS_CONSOLIDATED_CONSENT_FOOTER
    - IDS_CONSOLIDATED_CONSENT_FOOTER_CHILD
    - IDS_CONSOLIDATED_CONSENT_FOOTER_LEARN_MORE
    - IDS_CONSOLIDATED_CONSENT_GOOGLE_EULA_TITLE
    - IDS_CONSOLIDATED_CONSENT_HEADER
    - IDS_CONSOLIDATED_CONSENT_HEADER_CHILD
    - IDS_CONSOLIDATED_CONSENT_HEADER_DEMO_MODE
    - IDS_CONSOLIDATED_CONSENT_HEADER_MANAGED
    - IDS_CONSOLIDATED_CONSENT_LEARN_MORE
    - IDS_CONSOLIDATED_CONSENT_LOADING
    - IDS_CONSOLIDATED_CONSENT_LOCATION_OPT_IN_LEARN_MORE_LINK
    - IDS_CONSOLIDATED_CONSENT_OK
    - IDS_CONSOLIDATED_CONSENT_PLAY_LOAD_ERROR_MESSAGE
    - IDS_CONSOLIDATED_CONSENT_PLAY_LOAD_ERROR_TITLE
    - IDS_CONSOLIDATED_CONSENT_PRIVACY_POLICY_TITLE
    - IDS_CONSOLIDATED_CONSENT_RECOVERY_OPT_IN
    - IDS_CONSOLIDATED_CONSENT_RECOVERY_OPT_IN_TITLE
    - IDS_CONSOLIDATED_CONSENT_SUBHEADER
    - IDS_CONSOLIDATED_CONSENT_SUBHEADER_ARC_DISABLED
    - IDS_CONSOLIDATED_CONSENT_SUBHEADER_DEMO_MODE
    - IDS_CONSOLIDATED_CONSENT_TERMS
    - IDS_CONSOLIDATED_CONSENT_TERMS_ARC_DISABLED
    - IDS_CONSOLIDATED_CONSENT_TERMS_LOAD_ERROR
    - IDS_CONSOLIDATED_CONSENT_TERMS_TITLE
    - IDS_CONSOLIDATED_CONSENT_TRY_AGAIN
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_DEMO_MODE
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_LEARN_MORE_ARC_DISABLED_CHILD_OWNER
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_LEARN_MORE_ARC_DISABLED_OWNER
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_LEARN_MORE_CHILD_OWNER
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_LEARN_MORE_LINK
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_LEARN_MORE_OWNER
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_OWNER
    - IDS_CONSOLIDATED_CONSENT_USAGE_OPT_IN_TITLE
    - IDS_ENROLLMENT_NUDGE_ENROLL_BUTTON
    - IDS_ENROLLMENT_NUDGE_MESSAGE
    - IDS_ENROLLMENT_NUDGE_TITLE
    - IDS_ENROLLMENT_NUDGE_USE_ANOTHER_ACCOUNT_BUTTON
    - IDS_ENTERPRISE_ENROLLMENT_ATTRIBUTE_EXPLANATION
    - IDS_ENTERPRISE_ENROLLMENT_EXPLAIN_ATTRIBUTE_LINK
    - IDS_TPM_CHECK_SUBTITLE
    - IDS_TPM_CHECK_TITLE
    
    Also move 60 string image files from
    chrome/app/chromeos_strings_grdp to ash/login/resources/login_strings_grd.
    
    Bug: 487122295
    Test: CQ passes
    Skip-Translation-Screenshots-Check: True
    Change-Id: I5def3419b25726192f3cf601cffa3c531e00e2ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7929353
    Commit-Queue: Yoshiki IGUCHI <yoshiki@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655356}

--- chrome/browser/ui/webui/ash/login/consolidated_consent_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/consolidated_consent_screen_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
+#include "ash/login/resources/grit/ash_login_strings.h"
 #include "base/command_line.h"
 #include "base/values.h"
 #include "chrome/browser/ash/login/oobe_screen.h"
