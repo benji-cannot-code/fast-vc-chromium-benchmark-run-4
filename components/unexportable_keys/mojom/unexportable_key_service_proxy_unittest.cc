5c9e4a08c5210689442d3deb3c26a6c5362a3d6d
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-07-01 01:05:21 -0700

    Use WithArg to simplify mock expectations in UnexportableKeyServiceProxy unittest.
    The WillOnce lambda now uses testing::WithArg<2> to only capture the
    third argument (the callback) of the mocked
    GenerateSigningKeySlowlyAsync method, improving clarity.
    
    Change-Id: I5242a25ce28ac41aea0b565bf0a4c187776367d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8028861
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655331}

--- components/unexportable_keys/mojom/unexportable_key_service_proxy_unittest.cc
+++ components/unexportable_keys/mojom/unexportable_key_service_proxy_unittest.cc
@@ -51,6 +51,7 @@ using ::testing::Optional;
 using ::testing::Return;
 using ::testing::SizeIs;
 using ::testing::UnorderedElementsAre;
+using ::testing::WithArg;
 
 constexpr BackgroundTaskPriority kTestPriority =
     BackgroundTaskPriority::kBestEffort;
@@ -821,10 +822,10 @@ TEST(UnexportableKeyServiceProxyTest, DestroyProxyBeforeCallback) {
       pending_callback;
   base::RunLoop run_loop;
   EXPECT_CALL(mock_uks, GenerateSigningKeySlowlyAsync)
-      .WillOnce([&](auto, auto, auto cb) {
+      .WillOnce(WithArg<2>([&](auto cb) {
         pending_callback = std::move(cb);
         run_loop.Quit();
-      });
+      }));
   TestFuture<ServiceErrorOr<mojom::NewSigningKeyDataPtr>> future;
   uks_remote->GenerateSigningKey(
       {crypto::SignatureVerifier::SignatureAlgorithm::RSA_PKCS1_SHA1},
@@ -858,10 +859,10 @@ TEST(UnexportableKeyServiceProxyTest, DestroyProxyAndServiceBeforeCallback) {
       pending_callback;
   base::RunLoop run_loop;
   EXPECT_CALL(*mock_uks, GenerateSigningKeySlowlyAsync)
-      .WillOnce([&](auto, auto, auto cb) {
+      .WillOnce(WithArg<2>([&](auto cb) {
         pending_callback = std::move(cb);
         run_loop.Quit();
-      });
+      }));
   TestFuture<ServiceErrorOr<mojom::NewSigningKeyDataPtr>> future;
   uks_remote->GenerateSigningKey(
       {crypto::SignatureVerifier::SignatureAlgorithm::RSA_PKCS1_SHA1},
