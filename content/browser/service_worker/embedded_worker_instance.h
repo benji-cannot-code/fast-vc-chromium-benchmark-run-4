142f2f09d85f68bc93514b970bcfd076506e9aa2
Rob Pitkin <robpitkin@google.com>
2026-06-30 17:27:34 -0700

    hid: Enable WebHID content browser tests on Android
    This CL enables WebHID browser tests (hid_browsertest.cc) to compile and
    run on Android.
    
    To achieve this, the following changes were made:
    
    - Compiled hid_service.cc and related test utils on Android.
    
    - Registered the HidService Mojo binder in browser_interface_binders.cc
    for Android.
    
    - Enabled the WebHID Blink feature in hid_browsertest.cc command line.
    
    - Fixed compilation errors in hid_service.cc by guarding desktop-only
    features (Service Worker registration and FIDO observer) with
    preprocessor guards.
    
    includes the service implementation and test code. The increase is
    attributed to the addition of the `services/device/hid` directory and
    the `content/browser/hid` directory.
    
    Binary-Size: Enabling HID services and tests on Android necessarily
    Bug: 514440682
    Test: content_browsertests --gtest_filter=*Hid*
    Change-Id: I15c1a9bfd7c5b2166bc662650442289a52ce67e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903792
    Commit-Queue: Rob Pitkin <robpitkin@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655163}

--- content/browser/service_worker/embedded_worker_instance.h
+++ content/browser/service_worker/embedded_worker_instance.h
@@ -41,9 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/worker/subresource_loader_updater.mojom.h"
 #include "url/gurl.h"
 
-#if !BUILDFLAG(IS_ANDROID)
 #include "third_party/blink/public/mojom/hid/hid.mojom-forward.h"
-#endif
 
 namespace content {
 
@@ -247,10 +245,8 @@ class CONTENT_EXPORT EmbeddedWorkerInstance
       mojo::PendingReceiver<blink::mojom::CacheStorage> receiver,
       const storage::BucketLocator& bucket_locator);
 
-#if !BUILDFLAG(IS_ANDROID)
   void BindHidService(const url::Origin& origin,
                       mojo::PendingReceiver<blink::mojom::HidService> receiver);
-#endif  // !BUILDFLAG(IS_ANDROID)
 
   void BindUsbService(
       const url::Origin& origin,
