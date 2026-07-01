a2dca010d178fd2925bd45c847556e8e4b9481e7
Zoraiz Naeem <zoraiznaeem@google.com>
2026-07-01 14:54:00 -0700

    [ash] Remove ash-clear-fast-ink-buffer switch
    This removes the ash-clear-fast-ink-buffer switch and its associated
    code as it is no longer used after crrev.com/c/8027604.
    
    Bug: b:513393001
    Change-Id: Ibba29995011a320e3cd93fdc1a27ba2c0cb6a0fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8028158
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655772}

--- ash/constants/ash_switches.h
+++ ash/constants/ash_switches.h
@@ -64,7 +64,6 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kArcTosHostForTests[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kArcVmMountDebugFs[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kArcVmUreadaheadMode[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kArcVmUseHugePages[];
-COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kAshClearFastInkBuffer[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kAshConstrainPointerToRoot[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kAshContextualNudgesInterval[];
@@ -491,7 +490,6 @@ COMPONENT_EXPORT(ASH_CONSTANTS)
 std::optional<base::TimeDelta> ContextualNudgesInterval();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool ContextualNudgesResetShownCount();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsUsingShelfAutoDim();
-COMPONENT_EXPORT(ASH_CONSTANTS) bool ShouldClearFastInkBuffer();
 
 // Returns whether the device has hps hardware.
 COMPONENT_EXPORT(ASH_CONSTANTS) bool HasHps();
