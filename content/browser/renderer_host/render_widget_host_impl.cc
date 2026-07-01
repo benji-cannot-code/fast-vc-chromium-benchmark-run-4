0159b68c004ca246e39643b99100370522ce14d0
Christopher Cameron <ccameron@chromium.org>
2026-07-01 10:59:16 -0700

    Disable HDR and HBD for software rendering
    If hardware acceleration is disabled, then override the screen's
    high dynamic range and high bit depth properties to disable these
    features.
    
    These features aren't implemented in the software renderer, and even
    if they were they would be terribly slow.
    
    Bug: 524780944
    Change-Id: Ia409d0bbedb94066c19a8ca1b4396192ae841812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8010523
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655625}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -97,6 +97,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/device_service.h"
 #include "content/public/browser/disallow_activation_reason.h"
+#include "content/public/browser/gpu_data_manager.h"
 #include "content/public/browser/keyboard_event_processing_result.h"
 #include "content/public/browser/peak_gpu_memory_tracker_factory.h"
 #include "content/public/browser/render_frame_metadata_provider.h"
@@ -1056,6 +1057,15 @@ blink::VisualProperties RenderWidgetHostImpl::GetVisualProperties() {
   blink::VisualProperties visual_properties;
   visual_properties.screen_infos = GetScreenInfos();
   auto& current_screen_info = visual_properties.screen_infos.mutable_current();
+
+  // If hardware acceleration is disabled then do not report the display as
+  // HDR or high bit depth because it is too resource intensive to run on the
+  // CPU.
+  if (!GpuDataManager::GetInstance()->HardwareAccelerationEnabled()) {
+    for (auto& screen_info : visual_properties.screen_infos.screen_infos) {
+      display::DisplayUtil::DisableHdrAndHighBitDepth(&screen_info);
+    }
+  }
 
   // For testing, override the raster color profile.
   // Note: this needs to be done here and not earlier in the pipeline because
