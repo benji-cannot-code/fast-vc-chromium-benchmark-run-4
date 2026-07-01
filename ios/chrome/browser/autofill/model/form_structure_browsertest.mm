9934f2f1b123c781d6c1a572917b9a27fbb325f0
Przemek Perkowski <perkowski@google.com>
2026-07-01 05:09:56 -0700

    Reland "Optimize DataDrivenHeuristics unittests"
    This reverts commit e5a830855e42daf2101cc47d8d9dc308993524cc.
    
    Reason for revert: fixed incorrect preprocessor directives that had led to "unused function" compilation errors on iOS.
    
    Introduced 2 changes compared to the original change:
    * organized preprocessor directives making sure every helper function is
      defined iff it's called in every build config case
    * organized the namespaces
    
    Original change's description:
    > Revert "Optimize DataDrivenHeuristics unittests"
    >
    > This reverts commit 42bca1640816af330ecf40119b3221bf06b8f748.
    >
    > Reason for revert: Breaks iOS build
    >
    > Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/device-builder/337287/overview
    >
    > Original change's description:
    > > Optimize DataDrivenHeuristics unittests
    > >
    > > Introduce sharding of 20 files per unittest setup to balance the latency
    > > and capacity needs of the unittests.
    > > This is a followup to crrev.com/c/7895060.
    > >
    > > Bug: 40287840
    > > Change-Id: I92d2c747de7403e78028ae96153f30756a6a6964
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8018668
    > > Reviewed-by: Dominic Battré <battre@chromium.org>
    > > Commit-Queue: Przemek Perkowski <perkowski@google.com>
    > > Reviewed-by: Maryia Mankevich <mmaryia@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1654787}
    >
    > Bug: 40287840
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I17008b065bd77abecdf956d949fb81a89de502ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025351
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1654806}
    
    Bug: 40287840
    Change-Id: I56be3cc27774ab1fbfc870098e4b3133c19b1f47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8024867
    Reviewed-by: Maryia Mankevich <mmaryia@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Przemek Perkowski <perkowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655424}

--- ios/chrome/browser/autofill/model/form_structure_browsertest.mm
+++ ios/chrome/browser/autofill/model/form_structure_browsertest.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/files/file_path.h"
 #import "base/files/file_util.h"
 #import "base/memory/ptr_util.h"
+#import "base/no_destructor.h"
 #import "base/path_service.h"
 #import "base/strings/strcat.h"
 #import "base/strings/string_util.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/task/thread_pool/thread_pool_instance.h"
 #import "base/test/ios/wait_util.h"
 #import "base/test/scoped_feature_list.h"
+#import "base/threading/thread_restrictions.h"
 #import "base/time/time.h"
 #import "base/time/time_override.h"
 #import "components/autofill/core/browser/foundations/autofill_manager_test_api.h"
@@ -68,7 +70,6 @@ using base::test::ios::kWaitForJSCompletionTimeout;
 using base::test::ios::WaitUntilConditionOrTimeout;
 
 namespace autofill {
-
 namespace {
 
 const base::FilePath::CharType kFeatureName[] = FILE_PATH_LITERAL("autofill");
@@ -79,6 +80,8 @@ base::FilePath GetTestDataDir() {
   base::PathService::Get(ios::DIR_TEST_DATA, &dir);
   return dir;
 }
+
+#if !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
 
 base::FilePath GetIOSInputDirectory() {
   base::FilePath dir;
@@ -92,7 +95,6 @@ base::FilePath GetIOSInputDirectory() {
       .AppendASCII("input");
 }
 
-#if !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
 base::FilePath GetIOSOutputDirectory() {
   base::FilePath dir;
   CHECK(base::PathService::Get(base::DIR_SRC_TEST_DATA_ROOT, &dir));
@@ -104,35 +106,48 @@ base::FilePath GetIOSOutputDirectory() {
       .Append(kTestName)
       .AppendASCII("output");
 }
-#endif
 
-const std::vector<base::FilePath> GetTestFiles() {
-  base::FilePath dir(GetIOSInputDirectory());
-  std::string input_list_string;
-  if (!base::ReadFileToString(dir.AppendASCII("autofill_test_files"),
-                              &input_list_string)) {
-    return {};
-  }
-  std::vector<base::FilePath> result;
-  for (std::string_view piece :
-       base::SplitStringPiece(input_list_string, "\n", base::TRIM_WHITESPACE,
-                              base::SPLIT_WANT_NONEMPTY)) {
-    result.push_back(dir.AppendASCII(piece));
-  }
-  return result;
+const std::vector<base::FilePath>& GetTestFiles() {
+  static const base::NoDestructor<std::vector<base::FilePath>> files([] {
+    base::ScopedAllowBlockingForTesting allow_blocking;
+    base::FilePath dir(GetIOSInputDirectory());
+    std::string input_list_string;
+    if (!base::ReadFileToString(dir.AppendASCII("autofill_test_files"),
+                                &input_list_string)) {
+      return std::vector<base::FilePath>{};
+    }
+    std::vector<base::FilePath> result;
+    for (std::string_view piece :
+         base::SplitStringPiece(input_list_string, "\n", base::TRIM_WHITESPACE,
+                                base::SPLIT_WANT_NONEMPTY)) {
+      result.push_back(dir.AppendASCII(piece));
+    }
+    return result;
+  }());
+  return *files;
 }
 
-}  // namespace
+constexpr int kMaxFilesInShard = 20;
+
+int GetNumShards() {
+  const size_t num_files = GetTestFiles().size();
+  return std::max(1, static_cast<int>((num_files + kMaxFilesInShard - 1) /
+                                      kMaxFilesInShard));
+}
+#else   // !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
+int GetNumShards() {
+  return 1;
+}
+#endif  // !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
 
 // Test fixture for verifying Autofill heuristics. Each input is an HTML
 // file that contains one or more forms. The corresponding output file lists the
 // heuristically detected type for each field.
 // This is based on FormStructureBrowserTest from the Chromium Project.
 // TODO(crbug.com/41015125): Unify the tests.
-class FormStructureBrowserTest
-    : public PlatformTest,
-      public testing::DataDrivenTest,
-      public testing::WithParamInterface<base::FilePath> {
+class FormStructureBrowserTest : public PlatformTest,
+                                 public testing::DataDrivenTest,
+                                 public testing::WithParamInterface<int> {
  public:
   FormStructureBrowserTest(const FormStructureBrowserTest&) = delete;
   FormStructureBrowserTest& operator=(const FormStructureBrowserTest&) = delete;
@@ -360,8 +375,6 @@ std::string FormStructureBrowserTest::FormStructuresToString(
   return base::JoinString(forms_string, "\n");
 }
 
-namespace {
-
 #if !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
 // To disable a data driven test, please add the name of the test file
 // (i.e., "NNN_some_site.html") as a literal to the initializer_list given
@@ -395,9 +408,7 @@ bool IsFailingTestName(const std::string& test_name) {
       });
   return kFailingTestNames.contains(test_name);
 }
-#endif
-
-}  // namespace
+#endif  // !BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
 
 // If disabling a test, prefer to add the name names of the specific test cases
 // to IsFailingTestName(), directly above, instead of renaming the test to
@@ -406,15 +417,23 @@ TEST_P(FormStructureBrowserTest, DataDrivenHeuristics) {
 #if BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
   GTEST_SKIP() << "DataDrivenHeuristics tests are only supported with legacy "
                   "parsing patterns";
-#else
-  bool is_expected_to_pass = !IsFailingTestName(GetParam().BaseName().value());
-  RunOneDataDrivenTest(GetParam(), GetIOSOutputDirectory(),
-                       is_expected_to_pass);
-#endif
+#else   // BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
+  const int shard = GetParam();
+  const int num_shards = GetNumShards();
+  const std::vector<base::FilePath>& files = GetTestFiles();
+  for (size_t i = shard; i < files.size(); i += num_shards) {
+    const base::FilePath& file = files[i];
+    SCOPED_TRACE("Running " + file.MaybeAsASCII());
+    const bool is_expected_to_pass =
+        !IsFailingTestName(file.BaseName().value());
+    RunOneDataDrivenTest(file, GetIOSOutputDirectory(), is_expected_to_pass);
+  }
+#endif  // BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
 }
 
 INSTANTIATE_TEST_SUITE_P(AllForms,
                          FormStructureBrowserTest,
-                         testing::ValuesIn(GetTestFiles()));
+                         testing::Range(0, GetNumShards()));
 
+}  // namespace
 }  // namespace autofill
