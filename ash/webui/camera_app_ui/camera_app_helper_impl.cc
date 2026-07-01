114193345d7a150aa57f9c339148eb74605e0d13
Kalvin Lee <kdlee@chromium.org>
2026-07-01 03:15:48 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    ash/webui/camera_app_ui
    
    This CL was uploaded by git cl split.
    
    R=chuhsuan@chromium.org
    
    Bug: 529948677
    Change-Id: Ifb67efe0a87a988354f576a4faee2f62ddef5b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030124
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655393}

--- ash/webui/camera_app_ui/camera_app_helper_impl.cc
+++ ash/webui/camera_app_ui/camera_app_helper_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/cpp/window_properties.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/functional/callback_helpers.h"
 #include "base/notreached.h"
