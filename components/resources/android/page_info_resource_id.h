f59ae3f8e1c93d137fd60e66c7b26a39849041b5
Ziwen Lin <ziwlin@microsoft.com>
2026-06-30 18:49:29 -0700

    [AAPT][Drawable] Share duplicate lock icon resource
    facilitated_payments_lock_icon.xml and page_info lock.xml are
    byte-identical after aapt2 link. Move the shared lock vector to
    browser_ui styles and update facilitated payments and page_info to use
    it.
    
    Android Binary Size -672 bytes
    
    Base Module Size -672 bytes
    
    Doc：https://docs.google.com/document/d/1QV7sBBB2Y2PxB6TUOAhwQDS5HhyLldvsrzEav3lxcZs/edit?usp=sharing
    
    Bug: 40127968
    Change-Id: If9e67979cb0a8c3d83fa6276366c31cfaab6c020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8017685
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Ziwen Lin <ziwlin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1655197}

--- components/resources/android/page_info_resource_id.h
+++ components/resources/android/page_info_resource_id.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Good:
 DECLARE_RESOURCE_ID(IDR_PAGEINFO_GOOD, R.drawable.omnibox_https_valid_lock)
 // Good (new version):
-DECLARE_RESOURCE_ID(IDR_PAGEINFO_GOOD_NEW, R.drawable.lock)
+DECLARE_RESOURCE_ID(IDR_PAGEINFO_GOOD_NEW, R.drawable.ic_lock_24dp)
 // Bad:
 DECLARE_RESOURCE_ID(IDR_PAGEINFO_BAD, R.drawable.omnibox_not_secure_warning)
 // Internal page:
