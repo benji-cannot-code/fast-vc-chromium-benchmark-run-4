e6e291e555bdb4cb427d1d31fbdcbfa5d3739837
Kalvin Lee <kdlee@chromium.org>
2026-07-01 10:20:28 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    ash/webui/eche_app_ui
    
    This CL was uploaded by git cl split.
    
    R=crisrael@google.com
    
    Bug: 529948677
    Change-Id: Ieeab552a73f2167f8debc10724693a75371bf568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8028762
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655601}

--- ash/webui/eche_app_ui/eche_uid_provider.cc
+++ ash/webui/eche_app_ui/eche_uid_provider.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/check.h"
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "chromeos/ash/components/multidevice/logging/logging.h"
 #include "components/prefs/pref_service.h"
 #include "crypto/random.h"
