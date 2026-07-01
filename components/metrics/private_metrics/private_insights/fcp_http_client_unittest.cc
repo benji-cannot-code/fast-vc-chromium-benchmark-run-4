07ab458015ba844db2a0168ca0d864f62d6aa5a8
Kamil Jarosz <kjarosz@google.com>
2026-07-01 08:14:57 -0700

    private_insights: Log runner count histogram in FcpHttpClient
    Record the `PrivateMetrics.PrivateInsights.FcpHttpClient.RunnerCount`
    UMA histogram to track the number of active HTTP request runners in
    `FcpHttpRequestManager`.
    
    The metric is recorded whenever a new request is started on the UI
    thread (`StartRequestOnUI`), helping monitor concurrent request
    concurrency and usage.
    
    Also updates unit tests in `fcp_http_client_unittest.cc` and adds
    the corresponding histogram definition in `histograms.xml`.
    
    Fix: b:530087751
    Change-Id: Ifbeba6767652e8c9d38879ffb9764c1bd8f9d068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8031340
    Reviewed-by: Matt Dembski <dembski@google.com>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655516}

--- components/metrics/private_metrics/private_insights/fcp_http_client_unittest.cc
+++ components/metrics/private_metrics/private_insights/fcp_http_client_unittest.cc
@@ -247,6 +247,7 @@ TEST_F(FcpHttpClientTest, PerformRequestsSuccessfulSingle) {
   histogram_tester.ExpectUniqueSample(kFcpHttpClientBatchSizeHistogram, 1, 1);
   histogram_tester.ExpectTotalCount(kFcpHttpClientBytesReceivedHistogram, 1);
   histogram_tester.ExpectTotalCount(kFcpHttpClientBytesSentHistogram, 1);
+  histogram_tester.ExpectUniqueSample(kFcpHttpClientRunnerCountHistogram, 1, 1);
 }
 
 TEST_F(FcpHttpClientTest, PerformRequestsSuccessfulBatch) {
@@ -284,6 +285,7 @@ TEST_F(FcpHttpClientTest, PerformRequestsSuccessfulBatch) {
   EXPECT_TRUE(callback2.response_completed_called_);
   EXPECT_EQ(callback2.body_received_, "resp2");
   histogram_tester.ExpectUniqueSample(kFcpHttpClientBatchSizeHistogram, 2, 1);
+  histogram_tester.ExpectTotalCount(kFcpHttpClientRunnerCountHistogram, 2);
 }
 
 TEST_F(FcpHttpClientTest, PerformRequestsNetError) {
