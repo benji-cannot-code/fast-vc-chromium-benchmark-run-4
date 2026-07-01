e2fe4fb12c503db125b7a055b87e7dca09a18695
Kunal Daftari <kunaldaftari@google.com>
2026-07-01 11:23:41 -0700

    [Vertical Tabs] Moving FirstPresentation Logic to RegionView
    Because TabStripView can be destroyed during runtime, we are moving the logic to a class whose prescence is guaranteed, VerticalTabStripRegionView.
    
    Bug: 530202496
    Change-Id: Ie940b9901c03b42e1963fbdee9493b9da4dc0fa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032000
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655643}

--- chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h
+++ chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h
@@ -55,7 +55,8 @@ class VerticalTabStripRegionView final
     : public BaseTabStripRegionView,
       public views::ResizeAreaDelegate,
       public OmniboxTabHelper::Observer,
-      public tabs::VerticalTabStripStateController::Delegate {
+      public tabs::VerticalTabStripStateController::Delegate,
+      public views::WidgetObserver {
   METADATA_HEADER(VerticalTabStripRegionView, BaseTabStripRegionView)
 
  public:
@@ -137,6 +138,9 @@ class VerticalTabStripRegionView final
       base::RepeatingCallback<void(bool)> callback) override;
   bool IsCollapsing() override;
   void RequestCollapse(bool collapse) override;
+
+  // views::WidgetObserver:
+  void OnWidgetVisibilityChanged(views::Widget* widget, bool visible) override;
 
   views::Separator* tabs_separator_for_testing() {
     return tab_strip_view_->GetTabsSeparator();
@@ -296,6 +300,10 @@ class VerticalTabStripRegionView final
 
   // The mouse exit event debounce timer.
   base::OneShotTimer mouse_exit_timer_;
+
+  bool is_first_window_presentation_ = true;
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      widget_observation_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_FRAME_VERTICAL_TAB_STRIP_REGION_VIEW_H_
