1081ee4a7144228c1f67c812dae375b132e96061
Daniel Rubery <drubery@chromium.org>
2026-07-01 09:19:46 -0700

    Reland "Resolve leak in AppPlatformMetricsServiceBrowserTest"
    This reverts commit 32aefae9e1b097dc68b168add6d9b01f8541d331. Patchset 1
    is a pure reland. Patchset 2 fixes the failures by registering the test with a different user so that `chromeos::IsManagedGuestSession` passes.
    
    Fixed: 521490538
    Link: https://chromium-review.googlesource.com/id/I52aa0524521baff4dbde744cf4059f036a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8021586
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655542}

--- chrome/browser/apps/app_service/app_service_proxy_ash.cc
+++ chrome/browser/apps/app_service/app_service_proxy_ash.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/task_traits.h"
+#include "base/time/default_clock.h"
+#include "base/time/default_tick_clock.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_factory.h"
 #include "chrome/browser/apps/app_service/app_icon/app_icon_util.h"
 #include "chrome/browser/apps/app_service/app_install/app_install_service.h"
@@ -149,7 +151,10 @@ void AppServiceProxyAsh::Initialize() {
   if (!profile_->AsTestingProfile() &&
       !::ash::IsShimlessRmaAppBrowserContext(profile_)) {
     app_platform_metrics_service_ =
-        std::make_unique<apps::AppPlatformMetricsService>(profile_);
+        std::make_unique<apps::AppPlatformMetricsService>(
+            profile_, base::DefaultClock::GetInstance(),
+            base::DefaultTickClock::GetInstance(),
+            base::SequencedTaskRunner::GetCurrentDefault());
     base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE, base::BindOnce(&AppServiceProxyAsh::InitAppPlatformMetrics,
                                   weak_ptr_factory_.GetWeakPtr()));
--- chrome/browser/apps/app_service/metrics/app_platform_metrics.cc
+++ chrome/browser/apps/app_service/metrics/app_platform_metrics.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/values_util.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/time/default_tick_clock.h"
+#include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
@@ -186,9 +188,11 @@ void RecordAppLaunchPerAppTypeV2(apps::AppTypeNameV2 app_type_name_v2) {
                                 app_type_name_v2);
 }
 
-base::TimeDelta GetDurationAndResetStartTime(base::TimeTicks& start_time) {
-  base::TimeDelta duration = base::TimeTicks::Now() - start_time;
-  start_time = base::TimeTicks::Now();
+base::TimeDelta GetDurationAndResetStartTime(
+    base::TimeTicks& start_time,
+    const base::TickClock& tick_clock) {
+  base::TimeDelta duration = tick_clock.NowTicks() - start_time;
+  start_time = tick_clock.NowTicks();
   return duration;
 }
 
@@ -382,8 +386,11 @@ base::DictValue AppPlatformMetrics::UsageTime::ConvertToDict() const {
 AppPlatformMetrics::AppPlatformMetrics(
     Profile* profile,
     apps::AppRegistryCache& app_registry_cache,
-    InstanceRegistry& instance_registry)
-    : profile_(profile), app_registry_cache_(app_registry_cache) {
+    InstanceRegistry& instance_registry,
+    const base::TickClock* tick_clock)
+    : profile_(profile),
+      app_registry_cache_(app_registry_cache),
+      tick_clock_(CHECK_DEREF(tick_clock)) {
   app_registry_cache_observer_.Observe(&app_registry_cache);
   instance_registry_observation_.Observe(&instance_registry);
   if (chromeos::IsManagedGuestSession()) {
@@ -941,14 +948,15 @@ void AppPlatformMetrics::SetWindowActivated(
     return;
   }
 
-  running_start_time_[instance_id].start_time = base::TimeTicks::Now();
+  running_start_time_[instance_id].start_time = tick_clock_->NowTicks();
   running_start_time_[instance_id].app_type_name = app_type_name;
   running_start_time_[instance_id].app_type_name_v2 = app_type_name_v2;
 
   ++activated_count_[app_type_name];
   should_refresh_activated_count_pref = true;
 
-  start_time_per_five_minutes_[instance_id].start_time = base::TimeTicks::Now();
+  start_time_per_five_minutes_[instance_id].start_time =
+      tick_clock_->NowTicks();
   start_time_per_five_minutes_[instance_id].app_type_name = app_type_name;
   start_time_per_five_minutes_[instance_id].app_type_name_v2 = app_type_name_v2;
   start_time_per_five_minutes_[instance_id].app_id = app_id;
@@ -973,10 +981,10 @@ void AppPlatformMetrics::SetWindowInActivated(
   AppTypeNameV2 app_type_name_v2 = it->second.app_type_name_v2;
 
   running_duration_[app_type_name] +=
-      base::TimeTicks::Now() - it->second.start_time;
+      tick_clock_->NowTicks() - it->second.start_time;
 
   base::TimeDelta running_time =
-      base::TimeTicks::Now() -
+      tick_clock_->NowTicks() -
       start_time_per_five_minutes_[instance_id].start_time;
   app_type_running_time_per_five_minutes_[app_type_name] += running_time;
   app_type_v2_running_time_per_five_minutes_[app_type_name_v2] += running_time;
@@ -1080,7 +1088,7 @@ void AppPlatformMetrics::RecordAppsCount(AppType app_type) {
 void AppPlatformMetrics::RecordAppsRunningDuration() {
   for (auto& it : running_start_time_) {
     running_duration_[it.second.app_type_name] +=
-        GetDurationAndResetStartTime(it.second.start_time);
+        GetDurationAndResetStartTime(it.second.start_time, *tick_clock_);
   }
 
   base::TimeDelta total_running_duration;
@@ -1111,7 +1119,7 @@ void AppPlatformMetrics::RecordAppsRunningDuration() {
 void AppPlatformMetrics::RecordAppsUsageTime() {
   for (auto& it : start_time_per_five_minutes_) {
     base::TimeDelta running_time =
-        GetDurationAndResetStartTime(it.second.start_time);
+        GetDurationAndResetStartTime(it.second.start_time, *tick_clock_);
     app_type_running_time_per_five_minutes_[it.second.app_type_name] +=
         running_time;
     app_type_v2_running_time_per_five_minutes_[it.second.app_type_name_v2] +=
@@ -1366,7 +1374,7 @@ void AppPlatformMetrics::ClearAppsUsageTimeForInstance(
 void AppPlatformMetrics::UpdateMetricsBeforeShutdown() {
   for (auto& it : running_start_time_) {
     running_duration_[it.second.app_type_name] +=
-        GetDurationAndResetStartTime(it.second.start_time);
+        GetDurationAndResetStartTime(it.second.start_time, *tick_clock_);
   }
 
   RecordAppsUsageTime();
