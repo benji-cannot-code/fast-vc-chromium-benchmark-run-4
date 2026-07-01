78a1e1ef5b673f57340b8fbaf45fcf9861cc6948
Sky Malice <skym@chromium.org>
2026-07-01 08:13:13 -0700

    [AIM] Fix standby height glitch.
    Keep the fusebox in DISABLED state when in STANDBY. Otherwise sequencing
    can cause a larger layout height.
    
    Bug: 527953310
    Change-Id: Id4a6b4824aeb64f80183d430c9bb8120a4afbd6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8028112
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655515}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediatorUnitTest.java
@@ -602,6 +602,14 @@ public class FuseboxMediatorUnitTest {
     }
 
     @Test
+    public void updateFuseboxState_standby_isDisabled() {
+        mInput.setAutocompleteState(AutocompleteState.STANDBY);
+        recreateMediator();
+
+        assertEquals(FuseboxState.DISABLED, mModel.get(FuseboxProperties.FUSEBOX_STATE).intValue());
+    }
+
+    @Test
     public void updateFuseboxState_standbyNoFocus_isDisabled() {
         mInput.setAutocompleteState(AutocompleteState.STANDBY_NO_FOCUS);
         recreateMediator();
