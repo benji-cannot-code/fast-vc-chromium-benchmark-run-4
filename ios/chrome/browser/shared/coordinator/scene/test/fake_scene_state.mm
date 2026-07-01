8e8116d50cb209709e536b5953377741b326ccfc
Sylvain Defresne <sdefresne@chromium.org>
2026-07-01 02:08:25 -0700

    [ios] Cleanup FakeSceneState implementation
    Use a local variable to remove unnecessary cast of the browser
    provider interface instance to the sub-class that was created.
    
    Pass GURL by const reference instead of via value (avoid copies).
    
    Bug: none
    Change-Id: I7c928c73c3651574ee7d1e9d4966e86a2a8bb6a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8024789
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655358}

--- ios/chrome/browser/shared/coordinator/scene/test/fake_scene_state.mm
+++ ios/chrome/browser/shared/coordinator/scene/test/fake_scene_state.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <utility>
 
-#import "base/apple/foundation_util.h"
 #import "base/check.h"
 #import "ios/chrome/browser/shared/coordinator/scene/test/stub_browser_provider.h"
 #import "ios/chrome/browser/shared/coordinator/scene/test/stub_browser_provider_interface.h"
@@ -38,20 +37,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     DCHECK(profile);
     DCHECK(!profile->IsOffTheRecord());
     self.activationLevel = SceneActivationLevelForegroundInactive;
-    self.browserProviderInterface = [[StubBrowserProviderInterface alloc] init];
+    StubBrowserProviderInterface* browserProviderInterface =
+        [[StubBrowserProviderInterface alloc] init];
+    self.browserProviderInterface = browserProviderInterface;
     self.appState = appState;
 
     _browser = std::make_unique<TestBrowser>(profile, self);
-    base::apple::ObjCCastStrict<StubBrowserProvider>(
-        self.browserProviderInterface.mainBrowserProvider)
-        .browser = _browser.get();
+    browserProviderInterface.mainBrowserProvider.browser = _browser.get();
     std::ignore = _browser->CreateInactiveBrowser();
 
     _incognito_browser =
         std::make_unique<TestBrowser>(profile->GetOffTheRecordProfile(), self);
-    base::apple::ObjCCastStrict<StubBrowserProvider>(
-        self.browserProviderInterface.incognitoBrowserProvider)
-        .browser = _incognito_browser.get();
+    browserProviderInterface.incognitoBrowserProvider.browser =
+        _incognito_browser.get();
   }
   return self;
 }
@@ -60,7 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CHECK(_shutdown) << "-shutdown must be called before -dealloc";
 }
 
-- (void)appendWebStateWithURL:(const GURL)URL {
+- (void)appendWebStateWithURL:(const GURL&)URL {
   auto test_web_state = std::make_unique<web::FakeWebState>();
   test_web_state->SetCurrentURL(URL);
   WebStateList* web_state_list =
@@ -69,7 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   web_state_list->InsertWebState(std::move(test_web_state));
 }
 
-- (void)appendWebStatesWithURL:(const GURL)URL count:(int)count {
+- (void)appendWebStatesWithURL:(const GURL&)URL count:(int)count {
   for (int i = 0; i < count; i++) {
     [self appendWebStateWithURL:URL];
   }
