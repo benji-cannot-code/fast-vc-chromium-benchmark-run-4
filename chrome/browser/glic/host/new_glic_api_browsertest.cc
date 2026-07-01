b937cef1d436031a6adbcb0d5909e001111c93bd
Dan Harrington <harringtond@chromium.org>
2026-07-01 10:01:40 -0700

    Migrate testShowProfilePicker to new fixture
    Bug: b:477918640
    Change-Id: Iffcc0f5f38cdb7e8fc4fda7ff6e9e1ef6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7944780
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655578}

--- chrome/browser/glic/host/new_glic_api_browsertest.cc
+++ chrome/browser/glic/host/new_glic_api_browsertest.cc
@@ -92,6 +92,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/browser_context_helper/browser_context_types.h"
 #endif
 
+#if !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/profiles/profile_picker.h"
+#endif
+
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/glic/widget/glic_widget.h"
 #include "chrome/browser/media/audio_ducker.h"
@@ -589,6 +593,17 @@ IN_PROC_BROWSER_TEST_P(NewGlicApiTest, testClosePanel) {
   ExecuteJsTest();
   ASSERT_OK(WaitForGlicClose());
 }
+
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
+IN_PROC_BROWSER_TEST_P(NewGlicApiTest, testShowProfilePicker) {
+  base::test::TestFuture<void> profile_picker_opened;
+  ProfilePicker::AddOnProfilePickerOpenedCallbackForTesting(
+      profile_picker_opened.GetCallback());
+  ASSERT_OK(OpenGlicForActiveTab());
+  ExecuteJsTest();
+  ASSERT_TRUE(profile_picker_opened.Wait());
+}
+#endif
 
 class NewGlicApiTestWithWebActuationSettingEnabled : public NewGlicApiTest {
  public:
