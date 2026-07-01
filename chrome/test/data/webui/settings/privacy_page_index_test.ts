d758342fe2c971f51dd1c5d6838df3a518178c2e
Lingqi Chi <lingqi@chromium.org>
2026-07-01 02:15:57 -0700

    Disable SettingsTest.PrivacyPageIndexSiteSettings. RoutingBluetoothScanning
    Bug: 530054694
    Change-Id: I71291aaea1ba486f7789b85f77b92fcd4f8ff2bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030060
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655365}

--- chrome/test/data/webui/settings/privacy_page_index_test.ts
+++ chrome/test/data/webui/settings/privacy_page_index_test.ts
@@ -471,7 +471,8 @@ suite('PrivacyPageIndex', function() {
           routes.SITE_SETTINGS_BLUETOOTH_DEVICES,
           ['siteSettingsBluetoothDevices'], 'privacy');
     });
-
+    // TODO(https://crbug.com/530054694): Flaky on Linux.
+    // <if expr="not is_linux">
     test('RoutingBluetoothScanning', async function() {
       assertFalse(
           loadTimeData.getBoolean('enableExperimentalWebPlatformFeatures'));
@@ -482,6 +483,7 @@ suite('PrivacyPageIndex', function() {
           routes.SITE_SETTINGS_BLUETOOTH_SCANNING,
           ['siteSettingsBluetoothScanning'], 'privacy');
     });
+    // </if>
 
     test('RoutingCapturedSurfaceControl', async function() {
       assertFalse(loadTimeData.getBoolean('enableCapturedSurfaceControl'));
