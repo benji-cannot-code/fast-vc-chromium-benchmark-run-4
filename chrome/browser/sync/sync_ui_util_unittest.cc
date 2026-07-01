fbf10f69904b9f294e60085c95875308402d7265
David Roger <droger@chromium.org>
2026-07-01 07:23:33 -0700

    [signin] Prepare tests for enabling kReplaceSyncPromosWithSigninPromosNewSignin
    Bug: 411329731
    Change-Id: I136cfe2ccf298bb58f0a92b3e7086b88c9db1c3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025336
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655477}

--- chrome/browser/sync/sync_ui_util_unittest.cc
+++ chrome/browser/sync/sync_ui_util_unittest.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/sync_ui_util.h"
 
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
+#include "components/sync/base/features.h"
 #include "components/sync/test/test_sync_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -19,6 +21,12 @@ TEST(SyncUIUtilTest, ShouldShowSyncPassphraseError) {
 }
 
 TEST(SyncUIUtilTest, ShouldShowSyncPassphraseError_SyncDisabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      /*enabled_features=*/{},
+      /*disabled_features=*/{
+          syncer::kReplaceSyncPromosWithSignInPromos,
+          syncer::kReplaceSyncPromosWithSigninPromosNewSignin});
   syncer::TestSyncService service;
   service.SetInitialSyncFeatureSetupComplete(false);
   service.SetPassphraseRequired();
