43320282ec1c167cce49443a0812593b071f7cc8
Kalvin Lee <kdlee@chromium.org>
2026-07-01 16:20:04 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    chrome/browser/ash/login
    
    This CL was uploaded by git cl split.
    
    R=achuith@chromium.org
    
    Bug: 529948677
    Change-Id: Iede22dcfa1e2913381ba5e0711f83ecf0244d337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030292
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655811}

--- chrome/browser/ash/login/users/avatar/user_image_manager_impl.cc
+++ chrome/browser/ash/login/users/avatar/user_image_manager_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/image_downloader.h"
 #include "base/check.h"
 #include "base/check_deref.h"
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
