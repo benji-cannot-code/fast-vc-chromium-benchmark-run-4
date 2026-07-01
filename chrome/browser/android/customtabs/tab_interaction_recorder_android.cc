b9b3081bd4143267d56a68822aafeb153f7772ce
Kalvin Lee <kdlee@chromium.org>
2026-07-01 12:19:45 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    chrome/browser/android/customtabs
    
    This CL was uploaded by git cl split.
    
    R=adelm@google.com
    
    Bug: 529948677
    Change-Id: Idc8431f1818e5fc9d9400cfa41973cd345d69a0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030382
    Commit-Queue: Moe Adel <adelm@google.com>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655682}

--- chrome/browser/android/customtabs/tab_interaction_recorder_android.cc
+++ chrome/browser/android/customtabs/tab_interaction_recorder_android.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/android/jni_android.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/android/customtabs/custom_tab_session_state_tracker.h"
--- chrome/browser/android/customtabs/tab_interaction_recorder_android.h
+++ chrome/browser/android/customtabs/tab_interaction_recorder_android.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ANDROID_CUSTOMTABS_TAB_INTERACTION_RECORDER_ANDROID_H_
 
 #include "base/android/jni_android.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
