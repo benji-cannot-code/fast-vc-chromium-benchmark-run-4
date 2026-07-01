2ed729eb93a591073341e5bbbc97a1cc3c925552
Gauthier Ambard <gambard@chromium.org>
2026-07-01 08:11:21 -0700

    [iOS] Always use a 2x scale for snapshot on iPhone
    iPhones don't have 1x scale anymore. We can always use 2x as this is the
    max in the code.
    
    This is to remove use of mainScreen which is deprecated.
    
    Bug: 527832290
    Change-Id: Ic9aa33a5b9459f52d760dce7cf882091c300f746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8029988
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655513}

--- ios/chrome/browser/snapshots/model/snapshot_scale.mm
+++ ios/chrome/browser/snapshots/model/snapshot_scale.mm
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   // Cap snapshot resolution to 2x to reduce the amount of memory used.
-  return [UIScreen mainScreen].scale == 1.0 ? kImageScale1X : kImageScale2X;
+  return kImageScale2X;
 }
 
 + (CGFloat)floatImageScaleForDevice {
