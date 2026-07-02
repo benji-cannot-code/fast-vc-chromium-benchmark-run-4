95c8138bf7a4817190b62ca87694f206a62ff4e8
Kalvin Lee <kdlee@chromium.org>
2026-07-02 09:23:06 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    base/android
    
    This CL was uploaded by git cl split.
    
    R=yfriedman@chromium.org
    
    Bug: 529948677
    Change-Id: I386c170034cd71bfa7e2ba8dbccd051c2ebaeca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030320
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656133}

--- base/android/feature_map.cc
+++ base/android/feature_map.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/android/jni_string.h"
+#include "base/containers/span.h"
 #include "base/logging.h"
 #include "base/metrics/field_trial_params.h"
 
--- base/android/jni_array.cc
+++ base/android/jni_array.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/containers/extend.h"
 #include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_view_util.h"
 
