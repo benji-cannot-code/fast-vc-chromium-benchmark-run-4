236adf47aca3ce4ee2505007956971004f655faf
Gabriel Brito <gabrielbrito@microsoft.com>
2026-07-01 22:58:02 -0700

    [Gamepad] Use GameInput only in Win11
    This CL makes the GameInputDataFetcher available only in Win11 machines.
    Unfortunately, Win10 uses an older version of GameInput that has bugs.
    
    Bug: 488268858
    Change-Id: I908ee054e7fd4dcc416646666429d55099a95c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8013145
    Commit-Queue: Gabriel Brito <gabrielbrito@microsoft.com>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655905}

--- device/gamepad/gamepad_platform_data_fetcher.h
+++ device/gamepad/gamepad_platform_data_fetcher.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_ANDROID)
 #include "device/gamepad/gamepad_platform_data_fetcher_android.h"
 #elif BUILDFLAG(IS_WIN)
+#include "base/win/windows_version.h"
 #include "device/gamepad/gameinput_data_fetcher.h"
 #include "device/gamepad/nintendo_data_fetcher.h"
 #include "device/gamepad/raw_input_data_fetcher_win.h"
@@ -45,7 +46,8 @@ void AddGamepadPlatformDataFetchers(GamepadDataFetcherManager* manager) {
 
 #elif BUILDFLAG(IS_WIN)
 
-  if (base::FeatureList::IsEnabled(
+  if (base::win::GetVersion() >= base::win::Version::WIN11 &&
+      base::FeatureList::IsEnabled(
           features::kEnableWindowsGameInputDataFetcher)) {
     manager->AddFactory(new GameInputDataFetcher::Factory());
   } else {
