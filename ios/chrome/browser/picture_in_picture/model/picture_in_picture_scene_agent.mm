f6ae3adb8c6d83a7416b9944d591dfa0e70bfb1d
Sylvain Defresne <sdefresne@chromium.org>
2026-07-02 07:03:14 -0700

    [ios] Avoid crashing if currentBrowserProvider is nil
    The currentBrowserProvider could be nil (e.g. while destroying the
    last incognito tab, ...). As PictureInPictureSceneAgent uses it to
    access the Browser which is a C++ object check that the pointer is
    not null before dereferencing it (since it is undefined behaviour
    in C++).
    
    Fixed: 530146380
    Change-Id: I1398d99d14d9926370ffeebe9bccbb5b4902c0ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035862
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656072}

--- ios/chrome/browser/picture_in_picture/model/picture_in_picture_scene_agent.mm
+++ ios/chrome/browser/picture_in_picture/model/picture_in_picture_scene_agent.mm
@@ -21,14 +21,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)sceneState:(SceneState*)sceneState
     transitionedToActivationLevel:(SceneActivationLevel)level {
   switch (level) {
-    case SceneActivationLevelForegroundActive: {
-      id<PictureInPictureCommands> handler = HandlerForProtocol(
-          sceneState.browserProviderInterface.currentBrowserProvider.browser
-              ->GetCommandDispatcher(),
-          PictureInPictureCommands);
-      [handler dismissPictureInPictureIfNotPipRestore];
+    case SceneActivationLevelForegroundActive:
+      // The `currentBrowserProvider` may be nil, avoid crashing when this
+      // is the case (as `browser` is a pointer to a C++ object it is not
+      // safe to dereference if null).
+      if (Browser* browser = sceneState.browserProviderInterface
+                                 .currentBrowserProvider.browser) {
+        id<PictureInPictureCommands> handler = HandlerForProtocol(
+            browser->GetCommandDispatcher(), PictureInPictureCommands);
+        [handler dismissPictureInPictureIfNotPipRestore];
+      }
       break;
-    }
     case SceneActivationLevelUnattached:
     case SceneActivationLevelDisconnected:
     case SceneActivationLevelBackground:
