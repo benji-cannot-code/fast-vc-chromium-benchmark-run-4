2bcb8608c80849e1d0a578ebbae25457dc9f8fc0
Eriko Kurimoto <elkurin@chromium.org>
2026-07-01 20:53:35 -0700

    WaaP: Re-enable and fix BrowserActionInteractiveTest with InitialWebUI
    The test was flaky because the incognito window was not always fully
    ready when the extension's background page tried to call openPopup. This
    change modifies the test to wait for the incognito window to be active
    before triggering the openPopup call from the extension's background
    script.
    
    Bug: 477426026
    Change-Id: Ia435696e4f791b5c53b7200e6df6cabcfa01a4a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025483
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655881}

--- chrome/test/data/extensions/api_test/browser_action/open_popup_background/background.js
+++ chrome/test/data/extensions/api_test/browser_action/open_popup_background/background.js
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
+chrome.test.sendMessage('ready', function(reply) {
+  if (reply === 'openPopup') {
     chrome.browserAction.openPopup(function(popup) {
       chrome.test.sendMessage(
           popup ? 'opened' :
-              'failed: ' + chrome.runtime.lastError ?
+              (chrome.runtime.lastError ?
                   chrome.runtime.lastError.message :
-                  'unknown error');
+                  'failed'));
     });
   }
 });
