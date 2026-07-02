54bbde6ce68fde8e1206f2905fca6cc80edc63e8
Sky Malice <skym@chromium.org>
2026-07-02 12:48:00 -0700

    [AIM] Always use transparent scrim on android desktop.
    Bug: 493921540
    Change-Id: I7a48d05d80af59d115fe750e1135566b945c8ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8033123
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656221}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarFocusScrimHandler.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarFocusScrimHandler.java
@@ -117,8 +117,7 @@ public class LocationBarFocusScrimHandler {
         boolean useTransparentScrim =
                 isTablet
                         && (OmniboxFeatures.isMultimodalInputEnabled(mContext)
-                                || (OmniboxCapabilities.isDesktopPlatform()
-                                        && OmniboxFeatures.sAndroidDesktopAimGate.isEnabled()));
+                                || OmniboxCapabilities.isDesktopPlatform());
         boolean useLightColor =
                 !isTablet
                         && !mLocationBarDataProvider.isIncognitoBranded()
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarFocusScrimHandlerUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarFocusScrimHandlerUnitTest.java
@@ -33,6 +33,7 @@ import org.chromium.chrome.browser.browser_controls.BottomControlsStacker;
 import org.chromium.chrome.browser.browser_controls.BottomControlsStacker.LayerType;
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
 import org.chromium.components.browser_ui.widget.scrim.ScrimProperties;
+import org.chromium.components.omnibox.OmniboxCapabilities;
 import org.chromium.components.omnibox.OmniboxFeatureList;
 
 /** Unit tests for {@link LocationBarFocusScrimHandler}. */
@@ -115,5 +116,18 @@ public class LocationBarFocusScrimHandlerUnitTest {
         assertEquals(
                 BOTTOM_CHIN_HEIGHT,
                 mScrimHandler.getScrimModelForTesting().get(ScrimProperties.BOTTOM_MARGIN));
+    }
+
+    @Test
+    @Config(qualifiers = "sw800dp")
+    public void testUpdateScrimVisualState_transparentScrim_Desktop() {
+        OmniboxCapabilities.setIsDesktopPlatformForTesting(true);
+        mScrimHandler.updateScrimVisualState();
+        assertEquals(
+                Color.TRANSPARENT,
+                mScrimHandler
+                        .getScrimModelForTesting()
+                        .get(ScrimProperties.BACKGROUND_COLOR)
+                        .intValue());
     }
 }
