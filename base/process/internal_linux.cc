7da3812403ec11885711a9a59508fb5b0591a1c5
Kalvin Lee <kdlee@chromium.org>
2026-07-02 01:20:47 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    base/process
    
    This CL was uploaded by git cl split.
    
    R=grt@chromium.org, tsepez@chromium.org
    
    Bug: 529948677
    Change-Id: I14e70cb3e0234424ee200061f6377b7b23517696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030203
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655944}

--- base/process/internal_linux.cc
+++ base/process/internal_linux.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/notreached.h"
--- base/process/launch_fuchsia.cc
+++ base/process/launch_fuchsia.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/command_line.h"
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/fuchsia/default_job.h"
 #include "base/fuchsia/file_utils.h"
