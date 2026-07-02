b2c6658e45e2fe4e5eee2f52608c915825e05e1d
Kalvin Lee <kdlee@chromium.org>
2026-07-01 18:48:41 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    base/memory
    
    This CL was uploaded by git cl split.
    
    R=grt@chromium.org, sammc@chromium.org
    
    Bug: 529948677
    Change-Id: I70addc4d9bad3367da9263f2f1709509a46342dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030340
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655856}

--- base/memory/discardable_shared_memory.cc
+++ base/memory/discardable_shared_memory.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomicops.h"
 #include "base/bits.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/memory/discardable_memory.h"
--- base/memory/discardable_shared_memory_unittest.cc
+++ base/memory/discardable_shared_memory_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/containers/span.h"
 #include "base/files/scoped_file.h"
 #include "base/memory/page_size.h"
 #include "base/memory/shared_memory_tracker.h"
