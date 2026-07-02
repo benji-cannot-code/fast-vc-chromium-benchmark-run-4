18637d5dbe04fbb7978d1e805f8c685a872c7444
Kalvin Lee <kdlee@chromium.org>
2026-07-02 01:29:05 -0700

    Checked Span: Opt out `RWBuffer::BufferBlock::Buffer()`
    For context --- Checked Span silently enabled PartitionAlloc bounds
    checks for `UNSAFE_*` constructors of `base::span`. Adding
    `base::unchecked` simply restores the default behavior of not having
    PartitionAlloc do any bounds-checking. There is no impact to your
    business logic.
    
    This method shows up hot in profiles for this test. It's not as hot as
    `SegmentStream::peek()` (looking at Pprof, less than half the total
    regression) but still a noticeable amount.
    
    Bug: 525087502
    Change-Id: I1278bf318912caa5de9a4c4359ca7ab87694e972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8022443
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1655949}

--- third_party/blink/renderer/platform/image-decoders/rw_buffer.h
+++ third_party/blink/renderer/platform/image-decoders/rw_buffer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_IMAGE_DECODERS_RW_BUFFER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_IMAGE_DECODERS_RW_BUFFER_H_
 
+#include "base/containers/span.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
