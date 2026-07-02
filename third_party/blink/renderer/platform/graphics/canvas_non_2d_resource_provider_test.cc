b4c35b77832456d26d33298191ef210579b50426
Colin Blundell <blundell@chromium.org>
2026-07-02 00:08:00 -0700

    [Blink] Refresh references in CanvasNon2DResourceProvider tests
    Bug: 352263194
    Change-Id: Id3e6e85d713e0c88841e06570c5f190ba34c4b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027833
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655933}

--- third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider_test.cc
@@ -145,8 +145,7 @@ TEST_F(CanvasNon2DResourceProviderTest, BeginExternalOverwrite) {
   EXPECT_EQ(client_si_from_second_call, client_si);
 }
 
-TEST_F(CanvasNon2DResourceProviderTest,
-       CanvasResourceProviderAcceleratedOverlay) {
+TEST_F(CanvasNon2DResourceProviderTest, AcceleratedOverlay) {
   const gfx::Size kSize(10, 10);
   const SkImageInfo kInfo =
       SkImageInfo::MakeN32Premul(10, 10, SkColorSpace::MakeSRGB());
@@ -166,9 +165,8 @@ TEST_F(CanvasNon2DResourceProviderTest,
   EXPECT_TRUE(provider && provider->IsValid());
   EXPECT_FALSE(provider->IsSoftware());
   EXPECT_TRUE(provider->IsSingleBuffered());
-  // As it is an CanvasResourceProviderSharedImage and an accelerated canvas, it
-  // will internally force it to RGBA8 on MacOS, or otherwise RGBA8 if not on
-  // Windows
+  // As it is an accelerated canvas, it will internally force it to RGBA8 on
+  // MacOS, or otherwise RGBA8 if not on Windows
 #if BUILDFLAG(IS_MAC)
   EXPECT_TRUE(GetSkImageInfo(provider.get()) ==
               kInfo.makeColorType(kBGRA_8888_SkColorType));
@@ -180,7 +178,7 @@ TEST_F(CanvasNon2DResourceProviderTest,
 #endif
 }
 
-TEST_F(CanvasNon2DResourceProviderTest, CanvasResourceProviderTexture) {
+TEST_F(CanvasNon2DResourceProviderTest, Texture) {
   const gfx::Size kSize(10, 10);
   const SkImageInfo kInfo =
       SkImageInfo::MakeN32Premul(10, 10, SkColorSpace::MakeSRGB());
@@ -197,16 +195,14 @@ TEST_F(CanvasNon2DResourceProviderTest, CanvasResourceProviderTexture) {
   EXPECT_TRUE(provider && provider->IsValid());
   EXPECT_FALSE(provider->IsSoftware());
   EXPECT_FALSE(provider->IsSingleBuffered());
-  // As it is an CanvasResourceProviderSharedImage and an accelerated canvas, it
-  // will internally force it to kRGBA8
+  // As it is an accelerated canvas, it will internally force it to kRGBA8
   EXPECT_EQ(GetSkImageInfo(provider.get()),
             kInfo.makeColorType(kRGBA_8888_SkColorType));
 
   EXPECT_FALSE(provider->IsSingleBuffered());
 }
 
-TEST_F(CanvasNon2DResourceProviderTest,
-       CanvasResourceProviderSharedImageEndExternalWrite) {
+TEST_F(CanvasNon2DResourceProviderTest, EndExternalWrite) {
   // Set up this test to use GPU rasterization to be able to verify
   // conditions against the test raster interface.
   SharedGpuContext::Reset();
@@ -248,8 +244,7 @@ TEST_F(CanvasNon2DResourceProviderTest,
   EXPECT_NE(GetSyncToken(resource.get()), old_compositor_read_sync_token);
 }
 
-TEST_F(CanvasNon2DResourceProviderTest,
-       CanvasResourceProviderSoftwareSharedImage_GPUCompositing) {
+TEST_F(CanvasNon2DResourceProviderTest, SoftwareSharedImage_GPUCompositing) {
   std::unique_ptr<WebGraphicsSharedImageInterfaceProvider>
       test_web_shared_image_interface_provider =
           TestWebGraphicsSharedImageInterfaceProvider::Create();
@@ -261,8 +256,7 @@ TEST_F(CanvasNon2DResourceProviderTest,
       test_web_shared_image_interface_provider.get()));
 }
 
-TEST_F(CanvasNon2DResourceProviderTest,
-       CanvasResourceProviderSoftwareSharedImage_SWCompositing) {
+TEST_F(CanvasNon2DResourceProviderTest, SoftwareSharedImage_SWCompositing) {
   platform_->SetGpuCompositingDisabled(true);
 
   const gfx::Size kSize(10, 10);
@@ -308,9 +302,8 @@ TEST_F(CanvasNon2DResourceProviderTest,
   EXPECT_TRUE(provider && provider->IsValid());
   EXPECT_FALSE(provider->IsSoftware());
   EXPECT_TRUE(provider->IsSingleBuffered());
-  // As it is an CanvasResourceProviderSharedImage and an accelerated canvas, it
-  // will internally force it to RGBA8 on MacOS, or otherwise RGBA8 if not on
-  // Windows
+  // As it is an accelerated canvas, it will internally force it to RGBA8 on
+  // MacOS, or otherwise RGBA8 if not on Windows
 #if BUILDFLAG(IS_MAC)
   EXPECT_TRUE(GetSkImageInfo(provider.get()) ==
               kInfo.makeColorType(kBGRA_8888_SkColorType));
@@ -339,8 +332,8 @@ TEST_F(CanvasNon2DResourceProviderTest,
   provider = CanvasNon2DResourceProvider::Create(
       gfx::Size(kMaxTextureSize + 1, kMaxTextureSize), color_params,
       context_provider_wrapper_, gpu::SharedImageUsageSet());
-  // The CanvasResourceProvider for SharedImage should not be created
-  // if the texture size is greater than the maximum value.
+  // The provider should not be created if the texture size is greater than
+  // the maximum value.
   EXPECT_FALSE(provider);
 }
 
