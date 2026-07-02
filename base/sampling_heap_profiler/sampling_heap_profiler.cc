bdb36b43cadade2c8ebc455ccb49c5d4b49dd202
Kalvin Lee <kdlee@chromium.org>
2026-07-02 14:50:33 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    base/sampling_heap_profiler
    
    This CL was uploaded by git cl split.
    
    R=joenotcharles@google.com
    
    Bug: 529948677
    Change-Id: I69c90b43476574ce9e90f2b39dfa3796162b251c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030343
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656273}

--- base/sampling_heap_profiler/sampling_heap_profiler.cc
+++ base/sampling_heap_profiler/sampling_heap_profiler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/dispatcher/tls.h"
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/containers/to_vector.h"
 #include "base/debug/stack_trace.h"
 #include "base/functional/bind.h"
