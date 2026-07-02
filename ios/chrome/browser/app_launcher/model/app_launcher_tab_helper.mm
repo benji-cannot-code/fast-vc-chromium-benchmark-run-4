9bd2a6d75e563ee5abac63b87597d337983f0779
Quentin Pubert <qpubert@google.com>
2026-07-02 06:30:47 -0700

    [iOS] Disallow cross-origin navigations to all call URL schemes
    This CL updates AppLauncherTabHelper to also disallow navigations to
    call URLs other than tel: e.g. telprompt:, facetime:, facetime-audio:
    from cross-origin frames. These schemes appear to behave the same as
    tel: on iOS so they should be treated the same.
    
    Fixed: 514055709
    Change-Id: I673f00715aaa5f2b20892fa886742dc7166cf47b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8024832
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656056}

--- ios/chrome/browser/app_launcher/model/app_launcher_tab_helper.mm
+++ ios/chrome/browser/app_launcher/model/app_launcher_tab_helper.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/public/web_client.h"
 #import "net/base/apple/url_conversions.h"
 #import "url/gurl.h"
+#import "url/url_constants_ios.h"
 
 namespace {
 
@@ -47,6 +48,16 @@ bool IsValidAppUrl(const GURL& app_url) {
     return false;
   }
   return true;
+}
+
+// Returns true if `url` has a scheme that shows a prompt to initiate a phone or
+// video call.
+bool UrlHasCallWithPromptScheme(const GURL& url) {
+  return url.SchemeIs(url::kTelScheme) || url.SchemeIs(url::kTelPromptScheme) ||
+         url.SchemeIs(url::kFaceTimeScheme) ||
+         url.SchemeIs(url::kFaceTimePromptScheme) ||
+         url.SchemeIs(url::kFaceTimeAudioScheme) ||
+         url.SchemeIs(url::kFaceTimeAudioPromptScheme);
 }
 
 // Returns True if `app_url` has a Chrome bundle URL scheme.
@@ -135,7 +146,7 @@ void AppLauncherTabHelper::RequestToLaunchApp(const GURL& url,
   }
 
   if (!(is_user_initiated ||
-        (url.SchemeIs(url::kTelScheme) && user_tapped_recently))) {
+        (UrlHasCallWithPromptScheme(url) && user_tapped_recently))) {
     ShowAppLaunchAlert(AppLauncherAlertCause::kNoUserInteraction, url);
     return;
   }
@@ -322,8 +333,9 @@ AppLauncherTabHelper::GetPolicyDecisionAndOptionalAppLaunchRequest(
             kNoAppLaunchRequest};
   }
 
-  // Disallow navigations to tel: URLs from cross-origin frames.
-  if (request_url.SchemeIs(url::kTelScheme) &&
+  // Disallow navigations to call or messaging URLs (tel:, telprompt:,
+  // facetime:, facetime-audio:, sms:) from cross-origin frames.
+  if (UrlHasCallWithPromptScheme(request_url) &&
       request_info.target_frame_is_cross_origin) {
     return {PolicyDecision::Cancel(), kNoAppLaunchRequest};
   }
