92a37912fa9552689b0205498400ac9faea3fe32
Federica Germinario <fedegermi@google.com>
2026-07-02 09:31:16 -0700

    [iOS] Clean up ForceExperienceForDeviceSwitcher in egtests
    Remove ForceExperienceForDeviceSwitcher from NSUserDefaults in
    ResetFirstRunRecency to prevent NSUserDefaults test pollution across
    EarlGrey test runs.
    
    Bug: 530520627
    Change-Id: I8483b9d8cbdae4096d8538d611a0401f329993e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035760
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656139}

--- ios/chrome/browser/bubble/ui_bundled/gesture_iph/gesture_in_product_help_view_egtest_utils.mm
+++ ios/chrome/browser/bubble/ui_bundled/gesture_iph/gesture_in_product_help_view_egtest_utils.mm
@@ -36,6 +36,8 @@ void MakeFirstRunRecent() {
 
 void ResetFirstRunRecency() {
   [ChromeEarlGrey removeUserDefaultsObjectForKey:kFirstRunRecencyKey];
+  [ChromeEarlGrey
+      removeUserDefaultsObjectForKey:@"ForceExperienceForDeviceSwitcher"];
 }
 
 void RelaunchWithIPHFeature(NSString* feature, BOOL safari_switcher) {
