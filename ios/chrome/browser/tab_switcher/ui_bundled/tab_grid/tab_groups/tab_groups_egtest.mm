8bfe48df05dc08734c9124cc48eabaad6f0abb4e
Federica Germinario <fedegermi@google.com>
2026-07-02 06:04:15 -0700

    [iOS] Clean up InactiveTabsTestMode in tab_groups_egtest
    Remove InactiveTabsTestMode from NSUserDefaults in tearDownHelper to
    prevent NSUserDefaults test pollution across EarlGrey test runs.
    
    Bug: 530520627
    Change-Id: I43491c04b1c33eb7095e95ad93bf3fe4a82cb368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035121
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656043}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_egtest.mm
@@ -307,6 +307,7 @@ void TapTabGroupTitle() {
 - (void)tearDownHelper {
   [ChromeEarlGrey
       resetDataForLocalStatePref:prefs::kIncognitoAuthenticationSetting];
+  [ChromeEarlGrey removeUserDefaultsObjectForKey:@"InactiveTabsTestMode"];
   [super tearDownHelper];
 }
 
