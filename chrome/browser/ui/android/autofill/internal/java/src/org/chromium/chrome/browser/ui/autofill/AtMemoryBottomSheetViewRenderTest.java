199f3d17f32cc55be7f86636254e18d4ba7bae26
Atalyk Akash <atalyk@google.com>
2026-07-03 03:32:58 -0700

    [AtMemory][Android] Align Flyout chips
    The UX requirement for the chips in the Flyout screen is for chips to
    have the same height in the same row. This change aligns the height of
    chips in the same row.
    
    Screenshot portrait: screen/6zxkcmuGt6fR4cy
    Screenshot landscape: screen/9xn6JfEbJJhTTge
    
    Mock: screen/5J3ufYogmGCDBWh
    Video: screen/AXpYUtkJBqmn3Cg
    Bug: 530106354
    Change-Id: Idedb5fa1021007e42dc70353d86f1e89fbd70fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8028348
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Atalyk Akash <atalyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656470}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetViewRenderTest.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetViewRenderTest.java
@@ -94,6 +94,7 @@ public class AtMemoryBottomSheetViewRenderTest {
     public final RenderTestRule mRenderTestRule =
             RenderTestRule.Builder.withPublicCorpus()
                     .setRevision(4)
+                    .setDescription("Align flyout chip heights in the same row")
                     .setBugComponent(Component.UI_BROWSER_AUTOFILL)
                     .build();
 
