e2cebeb1cad80d0138458b444188fd5972db1c9a
Angela Novakovic <novakovica@google.com>
2026-07-03 05:47:51 -0700

    [iOS][AtMemory] Add and map new custom symbols for Ai Autofill
    This CL adds new custom symbols and updates the
    DefaultIconForAutofillAIEntityType to map to the correct icon.
    
    The Gemini-branded custom symbols that were added:
    1) passport.spark
    2) car.spark
    3) person.text.rectangle.spark
    4) person.text.rectangle.2.spark
    5) location.spark
    
    The non-Gemini-branded custom symbols that were added:
    1) person.text.rectangle.2
    2) airplane.up
    
    The default symbol that was update:
    1) person.text.rectangle (replaced person.fill.checkmark)
    
    Bug: 523320919
    Change-Id: I22dc4c4acf662a1770fc22faf3f4c82efb1a6242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027720
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656521}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.h
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.h
@@ -63,6 +63,7 @@ extern NSString* const kReadingListSymbol;
 extern NSString* const kRecentTabsSymbol;
 extern NSString* const kLanguageSymbol;
 extern NSString* const kPassportSymbol;
+extern NSString* const kPassportSparkSymbol;
 extern NSString* const kPasswordSymbol;
 #if !BUILDFLAG(IS_IOS_MACCATALYST)
 extern NSString* const kMulticolorPasswordSymbol;
@@ -97,6 +98,12 @@ extern NSString* const kLineThreeSparkSymbol;
 extern NSString* const kDocumentBadgeSpark;
 extern NSString* const kBagSparkSymbol;
 extern NSString* const kTruckBoxSparkSymbol;
+extern NSString* const kAirplaneUpSymbol;
+extern NSString* const kCarSparkSymbol;
+extern NSString* const kLocationSparkSymbol;
+extern NSString* const kPersonTextRectangle2Symbol;
+extern NSString* const kPersonTextRectangle2SparkSymbol;
+extern NSString* const kPersonTextRectangleSparkSymbol;
 extern NSString* const kDeepSearchSymbol;
 
 // Use custom symbol for camera because the default video icon in iOS should
@@ -161,8 +168,6 @@ extern NSString* const kSaveImageActionSymbol;
 extern NSString* const kOpenImageActionSymbol;
 extern NSString* const kQRCodeSymbol;
 extern NSString* const kPrinterSymbol;
-extern NSString* const kAirplaneSymbol;
-extern NSString* const kAirplaneUpRightSymbol API_AVAILABLE(ios(26.0));
 extern NSString* const kSuitcaseSymbol;
 extern NSString* const kCarSymbol;
 extern NSString* const kCreditCardSymbol;
@@ -213,7 +218,6 @@ extern NSString* const kExpandSymbol;
 extern NSString* const kBackSymbol;
 extern NSString* const kForwardSymbol;
 extern NSString* const kPersonFillSymbol;
-extern NSString* const kPersonFillCheckmarkSymbol;
 extern NSString* const kPersonTextRectangleSymbol;
 extern NSString* const kPersonBadgeKeyFillSymbol;
 extern NSString* const kPersonClockFillSymbol;
