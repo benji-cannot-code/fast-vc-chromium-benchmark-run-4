67cc5d8ea9698946129187fbdce5f4bddeb78e2f
Xi Han <hanxi@google.com>
2026-07-03 12:43:25 -0700

    [ThemeSync] Add tests for Supporting multiple theme collection images in history.
    This is a follow up CL for https://crrev.com/c/8020724, and add tests
    for that CL.
    
    Bug: 488439751
    Change-Id: I31182f11d125cf7449fa49bfb98ddc6806303072
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8038704
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656719}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/theme_collections/NtpThemeCollectionManagerUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/theme/theme_collections/NtpThemeCollectionManagerUnitTest.java
@@ -46,6 +46,7 @@ import org.chromium.components.image_fetcher.ImageFetcher;
 import org.chromium.url.GURL;
 import org.chromium.url.JUnitTestGURLs;
 
+import java.io.File;
 import java.util.List;
 
 /** Unit tests for {@link NtpThemeCollectionManager}. */
@@ -451,6 +452,19 @@ public class NtpThemeCollectionManagerUnitTest {
         mNtpThemeCollectionManager.onCustomBackgroundImageUpdated(info);
 
         verifyThemeUpdateOutcome(info, /* shouldUpdateTheme= */ false);
+    }
+
+    @Test
+    public void testGetFileName() {
+        String expectedFileName = "image.jpg";
+        assertNull(NtpThemeCollectionManager.getFileName(/* path= */ null));
+        assertNull(NtpThemeCollectionManager.getFileName(""));
+        assertEquals(expectedFileName, NtpThemeCollectionManager.getFileName(expectedFileName));
+        assertEquals(expectedFileName, NtpThemeCollectionManager.getFileName("/path/to/image.jpg"));
+        assertEquals(
+                expectedFileName,
+                NtpThemeCollectionManager.getFileName(
+                        "path" + File.separator + "to" + File.separator + "image.jpg"));
     }
 
     private CustomBackgroundInfo createBackgroundInfo(boolean isDailyRefresh) {
