c255cda84777bf24529186412ebf311a6dc6433c
Simge Şengün <simges@google.com>
2026-07-03 05:32:43 -0700

    Add OptimizationGuide support in MultistepFilterServiceFactory
    This change introduces a conditional path to create an
    OptimizationGuideAnnotationIndexClient instead of a
    NetworkAnnotationIndexClient within the MultistepFilterServiceFactory.
    The new path is controlled by a new
    `multistep-filter-optimization-guide` switch
    
    Fixed: 522752358
    Change-Id: Iede867537da53aa2fc04250a5f44bb49231d09fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8031423
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Simge Şengün <simges@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656512}

--- chrome/browser/multistep_filter/core/multistep_filter_service_factory.cc
+++ chrome/browser/multistep_filter/core/multistep_filter_service_factory.cc
@@ -4,10 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chrome/browser/multistep_filter/core/multistep_filter_service_factory.h"
 
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/multistep_filter/core/multistep_filter_log_router_factory.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
@@ -15,11 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/multistep_filter/core/annotation_index/annotation_index_client.h"
 #include "components/multistep_filter/core/annotation_index/network_annotation_index_client.h"
+#include "components/multistep_filter/core/annotation_index/optimization_guide_annotation_index_client.h"
 #include "components/multistep_filter/core/extraction/filter_extractor.h"
 #include "components/multistep_filter/core/features.h"
 #include "components/multistep_filter/core/multistep_filter_service.h"
 #include "components/multistep_filter/core/prefs/multistep_filter_retention_prefs.h"
 #include "components/multistep_filter/core/storage/filter_store.h"
+#include "components/multistep_filter/core/switches.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/unified_consent/url_keyed_data_collection_consent_helper.h"
@@ -46,6 +51,7 @@ MultistepFilterServiceFactory::MultistepFilterServiceFactory()
   DependsOn(HistoryServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(MultistepFilterLogRouterFactory::GetInstance());
+  DependsOn(OptimizationGuideKeyedServiceFactory::GetInstance());
   DependsOn(SyncServiceFactory::GetInstance());
 }
 
@@ -64,11 +70,18 @@ MultistepFilterServiceFactory::BuildServiceInstanceForBrowserContext(
   MultistepFilterLogRouter* log_router =
       MultistepFilterLogRouterFactory::GetForProfile(profile);
 
-  std::unique_ptr<NetworkAnnotationIndexClient> annotation_index_client =
-      NetworkAnnotationIndexClient::Create(
-          context->GetDefaultStoragePartition()
-              ->GetURLLoaderFactoryForBrowserProcess(),
-          identity_manager, log_router);
+  std::unique_ptr<AnnotationIndexClient> annotation_index_client;
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kMultistepFilterOptimizationGuide)) {
+    annotation_index_client = OptimizationGuideAnnotationIndexClient::Create(
+        OptimizationGuideKeyedServiceFactory::GetForProfile(profile),
+        log_router);
+  } else {
+    annotation_index_client = NetworkAnnotationIndexClient::Create(
+        context->GetDefaultStoragePartition()
+            ->GetURLLoaderFactoryForBrowserProcess(),
+        identity_manager, log_router);
+  }
 
   MultistepFilterService::Params params;
   params.annotation_index_client = std::move(annotation_index_client);
