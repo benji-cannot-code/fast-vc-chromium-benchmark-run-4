20dffd8c45058441d750720ea8432cb8409ee85e
Łukasz Barczyński <lbarczynski@google.com>
2026-07-03 10:28:46 -0700

    [Android] Remove ChromeBrowserTestRule in the site settings tests
    ChromeBrowserTestRule was used in these tests only to initialize native.
    Replacing it with just the initialization to reduce the usage of
    ChromeBrowserTestRule, which is just an adapter for SigninTestRule
    without any specific logic.
    
    This CL is part of the fixit week effort and part of a bigger chain. The
    goal is to completely remove ChromeBrowserTestRule from the source,
    because it's redundant.
    
    Bug: 499170957
    Change-Id: Ic3c58f9cf70ebd04fe6a37116f00d4c2855e14a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7927995
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Łukasz Barczyński <lbarczynski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656653}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/WebsitePreferenceBridgeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/WebsitePreferenceBridgeTest.java
@@ -13,7 +13,7 @@ import androidx.test.filters.SmallTest;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -32,13 +32,13 @@ import org.chromium.chrome.browser.browsing_data.BrowsingDataType;
 import org.chromium.chrome.browser.browsing_data.TimePeriod;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.profiles.ProfileManager;
-import org.chromium.chrome.test.ChromeBrowserTestRule;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
 import org.chromium.components.content_settings.ContentSetting;
 import org.chromium.components.content_settings.ContentSettingsType;
 import org.chromium.components.permissions.PermissionsAndroidFeatureList;
 import org.chromium.content_public.browser.BrowserContextHandle;
+import org.chromium.content_public.browser.test.NativeLibraryTestUtils;
 import org.chromium.url.GURL;
 
 import java.util.Arrays;
@@ -51,8 +51,6 @@ import java.util.concurrent.TimeoutException;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Batch(Batch.PER_CLASS)
 public class WebsitePreferenceBridgeTest {
-    @Rule public final ChromeBrowserTestRule mBrowserTestRule = new ChromeBrowserTestRule();
-
     /** Class to parameterize the params for {@link }. */
     public static class EmbargoedParams implements ParameterProvider {
         @Override
@@ -61,6 +59,11 @@ public class WebsitePreferenceBridgeTest {
                     new ParameterSet().value(true).name("Embargoed"),
                     new ParameterSet().value(false).name("Normal"));
         }
+    }
+
+    @Before
+    public void setUp() {
+        NativeLibraryTestUtils.loadNativeLibraryAndInitBrowserProcess();
     }
 
     @After
