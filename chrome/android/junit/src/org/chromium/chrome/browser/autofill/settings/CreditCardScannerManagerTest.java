6187cb5f275335dee39cd54640d6d580f835726f
Gazal <agazal@google.com>
2026-07-03 13:40:08 -0700

    [code-health] Replace FieldType enum in CreditCardScannerManager
    This CL replaces the FieldType enum in CreditCardScannerManager
    with a @IntDef annotated @interface. Enums in Android/Java
    introduce runtime memory overhead due to class loading and
    allocation of enum objects. Using @IntDef provides lightweight
    integer constants while retaining compile-time type safety.
    The member field Set<FieldType> mFieldsFilledByScanner is
    converted to Set<Integer> to avoid enum overhead.
    
    BUG: 528754503,530611790
    Change-Id: Ic65a161173d2a51a51beeee75d9f311dddc1fd38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036071
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656739}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/CreditCardScannerManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/CreditCardScannerManagerTest.java
@@ -192,7 +192,7 @@ public class CreditCardScannerManagerTest {
     public void onFieldEditToScannedField() {
         CreditCardScannerManager manager = new CreditCardScannerManager(mDelegate);
         manager.setScanResultForTesting(ScanResult.SCANNED_WITHOUT_ADDITIONAL_USER_EDITS);
-        FieldType field = FieldType.NUMBER;
+        @FieldType int field = FieldType.NUMBER;
         manager.getFieldsFilledByScannerForTesting().add(field);
 
         manager.fieldEdited(field);
@@ -273,7 +273,7 @@ public class CreditCardScannerManagerTest {
         manager.onScanCompleted(
                 /* cardholderName= */ "", cardNumber, /* expirationMonth= */ 0, expirationYear);
 
-        Set<FieldType> fieldTypes = manager.getFieldsFilledByScannerForTesting();
+        Set<@FieldType Integer> fieldTypes = manager.getFieldsFilledByScannerForTesting();
 
         // Only fields populated with real values (i.e. card number and expiration year) should be
         // in the set as fields filled by the scanner.
