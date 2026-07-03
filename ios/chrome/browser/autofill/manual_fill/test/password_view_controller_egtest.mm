874c5b627d3c778a18189f8dcf55e33d741d1552
Rafał Godlewski <rgod@google.com>
2026-07-03 04:13:53 -0700

    [iOS] Partially re-enable some password controller tests
    * Ensures `kIosSyncInfobarErrorLastDismissedTimestamp` is cleared
    before adding any expectations on sync error infobar, as it has 24h
    timeout, effectively making it not appear a lot
    * Additionally, makes swiping up the infobar conditional, as it can
    get dismissed automatically if app takes long to idle
    
    Those tests are still disabled on device, but that failure was for
    a different reason, for which I don't see logs anymore.
    
    I'll double check first if this fix resolves the infobar expectation
    issue and then will consider re-enabling those tests on device, to see
    what is happening.
    
    Fixed: 455768802,454547779
    Change-Id: I53f672923ba7fd97853e23661282b6593ce5dc18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8024716
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656484}

--- ios/chrome/browser/autofill/manual_fill/test/password_view_controller_egtest.mm
+++ ios/chrome/browser/autofill/manual_fill/test/password_view_controller_egtest.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/password/password_details/password_details_table_view_constants.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/password_manager_egtest_utils.h"
 #import "ios/chrome/browser/settings/ui_bundled/password/passwords_table_view_constants.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
@@ -288,6 +289,9 @@ void CheckKeyboardIsUpAndNotCovered() {
   [MetricsAppInterface overrideMetricsAndCrashReportingForTesting];
   chrome_test_util::GREYAssertErrorNil(
       [MetricsAppInterface setupUserActionTester]);
+
+  [ChromeEarlGrey
+      clearUserPrefWithName:prefs::kIosSyncInfobarErrorLastDismissedTimestamp];
 }
 
 - (void)tearDownHelper {
@@ -1066,11 +1070,6 @@ void CheckKeyboardIsUpAndNotCovered() {
   DISABLED_testPasswordGenerationFallbackSignedInEncryptionError
 #endif
 - (void)MAYBE_testPasswordGenerationFallbackSignedInEncryptionError {
-  // TODO(crbug.com/455768802): Re-enable the test.
-  if (@available(iOS 26.1, *)) {
-    EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.1.");
-  }
-
   // Encrypt synced data with a passphrase to enable passphrase encryption for
   // the signed in account.
   [ChromeEarlGrey addSyncPassphrase:kPassphrase];
@@ -1089,10 +1088,17 @@ void CheckKeyboardIsUpAndNotCovered() {
 
   [self loadLoginPage];
 
-  // Swipe up the sync infobar error.
+  // Swipe up the sync infobar error if it is visible.
+  NSError* error = nil;
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
                                           kInfobarBannerViewIdentifier)]
-      performAction:grey_swipeFastInDirection(kGREYDirectionUp)];
+      assertWithMatcher:grey_sufficientlyVisible()
+                  error:&error];
+  if (!error) {
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                            kInfobarBannerViewIdentifier)]
+        performAction:grey_swipeFastInDirection(kGREYDirectionUp)];
+  }
 
   // Bring up the keyboard.
   [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
