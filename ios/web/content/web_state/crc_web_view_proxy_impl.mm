147225126aa197382b99b5c8c42700da82189d83
xiaobincui <xiaobincui@microsoft.com>
2026-07-02 20:42:29 -0700

    [ios blink] Fix web contents overlapped by browser toolbars
    The fullscreen mediator sets obscuredInsets on the web view proxy to
    communicate toolbar positions, but CRCWebViewProxyImpl dropped them
    silently. The RWHV also ignored the parent scroll view's contentInset
    when computing its frame, rendering at full screen size behind the
    browser chrome.
    
    Bug: 526182983
    Change-Id: I20f4750d91fd483463e6832811cb90b80f8f0194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7977340
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@google.com>
    Commit-Queue: Xiaobin Cui <xiaobincui@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1656342}

--- ios/web/content/web_state/crc_web_view_proxy_impl.mm
+++ ios/web/content/web_state/crc_web_view_proxy_impl.mm
@@ -122,10 +122,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (UIEdgeInsets)obscuredInsets {
-  return UIEdgeInsetsZero;
+  return _contentView.contentInset;
 }
 
 - (void)setObscuredInsets:(UIEdgeInsets)obscuredInsets {
+  _contentView.contentInset = obscuredInsets;
 }
 
 - (void)setMinimumViewportInset:(UIEdgeInsets)minInset
