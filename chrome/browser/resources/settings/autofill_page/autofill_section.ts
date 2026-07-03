856fa50eb6f868d552c270cc0c09c2548eab78fb
Stephan Drab <sdrab@google.com>
2026-07-03 00:08:42 -0700

    Add "learn more" link to the gmail otp preference toggle description.
    - http://screen/98FNP83MC5YtXNA
    - http://go/scrcast/NTU5MjM2NDkwMTk5MDQwMHwxZDU2N2IxYy02OQ
    
    The link is currently a placeholder, it will be updated in an upcoming
    change
    
    Bug: b:484907871
    Change-Id: Ibc2d7e201c01f6415faf1e1874d4fa0815c002c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7992617
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Trang Mai <trangm@google.com>
    Commit-Queue: Stephan Drab <sdrab@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656389}

--- chrome/browser/resources/settings/autofill_page/autofill_section.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_section.ts
@@ -631,6 +631,11 @@ export class SettingsAutofillSectionElement extends
         loadTimeData.getString('plusAddressManagementUrl'));
   }
 
+  private onGmailOtpFillingLinkClick_() {
+    OpenWindowProxyImpl.getInstance().openUrl(
+        loadTimeData.getString('gmailOtpFillingLearnMoreUrl'));
+  }
+
   // SettingsViewMixin implementation.
   override focusBackButton() {
     this.shadowRoot!.querySelector('settings-subpage')!.focusBackButton();
