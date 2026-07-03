9c5824648e3e02dc2c29e0c2443c96eea8e04258
Colin Blundell <blundell@chromium.org>
2026-07-02 23:51:09 -0700

    [Blink] Update outdated references to CanvasResourceProvider in comments
    Bug: 352263194
    Change-Id: I111cd32692fa4e14dcb80548b2e36fd828fb3804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035223
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656380}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_resource_provider_cache.h
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_resource_provider_cache.h
@@ -73,7 +73,7 @@ class PLATFORM_EXPORT WebGPURecyclableResourceCache {
   }
 
  private:
-  // The maximum number of unused CanvasResourceProviders size, 128 MB.
+  // The maximum number of unused CanvasNon2DResourceProviders size, 128 MB.
   static constexpr int kMaxRecyclableResourceCachesInKB = 128 * 1024;
   static constexpr int kMaxRecyclableResourceCachesInBytes =
       kMaxRecyclableResourceCachesInKB * 1024;
@@ -121,7 +121,7 @@ class PLATFORM_EXPORT WebGPURecyclableResourceCache {
   // Start the clean-up function runs when there are unused resources.
   void StartResourceCleanUpTimer();
 
-  // This is the place to keep the unused CanvasResourceProviders. They are
+  // This is the place to keep the unused CanvasNon2DResourceProviders. They are
   // waiting to be used. MRU is in the front of the deque.
   DequeResourceProvider unused_providers_;
 
