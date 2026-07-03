fd804044a80852a8a16ca07c622ea7261b1e4dc8
Nina Satragno <nsatragno@google.com>
2026-07-03 12:41:36 -0700

    [webauthn] Fix credBlob JSON serialization bug
    Fix a in the credBlob extension JSON serialization where Blink
    incorrectly invoked the getCredBlob buffer getter instead of the boolean
    credBlob getter, triggering a DCHECK crash on the creation path.
    
    TAG=agy
    CONV=2fa9806d-3571-474e-a097-501b66df7c6b
    
    Fixed: 530457807
    Change-Id: I7f8ef599fb85237e2b935a231ff96d8eab76a4cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036924
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656717}

--- third_party/blink/renderer/modules/credentialmanagement/json.cc
+++ third_party/blink/renderer/modules/credentialmanagement/json.cc
@@ -280,7 +280,7 @@ AuthenticationExtensionsClientOutputsToJSON(
     json->setLargeBlob(builder.ToScriptObject());
   }
   if (in.hasCredBlob()) {
-    json->setCredBlob(in.getCredBlob());
+    json->setCredBlob(in.credBlob());
   }
   if (in.hasGetCredBlob()) {
     json->setGetCredBlob(WebAuthnBase64UrlEncode(in.getCredBlob()));
