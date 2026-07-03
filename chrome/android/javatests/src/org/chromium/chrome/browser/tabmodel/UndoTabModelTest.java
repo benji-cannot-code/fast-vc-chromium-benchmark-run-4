8f4e659b7addff7391a94289211d5b293b83726e
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-03 11:01:14 -0700

    [TabModel] Improve tab index lookup
    Bug: 525079251
    Change-Id: Ifcf977e33a20b8112dc86836588df7e15f0766f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8037120
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656684}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/UndoTabModelTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/UndoTabModelTest.java
@@ -592,9 +592,7 @@ public class UndoTabModelTest {
     private void selectTab(final TabModel model, final Tab tab) {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    model.setIndex(
-                            TabModelUtils.getTabIndexById(model, tab.getId()),
-                            TabSelectionType.FROM_USER);
+                    model.setIndex(model.indexOf(tab), TabSelectionType.FROM_USER);
                 });
     }
 
