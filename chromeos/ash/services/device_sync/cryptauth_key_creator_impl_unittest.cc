7e718651395bc17884ba375be7b42d5faee17fd2
Elly <ellyjones@chromium.org>
2026-07-03 10:40:37 -0700

    cros/device-sync: use crypto/kdf in unit tests
    This is a followup to r1650389, migrating the unit tests as well. The
    new API is a bit bulkier here since device-sync uses strings to
    represent keys and other cryptographic material, but so it goes.
    
    Bug: 430616377
    Change-Id: I3676d7369b35f56b7e9d0e50b65749eb93b4a82a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8021347
    Auto-Submit: Elly <ellyjones@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656673}

--- chromeos/ash/services/device_sync/cryptauth_key_creator_impl_unittest.cc
+++ chromeos/ash/services/device_sync/cryptauth_key_creator_impl_unittest.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/device_sync/cryptauth_key_bundle.h"
 #include "chromeos/ash/services/device_sync/cryptauth_key_creator.h"
 #include "chromeos/ash/services/device_sync/proto/cryptauth_common.pb.h"
-#include "crypto/hkdf.h"
+#include "crypto/kdf.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
@@ -119,10 +119,15 @@ class DeviceSyncCryptAuthKeyCreatorImplTest : public testing::Test {
     std::string expected_handshake_secret =
         DeriveSecret(fake_server_ephemeral_dh_, client_ephemeral_dh);
 
-    std::string expected_symmetric_key_material = crypto::HkdfSha256(
-        expected_handshake_secret, kCryptAuthSymmetricKeyDerivationSalt,
-        CryptAuthKeyBundle::KeyBundleNameEnumToString(bundle_name),
-        NumBytesForSymmetricKeyType(key_to_create.type));
+    std::string expected_symmetric_key_material(
+        NumBytesForSymmetricKeyType(key_to_create.type), '\0');
+    crypto::kdf::Hkdf(
+        crypto::hash::kSha256, base::as_byte_span(expected_handshake_secret),
+        base::as_byte_span(
+            std::string_view(kCryptAuthSymmetricKeyDerivationSalt)),
+        base::as_byte_span(
+            CryptAuthKeyBundle::KeyBundleNameEnumToString(bundle_name)),
+        base::as_writable_byte_span(expected_symmetric_key_material));
 
     return CryptAuthKey(expected_symmetric_key_material, key_to_create.status,
                         key_to_create.type, key_to_create.handle);
