41b25e0bfbe34b0eb92d68e768a4354ac73a0d2b
Yanling <yanlinghu@google.com>
2026-07-03 13:02:24 -0700

    [NtpVision] Add feature flag
    The feature flag is added as in the screenshot:
    https://drive.google.com/file/d/1WybavzbQz7K4O9KWZ_f8XxU_fHCcyrJi/view?usp=sharing&resourcekey=0-6LjA_hcoupRcooTc_KsRBA
    
    Bug: 
    Change-Id: I2e0eda5a8551d530b56184841428f75399d0c7d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032896
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656724}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -5510,6 +5510,10 @@ inline constexpr char kLongScreenshotsNoMemoryCheckName[] =
 inline constexpr char kLongScreenshotsNoMemoryCheckDescription[] =
     "Bypass memory pressure checks for long screenshots.";
 
+inline constexpr char kNtpVisionName[] = "New tab page vision";
+inline constexpr char kNtpVisionDescription[] =
+    "Enable the new vision of the new tab page";
+
 inline constexpr char kUpdatePaddingForDisplayCalculationName[] =
     "Update Padding For Display Calculation";
 inline constexpr char kUpdatePaddingForDisplayCalculationDescription[] =
