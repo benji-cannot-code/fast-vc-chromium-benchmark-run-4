0197753d6f3a5f580fadb0250202fcd3cf1941b8
Kunal Daftari <kunaldaftari@google.com>
2026-07-06 16:21:42 -0700

    [BAM] Migrating 2 Bookmarks Commands
    kActionBookmarkBarRedo, kActionBookmarkBarUndo
    
    Bug: 525118424
    Change-Id: I2dfa7d8a2e8fa60a69d3177cbc44ed356b4d77d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8013185
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657565}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -161,6 +161,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webid/account_selection_view.h"
 #include "chrome/browser/ui/webui/inspect/inspect_ui.h"
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_section.h"
+#include "chrome/browser/undo/bookmark_undo_service_factory.h"
 #include "chrome/browser/web_applications/web_app_install_params.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/common/chrome_features.h"
@@ -198,6 +199,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/split_tabs/split_tab_visual_data.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/tabs/public/tab_interface.h"
+#include "components/undo/bookmark_undo_service.h"
 #include "content/public/common/profiling.h"
 #include "extensions/common/extension_urls.h"
 #if BUILDFLAG(IS_CHROMEOS)
@@ -3000,6 +3002,32 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
               },
               bwi))
           .SetActionId(kActionBookmarkBarShowManagedBookmarks)
+          .Build());
+
+  root_action_item_->AddChild(
+      actions::ActionItem::Builder(
+          base::BindRepeating(
+              [](BrowserWindowInterface* bwi, actions::ActionItem* item,
+                 actions::ActionInvocationContext context) {
+                BookmarkUndoServiceFactory::GetForProfile(bwi->GetProfile())
+                    ->undo_manager()
+                    ->Undo();
+              },
+              bwi))
+          .SetActionId(kActionBookmarkBarUndo)
+          .Build());
+
+  root_action_item_->AddChild(
+      actions::ActionItem::Builder(
+          base::BindRepeating(
+              [](BrowserWindowInterface* bwi, actions::ActionItem* item,
+                 actions::ActionInvocationContext context) {
+                BookmarkUndoServiceFactory::GetForProfile(bwi->GetProfile())
+                    ->undo_manager()
+                    ->Redo();
+              },
+              bwi))
+          .SetActionId(kActionBookmarkBarRedo)
           .Build());
 
   root_action_item_->AddChild(
