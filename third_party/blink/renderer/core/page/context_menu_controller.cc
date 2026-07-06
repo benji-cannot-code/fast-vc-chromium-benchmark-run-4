3d7c18904de4d560ef573dd044079a7a639f84f3
tannal <mtan@igalia.com>
2026-07-06 10:08:11 -0700

    [mathml] Implement download attribute for MathML a element
    Bug: 510487697
    Change-Id: Idc522f1a7094ea0671fd740e1396c5cad047092a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7969459
    Reviewed-by: Frédéric Wang Nélar <fwang@igalia.com>
    Commit-Queue: Frédéric Wang Nélar <fwang@igalia.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657276}

--- third_party/blink/renderer/core/page/context_menu_controller.cc
+++ third_party/blink/renderer/core/page/context_menu_controller.cc
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/input_type_names.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
+#include "third_party/blink/renderer/core/mathml/mathml_anchor_element.h"
 #include "third_party/blink/renderer/core/page/context_menu_provider.h"
 #include "third_party/blink/renderer/core/page/focus_controller.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -174,8 +175,8 @@ uint32_t EnumToBitmask(enumType outcome) {
 }
 
 // Populates context menu data common to all anchor element types (HTML <a>,
-// SVG <a>, etc.): suggested download filename, referrer policy suppression,
-// and link text.
+// SVG <a>, MathML <a> etc.): suggested download filename, referrer policy
+// suppression, and link text.
 template <typename AnchorType>
 void PopulateAnchorContextMenuData(AnchorType* anchor,
                                    const QualifiedName& download_attr,
@@ -829,6 +830,10 @@ bool ContextMenuController::ShowContextMenu(
                                   selected_frame, data);
   } else if (auto* svg_anchor = DynamicTo<SVGAElement>(result.URLElement())) {
     PopulateAnchorContextMenuData(svg_anchor, svg_names::kDownloadAttr,
+                                  selected_frame, data);
+  } else if (auto* mathml_anchor =
+                 DynamicTo<MathMLAnchorElement>(result.URLElement())) {
+    PopulateAnchorContextMenuData(mathml_anchor, html_names::kDownloadAttr,
                                   selected_frame, data);
   }
 
