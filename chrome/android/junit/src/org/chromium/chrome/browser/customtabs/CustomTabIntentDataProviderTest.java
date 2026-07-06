32be2725b1c125a64378b6b51793f4afac1a4221
Evan Luo <evanluo@google.com>
2026-07-06 15:44:06 -0700

    [reader mode] Clean up reader mode custom tab launch logic
    With distill-in-app enabled, reader mode no longer launches into Custom
    Tab UI type of READER_MODE, this aims to clean up all the CCT launch
    logic and handling for reader mode.
    
    Bug: 509854468
    Change-Id: I79fb2ecf1edc8442915917fae0a2ae169b14e4df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7952699
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657529}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProviderTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProviderTest.java
@@ -2610,7 +2610,6 @@ public class CustomTabIntentDataProviderTest {
         assertEquals(stateOff, getOibStateForType(CustomTabsUiType.AUTH_TAB));
         assertEquals(stateOff, getOibStateForType(CustomTabsUiType.MEDIA_VIEWER));
         assertEquals(stateOff, getOibStateForType(CustomTabsUiType.POPUP));
-        assertEquals(stateOff, getOibStateForType(CustomTabsUiType.READER_MODE));
         assertEquals(stateOff, getOibStateForType(CustomTabsUiType.OFFLINE_PAGE));
     }
 
