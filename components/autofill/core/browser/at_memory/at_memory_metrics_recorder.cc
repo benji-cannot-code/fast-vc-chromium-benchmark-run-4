85cc415686ce93176d9b7520a04b20840cf02ec6
Jan Keitel <jkeitel@google.com>
2026-07-06 04:58:26 -0700

    Avoid copy in AtMemoryRecorder::OnQuerySubmitted
    Bug: 40100455
    Change-Id: I8bcdcb85552d98242eb96e5d65d4a0c28a86b3cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8037811
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657110}

--- components/autofill/core/browser/at_memory/at_memory_metrics_recorder.cc
+++ components/autofill/core/browser/at_memory/at_memory_metrics_recorder.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/browser/at_memory/at_memory_metrics_recorder.h"
 
+#include <optional>
+#include <string_view>
+#include <utility>
+
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/strings/utf_string_conversions.h"
@@ -120,7 +124,7 @@ void AtMemoryMetricsRecorder::OnPopupShown(
                                 *source_);
 }
 
-void AtMemoryMetricsRecorder::OnQuerySubmitted(std::u16string query) {
+void AtMemoryMetricsRecorder::OnQuerySubmitted(std::u16string_view query) {
   MaybeLogSuggestionAccepted();
   suggestion_accepted_ = false;
   ++query_count_;
--- components/autofill/core/browser/at_memory/at_memory_metrics_recorder.h
+++ components/autofill/core/browser/at_memory/at_memory_metrics_recorder.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <string>
+#include <string_view>
 
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
@@ -74,7 +75,7 @@ class AtMemoryMetricsRecorder {
   void OnPopupShown(AutofillSuggestionTriggerSource trigger_source);
 
   // Records that a search query was submitted during this session.
-  void OnQuerySubmitted(std::u16string query);
+  void OnQuerySubmitted(std::u16string_view query);
 
   // Records that a response for the pending query was received.
   void OnQueryResponseReceived(
