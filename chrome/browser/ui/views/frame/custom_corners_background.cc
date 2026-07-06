8be1056d8fca002c6f39c1331e971cd4b364fdb3
Dana Fried <dfried@chromium.org>
2026-07-06 11:34:58 -0700

    [Glass] Clip glass EoH effect to tab strip bounds
    This renames the feature params to be more general, moves the logic to
    CustomCornersBackground, and ensures that the background effect is
    clipped to the same bounds as the background.
    
    Bug: 526614803
    Change-Id: Iaa8687df8a1179134af520a56e67dd6caab9bbf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8040743
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657342}

--- chrome/browser/ui/views/frame/custom_corners_background.cc
+++ chrome/browser/ui/views/frame/custom_corners_background.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/themed_background.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkPathBuilder.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_variant.h"
 #include "ui/compositor/layer.h"
@@ -138,6 +139,28 @@ CustomCornersBackground::Corner CustomCornersBackground::GetWindowCorner(
     }
   }
   return corner;
+}
+
+void CustomCornersBackground::SetUseBackgroundBlur(bool use_background_blur) {
+  static const bool background_blur_enabled =
+      features::kBackgroundBlurOpacity.Get() < 1.0f;
+  static const float background_blur_radius =
+      features::kBackgroundBlurBlurRadius.Get();
+  use_background_blur &=
+      background_blur_enabled && background_blur_radius > 0.0f;
+  if (!view_->layer()) {
+    CHECK(!use_background_blur);
+    return;
+  }
+  auto* const layer = view_->layer();
+  CHECK(!layer->fills_bounds_opaquely());
+  if (!use_background_blur) {
+    layer->SetBackgroundBlur(0.0f);
+    return;
+  }
+  layer->SetBackgroundBlur(background_blur_radius);
+  layer->SetBackdropFilterBounds(GetBackgroundPath(view_->GetLocalBounds()));
+  SchedulePaintHost();
 }
 
 SkPath CustomCornersBackground::GetBackgroundPath(const gfx::Rect& in_bounds,
