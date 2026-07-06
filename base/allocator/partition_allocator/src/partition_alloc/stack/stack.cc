f403014cc20d715f35fd476e035d0e802d2c295e
Egor Pasko <pasko@chromium.org>
2026-07-06 16:20:11 -0700

    PartitionAlloc: Restore StackTopRegistry::GetCurrentThreadStackTop
    The StackTopRegistry::GetCurrentThreadStackTop() is soon going to be
    used for MiracleObject, requiring stack scanning. Revert it to the
    original form (as before crrev.com/1657146). Previously I replaced the
    body of this method with PA_NOTREACHED because I mistakenly assumed no
    usage is approaching.
    
    Bug: 518605818, 530922114
    Change-Id: Idb485293117a055d09f19635fbe9bdf8dba05d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8044903
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657562}

--- base/allocator/partition_allocator/src/partition_alloc/stack/stack.cc
+++ base/allocator/partition_allocator/src/partition_alloc/stack/stack.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/buildflags.h"
 #include "partition_alloc/internal/partition_root_internal.h"
 #include "partition_alloc/partition_alloc_base/compiler_specific.h"
-#include "partition_alloc/partition_alloc_base/notreached.h"
 #include "partition_alloc/partition_alloc_check.h"
 
 #if PA_BUILDFLAG(IS_WIN)
@@ -174,11 +173,11 @@ void StackTopRegistry::NotifyThreadDestroyed() {
   stack_tops_.erase(tid);
 }
 
-// TODO(crbug.com/530922114): Remove StackTopRegistry.
 void* StackTopRegistry::GetCurrentThreadStackTop() const {
-  // The registry contains approximate stack top pointer. This reduces stack
-  // scan ranges. Protect this method from accidental use for security checks.
-  PA_NOTREACHED();
+  const auto tid = base::PlatformThread::CurrentId();
+  ScopedGuard guard(lock_);
+  auto it = stack_tops_.find(tid);
+  return it != stack_tops_.end() ? it->second : nullptr;
 }
 
 }  // namespace partition_alloc::internal
