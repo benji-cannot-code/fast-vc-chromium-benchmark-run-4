b527b9527ed6d6dc9eff8cad7768ae0dd4cb2a27
Jakub Kudzia <jkudzia@google.com>
2026-07-06 12:03:19 -0700

    Fix naming inconsistency for url in recent_pages field in ContextualCueLogEvent.proto
    Renamed JSON field 'page' to 'url' in recent_pages field in ContextualCueLogEvent.proto to match PageInfo.url.
    
    BUG=531628471
    TAG=agy
    CONV=27be748c-e807-4045-b951-db389ff90ff3
    
    Change-Id: I00431fffeb5e7d45156439b210ac10bb0a14196b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035092
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657366}

--- components/metrics/private_metrics/private_insights/contextual_cue_log_event_helpers_unittest.cc
+++ components/metrics/private_metrics/private_insights/contextual_cue_log_event_helpers_unittest.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/test/values_test_util.h"
 #include "components/metrics/private_metrics/private_insights/events/contextual_cue_log_event.pb.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace private_insights {
@@ -22,8 +24,9 @@ TEST(ContextualCueLogEventHelpersTest, SerializeOneItemList) {
   page_info.set_url("http://url1.com");
   page_info.set_title("Title 1");
   std::vector<events::ContextualCueLogEvent::PageInfo> list = {page_info};
-  EXPECT_EQ("[{\"page\":\"http://url1.com\",\"title\":\"Title 1\"}]",
-            SerializePageInfoListToJson(list));
+  EXPECT_THAT(SerializePageInfoListToJson(list),
+              base::test::IsJson(
+                  "[{\"url\":\"http://url1.com\",\"title\":\"Title 1\"}]"));
 }
 
 TEST(ContextualCueLogEventHelpersTest, SerializeMultipleItemsList) {
@@ -39,11 +42,11 @@ TEST(ContextualCueLogEventHelpersTest, SerializeMultipleItemsList) {
 
   std::vector<events::ContextualCueLogEvent::PageInfo> list = {
       page_info1, page_info2, page_info3};
-  EXPECT_EQ(
-      "[{\"page\":\"http://url1.com\",\"title\":\"Title 1\"},"
-      "{\"page\":\"http://url2.com\",\"title\":\"Title 2\"},"
-      "{\"page\":\"http://url3.com\",\"title\":\"Title 3\"}]",
-      SerializePageInfoListToJson(list));
+  EXPECT_THAT(SerializePageInfoListToJson(list),
+              base::test::IsJson(
+                  "[{\"url\":\"http://url1.com\",\"title\":\"Title 1\"},"
+                  "{\"url\":\"http://url2.com\",\"title\":\"Title 2\"},"
+                  "{\"url\":\"http://url3.com\",\"title\":\"Title 3\"}]"));
 }
 
 TEST(ContextualCueLogEventHelpersTest, SerializeSpecialCharactersList) {
@@ -60,13 +63,14 @@ TEST(ContextualCueLogEventHelpersTest, SerializeSpecialCharactersList) {
   std::vector<events::ContextualCueLogEvent::PageInfo> list = {
       page_info1, page_info2, page_info3};
   std::string json = SerializePageInfoListToJson(list);
-  EXPECT_EQ(
-      "[{\"page\":\"http://url1.com/a,b\",\"title\":\"Title 1, with comma\"},"
-      "{\"page\":\"http://url2.com/\\\"quotes\\\"\",\"title\":\"Title 2 "
-      "\\\"with quotes\\\"\"},"
-      "{\"page\":\"http://url3.com/newline\",\"title\":\"Title 3\\nwith "
-      "newline\"}]",
-      json);
+  EXPECT_THAT(json,
+              base::test::IsJson(
+                  "[{\"url\":\"http://url1.com/a,b\",\"title\":\"Title 1, with "
+                  "comma\"},"
+                  "{\"url\":\"http://url2.com/"
+                  "\\\"quotes\\\"\",\"title\":\"Title 2 \\\"with quotes\\\"\"},"
+                  "{\"url\":\"http://url3.com/newline\",\"title\":\"Title "
+                  "3\\nwith newline\"}]"));
 }
 
 }  // namespace private_insights
