21574cae7e956c918aae4e4e2370ac2018aa5506
Raphael Kubo da Costa <kubo@igalia.com>
2026-07-06 06:52:30 -0700

    iOS blink: Remove unused methods from shell_application_ios.mm
    content_shell is a scenes-based application (ShellAppDelegate implements
    -application:configurationForConnectingSceneSession:options:), so these
    methods are never called.
    
    Bug: 40254930
    Change-Id: Iaf457804e058c39a01b61c1fe8a577961fa40f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8034109
    Commit-Queue: Raphael Kubo da Costa <kubo@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Raphael Kubo da Costa <kubo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1657154}

--- content/shell/app/ios/shell_application_ios.mm
+++ content/shell/app/ios/shell_application_ios.mm
@@ -85,18 +85,6 @@ static std::unique_ptr<content::ShellMainDelegate> g_main_delegate;
   return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication*)application {
-}
-
-- (void)applicationDidEnterBackground:(UIApplication*)application {
-}
-
-- (void)applicationWillEnterForeground:(UIApplication*)application {
-}
-
-- (void)applicationDidBecomeActive:(UIApplication*)application {
-}
-
 - (void)applicationWillTerminate:(UIApplication*)application {
 }
 
