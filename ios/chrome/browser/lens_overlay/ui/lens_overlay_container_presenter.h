4c2f40d1f75b4debf5431c8551719787f3bcb108
Gauthier Ambard <gambard@chromium.org>
2026-07-06 08:04:00 -0700

    [iOS] Update LensOverlay did disappear method
    The method should be called when the overlay disappear, including from
    a bottom navigation. Currently it was only called if the user cancelled
    the overlay completely.
    
    Fixed: 530910243
    Change-Id: Ibf8fa69d2063ec4d3b9fd11524af4377bc8e71a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8033046
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657193}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_container_presenter.h
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_container_presenter.h
@@ -63,6 +63,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)lensOverlayContainerPresenterWillDismissPresentation:
     (LensOverlayContainerPresenter*)containerPresenter;
 
+// Notifies the delegate that the container presentation has been
+// dismissed.
+- (void)lensOverlayContainerPresenterDidDismissPresentation:
+    (LensOverlayContainerPresenter*)containerPresenter;
+
 // Informs the delegate that the container presentation was readjusted.
 - (void)lensOverlayContainerPresenterDidReadjustPresentation:
     (LensOverlayContainerPresenter*)containerPresenter;
