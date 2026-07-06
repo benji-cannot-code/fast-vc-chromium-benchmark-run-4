c71bf2190ddd726046d857a26eefa45466e647b2
Gazal <agazal@google.com>
2026-07-06 09:34:52 -0700

    [Settings Containment] Disable failing tests and clean up flag overrides
    Disable SiteSettings, FledgeFragment, and TopicsFragment tests that fail
    with Settings Containment enabled, and remove their @DisableFeatures
    overrides. These tests pass locally on both x86 and x64, but they
    inconsistently fail the tryjobs.
    
    Also updates testOnlyExpectedPreferencesAllSites to the containment
    layout.
    
    Bug: 433576895
    Change-Id: If2cf80ef649bdc3cca0bb31efd4c82350524ba94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8040824
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657248}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/FledgeFragmentTest.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/FledgeFragmentTest.java
@@ -48,6 +48,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.UserActionTester;
@@ -360,8 +361,7 @@ public final class FledgeFragmentTest {
 
     @Test
     @SmallTest
-    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
-    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
+    @DisabledTest(message = "https://crbug.com/433576895")
     public void testMaxDisplayedSites() {
         setFledgePrefEnabled(true);
         for (int i = 0; i < FledgeFragment.MAX_DISPLAYED_SITES + 1; i++) {
@@ -394,8 +394,7 @@ public final class FledgeFragmentTest {
 
     @Test
     @SmallTest
-    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
-    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
+    @DisabledTest(message = "https://crbug.com/433576895")
     public void testBlockSites() {
         setFledgePrefEnabled(true);
         mFakePrivacySandboxBridge.setCurrentFledgeSites(SITE_NAME_1, SITE_NAME_2);
@@ -545,8 +544,7 @@ public final class FledgeFragmentTest {
 
     @Test
     @SmallTest
-    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
-    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
+    @DisabledTest(message = "https://crbug.com/433576895")
     public void testFooterTopicsLinkAdTopicsContentParity() throws IOException {
         setFledgePrefEnabled(true);
         startFledgeSettings();
@@ -560,8 +558,7 @@ public final class FledgeFragmentTest {
 
     @Test
     @SmallTest
-    // TODO(crbug.com/433576895): Re-enable containment feature once the test is fixed.
-    @DisableFeatures(ChromeFeatureList.ANDROID_SETTINGS_CONTAINMENT)
+    @DisabledTest(message = "https://crbug.com/433576895")
     public void testFooterCookieSettingsLink() throws IOException {
         setFledgePrefEnabled(true);
         startFledgeSettings();
