44be7cb0bc1f8d1fb193440b5bbe70a0dba1f54f
James Cook <jamescook@chromium.org>
2026-07-06 11:10:39 -0700

    settings: Add metrics to Android settings in a tab
    Extract SettingsMetricReport from SettingsActivity into a separate
    helper class. Use it from SettingsPageFragmentDelegateImpl. This results
    in the same histograms being recorded with SettingsInTab on and off,
    which is intentional. I believe it will make it easier to compare
    different populations if the metrics have the same names.
    
    Bug: 521895796
    Change-Id: I568a3388d0d28df53cb322a32cc7afd4e9d0faa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032358
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657322}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsPageFragmentDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsPageFragmentDelegateImpl.java
@@ -65,6 +65,7 @@ public class SettingsPageFragmentDelegateImpl
     private FragmentManager.@Nullable FragmentLifecycleCallbacks mDependencyProvider;
     private FragmentManager.@Nullable FragmentLifecycleCallbacks mTitleUpdaterLifecycleCallbacks;
     private FragmentManager.@Nullable FragmentLifecycleCallbacks mWideDisplayPaddingApplier;
+    private FragmentManager.@Nullable FragmentLifecycleCallbacks mSettingsMetricsReporter;
     private @Nullable Toolbar mToolbar;
     private @Nullable MultiColumnTitleUpdater mMultiColumnTitleUpdater;
 
@@ -136,6 +137,10 @@ public class SettingsPageFragmentDelegateImpl
         fragmentManager.registerFragmentLifecycleCallbacks(
                 mWideDisplayPaddingApplier, /* recursive= */ true);
 
+        mSettingsMetricsReporter = new SettingsMetricsReporter(mainFragmentTag);
+        fragmentManager.registerFragmentLifecycleCallbacks(
+                mSettingsMetricsReporter, /* recursive= */ true);
+
         // Inflate the settings layout into the container view.
         // TODO(crbug.com/521895796): Rename settings_activity.xml since with settings-in-a-tab it
         // doesn't map directly to its own activity.
@@ -197,6 +202,10 @@ public class SettingsPageFragmentDelegateImpl
         assumeNonNull(mWideDisplayPaddingApplier);
         fragmentManager.unregisterFragmentLifecycleCallbacks(mWideDisplayPaddingApplier);
         mWideDisplayPaddingApplier = null;
+
+        assumeNonNull(mSettingsMetricsReporter);
+        fragmentManager.unregisterFragmentLifecycleCallbacks(mSettingsMetricsReporter);
+        mSettingsMetricsReporter = null;
 
         if (mMultiColumnTitleUpdater != null) {
             MultiColumnSettings multiColumnSettings = getMultiColumnSettings();
