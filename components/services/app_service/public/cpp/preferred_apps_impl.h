4716d4d1c9f43796c717455a530417928884d3d8
Kunjan Vinay Patel <ksukh@google.com>
2026-07-06 14:17:32 -0700

    [PWA/ Nav Capturing] Prioritize scope over scope extensions in ChromeOS.
    This CL ensures Navigation Capturing strictly prioritizes an app where
    the target URL falls within the app's scope instead of its scope
    extensions. If no apps are found that can handle an url that is in
    scope, fallback to using scope extensions for the web app.
    
    Changes Include:
    - Added `IsWebAppAndExtendedScope ` method to the `PreferredAppsList::Delegate` interface, to query whether a URL belongs to an app's scope extension.
    - Updating `PreferredAppsList::FindPreferredAppForIntent` to use this delegate to defer scope extensions matches, ensuring app's scope matches are prioritized regardless of match level or prefix length.
    - Enabling `PrimaryScopeTakesPriorityOverExtendedScope` and added `CrossOriginCrossPathCoexistence` browser test to verify this behavior on ChromeOS in V2DefaultOn mode, while preserving legacy assertions for `V2DefaultOff` mode.
    
    Bug: 528069266
    Change-Id: Ib93ab30e35047729599043fa58dbb803ed668d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8013179
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Kunjan Patel <ksukh@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657477}

--- components/services/app_service/public/cpp/preferred_apps_impl.h
+++ components/services/app_service/public/cpp/preferred_apps_impl.h
@@ -49,6 +49,9 @@ class PreferredAppsImpl : public PreferredAppsList::Delegate {
                                const IntentFilterPtr& first_filter,
                                const std::string& second_app_id,
                                const IntentFilterPtr& second_filter) = 0;
+
+    virtual bool IsWebAppInExtendedScope(const GURL& url,
+                                         const std::string& app_id) const = 0;
   };
 
   PreferredAppsImpl(
@@ -71,6 +74,8 @@ class PreferredAppsImpl : public PreferredAppsList::Delegate {
                      const IntentFilterPtr& first_filter,
                      const std::string& second_app_id,
                      const IntentFilterPtr& second_filter) override;
+  bool IsWebAppInExtendedScope(const GURL& url,
+                               const std::string& app_id) const override;
 
   void SetLongestPrefixMatchEnabled(bool enabled) {
     preferred_apps_list_.SetLongestPrefixMatchEnabled(enabled);
