f627bce3c24bcc01141667c5dd79498d5e4ab1b5
Angela Novakovic <novakovica@google.com>
2026-07-06 09:50:30 -0700

    [iOS][HofT] Add Shopping settings subpage infrastructure
    Introduces the coordinator, mediator, and table view controller for the
    new Shopping settings subpage (accessible under Autofill and Passwords).
    
    Bug: 530620605
    Change-Id: I5a0578c0d24aeaec4795d0be3b0a7dc66da96b88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032658
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657263}

---
+++ ios/chrome/browser/settings/autofill/autofill_and_passwords/coordinator/shopping_mediator.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_AUTOFILL_AND_PASSWORDS_COORDINATOR_SHOPPING_MEDIATOR_H_
+#define IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_AUTOFILL_AND_PASSWORDS_COORDINATOR_SHOPPING_MEDIATOR_H_
+
+#import "ios/chrome/browser/settings/autofill/autofill_and_passwords/coordinator/autofill_ai_base_mediator.h"
+
+@protocol ShoppingConsumer;
+
+// Mediator for the Shopping settings page.
+@interface ShoppingMediator : AutofillAIBaseMediator
+
+// Consumer for this mediator.
+@property(nonatomic, weak) id<ShoppingConsumer> consumer;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SETTINGS_AUTOFILL_AUTOFILL_AND_PASSWORDS_COORDINATOR_SHOPPING_MEDIATOR_H_
