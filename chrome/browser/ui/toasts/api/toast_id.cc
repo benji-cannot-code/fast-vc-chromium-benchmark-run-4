b908a7ecc25cec6ed54e9e1dd6b0faff01abbe39
Amya Singhal <amyasinghal@google.com>
2026-07-06 10:46:46 -0700

    Voice Typing: Add a Toast UI when dictation stopped because of tab switch
    Video recording:
    https://drive.google.com/file/d/1kPv-wLtt6UBCpr-DrOvAegUIw9xxjZ4u/view?usp=sharing
    
    Bug: b:529137727 b:530590563
    Change-Id: Ie4333cccec489b3350f21d908c26b5e7ffdbca0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8026209
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Amya Singhal <amyasinghal@google.com>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657300}

--- chrome/browser/ui/toasts/api/toast_id.cc
+++ chrome/browser/ui/toasts/api/toast_id.cc
@@ -106,6 +106,8 @@ std::string_view GetToastName(ToastId toast_id) {
       return "EnterpriseCopyKeptInManagedChrome";
     case ToastId::kDictationError:
       return "DictationError";
+    case ToastId::kDictationStopped:
+      return "DictationStopped";
   }
 
   NOTREACHED();
