0c70c2f08d6f779f97aaaa35d4f332a8081c8cb7
Gazal <agazal@google.com>
2026-07-06 08:30:32 -0700

    [code-health] Optimize Logging in android_webview
    Bug: 529082276, 530982078
    
    Change-Id: I53d9fed33f28c672eb4b29f99761e28df8d9c20f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8040822
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657212}

--- android_webview/java/src/org/chromium/android_webview/AwServiceWorkerSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwServiceWorkerSettings.java
@@ -58,7 +58,7 @@ public class AwServiceWorkerSettings {
 
     /** See {@link android.webkit.ServiceWorkerWebSettings#setCacheMode}. */
     public void setCacheMode(int mode) {
-        if (TRACE) Log.d(TAG, "setCacheMode=" + mode);
+        if (TRACE) Log.d(TAG, "setCacheMode=%d", mode);
         synchronized (mAwServiceWorkerSettingsLock) {
             if (mCacheMode != mode) {
                 mCacheMode = mode;
@@ -75,7 +75,7 @@ public class AwServiceWorkerSettings {
 
     /** See {@link android.webkit.ServiceWorkerWebSettings#setAllowContentAccess}. */
     public void setAllowContentAccess(boolean allow) {
-        if (TRACE) Log.d(TAG, "setAllowContentAccess=" + allow);
+        if (TRACE) Log.d(TAG, "setAllowContentAccess=%b", allow);
         synchronized (mAwServiceWorkerSettingsLock) {
             if (mAllowContentUrlAccess != allow) {
                 mAllowContentUrlAccess = allow;
@@ -92,7 +92,7 @@ public class AwServiceWorkerSettings {
 
     /** See {@link android.webkit.ServiceWorkerWebSettings#setAllowFileAccess}. */
     public void setAllowFileAccess(boolean allow) {
-        if (TRACE) Log.d(TAG, "setAllowFileAccess=" + allow);
+        if (TRACE) Log.d(TAG, "setAllowFileAccess=%b", allow);
         synchronized (mAwServiceWorkerSettingsLock) {
             if (mAllowFileUrlAccess != allow) {
                 mAllowFileUrlAccess = allow;
@@ -108,7 +108,7 @@ public class AwServiceWorkerSettings {
     }
 
     public void setBlockSpecialFileUrls(boolean block) {
-        if (TRACE) Log.d(TAG, "setBlockSpecialFileUrls=" + block);
+        if (TRACE) Log.d(TAG, "setBlockSpecialFileUrls=%b", block);
         synchronized (mAwServiceWorkerSettingsLock) {
             mBlockSpecialFileUrls = block;
         }
@@ -122,7 +122,7 @@ public class AwServiceWorkerSettings {
 
     /** See {@link android.webkit.ServiceWorkerWebSettings#setBlockNetworkLoads}. */
     public void setBlockNetworkLoads(boolean flag) {
-        if (TRACE) Log.d(TAG, "setBlockNetworkLoads=" + flag);
+        if (TRACE) Log.d(TAG, "setBlockNetworkLoads=%b", flag);
         synchronized (mAwServiceWorkerSettingsLock) {
             if (!flag && !mHasInternetPermission) {
                 throw new SecurityException(
--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewThreadTestActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewThreadTestActivity.java
@@ -133,7 +133,7 @@ public class WebViewThreadTestActivity extends Activity {
                                 mWebView = null;
                                 break;
                             default:
-                                Log.d(TAG, "Unknown message: " + msg.what);
+                                Log.d(TAG, "Unknown message: %d", msg.what);
                                 break;
                         }
                     }
@@ -246,7 +246,7 @@ public class WebViewThreadTestActivity extends Activity {
             try {
                 mWebViewThread.join();
             } catch (Exception e) {
-                Log.d(TAG, "While joining mWebViewThread: " + e);
+                Log.d(TAG, "While joining mWebViewThread", e);
             }
         }
         super.onDestroy();
