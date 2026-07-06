71b659ea32f870d6b4b3776070037e40f302f4fb
Lukasz Anforowicz <lukasza@chromium.org>
2026-07-06 10:51:24 -0700

    [crubit] Unified `#include` paths for `crubit/support` libraries.
    The high-level goal of this CL is to allow Chromium to use the same
    `#include` paths as other Crubit clients.  This CL accomplishes this
    for Crubit support libraries by:
    
    1. Modifying `tools/rust/build_crubit.py` to package `crubit/support`
       under `third_party/rust-toolchain/lib/third_party/crubit/support`
       (not `third_party/rust-toolchain/lib/crubit/support` as earlier).
       The re-packaged Crubit is distributed through updated `//DEPS`.
    2. Using `include_dirs` in `//build/rust/crubit/BUILD.gn` so that
       Crubit clients in Chromium can use the unified `#include` paths
       like `#include "third_party/crubit/support/rs_std/slice_ref.h"`
       (the same path is used by other Crubit clients - e.g. google3).
    
    Bug: 470466915
    Change-Id: I0d9c0e382f421419c061017cae2895034168e97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8031811
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657307}

--- build/rust/tests/test_cpp_api_from_rust/unittests.cc
+++ build/rust/tests/test_cpp_api_from_rust/unittests.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/rust/std/rules/alloc.h"
 #include "build/rust/tests/test_cpp_api_from_rust/rust_lib.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/rust-toolchain/lib/crubit/support/rs_std/char.h"
+#include "third_party/crubit/support/rs_std/char.h"
 
 TEST(RustCcBindingsFromRs, TestI32) {
   EXPECT_EQ(12, rust_lib::mul_two_ints_via_rust(3, 4));
