8588dca881e5f754708b5797e1f9d25818bb2433
William Liu <liuwilliam@chromium.org>
2026-07-06 11:58:43 -0700

    [indigo] Add a switch to skip the enterprise check
    Bug: b:512247450, b:529737129
    Change-Id: Icfd7a668258a7754b9f431366f60bcc2326dbac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8037573
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Auto-Submit: William Liu <liuwilliam@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657362}

--- chrome/browser/indigo/indigo_service_unittest.cc
+++ chrome/browser/indigo/indigo_service_unittest.cc
@@ -51,7 +51,7 @@ class IndigoServiceTest : public testing::Test {
   IndigoServiceTest() {
     scoped_feature_list_.InitAndEnableFeatureWithParameters(
         features::kIndigo,
-        {{features::kIndigoAllowForEnterprise.name, "true"}});
+        {{features::kIndigoSkipEnterpriseCheck.name, "true"}});
   }
 
   void SetUp() override {
@@ -209,7 +209,7 @@ TEST_F(IndigoServiceTest, GlicRequirementEnabledAndDisabled) {
   scoped_feature_list_.Reset();
   scoped_feature_list_.InitAndEnableFeatureWithParameters(
       features::kIndigo, {{features::kIndigoRequireGlicEnabling.name, "true"},
-                          {features::kIndigoAllowForEnterprise.name, "true"}});
+                          {features::kIndigoSkipEnterpriseCheck.name, "true"}});
 
   CreateService();
   MakeAccountAvailableAndCapable();
@@ -645,6 +645,22 @@ TEST_P(IndigoServiceManagementPolicyDefaultEnabledTest, ManagedDomain) {
   CreateService();
   MakeAccountAvailableAndCapable("test@example.com", "example.com");
   EXPECT_TRUE(LocalEligibilityBecomes(LocalEligibility::kManagedDomain));
+}
+
+TEST_P(IndigoServiceManagementPolicyDefaultEnabledTest,
+       SkipEnterpriseCheckKillswitch) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeatureWithParameters(
+      features::kIndigo, {{features::kIndigoSkipEnterpriseCheck.name, "true"}});
+
+  CreateService();
+  policy::ScopedManagementServiceOverrideForTesting
+      scoped_management_service_override(
+          policy::ManagementServiceFactory::GetForProfile(&profile_),
+          policy::EnterpriseManagementAuthority::CLOUD);
+  MakeAccountAvailableAndCapable();
+
+  EXPECT_TRUE(LocalEligibilityBecomes(LocalEligibility::kEligible));
 }
 
 TEST_P(IndigoServiceManagementPolicyDefaultEnabledTest,
