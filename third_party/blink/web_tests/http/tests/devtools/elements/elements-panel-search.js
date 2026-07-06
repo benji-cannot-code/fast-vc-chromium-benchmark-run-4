aa098f57c24136d824cce13c03018428e0486560
Simon Zünd <szuend@chromium.org>
2026-07-05 23:10:45 -0700

    Pass TargetManager instance to DOMModel.cancelSearch.
    Bug: 493763857
    Change-Id: Ie628b57bdfc6447cf2bb9bfa118e46c4896ceb68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8040811
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656989}

--- third_party/blink/web_tests/http/tests/devtools/elements/elements-panel-search.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/elements-panel-search.js
@@ -59,7 +59,7 @@ import * as SDK from 'devtools/core/sdk/sdk.js';
   async function searchCallback(next, resultCount) {
     if (resultCount == 0) {
       TestRunner.addResult('Nothing found');
-      SDK.DOMModel.DOMModel.cancelSearch();
+      SDK.DOMModel.DOMModel.cancelSearch(SDK.TargetManager.TargetManager.instance());
       next();
       return;
     }
@@ -72,7 +72,7 @@ import * as SDK from 'devtools/core/sdk/sdk.js';
       TestRunner.addResult(markupVa_lue.split('').join(' '));
     }
 
-    SDK.DOMModel.DOMModel.cancelSearch();
+    SDK.DOMModel.DOMModel.cancelSearch(SDK.TargetManager.TargetManager.instance());
     next();
   }
 
