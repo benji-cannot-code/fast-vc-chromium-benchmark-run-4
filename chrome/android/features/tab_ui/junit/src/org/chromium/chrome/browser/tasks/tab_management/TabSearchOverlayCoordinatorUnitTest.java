bb7cf132c772e48084b81396b3eb09c66d78d5e0
Brandon Fong <bjfong@google.com>
2026-07-06 13:50:39 -0700

    [search] Add logic to support status icon override behavior
    Tab Search requires a custom status icon while utilizing the same logic
    as the hub search page classification. Add logic to support the override
    of the status icon resource in the location bar.
    
    Bug: 527090329
    Change-Id: Ifa635c531489bb6b3222505d2e6a30690496ff82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8041041
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657459}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabSearchOverlayCoordinatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabSearchOverlayCoordinatorUnitTest.java
@@ -148,6 +148,8 @@ public class TabSearchOverlayCoordinatorUnitTest {
                         any(),
                         any());
         verify(mSearchUiCoordinator)
+                .setDefaultStatusIconOverrideResId(R.drawable.ic_suggestion_magnifier);
+        verify(mSearchUiCoordinator)
                 .beginQuery(eq(IntentOrigin.HUB), eq(SearchType.TEXT), eq(""), eq(mWindowAndroid));
     }
 
