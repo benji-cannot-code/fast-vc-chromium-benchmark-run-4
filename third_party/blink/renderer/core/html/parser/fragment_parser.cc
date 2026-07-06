9354c56e5d84567f97964519ffbefcaab8c14c92
Noam Rosenthal <nrosenthal@chromium.org>
2026-07-06 05:56:28 -0700

    Make TrustedParserOptions always pass a Sanitizer object
    This simplifies callbacks that don't have to switch between types.
    Also made extra checks that sanitizer cannot be modified inside a
    TrustedParserOptions.
    
    Bug: 491743369
    Change-Id: I53096a7c699ce4430929f5cc3bbb58a3d652c674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7985568
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657129}

--- third_party/blink/renderer/core/html/parser/fragment_parser.cc
+++ third_party/blink/renderer/core/html/parser/fragment_parser.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/timer/elapsed_timer.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_parse_html_unsafe_options.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_union_sanitizer_sanitizerconfig_sanitizerpresets.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_sethtmlunsafeoptions_trustedparseroptions.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
@@ -167,7 +168,12 @@ FragmentParserOptions::FragmentParserOptions(TrustedParserOptions* options)
                     RuntimeEnabledFeatures::SetHTMLCanRunScriptsEnabled())
                        ? RunScripts::kRunScripts
                        : RunScripts::kDontRunScripts),
-      sanitizer_init_(options->sanitizer()) {}
+      sanitizer_init_(
+          options->EffectiveSanitizer()
+              ? MakeGarbageCollected<
+                    V8UnionSanitizerOrSanitizerConfigOrSanitizerPresets>(
+                    options->EffectiveSanitizer())
+              : nullptr) {}
 
 FragmentParserOptions::FragmentParserOptions(SetHTMLUnsafeOptions* options)
     : run_scripts_((options->runScripts() &&
