cc914b74d96e1d5c38c1794bc8a711aabc857c81
rosiey <rosiey@google.com>
2026-07-06 16:10:57 -0700

    [Glic] Use 3D raster product logo in tab close confirmation dialog
    Replace the flat 2D vector icon with the 3D shaded raster bitmap
    resource (IDR_PRODUCT_LOGO_64) in the Actor tab close confirmation
    warning dialog.
    
    Result: http://shortn/_6OktwipjlZ
    Fixed: b:529899356
    Change-Id: I4f72693e7e96e0e51310fe8dd21d837158b63bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8048052
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657553}

--- chrome/browser/actor/ui/actor_task_unload_handler.cc
+++ chrome/browser/actor/ui/actor_task_unload_handler.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/tabs/public/tab_interface.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/color/color_id.h"
 #include "ui/events/event.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
@@ -300,8 +302,8 @@ ActorTaskTabCloseConfirmDialog::CreateDelegate(
       kIconBadgeBorderColor));
 
   auto icon = std::make_unique<views::ImageView>();
-  icon->SetImage(::ui::ImageModel::FromVectorIcon(
-      vector_icons::kProductIcon, ::ui::kColorIcon, kProductIconSize));
+  icon->SetImage(::ui::ImageModel::FromResourceId(IDR_PRODUCT_LOGO_64));
+  icon->SetImageSize(gfx::Size(kProductIconSize, kProductIconSize));
   icon_container->AddChildView(std::move(icon));
   container->AddChildView(std::move(icon_container));
 
