f127f5ec0e7c56b84db86ff45eef8ac8ade3722b
Chris Lu <thegreenfrog@google.com>
2026-07-07 12:27:55 -0700

    [iOS] Fix UIViewControllerHierarchyInconsistency crash on NTP Redesign
    When the NTP redesign is enabled, NewTabPageCoordinator was returning
    NTPRedesignViewController as its viewController property. However,
    internally it was also adding NTPRedesignViewController as a child view
    controller of its containerViewController. This caused the view controller to have two parents (containerViewController internally and
    BrowserContentViewController externally), leading to a hierarchy
    inconsistency crash when views were added/removed during new tab
    transitions.
    
    Fixed: 530270248
    Change-Id: I05f8bf24da2b8197ee6269a8d76459b2396351fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8057484
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658200}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator.mm
@@ -931,9 +931,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self.isOffTheRecord) {
     return self.incognitoViewController;
   }
-  if (IsNTPRedesignEnabled()) {
-    return self.NTPRedesignViewController;
-  }
   return self.containerViewController;
 }
 
