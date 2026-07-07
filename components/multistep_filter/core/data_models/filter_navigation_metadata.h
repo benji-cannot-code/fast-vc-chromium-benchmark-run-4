0bab8d36c4d7f3aa5df72c6c39d17d1780c48d31
Aditya Gupta <adigupt@google.com>
2026-07-07 02:05:11 -0700

    [Multistep filter] Introduce FilterNavigationMetadata into core/data_models
    This CL introduces the `FilterNavigationMetadata` struct and moves
    `NavigationMetadata` from the content layer to
    `components/multistep_filter/core/data_models`.
    
    Bug: b:531717350
    Change-Id: Ice292d5b018f087c4cf33c07cf11ad1dbb8998a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8048560
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Aditya Gupta <adigupt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657792}

---
+++ components/multistep_filter/core/data_models/filter_navigation_metadata.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MULTISTEP_FILTER_CORE_DATA_MODELS_FILTER_NAVIGATION_METADATA_H_
+#define COMPONENTS_MULTISTEP_FILTER_CORE_DATA_MODELS_FILTER_NAVIGATION_METADATA_H_
+
+#include <optional>
+
+#include "components/multistep_filter/core/data_models/url_filter_suggestion.h"
+#include "url/gurl.h"
+
+namespace multistep_filter {
+
+// Immutable snapshot of tab navigation state (network, security, and user
+// gestures). Populated by the content layer and dispatched into the Core layer.
+struct FilterNavigationMetadata {
+  int64_t navigation_id = 0;
+  GURL url;
+  GURL prev_url;
+  bool is_cryptographic_scheme = false;
+  bool is_http_allowed_for_testing = false;
+  int net_error_code = 0;
+  int http_response_code = 0;
+  bool is_error_page_navigation = false;
+  bool has_user_gesture = false;
+  bool was_filter_initiated_navigation = false;
+  bool is_same_document_navigation = false;
+  std::optional<UrlFilterSuggestion> applied_suggestion;
+};
+
+}  // namespace multistep_filter
+
+#endif  // COMPONENTS_MULTISTEP_FILTER_CORE_DATA_MODELS_FILTER_NAVIGATION_METADATA_H_
