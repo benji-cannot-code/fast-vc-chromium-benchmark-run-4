b568dd2e43740767f97f6a857a6252e7fced5326
Dan Harrington <harringtond@chromium.org>
2026-07-07 15:26:26 -0700

    Add early exit in AcceleratorManagerAndroid::ProcessKeyEvent for invalid actions
    Avoid crashing in ui::KeyEvent when constructing it with an unsupported
    or unknown Android event action (e.g., ACTION_MULTIPLE) by returning
    false early.
    
    Bug: 529458436
    Change-Id: I63b79356fbe2fe995abee28c852f2ad46a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8059985
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658330}

--- ui/android/accelerator_manager_android.cc
+++ ui/android/accelerator_manager_android.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/android/accelerator_manager_android.h"
 
+#include <android/input.h>
+
 #include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
 #include "ui/android/window_android.h"
@@ -68,6 +70,11 @@ bool AcceleratorManagerAndroid::ProcessKeyEvent(
     JNIEnv* env,
     const base::android::JavaRef<jobject>& j_key_event) {
   KeyEventAndroid key_event_android(j_key_event);
+  // ui::KeyEvent will CHECK fail if the type is not one of these.
+  if (key_event_android.Action() != AKEY_EVENT_ACTION_DOWN &&
+      key_event_android.Action() != AKEY_EVENT_ACTION_UP) {
+    return false;
+  }
   ui::PlatformEvent native_event(key_event_android);
   ui::KeyEvent key_event(native_event);
   ui::Accelerator accelerator(key_event);
