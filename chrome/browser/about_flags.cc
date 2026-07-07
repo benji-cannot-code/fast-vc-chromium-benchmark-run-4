59a12f05e3722154aaca3c8c8cb9f4f8f1122495
Ian Zhao <yinguang@google.com>
2026-07-07 10:34:34 -0700

    Add about:flags entry for Semantic Embedder API
    This CL adds the semantic-embedder-api flag to chrome://flags to allow
    external gating and manual testing of the new Semantic Embedder API.
    Enabling this flag activates both the AIEmbeddingsAPI and
    AIEmbeddingsAPIForWorkers blink features.
    
    Bug: 428233906
    Change-Id: Ib93d88fb6a96e576fcda243c9df4de2af4f72d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8029424
    Commit-Queue: Ian Zhao <yinguang@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Isaac Ahouma <iahouma@google.com>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658068}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4612,6 +4612,12 @@ const FeatureEntry::Choice kAISummarizationPerformancePreferenceChoices[] = {
      "AISummarizationPerformancePreference,OnDeviceModelLitertLmBackend,"
      "OptimizationGuideManifestBroker"}};
 
+// Semantic Embedder. Enables the API for both Window and Worker contexts.
+const FeatureEntry::Choice kAISemanticEmbedderChoices[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {flags_ui::kGenericExperimentChoiceEnabled, switches::kEnableFeatures,
+     "AIEmbeddingsAPI,AIEmbeddingsAPIForWorkers"}};
+
 // Classifier requires LiteRT-LM.
 const FeatureEntry::Choice kAIClassifierChoices[] = {
     {flags_ui::kGenericExperimentChoiceDefault, "", ""},
@@ -10276,6 +10282,11 @@ const FeatureEntry kFeatureEntries[] = {
     {"proofreader-api", flag_descriptions::kProofreaderAPIName,
      flag_descriptions::kProofreaderAPIDescription, kOsDesktop,
      MULTI_VALUE_TYPE(kAIProofreaderChoices), flag_descriptions::kAIAPIsLinks},
+
+    {"semantic-embedder-api", flag_descriptions::kSemanticEmbedderAPIName,
+     flag_descriptions::kSemanticEmbedderAPIDescription, kOsDesktop,
+     MULTI_VALUE_TYPE(kAISemanticEmbedderChoices),
+     flag_descriptions::kAIAPIsLinks},
 
     {"classifier-api", flag_descriptions::kClassifierAPIName,
      flag_descriptions::kClassifierAPIDescription, kOsDesktop,
