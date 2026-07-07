505c1f8d15361c43224e98cf46f9f76af44237ea
Mitsuru Oshima <oshima@chromium.org>
2026-07-07 10:31:13 -0700

    Reland "Views/Menu: Replace SUPPORTS_OZONE_X11 compile-time checks with runtime checks in tests"
    This reverts commit 527bf90f72d9183fcdf048f5134860fd970d3ef8.
    
    Reason for revert: mutter build does not have X11, which is fixed.
    
    Original change's description:
    > Revert "Views/Menu: Replace SUPPORTS_OZONE_X11 compile-time checks with runtime checks in tests"
    >
    > This reverts commit 18a6bd16981a5a6801711e18e4a4f4d4ff5bd345.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8677363615408605969
    >
    > Sample failed build: https://ci.chromium.org/b/8677363615408605969
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8027850&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8677363615408605969&type=BUG
    >
    > Original change's description:
    > > Views/Menu: Replace SUPPORTS_OZONE_X11 compile-time checks with runtime checks in tests
    > >
    > > This CL replaces compile-time BUILDFLAG(SUPPORTS_OZONE_X11) and
    > > BUILDFLAG(SUPPORTS_OZONE_WAYLAND) checks in views and menu tests with
    > > runtime checks using ui::OzonePlatform.
    > >
    > > This allows tests to run on X11 even if the build supports Wayland
    > > (and vice versa), and enables tests that were previously compiled out
    > > on Linux to run and skip at runtime when appropriate.
    > >
    > > Specifically:
    > > - Re-enabled MenuViewDragAndDropTest.TestInMenuDrag and
    > >   MenuViewDragAndDropTestNestedDrag.MenuViewDragAndDropNestedDrag on Linux,
    > >   adding runtime skips for Wayland/X11 where they are not supported.
    > > - Re-enabled DesktopWidgetDragTestInteractive.RunDragDropLoopUpdatesMouseButtonState
    > >   on Linux, skipping it at runtime on X11.
    > > - Updated MenuControllerTest to use runtime checks for touch ID release
    > >   and capture restoration tests.
    > > - Added dependency on //ui/ozone for views_interactive_ui_tests target.
    > >
    > > Bug: 529612149
    > > TAG=agy
    > > CONV=2069bd38-a45e-4661-93d0-576f5eef7dc1
    > >
    > > Change-Id: If6670b74844952da8b6cdf4af97ba89e30b83e83
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027850
    > > Reviewed-by: Elly <ellyjones@chromium.org>
    > > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1656191}
    > >
    >
    > Bug: 529612149
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I097d1612d8c5bd0e04de6c49feb4d8dc7188e500
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035970
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1656193}
    
    Bug: 529612149
    Change-Id: Ic734259fcd3b8738d6a4fc1acb1e06ce5f8f5457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8037402
    Reviewed-by: Elly <ellyjones@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658065}

--- chrome/browser/ui/views/menu_view_drag_and_drop_test.cc
+++ chrome/browser/ui/views/menu_view_drag_and_drop_test.cc
@@ -387,7 +387,11 @@ void MenuViewDragAndDropTestTestInMenuDrag::OnWidgetDragDropCompleted(
 
 // TODO(crbug.com/375959961): For X11, the menu is always closed on drag
 // completion because the native widget's state is not properly updated.
-#if !BUILDFLAG(SUPPORTS_OZONE_X11)
+#if BUILDFLAG(IS_OZONE)
+  if (!::ui::OzonePlatform::RunningOnX11ForTest()) {
+    EXPECT_TRUE(menu()->GetSubmenu()->IsShowing());
+  }
+#else
   EXPECT_TRUE(menu()->GetSubmenu()->IsShowing());
 #endif
 
@@ -422,7 +426,7 @@ void MenuViewDragAndDropTestTestInMenuDrag::StartDrag() {
 // Test that an in-menu drag asks the delegate to stay open.
 // TODO(pkasting): https://crbug.com/41445629 Fails on Mac.
 // TODO(crbug.com/40911016): Re-enable this test for linux.
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_MAC)
 #define MAYBE_TestInMenuDrag DISABLED_TestInMenuDrag
 #else
 #define MAYBE_TestInMenuDrag TestInMenuDrag
@@ -482,6 +486,13 @@ void MenuViewDragAndDropTestTestInMenuDragNoDrop::OnWidgetDragDropCompleted(
 }
 
 void MenuViewDragAndDropTestTestInMenuDragNoDrop::DoTestWithMenuOpen() {
+#if BUILDFLAG(IS_OZONE)
+  if (::ui::OzonePlatform::RunningOnX11ForTest()) {
+    Done();
+    GTEST_SKIP() << "For X11, the menu is closed on drag completion because "
+                    "the native widget's state is not properly updated.";
+  }
+#endif
   MenuViewDragAndDropTest::DoTestWithMenuOpen();
 
   // We're going to drag the second menu element.
@@ -506,16 +517,9 @@ void MenuViewDragAndDropTestTestInMenuDragNoDrop::StartDrag() {
 
 // TODO(crbug.com/375959961): For X11, the menu is closed on drag completion
 // because the native widget's state is not properly updated.
-#if BUILDFLAG(SUPPORTS_OZONE_X11)
-#define MAYBE_TestInMenuDragNoDrop DISABLED_TestInMenuDragNoDrop
-#else
-#define MAYBE_TestInMenuDragNoDrop TestInMenuDragNoDrop
-#endif
-
 // Test that an in-menu (i.e., entirely implemented in the menu code) does not
 // close the menu when the drag is complete but there is no drop.
-VIEW_TEST(MenuViewDragAndDropTestTestInMenuDragNoDrop,
-          MAYBE_TestInMenuDragNoDrop)
+VIEW_TEST(MenuViewDragAndDropTestTestInMenuDragNoDrop, TestInMenuDragNoDrop)
 
 class MenuViewDragAndDropTestNestedDrag : public MenuViewDragAndDropTest {
  public:
@@ -564,7 +568,13 @@ void MenuViewDragAndDropTestNestedDrag::OnWidgetDragDropCompleted(
   // all subsequent mouse movements will be delivered as "MouseDragged" events
   // to MenuController.
   // Until this is fixed, the menu closes at the end of drags.
-  EXPECT_NE(submenu->IsShowing(), BUILDFLAG(SUPPORTS_OZONE_X11));
+  bool expected_showing = true;
+#if BUILDFLAG(IS_OZONE)
+  if (::ui::OzonePlatform::RunningOnX11ForTest()) {
+    expected_showing = false;
+  }
+#endif
+  EXPECT_EQ(submenu->IsShowing(), expected_showing);
 
   // Clean up.
   submenu->Close();
@@ -573,6 +583,14 @@ void MenuViewDragAndDropTestNestedDrag::OnWidgetDragDropCompleted(
 }
 
 void MenuViewDragAndDropTestNestedDrag::DoTestWithMenuOpen() {
+#if BUILDFLAG(IS_OZONE)
+  if (::ui::OzonePlatform::RunningOnWaylandForTest()) {
+    Done();
+    // TODO(crbug.com/41496561): Test is failing under ChromeRefresh2023 on
+    // wayland.
+    GTEST_SKIP() << "Skipping for Wayland";
+  }
+#endif
   MenuViewDragAndDropTest::DoTestWithMenuOpen();
 
   // Cause the target's second child to trigger a mouse up when dragged over.
--- ui/views/widget/widget_interactive_uitest.cc
+++ ui/views/widget/widget_interactive_uitest.cc
@@ -68,6 +68,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/win/hwnd_util.h"
 #endif
 
+#if BUILDFLAG(IS_OZONE)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 namespace views::test {
 
 namespace {
@@ -2736,17 +2740,16 @@ TEST_F(DesktopWidgetDragTestInteractive, MAYBE_CancelDragDropLoop) {
 
 // Tests that mouse movements made after a drag ends will be handled as
 // moves instead of drags.
-// TODO(crbug.com/375959961): On X11, the native widget's mouse button state is
-// not updated when the mouse button is released to end a drag.
-#if BUILDFLAG(SUPPORTS_OZONE_X11)
-#define MAYBE_RunDragDropLoopUpdatesMouseButtonState \
-  DISABLED_RunDragDropLoopUpdatesMouseButtonState
-#else
-#define MAYBE_RunDragDropLoopUpdatesMouseButtonState \
-  RunDragDropLoopUpdatesMouseButtonState
-#endif
 TEST_F(DesktopWidgetDragTestInteractive,
-       MAYBE_RunDragDropLoopUpdatesMouseButtonState) {
+       RunDragDropLoopUpdatesMouseButtonState) {
+#if BUILDFLAG(IS_OZONE)
+  if (::ui::OzonePlatform::RunningOnX11ForTest()) {
+    // TODO(crbug.com/375959961): On X11, the native widget's mouse button state
+    // is not updated when the mouse button is released to end a drag.
+    GTEST_SKIP() << "On X11, the native widget's mouse button state is not "
+                    "updated when the mouse button is released to end a drag.";
+  }
+#endif
 #if BUILDFLAG(IS_WIN)
   // The test base (views::ViewsTestBase) removes input state lookup.
   // Windows depends on it for getting the correct mouse button state during
