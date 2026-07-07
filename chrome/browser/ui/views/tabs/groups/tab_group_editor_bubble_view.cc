00a190751a0b883791cb73d274d4229abeb61cc8
Mike Cadelago <mcadelago@google.com>
2026-07-07 12:21:23 -0700

    Tab Group Home: Add Home button to Editor Bubble
    This CL adds a "Home" button to the Tab Group Editor Bubble.
    When clicked, it opens a new tab navigating directly to chrome://tab-group-home/.
    
    This button is gated behind the `tabs::kTabGroupHome` feature flag,
    which has its expiry milestone updated to 160.
    
    Bug: none
    Test: TabGroupEditorBubbleViewDialogBrowserTestWithTabGroupHome.TabGroupHomeTrigger
    
    TAG=agy
    CONV=5518d78a-7c35-4409-bd56-441eec0271bf
    
    Change-Id: Ic48ea5a478215093060c6e986b494a986a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7948139
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Mike Cadelago <mcadelago@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658193}

--- chrome/browser/ui/views/tabs/groups/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/groups/tab_group_editor_bubble_view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/chrome_pages.h"
+#include "chrome/browser/ui/tabs/features.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_metrics.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_pref_names.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
@@ -71,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
 #include "components/tabs/public/tab_group.h"
+#include "components/vector_icons/vector_icons.h"
 #include "tab_group_editor_bubble_view.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/accelerators/accelerator.h"
@@ -106,6 +108,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/view_utils.h"
+#include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "ash/ash_element_identifiers.h"
@@ -559,6 +562,9 @@ void TabGroupEditorBubbleView::RebuildMenuContents() {
 
   if (!CanSaveGroups()) {
     simple_menu_items_.push_back(AddChildView(BuildNewTabInGroupButton()));
+    if (base::FeatureList::IsEnabled(tabs::kTabGroupHome)) {
+      simple_menu_items_.push_back(AddChildView(BuildHomeButton()));
+    }
     simple_menu_items_.push_back(
         AddChildView(BuildMoveGroupToNewWindowButton()));
     AddChildView(BuildSeparator());
@@ -566,6 +572,9 @@ void TabGroupEditorBubbleView::RebuildMenuContents() {
     simple_menu_items_.push_back(AddChildView(BuildCloseGroupButton()));
   } else {
     simple_menu_items_.push_back(AddChildView(BuildNewTabInGroupButton()));
+    if (base::FeatureList::IsEnabled(tabs::kTabGroupHome)) {
+      simple_menu_items_.push_back(AddChildView(BuildHomeButton()));
+    }
     simple_menu_items_.push_back(
         AddChildView(BuildMoveGroupToNewWindowButton()));
 
@@ -687,6 +696,18 @@ TabGroupEditorBubbleView::BuildNewTabInGroupButton() {
 
   menu_item->SetProperty(views::kElementIdentifierKey,
                          kTabGroupEditorBubbleNewTabInGroupButtonId);
+  return menu_item;
+}
+
+std::unique_ptr<views::LabelButton>
+TabGroupEditorBubbleView::BuildHomeButton() {
+  std::unique_ptr<views::LabelButton> menu_item = CreateMenuItem(
+      TAB_GROUP_HEADER_CXMENU_HOME,
+      l10n_util::GetStringUTF16(IDS_TAB_GROUP_HEADER_CXMENU_HOME),
+      base::BindRepeating(&TabGroupEditorBubbleView::HomePressed,
+                          base::Unretained(this)),
+      ui::ImageModel::FromVectorIcon(vector_icons::kHomeIcon,
+                                     ui::kColorMenuIcon, kDefaultIconSize));
   return menu_item;
 }
 
@@ -888,6 +909,12 @@ void TabGroupEditorBubbleView::NewTabInGroupPressed() {
   model->delegate()->AddTabAt(GURL(), tabs.end(), true, group_);
   // Close the widget to allow users to continue their work in their newly
   // created tab.
+  GetWidget()->Close();
+}
+
+void TabGroupEditorBubbleView::HomePressed() {
+  chrome::AddSelectedTabWithURL(browser_, GURL("chrome://tab-group-home/"),
+                                ui::PAGE_TRANSITION_TYPED);
   GetWidget()->Close();
 }
 
