b1b9c2c38b220c3aa4487816163469dc073c6064
Avi Drissman <avi@chromium.org>
2026-07-07 11:59:46 -0700

    Avoid duplicate icons in the Dock
    When adding Chromium to the Dock, be sure to remove any existing copies
    in the "recent" section of the file to avoid getting duplicate copies.
    
    Also, while here, update enum style to match modern style.
    
    Fixed: 531769713
    Change-Id: I3d002a9186c766104bcb603abbbea8726a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8049240
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658174}

--- chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_mac_util.mm
+++ chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_mac_util.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace default_browser {
 
 bool ShouldOfferToPin() {
-  return dock::ChromeIsInTheDock() == dock::ChromeInDockFalse;
+  return dock::ChromeIsInTheDock() == dock::ChromeInDockStatus::kNotPresent;
 }
 
 void PinChromeToDock() {
