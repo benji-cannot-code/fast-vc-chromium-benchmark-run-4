54d5296e6a8efa5353905d7c3cc90fcde2cdbb9b
Baran Erfani <baranerf@google.com>
2026-07-07 12:55:11 -0700

    Reland "[Indigo] Invoke anchored message conditionally based on contextual cueing flag"
    This is a reland of commit 95bdf4e9984a7297823571d24590c6fee87e7c17
    
    Fix: Added and enabled kContextualCueingV2 in indigo_browsertest to
    resolve the failures here:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/110730/overview
    
    Original change's description:
    > [Indigo] Invoke anchored message conditionally based on contextual cueing flag
    >
    > Demo: http://screencast/cast/NjMwMjkxMDUzNDEyMzUyMHxhNTNkNTNiNi0xZQ
    >
    > The feature discovery anchored message is only triggered when
    > kContextualCueingV2 is enabled. Otherwise, we only show the suggestion
    > chip (and the anchored message triggered by it).
    >
    > Bug: b:519267141
    > Change-Id: I8f6d3771bfd6650e148fd999a8a851a89f051bf2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8018584
    > Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    > Commit-Queue: Baran Erfani <baranerf@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1657467}
    
    Bug: b:519267141
    Change-Id: Idcea3d19f97091ec068c5b786cf3da581b090b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8051567
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Auto-Submit: Baran Erfani <baranerf@google.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658223}

--- chrome/browser/indigo/indigo_browsertest.cc
+++ chrome/browser/indigo/indigo_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "build/build_config.h"
+#include "chrome/browser/contextual_cueing/features.h"
 #include "chrome/browser/indigo/fake_api.h"
 #include "chrome/browser/indigo/indigo_image_replacement_manager.h"
 #include "chrome/browser/indigo/indigo_page_action_controller.h"
@@ -182,7 +183,8 @@ class IndigoBrowserTest : public InteractiveBrowserTest {
           {{features::kIndigoGenerateUrl.name,
             fake_api_.GetGenerateUrl().spec()},
            {features::kIndigoSkipEnterpriseCheck.name, "true"}}},
-         {blink::features::kImageReplacement, {}}},
+         {blink::features::kImageReplacement, {}},
+         {contextual_cueing::kContextualCueingV2, {}}},
         {});
     InteractiveBrowserTest::SetUp();
   }
@@ -918,7 +920,7 @@ IN_PROC_BROWSER_TEST_F(IndigoBrowserTest, SuggestionChipClickFlow) {
       IndigoServiceFactory::GetForProfile(browser()->profile());
   // Set anchored message as already shown so the suggestion chip shows
   // automatically instead of the anchored message.
-  service->AnchoredMessageShown();
+  service->ContextualCueShown();
   const GURL main_tab_url = embedded_test_server()->GetURL("/image.html");
   RunTestSequence(
       InstrumentTab(kWebContentsId),
--- chrome/browser/indigo/indigo_service.cc
+++ chrome/browser/indigo/indigo_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/component_updater/indigo_component_installer.h"
+#include "chrome/browser/contextual_cueing/features.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/glic/public/glic_enabling.h"
@@ -287,12 +289,15 @@ IndigoService::RegisterLocalEligibilityChangedCallback(
   return local_eligibility_callback_list_.Add(std::move(callback));
 }
 
-bool IndigoService::CanShowAnchoredMessage() const {
-  return base::TimeTicks::Now() >= anchored_message_not_before_;
+bool IndigoService::CanShowContextualCue() const {
+  // TODO(b/519267141): This should also be guarded by the contextual cueing
+  // pref, which gets changed when the user changes the setting.
+  return base::FeatureList::IsEnabled(contextual_cueing::kContextualCueingV2) &&
+         base::TimeTicks::Now() >= contextual_cue_not_before_;
 }
 
-void IndigoService::AnchoredMessageShown() {
-  anchored_message_not_before_ =
+void IndigoService::ContextualCueShown() {
+  contextual_cue_not_before_ =
       base::TimeTicks::Now() +
       features::kIndigoAnchoredMessageResetDuration.Get();
 }
