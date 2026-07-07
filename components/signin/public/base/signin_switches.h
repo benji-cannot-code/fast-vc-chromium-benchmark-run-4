67625997656830ab441cf1082f2a2ed1d3a7d9c5
Samar <samarchehade@google.com>
2026-07-07 03:16:56 -0700

    [Extensions][Al] Enable activityless signin by default for extensions
    Bug: 403867715
    Change-Id: I1877e37eb0c66407ef7a3d3a133633bc261ab2ea
    Fixed: 531412160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036366
    Commit-Queue: Samar Chehade <samarchehade@google.com>
    Auto-Submit: Samar Chehade <samarchehade@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657837}

--- components/signin/public/base/signin_switches.h
+++ components/signin/public/base/signin_switches.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "build/build_config.h"
 #include "components/signin/public/base/signin_buildflags.h"
+#include "extensions/buildflags/buildflags.h"
 
 class PrefService;
 
