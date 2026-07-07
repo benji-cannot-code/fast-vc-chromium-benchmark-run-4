58f51210fbd9e988b1a36b11ee44452cfa1061ca
Dan Harrington <harringtond@chromium.org>
2026-07-07 16:58:46 -0700

    Sync more glic mojo structs to glic_api.ts [2]
    Use @generate glic_api to generate more glic interfaces.
    * Where comments mismatch, prefer the better comments - usually the
      glic_api.ts comments.
    * Where names mismatch, keep the glic_api.ts names. Note that even if
      these names are worse, we can't change them now, and it's better to
      keep the names the same.
    
    Bug: None
    Change-Id: I8d3183f1bd22a8bb88928d18e7566a8b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8052115
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658384}

--- chrome/browser/resources/glic/glic_api_impl/host/conversions.ts
+++ chrome/browser/resources/glic/glic_api_impl/host/conversions.ts
@@ -124,7 +124,7 @@ export function pdfDocumentDataToClient(
   }
   return {
     origin: originToClient(pdfDocumentData.origin),
-    pdfSizeLimitExceeded: pdfDocumentData.sizeLimitExceeded,
+    pdfSizeLimitExceeded: pdfDocumentData.pdfSizeLimitExceeded,
     pdfData,
   };
 }
