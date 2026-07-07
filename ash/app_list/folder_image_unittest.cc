0b068a70e6e92c4a56f65e8637159ebacf71e76e
hiroki oshima <hiroki.oshima@gmail.com>
2026-07-06 22:20:07 -0700

    fix FolderImageSource dangling pointer
    Bug: 40212619
    Change-Id: I96aab1ef94f22119f3b501a963be26c701a3e7cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7906081
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657692}

--- ash/app_list/folder_image_unittest.cc
+++ ash/app_list/folder_image_unittest.cc
@@ -88,6 +88,7 @@ class FolderImageTest : public testing::Test,
 
   void TearDown() override {
     folder_image_->RemoveObserver(&observer_);
+    folder_image_.reset();
     AppListConfigProvider::Get().ResetForTesting();
   }
 
--- ash/app_list/model/app_list_model_unittest.cc
+++ ash/app_list/model/app_list_model_unittest.cc
@@ -78,7 +78,11 @@ class AppListModelTest : public testing::Test {
     model_ = std::make_unique<test::AppListTestModel>();
     model_->AddObserver(&observer_);
   }
-  void TearDown() override { model_->RemoveObserver(&observer_); }
+  void TearDown() override {
+    model_->RemoveObserver(&observer_);
+    model_.reset();
+    AppListConfigProvider::Get().ResetForTesting();
+  }
 
  protected:
   static bool ItemObservedByFolder(const AppListFolderItem* folder,
@@ -272,9 +276,6 @@ TEST_F(AppListModelFolderTest, MergeItemIntoFolder) {
 
 // Tests Icon generation configuration for folders on different grid types.
 TEST_F(AppListModelFolderTest, NonSharedConfigIconGeneration) {
-  // Ensure any configs set by previous tests are cleared.
-  AppListConfigProvider::Get().ResetForTesting();
-
   // Start with kRegular config available.
   const AppListConfig* regular_config =
       AppListConfigProvider::Get().GetConfigForType(AppListConfigType::kRegular,
@@ -321,8 +322,6 @@ TEST_F(AppListModelFolderTest, NonSharedConfigIconGeneration) {
   EXPECT_FALSE(ItemObservedByFolder(
       folder, folder->item_list()->item_at(num_observed_apps),
       AppListConfigType::kDense));
-
-  AppListConfigProvider::Get().ResetForTesting();
 }
 
 TEST_F(AppListModelFolderTest, MergeItems) {
