5eafe467de7ca1eb8453159ba0add67bb968d4e8
Jihad Hanna <jihadghanna@google.com>
2026-07-07 02:04:58 -0700

    Add did-trigger-js-autofill to autofill-information HTML debug attribute
    This is mainly a debugging feature but will also help during
    human-testing.
    
    Bug: 529775544
    Change-Id: I46a7ee9bbaf314c69e0272edea6dc9624a16e7a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8044782
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657791}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -142,6 +142,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autocomplete_parsing_util.h"
 #include "components/autofill/core/common/autofill_clock.h"
 #include "components/autofill/core/common/autofill_data_validation.h"
+#include "components/autofill/core/common/autofill_debug_features.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_internals/log_message.h"
 #include "components/autofill/core/common/autofill_internals/logging_scope.h"
@@ -2491,6 +2492,10 @@ void BrowserAutofillManager::OnDidDetectJavaScriptAutofillImpl(
   constexpr size_t kMinFieldsChanged = 3;
   if (address_fields_count >= kMinFieldsChanged) {
     trigger_field->set_did_trigger_javascript_autofill(true);
+    if (base::FeatureList::IsEnabled(
+            features::debug::kAutofillShowTypePredictions)) {
+      driver().SendTypePredictionsToRenderer(*form_structure);
+    }
   }
 }
 
