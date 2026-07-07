6e47cac9b7426e1d74aa0dd0108953023360f185
Kalvin Lee <kdlee@chromium.org>
2026-07-07 01:03:24 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    ash/webui/file_manager
    
    This CL was uploaded by git cl split.
    
    R=simmonsjosh@google.com
    
    Bug: 529948677
    Change-Id: Ia045fd1db151136b1ddea5ab1d0815d547d29fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030261
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Josh Simmons <simmonsjosh@google.com>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657753}

--- ash/webui/file_manager/resource_loader.cc
+++ ash/webui/file_manager/resource_loader.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/file_manager/resource_loader.h"
 
+#include "base/containers/span.h"
 #include "base/strings/string_util.h"
 
 namespace ash {
