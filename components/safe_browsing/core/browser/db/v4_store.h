d09c91105df4135ffa1e09c321e33fb2aff5fb74
thefrog <thefrog@chromium.org>
2026-07-07 11:09:20 -0700

    Implement V5Store::RecordAndReturnFileSize
    This CL mostly just moves the V4Store::RecordAndReturnFileSize over to
    the shared SBStore::RecordAndReturnFileSize. A change introduced to the
    method is that relevant v5 store suffixes are also allowed to log
    specific linear histograms.
    
    New histograms (just parallels of existing v4 histograms):
     - SafeBrowsing.V5Database.Size
     - SafeBrowsing.V5Database.Size{Store}_v5
     - SafeBrowsing.V5Database.SizeLinear
     - SafeBrowsing.V5Database.SizeLinear.UrlSoceng_v5
     - SafeBrowsing.V5Database.SizeLinear.ChromeExtMalware_v5
     - SafeBrowsing.V5Database.SizeLinear.UrlMalBin_v5
    
    Bug: 362791941
    Change-Id: Id1724966f71750ad622da5d5069c44dc6f30d807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8021382
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658109}

--- components/safe_browsing/core/browser/db/v4_store.h
+++ components/safe_browsing/core/browser/db/v4_store.h
@@ -195,7 +195,6 @@ class V4Store : public SBStore {
                    const scoped_refptr<base::SequencedTaskRunner>& runner,
                    UpdatedStoreReadyCallback callback) override;
 
-  int64_t RecordAndReturnFileSize(const std::string& base_metric) override;
 
   std::string DebugString() const;
 
