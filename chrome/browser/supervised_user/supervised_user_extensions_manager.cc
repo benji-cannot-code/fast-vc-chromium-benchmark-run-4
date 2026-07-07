3cb11e48f473624043c7d37e397eb67b21de4130
Tomek Jurkiewicz <tju@google.com>
2026-07-07 10:11:19 -0700

    Remove SupervisedUserServiceFactory::GetForBrowserContext
    This change removes the single-use GetForBrowserContext static method from
    SupervisedUserServiceFactory and updates its call sites to use
    GetForProfile(Profile::FromBrowserContext(context)) instead.
    
    Bug: 469335277
    Change-Id: I1648fc998b9affedac5a9b7e55f7b2c912f8a2fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8023762
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1658043}

--- chrome/browser/supervised_user/supervised_user_extensions_manager.cc
+++ chrome/browser/supervised_user/supervised_user_extensions_manager.cc
@@ -161,7 +161,8 @@ bool SupervisedUserExtensionsManager::IsExtensionAllowed(
 
 bool SupervisedUserExtensionsManager::CanInstallExtensions() const {
   supervised_user::SupervisedUserService* supervised_user_service =
-      SupervisedUserServiceFactory::GetForBrowserContext(context_);
+      SupervisedUserServiceFactory::GetForProfile(
+          Profile::FromBrowserContext(context_));
   bool has_custodian = supervised_user_service->GetCustodian() ||
                        supervised_user_service->GetSecondCustodian();
     return has_custodian;
@@ -390,7 +391,8 @@ void SupervisedUserExtensionsManager::UpdateApprovedExtension(
 std::u16string SupervisedUserExtensionsManager::GetExtensionsLockedMessage()
     const {
   supervised_user::SupervisedUserService* supervised_user_service =
-      SupervisedUserServiceFactory::GetForBrowserContext(context_);
+      SupervisedUserServiceFactory::GetForProfile(
+          Profile::FromBrowserContext(context_));
   std::optional<supervised_user::Custodian> custodian =
       supervised_user_service->GetCustodian();
   return l10n_util::GetStringFUTF16(
