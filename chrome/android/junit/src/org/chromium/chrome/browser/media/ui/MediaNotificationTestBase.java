adac006e3a6b56575636bb7b803a6728beb1c7a9
Jenna Himawan <jhimawan@google.com>
2026-07-07 09:48:01 -0700

    Fix MediaNotificationServiceLifecycleTest failure and reset state
    Reset MediaNotificationManager.sMultipleMediaNotificationsEnabled in
    MediaNotificationTestBase setUp and tearDown. In testProcessIntentFailureStopsService,
    conditionally assert null controller based on isMultipleMediaNotificationsEnabled.
    
    TAG=agy
    CONV=d048c1b4-9af6-4bba-a4e2-bfedcb1b19a8
    
    Bug: 522397811
    Change-Id: Ibeaddbdcc791a70cd93923d4e4edc58df1e507f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8040023
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658021}

--- chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaNotificationTestBase.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/media/ui/MediaNotificationTestBase.java
@@ -105,6 +105,11 @@ public class MediaNotificationTestBase {
         FeatureOverrides.newBuilder()
                 .disable(ChromeFeatureList.ALLOW_MULTIPLE_MEDIA_NOTIFICATIONS)
                 .applyWithoutOverwrite();
+        // The MediaNotificationManager is in components, but the flag is in Chrome. In production
+        // code, we expect Chrome code to call
+        // MediaNotificationManager.setMultipleMediaNotificationsEnabled. In the test, this is not
+        // happening, so we have to do it manually.
+        MediaNotificationManager.setMultipleMediaNotificationsEnabled(false);
 
         mMockContext = spy(RuntimeEnvironment.application);
         ContextUtils.initApplicationContextForTests(mMockContext);
@@ -184,6 +189,7 @@ public class MediaNotificationTestBase {
     public void tearDown() {
         MediaNotificationManager.hideForAllTabs(getNotificationId());
         MediaNotificationManager.setService(getNotificationId(), null);
+        MediaNotificationManager.setMultipleMediaNotificationsEnabled(false);
     }
 
     MediaNotificationController getController() {
