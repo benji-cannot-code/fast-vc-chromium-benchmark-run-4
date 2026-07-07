6a0f8880a6a2210e28e495e68519033792ed2d4b
Colin Blundell <blundell@chromium.org>
2026-07-07 01:48:38 -0700

    [Blink] Remove CanvasNon2DResourceProvider raster measurement
    This is vestigial from this code being shared with Canvas2D.
    
    Bug: 352263194
    Change-Id: Ice2f963767728d95e0f5b8357dbdfdf297490b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036905
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657777}

--- third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/flush_for_image_listener.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
 #include "third_party/blink/renderer/platform/graphics/memory_managed_paint_recorder.h"
-#include "third_party/blink/renderer/platform/graphics/scoped_raster_timer.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/web_graphics_context_3d_provider_wrapper.h"
 #include "third_party/blink/renderer/platform/instrumentation/canvas_memory_dump_provider.h"
@@ -62,7 +61,6 @@ class CanvasImageProvider;
 class CanvasResourceProviderDelegate;
 class WebGraphicsSharedImageInterfaceProvider;
 
-
 class PLATFORM_EXPORT CanvasNon2DResourceProvider
     : public CanvasMemoryDumpClient,
       public MemoryManagedPaintRecorder::Client,
@@ -70,8 +68,7 @@ class PLATFORM_EXPORT CanvasNon2DResourceProvider
       public FlushForImageObserver,
       public WebGraphicsContext3DProviderWrapper::DestructionObserver,
       public viz::ContextLostObserver,
-      public BitmapGpuChannelLostObserver,
-      public ScopedRasterTimer::Host {
+      public BitmapGpuChannelLostObserver {
  public:
   static std::unique_ptr<CanvasNon2DResourceProvider> Create(
       gfx::Size size,
