3e605570649d5fbbad8d8771fa944104ed85f00c
Ameur Hosni <ameurhosni@google.com>
2026-07-07 10:44:50 -0700

    [IOS][AIM] Cleanup kCobrowseAimHistory flag
    Bug: 532113397
    Change-Id: I6913d2a0adb36c66752d2d4f99b6eee07d28e034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8052812
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658081}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1198,12 +1198,6 @@ bool IsPlusButtonInFakeboxEnabled() {
   return base::FeatureList::IsEnabled(kPlusButtonInFakebox);
 }
 
-BASE_FEATURE(kCobrowseAimHistory, base::FEATURE_DISABLED_BY_DEFAULT);
-
-bool IsCobrowseAimHistoryEnabled() {
-  return base::FeatureList::IsEnabled(kCobrowseAimHistory);
-}
-
 BASE_FEATURE(kAssistantAimMinimizedState, base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsAssistantAimMinimizedStateEnabled() {
