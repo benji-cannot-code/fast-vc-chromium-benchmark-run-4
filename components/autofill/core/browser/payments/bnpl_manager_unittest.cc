9b3705ff8c2c975cf572f41ca2acfd3ba420a56e
viplavkadam <viplavkadam@google.com>
2026-07-06 21:30:00 -0700

    [BNPL-Ai][Clank] Enable Ai based amount extraction by default
    Bug: 502668950
    Change-Id: Iec6e2d721256169b6fb99901f365b26ba4221148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8046402
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1657687}

--- components/autofill/core/browser/payments/bnpl_manager_unittest.cc
+++ components/autofill/core/browser/payments/bnpl_manager_unittest.cc
@@ -2555,6 +2555,9 @@ TEST_F(BnplManagerTest,
 
 TEST_F(BnplManagerTest,
        OnUserDecisionToUseBnpl_AiBasedAmountExtractionFeatureDisabled) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAutofillEnableAiBasedAmountExtraction);
   EXPECT_CALL(*mock_amount_extraction_manager_,
               TriggerCheckoutAmountExtractionWithAi())
       .Times(0);
@@ -2884,6 +2887,9 @@ TEST_F(BnplManagerTest,
 
 TEST_F(BnplManagerTest,
        OnAmountExtractionReturned_WithTimeout_AfterBnplSelected) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAutofillEnableAiBasedAmountExtraction);
   SetUpLinkedBnplIssuer(/*price_lower_bound_in_micros=*/10'000'000,
                         /*price_higher_bound_in_micros=*/200'000'000,
                         IssuerId::kBnplAffirm,
@@ -2912,6 +2918,9 @@ TEST_F(BnplManagerTest,
 
 TEST_F(BnplManagerTest,
        OnAmountExtractionReturned_WithInvalidAmount_AfterBnplSelected) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAutofillEnableAiBasedAmountExtraction);
   SetUpLinkedBnplIssuer(/*price_lower_bound_in_micros=*/10'000'000,
                         /*price_higher_bound_in_micros=*/200'000'000,
                         IssuerId::kBnplAffirm,
@@ -2940,6 +2949,9 @@ TEST_F(BnplManagerTest,
 
 TEST_F(BnplManagerTest,
        OnAmountExtractionReturned_WithUnsupportedAmount_AfterBnplSelected) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAutofillEnableAiBasedAmountExtraction);
   const int64_t extracted_amount = 0;
   SetUpLinkedBnplIssuer(/*price_lower_bound_in_micros=*/10'000'000,
                         /*price_higher_bound_in_micros=*/3'000'000'000,
@@ -2984,6 +2996,9 @@ TEST_F(BnplManagerTest,
 
 TEST_F(BnplManagerTest,
        OnAmountExtractionReturned_WithValidAmount_AfterBnplSelected) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAutofillEnableAiBasedAmountExtraction);
   const int64_t extracted_amount = 1000000000;
   SetUpLinkedBnplIssuer(/*price_lower_bound_in_micros=*/10'000'000,
                         /*price_higher_bound_in_micros=*/3'000'000'000,
@@ -3045,6 +3060,9 @@ TEST_F(BnplManagerTest,
 TEST_F(
     BnplManagerTest,
     OnUserDecisionToUseBnpl_WhenAmountIsNotSet_ShowProgressUiNotSelectionUi) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      features::kAutofillEnableAiBasedAmountExtraction);
   EXPECT_CALL(GetBnplUiDelegate(), ShowSelectBnplIssuerUi).Times(0);
   EXPECT_CALL(GetBnplUiDelegate(),
               ShowProgressUi(
