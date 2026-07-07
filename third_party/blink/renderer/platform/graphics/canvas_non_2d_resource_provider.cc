1b8bd1ccc13a4eaf4ccf29ad54c6262320d3e92a
Colin Blundell <blundell@chromium.org>
2026-07-06 23:30:04 -0700

    [Blink] Tighten CanvasNon2DResourceProvider public interface
    Bug: 352263194
    Change-Id: I98742a7d700d70d84935829c35533095b35c8522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8045140
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657699}

--- third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/functional/function_ref.h"
+#include "base/memory/ptr_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
@@ -146,9 +147,9 @@ CanvasNon2DResourceProvider::Create(
   }
 #endif
 
-  auto provider = std::make_unique<CanvasNon2DResourceProvider>(
+  auto provider = base::WrapUnique(new CanvasNon2DResourceProvider(
       size, format, alpha_type, color_space, hdr_metadata,
-      context_provider_wrapper, shared_image_usage_flags, delegate);
+      context_provider_wrapper, shared_image_usage_flags, delegate));
 
   return provider->IsValid() ? std::move(provider) : nullptr;
 }
@@ -207,9 +208,9 @@ CanvasNon2DResourceProvider::CreateForSoftwareCompositor(
   CHECK(format == viz::SharedImageFormat::N32Format() ||
         format == viz::SinglePlaneFormat::kRGBA_F16);
 
-  auto provider = std::make_unique<CanvasNon2DResourceProvider>(
+  auto provider = base::WrapUnique(new CanvasNon2DResourceProvider(
       size, format, alpha_type, color_space, hdr_metadata,
-      shared_image_interface_provider, delegate);
+      shared_image_interface_provider, delegate));
   return provider->IsValid() ? std::move(provider) : nullptr;
 }
 
