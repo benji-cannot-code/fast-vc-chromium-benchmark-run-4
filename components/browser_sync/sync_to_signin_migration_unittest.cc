83293d13671a485f5de138b5fd551bf7bf4efccc
Ari Chivukula <arichiv@chromium.org>
2026-07-07 09:21:01 -0700

    [Encrypt Bookmarks] Rename encrypted files for second rollout
    To prevent reuse of stale data the file name of the encrypted bookmarks
    is updated slightly. A comment is added to encourage later cleanup.
    
    Bug: 435317726
    Change-Id: I8d61d7fd28665888a316db0651605a52e832c2e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8054504
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657995}

--- components/browser_sync/sync_to_signin_migration_unittest.cc
+++ components/browser_sync/sync_to_signin_migration_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
+#include "components/bookmarks/common/bookmark_constants.h"
 #include "components/bookmarks/common/bookmark_features.h"
 #include "components/browser_sync/browser_sync_switches.h"
 #include "components/prefs/testing_pref_service.h"
@@ -1532,17 +1533,21 @@ class SyncToSigninMigrationDataTypesTest
   }
 
   base::FilePath GetBookmarksLocalStorePath() const {
-    return fake_profile_dir_.GetPath().AppendASCII("Bookmarks");
+    return fake_profile_dir_.GetPath().Append(
+        bookmarks::kLocalOrSyncableBookmarksFileName);
   }
   base::FilePath GetBookmarksAccountStorePath() const {
-    return fake_profile_dir_.GetPath().AppendASCII("AccountBookmarks");
+    return fake_profile_dir_.GetPath().Append(
+        bookmarks::kAccountBookmarksFileName);
   }
 
   base::FilePath GetEncryptedBookmarksLocalStorePath() const {
-    return fake_profile_dir_.GetPath().AppendASCII("EncryptedBookmarks");
+    return fake_profile_dir_.GetPath().Append(
+        bookmarks::kEncryptedLocalOrSyncableBookmarksFileName);
   }
   base::FilePath GetEncryptedBookmarksAccountStorePath() const {
-    return fake_profile_dir_.GetPath().AppendASCII("EncryptedAccountBookmarks");
+    return fake_profile_dir_.GetPath().Append(
+        bookmarks::kEncryptedAccountBookmarksFileName);
   }
 
   base::FilePath GetPasswordsLocalStorePath() const {
