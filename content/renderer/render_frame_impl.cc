568c59f5899003278112b3941ad827314d9d30cf
Shunya Shishido <sisidovski@chromium.org>
2026-07-06 21:27:24 -0700

    Reduce unnecessary URLLoaderFactory cloning
    This CL introduces the base::Feature 'ReduceMojoURLLoaderFactoryCloning'
    and reduces the redundant double-cloning of URLLoaderFactoryBundle when
    creating new local main frames in RenderFrameImpl::CreateNewWindow().
    
    Design doc:
    https://docs.google.com/document/d/1YvgZowHjNnceuox5G_a9NbnElrSW2OcJ2h3jikBSzbY/edit?usp=sharing
    
    TAG=agy
    
    Bug: 530477250
    Change-Id: I6914b159102c9c24b320341ece1f71b30b222397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8001417
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657686}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -6871,6 +6871,11 @@ RenderFrameImpl::CloneLoaderFactories() {
       std::move(pending_bundle));
 }
 
+std::unique_ptr<network::PendingSharedURLLoaderFactory>
+RenderFrameImpl::CloneLoaderFactoryBundle() {
+  return GetLoaderFactoryBundle()->Clone();
+}
+
 blink::scheduler::WebAgentGroupScheduler&
 RenderFrameImpl::GetAgentGroupScheduler() {
   return agent_scheduling_group_->agent_group_scheduler();
@@ -7126,7 +7131,10 @@ WebView* RenderFrameImpl::CreateNewWindow(
   main_frame_params->widget_params = std::move(widget_params);
   main_frame_params->subresource_loader_factories =
       base::WrapUnique(static_cast<blink::PendingURLLoaderFactoryBundle*>(
-          CloneLoaderFactories()->Clone().release()));
+          base::FeatureList::IsEnabled(
+              features::kReduceMojoURLLoaderFactoryCloning)
+              ? CloneLoaderFactoryBundle().release()
+              : CloneLoaderFactories()->Clone().release()));
 
   view_params->main_frame =
       mojom::CreateMainFrameUnion::NewLocalParams(std::move(main_frame_params));
