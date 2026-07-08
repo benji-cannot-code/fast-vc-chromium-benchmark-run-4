ef1cf651e9174cad74618c217d3de9c05f6b3dad
Colin Blundell <blundell@chromium.org>
2026-07-08 00:47:36 -0700

    [Blink] Eliminate WebGpuRecyclableResourceProvider::FlushRecording()
    Inline the method into its one callsite.
    
    Bug: 352263194
    Change-Id: I641378e224264999d323e3f9aa64f4727e5f9838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8045484
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658551}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
@@ -176,7 +176,6 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
   void OnContextLost() override;
 
   bool ShouldReplaceTargetBuffer();
-  void FlushRecording(cc::PaintRecord last_recording);
 
   std::unique_ptr<gpu::RasterScopedAccess> WillDrawInternal();
 
