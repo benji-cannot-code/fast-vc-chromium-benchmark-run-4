eaa6e18f607f8cdacee0fc19360f9a5e024737e0
Andy Paicu <andypaicu@chromium.org>
2026-07-08 05:13:22 -0700

    Implement cached Mac media permission settings
    Implement a cache for system media permission settings on Mac. The cache
    is used for the IsAllowed/Denied/Prompt functions, and is updated when
    browser window activation happens.
    
    Also remove previous code which attempted to bandaid-fix the performance
    issue of checking system permissions on Mac during hot navigation paths.
    
    Bug: 524529903
    Change-Id: I691da73108e70534a1db0a91e8ae025f9c6b976c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035761
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658660}

--- chrome/browser/permissions/system/system_media_capture_permissions_mac.mm
+++ chrome/browser/permissions/system/system_media_capture_permissions_mac.mm
@@ -171,7 +171,7 @@ void RequestSystemVideoCapturePermission(base::OnceClosure callback) {
 
 void SetMediaAuthorizationWrapperForTesting(
     MediaAuthorizationWrapper* wrapper) {
-  CHECK(!g_media_authorization_wrapper_for_tests);
+  CHECK(!wrapper || !g_media_authorization_wrapper_for_tests);
   g_media_authorization_wrapper_for_tests = wrapper;
 }
 
