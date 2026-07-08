ab37a93f61cab9aaaa4afc00b703760f2be4fc94
Will Harris <wfh@chromium.org>
2026-07-08 06:23:10 -0700

    Small fixes to service tests
    Two fixes are made - the first is to pass the null terminated string
    for the event name rather than std::data. Even though std::string
    is guaranteeed to be null terminated since C++11, it makes more sense
    to use c_str here.
    
    The second moves the ServiceEnvironment pointer to C++17 inline
    variables which removes an unneeded out-of-line definition.
    
    No functional changes expected.
    
    Bug: None
    Change-Id: I4e4cbe046674bc146c11867e446c53f70983954a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8060405
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658685}

--- chrome/elevation_service/elevation_service_unittest.cc
+++ chrome/elevation_service/elevation_service_unittest.cc
@@ -118,11 +118,8 @@ class ElevationServiceTest : public ::testing::Test {
   }
 
  private:
-  static ServiceEnvironment* service_environment_;
+  static inline ServiceEnvironment* service_environment_ = nullptr;
 };
-
-// static
-ServiceEnvironment* ElevationServiceTest::service_environment_ = nullptr;
 
 TEST_F(ElevationServiceTest, AcceptInvitation) {
   base::win::ScopedCOMInitializer com_initializer;
@@ -176,7 +173,7 @@ MULTIPROCESS_TEST_MAIN(RunIsolatedChromeInChild) {
 
   base::WaitableEvent event(base::win::ScopedHandle(::CreateEventA(
       /*lpEventAttributes=*/nullptr, /*bManualReset=*/FALSE,
-      /*bInitialState=*/FALSE, /*lpName=*/std::data(event_name))));
+      /*bInitialState=*/FALSE, /*lpName=*/event_name.c_str())));
 
   DWORD last_error = 0;
   ULONG_PTR proc_handle;
