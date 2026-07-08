02217c7b8c2f921a547bd52cc7762cfc4d17d6f3
Joe Mason <joenotcharles@google.com>
2026-07-08 07:48:01 -0700

    Add a feature to revert to the pre-M149 tab switch measurement on Mac
    The change to this measurement may have introduced an unintended
    regression. This feature will be used to verify the upcoming regression
    fix against the old behaviour.
    
    Bug: 469624808
    Change-Id: Iccea4f4f0fca109f4470e0e137e24d0068aa165f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8060164
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658744}

--- third_party/blink/common/page/content_to_visible_time_request.cc
+++ third_party/blink/common/page/content_to_visible_time_request.cc
@@ -12,6 +12,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 std::optional<RecordContentToVisibleTimeRequest>
+RecordContentToVisibleTimeRequest::ExtractAllTabSwitchEvents() {
+  // Sort all events to extract to the end.
+  auto first_result = std::partition(
+      events.begin(), events.end(), [](const VisibleTimeEvent& event) {
+        return !std::holds_alternative<VisibleTimeEvent::TabSwitchReason>(
+            event.reason);
+      });
+  if (first_result == events.end()) {
+    return std::nullopt;
+  }
+
+  // Move the extracted events to the result vector.
+  auto result = std::make_optional<RecordContentToVisibleTimeRequest>();
+  result->events.insert(result->events.end(),
+                        std::make_move_iterator(first_result),
+                        std::make_move_iterator(events.end()));
+  events.erase(first_result, events.end());
+  return result;
+}
+
+std::optional<RecordContentToVisibleTimeRequest>
 RecordContentToVisibleTimeRequest::ExtractTabSwitchEventsWithSavedFrame() {
   // Sort all events to extract to the end.
   auto first_result = std::partition(
@@ -31,6 +52,14 @@ RecordContentToVisibleTimeRequest::ExtractTabSwitchEventsWithSavedFrame() {
                         std::make_move_iterator(events.end()));
   events.erase(first_result, events.end());
   return result;
+}
+
+bool RecordContentToVisibleTimeRequest::AllEventsAreTabSwitches() const {
+  return !events.empty() &&
+         std::ranges::all_of(events, [](const blink::VisibleTimeEvent& event) {
+           return std::holds_alternative<VisibleTimeEvent::TabSwitchReason>(
+               event.reason);
+         });
 }
 
 bool RecordContentToVisibleTimeRequest::AllEventsAreTabSwitchesWithSavedFrame()
