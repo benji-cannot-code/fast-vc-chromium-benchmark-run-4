d75f531992a7380ad9b3a183c2ded868231bccaa
hiroki oshima <hiroki.oshima@gmail.com>
2026-07-08 02:23:32 -0700

    fix ShelfTooltipManager dangling pointer
    Bug: 40212619
    Change-Id: I336455c548a9c1c47854b66daea39f0f37890d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831635
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1658596}

--- ash/shelf/shelf_tooltip_manager.cc
+++ ash/shelf/shelf_tooltip_manager.cc
@@ -187,6 +187,10 @@ void ShelfTooltipManager::OnKeyEvent(ui::KeyEvent* event) {
   Close();
 }
 
+void ShelfTooltipManager::OnShelfShuttingDown() {
+  bubble_ = nullptr;
+}
+
 void ShelfTooltipManager::OnShelfVisibilityStateChanged(
     ShelfVisibilityState new_state) {
   if (new_state == SHELF_HIDDEN)
