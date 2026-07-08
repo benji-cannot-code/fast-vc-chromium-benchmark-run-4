13998a06be8b0314e9c0202ac31308054135455a
Michael Wu <mklw@google.com>
2026-07-08 11:24:31 -0700

    [Tab Strip Glic Button] Update Glic button a11y and tooltip behavior
    - Decouples CompositorButton's tooltip text and accessibility description.
    - Restricts Glic button tooltip to "Open Gemini in Chrome" when the UI is closed.
    - Sets the accessibility description to "Ask Gemini, disabled" in Incognito.
    
    Bug: 518925727
    Change-Id: Ide8bb35a20650a599512613de27af7c0c1d7ddef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951124
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1658944}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -2449,6 +2449,8 @@ public class CompositorViewHolder extends FrameLayout
 
             node.setBoundsInParent(rectToPx(mTouchTarget));
             node.setContentDescription(getAccessibilityDescription(view));
+            node.setEnabled(view.isEnabled());
+            node.setClassName(view.getAccessibilityClassName());
             if (view.hasClickAction()) {
                 node.addAction(AccessibilityNodeInfoCompat.ACTION_CLICK);
                 node.setClickable(true);
