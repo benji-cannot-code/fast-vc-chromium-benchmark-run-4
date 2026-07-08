6c9c987c68f75bc41d92d28e85e4710c02eda2de
Colin Blundell <blundell@chromium.org>
2026-07-08 00:01:17 -0700

    [Blink] Eliminate WebGpuMailboxTexture call of ProduceCanvasResource()
    Effectively inlines the method body [1] at the callsite. Step toward
    elimination of this method existing on WebGpuRecyclableResourceProvider.
    Note that EndWriteAccess() is a no-op if the GPU context is lost [2].
    Note also that CanvasResourceSharedImage::GetSharedImage() is guaranteed
    to be non-null if the resource is non-null, as it is non-null on
    construction [3] and is never subsequently modified [4].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.cc;l=548-561;drc=458a490c2544e4ea08bb9457d8fe8f5c65af07c4
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.cc;l=307-309;drc=458a490c2544e4ea08bb9457d8fe8f5c65af07c4;bpv=1;bpt=1
    [3] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/shared_image_pool.cc;l=31;drc=458a490c2544e4ea08bb9457d8fe8f5c65af07c4
    [4] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/shared_image_pool.h;l=126;drc=458a490c2544e4ea08bb9457d8fe8f5c65af07c4;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I88f365f909522696d233749e0a73110a40fccdf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035093
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658536}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
@@ -75,6 +75,10 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
   SkAlphaType GetAlphaType() const { return alpha_type_; }
 
   scoped_refptr<CanvasResource> ProduceCanvasResource();
+  scoped_refptr<gpu::ClientSharedImage> GetSharedImage() const;
+  gpu::SyncToken GetSyncToken() const;
+
+  void EndWriteAccess();
 
   // NOTE: Can only be used if this instance is accelerated.
   bool UploadToBackingSharedImage(const SkPixmap& pixmap,
@@ -146,7 +150,6 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
   size_t GetSize() const override;
 
   void EnsureWriteAccess();
-  void EndWriteAccess();
 
   scoped_refptr<CanvasResourceSharedImage> NewOrRecycledResource();
   bool IsValid() const;
