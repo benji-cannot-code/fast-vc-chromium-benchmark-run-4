bb7486afa28fa399593e8295eb90b0210a8f63dd
Will Harris <wfh@chromium.org>
2026-07-07 17:32:33 -0700

    Remove some unneeded includes and deps in c/b/os_crypt
    Bug: None
    Change-Id: Ia35c7f2171871722b31d5fc0d6264718e61959a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8060504
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658410}

--- chrome/browser/os_crypt/app_bound_encryption_provider_win.cc
+++ chrome/browser/os_crypt/app_bound_encryption_provider_win.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/types/expected.h"
 #include "chrome/browser/os_crypt/app_bound_encryption_win.h"
-#include "components/crash/core/common/crash_key.h"
 #include "components/os_crypt/async/common/algorithm.mojom.h"
 #include "components/os_crypt/async/common/encryptor.h"
 #include "components/prefs/pref_registry_simple.h"
