57a6f62bb2dae6d120396eacbf82255119c1a3b0
Aditya Gupta <adigupt@google.com>
2026-07-08 11:59:03 -0700

    [Multistep filter] Clean up obsolete service code
    This CL removes unused suggestion generation and extraction logic,
    observers, and testing APIs from MultistepFilterService, and updates
    corresponding browser tests.
    
    Bug: b:531717350
    Change-Id: Ifa1bc1dd35791679d81592b0a6c83323308cedd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061789
    Commit-Queue: Aditya Gupta <adigupt@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658973}

--- components/multistep_filter/core/multistep_filter_ui_delegate.h
+++ components/multistep_filter/core/multistep_filter_ui_delegate.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "base/memory/weak_ptr.h"
 #include "components/multistep_filter/core/data_models/url_filter_suggestion.h"
 
 namespace multistep_filter {
@@ -30,8 +29,6 @@ class MultistepFilterUiDelegate {
   // Called when a suggestion is generated (or fails to generate).
   virtual void OnSuggestionGenerated(
       std::optional<UrlFilterSuggestion> suggestion) = 0;
-
-  virtual base::WeakPtr<MultistepFilterUiDelegate> GetWeakPtr() = 0;
 };
 
 }  // namespace multistep_filter
