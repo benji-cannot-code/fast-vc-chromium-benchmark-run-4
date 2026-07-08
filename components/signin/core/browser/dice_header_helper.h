f79f00f278deb28723a45c4667b25d6b83272d44
Monica Salama <msalama@chromium.org>
2026-07-08 01:05:01 -0700

    [LinkedAcc] Send version=2 in X-Chrome-ID-Consistency-Request
    This adds the kDiceHeaderVersion2 feature flag which, when enabled,
    updates the X-Chrome-ID-Consistency-Request header to send version=2.
    This signals to Gaia that Chrome supports semicolon-separated (';')
    key=value pairs in the X-Chrome-ID-Consistency-Response header.
    
    Bug: 475435113
    Change-Id: Ia3fa80474e81744b358bc3f97167ee00593aa6f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8053508
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Sasha Kunitskaya <saskekun@google.com>
    Commit-Queue: Monica Salama <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658566}

--- components/signin/core/browser/dice_header_helper.h
+++ components/signin/core/browser/dice_header_helper.h
@@ -22,6 +22,7 @@ namespace signin {
 
 // Version of the Dice protocol.
 extern const char kDiceProtocolVersion[];
+extern const char kDiceProtocolVersion2[];
 extern const char kGoogleSignoutResponseHeader[];
 
 // SigninHeaderHelper implementation managing the Dice header.
