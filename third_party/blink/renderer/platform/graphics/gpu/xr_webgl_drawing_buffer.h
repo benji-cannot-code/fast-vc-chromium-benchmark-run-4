1495a035fc33eb84a70ba276916c92b95c286cae
Saifuddin Hitawala <hitawala@chromium.org>
2026-07-08 06:50:11 -0700

    [blink] Remove size from ColorBuffer in favor of shared image params
    Remove size from ColorBuffer as it is the same as that within
    shared image. This remove it and updates callsites to use data from
    shared image within ColorBuffer.
    
    Bug: 494245873
    Change-Id: Ic3f873ae1cb486fcaf7ea723d81f6a0ff5970390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8057104
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658704}

--- third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.h
+++ third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.h
@@ -63,7 +63,6 @@ class PLATFORM_EXPORT XRWebGLDrawingBuffer
   struct PLATFORM_EXPORT ColorBuffer
       : public ThreadSafeRefCounted<ColorBuffer> {
     ColorBuffer(base::WeakPtr<XRWebGLDrawingBuffer>,
-                const gfx::Size&,
                 scoped_refptr<gpu::ClientSharedImage> shared_image,
                 std::unique_ptr<gpu::SharedImageTexture> texture);
     ColorBuffer(const ColorBuffer&) = delete;
@@ -85,7 +84,6 @@ class PLATFORM_EXPORT XRWebGLDrawingBuffer
     // destroyed by the BeginDestruction method, which will eventually drain all
     // of its ColorBuffers.
     base::WeakPtr<XRWebGLDrawingBuffer> drawing_buffer;
-    const gfx::Size size;
 
     // The client shared image backing this color buffer.
     scoped_refptr<gpu::ClientSharedImage> shared_image;
