57fdc746177df38706d5c2f990b9de72c8100501
Colin Blundell <blundell@chromium.org>
2026-07-08 01:08:33 -0700

    [Blink] Eliminate WebGPURecyclableRP holding SharedImagePool
    Have it create the CanvasResourceSI directly.
    
    Note: The ClientSI creation code comes from [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/shared_image_pool.cc;l=124-128;drc=458a490c2544e4ea08bb9457d8fe8f5c65af07c4
    
    Bug: 352263194
    Change-Id: Ie3925f4703aef4342d01e2bd588a376063b685dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8054046
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658569}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gpu/raster_context_provider.h"
 #include "components/viz/common/resources/shared_image_format.h"
 #include "gpu/command_buffer/client/client_shared_image.h"
-#include "gpu/command_buffer/client/shared_image_pool.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/ipc/client/client_shared_image_interface.h"
@@ -155,9 +154,6 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
   gpu::raster::RasterInterface* RasterInterface() const;
   base::WeakPtr<WebGpuRecyclableResourceProvider> CreateWeakPtr();
 
-  // The maximum number of in-flight resources waiting to be used for
-  // recycling.
-  static constexpr int kMaxRecycledCanvasResources = 3;
 
   CanvasResourceSharedImage* resource() {
     return static_cast<CanvasResourceSharedImage*>(resource_.get());
@@ -186,9 +182,6 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
   std::unique_ptr<CanvasImageProvider> canvas_image_provider_;
   std::unique_ptr<MemoryManagedPaintRecorder> recorder_for_external_draws_;
 
-  // If this instance is single-buffered or |resource_recycling_enabled_| is
-  // false, |image_pool_| will not recycle resources.
-  std::unique_ptr<gpu::SharedImagePool<CanvasResourceSharedImage>> image_pool_;
 
   scoped_refptr<CanvasResourceSharedImage> resource_;
 
