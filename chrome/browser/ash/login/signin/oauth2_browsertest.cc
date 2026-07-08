c50a18f88e06e71a6fb06e31b13a93ab769fa856
Antonio Gomes <tonikitoo@igalia.com>
2026-07-08 11:07:17 -0700

    Migrate oauth2_browsertest off ScopedTabbedBrowserDisplayer::browser()
    FindOrCreateVisibleBrowser() and its call site only pass the browser to
    BrowserWindowInterface-accepting APIs (chrome::AddTabAt,
    ui_test_utils::NavigateToURLWithDisposition, GetCurrentTabTitle) and
    otherwise use GetTabStripModel(), so they can operate on
    BrowserWindowInterface instead of the concrete Browser. Switch them to
    the browser_window_interface() accessor added in the parent CL.
    
    R=chromium-bedrock-reviews@google.com
    
    Bug: 353332589
    Change-Id: I5c9ca9f39c7b5ea7d074bde7f8ed509d6b6e01e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061362
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Antonio Gomes Netto <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1658927}

--- chrome/browser/ash/login/signin/oauth2_browsertest.cc
+++ chrome/browser/ash/login/signin/oauth2_browsertest.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/ash/login/gaia_screen_handler.h"
@@ -911,11 +912,12 @@ class MergeSessionTest : public OAuth2Test,
   GURL non_google_page_url_;
 };
 
-Browser* FindOrCreateVisibleBrowser(Profile* profile) {
+BrowserWindowInterface* FindOrCreateVisibleBrowser(Profile* profile) {
   chrome::ScopedTabbedBrowserDisplayer displayer(profile);
-  Browser* browser = displayer.browser();
-  if (browser->tab_strip_model()->count() == 0)
+  BrowserWindowInterface* browser = displayer.browser_window_interface();
+  if (browser->GetTabStripModel()->count() == 0) {
     chrome::AddTabAt(browser, GURL(), -1, true);
+  }
   return browser;
 }
 
@@ -924,14 +926,14 @@ IN_PROC_BROWSER_TEST_P(MergeSessionTest, PageThrottle) {
                       /*is_under_advanced_protection=*/false);
 
   // Try to open a page from google.com.
-  Browser* browser = FindOrCreateVisibleBrowser(GetProfile());
+  BrowserWindowInterface* browser = FindOrCreateVisibleBrowser(GetProfile());
   ui_test_utils::NavigateToURLWithDisposition(
       browser, fake_google_page_url_, WindowOpenDisposition::CURRENT_TAB,
       ui_test_utils::BROWSER_TEST_NO_WAIT);
 
   // JavaScript dialog wait setup.
   content::WebContents* tab =
-      browser->tab_strip_model()->GetActiveWebContents();
+      browser->GetTabStripModel()->GetActiveWebContents();
   auto* js_dialog_manager =
       javascript_dialogs::TabModalDialogManager::FromWebContents(tab);
   base::test::TestFuture<void> dialog_wait;
