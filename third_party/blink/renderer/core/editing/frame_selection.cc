db7e1be45162ede1a591045411a278b3d9ed3e37
Stefan Zager <szager@chromium.org>
2026-07-08 15:12:23 -0700

    Verify new focused selection before triggering spell checker
    The previous call site to RespondToChangedSelection() was _before_ the
    DOM selection update had been applied.
    
    Bug: 502252964
    Change-Id: I2852df5e545cd37abeda19c051f61c32962bb08d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063807
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659095}

--- third_party/blink/renderer/core/editing/frame_selection.cc
+++ third_party/blink/renderer/core/editing/frame_selection.cc
@@ -330,7 +330,6 @@ bool FrameSelection::SetSelectionDeprecated(
   is_handle_visible_ = should_show_handle;
   ScheduleVisualUpdateForVisualOverflowIfNeeded();
 
-  frame_->GetEditor().RespondToChangedSelection();
   DCHECK_EQ(current_document, GetDocument());
   return true;
 }
@@ -433,6 +432,7 @@ void FrameSelection::DidSetSelectionDeprecated(
         *Event::Create(event_type_names::kSelectionchange),
         TaskType::kMiscPlatformAPI);
   }
+  frame_->GetEditor().RespondToChangedSelection();
 }
 
 void FrameSelection::SetSelectionForAccessibility(
