6f140095b60d11f29696670f90177225b2300828
Sky Malice <skym@chromium.org>
2026-07-08 15:37:47 -0700

    [AIM] Preserve AutocompleteState through tab switch.
    AutocompleteState is set to ENABLED in the constructor and reset of
    AutocompleteInput. This is convenient for creating new objects to pass
    to beginInput(), but is the opposite of what we want to do when
    deactivated (unfocus). Now that this is manually overriding the state,
    we can trust the state when switching back to a tab. Some location bar
    mediator logic needed to be updated to account for not getting ENABLED
    states automatically now.
    
    The functional difference is when you tab tab to an existing NTP, it's
    correctly tracking the difference between STANDBY and DISABLED.
    
    Bug: 522911537, 529854485, 526655008
    Change-Id: I67e8e473271faab56d8cd7fe88d5324922648bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065607
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659102}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/FuseboxSessionState.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/FuseboxSessionState.java
@@ -23,6 +23,7 @@ import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.components.metrics.OmniboxEventProtos.OmniboxEventProto.PageClassification;
 import org.chromium.components.omnibox.AutocompleteInput;
+import org.chromium.components.omnibox.AutocompleteInput.AutocompleteState;
 import org.chromium.components.omnibox.AutocompleteRequestType;
 import org.chromium.components.omnibox.OmniboxCapabilities;
 import org.chromium.components.omnibox.OmniboxFeatures;
@@ -210,6 +211,8 @@ public class FuseboxSessionState implements UserData {
         if (!mIsActive) return;
 
         mAutocompleteInput.reset();
+        mAutocompleteInput.setAutocompleteState(AutocompleteState.DISABLED);
+
         if (mProfileSupplier != null && mPendingProfileCallback != null) {
             mProfileSupplier.removeObserver(mPendingProfileCallback);
             mPendingProfileCallback = null;
