a4076b8a4387aa4694d749a0cea1769edf01b29e
Aida Zolic <aidazolic@chromium.org>
2026-07-08 03:58:04 -0700

    Add OOBE config option to skip update opt out
    Adds a separate OOBE config option to skip the forced EU zone update
    opt out notice.
    This will be used in managed, automated enrollment flows following,
    e.g. a device migration between domains.
    
    Bug: 530427463
    Change-Id: I8556dc57af243ccb47dc4d3b6e40cfb5f582a976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8039683
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658635}

--- chrome/browser/ash/login/screens/update_screen.cc
+++ chrome/browser/ash/login/screens/update_screen.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <string_view>
 
+#include "ash/constants/ash_features.h"
 #include "ash/constants/ash_login_pref_names.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/login/resources/grit/ash_login_strings.h"
@@ -108,8 +109,19 @@ void RecordUpdateCheckTimeout(bool timeout) {
   base::UmaHistogramBoolean("OOBE.UpdateScreen.CheckTimeout", timeout);
 }
 
-// Determines if the device is in EU zone to show info about opt out.
+// Determines whether to show the opt out notice to the user.
+// Returns false if the device is outside the EU zone or the opt out is
+// explicitly skipped via the OOBE configuration (e.g. during automated flows).
 bool CheckIfOptOutIsEnabled(PrefService& local_state) {
+  if (ash::features::IsDeviceMoveConfigSaveEnabled()) {
+    const base::DictValue& configuration =
+        OobeConfiguration::Get()->configuration();
+    if (configuration.FindBool(configuration::kSkipUpdateOptOutScreen)
+            .value_or(false)) {
+      return false;
+    }
+  }
+
   auto country = system::GetCountryCodeFromTimezoneIfAvailable(
       local_state.GetString(ash::prefs::kSigninScreenTimezone));
   if (!country.has_value()) {
