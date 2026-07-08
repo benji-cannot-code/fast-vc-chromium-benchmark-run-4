22841c8ae46a89c90ac15f940d51cdcb56169aa4
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-08 08:19:34 -0700

    [unsafe-buffers] Remove allow_unsafe_buffers from sandbox/
    This is a mechanical change transitioning the last few files in sandbox/
    from coarse, file-level pragmas to fine-grained, instruction-level
    UNSAFE_TODO() macros.
    
    Bug: 351564777, 501112808
    Doc: go/complete_unsafe_todo_migration
    Change-Id: Ie018f5d447476d25e57901e1317be8f65cf151bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035239
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658776}

--- sandbox/win/src/target_process.cc
+++ sandbox/win/src/target_process.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "sandbox/win/src/target_process.h"
 
 #include <windows.h>
@@ -22,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/containers/span.h"
 #include "base/memory/free_deleter.h"
 #include "base/numerics/safe_conversions.h"
@@ -49,17 +45,18 @@ void CopyPolicyToTarget(base::span<const uint8_t> source, void* dest) {
   if (!source.size()) {
     return;
   }
-  memcpy(dest, source.data(), source.size());
+  UNSAFE_TODO(memcpy(dest, source.data(), source.size()));
   sandbox::PolicyGlobal* policy =
       reinterpret_cast<sandbox::PolicyGlobal*>(dest);
 
   size_t offset = reinterpret_cast<size_t>(source.data());
 
   for (size_t i = 0; i < sandbox::kSandboxIpcCount; i++) {
-    size_t buffer = reinterpret_cast<size_t>(policy->entry[i]);
+    size_t buffer = reinterpret_cast<size_t>(UNSAFE_TODO(policy->entry[i]));
     if (buffer) {
       buffer -= offset;
-      policy->entry[i] = reinterpret_cast<sandbox::PolicyBuffer*>(buffer);
+      UNSAFE_TODO(policy->entry[i]) =
+          reinterpret_cast<sandbox::PolicyBuffer*>(buffer);
     }
   }
 }
@@ -174,15 +171,16 @@ ResultCode TargetProcess::Init(
   size_t current_offset = shared_IPC_size;
   // PolicyGlobal region.
   if (policy.has_value()) {
-    CopyPolicyToTarget(policy.value(),
-                       reinterpret_cast<char*>(shared_memory) + current_offset);
+    CopyPolicyToTarget(
+        policy.value(),
+        UNSAFE_TODO(reinterpret_cast<char*>(shared_memory) + current_offset));
     current_offset += policy->size();
   }
 
   // Delegate Data region.
   if (delegate_data.has_value()) {
-    memcpy(reinterpret_cast<char*>(shared_memory) + current_offset,
-           delegate_data->data(), delegate_data->size());
+    UNSAFE_TODO(memcpy(reinterpret_cast<char*>(shared_memory) + current_offset,
+                       delegate_data->data(), delegate_data->size()));
     current_offset += delegate_data->size();
   }
 
