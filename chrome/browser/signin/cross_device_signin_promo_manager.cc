ebf082b89eb4bb2e08fe744e297cac4454833a05
Paris <pmeuleman@chromium.org>
2026-07-08 06:50:45 -0700

    [XDeviceSignin] Only suppress the promo if other mobile devices
    Change from suppressing the promos when any other device was present to
    suppress only when a phone is present.
    
    Fixed: 532406911
    Change-Id: I3cbbbb113053873ccfc0930112c69436f862915b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061087
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1658705}

--- chrome/browser/signin/cross_device_signin_promo_manager.cc
+++ chrome/browser/signin/cross_device_signin_promo_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/signin/cross_device_signin_promo_manager.h"
 
+#include <algorithm>
 #include <optional>
 #include <string_view>
 
@@ -187,7 +188,7 @@ bool IsUserSignedInWithNoError(Profile* profile) {
          state == signin_util::SignedInState::kSyncing;
 }
 
-bool HasOtherSignedInDevices(Profile* profile) {
+bool HasMobileDevice(Profile* profile) {
   syncer::DeviceInfoSyncService* device_info_sync_service =
       DeviceInfoSyncServiceFactory::GetForProfile(profile);
   CHECK(device_info_sync_service);
@@ -195,13 +196,15 @@ bool HasOtherSignedInDevices(Profile* profile) {
       device_info_sync_service->GetDeviceInfoTracker();
   CHECK(device_info_tracker);
 
-  for (const syncer::DeviceInfo* device_info :
-       device_info_tracker->GetAllDeviceInfo()) {
-    if (!device_info_tracker->IsRecentLocalCacheGuid(device_info->guid())) {
-      return true;
-    }
-  }
-  return false;
+  return std::ranges::any_of(
+      device_info_tracker->GetAllDeviceInfo(),
+      [device_info_tracker](const syncer::DeviceInfo* device_info) {
+        if (device_info_tracker->IsRecentLocalCacheGuid(device_info->guid())) {
+          return false;
+        }
+        return device_info->form_factor() ==
+               syncer::DeviceInfo::FormFactor::kPhone;
+      });
 }
 
 bool IsHistorySyncEnabled(Profile* profile) {
@@ -229,9 +232,9 @@ bool ShouldShowCrossDeviceSigninPromo(
         entry_point, CrossDeviceSigninPromoShouldShowResult::kNotSignedIn);
     return false;
   }
-  if (HasOtherSignedInDevices(profile)) {
+  if (HasMobileDevice(profile)) {
     RecordShouldShowResult(
-        entry_point, CrossDeviceSigninPromoShouldShowResult::kHasOtherDevices);
+        entry_point, CrossDeviceSigninPromoShouldShowResult::kHasMobileDevice);
     return false;
   }
 
