1c31bbcda8fec6fad4ad0702cff76e8e99d84160
Hyowon Kim <hyowon@igalia.com>
2026-07-07 18:58:36 -0700

    Pass real StyleSheetContents to @supports selector() namespace check
    SupportsComplexSelector always passed nullptr for the style sheet, so
    namespace prefixes in `@supports selector(...)` never resolved. This
    CL passes the real one instead.
    
    Bug: 40804326
    Change-Id: Ic343758ac37dcbd836e9c522dc0559cb0280e7f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8038500
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1658446}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.h
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.h
@@ -85,7 +85,8 @@ class CORE_EXPORT CSSSelectorParser {
   CSSSelectorList* ConsumeNthChildOfSelectors(CSSParserTokenStream&);
 
   static bool SupportsComplexSelector(CSSParserTokenStream&,
-                                      const CSSParserContext*);
+                                      const CSSParserContext*,
+                                      StyleSheetContents*);
 
   static CSSSelector::PseudoType ParsePseudoType(const AtomicString&,
                                                  bool has_arguments,
