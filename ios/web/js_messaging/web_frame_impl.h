a4b4fd6934776f4c07b79a9d5824ff2d008025a0
Mike Dougherty <michaeldo@chromium.org>
2026-07-08 11:25:43 -0700

    Refactor WebFrameImpl JS execution methods to use ScriptContext
    There is no intended functional change in this CL. However, it will
    allow for improved error reporting in a follow-up CL.
    
    Bug: 532179256
    Change-Id: I8d48a46c7bd50a6cadca1d832e00d140c09447b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8058005
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658946}

--- ios/web/js_messaging/web_frame_impl.h
+++ ios/web/js_messaging/web_frame_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_WEB_JS_MESSAGING_WEB_FRAME_IMPL_H_
 
 #import <map>
+#import <optional>
 #import <string>
 
 #import "base/cancelable_callback.h"
@@ -21,11 +22,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "url/gurl.h"
 #import "url/origin.h"
 
+@class NSError;
+@class NSString;
 @class WKFrameInfo;
 
 namespace web {
 
 class JavaScriptContentWorld;
+class WebFrameImpl;
+
+// A structure to represent a JavaScript execution and its error, if any.
+struct ScriptContext {
+  ScriptContext(base::WeakPtr<web::WebState> web_state,
+                base::WeakPtr<web::WebFrameImpl> web_frame,
+                url::Origin security_origin,
+                bool is_main_frame,
+                NSString* script = nil,
+                std::optional<std::string> api = std::nullopt);
+  ScriptContext(const ScriptContext&);
+  ScriptContext(ScriptContext&&);
+  ScriptContext& operator=(const ScriptContext&);
+  ScriptContext& operator=(ScriptContext&&);
+  ~ScriptContext();
+
+  base::WeakPtr<web::WebState> web_state;
+  base::WeakPtr<web::WebFrameImpl> web_frame;
+  url::Origin security_origin;
+  bool is_main_frame;
+  NSString* script = nil;
+  std::optional<std::string> api = std::nullopt;
+  NSError* error = nil;
+};
 
 class WebFrameImpl final : public WebFrame,
                            public WebFrameInternal,
