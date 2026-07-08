4a5af281695eea648256bc1bf65c4c4a16cdcaf0
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-07-08 11:21:45 -0700

    [Extensions] Remove feature kApiUserScriptsMultipleWorlds
    The kApiUserScriptsMultipleWorlds feature launched over a year ago in
    crrev.com.com/78e7de28d840e7455b456bf7a7c2c3e6c25e71e4. Remove the
    corresponding base::Feature.
    
    This CL should have no behavior change (since the base::Feature
    was enabled on all channels).
    
    Bug: 331680187
    Change-Id: Ieeab3e567b39798dd9f20c8f9c103728b06b7169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027207
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658939}

--- chrome/common/extensions/extension_test_util.cc
+++ chrome/common/extensions/extension_test_util.cc
@@ -125,8 +125,6 @@ std::vector<const char*> GetExpectedDelegatedFeaturesForTest() {
       // userScripts availability:
       "userScripts",
       "userScripts.execute",
-      "userScripts.getWorldConfigurations",
-      "userScripts.resetWorldConfiguration",
   };
 }
 
