4bca8814dd68e0e1a53ac2630706fd2679ee89dc
amraboelkher <amraboelkher@chromium.org>
2026-07-08 08:41:24 -0700

    [dbsc] Introduce SparePoolKeyType concepts for key pools
    Introduces concepts `SparePoolKeyType` and `SparePoolKeyIdType` to
    cleanly constrain the template parameters of the spare key pool classes
    and helpers. This replaces the verbose `static_assert` checks and
    improves type safety.
    
    `SparePoolKeyIdType` was additionally introduced to allow Template
    Argument Deduction (TAD) at callsites that use
    `ServiceErrorOr<KeyIdType>`, bypassing the non-deduced context
    limitation of dependent types.
    
    NO_IFTTT=No actual changes in *SpareKeyPoolHistogram*.
    
    Bug: 529693533
    Change-Id: I0760947f984f5306ff399d2429489db26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8034242
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658795}

--- components/unexportable_keys/unexportable_key_service_impl.h
+++ components/unexportable_keys/unexportable_key_service_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_UNEXPORTABLE_KEYS_UNEXPORTABLE_KEY_SERVICE_IMPL_H_
 
 #include <algorithm>
+#include <concepts>
 #include <functional>
 
 #include "base/containers/span.h"
@@ -29,6 +30,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 namespace unexportable_keys {
+
+// Concept defining the valid wrapper classes that can be stored and managed
+// within the spare key pool.
+template <typename T>
+concept SparePoolKeyType =
+    std::same_as<T, RefCountedUnexportableSigningKey> ||
+    std::same_as<T, RefCountedUnexportableAttestationKey>;
 
 // LINT.IfChange(SpareKeyPoolRetrievalResult)
 // These values are persisted to logs. Entries should not be renumbered and
@@ -156,6 +164,7 @@ class COMPONENT_EXPORT(UNEXPORTABLE_KEYS) UnexportableKeyServiceImpl
   // of idle pre-generated keys (of a specific key type) to mitigate the
   // significant latency (~1s) of on-demand Windows TPM key generation.
   template <typename KeyType>
+    requires SparePoolKeyType<KeyType>
   class SpareKeyPool;
 
   using SpareSigningKeyPool = SpareKeyPool<RefCountedUnexportableSigningKey>;
