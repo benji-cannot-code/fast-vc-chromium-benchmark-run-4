2c778213d20b4919cb310b458cc1f7175595cfe9
Jason Jiang <jasonnjiang@google.com>
2026-07-08 10:01:47 -0700

    Fix Extension Request Access Button going underneath the extensions icon
    extensions_request_access_button now calculates its clipping when
    animating in order to animate the correct bounds for the Allow #? to
    correctly clip before the extensions icon
    
    Previously the Allow #? was clipping behind the extensions icon, causing
    a weird visual.
    
    Recording: https://screencast.googleplex.com/cast/NjU3ODg3OTM2Mjc2MDcwNHxkYTgyNTMxZS00Ng
    
    Bug: 395627283
    Change-Id: I75a6c673428e6d77fcbc6ba3e6639c8708afec36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7986073
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Jason Jiang <jasonnjiang@google.com>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658861}

--- chrome/browser/ui/views/extensions/extensions_request_access_button.h
+++ chrome/browser/ui/views/extensions/extensions_request_access_button.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/extensions/extensions_container.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_chip_button.h"
 #include "extensions/common/extension_id.h"
 #include "ui/views/input_event_activation_protector.h"
+#include "ui/views/view_observer.h"
 
 namespace ui {
 class Event;
@@ -90,6 +92,8 @@ class ExtensionsRequestAccessButton : public ToolbarChipButton {
   // views::View:
   void VisibilityChanged(views::View* starting_from, bool is_visible) override;
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
+  void AddedToWidget() override;
+  void RemovedFromWidget() override;
 
   raw_ptr<BrowserWindowInterface> browser_;
   raw_ptr<ExtensionsToolbarViewModel> extensions_toolbar_view_model_;
@@ -112,6 +116,27 @@ class ExtensionsRequestAccessButton : public ToolbarChipButton {
   bool remove_confirmation_for_testing_{false};
 
   std::unique_ptr<views::InputEventActivationProtector> input_protector_;
+
+  class SiblingObserver : public views::ViewObserver {
+   public:
+    explicit SiblingObserver(ExtensionsRequestAccessButton* button);
+    ~SiblingObserver() override;
+
+    // views::ViewObserver:
+    void OnViewBoundsChanged(views::View* observed_view) override;
+    void OnViewIsDeleting(views::View* observed_view) override;
+
+   private:
+    raw_ptr<ExtensionsRequestAccessButton> button_;
+  };
+
+  void OnSiblingDeleting();
+
+  SiblingObserver sibling_observer_{this};
+  base::ScopedObservation<views::View, views::ViewObserver>
+      sibling_observation_{&sibling_observer_};
+
+  void UpdateClipPath(views::View* extensions_button);
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_REQUEST_ACCESS_BUTTON_H_
