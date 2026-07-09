cc920d7ca9cbd60a517c050f8439a11cc3b4a9fe
Marc Treib <treib@chromium.org>
2026-07-09 03:19:46 -0700

    Sync: Improve user agent for Android desktop devices
    Before this CL, the sync user agent for Android desktop devices started
    with "Chrome ANDROID-PHONE" which was, at best, quite misleading.
    
    The reason was that the code only considered "tablet", and assumed all
    other Android devices must be phones.
    
    This CL adds handling for "desktop" form factors on Android. The user
    agent now starts with "Chrome ANDROID-DESKTOP".
    
    Fixed: 532199271
    Change-Id: I85fc227700de58bf05d797b9144c801b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063929
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659440}

--- components/sync/base/sync_util_unittest.cc
+++ components/sync/base/sync_util_unittest.cc
@@ -7,9 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "components/sync/base/command_line_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/device_info.h"
+#endif
 
 namespace syncer {
 
@@ -48,5 +53,15 @@ TEST(SyncUtilTest, FormatUserAgentForSync) {
   ASSERT_TRUE(base::StartsWith(user_agent, "Chrome TEST",
                                base::CompareCase::SENSITIVE));
 }
+
+#if BUILDFLAG(IS_ANDROID)
+TEST(SyncUtilTest, MakeUserAgentForSyncAndroidDesktop) {
+  base::android::device_info::set_is_desktop_for_testing(true);
+  std::string user_agent = MakeUserAgentForSync(version_info::Channel::UNKNOWN);
+  EXPECT_TRUE(base::StartsWith(user_agent, "Chrome ANDROID-DESKTOP",
+                               base::CompareCase::SENSITIVE));
+  base::android::device_info::reset_is_desktop_for_testing();
+}
+#endif
 
 }  // namespace syncer
