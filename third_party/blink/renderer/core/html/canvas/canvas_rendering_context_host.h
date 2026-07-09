66a526a5c19d431463befcc3b2252ac610e88cb5
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-07-09 08:50:45 -0700

    Remove CanvasRenderingContextHost::PushFrame
    It's not used on the interface level, it's internal implementation of
    OffscreenCanvas at this point.
    
    Change-Id: I927fb43ba1f10742772d72a0fc5f4e081bbd2255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8006150
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659590}

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context_host.h
@@ -34,7 +34,6 @@ class Layer;
 namespace blink {
 
 class CanvasRenderingContext;
-class CanvasResource;
 class CanvasResourceDispatcher;
 class ComputedStyle;
 class KURL;
@@ -71,7 +70,6 @@ class CORE_EXPORT CanvasRenderingContextHost
 
   virtual void PostFinalizeFrame(FlushReason) = 0;
   void NotifyCachesOfSwitchingFrame();
-  virtual bool PushFrame(scoped_refptr<CanvasResource>&& frame) = 0;
   virtual bool OriginClean() const = 0;
   virtual void SetOriginTainted() = 0;
   virtual CanvasRenderingContext* RenderingContext() const = 0;
