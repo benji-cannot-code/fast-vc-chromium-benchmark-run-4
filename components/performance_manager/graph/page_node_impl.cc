4fe3fc7a12cb4b9ceac9377ff1bdd453937c794a
Etienne Pierre-doray <etiennep@chromium.org>
2026-07-09 12:43:34 -0700

    [PM] Emit AttachedPage event as a slice
    This CL emits AttachedPage (connecting frame to page) as a slice.
    The reason it was prevously a pair of instant event was to reduce
    data losss, but this is now mitigated with persistent tracing
    categories.
    
    Change-Id: I9d45cd9b80f1c7474e12287d83e6e878531a46a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070519
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659780}

--- components/performance_manager/graph/page_node_impl.cc
+++ components/performance_manager/graph/page_node_impl.cc
@@ -312,7 +312,8 @@ void PageNodeImpl::TraceFrame(base::PassKey<FrameNodeImpl>,
       frame_node->IsMainFrame() ? "MainFrameAttached" : "FrameAttached";
   TRACE_EVENT_INSTANT("performance_manager.graph",
                       perfetto::StaticString(event_name), frames_track_,
-                      perfetto::Flow::FromPointer(frame_node));
+                      perfetto::Flow::Global(base::UnguessableTokenHash()(
+                          frame_node->GetFrameToken().value())));
 }
 
 void PageNodeImpl::RemoveFrame(base::PassKey<FrameNodeImpl>,
@@ -329,9 +330,11 @@ void PageNodeImpl::RemoveFrame(base::PassKey<FrameNodeImpl>,
   }
   const char* event_name =
       frame_node->IsMainFrame() ? "MainFrameDetached" : "FrameDetached";
-  TRACE_EVENT_INSTANT("performance_manager.graph",
-                      perfetto::StaticString(event_name), frames_track_,
-                      perfetto::Flow::FromPointer(frame_node));
+  TRACE_EVENT_INSTANT(
+      "performance_manager.graph", perfetto::StaticString(event_name),
+      frames_track_,
+      perfetto::TerminatingFlow::Global(
+          base::UnguessableTokenHash()(frame_node->GetFrameToken().value())));
 }
 
 void PageNodeImpl::SetLoadingState(LoadingState loading_state) {
