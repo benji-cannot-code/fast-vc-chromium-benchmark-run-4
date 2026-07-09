7624eed1c0a26e674c4adc9c7ad0e62c210caaf0
Ian Wells <iwells@chromium.org>
2026-07-09 14:54:03 -0700

    Register CCPageActionObserver before showing the anchored message
    The show start time isn't recorded if the message is shown before the
    observation begins. The test didn't catch this because it was
    incorrectly allowing the duration to be 0.
    
    Bug: 533059587
    Change-Id: Ic5d77d7d4d460fc2e591fd6cb97e094bd439f93c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072218
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659855}

--- chrome/browser/contextual_cueing/contextual_cueing_controller.cc
+++ chrome/browser/contextual_cueing/contextual_cueing_controller.cc
@@ -976,6 +976,7 @@ void ContextualCueingController::ShowCue(
   }
 
   ObserveSidePanel();
+  page_action_observer_->RegisterAsPageActionObserver(*page_action_controller);
 
   page_action_controller->Show(kActionAnchoredContextualCue);
   page_action_controller->SetAnchoredMessageIcon(
@@ -1005,8 +1006,6 @@ void ContextualCueingController::ShowCue(
   CUEING_LOG(base::StringPrintf(
       "Showing cue for CUJ %s: %s [%s]", cue.suggested_cuj(),
       strings.anchored_message_text(), strings.action_text()));
-
-  page_action_observer_->RegisterAsPageActionObserver(*page_action_controller);
 
   contextual_cueing_service_->OnCueShown(
       active_tab->GetContents()->GetLastCommittedURL(), cue_type);
