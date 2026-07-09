618b158658faea0dd8dba430cc13ac62c3150832
Christoph Schwering <schwering@google.com>
2026-07-09 02:00:29 -0700

    [Autofill] Clean up kAutofillEnableCacheForRegexMatching
    The feature was launched in 2024 in crrev.com/c/5560945 with the
    parameter value 1000.
    
    Bug: 40279279
    Change-Id: I3c0747d23f6671260d40656c03daedafcebba8f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061094
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659401}

--- components/autofill/core/browser/form_parsing/form_field_parser.cc
+++ components/autofill/core/browser/form_parsing/form_field_parser.cc
@@ -160,13 +160,7 @@ ParsingContext::ParsingContext(base::span<const FormFieldData> fields,
       pattern_file(pattern_file),
       active_features(active_features),
       regex_cache(GetAutofillRegexCache()),
-      log_manager(log_manager) {
-  if (base::FeatureList::IsEnabled(
-          features::kAutofillEnableCacheForRegexMatching)) {
-    matches_cache.emplace(
-        features::kAutofillEnableCacheForRegexMatchingCacheSizeParam.Get());
-  }
-}
+      log_manager(log_manager) {}
 
 ParsingContext::ParsingContext(
     base::span<const std::unique_ptr<AutofillField>> fields,
@@ -181,13 +175,7 @@ ParsingContext::ParsingContext(
       pattern_file(pattern_file),
       active_features(active_features),
       regex_cache(GetAutofillRegexCache()),
-      log_manager(log_manager) {
-  if (base::FeatureList::IsEnabled(
-          features::kAutofillEnableCacheForRegexMatching)) {
-    matches_cache.emplace(
-        features::kAutofillEnableCacheForRegexMatchingCacheSizeParam.Get());
-  }
-}
+      log_manager(log_manager) {}
 
 ParsingContext::~ParsingContext() = default;
 
@@ -198,9 +186,9 @@ bool FormFieldParser::MatchesRegexWithCache(
     std::u16string_view pattern,
     std::vector<std::u16string>* groups) {
   RegexMatchesCache::Key key;
-  if (!groups && context.matches_cache) {
+  if (!groups) {
     key = RegexMatchesCache::BuildKey(input, pattern);
-    std::optional<bool> cache_entry = context.matches_cache->Get(key);
+    std::optional<bool> cache_entry = context.matches_cache.Get(key);
     if (cache_entry.has_value()) {
       return cache_entry.value();
     }
@@ -208,8 +196,8 @@ bool FormFieldParser::MatchesRegexWithCache(
   const icu::RegexPattern* regex_pattern =
       context.regex_cache->GetRegexPattern(pattern);
   bool result = MatchesRegex(input, regex_pattern, groups);
-  if (!groups && context.matches_cache) {
-    context.matches_cache->Put(key, result);
+  if (!groups) {
+    context.matches_cache.Put(key, result);
   }
   return result;
 }
