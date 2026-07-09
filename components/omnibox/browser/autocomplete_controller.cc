728d3d918435c99d4de3b1b8dc0ff9fa2ba2690c
Nihar Majmudar <niharm@google.com>
2026-07-09 08:15:44 -0700

    [omnibox] Clean up page classifications.
    -Remove page classification functions that are simple and only include a
    single classification / unused functions.
    -Reorder functions in page
    classifications file.
    -Condense metrics::OmniboxEventProto naming.
    
    This still isn't an ideal state as the file contains lots of ORs to
    allow certain behavior, but it shouldn't be a precedent to add to this
    file for single page classifications.
    
    Change-Id: Ie0f9c6dfc09ee929b13f99622780fce95f249af1
    Bug: 532107055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8054445
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659563}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -123,6 +123,7 @@ namespace {
 
 using ScoringSignals = ::metrics::OmniboxScoringSignals;
 using ProviderType = AutocompleteProvider::Type;
+using OEP = metrics::OmniboxEventProto;
 
 constexpr bool kIsDesktop =
     !(BUILDFLAG(IS_IOS) ||
@@ -546,8 +547,7 @@ AutocompleteController::AutocompleteController(
       template_url_service_(provider_client_->GetTemplateURLService()),
       triggered_feature_service_(
           provider_client_->GetOmniboxTriggeredFeatureService()),
-      steady_state_omnibox_position_(
-          metrics::OmniboxEventProto::UNKNOWN_POSITION),
+      steady_state_omnibox_position_(OEP::UNKNOWN_POSITION),
       config_(config) {
   config_.provider_types &= ~OmniboxFieldTrial::GetDisabledProviderTypes();
 
@@ -993,8 +993,7 @@ void AutocompleteController::UpdateSearchTermsArgsWithAdditionalSearchboxStats(
 
 #if BUILDFLAG(IS_IOS)
   // Append the omnibox position when it's set to experiment_stats_v2.
-  if (steady_state_omnibox_position_ !=
-      metrics::OmniboxEventProto::UNKNOWN_POSITION) {
+  if (steady_state_omnibox_position_ != OEP::UNKNOWN_POSITION) {
     const auto omnibox_position_stat = GetOmniboxPositionExperimentStatsV2();
     auto* reported_experiment_stats_v2 =
         search_terms_args.searchbox_stats.add_experiment_stats_v2();
@@ -1106,8 +1105,7 @@ bool AutocompleteController::ShouldRunProvider(
   // If zero prefix suggest is disabled for the Lens contextual searchbox, only
   // run the typed search provider. Else, will use the IsLensSearchbox check
   // below.
-  if (omnibox::IsLensContextualSearchbox(
-          input_.current_page_classification()) &&
+  if (input_.current_page_classification() == OEP::CONTEXTUAL_SEARCHBOX &&
       !lens::features::ShowContextualSearchboxZeroPrefixSuggest()) {
     return provider->type() == AutocompleteProvider::TYPE_SEARCH;
   }
@@ -1133,7 +1131,7 @@ bool AutocompleteController::ShouldRunProvider(
 
   // For contextual realbox queries, we only want to run a subset of providers
   // to filter out irrelevant suggestions (like history suggestions).
-  if (omnibox::IsNTPRealbox(input_.current_page_classification()) &&
+  if (input_.current_page_classification() == OEP::NTP_REALBOX &&
       input_.lens_overlay_suggest_inputs().has_value()) {
     return provider->type() == AutocompleteProvider::TYPE_ZERO_SUGGEST ||
            provider->type() == AutocompleteProvider::TYPE_SEARCH;
@@ -2378,7 +2376,7 @@ size_t AutocompleteController::InjectAdHocMatch(AutocompleteMatch match) {
 
 #if BUILDFLAG(IS_IOS)
 void AutocompleteController::SetSteadyStateOmniboxPosition(
-    metrics::OmniboxEventProto::OmniboxPosition position) {
+    OEP::OmniboxPosition position) {
   steady_state_omnibox_position_ = position;
 }
 #endif
@@ -2395,10 +2393,10 @@ AutocompleteController::GetOmniboxPositionExperimentStatsV2() const {
   omnibox::metrics::ChromeSearchboxStats::ExperimentStatsV2 experiment_stats_v2;
   experiment_stats_v2.set_type_int(kOmniboxPositionFieldNumber);
   switch (steady_state_omnibox_position_) {
-    case metrics::OmniboxEventProto::TOP_POSITION:
+    case OEP::TOP_POSITION:
       experiment_stats_v2.set_int_value(kTopOmniboxValue);
       break;
-    case metrics::OmniboxEventProto::BOTTOM_POSITION:
+    case OEP::BOTTOM_POSITION:
       experiment_stats_v2.set_int_value(kBottomOmniboxValue);
       break;
     default:
@@ -2851,8 +2849,7 @@ void AutocompleteController::MaybeRemoveCompanyEntityImages(
 void AutocompleteController::MaybeCleanSuggestionsForKeywordMode(
     const AutocompleteInput& input,
     AutocompleteResult* result) {
-  if (!kIsDesktop || input.current_page_classification() ==
-                         metrics::OmniboxEventProto::NTP_REALBOX) {
+  if (!kIsDesktop || input.current_page_classification() == OEP::NTP_REALBOX) {
     // Realbox doesn't support keyword mode yet, so keep original list intact.
     return;
   }
