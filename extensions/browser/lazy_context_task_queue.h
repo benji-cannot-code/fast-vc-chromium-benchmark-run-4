8cb0dbb11616634fde8a57cc08d3cca81c67a3a3
Keishi Hattori <keishi@google.com>
2026-07-09 11:43:39 -0700

    Convert RAW_PTR_EXCLUSION to raw_ptr<T, kUnprotectedInRelease> in extensions/browser
    This replaces RAW_PTR_EXCLUSION with raw_ptr<T, kUnprotectedInRelease>
    in extensions/browser to provide better safety in certain builds while
    avoiding performance overhead in release builds.
    
    This CL was uploaded by git cl split.
    
    R=tjudkins@chromium.org
    
    Bug: 501113069
    Change-Id: I251646e1c06b9f884418cc67a0c00a9bad1b80cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8062607
    Auto-Submit: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659729}

--- extensions/browser/lazy_context_task_queue.h
+++ extensions/browser/lazy_context_task_queue.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_BROWSER_LAZY_CONTEXT_TASK_QUEUE_H_
 
 #include "base/functional/callback.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "extensions/common/extension_id.h"
 #include "url/gurl.h"
 
@@ -33,20 +33,23 @@ class LazyContextTaskQueue {
   // consumers that add tasks to LazyContextTaskQueue.
   struct ContextInfo {
     const ExtensionId extension_id;
-    // `render_process_host` is not a raw_ptr<...> for performance reasons
+    // `render_process_host` uses kUnprotectedInRelease for performance reasons
     // (based on analysis of sampling profiler data).
-    RAW_PTR_EXCLUSION content::RenderProcessHost* const render_process_host;
+    const raw_ptr<content::RenderProcessHost, kUnprotectedInRelease>
+        render_process_host;
     const int64_t service_worker_version_id;
     const int worker_thread_id;
     const GURL url;
-    // `browser_context` is not a raw_ptr<...> for performance reasons (based on
-    // analysis of sampling profiler data).
-    RAW_PTR_EXCLUSION content::BrowserContext* const browser_context = nullptr;
+    // `browser_context` uses kUnprotectedInRelease for performance reasons
+    // (based on analysis of sampling profiler data).
+    const raw_ptr<content::BrowserContext, kUnprotectedInRelease>
+        browser_context = nullptr;
     // This data member will have a nullptr value for Service Worker-related
     // tasks.
-    // `web_contents` is not a raw_ptr<...> for performance reasons (based on
-    // analysis of sampling profiler data).
-    RAW_PTR_EXCLUSION content::WebContents* const web_contents = nullptr;
+    // `web_contents` uses kUnprotectedInRelease for performance reasons (based
+    // on analysis of sampling profiler data).
+    const raw_ptr<content::WebContents, kUnprotectedInRelease> web_contents =
+        nullptr;
 
     explicit ContextInfo(ExtensionHost* host);
 
