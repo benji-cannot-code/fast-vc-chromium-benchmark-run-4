939b272cd0db90633ec69b8ccc65c9ed434034a0
Xi Han <hanxi@google.com>
2026-07-09 12:09:22 -0700

    [ThemeSync] Don't recreate if the same theme is selected from history.
    In this CL, we fix the issue that the activity is recreated again even
    when the same NTP background theme data is selected from history.
    1. No longer updates mInitiallySelectedNtpBackgroundData and use it to
       decide whether should recreate the activity.
    2. Simplify the logic in shouldRecreateActivity().
    Demo: http://shortn/_dhIWuIys9b.
    
    Bug: 488439751
    Change-Id: I5ddf3d4dc31ca775196bb5b209316d682ff3d421
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8067576
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659746}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme_sync/NtpThemeSyncHistoryCoordinator.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme_sync/NtpThemeSyncHistoryCoordinator.java
@@ -24,7 +24,6 @@ import org.chromium.chrome.browser.ntp_customization.theme.chrome_colors.NtpThem
 import org.chromium.chrome.browser.ntp_customization.theme_sync.data.NtpBackgroundDataBase;
 import org.chromium.chrome.browser.ntp_customization.theme_sync.data.NtpBackgroundDataBase.PlatformType;
 import org.chromium.chrome.browser.ntp_customization.theme_sync.data.NtpBackgroundDataColor;
-import org.chromium.chrome.browser.ntp_customization.theme_sync.data.NtpBackgroundDataCustomizedColor;
 import org.chromium.chrome.browser.ntp_customization.theme_sync.data.NtpBackgroundDataGroup;
 import org.chromium.chrome.browser.ntp_customization.theme_sync.data.NtpBackgroundDataManager;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -32,6 +31,7 @@ import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /** Coordinator for the NTP theme sync history. */
 @NullMarked
@@ -45,7 +45,7 @@ public class NtpThemeSyncHistoryCoordinator {
 
     private NtpBackgroundDataGroup @Nullable [] mNtpBackgroundDataGroups;
     private @Nullable NtpThemeSyncHistoryRecyclerViewAdaptor mRecyclerViewAdaptor;
-    private @Nullable NtpBackgroundDataBase mLastSelectedNtpBackgroundData;
+    private @Nullable NtpBackgroundDataBase mInitiallySelectedNtpBackgroundData;
     private int mLastSelectedIndex;
 
     public NtpThemeSyncHistoryCoordinator(
@@ -79,7 +79,7 @@ public class NtpThemeSyncHistoryCoordinator {
         mDataShowingList = new ArrayList<>();
     }
 
-    /** Setup the recycler view. */
+    /** Setups the recycler view. */
     private void setupRecyclerView(ViewGroup parentView) {
         RecyclerView recyclerView =
                 parentView.findViewById(R.id.ntp_theme_sync_history_recycler_view);
@@ -156,10 +156,9 @@ public class NtpThemeSyncHistoryCoordinator {
     /** Called before showing the NTP theme customization history items. */
     public void prepareToShow() {
         mLastSelectedIndex = prepareData();
-        if (mLastSelectedIndex != RecyclerView.NO_POSITION) {
-            mLastSelectedNtpBackgroundData = mDataShowingList.get(mLastSelectedIndex);
-        } else {
-            mLastSelectedNtpBackgroundData = null;
+        if (mInitiallySelectedNtpBackgroundData == null
+                && mLastSelectedIndex != RecyclerView.NO_POSITION) {
+            mInitiallySelectedNtpBackgroundData = mDataShowingList.get(mLastSelectedIndex);
         }
 
         mRecyclerViewAdaptor =
@@ -178,45 +177,19 @@ public class NtpThemeSyncHistoryCoordinator {
 
         NtpCustomizationConfigManager.getInstance()
                 .onBackgroundDataChanged(mContext, backgroundData);
-        mLastSelectedNtpBackgroundData = backgroundData;
     }
 
     /** Returns whether to recreate the activity to apply the new theme color. */
     private boolean shouldRecreateActivity(NtpBackgroundDataBase backgroundData) {
-        boolean isDifferentColor = true;
-        if (mLastSelectedNtpBackgroundData != null && isColorTheme(backgroundData)) {
-            // We check the following color theme cases to see if color matches.
-            if (backgroundData instanceof NtpBackgroundDataColor ntpBackgroundDataColor
-                    && mLastSelectedNtpBackgroundData
-                            instanceof NtpBackgroundDataColor lastSelectedBackgroundDataColor) {
-                isDifferentColor =
-                        !lastSelectedBackgroundDataColor
-                                .getNtpThemeColorInfo()
-                                .equals(ntpBackgroundDataColor.getNtpThemeColorInfo());
-            } else if (backgroundData instanceof NtpBackgroundDataCustomizedColor customizedColor
-                    && mLastSelectedNtpBackgroundData
-                            instanceof
-                            NtpBackgroundDataCustomizedColor lastSelectedCustomizedColor) {
-                isDifferentColor =
-                        !customizedColor
-                                .getNtpThemeColorFromHexInfo()
-                                .equals(lastSelectedCustomizedColor.getNtpThemeColorFromHexInfo());
-            }
-        }
-        return isDifferentColor;
+        return !Objects.equals(mInitiallySelectedNtpBackgroundData, backgroundData);
     }
 
     /** Called to destroy the NtpThemeSyncHistoryCoordinator. */
     public void destroy() {
         mDataShowingList.clear();
-        mLastSelectedNtpBackgroundData = null;
+        mInitiallySelectedNtpBackgroundData = null;
         mPropertyModel.set(NtpThemeSyncHistoryProperties.MORE_OPTIONS_CLICK_LISTENER, null);
         mPropertyModel.set(NtpThemeSyncHistoryProperties.RECYCLER_VIEW_LAYOUT_MANAGER, null);
-    }
-
-    private boolean isColorTheme(NtpBackgroundDataBase backgroundData) {
-        return backgroundData instanceof NtpBackgroundDataColor
-                || backgroundData instanceof NtpBackgroundDataCustomizedColor;
     }
 
     PropertyModel getPropertyModelForTesting() {
