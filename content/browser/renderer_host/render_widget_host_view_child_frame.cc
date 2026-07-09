fd87ccfd65d1db7f7a6f8d555b2568e4a15b93cf
Liang Zhao <lzhao@microsoft.com>
2026-07-09 13:29:32 -0700

    Ensure cached OOPIF in SurfaceEmbed is unregistered properly
    When a tab navigates to a new page and puts the old page in BFCache,
    the frame connector of the cached page is reset when the page is
    navigated away. After that, the view of an OOPIF on the page would
    not be able to get to the root view any more via
    GetRootRenderWidgetHostView(). This would prevent those views from
    unregistering from TouchSelectionControllerClientAura's observer list
    when they are destroyed during shutdown and trigger UAF CHECK when
    that object tries to notify the observers for OnManagerWillDestroy.
    
    To ensure that we always properly unregister the OOPIF views, we
    let RenderWidgetHostViewChildFrame remember the root view which owns the
    TouchSelectionControllerClientManager object that the child frame view
    registers with, and use the remembered view to do the unregistering.
    
    Test:
      - Added SurfaceEmbedConnectorWithOOPIFInBFCache to cover this
        scenario. The test crashes without the change and pass with the
        change.
    
    Bug: 529369152
    Change-Id: I614bcc1302f53cd852815aca4f538747edbc09a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8021594
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Liang Zhao <lzhao@microsoft.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659806}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -113,7 +113,8 @@ void RenderWidgetHostViewChildFrame::
   if (!selection_controller_client_)
     return;
 
-  auto* root_view = frame_connector_->GetRootRenderWidgetHostView();
+  auto* root_view = view_for_touch_selection_client_manager_.get();
+  view_for_touch_selection_client_manager_.reset();
   if (root_view) {
     auto* manager = root_view->GetTouchSelectionControllerClientManager();
     if (manager) {
@@ -184,6 +185,7 @@ void RenderWidgetHostViewChildFrame::SetFrameConnector(
           std::make_unique<TouchSelectionControllerClientChildFrame>(this,
                                                                      manager);
       manager->AddObserver(this);
+      view_for_touch_selection_client_manager_ = root_view->GetWeakPtr();
 
 #if BUILDFLAG(IS_ANDROID)
       auto* observer = root_view->GetTouchSelectionControllerInputObserver();
