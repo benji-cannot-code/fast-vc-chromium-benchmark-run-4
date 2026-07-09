ab4e2a06d0f6d651873e1cd159a58a4060d44e7d
dljames <dljames@chromium.org>
2026-07-09 16:10:45 -0700

    [BAM] Define action items for Bookmark Bar submenu visibility commands
    Maps IDC_BOOKMARK_BAR_SUBMENU_ALWAYS_SHOW, ALWAYS_HIDE, and ONLY_ON_NTP
    to their corresponding ActionItems in chrome_action_id.h and registers
    ActionItems calling chrome::SetBookmarkBarVisibilityState in
    BrowserActions.
    
    TAG=agy
    CONV=9cdfa583-d9a2-4f72-b35d-d56b1d04fb67
    
    Bug: 525118424
    Change-Id: I9a72ff1f34af7c050eeae5a38bc17ec175dff861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8068417
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659928}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -139,6 +139,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionFeedback, IDC_FEEDBACK) \
   E(kActionReportUnsafeSite, IDC_REPORT_UNSAFE_SITE) \
   E(kActionShowBookmarkBar, IDC_SHOW_BOOKMARK_BAR) \
+  E(kActionBookmarkBarSubmenuAlwaysShow, \
+    IDC_BOOKMARK_BAR_SUBMENU_ALWAYS_SHOW) \
+  E(kActionBookmarkBarSubmenuAlwaysHide, \
+    IDC_BOOKMARK_BAR_SUBMENU_ALWAYS_HIDE) \
+  E(kActionBookmarkBarSubmenuOnlyOnNtp, \
+    IDC_BOOKMARK_BAR_SUBMENU_ONLY_ON_NTP) \
   E(kActionShowHistory, IDC_SHOW_HISTORY) \
   E(kActionShowBookmarkManager, IDC_SHOW_BOOKMARK_MANAGER) \
   E(kActionImportSettings, IDC_IMPORT_SETTINGS) \
