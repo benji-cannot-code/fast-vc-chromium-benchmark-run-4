df3761386316083bfdf955686b71570c2397e84a
Joe Mason <joenotcharles@google.com>
2026-07-09 08:53:47 -0700

    Use ByteSize in //services
    ByteCount is deprecated.
    
    Bug: 448661443
    Change-Id: Id32452a9268f74d7999df39371da6cb7f63f26e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065029
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659595}

--- services/tracing/public/cpp/perfetto/perfetto_config.h
+++ services/tracing/public/cpp/perfetto/perfetto_config.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
-#include "base/byte_count.h"
+#include "base/byte_size.h"
 #include "base/component_export.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/chrome_config.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/trace_config.h"
@@ -22,9 +22,9 @@ class TraceConfig;
 
 namespace tracing {
 
-inline constexpr base::ByteCount kMetadataBufferSize = base::KiB(256);
+inline constexpr base::ByteSize kMetadataBufferSize = base::KiBU(256);
 
-base::ByteCount COMPONENT_EXPORT(TRACING_CPP) GetDefaultTraceBufferSize();
+base::ByteSize COMPONENT_EXPORT(TRACING_CPP) GetDefaultTraceBufferSize();
 
 // Creates a perfetto trace config.
 perfetto::TraceConfig COMPONENT_EXPORT(TRACING_CPP) GetDefaultPerfettoConfig(
