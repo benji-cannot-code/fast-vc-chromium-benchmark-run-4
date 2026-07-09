f406c4ce750daee3e5847f8681edebbc5277ff57
Gary Klassen <gklassen@chromium.org>
2026-07-09 12:51:47 -0700

    [inline] Add "Hide for this site" toast notification and update options icon
    To provide clear user feedback and control when dismissing the inline selection
    affordance, this CL:
    1. Replaces the options menu button icon with a down arrow vector icon
       (vector_icons::kKeyboardArrowDownIcon / vector_icons::kCaretDownOldIcon).
    2. Triggers a toast notification displaying "Hidden from this site" when the
       user selects "Hide for this site" from the selection options menu.
    3. Includes an interactive "Manage" button on the toast that directly opens
       the Chrome Content Settings Exceptions page for inline cue menu permissions.
    
    R=dtapuska@chromium.org, wusirui@google.com
    
    Bug: 528392999
    Test: GlicSelectionWidgetTest.*, GlicSelectionObserverTest.*
    Change-Id: I4bc4c8c1d2d2842a7b497542035fbb82d60c93d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8022384
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659786}

--- chrome/browser/glic/glic_selection_observer.h
+++ chrome/browser/glic/glic_selection_observer.h
@@ -33,6 +33,7 @@ class RenderFrameHost;
 }  // namespace content
 
 class BrowserWindowInterface;
+enum class ToastId;
 
 namespace optimization_guide {
 class PageContextEligibilityObserver;
@@ -122,6 +123,7 @@ class GlicSelectionObserver
   void OnCopyLink();
   void OnHideForThisSite();
   void OnSettings();
+  void ShowHiddenToast(ToastId toast_id);
 
   void CopyLinkToHighlight(content::WeakDocumentPtr weak_document_ptr);
 
