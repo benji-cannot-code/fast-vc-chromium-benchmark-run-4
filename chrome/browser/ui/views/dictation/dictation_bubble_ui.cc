292d37bd518518ffbd817676b889bd3c400f2a95
Amya Singhal <amyasinghal@google.com>
2026-07-09 07:46:47 -0700

    [Voice Typing] Move common dictation strings to generated_resources.grd
    Bug: b:525857719 b:529831909
    Change-Id: Ided8ed92107376ab2e006dd8e575dfcb3d0e9b05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8048180
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Gary Klassen <gklassen@chromium.org>
    Commit-Queue: Amya Singhal <amyasinghal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659541}

--- chrome/browser/ui/views/dictation/dictation_bubble_ui.cc
+++ chrome/browser/ui/views/dictation/dictation_bubble_ui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/browser/ui/views/dictation/waveform_view.h"
 #include "chrome/grit/branded_strings.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/interaction/element_identifier.h"
