d4e63726ab98f2b5849cabc7867e82d3002565f5
Keishi Hattori <keishi@google.com>
2026-07-09 00:43:43 -0700

    Convert RAW_PTR_EXCLUSION to raw_ptr<T, kUnprotectedInRelease> in components/feedback
    This replaces RAW_PTR_EXCLUSION with raw_ptr<T, kUnprotectedInRelease> in
    components/feedback to provide better safety in certain builds while avoiding
    performance overhead in release builds.
    
    This CL was uploaded by git cl split.
    
    R=jimmyxgong@chromium.org
    
    Bug: 501113069
    Change-Id: I80897e1ae42c5507dbfcd6ad2f6ee05dcf8b09de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061508
    Auto-Submit: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659368}

--- components/feedback/redaction_tool/url_canon_stdstring.h
+++ components/feedback/redaction_tool/url_canon_stdstring.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/compiler_specific.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "components/feedback/redaction_tool/url_canon.h"
 
 namespace redaction_internal {
@@ -53,9 +53,9 @@ class StdStringCanonOutput : public CanonOutput {
   void Resize(size_t sz) override;
 
  protected:
-  // RAW_PTR_EXCLUSION: Performance reasons: based on analysis of sampling
-  // profiler data and tab_search:top100:2020.
-  RAW_PTR_EXCLUSION std::string* str_;
+  // Uses kUnprotectedInRelease: Performance reasons: based on analysis of
+  // sampling profiler data and tab_search:top100:2020.
+  raw_ptr<std::string, kUnprotectedInRelease> str_;
 };
 
 }  // namespace redaction_internal
