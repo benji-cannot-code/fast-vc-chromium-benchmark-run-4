c9f6490829e74ba887fc29b2d09c89a208381cce
Vasili Svirski <vsvirski@google.com>
2026-07-08 17:31:03 -0700

    Fix undefined shift in MTC landmark calculation
    '1 << split' and '1 << left_split' in SubtreesForLandmarkRange used
    'int' shift arithmetic. Once 'split >= 32', reachable when a landmark
    range spans more than 2^32 entries, this is undefined behavior. Fix this
    by shifting a 'uint64_t' literal instead.
    
    Bug: none
    Change-Id: I9f0e36fd1240ca9abf37d45a0110abab35ec0898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8015703
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Vasili Svirski <vsvirski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659165}

---
+++ net/test/cert_builder_unittest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/test/cert_builder.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace net {
+
+TEST(MtcLogBuilderTest, SubtreesForLandmarkRangeLargeSplit) {
+  constexpr uint64_t kEnd = uint64_t{1} << 40;
+  constexpr uint64_t kMid = uint64_t{1} << 39;
+
+  std::vector<bssl::Subtree> subtrees =
+      MtcLogBuilder::SubtreesForLandmarkRangeForTesting(0, kEnd);
+
+  ASSERT_EQ(subtrees.size(), 2u);
+  ASSERT_EQ(subtrees[0].start, 0u);
+  ASSERT_EQ(subtrees[0].end, kMid);
+  ASSERT_EQ(subtrees[1].start, kMid);
+  ASSERT_EQ(subtrees[1].end, kEnd);
+}
+
+}  // namespace net
