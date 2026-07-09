baeac965da62822774d04f138ad94d383e35d0a3
Mariam Ali <alimariam@google.com>
2026-07-09 03:10:40 -0700

    [pContext][Clank] Introduce Touch To Fill Autofill interfaces
    This change introduces the core interfaces and a skeleton implementation
    for a new Touch To Fill surface dedicated to general Autofill features
    on Clank like AutofillProfile and EntityInstance.
    
    Bug: b:527817991
    Change-Id: Ic28fa9274e17a0af8dd27f52ebe58083ef40a8a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8034470
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659437}

---
+++ chrome/browser/touch_to_fill/autofill/android/touch_to_fill_autofill_view.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_TOUCH_TO_FILL_AUTOFILL_ANDROID_TOUCH_TO_FILL_AUTOFILL_VIEW_H_
+#define CHROME_BROWSER_TOUCH_TO_FILL_AUTOFILL_ANDROID_TOUCH_TO_FILL_AUTOFILL_VIEW_H_
+
+namespace autofill {
+
+class TouchToFillAutofillController;
+
+// The UI interface which prompts the user with notices or suggestions
+// using the Touch To Fill Autofill surface.
+class TouchToFillAutofillView {
+ public:
+  virtual ~TouchToFillAutofillView() = default;
+
+  virtual bool ShowPersonalContextNotice(
+      TouchToFillAutofillController* controller) = 0;
+  virtual void Hide() = 0;
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_TOUCH_TO_FILL_AUTOFILL_ANDROID_TOUCH_TO_FILL_AUTOFILL_VIEW_H_
