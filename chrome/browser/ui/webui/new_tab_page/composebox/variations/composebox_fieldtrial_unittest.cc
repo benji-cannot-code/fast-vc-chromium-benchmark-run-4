9680196c33dec0467f39e14148f1e9d95477cd37
Riley Tatum <rtatum@google.com>
2026-07-09 10:59:08 -0700

    Reland "[next] Launch Omnibox and NTP Next features"
    This reverts commit 76cca6bde4e5374b0e1d2b5d52aa7f9ff0afb84c.
    
    Reason for revert: Fixing tests
    
    Original change's description:
    > Revert "[next] Launch Omnibox and NTP Next features"
    >
    > This reverts commit 2e92ae12ce9bc226bb996030a4f03e77ad62bb9b.
    >
    > Reason for revert: Causing multiple CI failures
    >
    > Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/linux64-rel-ready/69253/overview
    > https://ci.chromium.org/ui/p/chrome/builders/ci/mac64/296405/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/110547/overview
    >
    > Original change's description:
    > > [next] Launch Omnibox and NTP Next features
    > >
    > > Fixed: 505856800
    > > Change-Id: Icaf8358228ba09e0fec80b2dfbec54206d8ceb5c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791453
    > > Commit-Queue: Riley Tatum <rtatum@google.com>
    > > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > > Reviewed-by: Aviv Kiss <avivkiss@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1654470}
    >
    > Bug: 505856800
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ie15b93b7e00748e0967b444029e14bfaee7922fe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8017587
    > Auto-Submit: Jiacheng Guo <gjc@google.com>
    > Owners-Override: Jiacheng Guo <gjc@google.com>
    > Commit-Queue: Jiacheng Guo <gjc@google.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1654553}
    
    Bug: 505856800
    Change-Id: I18e6ead9350ac3eb7a1ed8eb34693d9cc6059dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027107
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659695}

--- chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial_unittest.cc
+++ chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial_unittest.cc
@@ -68,25 +68,26 @@ class NtpFieldTrialEnabledTest : public testing::Test {
 using IsNtpComposeboxEnabledTest = NtpFieldTrialEnabledTest;
 
 TEST_F(IsNtpComposeboxEnabledTest, ReturnsFalseForNullProfile) {
+  base::test::ScopedFeatureList features;
+  features.InitAndDisableFeature(ntp_composebox::kNtpComposebox);
   EXPECT_FALSE(ntp_composebox::IsNtpComposeboxEnabled(nullptr));
 }
 
-TEST_F(IsNtpComposeboxEnabledTest, ReturnsFalseWhenFeatureDisabledByDefault) {
-  // kNtpComposebox is FEATURE_DISABLED_BY_DEFAULT.
+TEST_F(IsNtpComposeboxEnabledTest, ReturnsFalseWhenFeatureDisabled) {
+  base::test::ScopedFeatureList features;
+  features.InitAndDisableFeature(ntp_composebox::kNtpComposebox);
   SetAllEligible(true);
   EXPECT_FALSE(ntp_composebox::IsNtpComposeboxEnabled(profile_.get()));
 }
 
 TEST_F(IsNtpComposeboxEnabledTest, ReturnsFalseWhenNotEligible) {
   base::test::ScopedFeatureList features;
-  features.InitAndEnableFeature(ntp_composebox::kNtpComposebox);
   SetAllEligible(false);
   EXPECT_FALSE(ntp_composebox::IsNtpComposeboxEnabled(profile_.get()));
 }
 
-TEST_F(IsNtpComposeboxEnabledTest, ReturnsTrueWhenEnabledAndEligible) {
+TEST_F(IsNtpComposeboxEnabledTest, ReturnsTrueWhenEligible) {
   base::test::ScopedFeatureList features;
-  features.InitAndEnableFeature(ntp_composebox::kNtpComposebox);
   SetAllEligible(true);
   EXPECT_TRUE(ntp_composebox::IsNtpComposeboxEnabled(profile_.get()));
 }
@@ -94,7 +95,6 @@ TEST_F(IsNtpComposeboxEnabledTest, ReturnsTrueWhenEnabledAndEligible) {
 TEST_F(IsNtpComposeboxEnabledTest,
        ReturnsFalseWhenAimEligibleButNotFuseboxEligible) {
   base::test::ScopedFeatureList features;
-  features.InitAndEnableFeature(ntp_composebox::kNtpComposebox);
   ON_CALL(*mock_service_, IsAimLocallyEligible())
       .WillByDefault(testing::Return(true));
   ON_CALL(*mock_service_, IsAimEligible())
@@ -109,11 +109,16 @@ TEST_F(IsNtpComposeboxEnabledTest,
 using IsNtpRealboxNextEnabledTest = NtpFieldTrialEnabledTest;
 
 TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseForNullProfile) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      {}, {ntp_composebox::kNtpComposebox, ntp_realbox::kNtpRealboxNext});
   EXPECT_FALSE(ntp_realbox::IsNtpRealboxNextEnabled(nullptr));
 }
 
-TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseWhenFeatureDisabledByDefault) {
-  // Both kNtpComposebox and kNtpRealboxNext are FEATURE_DISABLED_BY_DEFAULT.
+TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseWhenComposeboxDisabled) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      {}, {ntp_composebox::kNtpComposebox, ntp_realbox::kNtpRealboxNext});
   SetAllEligible(true);
   EXPECT_FALSE(ntp_realbox::IsNtpRealboxNextEnabled(profile_.get()));
 }
@@ -121,33 +126,23 @@ TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseWhenFeatureDisabledByDefault) {
 TEST_F(IsNtpRealboxNextEnabledTest,
        ReturnsFalseWhenOnlyRealboxNextEnabled) {
   base::test::ScopedFeatureList features;
-  features.InitWithFeatures({ntp_realbox::kNtpRealboxNext}, {});
+  features.InitAndDisableFeature(ntp_composebox::kNtpComposebox);
   SetAllEligible(true);
-  // kNtpComposebox is still disabled by default.
   EXPECT_FALSE(ntp_realbox::IsNtpRealboxNextEnabled(profile_.get()));
 }
 
 TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseWhenNotEligible) {
-  base::test::ScopedFeatureList features;
-  features.InitWithFeatures(
-      {ntp_composebox::kNtpComposebox, ntp_realbox::kNtpRealboxNext}, {});
   SetAllEligible(false);
   EXPECT_FALSE(ntp_realbox::IsNtpRealboxNextEnabled(profile_.get()));
 }
 
-TEST_F(IsNtpRealboxNextEnabledTest, ReturnsTrueWhenEnabledAndEligible) {
-  base::test::ScopedFeatureList features;
-  features.InitWithFeatures(
-      {ntp_composebox::kNtpComposebox, ntp_realbox::kNtpRealboxNext}, {});
+TEST_F(IsNtpRealboxNextEnabledTest, ReturnsTrueWhenEligible) {
   SetAllEligible(true);
   EXPECT_TRUE(ntp_realbox::IsNtpRealboxNextEnabled(profile_.get()));
 }
 
 TEST_F(IsNtpRealboxNextEnabledTest,
        ReturnsFalseWhenAimEligibleButNotFuseboxEligible) {
-  base::test::ScopedFeatureList features;
-  features.InitWithFeatures(
-      {ntp_composebox::kNtpComposebox, ntp_realbox::kNtpRealboxNext}, {});
   ON_CALL(*mock_service_, IsAimLocallyEligible())
       .WillByDefault(testing::Return(true));
   ON_CALL(*mock_service_, IsAimEligible())
