45b08543cc822ca93b4474dd6f43ce44b2a13508
Yanling <yanlinghu@google.com>
2026-07-09 08:18:16 -0700

    [Web UI] Remove NTP theme on Android desktop.
    This CL removes the NTP theme bottom sheet and its promo by default on
    Android desktop when the web UI is enabled.
    
    Specifically, this includes the following changes:
    * Removes the entry point for the NTP theme from the main NTP customization bottom sheet.
    * Hides the theme tip bottom sheet.
    * Hides the NTP theme IPH.
    
    (Note: The magic stack is already hidden on Android desktop.)
    
    Bug: 532191933
    Change-Id: Idb183706fd1e7a9a4c59649c586d214988e1c822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8058844
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659565}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationUtils.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NtpCustomizationUtils.java
@@ -36,6 +36,7 @@ import static org.chromium.chrome.browser.preferences.ChromePreferenceKeys.NTP_C
 import static org.chromium.chrome.browser.preferences.ChromePreferenceKeys.NTP_CUSTOMIZATION_THEME_COLOR_ID;
 import static org.chromium.chrome.browser.preferences.ChromePreferenceKeys.NTP_CUSTOMIZATION_THEME_IS_SNACKBAR_SHOWN;
 import static org.chromium.chrome.browser.preferences.ChromePreferenceKeys.NTP_CUSTOMIZATION_THEME_TIP_BOTTOM_SHEET_SHOWN_TIMESTAMP_MS;
+import static org.chromium.chrome.browser.url_constants.UrlOverrideUtils.isWebUiNtpOverrideEnabled;
 import static org.chromium.components.browser_ui.styles.SemanticColorUtils.getDefaultIconColor;
 
 import android.app.Activity;
@@ -324,6 +325,10 @@ public class NtpCustomizationUtils {
     public static boolean isNtpThemeCustomizationEnabled(
             WindowAndroid windowAndroid, boolean isLff) {
         if (!isNtpThemeCustomizationEnabled()) {
+            return false;
+        }
+
+        if (isWebUiNtpOverrideEnabled()) {
             return false;
         }
 
