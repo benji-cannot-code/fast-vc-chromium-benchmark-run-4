4746ec572617b9927362ee7cec1e3494df844fe8
Cici Ruan <cuicuiruan@google.com>
2026-07-09 15:37:30 -0700

    Revert "[media] Update NativePixmapFrameResource to handle ColorSpace changes."
    This reverts commit 3eed69e8ae5685499b0ff4f90b12209d0e64e371.
    
    Reason for revert: The test can't compile for uprev.
    
    Failure Link: https://ci.chromium.org/ui/p/chromeos/builders/all-partners/arm-generic-cq/b8676727595331654065/overview
    
    Original change's description:
    > [media] Update NativePixmapFrameResource to handle ColorSpace changes.
    >
    > This change updates platform VF utility methods -
    > CreateVideoFrameFromGpuMemoryBufferHandle and
    > CreateMappableSharedImageVideoFrame to take in ColorSpace and not
    > pass any default ColorSpaces, and let the client calling these
    > utilities pass the proper ColorSpace and default if needed. As part of
    > this, NativePixmapFrameResource now passes a default ColorSpace
    > in similar manner behind a killswitch. The other callsite is
    > V4L2VideoEncodeAccelerator which passes the input frame's default
    > color space.
    >
    > Note these defaults in platform VideoFrame utils were added by us as
    > part of larger refactor to ensure that VideoFrames backed by
    > SharedImages have the same ColorSpace for both SharedImage and
    > VideoFrame, and creators of VideoFrame pass a valid color space.
    >
    > Also update various tests to pass in a proper color space.
    >
    > Bug: 491815851, 425634684
    > Change-Id: I13e7292ff0eb7af8bdf2b4141b4cf03f987217ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830867
    > Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1658796}
    
    Bug: 491815851, 425634684
    Change-Id: Ifcff69c79b1b9b645b35d1303db8bf172460d49b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071258
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Owners-Override: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659903}

--- media/gpu/chromeos/platform_video_frame_utils_unittest.cc
+++ media/gpu/chromeos/platform_video_frame_utils_unittest.cc
@@ -151,7 +151,6 @@ TEST(PlatformVideoFrameUtilsTest, CreateVideoFrame) {
   constexpr gfx::Size kCodedSize(320, 240);
   constexpr gfx::Rect kVisibleRect(kCodedSize);
   constexpr gfx::Size kNaturalSize(kCodedSize);
-  constexpr gfx::ColorSpace kColorSpace(gfx::ColorSpace::CreateREC709());
   constexpr auto kTimeStamp = base::Milliseconds(1234);
   constexpr gfx::BufferUsage kBufferUsage =
       gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE;
@@ -170,8 +169,8 @@ TEST(PlatformVideoFrameUtilsTest, CreateVideoFrame) {
         break;
       case VideoFrame::STORAGE_MAPPABLE_SHARED_IMAGE:
         frame = CreateMappableSharedImageVideoFrame(
-            kPixelFormat, kColorSpace, kCodedSize, kVisibleRect, kNaturalSize,
-            kTimeStamp, kBufferUsage, test_sii.get());
+            kPixelFormat, kCodedSize, kVisibleRect, kNaturalSize, kTimeStamp,
+            kBufferUsage, test_sii.get());
         break;
       default:
         NOTREACHED();
