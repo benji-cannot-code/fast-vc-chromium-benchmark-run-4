0ca6037659d16cc1683dd05670d3afc0046fa045
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-09 09:13:30 -0700

    [unsafe-buffers] Remove allow_unsafe_buffers from Blink color_mac.mm
    This is a mechanical change transitioning Blink color_mac.mm from
    coarse, file-level pragmas to fine-grained, instruction-level macros. It
    converts raw static arrays to std::array and base::NoDestructor.
    
    Bug: 351564777, 501112808
    Doc: go/complete_unsafe_todo_migration
    Change-Id: I4734f42808ff2e479ddbae64efa9f8d5002a5c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8045191
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659609}

--- third_party/blink/renderer/platform/mac/color_mac.mm
+++ third_party/blink/renderer/platform/mac/color_mac.mm
@@ -25,14 +25,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #import "third_party/blink/renderer/platform/mac/color_mac.h"
 
 #import <AppKit/AppKit.h>
+
+#include <array>
+
+#include "base/no_destructor.h"
 
 namespace blink {
 
@@ -61,13 +60,14 @@ NSColor* NsColor(const Color& color) {
                                                         alpha:1];
     return white_color;
   } else {
-    const int kCacheSize = 32;
-    static unsigned cached_rgba_values[kCacheSize];
-    static NSColor* cached_colors[kCacheSize];
+    constexpr int kCacheSize = 32;
+    static std::array<unsigned, kCacheSize> cached_rgba_values;
+    static base::NoDestructor<std::array<NSColor*, kCacheSize>> cached_colors;
 
     for (int i = 0; i != kCacheSize; ++i) {
-      if (cached_rgba_values[i] == c)
-        return cached_colors[i];
+      if (cached_rgba_values[i] == c) {
+        return (*cached_colors)[i];
+      }
     }
 
     NSColor* result = [NSColor
@@ -78,7 +78,7 @@ NSColor* NsColor(const Color& color) {
 
     static int cursor;
     cached_rgba_values[cursor] = c;
-    cached_colors[cursor] = result;
+    (*cached_colors)[cursor] = result;
     if (++cursor == kCacheSize) {
       cursor = 0;
     }
