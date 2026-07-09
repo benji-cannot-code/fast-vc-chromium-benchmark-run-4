768cd6e3984916c684c4e5591267179d4d7010af
Kevin <kevinshah@google.com>
2026-07-09 11:40:09 -0700

    [ContextualTasks] Fix AIM page redirect on Android cold start
    Defer ineligible redirect checks while OAuth refresh tokens load
    asynchronously for signed-in and eligible users on Android startup. Also
    observe OnRefreshTokensLoaded in ContextualTasksEligibilityManager to
    re-evaluate eligibility when token loading completes.
    
    Bug: b:531741493
    Change-Id: Ia20f29ae180c36ebc4eaaa2574aa379cbd2268a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065680
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Kevin Shah <kevinshah@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659724}

--- chrome/browser/contextual_tasks/contextual_tasks_eligibility_manager.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_eligibility_manager.cc
@@ -103,6 +103,10 @@ void ContextualTasksEligibilityManager::OnAccountsCookieDeletedByUserAction() {
   MaybeNotifyEligibilityChanged();
 }
 
+void ContextualTasksEligibilityManager::OnRefreshTokensLoaded() {
+  MaybeNotifyEligibilityChanged();
+}
+
 bool ContextualTasksEligibilityManager::IsEligible() const {
   return is_eligible_;
 }
