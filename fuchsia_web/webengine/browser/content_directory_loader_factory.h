5a4f95f1870841565432523808229e927edd7577
Sergey Ulanov <sergeyu@chromium.org>
2026-07-09 11:14:26 -0700

    [Fuchsia] Use file_open3_at to access content directories
    Also removed dependency on GURL::DeprecatedGetOriginAsURL()
    
    Bug: 504196688
    Change-Id: Id2e03d49742c9d8e50969ebfa9064fc4879b0a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8028540
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659706}

--- fuchsia_web/webengine/browser/content_directory_loader_factory.h
+++ fuchsia_web/webengine/browser/content_directory_loader_factory.h
@@ -10,12 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fuchsia/web/cpp/fidl.h>
 #include <lib/fidl/cpp/interface_handle.h>
 
+#include "base/files/file_path.h"
 #include "base/memory/self_deleting.h"
 #include "base/task/sequenced_task_runner.h"
 #include "fuchsia_web/webengine/web_engine_export.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
+#include "net/base/net_errors.h"
 #include "services/network/public/cpp/self_deleting_url_loader_factory.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 
@@ -55,8 +57,18 @@ class ContentDirectoryLoaderFactory
       mojo::PendingRemote<network::mojom::URLLoaderClient> client,
       const net::MutableNetworkTrafficAnnotationTag& traffic_annotation) final;
 
+  net::Error OpenFileFromDirectory(
+      const std::string& content_directory_name,
+      const base::FilePath& relative_file_path,
+      fidl::InterfaceRequest<fuchsia::io::Node> file_request);
+
   // Used for executing blocking URLLoader routines.
   const scoped_refptr<base::SequencedTaskRunner> task_runner_;
+
+  fidl::InterfaceHandle<fuchsia::io::Directory> content_directories_handle_;
+
+  std::string cached_directory_name_;
+  fidl::InterfaceHandle<fuchsia::io::Directory> cached_directory_;
 };
 
 #endif  // FUCHSIA_WEB_WEBENGINE_BROWSER_CONTENT_DIRECTORY_LOADER_FACTORY_H_
