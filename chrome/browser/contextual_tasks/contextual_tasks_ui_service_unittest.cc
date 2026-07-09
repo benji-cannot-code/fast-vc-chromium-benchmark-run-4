0396ff6c3d3f670477f1dab708667bb86d632426
Nihar Majmudar <niharm@google.com>
2026-07-09 08:44:14 -0700

    [Contextual Tasks] Add user action for full page aim navigation.
    This CL adds two user actions:
    
    1) ContextualTasks.AimFullTab.Shown which gets recorded any time
    navigation leads to a AIM full page in cobrowsing
    2) ContextualTasks.TabChange.UserAction.OpenFullTab which gets recorded
    any time a tab switch opens up AIM full page in cobrowisng.
    
    Created in order to have parity with side panel metrics.
    
    Change-Id: I1f12371a6bfc880445555fa73daba6da1695a027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027249
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659586}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service_unittest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service_unittest.cc
@@ -1767,6 +1767,7 @@ TEST_F(ContextualTasksUiServiceTest,
 }
 
 TEST_F(ContextualTasksUiServiceTest, OnNavigationToAiPageIntercepted_SameTab) {
+  base::UserActionTester user_action_tester;
   ContextualTasksUiService service(
       profile_.get(), /*delegate=*/nullptr, contextual_tasks_service_.get(),
       /*identity_manager=*/nullptr, aim_eligibility_service_.get(),
@@ -1799,6 +1800,9 @@ TEST_F(ContextualTasksUiServiceTest, OnNavigationToAiPageIntercepted_SameTab) {
 
   service.OnNavigationToAiPageIntercepted(intercepted_url,
                                           weak_factory.GetWeakPtr(), false);
+
+  EXPECT_EQ(
+      user_action_tester.GetActionCount("ContextualTasks.AimFullTab.Shown"), 1);
 
   GURL expected_initial_url(
       "https://google.com/search?udm=50&q=test+query&sourceid=chrome&ccb=1");
