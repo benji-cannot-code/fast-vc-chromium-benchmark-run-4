1311c81fca3ccbfbef6118eb4195f23a67897fb2
Mikel Astiz <mastiz@chromium.org>
2026-07-09 04:15:45 -0700

    [sync] Introduce TabContextSyncServiceImpl and factory
    Implements TabContextSyncService with an partially-implemented
    in-memory sync bridge and creates TabContextSyncServiceFactory to
    provide profile-keyed instances (excluding iOS).
    
    Bug: 527991726
    Change-Id: Ib3b357a25ec79bfd177f16874f0d4027c78828a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063931
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659456}

--- components/sync_tab_context/tab_context_sync_service.h
+++ components/sync_tab_context/tab_context_sync_service.h
@@ -10,7 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/sync_tab_context/container_id.h"
+
+namespace syncer {
+class DataTypeControllerDelegate;
+}  // namespace syncer
 
 namespace sync_tab_context {
 
@@ -43,6 +49,10 @@ class TabContextSyncService : public KeyedService {
   virtual void GetContainerAccessToken(
       const ContainerId& container_id,
       base::OnceCallback<void(std::optional<std::string>)> cb) = 0;
+
+  // Returns the delegate responsible for integrating with sync.
+  virtual base::WeakPtr<syncer::DataTypeControllerDelegate>
+  GetSyncControllerDelegate() = 0;
 };
 
 }  // namespace sync_tab_context
