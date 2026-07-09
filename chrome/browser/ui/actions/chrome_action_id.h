e1b5618d49898a89db939f20849ec80f1f0b5e84
dljames <dljames@chromium.org>
2026-07-09 10:20:42 -0700

    [BAM] Define action item equivalent for IDC_TOGGLE_VERTICAL_TABS
    Maps IDC_TOGGLE_VERTICAL_TABS to kActionToggleVerticalTabs in
    chrome_action_id.h and registers the corresponding ActionItem calling
    chrome::ToggleVerticalTabs(bwi) in BrowserActions.
    
    TAG=agy
    CONV=9cdfa583-d9a2-4f72-b35d-d56b1d04fb67
    
    Bug: 525118424
    Change-Id: Icb764698d91207d5b14e440462214a1e63b212ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8067848
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659670}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -397,6 +397,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionCommercePriceInsights) \
   E(kActionCommerceDiscounts) \
   /*Vertical Tabs*/ \
+  E(kActionToggleVerticalTabs, IDC_TOGGLE_VERTICAL_TABS) \
   E(kActionToggleCollapseVertical, IDC_TOGGLE_VERTICAL_TABS_COLLAPSE) \
   /*Projects Panel*/ \
   E(kActionToggleProjectsPanel) \
