4e207e5f6af1d56b543e4a9b8cd759a9ea0497a9
Charles Hager <clhager@google.com>
2026-07-09 12:52:26 -0700

    Add back-press handler for missing device lock dialog
    Originally, the scrim from the dialog covered the Chrome back button
    toolbar, and the dialog ignored taps.
    
    Newer versions of Android Automotive have a system back, however, which
    can bypass modal dialogs that do not explicitly include a backpress
    handler. This change fixes that by consuming all back press events while
    the dialog is showing.
    
    Note that, previously, the dialog would continue to show every time an
    activity was resumed (ie every time Chrome was opened, or when opening
    or closing Settings), but it could still be bypassed using back press.
    
    Bug: 532202151
    Change-Id: Ib924c20cd97587d8e88c6929de7d967af36837b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070431
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659787}

--- chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/MissingDeviceLockCoordinator.java
+++ chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/MissingDeviceLockCoordinator.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -93,11 +94,21 @@ public class MissingDeviceLockCoordinator {
                         mMediator.getModel(), mView, MissingDeviceLockViewBinder::bind);
         mModalDialogManager = modalDialogManager;
 
+        // Consume any back press events to ensure that this dialog cannot be bypassed by system
+        // backs.
+        OnBackPressedCallback onBackPressedCallback =
+                new OnBackPressedCallback(true) {
+                    @Override
+                    public void handleOnBackPressed() {}
+                };
         mModalDialogPropertyModel =
                 new PropertyModel.Builder(ModalDialogProperties.ALL_KEYS)
                         .with(ModalDialogProperties.CONTROLLER, mModalDialogController)
                         .with(ModalDialogProperties.CUSTOM_VIEW, mView)
                         .with(ModalDialogProperties.CANCEL_ON_TOUCH_OUTSIDE, false)
+                        .with(
+                                ModalDialogProperties.APP_MODAL_DIALOG_BACK_PRESS_HANDLER,
+                                onBackPressedCallback)
                         .with(
                                 ModalDialogProperties.DIALOG_STYLES,
                                 ModalDialogProperties.DialogStyles.NORMAL)
