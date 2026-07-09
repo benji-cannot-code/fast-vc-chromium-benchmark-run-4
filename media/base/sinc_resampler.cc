a3f81e21579ebe09e9bed3089dcada14c8c7c1d7
Pablo Barrera <barrerap@google.com>
2026-07-09 07:57:03 -0700

    Use base::span in SincResampler callback
    Most of SincResampler is using base::span but the ReadCb was not updated
    to use it yet.
    
    Performance regression checked on local laptops with
    
    `./out/Release/media_perftests --gtest_filter=*Sinc* -gtest_repeat=10`
    
    The results are inconclusive due to high variance between runs. The
    following table shows the mean runs per second for each test in a
    Macbook Pro M4 and a Windows ThinkPad P1 Gen 6. The ratio is defined as
    mean_patched_runs_per_second / mean_baseline_runs_per_second.
    
    ```
    | Test                | M4 ratio | M4 runs/s | Win ratio | Win runs/s |
    | ------------------- | -------: | --------: | --------: | ---------: |
    | optimized_aligned   |  1.00717 | 144872146 |   0.99984 |    8389099 |
    | optimized_unaligned |  1.01381 | 144623194 |   1.08421 |  153298714 |
    | unoptimized_aligned |  1.01887 |  49336626 |   1.01429 |   11094451 |
    ```
    
    Bug: 512016773
    Change-Id: I97ba4ce2de9f016735f629b4c6c6814e0b140104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8042981
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Pablo Barrera González <barrerap@google.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659549}

--- media/base/sinc_resampler.cc
+++ media/base/sinc_resampler.cc
@@ -288,7 +288,7 @@ void SincResampler::Resample(base::span<float> destination) {
 
   // Step (1) -- Prime the input buffer at the start of the input stream.
   if (!buffer_primed_ && remaining_frames) {
-    read_cb_.Run(request_frames_, r0_.data());
+    read_cb_.Run(r0_.first(request_frames_));
     buffer_primed_ = true;
   }
 
@@ -353,7 +353,7 @@ void SincResampler::Resample(base::span<float> destination) {
     }
 
     // Step (5) -- Refresh the buffer with more input.
-    read_cb_.Run(request_frames_, r0_.data());
+    read_cb_.Run(r0_.first(request_frames_));
   }
 }
 
