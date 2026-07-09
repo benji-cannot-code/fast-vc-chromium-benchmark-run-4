dae2bd8f22a1eeabd1ca46db7cdb6f8595865f02
Dan Harrington <harringtond@chromium.org>
2026-07-09 09:27:33 -0700

    Add lint checks for glic api
    - checks properties are camel case
    - checks interfaces are 'export declare'
    
    Change-Id: Ib764dc5ed0c6b47010742a3268ac09206a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8031921
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659619}

---
+++ chrome/browser/resources/glic/presubmit/eslint.config.mjs
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import typescriptEslint from '../../../../../third_party/node/node_modules/@typescript-eslint/eslint-plugin/dist/index.js';
+import tsParser from '../../../../../third_party/node/node_modules/@typescript-eslint/parser/dist/index.js';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'property',
+          format: ['camelCase'],
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+              ':not(TSModuleBlock) > ' +
+              'ExportNamedDeclaration > ' +
+              'TSInterfaceDeclaration:not([declare=true])' +
+              ':not([id.name="PrivateTypes"])' +
+              ':not([id.name="ClosedEnums"])',
+          message: 'Exported interfaces must be declared with `declare`.',
+        },
+      ],
+    },
+  },
+];
