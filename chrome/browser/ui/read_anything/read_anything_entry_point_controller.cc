7db09ff492983728608550d3e1ed96b9905aba2d
Eitan Goldberger <eitang@google.com>
2026-07-09 12:57:10 -0700

    Change kMaxChipIgnoredCount from 5 to 3.
    Change-Id: Id29b78b15fae96d18ec4eb507ab51ef6ac4b8359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070859
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Auto-Submit: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659789}

--- chrome/browser/ui/read_anything/read_anything_entry_point_controller.cc
+++ chrome/browser/ui/read_anything/read_anything_entry_point_controller.cc
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr int kMaxChipIgnoredCount = 5;
+constexpr int kMaxChipIgnoredCount = 3;
 constexpr const char* kDenyList[] = {
     "mail.google.com",
     "whatsapp.com",
