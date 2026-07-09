1b82e093b63ad6fb1c679edcca695293cea2980e
Eshwar Stalin <estalin@chromium.org>
2026-07-09 14:54:29 -0700

    [Glass Frame] Add a feature param for using NSVisualEffectView
    This CL allows testing NSVisualEffectView as the material for the glass
    frame instead of NSGlassEffectView. Picking materials and opacity based
    is now configurable via Finch params to make it easier to test various
    materials.
    
    Bug: 525118930
    Change-Id: I4c1621b5b0f262feb6875b3d77b674812f57c703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065681
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659856}

--- chrome/browser/ui/views/frame/browser_native_widget_mac.h
+++ chrome/browser/ui/views/frame/browser_native_widget_mac.h
@@ -23,6 +23,9 @@ class BrowserWidget;
 class BrowserView;
 @class BrowserWindowTouchBarController;
 @class BrowserWindowTouchBarViewsDelegate;
+namespace tabs {
+class VerticalTabStripStateController;
+}  // namespace tabs
 
 ////////////////////////////////////////////////////////////////////////////////
 //  BrowserNativeWidgetMac is a NativeWidgetMac subclass that provides
@@ -95,6 +98,8 @@ class BrowserNativeWidgetMac : public views::NativeWidgetMac,
  private:
   bool IsBrowserWidgetEligible() const;
   void UpdateBackground(bool is_eligible);
+  void OnVerticalTabStripModeChanged(
+      tabs::VerticalTabStripStateController* controller);
 
   raw_ptr<BrowserView> browser_view_;  // Weak. Our ClientView.
   BrowserWindowTouchBarViewsDelegate* __strong touch_bar_delegate_;
@@ -103,7 +108,7 @@ class BrowserNativeWidgetMac : public views::NativeWidgetMac,
   std::optional<ui::NativeTheme::PreferredColorScheme>
       last_preferred_color_scheme_;
   std::optional<SkColor> last_theme_color_;
-
+  base::CallbackListSubscription vertical_tab_subscription_;
   base::CallbackListSubscription glass_frame_service_subscription_;
 };
 
