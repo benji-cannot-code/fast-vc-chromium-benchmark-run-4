c53571221f87f7936ab518698d033637b62618da
Yuki Shiino <yukishiino@chromium.org>
2026-07-09 01:57:11 -0700

    [dcheck-to-check] Convert DCHECKs in content/browser/renderer_host/ [23/n]
    As part of effort to harden Chromium's safety, turn DCHECKs
    into CHECKs. The converted CHECKs are not fatal until M153.
    
    This patch is generated by the following script.
    https://crrev.com/c/7849874/11/tools/memory/dcheck/replace_dchecks_with_checks.py
    
    Low-Coverage-Reason: This is mechanical conversions from DCHECKs to CHECKs. This doesn't add a new logic nor modify existing logics.
    Bug: 525612731, 501112901
    Change-Id: I269675b800d142e55d52ea972b0f8cd3bf92d49e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7929267
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659397}

--- content/browser/renderer_host/renderer_sandboxed_process_launcher_delegate.cc
+++ content/browser/renderer_host/renderer_sandboxed_process_launcher_delegate.cc
@@ -107,7 +107,7 @@ std::string RendererSandboxedProcessLauncherDelegateWin::GetSandboxTag() {
 
 bool RendererSandboxedProcessLauncherDelegateWin::InitializeConfig(
     sandbox::TargetConfig* config) {
-  DCHECK(!config->IsConfigured());
+  CHECK(!config->IsConfigured(), base::NotFatalUntil::M153);
 
   sandbox::policy::SandboxWin::AddBaseHandleClosePolicy(config);
 
