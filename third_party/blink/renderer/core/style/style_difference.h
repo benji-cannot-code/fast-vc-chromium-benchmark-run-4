e67d71f551b3173deb359631a557ee4d1876a556
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-07-09 15:11:29 -0700

    [layout] Shift ax-style invalidation to StyleDidChange, use field-diff.
    Most style invalidation logic for layout-objects should exist within
    StyleDidChange (not StyleWillChange). This shifts the accessibility
    style invalidation logic to StyleDidChange.
    
    Additionally change the logic to use the more efficient field-diff
    logic. Thee are some minor differences here, but shouldn't affect real
    world performance in any meaningful way.
    
    Change-Id: I17cea2713709afe87ba6de3e5e51ec715fc0e98b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070434
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659882}

--- third_party/blink/renderer/core/style/style_difference.h
+++ third_party/blink/renderer/core/style/style_difference.h
@@ -41,6 +41,7 @@ class StyleDifference {
     layout_type_ = std::max(layout_type_, other.layout_type_);
     needs_box_paint_property_update |= other.needs_box_paint_property_update;
     ax_visibility_or_inert_changed |= other.ax_visibility_or_inert_changed;
+    ax_style_changed |= other.ax_style_changed;
   }
 
   bool HasDifference() const {
@@ -53,7 +54,7 @@ class StyleDifference {
            text_decoration_or_color_changed || transform_changed ||
            transform_data_changed || z_index_changed || paint_type_ ||
            layout_type_ || needs_box_paint_property_update ||
-           ax_visibility_or_inert_changed;
+           ax_visibility_or_inert_changed || ax_style_changed;
   }
 
   // For simple paint invalidation, we can directly invalidate the
@@ -106,6 +107,7 @@ class StyleDifference {
   unsigned z_index_changed : 1 = false;
   unsigned needs_box_paint_property_update : 1 = false;
   unsigned ax_visibility_or_inert_changed : 1 = false;
+  unsigned ax_style_changed : 1 = false;
 
  private:
   enum PaintType { kNoPaint = 0, kSimplePaint, kNormalPaint };
@@ -119,7 +121,7 @@ class StyleDifference {
   // writes to the stack and then reading the same data back again with a large
   // read can cause store-to-load forward stalls). Feel free to take bits from
   // here if you need them for something else.
-  unsigned padding_ [[maybe_unused]] : 8;
+  unsigned padding_ [[maybe_unused]] : 7;
 
   friend CORE_EXPORT std::ostream& operator<<(std::ostream&,
                                               const StyleDifference&);
