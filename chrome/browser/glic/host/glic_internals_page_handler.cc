de6475fd0db20b17e8a937226df574524d1720d4
Theodore Olsauskas-Warren <sauski@google.com>
2026-07-09 10:16:11 -0700

    [Glic] Add experimental triggering state to Glic internals
    To make debugging experimental triggering easier, the state that is
    synced and used for eligibility is included in the
    chrome://glic/internals page.
    
    Bug: None
    Change-Id: Icb8d214606f434ee2fffec347505e903eb910bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8046165
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659664}

--- chrome/browser/glic/host/glic_internals_page_handler.cc
+++ chrome/browser/glic/host/glic_internals_page_handler.cc
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/skills/features.h"
 #include "components/subscription_eligibility/subscription_eligibility_service.h"
 #include "components/sync/service/sync_service.h"
+#include "components/sync_device_info/device_info.h"
 #include "components/tabs/public/tab_interface.h"
 #include "components/variations/service/variations_service.h"
 #include "components/variations/service/variations_service_utils.h"
@@ -61,6 +62,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace glic {
 
 namespace {
+
+mojom::GlicExperimentalTriggeringState ConvertGlicExperimentalTriggeringState(
+    syncer::DeviceInfo::GlicExperimentalTriggeringState state) {
+  switch (state) {
+    case syncer::DeviceInfo::GlicExperimentalTriggeringState::kUnavailable:
+      return mojom::GlicExperimentalTriggeringState::kUnavailable;
+    case syncer::DeviceInfo::GlicExperimentalTriggeringState::kNeedsOptIn:
+      return mojom::GlicExperimentalTriggeringState::kNeedsOptIn;
+    case syncer::DeviceInfo::GlicExperimentalTriggeringState::kReady:
+      return mojom::GlicExperimentalTriggeringState::kReady;
+  }
+}
 
 mojom::ProfileEnablementPtr BuildProfileEnablement(
     content::BrowserContext* browser_context,
@@ -94,6 +107,15 @@ mojom::ProfileEnablementPtr BuildProfileEnablement(
   auto* service = GlicKeyedService::Get(profile);
   result->actuation_is_consented =
       (service && service->enabling().GetUserEnabledActuationOnWeb());
+
+  if (service) {
+    result->glic_experimental_triggering_state =
+        ConvertGlicExperimentalTriggeringState(
+            service->enabling().GetExperimentalTriggeringState());
+  } else {
+    result->glic_experimental_triggering_state =
+        mojom::GlicExperimentalTriggeringState::kUnavailable;
+  }
 
   using CannotActReason = ::glic::CannotActReason;
   if (actor_policy_checker) {
