7a869bb5fc75ef51a4661a637057ce9ea56bde7a
Stephan Drab <sdrab@google.com>
2026-07-09 09:13:13 -0700

    Add metrics for the Gmail OTP settings toggle
    Bug: b:522370224
    Change-Id: Ic8df546754fca54ddeb7137da701eefdf2e928e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063888
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Stephan Drab <sdrab@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659608}

--- chrome/test/data/webui/settings/autofill_section_test.ts
+++ chrome/test/data/webui/settings/autofill_section_test.ts
@@ -587,6 +587,7 @@ suite('AutofillSectionUiTest', function() {
   });
 
   test('OtpFillingToggleDisabledThenToggledAndEnabled', async function() {
+    const metricsTracker = fakeMetricsPrivate();
     loadTimeData.overrideValues({autofillGmailOtpFillingEnabled: true});
     const {section, toggle} = await createAutofillSectionForGmailOtpFilling();
     assertTrue(!!toggle);
@@ -602,6 +603,14 @@ suite('AutofillSectionUiTest', function() {
     assertTrue(toggle.checked);
     assertTrue(
         section.getPref<boolean>('autofill.gmail_otp_filling.enabled').value);
+    assertEquals(
+        1, metricsTracker.count('Autofill.GmailOtpOptIn.SettingsChange', true));
+
+    toggle.click();
+    assertFalse(toggle.checked);
+    assertEquals(
+        1,
+        metricsTracker.count('Autofill.GmailOtpOptIn.SettingsChange', false));
   });
 });
 
