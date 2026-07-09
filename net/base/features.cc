0c13eef81abea175909f078c16a42f149af0a9a8
Ari Chivukula <arichiv@chromium.org>
2026-07-08 17:12:12 -0700

    [TCP] Remove TcpSocketPoolLimitRandomizationForProxy feature
    This is launched in M150 and default enabled in M151, so after the
    branch cut we can remove this for M152.
    
    Fixed: 497837927
    Change-Id: I6a453919aa51a331dbe090102de575a7e15113d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7893516
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659155}

--- net/base/features.cc
+++ net/base/features.cc
@@ -671,9 +671,6 @@ BASE_FEATURE_PARAM(double,
                    "TcpSocketPoolLimitRandomizationNoise",
                    0.2);
 
-BASE_FEATURE(kTcpSocketPoolLimitRandomizationForProxy,
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kNetTaskScheduler, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kNetTaskSchedulerHostResolver, base::FEATURE_DISABLED_BY_DEFAULT);
 
