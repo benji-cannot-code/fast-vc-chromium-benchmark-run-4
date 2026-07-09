9cc870b88e2d4decc3ff6a4d54a70aed87c770f4
Noam Rosenthal <nrosenthal@chromium.org>
2026-07-09 12:26:36 -0700

    Make everything in PIs case sensitive
    This includes PI targets and attribute names. Following the merged
    processing instruction spec in DOM+HTML.
    
    Also now we don't need the intermediate buffer when parsing.
    
    Existing upstream tests already assert this, though some needed a bit of
    result tweaking.
    
    Bug: 481087638
    Change-Id: I50913cec8d0dbb2e48df5b4c52db6f1d92530f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7946164
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659761}

--- third_party/blink/renderer/core/dom/processing_instruction.cc
+++ third_party/blink/renderer/core/dom/processing_instruction.cc
@@ -191,7 +191,6 @@ bool ProcessingInstruction::ValidateAttributeName(
 const AtomicString& ProcessingInstruction::GetAttributeValue(
     const AtomicString& name,
     const AtomicString& default_value) {
-  DCHECK_EQ(name, name.ToAsciiLower());
   ProcessAttributesIfNeeded();
   for (const auto& pair : attributes_) {
     if (pair.key == name) {
@@ -201,8 +200,7 @@ const AtomicString& ProcessingInstruction::GetAttributeValue(
   return default_value;
 }
 
-bool ProcessingInstruction::HasAttribute(const AtomicString& name) {
-  DCHECK_EQ(name, name.ToAsciiLower());
+bool ProcessingInstruction::hasAttribute(const AtomicString& name) {
   ProcessAttributesIfNeeded();
   for (const auto& pair : attributes_) {
     if (pair.key == name) {
@@ -214,7 +212,6 @@ bool ProcessingInstruction::HasAttribute(const AtomicString& name) {
 
 void ProcessingInstruction::SetAttribute(const AtomicString& name,
                                          const AtomicString& value) {
-  DCHECK_EQ(name, name.ToAsciiLower());
   DCHECK(ValidateAttributeName(name, ASSERT_NO_EXCEPTION));
 
   ProcessAttributesIfNeeded();
@@ -229,8 +226,7 @@ void ProcessingInstruction::SetAttribute(const AtomicString& name,
   UpdateDataFromAttributes();
 }
 
-void ProcessingInstruction::RemoveAttribute(const AtomicString& name) {
-  DCHECK_EQ(name, name.ToAsciiLower());
+void ProcessingInstruction::removeAttribute(const AtomicString& name) {
   ProcessAttributesIfNeeded();
   const wtf_size_t size = attributes_.size();
   for (wtf_size_t i = 0; i < size; ++i) {
@@ -249,8 +245,7 @@ void ProcessingInstruction::ToggleAttribute(const AtomicString& name,
     return;
   }
 
-  DCHECK_EQ(name, name.ToAsciiLower());
-  const bool already_there = HasAttribute(name);
+  const bool already_there = hasAttribute(name);
   force = force.value_or(!already_there);
 
   if (*force) {
@@ -258,7 +253,7 @@ void ProcessingInstruction::ToggleAttribute(const AtomicString& name,
       SetAttribute(name, g_empty_atom);
     }
   } else {
-    RemoveAttribute(name);
+    removeAttribute(name);
   }
 }
 
