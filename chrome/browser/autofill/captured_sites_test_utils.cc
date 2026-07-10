5c1cff36af8333644cf479bed899f959afc71957
Johannes Henkel <johannes@chromium.org>
2026-07-10 16:06:52 -0700

    [soft navs] Set timestamp on simulated event.
    It turns out that for some tests, mouse events trigger soft navigation
    detection, which since crrev.com/c/8072798 notices when the timestamp
    on the interaction is unrealistically early, leading to a
    check-failure. This leads to test failures in
    CapturedSitesPasswordManagerBrowserTest (see first bug).  Setting the
    timestamp to the current time makes it pass, and it's also correct in
    spirit, for these tests.
    
    Bug: 533385845,490814752
    Change-Id: Iab33b46b1d748c916a621d2e4e08c02e12bd69b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076959
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Johannes Henkel <johannes@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660584}

--- chrome/browser/autofill/captured_sites_test_utils.cc
+++ chrome/browser/autofill/captured_sites_test_utils.cc
@@ -2452,6 +2452,7 @@ bool TestRecipeReplayer::SimulateLeftMouseClickAt(
       blink::WebInputEvent::GetStaticTimeStampForTests());
   mouse_event.button = blink::WebMouseEvent::Button::kLeft;
   mouse_event.SetPositionInWidget(point.x(), point.y());
+  mouse_event.SetTimeStamp(base::TimeTicks::Now());
 
   // Mac needs positionInScreen for events to plugins.
   gfx::Rect offset =
