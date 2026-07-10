43dbdf164e49d5c0f4e754d0d6c1f1b90127c100
Duncan Mercer <mercerd@google.com>
2026-07-10 14:01:09 -0700

    [contextual_tasks] Add ContextualTasksSidePanelRearchitecture Flag
    This is a separate flag specific to the side panel portions of the
    rearchitecture so that we can enable/disable the side panel changes
    separate from the full tab.
    
    Bug: b:533469161
    Change-Id: If2a437a275e3da7e5b1da9d12096d75e61d1d649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076180
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660516}

--- components/contextual_tasks/public/features.h
+++ components/contextual_tasks/public/features.h
@@ -125,6 +125,9 @@ BASE_DECLARE_FEATURE(kContextualTasksUploadChunking);
 // new side panel and ghost loader for contextual tasks.
 BASE_DECLARE_FEATURE(kContextualTasksRearchitecture);
 
+// Enables the side panel changes as part of the rearchitecture.
+BASE_DECLARE_FEATURE(kContextualTasksSidePanelRearchitecture);
+
 // Enables sticky conversation UI that follows the user around.
 BASE_DECLARE_FEATURE(kContextualTasksEnableStickyConversation);
 
@@ -464,6 +467,8 @@ extern const char kContextualTasksRearchitectureName[];
 extern const char kContextualTasksRearchitectureDescription[];
 extern const char kContextualTasksEphemeralBrandedEntryPointName[];
 extern const char kContextualTasksEphemeralBrandedEntryPointDescription[];
+extern const char kContextualTasksSidePanelRearchitectureName[];
+extern const char kContextualTasksSidePanelRearchitectureDescription[];
 
 }  // namespace flag_descriptions
 
