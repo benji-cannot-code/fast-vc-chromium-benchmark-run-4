1e4aea2487828045dbcf86a571468cfb1e7898ab
Fuhsin Liao <fuhsinliao@google.com>
2026-07-09 22:26:49 -0700

    DatePicker: Use M3's DatePickcer Library
    Demo: https://screencast.googleplex.com/cast/NTEwNzg0MjU5NDg5NzkyMHwwMDhmNzIxZi0wOA
    
    This cl introduces Material3's DatePicker UI to replace the built-in
    Android DatePicker UI. This cl doesn't change any thing about other
    types of calendar (e.g. Time/DateTime/Week/Month picker).
    
    This cl utilizes a ServiceLoader to load the actual implementation of
    MaterialDatePicker(in //chrome, a higher-level directory) to the
    InputDialogContainer(in a lower-level directory), and replace the
    DatePicker's UI with MaterialDatePicker if the loading is non-null,
    otherwise falls back to the default Android's DatePicker model.
    
    For more info please refer to:
    https://docs.google.com/document/d/1tH8OyZlddRUQMkYxqxMl9UiwrfNe18xjcKlu6DFoVMI/edit?tab=t.0
    
    Binary-Size: Added a new library.
    Bug: 483569814
    Change-Id: Ib34d426b2a3b2b4f58b64ed8a027e5f6f26769e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801519
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660063}

---
+++ ui/android/java/src/org/chromium/ui/base/MaterialDatePickerProvider.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.base;
+
+import android.content.Context;
+
+import org.chromium.base.Callback;
+import org.chromium.build.annotations.NullMarked;
+
+@NullMarked
+/**
+ * Provider interface to display a Material Date Picker dialog. This interface is implemented in
+ * higher layers and registered via ServiceLoader.
+ */
+public interface MaterialDatePickerProvider {
+    /**
+     * Shows a Material Date Picker dialog.
+     *
+     * @param context The Android context to launch the dialog from.
+     * @param minTime The minimum selectable date in UTC milliseconds since epoch.
+     * @param maxTime The maximum selectable date in UTC milliseconds since epoch.
+     * @param selectionCallback The callback invoked when the user selects a date.
+     * @param onDismiss The callback invoked when the dialog is dismissed or canceled.
+     * @return Whether the Material Date Picker dialog was successfully shown.
+     */
+    boolean showDatePicker(
+            Context context,
+            long minTime,
+            long maxTime,
+            Callback<Long> selectionCallback,
+            Runnable onDismiss);
+}
