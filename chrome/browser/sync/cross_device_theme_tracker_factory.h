377bc86ffa6c5653d03cb1bbe48ec95cd0d20984
Jenna Himawan <jhimawan@google.com>
2026-07-10 12:46:04 -0700

    CrossDeviceThemeTracker: Move LocalThemeSpecifics to Tracker header
    - Move LocalThemeSpecifics type alias definition to
      cross_device_theme_tracker.h under namespace themes, so it can be
      shared between ChromeSyncControllerBuilder and
      CrossDeviceThemeTrackerFactory.
    - Update CrossDeviceThemeTrackerFactory to use LocalThemeSpecifics to
      improve type safety and consistency.
    
    TAG=agy
    CONV=bfdbedd4-63c8-4089-90ed-438f127ba505
    
    Bug: 517615321
    Change-Id: I9fc5b3c80ec88408c8b3f7f443e1919379a4fea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070129
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660468}

--- chrome/browser/sync/cross_device_theme_tracker_factory.h
+++ chrome/browser/sync/cross_device_theme_tracker_factory.h
@@ -8,15 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
-#include "components/sync/protocol/theme_specifics.pb.h"
 #include "components/themes/cross_device/cross_device_theme_tracker.h"
 
 class Profile;
 
 class CrossDeviceThemeTrackerFactory : public ProfileKeyedServiceFactory {
  public:
-  static themes::CrossDeviceThemeTracker<sync_pb::ThemeSpecifics>*
-  GetForProfile(Profile* profile);
+  using LocalThemeSpecifics = themes::LocalThemeSpecifics;
+
+  static themes::CrossDeviceThemeTracker<LocalThemeSpecifics>* GetForProfile(
+      Profile* profile);
   static CrossDeviceThemeTrackerFactory* GetInstance();
 
  private:
--- components/themes/cross_device/cross_device_theme_tracker.h
+++ components/themes/cross_device/cross_device_theme_tracker.h
@@ -14,18 +14,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
+#include "build/build_config.h"
+#include "build/buildflag.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/base/data_type.h"
 #include "components/sync/model/data_type_controller_delegate.h"
 #include "components/sync/model/data_type_local_change_processor.h"
 #include "components/sync/model/data_type_sync_bridge.h"
+#if BUILDFLAG(IS_ANDROID)
+#include "components/sync/protocol/theme_android_specifics.pb.h"
+#else
+#include "components/sync/protocol/theme_specifics.pb.h"
+#endif
 #include "components/sync/protocol/theme_types.pb.h"
 #include "components/sync_device_info/device_info.h"
 #include "components/sync_device_info/device_info_tracker.h"
 #include "components/themes/cross_device/theme_comparer.h"
 
 namespace themes {
+
+#if BUILDFLAG(IS_ANDROID)
+using LocalThemeSpecifics = sync_pb::ThemeAndroidSpecifics;
+#else
+using LocalThemeSpecifics = sync_pb::ThemeSpecifics;
+#endif  // BUILDFLAG(IS_ANDROID)
 
 enum class ServiceStatus {
   kInitializing,
