2df640f21ab28e5c17dbe778a11ddf1d760d5f37
Philipp Hancke <philipp.hancke@googlemail.com>
2026-07-10 04:10:02 -0700

    webrtc: remove dead code
    Bug: chromium:40602745
    Change-Id: I2f6393ebe6e44c725190318126ad853a7be37c45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8064203
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660168}

--- third_party/blink/renderer/modules/peerconnection/rtc_void_request_promise_impl.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_void_request_promise_impl.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
-#include "third_party/blink/renderer/modules/peerconnection/rtc_session_description_enums.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/peerconnection/rtc_void_request.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
