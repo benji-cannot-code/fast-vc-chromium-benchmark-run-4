bdde9a3b21188b9207db499d20eae22ff2048e7e
Seung Jae Lim <andysjlim@chromium.org>
2026-07-10 15:07:11 -0700

    Introduce typestate for mojo messages
    This introduces typestates of 4 states, newly-created (Writable),
    Sendable, FullyReadableWithHandles, and ReadableBytesOnly. This handles
    all state the Mojo messages are in. One benefit of this is to make most
    functions catered to specific states and keep it clear on what the
    current state of the message is at and what capabilities the message is
    able to have at that state.
    
    Bug: 493265340
    Change-Id: Ia2a351c39060d0217abe362aae60f0d2f6a6dde3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8056304
    Commit-Queue: Andy Lim <andysjlim@chromium.org>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660556}

--- mojo/public/rust/bindings/multiplex_router/router.rs
+++ mojo/public/rust/bindings/multiplex_router/router.rs
@@ -259,7 +259,11 @@ impl MultiplexRouter {
         // a disconnected ID will be ignored on the other side.
         if self.shared_state.lock().unwrap().registry.endpoint_map.contains_key(&interface_id) {
             msg.header.interface_id = interface_id;
-            self.endpoint_watcher.with(|watcher| watcher.send_message(msg.into()));
+            self.endpoint_watcher.with(|watcher| {
+                watcher.send_message(
+                    system::message::WritableMessage::from(msg).finalize_for_sending(),
+                )
+            });
         }
     }
 
@@ -288,7 +292,9 @@ impl MultiplexRouter {
             self.endpoint_watcher.with(|watcher| {
                 // The send can only fail if the entire other side is closed,
                 // in which case we don't need to tell it anything.
-                let _ = watcher.send_message(msg.into());
+                let _ = watcher.send_message(
+                    system::message::WritableMessage::from(msg).finalize_for_sending(),
+                );
             });
 
             self.schedule_all_possible_tasks();
@@ -302,7 +308,7 @@ impl MultiplexRouter {
     /// run whenever there's an incoming message.
     fn incoming_message_handler(
         &self,
-        raw_message: system::message::RawMojoMessage,
+        raw_message: system::message::FullyReadableWithHandlesMessage,
         _sender: ResponseSender,
     ) {
         let Some(message) = MojomMessage::parse_raw_or_report_bad_message(raw_message) else {
