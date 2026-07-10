4e2caac2189d4489a70ba64602f1ec0ec4f10243
Sylvain Defresne <sdefresne@chromium.org>
2026-07-10 10:31:33 -0700

    [ios] Avoid hard-coding the size of kChromeHostURLs array
    On 2026-07-10, two CLs landed and both added one item to this array,
    causing a semantic conflict (because gerrit rebased the second CL on
    top of the tree and the change in size was identical and "merged")
    which lead to one of the CL being reverted.
    
    The two CLs were:
    - https://crrev.com/c/8054128
    - https://crrev.com/c/8054827
    
    Change the definition of kChromeHostURLs to use std::to_array<...>
    which allow the compiler to compute the size for the array during
    the compilation and eliminating that class of errors.
    
    Bug: none
    Change-Id: I8314ca7f4f5c402b4d0476e875c2b6a3eec9b7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071106
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ahmed Raslan <rslan@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Ahmed Raslan <rslan@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660374}

--- ios/chrome/browser/shared/model/url/chrome_url_constants.h
+++ ios/chrome/browser/shared/model/url/chrome_url_constants.h
@@ -230,7 +230,7 @@ inline constexpr char kGeminiAppStorePromoURL[] =
     "https://www.google.com/chrome/mobile/#scrollable-gemini-in-chrome-slide-1";
 
 // Gets the hosts/domains that are shown in chrome://chrome-urls.
-inline constexpr std::array<std::string_view, 23> kChromeHostURLs = {
+inline constexpr auto kChromeHostURLs = std::to_array<std::string_view>({
     commerce::kChromeUICommerceInternalsHost,
     kChromeUIChromeURLsHost,
     kChromeUICreditsHost,
@@ -254,6 +254,6 @@ inline constexpr std::array<std::string_view, 23> kChromeHostURLs = {
     kChromeUIVersionHost,
     optimization_guide_internals::kChromeUIOptimizationGuideInternalsHost,
     regional_capabilities::kChromeUIRegionalCapabilitiesInternalsHost,
-};
+});
 
 #endif  // IOS_CHROME_BROWSER_SHARED_MODEL_URL_CHROME_URL_CONSTANTS_H_
