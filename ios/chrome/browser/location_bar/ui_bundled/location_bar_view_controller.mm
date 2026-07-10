45758183ec70a000883c23ef5ab3ebe4be06eb74
Ewann Pellé <ewannpv@chromium.org>
2026-07-10 07:24:31 -0700

    [iOS][Assistant] Restrict omnibox entry point to debug mode
    Fixed: 532866925
    Change-Id: If66a764200e82960356195d195bac99ca262bbc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063936
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660250}

--- ios/chrome/browser/location_bar/ui_bundled/location_bar_view_controller.mm
+++ ios/chrome/browser/location_bar/ui_bundled/location_bar_view_controller.mm
@@ -1113,7 +1113,7 @@ const CGFloat kGeminiLiveCircleSize = 20.0;
   }
 
   // Used to easily trigger the Assistant sheet during development.
-  if (IsAssistantContainerEnabled()) {
+  if (IsAssistantContainerDebugEnabled()) {
     UIAction* assistantAction = [UIAction
         actionWithTitle:l10n_util::GetNSString(IDS_IOS_APP_BAR_ASK_GEMINI)
                   image:DefaultSymbolWithPointSize(kMagicStackSymbol,
