e3e18983bf4461e0ec830d07b9c2e0c74531d655
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-10 13:11:40 -0700

    Add TabAndroidGracefulShutdown to fieldtrial_testing_config.json
    Bug: 494305655
    Change-Id: Iaea37c74982f61bb05b2e7fde0443bb16a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8039830
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660487}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabViewAndroidDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabViewAndroidDelegate.java
@@ -97,6 +97,7 @@ public class TabViewAndroidDelegate extends ViewAndroidDelegate {
 
     @Override
     public void onBackgroundColorChanged(int color) {
+        if (mTab.isDestroyed()) return;
         mTab.changeWebContentBackgroundColor(color);
     }
 
@@ -107,6 +108,7 @@ public class TabViewAndroidDelegate extends ViewAndroidDelegate {
             int topControlsMinHeightOffsetY,
             int bottomControlsOffsetY,
             int bottomControlsMinHeightOffsetY) {
+        if (mTab.isDestroyed()) return;
         TabBrowserControlsOffsetHelper.get(mTab)
                 .setOffsets(
                         topControlsOffsetY,
@@ -123,6 +125,7 @@ public class TabViewAndroidDelegate extends ViewAndroidDelegate {
 
     /** Sets the Visual Viewport bottom inset. */
     private void updateVisualViewportBottomInset() {
+        if (mTab.isDestroyed()) return;
         int inset =
                 mTab.isHidden()
                                 || mCurrentInsetSupplier == null
@@ -188,16 +191,19 @@ public class TabViewAndroidDelegate extends ViewAndroidDelegate {
 
     @Override
     public void onProvideAutofillVirtualStructure(ViewStructure structure, int flags) {
+        if (mTab.isDestroyed()) return;
         mTab.onProvideAutofillVirtualStructure(structure, flags);
     }
 
     @Override
     public void autofill(final SparseArray<AutofillValue> values) {
+        if (mTab.isDestroyed()) return;
         mTab.autofill(values);
     }
 
     @Override
     public boolean providesAutofillStructure() {
+        if (mTab.isDestroyed()) return false;
         return mTab.providesAutofillStructure();
     }
 
