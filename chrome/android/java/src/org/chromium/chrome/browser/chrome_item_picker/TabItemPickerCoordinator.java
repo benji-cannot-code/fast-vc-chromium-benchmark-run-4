87036dc65a5199ab7e26ded34101c21837a09624
Sky Malice <skym@chromium.org>
2026-07-09 17:36:41 -0700

    [AIM] Emit count of tabs upon picker cancel.
    Bug: 532155530
    Change-Id: I07e76b31e2ef55fecfb0bd794755f4d63d313a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8056345
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659981}

--- chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/chrome_item_picker/TabItemPickerCoordinator.java
@@ -226,7 +226,7 @@ public class TabItemPickerCoordinator {
                 new TabModelSelectorObserver() {
                     @Override
                     public void onDestroyed() {
-                        cancelPicker(mActivity);
+                        cancelPicker();
                         tabModelSelector.removeObserver(this);
                     }
                 };
@@ -238,7 +238,7 @@ public class TabItemPickerCoordinator {
                     new IncognitoTabModelObserver() {
                         @Override
                         public void didBecomeEmpty() {
-                            cancelPicker(mActivity);
+                            cancelPicker();
                             incognitoTabModel.removeIncognitoObserver(this);
                         }
                     };
@@ -360,11 +360,17 @@ public class TabItemPickerCoordinator {
         controller.selectTabs(selectionSet);
     }
 
-    private static void cancelPicker(Activity activity) {
-        if (activity instanceof ChromeItemPickerActivity cipa) {
+    /** Cancels the item picker activity. */
+    void cancelPicker() {
+        int selectedCount =
+                mNavigationProvider != null ? mNavigationProvider.getSelectedItemsCount() : 0;
+        RecordHistogram.recordCount100Histogram(
+                "Android.TabItemPicker.Cancel.SelectedTabs.Count", selectedCount);
+
+        if (mActivity instanceof ChromeItemPickerActivity cipa) {
             cipa.finishWithCancel();
         } else {
-            activity.finish();
+            mActivity.finish();
         }
     }
 
@@ -379,10 +385,12 @@ public class TabItemPickerCoordinator {
         private final TabContentManager mTabContentManager;
         private final Set<Integer> mCachedTabIds;
         private final Set<TabListEditorItemSelectionId> mInitialSelectedTabIds;
+        private final Runnable mCancelRunnable;
         private final Map<Tab, Long> mLoadingTabsToStartTimes = new HashMap<>();
         private final LoadIfNeededCallback mLoadIfNeededCallback = this::onTabLoadFinished;
 
         private boolean mIsDestroyed;
+        private int mSelectedItemsCount;
 
         public ItemPickerNavigationProvider(
                 Activity activity,
@@ -390,17 +398,26 @@ public class TabItemPickerCoordinator {
                 TabModelSelector tabModelSelector,
                 TabContentManager tabContentManager,
                 Set<Integer> cachedTabIds,
-                Set<TabListEditorItemSelectionId> initialSelectedTabIds) {
+                Set<TabListEditorItemSelectionId> initialSelectedTabIds,
+                Runnable cancelRunnable) {
             mActivity = activity;
             mControllerSupplier = controllerSupplier;
             mTabModelSelector = tabModelSelector;
             mTabContentManager = tabContentManager;
             mCachedTabIds = cachedTabIds;
             mInitialSelectedTabIds = initialSelectedTabIds;
+            mCancelRunnable = cancelRunnable;
+            mSelectedItemsCount = initialSelectedTabIds.size();
+        }
+
+        /** Returns the number of currently selected items in the picker. */
+        public int getSelectedItemsCount() {
+            return mSelectedItemsCount;
         }
 
         @Override
         public void onSelectionStateChange(Set<TabListEditorItemSelectionId> selectedItems) {
+            mSelectedItemsCount = selectedItems.size();
             boolean hasSelectionChanged = !Objects.equals(mInitialSelectedTabIds, selectedItems);
             mEnableDoneButtonSupplier.set(hasSelectionChanged);
 
@@ -537,8 +554,7 @@ public class TabItemPickerCoordinator {
                 controller.hide();
             }
 
-            // Route back press to the Activity's cancel handler.
-            cancelPicker(mActivity);
+            mCancelRunnable.run();
         }
 
         @Override
@@ -647,7 +663,8 @@ public class TabItemPickerCoordinator {
                         assumeNonNull(mTabModelSelector),
                         tabContentManager,
                         mCachedTabIdsSet,
-                        mInitialSelectedTabIds);
+                        mInitialSelectedTabIds,
+                        this::cancelPicker);
 
         TabListEditorCoordinator coordinator =
                 new TabListEditorCoordinator(
@@ -682,5 +699,9 @@ public class TabItemPickerCoordinator {
 
     public @Nullable ItemPickerNavigationProvider getItemPickerNavigationProviderForTesting() {
         return mNavigationProvider;
+    }
+
+    public void setNavigationProviderForTesting(ItemPickerNavigationProvider navigationProvider) {
+        mNavigationProvider = navigationProvider;
     }
 }
