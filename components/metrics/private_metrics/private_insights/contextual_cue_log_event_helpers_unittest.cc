ed1fec1f848cb44b8f8043de356fea50d0c9cd54
Jakub Kudzia <jkudzia@google.com>
2026-07-10 08:52:00 -0700

    Add tabs_shown field to ContextualCueLogEvent
    Extend ContextualCueLogEvent proto to include tabs_shown field,
    representing tabs chosen for comparison (from
    AnchoredMessage.tabs_to_show).
    
    BUG=530498978
    TAG=agy
    CONV=47650c78-5a91-46a3-8fb0-ff060d476588
    
    Change-Id: I8caffdc17045d23d8854bf0739ebdb15ad0de5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8051670
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Jakub Kudzia <jkudzia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660307}

--- components/metrics/private_metrics/private_insights/contextual_cue_log_event_helpers_unittest.cc
+++ components/metrics/private_metrics/private_insights/contextual_cue_log_event_helpers_unittest.cc
@@ -13,10 +13,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace private_insights {
+namespace {
+
+std::optional<std::pair<std::string, std::string>> ExtractFromPageInfo(
+    const events::ContextualCueLogEvent::PageInfo& page_info) {
+  return std::make_pair(page_info.url(), page_info.title());
+}
+
+}  // namespace
 
 TEST(ContextualCueLogEventHelpersTest, SerializeEmptyList) {
   std::vector<events::ContextualCueLogEvent::PageInfo> empty_list;
-  EXPECT_EQ("[]", SerializePageInfoListToJson(empty_list));
+  EXPECT_EQ("[]",
+            SerializeCollectionToPageInfoJson(empty_list, ExtractFromPageInfo));
 }
 
 TEST(ContextualCueLogEventHelpersTest, SerializeOneItemList) {
@@ -24,7 +33,7 @@ TEST(ContextualCueLogEventHelpersTest, SerializeOneItemList) {
   page_info.set_url("http://url1.com");
   page_info.set_title("Title 1");
   std::vector<events::ContextualCueLogEvent::PageInfo> list = {page_info};
-  EXPECT_THAT(SerializePageInfoListToJson(list),
+  EXPECT_THAT(SerializeCollectionToPageInfoJson(list, ExtractFromPageInfo),
               base::test::IsJson(
                   "[{\"url\":\"http://url1.com\",\"title\":\"Title 1\"}]"));
 }
@@ -42,7 +51,7 @@ TEST(ContextualCueLogEventHelpersTest, SerializeMultipleItemsList) {
 
   std::vector<events::ContextualCueLogEvent::PageInfo> list = {
       page_info1, page_info2, page_info3};
-  EXPECT_THAT(SerializePageInfoListToJson(list),
+  EXPECT_THAT(SerializeCollectionToPageInfoJson(list, ExtractFromPageInfo),
               base::test::IsJson(
                   "[{\"url\":\"http://url1.com\",\"title\":\"Title 1\"},"
                   "{\"url\":\"http://url2.com\",\"title\":\"Title 2\"},"
@@ -62,7 +71,8 @@ TEST(ContextualCueLogEventHelpersTest, SerializeSpecialCharactersList) {
 
   std::vector<events::ContextualCueLogEvent::PageInfo> list = {
       page_info1, page_info2, page_info3};
-  std::string json = SerializePageInfoListToJson(list);
+  std::string json =
+      SerializeCollectionToPageInfoJson(list, ExtractFromPageInfo);
   EXPECT_THAT(json,
               base::test::IsJson(
                   "[{\"url\":\"http://url1.com/a,b\",\"title\":\"Title 1, with "
