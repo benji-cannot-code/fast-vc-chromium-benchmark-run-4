b1772657f7336335923ef0ea9e838dd45c723467
Wan-Teh Chang <wtc@google.com>
2026-07-10 14:37:43 -0700

    Roll src/third_party/dav1d/libdav1d/ c150ba6c9..07f1214d0 (7 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/c150ba6c9b9b..07f1214d0e68
    
    $ git log c150ba6c9..07f1214d0 --date=short --no-merges --format='%ad %ae %s'
    2026-07-09 Arpad.Panyik AArch64: Simplify some Neon load/store addressing
    2026-07-08 Arpad.Panyik AArch64: Use ldrel for constant loads
    2026-07-08 Arpad.Panyik AArch64: Add ldrel macro
    2026-07-06 martin arm: Fix code style in asm.S
    2026-06-15 GermanAizek lr_apply: move out of loop constant-calculated expressions
    2026-06-25 GermanAizek ipred_cfl_c: use const for variable dc
    2026-07-05 GermanAizek decode: move out of loop constant-calculated expressions
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ice12cfa2ac068a4f84dc51c696c15b808b1d5060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076638
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660541}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.3-68-gc150ba6c"
+#define DAV1D_VERSION "1.5.3-75-g07f1214d"
