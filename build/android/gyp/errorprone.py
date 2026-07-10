6e1f120586040964d10fb61b2c94f96ff9a971f0
Andrew Grieve <agrieve@chromium.org>
2026-07-10 06:47:15 -0700

    Android: Target Java 25 rather than Java 21
    The main draw (IMO) is unnamed variables: try (_ = new ScopedThing()) {
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Change-Id: Ia6b9983cb605e542a09561608150c6262776b89c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036922
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660233}

--- build/android/gyp/errorprone.py
+++ build/android/gyp/errorprone.py
@@ -88,6 +88,7 @@ ERRORPRONE_WARNINGS_TO_DISABLE = [
     'TryFailRefactoring',
     'TypeParameterNaming',
     'UngroupedOverloads',
+    'UnnamedVariable',
     'UnnecessaryAnonymousClass',
     'UnnecessaryBoxedAssignment',
     'UnnecessaryDefaultInEnumSwitch',
