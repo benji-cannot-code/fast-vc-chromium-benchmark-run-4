044edf1e9d0045b08619bdb0ba98f5f62424022a
Maria del Pilar Dominguez Molina <mardelpi@google.com>
2026-07-10 14:38:00 -0700

    [ChroMenu Glow Up] Implement MenuItemActionViewInterface on MenuItemView
    Implements MenuItemActionViewInterface (inheriting from
    BaseActionViewInterface) and overrides GetActionViewInterface() on
    views::MenuItemView. This allows any standard MenuItemView to be
    dynamically bound to an actions::ActionItem using ActionViewController
    without requiring a custom MenuItemView subclass.
    
    Will continue the implementation of this interface on the child CL
    8072198
    
    Bug-Id: 528054478
    Change-Id: I7b8fa88120d277238e20dd483efaa977f6b92656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071135
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: María del Pilar Domínguez Molina <mardelpi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660542}

--- ui/views/controls/menu/menu_item_view.cc
+++ ui/views/controls/menu/menu_item_view.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/accessibility/ax_virtual_view.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/actions/action_view_interface.h"
 #include "ui/views/badge_painter.h"
 #include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/image_view.h"
@@ -1902,6 +1903,23 @@ void MenuItemView::UpdateAccessibleExpandedCollapsedState() {
     GetViewAccessibility().SetIsExpanded();
   } else {
     GetViewAccessibility().SetIsCollapsed();
+  }
+}
+
+std::unique_ptr<ActionViewInterface> MenuItemView::GetActionViewInterface() {
+  return std::make_unique<MenuItemActionViewInterface>(this);
+}
+
+MenuItemActionViewInterface::MenuItemActionViewInterface(
+    MenuItemView* action_view)
+    : BaseActionViewInterface(action_view), action_view_(action_view) {}
+
+void MenuItemActionViewInterface::ActionItemChangedImpl(
+    actions::ActionItem* action_item) {
+  BaseActionViewInterface::ActionItemChangedImpl(action_item);
+  action_view_->SetTitle(std::u16string(action_item->GetText()));
+  if (!action_item->GetImage().IsEmpty()) {
+    action_view_->SetIcon(action_item->GetImage());
   }
 }
 
