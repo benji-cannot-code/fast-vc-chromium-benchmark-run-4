ba1da6ce23c3e15b5633eb010af48c0cfe4df964
Colin Blundell <blundell@chromium.org>
2026-07-09 23:36:41 -0700

    [Blink] Eliminate WebGpuRecyclableRP using CanvasResourceSI
    Fold all the state to be directly on WebGpuRecyclableResourceProvider
    itself.
    
    Notes:
    
    * The setting of `release_sync_token_` in the constructor comes from
      here [1]
    * The WaitSyncToken() call there comes from here [2]
    * No code is added to the destructor per [3][4]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/shared_image_pool.cc;l=32;drc=0bedfcc1f8e786bfd905e2d16909da4504df025f;bpv=1;bpt=1
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource.cc;l=226;drc=0bedfcc1f8e786bfd905e2d16909da4504df025f?q=CanvasResourceSharedImage::Initia&ss=chromium%2Fchromium%2Fsrc
    [3] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/shared_image_pool.cc;l=37-38?q=ClientImage::Client&ss=chromium
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource.cc;l=75;drc=0bedfcc1f8e786bfd905e2d16909da4504df025f;bpv=1;bpt=1
    
    Bug: 352263194
    Change-Id: I9815fd9d172f3017bbfc3c9e964997d4a1028c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8064716
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660086}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_recyclable_resource_provider.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/client/client_shared_image_interface.h"
 #include "third_party/blink/public/platform/web_graphics_shared_image_interface_provider.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_2d_color_params.h"
-#include "third_party/blink/renderer/platform/graphics/canvas_resource.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
 #include "third_party/blink/renderer/platform/graphics/memory_managed_paint_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
@@ -138,13 +137,6 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
 
   gpu::raster::RasterInterface* RasterInterface() const;
 
-  CanvasResourceSharedImage* resource() {
-    return static_cast<CanvasResourceSharedImage*>(resource_.get());
-  }
-  const CanvasResourceSharedImage* resource() const {
-    return static_cast<const CanvasResourceSharedImage*>(resource_.get());
-  }
-
   base::WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper()
       const {
     return context_provider_wrapper_;
@@ -163,8 +155,9 @@ class PLATFORM_EXPORT WebGpuRecyclableResourceProvider
   std::unique_ptr<CanvasImageProvider> canvas_image_provider_;
   std::unique_ptr<MemoryManagedPaintRecorder> recorder_for_external_draws_;
 
-
-  scoped_refptr<CanvasResourceSharedImage> resource_;
+  scoped_refptr<gpu::ClientSharedImage> shared_image_;
+  gpu::SyncToken acquire_sync_token_;
+  gpu::SyncToken release_sync_token_;
 
   bool is_cleared_ = false;
 
