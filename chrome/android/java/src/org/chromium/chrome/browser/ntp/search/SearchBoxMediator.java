8d6a6fca0428aafbea697b84315772ace2317eb4
Peter Kang <peterkang@google.com>
2026-07-10 11:31:45 -0700

    [Clank Search] Route AI Mode chip on AL fakebox
    Added logic to navigate to AI mode url when AI chip is clicked. Added
    loadUrl method to NewTabPageManager.
    
    Bug: 521458686
    Change-Id: I3b90b7c4225bca2ba46512c96ca8a52da45f0815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8051226
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Peter Kang <peterkang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660413}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediator.java
@@ -36,9 +36,11 @@ import org.chromium.components.omnibox.AutocompleteRequestType;
 import org.chromium.components.omnibox.OmniboxCapabilities;
 import org.chromium.components.search_engines.TemplateUrlService;
 import org.chromium.components.search_engines.TemplateUrlService.TemplateUrlServiceObserver;
+import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
+import org.chromium.url.GURL;
 
 import java.util.function.Supplier;
 
@@ -92,6 +94,7 @@ class SearchBoxMediator implements DestroyObserver {
         mModel.set(SearchBoxProperties.VOICE_SEARCH_CLICK_CALLBACK, this::onVoiceSearchClick);
         mModel.set(SearchBoxProperties.PLUS_BUTTON_CLICK_CALLBACK, this::onPlusButtonClick);
         mModel.set(SearchBoxProperties.LENS_CLICK_CALLBACK, this::onLensClick);
+        mModel.set(SearchBoxProperties.AI_CHIP_CLICK_CALLBACK, this::onAiChipClick);
 
         updateAiChip();
         updateStartIcon();
@@ -101,6 +104,7 @@ class SearchBoxMediator implements DestroyObserver {
     public void onDestroy() {
         mActivityLifecycleDispatcher.unregister(this);
 
+        mModel.set(SearchBoxProperties.AI_CHIP_CLICK_CALLBACK, null);
         mModel.set(SearchBoxProperties.LENS_CLICK_CALLBACK, null);
         mModel.set(SearchBoxProperties.VOICE_SEARCH_CLICK_CALLBACK, null);
         mModel.set(SearchBoxProperties.PLUS_BUTTON_CLICK_CALLBACK, null);
@@ -376,5 +380,12 @@ class SearchBoxMediator implements DestroyObserver {
                 OmniboxCapabilities.isDesktopPlatform()
                         && ComposeplateUtils.isComposeplateEnabled(mProfile);
         mModel.set(SearchBoxProperties.AI_CHIP_VISIBILITY, shouldShow);
+    }
+
+    private void onAiChipClick(View view) {
+        GURL url = mTemplateUrlService.getComposeplateUrl();
+        if (url != null) {
+            mNewTabPageManager.loadUrl(new LoadUrlParams(url), /* incognito= */ false);
+        }
     }
 }
