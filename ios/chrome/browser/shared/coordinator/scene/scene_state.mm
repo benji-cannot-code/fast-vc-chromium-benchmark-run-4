e9e123323fdd0581c571c999f086e8fe31ccff9e
Sylvain Defresne <sdefresne@chromium.org>
2026-07-10 08:08:34 -0700

    [ios] Register AppState as an observer of SceneState upon connection
    Instead of having SceneState register AppState as a SceneStateObserver
    in its initializer, perform the registration in the -sceneWillConnect:
    method of AppState.
    
    The previous registration was backwards and there for legacy reasons,
    but in general it is better if the observer control its registration
    instead of having the observed object doing the registration.
    
    Bug: 519105565
    Change-Id: I6108b074bf4aa885c4866b682d1fdbd864478c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065471
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660278}

--- ios/chrome/browser/shared/coordinator/scene/scene_state.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_state.mm
@@ -84,11 +84,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _layoutState = [[LayoutState alloc] init];
     _lensOverlayStateNotifier = [[LensOverlayStateNotifier alloc] init];
     _prefs = nil;
-
-    // AppState might be nil in tests.
-    if (appState) {
-      [self addObserver:appState];
-    }
   }
   return self;
 }
