70f26f5ea70dad73cec1de706b7cc0aca9ec207c
Emily Shack <emshack@chromium.org>
2026-07-10 08:09:34 -0700

    [GlowUp] Round settings privacy sandbox icons
    See go/chrome-icon-modernization-dd. This is part of a larger effort to
    standardize icon naming, enabling easier bulk updates in the future, as
    well as applying 100% roundedness to all existing icons.
    
    Bug: 511303545
    Change-Id: Ib10417fd9dc613630a0d2dac96caedc9d40e8f8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070118
    Commit-Queue: Abe Boujane <boujane@google.com>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660279}

--- chrome/browser/privacy_sandbox/browsing_topics_settings_interactive_uitest.cc
+++ chrome/browser/privacy_sandbox/browsing_topics_settings_interactive_uitest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/strings/stringprintf.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/privacy_sandbox/privacy_sandbox_prefs.h"
 #include "content/public/test/browser_test.h"
+#include "ui/base/ui_base_features.h"
 
 #if BUILDFLAG(IS_MAC)
 #include "base/mac/mac_util.h"
@@ -233,6 +235,15 @@ IN_PROC_BROWSER_TEST_F(PrivacySandboxSettingsTopicsInteractiveTest,
 // check all icons to make sure default one is not used.
 IN_PROC_BROWSER_TEST_F(PrivacySandboxSettingsTopicsInteractiveTest,
                        ConfirmDefaultIconIsNotUsed) {
+  const std::string expected_artist_icon =
+      base::FeatureList::IsEnabled(features::kWebUIRoundedIcons)
+          ? "firstLevelTopics20:artist"
+          : "firstLevelTopics20:artist-old";
+  const std::string expected_category_icon =
+      base::FeatureList::IsEnabled(features::kWebUIRoundedIcons)
+          ? "firstLevelTopics20:category"
+          : "firstLevelTopics20:category-old";
+
   RunTestSequence(
       InstrumentTab(kPrivacySandboxTopicsElementId),
       NavigateWebContents(kPrivacySandboxTopicsElementId,
@@ -243,16 +254,18 @@ IN_PROC_BROWSER_TEST_F(PrivacySandboxSettingsTopicsInteractiveTest,
                              GetManageTopicsPageQuery() + "settings-subpage")),
       CheckJsResultAt(kPrivacySandboxTopicsElementId,
                       GetManageTopicsPageQuery(),
-                      R"(
+                      base::StringPrintf(R"(
         (el) => Array.from(el.shadowRoot.querySelectorAll('cr-icon')).some(
-                    el => el.icon === 'firstLevelTopics20:artist')
-        )"),
+                    el => el.icon === '%s')
+        )",
+                                         expected_artist_icon.c_str())),
       CheckJsResultAt(kPrivacySandboxTopicsElementId,
                       GetManageTopicsPageQuery(),
-                      R"(
+                      base::StringPrintf(R"(
         (el) => Array.from(el.shadowRoot.querySelectorAll('cr-icon')).some(
-                el => el.icon === 'firstLevelTopics20:category')
+                el => el.icon === '%s')
         )",
+                                         expected_category_icon.c_str()),
                       false));
 }
 
