c0aedba07411bcef4a567a25987e7e6a0e357aef
Ryan Sultanem <rsult@google.com>
2026-07-10 06:13:48 -0700

    [SyncPreview] Ensure all accounts are fetched on account updates
    Modify OnRefreshTokenUpdatedForAccount and
    OnRefreshTokenRemovedForAccount to call EnsureAllAccountsFetched instead
    of fetching only the updated account.
    
    EnsureAllAccountsFetched checks all accounts with refresh tokens, and
    only triggers a fetch for those not currently in cached_data_. This
    ensures all active accounts have preview data populated in the memory
    cache when an account is added or removed, preventing missing data when
    running the heuristic selection.
    
    Bug: 532419984
    Change-Id: I6117c3565ef2e8fe87a75941d9899e1275de9fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8031343
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660212}

--- components/signin/core/browser/account_preview_data_service_impl.cc
+++ components/signin/core/browser/account_preview_data_service_impl.cc
@@ -43,10 +43,6 @@ AccountPreviewDataServiceImpl::AccountPreviewDataServiceImpl(
           &AccountPreviewDataServiceImpl::RefreshAllAccountPreviewData,
           weak_ptr_factory_.GetWeakPtr()));
   repeating_timer_->Start();
-
-  if (identity_manager_->AreRefreshTokensLoaded()) {
-    OnRefreshTokensLoaded();
-  }
 }
 
 AccountPreviewDataServiceImpl::~AccountPreviewDataServiceImpl() = default;
@@ -88,7 +84,7 @@ void AccountPreviewDataServiceImpl::OnRefreshTokenUpdatedForAccount(
   // fetching requests. Startup should only rely on the repeating timer and
   // refresh all accounts preview data.
   if (identity_manager_->AreRefreshTokensLoaded()) {
-    FetchAccountPreviewData(account_info.gaia);
+    EnsureAllAccountsFetched();
   }
 }
 
@@ -103,6 +99,10 @@ void AccountPreviewDataServiceImpl::OnRefreshTokenRemovedForAccount(
   GaiaId gaia_id = info.gaia;
   cached_data_.erase(gaia_id);
   active_fetchers_.erase(gaia_id);
+
+  // TODO(crbug.com/532419984): Restrict this computation if the removed account
+  // is the current preferred account.
+  EnsureAllAccountsFetched();
 }
 
 void AccountPreviewDataServiceImpl::SetFetchCompleteCallbackForTesting(
@@ -129,7 +129,7 @@ void AccountPreviewDataServiceImpl::OnFetchCompleted(
 
 void AccountPreviewDataServiceImpl::OnRefreshTokensLoaded() {
   if (deferred_refresh_pending_) {
-    RefreshAllAccountPreviewData();
+    EnsureAllAccountsFetched();
   }
 }
 
@@ -143,8 +143,12 @@ void AccountPreviewDataServiceImpl::OnIdentityManagerShutdown(
 }
 
 void AccountPreviewDataServiceImpl::RefreshAllAccountPreviewData() {
-  CHECK(identity_manager_);
+  cached_data_.clear();
+  EnsureAllAccountsFetched();
+}
 
+void AccountPreviewDataServiceImpl::EnsureAllAccountsFetched() {
+  CHECK(identity_manager_);
   if (!identity_manager_->AreRefreshTokensLoaded()) {
     deferred_refresh_pending_ = true;
     return;
@@ -153,7 +157,9 @@ void AccountPreviewDataServiceImpl::RefreshAllAccountPreviewData() {
   deferred_refresh_pending_ = false;
   for (const auto& account :
        identity_manager_->GetAccountsWithRefreshTokens()) {
-    FetchAccountPreviewData(account.gaia);
+    if (!cached_data_.contains(account.gaia)) {
+      FetchAccountPreviewData(account.gaia);
+    }
   }
 }
 
