8a1d60fff8b6c2193c62a5a05ed7318cf1abbdee
Lei Zhang <thestig@chromium.org>
2026-07-10 12:29:18 -0700

    Fix UNSAFE_TODO() in base/test/test_discardable_memory_allocator.cc
    Replace memset() with std::ranges::fill(). Then adjust includes and
    clean up an unused one.
    
    TAG=agy
    CONV=df4dc1b1-36fc-4eb2-b9d7-ce283b8847cb
    
    Change-Id: Ib24f983e262a00aee279b86ec20f923937baf7cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076118
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660456}

--- base/test/test_discardable_memory_allocator.cc
+++ base/test/test_discardable_memory_allocator.cc
@@ -5,14 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/test_discardable_memory_allocator.h"
 
+#include <algorithm>
 #include <cstdint>
-#include <cstring>
+#include <memory>
 
 #include "base/check.h"
-#include "base/compiler_specific.h"
 #include "base/containers/heap_array.h"
 #include "base/memory/discardable_memory.h"
-#include "base/memory/ptr_util.h"
 
 namespace base {
 namespace {
@@ -34,7 +33,7 @@ class DiscardableMemoryImpl : public DiscardableMemory {
     is_locked_ = false;
     // Force eviction to catch clients not correctly checking the return value
     // of Lock().
-    UNSAFE_TODO(memset(data_.data(), 0, data_.size()));
+    std::ranges::fill(data_, 0u);
   }
 
   void* data() const override {
