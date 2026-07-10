e0fa78dc267630af690088a38ccb70292efab7af
Michael Tatarski <mtatarski@google.com>
2026-07-10 08:54:49 -0700

    [STTS] Dedupe and standardize menu phrasing to "Send to your device"
    This CL updates the Android menu string resource
    (IDS_MENU_SEND_TO_DEVICES in android_chrome_strings.grd) from "Send to
    devices" to singular "Send to your device" so it aligns with the
    phrasing used on other platforms and in tab context menus.
    
    In addition, it consolidates Android sharing hub and toolbar long-press
    callers to uniformly use IDS_MENU_SEND_TO_DEVICES
    (R.string.menu_send_to_devices) and removes the duplicate/obsolete
    IDS_SHARING_SEND_TAB_TO_SELF string resource.
    
    Example for updated UIs:
    
    -https://screenshot.googleplex.com/8WHbNMKzpRfxSCv.png
    -https://screenshot.googleplex.com/3SVbTDLrwyPcTES.png
    
    Bug: 530085729
    Change-Id: Ib7d8d4a720703ea17be91f6d943c8869f63c18d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8069780
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660311}

--- chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/android_share_sheet/AndroidShareSheetControllerUnitTest.java
+++ chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/android_share_sheet/AndroidShareSheetControllerUnitTest.java
@@ -230,7 +230,7 @@ public class AndroidShareSheetControllerUnitTest {
             assertCustomActions(
                     intent,
                     R.string.sharing_long_screenshot,
-                    R.string.sharing_send_tab_to_self,
+                    R.string.menu_send_to_devices,
                     R.string.qr_code_share_icon_label);
         } else {
             assertCustomActions(
@@ -238,7 +238,7 @@ public class AndroidShareSheetControllerUnitTest {
                     R.string.sharing_tab_group,
                     R.string.sharing_long_screenshot,
                     R.string.print_share_activity_title,
-                    R.string.sharing_send_tab_to_self,
+                    R.string.menu_send_to_devices,
                     R.string.qr_code_share_icon_label);
         }
     }
@@ -379,7 +379,7 @@ public class AndroidShareSheetControllerUnitTest {
         assertCustomActions(
                 intent,
                 R.string.sharing_copy_image_with_link,
-                R.string.sharing_send_tab_to_self,
+                R.string.menu_send_to_devices,
                 R.string.qr_code_share_icon_label);
     }
 
@@ -404,7 +404,7 @@ public class AndroidShareSheetControllerUnitTest {
         assertCustomActions(
                 intent,
                 R.string.sharing_copy_image_with_link,
-                R.string.sharing_send_tab_to_self,
+                R.string.menu_send_to_devices,
                 R.string.qr_code_share_icon_label);
 
         chooseCustomAction(
@@ -574,7 +574,7 @@ public class AndroidShareSheetControllerUnitTest {
         assertCustomActions(
                 chooserIntent,
                 R.string.sharing_copy_highlight_without_link,
-                R.string.sharing_send_tab_to_self,
+                R.string.menu_send_to_devices,
                 R.string.qr_code_share_icon_label);
 
         // Toggle the modify action again, link is removed from text.
@@ -656,7 +656,7 @@ public class AndroidShareSheetControllerUnitTest {
         assertCustomActions(
                 intent,
                 R.string.sharing_copy_image_with_link,
-                R.string.sharing_send_tab_to_self,
+                R.string.menu_send_to_devices,
                 R.string.qr_code_share_icon_label);
         chooseCustomAction(intent, R.string.qr_code_share_icon_label, ShareCustomAction.QR_CODE);
 
@@ -686,7 +686,7 @@ public class AndroidShareSheetControllerUnitTest {
                 intent,
                 R.string.sharing_copy_image,
                 R.string.sharing_copy_image_with_link,
-                R.string.sharing_send_tab_to_self,
+                R.string.menu_send_to_devices,
                 R.string.qr_code_share_icon_label);
 
         Intent shareIntent = intent.getParcelableExtra(Intent.EXTRA_INTENT);
@@ -717,7 +717,7 @@ public class AndroidShareSheetControllerUnitTest {
                         .build();
         mController.showShareSheet(params, chromeShareExtras, 1L);
         Intent intent = Shadows.shadowOf((Activity) mActivity).peekNextStartedActivity();
-        assertCustomActions(intent, R.string.sharing_copy_image, R.string.sharing_send_tab_to_self);
+        assertCustomActions(intent, R.string.sharing_copy_image, R.string.menu_send_to_devices);
 
         Intent shareIntent = intent.getParcelableExtra(Intent.EXTRA_INTENT);
         Assert.assertTrue(
@@ -744,7 +744,7 @@ public class AndroidShareSheetControllerUnitTest {
             assertCustomActions(
                     intent,
                     R.string.sharing_long_screenshot,
-                    R.string.sharing_send_tab_to_self,
+                    R.string.menu_send_to_devices,
                     R.string.qr_code_share_icon_label);
         } else {
             assertCustomActions(
@@ -752,7 +752,7 @@ public class AndroidShareSheetControllerUnitTest {
                     R.string.sharing_tab_group,
                     R.string.sharing_long_screenshot,
                     R.string.print_share_activity_title,
-                    R.string.sharing_send_tab_to_self,
+                    R.string.menu_send_to_devices,
                     R.string.qr_code_share_icon_label);
         }
         chooseCustomAction(
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarLongPressMenuHandler.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarLongPressMenuHandler.java
@@ -263,7 +263,7 @@ public class ToolbarLongPressMenuHandler implements ConfigurationChangedObserver
         }
         itemList.add(
                 new ListItemBuilder()
-                        .withTitleRes(R.string.sharing_send_tab_to_self)
+                        .withTitleRes(R.string.menu_send_to_devices)
                         .withMenuId(MenuItemType.SEND_TAB_TO_SELF)
                         .build());
     }
