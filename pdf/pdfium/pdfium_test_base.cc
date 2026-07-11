4bcc8f65d8c92e779095d95725426e12cc998fb6
Aryan <aryankrishnan4b@gmail.com>
2026-07-10 18:04:55 -0700

    Update //pdf to use the Brotli feature flag
    This CL enables BrotliDecode based on the state of the pdf-enable-brotli
    feature flag by editing the library configuration options in
    pdfium_engine.cc and pdfium_test_base.cc.
    
    Specifically, this CL updates the configuration version to 6 and adds
    additional configuration to enable brotli based on the state of the
    base::Feature.
    
    In the process, this CL also sets the default build override to true due
    to the presence of the flag as a runtime check for whether Brotli is
    enabled.
    
    Bug: 475855993
    Change-Id: Ib90fe6032fc8d872d44f8deab9bc6dc5168402d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8055324
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660629}

--- pdf/pdfium/pdfium_test_base.cc
+++ pdf/pdfium/pdfium_test_base.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
 #include "pdf/loader/url_loader.h"
+#include "pdf/pdf_features.h"
 #include "pdf/pdfium/pdfium_engine.h"
 #include "pdf/pdfium/pdfium_form_filler.h"
 #include "pdf/test/test_client.h"
@@ -153,13 +154,17 @@ void PDFiumTestBase::InitializePDFiumSDK() {
 #endif
 
   FPDF_LIBRARY_CONFIG config;
-  config.version = 4;
+  config.version = 6;
   config.m_pUserFontPaths = font_paths_.data();
   config.m_pIsolate = nullptr;
   config.m_v8EmbedderSlot = 0;
   config.m_pPlatform = nullptr;
   config.m_RendererType =
       GetParam() ? FPDF_RENDERERTYPE_SKIA : FPDF_RENDERERTYPE_AGG;
+  config.m_FontLibraryType = FPDF_FONTBACKENDTYPE_FREETYPE;
+  config.m_BrotliEnabled =
+      base::FeatureList::IsEnabled(features::kPdfBrotliDecode);
+
   FPDF_InitLibraryWithConfig(&config);
 }
 
