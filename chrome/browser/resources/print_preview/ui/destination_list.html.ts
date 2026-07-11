582d8d8754dea20ed063e1c53982d1293583b5f5
rbpotter <rbpotter@chromium.org>
2026-07-10 17:31:49 -0700

    WebUI: Add TemplatizedDomNodes to specify types of templated elements
    Support specifying the types of templated elements using a
    TemplatizedDomNodes interface that maps DOM ids to specific types.
    
    Also updating Print Preview, which uses cr-infinite-list (a templated
    element class) as an example.
    
    Bug: 516847140
    Change-Id: I564d7c670a8494ff6778d3e916c0b2c111c0392c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072480
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660617}

--- chrome/browser/resources/print_preview/ui/destination_list.html.ts
+++ chrome/browser/resources/print_preview/ui/destination_list.html.ts
@@ -3,11 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type {CrInfiniteListElement} from 'chrome://resources/cr_elements/cr_infinite_list/cr_infinite_list.js';
 import {html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import type {Destination} from '../data/destination.js';
 
 import type {PrintPreviewDestinationListElement} from './destination_list.js';
+
+export interface TemplatizedDomNodes {
+  'list': CrInfiniteListElement<Destination>;
+}
 
 export function getHtml(this: PrintPreviewDestinationListElement) {
   // clang-format off
---
+++ ui/webui/resources/tools/tests/eslint_ts/with_webui_plugin_lit_element_bindings_templatized_dom_nodes_violations.html.ts
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {GenericListElement} from './with_webui_plugin_lit_element_bindings_templatized_dom_nodes_child.js';
+import type {TemplatizedDomNodesViolationsElement} from './with_webui_plugin_lit_element_bindings_templatized_dom_nodes_violations.js';
+
+export interface TemplatizedDomNodes {
+  'number-list': GenericListElement<number>;
+  'string-list': GenericListElement<string>;
+}
+
+export function getHtml(this: TemplatizedDomNodesViolationsElement) {
+  // clang-format off
+  return html`
+<generic-list id="number-list" .items="${this.numbers}"
+    .selectedItem="${this.selectedNumber}">
+</generic-list>
+<generic-list id="string-list" .items="${this.numbers}"
+    .selectedItem="${this.selectedNumber}">
+</generic-list>
+`;
+  // clang-format on
+}
