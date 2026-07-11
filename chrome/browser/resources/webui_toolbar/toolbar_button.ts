0de35414b50525d5c2f41fd8f008a78b4b769096
Dana Fried <dfried@chromium.org>
2026-07-10 17:59:50 -0700

    [User Education|WebUI] Dedupe help bubble mixin logic
    The vast majority of the code between HelpBubbleMixin and
    HelpBubbleMixinLit was exactly the same. We want to make some
    improvements and simplifications to this code, but not in two identical
    places simultaneously.
    
    This CL breaks out the mixin interface and implementation into two
    separate files, and the mixin implementations simply reference those.
    No behaviors should change as a result.
    
    Follow-up items:
     - Simplify the interface (especially for testing)
     - Offload more bookkeeping to TrackedElementManager
     - Support secondary identifiers for compatibility with new Webium/
       WebUI IPH upgrades to element tracker.
    
    Bug: 530588544
    Change-Id: I5250d52bd455258b9566650218e74b594ea1b08e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8073563
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1660627}

--- chrome/browser/resources/webui_toolbar/toolbar_button.ts
+++ chrome/browser/resources/webui_toolbar/toolbar_button.ts
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {loadTimeData} from '//resources/js/load_time_data.js';
 import type {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import {MenuSourceType} from '//resources/mojo/ui/base/mojom/menu_source_type.mojom-webui.js';
+import type {HelpBubbleMixinInterface} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin_interface.js';
 import {HelpBubbleMixinLit} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin_lit.js';
-import type {HelpBubbleMixinLitInterface} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin_lit.js';
 import {isMac} from 'chrome://resources/js/platform.js';
 
 import {EventDispositionFlag} from './browser_proxy.js';
@@ -36,7 +36,7 @@ export interface HelpBubbleAnchor {
  */
 export const HelpBubbleAnchorMixin =
     <T extends Constructor<CrLitElement>>(superClass: T): T&
-    Constructor<HelpBubbleMixinLitInterface>&Constructor<HelpBubbleAnchor> => {
+    Constructor<HelpBubbleMixinInterface>&Constructor<HelpBubbleAnchor> => {
       class HelpBubbleAnchorMixin extends HelpBubbleMixinLit
       (superClass) {
         static get properties() {
@@ -60,8 +60,7 @@ export const HelpBubbleAnchorMixin =
         }
       }
 
-      return HelpBubbleAnchorMixin as T &
-          Constructor<HelpBubbleMixinLitInterface>&
+      return HelpBubbleAnchorMixin as T & Constructor<HelpBubbleMixinInterface>&
           Constructor<HelpBubbleAnchor>;
     };
 
