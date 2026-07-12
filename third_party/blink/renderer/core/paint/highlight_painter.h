69ce9e16564535840f8e262a237ba6ebc83cbf8f
Steve Kobes <skobes@chromium.org>
2026-07-12 14:33:44 -0700

    Use unrounded rect for selection highlight
    The physical_box in TextFragmentPainter::Paint has a rounded y-offset.
    Storing this offset in SelectionPaintState led to a 1px gap or overlap
    between selection highlights of adjacent lines (see bug comment #4).
    
    To fix it, store the unrounded offset in SelectionPaintState. But keep
    the rounded physical_box for other uses in TextFragmentPainter, such as
    drawing the actual text and decorations.
    
    Note that we have two ways into PaintHighlightBackground for selection,
    a fast path (Case::kFastSelection) and the full PaintHighlightOverlays
    algorithm encountering layer type kSelection. Handle both.
    
    This change does NOT affect custom highlights (see crbug.com/40281215)
    or inline backgrounds like <span style="background:red">. Unlike
    selection, neither of these fills the line-height.
    
    Bug: 526240271
    Change-Id: I1e6769b0c0fe2dc1cbba9a45b2c4bdedc7087955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071944
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660887}

--- third_party/blink/renderer/core/paint/highlight_painter.h
+++ third_party/blink/renderer/core/paint/highlight_painter.h
@@ -86,6 +86,8 @@ class CORE_EXPORT HighlightPainter {
     // ops done while rotated need coordinates in this rotated space, but ops
     // done outside of these rotations need the original physical rect.
     const PhysicalRect& PhysicalSelectionRect();
+    PhysicalRect ComputePhysicalSelectionRect(unsigned start,
+                                              unsigned end) const;
     const LineRelativeRect& LineRelativeSelectionRect();
 
     void PaintSelectionBackground(
@@ -282,7 +284,6 @@ class CORE_EXPORT HighlightPainter {
   TextDecorationPainter& decoration_painter_;
   const PaintInfo& paint_info_;
   const InlineCursor& cursor_;
-  InlineCursor root_inline_cursor_;
   const FragmentItem& fragment_item_;
   const PhysicalOffset& box_origin_;
   const ComputedStyle& originating_style_;
