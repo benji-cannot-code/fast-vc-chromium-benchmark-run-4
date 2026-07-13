0b191b2ce8c8fd968f8316a0951807475edc0d72
Simon Zünd <szuend@chromium.org>
2026-07-13 02:25:47 -0700

    Update ResourceTreeModel.resourceForURL calls in DevTools web tests
    Pass TargetManager as the first argument to
    ResourceTreeModel.resourceForURL in blink web tests.
    
    TAG=agy
    CONV=e2756dee-569f-4f17-bc0f-85e241332a11
    
    Bug: 493763857
    Change-Id: I762caaee1ffaf99d9108873c650f7320d0ec1736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8084821
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661001}

--- third_party/blink/web_tests/http/tests/devtools/resource-tree/resource-metadata.js
+++ third_party/blink/web_tests/http/tests/devtools/resource-tree/resource-metadata.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {TestRunner} from 'test_runner';
 import * as SDK from 'devtools/core/sdk/sdk.js';
+import * as Main from 'devtools/entrypoints/main/main.js';
 
 
 (async function() {
@@ -21,7 +22,7 @@ import * as SDK from 'devtools/core/sdk/sdk.js';
     })();
   `);
 
-  var resource = SDK.ResourceTreeModel.ResourceTreeModel.resourceForURL(url);
+  var resource = SDK.ResourceTreeModel.ResourceTreeModel.resourceForURL(Main.MainImpl.MainImpl.universeForTest.targetManager, url);
   if (!resource) {
     TestRunner.addResult('ERROR: Failed to find resource.');
     TestRunner.completeTest();
