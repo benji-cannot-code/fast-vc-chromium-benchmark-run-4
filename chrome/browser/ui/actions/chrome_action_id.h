4636e453910674e613da48b199613e368cf50400
dljames <dljames@chromium.org>
2026-07-13 15:28:57 -0700

    [BAM] Migrate IDC_SHOW_READING_MODE_KEYBOARD to the Actions framework
    Maps IDC_SHOW_READING_MODE_KEYBOARD to kActionShowReadingModeKeyboard in
    SIDE_PANEL_ACTION_IDS and registers the ActionItem inside
    BrowserActions::InitializeSidePanelActions(), delegating to
    ReadAnythingEntryPointController::ToggleUI(bwi, kKeyboardShortcut).
    
    TAG=agy
    CONV=99f2f692-d7a4-43b9-93f8-2a04962ec746
    
    Bug: 525118424
    Change-Id: Id12845a3df824e49cbd53cacc9576faebd32cef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8077518
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661420}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -484,6 +484,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E(kActionSidePanelShowLens) \
   E(kActionSidePanelShowLensOverlayResults, IDC_CONTENT_CONTEXT_LENS_OVERLAY) \
   E(kActionSidePanelShowReadAnything, IDC_SHOW_READING_MODE_SIDE_PANEL) \
+  E(kActionShowReadingModeKeyboard, IDC_SHOW_READING_MODE_KEYBOARD) \
   E(kActionSidePanelShowReadingList, IDC_READING_LIST_MENU_SHOW_UI) \
   E(kActionSidePanelShowSearchCompanion) \
   E(kActionSidePanelShowShoppingInsights) \
