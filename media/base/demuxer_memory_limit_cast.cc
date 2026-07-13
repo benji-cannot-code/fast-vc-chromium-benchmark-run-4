51d0fb22353202c1f6b4570e86aec077d57f4aed
Joe Mason <joenotcharles@google.com>
2026-07-13 14:24:12 -0700

    Reland "Use ByteSize in //media/base"
    This is a reland of commit 8ccaab86e42ddc2b9b5dd853b72d913097ba5569
    
    Reason: fixed uses of ByteCount that weren't built on CQ
    
    Original change's description:
    > Use ByteSize in //media/base
    >
    > ByteCount is deprecated.
    >
    > Bug: 448661443
    > Change-Id: Iae9d4d97953abfecc8b600ee36bd8ab3b1d1d002
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070879
    > Auto-Submit: Joe Mason <joenotcharles@google.com>
    > Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1659900}
    
    Bug: 448661443
    Cq-Include-Trybots: luci.chromium.try:linux-cast-arm64-rel,linux-cast-arm-rel,linux-cast-x64-dbg,linux-cast-x64-rel,android-cast-arm-dbg,android-cast-arm-rel,chromeos-arm-generic-dbg,chromeos-arm-generic-rel,fuchsia-x64-cast-receiver-dbg,fuchsia-x64-cast-receiver-rel
    Change-Id: I7392a29ad484c84334816244d813076ea98c623d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8075319
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661381}

--- media/base/demuxer_memory_limit_cast.cc
+++ media/base/demuxer_memory_limit_cast.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-base::ByteCount GetDemuxerStreamAudioMemoryLimit(
+base::ByteSize GetDemuxerStreamAudioMemoryLimit(
     const AudioDecoderConfig* audio_config) {
   if (!audio_config) {
     return internal::kDemuxerStreamAudioMemoryLimitLow;
@@ -35,7 +35,7 @@ base::ByteCount GetDemuxerStreamAudioMemoryLimit(
   }
 }
 
-base::ByteCount GetDemuxerStreamVideoMemoryLimit(
+base::ByteSize GetDemuxerStreamVideoMemoryLimit(
     DemuxerType demuxer_type,
     const VideoDecoderConfig* video_config) {
   switch (demuxer_type) {
@@ -59,7 +59,7 @@ base::ByteCount GetDemuxerStreamVideoMemoryLimit(
   }
 }
 
-base::ByteCount GetDemuxerMemoryLimit(DemuxerType demuxer_type) {
+base::ByteSize GetDemuxerMemoryLimit(DemuxerType demuxer_type) {
   return GetDemuxerStreamAudioMemoryLimit(nullptr) +
          GetDemuxerStreamVideoMemoryLimit(demuxer_type, nullptr);
 }
