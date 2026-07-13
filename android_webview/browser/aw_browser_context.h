700dc00292fad7145512ed964d20325043ebffdd
Traian Dobrescu <traiand@google.com>
2026-07-13 03:56:46 -0700

    Cross Origin Isolation Allow List for Webview Browser Context
    - Added an origin Allowlist with AwBrowserContext
    - Added a setter and getter for the allowlist
    
    Bug: b:452920345
    Change-Id: Ic6da03c6f4cc72571d2498b6df641bdcc71b29db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8052535
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Traian Dobrescu <traiand@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661041}

--- android_webview/browser/aw_browser_context.h
+++ android_webview/browser/aw_browser_context.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "components/keyed_service/core/simple_factory_key.h"
+#include "components/origin_matcher/origin_matcher.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/visitedlink/browser/visitedlink_delegate.h"
 #include "content/public/browser/browser_context.h"
@@ -132,10 +133,10 @@ class AwBrowserContext : public content::BrowserContext,
       const base::android::JavaRef<jobject>& io_thread_client);
 
   int AllowedPrerenderingCount() const;
-  void SetAllowedPrerenderingCount(JNIEnv* const env, int allowed_count);
-  void ClearAllowedPrerenderingCount(JNIEnv* const env);
+  void SetAllowedPrerenderingCount(JNIEnv* env, int allowed_count);
+  void ClearAllowedPrerenderingCount(JNIEnv* env);
 
-  void WarmUpSpareRenderer(JNIEnv* const env);
+  void WarmUpSpareRenderer(JNIEnv* env);
 
   // content::BrowserContext implementation.
   base::FilePath GetPath() const override;
@@ -266,6 +267,11 @@ class AwBrowserContext : public content::BrowserContext,
   }
   AwPrefetchManager& GetPrefetchManager() { return *prefetch_manager_.get(); }
 
+  std::vector<std::string> SetCrossOriginIsolatedAllowList(
+      JNIEnv* env,
+      const std::vector<std::string>& origin_patterns);
+  std::vector<std::string> GetCrossOriginIsolatedAllowList(JNIEnv* env);
+
  private:
   friend class AwBrowserContextIoThreadHandle;
   friend class AwBrowserContextTest;
@@ -342,6 +348,9 @@ class AwBrowserContext : public content::BrowserContext,
   bool enable_stale_dns_ = false;
 
   std::vector<scoped_refptr<AwOriginMatchedHeader>> origin_matched_headers_;
+
+  std::unique_ptr<origin_matcher::OriginMatcher>
+      cross_origin_allow_list_matcher_;
 
   base::WeakPtrFactory<AwBrowserContext> weak_method_factory_{this};
 };
