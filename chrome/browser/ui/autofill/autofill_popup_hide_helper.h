50547fa61658b17f60ec4f90316926624e78acc2
Christoph Schwering <schwering@google.com>
2026-07-13 06:16:29 -0700

    [Autofill] Hide popup in RenderFrameHostStateChanged()
    The CL replaces RenderFrameDeleted() and DidFinishNavigation() with
    RenderFrameHostStateChanged()... but keeps RenderFrameDeleted() again
    because of a bug in RenderFrameHostStateChanged().
    
    The intention is that RenderFrameHostStateChanged() is also
    notified when a navigation in a parent frame makes the descendant
    frame go into bfcache.
    
    Bug: 514038302
    Change-Id: I3fae9459774c66233a6236e473ab77c46a62225a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071266
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661091}

--- chrome/browser/ui/autofill/autofill_popup_hide_helper.h
+++ chrome/browser/ui/autofill/autofill_popup_hide_helper.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_ANDROID)
 
 namespace content {
-class NavigationHandle;
 class RenderFrameHost;
 class RenderWidgetHost;
 enum class Visibility;
@@ -71,9 +70,11 @@ class AutofillPopupHideHelper : public content::WebContentsObserver,
       content::RenderWidgetHost* render_widget_host) override;
   void PrimaryMainFrameWasResized(bool width_changed) override;
   void OnVisibilityChanged(content::Visibility visibility) override;
+  void RenderFrameHostStateChanged(
+      content::RenderFrameHost* render_frame_host,
+      content::RenderFrameHost::LifecycleState old_state,
+      content::RenderFrameHost::LifecycleState new_state) override;
   void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
 
 #if !BUILDFLAG(IS_ANDROID)
   // ZoomObserver:
