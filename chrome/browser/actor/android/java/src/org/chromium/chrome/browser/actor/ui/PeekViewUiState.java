72baf3d88262170212f473e694b437281bc7d3f3
Fiaz Muhammad <mfiaz@google.com>
2026-07-13 15:54:51 -0700

    [ContextualTasks] Fix hardcoded icon for peekview in TabBottomSheet
    - Adds PEEK_ICON_ID to TabBottomSheetPeekProperties enabling
            features to customize the peek view icon.
    - Updates TabBottomSheetPeekView and
            TabBottomSheetPeekViewBinder to bind PEEK_ICON_ID.
    - Updates ContextualTaskBottomSheetComponentProvider to return
            null for createPeekViewManager.
    - Updates ActorControls PeekViewUiState to set its drawable ID
            and bind it in ActorControlMediator.
    - Adds unit tests in TabBottomSheetPeekViewBinderTest.
    
    Bug: 508265401, 531793607
    Change-Id: Ibbd0f638f1460c87e72dd4a3d5e4a1b3f8e67c54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8048460
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661438}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ui/PeekViewUiState.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ui/PeekViewUiState.java
@@ -38,12 +38,14 @@ public class PeekViewUiState {
     @Retention(RetentionPolicy.SOURCE)
     public @interface Visibility {}
 
-    private static final @DrawableRes int ICON_ACTING = R.drawable.ic_pause_white_24dp;
-    private static final @DrawableRes int ICON_PAUSED = R.drawable.ic_play_arrow_white_24dp;
-    private static final @DrawableRes int ICON_WAITING = Resources.ID_NULL;
-    private static final @DrawableRes int ICON_DEFAULT = Resources.ID_NULL;
+    private static final @DrawableRes int PEEK_ICON = R.drawable.ic_spark_blue_16dp;
+    private static final @DrawableRes int BUTTON_ICON_ACTING = R.drawable.ic_pause_white_24dp;
+    private static final @DrawableRes int BUTTON_ICON_PAUSED = R.drawable.ic_play_arrow_white_24dp;
+    private static final @DrawableRes int BUTTON_ICON_WAITING = Resources.ID_NULL;
+    private static final @DrawableRes int BUTTON_ICON_DEFAULT = Resources.ID_NULL;
 
     public final @StateType int type;
+    public final @DrawableRes int peekIconResId;
 
     // The status of the actor task based on its current state (acting, paused, or waiting)
     public final @StringRes int descriptionResId;
@@ -61,6 +63,7 @@ public class PeekViewUiState {
 
     private PeekViewUiState(
             @StateType int type,
+            @DrawableRes int peekIconResId,
             @StringRes int descriptionResId,
             @DrawableRes int buttonIconResId,
             @ColorRes int buttonBackgroundResId,
@@ -72,6 +75,7 @@ public class PeekViewUiState {
             @StyleRes int titleTextAppearanceResId,
             @StringRes int buttonContentDescriptionResId) {
         this.type = type;
+        this.peekIconResId = peekIconResId;
         this.descriptionResId = descriptionResId;
         this.buttonIconResId = buttonIconResId;
         this.buttonBackgroundResId = buttonBackgroundResId;
@@ -122,6 +126,7 @@ public class PeekViewUiState {
         }
         PeekViewUiState that = (PeekViewUiState) o;
         return type == that.type
+                && peekIconResId == that.peekIconResId
                 && descriptionResId == that.descriptionResId
                 && buttonIconResId == that.buttonIconResId
                 && buttonBackgroundResId == that.buttonBackgroundResId
@@ -138,6 +143,7 @@ public class PeekViewUiState {
     public int hashCode() {
         return Objects.hash(
                 type,
+                peekIconResId,
                 descriptionResId,
                 buttonIconResId,
                 buttonBackgroundResId,
@@ -154,8 +160,9 @@ public class PeekViewUiState {
     public static final PeekViewUiState ACTING =
             new PeekViewUiState(
                     /* type= */ StateType.ACTING,
+                    /* peekIconResId= */ PEEK_ICON,
                     /* descriptionResId= */ R.string.peek_state_acting,
-                    /* buttonIconResId= */ ICON_ACTING,
+                    /* buttonIconResId= */ BUTTON_ICON_ACTING,
                     /* buttonBackgroundResId= */ R.color.actor_control_icon_button_background_color,
                     /* buttonHorizontalPaddingResId= */ Resources.ID_NULL,
                     /* iconTintResId= */ R.color.default_icon_color_tint_list,
@@ -169,8 +176,9 @@ public class PeekViewUiState {
     public static final PeekViewUiState PAUSED =
             new PeekViewUiState(
                     /* type= */ StateType.PAUSED,
+                    /* peekIconResId= */ PEEK_ICON,
                     /* descriptionResId= */ R.string.peek_state_paused,
-                    /* buttonIconResId= */ ICON_PAUSED,
+                    /* buttonIconResId= */ BUTTON_ICON_PAUSED,
                     /* buttonBackgroundResId= */ R.color.actor_control_icon_button_background_color,
                     /* buttonHorizontalPaddingResId= */ Resources.ID_NULL,
                     /* iconTintResId= */ R.color.default_icon_color_tint_list,
@@ -184,8 +192,9 @@ public class PeekViewUiState {
     public static final PeekViewUiState WAITING =
             new PeekViewUiState(
                     /* type= */ StateType.WAITING,
+                    /* peekIconResId= */ PEEK_ICON,
                     /* descriptionResId= */ R.string.peek_state_waiting,
-                    /* buttonIconResId= */ ICON_WAITING,
+                    /* buttonIconResId= */ BUTTON_ICON_WAITING,
                     /* buttonBackgroundResId= */ R.color.actor_view_button_background_color,
                     /* buttonHorizontalPaddingResId= */ R.dimen
                             .actor_control_view_button_horizontal_padding,
@@ -199,8 +208,9 @@ public class PeekViewUiState {
     public static final PeekViewUiState DEFAULT =
             new PeekViewUiState(
                     /* type= */ StateType.DEFAULT,
+                    /* peekIconResId= */ PEEK_ICON,
                     /* descriptionResId= */ Resources.ID_NULL,
-                    /* buttonIconResId= */ ICON_DEFAULT,
+                    /* buttonIconResId= */ BUTTON_ICON_DEFAULT,
                     /* buttonBackgroundResId= */ Resources.ID_NULL,
                     /* buttonHorizontalPaddingResId= */ Resources.ID_NULL,
                     /* iconTintResId= */ Resources.ID_NULL,
