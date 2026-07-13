7b0048e323cee4fd18a214b6b0531e7525fd8f2d
Yoav Weiss <yoavweiss@chromium.org>
2026-07-13 01:43:55 -0700

    EarlyHints preload - support the integrity attribute
    Bug: 532843494
    Change-Id: I87ed0271dbe1d9fb108729e7f26b7048f8f243e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8073744
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660992}

---
+++ third_party/blink/web_tests/external/wpt/loading/early-hints/preload-integrity.h2.window.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/utils.js
+// META: script=resources/early-hints-helpers.sub.js
+
+test(() => {
+    // `empty.js` is the 15-byte "// Empty script" resource. These are its
+    // stable subresource integrity digests.
+    const SHA256 = "sha256-Ob5toHqY7M191fMXYeLJWkPa3s6KKUJG56w6yx2Va9g=";
+    const SHA384 =
+        "sha384-l9bPabkzR+5OF8EAN7erMPo/2NdApkGLXblgglaCaoG0NipjZ/+MitE1db4KmHTk";
+    const preloads = [
+        {
+            "url": "empty.js?" + token(),
+            "as_attr": "script",
+            "integrity_attr": SHA256,
+        },
+        {
+            "url": "empty.js?" + token(),
+            "as_attr": "script",
+            "integrity_attr": SHA384,
+        },
+    ];
+    navigateToTestWithEarlyHints("resources/preload-integrity.html", preloads);
+});
