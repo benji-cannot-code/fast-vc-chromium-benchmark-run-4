3a587b773a52db8afadfeddc58978bccb35bf33c
Joey Arhar <jarhar@chromium.org>
2026-07-13 16:15:06 -0700

    Retarget in pseudoTarget getter
    Retargeting is needed in order to prevent leaking nodes from shadow
    roots and is part of the spec pr: https://github.com/w3c/uievents/pull/413
    
    Bug: 40639103
    Change-Id: I89237f2d31167eb48e79004dff9e7c9eb3a40338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008270
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661457}

--- third_party/blink/renderer/core/dom/events/event.cc
+++ third_party/blink/renderer/core/dom/events/event.cc
@@ -427,7 +427,7 @@ CSSPseudoElement* Event::pseudoTarget() const {
   if (!RuntimeEnabledFeatures::CSSPseudoElementInterfaceEnabled()) {
     return nullptr;
   }
-  if (!pseudo_element_target_) {
+  if (!pseudo_element_target_ || !current_target_) {
     return nullptr;
   }
 
@@ -447,7 +447,11 @@ CSSPseudoElement* Event::pseudoTarget() const {
     }
   }
 
-  return pseudo_element_target_.Get();
+  Element* originating_element = pseudo_element_target_->element();
+  if (Retarget(originating_element) == originating_element) {
+    return pseudo_element_target_.Get();
+  }
+  return nullptr;
 }
 
 }  // namespace blink
