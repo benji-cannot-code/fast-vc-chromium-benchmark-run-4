80279f73abfcdc069463e307c1d4121f95aaed52
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-07-12 21:35:18 -0700

    Revert "[soft navs] CHECK(!TimeOrigin().is_null()) for any SoftNavigationContext"
    This reverts commit 10898274ef3d9ec9364f65731634a211021761b7.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5733269195718656
    
    Sample build with failed test: https://ci.chromium.org/b/8676640291144971329
    Affected test(s):
    [://android_webview/test\:webview_cts_tests!junit:android.webkit.cts:WebViewRenderProcessClientTest#testWebViewRenderProcessClientWithExecutor](https://ci.chromium.org/ui/test/chromium/:%2F%2Fandroid_webview%2Ftest%5C:webview_cts_tests%21junit:android.webkit.cts:WebViewRenderProcessClientTest%23testWebViewRenderProcessClientWithExecutor?q=VHash%3A372157ef91cf067b)
    [://android_webview/test\:webview_cts_tests!junit:android.webkit.cts:WebViewRenderProcessClientTest#testWebViewRenderProcessClientWithoutExecutor](https://ci.chromium.org/ui/test/chromium/:%2F%2Fandroid_webview%2Ftest%5C:webview_cts_tests%21junit:android.webkit.cts:WebViewRenderProcessClientTest%23testWebViewRenderProcessClientWithoutExecutor?q=VHash%3A372157ef91cf067b)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5733269195718656&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8072864&type=BUG
    
    Original change's description:
    > [soft navs] CHECK(!TimeOrigin().is_null()) for any SoftNavigationContext
    >
    > This fixes the remaining tests that create input events that don't
    > have a timestamp by calling SetTimeStamp(base::TimeTicks::Now()), and
    > adds a CHECK statement to the constructor of SoftNavigationContext.
    >
    > Change-Id: I6b390ea920235d60e2ddf6787efacf1dcfe3f827
    > Bug: 490814752
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072864
    > Reviewed-by: Scott Haseley <shaseley@chromium.org>
    > Commit-Queue: Johannes Henkel <johannes@chromium.org>
    > Reviewed-by: Stefan Zager <szager@chromium.org>
    > Reviewed-by: Michal Mocny <mmocny@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1660393}
    >
    
    Bug: 490814752
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iabd4632a8b1540983e70f82fd511d5dbfedfbf5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8078002
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1660943}

--- content/renderer/render_view_browsertest_mac.mm
+++ content/renderer/render_view_browsertest_mac.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/time/time.h"
 #include "components/input/native_web_keyboard_event.h"
 #include "content/public/test/render_view_test.h"
 #include "content/renderer/render_frame_impl.h"
@@ -88,6 +87,10 @@ TEST_F(RenderViewTest, MacTestCmdUp) {
   const int kMaxOutputCharacters = 1024;
   std::string output;
 
+  NSEvent* arrowDownKeyDown =
+      CmdDeadKeyEvent(NSEventTypeKeyDown, kVK_DownArrow);
+  NSEvent* arrowUpKeyDown = CmdDeadKeyEvent(NSEventTypeKeyDown, kVK_UpArrow);
+
   // First test when javascript does not eat keypresses -- should scroll.
   RenderFrameImpl::FromWebFrame(web_view_->MainFrame()->ToWebLocalFrame())
       ->set_send_content_state_immediately(true);
@@ -96,10 +99,8 @@ TEST_F(RenderViewTest, MacTestCmdUp) {
   const char* kArrowDownScrollDown = "40,false,false,true,false\n9844";
   blink_widget->AddEditCommandForNextKeyEvent(
       blink::WebString::FromLatin1("moveToEndOfDocument"), blink::WebString());
-  input::NativeWebKeyboardEvent arrowDownKeyDown1(base::apple::OwnedNSEvent(
-      CmdDeadKeyEvent(NSEventTypeKeyDown, kVK_DownArrow)));
-  arrowDownKeyDown1.SetTimeStamp(base::TimeTicks::Now());
-  SendNativeKeyEvent(arrowDownKeyDown1);
+  SendNativeKeyEvent(input::NativeWebKeyboardEvent(
+      base::apple::OwnedNSEvent(arrowDownKeyDown)));
   base::RunLoop().RunUntilIdle();
   ExecuteJavaScriptForTests("scroll.textContent = window.pageYOffset");
   output = TestWebFrameContentDumper::DumpWebViewAsText(web_view_,
@@ -111,10 +112,8 @@ TEST_F(RenderViewTest, MacTestCmdUp) {
   blink_widget->AddEditCommandForNextKeyEvent(
       blink::WebString::FromLatin1("moveToBeginningOfDocument"),
       blink::WebString());
-  input::NativeWebKeyboardEvent arrowUpKeyDown1(base::apple::OwnedNSEvent(
-      CmdDeadKeyEvent(NSEventTypeKeyDown, kVK_UpArrow)));
-  arrowUpKeyDown1.SetTimeStamp(base::TimeTicks::Now());
-  SendNativeKeyEvent(arrowUpKeyDown1);
+  SendNativeKeyEvent(
+      input::NativeWebKeyboardEvent(base::apple::OwnedNSEvent(arrowUpKeyDown)));
   base::RunLoop().RunUntilIdle();
   ExecuteJavaScriptForTests("scroll.textContent = window.pageYOffset");
   output = TestWebFrameContentDumper::DumpWebViewAsText(web_view_,
@@ -130,10 +129,8 @@ TEST_F(RenderViewTest, MacTestCmdUp) {
   const char* kArrowDownNoScroll = "40,false,false,true,false\n100";
   blink_widget->AddEditCommandForNextKeyEvent(
       blink::WebString::FromLatin1("moveToEndOfDocument"), blink::WebString());
-  input::NativeWebKeyboardEvent arrowDownKeyDown2(base::apple::OwnedNSEvent(
-      CmdDeadKeyEvent(NSEventTypeKeyDown, kVK_DownArrow)));
-  arrowDownKeyDown2.SetTimeStamp(base::TimeTicks::Now());
-  SendNativeKeyEvent(arrowDownKeyDown2);
+  SendNativeKeyEvent(input::NativeWebKeyboardEvent(
+      base::apple::OwnedNSEvent(arrowDownKeyDown)));
   base::RunLoop().RunUntilIdle();
   ExecuteJavaScriptForTests("scroll.textContent = window.pageYOffset");
   output = TestWebFrameContentDumper::DumpWebViewAsText(web_view_,
@@ -145,10 +142,8 @@ TEST_F(RenderViewTest, MacTestCmdUp) {
   blink_widget->AddEditCommandForNextKeyEvent(
       blink::WebString::FromLatin1("moveToBeginningOfDocument"),
       blink::WebString());
-  input::NativeWebKeyboardEvent arrowUpKeyDown2(base::apple::OwnedNSEvent(
-      CmdDeadKeyEvent(NSEventTypeKeyDown, kVK_UpArrow)));
-  arrowUpKeyDown2.SetTimeStamp(base::TimeTicks::Now());
-  SendNativeKeyEvent(arrowUpKeyDown2);
+  SendNativeKeyEvent(
+      input::NativeWebKeyboardEvent(base::apple::OwnedNSEvent(arrowUpKeyDown)));
   base::RunLoop().RunUntilIdle();
   ExecuteJavaScriptForTests("scroll.textContent = window.pageYOffset");
   output = TestWebFrameContentDumper::DumpWebViewAsText(web_view_,
