46293f029a27715bbf5c41b927f11154b9d2e686
Brandon Fong <bjfong@google.com>
2026-07-13 12:19:08 -0700

    [search] Add a show and hide slide animation for the tab search panel
    Add a show and hide slide animation when the tab search panel is first
    triggered and then hidden similar to vertical tabs.
    
    Bug: 527090329
    Change-Id: I72290487e3ac056139a1da868ac2456512faed1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8077139
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661295}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSearchOverlayCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSearchOverlayCoordinator.java
@@ -177,6 +177,7 @@ public class TabSearchOverlayCoordinator {
         if (mSearchUiCoordinator == null) {
             mSearchUiCoordinator = new SearchUiCoordinator(mActivity, mSearchBoxDataProvider);
             mSearchUiCoordinator.getLocationBarUiOverrides().setVoiceEntrypointAllowed(false);
+            mSearchUiCoordinator.getLocationBarUiOverrides().setLensEntrypointAllowed(false);
             mSearchUiCoordinator.getLocationBarUiOverrides().setEmbedderControlledHint(true);
         }
 
@@ -239,7 +240,7 @@ public class TabSearchOverlayCoordinator {
         setSearchUiElements();
 
         TabSearchOverlayViewBinder.ViewHolder viewHolder =
-                new TabSearchOverlayViewBinder.ViewHolder(panelContainer, scrim);
+                new TabSearchOverlayViewBinder.ViewHolder(panelContainer, scrim, panelView);
         mChangeProcessor =
                 PropertyModelChangeProcessor.create(
                         mModel, viewHolder, TabSearchOverlayViewBinder::bind);
