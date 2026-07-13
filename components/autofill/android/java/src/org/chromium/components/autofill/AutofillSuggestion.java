12344d568ef198fbdd3f65c5db265760bf76c68c
Atalyk Akash <atalyk@google.com>
2026-07-13 05:44:25 -0700

    [AtMemory][Android] Add IsAcceptable to AutofillSuggestion
    IsAcceptable field will be used to check if the suggestion
    can be accepted or not.
    
    Bug: 513149061
    Change-Id: I0a9fda83c4c0ae3fca3308c7790be8b5ca2e2fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8084467
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Josef Raska <josefraska@google.com>
    Commit-Queue: Atalyk Akash <atalyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661080}

--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillSuggestion.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillSuggestion.java
@@ -35,6 +35,7 @@ public class AutofillSuggestion {
     private final @Nullable GURL mCustomIconUrl;
     private final @Nullable Payload mPayload;
     private final List<AutofillSuggestion> mChildren;
+    private final boolean mIsAcceptable;
 
     public sealed interface Payload
             permits AutofillAiPayload, AutofillProfilePayload, PaymentsPayload {}
@@ -59,6 +60,7 @@ public class AutofillSuggestion {
      *     (e.g., if it requires a fetch from the server).
      * @param payload Additional data passed with the suggestion.
      * @param children The list of children suggestions.
+     * @param isAcceptable Whether the suggestion is acceptable.
      */
     @VisibleForTesting
     public AutofillSuggestion(
@@ -76,7 +78,8 @@ public class AutofillSuggestion {
             @Nullable String iphDescriptionText,
             @Nullable GURL customIconUrl,
             @Nullable Payload payload,
-            List<AutofillSuggestion> children) {
+            List<AutofillSuggestion> children,
+            boolean isAcceptable) {
         mLabel = label;
         mSecondaryLabel = secondaryLabel;
         mSublabel = sublabel;
@@ -92,6 +95,7 @@ public class AutofillSuggestion {
         mCustomIconUrl = customIconUrl;
         mPayload = payload;
         mChildren = children;
+        mIsAcceptable = isAcceptable;
     }
 
     public @Nullable String getLabel() {
@@ -185,6 +189,10 @@ public class AutofillSuggestion {
         return mChildren;
     }
 
+    public boolean isAcceptable() {
+        return mIsAcceptable;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -206,7 +214,8 @@ public class AutofillSuggestion {
                 && Objects.equals(this.mIphDescriptionText, other.mIphDescriptionText)
                 && Objects.equals(this.mCustomIconUrl, other.mCustomIconUrl)
                 && Objects.equals(this.mPayload, other.mPayload)
-                && Objects.equals(this.mChildren, other.mChildren);
+                && Objects.equals(this.mChildren, other.mChildren)
+                && this.mIsAcceptable == other.mIsAcceptable;
     }
 
     @Override
@@ -225,7 +234,8 @@ public class AutofillSuggestion {
                 this.mIphDescriptionText,
                 this.mCustomIconUrl,
                 this.mPayload,
-                this.mChildren);
+                this.mChildren,
+                this.mIsAcceptable);
     }
 
     /** Builder for the {@link AutofillSuggestion}. */
@@ -245,6 +255,7 @@ public class AutofillSuggestion {
         private int mSuggestionType;
         private @Nullable Payload mPayload;
         private List<AutofillSuggestion> mChildren = Collections.emptyList();
+        private boolean mIsAcceptable;
 
         public Builder setIconId(int iconId) {
             this.mIconId = iconId;
@@ -321,6 +332,11 @@ public class AutofillSuggestion {
             return this;
         }
 
+        public Builder setIsAcceptable(boolean isAcceptable) {
+            this.mIsAcceptable = isAcceptable;
+            return this;
+        }
+
         public AutofillSuggestion build() {
             assert mSuggestionType == SuggestionType.SEPARATOR || !TextUtils.isEmpty(mLabel)
                     : "Only separators may have an empty label.";
@@ -341,7 +357,8 @@ public class AutofillSuggestion {
                     mIphDescriptionText,
                     mCustomIconUrl,
                     mPayload,
-                    mChildren);
+                    mChildren,
+                    mIsAcceptable);
         }
     }
 }
