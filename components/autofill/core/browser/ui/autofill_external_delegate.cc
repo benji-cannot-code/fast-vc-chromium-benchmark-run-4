8b88a594a3ad64255f001f2da9f2b9137f7b126f
Jan Keitel <jkeitel@google.com>
2026-07-13 07:36:03 -0700

    Accepting PContext notice suggestions should not crash Chrome
    Fixed: 534221851
    Change-Id: I7f310083315235e9d6970a18aa0d65c142040b5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085741
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661109}

--- components/autofill/core/browser/ui/autofill_external_delegate.cc
+++ components/autofill/core/browser/ui/autofill_external_delegate.cc
@@ -1047,6 +1047,10 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
           suggestion.main_text.value);
       // The popup remains open to show search results once the query completes.
       return;
+    case SuggestionType::kPersonalContextNotice:
+      // Accepting the suggestion is a no-op - accepting the notice happens via
+      // `RemoveSuggestion`.
+      return;
     case SuggestionType::kAccountStoragePasswordEntry:
     case SuggestionType::kAllSavedPasswordsEntry:
     case SuggestionType::kAtMemoryAiDisclosure:
@@ -1066,7 +1070,6 @@ void AutofillExternalDelegate::DidAcceptSuggestion(
     case SuggestionType::kPasswordEntry:
     case SuggestionType::kPasswordFieldByFieldFilling:
     case SuggestionType::kPendingStateSignin:
-    case SuggestionType::kPersonalContextNotice:
     case SuggestionType::kSeparator:
     case SuggestionType::kTitle:
     case SuggestionType::kTroubleSigningInEntry:
