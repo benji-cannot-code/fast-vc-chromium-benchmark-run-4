beb790a20235303bab16b7de5b8bf35a9da3da66
Isabelle Goralsky <igoralsky@google.com>
2026-07-13 18:13:03 -0700

    Add textbox for user prompt input to tab grouping.
    The textbox is currently disabled but will be enabled in a follow-up CL
    once the user prompt is connected to the backend.
    
    Bug: b:533515867
    Change-Id: If6745fe8e1a1a56dba6e3a3e5d5a32f415d977cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085303
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661543}

--- chrome/browser/resources/context_hub/tab_groups/tab_groups.ts
+++ chrome/browser/resources/context_hub/tab_groups/tab_groups.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import '//resources/cr_elements/cr_collapse/cr_collapse.js';
 import '//resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
+import '//resources/cr_elements/cr_input/cr_input.js';
 import '/strings.m.js';
 
 import {loadTimeData} from '//resources/js/load_time_data.js';
@@ -44,6 +45,7 @@ export class TabGroupsElement extends CrLitElement {
       isGrouped_: {type: Boolean},
       isGrouping_: {type: Boolean},
       autoTabGroupsEnabled_: {type: Boolean},
+      inputValue_: {type: String},
     };
   }
 
@@ -54,6 +56,7 @@ export class TabGroupsElement extends CrLitElement {
   protected accessor isGrouping_: boolean = false;
   protected accessor autoTabGroupsEnabled_: boolean =
       loadTimeData.getBoolean('kAutoTabGroups');
+  protected accessor inputValue_: string = '';
 
   override connectedCallback() {
     super.connectedCallback();
@@ -126,6 +129,10 @@ export class TabGroupsElement extends CrLitElement {
       }
       return g;
     });
+  }
+
+  protected onInputValueChanged_(e: CustomEvent<{value: string}>) {
+    this.inputValue_ = e.detail.value;
   }
 }
 
