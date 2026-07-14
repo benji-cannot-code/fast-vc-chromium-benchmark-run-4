9beccd3bc12548fd0a3457701951b5a190ffc763
Patrick Noland <pnoland@google.com>
2026-07-14 07:30:56 -0700

    Disable ChangeBounds anim in popover layout mode
    This breaks the glif anim, plus W/M/L do no animate this transition.
    Also, optimize GLifStrokeDrawable by removing allocations in draw()
    
    Bug: 529870155
    Change-Id: Ia62be8ae6471905aa60a6a1bc31fa0fe7dbef206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8067850
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661843}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
@@ -58,6 +58,7 @@ import org.chromium.chrome.browser.lifecycle.NativeInitObserver;
 import org.chromium.chrome.browser.locale.LocaleManager;
 import org.chromium.chrome.browser.omnibox.LocationBarMediator.OmniboxUma;
 import org.chromium.chrome.browser.omnibox.fusebox.FuseboxCoordinator;
+import org.chromium.chrome.browser.omnibox.fusebox.FuseboxCoordinator.FuseboxLayoutMode;
 import org.chromium.chrome.browser.omnibox.fusebox.FuseboxCoordinator.FuseboxState;
 import org.chromium.chrome.browser.omnibox.fusebox.FuseboxCoordinator.PopupState;
 import org.chromium.chrome.browser.omnibox.geo.GeolocationHeader;
@@ -1022,11 +1023,14 @@ public class LocationBarCoordinator
         // <--> expanded; they don't look good otherwise.
         boolean transitioningFromOrToDisabledState =
                 mCurrentFuseboxState == FuseboxState.DISABLED || newState == FuseboxState.DISABLED;
+        // The popover UI doesn't look good when animating bounds changes.
+        boolean isPopover =
+                getFuseboxLayoutModeSupplier().get() == FuseboxLayoutMode.SUGGESTIONS_POPOVER;
 
         mCurrentFuseboxState = newState;
         updateUrlBarForMultilineInput();
 
-        if (transitioningFromOrToDisabledState) return;
+        if (transitioningFromOrToDisabledState || isPopover) return;
 
         ChangeBounds changeBounds = new ChangeBounds();
         changeBounds
@@ -1069,6 +1073,11 @@ public class LocationBarCoordinator
         } else {
             TransitionManager.beginDelayedTransition(mLocationBarLayout, transition);
         }
+    }
+
+    @VisibleForTesting
+    NonNullObservableSupplier<Integer> getFuseboxLayoutModeSupplier() {
+        return mFuseboxCoordinator.getFuseboxLayoutModeSupplier();
     }
 
     /* package */ void onPopupStateChange(@PopupState int popupState) {
@@ -1390,6 +1399,10 @@ public class LocationBarCoordinator
     /** Set an instance of UrlBarCoordinator for testing. */
     void setUrlCoordinatorForTesting(UrlBarCoordinator urlCoordinator) {
         mUrlCoordinator = urlCoordinator;
+    }
+
+    void setFuseboxCoordinatorForTesting(FuseboxCoordinator fuseboxCoordinator) {
+        mFuseboxCoordinator = fuseboxCoordinator;
     }
 
     /** Set an instance of UrlBar for testing. */
