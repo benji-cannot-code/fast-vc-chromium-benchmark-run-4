b7fcd5c71a9579c3dc62951698a0589de5c898f6
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-07-14 13:46:02 -0700

    Revert "Remove Find in Page vertical track for Desktop Chrome"
    This reverts commit 4fb225a5465a1e3b56bd2512e315a85fe76b845d.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6608889077301248
    
    Sample build with failed test: https://ci.chromium.org/b/8676281169657693793
    Affected test(s):
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.findinpage:FindTest#testResultsBarGenericMotionEventConsumed](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.findinpage:FindTest%23testResultsBarGenericMotionEventConsumed?q=VHash%3Ab5ffcd6cef267eef)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.findinpage:FindTest#testResultsBarInitiallyVisible](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.findinpage:FindTest%23testResultsBarInitiallyVisible?q=VHash%3Ab5ffcd6cef267eef)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.findinpage:FindTest#testResultsBarVisibleAfterTypingText](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.findinpage:FindTest%23testResultsBarVisibleAfterTypingText?q=VHash%3Ab5ffcd6cef267eef)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6608889077301248&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8025419&type=BUG
    
    Original change's description:
    > Remove Find in Page vertical track for Desktop Chrome
    >
    > Summary of changes:
    >
    > FindToolbar.java: Early return for desktop screens to prevent result bar
    > (vertical side track) creation.
    >
    > find_request_manager.cc: Before asking a frame for its search results,
    > check if the frame is alive. This prevents checking a dead frame which
    > will trigger a DCHECK, causing the browser process to crash.
    >
    > text_finder.cc: Before measuring the pixel coordinates of any search
    > match, tell the renderer to update the layout first to prevent layout
    > (renderer) crashes when stepping through search results.
    >
    > Bug: 517595935
    > Change-Id: I1d66fac3df7c1c62364337e8c486f0c9ddd92ac6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025419
    > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > Reviewed-by: Theresa Wellington <twellington@chromium.org>
    > Commit-Queue: Neha Sapre <nehasapre@google.com>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1661944}
    >
    
    Bug: 517595935
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I225adeb907329426743cfbbcb38f03ad9660aef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8093942
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Owners-Override: Fred Shih <ffred@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662144}

--- third_party/blink/renderer/core/editing/finder/text_finder.cc
+++ third_party/blink/renderer/core/editing/finder/text_finder.cc
@@ -670,8 +670,6 @@ void TextFinder::InvalidateFindMatchRects() {
 }
 
 void TextFinder::UpdateFindMatchRects() {
-  GetFrame()->GetDocument()->UpdateStyleAndLayout(
-      DocumentUpdateReason::kFindInPage);
   gfx::Size current_document_size = OwnerFrame().DocumentSize();
   if (document_size_for_current_find_match_rects_ != current_document_size) {
     document_size_for_current_find_match_rects_ = current_document_size;
@@ -711,8 +709,6 @@ gfx::RectF TextFinder::ActiveFindMatchRect() {
   if (!current_active_match_frame_ || !active_match_)
     return gfx::RectF();
 
-  GetFrame()->GetDocument()->UpdateStyleAndLayoutForRange(
-      active_match_.Get(), DocumentUpdateReason::kFindInPage);
   return FindInPageRectFromRange(EphemeralRange(ActiveMatch()));
 }
 
@@ -788,9 +784,6 @@ int TextFinder::SelectFindMatch(unsigned index, gfx::Rect* selection_rect) {
     // Make sure no node is focused. See http://crbug.com/38700.
     OwnerFrame().GetFrame()->GetDocument()->ClearFocusedElement();
   }
-
-  OwnerFrame().GetFrame()->GetDocument()->UpdateStyleAndLayoutForRange(
-      active_match_.Get(), DocumentUpdateReason::kFindInPage);
 
   gfx::Rect active_match_rect;
   gfx::Rect active_match_bounding_box =
