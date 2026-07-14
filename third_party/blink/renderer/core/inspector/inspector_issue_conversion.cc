4c2570e67e3bd4d99a6668fae616dbdccf25b0b2
Andrew Paseltiner <apaseltiner@chromium.org>
2026-07-14 12:50:25 -0700

    Remove obsolete Attribution Reporting issues from DevTools protocol
    Handling of these has already been removed from the DevTools frontend in
    http://crrev.com/c/devtools/devtools-frontend/+/7962422.
    
    Bug: 513231893
    Change-Id: If9137b8138723dd7cc6f275d2a93cc452fe9393e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7973736
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662092}

--- third_party/blink/renderer/core/inspector/inspector_issue_conversion.cc
+++ third_party/blink/renderer/core/inspector/inspector_issue_conversion.cc
@@ -68,7 +68,6 @@ blink::protocol::String InspectorIssueCodeValue(
     case mojom::blink::InspectorIssueCode::kCookieDeprecationMetadataIssue:
     case mojom::blink::InspectorIssueCode::kGenericIssue:
     case mojom::blink::InspectorIssueCode::kDeprecationIssue:
-    case mojom::blink::InspectorIssueCode::kAttributionReportingIssue:
       NOTREACHED();
   }
 }
