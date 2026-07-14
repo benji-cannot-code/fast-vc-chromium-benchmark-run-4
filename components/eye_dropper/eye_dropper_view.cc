8c32ee8770f797c8321340d3938b1b3712fe1f5e
Allen Bauer <kylixrd@chromium.org>
2026-07-14 09:49:31 -0700

    Cache whether the DesktopWindowTreeHostWin is modal.
    Under CLIENT_OWNS_WIDGET ownership model, if the underlying desktop
    native widget was "modal", the owner window is disabled while the widget
    is visible. If the native_widget_delegate is torn down before the
    DesktopWindowTreeHostWin, it leaves the WeakPtr reference null. When the
    underlying HWND is finally destroyed and the "IsModal()" function is
    called, if the delegate is null it returns False even though it had been
    true. This has the effect of leaving the owner window disabled, which
    looks like a hang if it is the main browser window. Modality of a Window
    should be immutable, so we cache the fact that the HWND was "modal" and
    return that value when IsModal() is called.
    
    Change-Id: Id89104768eb6622557e729a10a3af00b9b597d66
    Bug: 40232479, 527089994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007736
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661938}

--- components/eye_dropper/eye_dropper_view.cc
+++ components/eye_dropper/eye_dropper_view.cc
@@ -407,7 +407,6 @@ EyeDropperView::~EyeDropperView() {
   pre_dispatch_handler_.reset();
   if (widget_) {
     widget_->RemoveObserver(this);
-    widget_->CloseNow();
     widget_.reset();
   }
   delegate_.reset();
