09527f20e7620ae1f5aa0655c6cebb892eab1e6c
Shunya Shishido <sisidovski@chromium.org>
2026-07-13 20:46:26 -0700

    Add UseCounter for DeclarativePerformanceObserver
    This CL registers a UseCounter for the DeclarativePerformanceObserver
    when the observer is constructed in the browser process.  This helps us
    track adoption of the declarative performance observer feature.  The UMA
    mapping in enums.xml was also updated.
    
    TAG=agy
    CONV=a41834ac-f2c3-4380-90b1-c8d4dea5ce14
    
    Bug: 534399206
    Change-Id: Ifab51e39441a6629cd47db3538e4954a7156e3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8089667
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661601}

--- content/browser/declarative_performance_observer/declarative_performance_observer.cc
+++ content/browser/declarative_performance_observer/declarative_performance_observer.cc
@@ -9,16 +9,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "content/browser/declarative_performance_observer/declarative_performance_observer_store.h"
 #include "content/browser/storage_partition_impl.h"
+#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/navigation_handle_timing.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_client.h"
 #include "mojo/public/cpp/bindings/message.h"
 #include "net/base/load_timing_info.h"
 #include "net/base/net_errors.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom.h"
 
 namespace content {
 
@@ -31,6 +34,9 @@ DeclarativePerformanceObserver::DeclarativePerformanceObserver(
     RenderFrameHost* rfh,
     NavigationHandle* navigation_handle)
     : DocumentUserData<DeclarativePerformanceObserver>(rfh) {
+  GetContentClient()->browser()->LogWebFeatureForCurrentPage(
+      rfh, blink::mojom::WebFeature::kDeclarativePerformanceObserver);
+
   const network::mojom::DeclarativePerformanceObserverPolicy* policy =
       navigation_handle->GetDeclarativePerformanceObserverPolicy();
   CHECK(policy);
