9423b721b5db31c54ba92e7ffdda4351d64dd0dc
Aldo Culquicondor <acondor@chromium.org>
2026-07-14 09:12:52 -0700

    Run GlicPinnedTabManagerBrowserTest on Android
    To achieve it, this CL migrates GlicPinnedTabManagerBrowserTest to
    GlicBrowserTest and avoided dependencies on the TabStripModel
    
    Bug: b:534710453
    Change-Id: I18dbe21c51a94449f60504c7225364fb0c32d73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8073131
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661908}

--- chrome/browser/glic/test_support/glic_test_util.cc
+++ chrome/browser/glic/test_support/glic_test_util.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_list/tab_list_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/browser/ui/browser_window/public/create_browser_window.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
@@ -383,6 +384,16 @@ bool IsSidePanelEnabled() {
 #else
   return false;
 #endif
+}
+
+BrowserWindowInterface* CreateBrowserWindow(Profile* profile) {
+  base::test::TestFuture<BrowserWindowInterface*> future;
+  CreateBrowserWindow(
+      BrowserWindowCreateParams(*profile, /*from_user_gesture=*/false),
+      future.GetCallback());
+  BrowserWindowInterface* window = future.Get();
+  CHECK(window);
+  return window;
 }
 
 DEFINE_ELEMENT_IDENTIFIER_VALUE(kGlicHostElementId);
