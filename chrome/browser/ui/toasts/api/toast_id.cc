5aaf3748269fa44173926ba7d7dd435b6e13efbc
Elias Homsi <eliashomsi@google.com>
2026-07-14 13:40:59 -0700

    Add enterprise copy warning and blocked toasts.
    This change introduces new UI strings and toast registrations for
    enterprise data loss prevention (DLP) policies related to copying
    content. It includes a warning toast with a "Copy anyway" button and a
    blocked toast. Right now there is a no-op on the button itself, this is
    done on purpose to split the change and register to toasts for now.
    
    Bug: 325455508
    Change-Id: Ie74ba5db4cdfb3a87fa3ad4f5382b104f9988136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8069981
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662141}

--- chrome/browser/ui/toasts/api/toast_id.cc
+++ chrome/browser/ui/toasts/api/toast_id.cc
@@ -110,6 +110,10 @@ std::string_view GetToastName(ToastId toast_id) {
       return "DictationStopped";
     case ToastId::kGlicSelectionHiddenForSite:
       return "GlicSelectionHiddenForSite";
+    case ToastId::kEnterpriseCopyWarning:
+      return "EnterpriseCopyWarning";
+    case ToastId::kEnterpriseCopyBlocked:
+      return "EnterpriseCopyBlocked";
   }
 
   NOTREACHED();
