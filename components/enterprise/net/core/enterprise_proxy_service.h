fd0a1c1974f2f6866ff9ecf6045f99f30ac4fdd7
Zonghan Xu <xzonghan@google.com>
2026-07-14 13:36:09 -0700

    [SG Android] Refactor Enterprise Proxy data types
    Adjust types in enterprise networking directory to match future implementation
    
    Bug: 507058810
    Change-Id: Id7ecc349bf9bde83a1ace096091e11ea61a896ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8087220
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662137}

--- components/enterprise/net/core/enterprise_proxy_service.h
+++ components/enterprise/net/core/enterprise_proxy_service.h
@@ -85,7 +85,7 @@ class EnterpriseProxyService : public KeyedService {
     ProxyProvisioningDomain& operator=(ProxyProvisioningDomain&&) noexcept;
     ~ProxyProvisioningDomain();
 
-    ProxyProvisioningDomainPolicy policy;
+    ProvisioningDomainConfig policy;
     std::unique_ptr<ProvisioningDomainFetcher> fetcher;
 
     ProvisioningDomainProxyConfig fetched_config;
