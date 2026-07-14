10755818104f6367c169f1fc499ffc2d7ecfbe64
Simeon Anfinrud <sanfin@chromium.org>
2026-07-14 11:06:04 -0700

    [chromecast] Use WeakPtr for AudioDecoderSoftwareWrapper decode callback
    This resolves a potential Use-After-Free by replacing base::Unretained(this)
    with a WeakPtr in the callback passed to CastAudioDecoder::Decode.
    If AudioDecoderSoftwareWrapper is destroyed while a decode is pending,
    the callback will safely be a no-op instead of executing on a deleted pointer.
    
    BUG=516988476
    TEST=Compiled and passed unit tests.
    
    Change-Id: I7d973d2cc3a19465ae56b52be6d8223f17dc7c1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8077607
    Auto-Submit: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Commit-Queue: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662004}

--- chromecast/media/common/audio_decoder_software_wrapper.h
+++ chromecast/media/common/audio_decoder_software_wrapper.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chromecast/media/api/cast_audio_decoder.h"
 #include "chromecast/public/media/media_pipeline_backend.h"
 
@@ -69,6 +70,8 @@ class AudioDecoderSoftwareWrapper
   AudioConfig output_config_;
   scoped_refptr<DecoderBufferBase> pending_pushed_buffer_;
   bool decoder_error_;
+
+  base::WeakPtrFactory<AudioDecoderSoftwareWrapper> weak_factory_{this};
 };
 
 }  // namespace media
