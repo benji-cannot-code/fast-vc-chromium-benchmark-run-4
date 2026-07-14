c8fd9ff0171ee943e58cdd6597b830bbe1d9dd4a
Christoph Schwering <schwering@google.com>
2026-07-14 02:49:48 -0700

    [Autofill] Handle AtMemory shortcut keydown in AutofillAgent
    This CL observes the configurable AtMemory keyboard shortcut (e.g.,
    Ctrl+Space) in AutofillAgent.
    
    The reasons for implementing the shortcut handling in the renderer
    are in go/atmemory-shortcut.
    
    TAG=agy
    CONV=0067120f-a154-4027-b364-d763ec4d2298
    
    Bug: 494158096
    Change-Id: Ie30382f4d7c90dc291eb7b71334c3622332ba09a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7984014
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661712}

--- components/autofill/content/renderer/autofill_agent_browsertest.cc
+++ components/autofill/content/renderer/autofill_agent_browsertest.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_view.h"
 #include "ui/accessibility/ax_mode.h"
 #include "ui/accessibility/ax_node_data.h"
+#include "ui/events/event_constants.h"
 #include "v8/include/v8.h"
 
 namespace autofill {
@@ -80,6 +81,7 @@ using ::blink::WebSelectElement;
 using ::blink::WebString;
 using ::testing::_;
 using ::testing::AllOf;
+using ::testing::AnyNumber;
 using ::testing::AtMost;
 using ::testing::DoAll;
 using ::testing::ElementsAre;
@@ -1896,6 +1898,14 @@ TEST_F(AutofillAgentTest, RequestRefillTimesOut) {
 
 class AutofillAgentTest_AtMemory : public AutofillAgentTest {
  public:
+  AutofillAgentTest_AtMemory() {
+    scoped_feature_list_.InitWithFeatures(
+        /*enabled_features=*/{blink::features::kAutofillKeydownEditableElement,
+                              features::kAutofillAtMemoryTriggerShortcut,
+                              features::kAutofillAtMemory},
+        /*disabled_features=*/{});
+  }
+
   void SetUp() override {
     AutofillAgentTest::SetUp();
     SetTrigger("@@");
@@ -1905,6 +1915,17 @@ class AutofillAgentTest_AtMemory : public AutofillAgentTest {
     blink::RendererPreferences prefs =
         GetMainRenderFrame()->GetWebView()->GetRendererPreferences();
     prefs.autofill_trigger_string = std::move(trigger_string);
+    prefs.autofill_shortcut_key_code = ui::VKEY_UNKNOWN;
+    prefs.autofill_shortcut_modifiers = ui::EF_NONE;
+    GetMainRenderFrame()->GetWebView()->SetRendererPreferences(prefs);
+  }
+
+  void SetTrigger(ui::KeyboardCode key_code, int modifiers) {
+    blink::RendererPreferences prefs =
+        GetMainRenderFrame()->GetWebView()->GetRendererPreferences();
+    prefs.autofill_trigger_string = "";
+    prefs.autofill_shortcut_key_code = key_code;
+    prefs.autofill_shortcut_modifiers = modifiers;
     GetMainRenderFrame()->GetWebView()->SetRendererPreferences(prefs);
   }
 
@@ -1921,8 +1942,7 @@ class AutofillAgentTest_AtMemory : public AutofillAgentTest {
   }
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_{
-      features::kAutofillAtMemory};
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(AutofillAgentTest_AtMemory, AtMemorySearchTrigger) {
@@ -1967,7 +1987,7 @@ TEST_F(AutofillAgentTest_AtMemory, AtMemorySearchTrigger) {
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
                   _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
 
   // Typing sequence: "a", "a@", "a@@", "a@@b"
   SimulateSlowTyping("a");
@@ -1980,6 +2000,116 @@ TEST_F(AutofillAgentTest_AtMemory, AtMemorySearchTrigger) {
   check_point.Call(4);
 }
 
+// Tests that the keyboard shortcut triggers AtMemory in an <input>.
+TEST_F(AutofillAgentTest_AtMemory, AtMemoryShortcutTrigger) {
+  SetTrigger(ui::VKEY_Y, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
+
+  LoadHTML(R"(<input id="f">)");
+  WaitForFormsSeen();
+  Focus("f");
+
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Eq(AutofillSuggestionTriggerSource::kAtMemory), _));
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
+      .Times(AnyNumber());
+
+  blink::WebKeyboardEvent event(
+      blink::WebInputEvent::Type::kRawKeyDown,
+      blink::WebInputEvent::kControlKey | blink::WebInputEvent::kShiftKey,
+      base::TimeTicks::Now());
+  event.windows_key_code = ui::VKEY_Y;
+  SendWebKeyboardEvent(event);
+
+  task_environment_.RunUntilIdle();
+}
+
+// Tests that the keyboard shortcut triggers AtMemory in a <textarea>.
+TEST_F(AutofillAgentTest_AtMemory, AtMemoryShortcutTriggerTextArea) {
+  SetTrigger(ui::VKEY_Y, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
+
+  LoadHTML(R"(<textarea id="f"></textarea>)");
+  WaitForFormsSeen();
+  Focus("f");
+
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Eq(AutofillSuggestionTriggerSource::kAtMemory), _));
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
+      .Times(AnyNumber());
+
+  blink::WebKeyboardEvent event(
+      blink::WebInputEvent::Type::kRawKeyDown,
+      blink::WebInputEvent::kControlKey | blink::WebInputEvent::kShiftKey,
+      base::TimeTicks::Now());
+  event.windows_key_code = ui::VKEY_Y;
+  SendWebKeyboardEvent(event);
+
+  task_environment_.RunUntilIdle();
+}
+
+// Tests that the keyboard shortcut triggers AtMemory even with CapsLock and
+// NumLock.
+TEST_F(AutofillAgentTest_AtMemory,
+       AtMemoryShortcutTriggerWithCapsLockAndNumLock) {
+  SetTrigger(ui::VKEY_Y, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
+
+  LoadHTML(R"(<input id="f">)");
+  WaitForFormsSeen();
+  Focus("f");
+
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Eq(AutofillSuggestionTriggerSource::kAtMemory), _));
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
+      .Times(AnyNumber());
+
+  blink::WebKeyboardEvent event(
+      blink::WebInputEvent::Type::kRawKeyDown,
+      blink::WebInputEvent::kControlKey | blink::WebInputEvent::kShiftKey |
+          blink::WebInputEvent::kCapsLockOn | blink::WebInputEvent::kNumLockOn,
+      base::TimeTicks::Now());
+  event.windows_key_code = ui::VKEY_Y;
+  SendWebKeyboardEvent(event);
+
+  task_environment_.RunUntilIdle();
+}
+
+// Tests that the keyboard shortcut does not trigger AtMemory if it's an
+// auto-repeat event.
+TEST_F(AutofillAgentTest_AtMemory, AtMemoryShortcutTriggerRepeatBlocked) {
+  SetTrigger(ui::VKEY_Y, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
+
+  LoadHTML(R"(<input id="f">)");
+  WaitForFormsSeen();
+  Focus("f");
+
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Eq(AutofillSuggestionTriggerSource::kAtMemory), _))
+      .Times(0);
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
+      .Times(AnyNumber());
+
+  blink::WebKeyboardEvent event(blink::WebInputEvent::Type::kRawKeyDown,
+                                blink::WebInputEvent::kControlKey |
+                                    blink::WebInputEvent::kShiftKey |
+                                    blink::WebInputEvent::kIsAutoRepeat,
+                                base::TimeTicks::Now());
+  event.windows_key_code = ui::VKEY_Y;
+  SendWebKeyboardEvent(event);
+
+  task_environment_.RunUntilIdle();
+}
+
 // Tests that typing "@@" into an empty field triggers the @memory search popup.
 TEST_F(AutofillAgentTest_AtMemory, MemorySearchTriggerTypedIntoEmptyField) {
   // 1. Setup Expectations:
@@ -1987,7 +2117,7 @@ TEST_F(AutofillAgentTest_AtMemory, MemorySearchTriggerTypedIntoEmptyField) {
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
                   _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
   // Expect the specific @memory trigger.
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
@@ -2007,7 +2137,7 @@ TEST_F(AutofillAgentTest_AtMemory, MemorySearchTriggerInMiddle) {
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
                   _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
   // Expect the specific @memory trigger.
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
@@ -2027,7 +2157,7 @@ TEST_F(AutofillAgentTest_AtMemory, MemorySearchNotTriggeredOnPasswordField) {
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
                   _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
   // Expect no @memory trigger.
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(_, _, _,
@@ -2122,7 +2252,7 @@ TEST_F(AutofillAgentTest_AtMemory, NonStandardTriggerString) {
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
                   _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
   // Expect the specific @memory trigger.
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
@@ -2193,7 +2323,7 @@ TEST_F(AutofillAgentTest_AtMemoryContentEditable, TriggerSequence) {
       autofill_driver(),
       AskForValuesToFill(
           _, _, _, testing::Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
 
   SimulateSlowTyping("@");
   check_point.Call(1);
@@ -2363,7 +2493,7 @@ TEST_F(AutofillAgentTest_AtMemoryContentEditable, NonStandardTriggerString) {
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
                   _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
-      .Times(testing::AnyNumber());
+      .Times(AnyNumber());
   // Expect the specific @memory trigger.
   EXPECT_CALL(autofill_driver(),
               AskForValuesToFill(
@@ -2374,6 +2504,32 @@ TEST_F(AutofillAgentTest_AtMemoryContentEditable, NonStandardTriggerString) {
   WaitForFormsSeen();
   Focus("f");
   SimulateSlowTyping("Foobar");
+}
+
+// Tests that the keyboard shortcut triggers AtMemory in a contenteditable.
+TEST_F(AutofillAgentTest_AtMemoryContentEditable, AtMemoryShortcutTrigger) {
+  SetTrigger(ui::VKEY_Y, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN);
+
+  LoadHTML(R"(<div contenteditable id="f"></div>)");
+  WaitForFormsSeen();
+  Focus("f");
+
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Eq(AutofillSuggestionTriggerSource::kAtMemory), _));
+  EXPECT_CALL(autofill_driver(),
+              AskForValuesToFill(
+                  _, _, _, Ne(AutofillSuggestionTriggerSource::kAtMemory), _))
+      .Times(AnyNumber());
+
+  blink::WebKeyboardEvent event(
+      blink::WebInputEvent::Type::kRawKeyDown,
+      blink::WebInputEvent::kControlKey | blink::WebInputEvent::kShiftKey,
+      base::TimeTicks::Now());
+  event.windows_key_code = ui::VKEY_Y;
+  SendWebKeyboardEvent(event);
+
+  task_environment_.RunUntilIdle();
 }
 
 class AutofillAgentTest_AtMemoryInactivityNudge
