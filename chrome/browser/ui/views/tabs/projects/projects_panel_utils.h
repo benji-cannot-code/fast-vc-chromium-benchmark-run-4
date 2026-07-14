d8d08d2ac35aaabdeb542ac6661909151261281a
Kunal Daftari <kunaldaftari@google.com>
2026-07-14 09:22:21 -0700

    [Organizer] Remove obsolete projects panel views and tests
    Now that the controls (close button) is the only element visible in the
    projects panel, we can safely delete the other elements (threads, scroll
    handler, tab group items) and their corresponding tests.
    
    Bug: 532670693
    Change-Id: I839897fbe2745bb604b9175daaaca98cf534a9d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8087237
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661917}

--- chrome/browser/ui/views/tabs/projects/projects_panel_utils.h
+++ chrome/browser/ui/views/tabs/projects/projects_panel_utils.h
@@ -6,14 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_UTILS_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_UTILS_H_
 
-#include "components/contextual_tasks/public/contextual_task.h"
-#include "ui/gfx/vector_icon_types.h"
-
-namespace views {
-class Button;
-class View;
-}  // namespace views
-
 class Profile;
 
 namespace projects_panel {
@@ -21,17 +13,6 @@ namespace projects_panel {
 // Returns whether the Projects Panel and its entrypoints should be visible in
 // the UI for the profile.
 bool IsProjectsPanelVisibleForProfile(Profile* profile);
-
-void ConfigureInkDropForButton(views::Button* view);
-
-const gfx::VectorIcon& GetIconForThreadType(
-    contextual_tasks::ThreadType thread_type);
-
-// Returns whether this view is the first focusable view in the panel.
-// Currently, this is the panel close button. This is used for accessibility
-// reasons (e.g., to focus the 3-dot button in a tab group item view) when
-// determining whether the user performed a reverse focus traversal.
-bool IsFirstFocusableViewInPanel(views::View* view);
 
 }  // namespace projects_panel
 
