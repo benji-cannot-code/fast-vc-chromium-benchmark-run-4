d8eedcc2488a076907f1867046abb263caeee66b
Yanling <yanlinghu@google.com>
2026-07-14 07:28:00 -0700

    [NTP Aurora] Add feature flags and params
    In chrome://flags under "New Tab Page Aurora", each variation behaves as
    follows:
    
    * Default ->  Feature disabled (false)
    * Enabled ->  Feature enabled (true) with Java defaults (padding_style = 0, change_button_color = false)
    * Enabled Tight padding ->  padding_style = 1, change_button_color = true
    * Enabled Medium padding ->  padding_style = 2, change_button_color = true
    * Enabled Large padding ->  padding_style = 3, change_button_color = true
    * Disabled -> Feature disabled (false)
    
    Bug: 534357676
    Change-Id: Ia6bb556541cd53eb38a004d8529f71eea7a4b29a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072658
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661840}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -547,6 +547,7 @@ public abstract class ChromeFeatureList {
     public static final String NOTIFICATION_TRAMPOLINE = "NotificationTrampoline";
     public static final String NOTIFICATION_TRAMPOLINE_NO_NEW_TASK =
             "NotificationTrampolineNoNewTask";
+    public static final String NTP_AURORA = "NtpAurora";
     public static final String NTP_MVC_REFACTOR = "NtpMvcRefactor";
     public static final String NTP_VISION = "NtpVision";
     public static final String OMAHA_MIN_SDK_VERSION_ANDROID = "OmahaMinSdkVersionAndroid";
@@ -1117,6 +1118,8 @@ public abstract class ChromeFeatureList {
                     NOTIFICATION_TRAMPOLINE_NO_NEW_TASK,
                     /* defaultValue= */ false,
                     /* defaultValueInTests= */ true);
+    public static final CachedFlag sNtpAurora =
+            newCachedFlag(NTP_AURORA, /* defaultValue= */ false);
     public static final CachedFlag sNtpMvcRefactor =
             newCachedFlag(NTP_MVC_REFACTOR, /* defaultValue= */ false);
     public static final CachedFlag sNtpVision =
@@ -1399,6 +1402,7 @@ public abstract class ChromeFeatureList {
                     sNewTabPageCustomizationV2,
                     sNotificationTrampoline,
                     sNotificationTrampolineNoNewTask,
+                    sNtpAurora,
                     sNtpMvcRefactor,
                     sNtpVision,
                     sOnStartupWindowPolicy,
@@ -1879,6 +1883,10 @@ public abstract class ChromeFeatureList {
     public static final IntCachedFeatureParam sNotificationTrampolineTimeoutPriorNativeInitMs =
             newIntCachedFeatureParam(
                     NOTIFICATION_TRAMPOLINE, "timeout_in_millis_prior_native_init", 5 * 1000);
+    public static final IntCachedFeatureParam sNtpAuroraPaddingStyle =
+            newIntCachedFeatureParam(NTP_AURORA, "padding_style", 0);
+    public static final BooleanCachedFeatureParam sNtpAuroraChangeButtonColor =
+            newBooleanCachedFeatureParam(NTP_AURORA, "change_button_color", false);
     public static final IntCachedFeatureParam sOmahaMinSdkVersionMinSdkVersion =
             newIntCachedFeatureParam(OMAHA_MIN_SDK_VERSION_ANDROID, "min_sdk_version", -1);
 
@@ -2042,6 +2050,8 @@ public abstract class ChromeFeatureList {
                     sNotificationTrampolineLongJobDurationMs,
                     sNotificationTrampolineNormalJobDurationMs,
                     sNotificationTrampolineTimeoutPriorNativeInitMs,
+                    sNtpAuroraChangeButtonColor,
+                    sNtpAuroraPaddingStyle,
                     sOmahaMinSdkVersionMinSdkVersion,
                     sPCctMinimumHeightRatio,
                     sPriceChangeModuleSkipShoppingPersistedTabDataDelayedInit,
