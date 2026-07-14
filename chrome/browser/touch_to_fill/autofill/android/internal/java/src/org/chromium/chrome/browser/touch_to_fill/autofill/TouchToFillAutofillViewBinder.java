26f4e57b9b084fe5ad3b0f9626fc2d8635549de1
Christian Dullweber <dullweber@chromium.org>
2026-07-14 04:16:34 -0700

    pContext: Add bottomsheet UI
    Add bottomsheet for autofill notice.
    
    Screenshot: http://b/521719322#comment2
    Bug: b:521719322
    Change-Id: I2413bbaf261d7beae6d49917fdf65a4437f5dfdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8064282
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661745}

---
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/autofill/TouchToFillAutofillViewBinder.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.touch_to_fill.autofill;
+
+import static org.chromium.chrome.browser.touch_to_fill.autofill.TouchToFillAutofillProperties.ACKNOWLEDGE_HANDLER;
+import static org.chromium.chrome.browser.touch_to_fill.autofill.TouchToFillAutofillProperties.DISMISS_HANDLER;
+import static org.chromium.chrome.browser.touch_to_fill.autofill.TouchToFillAutofillProperties.SETTINGS_LINK_HANDLER;
+import static org.chromium.chrome.browser.touch_to_fill.autofill.TouchToFillAutofillProperties.VISIBLE;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * Provides functions that map {@link TouchToFillAutofillProperties} changes in a {@link
+ * PropertyModel} to the suitable method in {@link TouchToFillAutofillView}.
+ */
+@NullMarked
+class TouchToFillAutofillViewBinder {
+    static void bind(PropertyModel model, TouchToFillAutofillView view, PropertyKey propertyKey) {
+        if (propertyKey == VISIBLE) {
+            view.setVisible(model.get(VISIBLE));
+        } else if (propertyKey == ACKNOWLEDGE_HANDLER) {
+            view.setAcknowledgeHandler(model.get(ACKNOWLEDGE_HANDLER));
+        } else if (propertyKey == SETTINGS_LINK_HANDLER) {
+            view.setSettingsLinkHandler(model.get(SETTINGS_LINK_HANDLER));
+        } else if (propertyKey == DISMISS_HANDLER) {
+            view.setDismissHandler(model.get(DISMISS_HANDLER));
+        }
+    }
+
+    private TouchToFillAutofillViewBinder() {}
+}
