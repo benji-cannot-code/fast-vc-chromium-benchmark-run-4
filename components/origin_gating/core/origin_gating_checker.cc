27fd0cbbaca20183254ed247c532e575d7219bdf
Chris Fredrickson <cfredric@chromium.org>
2026-07-14 15:00:01 -0700

    Add missing GUARDED_BY_CONTEXT annotations
    SequenceCheckers are most effective when we use thread annotations to
    protect specific members from mutation on different sequences; they
    don't get protected by default. The annotations make it a compile-time
    error to access one of those members without having first verified the
    sequence.
    
    Change-Id: I1ce68e590fa05590a0ef43ac8c765dd1721cf0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8090903
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Joshua Thomas <masnoble@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662192}

--- components/origin_gating/core/origin_gating_checker.cc
+++ components/origin_gating/core/origin_gating_checker.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/notreached.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/thread_annotations.h"
 #include "components/origin_gating/core/origin_gating_cache.h"
 #include "components/origin_gating/core/types.h"
 #include "third_party/abseil-cpp/absl/functional/overload.h"
@@ -110,7 +111,8 @@ void OriginGatingChecker::RunNextPredicate(
 
   std::visit(
       absl::Overload{
-          [&](DecisionSource source_enum) {
+          [&](DecisionSource source_enum) VALID_CONTEXT_REQUIRED(
+              sequence_checker_) {
             switch (source_enum) {
               case DecisionSource::kAllowSameOrigin: {
                 Decision decision = EvaluateAllowSameOrigin(
@@ -164,7 +166,8 @@ void OriginGatingChecker::RunNextPredicate(
                 NOTREACHED();
             }
           },
-          [&](const CustomPredicate& custom_predicate) {
+          [&](const CustomPredicate& custom_predicate) VALID_CONTEXT_REQUIRED(
+              sequence_checker_) {
             GatingDecisionContext* raw_context = context.get();
             custom_predicate.Run(
                 raw_context, input.event, input.source, input.destination,
