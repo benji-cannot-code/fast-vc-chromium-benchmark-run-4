c9c42c0bfa8144f23f595b192fdbd17e2c948b83
Jenna Himawan <jhimawan@google.com>
2026-07-14 09:50:44 -0700

    CrossDeviceThemeTracker: Clean up unused profiles dependency and factory includes
    Follow-ups from crrev.com/c/8077759
    
    TAG=agy
    CONV=67c79864-a654-466c-a25d-ce0422a85341
    
    Bug: 517615321
    Change-Id: I97fa343a1a0046389ef33ffb9e72e61e472141a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8088454
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661940}

--- chrome/browser/themes/cross_device/cross_device_theme_tracker_android_unittest.cc
+++ chrome/browser/themes/cross_device/cross_device_theme_tracker_android_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/run_until.h"
-#include "chrome/browser/sync/cross_device_theme_tracker_factory.h"
 #include "chrome/browser/themes/cross_device/cross_device_theme_tracker_test_util.h"
 #include "components/sync/protocol/theme_android_specifics.pb.h"
 #include "components/sync/protocol/theme_ios_specifics.pb.h"
