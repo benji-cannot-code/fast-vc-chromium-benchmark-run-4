54da12edb15b70047d3f1102bfc788e25ae7b374
Dan Harrington <harringtond@chromium.org>
2026-07-14 10:52:40 -0700

    Fix glic e2e test build
    http: //shortn/_iT3MryFPfH - fix this build error
    Change-Id: Iaae11cfd3593338bfd77b8cf2f38d4796a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8093070
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661991}

--- chrome/browser/glic/e2e_test/glic_e2e_test.h
+++ chrome/browser/glic/e2e_test/glic_e2e_test.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/host/host.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/service/glic_instance_coordinator.h"
+#include "chrome/browser/glic/test_support/glic_test_util.h"
 #include "chrome/browser/signin/e2e_tests/live_test.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "chrome/test/interaction/webcontents_interaction_test_util.h"
