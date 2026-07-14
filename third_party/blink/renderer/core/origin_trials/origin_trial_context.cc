004ac5d15417d7569854b990a7b3520ba4701db8
Nan Lin <linnan@chromium.org>
2026-07-14 12:14:55 -0700

    Remove files under //components/attribution_reporting
    As part of the removal of the Attribution Reporting API, this cl removes
    files under //components/attribution_reporting.
    
    This remaining files are features.* and is_origin_suitable.* which will
    be cleaned up as a follow up.
    
    I2D&R:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/4K2RRt6VYCQ
    
    Bug: 513231893
    Change-Id: I6aa2f1d4dd8c94462052126a8285a35f45d2b097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7999117
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662070}

--- third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
+++ third_party/blink/renderer/core/origin_trials/origin_trial_context.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "components/attribution_reporting/features.h"
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/features_generated.h"
