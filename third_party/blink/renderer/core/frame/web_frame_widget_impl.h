5234edf71555125b72383d18a6fb1a5c647f537f
Chuong Ho <hdchuong@google.com>
2026-07-14 15:46:58 -0700

    blink: expose edit context data in UpdateCursorAnchorInfo
    When an EditContext is active on Android, standard DOM-based
    calculations (`VisibleBoundsInLocalRoot()` and
    `CalculateSelectionBounds()`) do not reflect the custom layout bounds
    provided by web applications using the EditContext API. This CL updates
    `WebFrameWidgetImpl::UpdateCursorAnchorInfo` on Android to support
    EditContext.
    
    Bug: b:392744952
    Change-Id: Id6d6e27ab18afeaff60432a768c80533c99fb8c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7924487
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662225}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.h
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.h
@@ -276,6 +276,10 @@ class CORE_EXPORT WebFrameWidgetImpl
       Vector<gfx::Rect>* bounds_in_dips) override;
   // Return the last calculated cursor anchor info.
   mojom::blink::InputCursorAnchorInfoPtr& GetLastCursorAnchorInfoForTesting();
+  mojom::blink::InputCursorAnchorInfoPtr CalculateCursorAnchorInfoForTesting(
+      bool update_requested) {
+    return CalculateCursorAnchorInfo(update_requested);
+  }
   bool HasImeRenderWidgetHost() const override {
     return !!ime_render_widget_host_;
   }
@@ -783,7 +787,6 @@ class CORE_EXPORT WebFrameWidgetImpl
   // coordinate space.
   Vector<gfx::Rect> CalculateVisibleLineBoundsOnScreen();
 #endif  // BUILDFLAG(IS_ANDROID)
-
   // Returns true if this widget corresponds to a frame which is being replaced.
   // The compositor for the widget has been detached and passed to the new
   // widget.
@@ -848,6 +851,7 @@ class CORE_EXPORT WebFrameWidgetImpl
   bool doing_drag_and_drop_ = false;
 
  private:
+  friend class WebFrameWidgetSimTest;
   friend class WebViewImpl;
   friend class ReportTimeSwapPromise;
   friend class WebFrameWidgetScrollContainerHitTest;
@@ -1164,6 +1168,9 @@ class CORE_EXPORT WebFrameWidgetImpl
   ComputeProximateCharacterBounds(
       const PositionWithAffinity& pivot_position) const;
 #endif  // BUILDFLAG(IS_WIN)
+
+  mojom::blink::InputCursorAnchorInfoPtr CalculateCursorAnchorInfo(
+      bool update_requested);
 
   // Stores the last cursor anchor info calculated for the currently focused
   // editable element.
