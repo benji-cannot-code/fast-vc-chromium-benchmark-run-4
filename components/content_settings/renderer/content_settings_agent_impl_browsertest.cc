644564b4acc884fa6d539bd5f4a861b658e2d72e
Shunya Shishido <sisidovski@chromium.org>
2026-07-14 08:18:23 -0700

    [ContentSettings] Add UMA metric for AllowStorageAccessSync
    This adds a metric to measure the synchronous IPC blocking time when
    the renderer asks the browser for storage permission status.
    
    Bug: 534183656
    Change-Id: I65e82c0b3830bd2b3b7ee7ab2e8e564d03fc9b3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8089831
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661873}

--- components/content_settings/renderer/content_settings_agent_impl_browsertest.cc
+++ components/content_settings/renderer/content_settings_agent_impl_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "components/content_settings/core/common/content_settings.h"
@@ -282,6 +283,7 @@ TEST_P(ContentSettingsAgentImplBrowserTest, DidBlockContentType) {
 // Tests that multiple invocations of AllowStorageAccessSync result in a single
 // IPC.
 TEST_P(ContentSettingsAgentImplBrowserTest, AllowStorageAccessSync) {
+  base::HistogramTester histogram_tester;
   // Load some HTML, so we have a valid security origin.
   LoadHTMLWithUrlOverride("<html></html>", "https://example.com/");
   MockContentSettingsAgentImpl mock_agent(GetMainRenderFrame());
@@ -289,6 +291,10 @@ TEST_P(ContentSettingsAgentImplBrowserTest, AllowStorageAccessSync) {
       blink::WebContentSettingsClient::StorageType::kLocalStorage);
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(1, mock_agent.allow_storage_access_count());
+  histogram_tester.ExpectTotalCount("ContentSettings.AllowStorageAccessSync",
+                                    1);
+  histogram_tester.ExpectBucketCount(
+      "ContentSettings.AllowStorageAccessSync.CacheHit", false, 1);
 
   // Accessing localStorage from the same origin again shouldn't result in a
   // new IPC.
@@ -296,6 +302,10 @@ TEST_P(ContentSettingsAgentImplBrowserTest, AllowStorageAccessSync) {
       blink::WebContentSettingsClient::StorageType::kLocalStorage);
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(1, mock_agent.allow_storage_access_count());
+  histogram_tester.ExpectTotalCount("ContentSettings.AllowStorageAccessSync",
+                                    2);
+  histogram_tester.ExpectBucketCount(
+      "ContentSettings.AllowStorageAccessSync.CacheHit", true, 1);
 }
 
 // Tests that multiple invocations of AllowStorageAccess result in a single IPC.
