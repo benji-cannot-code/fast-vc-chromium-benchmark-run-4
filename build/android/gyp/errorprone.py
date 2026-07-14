f36096453de913c76d738ccaad9132ed37d50546
Andrew Grieve <agrieve@chromium.org>
2026-07-14 12:09:22 -0700

    Android: Temporarily disable TimeUnitMismatch Error Prone check
    To allow for unnamed variables. Error Prone has already fixed the crash
    upstream, so this can be reverted after Error Prone rolls.
    
    Bug: 534779546
    Change-Id: I087ca33425e5864a95e60f641524d50920be932b
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8093111
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662064}

--- build/android/gyp/errorprone.py
+++ build/android/gyp/errorprone.py
@@ -85,6 +85,9 @@ ERRORPRONE_WARNINGS_TO_DISABLE = [
     'ThrowSpecificExceptions',
     'ThrowsUncheckedException',
     'TooManyParameters',
+
+    # TODO(crbug.com/534779546): Re-enabled after next Error Prone roll.
+    'TimeUnitMismatch',
     'TryFailRefactoring',
     'TypeParameterNaming',
     'UngroupedOverloads',
@@ -216,7 +219,6 @@ ERRORPRONE_WARNINGS_TO_ENABLE = [
     'MultiVariableDeclaration',
     'RedundantOverride',
     'StaticQualifiedUsingExpression',
-    'TimeUnitMismatch',
     'UnnecessaryStaticImport',
     'UseBinds',
     'WildcardImport',
