ef342d393972fd05dc68914486b63b50bb89bd4c
John Chen <johnchen@chromium.org>
2026-07-13 17:52:53 -0700

    [Perf] Prepare win-11-perf for OS upgrade
    Relax 'os' dimension while the OS is being upgraded.
    
    Bug: b:530232282
    Change-Id: If7cac87254cc28a46bd29aaad85173d9723eab52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8086670
    Auto-Submit: John Chen <johnchen@chromium.org>
    Reviewed-by: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Commit-Queue: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661528}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -1379,7 +1379,8 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            'os': 'Windows-11-22631.2428',
+            # TODO(b/530232282): Temporary relax 'os' dimension for OS upgrade.
+            'os': 'Windows-11',
             'gpu': '102b:0536-4.5.0.5',
             'synthetic_product_name': 'PowerEdge R350 (Dell Inc.)'
         },
@@ -1403,7 +1404,8 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            'os': 'Windows-11-22631.2428',
+            # TODO(b/530232282): Temporary relax 'os' dimension for OS upgrade.
+            'os': 'Windows-11',
             'gpu': '102b:0536-4.5.0.5',
             'synthetic_product_name': 'PowerEdge R350 (Dell Inc.)'
         },
