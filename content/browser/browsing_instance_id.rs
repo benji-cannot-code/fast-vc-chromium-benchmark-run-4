d7c9960da8e410fe1260a405a9b0a0769f9bd6c0
Liam Brady <lbrady@google.com>
2026-07-14 10:12:33 -0700

    Pass BrowsingInstanceId and ChildProcessId by value across Rust FFI.
    This CL adds Rust targets `content_browser` and `content_common` to
    define `BrowsingInstanceId` and `ChildProcessId` in that match their C++
    equivalents. These ID types are treated as `cxx::ExternType` trivial
    kinds and passed by value across the C++/Rust FFI boundary without
    wrapping/unwrapping integers. Existing uses of `BrowsingInstanceId`
    inside Rust `ChildProcessSecurityPolicyImpl` have been converted to use
    the new representations. While `ChildProcessId` is currently unused in
    Rust CPSP, it will be used in future CLs. These are being put in the
    non-public directories (even though the C++ equivalents are in public)
    until there is a use case for these to be publicly defined.
    
    Bug: 519701929, 522844976
    Change-Id: I58b856df650f5f1a46f6dfe7b2bf846ca16f9d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963305
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661961}

---
+++ content/browser/browsing_instance_id.rs
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#![allow(unsafe_code)]
+
+use std::num::NonZero;
+
+/// Matches C++ `content::BrowsingInstanceId`, wrapping `Option<NonZero<i32>>`
+/// to idiomatically represent valid vs. invalid (`0`) IDs. With
+/// `#[repr(transparent)]`, `None` has the memory bit pattern `0x00000000`,
+/// which matches the invalid value for `IdType32`.
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+pub struct BrowsingInstanceId(pub Option<NonZero<i32>>);
+
+// SAFETY: `content::BrowsingInstanceId` type is trivially movable and trivially
+// destructible (the underlying structure is simply an integer) as
+// enforced/documented by `browsing_instance_id_unittest.cc`.
+unsafe impl cxx::ExternType for BrowsingInstanceId {
+    type Id = cxx::type_id!("content::BrowsingInstanceId");
+    type Kind = cxx::kind::Trivial;
+}
