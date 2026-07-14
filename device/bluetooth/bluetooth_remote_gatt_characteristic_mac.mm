7023ee8246f75a53638baaa023b937cef883cbc8
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-13 23:29:51 -0700

    [unsafe-buffers] Fix easy UNSAFE_TODOs in device/
    Remove UNSAFE_TODOs by using safer alternatives:
    - Use base::apple::NSDataToSpan to avoid raw pointer arithmetic on NSData.
    - Use std::array instead of raw arrays in GamepadDeviceMac.
    - Rewrite CanonicalStringToBLUETOOTH_ADDRESS using HexStringToBytes and
      std::reverse_copy with span.
    
    Change-Id: Id97ffa839909bca90f48c2447bf777365e7e8740
    Bug: 501112808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072243
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661642}

--- device/bluetooth/bluetooth_remote_gatt_characteristic_mac.mm
+++ device/bluetooth/bluetooth_remote_gatt_characteristic_mac.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/bluetooth_remote_gatt_characteristic_mac.h"
 
+#include "base/apple/foundation_util.h"
 #include "base/compiler_specific.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
@@ -310,8 +311,8 @@ void BluetoothRemoteGattCharacteristicMac::DidUpdateValue(NSError* error) {
 
 void BluetoothRemoteGattCharacteristicMac::UpdateValue() {
   NSData* nsdata_value = [cb_characteristic_ value];
-  const uint8_t* buffer = static_cast<const uint8_t*>(nsdata_value.bytes);
-  value_.assign(buffer, UNSAFE_TODO(buffer + nsdata_value.length));
+  auto span = base::apple::NSDataToSpan(nsdata_value);
+  value_.assign(span.begin(), span.end());
 }
 
 void BluetoothRemoteGattCharacteristicMac::DidWriteValue(NSError* error) {
