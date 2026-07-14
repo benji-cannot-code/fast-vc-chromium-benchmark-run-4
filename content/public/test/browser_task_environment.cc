a7fc34bb4cbe967ca29224e7453a5224273671b3
Joe Mason <joenotcharles@google.com>
2026-07-14 07:51:02 -0700

    Don't signal startup complete after shutdown has started
    Code that runs in SetBrowserStartupIsComplete may access state that's no
    longer valid. Before StartupInProgressRef was added, this was ensured by
    a check for PerformanceManager::IsAvailable() in
    StartupObserver::OnStartupComplete, but that's no longer the only path
    that can trigger SetBrowserStartupIsComplete.
    
    Bug: 528419929
    Change-Id: I41389c0974b802c5b5f8674e8d8f46b2492e8480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8087297
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661857}

--- content/public/test/browser_task_environment.cc
+++ content/public/test/browser_task_environment.cc
@@ -246,4 +246,8 @@ void BrowserTaskEnvironment::RunIOThreadUntilIdle() {
   io_thread_idle.Wait();
 }
 
+void BrowserTaskEnvironment::ShutdownBrowserTaskExecutor() {
+  BrowserTaskExecutor::Shutdown();
+}
+
 }  // namespace content
