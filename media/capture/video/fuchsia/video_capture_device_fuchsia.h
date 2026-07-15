3670edc5ecea0d1026315a6ef53eb8eefeea11bb
Guocheng Wei <guochengwei@chromium.org>
2026-07-14 18:09:37 -0700

    [fuchsia] Track Fuchsia camera mute state via fuchsia.camera3.Device
    Bug: 461575821
    Change-Id: Ia476645262b824d3d7aa9468ba57a499fae3002c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915188
    Commit-Queue: Guocheng Wei <guochengwei@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662308}

--- media/capture/video/fuchsia/video_capture_device_fuchsia.h
+++ media/capture/video/fuchsia/video_capture_device_fuchsia.h
@@ -70,6 +70,12 @@ class CAPTURE_EXPORT VideoCaptureDeviceFuchsia final
   // Callback for WatchOrientation().
   void OnWatchOrientationResult(fuchsia::camera3::Orientation orientation);
 
+  // Watches for camera mute state updates and notifies the client accordingly.
+  void WatchMuteState();
+
+  // Callback for WatchMuteState().
+  void OnWatchMuteStateResult(bool software_muted, bool hardware_muted);
+
   // Watches for sysmem buffer collection updates from the camera.
   void WatchBufferCollection();
 
@@ -110,6 +116,7 @@ class CAPTURE_EXPORT VideoCaptureDeviceFuchsia final
   base::TimeTicks start_time_;
 
   bool started_ = false;
+  bool is_muted_ = false;
 
   size_t frames_received_ = 0;
 
