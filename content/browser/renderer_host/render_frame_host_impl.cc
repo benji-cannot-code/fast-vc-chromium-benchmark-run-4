1972410e96ba2120323a58f40ff3e144b707ee92
Tzarial <zork@google.com>
2026-07-15 16:14:32 -0700

    [agy][content] Verify sender gesture for delegated capabilities
    Add browser-side validation in RouteMessageEvent to ensure the sender
    frame had user activation when a postMessage delegates a capability.
    
    To support asynchronous consumption of user activation (where WPT tests
    synchronously consume the gesture inside DOMWindow::postMessage in the
    renderer prior to message routing, sending an asynchronous IPC to
    consume it in the browser), RenderFrameHostImpl tracks the timestamp
    when its transient user activation was last consumed. RouteMessageEvent
    allows delegation if the sender frame has active user activation or has
    consumed it within a short grace period.
    
    Fixed: 518125889
    Test: SecurityExploitBrowserTest.PostMessageDelegatedCapabilityWithoutUserActivation
    Change-Id: Id36fdbbcf637676abfcecfba1b31067cfeba1e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7994936
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Tzarial <zork@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662927}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -7668,10 +7668,13 @@ void RenderFrameHostImpl::ClearUserActivation() {
   user_activation_state_.Clear();
   history_user_activation_state_.Clear();
   GetMainFrame()->honor_sticky_activation_for_history_intervention_ = true;
+  last_user_activation_consumed_time_ = base::TimeTicks();
 }
 
 void RenderFrameHostImpl::ConsumeTransientUserActivation() {
-  user_activation_state_.ConsumeIfActive();
+  if (user_activation_state_.ConsumeIfActive()) {
+    last_user_activation_consumed_time_ = base::TimeTicks::Now();
+  }
 }
 
 void RenderFrameHostImpl::ActivateUserActivation(
@@ -11392,6 +11395,9 @@ void RenderFrameHostImpl::DidChangeSrcDoc(
 
 void RenderFrameHostImpl::ReceivedDelegatedCapability(
     blink::mojom::DelegatedCapability delegated_capability) {
+  if (lifecycle_state() != LifecycleStateImpl::kActive) {
+    return;
+  }
   // Every delegated capability that is checked or consumed on the browser side
   // needs to be (a) activated here and (b) consumed when RFH handles the
   // capability.
