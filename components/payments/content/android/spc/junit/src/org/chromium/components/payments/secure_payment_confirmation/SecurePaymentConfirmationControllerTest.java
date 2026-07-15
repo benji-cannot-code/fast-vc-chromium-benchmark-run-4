bb68d79911024724f8e3d895c03ad9f53d4bb6f2
Jayden Nikifork <nikifork@google.com>
2026-07-15 11:39:39 -0700

    [ux][spc] Round corners of payment instrument icon
    java code, it's difficult to validate this via unit or integration
    tests. Skia gold tests should suffice.
    
    Before: http://screen/api/BzGkXtpkBCu8ZBd.png
    After: http://screen/api/BYMmonAToADoJQR.png
    Figma: https://www.figma.com/design/Zdsg8nir5FvMPTF8k1IDpi/Secure-Payment-Confirmation?node-id=4387-3752
    Low-Coverage-Reason: HARD_TO_TEST We are modifying an image bitmap via
    Bug: 426515606
    Change-Id: Id7c882225547b19f76d87d7f443c02a67082fc88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071298
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Commit-Queue: Jayden Nikifork <nikifork@google.com>
    Reviewed-by: Xuehui Chen <xuehuichen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662762}

--- components/payments/content/android/spc/junit/src/org/chromium/components/payments/secure_payment_confirmation/SecurePaymentConfirmationControllerTest.java
+++ components/payments/content/android/spc/junit/src/org/chromium/components/payments/secure_payment_confirmation/SecurePaymentConfirmationControllerTest.java
@@ -29,6 +29,8 @@ import android.graphics.drawable.Drawable;
 import android.text.SpannableString;
 import android.text.style.ClickableSpan;
 
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -231,7 +233,9 @@ public class SecurePaymentConfirmationControllerTest {
                         mPayeeOrigin, SchemeDisplay.OMIT_HTTP_AND_HTTPS),
                 storeItem.model.get(ItemProperties.SECONDARY_TEXT));
         ListItem paymentItem = itemList.get(1);
-        assertEquals(mPaymentIcon, paymentItem.model.get(ItemProperties.ICON));
+        assertEquals(
+                ((BitmapDrawable) mPaymentIcon).getBitmap(),
+                ((RoundedBitmapDrawable) paymentItem.model.get(ItemProperties.ICON)).getBitmap());
         assertEquals(
                 mPaymentInstrumentLabelPrimary, paymentItem.model.get(ItemProperties.PRIMARY_TEXT));
         assertEquals(
