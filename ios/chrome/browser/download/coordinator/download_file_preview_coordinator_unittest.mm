f8181e120fe4d9e4a2760b6d51f37f1779cc3fd3
Federica Germinario <fedegermi@google.com>
2026-07-15 09:59:53 -0700

    [ios] Tentative fix for test DownloadFilePreviewCoordinatorTest.Stop
    Increase the timeout from 4 to 10 seconds to fix test failure on ios27
    bots.
    
    Bug: 527963359
    Change-Id: I62099aaf380e4fe9c1ee1a0733ec254456556f17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8099482
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662693}

--- ios/chrome/browser/download/coordinator/download_file_preview_coordinator_unittest.mm
+++ ios/chrome/browser/download/coordinator/download_file_preview_coordinator_unittest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/path_service.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
+#import "base/test/test_timeouts.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/test/scoped_key_window.h"
@@ -135,7 +136,7 @@ TEST_F(DownloadFilePreviewCoordinatorTest, Stop) {
   [coordinator_ stop];
 
   // Wait for dismissal.
-  EXPECT_TRUE(WaitUntilConditionOrTimeout(kWaitForUIElementTimeout, ^{
+  EXPECT_TRUE(WaitUntilConditionOrTimeout(TestTimeouts::action_timeout(), ^{
     return base_view_controller_.presentedViewController == nil;
   }));
 }
