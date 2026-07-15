0c10f679cfffd0bbf06a8b1206435b1e1e624d0a
Chris Lu <thegreenfrog@google.com>
2026-07-15 15:04:36 -0700

    [iOS] Fix default logic initial omnibox position preference
    With kChromeNextIA disabled, even if the omnibox preference is set to
    bottom, the initial position was always top. This was caused by the
    ownership check in transitionOmniboxToToolbarType: returning early when
    currentBrowserProvider.browser is nil during early startup.
    
    This CL updates the check to allow updates during startup if there is
    no active browser.
    
    Original regression: https://chromium-review.git.corp.google.com/c/chromium/src/+/7977939
    
    Bug: 535255250
    Change-Id: Id9f46c290b2e5db816a62af9eacf963999988fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102382
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662889}

--- ios/chrome/browser/toolbar/coordinator/main_toolbar_coordinator.mm
+++ ios/chrome/browser/toolbar/coordinator/main_toolbar_coordinator.mm
@@ -1104,9 +1104,10 @@ inline LayoutStateToolbarPassKey PassKey() {
 
   // Only the visible coordinator (normal vs. incognito) is allowed to update
   // the shared LayoutState.
-  if (self.browser !=
-      self.browser->GetSceneState()
-          .browserProviderInterface.currentBrowserProvider.browser) {
+  Browser* activeBrowser = self.browser->GetSceneState()
+                               .browserProviderInterface
+                               .currentBrowserProvider.browser;
+  if (activeBrowser && self.browser != activeBrowser) {
     return;
   }
 
