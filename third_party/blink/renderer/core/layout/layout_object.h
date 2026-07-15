dab7cfb28dd512c5b4401ad61282b69e4548225d
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-07-14 18:03:13 -0700

    [layout] Remove LayoutObject::ScheduleRelayout
    This function adds complexity compared to just inlining the appropriate
    layout scheduling calls.
    
    It also removes a duplicate "IsRooted" check.
    
    There should be no behaviour change.
    
    Change-Id: I70cbaab8211a430ef469365cf64c15463808fda5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8090782
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662303}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -3656,8 +3656,6 @@ class CORE_EXPORT LayoutObject : public GarbageCollected<LayoutObject>,
                                          const LayoutBoxModelObject* ancestor,
                                          MapCoordinatesFlags = 0) const;
 
-  void ScheduleRelayout();
-
   void AddAsImageObserver(StyleImage*);
   void RemoveAsImageObserver(StyleImage*);
 
