57a5f134cf0a074421536ea364b53fe31dfb010e
Emily Shack <emshack@chromium.org>
2026-07-15 10:18:06 -0700

    [GlowUp] Round read anything icons (1/2)
    This CL rounds 15 icons to keep CL size manageable, the other icons will
    be addressed in follow-ups as read_anything/app/icons.html is a large
    file.
    
    See go/chrome-icon-modernization-dd. This is part of a larger effort to
    standardize icon naming, enabling easier bulk updates in the future, as
    well as applying 100% roundedness to all existing icons.
    
    Bug: 511303545
    Change-Id: Ibdaee77f0695dbde9eaf221f0d259c045337c9d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8077180
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662709}

--- chrome/browser/resources/side_panel/read_anything/menus/grouped_action_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/menus/grouped_action_menu.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '/strings.m.js';
 import '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import '//resources/cr_elements/cr_icon/cr_icon.js';
 import '//resources/cr_elements/cr_lazy_render/cr_lazy_render_lit.js';
@@ -11,6 +12,7 @@ import type {CrActionMenuElement} from '//resources/cr_elements/cr_action_menu/c
 import type {CrLazyRenderLitElement} from '//resources/cr_elements/cr_lazy_render/cr_lazy_render_lit.js';
 import {WebUiListenerMixinLit} from '//resources/cr_elements/web_ui_listener_mixin_lit.js';
 import {assert} from '//resources/js/assert.js';
+import {loadTimeData} from '//resources/js/load_time_data.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {ShowAtConfigPrefs} from '../content/read_anything_types.js';
@@ -49,6 +51,7 @@ export class GroupedActionMenuElement extends GroupedActionMenuElementBase {
       label: {type: String},
       nonModal: {type: Boolean},
       closeOnClick: {type: Boolean},
+      webuiRoundedIconsEnabled_: {type: Boolean},
     };
   }
 
@@ -58,6 +61,8 @@ export class GroupedActionMenuElement extends GroupedActionMenuElementBase {
 
   // Initializing to random value, but this is set by the parent.
   accessor label: string = '';
+  protected accessor webuiRoundedIconsEnabled_: boolean =
+      loadTimeData.getBoolean('webuiRoundedIconsEnabled');
 
   open(anchor: HTMLElement, showAtConfig?: ShowAtConfigPrefs) {
     openMenu(
