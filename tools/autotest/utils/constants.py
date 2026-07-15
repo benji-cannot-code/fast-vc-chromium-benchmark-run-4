5d122e422dea7b00f2c48c75626f2d06c8093fc1
Egor Pasko <pasko@chromium.org>
2026-07-15 09:13:30 -0700

    autotest: Remove a few line-too-long presubmit warnings
    In tools/autotest/utils/constants.py a few "longer than 80 characters"
    warnings are about strings holding full path build targets. Breaking
    them via implicit string concatenation would make it less readable.
    
    Instead disable the warning for the whole TEST_TARGET_ALLOWLIST
    definition.
    
    Eliminate a couple of other warnings using more traditional python line
    splitting.
    
    Bug: None
    Change-Id: Ie8685eb0bbd23a4fb58b0874b3bbafea06025273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8098206
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662653}

--- tools/autotest/utils/constants.py
+++ tools/autotest/utils/constants.py
@@ -24,6 +24,7 @@ DEPOT_TOOLS_DIR: Path = SRC_DIR / 'third_party' / 'depot_tools'
 
 # Some test suites use suffixes that would also match non-test-suite targets.
 # Those test suites should be manually added here.
+# pylint: disable=line-too-long
 TEST_TARGET_ALLOWLIST: list[str] = [
 
     # The tests below this line were output from the ripgrep command just below:
@@ -113,9 +114,11 @@ TEST_TARGET_ALLOWLIST: list[str] = [
     '//third_party/rapidhash:rapidhash_fuzztests',
     '//ui/ozone:ozone_integration_tests',
 ]
+# pylint: disable=line-too-long
+
 r"""
- You can run this command to find test targets that do not match _TEST_TARGET_SUFFIXES,
- and use it to update _TEST_TARGET_ALLOWLIST.
+ You can run this command to find test targets that do not match
+ _TEST_TARGET_SUFFIXES, and use it to update _TEST_TARGET_ALLOWLIST.
 rg '^(instrumentation_test_runner|test)\("([^"]*)' -o -g'BUILD.gn' -r'$2' -N \
   | rg -v '(_browsertests|_perftests|_wpr_tests|_unittests)$' \
   | rg '^(.*)/BUILD.gn(.*)$' -r'\'//$1$2\',' \
@@ -134,7 +137,8 @@ PREF_MAPPING_FILE_PATTERN: str = re.escape(
     r'/') + r'.*\.json'
 
 # `rg` always uses forward slashes for globs, even on Windows.
-PREF_MAPPING_FILE_NAME_GLOB: str = '*components/policy/test/data/pref_mapping/*.json'
+PREF_MAPPING_FILE_NAME_GLOB: str = (
+    '*components/policy/test/data/pref_mapping/*.json')
 GTEST_FILE_NAME_GLOB: str = '*{test,tests}*.{cc,mm,java}'
 
 # Regex version of `(PREF_MAPPING_FILE_GLOB) | (GTEST_FILE_GLOB)`
