3d210ac1aa47f6c192808aee4fc3f32f1640d53b
Masa Fujita <massan@google.com>
2026-07-14 19:10:03 -0700

    extensions: Fix crash on drilldown navigation
    This CL fixes a crash that happens when using drilldown navigation for
    extension injected menus. We were using an empty PropertyModel to create
    the dividers. ModelListAdapter tries to recycle views, so we should
    ensure that all dividers use the same set of keys
    (ListSectionDividerProperties.ALL_KEYS). We can achieve this by using
    the proper divider builder.
    
    Bug: 433410990
    Change-Id: I82ce225d09d0da7f21d6b02ad674f83f9612cb24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8063417
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662329}

--- chrome/android/junit/src/org/chromium/chrome/browser/contextmenu/ContextMenuMediatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/contextmenu/ContextMenuMediatorTest.java
@@ -92,7 +92,11 @@ public class ContextMenuMediatorTest {
     public void setup() {
         mMediator =
                 new ContextMenuMediator(
-                        mActivity, mHeaderCoordinator, mClickCallback, mDismissDialog);
+                        mActivity,
+                        mHeaderCoordinator,
+                        /* isIncognito= */ false,
+                        mClickCallback,
+                        mDismissDialog);
 
         mListItemWithModelClickCallback =
                 new ListItem(
