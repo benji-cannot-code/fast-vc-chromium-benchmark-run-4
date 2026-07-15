d372270a83a8377910407dc04ae39f95f22d9bb5
John Chen <johnchen@chromium.org>
2026-07-15 10:12:47 -0700

    [Perf] Update `os` dimension for win-11-perf
    These bots just finished an OS upgrade. Updating the `os` dimension to
    match the new OS version.
    
    Bug: b:530232282
    Change-Id: I0fe88fd90dd038618913cfb69739ab75a41e83a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8093814
    Auto-Submit: John Chen <johnchen@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662704}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -1379,8 +1379,7 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            # TODO(b/530232282): Temporary relax 'os' dimension for OS upgrade.
-            'os': 'Windows-11',
+            'os': 'Windows-11-26200',
             'gpu': '102b:0536-4.5.0.5',
             'synthetic_product_name': 'PowerEdge R350 (Dell Inc.)'
         },
@@ -1404,8 +1403,7 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            # TODO(b/530232282): Temporary relax 'os' dimension for OS upgrade.
-            'os': 'Windows-11',
+            'os': 'Windows-11-26200',
             'gpu': '102b:0536-4.5.0.5',
             'synthetic_product_name': 'PowerEdge R350 (Dell Inc.)'
         },
