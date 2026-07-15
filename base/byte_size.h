c8f5a351841766f33cbf27fc267a65607eeefb4e
Joe Mason <joenotcharles@google.com>
2026-07-15 14:54:27 -0700

    Remove base::ByteCount
    All uses have been replaced with base::ByteSize.
    
    NO_IFTTT=Macros aren't affected
    
    Bug: 448661443
    Change-Id: Icb1bcc97e2e2449d3bb40a37277d6d58080f95f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8067362
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662884}

--- base/byte_size.h
+++ base/byte_size.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iosfwd>
 
 #include "base/base_export.h"
-#include "base/byte_count.h"
 #include "base/numerics/checked_math.h"
 #include "base/numerics/safe_conversions.h"
 
@@ -183,16 +182,6 @@ class BASE_EXPORT ByteSize : public internal::ByteSizeBase {
   static constexpr ByteSize FromByteSizeDelta(ByteSizeDelta delta);
   constexpr ByteSizeDelta AsByteSizeDelta() const;
 
-  // Converts ByteSize to and from a deprecated ByteCount. Converting from a
-  // ByteCount CHECK's that it's in range (ie. non-negative). Converting to a
-  // ByteCount always succeeds.
-  static constexpr ByteSize FromDeprecatedByteCount(ByteCount count) {
-    return ByteSize(count.InBytesUnsigned());
-  }
-  constexpr ByteCount AsDeprecatedByteCount() const {
-    return ByteCount::FromUnsigned(InBytes());
-  }
-
   // Returns a value corresponding to the "maximum" number of bytes possible.
   // Useful as a constant to mean "unlimited".
   static constexpr ByteSize Max() {
@@ -312,14 +301,6 @@ class BASE_EXPORT ByteSizeDelta : public internal::ByteSizeBase {
     return ByteSize(checked_cast<uint64_t>(InBytes()));
   }
 
-  // Converts ByteSizeDelta to and from a deprecated ByteCount. Always succeeds.
-  static constexpr ByteSizeDelta FromDeprecatedByteCount(ByteCount count) {
-    return ByteSizeDelta(count.InBytes());
-  }
-  constexpr ByteCount AsDeprecatedByteCount() const {
-    return ByteCount(InBytes());
-  }
-
   // Returns a value corresponding to the "maximum" (positive) number of bytes
   // possible. Useful as a constant to mean "unlimited" in the positive
   // direction.
@@ -431,8 +412,7 @@ class BASE_EXPORT ByteSizeDelta : public internal::ByteSizeBase {
 // be converted to CheckedNumeric BEFORE multiplying to detect overflows, while
 // floats must be converted AFTER multiplying to avoid premature truncation.
 //
-// TODO(crbug.com/448661443): After all uses of KiB, etc, are migrated to
-// explicit signed/ unsigned, rename KiBU to KiB.
+// TODO(crbug.com/448661443): Rename KiBU, etc, to KiB, etc.
 
 template <typename T>
   requires std::integral<T>
--- base/metrics/histogram_functions_internal_overloads.h
+++ base/metrics/histogram_functions_internal_overloads.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/base_export.h"
-#include "base/byte_count.h"
 #include "base/byte_size.h"
 #include "base/check_op.h"
 #include "base/metrics/histogram.h"
@@ -135,25 +134,16 @@ BASE_EXPORT void UmaHistogramMicrosecondsTimes(const char* name,
 // LINT.IfChange(UmaHistogramMemory)
 BASE_EXPORT void UmaHistogramMemoryKB(const std::string& name, int sample);
 BASE_EXPORT void UmaHistogramMemoryKB(const char* name, int sample);
-BASE_EXPORT void UmaHistogramMemoryKB(const std::string& name,
-                                      ByteCount sample);
-BASE_EXPORT void UmaHistogramMemoryKB(const char* name, ByteCount sample);
 BASE_EXPORT void UmaHistogramMemoryKB(const std::string& name, ByteSize sample);
 BASE_EXPORT void UmaHistogramMemoryKB(const char* name, ByteSize sample);
 
 BASE_EXPORT void UmaHistogramMemoryMB(const std::string& name, int sample);
 BASE_EXPORT void UmaHistogramMemoryMB(const char* name, int sample);
-BASE_EXPORT void UmaHistogramMemoryMB(const std::string& name,
-                                      ByteCount sample);
-BASE_EXPORT void UmaHistogramMemoryMB(const char* name, ByteCount sample);
 BASE_EXPORT void UmaHistogramMemoryMB(const std::string& name, ByteSize sample);
 BASE_EXPORT void UmaHistogramMemoryMB(const char* name, ByteSize sample);
 
 BASE_EXPORT void UmaHistogramMemoryLargeMB(const std::string& name, int sample);
 BASE_EXPORT void UmaHistogramMemoryLargeMB(const char* name, int sample);
-BASE_EXPORT void UmaHistogramMemoryLargeMB(const std::string& name,
-                                           ByteCount sample);
-BASE_EXPORT void UmaHistogramMemoryLargeMB(const char* name, ByteCount sample);
 BASE_EXPORT void UmaHistogramMemoryLargeMB(const std::string& name,
                                            ByteSize sample);
 BASE_EXPORT void UmaHistogramMemoryLargeMB(const char* name, ByteSize sample);
