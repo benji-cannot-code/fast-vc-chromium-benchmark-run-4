d2d232f5e3a4ea6b45e4cfe5cd90b98ade9e5915
Sylvain Defresne <sdefresne@chromium.org>
2026-07-15 03:31:29 -0700

    [ios] Split //ios/chrome/browser/scoped_ui_blocker/ui_bundled target
    Move the headers defining the protocols in a separate target. This
    will allow the implementation of ScopedUIBlocker to depend on the
    AppState, ProfileState and SceneState headers without introducing
    circular dependencies.
    
    Bug: 519105565
    Change-Id: I23fa6361a4835d78a11b6adccf9315b48378aeed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071102
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662484}

--- ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.mm
+++ ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.h"
 
 #import "base/check.h"
+#import "base/logging.h"
 #import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_manager.h"
 #import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_target.h"
 
--- ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_target.h
+++ ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_target.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.h"
+#import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_extent.h"
 
 @protocol UIBlockerManager;
 @class UIScene;
