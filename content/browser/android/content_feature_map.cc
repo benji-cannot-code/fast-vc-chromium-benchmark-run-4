7d4d6f6c5721efca7dcd2101e32c3b0f7d693921
Vaibhav Devmurari <vdevmurari@google.com>
2026-07-15 15:27:08 -0700

    Flag guard the code that removes the workaround for setLocalFocus
    Original CL: https://chromium-review.googlesource.com/8019573
    
    Bug: 525387925
    Change-Id: Ib5c7393832fb0672a65a881c43167c72efe8e8d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8099402
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Vaibhav Devmurari <vdevmurari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662899}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -58,6 +58,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kAndroidMediaInsertion,
     &features::kAndroidPkAutocorrectUnderline,
     &features::kAndroidPkAutocorrectUnderlineV2,
+    &features::kAndroidRemoveSetLocalFocusWorkaroundOnBaklava,
     &features::kAndroidBlockMisspellingSuggestionSpanInCompositionMode,
     &features::kAndroidBlockGrammarSuggestionSpanInCompositionMode,
     &features::kStrictHighRankProcessLRU,
