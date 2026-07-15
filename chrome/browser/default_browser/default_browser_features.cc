63e28ba0538100ddfd52e4bb1ebc71b5345c009d
Atharv Maan <atharvmaan@chromium.org>
2026-07-15 08:07:40 -0700

    Add a flag to gate continuous docking logic in Visual Guided Setter
    Bug: 533024669
    Change-Id: I37c741ec54a060526c13ea2721db3f6796b19632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8074962
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662604}

--- chrome/browser/default_browser/default_browser_features.cc
+++ chrome/browser/default_browser/default_browser_features.cc
@@ -36,6 +36,14 @@ bool IsDefaultBrowserPromptSurfacesEnabled() {
 #endif
 }
 
+bool IsVisualGuidedSetterDockingEnabled() {
+#if BUILDFLAG(IS_WIN)
+  return base::FeatureList::IsEnabled(kVisualGuidedSetterDocking);
+#else
+  return false;
+#endif
+}
+
 DefaultBrowserPromptSurface GetDefaultBrowserPromptSurface() {
   if (!IsDefaultBrowserPromptSurfacesEnabled()) {
     return DefaultBrowserPromptSurface::kInfobar;
@@ -88,6 +96,8 @@ BASE_FEATURE(kDefaultBrowserChangedOsNotification,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDefaultBrowserSetterSelection, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kVisualGuidedSetterDocking, base::FEATURE_DISABLED_BY_DEFAULT);
 
 constexpr inline auto kDefaultBrowserSetterSelectionOption =
     std::to_array<base::FeatureParam<DefaultBrowserSetterType>::Option>(
