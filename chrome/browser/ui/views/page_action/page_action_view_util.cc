b2d0006e99f9b5acbc06fc3351a5447b2bd44e49
dljames <dljames@chromium.org>
2026-07-15 15:46:38 -0700

    [BAM] Plumb SidePanelOpenTrigger when invoking side panels from Page Actions
    This child CL of side-panel-trigger-via-context plumbs kSidePanelOpenTriggerKey
    onto the ActionInvocationContext whenever a side panel action item is invoked
    via PageActionView or WebUIPageActionControl. This resolves side panel trigger
    CHECK failures (-1 vs 0) when page action icons (e.g. Read Anything, Lens) are
    activated under UseActionsForBrowserCommands.
    
    TAG=agy
    CONV=33ba8294-f5bc-4898-ac14-42a6c66dee55
    
    Bug: 525118424
    Change-Id: Ib020f75b1a4daaab775b86a584ff958622226a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8094465
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662912}

---
+++ chrome/browser/ui/views/page_action/page_action_view_util.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/page_action/page_action_view_util.h"
+
+#include "chrome/browser/ui/actions/chrome_action_id.h"
+
+namespace page_actions {
+
+std::optional<SidePanelOpenTrigger> GetSidePanelOpenTriggerForPageAction(
+    std::optional<actions::ActionId> action_id) {
+  if (!action_id.has_value()) {
+    return std::nullopt;
+  }
+  if (action_id == kActionSidePanelShowReadAnything) {
+    return SidePanelOpenTrigger::kReadAnythingOmniboxChip;
+  }
+  if (action_id == kActionSidePanelShowLensOverlayResults) {
+    return SidePanelOpenTrigger::kSideSearchPageAction;
+  }
+  if (action_id == kActionSidePanelShowContextualTasks) {
+    return SidePanelOpenTrigger::kContextualTasks;
+  }
+  return std::nullopt;
+}
+
+}  // namespace page_actions
--- chrome/browser/ui/views/page_action/webui_page_action_control.cc
+++ chrome/browser/ui/views/page_action/webui_page_action_control.cc
@@ -17,7 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/page_action/page_action_controller.h"
 #include "chrome/browser/ui/page_action/page_action_model.h"
 #include "chrome/browser/ui/page_action/page_action_triggers.h"
+#include "chrome/browser/ui/side_panel/side_panel_action_callback.h"
+#include "chrome/browser/ui/side_panel/side_panel_enums.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "chrome/browser/ui/views/page_action/page_action_view_util.h"
 #include "chrome/browser/ui/views/toolbar/webui_toolbar_web_view.h"
 #include "components/tabs/public/tab_interface.h"
 #include "mojo/public/mojom/base/error.mojom.h"
@@ -343,7 +346,7 @@ void WebUIPageActionControl::WebUIPageActionDelegate::NotifyClick(
     PageActionTrigger trigger) {
   click_callback_.Run(trigger);
 
-  action_item_->InvokeAction(
+  auto builder =
       actions::ActionInvocationContext::Builder()
           .SetProperty(
               page_actions::kPageActionTriggerKey,
@@ -354,8 +357,15 @@ void WebUIPageActionControl::WebUIPageActionDelegate::NotifyClick(
               page_actions::kPageActionEntryPointKey,
               static_cast<
                   std::underlying_type_t<page_actions::PageActionEntryPoint>>(
-                  page_actions::PageActionEntryPoint::kSuggestionChip))
-          .Build());
+                  page_actions::PageActionEntryPoint::kSuggestionChip));
+  if (auto side_panel_trigger =
+          GetSidePanelOpenTriggerForPageAction(action_item_->GetActionId())) {
+    builder = std::move(builder).SetProperty(
+        kSidePanelOpenTriggerKey,
+        static_cast<std::underlying_type_t<SidePanelOpenTrigger>>(
+            *side_panel_trigger));
+  }
+  action_item_->InvokeAction(std::move(builder).Build());
 }
 
 void WebUIPageActionControl::WebUIPageActionDelegate::
