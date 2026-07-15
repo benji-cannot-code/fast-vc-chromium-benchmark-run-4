237ea2bf4c9185cb981432cd341302f0565bcb82
manukh <manukh@chromium.org>
2026-07-15 14:33:40 -0700

    [omnibox][keyword] Keep "+ Add" context button visible in keyword mode
    Originally, crrev.com/c/7153622 added logic to hide the button when in
    keyword mode.
    
    Original behavior:
    https://screencast.googleplex.com/cast/NjY5MDgyNTgwNjI4Mjc1MnwxN2UxYTk2Zi04OQ
    
    But it looked jarring to show/hide the button and change the popup
    height as the user arrowed through keyword and non-keyword matches.
    
    This CL reverts to always showing the button regardless of keyword
    state. And it cleans up mojom `page::SetKeywordSelected()` and
    `OmniboxEditModel::Observer::OnKeywordStateChanged()` as they're not
    needed otherwise.
    
    The right-hand side AIM entrypoint will remain hidden when in keyword
    mode, because it is confusing to show a button that is sometimes
    tabbable and sometimes not. Also because showing & hiding it isn't as
    jarring.
    
    Bug: b:535194460 b:460108925
    Change-Id: I5947802a0292a00815c2fad00c2cd92c4226d067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8100272
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Caitlin Chen <caitc@google.com>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662867}

--- chrome/browser/resources/omnibox_popup/app.ts
+++ chrome/browser/resources/omnibox_popup/app.ts
@@ -118,7 +118,6 @@ export class OmniboxPopupAppElement extends I18nMixinLit
         reflect: true,
       },
 
-      isInKeywordMode_: {type: Boolean},
       result_: {type: Object},
       searchboxLayoutMode_: {reflect: true, type: String},
       showContextEntrypoint_: {
@@ -144,7 +143,6 @@ export class OmniboxPopupAppElement extends I18nMixinLit
       canShowSecondarySideMediaQueryList.matches;
   accessor hasSecondarySide: boolean = false;
   accessor isDebug: boolean = false;
-  protected accessor isInKeywordMode_: boolean = false;
   protected accessor hasVisibleMatches_: boolean = false;
   protected accessor result_: AutocompleteResult|null = null;
   protected accessor searchboxLayoutMode_: string =
@@ -205,10 +203,6 @@ export class OmniboxPopupAppElement extends I18nMixinLit
           .addListener(this.onAutocompleteResultChanged_.bind(this)),
       this.searchboxBrowserProxy_.callbackRouter.updateSelection.addListener(
           this.onUpdateSelection_.bind(this)),
-      this.searchboxBrowserProxy_.callbackRouter.setKeywordSelected.addListener(
-          (isKeywordSelected: boolean) => {
-            this.isInKeywordMode_ = isKeywordSelected;
-          }),
       this.searchboxBrowserProxy_.callbackRouter.updateLensSearchEligibility
           .addListener((eligible: boolean) => {
             this.isLensSearchEligible_ = this.isLensSearchEnabled_ && eligible;
@@ -283,7 +277,6 @@ export class OmniboxPopupAppElement extends I18nMixinLit
 
     if (changedPrivateProperties.has('isAimPopupEligible_') ||
         changedPrivateProperties.has('searchboxLayoutMode_') ||
-        changedPrivateProperties.has('isInKeywordMode_') ||
         changedPrivateProperties.has('result_') ||
         changedPrivateProperties.has('isLensSearchEligible_')) {
       this.showContextEntrypoint_ = this.computeShowContextEntrypoint_();
@@ -310,8 +303,7 @@ export class OmniboxPopupAppElement extends I18nMixinLit
   }
 
   private computeShowContextEntrypoint_(): boolean {
-    if (this.hideContextButton_ || !this.isAimPopupEligible_ ||
-        this.isInKeywordMode_) {
+    if (this.hideContextButton_ || !this.isAimPopupEligible_) {
       return false;
     }
 
