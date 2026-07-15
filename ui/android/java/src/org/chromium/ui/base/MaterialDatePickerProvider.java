29dcdc6b7c25e9be5f438e41e23bd6a186318cbf
Fuhsin Liao <fuhsinliao@google.com>
2026-07-14 22:24:10 -0700

    [DatePicker] Add initial time to the DatePicker
    Demo: https://screenshot.googleplex.com/5dyvnA5EZ5UXsWZ
    
    To match desktop chrome experience and to improve user experiences, this
    cl adds a feature to show the previous selected date in the "Select a
    date" text area.
    
    Bug: 483569814
    Change-Id: I1a94f6d4c04e4f336d6d8e750c5a71b404473de9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8090111
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662362}

--- ui/android/java/src/org/chromium/ui/base/MaterialDatePickerProvider.java
+++ ui/android/java/src/org/chromium/ui/base/MaterialDatePickerProvider.java
@@ -20,6 +20,7 @@ public interface MaterialDatePickerProvider {
      * Shows a Material Date Picker dialog.
      *
      * @param context The Android context to launch the dialog from.
+     * @param initialTime The initially selected date in UTC milliseconds since epoch.
      * @param minTime The minimum selectable date in UTC milliseconds since epoch.
      * @param maxTime The maximum selectable date in UTC milliseconds since epoch.
      * @param selectionCallback The callback invoked when the user selects a date.
@@ -28,6 +29,7 @@ public interface MaterialDatePickerProvider {
      */
     boolean showDatePicker(
             Context context,
+            long initialTime,
             long minTime,
             long maxTime,
             Callback<Long> selectionCallback,
