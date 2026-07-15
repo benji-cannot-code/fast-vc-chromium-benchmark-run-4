fe709f5d44896c79745caccd7f6077d2a223d9e2
Stephen Nusko <nuskos@chromium.org>
2026-07-15 06:51:50 -0700

    Enable BackupRefPtr feature flag on CastOS.
    This change removes the explicit exclusion of CastOS from the
    `enable_backup_ref_ptr_feature_flag` condition, allowing BackupRefPtr to
    be enabled on CastOS.
    
    See internal email thread:
    https://groups.google.com/a/google.com/g/chrome-memory-safety/c/hd-IhSKGEQM
    
    Also see public bug: crbug.com/41489655
    
    is_castos is mostly now deprecated and as such we can move the last
    unsupported platform for BRP to enabled.
    
    Bug: 41489655
    DanglingUntriaged-notes: Enabling BRP on CastOS, pre-existing.
    Change-Id: I949cec6fee20d2270a1fc751fbca00ba6d1b8f0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8014988
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662548}

--- base/allocator/partition_alloc_support.cc
+++ base/allocator/partition_alloc_support.cc
@@ -1141,9 +1141,8 @@ void PartitionAllocSupport::ReconfigureAfterFeatureListInit(
 
   // Configure ASAN hooks to report the `MiraclePtr status`. This is enabled
   // only if BackupRefPtr is normally enabled in the current process for the
-  // current platform. Note that CastOS is not protected by BackupRefPtr
-  // a the moment, so they are excluded.
-#if PA_BUILDFLAG(USE_ASAN_BACKUP_REF_PTR) && !PA_BUILDFLAG(IS_CASTOS)
+  // current platform.
+#if PA_BUILDFLAG(USE_ASAN_BACKUP_REF_PTR)
 #if PA_BUILDFLAG(USE_ASAN_BACKUP_REF_PTR_V2)
   base::RawPtrAsanService::GetInstance().Configure(
       ShouldEnableFeatureOnProcess(
@@ -1169,7 +1168,7 @@ void PartitionAllocSupport::ReconfigureAfterFeatureListInit(
                                                EnableInstantiationCheck(false));
   }
 #endif  // PA_BUILDFLAG(USE_ASAN_BACKUP_REF_PTR_V2)
-#endif  // PA_BUILDFLAG(USE_ASAN_BACKUP_REF_PTR) && !PA_BUILDFLAG(IS_CASTOS)
+#endif  // PA_BUILDFLAG(USE_ASAN_BACKUP_REF_PTR)
 
 #if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
   auto bucket_distribution = allocator_shim::BucketDistribution::kNeutral;
