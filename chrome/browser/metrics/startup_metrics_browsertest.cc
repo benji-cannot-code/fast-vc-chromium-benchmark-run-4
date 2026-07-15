eb611d0881295d97ae6b28b62ca3cb9b8a003a6b
Alex Attar <aattar@google.com>
2026-07-15 14:48:52 -0700

    Add metric for startup hard fault bytes
    This adds the Startup.BrowserMessageLoopStartHardFaultBytes metric to
    record the number of bytes loaded via hard faults in the browser process
    from startup to the start of the main thread's message loop.
    
    Previously, only the hard fault count was recorded. However, this count
    cannot be reliably compared across platforms with different OS page
    sizes (e.g., 4KB on Windows/Linux/Intel Mac vs. 16KB on Apple Silicon
    Mac). By multiplying the hard fault count by the OS page size, this new
    metric provides a normalized value for accurate cross-platform
    comparisons.
    
    Furthermore, this metric will help us better understand movement with
    temperature buckets, such as tracking movement inside the luke-warm
    bucket.
    
    Change-Id: If620fa570f9d74b2e3acb1808d7e081c466ddc63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032469
    Reviewed-by: Sean Maher <spvm@chromium.org>
    Commit-Queue: Alex Attar <aattar@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662879}

--- chrome/browser/metrics/startup_metrics_browsertest.cc
+++ chrome/browser/metrics/startup_metrics_browsertest.cc
@@ -38,6 +38,7 @@ constexpr const char* kStartupMetrics[] = {
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+    "Startup.BrowserMessageLoopStartHardFaultBytes",
     "Startup.BrowserMessageLoopStartHardFaultCount",
     "Startup.Temperature",
 #endif
