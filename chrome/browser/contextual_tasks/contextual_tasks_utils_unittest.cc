3fbc84689c3eaeeadbef384da84928696de85825
Josiah Kiehl <kiehl@google.com>
2026-07-16 15:00:12 -0700

    Contextual Tasks: Fix test crash in ContextualTasksUtilsTest
    ActionIdMap is global and mappings were not reset, causing
    duplicate key crashes in subsequent tests.
    
    Fix this by adding actions::ActionIdMap::ResetMapsForTesting() to
    TearDown() in ContextualTasksUtilsTest.
    
    Bug: b:527483073
    Change-Id: Ia8c7d17982bccd841af7289a7eb9f4f69b17e4a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8079945
    Auto-Submit: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663532}

--- chrome/browser/contextual_tasks/contextual_tasks_utils_unittest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_utils_unittest.cc
@@ -42,6 +42,7 @@ class ContextualTasksUtilsTest : public testing::Test {
   }
 
   void TearDown() override {
+    actions::ActionIdMap::ResetMapsForTesting();
     actions::ActionManager::Get().ResetForTesting();
     actions::ActionIdMap::ResetMapsForTesting();
     browser_window_.reset();
