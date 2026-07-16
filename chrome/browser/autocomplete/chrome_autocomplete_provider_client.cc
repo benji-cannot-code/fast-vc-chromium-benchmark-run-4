44e84d47793e69fbb84e755fe3bf8257bcfc16dd
Caitlin Chen <caitc@google.com>
2026-07-16 10:14:12 -0700

    [omnibox] Add routing for omnibox action to WebUI Composebox.
    Introduce the `Omnibox_AskGComposeBox` boolean feature parameter to the
    `kWebUIOmniboxAskGAboutThisPage` feature.
    
    Route `ContextualSearchOpenLensAction::Execute()` to a new client method
    `OpenComposeboxForAskG()` when the feature parameter is active.
    * Flag checks are done in client to avoid adding dependencies in
    browser/actions
    * Check if AimPopup is enabled so we can default to lens overlay.
    
    Implement `OpenComposeboxForAskG()` in `OmniboxEditModelActionClient` to
    open the composebox through the edit model.
    
    Implement `OpenComposeboxForAskG()` in `OmniboxEditModel` to transition
    the logical popup state directly to `OmniboxPopupState::kAim`.
    
    http://screencast/cast/NTM5MDYzMzg0NDg2NzA3MnwyZjk4OWY5MC1kOA
    
    Bug: 532597151
    Change-Id: I848d237d83d244938a9aef6695284ad8e8561305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8077378
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Caitlin Chen <caitc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663252}

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
@@ -882,3 +882,14 @@ void ChromeAutocompleteProviderClient::PromptPageTranslation() {
   }
 #endif  // !BUILDFLAG(IS_ANDROID)
 }
+
+bool ChromeAutocompleteProviderClient::ShouldOpenComposeboxForAskG() const {
+#if !BUILDFLAG(IS_ANDROID)
+  return omnibox::IsAimPopupFeatureEnabled() && omnibox::kAskGComposeBox.Get();
+#else
+  return false;
+#endif
+}
+
+// This is implemented in OmniboxEditModelActionClient.
+void ChromeAutocompleteProviderClient::OpenComposeboxForAskG() {}
