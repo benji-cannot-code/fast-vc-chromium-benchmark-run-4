8d96069286657f7be8584176e7e76fef48945e69
Adam Kassi Lahlou <adamkl@google.com>
2026-07-16 13:11:27 -0700

    [Watermark Timestamps] Implement GetTimestampTimezone function
    Define and declare GetDefaultTimestampTimezone & GetTimestampTimezone
    functions in watermark settings, that will be used in later CLs to pass
    the policy enforced time zone to the watermark string.
    
    Test coverage for these functions will be added in
    `watermark_browsertest.cc` in a following CL.
    
    Bug: 534487420
    Change-Id: I99fa3cd0d7f7a97d893d9b0b5fe00d7c5d2fec23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102325
    Reviewed-by: Haihan Chen <haihan@google.com>
    Commit-Queue: Adam Kassi Lahlou <adamkl@google.com>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663450}

--- chrome/browser/enterprise/watermark/settings.cc
+++ chrome/browser/enterprise/watermark/settings.cc
@@ -8,12 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/enterprise/watermark/watermark_features.h"
 #include "chrome/common/channel_info.h"
 #include "components/enterprise/connectors/core/connectors_prefs.h"
+#include "components/enterprise/data_protection/features.h"
 #include "components/prefs/pref_service.h"
 #include "components/version_info/version_info.h"
+#include "third_party/icu/source/i18n/unicode/timezone.h"
 
 namespace {
 
@@ -55,6 +58,20 @@ int GetOpacity(const PrefService* prefs,
 
   return enterprise_watermark::PercentageToSkAlpha(default_percent_value);
 }
+
+// Helper function that verifies that the timezone string is valid.
+// Returns the default value if the string is invalid.
+// Validity is defined by the IANA timezone database.
+std::string ResolveTimestampTimezone(const std::string& timezone) {
+  std::unique_ptr<icu::TimeZone> icu_timezone(
+      icu::TimeZone::createTimeZone(icu::UnicodeString::fromUTF8(timezone)));
+
+  if (!icu_timezone || *icu_timezone == icu::TimeZone::getUnknown()) {
+    return enterprise_watermark::GetDefaultTimestampTimezone();
+  }
+
+  return timezone;
+}
 }  // namespace
 
 namespace enterprise_watermark {
@@ -81,6 +98,10 @@ int GetDefaultFontSize() {
   return enterprise_connectors::kWatermarkStyleFontSizeDefault;
 }
 
+std::string GetDefaultTimestampTimezone() {
+  return enterprise_connectors::kWatermarkStyleTimestampTimezoneDefault;
+}
+
 SkColor GetFillColor(const PrefService* prefs) {
   int alpha =
       GetOpacity(prefs, enterprise_connectors::kWatermarkStyleFillOpacityPref,
@@ -105,6 +126,16 @@ int GetFontSize(const PrefService* prefs) {
   int font_size_from_pref =
       prefs->GetInteger(enterprise_connectors::kWatermarkStyleFontSizePref);
   return std::max(font_size_from_pref, kMinFontSize);
+}
+
+std::string GetTimestampTimezone(const PrefService* prefs) {
+  if (!base::FeatureList::IsEnabled(
+          enterprise_data_protection::kEnableWatermarkTimestampTimezone)) {
+    return GetDefaultTimestampTimezone();
+  }
+
+  return ResolveTimestampTimezone(prefs->GetString(
+      enterprise_connectors::kWatermarkStyleTimestampTimezonePref));
 }
 
 }  // namespace enterprise_watermark
