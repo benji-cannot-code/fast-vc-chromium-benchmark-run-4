2a124325e1b6cf7e44b392ea29b4bbbcd0f856fc
Alison Maher <almaher@microsoft.com>
2026-07-16 08:48:58 -0700

    [Masonry] Container fragmentation support
    This CL adds support for grid-lanes container fragmentation.
    
    Fragmentation of grid-lanes items will be handled in follow-up CLs.
    
    Similar to CL:8020862, but for grid-lanes.
    
    Bug: 343257585
    Change-Id: I1da1c260928f12bcf12407723023227075b64784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102962
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663150}

--- third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm.h"
 
 #include "base/notreached.h"
+#include "third_party/blink/renderer/core/layout/fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/grid/grid_baseline_accumulator.h"
 #include "third_party/blink/renderer/core/layout/grid/grid_data.h"
 #include "third_party/blink/renderer/core/layout/grid/grid_item.h"
@@ -187,9 +188,16 @@ const LayoutResult* GridLanesLayoutAlgorithm::Layout() {
   intrinsic_block_size_ =
       ClampIntrinsicBlockSize(GetConstraintSpace(), node, GetBreakToken(),
                               BorderScrollbarPadding(), intrinsic_block_size_);
+
+  LayoutUnit previously_consumed_block_size;
+  if (GetBreakToken()) [[unlikely]] {
+    previously_consumed_block_size = GetBreakToken()->ConsumedBlockSize();
+  }
+
   auto block_size = ComputeBlockSizeForFragment(
       GetConstraintSpace(), Node(), BorderPadding(),
-      contain_intrinsic_block_size_.value_or(intrinsic_block_size_),
+      previously_consumed_block_size +
+          contain_intrinsic_block_size_.value_or(intrinsic_block_size_),
       container_builder_.InlineSize());
   container_builder_.SetFragmentsTotalBlockSize(block_size);
   container_builder_.SetIntrinsicBlockSize(intrinsic_block_size_);
@@ -217,6 +225,16 @@ const LayoutResult* GridLanesLayoutAlgorithm::Layout() {
     container_builder_.SetInflowBounds(LogicalRect(offset, size));
   }
   container_builder_.SetMayHaveDescendantAboveBlockStart(false);
+
+  if (InvolvedInBlockFragmentation(container_builder_)) [[unlikely]] {
+    FinishFragmentation(&container_builder_);
+  } else {
+#if DCHECK_IS_ON()
+    // If we're not participating in a fragmentation context, no block
+    // fragmentation related fields should have been set.
+    container_builder_.CheckNoBlockFragmentation();
+#endif
+  }
 
   // Place out-of-flow items after setting the intrinsic block size, since
   // out-of-flow items don't contribute to the intrinsic size of the container.
