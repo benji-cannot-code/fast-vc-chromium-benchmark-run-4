a51e48777b6dee6e0582309be8c0bfbf4b6fa26a
Lei Zhang <thestig@chromium.org>
2026-07-16 09:06:36 -0700

    Clean up tab_sharing_test_utils code
    - Do IWYU in tab_sharing_test_utils.h and remove an unnecessary build
      dependency.
    - Rename the generic GetButtonOrLabelText() to
      GetTabSharingButtonOrLabelText() to avoid potential name conflicts
    - Call views::View::GetClassName() just once.
    
    Change-Id: I6b1b4d5b666939293cc54758205ca7b7ac88b239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102963
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663155}

--- chrome/browser/ui/views/tab_sharing/tab_sharing_test_utils.cc
+++ chrome/browser/ui/views/tab_sharing/tab_sharing_test_utils.cc
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/label.h"
 
-std::optional<std::u16string_view> GetButtonOrLabelText(
+std::optional<std::u16string_view> GetTabSharingButtonOrLabelText(
     const views::View& button_or_label) {
-  if (button_or_label.GetClassName() == "MdTextButton") {
+  std::string_view class_name = button_or_label.GetClassName();
+  if (class_name == "MdTextButton") {
     return static_cast<const views::MdTextButton&>(button_or_label).GetText();
-  } else if (button_or_label.GetClassName() == "Label") {
+  }
+  if (class_name == "Label") {
     return static_cast<const views::Label&>(button_or_label).GetText();
   }
   return std::nullopt;
