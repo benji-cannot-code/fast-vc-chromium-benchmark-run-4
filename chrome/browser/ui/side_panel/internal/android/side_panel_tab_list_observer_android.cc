488eb0e7f0482e55eb164710a8c0af428b7cbcea
Linyu He <linyuh@google.com>
2026-07-16 16:50:04 -0700

    SidePanel: Close side panel when all tabs are closing
    Android has a stable 0-tab state, such as when the user has closed all
    tabs in the Grid Tab Switcher (GTS), but hasn't created any new tab.
    
    The side panel infrastructure should reflect this state by closing the
    side panel when all tabs are closing. Otherwise, an obsolete side panel
    can be shown when the user closes GTS. At this point, the side panel
    system is in an invalid state and can cause a crash.
    
    The production code change is in C++, but the test is added on the Java
    side since it's not easy to exercise the user journey to close all tabs
    in C++ browser tests.
    
    TabSwitcherAppMenuFacility is updated to support the "Close all tabs"
    option in GTS.
    
    Cq-Include-Trybots: luci.chromium.try:android-desktop-x64-rel
    Include-Ci-Only-Tests: true
    Bug: 534280270
    Change-Id: Id47fd2a7b628951377aaaf56f10aa5cdae565daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8104600
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663581}

--- chrome/browser/ui/side_panel/internal/android/side_panel_tab_list_observer_android.cc
+++ chrome/browser/ui/side_panel/internal/android/side_panel_tab_list_observer_android.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "chrome/browser/tab_list/tab_list_interface.h"
 #include "chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.h"
+#include "chrome/browser/ui/side_panel/side_panel_enums.h"
 
 SidePanelTabListObserverAndroid::SidePanelTabListObserverAndroid(
     TabListInterface* tab_list,
@@ -76,6 +77,39 @@ void SidePanelTabListObserverAndroid::OnTabRemoved(
       coordinator_->OnTabReparented(tab);
     }
   }
+}
+
+void SidePanelTabListObserverAndroid::OnAllTabsAreClosing(
+    TabListInterface& tab_list) {
+  // Usually when a tab is closed, OnActiveTabChanged() will be called and it
+  // will update the side panel states, including closing the side panel if the
+  // new active tab doesn't need it.
+  //
+  // However, when the user closes all tabs, such as via the three-dot menu in
+  // the Grid Tab Switcher (GTS), OnActiveTabChanged() won't be called, but we
+  // also need to close the side panel if it's shown. Otherwise, when the user
+  // creates a new tab, the side panel for a destroyed tab will remain.
+  //
+  // A common question might be: When the user creates a new tab after closing
+  // all tabs, shouldn't OnActiveTabChanged() fix the side panel states?
+  //
+  // The answer:
+  //
+  // First of all, Chrome on Android has a _stable_ 0-tab UI state, such as
+  // when the user has closed all tabs in GTS, but hasn't created any new tab.
+  // Side panel code should reflect this state because GTS is laid on top of the
+  // main browser UI (i.e., the main browser UI is still alive).
+  //
+  // Secondly, OnActiveTabChanged() only closes the side panel if
+  // (1) the side panel is currently shown,
+  // (2) the new active tab doesn't have an active SidePanelEntry, and
+  // (3) the old tab hasn't been deleted.
+  //
+  // Relying on OnActiveTabChanged() won't meet the condition in (3), and we
+  // shouldn't change (3) as it prevents holding/dereferencing an _invalid_
+  // pointer to the SidePanelRegistry of the deleted tab.
+  coordinator_->Close(SidePanelEntryHideReason::kSidePanelClosed,
+                      /*suppress_animations=*/true);
 }
 
 void SidePanelTabListObserverAndroid::OnTabListDestroyed(
--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherAppMenuFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherAppMenuFacility.java
@@ -19,6 +19,7 @@ import org.chromium.chrome.test.transit.ntp.IncognitoNewTabPageStation;
 import org.chromium.chrome.test.transit.ntp.RegularNewTabPageStation;
 import org.chromium.chrome.test.transit.quick_delete.QuickDeleteDialogFacility;
 import org.chromium.chrome.test.transit.settings.SettingsStation;
+import org.chromium.chrome.test.transit.ui.ModalDialogFacility;
 
 import java.util.Collections;
 
@@ -135,6 +136,18 @@ public class TabSwitcherAppMenuFacility<HostStationT extends TabSwitcherStation>
     /** Select "Settings" from the app menu. */
     public SettingsStation openSettings() {
         return mSettings.scrollToAndSelectTo().arriveAt(createSettingsStation());
+    }
+
+    /** Select "Close all tabs" from the app menu. */
+    public ModalDialogFacility clickCloseAllTabs() {
+        assertNotNull(mCloseAllTabs);
+
+        var modalDialogFacility = new ModalDialogFacility();
+        modalDialogFacility.declarePositiveButton();
+        modalDialogFacility.declareNegativeButton();
+
+        mCloseAllTabs.scrollToAndSelectTo().enterFacility(modalDialogFacility);
+        return modalDialogFacility;
     }
 
     /** Select "Select tabs" from the app menu. */
