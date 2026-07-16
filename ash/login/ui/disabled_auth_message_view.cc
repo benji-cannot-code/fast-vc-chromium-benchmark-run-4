8d7906986fca6e6ec6d63f726a62ded75d8a4985
Danilo Tedeschi <danft@google.com>
2026-07-16 09:50:03 -0700

    IcuBridge migration: ash/login/ui/
    Migrate ash/login/ui/disabled_auth_message_view.cc to use IcuBridge.
    
    Change-Id: Ic174ebc6e4f146acc1f58f6cb4fe8c386ab190b4
    Bug: 534455293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8086460
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663212}

--- ash/login/ui/disabled_auth_message_view.cc
+++ ash/login/ui/disabled_auth_message_view.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
+#include "base/i18n/icubridge/date_time_formatter.h"
+#include "base/i18n/icubridge/icu_bridge.h"
 #include "base/i18n/time_formatting.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -30,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 namespace {
+
+using ::base::i18n::IcuBridge;
 
 constexpr int kVerticalBorderDp = 16;
 constexpr int kHorizontalBorderDp = 16;
@@ -77,7 +81,8 @@ LockScreenMessage GetWindowLimitMessage(const base::Time& unlock_time,
   } else {
     message.content = l10n_util::GetStringFUTF16(
         IDS_ASH_LOGIN_COME_BACK_DAY_OF_WEEK_MESSAGE,
-        base::LocalizedTimeFormatWithPattern(unlock_time, "EEEE"),
+        IcuBridge::GetInstance().date_time_formatter().Format(
+            unlock_time, base::i18n::datetime_options::E::Long()),
         time_to_display);
   }
   message.icon = ui::ImageModel::FromVectorIcon(
