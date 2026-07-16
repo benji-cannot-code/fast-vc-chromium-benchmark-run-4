dec4cfdbb674a5c27c87d2505f688b0ee8851123
Alexandra Pereira <alexandrasp@google.com>
2026-07-16 07:39:21 -0700

    [iOS/web] Remove unused js_messaging import in earl grey DEPS
    `chrome_earl_grey_app_interface.mm` imports `web_view_js_utils.h`
    from `ios/web/js_messaging/`, but does not use any symbols from it.
    
    This CL removes the unused import and cleans up the inclusion rule in
    ios/chrome/test/earl_grey/DEPS.
    
    Bug: 493548553
    Change-Id: I5f6f9433c3121cc87fba3bbfbe70de413da4eaf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8100963
    Auto-Submit: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663134}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.mm
@@ -107,7 +107,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/testing/verify_custom_webkit.h"
 #import "ios/web/common/features.h"
 #import "ios/web/common/uikit_ui_util.h"
-#import "ios/web/js_messaging/web_view_js_utils.h"
 #import "ios/web/public/browser_state_utils.h"
 #import "ios/web/public/js_messaging/content_world.h"
 #import "ios/web/public/js_messaging/web_frame.h"
