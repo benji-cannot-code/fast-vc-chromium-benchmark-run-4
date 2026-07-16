db3ff9da1dba80a3fd6d9b0bc178d3fd59a2e859
Egor Pasko <pasko@chromium.org>
2026-07-16 10:35:27 -0700

    ipcz: Allow //base. Part 1: Remove standalone build
    This is Part 1 of a series of changes to allow using //base in
    //third_party/ipcz. See the bug for background and motivation.
    
    Remove //third_party/ipcz/src/standalone. This includes removing
    polyfills for logging.h, stack_trace.h, unsafe_buffers.h.
    
    Remove GN arg build_ipcz_standalone and macro IPCZ_STANDALONE.
    
    Cleanup in GN:
    * Remove the template needed for target suffixes "_standalone"
      and "_chromium". The new targets will not have these suffixes.
      Replace GN targets like "something_chromium" to "something".
    * Remove ipcz target "ipcz_tests" - these tests run as part of
      "//ipc:ipc_tests". Preserving test coverage.
    
    Retain the C compilation test (compile_c_test.c) for now as the C
    interface is still there.
    
    Bug: 526596517
    Change-Id: I2ccb79dcf1a8117d0bdb1ce7f21495a641ba2a59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8005116
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663283}

--- third_party/ipcz/src/connect_test.cc
+++ third_party/ipcz/src/connect_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/sanitizer_buildflags.h"
 #include "build/build_config.h"
 #include "ipcz/ipcz.h"
 #include "ipcz/node_messages.h"
@@ -19,10 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "test/test_transport_listener.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/synchronization/notification.h"
-
-#if !defined(IPCZ_STANDALONE)
-#include "base/sanitizer_buildflags.h"  // nogncheck
-#endif
 
 namespace ipcz {
 namespace {
@@ -79,10 +76,7 @@ MULTINODE_TEST_NODE(ConnectTestNode, ExpectDisconnectFromBroker) {
   Close(b);
 }
 
-#if defined(IPCZ_STANDALONE)
-#define MAYBE_DisconnectWithoutBrokerHandshake \
-  DISABLED_DisconnectWithoutBrokerHandshake
-#elif BUILDFLAG(USING_SANITIZER)
+#if BUILDFLAG(USING_SANITIZER)
 // TODO(crbug.com/1400965): Fix the failing MojoIpczInProcess on linux tsan.
 #define MAYBE_DisconnectWithoutBrokerHandshake \
   DISABLED_DisconnectWithoutBrokerHandshake
--- third_party/ipcz/src/util/log.h
+++ third_party/ipcz/src/util/log.h
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IPCZ_SRC_UTIL_LOG_H_
 #define IPCZ_SRC_UTIL_LOG_H_
 
-#if defined(IPCZ_STANDALONE)
-#include "standalone/base/logging.h"  // nogncheck
-#else
-#include "base/logging.h"  // nogncheck
-#endif
+#include "base/logging.h"
 
 #endif  // IPCZ_SRC_UTIL_LOG_H_
