2efb4910ac5b927110e7997e3f03b388eefa354d
Xianzhu Wang <wangxianzhu@chromium.org>
2026-07-16 10:27:58 -0700

    Allow merge of StickyPositionConstrains within a given scroll range
    In addition to the original equivalency condition, now also allow
    merge of StickyPositionConstraints if they can produce offsets with a
    constant difference within a given scroll range.
    
    This will allow merge of many sticky elements with different
    constraints if they move together within a (large) scroll range.
    
    In the next CL, blink will pass the scroll range based on the
    scrolling contents cull rect of the containing scroller.
    
    Bug: 40533727
    Change-Id: I740852a4ef1e20f7d2c7a1a3c987442368ec8c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7903970
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663274}

--- cc/trees/sticky_position_constraint.h
+++ cc/trees/sticky_position_constraint.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_TREES_STICKY_POSITION_CONSTRAINT_H_
 #define CC_TREES_STICKY_POSITION_CONSTRAINT_H_
 
-#include "cc/cc_export.h"
+#include <optional>
 
+#include "cc/cc_export.h"
 #include "cc/paint/element_id.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect.h"
@@ -66,9 +67,21 @@ struct CC_EXPORT StickyPositionConstraint {
   ElementId nearest_element_shifting_sticky_box;
   ElementId nearest_element_shifting_containing_block;
 
-  // Returns true if `this` and `other` are equivalent and either one can be
-  // used to get the same rendering result.
-  bool CanMerge(const StickyPositionConstraint& other) const;
+  // Returns whether the blink layerization algorithm can merge `this` and
+  // `other`:
+  // - kCanAlwaysMerge if the two constraints are equivalent;
+  // - kCanMergeWithinScrollRange if `scroll_range` is provided and the two
+  //   constraints always produce StickyPositionOffset values with a constant
+  //   difference for any scroll position within `scroll_range`;
+  // - kCannotMerge otherwise.
+  enum class CanMergeResult {
+    kCannotMerge,
+    kCanAlwaysMerge,
+    kCanMergeWithinScrollRange,
+  };
+  CanMergeResult CanMerge(
+      const StickyPositionConstraint& other,
+      const std::optional<gfx::RectF>& scroll_range = std::nullopt) const;
 
   // Returns the offset that should be applied to the sticky box based on the
   // current scroll position and the constraint. Pixel snapping is not applied
