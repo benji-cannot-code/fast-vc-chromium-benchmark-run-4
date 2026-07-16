f7be8e6947d19bbfbb8f02ff5652b5e322d3c075
kenoss <kenoss@chromium.org>
2026-07-15 21:27:48 -0700

    Prerender2Fallback: Revert PrefetchSchedulerPolicy param for Desktop
    In [1], we updated a feature param for scheduler policy for consistency.
    This CL reverts the change and we'll update it with A/B testing again.
    
    [1] https://crrev.com/c/7889219
    
    Bug: 342089123
    Change-Id: I0e228bf8042ed0430f057c578fffbfd2c2bcdccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085004
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663034}

--- content/browser/preloading/prerender/prerender_features.cc
+++ content/browser/preloading/prerender/prerender_features.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/preloading/prerender/prerender_features.h"
 
+#include "build/build_config.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
@@ -26,7 +27,12 @@ const base::FeatureParam<Prerender2FallbackPrefetchSchedulerPolicy>
     kPrerender2FallbackPrefetchSchedulerPolicy{
         &kPrerender2FallbackPrefetchSpecRules,
         "kPrerender2FallbackPrefetchSchedulerPolicy",
+// TODO(crbug.com/342089123): Use consistent policy if possible.
+#if BUILDFLAG(IS_ANDROID)
         Prerender2FallbackPrefetchSchedulerPolicy::kNotUse,
+#else
+        Prerender2FallbackPrefetchSchedulerPolicy::kBurst,
+#endif
         &kPrerender2FallbackPrefetchSchedulerPolicyOptios};
 
 BASE_FEATURE(kPrerender2NoVarySearch, base::FEATURE_ENABLED_BY_DEFAULT);
