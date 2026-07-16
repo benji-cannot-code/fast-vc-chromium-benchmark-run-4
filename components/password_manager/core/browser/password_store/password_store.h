66749e464081878ec60be1d6e6dab39eda4144e0
Yurii Lahodiuk <ylahodiuk@google.com>
2026-07-16 02:25:23 -0700

    Propagate storage errors only if ActionableError changes
    Before this change `PasswordStore` could notify its observers by calling
    `OnErrorStateChanged` even if `ActionableError` did not change. In the
    scope of the current change we ensure that `OnErrorStateChanged` will be
    called only if `ActionableError` differs from the last reported error.
    
    Bug: 534257233
    Change-Id: I6342de448adb5ba028a6a069a05f37eeeeec0583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8093088
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663083}

--- components/password_manager/core/browser/password_store/password_store.h
+++ components/password_manager/core/browser/password_store/password_store.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_STORE_PASSWORD_STORE_H_
 
 #include <memory>
+#include <optional>
 #include <ostream>
 #include <string>
 #include <vector>
@@ -141,6 +142,10 @@ class PasswordStore : public PasswordStoreInterface {
   // Called when the backend reports that sync has been enabled or disabled.
   void NotifySyncEnabledOrDisabledOnMainSequence();
 
+  // Helper to notify observers via OnErrorStateChanged if the actionable error
+  // differs from `last_reported_actionable_error_`.
+  void NotifyObserversIfErrorStateChanged(ActionableError error);
+
   // The following methods notify observers that the password store may have
   // been modified via NotifyLoginsChangedOnMainSequence(). Note that there is
   // no guarantee that the called method will actually modify the password store
@@ -171,6 +176,10 @@ class PasswordStore : public PasswordStoreInterface {
   // here to be executed afterwards by chaining in FIFO order. Callback is
   // invoked inside OnInitCompleted().
   base::OnceClosure post_init_callback_ = base::DoNothing();
+
+  // Caches the most recent reported actionable error so duplicate reports
+  // with the exact same error are skipped.
+  std::optional<ActionableError> last_reported_actionable_error_;
 };
 
 }  // namespace password_manager
