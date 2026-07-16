e5d345cdaa88e84703d14a904c06008e70f7475e
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-16 10:05:41 -0700

    [Batch] PrivacyPreferencesManagerImplNativeTest
    Bug: 535536257
    Change-Id: Ic7048d5e983ee4797aeba36ed1db24e26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8105419
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663238}

--- chrome/android/javatests/src/org/chromium/chrome/browser/privacy/settings/PrivacyPreferencesManagerImplNativeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/privacy/settings/PrivacyPreferencesManagerImplNativeTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.AdvancedMockContext;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -26,6 +27,7 @@ import org.chromium.content_public.browser.test.NativeLibraryTestUtils;
 
 /** Tests "Usage and Crash reporting" settings screen. */
 @RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
 public class PrivacyPreferencesManagerImplNativeTest {
 
     @Before
