38014a8fb68ebfe05995d364153e8c68128a9bfc
Zhe Li <zheliooo@google.com>
2026-07-16 10:59:58 -0700

    [WindowManagerDeprecation] Show toast when instance creation limit is reached
    Screenshot: https://drive.google.com/file/d/1EpZIjoidZxcUuM2qitwgNwycmb3iY6Ax/view?usp=sharing
    Fixed: 527593501
    Change-Id: Ib70445cee20c3e1d5b8571311b68e72c627796e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076862
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663300}

--- chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31UnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerApi31UnitTest.java
@@ -42,6 +42,7 @@ import android.content.res.Resources;
 import android.text.TextUtils;
 import android.util.Pair;
 import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
 
 import androidx.test.core.app.ApplicationProvider;
 
@@ -62,6 +63,7 @@ import org.mockito.stubbing.Answer;
 import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowDialog;
+import org.robolectric.shadows.ShadowToast;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
@@ -121,6 +123,7 @@ import org.chromium.components.tab_group_sync.TabGroupSyncService;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.test.util.MockitoHelper;
+import org.chromium.ui.widget.ToastManager;
 import org.chromium.url.GURL;
 import org.chromium.url.JUnitTestGURLs;
 
@@ -135,7 +138,9 @@ import java.util.stream.Collectors;
 
 /** Unit tests for {@link MultiInstanceManagerApi31}. */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
+@Config(
+        manifest = Config.NONE,
+        shadows = {ShadowToast.class})
 @EnableFeatures({
     ChromeFeatureList.SESSION_RESTORE_AFTER_CRASH,
     ChromeFeatureList.INCOGNITO_AS_WINDOW_FULL_SCREEN
@@ -411,6 +416,8 @@ public class MultiInstanceManagerApi31UnitTest {
         TabWindowManagerSingleton.resetTabModelSelectorFactoryForTesting();
         ApplicationStatus.destroyForJUnitTests();
         mMultiInstanceManager.mTestBuildInstancesList = false;
+        ShadowToast.reset();
+        ToastManager.resetForTesting();
     }
 
     private void setupActivityForCreateNewWindowIntent(Activity activity) {
@@ -2126,6 +2133,7 @@ public class MultiInstanceManagerApi31UnitTest {
     }
 
     @Test
+    @DisableFeatures(ChromeFeatureList.IN_APP_WINDOW_MANAGER_DEPRECATION)
     public void showInstanceCreationLimitMessage() {
         when(mCurrentActivity.getResources()).thenReturn(mock(Resources.class));
 
@@ -2140,6 +2148,7 @@ public class MultiInstanceManagerApi31UnitTest {
     }
 
     @Test
+    @DisableFeatures(ChromeFeatureList.IN_APP_WINDOW_MANAGER_DEPRECATION)
     public void testShowInstanceCreationLimitMessage_SuppressesDuplicates() {
         when(mCurrentActivity.getResources()).thenReturn(mock(Resources.class));
 
@@ -2161,6 +2170,23 @@ public class MultiInstanceManagerApi31UnitTest {
         // Third invocation now enqueues again because the first one was dismissed.
         mMultiInstanceManager.showInstanceCreationLimitMessage();
         verify(mMessageDispatcher).enqueueWindowScopedMessage(any(), eq(false));
+    }
+
+    @Test
+    @EnableFeatures(ChromeFeatureList.IN_APP_WINDOW_MANAGER_DEPRECATION)
+    public void testInstanceCreationLimitWarning_WindowManagerDeprecated() {
+        when(mCurrentActivity.getResources())
+                .thenReturn(ApplicationProvider.getApplicationContext().getResources());
+        when(mCurrentActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                .thenReturn(LayoutInflater.from(ApplicationProvider.getApplicationContext()));
+
+        mMultiInstanceManager.showInstanceCreationLimitMessage();
+
+        // Verify that the message is NOT enqueued via message dispatcher.
+        verify(mMessageDispatcher, never()).enqueueWindowScopedMessage(any(), anyBoolean());
+
+        // Verify that the toast is shown.
+        assertNotNull("Toast should have been shown.", ShadowToast.getLatestToast());
     }
 
     @Test
