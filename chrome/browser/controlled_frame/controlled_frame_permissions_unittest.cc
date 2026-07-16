3244357ed042f4ea9c4a65ed1fc32ea991f07cd3
Awad Osman <awado@google.com>
2026-07-16 07:54:38 -0700

    Add local allowlist for Suspicious Site Warnings
    Implements SuspiciousSiteWarningAllowlist to store local allowlist
    decisions when a user bypasses a Suspicious Site Warning (SSW).
    Decisions are stored in HostContentSettingsMap under
    SUSPICIOUS_SITE_WARNING_DATA with a default 30-day expiration timeout.
    Also ensures allowlist entries are cleared alongside browsing history
    deletion.
    
    This CL will be integrated with the Android UI changes in
    crrev.com/c/8030929.
    
    TAG=agy
    CONV=97967a7b-9842-49a8-91b4-125d55d588be
    
    Bug: b:534743024
    Change-Id: I838da268523eeaab000f74cc3fc9a25ab49f0538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8092628
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663139}

--- chrome/browser/controlled_frame/controlled_frame_permissions_unittest.cc
+++ chrome/browser/controlled_frame/controlled_frame_permissions_unittest.cc
@@ -203,6 +203,7 @@ TEST_F(ControlledFramePermissionsTest, Verify) {
       case ContentSettingsType::SUSPICIOUS_NOTIFICATION_SHOW_ORIGINAL:
       case ContentSettingsType::SUB_APPS_WITHOUT_PROMPTS:
       case ContentSettingsType::INLINE_CUE_MENU:
+      case ContentSettingsType::SUSPICIOUS_SITE_WARNING_DATA:
         break;
 
       default:
