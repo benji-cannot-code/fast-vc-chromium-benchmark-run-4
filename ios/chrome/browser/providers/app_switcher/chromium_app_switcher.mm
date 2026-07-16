ecab33a1f6ba4880d32739a8340cd9dac8d9d0b6
Joemer Ramos <joemerramos@google.com>
2026-07-16 09:02:37 -0700

    [ios] Add FetchAppSwitcherParams public provider API
    Introduce the FetchAppSwitcherParams public provider interface and its
    dummy Chromium and test helper implementations. This new API will be
    used to fetch launch parameters from external intents triggered by the
    App Switcher (such as AI summarization or incognito requests).
    
    Internal implementation: https://crrev.com/i/9549815
    
    Bug: 524711740
    Change-Id: I9fb05182036373d9cc3c1212bb1bbc42ac213350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8089775
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663154}

---
+++ ios/chrome/browser/providers/app_switcher/chromium_app_switcher.mm
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "base/functional/bind.h"
+#import "base/location.h"
+#import "base/task/sequenced_task_runner.h"
+#import "ios/public/provider/chrome/browser/app_switcher/app_switcher_api.h"
+
+namespace ios::provider {
+
+void FetchAppSwitcherParams(const GURL& url,
+                            std::string_view app_id,
+                            AppSwitcherResponseCallback callback) {
+  // App switcher parameters fetching is not supported in Chromium.
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE,
+      base::BindOnce(std::move(callback), AppSwitcherUrlOpeningResult{}));
+}
+
+}  // namespace ios::provider
---
+++ ios/chrome/test/providers/app_switcher/test_app_switcher.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_TEST_PROVIDERS_APP_SWITCHER_TEST_APP_SWITCHER_H_
+#define IOS_CHROME_TEST_PROVIDERS_APP_SWITCHER_TEST_APP_SWITCHER_H_
+
+#import <string_view>
+
+#import "ios/public/provider/chrome/browser/app_switcher/app_switcher_api.h"
+
+using AppSwitcherResponseCompletion =
+    void (^)(ios::provider::AppSwitcherUrlOpeningResult result);
+
+// A protocol to replace the App Switcher provider in tests.
+@protocol AppSwitcherProviderTestHelper
+
+- (void)sendAppSwitcherResponseForUrl:(const GURL&)url
+                                appId:(std::string_view)appId
+                           completion:(AppSwitcherResponseCompletion)completion;
+
+@end
+
+namespace ios::provider {
+namespace test {
+
+// Sets the global helper for the tests.
+// Resets it if `helper` is nil.
+void SetAppSwitcherProviderTestHelper(id<AppSwitcherProviderTestHelper> helper);
+
+}  // namespace test
+}  // namespace ios::provider
+
+#endif  // IOS_CHROME_TEST_PROVIDERS_APP_SWITCHER_TEST_APP_SWITCHER_H_
