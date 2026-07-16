ef533c4365f728defd7e03a15e0708ce4802f396
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-07-16 05:45:45 -0700

    Fix IWYU in ios_smart_tab_grouping_request_wrapper.mm
    https://ci.chromium.org/ui/p/chrome/builders/try/ios-device/2779/overview
    
    Change-Id: I9df426259ff9ecca10d21ab05ad82088e8b06b0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8107135
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663109}

--- ios/chrome/browser/intelligence/proto_wrappers/ios_smart_tab_grouping_request_wrapper.mm
+++ ios/chrome/browser/intelligence/proto_wrappers/ios_smart_tab_grouping_request_wrapper.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/barrier_closure.h"
 #import "base/functional/bind.h"
+#import "base/logging.h"
 #import "base/memory/raw_ptr.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/sys_string_conversions.h"
