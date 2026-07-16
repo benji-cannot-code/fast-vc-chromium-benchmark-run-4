8d2c6a9e22b50a1ed2078bfaab36073f323b03dd
Caleb Raitto <caraitto@chromium.org>
2026-07-16 09:11:36 -0700

    Add accesssibilty notifications for page actions
    These work similarly to those in WebUIContentSettingImageControl.
    
    Bug: 535300071
    Change-Id: Ie334f9dee20fe68c885043485dd31ef95b3212df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8103204
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Tianyang Xu <xtlsheep@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663159}

--- chrome/browser/ui/views/page_action/webui_page_action_control.cc
+++ chrome/browser/ui/views/page_action/webui_page_action_control.cc
@@ -280,6 +280,7 @@ class WebUIPageActionControl::WebUIPageActionDelegate
 
   // The last state sent to the WebUI. Null if the action was not visible.
   toolbar_ui_api::mojom::PageActionStatePtr old_state_;
+  bool was_chip_visible_ = false;
 };
 
 void WebUIPageActionControl::WebUIPageActionDelegate::SetController(
@@ -287,6 +288,7 @@ void WebUIPageActionControl::WebUIPageActionDelegate::SetController(
   observation_.Reset();
   action_item_subscription_ = {};
   controller_ = controller;
+  was_chip_visible_ = false;
 
   if (controller_) {
     controller_->RegisterCallbacks(page_actions::PageActionPassKey(),
@@ -305,6 +307,14 @@ void WebUIPageActionControl::WebUIPageActionDelegate::SetController(
 
 void WebUIPageActionControl::WebUIPageActionDelegate::OnPageActionModelChanged(
     const page_actions::PageActionModelInterface& model) {
+  const bool is_chip_visible =
+      model.GetVisible() && model.ShouldShowSuggestionChip();
+
+  if (model.GetShouldAnnounceChip() && !was_chip_visible_ && is_chip_visible) {
+    owner_->AnnounceAlert(model.GetText());
+  }
+  was_chip_visible_ = is_chip_visible;
+
   toolbar_ui_api::mojom::PageActionStatePtr new_state = GetState();
 
   if (!old_state_.Equals(new_state)) {
@@ -319,6 +329,7 @@ void WebUIPageActionControl::WebUIPageActionDelegate::
   observation_.Reset();
   action_item_subscription_ = {};
   controller_ = nullptr;
+  was_chip_visible_ = false;
   if (old_state_) {
     old_state_ = nullptr;
     owner_->NotifyPageActionStateChanged();
@@ -477,6 +488,12 @@ void WebUIPageActionControl::OnPageActionChipShowingChanged(
 void WebUIPageActionControl::NotifyPageActionStateChanged() {
   if (webui_delegate_) {
     webui_delegate_->OnPageActionChanged(GetPageActionStates());
+  }
+}
+
+void WebUIPageActionControl::AnnounceAlert(const std::u16string& announcement) {
+  if (webui_delegate_) {
+    webui_delegate_->AnnounceAlert(announcement);
   }
 }
 
