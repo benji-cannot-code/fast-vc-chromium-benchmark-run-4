902cb23a5a4abf405f997954a365e107e1dd0b4e
Tiffany Song <tiffanysong@google.com>
2026-07-17 13:21:23 -0700

    [AW] Skip UpdateOriginSalts when pseudo-partitioning
    The `UpdateOriginSalts` method in the VisitedLinkEventListener is
    relatively resource intensive. If we are pseudo-partitioning, like for
    Android WebView, we use a constant salt for each table entry. Thus,
    there are no origin salts to update.
    
    This CL is part of a larger project to migrate Android WebView :visited
    links storage infrastructure onto the partitioned :visited link storage
    infrastructure while maintaining existing WebView behavior in order to
    remove the legacy, unpartitioned infrastructure.
    - Project doc: go/webview-partitioned-apis
    - Issue tree: https://crbug.com/506963484/dependencies
    
    Fixed: 524174788
    Change-Id: I453c91310b52a7ff8efee2e196ba045ef2434925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8086562
    Commit-Queue: Tiffany Song <tiffanysong@google.com>
    Reviewed-by: Kyra Seevers <kyraseevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664141}

--- components/visitedlink/browser/visitedlink_event_listener.h
+++ components/visitedlink/browser/visitedlink_event_listener.h
@@ -42,7 +42,8 @@ class VisitedLinkEventListener
   // Used by PartitionedVisitedLinkWriter to provide and store a raw pointer to
   // the owning object.
   VisitedLinkEventListener(content::BrowserContext* browser_context,
-                           PartitionedVisitedLinkWriter* partitioned_writer);
+                           PartitionedVisitedLinkWriter* partitioned_writer,
+                           bool is_pseudo_partitioned);
 
   VisitedLinkEventListener(const VisitedLinkEventListener&) = delete;
   VisitedLinkEventListener& operator=(const VisitedLinkEventListener&) = delete;
@@ -58,6 +59,8 @@ class VisitedLinkEventListener
   // Sets a custom timer to use for coalescing events for testing.
   // |coalesce_timer_override| must outlive this.
   void SetCoalesceTimerForTest(base::OneShotTimer* coalesce_timer_override);
+
+  bool is_pseudo_partitioned() const { return is_pseudo_partitioned_; }
 
   // content::RenderProcessHostCreationObserver:
   void OnRenderProcessHostCreated(content::RenderProcessHost* rph) override;
@@ -104,6 +107,9 @@ class VisitedLinkEventListener
   // Used to filter RENDERER_PROCESS_CREATED notifications to renderers that
   // belong to this BrowserContext.
   raw_ptr<content::BrowserContext> browser_context_;
+
+  // Whether the visited link hashtable is pseudo-partitioned.
+  bool is_pseudo_partitioned_ = false;
 };
 
 }  // namespace visitedlink
