cb624cd98b261190a5df9acc0b80c1132c899203
Palak Agarwal <agpalak@google.com>
2026-07-17 11:20:25 -0700

    audio-preferred captures in getDisplayMedia
    This CL comprises mostly of changes in blink to plumb the audioSelection
    constraint from JS to StreamControls to MediaStreamRequest.
    
    This includes:
    - Adding audio_selection_preferred to StreamControls mojom and C++ structs.
    - Parsing the audioSelection constraint in Blink.
    - Adding GetDisplayMediaAudioSelection runtime feature.
    
    Bug: 535514300
    Change-Id: If34715999d061df52d0b8b1cce0b2046308674ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8107082
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664045}

--- third_party/blink/renderer/modules/mediastream/media_devices.cc
+++ third_party/blink/renderer/modules/mediastream/media_devices.cc
@@ -437,6 +437,9 @@ MediaStreamConstraints* ToMediaStreamConstraints(
   if (source->hasMonitorTypeSurfaces()) {
     constraints->setMonitorTypeSurfaces(source->monitorTypeSurfaces());
   }
+  if (source->hasAudioSelection()) {
+    constraints->setAudioSelection(source->audioSelection());
+  }
   return constraints;
 }
 
