34e35a169832a5cf7024b0c03398776c2faca7b0
Hitarth Kothari <hitarthkothari@google.com>
2026-07-17 14:12:54 -0700

    Throw Java RuntimeException on Android for unregistered preference writes
    When writing to an unregistered preference on Android via JNI, C++
    triggers a non-fatal DumpWithoutCrashing stack trace. This stack trace
    originates from C++ and does not contain the Java caller stack, which
    makes it hard to trace back to the incorrect Java caller.
    This CL updates PrefService::SetUserPrefValue to throw a Java
    RuntimeException if called on Android before invoking the assertion, allowing
    the JVM to raise a Java exception with a complete Java stack trace when
    control returns.
    
    Bug: 535326218
    Change-Id: I5511fc60585ceee614c326f118eedab45f704f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8120410
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664163}

--- components/prefs/pref_service.cc
+++ components/prefs/pref_service.cc
@@ -34,6 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include <jni.h>
+
+#include "base/android/jni_android.h"
 #include "components/prefs/android/pref_service_android.h"
 #endif
 
@@ -544,9 +547,20 @@ void PrefService::SetUserPrefValue(std::string_view path,
 
   const Preference* pref = FindPreference(path);
   if (!pref) {
+#if BUILDFLAG(IS_ANDROID)
+    // TODO(crbug.com/535326218): Throw Java exception so that we get a Java
+    // stack trace of the caller of the unregistered preference.
+    JNIEnv* env = base::android::AttachCurrentThread();
+    jclass clazz = env->FindClass("java/lang/RuntimeException");
+    std::string msg =
+        "Trying to write an unregistered pref: " + std::string(path);
+    env->ThrowNew(clazz, msg.c_str());
+    return;
+#else
     DUMP_WILL_BE_NOTREACHED()
         << "Trying to write an unregistered pref: " << path;
     return;
+#endif
   }
   if (pref->GetType() != new_value.type()) {
     NOTREACHED() << "Trying to set pref " << path << " of type "
