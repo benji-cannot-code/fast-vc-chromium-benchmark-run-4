7b39ea47b1e0d5e25db346d28d10032154b51bac
Peter Kvitek <kvitekp@chromium.org>
2026-07-17 14:34:36 -0700

    [headless] Deflake headless mode StartFullscreenSwitch* tests
    This CL adds logic to ensure the browser window is resized and ready
    before taking it's size.
    
    Bug: 424797525, 520311348, 520432613, 423951863
    Change-Id: Iab673ebb6eef77ce5cb6242201f10157560805f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8115757
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664177}

--- chrome/browser/headless/test/data/protocol/sanity/start-fullscreen-switch.js
+++ chrome/browser/headless/test/data/protocol/sanity/start-fullscreen-switch.js
@@ -10,10 +10,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const {session} =
       await testRunner.startBlank('Tests --start-fullscreen switch.');
 
+  // Wait for the browser window to be resized to fullscreen and ready.
+  await session.evaluateAsync(() => new Promise(resolve => {
+                                function checkSize() {
+                                  if (window.outerWidth === screen.width &&
+                                      window.outerHeight === screen.height) {
+                                    resolve();
+                                    return;
+                                  }
+                                  window.addEventListener(
+                                      'resize', checkSize, {once: true});
+                                }
+                                checkSize();
+                              }));
+
   const expression = `
       let lines = [];
       lines.push('Screen: ' + screen.width + 'x' + screen.height);
-      lines.push('Outer: ' + outerWidth + 'x' + outerHeight);
+      lines.push('Outer: ' + window.outerWidth + 'x' + window.outerHeight);
       lines.join(', ');
     `;
 
