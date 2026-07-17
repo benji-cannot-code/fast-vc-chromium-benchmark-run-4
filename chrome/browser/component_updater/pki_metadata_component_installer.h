27630dc2c142f310fe56eeec40d7d9be1ebec2a5
Carlos IL <carlosil@chromium.org>
2026-07-17 09:58:24 -0700

    Parse the correct mtc_config.pb file in pki metadata component
    The installer was previously trying to parse a SignerSet message
    directly, but the contents of the component are an MTCConfig message,
    which wraps a SignerSet.
    
    Fixed: 535696684
    Change-Id: Ie176ebdc6b7303509795d7a5c01b6c6c347a457f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8112317
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663996}

--- chrome/browser/component_updater/pki_metadata_component_installer.h
+++ chrome/browser/component_updater/pki_metadata_component_installer.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/component_updater/component_installer.h"
 #include "mojo/public/cpp/base/proto_wrapper.h"
 #include "net/base/hash_value.h"
+#include "net/cert/root_store_proto_lite/mtc_config.pb.h"
 #include "net/net_buildflags.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/protobuf/src/google/protobuf/repeated_field.h"
@@ -106,8 +107,8 @@ class PKIMetadataComponentInstallerService final {
 #if BUILDFLAG(CHROME_ROOT_STORE_SUPPORTED)
   static std::optional<mojo_base::ProtoWrapper> ParseChromeRootStore(
       const base::FilePath& crs_pb_path);
-  static std::optional<mojo_base::ProtoWrapper> ParseSignerSet(
-      const base::FilePath& signer_pb_path);
+  static std::optional<chrome_root_store::MtcConfig> ParseMtcConfig(
+      const base::FilePath& mtc_config_pb_path);
 
   // Updates SystemNetworkContextManager and cert verifiers with the component
   // delivered Chrome Root Store data and optional Signer Set data.
