84b9f98bfabcb397a2445f2ef8f6e43740904c0c
Fernando Ramirez <ferny@google.com>
2026-07-16 17:27:45 -0700

    [AF] Hide omnibox autofill chip on frame containing trigger field
    Design: go/autofill-omnibox-dd
    Fixed: 490215469
    Change-Id: I0ffa52eaad0f223e474a6557e07b78d540786e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8110737
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663595}

--- components/autofill/core/browser/ui/payments/omnibox_autofill_delegate_unittest.cc
+++ components/autofill/core/browser/ui/payments/omnibox_autofill_delegate_unittest.cc
@@ -470,6 +470,9 @@ TEST_F(OmniboxAutofillDelegateTest,
 
 TEST_F(OmniboxAutofillDelegateTest,
        OnAutofillManagerStateChanged_WasActive_HideChip) {
+  FormData form = CreateTestCreditCardFormData();
+  FormsSeen({form});
+
   payments_autofill_client().ShowOmniboxAutofillChip(
       /*suggestions=*/{},
       /*on_suggestions_shown=*/base::DoNothing(),
@@ -527,6 +530,9 @@ TEST_F(OmniboxAutofillDelegateTest,
 
 TEST_F(OmniboxAutofillDelegateTest,
        OnAutofillManagerStateChanged_WasNotActive_DoesNotHideChip) {
+  FormData form = CreateTestCreditCardFormData();
+  FormsSeen({form});
+
   payments_autofill_client().ShowOmniboxAutofillChip(
       /*suggestions=*/{},
       /*on_suggestions_shown=*/base::DoNothing(),
