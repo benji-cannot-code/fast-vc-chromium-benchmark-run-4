7411c205d03ac2bd3f0f61caad7c529581410939
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-17 12:22:21 -0700

    [Batch] MediaViewerUtilsTest
    Bug: 535536257
    Change-Id: Iedb4a19a748ebc11d6e8c6106ae4fb246a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118287
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664106}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/MediaViewerUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/MediaViewerUtilsTest.java
@@ -19,6 +19,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
@@ -33,6 +34,7 @@ import org.chromium.ui.util.ColorUtils;
 /** Integration test suite for the MediaViewerUtils. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@Batch(Batch.PER_CLASS)
 public class MediaViewerUtilsTest {
     @Rule
     public CustomTabActivityTestRule mCustomTabActivityTestRule = new CustomTabActivityTestRule();
