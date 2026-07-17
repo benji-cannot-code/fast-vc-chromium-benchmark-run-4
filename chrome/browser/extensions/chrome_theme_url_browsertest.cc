76ed3b3983e77c97d363b39018147faca56d0c3d
Mickey Burks <mickeyburks@chromium.org>
2026-07-17 15:17:04 -0700

    [WebUI] Register ThemeSource explicitly, part 6.
    In this part adding ThemeSource registration in
     - chrome_theme_url_browsertest.cc
    
    For details, refer to go/chrome-webui-explicit-theme-source-analysis
    
    Bug: 40757220
    Change-Id: Ib85ba8594e629ef9d3b1189a1f85165fc87b252b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8104770
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664201}

--- chrome/browser/extensions/chrome_theme_url_browsertest.cc
+++ chrome/browser/extensions/chrome_theme_url_browsertest.cc
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/extensions/extension_browsertest.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/theme_source.h"
+#include "content/public/browser/url_data_source.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/extension_registrar.h"
 #include "extensions/test/extension_test_message_listener.h"
@@ -13,6 +16,8 @@ namespace extensions {
 // Tests that chrome://theme/ URLs are only accessible to component extensions.
 IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
                        OnlyComponentExtensionsCanAccessChromeThemeUrls) {
+  content::URLDataSource::Add(profile(),
+                              std::make_unique<ThemeSource>(profile()));
   const base::FilePath extension_path(
       test_data_dir_.AppendASCII("browsertest")
                     .AppendASCII("chrome_theme_url"));
