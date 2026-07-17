1fb913820e27c94c42f4c029fafc8845d4df7c6f
Qingxin Wu <qingxinwu@google.com>
2026-07-17 12:24:14 -0700

    Update WebUI toolbar code to follow static element style guide
    ... to use this.$.xx instead of queryselector for static element (style
    guide docs/webui/webui_lit_style_guide.md). Went through typescript
    files in chrome/browser/resources/webui_toolbar/ and found
    `PinnedToolbarActionElement` didn't follow rule.
    
    Change-Id: I25404a7274eed9411064ebe08c9db94da10f04e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118667
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664109}

--- chrome/browser/resources/webui_toolbar/pinned_toolbar_action.ts
+++ chrome/browser/resources/webui_toolbar/pinned_toolbar_action.ts
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 // <if expr="_google_chrome">
 import './internal/icons.html.js';
-
 // </if>
 
+import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
+
+import type {CrIconButtonElement} from '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import {assertNotReachedCase} from '//resources/js/assert.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
@@ -24,6 +25,12 @@ import {getCss} from './toolbar_button.css.js';
 import {getContextMenuPosition, getContextMenuSourceType, HelpBubbleAnchorMixin, setHasHelpBubble} from './toolbar_button.js';
 
 const PinnedToolbarActionElementBase = HelpBubbleAnchorMixin(CrLitElement);
+
+export interface PinnedToolbarActionElement {
+  $: {
+    button: CrIconButtonElement,
+  };
+}
 
 export class PinnedToolbarActionElement extends PinnedToolbarActionElementBase {
   static get is() {
@@ -154,10 +161,7 @@ export class PinnedToolbarActionElement extends PinnedToolbarActionElementBase {
   // automatically delegate focus to their shadow DOM content unless configured
   // with delegatesFocus, which Lit doesn't do by default for wrapper elements.
   override focus() {
-    const button = this.shadowRoot.querySelector('cr-icon-button');
-    if (button) {
-      button.focus();
-    }
+    this.$.button.focus();
   }
 
   protected onDragstart_(e: DragEvent) {
