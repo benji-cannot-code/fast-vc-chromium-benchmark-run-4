02abe6523c689e80c7ea20c551d0de88f43233a2
Nate Fischer <ntfschr@google.com>
2026-07-17 00:24:13 -0700

    AW: Re-enable NavigationListener and JsJavaInteraction tests
    This re-enables several test cases in NavigationListenerTest and
    JsJavaInteractionTest as their underlying issues (crbug.com/332809183,
    crbug.com/468684305, crbug.com/383517164) have been resolved.
    
    This skips a few cases in NavigationListenerTest because they have a
    different root cause (testNavigationHistoryNavigationBFCacheEnabled
    testNavigationHistoryNavigationToEvictedPageBFCacheEnabled).
    
    TAG=agy
    CONV=892edbaa-62e3-4878-a912-53edc6fa29a0
    
    Fixed: 332809183, 383517164, 468684305
    Test: run_webview_instrumentation_test_apk -f NavigationListenerTest#* --repeat=20
    Test: run_webview_instrumentation_test_apk -f JsJavaInteractionTest#* --repeat=20
    Change-Id: I358d9ab53617e93dd622f4e6165e56ed8c7f6747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8113128
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663709}

--- android_webview/javatests/src/org/chromium/android_webview/test/JsJavaInteractionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/JsJavaInteractionTest.java
@@ -30,7 +30,6 @@ import org.chromium.android_webview.WebMessageListener;
 import org.chromium.android_webview.test.TestAwContentsClient.OnReceivedTitleHelper;
 import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.MessagePayload;
@@ -941,7 +940,6 @@ public class JsJavaInteractionTest extends AwParameterizedTest {
     @Test
     @MediumTest
     @Feature({"AndroidWebView", "JsJavaInteraction"})
-    @DisabledTest(message = "The test was very flaky, see https://crbug.com/383517164")
     public void testJsObjectRemoveOnMessage() throws Throwable {
         addWebMessageListenerOnUiThread(mAwContents, JS_OBJECT_NAME, new String[] {"*"}, mListener);
 
--- android_webview/javatests/src/org/chromium/android_webview/test/NavigationListenerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/NavigationListenerTest.java
@@ -97,7 +97,6 @@ public class NavigationListenerTest extends AwParameterizedTest {
     @LargeTest
     @Feature({"AndroidWebView", "NavigationListener"})
     @CommandLineFlags.Add({"enable-features=EnableNavigationListener"})
-    @DisabledTest(message = "Flaky, see crbug.com/468684305")
     public void testNavigationVariousCases() throws Throwable {
         // Add the special listener object which will receive navigation messages.
         addWebMessageListenerOnUiThread(
@@ -299,7 +298,6 @@ public class NavigationListenerTest extends AwParameterizedTest {
     @MediumTest
     @Feature({"AndroidWebView", "NavigationListener"})
     @CommandLineFlags.Add({"enable-features=EnableNavigationListener"})
-    @DisabledTest(message = "Flaky, see crbug.com/468684305")
     public void testNavigationPageWithIframe() throws Throwable {
         // Add the special listener object which will receive navigation messages.
         setUpAndGetInitialProxy();
@@ -602,7 +600,6 @@ public class NavigationListenerTest extends AwParameterizedTest {
     @MediumTest
     @Feature({"AndroidWebView", "NavigationListener"})
     @CommandLineFlags.Add({"enable-features=EnableNavigationListener"})
-    @DisabledTest(message = "crbug.com/332809183")
     public void testNavigationLoadDataWithBaseURL() throws Throwable {
         // Add the special listener object which will receive navigation messages.
         setUpAndGetInitialProxy();
@@ -639,7 +636,6 @@ public class NavigationListenerTest extends AwParameterizedTest {
     @MediumTest
     @Feature({"AndroidWebView", "NavigationListener"})
     @CommandLineFlags.Add({"enable-features=EnableNavigationListener"})
-    @DisabledTest(message = "crbug.com/332809183")
     public void testNavigationIntercepted() throws Throwable {
         // Add the special listener object which will receive navigation messages.
         setUpAndGetInitialProxy();
@@ -780,7 +776,6 @@ public class NavigationListenerTest extends AwParameterizedTest {
         "disable-features=WebViewBackForwardCache"
     })
     @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO}) // crbug.com/458118167
-    @DisabledTest(message = "Flaky, see crbug.com/468684305")
     public void testNavigationHistoryNavigationBFCacheDisabled() throws Throwable {
         // Navigation #1: Set up the listener and navigate to `url`. This will create a new page and
         // an associated JsReplyProxy.
@@ -892,7 +887,6 @@ public class NavigationListenerTest extends AwParameterizedTest {
     @Feature({"AndroidWebView", "NavigationListener"})
     @CommandLineFlags.Add({"enable-features=EnableNavigationListener"})
     @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO}) // crbug.com/458118167
-    @DisabledTest(message = "Flaky, see crbug.com/468684305")
     public void testNavigationHistoryNavigationBFCacheEnabled_ListenerDisablesBFCache()
             throws Throwable {
         mAwContents.getSettings().setBackForwardCacheEnabled(true);
