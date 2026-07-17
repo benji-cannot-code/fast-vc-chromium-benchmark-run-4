e47c170658dce643e13fb962baf3add5e05e8074
Guillaume Jenkins <gujen@google.com>
2026-07-17 09:51:05 -0700

    [iOS] PreviousActiveDays* metrics: null-check profile
    The PreviousActiveDays* metrics are logged in an asynchronous callback
    after the Feature Engagement Tracker is successfully initialized for the
    current profile. However, it is possible for the profile to change by
    the time the callback is run. This causes the profile to become null in
    the profile state, so the DCHECK in the callback fails.
    
    This CL addresses that by turning the DCHECK into a null check. The
    metric is not critical, so dropping the record during this edge case is
    an acceptable mitigation.
    
    Fixed: 535647631
    Change-Id: I7a493a72041b492cf53b41a1e5a265faed3d9fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8103431
    Auto-Submit: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663986}

--- ios/chrome/app/profile/session_metrics_profile_agent.mm
+++ ios/chrome/app/profile/session_metrics_profile_agent.mm
@@ -166,12 +166,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)onFeatureEngagementTrackerInitialized:(BOOL)success {
-  DCHECK(self.profileState.profile);
-  if (!IsRecordRecentActiveDaysEnabled()) {
-    return;
-  }
-
-  if (!success) {
+  if (!IsRecordRecentActiveDaysEnabled() || !self.profileState.profile ||
+      !success) {
     return;
   }
 
