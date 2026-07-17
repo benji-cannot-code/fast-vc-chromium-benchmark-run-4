e1aeb8332311d4537171920d022138728a8c79ee
Kent Tamura <tkent@chromium.org>
2026-07-16 23:07:25 -0700

    Propagate block-end annotation space from previous sibling block
    This is part of a phased reland of https://crrev.com/c/8000812.
    
    Apply ConstraintSpace::PreviousSiblingBlockEndAnnotationSpace() in
    ComputeInitialBlockStartAnnotationSpace(). Also, remove the `overflow`
    condition because NewFormattingContext check is enough.
    
    Fixed: 445727139, 532769350
    Change-Id: I4ad33f91b131b9a4d480b8d8c0b67b7e6fbc964b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8113826
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663687}

--- third_party/blink/renderer/core/layout/block_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/block_layout_algorithm_test.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/block_node.h"
 #include "third_party/blink/renderer/core/layout/constraint_space.h"
 #include "third_party/blink/renderer/core/layout/constraint_space_builder.h"
+#include "third_party/blink/renderer/core/layout/inline/inline_cursor.h"
 #include "third_party/blink/renderer/core/layout/layout_block_flow.h"
 #include "third_party/blink/renderer/core/layout/layout_object_inlines.h"
 #include "third_party/blink/renderer/core/layout/layout_result.h"
@@ -2472,7 +2473,7 @@ TEST_F(BlockLayoutAlgorithmTest, ComputeInitialBlockStartAnnotationSpace) {
         contain: paint;
       }
     </style>
-    <div id="target1"></div>
+    <div id="target1"><ruby>base<rt>annotation</rt></ruby></div>
     <div id="target2"></div>
     <div id="target3"></div>
     <div id="target4"></div>
@@ -2499,7 +2500,17 @@ TEST_F(BlockLayoutAlgorithmTest, ComputeInitialBlockStartAnnotationSpace) {
   // margin-top(20) + padding-top(10) = 30
   {
     ScopedAnnotationSpaceOnStartForTest enable_flag(true);
-    BlockLayoutAlgorithm algorithm({node1, fragment_geometry1, space1});
+    ConstraintSpaceBuilder builder(
+        WritingMode::kHorizontalTb,
+        {WritingMode::kHorizontalTb, TextDirection::kLtr},
+        node1.CreatesNewFormattingContext());
+    builder.SetAvailableSize(LogicalSize(LayoutUnit(1000), kIndefiniteSize));
+    builder.SetPercentageResolutionSize(
+        LogicalSize(LayoutUnit(1000), kIndefiniteSize));
+    builder.SetInlineAutoBehavior(AutoSizeBehavior::kStretchImplicit);
+    builder.SetContainsAnnotations(true);
+    ConstraintSpace space = builder.ToConstraintSpace();
+    BlockLayoutAlgorithm algorithm({node1, fragment_geometry1, space});
     EXPECT_EQ(LayoutUnit(30),
               algorithm.ComputeInitialBlockStartAnnotationSpace());
   }
@@ -2580,6 +2591,12 @@ TEST_F(BlockLayoutAlgorithmTest, PreviousSiblingBlockEndAnnotationSpace) {
         border-top: 0 solid black;
         overflow: visible;
       }
+      #target7 {
+        margin-top: 0px;
+        padding-top: 0px;
+        border-top: 0 solid black;
+        overflow: clip;
+      }
       .prev-ruby { font-size: 10px; line-height: 60px; }
       .prev-rt { font-size: 5px; line-height: 5px; ruby-position: under; }
       .prev-no-ruby {
@@ -2657,6 +2674,16 @@ TEST_F(BlockLayoutAlgorithmTest, PreviousSiblingBlockEndAnnotationSpace) {
         <ruby class="target-ruby">base<rt class="target-rt">annotation</rt></ruby>
       </div>
     </div>
+
+    <!-- Case 7: target has overflow:clip -->
+    <div id="c7" class="container">
+      <div id="prev7" class="prev-basic">
+        <ruby class="prev-ruby">base<rt class="prev-rt">annotation</rt></ruby>
+      </div>
+      <div id="target7">
+        <ruby class="target-ruby">base<rt class="target-rt">annotation</rt></ruby>
+      </div>
+    </div>
   )HTML");
   UpdateAllLifecyclePhasesForTest();
 
@@ -2678,6 +2705,40 @@ TEST_F(BlockLayoutAlgorithmTest, PreviousSiblingBlockEndAnnotationSpace) {
 
   const auto* prev6 = GetLayoutBoxByElementId("prev6")->GetLayoutResult(0);
   EXPECT_GT(prev6->BlockEndAnnotationSpace(), LayoutUnit(0));
+
+  const auto* prev7 = GetLayoutBoxByElementId("prev7")->GetLayoutResult(0);
+  EXPECT_GT(prev7->BlockEndAnnotationSpace(), LayoutUnit(0));
+
+  // Helper to get first line offset inside target
+  auto get_first_line_offset = [this](const char* target_id) -> LayoutUnit {
+    LayoutBlockFlow* block_flow = GetLayoutBlockFlowByElementId(target_id);
+    InlineCursor cursor(*block_flow);
+    cursor.MoveToFirstLine();
+    DCHECK(cursor.IsNotNull());
+    return cursor.Current().OffsetInContainerFragment().top;
+  };
+
+  // target1, target6, and target7 have PreviousSiblingBlockEndAnnotationSpace
+  // from their previous siblings. They have line-height 60px, so their
+  // BlockEndAnnotationSpace is around 25px.
+  // Targets' annotation overflow is 20px (because rt font-size is 20px).
+  // Since 20px <= 25px, the overflow is fully accommodated, so offset should
+  // be 0.
+  //
+  // target3, target4, target5 have no PreviousSiblingBlockEndAnnotationSpace,
+  // so their lines should be pushed down by the 20px annotation.
+  //
+  // target2 also should be pushed down because prev2 has height constraint and
+  // no annotation space.
+  constexpr LayoutUnit kPushDown(20);
+  constexpr LayoutUnit kOverflowToPrevious(0);
+  EXPECT_EQ(kOverflowToPrevious, get_first_line_offset("target1"));
+  EXPECT_EQ(kPushDown, get_first_line_offset("target2"));
+  EXPECT_EQ(kPushDown, get_first_line_offset("target3"));
+  EXPECT_EQ(kPushDown, get_first_line_offset("target4"));
+  EXPECT_EQ(kPushDown, get_first_line_offset("target5"));
+  EXPECT_EQ(kOverflowToPrevious, get_first_line_offset("target6"));
+  EXPECT_EQ(kOverflowToPrevious, get_first_line_offset("target7"));
 }
 
 }  // namespace
