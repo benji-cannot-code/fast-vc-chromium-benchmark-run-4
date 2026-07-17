c2866aafc16677332ca5d9a403ff005bca629786
Dominique Fauteux-Chapleau <domfc@chromium.org>
2026-07-17 05:19:02 -0700

    Apply DataControlsRule to FindBar text prepoluation
    This CL adds logic so that the FindBar classes will not
    use text from another tab to prepopulate itself if
    copy-pasting from that tab is restricted by policy.
    
    Bug: 512294128
    Change-Id: Ia7b4582f137dcf6b08ea6fec29edf91d6c7418b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7886100
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663825}

--- chrome/browser/ui/find_bar/find_bar_state.h
+++ chrome/browser/ui/find_bar/find_bar_state.h
@@ -6,13 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_FIND_BAR_FIND_BAR_STATE_H_
 #define CHROME_BROWSER_UI_FIND_BAR_FIND_BAR_STATE_H_
 
+#include <optional>
 #include <string>
 
 #include "base/memory/raw_ptr.h"
 #include "components/find_in_page/find_tab_helper.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 
 class Profile;
+
+namespace content {
+class WebContents;
+}
 
 namespace content {
 class BrowserContext;
@@ -35,12 +41,15 @@ class FindBarState : public KeyedService,
   static void ConfigureWebContents(content::WebContents* web_contents);
 
   // find_in_page::FindTabHelper::Delegate:
-  void SetLastSearchText(const std::u16string& text) override;
-  std::u16string GetSearchPrepopulateText() override;
+  void SetLastSearchText(const std::u16string& text,
+                         content::WebContents* web_contents) override;
+  std::u16string GetSearchPrepopulateText(
+      content::WebContents* web_contents) override;
 
  private:
   raw_ptr<Profile> profile_;
   std::u16string last_prepopulate_text_;
+  std::optional<ui::DataTransferEndpoint> last_prepopulate_source_dte_;
 };
 
 #endif  // CHROME_BROWSER_UI_FIND_BAR_FIND_BAR_STATE_H_
