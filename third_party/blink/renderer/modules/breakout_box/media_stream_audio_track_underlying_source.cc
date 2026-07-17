ee7b6f5efe51fe7f45ad28d3ad2b71326c2db28c
Guido Urdaneta <guidou@chromium.org>
2026-07-17 04:59:51 -0700

    [BreakoutBox] Disable BreakoutBoxExposePageRelativeAudioCaptureTime
    There is a bug that results in incorrect timestamps exposed by
    MediaStreamTrackProcessor and which can cause regressions in
    applications using it together with MediaRecorder.
    
    Bug: 535904638
    Change-Id: I8dfae36ead99d8b6603ab0fe57bb608d362a3724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116926
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663817}

--- third_party/blink/renderer/modules/breakout_box/media_stream_audio_track_underlying_source.cc
+++ third_party/blink/renderer/modules/breakout_box/media_stream_audio_track_underlying_source.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 BASE_FEATURE(kBreakoutBoxExposePageRelativeAudioCaptureTime,
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Expects all calls to SetFormat() and CopyIntoAudioBuffer() to come from the
 // same thread/sequence. This is almost certainly the realtime Audio capture
