2ff14ca77d523ab1537afbc10982918370d09972
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-07-17 14:53:13 -0700

    [Extensions] Remove DevicePermissionsManager::AllowHidDevice()
    It's never used in production.
    
    There's lots more HID-related stuff that can be removed, but this is
    some easy low-hanging fruit.
    
    Bug: 482925048
    Change-Id: Ibd8aed7efc39a862ce80605e75b15eaa76db541e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8112947
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664186}

--- extensions/browser/api/device_permissions_manager.cc
+++ extensions/browser/api/device_permissions_manager.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "content/public/browser/browser_thread.h"
-#include "extensions/browser/api/hid/hid_device_manager.h"
 #include "extensions/browser/api/usb/usb_device_manager.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_prefs.h"
@@ -266,15 +265,6 @@ DevicePermissionEntry::DevicePermissionEntry(
   if (device.product_name)
     product_string_ = *device.product_name;
 }
-
-DevicePermissionEntry::DevicePermissionEntry(
-    const device::mojom::HidDeviceInfo& device)
-    : device_guid_(device.guid),
-      type_(Type::HID),
-      vendor_id_(device.vendor_id),
-      product_id_(device.product_id),
-      serial_number_(base::UTF8ToUTF16(device.serial_number)),
-      product_string_(base::UTF8ToUTF16(device.product_name)) {}
 
 DevicePermissionEntry::DevicePermissionEntry(
     Type type,
@@ -534,42 +524,6 @@ void DevicePermissionsManager::AllowUsbDevice(
     UsbDeviceManager* device_manager = UsbDeviceManager::Get(context_);
     DCHECK(device_manager);
     device_manager->EnsureConnectionWithDeviceManager();
-  }
-}
-
-void DevicePermissionsManager::AllowHidDevice(
-    const ExtensionId& extension_id,
-    const device::mojom::HidDeviceInfo& device) {
-  DCHECK(thread_checker_.CalledOnValidThread());
-  DevicePermissions* device_permissions = GetForExtension(extension_id);
-
-  auto device_entry = base::MakeRefCounted<DevicePermissionEntry>(device);
-
-  if (device_entry->IsPersistent()) {
-    for (const auto& entry : device_permissions->entries()) {
-      if (entry->vendor_id() == device_entry->vendor_id() &&
-          entry->product_id() == device_entry->product_id() &&
-          entry->serial_number() == device_entry->serial_number()) {
-        return;
-      }
-    }
-
-    device_permissions->entries_.insert(device_entry);
-    SaveDevicePermissionEntry(context_, extension_id, device_entry);
-  } else if (!device_permissions->ephemeral_hid_devices_.contains(
-                 device.guid)) {
-    // Non-persistent devices cannot be reliably identified when they are
-    // reconnected so such devices are only remembered until disconnect.
-    // Register an observer here so that this set doesn't grow undefinitely.
-    device_permissions->entries_.insert(device_entry);
-    device_permissions->ephemeral_hid_devices_[device.guid] = device_entry;
-
-    // Make sure the HidDeviceManager is active. HidDeviceManager is
-    // responsible for removing the permission entry for an ephemeral hid
-    // device. Only do this when an ephemeral device has been added.
-    HidDeviceManager* device_manager = HidDeviceManager::Get(context_);
-    DCHECK(device_manager);
-    device_manager->LazyInitialize();
   }
 }
 
