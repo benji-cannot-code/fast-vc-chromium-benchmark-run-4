dd250cc3cdc908c72a4fa5770ae1e4c92d282d02
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-17 09:23:13 -0700

    [Batch] NavigationSheetTest
    Bug: 535536257
    Change-Id: Iee1ae043b73834714c226fb73071f4896a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8105105
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663956}

--- chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationSheetTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationSheetTest.java
@@ -23,9 +23,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.R;
@@ -55,6 +57,7 @@ import java.util.concurrent.ExecutionException;
 /** Tests for the gesture navigation sheet. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@Batch(Batch.PER_CLASS)
 public class NavigationSheetTest {
     @Rule
     public FreshCtaTransitTestRule mActivityTestRule =
@@ -255,6 +258,7 @@ public class NavigationSheetTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.PHONE)
+    @RequiresRestart
     public void testLongPressBackAfterActivityDestroy() {
         KeyEvent event = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_BACK);
         ChromeTabbedActivity activity = mActivityTestRule.getActivity();
