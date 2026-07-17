d3bfae64a1f33c8a44ca83760001c75d8e4ea40d
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-17 08:01:05 -0700

    Optimize TabGroupListBottomSheetTest batching
    Align feature flags at class level and use fastAutoResetCtaActivityRule
    to enable batching of phone tests and reduce restart overhead.
    
    Bug: 535536257
    Change-Id: I6fa7b91b723bf045fe0919d90a9762856a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118006
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663902}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetTest.java
@@ -23,7 +23,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.ImportantFormFactors;
@@ -52,26 +51,22 @@ import org.chromium.ui.test.util.DeviceRestriction;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
 // TODO(crbug.com/419289558): Re-enable color surface feature flags
-@Features.DisableFeatures({
+@DisableFeatures({
     ChromeFeatureList.ANDROID_SURFACE_COLOR_UPDATE,
     ChromeFeatureList.GRID_TAB_SWITCHER_SURFACE_COLOR_UPDATE,
-    ChromeFeatureList.ANDROID_THEME_MODULE
+    ChromeFeatureList.ANDROID_THEME_MODULE,
+    ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW
 })
+@EnableFeatures(ChromeFeatureList.NEW_TAB_PAGE_CUSTOMIZATION_V2 + ":show_tip_bottom_sheet/false")
 @Batch(Batch.PER_CLASS)
 @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511288348
 public class TabGroupListBottomSheetTest {
     @Rule
     public AutoResetCtaTransitTestRule mCtaTestRule =
-            ChromeTransitTestRules.autoResetCtaActivityRule();
+            ChromeTransitTestRules.fastAutoResetCtaActivityRule();
 
     @Test
     @MediumTest
-    @EnableFeatures(
-            ChromeFeatureList.NEW_TAB_PAGE_CUSTOMIZATION_V2 + ":show_tip_bottom_sheet/false")
-    @DisableFeatures({
-        ChromeFeatureList.SUBMENUS_IN_APP_MENU,
-        ChromeFeatureList.SUBMENUS_IN_APP_MENU_LFF
-    })
     public void testNewGroup_RegularNewTabPageStation() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         WebPageStation pageStation =
@@ -97,10 +92,6 @@ public class TabGroupListBottomSheetTest {
 
     @Test
     @MediumTest
-    @DisableFeatures({
-        ChromeFeatureList.SUBMENUS_IN_APP_MENU,
-        ChromeFeatureList.SUBMENUS_IN_APP_MENU_LFF
-    })
     public void testNewGroup_RegularWebPageStation() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         WebPageStation pageStation =
@@ -128,7 +119,6 @@ public class TabGroupListBottomSheetTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.PHONE)
-    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testNewGroup_IncognitoNewTabPageStation_Phone() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         WebPageStation pageStation =
@@ -195,7 +185,6 @@ public class TabGroupListBottomSheetTest {
     @Test
     @MediumTest
     @Restriction(DeviceFormFactor.PHONE)
-    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testNewGroup_IncognitoWebPageStation_Phone() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         WebPageStation pageStation =
