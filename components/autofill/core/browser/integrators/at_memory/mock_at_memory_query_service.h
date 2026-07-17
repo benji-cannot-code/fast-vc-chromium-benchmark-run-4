ed0d72a7268ad3004dfcafa7830f38cea73db74f
Gianmarco Picarella <picarella@google.com>
2026-07-17 01:28:20 -0700

    Implement authentication and SPIIs unmasking in `AtMemoryQueryService`
    This CL is a refinement of [1]. It brings the following changes:
    
    * Exposes a new method
    `AtMemoryQueryService::AuthenticateAndFetchPiiEntity(..)` which
    implements device authentication and SPII unmasking from
    `PersonalContextService`.
    
    * Implements unit tests to verify that the new logic works as
    expected.
    
    * Implements histogram `Autofill.AtMemory.ReauthToUnmask` recorded
    whenever the user performs device authentication in order to unmask a
    sensitive PII entity.
    
    [1]:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/8028343
    
    Bug: 525386262, 525385535
    Change-Id: Iae5ae6c3f83035c7233d94a9c1bb2e76913fc8d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8074418
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Gianmarco Picarella <picarella@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663734}

--- components/autofill/core/browser/integrators/at_memory/mock_at_memory_query_service.h
+++ components/autofill/core/browser/integrators/at_memory/mock_at_memory_query_service.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+class AutofillClient;
+
 class MockAtMemoryQueryService : public AtMemoryQueryService {
  public:
   MockAtMemoryQueryService();
@@ -27,6 +29,17 @@ class MockAtMemoryQueryService : public AtMemoryQueryService {
        std::u16string_view title,
        base::RepeatingCallback<
            void(accessibility_annotator::MemorySearchResults)> update_callback),
+      (override));
+
+  MOCK_METHOD(
+      void,
+      AuthenticateAndFetchPiiEntity,
+      (const AutofillClient& client,
+       const std::u16string& auth_message,
+       std::u16string_view masked_value,
+       accessibility_annotator::MemoryDataType data_type,
+       base::span<const accessibility_annotator::EntryMetadata> metadata_list,
+       FetchUnmaskedPiiEntitiesCallback callback),
       (override));
 };
 
