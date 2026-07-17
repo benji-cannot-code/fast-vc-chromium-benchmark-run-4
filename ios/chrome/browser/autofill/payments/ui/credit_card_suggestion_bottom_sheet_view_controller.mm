91f7590a76646250c5c13643c726c35d8b5dc1c2
Vincent Boisselle <vincb@google.com>
2026-07-17 15:02:14 -0700

    [iOS] Bold full header subtitle in bottom sheets
    Bolds the full header subtitle label text in both
    CreditCardSuggestionBottomSheetViewController and
    CredentialSuggestionBottomSheetViewController.
    
    UX mock deck: http://shortn/_kljKBRuetV
    
    Bug: 532580473
    Change-Id: I193a140db8476f7bc2d15aa2fa8ecce1d3711822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8110477
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664195}

--- ios/chrome/browser/autofill/payments/ui/credit_card_suggestion_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/autofill/payments/ui/credit_card_suggestion_bottom_sheet_view_controller.mm
@@ -436,7 +436,8 @@ CGFloat const kTitleLogoHeight = 32;
 #pragma mark - ConfirmationAlertViewController
 
 - (void)customizeSubtitle:(UITextView*)subtitle {
-  subtitle.textContainerInset = UIEdgeInsetsZero;
+  subtitle.font =
+      PreferredFontForTextStyle(UIFontTextStyleFootnote, UIFontWeightSemibold);
 }
 
 @end
