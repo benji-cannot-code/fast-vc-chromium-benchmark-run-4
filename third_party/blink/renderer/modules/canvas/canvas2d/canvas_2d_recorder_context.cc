496b2357e7aead40ae175fd5d25982c9819c5851
Kevin Babbitt <kbabbitt@microsoft.com>
2026-07-17 09:34:50 -0700

    Canvas2D: Respect EXIF orientation for cross-origin CSSImageValue
    Enforce kRespectImageOrientation for any source that would taint the
    origin, including CSS image values.
    
    The IsImageBitmap() || IsImageElement() checks which previously gated
    the WouldTaintOrigin() check had been introduced to fix a CHECK failure
    in SVGImage::CheckLoaded(); see https://crrev.com/c/2521049. However, in
    https://crrev.com/c/5892795 we removed the call to CheckLoaded() on this
    path, so the image type gates are no longer needed.
    
    Fixed: 517655953
    Change-Id: I1e1838c3af56264fb49c4086c9cf6ae28fe84d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102890
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663972}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_2d_recorder_context.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_2d_recorder_context.cc
@@ -856,8 +856,7 @@ void Canvas2DRecorderContext::reset() {
 RespectImageOrientationEnum
 Canvas2DRecorderContext::RespectImageOrientationInternal(
     CanvasImageSource* image_source) {
-  if ((image_source->IsImageBitmap() || image_source->IsImageElement()) &&
-      image_source->WouldTaintOrigin()) {
+  if (image_source->WouldTaintOrigin()) {
     return kRespectImageOrientation;
   }
   return RespectImageOrientation();
