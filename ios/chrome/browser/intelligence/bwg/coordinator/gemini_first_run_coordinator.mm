a8960a263b7abad173e1474518c31e72481b60c6
Yasaman Sedaghat <yasamans@google.com>
2026-07-17 12:39:21 -0700

    [ios] flag guard a crash fix because it is introduced post early release
    to be extra safe
    
    Fixed: 531793291
    Change-Id: I0b4ea982be7bbc12a6b8cebbf8fa662b1dabb32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8115891
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Yasaman Sedaghat <yasamans@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664117}

--- ios/chrome/browser/intelligence/bwg/coordinator/gemini_first_run_coordinator.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/gemini_first_run_coordinator.mm
@@ -153,13 +153,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stopWithCompletion:(ProceduralBlock)completion {
   // Retain self to survive synchronous teardown from the completion block.
-  __strong __typeof(self) strongSelf = self;
-  GeminiTabHelper* geminiTabHelper = [strongSelf activeWebStateGeminiTabHelper];
-  if (geminiTabHelper) {
-    geminiTabHelper->SetPreventContextualPanelEntryPoint(NO);
+  __strong __typeof(self) strongSelf =
+      IsGeminiCoordinatorTeardownFixEnabled() ? self : nil;
+  if (strongSelf) {
+    GeminiTabHelper* geminiTabHelper =
+        [strongSelf activeWebStateGeminiTabHelper];
+    if (geminiTabHelper) {
+      geminiTabHelper->SetPreventContextualPanelEntryPoint(NO);
+    }
+    [strongSelf presentPageActionMenuIPH];
+  } else {
+    GeminiTabHelper* geminiTabHelper = [self activeWebStateGeminiTabHelper];
+    if (geminiTabHelper) {
+      geminiTabHelper->SetPreventContextualPanelEntryPoint(NO);
+    }
+    [self presentPageActionMenuIPH];
   }
 
-  [strongSelf presentPageActionMenuIPH];
   _viewController = nil;
   _geminiHandler = nil;
   _helpCommandsHandler = nil;
@@ -169,7 +179,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _tracker = nil;
   _completion = nil;
   if (!_consentCompletion) {
-    [strongSelf dismissPresentedViewWithCompletion:completion];
+    if (strongSelf) {
+      [strongSelf dismissPresentedViewWithCompletion:completion];
+    } else {
+      [self dismissPresentedViewWithCompletion:completion];
+    }
   }
   [super stop];
 }
