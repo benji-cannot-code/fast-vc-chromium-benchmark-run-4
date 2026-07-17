8f6d7725e554676e553cec3fa29df7f24f9e8922
Lei Zhang <thestig@chromium.org>
2026-07-16 17:04:06 -0700

    [PDF] Avoid no-scripting crash in PdfViewWebPluginClient::PostMessage()
    Add a call to blink::WebPluginScriptForbiddenScope::IsForbidden() and
    return early in PdfViewWebPluginClient::PostMessage() when scripting is
    not allowed. This avoids a crash soon after when PostMessage() calls
    blink::WebSerializedScriptValue::Serialize().
    
    Bug: 515786546
    Change-Id: I160a95b17a93118934a38b66d79c41dfc2949824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7998839
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663586}

--- components/pdf/renderer/pdf_view_web_plugin_client.cc
+++ components/pdf/renderer/pdf_view_web_plugin_client.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/public/web/web_local_frame_client.h"
 #include "third_party/blink/public/web/web_plugin_container.h"
+#include "third_party/blink/public/web/web_plugin_script_forbidden_scope.h"
 #include "third_party/blink/public/web/web_serialized_script_value.h"
 #include "third_party/blink/public/web/web_view.h"
 #include "third_party/blink/public/web/web_widget.h"
@@ -94,7 +95,7 @@ blink::WebURL PdfViewWebPluginClient::CompleteURL(
 }
 
 void PdfViewWebPluginClient::PostMessage(base::DictValue message) {
-  if (!HasFrame()) {
+  if (!HasFrame() || blink::WebPluginScriptForbiddenScope::IsForbidden()) {
     return;
   }
 
