58f95d94e82dc5e69e80424d65bf070a01a83193
Rafał Godlewski <rgod@google.com>
2026-07-17 01:24:03 -0700

    [iOS][WebAuthn] Handle signalUnknownCredential in PasskeyTabHelper
    * Implements validating the origin. For now remote validation is
    skipped as it expects that all params structs would have a RequestID()
    [1] function, whereas request_id is not strictly necessary for signal
    calls (the promise is not deferred). I left a todo and will implement it
    for all signal functions at once.
    * Uses quota tracker to rate limit requests.
    * Implements hiding the passkey, if not already hidden
    
    [1]
    https://source.chromium.org/chromium/chromium/src/+/main:components/webauthn/ios/passkey_tab_helper.mm;l=313;drc=c7d0034ef082c70a69354c51c6b165595ad32911
    
    Bug: 460487030
    Change-Id: Ife92c2626f7150c3dd107afae5e870697806eaf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8103544
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663731}

--- components/webauthn/ios/passkey_tab_helper.mm
+++ components/webauthn/ios/passkey_tab_helper.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/password_manager/core/browser/password_store/password_store_interface.h"
 #import "components/webauthn/core/browser/client_data_json.h"
 #import "components/webauthn/core/browser/common_utils.h"
+#import "components/webauthn/core/browser/passkey_change_quota_tracker.h"
 #import "components/webauthn/core/browser/passkey_model.h"
 #import "components/webauthn/core/browser/passkey_model_utils.h"
 #import "components/webauthn/core/browser/remote_validation.h"
@@ -360,9 +361,38 @@ void PasskeyTabHelper::HandleCreateRequestedEvent(
 }
 
 void PasskeyTabHelper::HandleSignalUnknownCredentialEvent(
+    const url::Origin& origin,
     SignalUnknownCredentialParams params) {
-  // TODO(crbug.com/460487030): Implement browser-layer logic.
-  return;
+  if (OriginAllowedToMakeWebAuthnRequests(origin) !=
+      ValidationStatus::kSuccess) {
+    return;
+  }
+
+  if (!OriginIsAllowedToClaimRelyingPartyId(params.rp_id, origin)) {
+    // TODO(crbug.com/460487030): Perform remote RP ID validation.
+    return;
+  }
+
+  PasskeyChangeQuotaTracker* quota_tracker =
+      PasskeyChangeQuotaTracker::GetInstance();
+  if (!quota_tracker->CanMakeChange(origin)) {
+    return;
+  }
+
+  std::string credential_id(params.credential_id.begin(),
+                            params.credential_id.end());
+  std::optional<sync_pb::WebauthnCredentialSpecifics> credential_specifics =
+      passkey_model_->GetPasskey(
+          params.rp_id, credential_id,
+          webauthn::PasskeyModel::ShadowedCredentials::kExclude);
+  if (!credential_specifics || credential_specifics->hidden()) {
+    return;
+  }
+
+  passkey_model_->HidePasskey(credential_id, base::Time::Now());
+  quota_tracker->TrackChange(origin);
+  // TODO(crbug.com/460487030): Display UI confirmation.
+  // TODO(crbug.com/460487030): Log metrics.
 }
 
 void PasskeyTabHelper::HandleCreateRequestedEvent(
