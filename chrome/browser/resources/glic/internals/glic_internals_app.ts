7d79a48b5995119aeb881779362a4ceaa00ce02f
Marke Hallowell <wry@chromium.org>
2026-07-17 08:24:20 -0700

    [GlicInvoke] Allow for basic tab selection in debug controls.
    Adds a very simple mechanism for invoking into specific tabs. Tabs are
    pulled at load time and there is a button to manually refresh them.
    
    Bug: b:535561068
    Change-Id: I39c8b46dba74df8f6ac063aaeafddfce6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8109382
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663919}

--- chrome/browser/resources/glic/internals/glic_internals_app.ts
+++ chrome/browser/resources/glic/internals/glic_internals_app.ts
@@ -56,6 +56,8 @@ export class GlicInternalsAppElement extends CrLitElement {
       selectedTabIndex_: {type: Number},
       invokeConversationType_: {type: String},
       invokeConversationId_: {type: String},
+      invokeSpecificTabIndex_: {type: Number},
+      availableTabs_: {type: Array},
       tabNames_: {type: Array},
       featureModeEnumValues_: {type: Array},
     };
@@ -84,6 +86,8 @@ export class GlicInternalsAppElement extends CrLitElement {
       FreCompletionWaitMode.kDefault;
   protected accessor invokeConversationType_: string = 'default';
   protected accessor invokeConversationId_: string = '';
+  protected accessor invokeSpecificTabIndex_: number = 0;
+  protected accessor availableTabs_: string[] = [];
 
   protected accessor selectedTabIndex_: number = 0;
   protected accessor tabNames_: string[] = ['General', 'Debug Controls'];
@@ -319,8 +323,26 @@ export class GlicInternalsAppElement extends CrLitElement {
         (e.target as HTMLInputElement).value;
   }
 
-  protected onInvokeSurfaceTypeChange_(e: Event) {
+  protected async onInvokeSurfaceTypeChange_(e: Event) {
     this.invokeSurfaceType_ = (e.target as HTMLSelectElement).value;
+    if (this.invokeSurfaceType_ === 'specificTab') {
+      await this.refreshOpenTabs_();
+    }
+  }
+
+  protected async refreshOpenTabs_() {
+    const {tabTitles} = await this.pageHandler_.getOpenTabs();
+    this.availableTabs_ = tabTitles;
+    this.invokeSpecificTabIndex_ = 0;
+  }
+
+  protected onRefreshTabsClick_() {
+    this.refreshOpenTabs_();
+  }
+
+  protected onInvokeSpecificTabIndexChange_(e: Event) {
+    this.invokeSpecificTabIndex_ =
+        Number((e.target as HTMLSelectElement).value);
   }
 
   protected onInvokeZssOverrideChange_(e: Event) {
@@ -347,9 +369,12 @@ export class GlicInternalsAppElement extends CrLitElement {
         Number((e.target as HTMLSelectElement).value);
   }
   protected onTriggerInvokeClick_() {
-    const surface = this.invokeSurfaceType_ === 'newTab' ?
-        {newTab: {openInForeground: this.invokeOpenInForeground_}} :
-        {defaultSurface: {}};
+    let surface: TriggerInvokeFromInternalsOptions['surface'];
+    if (this.invokeSurfaceType_ === 'newTab') {
+      surface = {newTab: {openInForeground: this.invokeOpenInForeground_}};
+    } else {
+      surface = {defaultSurface: {}};
+    }
 
     let payload = null;
     if (this.invokeInvocationSource_ === InvocationSource.kUniversalCart) {
@@ -396,6 +421,9 @@ export class GlicInternalsAppElement extends CrLitElement {
       focusOnShow: this.invokeFocusOnShow_,
       freCompletionWaitMode: this.invokeFreCompletionWaitMode_,
       surface: surface,
+      specificTabIndex: this.invokeSurfaceType_ === 'specificTab' ?
+          this.invokeSpecificTabIndex_ :
+          null,
       actuationTarget: this.invokeActuationTarget_,
       showPanel: this.invokeAutoSubmit_ ? this.invokeShowPanel_ : null,
       payload: payload,
