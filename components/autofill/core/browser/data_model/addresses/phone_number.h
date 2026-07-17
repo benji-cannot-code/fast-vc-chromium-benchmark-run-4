3fd6522a5bf72501aa87cdbc4ed8c52b56d4968f
Tamino Bauknecht <taminob@chromium.org>
2026-07-17 03:36:55 -0700

    autofill: Add PhoneCombineHelper::GetRegionCode()
    This change introduces `GetRegionCode()` in
    `PhoneNumber::PhoneCombineHelper` which allows getting the region code
    from the individual components of a phone number.
    
    A follow-up CL will use this as a fallback country for the profile if
    no explicit country was submitted by the user.
    
    Bug: 509874545
    Change-Id: I3f0d41ccb19a0826d9c9e98f253fb93aa4ac61cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8052532
    Commit-Queue: Tamino Bauknecht <taminob@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663782}

--- components/autofill/core/browser/data_model/addresses/phone_number.h
+++ components/autofill/core/browser/data_model/addresses/phone_number.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
+#include <optional>
 #include <string>
 #include <string_view>
 
@@ -100,6 +101,10 @@ class PhoneNumber : public FormGroup {
     bool ParseNumber(const AutofillProfile& profile,
                      std::string_view app_locale,
                      std::u16string* value) const;
+
+    // Parses the combined phone number and returns the region to which it
+    // belongs.
+    std::optional<std::u16string> GetRegionCode() const;
 
     // Returns true if both `phone_` and `whole_number_` are empty.
     bool IsEmpty() const;
