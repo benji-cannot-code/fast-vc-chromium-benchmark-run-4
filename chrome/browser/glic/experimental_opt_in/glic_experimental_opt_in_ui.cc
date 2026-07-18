21a22ed5456c683fa4c5d70c50d2a3c9401913cd
Mickey Burks <mickeyburks@chromium.org>
2026-07-18 12:18:22 -0700

    [WebUI] Register ThemeSource explicitly, part 8.
    In this part adding ThemeSource registration in
     - Glic experimental opt in
    
    For details, refer to go/chrome-webui-explicit-theme-source-analysis
    
    Bug: 40757220
    Change-Id: Iebf49940143702966d1b9659fdd0eb049d0cb58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8104772
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664383}

--- chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_ui.cc
+++ chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_ui.cc
@@ -15,12 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/glic/resources/grit/glic_browser_resources.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/glic_experimental_opt_in_resources.h"
 #include "chrome/grit/glic_experimental_opt_in_resources_map.h"
+#include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -88,6 +90,8 @@ GlicExperimentalOptInUI::GlicExperimentalOptInUI(content::WebUI* web_ui)
   Profile* profile = Profile::FromWebUI(web_ui);
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
       profile, chrome::kChromeUIGlicExperimentalOptInHost);
+
+  content::URLDataSource::Add(profile, std::make_unique<ThemeSource>(profile));
 
   webui::SetupWebUIDataSource(
       source, kGlicExperimentalOptInResources,
