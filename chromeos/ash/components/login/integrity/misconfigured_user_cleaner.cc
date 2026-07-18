a7977e020beeb1332d2c6c0248007e50debac64f
Mitsuru Oshima <oshima@chromium.org>
2026-07-18 04:02:50 -0700

    Add warning log for misconfigured user data on restart
    When a restart is requested due to misconfigured user data, and
    DCHECK is enabled, print a warning message suggesting to pass
    --first-exec-after-boot to clean it up.
    
    This is helpful when using linux-chromeos build locally.
    
    TAG=agy
    CONV=4ab5be4e-5d3e-47d3-943c-521490355097
    
    Change-Id: I556cd5e5253aa0d694866a4c7105e3d5ffc4d150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8114602
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664347}

--- chromeos/ash/components/login/integrity/misconfigured_user_cleaner.cc
+++ chromeos/ash/components/login/integrity/misconfigured_user_cleaner.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/session/session_controller.h"
 #include "base/command_line.h"
+#include "base/dcheck_is_on.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
@@ -83,6 +84,11 @@ void MisconfiguredUserCleaner::DoCleanup(
     integrity_manager.RemoveUser(account_id);
     integrity_manager.ClearPrefs();
   } else {
+#if DCHECK_IS_ON()
+    LOG(ERROR) << "\n\n\n\n\n==== Misconfigured User Data Detected ===\n"
+               << " Pass --first-exec-after-boot to remove the misconfigured "
+                  "data\n\n\n";
+#endif
     chromeos::PowerManagerClient::Get()->RequestRestart(
         power_manager::RequestRestartReason::REQUEST_RESTART_OTHER,
         "Restarting for logged-in misconfigured user removal");
