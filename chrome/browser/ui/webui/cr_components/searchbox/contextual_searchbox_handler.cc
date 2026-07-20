08b0f4e54eb7594a16f66e4aa91feb448aade081
Juliet Lévesque Knighton <julietlevesque@google.com>
2026-07-20 15:38:44 -0700

    [loomnibox] Fix crashes in the submit query flow
    During the loomnibox file upload flow, the WebUI is synchronously
    destroyed because the fulfillment happens in Chrome. This can cause
    crashes when attempting to open a URL.
    
    This CL handles this synchronous destruction safely by posting a task to
    ComposeboxHandler::OpenUrl to make it async
    
    Tested=verified that file uploads don't crash from loomnibox and all file upload flows in other entry points in Chrome still WAI
    
    Fixed: b:536013226
    Change-Id: If0c7db4bcb37049cc675403b2c295c243254dbe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8120070
    Commit-Queue: Juliet Lévesque Knighton <julietlevesque@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664987}

--- chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.cc
+++ chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.cc
@@ -2064,7 +2064,14 @@ void ContextualSearchboxHandler::OpenUrl(
         chrome::kChromeUIOmniboxEverywhereHost) {
       if (auto* service =
               OmniboxEverywhereServiceFactory::GetForProfile(profile_)) {
-        service->OpenUrl(url, disposition);
+        base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+            FROM_HERE,
+            base::BindOnce(
+                [](OmniboxEverywhereService* service, const GURL& url,
+                   WindowOpenDisposition disposition) {
+                  service->OpenUrl(url, disposition);
+                },
+                base::Unretained(service), url, disposition));
         return;
       }
     }
