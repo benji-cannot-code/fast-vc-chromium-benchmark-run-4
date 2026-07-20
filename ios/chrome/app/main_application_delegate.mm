55ed1d71053b270d608105ce51e95b349238b96a
Hira Mahmood <hiramahmood@google.com>
2026-07-20 13:46:03 -0700

    [iOS] Update return in provisionalNotificationTypesEnabled
    A previous feature cleanup CL (crrev.com/c/7623076) inadvertently
    removed Send Tab to Self from the `provisionalNotificationTypesEnabled`
    check. This caused the method to return NO for users without Content
    Notifications enabled, passing a nil profile to the APNs registration
    callback.
    
    This CL fixes the omission by ensuring the method returns YES (since
    send tab is enabled by default) so the profile is correctly passed to
    the APNs registration callback.
    
    Bug: 489102216
    Change-Id: I5726e233486df007ee87e8c8fbf4cdb5e2bf61f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8117511
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664916}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -404,10 +404,7 @@ constexpr base::TimeDelta kMainIntentCheckDelay = base::Seconds(1);
     return NO;
   }
 
-  ProfileIOS* profile = browser->GetProfile();
-
-  return IsContentNotificationEnabled(profile) ||
-         IsContentNotificationRegistered(profile);
+  return YES;
 }
 
 @end
