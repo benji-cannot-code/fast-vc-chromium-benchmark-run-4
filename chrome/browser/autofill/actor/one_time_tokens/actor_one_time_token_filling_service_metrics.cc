a531ae1794356b12b2cbd3f7c79f52b8e2add301
sdrab <sdrab@google.com>
2026-07-20 07:23:19 -0700

    Add metrics to the ActorOneTimeTokenFillingServiceImpl
    Bug: b:532425124
    Change-Id: I09964fdba854993e457d7589e807f65f6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8092228
    Auto-Submit: Stephan Drab <sdrab@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664666}

---
+++ chrome/browser/autofill/actor/one_time_tokens/actor_one_time_token_filling_service_metrics.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autofill/actor/one_time_tokens/actor_one_time_token_filling_service_metrics.h"
+
+#include "base/metrics/histogram_functions.h"
+
+namespace autofill {
+
+void RecordActorOneTimeTokenFillingServiceRetrieveOtp(
+    ActorOneTimeTokenFillingServiceRetrieveOtp event) {
+  base::UmaHistogramEnumeration(
+      kActorOneTimeTokenFillingServiceRetrieveOtpHistogram, event);
+}
+
+void RecordActorOneTimeTokenFillingServiceFillOtp(
+    ActorOneTimeTokenFillingServiceFillOtp event) {
+  base::UmaHistogramEnumeration(
+      kActorOneTimeTokenFillingServiceFillOtpHistogram, event);
+}
+
+}  // namespace autofill
