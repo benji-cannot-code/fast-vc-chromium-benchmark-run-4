0cf753ab13a5c9ff795d1f601147660a45a67fc0
Jingyun Liu <jingyun@google.com>
2026-07-20 11:23:32 -0700

    [ASR] Add a flag to enable decoder prefill.
    - This param controls how far back into the past audio history the decoder is allowed to look. Values are: -1(disabled), 0, 5, 10, 20...
    - next cl: plumb the params into AdvancedSpeechProcessorOptions in internals
    
    Bug: 528015401
    Change-Id: Ic89d4fd909981dc7e0a4e36d940989cdfed6b648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8095792
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664811}

--- services/on_device_model/ml/session_accessor.cc
+++ services/on_device_model/ml/session_accessor.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/on_device_model/ml/chrome_ml.h"
 #include "services/on_device_model/ml/chrome_ml_types.h"
 #include "services/on_device_model/ml/constraint_factory.h"
+#include "services/on_device_model/public/cpp/features.h"
 
 namespace ml {
 
@@ -511,7 +512,13 @@ std::optional<odmm::AsrError> SessionAccessor::CreateAsrStreamInternal(
   ChromeMLASRStreamOptions options{
       .sample_rate_hz = asr_options->sample_rate_hz,
       .output_fn = &output_fn,
+      .decoder_prefill_backoff = -1,
   };
+  if (base::FeatureList::IsEnabled(
+          on_device_model::features::kOnDeviceModelAsrDecoderPrefill)) {
+    options.decoder_prefill_backoff =
+        on_device_model::features::kOnDeviceModelAsrDecoderPrefillBackoff.Get();
+  }
   asr_stream_ = chrome_ml_->ASRCreateStream(session_, &options);
   if (asr_stream_ == 0) {
     return odmm::AsrError::kInitializationFailed;
