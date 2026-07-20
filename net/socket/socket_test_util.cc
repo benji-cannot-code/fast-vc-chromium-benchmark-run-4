7315c907b625a5e7b2f01c9e5d738c872960194f
Carlos IL <carlosil@chromium.org>
2026-07-20 10:50:49 -0700

    Remove DNS dependency for TAIs
    Recent changes to the Trust Anchor IDs draft make it easier to manage large numbers of TAIs without relying on DNS. This CL adds support for sending servers known TAIs even if the server did not explicitly request them via the DNS record.
    
    Bug: 508686193
    
    Change-Id: Ib495f7d81f5b4ef7fde48e7b3f320a0b9d26c710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8019511
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664794}

--- net/socket/socket_test_util.cc
+++ net/socket/socket_test_util.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_timing_info.h"
 #include "net/base/net_errors.h"
 #include "net/base/proxy_server.h"
+#include "net/cert/x509_util.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_response_headers.h"
@@ -62,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/test_data_directory.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/strings/ascii.h"
 
@@ -1045,8 +1047,13 @@ std::unique_ptr<SSLClientSocket> MockClientSocketFactory::CreateSSLClientSocket(
               ssl_config.ech_config_list);
   }
   if (next_ssl_data->expected_trust_anchor_ids) {
-    EXPECT_EQ(*next_ssl_data->expected_trust_anchor_ids,
-              ssl_config.trust_anchor_ids);
+    EXPECT_TRUE(ssl_config.trust_anchor_ids.has_value());
+    if (ssl_config.trust_anchor_ids.has_value()) {
+      EXPECT_THAT(
+          x509_util::ParseTlsTrustAnchorIDs(*ssl_config.trust_anchor_ids),
+          testing::UnorderedElementsAreArray(
+              *next_ssl_data->expected_trust_anchor_ids));
+    }
   }
   if (next_ssl_data->expect_no_trust_anchor_ids) {
     EXPECT_EQ(std::nullopt, ssl_config.trust_anchor_ids);
