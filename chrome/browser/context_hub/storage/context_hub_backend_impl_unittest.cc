b7f3d77155f9aa5cec51538d8f0c0fec7fb65434
Amando Jimenez <amandoj@google.com>
2026-07-20 13:27:11 -0700

    Integrate ContextHubDatabase into ContextHubService.
    Created ContextHubBackendFactory and made the DB conditional based on
    storage flag and memory banks flag. If both flags are enabled we will
    store memories in db.
    
    Bug: b:530253460
    Change-Id: Icac974357835eb2e113707f2c02a75b88bfec87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8068296
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Amando Jimenez <amandoj@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664901}

--- chrome/browser/context_hub/storage/context_hub_backend_impl_unittest.cc
+++ chrome/browser/context_hub/storage/context_hub_backend_impl_unittest.cc
@@ -30,12 +30,6 @@ class ContextHubBackendImplTest : public testing::Test {
     backend_ = std::make_unique<ContextHubBackendImpl>(db_path_);
   }
 
-  void TearDown() override {
-    if (backend_) {
-      backend_->Shutdown();
-    }
-  }
-
   base::test::TaskEnvironment task_environment_;
   base::ScopedTempDir temp_dir_;
   base::FilePath db_path_;
@@ -126,7 +120,6 @@ TEST_F(ContextHubBackendImplTest, OperationsQueuedBeforeInit) {
   backend->GetAllMemoryBankEntries(empty_future.GetCallback());
   EXPECT_TRUE(empty_future.Get().empty());
 
-  backend->Shutdown();
 }
 
 }  // namespace context_hub
