fe17590d2a0f244d41bef0fa221d47a1f61586c0
Min Qin <qinmin@chromium.org>
2026-07-20 16:10:36 -0700

    [ExperimentalTriggering] Add support for continuing an Glic actuation session
    This CL implements ContinueActuationRequest handling in Glic:
    - Define ContinueActuationRequest proto message containing the continuation prompt.
    - GlicExperimentalTriggeringMessageHandler is updated to parse ContinueActuationRequest and resolve the target conversation to the task metadata conversation ID (falling back to new conversation if empty).
    - GlicInstanceCoordinatorImpl is updated to erase the old invoke handler if the instance is already actuating so that the continue request can be processed.
    
    Bug: 527745430
    Change-Id: I92d85c397931a0e0554e6d3d7bebf09baeef8b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008807
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Charles Harrison <csharrison@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665006}

--- chrome/browser/sharing/glic_experimental_triggering/actor_log.cc
+++ chrome/browser/sharing/glic_experimental_triggering/actor_log.cc
@@ -77,6 +77,8 @@ std::string_view ExperimentalTriggeringRequestPayloadCaseToString(
       return "DeviceOptInRequest";
     case Request::kGetScreenshotRequest:
       return "GetScreenshotRequest";
+    case Request::kContinueActuationRequest:
+      return "ContinueActuationRequest";
     case Request::PAYLOAD_NOT_SET:
       return "PAYLOAD_NOT_SET";
   }
