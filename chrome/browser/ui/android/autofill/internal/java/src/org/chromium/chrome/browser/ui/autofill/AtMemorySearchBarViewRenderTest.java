82c19d4317f02e98265790dbcaa30b0579124de7
Karol Sygiet <sygiet@google.com>
2026-07-20 11:11:28 -0700

    [AtMemory] Update strings
    This CL updates the strings to match the newest design. Translatable
    strings got their screenshots updated.
    
    Source: go/atmemory-mvp-strings
    Promo bubble: screen/BvYup2FyZuYun8U
    AtMemory popup hint: screen/9wj7FGQxNKdZEt6
    
    Bug: 517786635
    Change-Id: Ida40bf983b6d49fccebdd8a8bf61e0fa0da34c3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124754
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664803}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemorySearchBarViewRenderTest.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemorySearchBarViewRenderTest.java
@@ -56,6 +56,8 @@ public class AtMemorySearchBarViewRenderTest {
     @Rule
     public RenderTestRule mRenderTestRule =
             RenderTestRule.Builder.withPublicCorpus()
+                    .setRevision(2)
+                    .setDescription("Strings updated")
                     .setBugComponent(RenderTestRule.Component.UI_BROWSER_AUTOFILL)
                     .build();
 
