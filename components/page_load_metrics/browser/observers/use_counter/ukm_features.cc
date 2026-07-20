c6af5209d757a1e3c66fb6326716fd51dfb4706f
Adriana Ixba <aixba@chromium.org>
2026-07-20 10:58:23 -0700

    [E2E] Opt in to UKM for ViewportFitCover
    This CL opts ViewportFitCover to UKM to capture urls. Privacy review
    approved.
    
    UKM collection review doc:
    https://docs.google.com/document/d/1wHdWY2vY91YZNCMtQS__4s6tVvLNHxkM7KCyGCoZQ10/edit?resourcekey=0-dqJy843XwN77scE8gRYnxA&tab=t.0#heading=h.k5jx6iluw4yt
    
    TAG=agy
    CONV=80054289-8aa5-4df6-9492-7e9c3c46999e
    
    Bug: 535643496,528430690
    Change-Id: I1de0f446cdc848be118e7ea01656aa304b862248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8109278
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Adriana Ixba <aixba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664800}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -496,6 +496,7 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kWebSpeechTtsSynthesize,
           WebFeature::kWebSpeechTtsSuccess,
           WebFeature::kWebSpeechTtsError,
+          WebFeature::kViewportFitCover,
           // NOTE: before adding new use counters here, verify in UMA that
           // their emissions are very rare, e.g. <1% of page loads.
       }));
