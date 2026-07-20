da5f4ef3f961026ece84084b6bac6de8060e4342
Tzarial <zork@google.com>
2026-07-20 08:33:47 -0700

    [agy][gl] Add ScopedGLFramebuffer
    Add ScopedGLFramebuffer, a RAII wrapper for GL framebuffer IDs.
    It automatically deletes the framebuffer when it goes out of scope.
    
    Fixed: 529817092
    Test: gl_unittests --gtest_filter=*ScopedGLFramebufferTest*
    Change-Id: I6f749705f3c1bd51376fdbb7714b072ab6b65203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085042
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Tzarial <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664703}

--- gpu/command_buffer/service/shared_image/gl_texture_holder.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_holder.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_version_info.h"
 #include "ui/gl/progress_reporter.h"
 #include "ui/gl/scoped_binders.h"
+#include "ui/gl/scoped_gl_framebuffer.h"
 #include "ui/gl/scoped_make_current.h"
 #include "ui/gl/scoped_restore_texture.h"
 
@@ -58,28 +59,7 @@ constexpr int ComputeBestAlignment(size_t bytes_per_pixel, size_t stride) {
   return bytes_per_pixel;
 }
 
-class ScopedTemporaryFramebuffer {
- public:
-  explicit ScopedTemporaryFramebuffer(gl::GLApi* api) : api_(api) {
-    api_->glGenFramebuffersEXTFn(1, &id_);
-  }
 
-  ScopedTemporaryFramebuffer(const ScopedTemporaryFramebuffer&) = delete;
-  ScopedTemporaryFramebuffer& operator=(const ScopedTemporaryFramebuffer&) =
-      delete;
-
-  ~ScopedTemporaryFramebuffer() {
-    if (id_ != 0) {
-      api_->glDeleteFramebuffersEXTFn(1, &id_);
-    }
-  }
-
-  GLuint id() const { return id_; }
-
- private:
-  const raw_ptr<gl::GLApi> api_;
-  GLuint id_ = 0;
-};
 
 }  // anonymous namespace
 
@@ -406,14 +386,14 @@ bool GLTextureHolder::ReadbackToMemory(const SkPixmap& pixmap) {
   }
 
   gl::GLApi* api = gl::g_current_gl_context;
-  // ScopedTemporaryFramebuffer must be declared before ScopedFramebufferBinder
+  // ScopedGLFramebuffer must be declared before ScopedFramebufferBinder
   // so that when this scope exits, ScopedFramebufferBinder is destroyed first
   // (restoring the previous framebuffer binding) before the temporary FBO is
   // deleted. Some drivers retain an internal reference to the previously bound
   // FBO across bind transitions; deleting it while bound can trigger a
   // driver UAF (see https://crbug.com/525317502).
-  ScopedTemporaryFramebuffer temp_fbo(api);
-  gl::ScopedFramebufferBinder scoped_framebuffer_binder(temp_fbo.id());
+  auto temp_fbo = gl::CreateScopedGLFramebuffer(api);
+  gl::ScopedFramebufferBinder scoped_framebuffer_binder(temp_fbo.get());
 
   // This uses GL_FRAMEBUFFER instead of GL_READ_FRAMEBUFFER as the target for
   // GLES2 compatibility.
---
+++ ui/gl/scoped_gl_framebuffer.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_GL_SCOPED_GL_FRAMEBUFFER_H_
+#define UI_GL_SCOPED_GL_FRAMEBUFFER_H_
+
+#include "base/scoped_generic.h"
+#include "ui/gl/gl_bindings.h"
+#include "ui/gl/gl_export.h"
+
+namespace gl {
+
+struct GL_EXPORT DeleteGLFramebufferTraits {
+  static GLuint InvalidValue();
+  static void Free(GLuint framebuffer);
+};
+
+// A RAII wrapper for a framebuffer ID that automatically deletes the
+// framebuffer when the object goes out of scope. The deletion happens on the
+// current GL context.
+using ScopedGLFramebuffer =
+    base::ScopedGeneric<GLuint, DeleteGLFramebufferTraits>;
+
+// Helper to create a ScopedGLFramebuffer.
+GL_EXPORT ScopedGLFramebuffer CreateScopedGLFramebuffer(GLApi* api);
+
+}  // namespace gl
+
+#endif  // UI_GL_SCOPED_GL_FRAMEBUFFER_H_
