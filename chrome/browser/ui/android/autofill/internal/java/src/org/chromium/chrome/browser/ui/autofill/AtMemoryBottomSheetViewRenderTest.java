9858bc8a7956cf598eee712b8839eba1bcee9536
Atalyk Akash <atalyk@google.com>
2026-07-20 07:49:14 -0700

    [AtMemory][Android] Fix @memory render tests
    The change fix the flakiness in render tests which is caused by
    unfinished bottom sheet state transitions.
    
    Bug: 535897013
    Change-Id: Idd344a2c2e21a631b6c695940399e5830ba51875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8117131
    Commit-Queue: Atalyk Akash <atalyk@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664682}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetViewRenderTest.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetViewRenderTest.java
@@ -54,6 +54,7 @@ import org.chromium.components.autofill.AutofillSuggestion;
 import org.chromium.components.autofill.SuggestionType;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetControllerFactory;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetTestSupport;
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager;
 import org.chromium.components.browser_ui.widget.scrim.ScrimManager.ScrimClient;
 import org.chromium.ui.KeyboardVisibilityDelegate;
@@ -194,6 +195,7 @@ public class AtMemoryBottomSheetViewRenderTest {
                     mBottomSheetController.requestShowContent(content, false);
                 });
 
+        BottomSheetTestSupport.waitForOpen(mBottomSheetController);
         ViewUtils.waitForStableView(mView.getContentView());
         CriteriaHelper.pollUiThread(
                 () -> {
@@ -234,6 +236,7 @@ public class AtMemoryBottomSheetViewRenderTest {
                     mBottomSheetController.requestShowContent(content, false);
                 });
 
+        BottomSheetTestSupport.waitForOpen(mBottomSheetController);
         ViewUtils.waitForStableView(mView.getContentView());
         CriteriaHelper.pollUiThread(
                 () -> {
@@ -277,6 +280,7 @@ public class AtMemoryBottomSheetViewRenderTest {
                     mBottomSheetController.requestShowContent(content, false);
                 });
 
+        BottomSheetTestSupport.waitForOpen(mBottomSheetController);
         ViewUtils.waitForStableView(mView.getContentView());
         CriteriaHelper.pollUiThread(
                 () -> {
@@ -313,6 +317,7 @@ public class AtMemoryBottomSheetViewRenderTest {
                     mBottomSheetController.requestShowContent(content, false);
                 });
 
+        BottomSheetTestSupport.waitForOpen(mBottomSheetController);
         ViewUtils.waitForStableView(mView.getContentView());
         CriteriaHelper.pollUiThread(
                 () -> {
@@ -327,19 +332,8 @@ public class AtMemoryBottomSheetViewRenderTest {
                 "at_memory_bottom_sheet_view_zero_state");
     }
 
-    private static PropertyModel createSuggestionModel(
-            String title, String details, int iconResId, boolean isFlyoutVisible) {
-        return new PropertyModel.Builder(SuggestionItemProperties.ALL_KEYS)
-                .with(TITLE, title)
-                .with(DETAILS, details)
-                .with(ICON, iconResId)
-                .with(IS_FLYOUT_VISIBLE, isFlyoutVisible)
-                .build();
-    }
-
     @Test
     @Feature({"RenderTest"})
-    @DisabledTest(message = "Enabled after fixing crbug.com/535894236")
     public void testAtMemoryBottomSheetFlyoutScreen() throws Exception {
         ContextThemeWrapper themeWrapper =
                 new ContextThemeWrapper(mActivity, R.style.Theme_BrowserUI_DayNight);
@@ -376,9 +370,20 @@ public class AtMemoryBottomSheetViewRenderTest {
                     mBottomSheetController.requestShowContent(content, false);
                 });
 
+        BottomSheetTestSupport.waitForOpen(mBottomSheetController);
         ViewUtils.waitForStableView(mView.getContentView());
         mRenderTestRule.render(
                 mActivity.findViewById(android.R.id.content), "at_memory_flyout_screen");
+    }
+
+    private static PropertyModel createSuggestionModel(
+            String title, String details, int iconResId, boolean isFlyoutVisible) {
+        return new PropertyModel.Builder(SuggestionItemProperties.ALL_KEYS)
+                .with(TITLE, title)
+                .with(DETAILS, details)
+                .with(ICON, iconResId)
+                .with(IS_FLYOUT_VISIBLE, isFlyoutVisible)
+                .build();
     }
 
     private AutofillSuggestion createManageSuggestion() {
