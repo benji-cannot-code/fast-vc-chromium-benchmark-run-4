af4872f3ab739fb1a367fbd1c6b15a139a2e28ef
Nikolay Vitkov <nvitkov@chromium.org>
2026-07-20 06:17:03 -0700

    Move test to Inspector Protocol
    The test did not run any DevTools code.
    
    Fixed: 528347555
    Change-Id: If50c870b89e899d291eee9952f58cdeb4c3b232b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124756
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Auto-Submit: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664637}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/dom/resolve-node-blocked.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Tests that JS object to node resolution still works even if script evals are prohibited by Content-Security-Policy. The test passes if it doesn't crash. Bug 78705.`);
+
+  await page.navigate('../resources/content-security-policy-issue-eval.php');
+
+  await dp.DOM.enable();
+  await dp.DOM.getDocument();
+  const evaluateResponse = await dp.Runtime.evaluate({expression: 'document'});
+  testRunner.log('didReceiveDocumentObject');
+
+  const requestNodeResponse = await dp.DOM.requestNode(
+      {objectId: evaluateResponse.result.result.objectId});
+  testRunner.log('didRequestNode error = ' +
+                 (requestNodeResponse.result.nodeId ? 'null' : 'error'));
+
+  testRunner.completeTest();
+})
