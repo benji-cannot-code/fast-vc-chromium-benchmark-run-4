3dc6ec979b9e3ce045ade7d184b88e689d112731
Isabelle Goralsky <igoralsky@google.com>
2026-07-19 19:26:56 -0700

    Add support to pass in user command for tab grouping to MES.
    This change updates the Context Hub service to accept a user-provided
    command when grouping tabs. This webUI frontend is modified to allow
    input to the textbox based on enablement conditions. Also update MES
    output handling to use TabGroupMinimal instead of TabGroup.
    
    Bug: b:534492009
    Change-Id: Ie293854343b715b500bb872a3d9f9a311a7a6bb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118527
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664509}

--- chrome/browser/resources/context_hub/tab_groups/tab_groups.ts
+++ chrome/browser/resources/context_hub/tab_groups/tab_groups.ts
@@ -76,11 +76,7 @@ export class TabGroupsElement extends CrLitElement {
   }
 
   protected async onGroupTabsClick_() {
-    if (!this.autoTabGroupsEnabled_) {
-      return;
-    }
-    if (this.isGrouped_) {
-      this.fetchTabs_();
+    if (!this.autoTabGroupsEnabled_ || this.isGrouping_) {
       return;
     }
 
@@ -106,6 +102,14 @@ export class TabGroupsElement extends CrLitElement {
     } finally {
       this.isGrouping_ = false;
     }
+  }
+
+  protected onUngroupTabsClick_() {
+    if (!this.autoTabGroupsEnabled_ || this.isGrouping_) {
+      return;
+    }
+    this.inputValue_ = '';
+    this.fetchTabs_();
   }
 
   protected onTabClick_(e: Event) {
@@ -134,6 +138,12 @@ export class TabGroupsElement extends CrLitElement {
 
   protected onInputValueChanged_(e: CustomEvent<{value: string}>) {
     this.inputValue_ = e.detail.value;
+  }
+
+  protected onInputKeydown_(e: KeyboardEvent) {
+    if (e.key === 'Enter' && this.inputValue_.trim().length > 0) {
+      this.onGroupTabsClick_();
+    }
   }
 }
 
