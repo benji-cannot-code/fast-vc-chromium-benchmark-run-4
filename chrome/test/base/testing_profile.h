caf66b0982d0a5a80e1f1a002ca1ddd0ffd930e0
Nick Diego Yamane <nickdiego@igalia.com>
2026-07-20 04:24:20 -0700

    [Bedrock] Extract //chrome/test/base:testing_profile thin target
    Add a header-only testing_profile source_set to chrome/test/base/BUILD.gn
    exposing testing_profile.h, testing_profile_key.h, and
    testing_profile_manager.h. The .cc implementations remain in
    //chrome/test:test_support until fake_profile_manager and
    testing_browser_process are also extracted from the monolith.
    
    R=chromium-bedrock-reviews@google.com
    
    Bug: 353332589
    Change-Id: I8b67300af89f049dcb4cd2b881149acc34a57a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8089948
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664606}

--- chrome/test/base/testing_profile.h
+++ chrome/test/base/testing_profile.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_WIN)
-#include "chrome/browser/profiles/profile_load_tracker_win.h"
+class ProfileLoadTracker;
 #endif
 
 class ExtensionSpecialStoragePolicy;
