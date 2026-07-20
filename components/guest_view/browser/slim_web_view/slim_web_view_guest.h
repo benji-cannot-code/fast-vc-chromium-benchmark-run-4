150dea890026f141987703e141213a14e5ad9e23
Bryant Chandler <bryantchandler@google.com>
2026-07-20 13:57:10 -0700

    [guest_view] Add zoom support to SlimWebView
    This CL implements SetZoom/GetZoom APIs on the SlimWebView Mojom and
    PageHandler, and exposes setZoom/getZoom methods in the TypeScript WebUI
    wrapper (slim_webview.ts) to control the guest WebContents zoom level.
    
    Bug: b:531825599, 500052160
    Change-Id: I246215360aebb9c081100c29757eadd06a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118688
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664923}

--- components/guest_view/browser/slim_web_view/slim_web_view_guest.h
+++ components/guest_view/browser/slim_web_view/slim_web_view_guest.h
@@ -53,6 +53,8 @@ class SlimWebViewGuest : public GuestView<SlimWebViewGuest> {
 
   void Navigate(const GURL& url);
   void SetUserAgentOverride(const std::string& user_agent_override);
+  void SetZoom(double zoom_factor);
+  double GetZoom() const;
 
   // Returns true if an origin allowlist was provided at creation time.
   bool HasAllowedOrigins() const;
@@ -123,6 +125,7 @@ class SlimWebViewGuest : public GuestView<SlimWebViewGuest> {
                        const base::DictValue& create_params,
                        GuestPageCreatedCallback callback) final;
   void GuestViewDidStopLoading() final;
+  void GuestZoomChanged(double old_zoom_level, double new_zoom_level) final;
 
   void LoadAbort(bool is_top_level, const GURL& url, net::Error error_code);
 
