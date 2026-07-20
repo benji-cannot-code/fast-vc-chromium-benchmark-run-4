08f819f80d715e1293250dbac8239181c1279c25
dpapad <dpapad@chromium.org>
2026-07-20 14:16:31 -0700

    Settings Lit: Migrate a11y_page/, part3.
    In this part migrating ax_annotations_section.ts and captions_page.ts to
    Lit.
    
    Bug: 393471368
    Change-Id: I5e322180c816fb2b8a12c7d503b840bc6f161ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8098348
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664936}

--- chrome/test/data/webui/settings/ax_annotations_section_test.ts
+++ chrome/test/data/webui/settings/ax_annotations_section_test.ts
@@ -6,14 +6,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://settings/lazy_load.js';
 
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
-import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import type {SettingsAxAnnotationsSectionElement, SettingsToggleButtonElement} from 'chrome://settings/lazy_load.js';
-import {ScreenAiInstallStatus} from 'chrome://settings/lazy_load.js';
+import type {AxAnnotationsBrowserProxy, SettingsAxAnnotationsSectionElement, SettingsToggleButtonElement} from 'chrome://settings/lazy_load.js';
+import {AxAnnotationsBrowserProxyImpl, ScreenAiInstallStatus} from 'chrome://settings/lazy_load.js';
 import {loadTimeData, PrefsBrowserProxy, PrefService} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestPrefsBrowserProxy} from './test_prefs_browser_proxy.js';
+
+class TestAxAnnotationsBrowserProxy extends TestBrowserProxy implements
+    AxAnnotationsBrowserProxy {
+  private screenAiInstallStatus_: ScreenAiInstallStatus =
+      ScreenAiInstallStatus.NOT_DOWNLOADED;
+
+  constructor() {
+    super([
+      'getScreenAiInstallState',
+    ]);
+  }
+
+  setScreenAiInstallStatus(status: ScreenAiInstallStatus) {
+    this.screenAiInstallStatus_ = status;
+  }
+
+  getScreenAiInstallState() {
+    this.methodCalled('getScreenAiInstallState');
+    return Promise.resolve(this.screenAiInstallStatus_);
+  }
+}
 
 function getInitialPrefs(): chrome.settingsPrivate.PrefObject[] {
   return [
@@ -28,6 +49,7 @@ function getInitialPrefs(): chrome.settingsPrivate.PrefObject[] {
 suite('SettingsAxAnnotationsSectionTest', () => {
   let testElement: SettingsAxAnnotationsSectionElement;
   let prefService: PrefService;
+  let browserProxy: TestAxAnnotationsBrowserProxy;
 
   suiteSetup(function() {
     loadTimeData.overrideValues({
@@ -38,6 +60,10 @@ suite('SettingsAxAnnotationsSectionTest', () => {
   setup(async function() {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
 
+    browserProxy = new TestAxAnnotationsBrowserProxy();
+    browserProxy.setScreenAiInstallStatus(ScreenAiInstallStatus.NOT_DOWNLOADED);
+    AxAnnotationsBrowserProxyImpl.setInstanceForTesting(browserProxy);
+
     const prefsBrowserProxy = new TestPrefsBrowserProxy(getInitialPrefs());
     PrefsBrowserProxy.setInstance(prefsBrowserProxy);
     PrefService.resetInstanceForTesting();
@@ -46,7 +72,8 @@ suite('SettingsAxAnnotationsSectionTest', () => {
 
     testElement = document.createElement('settings-ax-annotations-section');
     document.body.appendChild(testElement);
-    flush();
+    await browserProxy.whenCalled('getScreenAiInstallState');
+    return microtasksFinished();
   });
 
   test('main node annotations toggle and pref', async () => {
@@ -56,10 +83,9 @@ suite('SettingsAxAnnotationsSectionTest', () => {
     // state, but is managed by a11y_page.ts. Thus, no need to simulate enabling
     // screen reader in this test.
     const toggle =
-        testElement.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+        testElement.shadowRoot.querySelector<SettingsToggleButtonElement>(
             '#mainNodeAnnotationsToggle');
     assertTrue(!!toggle);
-    await flushTasks();
 
     // The main node annotations pref is off by default, so the button should be
     // toggled off.
@@ -71,7 +97,7 @@ suite('SettingsAxAnnotationsSectionTest', () => {
     assertFalse(toggle.checked);
 
     toggle.click();
-    await flushTasks();
+    await microtasksFinished();
     assertTrue(
         prefService
             .getPref<boolean>('settings.a11y.enable_main_node_annotations')
@@ -87,35 +113,39 @@ suite('SettingsAxAnnotationsSectionTest', () => {
     // state, but is managed by a11y_page.ts. Thus, no need to simulate enabling
     // screen reader in this test.
     const toggle =
-        testElement.shadowRoot!.querySelector<SettingsToggleButtonElement>(
+        testElement.shadowRoot.querySelector<SettingsToggleButtonElement>(
             '#mainNodeAnnotationsToggle');
     assertTrue(!!toggle);
-    await flushTasks();
 
     webUIListenerCallback(
         'screen-ai-state-changed', ScreenAiInstallStatus.NOT_DOWNLOADED);
+    await microtasksFinished();
     assertEquals(
         testElement.i18n('mainNodeAnnotationsSubtitle'), toggle.subLabel);
 
     webUIListenerCallback(
         'screen-ai-state-changed', ScreenAiInstallStatus.DOWNLOAD_FAILED);
+    await microtasksFinished();
     assertEquals(
         testElement.i18n('mainNodeAnnotationsDownloadErrorLabel'),
         toggle.subLabel);
 
     webUIListenerCallback(
         'screen-ai-state-changed', ScreenAiInstallStatus.DOWNLOADING);
+    await microtasksFinished();
     assertEquals(
         testElement.i18n('mainNodeAnnotationsDownloadingLabel'),
         toggle.subLabel);
 
     webUIListenerCallback('screen-ai-downloading-progress-changed', 50);
+    await microtasksFinished();
     assertEquals(
         testElement.i18n('mainNodeAnnotationsDownloadProgressLabel', 50),
         toggle.subLabel);
 
     webUIListenerCallback(
         'screen-ai-state-changed', ScreenAiInstallStatus.DOWNLOADED);
+    await microtasksFinished();
     assertEquals(
         testElement.i18n('mainNodeAnnotationsSubtitle'), toggle.subLabel);
   });
