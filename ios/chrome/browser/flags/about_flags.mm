fe739f6695583825c37827e684d0267b1f1dd563
Radu Nitescu <radunitescu@google.com>
2026-07-20 01:01:24 -0700

    [iOS] Add contextual tasks flag in iOS
    Enabling this flag is required to satisfy the updated cobrowse
    eligibility checks
    
    Change-Id: I82b43d7b678fffc9b48baae7dd3672f69447973a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8108257
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664547}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/commerce/core/commerce_feature_list.h"
 #import "components/commerce/core/flag_descriptions.h"
 #import "components/content_settings/core/common/features.h"
+#import "components/contextual_tasks/public/features.h"
 #import "components/data_sharing/public/features.h"
 #import "components/data_sharing/public/switches.h"
 #import "components/desktop_to_mobile_promos/features.h"
@@ -2215,6 +2216,9 @@ constexpr auto kFeatureEntries = std::to_array<flags_ui::FeatureEntry>({
      FEATURE_WITH_PARAMS_VALUE_TYPE(kComposeboxDevTools,
                                     kComposeboxDevToolsVariations,
                                     "ComposeboxDevTools")},
+    {"contextual-tasks", flag_descriptions::kContextualTasksName,
+     flag_descriptions::kContextualTasksDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(contextual_tasks::kContextualTasks)},
     {"autofill-manual-testing-data",
      flag_descriptions::kAutofillManualTestingDataName,
      flag_descriptions::kAutofillManualTestingDataDescription, flags_ui::kOsIos,
