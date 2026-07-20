3462e05b41431f75ac45d39c683f227fb5b3432b
Adem Derinel <derinel@google.com>
2026-07-20 01:07:50 -0700

    [RPS] Load and show remote actor header illustration
    Load the remote actor sharing illustration header in PasswordCombinedSelectorView.
    The illustration is registered conditionally for branded builds only,
    and loaded using BUILDFLAG(GOOGLE_CHROME_BRANDING) with a nocheck comment.
    Also update browser tests to verify the illustration visibility, and add
    interactive InvokeUi tests to support screenshot verification.
    
    Screenshots:
    Single: https://screenshot.googleplex.com/3JuCzxQL9mNGiDV
    Multiple: https://screenshot.googleplex.com/AJ4tsDitNHmX45q
    
    TAG=agy
    CONV=2633a814-62dd-486d-9e13-0ea9528ca59f
    
    Bug: 532482932
    Change-Id: Ib68eaea3e4158102392e5b4429e592aaf855e9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8074898
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664552}

--- chrome/browser/ui/views/passwords/password_combined_selector_view.cc
+++ chrome/browser/ui/views/passwords/password_combined_selector_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/ui/passwords/account_avatar_fetcher.h"
 #include "chrome/browser/ui/passwords/password_combined_selector_controller.h"
 #include "chrome/browser/ui/passwords/ui_utils.h"
@@ -34,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/radio_button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -447,8 +449,7 @@ PasswordCombinedSelectorView::PasswordCombinedSelectorView(
     : controller_(controller), web_contents_(web_contents) {
   SetButtons(static_cast<int>(ui::mojom::DialogButton::kOk) |
              static_cast<int>(ui::mojom::DialogButton::kCancel));
-  SetButtonLabel(ui::mojom::DialogButton::kOk,
-                 controller_->GetOkButtonLabel());
+  SetButtonLabel(ui::mojom::DialogButton::kOk, controller_->GetOkButtonLabel());
   set_fixed_width(views::LayoutProvider::Get()->GetDistanceMetric(
       views::DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH));
   SetModalType(ui::mojom::ModalType::kChild);
@@ -569,7 +570,20 @@ void PasswordCombinedSelectorView::OnRadioButtonChecked(int index) {
 
 void PasswordCombinedSelectorView::OnWidgetInitialized() {
   views::DialogDelegate::OnWidgetInitialized();
-  if (controller_->ShouldShowTopIllustration()) {
-    // TODO(crbug.com/532482932): Load remote actor illustration once DEPS roll lands.
+  if (!controller_->ShouldShowTopIllustration()) {
+    return;
   }
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)  // nocheck
+  ui::ResourceBundle& bundle = ui::ResourceBundle::GetSharedInstance();
+  auto image_view = std::make_unique<views::ImageView>();
+  image_view->SetImage(ui::ImageModel::FromImageSkia(
+      *bundle.GetImageSkiaNamed(IDR_REMOTE_ACTOR_SHARING_ILLUSTRATION)));
+  image_view->SetVerticalAlignment(views::ImageView::Alignment::kLeading);
+  if (auto* frame_view = GetBubbleFrameView()) {
+    frame_view->SetHeaderView(std::move(image_view));
+  }
+#else
+  // TODO(crbug.com/532482932): Add unbranded header.
+#endif
 }
