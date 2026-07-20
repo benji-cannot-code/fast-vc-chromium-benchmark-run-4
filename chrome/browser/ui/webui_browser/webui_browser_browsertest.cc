a3d801cfe9176675a3e55e1c4163c744079045f6
Keren Zhu <kerenzhu@chromium.org>
2026-07-20 11:57:46 -0700

    webui-browser: fix dangling Browser* when shutdown by closing all tabs
    When shutting down a WebUI Browser by closing all tabs on macOS,
    WebUIBrowserNativeWidgetMac (a subclass of NativeWidget) is destroyed
    after Browser, causing the member raw_ptr<Browser> in
    WebUIBrowserNativeWidgetMac to become dangling.
    
    The destruction order when closing all tabs:
    
    UnloadController::ProcessPendingTabs
      -> UnloadController::OnWindowClosing
      -> Browser::OnWindowCloseComplete
      -> (posted task) Browser::SynchronouslyDestroyBrowser
      -> ~Browser -> ~Widget -> NativeWidgetMac::Close
      -> NativeWidgetNSWindowBridge::CloseWindow
      -> (posted task) -[NSWindow close]
      -> NativeWidgetMac::WindowDestroyed
      -> ~NativeWidget
    
    Note that Browser is destroyed before NativeWidget.
    
    Include-Ci-Only-Tests: true
    Fixed: 535621599
    Change-Id: I19165065742b8ec00ea225235bcc2bc858af5bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8113109
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664833}

--- chrome/browser/ui/webui_browser/webui_browser_browsertest.cc
+++ chrome/browser/ui/webui_browser/webui_browser_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/desktop_browser_window_capabilities.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -134,6 +135,21 @@ IN_PROC_BROWSER_TEST_F(WebUIBrowserTest, StartupAndShutdown) {
       browser()->tab_strip_model()->GetActiveWebContents();
   ASSERT_TRUE(web_contents);
   EXPECT_TRUE(content::WaitForLoadStop(web_contents));
+}
+
+// Ensures that WebUIBrowser does not crash when closing tabs (rather than
+// closing window).
+IN_PROC_BROWSER_TEST_F(WebUIBrowserTest, ShutdownByClosingTabs) {
+  auto* window = WebUIBrowserWindow::FromBrowser(browser());
+  ASSERT_TRUE(window);
+
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  ASSERT_TRUE(web_contents);
+  EXPECT_TRUE(content::WaitForLoadStop(web_contents));
+  browser()->tab_strip_model()->CloseAllTabs();
+  ASSERT_TRUE(base::test::RunUntil(
+      []() { return GlobalBrowserCollection::GetInstance()->IsEmpty(); }));
 }
 
 // Verifies that WebUIBrowserWindow allows keyboard lock for tab WebContents.
