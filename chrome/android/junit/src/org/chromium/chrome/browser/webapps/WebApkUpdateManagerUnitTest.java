b8efb426dd55beef3747edd0bfee6aaaa72c2d77
utzcoz <utzcoz@gmail.com>
2026-07-20 05:57:47 -0700

    Android: Re-enable WebApkUpdateManagerUnitTest empty-scope test
    testManifestEmptyScopeShouldNotUpgrade was disabled with a TODO to
    re-enable it. It passes on the default SDK levels with the current
    Robolectric (4.16.1), so remove the @Ignore.
    
    Bug: 40827678
    Test: out/Default/bin/run_chrome_junit_tests --test-filter "*WebApkUpdateManagerUnitTest*"
    Change-Id: I812b54ae1dd0b47c5977925d308fb2dd68aa1bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116380
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664633}

--- chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebApkUpdateManagerUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebApkUpdateManagerUnitTest.java
@@ -24,7 +24,6 @@ import android.text.TextUtils;
 
 import org.json.JSONArray;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -1149,9 +1148,8 @@ public class WebApkUpdateManagerUnitTest {
 
     /**
      * Test that an upgrade is not requested when the Web Manifest did not change and the Web
-     * Manifest scope is empty. TODO(crbug.com/40827678): Re-enable test.
+     * Manifest scope is empty.
      */
-    @Ignore
     @Test
     public void testManifestEmptyScopeShouldNotUpgrade() {
         ManifestData oldData = defaultManifestData();
