c127f088e92cf099a55ec00f5380e0b8fc03b240
Haochen Feng <haochenf@google.com>
2026-07-20 13:39:58 -0700

    [AF] Onboard card recommendations for model execution
    This CL contains code in the Chrome repo to enable the AI card
    recommendation feature to trigger Gemini via `RemoteModelExecutor`. It
    adds `kCardRecommendations`.
    
    One design consideration is that in the switch statement for
    `IsAccessTokenRequiredForFeature`, we return true, as this feature deals
    with sensitive information.
    
    Design: go/autofill-ai-card-recommendations-design
    Bug: 524309210
    Change-Id: I36c96719be08d003a85e74f4cc007251e2af883a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061024
    Reviewed-by: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Haochen Feng <haochenf@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664911}

--- components/optimization_guide/core/model_execution/feature_keys.h
+++ components/optimization_guide/core/model_execution/feature_keys.h
@@ -56,6 +56,8 @@ enum class ModelBasedCapabilityKey {
       proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_CONTEXTUAL_CUEING,
   kUpdaterChat =
       proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_UPDATER_CHAT,
+  kCardRecommendations = proto::ModelExecutionFeature::
+      MODEL_EXECUTION_FEATURE_CARD_RECOMMENDATIONS,
   kContextHub =
       proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_CONTEXT_HUB,
   kReadAloudGenerateText = proto::ModelExecutionFeature::
@@ -109,6 +111,8 @@ inline std::ostream& operator<<(std::ostream& out,
       return out << "ContextualCueing";
     case ModelBasedCapabilityKey::kUpdaterChat:
       return out << "UpdaterChat";
+    case ModelBasedCapabilityKey::kCardRecommendations:
+      return out << "CardRecommendations";
     case ModelBasedCapabilityKey::kContextHub:
       return out << "ContextHub";
     case ModelBasedCapabilityKey::kReadAloudGenerateText:
@@ -222,6 +226,9 @@ inline proto::ModelExecutionFeature ToModelExecutionFeatureProto(
           MODEL_EXECUTION_FEATURE_CONTEXTUAL_CUEING;
     case ModelBasedCapabilityKey::kUpdaterChat:
       return proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_UPDATER_CHAT;
+    case ModelBasedCapabilityKey::kCardRecommendations:
+      return proto::ModelExecutionFeature::
+          MODEL_EXECUTION_FEATURE_CARD_RECOMMENDATIONS;
     case ModelBasedCapabilityKey::kContextHub:
       return proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_CONTEXT_HUB;
     case ModelBasedCapabilityKey::kReadAloudGenerateText:
