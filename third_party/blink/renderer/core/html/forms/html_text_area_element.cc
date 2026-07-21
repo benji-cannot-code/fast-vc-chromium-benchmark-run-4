e3adb7c2fdb3166b9cb65a09d748a998c3826e1a
Kent Tamura <tkent@chromium.org>
2026-07-21 00:33:45 -0700

    Remove "TextAreaScrollTopPreview" runtime flag
    It has been enabled by default since M149. As no regressions have
    been reported, this CL removes the runtime flag and makes the
    behavior permanent.
    
    Bug: 506163510
    Change-Id: I593c12a30e66ac0a13722d7bda6e91b5c4a011bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129577
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665240}

--- third_party/blink/renderer/core/html/forms/html_text_area_element.cc
+++ third_party/blink/renderer/core/html/forms/html_text_area_element.cc
@@ -226,16 +226,14 @@ int HTMLTextAreaElement::scrollHeight() {
 }
 
 double HTMLTextAreaElement::scrollLeft() {
-  if (RuntimeEnabledFeatures::TextAreaScrollTopPreviewEnabled() &&
-      !SuggestedValue().empty()) {
+  if (!SuggestedValue().empty()) {
     return 0;
   }
   return TextControlElement::scrollLeft();
 }
 
 double HTMLTextAreaElement::scrollTop() {
-  if (RuntimeEnabledFeatures::TextAreaScrollTopPreviewEnabled() &&
-      !SuggestedValue().empty()) {
+  if (!SuggestedValue().empty()) {
     return 0;
   }
   return TextControlElement::scrollTop();
