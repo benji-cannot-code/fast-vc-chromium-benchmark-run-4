b115d822c9f573b61b06a87babaa8cff39681a76
quacksort <quacksort@google.com>
2026-07-21 03:16:50 -0700

    [Signin][Android] Add SubscriptionEligibilityService to Clank
    This exposes the native SubscriptionEligibilityService to the Java
    layer. It's needed to retrieve the profile AI tier subscription status
    in order to show the AI tier status ring.
    
    Bug: 522145863
    Change-Id: I48d97bf3a96b370942a1752d93b50197dd77bd55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8041302
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Lucia Giorgi <quacksort@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665344}

--- components/subscription_eligibility/subscription_eligibility_service.h
+++ components/subscription_eligibility/subscription_eligibility_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -42,6 +43,8 @@ class SubscriptionEligibilityService : public KeyedService {
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
+  base::WeakPtr<SubscriptionEligibilityService> GetWeakPtr();
+
  private:
   // Invoked when underlying pref for ai subscription tier changes.
   void OnAiSubscriptionTierUpdated();
@@ -53,6 +56,8 @@ class SubscriptionEligibilityService : public KeyedService {
   base::ObserverList<Observer> observers_;
 
   std::optional<int32_t> forced_tier_;
+
+  base::WeakPtrFactory<SubscriptionEligibilityService> weak_ptr_factory_{this};
 };
 
 }  // namespace subscription_eligibility
