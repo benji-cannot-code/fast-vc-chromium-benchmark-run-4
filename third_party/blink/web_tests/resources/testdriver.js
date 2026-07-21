6b91dc6739637f3f02faa42394763bd3ecd59483
Mason Freed <masonf@chromium.org>
2026-07-20 18:05:44 -0700

    Fix a testdriver regression
    This code:
    
    ```
      return test_driver.click(button)
        .then(wait_click)
        .then(function () {
      ...
    ```
    
    has a bug at line 2. The `wait_click` variable there is a
    Promise, not a function, so passing it to `.then()` makes
    it a no-op. That leads to the code immediately running the post-
    click actions, without actually waiting for the click to get
    processed. That causes test flakiness.
    
    NOTE: this was already fixed, 2 years ago, in this CL:
    
    https://crrev.com/c/6094526
    
    See also:
    https://github.com/web-platform-tests/wpt/pull/49691
    https://github.com/web-platform-tests/wpt/pull/49719
    
    It appears that this broken line of code has come back somehow. I'm
    not sure how to guard against that in the future? I guess I'm going
    to make a local CL that touches that line, and monitor it for a merge
    conflict. It seems like there should be a better way.
    
    Fixed: 388024336
    Change-Id: Ifa293407c7660203d181379efc0d1ed33e4c5ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8120765
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665089}

--- third_party/blink/web_tests/resources/testdriver.js
+++ third_party/blink/web_tests/resources/testdriver.js
@@ -421,7 +421,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             let wait_click = new Promise(resolve => button.addEventListener("click", resolve));
 
             return test_driver.click(button)
-              .then(wait_click)
+              .then(() => wait_click)
               .then(function () {
                     button.remove();
 
