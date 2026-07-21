acf0575e520f12dad3f35d8639daa418760f9b97
henrika <henrika@google.com>
2026-07-21 00:38:45 -0700

    webrtc: Replace raw pointer logging with tokens in MediaStreamPowerLogger
    Bug: 518100026
    Change-Id: I8b442d5648f2cab333a07a60093defd15c11975f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124755
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665243}

--- content/browser/renderer_host/media/media_stream_power_logger.h
+++ content/browser/renderer_host/media/media_stream_power_logger.h
@@ -7,14 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_RENDERER_HOST_MEDIA_MEDIA_STREAM_POWER_LOGGER_H_
 
 #include "base/power_monitor/power_observer.h"
+#include "base/unguessable_token.h"
+#include "content/common/content_export.h"
 
 namespace content {
 
 // Injects system power event log entries into the WebRTC text logs, for
 // debugging of unexpected call ending and performance changes eg when calls end
 // due to a laptop's lid being closed.
-class MediaStreamPowerLogger : public base::PowerSuspendObserver,
-                               public base::PowerThermalObserver {
+class CONTENT_EXPORT MediaStreamPowerLogger
+    : public base::PowerSuspendObserver,
+      public base::PowerThermalObserver {
  public:
   MediaStreamPowerLogger();
   ~MediaStreamPowerLogger() override;
@@ -27,6 +30,9 @@ class MediaStreamPowerLogger : public base::PowerSuspendObserver,
   void OnThermalStateChange(
       base::PowerThermalObserver::DeviceThermalState new_state) override;
   void OnSpeedLimitChange(int new_limit) override;
+
+ private:
+  const base::UnguessableToken id_;
 };
 }  // namespace content
 
