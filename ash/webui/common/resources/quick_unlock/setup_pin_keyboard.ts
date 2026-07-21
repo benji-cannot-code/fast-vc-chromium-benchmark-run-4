1e58bf89a9b884c17f9a2a4d615345bc5450e4d8
Ivan Šandrk <isandrk@chromium.org>
2026-07-21 05:25:10 -0700

    LPP: Remove spurious error log on auth token expiration
    Auth tokens naturally become invalid due to the expected 5-minute
    timeout, user cancellation, or explicit system cleanup. Previously,
    these routine expirations were unconditionally logged via
    `console.error`, triggering false-positive crash reports.
    
    Bug: 521985068
    Change-Id: Ia09201b6901ff779ba8e5fceae9651a4fe44ce89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126061
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Fahad Mansoor <fahadmansoor@google.com>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665388}

--- ash/webui/common/resources/quick_unlock/setup_pin_keyboard.ts
+++ ash/webui/common/resources/quick_unlock/setup_pin_keyboard.ts
@@ -541,7 +541,6 @@ export class SetupPinKeyboardElement extends SetupPinKeyboardElementBase {
       }
       this.localAuthFactorsComplexity_ = newValue;
     } catch (e) {
-      console.error('Error calling fetchLocalAuthFactorsComplexity_:', e);
       this.localAuthFactorsComplexity_ = LocalAuthFactorsComplexity.kUnset;
       switch (e) {
         case ConfigureResult.kInvalidTokenError:
