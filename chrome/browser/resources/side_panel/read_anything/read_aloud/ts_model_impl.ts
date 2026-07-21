ebc7a4e39730b8d4f760728a5c409935e5c68964
Lauren Winston <lwinston@google.com>
2026-07-21 10:15:13 -0700

    [Read anything] Move common traversal methods to tree_traversal.ts
    Bug: none
    Change-Id: I57f36eb2e7dd26e2ad9308b5050ac2d6326c83a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127099
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Martín Gómez <martinglopez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665587}

--- chrome/browser/resources/side_panel/read_anything/read_aloud/ts_model_impl.ts
+++ chrome/browser/resources/side_panel/read_anything/read_aloud/ts_model_impl.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert} from '//resources/js/assert.js';
 
-import {getReadingModeTextNodes} from '../shared/common.js';
+import {getReadingModeTextNodes} from '../shared/tree_traversal.js';
 
 import type {ReadAloudModelBrowserProxy} from './read_aloud_model_browser_proxy.js';
 import type {OffsetByNode, Segment, SegmentedSentence, Sentence} from './read_aloud_types.js';
