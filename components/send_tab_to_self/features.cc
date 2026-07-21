2a564369f43746bca998f02b4bd768ac4931a325
Ankush Singh <ankushkush@google.com>
2026-07-21 02:02:35 -0700

    STTS: Open received tab when in tab switcher
    Currently, with kSendTabToSelfAutoOpen enabled, tabs are auto-opened in
    the background along with an infobar notification. Originally, it was
    intentionally made to not do this when the tab switcher is open and
    delay the opening to when the user switches to any tab.
    
    Now, the idea is to open the tab right away, besides the current tab,
    when in the tab switcher.
    
    This CL does this and updates the behavior, in general, to open the tab
    relative to the current tab.
    
    Screencast(Googlers-only):
    https://drive.google.com/file/d/1rJY-Q1MyJA77MdWDL5kaupSdOr8AQqGu/view?usp=sharing
    
    Fixed: 532029897
    Change-Id: I4fd5575bfc64d26233b2d5d0c69507586a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065251
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665298}

--- components/send_tab_to_self/features.cc
+++ components/send_tab_to_self/features.cc
@@ -39,6 +39,11 @@ BASE_FEATURE(kSendTabToSelfPropagateNavigationHistory,
 
 BASE_FEATURE(kSendTabToSelfAutoOpen, base::FEATURE_DISABLED_BY_DEFAULT);
 
+#if BUILDFLAG(IS_IOS)
+BASE_FEATURE(kSendTabToSelfSupportAutoOpenInTabGrid,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_IOS)
+
 BASE_FEATURE(kSendTabToSelfEnhancedDesktopUI,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
