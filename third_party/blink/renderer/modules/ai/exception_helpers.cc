210249239dfd98f7966cdd5652bd8a9608f2f4ae
Aryan Krishnan <aryankrishnan4b@gmail.com>
2026-07-20 18:20:54 -0700

    Link to chrome://crashes in case of model crashes
    This CL updates the text for the model crashing too many times to link
    to chrome://crashes to allow for easier debugging and to more easily
    close the loop when developers report model crashes.
    
    Bug: 523313375
    Change-Id: I82ad80cafd945d94f9bb10a7c8abe9dcaba74d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118847
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Auto-Submit: Aryan Krishnan <aryankrishnan4b@gmail.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665097}

--- third_party/blink/renderer/modules/ai/exception_helpers.cc
+++ third_party/blink/renderer/modules/ai/exception_helpers.cc
@@ -321,7 +321,8 @@ String ConvertModelAvailabilityCheckResultToDebugString(
       return "The GPU is blocked.";
     case mojom::blink::ModelAvailabilityCheckResult::
         kUnavailableTooManyRecentCrashes:
-      return "The model process crashed too many times for this version.";
+      return "The model process crashed too many times for this version. Check "
+             "chrome://crashes for additional information.";
     case mojom::blink::ModelAvailabilityCheckResult::
         kUnavailableSafetyModelNotAvailable:
       return "The safety model was required but not available.";
