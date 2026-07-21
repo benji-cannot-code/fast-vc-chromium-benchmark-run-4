a85e33f53c8ba30f750794a8d83826b8d299b061
pchodur <pchodur@google.com>
2026-07-21 02:09:17 -0700

    Stop exposing transformed window coordinates to blink
    This commit changes the way the window bounds are exposed to blink.
    Before the change, the bounds were exposed with any transforms that were
    applied. After the change, the bounds are exposed skipping the
    transformations. This means that, for example, opening overview mode
    on ChromeOS will not modify the window.screenX and window.screenY.
    
    Bug: 351975377
    Change-Id: I3c64a47c374e3122052d7b6888ab4e906a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7967980
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Patryk Chodur <pchodur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665305}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -664,12 +664,12 @@ void RenderWidgetHostImpl::SendScreenRects() {
   }
 
   if (last_view_screen_rect_ == view_->GetViewBounds() &&
-      last_window_screen_rect_ == view_->GetBoundsInScreen()) {
+      last_window_screen_rect_ == view_->GetBoundsInScreenWithoutTransform()) {
     return;
   }
 
   last_view_screen_rect_ = view_->GetViewBounds();
-  last_window_screen_rect_ = view_->GetBoundsInScreen();
+  last_window_screen_rect_ = view_->GetBoundsInScreenWithoutTransform();
   blink_widget_->UpdateScreenRects(
       last_view_screen_rect_, last_window_screen_rect_,
       base::BindOnce(&RenderWidgetHostImpl::OnUpdateScreenRectsAck,
@@ -2942,7 +2942,7 @@ void RenderWidgetHostImpl::OnUpdateScreenRectsAck() {
   view_->SendInitialPropertiesIfNeeded();
 
   if (view_->GetViewBounds() == last_view_screen_rect_ &&
-      view_->GetBoundsInScreen() == last_window_screen_rect_) {
+      view_->GetBoundsInScreenWithoutTransform() == last_window_screen_rect_) {
     return;
   }
 
--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -505,6 +505,10 @@ WidgetType RenderWidgetHostViewBase::GetWidgetType() {
   return widget_type_;
 }
 
+gfx::Rect RenderWidgetHostViewBase::GetBoundsInScreenWithoutTransform() {
+  return GetBoundsInScreen();
+}
+
 gfx::AcceleratedWidget
     RenderWidgetHostViewBase::AccessibilityGetAcceleratedWidget() {
   return gfx::kNullAcceleratedWidget;
