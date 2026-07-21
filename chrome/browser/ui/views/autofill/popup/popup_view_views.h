43ab258730a11b6fa6810f30f9c45ff76c24516e
Jan Keitel <jkeitel@google.com>
2026-07-21 06:21:03 -0700

    Allow customizing sub-popup hide timer
    This CL introduces a mechanism that allows customizing the sub-popup
    hide timer and changes the value for AtMemory from 2.5s to 1s.
    
    Bug: 535128201
    Change-Id: Ib5e41eb778650679c3d9c47179bdc1eed3f3603a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118248
    Reviewed-by: Ireneusz Szulc <ireneuszszulc@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665418}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.h
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.h
@@ -119,7 +119,9 @@ class PopupViewViews : public PopupBaseView,
   // Constructor for creating sub-popups.
   PopupViewViews(base::WeakPtr<AutofillPopupController> controller,
                  base::WeakPtr<ExpandablePopupParentView> parent,
-                 views::Widget* parent_widget);
+                 views::Widget* parent_widget,
+                 std::optional<const AutofillPopupView::SubPopupConfig>
+                     sub_popup_config = std::nullopt);
 
   // Constructor for creating root level popups.
   // Providing `std::nullopt` to the `search_bar_config` results in creating a
@@ -131,7 +133,9 @@ class PopupViewViews : public PopupBaseView,
       std::optional<const AutofillPopupView::SearchBarConfig>
           search_bar_config = std::nullopt,
       std::optional<const AutofillPopupView::TabbedPaneConfig>
-          tabbed_pane_config = std::nullopt);
+          tabbed_pane_config = std::nullopt,
+      std::optional<const AutofillPopupView::SubPopupConfig> sub_popup_config =
+          std::nullopt);
   PopupViewViews(const PopupViewViews&) = delete;
   PopupViewViews& operator=(const PopupViewViews&) = delete;
   ~PopupViewViews() override;
@@ -373,6 +377,8 @@ class PopupViewViews : public PopupBaseView,
       search_bar_config_;
   const std::optional<const AutofillPopupView::TabbedPaneConfig>
       tabbed_pane_config_;
+  const std::optional<const AutofillPopupView::SubPopupConfig>
+      sub_popup_config_;
   raw_ptr<PopupSearchBarView> search_bar_ = nullptr;
   raw_ptr<views::TabbedPane> tabbed_pane_ = nullptr;
   raw_ptr<views::BoxLayoutView> suggestions_container_ = nullptr;
