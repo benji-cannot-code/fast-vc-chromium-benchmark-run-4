44efebd68d973f4cec602a21374c10b30a606194
Fredrik Söderquist <fs@opera.com>
2026-07-21 16:33:22 -0700

    Pass std::string_view to RuntimeEnabledFeatures::*FeatureFromString()
    Similarly adjust WebRuntimeFeatures::EnableFeatureFromString(). This
    avoids creating temporary strings in for example
    SetRuntimeFeaturesFromChromiumFeatures().
    
    Change-Id: Ie6c742ce0716a72c14b90d86d8e0a7e97277a365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8125834
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665888}

--- third_party/blink/renderer/platform/exported/web_runtime_features.cc
+++ third_party/blink/renderer/platform/exported/web_runtime_features.cc
@@ -40,7 +40,7 @@ void WebRuntimeFeatures::EnableExperimentalFeatures(bool enable) {
   RuntimeEnabledFeatures::SetExperimentalFeaturesEnabled(enable);
 }
 
-void WebRuntimeFeatures::EnableFeatureFromString(const std::string& name,
+void WebRuntimeFeatures::EnableFeatureFromString(std::string_view name,
                                                  bool enable) {
   RuntimeEnabledFeatures::SetFeatureEnabledFromString(name, enable);
 }
