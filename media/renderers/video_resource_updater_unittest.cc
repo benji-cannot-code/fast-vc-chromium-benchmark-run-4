089143b7033299645d2bfa46dbfa0f7f6c5fc95b
Saifuddin Hitawala <hitawala@chromium.org>
2026-07-21 07:11:37 -0700

    [media] Add DumpWillBeCheck for format mismatch in WrapSharedImage
    All callers of WrapSharedImage should be passing similar formats.
    This change adds a DUMP_WILL_BE_CHECK with crash keys to fish out
    anything that missed before upgrading to CHECK.
    
    Also update tests that had mismatches.
    
    Bug: 492116792
    Change-Id: Ia30d85f0c55a08207ac49c99780281d40fae24ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102889
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665452}

--- media/renderers/video_resource_updater_unittest.cc
+++ media/renderers/video_resource_updater_unittest.cc
@@ -244,7 +244,7 @@ class VideoResourceUpdaterTest : public testing::Test {
 
   scoped_refptr<VideoFrame> CreateTestRGBAHardwareVideoFrame() {
     return CreateTestHardwareVideoFrame(viz::SinglePlaneFormat::kRGBA_8888,
-                                        PIXEL_FORMAT_ARGB, kSRGBColorSpace,
+                                        PIXEL_FORMAT_ABGR, kSRGBColorSpace,
                                         GL_TEXTURE_2D,
                                         /*needs_raster_access=*/false);
   }
@@ -252,7 +252,7 @@ class VideoResourceUpdaterTest : public testing::Test {
   scoped_refptr<VideoFrame> CreateTestStreamTextureHardwareVideoFrame(
       bool needs_copy) {
     scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
-        viz::SinglePlaneFormat::kRGBA_8888, PIXEL_FORMAT_ARGB, kSRGBColorSpace,
+        viz::SinglePlaneFormat::kRGBA_8888, PIXEL_FORMAT_ABGR, kSRGBColorSpace,
         GL_TEXTURE_EXTERNAL_OES, /*needs_raster_access=*/needs_copy);
     video_frame->metadata().copy_required = needs_copy;
     return video_frame;
@@ -261,7 +261,7 @@ class VideoResourceUpdaterTest : public testing::Test {
 #if BUILDFLAG(IS_WIN)
   scoped_refptr<VideoFrame> CreateTestDCompSurfaceVideoFrame() {
     scoped_refptr<VideoFrame> video_frame = CreateTestHardwareVideoFrame(
-        viz::SinglePlaneFormat::kRGBA_8888, PIXEL_FORMAT_ARGB, kSRGBColorSpace,
+        viz::SinglePlaneFormat::kRGBA_8888, PIXEL_FORMAT_ABGR, kSRGBColorSpace,
         GL_TEXTURE_EXTERNAL_OES, /*needs_raster_access=*/false);
     video_frame->metadata().dcomp_surface = true;
     return video_frame;
