b60c793b6b5c98e0eed74a8333e039cfb31675ac
Yanling <yanlinghu@google.com>
2026-07-21 07:09:40 -0700

    [Ntp] Add tooltip text for profile and MVT tiles
    In this CL, we add tooltip text for profile button and most visited tile
    s.
    
    Demo: https://docs.google.com/document/d/1bMRVyyqOd0-cAmyTbL9IhKNU9uSvDdpdCngSzSXIHnk/edit?tab=t.0#bookmark=id.mpxolqm6i9ik
    Bug: 532874932, 533325867
    Change-Id: I6574a89ace0e6acb1dec5f2816875b0b57d26754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8119754
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665450}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTabletUnitTest.java
@@ -673,7 +673,9 @@ public final class ToolbarTabletUnitTest {
         updateOptionalButton(
                 /* buttonVariant= */ AdaptiveToolbarButtonVariant.READER_MODE,
                 /* tooltipTextResId= */ 0);
-        assertEquals(null, mToolbarTablet.getOptionalButtonViewForTesting().getTooltipText());
+        assertEquals(
+                mActivity.getString(R.string.actionbar_share),
+                mToolbarTablet.getOptionalButtonViewForTesting().getTooltipText());
 
         // Test whether share button tooltip Text is set correctly.
         updateOptionalButton(
