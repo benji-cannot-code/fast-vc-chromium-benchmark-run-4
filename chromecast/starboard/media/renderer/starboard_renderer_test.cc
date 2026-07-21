64741cd53e07f1ab02c2c2bc14a0ddad30fd59fe
Richard Nichols <rknichols@google.com>
2026-07-21 06:48:16 -0700

    [chromecast] Own audio extra_data in DemuxerStreamReader
    StarboardAudioSampleInfo::audio_specific_config is a non-owning pointer
    into an AudioDecoderConfig's extra_data. The DemuxerStreamReader already
    has a chromium_audio_config_ member intended to back this pointer, but
    it was only populated on kConfigChanged, not for the initial config
    supplied at construction time. As a result, audio_specific_config
    referenced the extra_data of a stack-local AudioDecoderConfig in
    StarboardPlayerManager::Create() that went out of scope before the first
    sample was written to starboard.
    
    Populate chromium_audio_config_ in the constructor and re-point
    audio_specific_config at the owned copy, matching what
    UpdateAudioConfig() already does for subsequent configs.
    
    Change-Id: I80ba0b17e7ec83d86fa3d5e27b92ff6788d910a2
    Bug: 513261751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8102143
    Commit-Queue: Richard Nichols <rknichols@google.com>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665437}

--- chromecast/starboard/media/renderer/starboard_renderer_test.cc
+++ chromecast/starboard/media/renderer/starboard_renderer_test.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
+#include "base/test/gtest_util.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
