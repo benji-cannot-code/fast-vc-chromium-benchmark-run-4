e7b0ea2d0f27f6421b569c9bb2a70fa85098deea
pchodur <pchodur@google.com>
2026-07-21 12:55:17 -0700

    Configure AWC runtime feature for OT
    This commit configures the Additional Windowing Controls runtime feature
    (DesktopPWAsAdditionalWindowingControls) for the Origin Trial. The
    feature is enabled by default in the browser process (can be disabled
    with the Finch and contains additional checks like window-management
    permission), and in the renderer process it is enabled depending on the
    feature flag or origin trial token.
    
    Bug: 288265319
    Change-Id: I6c4272a69ea5e98ce27409eb5411b38b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118367
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Commit-Queue: Patryk Chodur <pchodur@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665697}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -5931,8 +5931,14 @@ bool WebFrameWidgetImpl::SetResizableRequested(
 void WebFrameWidgetImpl::OnWindowShowStateChanged(
     ui::mojom::blink::WindowShowState old_state,
     ui::mojom::blink::WindowShowState new_state) {
-  if (!RuntimeEnabledFeatures::
-          DesktopPWAsAdditionalWindowingControlsEnabled()) {
+  LocalFrame* frame = local_root_ ? local_root_->GetFrame() : nullptr;
+  Document* document = frame ? frame->GetDocument() : nullptr;
+  ExecutionContext* execution_context =
+      document ? document->GetExecutionContext() : nullptr;
+
+  if (!execution_context ||
+      !RuntimeEnabledFeatures::DesktopPWAsAdditionalWindowingControlsEnabled(
+          execution_context)) {
     return;
   }
 
@@ -5961,8 +5967,14 @@ void WebFrameWidgetImpl::OnWindowShowStateChanged(
 }
 
 void WebFrameWidgetImpl::OnResizableChanged(bool new_resizable) {
-  if (!RuntimeEnabledFeatures::
-          DesktopPWAsAdditionalWindowingControlsEnabled() ||
+  LocalFrame* frame = local_root_ ? local_root_->GetFrame() : nullptr;
+  Document* document = frame ? frame->GetDocument() : nullptr;
+  ExecutionContext* execution_context =
+      document ? document->GetExecutionContext() : nullptr;
+
+  if (!execution_context ||
+      !RuntimeEnabledFeatures::DesktopPWAsAdditionalWindowingControlsEnabled(
+          execution_context) ||
       !ForMainFrame()) {
     return;
   }
