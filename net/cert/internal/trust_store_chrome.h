d1a10af026f61c62acb2a90de9652baecc56808c
Carlos IL <carlosil@chromium.org>
2026-07-21 09:47:05 -0700

    [MTCs] Handle flag in component update to disable mirroring requirements
    Currently this is just plumbed through to TrustStoreChrome where it sets
    a flag, no behavior changes yet.
    
    Bug: 535679907
    Change-Id: I8162a66fa3b8c45f0874b544f9f72fa2870e2502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116272
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665564}

--- net/cert/internal/trust_store_chrome.h
+++ net/cert/internal/trust_store_chrome.h
@@ -278,6 +278,12 @@ class NET_EXPORT ChromeRootStoreData {
   void SetSignerSet(ChromeRootStoreSignerSet signer_set) {
     signer_set_ = std::move(signer_set);
   }
+  bool disable_mtc_mirroring_requirements() const {
+    return disable_mtc_mirroring_requirements_;
+  }
+  void SetDisableMtcMirroringRequirements(bool disable) {
+    disable_mtc_mirroring_requirements_ = disable;
+  }
   int64_t version() const { return version_; }
 
  private:
@@ -292,6 +298,7 @@ class NET_EXPORT ChromeRootStoreData {
   std::vector<Anchor> eutl_certs_;
   std::vector<MtcAnchor> mtc_trust_anchors_;
   std::optional<ChromeRootStoreSignerSet> signer_set_;
+  bool disable_mtc_mirroring_requirements_ = false;
   int64_t version_;
 };
 
