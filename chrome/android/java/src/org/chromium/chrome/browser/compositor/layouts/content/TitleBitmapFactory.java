ddbd6259660cf8aed9cde616252207a50290ef33
Michael Wu <mklw@google.com>
2026-07-20 17:34:15 -0700

    [Tab Strip Glic Button] Increase text font size based on UX request
    Changed from 12sp to 14sp:
    https://screenshot.googleplex.com/9RW6h8owGfRDFw3
    
    Bug: 536133227
    Change-Id: If50adc035a34fa156bc3907da528ea8cff8f381a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127581
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665074}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TitleBitmapFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TitleBitmapFactory.java
@@ -127,7 +127,7 @@ public class TitleBitmapFactory {
         StyleUtils.applyTextAppearanceToTextPaint(
                 context,
                 mButtonTextPaint,
-                R.style.TextAppearance_TextSmallThick_Primary,
+                R.style.TextAppearance_TextMediumThick_Primary,
                 /* applyFontFamily= */ true,
                 /* applyTextSize= */ true,
                 !mIncognito);
