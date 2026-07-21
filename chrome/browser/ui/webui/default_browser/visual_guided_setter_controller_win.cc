bc27715d560082ca77e39d2bda5cdf0d3abc2aaf
Atharv Maan <atharvmaan@chromium.org>
2026-07-21 09:39:30 -0700

    Update Arrow overlay on Settings window resize for Visual Guided Setter
    We do this by leveraging SettingsWindowFinder to hook into
    `EVENT_OBJECT_LOCATIONCHANGE` for the Settings window and run a callback
    to update the Arrow overlay location.
    
    Bug: b:534893936
    Change-Id: I7ba2773f241032eed491771891cc86e283561661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8111157
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665554}

--- chrome/browser/ui/webui/default_browser/visual_guided_setter_controller_win.cc
+++ chrome/browser/ui/webui/default_browser/visual_guided_setter_controller_win.cc
@@ -268,10 +268,20 @@ void VisualGuidedSetterControllerWin::OnSettingsWindowFound(HWND hwnd) {
   }
 
   settings_hwnd_ = hwnd;
+
+  if (!is_continuous_docking_enabled_) {
+    settings_window_finder_->StartObservingLocationChanges(
+        settings_hwnd_,
+        base::BindRepeating(
+            &VisualGuidedSetterControllerWin::UpdateDockedLayout,
+            weak_ptr_factory_.GetWeakPtr()));
+  }
+
   if (parent_widget_ && !parent_widget_->IsVisible()) {
     ::ShowWindow(settings_hwnd_, SW_HIDE);
     return;
   }
+
   if (is_continuous_docking_enabled_) {
     StartRuntimeTimers();
   }
@@ -301,6 +311,7 @@ void VisualGuidedSetterControllerWin::StartRuntimeTimers() {
 
 void VisualGuidedSetterControllerWin::StopAllTimers() {
   settings_window_finder_->Stop();
+  settings_window_finder_->StopObservingLocationChanges();
   dock_timer_.Stop();
 }
 
