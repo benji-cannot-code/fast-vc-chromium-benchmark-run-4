9dcfcafd61da194c7d5f4e4b2a2ada08491cf8f4
Marc Treib <treib@chromium.org>
2026-07-21 05:40:38 -0700

    Simplify AddressDataManager::IsAutofillSyncToggleAvailable() to "false"
    In practice, the series of checks always evaluated to "false":
    * On all platforms except ChromeOS,
      syncer::IsReplaceSyncPromosWithSignInPromosEnabled() is always true.
    * On ChromeOS, the above is not true *yet* (though soon will be), but
      in the meantime, HasSyncConsent() is always true. (Except maybe in
      some edge cases like Kiosk devices, but then there'd be no account at
      all and so the sync TransportState would be DISABLED).
    
    Bug: 40897778
    Change-Id: Iafb97247fef18a5684f81736ae6fd2e26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128080
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665394}

--- components/autofill/core/browser/data_manager/addresses/address_data_manager.h
+++ components/autofill/core/browser/data_manager/addresses/address_data_manager.h
@@ -53,7 +53,6 @@ namespace autofill {
 class AccountNameEmailStore;
 class AddressDataCleaner;
 class AlternativeStateNameMapUpdater;
-class ContactInfoPreconditionChecker;
 class HomeAndWorkMetadataStore;
 
 // Contains all address-related logic of the `PersonalDataManager`. See comment
@@ -416,9 +415,6 @@ class AddressDataManager : public AutofillWebDataServiceObserverOnUISequence {
       /*check_empty=*/false,
       base::ObserverListReentrancyPolicy::kAllowReentrancyUntriaged>
       observers_;
-
-  std::unique_ptr<ContactInfoPreconditionChecker>
-      contact_info_precondition_checker_;
 
   WebDataServiceBase::Handle pending_profile_query_ = 0;
 
