d0db6a68198008a60cdabd311af03f3fa38c431d
Kevin McNee <mcnee@chromium.org>
2026-07-21 16:26:54 -0700

    Dictation: If a new stream fails, don't drop finalizing streams
    Currently, if the active stream fails, we immediately end the session,
    including dropping any streams that are finalizing. We now allow the
    remaining streams to finalize before ending the session.
    
    Additionally, we do not allow new stream creation while in this shutting
    down state.
    
    Bug: b:525854555
    Change-Id: I9ad05c190edffb1e24eb037974d153bd164bf580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127037
    Commit-Queue: David Bokan <bokan@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665884}

--- chrome/browser/dictation/dictation_interactive_browser_test_base.h
+++ chrome/browser/dictation/dictation_interactive_browser_test_base.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/dictation/dictation_browser_test_base.h"
+#include "chrome/browser/dictation/dictation_multiplexer.h"
 #include "chrome/browser/dictation/session_state.h"
 #include "chrome/common/extensions/api/dictation_private.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
@@ -31,6 +32,7 @@ class DictationInteractiveBrowserTestBase
   using ExtensionStreamState = extensions::api::dictation_private::StreamState;
   using ExtensionTranscriptionType =
       extensions::api::dictation_private::TranscriptionType;
+  using StreamId = DictationMultiplexer::StreamId;
 
   DictationInteractiveBrowserTestBase();
   ~DictationInteractiveBrowserTestBase() override;
@@ -48,9 +50,17 @@ class DictationInteractiveBrowserTestBase
   // until the StreamStart event has been received in the extension.
   MultiStep StartSession();
 
+  // If a StreamId isn't specified, then these operate on the last started
+  // stream.
   MultiStep ExtensionAPISetStreamState(ExtensionStreamState state);
+  MultiStep ExtensionAPISetStreamState(const StreamId& stream_id,
+                                       ExtensionStreamState state);
   MultiStep ExtensionAPIUpdateTranscription(ExtensionTranscriptionType type,
                                             std::string_view text);
+  MultiStep ExtensionAPIUpdateTranscription(const StreamId& stream_id,
+                                            ExtensionTranscriptionType type,
+                                            std::string_view text);
+  MultiStep ExtensionAPIWaitForStreamStart();
 
   base::RepeatingCallback<SessionState()> GetSessionState();
   base::RepeatingCallback<bool()> HasAttachedStreamProvider();
--- chrome/browser/dictation/session_ui_impl.cc
+++ chrome/browser/dictation/session_ui_impl.cc
@@ -108,7 +108,9 @@ void SessionUiImpl::OnError(StreamType stream_type) {
   }
 
   if (stream_type == StreamType::kAttached) {
-    controller_->UiRequestEndSession();
+    // If the attached stream failed, we still want to let any finalizing
+    // streams finish before ending the session.
+    controller_->FinalizeAndShutdown();
   }
 }
 
