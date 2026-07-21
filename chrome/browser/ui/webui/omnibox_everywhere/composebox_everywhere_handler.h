6ecc113d86eb73982c75e341e571b87de598b868
Juliet Lévesque Knighton <julietlevesque@google.com>
2026-07-21 13:09:32 -0700

    [loomnibox] Add drive picker logic to composebox
    When loomnibox shifts to composebox, the drive
    picker logic isn't handled. This CL adds logic to listen for drive
    events to the composebox everywhere handler to correctly handle events.
    
    TESTED=+ menu > drive option > select file > + menu > drive option > select file works correctly now
    
    Change-Id: Ibc0d439dc504115d965802c98e8c0b555e9370ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8123278
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Juliet Lévesque Knighton <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665720}

--- chrome/browser/ui/webui/omnibox_everywhere/composebox_everywhere_handler.h
+++ chrome/browser/ui/webui/omnibox_everywhere/composebox_everywhere_handler.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 
 class Profile;
+class OmniboxEverywhereService;
 
 namespace content {
 class WebContents;
@@ -35,6 +36,15 @@ class ComposeboxEverywhereHandler : public ComposeboxHandler {
       delete;
 
   ~ComposeboxEverywhereHandler() override;
+
+  // searchbox::mojom::PageHandler:
+  void OnDriveUploadClicked(OnDriveUploadClickedCallback callback) override;
+
+  // ContextualSearchboxHandler:
+  void CleanupDrivePicker() override;
+
+ private:
+  raw_ptr<OmniboxEverywhereService> service_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_OMNIBOX_EVERYWHERE_COMPOSEBOX_EVERYWHERE_HANDLER_H_
