31748e8264152e254383ea489df41c730b2cc575
Rune Lillesveen <futhark@chromium.org>
2026-07-21 01:54:40 -0700

    Track line-height units properly for container queries
    This fixes rlh unit invalidation issues for style() container queries
    
    Bug: 524390004
    
    Change-Id: I1b7bccb15a10faf1679e126844d0a544347322b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8011494
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665286}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -441,7 +441,7 @@ void ApplyLengthConversionFlags(StyleResolverState& state) {
     builder.SetHasEmUnits();
   }
   if (flags & static_cast<Flags>(Flag::kRootFontRelative)) {
-    builder.SetHasRootFontRelativeUnits();
+    builder.SetHasRootRelativeUnits();
   }
   if (flags & static_cast<Flags>(Flag::kGlyphRelative)) {
     builder.SetHasGlyphRelativeUnits();
@@ -489,6 +489,7 @@ void ApplyLengthConversionFlags(StyleResolverState& state) {
   }
   if (flags & static_cast<Flags>(Flag::kRlhRelative)) {
     builder.SetHasLineHeightRelativeUnits();
+    builder.SetHasRootRelativeUnits();
     UseCounter::Count(state.GetDocument(), WebFeature::kHasRlhUnits);
   }
   if (flags & static_cast<Flags>(Flag::kChRelative)) {
@@ -1464,7 +1465,7 @@ const ComputedStyle* StyleResolver::ResolveStyle(
 
   GetDocument().AddViewportUnitFlags(state.StyleBuilder().ViewportUnitFlags());
 
-  if (state.StyleBuilder().HasRootFontRelativeUnits()) {
+  if (state.StyleBuilder().HasRootRelativeUnits()) {
     GetDocument().GetStyleEngine().SetUsesRootRelativeUnits(true);
   }
 
@@ -1911,8 +1912,8 @@ void StyleResolver::ApplyBaseStyleNoCache(
   if (match_result.DependsOnDynamicViewportUnits()) {
     builder.SetHasDynamicViewportUnits();
   }
-  if (match_result.DependsOnRootFontContainerQueries()) {
-    builder.SetHasRootFontRelativeUnits();
+  if (match_result.DependsOnRootUnitContainerQueries()) {
+    builder.SetHasRootRelativeUnits();
   }
   if (match_result.ConditionallyAffectsAnimations()) {
     state.SetConditionallyAffectsAnimations();
