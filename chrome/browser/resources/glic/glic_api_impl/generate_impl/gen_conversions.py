cf31e921d005bb30a4262663cd2f7720b0afa6b2
Takuto Ikuta <tikuta@chromium.org>
2026-07-21 07:51:59 -0700

    glic: Use action_with_pydeps for generate_enum_conversions
    Update generate_enum_conversions in
    chrome/browser/resources/glic/BUILD.gn to use action_with_pydeps.
    
    - Directly invoke gen_conversions.py instead of run_module.py.
    - Support importing parse in gen_conversions.py when run directly.
    - Add gen_conversions.pydeps.
    - Remove obsolete run_module.py.
    
    Bug: b:533462055, 513105742
    Change-Id: Icbf9c76cf827ee4412c490331816e57f5d6a1c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8130417
    Commit-Queue: Dan H <harringtond@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665480}

--- chrome/browser/resources/glic/glic_api_impl/generate_impl/gen_conversions.py
+++ chrome/browser/resources/glic/glic_api_impl/generate_impl/gen_conversions.py
@@ -6,8 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import io
 import os
+import sys
 
-from . import parse
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from generate_impl import parse
 
 
 def _FunctionSignatureString(function_name, param_type, return_type):
