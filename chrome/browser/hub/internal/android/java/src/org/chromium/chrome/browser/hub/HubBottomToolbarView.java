47fc3029cf28db1d856840957c51f7c8a13ea139
Dan Polanco <polardz@google.com>
2026-07-21 14:29:39 -0700

    [IA][BottomBar] Properly colour bottom chin area in GTS / Hub
    http://go/scrcast/NjUwODcyMzI4NjU3MzA1Nnw2YjdiNGQ2My1kOA
    
    Bug: 535604083
    Fixed: 535279342
    Change-Id: I04c051b6d8d85d230ba2bc2188e01f3160ffc11b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8123694
    Commit-Queue: Dan Polanco <polardz@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665797}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubBottomToolbarView.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubBottomToolbarView.java
@@ -31,7 +31,7 @@ public class HubBottomToolbarView extends LinearLayout {
         mColorMixerHelper.registerBlend(
                 new SingleHubViewColorBlend(
                         PANE_COLOR_BLEND_ANIMATION_DURATION_MS,
-                        colorScheme -> HubColors.getBackgroundColor(context, colorScheme),
+                        colorScheme -> HubColors.getHubBottomToolbarColor(context, colorScheme),
                         this::setBackgroundColor));
     }
 
