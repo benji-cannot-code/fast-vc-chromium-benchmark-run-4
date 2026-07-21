ecd64c199b1d5af68e028a8d434b3c602dc9dad2
Eriko Kurimoto <elkurin@chromium.org>
2026-07-20 17:48:04 -0700

    Revert "[GlicTabGroup] Add tab to group when bound to a tab group instance."
    This reverts commit 6a899b64b9a5bb5b4a20f1b73ae50b0e87d46d41.
    
    Reason for revert: The culprit of wayland bot failure with
    PanelFocusDependentHotkeyManagerBrowserTest.CloseHotkeyEscKey 
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-wayland-weston-rel-tests/17091/overview
    
    Original change's description:
    > [GlicTabGroup] Add tab to group when bound to a tab group instance.
    >
    > Bug: None
    > Change-Id: I99e5e96f8bf7ac0067de58c194a77cf16a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8120413
    > Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    > Commit-Queue: Marke Hallowell <wry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1664940}
    
    Bug: None
    Change-Id: I02b976ae785cc670648883fd865c6807645a42c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124016
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665080}

--- chrome/browser/glic/service/glic_instance_impl.cc
+++ chrome/browser/glic/service/glic_instance_impl.cc
@@ -1588,10 +1588,6 @@ GlicInstanceImpl::EmbedderEntry& GlicInstanceImpl::BindTab(
     GetSharingManagerInternal().PinTabs({tab->GetHandle()}, pin_trigger);
   }
 
-  if (tab_group_id_.has_value()) {
-    EnsureTabInGroup(tab, tab_group_id_.value());
-  }
-
   UpdateFloatingPanelCanAttach();
 
   return new_entry;
