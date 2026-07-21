983365a95962f2d1aa6e9c1d7eb17768f1ff8121
rbpotter <rbpotter@chromium.org>
2026-07-21 16:35:19 -0700

    WebUI: Leverage autogenerated Mojo proxy factories in more places
    Specifically:
     - Context Hub
     - Feature Showcase
     - Extensions zero state
    
    Bug: 513276098
    Change-Id: I448ae26757ef6e3f025f312fb02b36f90424d345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8112538
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665889}

--- chrome/browser/resources/context_hub/memory_banks/memory_banks.ts
+++ chrome/browser/resources/context_hub/memory_banks/memory_banks.ts
@@ -10,8 +10,7 @@ import '//resources/cr_elements/cr_search_field/cr_search_field.js';
 
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
-import {BrowserProxyImpl} from '../browser_proxy.js';
-import {EntryType} from '../context_hub.mojom-webui.js';
+import {browserProxyFactory, EntryType} from '../context_hub.mojom-webui.js';
 import type {MemoryBankEntry} from '../context_hub.mojom-webui.js';
 
 import {getCss} from './memory_banks.css.js';
@@ -48,8 +47,8 @@ export class MemoryBanksElement extends CrLitElement {
   }
 
   private async fetchEntries() {
-    const {entries} =
-        await BrowserProxyImpl.getInstance().handler.getAllMemoryBankEntries();
+    const {entries} = await browserProxyFactory.getInstance()
+                          .handler.getAllMemoryBankEntries();
     this.entries = entries;
   }
 
@@ -156,7 +155,8 @@ export class MemoryBanksElement extends CrLitElement {
 
   protected async onDeleteClick_() {
     const ids = Array.from(this.selectedIds);
-    await BrowserProxyImpl.getInstance().handler.deleteMemoryBankEntries(ids);
+    await browserProxyFactory.getInstance().handler.deleteMemoryBankEntries(
+        ids);
     this.selectedIds = new Set();
     await this.fetchEntries();
   }
--- chrome/test/data/webui/extensions_zero_state/zero_state_promo_app_test.ts
+++ chrome/test/data/webui/extensions_zero_state/zero_state_promo_app_test.ts
@@ -4,10 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 import 'chrome://extensions-zero-state/zero_state_promo_app.js';
 
-import {WebStoreLinkClicked} from 'chrome://extensions-zero-state/zero_state_promo.mojom-webui.js';
+import {browserProxyFactory as zeroStatePromoProxyFactory, WebStoreLinkClicked} from 'chrome://extensions-zero-state/zero_state_promo.mojom-webui.js';
+import type {PageHandlerInterface} from 'chrome://extensions-zero-state/zero_state_promo.mojom-webui.js';
 import type {ZeroStatePromoAppElement} from 'chrome://extensions-zero-state/zero_state_promo_app.js';
-import type {ZeroStatePromoBrowserProxy} from 'chrome://extensions-zero-state/zero_state_promo_browser_proxy.js';
-import {ZeroStatePromoBrowserProxyImpl} from 'chrome://extensions-zero-state/zero_state_promo_browser_proxy.js';
 import {CustomHelpBubbleUserAction} from 'chrome://resources/cr_components/help_bubble/custom_help_bubble.mojom-webui.js';
 import type {CustomHelpBubbleHandlerInterface} from 'chrome://resources/cr_components/help_bubble/custom_help_bubble.mojom-webui.js';
 import type {CustomHelpBubbleProxy} from 'chrome://resources/cr_components/help_bubble/custom_help_bubble_proxy.js';
@@ -41,7 +40,7 @@ export class TestBubbleProxy implements CustomHelpBubbleProxy {
 }
 
 export class TestPromoProxy extends TestBrowserProxy implements
-    ZeroStatePromoBrowserProxy {
+    PageHandlerInterface {
   constructor() {
     super(['launchWebStoreLink']);
   }
@@ -61,7 +60,7 @@ suite('ChipsUiV1Test', () => {
     CustomHelpBubbleProxyImpl.setInstance(new TestBubbleProxy(bubbleHandler));
 
     promoProxy = new TestPromoProxy();
-    ZeroStatePromoBrowserProxyImpl.setInstance(promoProxy);
+    zeroStatePromoProxyFactory.setInstance({handler: promoProxy});
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     zeroStatePromoApp =
@@ -161,7 +160,7 @@ suite('ChipsUiV2Test', () => {
     CustomHelpBubbleProxyImpl.setInstance(new TestBubbleProxy(bubbleHandler));
 
     promoProxy = new TestPromoProxy();
-    ZeroStatePromoBrowserProxyImpl.setInstance(promoProxy);
+    zeroStatePromoProxyFactory.setInstance({handler: promoProxy});
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     zeroStatePromoApp =
@@ -261,7 +260,7 @@ suite('ChipsUiV3Test', () => {
     CustomHelpBubbleProxyImpl.setInstance(new TestBubbleProxy(bubbleHandler));
 
     promoProxy = new TestPromoProxy();
-    ZeroStatePromoBrowserProxyImpl.setInstance(promoProxy);
+    zeroStatePromoProxyFactory.setInstance({handler: promoProxy});
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     zeroStatePromoApp =
@@ -361,7 +360,7 @@ suite('PlainLinkUiTest', () => {
     CustomHelpBubbleProxyImpl.setInstance(new TestBubbleProxy(bubbleHandler));
 
     promoProxy = new TestPromoProxy();
-    ZeroStatePromoBrowserProxyImpl.setInstance(promoProxy);
+    zeroStatePromoProxyFactory.setInstance({handler: promoProxy});
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     zeroStatePromoApp =
