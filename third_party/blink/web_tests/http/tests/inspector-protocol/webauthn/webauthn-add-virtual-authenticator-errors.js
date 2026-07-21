47c1c21b762bc0f6e33895b96cb7458c255de8f2
Nina Satragno <nsatragno@google.com>
2026-07-21 10:43:27 -0700

    [cdp] Add hasCmtgKey to virtual authenticator
    Add the hasCmtgKey option to the WebAuthn.addVirtualAuthenticator CDP
    command. `hasCmtgKey` enables Credential Manager Trust Group Keys
    support for WebAuthn virtual authenticators.
    
    This is intended to be used by both devtools and webdriver.
    
    See https://github.com/w3c/webauthn/pull/2445
    
    TAG=agy
    CONV=00274877-c5d3-46c3-a466-9346b48eefc4
    
    Bug: 485888879
    Change-Id: I7da630ac0cd4decd9d5fab6079cfa84ccf1751b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8132258
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665615}

--- third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-add-virtual-authenticator-errors.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-add-virtual-authenticator-errors.js
@@ -108,5 +108,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   });
   testRunner.log(alreadyHasInternalAuthenticatorError);
 
+  const cmtgRequiresRKError = await dp.WebAuthn.addVirtualAuthenticator({
+    options: {
+      protocol: "ctap2",
+      transport: "usb",
+      hasResidentKey: false,
+      hasUserVerification: false,
+      hasCmtgKey: true
+    },
+  });
+  testRunner.log(cmtgRequiresRKError);
+
+  const cmtgRequiresCtap2Error = await dp.WebAuthn.addVirtualAuthenticator({
+    options: {
+      protocol: "u2f",
+      transport: "usb",
+      hasResidentKey: true,
+      hasUserVerification: false,
+      hasCmtgKey: true
+    },
+  });
+  testRunner.log(cmtgRequiresCtap2Error);
+
   testRunner.completeTest();
 })
