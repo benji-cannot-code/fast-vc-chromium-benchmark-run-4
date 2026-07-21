478499131c628fb0b25a27be08cbd2d65145e5dd
Kartar Singh <kartarsingh@google.com>
2026-07-21 16:07:31 -0700

    Refactor FrameDeadlineDecider sequence tracking state
    Encapsulate `curr_sequence_present_delta_` and
    `curr_sequence_deadline_index` inside a FrameSequenceState struct
    wrapped in std::optional<FrameSequenceState>.
    
    TAG=agy
    
    Bug: 500826814
    Change-Id: I07303c7d9647e106e95636666d705461404ad1d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126496
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665875}

--- components/viz/service/display/frame_deadline_decider.h
+++ components/viz/service/display/frame_deadline_decider.h
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_FRAME_DEADLINE_DECIDER_H_
 #define COMPONENTS_VIZ_SERVICE_DISPLAY_FRAME_DEADLINE_DECIDER_H_
 
-#include "base/memory/raw_ref.h"
+#include <optional>
+
 #include "base/time/time.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/service/viz_service_export.h"
@@ -54,9 +55,12 @@ class VIZ_SERVICE_EXPORT FrameDeadlineDecider {
   size_t FindClosestDeadlineByPresentation(
       const PossibleDeadlines& possible_deadlines) const;
 
-  bool in_frame_sequence_ = false;
-  base::TimeDelta curr_sequence_present_delta_;
-  size_t curr_sequence_deadline_index_ = 0;
+  struct FrameSequenceState {
+    base::TimeDelta present_delta;
+    size_t deadline_index = 0;
+  };
+
+  std::optional<FrameSequenceState> frame_sequence_state_;
   const bool use_platform_preferred_deadlines_;
 };
 
