9a20ddd13f08f6a5f60ac8ca622645a2696ffbf4
Javier Castro <jacastro@chromium.org>
2026-07-21 00:54:53 -0700

    [Site Familiarity] Add feature to enable JS Opt blocking for ESB users
    Introduce kEnableBlockV8OptimizerOnUnfamiliarSitesForEsbClients feature,
    which enables blocking the V8 optimizer on unfamiliar sites only for
    users with Enhanced Safe Browsing (ESB) enabled.
    
    This is a variant of kMigrateToBlockV8OptimizerOnUnfamiliarSites which
    affects only enhanced protection users.
    
    We perform the ESB check before the feature check to avoid enrolling
    non-ESB users in the Finch experiment.
    
    Bug: 523286171
    Test: browser_tests: *ServiceWorkerIntercept*
    Test: unit_tests: SiteFamiliarityUtils*Esb*
    
    TAG=agy
    CONV=ac48ddf1-1ac0-4e20-b94c-83926c2ca431
    
    Change-Id: I442517a25478c7438aec495c1a009417ee14248f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7932765
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665254}

--- chrome/browser/site_protection/site_familiarity_fetcher.cc
+++ chrome/browser/site_protection/site_familiarity_fetcher.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing/content/browser/web_ui/web_ui_content_info_singleton.h"
 #include "components/safe_browsing/core/common/features.h"
+#include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/site_engagement/content/site_engagement_service.h"
 #include "extensions/common/constants.h"
 #include "url/origin.h"
@@ -29,13 +30,6 @@ BASE_FEATURE(kSkipSiteFamiliarityDeferralForDefaultSearchEngine,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace {
-
-// The minimum amount of time ago that a site must have been visited in order to
-// be considered familiar.
-base::TimeDelta GetMinAgeOfInitialVisitForFamiliarity() {
-  return safe_browsing::
-      kMigrateToBlockV8OptimizerOnUnfamiliarSitesMinAgeOfInitialVisit.Get();
-}
 
 std::set<GURL>& GetFamiliarUrlsForTesting() {
   static base::NoDestructor<std::set<GURL>> familiar_urls_for_testing;
@@ -135,9 +129,7 @@ void SiteFamiliarityFetcher::Start(const GURL& url,
       site_engagement::SiteEngagementService::Get(profile_);
   has_engagement_score_higher_than_threshold_ =
       site_engagement_service->GetScore(fetch_url_) >=
-      safe_browsing::
-          kMigrateToBlockV8OptimizerOnUnfamiliarSitesMinSiteEngagementScore
-              .Get();
+      GetMinSiteEngagementScoreForFamiliarity(profile_);
 
   if (has_engagement_score_higher_than_threshold_) {
     CRSBLOG << "SiteFamiliarityFetcher::Start [URL]: " << fetch_url_
@@ -151,7 +143,7 @@ void SiteFamiliarityFetcher::Start(const GURL& url,
                                            ServiceAccessType::EXPLICIT_ACCESS);
   history_service->GetLastVisitToOrigin(
       url::Origin::Create(fetch_url_), base::Time(),
-      base::Time::Now() - GetMinAgeOfInitialVisitForFamiliarity(),
+      base::Time::Now() - GetMinAgeOfInitialVisitForFamiliarity(profile_),
       history::VisitQuery404sPolicy::kInclude404s,
       base::BindOnce(&SiteFamiliarityFetcher::OnFetchedHistory,
                      weak_factory_.GetWeakPtr()),
