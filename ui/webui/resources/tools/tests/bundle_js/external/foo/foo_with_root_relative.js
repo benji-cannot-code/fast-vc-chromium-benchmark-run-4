3ab08cc26b532a51903e879a53f10afdd544bfc7
Josiah Kiehl <kiehl@google.com>
2026-07-21 16:39:07 -0700

    WebUI: Block root-relative imports in external paths in Rollup
    This CL adds an assertion to `rollup_plugin.mjs` to block root-relative
    imports (starting with `/` but not `//`) inside files resolved from
    external paths. Such imports violate layering (shared code importing
    host-specific files like `/strings.m.js`).
    
    Add a unit test in `bundle_js_test.py` to verify this assertion throws
    an error.
    
    Bug: b:527483073
    Bypass-Check-License: Expected test output contains bundled 2023 license header from existing test file bar.js
    Change-Id: Ifc2d45915fb209b68de29720ffbe67351f05c1e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127136
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665890}

---
+++ ui/webui/resources/tools/tests/bundle_js/external/foo/foo_with_root_relative.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'some-fake-scheme://bar/bar.js';
+import '/strings.m.js';
+alert('Hello from external/foo/foo_with_root_relative.js');
