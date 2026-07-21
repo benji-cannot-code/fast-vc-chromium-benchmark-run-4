a0bea803ced968699e60d08cf7054a2c003640d8
Christoph Schwering <schwering@google.com>
2026-07-21 03:16:28 -0700

    [Autofill] Do not call renderer events from browser
    Bug: 525357662
    Change-Id: If2e41db693bcbf4027c998e92aa05496de9e2eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128140
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665343}

--- chrome/browser/keyboard_accessory/android/at_memory_accessory_controller_impl.cc
+++ chrome/browser/keyboard_accessory/android/at_memory_accessory_controller_impl.cc
@@ -13,13 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/browser/content_autofill_client.h"
 #include "components/autofill/content/browser/content_autofill_driver.h"
 #include "components/autofill/content/browser/renderer_forms_from_browser_form.h"
-#include "components/autofill/core/browser/foundations/browser_autofill_manager.h"
 #include "content/public/browser/web_contents.h"
 
 using ::autofill::AccessoryAction;
 using ::autofill::AccessorySheetData;
 using ::autofill::AccessorySheetField;
-using ::autofill::BrowserAutofillManager;
+using ::autofill::AutofillDriver;
+using ::autofill::AutofillSuggestionTriggerSource;
 using ::autofill::ContentAutofillDriver;
 using ::autofill::FieldGlobalId;
 using ::autofill::FindRenderFrameHostByToken;
@@ -79,15 +79,12 @@ void AtMemoryAccessoryControllerImpl::OnOptionSelected(
     return;
   }
 
-  ContentAutofillDriver* driver =
-      ContentAutofillDriver::GetForRenderFrameHost(rfh);
+  AutofillDriver* driver = ContentAutofillDriver::GetForRenderFrameHost(rfh);
   if (!driver) {
     return;
   }
-
-  BrowserAutofillManager* bam =
-      static_cast<BrowserAutofillManager*>(&driver->GetAutofillManager());
-  bam->TriggerAtMemorySuggestions(field_id);
+  driver->RendererShouldTriggerSuggestions(
+      field_id, AutofillSuggestionTriggerSource::kAtMemory);
 }
 
 void AtMemoryAccessoryControllerImpl::OnToggleChanged(
