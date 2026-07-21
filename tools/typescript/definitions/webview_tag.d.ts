95be0681583555259fceb1ee06bba26347f78d40
Kevin McNee <mcnee@chromium.org>
2026-07-21 09:42:00 -0700

    Add typescript definition for webview tag zoomchange event
    Use this instead of ad hoc types in glic code.
    
    Bug: b:531825599, b:500052160
    Change-Id: I12256b304d505fb238209bbdeda93ce1780fb13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116391
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665558}

--- tools/typescript/definitions/webview_tag.d.ts
+++ tools/typescript/definitions/webview_tag.d.ts
@@ -192,6 +192,11 @@ declare global {
         readonly newWidth: number;
       }
 
+      export interface ZoomChangeEvent extends Event {
+        readonly oldZoomFactor: number;
+        readonly newZoomFactor: number;
+      }
+
       export interface PermissionRequestEvent extends Event {
         readonly permission: PermissionType;
         request: MediaPermissionRequest|GeolocationPermissionRequest|
