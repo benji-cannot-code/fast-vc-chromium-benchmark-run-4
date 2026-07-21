3b133e2ac0b20b5b9ca92bb16d0440b4428fd8ca
Sylvain Defresne <sdefresne@chromium.org>
2026-07-21 02:39:08 -0700

    [ios] Add missing includes
    Those includes are transitively imported by fake_scene_state.h. Add
    explicit imports to allow refactoring fake_scene_state.h (including
    removing those imports).
    
    Bug: none
    Change-Id: Ic8f969feee124a7b4364346858a6513d1ef5c9d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8107262
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665328}

--- ios/chrome/browser/cobrowse/model/cobrowse_tab_helper_unittest.mm
+++ ios/chrome/browser/cobrowse/model/cobrowse_tab_helper_unittest.mm
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/coordinator/scene/state/tab_grid_state.h"
 #import "ios/chrome/browser/shared/coordinator/scene/test/fake_scene_state.h"
+#import "ios/chrome/browser/shared/model/browser/browser_provider.h"
+#import "ios/chrome/browser/shared/model/browser/browser_provider_interface.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
