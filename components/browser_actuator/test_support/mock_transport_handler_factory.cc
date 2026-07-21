95d8d6b35e6d228e8a6b6f5da36f5c4fc1cbf45a
anushamuley <anushamuley@google.com>
2026-07-20 21:04:25 -0700

    [browser-actuator] Implement TransportHandlerFactoryRegistryImpl
    Implements a new registry to manage the lifecycles of feature handler
    factories and the mapping of PayloadType to a registered
    TransportHandlerFactory instance.
    
    Bug: 532660606
    Change-Id: Ib230ea7f32012df01294bc49115c04a47e06cee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116342
    Auto-Submit: Anusha Muley <anushamuley@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665169}

---
+++ components/browser_actuator/test_support/mock_transport_handler_factory.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/browser_actuator/test_support/mock_transport_handler_factory.h"
+
+namespace browser_actuator {
+
+MockTransportHandlerFactory::MockTransportHandlerFactory(
+    const std::vector<PayloadType>& supported_types)
+    : supported_types_(supported_types) {}
+
+MockTransportHandlerFactory::~MockTransportHandlerFactory() = default;
+
+std::vector<PayloadType> MockTransportHandlerFactory::GetSupportedPayloadTypes()
+    const {
+  return supported_types_;
+}
+
+}  // namespace browser_actuator
