ce50afdd10e24110d28daf7712716402d9a93014
Sylvain Defresne <sdefresne@chromium.org>
2026-07-22 06:19:56 -0700

    [ios] Use ScopedKeyWindow in SafeModeCoordinatorTest
    Instead of mocking an UIWindow and UIWindowScene, use ScopedKeyWindow
    to use real objects for SafeModeCoordinatorTest.
    
    Bug: 519105565
    Change-Id: I5d96d3089670e25d17d3c237420dcf2832af4d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131364
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666220}

--- ios/chrome/browser/safe_mode/ui_bundled/safe_mode_view_controller_unittest.mm
+++ ios/chrome/browser/safe_mode/ui_bundled/safe_mode_view_controller_unittest.mm
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "testing/platform_test.h"
 #import "third_party/crashpad/crashpad/client/crash_report_database.h"
 #import "third_party/crashpad/crashpad/client/crashpad_client.h"
-#import "third_party/ocmock/gtest_support.h"
 
 namespace {
 
