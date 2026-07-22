af24040d7e9dadb46ab5c2b840604bc60940e0d8
hiroki oshima <hiroki.oshima@gmail.com>
2026-07-21 19:21:12 -0700

    remove PinRequestWidget::TestApi raw_ptr dangling marker
    Bug: 40212619
    Change-Id: I6adcd1df7d3fcd10247741aabbb1957ca01aa075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904619
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665980}

--- ash/login/ui/pin_request_widget.cc
+++ ash/login/ui/pin_request_widget.cc
@@ -31,7 +31,9 @@ base::RepeatingClosure& GetOnShownCallback() {
 }  // namespace
 
 PinRequestWidget::TestApi::TestApi(PinRequestWidget* widget)
-    : pin_request_widget_(widget) {}
+    : pin_request_widget_(widget) {
+  DCHECK(widget);
+}
 
 PinRequestWidget::TestApi::~TestApi() = default;
 
@@ -79,8 +81,7 @@ void PinRequestWidget::ClearInput() {
 
 void PinRequestWidget::Close(bool success) {
   DCHECK_EQ(instance_, this);
-  PinRequestWidget* instance = instance_;
-  instance_ = nullptr;
+  PinRequestWidget* instance = std::exchange(instance_, nullptr);
   std::move(on_pin_request_done_).Run(success);
   widget_->Close();
   delete instance;
