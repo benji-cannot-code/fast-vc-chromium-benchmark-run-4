c5a4c694cb775ad1e7b2372adb3cbbdcc9aea6e5
Gauthier Ambard <gambard@chromium.org>
2026-07-22 03:26:23 -0700

    [iOS] Reverse top condition in the omnibox
    For readability, positive conditions are easier to read.
    
    Bug: 537312208
    Change-Id: I31f48e542d9ccba184a0bff782f7667ed63a3163
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129081
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666149}

--- ios/chrome/browser/toolbar/ui/toolbar_view_controller.mm
+++ ios/chrome/browser/toolbar/ui/toolbar_view_controller.mm
@@ -591,12 +591,12 @@ const base::TimeDelta kProgressBarEndAnimationDuration =
     }
     _locationBarKeyboardCenterXConstraint.active = YES;
   } else {
-    if (!_topPosition) {
-      _locationBarTopConstraint.active = YES;
-      _locationBarBottomPaddingConstraint.active = NO;
-    } else {
+    if (_topPosition) {
       _locationBarTopConstraint.active = NO;
       _locationBarBottomPaddingConstraint.active = YES;
+    } else {
+      _locationBarTopConstraint.active = YES;
+      _locationBarBottomPaddingConstraint.active = NO;
     }
     [self.toolbarHeightDelegate secondaryToolbarRemovedFromKeyboard];
 
@@ -704,12 +704,12 @@ const base::TimeDelta kProgressBarEndAnimationDuration =
 
   _locationBarBackground.alpha = progress;
 
-  if (!_topPosition) {
-    _locationBarTopConstraint.constant =
-        [self locationBarTopPaddingForFullscreenProgress:progress];
-  } else {
+  if (_topPosition) {
     _locationBarBottomPaddingConstraint.constant =
         -[self locationBarBottomPaddingForFullscreenProgress:progress];
+  } else {
+    _locationBarTopConstraint.constant =
+        [self locationBarTopPaddingForFullscreenProgress:progress];
   }
 
   _bannerPromoBackgroundHeightConstraint.constant =
@@ -822,12 +822,12 @@ const base::TimeDelta kProgressBarEndAnimationDuration =
 - (void)showBannerPromoAnimationBlock {
   _bannerPromoBackgroundHeightConstraint.constant =
       [self bannerPromoBackgroundHeightForFullscreenProgress:1];
-  if (!_topPosition) {
-    _locationBarTopConstraint.constant =
-        [self locationBarTopPaddingForFullscreenProgress:_fullscreenProgress];
-  } else {
+  if (_topPosition) {
     _locationBarBottomPaddingConstraint.constant = -[self
         locationBarBottomPaddingForFullscreenProgress:_fullscreenProgress];
+  } else {
+    _locationBarTopConstraint.constant =
+        [self locationBarTopPaddingForFullscreenProgress:_fullscreenProgress];
   }
   [self.toolbarHeightDelegate toolbarsHeightChanged];
   [self.view.superview layoutIfNeeded];
@@ -850,12 +850,12 @@ const base::TimeDelta kProgressBarEndAnimationDuration =
 
   _bannerPromoVisible = NO;
 
-  if (!_topPosition) {
-    _locationBarTopConstraint.constant =
-        [self locationBarTopPaddingForFullscreenProgress:_fullscreenProgress];
-  } else {
+  if (_topPosition) {
     _locationBarBottomPaddingConstraint.constant = -[self
         locationBarBottomPaddingForFullscreenProgress:_fullscreenProgress];
+  } else {
+    _locationBarTopConstraint.constant =
+        [self locationBarTopPaddingForFullscreenProgress:_fullscreenProgress];
   }
 
   [self.toolbarHeightDelegate toolbarsHeightChanged];
@@ -1331,12 +1331,12 @@ const base::TimeDelta kProgressBarEndAnimationDuration =
       constraintEqualToAnchor:self.view.topAnchor
                      constant:kToolbarPadding];
 
-  if (!_topPosition) {
-    _locationBarTopConstraint.active = YES;
-    _locationBarBottomPaddingConstraint.active = NO;
-  } else {
+  if (_topPosition) {
     _locationBarTopConstraint.active = NO;
     _locationBarBottomPaddingConstraint.active = YES;
+  } else {
+    _locationBarTopConstraint.active = YES;
+    _locationBarBottomPaddingConstraint.active = NO;
   }
 
   [NSLayoutConstraint activateConstraints:@[
