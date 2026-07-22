220594215ccc40a95717dad0211a1d68a06f2c4c
Alan Ding <alanding@google.com>
2026-07-21 22:21:16 -0700

    on_device_model: Add explicit feature flag to enable GPU program cache
    Separates GPU program cache vs. weight cache enablement through
    different feature flags. Leave weight cache disabled for now due to
    storage increase concerns
    
    Bug: 512670810,512669586,500837296,528015401
    Change-Id: I2cb58391285eea1ab0a3a5f264903474ef98aaeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126515
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Alan Ding <alanding@chromium.org>
    Reviewed-by: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666021}

--- components/optimization_guide/core/model_execution/manifest_broker/manifest_solution_factory_unittest.cc
+++ components/optimization_guide/core/model_execution/manifest_broker/manifest_solution_factory_unittest.cc
@@ -148,7 +148,8 @@ TEST_F(ManifestSolutionFactoryTest, ExecuteTestGpuCacheFeature) {
   feature_list.InitWithFeaturesAndParameters(
       {{features::kOptimizationGuideOnDeviceModel,
         {{"on_device_model_topk", "1"}, {"on_device_model_temperature", "0"}}},
-       {on_device_model::features::kOnDeviceModelGpuCache, {}}},
+       {on_device_model::features::kOnDeviceModelGpuProgramCache, {}},
+       {on_device_model::features::kOnDeviceModelGpuWeightCache, {}}},
       {});
   ScenarioBuilder(fake_.component_state())
       .AddBaseModel(
@@ -184,6 +185,7 @@ TEST_F(ManifestSolutionFactoryTest, ExecuteTestGpuCacheFeature) {
 
   std::string expected_response =
       ("Fastest inference"
+       "Cache weight: 1015"
        "Encoder cache weight: 1016"
        "Adapter cache weight: 1017"
        "Shader cache data: 0xcafebabe"
