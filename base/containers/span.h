bc8530b5e8ef81a59c5f165bcc3cd9d3911331e4
Kalvin Lee <kdlee@chromium.org>
2026-07-22 09:28:23 -0700

    Checked Span: Don't check zero-sized spans
    This fix is speculative. The concrete justification is that this CL
    appears to prevent a crash in `media_pipeline_integration_fuzzer` ---
    a consistent repro disappears in my checkout when this CL is applied.
    
    In principle, `span::data()` is an uninteresting heuristic for
    determining whether or not we should bounds-check a span. `size()` alone
    suffices, and relying on `data()` at all was a mistake from the outset.
    
    The speculative justification for this CL is written out in full on the
    issue tracker; in short, a caller could attempt to construct a Checked
    Span from an end-of-allocation pointer, indistinguishable from the start
    of the next slot. If no such slot actually exists, PartitionAlloc could
    crash.
    
    Fixed: 535964021
    Validate-Test-Flakiness: skip
    Change-Id: I7213ce840f85d209077b335ea1f727031feb0583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8130658
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666392}

--- base/containers/span.h
+++ base/containers/span.h
@@ -531,10 +531,8 @@ class GSL_POINTER span {
       : span(unchecked, first, count) {
 #if PA_BUILDFLAG(CHECKED_SPAN)
     if (!std::is_constant_evaluated()) {
-      if (data() || size()) {
-        CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
-                                                    sizeof(element_type)));
-      }
+      CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
+                                                  sizeof(element_type)));
     }
 #endif  // PA_BUILDFLAG(CHECKED_SPAN)
   }
@@ -567,10 +565,8 @@ class GSL_POINTER span {
       : UNSAFE_BUFFERS(span(unchecked, first, last)) {
 #if PA_BUILDFLAG(CHECKED_SPAN)
     if (!std::is_constant_evaluated()) {
-      if (data() || size()) {
-        CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
-                                                    sizeof(element_type)));
-      }
+      CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
+                                                  sizeof(element_type)));
     }
 #endif  // PA_BUILDFLAG(CHECKED_SPAN)
   }
@@ -1080,10 +1076,8 @@ class GSL_POINTER span<ElementType, dynamic_extent, InternalPtrType> {
       : span(unchecked, first, count) {
 #if PA_BUILDFLAG(CHECKED_SPAN)
     if (!std::is_constant_evaluated()) {
-      if (data() || size()) {
-        CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
-                                                    sizeof(element_type)));
-      }
+      CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
+                                                  sizeof(element_type)));
     }
 #endif  // PA_BUILDFLAG(CHECKED_SPAN)
   }
@@ -1114,10 +1108,8 @@ class GSL_POINTER span<ElementType, dynamic_extent, InternalPtrType> {
       : UNSAFE_BUFFERS(span(unchecked, first, last)) {
 #if PA_BUILDFLAG(CHECKED_SPAN)
     if (!std::is_constant_evaluated()) {
-      if (data() || size()) {
-        CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
-                                                    sizeof(element_type)));
-      }
+      CHECK(!partition_alloc::IsExtentOutOfBounds(data(), size_bytes(),
+                                                  sizeof(element_type)));
     }
 #endif  // PA_BUILDFLAG(CHECKED_SPAN)
   }
