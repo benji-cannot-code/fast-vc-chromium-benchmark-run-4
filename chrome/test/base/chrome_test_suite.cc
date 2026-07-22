84304eced992690066c6b4d0eb7c5ad7733241b7
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-22 07:02:30 -0700

    [unsafe-buffers] Remove allow_unsafe_buffers from chrome/test/base
    This is a mechanical change transitioning the last few files in
    chrome/test/base from coarse, file-level pragmas to fine-grained,
    instruction-level UNSAFE_TODO() or UNSAFE_BUFFERS() macros, or refactoring
    to use safer structures.
    
    Bug: 351564777
    Bug: 501112808
    Doc: go/complete_unsafe_todo_migration
    Change-Id: I203a19a59cc48c0cfcead7e3f7e5951dca6b24f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131617
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666250}

--- chrome/test/base/chrome_test_suite.cc
+++ chrome/test/base/chrome_test_suite.cc
@@ -3,10 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
 
 #include "chrome/test/base/chrome_test_suite.h"
 
@@ -18,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
@@ -51,14 +49,11 @@ namespace {
 
 bool IsCrosPythonProcess() {
 #if BUILDFLAG(IS_CHROMEOS)
-  char buf[80];
-  int num_read = readlink(base::kProcSelfExe, buf, sizeof(buf) - 1);
-  if (num_read == -1) {
+  base::FilePath target;
+  if (!base::ReadSymbolicLink(base::FilePath(base::kProcSelfExe), &target)) {
     return false;
   }
-  buf[num_read] = 0;
-  const char kPythonPrefix[] = "/python";
-  return !strncmp(strrchr(buf, '/'), kPythonPrefix, sizeof(kPythonPrefix) - 1);
+  return target.BaseName().value().starts_with("python");
 #else
   return false;
 #endif  // BUILDFLAG(IS_CHROMEOS)
--- chrome/test/base/interactive_test_utils_win.cc
+++ chrome/test/base/interactive_test_utils_win.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/test/base/interactive_test_utils.h"
 
 #include <windows.h>
@@ -17,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/command_line.h"
+#include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
@@ -131,8 +127,10 @@ bool ShowAndFocusNativeWindow(gfx::NativeWindow window) {
         base::BindRepeating(
             [](HWND foreground_window,
                const base::RepeatingClosure& quit_closure, int* polls) {
-              if (!*polls-- || ::GetForegroundWindow() != foreground_window)
+              if (UNSAFE_TODO(!*polls--) ||
+                  ::GetForegroundWindow() != foreground_window) {
                 quit_closure.Run();
+              }
             },
             foreground_window, run_loop.QuitClosure(),
             base::Owned(std::make_unique<int>(
