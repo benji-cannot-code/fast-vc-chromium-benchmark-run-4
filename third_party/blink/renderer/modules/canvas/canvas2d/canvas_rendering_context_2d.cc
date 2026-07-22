bf0f2c158c04611814449258fd16e70084862e33
Colin Blundell <blundell@chromium.org>
2026-07-22 00:03:18 -0700

    [Blink] Remove Canvas2DResourceProvider::IsPrinting()
    Will facilitate continuing to move Flush() into the 2D contexts. Note
    that the provider's delegate is the canvas itself [1][2]. Note also that
    the canvas is guaranteed to exist on all paths into these methods.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc;l=1250;drc=1c742dacfb3e5adef0d4cd8f4853bd73a3c207a5
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=277;drc=1c742dacfb3e5adef0d4cd8f4853bd73a3c207a5
    
    Bug: 352263194
    Change-Id: I4d5ae5b450018ea098f5896ea255da4b003957e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131741
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666057}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -564,8 +564,7 @@ void CanvasRenderingContext2D::WillDraw(
 
 void CanvasRenderingContext2D::FlushIfRecordingLimitExceeded() {
   if (shared_image_provider_) {
-    if (shared_image_provider_->IsPrinting() &&
-        shared_image_provider_->clear_frame()) {
+    if (Host()->IsPrinting() && shared_image_provider_->clear_frame()) {
       return;
     }
     const MemoryManagedPaintRecorder* recorder = Recorder();
