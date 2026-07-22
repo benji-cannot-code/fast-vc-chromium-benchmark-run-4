9db13cd7d68b28bd90474cdd9b804fa445abe811
Dominic Battre <battre@chromium.org>
2026-07-22 03:25:10 -0700

    Introduce a map of FieldGlobalIds to PageTargets
    In a followup CL we will add the ability to report filled data back to
    the model. This CL provides the mapping of FieldGlobalIds to PageTargets
    so that the filled fields can be reportedy by the PageTargets used in
    the attempt_form_filling call. These values are otherwise lost early in
    the process.
    
    Bug: b:524062127
    RFC=go/aix-rfc-29
    TAG=agy
    CONV=f109b895-6c3b-48c5-b9f7-a3449f9425a3
    
    Change-Id: I7ec034160db0c3e32d121bd9d071cdd36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8120130
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666147}

--- components/autofill/core/browser/actor/actor_form_filling_service_unittest.cc
+++ components/autofill/core/browser/actor/actor_form_filling_service_unittest.cc
@@ -779,7 +779,7 @@ TEST_F(ActorFormFillingServiceTest, FillAfterFetchingServerCard) {
   FillSuggestionsFuture fill_future;
   service().FillSuggestions(
       client(), {ActorFormFillingSelection(requests[0].suggestions[0].id)},
-      fill_future.GetCallback());
+      /*trigger_field_map=*/{}, fill_future.GetCallback());
 
   ASSERT_GT(ActorFillingObserver::GetMaximumTimeout(), base::Seconds(1));
   task_environment()->FastForwardBy(ActorFillingObserver::GetMaximumTimeout() -
@@ -830,7 +830,7 @@ TEST_F(ActorFormFillingServiceTest, TimeoutWithFetching) {
   FillSuggestionsFuture fill_future;
   service().FillSuggestions(
       client(), {ActorFormFillingSelection(requests[0].suggestions[0].id)},
-      fill_future.GetCallback());
+      /*trigger_field_map=*/{}, fill_future.GetCallback());
 
   ASSERT_GT(ActorFillingObserver::GetMaximumTimeout(), base::Seconds(2));
   task_environment()->FastForwardBy(base::Seconds(1));
