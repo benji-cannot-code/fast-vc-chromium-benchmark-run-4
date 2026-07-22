9df8e813e3827fe6874fb69466aade77c96fb5ac
Nina Satragno <nsatragno@google.com>
2026-07-22 08:13:31 -0700

    [cdp] Support CMTG in WebAuthn.setCredentialProperties
    Allow setting CMTG properties on credentials via the
    WebAuthn.setCredentialProperties CDP command.
    
    This is intended to be used by both devtools and webdriver.
    
    See https://github.com/w3c/webauthn/pull/2445
    
    TAG=agy
    CONV=00274877-c5d3-46c3-a466-9346b48eefc4
    
    Bug: 485888879
    Change-Id: I5dcecf1e6e2a760f8130fdddb5c997e2a575bc5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131762
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666311}

--- third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-set-credential-properties-errors.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/webauthn/webauthn-set-credential-properties-errors.js
@@ -52,5 +52,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     authenticatorId, credentialId,
   }));
 
+  testRunner.log('Set CMTG properties on non-CMTG authenticator:');
+  testRunner.log(await dp.WebAuthn.setCredentialProperties({
+    authenticatorId,
+    credentialId,
+    activeCmtgKeyIndex: 0,
+  }));
+
+  // Create a CMTG-enabled authenticator and add a credential (without
+  // pre-configured CMTG keys).
+  const cmtgAuthId = (await dp.WebAuthn.addVirtualAuthenticator({
+                       options: {
+                         protocol: 'ctap2',
+                         transport: 'usb',
+                         hasResidentKey: true,
+                         hasUserVerification: true,
+                         isUserVerified: true,
+                         hasCmtgKey: true,
+                       }
+                     })).result.authenticatorId;
+  const cmtgCredId = btoa('cmtg-cred');
+  await dp.WebAuthn.addCredential({
+    authenticatorId: cmtgAuthId,
+    credential: {
+      credentialId: cmtgCredId,
+      rpId: 'devtools.test',
+      privateKey: await session.evaluateAsync('generateBase64Key()'),
+      signCount: 0,
+      isResidentCredential: true,
+      userHandle,
+    }
+  });
+
+  // Test setting activeCmtgKeyIndex out of bounds (index 0 is out of bounds
+  // because cmtgKeys is empty).
+  testRunner.log('Set activeCmtgKeyIndex out of bounds on CMTG authenticator:');
+  testRunner.log(await dp.WebAuthn.setCredentialProperties({
+    authenticatorId: cmtgAuthId,
+    credentialId: cmtgCredId,
+    activeCmtgKeyIndex: 0,
+  }));
+
   testRunner.completeTest();
 })
