5e1bc0c9e061111bc3cef5d585974bfa1771800d
Karol Sygiet <sygiet@google.com>
2026-07-21 22:09:05 -0700

    [AtMemory] Display offline suggestion before starting the query
    If the user happens to be offline why typing the query, there's now the
    offline indication displayed during typing. Previously user saw it only
    when sending the full query.
    
    Implementation: screen/72AS8JTC5r4k6id
    
    Bug: 537220971
    Change-Id: Ie1155b300d9666e4c8c6b22d8544bcf2e7805c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128160
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666017}

--- components/autofill/core/browser/at_memory/at_memory_manager.cc
+++ components/autofill/core/browser/at_memory/at_memory_manager.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/unique_ids.h"
 #include "components/personal_context/core/personal_context_types.h"
 #include "components/strings/grit/components_strings.h"
+#include "net/base/network_change_notifier.h"
 #include "third_party/abseil-cpp/absl/functional/overload.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -485,12 +486,12 @@ Suggestion CreateNoDataSuggestion() {
 
 // Creates a suggestion to display when AtMemory search fails to connect to the
 // server.
-Suggestion CreateNoConnectionSuggestion() {
-  Suggestion suggestion(
-      l10n_util::GetStringUTF16(IDS_AUTOFILL_AT_MEMORY_NO_CONNECTION),
-      SuggestionType::kAtMemoryNoConnection);
-  suggestion.acceptability =
-      Suggestion::Acceptability::kUnacceptable;
+Suggestion CreateNoConnectionSuggestion(std::u16string query) {
+  Suggestion suggestion(std::move(query),
+                        SuggestionType::kAtMemoryNoConnection);
+  suggestion.labels = {{Suggestion::Text(
+      l10n_util::GetStringUTF16(IDS_AUTOFILL_AT_MEMORY_NO_CONNECTION))}};
+  suggestion.acceptability = Suggestion::Acceptability::kUnacceptable;
   suggestion.filtration_policy = Suggestion::FiltrationPolicy::kStatic;
   suggestion.icon = Suggestion::Icon::kSadTab;
   return suggestion;
@@ -611,7 +612,11 @@ bool AtMemoryManager::OnFilterChanged(const std::u16string& filter) {
     return true;
   }
   std::vector<Suggestion> suggestions;
-  suggestions.push_back(CreateSearchAffordanceSuggestion(filter));
+  if (net::NetworkChangeNotifier::IsOffline()) {
+    suggestions.push_back(CreateNoConnectionSuggestion(filter));
+  } else {
+    suggestions.push_back(CreateSearchAffordanceSuggestion(filter));
+  }
 
   if (!owner_->client().ShouldShowPersonalContextAtMemoryNotice()) {
     suggestions.emplace_back(SuggestionType::kSeparator);
@@ -1052,7 +1057,7 @@ void AtMemoryManager::OnSearchResultsReceived(
     case accessibility_annotator::MemorySearchStatus::kPartialResponseSuccess:
       break;
     case accessibility_annotator::MemorySearchStatus::kNoConnectionFailure:
-      suggestions.push_back(CreateNoConnectionSuggestion());
+      suggestions.push_back(CreateNoConnectionSuggestion(query));
       break;
     case accessibility_annotator::MemorySearchStatus::kInferenceFailure:
     case accessibility_annotator::MemorySearchStatus::kInternalFailure:
