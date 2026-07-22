f494177b7adcb53b633e414f1aca481e31d520fa
Yerassyl Kutylbek <kutylbeky@google.com>
2026-07-22 10:03:39 -0700

    Register private-ai-internals host in DevUiLoaderThrottle
    Fixed: b:537724326
    Change-Id: I82b4fce42b34f3f0130f568ecc86bae56ab3770f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127500
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Yerassyl Kutylbek <kutylbeky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666429}

--- chrome/browser/dev_ui/android/dev_ui_loader_throttle.cc
+++ chrome/browser/dev_ui/android/dev_ui_loader_throttle.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/commerce/core/commerce_constants.h"
 #include "components/history_clusters/history_clusters_internals/webui/url_constants.h"
 #include "components/optimization_guide/optimization_guide_internals/webui/url_constants.h"
+#include "components/private_ai/private_ai_internals/webui/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/common/buildflags.h"
@@ -89,7 +90,8 @@ bool IsWebUiHostInDevUiDfm(const std::string& host) {
          host == history_clusters_internals::
                      kChromeUIHistoryClustersInternalsHost ||
          host == optimization_guide_internals::
-                     kChromeUIOptimizationGuideInternalsHost;
+                     kChromeUIOptimizationGuideInternalsHost ||
+         host == private_ai_internals::kChromeUIPrivateAiInternalsHost;
 }
 
 }  // namespace
