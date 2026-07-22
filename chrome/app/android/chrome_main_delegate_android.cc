b56155dc4077fc42c7bcd0fa83d1aa472abceed8
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-22 10:49:10 -0700

    Cleanup ChromeMainDelegateAndroid::SecureDataDirectory
    Android min SDK version is now > 21, so directory permission setup
    is handled by PathUtils.java. Remove
    ChromeMainDelegateAndroid::SecureDataDirectory, its invocation,
    associated unit test, and unused base header includes.
    
    Bug: 41382891
    Change-Id: I0119ab4ebfb28f81e2b1f3fdf71a24c86a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127270
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666466}

--- chrome/app/android/chrome_main_delegate_android.cc
+++ chrome/app/android/chrome_main_delegate_android.cc
@@ -12,12 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/pre_freeze_background_memory_trimmer.h"
 #include "base/android/sys_utils.h"
-#include "base/base_paths_android.h"
 #include "base/feature_list.h"
-#include "base/files/file_path.h"
-#include "base/files/file_util.h"
 #include "base/logging.h"
-#include "base/path_service.h"
 #include "base/sampling_heap_profiler/poisson_allocation_sampler.h"
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/android/chrome_startup_flags.h"
@@ -88,23 +84,6 @@ void ChromeMainDelegateAndroid::PreSandboxStartup() {
   }
 }
 
-void ChromeMainDelegateAndroid::SecureDataDirectory() {
-  // By default, Android creates the directory accessible by others.
-  // We'd like to tighten security and make it accessible only by
-  // the browser process.
-  // TODO(crbug.com/41382891): Remove this once minsdk >= 21,
-  // at which point this will be handled by PathUtils.java.
-  base::FilePath data_path;
-  bool ok = base::PathService::Get(base::DIR_ANDROID_APP_DATA, &data_path);
-  if (ok) {
-    ok = base::SetPosixFilePermissions(data_path,
-                                       base::FILE_PERMISSION_USER_MASK);
-  }
-  if (!ok) {
-    LOG(ERROR) << "Failed to set data directory permissions";
-  }
-}
-
 std::variant<int, content::MainFunctionParams>
 ChromeMainDelegateAndroid::RunProcess(
     const std::string& process_type,
@@ -113,8 +92,6 @@ ChromeMainDelegateAndroid::RunProcess(
   // Defer to the default main method outside the browser process.
   if (!process_type.empty())
     return std::move(main_function_params);
-
-  SecureDataDirectory();
 
   // Because the browser process can be started asynchronously as a series of
   // UI thread tasks a second request to start it can come in while the
--- chrome/app/android/chrome_main_delegate_android.h
+++ chrome/app/android/chrome_main_delegate_android.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Android override of ChromeMainDelegate
 class ChromeMainDelegateAndroid : public ChromeMainDelegate {
  public:
-  static void SecureDataDirectory();  // visible for testing
-
   ChromeMainDelegateAndroid();
 
   ChromeMainDelegateAndroid(const ChromeMainDelegateAndroid&) = delete;
