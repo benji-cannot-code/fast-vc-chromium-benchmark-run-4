913112b1559ac342bad85eaf73471e18e4b500b9
Alex Turner <alexmt@chromium.org>
2026-07-22 07:50:16 -0700

    Remove aggregation_service from allowed non-Blink usage
    This was previously used in the Private Aggregation integration with
    Protected Audience and Shared Storage, but those have been removed. So,
    the entries are no longer required.
    
    Bug: 531746235
    Change-Id: I1e8d6bd12e96cbb89cb6616a801a87e8eadc703c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128884
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666286}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -2979,15 +2979,6 @@ _CONFIG = [
     },
     {
         'paths': [
-            'third_party/blink/renderer/modules/ad_auction/',
-            'third_party/blink/renderer/modules/shared_storage/',
-        ],
-        'allowed': [
-            'aggregation_service::.+',
-        ]
-    },
-    {
-        'paths': [
             'third_party/blink/renderer/core/scheduler/',
             'third_party/blink/renderer/modules/shared_storage/',
         ],
