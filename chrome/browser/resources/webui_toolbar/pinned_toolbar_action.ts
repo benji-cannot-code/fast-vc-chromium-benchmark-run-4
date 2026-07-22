2c902a1895dde52dccb84d4b2e06c58803634b45
Mingyu Lei <leimy@chromium.org>
2026-07-21 18:45:52 -0700

    Webium: Migrate webui-toolbar icons to CrLazyIconset and roll DEPS
    This CL migrates the `webui-toolbar` public icons from `icons.html.ts`
    to a new `icons.ts` file using `CrLazyIconset`, improving WebUI startup
    performance by avoiding eager parsing of the entire iconset during
    initialization.
    
    It also rolls the `src/chrome/browser/resources/webui_toolbar/internal`
    submodule pointer in DEPS to include the corresponding migration changes
    for internal icons.
    
    See the benchmark and profiling results here:
    https://docs.google.com/document/d/17HWVmeM9Yo8VjHwKNnxq_0n9CB8un3xIrDlz2ZzdoK4/edit?resourcekey=0-_VsI0nIdnkAin92rSY1dcQ&tab=t.0
    
    Change-Id: If7d136ca8ca97c6300aa9450e5daea29679ab2e1
    Bug: 524526352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8084767
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665968}

--- chrome/browser/resources/webui_toolbar/pinned_toolbar_action.ts
+++ chrome/browser/resources/webui_toolbar/pinned_toolbar_action.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // <if expr="_google_chrome">
-import './internal/icons.html.js';
+import './internal/icons.js';
 // </if>
 
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
