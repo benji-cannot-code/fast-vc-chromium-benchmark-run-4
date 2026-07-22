dabb2fb1a2645c104bad97bafe88c7f1efb79616
Gauthier Ambard <gambard@chromium.org>
2026-07-22 09:44:50 -0700

    [iOS] Replace Symbol helper with new helpers
    This CL changes the symbol helpers, replacing the dual definitions
    Custom/Settings by a version that has only a single helper.
    
    This has been mostly vibe-coded.
    
    This CL was uploaded by git cl split.
    
    R=bmcclure@google.com
    
    Bug: 537673111
    Change-Id: I9773425a8a89f203cccc2b8e031d2e1ad9ac95ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131664
    Reviewed-by: Briana McClure <bmcclure@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666413}

--- ios/chrome/browser/first_run/best_features/coordinator/best_features_screen_detail_coordinator.mm
+++ ios/chrome/browser/first_run/best_features/coordinator/best_features_screen_detail_coordinator.mm
@@ -100,8 +100,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     _baseNavigationController.navigationBarHidden = NO;
     UIBarButtonItem* backButton = [[UIBarButtonItem alloc]
-        initWithImage:DefaultSymbolWithPointSize(kChevronBackwardSymbol,
-                                                 kSymbolActionPointSize)
+        initWithImage:SymbolWithPointSize(SymbolChevronBackward,
+                                          kSymbolActionPointSize)
                 style:UIBarButtonItemStylePlain
                target:self
                action:@selector(dismiss)];
