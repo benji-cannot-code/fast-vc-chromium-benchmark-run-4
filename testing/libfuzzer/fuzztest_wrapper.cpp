54c6f061429b2da252713be6a6f87fddca8aa86b
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-07-22 10:49:42 -0700

    fuzztest: Propagate SIGTERM from wrapper to underlying fuzzer
    In ClusterFuzz, FuzzTests are not executed directly. We build a wrapper
    that calls into the main fuzzer binary. This wrapper doesn't have a
    handler for SIGTERM, so when ClusterFuzz sends SIGTERM, it only kills
    the wrapper, and the fuzzer keeps running, often deleting and adding new
    files.
    
    This causes issues with the corpus merging step that happens right after
    i.e. it deletes files that libfuzzer expect to exist. This triggers a
    bug in libfuzzer that causes it to enter an infinite loop.
    
    Bug: 529865766
    Change-Id: I29b5b9e4b64b6b7a7fc92126d85d0d34e6ceafc1
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,linux-libfuzzer-high-end-asan-rel,chromeos-libfuzzer-asan-rel,mac-libfuzzer-asan-rel,win-libfuzzer-asan-rel,linux-centipede-asan-rel,win_chromium_compile_dbg_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131498
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666467}

--- testing/libfuzzer/fuzztest_wrapper.cpp
+++ testing/libfuzzer/fuzztest_wrapper.cpp
@@ -23,8 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/libfuzzer/buildflags.h"
 
 #if BUILDFLAG(IS_POSIX)
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
+#include "base/posix/eintr_wrapper.h"
 #include "base/posix/safe_strerror.h"
 #endif
 
@@ -67,9 +71,42 @@ void HandleReplayMode(auto& args) {
 
 #endif  // BUILDFLAG(USE_CENTIPEDE)
 
+base::ProcessId g_child_pid = 0;
+
+// Setup signal handlers so we can propagate signals, i.e. SIGTERM and SIGINT,
+// to the underlying fuzzer.
+void SetupSignalHandlers() {
+#if BUILDFLAG(IS_POSIX)
+  auto* signal_handler = +[](int signum) {
+    if (g_child_pid > 0) {
+      kill(g_child_pid, signum);
+      int status;
+      HANDLE_EINTR(waitpid(g_child_pid, &status, /*options=*/0));
+    }
+
+    struct sigaction action;
+    action.sa_handler = SIG_DFL;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(signum, &action, /*oldact=*/nullptr);
+    kill(getpid(), signum);
+  };
+
+  struct sigaction action;
+  action.sa_handler = signal_handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+
+  sigaction(SIGTERM, &action, /*oldact=*/nullptr);
+  sigaction(SIGINT, &action, /*oldact=*/nullptr);
+#endif
+}
+
 }  // namespace
 
 int main(int argc, const char* const* argv) {
+  SetupSignalHandlers();
   base::CommandLine::Init(argc, argv);
   base::FilePath fuzzer_path;
   if (!base::PathService::Get(base::DIR_EXE, &fuzzer_path)) {
@@ -132,6 +169,7 @@ int main(int argc, const char* const* argv) {
            "errors above.\n";
     return kErrorExitCode;
   }
+  g_child_pid = p.Pid();
   int exit_code;
   if (!p.WaitForExit(&exit_code)) {
     std::cerr
