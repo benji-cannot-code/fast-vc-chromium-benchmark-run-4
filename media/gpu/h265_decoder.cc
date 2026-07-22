1b1ed7900cae9dc4e0b9f39a7c2ef73ab10ad76d
Dale Curtis <dalecurtis@chromium.org>
2026-07-21 17:12:10 -0700

    h265: Finish previous frame before updating alpha-layer SPS/PPS
    When an alpha-layer (non-zero nuh_layer_id) SPS or PPS NAL unit is
    encountered during H.265 decoding, FinishPrevFrameIfPresent() was not
    being called prior to parsing the parameter set and forwarding it to the
    accelerator. If an alpha-layer SPS or PPS arrived mid-picture, updating
    parameter sets before completing the current picture could cause state
    collisions or unvalidated parameter set updates in hardware accelerators
    (such as VideoToolbox on macOS).
    
    This change adds CHECK_ACCELERATOR_RESULT(FinishPrevFrameIfPresent()) to
    the case H265NALU::SPS_NUT and case H265NALU::PPS_NUT branches within
    the non-zero nuh_layer_id handling block in H265Decoder::Decode().
    
    Also adds a unit test coverage in H265DecoderTest verifying that
    FinishPrevFrameIfPresent() completes the pending picture before
    processing the alpha-layer SPS/PPS.
    
    TAG=agy
    CONV=4eee7864-0106-476f-b647-78eba7f8c730
    
    Bug: 536444242
    Change-Id: I8d0eeb6b7221cd8ae4636c6e79782288d198131b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128206
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>
    Commit-Queue: Qiu, Jianlin <jianlin.qiu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1665919}

--- media/gpu/h265_decoder.cc
+++ media/gpu/h265_decoder.cc
@@ -312,6 +312,7 @@ H265Decoder::DecodeResult H265Decoder::Decode() {
             break;
           }
           case H265NALU::SPS_NUT: {
+            CHECK_ACCELERATOR_RESULT(FinishPrevFrameIfPresent());
             int sps_id;
             par_res = parser_.ParseSPS(&sps_id);
             if (par_res != H265Parser::kOk) {
@@ -321,6 +322,7 @@ H265Decoder::DecodeResult H265Decoder::Decode() {
             break;
           }
           case H265NALU::PPS_NUT: {
+            CHECK_ACCELERATOR_RESULT(FinishPrevFrameIfPresent());
             int pps_id;
             par_res = parser_.ParsePPS(*curr_nalu_, &pps_id);
             if (par_res != H265Parser::kOk) {
