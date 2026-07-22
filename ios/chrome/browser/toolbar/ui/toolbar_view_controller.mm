dd378d20dbe58b5f386d24adccfb529b7fb22047
Eric Ekey <ericekey@google.com>
2026-07-22 13:42:15 -0700

    [iOS][AppBar] Fix Toolbar Buttons Appearing in Collapsed Toolbar
    This change fixes a bug where full-sized toolbar buttons (Reload/Stop,
    Share) could appear in the collapsed toolbar. This would occur if the
    visibility state of a toolbar button changed while the toolbar was
    undergoing a fullscreen transition, usually by entering fullscreen
    during an in-progress page load. This change fixes the issue by ensuring
    toolbar buttons are synchronized with fullscreen progress when their
    visibility is changed by the ToolbarViewController. Management of alpha
    and transform for toolbar buttons is centralized in the
    ToolbarViewController and removed from the ToolbarButton class.
    
    Fixed: 537564867
    Change-Id: I0df2272869325172ab46a6e438d10a06cb6e41d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8134399
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666588}

--- ios/chrome/browser/toolbar/ui/toolbar_view_controller.mm
+++ ios/chrome/browser/toolbar/ui/toolbar_view_controller.mm
@@ -526,6 +526,9 @@ constexpr CGFloat kGlassShadowOpacity = 0.09;
 }
 
 - (void)setShareEnabled:(BOOL)enabled {
+  if (enabled == _shareButton.isEnabled) {
+    return;
+  }
   _shareButton.enabled = enabled;
 }
 
@@ -560,6 +563,10 @@ constexpr CGFloat kGlassShadowOpacity = 0.09;
     _isLoading = !mustHideLoadingUI;
     _reloadButton.forceHidden = !mustHideLoadingUI;
     _stopButton.forceHidden = mustHideLoadingUI;
+    if (self.isViewLoaded) {
+      [self updateButtons:@[ _reloadButton, _stopButton ]
+          forFullscreenProgress:_fullscreenProgress];
+    }
 
     if (loadingStateChanged && isLoading) {
       [_progressBar setProgress:0.0 animated:NO];
@@ -1042,6 +1049,7 @@ constexpr CGFloat kGlassShadowOpacity = 0.09;
   for (UIView* button in buttons) {
     if (button.hidden) {
       button.alpha = 0;
+      button.transform = CGAffineTransformMakeScale(0.01, 0.01);
       continue;
     }
     if (progress > kFullscreenProgressThreshold) {
@@ -1783,6 +1791,10 @@ constexpr CGFloat kGlassShadowOpacity = 0.09;
   _leadingStackView.hidden = !_visible;
   _locationBarContainer.hidden = !_visible;
   _trailingStackView.hidden = !_visible;
+  [self updateButtons:_leadingStackView.arrangedSubviews
+      forFullscreenProgress:_fullscreenProgress];
+  [self updateButtons:_trailingStackView.arrangedSubviews
+      forFullscreenProgress:_fullscreenProgress];
   _progressBarContainer.hidden = !_visible || CanShowTabStrip(self);
   if (IsGlassToolbarEnabled()) {
     _glassBackgroundContainer.hidden = !_visible;
