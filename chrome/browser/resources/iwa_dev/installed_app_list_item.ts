4bbbb96a8c851b3990d27d347f9f0b4720fa4f3f
Paulina Gacek <paulinagacek@google.com>
2026-07-22 06:44:02 -0700

    iwa: Implement uninstallation of Isolated Web Apps
    This CL adds the ability to uninstall IWAs from chrome://iwa-dev page.
    
    Changes include:
    - Adding the `UninstallApp` method to the `PageHandler` Mojo
      interface.
    - Implementing the backend uninstallation logic in `IwaDevPageHandler`.
    - Adding an "Uninstall" button to `<installed-app-list-item>` that
      dispatches an uninstall request.
    - Updating the main `<iwa-dev-app>` to orchestrate showing the dialog
      and calling the Mojo backend.
    - Adding WebUI tests in `app_test.ts` and C++ browser tests in
      `iwa_dev_page_handler_browsertest.cc`.
    
    R=​chrome-ipc-reviews@google.com
    
    Bug: b:513511510
    Test: testing/xvfb.py ./out/current_link/browser_tests --gtest_filter="IwaDev*"
    Change-Id: I2b6e8cccdc6b18242f87789df64b7868c08ff4c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116646
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666240}

--- chrome/browser/resources/iwa_dev/installed_app_list_item.ts
+++ chrome/browser/resources/iwa_dev/installed_app_list_item.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '//resources/cr_elements/cr_button/cr_button.js';
+
 import {assertNotReached} from '//resources/js/assert.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
@@ -87,6 +89,10 @@ export class InstalledAppListItemElement extends CrLitElement {
     label: '',
     description: '',
   };
+
+  protected onUninstallClick() {
+    this.fire('request-uninstall', {app: this.app});
+  }
 
   override willUpdate(changedProperties: PropertyValues<this>) {
     super.willUpdate(changedProperties);
--- chrome/browser/ui/webui/iwa_dev/iwa_dev_page_handler_browsertest.cc
+++ chrome/browser/ui/webui/iwa_dev/iwa_dev_page_handler_browsertest.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/iwa_dev/iwa_dev_page_handler.h"
 
+#include <string>
+#include <string_view>
+#include <vector>
+
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/ui/browser.h"
@@ -25,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webapps/isolated_web_apps/types/update_channel.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "extensions/browser/extension_dialog_auto_confirm.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -132,7 +137,6 @@ class IwaDevHandlerBrowserTest
   IwaDevPageHandler* GetHandler() {
     content::WebContents* web_contents =
         browser()->tab_strip_model()->GetActiveWebContents();
-
     IwaDevUI* controller =
         static_cast<IwaDevUI*>(web_contents->GetWebUI()->GetController());
     CHECK(controller);
@@ -146,6 +150,14 @@ class IwaDevHandlerBrowserTest
         future;
     GetHandler()->GetInstalledAppsInfo(future.GetCallback());
     return future.Take();
+  }
+
+  bool UninstallApp(const std::string& app_id) {
+    extensions::ScopedTestDialogAutoConfirm auto_accept(
+        extensions::ScopedTestDialogAutoConfirm::ACCEPT);
+    base::test::TestFuture<bool> future;
+    GetHandler()->UninstallApp(app_id, future.GetCallback());
+    return future.Get();
   }
 
   web_app::IsolatedWebAppUrlInfo InstallBundle(std::string_view name,
@@ -293,3 +305,39 @@ IN_PROC_BROWSER_TEST_F(IwaDevHandlerObserverBrowserTest, IgnoresNormalWebApps) {
   mock_page().FlushForTesting();
   EXPECT_FALSE(mock_page().uninstalled_future().IsReady());
 }
+
+struct IwaDevAppTestCase {
+  const char* name;
+  web_app::IsolatedWebAppUrlInfo (IwaDevHandlerBrowserTest::*installer)();
+};
+
+class IwaDevHandlerAppTypeBrowserTest
+    : public IwaDevHandlerBrowserTest,
+      public ::testing::WithParamInterface<IwaDevAppTestCase> {};
+
+IN_PROC_BROWSER_TEST_P(IwaDevHandlerAppTypeBrowserTest, UninstallApp) {
+  web_app::IsolatedWebAppUrlInfo app = (this->*GetParam().installer)();
+
+  auto apps_before = GetInstalledAppsInfo();
+  ASSERT_EQ(apps_before.size(), 1u);
+  EXPECT_EQ(apps_before[0]->app_id, app.app_id());
+
+  EXPECT_TRUE(UninstallApp(app.app_id()));
+
+  auto apps_after = GetInstalledAppsInfo();
+  EXPECT_TRUE(apps_after.empty());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    All,
+    IwaDevHandlerAppTypeBrowserTest,
+    ::testing::Values(
+        IwaDevAppTestCase{"ProxyApp",
+                          &IwaDevHandlerBrowserTest::InstallProxyApp},
+        IwaDevAppTestCase{"LocalBundleApp",
+                          &IwaDevHandlerBrowserTest::InstallBundleApp},
+        IwaDevAppTestCase{"ManifestApp",
+                          &IwaDevHandlerBrowserTest::InstallUpdateManifestApp}),
+    [](const testing::TestParamInfo<IwaDevAppTestCase>& info) {
+      return info.param.name;
+    });
