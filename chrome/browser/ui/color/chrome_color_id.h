e54e4932de399c32794d78288cc5949c883dfd87
anthie@google.com <anthie@google.com>
2026-07-22 06:59:17 -0700

    Avatar badge - data types and color mixers [Part 4a]
    This CL introduces the AvatarBadgePathOp enum data types, public
    decoration specs, and color mixer definitions for the avatar badge.
    
    Bug: 532163925, 530888562
    Change-Id: I0645e152707cc122cc797d7b4c3e875f3b6c20b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116613
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666247}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -61,6 +61,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorAvatarFillForContrast) \
   E_CPONLY(kColorAvatarRingGradientStart) \
   E_CPONLY(kColorAvatarRingGradientEnd) \
+  E_CPONLY(kColorAvatarBadgeWaveBase0) \
+  E_CPONLY(kColorAvatarBadgeWaveBase1) \
+  E_CPONLY(kColorAvatarBadgeWaveBase2) \
+  E_CPONLY(kColorAvatarBadgeWaveBase3) \
+  E_CPONLY(kColorAvatarBadgeWaveOverlay0) \
+  E_CPONLY(kColorAvatarBadgeWaveOverlay1) \
+  E_CPONLY(kColorAvatarBadgeWaveOverlay2) \
+  E_CPONLY(kColorAvatarBadgeWaveOverlay3) \
+  E_CPONLY(kColorAvatarBadgeShadow) \
+  E_CPONLY(kColorAvatarBadgeBackground) \
   /* Bookmark Manager page colors. */ \
   E_CPONLY(kColorBookmarkManagerItemBackgroundSelected) \
   E_CPONLY(kColorBookmarkManagerItemOutline) \
---
+++ chrome/browser/ui/profiles/avatar_badge_types.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PROFILES_AVATAR_BADGE_TYPES_H_
+#define CHROME_BROWSER_UI_PROFILES_AVATAR_BADGE_TYPES_H_
+
+// Operation types for SVG wave path rendering of the profile avatar badge.
+enum class AvatarBadgePathOp {
+  kMoveTo = 0,
+  kLineTo = 1,
+  kCubicTo = 2,
+  kClose = 3,
+};
+
+#endif  // CHROME_BROWSER_UI_PROFILES_AVATAR_BADGE_TYPES_H_
