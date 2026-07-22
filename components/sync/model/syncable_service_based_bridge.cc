2b3155253a2280d3170bae708989900e461d66f1
Marc Treib <treib@chromium.org>
2026-07-22 09:17:17 -0700

    SyncableServiceBasedBridge: Make GetStorageKey() NOTREACHED()
    This method is not supported, per SupportsGetStorageKey() returning
    false.
    
    And notably, SyncableServiceBasedBridge actually uses the client tag
    *hash* as the storage key, so the current implementation was actually
    wrong. It was (likely accidentally) implemented in
    https://crrev.com/c/6681787.
    
    Bug: 40726283
    Change-Id: Ie475e32276ed2fac6d1146f6593da798c5d1d0af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8134897
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666379}

--- components/sync/model/syncable_service_based_bridge.cc
+++ components/sync/model/syncable_service_based_bridge.cc
@@ -326,7 +326,8 @@ std::string SyncableServiceBasedBridge::GetClientTag(
 
 std::string SyncableServiceBasedBridge::GetStorageKey(
     const EntityData& entity_data) const {
-  return syncable_service_->GetClientTag(entity_data);
+  // Not supported as per SupportsGetStorageKey().
+  NOTREACHED();
 }
 
 sync_pb::EntitySpecifics
