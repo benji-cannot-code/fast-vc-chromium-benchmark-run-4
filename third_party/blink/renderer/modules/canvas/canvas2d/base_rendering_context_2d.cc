568e9368d5a9e519538e26c02a727de972832aa9
Colin Blundell <blundell@chromium.org>
2026-07-23 00:06:34 -0700

    [Blink] Move ScopedRasterTimer from 2d providers' Flush() to BaseRC2D
    Bug: 352263194
    Change-Id: I874e4a16500018b4c987345d0ce72d5bef9c4018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126373
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666871}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -87,6 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/image.h"
 #include "third_party/blink/renderer/platform/graphics/image_data_buffer.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_image.h"
+#include "third_party/blink/renderer/platform/graphics/scoped_raster_timer.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/video_frame_image_util.h"
@@ -750,6 +751,10 @@ std::optional<cc::PaintRecord> BaseRenderingContext2D::FlushCanvasInternal(
                        reason == FlushReason::kPrinting ||
                        reason == FlushReason::kCanvasPushFrameWhilePrinting;
   if (shared_image_provider) {
+    ScopedRasterTimer timer(shared_image_provider->IsAccelerated()
+                                ? shared_image_provider->RasterInterface()
+                                : nullptr,
+                            *shared_image_provider);
     bool preserve_recording =
         want_to_print && shared_image_provider->clear_frame();
     recording = shared_image_provider->Flush(preserve_recording);
@@ -757,6 +762,7 @@ std::optional<cc::PaintRecord> BaseRenderingContext2D::FlushCanvasInternal(
       shared_image_provider->ReleaseImageProviderImages();
     }
   } else if (bitmap_provider) {
+    ScopedRasterTimer timer(nullptr, *bitmap_provider);
     bool preserve_recording = want_to_print && bitmap_provider->clear_frame();
     recording = bitmap_provider->Flush(preserve_recording);
     if (recording) {
