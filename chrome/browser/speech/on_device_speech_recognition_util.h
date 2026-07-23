132e6e1be9a306cba0903cd4102503d728158e1b
Jingyun Liu <jingyun@google.com>
2026-07-23 12:58:59 -0700

    use the GetEligibilityAsync instead of the sync one for webspeech API availability
    - fix the incorrect "downloadable" when availability is called before manifest broker init, even though the model is already downloaded to disk in a previous session
    - keeping the sync function since SpeechRecognitionDispatcherHost is still using it
    - refactored some common logic between the async and sync functions
    - for JS API, we'll wait for manifest broker to be initialized before resolving the promise
    
    Bug: 528015401
    Change-Id: I48c3aa00cf10aefef262ce5e9907002737ea18ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139026
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667343}

--- chrome/browser/speech/on_device_speech_recognition_util.h
+++ chrome/browser/speech/on_device_speech_recognition_util.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/functional/callback_forward.h"
 #include "media/mojo/mojom/speech_recognizer.mojom.h"
 
 namespace content {
@@ -25,6 +26,15 @@ media::mojom::AvailabilityStatus GetOnDeviceSpeechRecognitionAvailabilityStatus(
     content::BrowserContext* context,
     std::string_view language,
     media::mojom::SpeechRecognitionQuality quality);
+
+// Asynchronously returns the on-device speech recognition availability status
+// for a given language code (i.e. "en-US") and quality. Similar to
+// `GetOnDeviceSpeechRecognitionAvailabilityStatus` but async.
+void GetOnDeviceSpeechRecognitionAvailabilityStatusAsync(
+    content::BrowserContext* context,
+    std::string_view language,
+    media::mojom::SpeechRecognitionQuality quality,
+    base::OnceCallback<void(media::mojom::AvailabilityStatus)> callback);
 
 }  // namespace speech
 
