7ff9d5fcfcfbb88ab0887cb03906ca4e74d4d57a
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-23 08:26:03 -0700

    [unsafe-buffers] Remove allow_unsafe_libc_calls from gpu autogen files
    This CL removes the file-wide `#pragma allow_unsafe_libc_calls` from
    the generated GPU command buffer format files.
    
    The generator `build_cmd_buffer_lib.py` is updated to no longer add this
    pragma, as all unsafe C-library calls in the generated files are now
    wrapped in `UNSAFE_TODO` or `UNSAFE_BUFFERS`.
    
    Bug: 351564777
    Bug: 501112808
    
    Change-Id: I37122b264c9a4d1d105906c55670ef82b0dc967a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131284
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667124}

--- gpu/command_buffer/common/raster_cmd_format_autogen.h
+++ gpu/command_buffer/common/raster_cmd_format_autogen.h
@@ -9,11 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
-#pragma allow_unsafe_libc_calls
-#endif
-
 #ifndef GPU_COMMAND_BUFFER_COMMON_RASTER_CMD_FORMAT_AUTOGEN_H_
 #define GPU_COMMAND_BUFFER_COMMON_RASTER_CMD_FORMAT_AUTOGEN_H_
 
