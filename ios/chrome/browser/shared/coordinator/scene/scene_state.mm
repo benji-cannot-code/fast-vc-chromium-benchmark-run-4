7476ae6386ae4646dc181cf30a5f9a464de1459b
Sylvain Defresne <sdefresne@chromium.org>
2026-07-23 03:12:10 -0700

    [ios] Remove explicit setter for -scene property in SceneState
    The setter is only assigning the parameter to the ivar, so there
    is no need to have an explicit setter, the setter generated by the
    compiler is good enough.
    
    Bug: 519105565
    Change-Id: Ib2abdc5af47285938bf3595f8e4b8a592e490c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131905
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666964}

--- ios/chrome/browser/shared/coordinator/scene/scene_state.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_state.mm
@@ -139,10 +139,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return _sceneStateOptions.identifier;
 }
 
-- (void)setScene:(UIWindowScene*)scene {
-  _scene = scene;
-}
-
 - (void)setActivationLevel:(SceneActivationLevel)newLevel {
   if (_activationLevel == newLevel) {
     return;
