e7cb89f178e7f3a4fcb09344c5b77b0bcdc2a99d
Bhuvana Betini <bbetini@google.com>
2026-07-23 16:00:27 -0700

    [Actor] Disable PiP For Background Actuation
    Bug: 537330680
    Change-Id: I89767e49d7f7eb608d7dee648caa19551aacb05b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138229
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667482}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -1466,7 +1466,8 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         if (getProfileProviderSupplier().get() == null
                 || !GlicEnabling.isProfileEligible(
                         getProfileProviderSupplier().get().getOriginalProfile())
-                || DeviceFormFactor.isNonMultiDisplayContextOnTablet(this)) {
+                || DeviceFormFactor.isNonMultiDisplayContextOnTablet(this)
+                || ChromeFeatureList.isEnabled(ChromeFeatureList.GLIC_BACKGROUND_ACTUATION)) {
             return null;
         }
 
