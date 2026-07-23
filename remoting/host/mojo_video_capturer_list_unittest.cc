fcf5ffe6e5d02a8e2be6738fb8849067e984656b
Yuwei Huang <yuweih@chromium.org>
2026-07-23 16:50:18 -0700

    [remoting] Extract PeerSessionImpl execution logic from ClientSession
    More refactoring for the peer connection process work. Eventually
    PeerSessionImpl will be moved to the PC process.
    
    The majority of the code is now moved to PeerSessionImpl, which is
    created post-authentication. ClientSession will remain to handle
    pre-authentication stuff and relay state changes to its listener. In
    future CLs, a IpcPeerSession class will be introduced for ClientSession
    in the network process to communicate with PeerSessionImpl in the PC
    process.
    
    The 3 CLs in the chain will be submitted together to minimize churn.
    
    Tested: Successfully connected to dev host and verified SK forwarding
    in SKv1 and SKv2 mode, and WebAuthn forwarding.
    Bug: 502281489
    
    TAG=agy
    CONV=dd47ce0e-b543-4349-83f8-8ad9629fe941
    
    Change-Id: I7daa3bf00c78cc2d86fd5af43773c76e8e8d144f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129435
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667508}

--- remoting/host/mojo_video_capturer_list_unittest.cc
+++ remoting/host/mojo_video_capturer_list_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/test/task_environment.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "remoting/base/auto_thread_task_runner.h"
 #include "remoting/host/host_mock_objects.h"
 #include "remoting/protocol/fake_desktop_capturer.h"
