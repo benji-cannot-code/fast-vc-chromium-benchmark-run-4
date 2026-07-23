6f1c3ef75e51100287d8715224548907a6474e43
Fernando Ramirez <ferny@google.com>
2026-07-22 17:20:05 -0700

    [AF] Log omnibox autofill chip clicked events
    Design: go/autofill-omnibox-dd
    Bug: 490214967
    Change-Id: Iadc86029fd29e8b57f9c0e8d5d41d43c9707520e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8111058
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666736}

--- components/autofill/core/browser/metrics/payments/omnibox_autofill_metrics.h
+++ components/autofill/core/browser/metrics/payments/omnibox_autofill_metrics.h
@@ -67,7 +67,9 @@ enum class OmniboxAutofillShowChipDecisionPart1 {
 enum class OmniboxAutofillEvents {
   kChipShown = 0,
   kChipShownOnce = 1,
-  kMaxValue = kChipShownOnce,
+  kChipClicked = 2,
+  kChipClickedOnce = 3,
+  kMaxValue = kChipClickedOnce,
 };
 // LINT.ThenChange(/tools/metrics/histograms/metadata/autofill/enums.xml:OmniboxAutofillEvents)
 
