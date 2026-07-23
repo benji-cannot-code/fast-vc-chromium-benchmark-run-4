fc530d0cf3ed2a379dd282d5bc543bd0a8042773
Yasaman Sedaghat <yasamans@google.com>
2026-07-23 08:15:24 -0700

    [ios] Refactor logic related to updating the visibility of floaty
    To make the decision in a central place.
    
    Bug: 476145805
    Change-Id: I6ed5a90db8bd693e5b312a14b1619ad60372d97a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868609
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667111}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_tab_picker_handler.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_utils.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_view_state_change_handler.h"
+#import "ios/chrome/browser/intelligence/bwg/utils/gemini_availability.h"
 #import "ios/chrome/browser/intelligence/bwg/utils/gemini_constants.h"
 #import "ios/chrome/browser/intelligence/bwg/utils/gemini_feature_availability.h"
 #import "ios/chrome/browser/intelligence/bwg/utils/gemini_prefs.h"
@@ -608,6 +609,28 @@ bool GeminiBrowserAgent::IsGeminiAvailableForActiveWebState() const {
       browser_->GetWebStateList()->GetActiveWebState();
   GeminiTabHelper* tab_helper = GetActiveTabHelper(active_web_state);
   return tab_helper && tab_helper->IsGeminiAvailableForWebState();
+}
+
+bool GeminiBrowserAgent::IsFloatyVisible() const {
+  if (!is_floaty_invoked_) {
+    return false;
+  }
+
+  web::WebState* active_web_state =
+      browser_->GetWebStateList()->GetActiveWebState();
+  if (!active_web_state) {
+    return false;
+  }
+
+  bool is_web_state_visible = active_web_state->IsVisible();
+  if (!is_web_state_visible && !IsChromeNextIaEnabled()) {
+    return false;
+  }
+
+  ProfileIOS* profile = browser_->GetProfile();
+  return gemini::IsGeminiAvailable(gemini::EntryPoint::Unknown, profile,
+                                   active_web_state)
+      .enabled;
 }
 
 bool GeminiBrowserAgent::IsInGeminiLiveMode() const {
