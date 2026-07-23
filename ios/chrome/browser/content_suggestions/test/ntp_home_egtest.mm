4a43291869e0488174938d79753160547e0ef416
Scott Yoder <scottyoder@google.com>
2026-07-23 05:38:45 -0700

    [ios] Add AppBar and NTP features to ChromeNextIa field trial config
    This change adds four new features the ChromeNextIa field trial config:
     * kAppBarHideInFullscreen
     * kAppBarHideLabels
     * kOverflowMenuHomeCustomizationEntrypoint
     * kOverflowMenuNTPRefactor
    
    Change-Id: I6011703c3f9807725d5d170d8d32db48044d5077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118347
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667018}

--- ios/chrome/browser/content_suggestions/test/ntp_home_egtest.mm
+++ ios/chrome/browser/content_suggestions/test/ntp_home_egtest.mm
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_constants.h"
 #import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.h"
 #import "ios/chrome/browser/omnibox/public/omnibox_constants.h"
+#import "ios/chrome/browser/popup_menu/overflow_menu/public/features.h"
 #import "ios/chrome/browser/popup_menu/public/popup_menu_constants.h"
 #import "ios/chrome/browser/reading_list/ui_bundled/reading_list_constants.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_constants.h"
@@ -242,6 +243,14 @@ bool AreNumbersEqual(CGFloat num1, CGFloat num2) {
   // TODO(crbug.com/522830813): Test is failing.
   if ([self isRunningTest:@selector(DISABLED_testMinimumHeight)]) {
     config.features_enabled.push_back(kChromeNextIa);
+  }
+
+  if ([self isRunningTest:@selector(
+                              testToggleModuleVisiblityInCustomizationMenu)] ||
+      [self isRunningTest:@selector(testNavigateInCustomizationMenu)]) {
+    // TODO(crbug.com/537272655): Re-enable once the customization menu
+    // coordinator is fully testable in the minimal UI test environment.
+    config.features_enabled.push_back(kOverflowMenuHomeCustomizationEntrypoint);
   }
 
   return config;
