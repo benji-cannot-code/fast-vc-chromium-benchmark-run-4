abaaf410ff47f613c8b931ae38a50bd58def4ece
Gazal <agazal@google.com>
2026-07-23 15:08:36 -0700

    [Vertical Tabs] Add unit tests for ItemTouchHelper2 external drag
    Tests cover visibility toggling, dimensions collapsing, alpha
    modifications, and item recyclability states during the drag lifecycle.
    
    Bug: 509226293, 521984130
    Change-Id: I3575a7a447195904e898b2b3f94afcee9ded86a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141920
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667438}

--- third_party/androidx/local_modifications/recyclerview/java/org/chromium/ui/recyclerview/widget/ItemTouchHelper2.java
+++ third_party/androidx/local_modifications/recyclerview/java/org/chromium/ui/recyclerview/widget/ItemTouchHelper2.java
@@ -2584,6 +2584,9 @@ public class ItemTouchHelper2 extends RecyclerView.ItemDecoration
 
     /******************************************************************
      * BEGIN: External drag support
+     *
+     * Unit tests for this section are located in:
+     * //ui/android/junit/src/org/chromium/ui/recyclerview/widget/ItemTouchHelper2UnitTest.java
      ******************************************************************/
 
     /** Allows to handle long press events externally. */
