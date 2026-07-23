fd14024173d87e99f83d3e9f8aa4043736c3249e
Charles Hager <clhager@google.com>
2026-07-23 15:07:03 -0700

    Add feature flag for contextual search panel close button
    This adds a new feature flag, ContextualPanelCloseButtonOnTablets, to
    guard a rework to the overlay panel / contextual search panel on large
    form factors. This rework will hide the scrim and add a close button to
    the side of the panel.
    
    Bug: 512584438
    Change-Id: I4748006da7f8176516bed6b0e220e54b2cbe0492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8135219
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667435}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -374,6 +374,8 @@ public abstract class ChromeFeatureList {
     public static final String CONTEXTUAL_PAGE_ACTIONS = "ContextualPageActions";
     public static final String CONTEXTUAL_PAGE_ACTION_TAB_GROUPING =
             "ContextualPageActionTabGrouping";
+    public static final String CONTEXTUAL_PANEL_CLOSE_BUTTON_ON_TABLETS =
+            "ContextualPanelCloseButtonOnTablets";
     public static final String CONTEXTUAL_SEARCH_DISABLE_ONLINE_DETECTION =
             "ContextualSearchDisableOnlineDetection";
     public static final String CONTEXTUAL_SEARCH_SUPPRESS_SHORT_VIEW =
@@ -918,6 +920,8 @@ public abstract class ChromeFeatureList {
             newCachedFlag(CONTEXT_MENU_COPY_VIDEO_FRAME_ANDROID, true);
     public static final CachedFlag sContextMenuDownloadVideoFrame =
             newCachedFlag(CONTEXT_MENU_DOWNLOAD_VIDEO_FRAME_ANDROID, true);
+    public static final CachedFlag sContextualPanelCloseButtonOnTablets =
+            newCachedFlag(CONTEXTUAL_PANEL_CLOSE_BUTTON_ON_TABLETS, true);
     public static final CachedFlag sContextualTasks = newCachedFlag(CONTEXTUAL_TASKS, false);
     public static final CachedFlag sCpaTabGroupingButton =
             newCachedFlag(
@@ -1331,6 +1335,7 @@ public abstract class ChromeFeatureList {
                     sCompositorViewRemeasureFix,
                     sContextMenuCopyVideoFrame,
                     sContextMenuDownloadVideoFrame,
+                    sContextualPanelCloseButtonOnTablets,
                     sContextualTasks,
                     sCpaTabGroupingButton,
                     sCrossDeviceTabPaneAndroid,
