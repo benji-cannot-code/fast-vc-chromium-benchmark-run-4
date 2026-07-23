81708aa44ea2ab38d1c8880341e160883b0948c8
Adithya Srinivasan <adithyas@chromium.org>
2026-07-23 08:10:34 -0700

    indigo: Handle tab discarding
    Unregisters previously tracked RWH on tab discard (and also clears entry
    point and toolbar).
    
    Bug: b:538096720
    Change-Id: I4ed66d7972c662a53eaf0aaf022472e4f50eeece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8135164
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667103}

--- chrome/browser/indigo/indigo_page_action_controller.h
+++ chrome/browser/indigo/indigo_page_action_controller.h
@@ -166,6 +166,11 @@ class IndigoPageActionController : public tabs::ContentsObservingTabFeature,
   void FrameSizeChanged(content::RenderFrameHost* render_frame_host,
                         const gfx::Size& frame_size) override;
 
+  // tabs::ContentsObservingTabFeature:
+  void OnDiscardContents(tabs::TabInterface* tab,
+                         content::WebContents* old_contents,
+                         content::WebContents* new_contents) override;
+
   // content::TrackedElementObserver:
   void OnTrackedElementRectsChanged(const viz::TrackedElementRects& rects,
                                     float device_scale_factor) override;
@@ -206,6 +211,9 @@ class IndigoPageActionController : public tabs::ContentsObservingTabFeature,
   };
 
  private:
+  // Resets the page triggering and classification state.
+  void ResetTriggeringState();
+
   // Updates the visibility and states of all entry points.
   void UpdateEntryPointsState();
 
