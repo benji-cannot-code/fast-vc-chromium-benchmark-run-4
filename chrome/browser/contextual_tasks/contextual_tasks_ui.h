b2a4fca751421484a906a8fd8d0a0575e6f07a15
Josiah Kiehl <kiehl@google.com>
2026-07-23 12:17:46 -0700

    Contextual Tasks: Refactor WebUI to support early resource registration
    The data source was registered in the WebUI controller constructor,
    which requires a running WebUI page instance. The extension needs
    the resources registered on startup before the page is visited.
    
    Extract resource/string registration into a static
    RegisterWebUIDataSource(Profile*) method to allow early
    registration.
    
    Bug: b:527483073
    Change-Id: I18bb40005cbf9e42e5e6f38136bbe835083d3c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8081320
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667307}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.h
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
 #include "base/uuid.h"
+#include "base/values.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_auto_suggestion_manager.h"
@@ -78,6 +79,7 @@ class TabInterface;
 class ContextualTasksInternalsPageHandler;
 
 class ContextualTasksPageHandler;
+class Profile;
 
 class ContextualTasksUI
     : public contextual_tasks::ContextualTasksUIInterface,
@@ -127,6 +129,9 @@ class ContextualTasksUI
   ContextualTasksUI(const ContextualTasksUI&) = delete;
   ContextualTasksUI& operator=(const ContextualTasksUI&) = delete;
   ~ContextualTasksUI() override;
+
+  static content::WebUIDataSource* RegisterWebUIDataSource(Profile* profile);
+  static base::DictValue GetContextualTasksLoadTimeData(Profile* profile);
 
 #if !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   using SlimWebViewPageHandlerFactory::BindInterface;
