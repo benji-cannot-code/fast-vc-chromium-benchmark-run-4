f7403510b68d412336b1cf9f0a674a37df0aac0e
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-07-23 13:41:20 -0700

    Remove GetTextureTargetForIOSurfaces()
    Mac allows only metal and swiftshader GL implementations now [1]. Which
    means this function always returns TEXTURE_2D.
    
    Note, that defaultBackendFormat always used TEXTURE_2D, so there is no
    need to update texture target on backend format anymore.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ui/gl/init/gl_factory_mac.cc;drc=3c8e0ce56586d4887b2f4832c8703ff4d331b507;l=28
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/gpu/ganesh/gl/GrGLCaps.cpp;drc=3c8e0ce56586d4887b2f4832c8703ff4d331b507;l=5298
    
    Bug: 519633318
    Change-Id: Ib1f8e2055be3ed53407fa7838e4f77f649d21991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138223
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667377}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing_factory_unittest.cc
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing_factory_unittest.cc
@@ -125,11 +125,7 @@ class IOSurfaceImageBackingFactoryTest : public SharedImageTestBase {
     backing_factory_ = std::make_unique<IOSurfaceImageBackingFactory>(
         context_state_->gr_context_type(), context_state_->GetMaxTextureSize(),
         context_state_->feature_info(), /*progress_reporter=*/nullptr,
-#if BUILDFLAG(IS_MAC)
-        GetTextureTargetForIOSurfaces());
-#else
         GL_TEXTURE_2D);
-#endif
   }
 
  protected:
@@ -203,12 +199,6 @@ TEST_F(IOSurfaceImageBackingFactoryTest, GL_SkiaGL) {
   EXPECT_TRUE(backing);
   backing->SetCleared();
 
-  GLenum expected_target =
-#if BUILDFLAG(IS_MAC)
-      GetTextureTargetForIOSurfaces();
-#else
-      GL_TEXTURE_2D;
-#endif
   std::unique_ptr<SharedImageRepresentationFactoryRef> factory_ref =
       shared_image_manager_.Register(std::move(backing), &memory_type_tracker_);
 
@@ -218,8 +208,6 @@ TEST_F(IOSurfaceImageBackingFactoryTest, GL_SkiaGL) {
         shared_image_representation_factory_.ProduceGLTexturePassthrough(
             mailbox);
     EXPECT_TRUE(gl_representation);
-    EXPECT_EQ(expected_target,
-              gl_representation->GetTexturePassthrough()->target());
 
     // Access the SharedImageRepresentationGLTexutre
     auto scoped_write_access = gl_representation->BeginScopedAccess(
@@ -585,15 +573,7 @@ TEST_P(IOSurfaceImageBackingFactoryDawnTest, GL_Dawn_Skia_UnclearTexture) {
     auto gl_representation =
         shared_image_representation_factory_.ProduceGLTexturePassthrough(
             factory_ref->mailbox());
-    GLenum expected_target =
-#if BUILDFLAG(IS_MAC)
-        GetTextureTargetForIOSurfaces();
-#else
-        GL_TEXTURE_2D;
-#endif
     EXPECT_TRUE(gl_representation);
-    EXPECT_EQ(expected_target,
-              gl_representation->GetTexturePassthrough()->target());
 
     std::unique_ptr<GLTexturePassthroughImageRepresentation::ScopedAccess>
         gl_scoped_access = gl_representation->BeginScopedAccess(
@@ -921,11 +901,7 @@ class IOSurfaceImageBackingFactoryParameterizedTestBase
     backing_factory_ = std::make_unique<IOSurfaceImageBackingFactory>(
         context_state_->gr_context_type(), context_state_->GetMaxTextureSize(),
         context_state_->feature_info(), &progress_reporter_,
-#if BUILDFLAG(IS_MAC)
-        GetTextureTargetForIOSurfaces());
-#else
         GL_TEXTURE_2D);
-#endif
   }
 
   viz::SharedImageFormat get_format() { return std::get<0>(GetParam()); }
@@ -1177,12 +1153,6 @@ TEST_P(IOSurfaceImageBackingFactoryScanoutTest, InitialData) {
   std::unique_ptr<SharedImageRepresentationFactoryRef> shared_image =
       shared_image_manager_.Register(std::move(backing), &memory_type_tracker_);
   EXPECT_TRUE(shared_image);
-  GLenum expected_target =
-#if BUILDFLAG(IS_MAC)
-      GetTextureTargetForIOSurfaces();
-#else
-      GL_TEXTURE_2D;
-#endif
 
   if (gr_context_type == GrContextType::kGL) {
     // First, validate a GLTexturePassthroughImageRepresentation.
@@ -1191,8 +1161,6 @@ TEST_P(IOSurfaceImageBackingFactoryScanoutTest, InitialData) {
             mailbox);
     EXPECT_TRUE(gl_representation);
     EXPECT_TRUE(gl_representation->GetTexturePassthrough()->service_id());
-    EXPECT_EQ(expected_target,
-              gl_representation->GetTexturePassthrough()->target());
     EXPECT_EQ(size, gl_representation->size());
     EXPECT_EQ(format, gl_representation->format());
     EXPECT_EQ(color_space, gl_representation->color_space());
