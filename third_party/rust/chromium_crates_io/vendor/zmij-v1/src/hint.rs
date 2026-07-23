d10089a53c48ff33c1a9a97f00c95755b4c5b8d2
Liza Burakova <liza@chromium.org>
2026-07-23 16:18:52 -0700

    Roll zmij: 1.0.21 => 1.0.23 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * zmij: 1.0.21 => 1.0.23; https://docs.rs/crate/zmij/1.0.23
    
    Bug: None
    Change-Id: Iaece08acba5c77c12b276a7f78763a403a59e40e
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8109678
    Auto-Submit: Liza Burakova <liza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667489}

--- third_party/rust/chromium_crates_io/vendor/zmij-v1/src/hint.rs
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-pub fn select_unpredictable<T>(condition: bool, true_val: T, false_val: T) -> T {
-    if condition {
-        true_val
-    } else {
-        false_val
-    }
-}
