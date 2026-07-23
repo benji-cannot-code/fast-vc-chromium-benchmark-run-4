df772234ff010c8c305df47f5b26d18ee861b633
Arthur Milchior <arthurmilchior@google.com>
2026-07-23 03:11:33 -0700

    [iOS]Add AI Tier and Premium Ring image provider APIs
    This CL declares two new provider APIs under intelligence provider
    group:
    - GetAITierName: converts an integer AI tier (AITier) to an NSString.
      In public Chromium, it returns the number as a string.
    - GetPremiumRingImage: returns the premium ring image.
      In public Chromium, it returns a flat black version of the ring.
    
    Also adds public unit tests, and ownership of the signin subfolder
    
    This is part of the chain:
    + crrev.com/c/8129950
    * crrev.com/i/9579777
    
    TAG=AGY
    
    Bug: 522144942
    Change-Id: I9c247fb3f87e114f6b71ef6b5557071c9ff4e99c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129950
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666963}

---
+++ ios/chrome/test/providers/intelligence/signin/signin_test_ai.mm
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Foundation/Foundation.h>
+
+#import "ios/public/provider/chrome/browser/intelligence/signin/signin_ai_logo.h"
+
+namespace ios::provider {
+
+NSString* GetAITierName(int ai_tier) {
+  if (ai_tier <= 0) {
+    return nil;
+  }
+  return [NSString stringWithFormat:@"%d", ai_tier];
+}
+
+UIImage* GetPremiumRingImage() {
+  return [UIImage imageNamed:@"premium_ring"];
+}
+
+}  // namespace ios::provider
