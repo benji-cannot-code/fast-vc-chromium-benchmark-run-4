8cff207f70042e6a846a8b51455e3e748eb43a35
Lei Zhang <thestig@chromium.org>
2026-07-22 18:01:32 -0700

    Print Preview: Remove `RequestPrintPreviewParams::is_modifiable`
    Now that nobody reads this bit anymore, delete it and simplify all the
    setters.
    
    Bug: 498367544
    Change-Id: Ie6e9c9f82d29a58f0438356ff7f5a696a4796566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129023
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666758}

--- chrome/browser/printing/print_preview_dialog_controller.cc
+++ chrome/browser/printing/print_preview_dialog_controller.cc
@@ -220,9 +220,8 @@ PrintPreviewDialogController::CreatePrintPreviewDialogDelegateForTesting(
 WebContents* PrintPreviewDialogController::GetOrCreatePreviewDialogForTesting(
     WebContents* initiator) {
   constexpr bool kIsPdf = false;
-  mojom::RequestPrintPreviewParams params;
-  params.is_modifiable = !kIsPdf;
-  return GetOrCreatePreviewDialog(initiator, params, kIsPdf);
+  return GetOrCreatePreviewDialog(initiator, mojom::RequestPrintPreviewParams(),
+                                  kIsPdf);
 }
 
 WebContents* PrintPreviewDialogController::GetOrCreatePreviewDialog(
@@ -316,7 +315,7 @@ void PrintPreviewDialogController::EraseInitiatorInfo(
   web_contents_collection_.StopObserving(it->second.initiator);
   it->second.initiator = nullptr;
   it->second.request_params = {};
-  // Set to true to match the behavior for resetting `request_params`.
+  // Set to true to match the original behavior for resetting `request_params`.
   it->second.is_pdf = true;
   it->second.scoper.reset();
 }
