f8f68537ac7e3e95a5bcd4c5e8725658bce1d0c8
Piotr Domaradzki <doland@google.com>
2026-07-23 06:27:05 -0700

    Check if AtMem action is legal in Autocomplete suggestion generator.
    Bug: 494131942
    Change-Id: I67ca73d0c58f3d4469b9383ad6bd65e7973de0b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124498
    Commit-Queue: Piotr Domaradzki <doland@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667045}

--- components/autofill/core/browser/suggestions/autocomplete_suggestion_generator.h
+++ components/autofill/core/browser/suggestions/autocomplete_suggestion_generator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 
+#include "base/functional/callback.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -34,8 +35,7 @@ namespace autofill {
 class AutocompleteSuggestionGenerator : public SuggestionGenerator {
  public:
   explicit AutocompleteSuggestionGenerator(
-      scoped_refptr<AutofillWebDataService> profile_database,
-      bool at_memory_enabled);
+      scoped_refptr<AutofillWebDataService> profile_database);
   ~AutocompleteSuggestionGenerator() override;
 
   void GenerateSuggestions(
@@ -64,6 +64,7 @@ class AutocompleteSuggestionGenerator : public SuggestionGenerator {
   // `result` contains the Autocomplete suggestions retrieved from the DB that,
   // if valid, will be passed to the callback in `query_handler`.
   void OnAutofillValuesReturned(QueryHandler query_handler,
+                                bool is_at_memory_enabled,
                                 WebDataServiceBase::Handle current_handle,
                                 std::unique_ptr<WDTypedResult> result);
 
@@ -77,8 +78,6 @@ class AutocompleteSuggestionGenerator : public SuggestionGenerator {
   // is in flight.
   std::optional<WebDataServiceBase::Handle> pending_query_;
 
-  // Whether the AtMemory feature is enabled.
-  const bool at_memory_enabled_;
 
   base::WeakPtrFactory<AutocompleteSuggestionGenerator> weak_ptr_factory_{this};
 };
