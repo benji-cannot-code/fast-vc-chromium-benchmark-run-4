8461ca609bb106a09970c9a5035301985ca054d8
Paul Jensen <pauljensen@chromium.org>
2026-07-23 15:13:42 -0700

    WebUI toolbar: dedup toolbar action containers
    Pinned toolbar actions and extensions had a lot of duplicate code,
    mostly to handle animations and registering with the
    TrackedElementManager.  This change dedups the action containers code.
    
    Change-Id: Iea6830afeccca3ce442e81533deb12162a5ac7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076999
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667444}

--- chrome/browser/resources/webui_toolbar/extensions.html.ts
+++ chrome/browser/resources/webui_toolbar/extensions.html.ts
@@ -11,7 +11,7 @@ export function getHtml(this: ExtensionsElement) {
   // clang-format off
   return html`<!--_html_template_start_-->
 ${repeat(
-    this.keyedStates_,
+    this.keyedStates,
     (keyedState) => keyedState.key,
     (keyedState) => html`
       <webui-toolbar-extension
@@ -22,10 +22,10 @@ ${repeat(
       </webui-toolbar-extension>
     `,
 )}
-${this.keyedStates_.length > 0 ? html`
+${this.keyedStates.length > 0 ? html`
   <toolbar-divider
-      class="${this.animateInDivider_() ? 'animate-in' : ''}
-             ${this.allExiting_() ? 'exiting' : ''}">
+      class="${this.animateInDivider() ? 'animate-in' : ''}
+             ${this.allExiting() ? 'exiting' : ''}">
   </toolbar-divider>
 ` : ''}
 <!--_html_template_end_-->`;
