73015600985ae65cf5814229a477ed1c2325512c
Yuwei Huang <yuweih@chromium.org>
2026-07-23 15:37:27 -0700

    [remoting] Move client_session.cc/h to peer_session_impl.cc/h
    The whole point of this and the child CL is to preserve git history in
    peer_session_impl.cc/h. As you can see in crrev.com/c/8129435, most
    logic in ClientSession will be moved into PeerSessionImpl, so I'd
    prioritize preserving the history in PeerSessionImpl as opposed to
    ClientSession.
    
    I tried very hard to trick Gerrit to believe that PeerSessionImpl is
    branched from ClientSession, including minimizing changes and adding
    trivial changes to client_session.cc/h, but had no luck. Hence the
    two-CL dance.
    
    This CL only moves the file and fixes the header guard. It intentionally
    keeps the class name unchanged to make things easier. The child CL will
    resurrect client_session.cc/h and presumably the git history will be
    lost in it.
    
    The next 3 CLs will be submitted together to minimize churn.
    
    TAG=agy
    CONV=dd47ce0e-b543-4349-83f8-8ad9629fe941
    
    Bypass-Check-License: Moved file.
    Bug: 502281489
    Change-Id: Ic52c53f47ca8818ae01d03ae9151be5ec8203a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8132649
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667464}

--- remoting/host/peer_session_impl.h
+++ remoting/host/peer_session_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_HOST_CLIENT_SESSION_H_
-#define REMOTING_HOST_CLIENT_SESSION_H_
+#ifndef REMOTING_HOST_PEER_SESSION_IMPL_H_
+#define REMOTING_HOST_PEER_SESSION_IMPL_H_
 
 #include <cstdint>
 #include <map>
@@ -497,4 +497,4 @@ class ClientSession : public protocol::HostStub,
 
 }  // namespace remoting
 
-#endif  // REMOTING_HOST_CLIENT_SESSION_H_
+#endif  // REMOTING_HOST_PEER_SESSION_IMPL_H_
