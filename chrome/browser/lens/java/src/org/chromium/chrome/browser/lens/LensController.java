8a229ce44ce3c6bb37581898020d889d245c176e
Samuel Huang <huangs@chromium.org>
2026-07-23 08:51:57 -0700

    [Lens Overlay] Expose Lens icon resource ID via LensController
    The Lens icon resource ID is currently protected within its package. The
    upcoming Omnibox EP (Entry Point) feature requires access to this icon
    from the omnibox UI module.
    
    This CL exposes the resource ID through the public LensController
    singleton, allowing decoupled cross-module access.
    
    Also adds LensControllerTest to verify resource retrieval.
    
    Bug: 521877022, 497048939, 493627069
    Change-Id: Ifc30ed78ea8e7caabf0425804a37c8e104bee587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129209
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667142}

--- chrome/browser/lens/java/src/org/chromium/chrome/browser/lens/LensController.java
+++ chrome/browser/lens/java/src/org/chromium/chrome/browser/lens/LensController.java
@@ -6,6 +6,8 @@ package org.chromium.chrome.browser.lens;
 
 import android.content.Context;
 
+import androidx.annotation.DrawableRes;
+
 import org.chromium.base.Callback;
 import org.chromium.base.ResettersForTesting;
 import org.chromium.base.ServiceLoaderUtil;
@@ -40,6 +42,15 @@ public class LensController {
             delegate = new LensControllerDelegate();
         }
         mDelegate = delegate;
+    }
+
+    /**
+     * Retrieve the Lens icon resource id.
+     *
+     * @return The resource id for the Lens icon.
+     */
+    public @DrawableRes int getLensIconResourceId() {
+        return mDelegate.getLensIconResourceId();
     }
 
     /**
