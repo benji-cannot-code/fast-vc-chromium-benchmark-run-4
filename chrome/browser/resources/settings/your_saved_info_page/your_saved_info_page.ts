ed2a879ad75992c780bda1f5f74546bb5d40cc90
Mariam Ali <alimariam@google.com>
2026-07-23 05:34:07 -0700

    Context Settings: Add spark icon
    UX goal: https://screenshot.googleplex.com/4BC3cyuGn4CYsQW
    Implementation: https://screenshot.googleplex.com/4UWfZcKFuH8ujhH
    
    Bug: b:519496250
    Change-Id: Ib6285b9ac86905317417220042e18d962f91c6c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032900
    Reviewed-by: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Stanislav Mikheyev <mikhe@google.com>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667016}

--- chrome/browser/resources/settings/your_saved_info_page/your_saved_info_page.ts
+++ chrome/browser/resources/settings/your_saved_info_page/your_saved_info_page.ts
@@ -9,11 +9,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * and manage their saved info.
  */
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import './account_card.js';
 import './category_reference_card.js';
 import './collapsible_autofill_settings_card.js';
 import '/shared/settings/prefs/prefs.js';
 import '../settings_page/settings_section.js';
+import '../icons.html.js';
+// <if expr="_google_chrome">
+import '../internal/icons.html.js';
+
+// </if>
 
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
@@ -116,6 +122,19 @@ export class SettingsYourSavedInfoPageElement extends
           return loadTimeData.getBoolean('showSuggestionsFromGeminiSettings');
         },
       },
+      spark_: {
+        type: String,
+        value: () => {
+          // <if expr="_google_chrome">
+          return loadTimeData.getBoolean('glicAssetsV2Enabled') ?
+              'settings-internal:sparkv2' :
+              'settings-internal:spark';
+          // </if>
+          // <if expr="not _google_chrome">
+          return 'settings20:lightbulb';
+          // </if>
+        },
+      },
     };
   }
 
@@ -142,6 +161,7 @@ export class SettingsYourSavedInfoPageElement extends
   private setPersonalDataListener_: PersonalDataChangedListener|null = null;
   private onAutofillAiEntitiesChangedListener_: EntityInstancesChangedListener|
       null = null;
+  declare private spark_: string;
 
   override connectedCallback() {
     super.connectedCallback();
