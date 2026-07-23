f2991bdea4492343427a8f29bdc7c7395b74c2ec
Joe Mason <joenotcharles@google.com>
2026-07-23 15:56:03 -0700

    Validate file changes received from drivefs before applying them
    Fixed: 513402542
    Change-Id: I9a227b6c147fddee5cb21575e178f609ae8a3f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141484
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667475}

--- chrome/browser/ui/ash/holding_space/holding_space_file_system_delegate.cc
+++ chrome/browser/ui/ash/holding_space/holding_space_file_system_delegate.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/experiences/arc/session/arc_service_manager.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+#include "mojo/public/cpp/bindings/message.h"
 #include "url/gurl.h"
 
 namespace ash {
@@ -187,6 +188,14 @@ void HoldingSpaceFileSystemDelegate::OnConnectionReady() {
 void HoldingSpaceFileSystemDelegate::OnFilesChanged(
     const std::vector<drivefs::mojom::FileChange>& changes) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+  // Validate all changes before applying any of them.
+  for (const auto& change : changes) {
+    if (change.path.ReferencesParent()) {
+      mojo::ReportBadMessage("File paths must be canonicalized");
+      return;
+    }
+  }
 
   // When a file is moved, a `kDelete` change will be followed by a `kCreate`
   // change with the same `stable_id` in the same set of `changes`. In some
