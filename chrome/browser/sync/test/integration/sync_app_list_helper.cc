1266e13dc81599ccfb75278cd68a56eeb3e81379
Di Wu <diwux@google.com>
2026-07-24 06:43:28 -0700

    Ash: Remove unused generic extensions dependencies from ChromeOS code
    This CL is part of an ongoing Ash Refactoring series to remove
    dependencies on generic //chrome code from Ash/ChromeOS code that
    still lives under interim locations inside //chrome/browser.
    
    Two ChromeOS-only files each still included a piece of generic
    extensions code they no longer use, one for recording app launch
    metrics and one for deciding which extensions can sync. Both
    includes are leftovers from logic that moved away long ago; nothing
    in either file refers to them anymore.
    
    Deleting the two includes clears the last ChromeOS dependency on
    both headers, with nothing needed in their place.
    
    Bug: b:332804822
    Test: CQ
    Change-Id: I2e11e0cc496693a33b618c9a584ff7f50937d283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142770
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667825}

--- chrome/browser/sync/test/integration/sync_app_list_helper.cc
+++ chrome/browser/sync/test/integration/sync_app_list_helper.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
-#include "chrome/common/extensions/sync_helper.h"
 #include "extensions/browser/app_sorting.h"
 #include "extensions/browser/extension_system.h"
 
