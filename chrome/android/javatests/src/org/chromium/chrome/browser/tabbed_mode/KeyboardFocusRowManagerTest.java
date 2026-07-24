a900cb18537bcfb7e1a632138084fadc08c9c154
Fuhsin Liao <fuhsinliao@google.com>
2026-07-24 08:40:30 -0700

    Fix flaky test in KeyboardFocusRowManagerTest
    This cl fixes the flakiness in
    testSwitchKeyboardFocusRow_withBookmarkBarFocus test. The reason only
    this test fails in the suite is that it switches focus to the Bookmark
    bar using onMenuOrKeyboardAction rather than switchRow(). Since it
    bypasses the waitForIdleSync() call provided by switchRow(), a race
    condition occurs where assertOnBookmarksBar() runs before the focus
    transition from the omnibox has finished.
    
    To fix this, we added a waitForIdleSync() call after requesting the
    bookmark focus
    
    Bug: 537025880
    Change-Id: Ia507b8d49d6dd882d857e440205e6c7ff31de5c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141783
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667878}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/KeyboardFocusRowManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/KeyboardFocusRowManagerTest.java
@@ -31,8 +31,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -292,9 +290,6 @@ public class KeyboardFocusRowManagerTest {
     @SmallTest
     @Feature("KeyboardShortcuts")
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
-    // TODO(crbug.com/537025880): Re-enable when fixed
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM)
-    @DisabledTest(message = "crbug.com/535473683")
     public void testSwitchKeyboardFocusRow_withBookmarkBarFocus() {
         setUserPrefsShowBookmarksBar(true);
 
@@ -310,6 +305,7 @@ public class KeyboardFocusRowManagerTest {
         // Focus directly on bookmarks bar with shortcut even though it's not next in cycle order.
         ThreadUtils.runOnUiThreadBlocking(
                 () -> mActivity.onMenuOrKeyboardAction(R.id.focus_bookmarks, false));
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         assertOnBookmarksBar();
 
         // Now switch and make sure we appropriately switch given our new cycle position.
