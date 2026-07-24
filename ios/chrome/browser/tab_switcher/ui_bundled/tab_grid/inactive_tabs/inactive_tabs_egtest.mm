958f06f4a590567ac4740b9f55b1fd8fc7bfb69b
Federica Germinario <fedegermi@google.com>
2026-07-24 06:45:26 -0700

    Clean up InactiveTabsTestMode in tearDownHelper of InactiveTabsTestCase
    InactiveTabsTestCase launches the app with the -InactiveTabsTestMode
    flag across multiple tests, setting user defaults. This CL clears
    the default right inside tearDownHelper to avoid polluting later tests.
    
    Bug: 530520627
    Change-Id: Ib79322d836e2085d7093d52c93a079109abfc043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118246
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667826}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/inactive_tabs/inactive_tabs_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/inactive_tabs/inactive_tabs_egtest.mm
@@ -128,6 +128,7 @@ id<GREYMatcher> GetMatcherForUserEducationSettingsButton() {
 }
 
 - (void)tearDownHelper {
+  [ChromeEarlGrey removeUserDefaultsObjectForKey:@"InactiveTabsTestMode"];
   [ChromeEarlGrey
       removeUserDefaultsObjectForKey:kInactiveTabsUserEducationShownOnceKey];
   [super tearDownHelper];
