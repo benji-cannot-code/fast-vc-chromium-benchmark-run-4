a9b81a1d5ad943170468f3631e2ecaed66cdcac6
Aisulu R <aisulu@google.com>
2026-07-24 15:01:39 -0700

    [PersonalContext] Record basic metrics for notice on Desktop
    This CL adds UMA histogram tracking for the notice similarly to Clank.
    
    Specifically, it records the `kShown` and `kAcknowledged` notice
    interaction metrics depending on the main filling product source
    (AtMemory and Ambient Autofill).
    
    The other actions like Dismiss, etc. will be added later once we align
    between surfaces and platforms.
    
    Bug: b:520201413
    Change-Id: Ibc9eba2805003fcbbed132f273c09e204bf7b1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138108
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Aisulu Rakhmetullina <aisulu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668191}

--- chrome/browser/ui/views/autofill/popup/popup_personal_context_notice_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_personal_context_notice_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "cc/paint/paint_flags.h"
 #include "chrome/browser/profiles/profile.h"
@@ -154,11 +155,18 @@ PopupPersonalContextNoticeView::PopupPersonalContextNoticeView(
       IDS_AUTOFILL_POPUP_PERSONAL_CONTEXT_NOTICE_OK_BUTTON);
 
   if (controller_) {
-    FillingProduct product = controller_->GetMainFillingProduct();
-    if (product == FillingProduct::kAtMemory &&
-        !IsLoggingDisabledByPolicy(controller_.get())) {
-      context_text = l10n_util::GetStringUTF16(
-          IDS_AUTOFILL_POPUP_PERSONAL_CONTEXT_NOTICE_CONTEXT_WITH_LOGGING);
+    if (controller_->GetMainFillingProduct() == FillingProduct::kAtMemory) {
+      base::UmaHistogramEnumeration(
+          "PersonalContext.AtMemory.NoticeInteractions",
+          PersonalContextAtMemoryNoticeInteractions::kShown);
+      if (!IsLoggingDisabledByPolicy(controller_.get())) {
+        context_text = l10n_util::GetStringUTF16(
+            IDS_AUTOFILL_POPUP_PERSONAL_CONTEXT_NOTICE_CONTEXT_WITH_LOGGING);
+      }
+    } else {
+      base::UmaHistogramEnumeration(
+          "PersonalContext.AmbientAutofill.NoticeInteractions",
+          PersonalContextAmbientAutofillNoticeInteractions::kShown);
     }
   }
 
@@ -267,6 +275,15 @@ bool PopupPersonalContextNoticeView::IsSelectable() const {
 
 void PopupPersonalContextNoticeView::OnGotItButtonClicked() {
   if (controller_) {
+    if (controller_->GetMainFillingProduct() == FillingProduct::kAtMemory) {
+      base::UmaHistogramEnumeration(
+          "PersonalContext.AtMemory.NoticeInteractions",
+          PersonalContextAtMemoryNoticeInteractions::kAcknowledged);
+    } else {
+      base::UmaHistogramEnumeration(
+          "PersonalContext.AmbientAutofill.NoticeInteractions",
+          PersonalContextAmbientAutofillNoticeInteractions::kAcknowledged);
+    }
     // TODO(crbug.com/520201413): Add metrics to track the cases when
     // `RemoveSuggestion` returns false.
     controller_->RemoveSuggestion(
