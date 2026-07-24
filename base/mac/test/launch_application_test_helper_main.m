133b68021333cd6be19ac74f25e223ed730f1549
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-24 12:33:55 -0700

    [unsafe-buffers] Remove allow_unsafe_buffers from base/mac/
    Removing the last file-wide #pragma opting out files from
    spanification.
    
    Bug: 351564777, 40284755
    Change-Id: If9bf857bfde6e4e9af48ecab73378e4e41b6d0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138973
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668086}

--- base/mac/test/launch_application_test_helper_main.m
+++ base/mac/test/launch_application_test_helper_main.m
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 // This is a helper application for launch_application_unittest.mm. This
 // application records several events by writing them to a named pipe;
 // the unit tests then use this information to verify that this helper was
@@ -16,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // with .app replaced by .fifo.
 
 #import <Cocoa/Cocoa.h>
+
+#import "base/compiler_specific.h"
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 @end
@@ -118,9 +115,12 @@ __attribute__((visibility("default"))) int main(int argc, char** argv) {
   [NSApplication sharedApplication];
 
   NSMutableArray* command_line = [[NSMutableArray alloc] initWithCapacity:argc];
-  for (int i = 0; i < argc; ++i) {
-    [command_line addObject:[NSString stringWithUTF8String:argv[i]]];
-  }
+  // SAFETY: Standard C++ main contract.
+  UNSAFE_BUFFERS({
+    for (int i = 0; i < argc; ++i) {
+      [command_line addObject:[NSString stringWithUTF8String:argv[i]]];
+    }
+  });
 
   AppDelegate* delegate =
       [[AppDelegate alloc] initWithCommandLine:command_line];
