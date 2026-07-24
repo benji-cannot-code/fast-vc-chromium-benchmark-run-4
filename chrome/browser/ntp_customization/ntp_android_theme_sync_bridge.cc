a6c6ef199147615855cbe31f24854e01ea27ea19
Xinyi Ji <xinyiji@google.com>
2026-07-24 15:00:40 -0700

    [NTP Customization Sync] Polish loop control in OnDataLoaded (Step 2.2)
    Updates the record loading loop in
    NtpAndroidThemeSyncBridge::OnDataLoaded to break immediately on parse
    success and continue on deserialization error.
    
    Bug: 488439751
    Change-Id: Ifa0c0347fe73ddbaf9ab0b33defc6c6359307a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8143860
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668190}

--- chrome/browser/ntp_customization/ntp_android_theme_sync_bridge.cc
+++ chrome/browser/ntp_customization/ntp_android_theme_sync_bridge.cc
@@ -248,10 +248,11 @@ void NtpAndroidThemeSyncBridge::OnDataLoaded(
       sync_pb::ThemeAndroidSpecifics specifics;
       if (specifics.ParseFromString(record.value)) {
         current_theme_ = specifics;
+        break;
       } else {
         DLOG(ERROR) << "Failed to deserialize ThemeAndroidSpecifics.";
+        continue;
       }
-      continue;
     }
   }
 
