ee0451089506ff90bd7f88a4bb90ceb20454dcc5
Andrew Paseltiner <apaseltiner@chromium.org>
2026-07-24 11:07:54 -0700

    Minor cleanup for Rust CBOR implementation
    - Deduplicate get_float_argument and get_argument logic
    - Simplify error handling in to_bytestring and tostring
    - Simplify assertions in unit tests
    - Remove unnecessary explicit call to deref method
    
    Bug: 454485901
    Change-Id: I6f5eca627ad4be998f7dbd6850fec8bb57253e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138940
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Zgroza (Luke) Klimek <zgroza@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668012}

--- components/cbor/rust/writer.rs
+++ components/cbor/rust/writer.rs
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 use alloc::vec::Vec;
-use core::ops::Deref;
 
 use crate::constants::*;
 use crate::values::{MapKey, Value};
@@ -50,7 +49,7 @@ pub(crate) fn append_value(val: &Value, out: &mut Vec<u8>) {
         Value::Int(v) => write_header(out, MAJOR_TYPE_NEGATIVE_INT, !v as u64),
         Value::Bytestring(s) => {
             write_header(out, MAJOR_TYPE_BYTE_STRING, s.len() as u64);
-            out.extend_from_slice(s.deref());
+            out.extend_from_slice(s);
         }
         Value::String(s) => {
             write_header(out, MAJOR_TYPE_TEXT_STRING, s.len() as u64);
