a5d500751b521e18dc5d5aa58d81d8c7482b17db
Rune Lillesveen <futhark@chromium.org>
2026-07-24 11:29:35 -0700

    Escape CSS syntax component identifiers
    Missing escape of custom identifiers used in syntax definition for
    @function parameter and return types.
    
    Also added a test for escaped identifiers in @property syntax
    descriptors.
    
    Bug: 518094442
    Change-Id: Idf41c16dccabdcdf3350e2eb5dc3fa58e16e0f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8143363
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668036}

---
+++ third_party/blink/renderer/core/css/css_syntax_component.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/css/css_syntax_component.h"
+
+#include "third_party/blink/renderer/core/css/css_markup.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
+
+namespace blink {
+
+String CSSSyntaxComponent::ToString() const {
+  StringBuilder builder;
+  if (type_ == CSSSyntaxType::kIdent) {
+    SerializeIdentifier(string_, builder);
+  } else {
+    builder.Append(blink::ToString(type_));
+  }
+  builder.Append(blink::ToString(repeat_));
+  return builder.ReleaseString();
+}
+
+}  // namespace blink
--- third_party/blink/renderer/core/css/css_syntax_component.h
+++ third_party/blink/renderer/core/css/css_syntax_component.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_value.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
-#include "third_party/blink/renderer/platform/wtf/text/strcat.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -106,11 +105,7 @@ class CSSSyntaxComponent {
     DCHECK(IsRepeatable());
     return repeat_ == CSSSyntaxRepeat::kSpaceSeparated ? ' ' : ',';
   }
-  String ToString() const {
-    String result =
-        (type_ == CSSSyntaxType::kIdent) ? string_ : blink::ToString(type_);
-    return StrCat({result, blink::ToString(repeat_)});
-  }
+  String ToString() const;
 
  private:
   CSSSyntaxType type_;
