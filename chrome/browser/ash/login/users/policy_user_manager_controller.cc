38c7e5627b3c6c2263c9acc094786de57d0e3088
Jun Ishiguro <junis@google.com>
2026-07-24 03:03:21 -0700

    login: Remove g_browser_process from PolicyUserManagerController
    Decouple PolicyUserManagerController from global g_browser_process by
    injecting DeviceLocalAccountPolicyService via its constructor.
    
    TAG=agy
    
    Bug: b:404133029
    Change-Id: I1e965827968b0ddb2fcce84f998d26014c3fe27a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8133681
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667747}

--- chrome/browser/ash/login/users/policy_user_manager_controller.cc
+++ chrome/browser/ash/login/users/policy_user_manager_controller.cc
@@ -13,11 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/syslog_logging.h"
 #include "base/task/single_thread_task_runner.h"
-#include "chrome/browser/ash/policy/core/browser_policy_connector_ash.h"
 #include "chrome/browser/ash/policy/core/device_local_account.h"
 #include "chrome/browser/ash/policy/core/device_local_account_policy_broker.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/browser_process_platform_part.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
 #include "chromeos/ash/components/settings/cros_settings_provider.h"
@@ -76,8 +73,17 @@ PolicyUserManagerController::PolicyUserManagerController(
     user_manager::UserManager* user_manager,
     CrosSettings* cros_settings,
     DeviceSettingsService* device_settings_service,
-    policy::MinimumVersionPolicyHandler* minimum_version_policy_handler)
-    : user_manager_(user_manager), cros_settings_(cros_settings) {
+    policy::MinimumVersionPolicyHandler* minimum_version_policy_handler,
+    policy::DeviceLocalAccountPolicyService*
+        device_local_account_policy_service)
+    : user_manager_(user_manager),
+      cros_settings_(cros_settings),
+      device_local_account_policy_service_(
+          device_local_account_policy_service) {
+  if (!device_local_account_policy_service_) {
+    CHECK_IS_TEST();
+  }
+
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   // UserManager outlives PolicyUserManagerController, and subscriptions are
@@ -147,10 +153,9 @@ PolicyUserManagerController::~PolicyUserManagerController() = default;
 
 void PolicyUserManagerController::OwnershipStatusChanged() {
   if (!device_local_account_policy_service_observation_.IsObserving()) {
+    CHECK(device_local_account_policy_service_);
     device_local_account_policy_service_observation_.Observe(
-        g_browser_process->platform_part()
-            ->browser_policy_connector_ash()
-            ->GetDeviceLocalAccountPolicyService());
+        device_local_account_policy_service_);
   }
   RetrieveTrustedDevicePolicies();
 }
