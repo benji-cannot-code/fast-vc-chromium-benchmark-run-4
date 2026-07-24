f81ad85b02af6f847967bf12eb4ed911fe802d02
Atharv Maan <atharvmaan@chromium.org>
2026-07-23 19:11:12 -0700

    [OmniboxEverywhere] Add prefs to track background mode state
    Bug: b:532190282
    Change-Id: If26e554f8b5a87f4b8b1eb683b293c0690b8e051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8134827
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667595}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_prefs.cc
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_prefs.cc
@@ -11,6 +11,7 @@ namespace omnibox_everywhere::prefs {
 
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kHotkeyEnabled, true);
+  registry->RegisterBooleanPref(kOmniboxEverywhereBackgroundMode, false);
 }
 
 }  // namespace omnibox_everywhere::prefs
