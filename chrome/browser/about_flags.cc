a37453aa86570d1bf7e3dc281717c8597eb6fc30
Mikel Astiz <mastiz@chromium.org>
2026-07-24 04:56:37 -0700

    [omnibox] Add chrome://flags entry on mobile
    This patch adds a chrome://flags entry for
    OmniboxCrossDeviceTabZeroSuggest on Android and iOS too (it was
    previously desktop-only).
    
    Bug: 508162292
    Change-Id: I498de300275c605af07337574ff8b2d9d17af0ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137559
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667785}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -12074,7 +12074,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"omnibox-cross-device-tab-zero-suggest",
      flag_descriptions::kOmniboxCrossDeviceTabZeroSuggestName,
      flag_descriptions::kOmniboxCrossDeviceTabZeroSuggestDescription,
-     kOsDesktop,
+     kOsDesktop | kOsAndroid,
      FEATURE_VALUE_TYPE(omnibox::kOmniboxCrossDeviceTabZeroSuggest)},
 
     {"contextual-tasks-context",
