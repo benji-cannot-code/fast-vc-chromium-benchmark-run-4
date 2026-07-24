201147c3b69361690e3a0a4d6ec897a784adfedb
Di Wu <diwux@google.com>
2026-07-24 01:44:05 -0700

    Ash: Remove no-op extensions client setup from a ChromeOS API test
    This CL is part of an ongoing Ash Refactoring series to remove
    dependencies on generic //chrome code from Ash/ChromeOS code that
    still lives under interim locations inside //chrome/browser.
    
    A ChromeOS enterprise API browser test still installed its own copy
    of the browser's extensions client and uninstalled it on teardown.
    That is a leftover from unit test style setup: in a browser test the
    real client is already installed by browser startup before the test
    begins, and installing a client is ignored once one exists. Both the
    install and the uninstall have long been doing nothing.
    
    Deleting the ritual removes the test's only dependency on this piece
    of generic browser code, with nothing needed in its place.
    
    Bug: b:332804822
    Test: CQ
    Change-Id: I6bb232197a300e016ca9338316ff97d6dcf63e1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8143060
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667718}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api_browsertest.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_api_browsertest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/extensions/api/enterprise_platform_keys.h"
-#include "chrome/common/extensions/chrome_extensions_client.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "chromeos/ash/components/dbus/attestation/keystore.pb.h"
@@ -72,10 +71,6 @@ class EPKChallengeKeyTestBase : public PlatformKeysTestBase {
   void SetUpOnMainThread() override {
     PlatformKeysTestBase::SetUpOnMainThread();
 
-    chrome_extensions_client_ =
-        std::make_unique<extensions::ChromeExtensionsClient>();
-    extensions::ExtensionsClient::Set(chrome_extensions_client_.get());
-
     extension_ = ExtensionBuilder("Test").Build();
 
     prefs_ = profile()->GetPrefs();
@@ -92,12 +87,6 @@ class EPKChallengeKeyTestBase : public PlatformKeysTestBase {
                 base::BindRepeating(&EPKChallengeKeyTestBase::
                                         CreateKeyPermissionsManagerService,
                                     base::Unretained(this)));
-  }
-
-  void TearDownOnMainThread() override {
-    extensions::ExtensionsClient::Set(nullptr);
-    chrome_extensions_client_.reset();
-    PlatformKeysTestBase::TearDownOnMainThread();
   }
 
   void SetMockTpmChallenger() {
@@ -174,7 +163,6 @@ class EPKChallengeKeyTestBase : public PlatformKeysTestBase {
 
   scoped_refptr<const extensions::Extension> extension_;
   ash::platform_keys::MockKeyPermissionsManager key_permissions_manager_;
-  std::unique_ptr<extensions::ChromeExtensionsClient> chrome_extensions_client_;
   raw_ptr<PrefService, DanglingUntriaged> prefs_ = nullptr;
   raw_ptr<ash::attestation::MockTpmChallengeKey, DanglingUntriaged>
       mock_tpm_challenge_key_ = nullptr;
