a677d173a326e496306720ea138ff12ef8208a47
Achuith Bhandarkar <achuith@chromium.org>
2026-07-24 12:24:14 -0700

    c/b/ash/policy: Use TestingBrowserProcess in policy unittests
    Replace standalone TestingPrefServiceSimple member variables with
    TestingBrowserProcess local state in policy unittests.
    
    TAG=agy
    BUG=534323787
    CONV=653383f6-117d-4692-abbd-90004e4c85e4
    
    Change-Id: Ifa601570ab332dda1d39c854bec2266066490c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142361
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668073}

--- chrome/browser/ash/policy/status_collector/activity_storage_unittest.cc
+++ chrome/browser/ash/policy/status_collector/activity_storage_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/time/time.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
@@ -15,23 +16,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace policy {
+
+namespace {
+
 using ::testing::AllOf;
 using ::testing::Property;
 using ::testing::UnorderedElementsAre;
 
-namespace em = enterprise_management;
+namespace em = ::enterprise_management;
 
-namespace {
 constexpr char kPrefName[] = "pref-name";
-}  // namespace
 
-namespace policy {
+}  // namespace
 
 class ActivityStorageTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    local_state_.registry()->RegisterDictionaryPref(kPrefName);
-    storage_ = std::make_unique<ActivityStorage>(&local_state_, kPrefName,
+    TestingPrefServiceSimple* local_state =
+        TestingBrowserProcess::GetGlobal()->GetTestingLocalState();
+    local_state->registry()->RegisterDictionaryPref(kPrefName);
+    storage_ = std::make_unique<ActivityStorage>(local_state, kPrefName,
                                                  base::Days(0));
   }
 
@@ -60,7 +65,6 @@ class ActivityStorageTest : public ::testing::Test {
 
  private:
   content::BrowserTaskEnvironment task_environment_;
-  TestingPrefServiceSimple local_state_;
   std::unique_ptr<ActivityStorage> storage_;
 };
 
--- chrome/browser/ash/policy/status_collector/enterprise_activity_storage_unittest.cc
+++ chrome/browser/ash/policy/status_collector/enterprise_activity_storage_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/time/time.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
@@ -25,16 +26,18 @@ using ::testing::UnorderedElementsAre;
 
 namespace em = ::enterprise_management;
 
-const char kPrefName[] = "pref-name";
+constexpr char kPrefName[] = "pref-name";
 
 }  // namespace
 
 class EnterpriseActivityStorageTest : public ::testing::Test {
  protected:
   void SetUp() override {
-    local_state_.registry()->RegisterDictionaryPref(kPrefName);
+    TestingPrefServiceSimple* local_state =
+        TestingBrowserProcess::GetGlobal()->GetTestingLocalState();
+    local_state->registry()->RegisterDictionaryPref(kPrefName);
     storage_ =
-        std::make_unique<EnterpriseActivityStorage>(&local_state_, kPrefName);
+        std::make_unique<EnterpriseActivityStorage>(local_state, kPrefName);
   }
 
   static testing::Matcher<em::TimePeriod> EqActivity(
@@ -62,7 +65,6 @@ class EnterpriseActivityStorageTest : public ::testing::Test {
 
  private:
   content::BrowserTaskEnvironment task_environment_;
-  TestingPrefServiceSimple local_state_;
   std::unique_ptr<EnterpriseActivityStorage> storage_;
 };
 
