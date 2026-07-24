71d2ccef67b8f9f5fba79d173639f694179cdfaa
Nikolaos Papaspyrou <nikolaos@chromium.org>
2026-07-24 09:42:34 -0700

    cpu_performance: Ship the CPU Performance API
    This CL enables CpuPerformance for Stable.
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/igPwzkxhQtk/m/Epzx3q7MBwAJ
    
    TAG=agy
    CONV=8a29dc4d-cbb7-45d9-b0ec-f9ba0a26158a
    
    Bug: 449760252
    Change-Id: Ia9da4f6940cc11813a552338a5068ec2799f3228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139880
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Owners-Override: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667924}

---
+++ third_party/blink/web_tests/external/wpt/cpu-performance/cpu-performance.https.window.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+test(function() {
+  assert_equals(typeof navigator.cpuPerformance, 'number',
+                'navigator.cpuPerformance returns a number');
+  assert_true(Number.isInteger(navigator.cpuPerformance),
+              'navigator.cpuPerformance returns an integer value');
+  assert_true(navigator.cpuPerformance >= 0,
+              'navigator.cpuPerformance returns a non-negative value');
+}, 'navigator.cpuPerformance is valid');
--- third_party/blink/web_tests/virtual/cpu-performance/README.md
+++
@@ -1,3 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-These are virtual tests for the CPU Performance API.
-They run with the flag --enable-features=CpuPerformance.
