473d4d7cfdfab37d750b195f2b7e9d302396e50f
Tomasz Wiszkowski <ender@google.com>
2026-07-24 13:28:39 -0700

    Fix incorrect window focus tracking in Autocomplete.
    The Autocomplete was previously tracking top activity instead of its
    embedding window focus. This technically belongs in LocationBar now
    that we have proper begin/endInput calls, but for now - this fixes
    suggestions not showing at all, or showing in the wrong window.
    
    Change-Id: If9c4e09e1b4d2e02068a80f4857deff2228545fa
    Fixed: 532032970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142242
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668133}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxPopupUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxPopupUnitTest.java
@@ -233,6 +233,7 @@ public class FuseboxPopupUnitTest {
     @Test
     public void testSetPopupState_Bottom() {
         mFuseboxPopup.setPopupState(PopupState.BOTTOM);
+        Shadows.shadowOf(Looper.getMainLooper()).idle();
         verify(mDynamicRectProvider).setPopupState(PopupState.BOTTOM);
         verify(mPopupWindow).show();
     }
