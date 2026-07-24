5bdaf11acc61531640ed671bdfd20c66f736235d
Aisulu R <aisulu@google.com>
2026-07-24 10:03:28 -0700

    Context: Desktop setting: use new settings path "enhancedAutofill"
    The old path naming is now obsolete.
    
    Screencast: http://go/scrcast/NTc3NjgwODc4Mjg1NjE5Mnw4YWQ2MjA0OC1iMw (main navigation, incl. a refresh after navigation)
    Screencast: http://go/scrcast/NDY5ODU3NzQ2ODEyOTI4MHw5NTQzNmU5MS02Zg (various entry points, incl. a refresh after navigation)
    Bug: b:530912967
    Change-Id: Ic2e0a34316284b7fcd84ca944b6372df85de5d36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8125798
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Aisulu Rakhmetullina <aisulu@google.com>
    Reviewed-by: Stanislav Mikheyev <mikhe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667942}

--- chrome/test/data/webui/settings/travel_page_test.ts
+++ chrome/test/data/webui/settings/travel_page_test.ts
@@ -385,8 +385,7 @@ suite('TravelPage', function() {
       assertTrue(!!button);
 
       button.click();
-      assertEquals(
-          '/suggestionsFromGemini', Router.getInstance().currentRoute.path);
+      assertEquals('/enhancedAutofill', Router.getInstance().currentRoute.path);
       const action = await metricsBrowserProxy.whenCalled('recordAction');
       assertEquals(
           'PersonalContext.Settings.EntryPoint.TravelSettings', action);
