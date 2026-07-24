88407adf2fdaedada564aab31cd897c623e240e0
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-07-23 20:25:02 -0700

    Revert "Disable ContextualSearch on Desktop."
    This reverts commit da29afa141266431182c92406d8b92bbcaf0cb26.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4906988824690688
    
    Sample build with failed test: https://ci.chromium.org/b/8675439867582681713
    Affected test(s):
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchCriticalTest#testResolveCausesOneLowPriorityRequest](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchCriticalTest%23testResolveCausesOneLowPriorityRequest?q=VHash%3A9d2db2838dc1d70b)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchHistogramTest#testRelatedSearchesItemNotSelected](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchHistogramTest%23testRelatedSearchesItemNotSelected?q=VHash%3A9d2db2838dc1d70b)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchHistogramTest#testRelatedSearchesItemNotSelectedUKMEnabled](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchHistogramTest%23testRelatedSearchesItemNotSelectedUKMEnabled?q=VHash%3A9d2db2838dc1d70b)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchInstrumentationTest#testNonResolveGesture](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchInstrumentationTest%23testNonResolveGesture?q=VHash%3A9d2db2838dc1d70b)
    [://chrome/android\:chrome_public_test_apk!junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchInstrumentationTest#testPrivacyNeutralPeekExpandMaximize](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Fandroid%5C:chrome_public_test_apk%21junit:org.chromium.chrome.browser.contextualsearch:ContextualSearchInstrumentationTest%23testPrivacyNeutralPeekExpandMaximize?q=VHash%3A9d2db2838dc1d70b)
    and 21 more ...
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4906988824690688&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8128244&type=BUG
    
    Original change's description:
    > Disable ContextualSearch on Desktop.
    >
    > Change-Id: I92a3fece7f19661d5e92cb2026746361098f550a
    > Fixed: 535990436
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128244
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1667494}
    >
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I20df9c77bfe8ffe3d8f167c9f871c05b47f77aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141803
    Owners-Override: Taiyo Mizuhashi <taiyo@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667617}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -1433,10 +1433,6 @@ public class RootUiCoordinator
     }
 
     protected boolean isContextualSearchEnabled() {
-        // Caution: this cannot return anything else. A lot of code implicitly assumes the
-        // mContextualSearchManager is always available.
-        // Only special cases can currently override this value - ones where no logic ever
-        // calls `assumeNonNull` on `mContextualSearchManagerSupplier`.
         return true;
     }
 
