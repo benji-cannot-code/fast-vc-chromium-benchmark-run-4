19569c5263b311410a1cae27b8fd2b09694dd7f8
Evan Luo <evanluo@google.com>
2026-07-24 14:56:11 -0700

    [bookmarks] Fix NPE on bookmarks manager Open in incognito tab
    When adding logic for opening in tab group, missed a firstTab != null
    check that is causing a NullPointerException when attempting to Open in
    incognito tab from Bookmarks Manager.
    
    Bug: 538002508
    Change-Id: Ia7446bbba37294b276c9b2b1c7f4db4fd92d90e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8144123
    Auto-Submit: Evan Luo <evanluo@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668186}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -5059,7 +5059,7 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
             }
 
             TabModel tabModel =
-                    mTabModelSelector != null
+                    mTabModelSelector != null && firstTab != null
                             ? mTabModelSelector.getModel(firstTab.isIncognito())
                             : null;
             if (tabModel != null) {
