63ae890e083539408fb9d4a0749337a38e29d615
Timofey Chudakov <tchudakov@google.com>
2026-07-24 01:27:16 -0700

    [AutofillAi] Force enablement respects account requirements.
    This CL fixes the issue when the Ambient Autofill cache is not
    wiped out when the user is signed out and the force enablement
    is on.
    
    The root cause is the discrepancy between the GetEligibilityState()
    and eligibility_state_ in the PersonalContextEligibilityService:
    
    * eligibility_state_ always returns true when the force enablement
    is on.
    * eligibility_state_ stores the real value of the user's eligibility
    for the feature, which is `false` in practice.
    
    When the user signs out, the UpdateEligibilityState compares the new
    eligibility state (which is `false` because the user has signed out)
    and the eligibility_state_ (which is `false` as well). Since there's
    no change, the `OnEligibilityStateChanged` callback is not invoked
    => so the cache is not wiped out.
    
    This issue is fixed by moving the force enablement feature check from
    the GetEligibilityState() to ComputeEligibilityState() so that the
    private member eligibility_state_ always stores the real value. This
    change also modifies the behavior of the force enablement feature,
    which now respects account requirements.
    
    The reason why this bug happens only on Clank is because the service
    is recreated when when user signs out on Desktop.
    
    Fixed: 537368900
    Change-Id: I0e513e76545fddf316b2e4192c9638acaf215dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137281
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667704}

--- components/personal_context/core/personal_context_eligibility_service_impl_test_api.h
+++ components/personal_context/core/personal_context_eligibility_service_impl_test_api.h
@@ -22,9 +22,16 @@ class PersonalContextEligibilityServiceImplTestApi {
     return service_->ComputeEligibilityState().first;
   }
 
+  void UpdateEligibilityState() { service_->UpdateEligibilityState(); }
+
  private:
   const raw_ref<PersonalContextEligibilityServiceImpl> service_;
 };
+
+inline PersonalContextEligibilityServiceImplTestApi test_api(
+    PersonalContextEligibilityServiceImpl& service) {
+  return PersonalContextEligibilityServiceImplTestApi(&service);
+}
 
 }  // namespace personal_context
 
