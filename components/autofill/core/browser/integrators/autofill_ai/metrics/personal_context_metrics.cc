5fe1b18a66836a681aa2ce7083f8139b6c46cd00
Vidhan Jain <vidhanj@google.com>
2026-07-24 07:37:07 -0700

    Move prefetch latency and eligibility logging to personal_context_metrics
    Change-Id: I357b5d310e21ff25ff63403cf4e3942db29d0811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139663
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667841}

--- components/autofill/core/browser/integrators/autofill_ai/metrics/personal_context_metrics.cc
+++ components/autofill/core/browser/integrators/autofill_ai/metrics/personal_context_metrics.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
+#include "base/strings/strcat.h"
 #include "components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
+#include "components/autofill/core/browser/integrators/autofill_ai/metrics/autofill_ai_metrics.h"
 #include "components/autofill/core/browser/network/autofill_ai/autofill_ai_personal_context_access_manager.h"
 
 namespace autofill {
@@ -69,6 +71,20 @@ void LogPersonalContextPrefetchTriggerResults(
     base::UmaHistogramEnumeration(
         "Autofill.Ai.PersonalContext.Prefetch.TriggerResult", trigger_result);
   }
+}
+
+void LogPersonalContextPrefetchTotalLatency(EntityType type,
+                                            base::TimeDelta latency) {
+  base::UmaHistogramMediumTimes(
+      base::StrCat({"Autofill.Ai.PersonalContext.Prefetch.TotalLatency.",
+                    EntityTypeToMetricsString(type)}),
+      latency);
+}
+
+void LogPersonalContextNonEligibilityReason(
+    personal_context::PersonalContextNonEligibilityReason reason) {
+  base::UmaHistogramEnumeration(
+      "Autofill.Ai.PersonalContext.NonEligibilityReason", reason);
 }
 
 }  // namespace autofill
