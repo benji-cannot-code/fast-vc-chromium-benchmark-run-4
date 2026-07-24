9ff3f77ac8b407f6cbe3792139b0b0ed8fd5f625
Kevin <kevinshah@google.com>
2026-07-23 18:52:03 -0700

    Revert "[Contextual Tasks] Fix zooming issue for AL"
    This reverts commit c3d3e9fb9dd2126cbdb391a07ee7f9a5a980e24e.
    
    Reason for revert: This CL causes regression in composebox positoining.
    
    Failure Link: https://b.corp.google.com/issues/537473345
    
    Original change's description:
    > [Contextual Tasks] Fix zooming issue for AL
    >
    > Enable zoom level synchronization unconditionally across platforms. This
    > CL removes !BUILDFLAG(IS_ANDROID) guards around HostZoomMap
    > subscriptions and zoom methods in ContextualTasksUI, enabling 3-dot menu
    > and shortcut zoom for Desktop Android.
    > https://screencast.googleplex.com/cast/NTM3ODU0NTEyMjU0MTU2OHw2OWYwY2Q3Ni0xZg
    > https://screencast.googleplex.com/cast/NDg5MjI1NzI0ODM0NjExMnxhNzUyZjY5NS01Yg
    >
    > Bug: b:531632845, b:534422357
    > Change-Id: I29e1e5373a04d0349b0256088c154515d39d498f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127260
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Commit-Queue: Kevin Shah <kevinshah@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1665164}
    
    Bug: b:531632845, b:534422357
    Change-Id: Ic18d7608ebbe28acb4c332bd500ae2e156f945a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139666
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Kevin Shah <kevinshah@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667588}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -106,7 +106,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #endif
 
-#include "components/zoom/zoom_controller.h"  // nogncheck
 #include "components/omnibox/common/omnibox_features.h"
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -114,6 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/lens/lens_search_controller.h"
 #include "chrome/browser/ui/views/user_education/browser_help_bubble.h"
 #include "components/omnibox/browser/searchbox.mojom-forward.h"
+#include "components/zoom/zoom_controller.h"  // nogncheck
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/resource/resource_scale_factor.h"
 #endif
@@ -1362,7 +1362,9 @@ void ContextualTasksUI::OnSidePanelStateChanged() {
 
   PostMessageToWebview(message);
 
+#if !BUILDFLAG(IS_ANDROID)
   UpdateZoom();
+#endif
 }
 
 void ContextualTasksUI::OnLensOverlayStateChanged(
@@ -1554,8 +1556,10 @@ void ContextualTasksUI::PushTaskDetailsToPage(std::optional<base::Uuid> id,
     page_->SetTaskDetails(id.value_or(base::Uuid()), url,
                           replace_navigation_entry);
   }
+#if !BUILDFLAG(IS_ANDROID)
   tracked_zoom_host_ = url.host();
   UpdateZoom();
+#endif
 }
 
 bool ContextualTasksUI::CanExpandToFullTab() const {
@@ -1998,7 +2002,6 @@ base::RefCountedMemory* ContextualTasksUI::GetFaviconResourceBytes(
           IDR_NTP_FAVICON, scale_factor));
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 }
-#endif  // !BUILDFLAG(IS_ANDROID)
 
 void ContextualTasksUI::SyncZoom(bool site_to_webui) {
   if (tracked_zoom_host_.empty()) {
@@ -2068,5 +2071,7 @@ void ContextualTasksUI::OnZoomLevelChanged(
     SyncZoom(/*site_to_webui=*/false);
   }
 }
+
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 WEB_UI_CONTROLLER_TYPE_IMPL(ContextualTasksUI)
