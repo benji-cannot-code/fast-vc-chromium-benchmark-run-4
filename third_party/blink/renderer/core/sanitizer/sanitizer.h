d39d19d2fbe8736db778bff108008eac620c24dc
Daniel Vogelheim <vogelheim@chromium.org>
2026-07-24 01:14:11 -0700

    [Sanitizer] Rework default handling for PIs in Sanitizer
    Per spec, Sanitizer (unsafe or empty config) defaults to
    `removeProcessingInstructions: []`, while the "default" config uses
    `processingInstructions: []`. We update the handling to match this.
    
    As this refers to an updated spec, I'm also re-numbering the
    "spec steps" in the affected method.
    
    Bug: 537790295
    Change-Id: I695ce9a4a4a8258c6630fb2aed5c43f6e5a93a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139242
    Reviewed-by: Noam Rosenthal <nrosenthal@google.com>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667694}

--- third_party/blink/renderer/core/sanitizer/sanitizer.h
+++ third_party/blink/renderer/core/sanitizer/sanitizer.h
@@ -67,6 +67,8 @@ class CORE_EXPORT Sanitizer final : public ScriptWrappable {
             std::unique_ptr<SanitizerNameSet>,
             SanitizerNameMap,
             SanitizerNameMap,
+            std::unique_ptr<HashSet<AtomicString>>,
+            std::unique_ptr<HashSet<AtomicString>>,
             bool,
             bool);
 
