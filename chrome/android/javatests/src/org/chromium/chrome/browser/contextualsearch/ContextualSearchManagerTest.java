9330d4551904fe320c28886505690a3434ac662e
Tomasz Wiszkowski <ender@google.com>
2026-07-24 13:08:23 -0700

    Reland "Disable ContextualSearch on Desktop."
    This reverts commit b92d891e411623d14ebab771fabca485f604df14.
    
    Reason for revert: make sure CQ runs tests rather than just stamp CL.
    
    Original change's description:
    > Revert "Disable ContextualSearch on Desktop."
    >
    > This reverts commit da29afa141266431182c92406d8b92bbcaf0cb26.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test
    > failure.
    >
    > Sample build with failed test: https://ci.chromium.org/b/8675439867582681713
    > Original change's description:
    > > Disable ContextualSearch on Desktop.
    > >
    > > Change-Id: I92a3fece7f19661d5e92cb2026746361098f550a
    > > Fixed: 535990436
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128244
    > > Reviewed-by: Sky Malice <skym@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1667494}
    > >
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I20df9c77bfe8ffe3d8f167c9f871c05b47f77aed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141803
    > Owners-Override: Taiyo Mizuhashi <taiyo@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1667617}
    
    Include-Ci-Only-Tests: true
    Change-Id: I82570c767ffad0eef07053218e900a7dd162f0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141389
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668120}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -97,7 +97,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-@DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511287853
+@DisableIf.Device(DeviceFormFactor.DESKTOP) // Explicitly not supported.
 // TODO(donnd): Create class with limited API to encapsulate the internals of simulations.
 
 /** Tests the Contextual Search Manager using instrumentation tests. */
@@ -751,7 +751,6 @@ public class ContextualSearchManagerTest extends ContextualSearchInstrumentation
     @Test
     @SmallTest
     @Feature({"ContextualSearch"})
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481444937
     public void testContextualDictionaryDefinitions() throws Exception {
         runDictionaryCardTest(CardTag.CT_CONTEXTUAL_DEFINITION);
     }
@@ -932,7 +931,6 @@ public class ContextualSearchManagerTest extends ContextualSearchInstrumentation
     // TODO(crbug.com/473893732): Update the test for lock top control or use restriction.
     @DisableFeatures(ChromeFeatureList.LOCK_TOP_CONTROLS_ON_LARGE_TABLETS_V2)
     // Previously flaky and disabled 4/2021.  https://crbug.com/40750236
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481444937
     public void testTriggeringContextualSearchHidesFindInPageOverlay() throws Exception {
         MenuUtils.invokeCustomMenuActionSync(
                 InstrumentationRegistry.getInstrumentation(),
