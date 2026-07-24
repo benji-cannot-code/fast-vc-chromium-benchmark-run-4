c8068415446584872e11cd2b7e0cff93aa121099
Timofey Chudakov <tchudakov@google.com>
2026-07-24 09:24:59 -0700

    [AutofillAi] Implement loading dialog.
    This CL adds an API to show a loading dialog with no buttons.
    
    Screencast: http://shortn/_oD4ddNEek8
    
    Will be implemented in the next CLs:
    * A method to dismiss the dialog (the dialog will be dismissed
    from C++ because it has no buttons the user can click).
    * A way to trigger the dialog from chrome_autofill_client
    * A trigger point in the AutofillAiAccessManager.
    
    Bug: 536814322
    Change-Id: I409181ac4f5f19d5937f39f18eec24f24a8d52a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8143219
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667910}

--- chrome/browser/ui/android/autofill/dialog/autofill_dialog_view_android.h
+++ chrome/browser/ui/android/autofill/dialog/autofill_dialog_view_android.h
@@ -23,6 +23,7 @@ class AutofillDialogViewAndroid : public AutofillDialogView {
   ~AutofillDialogViewAndroid() override;
 
   void Show() override;
+  void ShowLoadingDialog() override;
   void Dismiss() override;
 
   // --------------------------------------------------------------------------
@@ -32,6 +33,8 @@ class AutofillDialogViewAndroid : public AutofillDialogView {
   void OnDismissed(JNIEnv* env);
 
  private:
+  void InitJavaObject();
+
   raw_ref<AutofillDialogController> controller_;
   // The corresponding java object.
   base::android::ScopedJavaGlobalRef<jobject> java_object_;
