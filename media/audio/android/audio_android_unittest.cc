963837c201ac21323a663d4830b68e99bdc07f34
Thomas Guilbert <tguilbert@chromium.org>
2026-07-24 12:37:33 -0700

    Reland "Enable kAAudioPerStreamDeviceSelection by default"
    This reverts commit de92b900b2385efee8e9b2cba2bcdaa595487c4b, relanding
    the CL which turns on kAAudioPerStreamDeviceSelection by default.
    
    The original CL caused the `setSinkId()` API to unexpectedly launch on
    Android as well. This API is tied to the `EnableAudioOutputDevices`
    runtime flag, which is conditionally set by
    kAAudioPerStreamDeviceSelection on Android. This CL now ensures the API
    is only set on desktop.
    
    Original change's description:
    > Revert "Enable kAAudioPerStreamDeviceSelection by default"
    >
    > This reverts commit b725e6c6c12d58a0c801d2537170a30f1d8464dc.
    >
    > Reason for revert: Causing test failure in external/wpt/audio-output/idlharness.https.window.html
    >
    > Failure Link: see b/537839417
    >
    > Original change's description:
    > > Enable kAAudioPerStreamDeviceSelection by default
    > >
    > > This CL enables the kAAudioPerStreamDeviceSelection flag by default, but
    > > gates the feature for desktop versions of android.
    > >
    > > This matches the current behavior of the Finch experiments, currently
    > > rolled out to 100% on desktop Android devices.
    > >
    > > Bug: 425608358
    > > Change-Id: Id8f929dfd19a64c683941b04ab5a71d83cdcc109
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8111797
    > > Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    > > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1665782}
    >
    > Bug: 425608358
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I092beb5095736cec86775ad1a32fadf23d3000aa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8135260
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Hailey Wang <haileywang@google.com>
    > Owners-Override: Hailey Wang <haileywang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1666603}
    
    Bug: 425608358
    Change-Id: I38a501ffbe2470a61baae8f5d670423587af5fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131033
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668092}

--- media/audio/android/audio_android_unittest.cc
+++ media/audio/android/audio_android_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/android/device_info.h"
 #include "base/compiler_specific.h"
 #include "base/containers/heap_array.h"
 #include "base/containers/span.h"
@@ -465,6 +466,12 @@ class AudioAndroidOutputTest : public testing::TestWithParam<AudioApi> {
         break;
       case AudioApi::OpenSLES:
         break;
+    }
+
+    if (enable_aaudio_per_stream_device_selection &&
+        !base::android::device_info::is_desktop()) {
+      GTEST_SKIP()
+          << "Per-stream device selection is only supported on desktop.";
     }
 
     if (!enable_aaudio) {
@@ -1216,6 +1223,15 @@ TEST_P(AudioAndroidInputTest, OpenAndCloseInputStream) {
 // closed, emitting a histogram value for successfully setting the
 // device ID if AAudioWithPerStreamDeviceSelection is enabled.
 TEST_P(AudioAndroidInputTest, OpenAndCloseInputStreamWithDevice) {
+  const bool is_per_stream_selection_enabled =
+      GetParam() == AudioApi::AAudioWithPerStreamDeviceSelection;
+
+  if (is_per_stream_selection_enabled &&
+      !base::android::device_info::is_desktop()) {
+    GTEST_SKIP() << "Per-stream device selection is only supported on "
+                 << "desktop.";
+  }
+
   std::optional<AudioDeviceDescription> device =
       GetFirstNonDefaultInputDevice();
   if (!device.has_value()) {
@@ -1227,7 +1243,7 @@ TEST_P(AudioAndroidInputTest, OpenAndCloseInputStreamWithDevice) {
   base::HistogramTester histogram_tester;
   OpenAudioInputStreamOnAudioThread();
 
-  if (GetParam() == AudioApi::AAudioWithPerStreamDeviceSelection) {
+  if (is_per_stream_selection_enabled) {
     constexpr std::string_view kHistogramPrefix =
         "Media.Audio.Android.AAudioSetDeviceId.Input.";
     const std::string kSuccessHistogram =
