6ed05117fbda7693a7e2f61da5534d491d766b89
Gazal <agazal@google.com>
2026-07-27 14:19:16 -0700

    [Code Health] Optimize logging in media/base/android
    Convert some log statements in the media/base/android directory to use
    format specifiers. This avoids runtime string concatenation and
    StringBuilder overhead in non-production log statements.
    
    Bug: 529082276
    Change-Id: I34b0f8d40db14bb1f816985f7f85b4023362cf12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8146028
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668979}

--- media/base/android/java/src/org/chromium/media/MediaDrmBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaDrmBridge.java
@@ -535,7 +535,7 @@ public class MediaDrmBridge {
         Log.d(TAG, "Set origin: %s", origin);
 
         if (!isWidevine()) {
-            Log.d(TAG, "Property " + ORIGIN + " isn't supported");
+            Log.d(TAG, "Property %s isn't supported", ORIGIN);
             return true;
         }
 
@@ -1776,7 +1776,7 @@ public class MediaDrmBridge {
                                 return;
                             }
 
-                            Log.d(TAG, "SessionLost: " + sessionId);
+                            Log.d(TAG, "SessionLost: %s", sessionId);
                             if (mMediaDrm != null) {
                                 closeSessionNoException(sessionId);
                             }
