04a5ec12a4e95c34a5c71d7fdaa437d7b45a7efa
Eric Ekey <ericekey@google.com>
2026-07-27 07:09:08 -0700

    [iOS][AppBar] Restore Gemini Floaty after Closing History Sheet
    This change fixes a bug where the collapsed Gemini floaty would not be
    restored after opening and closing the History sheet.
    
    Bug: 537794294
    Change-Id: I8e0c811d57ff2ee79f41544b19c3b7dedb1ecdcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8140999
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668621}

--- ios/chrome/browser/history/ui_bundled/history_coordinator_impl.mm
+++ ios/chrome/browser/history/ui_bundled/history_coordinator_impl.mm
@@ -66,8 +66,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Private
 
 - (void)dismissHistoryNavigationWithCompletion:(ProceduralBlock)completion {
-  [_historyNavigationController dismissViewControllerAnimated:YES
-                                                   completion:completion];
+  [_historyNavigationController.presentingViewController
+      dismissViewControllerAnimated:YES
+                         completion:completion];
   _historyNavigationController = nil;
   _viewController.historyService = nullptr;
 }
