709777915bd5df1a905daebc829d8633c60c94b0
Dmytro Yeroshkin <yeroshkin@chromium.org>
2026-07-27 13:27:26 -0700

    Set SearchEngineExplicitChoiceDialog enabled by default
    Test removed because it required `SearchEngineUnconditionalDialog` to be disabled.
    
    Bug: 529868409
    Change-Id: If085f63fdbed1ea224473caa7caea8692425573e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142064
    Commit-Queue: Dmytro Yeroshkin <yeroshkin@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668928}

--- chrome/browser/ui/search_engines/default_search_extension_controlled_controller_unittest.cc
+++ chrome/browser/ui/search_engines/default_search_extension_controlled_controller_unittest.cc
@@ -253,6 +253,10 @@ TEST_F(DefaultSearchExtensionControlledControllerTest,
 
 TEST_F(DefaultSearchExtensionControlledControllerTest,
        ShouldRequestConfirmationFalseForSimpleOverrideExtension) {
+  base::test::ScopedFeatureList features;
+  features.InitAndDisableFeature(
+      extensions_features::kSearchEngineUnconditionalDialog);
+
   // Use the Simple extension helper (no extra permissions).
   auto extension =
       AddSimpleEnabledExtension(&profile_, "Simple Extension", kExtensionId);
