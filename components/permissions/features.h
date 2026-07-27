7fa2536352f62efaf82cb94c80542cc37ac2542b
Antonio Sartori <antoniosartori@chromium.org>
2026-07-27 08:03:27 -0700

    [permissions] Account for same-origin navigations in gesture-gating
    This CL adds a new field-trial param for
    PermissionsGestureGatedPrompts that excludes gestureless prompts for
    muting if the page has navigated same-origin (since that same-origin
    navigation most likely occurred because of a user gesture).
    
    Bug: 412962300
    Change-Id: If59be68a24a2c1ceea610e1a17fdf7f9211a94c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8153939
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668639}

--- components/permissions/features.h
+++ components/permissions/features.h
@@ -194,6 +194,10 @@ COMPONENT_EXPORT(PERMISSIONS_COMMON)
 extern const base::FeatureParam<bool>
     kPermissionsGestureGatedPromptsMuteGeolocation;
 
+COMPONENT_EXPORT(PERMISSIONS_COMMON)
+extern const base::FeatureParam<bool>
+    kPermissionsGestureGatedPromptsExcludeSameOriginNavigations;
+
 #if !BUILDFLAG(IS_ANDROID)
 COMPONENT_EXPORT(PERMISSIONS_COMMON)
 extern const base::FeatureParam<bool> kKeyboardLockPromptUIStyle;
