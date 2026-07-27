ee5ad43dd81a63d7476ea9aa454e2a163d918cf9
David Sanders <dsanders11@ucsbalum.com>
2026-07-27 16:23:45 -0700

    Refactor WebTextCheckingResult default constructor to =default
    Currently `should_hide_suggestion_menu` is left uninitialized in the
    default constructor (this was flagged by a UBSan build), so switch to
    default member initialization and change the default constructor to
    =default.
    
    Change-Id: I1faed68b9755ca13bb332774e184c3f23ac9c269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8151924
    Auto-Submit: David Sanders <dsanders11@ucsbalum.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669072}

--- third_party/blink/public/web/web_text_checking_result.h
+++ third_party/blink/public/web/web_text_checking_result.h
@@ -44,8 +44,7 @@ struct TextCheckingResult;
 
 // A checked entry of text checking.
 struct WebTextCheckingResult {
-  WebTextCheckingResult()
-      : decoration(kWebTextDecorationTypeSpelling), location(0), length(0) {}
+  WebTextCheckingResult() = default;
 
   WebTextCheckingResult(
       WebTextDecorationType decoration,
@@ -63,11 +62,11 @@ struct WebTextCheckingResult {
   operator TextCheckingResult() const;
 #endif
 
-  WebTextDecorationType decoration;
-  int location;
-  int length;
+  WebTextDecorationType decoration = kWebTextDecorationTypeSpelling;
+  int location = 0;
+  int length = 0;
   std::vector<WebString> replacements;
-  bool should_hide_suggestion_menu;
+  bool should_hide_suggestion_menu = false;
 };
 
 }  // namespace blink
