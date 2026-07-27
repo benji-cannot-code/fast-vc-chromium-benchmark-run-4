06fa307fb409ab9b9816bbab3e06085eb1f95307
Simon Hangl <simonha@google.com>
2026-07-27 06:20:44 -0700

    [MultiCapture] Cleanly terminate multi-capture streams on session switch
    When switching users on ChromeOS during an active multi-capture, the
    video frames are inherently no longer delivered across sessions. However,
    the stream itself was not explicitly stopped.
    
    This CL ensures these streams are properly cleaned up on session switch.
    It plumbs a `stop_callback` up to the browser layer and introduces a new
    Keyed Service, `MultiCaptureSessionController`. This controller observes
    `user_manager::UserManager::UserSessionStateObserver` and gracefully
    terminates any active captures in the background when the active user
    changes.
    
    The alternative would be to use
    `ash::Shell::Get()->system_tray_notifier()->NotifyScreenAccessStart`,
    however, this will introduce a stop button for the user. The absence
    of such a button is a key requirement for multi capture.
    
    Bug: 519006835
    Change-Id: I687eb61a4d5299dc8e0521c39d6c6f4537847369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131909
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668580}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -468,6 +468,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/dlp/dlp_download_observer_factory.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
 #include "chrome/browser/media/webrtc/multi_capture/multi_capture_data_service_factory.h"
+#include "chrome/browser/media/webrtc/multi_capture/multi_capture_session_controller_factory.h"
 #include "chrome/browser/media/webrtc/multi_capture/multi_capture_usage_indicator_service_factory.h"
 #include "chrome/browser/media_galleries/gallery_watch_manager.h"
 #include "chrome/browser/media_galleries/media_file_system_registry.h"
@@ -907,6 +908,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if BUILDFLAG(IS_CHROMEOS)
   multi_capture::MultiCaptureDataServiceFactory::GetInstance();
   multi_capture::MultiCaptureUsageIndicatorServiceFactory::GetInstance();
+  multi_capture::MultiCaptureSessionControllerFactory::GetInstance();
 
   if (chromeos::features::
           IsMicrosoftOneDriveIntegrationForEnterpriseEnabled()) {
