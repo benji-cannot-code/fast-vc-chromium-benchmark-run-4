b7c6f4c299bae0bdffda77bd26e9f4fd133372a6
Noam Rosenthal <nrosenthal@chromium.org>
2026-07-27 09:17:39 -0700

    Support <template for="..." buffer>
    The "buffer" boolean attribute makes it so that the template content
    streams into the template, and only applies to the target as a patch
    once the template is done.
    
    During streaming, the content is available in template.content.
    
    (behind the DeclalrativeFragment flag)
    
    Bug: 535664974
    Change-Id: I2ca998553e26725bb8e488f08efe2e196b2d080a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124335
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668718}

--- third_party/blink/renderer/core/html/html_template_element.cc
+++ third_party/blink/renderer/core/html/html_template_element.cc
@@ -117,7 +117,7 @@ ContainerNode* HTMLTemplateElement::InsertionTarget() const {
 
 void HTMLTemplateElement::FinishParsingChildren() {
   if (patch_) {
-    patch_->Finalize();
+    patch_->Finalize(this);
     patch_ = nullptr;
   }
 }
