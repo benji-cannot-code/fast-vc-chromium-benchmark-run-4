251c28e040463effd4ee52822a473ed4c17f07f8
thefrog <thefrog@chromium.org>
2026-07-27 15:31:44 -0700

    Log webshare Safe Browsing check result
    Logs the specific result returned by the underlying Safe Browsing check.
    This will be used for validating there are no unexpected changes to
    results to consumer calls during the v4 -> v5 migration.
    
    New histogram: WebShare.SafeBrowsingCheck.Result
    
    Bug: 362791941, 539404720
    Change-Id: I20256fee3a4cb61148bf8e8e92f9d096bc7fbf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8156739
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669037}

--- chrome/browser/webshare/safe_browsing_request.cc
+++ chrome/browser/webshare/safe_browsing_request.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/location.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_runner.h"
 #include "base/task/task_traits.h"
@@ -66,6 +67,7 @@ class SafeBrowsingRequest::SafeBrowsingClient
 
     if (database_manager_->CheckDownloadUrl({url}, this)) {
       timeout_.Stop();
+      LogCheckResult(SafeBrowsingRequest::CheckResult::kSafe);
       SendResultToHandler(/*is_url_safe=*/true);
     }
   }
@@ -76,6 +78,7 @@ class SafeBrowsingRequest::SafeBrowsingClient
 
   void OnTimeout() {
     database_manager_->CancelCheck(this);
+    LogCheckResult(SafeBrowsingRequest::CheckResult::kTimeout);
     SendResultToHandler(/*is_url_safe=*/true);
   }
 
@@ -92,7 +95,13 @@ class SafeBrowsingRequest::SafeBrowsingClient
     timeout_.Stop();
     bool is_url_safe =
         threat_type == safe_browsing::SBThreatType::SB_THREAT_TYPE_SAFE;
+    LogCheckResult(is_url_safe ? SafeBrowsingRequest::CheckResult::kSafe
+                               : SafeBrowsingRequest::CheckResult::kUnsafe);
     SendResultToHandler(is_url_safe);
+  }
+
+  void LogCheckResult(SafeBrowsingRequest::CheckResult result) {
+    base::UmaHistogramEnumeration("WebShare.SafeBrowsingCheck.Result", result);
   }
 
   base::OneShotTimer timeout_;
