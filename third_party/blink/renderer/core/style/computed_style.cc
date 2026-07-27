b508ae0f039224b99348248849c94d68522f6c58
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-07-27 16:10:35 -0700

    [layout] Fix OOF-positioned margin under-invalidation.
    In crrev.com/c/7812714 we simplified the OOF layout code by making the
    constraint-space construction the same for the two places where we
    invoke layout on a non-replaced element.
    
    This introduced an under-invaldation bug when margins change.
    
    Previously when a margin changed, we'd calculate a new fixed-block size,
    which would cause the cache-miss. Importantly for OOF-positioned we
    don't mark the layout-object as dirty when a margin changes.
    
    With the new code we would potentially just compare the
    constraint-spaces directly (without computing the block-size), causes
    the incorrect cache-hit.
    
    Skipping the margin layout invalidation likely isn't important for
    real-world use-cases so just mark a layout object as needing layout for
    this case.
    
    Additionally remove the two bespoke field-diffs:
     - "margin" just becomes "layout" for OOF.
     - "out-of-flow" switches to "inset", and now we just check if we have
       non-static position.
    
    Bug: 539404705
    Change-Id: If54326af096c8cb30308e7274c515e859760ea9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8157320
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669065}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -776,9 +776,7 @@ StyleDifference ComputedStyle::VisualInvalidationDiff(
   if (!diff.NeedsFullLayout()) {
     if (DiffNeedsFullLayout(document, other, field_diff)) {
       diff.SetNeedsFullLayout();
-    } else if ((field_diff & kOutOfFlow) && HasOutOfFlowPosition()) {
-      diff.SetNeedsPositionedLayout();
-    } else if ((field_diff & kInset) && HasInFlowPosition()) {
+    } else if ((field_diff & kInset) && GetPosition() != EPosition::kStatic) {
       diff.SetNeedsPositionedLayout();
     }
   }
@@ -979,10 +977,6 @@ bool ComputedStyle::DiffNeedsFullLayout(const Document& document,
         BorderLeftWidth() != other.BorderLeftWidth()) {
       return true;
     }
-  }
-
-  if ((field_diff & kMargin) && !HasOutOfFlowPosition()) {
-    return true;
   }
 
   if (field_diff & kStroke) {
