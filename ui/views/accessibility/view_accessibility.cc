3320ae5800863b1eb61eb28655fb90fdbf243d13
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2026-07-27 15:15:00 -0700

    [views-ax] Set the offset container for virtual view bounds
    AXVirtualView bounds are expressed in the local coordinate space of the
    View that owns the virtual tree, but the serialized node data never says
    so. Consumers therefore read those bounds as root-relative, which puts
    every virtual view at the wrong position whenever its owner View does
    not sit at the origin of the widget.
    
    This CL fixes it by caching the owner View's node id as the offset
    container on every virtual view, refreshed whenever the owner changes
    and cleared once a virtual view no longer has one.
    
    Bug: 40672441
    Change-Id: If47a47ca1b07a470e2aa34e1d9962b439aae7e0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8144130
    Reviewed-by: Elly <ellyjones@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1669029}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -159,6 +159,7 @@ void ViewAccessibility::AddVirtualChildViewAt(
 
   AXVirtualView* added_view = virtual_children_[index].get();
   added_view->OnViewHasNewAncestor(view_);
+  added_view->OnOwnerViewChanged();
 
   AXUpdateNotifier::Get()->NotifyChildAdded(added_view, this);
   FireLiveRegionChangedIfNeeded(LiveRegionEventTrigger::kAdditions);
@@ -180,6 +181,7 @@ std::unique_ptr<AXVirtualView> ViewAccessibility::RemoveVirtualChildView(
   FireLiveRegionChangedIfNeeded(LiveRegionEventTrigger::kRemovals);
 
   child->set_parent_view(nullptr);
+  child->OnOwnerViewChanged();
 
   // If the removed child (or any of its descendants) was the active descendant,
   // clear it.
