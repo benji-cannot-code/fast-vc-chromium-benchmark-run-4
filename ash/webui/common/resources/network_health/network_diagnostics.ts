8da9dca565be29d78b34574649e60101d680133e
James Hawkins <jhawkins@google.com>
2026-07-27 15:57:54 -0700

    ash/webui/common: Add declare keyword to Polymer properties (part 23)
    Add the 'declare' keyword to Polymer property declarations in common
    mixins and remaining elements to satisfy the
    @webui-eslint/polymer-property-declare rule and ensure correct property
    initialization semantics.
    
    Add documentation for focus_row_mixin describing how the `id` attribute shadow is used.
    
    R=--bypass-hooks
    
    Bug: 537935977
    Test: browser_tests --gtest_filter=*AshCommon*
    Change-Id: I8c97008bac8ba466cad879a6f4759cae40aa57bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8149039
    Auto-Submit: James Hawkins <jhawkins@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669056}

--- ash/webui/common/resources/network_health/network_diagnostics.ts
+++ ash/webui/common/resources/network_health/network_diagnostics.ts
@@ -220,7 +220,7 @@ export class NetworkDiagnosticsElement extends NetworkDiagnosticsElementBase {
     };
   }
 
-  private routines_: Routine[];
+  declare private routines_: Routine[];
 
   /**
    * Runs all supported network diagnostics routines.
