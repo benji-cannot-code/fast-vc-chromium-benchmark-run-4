78de788ade312ced1641921754b9de727ef730bb
Colin Blundell <blundell@chromium.org>
2026-07-27 04:32:03 -0700

    [Blink] Begin CRC2D::WillDraw() enforcement of having provider
    This CL introduces WillDrawWithProvider() on
    Canvas2DRecorderContext.It also does an initial migration of the 3
    callsites inside Canvas2DRecorderContext::DrawInternal() to use
    WillDrawWithProvider(). Canvas2DRecorderContext::DrawInternal() is
    only invoked by Canvas2DRecorderContext::Draw(), which first calls
    GetOrCreatePaintCanvas() and short-circuits out if it's false [1]. This
    method returns false in the canvas2d contexts if there's no
    provider [2][3], thus allowing us to add a CHECK in these contexts'
    impls of WillDrawWithProvider() that a provider is indeed present.
    
    Once all callsites are converted to use WillDrawWithProvider(), I'll
    rename it back to WillDraw() and fold the current WillDraw() into
    it. This will facilitate removing this code [4], as we'll be able to
    realize about the presence of the provider there.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_2d_recorder_context.h;l=879-882;drc=2c2e022b251a82064c60e67b17f9103162a501aa?q=Canvas2DRecorderContext::DrawInter&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc;l=485-499;drc=2c2e022b251a82064c60e67b17f9103162a501aa
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=421;drc=2c2e022b251a82064c60e67b17f9103162a501aa
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc;l=903-905;drc=2c2e022b251a82064c60e67b17f9103162a501aa?q=CanvasRenderingContext2D::Pre&ss=chromium%2Fchromium%2Fsrc
    
    Bug: 352263194
    Change-Id: I1116df4134367dfc04bdad640257a74c083298b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8143140
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668502}

--- third_party/blink/renderer/modules/csspaint/paint_rendering_context_2d.h
+++ third_party/blink/renderer/modules/csspaint/paint_rendering_context_2d.h
@@ -69,6 +69,8 @@ class MODULES_EXPORT PaintRenderingContext2D
   }
 
   void WillDraw(const gfx::Rect&, CanvasPerformanceMonitor::DrawType) final;
+  void WillDrawWithProvider(const gfx::Rect& dirty_rect,
+                            CanvasPerformanceMonitor::DrawType) final;
 
   sk_sp<PaintFilter> StateGetFilter() final;
 
