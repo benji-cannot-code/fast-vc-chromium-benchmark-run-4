348c51acca09fa39b9e77773b8bf562f03b944f6
David M <davidjm@google.com>
2026-07-27 13:57:08 -0700

    Add @RequiresRestart to testResultsBarVisibleAfterTypingText
    There are new failures on testBackKeyDoesNotDismissFindWhenImeIsPresent
    that seem to be caused by a previous failure in the batch.
    Adding @RequiresRestart to testResultsBarVisibleAfterTypingText
    to see if that fixes the problem. Failing that will add
    DisableIf to the entire class.
    
    Bug: 539532908
    Change-Id: I2960873f7155cae857b72316499cabe747d24e81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8156240
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668958}

--- chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
@@ -47,6 +47,7 @@ import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.KeyUtils;
+import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.tab.Tab;
@@ -300,6 +301,7 @@ public class FindTest {
     @Test
     @MediumTest
     @Feature({"FindInPage"})
+    @RequiresRestart("crbug.com/539532908")
     public void testResultsBarVisibleAfterTypingText() {
         mActivityTestRule.loadUrl(mActivityTestRule.getTestServer().getURL(FILEPATH));
         findInPageFromMenu();
