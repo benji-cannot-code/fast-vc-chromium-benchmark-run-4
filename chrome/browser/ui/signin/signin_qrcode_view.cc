64156db8970a6a41e074e555b0639567e799de09
Duong Dac <ddac@chromium.org>
2026-07-27 03:10:18 -0700

    [MagiChrome] Align QRCode banner texts to the left
    Previously, the texts become centered once they wrap around.
    
    Bug: 507711973
    Change-Id: I22eb7f02904c1bbbf85f34a6294c4a414f4675da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138491
    Reviewed-by: Monica Salama <msalama@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668455}

--- chrome/browser/ui/signin/signin_qrcode_view.cc
+++ chrome/browser/ui/signin/signin_qrcode_view.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/text_constants.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/throbber.h"
@@ -72,13 +73,15 @@ SigninQRCodeView::SigninQRCodeView() {
                           .SetText(l10n_util::GetStringUTF16(
                               IDS_SIGNIN_QRCODE_BANNER_TITLE))
                           .SetTextContext(views::style::CONTEXT_DIALOG_TITLE)
-                          .SetTextStyle(views::style::STYLE_HEADLINE_4),
+                          .SetTextStyle(views::style::STYLE_HEADLINE_4)
+                          .SetHorizontalAlignment(gfx::ALIGN_LEFT),
                       views::Builder<views::Label>()
                           .SetText(l10n_util::GetStringUTF16(
                               IDS_SIGNIN_QRCODE_BANNER_DESCRIPTION))
                           .SetTextContext(views::style::CONTEXT_LABEL)
                           .SetTextStyle(views::style::STYLE_BODY_3)
-                          .SetMultiLine(true)))
+                          .SetMultiLine(true)
+                          .SetHorizontalAlignment(gfx::ALIGN_LEFT)))
           .Build());
 }
 
