ddbfb796c823019532a85fb409bf0c7254eb9149
Gauthier Ambard <gambard@chromium.org>
2026-07-27 09:12:01 -0700

    [iOS] Only activate selection mode constraints when needed
    Before the user enter selection mode, there is no need to activate the
    related constraints.
    
    Bug: 538028422
    Change-Id: If53996ee9ba8a8963a791555ab05c8fb3c6987f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8155259
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668712}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_top_toolbar.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/toolbars/tab_grid_top_toolbar.mm
@@ -105,6 +105,10 @@ CGFloat HorizontalMargin() {
 
   // The layout guide center for this view.
   LayoutGuideCenter* _layoutGuideCenter;
+
+  // Constraints for selection mode, activated the first time selection mode is
+  // entered.
+  NSArray<NSLayoutConstraint*>* _selectionModeConstraints;
 }
 
 - (instancetype)initWithLayoutGuideCenter:
@@ -142,6 +146,11 @@ CGFloat HorizontalMargin() {
   self.selectedTabsCount = 0;
   // Reset the Select All button to its default title.
   [self configureSelectionButtonTitleSelectAll:YES];
+  if (_mode == TabGridMode::kSelection) {
+    [NSLayoutConstraint activateConstraints:_selectionModeConstraints];
+  } else {
+    [NSLayoutConstraint deactivateConstraints:_selectionModeConstraints];
+  }
   [self setButtonsForTraitCollection:self.traitCollection];
   if (mode == TabGridMode::kSearch) {
     // Focus the search bar, and make it a first responder once the user enter
@@ -314,6 +323,32 @@ CGFloat HorizontalMargin() {
 }
 
 #pragma mark - Private
+
+// Constraints for the selection mode.
+- (NSArray<NSLayoutConstraint*>*)constraintsForSelectionModeWithContainerView:
+    (UIView*)containerView {
+  NSLayoutConstraint* centeredLabelConstraint =
+      [_selectedTabsLabel.centerXAnchor
+          constraintEqualToAnchor:containerView.centerXAnchor];
+  centeredLabelConstraint.priority = UILayoutPriorityDefaultHigh;
+
+  return @[
+    // Horizontal layout:
+    [_selectAllButton.leadingAnchor
+        constraintEqualToAnchor:containerView.leadingAnchor
+                       constant:HorizontalMargin()],
+    centeredLabelConstraint,
+    [_selectedTabsLabel.leadingAnchor
+        constraintGreaterThanOrEqualToAnchor:_selectAllButton.trailingAnchor
+                                    constant:HorizontalMargin()],
+    [_selectedTabsLabel.trailingAnchor
+        constraintLessThanOrEqualToAnchor:_exitSelectionButton.leadingAnchor
+                                 constant:-HorizontalMargin()],
+    [_exitSelectionButton.trailingAnchor
+        constraintEqualToAnchor:containerView.trailingAnchor
+                       constant:-HorizontalMargin()],
+  ];
+}
 
 // Returns a new button to be used.
 - (UIButton*)createButtonWithImage:(UIImage*)image
@@ -671,29 +706,10 @@ CGFloat HorizontalMargin() {
       constraintEqualToAnchor:_exitTabGridButton.leadingAnchor
                      constant:-HorizontalMargin()];
 
-  NSLayoutConstraint* centeredLabelConstraint =
-      [_selectedTabsLabel.centerXAnchor
-          constraintEqualToAnchor:containerView.centerXAnchor];
-  centeredLabelConstraint.priority = UILayoutPriorityDefaultHigh;
-
   [NSLayoutConstraint activateConstraints:@[
     searchBarMaximumWidth,
-    [_selectAllButton.leadingAnchor
-        constraintEqualToAnchor:containerView.leadingAnchor
-                       constant:HorizontalMargin()],
     [_pageControl.centerXAnchor
         constraintEqualToAnchor:containerView.centerXAnchor],
-
-    centeredLabelConstraint,
-    [_selectedTabsLabel.leadingAnchor
-        constraintGreaterThanOrEqualToAnchor:_selectAllButton.trailingAnchor
-                                    constant:HorizontalMargin()],
-    [_selectedTabsLabel.trailingAnchor
-        constraintLessThanOrEqualToAnchor:_exitSelectionButton.leadingAnchor
-                                 constant:-HorizontalMargin()],
-    [_exitSelectionButton.trailingAnchor
-        constraintEqualToAnchor:containerView.trailingAnchor
-                       constant:-HorizontalMargin()],
 
     [_exitTabGridButton.trailingAnchor
         constraintEqualToAnchor:containerView.trailingAnchor
@@ -709,6 +725,15 @@ CGFloat HorizontalMargin() {
         constraintLessThanOrEqualToAnchor:containerView.trailingAnchor
                                  constant:-HorizontalMargin()],
   ]];
+
+  _selectionModeConstraints =
+      [self constraintsForSelectionModeWithContainerView:containerView];
+
+  if (_mode == TabGridMode::kSelection) {
+    [NSLayoutConstraint activateConstraints:_selectionModeConstraints];
+  } else {
+    [NSLayoutConstraint deactivateConstraints:_selectionModeConstraints];
+  }
 
   [self setButtonsForTraitCollection:self.traitCollection];
 }
