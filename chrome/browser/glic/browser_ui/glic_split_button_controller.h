4919e387b660e0fd3c2d527a55b55befae3a95bd
Ian Wells <iwells@chromium.org>
2026-07-27 16:46:15 -0700

    GlicSplitButtonController: deduplicate GlicButton click handling
    TabStripActionContainer and ToolbarView duplicate some code in their
    OnGlicButtonClicked methods. I believe any differences that have crept
    in aren't intentional. Centralize this code in
    GlicSplitButtonController.
    
    Context: https://docs.google.com/document/d/1xGsDDmHERiSTH1JXUYrRKNMTnobkDShGJ8EBQSQIHFA/edit?usp=sharing
    Bug: 511309088
    Change-Id: I49717c2f7b028076a848640cff5dc78eb2200afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118631
    Reviewed-by: Vince Lugli <lugli@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669085}

--- chrome/browser/glic/browser_ui/glic_split_button_controller.h
+++ chrome/browser/glic/browser_ui/glic_split_button_controller.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
+#include "chrome/browser/glic/host/glic.mojom.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
 
 class ActorTaskListBubbleController;
@@ -39,9 +40,15 @@ class GlicSplitButtonController {
 
   ~GlicSplitButtonController();
 
+  // TODO(crbug.com/511309088): Rename these to toolbar and tab strip delegate
+  // since they no longer necessarily correspond to vertical tab mode.
   void SetHorizontalTabsDelegate(GlicSplitButtonDelegate* delegate);
   void SetVerticalTabsDelegate(GlicSplitButtonDelegate* delegate);
   base::WeakPtr<GlicSplitButtonController> GetWeakPtr();
+
+#if !BUILDFLAG(IS_ANDROID)
+  void OnGlicButtonClicked();
+#endif
 
   GlicSplitButtonDelegate* GetActiveDelegate();
   void CallOnBoth(base::RepeatingCallback<void(GlicSplitButtonDelegate&)> fn);
@@ -59,9 +66,14 @@ class GlicSplitButtonController {
 #endif
 
  private:
+  bool IsToolbarButton() const;
+  mojom::InvocationSource GetInvocationSource(
+      GlicSplitButtonDelegate& delegate) const;
+
   const raw_ptr<BrowserWindowInterface> browser_;
   raw_ptr<GlicSplitButtonDelegate> horizontal_tabs_delegate_ = nullptr;
   raw_ptr<GlicSplitButtonDelegate> vertical_tabs_delegate_ = nullptr;
+  raw_ptr<GlicKeyedService> glic_service_ = nullptr;
 
   std::unique_ptr<GlicNudgeController> glic_nudge_controller_;
 #if !BUILDFLAG(IS_ANDROID)
