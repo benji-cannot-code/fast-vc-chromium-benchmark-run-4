1291abcacfb00819040213c789fb015c3fee5996
Joel Hockey <joelhockey@chromium.org>
2026-07-26 23:07:25 -0700

    Disable testBeginInput_unfocusedWindow_standby
    Bug: 532032970
    
    Change-Id: I8223ea4edc5da7114713ff9d7db0d4aadb092211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8151445
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: ben chin <lungc@chromium.org>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Masa Fujita <massan@google.com>
    Commit-Queue: ben chin <lungc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668409}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
@@ -73,6 +73,7 @@ import org.chromium.base.supplier.SettableMonotonicObservableSupplier;
 import org.chromium.base.supplier.SettableNonNullObservableSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.RobolectricUtil;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.UserActionTester;
@@ -4363,6 +4364,7 @@ public class LocationBarMediatorTest {
     }
 
     @Test
+    @DisabledTest(message = "https://crbug.com/532032970")
     public void testBeginInput_unfocusedWindow_standby() {
         mMediator.onFinishNativeInitialization();
         mProfileSupplier.set(mProfile);
