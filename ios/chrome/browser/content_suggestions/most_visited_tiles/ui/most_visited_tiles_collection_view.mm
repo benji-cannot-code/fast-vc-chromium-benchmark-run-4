d0d644b2741d20ead4a3b9c3e03d7838cf6d4541
Chris Lu <thegreenfrog@google.com>
2026-07-27 09:00:24 -0700

    [ios] Fix MVT NTP Redesign bug
    Disables cell selection on MostVisitedTilesCollectionView when the
    kNewTabPageRedesign feature is enabled. This prevents the collection
    view selection gesture recognizer from intercepting and canceling the
    inner MVT tap gesture recognizers, restoring tap/navigation functionality.
    
    Bug: 526677926
    Change-Id: I542860940cf8146c908e1250cf478b353dc8cfb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8145601
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668705}

--- ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_tiles_collection_view.mm
+++ ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_tiles_collection_view.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_suggestions/public/content_suggestions_constants.h"
 #import "ios/chrome/browser/content_suggestions/ui/cells/content_suggestions_cells_constants.h"
 #import "ios/chrome/browser/content_suggestions/ui/content_suggestions_image_data_source.h"
+#import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.h"
 #import "ios/chrome/browser/shared/ui/util/layout_guide_names.h"
 #import "ios/chrome/browser/shared/ui/util/util_swift.h"
 #import "url/gurl.h"
@@ -74,6 +75,9 @@ const int kPlusButtonIdentifier = -1;
     self.dragDelegate = self;
     self.dropDelegate = self;
     self.dragInteractionEnabled = YES;
+    if (IsNTPRedesignEnabled()) {
+      self.allowsSelection = NO;
+    }
     [self registerClass:[UICollectionViewCell class]
         forCellWithReuseIdentifier:kCellReuseIdentifier];
     [self initializeDataSource];
