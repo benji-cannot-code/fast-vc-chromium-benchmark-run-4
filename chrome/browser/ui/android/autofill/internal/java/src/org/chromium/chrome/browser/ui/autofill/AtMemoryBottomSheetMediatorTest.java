477795e125d359a032ade98e9df26e8817a4b799
Piotr Kotynia <piotrkotynia@google.com>
2026-07-27 00:55:43 -0700

    [@memory][Android] Make search affordance have no background
    UI: https://screenshot.googleplex.com/image/7M47WmBxfuSGZqv.png
    Bug: 537236621
    Change-Id: I39962fbaa98650cca98c065a468364c3ce1aa956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8151160
    Reviewed-by: Atalyk Akash <atalyk@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668419}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetMediatorTest.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetMediatorTest.java
@@ -332,7 +332,7 @@ public class AtMemoryBottomSheetMediatorTest {
 
         mMediator.show(List.of(createSearchAffordance("flight")));
         assertEquals(1, mModelList.size());
-        assertEquals(HomeProperties.ItemType.SUGGESTION, mModelList.get(0).type);
+        assertEquals(HomeProperties.ItemType.SUGGESTION_WITH_NO_BACKGROUND, mModelList.get(0).type);
         assertEquals("flight", mModelList.get(0).model.get(TITLE));
 
         mModelList.get(0).model.get(ON_SUGGESTION_CLICKED).run();
@@ -365,7 +365,7 @@ public class AtMemoryBottomSheetMediatorTest {
     public void testOnQueryTextChanged_resumeTypingAfterEmptyQuery() {
         mMediator.show(List.of(createSearchAffordance("f")));
         assertEquals(1, mModelList.size());
-        assertEquals(HomeProperties.ItemType.SUGGESTION, mModelList.get(0).type);
+        assertEquals(HomeProperties.ItemType.SUGGESTION_WITH_NO_BACKGROUND, mModelList.get(0).type);
 
         mMediator.show(List.of());
         assertEquals(1, mModelList.size());
@@ -373,7 +373,7 @@ public class AtMemoryBottomSheetMediatorTest {
 
         mMediator.show(List.of(createSearchAffordance("a")));
         assertEquals(1, mModelList.size());
-        assertEquals(HomeProperties.ItemType.SUGGESTION, mModelList.get(0).type);
+        assertEquals(HomeProperties.ItemType.SUGGESTION_WITH_NO_BACKGROUND, mModelList.get(0).type);
         assertEquals("a", mModelList.get(0).model.get(TITLE));
     }
 
@@ -553,7 +553,7 @@ public class AtMemoryBottomSheetMediatorTest {
         mMediator.show(List.of(searchAffordance, separator, noticeSuggestion));
 
         assertEquals(2, mModelList.size());
-        assertEquals(HomeProperties.ItemType.SUGGESTION, mModelList.get(0).type);
+        assertEquals(HomeProperties.ItemType.SUGGESTION_WITH_NO_BACKGROUND, mModelList.get(0).type);
         assertEquals(HomeProperties.ItemType.NOTICE, mModelList.get(1).type);
 
         Runnable okClickListener =
