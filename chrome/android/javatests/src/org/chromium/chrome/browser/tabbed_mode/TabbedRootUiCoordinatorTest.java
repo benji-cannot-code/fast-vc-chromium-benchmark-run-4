b69035aa25a0e5c95da5a6315572dff1426fda91
Gazal <agazal@google.com>
2026-07-28 07:58:26 -0700

    [Vertical Tabs] Add default enablement parameter
    Add "enable_by_default" parameter to AndroidVerticalTabs feature. If
    true, Vertical Tabs are enabled by default for eligible users. User
    preference (toggled via settings) takes precedence over this default.
    
    Bug: 509226293
    Change-Id: I9f2536462962ccf406cd2188132c574cd2b27c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160740
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669492}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinatorTest.java
@@ -441,6 +441,8 @@ public class TabbedRootUiCoordinatorTest {
 
     @Test
     @MediumTest
+    @Restriction({DeviceFormFactor.ONLY_TABLET})
+    @EnableFeatures(ChromeFeatureList.ANDROID_VERTICAL_TABS)
     public void testToggleTabStripMetrics() {
         mPage = mActivityTestRule.startOnBlankPage();
         mTabbedRootUiCoordinator =
