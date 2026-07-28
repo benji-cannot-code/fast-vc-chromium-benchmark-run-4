842382b2956aa705af4fbdf9e312977611c63b17
Free Debreuil <freedebreuil@google.com>
2026-07-28 15:55:24 -0700

    [Blink] Add use counter for overscroll-behavior on the clip axis
    Add a usage counter for when an `overscroll-behavior` that is not `auto`
    is specified on the axis set to `clip` of a single-axis scroll
    container.
    
    This is metrics-only and does not change behavior.
    
    Fixed: 538536358
    Change-Id: I9f21c7aab3de6c04f5b1a8e2d94c07e6b53a1f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8140047
    Commit-Queue: Free Debreuil <freedebreuil@google.com>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669838}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -668,6 +668,7 @@ void StyleAdjuster::AdjustOverflow(ComputedStyleBuilder& builder,
          builder.OverflowY() != EOverflow::kVisible);
 
   bool single_axis_scroller = false;
+  bool single_axis_scroller_overscroll_behavior = false;
 
   bool overflow_is_clip_or_visible =
       IsOverflowClipOrVisible(builder.OverflowY()) &&
@@ -699,6 +700,8 @@ void StyleAdjuster::AdjustOverflow(ComputedStyleBuilder& builder,
       builder.SetOverflowX(EOverflow::kAuto);
     } else if (builder.OverflowX() == EOverflow::kClip) {
       single_axis_scroller = true;
+      single_axis_scroller_overscroll_behavior =
+          builder.OverscrollBehaviorX() != EOverscrollBehavior::kAuto;
       if (!RuntimeEnabledFeatures::SingleAxisScrollContainersEnabled()) {
         builder.SetOverflowX(EOverflow::kHidden);
       }
@@ -710,6 +713,8 @@ void StyleAdjuster::AdjustOverflow(ComputedStyleBuilder& builder,
       builder.SetOverflowY(EOverflow::kAuto);
     } else if (builder.OverflowY() == EOverflow::kClip) {
       single_axis_scroller = true;
+      single_axis_scroller_overscroll_behavior =
+          builder.OverscrollBehaviorY() != EOverscrollBehavior::kAuto;
       if (!RuntimeEnabledFeatures::SingleAxisScrollContainersEnabled()) {
         builder.SetOverflowY(EOverflow::kHidden);
       }
@@ -718,6 +723,13 @@ void StyleAdjuster::AdjustOverflow(ComputedStyleBuilder& builder,
 
   if (single_axis_scroller) {
     UseCounter::Count(document, WebFeature::kSingleAxisScroller);
+
+    // Count when an overscroll-behavior that is not `auto` overlaps the axis
+    // set to `clip` of a single-axis scroll container.
+    if (single_axis_scroller_overscroll_behavior) {
+      UseCounter::Count(document,
+                        WebFeature::kSingleAxisScrollerOverscrollBehavior);
+    }
   }
 
   if (element && !element->IsPseudoElement() &&
