70d00bd1358d715e8283a597dd1eb835e9380cda
Nicola Tommasi <tommasin@chromium.org>
2026-07-28 05:35:54 -0700

    [pContext][cleanup] Remove unused enums
    Those enums aren't used anymore, so this CL removes them.
    
    Bug: b:534722155
    Change-Id: I6524a4e66804192c3e10b981d4771dda1409ba7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161764
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669381}

--- components/personal_context/first_run/personal_context_first_run_service.h
+++ components/personal_context/first_run/personal_context_first_run_service.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/personal_context/first_run/personal_context_first_run_types.h"
 
 namespace personal_context {
 
