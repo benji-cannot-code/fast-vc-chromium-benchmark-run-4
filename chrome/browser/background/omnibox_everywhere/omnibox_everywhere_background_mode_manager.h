4dea8fe7efc749518320c614ff0257af9d74f4bb
Atharv Maan <atharvmaan@chromium.org>
2026-07-27 21:42:53 -0700

    [OmniboxEverywhere] Add OmniboxEverywhereBackgroundModeManager class
    Bug: b:532190282
    Change-Id: Iec43acf82a0ce0820a35ee911a71831d860a4215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8140443
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669200}

---
+++ chrome/browser/background/omnibox_everywhere/omnibox_everywhere_background_mode_manager.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_BACKGROUND_OMNIBOX_EVERYWHERE_OMNIBOX_EVERYWHERE_BACKGROUND_MODE_MANAGER_H_
+#define CHROME_BROWSER_BACKGROUND_OMNIBOX_EVERYWHERE_OMNIBOX_EVERYWHERE_BACKGROUND_MODE_MANAGER_H_
+
+#include "components/prefs/pref_member.h"
+
+namespace omnibox_everywhere {
+
+// Manages OmniboxEverywhere's background mode and status icon based on pref
+// state.
+class OmniboxEverywhereBackgroundModeManager {
+ public:
+  OmniboxEverywhereBackgroundModeManager();
+  OmniboxEverywhereBackgroundModeManager(
+      const OmniboxEverywhereBackgroundModeManager&) = delete;
+  OmniboxEverywhereBackgroundModeManager& operator=(
+      const OmniboxEverywhereBackgroundModeManager&) = delete;
+  ~OmniboxEverywhereBackgroundModeManager();
+
+ private:
+  void OnPrefChanged();
+
+  BooleanPrefMember background_mode_pref_member_;
+};
+
+}  // namespace omnibox_everywhere
+
+#endif  // CHROME_BROWSER_BACKGROUND_OMNIBOX_EVERYWHERE_OMNIBOX_EVERYWHERE_BACKGROUND_MODE_MANAGER_H_
