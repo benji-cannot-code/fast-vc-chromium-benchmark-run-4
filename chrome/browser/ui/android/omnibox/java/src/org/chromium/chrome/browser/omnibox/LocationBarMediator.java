07e0a62f78ce3a1911b41b9521e06e8e84e71c0c
Tomasz Wiszkowski <ender@google.com>
2026-07-28 16:40:06 -0700

    Revert "Hide standby ring when window loses focus."
    Bug: 539293180 This reverts commit
    74c50be64b1d1be9c327120e8030da1cf8f47114.
    
    Change-Id: I29a68bca81cf3940b28b6e3b2ab3b5facebb215a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161529
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669864}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -281,7 +281,6 @@ class LocationBarMediator
     private UrlBarCoordinator mUrlCoordinator;
     private GURL mOriginalUrl = GURL.emptyGURL();
     private @Nullable Animator mUrlFocusChangeAnimator;
-    private boolean mWindowHasFocus;
     private boolean mNativeInitialized;
     private boolean mUrlFocusedWithoutAnimations;
     private boolean mIsUrlFocusChangeInProgress;
@@ -368,8 +367,6 @@ class LocationBarMediator
             mPageZoomIndicatorCoordinator.setOnDismissCallbacks(
                     () -> updateZoomButtonVisibility(/* notifyEmbedder= */ true));
         }
-        Activity activity = mWindowAndroid.getActivity().get();
-        mWindowHasFocus = activity != null && activity.hasWindowFocus();
         AppBannerManager.addObserver(this);
         mScrimHandler = scrimHandler;
         if (mScrimHandler != null) {
@@ -2681,7 +2678,6 @@ class LocationBarMediator
     private void updateShowStandbyRing() {
         boolean showStandbyRing =
                 mCurrentInput != null
-                        && mWindowHasFocus
                         && mCurrentInput.getAutocompleteState() == AutocompleteState.STANDBY
                         && mSelectionController.getSelectedView() == mUrlBarSelectableView;
         mLocationBarLayout.setShowStandbyRing(showStandbyRing);
@@ -3203,14 +3199,12 @@ class LocationBarMediator
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-        mWindowHasFocus = hasFocus;
         if (!hasFocus) {
             if (mCurrentInput != null
                     && mCurrentInput.getAutocompleteState() == AutocompleteState.ENABLED) {
                 mCurrentInput.setAutocompleteState(AutocompleteState.STANDBY);
             }
         }
-        updateShowStandbyRing();
     }
 
     /* package */ void setLocationBarButtonTranslationForNtpAnimation(float translationX) {
