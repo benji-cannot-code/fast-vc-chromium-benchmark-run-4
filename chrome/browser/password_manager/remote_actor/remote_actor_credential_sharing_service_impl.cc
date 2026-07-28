a1197ac0e45f8e4d37b8fb4b2a1f227b15a6bd1c
Adem Derinel <derinel@google.com>
2026-07-28 00:47:08 -0700

    [RPS] Implement RemoteActorCredentialPermissionClient.
    This CL implements RemoteActorCredentialPermissionClient, which is
    responsible for communicating with the Agentic Permission Service (APS)
    to grant and revoke permissions for password sharing with remote actors.
    
    It sends a POST request to the APS OnePlatform update endpoint to
    authorize the agent (identified by its OAuth client ID) to access
    credentials for the target web origin.
    
    Bug: 532483845
    
    TAG=agy
    CONV=a99236f6-4532-4f52-8c3c-dc6d3c79b528
    
    Change-Id: I2707513a4d1ba84eb07f8de6b76dd5fa7c3b5e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8068077
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669264}

--- chrome/browser/password_manager/remote_actor/remote_actor_credential_sharing_service_impl.cc
+++ chrome/browser/password_manager/remote_actor/remote_actor_credential_sharing_service_impl.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/password_manager/remote_actor/remote_actor_credential_sharing_service_impl.h"
 
+#include <utility>
+
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/task/sequenced_task_runner.h"
+#include "chrome/browser/password_manager/remote_actor/remote_actor_credential_permission_client.h"
 #include "chrome/browser/password_manager/remote_actor/remote_actor_credential_store_client.h"
-#include "components/signin/public/identity_manager/identity_manager.h"
 
 namespace password_manager {
 
@@ -21,7 +23,11 @@ RemoteActorCredentialSharingServiceImpl::
       url_loader_factory_(url_loader_factory),
       credential_store_(std::make_unique<RemoteActorCredentialStoreClient>(
           identity_manager,
-          std::move(url_loader_factory))) {}
+          url_loader_factory)),
+      permission_client_(
+          std::make_unique<RemoteActorCredentialPermissionClient>(
+              identity_manager,
+              std::move(url_loader_factory))) {}
 
 RemoteActorCredentialSharingServiceImpl::
     ~RemoteActorCredentialSharingServiceImpl() = default;
