29bad3f57e3baedc94514ae6079e16a8addd0b3d
Bruno Braga <brunobraga@google.com>
2026-07-28 08:23:08 -0700

    Add Autofill AI entities tab to autofill-internals
    This CL introduces a new "AutofillAI entities" tab to the
    chrome://autofill-internals diagnostic page. This tab allows developers
    and debuggers to inspect stored Autofill AI entity instances and their
    associated attributes from EntityDataManager.
    
    Key changes:
    - WebUI HTML/TS: Added tab link, container, and table rendering logic
      grouped by entityType and recordType.
    - C++ Handler: Added getAutofillAiEntities message callback and
      SendAutofillAiEntitiesToWebUI() in InternalsUIHandler.
    - Observer Pattern: InternalsUIHandler observes EntityDataManager to
      reactively update the table when entity data changes.
    - Security Redaction: Any attribute where attribute_type.is_obfuscated()
      is true is strictly output as "<redacted>".
    
    Bug: 539818688
    
    Change-Id: I6bdd1157a224b26aa8f232e169cf7aeba6fa716d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8062311
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669513}

--- chrome/browser/ui/webui/autofill_and_password_manager_internals/internals_ui_handler.h
+++ chrome/browser/ui/webui/autofill_and_password_manager_internals/internals_ui_handler.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/values.h"
+#include "components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h"
 #include "components/autofill/core/browser/logging/log_receiver.h"
 #include "content/public/browser/browsing_data_remover.h"
 #include "content/public/browser/web_ui_message_handler.h"
@@ -60,7 +62,8 @@ class AutofillCacheResetter : public content::BrowsingDataRemover::Observer {
 // chrome://autofill-internals that takes care of subscribing to the autofill
 // logging instance.
 class InternalsUIHandler : public content::WebUIMessageHandler,
-                           public LogReceiver {
+                           public LogReceiver,
+                           public EntityDataManager::Observer {
  public:
   using GetLogRouterFunction =
       base::RepeatingCallback<LogRouter*(content::BrowserContext*)>;
@@ -88,9 +91,15 @@ class InternalsUIHandler : public content::WebUIMessageHandler,
   void StartSubscription();
   void EndSubscription();
 
+  // EntityDataManager::Observer:
+  void OnEntityInstancesChanged() override;
+
+  void SendAutofillAiEntitiesToWebUI();
+
   // JavaScript call handler.
   void OnDeleteAutofillAiCacheEntry(const base::ListValue& args);
   void OnGetAutofillAiCache(const base::ListValue& args);
+  void OnGetAutofillAiEntities(const base::ListValue& args);
   void OnLoaded(const base::ListValue& args);
   void OnResetCache(const base::ListValue& args);
   void OnDumpAddresses(const base::ListValue& args);
@@ -111,6 +120,9 @@ class InternalsUIHandler : public content::WebUIMessageHandler,
 
   // Whether |this| is registered as a log receiver with the LogRouter.
   bool registered_with_log_router_ = false;
+
+  base::ScopedObservation<EntityDataManager, EntityDataManager::Observer>
+      entity_data_observation_{this};
 
   std::optional<AutofillCacheResetter> autofill_cache_resetter_;
 };
