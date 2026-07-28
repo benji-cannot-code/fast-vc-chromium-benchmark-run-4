c2efc7f14d3e3d4a558002c28227c877d3fa0d4d
Jakob Koblinsky <jkoblinsky@google.com>
2026-07-28 09:39:09 -0700

    [Omnibox] Move preview match URL setting to AutocompleteMediator
    We no longer need the indirection through LocationBarMediator.
    
    Bug: 530538598
    Change-Id: I210f5b8cbb3c079e3b3996ea2c778805f54c89c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8156223
    Commit-Queue: Jakob Koblinsky <jkoblinsky@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669572}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -1194,6 +1194,7 @@ class AutocompleteMediator
         } else {
             input.resetPreviewText();
         }
+        input.setPreviewMatchUrl(getPreviewMatchUrl(defaultMatch));
 
         if (!(mAutocompleteResult != null && mAutocompleteResult.equals(autocompleteResult))) {
             mAutocompleteResult = autocompleteResult;
@@ -1206,6 +1207,21 @@ class AutocompleteMediator
 
         mListPropertyModel.set(SuggestionListProperties.LIST_IS_FINAL, isFinal);
         measureSuggestionRequestToUiModelTime(isFinal);
+    }
+
+    private @Nullable GURL getPreviewMatchUrl(@Nullable AutocompleteMatch defaultMatch) {
+        if (mAutocompleteInput == null) return null;
+
+        // Non-conventional modes will not have site favicons.
+        if (!mAutocompleteInput.isConventionalRequestType()) return null;
+
+        // Zero suggest is always considered Search, there may be a match, but we shouldn't show it.
+        if (TextUtils.isEmpty(mAutocompleteInput.getUserText())) return null;
+
+        // Search suggestions will not have site favicons.
+        if (defaultMatch == null || defaultMatch.isSearchSuggestion()) return null;
+
+        return defaultMatch.getUrl();
     }
 
     /**
