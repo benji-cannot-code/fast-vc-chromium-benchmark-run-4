56b0339f43723522fc529bc0e74b76086c118680
Dibyajyoti Pal <dibyapal@google.com>
2026-07-28 14:15:34 -0700

    [Sync] Fix heap-use-after-free in FakeServerSyncInvalidationSender
    When a Profile is destroyed during test execution or shutdown (e.g. via
    InProcessBrowserTest::CloseBrowserSynchronously), its associated
    KeyedServices—including FakeGCMDriverForInstanceID are freed before
    ProfileObserver::OnProfileWillBeDestroyed is invoked.
    
    Previously, RemoveFakeGCMDriver() and
    ~FakeServerSyncInvalidationSender() attempted to execute
    fake_gcm_driver->RemoveConnectionObserver(this)` on driver pointers that
    were already destructed or freed, triggering an AddressSanitizer (ASan)
    heap-use-after-free.
    
    This CL fixes the issue by maintaining a
    base::WeakPtr<FakeGCMDriverForInstanceID> everywhere so that in case it
    gets destroyed in the middle of a test run as part of a profile
    shutdown, the test can still be shutdown gracefully instead of
    triggering a crash.
    
    ASAN failure: https://paste.googleplex.com/5638456368824320
    
    Bug: 536419371
    Change-Id: I3a8c5f3071a202a5eea5987c4dd0358037e0373d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8140829
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669758}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -995,8 +995,10 @@ void SyncTest::OnProfileWillBeDestroyed(Profile* profile) {
 
   if (server_type_ == IN_PROCESS_FAKE_SERVER) {
     CHECK(profile_to_fake_gcm_driver_.contains(profile));
-    fake_server_sync_invalidation_sender_->RemoveFakeGCMDriver(
-        profile_to_fake_gcm_driver_[profile]);
+    if (fake_server_sync_invalidation_sender_) {
+      fake_server_sync_invalidation_sender_->RemoveFakeGCMDriver(
+          profile_to_fake_gcm_driver_[profile]);
+    }
     profile_to_fake_gcm_driver_.erase(profile);
   }
 
@@ -1382,12 +1384,14 @@ syncer::DataTypeSet AllowedTypesInStandaloneTransportMode() {
 #if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #if BUILDFLAG(IS_CHROMEOS)
     if (base::FeatureList::IsEnabled(
-            syncer::kReplaceSyncPromosWithSignInPromos))
-#endif
-    {
+            syncer::kReplaceSyncPromosWithSignInPromos)) {
       allowed_types.Put(syncer::EXTENSIONS);
       allowed_types.Put(syncer::EXTENSION_SETTINGS);
     }
+#else
+    allowed_types.Put(syncer::EXTENSIONS);
+    allowed_types.Put(syncer::EXTENSION_SETTINGS);
+#endif
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   }
   allowed_types.Put(syncer::AUTOFILL_VALUABLE);
