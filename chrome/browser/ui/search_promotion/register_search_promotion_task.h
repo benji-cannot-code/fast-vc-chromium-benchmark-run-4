d8b90f8dae42abfe66c3921211461441568e97dc
Quinton Lucyk <qlucyk@google.com>
2026-07-28 13:30:22 -0700

    Simplify Search Promotion task execution.
    Remove browser-side handling of delegated task exit codes in favor of
    framework-level metrics in platform_experience.
    
    Bug: 467255671
    Change-Id: I20ce29f0bb30fa4fc1bfa7eea9f855db435aabeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8135325
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Reviewed-by: Marwan Tammam <quarz@chromium.org>
    Commit-Queue: Quinton Lucyk <qlucyk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669712}

--- chrome/browser/ui/search_promotion/register_search_promotion_task.h
+++ chrome/browser/ui/search_promotion/register_search_promotion_task.h
@@ -15,18 +15,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/platform_experience/delegated_tasks/delegated_task.h"
 #include "url/gurl.h"
 
+enum class SearchPromotionExitCode {
+  // Pre-execution registration errors:
+  kInvalidExtensionId = 100,
+  kInvalidPostInstallUrl = 101,
+  kRegistryWriteFailed = 102,
+
+  // Arm A:
+  kUrlLaunchFailed = 103,
+  kUrlLaunchSuccess = 104,
+
+  // Arm B:
+  kTimeout = 105,
+  kSuccessBackground = 106,
+  kSuccessWithForegroundFallback = 107,
+  kForegroundFallbackLaunchFailed = 108,
+
+  kMaxValue = kForegroundFallbackLaunchFailed,
+};
+
 namespace base {
 class CommandLine;
 }
-
-enum class SearchPromotionExitCode {
-  kInvalidExtensionId = 100,
-  kInvalidPostInstallUrl = 101,
-  kForegroundFallbackLaunchFailed = 102,
-  kTimeout = 103,
-  kSuccessBackground = 104,
-  kSuccessWithForegroundFallback = 105,
-};
 
 class RegisterSearchPromotionTask : public platform_experience::DelegatedTask {
  public:
@@ -46,8 +56,6 @@ class RegisterSearchPromotionTask : public platform_experience::DelegatedTask {
 
   static std::unique_ptr<RegisterSearchPromotionTask> FromCommandLine(
       const base::CommandLine& command_line);
-
-  static std::optional<SearchPromotionExitCode> ParseExitCode(int exit_code);
 
   const GURL& post_install_url() const { return post_install_url_; }
   std::string_view extension_id() const { return extension_id_; }
