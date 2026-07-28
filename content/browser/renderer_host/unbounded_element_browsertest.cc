121d02511d6646995d644761e4ef61821f4e877b
Mason Freed <masonf@chromium.org>
2026-07-28 14:54:11 -0700

    Re-enable UnboundedElementBrowserTest on Linux
    This re-enables Linux testing, and fixes a few issues:
    - Report bounds in DIPs under high-DPI scaling.
    - Transform mouse routing coordinates via root view, matching Mac.
    - Safely handle null native windows during test teardown.
    - Skip popup event routing tests on Wayland - flaky.
    
    Fuchsia-Binary-Size: Test failure results don't make much sense.
    Fixed: 525899641,523970924
    Change-Id: I79f25be7356109d58f6d54000b123c41682289d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8136160
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669794}

--- content/browser/renderer_host/unbounded_element_browsertest.cc
+++ content/browser/renderer_host/unbounded_element_browsertest.cc
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window_tracker.h"
 #endif
 
+#if BUILDFLAG(IS_OZONE)
+#include "ui/ozone/public/ozone_platform.h"
+#endif
+
 namespace content {
 
 class UnboundedElementBrowserTest : public ContentBrowserTest {
@@ -41,9 +45,6 @@ class UnboundedElementBrowserTest : public ContentBrowserTest {
   void SetUp() override {
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
     // TODO(crbug.com/508672616): Not yet implemented on Android/iOS.
-    GTEST_SKIP();
-#elif BUILDFLAG(IS_LINUX)
-    // TODO(crbug.com/525899641): Flaky/failing on Linux Aura/Wayland.
     GTEST_SKIP();
 #else
     feature_list_.InitWithFeatures(
@@ -78,13 +79,16 @@ class UnboundedElementBrowserTest : public ContentBrowserTest {
       UnboundedSurfaceWindow& window) {
     auto tracker = std::make_unique<aura::WindowTracker>();
     gfx::NativeWindow native_window = window.GetNativeWindow();
-    CHECK(native_window);
+    if (!native_window) {
+      return tracker;
+    }
     tracker->Add(native_window);
 #if BUILDFLAG(IS_WIN)
     // Explicitly wait for the top-level window to be destroyed on windows, to
     // avoid the test runner's leak detection check from failing.
-    CHECK(native_window->parent());
-    tracker->Add(native_window->parent());
+    if (native_window->parent()) {
+      tracker->Add(native_window->parent());
+    }
 #endif
     return tracker;
   }
@@ -376,6 +380,13 @@ class UnboundedElementHighDPIBrowserTest : public UnboundedElementBrowserTest {
 
 IN_PROC_BROWSER_TEST_F(UnboundedElementHighDPIBrowserTest,
                        CompositorPopupAllocationHighDPI) {
+#if BUILDFLAG(IS_OZONE)
+  if (ui::OzonePlatform::RunningOnWaylandForTest()) {
+    // TODO(crbug.com/523970924): Some Wayland compositors (such as Mutter)
+    // configure native popup bounds in physical pixels under high-DPI scaling.
+    GTEST_SKIP();
+  }
+#endif
   GURL url(embedded_test_server()->GetURL("/title1.html"));
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
@@ -441,19 +452,20 @@ IN_PROC_BROWSER_TEST_F(UnboundedElementBrowserTest,
   EXPECT_GE(popup_bounds.height(), 90);
 }
 
-// TODO(crbug.com/523970924): Re-enable the test.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_PopupInputEventRouting DISABLED_PopupInputEventRouting
-#else
-#define MAYBE_PopupInputEventRouting PopupInputEventRouting
-#endif
-IN_PROC_BROWSER_TEST_F(UnboundedElementBrowserTest,
-                       MAYBE_PopupInputEventRouting) {
+IN_PROC_BROWSER_TEST_F(UnboundedElementBrowserTest, PopupInputEventRouting) {
 #if BUILDFLAG(IS_CHROMEOS)
   // TODO(crbug.com/508672616): Not yet working on ChromeOS due to Aura/Ash
   // popup container positioning and coordinate conversion issues.
   GTEST_SKIP();
-#else
+#elif BUILDFLAG(IS_OZONE)
+  if (ui::OzonePlatform::RunningOnWaylandForTest()) {
+    // TODO(crbug.com/523970924): Not yet working on Wayland due to
+    // Ozone/Wayland popup container positioning and coordinate conversion
+    // issues.
+    GTEST_SKIP();
+  }
+#endif
+#if !BUILDFLAG(IS_CHROMEOS)
   GURL url(embedded_test_server()->GetURL("/title1.html"));
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
@@ -499,21 +511,21 @@ IN_PROC_BROWSER_TEST_F(UnboundedElementBrowserTest,
 #endif
 }
 
-// TODO(crbug.com/523970924): Re-enable the test.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_PopupOutsideViewportInputEventRouting \
-  DISABLED_PopupOutsideViewportInputEventRouting
-#else
-#define MAYBE_PopupOutsideViewportInputEventRouting \
-  PopupOutsideViewportInputEventRouting
-#endif
 IN_PROC_BROWSER_TEST_F(UnboundedElementBrowserTest,
-                       MAYBE_PopupOutsideViewportInputEventRouting) {
+                       PopupOutsideViewportInputEventRouting) {
 #if BUILDFLAG(IS_CHROMEOS)
   // TODO(crbug.com/508672616): Not yet working on ChromeOS due to Aura/Ash
   // popup container positioning and coordinate conversion issues.
   GTEST_SKIP();
-#else
+#elif BUILDFLAG(IS_OZONE)
+  if (ui::OzonePlatform::RunningOnWaylandForTest()) {
+    // TODO(crbug.com/523970924): Not yet working on Wayland due to
+    // Ozone/Wayland popup container positioning and coordinate conversion
+    // issues.
+    GTEST_SKIP();
+  }
+#endif
+#if !BUILDFLAG(IS_CHROMEOS)
   GURL url(embedded_test_server()->GetURL("/title1.html"));
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
