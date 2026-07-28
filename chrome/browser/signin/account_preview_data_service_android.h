07ebe5c0f1824267f6689403a2035d8611fe0060
Menghan Yang <myuu@google.com>
2026-07-28 07:06:55 -0700

    [Signin][Android][SAS] Create java infra for AccountPreviewDataService
    This CL introduces the Java implementation and JNI bridge for
    AccountPreviewDataService and AccountPreviewPreference, and related unit
    tests.
    
    Bug: 532967033
    Change-Id: I38559c4f6fa914bdcff1500071a92a1da624e770
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8076139
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669444}

---
+++ chrome/browser/signin/account_preview_data_service_android.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SIGNIN_ACCOUNT_PREVIEW_DATA_SERVICE_ANDROID_H_
+#define CHROME_BROWSER_SIGNIN_ACCOUNT_PREVIEW_DATA_SERVICE_ANDROID_H_
+
+#include "base/android/scoped_java_ref.h"
+
+namespace signin {
+class AccountPreviewDataService;
+}
+
+namespace jni_zero {
+
+template <>
+base::android::ScopedJavaLocalRef<jobject> ToJniType(
+    JNIEnv* env,
+    signin::AccountPreviewDataService* service);
+
+}  // namespace jni_zero
+
+#endif  // CHROME_BROWSER_SIGNIN_ACCOUNT_PREVIEW_DATA_SERVICE_ANDROID_H_
