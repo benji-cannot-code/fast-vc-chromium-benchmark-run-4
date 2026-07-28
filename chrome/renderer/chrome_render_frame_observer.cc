36d3e9257b4a3f4f53bbab21f10a6d788ac83bc8
Kirubel <kaklilu@chromium.org>
2026-07-28 07:36:04 -0700

    [actor] Decouple PageStabilityMonitorDelegate from Journal
    PageStabilityMonitorDelegate is now an interface that has a LogEvent
    method that must be implemented by derived classes. This CL also adds a
    ChromePageStabilityMonitorDelegate, which logs the event in the Journal
    for the renderer process.
    
    This prepares the delegate for relocation to components/actor/core/ so
    that it can be reused on iOS. An IOSPageStabilityMonitorDelegate will be
    added which logs directly to the AggregatedJournal since iOS doesn't
    have access to the renderer processes.
    
    Bug: b:531873110
    Link: https://chromium-review.googlesource.com/id/I17b9a4661a4a01f569e90a3ca4a4c2986a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129489
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Kirubel <kaklilu@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669477}

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/open_search_description_document_handler.mojom.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/renderer/actor/chrome_page_stability_monitor_delegate.h"
 #include "chrome/renderer/actor/journal.h"
-#include "chrome/renderer/actor/page_stability_monitor_delegate.h"
 #include "chrome/renderer/actor/tool_executor.h"
 #include "chrome/renderer/benchmarking_bindings.h"
 #include "chrome/renderer/chrome_content_settings_agent_delegate.h"
@@ -729,7 +729,7 @@ void ChromeRenderFrameObserver::CreatePageStabilityMonitor(
   page_stability_monitor_ = std::make_unique<
       page_content_annotations::PageStabilityMonitor>(
       *render_frame(), supports_paint_stability,
-      std::make_unique<actor::PageStabilityMonitorDelegate>(
+      std::make_unique<actor::ChromePageStabilityMonitorDelegate>(
           task_id, *actor_journal_,
           actor::PageStabilityMonitorDelegate::Thresholds{
               .timeout_delay = features::kGlicActorPageStabilityTimeout.Get(),
