b9e233321c1b992bdca5cf12aa2a98d66ac4c437
Mingyu Lei <leimy@chromium.org>
2026-07-27 22:13:26 -0700

    Revert "WebUILocationBar: rest of keyboard shortcuts"
    This reverts commit fc2f71a137b1831a771abfad4a1207ab0b28fde8.
    
    Reason for revert: Constantly failing tests
    
    Failure Link: https://ci.chromium.org/ui/test/chrome/%3A%2F%2Fchrome%2Ftest%5C%3Ainteractive_ui_tests!gtest%3A%3AWebUILocationBarInteractiveUiTest%23NavigateSuggestionsTab
    
    Original change's description:
    > WebUILocationBar: rest of keyboard shortcuts
    >
    > To support them, this also implements fake focus on AIM button (e.g.
    > keeping the actual focus on the input, letting the typing go there, but
    > painting the focus ring on AIM button) and hiding of other page actions.
    >
    > Bug: 503785596
    > Change-Id: Id712273d4a908230c81ba96db69f2d50c2a8390e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127751
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    > Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    > Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1669055}
    
    Bug: 503785596
    Change-Id: Iea1a9b1efee3676fc4532bab0a4157d926b73547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8159100
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Owners-Override: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669206}

--- chrome/browser/resources/webui_toolbar/app.ts
+++ chrome/browser/resources/webui_toolbar/app.ts
@@ -381,7 +381,6 @@ export class ToolbarAppElement extends AppElementBase {
       locationBarFlags: {
         userInputInProgress: false,
         popupOpen: false,
-        forceAimButtonFocusRing: false,
       },
       selectedKeyword: null,
       contentSettingImageStates: [],
--- chrome/browser/resources/webui_toolbar/location_bar.html.ts
+++ chrome/browser/resources/webui_toolbar/location_bar.html.ts
@@ -36,8 +36,7 @@ ${this.locationBarState.selectedKeyword ? html`
   </selected-keyword>
 ` : nothing}
 <readonly-omnibox id="omnibox"
-  .browserOmniboxState="${this.locationBarState.omniboxViewState}"
-  .isPopupOpen="${this.isPopupOpen}">
+  .browserOmniboxState="${this.locationBarState.omniboxViewState}">
 </readonly-omnibox>
 <content-settings-icons id="contentSettings"
     .contentSettingImageStates=
