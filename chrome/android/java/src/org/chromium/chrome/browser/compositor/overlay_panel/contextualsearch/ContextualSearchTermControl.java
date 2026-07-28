c79fbc484a11c13464f01e7851b9f461b5ec5967
Dibyajyoti Pal <dibyapal@chromium.org>
2026-07-28 11:18:14 -0700

    Revert "Fix text and button icons overlapping for contextual search bar"
    This reverts commit f6806252598fedd74ccf6d2cdfc3052837891842.
    
    Reason for revert: Causing the tree to shut down by failing the `android-desktop-x64-compile-rel` bot, please see
    compilation failure.
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/android-desktop-x64-compile-rel/86509/overview
    
    Original change's description:
    > Fix text and button icons overlapping for contextual search bar
    >
    > Bug: 539419110
    > Change-Id: I73ec03851a075e2e676c598c6357bfa7eb5fb697
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158231
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Commit-Queue: Charles Hager <clhager@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1669602}
    
    Bug: 539419110
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie9c8eeb975ba6632d092120d2af15d741f60d6ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163647
    Owners-Override: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1669618}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlay_panel/contextualsearch/ContextualSearchTermControl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlay_panel/contextualsearch/ContextualSearchTermControl.java
@@ -19,7 +19,6 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.compositor.overlay_panel.OverlayPanel;
 import org.chromium.chrome.browser.compositor.overlay_panel.OverlayPanelTextViewInflater;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.ui.resources.dynamics.DynamicResourceLoader;
 
 /**
@@ -59,9 +58,7 @@ public class ContextualSearchTermControl extends OverlayPanelTextViewInflater {
                 container,
                 resourceLoader,
                 R.dimen.contextual_search_end_padding,
-                ChromeFeatureList.sContextualPanelCloseButtonOnTablets.isEnabled()
-                        ? R.dimen.contextual_search_end_buttons_width
-                        : R.dimen.contextual_search_padded_button_width);
+                R.dimen.contextual_search_padded_button_width);
     }
 
     /**
