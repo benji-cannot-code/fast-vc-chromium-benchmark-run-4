626b3922892160f008ff10c68d5f89795aa9dd1f
Charles Hager <clhager@google.com>
2026-07-28 16:08:37 -0700

    Fix text and button icons overlapping for contextual search bar
    This change also enables this change across all form factors, not just tablets.
    
    Bug: 539419110
    Change-Id: I70944f9247905dd2bd8c1c2dfd9cf7530e4b118d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162844
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669847}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlay_panel/contextualsearch/ContextualSearchTermControl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlay_panel/contextualsearch/ContextualSearchTermControl.java
@@ -19,6 +19,7 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.compositor.overlay_panel.OverlayPanel;
 import org.chromium.chrome.browser.compositor.overlay_panel.OverlayPanelTextViewInflater;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.ui.resources.dynamics.DynamicResourceLoader;
 
 /**
@@ -58,7 +59,9 @@ public class ContextualSearchTermControl extends OverlayPanelTextViewInflater {
                 container,
                 resourceLoader,
                 R.dimen.contextual_search_end_padding,
-                R.dimen.contextual_search_padded_button_width);
+                ChromeFeatureList.sContextualPanelCloseButton.isEnabled()
+                        ? R.dimen.contextual_search_end_buttons_width
+                        : R.dimen.contextual_search_padded_button_width);
     }
 
     /**
--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -377,8 +377,7 @@ public abstract class ChromeFeatureList {
     public static final String CONTEXTUAL_PAGE_ACTIONS = "ContextualPageActions";
     public static final String CONTEXTUAL_PAGE_ACTION_TAB_GROUPING =
             "ContextualPageActionTabGrouping";
-    public static final String CONTEXTUAL_PANEL_CLOSE_BUTTON_ON_TABLETS =
-            "ContextualPanelCloseButtonOnTablets";
+    public static final String CONTEXTUAL_PANEL_CLOSE_BUTTON = "ContextualPanelCloseButton";
     public static final String CONTEXTUAL_SEARCH_DISABLE_ONLINE_DETECTION =
             "ContextualSearchDisableOnlineDetection";
     public static final String CONTEXTUAL_SEARCH_SUPPRESS_SHORT_VIEW =
@@ -928,8 +927,8 @@ public abstract class ChromeFeatureList {
             newCachedFlag(CONTEXT_MENU_COPY_VIDEO_FRAME_ANDROID, true);
     public static final CachedFlag sContextMenuDownloadVideoFrame =
             newCachedFlag(CONTEXT_MENU_DOWNLOAD_VIDEO_FRAME_ANDROID, true);
-    public static final CachedFlag sContextualPanelCloseButtonOnTablets =
-            newCachedFlag(CONTEXTUAL_PANEL_CLOSE_BUTTON_ON_TABLETS, true);
+    public static final CachedFlag sContextualPanelCloseButton =
+            newCachedFlag(CONTEXTUAL_PANEL_CLOSE_BUTTON, true);
     public static final CachedFlag sContextualTasks = newCachedFlag(CONTEXTUAL_TASKS, false);
     public static final CachedFlag sCpaTabGroupingButton =
             newCachedFlag(
@@ -1348,7 +1347,7 @@ public abstract class ChromeFeatureList {
                     sCompositorViewRemeasureFix,
                     sContextMenuCopyVideoFrame,
                     sContextMenuDownloadVideoFrame,
-                    sContextualPanelCloseButtonOnTablets,
+                    sContextualPanelCloseButton,
                     sContextualTasks,
                     sCpaTabGroupingButton,
                     sCrossDeviceTabPaneAndroid,
