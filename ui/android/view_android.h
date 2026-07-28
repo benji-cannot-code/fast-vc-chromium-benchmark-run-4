775ddb60b07fc56ea041894036d4b2d029188fbc
Jinsuk Kim <jinsukkim@google.com>
2026-07-28 15:16:23 -0700

    [Vertical Tabs] Fix x offset of HTML select dropdown
    This CL adds |content_offset_x_| and setter/getter to ui:ViewAndroid,
    and passes the SideUI left margin (CONTENT_OFFSET_X). It is now added
    to ViewAndroid::SetAnchorRect's |left_margin|, fixing the wrong offset
    issue with HTML <select> dropdowns.
    
    Bug: 537012693
    Change-Id: I4c8fd01d7de43d20d1617bc3908f470f68874f90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162546
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669810}

--- ui/android/view_android.h
+++ ui/android/view_android.h
@@ -115,6 +115,10 @@ class UI_ANDROID_EXPORT ViewAndroid {
   void UpdateFrameInfo(const FrameInfo& frame_info);
   // content_offset is in dip.
   float content_offset() const { return frame_info_.content_offset; }
+  float content_offset_x() const { return content_offset_x_; }
+  void set_content_offset_x(float content_offset_x) {
+    content_offset_x_ = content_offset_x;
+  }
   gfx::SizeF viewport_size() const { return frame_info_.viewport_size; }
 
   // Returns the window at the root of this hierarchy, or |null|
@@ -355,6 +359,9 @@ class UI_ANDROID_EXPORT ViewAndroid {
   gfx::Size physical_size_;
 
   FrameInfo frame_info_;
+
+  // Left content offset in device pixels.
+  float content_offset_x_ = 0.f;
 
   std::unique_ptr<EventForwarder> event_forwarder_;
 
