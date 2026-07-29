17b7649f8ef0152e434435d18445044212f6c683
Shadmehr Hashem Zehi <shadmehr@google.com>
2026-07-29 12:08:47 -0700

    [Android] Re-enable some tests that use back press
    Enables testFirstRunPages_NoCctPolicy_OnBackPressed,
    testFirstRunPages_WithCctPolicy_OnBackPressed, and
    testFirstRunPages_ProgressHistogramRecordedOnlyOnce, which were
    previously disabled due to flakiness.
    
    Bug: 431982831, 537363058
    Change-Id: I1168c854f7cf195ae7e91967390cb15f76700ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8155421
    Commit-Queue: Shadmehr Hashem Zehi <shadmehr@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670479}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -472,7 +472,6 @@ public class FirstRunIntegrationTest {
     @MediumTest
     // Sign-in is not supported on automotive devices.
     @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
-    @DisabledTest(message = "crbug.com/431982831")
     public void testFirstRunPages_NoCctPolicy_OnBackPressed() throws Exception {
         mSigninTestRule.addAccount(TestAccounts.AADC_ADULT_ACCOUNT);
         initializePreferences(FirstRunPagesTestCase.createWithShowAllPromos());
@@ -501,7 +500,6 @@ public class FirstRunIntegrationTest {
     @MediumTest
     // Sign-in is not supported on automotive devices.
     @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
-    @DisabledTest(message = "crbug.com/431982831")
     public void testFirstRunPages_WithCctPolicy_OnBackPressed() throws Exception {
         mSigninTestRule.addAccount(TestAccounts.AADC_ADULT_ACCOUNT);
         initializePreferences(FirstRunPagesTestCase.createWithShowAllPromos().withCctTosDisabled());
@@ -592,7 +590,6 @@ public class FirstRunIntegrationTest {
     @MediumTest
     // Sign-in is not supported on automotive devices.
     @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO})
-    @DisabledTest(message = "Flaky, see crbug.com/431982831")
     public void testFirstRunPages_ProgressHistogramRecordedOnlyOnce() throws Exception {
         mSigninTestRule.addAccount(TestAccounts.AADC_ADULT_ACCOUNT);
         HistogramWatcher histograms =
