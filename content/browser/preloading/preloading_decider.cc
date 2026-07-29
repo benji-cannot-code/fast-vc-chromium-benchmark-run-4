1abaae5ef084b8fa5b4d89a255acb8b4b457e195
Yoav Weiss <yoavweiss@chromium.org>
2026-07-29 06:11:52 -0700

    SpecRules: move viewport heuristics to the renderer
    Continuing the work to move the Speculation Rules heuristics to the
    renderer, this CL handles the viewport-based heuristics.
    
    Change-Id: I6fee1f0e7a63c271f23a10cb63291322823f06e9
    
    Bug: 532860179
    Change-Id: I6fee1f0e7a63c271f23a10cb63291322823f06e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070383
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670176}

--- content/browser/preloading/preloading_decider.cc
+++ content/browser/preloading/preloading_decider.cc
@@ -76,6 +76,10 @@ PreloadingPredictor PredictorForRendererHeuristic(
       return preloading_predictor::kUrlPointerDownOnAnchor;
     case blink::mojom::SpeculationHeuristic::kPointerHover:
       return preloading_predictor::kUrlPointerHoverOnAnchor;
+    case blink::mojom::SpeculationHeuristic::kViewportModerate:
+      return preloading_predictor::kModerateViewportHeuristic;
+    case blink::mojom::SpeculationHeuristic::kViewportEager:
+      return preloading_predictor::kEagerViewportHeuristic;
   }
   NOTREACHED();
 }
@@ -361,11 +365,8 @@ void PreloadingDecider::OnPointerHover(
 void PreloadingDecider::OnModerateViewportHeuristicTriggered(const GURL& url) {
   CHECK(base::FeatureList::IsEnabled(
       blink::features::kPreloadingModerateViewportHeuristics));
-  static const base::FeatureParam<bool> kShouldEnactCandidates{
-      &blink::features::kPreloadingModerateViewportHeuristics,
-      "enact_candidates", BUILDFLAG(IS_ANDROID)};
-  const bool should_enact_candidates = kShouldEnactCandidates.Get();
-  if (!should_enact_candidates) {
+  if (!blink::features::kPreloadingModerateViewportHeuristicsEnactCandidates
+           .Get()) {
     AddPreloadingPrediction(url,
                             preloading_predictor::kModerateViewportHeuristic,
                             PreloadingConfidence(100));
