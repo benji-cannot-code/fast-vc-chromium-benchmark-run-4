dd644a3c25ff9add572e0e89d7a226e3971b1c91
Lei Zhang <thestig@chromium.org>
2026-07-28 21:21:20 -0700

    Printing: Check IsActive() in SetupScriptedPrintAndroid()
    PrintViewManagerBasic::SetupScriptedPrintAndroid() should consistently
    check if a RFH is active, like PrintViewManager.
    
    Bug: 533084258
    Change-Id: Ic3c40cb300de21f7b549ded5fa4601e698416da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162027
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669987}

--- chrome/browser/printing/print_view_manager_basic.cc
+++ chrome/browser/printing/print_view_manager_basic.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include <utility>
+
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "chrome/browser/printing/print_job_manager.h"
@@ -75,6 +77,10 @@ void PrintViewManagerBasic::SetupScriptedPrintAndroid(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   content::RenderFrameHost& rfh = CurrentTargetFrame();
   DCHECK(rfh.IsRenderFrameLive());
+  if (!rfh.IsActive()) {
+    std::move(callback).Run();
+    return;
+  }
 
   // Start Printing Flow via PrinterQuery
   std::unique_ptr<PrinterQuery> printer_query =
@@ -102,7 +108,7 @@ void PrintViewManagerBasic::OnSetupScriptedPrintAndroidDone(
 void PrintViewManagerBasic::PdfWritingDone(int page_count) {
   pdf_writing_done_callback().Run(page_count);
 }
-#endif
+#endif  // BUILDFLAG(IS_ANDROID)
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(PrintViewManagerBasic);
 
