67a132b4a7dff7c55cefc3da8b2cd8ee25799b42
Devon Loehr <dloehr@google.com>
2026-07-29 13:24:32 -0700

    Expose RunsTasksInCurrentSequence to Rust
    This will be necessary for managing sequences once we have fuller
    support for that in Rust.
    
    Bug: 540355135
    Change-Id: I1b108afd9b01fe0bd11e0424b58839437cbde329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8159344
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670538}

--- base/task/sequenced_task_runner_rust_shim.cc
+++ base/task/sequenced_task_runner_rust_shim.cc
@@ -36,4 +36,8 @@ void Release(const SequencedTaskRunner& runner) {
   runner.Release();
 }
 
+bool RunsTasksInCurrentSequence(const SequencedTaskRunner& runner) {
+  return runner.RunsTasksInCurrentSequence();
+}
+
 }  // namespace base
