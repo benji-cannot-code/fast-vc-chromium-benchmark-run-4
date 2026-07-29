57fc0683e6a7916c30a26b9f2841423877b51294
Mike Wittman <wittman@chromium.org>
2026-07-29 10:24:39 -0700

    [signin][ash] Notify observer on dialog bounds change
    Make InlineLoginDialog inherit from views::WidgetObserver to
    observe the widget associated with dialog_window(). When the dialog
    window moves or resizes, OnWidgetBoundsChanged() iterates through
    all registered ModalDialogHostObserver instances and invokes
    OnPositionRequiresUpdate(). This ensures open web-contents modal
    dialogs dynamically recalculate their origin to follow parent window
    geometry changes.
    
    Fixed: 533550597
    Test: out/cd/browser_tests --gtest_filter=InlineLoginDialogTest.*
    Change-Id: Ie1fad8b2b51bcacad08ca0cc835953ae66432831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8112357
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670369}

--- chrome/browser/ui/webui/signin/ash/inline_login_dialog.h
+++ chrome/browser/ui/webui/signin/ash/inline_login_dialog.h
@@ -12,12 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/gtest_prod_util.h"
 #include "base/observer_list.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/ui/webui/ash/system_web_dialog/system_web_dialog_delegate.h"
 #include "chrome/browser/ui/webui/signin/ash/inline_login_handler_modal_delegate.h"
 #include "components/account_manager_core/account_addition_options.h"
 #include "components/web_modal/modal_dialog_host.h"
 #include "components/web_modal/web_contents_modal_dialog_host.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
+#include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_observer.h"
 
 class GURL;
 
@@ -27,7 +30,8 @@ class AccountManagerDialogCoordinator;
 
 // Extends from |SystemWebDialogDelegate| to create an always-on-top dialog.
 class InlineLoginDialog : public SystemWebDialogDelegate,
-                          public web_modal::WebContentsModalDialogHost {
+                          public web_modal::WebContentsModalDialogHost,
+                          public views::WidgetObserver {
  public:
   InlineLoginDialog(const InlineLoginDialog&) = delete;
   InlineLoginDialog& operator=(const InlineLoginDialog&) = delete;
@@ -43,6 +47,13 @@ class InlineLoginDialog : public SystemWebDialogDelegate,
   gfx::Point GetDialogPosition(const gfx::Size& size) override;
   void AddObserver(web_modal::ModalDialogHostObserver* observer) override;
   void RemoveObserver(web_modal::ModalDialogHostObserver* observer) override;
+
+  // views::WidgetObserver overrides:
+  void OnWidgetBoundsChanged(views::Widget* widget,
+                             const gfx::Rect& new_bounds) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
+
+  void AttachWidgetObserver();
 
  protected:
   FRIEND_TEST_ALL_PREFIXES(InlineLoginDialogTest, ReturnsEmptyDialogArgs);
@@ -94,6 +105,8 @@ class InlineLoginDialog : public SystemWebDialogDelegate,
   base::OnceClosure close_dialog_closure_;
   base::ObserverList<web_modal::ModalDialogHostObserver>
       modal_dialog_host_observer_list_;
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      widget_observation_{this};
 };
 
 }  // namespace ash
