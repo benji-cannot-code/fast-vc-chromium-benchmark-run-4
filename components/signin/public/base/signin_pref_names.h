ea54ba2a26c677aee72c6065456607524bee0868
Ryan Sultanem <rsult@google.com>
2026-07-29 04:18:28 -0700

    [SyncPreview] Log metrics for account preview fetching events
    Logs different metrics related to trigger causes for fetching sync
    preview data.
    
    Bug: 532419984
    Change-Id: I04ce380e7058b3c4d7056ee711d771eb6407968c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8073747
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670122}

--- components/signin/public/base/signin_pref_names.h
+++ components/signin/public/base/signin_pref_names.h
@@ -23,6 +23,8 @@ extern const char kAccountPreviewPreference[];
 COMPONENT_EXPORT(SIGNIN_SWITCHES)
 extern const char kAccountPreviewDataLastUpdatePref[];
 COMPONENT_EXPORT(SIGNIN_SWITCHES)
+extern const char kAccountPreviewNonPeriodicFetchCountPref[];
+COMPONENT_EXPORT(SIGNIN_SWITCHES)
 extern const char kGaiaCookieHash[];
 COMPONENT_EXPORT(SIGNIN_SWITCHES)
 extern const char kGaiaCookieChangedTime[];
