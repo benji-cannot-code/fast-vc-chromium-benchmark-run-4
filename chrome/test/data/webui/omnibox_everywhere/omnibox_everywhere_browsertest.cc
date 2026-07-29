e39a85a6497c5349929ea70394032f3b6a2399cf
Juliet Levesque <julietlevesque@google.com>
2026-07-29 12:13:01 -0700

    [loomnibox] Enable tab selection handling in composebox
    This change enables handling of tab selections from the context menu in Omnibox Everywhere, aligning its behavior and layout with the New Tab Page composebox. Previously, tabs appeared in the menu but selecting them had no effect or visual feedback.
    
    Screencast: https://screencast.googleplex.com/cast/NjA1ODI0NDY2MDI2NDk2MHwzZWIxZWZmMS0zYg
    
    Fixed: b:533110242
    Change-Id: I60cb16ec2faf45811c4388b55bb327d506916cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8155760
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Juliet Lévesque Knighton <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670485}

---
+++ chrome/test/data/webui/omnibox_everywhere/omnibox_everywhere_browsertest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/ui/omnibox/omnibox_next_features.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "components/omnibox/common/omnibox_features.h"
+#include "content/public/test/browser_test.h"
+
+class OmniboxEverywhereWebUITest : public WebUIMochaBrowserTest {
+ protected:
+  OmniboxEverywhereWebUITest() {
+    set_test_loader_host(chrome::kChromeUIOmniboxEverywhereHost);
+    scoped_feature_list_.InitAndEnableFeature(omnibox::kOmniboxEverywhere);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(OmniboxEverywhereWebUITest, TabSelectionAndRestoration) {
+  RunTest("omnibox_everywhere/omnibox_test.js", "mocha.run();");
+}
