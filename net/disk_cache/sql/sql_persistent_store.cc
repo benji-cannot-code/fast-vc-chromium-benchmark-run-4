6772502f5cb8d38170d0217dbfbd5563d89a29ba
Tsuyoshi Horo <horo@chromium.org>
2026-07-29 00:05:47 -0700

    net/disk_cache/sql: Delete live shared cache entries on DeleteLiveEntry
    When a live entry is deleted from SqlPersistentStore via
    DeleteLiveEntry, also notify SqlSharedCacheManager to delete the
    associated shared cache resource if present.
    
    To achieve this:
    - Updated kDeleteLiveEntry_DeleteFromResources query to return
      shared_cache_db_id and shared_cache_row_id when shared cache mode is
      enabled.
    - Defined DeleteLiveEntryResult struct and updated DeleteLiveEntry
      across Backend, BackendShard, and SqlPersistentStore to collect
      deleted shared cache resources and notify SqlSharedCacheManager via
      OnSharedCacheResourcesDeleted.
    - Added unit test DeleteLiveEntryDeletesSharedCacheResource under
      SqlPersistentStoreSharedCacheTest.
    
    Bug: 473666511
    Change-Id: Iab486a43d43a13ab7c3a9764b7e7b18f39d5749a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8153379
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670023}

--- net/disk_cache/sql/sql_persistent_store.cc
+++ net/disk_cache/sql/sql_persistent_store.cc
@@ -270,7 +270,8 @@ void SqlPersistentStore::DeleteDoomedEntry(const CacheEntryKey& key,
 
 void SqlPersistentStore::DeleteLiveEntry(const CacheEntryKey& key,
                                          ErrorCallback callback) {
-  GetShard(key).DeleteLiveEntry(key, std::move(callback));
+  GetShard(key).DeleteLiveEntry(
+      key, WrapCallbackWithSharedCacheDelete(std::move(callback)));
 }
 
 void SqlPersistentStore::DeleteAllEntries(ErrorCallback callback) {
--- net/disk_cache/sql/sql_persistent_store_backend_shard.h
+++ net/disk_cache/sql/sql_persistent_store_backend_shard.h
@@ -67,7 +67,8 @@ class SqlPersistentStore::BackendShard {
   void DeleteDoomedEntry(const CacheEntryKey& key,
                          ResId res_id,
                          DeletedSharedCacheResourceOrErrorCallback callback);
-  void DeleteLiveEntry(const CacheEntryKey& key, ErrorCallback callback);
+  void DeleteLiveEntry(const CacheEntryKey& key,
+                       DeletedSharedCacheResourcesOrErrorCallback callback);
   void DeleteAllEntries(ErrorCallback callback);
   void DeleteLiveEntriesBetween(base::Time initial_time,
                                 base::Time end_time,
@@ -241,6 +242,10 @@ class SqlPersistentStore::BackendShard {
   base::OnceCallback<void(HashAndResIdListOrErrorAndStoreStatus)>
   WrapErrorCallbackToRemoveFromIndex(ErrorCallback callback,
                                      IndexMismatchLocation location);
+  base::OnceCallback<void(DeleteLiveEntryResultOrErrorAndStoreStatus)>
+  WrapErrorCallbackToRemoveFromIndex(
+      DeletedSharedCacheResourcesOrErrorCallback callback,
+      IndexMismatchLocation location);
   void OnEvictionFinished(EvictionResultCallback callback,
                           EvictionResultWithMetadata result);
   void RecordIndexMismatch(IndexMismatchLocation location);
