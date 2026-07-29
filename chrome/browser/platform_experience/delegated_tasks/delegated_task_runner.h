1157eea2d6cb3daa77bb7d0a98ec9804c05561b7
Quinton Lucyk <qlucyk@google.com>
2026-07-29 12:47:29 -0700

    Offload PehLauncher to ThreadPool in DelegatedTaskRunner
    Offload PehLauncher's blocking methods to ThreadPool using
    base::SequenceBound to avoid main thread I/O.
    An adapter callback ensures child processes are terminated if the runner
    is destroyed or times out while launch is in-flight
    
    Bug: 525018433
    Change-Id: Ie2a6f9419a20de8517417e5a8ece54c157f66b3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163324
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Quinton Lucyk <qlucyk@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670515}

--- chrome/browser/platform_experience/delegated_tasks/delegated_task_runner.h
+++ chrome/browser/platform_experience/delegated_tasks/delegated_task_runner.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/process/process.h"
+#include "base/threading/sequence_bound.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
 #include "base/win/object_watcher.h"
@@ -54,6 +55,9 @@ class DelegatedTaskRunner : public base::win::ObjectWatcher::Delegate {
                    DelegatedTaskCompletionCallback callback);
 
  private:
+  void OnProcessLaunched(base::Process process);
+  void OnBinaryPathRetrieved(const base::FilePath& peh_binary_path);
+
   void CleanupAndReturnResult(
       DelegatedTaskExitCodeOrStatus exit_code_or_status);
 
@@ -66,7 +70,7 @@ class DelegatedTaskRunner : public base::win::ObjectWatcher::Delegate {
   DelegatedTaskCompletionCallback completion_callback_;
 
   std::unique_ptr<DelegatedTask> task_;
-  std::unique_ptr<PehLauncher> peh_launcher_;
+  base::SequenceBound<std::unique_ptr<PehLauncher>> peh_launcher_;
 
   base::WeakPtrFactory<DelegatedTaskRunner> weak_factory_{this};
 };
