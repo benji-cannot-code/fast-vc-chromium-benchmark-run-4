309a7dd4433e6590fd1cf87ffd70b89f89358cc1
Saifuddin Hitawala <hitawala@chromium.org>
2026-07-29 07:13:51 -0700

    [gpu] Cleanup launched shared image killswitches
    Remove kAllowEs3F16CoreTypeForGlSi and
    kD3DBackingUploadWithUpdateSubresource killswitches which have been
    enabled by default for a while.
    
    Bug: None
    Change-Id: I1425b340f2f6c0867877dd714b241c260bb598f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162549
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670224}

--- gpu/command_buffer/service/shared_image/d3d_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/d3d_image_backing_factory.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/service/shared_image/d3d_image_backing.h"
 #include "gpu/command_buffer/service/shared_image/d3d_image_utils.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h"
-#include "gpu/config/gpu_finch_features.h"
 #include "gpu/ipc/common/dxgi_helpers.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/buffer_usage_util.h"
@@ -232,12 +231,6 @@ DXGI_FORMAT GetDXGITypelessFormat(viz::SharedImageFormat format) {
   return DXGI_FORMAT_UNKNOWN;
 }
 
-bool UseUpdateSubresource1(const GpuDriverBugWorkarounds& workarounds) {
-  return base::FeatureList::IsEnabled(
-             features::kD3DBackingUploadWithUpdateSubresource) &&
-         !workarounds.disable_d3d11_update_subresource1;
-}
-
 // CPU_READ is needed for RenderableGMBVideoFramePool case where the DXGI handle
 // is mapped on GPU process.
 constexpr SharedImageUsageSet kSupportedUsage =
@@ -271,7 +264,7 @@ D3DImageBackingFactory::D3DImageBackingFactory(
       dxgi_shared_handle_manager_(std::move(dxgi_shared_handle_manager)),
       angle_d3d11_device_(gl::QueryD3D11DeviceObjectFromANGLE()),
       gl_format_caps_(gl_format_caps),
-      use_update_subresource1_(UseUpdateSubresource1(workarounds)),
+      use_update_subresource1_(!workarounds.disable_d3d11_update_subresource1),
       enable_webnn_only_d3d_factory_(enable_webnn_only_d3d_factory) {
   CHECK(angle_d3d11_device_ || enable_webnn_only_d3d_factory)
       << "D3DImageBackingFactory requires a D3D11 device.";
