23682dc64c25f7d42c6d9ad161de45bc036fa66f
Justin Cohen <justincohen@google.com>
2026-07-29 14:40:52 -0700

    ios: Replace XCUIDeviceButtonHome with springboard activate
    We speculate that XCUIDeviceButtonHome pressButton is broken when Xcode
    27 is installed on devices older than iOS 27, even after downgrading to
    older Xcode versions. Replacing with activating com.apple.springboard to
    background applications.
    
    Uses AppLaunchManager backgroundApplication helper in EG promo test and
    guards Springboard activation workaround with @available(iOS 27, *).
    
    Bug: 540470551
    Change-Id: I56def02429236f9d5da04c3f2f171cecf53150ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8166982
    Commit-Queue: Justin Cohen <justincohen@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670615}

--- ios/testing/earl_grey/app_launch_manager.h
+++ ios/testing/earl_grey/app_launch_manager.h
@@ -66,6 +66,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // no-op.
 - (void)backgroundAndForegroundApp;
 
+// Moves app to background and waits until it is backgrounded.
+- (BOOL)backgroundApplication;
+
 // Adds an observer for AppLaunchManager.
 - (void)addObserver:(id<AppLaunchManagerObserver>)observer;
 
