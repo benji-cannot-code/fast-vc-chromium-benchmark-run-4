bab07f65f453f9c62e04afe967d2876eac43fedd
Atalyk Akash <atalyk@google.com>
2026-07-29 10:48:03 -0700

    [AtMemory][Android] Fix flyout chip title alignment
    The change fixes the alignment of the title in the flyout screen. After
    discussing with the UX team it was said to be two lines and aligning
    start.
    
    Before: screen/5afav3horcp68
    After: screen/3gbmst3jrq9eg
    Bug: 538100057
    Change-Id: I11e495385b8800fe4f906b854923a9f999b5ed02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8157206
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Atalyk Akash <atalyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670401}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryFlyoutView.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryFlyoutView.java
@@ -158,6 +158,8 @@ public class AtMemoryFlyoutView extends LinearLayout {
         chip.setId(View.generateViewId());
 
         TextView primaryTextView = chip.getPrimaryTextView();
+        primaryTextView.setMaxLines(2);
+        primaryTextView.setEllipsize(TextUtils.TruncateAt.END);
         primaryTextView.setText(suggestion.getLabel());
 
         TextView secondaryTextView = chip.getSecondaryTextView();
