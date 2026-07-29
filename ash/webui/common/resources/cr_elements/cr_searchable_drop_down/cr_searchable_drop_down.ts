3a1c1031a20d2846f797ee96231e03b3041e12bc
James Hawkins <jhawkins@google.com>
2026-07-29 15:42:51 -0700

    typescript: Modernize property declarations in ash cr_elements
    Bug: none
    Test: browser_tests --gtest_filter=*CrElements*
    Change-Id: I11e112f534beaa985ecbdaa67ce7166071c82009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163223
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670650}

--- ash/webui/common/resources/cr_elements/cr_searchable_drop_down/cr_searchable_drop_down.ts
+++ ash/webui/common/resources/cr_elements/cr_searchable_drop_down/cr_searchable_drop_down.ts
@@ -28,6 +28,7 @@ import '//resources/polymer/v3_0/iron-dropdown/iron-dropdown.js';
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
 import '//resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 
+import {assert} from '//resources/js/assert.js';
 import type {IronDropdownElement} from '//resources/polymer/v3_0/iron-dropdown/iron-dropdown.js';
 import type {DomRepeatEvent} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -159,7 +160,7 @@ export class CrSearchableDropDownElement extends PolymerElement {
   declare private opened_: boolean;
   private openDropdownTimeoutId_: number = 0;
   private resizeObserver_: ResizeObserver|null = null;
-  private pointerDownListener_: (e: Event) => void;
+  private pointerDownListener_: ((e: Event) => void)|null = null;
 
   override connectedCallback() {
     super.connectedCallback();
@@ -180,8 +181,10 @@ export class CrSearchableDropDownElement extends PolymerElement {
   override disconnectedCallback() {
     super.disconnectedCallback();
 
+    assert(this.pointerDownListener_);
     document.removeEventListener('pointerdown', this.pointerDownListener_);
-    this.resizeObserver_!.unobserve(this.$.search);
+    assert(this.resizeObserver_);
+    this.resizeObserver_.unobserve(this.$.search);
   }
 
   /**
