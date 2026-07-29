f6b58f5b77a185194371443de465923d1a32fefd
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-29 06:19:39 -0700

    [unsafe-buffers] Update allow_unsafe_buffers in updater launcher_main.c
    Keep #pragma allow_unsafe_buffers in launcher_main.c with a comment
    explaining that it is a C file and cannot use C++ base/compiler_specific.h.
    
    Bug: 351564777, 501112808
    Change-Id: I38e88e36f8d2a52f67488b2dd65628ab0fd30092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8153442
    Reviewed-by: Adam Norberg <norberg@google.com>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670183}

--- chrome/updater/mac/launcher_main.c
+++ chrome/updater/mac/launcher_main.c
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This is C code and has minimal dependencies, so it cannot use C++ safety
+// utilities like base::span or the UNSAFE_TODO macro. As a consequence, this
+// pragma cannot be removed.
 #ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
 #pragma allow_unsafe_buffers
 #endif
 
