b4f483ee5365621b2910699e65d61b69ae4f8693
Alison Gale <agale@chromium.org>
2026-07-29 07:34:25 -0700

    Clean up SplitViewLinkOpen flag
    Change-Id: If1f92d844091a63620c3421b8f26873cb9d341e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160153
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670242}

--- third_party/blink/renderer/core/loader/navigation_policy_test.cc
+++ third_party/blink/renderer/core/loader/navigation_policy_test.cc
@@ -98,17 +98,6 @@ class NavigationPolicyTest : public testing::Test {
   WebWindowFeatures features;
 };
 
-class NavigationPolicyWithSplitViewEnabledTest : public NavigationPolicyTest {
- protected:
-  void SetUp() override {
-    WebRuntimeFeatures::EnableFeatureFromString("SplitViewLinkOpen", true);
-  }
-
-  void TearDown() override {
-    WebRuntimeFeatures::EnableFeatureFromString("SplitViewLinkOpen", false);
-  }
-};
-
 TEST_F(NavigationPolicyTest, LeftClick) {
   int modifiers = 0;
   WebMouseEvent::Button button = WebMouseEvent::Button::kLeft;
@@ -437,9 +426,7 @@ TEST_F(NavigationPolicyTest, EventAltClickWithDifferentUserEvent) {
             GetPolicyFromEvent(modifiers, button, 0, button));
 }
 
-
-TEST_F(NavigationPolicyWithSplitViewEnabledTest,
-       EventAltControlOrMetaLeftClick) {
+TEST_F(NavigationPolicyTest, EventAltControlOrMetaLeftClick) {
 #if BUILDFLAG(IS_MAC)
   int modifiers = WebInputEvent::kMetaKey | WebInputEvent::kAltKey;
 #else
@@ -450,8 +437,7 @@ TEST_F(NavigationPolicyWithSplitViewEnabledTest,
             NavigationPolicyFromEvent(GetEvent(modifiers, button)));
 }
 
-TEST_F(NavigationPolicyWithSplitViewEnabledTest,
-       EventAltControlOrMetaLeftClickWithUserEvent) {
+TEST_F(NavigationPolicyTest, EventAltControlOrMetaLeftClickWithUserEvent) {
 #if BUILDFLAG(IS_MAC)
   int modifiers = WebInputEvent::kMetaKey | WebInputEvent::kAltKey;
 #else
--- ui/base/window_open_disposition_utils.cc
+++ ui/base/window_open_disposition_utils.cc
@@ -5,9 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/window_open_disposition_utils.h"
 
-#include "base/feature_list.h"
 #include "build/build_config.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/events/event_constants.h"
 
 namespace ui {
@@ -27,8 +25,7 @@ WindowOpenDisposition DispositionFromClick(
   const bool new_tab_modifier = middle_button || ctrl_key;
   const bool split_view_modifier = ctrl_key;
 #endif
-  if (split_view_modifier && alt_key && !shift_key &&
-      base::FeatureList::IsEnabled(features::kSplitViewLinkOpen)) {
+  if (split_view_modifier && alt_key && !shift_key) {
     return WindowOpenDisposition::NEW_SPLIT_VIEW;
   }
   if (new_tab_modifier) {
