34a8699bc1dc1e3c6dea1a6b431aca1160529b65
Stefano Duo <stefanoduo@google.com>
2026-07-29 03:38:42 -0700

    Move CronetPccAuditLogger#initialize's latency off the critical path
    There is no need for CronetEngine.Builder#build to wait for
    CronetPccAuditLogger's initialization. Instead, we must only guarantee
    that CronetPccAuditLogger is ready prior to starting a net::UrlRequest
    (or BidirectionalStream) in native code. Restructure
    CronetPccAuditLogger's initialization to follow the common pattern for
    Cronet's global singletons (e.g., NetworkChangeNotifier and HttpFlags).
    
    Bug: 439562106
    Cq-Include-Trybots: luci.chrome.try:cronet-arm64-gn2bp-debug
    Change-Id: Idc503758277bd2859a5b85967a3a304dd50eed15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8052648
    Auto-Submit: Sebastian Poreba <sporeba@google.com>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670111}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequest.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequest.java
@@ -251,7 +251,6 @@ public final class CronetUrlRequest extends ExperimentalUrlRequest {
                                             mDictionaryId,
                                             mNetworkHandle);
                     mRequestContext.onRequestStarted();
-                    CronetPccAuditLogger.maybeWrite(mInitialUrl);
                     if (!CronetUrlRequestJni.get()
                             .setHttpMethod(mUrlRequestAdapter, mInitialMethod)) {
                         throw new IllegalArgumentException("Invalid http method " + mInitialMethod);
@@ -634,7 +633,6 @@ public final class CronetUrlRequest extends ExperimentalUrlRequest {
 
         // Have to do this after creating responseInfo.
         mUrlChain.add(newLocation);
-        CronetPccAuditLogger.maybeWrite(newLocation);
         Runnable task =
                 new Runnable() {
                     @Override
--- components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetUrlRequestContext.java
@@ -259,7 +259,6 @@ public class CronetUrlRequestContext extends CronetEngineBase {
             mNetworkQualityEstimatorEnabled = builder.networkQualityEstimatorEnabled();
             boolean triggeredInitialization =
                     CronetLibraryLoader.ensureInitialized(builder.getContext(), builder);
-            CronetPccAuditLogger.initialize();
             if (builder.httpCacheMode() == HttpCacheType.DISK) {
                 mInUseStoragePath = builder.storagePath();
                 synchronized (sInUseStoragePaths) {
