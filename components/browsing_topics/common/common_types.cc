89a059fda820601ec109db8a11bf06ed4010ea9f
Yao Xiao <yaoxia@chromium.org>
2026-07-29 10:18:52 -0700

    [Topics] Remove dead code in //components/browsing_topics
    The Topics API is being deprecated and was effectively disabled after
    the //chrome layer hooks were removed in https://crrev.com/c/8140301.
    
    This CL continues the removal of dead code within
    //components/browsing_topics. To keep the review scope manageable, some
    code is temporarily retained because it is still referenced by other
    privacy_sandbox components and test files.
    
    The remaining Topics logic in the rest of the codebase will be removed
    in separate, follow-up CLs.
    
    I2D&R:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/_R85yctz4Rs
    
    Bug: 461709147
    Change-Id: Iacc7e10f988635b534a8a1cdbf2393d549329379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8165426
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670360}

--- components/browsing_topics/common/common_types.cc
+++ components/browsing_topics/common/common_types.cc
@@ -5,17 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/browsing_topics/common/common_types.h"
 
-#include "third_party/blink/public/common/features.h"
-
 namespace browsing_topics {
-
-ConfigVersion CurrentConfigVersion() {
-  if (!blink::features::kBrowsingTopicsPrioritizedTopicsList.Get().empty()) {
-    return ConfigVersion::kUsePrioritizedTopicsList;
-  } else {
-    return ConfigVersion::kInitial;
-  }
-}
 
 ApiUsageContextQueryResult::ApiUsageContextQueryResult() = default;
 
