d76dccf50e513999947d825f63afb1a6682a6d56
Jinsuk Kim <jinsukkim@google.com>
2026-07-29 13:22:03 -0700

    [Vertical Tabs] Remove an artifact visible with Fusebox
    When Fusebox appears, ToolbarTablet's own background is set to
    transparent. The gap [0, paddingStart] on the left end now has no
    background drawn behind it, it creates a visible transparent
    gap/artifact between ToolbarTablet and toolbar_tablet_layout.
    
    To prevent it, this CL sets the horizontal padding directly on
    R.id.toolbar_tablet_layout instead of ToolbarTablet. The padding was
    required for toolbar buttons, so this should have the same effect
    since R.id.toolbar_tablet_layout is the actual container of buttons.
    
    Bug: 540347441
    Change-Id: I6b4d8c9df65f508abe7f38da55475d874387873f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160979
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670536}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarTablet.java
@@ -87,6 +87,13 @@ public class ToolbarTablet extends ToolbarLayout {
     private ImageButton mBookmarkButton;
     private View mFixedHeightBackground;
 
+    /**
+     * The inner layout containing toolbar components, used to apply horizontal padding to. The
+     * padding used to set to ToolbarTablet, but it caused a transparent gap on the left when
+     * Fusebox gets enabled and turned the background transparent.
+     */
+    private View mToolbarTabletLayout;
+
     private boolean mIsInTabSwitcherMode;
     private boolean mToolbarButtonsVisible;
     private boolean mOptionalButtonForciblyHidden;
@@ -146,6 +153,7 @@ public class ToolbarTablet extends ToolbarLayout {
 
         mBookmarkButton = findViewById(R.id.bookmark_button);
         mFixedHeightBackground = findViewById(R.id.toolbar_tablet_fixed_height_bg);
+        mToolbarTabletLayout = findViewById(R.id.toolbar_tablet_layout);
 
         // Initialize values needed for showing/hiding toolbar buttons when the activity size
         // changes.
@@ -452,7 +460,7 @@ public class ToolbarTablet extends ToolbarLayout {
         mToolbarWidthConsumers[ToolbarComponentId.TAB_SWITCHER] = tabSwitcherButtonCoordinator;
         mToolbarWidthConsumers[ToolbarComponentId.MENU] = menuButtonCoordinator;
         mToolbarWidthConsumers[ToolbarComponentId.PADDING] =
-                new ToolbarPaddingWidthConsumer(this, mStartPaddingWithButtons);
+                new ToolbarPaddingWidthConsumer(mToolbarTabletLayout, mStartPaddingWithButtons);
     }
 
     @Override
@@ -540,9 +548,8 @@ public class ToolbarTablet extends ToolbarLayout {
     }
 
     private int getControlContainerMargin() {
-        View toolbarTabletLayout = findViewById(R.id.toolbar_tablet_layout);
-        if (toolbarTabletLayout == null) return 0;
-        var lp = (MarginLayoutParams) toolbarTabletLayout.getLayoutParams();
+        if (mToolbarTabletLayout == null) return 0;
+        var lp = (MarginLayoutParams) mToolbarTabletLayout.getLayoutParams();
         return lp != null ? lp.leftMargin + lp.rightMargin : 0;
     }
 
@@ -772,7 +779,7 @@ public class ToolbarTablet extends ToolbarLayout {
         return mGlicActionChip;
     }
 
-    private class ToolbarPaddingWidthConsumer implements ToolbarWidthConsumer {
+    private static class ToolbarPaddingWidthConsumer implements ToolbarWidthConsumer {
         private final View mToolbarView;
         private final int mHorizontalPadding;
         private boolean mHasSpaceToShow;
@@ -798,7 +805,10 @@ public class ToolbarTablet extends ToolbarLayout {
             assert availableWidth >= 0;
             int paddingWidth = Math.min(availableWidth, 2 * mHorizontalPadding);
             mToolbarView.setPaddingRelative(
-                    paddingWidth / 2, getPaddingTop(), paddingWidth / 2, getPaddingBottom());
+                    paddingWidth / 2,
+                    mToolbarView.getPaddingTop(),
+                    paddingWidth / 2,
+                    mToolbarView.getPaddingBottom());
             mHasSpaceToShow = paddingWidth > 0;
             return paddingWidth;
         }
@@ -920,11 +930,11 @@ public class ToolbarTablet extends ToolbarLayout {
     private void setStartPaddingBasedOnButtonVisibility(boolean buttonsVisible) {
         buttonsVisible = buttonsVisible || mHomeButton.getVisibility() == View.VISIBLE;
 
-        this.setPaddingRelative(
+        mToolbarTabletLayout.setPaddingRelative(
                 buttonsVisible ? mStartPaddingWithButtons : mStartPaddingWithoutButtons,
-                getPaddingTop(),
-                ViewCompat.getPaddingEnd(this),
-                getPaddingBottom());
+                mToolbarTabletLayout.getPaddingTop(),
+                ViewCompat.getPaddingEnd(mToolbarTabletLayout),
+                mToolbarTabletLayout.getPaddingBottom());
     }
 
     /**
@@ -1071,9 +1081,13 @@ public class ToolbarTablet extends ToolbarLayout {
         mToolbarWidthConsumers[ToolbarComponentId.MENU] = coordinator;
     }
 
+    void setToolbarTabletLayoutForTesting(View view) {
+        mToolbarTabletLayout = view;
+    }
+
     void ensurePaddingWidthConsumer() {
         mToolbarWidthConsumers[ToolbarComponentId.PADDING] =
-                new ToolbarPaddingWidthConsumer(this, mStartPaddingWithButtons);
+                new ToolbarPaddingWidthConsumer(mToolbarTabletLayout, mStartPaddingWithButtons);
     }
 
     void ensureLocationBarMidWidthConsumer() {
