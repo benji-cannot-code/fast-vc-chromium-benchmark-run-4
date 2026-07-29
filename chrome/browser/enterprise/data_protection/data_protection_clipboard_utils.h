89bb2147d305c692c761347f25277f8595dabdcd
Elias Homsi <eliashomsi@google.com>
2026-07-29 07:22:15 -0700

    Reland "[Copy Trigger] Integrate Content Analysis into Clipboard Copy"
    This change adds support for Enterprise Content Analysis checks when
    copying data to the clipboard. When copy trigger and feature flag are
    enabled, a scan is triggered. While the scan is ongoing, a placeholder
    message is written to the clipboard.
    
    Skip-Translation-Screenshots-Check because some strings are not
    displayed in the UI. Instead, it may become visible to the user anywhere
    they attempt to paste the data outside of Chromium.
    
    NO_IFTTT=outdated
    NO_IFTTT=outdated
    
    Skip-Translation-Screenshots-Check: True
    Bug: 325455508
    Change-Id: I3a738faf64a5d98a4168288e9656d7793970a834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158503
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670229}

--- chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.h
+++ chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard_buffer.h"
 #include "ui/base/clipboard/clipboard_metadata.h"
 
+class Profile;
+
 namespace content {
 class ClipboardEndpoint;
 class RenderFrameHost;
@@ -27,8 +29,8 @@ namespace enterprise_data_protection {
 // This function checks if a paste is allowed to proceed according to the
 // following policies:
 // - DataLeakPreventionRulesList
-// - OnBulkDataEntryEnterpriseConnector
 // - DataControlsRules
+// - OnBulkDataEntryEnterpriseConnector
 //
 // This function will always call `callback` after policies are evaluated with
 // true if the paste is allowed to proceed and false if it is not. However, if
@@ -69,6 +71,7 @@ bool IsPastePolicyCheckRequired(const content::ClipboardEndpoint& source,
 // written to the OS clipboard according to the following policies:
 // - CopyPreventionSettings
 // - DataControlsRules
+// - CopyIfAllowedByContentAnalysis
 //
 // If the copy is not allowed, `callback` is called with a replacement string
 // that should instead be put into the OS clipboard.
