1231273be2837b29d8bf7abd68d5dc1e062cb0a0
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-07-29 12:40:52 -0700

    [iOS] Wire up Google Lens task navigation action and completion trigger in Level Up
    Connects the "Google Lens" task card in Level Up:
    
    Change-Id: I62bf86e9bd606a297fdcd56afdeefdee8b99d534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167481
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670506}

--- ios/chrome/browser/level_up/model/tasks/lens_search_task_info.mm
+++ ios/chrome/browser/level_up/model/tasks/lens_search_task_info.mm
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/functional/bind.h"
 #import "base/functional/callback_helpers.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/level_up/model/task_info.h"
 #import "ios/chrome/browser/level_up/model/tasks/task_factories.h"
+#import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/shared/public/commands/new_tab_page_commands.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -32,13 +35,19 @@ class LensSearchTaskInfo : public TaskInfo {
   LevelUpTaskCategory GetCategory() const override {
     return LevelUpTaskCategory::kSearch;
   }
-  std::string GetTriggerUserAction() const override { return ""; }
+  std::string GetTriggerUserAction() const override {
+    return "Mobile.LensOverlay.NewResult";
+  }
   std::string GetCompletionSnackbarMessage() const override {
     return l10n_util::GetStringUTF8(
         IDS_IOS_LEVEL_UP_TASK_COMPLETED_LENS_SEARCH);
   }
   TaskInfo::NavigationAction GetNavigationAction() const override {
-    return base::DoNothing();
+    return base::BindRepeating(^(CommandDispatcher* dispatcher) {
+      id<NewTabPageCommands> handler =
+          HandlerForProtocol(dispatcher, NewTabPageCommands);
+      [handler presentLensIconBubble];
+    });
   }
 };
 
