15faabca924e40001e1613aea9e7f7bb46a52613
Adam Rice <ricea@chromium.org>
2026-07-29 10:45:01 -0700

    Remove traces of Python server from //net BUILD file
    This change improves readability by cleaning up dead code. Specifically:
    - Removed `SetPythonPathInEnvironment` completely, along with its test,
      as it was no longer used by anything else.
    - Removed an unused Mac-specific argument from `GetPython3Command`.
    - Removed the unused `//net:net_test_config` config and its usages from
      `BUILD.gn` files.
    - Removed unused `#include`s in various files.
    
    Change-Id: Iae0b4497bfcf08c02fb90f4b0790dd9e97cbbbf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8119591
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670396}

--- net/test/python_utils.h
+++ net/test/python_utils.h
@@ -6,18 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_TEST_PYTHON_UTILS_H_
 #define NET_TEST_PYTHON_UTILS_H_
 
-#include <vector>
-
-#include "base/environment.h"
-
 namespace base {
 class CommandLine;
-class FilePath;
 }
-
-// Modifies |map| to use the specified Python path.
-void SetPythonPathInEnvironment(const std::vector<base::FilePath>& python_path,
-                                base::EnvironmentMap* map);
 
 // Returns the command that should be used to launch Python 3.
 [[nodiscard]] bool GetPython3Command(base::CommandLine* python_cmd);
