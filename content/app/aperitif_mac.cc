f43a104bb1b828b2a2e83558b709a275b8d018bf
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-07-29 05:56:02 -0700

    [unsafe-buffers] Remove allow_unsafe_pragmas from content/
    Remove #pragma allow_unsafe_buffers and #pragma allow_unsafe_libc_calls
    from content/ files, replacing them with UNSAFE_TODO() where necessary.
    
    Bug: 351564777, 501112808
    Change-Id: Iaaa9024b05b1cd1d375304b22895da376a458033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8152304
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670165}

--- content/app/aperitif_mac.cc
+++ content/app/aperitif_mac.cc
@@ -19,12 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/cdefs.h>
 
 #include "base/allocator/early_zone_registration_apple.h"
+#include "base/compiler_specific.h"
 #include "sandbox/mac/seatbelt_exec.h"
-
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
-#pragma allow_unsafe_libc_calls
-#endif
 
 extern "C" {
 // abort_report_np() records the message in a special section that both the
@@ -42,12 +38,12 @@ namespace {
   va_list valist;
   va_start(valist, format);
   char message[4096];
-  int rv = vsnprintf(message, sizeof(message), format, valist);
+  int rv = UNSAFE_TODO(vsnprintf(message, sizeof(message), format, valist));
   va_end(valist);
   if (rv >= 0) {
-    fprintf(stderr, "aperitif: %s\n", message);
+    UNSAFE_TODO(fprintf(stderr, "aperitif: %s\n", message));
     fflush(stderr);
-    abort_report_np("aperitif: %s", message);
+    UNSAFE_TODO(abort_report_np("aperitif: %s", message));
   }
   abort();
 }
--- content/app/ios/appex/child_process_bridge.mm
+++ content/app/ios/appex/child_process_bridge.mm
@@ -3,15 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/342213636): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "content/app/ios/appex/child_process_bridge.h"
 
 #include <pthread.h>
 #include <xpc/xpc.h>
+
+#include <vector>
 
 #include "base/apple/bundle_locations.h"
 #include "base/apple/mach_port_rendezvous_ios.h"
@@ -27,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Leaked variables for now.
 static size_t g_argc = 0;
-static const char** g_argv = nullptr;
+static std::vector<const char*> g_argv;
 static pthread_t g_main_thread;
 static id<ChildProcessExtension> g_swift_process;
 static xpc_connection_t g_connection;
@@ -72,7 +69,7 @@ extern "C" IOS_INIT_EXPORT void ChildProcessInit(
 }
 
 void* RunMain(void* data) {
-  ChildProcessMain((int)g_argc, g_argv);
+  ChildProcessMain((int)g_argc, g_argv.data());
   return nullptr;
 }
 
@@ -83,7 +80,7 @@ extern "C" IOS_INIT_EXPORT void ChildProcessHandleNewConnection(
     CHECK_EQ(msg_type, XPC_TYPE_DICTIONARY);
     xpc_object_t args_array = xpc_dictionary_get_array(msg, "args");
     g_argc = xpc_array_get_count(args_array);
-    g_argv = new const char*[g_argc];
+    g_argv.resize(g_argc);
     for (size_t i = 0; i < g_argc; ++i) {
       g_argv[i] = strdup(xpc_array_get_string(args_array, i));
     }
