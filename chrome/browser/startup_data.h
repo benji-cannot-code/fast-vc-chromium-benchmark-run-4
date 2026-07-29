f4a11b964aeeec4911f1a83b7f19399a6e44cc13
Greg Thompson <grt@chromium.org>
2026-07-29 05:31:17 -0700

    Improve deps for chrome/browser:startup_data
    This target only needs leveldb_proto on Android, so conditionally
    include the header only on Android builds and conditionally depend on
    the target.
    
    Also, switch from `deps` to `public_deps` since those dependencies are
    needed to include the target's public header.
    
    This change also add a missing forward decl that was uncovered.
    
    Bug: none
    Change-Id: I1907cc63a2b5c9c246e8b3db7f205072175ca49e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163669
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670154}

--- chrome/browser/startup_data.h
+++ chrome/browser/startup_data.h
@@ -10,8 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
-#include "components/leveldb_proto/public/proto_database_provider.h"
 #include "extensions/buildflags/buildflags.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "components/leveldb_proto/public/proto_database_provider.h"  // nogncheck
+#endif
 
 #if BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
 namespace extensions {
