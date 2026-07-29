1d188b46510fc6af574b6a8dac6a5fb953bc9861
anushamuley <anushamuley@google.com>
2026-07-29 07:27:47 -0700

    [browser_actuator] Implement Handler and Session Payload Routing
    Implements payload routing in TransportSessionImpl to lazily instantiate
    handlers from factories and dispatch messages to them for the specific
    payload types they would like to listen for. Updates session state for
    downstream messages.
    
    Bug: 534398806
    Change-Id: I3b789009da02f41238e08c6fa251f4469f87601d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131539
    Commit-Queue: Anusha Muley <anushamuley@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670236}

---
+++ components/browser_actuator/test_support/mock_transport_handler.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/browser_actuator/test_support/mock_transport_handler.h"
+
+#include <utility>
+
+namespace browser_actuator {
+
+MockTransportHandler::MockTransportHandler() = default;
+MockTransportHandler::~MockTransportHandler() = default;
+
+CallbackTransportHandler::CallbackTransportHandler(
+    base::OnceClosure on_message_cb)
+    : on_message_cb_(std::move(on_message_cb)) {}
+
+CallbackTransportHandler::~CallbackTransportHandler() = default;
+
+void CallbackTransportHandler::OnMessage(std::string_view payload) {
+  if (on_message_cb_) {
+    std::move(on_message_cb_).Run();
+  }
+}
+
+}  // namespace browser_actuator
