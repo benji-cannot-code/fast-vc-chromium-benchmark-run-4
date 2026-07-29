26fdbca0c44966cf878317f8c36cb4f4f0aeb1ba
Jack Thiesen <jthiesen@chromium.org>
2026-07-29 10:53:52 -0700

    [Vertical Tabs] Add Default Enablement Parameter
    Add "enable_by_default" parameter to AndroidVerticalTabs feature. If
    true, VT is enabled by default. User preferences will override this
    setting.
    
    Bug: 523389406, 509226293
    Change-Id: Ic75743e45cd71dc97335991478da9bc3aea0ac4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162558
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670407}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabUtilsUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabUtilsUnitTest.java
@@ -76,8 +76,7 @@ public class VerticalTabUtilsUnitTest {
     @Config(qualifiers = "sw600dp")
     public void testIsVerticalTabsEnabled_FalseWhenNotEligible() {
         FeatureOverrides.disable(ChromeFeatureList.ANDROID_VERTICAL_TABS);
-        ChromeSharedPreferences.getInstance()
-                .writeBoolean(ChromePreferenceKeys.VERTICAL_TABS_ENABLED, true);
+        VerticalTabUtils.setVerticalTabsEnabled(true);
         assertFalse(VerticalTabUtils.isVerticalTabsEnabled(mContext));
     }
 
@@ -86,8 +85,7 @@ public class VerticalTabUtilsUnitTest {
     @Config(qualifiers = "sw600dp")
     public void testIsVerticalTabsEnabled_FalseWhenPreferenceDisabled() {
         FeatureOverrides.enable(ChromeFeatureList.ANDROID_VERTICAL_TABS);
-        ChromeSharedPreferences.getInstance()
-                .writeBoolean(ChromePreferenceKeys.VERTICAL_TABS_ENABLED, false);
+        VerticalTabUtils.setVerticalTabsEnabled(false);
         assertFalse(VerticalTabUtils.isVerticalTabsEnabled(mContext));
     }
 
@@ -96,8 +94,7 @@ public class VerticalTabUtilsUnitTest {
     @Config(qualifiers = "sw600dp")
     public void testIsVerticalTabsEnabled_Enabled() {
         FeatureOverrides.enable(ChromeFeatureList.ANDROID_VERTICAL_TABS);
-        ChromeSharedPreferences.getInstance()
-                .writeBoolean(ChromePreferenceKeys.VERTICAL_TABS_ENABLED, true);
+        VerticalTabUtils.setVerticalTabsEnabled(true);
         assertTrue(VerticalTabUtils.isVerticalTabsEnabled(mContext));
     }
 
@@ -179,5 +176,56 @@ public class VerticalTabUtilsUnitTest {
         VerticalTabUtils.recordLayoutToggle(entryPoint, isEnabling);
 
         histogramWatcher.assertExpected();
+    }
+
+    @Test
+    @SmallTest
+    @Config(qualifiers = "sw600dp")
+    public void testIsVerticalTabsEnabled_DefaultFalseWhenEligible() {
+        FeatureOverrides.enable(ChromeFeatureList.ANDROID_VERTICAL_TABS);
+        FeatureOverrides.overrideParam(
+                ChromeFeatureList.ANDROID_VERTICAL_TABS,
+                VerticalTabUtils.ENABLE_BY_DEFAULT_PARAM,
+                false);
+
+        // Preference is not set, should default to false.
+        assertFalse(VerticalTabUtils.isVerticalTabsEnabled(mContext));
+    }
+
+    @Test
+    @SmallTest
+    @Config(qualifiers = "sw600dp")
+    public void testIsVerticalTabsEnabled_DefaultTrueWhenEligible() {
+        FeatureOverrides.enable(ChromeFeatureList.ANDROID_VERTICAL_TABS);
+        FeatureOverrides.overrideParam(
+                ChromeFeatureList.ANDROID_VERTICAL_TABS,
+                VerticalTabUtils.ENABLE_BY_DEFAULT_PARAM,
+                true);
+
+        // Preference is not set, should default to true when eligible.
+        assertTrue(VerticalTabUtils.isVerticalTabsEnabled(mContext));
+    }
+
+    @Test
+    @SmallTest
+    @Config(qualifiers = "sw600dp")
+    public void testIsVerticalTabsEnabled_RespectsPrefOverDefault() {
+        FeatureOverrides.enable(ChromeFeatureList.ANDROID_VERTICAL_TABS);
+        FeatureOverrides.overrideParam(
+                ChromeFeatureList.ANDROID_VERTICAL_TABS,
+                VerticalTabUtils.ENABLE_BY_DEFAULT_PARAM,
+                true);
+
+        // Explicitly set preference to false.
+        VerticalTabUtils.setVerticalTabsEnabled(false);
+
+        // Should respect preference (false) over default (true).
+        assertFalse(VerticalTabUtils.isVerticalTabsEnabled(mContext));
+
+        // Explicitly set preference to true.
+        VerticalTabUtils.setVerticalTabsEnabled(true);
+
+        // Should respect preference (true).
+        assertTrue(VerticalTabUtils.isVerticalTabsEnabled(mContext));
     }
 }
