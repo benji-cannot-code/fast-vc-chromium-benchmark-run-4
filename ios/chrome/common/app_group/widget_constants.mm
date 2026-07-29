5ad90f3437a3dec7da55839645a0e79adba06b7d
Federica Germinario <fedegermi@google.com>
2026-07-29 09:59:42 -0700

    [ios] Record metrics when app is launched via iOS WidgetKit widgets
    Migrate WidgetKit metrics logging from ChromeAppStartupParameters into
    TaskRequestForURLContext.
    
    Long term objective is to make TaskRequestForURLContext independent
    of ChromeAppStartupParameters, additional refactoring will be done
    in follow-up CLs.
    
    Bug: 493816082
    Change-Id: I8fa39d4a8dab3ca336422b084fbf9fc5a56bfc72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139430
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670333}

---
+++ ios/chrome/common/app_group/widget_constants.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/common/app_group/widget_constants.h"
+
+NSString* const kWidgetKitSchemeChrome = @"chromewidgetkit";
+
+NSString* const kWidgetKitHostSearchWidget = @"search-widget";
+NSString* const kWidgetKitHostQuickActionsWidget = @"quick-actions-widget";
+NSString* const kWidgetKitHostDinoGameWidget = @"dino-game-widget";
+NSString* const kWidgetKitHostLockscreenLauncherWidget =
+    @"lockscreen-launcher-widget";
+NSString* const kWidgetKitHostShortcutsWidget = @"shortcuts-widget";
+NSString* const kWidgetKitHostSearchPasswordsWidget =
+    @"search-passwords-widget";
+
+NSString* const kWidgetKitActionSearch = @"/search";
+NSString* const kWidgetKitActionIncognito = @"/incognito";
+NSString* const kWidgetKitActionVoiceSearch = @"/voicesearch";
+NSString* const kWidgetKitActionQRReader = @"/qrreader";
+NSString* const kWidgetKitActionLens = @"/lens";
+NSString* const kWidgetKitActionGame = @"/game";
+NSString* const kWidgetKitActionOpenURL = @"/open";
+NSString* const kWidgetKitActionSearchPasswords = @"/search-passwords";
