c96591ed7754f51a398798e1b65eee7ca220852d
Brandon Wylie <wylieb@google.com>
2026-07-29 13:09:19 -0700

    Revert "Reland "Remove Find in Page vertical track for Desktop Chrome""
    This reverts commit 9edb83f36a924c394f994f744ae01374baad0d7b.
    
    Reason for revert: Causing test flakiness on test-tablet (crbug.com/540439603)
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/test-tablet/46030
    
    Original change's description:
    > Reland "Remove Find in Page vertical track for Desktop Chrome"
    >
    > This is a reland of commit 4fb225a5465a1e3b56bd2512e315a85fe76b845d
    >
    > Fixed failing
    > chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
    > tests that caused revert.
    >
    > Original change's description:
    > > Remove Find in Page vertical track for Desktop Chrome
    > >
    > > Summary of changes:
    > >
    > > FindToolbar.java: Early return for desktop screens to prevent result bar
    > > (vertical side track) creation.
    > >
    > > find_request_manager.cc: Before asking a frame for its search results,
    > > check if the frame is alive. This prevents checking a dead frame which
    > > will trigger a DCHECK, causing the browser process to crash.
    > >
    > > text_finder.cc: Before measuring the pixel coordinates of any search
    > > match, tell the renderer to update the layout first to prevent layout
    > > (renderer) crashes when stepping through search results.
    > >
    > > Bug: 517595935
    > > Change-Id: I1d66fac3df7c1c62364337e8c486f0c9ddd92ac6
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025419
    > > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > > Reviewed-by: Theresa Wellington <twellington@chromium.org>
    > > Commit-Queue: Neha Sapre <nehasapre@google.com>
    > > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1661944}
    >
    > Bug: 517595935
    > Change-Id: Ie562c63e203bf379a7e84512983f420d4b600007
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8094634
    > Reviewed-by: Theresa Wellington <twellington@chromium.org>
    > Commit-Queue: Neha Sapre <nehasapre@google.com>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1665279}
    
    Bug: 517595935
    Change-Id: I0e9c2b06f99e8fb831c4f567dda9ba962379f440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8166525
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Owners-Override: Brandon Wylie <wylieb@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1670519}

--- chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/findinpage/FindTest.java
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.findinpage;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -22,6 +18,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import androidx.test.espresso.Espresso;
 import androidx.test.filters.MediumTest;
 import androidx.test.filters.SmallTest;
@@ -62,7 +61,6 @@ import org.chromium.chrome.test.util.MenuUtils;
 import org.chromium.content_public.browser.test.util.TouchCommon;
 import org.chromium.content_public.browser.test.util.UiUtils;
 import org.chromium.ui.base.DeviceFormFactor;
-import org.chromium.ui.base.DeviceInput;
 
 /** Find in page tests. */
 @RunWith(ChromeJUnit4ClassRunner.class)
@@ -79,8 +77,6 @@ public class FindTest {
 
     @Before
     public void setUp() throws Exception {
-        DeviceInput.setSupportsKeyboardForTesting(false);
-        DeviceInput.setSupportsPrecisionPointerForTesting(false);
         mActivityTestRule.waitForActivityNativeInitializationComplete();
         mPage = mActivityTestRule.startOnBlankPage();
 
