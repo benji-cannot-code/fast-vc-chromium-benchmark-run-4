3858474a664bf9a6cf6a759aa124350c3b6fba71
Radu Nitescu <radunitescu@google.com>
2026-07-29 08:15:06 -0700

    [iOS] Update keyboard send to return in cobrowse
    Cobrowse should show the new line button in the virtual keyboard
    
    https://screenshot-v2.corp.google.com/image/1it0nliad4688.png
    
    Fixed: 539426291
    Change-Id: I02c412ba8867ef128952397cf9744cca1c8a134d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158462
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670265}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -2164,6 +2164,8 @@ lens::ImageEncodingOptions GetDefaultImageEncodingOptions() {
        trailingAction);
 
   [self.consumer updateVisibleControls:visibleControls];
+  BOOL shouldDisableSending = !_modeHolder.isRegularSearch && !canSend;
+  [self.consumer disableSending:shouldDisableSending];
 }
 
 /// Updates the consumer whether to show in compact mode.
--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator_unittest.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator_unittest.mm
@@ -134,6 +134,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)updatePreferredContentSizeForNewTextFieldHeight {
 }
 
+- (void)disableSending:(BOOL)disableSending {
+}
+
 @end
 
 namespace {
