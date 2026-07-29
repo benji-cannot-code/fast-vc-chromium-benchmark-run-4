3f6042fbc4428f695ef9bf01d83240198583cce9
Przemek Perkowski <perkowski@google.com>
2026-07-29 03:32:15 -0700

    Condition privacy notice string on enterprise policy on Android.
    When enterprise blocks the mqls logging, the data will not be reviewed
    by humans. The notice should properly reflect that. This is a followup
    to crrev.com/c/8138479 that applied the same logic on Desktop.
    
    Screencast: http://go/scrcast/Njc1MDQ5MTc2MDM5NDI0MHxmM2FmMTRiMC0wZQ
    NO_IFTTT=False positive when adding a new ifttt.
    Bug: 524157152
    
    Change-Id: I886b2971e6593fbf966e4807a69075076a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141827
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Przemek Perkowski <perkowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670106}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetMediatorTest.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetMediatorTest.java
@@ -49,6 +49,7 @@ import org.chromium.chrome.browser.autofill.settings.personal_context.AutofillPe
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.personal_context.first_run.PersonalContextFirstRunService;
 import org.chromium.chrome.browser.personal_context.first_run.PersonalContextFirstRunServiceJni;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.SettingsNavigationFactory;
 import org.chromium.chrome.browser.ui.autofill.AtMemoryBottomSheetProperties.FlyoutProperties;
 import org.chromium.chrome.browser.ui.autofill.AtMemoryBottomSheetProperties.HomeProperties;
@@ -57,6 +58,9 @@ import org.chromium.chrome.browser.ui.autofill.internal.R;
 import org.chromium.components.autofill.AutofillSuggestion;
 import org.chromium.components.autofill.SuggestionType;
 import org.chromium.components.browser_ui.settings.SettingsNavigation;
+import org.chromium.components.prefs.PrefService;
+import org.chromium.components.user_prefs.UserPrefs;
+import org.chromium.components.user_prefs.UserPrefsJni;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -72,6 +76,9 @@ public class AtMemoryBottomSheetMediatorTest {
     @Mock private HomeProperties.SearchDelegate mSearchDelegate;
     @Mock private PersonalContextFirstRunService.Natives mFirstRunServiceJniMock;
     @Mock private SettingsNavigation mSettingsNavigation;
+    @Mock private UserPrefs.Natives mUserPrefsJniMock;
+    @Mock private PrefService mPrefService;
+    @Mock private Profile mProfile;
 
     private PropertyModel mModel;
     private PropertyModel mHomeModel;
@@ -82,9 +89,15 @@ public class AtMemoryBottomSheetMediatorTest {
     public void setUp() {
         PersonalContextFirstRunServiceJni.setInstanceForTesting(mFirstRunServiceJniMock);
         SettingsNavigationFactory.setInstanceForTesting(mSettingsNavigation);
+        UserPrefsJni.setInstanceForTesting(mUserPrefsJniMock);
+        when(mUserPrefsJniMock.get(mProfile)).thenReturn(mPrefService);
+
         mMediator =
                 new AtMemoryBottomSheetMediator(
-                        ApplicationProvider.getApplicationContext(), mDelegate, mSearchDelegate);
+                        ApplicationProvider.getApplicationContext(),
+                        mDelegate,
+                        mSearchDelegate,
+                        mProfile);
         mModel = mMediator.getModel();
         mHomeModel = mMediator.getHomeModel();
         mModelList = mHomeModel.get(HomeProperties.SHEET_ITEMS);
@@ -599,5 +612,55 @@ public class AtMemoryBottomSheetMediatorTest {
                 userActionTester
                         .getActions()
                         .contains("PersonalContext.AtMemory.Notice.SettingsLinkClick"));
+    }
+
+    @Test
+    public void testNoticeIsLoggingDisallowed_whenPolicyIsAllowWithoutLogging() {
+        when(mPrefService.getInteger(
+                        AtMemoryBottomSheetMediator.FIND_AND_FILL_WITH_GEMINI_SETTINGS))
+                .thenReturn(1); // 1 = kAllowWithoutLogging
+
+        AutofillSuggestion noticeSuggestion =
+                new AutofillSuggestion.Builder()
+                        .setSuggestionType(SuggestionType.PERSONAL_CONTEXT_NOTICE)
+                        .setSubLabel("")
+                        .build();
+
+        mMediator.show(List.of(noticeSuggestion));
+
+        assertEquals(1, mModelList.size());
+        assertEquals(HomeProperties.ItemType.NOTICE, mModelList.get(0).type);
+        assertFalse(
+                mModelList
+                        .get(0)
+                        .model
+                        .get(
+                                AtMemoryBottomSheetProperties.NoticeItemProperties
+                                        .IS_LOGGING_ALLOWED));
+    }
+
+    @Test
+    public void testNoticeIsLoggingAllowed_whenPolicyIsAllowed() {
+        when(mPrefService.getInteger(
+                        AtMemoryBottomSheetMediator.FIND_AND_FILL_WITH_GEMINI_SETTINGS))
+                .thenReturn(0); // 0 = kAllow
+
+        AutofillSuggestion noticeSuggestion =
+                new AutofillSuggestion.Builder()
+                        .setSuggestionType(SuggestionType.PERSONAL_CONTEXT_NOTICE)
+                        .setSubLabel("")
+                        .build();
+
+        mMediator.show(List.of(noticeSuggestion));
+
+        assertEquals(1, mModelList.size());
+        assertEquals(HomeProperties.ItemType.NOTICE, mModelList.get(0).type);
+        assertTrue(
+                mModelList
+                        .get(0)
+                        .model
+                        .get(
+                                AtMemoryBottomSheetProperties.NoticeItemProperties
+                                        .IS_LOGGING_ALLOWED));
     }
 }
