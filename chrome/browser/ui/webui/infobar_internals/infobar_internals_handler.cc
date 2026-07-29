6173ee79162573a05a23a6266144a52c0d494509
Muhammad Salmaan <musalmaan@chromium.org>
2026-07-29 07:29:09 -0700

    Add Chrome for Testing infobar to infobar internals page
    This CL adds the migrated Chrome for Testing (CFT) to the internals
    page.
    
    Bug: 530927737
    Change-Id: I494cc77a1bd21d1f0dfa89c7621ac48b57f6cf36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160686
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670238}

--- chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.cc
+++ chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/branding_buildflags.h"
+#include "build/buildflag.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/buildflags.h"
 #include "chrome/browser/devtools/devtools_infobar_delegate.h"
@@ -43,6 +44,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
+
+#if BUILDFLAG(CHROME_FOR_TESTING)
+#include "chrome/browser/ui/startup/chrome_for_testing_infobar_delegate.h"
+#endif
 
 #if BUILDFLAG(ENABLE_PLUGINS)
 #include "chrome/browser/plugins/reload_plugin_infobar_delegate.h"
@@ -109,6 +114,13 @@ void InfoBarInternalsHandler::GetInfoBars(GetInfoBarsCallback callback) {
         "The Alternate Nav infobar is shown when a user searches for a term "
         "they may have meant to navigate to."));
   }
+#if BUILDFLAG(CHROME_FOR_TESTING)
+  infobar_list.emplace_back(InfoBarEntry::New(
+      /*type=*/InfoBarType::kChromeForTesting, /*name=*/"Chrome for Testing",
+      /*description=*/
+      "The Chrome for Testing infobar warns users that this version is only "
+      "for automated testing."));
+#endif
   infobar_list.emplace_back(InfoBarEntry::New(
       /*type=*/InfoBarType::kCollectedCookies, /*name=*/"Collected Cookies",
       /*description=*/
@@ -241,6 +253,23 @@ bool InfoBarInternalsHandler::TriggerInfoBarInternal(InfoBarType type) {
           web_contents, u"test", match, GURL("https://youtube.com/"));
       return true;
     }
+#if BUILDFLAG(CHROME_FOR_TESTING)
+    case InfoBarType::kChromeForTesting: {
+      if (infobars::IsInfoBarMigrated(
+              infobars::InfoBarDelegate::CHROME_FOR_TESTING_INFOBAR_DELEGATE)) {
+        auto* browser_infobar_manager =
+            infobars::BrowserInfoBarManager::From(g_browser_process);
+        if (!browser_infobar_manager) {
+          return false;
+        }
+        browser_infobar_manager->ShowGlobally(
+            infobars::InfoBarDelegate::CHROME_FOR_TESTING_INFOBAR_DELEGATE);
+      } else {
+        ChromeForTestingInfoBarDelegate::Create();
+      }
+      return true;
+    }
+#endif
     case InfoBarType::kCollectedCookies: {
       if (!bwi || !bwi->GetActiveTabInterface()) {
         return false;
