83747820b1f1d4c2cdac883d17c4e31875534384
dljames <dljames@chromium.org>
2026-07-28 19:01:57 -0700

    [BAM] Support explicit ActionInvocationSource on ActionInvocationContext
    Add ActionInvocationSource enum and kActionInvocationSourceKey to
    ActionInvocationContext. ActionViewInterface sets kViewButton on button
    clicks, and CommandActionUpdater sets kKeyboardShortcut on command
    execution. kActionToggleCollapseVertical evaluates
    kActionInvocationSourceKey to log distinct user metrics for vertical tab
    strip collapse.
    
    TAG=agy
    CONV=57aa08e7-e6de-4228-8302-9477a8fb9001
    
    Bug: 525118424
    Change-Id: Ieef04c79b9e100425f4c863feed179e59fca9fd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160322
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1669944}

--- chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
+++ chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
@@ -313,6 +313,7 @@ void VerticalTabStripStateController::OnModeChanged() {
   is_vertical_tabs_enabled_ = new_enabled;
 
   NotifyModeChanged();
+  UpdateCollapseActionItem();
 }
 
 void VerticalTabStripStateController::OnExpandOnHoverEnabledChanged() {
@@ -400,6 +401,7 @@ void VerticalTabStripStateController::UpdateCollapseActionItem() {
         chrome::GetCleanTitleAndTooltipText(l10n_util::GetStringUTF16(text)));
     collapse_action->SetTooltipText(
         chrome::GetCleanTitleAndTooltipText(l10n_util::GetStringUTF16(text)));
+    collapse_action->SetEnabled(ShouldDisplayVerticalTabs());
   }
 }
 
