2140d00dba44d283aadd953fc4cc586e1353e60e
Rafał Godlewski <rgod@google.com>
2026-07-30 07:15:50 -0700

    [iOS] Move credential_provider_migrator_app_agent to feature folder
    Bug: None
    Bypass-Check-License: Moved files
    Change-Id: Ib5f58c2541f4ecba1cc68d346ed36b062aa78775
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8164705
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671038}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -160,7 +160,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/device_form_factor.h"
 
 #if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
-#import "ios/chrome/app/credential_provider_migrator_app_agent.h"
+#import "ios/chrome/browser/credential_provider/model/credential_provider_migrator_app_agent.h"
 #endif
 
 #if BUILDFLAG(IOS_ENABLE_SANDBOX_DUMP)
