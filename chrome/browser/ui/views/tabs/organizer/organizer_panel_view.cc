f81c47d4b29016443e060ff4ab4f0db89e3d5f1b
Chase Hartsell <chazzy@google.com>
2026-07-30 13:40:47 -0700

    [organizer] Remove custom layout and use FlexLayout
    Removes the OrganizerPanelViewLayout and switches to using a simple
    FlexLayout since the panel only contains the control view and a web
    view. The web view extends to the left, right, and bottom edges of the
    panel so padding can be applied within the WebUI.
    
    Demo: http://go/scrcast/NTg0NjY4NjM4MjQyNDA2NHwzYzdhYTdkOC0xMA
    
    Bug: 540084234
    Change-Id: I94b0edabbf471e6c7b0f7a9814fcf9674061ea3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8176421
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671381}

--- chrome/browser/ui/views/tabs/organizer/organizer_panel_view.cc
+++ chrome/browser/ui/views/tabs/organizer/organizer_panel_view.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/organizer/organizer_panel_state_controller.h"
 #include "chrome/browser/ui/views/tabs/organizer/layout_constants.h"
 #include "chrome/browser/ui/views/tabs/organizer/organizer_panel_controls_view.h"
-#include "chrome/browser/ui/views/tabs/organizer/organizer_panel_view_layout.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/saved_tab_groups/public/features.h"
@@ -37,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/event_monitor.h"
 #include "ui/views/focus/focus_search.h"
+#include "ui/views/layout/flex_layout.h"
+#include "ui/views/layout/flex_layout_types.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/view_shadow.h"
 #include "ui/views/widget/widget.h"
@@ -81,17 +82,24 @@ OrganizerPanelView::OrganizerPanelView(
 
   SetIsElevated(true);
 
+  content_container_->SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kVertical)
+      .SetCrossAxisAlignment(views::LayoutAlignment::kStretch);
+
   controls_view_ = content_container_->AddChildView(
       std::make_unique<OrganizerPanelControlsView>(root_action_item_.get()));
+  controls_view_->SetProperty(views::kMarginsKey,
+                              organizer_panel::kOrganizerPanelControlsMargins);
 
   if (browser_ && browser_->GetProfile()) {
     auto web_view = std::make_unique<views::WebView>(browser_->GetProfile());
     web_view->LoadInitialURL(GURL(chrome::kChromeUIOrganizerPanelURL));
+    web_view->SetProperty(
+        views::kFlexBehaviorKey,
+        views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToZero,
+                                 views::MaximumFlexSizeRule::kUnbounded));
     web_view_ = content_container_->AddChildView(std::move(web_view));
   }
-
-  content_container_->SetLayoutManager(
-      std::make_unique<OrganizerPanelViewLayout>(controls_view_));
 
   resize_animation_.SetTweenType(gfx::Tween::Type::EASE_IN_OUT_EMPHASIZED);
 
