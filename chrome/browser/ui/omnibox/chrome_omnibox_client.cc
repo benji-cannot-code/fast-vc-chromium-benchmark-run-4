3935bbc802e16e73b4ce02085348fc4f458be4a2
Maks Orlovich <morlovich@chromium.org>
2026-07-30 12:45:00 -0700

    ChromeOmniboxClient::CheckConditionsAndLaunchSurvey: work with any location bar
    Bug: 469743571
    Change-Id: I69815859a019ca99dc08e16195ccef8b1b372f37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178466
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671337}

--- chrome/browser/ui/omnibox/chrome_omnibox_client.cc
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.cc
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/omnibox/omnibox_next_features.h"
 #include "chrome/browser/ui/omnibox/omnibox_tab_helper.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_closer.h"
 #include "chrome/browser/ui/views/search_engines/dse_reset_dialog.h"
 #include "chrome/common/channel_info.h"
@@ -594,12 +593,10 @@ void ChromeOmniboxClient::CheckConditionsAndLaunchSurvey() {
     return;
   }
 
-  auto* location_bar_view = browser_view->GetLocationBarView();
+  auto* location_bar = browser_view->GetLocationBar();
 
   // Don't show the HaTS survey if the location bar has focus.
-  if (location_bar_view &&
-      !location_bar_view->Contains(
-          location_bar_view->GetFocusManager()->GetFocusedView())) {
+  if (location_bar && !location_bar->IsFocusWithin()) {
     hats_service->LaunchSurvey(
         survey_trigger, base::DoNothing(), base::DoNothing(), {},
         {{"page classification",
