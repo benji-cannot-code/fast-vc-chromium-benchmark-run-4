db10b96629731f0b24d132181a73cd6d20f7a348
Helmut Januschka <helmut@januschka.com>
2026-07-30 02:21:47 -0700

    Keep browser alive while fetch keepalive URLLoaders exist
    Holds a ScopedKeepAlive while any fetch keepalive URLLoader (including
    pending fetchLater) exists, gated by the default-off
    kKeepAliveBrowserProcessAlive feature, so closing the last window does
    not drop pending requests.
    
    Bug: 408010432
    Change-Id: I8a8d2d7a8770e8f1cdedf9d279db2359a5192ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7972217
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Auto-Submit: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1670925}

--- components/keep_alive_registry/keep_alive_types.cc
+++ components/keep_alive_registry/keep_alive_types.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/keep_alive_registry/keep_alive_types.h"
+
 #include "base/logging.h"
 #include "base/notreached.h"
 
@@ -93,6 +94,8 @@ std::ostream& operator<<(std::ostream& out, const KeepAliveOrigin& origin) {
       return out << "SESSION_DATA_DELETER";
     case KeepAliveOrigin::UMA_LOG:
       return out << "UMA_LOG";
+    case KeepAliveOrigin::FETCH_KEEPALIVE_REQUEST:
+      return out << "FETCH_KEEPALIVE_REQUEST";
   }
 
   NOTREACHED() << static_cast<int>(origin);
--- content/browser/loader/keep_alive_url_loader.cc
+++ content/browser/loader/keep_alive_url_loader.cc
@@ -472,6 +472,9 @@ KeepAliveURLLoader::KeepAliveURLLoader(
   if (IsFetchLater()) {
     base::UmaHistogramBoolean("FetchLater.Browser.Total", true);
   }
+
+  GetContentClient()->browser()->OnFetchKeepAliveRequestCreated(
+      *storage_partition_->browser_context());
 }
 
 void KeepAliveURLLoader::Start() {
@@ -538,6 +541,8 @@ KeepAliveURLLoader::~KeepAliveURLLoader() {
   if (IsStarted()) {
     GetContentClient()->browser()->OnKeepaliveRequestFinished();
   }
+  GetContentClient()->browser()->OnFetchKeepAliveRequestDestroyed(
+      *storage_partition_->browser_context());
 }
 
 void KeepAliveURLLoader::set_on_delete_callback(
