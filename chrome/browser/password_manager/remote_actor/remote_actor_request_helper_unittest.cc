e08ed7b8f4728e46df10fca6cef7842534ac1b50
Tanmoy Mollik <triploblastic@google.com>
2026-07-30 02:34:27 -0700

    [google_apis] Use factory method to create GoogleServiceAuthError
    Bug: 487926694
    Change-Id: Id69827c50fb8c9eaab50003db34d71c5c44807f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163673
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670933}

--- chrome/browser/password_manager/remote_actor/remote_actor_request_helper_unittest.cc
+++ chrome/browser/password_manager/remote_actor/remote_actor_request_helper_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "google_apis/gaia/gaia_urls.h"
+#include "net/base/net_errors.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_status_code.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -119,7 +120,7 @@ TEST_F(RemoteActorRequestTest, AccessTokenFetchFailure) {
 
   // Fail to fetch token
   identity_test_env_.WaitForAccessTokenRequestIfNecessaryAndRespondWithError(
-      GoogleServiceAuthError(GoogleServiceAuthError::CONNECTION_FAILED));
+      GoogleServiceAuthError::FromConnectionError(net::ERR_FAILED));
 
   run_loop.Run();
 
