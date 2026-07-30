7aa0e47bff8d25349a54bbf9240b1bb960b5cd11
Xianzhu Wang <wangxianzhu@chromium.org>
2026-07-29 21:09:18 -0700

    Revert "Avoid some DeprecatedLocation/FrameRect in WebPluginContainerImpl"
    This reverts commit e6d575d0dcf11583cd29d1c970890256cbb6c30f.
    
    Reason for revert: The original CL caused regression of pdf viewer.
    
    Failure Link: https://crbug.com/540613892
    
    Original change's description:
    > Avoid some DeprecatedLocation/FrameRect in WebPluginContainerImpl
    >
    > See crrev.com/c/8161244 for the reason of deprecating
    > EmbeddedContentView::Location().
    >
    > Some usages of DeprecatedLocation/FrameRect are modified to use
    > LayoutObject coord mapping routines etc. behind killswitch
    > AvoidEmbeddedContentViewLocation. The usages in SetFrameRect() and
    > Paint() will be removed later.
    >
    > The tests for IsRectTopmost were wrong. The rect passed to it should be
    > in the plugin's coordinate space, but the tests passed rects in the
    > containing frame's coordinate space. To work around the mistake, the
    > tests set an artificial frame rect. The tests no longer worked with
    > the modified code which doesn't use frame rect. The modified tests
    > will pass with either the original code or modified code.
    >
    > Bug: 41458361
    > Change-Id: If36cf368694f1079cbf54c6d4c4e0be4b55b02c1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161946
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1669986}
    
    Bug: 41458361
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Idb91924336a0468c61c76bb1bf98d3647ce2dff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8170450
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670820}

--- third_party/blink/renderer/core/exported/web_plugin_container_test.cc
+++ third_party/blink/renderer/core/exported/web_plugin_container_test.cc
@@ -1294,7 +1294,9 @@ TEST_F(WebPluginContainerTest, IsRectTopmostTest) {
 
   auto* plugin_container_impl = To<WebPluginContainerImpl>(
       GetWebPluginContainer(web_view, WebString("translated-plugin")));
-  gfx::Rect rect(plugin_container_impl->GetElement().BoundsInWidget().size());
+  plugin_container_impl->SetFrameRect(gfx::Rect(0, 0, 300, 300));
+
+  gfx::Rect rect = plugin_container_impl->GetElement().BoundsInWidget();
   EXPECT_TRUE(plugin_container_impl->IsRectTopmost(rect));
 
   // Cause the plugin's frame to be detached.
@@ -1315,14 +1317,14 @@ TEST_F(WebPluginContainerTest, IsRectTopmostTestWithOddAndEvenDimensions) {
 
   auto* even_plugin_container_impl = To<WebPluginContainerImpl>(
       GetWebPluginContainer(web_view, WebString("translated-plugin")));
-  gfx::Rect even_rect(
-      even_plugin_container_impl->GetElement().BoundsInWidget().size());
+  even_plugin_container_impl->SetFrameRect(gfx::Rect(0, 0, 300, 300));
+  auto even_rect = even_plugin_container_impl->GetElement().BoundsInWidget();
   EXPECT_TRUE(even_plugin_container_impl->IsRectTopmost(even_rect));
 
   auto* odd_plugin_container_impl = To<WebPluginContainerImpl>(
       GetWebPluginContainer(web_view, WebString("odd-dimensions-plugin")));
-  gfx::Rect odd_rect(
-      odd_plugin_container_impl->GetElement().BoundsInWidget().size());
+  odd_plugin_container_impl->SetFrameRect(gfx::Rect(0, 0, 300, 300));
+  auto odd_rect = odd_plugin_container_impl->GetElement().BoundsInWidget();
   EXPECT_TRUE(odd_plugin_container_impl->IsRectTopmost(odd_rect));
 }
 
@@ -1485,7 +1487,7 @@ TEST_F(WebPluginContainerTest, ClippedRectsForSubpixelPositionedPlugin) {
 }
 
 TEST_F(WebPluginContainerTest, TopmostAfterDetachTest) {
-  static constexpr gfx::Rect kTopmostRect(0, 0, 40, 40);
+  static constexpr gfx::Rect kTopmostRect(10, 10, 40, 40);
 
   // Plugin that checks isRectTopmost in destroy().
   class TopmostPlugin : public FakeWebPlugin {
@@ -1515,6 +1517,8 @@ TEST_F(WebPluginContainerTest, TopmostAfterDetachTest) {
 
   auto* plugin_container_impl = To<WebPluginContainerImpl>(
       GetWebPluginContainer(web_view, WebString("translated-plugin")));
+  plugin_container_impl->SetFrameRect(gfx::Rect(0, 0, 300, 300));
+
   EXPECT_TRUE(plugin_container_impl->IsRectTopmost(kTopmostRect));
 
   TopmostPlugin* test_plugin =
