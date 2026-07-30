b0037002b1add4501622a26053dba8657b4ff6f8
Quentin Pubert <qpubert@google.com>
2026-07-30 02:29:02 -0700

    [iOS] Clean up kEnableReaderModeTranslationWithInfobar feature flag
    Cleans up the launched reader-mode-translation-with-infobar-enabled
    feature flag.
    
    Bug: 509857503
    Change-Id: I5fa1e362b04d219d98f8dfd3ad6419a4bf9721fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8166880
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670930}

--- ios/chrome/browser/popup_menu/overflow_menu/coordinator/overflow_menu_mediator.mm
+++ ios/chrome/browser/popup_menu/overflow_menu/coordinator/overflow_menu_mediator.mm
@@ -2117,10 +2117,7 @@ void GetPresetNTPBackgroundPreview(
 
 // Returns whether translate is enabled on the current page.
 - (BOOL)isTranslateEnabled {
-  return
-      [self canManuallyTranslate:NO] && ![self isLensOverlayVisible] &&
-      (![self isReaderModeActive] ||
-       base::FeatureList::IsEnabled(kEnableReaderModeTranslationWithInfobar));
+  return [self canManuallyTranslate:NO] && ![self isLensOverlayVisible];
 }
 
 // Returns whether lens overlay is enabled on the current page.
