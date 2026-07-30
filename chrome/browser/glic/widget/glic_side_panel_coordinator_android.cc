87a3978eee746df82112d36248ad3edc0397a027
Hitarth Kothari <hitarthkothari@google.com>
2026-07-30 08:14:36 -0700

    Save and restore Glic bottom sheet state during Android activity recreation.
    This change saves the Glic bottom sheet state (expanded or peeked) when
    a tab is deactivated or detached, which typically occurs during Android
    activity recreation (such as screen rotation or folding/unfolding). The
    saved state is then used to restore the bottom sheet to its previous
    state upon re-initialization.
    
    Bug: 540255804, 533324799
    Change-Id: I54c4036f762751230f362d118259d8647583c507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8165091
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671079}

--- chrome/browser/glic/widget/glic_side_panel_coordinator_android.cc
+++ chrome/browser/glic/widget/glic_side_panel_coordinator_android.cc
@@ -163,6 +163,16 @@ void GlicSidePanelCoordinatorAndroid::SetState(State state) {
   state_callbacks_.Notify(state_);
 }
 
+void GlicSidePanelCoordinatorAndroid::SaveStateBeforeDeactivation() {
+  if (state_ == State::kShown) {
+    initial_state_override_for_activity_recreation_ =
+        ShowOptions::InitialState::kExpanded;
+  } else if (state_ == State::kPeek) {
+    initial_state_override_for_activity_recreation_ =
+        ShowOptions::InitialState::kPeeked;
+  }
+}
+
 void GlicSidePanelCoordinatorAndroid::OnTabDidActivate(
     tabs::TabInterface* tab) {
   if (state_ == State::kClosed) {
@@ -181,6 +191,7 @@ void GlicSidePanelCoordinatorAndroid::OnTabWillDeactivate(
   if (state_ == State::kClosed) {
     return;
   }
+  SaveStateBeforeDeactivation();
   SetState(State::kBackgrounded);
 
   tab_bottom_sheet_bridge_->Close(/* animate= */ false);
@@ -195,6 +206,7 @@ void GlicSidePanelCoordinatorAndroid::OnTabWillDetach(
   // foldable fold/unfold).
   pending_show_options_.reset();
   if (state_ != State::kClosed) {
+    SaveStateBeforeDeactivation();
     SetState(State::kBackgrounded);
     tab_bottom_sheet_bridge_->Close(/* animate= */ false);
   }
@@ -233,6 +245,12 @@ void GlicSidePanelCoordinatorAndroid::OnManagerInitialized(
                                        /*request_focus=*/false);
     ShowOptions options = std::exchange(pending_show_options_, std::nullopt)
                               .value_or(ShowOptions());
+    if (initial_state_override_for_activity_recreation_.has_value()) {
+      options.initial_state =
+          std::exchange(initial_state_override_for_activity_recreation_,
+                        std::nullopt)
+              .value();
+    }
     Show(options);
   }
 }
