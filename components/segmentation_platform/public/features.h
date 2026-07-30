7a95e8db45a0a83bd969e11d141a631dc8305ab5
Chris Lu <thegreenfrog@google.com>
2026-07-29 23:06:54 -0700

    [Bling] Remove launched Magic Stack segmentation ranking flags
    Remove ios-magic-stack-segmentation-ranking and ios-magic-stack-segmentation-ranking-split-by-surface feature flags which are now launched and enabled by default on iOS.
    
    Unconditionally enable segmentation ranking and surface metadata in the Magic Stack and remove unused flag definitions, descriptions, metadata, field trial configs, and tests.
    
    Bug: None
    Change-Id: Ic61b2d5b3d38ed83a36e0f23c4ca0f66bb9b9a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167585
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670845}

--- components/segmentation_platform/public/features.h
+++ components/segmentation_platform/public/features.h
@@ -85,9 +85,6 @@ BASE_DECLARE_FEATURE(kSegmentationPlatformPasswordManagerUser);
 // Feature flag for enabling tab resumption ranker.
 BASE_DECLARE_FEATURE(kSegmentationPlatformTabResumptionRanker);
 
-// Feature flag for enabling ios module ranker.
-BASE_DECLARE_FEATURE(kSegmentationPlatformIosModuleRanker);
-
 // Feature flag for enabling android home module ranker.
 BASE_DECLARE_FEATURE(kSegmentationPlatformAndroidHomeModuleRanker);
 
@@ -106,9 +103,6 @@ BASE_DECLARE_FEATURE(kSegmentationPlatformComposePromotion);
 
 // Feature flag for using SQL database for UMA signals.
 BASE_DECLARE_FEATURE(kSegmentationPlatformUmaFromSqlDb);
-
-// Feature flag for having separate models for the Start and NTP surface.
-BASE_DECLARE_FEATURE(kSegmentationPlatformIosModuleRankerSplitBySurface);
 
 // Feature flag for enabling the URL visit resumption ranker.
 BASE_DECLARE_FEATURE(kSegmentationPlatformURLVisitResumptionRanker);
