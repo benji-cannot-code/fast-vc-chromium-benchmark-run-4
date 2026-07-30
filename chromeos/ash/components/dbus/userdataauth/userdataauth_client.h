65d1359e1f860966d71449f4330eb9fb9c3a627f
John Admanski <jadmanski@google.com>
2026-07-29 23:59:52 -0700

    Drop GetRecoverableKeyStore reference
    This using declaration was unfortunately missed when removing all the
    other references to this message, which is preventing a system_api sync.
    
    BUG=b:537422515
    
    Change-Id: I722bbdd752e0857fe4184f776b15ff0af6fb3e33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160653
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Auto-Submit: John Admanski <jadmanski@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670872}

--- chromeos/ash/components/dbus/userdataauth/userdataauth_client.h
+++ chromeos/ash/components/dbus/userdataauth/userdataauth_client.h
@@ -138,9 +138,6 @@ class COMPONENT_EXPORT(USERDATAAUTH_CLIENT) UserDataAuthClient {
   using GetArcDiskFeaturesCallback =
       chromeos::DBusMethodCallback<::user_data_auth::GetArcDiskFeaturesReply>;
 
-  using GetRecoverableKeyStoresCallback = chromeos::DBusMethodCallback<
-      ::user_data_auth::GetRecoverableKeyStoresReply>;
-
   using SetUserDataStorageWriteEnabledCallback = chromeos::DBusMethodCallback<
       ::user_data_auth::SetUserDataStorageWriteEnabledReply>;
 
