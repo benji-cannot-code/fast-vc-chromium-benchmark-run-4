f8e3999867efcf414afb31c0d0b926a5d507843b
Lei Zhang <thestig@chromium.org>
2026-07-30 12:34:57 -0700

    Change ui::ResourceBundle data retrieval methods to return scoped_refptr
    LoadDataResourceBytes(), LoadDataResourceBytesForScale() and
    LoadLocalizedResourceBytes() currently return raw pointers, which is
    prone to memory leaks. Instead, return scoped_refptr since
    base::RefCountedMemory is by definition a ref-counted class.
    
    Modernize the code and get rid of more `new` usage in ResourceBundle.
    
    Change-Id: I7d9d9fd2cda228e0d398dd9dab84e97f27136f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178764
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671324}

--- ui/base/resource/resource_bundle.h
+++ ui/base/resource/resource_bundle.h
@@ -154,7 +154,7 @@ class COMPONENT_EXPORT(UI_BASE) ResourceBundle {
 
     // Return a ref counted memory resource or null to attempt retrieval of the
     // default resource.
-    virtual base::RefCountedMemory* LoadDataResourceBytes(
+    virtual scoped_refptr<base::RefCountedMemory> LoadDataResourceBytes(
         int resource_id,
         ResourceScaleFactor scale_factor) = 0;
 
@@ -345,7 +345,8 @@ class COMPONENT_EXPORT(UI_BASE) ResourceBundle {
   bool HasDataResource(int resource_id) const;
 
   // Loads the raw bytes of a scale independent data resource or null.
-  base::RefCountedMemory* LoadDataResourceBytes(int resource_id) const;
+  scoped_refptr<base::RefCountedMemory> LoadDataResourceBytes(
+      int resource_id) const;
 
   // Whether the |resource_id| is gzipped in this bundle. False is also returned
   // if the resource is not found.
@@ -360,7 +361,7 @@ class COMPONENT_EXPORT(UI_BASE) ResourceBundle {
   // before returning. Use ResourceHandle::kScaleFactorNone for scale
   // independent image resources (such as wallpaper). Returns null if we fail
   // to read the resource.
-  base::RefCountedMemory* LoadDataResourceBytesForScale(
+  scoped_refptr<base::RefCountedMemory> LoadDataResourceBytesForScale(
       int resource_id,
       ResourceScaleFactor scale_factor) const;
 
@@ -399,7 +400,8 @@ class COMPONENT_EXPORT(UI_BASE) ResourceBundle {
 
   // Get a localized resource (for example, localized image logo) given a
   // resource id.
-  base::RefCountedMemory* LoadLocalizedResourceBytes(int resource_id) const;
+  scoped_refptr<base::RefCountedMemory> LoadLocalizedResourceBytes(
+      int resource_id) const;
 
   // Returns a font list derived from the platform-specific "Base" font list.
   // The result is always cached and exists for the lifetime of the process.
