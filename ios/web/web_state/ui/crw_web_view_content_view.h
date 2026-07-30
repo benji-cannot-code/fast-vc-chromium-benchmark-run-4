5b71526621981b01757a01f497391d2de5c4a291
Scott Yoder <scottyoder@google.com>
2026-07-30 04:54:57 -0700

    [ios] Add viewportInsetsDidChange hook to CRWWebViewContentView
    Add viewportInsetsDidChangeWithMinInset:maxInset: hook to
    CRWWebViewContentView. This callback is invoked whenever the minimum
    and maximum viewport insets are successfully applied to the underlying
    web view.
    
    The default public implementation is a no-op hook method that can be
    overridden or swizzled by internal build providers to synchronize
    viewport layout sizes.
    
    Bug: 537727241
    Change-Id: Iebbe722797a057fed8d27931760aa0073cb4ace5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167186
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670981}

--- ios/web/web_state/ui/crw_web_view_content_view.h
+++ ios/web/web_state/ui/crw_web_view_content_view.h
@@ -41,6 +41,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)updateFullscreenState:(CrFullscreenState)fullscreenState;
 
+// Called when the minimum/maximum viewport insets change.
+- (void)viewportInsetsDidChangeWithMinInset:(UIEdgeInsets)minInset
+                                   maxInset:(UIEdgeInsets)maxInset;
+
 @end
 
 #endif  // IOS_WEB_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
