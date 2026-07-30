a5b24dd62046e1d55826e96198bb8bddbd007c73
Kunal Daftari <kunaldaftari@google.com>
2026-07-30 12:41:23 -0700

    [Focus] Visibility of Collapse Icon and Tab Group Editor Bubble Button
    When in focus mode, we should always show the tab group editor bubble
    button.
    
    Additionally, we should not show the collapse icon because there is no
    "collapse" action in focus mode.
    
    Test: TabGroupHeaderViewTest.FocusModeVisibility
    Bug: 540861384, 540860864
    Change-Id: I206a9f5465d9cdc84b186b8ab60192860b0a4253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8179443
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671332}

--- chrome/browser/ui/views/tabs/common/tab_group_header_view.cc
+++ chrome/browser/ui/views/tabs/common/tab_group_header_view.cc
@@ -161,6 +161,7 @@ TabGroupHeaderView::TabGroupHeaderView(
 
   if (editor_bubble_button_) {
     ConfigureEditorBubbleButton(editor_bubble_button_);
+    editor_bubble_button_->SetVisible(delegate_->IsGroupFocused());
   }
   editor_bubble_opened_subscription_ =
       editor_bubble_tracker_.RegisterOnBubbleOpened(base::BindRepeating(
@@ -189,7 +190,7 @@ TabGroupHeaderView::TabGroupHeaderView(
           /*adjust_height_for_width=*/false,
           views::MinimumFlexSizeRule::kScaleToZero)
           .WithOrder(2));
-  // The collapse icon should always be seen.
+  // The collapse icon should be seen when not in focus mode.
   // Let the collapse icon grow to fill the remaining available space while
   // keeping the icon trailing aligned.
   if (collapse_icon_) {
@@ -199,6 +200,7 @@ TabGroupHeaderView::TabGroupHeaderView(
         views::kFlexBehaviorKey,
         views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
                                  views::MaximumFlexSizeRule::kPreferred));
+    collapse_icon_->SetVisible(!delegate_->IsGroupFocused());
   }
 
   sync_icon_->SetProperty(views::kMarginsKey,
@@ -547,10 +549,12 @@ void TabGroupHeaderView::OnDataChanged(
     // Update editor bubble button.
     if (editor_bubble_button_) {
       UpdateEditorButtonColors(editor_bubble_button_, foreground_color);
+      UpdateEditorBubbleButtonVisibility();
     }
 
     // Update collapse icon.
     if (collapse_icon_) {
+      collapse_icon_->SetVisible(!delegate_->IsGroupFocused());
       collapse_icon_->SetImage(ui::ImageModel::FromVectorIcon(
           tab_group_visual_data_.is_collapsed()
               ? features::IsRoundedIconsEnabled()
@@ -706,10 +710,9 @@ void TabGroupHeaderView::SetEditorBubbleButtonVisibilityOnHover(
   if (!editor_bubble_button_) {
     return;
   }
-  if (editor_bubble_button_) {
-    editor_bubble_button_->SetVisible(editor_bubble_tracker_.is_open() ||
-                                      is_hovered);
-  }
+  editor_bubble_button_->SetVisible(delegate_->IsGroupFocused() ||
+                                    editor_bubble_tracker_.is_open() ||
+                                    is_hovered);
 }
 
 void TabGroupHeaderView::ShowEditorBubble() {
