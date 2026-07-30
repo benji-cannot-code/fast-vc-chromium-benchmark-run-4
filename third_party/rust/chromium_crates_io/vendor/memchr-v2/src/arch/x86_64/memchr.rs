6d217540217235851bace0c950791828ada0279c
Martin Verde <thesalsa@google.com>
2026-07-29 17:52:35 -0700

    Roll memchr: 2.8.2 => 2.8.3 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * memchr: 2.8.2 => 2.8.3; https://docs.rs/crate/memchr/2.8.3
    
    Bug: None
    Change-Id: Ib226fce8e3d371e547f164cc8e25125b5c946c51
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142879
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Martin Pan-Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670732}

--- third_party/rust/chromium_crates_io/vendor/memchr-v2/src/arch/x86_64/memchr.rs
+++ third_party/rust/chromium_crates_io/vendor/memchr-v2/src/arch/x86_64/memchr.rs
@@ -172,7 +172,7 @@ macro_rules! unsafe_ifunc {
 ///
 /// Pointers must be valid. See `One::find_raw`.
 #[inline(always)]
-pub(crate) fn memchr_raw(
+pub(crate) unsafe fn memchr_raw(
     n1: u8,
     start: *const u8,
     end: *const u8,
@@ -195,7 +195,7 @@ pub(crate) fn memchr_raw(
 ///
 /// Pointers must be valid. See `One::rfind_raw`.
 #[inline(always)]
-pub(crate) fn memrchr_raw(
+pub(crate) unsafe fn memrchr_raw(
     n1: u8,
     start: *const u8,
     end: *const u8,
@@ -218,7 +218,7 @@ pub(crate) fn memrchr_raw(
 ///
 /// Pointers must be valid. See `Two::find_raw`.
 #[inline(always)]
-pub(crate) fn memchr2_raw(
+pub(crate) unsafe fn memchr2_raw(
     n1: u8,
     n2: u8,
     start: *const u8,
@@ -243,7 +243,7 @@ pub(crate) fn memchr2_raw(
 ///
 /// Pointers must be valid. See `Two::rfind_raw`.
 #[inline(always)]
-pub(crate) fn memrchr2_raw(
+pub(crate) unsafe fn memrchr2_raw(
     n1: u8,
     n2: u8,
     start: *const u8,
@@ -268,7 +268,7 @@ pub(crate) fn memrchr2_raw(
 ///
 /// Pointers must be valid. See `Three::find_raw`.
 #[inline(always)]
-pub(crate) fn memchr3_raw(
+pub(crate) unsafe fn memchr3_raw(
     n1: u8,
     n2: u8,
     n3: u8,
@@ -295,7 +295,7 @@ pub(crate) fn memchr3_raw(
 ///
 /// Pointers must be valid. See `Three::rfind_raw`.
 #[inline(always)]
-pub(crate) fn memrchr3_raw(
+pub(crate) unsafe fn memrchr3_raw(
     n1: u8,
     n2: u8,
     n3: u8,
@@ -322,7 +322,11 @@ pub(crate) fn memrchr3_raw(
 ///
 /// Pointers must be valid. See `One::count_raw`.
 #[inline(always)]
-pub(crate) fn count_raw(n1: u8, start: *const u8, end: *const u8) -> usize {
+pub(crate) unsafe fn count_raw(
+    n1: u8,
+    start: *const u8,
+    end: *const u8,
+) -> usize {
     // SAFETY: We provide a valid function pointer type.
     unsafe_ifunc!(
         One,
