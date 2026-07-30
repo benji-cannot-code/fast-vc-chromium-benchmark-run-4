437c7f9e0afbdfc3b47397f4475ba42571d19d35
Timofey Chudakov <tchudakov@google.com>
2026-07-30 07:08:23 -0700

    [AutofillAi] Set ERROR_MESSAGE to null in the EntityEditor.
    The DateFieldView.setErrorMessage still shows the error message
    indicator if the error message is empty but not null. This CL
    changes to logic in the entity editor to set the error message
    to null so that the error message indicator is not shown.
    
    The commit stage is disabled in the entity editor on the following
    recordings:
    Recording before: http://shortn/_IiEcEvueJd
    Recording after: http://shortn/_ajZFUm2dlS
    
    Bug: 509798874
    Change-Id: I356262bab89ac60594b232482d220f4f15b54920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8175703
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671034}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/autofill_ai/EntityEditorMediator.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/autofill_ai/EntityEditorMediator.java
@@ -238,7 +238,7 @@ class EntityEditorMediator {
             if (!isEditable(editorItem)) {
                 continue;
             }
-            editorItem.model.set(ERROR_MESSAGE, "");
+            editorItem.model.set(ERROR_MESSAGE, null);
         }
     }
 
@@ -361,7 +361,7 @@ class EntityEditorMediator {
         if (itemModel.get(IS_REQUIRED)) {
             for (EditorItem item : mEditorModel.get(EDITOR_FIELDS)) {
                 if (isEditable(item) && item.model.get(IS_REQUIRED)) {
-                    item.model.set(ERROR_MESSAGE, "");
+                    item.model.set(ERROR_MESSAGE, null);
                 }
             }
             // Hide the notice as well.
