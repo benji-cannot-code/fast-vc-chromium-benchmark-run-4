0ca59bdcb30f90de103a19b82cf323d7ae5ed334
Ankush Singh <ankushkush@google.com>
2026-07-30 07:34:50 -0700

    STTS: Support plural strings for auto-open infobar title
    Change message banner title from 'Links Received' to 'Link received' for
    single tab or 'N links received' for multiple tabs when receiving tabs
    on iOS.
    
    Bug: 532156279
    Change-Id: I945ef117cc97b07424736d8321f66143236d737a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8164949
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671051}

--- ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.mm
+++ ios/chrome/browser/send_tab_to_self/model/send_tab_to_self_browser_agent.mm
@@ -150,7 +150,7 @@ void SendTabToSelfBrowserAgent::DisplayNewEntries(
       // Only display the infobar banner if the active WebState is currently
       // visible (i.e., user is not in the Tab Grid screen or a Settings page).
       if (web_state->IsVisible()) {
-        DisplayInfoBar(web_state, new_entries.back());
+        DisplayInfoBar(web_state, new_entries.back(), new_entries.size());
       }
     } else {
       for (size_t ii = 0; ii < new_entries.size(); ++ii) {
@@ -185,7 +185,7 @@ void SendTabToSelfBrowserAgent::DisplayNewEntries(
   // Since we can only show one infobar at the time, pick the most recent entry.
   // TODO(crbug.com/40619532): Create a function that returns the most recently
   // shared entry.
-  DisplayInfoBar(web_state, new_entries.back());
+  DisplayInfoBar(web_state, new_entries.back(), new_entries.size());
 }
 
 void SendTabToSelfBrowserAgent::DismissEntries(
@@ -244,7 +244,7 @@ void SendTabToSelfBrowserAgent::OnActiveWebStateChanged(
   }
 
   if (pending_entry_) {
-    DisplayInfoBar(new_active, pending_entry_);
+    DisplayInfoBar(new_active, pending_entry_, /*opened_tab_count=*/1);
     CleanUpObserversAndVariables();
   }
 }
@@ -261,7 +261,7 @@ void SendTabToSelfBrowserAgent::WasShown(web::WebState* web_state) {
   DCHECK(pending_entry_);
   DCHECK(pending_web_state_);
 
-  DisplayInfoBar(pending_web_state_, pending_entry_);
+  DisplayInfoBar(pending_web_state_, pending_entry_, /*opened_tab_count=*/1);
 
   CleanUpObserversAndVariables();
 }
@@ -281,7 +281,8 @@ void SendTabToSelfBrowserAgent::WebStateDestroyed(web::WebState* web_state) {
 
 void SendTabToSelfBrowserAgent::DisplayInfoBar(
     web::WebState* web_state,
-    const send_tab_to_self::SendTabToSelfEntry* entry) {
+    const send_tab_to_self::SendTabToSelfEntry* entry,
+    size_t opened_tab_count) {
   infobars::InfoBarManager* infobar_manager =
       InfoBarManagerImpl::FromWebState(web_state);
 
@@ -293,7 +294,7 @@ void SendTabToSelfBrowserAgent::DisplayInfoBar(
 
   infobar_manager->AddInfoBar(CreateConfirmInfoBar(
       send_tab_to_self::IOSSendTabToSelfInfoBarDelegate::Create(
-          entry, model_,
+          entry, opened_tab_count, model_,
           HandlerForProtocol(browser_->GetCommandDispatcher(), SceneCommands),
           browser_->GetWebStateList())));
 }
@@ -363,7 +364,7 @@ void SendTabToSelfBrowserAgent::CheckAndOpenPendingEntriesIfBrowserVisible() {
             kTabsOpenedInBackgroundUponActivation);
   }
   if (web_state->IsVisible()) {
-    DisplayInfoBar(web_state, pending_entries.back());
+    DisplayInfoBar(web_state, pending_entries.back(), pending_entries.size());
   }
 }
 
