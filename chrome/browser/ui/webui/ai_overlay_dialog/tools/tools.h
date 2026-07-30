f0e4980b8a16ce172b485d215290a746c47d2477
Gary Klassen <gklassen@chromium.org>
2026-07-30 11:29:35 -0700

    [ttc] Implement bookmark tools and browser tests
    Change-Id: Ie50d2aa7bcce4ecc953a7c9a4ebae8083d658792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118730
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671261}

--- chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.h
+++ chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.h
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "base/callback_list.h"
-#include "base/memory/raw_ptr.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.mojom.h"
 #include "content/public/browser/weak_document_ptr.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -57,6 +56,8 @@ class AiOverlayTools : public ai_overlay_dialog::mojom::AiOverlayTools {
                        SeekToTimestampCallback callback) override;
   void TranslatePage(const std::string& target_language,
                      TranslatePageCallback callback) override;
+  void AddBookmark(AddBookmarkCallback callback) override;
+  void RemoveBookmark(RemoveBookmarkCallback callback) override;
   void InvokeGlic(const std::string& prompt,
                   InvokeGlicCallback callback) override;
 
@@ -92,6 +93,7 @@ class AiOverlayTools : public ai_overlay_dialog::mojom::AiOverlayTools {
   std::unique_ptr<AnnotationTask> annotation_task_;
   content::WeakDocumentPtr annotation_document_;
   mojo::Remote<blink::mojom::AnnotationAgentContainer> annotation_container_;
+  base::CancelableTaskTracker task_tracker_;
 };
 
 }  // namespace ttc
