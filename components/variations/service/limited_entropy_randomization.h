9277504a7ca82cd48cb466f720c63ec890afa02e
Caitlin Fischer <caitlinfischer@google.com>
2026-07-30 05:26:05 -0700

    Use ClientFilterableState.platform to determine the entropy limit.
    This change is being made as part of the work to support high-entropy
    field trial randomization with the limited entropy randomization source
    on Android WebView.
    
    The logic cannot continue to depend on build flags because Android
    WebView won't necessarily have the same max limited entropy value (in
    bits) as Android Chrome.
    
    Bug: 532519440
    Change-Id: I32a17c3c83044bb47faacf9b76809ca487e3bcfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8123459
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670987}

--- components/variations/service/limited_entropy_randomization.h
+++ components/variations/service/limited_entropy_randomization.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string_view>
 
+#include "components/variations/proto/study.pb.h"
+
 // Provides functions to validate that the variations seed is
 // correctly configured to respect an entropy limit. See below for details.
 //
@@ -76,12 +78,13 @@ struct MisconfiguredEntropyResult {
   std::optional<bool> seed_has_active_low_layer;
 };
 
-// The maximum amount of total entropy, in bits, for field trials with Google
-// web experiment ids.
+// Returns the platform-specific maximum amount of total entropy, in bits, for
+// field trials (A) with Google web experiment IDs or Google web trigger
+// experiment IDs and (B) that are randomized with the limited entropy source.
 //
 // The cumulative probability of group assignments across all such field trials
-// on the client must be at least 1 / (2 ^ GetGoogleWebEntropyLimitInBits()).
-double GetGoogleWebEntropyLimitInBits();
+// on the client can be at most 1 / (2 ^ GetGoogleWebEntropyLimitInBits()).
+double GetMaxLimitedEntropyInBits(Study::Platform platform);
 
 // Returns an object whose is_misconfigured field is true if the entropy from
 // the variations seed is misconfigured or if the entropy cannot be computed. If
@@ -100,11 +103,11 @@ double GetGoogleWebEntropyLimitInBits();
 // * client_state: The client state to use for filtering studies.
 // * seed: The seed to check for misconfigured entropy.
 // * entropy_limit_in_bits: The entropy limit to use for checking. Exposed for
-//     testing. Should be set to GetGoogleWebEntropyLimitInBits() in production.
+//     testing. Should be set to GetMaxLimitedEntropyInBits() in production.
 MisconfiguredEntropyResult SeedHasMisconfiguredEntropy(
     const ClientFilterableState& client_state,
     const VariationsSeed& seed,
-    double entropy_limit_in_bits = GetGoogleWebEntropyLimitInBits());
+    double entropy_limit_in_bits);
 
 }  // namespace variations
 
--- components/variations/service/limited_entropy_randomization_unittest.cc
+++ components/variations/service/limited_entropy_randomization_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace variations {
-
 namespace {
 
 constexpr int kTestLayerId = 101;
@@ -796,18 +795,14 @@ TEST_F(LimitedEntropyRandomizationTest,
   }
 }
 
-TEST(GetGoogleWebEntropyLimitInBits, IsPlatformSpecific) {
-  constexpr double kExpectedEntropyLimitInBits =
-#if BUILDFLAG(IS_ANDROID)
-      21.0;
-#elif BUILDFLAG(IS_IOS) || BUILDFLAG(IS_WIN)
-      18.0;
-#elif BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS)
-      16.0;
-#else
-      1.0;
-#endif
-  EXPECT_EQ(GetGoogleWebEntropyLimitInBits(), kExpectedEntropyLimitInBits);
+TEST(GetMaxLimitedEntropyInBits, IsPlatformSpecific) {
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_ANDROID), 21.0);
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_WINDOWS), 18.0);
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_IOS), 18.0);
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_MAC), 16.0);
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_CHROMEOS), 16.0);
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_ANDROID_WEBVIEW), 1.0);
+  EXPECT_EQ(GetMaxLimitedEntropyInBits(Study::PLATFORM_LINUX), 1.0);
 }
 
 }  // namespace variations
