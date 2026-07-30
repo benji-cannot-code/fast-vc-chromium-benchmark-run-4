ae698eac23bc01e80e6d4cf55c5748625dd8aa85
Stefan Zager <szager@chromium.org>
2026-07-30 13:52:47 -0700

    [HiC] An origin-tainted filter should taint its subtree
    Bug: 532952073
    Change-Id: I86a21f9c6fc1928dab4b96f078121818d3409fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131725
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671390}

--- third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.h
+++ third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.h
@@ -169,6 +169,8 @@ class PLATFORM_EXPORT EffectPaintPropertyNode final
 
     bool needs_effect_for_2d_scale_transform = false;
 
+    bool is_in_tainted_subtree = false;
+
     bool is_in_canvas_subtree = false;
 
     PaintPropertyChangeType ComputeChange(
@@ -303,6 +305,8 @@ class PLATFORM_EXPORT EffectPaintPropertyNode final
   bool RequiresCompositingForCanvasChild() const {
     return state_.direct_compositing_reasons & CompositingReason::kCanvasChild;
   }
+
+  bool IsInTaintedSubtree() const { return state_.is_in_tainted_subtree; }
 
   bool IsInCanvasSubtree() const { return state_.is_in_canvas_subtree; }
 
