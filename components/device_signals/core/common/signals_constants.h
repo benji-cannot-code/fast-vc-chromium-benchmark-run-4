e4258d0e41603f488de85ebabe30f669759f2e35
Michał Kaczmarczyk <mickaczmarczyk@google.com>
2026-07-30 10:47:54 -0700

    [enterprise]Add vendorId signal to OsSignalsResponse
    This change introduces a new field `vendor_id` to the
    `OsSignalsResponse` struct for iOS-specific vendor ID information and
    adds a corresponding constant `kVendorId`.
    
    Bug: 421927804
    Change-Id: I2faf50390ea036db2b47e5d83ca12493e79315d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118548
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671218}

--- components/device_signals/core/common/signals_constants.h
+++ components/device_signals/core/common/signals_constants.h
@@ -47,6 +47,7 @@ extern const char kSiteIsolationEnabled[];
 extern const char kSystemDnsServers[];
 extern const char kTrigger[];
 extern const char kUserEnrollmentDomain[];
+extern const char kVendorId[];
 extern const char kWindowsMachineDomain[];
 extern const char kWindowsUserDomain[];
 
