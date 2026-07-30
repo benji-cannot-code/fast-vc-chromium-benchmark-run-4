ac18848611c129fc935bf88d775b411bce9f55df
rbpotter <rbpotter@chromium.org>
2026-07-30 15:47:12 -0700

    Settings Lit: Migrate search_page/, part 4
    In this part migrating search_engine_edit_dialog and
    search_engine_list_dialog.
    
    Bug: 393471368
    Change-Id: I83fe95907eab3b67a715279ab9f019a1dccba6bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167941
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671481}

--- chrome/test/data/webui/settings/search_engine_edit_dialog_test.ts
+++ chrome/test/data/webui/settings/search_engine_edit_dialog_test.ts
@@ -11,7 +11,7 @@ import type {CrInputElement, SettingsSearchEngineEditDialogElement} from 'chrome
 import {loadTimeData, SearchEnginesBrowserProxyImpl} from 'chrome://settings/settings.js';
 import type {SearchEngine} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {createSampleSearchEngine, TestSearchEnginesBrowserProxy} from './test_search_engines_browser_proxy.js';
 // clang-format on
@@ -35,7 +35,6 @@ suite('SearchEngineEditDialog', function() {
     dialog = document.createElement('settings-search-engine-edit-dialog');
     dialog.model = engine;
     document.body.appendChild(dialog);
-    return flushTasks();
   }
 
   /**
@@ -49,12 +48,12 @@ suite('SearchEngineEditDialog', function() {
       engine: SearchEngine, expectedDialogTitle: string,
       expectedReadonly: boolean) {
     const policySubtitleContainer =
-        dialog.shadowRoot!.querySelector('#policySubtitleContainer');
+        dialog.shadowRoot.querySelector('#policySubtitleContainer');
     assertTrue(!!policySubtitleContainer);
     assertEquals(
         expectedDialogTitle,
-        dialog.shadowRoot!.querySelector(
-                              'div[slot="title"]')!.textContent.trim());
+        dialog.shadowRoot.querySelector(
+                             'div[slot="title"]')!.textContent.trim());
 
     // Check that the cr-input fields are pre-populated.
     assertEquals(engine.name, dialog.$.searchEngine.value);
@@ -103,7 +102,7 @@ suite('SearchEngineEditDialog', function() {
      */
     function inputAndValidate(inputId: string): Promise<void> {
       const inputElement =
-          dialog.shadowRoot!.querySelector<CrInputElement>(`#${inputId}`)!;
+          dialog.shadowRoot.querySelector<CrInputElement>(`#${inputId}`)!;
       browserProxy.resetResolver('validateSearchEngineInput');
       inputElement.dispatchEvent(
           new CustomEvent('input', {bubbles: true, composed: true}));
@@ -152,9 +151,10 @@ suite('SearchEngineEditDialog', function() {
     return browserProxy.whenCalled('searchEngineEditCancelled');
   });
 
-  test('DialogValidateInputsWhenEnginesChanged', function() {
+  test('DialogValidateInputsWhenEnginesChanged', async function() {
     dialog.model = createSampleSearchEngine({name: 'G'});
-    dialog.set('keyword_', 'G');
+    dialog.$.keyword.value = 'G';
+    await microtasksFinished();
     webUIListenerCallback('search-engines-changed', {
       activeSiteShortcuts: [],
       inactiveSiteShortcuts: [createSampleSearchEngine({name: 'G'})],
@@ -288,15 +288,23 @@ suite('SearchEngineEditDialogInSearchEnginesPage', function() {
     return browserProxy.whenCalled('searchEngineEditCancelled');
   });
 
-  test('DialogValidateInputsWhenEnginesChanged', function() {
+  test('DialogValidateInputsWhenEnginesChanged', async function() {
     dialog.model = createSampleSearchEngine({name: 'G'});
-    dialog.set('keyword_', 'G');
+    await microtasksFinished();
+    browserProxy.resetResolver('validateSearchEngineInput');
+
     webUIListenerCallback('search-engines-changed', {
       defaults: [],
       actives: [],
       others: [createSampleSearchEngine({name: 'G'})],
       extensions: [],
     });
-    return browserProxy.whenCalled('validateSearchEngineInput');
+
+    // This may be called 3 times, once for each of searchEngine, keyword,
+    // and queryUrl, but searchEngine comes first.
+    const args = await browserProxy.whenCalled('validateSearchEngineInput');
+    assertEquals(2, args.length);
+    assertEquals('searchEngine', args[0]);
+    assertEquals('G', args[1]);
   });
 });
