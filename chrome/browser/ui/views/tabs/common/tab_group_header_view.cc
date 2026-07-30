16a43a24d387835cc62558d2c331bf18e5278be6
Kunal Daftari <kunaldaftari@google.com>
2026-07-30 12:49:01 -0700

    [Focus] Ensuring that left click does not collapse tab group
    While discussions are ongoing about how to handle the left click when in
    focus mode, we will just remove the functionality to avoid accidentally
    leaving the focus mode state.
    
    Test: TabGroupHeaderViewTest.LeftClickInFocusModeDoesNotToggleCollapse
    Bug: 540109292
    Change-Id: Ib86dd2e25c2999af26793133b97a101fac616540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8179563
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671341}

--- chrome/browser/ui/views/tabs/common/tab_group_header_view.cc
+++ chrome/browser/ui/views/tabs/common/tab_group_header_view.cc
@@ -266,8 +266,9 @@ gfx::Size TabGroupHeaderView::CalculatePreferredSize(
 }
 
 bool TabGroupHeaderView::OnKeyPressed(const ui::KeyEvent& event) {
-  if (event.key_code() == ui::VKEY_SPACE ||
-      event.key_code() == ui::VKEY_RETURN) {
+  if ((event.key_code() == ui::VKEY_SPACE ||
+       event.key_code() == ui::VKEY_RETURN) &&
+      !delegate_->IsGroupFocused()) {
     delegate_->ToggleCollapsedState(
         ToggleTabGroupCollapsedStateOrigin::kKeyboard);
     views::ElementTrackerViews::GetInstance()->NotifyViewActivated(
@@ -329,7 +330,8 @@ void TabGroupHeaderView::OnMouseReleased(const ui::MouseEvent& event) {
 
   bool open_editor_bubble =
       event.IsRightMouseButton() && !editor_bubble_tracker_.is_open();
-  bool toggle_collapse = event.IsLeftMouseButton();
+  bool toggle_collapse =
+      event.IsLeftMouseButton() && !delegate_->IsGroupFocused();
   if (open_editor_bubble) {
     ShowEditorBubble();
   } else if (toggle_collapse) {
@@ -350,10 +352,12 @@ void TabGroupHeaderView::OnGestureEvent(ui::GestureEvent* event) {
       break;
 
     case ui::EventType::kGestureTap:
-      delegate_->ToggleCollapsedState(
-          ToggleTabGroupCollapsedStateOrigin::kGesture);
-      views::ElementTrackerViews::GetInstance()->NotifyViewActivated(
-          kTabGroupHeaderElementId, this);
+      if (!delegate_->IsGroupFocused()) {
+        delegate_->ToggleCollapsedState(
+            ToggleTabGroupCollapsedStateOrigin::kGesture);
+        views::ElementTrackerViews::GetInstance()->NotifyViewActivated(
+            kTabGroupHeaderElementId, this);
+      }
       event->SetHandled();
       break;
 
