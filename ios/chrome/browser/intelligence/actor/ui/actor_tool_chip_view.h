3a59a2e560e14c36fe6f877363d0761254e211aa
Francis Beauchamp <fbeauchamp@google.com>
2026-07-30 07:01:56 -0700

    [iOS] Implement parameterless initializer for ActorToolChipView.
    Provide a default parameterless init constructor for the tool chip
    view, eliminating verbose placeholder configurations at the call site
    when the chip is initially pre-instantiated in a hidden state.
    
    Bug: 532209191
    Change-Id: I595594170a6e7ea0402b97469e7e705dbd43a943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8132098
    Reviewed-by: Joe Peplowski <joepeplowski@google.com>
    Commit-Queue: Francis Beauchamp <fbeauchamp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671027}

--- ios/chrome/browser/intelligence/actor/ui/actor_tool_chip_view.h
+++ ios/chrome/browser/intelligence/actor/ui/actor_tool_chip_view.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithText:(NSString*)text
                         icon:(UIImage*)icon NS_DESIGNATED_INITIALIZER;
 
-- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)init;
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
 
