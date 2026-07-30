ce54904de858c2ec0a9ccd5ed556ec5f6894e4cf
Hubert Chao <hchao@chromium.org>
2026-07-30 13:59:01 -0700

    [LNA] remove more treat-as-public cases
    Remove tests in chrome_web_platform_security_metrics_browsertest.cc that
    were duplicate.
    
    Move 1 to local_network_access_counters_browsertests.cc, then remove
    some unused test files and rename 2 to non-treat-as-public variants.
    
    Bypass-Check-License: moved files
    Bug: 527730705
    Change-Id: Ia323c5ba48a152f379a11b8189de7b64a4dc9c6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161929
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671395}

--- chrome/test/data/workers/shared_post_ready_with_pna_headers.js
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-self.addEventListener("connect", (evt) => {
-  evt.ports[0].postMessage("ready");
-});
--- content/browser/btm/btm_bounce_detector_browsertest.cc
+++ content/browser/btm/btm_bounce_detector_browsertest.cc
@@ -2121,7 +2121,7 @@ IN_PROC_BROWSER_TEST_F(BtmBounceDetectorBrowserTest,
     (async () => {
       const worker = await new Promise((resolve, reject) => {
         const worker =
-            new SharedWorker("/workers/shared_fetcher_treat_as_public.js");
+            new SharedWorker("/workers/shared_fetcher.js");
         worker.port.addEventListener("message", () => resolve(worker));
         worker.addEventListener("error", reject);
         worker.port.start();
@@ -2184,7 +2184,7 @@ IN_PROC_BROWSER_TEST_F(BtmBounceDetectorBrowserTest,
             EvalJs(GetActiveWebContents(), JsReplace(
                                                R"(
     (async () => {
-      const worker = new Worker("/workers/fetcher_treat_as_public.js");
+      const worker = new Worker("/workers/fetcher.js");
 
       const messagePromise = new Promise((resolve) => {
         const listener = (event) => resolve(event.data);
