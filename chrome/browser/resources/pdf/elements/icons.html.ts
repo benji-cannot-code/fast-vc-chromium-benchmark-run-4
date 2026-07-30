0e0510e1a57ca09b5be1c69b1af6c43934f905e3
Emily Shack <emshack@chromium.org>
2026-07-29 18:44:48 -0700

    [GlowUp] Check in pdf/elements/icons.html.ts
    This will enable us to, in a follow-up CL, conditionally return a
    different iconset when the rounded WebUI icons flag is enabled.
    
    See go/chrome-icon-modernization-dd. This is part of a larger effort to
    standardize icon naming, enabling easier bulk updates in the future, as
    well as applying 100% roundedness to all existing icons.
    
    Bug: 511303545
    Change-Id: I5d886c603cfbe65acea58fd9c6665025a16d9a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141671
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670752}

--- chrome/browser/resources/pdf/elements/icons.html.ts
+++ chrome/browser/resources/pdf/elements/icons.html.ts
@@ -1,5 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<cr-iconset size="20" name="pdf">
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_elements/cr_icon/cr_iconset.js';
+
+import {getTrustedHTML} from 'chrome://resources/js/static_types.js';
+
+const div = document.createElement('div');
+div.innerHTML = getTrustedHTML`<cr-iconset size="20" name="pdf">
   <svg>
     <defs>
       <g id="add" viewBox="0 -960 960 960"><path d="M444-444H240v-72h204v-204h72v204h204v72H516v204h-72v-204Z"></path></g>
@@ -73,3 +82,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </svg>
   </cr-iconset>
 </if>
+`;
+
+const iconsets = div.querySelectorAll('cr-iconset');
+for (const iconset of iconsets) {
+  document.head.appendChild(iconset);
+}
