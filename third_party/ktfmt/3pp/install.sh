9b4904f59dab6a372767059302c944d374512630
Sam Maier <smaier@google.com>
2026-07-30 07:03:41 -0700

    Add ktfmt to 3pp
    Adds a Kotlin formatter, to be autorolled.
    
    Bug: 539987971
    Change-Id: I975ae65235561625a7fe112f533c3d656a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162623
    Auto-Submit: Sam Maier <smaier@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Sam Maier <smaier@google.com>
    Reviewed-by: Rachael Newitt <renewitt@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671029}

---
+++ third_party/ktfmt/3pp/install.sh
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+set -o pipefail
+
+PREFIX="$1"
+mkdir -p "$PREFIX"
+
+# Copy ktfmt.jar to the package.
+cp ktfmt.jar "$PREFIX"
+
+# Add README.chromium file to the package after substituting the version with $_3PP_VERSION.
+THIS_DIR=$(dirname "$0")
+sed 's/$_3PP_VERSION/'"$_3PP_VERSION"'/' "$THIS_DIR/README.chromium.template" > "$PREFIX/README.chromium"
+
+# Add license file directly from upstream to the package.
+curl -s -S -f "https://raw.githubusercontent.com/Kotlin/ktfmt/master/LICENSE" -o "$PREFIX/LICENSE"
