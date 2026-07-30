e6c09750eb9fbea5bf60bb58c27eb4b7e7b0113a
Bo Liu <boliu@chromium.org>
2026-07-30 07:24:18 -0700

    android: Check for non-finite values in MotionEvent
    Native side no longer accepts non-finite values for events, and there
    there are apps in webview that inject NaN events. Check for those.
    
    Note the check is no way exhaustive and can be expanded in the future if
    needed.
    
    Bug: 538075655
    Change-Id: Id64e58d73d74c4b6aefdc4007b1bcccaf98d9154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162586
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671044}

--- ui/android/java/src/org/chromium/ui/base/EventForwarder.java
+++ ui/android/java/src/org/chromium/ui/base/EventForwarder.java
@@ -225,6 +225,9 @@ public class EventForwarder {
      * @see View#onTouchEvent(MotionEvent)
      */
     public boolean onTouchEvent(MotionEvent event) {
+        if (!validateMotionEvent(event)) {
+            return false;
+        }
         boolean requiresSpecialHandling = touchEventRequiresSpecialHandling(event);
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
@@ -249,6 +252,9 @@ public class EventForwarder {
      * @param event the MotionEvent targeting the handle.
      */
     public boolean onTouchHandleEvent(MotionEvent event) {
+        if (!validateMotionEvent(event)) {
+            return false;
+        }
         final boolean isTouchHandleEvent = true;
         return sendTouchEvent(event, isTouchHandleEvent);
     }
@@ -453,6 +459,9 @@ public class EventForwarder {
      */
     public boolean onHoverEvent(MotionEvent event) {
         TraceEvent.begin("onHoverEvent");
+        if (!validateMotionEvent(event)) {
+            return false;
+        }
 
         if (mStylusWritingDelegate != null) {
             mStylusWritingDelegate.handleHoverEvent(event);
@@ -526,6 +535,9 @@ public class EventForwarder {
      */
     public boolean onMouseEvent(MotionEvent event) {
         TraceEvent.begin("sendMouseEvent");
+        if (!validateMotionEvent(event)) {
+            return false;
+        }
         boolean didOffsetEvent = false;
         try {
             if (hasTouchEventOffset()) {
@@ -867,6 +879,9 @@ public class EventForwarder {
      */
     public boolean onGenericMotionEvent(MotionEvent event) {
         if (mNativeEventForwarder == 0) return false;
+        if (!validateMotionEvent(event)) {
+            return false;
+        }
         boolean isMouseEvent =
                 (event.getSource() & InputDevice.SOURCE_CLASS_POINTER) != 0
                         && event.getToolType(0) == MotionEvent.TOOL_TYPE_MOUSE;
@@ -899,6 +914,9 @@ public class EventForwarder {
      */
     @VisibleForTesting
     public boolean onCapturedPointerEvent(MotionEvent event, int deviceRotation) {
+        if (!validateMotionEvent(event)) {
+            return false;
+        }
         boolean shouldConvertToMouseEvent =
                 isTrackpadToMouseEventConversionEnabled()
                         && event.isFromSource(InputDevice.SOURCE_TOUCHPAD);
@@ -1061,6 +1079,18 @@ public class EventForwarder {
     @CalledByNative
     public static @Nullable EventForwarder getJavaObject(long nativeEventForwarder) {
         return sEventForwarders.get(nativeEventForwarder);
+    }
+
+    private static boolean validateMotionEvent(MotionEvent event) {
+        for (int i = 0; i < event.getPointerCount(); ++i) {
+            if (!Float.isFinite(event.getX(i))
+                    || !Float.isFinite(event.getY(i))
+                    || !Float.isFinite(event.getRawX(i))
+                    || !Float.isFinite(event.getRawY(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
