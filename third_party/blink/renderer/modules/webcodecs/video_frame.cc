bf06c11e33a59c5eea2feb158400f7ecd349eb10
Saifuddin Hitawala <hitawala@chromium.org>
2026-07-30 10:14:35 -0700

    [webcodecs] Disable kUseSharedImageFormatForWebcodecsVideoFrame
    Disable the feature for now while we investigate the regressions.
    Then re-enable it. Also suppress DUMP_WILL_BE_CHECK as that now
    goes off due to mismatch.
    
    Bug: 540129762, 537182166
    Change-Id: I6d8e3eb3df78dc525eb42e246b51fb3b8b13bf35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8172228
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671185}

--- third_party/blink/renderer/modules/webcodecs/video_frame.cc
+++ third_party/blink/renderer/modules/webcodecs/video_frame.cc
@@ -230,7 +230,7 @@ bool IsFormatEnabled(media::VideoPixelFormat fmt) {
 }
 
 BASE_FEATURE(kUseSharedImageFormatForWebcodecsVideoFrame,
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 class CachedVideoFramePool : public GarbageCollected<CachedVideoFramePool>,
                              public Supplement<ExecutionContext>,
