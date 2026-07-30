53ab195aaed0559ffff771c2dfa5fa66fd84be5a
Calder Kitagawa <ckitagawa@chromium.org>
2026-07-30 12:08:00 -0700

    [Tab] deprecate getActivity (1/n)
    Migrate TabWebContentsObserver to use TabUtils.getActivity(mTab) and
    HelpAndFeedbackLauncherImpl instead of calling mTab.getActivity().
    
    Bug: 466371728
    Change-Id: Ib716139c018f05bb16f81e15ed71e5b66a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163330
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671298}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -149,11 +149,14 @@ public class TabWebContentsObserver extends TabWebContentsUserData {
 
                 /* buttonAction= */ () -> {
                     if (sadTab.showSendFeedbackView()) {
-                        assumeNonNull(mTab.getActivity())
-                                .startHelpAndFeedback(
-                                        mTab.getUrl().getSpec(),
-                                        "MobileSadTabFeedback",
-                                        mTab.getProfile());
+                        Activity activity = TabUtils.getActivity(mTab);
+                        if (activity != null) {
+                            HelpAndFeedbackLauncherImpl.getForProfile(mTab.getProfile())
+                                    .showHelpAndFeedbackForUrl(
+                                            activity,
+                                            mTab.getUrl().getSpec(),
+                                            "MobileSadTabFeedback");
+                        }
                     } else {
                         mTab.reload();
                     }
