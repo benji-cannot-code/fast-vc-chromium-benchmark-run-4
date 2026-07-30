e4ef068ccb1e37906b5c230585120b9915b3cf8a
Andrew Grieve <agrieve@chromium.org>
2026-07-30 09:08:26 -0700

    Update BuildCompat.kt and graphics-core.cpp
    Seems like no-op changes. Updated via their update_source.sh
    
    Change-Id: I8d62e16e55959cabb6f02df6f5f793df882b191e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177183
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671125}

--- third_party/androidx/local_modifications/graphics_core/cpp/graphics-core.cpp
+++ third_party/androidx/local_modifications/graphics_core/cpp/graphics-core.cpp
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <poll.h>
 #include <unistd.h>
 #include <ctime>
-#include <errno.h>
+#include <unistd.h>
 #include <android/native_activity.h>
 #include <android/surface_control.h>
 #include <android/api-level.h>
