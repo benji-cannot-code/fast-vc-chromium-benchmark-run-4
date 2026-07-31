4c2323a1791d8a709ee8879f719fbf0c5b765b6e
Yixi Wu <yixiwu@google.com>
2026-07-31 10:54:29 -0700

    [iOS] Add pasted content analysis method to DataControlsTabHelper
    Pasted content analysis is guarded by a feature flag here:
    https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/enterprise/connectors/connectors_util.mm;l=195;drc=2c2e022b251a82064c60e67b17f9103162a501aa#:~:text=194-,195,-196
    
    This CL moves the logic in the original `FinishPaste` to
    PasteIfAllowedByContentAnalysis`. Adds the `RunPastedContentAnalysis`
    method but currently just allow the paste. A subsequent CL will create
    the PastedContentHandlerIOS inside the `RunPastedContentAnalysis` and
    upload the pasteboard content for scanning. Then the action outcome will
    be determine by the scan result.
    
    Moved the logic of creating a CopiedTextSource to a static function in
    ios_clipboard_context so that it can be reused without creating an
    instance of IOSClipboardContext.
    
    Bug: 537767156
    Change-Id: Id5ded4dc460269578706a3945cb4f00d16695e84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142901
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671964}

--- ios/chrome/browser/enterprise/data_controls/model/data_controls_tab_helper.h
+++ ios/chrome/browser/enterprise/data_controls/model/data_controls_tab_helper.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "base/memory/weak_ptr.h"
 #import "base/scoped_observation.h"
+#import "components/enterprise/common/proto/connectors.pb.h"
 #import "components/enterprise/data_controls/core/browser/verdict.h"
+#import "ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager.h"
 #import "ios/chrome/browser/enterprise/data_controls/model/data_controls_pasteboard_manager_observer.h"
 #import "ios/chrome/browser/enterprise/data_controls/utils/clipboard_utils.h"
 #import "ios/chrome/browser/enterprise/enterprise_dialog/model/warning_dialog.h"
@@ -19,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "url/gurl.h"
 
 @protocol SnackbarCommands;
+
+namespace enterprise_connectors {
+struct RequestHandlerResult;
+}
 
 namespace web {
 class WebState;
@@ -101,6 +107,33 @@ class DataControlsTabHelper
   // Returns true if clipboard data controls are enabled.
   bool IsClipboardDataControlsEnabled() const;
 
+  // Block the paste if the action outcome of Data Controls is Block, or the
+  // user decides to cancel on Warn. Otherwise, start a Pasted Content Analysis
+  // if it is enabled for either the source profile or destination profile.
+  void PasteIfAllowedByDataControls(
+      const GURL& destination_url,
+      const GURL& source_url,
+      base::WeakPtr<ProfileIOS> destination_profile,
+      base::WeakPtr<ProfileIOS> source_profile,
+      const ui::ClipboardMetadata& metadata,
+      Verdict verdict,
+      base::OnceCallback<void(bool)> callback,
+      bool bypassed);
+
+  // Allow or block the paste or show a warning dialog based on the `result`.
+  void PasteIfAllowedByContentAnalysis(
+      base::OnceCallback<void(bool)> callback,
+      enterprise_connectors::RequestHandlerResult result);
+
+  // Run the pasted content analysis using the `PastedContentHandlerIOS` with
+  // the info from the parameters.
+  void RunPastedContentAnalysis(
+      const GURL& destination_url,
+      base::WeakPtr<ProfileIOS> profile,
+      enterprise_connectors::ContentMetaData::CopiedTextSource copied_source,
+      base::OnceCallback<void(bool)> callback,
+      std::optional<PasteboardContentDLP> pasteboard_content);
+
   // Finalizes the copy action invoking the callback.
   void FinishCopy(const GURL& source_url,
                   base::WeakPtr<ProfileIOS> source_profile,
@@ -109,15 +142,12 @@ class DataControlsTabHelper
                   base::OnceCallback<void(bool)> callback,
                   bool bypassed);
 
-  // Finalizes the paste action invoking the callback.
-  void FinishPaste(const GURL& destination_url,
-                   const GURL& source_url,
-                   base::WeakPtr<ProfileIOS> destination_profile,
-                   base::WeakPtr<ProfileIOS> source_profile,
-                   const ui::ClipboardMetadata& metadata,
-                   Verdict verdict,
-                   base::OnceCallback<void(bool)> callback,
-                   bool bypassed);
+  // Restores the pasteboard item to pasteboard if needed and then finalizes the
+  // paste action invoking the callback and resets `paste_event_state_` to
+  // `kIdle`.
+  void FinishPaste(base::OnceCallback<void(bool)> callback,
+                   bool verdict_or_scan_success,
+                   bool analysis_warn_bypassed);
 
   // Finalizes the share action invoking the callback.
   void FinishShare(const GURL& source_url,
