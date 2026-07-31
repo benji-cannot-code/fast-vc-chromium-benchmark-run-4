1a9f0fafb584e62c37d46b95b04bd11911c70c4f
Vidhan Jain <vidhanj@google.com>
2026-07-31 07:51:26 -0700

    [iOS] Convert properties to ivars in FormObserverHelper
    Change-Id: I0d48a9aaf45e8c53ed5fe1e5a0326528225c38f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8182726
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671842}

--- ios/chrome/browser/autofill/manual_fill/model/form_observer_helper.mm
+++ ios/chrome/browser/autofill/manual_fill/model/form_observer_helper.mm
@@ -5,22 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/autofill/manual_fill/model/form_observer_helper.h"
 
+#import "base/memory/raw_ptr.h"
 #import "components/autofill/core/common/form_data.h"
 #import "components/autofill/ios/form_util/form_activity_observer_bridge.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer_bridge.h"
 
 @interface FormObserverHelper () <FormActivityObserver, WebStateListObserving>
-// The WebStateList this instance is observing in order to update the
-// active WebState.
-@property(nonatomic, assign) WebStateList* webStateList;
-
-// The WebState this instance is observing. Can be nullptr.
-@property(nonatomic, assign) web::WebState* webState;
-
 @end
 
 @implementation FormObserverHelper {
+  // The WebStateList this instance is observing.
+  raw_ptr<WebStateList> _webStateList;
+
+  // The WebState this instance is observing. Can be nullptr.
+  raw_ptr<web::WebState> _webState;
+
   // Bridge to observe the web state list from Objective-C.
   std::unique_ptr<WebStateListObserverBridge> _webStateListObserver;
 
@@ -89,7 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                        change:(const WebStateListChange&)change
                        status:(const WebStateListStatus&)status {
   if (status.active_web_state_change()) {
-    self.webState = status.new_active_web_state;
+    [self setWebState:status.new_active_web_state];
   }
 }
 
