d487f5e41e66391e269ae3d1018a4b97210242ed
Yurii Lahodiuk <ylahodiuk@google.com>
2026-07-31 09:29:09 -0700

    Do not define pre-specified behavior in MockPasswordFormManagerForUI
    Bug: 538574948
    Change-Id: I5a25e4505af1dc16ec96b86b7e557b79e8a864f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8164722
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671907}

--- components/password_manager/core/browser/mock_password_form_manager_for_ui.cc
+++ components/password_manager/core/browser/mock_password_form_manager_for_ui.cc
@@ -7,12 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager {
 
-MockPasswordFormManagerForUI::MockPasswordFormManagerForUI() {
-  // TODO(crbug.com/538574948): Avoid defining pre-specified behavior in this
-  // mock.
-  ON_CALL(*this, IsFetchCompleted()).WillByDefault(testing::Return(true));
-  ON_CALL(*this, IsPasswordUpdate()).WillByDefault(testing::Return(false));
-}
+MockPasswordFormManagerForUI::MockPasswordFormManagerForUI() = default;
 MockPasswordFormManagerForUI::~MockPasswordFormManagerForUI() = default;
 
 }  // namespace password_manager
