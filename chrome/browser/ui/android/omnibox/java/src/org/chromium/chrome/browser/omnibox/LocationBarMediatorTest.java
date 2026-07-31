bb030f352a61d0dbf1207b206082c3351a70329f
Patrick Noland <pnoland@google.com>
2026-07-31 14:25:05 -0700

    Adjust urlbar a11y order to account for aim chip
    Bug: 539588696
    Change-Id: I517a099f3859ef56c1381a078a2802f1181a99b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8169241
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672110}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
@@ -4270,6 +4270,17 @@ public class LocationBarMediatorTest {
     }
 
     @Test
+    public void testUrlBarAccessibilityOrder() {
+        mActivationChipVisibilitySupplier.set(true);
+        verify(mUrlBar, atLeastOnce())
+            .setAccessibilityTraversalBefore(R.id.fusebox_activation_chip);
+
+        mActivationChipVisibilitySupplier.set(false);
+        verify(mUrlBar, atLeastOnce())
+            .setAccessibilityTraversalBefore(R.id.omnibox_suggestions_dropdown);
+    }
+
+    @Test
     public void testOnWindowFocusChanged_updatesStandbyRing() {
         mMediator.onFinishNativeInitialization();
         mProfileSupplier.set(mProfile);
