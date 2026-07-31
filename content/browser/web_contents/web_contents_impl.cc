5e84f90d9740879d69f0babbcbd56fbf9a79a10b
Etienne Pierre-doray <etiennep@chromium.org>
2026-07-31 04:47:36 -0700

    Avoid clearing pointer_lock_widget_ if widget is still locked.
    When a duplicate pointer lock response is received (which can happen
    if the browser-side delegate responds multiple times to a single request, e.g. in tests), the second
    response might fail because the callback has already been run and
    cleared. Previously, this failure would cause WebContentsImpl to
    clear pointer_lock_widget_, even though the lock was still active.
    This led to a CHECK failure later when the pointer was unlocked.
    This CL fixes the issue by checking if the widget is still locked
    before clearing the pointer.
    
    Bug: 534667063
    Change-Id: Ie0e4f13e0293e96a103365102266af0fba65d8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177063
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671751}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -7330,6 +7330,11 @@ bool WebContentsImpl::GotResponseToPointerLockRequest(
     if (pointer_lock_widget_->GotResponseToPointerLockRequest(result)) {
       return true;
     }
+
+    if (pointer_lock_widget_ && pointer_lock_widget_->GetView() &&
+        pointer_lock_widget_->GetView()->IsPointerLocked()) {
+      return true;
+    }
   }
 
   SetPointerLockWidgetInParentChain(nullptr);
