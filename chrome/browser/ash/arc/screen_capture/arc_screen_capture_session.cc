f92b451d9a2902333c24d34e29ba39a98baa4cd7
Saifuddin Hitawala <hitawala@chromium.org>
2026-07-31 07:42:43 -0700

    [arc] Add UMA to evaluate incoming SIFormat from Arc
    It would be ideal to invalidate SharedImage creation at IPC boundary
    for unsupported single channel SharedImageFormats. In order to do
    that, we add an UMA to investigate usages from Arc as that seems like
    last client for many of the single channel SIFormats.
    
    Bug: 416285370
    Change-Id: I7f30a43f247b1785dd492645825a9785bdcd1d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177464
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671835}

--- chrome/browser/ash/arc/screen_capture/arc_screen_capture_session.cc
+++ chrome/browser/ash/arc/screen_capture/arc_screen_capture_session.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shell.h"
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ash/notifications/screen_capture_notification_ui_ash.h"
 #include "chrome/browser/media/webrtc/desktop_capture_access_handler.h"
@@ -266,6 +267,8 @@ void ArcScreenCaptureSession::SetOutputBuffer(
       std::move(platform_file));
 
   viz::SharedImageFormat si_format = GetSharedImageFormat(buffer_format);
+  UMA_HISTOGRAM_ENUMERATION("Arc.ScreenCaptureSession.SharedImageFormat",
+                            viz::GetSharedImageFormatUMA(si_format));
   auto client_shared_image = sii->CreateSharedImage(
       {si_format, size_, gfx::ColorSpace(),
        // NOTE: This SI will be used as the destination of a copy of the desktop
