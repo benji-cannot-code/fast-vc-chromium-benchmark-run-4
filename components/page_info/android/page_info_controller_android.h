4478f124a964014de63b041576f30c584955fe4e
Richard Chen <richche@google.com>
2026-07-31 03:45:28 -0700

    Update help center URL for suspicious site warnings
    Replace the hardcoded Android suspicious site warning URL with a
    centralized p-link (`kUnsafeSiteWarningHelpCenterURL`).
    
    Key improvements:
    * Remove the generic `openUrl(String)` JNI method from PageInfo to
      prevent arbitrary URL execution from the Java layer.
    * Introduce an `is_suspicious_site` boolean flag in the native
      delegate to securely determine the correct help center routing.
    * Centralize URL constant definitions in `url_constants.h`.
    * Add metric tracking for when the unsafe site help center is opened
    
    Bug: b:532598569
    Change-Id: I0f23320d80caa3a5c09d2ae48eafe4dee6373eb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177967
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671738}

--- components/page_info/android/page_info_controller_android.h
+++ components/page_info/android/page_info_controller_android.h
@@ -40,7 +40,7 @@ class PageInfoControllerAndroid : public PageInfoUI {
 
   void OnSuspiciousSiteBackToSafety(JNIEnv* env);
   void OnSuspiciousSiteMarkAsSafe(JNIEnv* env);
-  void OpenUrl(JNIEnv* env, const base::android::JavaRef<jstring>& jurl);
+  void OpenSafeBrowsingHelpCenter(JNIEnv* env);
   void SetIsSuspiciousSite(JNIEnv* env, bool is_suspicious_site);
 
   // PageInfoUI implementations.
