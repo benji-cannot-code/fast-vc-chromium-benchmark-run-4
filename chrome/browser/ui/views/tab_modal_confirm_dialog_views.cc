2a50bf44b7cab00d5afd7d53052c5346323ab5d5
Bryan Kim <bryankim@google.com>
2026-07-31 00:18:23 -0700

    Componentize tab_modal_confirm_dialog
    Define source_set("tab_modal_confirm_dialog") and move the relevant
    files out of the monolith target //chrome/browser/ui:ui.
    
    TAG=agy
    CONV=248a8a6d-966c-4cc7-989e-d9ea0ea28d7a
    
    Bug: 353332589
    Change-Id: Ibf729a2dee69da84a5404bea8e78c5af8ee5d691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162559
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671667}

--- chrome/browser/ui/views/tab_modal_confirm_dialog_views.cc
+++ chrome/browser/ui/views/tab_modal_confirm_dialog_views.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
-#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/common/chrome_switches.h"
 #include "components/constrained_window/constrained_window_views.h"
