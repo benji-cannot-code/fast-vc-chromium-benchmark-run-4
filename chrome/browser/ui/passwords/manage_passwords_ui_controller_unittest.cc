19bd4c2ff3e8ff1853c536b312352681a74b703c
Vidhan Jain <vidhanj@google.com>
2026-07-31 05:40:55 -0700

    Address comments in bubble manager cleanups cls
    Bug: 540266470
    Change-Id: Id2231bfe2e03f5cb7aaaf7977cd3b57297056694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8174548
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671774}

--- chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
+++ chrome/browser/ui/passwords/manage_passwords_ui_controller_unittest.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/passwords/manage_passwords_page_action_controller.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/affiliations/core/browser/mock_affiliation_service.h"
-#include "components/autofill/core/common/autofill_features.h"
 #include "components/device_reauth/device_authenticator.h"
 #include "components/metrics/profile_metrics_service.h"
 #include "components/password_manager/core/browser/features/password_features.h"
