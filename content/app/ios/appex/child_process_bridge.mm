aabd9e89afaf12e7068a77235ce9881502a6b879
Gyuyoung Kim <gyuyoung@igalia.com>
2026-07-30 17:12:57 -0700

    [iOS Blink] Add UNSAFE_TODO() macro to fix a build break
    https://crrev.com/c/8152304 removed the allow_unsafe_buffers pragma
    from child_process_bridge.mm but missed wrapping the strdup() call,
    triggering -Wunsafe-buffer-usage-in-libc-call as an error on iOS
    Blink. Wrap it in UNSAFE_TODO(), matching the pattern used elsewhere
    in that CL.
    
    Bug: 351564777, 501112808
    Change-Id: I8a2b8b02e8f9527aa848d188b8728d9ca819fdc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8172403
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1671532}

--- content/app/ios/appex/child_process_bridge.mm
+++ content/app/ios/appex/child_process_bridge.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/mach_port_rendezvous_ios.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
+#include "base/compiler_specific.h"
 #include "base/files/file_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/system/sys_info.h"
@@ -82,7 +83,7 @@ extern "C" IOS_INIT_EXPORT void ChildProcessHandleNewConnection(
     g_argc = xpc_array_get_count(args_array);
     g_argv.resize(g_argc);
     for (size_t i = 0; i < g_argc; ++i) {
-      g_argv[i] = strdup(xpc_array_get_string(args_array, i));
+      g_argv[i] = UNSAFE_TODO(strdup(xpc_array_get_string(args_array, i)));
     }
 
     // Setup stdout/stderr.
