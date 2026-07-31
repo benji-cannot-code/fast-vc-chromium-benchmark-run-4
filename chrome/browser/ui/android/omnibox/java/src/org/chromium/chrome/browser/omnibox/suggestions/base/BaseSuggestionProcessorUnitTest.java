d6af74494c0bea3e31d4f78adebdab6a530dd790
Charles Cai <charlesyc@google.com>
2026-07-31 15:27:58 -0700

    Reduce close button icon to 18dp on Android desktop experience
    The icon was using different sizes of PNGs. This CL introduces the
    vector representation of the icon used for desktop experience devices.
    It mirrors the standard material_ic_close_24dp, but smaller.
    
    Bug: 540443467
    Change-Id: Ibed7abd9da083b3db69fc4b0c1bb75eb3a94515b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178527
    Commit-Queue: Charles Cai <charlesyc@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672131}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionProcessorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionProcessorUnitTest.java
@@ -18,6 +18,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.view.Gravity;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -393,7 +395,10 @@ public class BaseSuggestionProcessorUnitTest {
                         R.string.accessibility_omnibox_remove_suggestion,
                         mSuggestion.getFillIntoEdit());
         assertEquals(expectedDescription, action.accessibilityDescription);
-        assertEquals(R.drawable.btn_close, shadowOf(action.icon.drawable).getCreatedFromResId());
+        var layerDrawable = (LayerDrawable) action.icon.drawable;
+        assertEquals(
+                R.drawable.btn_close, shadowOf(layerDrawable.getDrawable(0)).getCreatedFromResId());
+        assertEquals(Gravity.CENTER, layerDrawable.getLayerGravity(0));
 
         var monitor = new UserActionTester();
         action.callback.run();
@@ -413,7 +418,10 @@ public class BaseSuggestionProcessorUnitTest {
                         R.string.accessibility_omnibox_remove_suggestion,
                         mSuggestion.getFillIntoEdit());
         assertEquals(expectedDescription, action.accessibilityDescription);
-        assertEquals(R.drawable.btn_close, shadowOf(action.icon.drawable).getCreatedFromResId());
+        var layerDrawable = (LayerDrawable) action.icon.drawable;
+        assertEquals(
+                R.drawable.btn_close, shadowOf(layerDrawable.getDrawable(0)).getCreatedFromResId());
+        assertEquals(Gravity.CENTER, layerDrawable.getLayerGravity(0));
 
         UserActionTester monitor = new UserActionTester();
         action.callback.run();
