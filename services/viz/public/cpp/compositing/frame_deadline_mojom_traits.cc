8035722f23802e3151413d47f21429c302e6b8f3
Aldo Culquicondor <acondor@chromium.org>
2026-07-31 12:59:10 -0700

    Reland "Viz deserialization crash keys for CompositorFrame and HitTestRegionList"
    This is a reland of commit e939020cf1aa2df5fd411e7eb303ed11c2a04c56
    
    Only including the test if the USE_CRASHPAD_ANNOTATION build flag is
    true and added an test-only access method so that tests in component
    builds can successfully read the keys.
    
    Original change's description:
    > Viz deserialization crash keys for CompositorFrame and HitTestRegionList
    >
    > This would allow us to find validation culprits more easily in
    > SubmitCompositorFrame.
    >
    > Bug: 537501108
    > Change-Id: I07cabb2e3f6ff7f6d9e2f88f984e0e61e6b49f80
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8125730
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    > Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1670660}
    
    Bug: 537501108
    Cq-Include-Trybots: luci.chromium.try:win10-dbg
    Change-Id: I7875fe9518080818b471026edc3d6e5a71f75906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8176563
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672038}

--- services/viz/public/cpp/compositing/frame_deadline_mojom_traits.cc
+++ services/viz/public/cpp/compositing/frame_deadline_mojom_traits.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "services/viz/public/cpp/compositing/frame_deadline_mojom_traits.h"
+
 #include "base/time/time.h"
+#include "services/viz/public/cpp/crash_keys.h"
 
 namespace mojo {
 
@@ -16,6 +18,7 @@ bool StructTraits<viz::mojom::FrameDeadlineDataView, viz::FrameDeadline>::Read(
   base::TimeDelta frame_interval;
   if (!data.ReadFrameStartTime(&frame_start_time) ||
       !data.ReadFrameInterval(&frame_interval)) {
+    viz::SetDeserializationCrashKeyString("Failed read FrameDeadline");
     return false;
   }
   *out = viz::FrameDeadline(frame_start_time, data.deadline_in_frames(),
