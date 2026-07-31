8b5a99be90046297772d16b0a5edd19e85ea5e8c
Frédéric Wang Nélar <fwang@igalia.com>
2026-07-31 10:17:50 -0700

    Add tests for available-dictionary header with large resources.
    This is a follow-up of CL 8126502.
    
    Bug: 522338661, 40255884
    Change-Id: I3b67622132675e3c528ecbea3b7962a30ecf9995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138686
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Commit-Queue: Frédéric Wang Nélar <fwang@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1671938}

---
+++ third_party/blink/web_tests/external/wpt/fetch/compression-dictionary/resources/static/dictionary-hashes.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Automatically generated. Do not edit.
+const kDictionaryHashes = {
+  'image-001.png': ':PpgmD4o9EwEsBe65al5+mvqXLnkKb4AqwwJVtmSfAbk=:',
+  'script-001.js': ':3zCnkOGQfE97PjI3XULs95l8v5tOsI1u0JfZ/68b3Ms=:',
+  'style-001.css': ':QRELpj3LbMtGHz8NKNX0rwxfKgwg+nPLzkJpBYGBYF8=:',
+  'subframe-001.html': ':lfrMRj63B8KxO9at3XzK4ouUSE274E/2YlxQBhPgvzk=:',
+};
