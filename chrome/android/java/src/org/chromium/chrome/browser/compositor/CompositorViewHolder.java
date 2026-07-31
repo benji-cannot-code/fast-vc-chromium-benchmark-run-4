1a973397fdd3682375409104c6e7ec1589fd138c
Jinsuk Kim <jinsukkim@google.com>
2026-07-31 12:33:32 -0700

    [Vertical Tabs] Ensures WebContents settles downs to the right width
    CompositorViewHolder#updateWebContentsSize() was not taking into account
    the Vertical Tabs for adjusting its width. This CL now checks both
    Side Panel and Vertical Tabs to determine the final width using
    their SideUiSpecs.
    
    Bug: 539978724
    Change-Id: I8e01e11a0d8fd352fa75d228ed26fa4bd65a0564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167342
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672023}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -96,6 +96,7 @@ import org.chromium.chrome.browser.ui.side_ui.SideUiCoordinator.AnchorSide;
 import org.chromium.chrome.browser.ui.side_ui.SideUiCoordinator.SideUiSpecs;
 import org.chromium.chrome.browser.ui.side_ui.SideUiObserver;
 import org.chromium.chrome.browser.ui.side_ui.SideUiStateProvider;
+import org.chromium.chrome.browser.ui.vertical_tabs.VerticalTabUtils;
 import org.chromium.components.browser_ui.widget.TouchEventObserver;
 import org.chromium.components.browser_ui.widget.TouchEventProvider;
 import org.chromium.components.content_capture.OnscreenContentProvider;
@@ -1145,7 +1146,8 @@ public class CompositorViewHolder extends FrameLayout
         // Note that a non-null widthOverride already considered side-anchored UI (see callers of
         // this method), so we only need to consider side-anchored UI when widthOverride is null.
         int horizontalViewportInsets = 0;
-        if (AndroidSidePanelEnabledFn.isEnabled()
+        if ((AndroidSidePanelEnabledFn.isEnabled()
+                        || VerticalTabUtils.isVerticalTabsEligible(mActivity))
                 && mSideUiStateProvider != null
                 && widthOverride == null) {
             SideUiSpecs sideUiSpecs = mSideUiStateProvider.getCurrentSideUiSpecs();
