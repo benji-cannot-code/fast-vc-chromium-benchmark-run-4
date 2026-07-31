206fd7ebdbbde40bc625839640e9a99e9c838dcf
Brandon Fong <bjfong@google.com>
2026-07-31 16:58:27 -0700

    [search] Move the initialization logic for tab search outside of VT
    Currently if Vertical Tabs is disabled, the tab search side panel will
    not show. Decouple the behavior from that so it will show regardless of
    enable status for VT.
    
    Bug: 532630573
    Change-Id: I85cd7d840418784cb19733818f7fcf70c13360ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8182848
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672177}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -2282,6 +2282,27 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
             stripBottomPxSupplier = stripLayoutHelperManager.getStripBottomPxSupplier();
         }
 
+        if (ChromeFeatureList.sTabSearchForDesktop.isEnabled()) {
+            ViewGroup tabSearchParent =
+                    anchorContainerParent != null
+                            ? anchorContainerParent
+                            : assumeNonNull(mCoordinator);
+            mTabSearchOverlayCoordinator =
+                    new TabSearchOverlayCoordinator(
+                            mActivity,
+                            tabSearchParent,
+                            mWindowAndroid,
+                            mProfileSupplier,
+                            assumeNonNull(mSnackbarManagerSupplier.get()),
+                            mModalDialogManagerSupplier,
+                            mActivityLifecycleDispatcher,
+                            mTabModelSelectorSupplier,
+                            mEdgeToEdgeManager.getEdgeToEdgeSystemBarColorHelper(),
+                            mBackPressManager,
+                            mCompositorViewHolderSupplier,
+                            mTabGroupUiActionHandlerSupplier);
+        }
+
         mSideUiCoordinator =
                 SideUiCoordinatorFactory.create(
                         mActivity,
@@ -2390,23 +2411,6 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
         View secondaryUiContainer = mActivity.findViewById(R.id.secondary_ui_container);
         mSecondaryUiContainerMarginAdjuster = new ViewMarginAdjusterForSideUi(secondaryUiContainer);
         mSideUiCoordinator.addObserver(mSecondaryUiContainerMarginAdjuster);
-
-        if (ChromeFeatureList.sTabSearchForDesktop.isEnabled()) {
-            mTabSearchOverlayCoordinator =
-                    new TabSearchOverlayCoordinator(
-                            mActivity,
-                            anchorContainerParent,
-                            mWindowAndroid,
-                            mProfileSupplier,
-                            assumeNonNull(mSnackbarManagerSupplier.get()),
-                            mModalDialogManagerSupplier,
-                            mActivityLifecycleDispatcher,
-                            mTabModelSelectorSupplier,
-                            mEdgeToEdgeManager.getEdgeToEdgeSystemBarColorHelper(),
-                            mBackPressManager,
-                            mCompositorViewHolderSupplier,
-                            mTabGroupUiActionHandlerSupplier);
-        }
     }
 
     @SuppressWarnings("UseSharedPreferencesManagerFromChromeCheck")
