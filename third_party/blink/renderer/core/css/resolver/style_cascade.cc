3877fe46872828836ad83836edfce9c846981624
Rune Lillesveen <futhark@chromium.org>
2026-07-31 07:59:29 -0700

    Invalidate if(navigation(phase:)) changes
    Missing call to RouteMap::SetNeedsStyleUpdateOnNavigation()
    
    Bug: 436805487
    
    Change-Id: I30acc44350b61c5069b6fbcc77b9577b01b5308c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8176845
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671846}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
+#include "third_party/blink/renderer/core/route_matching/route_map.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
@@ -2848,6 +2849,8 @@ bool StyleCascade::EvalIfCondition(CSSParserTokenStream& stream,
         const NavigationExpNode& node) override {
       // Evaluate navigation() function
       resolver_state_.StyleBuilder().SetAffectedByFunctionalNavigation();
+      RouteMap::Ensure(resolver_state_.GetDocument())
+          .SetNeedsStyleUpdateOnNavigation();
       StyleEngine& style_engine =
           resolver_state_.GetDocument().GetStyleEngine();
       bool result =
