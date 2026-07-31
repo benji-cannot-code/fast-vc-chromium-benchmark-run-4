1997cb5e39971438cb89fbdf8a3aec9b5593c3f6
Martin Verde <thesalsa@google.com>
2026-07-30 19:19:32 -0700

    Roll rustversion: 1.0.22 => 1.0.23 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * rustversion: 1.0.22 => 1.0.23;
      https://docs.rs/crate/rustversion/1.0.23
    
    Bug: None
    Change-Id: Ice67a0ae5c94e23057a1c33d9801690e6f998c76
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139864
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Martin Pan-Verde <thesalsa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671601}

--- third_party/rust/chromium_crates_io/vendor/rustversion-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/rustversion-v1/src/lib.rs
@@ -176,14 +176,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! <br>
 
-#![doc(html_root_url = "https://docs.rs/rustversion/1.0.22")]
+#![doc(html_root_url = "https://docs.rs/rustversion/1.0.23")]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
     clippy::derive_partial_eq_without_eq,
     clippy::doc_markdown,
     clippy::enum_glob_use,
-    clippy::from_iter_instead_of_collect,
     // https://github.com/rust-lang/rust-clippy/issues/8539
     clippy::iter_with_drain,
     clippy::module_name_repetitions,
