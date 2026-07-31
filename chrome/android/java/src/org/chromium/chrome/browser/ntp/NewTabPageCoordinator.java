6f3eaa7b939abfaad0bfbf3925d83cefd45431aa
Xi Han <hanxi@google.com>
2026-07-31 13:09:20 -0700

    [Aurora] Add extra top padding for google logo.
    In this CL, we adjust Google logo's top padding based on the
    PaddingStyle, use 24dp, 32dp, and 48dp compared with the default 12dp.
    Both NTP and the NTP customization preview dialog will adjust top
    padding together. Screenshots: http://shortn/_sMdowfz7H1.
    
    Bug: 534357676
    Change-Id: I24edc6c240afecbd6fed6dfae17f06d77bc7c101
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8183231
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672055}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
@@ -998,7 +998,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
             return LogoUtils.getTopMarginForDoodle(resources);
         }
 
-        return resources.getDimensionPixelSize(R.dimen.ntp_logo_margin_top);
+        return LogoUtils.getTopMarginForLogo(resources);
     }
 
     /**
