292fc65e698e89e7635b0b23e7b474c10505febc
Katie Lu <ktlu@google.com>
2026-07-31 20:36:32 -0700

    Add PDFTranslationCoordinator to determine if a PDF is translatable.
    This CL introduces PDFTranslationCoordinator, a DocumentUserData class that orchestrates the PDF-specific translation lifecycle. It checks if a PDF is translatable by querying PDFDocumentHelper to verify that the document has meaningful text, does not contain JavaScript, and is not password-protected.
    
    The new coordinator and its unit tests are guarded by the enable_pdf_translation build flag.
    
    Change-Id: I751a10917f36302cb101120dfe9e4658e6ca3155
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161930
    Reviewed-by: Paula Gómez <paugomez@google.com>
    Commit-Queue: Katie Lu <ktlu@google.com>
    Reviewed-by: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672235}

--- components/translate/core/browser/language_state.cc
+++ components/translate/core/browser/language_state.cc
@@ -58,6 +58,7 @@ void LanguageState::DidNavigate(bool is_same_document_navigation,
   navigation_from_google_ = navigation_from_google;
 
   SetTranslateEnabled(false);
+  pdf_translatability_status_ = PdfTranslatabilityStatus::kNotChecked;
 }
 
 void LanguageState::LanguageDetermined(
