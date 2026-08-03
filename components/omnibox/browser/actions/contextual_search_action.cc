339820dcd1d9b13bed0e4f1f696287ebd6a2eed0
Caitlin Chen <caitc@google.com>
2026-08-03 12:34:25 -0700

    [omnibox] Swap contextual search suggestion icon to sparkloupe
    This CL updates the WebUI Omnibox handler to explicitly use the
    sparkloupe icon for contextual search suggestions
    (GROUP_CONTEXTUAL_SEARCH) when the kAskGSwapIcon feature parameter is
    enabled. Previously, it was unconditionally overriding it to a rotated
    reply icon.
    
    This CL also fixes a crash in WebUI when kAskGSwapIcon is enabled on
    platforms with rounded icons disabled.
    ContextualSearchOpenLensAction::GetVectorIcon() was unconditionally
    returning kSearchSparkIcon (new rounded variant), which failed to map in
    SearchboxHandler::AutocompleteIconToResourceName when rounded icons were
    disabled. The action now correctly respects
    features::IsRoundedIconsEnabled() and returns kSearchSparkOldIcon when
    appropriate. Same as when !defined(SUPPORT_PEDALS_VECTOR_ICONS).
    
    Before: http://screenshot/0mo3q9u5295ag
    After: http://screenshot/7m65nvnve2e60
    BUG: b:541382481
    Change-Id: Ic98633f9f079261dbbc8b880cb71ecf23b79d5c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177982
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Caitlin Chen <caitc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672834}

--- components/omnibox/browser/actions/contextual_search_action.cc
+++ components/omnibox/browser/actions/contextual_search_action.cc
@@ -146,7 +146,9 @@ const gfx::VectorIcon& ContextualSearchOpenLensAction::GetVectorIcon() const {
                  .open_lens_action_ui_tweaks
              ? vector_icons::kGoogleLensLogoIcon
              : (omnibox::kAskGSwapIcon.Get()
-                    ? omnibox::kSearchSparkIcon
+                    ? (features::IsRoundedIconsEnabled()
+                           ? omnibox::kSearchSparkIcon
+                           : omnibox::kSearchSparkOldIcon)
                     : vector_icons::kGoogleLensMonochromeLogoIcon);
 
 #else
