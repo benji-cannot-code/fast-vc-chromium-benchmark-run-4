2a2afb1447571f8bc3913d0d11ec9d8222b758d3
Rui Chen <chesterchen@google.com>
2026-08-03 11:41:57 -0700

    [AW] Add cached flag experiment WebViewUseWVLESForLayeredStudy
    We add cached flag experiment WebViewUseWVLESForLayeredStudy to
    allow us to turn on Variations Synchronisation when constructing
    layers.
    Together with existing allowlist, this should ensure layered
    study can be synchronised on a device.
    
    Change-Id: I46ffaeec6e011c8733614a83e3dde8a4b19213bc
    Bug: 535956645
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116929
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Rui Chen <chesterchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672797}

--- android_webview/browser/variations/aw_entropy_providers.cc
+++ android_webview/browser/variations/aw_entropy_providers.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "android_webview/common/aw_cached_flags.h"
+#include "android_webview/common/aw_features.h"
+#include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "components/metrics/entropy_state.h"
 
@@ -59,7 +62,9 @@ AwEntropyProviders::DelegatingEntropyProvider::~DelegatingEntropyProvider() =
 double AwEntropyProviders::DelegatingEntropyProvider::GetEntropyForTrial(
     std::string_view trial_name,
     uint32_t randomization_seed) const {
-  if (nonembedded_low_entropy_source_allowlist_->contains(trial_name)) {
+  if (nonembedded_low_entropy_source_allowlist_->contains(trial_name) ||
+      (trial_name.empty() && android_webview::CachedFlags::IsEnabled(
+                                 features::kWebViewUseWVLESForLayeredStudy))) {
     return nonembedded_low_entropy_source_provider_.GetEntropyForTrial(
         trial_name, randomization_seed);
   }
--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -80,6 +80,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewStaticMethodsNotTriggerStartup,
     &features::kWebViewTestFeature,
     &features::kWebViewUseMetricsUploadServiceOnlySdkRuntime,
+    &features::kWebViewUseWVLESForLayeredStudy,
     &features::kWebViewWarmupNetworkService,
     // keep-sorted end
 };
