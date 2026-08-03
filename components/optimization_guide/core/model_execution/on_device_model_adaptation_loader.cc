cdab093da172279e16f1384790b42763394ff0ce
Steven Holte <holte@google.com>
2026-08-03 12:32:01 -0700

    Move kOnDeviceModelAdaptationWeightsFile to on_device_model_adaptation_loader.h
    Move kOnDeviceModelAdaptationWeightsFile out of
    optimization_guide_constants.h/.cc into on_device_model_adaptation_loader.h,
    and remove the unused optimization_guide_constants.h include from
    on_device_model_adaptation_loader.cc.
    
    Bug: 514743962
    TAG=agy
    CONV=a15456f7-937e-490f-8e68-39e1b55c3110
    
    Change-Id: Ie1eb77468b25a2c44373469bad1eec816a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8183675
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Zekun Jiang <zekunjiang@google.com>
    Auto-Submit: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672833}

--- components/optimization_guide/core/model_execution/on_device_model_adaptation_loader.cc
+++ components/optimization_guide/core/model_execution/on_device_model_adaptation_loader.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h"
 #include "components/optimization_guide/core/model_execution/on_device_model_service_controller.h"
 #include "components/optimization_guide/core/model_execution/usage_tracker.h"
-#include "components/optimization_guide/core/optimization_guide_constants.h"
 #include "components/optimization_guide/core/optimization_guide_enums.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_switches.h"
--- components/optimization_guide/core/model_execution/on_device_model_adaptation_loader.h
+++ components/optimization_guide/core/model_execution/on_device_model_adaptation_loader.h
@@ -26,6 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace optimization_guide {
 
+// Files expected to be in the on device model adaptation bundle.
+inline constexpr base::FilePath::CharType
+    kOnDeviceModelAdaptationWeightsFile[] =
+        FILE_PATH_LITERAL("adaptation_weights.bin");
+
 class OnDeviceModelFeatureAdapter;
 class OnDeviceModelMetadata;
 class OptimizationGuideModelProvider;
