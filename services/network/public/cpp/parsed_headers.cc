b0a8495605cd121cbf3e532ab0bec2e9731e3583
Yao Xiao <yaoxia@chromium.org>
2026-08-03 09:29:26 -0700

    [Topics] Remove obsolete Observe-Browsing-Topics header parsing
    The Topics API is being deprecated and was effectively disabled in prior
    cleanup work.
    
    This CL removes the obsolete `Observe-Browsing-Topics` response header
    parsing logic. Downstream implementations have already been removed.
    
    Bug: 461709147
    Change-Id: Ifa3eedd9a37af897ab0840afbdb4fbb88815fff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8179348
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672716}

--- services/network/public/cpp/parsed_headers.cc
+++ services/network/public/cpp/parsed_headers.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/reporting/reporting_header_parser.h"
 #include "net/url_request/clear_site_data.h"
 #include "services/network/public/cpp/avail_language_header_parser.h"
-#include "services/network/public/cpp/browsing_topics_parser.h"
 #include "services/network/public/cpp/client_hints.h"
 #include "services/network/public/cpp/connection_allowlist.h"
 #include "services/network/public/cpp/connection_allowlist_parser.h"
@@ -168,9 +167,6 @@ mojom::ParsedHeadersPtr PopulateParsedHeaders(
 
   // The code here only parses the No-Vary-Search header if it is present.
   parsed_headers->no_vary_search_with_parse_error = ParseNoVarySearch(*headers);
-
-  parsed_headers->observe_browsing_topics =
-      ParseObserveBrowsingTopicsFromHeader(*headers);
 
   parsed_headers->allow_cross_origin_event_reporting =
       ParseAllowCrossOriginEventReportingFromHeader(*headers);
