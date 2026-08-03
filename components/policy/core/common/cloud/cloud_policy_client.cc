40a4b8c8ce95fd5f29313c641591d661c7175efd
Ohene Osei <t-oheneosei@microsoft.com>
2026-08-03 15:32:31 -0700

    policy: use std::move instead of Clone() for DMAuth
    GetDeviceAttributeUpdatePermission and UpdateDeviceAttributes each
    copy the auth parameter via Clone() even though it is never used
    again afterward. Use std::move instead, matching the pattern already
    used elsewhere in this file.
    
    Fixed: 541251908
    Change-Id: I3c1b8c21e48a10526c894f8a9fa0c7445c64a6c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8176837
    Reviewed-by: Chris Davis <chrdavis@microsoft.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Ohene Osei <t-oheneosei@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1672925}

--- components/policy/core/common/cloud/cloud_policy_client.cc
+++ components/policy/core/common/cloud/cloud_policy_client.cc
@@ -1416,7 +1416,7 @@ void CloudPolicyClient::GetDeviceAttributeUpdatePermission(
   if (auth.has_oauth_token()) {
     params.oauth_token = auth.oauth_token();
   } else {
-    params.auth_data = auth.Clone();
+    params.auth_data = std::move(auth);
     params.oauth_token = std::string();
   }
   params.callback = base::BindOnce(
@@ -1444,7 +1444,7 @@ void CloudPolicyClient::UpdateDeviceAttributes(
   if (auth.has_oauth_token()) {
     params.oauth_token = auth.oauth_token();
   } else {
-    params.auth_data = auth.Clone();
+    params.auth_data = std::move(auth);
     params.oauth_token = std::string();
   }
   params.callback =
