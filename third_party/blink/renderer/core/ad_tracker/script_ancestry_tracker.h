dd4f653805a4777dc23e3f222cc870acf5aa01fd
Josh Karlin <jkarlin@chromium.org>
2026-08-03 14:58:27 -0700

    [AdTracker] Refactor AdTracker to inherit from ScriptAncestryTracker
    Refactors AdTracker to inherit from the ScriptAncestryTracker base
    class. The AdTracker still has to implement marking of ad frames, handle
    fetch requests in CalculateIsAdSubresource, and keep track of
    AdAncestry. The rest is handled by the base class.
    
    Bug: 522344717
    Change-Id: Ifdad72e2ca8046df71807a4ac1b449d456b21abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8157639
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672919}

--- third_party/blink/renderer/core/ad_tracker/script_ancestry_tracker.h
+++ third_party/blink/renderer/core/ad_tracker/script_ancestry_tracker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/gtest_prod_util.h"
 #include "third_party/blink/renderer/core/ad_tracker/ad_script_identifier.h"
 #include "third_party/blink/renderer/core/ad_tracker/monkey_patchable_api.h"
 #include "third_party/blink/renderer/core/ad_tracker/script_initiation_monitor.h"
@@ -224,6 +225,9 @@ class CORE_EXPORT ScriptAncestryTracker
   // Tracks active monkey-patched API calls in the current task scope to ensure
   // the ignore-monkey-patch heuristic only applies once per task.
   HashSet<MonkeyPatchableApi> monkey_patch_calls_in_scope_;
+
+  friend class AdTrackerTest;
+  friend class ScriptAncestryTrackerTest;
 };
 
 }  // namespace blink
