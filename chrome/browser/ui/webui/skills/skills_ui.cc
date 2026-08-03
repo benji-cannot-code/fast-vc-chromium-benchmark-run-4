316fab3c672ace5a149f62c42b2f3ea3cfc49a13
Michael Kawano <mhkawano@google.com>
2026-08-03 12:10:46 -0700

    Passing the language code to CSL
    Demo: https://screenshot-v2.corp.google.com/76m8Zp5h56ai85M
    Change-Id: I5d9df7821cbbbd9a8f45b4f3b94e30eb69fe787b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8182471
    Commit-Queue: Michael Kawano <mhkawano@google.com>
    Reviewed-by: marissa shen <marissashen@google.com>
    Reviewed-by: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672817}

--- chrome/browser/ui/webui/skills/skills_ui.cc
+++ chrome/browser/ui/webui/skills/skills_ui.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/skills_resources.h"
 #include "chrome/grit/skills_resources_map.h"
 #include "components/application_locale_storage/application_locale_storage.h"
+#include "components/google/core/common/google_util.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/skills/features.h"
 #include "components/skills/public/skill.h"
@@ -172,6 +173,10 @@ SkillsUI::SkillsUI(content::WebUI* web_ui)
   source->AddBoolean(
       "isSkillsWebViewV2Enabled",
       base::FeatureList::IsEnabled(features::kSkillsWebViewV2Enabled));
+
+  std::string application_locale = g_browser_process->GetApplicationLocale();
+  std::string google_locale = google_util::GetGoogleLocale(application_locale);
+  source->AddString("languageCode", google_locale);
 
   auto* command_line = base::CommandLine::ForCurrentProcess();
   source->AddBoolean("devMode", command_line->HasSwitch("skills-dev"));
