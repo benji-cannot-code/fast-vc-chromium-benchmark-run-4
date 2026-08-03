f368260d5ea135e042a03d50bb3e9ce25babaced
Danilo Tedeschi <danft@google.com>
2026-08-03 16:03:01 -0700

    Remove GetAvailableICULocales from l10n_util
    The function has been migrated to //base/i18n
    
    Change-Id: I8bba1b23974ecaa2b8b3952ea30bff697186c16f
    Bug: 526738932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8171103
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672953}

--- ui/base/l10n/l10n_util_unittest.cc
+++ ui/base/l10n/l10n_util_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/environment.h"
 #include "base/files/file_util.h"
 #include "base/i18n/case_conversion.h"
+#include "base/i18n/icubridge/supported_locales.h"
 #include "base/i18n/language_tag.h"
 #include "base/i18n/rtl.h"
 #include "base/i18n/time_formatting.h"
@@ -666,23 +667,6 @@ TEST_F(L10nUtilTest, GetParentLocales) {
               ElementsAre("sr_Cyrl_RS", "sr_Cyrl", "sr"));
 }
 
-TEST_F(L10nUtilTest, TimeDurationFormatAllLocales) {
-  base::test::ScopedRestoreICUDefaultLocale restore_locale;
-
-  // Verify that base::TimeDurationFormat() works for all available locales:
-  // http://crbug.com/707515
-  base::TimeDelta kDelta = base::Minutes(15 * 60 + 42);
-  for (const std::string& locale : l10n_util::GetAvailableICULocales()) {
-    base::i18n::SetICUDefaultLocale(locale);
-    std::u16string str;
-    const bool result =
-        base::TimeDurationFormat(kDelta, base::DURATION_WIDTH_NUMERIC, &str);
-    EXPECT_TRUE(result) << "Failed to format duration for " << locale;
-    if (result)
-      EXPECT_FALSE(str.empty()) << "Got empty string for " << locale;
-  }
-}
-
 TEST_F(L10nUtilTest, GetUserFacingUILocaleList) {
   // Convert the vector to a set for easy lookup.
   const base::flat_set<std::string> locales =
@@ -714,7 +698,7 @@ TEST_F(L10nUtilTest, GetUserFacingUILocaleList) {
   // our list of Accept-Language locales.
   EXPECT_FALSE(locales.contains("en-DE"));
   // Esperanto. Unlikely to be localised and historically included in
-  // GetAvailableICULocales.
+  // GetSupportedIcuLocales.
   EXPECT_FALSE(locales.contains("eo"));
 }
 
