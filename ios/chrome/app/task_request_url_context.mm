c33a32d819b5eb6d59610ee14c9d05c933467672
Federica Germinario <fedegermi@google.com>
2026-08-03 06:17:04 -0700

    [ios] Create subclass TaskRequestForXCallbackURLContext
    Create TaskRequestForXCallbackURLContext subclass to improve code
    readability inside class TaskRequestForURLContext.
    
    Bug: 493816082
    Change-Id: I8962839d7ca5ed04f70703ce628d0264785e80ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8183488
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672638}

--- ios/chrome/app/task_request_url_context.mm
+++ ios/chrome/app/task_request_url_context.mm
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/system_identity_manager.h"
 #import "ios/chrome/common/app_group/app_group_constants.h"
 #import "ios/chrome/common/app_group/widget_constants.h"
+#import "ios/chrome/common/x_callback_url.h"
 #import "net/base/apple/url_conversions.h"
 
 namespace {
@@ -223,6 +224,10 @@ void RecordRuntimeMetrics(UIOpenURLContext* url_context, bool is_first_run) {
 
 @end
 
+// Subclass handling X-Callback URLs (googlechrome://x-callback-url/).
+@interface TaskRequestForXCallbackURLContext : TaskRequestForURLContext
+@end
+
 @implementation TaskRequestForURLContext
 
 + (instancetype)taskRequestWithURLContext:(UIOpenURLContext*)URLContext
@@ -234,6 +239,12 @@ void RecordRuntimeMetrics(UIOpenURLContext* url_context, bool is_first_run) {
         [[TaskRequestForWidgetURLContext alloc] initWithURLContext:URLContext
                                                         sceneState:sceneState
                                                        isColdStart:isColdStart];
+  }
+  if (IsXCallbackURL(net::GURLWithNSURL(url))) {
+    return [[TaskRequestForXCallbackURLContext alloc]
+        initWithURLContext:URLContext
+                sceneState:sceneState
+               isColdStart:isColdStart];
   }
   return [[TaskRequestForURLContext alloc] initWithURLContext:URLContext
                                                    sceneState:sceneState
@@ -358,6 +369,36 @@ void RecordRuntimeMetrics(UIOpenURLContext* url_context, bool is_first_run) {
       base::RecordAction(base::UserMetricsAction(
           "MobileSearchPasswordsWidgetOpenPasswordManager"));
     }
+  }
+}
+
+@end
+
+#pragma mark - TaskRequestForXCallbackURLContext
+
+@implementation TaskRequestForXCallbackURLContext
+
+- (void)recordStartupMetrics {
+  [super recordStartupMetrics];
+
+  base::UmaHistogramEnumeration(kAppLaunchSource, AppLaunchSource::X_CALLBACK);
+
+  NSString* action = [self.URLContext.URL path];
+  if ([action isEqualToString:
+                  [NSString stringWithFormat:
+                                @"/%s",
+                                app_group::kChromeAppGroupXCallbackCommand]]) {
+    UMA_HISTOGRAM_ENUMERATION(kUMAMobileSessionStartActionHistogram,
+                              START_ACTION_XCALLBACK_APPGROUP_COMMAND,
+                              MOBILE_SESSION_START_ACTION_COUNT);
+  } else if ([action isEqualToString:@"/open"]) {
+    UMA_HISTOGRAM_ENUMERATION(kUMAMobileSessionStartActionHistogram,
+                              START_ACTION_XCALLBACK_OPEN,
+                              MOBILE_SESSION_START_ACTION_COUNT);
+  } else {
+    UMA_HISTOGRAM_ENUMERATION(kUMAMobileSessionStartActionHistogram,
+                              START_ACTION_XCALLBACK_OTHER,
+                              MOBILE_SESSION_START_ACTION_COUNT);
   }
 }
 
