5ded97306371f286f38e4f926dc4c2812beca839
Rahim Mammadli <rahimis@google.com>
2026-08-03 03:38:37 -0700

    Add pixel test coverage for kAtMemorySourceAttribution popup row view
    This CL extends `popup_row_factory_utils_browsertest.cc` to add pixel
    test coverage for `SuggestionType::kAtMemorySourceAttribution`.
    
    Bug: 538019057
    Change-Id: I954bf245a9ad77f4f6b310ddf89f40946a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8173768
    Commit-Queue: Rahim Mammadli <rahimis@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672572}

--- components/autofill/core/browser/at_memory/at_memory_manager.h
+++ components/autofill/core/browser/at_memory/at_memory_manager.h
@@ -115,6 +115,9 @@ class AtMemoryManager {
   void MaybeAppendPersonalContextNotice(
       std::vector<Suggestion>& suggestions) const;
 
+  // Creates a source attribution suggestion ("Suggested by Gemini").
+  static Suggestion CreateSourceAttributionSuggestion();
+
  private:
   friend class AtMemoryManagerTestApi;
 
