046c24901e6809ae14ccf1e5fa7d37798bc6e22b
Stanislav Mikheyev <mikhe@google.com>
2026-08-03 08:26:01 -0700

    YourSavedInfo: Rename allowEditing_ property to allowNewEntitiesAddition_
    Rename `allowEditing_` to `allowNewEntitiesAddition_` to prevent
    confusion with `allowEditingPref`. The property controls if users can
    add new entities. Users can always edit existing entities.
    
    TAG=agy
    CONV=94ea7e51-1f18-410a-8a70-e3be0b7d27ac
    
    Bug: 457730714
    Change-Id: I8ff0489c15ecacee058d2304534a999243f79cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8179260
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672683}

--- chrome/browser/resources/settings/autofill_page/autofill_ai_entries_list.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_ai_entries_list.ts
@@ -85,7 +85,7 @@ export class SettingsAutofillAiEntriesListElement extends
          If a user is not eligible for Autofill with Ai and they also have no
          data saved, then they cannot access this page at all.
        */
-      ineligibleUser: {
+      ineligibleUser_: {
         type: Boolean,
         value() {
           return !loadTimeData.getBoolean('userEligibleForAutofillAi');
@@ -144,17 +144,17 @@ export class SettingsAutofillAiEntriesListElement extends
           * Even if preference is true the user may still be prevented from
             adding entries due to other eligibility checks.
           * We assume that the provided preference is controlled by the address
-            autofill policy and extension API. If allowEditingPref is provided
+            autofill policy and extension API. If allowNewEntitiesAdditionPref is provided
             its value will be overridden by the address autofill preference when
             it is enforced.
       */
-      allowEditingPref: {
+      allowNewEntitiesAdditionPref: {
         type: Object,
         value: null,
       },
 
-      allowEditing_: {
-        type: Object,
+      allowNewEntitiesAddition_: {
+        type: Boolean,
         value: false,
       },
 
@@ -224,17 +224,17 @@ export class SettingsAutofillAiEntriesListElement extends
           'prefs.autofill.autofill_ai.opt_in_status.value, ' +
           'prefs.autofill.profile_enabled.value, ' +
           `prefs.${AiEnterpriseFeaturePrefName.AUTOFILL_AI}.value, ` +
-          'allowEditingPref.*)',
+          'allowNewEntitiesAdditionPref.*)',
     ];
   }
 
-  declare ineligibleUser: boolean;
+  declare private ineligibleUser_: boolean;
   declare allowedEntityTypes: Set<EntityTypeName>|null;
   declare listTitle: string;
   declare pageName: string;
   declare metricEntityTypes: Record<EntityTypeName, string>|null;
-  declare allowEditingPref: chrome.settingsPrivate.PrefObject<boolean>|null;
-  declare private allowEditing_: boolean;
+  declare allowNewEntitiesAdditionPref: chrome.settingsPrivate.PrefObject<boolean>|null;
+  declare private allowNewEntitiesAddition_: boolean;
   declare private completeEntityTypesList_: EntityType[];
   declare private activeEntityInstance_: EntityInstance|null;
   declare private showAddOrEditEntityInstanceDialog_: boolean;
@@ -276,7 +276,7 @@ export class SettingsAutofillAiEntriesListElement extends
 
     this.entityDataManager_.getWritableEntityTypes().then(
         (entityTypes: EntityType[]) => {
-          this.updateEntittyTypesList_(entityTypes);
+          this.updateEntityTypesList_(entityTypes);
         });
 
     this.addWebUiListener(
@@ -292,7 +292,7 @@ export class SettingsAutofillAiEntriesListElement extends
     this.entityInstancesChangedListener_ = null;
   }
 
-  private updateEntittyTypesList_(entityTypes: EntityType[]) {
+  private updateEntityTypesList_(entityTypes: EntityType[]) {
     // Filter only if the filter was set
     const filteredEntities = this.allowedEntityTypes ?
         entityTypes.filter(
@@ -481,14 +481,14 @@ export class SettingsAutofillAiEntriesListElement extends
     // controls server model calls and MQLS logging. Therefore not whether the
     // user can use Autofill AI.
     if (this.autofillAiAvailableByDefault_) {
-      this.allowEditing_ = this.isEditingAllowedByPref_ &&
+      this.allowNewEntitiesAddition_ = this.isNewEntitiesAdditionAllowedByPref_ &&
           this.canEnableOrDisableAutofillAi_ && meetsAddressPrefRequirement &&
           meetsAiPrefRequirement;
       return;
     }
     const optedIn = await this.entityDataManager_.getOptInStatus();
-    this.allowEditing_ = !this.ineligibleUser && optedIn &&
-        this.isEditingAllowedByPref_ && meetsAddressPrefRequirement &&
+    this.allowNewEntitiesAddition_ = !this.ineligibleUser_ && optedIn &&
+        this.isNewEntitiesAdditionAllowedByPref_ && meetsAddressPrefRequirement &&
         meetsAiPrefRequirement;
   }
 
@@ -500,12 +500,12 @@ export class SettingsAutofillAiEntriesListElement extends
   private onSyncStatusChanged_(_: SyncStatus) {
     this.entityDataManager_.getWritableEntityTypes().then(
         (entityTypes: EntityType[]) => {
-          this.updateEntittyTypesList_(entityTypes);
+          this.updateEntityTypesList_(entityTypes);
         });
   }
 
-  private get isEditingAllowedByPref_(): boolean {
-    return this.allowEditingPref?.value ?? true;
+  private get isNewEntitiesAdditionAllowedByPref_(): boolean {
+    return this.allowNewEntitiesAdditionPref?.value ?? true;
   }
 
   private typeNameToIconName_(name: EntityTypeName): string|undefined {
--- chrome/test/data/webui/settings/autofill_ai_entries_list_test.ts
+++ chrome/test/data/webui/settings/autofill_ai_entries_list_test.ts
@@ -254,9 +254,9 @@ suite('AutofillAiEntriesListUiReflectsEligibilityStatus', function() {
         assertFalse(addButton.disabled);
       });
 
-  test('AddButtonEnabledByDefaultWhenAllowEditingPrefUnset', async function() {
+  test('AddButtonEnabledByDefaultWhenAllowNewEntitiesAdditionPrefUnset', async function() {
     const entriesList = await createEntriesList();
-    entriesList.allowEditingPref = null; // Explicitly unset
+    entriesList.allowNewEntitiesAdditionPref = null; // Explicitly unset
     updateOptInStatus(true, entriesList);
     await flushTasks();
 
@@ -266,9 +266,9 @@ suite('AutofillAiEntriesListUiReflectsEligibilityStatus', function() {
     assertFalse(addButton.disabled);
   });
 
-  test('DisableAddButtotBasedOnAllowEditingPrefValue', async function() {
+  test('DisableAddButtonBasedOnAllowNewEntitiesAdditionPrefValue', async function() {
     const entriesList = await createEntriesList();
-    entriesList.allowEditingPref = {
+    entriesList.allowNewEntitiesAdditionPref = {
       key: '',
       type: chrome.settingsPrivate.PrefType.BOOLEAN,
       value: true,
@@ -281,15 +281,15 @@ suite('AutofillAiEntriesListUiReflectsEligibilityStatus', function() {
     assertTrue(!!addButton);
     assertFalse(addButton.disabled);
 
-    entriesList.set('allowEditingPref.value', false);
+    entriesList.set('allowNewEntitiesAdditionPref.value', false);
     await flushTasks();
 
     assertTrue(addButton.disabled);
   });
 
-  test('DisableAddButtotWhenAddressAutofillDisabled', async function() {
+  test('DisableAddButtonWhenAddressAutofillDisabled', async function() {
     const entriesList = await createEntriesList();
-    entriesList.allowEditingPref = {
+    entriesList.allowNewEntitiesAdditionPref = {
       key: '',
       type: chrome.settingsPrivate.PrefType.BOOLEAN,
       value: true,
@@ -311,7 +311,7 @@ suite('AutofillAiEntriesListUiReflectsEligibilityStatus', function() {
 
   test('DisableAddButtonWhenAiPredictionsDisabled', async function() {
     const entriesList = await createEntriesList();
-    entriesList.allowEditingPref = {
+    entriesList.allowNewEntitiesAdditionPref = {
       key: '',
       type: chrome.settingsPrivate.PrefType.BOOLEAN,
       value: true,
@@ -336,10 +336,10 @@ suite('AutofillAiEntriesListUiReflectsEligibilityStatus', function() {
   });
 
   test(
-      'AddressAutofillForcedTrueValueShouldNotOverrideAllowEditingPrefValue',
+      'AddressAutofillForcedTrueValueShouldNotOverrideAllowNewEntitiesAdditionPrefValue',
       async function() {
         const entriesList = await createEntriesList();
-        entriesList.allowEditingPref = {
+        entriesList.allowNewEntitiesAdditionPref = {
           key: '',
           type: chrome.settingsPrivate.PrefType.BOOLEAN,
           value: false,  // Editing is disabled
