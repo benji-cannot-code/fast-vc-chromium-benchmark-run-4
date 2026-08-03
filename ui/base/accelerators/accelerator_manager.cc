43beb4279d44d30627cfca94165f70b5093a0ee2
Bryant Chandler <bryantchandler@google.com>
2026-08-03 10:31:19 -0700

    [ui] Gracefully handle unregistering missing accelerators
    On Android, Activity recreation and panel migrations can cause hotkeys
    to be unregistered from an AcceleratorManager after their host window or
    activity has already been destroyed, or on a new AcceleratorManager
    where they were never registered.
    
    The DCHECK here seemed a little overly aggressive, since the checked
    cases definitely don't have the hotkey registered. From the perspective
    of the outside user, the goal of having the accelerator unregistered has
    been met.
    
    This CL updates ui::AcceleratorManager::Unregister to gracefully log a
    DLOG(WARNING) and return early if the accelerator is not registered or
    belongs to a different target.
    
    Change-Id: I81ee894f7b56c7b06d0dd1dad3a4d2506a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167780
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672755}

--- ui/base/accelerators/accelerator_manager.cc
+++ ui/base/accelerators/accelerator_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <ostream>
 
 #include "base/check.h"
+#include "base/logging.h"
 
 namespace ui {
 
@@ -32,10 +33,16 @@ void AcceleratorManager::Unregister(const Accelerator& accelerator,
                                     AcceleratorTarget* target) {
   DCHECK(target);
   AcceleratorTargetInfo* target_info = accelerators_.Find(accelerator);
-  DCHECK(target_info) << "Unregistering non-existing accelerator";
+  if (!target_info) {
+    DLOG(WARNING) << "Unregistering non-existing accelerator";
+    return;
+  }
 
   const bool was_registered = target_info->Unregister(target);
-  DCHECK(was_registered) << "Unregistering accelerator for wrong target";
+  if (!was_registered) {
+    DLOG(WARNING) << "Unregistering accelerator for wrong target";
+    return;
+  }
 
   // If the last target for the accelerator is removed, then erase the
   // entry from the map.
