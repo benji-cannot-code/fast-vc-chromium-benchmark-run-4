9a7cb61057f895d60eb6c3ad29a36f9582c72d09
Mei Liang <meiliang@chromium.org>
2026-08-04 15:09:40 -0700

    Refactor GlicExperimentalOptInController to Host-Delegation pattern (Part 1/2)
    This is a mechanical refactoring and involves no behavioral changes.
    
    Abstract the platform-specific UI presentation into a new GlicExperimentalOptInUIHost interface.
    - Introduce GlicExperimentalOptInUIHost interface and Delegate sub-interface.
    - Create GlicExperimentalOptInUIHostDesktop encapsulating Views dialog presentation, tab visibility observers, and duration UMA metrics.
    - Create GlicExperimentalOptInUIHostAndroid skeleton implementation.
    - Decouple GlicExperimentalOptInController from Views widget and delegate UI operations to GlicExperimentalOptInUIHost.
    - Enable GlicExperimentalOptInController in GlicKeyedService cross-platform.
    
    one-pager: https://docs.google.com/document/d/1U_Q4gxQz65ykb1mOTLAo00jsk_yIAoIQF7QCaVuzyb8/edit?resourcekey=0-SYFHQyvGyiuRxUPecjcvLA&tab=t.0
    
    No behavior change: https://screencast.googleplex.com/cast/NTg5NzM3MDA1ODM1ODc4NHxkMDkzMTY1Zi0wMw
    
    BYPASS_LARGE_CHANGE_WARNING: refactoring
    BUG=540987436
    
    Change-Id: I32c4b855b2e2f58094915cec7150e4d740fa7fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177816
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673711}

--- chrome/browser/glic/public/glic_keyed_service.h
+++ chrome/browser/glic/public/glic_keyed_service.h
@@ -59,10 +59,7 @@ class GlicShareImageHandler;
 class GlicTabDataObserver;
 class GlicTabFaviconObserver;
 class GlicInstanceCoordinator;
-
-#if !BUILDFLAG(IS_ANDROID)
 class GlicExperimentalOptInController;
-#endif
 
 enum class GlicPrewarmingChecksResult;
 
@@ -139,9 +136,9 @@ class GlicKeyedService : public KeyedService, public base::SupportsUserData {
   GlicEnabling& enabling() { return *enabling_.get(); }
 
   GlicMetrics* metrics() { return metrics_.get(); }
-#if !BUILDFLAG(IS_ANDROID)
+
   virtual GlicExperimentalOptInController& opt_in_controller();
-#endif
+
   virtual GlicInstanceCoordinator& instance_coordinator() const;
 
   // Return a `GlicActiveInstanceSharingManager` which tracks the sharing state
@@ -281,9 +278,7 @@ class GlicKeyedService : public KeyedService, public base::SupportsUserData {
 
   std::unique_ptr<GlicEnabling> enabling_;
   std::unique_ptr<GlicMetrics> metrics_;
-#if !BUILDFLAG(IS_ANDROID)
   std::unique_ptr<GlicExperimentalOptInController> opt_in_controller_;
-#endif
   // Is a GlicInstanceCoordinatorImpl.
   std::unique_ptr<GlicInstanceCoordinator> instance_coordinator_;
   std::unique_ptr<GlicShareImageHandler> share_image_handler_;
