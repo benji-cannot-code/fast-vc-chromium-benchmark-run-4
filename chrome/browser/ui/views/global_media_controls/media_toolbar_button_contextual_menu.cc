990cac6381ff74d60999104091b24fd1c1028058
Wenbin Zhang <wenbinzhang@google.com>
2026-08-03 23:52:31 -0700

    Revert "[bedrock] Decouple MediaToolbarButtonContextualMenu from Browser"
    This reverts commit f22248106e5d96ffa60cddc76c688d2658bf5714.
    
    Reason for revert: Internal builder started to fail due to a missing
    #include. The forward fix work in this CL:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/8189518
    
    I'll revert this to mitigate the failures here if forward fix cannot
    land in time.
    
    Failure Link:
    https://ci.chromium.org/ui/p/chrome/builders/try/linux-chrome-dbg-cog-cache-warmer/77845/overview
    
    Original change's description:
    > [bedrock] Decouple MediaToolbarButtonContextualMenu from Browser
    >
    > MediaToolbarButtonContextualMenu only requires Profile* to access
    > preferences, feedback checks, and notification services.
    >
    > This CL updates MediaToolbarButtonContextualMenu to accept Profile*
    > instead of Browser*, updates ToolbarView accordingly, and converts
    > MediaToolbarButtonContextualMenuTest to a unit test using
    > TestingProfile and BrowserTaskEnvironment, removing
    > BrowserWithTestWindowTest and Browser* dependencies.
    >
    > BUG=none
    >
    > Change-Id: I706e15ef3a9f2767b04c46cebf68700ddc484410
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8175808
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1673111}
    
    Bug: none
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iafa824b6e618f68e628446171b226753c971ce29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8191955
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Owners-Override: Ming-Ying Chung <mych@chromium.org>
    Auto-Submit: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673142}

--- chrome/browser/ui/views/global_media_controls/media_toolbar_button_contextual_menu.cc
+++ chrome/browser/ui/views/global_media_controls/media_toolbar_button_contextual_menu.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/feedback/show_feedback_page.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/global_media_controls/media_notification_service.h"
 #include "chrome/browser/ui/global_media_controls/media_notification_service_factory.h"
 #include "chrome/browser/ui/singleton_tabs.h"
@@ -20,16 +21,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 
 namespace {
-global_media_controls::MediaItemManager* GetItemManagerFromProfile(
-    Profile* profile) {
-  auto* service = MediaNotificationServiceFactory::GetForProfile(profile);
-  return service ? service->media_item_manager() : nullptr;
+global_media_controls::MediaItemManager* GetItemManagerFromBrowser(
+    Browser* browser) {
+  return MediaNotificationServiceFactory::GetForProfile(browser->GetProfile())
+      ->media_item_manager();
 }
 }  // namespace
 
 MediaToolbarButtonContextualMenu::MediaToolbarButtonContextualMenu(
-    Profile* profile)
-    : profile_(profile) {}
+    Browser* browser)
+    : browser_(browser) {}
 
 MediaToolbarButtonContextualMenu::~MediaToolbarButtonContextualMenu() = default;
 
@@ -41,7 +42,7 @@ MediaToolbarButtonContextualMenu::CreateMenuModel() {
       IDS_MEDIA_TOOLBAR_CONTEXT_SHOW_OTHER_SESSIONS);
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  if (chrome::CanShowFeedback(profile_)) {
+  if (chrome::CanShowFeedback(browser_->GetProfile())) {
     menu_model->AddItemWithStringId(
         IDC_MEDIA_TOOLBAR_CONTEXT_REPORT_CAST_ISSUE,
         IDS_MEDIA_TOOLBAR_CONTEXT_REPORT_CAST_ISSUE);
@@ -52,7 +53,7 @@ MediaToolbarButtonContextualMenu::CreateMenuModel() {
 
 bool MediaToolbarButtonContextualMenu::IsCommandIdChecked(
     int command_id) const {
-  PrefService* pref_service = profile_->GetPrefs();
+  PrefService* pref_service = browser_->GetProfile()->GetPrefs();
   switch (command_id) {
     case IDC_MEDIA_TOOLBAR_CONTEXT_SHOW_OTHER_SESSIONS:
       return pref_service->GetBoolean(
@@ -65,7 +66,7 @@ bool MediaToolbarButtonContextualMenu::IsCommandIdChecked(
 
 bool MediaToolbarButtonContextualMenu::IsCommandIdEnabled(
     int command_id) const {
-  PrefService* pref_service = profile_->GetPrefs();
+  PrefService* pref_service = browser_->GetProfile()->GetPrefs();
   switch (command_id) {
     case IDC_MEDIA_TOOLBAR_CONTEXT_SHOW_OTHER_SESSIONS:
       // The pref may be managed by an enterprise policy and not modifiable by
@@ -95,17 +96,17 @@ void MediaToolbarButtonContextualMenu::ExecuteCommand(int command_id,
 }
 
 void MediaToolbarButtonContextualMenu::MenuClosed(ui::SimpleMenuModel* source) {
-  if (!profile_) {
+  if (!browser_) {
     return;
   }
-  auto* item_manager = GetItemManagerFromProfile(profile_);
+  auto* item_manager = GetItemManagerFromBrowser(browser_);
   if (item_manager) {
     item_manager->OnItemsChanged();
   }
 }
 
 void MediaToolbarButtonContextualMenu::ToggleShowOtherSessions() {
-  PrefService* pref_service = profile_->GetPrefs();
+  PrefService* pref_service = browser_->GetProfile()->GetPrefs();
   pref_service->SetBoolean(
       media_router::prefs::kMediaRouterShowCastSessionsStartedByOtherDevices,
       !pref_service->GetBoolean(
@@ -116,7 +117,7 @@ void MediaToolbarButtonContextualMenu::ToggleShowOtherSessions() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 void MediaToolbarButtonContextualMenu::ReportIssue() {
   ShowSingletonTab(
-      profile_,
+      browser_,
       GURL(base::StrCat({"chrome://", chrome::kChromeUICastFeedbackHost})));
 }
 #endif
