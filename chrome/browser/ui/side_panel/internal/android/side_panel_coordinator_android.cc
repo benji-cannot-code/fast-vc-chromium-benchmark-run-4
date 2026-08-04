f36b203ca1429e5b029c5fcf1cf783d61cbaa512
Mark Schillaci <mschillaci@google.com>
2026-08-04 11:56:06 -0700

    [Side Panel Infra] Add metric to record onWillAutoClose / Restore
    This CL adds an additional metric to the Android SidePanel infra code
    to record when the SidePanel has been automatically closed or restored
    based on window width. This concept of the feature is unique to Android,
    so we keep all code within the Android class and add a category for
    SidePanel metrics with a ".Android." portion.
    
    Bug: 541373776
    Change-Id: I50e1c0d5a678c30476940f4a77afceab71cfbabc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177248
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Linyu He <linyuh@google.com>
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673558}

--- chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.cc
+++ chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/tab_list/tab_list_interface.h"
@@ -45,6 +47,20 @@ const gfx::Rect kNoBounds(kInvalidCoordinate,
                           kInvalidCoordinate,
                           kInvalidCoordinate,
                           kInvalidCoordinate);
+constexpr char kAndroidSidePanelHistogramPrefix[] = "SidePanel.Android";
+
+void RecordAutoCloseOrRestoreMetric(SidePanelEntry* entry, bool is_auto_close) {
+  if (!entry) {
+    return;
+  }
+  std::string_view entry_name =
+      SidePanelEntryIdToHistogramName(entry->key().id());
+  std::string_view action =
+      is_auto_close ? ".OnWillAutoClose" : ".OnWillAutoRestore";
+  base::UmaHistogramBoolean(
+      base::StrCat({kAndroidSidePanelHistogramPrefix, ".", entry_name, action}),
+      true);
+}
 }  // namespace
 
 using jni_zero::AttachCurrentThread;
@@ -311,6 +327,9 @@ void SidePanelCoordinatorAndroid::OnWillAutoClose() {
   has_insufficient_space_ = true;
 
   if (IsSidePanelShowing() && state_ != SidePanelState::kClosing) {
+    RecordAutoCloseOrRestoreMetric(GetEntryForCurrentKeyNonNull(),
+                                   /*is_auto_close=*/true);
+
     deferred_entry_tracker_.AddActiveEntries();
 
     // TODO(crbug.com/527985639): Rename `kWindowResized` as
@@ -345,6 +364,8 @@ void SidePanelCoordinatorAndroid::OnWillAutoRestore() {
           active_tab->GetHandle());
 
   if (key_to_show) {
+    RecordAutoCloseOrRestoreMetric(GetEntryForUniqueKey(*key_to_show),
+                                   /*is_auto_close=*/false);
     Show(*key_to_show, SidePanelOpenTrigger::kWindowResized,
          /*suppress_animations=*/true);
   }
