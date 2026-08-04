9ed83930a0dacb006cf6bbfb9c9d783330a35ebd
Jakob Koblinsky <jkoblinsky@google.com>
2026-08-04 12:24:24 -0700

    [StatusView] Use new addSyncObserverAndCall in StatusMediator
    Use addSyncObserverAndCall when observing PreviewMatchUrlSupplier in
    StatusMediator so that observers are properly invoked even when the
    initial value is null (e.g., clearing the exact match favicon/globe on a
    new session).
    
    Bug: 542629072
    Change-Id: Ie1741837f12ebf86d81e41f7ab62e7ded3178e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8195705
    Commit-Queue: Jakob Koblinsky <jkoblinsky@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673587}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
@@ -405,10 +405,7 @@ public class StatusMediator
             mInputSessionState
                     .getAutocompleteInput()
                     .getPreviewMatchUrlSupplier()
-                    .addSyncObserver(mOnPreviewMatchUrlChanged);
-
-            onPreviewMatchUrlChanged(
-                    mInputSessionState.getAutocompleteInput().getPreviewMatchUrl());
+                    .addSyncObserverAndCall(mOnPreviewMatchUrlChanged);
         }
     }
 
