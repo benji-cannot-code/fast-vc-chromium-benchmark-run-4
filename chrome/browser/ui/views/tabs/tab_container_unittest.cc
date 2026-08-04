4d7f4d2d0d7c826fa88865867807b7443984bdfe
Vince Lugli <lugli@google.com>
2026-08-03 17:47:30 -0700

    [Unified tabstrip] Foundation for tab_style_view unification
    Pull out functions from tab_style_views that can be added directly to
    tab.cc.
    
    Implements the delegates for tab and tab_view. Uses Tab's delegate by
    default, which just has moved code.
    
    TabView's delegate is unused in this change.
    
    Change-Id: Ic4f2a0ea78fc5aaa692c86047575a776988e86bb
    Bug: 523327868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8175206
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Vince Lugli <lugli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673011}

--- chrome/browser/ui/views/tabs/tab_container_unittest.cc
+++ chrome/browser/ui/views/tabs/tab_container_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/fake_tab_slot_controller.h"
 #include "chrome/browser/ui/views/tabs/shared/tab_strip_types.h"
+#include "chrome/browser/ui/views/tabs/tab/glow_hover_controller.h"
 #include "chrome/browser/ui/views/tabs/tab/tab_close_button.h"
 #include "chrome/browser/ui/views/tabs/tab_container_impl.h"
 #include "chrome/browser/ui/views/tabs/tab_group_header.h"
@@ -1357,7 +1358,7 @@ TEST_F(TabContainerTest, ZOrder_MixedScenario) {
   container_impl->CompleteAnimationAndLayout();
 
   // Hover over the grouped tab.
-  grouped_tab->tab_style_views()->ShowHover(TabStyle::ShowHoverStyle::kSubtle);
+  grouped_tab->ShowHover(TabStyle::ShowHoverStyle::kSubtle);
   grouped_tab->tab_style_views()
       ->GetHoverControllerForTesting()
       ->animation_for_testing()
@@ -1474,7 +1475,7 @@ TEST_F(TabContainerTest, ZOrder_HoveredTabIsAfterNormalTab) {
   container_impl->CompleteAnimationAndLayout();
 
   // Hover over the first tab.
-  tab1->tab_style_views()->ShowHover(TabStyle::ShowHoverStyle::kSubtle);
+  tab1->ShowHover(TabStyle::ShowHoverStyle::kSubtle);
   tab1->tab_style_views()
       ->GetHoverControllerForTesting()
       ->animation_for_testing()
