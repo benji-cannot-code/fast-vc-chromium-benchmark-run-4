c61ead0b1c6761be4b4f44b2c68941de2003ec52
Philip Pfaffe <pfaffe@chromium.org>
2026-08-04 03:38:36 -0700

    Remove kDevToolsAllowPopoverForcing flag
    The flag is obsolete, the feature has been enabled for a year without
    issues.
    
    Bug: 413580647
    Change-Id: I8bbd4ad14929ad999efc8d79f4755de080ccb255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8183947
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673223}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
@@ -2177,10 +2177,6 @@ protocol::Response InspectorDOMAgent::forceShowPopover(
     bool enable,
     std::optional<int> invoker_node_id,
     std::unique_ptr<protocol::Array<int>>* out_node_ids) {
-  if (!base::FeatureList::IsEnabled(features::kDevToolsAllowPopoverForcing)) {
-    return protocol::Response::ServerError("Feature is not enabled");
-  }
-
   Node* node = nullptr;
   protocol::Response response = AssertNode(node_id, node);
   if (!response.IsSuccess()) {
@@ -2216,8 +2212,7 @@ protocol::Response InspectorDOMAgent::forceShowPopover(
 
 void InspectorDOMAgent::WillHidePopover(HTMLElement* element,
                                         bool* force_open) {
-  if (base::FeatureList::IsEnabled(features::kDevToolsAllowPopoverForcing) &&
-      force_open && forced_popovers_.Contains(element)) {
+  if (force_open && forced_popovers_.Contains(element)) {
     *force_open = true;
   }
 }
