2681c85c8635feb66afc3077b73284e8f02b54a4
Kunal Daftari <kunaldaftari@google.com>
2026-08-04 11:52:27 -0700

    [Focus] Hide Tab Group Header Line in Focus Mode
    This change applies to all three tab strip implementations.
    
    Removes line / indentation if present.
    
    Test: TabGroupViewTest.GroupLineHiddenAndTabsAlignedInFocusMode
    Test: TabGroupViewsTest.UnderlineHiddenInFocusMode (Legacy)
    Bug: 540860867
    Change-Id: Ic28db916309ad4f8d754c800b2dc74cc629ed5a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8183091
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673552}

--- chrome/browser/ui/views/tabs/common/tab_group_view.cc
+++ chrome/browser/ui/views/tabs/common/tab_group_view.cc
@@ -263,7 +263,7 @@ void TabGroupView::UpdateChildVisibilityForCollapseState(bool collapsed) {
   if (!collection_node_) {
     return;
   }
-  group_line_->SetVisible(!collapsed);
+  group_line_->SetVisible(!collapsed && !IsGroupFocused());
   for (auto* child : collection_node_->GetDirectChildren()) {
     child->SetVisible(!collapsed);
   }
