4f2ed0b06d9a72ee244be813269ad3d8c5b7918d
Abe Boujane <boujane@google.com>
2026-08-04 13:54:33 -0700

    Clean up unused Topics functions in PrivacySandboxService and CanonicalTopic
    Remove 11 Topics-related public virtual methods from
    PrivacySandboxService and their implementations in
    PrivacySandboxServiceImpl that were used for past UX and no longer have
    call sites in production code. Also remove
    CanonicalTopic::GetLocalizedRepresentation() which became unreferenced
    after deleting GetBlockedTopics().
    
    Change-Id: Ie4f40ee51e4d12559858f19b03f021e58b8add95
    Bug: b:534866765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8189656
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673650}

--- components/privacy_sandbox/privacy_sandbox_test_util.h
+++ components/privacy_sandbox/privacy_sandbox_test_util.h
@@ -32,14 +32,6 @@ namespace privacy_sandbox_test_util {
 
 class PrivacySandboxServiceTestInterface {
  public:
-  virtual void TopicsToggleChanged(bool new_value) const = 0;
-  virtual void SetTopicAllowed(privacy_sandbox::CanonicalTopic topic,
-                               bool allowed) = 0;
-  virtual bool TopicsHasActiveConsent() const = 0;
-  virtual privacy_sandbox::TopicsConsentUpdateSource
-  TopicsConsentLastUpdateSource() const = 0;
-  virtual base::Time TopicsConsentLastUpdateTime() const = 0;
-  virtual std::string TopicsConsentLastUpdateText() const = 0;
   virtual void ForceChromeBuildForTests(bool force_chrome_build) const = 0;
 };
 
@@ -133,9 +125,7 @@ enum class StateKey {
   kIsIncognito = 7,
   kIsRestrictedAccount = 8,
   kHasCurrentTopics = 9,
-  kHasBlockedTopics = 10,
   kAdvanceClockBy = 11,
-  kActiveTopicsConsent = 12,
   kTrialsConsentDecisionMade = 14,
   kTrialsNoticeDisplayed = 15,
   kM1ConsentDecisionPreviouslyMade = 16,
@@ -159,7 +149,6 @@ enum class InputKey {
   kFledgeAuctionPartyOrigin = 3,
   kAdMeasurementReportingOrigin = 4,
   kAccessingOrigin = 7,
-  kTopicsToggleNewValue = 8,
   kForceChromeBuild = 9,
   // kPromptAction is Obsolete.
   // TODO(crbug.com/474716334): Remove this enum.
@@ -185,10 +174,6 @@ enum class OutputKey {
   kIsSharedStorageAllowedMetric = 14,
   kIsSharedStorageSelectURLAllowedMetric = 15,
   kIsPrivateAggregationAllowedMetric = 16,
-  kTopicsConsentGiven = 17,
-  kTopicsConsentLastUpdateReason = 18,
-  kTopicsConsentLastUpdateTime = 19,
-  kTopicsConsentStringIdentifiers = 20,
   // kPromptType and kM1PromptSuppressedReason are Obsolete.
   // TODO(crbug.com/474716334): Remove obsolete enums.
   kPromptType = 21,
