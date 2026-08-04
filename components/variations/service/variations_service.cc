dd97d8b0ba542ead8751039ea66e1477d8fe2744
Zaina Al-Mashni <zalmashni@google.com>
2026-08-04 10:59:16 -0700

    [GPC] Expose latest geo_level1 from VariationsService
    This change stores and exposes the latest geo level 1 (subcountry) code
    retrieved from the variations seed.
    
    It also adds a new command-line switch to allow overriding this value
    for testing purposes.
    
    Fixed: 538463686
    Change-Id: Ia1b4e644e7b5bdd1c3d94238b2e3a69541d2050c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137061
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673507}

--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -1416,6 +1416,10 @@ std::string VariationsService::GetLatestCountry() const {
   return field_trial_creator_.GetLatestCountry();
 }
 
+std::string VariationsService::GetLatestGeoLevel1() const {
+  return field_trial_creator_.GetLatestGeoLevel1();
+}
+
 bool VariationsService::SetUpFieldTrials(
     const std::vector<std::string>& variation_ids,
     const std::vector<base::FeatureList::FeatureOverrideInfo>& extra_overrides,
