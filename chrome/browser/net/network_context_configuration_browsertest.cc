1c2432a6e002d9e132ad8210ba574217a3a41a77
Ming-Ying Chung <mych@chromium.org>
2026-08-04 00:42:05 -0700

    [gardener] Disable flaky MaxConnectionsPerProxyForWebSocket test
    This has been flaky since https://ci.chromium.org/ui/test/chromium/%3A%2F%2Fchrome%2Ftest%5C%3Abrowser_tests!gtest%3A%3ANetworkContextConfigurationProxySettingsBrowserTest%23MaxConnectionsPerProxy%2FOnDiskAppWithIncognitoProfile.0?q=VHASH%3A3f8f3709591cee6a
    
    There has been an attempted to fix it https://crrev.com/c/8181967
    but it's still flaky.
    
    
    Bug: 542268672
    Change-Id: Ia436a7f9dcbbcc7ca44fb75807c1a294ff411dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8192335
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673160}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -2064,8 +2064,9 @@ IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationProxySettingsBrowserTest,
   RunMaxConnectionsPerProxyTest();
 }
 
+// TODO(crbug.com/542268672): Re-enable this test.
 IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationProxySettingsBrowserTest,
-                       MaxConnectionsPerProxyForWebSocket) {
+                       DISABLED_MaxConnectionsPerProxyForWebSocket) {
   RunMaxConnectionsPerProxyForWebSocketTest();
 }
 
