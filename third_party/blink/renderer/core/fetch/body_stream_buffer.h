48be3c94101ff6369e44fb68a69d830f84a460ff
Taylor Brandstetter <deadbeef@chromium.org>
2026-08-04 13:35:38 -0700

    Reland: Forwarding abort reason from AbortController.abort() to fetch() response
    Behind a flag ForwardReasonToFetchBodyAbort.
    
    The fetch promise itself was forwarding the abort reason. However the body methods and ReadableStream were not, as they're intended to.
    
    Relanding after fixing null pointer dereference from the loader client being garbage collected.
    
    Original CL: https://chromium-review.googlesource.com/c/chromium/src/+/8087935
    
    Bug: 502133195
    Change-Id: I3a605eb5c1eb04a5c3f4cf34291c0b83c9732385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8087935
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Taylor Brandstetter <deadbeef@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673636}

--- third_party/blink/renderer/core/fetch/body_stream_buffer.h
+++ third_party/blink/renderer/core/fetch/body_stream_buffer.h
@@ -145,6 +145,7 @@ class CORE_EXPORT BodyStreamBuffer final
 
   BytesConsumer* ReleaseHandle(ExceptionState&);
   void Abort();
+  void AbortLoader(FetchDataLoader::Client*);
   void Close(ExceptionState&);
   void GetError();
   void RaiseOOMError();
