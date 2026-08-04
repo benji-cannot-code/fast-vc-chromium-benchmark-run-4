51ff77861a6784a7947a3c5cc886f04a5d99ceff
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-08-04 00:41:28 -0700

    Revert "[DSBD] Prevent session restore when opening the Learn More popup"
    This reverts commit ec0de7e91d3a8acdb2b24fe072122ec2cad481ad.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8674417190381007297
    
    Sample failed build: https://ci.chromium.org/b/8674417190381007297
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8183107&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8674417190381007297&type=BUG
    
    Original change's description:
    > [DSBD] Prevent session restore when opening the Learn More popup
    >
    > This CL ensures that opening the "Learn More" privacy article popup from
    > the enterprise signals disclaimer does not trigger session restore for
    > the profile. It configures the popup browser creation params to not
    > trigger session restore and to omit this browser window from session
    > restore.
    >
    > Additionally, the <a> used until this point for the link has been
    > replaced with <span> to avoid triggering link opening in a new browser
    > window.
    >
    > Bug: b:512836948
    > Change-Id: I0a48f21db1e9966a92f543954b2c45b5c7e08d8e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8183107
    > Reviewed-by: Ryan Sultanem <rsult@google.com>
    > Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    > Reviewed-by: Yann Dago <ydago@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1673150}
    >
    
    Bug: b:512836948
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib15f54ca03d03796ffd79f554c445306b29d29ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8193476
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1673159}

--- chrome/browser/resources/signin/managed_user_profile_notice/signals_disclaimer.ts
+++ chrome/browser/resources/signin/managed_user_profile_notice/signals_disclaimer.ts
@@ -52,13 +52,6 @@ export class SignalsDisclaimerElement extends SignalsDisclaimerElementBase {
   protected onLearnMoreClick() {
     ManagedUserProfileNoticeBrowserProxyImpl.getInstance().learnMoreClicked();
   }
-
-  protected onLearnMoreKeydown(e: KeyboardEvent) {
-    if (e.key === 'Enter' || e.key === ' ') {
-      e.preventDefault();
-      this.onLearnMoreClick();
-    }
-  }
 }
 
 declare global {
