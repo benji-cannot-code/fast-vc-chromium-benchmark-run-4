5cf61014079fea8f4bccdd8dfd1d51669f41c836
Danilo Tedeschi <danft@google.com>
2026-08-04 09:12:20 -0700

    Do not crash in TitledUrlIndex::Normalize
    Avoid crash when icu::Normalizer2::getInstance fails.
    
    Change-Id: I39b8f5f505fbd98a8874e9f714d7cbb673e3a9dd
    Bug: 40064988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8184456
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Manish Goregaokar <manishearth@google.com>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673380}

--- components/bookmarks/browser/titled_url_index.cc
+++ components/bookmarks/browser/titled_url_index.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <iterator>
+#include <string>
 #include <utility>
 
 #include "base/i18n/case_conversion.h"
@@ -128,8 +129,8 @@ std::u16string TitledUrlIndex::Normalize(std::u16string_view text) {
   const icu::Normalizer2* normalizer2 =
       icu::Normalizer2::getInstance(nullptr, "nfkc", UNORM2_COMPOSE, status);
   if (U_FAILURE(status)) {
-    // Log and crash right away to capture the error code in the crash report.
-    LOG(FATAL) << "failed to create a normalizer: " << u_errorName(status);
+    LOG(ERROR) << "failed to create a normalizer: " << u_errorName(status);
+    return std::u16string(text);
   }
   icu::UnicodeString unicode_text(text.data(),
                                   static_cast<int32_t>(text.length()));
