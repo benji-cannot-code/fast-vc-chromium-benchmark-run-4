436f6b6e47107a5568fc00f523400cbad72f862e
Bruno Braga <brunobraga@google.com>
2026-08-04 09:38:23 -0700

    Autofill AI: Unmask sensitive entities after device authentication
    This CL completes the authentication workflow on the "AutofillAI
    entities" tab in chrome://autofill-internals by unmasking/unredacting
    sensitive entity attributes upon successful device reauthentication.
    
    BUG=539818688
    
    Change-Id: I103412af015bbee953dfafdd7053528a5b1b6805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8064269
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673410}

--- chrome/browser/ui/webui/autofill_and_password_manager_internals/internals_ui_handler.h
+++ chrome/browser/ui/webui/autofill_and_password_manager_internals/internals_ui_handler.h
@@ -111,7 +111,7 @@ class InternalsUIHandler
       const AutofillAiPersonalContextAccessManager& manager,
       std::optional<base::span<const EntityInstance>> entities) override;
 
-  void SendAutofillAiEntitiesToWebUI();
+  void SendAutofillAiEntitiesToWebUI(bool is_user_reauthenticated = false);
   void FetchNextPersonalContextType();
 
   // JavaScript call handler.
