f94060c4f0712367f3dc5bd2d30c3f01d34ee313
Hao Dong <spdonghao@google.com>
2026-08-04 14:32:44 -0700

    Reland "[Vertical Tabs] Fix tab loading blocking tab strip transitions."
    This reverts commit d7490ba7f8a46a306041dce31823125436112e1e.
    
    Reason for revert: Fix the NPE
    
    Original change's description:
    > Revert "[Vertical Tabs] Fix tab loading blocking tab strip transitions."
    >
    > This reverts commit 91a531fe5822558c4fdef2d7f3e1fc5c31feb13d.
    >
    > Reason for revert: This cl has caused multiple test suite failures, including 'ActorNotificationClickIntegrationTest', 'MultiWindowAppMenuTest','TabbedAppMenuTest', 'TabbedModeTabModelStoreTest', and a lot more.
    >
    > Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-17-x64-rel-emu-tests/2073/overview
    >
    > Original change's description:
    > > [Vertical Tabs] Fix tab loading blocking tab strip transitions.
    > >
    > > When toggling between Vertical Tabs and Horizontal Tabs while a tab is
    > > actively loading, the tab strip transition hangs until tab loading
    > > completes. This is because TabStripTopControlLayer relies on
    > > onBrowserControlsOffsetUpdate() to call handleTransitionFinished().
    > > However, when a tab is loading,
    > > BrowserControlsManager#onBrowserControlsOffsetChanged does not call
    > > onOffsetsChanged.
    > >
    > > This CL updates TabStripTopControlLayer.onTransitionRequested() to
    > > invoke handleTransitionStart() and handleTransitionFinished() after a
    > > delay (500ms) if it's isVerticalTabToggle, instead of waiting for
    > > onBrowserControlsOffsetChanged().
    > >
    > > Desktop Demo:
    > > Before: http://screencast/cast/NTA3NTkwMTYxOTM3MjAzMnxmYTEwZDM5MC05Zg
    > > After: http://screencast/cast/NTI5MzIxMjY0NTE5NTc3NnwxNTAzZWVjYy1kYw
    > >
    > > Tablet Demo:
    > > Before: http://screencast/cast/NDY0OTMzNDkwNDcxNzMxMnxhNDFiN2M3MS0yMw
    > > After: http://screencast/cast/NTMzNDMyMDMxNDA1NjcwNHxmMDI4NGMzZC1mMg
    > >
    > > Bug: 537036800
    > > Change-Id: I51c2bfe6ac1928dc3e04b91607b23b28d02a656b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160682
    > > Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    > > Commit-Queue: Hao Dong <spdonghao@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1670827}
    >
    > Bug: 537036800
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I756a1bda9286713376a0b952488e4ea70fae8d2f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8173603
    > Owners-Override: Fuhsin Liao <fuhsinliao@google.com>
    > Reviewed-by: Fuhsin Liao <fuhsinliao@google.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    > Auto-Submit: Fuhsin Liao <fuhsinliao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1670855}
    
    Bug: 537036800
    Change-Id: Ib46895a465c29bb71f83e7dc87442e82402e4f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8179923
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673677}

--- chrome/browser/ui/android/tabstrip/java/src/org/chromium/chrome/browser/tabstrip/TabStripTopControlLayer.java
+++ chrome/browser/ui/android/tabstrip/java/src/org/chromium/chrome/browser/tabstrip/TabStripTopControlLayer.java
@@ -41,9 +41,10 @@ public class TabStripTopControlLayer implements TopControlLayer, TabStripTransit
     private final ControlContainer mControlContainer;
     private final SettableNonNullObservableSupplier<Integer> mSupplier;
     private final @Nullable TokenHolder mLockTopControlsTokenJar;
-    private @Nullable Callback<Boolean> mTransitionFinishedCallback;
 
     private int mLockTopControlsToken = TokenHolder.INVALID_TOKEN;
+    private boolean mIsTabStripSuppressed;
+    private @Nullable Callback<Boolean> mTransitionFinishedCallback;
     private @Nullable BrowserControlsOffsetTagsInfo mOffsetTagsInfo;
 
     // Not null after #initializeWithNative.
@@ -58,6 +59,7 @@ public class TabStripTopControlLayer implements TopControlLayer, TabStripTransit
         public final int targetHeight;
         public final int topPadding;
         public final boolean applyScrimOverlay;
+        public final boolean isVerticalTabToggle;
         public final Runnable transitionStartedCallback;
         public final boolean hasAnimation;
         public final @TopControlVisibility int visibility;
@@ -69,11 +71,13 @@ public class TabStripTopControlLayer implements TopControlLayer, TabStripTransit
                 int targetHeight,
                 int topPadding,
                 boolean applyScrimOverlay,
+                boolean isVerticalTabToggle,
                 Runnable transitionStartedCallback) {
             this.startHeight = startHeight;
             this.targetHeight = targetHeight;
             this.topPadding = topPadding;
             this.applyScrimOverlay = applyScrimOverlay;
+            this.isVerticalTabToggle = isVerticalTabToggle;
             this.transitionStartedCallback = transitionStartedCallback;
 
             hasAnimation = calculateHasAnimation(startHeight, targetHeight, applyScrimOverlay);
@@ -244,15 +248,33 @@ public class TabStripTopControlLayer implements TopControlLayer, TabStripTransit
             int newHeight,
             int topPadding,
             boolean applyScrimOverlay,
+            boolean isTabStripSuppressed,
             Runnable transitionStartedCallback) {
         prepForTransitionRequested(
-                newHeight, topPadding, applyScrimOverlay, transitionStartedCallback);
+                newHeight,
+                topPadding,
+                applyScrimOverlay,
+                isTabStripSuppressed,
+                transitionStartedCallback);
         // TODO(crbug.com/41481630): Supplier can have an inconsistent value with
         //  mToolbar.getTabStripHeight().
         mSupplier.set(newHeight);
 
         if (isInTransition() && mTransitionState.targetHeight != mTransitionState.startHeight) {
             mTopControlsStacker.requestLayerUpdateSync(mTransitionState.hasAnimation);
+            // onBrowserControlsOffsetUpdate() can be frozen if a tab is actively loading
+            // (!tab.isUserInteractable()). To prevent the transition from hanging indefinitely and
+            // blocking V <-> H slide animations, synchronously complete the transition immediately.
+            // Re-check isInTransition() in case the transition was finished synchronously by
+            // earlier steps.
+            if (isInTransition()
+                    && mTransitionState.isVerticalTabToggle
+                    && mBrowserControls.getTopControlOffset() == 0) {
+                if (mTransitionState.checkIsFirstUpdate()) {
+                    handleTransitionStart();
+                }
+                handleTransitionFinished();
+            }
         }
     }
 
@@ -265,8 +287,12 @@ public class TabStripTopControlLayer implements TopControlLayer, TabStripTransit
             int newHeight,
             int topPadding,
             boolean applyScrimOverlay,
+            boolean isTabStripSuppressed,
             Runnable onHeightTransitionStartCallback) {
         if (mTabStrip == null && !canTransitionWithoutTabStrip()) return;
+
+        boolean isVerticalTabToggle = (isTabStripSuppressed != mIsTabStripSuppressed);
+        mIsTabStripSuppressed = isTabStripSuppressed;
 
         if (mTransitionState != null) {
             notifyTransitionFinished(false);
@@ -286,6 +312,7 @@ public class TabStripTopControlLayer implements TopControlLayer, TabStripTransit
                         newHeight,
                         topPadding,
                         applyScrimOverlay,
+                        isVerticalTabToggle,
                         onHeightTransitionStartCallback);
     }
 
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/tab_strip/TabStripTransitionCoordinatorUnitTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/tab_strip/TabStripTransitionCoordinatorUnitTest.java
@@ -1391,6 +1391,7 @@ public class TabStripTransitionCoordinatorUnitTest {
                 int newHeight,
                 int topPadding,
                 boolean applyScrimOverlay,
+                boolean isTabStripSuppressed,
                 Runnable transitionStartedCallback) {
             this.heightRequested = newHeight;
             this.applyScrimOverlay = applyScrimOverlay;
