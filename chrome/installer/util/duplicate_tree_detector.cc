ee99ecee55a6aa7998f971c6aa7ae960d8c5fca0
Greg Thompson <grt@chromium.org>
2026-08-04 03:48:17 -0700

    [installer] Always check for duplicates when moving the version dir
    "pv" is mysteriously rewound after a successful update sometimes. Since
    this is the signal used by the updater to determine whether or not an
    update is needed, this leads to what is effectively a same-version
    repair. This is all fine and dandy, except the shortcut to bypass moving
    the version directory on overinstall if nothing has changed was only
    happening when "pv" matched the current version. This is important, as
    the version directory cannot be moved during an in-use overinstall.
    
    This change makes the duplicate check unconditional. For ordinary
    updates, this will be a no-op. For these in-use overinstalls where the
    version directory is in-tact, this will allow the update to succeed.
    
    Bug: 493938735
    Change-Id: I48c0b5731ac8bf979656e17c32a025b74408c475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8176066
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673225}

--- chrome/installer/util/duplicate_tree_detector.cc
+++ chrome/installer/util/duplicate_tree_detector.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/installer/util/duplicate_tree_detector.h"
 
+#include <windows.h>
+
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
@@ -22,8 +24,10 @@ bool IsIdenticalFileHierarchy(const base::FilePath& src_path,
 
   base::File::Info dest_info;
   if (!base::GetFileInfo(dest_path, &dest_info)) {
-    PLOG(ERROR) << "Failed to get file info for destination path: "
-                << dest_path;
+    const auto error = ::GetLastError();
+    PLOG_IF(ERROR,
+            error != ERROR_FILE_NOT_FOUND && error != ERROR_PATH_NOT_FOUND)
+        << "Failed to get file info for destination path: " << dest_path;
     return false;
   }
 
