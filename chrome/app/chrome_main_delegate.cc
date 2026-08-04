bc43b76256964c619679dc4994050f0ceedb53e2
Greg Thompson <grt@chromium.org>
2026-08-03 23:42:00 -0700

    SeparateRendererBinary: chrome_renderer for Windows
    - Add ":chrome_renderer.dll" as a data dep on chrome_initial when
      enable_separate_renderer_binary is true.
    - Introduce the ENABLE_SEPARATE_RENDERER_BINARY buildflag in
      //chrome/common.
    - Add chrome_renderer.dll instead of chrome.dll to the set of allowed
      DLLs when launching renderers when the buildflag is enabled.
    - Load chrome_renderer.dll instead of chrome.dll during renderer process
      startup when the buildflag is enabled.
    
    Bug: 534570563
    Change-Id: I04edec96fd4e1b8c8cef9aa3a2c2c5f4d0789a1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8154562
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673134}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -139,13 +139,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_MAC)
 #include "base/apple/foundation_util.h"
 #include "chrome/app/chrome_main_mac.h"
-#include "chrome/browser/chrome_browser_application_mac.h"
 #include "chrome/browser/mac/code_sign_clone_manager.h"
 #include "chrome/browser/mac/relauncher.h"
 #include "components/crash/core/common/objc_zombie.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(BUILDING_CHROME_RENDERER)
+#include "chrome/browser/chrome_browser_application_mac.h"  // nogncheck
 #include "chrome/browser/shell_integration.h"  // nogncheck
 #endif  // !defined(BUILDING_CHROME_RENDERER)
 #endif
