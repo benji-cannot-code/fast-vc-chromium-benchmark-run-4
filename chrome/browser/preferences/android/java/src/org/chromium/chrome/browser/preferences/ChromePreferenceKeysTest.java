ba17fdfc963002f6e840d25bc201e394e030b9e7
Andrew Grieve <agrieve@chromium.org>
2026-08-05 08:46:29 -0700

    chrome_junit_tests: Fix crash on test listing /w is_official_build=true
    Feature registries are null in this context, so passing them to
    List.of() throws.
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Change-Id: I4eaffd18f267d408a1708b2aad183c0600120f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8204404
    Commit-Queue: Martin Kong <martinkong@google.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674192}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeysTest.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeysTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
 
 import androidx.test.filters.SmallTest;
 
@@ -20,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.shared_preferences.KeyPrefix;
 import org.chromium.base.shared_preferences.PreferenceKeyRegistry;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.build.BuildConfig;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -47,6 +49,7 @@ public class ChromePreferenceKeysTest {
     @Test
     @SmallTest
     public void testKeysAreNotReused() {
+        assumeTrue(BuildConfig.ENABLE_ASSERTS);
         // Build sets of all keys combined between registries and check for any intersections
         // between registries.
         Set<String> allKeysInUse = new HashSet<>();
