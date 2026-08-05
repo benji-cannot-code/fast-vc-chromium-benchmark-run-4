179ff9e298e360f39b6214ef4f57c74a6abd68f2
Bryan Kim <bryankim@google.com>
2026-08-05 13:31:32 -0700

    Componentize chrome/browser/ui/views/search_engines/
    Extract dse_reset_dialog sources from //chrome/browser/ui into
    //chrome/browser/ui/views/search_engines and
    //chrome/browser/ui/views/search_engines:impl.
    
    AGENT_GENERATED_CL
    TAG=agy
    CONV=ce1eee1f-f1f7-46b6-b8f3-5ead7926d5d8
    
    Bug: 353332589
    Change-Id: I5c8625280535603b3d33fb14cd8b74a26a5f0ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8190757
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674452}

--- chrome/browser/ui/views/search_engines/dse_reset_dialog.h
+++ chrome/browser/ui/views/search_engines/dse_reset_dialog.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_SEARCH_ENGINES_DSE_RESET_DIALOG_H_
 #define CHROME_BROWSER_UI_VIEWS_SEARCH_ENGINES_DSE_RESET_DIALOG_H_
 
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "components/omnibox/browser/autocomplete_match.h"
+
+class Browser;
 
 namespace search_engines {
 // Shows a bubble informing the user that their
