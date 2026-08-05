749b77d8289b784014185261a3ef9ba2d36e4841
Dibyajyoti Pal <dibyapal@google.com>
2026-08-05 10:41:56 -0700

    [PWA] Wait for TWA page load in launchTwaAndClick
    Ensure that the TWA tab page load finishes and that the tab view is
    fully rendered with hit-test data available before attempting to perform
    singleClickView on tab.getView() in launchTwaAndClick.
    
    Without waiting for the above changes, clicking on the view can interact
    with an uninitialized or loading page, leading to missing navigations
    and causing TimeoutExceptions while waiting for the target activity.
    
    TAG=agy CONV=45f52c2d-f3b1-4b6a-9807-c181b4144da4
    
    Bug: 494476895
    Test: UrlOverridingTest#testAuxiliaryNavigationWasReparented
    Change-Id: Iad007df598f8e9f7a9029146dc8849a8da0fc6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127935
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674295}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -2575,6 +2575,7 @@ public class UrlOverridingTest {
         launchTwa("com.foo.bar", url);
         ChromeActivity activity = mCustomTabActivityRule.getActivity();
         Tab tab = activity.getActivityTab();
+        ChromeTabUtils.waitForInteractable(tab);
 
         Assert.assertTrue(tab.isTabInPWA());
         Assert.assertFalse(tab.getWebContents().hasOpener());
@@ -2614,7 +2615,6 @@ public class UrlOverridingTest {
 
     @Test
     @LargeTest
-    @DisabledTest(message = "https://crbug.com/513674983")
     public void testTopLevelNavigationWasReparented() throws TimeoutException {
         InterceptNavigationDelegateClientImpl.setIsDesktopWindowingModeForTesting(true);
 
