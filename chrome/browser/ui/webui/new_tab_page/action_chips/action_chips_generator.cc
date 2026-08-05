1b12d65693c57b355277e070cf71b9cdff22c9b7
Ananya Seelam <ananyaseelam@google.com>
2026-08-05 15:20:50 -0700

    [suit] Move action chip fallback suggestion strings to `components/`
    Move `IDS_NTP_ACTION_CHIP_*` suggestion strings from
    `chrome/app/generated_resources.grd` to
    `components/omnibox_strings.grdp` so they can be shared with
    `components/omnibox/browser` which is needed to map these fallback
    suggestions to a fallback suggest inventory.
    
    Bug: b:539620529
    Change-Id: I922ad257647479214c3536977dbc6d5e773ac76f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8203606
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674552}

--- chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_generator.cc
+++ chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_generator.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/remote_suggestions_service.h"
 #include "components/omnibox/browser/search_suggestion_parser.h"
 #include "components/search/ntp_features.h"
+#include "components/strings/grit/components_strings.h"
 #include "components/url_formatter/url_formatter.h"
 #include "content/public/browser/web_contents.h"
 #include "third_party/omnibox_proto/groups.pb.h"
