41e0658486c7786124ab9785e28cb108d155b980
Danilo Tedeschi <danft@google.com>
2026-08-05 11:17:03 -0700

    IcuBridge migration 6/n: ash/system/time/
    Migrate ash/system/time/date_helper.h, date_helper.cc, and calendar_utils.cc
    to use IcuBridge's DateTimeFormatter for month_name_year_formatter_.
    
    Modern API equivalent mapping:
    - pattern "MMMM yyyy" (month name and year) is mapped to base::i18n::datetime_options::YM::Long() (e.g. "August 2026")
    
    Bug: 534455293
    Change-Id: Ic55d60436ac803d68cdb72cf73db1af95371f0d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8185581
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674326}

--- ash/system/time/date_helper.cc
+++ ash/system/time/date_helper.cc
@@ -209,8 +209,7 @@ base::Time DateHelper::GetLocalMidnight(base::Time date) {
 }
 
 DateHelper::DateHelper()
-    : month_name_year_formatter_(CreateSimpleDateFormatter("MMMM yyyy")),
-      time_zone_formatter_(CreateSimpleDateFormatter("zzzz")),
+    : time_zone_formatter_(CreateSimpleDateFormatter("zzzz")),
       twelve_hour_clock_formatter_(CreateSimpleDateFormatter("h:mm a")),
       twenty_four_hour_clock_formatter_(CreateSimpleDateFormatter("HH:mm")),
       day_of_week_formatter_(CreateSimpleDateFormatter("ee")),
@@ -249,7 +248,6 @@ DateHelper::~DateHelper() {
 }
 
 void DateHelper::ResetFormatters() {
-  month_name_year_formatter_ = CreateSimpleDateFormatter("MMMM yyyy");
   time_zone_formatter_ = CreateSimpleDateFormatter("zzzz");
   twelve_hour_clock_formatter_ = CreateSimpleDateFormatter("h:mm a");
   twenty_four_hour_clock_formatter_ = CreateSimpleDateFormatter("HH:mm");
