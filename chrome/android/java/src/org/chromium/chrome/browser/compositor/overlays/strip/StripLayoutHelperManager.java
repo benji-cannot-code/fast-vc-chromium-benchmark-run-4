74996e55829ed3e9199bc99d52ed37ad55b9704f
Brandon Fong <bjfong@google.com>
2026-08-05 14:14:52 -0700

    [search] Minimize the gap between the tab search button and first tab
    Due to the background of the tab search button, there is a larger than
    desired gap to the first tab in the horizontal tab strip. Minimize that
    gap so that the spacing around the button is uniform on all sides.
    
    Bug: 535675893
    Change-Id: If55aabb0613485b552fe946385111234456d1a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8192720
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674500}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -1167,7 +1167,9 @@ public class StripLayoutHelperManager
                 (2 * MIN_TAB_WIDTH_DP)
                         - TAB_OVERLAP_WIDTH_DP
                         + BUTTON_TOUCH_TARGET_SIZE_DP
-                        + getActiveStripLayoutHelper().getTabSearchButton().getWidth()
+                        + (getActiveStripLayoutHelper().getTabSearchButton().isVisible()
+                                ? BUTTON_TOUCH_TARGET_SIZE_DP
+                                : 0.f)
                         + mTrailingButtonsCoordinator.getTrailingButtonsWidthWithPadding()
                         + (shouldShowMsb ? BUTTON_TOUCH_TARGET_SIZE_DP : 0f);
         return Math.round(thresholdDp);
