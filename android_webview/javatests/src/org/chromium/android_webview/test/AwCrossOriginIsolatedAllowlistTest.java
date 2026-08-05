348feea19bc6af3379bc37aeac269c78fabbea92
Traian Dobrescu <traiand@google.com>
2026-08-05 03:32:59 -0700

    Moved CrossOriginIsolatedAllowlist tests to another file
    - Moved the CrossOriginIsolatedAllowlist tests from
      `AwSharedArrayBufferTest` to `AwCrossOriginIsolatedAllowlistTest`.
    - The `AwSharedArrayBufferTest` file will accommodate tests for a
      `AwSharedArrayBuffer` class in the future.
    - This file was added in [this cl](https://chromium-review.git.corp.google.com/c/chromium/src/+/8071103).
    
    Bug: b:452920345
    Change-Id: If6b17741348865f0f2cc2dd608ea210b8f1bb3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8175545
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Traian Dobrescu <traiand@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674009}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwCrossOriginIsolatedAllowlistTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwCrossOriginIsolatedAllowlistTest.java
@@ -29,7 +29,7 @@ import java.util.Set;
 @Batch(Batch.PER_CLASS)
 @RunWith(Parameterized.class)
 @UseParametersRunnerFactory(AwJUnit4ClassRunnerWithParameters.Factory.class)
-public class AwSharedArrayBufferTest extends AwParameterizedTest {
+public class AwCrossOriginIsolatedAllowlistTest extends AwParameterizedTest {
     @Rule public AwActivityTestRule mActivityTestRule;
 
     private TestAwContentsClient mContentsClient;
@@ -76,7 +76,7 @@ public class AwSharedArrayBufferTest extends AwParameterizedTest {
             </html>
             """;
 
-    public AwSharedArrayBufferTest(AwSettingsMutation param) {
+    public AwCrossOriginIsolatedAllowlistTest(AwSettingsMutation param) {
         mActivityTestRule = new AwActivityTestRule(param.getMutation());
     }
 
