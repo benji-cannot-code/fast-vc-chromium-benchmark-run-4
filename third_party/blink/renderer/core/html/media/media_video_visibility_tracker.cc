d034afeddb8487a2a331b1f16780d4743146bf27
Fredrik Söderquist <fs@opera.com>
2026-08-05 15:56:47 -0700

    Drop unnecessary .c_str() and .data()
    Use the String(const std::string&) constructor instead of
    String(const char*).
    
    Change-Id: Iaaf457aa59182e90e9776215a552c458b9133dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8192738
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1674582}

--- third_party/blink/renderer/core/html/media/media_video_visibility_tracker.cc
+++ third_party/blink/renderer/core/html/media/media_video_visibility_tracker.cc
@@ -324,8 +324,7 @@ void RecordVideoOcclusionState(
       intersection_rect_string.Ascii().c_str(),
       video_element_rect_string.Ascii().c_str(), visibility_threshold));
 
-  video_element.RecordVideoOcclusionState(
-      occlusion_state_string.Ascii().c_str());
+  video_element.RecordVideoOcclusionState(occlusion_state_string.Ascii());
 }
 
 }  // anonymous namespace
