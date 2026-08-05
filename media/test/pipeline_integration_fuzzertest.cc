dcddd8dd2caf93902ba2427bf8234629879fdac7
Syed AbuTalib <lowkey@google.com>
2026-08-04 18:14:51 -0700

    Add IAMF fuzzing to pipeline integration fuzzer
    Add fuzzing for IAMF MP4 files via MSE.
    
    Bug: 535279329
    Change-Id: Ia537c7fd2f0fa4a98ccadfb7228f4273571fa3d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8196227
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Syed AbuTalib <lowkey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673827}

--- media/test/pipeline_integration_fuzzertest.cc
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -43,6 +43,9 @@ enum FuzzerVariant {
 #if BUILDFLAG(ENABLE_AV1_DECODER)
   MP4_AV1,
 #endif
+#if BUILDFLAG(ENABLE_IAMF_TOOLS)
+  MP4_IAMF_OPUS,
+#endif
   MP4_FLAC,
   MP4_OPUS,
   MP3,
@@ -78,6 +81,10 @@ std::string MseFuzzerVariantEnumToMimeTypeString(FuzzerVariant variant) {
     case MP4_AV1:
       return "video/mp4; codecs=\"av01.0.04M.08\"";
 #endif  // BUILDFLAG(ENABLE_AV1_DECODER)
+#if BUILDFLAG(ENABLE_IAMF_TOOLS)
+    case MP4_IAMF_OPUS:
+      return "audio/mp4; codecs=\"iamf.001.001.Opus\"";
+#endif  // BUILDFLAG(ENABLE_IAMF_TOOLS)
     case MP4_FLAC:
       return "audio/mp4; codecs=\"flac\"";
     case MP4_OPUS:
