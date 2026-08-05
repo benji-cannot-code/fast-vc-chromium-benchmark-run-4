716ff3f01c593516cfeebbf5878128bdb6021497
Isaac Madgwick <isaacmadgwick@chromium.org>
2026-08-05 02:27:45 -0700

    Remove redundant IPC parameters in ShouldOverrideUrlLoading in WebView
    - The parameters: is_redirect and is_main_frame can be hard set to
    false and true as the browser process already knows what they should be
    
    Bug: 496616713
    Change-Id: I3682949d7293f2006f306b4db433cc885713d39a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8140140
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Isaac Madgwick <isaacmadgwick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673986}

--- android_webview/browser/renderer_host/aw_render_view_host_ext.h
+++ android_webview/browser/renderer_host/aw_render_view_host_ext.h
@@ -90,8 +90,6 @@ class AwRenderViewHostExt : public content::WebContentsObserver,
   void ShouldOverrideUrlLoading(
       const std::u16string& url,
       bool has_user_gesture,
-      bool is_redirect,
-      bool is_main_frame,
       ShouldOverrideUrlLoadingCallback callback) override;
 
   mojom::LocalMainFrame* GetLocalMainFrameRemote();
