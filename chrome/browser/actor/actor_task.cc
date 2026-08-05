d2d2d4d71cc0baff0809fd8c379228d8d7cba430
Praneeth Surapaneni <praneethsn@google.com>
2026-08-05 10:30:08 -0700

    [Glic] Add background tab consent wait state and task interrupt reason
    - Added kWaitingForExperimentalTriggeringConsent to mojom::ActorTaskInterruptReason.
    - Extended actor::ActorTask to support and store an InterruptReason when interrupted.
    - Plumbed conversion from Glic Mojo ActorTaskInterruptReason to C++ ActorTask::InterruptReason inside GlicActorClientSession.
    
    Bug: 507918067
    Change-Id: I83930e14a3f87cbfd1fb950f9adfea6cc26811c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178312
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Praneeth Surapaneni <praneethsn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674278}

--- chrome/browser/actor/actor_task.cc
+++ chrome/browser/actor/actor_task.cc
@@ -522,12 +522,14 @@ void ActorTask::Resume() {
   SetState(State::kReflecting);
 }
 
-void ActorTask::Interrupt(bool retain_user_control) {
+void ActorTask::Interrupt(bool retain_user_control,
+                          InterruptReason interrupt_reason) {
   if (GetState() != State::kReflecting && GetState() != State::kActing) {
     return;
   }
   interrupted_task_needs_user_control_ = retain_user_control;
   execution_engine_->PauseOngoingActions();
+  interrupt_reason_ = interrupt_reason;
   SetState(State::kWaitingOnUser);
 }
 
@@ -535,6 +537,7 @@ void ActorTask::Uninterrupt(State resumed_state) {
   if (GetState() != State::kWaitingOnUser) {
     return;
   }
+  interrupt_reason_ = std::nullopt;
   interrupted_task_needs_user_control_ = false;
   SetState(resumed_state);
   execution_engine_->DidUninterruptTask();
