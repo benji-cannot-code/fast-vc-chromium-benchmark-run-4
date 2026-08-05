d1cd4c4e5aa7688b64cc788e0f8ec194083626dc
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-08-05 13:14:11 -0700

    Fix build/build_config.h IWYU in view_focus_observer_unittest.cc
    https://source.chromium.org/chromium/chromium/src/+/main:ui/views/interaction/view_focus_observer_unittest.cc;l=85;drc=23915e158a2ae3474840f8759884733eb56fe77d
    
    Bug: 542921651
    Change-Id: I109359fb6c20390971a8e9f4dc4be6d0a0223232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8202349
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674431}

--- ui/views/interaction/view_focus_observer_unittest.cc
+++ ui/views/interaction/view_focus_observer_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/callback_helpers.h"
+#include "build/build_config.h"
 #include "ui/base/interaction/element_identifier.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/label_button.h"
