063b704322b8b87cdfd594a75abe63132f55395d
Alison Gale <agale@chromium.org>
2026-08-05 10:34:11 -0700

    Remove TODOs that reference closed bugs
    Let me know if any of these should instead be replaced by new bugs
    
    #top-chrome-bug-fixit
    
    Change-Id: Ia4839015c882af3460f0268d59a3d11bba3d3091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8193770
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674285}

--- chrome/browser/ui/tabs/tab_drag_api/desktop_tab_drag_impl/tab_drag_window_adapter_impl.cc
+++ chrome/browser/ui/tabs/tab_drag_api/desktop_tab_drag_impl/tab_drag_window_adapter_impl.cc
@@ -216,7 +216,6 @@ void TabDragWindowAdapterImpl::EndWindowMoveLoop() {
   }
 }
 
-// TODO(crbug.com/501070793) Implement this using the TabStripAPI.
 base::expected<void, mojo_base::mojom::ErrorPtr>
 TabDragWindowAdapterImpl::MigrateTabs(
     tabs_api::TabDragWindowId target_window_id,
