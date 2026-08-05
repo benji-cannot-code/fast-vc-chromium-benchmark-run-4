93313730942218d0e5607f01ae7e944ed900ef04
dpapad <dpapad@chromium.org>
2026-08-05 01:07:54 -0700

    Settings Lit: Migrate performance_page/, part 6.
    In this part migrating exception_edit_dialog.ts and
    exception_tabbed_add_dialog.ts to Lit.
    
    Bug: 393471368
    Change-Id: Ic87487d81eea217e7ee8e70b1545ef704c44a80a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8192754
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673957}

--- chrome/test/data/webui/settings/performance_page_test.ts
+++ chrome/test/data/webui/settings/performance_page_test.ts
@@ -454,6 +454,7 @@ suite('TabDiscardExceptionList', function() {
 
     clickMoreActionsButton(entry);
     clickEditMenuItem();
+    flush();
     await microtasksFinished();
     await inputDialog(getEditDialog(), getExceptionListEntry(0).entry.site);
     await microtasksFinished();
