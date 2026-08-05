6f9424eb61dc95e119d23858621ba3324a889111
Michał Kaczmarczyk <mickaczmarczyk@google.com>
2026-08-05 02:52:58 -0700

    Introduce a provider API for retrieving device identifiers on iOS.
    This adds a new provider API, GetDeviceIdentifier(), to retrieve device
    identifiers such as the Vendor ID on iOS.
    
    Bug: 421927804
    Change-Id: Icf8f426c64e0a776ba3e95727b6b504b73224f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177611
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674001}

---
+++ ios/chrome/test/providers/signin/test_device_identifier.mm
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/test/providers/signin/test_device_identifier.h"
+
+#import <utility>
+
+#import "base/no_destructor.h"
+#import "ios/public/provider/chrome/browser/signin/device_identifier_api.h"
+
+namespace ios::provider {
+
+namespace {
+
+std::string& GetTestDeviceIdentifierStorage() {
+  static base::NoDestructor<std::string> storage("test-device-identifier");
+  return *storage;
+}
+
+}  // namespace
+
+namespace test {
+
+void SetDeviceIdentifier(std::string identifier) {
+  GetTestDeviceIdentifierStorage() = std::move(identifier);
+}
+
+}  // namespace test
+
+std::string GetDeviceIdentifier() {
+  return GetTestDeviceIdentifierStorage();
+}
+
+}  // namespace ios::provider
