8a97d58a6f1854f4c6f08397c9db3cb034fbf422
hiroki oshima <hiroki.oshima@gmail.com>
2026-08-04 20:51:10 -0700

    remove GameDashboardController dangling marker
    Bug: 40212619
    Change-Id: I500f02b84beed669060b9faf2203ab70140e3b5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7880707
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673883}

--- ash/game_dashboard/game_dashboard_context_unittest.cc
+++ ash/game_dashboard/game_dashboard_context_unittest.cc
@@ -378,6 +378,7 @@ class GameDashboardContextTest : public GameDashboardTestBase {
   }
 
   void TearDown() override {
+    cros_network_.reset();
     GetContext()->RemovePostTargetHandler(&post_target_event_capturer_);
     CloseGameWindow();
     GameDashboardTestBase::TearDown();
