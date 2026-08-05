f1ffe5605b985aa8ed8acbbe1392de720342db53
Kalvin Lee <kdlee@chromium.org>
2026-08-05 12:12:20 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    ash/quick_pair
    
    This CL was uploaded by git cl split.
    
    R=brandosocarras@google.com
    
    Bug: 529948677
    Change-Id: Ie7bb4510a94da19eceb9ab88f68a2381cf48da09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8029941
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674372}

--- ash/quick_pair/fast_pair_handshake/fast_pair_gatt_service_client_impl_unittest.cc
+++ ash/quick_pair/fast_pair_handshake/fast_pair_gatt_service_client_impl_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/quick_pair/fast_pair_handshake/fake_fast_pair_data_encryptor.h"
 #include "ash/quick_pair/fast_pair_handshake/fast_pair_data_encryptor.h"
 #include "ash/quick_pair/fast_pair_handshake/fast_pair_data_encryptor_impl.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
