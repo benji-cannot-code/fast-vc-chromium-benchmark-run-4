0dd3992ca027bbc80a53d595f422f78110655eaf
Akrosh Gandhi <akroshg@microsoft.com>
2026-08-05 12:51:57 -0700

    Reduce PrefRegistry type metadata memory
    Most preferences use RegisteredPrefType::kOther, but PrefRegistry stores
    an entry for every registered preference. Store only non-default types
    and consult the defaults store to distinguish kOther from an unregistered
    preference.
    
    Share the sparse type map between regular and incognito registries so
    registrations made after a fork remain visible.
    
    Local instrumentation with 2,895 registered preferences and 118
    non-default types reduced estimated type-map memory from 253.8 KiB to
    13.8 KiB, saving about 240 KiB (94.6%). Across the observed registries,
    the modeled saving was about 288 KiB.
    
    Bug: None
    Test: components_unittests --gtest_filter=PrefRegistryTest.GetRegisteredPrefType:PrefServiceSyncableTest.ForkForIncognitoSharesRegisteredPrefTypes
    Change-Id: I164ff2168e690c164f7e1a81478b6418b5d2f16c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8190377
    Commit-Queue: Akrosh Gandhi <akroshg@microsoft.com>
    Reviewed-by: Chris Davis <chrdavis@microsoft.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674406}

--- components/prefs/pref_registry.cc
+++ components/prefs/pref_registry.cc
@@ -11,12 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check_op.h"
+#include "base/containers/map_util.h"
 #include "base/values.h"
 #include "components/prefs/default_pref_store.h"
 #include "components/prefs/pref_store.h"
 
 PrefRegistry::PrefRegistry()
-    : defaults_(base::MakeRefCounted<DefaultPrefStore>()) {}
+    : defaults_(base::MakeRefCounted<DefaultPrefStore>()),
+      registration_types_(base::MakeRefCounted<
+                          base::RefCountedData<PrefRegistrationTypeMap>>()) {}
 
 PrefRegistry::~PrefRegistry() {}
 
@@ -27,9 +30,14 @@ uint32_t PrefRegistry::GetRegistrationFlags(std::string_view pref_name) const {
 
 std::optional<PrefRegistry::RegisteredPrefType>
 PrefRegistry::GetRegisteredPrefType(std::string_view pref_name) const {
-  const auto& it = registration_types_.find(pref_name);
-  return it != registration_types_.end() ? std::make_optional(it->second)
-                                         : std::nullopt;
+  if (const RegisteredPrefType* pref_type =
+          base::FindOrNull(registration_types_->data, pref_name)) {
+    return *pref_type;
+  }
+  if (!defaults_->GetValue(pref_name, nullptr)) {
+    return std::nullopt;
+  }
+  return RegisteredPrefType::kOther;
 }
 
 scoped_refptr<PrefStore> PrefRegistry::defaults() {
@@ -67,15 +75,20 @@ void PrefRegistry::RegisterPreference(std::string_view path,
       << "Trying to register a previously registered pref: " << path;
   DCHECK(!registration_flags_.contains(path))
       << "Trying to register a previously registered pref: " << path;
-  DCHECK(!registration_types_.contains(path))
-      << "Trying to register a previously registered pref: " << path;
+  // `registration_types_` is not checked here because it is shared between
+  // regular and incognito profiles. Therefore, values can be written again
+  // when incognito profiles are created.
 
   defaults_->SetDefaultValue(path, std::move(default_value));
   if (flags != NO_REGISTRATION_FLAGS) {
     registration_flags_.insert_or_assign(path, flags);
   }
 
-  registration_types_.insert_or_assign(path, type);
+  // Only values diverging from `RegisteredPrefType::kOther` are persisted. All
+  // other prefs are assumed to be of type `kOther`.
+  if (type != RegisteredPrefType::kOther) {
+    registration_types_->data.insert_or_assign(path, type);
+  }
 
   OnPrefRegistered(path, flags);
 }
---
+++ components/prefs/pref_registry_unittest.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/prefs/pref_registry.h"
+
+#include "base/memory/scoped_refptr.h"
+#include "base/time/time.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(PrefRegistryTest, GetRegisteredPrefType) {
+  auto registry = base::MakeRefCounted<PrefRegistrySimple>();
+  registry->RegisterStringPref("string_pref", std::string());
+  registry->RegisterTimePref("time_pref", base::Time());
+
+  EXPECT_EQ(PrefRegistry::RegisteredPrefType::kOther,
+            registry->GetRegisteredPrefType("string_pref"));
+  EXPECT_EQ(PrefRegistry::RegisteredPrefType::kTime,
+            registry->GetRegisteredPrefType("time_pref"));
+  EXPECT_EQ(std::nullopt, registry->GetRegisteredPrefType("unregistered_pref"));
+}
