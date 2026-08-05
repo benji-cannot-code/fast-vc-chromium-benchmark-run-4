15516d31a720727693f4fda773a548eccfe96fee
Federica Germinario <fedegermi@google.com>
2026-08-05 07:30:51 -0700

    [ios] Create TaskRequestForStandardURLContext subclass
    This change creates TaskRequestForStandardURLContext and adds in there
    metrics collection of external actions.
    
    Bug: 493816082
    Change-Id: Icc5f52fbd1137800f1fb6373ad3877d35a24e6a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8180491
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674132}

---
+++ ios/chrome/app/task_request_for_standard_url_context.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_TASK_REQUEST_FOR_STANDARD_URL_CONTEXT_H_
+#define IOS_CHROME_APP_TASK_REQUEST_FOR_STANDARD_URL_CONTEXT_H_
+
+#import "ios/chrome/app/task_request_url_context.h"
+
+// Subclass of TaskRequestForURLContext for handling standard URLs (http,
+// https), file URLs, and external actions.
+@interface TaskRequestForStandardURLContext : TaskRequestForURLContext
+@end
+
+#endif  // IOS_CHROME_APP_TASK_REQUEST_FOR_STANDARD_URL_CONTEXT_H_
--- ios/chrome/app/task_request_url_context.mm
+++ ios/chrome/app/task_request_url_context.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/application_delegate/url_opener_params.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/app/startup/app_launch_metrics.h"
+#import "ios/chrome/app/task_request_for_standard_url_context.h"
 #import "ios/chrome/app/task_request_for_widget_url_context.h"
 #import "ios/chrome/app/task_request_for_xcallback_url_context.h"
 #import "ios/chrome/app/task_request_private.h"
@@ -174,9 +175,10 @@ void RecordRuntimeMetrics(UIOpenURLContext* url_context, bool is_first_run) {
                 sceneState:sceneState
                isColdStart:isColdStart];
   }
-  return [[TaskRequestForURLContext alloc] initWithURLContext:URLContext
-                                                   sceneState:sceneState
-                                                  isColdStart:isColdStart];
+  return
+      [[TaskRequestForStandardURLContext alloc] initWithURLContext:URLContext
+                                                        sceneState:sceneState
+                                                       isColdStart:isColdStart];
 }
 
 - (instancetype)initWithURLContext:(UIOpenURLContext*)URLContext
