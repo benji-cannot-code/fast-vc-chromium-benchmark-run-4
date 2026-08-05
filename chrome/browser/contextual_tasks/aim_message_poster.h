53f040aa6e8b39e61179531b5651c5bd14e60e2c
Josiah Kiehl <kiehl@google.com>
2026-08-05 09:58:10 -0700

    Contextual Tasks: Introduce AimMessagePoster interface (Refactor)
    Decouple message posting from the UI object in cobrowsing so it can be
    used in v1 and v2.
    
    Bug: b:542613142
    Test: unit_tests --gtest_filter=*ContextualTasks*
    Change-Id: Ifeb0c5159f0a9ec89da163c64dbad2f7d73b41c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8135730
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674248}

---
+++ chrome/browser/contextual_tasks/aim_message_poster.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CONTEXTUAL_TASKS_AIM_MESSAGE_POSTER_H_
+#define CHROME_BROWSER_CONTEXTUAL_TASKS_AIM_MESSAGE_POSTER_H_
+
+namespace lens {
+class ClientToAimMessage;
+}  // namespace lens
+
+namespace contextual_tasks {
+
+class AimMessagePoster {
+ public:
+  virtual ~AimMessagePoster() = default;
+  virtual void PostAimMessage(const lens::ClientToAimMessage& message) = 0;
+};
+
+}  // namespace contextual_tasks
+
+#endif  // CHROME_BROWSER_CONTEXTUAL_TASKS_AIM_MESSAGE_POSTER_H_
