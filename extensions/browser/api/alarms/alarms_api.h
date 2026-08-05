968314bc3101b489b4281aea8d8bfbc7da3e42b9
Anton Bershanskyi <bershanskyi@gmail.com>
2026-08-05 11:56:58 -0700

    [Extensions][Code Health] Clean up function arguments in AlarmManager
    Remove unused arguments in extensions::AlarmManager and various
    extensions::Alarm*Function classes.
    
    Bug: None
    Change-Id: Iea557efb3e0dda8e5ecb0c0ef587b303e7eb49d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8182410
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Anton Bershanskyi <bershanskyi@gmail.com>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674361}

--- extensions/browser/api/alarms/alarms_api.h
+++ extensions/browser/api/alarms/alarms_api.h
@@ -46,7 +46,7 @@ class AlarmsGetFunction : public ExtensionFunction {
   ResponseAction Run() override;
 
  private:
-  void Callback(const std::string& name, Alarm* alarm);
+  void Callback(Alarm* alarm);
   DECLARE_EXTENSION_FUNCTION("alarms.get", ALARMS_GET)
 };
 
@@ -70,7 +70,7 @@ class AlarmsClearFunction : public ExtensionFunction {
   ResponseAction Run() override;
 
  private:
-  void Callback(const std::string& name, bool success);
+  void Callback(bool removed);
   DECLARE_EXTENSION_FUNCTION("alarms.clear", ALARMS_CLEAR)
 };
 
