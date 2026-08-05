baa7e63a12cc4708fd7ac6001a2036ebf2a4cad4
Ivan Kabadzhov <ikabadzhov@google.com>
2026-08-05 13:58:54 -0700

    Add method to return CWS review URL
    Introduce util helper extensions::util::GetCWSWritingReviewUrl to build
    deep links redirecting users to the Chrome Web Store review write
    dialog. Validates extension ID structure and appends telemetry source
    query params.
    
    Bug: b:535481653
    Change-Id: I335c749d91d9154a28c7cda35644e89159ea80ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138182
    Commit-Queue: Ivan Kabadzhov <ikabadzhov@google.com>
    Reviewed-by: Eva Su <evasu@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674481}

--- chrome/browser/extensions/extension_util.cc
+++ chrome/browser/extensions/extension_util.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_is_test.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
+#include "base/notreached.h"
+#include "base/strings/strcat.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/extensions/sync/extension_sync_service.h"
@@ -21,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/sync_helper.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/crx_file/id_util.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/variations/variations_associated_data.h"
@@ -41,11 +44,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/user_script_manager.h"
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/extension_urls.h"
 #include "extensions/common/features/feature_developer_mode_only.h"
 #include "extensions/common/icons/extension_icon_set.h"
 #include "extensions/common/manifest_handlers/incognito_info.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
+#include "net/base/url_util.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
@@ -418,5 +423,28 @@ DseNtpOverrideType GetDseNtpOverrideType(const Extension& extension) {
   }
 }
 #endif
+
+GURL GetCWSWritingReviewUrl(const ExtensionId& extension_id,
+                            CWSReviewSource source) {
+  CHECK(crx_file::id_util::IdIsValid(extension_id));
+
+  const char* source_str = nullptr;
+  switch (source) {
+    case CWSReviewSource::kExtensionsMenu:
+      source_str = "extensions_menu";
+      break;
+    case CWSReviewSource::kExtensionsPage:
+      source_str = "extensions_page";
+      break;
+    case CWSReviewSource::kContextMenu:
+      source_str = "context_menu";
+      break;
+  }
+
+  GURL review_url = extension_urls::GetNewWebstoreLaunchURL().Resolve(
+      base::StrCat({"detail/", extension_id, "/reviews"}));
+  review_url = net::AppendQueryParameter(review_url, "action", "write");
+  return net::AppendQueryParameter(review_url, "source", source_str);
+}
 
 } // namespace extensions::util
