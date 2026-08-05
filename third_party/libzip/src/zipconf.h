2c4d0ec480b1dfd4259df600796ce85fd51622c1
Zekun Jiang <zekunjiang@google.com>
2026-08-05 16:17:40 -0700

    Prepare libzip for automated updates.
    This CL:
    1. adds a crowbar.txt file documenting the steps needed to update
       third_party/libzip.
    2. removes patches to add generated files and replace them with
       crowbar instructions to generate using cmake.
    3. Regenerate the config.h, zipconf.h and zip_err_str.c which wasn't
       regenerated during the last update to libzip in crrev.com/c/8109257.
    
    Bug: 514931473
    Change-Id: I8e768d07cb7865c24ecbd0a6b8d66652698cf87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8134903
    Reviewed-by: Rachael Newitt <renewitt@google.com>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674594}

--- third_party/libzip/src/zipconf.h
+++ third_party/libzip/src/zipconf.h
@@ -11,15 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#define LIBZIP_VERSION "1.7.3"
+#define LIBZIP_VERSION "1.11.4"
 #define LIBZIP_VERSION_MAJOR 1
-#define LIBZIP_VERSION_MINOR 7
-#define LIBZIP_VERSION_MICRO 3
+#define LIBZIP_VERSION_MINOR 11
+#define LIBZIP_VERSION_MICRO 4
 
 #define ZIP_STATIC
-
-#define _Nullable
-#define _Nonnull
 
 #if !defined(__STDC_FORMAT_MACROS)
 #define __STDC_FORMAT_MACROS 1
