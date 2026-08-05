4ffd93a69dfb41702ccf300301195c4c66430217
Zaina Al-Mashni <zalmashni@google.com>
2026-08-05 07:27:34 -0700

    Add session consistency Geolevel1 to metrics internals.
    This change populates the session consistency administrative area code
    (Geolevel1) in ClientFilterableState and exposes this value in the
    chrome://metrics-internals variations summary as "Administrative area
    (Session Consistency)" for development purposes.
    
    Bug: 524959454
    Change-Id: Idc3e4c02817abceac952d8e743b622ebcb7a6c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8196525
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674128}

--- components/variations/service/variations_field_trial_creator.cc
+++ components/variations/service/variations_field_trial_creator.cc
@@ -449,6 +449,7 @@ VariationsFieldTrialCreator::GetClientFilterableStateForVersion(
   state->session_consistency_country = GetLatestCountry();
   state->permanent_consistency_country = LoadPermanentConsistencyCountry(
       version, state->session_consistency_country);
+  state->session_consistency_geolevel1 = GetLatestGeoLevel1();
   // Update the stored permanent consistency country
   permanent_consistency_country_ = state->permanent_consistency_country;
   permanent_consistency_country_initialized_ = true;
