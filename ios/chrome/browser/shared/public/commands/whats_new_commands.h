9f9d746bed9a0cec7719f49add45625bd5aa92b3
Gauthier Ambard <gambard@chromium.org>
2026-08-06 07:02:24 -0700

    [iOS] Extract command handling out of BrowserCoordinator (3/N)
    This CL extracts some command protocols out of BrowserCoordinator
    into a dedicated object.
    This dedicated object is only responsible for handling those commands.
    
    Bug: 542879805
    Change-Id: Ib2df46b97637a9c0172f9598da88c5ba94f266ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8204286
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674940}

--- ios/chrome/browser/shared/public/commands/whats_new_commands.h
+++ ios/chrome/browser/shared/public/commands/whats_new_commands.h
@@ -6,17 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_WHATS_NEW_COMMANDS_H_
 #define IOS_CHROME_BROWSER_SHARED_PUBLIC_COMMANDS_WHATS_NEW_COMMANDS_H_
 
+@protocol PromosManagerUIHandler;
+
 // Commands related to What's new.
 @protocol WhatsNewCommands
 
 // Shows what's new.
 - (void)showWhatsNew;
 
+// Shows what's new and will display a promo afterwards.
+- (void)showWhatsNewWithPromosUIHandler:
+    (id<PromosManagerUIHandler>)promosUIHandler;
+
 // Dismisses what's new.
 - (void)dismissWhatsNew;
-
-// Shows what's new IPH.
-- (void)showWhatsNewIPH;
 
 @end
 
--- ios/chrome/browser/whats_new/coordinator/whats_new_coordinator.mm
+++ ios/chrome/browser/whats_new/coordinator/whats_new_coordinator.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
 #import "ios/chrome/browser/promos_manager/coordinator/promos_manager_ui_handler.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/shared/public/commands/help_commands.h"
 #import "ios/chrome/browser/shared/public/commands/lens_commands.h"
 #import "ios/chrome/browser/shared/public/commands/scene_commands.h"
 #import "ios/chrome/browser/shared/public/commands/settings_commands.h"
@@ -25,9 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/whats_new/coordinator/whats_new_mediator.h"
 #import "ios/chrome/browser/whats_new/public/constants.h"
 #import "ios/chrome/browser/whats_new/ui/whats_new_table_view_controller.h"
+#import "ios/chrome/browser/whats_new/ui/whats_new_table_view_delegate.h"
 
 @interface WhatsNewCoordinator () <UIAdaptivePresentationControllerDelegate,
-                                   UINavigationControllerDelegate>
+                                   UINavigationControllerDelegate,
+                                   WhatsNewTableViewDelegate>
 
 // The mediator to display What's New data.
 @property(nonatomic, strong) WhatsNewMediator* mediator;
@@ -53,7 +56,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-@implementation WhatsNewCoordinator
+@implementation WhatsNewCoordinator {
+  // Whether to show a promo bubble after dismissing What's New.
+  BOOL _shouldShowBubblePromoOnDismiss;
+
+  // The promos manager ui handler to alert for promo UI changes. Should only be
+  // set if this coordinator was a promo presented by the PromosManager.
+  __weak id<PromosManagerUIHandler> _promosUIHandler;
+}
+
+- (void)setShouldShowPromoOnDismissWithHandler:
+    (id<PromosManagerUIHandler>)promosUIHandler {
+  _shouldShowBubblePromoOnDismiss = YES;
+  _promosUIHandler = promosUIHandler;
+}
 
 #pragma mark - ChromeCoordinator
 
@@ -115,10 +131,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   base::UmaHistogramCounts10000("IOS.WhatsNew.ItemsClickedCount",
                                 self.clicksOnWhatsNewItemsCount);
 
-  [self.promosUIHandler promoWasDismissed];
+  [_promosUIHandler promoWasDismissed];
 
-  if (self.shouldShowBubblePromoOnDismiss) {
-    [self.whatsNewHandler showWhatsNewIPH];
+  if (_shouldShowBubblePromoOnDismiss) {
+    [HandlerForProtocol(self.browser->GetCommandDispatcher(), HelpCommands)
+        presentInProductHelpWithType:InProductHelpType::kWhatsNew];
   }
 
   [super stop];
