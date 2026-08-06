25fc0ce8a4e54f8a19f1dbec146fd18277d92558
Ibrahim Kanouche <kanouche@google.com>
2026-08-06 12:11:49 -0700

    Revert "[Lens] Clean up unused NewChromeLensOverlay method."
    This reverts commit 4bcf4bd3ef634283cb4542901935c43815be576e.
    
    Reason for revert: broken build tree https://ci.chromium.org/p/chrome/g/gardener.ios-internal/console?limit=200
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/simulator-builder-arm64/97369/overview
    
    Original change's description:
    > [Lens] Clean up unused NewChromeLensOverlay method.
    >
    > Change-Id: Id675542c045a9bf5cc1d832900787da5ccc95cd6
    > Bug: 541315801
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8199036
    > Auto-Submit: Charles Yang <cmyang@google.com>
    > Reviewed-by: Radu Nitescu <radunitescu@google.com>
    > Commit-Queue: Charles Yang <cmyang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1675066}
    
    Bug: 541315801
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ica6af1edcc2ba6adf6218421e01d50a039f69c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8215652
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Owners-Override: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675178}

--- ios/public/provider/chrome/browser/lens/lens_overlay_api.h
+++ ios/public/provider/chrome/browser/lens/lens_overlay_api.h
@@ -140,6 +140,11 @@ UIViewController<ChromeLensOverlay>* NewChromeLensOverlay(
     NSArray<UIAction*>* precedingMenuItems,
     NSArray<UIAction*>* additionalMenuItems);
 
+UIViewController<ChromeLensOverlay>* NewChromeLensOverlay(
+    LensImageSource* imageSource,
+    LensConfiguration* config,
+    NSArray<UIAction*>* additionalMenuItems);
+
 }  // namespace provider
 }  // namespace ios
 
