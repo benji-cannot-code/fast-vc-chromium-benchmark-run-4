5308beeeeee2e785f85048f430ea13bc7a1167b9
Charles Yang <cmyang@google.com>
2026-08-06 10:15:24 -0700

    [Lens] Clean up unused NewChromeLensOverlay method.
    Change-Id: Id675542c045a9bf5cc1d832900787da5ccc95cd6
    Bug: 541315801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8199036
    Auto-Submit: Charles Yang <cmyang@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Charles Yang <cmyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675066}

--- ios/chrome/browser/providers/lens/chromium_lens.mm
+++ ios/chrome/browser/providers/lens/chromium_lens.mm
@@ -44,14 +44,6 @@ UIViewController<ChromeLensOverlay>* NewChromeLensOverlay(
   return nil;
 }
 
-UIViewController<ChromeLensOverlay>* NewChromeLensOverlay(
-    LensImageSource* imageSource,
-    LensConfiguration* config,
-    NSArray<UIAction*>* additionalMenuItems) {
-  // Lens is not supported in Chromium.
-  return nil;
-}
-
 bool IsLensSupported() {
   // Lens is not supported in Chromium.
   return false;
