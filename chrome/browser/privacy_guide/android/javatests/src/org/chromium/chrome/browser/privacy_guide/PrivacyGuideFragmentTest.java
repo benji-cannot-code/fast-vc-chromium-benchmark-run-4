ea3ac7460316208bfc0123e13db8937f7488eb45
Mark Schillaci <mschillaci@google.com>
2026-08-06 10:01:52 -0700

    Disable testBackwardNavAllActions in PrivateGuideFragmentTest
    Bug: 543408467
    Change-Id: I479faa3ac5aba3fa7e6f0abe66a955207c48f1ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8217147
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Owners-Override: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675051}

--- chrome/browser/privacy_guide/android/javatests/src/org/chromium/chrome/browser/privacy_guide/PrivacyGuideFragmentTest.java
+++ chrome/browser/privacy_guide/android/javatests/src/org/chromium/chrome/browser/privacy_guide/PrivacyGuideFragmentTest.java
@@ -54,6 +54,7 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -387,6 +388,7 @@ public class PrivacyGuideFragmentTest {
 
     @Test
     @LargeTest
+    @DisabledTest(message = "crbug.com/543408467")
     @Feature({"PrivacyGuide"})
     public void testBackwardNavAllActions() {
         setMSBBState(false);
