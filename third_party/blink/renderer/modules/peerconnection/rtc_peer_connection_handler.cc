4c1e1d6f8a765c687233bb7811185109819a18bb
Danil Chapovalov <danilchap@chromium.org>
2026-08-06 01:57:15 -0700

    Cleanup collecting UMA WebRTC.PeerConnection.ConnectionState
    This histogram expired two years ago
    
    Bug: 331207460
    Change-Id: I3bd328bd003046b7b9b8573089a97c96dc8675a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8200665
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674805}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
@@ -1911,7 +1911,6 @@ void RTCPeerConnectionHandler::OnIceConnectionChange(
     webrtc::PeerConnectionInterface::IceConnectionState new_state) {
   TRACE_EVENT0("webrtc", "RTCPeerConnectionHandler::OnIceConnectionChange");
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
-  ReportICEState(new_state);
   track_metrics_.IceConnectionChange(new_state);
 }
 
@@ -2219,17 +2218,6 @@ scoped_refptr<base::SingleThreadTaskRunner>
 RTCPeerConnectionHandler::signaling_thread() const {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
   return signaling_thread_;
-}
-
-void RTCPeerConnectionHandler::ReportICEState(
-    webrtc::PeerConnectionInterface::IceConnectionState new_state) {
-  DCHECK(task_runner_->RunsTasksInCurrentSequence());
-  if (ice_state_seen_[new_state]) {
-    return;
-  }
-  ice_state_seen_[new_state] = true;
-  UMA_HISTOGRAM_ENUMERATION("WebRTC.PeerConnection.ConnectionState", new_state,
-                            webrtc::PeerConnectionInterface::kIceConnectionMax);
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include <array>
 #include <memory>
 #include <string>
 #include <vector>
@@ -300,11 +299,6 @@ class MODULES_EXPORT RTCPeerConnectionHandler {
     bool rtcp_mux = false;
   };
 
-  // Report to UMA whether an IceConnectionState has occurred. It only records
-  // the first occurrence of a given state.
-  void ReportICEState(
-      webrtc::PeerConnectionInterface::IceConnectionState new_state);
-
   void ReportFirstSessionDescriptions(const FirstSessionDescription& local,
                                       const FirstSessionDescription& remote);
 
@@ -435,10 +429,6 @@ class MODULES_EXPORT RTCPeerConnectionHandler {
   // unit tests) are ignored.
   std::unique_ptr<FirstSessionDescription> first_local_description_;
   std::unique_ptr<FirstSessionDescription> first_remote_description_;
-
-  // Track which ICE Connection state that this PeerConnection has gone through.
-  std::array<bool, webrtc::PeerConnectionInterface::kIceConnectionMax>
-      ice_state_seen_ = {};
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
