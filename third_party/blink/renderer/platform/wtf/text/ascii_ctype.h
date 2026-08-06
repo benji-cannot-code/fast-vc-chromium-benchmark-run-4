b2ab76846ad1b29761b9a964a3a47b065b0d14c7
Kent Tamura <tkent@chromium.org>
2026-08-06 15:37:20 -0700

    WTF: Support format type specifiers in blink::*Format*()
    Add support for type specifiers ('d', 'x', 'X', 's') in blink::Format(),
    VFormat(), and VFormatTo().
    
    Compile-time type checking is added in FormatString<Args...> and runtime
    checking in VFormatTo().
    
    Bug: 542260133
    Change-Id: If0829b5acfeb59caba7b27508dcc470cdbacfbc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8210320
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675351}

--- third_party/blink/renderer/platform/wtf/text/ascii_ctype.h
+++ third_party/blink/renderer/platform/wtf/text/ascii_ctype.h
@@ -56,7 +56,7 @@ constexpr inline bool IsAscii(CharType c) {
 }
 
 template <typename CharType>
-inline bool IsAsciiAlpha(CharType c) {
+constexpr inline bool IsAsciiAlpha(CharType c) {
   return (c | 0x20) >= 'a' && (c | 0x20) <= 'z';
 }
 
