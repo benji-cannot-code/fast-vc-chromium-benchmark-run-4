5f40c7af90d808aca1aa8f40da51a77c45e40a12
Xi Han <hanxi@google.com>
2026-08-06 16:19:21 -0700

    [Aurora] Adjust top padding of logo and doodle on landscape mode.
    In thi CL:
    1. Remove the 4dp top padding for Google Logo with MEDIUM and LARGE
       padding style on phones;
    2. Add landscape dimens for all types of ntp_logo_margin_top* and set
       to 12dp;
    3. Add landscape dimens for all types of doodle_margin_top* and set
       to 8dp;
    4. In NewTabPageCoordinator, add an observer of onDisplayStyleChanged
       on phones, and update LogoView's top margin when screen rotation
       changes.
    Screenshots: http://shortn/_Rp18LBPqDW.
    
    Bug: 534357676
    Change-Id: Ib2efaccdd2ac2f6985943572497783e8343df0f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8215829
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675372}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
@@ -370,7 +370,9 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
         mContextMenuStartPosition =
                 ReturnToChromeUtil.calculateContextMenuStartPosition(mActivity.getResources());
 
-        if (mIsLff) {
+        if (mIsLff
+                || NewTabPageUtils.getPaddingStyleForAurora()
+                        != NewTabPageUtils.PaddingStyle.DEFAULT) {
             mDisplayStyleObserver = this::onDisplayStyleChanged;
             mUiConfig.addObserver(mDisplayStyleObserver);
         } else {
@@ -973,6 +975,10 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
             mNtpSearchBox.setTopMargin(topMargin);
         }
 
+        setLogoTopMargin();
+    }
+
+    void setLogoTopMargin() {
         if (mLogoCoordinator != null) {
             mLogoCoordinator.setTopMargin(getLogoTopMargin());
         }
@@ -1450,7 +1456,12 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
     }
 
     private void onDisplayStyleChanged(UiConfig.DisplayStyle newDisplayStyle) {
-        if (!mIsLff) return;
+        if (!mIsLff) {
+            // Logo and Doodle have different top margin on landscape and portrait modes on phones,
+            // update when the screen rotates.
+            setLogoTopMargin();
+            return;
+        }
 
         updateDoodleOnTablet();
         updateSearchBoxTwoSideMargin();
--- chrome/browser/ui/android/logo/java/src/org/chromium/chrome/browser/logo/LogoProperties.java
+++ chrome/browser/ui/android/logo/java/src/org/chromium/chrome/browser/logo/LogoProperties.java
@@ -25,6 +25,7 @@ interface LogoProperties {
     //  if we're not going to read the value out of this in the ViewBinder.
     WritableFloatPropertyKey ALPHA = new WritableFloatPropertyKey();
     WritableIntPropertyKey LOGO_TOP_MARGIN = new WritableIntPropertyKey();
+    WritableIntPropertyKey LOGO_TOP_PADDING = new WritableIntPropertyKey();
     WritableIntPropertyKey LOGO_BOTTOM_MARGIN = new WritableIntPropertyKey();
     WritableIntPropertyKey LOGO_HEIGHT = new WritableIntPropertyKey();
     WritableObjectPropertyKey<Boolean> SET_END_FADE_ANIMATION =
@@ -59,6 +60,7 @@ interface LogoProperties {
             new PropertyKey[] {
                 ALPHA,
                 LOGO_TOP_MARGIN,
+                LOGO_TOP_PADDING,
                 LOGO_BOTTOM_MARGIN,
                 LOGO_HEIGHT,
                 SET_END_FADE_ANIMATION,
