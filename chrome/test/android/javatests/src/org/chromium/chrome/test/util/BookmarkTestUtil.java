507b08510eea5a453731d2910d2f340cb810eb54
Brandon Wylie <wylieb@google.com>
2026-08-06 14:49:16 -0700

    [bookmarks] Create new searchbox layout and insert it for desktop
    - Create a new desktop searchbox layout to customize the look.
    - Insert the searchbox above the selectable list layout per specs.
    
    Bug: 540124932
    Change-Id: I64ac15bac848cd186912b1558fbd9144172f6bab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161047
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675307}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/BookmarkTestUtil.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/BookmarkTestUtil.java
@@ -14,6 +14,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.ViewInteraction;
@@ -167,7 +168,10 @@ public class BookmarkTestUtil {
                         isDescendantOfA(
                                 allOf(
                                         withId(R.id.search_view),
-                                        isDescendantOfA(withId(R.id.bookmark_toolbar))))));
+                                        anyOf(
+                                                isDescendantOfA(withId(R.id.bookmark_toolbar)),
+                                                isDescendantOfA(
+                                                        withId(R.id.desktop_search_box_row)))))));
     }
 
     public static ChromeTabbedActivity waitForTabbedActivity() {
