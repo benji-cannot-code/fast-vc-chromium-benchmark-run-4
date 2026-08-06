f2a9ff6cecca202950d39b989797716de1e007ed
James Leung <jamesleung@google.com>
2026-08-06 13:49:39 -0700

    [voice_search][PEPC] Remove ignore-focus-events flag in other code path
    * Currently, there is a flag (when true) that blocks focus-loss
      events when permissin prompt is open. This allows omnibox to
      stay open when permission prompt is open.
    * However, the flag is not set to false (reset) when the omnibox is
      closed via `OmniboxPopupFullPresenter::DeactivatePopupAndKillFocus`
      function.
    * Add helper function to reset flag.
    * Add tests to verify this + other code path that blocks
      focus events added in this CL: crrev.com/c/8125677
    
    Before: screen/1l69qj7bhi41o
    
    After: screencast/NDY4MjA0MTY4NDY1NjEyOHwzNjA2MWNiNy05YQ
    
    Bug: b:542726148
    Change-Id: Iee8c2f324930004e0f9e8c1f0a57642e23bf959c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8189503
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675268}

--- chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
+++ chrome/browser/ui/views/omnibox/omnibox_popup_presenter_base.h
@@ -154,6 +154,9 @@ class OmniboxPopupPresenterBase
   // handlers.
   void SetPermissionPromptShowing(bool showing);
 
+  // Resets prompt showing and dismissal state flags.
+  void ResetPermissionPromptShowingState();
+
   // Handles common dismissal state updates when a permission prompt is closed.
   void HandlePermissionPromptDismissal();
 
