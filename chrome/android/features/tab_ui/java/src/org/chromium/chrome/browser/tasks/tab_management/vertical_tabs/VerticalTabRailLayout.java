49bbbc4b007d254d15d75741bc5274a8d8bbd33d
Joel Tan-Aristy <jtanaristy@google.com>
2026-08-06 16:14:19 -0700

    [Vertical Tabs] Updates vertical tabs sizing for larger touch targets
    Updates height for buttons, pinned tab buttons, and tabs to meet
    tablet touch targets.
    
    Updates tab touch targets for tablets as well.
    
    Before:
    https://screenshot-v2.corp.google.com/5vik8b6199dr8
    
    After:
    https://screenshot-v2.corp.google.com/607arrc92uu8o
    
    Bug: 534878291
    Change-Id: I391bd5fe32b09c94f88fec8ccfe2caee6660b6bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8192215
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Auto-Submit: Joel Tan-Aristy <jtanaristy@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675369}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabRailLayout.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabRailLayout.java
@@ -88,6 +88,12 @@ public class VerticalTabRailLayout extends ConstraintLayout {
         assert mNewTabButton != null;
         TooltipCompat.setTooltipText(
                 mNewTabButton, getContext().getString(R.string.accessibility_toolbar_btn_new_tab));
+
+        updateButtonSizes();
+    }
+
+    private void updateButtonSizes() {
+        VerticalTabListViewBinder.updateButtonSizes(this);
     }
 
     /** Returns the main tab list recycler view. */
@@ -189,12 +195,16 @@ public class VerticalTabRailLayout extends ConstraintLayout {
         boolean isCollapsed = railCollapseState == RailCollapseState.COLLAPSED;
         boolean isManuallyExpanded = railCollapseState == RailCollapseState.EXPANDED;
         Resources res = getResources();
+        int buttonSize = res.getDimensionPixelSize(R.dimen.vertical_tabs_header_button_size);
+        int newTabHeight = res.getDimensionPixelSize(R.dimen.vertical_tabs_new_tab_button_height);
 
         mHeaderContainer.setOrientation(
                 isCollapsed ? LinearLayout.VERTICAL : LinearLayout.HORIZONTAL);
         mHeaderContainer.setGravity(isCollapsed ? Gravity.CENTER_HORIZONTAL : Gravity.NO_GRAVITY);
 
         var collapseParams = (ViewGroup.MarginLayoutParams) mCollapseButton.getLayoutParams();
+        collapseParams.width = buttonSize;
+        collapseParams.height = buttonSize;
         collapseParams.setMarginEnd(
                 isCollapsed
                         ? 0
@@ -219,6 +229,8 @@ public class VerticalTabRailLayout extends ConstraintLayout {
 
         int gap = res.getDimensionPixelSize(R.dimen.vertical_tabs_header_button_gap);
         var gridParams = (ViewGroup.MarginLayoutParams) mGridButton.getLayoutParams();
+        gridParams.width = buttonSize;
+        gridParams.height = buttonSize;
         gridParams.setMarginEnd(isCollapsed ? 0 : gap);
         gridParams.bottomMargin = isCollapsed ? gap : 0;
         mGridButton.setLayoutParams(gridParams);
@@ -227,6 +239,10 @@ public class VerticalTabRailLayout extends ConstraintLayout {
                         ? R.drawable.vertical_tabs_top_rounded_button_background
                         : R.drawable.vertical_tabs_left_rounded_button_background);
 
+        var searchParams = (ViewGroup.MarginLayoutParams) mSearchButton.getLayoutParams();
+        searchParams.width = buttonSize;
+        searchParams.height = buttonSize;
+        mSearchButton.setLayoutParams(searchParams);
         mSearchButton.setBackgroundResource(
                 isCollapsed
                         ? R.drawable.vertical_tabs_bottom_rounded_button_background
@@ -235,14 +251,8 @@ public class VerticalTabRailLayout extends ConstraintLayout {
         mHeaderSpacer.setVisibility(isCollapsed ? View.GONE : View.VISIBLE);
 
         var newTabParams = mNewTabButton.getLayoutParams();
-        newTabParams.width =
-                isCollapsed
-                        ? res.getDimensionPixelSize(R.dimen.vertical_tabs_header_button_size)
-                        : ViewGroup.LayoutParams.MATCH_PARENT;
-        newTabParams.height =
-                isCollapsed
-                        ? res.getDimensionPixelSize(R.dimen.vertical_tabs_header_button_size)
-                        : res.getDimensionPixelSize(R.dimen.vertical_tabs_new_tab_button_height);
+        newTabParams.width = isCollapsed ? buttonSize : ViewGroup.LayoutParams.MATCH_PARENT;
+        newTabParams.height = isCollapsed ? buttonSize : newTabHeight;
         mNewTabButton.setLayoutParams(newTabParams);
     }
 }
