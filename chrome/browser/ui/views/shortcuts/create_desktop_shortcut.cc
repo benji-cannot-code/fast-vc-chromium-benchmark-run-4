0bacc8110f06dcdcf68898a635d44d05958af50f
Bryan Kim <bryankim@google.com>
2026-08-06 11:14:44 -0700

    Componentize chrome/browser/ui/views/shortcuts/
    Extract shortcuts sources from //chrome/browser/ui into
    //chrome/browser/ui/views/shortcuts and
    //chrome/browser/ui/views/shortcuts:impl. Test support fakes and test
    files remain in the target and will be moved/cleaned up in a follow-up.
    
    Bug: 353332589
    TAG=agy
    CONV=63ba6e15-f154-46a5-9c93-3e95dc0bde40
    
    Change-Id: Ib4f73586dcf3195c889139e6de4480591cb54f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8193114
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675126}

--- chrome/browser/ui/views/shortcuts/create_desktop_shortcut.cc
+++ chrome/browser/ui/views/shortcuts/create_desktop_shortcut.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/dialogs/browser_dialogs.h"
 #include "chrome/browser/ui/views/controls/site_icon_text_and_origin_view.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/shortcuts/create_desktop_shortcut_delegate.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
