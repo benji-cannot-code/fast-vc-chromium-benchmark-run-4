4bf11a95dc365f2ada181249c3ef98a1a526ce81
rosiey <rosiey@google.com>
2026-08-06 10:16:51 -0700

    [Actor] Fix low contrast handoff button icon & background in dark mode
    * Register kColorActorUiHandoffButtonBackground and
      kColorActorUiHandoffButtonForeground in chrome_color_id.h.
    
    * Map kColorActorUiHandoffButtonBackground to ui::kColorSysSurface2 and
      kColorActorUiHandoffButtonForeground to ui::kColorSysOnSurface in
      chrome_color_mixer.cc and material_chrome_color_mixer.cc.
    
    * Update HandoffButtonController to use new color IDs for button
      background, label text, and vector icon.
    
    * Add unit test ColorTokensHaveSufficientContrast in
      handoff_button_controller_unittest.cc to ensure contrast ratio
      is >= 3.0f.
    
    Result: http://shortn/_3MwvhAJEPJ
    
    http://shortn/_rb5nRO3k4d
    
    Bug: b:539636183
    Change-Id: Ife22dba1c036acf5b3fbc37e827142804da3a311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8206145
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675069}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -30,7 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorAppMenuYourChromeBackground) \
   E_CPONLY(kColorAppMenuToolsAndActionsBackground) \
   /* Actor UI colors.*/ \
+  E_CPONLY(kColorActorUiHandoffButtonBackground) \
   E_CPONLY(kColorActorUiHandoffButtonBorder) \
+  E_CPONLY(kColorActorUiHandoffButtonForeground) \
   E_CPONLY(kColorActorUiOverlayBorder) \
   E_CPONLY(kColorActorUiOverlayBorderGlow) \
   E_CPONLY(kColorActorUiScrimStart) \
