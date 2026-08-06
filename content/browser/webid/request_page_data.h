f59c916a1478ad9f6e53360891bedcce81c212c3
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-08-06 07:49:57 -0700

    Fix some STL IWYU in //content
    Bug: 542921651
    Change-Id: Iab4323df3e88dc0e45a0b96fa87ca065b37a4bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8203986
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674970}

--- content/browser/webid/request_page_data.h
+++ content/browser/webid/request_page_data.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_WEBID_REQUEST_PAGE_DATA_H_
 #define CONTENT_BROWSER_WEBID_REQUEST_PAGE_DATA_H_
 
-#include <memory>
-
 #include "base/time/time.h"
 #include "content/public/browser/page_user_data.h"
 #include "url/gurl.h"
--- content/public/browser/cdm_registry.h
+++ content/public/browser/cdm_registry.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_CDM_REGISTRY_H_
 #define CONTENT_PUBLIC_BROWSER_CDM_REGISTRY_H_
 
-#include <vector>
-
 #include "content/common/content_export.h"
 #include "content/public/common/cdm_info.h"
 
