433c08210982a13ddd48c9c99bf1f277363a8245
Victor Hugo Vianna Silva <victorvianna@google.com>
2026-08-06 10:45:35 -0700

    Fix some STL IWYU in //remoting
    Bug: 542921651
    Change-Id: I96d74b5d6a84b212bce68e6d905f4e26e5ca2646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8199830
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675090}

--- remoting/host/mojo_caller_security_checker.cc
+++ remoting/host/mojo_caller_security_checker.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/mojo_caller_security_checker.h"
 
 #include <memory>
+#include <string>
 
 #include "base/containers/fixed_flat_set.h"
 #include "base/files/file_path.h"
