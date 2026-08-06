c4c8e8a5dda21d402f04044d4d91b0f633de2907
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-08-06 05:44:56 -0700

    [Cleanup] Remove unnecessary if statement
    Bug: none
    Change-Id: Ia66a3ce24ba333bf32e720e53d0aac58898e536f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8207241
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674894}

--- chrome/browser/ui/infobars/browser_infobar_registry.cc
+++ chrome/browser/ui/infobars/browser_infobar_registry.cc
@@ -60,10 +60,9 @@ void RegisterInfoBars() {
             .AddOkButton(
                 l10n_util::GetStringUTF16(IDS_COLLECTED_COOKIES_INFOBAR_BUTTON),
                 base::BindRepeating([](content::WebContents* web_contents) {
-                  if (web_contents) {
-                    web_contents->GetController().Reload(
-                        content::ReloadType::NORMAL, true);
-                  }
+                  CHECK(web_contents);
+                  web_contents->GetController().Reload(
+                      content::ReloadType::NORMAL, true);
                 }))
             .Build();
     browser_infobar_manager->Register(std::move(spec));
@@ -112,12 +111,11 @@ void RegisterInfoBars() {
             .SetExpireOnNavigation(false)
             .SetDismissAction(
                 base::BindRepeating([](content::WebContents* web_contents) {
-                  if (web_contents) {
-                    Profile* profile = Profile::FromBrowserContext(
-                        web_contents->GetBrowserContext());
-                    KnownInterceptionDisclosureCooldown::GetInstance()
-                        ->Activate(profile);
-                  }
+                  CHECK(web_contents);
+                  Profile* profile = Profile::FromBrowserContext(
+                      web_contents->GetBrowserContext());
+                  KnownInterceptionDisclosureCooldown::GetInstance()->Activate(
+                      profile);
                 }))
             .Build();
     browser_infobar_manager->Register(std::move(spec));
