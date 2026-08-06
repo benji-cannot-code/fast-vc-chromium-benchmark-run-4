2fe5e8a2efcf5626159b24102b42bb48a012a4e5
dpapad <dpapad@chromium.org>
2026-08-06 12:55:00 -0700

    Settings Lit: Migrate languages_page/, part 1.
    In this part removing usage of iron-list and
    settings-checkbox-list-entry from add_languages_dialog.ts, in favor of a
    simpler dom-repeat + 'focusgroup' + plain cr-checkbox approach, in
    preparation of migrating to Lit.
    
    This is similar to crrev.com/c/8170010 which updated
    performance_page/tab_discard/exception_current_sites_list.ts in a
    similar way.
    
    Also, removing controls/settings_checkbox_list_entry.ts since there are
    no more usages of it in the codebase.
    
    Bug: 393471368
    Change-Id: Ic61f069bbf72a8fe1b84619a2cb3ebb2ecc66634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8212259
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675215}

--- chrome/browser/resources/settings/languages_page/add_languages_dialog.ts
+++ chrome/browser/resources/settings/languages_page/add_languages_dialog.ts
@@ -8,20 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * languages.
  */
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
+import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
 import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import 'chrome://resources/cr_elements/cr_search_field/cr_search_field.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
-import 'chrome://resources/polymer/v3_0/iron-list/iron-list.js';
-import '../controls/settings_checkbox_list_entry.js';
 import '../settings_shared.css.js';
 
+import type {CrCheckboxElement} from 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
 import type {CrDialogElement} from 'chrome://resources/cr_elements/cr_dialog/cr_dialog.js';
 import type {CrSearchFieldElement} from 'chrome://resources/cr_elements/cr_search_field/cr_search_field.js';
 import {FindShortcutMixin} from 'chrome://resources/cr_elements/find_shortcut_mixin.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import type {DomRepeatEvent} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import type {SettingsCheckboxListEntryElement} from '../controls/settings_checkbox_list_entry.js';
 import {ScrollableMixin} from '../scrollable_mixin.js';
 
 import {getTemplate} from './add_languages_dialog.html.js';
@@ -30,14 +30,8 @@ import {getFullName} from './languages_util.js';
 export interface SettingsAddLanguagesDialogElement {
   $: {
     dialog: CrDialogElement,
+    list: HTMLElement,
     search: CrSearchFieldElement,
-  };
-}
-
-interface Repeaterevent extends Event {
-  target: SettingsCheckboxListEntryElement;
-  model: {
-    item: chrome.languageSettingsPrivate.Language,
   };
 }
 
@@ -85,6 +79,13 @@ export class SettingsAddLanguagesDialogElement extends
   override connectedCallback() {
     super.connectedCallback();
 
+    // TODO(crbug.com/540914692): Workaround for Blink bug, by resetting
+    // focusgroup attribute restores FocusgroupData that was wiped out during
+    // detachment/attachment. Can probably remove this after migrating to Lit,
+    // since detachment/attachment happens due to the parent chain using dom-if.
+    this.$.list.setAttribute(
+        'focusgroup', this.$.list.getAttribute('focusgroup')!);
+
     this.$.dialog.showModal();
   }
 
@@ -128,11 +129,6 @@ export class SettingsAddLanguagesDialogElement extends
     return this.getLanguages_().length;
   }
 
-  /** @return A 1-based index for aria-posinset. */
-  private getAriaPosinset_(index: number): number {
-    return index + 1;
-  }
-
   private getDisplayText_(language: chrome.languageSettingsPrivate.Language):
       string {
     return getFullName(language);
@@ -147,13 +143,11 @@ export class SettingsAddLanguagesDialogElement extends
   }
 
   /** Handler for checking or unchecking a language item. */
-  private onLanguageCheckboxChange_(e: Repeaterevent) {
-    // Add or remove the item to the Set. No need to worry about data binding:
-    // willAdd_ is called to initialize the checkbox state (in case the
-    // iron-list re-uses a previous checkbox), and the checkbox can only be
-    // changed after that by user action.
+  private onLanguageCheckboxChange_(
+      e: DomRepeatEvent<chrome.languageSettingsPrivate.Language>) {
+    const checkbox = e.target as CrCheckboxElement;
     const language = e.model.item;
-    if (e.target.checked) {
+    if (checkbox.checked) {
       this.languagesToAdd_.add(language.code);
     } else {
       this.languagesToAdd_.delete(language.code);
