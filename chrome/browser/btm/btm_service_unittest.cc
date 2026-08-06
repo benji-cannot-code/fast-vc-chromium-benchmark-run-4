db2f768fee4244c2d5aba96cb47aea0b87d31a94
Kalvin Lee <kdlee@chromium.org>
2026-08-05 22:49:13 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    ['chrome/browser/btm', 'content/browser/btm']
    
    This CL was uploaded by git cl split.
    
    R=njeunje@chromium.org
    
    Bug: 529948677
    Change-Id: Ia4701bce67d06ae3c714044d30e4f0ad7b9da38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030294
    Reviewed-by: Jonathan Njeunje <njeunje@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674734}

--- chrome/browser/btm/btm_service_unittest.cc
+++ chrome/browser/btm/btm_service_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/btm_service.h"
 
+#include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
