b304d6b6419cda3cafe679850e3528f1807946db
Atalyk Akash <atalyk@google.com>
2026-08-06 03:24:04 -0700

    [AtMemory][Android] Fix bottom sheet measurement
    When requestShowContent is called before setting suggestions the bottom
    sheet locks the bottom sheet height to 0, which in some cases leads to
    empty recycle view (see before demo). Set suggestions make sure that the
    recycle view has data when the bottom sheet calculates the height.
    
    Before: screen/67up8t62tv7s8
    After: screen/5hff4dhu4qd2g
    Bug: 540736478
    Change-Id: I83b34e9187f6146d9de9692e6f69ea8d5d828397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8214219
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Atalyk Akash <atalyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674842}

--- chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetCoordinator.java
+++ chrome/browser/ui/android/autofill/internal/java/src/org/chromium/chrome/browser/ui/autofill/AtMemoryBottomSheetCoordinator.java
@@ -79,8 +79,8 @@ public class AtMemoryBottomSheetCoordinator {
 
     public void show(List<AutofillSuggestion> suggestions) {
         mBottomSheetController.addObserver(mBottomSheetObserver);
+        mMediator.show(suggestions);
         if (mBottomSheetController.requestShowContent(mContent, /* animate= */ true)) {
-            mMediator.show(suggestions);
             expand(/* expandInFullHeight= */ true);
         } else {
             onDismissed();
