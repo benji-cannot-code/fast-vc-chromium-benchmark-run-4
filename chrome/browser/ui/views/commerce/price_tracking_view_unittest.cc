ad705a6cdfa5e7a9a68658527be20f126b459f96
Wenbin Zhang <wenbinzhang@google.com>
2026-08-06 11:06:20 -0700

    [bedrock] Migrate price_tracking_view_unittest to ChromeViewsTestBase
    Migrate PriceTrackingViewTest away from BrowserWithTestWindowTest
    to ChromeViewsTestBase as part of Project Bedrock.
    
    WHAT WAS DONE:
    - Converted PriceTrackingViewTest from BrowserWithTestWindowTest
      to ChromeViewsTestBase.
    - Used AddProductInfoToExistingBookmark in SetUpDependencies to attach
      product info directly to the bookmark, avoiding timestamp ties in
      GetMostRecentlyAddedUserNodeForURL between duplicate nodes.
    
    TAG=agy
    CONV=0e2f8f65-9d3c-4886-8bb3-c908911c4a8c
    
    Bug: 407530310
    Change-Id: I29ca31134ac9081d40c7c0368698ca7691e12cb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8175961
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675117}

--- chrome/browser/ui/views/commerce/price_tracking_view_unittest.cc
+++ chrome/browser/ui/views/commerce/price_tracking_view_unittest.cc
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/commerce/shopping_service_factory.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/browser/sync/local_or_syncable_bookmark_sync_service_factory.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/testing_profile.h"
 #include "chrome/test/views/chrome_test_widget.h"
+#include "chrome/test/views/chrome_views_test_base.h"
 #include "components/bookmarks/browser/bookmark_utils.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/commerce/core/commerce_feature_list.h"
@@ -36,10 +37,24 @@ const char kTestURL[] = "about:blank";
 const uint64_t kProductClusterId = 12345L;
 }  // namespace
 
-class PriceTrackingViewTest : public BrowserWithTestWindowTest {
+class PriceTrackingViewTest : public ChromeViewsTestBase {
  public:
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
+    ChromeViewsTestBase::SetUp();
+
+    TestingProfile::Builder profile_builder;
+    profile_builder.AddTestingFactories(
+        IdentityTestEnvironmentProfileAdaptor::
+            GetIdentityTestEnvironmentFactoriesWithAppendedFactories(
+                {TestingProfile::TestingFactory{
+                     BookmarkModelFactory::GetInstance(),
+                     BookmarkModelFactory::GetDefaultFactory()},
+                 TestingProfile::TestingFactory{
+                     commerce::ShoppingServiceFactory::GetInstance(),
+                     base::BindRepeating([](content::BrowserContext* context) {
+                       return commerce::MockShoppingService::Build();
+                     })}}));
+    profile_ = profile_builder.Build();
 
     anchor_widget_ = std::make_unique<ChromeTestWidget>();
     views::Widget::InitParams widget_params(
@@ -52,22 +67,12 @@ class PriceTrackingViewTest : public BrowserWithTestWindowTest {
   void TearDown() override {
     price_tracking_view_ = nullptr;
     anchor_widget_.reset();
+    profile_.reset();
 
-    BrowserWithTestWindowTest::TearDown();
+    ChromeViewsTestBase::TearDown();
   }
 
-  TestingProfile::TestingFactories GetTestingFactories() override {
-    return IdentityTestEnvironmentProfileAdaptor::
-        GetIdentityTestEnvironmentFactoriesWithAppendedFactories(
-            {TestingProfile::TestingFactory{
-                 BookmarkModelFactory::GetInstance(),
-                 BookmarkModelFactory::GetDefaultFactory()},
-             TestingProfile::TestingFactory{
-                 commerce::ShoppingServiceFactory::GetInstance(),
-                 base::BindRepeating([](content::BrowserContext* context) {
-                   return commerce::MockShoppingService::Build();
-                 })}});
-  }
+  Profile* profile() { return profile_.get(); }
 
   void SetUpDependencies() {
     bookmarks::BookmarkModel* bookmark_model =
@@ -78,16 +83,14 @@ class PriceTrackingViewTest : public BrowserWithTestWindowTest {
     LocalOrSyncableBookmarkSyncServiceFactory::GetForProfile(profile())
         ->SetIsTrackingMetadataForTesting();
 
-    bookmarks::AddIfNotBookmarked(bookmark_model, GURL(kTestURL),
-                                  std::u16string());
+    const bookmarks::BookmarkNode* node = bookmarks::AddIfNotBookmarked(
+        bookmark_model, GURL(kTestURL), std::u16string());
 
-    commerce::AddProductBookmark(bookmark_model, u"title", GURL(kTestURL), 0,
-                                 false);
+    commerce::AddProductInfoToExistingBookmark(bookmark_model, node, u"title",
+                                               0);
   }
 
   PriceTrackingView* CreateViewAndShow(bool is_price_track_enabled) {
-    SkBitmap bitmap;
-    bitmap.allocN32Pixels(1, 1);
     commerce::ProductInfo info;
     info.product_cluster_id.emplace(kProductClusterId);
     auto price_tracking_View = std::make_unique<PriceTrackingView>(
@@ -135,6 +138,7 @@ class PriceTrackingViewTest : public BrowserWithTestWindowTest {
   raw_ptr<PriceTrackingView> price_tracking_view_;
 
  private:
+  std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<views::Widget> anchor_widget_;
 };
 
