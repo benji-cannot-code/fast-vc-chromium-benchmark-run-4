60c3c3c4c132320e20a5e123d621d0655d80bfae
Lily Balas <lilybalas@google.com>
2026-08-06 07:37:37 -0700

    [ios] Omnibox paste flow non-modal default experiments changing strings.
    This is a first set of small string experiments for non-modal default promos as listed here: https://docs.google.com/document/d/1Qw9PQsjRyrDc8afjZ3fkEBGJbSCTEPT7_1SIw9BtGic/edit?resourcekey=0-Cl5o_2VTNkG9qnzYQqRLSQ&tab=t.0#heading=h.ol5gtodp071y.
    
    Change-Id: I2086c46239891ac96c7d9c230d8de8ab7ccfb32c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8065294
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Lily Balas <lilybalas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674959}

--- ios/chrome/browser/default_browser/model/features.h
+++ ios/chrome/browser/default_browser/model/features.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_DEFAULT_BROWSER_MODEL_FEATURES_H_
 
 #import "base/feature_list.h"
+#import "base/metrics/field_trial_params.h"
 
 // Feature to enable sharing default browser status with 1P apps.
 BASE_DECLARE_FEATURE(kShareDefaultBrowserStatus);
@@ -33,4 +34,9 @@ bool IsIOSSettingsDefaultBrowserPromoV2Enabled();
 // parameters.
 SettingsDefaultBrowserPromoType CurrentSettingsDefaultBrowserPromoType();
 
+// Feature to enable the Omnibox Paste Flow copy experiments.
+BASE_DECLARE_FEATURE(kOmniboxPastePromoExperiment);
+
+// Returns the active experiment arm (1 through 10).
+int GetOmniboxPastePromoExperimentType();
 #endif  // IOS_CHROME_BROWSER_DEFAULT_BROWSER_MODEL_FEATURES_H_
