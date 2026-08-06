aeb39204af042d374c6020ea15b33a5543d4a61a
Gazal <agazal@google.com>
2026-08-05 18:06:10 -0700

    [Code Health] Replace anonymous classes with lambdas in Webapps
    Convert Java anonymous classes implementing SAM (Single Abstract Method)
    interfaces to lambdas. This allows R8 to perform Lambda Grouping and
    merge them, reducing overall DEX/binary size.
    
    Bug: 541671762
    Change-Id: I5039e7d0d25531c485e44fd39b3e3054e3e8685c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8192307
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674657}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappLauncherActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappLauncherActivity.java
@@ -421,12 +421,6 @@ public class WebappLauncherActivity extends Activity {
     private static void launchAfterDelay(Context appContext, Intent intent, int launchDelayMs) {
         new Handler()
                 .postDelayed(
-                        new Runnable() {
-                            @Override
-                            public void run() {
-                                IntentUtils.safeStartActivity(appContext, intent);
-                            }
-                        },
-                        launchDelayMs);
+                        () -> IntentUtils.safeStartActivity(appContext, intent), launchDelayMs);
     }
 }
