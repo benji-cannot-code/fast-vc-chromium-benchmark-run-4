d6d1eb6caf352a3b967de8a094a00f8c19d38de2
Peter E Conn <peconn@google.com>
2026-08-06 08:32:29 -0700

    Revert "Block prefetch requests for disallowed URLs"
    This reverts commit 9fcd98e4172070b8a8f224ca3d7723f97c54bc01.
    
    Reason for revert: this introduced a performance regression in WebView prefetch.
    
    Bug: 543291702
    
    Original change's description:
    > Block prefetch requests for disallowed URLs
    >
    > It used to be possible to prefetch URLs that otherwise should be
    > blocked. This was not rendering the content, however we can also block
    > the prefetch network request.
    >
    > This CL fixes the issue by intercepting prefetch requests in
    > AwProxyingURLLoaderFactory and checking them against the
    > AwSupervisedUserUrlClassifier. If the URL is disallowed, the
    > prefetch request is blocked with net::ERR_BLOCKED_BY_CLIENT.
    >
    > TAG=agy
    > CONV=10d00573-3f3a-4db3-847c-e564fce235fe
    >
    > Fixed: 517534914
    > Test: run_webview_instrumentation_test_apk -f AwSupervisedUserTest#*
    > Change-Id: I9c1de36e419a9a3f79779345031f37cbd8e805d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7966058
    > Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    > Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1653573}
    
    Bug: 517534914
    Change-Id: I7e8f94556dd740d9d067a957af6722962a38cf09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8214579
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674997}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSupervisedUserTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSupervisedUserTest.java
@@ -50,7 +50,9 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -287,6 +289,7 @@ public class AwSupervisedUserTest extends AwParameterizedTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
+    @Features.DisableFeatures({"Prerender2FallbackPrefetchSpecRules"})
     public void testPrerenderDisallowedSiteIsBlocked() throws Throwable {
         mActivityTestRule
                 .getAwSettingsOnUiThread(mAwContents)
@@ -319,6 +322,7 @@ public class AwSupervisedUserTest extends AwParameterizedTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "crbug.com/543291702")
     public void testPrefetchDisallowedSiteIsBlocked() throws Throwable {
         mActivityTestRule
                 .getAwSettingsOnUiThread(mAwContents)
@@ -355,6 +359,7 @@ public class AwSupervisedUserTest extends AwParameterizedTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "crbug.com/543291702")
     public void testSafeSitesCanBePrefetched() throws Throwable {
         mActivityTestRule
                 .getAwSettingsOnUiThread(mAwContents)
