ec7e8c824f05d0ce6aa02c21ed89b960842849f2
Hubert Chao <hchao@chromium.org>
2026-08-06 12:08:30 -0700

    [LNA] Add use counter for treat-as-public-address CSP header
    Use counter added to support I2D process (see discussion on
    crrev.com/c/8195486)
    
    Bug: 527730705
    Change-Id: I572ea831345909e370c3dec07dd409b8de1171ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8214663
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675176}

--- third_party/blink/renderer/core/frame/csp/content_security_policy.cc
+++ third_party/blink/renderer/core/frame/csp/content_security_policy.cc
@@ -353,6 +353,9 @@ void ContentSecurityPolicy::ReportUseCounters(
     const Vector<network::mojom::blink::ContentSecurityPolicyPtr>& policies) {
   for (const auto& policy : policies) {
     Count(GetUseCounterType(policy->header->type));
+    if (policy->treat_as_public_address) {
+      Count(WebFeature::kLocalNetworkAccessShouldTreatAsPublicAddressCsp);
+    }
     if (CSPDirectiveListAllowDynamic(*policy,
                                      CSPDirectiveName::ScriptSrcAttr) ||
         CSPDirectiveListAllowDynamic(*policy,
