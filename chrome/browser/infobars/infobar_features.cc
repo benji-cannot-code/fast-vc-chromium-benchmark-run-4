c3801eb911042ec98121fbac7b949d4af5c1d188
Amy Hwang <amyhwang@chromium.org>
2026-08-07 09:19:02 -0700

    [Infobar] Migrate local test policies infobar to centralized framework
    This CL migrates the LOCAL_TEST_POLICIES_APPLIED_INFOBAR (shown when
    local test policies are applied via chrome://policy/test) to the
    centralized infobar framework on Desktop.
    
    Key Changes:
    - Registered `LOCAL_TEST_POLICIES_APPLIED_INFOBAR` in the registry with global scope, low priority, and non-closeable.
    - Added `kMigratedLocalTestPolicies` feature parameter under the `kCentralizedInfoBarFramework` feature.
    - Refactored profile_policy_connector.cc to route showing/hiding
      through a new helper UpdateLocalTestInfoBar().
    
    This change is controlled by the `MigratedLocalTestPolicies` parameter
    of the `kCentralizedInfoBarFramework` feature flag. To test it out
    manually,
    
    use: --enable-features=CentralizedInfoBarFramework:MigratedLocalTestPolicies/true
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 530927289
    Change-Id: I6e33f664646666e3e4dd023cb4f1c501f088ed20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8191334
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Amy Hwang <amyhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675744}

--- chrome/browser/infobars/infobar_features.cc
+++ chrome/browser/infobars/infobar_features.cc
@@ -55,6 +55,11 @@ BASE_FEATURE_PARAM(bool,
                    &kCentralizedInfoBarFramework,
                    false);
 
+BASE_FEATURE_PARAM(bool,
+                   kMigratedLocalTestPolicies,
+                   &kCentralizedInfoBarFramework,
+                   false);
+
 const base::FeatureParam<bool>* GetInfoBarMigrationParam(
     InfoBarDelegate::InfoBarIdentifier infobar_id) {
   switch (infobar_id) {
@@ -76,12 +81,17 @@ const base::FeatureParam<bool>* GetInfoBarMigrationParam(
       return &kMigratedObsoleteSystem;
     case InfoBarDelegate::PIN_INFOBAR_DELEGATE:
       return &kMigratedPinInfoBar;
+    case InfoBarDelegate::LOCAL_TEST_POLICIES_APPLIED_INFOBAR:
+      return &kMigratedLocalTestPolicies;
     default:
       return nullptr;
   }
 }
 
 bool IsInfoBarMigrated(InfoBarDelegate::InfoBarIdentifier infobar_id) {
+#if BUILDFLAG(IS_ANDROID)
+  return false;
+#else
   if (!base::FeatureList::IsEnabled(kCentralizedInfoBarFramework)) {
     return false;
   }
@@ -96,6 +106,7 @@ bool IsInfoBarMigrated(InfoBarDelegate::InfoBarIdentifier infobar_id) {
   }
 
   return param->Get();
+#endif
 }
 
 }  // namespace infobars
