babf83db6f0cbf82e8b59deff3890194e9c7b1f4
Allen Bauer <kylixrd@chromium.org>
2026-08-07 12:13:24 -0700

    [ScrollView] Expose the UseContentsPreferredSize property
    This will make the ScrollView more useful when using the JSON View
    Builder. It also will now appear in the ui-devtools inspector.
    
    Change-Id: I7532dd83d2cd6e1cb2234b2ffad6a0b44e84fd28
    Bug: b/529761853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8221945
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675876}

--- ui/views/controls/scroll_view.cc
+++ ui/views/controls/scroll_view.cc
@@ -1624,6 +1624,18 @@ View* ScrollView::GetContentsViewportForTest() const {
   return contents_viewport_;
 }
 
+bool ScrollView::GetUseContentsPreferredSize() const {
+  return use_contents_preferred_size_;
+}
+
+void ScrollView::SetUseContentsPreferredSize(bool use_contents_preferred_size) {
+  if (use_contents_preferred_size == use_contents_preferred_size_) {
+    return;
+  }
+  use_contents_preferred_size_ = use_contents_preferred_size;
+  OnPropertyChanged(&use_contents_preferred_size_, PropertyEffects::kLayout);
+}
+
 BEGIN_METADATA(ScrollView)
 ADD_READONLY_PROPERTY_METADATA(int, MinHeight)
 ADD_READONLY_PROPERTY_METADATA(int, MaxHeight)
@@ -1634,6 +1646,7 @@ ADD_PROPERTY_METADATA(bool, HasFocusIndicator)
 ADD_PROPERTY_METADATA(ScrollView::ScrollBarMode, HorizontalScrollBarMode)
 ADD_PROPERTY_METADATA(ScrollView::ScrollBarMode, VerticalScrollBarMode)
 ADD_PROPERTY_METADATA(bool, TreatAllScrollEventsAsHorizontal)
+ADD_PROPERTY_METADATA(bool, UseContentsPreferredSize)
 END_METADATA
 
 // VariableRowHeightScrollHelper ----------------------------------------------
