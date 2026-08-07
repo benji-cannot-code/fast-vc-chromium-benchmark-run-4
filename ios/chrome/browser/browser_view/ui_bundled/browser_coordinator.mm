d9c95ba40b0bf043e5d6809c2cfc89fb193da97b
James Sun <jamessun@google.com>
2026-08-06 18:14:22 -0700

    [PDT][Bling] Present Wallet Reminder Notice Bottom Sheet from BrowserCoordinator
    Implement showWalletReminderNoticeOnOriginWebState and
    dismissWalletReminderNotice in BrowserCoordinator to create, start, and
    stop WalletReminderNoticeCoordinator.
    
    Bug: 543567232
    Change-Id: I8e9dfec6ab6a162873165017200a3a58ba290bf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8204087
    Commit-Queue: James Sun <jamessun@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675446}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_coordinator.h"
 #import "ios/chrome/browser/autofill/ui_bundled/error_dialog/autofill_error_dialog_coordinator.h"
 #import "ios/chrome/browser/autofill/ui_bundled/progress_dialog/autofill_progress_dialog_coordinator.h"
+#import "ios/chrome/browser/autofill/wallet_reminder_notice/coordinator/wallet_reminder_notice_coordinator.h"
 #import "ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_coordinator.h"
 #import "ios/chrome/browser/browser_content/model/edit_menu_builder.h"
 #import "ios/chrome/browser/browser_content/ui_bundled/browser_content_coordinator.h"
@@ -527,6 +528,9 @@ const char kChromeAppStoreUrl[] =
 
 @property(nonatomic, strong) VirtualCardEnrollmentBottomSheetCoordinator*
     virtualCardEnrollmentBottomSheetCoordinator;
+
+@property(nonatomic, strong)
+    WalletReminderNoticeCoordinator* walletReminderNoticeCoordinator;
 
 // Coordinator for the choice screen.
 @property(nonatomic, strong) ChromeCoordinator* choiceCoordinator;
@@ -1767,6 +1771,7 @@ const char kChromeAppStoreUrl[] =
   self.cardUnmaskAuthenticationCoordinator = nil;
 
   [self dismissSaveCardBottomSheet];
+  [self dismissWalletReminderNotice];
 
   [self.virtualCardEnrollmentBottomSheetCoordinator stop];
   self.virtualCardEnrollmentBottomSheetCoordinator = nil;
@@ -2277,6 +2282,30 @@ const char kChromeAppStoreUrl[] =
 - (void)dismissSaveCardBottomSheet {
   [self.saveCardBottomSheetCoordinator stop];
   self.saveCardBottomSheetCoordinator = nil;
+}
+
+- (void)showWalletReminderNoticeOnOriginWebState:(web::WebState*)originWebState
+                               legalMessageLines:(autofill::LegalMessageLines)
+                                                     legalMessageLines {
+  if (self.walletReminderNoticeCoordinator) {
+    [self.walletReminderNoticeCoordinator stop];
+  }
+
+  if (self.activeWebState != originWebState) {
+    return;
+  }
+
+  self.walletReminderNoticeCoordinator =
+      [[WalletReminderNoticeCoordinator alloc]
+          initWithBaseViewController:self.viewController
+                             browser:self.browser
+                   legalMessageLines:legalMessageLines];
+  [self.walletReminderNoticeCoordinator start];
+}
+
+- (void)dismissWalletReminderNotice {
+  [self.walletReminderNoticeCoordinator stop];
+  self.walletReminderNoticeCoordinator = nil;
 }
 
 - (void)showVirtualCardEnrollmentBottomSheet:
