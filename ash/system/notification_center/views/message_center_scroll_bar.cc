ca1aff9bfdbe3dc6cd6460c2cdd101cca1e742e5
Zack Han <zackhan@chromium.org>
2026-08-07 16:10:04 -0700

    Clean up `kNotificationScrollBar` and its helper function
    Since `kNotificationScrollBar` is no longer needed, this change also
    removes the redundant `SetVisible(...)` and `CompleteAllAnimations()`
    calls in `MessageCenterScrollBar`'s constructor, allowing it to rely on
    the default behavior inherited from `RoundedScrollBar`.
    
    Bug: 356234489
    Change-Id: Ia677aa6cef83a9143667a15c10720da0ac59245c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8220134
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676032}

--- ash/system/notification_center/views/message_center_scroll_bar.cc
+++ ash/system/notification_center/views/message_center_scroll_bar.cc
@@ -5,13 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/notification_center/views/message_center_scroll_bar.h"
 
-#include "ash/constants/ash_features.h"
 #include "ash/controls/rounded_scroll_bar.h"
 #include "base/metrics/histogram_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/compositor/layer.h"
 #include "ui/compositor/presentation_time_recorder.h"
-#include "ui/views/controls/scrollbar/base_scroll_bar_thumb.h"
 #include "ui/views/widget/widget.h"
 
 namespace {
@@ -47,10 +44,7 @@ BEGIN_METADATA(MessageCenterScrollBar)
 END_METADATA
 
 MessageCenterScrollBar::MessageCenterScrollBar()
-    : RoundedScrollBar(views::ScrollBar::Orientation::kVertical) {
-  GetThumb()->layer()->SetVisible(features::IsNotificationScrollBarEnabled());
-  GetThumb()->layer()->CompleteAllAnimations();
-}
+    : RoundedScrollBar(views::ScrollBar::Orientation::kVertical) {}
 
 MessageCenterScrollBar::~MessageCenterScrollBar() = default;
 
