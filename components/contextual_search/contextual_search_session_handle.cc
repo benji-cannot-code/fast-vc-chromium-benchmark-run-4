75b0e8a20f5e812d0af5a4b0f6727a19503de8e7
Nihar Majmudar <niharm@google.com>
2026-08-06 21:44:35 -0700

    [Contextual Tasks] Don't send suggest request with previous context or auto chips.
    Note: This is a little hacky :( on the client side but there's no
    investment in a proper server solution yet.
    
    If there was ever past context in a contextual tass thread or if there
    is currently an auto added chip in the contextual tasks composebox, do
    NOT send a suggest request. This is because the suggest request will
    return smart compose which might not be relevant to the thread / current
    auto chip.
    
    Smart compose only gets returned in the chrome-cobrowse-compose client
    which is why when files are uploaded and we use chrome-contextual, we
    don't need to worry about this.
    
    A proper solution in the future would involve sending a CGI param to the
    server to let it know when to not return smart compose.
    
    Change-Id: If161c4825b47c4cbb58a1ef2ac7b81d66b573db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8215649
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675504}

--- components/contextual_search/contextual_search_session_handle.cc
+++ components/contextual_search/contextual_search_session_handle.cc
@@ -486,6 +486,10 @@ void ContextualSearchSessionHandle::CreateSearchUrl(
     }
   }
 
+  if (!search_url_request_info->file_tokens.empty()) {
+    has_submitted_context_ = true;
+  }
+
   // Copy the tokens from this request to the list of all submitted tokens.
   submitted_context_tokens_.insert(submitted_context_tokens_.end(),
                                    search_url_request_info->file_tokens.begin(),
@@ -741,6 +745,10 @@ ContextualSearchSessionHandle::CreateClientToAimRequest(
   create_client_to_aim_request_info->file_tokens =
       std::move(file_tokens_set).extract();
 
+  if (!create_client_to_aim_request_info->file_tokens.empty()) {
+    has_submitted_context_ = true;
+  }
+
   // Copy the tokens from this request to the list of all submitted tokens.
   submitted_context_tokens_.insert(
       submitted_context_tokens_.end(),
@@ -795,6 +803,9 @@ void ContextualSearchSessionHandle::ClearSubmittedContextTokens() {
 
 void ContextualSearchSessionHandle::set_submitted_context_tokens(
     const std::vector<base::UnguessableToken>& tokens) {
+  if (!tokens.empty()) {
+    has_submitted_context_ = true;
+  }
   submitted_context_tokens_ = tokens;
 }
 
