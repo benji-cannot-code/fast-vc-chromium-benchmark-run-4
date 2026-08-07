0657bbb53790e5739837fd0db9d2a5153c084d33
Sourav <sinhasourav@google.com>
2026-08-07 13:15:45 -0700

    [iOS][AM] Integrate AtMemoryInlineNoticeView into Search ViewController
    This CL updates AtMemorySearchViewController to display the
    AtMemoryInlineNoticeView. It configures the table view to dequeue the
    notice cell and adds placeholder delegate methods for notice actions
    (OK/Settings), which will be wired up in a follow-up CL.
    
    Bug: 540433768
    Change-Id: Ib8bce06fdb67c08070da12796b479d6e17ed600b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8214944
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675933}

--- ios/chrome/browser/autofill/atmemory/ui/at_memory_inline_notice_view.h
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_inline_notice_view.h
@@ -47,6 +47,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Delegate to handle actions from the notice view.
 @property(nonatomic, weak) id<AtMemoryInlineNoticeViewDelegate> delegate;
 
+// Registers the cell class with the table view.
++ (void)registerCellForTableView:(UITableView*)tableView;
+
+// Dequeues a cell from the table view.
++ (UITableViewCell*)dequeueTableViewCell:(UITableView*)tableView;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UI_AT_MEMORY_INLINE_NOTICE_VIEW_H_
--- ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/autofill/atmemory/public/at_memory_commands.h"
 #import "ios/chrome/browser/autofill/atmemory/public/at_memory_constants.h"
+#import "ios/chrome/browser/autofill/atmemory/ui/at_memory_inline_notice_view.h"
 #import "ios/chrome/browser/autofill/atmemory/ui/at_memory_search_mutator.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -49,7 +50,8 @@ enum class ViewState {
 
 }  // namespace
 
-@interface AtMemorySearchViewController () <UISearchResultsUpdating>
+@interface AtMemorySearchViewController () <UISearchResultsUpdating,
+                                            AtMemoryInlineNoticeViewDelegate>
 @end
 
 @implementation AtMemorySearchViewController {
@@ -98,6 +100,8 @@ enum class ViewState {
 }
 
 - (void)loadModel {
+  [AtMemoryInlineNoticeConfiguration registerCellForTableView:self.tableView];
+
   __weak __typeof(self) weakSelf = self;
   _dataSource = [[UITableViewDiffableDataSource alloc]
       initWithTableView:self.tableView
@@ -224,6 +228,7 @@ enum class ViewState {
                       itemIdentifier:(ItemIdentifier)itemIdentifier {
   switch (itemIdentifier) {
     case ItemIdentifier::kNoticeItem:
+      return [self noticeCellForTableView:tableView];
     case ItemIdentifier::kSearchItem:
     case ItemIdentifier::kFetchingItem:
     case ItemIdentifier::kNoDataItem:
@@ -233,6 +238,31 @@ enum class ViewState {
       break;
   }
   NOTREACHED();
+}
+
+// Returns the table view cell for the notice state.
+- (UITableViewCell*)noticeCellForTableView:(UITableView*)tableView {
+  UITableViewCell* cell =
+      [AtMemoryInlineNoticeConfiguration dequeueTableViewCell:tableView];
+  cell.selectionStyle = UITableViewCellSelectionStyleNone;
+  cell.backgroundColor = [UIColor clearColor];
+  cell.contentView.backgroundColor = [UIColor clearColor];
+
+  AtMemoryInlineNoticeConfiguration* config =
+      [[AtMemoryInlineNoticeConfiguration alloc] init];
+  config.delegate = self;
+  cell.contentConfiguration = config;
+  return cell;
+}
+
+#pragma mark - AtMemoryInlineNoticeViewDelegate
+
+- (void)inlineNoticeViewDidTapOK:(AtMemoryInlineNoticeView*)view {
+  // TODO(crbug.com/540433768): Forward to mutator to handle notice dismissal.
+}
+
+- (void)inlineNoticeViewDidTapSettings:(AtMemoryInlineNoticeView*)view {
+  // TODO(crbug.com/540433768): Forward to mutator to handle settings redirect.
 }
 
 @end
