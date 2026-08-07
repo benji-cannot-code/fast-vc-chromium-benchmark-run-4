2ad586e834526573e6890f32f3a627104ca475fb
Angela Novakovic <novakovica@google.com>
2026-08-07 08:08:21 -0700

    [iOS][AtMemory] Update the title of AtMemorySearchViewController
    This CL updates the title of the AtMemorySearchViewController to be the
    same on iPhones and iPads.
    
    Fixed: 543808001
    Change-Id: I076a4de7eb0e07a1938ba239fb75413ac997759a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8220749
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675713}

--- ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/autofill/atmemory/public/at_memory_commands.h"
 #import "ios/chrome/browser/autofill/atmemory/public/at_memory_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
-#import "ui/base/device_form_factor.h"
 #import "ui/base/l10n/l10n_util.h"
 
 namespace {
@@ -92,14 +91,7 @@ enum class ViewState {
       kAtMemoryCloseButtonAccessibilityIdentifier;
   self.navigationItem.rightBarButtonItem = cancelButton;
 
-  self.title =
-      (self.traitCollection.horizontalSizeClass ==
-           UIUserInterfaceSizeClassCompact ||
-       self.traitCollection.verticalSizeClass ==
-           UIUserInterfaceSizeClassCompact)
-          ? l10n_util::GetNSString(IDS_IOS_AUTOFILL_AI_FIND_AND_FILL_TITLE)
-          : l10n_util::GetNSString(
-                IDS_IOS_PERSONAL_CONTEXT_AUTOFILL_SETTINGS_SWITCH_TITLE);
+  self.title = l10n_util::GetNSString(IDS_IOS_AUTOFILL_AI_FIND_AND_FILL_TITLE);
 
   [self loadModel];
 }
