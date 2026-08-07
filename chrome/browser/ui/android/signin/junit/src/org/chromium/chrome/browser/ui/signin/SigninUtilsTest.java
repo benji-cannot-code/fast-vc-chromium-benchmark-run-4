973a5c6aa46f1f1ede12575e7435550376981bd8
Boris Sazonov <bsazonov@chromium.org>
2026-08-07 03:37:41 -0700

    [Signin][Android] Open settings for all accounts in a new window
    No need to block Chrome UI while the user interacts with the system
    account settings.
    
    Bug: 537718490
    Change-Id: I0922482990085c245c15b1d754eca8cd9d02fdc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8210523
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675611}

---
+++ chrome/browser/ui/android/signin/junit/src/org/chromium/chrome/browser/ui/signin/SigninUtilsTest.java
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.signin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.robolectric.Shadows.shadowOf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.Settings;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+
+/** Unit tests for {@link SigninUtils}. */
+@RunWith(BaseRobolectricTestRunner.class)
+public class SigninUtilsTest {
+    @Test
+    @SmallTest
+    public void testOpenSettingsForAllAccounts() {
+        Activity activity = Robolectric.buildActivity(Activity.class).setup().get();
+
+        assertTrue(SigninUtils.openSettingsForAllAccounts(activity));
+
+        Intent intent = shadowOf(activity).getNextStartedActivity();
+        assertNotNull(intent);
+        assertEquals(Settings.ACTION_SYNC_SETTINGS, intent.getAction());
+        assertEquals(
+                Intent.FLAG_ACTIVITY_NEW_TASK,
+                intent.getFlags() & Intent.FLAG_ACTIVITY_NEW_TASK);
+    }
+}
