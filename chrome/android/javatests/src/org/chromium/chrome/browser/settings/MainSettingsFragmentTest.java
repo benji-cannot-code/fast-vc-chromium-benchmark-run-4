5e8a1ce9d31d10eefce42b53816ee7f4fb4a1cd1
James Cook <jamescook@chromium.org>
2026-08-07 04:43:23 -0700

    settings: Fix long lines in settings Java files
    These were causing presubmit warnings. Also fix a few warnings about
    unnecessary @NonNull as well.
    
    Bug: none
    Change-Id: Iddc063d121513cc3b1c1d2fb5962f514f3b15fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8218489
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675637}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
@@ -42,7 +42,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.preference.Preference;
@@ -564,7 +563,8 @@ public class MainSettingsFragmentTest {
     @SmallTest
     @EnableFeatures(
             ChromeFeatureList.HOME_BUTTON_REMOVAL
-                    + ":remove_home_button_everywhere/true/set_default_to_false_on_homepage_on_desktop/false")
+                    + ":remove_home_button_everywhere/true"
+                    + "/set_default_to_false_on_homepage_on_desktop/false")
     @DisableFeatures(ChromeFeatureList.ANDROID_BOTTOM_BAR)
     public void testHomeButtonRemovalEnabled() {
         startSettings();
@@ -666,9 +666,8 @@ public class MainSettingsFragmentTest {
 
     @Test
     @SmallTest
-    public void
-            testAccountManagementRowForChildAccountWithNonDisplayableAccountEmailWithEmptyDisplayName()
-                    throws InterruptedException {
+    public void testAccountManagementRowForChildAccountWithNonDisplayableEmailWithEmptyDisplayName()
+            throws InterruptedException {
         startSettings();
 
         // Account set up.
@@ -1121,9 +1120,9 @@ public class MainSettingsFragmentTest {
     }
 
     private void testNewPreferenceLabel(
-            @NonNull Class prefFragmentClass,
-            @NonNull String prefKey,
-            @NonNull String viewCountPrefKey,
+            Class prefFragmentClass,
+            String prefKey,
+            String viewCountPrefKey,
             @StringRes int titleId) {
         // Set up.
         final var prefs = ChromeSharedPreferences.getInstance();
