5b4abdb6e242244f23b2e067d7385de57663f273
Kevin McNee <mcnee@chromium.org>
2026-08-06 18:45:43 -0700

    Dictation: Have caret bubble correctly inherit light/dark theme
    Bug: b:525859277, b:539693982
    Change-Id: I752d782a38a6a79c4ec9840e4be2af1dd5744f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8212882
    Commit-Queue: David Bokan <bokan@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675460}

--- chrome/browser/dictation/session_ui_impl.cc
+++ chrome/browser/dictation/session_ui_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/dictation/features.h"
 #include "chrome/browser/dictation/session_ui_delegate.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
@@ -152,7 +153,8 @@ void SessionUiImpl::OnStartedStream(content::GlobalDOMNodeId target_id) {
   }
 
   if (!overlay_view_) {
-    gfx::NativeView parent_view = web_contents->GetContentNativeView();
+    gfx::NativeView parent_view = platform_util::GetViewForWindow(
+        web_contents->GetTopLevelNativeWindow());
     overlay_view_ = std::make_unique<DictationOverlayView>(
         parent_view,
         base::BindRepeating(&SessionUiImpl::OnToggleActiveStreamClicked,
