c91953d3c707fbe40917edaefd8fc5d7a16c1c04
Gary Klassen <gklassen@chromium.org>
2026-08-06 23:41:21 -0700

    [ttc] Add native user setting dictionary storage IPC primitives to PageHandler
    Change-Id: I6c60a9314615b3965d5b9375f88fd29fb47773b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8151940
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675540}

--- chrome/browser/ui/webui/ai_overlay_dialog/ai_overlay_dialog_page_handler.h
+++ chrome/browser/ui/webui/ai_overlay_dialog/ai_overlay_dialog_page_handler.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ai_overlay_dialog/ai_overlay_dialog.mojom.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 #include "url/gurl.h"
 
 class BrowserWindowInterface;
@@ -46,6 +47,9 @@ class AiOverlayDialogPageHandler
       int32_t width,
       int32_t height,
       CaptureRawViewportRegionCallback callback) override;
+  void SetRememberedNote(ai_overlay_dialog::mojom::RememberedNotePtr note,
+                         SetRememberedNoteCallback callback) override;
+  void GetRememberedNotes(GetRememberedNotesCallback callback) override;
 
   void DidChangePage(const GURL& url,
                      const std::optional<std::u16string>& title,
@@ -64,6 +68,11 @@ class AiOverlayDialogPageHandler
   mojo::Remote<ai_overlay_dialog::mojom::Page> page_;
   raw_ptr<BrowserWindowInterface> browser_;
   raw_ptr<actions::ActionItem> overlay_action_item_ = nullptr;
+
+  // In-memory dictionary storage for remembered conversational notes.
+  // TODO(crbug.com/542590634): Determine product and architecture requirements
+  // for long-term storage and persistence of remembered notes across restarts.
+  absl::flat_hash_map<std::string, std::string> remembered_notes_;
 };
 
 }  // namespace ttc
