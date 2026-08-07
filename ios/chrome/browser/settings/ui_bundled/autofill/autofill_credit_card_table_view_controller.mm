9d140cea51ba19aac05c8e654652b100a91c1d46
James Sun <jamessun@google.com>
2026-08-06 18:05:41 -0700

    [PDT][Bling] Add Google Wallet Legal Notice Footer to Payment Methods Settings
    Add a hyperlinked Google Wallet legal notice footer to the iOS Settings
    Payment Methods page (SectionIdentifierCards) when the feature flag
    kAutofillEnableWalletReminderNotice is enabled.
    
    Add ShouldShowGoogleWalletNotice() helper to PaymentsDataManager and
    IDS_AUTOFILL_SETTINGS_GOOGLE_WALLET_LEGAL_NOTICE to ios_strings.grd.
    Tapping the link opens https://wallet.google.com/wallet/settings in a
    new browser tab.
    
    Simulator result: Normal mode:
    https://drive.google.com/file/d/1e9_37B1KdEFAc-u4nr9uK8-7_PSIdBiv/view?usp=drive_link&resourcekey=0-_Nym1QC_yFsZvy4a-eXrCg
    
    Dark mode:
    https://drive.google.com/file/d/1qX_ZfVC-wRpaCkp55jjIZsRZDckpcwpJ/view?usp=sharing&resourcekey=0-z0TUusyjR6BX86lqYvswLg
    
    Url redirect video:
    https://drive.google.com/file/d/1r72siucy19ElsqvGPbzBx9LEkdUJ39vm/view?usp=drive_link&resourcekey=0-oMbDOLo8bUMhLl8sr6VbgQ
    
    Bug: 543564467
    Change-Id: If732c2b93a6a7064460f5de9153439c1e0b3ee60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8200105
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: James Sun <jamessun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675442}

--- ios/chrome/browser/settings/ui_bundled/autofill/autofill_credit_card_table_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/autofill/autofill_credit_card_table_view_controller.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/browser/data_manager/payments/payments_data_manager.h"
 #import "components/autofill/core/browser/data_manager/personal_data_manager.h"
 #import "components/autofill/core/browser/metrics/payments/mandatory_reauth_metrics.h"
+#import "components/autofill/core/common/autofill_payments_features.h"
 #import "components/autofill/core/common/autofill_prefs.h"
 #import "components/autofill/ios/browser/credit_card_util.h"
 #import "components/autofill/ios/browser/personal_data_manager_observer_bridge.h"
@@ -54,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/grit/ios_strings.h"
 #import "net/base/apple/url_conversions.h"
 #import "ui/base/l10n/l10n_util.h"
+#import "url/gurl.h"
 
 namespace {
 
@@ -78,6 +80,7 @@ enum ItemType : NSInteger {
   ItemTypeCVCStorageButton,
   ItemTypeCVCStorageButtonSubtitle,
   ItemTypePayOverTimeButton,
+  ItemTypeGoogleWalletLegalNoticeFooter,
 };
 
 }  // namespace
@@ -266,6 +269,11 @@ using autofill::autofill_metrics::MandatoryReauthOptInOrOutSource;
       [model addItem:[self itemForCreditCard:*creditCard]
           toSectionWithIdentifier:SectionIdentifierCards];
     }
+    if (base::FeatureList::IsEnabled(
+            autofill::features::kAutofillEnableWalletReminderNotice)) {
+      [model setFooter:[self googleWalletLegalMessageFooter]
+          forSectionWithIdentifier:SectionIdentifierCards];
+    }
   }
 }
 
@@ -361,6 +369,16 @@ using autofill::autofill_metrics::MandatoryReauthOptInOrOutSource;
       [[TableViewTextHeaderFooterItem alloc] initWithType:ItemTypeHeader];
   header.text = l10n_util::GetNSString(IDS_AUTOFILL_PAYMENT_METHODS);
   return header;
+}
+
+- (TableViewHeaderFooterItem*)googleWalletLegalMessageFooter {
+  TableViewLinkHeaderFooterItem* footer = [[TableViewLinkHeaderFooterItem alloc]
+      initWithType:ItemTypeGoogleWalletLegalNoticeFooter];
+  footer.text =
+      l10n_util::GetNSString(IDS_AUTOFILL_SETTINGS_GOOGLE_WALLET_LEGAL_NOTICE);
+  footer.urls = @[ [[CrURL alloc]
+      initWithGURL:GURL("https://wallet.google.com/wallet/settings")] ];
+  return footer;
 }
 
 // TODO(crbug.com/40123293): Add egtest for server cards.
@@ -626,6 +644,18 @@ using autofill::autofill_metrics::MandatoryReauthOptInOrOutSource;
 }
 
 #pragma mark - UITableViewDelegate
+
+- (UIView*)tableView:(UITableView*)tableView
+    viewForFooterInSection:(NSInteger)section {
+  UIView* footerView = [super tableView:tableView
+                 viewForFooterInSection:section];
+  TableViewLinkHeaderFooterView* footer =
+      base::apple::ObjCCast<TableViewLinkHeaderFooterView>(footerView);
+  if (footer) {
+    footer.delegate = self;
+  }
+  return footerView;
+}
 
 - (void)tableView:(UITableView*)tableView
     didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
