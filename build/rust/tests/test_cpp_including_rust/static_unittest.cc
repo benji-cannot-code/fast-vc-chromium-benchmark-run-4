f2d2acfad4096f99cade43e0b10db7e59d5ebeec
Kalvin Lee <kdlee@chromium.org>
2026-08-06 19:20:33 -0700

    PA: Use `MaxAllocationSize()`
    To more clearly express intent, `MaxDirectMapped()` is made public and
    renamed `MaxAllocationSize()`. This CL switches
    build/rust
    to use the new public spelling. It is otherwise a no-op.
    
    This CL was generated by Jetski.
    
    This CL was uploaded by git cl split.
    
    R=thakis@chromium.org
    
    Bug: 542850291
    Change-Id: I4a1ebc54512f0fd546be0ca27bacb76839626b4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8212239
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1675474}

--- build/rust/tests/test_cpp_including_rust/static_unittest.cc
+++ build/rust/tests/test_cpp_including_rust/static_unittest.cc
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+#include "partition_alloc/partition_alloc_constants.h"  // nogncheck
+#endif
+
 TEST(RustStaticTest, CppCallingIntoRust_BasicFFI) {
   EXPECT_EQ(7, add_two_ints_via_rust(3, 4));
 }
@@ -58,7 +62,7 @@ TEST(RustStaticTest, RustLargeAllocationFailure) {
   // PA will crash when an allocation fails rather than return null, but Rust
   // can be trusted to handle failure without introducing null derefs so this
   // should fail gracefully.
-  size_t max_size = partition_alloc::internal::MaxDirectMapped();
+  size_t max_size = partition_alloc::MaxAllocationSize();
   EXPECT_FALSE(allocate_huge_via_rust(max_size + 1u, 4u));
 
   // Same as above but with an alignment larger than PartitionAlloc's default
