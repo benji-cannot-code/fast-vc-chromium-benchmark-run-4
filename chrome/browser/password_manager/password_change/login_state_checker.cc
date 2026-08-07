37558aa7b554c562b23888d4ca4ff5abe24afe03
Anna Tsvirchkova <atsvirchkova@google.com>
2026-08-07 04:01:13 -0700

    [APC] Introduce the timeout for login check with PI
    When PI feature flag is on, login check will terminate upon the 5
    minutes timeout if it's still running.
    
    Bug: 538466617
    Change-Id: Ieedcc319fc096f4459edebaffe83de2f490f85b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8213288
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675629}

--- chrome/browser/password_manager/password_change/login_state_checker.cc
+++ chrome/browser/password_manager/password_change/login_state_checker.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/password_manager/password_change/annotated_page_content_capturer.h"
+#include "chrome/browser/password_manager/password_change/features.h"
 #include "chrome/browser/password_manager/password_change/password_change_logging_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/autofill/core/browser/logging/log_manager.h"
@@ -80,6 +81,7 @@ LoginStateChecker::LoginStateChecker(
       service_type_(service_type),
       client_(client),
       result_check_callback_(std::move(callback)) {
+  StartTimeoutTimer();
   CheckLoginState(/*ignore_attempts_limit=*/false);
 }
 
@@ -100,10 +102,21 @@ void LoginStateChecker::DidFinishNavigation(
   CheckLoginState(/*ignore_attempts_limit=*/false);
 }
 
+void LoginStateChecker::StartTimeoutTimer() {
+  if (service_type_ ==
+      optimization_guide::ModelExecutionServiceType::kPrivateAi) {
+    timer_.Start(
+        FROM_HERE, kLoginCheckTimeout,
+        base::BindOnce(&LoginStateChecker::TerminateLoginChecks,
+                       base::Unretained(this), /*logging_data=*/nullptr));
+  }
+}
+
 void LoginStateChecker::TerminateLoginChecks(
     std::unique_ptr<
         optimization_guide::proto::PasswordChangeSubmissionLoggingData>
         logging_data) {
+  timer_.Stop();
   // Reset content::WebContentsObserver.
   Observe(nullptr);
   capturer_.reset();
@@ -213,6 +226,9 @@ void LoginStateChecker::OnExecutionResponseCallback(
   }
 
   bool is_logged_in = response->is_logged_in_data().is_logged_in();
+  if (is_logged_in) {
+    timer_.Stop();
+  }
 
   LogBoolean(client_,
              SavePasswordProgressLogger::STRING_LOGIN_STATE_CHECK_RESULT,
