39a57d2d997de8740a7261e1a163076b3f85a7aa
Eldar Pulatov <epulatov.dev@gmail.com>
2026-08-07 21:04:54 -0700

    [guest_contents] Use typed process ID for frame token
    Part of the migration to strongly-typed content::ChildProcessId
    (crbug.com/379869738).
    
    Construct GlobalRenderFrameHostToken with RenderProcessHost::GetID()
    instead of the deprecated int getter.
    
    Bug: 379869738
    Change-Id: I185b681e0729fba9988247916797ccc9f2e65919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8223563
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676100}

--- components/guest_contents/browser/guest_contents_host_impl.cc
+++ components/guest_contents/browser/guest_contents_host_impl.cc
@@ -55,9 +55,7 @@ void GuestContentsHostImpl::Attach(
   content::RenderFrameHost* frame_to_swap =
       content::RenderFrameHost::FromFrameToken(
           content::GlobalRenderFrameHostToken(
-              outer_web_contents_->GetPrimaryMainFrame()
-                  ->GetProcess()
-                  ->GetDeprecatedID(),
+              outer_web_contents_->GetPrimaryMainFrame()->GetProcess()->GetID(),
               token_of_frame_to_swap));
 
   if (!frame_to_swap) {
