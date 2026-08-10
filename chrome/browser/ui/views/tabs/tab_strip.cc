d921320338aaaeb2d50b5b3dd793c1f87e48c426
Kunal Daftari <kunaldaftari@google.com>
2026-08-10 14:40:03 -0700

    [Focus] Ensuring A11Y Movement Does Not Move Tabs Out of Tab Group
    Modifies both legacy and modern tab strip to not allow tabs to move out
    of the tab group when they have accessibility focus.
    
    Bug: 540411925
    Change-Id: I6837e1a619c0d32966c21c5c824ebc33958e1e58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8225348
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676732}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1772,8 +1772,11 @@ void TabStrip::MoveTabFirst(Tab* tab) {
     return;
   }
 
+  std::optional<tab_groups::TabGroupId> focused_group = GetFocusedGroup();
   int target_index = 0;
-  if (!controller_->IsTabPinned(start_index.value())) {
+  if (focused_group.has_value() && tab->group() == focused_group) {
+    target_index = controller_->ListTabsInGroup(focused_group.value()).start();
+  } else if (!controller_->IsTabPinned(start_index.value())) {
     while (target_index < start_index &&
            controller_->IsTabPinned(target_index)) {
       ++target_index;
@@ -1784,14 +1787,14 @@ void TabStrip::MoveTabFirst(Tab* tab) {
     return;
   }
 
-  if (target_index != start_index) {
+  if (target_index != start_index.value()) {
     controller_->MoveTab(start_index.value(), target_index);
   }
 
   // The tab may unintentionally land in the first group in the tab strip, so we
   // remove the group to ensure consistent behavior. Even if the tab is already
   // at the front, it should "move" out of its current group.
-  if (tab->group().has_value()) {
+  if (tab->group().has_value() && tab->group() != focused_group) {
     controller_->RemoveTabFromGroup(target_index);
   }
 
@@ -1811,8 +1814,12 @@ void TabStrip::MoveTabLast(Tab* tab) {
 
   const int start_index = maybe_start_index.value();
 
+  std::optional<tab_groups::TabGroupId> focused_group = GetFocusedGroup();
   int target_index;
-  if (controller_->IsTabPinned(start_index)) {
+  if (focused_group.has_value() && tab->group() == focused_group) {
+    target_index =
+        controller_->ListTabsInGroup(focused_group.value()).end() - 1;
+  } else if (controller_->IsTabPinned(start_index)) {
     int temp_index = start_index + 1;
     while (temp_index < GetTabCount() && controller_->IsTabPinned(temp_index)) {
       ++temp_index;
@@ -1833,7 +1840,7 @@ void TabStrip::MoveTabLast(Tab* tab) {
   // The tab may unintentionally land in the last group in the tab strip, so we
   // remove the group to ensure consistent behavior. Even if the tab is already
   // at the back, it should "move" out of its current group.
-  if (tab->group().has_value()) {
+  if (tab->group().has_value() && tab->group() != focused_group) {
     controller_->RemoveTabFromGroup(target_index);
   }
 
@@ -2313,12 +2320,13 @@ void TabStrip::ShiftTabRelative(Tab* tab, int offset) {
   }
 
   const auto old_group = tab->group();
+  std::optional<tab_groups::TabGroupId> focused_group = GetFocusedGroup();
   if (!IsValidModelIndex(target_index) ||
       controller_->IsTabPinned(start_index) !=
           controller_->IsTabPinned(target_index)) {
     // Even if we've reached the boundary of where the tab could go, it may
     // still be able to "move" out of its current group.
-    if (old_group.has_value()) {
+    if (old_group.has_value() && old_group != focused_group) {
       AnnounceTabRemovedFromGroup(old_group.value());
       controller_->RemoveTabFromGroup(start_index);
     }
@@ -2330,6 +2338,12 @@ void TabStrip::ShiftTabRelative(Tab* tab, int offset) {
   std::optional<tab_groups::TabGroupId> target_group =
       tab_at(target_index)->group();
   if (old_group != target_group) {
+    // Do not allow tabs to enter or exit the focused tab group.
+    if (focused_group.has_value() &&
+        (old_group == focused_group || target_group == focused_group)) {
+      return;
+    }
+
     if (old_group.has_value()) {
       AnnounceTabRemovedFromGroup(old_group.value());
       controller_->RemoveTabFromGroup(start_index);
