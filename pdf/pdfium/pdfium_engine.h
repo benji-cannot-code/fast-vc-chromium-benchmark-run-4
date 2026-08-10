4989c93d9c1144641047fcf809001b9b7d56941e
Seung Hyun Jin <seunghyunjin@google.com>
2026-08-10 10:39:56 -0700

    Expose text direction APIs in PDFiumEngine
    Introduce GetFocusedFormTextDirection() and
    SetFocusedFormTextDirection() in PDFiumEngine by delegating to the
    underlying FORM_GetTextDirection() and FORM_SetTextDirection() APIs for
    the currently focused annotation.
    
    To prevent ambiguity between a "no focus" state and a "focused text
    field with default/natural direction" state,
    GetFocusedFormTextDirection() returns an std::optional wrapper. It
    returns std::nullopt if there is no focus (or if the focused
    annotation does not support text direction) and returns
    UNKNOWN_DIRECTION if a text field is focused and has natural direction.
    
    These methods are exposed to the PDF plugin wrapper in
    http://crrev.com/c/8175910 and will be integrated with Blink's context
    menu in http://crrev.com/c/8177874.
    
    Add unit tests to verify setting and getting the writing direction for
    LTR, RTL, and natural fields, as well as verifying failure cases on
    non-text fields and links.
    
    Bug: 537870008
    Change-Id: I021331a4a0d6761ccd8baa55b6d7cd743ee5ef84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8181103
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Seung Hyun Jin <seunghyunjin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676559}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
 #include "base/functional/function_ref.h"
+#include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
@@ -273,6 +274,16 @@ class PDFiumEngine : public DocumentLoader::Client,
   bool CanRedo() const;
   void Undo();
   void Redo();
+
+  // Sets the text direction for the currently focused form field. Returns false
+  // if there is no focused form field, or if the focused form field is of a
+  // type that does not support text direction (e.g. buttons).
+  bool SetFocusedFormTextDirection(base::i18n::TextDirection direction);
+
+  // Gets the text direction for the currently focused form field. Returns
+  // std::nullopt if there is no focused form field, or if the focused form
+  // field is of a type that does not support text direction.
+  std::optional<base::i18n::TextDirection> GetFocusedFormTextDirection() const;
 
   // Handles actions invoked by Accessibility clients.
   void HandleAccessibilityAction(const AccessibilityActionData& action_data);
