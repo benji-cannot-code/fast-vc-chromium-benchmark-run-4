bfb9d7db3fa55dda57f96455232a23c85f7ff01e
Florian Leimgruber <fleimgruber@google.com>
2026-08-10 09:01:20 -0700

    Remove incognito AutocompleteHistoryManager
    Currently, a separate instance of AutocompleteHistoryManager exists in
    and outside of incognito mode. Their only difference is the off the
    record (OTR) state, used to prevent saving in incognito mode.
    
    This CL changes the keyed service factory, redirecting to the non-OTR
    instance in incognito mode and removing the OTR state from the class.
    An equivalent change was made to the PersonalDataManager years ago in
    http://shortn/_R609Jy8fJa.
    
    To prevent Autocomplete from saving in incognito mode, logic is added to
    BrowserAutofillManager, preventing calls to SingleFieldFillRouter's
    OnWillSubmitForm().*
    - For Autocomplete, this is a no-op due to the OTR early return in the
      history manager.
    - For IBANs, this will change the conditions under which one of their
      metrics (http://shortn/_ayb9yAYdfP) is emitted. Arguably, this is a
      bug fix, as no Autofill (key) metrics are usually emitted in incognito
      and due to the inconsistency mentioned in (*).
    - For promo codes, it's a no-op since OnWillSubmitForm() doesn't contain
      promo-code related logic.
    
    As a drive-by, the CL also removes AutocompleteHistoryManager::Init()
    and inlines the logic into the constructor (similar to what has been
    done for the PDM in crrev.com/c/5476491).
    
    * This actually fixes an inconsistency. Currently, when a FormStructure
      exists for the FormData, OnWillSubmitForm() is never called (see
      http://shortn/_3AsFj4RsTg). But when no FormStructure exists, it is
      called at the moment (see http://shortn/_D9ZT1l9N8x). As of this CL
      it isn't called in either case.
    
    Bug: 40100455
    Change-Id: Ic2baa68aa78e3da462eac80db50089e501ffbc64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8222286
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676484}

--- components/autofill/core/browser/single_field_fillers/autocomplete/mock_autocomplete_history_manager.cc
+++ components/autofill/core/browser/single_field_fillers/autocomplete/mock_autocomplete_history_manager.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-MockAutocompleteHistoryManager::MockAutocompleteHistoryManager() = default;
+MockAutocompleteHistoryManager::MockAutocompleteHistoryManager()
+    : AutocompleteHistoryManager(/*profile_database=*/nullptr,
+                                 /*pref_service=*/nullptr) {}
 
 MockAutocompleteHistoryManager::~MockAutocompleteHistoryManager() = default;
 
--- ios/chrome/browser/autofill/model/autocomplete_history_manager_factory.mm
+++ ios/chrome/browser/autofill/model/autocomplete_history_manager_factory.mm
@@ -34,7 +34,7 @@ AutocompleteHistoryManagerFactory::GetInstance() {
 
 AutocompleteHistoryManagerFactory::AutocompleteHistoryManagerFactory()
     : ProfileKeyedServiceFactoryIOS("AutocompleteHistoryManager",
-                                    ProfileSelection::kOwnInstanceInIncognito) {
+                                    ProfileSelection::kRedirectedInIncognito) {
   DependsOn(ios::WebDataServiceFactory::GetInstance());
 }
 
@@ -43,12 +43,11 @@ AutocompleteHistoryManagerFactory::~AutocompleteHistoryManagerFactory() {}
 std::unique_ptr<KeyedService>
 AutocompleteHistoryManagerFactory::BuildServiceInstanceFor(
     ProfileIOS* profile) const {
-  auto service = std::make_unique<AutocompleteHistoryManager>();
   scoped_refptr<AutofillWebDataService> autofill_db =
       ios::WebDataServiceFactory::GetAutofillWebDataForProfile(
           profile, ServiceAccessType::EXPLICIT_ACCESS);
-  service->Init(autofill_db, profile->GetPrefs(), profile->IsOffTheRecord());
-  return service;
+  return std::make_unique<AutocompleteHistoryManager>(autofill_db,
+                                                      profile->GetPrefs());
 }
 
 }  // namespace autofill
