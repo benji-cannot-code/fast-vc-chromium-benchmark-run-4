963b1cd53c2639a470c478d43c0f5d6dd10e5129
Peter Kvitek <kvitekp@chromium.org>
2026-08-10 13:02:46 -0700

    [headless][mac] Fix current screen reporting on window move
    This CL adds a call to update screen properties after a window move so
    in case when the window moved to a different headless screen the current
    screen info gets properly updated. This fixes current screen association
    in headless mode where AppKit does not dispatch
    NSWindowDidChangeScreenNotification because the window did not cross any
    physical NSScreens.
    
    Drive by: increased test window height to overcome macOS minimum window
    height adjustmet.
    
    Bug: 543203215
    Change-Id: Iad58a37320cab3b6cfc503ca588120fa287a5a06
    Cq-Include-Trybots: luci.chromium.try:mac13-tests,mac14-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8217408
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676673}

--- components/headless/test/data/protocol/shared/move-window-between-screens.js
+++ components/headless/test/data/protocol/shared/move-window-between-screens.js
@@ -58,13 +58,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   await moveWindowAndLogScreen(
-      {left: 1, top: 1, width: 600, height: 300}, '#1');
+      {left: 1, top: 1, width: 600, height: 400}, '#1');
   await moveWindowAndLogScreen(
-      {left: 801, top: 1, width: 600, height: 300}, '#2');
+      {left: 801, top: 1, width: 600, height: 400}, '#2');
   await moveWindowAndLogScreen(
-      {left: 1, top: 601, width: 600, height: 300}, '#3');
+      {left: 1, top: 601, width: 600, height: 400}, '#3');
   await moveWindowAndLogScreen(
-      {left: 801, top: 601, width: 600, height: 300}, '#4');
+      {left: 801, top: 601, width: 600, height: 400}, '#4');
 
   testRunner.completeTest();
 });
