5d1dbd0e3c309fee984e4c6388ba80cdf33fb473
Ruoxi He <ruoxih@google.com>
2026-08-10 13:37:34 -0700

    [WebUI NTP on AL] Override NTP composebox submit button color for Android
    For AL, we use the GM3 primary color (#0B57D0) for the composebox submit button background color.
    
    This CL overrides kColorNewTabPageComposeboxSubmitButtonBackground to
    SkColorSetRGB(0x0B, 0x57, 0xD0) under #if BUILDFLAG(IS_ANDROID) in both
    new_tab_page_color_mixer.cc and material_new_tab_page_color_mixer.cc.
    
    This only affects Android WebUI NTP and leaves the Desktop color
    unaffected.
    
    Test:
    Before: http://screenshot/08r9bdu03hoj8
    After: http://screenshot/15a1foeefvlp8
    
    Fixed: b:534419479
    Change-Id: I8ce6a2a4ed32de92018f43b2fed89e7555cbf2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8230108
    Commit-Queue: Ruoxi He <ruoxih@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676693}

--- chrome/browser/ui/color/material_new_tab_page_color_mixer.cc
+++ chrome/browser/ui/color/material_new_tab_page_color_mixer.cc
@@ -148,6 +148,10 @@ void AddMaterialNewTabPageColorMixer(ui::ColorProvider* provider,
   // defined instead of using GM3 color variables.
   mixer[kColorNewTabPageCommonInputPlaceholder] = {SkColorSetARGB(0x60, 0x1F, 0x1F, 0x1F)};
   mixer[kColorNewTabPageRealboxNextIconHover] = {kColorSysStateHoverOnSubtle_Light};
+#if BUILDFLAG(IS_ANDROID)
+  mixer[kColorNewTabPageComposeboxSubmitButtonBackground] = {
+      SkColorSetRGB(0x0B, 0x57, 0xD0)};
+#else
   if (base::FeatureList::IsEnabled(ntp_features::kEnergyEffect)) {
     mixer[kColorNewTabPageComposeboxSubmitButtonBackground] = {
         SkColorSetRGB(0x33, 0x6E, 0xF3)};
@@ -155,6 +159,7 @@ void AddMaterialNewTabPageColorMixer(ui::ColorProvider* provider,
     mixer[kColorNewTabPageComposeboxSubmitButtonBackground] = {
         SkColorSetRGB(0x34, 0x6B, 0xF1)};
   }
+#endif
   mixer[kColorComposeboxBackground] = {SK_ColorWHITE};
   mixer[kColorComposeboxFileChipSpinner] = {kColorSysPrimary_Light};
   mixer[kColorComposeboxFont] = {
