b5cd406919b1e0b3b0f5482b4687349e786a6fba
Gaston Rodriguez <gastonr@microsoft.com>
2026-08-10 15:07:00 -0700

    [5] Delete stale shipped blink flags
    This CL removes flags that have been enabled by default for a long time.
    Code branches that accounted for the possibility of the flag enablement
    being false were simplified to assume that the flag would always be
    true.
    
    Flag, Shipping CL, Major version the ship landed
    SkipNonEditableInAtomicMove                       CL:7532695 v146
    FixLinebreakForPreTag                             CL:7529926 v146
    FixJustifyQueryCommandValue                       CL:7588313 v147
    DeleteTextInContentEditableBeforeRemovingChildren CL:7557953 v147
    ClampWordBoundaryToContentEditableScope           CL:7600404 v147
    SkipParagraphSplitForInlineInsertHTML             CL:7603169 v147
    
    Bug: 40723612,40952509,485397129,475155072,40848794,41024699
    Change-Id: I35e69118dee1022e8fbbd8e3a5e83b0f9355f7ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8191718
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1676749}

--- third_party/blink/renderer/core/editing/commands/insert_line_break_command.cc
+++ third_party/blink/renderer/core/editing/commands/insert_line_break_command.cc
@@ -67,8 +67,7 @@ bool InsertLineBreakCommand::ShouldUseBreakElement(
     return IsA<HTMLTextAreaElement>(text_control);
   }
   // Use <br> for <pre> elements to align with other browsers
-  if (RuntimeEnabledFeatures::FixLinebreakForPreTagEnabled() &&
-      EnclosingElementWithTag(p, html_names::kPreTag)) {
+  if (EnclosingElementWithTag(p, html_names::kPreTag)) {
     return true;
   }
   return IsRichlyEditablePosition(p) && p.AnchorNode()->GetLayoutObject() &&
