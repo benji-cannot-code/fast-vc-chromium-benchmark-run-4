7bcd7ae52e3c34bc0b16ceac02999e49ab672572
Danilo Tedeschi <danft@google.com>
2026-08-10 10:34:54 -0700

    Create IcuBridge::Normalizer and implement icu4c and icu4x
    The Icu4x normalizer is only enabled through flag.
    
    I will also add later counters for when the normalizers are null
    pointers.
    
    Change-Id: I9d4bbbc7ba89419e898dd17dd38f18016fd9816c
    Bug: 40064988
    Binary-Size: We will recover it (even save some) later.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8215671
    Reviewed-by: Manish Goregaokar <manishearth@google.com>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676555}

---
+++ base/i18n/icubridge/features.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/i18n/icubridge/features.h"
+
+namespace base::i18n {
+
+BASE_FEATURE(kUseIcu4xNormalizer, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace base::i18n
