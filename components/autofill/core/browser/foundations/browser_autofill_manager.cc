9f86b256eeac0731e44f81779f202fb815ac33c8
Florian Leimgruber <fleimgruber@google.com>
2026-08-10 09:13:15 -0700

    Avoid passing through is_autocomplete_enabled
    Since AutocompleteHistoryManager has a pref service, it can just check
    itself.
    
    Bug: 40100455
    Change-Id: Id9d0cb85f188a7471e63cd3c0ff966266ccfcc08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8230827
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676494}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -647,8 +647,7 @@ void MaybeImportFromSubmittedForm(AutofillClient& client,
         client.GetPaymentsAutofillClient()->IsAutofillPaymentMethodsEnabled(),
         ukm_source_id);
   }
-  client.GetSingleFieldFillRouter().OnWillSubmitForm(
-      form.ToFormData(), &form, client.IsAutocompleteEnabled());
+  client.GetSingleFieldFillRouter().OnWillSubmitForm(form.ToFormData(), &form);
 }
 
 // Generates a compose suggestion for the given `form` and `field` if conditions
@@ -984,8 +983,7 @@ void BrowserAutofillManager::OnFormSubmittedImpl(const FormData& form,
   if (!submitted_form) {
     // We always give Autocomplete a chance to save the data.
     if (!client().IsOffTheRecord()) {
-      client().GetSingleFieldFillRouter().OnWillSubmitForm(
-          form, nullptr, client().IsAutocompleteEnabled());
+      client().GetSingleFieldFillRouter().OnWillSubmitForm(form, nullptr);
     }
     return;
   }
--- components/autofill/core/browser/single_field_fillers/autocomplete/autocomplete_history_manager.h
+++ components/autofill/core/browser/single_field_fillers/autocomplete/autocomplete_history_manager.h
@@ -68,8 +68,7 @@ class AutocompleteHistoryManager : public KeyedService {
   // `fields` may be empty.
   virtual void OnWillSubmitFormWithFields(
       const std::vector<FormFieldData>& fields,
-      const FormStructure* form,
-      bool is_autocomplete_enabled);
+      const FormStructure* form);
 
   virtual void CancelPendingQuery();
 
