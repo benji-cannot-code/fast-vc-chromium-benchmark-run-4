555d84c05cc04c209f0353748f55d70bf96002f2
Zoraiz Naeem <zoraiznaeem@google.com>
2026-08-10 12:51:31 -0700

    Add comparator implementations for RoundedCornersF
    Bug: None
    Change-Id: I64c85c822f985652288b029bd80427c957d963fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8180344
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676661}

--- ui/gfx/geometry/rounded_corners_f.h
+++ ui/gfx/geometry/rounded_corners_f.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iosfwd>
 #include <limits>
 #include <string>
+#include <tuple>
 
 #include "base/component_export.h"
 #include "ui/gfx/geometry/vector2d_f.h"
@@ -19,6 +20,12 @@ namespace gfx {
 // corner radii in the order: top-left, top-right, bottom-right, bottom-left.
 class COMPONENT_EXPORT(GEOMETRY) RoundedCornersF {
  public:
+  static bool Compare(const RoundedCornersF& a, const RoundedCornersF& b) {
+    return std::tie(a.upper_left_, a.upper_right_, a.lower_right_,
+                    a.lower_left_) < std::tie(b.upper_left_, b.upper_right_,
+                                              b.lower_right_, b.lower_left_);
+  }
+
   // Creates an empty RoundedCornersF with all corners having zero radius.
   constexpr RoundedCornersF() : RoundedCornersF(0.0f) {}
 
