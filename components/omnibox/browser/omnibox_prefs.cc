c264418b78bfb7898f3471f92e03dd2c077ee360
Madhav Pruthi <madhavpruthi@google.com>
2026-08-10 07:02:48 -0700

    [Omnibox][Policy] Support IntranetRedirectBehavior policy on Android
    This CL enables the IntranetRedirectBehavior enterprise policy on
    Android and defines the INTRANET_REDIRECT message ID, metrics, and UI
    strings.
    
    BUG=467693484
    
    Change-Id: I82f73e3a539e62f6004cdd28b9fb11d0952c717e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8214544
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676404}

--- components/omnibox/browser/omnibox_prefs.cc
+++ components/omnibox/browser/omnibox_prefs.cc
@@ -60,6 +60,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
 
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kIsOmniboxInBottomPosition, false);
+  registry->RegisterIntegerPref(kIntranetRedirectBehavior, 0);
 }
 
 void SetUserPreferenceForZeroSuggestCachedResponse(PrefService* prefs,
