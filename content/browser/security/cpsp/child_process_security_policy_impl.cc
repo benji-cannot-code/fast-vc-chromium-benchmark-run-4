4eea871782701c7485992091b34d9b297eb532f7
Jacob Drake <jrdrake@google.com>
2026-08-09 20:35:57 -0700

    Narrow grants when committing `externalfile:` URLs.
    `externalfile:` on ChromeOS has characteristics similar to `file:` URLs
    on other platforms. This CL aligns `externalfile:` with `file:` by
    narrowing the scope of the grant to the specific URL, rather than the
    entire scheme. To support this, `externalfile:` grants are tracked
    internally as `GURL` objects instead of `base::FilePath` structures.
    
    Additionally, "request" and "commit" access are separated into distinct
    tracking sets to better support Site Isolation invariants. As part of
    this separation, drag-and-drop operations are updated to grant only
    request access, rather than commit access.
    
    Bug: 497559076
    Change-Id: I5313c297a8002139d5ba314c13218856d3327cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8181163
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Jacob Drake <jrdrake@google.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676248}

--- content/browser/security/cpsp/child_process_security_policy_impl.cc
+++ content/browser/security/cpsp/child_process_security_policy_impl.cc
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/file_system/file_system_url.h"
 #include "storage/browser/file_system/isolated_context.h"
 #include "storage/common/file_system/file_system_util.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 #include "third_party/blink/public/common/features.h"
 #include "url/gurl.h"
 #include "url/url_canon.h"
@@ -448,6 +449,15 @@ bool AllowProcessLockMismatchForNTP(const ProcessLock& expected_lock,
       expected_lock.GetProcessLockURL(), actual_lock.site_url());
 }
 
+#if BUILDFLAG(IS_CHROMEOS)
+GURL NormalizeExternalFileUrl(const GURL& url) {
+  GURL::Replacements replacements;
+  replacements.ClearQuery();
+  replacements.ClearRef();
+  return url.ReplaceComponents(replacements);
+}
+#endif
+
 }  // namespace
 
 ChildProcessSecurityPolicyImpl::Handle::Handle() = default;
@@ -711,6 +721,22 @@ class ChildProcessSecurityPolicyImpl::ProcessState {
     request_file_set_.insert(file.StripTrailingSeparators());
   }
 
+#if BUILDFLAG(IS_CHROMEOS)
+  // Grant navigation to a specific external file URL.
+  void GrantRequestOfExternalFileUrl(const GURL& url) {
+    CHECK(url.SchemeIs(kExternalFileScheme));
+    request_externalfile_set_.insert(NormalizeExternalFileUrl(url));
+  }
+
+  void GrantCommitOfExternalFileUrl(const GURL& url) {
+    CHECK(url.SchemeIs(kExternalFileScheme));
+    commit_externalfile_set_.insert(NormalizeExternalFileUrl(url));
+
+    // Commit access automatically implies request access.
+    request_externalfile_set_.insert(NormalizeExternalFileUrl(url));
+  }
+#endif
+
   // Revokes all permissions granted to a file.
   void RevokeAllPermissionsForFile(const base::FilePath& file) {
     base::FilePath stripped = file.StripTrailingSeparators();
@@ -791,6 +817,12 @@ class ChildProcessSecurityPolicyImpl::ProcessState {
       return true;
     }
 
+#if BUILDFLAG(IS_CHROMEOS)
+    if (url.SchemeIs(kExternalFileScheme)) {
+      return commit_externalfile_set_.contains(NormalizeExternalFileUrl(url));
+    }
+#endif
+
     // Check for permission for specific origin.
     if (CanCommitOrigin(url::Origin::Create(url))) {
       return true;
@@ -826,6 +858,12 @@ class ChildProcessSecurityPolicyImpl::ProcessState {
 #if BUILDFLAG(IS_ANDROID)
     if (url.SchemeIs(url::kContentScheme)) {
       return request_file_set_.contains(base::FilePath(url.spec()));
+    }
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS)
+    if (url.SchemeIs(kExternalFileScheme)) {
+      return request_externalfile_set_.contains(NormalizeExternalFileUrl(url));
     }
 #endif
 
@@ -997,6 +1035,7 @@ class ChildProcessSecurityPolicyImpl::ProcessState {
   typedef std::map<base::FilePath, FilePermissionFlags> FileMap;
   typedef std::map<std::string, FilePermissionFlags> FileSystemMap;
   typedef std::set<base::FilePath> FileSet;
+  using URLSet = absl::flat_hash_set<GURL>;
   typedef std::set<url::Origin> OriginSet;
 
   // Maps URL schemes to commit/request policies the child process has been
@@ -1030,6 +1069,12 @@ class ChildProcessSecurityPolicyImpl::ProcessState {
 
   // The set of files the child process is permitted to load.
   FileSet request_file_set_;
+
+#if BUILDFLAG(IS_CHROMEOS)
+  // The set of specific URLs the child process is permitted to load.
+  URLSet request_externalfile_set_;
+  URLSet commit_externalfile_set_;
+#endif
 
   // The set of origins in Android WebView and <webview> tags that are allowed
   // to bypass some navigation checks. Limited to opaque origins loaded with
@@ -1424,6 +1469,16 @@ void ChildProcessSecurityPolicyImpl::GrantCommitURL(int child_id,
     GrantCommitURL(child_id, GURL(origin.Serialize()));
   }
 
+#if BUILDFLAG(IS_CHROMEOS)
+  // `externalfile:` URLs, like `file:` URLs, should result in grants to the
+  // specific resource referenced, not the entire scheme:
+  if (url.SchemeIs(kExternalFileScheme)) {
+    GrantCommitOfExternalFileUrl(ChildProcessId::FromUnsafeValue(child_id),
+                                 url);
+    return;
+  }
+#endif
+
   // TODO(dcheng): In the future, URLs with opaque origins would ideally carry
   // around an origin with them, so we wouldn't need to grant commit access to
   // the entire scheme.
@@ -1483,6 +1538,40 @@ void ChildProcessSecurityPolicyImpl::GrantRequestOfSpecificFile(
     state->GrantRequestOfSpecificFile(canonical_path);
   }
 }
+
+#if BUILDFLAG(IS_CHROMEOS)
+void ChildProcessSecurityPolicyImpl::GrantRequestOfExternalFileUrl(
+    ChildProcessId child_id,
+    const GURL& url) {
+  if (!url.is_valid()) {
+    return;
+  }
+
+  base::AutoLock lock(lock_);
+  auto* state = process_states_.GetProcessStateForMutation(child_id);
+  if (!state) {
+    return;
+  }
+
+  state->GrantRequestOfExternalFileUrl(url);
+}
+
+void ChildProcessSecurityPolicyImpl::GrantCommitOfExternalFileUrl(
+    ChildProcessId child_id,
+    const GURL& url) {
+  if (!url.is_valid()) {
+    return;
+  }
+
+  base::AutoLock lock(lock_);
+  auto* state = process_states_.GetProcessStateForMutation(child_id);
+  if (!state) {
+    return;
+  }
+
+  state->GrantCommitOfExternalFileUrl(url);
+}
+#endif
 
 void ChildProcessSecurityPolicyImpl::GrantReadFile(ChildProcessId child_id,
                                                    const base::FilePath& file) {
