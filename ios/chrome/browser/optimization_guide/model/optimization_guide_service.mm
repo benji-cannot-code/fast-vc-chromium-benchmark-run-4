6e1bc651a706007af3515e37d00f307fb8768b65
Amarjot Gill <amarjotgill@google.com>
2026-08-10 07:22:37 -0700

    set download::SchedulingParams for model downloads on IOS implementation
    Bug: 540886875
    Change-Id: I9f57d7e4908e54b18c51da8ed96d9bf243614e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8222404
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676418}

--- ios/chrome/browser/optimization_guide/model/optimization_guide_service.mm
+++ ios/chrome/browser/optimization_guide/model/optimization_guide_service.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/task/thread_pool.h"
 #import "base/time/default_clock.h"
 #import "components/component_updater/pref_names.h"
+#import "components/download/public/background_service/download_params.h"
 #import "components/optimization_guide/core/delivery/prediction_manager.h"
 #import "components/optimization_guide/core/hints/command_line_top_host_provider.h"
 #import "components/optimization_guide/core/hints/hints_processing_util.h"
@@ -312,6 +313,15 @@ void OptimizationGuideService::RemoveObserverForOptimizationTargetModel(
   if (optimization_guide::features::IsOptimizationTargetPredictionEnabled()) {
     GetPredictionManager()->RemoveObserverForOptimizationTargetModel(
         optimization_target, observer);
+  }
+}
+
+void OptimizationGuideService::SetModelDownloadSchedulingParams(
+    optimization_guide::proto::OptimizationTarget optimization_target,
+    const download::SchedulingParams& params) {
+  if (optimization_guide::features::IsOptimizationTargetPredictionEnabled()) {
+    GetPredictionManager()->SetModelDownloadSchedulingParams(
+        optimization_target, params);
   }
 }
 
