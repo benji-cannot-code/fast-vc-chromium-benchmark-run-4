7d7930e2ab46f54272e114384052cd96e262d1d1
vikas soni <vikassoni@chromium.org>
2026-08-10 08:13:41 -0700

    [Webview] Refactor RenderThread context loss handling.
    Before this CL, intentional context loss on viz thread was handled in
    HardwareRenderer::OnViz::DisplayOutputSurface() and context loss on
    RenderThread was handled by
    OutputSurfaceProviderWebView::OnContextLost().
    OutputSurfaceProviderWebView::OnContextLost() required manual heap
    pointer tracking (expect_context_loss_ptr). This also made it harder for
    future work to have a single SharedContext for all the webview instance.
    
    This CL refactors RenderThread context loss handling to check
    gpu::SharedContextState::context_lost() on the RenderThread at every
    draw entrance and exit via an RAII guard, eliminating the need for
    OnContextLost callback pointer tracking.
    
    BUG: 538189645
    Change-Id: I7e43c6e4dbd61374c6c62b8e8f09df242786b66e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8213962
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676442}

--- android_webview/browser/gfx/output_surface_provider_webview.h
+++ android_webview/browser/gfx/output_surface_provider_webview.h
@@ -42,7 +42,6 @@ class OutputSurfaceProviderWebView {
   std::unique_ptr<viz::OutputSurface> CreateOutputSurface(
       viz::DisplayCompositorMemoryAndTaskController*
           display_compositor_controller);
-  void MarkAllowContextLoss();
 
   const viz::RendererSettings& renderer_settings() const {
     return renderer_settings_;
@@ -68,7 +67,6 @@ class OutputSurfaceProviderWebView {
   scoped_refptr<AwGLSurface> gl_surface_;
   scoped_refptr<gpu::SharedContextState> shared_context_state_;
   bool enable_vulkan_;
-  raw_ptr<bool> expect_context_loss_ = nullptr;
 };
 
 }  // namespace android_webview
