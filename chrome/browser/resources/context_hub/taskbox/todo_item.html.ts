676624b4ccf4736dd6fec9f6140ed0537436d856
Isabelle Goralsky <igoralsky@google.com>
2026-08-10 09:38:25 -0700

    Allow user to dismiss and complete Todos.
    Key changes:
    - Update todo_item to call updateAutoTodo with a dismissed status when the dismiss / complete button is clicked.
    - Disable the dismiss / complete button while todo generation is in progress.
    - Filter out dismissed / complete todos in both the WebUI frontend and the backend page handler so they are hidden from the UI.
    - Use Lit's repeat directive for rendering todo lists.
    - Follow-up CL will add a UI section for completed tasks
    
    Bug: b:543981198
    Change-Id: I3473398b8513a92b021c358ca613334297b3568b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8219809
    Auto-Submit: Isabelle Goralsky <igoralsky@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Commit-Queue: Isabelle Goralsky <igoralsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676514}

--- chrome/browser/resources/context_hub/taskbox/todo_item.html.ts
+++ chrome/browser/resources/context_hub/taskbox/todo_item.html.ts
@@ -60,7 +60,7 @@ export function getHtml(this: TodoItemElement) {
         @expanded-changed="${this.onExpandedChanged_}">
       <div class="todo-content">
         <cr-icon-button id="check-circle"
-            disabled=true
+            ?disabled="${this.disable_state_mgmt}"
             iron-icon="cr:check-circle"
             @click="${this.onCheckCircleClick_}">
         </cr-icon-button>
@@ -106,8 +106,9 @@ export function getHtml(this: TodoItemElement) {
             </div>
           </div>
         </div>
-        <cr-button disabled=true class="dismiss-button" @click="${
-                               this.onDismissClick_}">
+        <cr-button class="dismiss-button"
+            ?disabled="${this.disable_state_mgmt}"
+            @click="${this.onDismissClick_}">
           Dismiss Todo
         </cr-button>
       </div>
