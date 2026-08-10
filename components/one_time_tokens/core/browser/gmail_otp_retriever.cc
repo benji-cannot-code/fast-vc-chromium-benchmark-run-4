c4db1c7925bb441623c1ab2b2298475c20ac2983
Ioana Pandele <ioanap@chromium.org>
2026-08-10 07:37:14 -0700

    [ActorOtp] Sender site.com should match www.site.com filling frame
    ... but sender www.site.com should not match site.com filling frame.
    
    Bug: 543378210
    Change-Id: I6ca077d5c129f2882f676a561440b67e2fa378a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8228750
    Reviewed-by: Trang Mai <trangm@google.com>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676424}

--- components/one_time_tokens/core/browser/gmail_otp_retriever.cc
+++ components/one_time_tokens/core/browser/gmail_otp_retriever.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/one_time_tokens/core/browser/one_time_token_log_sink.h"
 #include "components/one_time_tokens/core/browser/one_time_token_service.h"
 #include "components/one_time_tokens/core/common/one_time_token_features.h"
+#include "components/url_formatter/url_formatter.h"
 #include "url/origin.h"
 #include "url/scheme_host_port.h"
 #include "url/url_constants.h"
@@ -132,15 +133,25 @@ void GmailOtpRetriever::CheckSenderDomainMatchesFrameToFill(
   std::string sender_domain = ExtractEmailDomain(sender_address);
 
   CHECK(!otp_frame_origin_.opaque());
+
+  std::string stripped_frame_host =
+      url_formatter::StripWWW(otp_frame_origin_.host());
+
   LOG_OTT(one_time_token_service_->log_sink())
       << "GmailOtpRetriever checking sender domain match: sender_address="
       << sender_address << ", sender_domain=" << sender_domain
-      << ", otp_frame_origin=" << otp_frame_origin_;
-  domain_relation_checker_->Check(
-      otp_frame_origin_.GetTupleOrPrecursorTupleIfOpaque(),
-      url::SchemeHostPort(url::kHttpsScheme, std::move(sender_domain),
-                          url::DefaultPortForScheme(url::kHttpsScheme)),
-      std::move(callback));
+      << ", otp_frame_origin=" << otp_frame_origin_
+      << " (normalized frame: " << stripped_frame_host << ")";
+
+  url::SchemeHostPort normalized_frame_tuple(otp_frame_origin_.scheme(),
+                                             std::move(stripped_frame_host),
+                                             otp_frame_origin_.port());
+  url::SchemeHostPort sender_tuple(
+      url::kHttpsScheme, std::move(sender_domain),
+      url::DefaultPortForScheme(url::kHttpsScheme));
+
+  domain_relation_checker_->Check(normalized_frame_tuple, sender_tuple,
+                                  std::move(callback));
 }
 
 void GmailOtpRetriever::CheckCachedTokenMatch(
