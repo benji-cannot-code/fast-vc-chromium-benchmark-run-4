b57e7c5fa3431b2329b884786f4313db9acd64ce
Hailey Wang <haileywang@google.com>
2026-08-10 16:52:35 -0700

    [Glic] Open chat when task is done or waiting
    Bug: 537037882
    Change-Id: I9694eacf5f4d39f56a9591d8a2fa6ce5140e3ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127150
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676819}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ui/ActorControlCoordinator.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ui/ActorControlCoordinator.java
@@ -32,6 +32,7 @@ public class ActorControlCoordinator implements PeekViewManager, ActorControlSta
     private final ActorControlStateTracker mStateTracker;
 
     private PeekViewUiState mPeekViewUiState = PeekViewUiState.DEFAULT;
+    private boolean mHasAutoExpanded;
 
     /**
      * Constructs a new {@link ActorControlCoordinator}.
@@ -82,23 +83,33 @@ public class ActorControlCoordinator implements PeekViewManager, ActorControlSta
                 case ActorTaskState.CREATED:
                 case ActorTaskState.CANCELLED:
                     setPeekViewContent(activeTaskTitle, PeekViewUiState.DEFAULT);
+                    mHasAutoExpanded = false;
                     break;
                 case ActorTaskState.ACTING:
                 case ActorTaskState.REFLECTING:
                     setPeekViewContent(activeTaskTitle, PeekViewUiState.ACTING);
+                    mHasAutoExpanded = false;
                     break;
                 case ActorTaskState.PAUSED_BY_USER:
                     setPeekViewContent(activeTaskTitle, PeekViewUiState.PAUSED);
+                    mHasAutoExpanded = false;
                     break;
                 case ActorTaskState.PAUSED_BY_ACTOR:
                 case ActorTaskState.WAITING_ON_USER:
                 case ActorTaskState.FINISHED:
                 case ActorTaskState.FAILED:
                     setPeekViewContent(activeTaskTitle, PeekViewUiState.WAITING);
+                    if (!mHasAutoExpanded
+                            && !mTabBottomSheetManager.isSheetShowing()
+                            && mStateTracker.isCurrentTabActuatedTab()) {
+                        mTabBottomSheetManager.setSheetExpanded(true);
+                        mHasAutoExpanded = true;
+                    }
                     break;
                 default:
                     assert false : "Unhandled ActorTaskState " + taskState;
                     clearPeekViewContent();
+                    mHasAutoExpanded = false;
                     break;
             }
         } else {
@@ -109,6 +120,7 @@ public class ActorControlCoordinator implements PeekViewManager, ActorControlSta
                                 .getString(R.string.peek_state_new_chat);
             }
             setPeekViewContent(title, PeekViewUiState.DEFAULT);
+            mHasAutoExpanded = false;
         }
     }
 
@@ -174,6 +186,9 @@ public class ActorControlCoordinator implements PeekViewManager, ActorControlSta
         assert mTabBottomSheetManager.isSheetInitialized();
         GlicMetrics.recordClosePeekView();
         mTabBottomSheetManager.tryToCloseBottomSheet(/* animate= */ true);
+        if (PeekViewUiState.WAITING.equals(mPeekViewUiState)) {
+            mStateTracker.clearWaitingState();
+        }
     }
 
     /** Called when the peek view is clicked. */
