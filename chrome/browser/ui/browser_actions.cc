f08ed7919512de887752a41f4b56c6ab1c0d42bf
Fernando Ramirez <ferny@google.com>
2026-08-10 18:28:10 -0700

    [PDT] Reshow bubble on page action icon click
    The Wallet reminder notice has the following dismissal behaviors:
     1. Clicking "Got it" closes both the bubble and the page action icon.
     2. Clicking outside the bubble dismisses the bubble while keeping the
        page action icon visible.
    
    This CL implements the logic to reshow the bubble when the user clicks
    the persisted page action icon after dismissing the bubble.
    
    Design: go/wallet-notice-chrome-design
    Fixed: b:543926719
    Change-Id: If8f8676a0593ca0d3537814449ef06bb6c73d663
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8224344
    Reviewed-by: Olivia Saul <osaul@google.com>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676893}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/payments/omnibox_autofill_page_action_controller.h"
 #include "chrome/browser/ui/autofill/payments/save_payment_icon_controller.h"
 #include "chrome/browser/ui/autofill/payments/virtual_card_enroll_bubble_controller_impl.h"
+#include "chrome/browser/ui/autofill/payments/wallet_reminder_notice_bubble_controller.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_action_prefs_listener.h"
@@ -4499,8 +4500,14 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
             base::BindRepeating(
                 [](BrowserWindowInterface* bwi, actions::ActionItem* item,
                    actions::ActionInvocationContext context) {
-                  // TODO(crbug.com/542731356): Show Wallet Reminder Notice
-                  // bubble.
+                  tabs::TabInterface* tab = bwi->GetActiveTabInterface();
+                  CHECK(tab);
+
+                  if (auto* controller =
+                          autofill::WalletReminderNoticeBubbleController::From(
+                              *tab)) {
+                    controller->ReshowBubble();
+                  }
                 },
                 bwi))
             .SetActionId(kActionWalletReminderNotice)
