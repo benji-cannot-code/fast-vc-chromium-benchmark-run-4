6bb7b527eb5b1bdc2cf9801a8e7a82fe65405d88
Marc Treib <treib@chromium.org>
2026-08-11 09:51:38 -0700

    [STTS][Android] Introduce DeviceResourceProvider interface
    ...and its public aka upstream implementation.
    
    An internal implementation which provides different icons will be added
    soon.
    
    Bug: 494114604
    Change-Id: Iba39db86f9de459fc2de7b325d391b766a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239432
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677305}

---
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/send_tab_to_self/DeviceResourceProviderFactory.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.share.send_tab_to_self;
+
+import org.chromium.base.ServiceLoaderUtil;
+import org.chromium.build.annotations.NullMarked;
+
+/** Factory for creating {@link DeviceResourceProvider}. */
+@NullMarked
+public class DeviceResourceProviderFactory {
+    /**
+     * Creates an instance of {@link DeviceResourceProvider}.
+     *
+     * @return {@link DeviceResourceProvider}
+     */
+    public static DeviceResourceProvider create() {
+        DeviceResourceProvider provider =
+                ServiceLoaderUtil.maybeCreate(DeviceResourceProvider.class);
+        if (provider == null) {
+            provider = new DeviceResourceProviderUpstreamImpl();
+        }
+        return provider;
+    }
+}
