17513be13d3225ce9066f3202cf8703a17afe3d9
Michael Wilson <mjwilson@chromium.org>
2026-08-11 12:10:52 -0700

    [WebAudio] Update reverb convolver for configurable quantum size
    The following was done:
    - Remove background rendering entirely, and cap convolver stage sizes
      at 8192
    - Add unit tests
    - Enable failing web test
    
    Attempts to keep the original background rendering thread model resulted
    in additional complexity and possible multithreading synchronization
    issues, so in the interest of simplicity we remove it. This has the
    additional effect of both OfflineAudioContext and AudioContext using the
    same code paths after this change, so any future optimizations can be
    evaluated by the current perf tests.
    
    This change is likely to cause a highly system-dependent performance
    impact. Faster machines may see a performance improvement since some
    overhead was removed, but slower machines may see additional glitching
    since more load is shifted to the real-time audio thread.
    
    Bug: 502309240
    Change-Id: I1e1c43282e54118ecbaffce0ee276dfc8d9ca504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162327
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Mahesh Kannan <kmaheshb@google.com>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677437}

--- third_party/blink/renderer/platform/audio/reverb_convolver_stage.h
+++ third_party/blink/renderer/platform/audio/reverb_convolver_stage.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ReverbAccumulationBuffer;
-class ReverbConvolver;
 class FFTConvolver;
 class DirectConvolver;
 
@@ -73,13 +72,7 @@ class ReverbConvolverStage final {
   // buffer size (stage_offset).
   void Process(base::span<const float> source);
 
-  void ProcessInBackground(ReverbConvolver* convolver,
-                           uint32_t frames_to_process);
-
   void Reset();
-
-  // Useful for background processing
-  size_t InputReadIndex() const { return input_read_index_; }
 
  private:
   std::unique_ptr<FFTFrame> fft_kernel_;
@@ -89,7 +82,6 @@ class ReverbConvolverStage final {
 
   raw_ptr<ReverbAccumulationBuffer> accumulation_buffer_;
   uint32_t accumulation_read_index_;
-  size_t input_read_index_;
 
   size_t pre_delay_length_;
   size_t post_delay_length_;
