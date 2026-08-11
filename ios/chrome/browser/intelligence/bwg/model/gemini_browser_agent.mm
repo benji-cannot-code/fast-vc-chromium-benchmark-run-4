bd3cb0ebe33ed77e809772cede65683ed01bb69b
Gayane Petrosyan <gayane@google.com>
2026-08-11 08:03:04 -0700

    [iOS] Early return in ForceDismissFloaty if floaty is not invoked
    The crash was due to calling ForceDismissFloaty when switching to
    incognito while floaty was not invoked and also because calling
    DismissFloaty shouldn't happen when bottom sheet migration is enabled.
    
    Calling `ForceDismissFloaty()` when floaty was not invoked called
    `DismissFloaty()`, which hits `CHECK(gemini_container_mediator_)` and
    crashes.
    
    This CL adds an early return to `ForceDismissFloaty()` for all code
    paths.
    
    Fixed: 541166486
    Change-Id: I72a81edc8aaff38f4e217635e1668c11c310e8e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8220101
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677212}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -614,9 +614,7 @@ void GeminiBrowserAgent::OnPrimaryAccountChanged(
   if (event_type != signin::PrimaryAccountChangeEvent::Type::kNone) {
     browser_->GetProfile()->GetPrefs()->ClearPref(prefs::kGeminiConversationId);
 
-    if (is_floaty_invoked_) {
-      ForceDismissFloaty();
-    }
+    ForceDismissFloaty();
   }
 }
 
@@ -625,9 +623,7 @@ void GeminiBrowserAgent::OnIdentityManagerShutdown(
   if (identity_manager_) {
     identity_manager_->RemoveObserver(this);
     identity_manager_ = nullptr;
-    if (is_floaty_invoked_) {
-      ForceDismissFloaty();
-    }
+    ForceDismissFloaty();
   }
 }
 
@@ -1433,6 +1429,9 @@ void GeminiBrowserAgent::DismissFloaty() {
 }
 
 void GeminiBrowserAgent::ForceDismissFloaty() {
+  if (!is_floaty_invoked_) {
+    return;
+  }
   is_floaty_temporarily_hidden_ = false;
   DismissFloaty();
 }
