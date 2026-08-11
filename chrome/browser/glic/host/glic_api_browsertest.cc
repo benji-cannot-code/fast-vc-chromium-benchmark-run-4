77f0441473c32324af1f72da2fa8110e16d5b3c7
Bryant Chandler <bryantchandler@google.com>
2026-08-11 10:00:04 -0700

    [glic] Migrate and fix flakiness of testNotifyPanelWillOpenIsCalledOnce
    Migrates the testNotifyPanelWillOpenIsCalledOnce browser test. Fixes a
    race condition during close/reopen where the C++ test continues before
    the reopened panel's WebUI receives notifyPanelWillOpen.
    
    Also adds a check to ensure GlicInstanceImpl is preserved on reopen.
    
    Bug: b:477918640
    Change-Id: Iad77173d915f13e7532cc8de173074996a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8233062
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677318}

--- chrome/browser/glic/host/glic_api_browsertest.cc
+++ chrome/browser/glic/host/glic_api_browsertest.cc
@@ -1348,13 +1348,6 @@ IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab, DISABLED_testMetrics) {
   histogram_tester->ExpectTotalCount("Glic.TabContext.UploadTime", 1);
 }
 
-
-
-IN_PROC_BROWSER_TEST_P(GlicApiTestWithOneTab,
-                       testNotifyPanelWillOpenIsCalledOnce) {
-  ExecuteJsTest();
-}
-
 IN_PROC_BROWSER_TEST_P(GlicApiTest,
                        testPanelWillOpenHasRecentlyActiveConversations) {
   // Open 3 tabs and register a conversation in each.
