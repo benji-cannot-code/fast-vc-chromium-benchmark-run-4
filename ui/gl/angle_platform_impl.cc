5ac39b05c32e9b7af1f2498df03c2f5983007dcf
Tzarial <zork@google.com>
2026-08-11 12:20:15 -0700

    [agy][gl] fix ANGLE task hang during shutdown
    When ANGLE tasks are posted to the ThreadPool during shutdown,
    they might be skipped (SKIP_ON_SHUTDOWN). This can lead to
    threads hanging while waiting for these tasks to complete (e.g.
    in AsyncWaitableEvent::wait).
    
    This CL adds a fallback to run the task synchronously on the
    calling thread if posting fails.
    
    Fixed: 539435331
    Test: gl_unittests --gtest_filter=*ANGLEPlatformImplTest*
    Change-Id: I691c85ba3c794f7a3278562ff433eb6d66b0edee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8231642
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Tzarial <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677439}

--- ui/gl/angle_platform_impl.cc
+++ ui/gl/angle_platform_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/angle_platform_impl.h"
 
+#include <atomic>
 #include <string>
 
 #include "base/base64.h"
@@ -25,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace angle {
 
 namespace {
+
+std::atomic<bool> g_post_task_failed_for_testing{false};
 
 double ANGLEPlatformImpl_currentTime(PlatformMethods* platform) {
   return base::Time::Now().InSecondsFSinceUnixEpoch();
@@ -152,20 +155,32 @@ void AnglePlatformImpl_runWorkerTask(PostWorkerTaskCallback callback, void* user
   callback(user_data);
 }
 
-void ANGLEPlatformImpl_postWorkerTask(PlatformMethods* platform,
-                                      PostWorkerTaskCallback callback,
-                                      void* user_data) {
-  base::ThreadPool::PostTask(
-      FROM_HERE, {base::TaskPriority::USER_BLOCKING},
-      base::BindOnce(&AnglePlatformImpl_runWorkerTask, callback, user_data));
-}
-
 void ANGLEPlatformImpl_recordShaderCacheUse(bool in_cache) {
   // Metrics were no longer required, we can remove once Angle no longer
   // requires the method.
 }
 
 }  // anonymous namespace
+
+void SetPostTaskFailedForTesting(bool failed) {
+  g_post_task_failed_for_testing.store(failed);
+}
+
+void ANGLEPlatformImpl_postWorkerTask(PlatformMethods* platform,
+                                      PostWorkerTaskCallback callback,
+                                      void* user_data) {
+  bool success = false;
+  if (!g_post_task_failed_for_testing.load()) {
+    success = base::ThreadPool::PostTask(
+        FROM_HERE, {base::TaskPriority::USER_BLOCKING},
+        base::BindOnce(&AnglePlatformImpl_runWorkerTask, callback, user_data));
+  }
+  if (!success) {
+    // Run the task synchronously if posting failed (e.g. during shutdown) to
+    // avoid GPU hangs. See https://crbug.com/539435331
+    AnglePlatformImpl_runWorkerTask(callback, user_data);
+  }
+}
 
 NO_SANITIZE("cfi-icall")
 bool InitializePlatform(EGLDisplay display,
