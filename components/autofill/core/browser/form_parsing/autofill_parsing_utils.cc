afc9bb118d6b17f6f8035cba6e099716f3a9b45b
Rebecca Zhao <qihuizhao@google.com>
2026-08-11 09:37:07 -0700

    Move feature flag to autofill_features
    This CL migrates feature flag from autofill_payments_features to
    autofill_features so that it will work for both Chromium and official
    Chrome. This CL also adds RegexFeature for IBAN parsing.
    
    Design: go/increase-iban-form-detection
    
    Bug: 542614273
    Change-Id: I978c2f5b1e072899fb352e0d9730d5889d65662b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8233100
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677288}

--- components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
+++ components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
@@ -30,6 +30,8 @@ const base::Feature* GetFeatureOfRegexFeature(RegexFeature feature) {
       return &features::kAutofillFixStateCountryMisclassification;
     case RegexFeature::kAutofillSupportCombinedZipAndCityFR:
       return &features::kAutofillSupportCombinedZipAndCityFR;
+    case RegexFeature::kAutofillEnableExpandIbanRegexPattern:
+      return &features::kAutofillEnableExpandIbanRegexPattern;
   }
   NOTREACHED();
 }
