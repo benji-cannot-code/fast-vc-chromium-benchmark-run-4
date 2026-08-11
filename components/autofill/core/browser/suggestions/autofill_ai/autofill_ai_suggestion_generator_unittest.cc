f9d783233366296582e2d5a651e4e06558a2e3b1
Friedrich Horschig <friedrichh@chromium.org>
2026-08-11 06:31:35 -0700

    [Autofill AI] Rename AutofillAiHideSuggestion to kAutofillAmbientAutofillSuppressionUI
    Rename the feature flag kAutofillAiHideSuggestion to
    kAutofillAmbientAutofillSuppressionUI and update its description to
    reflect cross-platform UI usage.
    
    Bug: 541184575
    Change-Id: I4c93733bba2da8710ac4ef0b87cc9f116a56b0c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239434
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Auto-Submit: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677156}

--- components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator_unittest.cc
+++ components/autofill/core/browser/suggestions/autofill_ai/autofill_ai_suggestion_generator_unittest.cc
@@ -570,7 +570,7 @@ TEST_F(
 TEST_F(AutofillAiSuggestionGeneratorTest,
        GetFillingSuggestion_PersonalContext_HideSuggestion) {
   base::test::ScopedFeatureList scoped_feature_list(
-      features::kAutofillAiHideSuggestion);
+      features::kAutofillAmbientAutofillSuppressionUI);
 
   EntityInstance passport_personal_context =
       GetPassportEntityInstanceWithRandomGuid(
