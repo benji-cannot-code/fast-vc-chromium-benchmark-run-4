f351346c506f9ef3738da9acd52895ee372164a0
Saifuddin Hitawala <hitawala@chromium.org>
2026-08-11 10:40:21 -0700

    [media] Remove VideoFrameMetadata allow_overlay
    This can now be removed and instead viz TransferableResource checks
    for SharedImage SCANOUT usage instead which does the right thing.
    
    Bug: 425634684
    Change-Id: I70a81ae1c5ebc8c6d843b33b34e965195e5d52e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8227007
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677357}

--- media/base/video_frame_unittest.cc
+++ media/base/video_frame_unittest.cc
@@ -71,7 +71,6 @@ media::VideoFrameMetadata GetFullVideoFrameMetadata() {
   metadata.transformation = media::VIDEO_ROTATION_90;
 
   // bools
-  metadata.allow_overlay = true;
   metadata.copy_required = true;
   metadata.end_of_stream = true;
   metadata.in_surface_view = true;
@@ -113,7 +112,6 @@ media::VideoFrameMetadata GetFullVideoFrameMetadata() {
 
 void VerifyVideoFrameMetadataEquality(const media::VideoFrameMetadata& a,
                                       const media::VideoFrameMetadata& b) {
-  EXPECT_EQ(a.allow_overlay, b.allow_overlay);
   EXPECT_EQ(a.capture_begin_time, b.capture_begin_time);
   EXPECT_EQ(a.capture_end_time, b.capture_end_time);
   EXPECT_EQ(a.capture_counter, b.capture_counter);
@@ -1061,7 +1059,6 @@ TEST(VideoFrameMetadata, PartialMergeMetadata) {
   partial_metadata.capture_update_rect = kTempRect;
   partial_metadata.reference_time = kTempTicks;
   partial_metadata.processing_time = kTempDelta;
-  partial_metadata.allow_overlay = false;
 
   // Merging partial metadata into full metadata partially override it.
   full_metadata.MergeMetadataFrom(partial_metadata);
@@ -1069,7 +1066,6 @@ TEST(VideoFrameMetadata, PartialMergeMetadata) {
   EXPECT_EQ(partial_metadata.capture_update_rect, kTempRect);
   EXPECT_EQ(partial_metadata.reference_time, kTempTicks);
   EXPECT_EQ(partial_metadata.processing_time, kTempDelta);
-  EXPECT_EQ(partial_metadata.allow_overlay, false);
 }
 
 TEST(VideoFrame, AccessPlaneDataSpans) {
