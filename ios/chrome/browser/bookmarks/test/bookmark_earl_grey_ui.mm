38676995348abd8e607810ab72856fe8fe767622
Huiting Yu <huitingyu@google.com>
2026-08-11 09:54:58 -0700

    [iOS] Re-enable and deflake testEditFunctionalityOnSingleFolderAccount
    Wait for Folder 1.1 to appear before navigating into it after exiting
    context bar edit mode, and wait for the app to idle after dismissing the
    undo snackbar. Re-enable testEditFunctionalityOnSingleFolderAccount.
    
    Fixed: 514455596
    Change-Id: Icb7566d0e4a00d42d491382de508995a22bd2a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8229079
    Auto-Submit: Huiting Yu <huitingyu@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677310}

--- ios/chrome/browser/bookmarks/test/bookmark_earl_grey_ui.mm
+++ ios/chrome/browser/bookmarks/test/bookmark_earl_grey_ui.mm
@@ -730,6 +730,7 @@ id<GREYMatcher> SearchIconButton() {
       performAction:grey_tap()];
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(editorId)]
       assertWithMatcher:grey_notVisible()];
+  [ChromeEarlGreyUI waitForAppToIdle];
 }
 
 - (NSString*)contextBarNewFolderString {
