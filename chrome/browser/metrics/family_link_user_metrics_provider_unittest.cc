5ef58fd8b7633461e0cca602250a12347f8d14f8
Tomek Jurkiewicz <tju@google.com>
2026-08-11 06:13:49 -0700

    Fix bug in web filter type reporting for supervised users
    The `Locally supervised regular users yield kDisabled filter type when
    they decide to control other features than browser content` part of the
    contract for GetWebFilterType function was not met for locally
    supervised users in "separate log records" mode (kDisabled was yield all
    locally supervised configurations).
    
    Without this fix, for locally supervised users, the web filter type was
    reported as disabled if the browser used "separate" reporting of
    supervision type for local and account supervision.
    
    Bug: 424071314
    Change-Id: Ieb0aa40d9b5190c5bfa459daa8fbe9d0451742ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8229324
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677151}

--- chrome/browser/metrics/family_link_user_metrics_provider_unittest.cc
+++ chrome/browser/metrics/family_link_user_metrics_provider_unittest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user/core/common/features.h"
 #include "components/supervised_user/core/common/pref_names.h"
 #include "components/supervised_user/core/common/supervised_user_constants.h"
+#include "components/supervised_user/test_support/features.h"
 #include "content/public/test/browser_task_environment.h"
 #include "extensions/buildflags/buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -573,13 +574,12 @@ TEST_F(FamilyLinkUserMetricsProviderWithContentFiltersAndroidTest,
 // Family-Link supervision and cannot be applied to these profiles.
 class
     FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest
-    : public FamilyLinkUserMetricsProviderWithContentFiltersAndroidTest,
-      public testing::WithParamInterface<ContentFiltersTestCase> {
+    : public WithFeatureOverrideAndParamInterface<ContentFiltersTestCase>,
+      public FamilyLinkUserMetricsProviderWithContentFiltersAndroidTest {
  protected:
-  void SetUpFeatureList() override {
-    scoped_feature_list_.InitWithFeatureStates(
-        {{kSupervisedUserEmitLogRecordSeparately, false}});
-  }
+  FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest()
+      : WithFeatureOverrideAndParamInterface<ContentFiltersTestCase>(
+            kSupervisedUserEmitLogRecordSeparately) {}
 
   void CreateProfiles(std::size_t count) {
     CHECK_GE(email_addresses_.size(), count) << "Not enough email addresses";
@@ -612,7 +612,7 @@ class
 TEST_P(
     FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest,
     AllFiltersDisabled) {
-  CreateProfiles(GetParam().profile_count);
+  CreateProfiles(GetTestCase().profile_count);
 
   base::HistogramTester histogram_tester;
   metrics_provider()->OnDidCreateMetricsLog();
@@ -629,7 +629,7 @@ TEST_P(
 TEST_P(
     FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest,
     SearchFilterEnabled) {
-  CreateProfiles(GetParam().profile_count);
+  CreateProfiles(GetTestCase().profile_count);
   SetSearchContentFilters(true);
 
   base::HistogramTester histogram_tester;
@@ -648,7 +648,7 @@ TEST_P(
 TEST_P(
     FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest,
     ContentFiltersEnabled) {
-  CreateProfiles(GetParam().profile_count);
+  CreateProfiles(GetTestCase().profile_count);
   SetBrowserContentFilters(true);
 
   base::HistogramTester histogram_tester;
@@ -667,7 +667,7 @@ TEST_P(
 TEST_P(
     FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest,
     AllFiltersEnabled) {
-  CreateProfiles(GetParam().profile_count);
+  CreateProfiles(GetTestCase().profile_count);
   SetBrowserContentFilters(true);
   SetSearchContentFilters(true);
 
@@ -687,12 +687,19 @@ TEST_P(
 INSTANTIATE_TEST_SUITE_P(
     ,
     FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest,
-    testing::ValuesIn<ContentFiltersTestCase>({
-        {1, "SingleProfile"},
-        {2, "MultipleProfiles"},
-    }),
-    [](const testing::TestParamInfo<ContentFiltersTestCase>& info) {
-      return info.param.test_name;
+    testing::Combine(testing::Bool(),
+                     testing::ValuesIn<ContentFiltersTestCase>({
+                         {1, "SingleProfile"},
+                         {2, "MultipleProfiles"},
+                     })),
+    [](const testing::TestParamInfo<
+        FamilyLinkUserMetricsProviderExclusiveContentFiltersAndFamilyLinkAndroidTest::
+            ParamType>& info) {
+      bool feature_enabled = std::get<0>(info.param);
+      ContentFiltersTestCase test_case = std::get<1>(info.param);
+      return base::StrCat({feature_enabled ? "With" : "Without",
+                           kSupervisedUserEmitLogRecordSeparately.name, "_",
+                           test_case.test_name});
     });
 #endif  // BUILDFLAG(IS_ANDROID)
 
