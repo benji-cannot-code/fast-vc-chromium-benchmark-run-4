5ace39a19847e2d575c4360fd1b5d4fd24f8bf36
Tove Petersson <tovep@chromium.org>
2026-08-11 07:59:58 -0700

    Reland "Fix screenshot capture hangs for chrome://feedback on macOS"
    This reverts commit 5e055646496bd5cb67e5101c3cf0a6c220db89c6.
    
    Reason for revert:
    Fixed failure in chrome/browser/compose/compose_dialog_browsertest.cc
    
    Original change's description:
    > Revert "Fix screenshot capture hangs for chrome://feedback on macOS"
    >
    > This reverts commit 128b98a69aa5562f82198c8cf0a63f10764ddba6.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5083696227942400
    >
    > Sample build with failed test: https://ci.chromium.org/b/8674733599567833265
    > Affected test(s):
    > [://chrome/test\:browser_tests!gtest::ComposeSessionBrowserTest#OpenFeedbackPage](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::ComposeSessionBrowserTest%23OpenFeedbackPage?q=VHash%3A812306a38c719b85)
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5083696227942400&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8153540&type=BUG
    >
    > Original change's description:
    > > Fix screenshot capture hangs for chrome://feedback on macOS
    > >
    > > With the new native SCK picker on Mac, the user needs to manually select
    > > the screen they want to share in the feedback UI. With this change, we
    > > now route the request to the new picker flow.
    > >
    > > Also:
    > > - Added a check for request_approved_for_test_ in the picker flow to
    > > auto-approve requests and prevent interactive UI tests from hanging.
    > > - Added unit tests verifying feedback UI screenshot requests.
    > >
    > > Bug: 536647286, 40286360
    > > Change-Id: Ib5ea9aa16c17231a4c4371d27e7fdba8e75ea6a7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8153540
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > > Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1671961}
    > >
    >
    > Bug: 536647286, 40286360
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I497f7ec83e512d0b51a9596add8180a36a1952f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178319
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1672330}
    
    Bug: 536647286, 40286360
    Change-Id: I4e064180bc7ab8cd1d696637a231b5c61a43b308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8230311
    Commit-Queue: Tove Petersson <tovep@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677209}

--- chrome/browser/compose/compose_dialog_browsertest.cc
+++ chrome/browser/compose/compose_dialog_browsertest.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/compose/chrome_compose_client.h"
 #include "chrome/browser/compose/compose_enabling.h"
 #include "chrome/browser/compose/compose_session.h"
+#include "chrome/browser/media/webrtc/desktop_capture_access_handler.h"
+#include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
@@ -54,6 +56,20 @@ class ComposeSessionBrowserTest : public InteractiveBrowserTest {
   }
 
   void TearDown() override { InteractiveBrowserTest::TearDown(); }
+
+  void SetUpOnMainThread() override {
+    InteractiveBrowserTest::SetUpOnMainThread();
+    MediaCaptureDevicesDispatcher::GetInstance()
+        ->desktop_capture_access_handler_for_test()
+        ->SetRequestApprovedForTest(true);
+  }
+
+  void TearDownOnMainThread() override {
+    MediaCaptureDevicesDispatcher::GetInstance()
+        ->desktop_capture_access_handler_for_test()
+        ->SetRequestApprovedForTest(false);
+    InteractiveBrowserTest::TearDownOnMainThread();
+  }
 
   feature_engagement::test::ScopedIphFeatureList* feature_list() {
     return &feature_list_;
