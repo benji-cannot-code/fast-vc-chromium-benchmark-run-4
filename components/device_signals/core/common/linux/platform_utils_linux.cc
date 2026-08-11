2c1e50d7bed7d46c039b42e13dad5cc78bc601f2
Sergey Poromov <poromov@google.com>
2026-08-10 18:32:33 -0700

    Fix device_signals missing ScreenlockSecured on ChromeOS
    Device signals natively returned UNKNOWN for screen lock secured status
    on ChromeOS. We update it to correctly evaluate this by leveraging
    chromeos::PowerPolicyController, which is the same underlying API that
    the legacy EV extension evaluated on ChromeOS (via the
    chrome.idle.getAutoLockDelay API).
    
    While the GetScreenlockSecured() function returned value is used to
    populate the chrome.enterprise.reportingPrivate API, it is safe to fix
    its behavior on ChromeOS. Today, the SEB extension ignores this field
    entirely, and the EV extension on ChromeOS relies on a hardcoded
    workaround (chrome.idle.getAutoLockDelay()).
    
    Bug: 543318534
    Change-Id: I58fbeede896dfee5a2d14dc084a953afaf1f11db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8204285
    Reviewed-by: Hamda Mare <hmare@google.com>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676899}

--- components/device_signals/core/common/linux/platform_utils_linux.cc
+++ components/device_signals/core/common/linux/platform_utils_linux.cc
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/system/sys_info.h"
 #include "build/chromeos_buildflags.h"
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/dbus/power/power_policy_controller.h"
+#endif
+
 #include "components/device_signals/core/common/common_types.h"
 #include "components/device_signals/core/common/platform_utils.h"
 #include "components/device_signals/core/common/signals_constants.h"
@@ -78,7 +82,11 @@ base::FilePath GetCrowdStrikeZtaFilePath() {
 // The current implementation support Gnone and Cinnamon only.
 SettingValue GetScreenlockSecured() {
 #if BUILDFLAG(IS_CHROMEOS)
-  // ChromeOS handles screen lock via user preferences, not OS-level tools.
+  if (chromeos::PowerPolicyController::IsInitialized()) {
+    base::TimeDelta delay = chromeos::PowerPolicyController::Get()
+                                ->GetMaxPolicyAutoScreenLockDelay();
+    return delay.is_zero() ? SettingValue::DISABLED : SettingValue::ENABLED;
+  }
   return SettingValue::UNKNOWN;
 #elif defined(USE_GIO)
   static constexpr char kLockScreenKey[] = "lock-enabled";
