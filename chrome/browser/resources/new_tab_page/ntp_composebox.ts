8006c465ad7b4d73eafee7a113ee57926b172f32
shomizoe <shomizoe@google.com>
2026-08-11 10:07:15 -0700

    [EnergyEffect] Remove the dark mode changes
    This CL removes the dark-mode-specific overrides introduced in
    crrev.com/c/8165143.
    
    Bug: b:54004138
    Change-Id: If8783feca82cf91fef56c8865b46c72c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8234878
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677330}

--- chrome/browser/resources/new_tab_page/ntp_composebox.ts
+++ chrome/browser/resources/new_tab_page/ntp_composebox.ts
@@ -81,14 +81,9 @@ export class NtpComposeboxElement extends ComposeboxEmbedderMixin
         reflect: true,
         type: Boolean,
       },
-      isDark: {
-        reflect: true,
-        type: Boolean,
-      },
     };
   }
 
-  accessor isDark: boolean = false;
   accessor entrypointName: string = 'Realbox';
   private searchboxCallbackRouter_: SearchboxPageCallbackRouter;
   private pageHandler_: PageHandlerRemote;
