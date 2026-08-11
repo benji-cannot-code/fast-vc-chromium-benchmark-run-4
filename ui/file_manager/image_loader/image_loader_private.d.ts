4028ae5ba32aa10ab21edfaeab1e6799e52c30ea
Tim Judkins <tjudkins@chromium.org>
2026-08-10 22:02:40 -0700

    [Extensions] Convert imageLoaderPrivate to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Gemini CLI. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    Also regenerates the associated TypeScript file, so it has the correct
    reference to the new filename.
    
    Fixed: 493989138
    Change-Id: I65630d646d447aac88b3a9d9e3c9011e96b282c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008426
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676941}

--- ui/file_manager/image_loader/image_loader_private.d.ts
+++ ui/file_manager/image_loader/image_loader_private.d.ts
@@ -1,14 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2024 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 /**
  * @fileoverview Definitions for chrome.imageLoaderPrivate API
- * Generated from: chrome/common/extensions/api/image_loader_private.idl
+ * Generated from: chrome/common/extensions/api/image_loader_private.webidl
  * run `tools/json_schema_compiler/compiler.py
- * chrome/common/extensions/api/image_loader_private.idl -g ts_definitions` to
- * regenerate.
+ * chrome/common/extensions/api/image_loader_private.webidl -g ts_definitions`
+ * to regenerate.
  */
 
 
