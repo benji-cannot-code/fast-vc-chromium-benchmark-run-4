d922480c70e6e1758fd6e27e900ce1c8d3a39933
Alfredo Higuera <alfredohiguera@google.com>
2026-08-11 16:50:22 -0700

    [Read Anything] Move View options above Theme options
    Rearranges the groups in the appearance menu to place the View options
    (presentation mode) above the color theme options.
    
    Also updates the WebUI tests to account for the new group order.
    
    New design: https://screenshot-v2.corp.google.com/BsQhJh3A868XV8L
    
    Bug: 545196163
    Change-Id: Iafaf9c1a86cd1c4616ffa4cc90f3cd6510fc1bed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8238659
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Alfredo Higuera <alfredohiguera@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677649}

--- chrome/test/data/webui/side_panel/read_anything/appearance_menu_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/appearance_menu_test.ts
@@ -44,7 +44,7 @@ suite('AppearanceMenuElement', () => {
     };
     await microtasksFinished();
 
-    const selectedItems = appearanceMenu.$.menu.menuGroups[0]!.items.filter(
+    const selectedItems = appearanceMenu.$.menu.menuGroups[1]!.items.filter(
         item => item.selected);
     assertEquals(1, selectedItems.length, 'selected');
     assertEquals(yellowTheme, selectedItems[0]!.data, 'data');
@@ -76,7 +76,7 @@ suite('AppearanceMenuElement', () => {
       await microtasksFinished();
 
       assertEquals(testTheme, calledTheme);
-      const selectedItems = appearanceMenu.$.menu.menuGroups[0]!.items.filter(
+      const selectedItems = appearanceMenu.$.menu.menuGroups[1]!.items.filter(
           item => item.selected);
       assertEquals(1, selectedItems.length);
       assertEquals(testTheme, selectedItems[0]!.data);
@@ -93,7 +93,7 @@ suite('AppearanceMenuElement', () => {
   test('restores saved color option', async () => {
     const color = chrome.readingMode.yellowTheme;
     const startingSelected =
-        appearanceMenu.$.menu.menuGroups[0]!.items.find(item => item.selected);
+        appearanceMenu.$.menu.menuGroups[1]!.items.find(item => item.selected);
     assertNotEquals(color, startingSelected?.data);
 
     appearanceMenu.settingsPrefs = {
@@ -103,14 +103,14 @@ suite('AppearanceMenuElement', () => {
     await microtasksFinished();
 
     const newSelected =
-        appearanceMenu.$.menu.menuGroups[0]!.items.find(item => item.selected);
+        appearanceMenu.$.menu.menuGroups[1]!.items.find(item => item.selected);
     assertEquals(color, newSelected?.data);
     assertNotEquals(startingSelected?.data, newSelected?.data);
   });
 
   test('does nothing if saved color is the same', async () => {
     const startingSelected =
-        appearanceMenu.$.menu.menuGroups[0]!.items.find(item => item.selected);
+        appearanceMenu.$.menu.menuGroups[1]!.items.find(item => item.selected);
 
     appearanceMenu.settingsPrefs = {
       ...TEST_RANDOM_VALUE_SETTINGS,
@@ -119,7 +119,7 @@ suite('AppearanceMenuElement', () => {
     await microtasksFinished();
 
     const newSelected =
-        appearanceMenu.$.menu.menuGroups[0]!.items.find(item => item.selected);
+        appearanceMenu.$.menu.menuGroups[1]!.items.find(item => item.selected);
     assertEquals(startingSelected?.data, newSelected?.data);
   });
 
@@ -128,7 +128,7 @@ suite('AppearanceMenuElement', () => {
         chrome.readingMode.inImmersiveOverlayPresentationState;
     await microtasksFinished();
 
-    const selectedItems = appearanceMenu.$.menu.menuGroups[1]!.items.filter(
+    const selectedItems = appearanceMenu.$.menu.menuGroups[0]!.items.filter(
         item => item.selected);
     assertEquals(1, selectedItems.length);
     assertEquals(
@@ -161,7 +161,7 @@ suite('AppearanceMenuElement', () => {
 
     assertEquals(0, closeAllMenusCount);
     assertEquals(immersiveState, appearanceMenu.presentationState);
-    let selectedItems = appearanceMenu.$.menu.menuGroups[1]!.items.filter(
+    let selectedItems = appearanceMenu.$.menu.menuGroups[0]!.items.filter(
         item => item.selected);
     assertEquals(1, selectedItems.length);
     assertEquals(immersiveState, selectedItems[0]!.data);
@@ -172,7 +172,7 @@ suite('AppearanceMenuElement', () => {
 
     assertEquals(0, closeAllMenusCount);
     assertEquals(sidePanelState, appearanceMenu.presentationState);
-    selectedItems = appearanceMenu.$.menu.menuGroups[1]!.items.filter(
+    selectedItems = appearanceMenu.$.menu.menuGroups[0]!.items.filter(
         item => item.selected);
     assertEquals(1, selectedItems.length);
     assertEquals(sidePanelState, selectedItems[0]!.data);
