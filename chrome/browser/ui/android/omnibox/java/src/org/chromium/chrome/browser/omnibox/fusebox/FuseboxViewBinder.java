2c05afa0c2904a2ede53b74e4f01440e88a3ea37
Charles Cai <charlesyc@google.com>
2026-08-11 10:46:11 -0700

    Make hover effect more pronounced for AIM buttons
    Update the hover and press ripple colors for the plus button and
    navigate/send button in AIM. Introduce scoped ripple color selectors for
    both buttons to avoid modifying global button styles across Chrome.
    
    Demo:
    http://screencast/cast/NjI0Nzc0MTAwODExNzc2MHw2OThlMzZmOC1iNw
    
    Bug: 534824022
    Change-Id: Ide0b865b93407f0f5ca593ef2880e840faecb7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8240373
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Charles Cai <charlesyc@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677364}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxViewBinder.java
@@ -580,7 +580,7 @@ class FuseboxViewBinder {
         }
     }
 
-    private static void updateNavigateButton(PropertyModel model, FuseboxViewHolder view) {
+    private void updateNavigateButton(PropertyModel model, FuseboxViewHolder view) {
         @BrandedColorScheme int brandedColorScheme = model.get(FuseboxProperties.COLOR_SCHEME);
         Context context = view.parentView.getContext();
         view.navigateButton
@@ -588,6 +588,7 @@ class FuseboxViewBinder {
                 .setTint(
                         OmniboxResourceProvider.getSendIconContrastColor(
                                 context, brandedColorScheme));
+        view.navigateButton.setBackground(mResourceProvider.getPopoverNavigateButtonBackground());
         @ColorInt
         int colorPrimary = OmniboxResourceProvider.getColorPrimary(context, brandedColorScheme);
         view.navigateButton.getBackground().setTint(colorPrimary);
