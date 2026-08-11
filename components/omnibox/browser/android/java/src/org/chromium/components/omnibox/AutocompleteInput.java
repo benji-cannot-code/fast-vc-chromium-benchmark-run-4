ff0350bceb718ba0340e99e3f175c9446588e683
Tomasz Wiszkowski <ender@google.com>
2026-08-11 16:00:17 -0700

    Draw a hairline separator on Desktop when attachments are present.
    Bypass-Check-License: Renamed files
    Change-Id: I72f0dd794f3fd086ea8d3dea1597b47046cdf541
    Fixed: 534435632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8238733
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677613}

--- components/omnibox/browser/android/java/src/org/chromium/components/omnibox/AutocompleteInput.java
+++ components/omnibox/browser/android/java/src/org/chromium/components/omnibox/AutocompleteInput.java
@@ -637,6 +637,10 @@ public class AutocompleteInput implements UserData {
         mHasAttachments = hasAttachments;
     }
 
+    public boolean hasAttachments() {
+        return mHasAttachments;
+    }
+
     public AutocompleteInput setSelection(TextSelection selection) {
         mSelection = selection;
         return this;
