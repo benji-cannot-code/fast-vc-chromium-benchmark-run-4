3222b1c61f7424678a9d2df35eed8a7d22606df9
David Pennington <dpenning@google.com>
2026-08-10 17:37:21 -0700

    [Glass] Only set translucent opacity initially with eligibility
    Bug: 543410881
    Change-Id: Iaac0ba0dda8c5da4cb8d5ac9f721bd4e730e6d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8201374
    Reviewed-by: Bryan Oltman <bryanoltman@google.com>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676849}

--- chrome/browser/ui/views/frame/browser_widget.cc
+++ chrome/browser/ui/views/frame/browser_widget.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_native_widget_factory.h"
 #include "chrome/browser/ui/views/frame/browser_root_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/frame/glass_frame_service.h"
 #include "chrome/browser/ui/views/frame/system_menu_model_builder.h"
 #include "chrome/browser/ui/views/frame/top_container_view.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
@@ -210,8 +211,11 @@ void BrowserWidget::InitBrowserWidget() {
     }
   }
 
-  if (features::IsGlassFrameEnabled()) {
-    params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
+  if (auto* const glass_frame_service = GlassFrameService::GetInstance()) {
+    if (glass_frame_service->IsBrowserWindowEligible(
+            browser_view_->browser())) {
+      params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
+    }
   }
 
   Init(std::move(params));
