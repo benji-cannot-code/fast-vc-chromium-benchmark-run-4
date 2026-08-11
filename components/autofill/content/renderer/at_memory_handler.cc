c85258bf92dab7a31f101d8e9fc540bf7bcd5310
Christoph Schwering <schwering@google.com>
2026-08-11 00:31:43 -0700

    [Autofill] Disable smart replace in AtMemory paste
    This CL disables smart replace when AtMemory pastes text into a field.
    
    Bug: 540805115
    Change-Id: Ie4e9664a8383f051de80d3849995afb7b847fa69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8232520
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677015}

--- components/autofill/content/renderer/at_memory_handler.cc
+++ components/autofill/content/renderer/at_memory_handler.cc
@@ -238,7 +238,7 @@ void AtMemoryHandler::ReplaceSelectionForAtMemory(WebElement& element,
 
   element.PasteText(WebString::FromUtf16(value),
                     /*replace_all=*/false,
-                    /*smart_replace=*/true);
+                    /*smart_replace=*/false);
 }
 
 std::optional<AtMemoryHandler::AskForValuesToFillInfo>
