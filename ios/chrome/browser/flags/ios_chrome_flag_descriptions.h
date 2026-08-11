52347a50618d0537c2027473bdc09df8a82cce03
Matt Reichhoff <mreichhoff@chromium.org>
2026-08-11 07:37:29 -0700

    [iOS] Re-drop unused flag strings
    Bad rebase in crrev.com/c/8128603, whoops.
    
    Change-Id: I7275f873ad5c87fc29d1c132fed3c01c87679656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8231963
    Commit-Queue: Kirubel <kaklilu@chromium.org>
    Auto-Submit: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Kirubel <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677190}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -1695,10 +1695,6 @@ inline constexpr char kPageContextPdfName[] = "Page Context PDF";
 inline constexpr char kPageContextPdfDescription[] =
     "Enables PDF support for Page Context in Gemini.";
 
-inline constexpr char kPageStabilityMetricsName[] = "Page Stability Metrics";
-inline constexpr char kPageStabilityMetricsDescription[] =
-    "Enables metrics to track DOM mutation frequency after user interactions.";
-
 inline constexpr char kPageToolsFeatureUnavailabilityName[] =
     "PageToolsFeatureUnavailability";
 inline constexpr char kPageToolsFeatureUnavailabilityDescription[] =
