ca193aa002d5bb2f8d5d544ece0e47037746c2c1
Wenbin Zhang <wenbinzhang@google.com>
2026-08-11 00:00:12 -0700

    [Bedrock] Fix and re-enable PinnedExtensionLayout on macOS
    Removes legacy unit-test-era '#if BUILDFLAG(IS_MAC)' workarounds from
    extensions_menu_view_browsertest.cc. In browser tests, macOS supports
    Views layout animations normally, and waiting for AnimatingLayoutManager
    ensures newly pinned toolbar action buttons reach their target layout
    coordinates before geometry assertions.
    
    Re-enables ExtensionsMenuViewBrowserTest.PinnedExtensionLayout on macOS.
    
    TAG=agy
    CONV=eeeb44ae-34fc-4d6f-9cc2-c072289133de
    
    Bug: 543043111, 543925326
    Change-Id: I0c48e530f318b9b193644a424bfe896b5454588a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8219470
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677006}

--- chrome/browser/ui/views/extensions/extensions_menu_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_view_browsertest.cc
@@ -150,11 +150,7 @@ class ExtensionsMenuViewBrowserTest : public InProcessBrowserTest {
   }
 
   void WaitForAnimation() {
-#if BUILDFLAG(IS_MAC)
-    // Animation is not reliable in tests on Mac or we avoid using animations.
-#else
     views::test::WaitForAnimatingLayoutManager(extensions_container());
-#endif
     LayoutContainerIfNecessary();
     LayoutMenuIfNecessary();
   }
@@ -191,14 +187,7 @@ class ExtensionsMenuViewBrowserTest : public InProcessBrowserTest {
       if (views::IsViewClass<ToolbarActionView>(child)) {
         ToolbarActionView* const action =
             static_cast<ToolbarActionView*>(child);
-#if BUILDFLAG(IS_MAC)
-        const bool is_visible =
-            extensions_container()->IsActionVisibleOnToolbar(
-                action->view_model()->GetId());
-#else
-        const bool is_visible = action->GetVisible();
-#endif
-        if (is_visible) {
+        if (action->GetVisible()) {
           result.push_back(action);
         }
       }
@@ -314,12 +303,10 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   ASSERT_TRUE(menu_item);
   ClickPinButton(menu_item);
   WaitForAnimation();
-#if !BUILDFLAG(IS_MAC)
   views::test::WaitForAnimatingLayoutManager(
       BrowserView::GetBrowserViewForBrowser(browser2)
           ->toolbar()
           ->extensions_container());
-#endif
 
   // Window that was already open gets the pinned extension.
   EXPECT_TRUE(is_action_visible_on_toolbar(browser2));
@@ -329,12 +316,10 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
       BrowserView::GetBrowserViewForBrowser(browser3)
           ->toolbar()
           ->extensions_container());
-#if !BUILDFLAG(IS_MAC)
   views::test::WaitForAnimatingLayoutManager(
       BrowserView::GetBrowserViewForBrowser(browser3)
           ->toolbar()
           ->extensions_container());
-#endif
 
   // Brand-new window also gets the pinned extension.
   EXPECT_TRUE(is_action_visible_on_toolbar(browser3));
@@ -364,13 +349,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   EXPECT_THAT(GetPinnedExtensionNames(), testing::ElementsAre(kName));
 }
 
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_PinnedExtensionLayout DISABLED_PinnedExtensionLayout
-#else
-#define MAYBE_PinnedExtensionLayout PinnedExtensionLayout
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
-                       MAYBE_PinnedExtensionLayout) {
+IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest, PinnedExtensionLayout) {
   for (int i = 0; i < 3; i++) {
     InstallExtension(base::StringPrintf("Test %d", i));
   }
@@ -492,12 +471,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   // Verify behavior before pin, after pin, and after unpin.
   for (int i = 0; i < 3; i++) {
     EXPECT_EQ(i, user_action_tester.GetActionCount(kPinButtonUserAction));
-#if BUILDFLAG(IS_MAC)
-    // TODO(crbug.com/40670141): No Mac animations in unit tests cause errors.
-#else
     EXPECT_EQ(i, counter.GetCount(ax::mojom::Event::kAlert));
     EXPECT_EQ(i, counter.GetCount(ax::mojom::Event::kTextChanged));
-#endif
     ClickPinButton(menu_item);
   }
 }
