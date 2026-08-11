0a50cfa8d8c277f214f90da90ed49841173ed694
Amarjot Gill <amarjotgill@google.com>
2026-08-11 09:05:09 -0700

    Introduce CoreMLDelegate in Bling.
    We will use CoreMLDelegate to allow passage_embedding TFLite model to
    offload computations to the device ANE
    
    Bug: 542668877
    Change-Id: I1fa9d9047f9d40d6ba0247fcbc9d0307f1e567ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8098066
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677261}

--- services/passage_embeddings/passage_embeddings_op_resolver.cc
+++ services/passage_embeddings/passage_embeddings_op_resolver.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "build/build_config.h"
 #include "services/on_device_model/ml_internal_buildflags.h"
 #include "third_party/tflite/buildflags.h"
 
@@ -16,11 +17,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(ENABLE_ML_INTERNAL)
 #include "services/on_device_model/ml/chrome_ml.h"  // nogncheck
 #endif
+#if BUILDFLAG(IS_IOS)
+#include "third_party/tflite/src/tensorflow/lite/delegates/coreml/coreml_delegate.h"
+#endif
 
 namespace passage_embeddings {
 
 HistoryOpResolver::HistoryOpResolver(bool allow_gpu_execution) {
-#if BUILDFLAG(ENABLE_ML_INTERNAL)
+#if BUILDFLAG(IS_IOS)
+  if (allow_gpu_execution) {
+    delegate_creators_.insert(
+        delegate_creators_.begin(), [](TfLiteContext* context) {
+          TfLiteCoreMlDelegateOptions options = {};
+          options.enabled_devices = TfLiteCoreMlDelegateDevicesWithNeuralEngine;
+          return std::unique_ptr<TfLiteDelegate, void (*)(TfLiteDelegate*)>(
+              TfLiteCoreMlDelegateCreate(&options), TfLiteCoreMlDelegateDelete);
+        });
+  }
+#elif BUILDFLAG(ENABLE_ML_INTERNAL)
   if (allow_gpu_execution) {
     auto* chrome_ml = ml::ChromeML::Get();
     if (chrome_ml && chrome_ml->HasCreateGpuDelegate() &&
