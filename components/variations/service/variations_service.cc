80600e1d4b7ee8b27b43e43cf9e0d2076b8589cc
Zaina Al-Mashni <zalmashni@google.com>
2026-08-10 18:26:52 -0700

    Add OnSeedFetched to VariationsService::Observer.
    This change adds an OnSeedFetched() method to the
    VariationsService::Observer interface, which is triggered when a
    variations seed is successfully fetched (either when a new seed is
    stored or when a 304 Not Modified response is received).
    
    Also renames the existing NotifyObservers() function to
    NotifyExperimentChangesDetected() to be more specific.
    
    Bug: 524959454
    Change-Id: I891bd79a847d0a55f8b7fc0f47ac43d64e55faf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8191658
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676891}

--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -864,6 +864,8 @@ void VariationsService::OnSeedStoreResult(bool is_delta_compressed,
     if (base::FeatureList::IsEnabled(kVariationsRuntimeMutability)) {
       SimulateAndApplyRuntimeMutableChanges(seed);
     }
+
+    NotifySeedFetched();
   }
 }
 
@@ -955,7 +957,8 @@ void VariationsService::FetchVariationsSeed() {
   DoActualFetch();
 }
 
-void VariationsService::NotifyObservers(const SeedSimulationResult& result) {
+void VariationsService::NotifyExperimentChangesDetected(
+    const SeedSimulationResult& result) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (result.kill_critical_group_change_count > 0) {
@@ -966,6 +969,14 @@ void VariationsService::NotifyObservers(const SeedSimulationResult& result) {
     for (auto& observer : observer_list_) {
       observer.OnExperimentChangesDetected(Observer::BEST_EFFORT);
     }
+  }
+}
+
+void VariationsService::NotifySeedFetched() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  for (auto& observer : observer_list_) {
+    observer.OnSeedFetched();
   }
 }
 
@@ -1135,7 +1146,7 @@ void VariationsService::PerformSimulationWithVersion(
       field_trial_creator_.GetClientFilterableStateForVersion(version);
   auto result = SimulateSeedStudies(seed, *client_state, *entropy_providers_);
 
-  NotifyObservers(result);
+  NotifyExperimentChangesDetected(result);
 }
 
 ApplyRuntimeMutableChangesResult VariationsService::ApplyRuntimeMutableChanges(
