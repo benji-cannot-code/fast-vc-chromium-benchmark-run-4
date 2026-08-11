19ff0c2ad9900997d9e4895fec724af8ae736982
Dominique Fauteux-Chapleau <domfc@chromium.org>
2026-08-11 12:01:04 -0700

    Implement ObfuscationRenameHandler
    This CL adds a new ObfuscationRenameHandler used to handle obfuscated
    downloads renaming logic. This will be used as part of a larger fix to
    address crbug.com/503210986 (see crrev.com/c/8179265 for WIP changes)
    
    Bug: 503210986
    Change-Id: Ia0cf678a4ea19c0b95cb5c98c8eeddd21e625b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8226084
    Reviewed-by: Elias Homsi <eliashomsi@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677427}

--- components/enterprise/obfuscation/core/download_obfuscator.h
+++ components/enterprise/obfuscation/core/download_obfuscator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file.h"
+#include "base/files/file_path.h"
 #include "base/supports_user_data.h"
 #include "base/types/expected.h"
 #include "components/enterprise/obfuscation/core/utils.h"
@@ -26,6 +27,7 @@ struct COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION) DownloadObfuscationData
   static const char kUserDataKey[];
 
   bool is_obfuscated = false;
+  base::FilePath original_target_path;
 };
 
 // DownloadObfuscator handles obfuscation or deobfuscation of download data.
