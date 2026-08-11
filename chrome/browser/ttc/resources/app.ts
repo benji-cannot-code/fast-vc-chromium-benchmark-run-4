f9507789140ad491382336b5c2e7963c5c191005
Gary Klassen <gklassen@chromium.org>
2026-08-11 09:29:44 -0700

    [ttc] Add fallback to default resource bundle URL and update branded CSP
    Change-Id: I12003180990497b2839f894178ba1da4dac1e979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8228348
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677282}

--- chrome/browser/ttc/resources/app.ts
+++ chrome/browser/ttc/resources/app.ts
@@ -19,7 +19,7 @@ import {AudioPlayer} from './audio_player.js';
 import {CaptionBlockManager, formatCaptions} from './caption_block_manager.js';
 import {LocalSpeechRecognition} from './local_speech_recognition.js';
 // <if expr="_google_chrome">
-import {Conversation, State} from './internal/conversation.js';
+import {Conversation, DEFAULT_TTC_BUNDLE_URL, State} from './internal/conversation.js';
 import type {ApiConfig, ConversationConfig, Persona} from './internal/conversation.js';
 export type ConversationMessage =|{
   type: 'inputTranscription',
@@ -67,6 +67,7 @@ type ConversationConfig = {
 };
 type ConversationMessage = any;
 type Persona = any;
+const DEFAULT_TTC_BUNDLE_URL = '';
 // </if>
 /* eslint-enable @typescript-eslint/no-explicit-any */
 import {errorLog, log} from './logging.js';
@@ -650,7 +651,8 @@ export class AppElement extends CrLitElement {
     this.initializationState = InitializationState.CONNECTING;
 
     try {
-      const ttcBundleUrl = loadTimeData.getString('ttcBundleUrl');
+      const ttcBundleUrl = loadTimeData.getString('ttcBundleUrl') ||
+          DEFAULT_TTC_BUNDLE_URL;
       const bundle = await this.initializeResourceBundle(ttcBundleUrl);
 
       log(FILE, 'Bundle initialized');
