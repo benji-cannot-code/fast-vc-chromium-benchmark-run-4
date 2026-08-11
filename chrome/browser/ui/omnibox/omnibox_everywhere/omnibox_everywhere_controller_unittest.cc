1c2947db8f9b5812cff04cbec89eddf254edc7f1
Kaan Alsan <alsan@chromium.org>
2026-08-11 09:46:46 -0700

    [OmniboxEverywhere] Update global hotkey to Option/Alt + Space
    Updates the combo per UX spec. This will be configurable eventually.
    
    Bug: 532203484
    Change-Id: I1a8eb80b6e7a3c84eebafb3720978eca64ef6b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8228848
    Auto-Submit: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677297}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_controller_unittest.cc
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_controller_unittest.cc
@@ -173,8 +173,7 @@ TEST_F(OmniboxEverywhereControllerTest, HotkeyPrefDisablesHotkey) {
           }),
       &fake_listener);
 
-  ui::Accelerator hotkey(ui::VKEY_SPACE,
-                         ui::EF_SHIFT_DOWN | ui::EF_PLATFORM_ACCELERATOR);
+  ui::Accelerator hotkey(ui::VKEY_SPACE, ui::EF_ALT_DOWN);
 
   // Controller should register the hotkey on initialization if pref is enabled.
   EXPECT_TRUE(fake_listener.IsRegistered(hotkey));
@@ -201,8 +200,7 @@ TEST_F(OmniboxEverywhereControllerTest, ControllerInitWithDisabledHotkeyPref) {
           }),
       &fake_listener);
 
-  ui::Accelerator hotkey(ui::VKEY_SPACE,
-                         ui::EF_SHIFT_DOWN | ui::EF_PLATFORM_ACCELERATOR);
+  ui::Accelerator hotkey(ui::VKEY_SPACE, ui::EF_ALT_DOWN);
 
   // Controller should NOT register the hotkey on initialization if pref is
   // disabled.
