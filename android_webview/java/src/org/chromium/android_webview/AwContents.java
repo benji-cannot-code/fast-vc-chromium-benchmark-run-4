02d9b8ab0a9a0bce0d0c446087feaaa898e0150b
Rupert Wiser <bewise@chromium.org>
2026-08-11 09:50:10 -0700

    webview: Rescale fonts on AwContents#adopt
    AwSettings was previously only calculating the system font scale during
    initialization. When adopting an AwContents into a new Context, the text
    scale would become stale or incorrectly compound against the old display
    settings.
    
    This extracts the font scaling logic so we can cleanly retrigger it
    dynamically against the new Context on adopt.
    
    Bug: b:531633054
    Change-Id: Ic2060ccb415c658b977ff2b4ac80c1598dc3b1cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8219550
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677302}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1451,6 +1451,11 @@ public class AwContents implements SmartClipProvider {
      */
     private void updateContext(Context newContext) {
         mContext = ClassLoaderContextWrapperFactory.get(newContext);
+
+        if (mSettings != null) {
+            mSettings.updateContext();
+        }
+
         WindowAndroidWrapper newWindowAndroid = getWindowAndroid(newContext);
         if (newWindowAndroid != mWindowAndroid) {
             mWindowAndroid = newWindowAndroid;
