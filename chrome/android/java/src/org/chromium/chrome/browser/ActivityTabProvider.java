46b7154d1d48ef85f899aaf7f557259d65936db9
Hitarth Kothari <hitarthkothari@google.com>
2026-08-12 10:32:30 -0700

    [WillCloseTabs] Handle last tab closure in ActivityTabProvider under tab closure refactor.
    This CL implements the willCloseTabs observer method in
    ActivityTabProvider to ensure that an activity tab change event is
    triggered when the last tab is closed under the
    TAB_CLOSURE_METHOD_REFACTOR feature. It also adds corresponding unit
    tests to verify this behavior when the feature is enabled.
    
    Bug: 545136199
    Change-Id: I1806cd308abba2479320fe1013e336f072c307c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239796
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678159}

--- chrome/android/java/src/org/chromium/chrome/browser/ActivityTabProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ActivityTabProvider.java
@@ -26,6 +26,7 @@ import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 /** A class that provides the current {@link Tab} for various states of the browser's activity. */
@@ -147,6 +148,16 @@ public class ActivityTabProvider implements Destroyable, Supplier<@Nullable Tab>
                         // If this is the last tab to close, make sure a signal is sent to the
                         // observers.
                         if (selector.getCurrentModel().getCount() <= 1) {
+                            triggerActivityTabChangeEvent(null);
+                        }
+                    }
+
+                    @Override
+                    public void willCloseTabs(
+                            List<Tab> tabs, boolean isAllTabs, boolean allowUndo) {
+                        // If all remaining tabs are closing, make sure a signal is sent to the
+                        // observers.
+                        if (isAllTabs) {
                             triggerActivityTabChangeEvent(null);
                         }
                     }
