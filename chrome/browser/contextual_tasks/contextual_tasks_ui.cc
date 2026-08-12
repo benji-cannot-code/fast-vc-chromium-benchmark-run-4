43e420035beff25c2913933e87683cf51dd8c4ad
Caitlin Chen <caitc@google.com>
2026-08-12 13:33:04 -0700

    [cobrowse] Support Close Button Text and Help Link in Contextual Tasks Tooltips
    - Configure askGTooltip to use a text close button "Got it".
    - Add support for an optional help link in contextual-tasks-info-tooltip.
    - Configure askGTooltip to show a "Learn More" link to https://support.google.com/chrome/answer/17025061.
    
    Before: http://screenshot/5nc4s93pCSwH4o4
    After: http://screenshot/2fuht8vo37gr0
    BUG: b:545342860
    Change-Id: I09dca3898c539311d0a4074433b4cc3d41759530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242409
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Commit-Queue: Caitlin Chen <caitc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678278}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -473,6 +473,7 @@ base::DictValue ContextualTasksUI::GetContextualTasksLoadTimeData(
       {"continueThread", IDS_CONTEXTUAL_TASKS_CONTINUE_THREAD_MESSAGE},
       {"feedback", IDS_LENS_SEND_FEEDBACK},
       {"help", IDS_CONTEXTUAL_TASKS_MENU_HELP},
+      {"learnMore", IDS_LEARN_MORE},
       {"moreOptionsTooltip",
        IDS_CONTEXTUAL_TASKS_SIDE_PANEL_MORE_OPTIONS_TOOL_TIP},
       {"myActivity", IDS_CONTEXTUAL_TASKS_MENU_MY_ACTIVITY},
@@ -489,6 +490,8 @@ base::DictValue ContextualTasksUI::GetContextualTasksLoadTimeData(
       {"onboardingBody", IDS_CONTEXTUAL_TASKS_FIRST_RUN_EXPERIENCE_DESCRIPTION},
       {"onboardingLink", IDS_CONTEXTUAL_TASKS_FIRST_RUN_EXPERIENCE_LEARN_MORE},
       {"onboardingAcceptButton",
+       IDS_CONTEXTUAL_TASKS_FIRST_RUN_EXPERIENCE_ACCEPT_BUTTON},
+      {"lensSearchTooltipAcceptButton",
        IDS_CONTEXTUAL_TASKS_FIRST_RUN_EXPERIENCE_ACCEPT_BUTTON},
       {"lensSearchTooltipTitle", IDS_LENS_COBROWSE_IPH_HEADER},
       {"lensSearchTooltipBody", IDS_LENS_COBROWSE_IPH_DESCRIPTION},
@@ -564,6 +567,8 @@ base::DictValue ContextualTasksUI::GetContextualTasksLoadTimeData(
 
   dict.Set("onboardingLinkUrl",
            contextual_tasks::GetContextualTasksOnboardingTooltipHelpUrl());
+  dict.Set("askGHelpUrl",
+           contextual_tasks::GetContextualTasksTabHelpUrl());
   dict.Set("composeboxImageFileTypes",
            contextual_tasks::kContextualTasksNextboxImageFileTypes.Get());
   dict.Set("composeboxAttachmentFileTypes",
