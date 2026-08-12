f9f334fcf45452bf11704fe8223e5ac5d8e55bde
Michael Tatarski <mtatarski@google.com>
2026-08-11 20:39:23 -0700

    [Code Health] Fix unsafe buffer access in remoting cli_util
    This CL eliminates UNSAFE_TODO and raw buffer manipulation in ReadString
    by using std::getline from std::cin.
    
    Bug: 490886229
    Change-Id: Ibc524a93dfda7f967a399641ad25b1186054b1a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8226985
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677748}

--- remoting/test/cli_util.cc
+++ remoting/test/cli_util.cc
@@ -5,10 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/test/cli_util.h"
 
-#include <string.h>
+#include <iostream>
 
 #include "base/command_line.h"
-#include "base/compiler_specific.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/thread_pool.h"
@@ -66,17 +65,13 @@ void RunCommandOptionsLoop(const std::vector<CommandOption>& options) {
 }
 
 std::string ReadString() {
-  const int kMaxLen = 1024;
-  std::string str(kMaxLen, 0);
-  char* result = UNSAFE_TODO(fgets(&str[0], kMaxLen, stdin));
-  if (!result) {
+  std::string str;
+  if (!std::getline(std::cin, str)) {
     return std::string();
   }
-  size_t newline_index = str.find('\n');
-  if (newline_index != std::string::npos) {
-    str[newline_index] = '\0';
+  if (!str.empty() && str.back() == '\r') {
+    str.pop_back();
   }
-  str.resize(strlen(&str[0]));
   return str;
 }
 
