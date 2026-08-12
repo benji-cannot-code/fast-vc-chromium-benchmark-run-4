876682547040d7c2ffa13d7d0176473817ebcbd3
Shiyi Zou <shiyi.zou@intel.com>
2026-08-11 22:31:14 -0700

    webnn: log the selected EP device using `EpDeviceInfo::ToSwitchValue()`
    This CL reuses `EpDeviceInfo`'s method `ToSwitchValue()` for logging the
    selected EP device in `Environment::SelectEpDeviceForCompiler()`.
    
    No functional change.
    
    Bug: None
    Change-Id: Id2b248999d5e448ef7ef959368bdf47751ece8fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8246378
    Commit-Queue: Hu, Ningxin <ningxin.hu@intel.com>
    Auto-Submit: Zou, Shiyi <shiyi.zou@intel.com>
    Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1677775}

--- services/webnn/public/cpp/ep_device_info.h
+++ services/webnn/public/cpp/ep_device_info.h
@@ -17,8 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn {
 
-// Identifies an execution provider device by EP name, device type, and hardware
-// device ID. This is the C++ counterpart of webnn.mojom.EpDeviceInfo.
+// Identifies an execution provider device by EP name, device type, hardware
+// device ID and vendor ID. This is the C++ counterpart of
+// webnn.mojom.EpDeviceInfo.
 struct COMPONENT_EXPORT(WEBNN_PUBLIC_CPP_WIN) EpDeviceInfo {
   std::string ep_name;
   mojom::Device device_type = mojom::Device::kCpu;
