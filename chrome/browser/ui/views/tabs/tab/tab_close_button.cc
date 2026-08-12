5a4a18c6766a09c613aeb57a260ffe0ac919d7f8
Emily Shack <emshack@chromium.org>
2026-08-11 19:40:03 -0700

    [GlowUp] Remove the _weightXXX icon suffix
    Removes this suffix from the icon checker presubmit, and converts the
    two icons that use it to custom icons, per feedback by dayeung@.
    
    Bug: 525119489
    Change-Id: Ie804d7bc41d1756ca24af4134d5865d4905dccfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242687
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677724}

--- chrome/browser/ui/views/tabs/tab/tab_close_button.cc
+++ chrome/browser/ui/views/tabs/tab/tab_close_button.cc
@@ -208,7 +208,7 @@ bool TabCloseButton::GetHitTestMask(SkPath* mask) const {
 }
 void TabCloseButton::UpdateIcon() {
   const auto& icon = features::IsRoundedIconsEnabled()
-                         ? kCloseWeight500Icon
+                         ? kCloseWeight500CustomIcon
                          : kCloseTabChromeRefreshOldIcon;
 
   SetImageModel(views::Button::STATE_NORMAL,
