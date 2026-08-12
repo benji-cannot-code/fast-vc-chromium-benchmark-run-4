5fbbe70b5a07cd4c269c90990a17a9dd2ce72d96
Michael Tatarski <mtatarski@google.com>
2026-08-12 09:16:51 -0700

    [Code health] Remove DanglingUntriaged in PrefServiceFlagsStorage
    This CL removes the DanglingUntriaged annotation from prefs_ in
    PrefServiceFlagsStorage by resetting flags_storage_ when closing the
    Chrome Labs bubble and during test teardown.
    
    Bug: 40061562, 40212619
    Change-Id: Ib400e9399037ab17c2d1a7e83a7163992280763a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8244368
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678097}

--- components/webui/flags/pref_service_flags_storage.h
+++ components/webui/flags/pref_service_flags_storage.h
@@ -51,7 +51,8 @@ class PrefServiceFlagsStorage : public FlagsStorage {
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
  private:
-  raw_ptr<PrefService, DanglingUntriaged> prefs_;
+  // Pointed-to PrefService outlives PrefServiceFlagsStorage instances.
+  raw_ptr<PrefService> prefs_;
 };
 
 }  // namespace flags_ui
