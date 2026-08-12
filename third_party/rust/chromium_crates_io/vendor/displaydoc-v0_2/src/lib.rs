057d0b8651ce708afc20fc7422bc655942e9f07e
Lukasz Anforowicz <lukasza@chromium.org>
2026-08-11 22:45:58 -0700

    Roll displaydoc: 0.2.6 => 0.2.7 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * displaydoc: 0.2.6 => 0.2.7; https://docs.rs/crate/displaydoc/0.2.7
    
    Bug: None
    Change-Id: I4f21e761e2b73f4ee9c65dcd3fe276503947c589
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8232338
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677780}

--- third_party/rust/chromium_crates_io/vendor/displaydoc-v0_2/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/displaydoc-v0_2/src/lib.rs
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! displaydoc = "0.2"
 //! ```
 //!
-//! *Compiler support: requires rustc 1.56+*
+//! *Compiler support: requires rustc 1.71+*
 //!
 //! <br>
 //!
