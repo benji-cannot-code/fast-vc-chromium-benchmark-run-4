4f91e6abb9d60e275dc428fd6c04f9a0b598a338
Huanpo Lin <robertlin@chromium.org>
2026-08-11 18:07:14 -0700

    Revert "[GlowUp] Round colored cr_components/searchbox/ icons"
    This reverts commit d361ca961652a6a314d486aa71b3753ede6de057.
    
    Reason for revert:
    All/RealboxHandlerTest.AnswerVectorIcons/1
    All/RealboxHandlerTest.MatchVectorIcons/1
    are failing and this CL seems to be related to the failure.
    
    Failure Link:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/111578/overview
    
    
    Original change's description:
    > [GlowUp] Round colored cr_components/searchbox/ icons
    >
    > These .svg icons define a stroke or fill color, but this color is always
    > overridden in practice so we don't need to maintain it or treat these as
    > custom icons. See go/chrome-icon-modernization-dd.
    >
    > Binary-Size: Increase is temporary, will go back down after launch
    > Bug: 511303545
    > Change-Id: I69343946242e6ce2abe10056480b1d6e5c89ade5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8234158
    > Commit-Queue: Emily Shack <emshack@chromium.org>
    > Reviewed-by: Josiah Kiehl <kiehl@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1677502}
    
    Bug: 511303545
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia9b841eeba55ba31fc18bbc02411706cf23ec06b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242268
    Owners-Override: Huanpo Lin <robertlin@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677692}

--- chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.cc
+++ chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/content_security_policy.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/pointer/touch_ui_controller.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/webui/webui_util.h"
 
 namespace {
@@ -73,10 +72,7 @@ void PopulateAiModeButtonUiConfig(content::WebUIDataSource* source,
   // Use AIM button service to dynamically populate the various AIM button
   // properties based on the current config, if present.
   GURL compose_icon(
-      features::IsRoundedIconsEnabled()
-          ? "chrome://resources/cr_components/searchbox/icons/search_spark.svg"
-          : "chrome://resources/cr_components/searchbox/icons/"
-            "search_spark_old.svg");
+      "chrome://resources/cr_components/searchbox/icons/search_spark.svg");
   if (auto* service = AiModeButtonServiceFactory::GetForProfile(profile)) {
     if (const auto* config = service->GetCurrentConfig()) {
       source->AddString("searchboxComposeButtonText", config->text);
--- ui/webui/resources/cr_components/searchbox/searchbox_compose_button.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_compose_button.ts
@@ -119,9 +119,7 @@ export class SearchboxComposeButtonElement extends
       loadTimeData.getBoolean('energyEffectAnimationEnabled');
 
   protected accessor arrowIcon_: string =
-      (document.documentElement.hasAttribute('webui-rounded-icons') ?
-           '//resources/cr_components/searchbox/icons/arrow_forward.svg' :
-           '//resources/cr_components/searchbox/icons/arrow_forward_old.svg');
+      '//resources/cr_components/searchbox/icons/arrow_forward.svg';
 
   protected accessor showAnimation_: boolean = false;
 
