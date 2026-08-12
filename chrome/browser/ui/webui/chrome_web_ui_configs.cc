5dc7d194cb19c4df20ea057878c01fac42f3b538
Ryosuke Miyake <rysk@google.com>
2026-08-12 10:23:11 -0700

    Fix missing bluetooth_internals mojom dependencies in configs
    chrome_web_ui_configs.cc includes bluetooth_internals_ui.h to register
    BluetoothInternalsUIConfig, which includes
    bluetooth_internals.mojom-forward.h.
    
    However:
    - source_set("bluetooth_internals") declared :mojo_bindings
      under private deps instead of public_deps, preventing consumers of its
      public header from receiving the generated mojom dependency.
    - source_set("configs") omitted
      //chrome/browser/ui/webui/bluetooth_internals from deps, relying on a
      // nogncheck annotation.
    
    This caused compilation failures under Siso's fail-on-bad-deps check on
    Windows.
    
    This CL:
    - Moves :mojo_bindings to public_deps in
      //chrome/browser/ui/webui/bluetooth_internals.
    - Adds //chrome/browser/ui/webui/bluetooth_internals to configs deps.
    - Removes the // nogncheck comment from chrome_web_ui_configs.cc.
    
    Bug: 545386333
    Change-Id: I975940cf6d8fc4aaf33c0eedf1ac586a30833993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8247458
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Ryosuke Miyake <rysk@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678149}

--- chrome/browser/ui/webui/chrome_web_ui_configs.cc
+++ chrome/browser/ui/webui/chrome_web_ui_configs.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/actor_internals/actor_internals_ui.h"
 #include "chrome/browser/ui/webui/autofill_and_password_manager_internals/autofill_internals_ui.h"
 #include "chrome/browser/ui/webui/autofill_and_password_manager_internals/password_manager_internals_ui.h"
-#include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_ui.h"  // nogncheck
+#include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_ui.h"
 #include "chrome/browser/ui/webui/chrome_finds_internals/chrome_finds_internals_ui.h"
 #include "chrome/browser/ui/webui/chrome_urls/chrome_urls_ui.h"
 #include "chrome/browser/ui/webui/commerce/commerce_internals_ui_config.h"
