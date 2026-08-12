1eddf5f85ae7d5772c65282e13cffe0933810004
Michael Tatarski <mtatarski@google.com>
2026-08-11 22:00:04 -0700

    [Code health] Use WeakPtr for DesktopMediaListController in list views
    This CL converts the controller reference in DesktopMediaListView and
    DesktopMediaTabList to base::WeakPtr to avoid dangling raw_ptr
    references.
    
    During DesktopMediaPickerDialogView destruction, the categories_
    container owning DesktopMediaListController instances is destructed
    before base views::View::~View() tears down the child view tree
    containing DesktopMediaListView and DesktopMediaTabList.
    
    Bug: 530543316
    Change-Id: I69b15117ee291d0360a94fb8fc4fbeb7037aad85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8231559
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677764}

--- chrome/browser/ui/views/desktop_capture/desktop_media_list_view.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_list_view.cc
@@ -74,7 +74,7 @@ DesktopMediaListView::DesktopMediaListView(
     DesktopMediaSourceViewStyle generic_style,
     DesktopMediaSourceViewStyle single_style,
     const std::u16string& accessible_name)
-    : controller_(controller),
+    : controller_(controller ? controller->GetWeakPtr() : nullptr),
       single_style_(single_style),
       generic_style_(generic_style) {
   SetBorder(views::CreateEmptyBorder(16));
@@ -88,7 +88,9 @@ DesktopMediaListView::DesktopMediaListView(
 DesktopMediaListView::~DesktopMediaListView() = default;
 
 void DesktopMediaListView::OnSelectionChanged() {
-  controller_->OnSourceSelectionChanged();
+  if (controller_) {
+    controller_->OnSourceSelectionChanged();
+  }
 }
 
 bool DesktopMediaListView::OnKeyPressed(const ui::KeyEvent& event) {
@@ -157,6 +159,9 @@ void DesktopMediaListView::ClearSelection() {
 }
 
 void DesktopMediaListView::OnSourceAdded(size_t index) {
+  if (!controller_) {
+    return;
+  }
   const DesktopMediaList::Source& source = controller_->GetSource(index);
 
   DesktopMediaSourceView* source_view =
@@ -198,6 +203,10 @@ void DesktopMediaListView::OnSourceAdded(size_t index) {
 }
 
 void DesktopMediaListView::OnSourceRemoved(size_t index) {
+  if (!controller_) {
+    return;
+  }
+
   DesktopMediaSourceView* view = AsDesktopMediaSourceView(children()[index]);
   DCHECK(view);
 
@@ -228,6 +237,9 @@ void DesktopMediaListView::OnSourceMoved(size_t old_index, size_t new_index) {
 }
 
 void DesktopMediaListView::OnSourceNameChanged(size_t index) {
+  if (!controller_) {
+    return;
+  }
   const DesktopMediaList::Source& source = controller_->GetSource(index);
   DesktopMediaSourceView* source_view =
       AsDesktopMediaSourceView(children()[index]);
@@ -235,6 +247,9 @@ void DesktopMediaListView::OnSourceNameChanged(size_t index) {
 }
 
 void DesktopMediaListView::OnSourceThumbnailChanged(size_t index) {
+  if (!controller_) {
+    return;
+  }
   const DesktopMediaList::Source& source = controller_->GetSource(index);
   DesktopMediaSourceView* source_view =
       AsDesktopMediaSourceView(children()[index]);
@@ -253,6 +268,10 @@ void DesktopMediaListView::OnDelegatedSourceListSelection() {
 }
 
 void DesktopMediaListView::SetStyle(const DesktopMediaSourceViewStyle& style) {
+  if (!controller_) {
+    return;
+  }
+
   const size_t old_columns = active_style_.columns;
   const gfx::Size old_item_size = active_style_.item_size;
   active_style_ = style;
