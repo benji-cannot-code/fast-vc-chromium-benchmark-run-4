15235b8e4a63cae45dc42001934a39b3d1fd2fb3
Hitarth Kothari <hitarthkothari@google.com>
2026-08-12 10:26:13 -0700

    [WillCloseTabs] Hide vertical tab hover card on willCloseTabs.
    Updates VerticalTabListCoordinator to hide the tab hover card when
    willCloseTabs is called, supporting the tab closure method refactor.
    Unit tests are also updated to cover both the legacy and refactored tab
    closure paths.
    
    Bug: 545136199
    Change-Id: I7fb5b137518353fd4b24f9bcbe49fcba1845415d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8243575
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678154}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/VerticalTabListCoordinator.java
@@ -713,6 +713,12 @@ public class VerticalTabListCoordinator {
                     }
 
                     @Override
+                    public void willCloseTabs(
+                            List<Tab> tabs, boolean isAllTabs, boolean allowUndo) {
+                        mTabHoverCardController.hideHoverCard();
+                    }
+
+                    @Override
                     public void tabClosureCommitted(Tab tab) {
                         mTabHoverCardController.hideHoverCard();
                     }
