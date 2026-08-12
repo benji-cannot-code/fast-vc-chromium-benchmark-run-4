91af64fe16f16a8afeb36d9bc7b6472c39fafc74
Hitarth Kothari <hitarthkothari@google.com>
2026-08-12 11:01:37 -0700

    [WillCloseTabs] Implement willCloseTabs in TabModelNotificationDotManager.
    This CL adds the willCloseTabs override to
    TabModelNotificationDotManager's TabModelObserver to ensure notification
    dots are updated correctly when multiple tabs are closed. This change is
    part of the TAB_CLOSURE_METHOD_REFACTOR feature. Unit tests have been
    updated and added to cover this new behavior.
    
    Bug: 545136199
    Change-Id: I31f96a1ba1876272b9ddfa98b6b08ed6ed09e67e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242511
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678180}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabModelNotificationDotManager.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabModelNotificationDotManager.java
@@ -96,6 +96,14 @@ public class TabModelNotificationDotManager implements Destroyable {
                 }
 
                 @Override
+                public void willCloseTabs(
+                        List<Tab> tabs, boolean isAllTabs, boolean allowUndo) {
+                    for (Tab tab : tabs) {
+                        maybeUpdateForTab(tab, /* mayAddDot= */ false);
+                    }
+                }
+
+                @Override
                 public void onFinishingTabClosure(Tab tab, @TabClosingSource int closingSource) {
                     maybeUpdateForTab(tab, /* mayAddDot= */ false);
                 }
