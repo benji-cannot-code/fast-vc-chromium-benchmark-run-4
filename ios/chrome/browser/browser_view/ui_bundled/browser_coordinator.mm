096510d7dc1dfd229bbe9d502606e15c7a65b281
Gauthier Ambard <gambard@chromium.org>
2026-08-12 10:23:51 -0700

    [iOS] Extract command handling out of BrowserCoordinator (9/N)
    This CL extracts some command protocols out of BrowserCoordinator
    into a dedicated object.
    This dedicated object is only responsible for handling those commands.
    
    Bug: 542879805
    Change-Id: Iebdfb25f864dd03aa67725c53994c7184204873c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8249519
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678150}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -149,7 +149,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_controller.h"
 #import "ios/chrome/browser/passwords/bottom_sheet/coordinator/credential_suggestion_bottom_sheet_coordinator.h"
 #import "ios/chrome/browser/passwords/model/password_controller_delegate.h"
-#import "ios/chrome/browser/passwords/password_suggestion/coordinator/password_suggestion_coordinator.h"
 #import "ios/chrome/browser/popup_menu/coordinator/popup_menu_coordinator.h"
 #import "ios/chrome/browser/popup_menu/overflow_menu/public/features.h"
 #import "ios/chrome/browser/prerender/model/prerender_browser_agent.h"
@@ -223,7 +222,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/non_modal_signin_promo_commands.h"
 #import "ios/chrome/browser/shared/public/commands/omnibox_commands.h"
 #import "ios/chrome/browser/shared/public/commands/open_new_tab_command.h"
-#import "ios/chrome/browser/shared/public/commands/password_suggestion_commands.h"
 #import "ios/chrome/browser/shared/public/commands/policy_change_commands.h"
 #import "ios/chrome/browser/shared/public/commands/popup_menu_commands.h"
 #import "ios/chrome/browser/shared/public/commands/promos_manager_commands.h"
@@ -351,8 +349,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     OverscrollActionsControllerDelegate,
     PasswordControllerDelegate,
     PasswordSettingsCoordinatorDelegate,
-    PasswordSuggestionCommands,
-    PasswordSuggestionCoordinatorDelegate,
     PolicyChangeCommands,
     PrerenderBrowserAgentDelegate,
     PromosManagerCommands,
@@ -461,10 +457,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Coordinator for the password settings UI presentation.
 @property(nonatomic, strong)
     PasswordSettingsCoordinator* passwordSettingsCoordinator;
-
-// Coordinator for the password suggestion UI presentation.
-@property(nonatomic, strong)
-    PasswordSuggestionCoordinator* passwordSuggestionCoordinator;
 
 // Coordinator for the popup menu.
 @property(nonatomic, strong) PopupMenuCoordinator* popupMenuCoordinator;
@@ -1066,7 +1058,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     @protocol(ReaderModeCommands),
     @protocol(NewTabPageCommands),
     @protocol(NonModalSignInPromoCommands),
-    @protocol(PasswordSuggestionCommands),
     @protocol(PolicyChangeCommands),
     @protocol(QuickDeleteCommands),
     @protocol(SendTabToSelfCommands),
@@ -1425,8 +1416,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   /* passwordSettingsCoordinator is created and started by a delegate method */
 
-  /* passwordSuggestionCoordinator is created and started by a BrowserCommand */
-
   /* paymentsScanCoordinator is created and started by a BrowserCommand */
 
   /* paymentsSuggestionBottomSheetCoordinator is created and started by a
@@ -1510,9 +1499,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self.passKitCoordinator stop];
   self.passKitCoordinator = nil;
-
-  [self.passwordSuggestionCoordinator stop];
-  self.passwordSuggestionCoordinator = nil;
 
   [self.printCoordinator stop];
   self.printCoordinator = nil;
@@ -2323,9 +2309,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self hideReaderModeBlurOverlay];
 
-  [self.passwordSuggestionCoordinator stop];
-  self.passwordSuggestionCoordinator = nil;
-
   [self stopSendTabToSelf];
 
   [self.passwordSettingsCoordinator stop];
@@ -3022,36 +3005,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ->SetPresentationContext(nullptr);
 }
 
-#pragma mark - PasswordSuggestionCommands
-
-- (void)showPasswordSuggestion:(NSString*)passwordSuggestion
-                     proactive:(BOOL)proactive
-                      webState:(web::WebState*)webState
-                         frame:(base::WeakPtr<web::WebFrame>)frame
-               decisionHandler:(void (^)(BOOL accept))decisionHandler {
-  // Do not present the bottom sheet if the calling web state does not match the
-  // active web state in order to stop the bottom sheet from showing in a tab
-  // different than the one that triggered it.
-  if (webState != self.activeWebState) {
-    return;
-  }
-
-  // Do not present the bottom sheet if it is already being presented.
-  if (self.passwordSuggestionCoordinator) {
-    return;
-  }
-
-  self.passwordSuggestionCoordinator = [[PasswordSuggestionCoordinator alloc]
-      initWithBaseViewController:self.viewController
-                         browser:self.browser
-              passwordSuggestion:passwordSuggestion
-                           frame:frame
-                 decisionHandler:decisionHandler
-                       proactive:proactive];
-  self.passwordSuggestionCoordinator.delegate = self;
-  [self.passwordSuggestionCoordinator start];
-}
-
 #pragma mark - PolicyChangeCommands
 
 - (void)showForceSignedOutPrompt {
@@ -3299,13 +3252,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
              mailComposerContext:context];
 
   [self.netExportCoordinator start];
-}
-
-#pragma mark - PasswordSuggestionCoordinatorDelegate
-
-- (void)closePasswordSuggestion {
-  [self.passwordSuggestionCoordinator stop];
-  self.passwordSuggestionCoordinator = nil;
 }
 
 #pragma mark - PrerenderBrowserAgentDelegate methods
