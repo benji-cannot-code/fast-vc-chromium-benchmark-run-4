2397e7fd4a2b117f740e7e5849d5e3faa511c110
Caroline Rising <corising@chromium.org>
2026-08-11 17:29:23 -0700

    [Unified tabstrip] Update horizontal tab strip height
    This matches the height used for the old horizontal tab strip.
    
    Bug: 543896817
    Change-Id: I8df4b89f9dd4aa32ba269e01627929bb51885300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239992
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677664}

--- chrome/browser/ui/views/tabs/common/unpinned_tab_container_view_layout.cc
+++ chrome/browser/ui/views/tabs/common/unpinned_tab_container_view_layout.cc
@@ -77,8 +77,7 @@ views::ProposedLayout UnpinnedTabContainerViewLayout::CalculateHorizontalLayout(
     return layouts;
   }
 
-  const int container_height = size_bounds.height().value_or(
-      GetLayoutConstant(LayoutConstant::kTabHeight));
+  const int container_height = TabStyle::Get()->GetStandardHeight();
 
   TabStripCollectionLayoutInfo collection = CollectVisibleChildLayoutInfo(
       children, container_height,
@@ -253,7 +252,7 @@ gfx::Size UnpinnedTabContainerViewLayout::CalculateHorizontalMinimumSize(
         GetChildOverlap(visible_children[i], visible_children[i + 1]);
   }
   min_width = std::max(0, min_width - overlap_total);
-  return gfx::Size(min_width, GetLayoutConstant(LayoutConstant::kTabHeight));
+  return gfx::Size(min_width, TabStyle::Get()->GetStandardHeight());
 }
 
 gfx::Size UnpinnedTabContainerViewLayout::CalculateVerticalMinimumSize(
