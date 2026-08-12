9a09a6198a7e0512072f6da13656f4a04f261d23
Viktor Semeniuk <vsemeniuk@google.com>
2026-08-12 03:26:18 -0700

    Add a metric to record where exactly password leak happened
    Possible values include https, http, localhost, private IP, android
    app, other.
    
    Change-Id: I9f627fa8c437b342535cf8b2038e839e727712c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239693
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677914}

--- components/password_manager/core/browser/leak_detection_delegate.cc
+++ components/password_manager/core/browser/leak_detection_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 
 #include "base/barrier_callback.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/types/expected.h"
 #include "build/build_config.h"
 #include "components/affiliations/core/browser/affiliation_service.h"
@@ -32,7 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
+#include "net/base/ip_address.h"
+#include "net/base/url_util.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "url/gurl.h"
+#include "url/url_constants.h"
 
 namespace password_manager {
 namespace {
@@ -72,6 +77,27 @@ LeakedPasswordDetails MergeResponses(
   CHECK(details.has_value());
   details->credentials.change_password_url = std::move(url);
   return std::move(*details);
+}
+
+LeakDetectionUrlType GetLeakDetectionUrlType(const GURL& url) {
+  if (net::IsLocalhost(url)) {
+    return LeakDetectionUrlType::kLocalhost;
+  }
+  if (url.SchemeIs(url::kAndroidScheme)) {
+    return LeakDetectionUrlType::kAndroidApp;
+  }
+  net::IPAddress ip_address;
+  if (ip_address.AssignFromIPLiteral(url.HostNoBracketsPiece()) &&
+      !ip_address.IsPubliclyRoutable()) {
+    return LeakDetectionUrlType::kPrivateOrIntranetIp;
+  }
+  if (url.SchemeIs(url::kHttpsScheme)) {
+    return LeakDetectionUrlType::kHttps;
+  }
+  if (url.SchemeIs(url::kHttpScheme)) {
+    return LeakDetectionUrlType::kHttp;
+  }
+  return LeakDetectionUrlType::kOther;
 }
 
 }  // namespace
@@ -135,6 +161,10 @@ void LeakDetectionDelegate::OnLeakDetectionDone(
   if (!is_leaked) {
     return;
   }
+
+  base::UmaHistogramEnumeration(
+      "PasswordManager.LeakDetection.LeakedCredentialsUrlType",
+      GetLeakDetectionUrlType(credentials.url));
 
   auto notify_callback =
       base::BindOnce(&LeakDetectionDelegate::NotifyUserCredentialsWereLeaked,
