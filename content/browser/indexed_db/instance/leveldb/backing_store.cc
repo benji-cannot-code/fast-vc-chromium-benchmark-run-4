944f8f3e0fc90dc4a00030c47df8c78f79932cd0
Michael Tatarski <mtatarski@google.com>
2026-08-12 11:56:04 -0700

    [Code health] Remove UnsafeDanglingUntriaged in WriteNewBlobs
    This CL replaces the UnsafeDanglingUntriaged raw pointer annotation
    in BackingStore::Transaction::WriteNewBlobs with base::Unretained.
    
    The pointed-to IndexedDBExternalObject is owned by the Transaction
    and is not mutated while serialization is in flight.
    
    Bug: 40061562
    Change-Id: I58a030ae72e6f348bf65f98ab1d24671a405e125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8230494
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678213}

--- content/browser/indexed_db/instance/leveldb/backing_store.cc
+++ content/browser/indexed_db/instance/leveldb/backing_store.cc
@@ -4614,7 +4614,7 @@ bool BackingStore::Transaction::WriteNewBlobs(BlobWriteCallback callback) {
                             storage::mojom::WriteBlobToFileResult::kSuccess);
                       },
                       weak_ptr_factory_.GetWeakPtr(),
-                      base::UnsafeDanglingUntriaged(&entry),
+                      base::Unretained(&entry),
                       write_result_callback));
           break;
         }
