f1137068f573182998fcf8c5b8320a1a08ee360d
Christoph Schwering <schwering@google.com>
2026-08-11 23:09:40 -0700

    [Autofill] Fully qualify using statements in AutofillAgent
    Bug: 40100455
    Change-Id: Icaccd8745d4c8ab77ce07e9690474ebacf67bcae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8241011
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677786}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -90,20 +90,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 
-using blink::WebAutofillClient;
-using blink::WebAutofillState;
-using blink::WebDocument;
-using blink::WebDOMEvent;
-using blink::WebElement;
-using blink::WebFormControlElement;
-using blink::WebFormElement;
-using blink::WebFormRelatedChangeType;
-using blink::WebFrame;
-using blink::WebInputElement;
-using blink::WebKeyboardEvent;
-using blink::WebLocalFrame;
-using blink::WebNode;
-using blink::WebString;
+using ::blink::WebAutofillClient;
+using ::blink::WebAutofillState;
+using ::blink::WebDocument;
+using ::blink::WebDOMEvent;
+using ::blink::WebElement;
+using ::blink::WebFormControlElement;
+using ::blink::WebFormElement;
+using ::blink::WebFormRelatedChangeType;
+using ::blink::WebFrame;
+using ::blink::WebInputElement;
+using ::blink::WebKeyboardEvent;
+using ::blink::WebLocalFrame;
+using ::blink::WebNode;
+using ::blink::WebString;
 
 namespace autofill {
 
