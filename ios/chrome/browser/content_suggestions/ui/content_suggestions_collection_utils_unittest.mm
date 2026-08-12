41e8979463922da1d5459ce4887ed412b48a1d69
Briana McClure <bmcclure@google.com>
2026-08-12 14:31:40 -0700

    [iOS][NTPRedesign] Rename NTP Phase 1 feature flag
    This change renames kNewTabPagePaddingUpdate to kNewTabPageUICleanup to
    clearly encapsulate all of the Phase 1 NTP Redesign changes. An
    IsNewTabPageUICleanupEnabled() helper method is also added to simplify
    checking if the flag is enabled.
    
    Fixed: 542594099
    Change-Id: Id5562b70d097189d4a534b1e2770db347a5bd3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8250718
    Reviewed-by: Chris Lu <thegreenfrog@google.com>
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678315}

--- ios/chrome/browser/content_suggestions/ui/content_suggestions_collection_utils_unittest.mm
+++ ios/chrome/browser/content_suggestions/ui/content_suggestions_collection_utils_unittest.mm
@@ -281,7 +281,7 @@ TEST_F(ContentSuggestionsCollectionUtilsTest, SameLogoAndDoodleHeight) {
   EXPECT_EQ(height_with_logo, height_with_doodle);
 }
 
-// Test padding helpers for kNewTabPagePaddingUpdate experiment arms.
+// Test padding helpers for kNewTabPageUICleanup experiment arms.
 TEST_F(ContentSuggestionsCollectionUtilsTest, NTPPaddingExperimentHelpers) {
   // Control (Disabled).
   EXPECT_EQ(DoodleTopMargin(SearchEngineLogoState::kLogo,
@@ -304,7 +304,7 @@ TEST_F(ContentSuggestionsCollectionUtilsTest, NTPPaddingExperimentHelpers) {
   {
     base::test::ScopedFeatureList scoped_feature_list;
     scoped_feature_list.InitAndEnableFeatureWithParameters(
-        kNewTabPagePaddingUpdate, {{kNewTabPagePaddingUpdateArmParam, "1"}});
+        kNewTabPageUICleanup, {{kNewTabPageUICleanupArmParam, "1"}});
     EXPECT_EQ(FakeToolbarHeight() + kLogoTopPaddingTight,
               LogoTopPadding(SearchEngineLogoState::kLogo,
                              IPhonePortraitTraitCollection()));
@@ -324,7 +324,7 @@ TEST_F(ContentSuggestionsCollectionUtilsTest, NTPPaddingExperimentHelpers) {
   {
     base::test::ScopedFeatureList scoped_feature_list;
     scoped_feature_list.InitAndEnableFeatureWithParameters(
-        kNewTabPagePaddingUpdate, {{kNewTabPagePaddingUpdateArmParam, "2"}});
+        kNewTabPageUICleanup, {{kNewTabPageUICleanupArmParam, "2"}});
     EXPECT_EQ(FakeToolbarHeight() + kLogoTopPaddingMedium,
               LogoTopPadding(SearchEngineLogoState::kLogo,
                              IPhonePortraitTraitCollection()));
@@ -344,7 +344,7 @@ TEST_F(ContentSuggestionsCollectionUtilsTest, NTPPaddingExperimentHelpers) {
   {
     base::test::ScopedFeatureList scoped_feature_list;
     scoped_feature_list.InitAndEnableFeatureWithParameters(
-        kNewTabPagePaddingUpdate, {{kNewTabPagePaddingUpdateArmParam, "3"}});
+        kNewTabPageUICleanup, {{kNewTabPageUICleanupArmParam, "3"}});
     EXPECT_EQ(FakeToolbarHeight() + kLogoTopPaddingPreferred,
               LogoTopPadding(SearchEngineLogoState::kLogo,
                              IPhonePortraitTraitCollection()));
@@ -364,7 +364,7 @@ TEST_F(ContentSuggestionsCollectionUtilsTest, NTPPaddingExperimentHelpers) {
   {
     base::test::ScopedFeatureList scoped_feature_list;
     scoped_feature_list.InitAndEnableFeatureWithParameters(
-        kNewTabPagePaddingUpdate, {{kNewTabPagePaddingUpdateArmParam, "1"}});
+        kNewTabPageUICleanup, {{kNewTabPageUICleanupArmParam, "1"}});
     EXPECT_EQ(162.0, LogoTopPadding(SearchEngineLogoState::kLogo,
                                     IPadTraitCollection()));
     EXPECT_EQ(162.0, LogoTopPadding(SearchEngineLogoState::kDoodle,
