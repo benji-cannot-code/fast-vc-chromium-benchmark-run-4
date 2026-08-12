24b2ada823087a3620718bd9d81cd7e4d6ed50bf
Hitarth Kothari <hitarthkothari@google.com>
2026-08-12 10:36:37 -0700

    [WillCloseTabs] Implement willCloseTabs in TabSwitcherGroupSuggestionService
    This CL implements the willCloseTabs method in
    TabSwitcherGroupSuggestionService's TabModelObserver to clear
    suggestions when tabs are about to be closed. This change is gated
    behind the TAB_CLOSURE_METHOD_REFACTOR feature flag, and corresponding
    unit tests have been added and updated to verify behavior with the flag
    both enabled and disabled.
    
    Bug: 545136199
    Change-Id: I41d7ad376a2509123f5c91986cb922b6ef0c7ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8245987
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678161}

--- chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabSwitcherGroupSuggestionService.java
+++ chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabSwitcherGroupSuggestionService.java
@@ -174,6 +174,12 @@ public class TabSwitcherGroupSuggestionService {
                 }
 
                 @Override
+                public void willCloseTabs(
+                        List<Tab> tabs, boolean isAllTabs, boolean allowUndo) {
+                    clearSuggestions();
+                }
+
+                @Override
                 public void willAddTab(Tab tab, @TabLaunchType int type) {
                     clearSuggestions();
                 }
