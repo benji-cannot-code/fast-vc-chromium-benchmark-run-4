c0d57dc72e8ccda8a4c36408bfe50f16c9425ff2
Daniel Cheng <dcheng@chromium.org>
2026-08-13 09:10:41 -0700

    Use `mojo::OptionalAsPointer` to serialize nullable `CdmCapability`s
    Mojo's `mojo::OptionalAsPointer` idiom allows `StructTraits` getters to
    indicate nullability by pointer. `KeySystemCapability` internally stores
    the `CdmCapability` fields as `base::expected`, so the `StructTraits`
    implementation had to copy and rewrap the result in a `std::optional`
    for Mojo serialization; using `mojo::OptionalAsPointer` avoids the copy
    altogether.
    
    Change-Id: I2b8db14a2f87ac2763e5d70c51681f4a978cb324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253329
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678876}

--- media/mojo/mojom/key_system_capability_mojom_traits.h
+++ media/mojo/mojom/key_system_capability_mojom_traits.h
@@ -6,27 +6,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_MOJO_MOJOM_KEY_SYSTEM_CAPABILITY_MOJOM_TRAITS_H_
 #define MEDIA_MOJO_MOJOM_KEY_SYSTEM_CAPABILITY_MOJOM_TRAITS_H_
 
+#include <optional>
+
 #include "media/base/cdm_capability.h"
 #include "media/base/key_system_capability.h"
 #include "media/mojo/mojom/key_system_support.mojom.h"
+#include "mojo/public/cpp/bindings/optional_as_pointer.h"
 
 namespace mojo {
 
 template <>
 struct StructTraits<media::mojom::KeySystemCapabilityDataView,
                     media::KeySystemCapability> {
-  static const std::optional<media::CdmCapability> sw_secure_capability(
-      const media::KeySystemCapability& input) {
+  static mojo::OptionalAsPointer<const media::CdmCapability>
+  sw_secure_capability(const media::KeySystemCapability& input) {
     return input.sw_cdm_capability_or_status.has_value()
-               ? std::optional(input.sw_cdm_capability_or_status.value())
-               : std::nullopt;
+               ? mojo::OptionalAsPointer(
+                     &input.sw_cdm_capability_or_status.value())
+               : nullptr;
   }
 
-  static const std::optional<media::CdmCapability> hw_secure_capability(
-      const media::KeySystemCapability& input) {
+  static mojo::OptionalAsPointer<const media::CdmCapability>
+  hw_secure_capability(const media::KeySystemCapability& input) {
     return input.hw_cdm_capability_or_status.has_value()
-               ? std::optional(input.hw_cdm_capability_or_status.value())
-               : std::nullopt;
+               ? mojo::OptionalAsPointer(
+                     &input.hw_cdm_capability_or_status.value())
+               : nullptr;
   }
 
   static const std::optional<media::CdmCapabilityQueryStatus>
