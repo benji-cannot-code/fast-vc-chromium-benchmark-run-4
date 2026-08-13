2f9a2b34bd9481893b79f0b57a0d36ef12a9cd22
Liza Burakova <liza@chromium.org>
2026-08-13 11:57:51 -0700

    Reland "[mac] Enable aperitif on helper executables."
    This is a reland of commit dfa0e77c016df17590e562dc975ac0ca0f64ff39
    
    Original change's description:
    > [mac] Enable aperitif on helper executables.
    >
    > This CL sets up "Aperitified" helper executables, as well as putting
    > these behind a feature flag. This does mean that we will be shipping
    > two versions of each helper, one that is linked w/ dlopen() and one
    > linked w/ libaperitif. This will allow us to do A/B testing on the
    > performance improvements of libaperitif.
    >
    > This CL also modifies the aperitif_unittest. Previously it looped
    > through every initializer printed with DYLD_PRINT_INITIALIZERS, but
    > now it stops the loop once it sees libaperitif is initialized.
    >
    > Bug: 40794783
    > Change-Id: I1eee5a7fa4b5aa50e64adb8cf00e89f2ade4b701
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7970768
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Commit-Queue: Liza Burakova <liza@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1676768}
    
    Bug: 40794783
    Change-Id: Ice4ab35a832aeaabe6d555dc424959f854b9ac6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242707
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Yue She <yueshe@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679014}

--- chrome/installer/mac/signing/modification_test.py
+++ chrome/installer/mac/signing/modification_test.py
@@ -25,6 +25,12 @@ def plist_read(*args):
         '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Alerts).app/Contents/Resources/base.lproj/InfoPlist.strings': {
             'CFBundleDisplayName': 'Product'
         },
+        '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Info.plist': {
+            'CFBundleIdentifier': bundle_id + '.AlertNotificationService'
+        },
+        '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Resources/base.lproj/InfoPlist.strings': {
+            'CFBundleDisplayName': 'Product'
+        },
         '/$W/app-entitlements.plist': {
             'com.apple.application-identifier': bundle_id
         },
@@ -465,7 +471,7 @@ class TestModification(unittest.TestCase):
             ],
         )
 
-        self.assertEqual(7, kwargs['copy_files'].call_count)
+        self.assertEqual(9, kwargs['copy_files'].call_count)
         kwargs['copy_files'].assert_has_calls(
             [
                 mock.call(
@@ -496,13 +502,21 @@ class TestModification(unittest.TestCase):
                     '/$I/Product Packaging/app_canary.icns',
                     '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Alerts).app/Contents/Resources/app.icns',
                 ),
+                mock.call(
+                    '/$I/Product Packaging/Assets_canary.car',
+                    '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Resources/Assets.car',
+                ),
+                mock.call(
+                    '/$I/Product Packaging/app_canary.icns',
+                    '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Resources/app.icns',
+                ),
             ]
         )
         kwargs['write_file'].assert_called_once_with(
             '/$W/App Product Canary.app/Contents/PkgInfo', 'APPLMooo'
         )
 
-        self.assertEqual(7, kwargs['write_plist'].call_count)
+        self.assertEqual(9, kwargs['write_plist'].call_count)
         kwargs['write_plist'].assert_has_calls(
             [
                 mock.call(
@@ -515,6 +529,18 @@ class TestModification(unittest.TestCase):
                 mock.call(
                     {'CFBundleDisplayName': 'Product Canary'},
                     '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Alerts).app/Contents/Resources/base.lproj/InfoPlist.strings',
+                    'binary1',
+                ),
+                mock.call(
+                    {
+                        'CFBundleIdentifier': 'test.signing.bundle_id.canary.AlertNotificationService'
+                    },
+                    '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Info.plist',
+                    'xml1',
+                ),
+                mock.call(
+                    {'CFBundleDisplayName': 'Product Canary'},
+                    '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Resources/base.lproj/InfoPlist.strings',
                     'binary1',
                 ),
                 mock.call(
@@ -606,7 +632,7 @@ class TestModification(unittest.TestCase):
 
         modification.customize_distribution(self.paths, dist, config)
 
-        self.assertEqual(7, kwargs['write_plist'].call_count)
+        self.assertEqual(9, kwargs['write_plist'].call_count)
         kwargs['write_plist'].assert_has_calls(
             [
                 mock.call(
@@ -619,6 +645,18 @@ class TestModification(unittest.TestCase):
                 mock.call(
                     {'CFBundleDisplayName': 'Product Canary'},
                     '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Alerts).app/Contents/Resources/base.lproj/InfoPlist.strings',
+                    'binary1',
+                ),
+                mock.call(
+                    {
+                        'CFBundleIdentifier': 'test.signing.bundle_id.canary.AlertNotificationService'
+                    },
+                    '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Info.plist',
+                    'xml1',
+                ),
+                mock.call(
+                    {'CFBundleDisplayName': 'Product Canary'},
+                    '/$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper (Aperitif Alerts).app/Contents/Resources/base.lproj/InfoPlist.strings',
                     'binary1',
                 ),
                 mock.call(
--- sandbox/policy/sandbox_type.cc
+++ sandbox/policy/sandbox_type.cc
@@ -218,6 +218,9 @@ sandbox::mojom::Sandbox SandboxTypeFromCommandLine(
       process_type == switches::kAppShimProcessType) {
     return Sandbox::kNoSandbox;
   }
+  if (process_type == switches::kNoOpForTestingProcessType) {
+    return Sandbox::kUtility;
+  }
 #endif
 
   NOTREACHED()
