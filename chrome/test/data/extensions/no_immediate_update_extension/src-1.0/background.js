429d65b0a7d0e75768594d3e987ff979ad895c73
Di Wu <diwux@google.com>
2026-08-12 22:12:30 -0700

    Ash: Migrate sign-in extension policy test off RunUntilIdle
    This changelist is part of an ongoing series to modernize Ash tests by
    migrating them away from RunUntilIdle() and toward targeted
    synchronization with APIs such as RunUntil(), TestFuture, RunLoop
    callbacks, observer waiters, or exact mock-time advancement.
    
    SigninProfileExtensionsAutoUpdatePolicyTest now waits for the running
    extension to report that it received the offered update before checking
    that it stays on the installed version. This ties the test to the
    extension-policy behavior it exercises. The browser-side observer and
    its directory include permission are no longer needed and are removed.
    
    Bug: b:506794349
    Test: CQ
    Change-Id: I587e5193c5b8efde217f6f65cfb57f5539926322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8229329
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678599}

--- chrome/test/data/extensions/no_immediate_update_extension/src-1.0/background.js
+++ chrome/test/data/extensions/no_immediate_update_extension/src-1.0/background.js
@@ -3,8 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Suppress the default behavior that reloads the extension on available update.
-chrome.runtime.onUpdateAvailable.addListener(() => {});
+// Suppress the default behavior that reloads the extension and let the test
+// know which update was offered.
+chrome.runtime.onUpdateAvailable.addListener((details) => {
+  chrome.test.sendMessage(details.version);
+});
 
 // Explicitly abort the test if Chrome decides to suspend us despite the
 // persistent background page.
