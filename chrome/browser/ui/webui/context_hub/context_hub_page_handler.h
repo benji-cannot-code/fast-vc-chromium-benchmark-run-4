269a39526d7a999d331994fb534e822153269407
Isabelle Goralsky <igoralsky@google.com>
2026-08-12 21:45:01 -0700

    Implement close tab functionality in Context Hub.
    This CL adds support for closing a tab from the Context Hub UI. It
    introduces a CloseTab method to the Mojo interface, which is handled by
    ContextHubPageHandler to close the specified tab via
    ContextHubTabProviderDesktop. Additionally, closing a tab deletes its
    corresponding auto-todo item from the ContextHubService.
    
    Bug: b:544881641
    Change-Id: I9b820a36b5c70d4453e7c545c3cc65248e3d1031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8238437
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Isabelle Goralsky <igoralsky@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678593}

--- chrome/browser/ui/webui/context_hub/context_hub_page_handler.h
+++ chrome/browser/ui/webui/context_hub/context_hub_page_handler.h
@@ -37,6 +37,7 @@ class ContextHubPageHandler : public browser::context_hub::mojom::PageHandler,
     virtual std::vector<content::WebContents*> GetTabs() = 0;
     virtual std::vector<content::WebContents*> GetUngroupedTabs() = 0;
     virtual void SwitchToTab(int64_t tab_id) = 0;
+    virtual void CloseTab(int64_t tab_id) = 0;
     virtual bool ConfirmTabGroups(
         base::span<const context_hub::TabGroupEntry> groups) = 0;
     virtual void RemoveGroupFromTabstripIfOpen(
@@ -85,6 +86,7 @@ class ContextHubPageHandler : public browser::context_hub::mojom::PageHandler,
   void GetExistingTabGroupsAndChats(
       GetExistingTabGroupsAndChatsCallback callback) override;
   void SwitchToTab(int64_t tab_id) override;
+  void CloseTab(int64_t tab_id) override;
   void ClearTabGroups(ClearTabGroupsCallback callback) override;
   void ClearTabGroupChatHistory(
       ClearTabGroupChatHistoryCallback callback) override;
