01aae69956878ada92a3111bda72654c50d4703e
Wenbin Zhang <wenbinzhang@google.com>
2026-08-13 11:40:02 -0700

    [bedrock] Remove Browser* from customize_chrome_page_handler_unittest
    Remove unused Browser and TestBrowserWindow members and setup from
    CustomizeChromePageHandlerTest.
    
    TAG=agy
    CONV=a89fc9fb-6826-4862-9b08-55f05a3a432a
    
    Bug: None
    Change-Id: I8722e1d116abcf5c5ec4b662135e4efb08dabaa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8248098
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678995}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler_unittest.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_page_handler_unittest.cc
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory_test_util.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_factory.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_window/public/create_browser_window.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/search/ntp_user_data_types.h"
 #include "chrome/browser/ui/select_file_policy/chrome_select_file_policy.h"
@@ -42,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/test/base/test_browser_window.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/application_locale_storage/application_locale_storage.h"
@@ -57,9 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/themes/ntp_custom_background_service_observer.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_task_environment.h"
-#include "content/public/test/scoped_web_ui_controller_factory_registration.h"
 #include "content/public/test/test_web_contents_factory.h"
-#include "content/public/test/web_ui_browsertest_util.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/extension_builder.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -320,13 +315,6 @@ class CustomizeChromePageHandlerTest : public testing::Test {
     EXPECT_EQ(handler_.get(), ntp_background_service_observer_);
     EXPECT_EQ(handler_.get(), ntp_custom_background_service_observer_);
 
-    auto browser_window = std::make_unique<TestBrowserWindow>();
-    BrowserWindowCreateParams browser_params(profile_.get(), true);
-    browser_params.type = BrowserWindowInterface::Type::TYPE_NORMAL;
-    browser_params.window = browser_window.release();
-    browser_ =
-        DeprecatedCreateOwnedBrowserWindowForTesting(std::move(browser_params));
-
     application_locale_storage_->Set("foo");
 
     scoped_feature_list_.Reset();
@@ -334,8 +322,6 @@ class CustomizeChromePageHandlerTest : public testing::Test {
   }
 
   void TearDown() override {
-    browser_->tab_strip_model()->CloseAllTabs();
-    browser_.reset();
     test_url_loader_factory_.ClearResponses();
   }
 
@@ -406,7 +392,6 @@ class CustomizeChromePageHandlerTest : public testing::Test {
     return *ntp_background_service_observer_;
   }
   MockThemeService& mock_theme_service() { return *mock_theme_service_; }
-  Browser& browser() { return *browser_; }
   base::HistogramTester& histogram_tester() { return histogram_tester_; }
   base::UserActionTester& user_action_tester() { return user_action_tester_; }
 
@@ -424,7 +409,6 @@ class CustomizeChromePageHandlerTest : public testing::Test {
   testing::NiceMock<MockPage> mock_page_;
   raw_ptr<MockThemeService> mock_theme_service_;
   base::test::ScopedFeatureList scoped_feature_list_;
-  std::unique_ptr<Browser> browser_;
   base::HistogramTester histogram_tester_;
   base::UserActionTester user_action_tester_;
   base::MockRepeatingCallback<void(const GURL& gurl)> mock_open_url_callback_;
