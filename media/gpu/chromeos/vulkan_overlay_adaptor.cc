e6a8dd2042c39346c4039d87fc5adde98e79eed8
Jose Arturo Barrera <jabgonzalez@google.com>
2026-08-13 07:06:20 -0700

    [MiraclePtr] Fix compilation errors after enabling plugin
    This CL addresses compilation errors discovered after enabling the
    `chromium-containers` plugin enforcement for container types. These
    cases were not caught or handled during the initial automated rewrite CLs.
    
    Containers considered:
    - "std::list"
    - "std::vector"
    
    This CL is a follow-up of crrev.com/c/8126922
    
    rewriting to `raw_ptr` was performed.
    
    Bug: b:501142609
    Change-Id: I6615385966f564c135314062ac60f06f2a5405a5
    DanglingUntriaged-notes: Dangling pointer found by checks after the
    Doc: go/miracleptr-in-containers
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8181467
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: José Arturo Barrera <jabgonzalez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678800}

--- media/gpu/chromeos/vulkan_overlay_adaptor.cc
+++ media/gpu/chromeos/vulkan_overlay_adaptor.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bits.h"
 #include "base/compiler_specific.h"
+#include "base/containers/to_vector.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/raw_ptr.h"
 #include "base/task/thread_pool.h"
 #include "gpu/vulkan/init/vulkan_factory.h"
 #include "gpu/vulkan/vulkan_function_pointers.h"
@@ -113,14 +115,17 @@ class VulkanOverlayAdaptor::VulkanDescriptorPool {
       VkDescriptorSetLayout descriptor_set_layout,
       VkDevice logical_device);
 
-  const std::vector<VkDescriptorSet>& Get();
+  std::vector<VkDescriptorSet> Get();
 
  private:
-  VulkanDescriptorPool(std::vector<VkDescriptorSet> descriptor_sets,
-                       VkDescriptorPool descriptor_pool,
-                       VkDevice logical_device);
+  VulkanDescriptorPool(
+      std::vector<base::RawPtrIfPtrT<VkDescriptorSet, DanglingUntriaged>>
+          descriptor_sets,
+      VkDescriptorPool descriptor_pool,
+      VkDevice logical_device);
 
-  const std::vector<VkDescriptorSet> descriptor_sets_;
+  const std::vector<base::RawPtrIfPtrT<VkDescriptorSet, DanglingUntriaged>>
+      descriptor_sets_;
 
   const VkDescriptorPool descriptor_pool_;
   const VkDevice logical_device_;
@@ -187,8 +192,10 @@ class VulkanOverlayAdaptor::VulkanTextureImage {
       VkDevice logical_device);
 
   VkImage GetImage();
-  const std::vector<VkImageView>& GetImageViews();
-  const std::vector<VkFramebuffer>& GetFramebuffers();
+  const std::vector<base::RawPtrIfPtrT<VkImageView, DanglingUntriaged>>&
+  GetImageViews() const;
+  const std::vector<base::RawPtrIfPtrT<VkFramebuffer, DanglingUntriaged>>&
+  GetFramebuffers() const;
   void TransitionImageLayout(
       gpu::VulkanCommandBuffer* command_buf,
       VkImageLayout new_layout,
@@ -196,15 +203,20 @@ class VulkanOverlayAdaptor::VulkanTextureImage {
       uint32_t dst_queue_family_index = VK_QUEUE_FAMILY_IGNORED);
 
  private:
-  VulkanTextureImage(gpu::VulkanImage& image,
-                     const std::vector<VkImageView>& image_views,
-                     const std::vector<VkFramebuffer>& framebuffers,
-                     VkImageLayout initial_layout,
-                     VkDevice logical_device);
+  VulkanTextureImage(
+      gpu::VulkanImage& image,
+      std::vector<base::RawPtrIfPtrT<VkImageView, DanglingUntriaged>>
+          image_views,
+      std::vector<base::RawPtrIfPtrT<VkFramebuffer, DanglingUntriaged>>
+          framebuffers,
+      VkImageLayout initial_layout,
+      VkDevice logical_device);
 
   const raw_ref<gpu::VulkanImage> image_;
-  const std::vector<VkImageView> image_views_;
-  const std::vector<VkFramebuffer> framebuffers_;
+  const std::vector<base::RawPtrIfPtrT<VkImageView, DanglingUntriaged>>
+      image_views_;
+  const std::vector<base::RawPtrIfPtrT<VkFramebuffer, DanglingUntriaged>>
+      framebuffers_;
 
   VkImageLayout current_layout_;
   const VkDevice logical_device_;
@@ -519,10 +531,11 @@ VulkanOverlayAdaptor::VulkanPipeline::Create(
 }
 
 VulkanOverlayAdaptor::VulkanDescriptorPool::VulkanDescriptorPool(
-    std::vector<VkDescriptorSet> descriptor_sets,
+    std::vector<base::RawPtrIfPtrT<VkDescriptorSet, DanglingUntriaged>>
+        descriptor_sets,
     VkDescriptorPool descriptor_pool,
     VkDevice logical_device)
-    : descriptor_sets_(descriptor_sets),
+    : descriptor_sets_(std::move(descriptor_sets)),
       descriptor_pool_(descriptor_pool),
       logical_device_(logical_device) {}
 
@@ -530,9 +543,8 @@ VulkanOverlayAdaptor::VulkanDescriptorPool::~VulkanDescriptorPool() {
   vkDestroyDescriptorPool(logical_device_, descriptor_pool_, nullptr);
 }
 
-const std::vector<VkDescriptorSet>&
-VulkanOverlayAdaptor::VulkanDescriptorPool::Get() {
-  return descriptor_sets_;
+std::vector<VkDescriptorSet> VulkanOverlayAdaptor::VulkanDescriptorPool::Get() {
+  return base::ToVector<VkDescriptorSet>(descriptor_sets_);
 }
 
 std::unique_ptr<VulkanOverlayAdaptor::VulkanDescriptorPool>
@@ -570,26 +582,31 @@ VulkanOverlayAdaptor::VulkanDescriptorPool::Create(
   alloc_info.descriptorSetCount = num_descriptor_sets;
   alloc_info.pSetLayouts = layouts.data();
 
-  std::vector<VkDescriptorSet> descriptor_sets(num_descriptor_sets);
-  if (vkAllocateDescriptorSets(logical_device, &alloc_info,
-                               descriptor_sets.data()) != VK_SUCCESS) {
+  std::vector<VkDescriptorSet> temp_sets(num_descriptor_sets);
+  if (vkAllocateDescriptorSets(logical_device, &alloc_info, temp_sets.data()) !=
+      VK_SUCCESS) {
     LOG(ERROR) << "Could not create descriptor sets!";
     return nullptr;
   }
 
+  auto descriptor_sets =
+      base::ToVector<base::RawPtrIfPtrT<VkDescriptorSet, DanglingUntriaged>>(
+          temp_sets);
+
   return base::WrapUnique(new VulkanDescriptorPool(
-      descriptor_sets, descriptor_pool, logical_device));
+      std::move(descriptor_sets), descriptor_pool, logical_device));
 }
 
 VulkanOverlayAdaptor::VulkanTextureImage::VulkanTextureImage(
     gpu::VulkanImage& image,
-    const std::vector<VkImageView>& image_views,
-    const std::vector<VkFramebuffer>& framebuffers,
+    std::vector<base::RawPtrIfPtrT<VkImageView, DanglingUntriaged>> image_views,
+    std::vector<base::RawPtrIfPtrT<VkFramebuffer, DanglingUntriaged>>
+        framebuffers,
     VkImageLayout initial_layout,
     VkDevice logical_device)
     : image_(image),
-      image_views_(image_views),
-      framebuffers_(framebuffers),
+      image_views_(std::move(image_views)),
+      framebuffers_(std::move(framebuffers)),
       current_layout_(initial_layout),
       logical_device_(logical_device) {}
 
@@ -607,13 +624,13 @@ VkImage VulkanOverlayAdaptor::VulkanTextureImage::GetImage() {
   return image_->image();
 }
 
-const std::vector<VkImageView>&
-VulkanOverlayAdaptor::VulkanTextureImage::GetImageViews() {
+const std::vector<base::RawPtrIfPtrT<VkImageView, DanglingUntriaged>>&
+VulkanOverlayAdaptor::VulkanTextureImage::GetImageViews() const {
   return image_views_;
 }
 
-const std::vector<VkFramebuffer>&
-VulkanOverlayAdaptor::VulkanTextureImage::GetFramebuffers() {
+const std::vector<base::RawPtrIfPtrT<VkFramebuffer, DanglingUntriaged>>&
+VulkanOverlayAdaptor::VulkanTextureImage::GetFramebuffers() const {
   return framebuffers_;
 }
 
@@ -645,7 +662,7 @@ VulkanOverlayAdaptor::VulkanTextureImage::Create(
   CHECK_EQ(formats.size(), sizes.size());
   CHECK_EQ(sizes.size(), aspects.size());
 
-  std::vector<VkImageView> image_views;
+  std::vector<base::RawPtrIfPtrT<VkImageView, DanglingUntriaged>> image_views;
   for (size_t i = 0; i < formats.size(); i++) {
     VkImageViewCreateInfo view_info{};
     view_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
@@ -668,17 +685,19 @@ VulkanOverlayAdaptor::VulkanTextureImage::Create(
       return nullptr;
     }
 
-    image_views.emplace_back(std::move(image_view));
+    image_views.push_back(std::move(image_view));
   }
 
-  std::vector<VkFramebuffer> framebuffers;
+  std::vector<base::RawPtrIfPtrT<VkFramebuffer, DanglingUntriaged>>
+      framebuffers;
   if (is_framebuffer) {
     for (size_t i = 0; i < sizes.size(); i++) {
       VkFramebufferCreateInfo framebuffer_info{};
       framebuffer_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
       framebuffer_info.renderPass = render_pass;
       framebuffer_info.attachmentCount = 1;
-      framebuffer_info.pAttachments = UNSAFE_TODO(image_views.data() + i);
+      VkImageView view_handle = image_views[i];
+      framebuffer_info.pAttachments = &view_handle;
       framebuffer_info.width = sizes[i].width();
       framebuffer_info.height = sizes[i].height();
       framebuffer_info.layers = 1;
@@ -696,7 +715,7 @@ VulkanOverlayAdaptor::VulkanTextureImage::Create(
         return nullptr;
       }
 
-      framebuffers.emplace_back(std::move(framebuffer));
+      framebuffers.push_back(std::move(framebuffer));
     }
   }
 
