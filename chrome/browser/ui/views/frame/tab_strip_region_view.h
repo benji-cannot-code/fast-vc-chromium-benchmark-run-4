85f66f98791694a92a74540ed0591577399a07ed
Kenichi Ishibashi <bashi@chromium.org>
2026-08-12 23:59:19 -0700

    Revert "Only show tab memory usage in accessible name when in hover card"
    This reverts commit a59c926c9bd81731eb65ae2515f447421adb7bfe.
    
    Reason for revert: Speculatively reverting since TabStripBrowsertest.AccessibleNameUpdatesOnTabFocus is failing.
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/59777/overview
    
    Original change's description:
    > Only show tab memory usage in accessible name when in hover card
    >
    > The tab's accessible name currently includes memory usage even if tab
    > memory usage isn't being shown on the hover card. This CL fixes this
    > bug by checking to see if the memory usage pref is enabled before
    > including memory usage in the accessible name.
    >
    > Bug: 484105980
    > Change-Id: Ic9bb73dd347889fd8fc38cbde15fa68fa7f5ddb9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8232720
    > Auto-Submit: Steven Luong <stluong@chromium.org>
    > Commit-Queue: Steven Luong <stluong@chromium.org>
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1678565}
    
    Bug: 484105980
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6af3494680b13ca10e433180662110b21a7ed88f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8255569
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1678635}

--- chrome/browser/ui/views/frame/tab_strip_region_view.h
+++ chrome/browser/ui/views/frame/tab_strip_region_view.h
@@ -22,7 +22,6 @@ struct TabData;
 
 class TabDragContext;
 class TabDragTarget;
-class TabHoverCardController;
 class TabStripObserver;
 
 class ExpandOnHoverLock {
@@ -68,7 +67,6 @@ class TabStripRegionView : public views::AccessiblePaneView,
   virtual const tabs::TabData& GetTabData(const tabs::TabHandle& tab) = 0;
   virtual views::View* GetTabStripView() = 0;
   virtual bool IsPositionInWindowCaption(const gfx::Point& point) = 0;
-  virtual TabHoverCardController* GetHoverCardController() = 0;
 
   // -- UI anchoring --
   virtual views::View* GetTabAnchorViewAt(int tab_index) = 0;
