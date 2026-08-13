7f2cabce2cad8ee5b6550173194fff492ec7218e
Fiaz Muhammad <mfiaz@google.com>
2026-08-13 09:55:14 -0700

    [Incognito] Implement short-circuiting for observer notifications
    Avoid broadcasting redundant incognito state changes to registered
    observers when the state has not changed.
    
    Bug: 545992990
    Change-Id: I51306f70486f24cca8ec1f1e9e61dc30e54b0063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257390
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678901}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/IncognitoStateProvider.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/IncognitoStateProvider.java
@@ -28,13 +28,18 @@ public class IncognitoStateProvider {
     /** A {@link TabModelSelector} used to know when incognito mode is entered or exited. */
     private @Nullable TabModelSelector mTabModelSelector;
 
+    /**
+     * The last emitted incognito state, or {@code null} if no state has been emitted yet. Used to
+     * prevent redundant observer broadcasts.
+     */
+    private @Nullable Boolean mLastIncognitoState;
+
     public IncognitoStateProvider() {
         mIncognitoStateObservers = new ObserverList<>();
 
         mCurrentTabModelObserver =
                 (tabModel) -> {
-                    // TODO(jinsukkim): Emit this only if the state is different.
-                    emitIncognitoStateChanged(tabModel.isIncognito());
+                    maybeEmitIncognitoStateChanged(tabModel.isIncognito());
                 };
     }
 
@@ -47,11 +52,13 @@ public class IncognitoStateProvider {
 
     /**
      * @param observer Add an observer to be notified of incognito state changes. Calls
-     *                 #onIncognitoStateChanged() on the added observer.
+     *     #onIncognitoStateChanged() on the added observer.
      */
     public void addIncognitoStateObserverAndTrigger(IncognitoStateObserver observer) {
         mIncognitoStateObservers.addObserver(observer);
-        observer.onIncognitoStateChanged(isIncognitoSelected());
+        boolean isIncognito = isIncognitoSelected();
+        mLastIncognitoState = isIncognito;
+        observer.onIncognitoStateChanged(isIncognito);
     }
 
     /**
@@ -69,7 +76,7 @@ public class IncognitoStateProvider {
         mTabModelSelector
                 .getCurrentTabModelSupplier()
                 .addSyncObserverAndPostIfNonNull(mCurrentTabModelObserver);
-        emitIncognitoStateChanged(mTabModelSelector.isIncognitoSelected());
+        maybeEmitIncognitoStateChanged(mTabModelSelector.isIncognitoSelected());
     }
 
     /** Destroy {@link IncognitoStateProvider} object. */
@@ -79,20 +86,28 @@ public class IncognitoStateProvider {
             mTabModelSelector = null;
         }
         mIncognitoStateObservers.clear();
+        mLastIncognitoState = null;
     }
 
     /**
-     * Update incognito-selected state.
+     * Emits an incognito state change notification to registered observers if the state differs
+     * from the last emitted state.
+     *
      * @param isIncognito Whether incognito mode is selected.
      */
-    private void emitIncognitoStateChanged(boolean isIncognito) {
+    private void maybeEmitIncognitoStateChanged(boolean isIncognito) {
+        if (Boolean.valueOf(isIncognito).equals(mLastIncognitoState)) {
+            return;
+        }
+        mLastIncognitoState = isIncognito;
+
         for (IncognitoStateObserver observer : mIncognitoStateObservers) {
             observer.onIncognitoStateChanged(isIncognito);
         }
     }
 
     public void setIncognitoStateForTesting(boolean isIncognito) {
-        emitIncognitoStateChanged(isIncognito);
+        maybeEmitIncognitoStateChanged(isIncognito);
     }
 
     public int getObserverCountForTesting() {
