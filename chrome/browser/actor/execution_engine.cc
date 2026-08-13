3605ffcfb5669bb1e4d3c06695ce7160b288ce76
Chris Fredrickson <cfredric@chromium.org>
2026-08-12 17:25:16 -0700

    Reorder safety lists & actor container checks for consistency
    In particular, this ensures that the actor container config and safety
    lists are treated consistently w.r.t. enterprise policies (which now
    have priority).
    
    Bug: b:505816395
    Change-Id: Ic660b595fe37a7a3327b09dfc89a4d862d9ddf42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8247451
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678417}

--- chrome/browser/actor/execution_engine.cc
+++ chrome/browser/actor/execution_engine.cc
@@ -665,9 +665,6 @@ ExecutionEngine::ExecutionEngine(
           *this,
           origin_gating::OriginGatingConfiguration(
               {
-                  {DecisionSource::kActorContainerConfig,
-                   {GateableEvent::kPageAction}},
-                  {CreateSafetyListPredicate(), {GateableEvent::kPageAction}},
                   {CustomPredicate(
                        base::BindRepeating(&EvaluateTabErrorDocument),
                        kTabErrorDocumentPredicateName),
@@ -706,9 +703,11 @@ ExecutionEngine::ExecutionEngine(
                                    kLookalikeUrlPredicateName),
                    kRequestsAndPageActions},
                   {DecisionSource::kActorContainerConfig,
-                   {GateableEvent::kNavigationResponse}},
+                   {GateableEvent::kNavigationResponse,
+                    GateableEvent::kPageAction}},
                   {CreateSafetyListPredicate(),
-                   {GateableEvent::kNavigationResponse}},
+                   {GateableEvent::kNavigationResponse,
+                    GateableEvent::kPageAction}},
                   {DecisionSource::kCacheWithUserConfirmation,
                    GateableEventSet::All()},
                   {DecisionSource::kAllowSameOrigin,
