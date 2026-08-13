7dc2d3c721d74afacf386a224c198c663a24c444
Philipp Hancke <philipp.hancke@googlemail.com>
2026-08-13 10:18:16 -0700

    Revert "webrtc-internals: bring back ontrack"
    This reverts commit 119f12eb3232d7c1421d68f703ea6066fc23ec5f.
    
    Reason for revert: tentative revert
    
    Original change's description:
    > webrtc-internals: bring back ontrack
    >
    > to make the dump more similar to what Javascript gets
    >
    > Bug: chromium:487376829
    > Change-Id: Iaab519790f31859727944ef5236c08cec0bc52d0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8223562
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    > Cr-Commit-Position: refs/heads/main@{#1675633}
    
    Bug: chromium:487376829,chromium:545610671
    Bug: chromium:487376829
    Change-Id: I2ea158af1d0b124d4ae165f01fa1f7e3e2f202f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256870
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678925}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.h
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.h
@@ -38,7 +38,6 @@ class RTCAnswerOptionsPlatform;
 class RTCIceCandidatePlatform;
 class RTCOfferOptionsPlatform;
 class RTCPeerConnectionHandler;
-class RTCTrackEvent;
 class UserMediaRequest;
 class WebLocalFrame;
 
@@ -167,11 +166,6 @@ class MODULES_EXPORT PeerConnectionTracker
       TransceiverUpdatedReason reason,
       const RTCRtpTransceiverPlatform& transceiver,
       size_t transceiver_index);
-
-  // Sends an update when the "track" event is fired for a remote track added
-  // by setRemoteDescription. Example event: "ontrack".
-  virtual void TrackOnTrack(RTCPeerConnectionHandler* pc_handler,
-                            const RTCTrackEvent& event);
 
   // Sends an update when a DataChannel is created.
   virtual void TrackCreateDataChannel(
