f2edea13d19e63795a54708baf6da886dee1b1d8
Joanne de Abreu <jdeabreu@google.com>
2026-08-13 06:27:53 -0700

    Add a feature flag to control draining the prefetch queue in WebView navigate.
    This CL introduces the WebViewNavigateDrainPrefetch feature flag as a kill switch. When enabled, it drains pending prefetch requests before loading a URL in the WebView navigate method. This allows disabling the prefetch draining behavior if needed.
    
    Bug: 545906279
    Change-Id: I4420143f8309b667d0201d083eb347779b8f9b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256949
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678784}

--- android_webview/common/aw_feature_map.cc
+++ android_webview/common/aw_feature_map.cc
@@ -66,6 +66,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewMoveWorkToProviderInitThreadPool,
     &features::kWebViewMultiProfileSkipDefaultProfile,
     &features::kWebViewNavigate,
+    &features::kWebViewNavigateDrainPrefetch,
     &features::kWebViewOptInToGmsBindServiceOptimization,
     &features::kWebViewPersistHttpServerProperties,
     &features::kWebViewPrefetchNativeLibrary,
