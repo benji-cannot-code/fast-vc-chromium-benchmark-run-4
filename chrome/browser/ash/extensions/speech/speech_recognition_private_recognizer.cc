ba8d361be468ff02058eccb81c938972e122e841
Mitsuru Oshima <oshima@chromium.org>
2026-08-13 10:19:17 -0700

    [ash] Remove obsolete crash key in SpeechRecognitionPrivateRecognizer
    The issue crbug.com/40821591 is fixed (even though it was wontfix'ed).
    Remove the temporary scoped crash key and unused header.
    
    TAG=agy
    BUG=40821591
    CONV=de9800f2-0d67-4744-badd-4c7ded38a3a1
    
    Change-Id: Id1685eab6abf53ae56fde5ebce57320d8cb23255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8251525
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Auto-Submit: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678926}

--- chrome/browser/ash/extensions/speech/speech_recognition_private_recognizer.cc
+++ chrome/browser/ash/extensions/speech/speech_recognition_private_recognizer.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/extensions/speech/speech_recognition_private_recognizer.h"
 
 #include "ash/public/cpp/projector/speech_recognition_availability.h"
-#include "base/debug/crash_logging.h"
 #include "chrome/browser/ash/extensions/speech/speech_recognition_private_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/speech/network_speech_recognizer.h"
@@ -58,9 +57,6 @@ void SpeechRecognitionPrivateRecognizer::OnSpeechResult(
 
 void SpeechRecognitionPrivateRecognizer::OnSpeechRecognitionStateChanged(
     SpeechRecognizerStatus new_state) {
-  // Crash keys for https://crbug.com/40821591.
-  SCOPED_CRASH_KEY_NUMBER("Accessibility", "Speech recognition state",
-                          new_state);
   SpeechRecognizerStatus next_state = new_state;
   if (new_state == SPEECH_RECOGNIZER_READY) {
     if (current_state_ == SPEECH_RECOGNIZER_OFF && speech_recognizer_) {
