c6df70591701d13a7cf13cdf34cb5e380bd9cc34
Kaan Alsan <alsan@google.com>
2026-08-13 14:35:39 -0700

    [OmniboxEverywhere] Rebuild widget on ephemeral mode pref change
    This allows changes to the ephemeral mode to take effect without having
    to restart chrome, since the OE widget doesn't get destroyed when
    dismissed.
    
    Bug: b:532195081
    Change-Id: Ibf384f16ce28f94213d76ce4158443ffd484b5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256573
    Reviewed-by: Hamzah Behery <behery@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679094}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.h
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 #include "chrome/browser/ui/browser_window/public/profile_browser_collection.h"
 #include "chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "content/public/browser/context_menu_params.h"
 #include "third_party/blink/public/mojom/page/draggable_region.mojom-forward.h"
 #include "third_party/skia/include/core/SkRegion.h"
@@ -168,6 +169,7 @@ class OmniboxEverywhereUIManager : public views::WidgetObserver,
   void EnsureContentsWrapperInitialized(Profile* profile);
   void CreateAndInitWidget(gfx::NativeWindow context);
   void ActivateAndFocus();
+  void OnEphemeralModelPrefChanged();
 
   std::unique_ptr<WebUIContentsWrapper> CreateContentsWrapper(Profile* profile);
 
@@ -199,6 +201,7 @@ class OmniboxEverywhereUIManager : public views::WidgetObserver,
   std::unique_ptr<ui::SimpleMenuModel> context_menu_model_;
   std::unique_ptr<views::MenuRunner> context_menu_runner_;
 
+  PrefChangeRegistrar local_state_pref_change_registrar_;
   base::ScopedObservation<views::Widget, views::WidgetObserver>
       widget_observation_{this};
   base::ScopedObservation<ProfileBrowserCollection, BrowserCollectionObserver>
