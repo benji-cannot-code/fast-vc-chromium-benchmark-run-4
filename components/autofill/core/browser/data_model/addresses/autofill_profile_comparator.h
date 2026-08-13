604579d501962ee2f2bb276345f93c1ae7aa1d63
Paweł Stus <pawelstus@google.com>
2026-08-13 08:25:35 -0700

    Extract `EmailInfo` into its own files.
    This CL creates separate header and source files for
    `EmailInfo` in an effort to refactor code contained in `contact_info`
    files. `NameInfo` is going to be extracted in a follow up CL.
    
    Bug: 545467287
    Change-Id: I034d2a9e08ae626bbace711ac30e71e43389bb85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8245923
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Paweł Stus <pawelstus@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678852}

--- components/autofill/core/browser/data_model/addresses/autofill_profile_comparator.h
+++ components/autofill/core/browser/data_model/addresses/autofill_profile_comparator.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+class EmailInfo;
+
 struct ProfileValueDifference {
   // The type of the field that is different.
   FieldType type;
