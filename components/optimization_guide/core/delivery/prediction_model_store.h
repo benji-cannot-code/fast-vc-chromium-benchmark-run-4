9c0fdc9aa8ea0a3dbdc2ceef95c186f719359afe
Zekun Jiang <zekunjiang@google.com>
2026-08-13 11:05:23 -0700

    Consolidate `LoadAndVerify..` functions.
    Expose only one version of this function that returns a ModelInfo.
    
    Bug: 515762868
    Change-Id: I05db90000f98c2b7b50ffd123ab13aaf651c2d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8058633
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678962}

--- components/optimization_guide/core/delivery/prediction_model_store.h
+++ components/optimization_guide/core/delivery/prediction_model_store.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/values.h"
 #include "components/optimization_guide/core/delivery/model_enums.h"
+#include "components/optimization_guide/core/delivery/model_info.h"
 #include "components/optimization_guide/core/delivery/model_store_metadata_entry.h"
 #include "components/optimization_guide/proto/models.pb.h"
 
@@ -120,7 +121,7 @@ class PredictionModelStore {
   void OnModelLoaded(proto::OptimizationTarget optimization_target,
                      const ClientCacheKey& model_cache_key,
                      PredictionModelLoadedCallback callback,
-                     std::unique_ptr<proto::PredictionModel> model);
+                     std::optional<ModelInfo> model_info);
 
   // Invoked when the model files are verified on a model update.
   void OnModelUpdateVerified(proto::OptimizationTarget optimization_target,
