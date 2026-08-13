018f1d1ca1dd896da31b5c7f9f2e9b133f8be5c3
Dominik Röttsches <drott@chromium.org>
2026-08-13 08:42:15 -0700

    Migrate OpenType format check bindings to Crubit
    We can remove the Box<> wrapped typing of FontFormatInfo along the way.
    
    DEPS checks for the generated output header files requires us to move
    the format_check target into the fonts subdirectory. This is a good
    opportunity to move more font specific build rules to the font subdir in
    the future and reduce the weight of the platform/BUILD.gn build dir.
    
    Exercise in using the Crubit bindings mechanism.
    
    No functional change.
    
    Change-Id: I9440c7285bb707b325b80ee074de51e714ee4884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8244248
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678858}

--- third_party/blink/renderer/platform/fonts/opentype/font_format_check.h
+++ third_party/blink/renderer/platform/fonts/opentype/font_format_check.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_OPENTYPE_FONT_FORMAT_CHECK_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_OPENTYPE_FONT_FORMAT_CHECK_H_
 
-#include "third_party/blink/renderer/platform/fonts/opentype/format_check.rs.h"
+#include "third_party/blink/renderer/platform/fonts/font_format.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/skia/include/core/SkData.h"
@@ -20,7 +20,7 @@ class PLATFORM_EXPORT FontFormatCheck {
 
  public:
   explicit FontFormatCheck(sk_sp<SkData>);
-  virtual ~FontFormatCheck() = default;
+  virtual ~FontFormatCheck();
   virtual bool IsVariableFont() const;
   virtual bool IsCbdtCblcColorFont() const;
   virtual bool IsEbdtEblcMonochromeFont() const;
@@ -47,7 +47,7 @@ class PLATFORM_EXPORT FontFormatCheck {
   enum class COLRVersion { kCOLRV0, kCOLRV1, kNoCOLR };
 
  private:
-  rust::Box<font_format_check::FontFormatInfo> format_info_;
+  font_format::FontFormatInfo format_info_;
 };
 
 }  // namespace blink
