1f6a1fa9ebbfa442ef24b6d4e894fd595786f541
Bhuvana Betini <bbetini@google.com>
2026-08-13 12:19:32 -0700

    [Actor] Start offscreen rendering before removing tab from tab model
    Bug: 542693536
    Change-Id: Ie286e048c19364729580e6753b96987db5700302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8243567
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679024}

--- chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorTabStateHelper.java
+++ chrome/browser/actor/android/java/src/org/chromium/chrome/browser/actor/ActorTabStateHelper.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.actor;
 
+import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.Token;
 import org.chromium.build.annotations.NullMarked;
@@ -38,10 +39,12 @@ public class ActorTabStateHelper {
      *
      * @param selector The TabModelSelector of the stopping activity.
      * @param windowId The window ID where the selector is located.
+     * @param onTabDetaching Callback invoked for each tab before it is removed from the TabModel,
+     *     e.g. to start offscreen rendering.
      * @return A list of prepared BackgroundSession objects.
      */
     public static List<BackgroundSession> detachActiveBackgroundSessions(
-            TabModelSelector selector, int windowId) {
+            TabModelSelector selector, int windowId, Callback<Tab> onTabDetaching) {
         ThreadUtils.assertOnUiThread();
         TabModel regularModel = selector.getModel(/* incognito= */ false);
         ActorKeyedService service = getActorKeyedService(regularModel);
@@ -50,7 +53,7 @@ public class ActorTabStateHelper {
             return Collections.emptyList();
         }
 
-        return findAndDetachActiveSessions(regularModel, service, windowId);
+        return findAndDetachActiveSessions(regularModel, service, windowId, onTabDetaching);
     }
 
     /**
@@ -58,7 +61,7 @@ public class ActorTabStateHelper {
      * Only creates and populates sessions for tabs whose placeholders were inserted correctly.
      */
     private static List<BackgroundSession> findAndDetachActiveSessions(
-            TabModel model, ActorKeyedService service, int windowId) {
+            TabModel model, ActorKeyedService service, int windowId, Callback<Tab> onTabDetaching) {
         List<BackgroundSession> sessions = new ArrayList<>();
 
         for (Tab originalTab : model) {
@@ -82,6 +85,7 @@ public class ActorTabStateHelper {
             } else {
                 sessions.add(new BackgroundSession(tabData, taskId));
             }
+            onTabDetaching.onResult(originalTab);
             // TODO(b/544014273) : Consider canceling the task if detaching tab was not successful
             model.getTabRemover().removeTab(originalTab, /* allowDialog= */ false);
         }
