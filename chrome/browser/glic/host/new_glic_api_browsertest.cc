16a72a0b24f9acbad2defa85452965e7bb87a133
Bryant Chandler <bryantchandler@google.com>
2026-08-13 08:18:24 -0700

    [glic] Migrate testPinTabsFailsWhenDoesNotExist to new_glic_api_browsertest
    Bug: b:477918640
    Change-Id: I97bfab05199fd6b18fa9a77ab258f9aa6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8236220
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678847}

--- chrome/browser/glic/host/new_glic_api_browsertest.cc
+++ chrome/browser/glic/host/new_glic_api_browsertest.cc
@@ -1380,6 +1380,11 @@ IN_PROC_BROWSER_TEST_P(NewGlicApiTest, testPinTabs) {
   ExecuteJsTest();
 }
 
+IN_PROC_BROWSER_TEST_P(NewGlicApiTest, testPinTabsFailsWhenDoesNotExist) {
+  ASSERT_OK(OpenGlicForActiveTab());
+  ExecuteJsTest();
+}
+
 #if !BUILDFLAG(IS_ANDROID)
 class NewGlicApiTestWithFileUploadPolicyEnabled : public NewGlicApiTest {
  public:
