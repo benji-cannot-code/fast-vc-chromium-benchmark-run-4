1eb6ebaa99932102c0abe6aec11510298148c319
Dale Curtis <dalecurtis@chromium.org>
2026-08-13 10:30:10 -0700

    Switch MediaCodecAudioDecoder to use more specific DecoderStatus
    Also extend the histograms to figure out where 20% failure rate is
    coming from on Android.
    
    Bug: 539524678
    
    Change-Id: Id6282e4f9502c1d3d8d10784ac8e17c7718431d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8244927
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678935}

--- media/filters/android/media_codec_audio_decoder.cc
+++ media/filters/android/media_codec_audio_decoder.cc
@@ -68,6 +68,8 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
   DCHECK(output_cb);
   DCHECK(waiting_cb);
 
+  InitCB bound_init_cb = base::BindPostTaskToCurrentDefault(std::move(init_cb));
+
   // Initialization and reinitialization should not be called during pending
   // decode.
   DCHECK(input_queue_.empty());
@@ -76,8 +78,7 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
 
   if (state_ == STATE_ERROR) {
     DVLOG(1) << "Decoder is in error state.";
-    base::BindPostTaskToCurrentDefault(std::move(init_cb))
-        .Run(DecoderStatus::Codes::kFailed);
+    std::move(bound_init_cb).Run(DecoderStatus::Codes::kFailedToCreateDecoder);
     return;
   }
 
@@ -132,8 +133,7 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
 
   if (!is_codec_supported) {
     DVLOG(1) << "Unsupported codec " << GetCodecName(config.codec());
-    base::BindPostTaskToCurrentDefault(std::move(init_cb))
-        .Run(DecoderStatus::Codes::kUnsupportedCodec);
+    std::move(bound_init_cb).Run(DecoderStatus::Codes::kUnsupportedCodec);
     return;
   }
 
@@ -147,11 +147,17 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
   SetInitialConfiguration();
 
   if (config_.is_encrypted() && !media_crypto_) {
-    if (!cdm_context || !cdm_context->GetMediaCryptoContext()) {
-      LOG(ERROR) << "The stream is encrypted but there is no CdmContext or "
-                    "MediaCryptoContext is not supported";
+    if (!cdm_context) {
+      LOG(ERROR) << "The stream is encrypted but there is no CdmContext";
       SetState(STATE_ERROR);
-      base::BindPostTaskToCurrentDefault(std::move(init_cb))
+      std::move(bound_init_cb).Run(DecoderStatus::Codes::kMissingCDM);
+      return;
+    }
+    if (!cdm_context->GetMediaCryptoContext()) {
+      LOG(ERROR)
+          << "The stream is encrypted but MediaCryptoContext is not supported";
+      SetState(STATE_ERROR);
+      std::move(bound_init_cb)
           .Run(DecoderStatus::Codes::kUnsupportedEncryptionMode);
       return;
     }
@@ -159,19 +165,17 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
     // Postpone initialization after MediaCrypto is available.
     // SetCdm uses init_cb in a method that's already bound to the current loop.
     SetState(STATE_WAITING_FOR_MEDIA_CRYPTO);
-    SetCdm(cdm_context, std::move(init_cb));
+    SetCdm(cdm_context, std::move(bound_init_cb));
     return;
   }
 
   if (!CreateMediaCodecLoop()) {
-    base::BindPostTaskToCurrentDefault(std::move(init_cb))
-        .Run(DecoderStatus::Codes::kFailed);
+    std::move(bound_init_cb).Run(DecoderStatus::Codes::kFailedToCreateDecoder);
     return;
   }
 
   SetState(STATE_READY);
-  base::BindPostTaskToCurrentDefault(std::move(init_cb))
-      .Run(DecoderStatus::Codes::kOk);
+  std::move(bound_init_cb).Run(DecoderStatus::Codes::kOk);
 }
 
 bool MediaCodecAudioDecoder::CreateMediaCodecLoop() {
@@ -205,14 +209,14 @@ void MediaCodecAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
       base::BindPostTaskToCurrentDefault(std::move(decode_cb));
 
   if (!DecoderBuffer::DoSubsamplesMatch(*buffer)) {
-    std::move(bound_decode_cb).Run(DecoderStatus::Codes::kFailed);
+    std::move(bound_decode_cb).Run(DecoderStatus::Codes::kInvalidArgument);
     return;
   }
 
   if (!buffer->end_of_stream() && buffer->timestamp() == kNoTimestamp) {
     DVLOG(2) << __func__ << " " << buffer->AsHumanReadableString()
              << ": no timestamp, skipping this buffer";
-    std::move(bound_decode_cb).Run(DecoderStatus::Codes::kFailed);
+    std::move(bound_decode_cb).Run(DecoderStatus::Codes::kMissingTimestamp);
     return;
   }
 
@@ -221,8 +225,9 @@ void MediaCodecAudioDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
     // We get here if an error happens in DequeueOutput() or Reset().
     DVLOG(2) << __func__ << " " << buffer->AsHumanReadableString()
              << ": Error state, returning decode error for all buffers";
-    ClearInputQueue(DecoderStatus::Codes::kFailed);
-    std::move(bound_decode_cb).Run(DecoderStatus::Codes::kFailed);
+    ClearInputQueue(DecoderStatus::Codes::kPlatformDecodeFailure);
+    std::move(bound_decode_cb)
+        .Run(DecoderStatus::Codes::kPlatformDecodeFailure);
     return;
   }
 
@@ -322,7 +327,7 @@ void MediaCodecAudioDecoder::OnMediaCryptoReady(
   // After receiving |media_crypto_| we can configure MediaCodec.
   if (!CreateMediaCodecLoop()) {
     SetState(STATE_UNINITIALIZED);
-    std::move(init_cb).Run(DecoderStatus::Codes::kFailed);
+    std::move(init_cb).Run(DecoderStatus::Codes::kFailedToCreateDecoder);
     return;
   }
 
@@ -351,7 +356,8 @@ void MediaCodecAudioDecoder::OnInputDataQueued(bool success) {
     return;
 
   std::move(input_queue_.front().second)
-      .Run(success ? DecoderStatus::Codes::kOk : DecoderStatus::Codes::kFailed);
+      .Run(success ? DecoderStatus::Codes::kOk
+                   : DecoderStatus::Codes::kPlatformDecodeFailure);
   input_queue_.pop_front();
 }
 
@@ -373,7 +379,7 @@ void MediaCodecAudioDecoder::SetState(State new_state) {
 void MediaCodecAudioDecoder::OnCodecLoopError() {
   // If the codec transitions into the error state, then so should we.
   SetState(STATE_ERROR);
-  ClearInputQueue(DecoderStatus::Codes::kFailed);
+  ClearInputQueue(DecoderStatus::Codes::kPlatformDecodeFailure);
 }
 
 bool MediaCodecAudioDecoder::OnDecodedEos(
