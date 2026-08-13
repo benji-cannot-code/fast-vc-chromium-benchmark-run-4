3322a9aa369ebb31e2da199e82478eaf4b01e97a
Andrew Grieve <agrieve@chromium.org>
2026-08-13 10:54:13 -0700

    Android: Remove LibraryLoader.ensureMainDexInitialized()
    And have test runners call LibraryLoader.resetForTesting() between tests
    
    This makes LibraryLoader.isInitialized() return false in tests, unless a
    test itself calls LibraryLoader.ensureInitialized(). We still call
    System.loadLibrary() before running tests so that JNI works. It's just
    the "isInitialized()" return value that changes.
    
    Bug: 543087102
    Change-Id: I10e1ee21380655e593c0664e183c6f217bdb06cf
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8224685
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678955}

--- chrome/android/junit/src/org/chromium/chrome/browser/ExternalIntentUrlCheckerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ExternalIntentUrlCheckerTest.java
@@ -18,7 +18,6 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.url.GURL;
 import org.chromium.url.JUnitTestGURLs;
@@ -32,9 +31,6 @@ public class ExternalIntentUrlCheckerTest {
 
     @Before
     public void setUp() {
-        // To allow use of GURL.
-        LibraryLoader.getInstance().ensureMainDexInitialized();
-
         ExternalIntentUrlCheckerJni.setInstanceForTesting(mNativeMock);
     }
 
--- components/browser_ui/util/android/java/src/org/chromium/components/browser_ui/util/DownloadUtilsTest.java
+++ components/browser_ui/util/android/java/src/org/chromium/components/browser_ui/util/DownloadUtilsTest.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.util;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,11 +21,6 @@ import org.chromium.url.GURL;
 @RunWith(BaseRobolectricTestRunner.class)
 public class DownloadUtilsTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
-
-    @Before
-    public void setUp() {
-        GURL.ensureNativeInitializedForGURL();
-    }
 
     @Test
     public void testFormatUrlForDisplayInNotification_InvalidOrEmptyUrl() {
