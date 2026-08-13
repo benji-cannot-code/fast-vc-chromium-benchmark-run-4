6b2e3f23e5c4e87df173887a4c618b92b00908af
Adam Rice <ricea@chromium.org>
2026-08-13 01:20:30 -0700

    Add QuicNetworkConnectivityTracker
    Track whether QUIC has worked on the current network and reset the
    status when the IP address changes.
    
    This will be used instead of calling GetLocalAddress() on the socket
    when Android UDP Connect Emulation is enabled.
    
    See the design doc:
    https://docs.google.com/document/d/10qtXPrUw2QW_kIZcqpEmxz-jPzouIisaSqV0QzW12Rw/edit?usp=sharing
    
    TAG=agy CONV=af4c8f91-69e6-43ea-ab3f-abf660b4334b
    
    Bug: 512171954
    Change-Id: I125a1fdc481f74762b5f08f6185db65e4db995bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8223063
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678678}

---
+++ net/quic/quic_network_connectivity_tracker.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/quic/quic_network_connectivity_tracker.h"
+
+namespace net {
+
+QuicNetworkConnectivityTracker::QuicNetworkConnectivityTracker() {
+  NetworkChangeNotifier::AddIPAddressObserver(this);
+}
+
+QuicNetworkConnectivityTracker::~QuicNetworkConnectivityTracker() {
+  NetworkChangeNotifier::RemoveIPAddressObserver(this);
+}
+
+void QuicNetworkConnectivityTracker::OnQuicWorkingOnCurrentNetwork() {
+  has_quic_ever_worked_on_current_network_ = true;
+}
+
+void QuicNetworkConnectivityTracker::ResetQuicWorkingOnCurrentNetwork() {
+  has_quic_ever_worked_on_current_network_ = false;
+}
+
+bool QuicNetworkConnectivityTracker::HasQuicEverWorkedOnCurrentNetwork() const {
+  return has_quic_ever_worked_on_current_network_;
+}
+
+void QuicNetworkConnectivityTracker::OnIPAddressChanged(
+    NetworkChangeNotifier::IPAddressChangeType change_type) {
+  has_quic_ever_worked_on_current_network_ = false;
+}
+
+}  // namespace net
