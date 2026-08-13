cc0f5be8e79a977215067514018b7db05afaaf50
Leo Zhao <leozhao@google.com>
2026-08-12 18:45:21 -0700

    [iOS][AtMemory] Add AtMemoryGranularFillCellContentConfiguration
    This CL adds `AtMemoryGranularFillCellContentConfiguration` and its
    corresponding content view (`AtMemoryGranularFillContentView`) for
    rendering AtMemory granular fill table view cells.
    
    Key additions:
    
    - `AtMemoryGranularFillCellContentConfiguration`: Implements
      `UIContentConfiguration` to represent an attribute label and a single
      selectable value chip button.
    
    - `AtMemoryGranularFillContentView`: Implements `UIContentView` with
      subviews for the attribute title and chip button.
    
    Bug: 522340351
    Change-Id: I32b63cdcd64c9ea5f6d2217235cd2e31c6bd6701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242513
    Reviewed-by: Angela Novakovic <novakovica@google.com>
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678510}

---
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_granular_fill_cell_content_configuration.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UI_AT_MEMORY_GRANULAR_FILL_CELL_CONTENT_CONFIGURATION_H_
+#define IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UI_AT_MEMORY_GRANULAR_FILL_CELL_CONTENT_CONFIGURATION_H_
+
+#import <UIKit/UIKit.h>
+
+// Content configuration for a custom table view cell showing an AtMemory
+// attribute name and a selectable value chip button.
+@interface AtMemoryGranularFillCellContentConfiguration
+    : NSObject <UIContentConfiguration>
+
+// Text to display as the attribute name.
+@property(nonatomic, copy) NSString* attributeName;
+
+// Attribute value string to display as a selectable chip button.
+@property(nonatomic, copy) NSString* attributeValue;
+
+// Returns a new default content configuration instance.
++ (instancetype)cellConfiguration;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UI_AT_MEMORY_GRANULAR_FILL_CELL_CONTENT_CONFIGURATION_H_
