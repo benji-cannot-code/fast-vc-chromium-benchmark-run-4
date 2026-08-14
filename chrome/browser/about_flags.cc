40671ce4617ae1130b351d9485f4585a2d65d96d
Joel Tan-Aristy <jtanaristy@google.com>
2026-08-14 16:59:50 -0700

    [Vertical Tabs] Add incognito-button variation to about:flags
    Adds the incognito_button param variation in all experimental flags.
    
    Bug: 537032526
    Change-Id: I84e7282ca7f7c065e2f77d06e2cd4cd6da2a230b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252593
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Auto-Submit: Joel Tan-Aristy <jtanaristy@google.com>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680031}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2576,10 +2576,13 @@ const FeatureEntry::FeatureParam kAndroidVerticalTabs_AutoResize[] = {
     {"auto_resize", "true"}};
 const FeatureEntry::FeatureParam kAndroidVerticalTabs_MultiSelect[] = {
     {"multi_select", "true"}};
+const FeatureEntry::FeatureParam kAndroidVerticalTabs_IncognitoButton[] = {
+    {"incognito_button", "true"}};
 const FeatureEntry::FeatureParam kAndroidVerticalTabs_All[] = {
     {"external_drag", "true"},
     {"auto_resize", "true"},
-    {"multi_select", "true"}};
+    {"multi_select", "true"},
+    {"incognito_button", "true"}};
 const FeatureEntry::FeatureParam kAndroidVerticalTabs_EnableByDefault[] = {
     {"enable_by_default", "true"}};
 
@@ -2587,6 +2590,7 @@ const FeatureEntry::FeatureVariation kAndroidVerticalTabsVariations[] = {
     {"with external-drag", kAndroidVerticalTabs_ExternalDrag, nullptr},
     {"with auto-resize", kAndroidVerticalTabs_AutoResize, nullptr},
     {"with multi-select", kAndroidVerticalTabs_MultiSelect, nullptr},
+    {"with incognito-button", kAndroidVerticalTabs_IncognitoButton, nullptr},
     {"with all experimental features", kAndroidVerticalTabs_All, nullptr},
     {"with enabled-by-default", kAndroidVerticalTabs_EnableByDefault, nullptr},
 };
