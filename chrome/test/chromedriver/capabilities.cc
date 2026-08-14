dce9eba7700e29c79f27308b846de056f1904941
Nina Satragno <nsatragno@google.com>
2026-08-14 15:29:09 -0700

    [chromedriver] CMTG keys in WebAuthn commands
    Implement support for Credential Manager Trust Group (CMTG) keys in
    ChromeDriver, mapping WebDriver extension commands to the new CDP
    WebAuthn options. This allows configuring virtual authenticators with
    CMTG support and managing CMTG keys associated to credentials.
    
    These new options shouldn't have any web visible impact until we launch
    the WebAuthn CMTG keys feature. When we request launch approval for CMTG
    keys, we'll point to these changes as evidence of testing support.
    
    It's possible the spec may change. If that happens, we'll simply update
    the implementation accordingly before launch.
    
    See https://github.com/w3c/webauthn/pull/2445
    
    TAG=agy
    CONV=6d517beb-bd52-49f8-bfdd-546798f61797
    
    Bug: 485888879
    Change-Id: I7c3580dc5aa92431328c615d429ff85e24174135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177736
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679972}

--- chrome/test/chromedriver/capabilities.cc
+++ chrome/test/chromedriver/capabilities.cc
@@ -1391,6 +1391,8 @@ Status Capabilities::Parse(const base::DictValue& desired_caps,
       base::BindRepeating(&ParseBoolean, nullptr);
   parser_map["webauthn:extension:largeBlob"] =
       base::BindRepeating(&ParseBoolean, nullptr);
+  parser_map["webauthn:extension:cmtgKey"] =
+      base::BindRepeating(&ParseBoolean, nullptr);
   // See https://github.com/fedidcg/FedCM/pull/478
   parser_map["fedcm:accounts"] = base::BindRepeating(&ParseBoolean, nullptr);
 
