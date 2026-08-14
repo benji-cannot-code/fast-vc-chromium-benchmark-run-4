f26b56bb6bc88fee434d33785ae0cdacfacddd89
Xi Han <hanxi@chromium.org>
2026-08-13 18:13:21 -0700

    Revert "[Extensions] Close devtools when devtools extensions are unloaded"
    This reverts commit d005bcff49a2529b2d834bcbf34f1cf717d06d50.
    
    Reason for revert: b/546216109
    
    Original change's description:
    > [Extensions] Close devtools when devtools extensions are unloaded
    >
    > Extensions can hook into devtools windows. When these extensions are
    > unloaded, we should close the associated devtools windows. This is
    > important, since an unloaded + reloaded extension can have different
    > APIs and permissions, which aren't captured in the devtools scripts.
    >
    > This is a bit disruptive if you have a devtools window open, but
    > a) is consistent with how we handle many other extension contexts (e.g.,
    >    closing open tabs, popups, unloading iframes, etc)
    > b) should be relatively rare -- extension unloads typically happen
    >    either when the extension is idle or from a direct signal (e.g.,
    >    a user action like disabling the extension).
    >
    > This ensures extension contexts -- including devtools contexts -- are
    > kept in sync with the latest version of the extension.
    >
    > Bug: 483435192
    > Change-Id: I856a58b00f1ca291bd910539e0d7e7a515f54505
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253197
    > Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1679051}
    
    Bug: 483435192
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ib6e58cbfcdeb7423e2221f8c592b674f2c1dbd2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256216
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Owners-Override: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679245}

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -1718,17 +1718,7 @@ void DevToolsWindow::ActivateWindow() {
 }
 
 void DevToolsWindow::CloseWindow() {
-  if (is_docked_) {
-    Close(DevToolsClosedByAction::kCloseButton);
-  } else {
-#if BUILDFLAG(IS_ANDROID)
-    main_web_contents_->Close();
-#else
-    if (browser_) {
-      browser_->GetWindow()->Close();
-    }
-#endif  // BUILDFLAG(IS_ANDROID)
-  }
+  Close(DevToolsClosedByAction::kCloseButton);
 }
 
 void DevToolsWindow::Close(DevToolsClosedByAction closed_by) {
