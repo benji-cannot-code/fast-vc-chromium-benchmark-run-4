4ad8d3a38441160395de6a4f05f6d21a16a0b134
Paul Jensen <pauljensen@chromium.org>
2026-08-14 11:56:11 -0700

    Tiny optimization to TrackedElementManager.getElementBounds_()
    Avoids an extra getDataForElement_() call and null check. No
    functional change.
    
    Change-Id: Ic3a36a25c290de19f2463ee159feb08aaf2bae4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256575
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679770}

--- ui/webui/resources/js/tracked_element/tracked_element_manager.ts
+++ ui/webui/resources/js/tracked_element/tracked_element_manager.ts
@@ -598,7 +598,7 @@ export class TrackedElementManager {
       return;
     }
 
-    const bounds: RectF = visible ? this.getElementBounds_(element) :
+    const bounds: RectF = visible ? this.getElementBounds_(trackedElement) :
                                     {x: 0, y: 0, width: 0, height: 0};
 
     const update = {visible, bounds, element};
@@ -648,22 +648,19 @@ export class TrackedElementManager {
     });
   }
 
-  private getElementBounds_(element: HTMLElement): RectF {
+  private getElementBounds_(trackedElement: TrackedElementData): RectF {
     const rect: RectF = {x: 0, y: 0, width: 0, height: 0};
-    const bounds = element.getBoundingClientRect();
+    const bounds = trackedElement.element.getBoundingClientRect();
     rect.x = bounds.x;
     rect.y = bounds.y;
     rect.width = bounds.width;
     rect.height = bounds.height;
 
-    const trackedElement = this.getDataForElement_(element);
-    if (trackedElement) {
-      const padding = trackedElement.padding;
-      rect.x -= padding.left;
-      rect.y -= padding.top;
-      rect.width += padding.left + padding.right;
-      rect.height += padding.top + padding.bottom;
-    }
+    const padding = trackedElement.padding;
+    rect.x -= padding.left;
+    rect.y -= padding.top;
+    rect.width += padding.left + padding.right;
+    rect.height += padding.top + padding.bottom;
     return rect;
   }
 
