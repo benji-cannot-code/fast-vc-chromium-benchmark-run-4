6a47254ddc35eb8d4daa1dc50ddd899ebc1195fe
Bryant Chandler <bryantchandler@google.com>
2026-08-14 15:03:13 -0700

    [glic] Migrate testSetOnboardingCompleted to new_glic_api_browsertest
    Bug: b:477918640
    Change-Id: I8ce7c54846d5546e7fccf0ab94c8421b6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253240
    Reviewed-by: Nick Birnie <birnie@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679961}

--- chrome/browser/glic/host/new_glic_api_browsertest.cc
+++ chrome/browser/glic/host/new_glic_api_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/gmock_callback_support.h"
 #include "base/test/gmock_expected_support.h"
+#include "base/test/metrics/user_action_tester.h"
 #include "base/test/scoped_logging_settings.h"
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
@@ -667,6 +668,43 @@ IN_PROC_BROWSER_TEST_P(NewGlicOnboardingApiTest, testIsOnboardingCompleted) {
   ASSERT_OK(OpenGlicForActiveTab());
   ExecuteJsTest();
   glic::SetFRECompletion(GetProfile(), prefs::FreStatus::kCompleted);
+  ContinueJsTest();
+}
+
+IN_PROC_BROWSER_TEST_P(NewGlicOnboardingApiTest, testSetOnboardingCompleted) {
+  ASSERT_OK(OpenGlicForActiveTab());
+  ExecuteJsTest();
+
+  ASSERT_FALSE(GlicEnabling::HasConsentedForProfile(GetProfile()));
+
+// Android doesn't use global hotkeys or a taskbar launcher
+#if !BUILDFLAG(IS_ANDROID)
+  base::test::TestFuture<void> default_browser_check_called;
+  // Ensure that CheckDefaultBrowserToEnableLauncher was called.
+  GlicLauncherConfiguration::SetCheckDefaultBrowserCallbackForTesting(
+      default_browser_check_called.GetRepeatingCallback());
+#endif
+
+  base::UserActionTester user_action_tester;
+  EXPECT_EQ(0,
+            user_action_tester.GetActionCount("Glic.Onboarding.OptInAccept"));
+
+  ContinueJsTest();
+
+  ASSERT_TRUE(base::test::RunUntil(
+      [&] { return GlicEnabling::HasConsentedForProfile(GetProfile()); }));
+
+// Android doesn't use global hotkeys or a taskbar launcher
+#if !BUILDFLAG(IS_ANDROID)
+  // Wait for the default browser check to be called.
+  EXPECT_TRUE(default_browser_check_called.Wait());
+  GlicLauncherConfiguration::SetCheckDefaultBrowserCallbackForTesting(
+      base::RepeatingClosure());
+#endif
+
+  EXPECT_EQ(1,
+            user_action_tester.GetActionCount("Glic.Onboarding.OptInAccept"));
+
   ContinueJsTest();
 }
 
