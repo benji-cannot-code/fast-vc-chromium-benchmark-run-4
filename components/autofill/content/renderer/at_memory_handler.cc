71602f990bce356191fc02b51003bd6f4a6623bb
Christoph Schwering <schwering@google.com>
2026-08-14 08:31:21 -0700

    [Autofill] Rename ShouldTriggerAtMemorySearch()
    The name in outdated: The function is called when AtMemory has already
    been triggered, and it's called only if AtMemory was triggered by the
    trigger string.
    
    Bug: 538494080
    Change-Id: I68371c78d4bf4345402fca8736e1f796cd257078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254980
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679567}

--- components/autofill/content/renderer/at_memory_handler.cc
+++ components/autofill/content/renderer/at_memory_handler.cc
@@ -142,7 +142,7 @@ std::optional<AtMemoryHandler::CaretInfo> AtMemoryHandler::GetCaretInfo(
   return std::nullopt;
 }
 
-bool AtMemoryHandler::ShouldTriggerAtMemorySearch(
+bool AtMemoryHandler::HasTriggerStringNextToCaret(
     const WebElement& element) const {
   if (!base::FeatureList::IsEnabled(features::kAutofillAtMemory)) {
     return false;
@@ -367,7 +367,7 @@ void AtMemoryHandler::ReplaceSelectionForAtMemory(WebElement element,
   }
 
   int offset = 0;
-  if (info->caused_by_trigger_string && ShouldTriggerAtMemorySearch(element)) {
+  if (info->caused_by_trigger_string && HasTriggerStringNextToCaret(element)) {
     offset = GetTriggerString().size();
   }
 
