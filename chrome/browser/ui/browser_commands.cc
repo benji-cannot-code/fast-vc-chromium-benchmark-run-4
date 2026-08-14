56f1b3ac90ae4a5f557df7513b13b7f83dbc90ef
Eshwar Stalin <estalin@chromium.org>
2026-08-14 14:01:48 -0700

    [BAM] Minor cleanup of CommandUpdater interface
    Removing the need for implementing multiple execute command functions
    with the same behavior (i.e. same disposition) and also removing that
    function from the public interface. Also making action context optional
    value as part of existing function instead of introducing multiple new
    functions.
    
    Change-Id: Ic2a96324734a11440507542cf0edc7381e87a660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259354
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679887}

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -814,16 +814,15 @@ bool ExecuteCommand(BrowserWindowInterface* browser,
                     int command,
                     base::TimeTicks time_stamp) {
   return browser->GetFeatures().browser_command_controller()->ExecuteCommand(
-      command, time_stamp);
+      command, std::nullopt, time_stamp);
 }
 
 bool ExecuteCommandWithContext(BrowserWindowInterface* browser,
                                int command,
                                actions::ActionInvocationContext context,
                                base::TimeTicks time_stamp) {
-  return browser->GetFeatures()
-      .browser_command_controller()
-      ->ExecuteCommandWithContext(command, std::move(context), time_stamp);
+  return browser->GetFeatures().browser_command_controller()->ExecuteCommand(
+      command, std::move(context), time_stamp);
 }
 
 bool ExecuteCommandWithDisposition(BrowserWindowInterface* browser,
@@ -832,7 +831,8 @@ bool ExecuteCommandWithDisposition(BrowserWindowInterface* browser,
                                    base::TimeTicks time_stamp) {
   return browser->GetFeatures()
       .browser_command_controller()
-      ->ExecuteCommandWithDisposition(command, disposition, time_stamp);
+      ->ExecuteCommandWithDisposition(command, disposition, std::nullopt,
+                                      time_stamp);
 }
 
 bool ExecuteCommandWithDispositionAndContext(
@@ -843,8 +843,8 @@ bool ExecuteCommandWithDispositionAndContext(
     base::TimeTicks time_stamp) {
   return browser->GetFeatures()
       .browser_command_controller()
-      ->ExecuteCommandWithDispositionAndContext(command, disposition,
-                                                std::move(context), time_stamp);
+      ->ExecuteCommandWithDisposition(command, disposition, std::move(context),
+                                      time_stamp);
 }
 
 void UpdateCommandEnabled(BrowserWindowInterface* browser,
