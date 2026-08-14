87e644c34fa3cd564c1ca6e6fe6687fc4e1e70ec
Fiaz Muhammad <mfiaz@google.com>
2026-08-14 13:02:13 -0700

    [TabStrip] Ungroup tabs before moving to a tab group in another window
    When adding grouped tabs to a tab group in another window from the
    tab strip context menu, ungroup the tabs before moving them to the
    destination window. This prevents transferring invalid group
    associations across windows.
    
    Bug: 529707774
    Change-Id: I56c9e1f6d5af11edb0d3ca0b3a9fa6f9f9cf48ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259080
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679836}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabContextMenuCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabContextMenuCoordinator.java
@@ -1172,6 +1172,7 @@ public class TabContextMenuCoordinator extends TabStripReorderingHelper<AnchorIn
                                     getTabModel(),
                                     /* tabMovedCallback= */ null);
                         } else {
+                            ungroupTabs(tabs);
                             mMultiInstanceOrchestrator.moveTabsToWindowByIdChecked(
                                     windowId,
                                     tabs,
