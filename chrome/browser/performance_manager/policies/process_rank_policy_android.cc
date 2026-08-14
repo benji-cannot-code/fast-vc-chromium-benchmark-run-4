e9e6a13cca5c9e52fc6b6c133781c496e1daa816
Aldo Culquicondor <acondor@chromium.org>
2026-08-14 06:39:22 -0700

    Pass the previous URL to PageNodeObserver::OnMainFrameUrlChanged.
    Updates PageNodeObserver::OnMainFrameUrlChanged to include the previous
    URL as a parameter. This will be used for better decision making in a
    new priority voter.
    
    Bug: 532217630
    Change-Id: I2e5a241bfde4271b0ed8ed2c6f25bb6a48d99e97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258254
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679495}

--- chrome/browser/performance_manager/policies/process_rank_policy_android.cc
+++ chrome/browser/performance_manager/policies/process_rank_policy_android.cc
@@ -244,7 +244,8 @@ void ProcessRankPolicyAndroid::OnHasPictureInPictureChanged(
 }
 
 void ProcessRankPolicyAndroid::OnMainFrameUrlChanged(
-    const PageNode* page_node) {
+    const PageNode* page_node,
+    const GURL& previous_url) {
   UpdateProcessRank(page_node);
 }
 
