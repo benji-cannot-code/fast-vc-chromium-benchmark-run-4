e0526b4642fb583ae07db7e73f9145305c4eb723
Alison Gale <agale@chromium.org>
2026-08-14 11:37:56 -0700

    Update callers to use CloseWebContents
    Replacing calls to CloseWebContentsAt in cases where we already have a
    TabInterface or WebContents.
    
    #top-chrome-bug-fixit
    
    Bug: 540843679
    Change-Id: I180164c4cfe3524dfce8dd74e6bffee4a9a17988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254880
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679741}

--- chrome/browser/page_load_metrics/observers/initial_webui_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/initial_webui_page_load_metrics_observer_browsertest.cc
@@ -266,9 +266,7 @@ class InitialWebUIPageLoadMetricsObserverBrowserTest
     }
 
     if (close_tab) {
-      int index =
-          browser()->tab_strip_model()->GetIndexOfWebContents(raw_contents);
-      browser()->tab_strip_model()->CloseWebContentsAt(index, 0);
+      browser()->tab_strip_model()->CloseWebContents(raw_contents, 0);
     }
     return raw_contents;
   }
@@ -336,8 +334,7 @@ IN_PROC_BROWSER_TEST_F(InitialWebUIPageLoadMetricsObserverBrowserTest,
   }
 
   // Manually close the tab now to trigger page end metrics.
-  int index = browser()->tab_strip_model()->GetIndexOfWebContents(web_contents);
-  browser()->tab_strip_model()->CloseWebContentsAt(index, 0);
+  browser()->tab_strip_model()->CloseWebContents(web_contents, 0);
 
   auto page_load_entries = GetEntriesForUrl("InitialWebUIPageLoad", url);
 
@@ -534,8 +531,7 @@ IN_PROC_BROWSER_TEST_F(InitialWebUIPageLoadMetricsObserverBrowserTest,
   metrics_waiter->Wait();
 
   // Close the tab to trigger OnComplete and OnHidden.
-  int index = browser()->tab_strip_model()->GetIndexOfWebContents(bg_contents);
-  browser()->tab_strip_model()->CloseWebContentsAt(index, 0);
+  browser()->tab_strip_model()->CloseWebContents(bg_contents, 0);
 
   // Verify InitialWebUIPageLoad has Page Load, Renderer Usage, Timing, and
   // Page End metrics. Note that paint metrics are not recorded for background
@@ -693,8 +689,7 @@ IN_PROC_BROWSER_TEST_F(InitialWebUIPageLoadMetricsObserverBrowserTest,
   chrome::Reload(browser(), WindowOpenDisposition::CURRENT_TAB);
   reload_observer.Wait();
 
-  int index = browser()->tab_strip_model()->GetIndexOfWebContents(contents);
-  browser()->tab_strip_model()->CloseWebContentsAt(index, 0);
+  browser()->tab_strip_model()->CloseWebContents(contents, 0);
 
   // We expect entries for both the original load and the reload.
   auto page_load_entries = GetEntriesForUrl("InitialWebUIPageLoad", url);
@@ -850,8 +845,7 @@ IN_PROC_BROWSER_TEST_F(InitialWebUIPageLoadMetricsObserverBrowserTest,
   observer->FlushMetricsOnAppEnterBackground();
 
   // Close the WebContents to finish recording and upload UKM
-  int index = browser()->tab_strip_model()->GetIndexOfWebContents(contents);
-  browser()->tab_strip_model()->CloseWebContentsAt(index, 0);
+  browser()->tab_strip_model()->CloseWebContents(contents, 0);
 
   // Verify that PageEndReason of END_APP_ENTER_BACKGROUND (value 6) is logged
   auto entries = GetEntriesForUrl("InitialWebUIPageLoad", url);
@@ -917,9 +911,7 @@ IN_PROC_BROWSER_TEST_F(InitialWebUIPageLoadMetricsObserverBrowserTest,
   // Close the tab. This triggers OnComplete and RecordPageEndMetrics.
   // Since it is currently in foreground, the final foreground session will be
   // added to TotalForegroundDuration.
-  int index =
-      browser()->tab_strip_model()->GetIndexOfWebContents(active_contents);
-  browser()->tab_strip_model()->CloseWebContentsAt(index, 0);
+  browser()->tab_strip_model()->CloseWebContents(active_contents, 0);
 
   entries = GetEntriesForUrl("InitialWebUIPageLoad", url);
   EXPECT_THAT(entries,
