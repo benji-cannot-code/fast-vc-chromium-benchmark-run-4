9f68b413ff6903a5535c689e693cfce208216135
Yann Dago <ydago@chromium.org>
2026-08-14 07:44:09 -0700

    [enterprise] Add UserCloudManagementStatusFetcher to fetch management status from DMServer.
    Add UserCloudManagementStatusFetcher to fetch user cloud management status
    and interception policies from DM Server.
    
    TAG=agy
    CONV=13ce0979-e797-45c1-a8f6-b45ee5ebb11f
    
    Bug: 543381833
    Change-Id: Ieb0fc47dc6c2f20b801ecf1b83828ad116a64b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8215060
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679531}

--- components/policy/core/common/cloud/device_management_service.cc
+++ components/policy/core/common/cloud/device_management_service.cc
@@ -276,6 +276,9 @@ std::string DeviceManagementService::JobConfiguration::GetJobTypeAsString(
     case DeviceManagementService::JobConfiguration::TYPE_UPLOAD_STATUS:
       return "UploadStatus";
     case DeviceManagementService::JobConfiguration::
+        TYPE_USER_MANAGEMENT_STATUS_AND_POLICIES:
+      return "UserManagementStatusAndPolicies";
+    case DeviceManagementService::JobConfiguration::
         TYPE_TOKEN_BASED_DEVICE_REGISTRATION:
       return "TokenBasedDeviceRegistration";
     case DeviceManagementService::JobConfiguration::
@@ -508,6 +511,8 @@ JobConfigurationBase::GetTrafficAnnotationTag() {
         TYPE_DETERMINE_PROMOTION_ELIGIBILITY:
     case DeviceManagementService::JobConfiguration::
         TYPE_GENERATE_CHROME_PROFILE_CHALLENGE:
+    case DeviceManagementService::JobConfiguration::
+        TYPE_USER_MANAGEMENT_STATUS_AND_POLICIES:
     case DeviceManagementService::JobConfiguration::TYPE_GCM_ID_UPDATE:
     case DeviceManagementService::JobConfiguration::
         TYPE_UPLOAD_FM_REGISTRATION_TOKEN:
