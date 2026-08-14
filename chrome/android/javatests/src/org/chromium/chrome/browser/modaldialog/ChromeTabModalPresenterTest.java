0cd0989d1082909e3e85540c594eff1bf3101a6e
Dan Polanco <polardz@google.com>
2026-08-14 13:19:06 -0700

    Re-enable ChromeTabModalPresenterTest#testSuspend_ToggleOverview
    Use BottomBarTestUtils.findViewById for reliable tab switcher button
    lookup and synchronize on the tab interactability callback when entering
    the tab switcher to ensure tab modal dialogs are properly suspended
    before showing subsequent dialogs.
    
    Fixed: 40895583
    Change-Id: I0826dce65c0834a02973e53af2993a88ba716371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254943
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679853}

--- chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/ChromeTabModalPresenterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/modaldialog/ChromeTabModalPresenterTest.java
@@ -214,7 +214,6 @@ public class ChromeTabModalPresenterTest {
     @SmallTest
     @Feature({"ModalDialog"})
     @Restriction(DeviceFormFactor.PHONE)
-    @DisabledTest(message = "https://crbug.com/40895583")
     public void testSuspend_ToggleOverview() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> mActivity.getActivityTab().addObserver(mTestObserver));
@@ -240,8 +239,11 @@ public class ChromeTabModalPresenterTest {
         ChromeModalDialogTestUtils.checkBrowserControls(mActivity, true);
         checkCurrentPresenter(mManager, ModalDialogType.TAB);
 
-        //  Tab modal dialogs should be suspended on entering tab switcher.
-        onView(withId(R.id.tab_switcher_button)).perform(click());
+        // Tab modal dialogs should be suspended on entering tab switcher.
+        int callCount = mTestObserver.onTabInteractabilityChangedCallback.getCallCount();
+        View tabSwitcherBtn = BottomBarTestUtils.findViewById(mActivity, R.id.tab_switcher_button);
+        onView(is(tabSwitcherBtn)).perform(click());
+        mTestObserver.onTabInteractabilityChangedCallback.waitForCallback(callCount);
         checkPendingSize(mManager, ModalDialogType.TAB, 2);
         onView(withId(R.id.tab_modal_dialog_container))
                 .check(
@@ -266,7 +268,7 @@ public class ChromeTabModalPresenterTest {
         checkPendingSize(mManager, ModalDialogType.TAB, 2);
 
         // Exit overview mode. The first dialog should be showing again.
-        int callCount = mTestObserver.onTabInteractabilityChangedCallback.getCallCount();
+        callCount = mTestObserver.onTabInteractabilityChangedCallback.getCallCount();
         pressBack();
         mTestObserver.onTabInteractabilityChangedCallback.waitForCallback(callCount);
 
