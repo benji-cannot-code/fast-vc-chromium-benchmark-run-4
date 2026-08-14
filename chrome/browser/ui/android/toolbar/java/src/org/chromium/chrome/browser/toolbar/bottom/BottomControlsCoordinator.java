662ea46d778f9990fed1fb25714e3f6493d042fd
Calder Kitagawa <ckitagawa@chromium.org>
2026-08-13 21:00:53 -0700

    BottomControlsCoordinator free resources on destroy
    Bug: 546231218
    Change-Id: I6a01c776e9994bc0e6feefa82fd6c53bce9cd194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257898
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679338}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/bottom/BottomControlsCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/bottom/BottomControlsCoordinator.java
@@ -80,6 +80,7 @@ public class BottomControlsCoordinator implements BackPressHandler {
 
     private final ScrollingBottomViewResourceFrameLayout mRootFrameLayout;
     private final ScrollingBottomViewSceneLayer mSceneLayer;
+    private final ResourceManager mResourceManager;
 
     private boolean mIsDestroyed;
 
@@ -120,6 +121,7 @@ public class BottomControlsCoordinator implements BackPressHandler {
             NullableObservableSupplier<@BrowserControlsState Integer> constraintsSupplier,
             Supplier<Boolean> readAloudRestoringSupplier) {
         mRootFrameLayout = root;
+        mResourceManager = resourceManager;
         root.setConstraintsSupplier(constraintsSupplier);
         PropertyModel model = new PropertyModel(BottomControlsProperties.ALL_KEYS);
 
@@ -154,7 +156,7 @@ public class BottomControlsCoordinator implements BackPressHandler {
                         edgeToEdgeControllerSupplier,
                         tabSupplier,
                         readAloudRestoringSupplier);
-        resourceManager
+        mResourceManager
                 .getDynamicResourceLoader()
                 .registerResource(root.getId(), root.getResourceAdapter());
 
@@ -226,6 +228,7 @@ public class BottomControlsCoordinator implements BackPressHandler {
         mIsDestroyed = true;
         // The previously-provided supplier will have been destroyed, so prevent further use of it.
         mRootFrameLayout.setConstraintsSupplier(null);
+        mResourceManager.getDynamicResourceLoader().unregisterResource(mRootFrameLayout.getId());
 
         BottomControlsContentDelegate contentDelegate = mContentDelegateSupplier.get();
         if (contentDelegate != null) contentDelegate.destroy();
