9e733a8403f13e8d47e670d6e10f8b7c16d6c72b
Andrew Grieve <agrieve@chromium.org>
2026-08-14 08:17:42 -0700

    Android: Fix race condition in ICU initialization
    When testing with is_asan=true, I hit:
    [FATAL:base/i18n/break_iterator.cc:42] NOTREACHED hit. ubrk_open failed for type 1 with error 1
    
    It turns out that ICU's default locale is "en_US_POSIX", which our
    icudtl.dat does not contain tables for.
    
    Bug: 545195031
    Change-Id: I3d3880076594d4f23a6e7a51b497418eab9a2ba6
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258750
    Reviewed-by: Danilo Tedeschi <danft@google.com>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679554}

--- base/i18n/icu_util.cc
+++ base/i18n/icu_util.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/memory_mapped_file.h"
+#include "base/i18n/rtl.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/metrics_hashes.h"
@@ -37,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/apk_assets.h"
+#include "base/android/jni_android.h"
+#include "base/android/locale_utils.h"
 #endif
 
 #if BUILDFLAG(IS_IOS)
@@ -371,6 +374,20 @@ bool DoCommonInitialization() {
   // add a boolean argument to this function to init the default tz only
   // when requested.
   InitializeIcuTimeZone();
+
+#if BUILDFLAG(IS_ANDROID)
+  // On Android, ICU's default locale ID comes from POSIX environment variables
+  // (LC_ALL, etc.), which are not set for Android apps, resulting in
+  // "en_US_POSIX". That is not a real locale and causes break iterators and
+  // other ICU operations to fail if called before SetICUDefaultLocale() is run
+  // in PostEarlyInitialization. Initialize the default ICU locale using the
+  // system default.
+  if (base::android::IsJavaAvailable()) {
+    SetICUDefaultLocale(base::android::GetDefaultLocaleString());
+  } else {
+    SetICUDefaultLocale("en-US");
+  }
+#endif
 
   utrace_setLevel(UTRACE_VERBOSE);
   return true;
