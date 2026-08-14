4b19ab6b2ec6f1de8b81295e8d5c9401ea4e0161
Christoph Schwering <schwering@google.com>
2026-08-14 05:10:44 -0700

    [Gardener] Fix NavigationInitiatorPageLoadMetricsBFCacheBrowserTest
    This CL fixes the implicit assumption in
    NavigationInitiatorPageLoadMetricsBFCacheBrowserTest that
    BackForwardCache is enabled. This assumption was wrong when
    `--disable-features=BackForwardCache`, as `linux-bfcache-rel` does.
    
    The test now explicitly enables or disables BackForwardCache.
    
    Example failing run:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-bfcache-rel/86088/overview
    
    TAG=agy
    CONV=83840aae-dcc3-40ea-8532-252365ba5550
    
    Bug: 517725655
    Change-Id: I71193d15c43173d3552acadf5b23b129fb5520bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8250960
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Owners-Override: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679472}

--- chrome/browser/page_load_metrics/observers/navigation_initiator_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/navigation_initiator_page_load_metrics_observer_browsertest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/browser/navigation_handle_user_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -62,9 +63,7 @@ class NavigationInitiatorPageLoadMetricsBrowserTest
       : prerender_helper_(
             base::BindRepeating(&NavigationInitiatorPageLoadMetricsBrowserTest::
                                     GetActiveWebContents,
-                                base::Unretained(this))) {}
-
-  void SetUp() override {
+                                base::Unretained(this))) {
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/{},
         /*disabled_features=*/
@@ -72,7 +71,9 @@ class NavigationInitiatorPageLoadMetricsBrowserTest
         // is enabled and then remove these two Features.
         {omnibox::internal::kWebUIOmniboxPopup,
          omnibox::internal::kWebUIOmniboxAimPopup});
+  }
 
+  void SetUp() override {
     prerender_helper_.RegisterServerRequestMonitor(embedded_test_server());
     InProcessBrowserTest::SetUp();
   }
@@ -539,6 +540,18 @@ class NavigationInitiatorPageLoadMetricsBFCacheBrowserTest
     : public NavigationInitiatorPageLoadMetricsBrowserTest,
       public ::testing::WithParamInterface<bool> {
  public:
+  NavigationInitiatorPageLoadMetricsBFCacheBrowserTest() {
+    if (IsBfcacheEnabled()) {
+      bfcache_feature_list_.InitWithFeaturesAndParameters(
+          content::GetDefaultEnabledBackForwardCacheFeaturesForTesting(),
+          content::GetDefaultDisabledBackForwardCacheFeaturesForTesting());
+    } else {
+      bfcache_feature_list_.InitWithFeatures(
+          /*enabled_features=*/{},
+          /*disabled_features=*/{features::kBackForwardCache});
+    }
+  }
+
   bool IsBfcacheEnabled() const { return GetParam(); }
 
   void SetUpOnMainThread() override {
@@ -550,6 +563,9 @@ class NavigationInitiatorPageLoadMetricsBFCacheBrowserTest
               TEST_REQUIRES_NO_CACHING);
     }
   }
+
+ private:
+  base::test::ScopedFeatureList bfcache_feature_list_;
 };
 
 INSTANTIATE_TEST_SUITE_P(All,
