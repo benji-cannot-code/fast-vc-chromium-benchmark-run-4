930ee87e9dd9227353d7c031cec972b6ea64aa58
Eric Ekey <ericekey@google.com>
2026-08-14 14:12:58 -0700

    [iOS][NTPRedesign] Refactor MVT Container
    This change removes the dependency on MagicStackModuleContainer for the
    standalone Most Visited Tiles presentation in the NTP Redesign.
    MagicStackModuleContainer is not needed for presentation since MVTs are
    not in the Magic Stack.
    
    Bug: None
    Change-Id: I3c803475b085dcc3d283b7fbe3f5480995c99a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257536
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679904}

--- ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_tiles_collection_view.h
+++ ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_tiles_collection_view.h
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// update it.
 @interface MostVisitedTilesCollectionView : UICollectionView
 
+/// Callback invoked when the collection view's content size changes.
+@property(nonatomic, copy) void (^onContentSizeChanged)(CGSize newSize);
+
 /// Initializes the collection view with `config` and `spacing` between the
 /// cells.
 - (instancetype)initWithConfig:(MostVisitedTilesConfig*)config
--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_redesign_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_redesign_view_controller.mm
@@ -8,9 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_redesign_view_controller.h"
 
 #import "components/strings/grit/components_strings.h"
-#import "ios/chrome/browser/content_suggestions/magic_stack/ui/magic_stack_module_container.h"
+#import "ios/chrome/browser/content_suggestions/magic_stack/ui/magic_stack_module_background_view.h"
 #import "ios/chrome/browser/content_suggestions/model/content_suggestions_metrics_recorder.h"
 #import "ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_item.h"
+#import "ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_tiles_collection_view.h"
 #import "ios/chrome/browser/content_suggestions/most_visited_tiles/ui/most_visited_tiles_config.h"
 #import "ios/chrome/browser/content_suggestions/ui/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/home_customization/ui/home_customization_framing_coordinates.h"
@@ -53,6 +54,13 @@ constexpr CGFloat kExpandedSheetOmniboxTopMargin = 16.0;
 // Spacing from the top of the bottom sheet to the MVTs container when
 // resting/collapsed.
 constexpr CGFloat kRestingSheetMVTTopMargin = 12.0;
+
+// Bottom padding between the MVT collection view and the bottom of its
+// container.
+constexpr CGFloat kMVTContainerBottomPadding = 10.0;
+
+// Corner radius for the MVT container when rendered with squircle styling.
+constexpr CGFloat kMVTContainerCornerRadius = 24.0;
 
 // Top margin of the Google logo view.
 constexpr CGFloat kLogoTopMargin = 40.0;
@@ -550,11 +558,21 @@ const CGFloat kMinDragHandleHeight = 24.0;
     _mostVisitedView = nil;
     return;
   }
-  MagicStackModuleContainer* container =
-      [[MagicStackModuleContainer alloc] initWithFrame:CGRectZero noInset:YES];
-  [container configureWithConfig:config];
 
-  _mostVisitedView = container;
+  MostVisitedTilesCollectionView* collectionView =
+      [[MostVisitedTilesCollectionView alloc] initWithConfig:config];
+
+  if (!IsMVTInBottomSheetEnabled()) {
+    __weak __typeof(_bottomSheetViewController) weakBottomSheetViewController =
+        _bottomSheetViewController;
+    collectionView.onContentSizeChanged = ^(CGSize) {
+      [weakBottomSheetViewController updateBottomSheetPositionAnimated:YES];
+    };
+  }
+
+  _mostVisitedView =
+      [self createContainerForMostVisitedCollectionView:collectionView
+                                             inSquircle:YES];
 
   if (IsMVTInBottomSheetEnabled()) {
     if (_bottomSheetViewController) {
@@ -665,6 +683,40 @@ const CGFloat kMinDragHandleHeight = 24.0;
 }
 
 #pragma mark - Private
+
+// Creates a container view that wraps `collectionView` with bottom padding and
+// optional squircle background styling.
+- (UIView*)createContainerForMostVisitedCollectionView:
+               (MostVisitedTilesCollectionView*)collectionView
+                                            inSquircle:(BOOL)inSquircle {
+  UIView* container = [[UIView alloc] init];
+  container.translatesAutoresizingMaskIntoConstraints = NO;
+
+  if (inSquircle) {
+    UIView* backgroundView = [[MagicStackModuleBackgroundView alloc] init];
+    backgroundView.translatesAutoresizingMaskIntoConstraints = NO;
+    [container addSubview:backgroundView];
+    AddSameConstraints(container, backgroundView);
+    container.layer.cornerRadius = kMVTContainerCornerRadius;
+    container.clipsToBounds = YES;
+  }
+
+  collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+  [container addSubview:collectionView];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [collectionView.topAnchor constraintEqualToAnchor:container.topAnchor],
+    [collectionView.leadingAnchor
+        constraintEqualToAnchor:container.leadingAnchor],
+    [collectionView.trailingAnchor
+        constraintEqualToAnchor:container.trailingAnchor],
+    [collectionView.bottomAnchor
+        constraintEqualToAnchor:container.bottomAnchor
+                       constant:-kMVTContainerBottomPadding],
+  ]];
+
+  return container;
+}
 
 // Add _mostVisitedView to the view hierarchy.
 - (void)embedMostVisitedView {
