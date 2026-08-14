6aa24c275f8b6dd721427f14b8bea3a85ec33cef
Anh Mac <anhmac@chromium.org>
2026-08-13 19:41:43 -0700

    [Extensions] Implement Unsplit() in TabListInterface
    This CL implements the Unsplit method in TabListInterface using
    TabStripModel::RemoveSplit, allowing a split tab view to be unsplit
    using its SplitTabId. Stub implementations are added for Android's
    TabModelJniBridge and related test helpers.
    
    Bug: 545251093,456257896
    Change-Id: Id8827ce54005d8331be0205c866ea80667e16e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242652
    Commit-Queue: Anh Mac <anhmac@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679300}

--- chrome/browser/ui/android/tab_model/tab_model_test_helper.h
+++ chrome/browser/ui/android/tab_model/tab_model_test_helper.h
@@ -130,6 +130,7 @@ class TestTabModel : public TabModel {
       std::optional<tab_groups::TabGroupId> group_id,
       const std::set<tabs::TabHandle>& tabs) override;
   void Ungroup(const std::set<tabs::TabHandle>& tabs) override;
+  void Unsplit(split_tabs::SplitTabId split_id) override;
   void MoveGroupTo(tab_groups::TabGroupId group_id, int index) override;
   void MoveTabToWindow(tabs::TabHandle tab,
                        SessionID destination_window_id,
@@ -277,6 +278,7 @@ class OwningTestTabModel : public TabModel {
       std::optional<tab_groups::TabGroupId> group_id,
       const std::set<tabs::TabHandle>& tabs) override;
   void Ungroup(const std::set<tabs::TabHandle>& tabs) override;
+  void Unsplit(split_tabs::SplitTabId split_id) override;
   void MoveGroupTo(tab_groups::TabGroupId group_id, int index) override;
   void MoveTabToWindow(tabs::TabHandle tab,
                        SessionID destination_window_id,
