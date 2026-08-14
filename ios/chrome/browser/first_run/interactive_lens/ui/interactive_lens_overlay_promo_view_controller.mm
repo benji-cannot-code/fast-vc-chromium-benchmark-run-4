c46a1a720c79a5a1c2d266c573f748ed171024d5
Cheick Cisse <cheickcisse@google.com>
2026-08-14 07:35:46 -0700

    [iOS] Update interactive Lens overlay promo subtitle
    This CL updates the interactive Lens overlay promo subtitle from
    "You can now open Google Lens from your address bar and search anything
    you see" to "You can use Google Lens in Chrome to search anything
    you see".
    
    Also moving the string to branded because the new string includes
    "Chrome".
    
    Screenshot:
    https://screenshot-v2.corp.google.com/image/4fpv00ngim1a0.png
    
    Change-Id: I8cdfd712edc05fa6f3ba281f36d22be5bf8ff785
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257838
    Auto-Submit: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679524}

--- ios/chrome/browser/first_run/interactive_lens/ui/interactive_lens_overlay_promo_view_controller.mm
+++ ios/chrome/browser/first_run/interactive_lens/ui/interactive_lens_overlay_promo_view_controller.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/util/chrome_button.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_api.h"
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h"
