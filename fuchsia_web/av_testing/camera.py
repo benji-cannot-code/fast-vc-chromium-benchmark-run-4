dd097df750a2b5909adf7cbdcd90233eae50433b
Alex Ovsienko <ovsienko@google.com>
2026-08-13 19:06:51 -0700

    Ruff: reformat fuchsia_web/av_testing.
    Preserve yapf style to the best capacity. Minding previous experience, reformat as Py3.8 as the team is mostly using this version.
    
    LSC doc:
    https://docs.google.com/document/d/1P6AE9aeKuFkxPAYInYxLFIpDgpiCAT-S_BQkNU0lb3s/edit?tab=t.0
    
    Onboarded using the following command: `~/cr/depot_tools/ruff_chromium format .`
    
    Bug: 40874143
    Change-Id: Idb35fd46cdb0a22642ec3a85391ac8c9fea80e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256113
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Owners-Override: Alex Ovsienko <ovsienko@google.com>
    Commit-Queue: Alex Ovsienko <ovsienko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679283}

--- fuchsia_web/av_testing/camera.py
+++ fuchsia_web/av_testing/camera.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-""" Starts an av_sync_record process to record the video from the camera. """
+"""Starts an av_sync_record process to record the video from the camera."""
 
 import logging
 import os.path
@@ -43,9 +43,11 @@ def start(parameters: Parameters) -> None:
     BINARY = '/usr/local/cipd/av_sync_record/av_sync_record'
     assert os.path.isfile(BINARY)
     args = [
-        BINARY, '--gid=', '--uid=',
+        BINARY,
+        '--gid=',
+        '--uid=',
         f'--camera_info_path={parameters.info_file}',
-        f'--video_output={parameters.video_file}'
+        f'--video_output={parameters.video_file}',
     ]
     if parameters.fps:
         args.append(f'--camera_fps={parameters.fps}')
--- fuchsia_web/av_testing/server.py
+++ fuchsia_web/av_testing/server.py
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-""" Starts an http server locally and serves the files in the folder of this
-    python file. It needs an integer parameter as the port."""
+"""Starts an http server locally and serves the files in the folder of this
+python file. It needs an integer parameter as the port."""
 
 import http.server
 import logging
@@ -27,7 +27,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def translate_path(self, path):
         prefix = '/videos/'
         if path.startswith(prefix):
-            return os.path.join(VIDEO_DIR, path[len(prefix):])
+            return os.path.join(VIDEO_DIR, path[len(prefix) :])
         return http.server.SimpleHTTPRequestHandler.translate_path(self, path)
 
 
@@ -35,6 +35,7 @@ def start(port: int) -> None:
     with ThreadedHTTPServer(("", port), Handler) as httpd:
         root_dir = os.path.dirname(__file__)
         os.chdir(root_dir)
-        logging.warning('Http server is running on port %s at %s', port,
-                        root_dir)
+        logging.warning(
+            'Http server is running on port %s at %s', port, root_dir
+        )
         httpd.serve_forever()
