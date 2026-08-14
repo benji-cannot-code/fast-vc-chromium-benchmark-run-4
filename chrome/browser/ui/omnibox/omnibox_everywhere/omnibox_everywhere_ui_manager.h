c2c85fd325b0b1e30d9bbc0738bb10bd56de5d9e
Kaan Alsan <alsan@chromium.org>
2026-08-14 11:18:30 -0700

    [OmniboxEverywhere] Fix flashing when showing OE on Windows
    Previously, Omnibox Everywhere had visual flashing due to background
    transparency setup and default Aura window show animations. This CL
    resolves the issue by configuring WebContents transparency and disabling
    Aura visibility transitions.
    
    Change-Id: Ib9bb19b2502bda430dc04da8655ec52f37e17da2
    Bug: b:532195081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258913
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679713}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.h
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.h
@@ -49,6 +49,9 @@ class OmniboxEverywhereUIManager : public views::WidgetObserver,
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kOmniboxEverywhereElementId);
 
+  static constexpr int kPopupFixedWidth = 848;
+  static constexpr int kDefaultRestingHeight = 152;
+
   enum ContextMenuCommandId {
     kCut = 1,
     kCopy = 2,
@@ -170,6 +173,7 @@ class OmniboxEverywhereUIManager : public views::WidgetObserver,
   void CreateAndInitWidget(gfx::NativeWindow context);
   void ActivateAndFocus();
   void OnEphemeralModelPrefChanged();
+  static gfx::Rect CalculateWidgetBounds(int height);
 
   std::unique_ptr<WebUIContentsWrapper> CreateContentsWrapper(Profile* profile);
 
