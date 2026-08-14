32eb96a4b7432aaeed4fc485d72e716239c67bc7
Xi Han <hanxi@google.com>
2026-08-14 07:02:57 -0700

    [Aurora] Change toolbar side padding on NTP when pinned without focusing.
    In this CL:
    1. Change side padding from 16dp to 8dp when the toolbar is pinned at
       top on NTP;
    2. Fix the mic and lens button visibility issue after focusing and
       unfocusing the search box at top due to the padding change. This is
       because the padding change leads to no width change of the toolbar
       when pinned at top, thus
       #layoutLocationBarWithoutAnimationExpansion() returns false even
       when the focus changed on toolbar. To fix it, we add a variable
       mLastUrlFocusOnNtp to track if the focus has been changed.
    3. Remove signin button's animation on NTP to prevent a sliding in
       animation after focusing and unfocusing search box on NTP.
    4. Remove expanding animation on NTP since there isn't any button
       location changes between focusing and unfocusing state.
    Demo: http://shortn/_6oRVccBJBY.
    
    Bug: 534357676
    Change-Id: I12624cef5eb2401596ab03a7009522bb2b28b1af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258049
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679505}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -202,6 +202,7 @@ public class ToolbarPhone extends ToolbarLayout
 
     protected boolean mDisableLocationBarRelayout;
     protected boolean mLayoutLocationBarInFocusedMode;
+    private boolean mLastUrlFocusOnNtp;
     private boolean mLayoutLocationBarWithoutExtraButton;
     private boolean mOptionalButtonShowTransitionRunning;
     private int mOptionalButtonTransitionWidthDelta;
@@ -263,6 +264,7 @@ public class ToolbarPhone extends ToolbarLayout
 
     private final int mToolbarSidePadding;
     private final int mToolbarSidePaddingForNtp;
+    private final boolean mIsAuroraEnabled;
     private final int mBackgroundHeightIncreaseWhenFocus;
     private int mTopPaddingForEdgeToEdgeNtp;
 
@@ -356,6 +358,7 @@ public class ToolbarPhone extends ToolbarLayout
         super(context, attrs);
         mToolbarSidePadding = OmniboxResourceProvider.getToolbarSidePadding(context);
         mToolbarSidePaddingForNtp = OmniboxResourceProvider.getToolbarSidePaddingForNtp(context);
+        mIsAuroraEnabled = NewTabPageUtils.isNtpAuroraButtonColorEnabled();
         mBackgroundHeightIncreaseWhenFocus =
                 OmniboxResourceProvider.getLocationBarBackgroundOnFocusHeightIncrease(context);
         mToolbarBackgroundColorForNtp =
@@ -775,6 +778,15 @@ public class ToolbarPhone extends ToolbarLayout
         if (mLayoutLocationBarInFocusedMode
                 || (mVisualState == VisualState.NEW_TAB_NORMAL
                         && mTabSwitcherState == STATIC_TAB)) {
+            if (mIsAuroraEnabled
+                    && mVisualState == VisualState.NEW_TAB_NORMAL
+                    && mTabSwitcherState == STATIC_TAB) {
+                boolean currentUrlFocus = urlHasFocus();
+                if (mLastUrlFocusOnNtp != currentUrlFocus) {
+                    changed = true;
+                    mLastUrlFocusOnNtp = currentUrlFocus;
+                }
+            }
             int priorVisibleWidth =
                     mLocationBar.getPhoneCoordinator().getOffsetOfFirstVisibleFocusedView();
             width =
@@ -2057,6 +2069,7 @@ public class ToolbarPhone extends ToolbarLayout
         if (isInTabSwitcherMode()
                 || mUrlFocusChangeInProgress
                 || urlHasFocus()
+                || isLocationBarShownInNtp()
                 || getToolbarDataProvider()
                         .getNewTabPageDelegate()
                         .transitioningAwayFromLocationBar()) {
@@ -2296,7 +2309,10 @@ public class ToolbarPhone extends ToolbarLayout
     private void populateUrlExpansionAnimatorSet(List<Animator> animators) {
         TraceEvent.begin("ToolbarPhone.populateUrlFocusingAnimatorSet");
         Animator animator = ObjectAnimator.ofFloat(this, mUrlFocusChangeFractionProperty, 1f);
-        animator.setDuration(URL_FOCUS_CHANGE_ANIMATION_DURATION_MS);
+        animator.setDuration(
+                mIsAuroraEnabled && isLocationBarShownInNtp()
+                        ? 0
+                        : URL_FOCUS_CHANGE_ANIMATION_DURATION_MS);
         animator.setInterpolator(Interpolators.FAST_OUT_SLOW_IN_INTERPOLATOR);
         animators.add(animator);
 
@@ -3249,6 +3265,11 @@ public class ToolbarPhone extends ToolbarLayout
             mHomeButton.setAccessibilityTraversalBefore(R.id.toolbar_buttons);
         } else {
             mHomeButton.setAccessibilityTraversalBefore(View.NO_ID);
+        }
+
+        if (mIsAuroraEnabled && mVisualState != VisualState.NEW_TAB_NORMAL) {
+            // Resets this flag once NTP isn't shown.
+            mLastUrlFocusOnNtp = false;
         }
 
         // If we are navigating to or from a brand color, allow the transition animation
