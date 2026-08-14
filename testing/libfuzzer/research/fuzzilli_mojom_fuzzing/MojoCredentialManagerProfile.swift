ee23316654438afd873f3e8fa06f53c0d651a6b4
Tigran Bantikyan <bantikyan@google.com>
2026-08-14 14:07:21 -0700

    fuzzing: Disable certain Fuzzilli mutators in experimental profiles
    This CL disables the ExplorationMutator, ProbingMutator, and
    PropertyAccessorMutator mutators. These mutators target JavaScript
    behavior and generic objects (e.g. loading builtin properties,
    getter/setter side effects), which is not relevant for fuzzing Mojo
    renderer-exposed IPC interfaces. Disabling them eliminates the
    generation of arcane JavaScript and focuses fuzzing on valid Mojo API
    calls.
    
    Bug: 535158017
    Change-Id: If2379c8ab1ce493bc05cd6848310f45a2e548f72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258220
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Tigran Bantikyan <bantikyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679895}

--- testing/libfuzzer/research/fuzzilli_mojom_fuzzing/MojoCredentialManagerProfile.swift
+++ testing/libfuzzer/research/fuzzilli_mojom_fuzzing/MojoCredentialManagerProfile.swift
@@ -235,7 +235,9 @@ let mojoCredentialManagerProfile = Profile(
     ] + commonMojoCodeGenerators,
     additionalProgramTemplates: WeightedList([]),
     disabledCodeGenerators: mojoDisabledGenerators,
-    disabledMutators: v8Profile.disabledMutators,
+    disabledMutators: [
+        "ExplorationMutator", "ProbingMutator", "PropertyAccessorMutator"
+    ],
     additionalBuiltins: mojoBuiltins.merging(commonMojoBuiltins) { (existing, _) in existing },
     additionalObjectGroups: [
         .credentialManager,
