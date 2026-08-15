627befef5949c1e93dd8400c431ded04aba169cc
Lauren Winston <lwinston@google.com>
2026-08-14 18:14:44 -0700

    [Reading mode] Add shortcut to ChromeOS shortcuts app
    Fixed: 494653065
    Change-Id: I81973357b548c3ab4df5c2dfc60202130d4ee1b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256483
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680071}

--- ash/webui/shortcut_customization_ui/backend/accelerator_layout_table.h
+++ ash/webui/shortcut_customization_ui/backend/accelerator_layout_table.h
@@ -114,6 +114,7 @@ enum NonConfigurableActions {
   kBrowserPageUp,
   kBrowserPageDown,
   kBrowserNewSplitView,
+  kBrowserShowReadingMode,
   // Ambient action ids:
   kAmbientDragLinkInSameTab,
   kAmbientCycleForwardMRU,
@@ -498,6 +499,7 @@ inline constexpr uint32_t kAcceleratorLayouts[] = {
     AcceleratorAction::kToggleFullscreenMagnifier,
     AcceleratorAction::kMagnifierZoomIn,
     AcceleratorAction::kMagnifierZoomOut,
+    NonConfigurableActions::kBrowserShowReadingMode,
 
     // Accessibility > Accessbility navigation
     AcceleratorAction::kAccessibilityAction,
