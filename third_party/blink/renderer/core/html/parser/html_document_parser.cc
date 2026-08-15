b76f3f2a06f7d88cf58e266ad1460f03372537b9
Noam Rosenthal <nrosenthal@chromium.org>
2026-08-15 01:15:54 -0700

    Fix chunked parser-blocking scripts in stream*HTMLUnsafe
    The state machine had a DCHECK for parser-blocking in the middle of the
    script, assuming we were not in the fragment parser.
    
    The DCHECK should specifically allow the parser policy used for
    streamHTMLUnsafe with runScripts.
    
    Bug: 545823947
    Change-Id: If4412d8e781db244349bb4d87184536bf025a85b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257431
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680158}

--- third_party/blink/renderer/core/html/parser/html_document_parser.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser.cc
@@ -1163,7 +1163,9 @@ TextPosition HTMLDocumentParser::GetTextPosition() const {
 }
 
 bool HTMLDocumentParser::IsWaitingForScripts() const {
-  if (IsParsingFragment()) {
+  if (IsParsingFragment() &&
+      GetParserContentPolicy() !=
+          kAllowScriptingContentAndMarkAsParserInserted) {
     // HTMLTreeBuilder may have a parser blocking script element, but we
     // ignore it during fragment parsing.
     DCHECK(!(tree_builder_->HasParserBlockingScript() ||
@@ -1171,6 +1173,12 @@ bool HTMLDocumentParser::IsWaitingForScripts() const {
              reentry_permit_->ParserPauseFlag()));
     return false;
   }
+
+  // kAllowScriptingContentAndMarkAsParserInserted is only used by HTML
+  // streaming, which is the only case where the fragment parser can have a
+  // parser blocking script.
+  CHECK(!IsParsingFragment() ||
+        RuntimeEnabledFeatures::NewHTMLSettingMethodsEnabled());
 
   // When the TreeBuilder encounters a </script> tag, it returns to the
   // HTMLDocumentParser where the script is transfered from the treebuilder to
