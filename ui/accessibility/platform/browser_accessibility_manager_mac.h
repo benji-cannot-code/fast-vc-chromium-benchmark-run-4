2e4f105c8bbbdad469b1cf0010495a8c959a0b43
Valerie Young <spectranaut@igalia.com>
2026-08-14 18:31:43 -0700

    [mac-a11y] Use AXTextStateChangeTypeEdit for character inputs
    This change alone fixes the reported bug by setting the correct
    value for the userInfo key NSAccessibilityTextStateChangeTypeKey.
    
    The placeholder text is still occasionally voiced by VoiceOver
    on an input with no accessible name in a manner consistent with
    VoiceOver and Safari.
    
    Bug: 512582992
    Change-Id: I96c2ff92f5be0f384b4a094c5043542b3deb5a45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252377
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1680085}

--- ui/accessibility/platform/browser_accessibility_manager_mac.h
+++ ui/accessibility/platform/browser_accessibility_manager_mac.h
@@ -104,6 +104,8 @@ class COMPONENT_EXPORT(AX_PLATFORM) BrowserAccessibilityManagerMac
   friend class BrowserAccessibilityManager;
 
   friend class content::BrowserAccessibilityCocoaBrowserTest;
+
+  friend class BrowserAccessibilityMacTest;
 };
 
 }  // namespace ui
