ed408aa5f3dfcc8410035557fbebe0f3494eeb45
David Bienvenu <davidbienvenu@chromium.org>
2026-08-15 14:53:49 -0700

    Fix flakes in IOSPromosUtilsTests
    IOSPromo tests leak a global singleton between tests that breaks a
    test that runs with kEnableCrossDevicePrefTracker disabled (the default)
    followed by a test with it enabled. In this case, when
    CrossDevicePrefTrackerFactory::GetInstance() is called with the feature
    disabled, ProfileSelections::BuildNoProfilesSelected() is passed to
    ProfileKeyedServiceFactory and permanently baked into the singleton
    instance for the remainder of the process.
    
    Even though a later test enables the feature, the singleton is not
    recreated, so the test fails because BuildNoProfilesSelected() is stil
    in effect.
    
    This CL fixes this by using ScopedProfileSelectionsForFactoryTesting,
    which overrides the existing factory ProfileSelections during the test,
    with the desired ProfilesSelections for when
    kEnableCrossDevicePrefTracker feature is enabled.
    
    Bug: 546850466
    Change-Id: I3ad81f53c07a64cbdbfd82cdd7cbfe914071fee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254302
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680211}

--- chrome/browser/ui/desktop_to_mobile_promos/ios_promos_utils_unittest.cc
+++ chrome/browser/ui/desktop_to_mobile_promos/ios_promos_utils_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/values_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
+#include "chrome/browser/profiles/profile_test_util.h"
 #include "chrome/browser/sync/device_info_sync_service_factory.h"
 #include "chrome/browser/sync/prefs/cross_device_pref_tracker/cross_device_pref_tracker_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
@@ -46,6 +47,13 @@ class IOSPromosUtilsTest : public testing::Test {
   IOSPromosUtilsTest() {
     feature_list_.InitAndEnableFeature(
         sync_preferences::features::kEnableCrossDevicePrefTracker);
+    scoped_profile_selections_ = std::make_unique<
+        profiles::testing::ScopedProfileSelectionsForFactoryTesting>(
+        CrossDevicePrefTrackerFactory::GetInstance(),
+        ProfileSelections::Builder()
+            .WithRegular(ProfileSelection::kOriginalOnly)
+            .WithAshInternals(ProfileSelection::kNone)
+            .Build());
   }
   ~IOSPromosUtilsTest() override = default;
 
@@ -96,6 +104,8 @@ class IOSPromosUtilsTest : public testing::Test {
  private:
   content::BrowserTaskEnvironment task_environment_;
   base::test::ScopedFeatureList feature_list_;
+  std::unique_ptr<profiles::testing::ScopedProfileSelectionsForFactoryTesting>
+      scoped_profile_selections_;
   std::unique_ptr<TestingProfile> profile_;
 };
 
