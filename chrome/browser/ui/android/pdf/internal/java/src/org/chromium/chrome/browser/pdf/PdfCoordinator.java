eef42fd91b7b1890934a653b9f56b466175c43d9
Sirisha Kavuluru <skavuluru@google.com>
2026-08-16 16:43:03 -0700

    [PDF] Route Zoom key events via KeyboardShortcut handler instead of onKey on view.
    Context: Page zoom shortcuts do not work on PDF when you switch tabs and then go back to PDF. Zoom shortcuts are currently handled via onKey() override on PDFView and this event is only triggered when view is in focus, which does not happen on tab switch.
    Fix: Handling zoom via KeyboardShortcuts via same entry point as other web pages. This is invoked at activity level and calls changeZoom on PDFPage.
    Bug: 539609112
    Change-Id: Ie5e7f4c8e0756cce0c9753ba7513e643ce277328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263946
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Auto-Submit: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680301}

--- chrome/browser/ui/android/pdf/internal/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
+++ chrome/browser/ui/android/pdf/internal/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
@@ -1393,6 +1393,21 @@ public class PdfCoordinator
     }
 
     /**
+     * Changes the zoom level of the PDF page.
+     *
+     * @param decrease Whether to decrease the zoom level.
+     * @return True if the PDF page can be zoomed out, false otherwise.
+     */
+    @Override
+    public boolean changeZoomLevel(boolean decrease) {
+        if (mToolbarCoordinator == null) return false;
+        Float nextZoomLevel = mToolbarCoordinator.getNextEngineZoomLevel(/* increase= */ !decrease);
+        if (nextZoomLevel == null) return false;
+        changeZoomLevel(nextZoomLevel);
+        return true;
+    }
+
+    /**
      * Sets the zoom level to a specified amount.
      *
      * @param zoomLevel The new value of the zoom.
@@ -1462,7 +1477,6 @@ public class PdfCoordinator
         if (mToolbarCoordinator != null) {
             pdfView.setFocusable(true);
             pdfView.setFocusableInTouchMode(true);
-            pdfView.setOnKeyListener(mToolbarCoordinator);
         }
     }
 
