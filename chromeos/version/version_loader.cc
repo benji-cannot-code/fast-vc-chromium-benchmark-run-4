38681e948f317aae4c09ba31fb902170ac5f188a
Danilo Tedeschi <danft@google.com>
2026-08-17 16:44:53 -0700

    IcuBridge migration: chromeos/version/
    Migrate chromeos/version/version_loader.cc to use base::Time::Exploded.
    
    Bug: 534455293
    Change-Id: I6c283ff56b1cb6c08163e4a5773bf98ea28e1670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259623
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680916}

--- chromeos/version/version_loader.cc
+++ chromeos/version/version_loader.cc
@@ -14,17 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
-#include "base/i18n/time_formatting.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/system/sys_info.h"
 #include "base/time/time.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
-#include "third_party/icu/source/i18n/unicode/timezone.h"
 
 namespace chromeos {
 namespace version_loader {
@@ -61,9 +60,10 @@ std::optional<std::string> GetVersion(VersionFormat format) {
     return std::nullopt;
   }
   if (format == VERSION_SHORT_WITH_DATE) {
-    version += base::UnlocalizedTimeFormatWithPattern(
-        base::SysInfo::GetLsbReleaseTime(), "-yy.MM.dd",
-        icu::TimeZone::getGMT());
+    base::Time::Exploded exploded;
+    base::SysInfo::GetLsbReleaseTime().UTCExplode(&exploded);
+    version += base::StringPrintf("-%02d.%02d.%02d", exploded.year % 100,
+                                  exploded.month, exploded.day_of_month);
   }
 
   return version;
