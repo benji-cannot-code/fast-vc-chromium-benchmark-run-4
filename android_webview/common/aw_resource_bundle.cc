d118dca98f6a771d8d68cb4879490882dced5fc6
Abhijith Nair <abhijithnair@chromium.org>
2026-08-17 12:07:04 -0700

    Remove obsolete PathService overrides from WebViewChromiumAwInit
    This change removes the PathService overrides for DIR_MODULE and
    DIR_RESOURCE_PAKS_ANDROID, as they are no longer needed.
    
    Bug: 400414092
    Change-Id: I9fde52d3dd3cd8093f59715580455d34d6afcf8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8246100
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680683}

--- android_webview/common/aw_resource_bundle.cc
+++ android_webview/common/aw_resource_bundle.cc
@@ -14,12 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/language_tag.h"
 #include "base/i18n/tag_converters.h"
 #include "base/logging.h"
-#include "base/path_service.h"
 #include "base/posix/global_descriptors.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/resource/resource_bundle_android.h"
-#include "ui/base/ui_base_paths.h"
 
 namespace android_webview {
 
@@ -42,12 +40,8 @@ void InitIcuAndResourceBundleBrowserSide() {
   SetDefaultIcuLocale(base::i18n::DefaultIcuLocaleSetterKey(),
                       locale_tag.value_or(GetKnownLanguageTag("en-US")));
 
-  // Try to directly mmap the resources.pak from the apk. Fall back to load
-  // from file, using PATH_SERVICE, otherwise.
-  base::FilePath pak_file_path;
-  base::PathService::Get(ui::DIR_RESOURCE_PAKS_ANDROID, &pak_file_path);
-  pak_file_path = pak_file_path.AppendASCII("resources.pak");
-  ui::LoadMainAndroidPackFile("assets/resources.pak", pak_file_path);
+  // We only load the resources.pak from the apk.
+  ui::LoadMainAndroidPackFile("assets/resources.pak", base::FilePath());
 }
 
 void InitResourceBundleRendererSide() {
