affc1ea49bac0077b3953adfdb9836fb2f8c5fce
mark a. foltz <mfoltz@chromium.org>
2026-08-17 13:57:39 -0700

    [Actor] Add ActorActionsRunner to execute actions in a transient task.
    This object encapsulates details of interacting with the
    ActorKeyedService and ActorTasks, for clients that just want to run a
    set of actions.
    
    Bug: 545676925
    Change-Id: Id922f50f7b6fd5854fab61c6f87f361028186612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8251117
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680768}

---
+++ chrome/browser/actor/enterprise_policy_checker.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/actor/enterprise_policy_checker.h"
+
+#include <utility>
+
+#include "base/no_destructor.h"
+
+namespace actor {
+
+NullEnterprisePolicyChecker::NullEnterprisePolicyChecker() = default;
+NullEnterprisePolicyChecker::~NullEnterprisePolicyChecker() = default;
+
+EnterprisePolicyChecker::UrlBlockReason NullEnterprisePolicyChecker::Evaluate(
+    const GURL& url) const {
+  return UrlBlockReason::kNotBlocked;
+}
+
+void NullEnterprisePolicyChecker::ValidateContentSentToRenderer(
+    content::RenderFrameHost* frame,
+    const std::string& content,
+    ContentValidationCallback callback) const {
+  std::move(callback).Run(ContentValidationReason::kAllowed);
+}
+
+const EnterprisePolicyChecker* GetNullEnterprisePolicyChecker() {
+  static base::NoDestructor<NullEnterprisePolicyChecker> checker;
+  return checker.get();
+}
+
+}  // namespace actor
