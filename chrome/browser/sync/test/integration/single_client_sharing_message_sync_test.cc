7e2fe8fe201f5da9b7f01fd2d31af524e82b8ae8
jood <jood@google.com>
2026-08-17 04:20:05 -0700

    [sync] Remove waiting for SharingMessage for all tests
    Waiting for `SHARING_MESSAGE` in `WaitForAsyncChangesToBeCommitted` adds
    unnecessary overhead and slows down every test suite execution.
    
    This CL removes the global wait logic and the custom test overrides for
    this datatype entirely. Tests that specifically require verification of
    `SHARING_MESSAGE` now do so locally in the test setup.
    
    Bug: 501729852
    Change-Id: I838879597843d68176d83c470d744156f516c553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8251380
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680457}

--- chrome/browser/sync/test/integration/single_client_sharing_message_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_sharing_message_sync_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/sharing/sharing_message_bridge_factory.h"
+#include "chrome/browser/sync/test/integration/device_info_helper.h"
 #include "chrome/browser/sync/test/integration/single_client_status_change_checker.h"
 #include "chrome/browser/sync/test/integration/sync_service_impl_harness.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
@@ -169,6 +170,16 @@ class SingleClientSharingMessageSyncTest
   }
   ~SingleClientSharingMessageSyncTest() override = default;
 
+  [[nodiscard]] bool SetupSync() {
+    if (!SyncTest::SetupSync()) {
+      return false;
+    }
+
+    // Wait for committing DeviceInfo with sharing_fields, it may happen
+    // asynchronously due to FCM token registration.
+    return device_info_helper::WaitForFullDeviceInfoCommitted(GetCacheGuid(0));
+  }
+
   bool WaitForSharingMessage(
       std::vector<SharingMessageSpecifics> expected_specifics) {
     return SharingMessageEqualityChecker(GetSyncService(0), GetFakeServer(),
@@ -179,8 +190,6 @@ class SingleClientSharingMessageSyncTest
   SyncTest::SetupSyncMode GetSetupSyncMode() const override {
     return GetParam();
   }
-
-  bool TestReliesOnSharingMessage() const override { return true; }
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/sync/test/integration/committed_all_nudged_changes_checker.h"
-#include "chrome/browser/sync/test/integration/device_info_helper.h"
 #include "chrome/browser/sync/test/integration/fake_sync_gcm_driver_for_instance_id.h"
 #include "chrome/browser/sync/test/integration/session_hierarchy_match_checker.h"
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
@@ -555,11 +554,6 @@ syncer::UserSelectableTypeSet SyncTest::GetRegisteredSelectableTypes(
 
 SyncTest::SetupSyncMode SyncTest::GetSetupSyncMode() const {
   return SetupSyncMode::kSyncTheFeature;
-}
-
-bool SyncTest::TestReliesOnSharingMessage() const {
-  // Temporarily fix on macOS (crbug.com/501729852).
-  return !BUILDFLAG(IS_MAC);
 }
 
 GURL SyncTest::GetInitialURL() const {
@@ -1196,17 +1190,6 @@ bool SyncTest::WaitForAsyncChangesToBeCommitted(size_t profile_index) const {
   // CommittedAllNudgedChangesChecker will wait for all the local changes to be
   // committed, it doesn't cover all the cases.
   if (server_type_ != EXTERNAL_LIVE_SERVER) {
-    // Wait for committing DeviceInfo with sharing_fields, it may happen
-    // asynchronously due to FCM token registration.
-    if (TestReliesOnSharingMessage() && GetSyncService(profile_index)
-                                            ->GetPreferredDataTypes()
-                                            .Has(syncer::SHARING_MESSAGE)) {
-      if (!device_info_helper::WaitForFullDeviceInfoCommitted(
-              GetCacheGuid(profile_index))) {
-        return false;
-      }
-    }
-
 #if BUILDFLAG(IS_ANDROID)
     // On Android, default about:blank page is loaded by default. Wait for
     // Session to be committed to prevent unexpected commit requests during
