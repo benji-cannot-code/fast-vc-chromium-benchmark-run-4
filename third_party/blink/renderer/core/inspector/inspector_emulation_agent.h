a90928c909a087f5b9f369e6a1178e74b6cdab97
Nikolaos Papaspyrou <nikolaos@chromium.org>
2026-08-17 04:18:09 -0700

    [DevTools] Support overriding CPU performance tier via CDP
    This CL adds backend support for overriding the CPU performance tier
    reported by navigator.cpuPerformance. This is the first part of the
    implementation for overriding the CPU performance tier and will be
    followed by DevTools frontend changes to expose this in the Sensors
    panel and integrate it with device emulation presets.
    
    Related CL in DevTools frontend: https://crrev.com/c/8162381
    
    Design doc: https://docs.google.com/document/d/1cVUQxigT9GMJweyR4c5Gmj16DEE7GnMTWy-FFPMkl14
    
    Bug: 449760252, 526856234, 526857820
    Test: third_party/blink/tools/run_web_tests.py inspector-protocol/emulation/device-emulation-cpu-performance.js
    
    TAG=agy
    CONV=20a48ccf-97dd-4982-9537-33af718bb55a
    
    Change-Id: Idc4a4e57408789436f0c8fbf4763de5ef2a9e169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085907
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Nikolaos Papaspyrou <nikolaos@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680454}

--- third_party/blink/renderer/core/inspector/inspector_emulation_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_emulation_agent.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
+#include "third_party/blink/public/mojom/cpu_performance.mojom-blink-forward.h"
 #include "third_party/blink/public/platform/web_theme_engine.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/html/parser/parser_synchronization_policy.h"
@@ -96,6 +97,8 @@ class CORE_EXPORT InspectorEmulationAgent final
       std::optional<bool> data_saver) override;
   protocol::Response setHardwareConcurrencyOverride(
       int hardware_concurrency) override;
+  protocol::Response setCPUPerformanceOverride(
+      std::optional<String> performance_tier) override;
   protocol::Response setUserAgentOverride(
       const String& user_agent,
       std::optional<String> accept_language,
@@ -117,6 +120,7 @@ class CORE_EXPORT InspectorEmulationAgent final
   void ApplyAcceptLanguageOverride(String* accept_lang);
   void ApplyDataSaverOverride(bool& data_saver);
   void ApplyHardwareConcurrencyOverride(unsigned int& hardware_concurrency);
+  void ApplyCPUPerformanceOverride(mojom::blink::PerformanceTier& tier);
   void ApplyUserAgentOverride(String* user_agent);
   void ApplyUserAgentMetadataOverride(
       std::optional<blink::UserAgentMetadata>* ua_metadata);
@@ -172,6 +176,7 @@ class CORE_EXPORT InspectorEmulationAgent final
   InspectorAgentState::Double emulated_os_text_scale_;
   InspectorAgentState::String navigator_platform_override_;
   InspectorAgentState::Integer hardware_concurrency_override_;
+  InspectorAgentState::String cpu_performance_override_;
   InspectorAgentState::Integer data_saver_override_;
   InspectorAgentState::String user_agent_override_;
   InspectorAgentState::Bytes serialized_ua_metadata_override_;
