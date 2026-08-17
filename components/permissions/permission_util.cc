148e0612b23afed184e8ee9ee0f515c24389df9d
James Leung <jamesleung@google.com>
2026-08-17 16:19:56 -0700

    [voice_search][pepc] Add check to prevent crashes
    * Add check that takes into account allowlisted surfaces before
      creating a permission prompt.
    * Add to that check to ensure that the flow prompt model is initialized
      before in `recreateView` to avoid a CHECK.
    * This also allows `PermissionRequestManagerTest.PEPCRequestNeverQuiet`
      to pass.
    
    Bug: b:546726612
    Change-Id: I9bfaf402ebe0316f488c9aba249b5da5bec1c3ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261205
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680903}

--- components/permissions/permission_util.cc
+++ components/permissions/permission_util.cc
@@ -401,6 +401,9 @@ bool PermissionUtil::IsLowPriorityPermissionRequest(
 bool PermissionUtil::ShouldCurrentRequestUsePermissionElementSecondaryUI(
     const PermissionPrompt::Delegate* delegate,
     content::WebContents* web_contents) {
+  if (delegate->Requests().empty()) {
+    return false;
+  }
   if (permissions::PermissionsClient::
           AllowEmbeddedPermissionPromptForAllowlistedSurfaces() &&
       permissions::PermissionsClient::Get()
@@ -414,6 +417,10 @@ bool PermissionUtil::ShouldCurrentRequestUsePermissionElementSecondaryUI(
 
 bool PermissionUtil::ShouldCurrentRequestUsePermissionElementSecondaryUI(
     const PermissionPrompt::Delegate* delegate) {
+  if (delegate->Requests().empty()) {
+    return false;
+  }
+
   if (!base::FeatureList::IsEnabled(blink::features::kGeolocationElement) &&
       !base::FeatureList::IsEnabled(blink::features::kUserMediaElement) &&
       !base::FeatureList::IsEnabled(blink::features::kWebAppInstallation)) {
