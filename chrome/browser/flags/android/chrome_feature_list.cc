7fda194a1bf135a950b51bd5e39ec76009962bf9
Eleanor Lee <eleanorlee@google.com>
2026-08-16 20:58:47 -0700

    [Community Contributions] Clean up EducationalTipDefaultBrowserPromoCard
    Bug: 343231383
    Change-Id: I3a64a8d72a98ee92ab940e2280950ead40628aa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253750
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680336}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -374,7 +374,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kEdgeToEdgeTablet,
     &kEdgeToEdgeUseBackupNavbarInsets,
     &kEdgelessTopInset,
-    &kEducationalTipDefaultBrowserPromoCard,
     &kEmailVerificationAndroid,
     &kEmptyTabListAnimationKillSwitch,
     &kEnableAndroidSidePanel,
@@ -782,7 +781,6 @@ BASE_FEATURE(kEdgeToEdgeMonitorConfigurations, base::FEATURE_ENABLED_BY_DEFAULT)
 BASE_FEATURE(kEdgeToEdgeTablet, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEdgeToEdgeUseBackupNavbarInsets, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEdgelessTopInset, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kEducationalTipDefaultBrowserPromoCard, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEmailVerificationAndroid, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEmptyTabListAnimationKillSwitch, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableAndroidSidePanel, base::FEATURE_DISABLED_BY_DEFAULT);
