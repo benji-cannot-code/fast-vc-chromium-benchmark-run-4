76e352287ac5ea79bac63527eb3fb4eb88138ce9
Rahul Singh (EDGE) <rahsin@microsoft.com>
2026-08-17 12:37:18 -0700

    Storage: Remove expired histograms and emitting code
    Cleans up code related to four storage histograms that have been expired
    for close to a year or more.
    
    Bug: none
    Change-Id: I90a742b811c8d6528f81527f236a288a455590ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253405
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680702}

--- chrome/browser/ui/views/storage/storage_pressure_bubble_view.cc
+++ chrome/browser/ui/views/storage/storage_pressure_bubble_view.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/storage/storage_pressure_bubble_view.h"
 
 #include "base/feature_list.h"
-#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/navigator/browser_navigator.h"
@@ -27,19 +26,6 @@ namespace {
 
 const char kAllSitesContentSettingsUrl[] =
     "chrome://settings/content/all?sort=data-stored";
-
-// These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
-enum class StoragePressureBubbleHistogramValue {
-  kShown = 0,
-  kIgnored = 1,
-  kOpenedAllSites = 2,
-  kMaxValue = kOpenedAllSites,
-};
-
-void RecordBubbleHistogramValue(StoragePressureBubbleHistogramValue value) {
-  base::UmaHistogramEnumeration("Storage.StoragePressure.Bubble", value);
-}
 
 }  // namespace
 
@@ -63,8 +49,6 @@ void StoragePressureBubbleView::ShowBubble(const url::Origin& origin) {
   StoragePressureBubbleView* bubble =
       new StoragePressureBubbleView(anchor, bwi, origin);
   views::BubbleDialogDelegateView::CreateBubble(bubble)->Show();
-
-  RecordBubbleHistogramValue(StoragePressureBubbleHistogramValue::kShown);
 }
 
 StoragePressureBubbleView::StoragePressureBubbleView(
@@ -73,8 +57,7 @@ StoragePressureBubbleView::StoragePressureBubbleView(
     const url::Origin& origin)
     : BubbleDialogDelegateView(anchor, views::BubbleBorder::TOP_RIGHT),
       bwi_(bwi),
-      origin_(origin),
-      ignored_(true) {
+      origin_(origin) {
   SetButtons(static_cast<int>(ui::mojom::DialogButton::kOk));
   SetTitle(IDS_SETTINGS_STORAGE_PRESSURE_BUBBLE_VIEW_TITLE);
   SetButtonLabel(ui::mojom::DialogButton::kOk,
@@ -85,17 +68,9 @@ StoragePressureBubbleView::StoragePressureBubbleView(
   set_close_on_deactivate(false);
 }
 
-StoragePressureBubbleView::~StoragePressureBubbleView() {
-  if (ignored_) {
-    RecordBubbleHistogramValue(StoragePressureBubbleHistogramValue::kIgnored);
-  }
-}
+StoragePressureBubbleView::~StoragePressureBubbleView() = default;
 
 void StoragePressureBubbleView::OnDialogAccepted() {
-  ignored_ = false;
-  RecordBubbleHistogramValue(
-      StoragePressureBubbleHistogramValue::kOpenedAllSites);
-
   const GURL all_sites_gurl(kAllSitesContentSettingsUrl);
   NavigateParams params(bwi_->GetBrowserForMigrationOnly(), all_sites_gurl,
                         ui::PAGE_TRANSITION_AUTO_TOPLEVEL);
--- chrome/browser/ui/views/storage/storage_pressure_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/storage/storage_pressure_bubble_view_browsertest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/storage/storage_pressure_bubble_view.h"
 
-#include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/storage_pressure_bubble.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -35,7 +34,6 @@ IN_PROC_BROWSER_TEST_F(StoragePressureBubbleViewTest, InvokeUi_default) {
 }
 
 IN_PROC_BROWSER_TEST_F(StoragePressureBubbleViewTest, AcceptDialog) {
-  base::HistogramTester histogram_tester;
   views::NamedWidgetShownWaiter widget_waiter(
       views::test::AnyWidgetTestPasskey{}, "StoragePressureBubbleView");
 
@@ -55,15 +53,9 @@ IN_PROC_BROWSER_TEST_F(StoragePressureBubbleViewTest, AcceptDialog) {
                 ->GetActiveWebContents()
                 ->GetVisibleURL(),
             GURL("chrome://settings/content/all?sort=data-stored"));
-
-  histogram_tester.ExpectBucketCount("Storage.StoragePressure.Bubble",
-                                     /*kOpenedAllSites=*/2, 1);
-  histogram_tester.ExpectBucketCount("Storage.StoragePressure.Bubble",
-                                     /*kIgnored=*/1, 0);
 }
 
 IN_PROC_BROWSER_TEST_F(StoragePressureBubbleViewTest, CancelDialog) {
-  base::HistogramTester histogram_tester;
   views::NamedWidgetShownWaiter widget_waiter(
       views::test::AnyWidgetTestPasskey{}, "StoragePressureBubbleView");
 
@@ -78,9 +70,4 @@ IN_PROC_BROWSER_TEST_F(StoragePressureBubbleViewTest, CancelDialog) {
   destroy_waiter.Wait();
 
   EXPECT_EQ(browser()->tab_strip_model()->count(), initial_tab_count);
-
-  histogram_tester.ExpectBucketCount("Storage.StoragePressure.Bubble",
-                                     /*kOpenedAllSites=*/2, 0);
-  histogram_tester.ExpectBucketCount("Storage.StoragePressure.Bubble",
-                                     /*kIgnored=*/1, 1);
 }
