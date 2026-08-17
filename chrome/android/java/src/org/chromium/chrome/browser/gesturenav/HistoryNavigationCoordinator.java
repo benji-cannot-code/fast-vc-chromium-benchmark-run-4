cbc2356d40f585a82e4709652de9b3091c72d8a1
Eleanor Lee <eleanorlee@google.com>
2026-08-17 08:02:31 -0700

    [Community Contributions] Clean up ActivateHistoryNavigationCoordinatorInGestureNavMode
    Change-Id: I681b9c63418eb93c762b3451bdd146d7ec63d36e
    Bug: 515269618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263370
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680547}

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/HistoryNavigationCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/HistoryNavigationCoordinator.java
@@ -17,7 +17,6 @@ import org.chromium.build.annotations.MonotonicNonNull;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.NullUnmarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.fullscreen.FullscreenManager;
 import org.chromium.chrome.browser.fullscreen.FullscreenOptions;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
@@ -235,10 +234,7 @@ public class HistoryNavigationCoordinator
             return mEnabled;
         }
 
-        if (ChromeFeatureList.sActivateHistoryNavigationCoordinatorInGestureNavMode.isEnabled()) {
-            return true;
-        }
-        return !UiUtils.isGestureNavigationMode(mWindow.getWindow());
+        return true;
     }
 
     @Override
