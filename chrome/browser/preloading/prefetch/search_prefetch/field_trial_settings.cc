cff537a87ef53500f99cdc8c6bcdaf74e1af4dfb
kenoss <kenoss@chromium.org>
2026-08-16 21:49:33 -0700

    SearchPrefetch: Remove allow_top_selection feature param
    Remove the `allow_top_selection` feature param from
    `kSearchNavigationPrefetch` and the helper function
    `AllowTopNavigationPrefetch()` as it's enabled by default for a while.
    
    TAG=agy
    CONV=138816e4-66a7-48c3-a771-372f38b6c824
    
    Bug: 394988793
    Change-Id: Ifafcc532e04733aa0cb1ab0e611404e965314377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8255869
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680345}

--- chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.cc
@@ -134,11 +134,6 @@ bool IsTouchDownPrefetchEnabled() {
                                                  "touch_down", true);
 }
 
-bool AllowTopNavigationPrefetch() {
-  return base::GetFieldTrialParamByFeatureAsBool(kSearchNavigationPrefetch,
-                                                 "allow_top_selection", true);
-}
-
 bool PrefetchSearchHistorySuggestions() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kSearchNavigationPrefetch, "prefetch_search_history", true);
