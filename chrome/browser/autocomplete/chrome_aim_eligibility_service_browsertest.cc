0a501174fa1ccf1d50df971d95a11d6eb413e976
Lily Chen <chlily@chromium.org>
2026-08-17 13:25:47 -0700

    [gardener] Disable a ChromeAimEligibilityServiceOAuthBrowserTest
    Test is flaky on ChromeOS.
    
    Sample failure:
    ci.chromium.org/ui/test-investigate/invocations/build-8673265237225605009
    
    Bug: 541665465
    Change-Id: Id4511e5505469e946446301fbea774556a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263532
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Owners-Override: Lily Chen <chlily@chromium.org>
    Reviewed-by: Wen-Chien Wang <wcwang@chromium.org>
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Owners-Override: Wen-Chien Wang <wcwang@chromium.org>
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680738}

--- chrome/browser/autocomplete/chrome_aim_eligibility_service_browsertest.cc
+++ chrome/browser/autocomplete/chrome_aim_eligibility_service_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
+#include "build/build_config.h"
 #include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -1249,8 +1250,14 @@ class ChromeAimEligibilityServiceOAuthBrowserTest
   base::test::ScopedFeatureList feature_list_;
 };
 
+// TODO(crbug.com/541665465): Test is flaky on ChromeOS.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_RequestIncludesOAuthToken DISABLED_RequestIncludesOAuthToken
+#else
+#define MAYBE_RequestIncludesOAuthToken RequestIncludesOAuthToken
+#endif
 IN_PROC_BROWSER_TEST_F(ChromeAimEligibilityServiceOAuthBrowserTest,
-                       RequestIncludesOAuthToken) {
+                       MAYBE_RequestIncludesOAuthToken) {
   // Setup: Make a primary account available with a refresh token.
   auto* identity_manager = identity_test_env()->identity_manager();
   AccountInfo primary_account_info = signin::MakeAccountAvailable(
