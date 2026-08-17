a00cf9243da8eb63a5af34267a6c920bb0ea7316
Daniel Cheng <dcheng@chromium.org>
2026-08-17 00:41:09 -0700

    Return by const ref in ui::AXActionData mojom traits
    Returning by value creates a temporary that is immediately discarded
    after the bytes are written to the wire; this is wasteful since the
    type in question is not consumed during serialization.
    
    This also removes the `std::optional` from the return type; since the
    underlying field is not an optional, the getter will never return a
    `std::nullopt` anyway.
    
    Bug: 546207269
    Change-Id: I36fce34abf7c5b592639ec550aafee6a5e60d912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8262106
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680386}

--- ui/accessibility/mojom/ax_action_data_mojom_traits.h
+++ ui/accessibility/mojom/ax_action_data_mojom_traits.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include <optional>
 #include <string>
 
 #include "ui/accessibility/ax_action_data.h"
@@ -73,8 +72,7 @@ struct StructTraits<ax::mojom::AXActionDataDataView, ui::AXActionData> {
   static ax::mojom::ScrollBehavior scroll_behavior(const ui::AXActionData& a) {
     return a.scroll_behavior;
   }
-  static const std::optional<ui::AXTreeID> child_tree_id(
-      const ui::AXActionData& a) {
+  static const ui::AXTreeID& child_tree_id(const ui::AXActionData& a) {
     return a.child_tree_id;
   }
   static const base::flat_map<ax::mojom::IntListAttribute,
