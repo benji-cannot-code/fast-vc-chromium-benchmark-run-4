cdf79568f8a3b14b8c4490ec7730f022acd716ab
Amya Singhal <amyasinghal@google.com>
2026-08-17 13:12:24 -0700

    [Voice Typing] add FRE exit status metric
    Metric doc:
    https://docs.google.com/document/d/1wyGBXpTU2B0FcPKJfv24rEJyMaFg07OLFtAyRDyJ96Q/edit?usp=sharing&resourcekey=0-SGysZRGMw0DEft49uoMcLQ
    
    Screen recording:
    https://drive.google.com/file/d/1nh17tNc5TuSAMdhZtUAz_qmyk4hCuxlF/view?usp=sharing
    
    Bug: b:545718072, b:539693982
    Change-Id: Ifbc3e39d3ba4b78a717a10d80d92ee879b32884c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8243759
    Commit-Queue: Amya Singhal <amyasinghal@google.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Baran Erfani <baranerf@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680728}

--- chrome/browser/dictation/metrics.cc
+++ chrome/browser/dictation/metrics.cc
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace dictation {
 
+void RecordDictationFirstRunExitStatus(DictationFirstRunExitStatus status) {
+  base::UmaHistogramEnumeration(kFirstRunExitStatusHistogramName, status);
+}
+
 void RecordDictationIsEnabledOnProfileInit(bool is_enabled) {
   base::UmaHistogramBoolean(kIsEnabledOnProfileInitHistogramName, is_enabled);
 }
