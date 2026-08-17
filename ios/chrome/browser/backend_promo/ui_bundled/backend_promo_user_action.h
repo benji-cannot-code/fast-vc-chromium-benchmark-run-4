33a035139b27079cd0287aa5a8fd90e183720353
Joemer Ramos <joemerramos@google.com>
2026-08-17 08:13:29 -0700

    [ios] Add Backend Promo Custom UI data models and interfaces
    Adds BackendPromoUserAction and BackendPromoCustomUIParams for backend promo custom UI.
    
    - Defines BackendPromoUserAction enum and BackendPromoUserActionDismissHandler block.
    - Defines BackendPromoCustomUIParams data container for title, body, and action button strings.
    
    Part 1 of 2 in the Backend Promo Custom UI chain.
    - Part 2 (Coordinator & Mediator): https://crrev.com/c/8229080
    - Internal Integration CL: https://crrev.com/i/9641980
    
    Bug: None
    Change-Id: Ie7ba049a5781f0dc722902ba086d057d766c68e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8232660
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680554}

---
+++ ios/chrome/browser/backend_promo/ui_bundled/backend_promo_user_action.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_BACKEND_PROMO_UI_BUNDLED_BACKEND_PROMO_USER_ACTION_H_
+#define IOS_CHROME_BROWSER_BACKEND_PROMO_UI_BUNDLED_BACKEND_PROMO_USER_ACTION_H_
+
+#import <Foundation/Foundation.h>
+
+// Enum representing the user action taken on the backend promo UI.
+typedef NS_ENUM(NSInteger, BackendPromoUserAction) {
+  BackendPromoUserActionAccepted,
+  BackendPromoUserActionDismissed,
+  BackendPromoUserActionCancelled,
+};
+
+#endif  // IOS_CHROME_BROWSER_BACKEND_PROMO_UI_BUNDLED_BACKEND_PROMO_USER_ACTION_H_
