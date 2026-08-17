15ab18de46cd63fdb075a131625756db38e202fb
Shunya Shishido <sisidovski@chromium.org>
2026-08-17 01:30:52 -0700

    DumpWithoutCrashing on StorageKey mismatch in GetOrCreateRegistration
    Previously, CHECK_EQ was introduced in GetOrCreateRegistration to verify
    that live registrations and versions match the StorageKey of the
    deserialized registration data. However, in certain contexts (such as
    in-memory StorageKey attributes differing from deserialized
    representations), this caused browser crashes in the wild.
    
    This CL replaces the CHECK_EQ with DumpWithoutCrashing and adds scoped
    crash keys to help investigate the root cause without crashing the
    browser.
    
    TAG=agy
    CONV=52f8ff1a-e359-4a9a-89a2-fe075e272ce4
    
    Bug: 539155958
    Change-Id: I20640d9e7cbfd6c523c0b9803afafd58d0d6bdb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8255191
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680401}

--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/byte_size.h"
 #include "base/check_is_test.h"
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -55,6 +58,9 @@ BASE_FEATURE(kServiceWorkerBackgroundUpdateForFindRegistrationForClientUrl,
 
 BASE_FEATURE(kReduceCallingServiceWorkerRegisteredStorageKeysOnStartup,
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kServiceWorkerDumpWithoutCrashingOnStorageKeyMismatch,
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool ReduceCallingServiceWorkerRegisteredStorageKeysOnStartupEnabled() {
   static const bool enabled = base::FeatureList::IsEnabled(
@@ -1196,6 +1202,18 @@ ServiceWorkerRegistry::GetOrCreateRegistration(
   scoped_refptr<ServiceWorkerRegistration> registration =
       context_->GetLiveRegistration(data.registration_id);
   if (registration) {
+    if (registration->key() != data.key &&
+        base::FeatureList::IsEnabled(
+            kServiceWorkerDumpWithoutCrashingOnStorageKeyMismatch)) {
+      // TODO(crbug.com/539155958): Investigate why the StorageKey of the live
+      // registration does not match `data.key`.
+      SCOPED_CRASH_KEY_STRING256("SWRegistry", "live_reg_key",
+                                 registration->key().GetDebugString());
+      SCOPED_CRASH_KEY_STRING256("SWRegistry", "data_key",
+                                 data.key.GetDebugString());
+      SCOPED_CRASH_KEY_NUMBER("SWRegistry", "reg_id", data.registration_id);
+      base::debug::DumpWithoutCrashing();
+    }
     return registration;
   }
 
@@ -1211,7 +1229,21 @@ ServiceWorkerRegistry::GetOrCreateRegistration(
 
   scoped_refptr<ServiceWorkerVersion> version =
       context_->GetLiveVersion(data.version_id);
-  if (!version) {
+  if (version) {
+    if (version->key() != data.key &&
+        base::FeatureList::IsEnabled(
+            kServiceWorkerDumpWithoutCrashingOnStorageKeyMismatch)) {
+      // TODO(crbug.com/539155958): Investigate why the StorageKey of the live
+      // version does not match `data.key`.
+      SCOPED_CRASH_KEY_STRING256("SWRegistry", "live_ver_key",
+                                 version->key().GetDebugString());
+      SCOPED_CRASH_KEY_STRING256("SWRegistry", "data_key",
+                                 data.key.GetDebugString());
+      SCOPED_CRASH_KEY_NUMBER("SWRegistry", "ver_id", data.version_id);
+      SCOPED_CRASH_KEY_NUMBER("SWRegistry", "reg_id", data.registration_id);
+      base::debug::DumpWithoutCrashing();
+    }
+  } else {
     version = base::MakeRefCounted<ServiceWorkerVersion>(
         registration.get(), data.script, data.script_type, data.version_id,
         std::move(version_reference), context_->AsWeakPtr(),
