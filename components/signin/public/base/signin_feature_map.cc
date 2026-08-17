8f560809fa00f963543c9d6d0cf2cb0f48b51809
Tanmoy Mollik <triploblastic@google.com>
2026-08-17 10:53:40 -0700

    [Android] Change the sign out button string for desktop android
    Screenshot: https://g-issues.chromium.org/545100789#attachment80607471
    Bug: 545100789
    Change-Id: Ibb88e384d08c3fb75c191a526a6399aed0ae1871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256270
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680651}

--- components/signin/public/base/signin_feature_map.cc
+++ components/signin/public/base/signin_feature_map.cc
@@ -38,6 +38,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &switches::kFullscreenSignInPromoUseDate,
     &switches::kSmartEmailLineBreaking,
     &switches::kSupportWebSigninAddSession,
+    &switches::kSignOutOfChrome,
     &switches::kSkipRefreshTokenCheckInIdentityManager,
     &switches::kChromeAndroidIdentitySurveyFirstRun,
     &switches::kChromeAndroidIdentitySurveyWeb,
