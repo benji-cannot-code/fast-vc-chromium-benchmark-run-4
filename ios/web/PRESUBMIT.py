e695911360e6d98729ef5966915133a6876dc929
Dylan Nagler <dnagler@google.com>
2026-08-17 11:50:20 -0700

    [presubmit] Add warning encouraging base::NotFatalUntil
    Encourages base::NotFatalUntil for CHECKs in core iOS directories and
    shared components (Autofill, Password Manager) used by ChromWebView.
    This ensures new checks can be rolled out safely without crashing
    the embedding iOS application on failure.
    
    Implementation extracts the linter from ios/web_view into a shared
    helper and enables it for ios/web/ and components/.
    
    The existing linter logic is moved with only minor adjustments
    (i.e. adding support for a path_filter and various code cleanup).
    
    Proposal: go/nfu-linter-proposal
    
    Bug: 518994317
    Change-Id: I072c1c9c60a63fec5b15c6be64acfa905c960912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8242747
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Dylan Nagler <dnagler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680665}

---
+++ ios/web/PRESUBMIT.py
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+def CheckChangeOnUpload(input_api, output_api):
+  results = []
+
+  # Import shared iOS NFU linter
+  original_sys_path = sys.path[:]
+  try:
+    sys.path.append(input_api.change.RepositoryRoot())
+    from build.ios import presubmit_support
+    results.extend(
+      presubmit_support.CheckNotFatalUntilAdoption(input_api, output_api)
+    )
+  finally:
+    sys.path = original_sys_path
+
+  return results
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChangeOnUpload(input_api, output_api)
