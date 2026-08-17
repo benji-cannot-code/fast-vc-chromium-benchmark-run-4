71d300b1e21b4e0200151585a4358e03857548a1
Thomas Lukaszewicz <tluk@chromium.org>
2026-08-16 18:14:24 -0700

    views: Scope Mac per-window theme change to the target widget
    Previously, NativeWidgetMacNSWindowHost::OnWindowNativeThemeChanged()
    called NT::GetInstanceForNativeUi()->NotifyOnNativeThemeUpdated(),
    triggering a process-wide NativeTheme update and cache flush for every
    open window.
    
    This change scopes the notification directly to the target Widget,
    eliminating O(N^2) cross-window traversals.
    
    TAG=agy
    CONV=21de72b6-d645-42ad-8d71-c0bf037b205b
    
    Bug: 40280130
    Change-Id: I82d62d922fb9df5874c250efacac12a748e7d5fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258271
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680309}

--- ui/views/cocoa/native_widget_mac_ns_window_host.mm
+++ ui/views/cocoa/native_widget_mac_ns_window_host.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/hit_test.h"
 #include "ui/base/ime/input_method.h"
 #include "ui/base/mojom/dialog_button.mojom.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/color/color_provider_key.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/recyclable_compositor_mac.h"
@@ -1157,7 +1158,13 @@ void NativeWidgetMacNSWindowHost::OnSpaceActivationChanged(
 }
 
 void NativeWidgetMacNSWindowHost::OnWindowNativeThemeChanged() {
-  ui::NativeTheme::GetInstanceForNativeUi()->NotifyOnNativeThemeUpdated();
+  if (base::FeatureList::IsEnabled(::features::kThemeChangeOptimization)) {
+    if (Widget* widget = GetWidget()) {
+      widget->ThemeChanged();
+    }
+  } else {
+    ui::NativeTheme::GetInstanceForNativeUi()->NotifyOnNativeThemeUpdated();
+  }
 }
 
 void NativeWidgetMacNSWindowHost::OnScrollEvent(
