c3bb048e6c4bf577772b6b10018104d2cfecf3ac
Gianmarco Picarella <picarella@google.com>
2026-08-17 13:20:40 -0700

    Consolidate Ambient non-eligibility logic into permission utils
    Consolidate Ambient Autofill non-eligibility logic and switch
    to using `SubscriptionEligibilityService` instead of reading
    raw preferences:
    
    * Move Ambient subscription tier and device checks into
    `IsEligibleForAmbientAutofill()` in
    `autofill_ai_permission_utils.{h,cc}` and remove
    `autofill_ai_personal_context_enablement_utils.{h,cc}`.
    
    * Observe
    `SubscriptionEligibilityService::Observer::OnAiSubscriptionTierUpdated`
    in `AutofillAiPersonalContextAccessManagerImpl` to recompute and
    log non-eligibility metrics on tier updates.
    
    * Wire `SubscriptionEligibilityService` through desktop and iOS
    `AutofillAiPersonalContextAccessManager` factories.
    
    Bug: 517144102
    Change-Id: I09ca18b447f2d592c6ac08aa038301bba1a3530b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257430
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Gianmarco Picarella <picarella@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680733}

--- chrome/browser/autofill/autofill_ai_personal_context_access_manager_factory.cc
+++ chrome/browser/autofill/autofill_ai_personal_context_access_manager_factory.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/personal_context/personal_context_eligibility_service_factory.h"
 #include "chrome/browser/personal_context/personal_context_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/subscription_eligibility/subscription_eligibility_service_factory.h"
 #include "components/autofill/core/browser/network/autofill_ai/autofill_ai_personal_context_access_manager_impl.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/personal_context/core/personal_context_eligibility_service.h"
 #include "components/personal_context/core/personal_context_service.h"
+#include "components/subscription_eligibility/subscription_eligibility_service.h"
 
 namespace autofill {
 
@@ -42,6 +44,8 @@ AutofillAiPersonalContextAccessManagerFactory::
               .Build()) {
   DependsOn(PersonalContextEligibilityServiceFactory::GetInstance());
   DependsOn(PersonalContextServiceFactory::GetInstance());
+  DependsOn(subscription_eligibility::SubscriptionEligibilityServiceFactory::
+                GetInstance());
 }
 
 AutofillAiPersonalContextAccessManagerFactory::
@@ -60,6 +64,9 @@ std::unique_ptr<KeyedService> AutofillAiPersonalContextAccessManagerFactory::
   personal_context::PersonalContextEligibilityService*
       personal_context_eligibility_service =
           PersonalContextEligibilityServiceFactory::GetForProfile(profile);
+  subscription_eligibility::SubscriptionEligibilityService*
+      subscription_eligibility_service = subscription_eligibility::
+          SubscriptionEligibilityServiceFactory::GetForProfile(profile);
 
   if (!personal_context_service || !personal_context_eligibility_service) {
     return nullptr;
@@ -67,7 +74,7 @@ std::unique_ptr<KeyedService> AutofillAiPersonalContextAccessManagerFactory::
 
   return std::make_unique<AutofillAiPersonalContextAccessManagerImpl>(
       personal_context_service, personal_context_eligibility_service,
-      profile->GetPrefs());
+      subscription_eligibility_service, profile->GetPrefs());
 }
 
 }  // namespace autofill
--- ios/chrome/browser/autofill/model/ios_autofill_ai_personal_context_access_manager_factory.mm
+++ ios/chrome/browser/autofill/model/ios_autofill_ai_personal_context_access_manager_factory.mm
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/common/autofill_features.h"
 #import "components/personal_context/core/personal_context_eligibility_service.h"
 #import "components/personal_context/core/personal_context_service.h"
+#import "components/subscription_eligibility/subscription_eligibility_service.h"
 #import "ios/chrome/browser/personal_context/model/ios_personal_context_eligibility_service_factory.h"
 #import "ios/chrome/browser/personal_context/model/ios_personal_context_service_factory.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/chrome/browser/subscription_eligibility/model/subscription_eligibility_service_factory.h"
 
 // static
 autofill::AutofillAiPersonalContextAccessManager*
@@ -37,6 +39,7 @@ IOSAutofillAiPersonalContextAccessManagerFactory::
                                     ProfileSelection::kNoInstanceInIncognito) {
   DependsOn(IOSPersonalContextEligibilityServiceFactory::GetInstance());
   DependsOn(IOSPersonalContextServiceFactory::GetInstance());
+  DependsOn(SubscriptionEligibilityServiceFactory::GetInstance());
 }
 
 IOSAutofillAiPersonalContextAccessManagerFactory::
@@ -55,6 +58,9 @@ IOSAutofillAiPersonalContextAccessManagerFactory::BuildServiceInstanceFor(
   personal_context::PersonalContextEligibilityService*
       personal_context_eligibility_service =
           IOSPersonalContextEligibilityServiceFactory::GetForProfile(profile);
+  subscription_eligibility::SubscriptionEligibilityService*
+      subscription_eligibility_service =
+          SubscriptionEligibilityServiceFactory::GetForProfile(profile);
 
   if (!personal_context_service || !personal_context_eligibility_service) {
     return nullptr;
@@ -62,5 +68,5 @@ IOSAutofillAiPersonalContextAccessManagerFactory::BuildServiceInstanceFor(
 
   return std::make_unique<autofill::AutofillAiPersonalContextAccessManagerImpl>(
       personal_context_service, personal_context_eligibility_service,
-      profile->GetPrefs());
+      subscription_eligibility_service, profile->GetPrefs());
 }
