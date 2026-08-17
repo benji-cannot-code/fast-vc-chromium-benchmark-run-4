db0042f676e1474df3e8a1b0bf06ec3fe905b142
Stephen McGruer <smcgruer@chromium.org>
2026-08-17 09:40:53 -0700

    Clean up dead PaymentInstrument Mojo endpoints
    Following the removal of Web Payment Handler PaymentInstruments JS API
    in Blink (CL 6402695), clean up 5 dead instrument management endpoints
    from PaymentManager Mojo interface and related code:
    
    - DeletePaymentInstrument
    - GetPaymentInstrument
    - KeysOfPaymentInstruments
    - HasPaymentInstrument
    - ClearPaymentInstruments
    
    This also removes residual writes and definitions for
    PaymentInstrumentKeyInfo (StoredPaymentInstrumentKeyInfoProto), which
    was only used to maintain ordered key lists for
    KeysOfPaymentInstruments.
    
    No disk migration or cleanup is necessary for existing stored data.
    PaymentInstrument records are still used and written by the JIT-install
    path. PaymentInstrumentKeyInfo records are no longer used, but are part
    of the normal service worker database and so are cleaned up if/when
    users clean up their server worker data for the given origin.
    
    Bug: 537253616
    Change-Id: I76e6182473293f99db8e37b0ec0e491fde7ccb94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8132638
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Luis Antunes <luisantunes@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680607}

--- content/browser/payments/payment_app_provider_impl_unittest.cc
+++ content/browser/payments/payment_app_provider_impl_unittest.cc
@@ -41,8 +41,6 @@ class PaymentManager;
 namespace {
 
 using ::payments::mojom::PaymentHandlerStatus;
-using ::payments::mojom::PaymentInstrument;
-using ::payments::mojom::PaymentInstrumentPtr;
 
 void GetAllPaymentAppsCallback(
     InstalledPaymentAppsFinder::PaymentApps* out_apps,
