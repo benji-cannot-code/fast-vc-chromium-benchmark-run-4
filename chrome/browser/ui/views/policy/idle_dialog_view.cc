59c6a3c51b5d43136aa0d903a66536b3a93ab396
Bryan Kim <bryankim@google.com>
2026-08-18 13:48:49 -0700

    Componentize chrome/browser/ui top-level leaf modules
    Extract screen_capture_notification_ui, browser_init_state,
    idle_dialog, and accessibility_util from //chrome/browser/ui:ui
    into standalone source_sets.
    
    AGENT_GENERATED_CL
    TAG=agy
    CONV=29fdac25-cf08-4035-81de-2076c933995d
    
    Bug: 353332589
    Change-Id: Id02ed6ca051ed86c2cdee867a2c3e082f16bea15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261756
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681655}

--- chrome/browser/ui/views/policy/idle_dialog_view.cc
+++ chrome/browser/ui/views/policy/idle_dialog_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/metrics/user_metrics.h"
-#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
@@ -19,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/vector_icons/vector_icons.h"
+#include "ui/base/base_window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/mojom/dialog_button.mojom.h"
