271c91ea0d234d61f56d7b2b6ab0636a5a81e360
Yoshisto Yanagisawa <yyanagisawa@chromium.org>
2026-08-18 01:19:11 -0700

    Rename WorkerContentSettingsProxy.RequestFileSystemAccessSync to AllowFileSystem
    Aligns the FileSystem content settings check method name with other
    storage access methods (AllowIndexedDB, AllowCacheStorage, AllowWebLocks)
    in WorkerContentSettingsProxy.
    
    TAG=agy
    CONV=c502b007-693e-4a1c-8a47-4233becea563
    
    Bug: 503624894
    Change-Id: I50c8b14d2a72b7929b21a5d0f3e7418b150b6336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256792
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681191}

--- content/browser/worker_host/shared_worker_content_settings_proxy_impl.cc
+++ content/browser/worker_host/shared_worker_content_settings_proxy_impl.cc
@@ -51,8 +51,8 @@ void SharedWorkerContentSettingsProxyImpl::AllowWebLocks(
   }
 }
 
-void SharedWorkerContentSettingsProxyImpl::RequestFileSystemAccessSync(
-    RequestFileSystemAccessSyncCallback callback) {
+void SharedWorkerContentSettingsProxyImpl::AllowFileSystem(
+    AllowFileSystemCallback callback) {
   if (!origin_.opaque()) {
     owner_->AllowFileSystem(origin_.GetURL(), std::move(callback));
   } else {
