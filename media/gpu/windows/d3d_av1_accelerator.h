5572f8ff9738d124cdde7535cfcf63f0c1e8b2e6
Sahir Vellani <sahir.vellani@microsoft.com>
2026-08-17 22:25:23 -0700

    Rename D3D11AV1Accelerator to D3DAV1Accelerator
    There is no D3D11-specific API usage in this class. Eventually we will
    be using a general D3DPictureBuffer instead of a D3D11PictureBuffer in
    order to support a full D3D12 based video decoding pipeline; therefore,
    preempt those changes with a rename here to avoid confusion.
    
    Bug: 542565485
    Change-Id: Ifa600559b8d92908afcaa89d1229c1b73f68958c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260940
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1681115}

--- media/gpu/windows/d3d_av1_accelerator.h
+++ media/gpu/windows/d3d_av1_accelerator.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors
+// Copyright 2026 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_WINDOWS_D3D11_AV1_ACCELERATOR_H_
-#define MEDIA_GPU_WINDOWS_D3D11_AV1_ACCELERATOR_H_
+#ifndef MEDIA_GPU_WINDOWS_D3D_AV1_ACCELERATOR_H_
+#define MEDIA_GPU_WINDOWS_D3D_AV1_ACCELERATOR_H_
 
 #include <d3d11_1.h>
 #include <d3d9.h>
@@ -20,16 +20,16 @@ typedef struct _DXVA_Tile_AV1 DXVA_Tile_AV1;
 
 namespace media {
 
-class D3D11AV1Accelerator : public AV1Decoder::AV1Accelerator {
+class D3DAV1Accelerator : public AV1Decoder::AV1Accelerator {
  public:
-  D3D11AV1Accelerator(D3D11VideoDecoderClient* client,
-                      MediaLog* media_log,
-                      bool disable_invalid_ref);
+  D3DAV1Accelerator(D3D11VideoDecoderClient* client,
+                    MediaLog* media_log,
+                    bool disable_invalid_ref);
 
-  D3D11AV1Accelerator(const D3D11AV1Accelerator&) = delete;
-  D3D11AV1Accelerator& operator=(const D3D11AV1Accelerator&) = delete;
+  D3DAV1Accelerator(const D3DAV1Accelerator&) = delete;
+  D3DAV1Accelerator& operator=(const D3DAV1Accelerator&) = delete;
 
-  ~D3D11AV1Accelerator() override;
+  ~D3DAV1Accelerator() override;
 
   scoped_refptr<AV1Picture> CreateAV1Picture(bool apply_grain) override;
 
@@ -62,4 +62,4 @@ class D3D11AV1Accelerator : public AV1Decoder::AV1Accelerator {
 
 }  // namespace media
 
-#endif  // MEDIA_GPU_WINDOWS_D3D11_AV1_ACCELERATOR_H_
+#endif  // MEDIA_GPU_WINDOWS_D3D_AV1_ACCELERATOR_H_
