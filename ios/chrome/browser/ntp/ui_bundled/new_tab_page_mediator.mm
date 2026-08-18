c50e04bb15c9ac041b0809c12eba294c71bf40ae
Hira Mahmood <hiramahmood@google.com>
2026-08-18 08:44:13 -0700

    [iOS] Add missing initialLoad check to kDefault background logging
    This CL adds an `initialLoad` check before logging
    IOS.HomeCustomization.Background.Ntp.Loaded for the kDefault bucket.
    
    This aligns the implementation with the rest of the buckets and the
    histogram's definition, which specifies it should only be recorded on
    initial load.
    
    Fixed: 548458867
    Change-Id: I6885c3c39a8e5927b906f4840ae7c2455de27f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257741
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681417}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
@@ -1028,8 +1028,10 @@ void CleanupImageFetcherCacheIfNeeded(PrefService* pref_service,
   [traitAccessor setObjectForNewTabPageTrait:[NewTabPageTrait defaultValue]];
   [traitAccessor setBoolForNewTabPageImageBackgroundTrait:NO];
   [self.logoMediator setLogoTintColor:nil];
-  base::UmaHistogramEnumeration("IOS.HomeCustomization.Background.Ntp.Loaded",
-                                HomeCustomizationBackgroundStyle::kDefault);
+  if (initialLoad) {
+    base::UmaHistogramEnumeration("IOS.HomeCustomization.Background.Ntp.Loaded",
+                                  HomeCustomizationBackgroundStyle::kDefault);
+  }
 }
 
 // Fetches and applies a custom background image.
