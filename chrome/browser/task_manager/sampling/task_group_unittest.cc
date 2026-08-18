5f09b68acb9112c9ae02ccb9ad08b136aa3ac3aa
Joe Mason <joenotcharles@google.com>
2026-08-18 16:37:54 -0700

    Replace KiBU/MiBU/etc with KiB/MiB/etc for chrome/browser/task_manager
    The base::KiBU() family of functions have been deprecated and replaced
    with base::KiB().
    
    Mechanically converted with:
    
    git grep -l base::[KMGTPE]iBU|xargs sed -i
    "s/base::\([KMGTPE]\)iBU/base::\1iB/g"
    
    This CL was uploaded by git cl split.
    
    R=creis@chromium.org
    
    Bug: 448661443
    Change-Id: Ia1ab3bce17b1c3b5acbccc5a2f529745f988e71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261284
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681832}

--- chrome/browser/task_manager/sampling/task_group_unittest.cc
+++ chrome/browser/task_manager/sampling/task_group_unittest.cc
@@ -175,7 +175,7 @@ TEST_F(TaskGroupTest, NetworkBytesSentReadZero) {
 // Test the task has correct network usage rate when only having read bytes.
 TEST_F(TaskGroupTest, NetworkBytesRead) {
   CreateTaskGroup(false);
-  constexpr base::ByteSize read_bytes = base::KiBU(1);
+  constexpr base::ByteSize read_bytes = base::KiB(1);
   FakeTask fake_task(base::Process::Current().Pid(), Task::RENDERER,
                      false /* is_running_in_vm */);
   fake_task.OnNetworkBytesRead(read_bytes);
@@ -206,7 +206,7 @@ TEST_F(TaskGroupTest, NetworkBytesRead2SecRefresh) {
   CreateTaskGroup(false);
   const int refresh_secs = 2;
   constexpr base::ByteSize read_bytes =
-      base::KiBU(1) * refresh_secs;  // for integer division
+      base::KiB(1) * refresh_secs;  // for integer division
   FakeTask fake_task(base::Process::Current().Pid(), Task::RENDERER,
                      false /* is_running_in_vm */);
   fake_task.OnNetworkBytesRead(read_bytes);
@@ -266,7 +266,7 @@ TEST_F(TaskGroupTest, NetworkBytesSentThenRead) {
 // refresh with no traffic and that cumulative is still correct.
 TEST_F(TaskGroupTest, NetworkBytesReadRefreshNone) {
   CreateTaskGroup(false);
-  constexpr base::ByteSize read_bytes = base::KiBU(1);
+  constexpr base::ByteSize read_bytes = base::KiB(1);
   FakeTask fake_task(base::Process::Current().Pid(), Task::RENDERER,
                      false /* is_running_in_vm */);
   fake_task.OnNetworkBytesRead(read_bytes);
@@ -281,7 +281,7 @@ TEST_F(TaskGroupTest, NetworkBytesReadRefreshNone) {
 // refresh with no traffic and that cumulative is still correct.
 TEST_F(TaskGroupTest, NetworkBytesSentRefreshNone) {
   CreateTaskGroup(false);
-  constexpr base::ByteSize sent_bytes = base::KiBU(1);
+  constexpr base::ByteSize sent_bytes = base::KiB(1);
   FakeTask fake_task(base::Process::Current().Pid(), Task::RENDERER,
                      false /* is_running_in_vm */);
   fake_task.OnNetworkBytesSent(sent_bytes);
@@ -296,7 +296,7 @@ TEST_F(TaskGroupTest, NetworkBytesSentRefreshNone) {
 // and that cumulative is still correct.
 TEST_F(TaskGroupTest, NetworkBytesTransferredRefreshNone) {
   CreateTaskGroup(false);
-  constexpr base::ByteSize read_bytes = base::KiBU(1);
+  constexpr base::ByteSize read_bytes = base::KiB(1);
   constexpr base::ByteSize sent_bytes = base::ByteSize(1);
   const int number_of_cycles = 2;
   FakeTask fake_task(base::Process::Current().Pid(), Task::RENDERER,
@@ -343,7 +343,7 @@ TEST_F(TaskGroupTest, NetworkBytesReadAsGroup) {
 // called and that the cumulative is as up to date as possible.
 TEST_F(TaskGroupTest, NetworkBytesTransferredRefreshOutOfOrder) {
   CreateTaskGroup(false);
-  constexpr base::ByteSize read_bytes = base::KiBU(1);
+  constexpr base::ByteSize read_bytes = base::KiB(1);
   constexpr base::ByteSize sent_bytes = base::ByteSize(1);
   const int number_of_cycles = 4;
   base::ByteSize number_of_bytes_transferred;
