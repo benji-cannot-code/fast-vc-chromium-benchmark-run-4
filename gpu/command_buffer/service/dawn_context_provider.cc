73d1b5316b09321a05ff202d2568ead694bf5919
Etienne Pierre-doray <etiennep@chromium.org>
2026-08-18 08:48:24 -0700

    [tracing] Set dawn_perfetto_dir and cleanup up DawnPlatform
    This CL follows-up on https://dawn-review.googlesource.com/c/dawn/+/329295
    
    Bug: 542534250
    Change-Id: I6a6232b9bf88bc70a7760fd5a3bd99dfa2d6f328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256195
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681421}

--- gpu/command_buffer/service/dawn_context_provider.cc
+++ gpu/command_buffer/service/dawn_context_provider.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "base/trace_event/process_memory_dump.h"
-#include "base/trace_event/trace_arguments.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "components/crash/core/common/crash_key.h"
@@ -352,19 +351,6 @@ std::vector<wgpu::FeatureName> GetRequiredFeatures(
 
   return features;
 }
-
-class Platform : public webgpu::DawnPlatform {
- public:
-  using webgpu::DawnPlatform::DawnPlatform;
-
-  ~Platform() override = default;
-
-  const unsigned char* GetTraceCategoryEnabledFlag(
-      dawn::platform::TraceCategory category) override {
-    return TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(
-        TRACE_DISABLED_BY_DEFAULT("gpu.graphite.dawn"));
-  }
-};
 
 #if BUILDFLAG(IS_WIN)
 bool GetANGLED3D11DeviceLUID(LUID* luid) {
@@ -732,7 +718,7 @@ class DawnSharedContext : public base::RefCountedThreadSafe<DawnSharedContext>,
   scoped_refptr<GpuPersistentCache> persistent_cache_;
   raw_ptr<dawn::platform::CachingInterface> caching_interface_ = nullptr;
 
-  Platform platform_;
+  webgpu::DawnPlatform platform_;
   std::unique_ptr<webgpu::DawnInstance> instance_;
   wgpu::Adapter adapter_;
   wgpu::Device device_;
