484855969f9782b4ab6d2fda91cf11ef6e0ed700
Sky Malice <skym@chromium.org>
2026-08-18 05:38:51 -0700

    [Clean] Fix inline Javadoc block tags in android_webview/.
    Fix malformed inline Javadoc tags (such as {@param} -> {@code} and
    {@see} -> {@link}) across Java files under android_webview/.
    
    TAG=agy
    CONV=64328260-c38e-49ce-a1c5-aff8c94e7b98
    
    Bug: 546532474
    Change-Id: I3be78314470cc5897d49ed89d3c5ff71e81a4622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265886
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681299}

--- android_webview/support_library/callback/java/src/org/chromium/support_lib_callback_glue/SupportLibWebViewContentsClientAdapter.java
+++ android_webview/support_library/callback/java/src/org/chromium/support_lib_callback_glue/SupportLibWebViewContentsClientAdapter.java
@@ -79,7 +79,7 @@ public class SupportLibWebViewContentsClientAdapter {
     }
 
     /**
-     * Indicates whether this client can handle the callback(s) assocated with {@param featureName}.
+     * Indicates whether this client can handle the callback(s) assocated with {@code featureName}.
      * This should be called with the correct feature name before invoking the corresponding
      * callback, and the callback must not be called if this returns {@code false} for the feature.
      *
