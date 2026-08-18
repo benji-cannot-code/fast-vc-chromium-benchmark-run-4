661e0c518d128a233167f82c624edbebf07a419b
Diana OuYang <dianaou@google.com>
2026-08-18 16:17:12 -0700

    Hide pin button in cobr overflow menu when not eligible.
    This change updates the visibility logic of the pin button in the
    overflow menu to require the page to be cobrowse eligible. It also adds
    WebUI tests to verify that the pin button is hidden when
    isCobrowseEligible is false.
    
    Bug: b:540964285
    Change-Id: I682cc621579c30ad4813a04231959da49d72a5df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258102
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Diana OuYang <dianaou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681810}

--- chrome/browser/resources/contextual_tasks/overflow_menu.ts
+++ chrome/browser/resources/contextual_tasks/overflow_menu.ts
@@ -151,7 +151,7 @@ export class OverflowMenuElement extends OverflowMenuElementBase {
 
   protected shouldShowPinButton_(): boolean {
     return this.isPinButtonEnabled && this.isAiPage &&
-        this.isHandshakeComplete;
+        this.isHandshakeComplete && this.isCobrowseEligible;
   }
 
   protected getPinButtonTooltip_(): string {
