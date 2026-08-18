6144842b872f4fe1662daecd8b08124445436016
Joe Mason <joenotcharles@google.com>
2026-08-17 18:29:03 -0700

    Rename KiBU / MiBU etc. to KiB / MiB and update callers in base/
    The old names are left as an alias until all call sites are updated.
    
    base/byte_size.h was edited by hand, then the rest of base/ was updated
    with
    
    git grep -l [KMGTPE]iBU|grep -v byte_size.h|xargs sed -i
    "s/\([KMGTPE]\)iBU(/\1iB(/g"
    
    NO_IFTTT=No changes to SysUtils.java needed
    
    Bug: 448661443
    Change-Id: I7f1c65cd32203ad1be600822529f4960f7de5e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8203066
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680999}

--- base/system/sys_info_unittest.cc
+++ base/system/sys_info_unittest.cc
@@ -59,7 +59,7 @@ namespace base {
 // Some Android (Cast) test devices have a large portion of physical memory
 // reserved. During investigation, around 115-150 MB were seen reserved, so we
 // track this here with a factory of safety of 2.
-static constexpr ByteSize kReservedPhysicalMemory = MiBU(300);
+static constexpr ByteSize kReservedPhysicalMemory = MiB(300);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 using SysInfoTest = PlatformTest;
@@ -587,11 +587,11 @@ TEST_F(SysInfoTest, MaxFrequencyPerProcessor) {
 
 TEST_F(SysInfoTest, MemoryOverride_LowEndDevice) {
   {
-    test::ScopedAmountOfPhysicalMemoryOverride memory_override(MiBU(512));
+    test::ScopedAmountOfPhysicalMemoryOverride memory_override(MiB(512));
     EXPECT_TRUE(SysInfo::IsLowEndDevice());
   }
   {
-    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiBU(4));
+    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiB(4));
     EXPECT_FALSE(SysInfo::IsLowEndDevice());
   }
 }
@@ -599,19 +599,19 @@ TEST_F(SysInfoTest, MemoryOverride_LowEndDevice) {
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
 TEST_F(SysInfoTest, MemoryOverride_IsNGbDevice) {
   {
-    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiBU(3));
+    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiB(3));
     EXPECT_TRUE(SysInfo::Is3GbDevice());
     EXPECT_FALSE(SysInfo::Is4GbDevice());
     EXPECT_FALSE(SysInfo::Is6GbDevice());
   }
   {
-    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiBU(4));
+    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiB(4));
     EXPECT_FALSE(SysInfo::Is3GbDevice());
     EXPECT_TRUE(SysInfo::Is4GbDevice());
     EXPECT_FALSE(SysInfo::Is6GbDevice());
   }
   {
-    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiBU(6));
+    test::ScopedAmountOfPhysicalMemoryOverride memory_override(GiB(6));
     EXPECT_FALSE(SysInfo::Is3GbDevice());
     EXPECT_FALSE(SysInfo::Is4GbDevice());
     EXPECT_TRUE(SysInfo::Is6GbDevice());
