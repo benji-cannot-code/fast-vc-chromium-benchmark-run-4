6b4c8e0a65eb70ef0e9c0ad16df7c31662f4d5ca
Briana McClure <bmcclure@google.com>
2026-08-17 18:18:57 -0700

    [iOS][NTPRedesign] Make NTP margins 16pt
    This change makes the NTP margin 16pt when kNewTabPageUICleanup is
    enabled. Constraints for the NTP modules are updated to maintain the
    intended padding based on the new margins.
    
    This change also updates `FeedHeaderViewController` to still create the
    feed header even if the title is hidden. This ensures the top padding of
    the Discover feed is accurate.
    
    New Margins on iPhone:
    http://screencast/cast/NjA4NDk2MzU1OTUzODY4OHw3M2I4NzI1OS00Zg
    
    Lanscape: http://screencast/cast/NjA4OTgxMDIyODgwNTYzMnxhMzg0MDY4Ny01Mw
    iPad: http://screencast/cast/NjYwMDUwOTc4Mjc1MzI4MHwzMTJlMTIzMC05Yw
    Fixed: 545560669
    Change-Id: I6ae7c5a09f3e22814ecdf0bd60ef8a06d8015457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257188
    Commit-Queue: Briana McClure <bmcclure@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680992}

--- ios/chrome/browser/content_suggestions/ui/content_suggestions_collection_utils.mm
+++ ios/chrome/browser/content_suggestions/ui/content_suggestions_collection_utils.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/content_suggestions/public/ntp_home_constants.h"
 #import "ios/chrome/browser/location_bar/ui_bundled/location_bar_constants.h"
 #import "ios/chrome/browser/ntp/search_engine_logo/ui/search_engine_logo_state.h"
+#import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_constants.h"
 #import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.h"
 #import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_constants.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
@@ -263,6 +264,11 @@ CGFloat SearchFieldTopMargin(SearchEngineLogoState logo_state) {
 CGFloat SearchFieldWidth(CGFloat width, UITraitCollection* trait_collection) {
   if (IsRegularXRegularSizeClass(trait_collection)) {
     return kSearchFieldLarge;
+  }
+
+  if (IsNewTabPageUICleanupEnabled()) {
+    return std::clamp(width - (2 * kNewTabPageHorizontalMargin),
+                      kSearchFieldSmallMin, kSearchFieldLarge);
   }
 
   if (IsAimEnabledInNtp() && !IsCompactHeight(trait_collection)) {
--- ios/chrome/browser/ntp/ui_bundled/feed_header_view_controller.mm
+++ ios/chrome/browser/ntp/ui_bundled/feed_header_view_controller.mm
@@ -34,6 +34,9 @@ const CGFloat kCustomSearchEngineLabelFontSize = 13;
 const CGFloat kCustomSearchEngineLabelHeight = 18;
 // The height of the header container.
 const CGFloat kDiscoverFeedHeaderHeight = 30;
+// Height of the Discover header when the Discover title is hidden. This is used
+// to ensure sufficient top padding.
+const CGFloat kDiscoverFeedHeaderHeightUICleanup = 6;
 // Max size that the Title and Segmented Control fonts will scale to.
 const CGFloat kMaxFontSize = 24;
 
@@ -87,8 +90,8 @@ const CGFloat kMaxFontSize = 24;
 #pragma mark - Public
 
 - (CGFloat)feedHeaderHeight {
-  if (![self shouldShowHeader]) {
-    return 0;
+  if (![self shouldShowTitleLabel]) {
+    return kDiscoverFeedHeaderHeightUICleanup;
   }
   return kDiscoverFeedHeaderHeight;
 }
@@ -99,7 +102,7 @@ const CGFloat kMaxFontSize = 24;
   }
   [self.titleLabel removeFromSuperview];
   self.titleLabel = nil;
-  if ([self shouldShowHeader]) {
+  if ([self shouldShowTitleLabel]) {
     self.titleLabel = [self createTitleLabel];
     [self.container addSubview:self.titleLabel];
   }
@@ -114,7 +117,7 @@ const CGFloat kMaxFontSize = 24;
 #pragma mark - Private
 
 - (void)configureHeaderViews {
-  if ([self shouldShowHeader]) {
+  if ([self shouldShowTitleLabel]) {
     self.titleLabel = [self createTitleLabel];
     [self.container addSubview:self.titleLabel];
   }
@@ -123,9 +126,8 @@ const CGFloat kMaxFontSize = 24;
   }
 }
 
-// Returns whether the Discover feed header should be shown. When NO, the
-// header view is hidden, its height is 0, and the title label is not created.
-- (BOOL)shouldShowHeader {
+// Returns whether the Discover feed header title label should be shown.
+- (BOOL)shouldShowTitleLabel {
   if (IsNewTabPageUICleanupEnabled() &&
       [self.NTPDelegate isGoogleDefaultSearchEngine]) {
     return NO;
@@ -175,8 +177,6 @@ const CGFloat kMaxFontSize = 24;
     self.feedHeaderConstraints = nil;
   }
   self.feedHeaderConstraints = [[NSMutableArray alloc] init];
-
-  self.view.hidden = ![self shouldShowHeader];
 
   [self anchorContainer];
   if (self.titleLabel) {
