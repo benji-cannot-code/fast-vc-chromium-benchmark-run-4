001586dd2228a2d1a4f8a8746eb14020f40971a4
Chris Fredrickson <cfredric@chromium.org>
2026-08-18 11:19:50 -0700

    Remove unused DomainRule struct and FindDomainPtr typedef
    Change-Id: I8ffb6b59d78c376f660fcd88c5f665e1aa48514c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264556
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681544}

--- net/base/registry_controlled_domains/registry_controlled_domain.h
+++ net/base/registry_controlled_domains/registry_controlled_domain.h
@@ -130,8 +130,6 @@ namespace url {
 class Origin;
 }
 
-struct DomainRule;
-
 namespace net::registry_controlled_domains {
 
 // This enum is a required parameter to all public methods declared for this
@@ -310,8 +308,6 @@ NET_EXPORT size_t
 PermissiveGetHostRegistryLength(std::u16string_view host,
                                 UnknownRegistryFilter unknown_filter,
                                 PrivateRegistryFilter private_filter);
-
-typedef const struct DomainRule* (*FindDomainPtr)(const char*, unsigned int);
 
 // Used for unit tests. Uses default domains.
 NET_EXPORT_PRIVATE void ResetFindDomainGraphForTesting();
