8ec2f48efc0c2f84aff4b558ce75aaa9bf9ce295
andrew johnson <andrewlj@google.com>
2026-08-18 08:27:03 -0700

    Create WalletReminderNoticeUiDelegateDesktop skeleton class
    This CL introduces the skeleton class for
    WalletReminderNoticeUiDelegateDesktop. It implements the
    WalletReminderNoticeUiDelegate interface.
    
    Fixed: b:545683978
    Change-Id: I8a46bafeccb675fdcd063a4a1a182f67c8e15212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259090
    Reviewed-by: Fernando Ramirez <ferny@google.com>
    Commit-Queue: Andrew Johnson <andrewlj@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681401}

---
+++ chrome/browser/ui/autofill/payments/wallet_reminder_notice_ui_delegate_desktop.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/autofill/payments/wallet_reminder_notice_ui_delegate_desktop.h"
+
+namespace autofill::payments {
+
+WalletReminderNoticeUiDelegateDesktop::WalletReminderNoticeUiDelegateDesktop() =
+    default;
+
+WalletReminderNoticeUiDelegateDesktop::
+    ~WalletReminderNoticeUiDelegateDesktop() = default;
+
+void WalletReminderNoticeUiDelegateDesktop::ShowWalletReminderNotice(
+    LegalMessageLines legal_message_lines) {
+  // TODO(crbug.com/545685236): Implement ShowWalletReminderNotice which will
+  // display the page action icon and bubble.
+}
+
+}  // namespace autofill::payments
