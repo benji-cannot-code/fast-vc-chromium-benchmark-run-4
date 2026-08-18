1fc7efe97aa5490073b8c5e42773eabd7fa9e8f8
Georg Neis <neis@chromium.org>
2026-08-17 21:28:05 -0700

    ash: Refactor kiosk fullscreen toggling to use BrowserDelegate
    ... and thus eliminate a browser_commands.h dependency.
    
    Bug: b:369688254
    Change-Id: I5310b54925266b198757842ffa0eac31eca278de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256813
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681093}

--- chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.cc
+++ chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.cc
@@ -19,12 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "chrome/browser/ash/browser_delegate/browser_controller.h"
+#include "chrome/browser/ash/browser_delegate/browser_delegate.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_policies.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_settings_navigation_throttle.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_troubleshooting_controller_ash.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_init_state.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
@@ -33,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "ui/base/base_window.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/widget/widget_observer.h"
@@ -225,7 +227,8 @@ bool KioskBrowserWindowHandler::PreTriageNewBrowserWindowWithoutUrl(
         KioskBrowserWindowType::kOpenedRegularBrowser);
     LOG(WARNING)
         << "Open additional fullscreen browser window in kiosk session";
-    chrome::ToggleFullscreenMode(browser, /*user_initiated=*/false);
+    ash::BrowserController::GetInstance()->GetDelegate(browser)->SetFullscreen(
+        true);
     on_browser_window_added_callback_.Run(/*is_closing=*/false);
     return WINDOW_ALLOWED;
   }
