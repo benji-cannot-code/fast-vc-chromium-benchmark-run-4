0ee52a131ca4991368ef1d9037dba5beaf7455c9
Marke Hallowell <wry@chromium.org>
2026-08-17 17:24:55 -0700

    [GlicInvoke] Migrate profile picker off prevent_close.
    Bug: b:540047670
    Change-Id: Ie05dfda00a1886e713f86302e593d9ec6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8216149
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680930}

--- chrome/browser/glic/glic_profile_manager_browsertest.cc
+++ chrome/browser/glic/glic_profile_manager_browsertest.cc
@@ -567,7 +567,7 @@ IN_PROC_BROWSER_TEST_F(GlicProfileManagerDidSelectProfileTest,
   auto* service = GetMockGlicKeyedService(profile);
 
   EXPECT_CALL(*service,
-              ToggleUI(nullptr, true, mojom::InvocationSource::kProfilePicker));
+              ShowUI(nullptr, mojom::InvocationSource::kProfilePicker));
 
   GlicProfileManager::GetInstance()->DidSelectProfile(profile);
 }
@@ -587,8 +587,8 @@ IN_PROC_BROWSER_TEST_F(GlicProfileManagerDidSelectProfileTest,
 
   auto* service = GetMockGlicKeyedService(profile);
 
-  EXPECT_CALL(*service, ToggleUI(testing::IsNull(), true,
-                                 mojom::InvocationSource::kProfilePicker));
+  EXPECT_CALL(*service, ShowUI(testing::IsNull(),
+                               mojom::InvocationSource::kProfilePicker));
 
   GlicProfileManager::GetInstance()->DidSelectProfile(profile);
 }
