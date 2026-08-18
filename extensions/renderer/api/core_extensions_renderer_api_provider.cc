bcba85b80c373c973566952364622dd54dd5ce5b
Dave Vandyke <kzar@chromium.org>
2026-08-17 17:31:25 -0700

    Add the chrome.publicSuffix extensions API
    Browser extensions quite often need to check the eTLD+1 (aka
    "registrable domain") for a given domain name. For example, content
    blocking extensions might need to differentiate between first-party and
    third-party requests. Currently, extension developers need to bundle
    Public Suffix List (PSL)[1] and lookup logic in their extension, which
    is unergonomic and inefficient since the browser already has all of that
    available internally.
    
    Let's add the chrome.publicSuffix extensions API therefore, that allows
    extensions to more easily check the PSL. This follows the WECG API
    proposal[2], see also the related WECG issue[3].
    
    1 - https://publicsuffix.org/list/
    2 - https://github.com/w3c/webextensions/blob/main/proposals/public-suffix.md
    3 - https://github.com/w3c/webextensions/issues/231
    
    Bug: 40740063
    Change-Id: I3c8205ac02dbb5cba01b1dd1a85f849e26202994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871095
    Commit-Queue: Dave Vandyke <kzar@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1680941}

--- extensions/renderer/api/core_extensions_renderer_api_provider.cc
+++ extensions/renderer/api/core_extensions_renderer_api_provider.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/renderer/api/file_system_natives.h"
 #include "extensions/renderer/api/i18n_hooks_delegate.h"
 #include "extensions/renderer/api/messaging/messaging_bindings.h"
+#include "extensions/renderer/api/public_suffix_hooks_delegate.h"
 #include "extensions/renderer/api/runtime_hooks_delegate.h"
 #include "extensions/renderer/api/web_request_hooks.h"
 #include "extensions/renderer/api/web_request_natives.h"
@@ -155,6 +156,8 @@ void CoreExtensionsRendererAPIProvider::AddBindingsSystemHooks(
   bindings->RegisterHooksDelegate("dom", std::make_unique<DOMHooksDelegate>());
   bindings->RegisterHooksDelegate("i18n",
                                   std::make_unique<I18nHooksDelegate>());
+  bindings->RegisterHooksDelegate(
+      "publicSuffix", std::make_unique<PublicSuffixHooksDelegate>());
   bindings->RegisterHooksDelegate("runtime",
                                   std::make_unique<RuntimeHooksDelegate>(
                                       bindings_system->messaging_service()));
