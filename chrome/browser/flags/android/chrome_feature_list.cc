98d03ef90e22d9d7034ae94737f97d438d7b61ad
Eleanor Lee <eleanorlee@google.com>
2026-08-18 01:36:56 -0700

    [Community Contributions] Clean up EscCancelDrag
    Bug: 441212738
    Change-Id: Ib1a5d860b17af30de83c8cf66e1c9531424cbb72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253751
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Fuhsin Liao <fuhsinliao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681197}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -383,7 +383,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kEnableToolbarPositioningInResizeMode,
     &kEnableXAxisActivityTransition,
     &kEnforceIncognitoIsolation,
-    &kEscCancelDrag,
     &kExperimentsForAgsa,
     &kFaviconDisableHostFallback,
     &kFlyoutInBookmarksBar,
@@ -790,7 +789,6 @@ BASE_FEATURE(kEnableSwipeToSwitchPane, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableToolbarPositioningInResizeMode, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableXAxisActivityTransition, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnforceIncognitoIsolation, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kEscCancelDrag, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kExperimentsForAgsa, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kFaviconDisableHostFallback, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kFlyoutInBookmarksBar, base::FEATURE_DISABLED_BY_DEFAULT);
