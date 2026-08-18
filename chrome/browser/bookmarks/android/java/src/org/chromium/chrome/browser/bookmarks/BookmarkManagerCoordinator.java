72b6e87397002944bc8099b814517a7c5f6624f6
Brandon Wylie <wylieb@google.com>
2026-08-18 16:03:32 -0700

    [bookmarks] Make WIDE_DISPLAY_THRESHOLD_DP public in BookmarkUtils
    - Move the threshold definition to `BookmarkUtils.java` and declare it explicitly as `public static final int WIDE_DISPLAY_THRESHOLD_DP = 840;`.
    - Update `BookmarkManagerCoordinator.java` to use the new `BookmarkUtils.WIDE_DISPLAY_THRESHOLD_DP` constant.
    
    Bug: 546178676
    Change-Id: I6ba07dab6e2eb8d771cc685f133dfed182175035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265785
    Reviewed-by: Evan Luo <evanluo@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681797}

--- chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
+++ chrome/browser/bookmarks/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
@@ -87,9 +87,6 @@ import java.util.function.Supplier;
 @NullMarked
 public class BookmarkManagerCoordinator
         implements SearchDelegate, BackPressHandler, OnAttachStateChangeListener {
-
-    private static final int WIDE_DISPLAY_THRESHOLD_DP = 840;
-
     private final SelectionDelegate<BookmarkId> mSelectionDelegate =
             new SelectionDelegate<>() {
                 @Override
@@ -721,7 +718,9 @@ public class BookmarkManagerCoordinator
         View navigationPane = mMainView.findViewById(R.id.navigation_pane);
         if (navigationPane != null) {
             navigationPane.setVisibility(
-                    config.screenWidthDp < WIDE_DISPLAY_THRESHOLD_DP ? View.GONE : View.VISIBLE);
+                    config.screenWidthDp < BookmarkUtils.WIDE_DISPLAY_THRESHOLD_DP
+                            ? View.GONE
+                            : View.VISIBLE);
         }
     }
 
