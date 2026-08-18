ba44f101eb335c0e65ff00d2a48bdebd063b4979
Andy Phan <andyphan@chromium.org>
2026-08-17 18:57:20 -0700

    [PDF Ink Signatures] Add operator== for InkTextBoxAttributes
    Add equality operator and copy support for InkTextBoxAttributes and
    migrate test assertions to compare attributes directly. Then remove
    MATCHER_P10 in favor of using operator==.
    
    GMock only supports up to MATCHER_P10. InkTextBoxAttributes currently
    has 10 fields, so adding future attributes (like strikethrough) would
    exceed this limit. Defining operator== allows tests to compare
    attributes directly.
    
    TAG=agy
    CONV=f97f0674-ca92-4b13-b50c-fafdcfb0050e
    
    Bug: 541374953
    Change-Id: I6a2863dceb0e9929024418677a0e0b66fbfa366a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8244727
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681023}

--- pdf/pdf_ink_text.h
+++ pdf/pdf_ink_text.h
@@ -56,6 +56,8 @@ std::string TextTypefaceToString(TextTypeface typeface);
 std::string TextAlignmentToString(TextAlignment alignment);
 
 struct InkTextBoxAttributes {
+  bool operator==(const InkTextBoxAttributes& other) const = default;
+
   // `rect` is in CSS screen coordinates.
   gfx::RectF rect;
   SkColor color;
