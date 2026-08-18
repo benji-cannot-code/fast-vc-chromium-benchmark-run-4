80e394f640e563804070913d4039b86dc9dadc05
Kevin McNee <mcnee@chromium.org>
2026-08-17 19:43:02 -0700

    Dictation: Replace overlay placeholder a11y text with proper resources
    We have two buttons in the overlay. One is a microphone image button
    that starts dictation and the other is a clickable waveform that stops
    the mic. We now use proper resources for the strings for the accessible
    names of these buttons and update the strings to describe what the
    buttons do.
    
    Bug: b:525859277
    Change-Id: Ie54d8720269f32c078ca7f483bfc8c20460269b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257341
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681047}

--- chrome/browser/ui/views/dictation/waveform_view_button.cc
+++ chrome/browser/ui/views/dictation/waveform_view_button.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "chrome/grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/views/layout/fill_layout.h"
 
@@ -14,7 +16,8 @@ namespace dictation {
 
 WaveformViewButton::WaveformViewButton(bool full_size, PressedCallback callback)
     : views::Button(std::move(callback)) {
-  SetAccessibleName(u"Dictation");
+  SetAccessibleName(
+      l10n_util::GetStringUTF16(IDS_DICTATION_ACCNAME_OVERLAY_WAVEFORM_BUTTON));
   SetLayoutManager(std::make_unique<views::FillLayout>());
   auto waveform_view = std::make_unique<WaveformView>(full_size);
   waveform_view->SetCanProcessEventsWithinSubtree(false);
