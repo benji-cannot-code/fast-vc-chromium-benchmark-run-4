aa9e8276b879943aacc26dfa2ef95081426d0ea4
Lukasz Anforowicz <lukasza@chromium.org>
2026-08-18 09:07:15 -0700

    Use locale-independent sorting order in `update...lang...sh`.
    This CL modifies
    `infra/update_inclusive_language_presubmit_exempt_dirs.sh` to use
    locale-independent (more hermetic and stable-over-time) sorting order.
    It also uses the new order to rebaseline/sort the corresponding
    `infra/inclusive_language_presubmit_exempt_dirs.txt` file.
    
    Change-Id: I5fc8e8812f00c30580ea44afd187d00ea11b3538
    Bug: 548436743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267309
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681442}

--- infra/update_inclusive_language_presubmit_exempt_dirs.sh
+++ infra/update_inclusive_language_presubmit_exempt_dirs.sh
@@ -28,4 +28,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 git grep --ignore-case -l -E '\b((black|white)list|master|slave)\b' | \
   xargs -n1 dirname | \
-  sort -u
+  LC_ALL=C sort -u
