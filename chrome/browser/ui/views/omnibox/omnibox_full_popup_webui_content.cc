3945d9e58fcb5199f045d8855e266b5d087f325e
Maks Orlovich <morlovich@chromium.org>
2026-08-18 12:35:57 -0700

    WebUILocationBar and WebUIFullPopup: fix dangling ptr abort on double context-menu
    Ctrl-Clicking the location bar with either impl on Mac (at least) twice
    would cause abort due to dangling pointer from previous runner to
    being-replaced model.
    
    Discovered by being bad at Mac.
    
    Bug: 469743571
    Change-Id: Iadc8ed9b65ec6a492825f13274a0d6f87389d7d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264082
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681585}

--- chrome/browser/ui/views/omnibox/omnibox_full_popup_webui_content.cc
+++ chrome/browser/ui/views/omnibox/omnibox_full_popup_webui_content.cc
@@ -110,6 +110,7 @@ void OmniboxFullPopupWebUIContent::ShowContextMenuComplete(
     const content::ContextMenuParams& params) {
   params_ = params;
 
+  menu_runner_.reset();
   menu_model_ = std::make_unique<ui::SimpleMenuModel>(this);
 
   content::WebContents* web_contents = GetWebContents();
