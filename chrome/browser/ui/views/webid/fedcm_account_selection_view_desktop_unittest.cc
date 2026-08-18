93a74ea6a77b31ae8bb8bffcb349d77cf5a2722a
Wenbin Zhang <wenbinzhang@google.com>
2026-08-17 18:39:36 -0700

    [bedrock] Remove Browser* dependencies from four desktop unit test suites
    Refactors unit tests across four desktop suites to remove Browser* and
    TestBrowserWindow dependencies for Project Bedrock:
    1. c/b/ui/views/bookmarks/bookmark_bar_view_unittest.cc:
        - Decouples BookmarkBarViewWithCounter from Browser and
          TestBrowserWindow using MockBrowserWindowInterface.
        - Updates constructor to accept BrowserWindowInterface*.
        - Prunes unused browser.h, create_browser_window.h, and
          test_browser_window.h.
    2. c/b/apps/app_shim/app_shim_manager_mac_unittest.cc:
        - Replaces TestBrowserWindowShow (subclassing TestBrowserWindow)
          with TestBaseWindowShow directly implementing ui::BaseWindow.
        - Prunes unused test_browser_window.h.
    3. c/b/ui/views/site_data/page_specific_site_data_dialog_unittest.cc:
        - Removes orphaned browser_with_test_window_test.h include.
    4. c/b/ui/views/webid/fedcm_account_selection_view_desktop_unittest.cc:
        - Removes orphaned browser_with_test_window_test.h include.
    
    TAG=agy
    CONV=a89fc9fb-6826-4862-9b08-55f05a3a432a
    
    Bug: 543043111
    Change-Id: I1b6229d9afb70aef910cfd0340be3ccd2f801c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259557
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681012}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop_unittest.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop_unittest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/webid/account_selection_view_test_base.h"
 #include "chrome/browser/ui/webid/account_selection_view.h"
 #include "chrome/browser/ui/webid/identity_ui_utils.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "components/constrained_window/constrained_window_views.h"
