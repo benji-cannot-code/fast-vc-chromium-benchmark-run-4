5ff391f76d2931dd0c5f3090d7ee576f77207e92
Wez <wez@chromium.org>
2026-08-18 09:11:45 -0700

    [fuchsia][cast] Add SetOrigin API to ApiBindings FIDL
    Adds ApiBindings.SetOrigin() to allow CastRunner to provide the
    requesting document origin to the Cast Agent when connecting API
    message ports.
    
    TAG: agy
    CONV: e281dc0e-bc02-4dc6-966a-3734aa65160d
    Bug: 526640074
    Change-Id: I82cbe492d645404287d62ceebbaf249aea729bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256491
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681447}

--- chromecast/bindings/bindings_manager_fuchsia.cc
+++ chromecast/bindings/bindings_manager_fuchsia.cc
@@ -58,5 +58,9 @@ void BindingsManagerFuchsia::Connect(
                                      std::move(message_port))));
 }
 
+void BindingsManagerFuchsia::SetOrigin(std::string origin) {
+  Manager::SetOrigin(std::move(origin));
+}
+
 }  // namespace bindings
 }  // namespace chromecast
