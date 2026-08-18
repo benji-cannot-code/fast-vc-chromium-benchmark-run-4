05deeb2c1e326c420b2100e891832fa4242be63e
Rodney Ding <rodneyding@google.com>
2026-08-18 13:29:06 -0700

    [Indigo] Expose isReplacedByUserAgent to content script
    Demo: http://screencast/cast/NjE1NzQyNTkyNDExMjM4NHwxMmIxZDk1Yi1hNQ
    
    - Added a blink public interface API because DEPS requirement
    - isReplacedByUserAgent is isolated in content script world
    
    Bug: b:542678270
    Change-Id: I1dca634b81567fae3582e187acd6f40557c0b4ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253859
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681626}

--- third_party/blink/renderer/core/image_replacement/image_replacement_test.cc
+++ third_party/blink/renderer/core/image_replacement/image_replacement_test.cc
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/image_replacement/image_replacement.mojom-blink.h"
+#include "third_party/blink/public/web/web_element.h"
 #include "third_party/blink/public/web/web_frame.h"
+#include "third_party/blink/public/web/web_image_replacement.h"
 #include "third_party/blink/public/web/web_script_source.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/native_event_listener.h"
@@ -203,6 +205,8 @@ TEST_F(ImageReplacementSimTest, ImageReplacementLifecycle) {
   ASSERT_TRUE(img->complete());
   ASSERT_TRUE(img->GetLayoutObject());
   EXPECT_TRUE(img->GetLayoutObject()->IsLayoutImage());
+  EXPECT_FALSE(img->replacedByUserAgent());
+  EXPECT_FALSE(WebImageReplacement::IsReplacedByUserAgent(WebElement(img)));
 
   auto result = ImageReplacement::CreateAndBindReceiver(*img);
   ASSERT_TRUE(result.has_value());
@@ -217,6 +221,8 @@ TEST_F(ImageReplacementSimTest, ImageReplacementLifecycle) {
 
   // Verify replacement state
   EXPECT_TRUE(img->GetLayoutObject()->IsLayoutImageReplacement());
+  EXPECT_TRUE(img->replacedByUserAgent());
+  EXPECT_TRUE(WebImageReplacement::IsReplacedByUserAgent(WebElement(img)));
   ASSERT_TRUE(img->UserAgentShadowRoot());
 
   auto* iframe =
@@ -240,6 +246,8 @@ TEST_F(ImageReplacementSimTest, ImageReplacementLifecycle) {
   Compositor().BeginFrame();
 
   EXPECT_TRUE(img->GetLayoutObject()->IsLayoutImage());
+  EXPECT_FALSE(img->replacedByUserAgent());
+  EXPECT_FALSE(WebImageReplacement::IsReplacedByUserAgent(WebElement(img)));
   EXPECT_FALSE(img->UserAgentShadowRoot()->HasChildren());
   EXPECT_FALSE(DocumentImageReplacements::FromIfExists(GetDocument())
                    ->GetImageReplacement(img));
