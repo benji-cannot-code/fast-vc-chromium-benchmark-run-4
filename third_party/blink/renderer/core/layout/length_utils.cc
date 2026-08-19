38aa5436f60dd212d4eee71068e8b3dd3956d5c1
Celeste Pan <celestepan@microsoft.com>
2026-08-19 10:17:41 -0700

    [Masonry] Use grid-axis content box size to resolve percentages in flow-tolerance
    We were previously using the stacking axis size of the content box to
    resolve percentages for the flow-tolerance feature, but per spec [1],
    flow-tolerance percentages should resolve "relative to the grid-axis
    content box size of the grid lanes container".
    
    [1] https://drafts.csswg.org/css-grid-3/#placement-tolerance
    
    Bug: 343257585, 517590298
    Change-Id: I68c7c9c42f80694e7b28a3602cb2882519db958f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252957
    Reviewed-by: Yanling Wang <yanlingwang@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Celeste Pan <celestepan@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1682431}

--- third_party/blink/renderer/core/layout/length_utils.cc
+++ third_party/blink/renderer/core/layout/length_utils.cc
@@ -1393,8 +1393,8 @@ LayoutUnit ResolveFlowToleranceForGridLanes(const ComputedStyle& style,
                                             const LogicalSize& available_size) {
   return ResolveFlowToleranceLength(
              style, (style.GridLanesTrackSizingDirection() == kForColumns)
-                        ? available_size.block_size
-                        : available_size.inline_size)
+                        ? available_size.inline_size
+                        : available_size.block_size)
       .value_or(LayoutUnit(style.GetFontDescription().ComputedPixelSize()));
 }
 
