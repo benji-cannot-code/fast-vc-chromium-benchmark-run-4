fa93a31019483efe897874bdd290f15456ac5355
Nina Satragno <nsatragno@chromium.org>
2026-08-19 08:18:37 -0700

    [crypto] Add support for exporting ML-DSA to COSE
    WebAuthn exports public keys in the COSE format. Add support for the
    ML-DSA family of public keys.
    
    Bug: 546370268
    Change-Id: I1652cb655230515d1294c4744705ab0cdde7e1a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260895
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682327}

--- crypto/cose.cc
+++ crypto/cose.cc
@@ -32,14 +32,17 @@ enum class CoseKeyKey : int {
   kEllipticCurve = -1,
   kEllipticX = -2,
   kEllipticY = -3,
+  kAkpPublicKey = -1,
 };
 
 // Enumerates COSE key types. See
-// https://tools.ietf.org/html/rfc8152#section-13
+// https://tools.ietf.org/html/rfc8152#section-13 and
+// https://tools.ietf.org/html/rfc9964#name-new-cose-key-types.
 enum class CoseKeyTypes : int {
   kOKP = 1,
   kEC2 = 2,
   kRSA = 3,
+  kAKP = 7,
 };
 
 // Enumerates COSE elliptic curves. See
@@ -49,9 +52,15 @@ enum class CoseCurves : int {
   kEd25519 = 6,
 };
 
+// Enumerates COSE algorithm identifiers. See
+// https://tools.ietf.org/html/rfc8152#section-13 and
+// https://tools.ietf.org/html/rfc9964#section-8.1.
 enum class CoseAlgorithmIdentifier : int {
   kEs256 = -7,
   kEdDSA = -8,
+  kMlDsa44 = -48,
+  kMlDsa65 = -49,
+  kMlDsa87 = -50,
   kRs256 = -257,
 };
 
@@ -111,6 +120,22 @@ std::vector<uint8_t> EcP256ToCoseKey(const keypair::PublicKey& key) {
   return cbor_bytes.value();
 }
 
+std::vector<uint8_t> MldsaToCoseKey(std::vector<uint8_t> pub_bytes,
+                                    CoseAlgorithmIdentifier alg) {
+  cbor::Value::MapValue map;
+  map.emplace(static_cast<int64_t>(CoseKeyKey::kKty),
+              static_cast<int64_t>(CoseKeyTypes::kAKP));
+  map.emplace(static_cast<int64_t>(CoseKeyKey::kAlg),
+              static_cast<int64_t>(alg));
+  map.emplace(static_cast<int64_t>(CoseKeyKey::kAkpPublicKey),
+              std::move(pub_bytes));
+
+  std::optional<std::vector<uint8_t>> cbor_bytes =
+      cbor::Writer::Write(cbor::Value(std::move(map)));
+  CHECK(cbor_bytes);
+  return cbor_bytes.value();
+}
+
 }  // namespace
 
 std::vector<uint8_t> PublicKeyToCoseKey(const keypair::PublicKey& key) {
@@ -118,6 +143,15 @@ std::vector<uint8_t> PublicKeyToCoseKey(const keypair::PublicKey& key) {
     return RsaToCoseKey(key.key());
   } else if (key.IsEcP256()) {
     return EcP256ToCoseKey(key);
+  } else if (key.IsMldsa44()) {
+    return MldsaToCoseKey(key.ToMldsa44PublicKey(),
+                          CoseAlgorithmIdentifier::kMlDsa44);
+  } else if (key.IsMldsa65()) {
+    return MldsaToCoseKey(key.ToMldsa65PublicKey(),
+                          CoseAlgorithmIdentifier::kMlDsa65);
+  } else if (key.IsMldsa87()) {
+    return MldsaToCoseKey(key.ToMldsa87PublicKey(),
+                          CoseAlgorithmIdentifier::kMlDsa87);
   }
 
   NOTREACHED();
--- crypto/cose.h
+++ crypto/cose.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crypto {
 
 // Converts a PublicKey in |key| to a COSE_Key structure, returning the
-// serialized CBOR bytes. Currently, we only support keys using the RSA and
-// EC-P256 algorithms.
+// serialized CBOR bytes. Currently, we support keys using the RSA, EC-P256, and
+// ML-DSA algorithms.
 CRYPTO_EXPORT std::vector<uint8_t> PublicKeyToCoseKey(
     const keypair::PublicKey& key);
 
