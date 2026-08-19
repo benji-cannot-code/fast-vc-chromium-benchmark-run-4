2ad3f22ff4841bffbb830f0a126260b349d13a39
Kent Tamura <tkent@chromium.org>
2026-08-18 22:47:16 -0700

    WTF: Remove StringBuilder::AppendFormat()
    Removes AppendFormat() in favor of modern, type-safe alternatives such
    as FormatTo(), Format(), and StrCat().
    
    AppendFormat() is being retired because:
    1. It lacks type safety (uses varargs).
    2. It requires converting Blink Strings to C-strings (e.g., via
       .Utf8().c_str()).
    3. It only supports Latin1 strings.
    
    Existing call sites are migrated to appropriate replacements
    (FormatTo, Format, StrCat, or AppendNumber). This is a refactoring
    with no intended behavioral changes.
    
    Bug: 542260133
    Change-Id: Ibb3c3a53b29b53400c5829bb0c4d33b18ced1544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264147
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682048}

--- third_party/blink/renderer/core/css/css_selector.cc
+++ third_party/blink/renderer/core/css/css_selector.cc
@@ -1342,13 +1342,15 @@ void CSSSelector::SerializeSimpleSelector(StringBuilder& builder,
           } else if (a == -1) {
             builder.Append("-n");
           } else {
-            builder.AppendFormat("%dn", a);
+            builder.AppendNumber(a);
+            builder.Append('n');
           }
 
           if (b < 0) {
             builder.Append(String::Number(b));
           } else if (b > 0) {
-            builder.AppendFormat("+%d", b);
+            builder.Append('+');
+            builder.AppendNumber(b);
           }
         }
 
--- third_party/blink/renderer/platform/bindings/runtime_call_stats.cc
+++ third_party/blink/renderer/platform/bindings/runtime_call_stats.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/traced_value.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+#include "third_party/blink/renderer/platform/wtf/text/format.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
@@ -117,7 +118,7 @@ void RuntimeCallStats::Dump(TracedValue& value) const {
 }
 
 namespace {
-const char row_format[] = "%-55s  %8" PRIu64 "  %9.3f\n";
+constexpr char kRowFormat[] = "{:55}  {:8}  {:9.3f}\n";
 }
 
 String RuntimeCallStats::ToString() const {
@@ -127,9 +128,8 @@ String RuntimeCallStats::ToString() const {
       "Name                                                    Count     Time "
       "(ms)\n\n");
   for (const auto& counter : counters_) {
-    UNSAFE_TODO(builder.AppendFormat(row_format, counter.GetName(),
-                                     counter.GetCount(),
-                                     counter.GetTime().InMillisecondsF()));
+    FormatTo(builder, kRowFormat, counter.GetName(), counter.GetCount(),
+             counter.GetTime().InMillisecondsF());
   }
 
 #if BUILDFLAG(RCS_COUNT_EVERYTHING)
@@ -183,11 +183,10 @@ Vector<RuntimeCallCounter*> RuntimeCallStats::CounterMapToSortedArray() const {
 
 void RuntimeCallStats::AddCounterMapStatsToBuilder(
     StringBuilder& builder) const {
-  builder.AppendFormat("\nNumber of counters in map: %u\n\n",
-                       counter_map_.size());
+  FormatTo(builder, "\nNumber of counters in map: {}\n\n", counter_map_.size());
   for (RuntimeCallCounter* counter : CounterMapToSortedArray()) {
-    builder.AppendFormat(row_format, counter->GetName(), counter->GetCount(),
-                         counter->GetTime().InMillisecondsF());
+    FormatTo(builder, kRowFormat, counter->GetName(), counter->GetCount(),
+             counter->GetTime().InMillisecondsF());
   }
 }
 #endif
