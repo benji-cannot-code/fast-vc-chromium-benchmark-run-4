2d49bc5bb9a0da1e2ab11baf03cbbf017f77a878
Fuhsin Liao <fuhsinliao@google.com>
2026-08-19 00:08:38 -0700

    Disable testPanelWillOpenBeforeClientReady test in NewGlicApiTest
    Disable the test due to merge conflict of the culprit cl
    crrev.com/c/8267043
    
    Bug: 548845985
    Change-Id: Ibdaa3fc26a2f85e24db5be66b03077fd1c562868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259996
    Reviewed-by: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Masa Fujita <massan@google.com>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: ben chin <lungc@chromium.org>
    Commit-Queue: Masa Fujita <massan@google.com>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Auto-Submit: Fuhsin Liao <fuhsinliao@google.com>
    Owners-Override: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682084}

--- chrome/browser/glic/host/new_glic_api_browsertest.cc
+++ chrome/browser/glic/host/new_glic_api_browsertest.cc
@@ -3012,7 +3012,9 @@ IN_PROC_BROWSER_TEST_P(NewGlicApiTestWithFastTimeout,
 #endif
 }
 
-IN_PROC_BROWSER_TEST_P(NewGlicApiTest, testPanelWillOpenBeforeClientReady) {
+// TODO(crbug.com/548845985): Re-enable this test after the bug fix.
+IN_PROC_BROWSER_TEST_P(NewGlicApiTest,
+                       DISABLED_testPanelWillOpenBeforeClientReady) {
   ToggleGlicForActiveTab(/*prevent_close=*/true);
   auto* instance = GetOnlyGlicInstance();
   ASSERT_TRUE(instance);
