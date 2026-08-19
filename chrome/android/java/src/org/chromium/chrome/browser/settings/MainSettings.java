b6b94a8b30a193e16aefdc872908398e05fa2868
Josef Raska <josefraska@google.com>
2026-08-19 07:36:42 -0700

    [Autofill][Android] Fix children indexing of Autofill Options
    The children preferences of AutofillOptionsFragment are not searchable
    if the search index does not see an enabled parent with a fragment set.
    We need to set parent fragment in both possible paths: If
    YOUR_SAVED_INFO_SETTINGS_PAGE_ANDROID flag is enabled/disabled.
    
    "Smarter form understanding" was used as an example children toggle.
    Updating its search title was fixed on the way.
    
    Bug video:
    https://screencast.googleplex.com/cast/NjYyODU3OTc4NDc4NTkyMHxhMjBiYzcyOC0zYg
    
    Fix video:
    https://screencast.googleplex.com/cast/NjMzMzc3ODg5MTg5ODg4MHw2Y2NhODA2YS1hYQ
    
    Bug: 546080698
    Change-Id: I0cc98b01722ecdeade69f2fc79163cd4ba430e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257575
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Josef Raska <josefraska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682283}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -775,9 +775,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
             assumeNonNull(activity);
             showDefaultBrowserSettings(activity);
             return false;
-        } else if (key.equals(PREF_AUTOFILL_OPTIONS)) {
-            openAutofillOptions(context);
-            return false;
         }
         // TODO(crbug.com/469676538): Handle the rest of preferences.
         return false;
@@ -1050,8 +1047,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
                     } else {
                         indexData.removeEntry(getUniqueId(PREF_AUTOFILL_AND_PASSWORDS));
 
-                        // TODO(crbug.com/440022435): Remove the PREF_AUTOFILL_OPTIONS index update
-                        // once Autofill AI is launched.
                         String autofillOptionsEntryId = getUniqueId(PREF_AUTOFILL_OPTIONS);
                         SettingsIndexData.Entry autofillOptionsEntry =
                                 indexData.getEntry(autofillOptionsEntryId);
@@ -1059,9 +1054,16 @@ public class MainSettings extends ChromeBaseSettingsFragment
                             indexData.updateEntry(
                                     autofillOptionsEntryId,
                                     new SettingsIndexData.Entry.Builder(autofillOptionsEntry)
+                                            // TODO(crbug.com/440022435): Remove the
+                                            // PREF_AUTOFILL_OPTIONS title index update
+                                            // once Autofill AI is launched.
                                             .setTitle(
                                                     AutofillOptionsMediator.getFragmentTitle(
                                                             context))
+                                            .setFragment(AutofillOptionsFragment.class.getName())
+                                            .setArguments(
+                                                    AutofillOptionsFragment.createRequiredArgs(
+                                                            AutofillOptionsReferrer.SETTINGS))
                                             .build());
                         }
                     }
