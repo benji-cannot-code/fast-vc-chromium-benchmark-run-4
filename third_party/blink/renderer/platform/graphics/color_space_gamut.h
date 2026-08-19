ae36d35d11e8d651b60c9d903c01a197727491c3
Christopher Cameron <ccameron@google.com>
2026-08-19 13:28:19 -0700

    MediaValuesDynamic::ColorGamut: Use better computation
    Remove all values except SRGB, P3, and BT2020, because those are the
    only ones surfaced, and the other values are no longer histograms.
    
    Remove the ad-hoc computation done in GetColorSpaceGamut and replace it
    with checks to see if the color space covers 90% of the P3 and BT2020
    gamuts.
    
    Add more test cases fo mq-color-gamut-api. One sub-test of note is that
    Chromium would previously claim that AdobeRGB gamut covers the P3 gamut,
    which is not the case.
    
    Update TestRunnerBindings::SetColorProfile to support many more color
    profiles for testing, and stop using hard-coded ICC profiles for several
    of the test cases.
    
    This is towards removing use of skcms from Chromium.
    
    Bug: 540759552
    Change-Id: I579139999c95b5bf8954b18ce33e063fb19ca7e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252298
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682582}

--- third_party/blink/renderer/platform/graphics/color_space_gamut.h
+++ third_party/blink/renderer/platform/graphics/color_space_gamut.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/platform_export.h"
 
-struct skcms_ICCProfile;
-
 namespace display {
 struct ScreenInfo;
 }
@@ -17,25 +15,14 @@ struct ScreenInfo;
 namespace blink {
 
 enum class ColorSpaceGamut {
-  // Values synced with 'Gamut' in src/tools/metrics/histograms/histograms.xml
-  kUnknown = 0,
-  kLessThanNTSC = 1,
-  NTSC = 2,
   SRGB = 3,
-  kAlmostP3 = 4,
   P3 = 5,
-  kAdobeRGB = 6,
-  kWide = 7,
   BT2020 = 8,
-  kProPhoto = 9,
-  kUltraWide = 10,
-  kEnd
 };
 
 namespace color_space_utilities {
 
 PLATFORM_EXPORT ColorSpaceGamut GetColorSpaceGamut(const display::ScreenInfo&);
-ColorSpaceGamut GetColorSpaceGamut(const skcms_ICCProfile*);
 
 }  // namespace color_space_utilities
 
