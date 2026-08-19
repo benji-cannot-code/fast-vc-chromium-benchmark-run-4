df4e35e453a9ed1f0161d69f94eb54b47df07444
Bryant Chandler <bryantchandler@google.com>
2026-08-19 10:34:11 -0700

    [glic] Remove legacy testNotifyActOnWebCapabilityChanged from glic_api_browsertest
    Bug: b:477918640
    Change-Id: I03afda996405929be631dfdedc1086b56a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260937
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682451}

--- chrome/test/data/webui/glic/browser_tests/glic_api_browsertest.ts
+++ chrome/test/data/webui/glic/browser_tests/glic_api_browsertest.ts
@@ -313,15 +313,6 @@ class ApiTests extends ApiTestFixtureBase {
     }
   }
 
-  async testNotifyActOnWebCapabilityChanged() {
-    assertDefined(this.host.getActOnWebCapability);
-    const actOnWebCapabilitySequence =
-        observeSequence(this.host.getActOnWebCapability());
-    await actOnWebCapabilitySequence.waitForValue(true);
-    await this.advanceToNextStep();
-    await actOnWebCapabilitySequence.waitForValue(false);
-  }
-
 
   async testPanelWillOpenHasRecentlyActiveConversations() {
     assertDefined(this.host.registerConversation);
