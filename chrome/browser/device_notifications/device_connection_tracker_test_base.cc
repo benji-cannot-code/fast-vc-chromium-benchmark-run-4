f34738fea95ac93342ca728cbe31c705c6485eaf
Farid <faridzad@google.com>
2026-08-18 17:15:42 -0700

    Bedrock: Migrate device_notifications to BrowserWindowInterface
    Migrate device_notifications component from Browser to
    BrowserWindowInterface.
    
    - Replaced chrome/browser/ui/browser.h include with
      browser_window_interface.h in
      device_connection_tracker_test_base.cc.
    - Added //chrome/browser/ui/browser_window to deps in
      chrome/browser/device_notifications/BUILD.gn.
    
    TAG=agy
    CONV=0235b6be-3711-4353-b9cf-f9603fbf6ded
    
    Bug: 496674143
    Change-Id: I33d1280e2e383fc8cafaf7e0ab69da12c0131930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259000
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681869}

--- chrome/browser/device_notifications/device_connection_tracker_test_base.cc
+++ chrome/browser/device_notifications/device_connection_tracker_test_base.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/delete_profile_helper.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_test_util.h"
-#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/test/base/profile_deletion_observer.h"
 #include "chrome/test/base/ui_test_utils.h"
 
