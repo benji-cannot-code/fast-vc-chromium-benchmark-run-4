3041273a78a3b37a5ac6e2f49ef94eea3b0bbe67
Dana Fried <dfried@chromium.org>
2026-08-19 09:30:04 -0700

    [User Education] Mock New Badge Controller
    This is a prerequisite for testing the logic around new WebUI API for
    User Education.
    
    Bug: 546619486, 361169212, 543113387
    Change-Id: I9ca164f72c7ebc887ccdda0117eb0be486069839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258002
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682391}

---
+++ components/user_education/test/mock_new_badge_controller.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/user_education/test/mock_new_badge_controller.h"
+
+namespace user_education::test {
+
+MockNewBadgeController::MockNewBadgeController() = default;
+MockNewBadgeController::~MockNewBadgeController() = default;
+
+}  // namespace user_education::test
