54457d6e4450c629db130254321ca596ed416c50
Alex Kallam <aakallam@chromium.org>
2026-08-19 16:43:11 -0700

    Roll ref-cast: 1.0.26 => 1.0.27 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * ref-cast-impl: 1.0.26 => 1.0.27;
      https://docs.rs/crate/ref-cast-impl/1.0.27
    * ref-cast: 1.0.26 => 1.0.27; https://docs.rs/crate/ref-cast/1.0.27
    
    Bug: None
    Change-Id: I20af1e3aeb17738dfcdec24b4b090aae10d41e94
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258344
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682728}

--- third_party/rust/chromium_crates_io/vendor/ref-cast-v1/tests/test_custom.rs
+++ third_party/rust/chromium_crates_io/vendor/ref-cast-v1/tests/test_custom.rs
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#![deny(clippy::let_underscore_must_use)]
+
 #[forbid(unsafe_code)]
 mod forbid_unsafe {
     use ref_cast::{ref_cast_custom, RefCastCustom};
