2e990629960856374cca4444ea3837b3dbd2e091
Ivan Kabadzhov <ikabadzhov@google.com>
2026-08-19 11:16:16 -0700

    Backend support for "Write a review" on chrome://extensions/ page
    This CL introduces the developerPrivate.openReviewPage extension API
    function, which allows navigating the user to the Chrome Web Store
    review page for a specified extension. The function validates that the
    extension exists and is eligible for review prompts, then opens the
    Chrome Web Store review URL in a new foreground tab. This change also
    includes unit and browser tests for the new function and registers the
    corresponding histogram value.
    
    Bug: b:535485215
    Change-Id: I615d5a40601889bf2119fac28e64746f51fe413b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8214063
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Ivan Kabadzhov <ikabadzhov@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682485}

--- chrome/browser/extensions/api/developer_private/developer_private_functions.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions.h
@@ -248,6 +248,17 @@ class DeveloperPrivateUpdateExtensionConfigurationFunction
   ResponseAction Run() override;
 };
 
+class DeveloperPrivateOpenReviewPageFunction
+    : public DeveloperPrivateAPIFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("developerPrivate.openReviewPage",
+                             DEVELOPERPRIVATE_OPENREVIEWPAGE)
+
+ protected:
+  ~DeveloperPrivateOpenReviewPageFunction() override;
+  ResponseAction Run() override;
+};
+
 class DeveloperPrivateReloadFunction : public DeveloperPrivateAPIFunction,
                                        public ExtensionRegistryObserver,
                                        public LoadErrorReporter::Observer {
