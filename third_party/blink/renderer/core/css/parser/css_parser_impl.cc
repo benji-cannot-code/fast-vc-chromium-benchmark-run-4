8b0b5d11a77304f0008cf63e487dbe50052d0473
Kurt Catti-Schmidt <kschmi@microsoft.com>
2026-08-19 09:43:44 -0700

    [css] Avoid devtools DCHECK with invalid @font-feature-values
    ConsumeFontFeatureValuesRule has an early return nullptr when an invalid
    family name is encountered. This ends up missing the call to
    observer_->EndRuleBody(stream.Offset());, leading to:
    
    DCHECK failed: parsed_flat_rules_.size() == source_data_->size() (1 vs.
    0).
    
    The fix is to scan for families before
    observer_->StartRuleBody(stream.Offset()); so it can early-return before
    this.
    
    Bug: 548251130
    Change-Id: Ida65f4ff576faa47e0eea79f00c4b0c73e79a21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263397
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682407}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.cc
@@ -1745,6 +1745,18 @@ StyleRuleFontFeatureValues* CSSParserImpl::ConsumeFontFeatureValuesRule(
     ConsumeErroneousAtRule(stream, CSSAtRuleID::kCSSAtRuleFontFeatureValues);
     return nullptr;
   }
+
+  Vector<AtomicString> families;
+  families.ReserveInitialCapacity(family_list->length());
+  for (const auto& family_entry : *family_list) {
+    const CSSFontFamilyValue* family_value =
+        DynamicTo<CSSFontFamilyValue>(*family_entry);
+    if (!family_value) {
+      ConsumeErroneousAtRule(stream, CSSAtRuleID::kCSSAtRuleFontFeatureValues);
+      return nullptr;
+    }
+    families.push_back(family_value->Value());
+  }
   wtf_size_t prelude_offset_end = stream.LookAheadOffset();
   if (!ConsumeEndOfPreludeForAtRuleWithBlock(
           stream, CSSAtRuleID::kCSSAtRuleFontFeatureValues)) {
@@ -1816,16 +1828,6 @@ StyleRuleFontFeatureValues* CSSParserImpl::ConsumeFontFeatureValuesRule(
           break;
       }
     }
-  }
-
-  Vector<AtomicString> families;
-  for (const auto& family_entry : *family_list) {
-    const CSSFontFamilyValue* family_value =
-        DynamicTo<CSSFontFamilyValue>(*family_entry);
-    if (!family_value) {
-      return nullptr;
-    }
-    families.push_back(family_value->Value());
   }
 
   auto* feature_values_rule = MakeGarbageCollected<StyleRuleFontFeatureValues>(
