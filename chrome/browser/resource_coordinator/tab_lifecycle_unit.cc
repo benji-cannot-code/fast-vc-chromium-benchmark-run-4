416dc3b386fb9321e7b3c1c0abce784585419e75
Joe Mason <joenotcharles@google.com>
2026-08-19 12:14:00 -0700

    Replace KiBU/MiBU/etc with KiB/MiB/etc for chrome/browser/resource_coordinator
    The base::KiBU() family of functions have been deprecated and replaced
    with base::KiB().
    
    Mechanically converted with:
    
    git grep -l base::[KMGTPE]iBU|xargs sed -i
    "s/base::\([KMGTPE]\)iBU/base::\1iB/g"
    
    This CL was uploaded by git cl split.
    
    Bug: 448661443
    Change-Id: I870f6fd6d6797a777d6a6ffb9044867aab9e41c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265459
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682538}

--- chrome/browser/resource_coordinator/tab_lifecycle_unit.cc
+++ chrome/browser/resource_coordinator/tab_lifecycle_unit.cc
@@ -367,7 +367,7 @@ bool TabLifecycleUnitSource::TabLifecycleUnit::Discard(
   const base::TimeTicks discard_start_time = NowTicks();
 
   last_discard_time_ = discard_start_time;
-  last_discard_memory_estimate_ = base::KiBU(tab_memory_footprint_estimate);
+  last_discard_memory_estimate_ = base::KiB(tab_memory_footprint_estimate);
 
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
@@ -464,11 +464,11 @@ void TabLifecycleUnitSource::TabLifecycleUnit::UpdatePreDiscardResourceUsage(
   if (pre_discard_resource_usage == nullptr) {
     performance_manager::user_tuning::UserPerformanceTuningManager::
         PreDiscardResourceUsage::CreateForWebContents(
-            web_contents, base::KiBU(tab_memory_footprint_estimate),
+            web_contents, base::KiB(tab_memory_footprint_estimate),
             discard_reason);
   } else {
     pre_discard_resource_usage->UpdateDiscardInfo(
-        base::KiBU(tab_memory_footprint_estimate), discard_reason);
+        base::KiB(tab_memory_footprint_estimate), discard_reason);
   }
 }
 
