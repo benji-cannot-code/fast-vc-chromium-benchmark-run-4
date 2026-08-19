8b2955453bd7860100238a35f55ba9e5b2980c5b
Saifuddin Hitawala <hitawala@chromium.org>
2026-08-19 10:45:42 -0700

    [viz] Rename OverlayPriority::kLow to kNone in TextureDrawQuad
    Rename OverlayPriority::kLow to kNone in TextureDrawQuad to be clearer
    as quad is not promoted to overlay in such cases. (except for
    DelegatedCompositing on Windows)
    
    Follow-up from crrev.com/c/8261629
    
    Bug: 390568190
    Change-Id: I7475890f7d8ec4fa8c33b0e6c9cf97281bc05393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264281
    Reviewed-by: Michael Tang <tangm@microsoft.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682457}

--- components/viz/service/display/overlay_unittest.cc
+++ components/viz/service/display/overlay_unittest.cc
@@ -2858,11 +2858,11 @@ TEST_F(SingleOverlayOnTopTest, RejectTransparentColorOnTopWithoutBlending) {
 }
 
 // Test makes sure promotion hint (|overlay_priority_hint| in |TextureDrawQuad|)
-// feature functions. The (current) expectation is that |kLow| will not promote
+// feature functions. The (current) expectation is that |kNone| will not promote
 // and that |kRequired| will be promoted in preference to |kRegular| candidates.
 TEST_F(SingleOverlayOnTopTest, CheckPromotionHintBasic) {
   // Test has two passes:
-  // Pass 1 checks kLow and kRegular values.
+  // Pass 1 checks kNone and kRegular values.
   constexpr size_t kTestRegularAtFrame = 3;
   // Pass 2 checks kRequired against kRegular values.
   constexpr size_t kTestRequiredAtFrame = 6;
@@ -2898,7 +2898,7 @@ TEST_F(SingleOverlayOnTopTest, CheckPromotionHintBasic) {
     sqs_partial->overlay_damage_index = surface_damage_rect_list.size();
     tex_quad_full->overlay_priority_hint = i > kTestRegularAtFrame
                                                ? OverlayPriority::kRegular
-                                               : OverlayPriority::kLow;
+                                               : OverlayPriority::kNone;
     // Damage is 100% of |display_rect|.
     surface_damage_rect_list.push_back(kOverlayBottomRightRect);
 
@@ -4259,10 +4259,7 @@ TEST_F(UnderlayCastTest, NoOverlayContentBounds) {
       resource_provider(), &pass_list, GetIdentityColorMatrix(),
       std::move(surface_damage_rect_list), GetDefaultPrimaryPlane(),
       &candidate_list, &damage_rect_);
-
 }
-
-
 
 TEST_F(UnderlayCastTest, NoOverlayPromotionWithoutProtectedContent) {
   auto pass = CreateRenderPass();
@@ -4280,7 +4277,6 @@ TEST_F(UnderlayCastTest, NoOverlayPromotionWithoutProtectedContent) {
       &candidate_list, &damage_rect_);
 
   ASSERT_TRUE(candidate_list.empty());
-
 }
 
 TEST_F(UnderlayCastTest, OverlayPromotionWithMaskFilter) {
@@ -4303,8 +4299,6 @@ TEST_F(UnderlayCastTest, OverlayPromotionWithMaskFilter) {
       resource_provider(), &pass_list, GetIdentityColorMatrix(),
       std::move(surface_damage_rect_list), GetDefaultPrimaryPlane(),
       &candidate_list, &damage_rect_);
-
-
 
   ASSERT_EQ(1U, pass_list.size());
   ASSERT_EQ(1U, pass_list.front()->quad_list.size());
