364fc6025bd8f87b3b03f0004602408e75f296fe
Dominik Klemba <tacet@google.com>
2026-08-18 22:36:53 -0700

    [Autofill] Clean up unused includes and forward declarations
    Removes unused headers and redundant forward declarations in
    personal_data_manager_factory.cc and payments_data_manager.h/.cc.
    
    Bug: 40100455
    Change-Id: Ia49a94d2a20bdbf12daac950a49943a57a345452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8255874
    Auto-Submit: Dominik Klemba <tacet@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682042}

--- components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
+++ components/autofill/core/browser/data_manager/payments/payments_data_manager.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/uuid.h"
 #include "build/buildflag.h"
 #include "components/autofill/core/browser/country_type.h"
-#include "components/autofill/core/browser/data_manager/personal_data_manager.h"
 #include "components/autofill/core/browser/data_model/payments/autofill_offer_data.h"
 #include "components/autofill/core/browser/data_model/payments/autofill_wallet_usage_data.h"
 #include "components/autofill/core/browser/data_model/payments/bank_account.h"
