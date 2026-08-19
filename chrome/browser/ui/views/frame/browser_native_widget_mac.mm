fb96daa2c00cad38b9feb0735a0088bc6acd1821
David Pennington <dpenning@google.com>
2026-08-19 15:44:30 -0700

    [Mac Glass Frame] Use theme luminance for glass frame tint opacity
    we were previously using OS based theme color to make a decision about
    the opacity of the tint, this was unexpected and resulted in chrome
    themes not having the correct tint opacity if they didnt match the
    os preferred theme.
    
    Bug: b:525119347
    Change-Id: I92b065ee6c8dbe3903b4bc748cdc3298549d5f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8271362
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682691}

--- chrome/browser/ui/views/frame/browser_native_widget_mac.mm
+++ chrome/browser/ui/views/frame/browser_native_widget_mac.mm
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/mojom/window_show_state.mojom.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/native_theme/native_theme.h"
 #import "ui/views/cocoa/native_widget_mac_ns_window_host.h"
 #include "ui/views/interaction/element_tracker_views.h"
@@ -372,7 +373,6 @@ void BrowserNativeWidgetMac::OnWidgetDestroyed(views::Widget* widget) {
     tint_view_ = nil;
   }
   browser_view_ = nullptr;
-  last_preferred_color_scheme_.reset();
   last_theme_color_.reset();
   last_is_vertical_tabs_.reset();
   last_is_glass_eligible_.reset();
@@ -796,7 +796,6 @@ void BrowserNativeWidgetMac::OnWindowDestroying(
 void BrowserNativeWidgetMac::OnWidgetActivationChanged(views::Widget* widget,
                                                        bool active) {
   last_theme_color_.reset();
-  last_preferred_color_scheme_.reset();
   UpdateBackgroundColor();
 }
 
@@ -901,7 +900,6 @@ void BrowserNativeWidgetMac::AnnounceTextInInProcessWindow(
 void BrowserNativeWidgetMac::OnVerticalTabStripModeChanged(
     tabs::VerticalTabStripStateController* controller) {
   last_theme_color_.reset();
-  last_preferred_color_scheme_.reset();
   UpdateBackgroundGeometry();
   UpdateBackgroundColor();
 }
@@ -954,7 +952,6 @@ void BrowserNativeWidgetMac::UpdateGlassEligibility(bool is_glass_eligible) {
       [tint_view_ removeFromSuperview];
       tint_view_ = nil;
     }
-    last_preferred_color_scheme_.reset();
     last_theme_color_.reset();
     last_is_vertical_tabs_.reset();
     return;
@@ -1054,8 +1051,6 @@ void BrowserNativeWidgetMac::UpdateBackgroundColor() {
     return;
   }
 
-  const ui::NativeTheme::PreferredColorScheme color_scheme =
-      browser_view_->GetNativeTheme()->preferred_color_scheme();
   const bool is_active = GetWidget()->ShouldPaintAsActive();
   const SkColor theme_color = browser_view_->GetColorProvider()->GetColor(
       is_active ? ui::kColorFrameActive : ui::kColorFrameInactive);
@@ -1064,23 +1059,19 @@ void BrowserNativeWidgetMac::UpdateBackgroundColor() {
 
   // Avoid updating the background view if the theme colors and the tab strip
   // orientation have not changed.
-  if (last_preferred_color_scheme_ == color_scheme &&
-      last_theme_color_ == theme_color &&
+  if (last_theme_color_ == theme_color &&
       last_is_vertical_tabs_ == is_vertical_tabs) {
     return;
   }
 
-  last_preferred_color_scheme_ = color_scheme;
   last_theme_color_ = theme_color;
   last_is_vertical_tabs_ = is_vertical_tabs;
 
   const CGFloat r = SkColorGetR(theme_color) / 255.0;
   const CGFloat g = SkColorGetG(theme_color) / 255.0;
   const CGFloat b = SkColorGetB(theme_color) / 255.0;
-  const CGFloat a =
-      GetGlassFrameTintOpacity(last_preferred_color_scheme_ ==
-                                   ui::NativeTheme::PreferredColorScheme::kDark,
-                               is_vertical_tabs);
+  const CGFloat a = GetGlassFrameTintOpacity(color_utils::IsDark(theme_color),
+                                             is_vertical_tabs);
 
   tint_view_.layer.backgroundColor =
       [NSColor colorWithSRGBRed:r green:g blue:b alpha:a].CGColor;
