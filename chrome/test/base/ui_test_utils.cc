73337013408cf89556b4ada1d0dad927b6a4fdd1
Nick Diego Yamane <nickdiego@igalia.com>
2026-08-19 09:36:44 -0700

    [Bedrock] Complete //chrome/test/base:ui_test_utils extraction
    Moves ui_test_utils.{cc,h} from //chrome/test:test_support_ui into the
    existing thin //chrome/test/base:ui_test_utils target, completing the
    extraction. Adds all required deps for the .cc implementation (browser
    bookmarks/history/task_manager, exclusive_access, location_bar, omnibox,
    navigator, net, device/network mojom, blink, etc.).
    
    Also removes ui_test_utils.{cc,h} from test_support_ui sources and adds
    the extracted target to test_support_ui public_deps for consumer
    continuity.
    
    Analysis/Plan: https://notes.nickdiego.dev/chromium/bedrocking-chrome-test
    
    R=chromium-bedrock-reviews@google.com
    
    Bug: 353332589
    Change-Id: Ib2a53fc969c504ad8c52a69cf4935eb90eadb9d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253805
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1682401}

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -62,7 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/autocomplete_change_observer.h"
-#include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/find_result_waiter.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/download/public/common/download_item.h"
