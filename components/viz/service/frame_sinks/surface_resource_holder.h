b15ba3641a71a25098b545f8bb84ee099053d4f4
kylechar <kylechar@chromium.org>
2026-08-19 07:24:25 -0700

    Delete ReservedResourceDelegate
    There used to be two implementations of things that allocated reserved
    ResourceIds. The only remaining implementation is
    SurfaceAnimationManager so we can get rid of the interface and
    associated indirection.
    
    Bug: none
    Change-Id: I15d72eb44e9953a5204943929ebae79070e66e9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267642
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682263}

--- components/viz/service/frame_sinks/surface_resource_holder.h
+++ components/viz/service/frame_sinks/surface_resource_holder.h
@@ -18,23 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace viz {
 class SurfaceResourceHolderClient;
 
-// ReservedResourceDelegate is an interface for tracking the lifetime of
-// resources. Code which submits resources that are managed fully on the viz
-// side (with resource IDs >= kVizReservedRangeStartId) should implement this.
-class ReservedResourceDelegate {
- public:
-  virtual ~ReservedResourceDelegate();
-
-  virtual void ReceiveFromChild(
-      const std::vector<TransferableResource>& resources) = 0;
-  virtual void RefResources(
-      const std::vector<TransferableResource>& resources) = 0;
-  // Unrefs resources managed by this delegate. The delegate is expected to
-  // remove all handled/unreferenced resources from `resources` in-place so that
-  // any remaining unhandled reserved resources can be detected and cleaned up.
-  virtual void UnrefResources(std::vector<ReturnedResourceViz>& resources) = 0;
-};
-
 // A SurfaceResourceHolder manages the lifetime of resources submitted by a
 // particular SurfaceFactory. Each resource is held by the service until
 // it is no longer referenced by any pending frames or by any
