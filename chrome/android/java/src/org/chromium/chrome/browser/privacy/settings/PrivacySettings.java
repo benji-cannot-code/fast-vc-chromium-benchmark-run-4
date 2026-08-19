aa94679e62347bb2f0ccd4970b66bd8ffb3ba869
Azamat Myrzabekov <azamatmz@google.com>
2026-08-19 04:22:03 -0700

    Hide Universal Opt-Out settings for ineligible users
    Bug: 548340311, 522291159
    Change-Id: Id3fc9dd12c2b81797f296ea079bd1a29afe25d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8266851
    Commit-Queue: Azamat Myrzabekov <azamatmz@google.com>
    Auto-Submit: Azamat Myrzabekov <azamatmz@google.com>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682190}

--- chrome/android/java/src/org/chromium/chrome/browser/privacy/settings/PrivacySettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/privacy/settings/PrivacySettings.java
@@ -70,6 +70,7 @@ import org.chromium.components.browser_ui.site_settings.SingleCategorySettings;
 import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
 import org.chromium.components.browser_ui.util.TraceEventVectorDrawableCompat;
 import org.chromium.components.content_settings.ContentSettingsType;
+import org.chromium.components.prefs.PrefService;
 import org.chromium.components.safe_browsing.OsAdditionalSecurityProvider;
 import org.chromium.components.safe_browsing.OsAdditionalSecurityUtil;
 import org.chromium.components.user_prefs.UserPrefs;
@@ -280,7 +281,7 @@ public class PrivacySettings extends ChromeBaseSettingsFragment
             scrollToPreference(PREF_ADVANCED_PROTECTION_INFO);
         }
 
-        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.UNIVERSAL_OPT_OUT_SETTINGS)) {
+        if (!shouldShowUniversalOptOutSettings()) {
             getPreferenceScreen().removePreference(findPreference(PREF_UNIVERSAL_OPT_OUT));
         }
 
@@ -395,7 +396,7 @@ public class PrivacySettings extends ChromeBaseSettingsFragment
                             : R.string.text_off);
         }
 
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.UNIVERSAL_OPT_OUT_SETTINGS)) {
+        if (shouldShowUniversalOptOutSettings()) {
             Preference universalOptOutPref = findPreference(PREF_UNIVERSAL_OPT_OUT);
             if (universalOptOutPref != null) {
                 universalOptOutPref.setSummary(
@@ -467,6 +468,16 @@ public class PrivacySettings extends ChromeBaseSettingsFragment
                                 getProfile(), ContentSettingsType.JAVASCRIPT_OPTIMIZER)
                         ? R.string.website_settings_category_javascript_optimizer_allowed_list
                         : R.string.website_settings_category_javascript_optimizer_blocked_list);
+    }
+
+    private boolean shouldShowUniversalOptOutSettings() {
+        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.UNIVERSAL_OPT_OUT_SETTINGS)) {
+            return false;
+        }
+
+        PrefService userPrefs = UserPrefs.get(getProfile());
+        return userPrefs.getBoolean(Pref.UNIVERSAL_OPT_OUT_ENABLED)
+                || userPrefs.getBoolean(Pref.UNIVERSAL_OPT_OUT_ELIGIBLE);
     }
 
     /** Shows the advanced-protection-section if needed. */
