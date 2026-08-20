35f2e7ff819ba4a66d33027864a07cd608bab534
Tomek Jurkiewicz <tju@google.com>
2026-08-20 01:46:10 -0700

    Use base::ScopedObservation for IdentityManager in ChildAccountService
    Replaces manual AddObserver and RemoveObserver calls with
    base::ScopedObservation to manage the lifetime of the
    signin::IdentityManager observation in ChildAccountService. This
    simplifies the code and ensures the observation is safely cleaned up.
    
    Fixes two unit tests that never actually initialized that service, so
    partial activation (in terms of starting observation in constructor)
    started to break flows. At the same time calling `SetIsChildAccount`
    started to be important because previously; without any observataion,
    child account status was inherited from TestingProfile and pre-set
    kSupervisedUserId.
    
    Bug: 469335277
    Change-Id: Id8c599827973f630fa79cf2b144a00c8a9b44a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260106
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682951}

--- components/supervised_user/core/browser/child_account_service.h
+++ components/supervised_user/core/browser/child_account_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -131,6 +132,10 @@ class ChildAccountService : public KeyedService,
 
   // Callbacks to run when the user status becomes known.
   std::vector<base::OnceClosure> status_received_callback_list_;
+
+  base::ScopedObservation<signin::IdentityManager,
+                          signin::IdentityManager::Observer>
+      identity_manager_observer_{this};
 
   base::WeakPtrFactory<ChildAccountService> weak_ptr_factory_{this};
 };
