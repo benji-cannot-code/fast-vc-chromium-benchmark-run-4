944f1e965b88bc0b1bff75fdd2689cdc983d19d9
Hidehiko Abe <hidehiko@chromium.org>
2026-08-19 18:37:43 -0700

    Remove IdentityManagerFactory from ChromeSessionManager
    Bug: 542786399
    Test: Tryjob
    Change-Id: I6c6964069595cb1e21330a6b6d900f07be5d4023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257455
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682788}

--- chrome/browser/ash/login/session/chrome_session_manager.cc
+++ chrome/browser/ash/login/session/chrome_session_manager.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/signin_profile_handler.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/login/login_display_host_webui.h"
 #include "chrome/browser/ui/webui/ash/login/app_launch_splash_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/shimless_rma_dialog/shimless_rma_dialog.h"
@@ -52,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/login/integrity/misconfigured_user_cleaner.h"
 #include "chromeos/ash/components/osauth/public/auth_hub.h"
+#include "chromeos/ash/components/signin/identity_manager_provider.h"
 #include "chromeos/ash/experiences/arc/arc_features.h"
 #include "chromeos/ash/experiences/arc/arc_prefs.h"
 #include "chromeos/ash/experiences/arc/arc_util.h"
@@ -144,33 +144,35 @@ void StartLoginOobeSession(PrefService& local_state) {
 
 // Seed the stub user account in the same way as it's done in
 // `UserSessionManager::InitProfilePreferences` for regular users.
-void UpsertStubUserToAccountManager(Profile* user_profile,
-                                    const user_manager::User* user) {
+void UpsertStubUserToAccountManager(const user_manager::User& user,
+                                    const base::FilePath& profile_path) {
   // 1. Make sure that the account is present in
   // `account_manager::AccountManager`.
   account_manager::AccountManager* account_manager =
       ash::AccountManagerFactory::Get()->GetAccountManager(
-          user_profile->GetPath().value());
+          profile_path.value());
 
   DCHECK(account_manager->IsInitialized());
 
   const ::account_manager::AccountKey account_key =
       ::account_manager::AccountKey::FromGaiaId(
-          user->GetAccountId().GetGaiaId());
+          user.GetAccountId().GetGaiaId());
 
   account_manager->UpsertAccount(
-      account_key, /*raw_email=*/user->GetDisplayEmail(),
+      account_key, /*raw_email=*/user.GetDisplayEmail(),
       account_manager::AccountManager::kInvalidToken);
 
   DCHECK(account_manager->IsTokenAvailable(account_key));
 
   // 2. Seed it into `IdentityManager`.
-  signin::IdentityManager* identity_manager =
-      IdentityManagerFactory::GetForProfile(user_profile);
+  // TODO(hidehiko): Consider to rework on user session management, and
+  // inject IdentityManager from callers.
+  auto* identity_manager =
+      ash::IdentityManagerProvider::Get().Find(user.GetAccountId());
   signin::AccountsMutator* accounts_mutator =
       identity_manager->GetAccountsMutator();
   CoreAccountId account_id = accounts_mutator->SeedAccountInfo(
-      user->GetAccountId().GetGaiaId(), user->GetDisplayEmail());
+      user.GetAccountId().GetGaiaId(), user.GetDisplayEmail());
 
   // 3. Set it as the Primary Account.
   const signin::ConsentLevel consent_level = [&]() {
@@ -197,7 +199,7 @@ void UpsertStubUserToAccountManager(Profile* user_profile,
 
   CHECK(identity_manager->HasPrimaryAccount(consent_level));
   CHECK_EQ(identity_manager->GetPrimaryAccountInfo(consent_level).gaia,
-           user->GetAccountId().GetGaiaId());
+           user.GetAccountId().GetGaiaId());
 
   DCHECK_EQ(account_id, identity_manager->GetPrimaryAccountId(
                             signin::ConsentLevel::kSignin));
@@ -260,7 +262,8 @@ void StartUserSession(
       InitializeAccountManager(
           std::move(shared_url_loader_factory), user_profile->GetPath(),
           /*initialization_callback=*/
-          base::BindOnce(&UpsertStubUserToAccountManager, user_profile, user));
+          base::BindOnce(&UpsertStubUserToAccountManager, std::ref(*user),
+                         user_profile->GetPath()));
     }
 
     user_session_mgr->OnUserProfileLoaded(user_profile, user);
