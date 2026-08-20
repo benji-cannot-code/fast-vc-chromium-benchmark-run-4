500bdd06223465eb47a85b8be7da2fe8ad807a2d
Hidehiko Abe <hidehiko@chromium.org>
2026-08-19 18:25:29 -0700

    Remove IdentityManagerFactory from MarketingBackendConnector
    Bug: 542786399
    Test: Tryjob
    Change-Id: Ib2075d99e331bde7864cc8815bd30839f438a8f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254663
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682780}

--- chrome/browser/ash/login/marketing_backend_connector.h
+++ chrome/browser/ash/login/marketing_backend_connector.h
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 
+namespace signin {
+class IdentityManager;
+}  // namespace signin
+
 namespace ash {
 
 class MarketingBackendConnector
@@ -56,7 +60,10 @@ class MarketingBackendConnector
   friend class ScopedRequestCallbackSetter;
   friend class base::RefCountedThreadSafe<MarketingBackendConnector>;
 
-  explicit MarketingBackendConnector(Profile* user_profile);
+  // `profile` and its `identity_manager` must not be nullptr, and must outlive
+  // this.
+  MarketingBackendConnector(Profile* profile,
+                            signin::IdentityManager* identity_manager);
   virtual ~MarketingBackendConnector();
 
   // Sends a request to the server to subscribe the user to all campaigns.
@@ -81,12 +88,14 @@ class MarketingBackendConnector
   // the language.
   std::string GetRequestContent();
 
+  const raw_ptr<Profile> profile_ = nullptr;
+  const raw_ref<signin::IdentityManager> identity_manager_;
+
   // Internal
   std::unique_ptr<signin::PrimaryAccountAccessTokenFetcher> token_fetcher_;
   std::unique_ptr<network::SimpleURLLoader> simple_url_loader_;
   scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
   std::string access_token_;
-  raw_ptr<Profile> profile_ = nullptr;
 
   static base::RepeatingCallback<void(std::string)>*
       request_finished_for_tests_;
