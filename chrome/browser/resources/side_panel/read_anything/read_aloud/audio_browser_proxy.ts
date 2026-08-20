15b7c0b9dec102b54e2746bc5c9043baab094467
Kristi Saney <kristislee@google.com>
2026-08-20 11:03:47 -0700

    [Reading mode] Add new AudioBrowserProxy and use it for speechRate
    See go/rm-browser-proxy.
    
    This is part of a larger move to the browser proxy pattern instead
    of calling chrome.readingMode directly or using FakeReadingMode in tests
    
    Bug: 497896810
    Change-Id: I2dd5436fb8ddc6383a4dab49b7c589467b5345b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269343
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683281}

---
+++ chrome/browser/resources/side_panel/read_anything/read_aloud/audio_browser_proxy.ts
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Browser proxy for C++ ReadAnythingAppController (chrome.readingMode) state,
+// preferences, and actions related to audio and speech synthesis (such as
+// speech rate, granularity, playing state, and voice packs).
+//
+// Distinct from SpeechBrowserProxy, which proxies the Web Speech API
+// (window.speechSynthesis) for speech playback and voice management.
+export interface AudioBrowserProxy {
+  getSpeechRate(): number;
+}
+
+export class AudioBrowserProxyImpl implements AudioBrowserProxy {
+  getSpeechRate(): number {
+    return chrome.readingMode.speechRate;
+  }
+
+  static getInstance(): AudioBrowserProxy {
+    return instance || (instance = new AudioBrowserProxyImpl());
+  }
+
+  static setInstance(obj: AudioBrowserProxy) {
+    instance = obj;
+  }
+}
+
+let instance: AudioBrowserProxy|null = null;
---
+++ chrome/test/data/webui/side_panel/read_anything/test_audio_browser_proxy.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {AudioBrowserProxy} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {TestBrowserProxy} from 'chrome-untrusted://webui-test/test_browser_proxy.js';
+
+export class TestAudioBrowserProxy extends TestBrowserProxy implements
+    AudioBrowserProxy {
+  speechRate: number = 1.0;
+
+  constructor() {
+    super([
+      'getSpeechRate',
+    ]);
+  }
+
+  getSpeechRate(): number {
+    this.methodCalled('getSpeechRate');
+    return this.speechRate;
+  }
+}
