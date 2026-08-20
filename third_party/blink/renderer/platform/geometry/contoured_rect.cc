5bed5218f0c8221a7bd74c771dd8a8d8a99e550e
Noam Rosenthal <nrosenthal@chromium.org>
2026-08-20 00:54:06 -0700

    Merge ShadowContourFollowsBorder flag
    Bug: 463996869
    Change-Id: I51689a4644785135d6bb4c115ff2f730c577804b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267384
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682922}

--- third_party/blink/renderer/platform/geometry/contoured_rect.cc
+++ third_party/blink/renderer/platform/geometry/contoured_rect.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <numbers>
 
 #include "third_party/blink/renderer/platform/geometry/path.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/format.h"
 #include "third_party/blink/renderer/platform/wtf/text/strcat.h"
@@ -47,28 +46,6 @@ float CornerRectIntercept(float y,
          std::pow(1 - std::pow(y / corner_rect.height(), curvature),
                   1 / curvature);
 }
-
-void ApplyOutsetAsTransform(FloatRoundedRect& rect,
-                            const gfx::OutsetsF& outsets) {
-  if (rect.IsEmpty()) {
-    return;
-  }
-
-  // For anything else, keep the same proportions between the original radii and
-  // the original rect.
-  gfx::RectF new_rect = rect.Rect();
-  FloatRoundedRect::Radii radii = rect.GetRadii();
-  new_rect.Outset(outsets);
-
-  float scale_x = new_rect.width() / rect.Rect().width();
-  float scale_y = new_rect.height() / rect.Rect().height();
-  radii.SetTopLeft(gfx::ScaleSize(radii.TopLeft(), scale_x, scale_y));
-  radii.SetTopRight(gfx::ScaleSize(radii.TopRight(), scale_x, scale_y));
-  radii.SetBottomRight(gfx::ScaleSize(radii.BottomRight(), scale_x, scale_y));
-  radii.SetBottomLeft(gfx::ScaleSize(radii.BottomLeft(), scale_x, scale_y));
-  rect.SetRadii(radii);
-  rect.SetRect(new_rect);
-}
 }  // namespace
 
 String ContouredRect::CornerCurvature::ToString() const {
@@ -93,20 +70,7 @@ bool ContouredRect::IntersectsQuad(const gfx::QuadF& quad) const {
 }
 
 void ContouredRect::OutsetWithCornerCorrection(const gfx::OutsetsF& outsets) {
-  if (RuntimeEnabledFeatures::ShadowContourFollowsBorderEnabled()) {
-    rect_.OutsetWithCornerCorrection(outsets);
-    return;
-  }
-
-  if (HasRoundCurvature()) {
-    rect_.OutsetWithCornerCorrection(outsets);
-    return;
-  }
-
-  ApplyOutsetAsTransform(rect_, outsets);
-  if (origin_rect_) {
-    ApplyOutsetAsTransform(*origin_rect_, outsets);
-  }
+  rect_.OutsetWithCornerCorrection(outsets);
 }
 
 bool ContouredRect::XInterceptsAtY(float y,
