2c31610faa8d1b58679e8380be80cf7507c40136
Bryant Chandler <bryantchandler@google.com>
2026-08-19 17:54:09 -0700

    [glic] Migrate testTabDataUpdateOnFaviconChangeForPinnedTab to new_glic_api_browsertest
    Bug: b:477918640
    Change-Id: I1cee7858cb89c7014d7f4fd97d7dd6d86a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258719
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682763}

--- chrome/test/data/webui/glic/browser_tests/new_glic_api_browsertest.ts
+++ chrome/test/data/webui/glic/browser_tests/new_glic_api_browsertest.ts
@@ -2694,6 +2694,31 @@ class ApiTests extends ApiTestFixtureBase {
         (tabs) =>
             tabs.some(t => t.tabId === tabId && t.url.includes('changed')));
   }
+
+  async testTabDataUpdateOnFaviconChangeForPinnedTab() {
+    assertDefined(this.host.getPinnedTabs);
+    assertDefined(this.host.pinTabs);
+
+    const tabId = (this.testParams as {tabId: string}).tabId;
+    assertNotEquals(tabId, this.getActiveTabId());
+
+    await this.host.pinTabs([tabId]);
+    const pinnedTabsUpdates = observeSequence(this.host.getPinnedTabs());
+
+    await pinnedTabsUpdates.waitFor(
+        (tabs) => tabs.some(t => t.tabId === tabId && t.favicon === undefined));
+
+    // Update the favicon.
+    await this.advanceToNextStep();
+
+    const tabs = await pinnedTabsUpdates.waitFor(
+        (tabs) => tabs.some(t => t.tabId === tabId && t.favicon !== undefined));
+
+    const tabData = tabs.find(t => t.tabId === tabId);
+    const blob = await tabData?.favicon?.();
+    assertEquals(blob?.type, 'image/bmp');
+    assertTrue(checkDefined(blob).size > 0);
+  }
 }
 
 class DaisyChainApiTests extends ApiTestFixtureBase {
