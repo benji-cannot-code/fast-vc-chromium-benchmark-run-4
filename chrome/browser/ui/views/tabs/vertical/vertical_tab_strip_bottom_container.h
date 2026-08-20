a3db9b76995defac7da568ac73d3f8611d2078bd
David Pennington <dpenning@google.com>
2026-08-19 21:10:47 -0700

    [Unified Tabstrip] Update NTB styling and fix Glass frame issue.
    In https://chromium-review.git.corp.google.com/c/chromium/src/+/8253400, IsGlassFrameEnabled() was checked directly in
    TabStripFlatEdgeButton::OnPaintBackground. Because
    TabStripFlatEdgeButton is the base class for buttons in the vertical
    tabstrip, this caused VTS buttons to lose their background styling when
    glass frame was enabled.
    
    This CL replaces the unconditional check with a flag
    paint_transparent_for_glass_ that defaults to false on
    TabStripFlatEdgeButton and is explicitly enabled only on the new tab
    button in HorizontalTabStripRegionViewNew.
    
    TAG=agy
    CONV=88436683-f0b7-4391-b524-c4ffe5130416
    
    Change-Id: I068f8da761fc288aa4b4aae8a92f4cd8d0fc5890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267946
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Vince Lugli <lugli@google.com>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682852}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_bottom_container.h
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_bottom_container.h
@@ -6,28 +6,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_VERTICAL_TAB_STRIP_BOTTOM_CONTAINER_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_VERTICAL_TAB_STRIP_BOTTOM_CONTAINER_H_
 
-#include "ui/views/context_menu_controller.h"
+#include "base/callback_list.h"
+#include "base/memory/raw_ptr.h"
+#include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/layout/flex_layout_view.h"
 
 class BrowserWindowInterface;
 class ExpandOnHoverLock;
-class NewTabButtonMenuModel;
 class TabStripFlatEdgeButton;
+
+namespace actions {
+class ActionItem;
+}  // namespace actions
 
 namespace tabs {
 class VerticalTabStripStateController;
 enum class VerticalTabStripCollapseState;
 }  // namespace tabs
 
-namespace views {
-class ActionViewController;
-class MenuRunner;
-}  // namespace views
-
 // Bottom container of the vertical tab strip which includes the new tab button.
-class VerticalTabStripBottomContainer : public views::FlexLayoutView,
-                                        public views::ContextMenuController {
-  METADATA_HEADER(VerticalTabStripBottomContainer, views::View)
+class VerticalTabStripBottomContainer : public views::FlexLayoutView {
+  METADATA_HEADER(VerticalTabStripBottomContainer, views::FlexLayoutView)
  public:
   VerticalTabStripBottomContainer(
       tabs::VerticalTabStripStateController* state_controller,
@@ -40,13 +39,8 @@ class VerticalTabStripBottomContainer : public views::FlexLayoutView,
 
   void OnCollapseStateChanged(tabs::VerticalTabStripCollapseState state);
 
-  // views::ContextMenuController:
-  void ShowContextMenuForViewImpl(
-      View* source,
-      const gfx::Point& point,
-      ui::mojom::MenuSourceType source_type) override;
-
  private:
+  void OnNewTabButtonContextMenuWillShow();
   void OnNewTabButtonContextMenuClosed();
 
   void UpdateButtonStyles(bool collapsed);
@@ -57,9 +51,6 @@ class VerticalTabStripBottomContainer : public views::FlexLayoutView,
   base::CallbackListSubscription collapsed_state_change_subscription_;
   base::CallbackListSubscription new_tab_button_pressed_subscription_;
 
-  std::unique_ptr<NewTabButtonMenuModel> context_menu_model_;
-  std::unique_ptr<views::MenuRunner> context_menu_runner_;
-  std::unique_ptr<views::ActionViewController> action_view_controller_;
   std::unique_ptr<ExpandOnHoverLock> expand_on_hover_lock_;
 };
 
