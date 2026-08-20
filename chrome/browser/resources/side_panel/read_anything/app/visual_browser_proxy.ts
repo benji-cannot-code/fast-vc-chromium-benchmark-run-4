4b55279bf461bc96ecf91f5cd84d7a458442046c
Kristi Saney <kristislee@google.com>
2026-08-20 11:00:05 -0700

    [Reading mode] Move togglePresentation() to a new VisualBrowserProxy
    See go/rm-browser-proxy.
    
    This is the start of a larger move to the browser proxy pattern instead
    of calling chrome.readingMode directly or using FakeReadingMode in tests
    
    Bug: 497896810
    Change-Id: Ia2367da2718592c50be0b6805a3db39e89410d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8268916
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683278}

---
+++ chrome/browser/resources/side_panel/read_anything/app/visual_browser_proxy.ts
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Interface for accessing and updating visual presentation, layout, and theme
+// settings (such as fonts, line and letter spacing, color themes, line focus,
+// and presentation states) in Read Anything.
+export interface VisualBrowserProxy {
+  getInSidePanelPresentationState(): number;
+  getInImmersiveOverlayPresentationState(): number;
+
+  togglePresentation(): void;
+}
+
+export class VisualBrowserProxyImpl implements VisualBrowserProxy {
+  getInSidePanelPresentationState(): number {
+    return chrome.readingMode.inSidePanelPresentationState;
+  }
+
+  getInImmersiveOverlayPresentationState(): number {
+    return chrome.readingMode.inImmersiveOverlayPresentationState;
+  }
+
+  togglePresentation(): void {
+    chrome.readingMode.togglePresentation();
+  }
+
+  static getInstance(): VisualBrowserProxy {
+    return instance || (instance = new VisualBrowserProxyImpl());
+  }
+
+  static setInstance(obj: VisualBrowserProxy) {
+    instance = obj;
+  }
+}
+
+let instance: VisualBrowserProxy|null = null;
---
+++ chrome/test/data/webui/side_panel/read_anything/test_visual_browser_proxy.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {VisualBrowserProxy} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {TestBrowserProxy} from 'chrome-untrusted://webui-test/test_browser_proxy.js';
+
+export class TestVisualBrowserProxy extends TestBrowserProxy implements
+    VisualBrowserProxy {
+  inSidePanelPresentationState: number = 1;
+  inImmersiveOverlayPresentationState: number = 2;
+
+  constructor() {
+    super([
+      'getInSidePanelPresentationState',
+      'getInImmersiveOverlayPresentationState',
+      'togglePresentation',
+    ]);
+  }
+
+  getInSidePanelPresentationState(): number {
+    this.methodCalled('getInSidePanelPresentationState');
+    return this.inSidePanelPresentationState;
+  }
+
+  getInImmersiveOverlayPresentationState(): number {
+    this.methodCalled('getInImmersiveOverlayPresentationState');
+    return this.inImmersiveOverlayPresentationState;
+  }
+
+  togglePresentation(): void {
+    this.methodCalled('togglePresentation');
+  }
+}
