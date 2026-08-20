d318a99dd349a72dc030ac87b430c6bd242d4c91
Neil Coronado <nemco@google.com>
2026-08-20 14:42:53 -0700

    [Metrics] Clean up expired tab strip group collapse histogram
    Clean up expired tab strip histogram.
    -Android.TabStrip.TabGroupCollapsed
    
    Fixed: 448108273
    Change-Id: Ide41b33f866d6b41ee3e04f6eb868d2969e0f344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261776
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683471}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperTest.java
@@ -5766,8 +5766,6 @@ public class StripLayoutHelperTest {
     @Test
     public void testHandleGroupTitleClick_Collapse() {
         // Initialize with 4 tabs. Group first three tabs.
-        HistogramWatcher histogramWatcher =
-                HistogramWatcher.newSingleRecordWatcher("Android.TabStrip.TabGroupCollapsed", true);
         initializeTest(false, false, 3, 4);
         mStripLayoutHelper.onSizeChanged(
                 STRIP_WIDTH, STRIP_HEIGHT, false, TIMESTAMP, PADDING_LEFT, PADDING_RIGHT, 0f);
@@ -5781,16 +5779,11 @@ public class StripLayoutHelperTest {
         // Verify the proper event was sent to the TabModel.
         verify(mModel)
                 .setTabGroupCollapsed(TAB_GROUP_ID_1, /* isCollapsed= */ true, /* animate= */ true);
-        // Verify we record the correct metric.
-        histogramWatcher.assertExpected("Should record true, since we're collapsing.");
     }
 
     @Test
     public void testHandleGroupTitleClick_Expand() {
         // Initialize with 4 tabs. Group first three tabs.
-        HistogramWatcher histogramWatcher =
-                HistogramWatcher.newSingleRecordWatcher(
-                        "Android.TabStrip.TabGroupCollapsed", false);
         initializeTest(false, false, 3, 4);
         mStripLayoutHelper.onSizeChanged(
                 STRIP_WIDTH, STRIP_HEIGHT, false, TIMESTAMP, PADDING_LEFT, PADDING_RIGHT, 0f);
@@ -5807,8 +5800,6 @@ public class StripLayoutHelperTest {
         verify(mModel)
                 .setTabGroupCollapsed(
                         TAB_GROUP_ID_1, /* isCollapsed= */ false, /* animate= */ true);
-        // Verify we record the correct metric.
-        histogramWatcher.assertExpected("Should record false, since we're expanding.");
     }
 
     @Test
