7c3aa008c4e6a92a6187c1f3c4056cb55d3454cf
Juliet Levesque <julietlevesque@google.com>
2026-08-20 08:59:32 -0700

    [loomnibox] Update to center UI elements
    https://screencast.googleplex.com/cast/NjMwMjc2NjI1MDA2NTkyMHwyZTVjMGI3ZS1lOA
    
    Fixed: b:545674855
    Change-Id: I000dcd8c32bbaf437c1b9570aa7bf1e8c8e037ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253728
    Commit-Queue: Juliet Lévesque Knighton <julietlevesque@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683205}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.cc
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.cc
@@ -65,7 +65,6 @@ DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(OmniboxEverywhereUIManager,
                                       kOmniboxEverywhereElementId);
 
 namespace {
-
 bool IsEphemeral() {
   bool is_ephemeral = false;
   if (g_browser_process && g_browser_process->local_state()) {
