ffcac350137b261ba1bc9ff01be3c943583027af
Gabriel Charette <gab@chromium.org>
2026-08-20 15:27:51 -0700

    Cleanup GCOnArrayBufferAllocationFailure feature flag
    This feature has launched and enables V8 GC retry on ArrayBuffer
    allocation failure. This CL removes the feature flag and keeps the retry
    behavior enabled.
    
    Fixed: 371904440
    b:483482004
    
    Change-Id: I053142f630e22881ea763b94a290dffc0d322db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258294
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683500}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1236,7 +1236,6 @@ public final class ProductionSupportedFlagList {
 
         // Features for PerfCombined2025_WebView study
         Flag.baseFeature("ReducePPMs"),
-        Flag.baseFeature("GCOnArrayBufferAllocationFailure"),
         Flag.baseFeature("RemoveCancelledScriptedIdleTasks"),
         Flag.baseFeature("MemoryCacheChangeStrongReferencePruneDelay"),
         Flag.baseFeature("MemoryCacheStrongReference"),
