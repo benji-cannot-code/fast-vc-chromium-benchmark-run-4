f38bce57c011271a3f30d967a72b95e78e6ffb07
Ziad Youssef <ziadyoussef@chromium.org>
2026-08-20 07:47:17 -0700

    Create Spare Renderer for the Default WebView Profile
    Experimentation for this feature will begin after both
    WebViewProfileStoreNotTriggerStartup and
    WebViewMultiProfileSkipDefaultProfile are launched. Together, these
    features prevent apps using a non-default profile from initializing the
    default profile in most cases, avoiding the unnecessary initialization
    of two renderer processes.
    
    TAG=agy CONV=94afb1d1-2572-4c4a-901c-f30b98a782ee
    
    Bug: 514322402
    Change-Id: I62a80df2738ba966c21f71184f843ebde6688656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8255810
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683143}

--- android_webview/common/aw_features.h
+++ android_webview/common/aw_features.h
@@ -16,6 +16,7 @@ namespace android_webview::features {
 // alongside the definition of their values in the .cc file.
 
 // Alphabetical:
+BASE_DECLARE_FEATURE(kCreateSpareRendererForDefaultProfile);
 BASE_DECLARE_FEATURE(kPostChromiumStartupInWebViewConstructor);
 BASE_DECLARE_FEATURE(kPrerender2WarmUpCompositorForWebView);
 BASE_DECLARE_FEATURE(kStartupNonBlockingWebViewConstructor);
