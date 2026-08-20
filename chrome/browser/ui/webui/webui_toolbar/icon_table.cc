5a37217e232af372cfe028a7b8761f548847c975
Russ Hamilton <behamilton@google.com>
2026-08-20 13:42:46 -0700

    [WebUI Toolbar] Add missing vector icons
    Add mappings in IconTable and corresponding SVGs in icons.ts for:
    - views::kVisibilityOffIcon
    - vector_icons::kPasskeyIcon
    - vector_icons::kDraftIcon
    
    Bug: 549782223
    Change-Id: Idf920bce8f4747b2c9fccb184120c80ab3fdca13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269782
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683419}

--- chrome/browser/ui/webui/webui_toolbar/icon_table.cc
+++ chrome/browser/ui/webui/webui_toolbar/icon_table.cc
@@ -213,6 +213,8 @@ const base::flat_map<const gfx::VectorIcon*, IconInfo>& KnownIcons() {
            {"webui-toolbar:dangerous_filled", IconType::kIconSet}},
           {{&vector_icons::kDomainIcon},
            {"webui-toolbar:domain", IconType::kIconSet}},
+          {{&vector_icons::kDraftIcon},
+           {"webui-toolbar:draft", IconType::kIconSet}},
           {{&vector_icons::kEditChromeRefreshOldIcon},
            {"webui-toolbar:edit", IconType::kIconSet}},
           {{&vector_icons::kEditIcon},
@@ -257,6 +259,8 @@ const base::flat_map<const gfx::VectorIcon*, IconInfo>& KnownIcons() {
            {"webui-toolbar:no_encryption", IconType::kIconSet}},
           {{&vector_icons::kNotSecureWarningChromeRefreshOldIcon},
            {"webui-toolbar:warning", IconType::kIconSet}},
+          {{&vector_icons::kPasskeyIcon},
+           {"webui-toolbar:passkey", IconType::kIconSet}},
           {{&vector_icons::kPasswordManagerIcon},
            {"webui-toolbar:password_manager", IconType::kIconSet}},
           {{&vector_icons::kPasswordManagerOldIcon},
@@ -300,6 +304,8 @@ const base::flat_map<const gfx::VectorIcon*, IconInfo>& KnownIcons() {
            {"webui-toolbar:visibility", IconType::kIconSet}},
           {{&views::kEyeRefreshOldIcon},
            {"webui-toolbar:visibility", IconType::kIconSet}},
+          {{&views::kVisibilityOffIcon},
+           {"webui-toolbar:visibility_off", IconType::kIconSet}},
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
           {{&vector_icons::kFastForwardCircleSparkIcon},
