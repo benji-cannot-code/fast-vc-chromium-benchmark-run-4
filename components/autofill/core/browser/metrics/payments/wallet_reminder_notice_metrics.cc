149da87755ef8fa8aa57e58c690a8c04f055fad6
Fernando Ramirez <ferny@google.com>
2026-08-19 18:53:56 -0700

    [PDT] Define Wallet reminder notice UMA metrics
    This CL adds 2 UMA histograms:
    1. Autofill.WalletReminderNotice.Interaction: Records the user's
    interaction on the notice UI.
    2. Autofill.WalletReminderNotice.ShowResult: Records the outcome of
    attempting to show the reminder notice.
    
    Design: go/wallet-notice-chrome-design
    Fixed: b:543584963
    Change-Id: Ieedb09823f64b6daa507d6d7d0b27798f2c50e6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8266693
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682793}

---
+++ components/autofill/core/browser/metrics/payments/wallet_reminder_notice_metrics.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/metrics/payments/wallet_reminder_notice_metrics.h"
+
+#include "base/metrics/histogram_functions.h"
+
+namespace autofill::autofill_metrics {
+
+void LogWalletReminderNoticeInteraction(
+    WalletReminderNoticeInteraction interaction) {
+  base::UmaHistogramEnumeration("Autofill.WalletReminderNotice.Interaction",
+                                interaction);
+}
+
+void LogWalletReminderNoticeShowResult(
+    WalletReminderNoticeShowResult show_result) {
+  base::UmaHistogramEnumeration("Autofill.WalletReminderNotice.ShowResult",
+                                show_result);
+}
+
+}  // namespace autofill::autofill_metrics
