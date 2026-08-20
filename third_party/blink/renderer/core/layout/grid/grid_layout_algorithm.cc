8ab389dae8c49e1fe2733b0bf6895851152b96e7
Javier Contreras Tenorio <javiercon@microsoft.com>
2026-08-20 14:51:43 -0700

    [css-grid] Remove grid gap suppression flag
    CSSGridGapSuppression has been enabled in stable for over three months,
    so the runtime flag is no longer needed.
    
    Remove the flag and make grid gap suppression unconditional. Also remove
    CSSGapDecoration's dependency on the flag while preserving its existing
    runtime gate.
    
    A follow up CL will remove the flag for CSS Gap Decorations since it has
    been in stable for over two months.
    
    Change-Id: I7d32a218a7becd40da34990d2648a3fbcf4c4344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8268231
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1683477}

--- third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.cc
@@ -202,8 +202,7 @@ const LayoutResult* GridLayoutAlgorithm::LayoutInternal() {
   // suppression. Hence, in such cases the total block size should be aligned
   // with the intrinsic block size after suppression, as this represents the
   // actual size of the subgrid once gap adjustments have been applied.
-  if (RuntimeEnabledFeatures::CSSGridGapSuppressionEnabled() &&
-      node.HasCachedPlacementData() &&
+  if (node.HasCachedPlacementData() &&
       !node.CachedPlacementData().HasStandaloneAxis(
           GridTrackSizingDirection::kForRows)) {
     container_builder_.SetFragmentsTotalBlockSize(intrinsic_block_size);
@@ -1760,8 +1759,7 @@ void GridLayoutAlgorithm::PlaceGridItems(
   // decorations).
   if ((RuntimeEnabledFeatures::CSSGapDecorationEnabled() &&
        Style().HasGapRule()) ||
-      (RuntimeEnabledFeatures::CSSGridGapSuppressionEnabled() &&
-       out_unfragmented_gap_geometry)) {
+      out_unfragmented_gap_geometry) {
     gap_accumulator = GapAccumulator();
     gap_accumulator->BuildGapGeometry(*layout_data);
 
@@ -1869,8 +1867,7 @@ void GridLayoutAlgorithm::PlaceGridItems(
       // If `out_unfragmented_gap_geometry` is present we just want to record
       // the initial position of all gaps for the purposes of fragmentation.
       // Don't add these to the builder.
-      if (RuntimeEnabledFeatures::CSSGridGapSuppressionEnabled() &&
-          out_unfragmented_gap_geometry) {
+      if (out_unfragmented_gap_geometry) {
         *out_unfragmented_gap_geometry = gap_geometry;
       } else {
         container_builder_.SetGapGeometry(gap_geometry);
@@ -2527,9 +2524,7 @@ void GridLayoutAlgorithm::PlaceGridItemsForFragmentation(
                  max_item_block_end - cloned_block_start_decoration);
   }
 
-  if (RuntimeEnabledFeatures::CSSGridGapSuppressionEnabled()) {
-    PlaceGaps();
-  }
+  PlaceGaps();
 
   if (has_subsequent_children)
     container_builder_.SetHasSubsequentChildren();
