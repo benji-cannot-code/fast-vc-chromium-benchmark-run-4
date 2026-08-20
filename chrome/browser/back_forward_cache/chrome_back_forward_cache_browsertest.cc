70376f4ae4f4f2bf7aa672fe77d6e3f53476b838
Lei Zhang <thestig@chromium.org>
2026-08-19 22:46:58 -0700

    BFCache: Create chrome/browser/back_forward_cache directory
    Create a dedicated sub-directory inside chrome/browser for bfcache code,
    similar to how content/browser/back_forward_cache exists. Start by
    moving chrome_back_forward_cache_browsertest.cc there and modularizing
    its build rules. As part of the move, auto-format the test / fix nits.
    
    Bypass-Check-License: Moved file.
    Change-Id: I99cce0580eb2099a62c15989a8da6e366b30beda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269573
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682878}

--- chrome/browser/back_forward_cache/chrome_back_forward_cache_browsertest.cc
+++ chrome/browser/back_forward_cache/chrome_back_forward_cache_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <array>
 #include <string_view>
+#include <utility>
 
 #include "base/command_line.h"
 #include "base/functional/callback.h"
@@ -199,8 +200,9 @@ IN_PROC_BROWSER_TEST_F(ChromeBackForwardCacheBrowserTest, BasicIframe) {
 
   content::RenderFrameHost* rfh_b = nullptr;
   rfh_a->ForEachRenderFrameHost([&](content::RenderFrameHost* rfh) {
-    if (rfh != rfh_a.get())
+    if (rfh != rfh_a.get()) {
       rfh_b = rfh;
+    }
   });
   EXPECT_TRUE(rfh_b);
   content::RenderFrameHostWrapper rfh_b_wrapper(rfh_b);
@@ -700,7 +702,7 @@ class ChromeBackForwardCacheBrowserWithEmbedTestBase
       blink::scheduler::WebSchedulerTrackedFeature feature,
       base::Location location) {
     content::FetchHistogramsFromChildProcesses();
-    base::HistogramBase::Sample32 sample = base::HistogramBase::Sample32(feature);
+    base::HistogramBase::Sample32 sample(std::to_underlying(feature));
     base::Bucket expected_blocklisted(sample, 1);
 
     EXPECT_THAT(histogram_tester_->GetAllSamples(
@@ -940,8 +942,9 @@ IN_PROC_BROWSER_TEST_P(
 #define MAYBE_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded \
   DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded
 #endif
-IN_PROC_BROWSER_TEST_P(ChromeBackForwardCacheBrowserWithEmbedPdfTest,
-                       MAYBE_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded) {
+IN_PROC_BROWSER_TEST_P(
+    ChromeBackForwardCacheBrowserWithEmbedPdfTest,
+    MAYBE_DoesNotCachePageWithEmbeddedPdfAppendedOnPageLoaded) {
   const auto tag = html_tag();
 
   // Navigate to A.
