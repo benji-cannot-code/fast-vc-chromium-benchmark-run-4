b84297e952a3d742b61e863958ad27b19263aa02
Abhijith Nair <abhijithnair@chromium.org>
2026-08-20 04:04:31 -0700

    [wv] Move non-UI-thread-capable startup tasks to StartupController
    This refactors WebView startup by moving
    runNonUiThreadCapableStartupTasks and its associated latch and helper
    methods from WebViewChromiumAwInit to StartupController.
    
    Bypass-Target-Sdk-Version-Check: False positive - moving code
    Bug: 544989980
    Change-Id: If5668d2fe15b4ee6a9ae76e44e8e800c2d9c8acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256638
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683026}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -587,7 +587,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
                                                 .WEBVIEW_MOVE_WORK_TO_PROVIDER_INIT_THREAD_POOL)) {
                     PostTask.postTask(
                             TaskTraits.USER_VISIBLE,
-                            () -> mAwInit.runNonUiThreadCapableStartupTasks());
+                            mAwInit.getStartupController()::runNonUiThreadCapableStartupTasks);
                 }
 
                 boolean enableSystemTracing =
@@ -672,7 +672,7 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
                     && !WebViewCachedFlags.get()
                             .isCachedFeatureEnabled(
                                     AwFeatures.WEBVIEW_MOVE_WORK_TO_PROVIDER_INIT_THREAD_POOL)) {
-                mAwInit.runNonUiThreadCapableStartupTasks();
+                mAwInit.getStartupController().runNonUiThreadCapableStartupTasks();
             }
 
             FlagOverrideHelper helper =
