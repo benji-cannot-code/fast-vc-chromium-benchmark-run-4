d2c9577806c483215d4dd862c36930fb70a542a1
Eleanor Lee <eleanorlee@google.com>
2026-08-19 19:29:33 -0700

    [Gardener] Disable failing tests in WebContentsAccessibilityEventsTest
    Bug: 549376305
    Change-Id: I2c3fecf772fee7ea16d175d38202ddc80f4c7ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8271282
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Owners-Override: Eleanor Lee <eleanorlee@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682811}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityEventsTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityEventsTest.java
@@ -24,6 +24,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -367,6 +368,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaCheckedChanged() {
         performTest("aria-checked-changed.html", "aria-checked-changed-expected-android.txt");
     }
@@ -526,6 +528,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaMeterValueChange() {
         performTest("aria-meter-value-change.html", "aria-meter-value-change-expected-android.txt");
     }
@@ -546,12 +549,14 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaPressedChanged() {
         performTest("aria-pressed-changed.html", "aria-pressed-changed-expected-android.txt");
     }
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaPressedChangesButtonRole() {
         performTest(
                 "aria-pressed-changes-button-role.html",
@@ -640,6 +645,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaSpinbuttonValueBothChanged() {
         performTest(
                 "aria-spinbutton-value-both-change.html",
@@ -648,6 +654,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaSpinbuttonValueChanged() {
         performTest(
                 "aria-spinbutton-value-change.html",
@@ -664,6 +671,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_ariaSpinbuttonValuetextChanged() {
         performTest(
                 "aria-spinbutton-valuetext-change.html",
@@ -862,6 +870,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_descriptionChangedPaneTitle() {
         performTest(
                 "description-changed-pane-title.html",
@@ -894,6 +903,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_dialogPaneNameChanged() {
         performTest("dialog-pane-name-change.html", "dialog-pane-name-change-expected-android.txt");
     }
@@ -944,6 +954,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_formRequiredChanged() {
         performTest("form-required-changed.html", "form-required-changed-expected-android.txt");
     }
@@ -1328,6 +1339,7 @@ public class WebContentsAccessibilityEventsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/549376305")
     public void test_pressedStateChanged() {
         performTest("pressed-state-changed.html", "pressed-state-changed-expected-android.txt");
     }
