6570299c33774d2a5e10e74fb3c3c22f49ff0af0
Rushan Suleymanov <rushans@google.com>
2026-08-20 04:43:24 -0700

    [Sync] Use string_view in WaitForFullDeviceInfoCommitted
    WaitForFullDeviceInfoCommitted currently accepts const std::string&.
    Using std::string_view is more flexible and avoids unnecessary
    conversions at call sites.
    
    TAG=agy
    CONV=e8e1eb35-4a7c-4cd1-acfc-421aacb49754
    
    Bug: 548614755
    Change-Id: I4957f40e6c0531d1e749c3c3e1ef00061995cac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8272631
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683042}

--- chrome/browser/sync/test/integration/device_info_helper.cc
+++ chrome/browser/sync/test/integration/device_info_helper.cc
@@ -4,8 +4,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/sync/test/integration/device_info_helper.h"
+
+#include <string_view>
+
 #include "components/sync/protocol/sync_entity.pb.h"
 #include "components/sync/test/fake_server.h"
+
+namespace {
+using testing::AllOf;
+using testing::Contains;
+using testing::ExplainMatchResult;
+}  // namespace
 
 ServerDeviceInfoMatchChecker::ServerDeviceInfoMatchChecker(
     const Matcher& matcher)
@@ -26,18 +35,16 @@ bool ServerDeviceInfoMatchChecker::IsExitConditionSatisfied(std::ostream* os) {
       fake_server()->GetSyncEntitiesByDataType(syncer::DEVICE_INFO);
 
   testing::StringMatchResultListener result_listener;
-  const bool matches =
-      testing::ExplainMatchResult(matcher_, entities, &result_listener);
+  const bool matches = ExplainMatchResult(matcher_, entities, &result_listener);
   *os << result_listener.str();
   return matches;
 }
 
 namespace device_info_helper {
 
-bool WaitForFullDeviceInfoCommitted(const std::string& cache_guid) {
+bool WaitForFullDeviceInfoCommitted(std::string_view cache_guid) {
   return ServerDeviceInfoMatchChecker(
-             testing::Contains(
-                 testing::AllOf(HasCacheGuid(cache_guid), HasSharingFields())))
+             Contains(AllOf(HasCacheGuid(cache_guid), HasSharingFields())))
       .Wait();
 }
 
--- chrome/browser/sync/test/integration/device_info_helper.h
+++ chrome/browser/sync/test/integration/device_info_helper.h
@@ -7,9 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SYNC_TEST_INTEGRATION_DEVICE_INFO_HELPER_H_
 
 #include <ostream>
-#include <string>
+#include <string_view>
 
-#include "base/memory/raw_ptr.h"
 #include "chrome/browser/sync/test/integration/fake_server_match_status_checker.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -54,7 +53,7 @@ MATCHER_P(HasCacheGuid, expected_cache_guid, "") {
   return arg.specifics().device_info().cache_guid() == expected_cache_guid;
 }
 
-bool WaitForFullDeviceInfoCommitted(const std::string& cache_guid);
+bool WaitForFullDeviceInfoCommitted(std::string_view cache_guid);
 
 }  // namespace device_info_helper
 
