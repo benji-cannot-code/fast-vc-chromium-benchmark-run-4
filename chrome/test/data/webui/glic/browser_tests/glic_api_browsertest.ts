dd196fc5b127604730be56cb30419c59f1d009a2
Bryant Chandler <bryantchandler@google.com>
2026-08-19 17:43:09 -0700

    [glic] Migrate testPanelWillOpenHasPromptSuggestion to new_glic_api_browsertest
    Bug: b:477918640
    Change-Id: I2ab9605607dd64ade8c0a7de5d2885686a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261239
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682753}

--- chrome/test/data/webui/glic/browser_tests/glic_api_browsertest.ts
+++ chrome/test/data/webui/glic/browser_tests/glic_api_browsertest.ts
@@ -232,11 +232,6 @@ class ApiTests extends ApiTestFixtureBase {
       // capturing the screenshot, but it still succeeds randomly.
     }
   }
-
-  async testPanelWillOpenHasPromptSuggestion() {
-    const invokeOptions = await observeSequence(this.client.invokeData).next();
-    assertEquals('Prompt Suggestion', invokeOptions.prompts?.[0]);
-  }
 }
 
 class DaisyChainApiTests extends ApiTestFixtureBase {
