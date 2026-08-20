dec7d99178fabb4eaef6de4d74af80804a295be7
Lei Zhang <thestig@chromium.org>
2026-08-20 00:54:56 -0700

    BFCache: Move ShouldPrioritizeForBackForwardCache() to chrome/b/bfcache
    Move ShouldPrioritizeForBackForwardCache() implementation from
    ChromeContentBrowserClient into a dedicated
    chrome/browser/back_forward_cache/back_forward_cache_util.cc using
    namespace chrome_back_forward_cache.
    
    ChromeContentBrowserClient::ShouldPrioritizeForBackForwardCache() now
    delegates to the chrome_back_forward_cache function.
    
    This reduces the size of chrome_content_browser_client.cc and gives the
    ownership of the code to BFCache code owners.
    
    Also fix a GN build error by making the browser_tests target
    non-Android.
    
    TAG=agy
    CONV=e450870e-21b2-4c61-8f19-32d9304658cb
    
    Bug: 548843025
    Change-Id: I07cb9809d3bd653730c5eae5bba2582c2e3c7a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261380
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682923}

---
+++ chrome/browser/back_forward_cache/back_forward_cache_util.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_BACK_FORWARD_CACHE_BACK_FORWARD_CACHE_UTIL_H_
+#define CHROME_BROWSER_BACK_FORWARD_CACHE_BACK_FORWARD_CACHE_UTIL_H_
+
+class GURL;
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace chrome_back_forward_cache {
+
+// Returns true if the given `url` is the search results page from the current
+// default search engine for `browser_context`.
+bool ShouldPrioritizeForBackForwardCache(
+    content::BrowserContext* browser_context,
+    const GURL& url);
+
+}  // namespace chrome_back_forward_cache
+
+#endif  // CHROME_BROWSER_BACK_FORWARD_CACHE_BACK_FORWARD_CACHE_UTIL_H_
