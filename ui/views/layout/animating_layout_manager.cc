20688e93074710c492cb1227bfd07d4ce85bd071
Gabriel Charette <gab@chromium.org>
2026-08-19 21:57:30 -0700

    Cleanup AvoidUnnecessaryShouldRenderRichAnimation feature flag
    This feature has launched and avoids querying ShouldRenderRichAnimation()
    in AnimatingLayoutManager when in kUseHostBounds mode. This CL removes
    the feature flag and keeps the optimization permanent.
    
    Fixed: 424432184
    b:483482004
    
    Change-Id: I237dca2d8de3f787ef12c3eddf000d27c86b4541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257998
    Reviewed-by: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682864}

--- ui/views/layout/animating_layout_manager.cc
+++ ui/views/layout/animating_layout_manager.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/auto_reset.h"
-#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/observer_list.h"
@@ -33,13 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 
 namespace {
-
-// When enabled, a call to gfx::Animation::ShouldRenderRichAnimation() is
-// avoided when not needed. Behind a feature to assess impact
-// (go/chrome-performance-work-should-be-finched).
-// TODO(crbug.com/40897031): Clean up when experiment is complete.
-BASE_FEATURE(kAvoidUnnecessaryShouldRenderRichAnimation,
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Returns the ChildLayout data for the child view in the proposed layout, or
 // nullptr if not found.
@@ -475,9 +467,7 @@ gfx::Size AnimatingLayoutManager::GetPreferredSize(const View* host) const {
   // (go/jank-from-should-render-rich-animation-jun2025). Avoid calling it when
   // `bounds_animation_mode_` is `kUseHostBounds`, since it won't affect the
   // outcome.
-  if (base::FeatureList::IsEnabled(
-          kAvoidUnnecessaryShouldRenderRichAnimation) &&
-      bounds_animation_mode_ == BoundsAnimationMode::kUseHostBounds) {
+  if (bounds_animation_mode_ == BoundsAnimationMode::kUseHostBounds) {
     return target_layout_manager()->GetPreferredSize(host);
   }
 
@@ -489,9 +479,7 @@ gfx::Size AnimatingLayoutManager::GetPreferredSize(const View* host) const {
 
   switch (bounds_animation_mode_) {
     case BoundsAnimationMode::kUseHostBounds: {
-      CHECK(!base::FeatureList::IsEnabled(
-          kAvoidUnnecessaryShouldRenderRichAnimation));
-      return target_layout_manager()->GetPreferredSize(host);
+      NOTREACHED();
     }
     case BoundsAnimationMode::kAnimateMainAxis: {
       // Animating only main axis, so cross axis is preferred size.
