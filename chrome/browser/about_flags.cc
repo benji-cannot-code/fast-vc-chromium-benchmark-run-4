26e6596a6ecd0c44784d9d601889bd5bc511180d
Andrea Orru <andreaorru@chromium.org>
2026-08-20 09:14:52 -0700

    mojo: Remove MOJO_SUPPORT_LEGACY_CORE buildflag and GN arg
    Following the deletion of legacy Mojo Core from //mojo, remove remaining
    references to mojo_support_legacy_core and MOJO_SUPPORT_LEGACY_CORE
    across the tree:
    
    * Remove BUILDFLAG(MOJO_SUPPORT_LEGACY_CORE) blocks guarding kMojoIpcz
      in chrome_restart_request.cc and chromeos_integration_test_mixin.cc.
    * Remove launch_as_mojo_client_browsertest.cc from content_browsertests,
      as the test was gated on mojo_support_legacy_core and tested legacy
      Mojo Core invitation behavior.
    * Remove mojo_support_legacy_core from mojo/features.gni and
      MOJO_SUPPORT_LEGACY_CORE from mojo/BUILD.gn.
    * Remove remaining mentions in comments in
      mojo_proxy/mojo_core/BUILD.gn.
    
    Bug: 359926651
    Change-Id: If87bb03818bf638c0e24926089e7d9d730bab4fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8270033
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Auto-Submit: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683210}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -247,7 +247,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "media/midi/midi_features.h"
 #include "media/webrtc/webrtc_features.h"
-#include "mojo/core/embedder/features.h"
 #include "net/base/features.h"
 #include "net/base/switches.h"
 #include "net/net_buildflags.h"
--- chrome/browser/ash/login/chrome_restart_request.cc
+++ chrome/browser/ash/login/chrome_restart_request.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_switches.h"
 #include "media/capture/capture_switches.h"
 #include "media/media_buildflags.h"
-#include "mojo/core/embedder/features.h"
 #include "sandbox/policy/switches.h"
 #include "third_party/blink/public/common/switches.h"
 #include "third_party/cros_system_api/switches/chrome_switches.h"
@@ -260,9 +259,6 @@ void DeriveFeatures(base::CommandLine* out_command_line) {
 #if BUILDFLAG(ENABLE_PLATFORM_HEVC)
       &media::kPlatformHEVCDecoderSupport,
 #endif
-#if BUILDFLAG(MOJO_SUPPORT_LEGACY_CORE)
-      &mojo::core::kMojoIpcz,
-#endif  // BUILDFLAG(MOJO_SUPPORT_LEGACY_CORE)
   };
   std::vector<std::string> enabled_features;
   std::vector<std::string> disabled_features;
