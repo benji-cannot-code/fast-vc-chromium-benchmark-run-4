c686444cb033fdd6dafc4fdd2f5f9faa8910b46a
mikt <mikt@google.com>
2026-08-20 03:39:02 -0700

    Remove getTemplateKeywordLoc usage from clang plugins
    Bug: 518880792
    Change-Id: Iaddc5a56009216f6119242b674b4fa925dc63e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260583
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683011}

--- tools/clang/raw_ptr_plugin/TypePredicateUtil.h
+++ tools/clang/raw_ptr_plugin/TypePredicateUtil.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "clang/AST/Decl.h"
-#include "clang/AST/DeclTemplate.h"
 #include "clang/AST/Type.h"
 #include "llvm/ADT/ScopeExit.h"
 
@@ -28,7 +27,6 @@ enum class InductionRule : unsigned {
   kBaseClass = (1 << 5),
   kVirtualBaseClass = (1 << 6),
   kField = (1 << 7),
-  kTemplateArgument = (1 << 8),
 };
 
 constexpr InductionRule operator|(InductionRule a, InductionRule b) {
@@ -230,28 +228,6 @@ class TypePredicate {
       }
     }
 
-    // Check template parameters.
-    if constexpr ((Rules & InductionRule::kTemplateArgument) !=
-                  InductionRule::kNone) {
-      if (auto* field_record_template =
-              clang::dyn_cast<clang::ClassTemplateSpecializationDecl>(decl)) {
-        const auto& template_args = field_record_template->getTemplateArgs();
-        for (unsigned i = 0; i < template_args.size(); i++) {
-          if (template_args[i].getKind() != clang::TemplateArgument::Type) {
-            continue;
-          }
-          match->MergeSubResult(
-              GetMatchResult(template_args[i].getAsType().getTypePtrOrNull(),
-                             visited),
-              field_record_template->getTemplateKeywordLoc());
-
-          // Verdict finalized: early return.
-          if (match->verdict_ == MatchResult::kMatch) {
-            return match;
-          }
-        }
-      }
-    }
 
     // All reachable types have been traversed but the root type has not
     // been marked as a match; therefore it must be no match.
