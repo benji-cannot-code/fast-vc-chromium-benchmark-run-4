0366cd7a53c18ff3d5e12bae8dea6c4650aa2330
Nina Satragno <nsatragno@chromium.org>
2026-08-20 06:36:56 -0700

    [crypto] Add Ed25519 support to PublicKeyToCoseKey
    Implement COSE key serialization for Ed25519 public keys in
    crypto::PublicKeyToCoseKey.
    
    TAG=agy
    CONV=bbe1cd03-aa81-478a-88c2-fa4723acc8da
    
    Bug: 546370268
    Change-Id: I93bdb9e79c28e4b4a65ad6777f40fae8d72002e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263948
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683087}

--- crypto/cose.h
+++ crypto/cose.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crypto {
 
 // Converts a PublicKey in |key| to a COSE_Key structure, returning the
-// serialized CBOR bytes. Currently, we support keys using the RSA, EC-P256, and
-// ML-DSA algorithms.
+// serialized CBOR bytes. Currently, we support keys using the RSA, EC-P256,
+// Ed25519, and ML-DSA algorithms.
 CRYPTO_EXPORT std::vector<uint8_t> PublicKeyToCoseKey(
     const keypair::PublicKey& key);
 
--- crypto/cose_unittest.cc
+++ crypto/cose_unittest.cc
@@ -16,6 +16,8 @@ using keypair::PrivateKey;
 using keypair::PublicKey;
 using test::FixedEcP256PublicKeyAsCoseForTesting;
 using test::FixedEcP256PublicKeyForTesting;
+using test::FixedEd25519PublicKeyAsCoseForTesting;
+using test::FixedEd25519PublicKeyForTesting;
 using test::FixedMldsa44PublicKeyAsCoseForTesting;
 using test::FixedMldsa44PublicKeyForTesting;
 using test::FixedMldsa65PublicKeyAsCoseForTesting;
@@ -35,6 +37,11 @@ TEST(CoseTest, EcP256) {
             FixedEcP256PublicKeyAsCoseForTesting());
 }
 
+TEST(CoseTest, Ed25519) {
+  EXPECT_EQ(PublicKeyToCoseKey(FixedEd25519PublicKeyForTesting()),
+            FixedEd25519PublicKeyAsCoseForTesting());
+}
+
 TEST(CoseTest, Mldsa44) {
   EXPECT_EQ(PublicKeyToCoseKey(FixedMldsa44PublicKeyForTesting()),
             FixedMldsa44PublicKeyAsCoseForTesting());
@@ -50,9 +57,9 @@ TEST(CoseTest, Mldsa87) {
             FixedMldsa87PublicKeyAsCoseForTesting());
 }
 
-TEST(CoseTest, EdP256) {
+TEST(CoseTest, EcP384) {
   EXPECT_NOTREACHED_DEATH(PublicKeyToCoseKey(
-      PublicKey::FromPrivateKey(PrivateKey::GenerateEd25519())));
+      PublicKey::FromPrivateKey(PrivateKey::GenerateEcP384())));
 }
 
 }  // namespace crypto
