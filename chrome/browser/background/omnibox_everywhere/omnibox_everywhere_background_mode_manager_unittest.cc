423263e4f613441743de258d1004b54fab827942
Juliet Levesque <julietlevesque@google.com>
2026-08-20 08:58:19 -0700

    [loomnibox] Configure hotkey in settings
    Updates the global accelerator registration in when the user modifies their custom hotkey
    in the loomnibox settings section.
    
    TESTED=Manually verified that I can set a custom hot key and it triggers loomnibox and saves to prefs
    
    Bug: b:543458988
    Change-Id: I99e33040d37e66d1e6a9a2f7f93a10eb2b046541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261752
    Commit-Queue: Juliet Lévesque Knighton <julietlevesque@google.com>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683203}

--- chrome/browser/background/omnibox_everywhere/omnibox_everywhere_background_mode_manager_unittest.cc
+++ chrome/browser/background/omnibox_everywhere/omnibox_everywhere_background_mode_manager_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "components/prefs/pref_service.h"
+#include "components/prefs/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/message_center/public/cpp/notifier_id.h"
@@ -132,6 +133,36 @@ TEST_F(OmniboxEverywhereBackgroundModeManagerTest, ContextMenuStructure) {
   EXPECT_TRUE(menu->GetAcceleratorForCommandId(
       IDC_OMNIBOX_EVERYWHERE_STATUS_ICON_MENU_TOGGLE, &accelerator));
   EXPECT_EQ(accelerator, prefs::GetDefaultOmniboxEverywhereHotkey());
+}
+
+TEST_F(OmniboxEverywhereBackgroundModeManagerTest,
+       ContextMenuUpdatesOnCustomHotkeyChange) {
+  bool callback_called = false;
+  OmniboxEverywhereBackgroundModeManager manager(base::BindRepeating(
+      [](bool* called) { *called = true; }, &callback_called));
+
+  StatusIcon* status_icon = manager.status_icon_for_testing();
+  ASSERT_NE(status_icon, nullptr);
+
+  StatusIconMenuModel* menu = status_icon->GetContextMenuForTesting();
+  ASSERT_NE(menu, nullptr);
+
+  ui::Accelerator accelerator;
+  EXPECT_TRUE(menu->GetAcceleratorForCommandId(
+      IDC_OMNIBOX_EVERYWHERE_STATUS_ICON_MENU_TOGGLE, &accelerator));
+  EXPECT_EQ(accelerator, prefs::GetDefaultOmniboxEverywhereHotkey());
+
+  // Update custom hotkey pref and verify the status icon context menu updates.
+  TestingBrowserProcess::GetGlobal()->GetTestingLocalState()->SetString(
+      prefs::kOmniboxEverywhereHotkey, "Ctrl+Shift+Space");
+
+  menu = status_icon->GetContextMenuForTesting();
+  ASSERT_NE(menu, nullptr);
+  EXPECT_TRUE(menu->GetAcceleratorForCommandId(
+      IDC_OMNIBOX_EVERYWHERE_STATUS_ICON_MENU_TOGGLE, &accelerator));
+  EXPECT_EQ(
+      accelerator,
+      ui::Accelerator(ui::VKEY_SPACE, ui::EF_SHIFT_DOWN | ui::EF_CONTROL_DOWN));
 }
 
 TEST_F(OmniboxEverywhereBackgroundModeManagerTest, ExecuteToggleCommand) {
