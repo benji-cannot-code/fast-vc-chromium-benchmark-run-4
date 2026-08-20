a7c30d5aaa649e460172f0f0da3407a424dc836b
Gabriel Charette <gab@chromium.org>
2026-08-20 14:41:24 -0700

    Cleanup ReloadTabUiResourcesIfChanged feature flag
    This feature has launched and avoids reloading tab icon and title
    resources if they haven't changed. This CL removes the feature flag and
    makes the reload avoidance permanent.
    
    NO_IFTTT=Feature was not defined in ChromeFeatureList.java
    Fixed: 416752339
    b:483482004
    
    Change-Id: I5a57322c93780e504373dbaf4b65fdced3c6a6e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259534
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683469}

--- chrome/browser/android/compositor/decoration_title.cc
+++ chrome/browser/android/compositor/decoration_title.cc
@@ -55,8 +55,7 @@ void DecorationTitle::Update(int title_resource_id,
 }
 
 void DecorationTitle::SetUIResourceIds() {
-  if (!needs_refresh_ && base::FeatureList::IsEnabled(
-                             chrome::android::kReloadTabUiResourcesIfChanged)) {
+  if (!needs_refresh_) {
     return;
   }
   ui::Resource* title_resource = resource_manager_->GetResource(
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -456,7 +456,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kReengagementNotification,
     &kRelatedSearchesAllLanguage,
     &kRelatedSearchesSwitch,
-    &kReloadTabUiResourcesIfChanged,
     &kRemoveTabFocusOnShowingAndSelect,
     &kRobustWindowManagementExperimental,
     &kSafetyFrePromo,
@@ -870,7 +869,6 @@ BASE_FEATURE(kRecordSuppressionMetrics, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kReengagementNotification, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kRelatedSearchesAllLanguage, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kRelatedSearchesSwitch, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kReloadTabUiResourcesIfChanged, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kRemoveTabFocusOnShowingAndSelect, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kRobustWindowManagementExperimental, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSafetyFrePromo, base::FEATURE_DISABLED_BY_DEFAULT);
