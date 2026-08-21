4919bc8ccf280f1e29aa209ce29875da521d595a
Lei Zhang <thestig@chromium.org>
2026-08-21 12:08:26 -0700

    Improve buildflag check in data_protection_clipboard_utils.*
    In c/b/enterprise/data_protection/data_protection_clipboard_utils.h,
    assert `BUILDFLAG(ENTERPRISE_DATA_CONTROLS)` evaluates to true, to
    ensure nobody is trying to include this header when the feature is not
    available. Then in data_protection_clipboard_utils.cc, remove the same
    buildflag check since it should always evaluate to true.
    
    Change-Id: I9da5c87a6ab7116f88f0055cb7ab17ed946a96f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8253847
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684116}

--- chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.h
+++ chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard_metadata.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 
+static_assert(BUILDFLAG(ENTERPRISE_DATA_CONTROLS));
+
 class Profile;
 
 namespace content {
