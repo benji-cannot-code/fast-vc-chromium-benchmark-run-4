14a26381666fa82c46e2cd39712de933bcaf6891
Sky Malice <skym@chromium.org>
2026-08-21 13:52:41 -0700

    [Clean] Remove unused constants in readaloud and data sharing tests
    Remove unused test URLs and locale constants in readaloud and
    data sharing test targets to improve code health.
    
    TAG=agy
    CONV=a972e5a2-4a76-45c9-b8df-b34b82327594
    
    Bug: 546549170
    Change-Id: Ica0e9870b7fc3ab734c721d8946ba0566ca67587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281965
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684203}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/TapToSeekSelectionManagerUnitTest.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/TapToSeekSelectionManagerUnitTest.java
@@ -28,8 +28,6 @@ import org.chromium.content_public.browser.SelectionClient;
 import org.chromium.content_public.browser.SelectionPopupController;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.touch_selection.SelectionEventType;
-import org.chromium.url.GURL;
-import org.chromium.url.JUnitTestGURLs;
 
 /** Unit tests for {@link TapToSeekSelectionManager} */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -39,7 +37,6 @@ public class TapToSeekSelectionManagerUnitTest {
 
     @Mock private ReadAloudController mReadAloudController;
 
-    private static final GURL sTestGURL = JUnitTestGURLs.EXAMPLE_URL;
     @Mock private Tab mTab;
     @Mock private Tab mTab2;
     @Mock private WebContents mWebContents;
