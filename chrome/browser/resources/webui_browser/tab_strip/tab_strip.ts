47f8aead8724c4a6b1046a4feafe1f3369a55595
David Yeung <dayeung@chromium.org>
2026-08-21 11:23:40 -0700

    Fix Add Tab position when detaching to a new window
    When detaching a tab into a new window, the new tab button's bounding
    rectangle was previously measured immediately upon starting the drag
    session. At that stage, the layout might not have stabilized, leading to
    incorrect positioning of the add tab button.
    
    CL fixes this issue by making the update lazily measures the new tab
    button's bounding box once the layout stabilizes. This temporarily
    clearing active transforms to ensure accurate coordinates.
    
    Bug: 536801500
    Change-Id: I83b537a24005513e0b225de2f205f862c6c4ca4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8275811
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684079}

--- chrome/browser/resources/webui_browser/tab_strip/tab_strip.ts
+++ chrome/browser/resources/webui_browser/tab_strip/tab_strip.ts
@@ -254,6 +254,8 @@ export class TabStripElement extends CrLitElement implements
         width: Math.round(width),
         height: Math.round(height),
       });
+
+      this.dragDelegate_.onRecalculateBounds();
     });
     this.resizeObserver_.observe(this);
     this.resizeObserver_.observe(this.$.tabstrip);
