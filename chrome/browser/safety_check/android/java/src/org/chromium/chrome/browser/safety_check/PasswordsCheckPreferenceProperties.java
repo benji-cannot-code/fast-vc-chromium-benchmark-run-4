d3897a472aa6bac5cfddedf864ebe55ddf251d6c
Sky Malice <skym@chromium.org>
2026-08-21 09:06:31 -0700

    [Clean] Migrate PasswordsCheckPreferenceProperties to IntDef keys
    Migrate PASSWORDS_STATE in PasswordsCheckPreferenceProperties to
    WritableIntDefPropertyKey<PasswordsState> to enforce IntDef annotations
    and avoid boxed heap objects.
    
    TAG=agy
    CONV=a1c92985-7ebe-44a7-8db6-f5a987cb4175
    
    Bug: 547916752
    Change-Id: I376b0abc4b82499f306d6ab6db11b475caba6d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8266130
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683946}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/PasswordsCheckPreferenceProperties.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/PasswordsCheckPreferenceProperties.java
@@ -13,6 +13,7 @@ import org.chromium.chrome.browser.pwd_check_wrapper.PasswordCheckController.Pas
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModel.ReadableObjectPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableIntDefPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
@@ -22,7 +23,8 @@ import java.lang.annotation.RetentionPolicy;
 @NullMarked
 class PasswordsCheckPreferenceProperties {
     /** State of the passwords check, one of the {@link PasswordsState} values. */
-    static final WritableIntPropertyKey PASSWORDS_STATE = new WritableIntPropertyKey();
+    static final WritableIntDefPropertyKey<PasswordsState> PASSWORDS_STATE =
+            new WritableIntDefPropertyKey<>(PasswordsState.UNCHECKED);
 
     /** Number of compromised passwords; only used when PASSWORDS_STATE is COMPROMISED_EXIST. */
     static final WritableIntPropertyKey COMPROMISED_PASSWORDS_COUNT = new WritableIntPropertyKey();
