50df82785ebe9b4fa4d0213c68f7f0c1a2d9fe80
Julia Przybytniowska <przybytniowska@google.com>
2026-08-21 00:06:28 -0700

    [Isolated Mode] Support opening bookmarks in isolated windows
    Implement menu entries in the Bookmark Manager to open bookmarks and
    folders in isolated windows. Demo added to in comment under bug.
    
    DD: go/chrome-enterprise-isolated-mode-client-dd
    
    UX Mocks:
    https://docs.google.com/presentation/d/12MY_DSRMLXqa67rru3nJb7rkOxNWxM_NRzftkp7Q7EU/edit?usp=sharing
    
    BUG=b:540240990
    
    Change-Id: I4bc57440b5895936840363a837ad5a2157eaf0be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264280
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683720}

--- chrome/browser/resources/bookmarks/command_manager.ts
+++ chrome/browser/resources/bookmarks/command_manager.ts
@@ -262,6 +262,9 @@ export class BookmarksCommandManagerElement extends
       case Command.OPEN_NEW_WINDOW:
       case Command.OPEN_SPLIT_VIEW:
         return itemIds.size > 0;
+      case Command.OPEN_ISOLATED:
+        return itemIds.size > 0 &&
+            loadTimeData.getBoolean('isIsolatedModeEnabled');
       case Command.ADD_BOOKMARK:
       case Command.ADD_FOLDER:
       case Command.SORT:
@@ -302,7 +305,13 @@ export class BookmarksCommandManagerElement extends
       case Command.OPEN_INCOGNITO:
         return this.expandIds_(itemIds).length > 0 &&
             state.prefs.incognitoAvailability !==
-            IncognitoAvailability.DISABLED;
+            IncognitoAvailability.DISABLED &&
+            !loadTimeData.getBoolean('isIsolatedModeEnabled');
+      case Command.OPEN_ISOLATED:
+        return this.expandIds_(itemIds).length > 0 &&
+            state.prefs.incognitoAvailability !==
+            IncognitoAvailability.DISABLED &&
+            loadTimeData.getBoolean('isIsolatedModeEnabled');
       case Command.OPEN_SPLIT_VIEW:
         return this.expandIds_(itemIds).length === 1 &&
             !this.isActiveTabInSplit_;
@@ -416,6 +425,7 @@ export class BookmarksCommandManagerElement extends
         chrome.bookmarkManagerPrivate.redo();
         break;
       case Command.OPEN_INCOGNITO:
+      case Command.OPEN_ISOLATED:
       case Command.OPEN_NEW_TAB:
       case Command.OPEN_NEW_WINDOW:
       case Command.OPEN_SPLIT_VIEW:
@@ -548,6 +558,7 @@ export class BookmarksCommandManagerElement extends
         command === Command.OPEN || command === Command.OPEN_NEW_TAB ||
         command === Command.OPEN_NEW_WINDOW ||
         command === Command.OPEN_INCOGNITO ||
+        command === Command.OPEN_ISOLATED ||
         command === Command.OPEN_SPLIT_VIEW ||
         command === Command.OPEN_NEW_GROUP);
 
@@ -560,7 +571,8 @@ export class BookmarksCommandManagerElement extends
     }
 
     const openBookmarkIdsCallback = function() {
-      const incognito = command === Command.OPEN_INCOGNITO;
+      const incognito = command === Command.OPEN_INCOGNITO ||
+          command === Command.OPEN_ISOLATED;
       if (command === Command.OPEN_NEW_WINDOW || incognito) {
         BookmarkManagerApiProxyImpl.getInstance().openInNewWindow(
             ids, incognito);
@@ -712,6 +724,10 @@ export class BookmarksCommandManagerElement extends
         return this.getPluralizedOpenAllString_(
             'menuOpenAllIncognito', 'menuOpenIncognito',
             'menuOpenAllIncognitoWithCount');
+      case Command.OPEN_ISOLATED:
+        return this.getPluralizedOpenAllString_(
+            'menuOpenAllIsolated', 'menuOpenIsolated',
+            'menuOpenAllIsolatedWithCount');
       case Command.OPEN_NEW_GROUP:
         return this.getPluralizedOpenAllString_(
             'menuOpenAllNewTabGroup', 'menuOpenNewTabGroup',
@@ -752,6 +768,7 @@ export class BookmarksCommandManagerElement extends
           Command.COPY,
           Command.PASTE,
           Command.OPEN_INCOGNITO,
+          Command.OPEN_ISOLATED,
           Command.OPEN_NEW_GROUP,
           Command.OPEN_NEW_TAB,
           Command.OPEN_NEW_WINDOW,
@@ -771,6 +788,7 @@ export class BookmarksCommandManagerElement extends
             Command.OPEN_SPLIT_VIEW,
             Command.OPEN_NEW_GROUP,
             Command.OPEN_INCOGNITO,
+            Command.OPEN_ISOLATED,
           ];
         }
         return defaultItemTreeCommands;
--- chrome/browser/ui/webui/bookmarks/bookmarks_ui.cc
+++ chrome/browser/ui/webui/bookmarks/bookmarks_ui.cc
@@ -20,11 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/page_not_available_for_guest/page_not_available_for_guest_ui.h"
 #include "chrome/browser/ui/webui/plural_string_handler.h"
 #include "chrome/browser/ui/webui/theme_source.h"
+#include "chrome/common/channel_info.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/bookmarks_resources.h"
 #include "chrome/grit/bookmarks_resources_map.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
+#include "components/enterprise/isolated_mode/settings.h"
 #include "components/favicon_base/favicon_url_parser.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/url_data_source.h"
@@ -104,6 +106,9 @@ content::WebUIDataSource* CreateAndAddBookmarksUIHTMLSource(Profile* profile) {
       {"menuOpenAllIncognito", IDS_BOOKMARK_MANAGER_MENU_OPEN_ALL_INCOGNITO},
       {"menuOpenAllIncognitoWithCount",
        IDS_BOOKMARK_MANAGER_MENU_OPEN_ALL_INCOGNITO_WITH_COUNT},
+      {"menuOpenAllIsolated", IDS_BOOKMARK_MANAGER_MENU_OPEN_ALL_ISOLATED},
+      {"menuOpenAllIsolatedWithCount",
+       IDS_BOOKMARK_MANAGER_MENU_OPEN_ALL_ISOLATED_WITH_COUNT},
       {"menuOpenAllNewTabGroup",
        IDS_BOOKMARK_MANAGER_MENU_OPEN_ALL_NEW_TAB_GROUP},
       {"menuOpenAllNewTabGroupWithCount",
@@ -112,6 +117,7 @@ content::WebUIDataSource* CreateAndAddBookmarksUIHTMLSource(Profile* profile) {
       {"menuOpenNewTabGroup", IDS_BOOKMARK_MANAGER_MENU_OPEN_IN_NEW_TAB_GROUP},
       {"menuOpenNewWindow", IDS_BOOKMARK_MANAGER_MENU_OPEN_IN_NEW_WINDOW},
       {"menuOpenIncognito", IDS_BOOKMARK_MANAGER_MENU_OPEN_INCOGNITO},
+      {"menuOpenIsolated", IDS_BOOKMARK_MANAGER_MENU_OPEN_ISOLATED},
       {"menuOpenSplitView", IDS_BOOKMARK_MANAGER_MENU_OPEN_IN_SPLIT_VIEW},
       {"menuRename", IDS_BOOKMARK_MANAGER_MENU_RENAME},
       {"menuShowInFolder", IDS_BOOKMARK_MANAGER_MENU_SHOW_IN_FOLDER},
@@ -145,6 +151,9 @@ content::WebUIDataSource* CreateAndAddBookmarksUIHTMLSource(Profile* profile) {
 
   source->AddBoolean("menuSimplification",
                      features::IsMenuSimplificationEnabled());
+  source->AddBoolean("isIsolatedModeEnabled",
+                     enterprise_isolated_mode::IsolatedModeReplacesIncognito(
+                         *profile->GetPrefs(), chrome::GetChannel()));
 
   source->AddResourcePath(
       "images/batch_upload_bookmarks_promo.svg",
