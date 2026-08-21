1ae8b63dc4b698291f4084c14a369d294aa4d92a
Scott Yoder <scottyoder@google.com>
2026-08-21 08:08:58 -0700

    [ios] Fix unsatisfiable constraints in NewTabPageHeaderView
    NewTabPageHeaderView previously retained the default value of
    translatesAutoresizingMaskIntoConstraints (YES). During NTP
    initialization, early calls to layoutIfNeeded before attachment to the
    view hierarchy caused UIKit to generate temporary height constraints
    that conflicted with headerViewHeightConstraint.
    
    This change disables translatesAutoresizingMaskIntoConstraints upon
    initialization and updates the header height constant prior to
    invoking layoutIfNeeded in updateFakeboxDisplay.
    
    Change-Id: Ib04e018e302a3c3beeca700aa6ba53563cddd1ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280351
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683919}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_header_view.mm
@@ -310,6 +310,7 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
                      (BOOL)useNewBadgeForCustomizationMenu {
   self = [super initWithFrame:CGRectZero];
   if (self) {
+    self.translatesAutoresizingMaskIntoConstraints = NO;
     _fakeLocationBar = [[FakeLocationBarView alloc] init];
     self.clipsToBounds = YES;
     _useNewBadgeForLensButton = useNewBadgeForLensButton;
@@ -2132,10 +2133,10 @@ CGFloat Interpolate(CGFloat from, CGFloat to, CGFloat percent) {
   self.fakeOmniboxContainer.hidden =
       CanShowTabStrip(self) &&
       (self.searchEngineLogoState == SearchEngineLogoState::kNone);
-  [self layoutIfNeeded];
   self.headerViewHeightConstraint.constant =
       content_suggestions::HeightForLogoHeader(self.searchEngineLogoState,
                                                self.traitCollection);
+  [self layoutIfNeeded];
 }
 
 - (void)addConstraintsForLogoView:(UIView*)logoView
