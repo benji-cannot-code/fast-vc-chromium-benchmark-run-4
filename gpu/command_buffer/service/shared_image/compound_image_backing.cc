8820a567ebd80b2dd49d9ab3ddbc30bb96373954
Saifuddin Hitawala <hitawala@chromium.org>
2026-08-21 13:44:06 -0700

    [gpu] Fix SharedImageBackingType UMA
    SharedImageBackingType UMA currently mostly shows CompoundImage
    backing as type. This change updates CompoundImageBacking to track
    and report the backings it creates/wraps while SIManager reports
    backings only if they aren't Compound.
    
    Bug: None
    Change-Id: Ic8491e9e162948ac65749cb2e08264dc04fab343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8268470
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684189}

--- gpu/command_buffer/service/shared_image/compound_image_backing.cc
+++ gpu/command_buffer/service/shared_image/compound_image_backing.cc
@@ -1042,6 +1042,12 @@ std::unique_ptr<SharedImageBacking> CompoundImageBacking::Create(
       std::move(copy_manager), std::move(buffer_usage)));
 }
 
+// static
+void CompoundImageBacking::RecordBackingTypeUMA(
+    SharedImageBackingType backing_type) {
+  UMA_HISTOGRAM_ENUMERATION("GPU.SharedImage.BackingType", backing_type);
+}
+
 std::unique_ptr<SharedImageBacking> CompoundImageBacking::WrapExternalBacking(
     SharedImageFactory* shared_image_factory,
     scoped_refptr<SharedImageCopyManager> copy_manager,
@@ -1053,6 +1059,8 @@ std::unique_ptr<SharedImageBacking> CompoundImageBacking::WrapExternalBacking(
   // We don't wrap an existing CompoundImageBacking which consists of a shm and
   // a gpu backing.
   CHECK_NE(backing->GetType(), SharedImageBackingType::kCompound);
+
+  RecordBackingTypeUMA(backing->GetType());
 
   backing->SetNotRefCounted();
 
@@ -1155,6 +1163,7 @@ CompoundImageBacking::CompoundImageBacking(
   shm_element.backing = std::move(shm_backing);
   has_shm_backing_ = true;
   elements_.push_back(std::move(shm_element));
+  RecordBackingTypeUMA(SharedImageBackingType::kSharedMemory);
 
   // Whenever CompoundImageBacking is created with a shm backing, mark it as
   // fully cleared.
@@ -2109,7 +2118,7 @@ void CompoundImageBacking::CreateBackingFromBackingFactory(
 
   // Since the owned GPU backing is never registered with SharedImageManager
   // it's not recorded in UMA histogram there.
-  UMA_HISTOGRAM_ENUMERATION("GPU.SharedImage.BackingType", backing->GetType());
+  RecordBackingTypeUMA(backing->GetType());
 
   backing->SetNotRefCounted();
 
