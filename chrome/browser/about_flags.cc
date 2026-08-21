c8b66592e22d3431d640ddd2cf5c4a2ca1b41728
Nancy Xiao <nancylanxiao@google.com>
2026-08-21 12:26:11 -0700

    Remove expired enterprise download flag
    Fixed: 540157455
    Change-Id: I61483eed36f0bd628c81ee0789f0f9578fdc4be0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8275612
    Auto-Submit: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Haihan Chen <haihan@google.com>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684132}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -294,7 +294,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/notifications/chime/android/features.h"
 #include "components/credential_management/android/features.h"
-#include "components/enterprise/connectors/core/features.h"
 #include "components/external_intents/android/external_intents_features.h"
 #include "components/facilitated_payments/core/features/features.h"
 #include "components/messages/android/messages_feature.h"
@@ -5103,11 +5102,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kSafeBrowsingSyncCheckerCheckAllowlistDescription,
      kOsAndroid,
      FEATURE_VALUE_TYPE(safe_browsing::kSafeBrowsingSyncCheckerCheckAllowlist)},
-    {"enable-download-enterprise-scan",
-     flag_descriptions::kEnableDownloadEnterpriseScanName,
-     flag_descriptions::kEnableDownloadEnterpriseScanDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(
-         enterprise_connectors::kEnableDownloadEnterpriseScanOnClank)},
 #endif  // BUILDFLAG(IS_ANDROID)
 
     {"enable-content-analysis-clipboard-copy",
