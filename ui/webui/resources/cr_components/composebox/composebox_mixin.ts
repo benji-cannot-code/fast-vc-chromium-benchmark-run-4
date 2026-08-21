2c1554aec6120bf5cda901c86ed529b1a75d82fe
Josiah Kiehl <kiehl@google.com>
2026-08-21 16:38:44 -0700

    [NTP] Show search loupe icon when Smart Tab Sharing is active
    When Smart Tab Sharing (STS) is activated from the Composebox/Realbox
    context menu, the entrypoint "+" button is replaced with the STS active
    coin pill in the action row, and search loupe icon is incorrectly
    removed as the STS toggle is not recognized as adding tabs to the query.
    
    This is a short term fix for the associated bug. See followup CL
    (https://crrev.com/c/8281744) for refactor to make such bugs less likely
    in future.
    
    Bug: b:549807865
    Test: autoninja -C out/Default browser_tests && xvfb-run -a out/Default/browser_tests --gtest_filter="CrComponentsComposeboxTest.ComposeboxMixin:NewTabPageAppTest.Composebox:NewTabPageTest.Composebox*"
    Change-Id: Ice5b6fe6a0abd90910af725252bc9ec5aad77952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8273738
    Reviewed-by: Sho Mizoe <shomizoe@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684304}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -2650,8 +2650,9 @@ export const ComposeboxEmbedderMixin =
         }
 
         hasTabs(): boolean {
-          return this.tabFaviconChipsToCoinsEnabled &&
-              Array.from(this.files.values()).some(f => !!f.url);
+          return (this.tabFaviconChipsToCoinsEnabled &&
+                  Array.from(this.files.values()).some(f => !!f.url)) ||
+              this.smartTabSharingActive;
         }
 
         shouldShowDivider(): boolean {
