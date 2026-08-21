8deb99e3626c447204140d12792ad35c33286251
Ken Buchanan <kenrb@chromium.org>
2026-08-21 15:56:46 -0700

    [WebAuthn] Remove Ambient feature flag parameter
    There is currently a toggle on the flag on whether to use the page
    action UI. Since this feature is being converted to use permission UI
    instead, that is unnecessary. This CL removes the toggle.
    
    After this change it will only use the original bubble implementation,
    which will be switched to a permission bubble.
    
    Bug: 532206357
    Change-Id: Id3448fcf90180a079411af4811d8f0f62a577ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277950
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684276}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4834,11 +4834,6 @@ const FeatureEntry::Choice kChildProcessSecurityPolicyRustChoices[] = {
      "ChildProcessSecurityPolicyRustProcessState"},
 };
 
-const FeatureEntry::FeatureParam kWebAuthnAmbientSignin_AnchoredMessage[] = {
-    {"display", "anchored_message"}};
-
-const FeatureEntry::FeatureVariation kWebAuthnAmbientSigninVariations[] = {
-    {"Anchored Message", kWebAuthnAmbientSignin_AnchoredMessage, nullptr}};
 
 const FeatureEntry::FeatureParam kCanvasAccessibilityBasic[] = {
     {"CanvasAccessibilityMode", "Basic"}};
@@ -12856,9 +12851,7 @@ const FeatureEntry kFeatureEntries[] = {
     {"web-authentication-ambient-signin",
      flag_descriptions::kWebAuthnAmbientSigninName,
      flag_descriptions::kWebAuthnAmbientSigninDescription, kOsDesktop,
-     FEATURE_WITH_PARAMS_VALUE_TYPE(device::kWebAuthnAmbientSignin,
-                                    kWebAuthnAmbientSigninVariations,
-                                    "WebAuthenticationAmbientSignin")},
+     FEATURE_VALUE_TYPE(device::kWebAuthnAmbientSignin)},
 
     {"devtools-protocol-monitor",
      flag_descriptions::kDevToolsProtocolMonitorName,
--- chrome/browser/ui/webauthn/ambient/ambient_signin_controller.h
+++ chrome/browser/ui/webauthn/ambient/ambient_signin_controller.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBAUTHN_AMBIENT_AMBIENT_SIGNIN_CONTROLLER_H_
 #define CHROME_BROWSER_UI_WEBAUTHN_AMBIENT_AMBIENT_SIGNIN_CONTROLLER_H_
 
-#include <cstdint>
 #include <memory>
 #include <string>
 #include <vector>
@@ -15,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
 #include "content/public/browser/document_user_data.h"
-
-struct AuthenticatorRequestDialogModel;
 
 namespace base {
 class CallbackListSubscription;
