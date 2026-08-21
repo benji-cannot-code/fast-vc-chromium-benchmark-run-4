145af524c6fcffbce680943d920824db48830ff6
Fiaz Muhammad <mfiaz@google.com>
2026-08-21 07:22:32 -0700

    [tristate] Migrate SensitiveContentClient state to @TriState
    - Migrate SensitiveContentClient.mContentRestoredFromTabStateIs
            Sensitive and getContentRestoredFromTabStateIsSensitive()
            from @Nullable Boolean to @TriState int.
    - Update SensitiveContentTest to verify @TriState semantics.
    - Add //base:tristate_java to GN dependencies in
            components/sensitive_content and chrome/android/javatests.
    
    Bug: 545986483
    Change-Id: I11bac8bed487fefc8435cd1696ac5e5803e2049d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258334
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683885}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
@@ -35,6 +35,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.TriState;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
@@ -504,7 +505,7 @@ public class SensitiveContentTest {
                         () ->
                                 SensitiveContentClient.fromWebContents(
                                         secondTabAfterFreeze[0].getWebContents()));
-        assertTrue(Boolean.TRUE.equals(client.getContentRestoredFromTabStateIsSensitive()));
+        assertEquals(TriState.TRUE, client.getContentRestoredFromTabStateIsSensitive());
         assertEquals(
                 View.CONTENT_SENSITIVITY_SENSITIVE,
                 secondTabAfterFreeze[0].getContentView().getContentSensitivity());
