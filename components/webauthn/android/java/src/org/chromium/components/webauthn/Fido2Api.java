b30aab47dc18f2c637c9a9eeca25a77fffaf0625
Sky Malice <skym@chromium.org>
2026-08-21 11:16:04 -0700

    [Clean] Fix inline Javadoc block tags in components/.
    Replaces malformed in-sentence block tags such as {@param ...} and
    {@see ...} with appropriate inline Javadoc tags ({@code ...} and
    {@link ...}) across Java files in components/.
    
    TAG=agy
    CONV=1c4e90e5-0681-4a64-916e-0c4116f84e03
    
    Bug: 546532474
    Change-Id: I2bc39a2c2ca9ca7905957e3fb2db3dbd19ca5f7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8273422
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684065}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2Api.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2Api.java
@@ -98,7 +98,7 @@ public final class Fido2Api {
     public interface Calls {
         /**
          * Serialize a browser's or an app's makeCredential request to a {@link Parcel}. Apps should
-         * not set {@param origin}.
+         * not set {@code origin}.
          *
          * @param options the options passed from the renderer.
          * @param origin the origin that the request should act as.
@@ -121,7 +121,7 @@ public final class Fido2Api {
 
         /**
          * Serialize a browser's or an app's getAssertion request to a {@link Parcel}. Apps should
-         * not set {@param origin}.
+         * not set {@code origin}.
          *
          * @param options the options passed from the renderer.
          * @param origin the origin that the request should act as.
