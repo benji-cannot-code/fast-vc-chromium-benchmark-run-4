00fb74fc640a8f5ef696e7359138caa95b9eb380
Chase Hartsell <chazzy@google.com>
2026-08-21 16:48:05 -0700

    [organizer] Create delegate for open tabs section
    Adds an OpenTabsDelegate for rendering a section with open tabs. For
    now, this delegate returns mock data but will be updated in a future
    CL to show tabs.
    
    Screenshot: http://screen/049nd1ksn8690
    
    Bug: 549784483
    Change-Id: I24f9c80dfecda5774186f9706cde1b098db77ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8272065
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684313}

--- chrome/browser/resources/organizer_panel/app.ts
+++ chrome/browser/resources/organizer_panel/app.ts
@@ -4,13 +4,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
+import './organizer_list.js';
 import '/strings.m.js';
 
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+import type {CrToolbarSearchFieldElement} from 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {getCss} from './app.css.js';
 import {getHtml} from './app.html.js';
+import {OpenTabsDelegate} from './delegates/open_tabs_delegate.js';
+import type {OrganizerListElement} from './organizer_list.js';
+import type {OrganizerListSectionDelegate} from './organizer_list_section_delegate.js';
+
+export interface OrganizerPanelAppElement {
+  $: {
+    list: OrganizerListElement,
+    searchField: CrToolbarSearchFieldElement,
+  };
+}
 
 export class OrganizerPanelAppElement extends CrLitElement {
   static get is() {
@@ -28,10 +40,14 @@ export class OrganizerPanelAppElement extends CrLitElement {
   static override get properties() {
     return {
       shortcut_: {type: String},
+      sectionDelegates_: {type: Array},
     };
   }
 
   protected accessor shortcut_: string = loadTimeData.getString('shortcutText');
+  protected accessor sectionDelegates_: OrganizerListSectionDelegate[] = [
+    new OpenTabsDelegate(),
+  ];
 }
 
 declare global {
