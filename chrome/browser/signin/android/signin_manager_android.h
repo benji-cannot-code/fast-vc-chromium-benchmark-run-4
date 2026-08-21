ab384d70c59ed071dbecf504d34414e078b290f2
Liza Bipin <mlbipin@google.com>
2026-08-21 02:29:25 -0700

    [Signin][Android] Remove unused headers from signin manager
    Bug: 525397809
    Change-Id: I4faab764e78d826d50b3cca58e49cbc84502feb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8266167
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683775}

--- chrome/browser/signin/android/signin_manager_android.h
+++ chrome/browser/signin/android/signin_manager_android.h
@@ -6,17 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SIGNIN_ANDROID_SIGNIN_MANAGER_ANDROID_H_
 #define CHROME_BROWSER_SIGNIN_ANDROID_SIGNIN_MANAGER_ANDROID_H_
 
-#include <memory>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "base/android/scoped_java_ref.h"
+#include "base/functional/callback_forward.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
 #include "base/time/time.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/prefs/pref_member.h"
 #include "google_apis/gaia/gaia_id.h"
 
 namespace policy {
