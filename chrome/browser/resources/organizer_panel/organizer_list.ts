be237adfc0fa1243c8ee7fcc48c58118a3d230f5
Chase Hartsell <chazzy@google.com>
2026-08-21 13:43:18 -0700

    [organizer] Create <organizer-list> element
    Takes in a list of section delegates and renders sections for each of
    them.
    
    Bug: 549784483
    Change-Id: I267a641bce230805e573d7edbc5ce6e3754fff50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8272064
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684188}

---
+++ chrome/browser/resources/organizer_panel/organizer_list.ts
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './organizer_list_section.js';
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {getCss} from './organizer_list.css.js';
+import {getHtml} from './organizer_list.html.js';
+import type {OrganizerListSectionDelegate} from './organizer_list_section_delegate.js';
+
+export class OrganizerListElement extends CrLitElement {
+  static get is() {
+    return 'organizer-list';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      sectionDelegates: {type: Array},
+    };
+  }
+
+  accessor sectionDelegates: OrganizerListSectionDelegate[] = [];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'organizer-list': OrganizerListElement;
+  }
+}
+
+customElements.define(OrganizerListElement.is, OrganizerListElement);
---
+++ chrome/test/data/webui/organizer_panel/test_section_delegate.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {OrganizerListItem} from 'chrome://organizer-panel.top-chrome/organizer_list_item.js';
+import type {OrganizerListSectionDelegate} from 'chrome://organizer-panel.top-chrome/organizer_list_section_delegate.js';
+
+export class TestSectionDelegate implements OrganizerListSectionDelegate {
+  private header_: string;
+  private items_: OrganizerListItem[];
+
+  constructor(header: string, items: OrganizerListItem[] = []) {
+    this.header_ = header;
+    this.items_ = items;
+  }
+
+  getHeader(): string {
+    return this.header_;
+  }
+
+  getItems(): OrganizerListItem[] {
+    return this.items_;
+  }
+}
