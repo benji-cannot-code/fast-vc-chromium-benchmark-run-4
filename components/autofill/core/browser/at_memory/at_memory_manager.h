2ad6c9e98022bb2dfcb9647006ec7eb6c27f5652
Przemek Perkowski <perkowski@google.com>
2026-08-21 07:43:26 -0700

    Make the spacing in between sublabels in AtMemory smaller.
    Replace the default spacing between AtMemory labels with a custom spacing which is 4px.
    
    Before: https://screenshot-v2.corp.google.com/0g5bliq99mnr0
    After: https://screenshot-v2.corp.google.com/2ak2vr2n5e698
    Bug: 537610592
    Change-Id: Ibf5d25d83a12a2127957e801793510406a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8251050
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Przemek Perkowski <perkowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683900}

--- components/autofill/core/browser/at_memory/at_memory_manager.h
+++ components/autofill/core/browser/at_memory/at_memory_manager.h
@@ -158,6 +158,11 @@ class AtMemoryManager {
   // Creates a source attribution suggestion ("Suggested by Gemini").
   static Suggestion CreateSourceAttributionSuggestion();
 
+  // Transforms an AtMemory search result entry into a `Suggestion`.
+  static Suggestion TransformResultIntoSuggestion(
+      const MemorySearchResult& entry,
+      std::string_view app_locale);
+
  private:
   friend class AtMemoryManagerTestApi;
 
