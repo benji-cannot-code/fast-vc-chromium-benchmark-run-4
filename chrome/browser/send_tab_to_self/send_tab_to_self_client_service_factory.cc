dbaa3dcfcd322e6494009def04d478779c92e052
Michael Tatarski <mtatarski@google.com>
2026-08-21 01:37:04 -0700

    [SendTabToSelf] Remove stale system notification dependencies
    This CL removes obsolete dependencies on NotificationDisplayServiceFactory from SendTabToSelfClientServiceFactory and chrome/browser/notifications from BUILD.gn.
    
    Bug: None
    Change-Id: Id76da565a9352a0754f449e9a16b5e2a62f3a65e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8251169
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683751}

--- chrome/browser/send_tab_to_self/send_tab_to_self_client_service_factory.cc
+++ chrome/browser/send_tab_to_self/send_tab_to_self_client_service_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/no_destructor.h"
 #include "build/build_config.h"
-#include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/send_tab_to_self/send_tab_to_self_client_service.h"
 #include "chrome/browser/sync/send_tab_to_self_sync_service_factory.h"
@@ -51,7 +50,6 @@ SendTabToSelfClientServiceFactory::SendTabToSelfClientServiceFactory()
               .WithGuest(ProfileSelection::kNone)
               .WithAshInternals(ProfileSelection::kNone)
               .Build()) {
-  DependsOn(NotificationDisplayServiceFactory::GetInstance());
   DependsOn(SendTabToSelfSyncServiceFactory::GetInstance());
 }
 
