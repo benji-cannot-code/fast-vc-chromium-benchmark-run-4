939fecb7061310a5297f7e282ec6b10ca09c95d3
James Cook <jamescook@chromium.org>
2026-08-21 09:37:19 -0700

    Migrate tracing settings UI test to `SettingsTestRule`
    Migrates Chrome for Android settings UI test classes from `SettingsActivityTestRule` to `SettingsTestRule` to support both the existing `SettingsActivity` on phone and the new `SettingsInTab` feature on tablet and desktop.
    
    BUG=521895796
    
    Change-Id: I675f6891bcff5eb9ed6bc9c9cb02655d9f780ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265950
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683995}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tracing/settings/TracingSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tracing/settings/TracingSettingsTest.java
@@ -40,8 +40,8 @@ import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
-import org.chromium.chrome.browser.settings.SettingsActivity;
-import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
+import org.chromium.chrome.browser.settings.SettingsActivityInterface;
+import org.chromium.chrome.browser.settings.SettingsTestRule;
 import org.chromium.chrome.browser.settings.SettingsNavigationFactory;
 import org.chromium.chrome.browser.tracing.TracingController;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
@@ -69,8 +69,8 @@ public class TracingSettingsTest {
             ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
     @Rule
-    public final SettingsActivityTestRule<TracingSettings> mSettingsActivityTestRule =
-            new SettingsActivityTestRule<>(TracingSettings.class);
+    public final SettingsTestRule<TracingSettings> mSettingsActivityTestRule =
+            new SettingsTestRule<>(TracingSettings.class);
 
     private MockNotificationManagerProxy mMockNotificationManager;
 
@@ -336,7 +336,7 @@ public class TracingSettingsTest {
                 settingsNavigation.createSettingsIntent(
                         context, TracingCategoriesSettings.class, categoriesPref.getExtras());
         mSettingsActivityTestRule.launchActivity(intent);
-        SettingsActivity categoriesActivity = mSettingsActivityTestRule.getActivity();
+        SettingsActivityInterface categoriesActivity = (SettingsActivityInterface) mSettingsActivityTestRule.getActivity();
 
         PreferenceFragmentCompat categoriesFragment =
                 (PreferenceFragmentCompat) categoriesActivity.getMainFragment();
