4377acf7eacae31056df07d51422940870d8489e
Steven Luong <stluong@chromium.org>
2026-08-21 18:47:31 -0700

    [ChroMenu] Introduce the fuzzy search item
    Introduces the fuzzy search item interface which will be used to
    represent searchable terms via fuzzy search.
    
    Fixed: 549168778
    Change-Id: I43b77be27d54a964b3f6313e984d292e519bcbb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8271342
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684410}

---
+++ chrome/browser/ui/fuzzy_search/fuzzy_search_item.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_FUZZY_SEARCH_FUZZY_SEARCH_ITEM_H_
+#define CHROME_BROWSER_UI_FUZZY_SEARCH_FUZZY_SEARCH_ITEM_H_
+
+#include <string>
+#include <vector>
+
+// The FuzzySearchItem is an interface used to represent the searchable terms
+// through the fuzzy search finder.
+class FuzzySearchItem {
+ public:
+  virtual ~FuzzySearchItem() = default;
+
+  // Returns the primary display title/label of the search item.
+  virtual const std::u16string& GetTitle() const = 0;
+
+  // Returns the secondary or contextual text (e.g., section header) for the
+  // item.
+  virtual const std::u16string& GetSecondaryText() const = 0;
+
+  // Returns alternative terms/keywords used for fuzzy search matching.
+  virtual const std::vector<std::u16string>& GetSynonyms() const = 0;
+};
+
+#endif  // CHROME_BROWSER_UI_FUZZY_SEARCH_FUZZY_SEARCH_ITEM_H_
---
+++ chrome/browser/ui/fuzzy_search/fuzzy_search_properties.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/fuzzy_search/fuzzy_search_properties.h"
+
+namespace fuzzy_search {
+
+DEFINE_OWNED_UI_CLASS_PROPERTY_KEY(std::vector<std::u16string>, kSynonymsKey)
+
+}  // namespace fuzzy_search
+
+DEFINE_UI_CLASS_PROPERTY_TYPE(std::vector<std::u16string>*)
