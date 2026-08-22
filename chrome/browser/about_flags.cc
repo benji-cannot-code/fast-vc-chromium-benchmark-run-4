81ae50e212b12aa7d6bca3c0b3015c1c71cf2f1e
Austin Zhang <austinzzr@google.com>
2026-08-21 18:08:43 -0700

    Add privileged-web-contents flag to chrome://flags
    Exposes the PrivilegedWebContents feature flag in chrome://flags to
    allow enabling PWC infrastructure without requiring command line switches.
    
    TAG=agy
    CONV=b609a06b-7dd8-4783-8b21-4f42f3dd3bfd
    
    Bug: 544697086
    Change-Id: Ic58fa57dae4b74791b58967bf8e6250606b074d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280175
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Austin Zhang <austinzzr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684375}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/preloading/prefetch/search_prefetch/field_trial_settings.h"
 #include "chrome/browser/preloading/preloading_features.h"
 #include "chrome/browser/preloading/search_preload/search_preload_features.h"
+#include "chrome/browser/pwc/pwc_features.mojom-features.h"
 #include "chrome/browser/sharing_hub/sharing_hub_features.h"
 #include "chrome/browser/site_isolation/about_flags.h"
 #include "chrome/browser/task_manager/common/task_manager_features.h"
@@ -11139,6 +11140,9 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnterprisePublishedSkillsPolicyEnabledDescription,
      kOsDesktop,
      FEATURE_VALUE_TYPE(features::kEnterprisePublishedSkillsPolicyEnabled)},
+    {"privileged-web-contents", flag_descriptions::kPrivilegedWebContentsName,
+     flag_descriptions::kPrivilegedWebContentsDescription, kOsDesktop,
+     FEATURE_VALUE_TYPE(pwc::mojom::features::kPrivilegedWebContents)},
     {"skills", flag_descriptions::kSkillsEnabledName,
      flag_descriptions::kSkillsEnabledDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(features::kSkillsEnabled)},
