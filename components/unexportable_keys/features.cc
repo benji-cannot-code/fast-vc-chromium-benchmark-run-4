f55c7918e0dabd17241f970647eb16480ace6a74
Konstantin Andrikopoulos <andrikopoulos@google.com>
2026-08-24 03:21:13 -0700

    [DBSC] Enable core DBSC features by default on macOS
    Enable core Device Bound Session Credentials (DBSC) features by default
    on macOS, matching the 100% rollout configuration in Finch:
    - Enable net::features::kDeviceBoundSessions on macOS.
    - Set default kDeviceBoundSessionsSchemaVersion to 4 on macOS.
    - Enable network::features::kUseUnexportableKeyServiceInBrowserProcess on macOS.
    - Enable unexportable_keys::kUnexportableKeyDeletion on macOS.
    - Update DeviceBoundSessionCredentialsMac in fieldtrial_testing_config.json
      to only retain restricted sites trial features.
    
    TAG=agy
    CONV=c80b6e4d-e21d-4df8-85b1-3820fd179f04
    
    Bug: 443933800
    Change-Id: If2ed1876417884cd4d7c6dcca22b471d54b607a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265127
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Konstantin Andrikopoulos <andrikopoulos@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684664}

--- components/unexportable_keys/features.cc
+++ components/unexportable_keys/features.cc
@@ -5,12 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/unexportable_keys/features.h"
 
+#include "build/build_config.h"
+
 namespace unexportable_keys {
 
 BASE_FEATURE(kEnableBoundSessionCredentialsSoftwareKeysForManualTesting,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kUnexportableKeyDeletion, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUnexportableKeyDeletion,
+#if BUILDFLAG(IS_MAC)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kEnableUnexportableKeysSpareKeyPool,
              base::FEATURE_DISABLED_BY_DEFAULT);
