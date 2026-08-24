097eb9fbac71ee1c8f2ad0de0cf0d092208d329b
Dmytro Yeroshkin <yeroshkin@chromium.org>
2026-08-24 13:48:29 -0700

    Switch AzureAD joined logic to Device joined
    We remove the legacy `AzureActiveDirectoryStatusProvider` in favour of
    `AzureActiveDirectoryDeviceStatusProvider`. This also allows us to reuse
    the pre-existing kill switch in the latter provider.
    
    Bug: 531448879
    Change-Id: I6ff30408cd9b5ecba922fc4a15338d3ef0b93b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282055
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Dmytro Yeroshkin <yeroshkin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684982}

--- components/policy/core/common/management/platform_management_status_provider_win.cc
+++ components/policy/core/common/management/platform_management_status_provider_win.cc
@@ -32,13 +32,6 @@ EnterpriseMDMManagementStatusProvider::FetchAuthority() {
   return base::win::IsDeviceRegisteredWithManagement() ? CLOUD : NONE;
 }
 
-AzureActiveDirectoryStatusProvider::AzureActiveDirectoryStatusProvider()
-    : ManagementStatusProvider(policy_prefs::kAzureActiveDirectoryManagement) {}
-
-EnterpriseManagementAuthority
-AzureActiveDirectoryStatusProvider::FetchAuthority() {
-  return base::win::IsJoinedToAzureAD() ? CLOUD_DOMAIN : NONE;
-}
 
 AzureActiveDirectoryDeviceStatusProvider::
     AzureActiveDirectoryDeviceStatusProvider()
