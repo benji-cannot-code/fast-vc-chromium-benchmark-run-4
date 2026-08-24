fb82db51306ce717606ca35046dbe69caa21c8a5
Quinton Lucyk <qlucyk@chromium.org>
2026-08-24 05:53:44 -0700

    [search-promotion] Decouple execution and targeting Finch parameters
    Refactor the SearchPromotion subsystem to decouple execution logic and
    user targeting into orthogonal Finch parameters:
    - "action": ("open", "install", "control")
    - "cohort": ("low", "medium", "power", "all")
    
    The experiment parameters are being expanded to support multiple stages
    of experimentation with distinct cohort tracking. Decoupling the
    execution action (open, install, control) from the targeting cohort
    (low, medium, power, all) gives us the flexibility to independently
    configure, test, and launch any action against any cohort without
    requiring future code changes.
    
    Bug: 467255671
    Change-Id: Id25107791ae341d9aa49d9fb1663c08cac67979f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267518
    Commit-Queue: Quinton Lucyk <qlucyk@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684701}

--- chrome/browser/ui/search_promotion/register_search_promotion_task.h
+++ chrome/browser/ui/search_promotion/register_search_promotion_task.h
@@ -22,11 +22,11 @@ enum class SearchPromotionExitCode {
   kInvalidPostInstallUrl = 101,
   kRegistryWriteFailed = 102,
 
-  // Arm A:
+  // Open action:
   kUrlLaunchFailed = 103,
   kUrlLaunchSuccess = 104,
 
-  // Arm B:
+  // Install action:
   kTimeout = 105,
   kSuccessBackground = 106,
   kSuccessWithForegroundFallback = 107,
