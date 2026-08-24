c2c7faa227da47f71d9a8bef869044c97780d903
Gayane Petrosyan <gayane@google.com>
2026-08-24 14:18:56 -0700

    [iOS] Add link opening handler directly to startup config.
    Currently gateway is only used in startup configuration to get the
    link opening handler. Add link opening handler directly to startup
    config directly so that gateway can be removed from it.
    
    This is so that we can move startup configuration to
    GeminiBrowserAgent to be created and passed to SDK on startup
    while the gateway stays bound to Gemini container lifecycle.
    
    Order of submission:
    1. this CL
    2. Internal CL using the new LinkOpeningHandler
    https://chrome-internal-review.git.corp.google.com/c/chrome/ios_internal/+/9700256
    3. Removing LinkOpeningHandler from gateway and moving ConfigurGemini
    https://chromium-review.git.corp.google.com/c/chromium/src/+/8270652
    4. Remove internal LinkOpeningHandler
    https://chrome-internal-review.git.corp.google.com/c/chrome/ios_internal/+/9683870
    
    Bug: 549393970
    Change-Id: I195e1f3b09a4001c60004ea61cd9b9902140dc69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8271244
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685004}

--- ios/chrome/browser/intelligence/bwg/coordinator/gemini_container_mediator.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/gemini_container_mediator.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_configuration.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_gateway_manager.h"
+#import "ios/chrome/browser/intelligence/bwg/model/gemini_link_opening_handler.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_page_context.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_page_state_change_handler.h"
 #import "ios/chrome/browser/intelligence/bwg/model/gemini_service.h"
@@ -132,6 +133,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[GeminiStartupConfiguration alloc] init];
   config.authService = authService;
   config.gateway = self.gateway;
+  config.linkOpeningHandler = _gatewayManager.linkOpeningHandler;
   config.imageRemixEnabled =
       gemini::IsFeatureAvailable(gemini::Feature::kImageRemix, _profile);
   config.geminiLiveEnabled =
