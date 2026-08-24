833243d406e7d3ea9526d92c1de8174f0a21a881
Paul Jensen <pauljensen@chromium.org>
2026-08-24 09:36:57 -0700

    [WebUI Toolbar] Add PageActionTestAccessor visibility test helpers
    Introduce PageActionTestAccessor in webui_test_utils with visibility
    methods (GetVisible, IsChipVisible, IsIconVisible, IsAnimating) to
    support inspecting page action visibility uniformly across Views and
    WebUI implementations without coupling tests to UI implementation
    details.
    
    Bug: 545160323
    Change-Id: Ia9433badae64ccc9a87b16e5a002ff3d0e2ae5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263604
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Tianyang Xu <xtlsheep@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Tianyang Xu <xtlsheep@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684809}

---
+++ chrome/browser/ui/views/page_action/test_support/page_action_test_accessor.h
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_TEST_SUPPORT_PAGE_ACTION_TEST_ACCESSOR_H_
+#define CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_TEST_SUPPORT_PAGE_ACTION_TEST_ACCESSOR_H_
+
+#include <string_view>
+
+#include "base/memory/raw_ptr.h"
+#include "ui/actions/action_id.h"
+
+class BrowserWindowInterface;
+
+namespace content {
+class WebContents;
+}
+
+namespace ui {
+class TrackedElementWebUI;
+}
+
+namespace views {
+class View;
+}
+
+namespace page_actions {
+
+class PageActionTestAccessor {
+ public:
+  PageActionTestAccessor(BrowserWindowInterface* browser,
+                         actions::ActionId action_id);
+  PageActionTestAccessor(const PageActionTestAccessor&) = delete;
+  PageActionTestAccessor& operator=(const PageActionTestAccessor&) = delete;
+  ~PageActionTestAccessor();
+
+  bool GetVisible();
+  bool IsChipVisible();
+  bool IsIconVisible();
+  bool IsAnimating();
+
+ private:
+  bool EvaluateWebUI(std::string_view element_predicate_js);
+  ui::TrackedElementWebUI* GetTrackedElement();
+  views::View* GetView();
+  content::WebContents* GetWebContents();
+
+  raw_ptr<BrowserWindowInterface> browser_;
+  actions::ActionId action_id_;
+};
+
+}  // namespace page_actions
+
+#endif  // CHROME_BROWSER_UI_VIEWS_PAGE_ACTION_TEST_SUPPORT_PAGE_ACTION_TEST_ACCESSOR_H_
