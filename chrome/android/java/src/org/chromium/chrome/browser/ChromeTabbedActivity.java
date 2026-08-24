9219f5585189f05c16399dfd32618e8ac7e11f33
Ritika Gupta <ritikagup@google.com>
2026-08-24 13:06:07 -0700

    [Background Actuation] Restore background tabs on warm start
    During a warm start, TabState is already initialized and
    onTabStateInitialized is not fired again. This manually checks if we
    should restore background tabs when the Activity is brought to the
    foreground during a warm start.
    
    Bug: 549768651
    Change-Id: I599af11cea0f685abfed87b0c1b23ea3ddb31d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269541
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684941}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1823,6 +1823,18 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
 
         super.onStartWithNative();
 
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.GLIC_BACKGROUND_ACTUATION)) {
+            TabModelSelector tabModelSelector = getTabModelSelector();
+            assert tabModelSelector != null;
+            TabModelUtils.runOnTabStateInitialized(
+                    tabModelSelector,
+                    (selector) -> {
+                        ActorForegroundServiceController.get()
+                                .restoreActiveWindowBackgroundTabs(
+                                        selector, getWindowAndroid(), getTabDelegateFactory());
+                    });
+        }
+
         FirstDrawDetector.waitForFirstDrawStrict(
                 mContentContainer,
                 () -> {
@@ -3834,14 +3846,6 @@ public class ChromeTabbedActivity extends ChromeActivity implements PreAttachInt
                     public void onTabStateInitialized() {
                         if (mMultiInstanceManager != null) {
                             mMultiInstanceManager.onTabStateInitialized();
-                        }
-                        if (ChromeFeatureList.isEnabled(
-                                ChromeFeatureList.GLIC_BACKGROUND_ACTUATION)) {
-                            ActorForegroundServiceController.get()
-                                    .restoreActiveWindowBackgroundTabs(
-                                            getTabModelSelector(),
-                                            getWindowAndroid(),
-                                            getTabDelegateFactory());
                         }
                     }
                 };
