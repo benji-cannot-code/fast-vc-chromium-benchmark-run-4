d9327d4a3473affbacbe4a2fcd0f81194ea57098
Sky Malice <skym@chromium.org>
2026-08-24 12:35:13 -0700

    [Clean] Remove unused variables in SSL and banners tests
    Remove unused preference strings, radio button fields, event strings,
    and package names in SSL and banner test targets.
    
    TAG=agy
    CONV=b191178e-6b1c-4d00-a709-8c4c34fd5548
    
    Bug: 546549170
    Change-Id: I16d90961b4599d5bd56a6fc933a8c21068f3db98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8285977
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684922}

--- chrome/browser/ssl/android/javatests/src/org/chromium/chrome/browser/ssl/HttpsFirstModeSettingsFragmentTest.java
+++ chrome/browser/ssl/android/javatests/src/org/chromium/chrome/browser/ssl/HttpsFirstModeSettingsFragmentTest.java
@@ -28,7 +28,6 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.AdvancedProtectionTestRule;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.widget.RadioButtonWithDescription;
-import org.chromium.components.browser_ui.widget.RadioButtonWithDescriptionLayout;
 import org.chromium.components.policy.test.annotations.Policies;
 
 /** Tests for {@link HttpsFirstModeSettingsFragment}. */
@@ -59,9 +58,6 @@ public class HttpsFirstModeSettingsFragmentTest {
     private HttpsFirstModeSettingsFragment mHttpsFirstModeSettingsFragment;
     private ChromeSwitchPreference mHttpsFirstModeTogglePref;
     private HttpsFirstModeVariantPreference mHttpsFirstModeVariantPref;
-    private RadioButtonWithDescriptionLayout mHttpsFirstModeVariantGroup;
-    private RadioButtonWithDescription mHttpsFirstModeVariantStrict;
-    private RadioButtonWithDescription mHttpsFirstModeVariantBalanced;
     private Preference mEnforcedByAdvancedProtectionWarning;
 
     private void startSettings() {
