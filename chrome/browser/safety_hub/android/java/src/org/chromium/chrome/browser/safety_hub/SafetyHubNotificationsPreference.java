da51275960a0369b82b28d09b9e49446764b5c64
Sky Malice <skym@chromium.org>
2026-08-24 09:54:01 -0700

    [Clean] Remove redundant semicolons in chrome/
    Remove redundant semicolons after declarations in chrome/.
    
    TAG=agy
    CONV=413648d3-bf67-4382-b3e3-d5453f2a7a7f
    
    Bug: 550418935
    Change-Id: I73ced38d6d593485e62db1480e72b2f2b50125df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8283467
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684823}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubNotificationsPreference.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubNotificationsPreference.java
@@ -106,7 +106,6 @@ public class SafetyHubNotificationsPreference extends ChromeBasePreference
                 assert false : "Not a valid menu item Id.";
         }
     }
-    ;
 
     NotificationPermissions getNotificationsPermissions() {
         return mNotificationPermissions;
