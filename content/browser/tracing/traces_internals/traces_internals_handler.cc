844b761bdb33f19115b63a41566e51df2a47a7c8
Etienne Pierre-doray <etiennep@chromium.org>
2026-08-24 10:33:39 -0700

    [tracing] Query track event categories via Perfetto service in TracingController
    This CL updates TracingController and TracesInternalsHandler to query
    categories dynamically from the Perfetto service via
    tracing::QueryTrackEventCategories instead of inspecting local static
    category registries (base, v8, webrtc and missing angle, dawn).
    
    It also migrates TracingController::GetTrackEventDescriptor and the
    corresponding DevTools Tracing.getTrackEventDescriptor command to
    asynchronously query categories from the service.
    
    Change-Id: Idfb2091489dbbf2de966b1e0f27db5df6a755f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269454
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684839}

--- content/browser/tracing/traces_internals/traces_internals_handler.cc
+++ content/browser/tracing/traces_internals/traces_internals_handler.cc
@@ -24,10 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/public/cpp/background_tracing/trace_report_database.h"
 #include "services/tracing/public/cpp/background_tracing/trace_upload_list.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_session.h"
+#include "third_party/perfetto/protos/perfetto/common/data_source_descriptor.gen.h"
+#include "third_party/perfetto/protos/perfetto/common/tracing_service_state.gen.h"
+#include "third_party/perfetto/protos/perfetto/common/track_event_descriptor.gen.h"
 #include "third_party/perfetto/protos/perfetto/config/trace_config.gen.h"
 #include "third_party/snappy/src/snappy.h"
-#include "third_party/webrtc_overrides/init_webrtc.h"
-#include "v8/include/v8-trace-categories.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "base/functional/bind.h"
@@ -109,29 +110,6 @@ class TraceReader : public base::RefCountedThreadSafe<TraceReader> {
 
   ~TraceReader() = default;
 };
-
-void AddCategoriesToList(
-    const perfetto::internal::TrackEventCategoryRegistry& registry,
-    std::vector<traces_internals::mojom::TraceCategoryPtr>& categories) {
-  for (size_t i = 0; i < registry.category_count(); ++i) {
-    const auto* category = registry.GetCategory(i);
-    auto mojom_category = traces_internals::mojom::TraceCategory::New();
-    mojom_category->name = category->name;
-    mojom_category->is_group = category->IsGroup();
-    if (category->description) {
-      mojom_category->description = category->description;
-    }
-    std::vector<std::string> tags_vector;
-    for (const char* tag : category->tags) {
-      if (!tag) {
-        break;
-      }
-      tags_vector.push_back(tag);
-    }
-    mojom_category->tags = std::move(tags_vector);
-    categories.push_back(std::move(mojom_category));
-  }
-}
 
 }  // namespace
 
@@ -280,16 +258,27 @@ void TracesInternalsHandler::StopTraceSession(
 
 void TracesInternalsHandler::GetTrackEventCategories(
     GetTrackEventCategoriesCallback callback) {
-  std::vector<traces_internals::mojom::TraceCategoryPtr> categories;
-
-  AddCategoriesToList(base::perfetto_track_event::internal::kCategoryRegistry,
-                      categories);
-#ifdef V8_USE_PERFETTO
-  AddCategoriesToList(v8::GetTrackEventCategoryRegistry(), categories);
-#endif  // V8_USE_PERFETTO
-  AddCategoriesToList(GetWebRtcTrackEventCategoryRegistry(), categories);
-
-  std::move(callback).Run(std::move(categories));
+  tracing::QueryTrackEventCategories(
+      CreateTracingSession(),
+      base::BindOnce(
+          [](GetTrackEventCategoriesCallback callback,
+             std::vector<perfetto::protos::gen::TrackEventCategory>
+                 categories) {
+            std::vector<traces_internals::mojom::TraceCategoryPtr>
+                mojom_categories;
+            for (const auto& category : categories) {
+              auto mojom_category =
+                  traces_internals::mojom::TraceCategory::New();
+              mojom_category->name = category.name();
+              mojom_category->is_group = false;
+              mojom_category->description = category.description();
+              mojom_category->tags = category.tags();
+              mojom_categories.push_back(std::move(mojom_category));
+            }
+            std::move(callback).Run(std::move(mojom_categories));
+          },
+          std::move(callback)),
+      task_runner_);
 }
 
 void TracesInternalsHandler::GetBufferUsage(GetBufferUsageCallback callback) {
