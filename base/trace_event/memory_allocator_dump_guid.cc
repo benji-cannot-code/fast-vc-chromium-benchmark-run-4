f5effba5e1eeb88c6a84a3950e98db7bd9034c6a
Elly <ellyjones@chromium.org>
2026-08-24 11:47:02 -0700

    base/trace_event: use SHA256 for memory dump GUIDs
    Instead of SHA1. Since //base can't depend on //crypto, this uses
    the BoringSSL SHA256 API directly.
    
    Bug: 430344933
    Change-Id: I21723a60b8bbfb76e3960dda7d88f1be0e7126b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259782
    Auto-Submit: Elly <ellyjones@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684881}

--- base/trace_event/memory_allocator_dump_guid.cc
+++ base/trace_event/memory_allocator_dump_guid.cc
@@ -7,16 +7,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/format_macros.h"
-#include "base/hash/sha1.h"
 #include "base/numerics/byte_conversions.h"
 #include "base/strings/stringprintf.h"
+#include "third_party/boringssl/src/include/openssl/sha2.h"
 
 namespace base::trace_event {
 
 namespace {
 
 uint64_t HashString(const std::string& str) {
-  SHA1Digest digest = SHA1Hash(base::as_byte_span(str));
+  std::array<uint8_t, SHA256_DIGEST_LENGTH> digest;
+  ::SHA256(reinterpret_cast<const uint8_t*>(str.data()), str.size(),
+           digest.data());
   return base::U64FromLittleEndian(base::span(digest).first<8u>());
 }
 
