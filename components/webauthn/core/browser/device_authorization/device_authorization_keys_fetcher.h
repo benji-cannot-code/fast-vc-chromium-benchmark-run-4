c99d720791adc642f05d7353b1355d225d49bab7
Rafał Godlewski <rgod@google.com>
2026-08-24 03:50:21 -0700

    [DeviceAuth] Add request/response protos for the keys fetcher
    * Introduces GetDeviceAuthorizationKeyRequest and
    GetDeviceAuthorizationKeyResponse protos
    * Updates DeviceAuthorizationKeysFetcher to serialize requests and
    parse responses into base::expected
    
    Bug: 405036154
    Change-Id: I5b329f6baeb8add4c6a4688290f6cdda22e8fbb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8279550
    Reviewed-by: Thomas Thrainer <thomasth@google.com>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684673}

--- components/webauthn/core/browser/device_authorization/device_authorization_keys_fetcher.h
+++ components/webauthn/core/browser/device_authorization/device_authorization_keys_fetcher.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/types/expected.h"
 #include "base/version_info/channel.h"
 
 namespace endpoint_fetcher {
@@ -25,6 +26,11 @@ namespace signin {
 class IdentityManager;
 }  // namespace signin
 
+namespace sync_pb {
+class GetDeviceAuthorizationKeyRequest;
+class GetDeviceAuthorizationKeyResponse;
+}  // namespace sync_pb
+
 namespace webauthn {
 
 inline constexpr char kDeviceAuthorizationKeyEndpointUrl[] =
@@ -35,8 +41,16 @@ inline constexpr char kDeviceAuthorizationKeyEndpointUrl[] =
 // passkeys.
 class DeviceAuthorizationKeysFetcher {
  public:
+  // Represents the possible error outcomes when fetching the keys.
+  enum class Error {
+    kNetworkError,
+    kHttpError,
+    kProtoParseError,
+    kAlreadyInProgress,
+  };
+
   using FetchKeysCallback = base::OnceCallback<void(
-      std::unique_ptr<endpoint_fetcher::EndpointResponse>)>;
+      base::expected<sync_pb::GetDeviceAuthorizationKeyResponse, Error>)>;
 
   explicit DeviceAuthorizationKeysFetcher(version_info::Channel channel);
   DeviceAuthorizationKeysFetcher(const DeviceAuthorizationKeysFetcher&) =
@@ -46,14 +60,13 @@ class DeviceAuthorizationKeysFetcher {
   ~DeviceAuthorizationKeysFetcher();
 
   // Initiates the request to fetch device authorization keys.
-  // TODO(crbug.com/405036154): Handle passing request params.
   void FetchDeviceAuthorizationKeys(
+      const sync_pb::GetDeviceAuthorizationKeyRequest& request,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
       signin::IdentityManager* identity_manager,
       FetchKeysCallback callback);
 
  private:
-  // TODO(crbug.com/405036154): Handle parsing response.
   void OnFetchCompleted(
       FetchKeysCallback callback,
       std::unique_ptr<endpoint_fetcher::EndpointResponse> response);
