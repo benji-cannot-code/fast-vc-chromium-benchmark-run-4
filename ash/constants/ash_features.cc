6ad36f86c836baaa56f93e5d6dc7de864715d216
quacksort <quacksort@google.com>
2026-08-24 01:24:40 -0700

    [CodeHealthRotation] Clean up base::Feature "OobeJellyModal"
    This flag is no longer used and can be removed
    
    Bug: 356235094
    Change-Id: Ie68adfe37357e846b42534addb6c5d8842f6540f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8261117
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Lucia Giorgi <quacksort@google.com>
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684630}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1384,9 +1384,6 @@ BASE_FEATURE(kOobeSplitModifierKeyboardInfo, base::FEATURE_ENABLED_BY_DEFAULT);
 // Enables OOBE Jelly features.
 BASE_FEATURE(kOobeJelly, base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Enables OOBE Jelly modal features.
-BASE_FEATURE(kOobeJellyModal, base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables OOBE perks discovery feature.
 BASE_FEATURE(kOobePerksDiscovery, base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -2911,10 +2908,6 @@ bool IsSplitKeyboardRefactorEnabled() {
 
 bool IsOobeAiIntroEnabled() {
   return base::FeatureList::IsEnabled(kFeatureManagementOobeAiIntro);
-}
-
-bool IsOobeJellyModalEnabled() {
-  return IsOobeJellyEnabled() && base::FeatureList::IsEnabled(kOobeJellyModal);
 }
 
 bool IsBootAnimationEnabled() {
