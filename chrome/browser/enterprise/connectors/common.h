289fff2711b247969766cf32381e71fe6fbb1db0
Nibir Nawfil Afif <nanibir@google.com>
2026-08-24 14:30:06 -0700

    Removing default argument from CollectFrameUrls() to reduce the chance of omitting the initiating frame ID by oversight,
    Bug: 536942336
    Change-Id: If0c1a2e3f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257580
    Reviewed-by: Shanthanu Bhardwaj <xanth@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685018}

--- chrome/browser/enterprise/connectors/common.h
+++ chrome/browser/enterprise/connectors/common.h
@@ -79,8 +79,7 @@ std::string GetProfileEmail(Profile* profile);
 google::protobuf::RepeatedPtrField<std::string> CollectFrameUrls(
     content::WebContents* web_contents,
     DeepScanAccessPoint access_point,
-    std::optional<content::GlobalRenderFrameHostId> initiating_frame_id =
-        std::nullopt);
+    std::optional<content::GlobalRenderFrameHostId> initiating_frame_id);
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 
