73f6b8c2d8b9be124bc9c1c6c579a0066f01e3c2
Sky Malice <skym@chromium.org>
2026-08-24 09:52:18 -0700

    [Clean] Use WritableIntDefPropertyKey in Settings, CCT, Tips, ReadAloud
    Migrates IntDef property keys across Settings, Custom Tabs, Tips Promo,
    and ReadAloud to WritableIntDefPropertyKey to ensure type safety.
    
    TAG=agy
    CONV=4ef2ec5e-33d1-40b3-bdd8-ee57a1942440
    
    Bug: 547916752
    Change-Id: I2e4dbb7d4924742ab3bba8e8928ddf7c5bdd2542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280801
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684820}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/mini/Properties.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/mini/Properties.java
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.readaloud.player.mini;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.readaloud.player.VisibilityState;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableIntDefPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
@@ -15,7 +17,8 @@ import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 @NullMarked
 class Properties {
     // VisibilityState for signaling the progress of transitions for the player as a whole.
-    public static final WritableIntPropertyKey VISIBILITY = new WritableIntPropertyKey();
+    public static final WritableIntDefPropertyKey<VisibilityState> VISIBILITY =
+            new WritableIntDefPropertyKey<>(VisibilityState.GONE);
     public static final WritableBooleanPropertyKey ANIMATE_VISIBILITY_CHANGES =
             new WritableBooleanPropertyKey();
     public static final WritableObjectPropertyKey<MiniPlayerMediator> MEDIATOR =
