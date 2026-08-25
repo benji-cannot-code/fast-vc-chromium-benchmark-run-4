d663f9cdfd56f6aef5614235b6fa5a20c06d2a89
Danilo Tedeschi <danft@google.com>
2026-08-24 17:02:17 -0700

    Migrate aw_main_delegate.cc to use the new ICU locale setter
    Change-Id: I357f3e1e2cd341419835f09221390f4f057aa4a8
    Bug: 542711615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8279692
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685089}

--- base/i18n/icubridge/default_icu_locale.h
+++ base/i18n/icubridge/default_icu_locale.h
@@ -36,6 +36,7 @@ class WebEngineMainDelegate;
 class WebEngineBrowserMainParts;
 
 namespace android_webview {
+class AwMainDelegate;
 void InitIcuAndResourceBundleBrowserSide();
 }
 
@@ -66,6 +67,7 @@ class BASE_I18N_EXPORT DefaultIcuLocaleSetterKey {
  private:
   friend class ScopedDefaultIcuLocale;
   friend class ::blink::LocaleController;
+  friend class ::android_webview::AwMainDelegate;
   friend void ::android_webview::InitIcuAndResourceBundleBrowserSide();
   friend class ::WebEngineMainDelegate;
   friend class ::WebEngineBrowserMainParts;
