62c429b284549e0f3684f64c4700af9541358f92
Chris Fredrickson <cfredric@chromium.org>
2026-08-25 06:31:45 -0700

    Rewrite RCD internals to be string_view-based
    C++17 brought us std::optional and std::string_view, which we can use to
    replace the length- and std::string::npos- based logic. This makes the
    code a bit more readable, and it should be just as efficient.
    
    This CL is intended to be a pure refactoring. I've introduced some
    stricter error checking in 2 places and included comments explaining why
    they're safe.
    
    Bug: 548509154
    Change-Id: Icbe03bdb46a0f3ebfff44e09591898221ab51c67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8285877
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685492}

--- net/base/lookup_string_in_fixed_set.cc
+++ net/base/lookup_string_in_fixed_set.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <iterator>
 #include <optional>
 #include <string_view>
 
@@ -202,20 +203,18 @@ std::optional<int> LookupStringInFixedSet(base::span<const uint8_t> graph,
   return lookup.GetResultForCurrentSequence();
 }
 
-// This function is only used by GetRegistryLengthInStrippedHost(), but is
+// This function is only used by GetRegistryInTrimmedHost(), but is
 // implemented here to allow inlining of
 // LookupStringInFixedSet::GetResultForCurrentSequence() and
 // LookupStringInFixedSet::Advance() at compile time. Tests on x86_64 linux
 // indicated about 10% increased runtime cost for GetRegistry() in average
 // if the implementation of this function was separated from the lookup methods.
-std::optional<DomainRuleTags> LookupSuffixInReversedSet(
+std::optional<SuffixMatch> LookupSuffixInReversedSet(
     base::span<const uint8_t> graph,
     bool include_private,
-    std::string_view host,
-    size_t* suffix_length) {
+    std::string_view host) {
   FixedSetIncrementalLookup lookup(graph);
-  *suffix_length = 0;
-  std::optional<DomainRuleTags> result;
+  std::optional<SuffixMatch> result;
   std::string_view::const_iterator pos = host.end();
   // Look up host from right to left.
   while (pos != host.begin() && lookup.Advance(*--pos)) {
@@ -229,10 +228,13 @@ std::optional<DomainRuleTags> LookupSuffixInReversedSet(
         if (value.value().Has(DomainRuleTag::kPrivate) && !include_private) {
           break;
         }
-        // Save length and return value. Since hosts are looked up from right to
-        // left, the last saved values will be from the longest match.
-        *suffix_length = host.end() - pos;
-        result = value;
+        // Save matching suffix and return value. Since hosts are looked up
+        // from right to left, the last saved values will be from the longest
+        // match.
+        result = SuffixMatch{
+            .tags = *value,
+            .suffix = host.substr(std::distance(host.begin(), pos)),
+        };
       }
     }
   }
