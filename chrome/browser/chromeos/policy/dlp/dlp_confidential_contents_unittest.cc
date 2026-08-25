617bbf8f9ad2ee584ee3c8795655062f5b57c6ab
Aida Zolic <aidazolic@chromium.org>
2026-08-25 00:16:24 -0700

    Log ConfidentialContentsCacheEvictedOnFull=false
    We track ConfidentialContentsCacheEvictedOnFull to check whether the
    cache size should be updated, and have a Finch subscription to notify
    when the evictions become more frequent.
    
    However, as the metric is very sparse (most of the time the cache size
    isn't exceeded), this causes Finch to report errors due to missing data.
    Instead, as suggested in the documentation, we can monitor the success
    rate instead (in this case, non evicted i.e. false) and alert when the
    true bucket spikes.
    Includes some minor formatting changes in the affected file.
    
    TAG=agy
    
    Bug: 549999839
    Change-Id: Ia52c3db1bec00af26410ceb44f9af76785b5a02e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8285122
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Aida Zolic <aidazolic@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685285}

--- chrome/browser/chromeos/policy/dlp/dlp_confidential_contents_unittest.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_confidential_contents_unittest.cc
@@ -258,10 +258,14 @@ TEST_F(DlpConfidentialContentsTest, CacheEvictsAfterTimeout) {
       data_controls::GetDlpHistogramPrefix() +
           data_controls::dlp::kConfidentialContentsCount,
       1, 1);
-  histogram_tester_.ExpectTotalCount(
+  histogram_tester_.ExpectBucketCount(
       data_controls::GetDlpHistogramPrefix() +
           data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
-      0);
+      false, 1);
+  histogram_tester_.ExpectBucketCount(
+      data_controls::GetDlpHistogramPrefix() +
+          data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
+      true, 0);
   task_runner->FastForwardBy(DlpConfidentialContentsCache::GetCacheTimeout());
   EXPECT_FALSE(cache.Contains(content, kRestriction));
 }
@@ -293,10 +297,14 @@ TEST_F(DlpConfidentialContentsTest, CacheEvictsWhenFull) {
                     data_controls::dlp::kConfidentialContentsCount)
                 ->TotalCount(),
             100);
-  histogram_tester_.ExpectTotalCount(
+  histogram_tester_.ExpectBucketCount(
       data_controls::GetDlpHistogramPrefix() +
           data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
-      0);
+      false, 100);
+  histogram_tester_.ExpectBucketCount(
+      data_controls::GetDlpHistogramPrefix() +
+          data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
+      true, 0);
 
   // Add an additional item which should lead to the first one being evicted.
   DlpConfidentialContent content101 =
@@ -318,6 +326,10 @@ TEST_F(DlpConfidentialContentsTest, CacheEvictsWhenFull) {
   histogram_tester_.ExpectBucketCount(
       data_controls::GetDlpHistogramPrefix() +
           data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
+      false, 100);
+  histogram_tester_.ExpectBucketCount(
+      data_controls::GetDlpHistogramPrefix() +
+          data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
       true, 1);
 }
 
@@ -333,10 +345,14 @@ TEST_F(DlpConfidentialContentsTest, CacheRemovesDuplicates) {
       data_controls::GetDlpHistogramPrefix() +
           data_controls::dlp::kConfidentialContentsCount,
       1, 1);
-  histogram_tester_.ExpectTotalCount(
+  histogram_tester_.ExpectBucketCount(
       data_controls::GetDlpHistogramPrefix() +
           data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
-      0);
+      false, 1);
+  histogram_tester_.ExpectBucketCount(
+      data_controls::GetDlpHistogramPrefix() +
+          data_controls::dlp::kConfidentialContentsCacheEvictedOnFull,
+      true, 0);
 }
 
 }  // namespace policy
