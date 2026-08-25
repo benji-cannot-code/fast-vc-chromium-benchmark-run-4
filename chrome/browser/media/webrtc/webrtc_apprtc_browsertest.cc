5348cf839baee44e78709b01446bf5b636542207
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-08-25 12:09:00 -0700

    Move infobar_responder.h in chrome/browser/infobars/test_support
    Bypass-Check-License: Simple rename
    Bug: None
    Change-Id: Ib962dfcfebace5d9814b566aa4f0c85fae5a7bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8286278
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685820}

--- chrome/browser/media/webrtc/webrtc_apprtc_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_apprtc_browsertest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/infobars/infobar_responder.h"
+#include "chrome/browser/infobars/test_support/infobar_responder.h"
 #include "chrome/browser/media/webrtc/webrtc_browsertest_base.h"
 #include "chrome/browser/media/webrtc/webrtc_browsertest_common.h"
 #include "chrome/browser/ui/browser.h"
