976d20e6349a25286ad5510143e5d91108249bf5
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-08-25 14:41:39 -0700

    fuzztest: Refactor fuzzing tests and add tests for fuzztests
    A few changes in this CL in order to create a suite of fuzzer smoke
    tests:
    
     - Moves fuzzers used by tests to testing/libfuzzer/tests/
     - Introduces (or rewrites) stub and crashing fuzzers for each of the fuzzer variations we care about.
     - Adds parameterized tests for all the fuzzer variations we care about.
    
    Note that we no longer use the string_compare fuzzers in tests. They
    essentially functioned as instrumentation smoke tests since they would
    only crash if the strcmp instrumentation was working. A follow up CL,
    similar to this one, will reorganize those fuzzers to test all the
    fuzzer variations we care about.
    
    Bug: 542680208
    Change-Id: Iad24803b0aedd07eedb65b0c86170e1ada7f6ee6
    Bypass-Check-License: fuzztest_cq_smoke_test.cc and llvm_stub_fuzzer.cc were previously fuzztest_smoketest.cc and empty_fuzzer.cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8231162
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Tigran Bantikyan <bantikyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685981}

--- testing/libfuzzer/tests/fuzztest_proto_stub_fuzzer.cc
+++ testing/libfuzzer/tests/fuzztest_proto_stub_fuzzer.cc
@@ -3,12 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// A minimal stub fuzztest used by
-// FuzzerSmokeTest.WrapperDoesNotLeakChildOnSIGTERM to verify that the fuzztest
-// wrapper does not leak child processes.
-
+#include "testing/libfuzzer/proto/string_compare_fuzzable.pb.h"
 #include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 
-static void Stub(int input) {}
+static void Stub(const fuzzable::string_compare::StringCompare& input) {}
 
-FUZZ_TEST(StubFuzzer, Stub);
+FUZZ_TEST(FuzzTestProtoStub, Stub);
