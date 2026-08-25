b42e3d104dc4c73c6ccda683df1daedc60e688b6
Radu Nitescu <radunitescu@google.com>
2026-08-25 01:38:25 -0700

    [iOS] Dismiss contextual plus menu in the input plate on new search
    When a new search is requested, dismiss the contextual menu
    
    Fixed: 522257345
    Change-Id: I17d060863b9e6ae8b43e54ecc9cf10cded988664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277856
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685377}

--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.h
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.h
@@ -60,6 +60,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// Shows the multimodal menu. Same as pressing the plus button.
 - (void)showMultimodalMenu;
 
+/// Dismisses the context menu.
+- (void)dismissContextMenu;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_COMPOSEBOX_UI_COMPOSEBOX_INPUT_PLATE_VIEW_CONTROLLER_H_
--- ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_input_plate_view_controller.mm
@@ -517,6 +517,10 @@ UIImage* SendButtonImage(BOOL highlighted,
   [_plusButton performPrimaryAction];
 }
 
+- (void)dismissContextMenu {
+  [_plusButton.contextMenuInteraction dismissMenu];
+}
+
 #pragma mark - ComposeboxInputItemCellDelegate
 
 - (void)composeboxInputItemCellDidTapCloseButton:
