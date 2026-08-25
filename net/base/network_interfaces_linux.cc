d8fae4580d914bb890470f4323bba0daab8be459
Jeffrey Yu <yuje@chromium.org>
2026-08-25 00:10:39 -0700

    [net] Remove kAddressTrackerLinuxIsProxied flag
    The net::features::kAddressTrackerLinuxIsProxied feature flag has been
    enabled by default. This CL removes the flag and cleans up the
    associated code, always assuming the feature is enabled.
    
    Consequently, AddressTrackerLinux is always proxied/brokered on Linux,
    and the network service no longer needs to run it directly, which
    improves the Linux network service sandbox.
    
    We also remove the now-obsolete //net dependency in sandbox/policy since
    we no longer need to check the feature flag there.
    
          NetworkServiceConnectionTypeSyncedBrowserTest.*
    
    Bug: b:356624159
    Test: AddressMapLinuxBrowserTest.*,
    Change-Id: I754eb10b3d941f6e28c9f3d122334f0292dd1c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7887860
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685277}

--- net/base/network_interfaces_linux.cc
+++ net/base/network_interfaces_linux.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
-#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_file.h"
 #include "base/strings/escape.h"
@@ -32,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "net/base/address_map_linux.h"
 #include "net/base/address_tracker_linux.h"
-#include "net/base/features.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_platform_api_util.h"
@@ -255,9 +253,7 @@ bool GetNetworkList(NetworkInterfaceList* networks, int policy) {
 #if BUILDFLAG(IS_LINUX)
   // If NetworkChangeNotifier already maintains a map owner in this process, use
   // it.
-  if (base::FeatureList::IsEnabled(features::kAddressTrackerLinuxIsProxied)) {
-    map_owner = NetworkChangeNotifier::GetAddressMapOwner();
-  }
+  map_owner = NetworkChangeNotifier::GetAddressMapOwner();
 #endif  // BUILDFLAG(IS_LINUX)
   if (!map_owner) {
     // If there is no existing map_owner, create an AddressTrackerLinux and
@@ -266,6 +262,7 @@ bool GetNetworkList(NetworkInterfaceList* networks, int policy) {
     temp_tracker->Init();
     map_owner = &temp_tracker.value();
   }
+  CHECK(map_owner);
 
   return internal::GetNetworkListImpl(
       networks, policy, map_owner->GetOnlineLinks(), map_owner->GetAddressMap(),
