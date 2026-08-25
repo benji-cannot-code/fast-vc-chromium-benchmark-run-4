e41ac4ecba2071172bea8e4beb066edd35f6b727
James Leung <jamesleung@google.com>
2026-08-25 10:10:35 -0700

    [contextual_tasks] Add Google3-added tool/model metrics
    * Add metric to track when Google3 added a tool or a model
    * Add tests
    
    Parent: crrev.com/c/8032361, crrev.com/c/8246238
    Bug: b:537564272
    Change-Id: I8d8dad852b75f8d31e4b7050be6add605a0d42a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8193079
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: James Leung <jamesleung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685686}

--- chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.cc
+++ chrome/browser/ui/webui/cr_components/searchbox/contextual_searchbox_handler.cc
@@ -1447,7 +1447,12 @@ void ContextualSearchboxHandler::UploadSnapshotTabContextIfPresent() {
 }
 
 void ContextualSearchboxHandler::SetActiveToolMode(omnibox::ToolMode tool,
-                                                   bool is_set_by_server) {
+                                                   bool is_set_by_aim) {
+  if (auto* metrics_recorder = GetMetricsRecorder()) {
+    if (is_set_by_aim) {
+      metrics_recorder->RecordToolChangedByAIM(tool);
+    }
+  }
   if (!input_state_model_) {
     return;
   }
@@ -1470,6 +1475,11 @@ void ContextualSearchboxHandler::RecordModelSelectionAction(
 
 void ContextualSearchboxHandler::SetActiveModelMode(omnibox::ModelMode model,
                                                     bool is_set_by_aim) {
+  if (auto* metrics_recorder = GetMetricsRecorder()) {
+    if (is_set_by_aim) {
+      metrics_recorder->RecordModelChangedByAIM(model);
+    }
+  }
   if (!input_state_model_) {
     return;
   }
