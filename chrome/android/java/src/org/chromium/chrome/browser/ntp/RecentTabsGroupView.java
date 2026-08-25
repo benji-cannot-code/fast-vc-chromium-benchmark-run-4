42387739d981ce50f9e53634edc6738a3c9d27a1
Calder Kitagawa <ckitagawa@chromium.org>
2026-08-25 12:04:32 -0700

    [RecentTabs] Fix talkback announcement for expand/collapse
    Fixed: 41459960
    Change-Id: I86ab999c829ec54781f6aefa7f53bbd85a67116b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8286444
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685813}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsGroupView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsGroupView.java
@@ -5,16 +5,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ntp;
 
+import static android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction.ACTION_COLLAPSE;
+import static android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction.ACTION_EXPAND;
+import static android.view.accessibility.AccessibilityNodeInfo.EXPANDED_STATE_COLLAPSED;
+import static android.view.accessibility.AccessibilityNodeInfo.EXPANDED_STATE_FULL;
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.LevelListDrawable;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.recent_tabs.ForeignSessionHelper.ForeignSession;
 import org.chromium.components.browser_ui.widget.TintedDrawable;
@@ -32,6 +42,7 @@ public class RecentTabsGroupView extends RelativeLayout {
 
     private static final int DRAWABLE_LEVEL_EXPANDED = 1;
 
+    private boolean mIsExpanded;
     private RecentTabsGroupView mRow;
     private ImageView mExpandCollapseIcon;
     private TextView mDeviceLabel;
@@ -43,7 +54,7 @@ public class RecentTabsGroupView extends RelativeLayout {
      * @param context The context this view will work in.
      * @param attrs The attribute set for this view.
      */
-    public RecentTabsGroupView(Context context, AttributeSet attrs) {
+    public RecentTabsGroupView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -84,7 +95,7 @@ public class RecentTabsGroupView extends RelativeLayout {
         mDeviceLabel.setText(session.name);
         mTimeLabel.setVisibility(View.VISIBLE);
         mTimeLabel.setText(getTimeString(session));
-        setGroupViewHeight(true);
+        setGroupViewHeight(/* isTimeLabelVisible= */ true);
         configureExpandedCollapsed(isExpanded);
     }
 
@@ -96,7 +107,7 @@ public class RecentTabsGroupView extends RelativeLayout {
     public void configureForRecentlyClosedTabs(boolean isExpanded) {
         mDeviceLabel.setText(R.string.recently_closed);
         mTimeLabel.setVisibility(View.GONE);
-        setGroupViewHeight(false);
+        setGroupViewHeight(/* isTimeLabelVisible= */ false);
         configureExpandedCollapsed(isExpanded);
     }
 
@@ -108,11 +119,48 @@ public class RecentTabsGroupView extends RelativeLayout {
     public void configureForPromo(boolean isExpanded) {
         mDeviceLabel.setText(R.string.ntp_recent_tabs_sync_promo_title);
         mTimeLabel.setVisibility(View.GONE);
-        setGroupViewHeight(false);
+        setGroupViewHeight(/* isTimeLabelVisible= */ false);
         configureExpandedCollapsed(isExpanded);
     }
 
+    @Override
+    public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
+        super.onInitializeAccessibilityNodeInfo(info);
+        info.addAction(mIsExpanded ? ACTION_COLLAPSE : ACTION_EXPAND);
+        if (Build.VERSION.SDK_INT >= 36) {
+            info.setExpandedState(mIsExpanded ? EXPANDED_STATE_FULL : EXPANDED_STATE_COLLAPSED);
+        }
+    }
+
+    @Override
+    public boolean performAccessibilityAction(int action, @Nullable Bundle arguments) {
+        if (action == ACTION_EXPAND.getId() || action == ACTION_COLLAPSE.getId()) {
+            if (getParent() instanceof ExpandableListView parent) {
+                int position = parent.getPositionForView(this);
+                if (position != ExpandableListView.INVALID_POSITION) {
+                    long packedPos = parent.getExpandableListPosition(position);
+                    if (packedPos != ExpandableListView.PACKED_POSITION_VALUE_NULL
+                            && ExpandableListView.getPackedPositionType(packedPos)
+                                    == ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
+                        int groupPos = ExpandableListView.getPackedPositionGroup(packedPos);
+                        if (groupPos >= 0) {
+                            if (action == ACTION_EXPAND.getId()) {
+                                parent.expandGroup(groupPos);
+                            } else {
+                                parent.collapseGroup(groupPos);
+                            }
+                            return true;
+                        }
+                    }
+                }
+            }
+            return performClick();
+        }
+        return super.performAccessibilityAction(action, arguments);
+    }
+
     private void configureExpandedCollapsed(boolean isExpanded) {
+        mIsExpanded = isExpanded;
         int level = isExpanded ? DRAWABLE_LEVEL_EXPANDED : DRAWABLE_LEVEL_COLLAPSED;
         mExpandCollapseIcon.getDrawable().setLevel(level);
     }
@@ -128,7 +176,9 @@ public class RecentTabsGroupView extends RelativeLayout {
 
     private CharSequence getTimeString(ForeignSession session) {
         long timeDeltaMs = System.currentTimeMillis() - session.modifiedTime;
-        if (timeDeltaMs < 0) timeDeltaMs = 0;
+        if (timeDeltaMs < 0) {
+            timeDeltaMs = 0;
+        }
 
         int daysElapsed = (int) (timeDeltaMs / (24L * 60L * 60L * 1000L));
         int hoursElapsed = (int) (timeDeltaMs / (60L * 60L * 1000L));
