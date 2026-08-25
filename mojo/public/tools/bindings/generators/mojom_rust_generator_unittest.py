74c958a448e6ac5b275e8d3ea785aa9289b9e54d
Devon Loehr <dloehr@google.com>
2026-08-25 07:47:16 -0700

    [rustmojo] Disambiguate imported crate names in generated code
    It's possible that different GN targets will have the same base
    name, so we need to ensure they have unique names after importing
    them.
    
    We do so by just turning the GN path in to the name. This does
    result in much less pleasant generated code, but since it's
    generated we don't expect people to look at it very often. We
    could be cleverer and only disambiguate where necessary, but
    that makes the generating code significantly more complex. Since
    that code is human-maintained, this is probably the right trade-
    off
    
    Bug: 551910509
    Change-Id: I80df3806c622e4d13f03cf3c67da0463065bc5f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282419
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685537}

--- mojo/public/tools/bindings/generators/mojom_rust_generator_unittest.py
+++ mojo/public/tools/bindings/generators/mojom_rust_generator_unittest.py
@@ -102,7 +102,7 @@ class TestGetQualifiedName(unittest.TestCase):
     name = mojom_rust_generator._GetQualifiedName(
       ty, self.mod_a, self.source_to_target_map
     )
-    self.assertEqual(name, 'b::b::MyStruct')
+    self.assertEqual(name, 'b_b::b::MyStruct')
 
   def test_nested_different_target(self):
     parent = MockKind('MyStruct', self.mod_b)
@@ -110,7 +110,7 @@ class TestGetQualifiedName(unittest.TestCase):
     name = mojom_rust_generator._GetQualifiedName(
       ty, self.mod_a, self.source_to_target_map
     )
-    self.assertEqual(name, 'b::b::MyStruct_MyEnum')
+    self.assertEqual(name, 'b_b::b::MyStruct_MyEnum')
 
 
 if __name__ == '__main__':
