f4a7bbe451c72ab0bdf08c58ad26b1d173eee844
David Bienvenu <davidbienvenu@chromium.org>
2026-08-25 12:01:47 -0700

    Use ScopedBypassEnablementChecksForTesting in c/b/actor
    Replace calls to SetBypassEnablementChecksForTesting(true) and
    SetBypassEnablementChecksForTesting(false) with
    ScopedBypassEnablementChecksForTesting. This can fix issues
    where tests forget to call SetBypassEnablementChecksForTesting(false)
    which breaks downstream tests because glic stays enabled.
    
    As soon as all calls to SetBypassEnablementChecksForTesting are
    replaced, if possible, I'll make it private so tests can't do this going
    forward.
    
    Bug: 8254302
    Change-Id: I25f5d8e0283020ba30a012db4c9ff76651195584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281764
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685810}

--- chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller_unittest.cc
+++ chrome/browser/actor/ui/task_list_bubble/actor_task_list_bubble_controller_unittest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/browser_ui/glic_split_button_controller.h"
 #include "chrome/browser/glic/browser_ui/glic_split_button_delegate.h"
 #include "chrome/browser/glic/glic_profile_manager.h"
+#include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/glic/test_support/glic_test_environment.h"
 #include "chrome/browser/glic/test_support/mock_glic_keyed_service.h"
@@ -154,8 +155,6 @@ class ActorTaskListBubbleControllerTest : public ChromeViewsTestBase {
         TestingBrowserProcess::GetGlobal());
     ASSERT_TRUE(testing_profile_manager_->SetUp());
 
-    glic::GlicEnabling::SetBypassEnablementChecksForTesting(true);
-
     profile_ = testing_profile_manager_->CreateTestingProfile(
         "profile",
         TestingProfile::TestingFactories{
@@ -237,7 +236,6 @@ class ActorTaskListBubbleControllerTest : public ChromeViewsTestBase {
     mock_glic_keyed_service_ = nullptr;
     profile_ = nullptr;
     testing_profile_manager_.reset();
-    glic::GlicEnabling::SetBypassEnablementChecksForTesting(false);
     anchor_widget_.reset();
     ChromeViewsTestBase::TearDown();
   }
@@ -265,6 +263,8 @@ class ActorTaskListBubbleControllerTest : public ChromeViewsTestBase {
         kActorTaskListBubbleView, context);
   }
 
+  glic::GlicEnabling::ScopedBypassEnablementChecksForTesting
+      scoped_glic_bypass_;
   raw_ptr<TestingProfile> profile_;
   std::unique_ptr<TestingProfileManager> testing_profile_manager_;
   signin::IdentityTestEnvironment identity_test_env_;
