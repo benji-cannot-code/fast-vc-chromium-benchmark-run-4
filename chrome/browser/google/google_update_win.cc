05d835c07a14d11e98dd9417d9e1b6d1923ae728
Robert Liao <robliao@chromium.org>
2026-08-25 13:54:52 -0700

    Convert GoogleUpdateWinTest to Microsoft::WRL::RuntimeClass
    This removes a dependency on ATL's CComObject family of classes.
    
    Bug: 41184135
    Change-Id: I0f920d15d3c875115dbe7d0f13fe5a889fc79821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8289011
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685925}

--- chrome/browser/google/google_update_win.cc
+++ chrome/browser/google/google_update_win.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
 #include "base/version.h"
-#include "base/win/atl.h"
 #include "base/win/scoped_bstr.h"
 #include "base/win/win_util.h"
 #include "chrome/browser/google/google_update_app_command.h"
