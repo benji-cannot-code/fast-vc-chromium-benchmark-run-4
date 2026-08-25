dd8a4c09163107397280e09e2e0a16d811d40d15
Advik Nakirikanti <adviknk@google.com>
2026-08-25 01:09:17 -0700

    [ContextHub] Register Smart Search personal context feature
    This CL introduces the foundational protobuf updates and feature
    registrations required for the Context Hub Smart Search feature to
    communicate with the Context Memory Service (CMS) backend.
    
    Key changes:
    - Personal Context: Added `CONTEXT_MEMORY_FEATURE_SMART_SEARCH` to the
      feature mappings in `context_memory_features.cc`, and updated
      `personal_context_fetcher.cc` and `personal_context_manager.cc` to
      support the new feature enum.
    - Protos: Updated `context_memory_service.proto` and added the
      new dependencies to `BUILD.gn`.
    - Metrics: Registered the new feature in `histograms.xml`.
    
    TAG=agy
    CONV=f47e21fa-270c-49bc-b172-809321d67069
    
    Bug: None
    Co-authored-by: Bill Yuan <billyuan@google.com>
    Change-Id: Ib5aba4b58d60354f6a35e42f225ddb70f56812ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8270713
    Reviewed-by: Isabelle Goralsky <igoralsky@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Advik Nakirikanti <adviknk@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685345}

--- components/personal_context/core/network/personal_context_manager.cc
+++ components/personal_context/core/network/personal_context_manager.cc
@@ -64,6 +64,7 @@ size_t GetMaxParallelFeatureFetchers(proto::ContextMemoryFeature feature) {
       return 2;
     case proto::CONTEXT_MEMORY_FEATURE_AT_MEMORY:
     case proto::CONTEXT_MEMORY_FEATURE_AUTO_TODOS:
+    case proto::CONTEXT_MEMORY_FEATURE_SMART_SEARCH:
       return 1;
     default:
       NOTREACHED();
@@ -79,6 +80,7 @@ size_t GetMaxParallelPiiFeatureFetchers(proto::ContextMemoryFeature feature) {
     case proto::CONTEXT_MEMORY_FEATURE_AMBIENT_AUTOFILL:
     case proto::CONTEXT_MEMORY_FEATURE_AT_MEMORY:
     case proto::CONTEXT_MEMORY_FEATURE_AUTO_TODOS:
+    case proto::CONTEXT_MEMORY_FEATURE_SMART_SEARCH:
       return 1;
     default:
       NOTREACHED();
