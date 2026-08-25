8f50050cfd737c9b98cecef0b47d0b452045d9a8
Sophie Crowley <crowleso@google.com>
2026-08-25 01:24:21 -0700

    [Signin][Al] Add AL Identity signout flags to fieldtrial config
    This initially was causing test failures and had to be reverted
    https://crrev.com/c/8279900. An extra dialog box now opens up on desktop
    (along with a modified sign out text string. Updated the tests to
    accommodate for these changes.
    
    Change-Id: I27f053c96ad42c536918d9aafd892b610479ed97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8276759
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Sophie Crowley <crowleso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685366}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/MainSettingsFragmentTest.java
@@ -12,6 +12,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.scrollTo;
 import static androidx.test.espresso.matcher.PreferenceMatchers.withKey;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -345,7 +346,12 @@ public class MainSettingsFragmentTest {
 
         onView(withText(accountInfo.getEmail())).perform(click());
         onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.scrollToLastPosition());
-        onView(withText(R.string.sign_out)).perform(click());
+        if (DeviceInfo.isDesktop()) {
+            onView(withText(R.string.manage_sync_settings_sign_out_of_chrome)).perform(click());
+            onView(withText(R.string.sign_out)).inRoot(isDialog()).perform(click());
+        } else {
+            onView(withText(R.string.sign_out)).perform(click());
+        }
         Assert.assertNull(mSyncTestRule.getSigninTestRule().getPrimaryAccount());
 
         Activity activity = mSettingsTestRule.getActivity();
@@ -375,7 +381,12 @@ public class MainSettingsFragmentTest {
 
         onView(withText(accountInfo.getEmail())).perform(click());
         onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.scrollToLastPosition());
-        onView(withText(R.string.sign_out)).perform(click());
+        if (DeviceInfo.isDesktop()) {
+            onView(withText(R.string.manage_sync_settings_sign_out_of_chrome)).perform(click());
+            onView(withText(R.string.sign_out)).inRoot(isDialog()).perform(click());
+        } else {
+            onView(withText(R.string.sign_out)).perform(click());
+        }
         Assert.assertNull(mSyncTestRule.getSigninTestRule().getPrimaryAccount());
 
         Activity activity = mSettingsTestRule.getActivity();
