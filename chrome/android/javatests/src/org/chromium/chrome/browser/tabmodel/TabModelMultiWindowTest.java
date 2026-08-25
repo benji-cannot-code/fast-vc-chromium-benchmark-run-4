89253ec90c5323de68c2a347c28b141b33d74cbe
Calder Kitagawa <ckitagawa@chromium.org>
2026-08-25 12:17:44 -0700

    Re-enable a couple tests in TabModelMultiWindowTest
    Fixed: 481443908
    Change-Id: Ie73ccdaf940c8a88cde62f57168e9de05d1dddcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8288147
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685831}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelMultiWindowTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabmodel/TabModelMultiWindowTest.java
@@ -36,7 +36,6 @@ import org.chromium.base.Token;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.ImportantFormFactors;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -118,7 +117,6 @@ public class TabModelMultiWindowTest {
     }
 
     @Test
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481443908
     @LargeTest
     public void testMoveTabGroupToWindow() {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
@@ -152,7 +150,6 @@ public class TabModelMultiWindowTest {
     }
 
     @Test
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/481443908
     @LargeTest
     public void testMoveTabGroupToWindowFailure() {
         // Create a tab group.
