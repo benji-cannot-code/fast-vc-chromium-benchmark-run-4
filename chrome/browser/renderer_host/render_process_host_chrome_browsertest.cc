f8bca4ee0e07f23f5063f658c5d3b4752fed093d
Patrick Monette <pmonette@chromium.org>
2026-08-25 07:38:46 -0700

    Enable PMLoadingPageVoter in fieldtrial_testing_config.json
    Bug: 532217630
    Change-Id: Ic88c989ec72e675bc61ac7576e42aec088de2297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8276899
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685530}

--- chrome/browser/renderer_host/render_process_host_chrome_browsertest.cc
+++ chrome/browser/renderer_host/render_process_host_chrome_browsertest.cc
@@ -362,7 +362,10 @@ IN_PROC_BROWSER_TEST_F(ChromeRenderProcessHostTest, DISABLED_ProcessPerTab) {
 class ChromeRenderProcessHostBackgroundingTest
     : public ChromeRenderProcessHostTest {
  public:
-  ChromeRenderProcessHostBackgroundingTest() = default;
+  ChromeRenderProcessHostBackgroundingTest() {
+    feature_list_.InitAndDisableFeature(
+        performance_manager::features::kPMLoadingPageVoter);
+  }
 
   ChromeRenderProcessHostBackgroundingTest(
       const ChromeRenderProcessHostBackgroundingTest&) = delete;
@@ -406,6 +409,8 @@ class ChromeRenderProcessHostBackgroundingTest
       EXPECT_EQ(expected_is_backgrounded, IsProcessBackgrounded(p));
     }
   }
+
+  base::test::ScopedFeatureList feature_list_;
 };
 
 #define EXPECT_PROCESS_IS_BACKGROUNDED(process_or_tab)                       \
