a8f61e8c184ee49ca41a4cbcb59a69c68baec444
Farid <faridzad@google.com>
2026-08-25 12:31:55 -0700

    Bedrock: More Converts in page_load_metrics and ai_overlay_dialog
    Migrate legacy Browser references to BrowserWindowInterface in
    page_load_metrics and remove unused browser.h includes in
    ai_overlay_dialog:
    - Update PageLoadMetricsBrowserTest.NewWindowClassification in
      page_load_metrics_browsertest.cc to use BrowserWindowInterface
      and GetTabStripModel() directly instead of downcasting to Browser*.
    - Remove unused chrome/browser/ui/browser.h include in tools.cc.
    
    TAG=agy
    CONV=7109b972-4cb1-4544-b7d4-451114638ca3
    
    Bug: 496674143
    Change-Id: Id015f9ac013ebb1eae7c433589e5fdc1530bc453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282028
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685852}

--- chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.cc
+++ chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_render_frame.mojom.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #if !BUILDFLAG(IS_ANDROID)
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
