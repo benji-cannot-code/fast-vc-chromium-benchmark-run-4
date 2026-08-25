c4b1cf817b6b7133ee38b4dcfccc4812591080c2
Oleh Desiatyrikov <desiatyrikov@google.com>
2026-08-25 06:41:01 -0700

    [ImmersivePlayer] Use seamless sphere mesh for 360° immersive video playback
    Maps ImmersiveProjectionType.SPHERE to
    XrSurfaceEntityShape.SEAMLESS_SPHERE in ImmersiveVideoPlaybackTypeUtils
    and updates coordinator test.
    
    Bug: 542316337
    Change-Id: I891ab42de87ca9910c223a4ef567dca324567210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269390
    Reviewed-by: Gurmeet Kalra <gurmeetk@google.com>
    Reviewed-by: Nick Mondello <mondello@google.com>
    Commit-Queue: Oleh Desiatyrikov (xWF) <desiatyrikov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685500}

--- chrome/android/modules/xr/internal/junit/src/org/chromium/chrome/browser/media/immersive_playback/ImmersiveVideoPlaybackCoordinatorTest.java
+++ chrome/android/modules/xr/internal/junit/src/org/chromium/chrome/browser/media/immersive_playback/ImmersiveVideoPlaybackCoordinatorTest.java
@@ -238,7 +238,7 @@ public class ImmersiveVideoPlaybackCoordinatorTest {
                 ImmersiveStereoMode.TOP_BOTTOM, ImmersiveProjectionType.SPHERE);
         ShadowLooper.idleMainLooper();
 
-        verify(mSurfaceEntityHolder).setSurfaceShape(XrSurfaceEntityShape.SPHERE);
+        verify(mSurfaceEntityHolder).setSurfaceShape(XrSurfaceEntityShape.SEAMLESS_SPHERE);
         verify(mSurfaceEntityHolder).setSurfaceStereoMode(XrSurfaceEntityStereoMode.TOP_BOTTOM);
 
         verify(mSurfaceMovableComponent).setMovable(false, false);
@@ -622,11 +622,11 @@ public class ImmersiveVideoPlaybackCoordinatorTest {
         ShadowLooper.idleMainLooper();
 
         ArgumentCaptor<XrPose> playerPoseCaptor = ArgumentCaptor.forClass(XrPose.class);
-        verify(mSurfaceEntityHolder).setEntityPose(playerPoseCaptor.capture(), eq(XrSpace.ACTIVITY));
+        verify(mSurfaceEntityHolder)
+                .setEntityPose(playerPoseCaptor.capture(), eq(XrSpace.ACTIVITY));
         XrPose expectedPlayerPose =
                 XrPose.create(
-                        ANCHOR_POSE.transformPoint(
-                                XrVector3.create(0f, 0f, -1.5f)),
+                        ANCHOR_POSE.transformPoint(XrVector3.create(0f, 0f, -1.5f)),
                         ANCHOR_POSE.getRotation());
         assertPoseEquals(expectedPlayerPose, playerPoseCaptor.getValue());
     }
@@ -662,7 +662,8 @@ public class ImmersiveVideoPlaybackCoordinatorTest {
 
         verify(mXrSceneCoreSessionManager).getHeadPoseInActivitySpace();
         ArgumentCaptor<XrPose> playerPoseCaptor = ArgumentCaptor.forClass(XrPose.class);
-        verify(mSurfaceEntityHolder).setEntityPose(playerPoseCaptor.capture(), eq(XrSpace.ACTIVITY));
+        verify(mSurfaceEntityHolder)
+                .setEntityPose(playerPoseCaptor.capture(), eq(XrSpace.ACTIVITY));
         assertEquals(ANCHOR_POSE.getTranslation(), playerPoseCaptor.getValue().getTranslation());
     }
 
