b5b910a5e5cd961674c106cd0a01823c295563bb
Louis Romero <lpromero@google.com>
2026-08-25 08:36:14 -0700

    Add PrivacyPrimitive provider API declaration and public stubs
    Introduces the ios::provider::privacy_primitive API declaration,
    public chromium stub, and test provider stub.
    
    This CL is part of a multi-CL change:
     => https://crrev.com/c/8280469
      - https://crrev.com/i/9706357
      - https://crrev.com/c/8276971
    
    Bug: 542114987
    Change-Id: I1c333f0259483a15f03f207fbf7aa0c5bdf9cfa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280469
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685590}

---
+++ ios/chrome/browser/providers/privacy_primitive/chromium_privacy_primitive.mm
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/privacy_primitive/privacy_primitive_api.h"
+
+namespace ios::provider {
+
+id<PrivacyPrimitiveService> CreatePrivacyPrimitiveService(
+    PrivacyPrimitiveConfiguration* configuration) {
+  return nil;
+}
+
+}  // namespace ios::provider
---
+++ ios/public/provider/chrome/browser/privacy_primitive/privacy_primitive_api.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PRIVACY_PRIMITIVE_PRIVACY_PRIMITIVE_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PRIVACY_PRIMITIVE_PRIVACY_PRIMITIVE_API_H_
+
+#import <UIKit/UIKit.h>
+
+@class PrivacyPrimitiveConfiguration;
+
+/// Protocol representing a server-driven Privacy Primitive / ConsentKit
+/// service.
+@protocol PrivacyPrimitiveService <NSObject>
+
+/// Displays the server-driven consent screen.
+/// `completionHandler` is called with `YES` if the flow completed successfully,
+/// or `NO` if cancelled or failed.
+- (void)showFlowWithPresentingViewController:(UIViewController*)viewController
+                           completionHandler:
+                               (void (^)(BOOL success))completionHandler;
+
+@end
+
+namespace ios::provider {
+
+/// Creates a new instance of `PrivacyPrimitiveService`.
+/// Returns `nil` if privacy primitives are unsupported or configuration is
+/// invalid.
+id<PrivacyPrimitiveService> CreatePrivacyPrimitiveService(
+    PrivacyPrimitiveConfiguration* configuration);
+
+}  // namespace ios::provider
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_PRIVACY_PRIMITIVE_PRIVACY_PRIMITIVE_API_H_
