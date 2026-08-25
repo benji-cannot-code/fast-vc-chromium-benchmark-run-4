94961e8886b3e7fe4e3b7db4063d2c0f1495db69
rbpotter <rbpotter@chromium.org>
2026-08-24 21:16:10 -0700

    cr-infinite-list: Fix sign error in dereference fix
    This should have been < focusedIndex - 1, not focusedIndex + 1.
    
    Bug: 543414479
    Change-Id: I7f7da713d4d0c92903fcbdcbabd67aaa5428d90e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8285858
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685266}

--- ui/webui/resources/cr_elements/cr_infinite_list/cr_infinite_list.ts
+++ ui/webui/resources/cr_elements/cr_infinite_list/cr_infinite_list.ts
@@ -117,7 +117,7 @@ export class CrInfiniteListElement<T> extends CrLitElement {
     assert(list);
 
     const renderedItems = list.domItems();
-    if (renderedItems.length < this.focusedIndex - 1) {
+    if (renderedItems.length <= this.focusedIndex) {
       return false;
     }
 
