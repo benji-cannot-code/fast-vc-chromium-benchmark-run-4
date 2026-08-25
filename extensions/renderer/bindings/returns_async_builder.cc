098860a44c8d5d88a03fcf1f93636e2a7257c015
Tim <tjudkins@chromium.org>
2026-08-25 13:50:07 -0700

    [Extensions] Update ReturnsAsyncBuilder to be promise based by default
    The ReturnsAsyncBuilder was first created when callbacks were the norm
    for asynchronous returns, but now that promises are the standard it can
    be changed to treat that as the default and make non-promise based the
    exception.
    
    This is only used in tests and no real behavior change is expected.
    
    Fixed: 512149009
    Change-Id: I7c60719f2a3ec6120f1cf56248fb81c28c005b3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257879
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685918}

--- extensions/renderer/bindings/returns_async_builder.cc
+++ extensions/renderer/bindings/returns_async_builder.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/renderer/bindings/returns_async_builder.h"
 
+#include "base/check.h"
+
 namespace extensions {
 
 ReturnsAsyncBuilder::ReturnsAsyncBuilder() = default;
@@ -13,31 +15,27 @@ ReturnsAsyncBuilder::ReturnsAsyncBuilder(
     std::vector<std::unique_ptr<ArgumentSpec>> signature)
     : signature_(std::move(signature)) {}
 
-ReturnsAsyncBuilder& ReturnsAsyncBuilder::MakeOptional() {
-  optional_ = true;
+ReturnsAsyncBuilder& ReturnsAsyncBuilder::DoesNotSupportPromises() {
+  promise_support_ = binding::APIPromiseSupport::kUnsupported;
   return *this;
 }
 
-ReturnsAsyncBuilder& ReturnsAsyncBuilder::AddPromiseSupport() {
-  promise_support_ = binding::APIPromiseSupport::kSupported;
+ReturnsAsyncBuilder& ReturnsAsyncBuilder::MakeRequired() {
+  optional_ = false;
   return *this;
 }
 
 std::unique_ptr<APISignature::ReturnsAsync> ReturnsAsyncBuilder::Build() {
+  // A signature cannot support promises and also require the callback.
+  CHECK(promise_support_ == binding::APIPromiseSupport::kUnsupported ||
+        optional_);
+
   std::unique_ptr<APISignature::ReturnsAsync> returns_async =
       std::make_unique<APISignature::ReturnsAsync>();
   if (signature_) {
     returns_async->signature = std::move(signature_);
   }
-  // Note: this mirrors the logic we do in BuildReturnsAsyncFromValues in
-  // extensions/renderer/bindings/api_signature.cc, where all promise supporting
-  // APIs mark the returns_async field as optional (i.e. the callback argument
-  // on the signature can be left off).
-  if (promise_support_ == binding::APIPromiseSupport::kSupported) {
-    returns_async->optional = true;
-  } else {
-    returns_async->optional = optional_;
-  }
+  returns_async->optional = optional_;
   returns_async->promise_support = promise_support_;
 
   return returns_async;
