89da19a58bad863151429816602ffaf7c0e994c7
Alex Kallam <aakallam@chromium.org>
2026-08-24 17:53:03 -0700

    Roll skrifa: 0.46.0 => 0.46.2 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * font-types: 0.12.3 => 0.12.4; https://docs.rs/crate/font-types/0.12.4
    * read-fonts: 0.43.0 => 0.43.2; https://docs.rs/crate/read-fonts/0.43.2
    * skrifa: 0.46.0 => 0.46.2; https://docs.rs/crate/skrifa/0.46.2
    
    Bug: None
    Change-Id: Idffb0d9d5ef565cfcaabfa9177f771bbb8b58a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280052
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685121}

--- third_party/rust/chromium_crates_io/vendor/font-types-v0_12/src/uint24.rs
+++ third_party/rust/chromium_crates_io/vendor/font-types-v0_12/src/uint24.rs
@@ -2,7 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// 24-bit unsigned integer.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[cfg_attr(feature = "bytemuck", derive(bytemuck::AnyBitPattern))]
+#[cfg_attr(
+    feature = "bytemuck",
+    derive(bytemuck::AnyBitPattern, bytemuck::NoUninit)
+)]
 #[repr(transparent)]
 pub struct Uint24(u32);
 
