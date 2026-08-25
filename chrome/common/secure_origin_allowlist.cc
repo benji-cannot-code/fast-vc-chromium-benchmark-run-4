4bebdd9d78954abc75923f1f05b38c7b9a352b4c
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-08-25 10:49:23 -0700

    [Extensions] Add extensions to secure origin allowlist
    Add kExtensionScheme to the list of schemes that bypass the secure
    origin check on Android, similar to the behavior on other desktop
    platforms. This allows embedded extension iframes to be considered
    secure contexts and use certain otherwise-blocked extension APIs.
    
    Fixed: 552025945
    Bug: 546631276
    Change-Id: I6e0ec521c9d4512980be36619a1f8283b530ede1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280408
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685737}

--- chrome/common/secure_origin_allowlist.cc
+++ chrome/common/secure_origin_allowlist.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "extensions/buildflags/buildflags.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "extensions/common/constants.h"
 #endif
 
@@ -20,7 +20,7 @@ namespace secure_origin_allowlist {
 
 std::set<std::string> GetSchemesBypassingSecureContextCheck() {
   std::set<std::string> schemes;
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
   schemes.insert(extensions::kExtensionScheme);
 #endif
   return schemes;
