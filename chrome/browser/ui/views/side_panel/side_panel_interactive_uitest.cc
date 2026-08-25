785c913a8b9e5c606bde66cbeebc1e4fb89b62a5
Lauren Winston <lwinston@google.com>
2026-08-25 10:56:07 -0700

    Remove ImmersiveReadAnything flag from side panel test.
    This flag is enabled by default and about to be deleted.
    
    Bug: 537491063
    Change-Id: I3486387db49152986529e777301f318812276864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277925
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685746}

--- chrome/browser/ui/views/side_panel/side_panel_interactive_uitest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_interactive_uitest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/views/side_panel/side_panel.h"
+
 #include <memory>
 
 #include "base/test/bind.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/interaction/browser_elements.h"
+#include "chrome/browser/ui/read_anything/read_anything_prefs.h"
 #include "chrome/browser/ui/side_panel/side_panel_action_callback.h"
 #include "chrome/browser/ui/side_panel/side_panel_entry.h"
 #include "chrome/browser/ui/side_panel/side_panel_entry_id.h"
@@ -30,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/side_panel/side_panel.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_coordinator.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_resize_area.h"
 #include "chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.h"
@@ -38,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/common/read_anything/read_anything.mojom.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/interaction/interaction_test_util_browser.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
@@ -45,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/interaction/webcontents_interaction_test_util.h"
 #include "chrome/test/user_education/interactive_feature_promo_test.h"
 #include "components/feature_engagement/public/feature_constants.h"
+#include "components/prefs/pref_service.h"
 #include "components/reading_list/core/reading_list_entry.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -276,8 +280,6 @@ class PinnedSidePanelInteractiveTest : public InteractiveFeaturePromoTest {
   PinnedSidePanelInteractiveTest()
       : InteractiveFeaturePromoTest(UseDefaultTrackerAllowingPromos(
             {feature_engagement::kIPHSidePanelGenericPinnableFeature})) {
-    scoped_feature_list_.InitWithFeatures({},
-                                          {features::kImmersiveReadAnything});
   }
   ~PinnedSidePanelInteractiveTest() override = default;
 
@@ -342,6 +344,14 @@ class PinnedSidePanelInteractiveTest : public InteractiveFeaturePromoTest {
   auto OpenReadingModeSidePanel() {
     return Steps(
         Do(([&]() {
+          // Reading mode opens in "Immersive" / full-screen view by default.
+          // In order to test that the side panel view of reading mode in side
+          // panel tests, set its presentation state to side panel.
+          browser()->GetProfile()->GetPrefs()->SetInteger(
+              prefs::kAccessibilityReadAnythingLastOpenedPresentationState,
+              static_cast<int>(
+                  read_anything::mojom::ReadAnythingPresentationState::
+                      kInSidePanel));
           chrome::ExecuteCommandWithContext(
               browser(), IDC_SHOW_READING_MODE_SIDE_PANEL,
               actions::ActionInvocationContext::Builder()
@@ -385,9 +395,6 @@ class PinnedSidePanelInteractiveTest : public InteractiveFeaturePromoTest {
   auto ShowSidePanelForKey(SidePanelEntryKey key) {
     return Do(([&]() { browser()->GetFeatures().side_panel_ui()->Show(key); }));
   }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Verify that we can open the ReadingMode side panel from the 3dot -> More
