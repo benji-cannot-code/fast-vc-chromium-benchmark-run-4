14f8cbf65488c69e1c2f7e960265a7290c3be66e
Luis Antunes <luisantunes@google.com>
2026-08-25 09:16:58 -0700

    [ios][autofill] Return immutable NSArray in AutofillLegalMessageLine
    Update +[AutofillLegalMessageLine convertFrom:] to return an immutable
    NSArray<AutofillLegalMessageLine*>* instead of NSMutableArray*.
    
    Also update save card modal consumer, mediator, and test properties to
    use immutable NSArray container properties per Objective-C property
    style guidelines.
    
    Bug: 413056780
    Bypass-Check-License: Modified existing files
    Change-Id: I3799a106376b4e4e99b84c37cb5b912fe8daff67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287805
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Luis Antunes <luisantunes@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685634}

--- ios/chrome/browser/autofill/model/message/autofill_legal_message_line.mm
+++ ios/chrome/browser/autofill/model/message/autofill_legal_message_line.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation AutofillLegalMessageLine
 
-+ (NSMutableArray<AutofillLegalMessageLine*>*)convertFrom:
++ (NSArray<AutofillLegalMessageLine*>*)convertFrom:
     (const autofill::LegalMessageLines&)autofillLegalMessageLines {
   NSMutableArray<AutofillLegalMessageLine*>* legalMessages =
       [[NSMutableArray alloc] init];
--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/save_card_bottom_sheet_mediator_unittest.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/save_card_bottom_sheet_mediator_unittest.mm
@@ -246,7 +246,7 @@ TEST_F(SaveCardBottomSheetMediatorTest, SetConsumer) {
       [[FakeSaveCardBottomSheetConsumer alloc] init];
   mediator_.consumer = consumer;
   TestCommonAttributesOfConsumer(consumer);
-  NSMutableArray<AutofillLegalMessageLine*>* messages =
+  NSArray<AutofillLegalMessageLine*>* messages =
       [AutofillLegalMessageLine convertFrom:model_->legal_messages()];
   for (NSUInteger index = 0; index < [messages count]; index++) {
     EXPECT_NSEQ(messages[index].messageText,
@@ -1031,7 +1031,7 @@ TEST_F(SaveCardBottomSheetMediatorTestForSaveScanAndFill, SetConsumer) {
   // save_card_delegate()->is_for_upload() is false), the model's
   // is_for_upload() is true because ui_info.is_for_upload is true, so legal
   // messages must be populated.
-  NSMutableArray<AutofillLegalMessageLine*>* messages =
+  NSArray<AutofillLegalMessageLine*>* messages =
       [AutofillLegalMessageLine convertFrom:model_->legal_messages()];
   ASSERT_GT([messages count], 0u);
   for (NSUInteger index = 0; index < [messages count]; index++) {
