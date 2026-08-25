a9c6761cf61ee7c85a03d6fd237849ca6a2dfe11
Yuwei Huang <yuweih@chromium.org>
2026-08-25 14:31:54 -0700

    [remoting] Enforces some session policies in ClientSession
    1. Fixes problem of session policies not being applied to
       DesktopEnvironmentOptions when Peer Connection process is enabled by
       factoring out and calling
       DesktopEnvironmentOptions::ApplySessionPolicies in ClientSession.
    2. Moves max session duration logic to ClientSession, which lives in
       the trusted network process. This ensures a compromised PC process
       cannot tamper with the policy.
    
    Bug: 502281489
    Change-Id: I01c97cb652bad5c8e8e2c165e49f436ae635b5e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287284
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685969}

--- remoting/host/client_session.h
+++ remoting/host/client_session.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/timer/timer.h"
 #include "remoting/base/errors.h"
 #include "remoting/base/local_session_policies_provider.h"
 #include "remoting/base/session_policies.h"
@@ -166,6 +167,8 @@ class ClientSession : public protocol::Session::EventHandler,
 
   std::vector<mojo::PendingReceiver<mojom::ChromotingSessionServices>>
       pending_session_services_receivers_;
+
+  base::OneShotTimer max_duration_timer_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
