b8672a5dbe53236a232b05f5462012b16ca3ef56
Louis Romero <lpromero@google.com>
2026-08-25 01:42:03 -0700

    Reset Drive consent state on primary account change in Composebox
    Because kDriveConsentState is currently registered as a Profile-level
    preference in ProfileIOS, switching primary accounts or signing out can
    leave the preference set to the previous account's consent state. This
    change observes signin::IdentityManager events in
    ComposeboxInputStateManager and resets kDriveConsentState to kNotReady
    immediately when the primary account is changed or cleared, preventing
    cross-account consent leakage while asynchronous FACS status queries are
    in flight.
    
    Bug: 551907302
    Change-Id: I3543670c87895e31bfa46df5707bc7d34f4a5135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8278572
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685381}

--- ios/chrome/browser/composebox/coordinator/composebox_input_state_manager.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_state_manager.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/search_engines/template_url_service.h"
 #import "components/search_engines/util.h"
 #import "components/signin/public/identity_manager/identity_manager.h"
+#import "components/signin/public/identity_manager/objc/identity_manager_observer_bridge.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_constants.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_mode_holder.h"
@@ -206,6 +207,7 @@ contextual_search::DriveConsentState ConsentStateFromDisclaimerStatus(
 }  // namespace
 
 @interface ComposeboxInputStateManager () <ComposeboxModeObserver,
+                                           IdentityManagerObserving,
                                            SearchEngineObserving>
 @end
 
@@ -226,6 +228,9 @@ contextual_search::DriveConsentState ConsentStateFromDisclaimerStatus(
   raw_ptr<AimEligibilityService> _aimEligibilityService;
   // Identity manager for checking account status.
   raw_ptr<signin::IdentityManager> _identityManager;
+  // Bridge to observe `IdentityManager` events.
+  std::unique_ptr<signin::IdentityManagerObserverBridge>
+      _identityManagerObserverBridge;
   // Template URL service for checking default search engine.
   raw_ptr<TemplateURLService> _templateURLService;
   // Whether the current session is incognito.
@@ -306,6 +311,11 @@ contextual_search::DriveConsentState ConsentStateFromDisclaimerStatus(
     _prefService = prefService;
     _aimEligibilityService = aimEligibilityService;
     _identityManager = identityManager;
+    if (_identityManager) {
+      _identityManagerObserverBridge =
+          std::make_unique<signin::IdentityManagerObserverBridge>(
+              _identityManager, self);
+    }
     _templateURLService = templateURLService;
     _urlLoaderFactory = urlLoaderFactory;
     // sessionHandle can be nil only in tests.
@@ -343,6 +353,7 @@ contextual_search::DriveConsentState ConsentStateFromDisclaimerStatus(
   _webStateList = nullptr;
   _prefService = nullptr;
   _aimEligibilityService = nullptr;
+  _identityManagerObserverBridge.reset();
   _identityManager = nullptr;
   _templateURLService = nullptr;
   _sessionHandle.reset();
@@ -706,6 +717,29 @@ contextual_search::DriveConsentState ConsentStateFromDisclaimerStatus(
 - (void)templateURLServiceShuttingDown:(TemplateURLService*)urlService {
   _searchEngineObserver.reset();
   _templateURLService = nullptr;
+}
+
+#pragma mark - IdentityManagerObserving
+
+- (void)primaryAccountDidChange:
+    (const signin::PrimaryAccountChangeEvent&)event {
+  switch (event.GetEventTypeFor(signin::ConsentLevel::kSignin)) {
+    case signin::PrimaryAccountChangeEvent::Type::kCleared:
+    case signin::PrimaryAccountChangeEvent::Type::kSet: {
+      // Immediately reset cached Drive consent state when switching accounts or
+      // signing out to prevent leaking the previous account's consent state
+      // while asynchronous FACS status queries are in flight.
+      if (_prefService) {
+        _prefService->SetInteger(
+            contextual_search::kDriveConsentState,
+            static_cast<int>(contextual_search::DriveConsentState::kNotReady));
+      }
+      [self updateSearchboxConfig];
+      break;
+    }
+    case signin::PrimaryAccountChangeEvent::Type::kNone:
+      break;
+  }
 }
 
 #pragma mark - ComposeboxModeObserver
