577503a91c170a948b990640f62ffdb3e728df62
Dominique Fauteux-Chapleau <domfc@chromium.org>
2026-08-25 10:13:08 -0700

    Support data region preference lookup in ConnectorsManager on Android
    Previously, `ConnectorsManager::GetDataRegion()` short-circuited to
    return `DataRegion::NO_PREFERENCE` on Android behind a
    `BUILDFLAG(IS_ANDROID)` guard.
    
    Since the `ChromeDataRegionSetting` policy and its corresponding
    preference (`prefs::kChromeDataRegionSetting`) are registered and
    supported on Android, this CL removes the platform guard in
    `ConnectorsManager::GetDataRegion()` so that Android builds respect user
    and machine data region policy settings when resolving regionalized
    analysis service endpoints.
    
    Change-Id: I222c35b9afa4a82c8a0a4b78e6c4d3258255830e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281253
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1685689}

--- chrome/browser/enterprise/connectors/connectors_manager.cc
+++ chrome/browser/enterprise/connectors/connectors_manager.cc
@@ -153,9 +153,6 @@ void ConnectorsManager::OnAnalysisPrefChanged(AnalysisConnector connector) {
 }
 
 DataRegion ConnectorsManager::GetDataRegion(AnalysisConnector connector) const {
-#if BUILDFLAG(IS_ANDROID)
-  return DataRegion::NO_PREFERENCE;
-#else
   // Connector's policy scope determines the DRZ policy scope to use.
   policy::PolicyScope scope = static_cast<policy::PolicyScope>(
       prefs()->GetInteger(AnalysisConnectorScopePref(connector)));
@@ -172,7 +169,6 @@ DataRegion ConnectorsManager::GetDataRegion(AnalysisConnector connector) const {
 
   return ChromeDataRegionSettingToEnum(
       pref_service->GetInteger(prefs::kChromeDataRegionSetting));
-#endif
 }
 
 std::unique_ptr<AnalysisServiceSettingsBase>
--- chrome/browser/enterprise/connectors/connectors_manager_unittest.cc
+++ chrome/browser/enterprise/connectors/connectors_manager_unittest.cc
@@ -46,10 +46,16 @@ namespace enterprise_connectors {
 
 namespace {
 
-#if !BUILDFLAG(IS_ANDROID)
 constexpr AnalysisConnector kAllAnalysisConnectors[] = {
-    AnalysisConnector::FILE_DOWNLOADED, AnalysisConnector::FILE_ATTACHED,
-    AnalysisConnector::BULK_DATA_ENTRY, AnalysisConnector::PRINT};
+#if BUILDFLAG(IS_ANDROID)
+    AnalysisConnector::FILE_DOWNLOADED,
+#else
+    AnalysisConnector::FILE_DOWNLOADED,
+    AnalysisConnector::FILE_ATTACHED,
+    AnalysisConnector::BULK_DATA_ENTRY,
+    AnalysisConnector::PRINT,
+#endif
+};
 
 constexpr DataRegion kAllDataRegions[] = {
     DataRegion::NO_PREFERENCE, DataRegion::UNITED_STATES, DataRegion::EUROPE};
@@ -92,7 +98,6 @@ constexpr char kDlpAndMalwareUrl[] = "https://foo.com";
 constexpr char kOnlyDlpUrl[] = "https://no.malware.com";
 constexpr char kOnlyMalwareUrl[] = "https://no.dlp.com";
 constexpr char kNoTagsUrl[] = "https://no.dlp.or.malware.ca";
-#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace
 
@@ -204,7 +209,6 @@ INSTANTIATE_TEST_SUITE_P(
                      testing::Bool()));
 #endif  // BUILDFLAG(ENTERPRISE_LOCAL_CONTENT_ANALYSIS)
 
-#if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
 class ConnectorsManagerConnectorPoliciesTest
     : public ConnectorsManagerTest,
       public testing::WithParamInterface<
@@ -317,7 +321,6 @@ INSTANTIATE_TEST_SUITE_P(
                                      kNoTagsUrl),
                      testing::Values(kNormalCloudAnalysisSettingsPref,
                                      kNormalLocalAnalysisSettingsPref)));
-#endif  // BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
 
 #if BUILDFLAG(IS_CHROMEOS)
 using VolumeInfo = SourceDestinationTestingHelper::VolumeInfo;
@@ -666,7 +669,6 @@ INSTANTIATE_TEST_SUITE_P(
 
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
-#if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
 class ConnectorsManagerAnalysisConnectorsTest
     : public ConnectorsManagerTest,
       public testing::WithParamInterface<
@@ -747,7 +749,6 @@ INSTANTIATE_TEST_SUITE_P(
     testing::Combine(testing::ValuesIn(kAllAnalysisConnectors),
                      testing::Values(kNormalCloudAnalysisSettingsPref,
                                      kNormalLocalAnalysisSettingsPref)));
-#endif  // BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
 
 #if BUILDFLAG(IS_CHROMEOS)
 
@@ -927,7 +928,6 @@ INSTANTIATE_TEST_SUITE_P(ConnectorsManagerLocalAnalysisConnectorTest,
                          testing::ValuesIn(kAllAnalysisConnectors));
 #endif  // BUILDFLAG(ENTERPRISE_LOCAL_CONTENT_ANALYSIS)
 
-#if !BUILDFLAG(IS_ANDROID)
 class ConnectorsManagerDataRegionTest
     : public ConnectorsManagerTest,
       public testing::WithParamInterface<std::tuple<AnalysisConnector,
@@ -994,5 +994,5 @@ INSTANTIATE_TEST_SUITE_P(
         testing::ValuesIn(kAllDataRegions),
         testing::Values(policy::PolicyScope::POLICY_SCOPE_USER,
                         policy::PolicyScope::POLICY_SCOPE_MACHINE)));
-#endif  // !BUILDFLAG(IS_ANDROID)
+
 }  // namespace enterprise_connectors
