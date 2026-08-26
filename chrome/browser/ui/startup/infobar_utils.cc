560f14eafd2585a3fe83e40c682aacdb587f62e9
Amy Hwang <amyhwang@chromium.org>
2026-08-26 11:40:59 -0700

    [Infobar] Migrate automation infobar to centralized framework
    This CL migrates the AUTOMATION_INFOBAR_DELEGATE (shown when Chrome is
    controlled by automated test software) to the centralized infobar
    framework.
    
    Key Changes:
    - Registered `AUTOMATION_INFOBAR_DELEGATE` in the registry with global
    scope, critical security priority, and non-closeable.
    - Added `kMigratedAutomation` feature parameter under the `kCentralizedInfoBarFramework` feature.
    - Updated emulation_handler.cc to route the DevTools path to BrowserInfoBarManager, using `kTab` override scope.
    - Updated browser_infobar_manager.h/cc to support `override_scope` in `Show()` to allow showing a globally registered infobar as tab-scoped.
    
    This change is controlled by the `MigratedAutomation` parameter of the
    `kCentralizedInfoBarFramework` feature flag. To test it out manually,
    use:
    --enable-features=CentralizedInfoBarFramework:MigratedAutomation/true
    
    Low-Coverage-Reason: TRIVIAL_CHANGE migrating infobars.
    Bug: 530925893
    Change-Id: I57e7f244e289cef6dad36117c48ae31ad98e9652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8207721
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Commit-Queue: Amy Hwang <amyhwang@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686571}

--- chrome/browser/ui/startup/infobar_utils.cc
+++ chrome/browser/ui/startup/infobar_utils.cc
@@ -148,7 +148,14 @@ void AddInfoBarsIfNecessary(BrowserWindowInterface* browser,
 #endif
 
     if (IsAutomationEnabled()) {
-      AutomationInfoBarDelegate::Create();
+      if (infobars::IsInfoBarMigrated(
+              infobars::InfoBarDelegate::AUTOMATION_INFOBAR_DELEGATE)) {
+        infobars::BrowserInfoBarManager::From(g_browser_process)
+            ->ShowGlobally(
+                infobars::InfoBarDelegate::AUTOMATION_INFOBAR_DELEGATE);
+      } else {
+        AutomationInfoBarDelegate::Create();
+      }
     }
   }
 
