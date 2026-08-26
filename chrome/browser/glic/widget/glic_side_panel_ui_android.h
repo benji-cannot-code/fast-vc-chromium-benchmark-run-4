0ce3f8e769f5b1238da522ad3d3d61a1d66e4010
Anthony Cui <cuianthony@google.com>
2026-08-26 11:16:44 -0700

    [GlicZoom] Add zoom action metric split by trigger source
    Support for triggering zoom in glic via different sources (hotkey with
    shift, scroll) has landed. Add a new split metric to capture zoom
    actions split by source. This should also help understand the source of
    continuing user feedback reports requesting the zoom feature, despite it
    being launched.
    
    Bug: b:530309623
    Change-Id: I72c23265ec48f347936779281179119b33657e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277322
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686557}

--- chrome/browser/glic/widget/glic_side_panel_ui_android.h
+++ chrome/browser/glic/widget/glic_side_panel_ui_android.h
@@ -126,7 +126,7 @@ class GlicSidePanelUi
   // LocalHotkeyManager::Panel:
   void FocusIfOpen() override;
   bool ActivateBrowser() override;
-  void Zoom(mojom::ZoomAction action) override;
+  void Zoom(mojom::ZoomAction action, ZoomSource source) override;
   BrowserWindowInterface* GetBrowserWindowInterface() override;
 
   PanelFocusDependentHotkeyManager*
