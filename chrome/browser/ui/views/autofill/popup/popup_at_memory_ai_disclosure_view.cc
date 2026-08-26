a1be28f2964d3e6149d2ab48f85211118ddb1da1
Bryan Kim <bryankim@google.com>
2026-08-26 15:52:31 -0700

    [Bedrock] Modularize views dialogs, certs, and web apps toolbar
    Extract leaf targets from static_library("ui") in
    chrome/browser/ui/BUILD.gn:
    - //chrome/browser/ui/views/web_apps/frame_toolbar:
      web_app_content_settings_container
    - //chrome/browser/ui/views:certificate_selector
    - //chrome/browser/ui/views:ssl_client_certificate_selector
    - //chrome/browser/ui/views:platform_keys_certificate_selector_chromeos
    - //chrome/browser/ui/views:external_protocol_handler_ash
    - //chrome/browser/ui/views/web_apps:
      protocol_handler_picker_coordinator
    - //chrome/browser/ui/views/autofill/popup:
      popup_at_memory_ai_disclosure_view
    
    AGENT_GENERATED_CL
    TAG=agy
    CONV=a8f61477-ee0c-4532-b311-cf223cd527cc
    
    Bug: 353332589
    Change-Id: I11111f9f490a7ab62191c939c3b8def4d58044b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280487
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686750}

--- chrome/browser/ui/views/autofill/popup/popup_at_memory_ai_disclosure_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_at_memory_ai_disclosure_view.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_base_view.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_view_utils.h"
-#include "chrome/browser/ui/views/autofill/popup/popup_view_views.h"
 #include "chrome/common/webui_url_constants.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
