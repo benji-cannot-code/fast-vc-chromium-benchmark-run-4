7621030755f59da41a5b052e6cd79db2f8c4a706
Thomas Lukaszewicz <tluk@chromium.org>
2026-08-25 18:14:44 -0700

    Migrate support_tool tests to BrowserWindowInterface
    Convert tests in chrome/browser/support_tool to use
    BrowserWindowInterface instead of Browser and eliminate calls to
    GetBrowserForMigrationOnly().
    
    Remove obsolete #include "chrome/browser/ui/browser.h" from test files
    and update DEPS.
    
    TAG=agy
    CONV=254bda88-3d43-4784-9c47-8e162cd86368
    
    Bug: 542648384
    Change-Id: Ie72c8d1e984c269adf454b0b4ace52e2b3f32f4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8293218
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686081}

--- chrome/browser/support_tool/support_tool_util_browsertest.cc
+++ chrome/browser/support_tool/support_tool_util_browsertest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/profile_policy_connector_builder.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/support_tool/data_collection_module.pb.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
