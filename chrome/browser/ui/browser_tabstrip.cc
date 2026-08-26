fed943ac7d0e55e68b29912b7dcb763b399e3905
Bryan Oltman <bryanoltman@google.com>
2026-08-26 11:33:07 -0700

    Activate browser window when adding a new foreground tab
    Explicitly set `params.window_action` to `kShowWindow` in
    `chrome/browser/ui/browser_tabstrip.cc`'s `AddAndReturnTabAt()` whenever
    adding a "foreground" tab to ensure that opening a new tab via the
    context menu causes the browser window gain key status on macOS.
    
    Fixed: 542600598
    Change-Id: Iea7c98d121df2ac7677fd88c808e865915e67300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256937
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686565}

--- chrome/browser/ui/browser_tabstrip.cc
+++ chrome/browser/ui/browser_tabstrip.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_tabstrip.h"
 
+#include <optional>
+
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "chrome/browser/ui/browser.h"
@@ -44,11 +46,15 @@ content::WebContents* AddAndReturnTabAt(
     int idx,
     bool foreground,
     std::optional<tab_groups::TabGroupId> group,
-    bool pinned) {
+    bool pinned,
+    std::optional<NavigateParams::WindowAction> window_action) {
   const GURL resolved_url = url.is_empty() ? GetNewTabURL(browser) : url;
   NavigateParams params(browser, resolved_url, ui::PAGE_TRANSITION_TYPED);
   params.disposition = foreground ? WindowOpenDisposition::NEW_FOREGROUND_TAB
                                   : WindowOpenDisposition::NEW_BACKGROUND_TAB;
+  if (window_action) {
+    params.window_action = window_action.value();
+  }
   params.tabstrip_index = idx;
   params.group = group;
   if (pinned) {
@@ -71,9 +77,10 @@ void AddTabAt(BrowserWindowInterface* browser,
               int idx,
               bool foreground,
               std::optional<tab_groups::TabGroupId> group,
-              bool pinned) {
+              bool pinned,
+              std::optional<NavigateParams::WindowAction> window_action) {
   /*void*/ AddAndReturnTabAt(browser, url, idx, foreground, std::move(group),
-                             pinned);
+                             pinned, window_action);
 }
 
 content::WebContents* AddSelectedTabWithURL(BrowserWindowInterface* browser,
