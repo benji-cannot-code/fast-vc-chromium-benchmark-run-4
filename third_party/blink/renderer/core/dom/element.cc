4d3d6145d127d9e068e61422dbf7aee40ef46660
Xianzhu Wang <wangxianzhu@chromium.org>
2026-08-25 17:00:35 -0700

    Fully inalidate LayoutObject in *Element::DidChangeIsInCanvasSubtree()
    In *::DidChangeIsInCanvasSubtree() we tried to invalidate the
    PaintLayer and all DisplayItemClients, but missed the scrollbars,
    maybe others. Now change to call LayoutObject paint invalidation
    methods instead of invalidating PaintLayer and DisplayItemClients
    directly.
    
    Thanks pdr@ for writing the test.
    
    Change-Id: Id58d6085f6e1d9d0fe3fe4a74d456d3172ed707f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8284881
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686049}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4503,9 +4503,8 @@ bool Element::IsCanvasOrInCanvasSubtree() const {
 void Element::DidChangeIsInCanvasSubtree() {
   if (auto* layout_object = GetLayoutObject()) {
     layout_object->SetNeedsPaintPropertyUpdate();
-    ObjectPaintInvalidator(*layout_object)
-        .SlowSetPaintingLayerNeedsRepaintAndInvalidateDisplayItemClient(
-            *layout_object, PaintInvalidationReason::kUncacheable);
+    layout_object->SetShouldDoFullPaintInvalidation();
+    layout_object->SetBackgroundNeedsFullPaintInvalidation();
   }
 }
 
