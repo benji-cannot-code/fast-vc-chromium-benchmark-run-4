30c070f3830480741c8a2af78387bbea053605bb
Timofey Chudakov <tchudakov@google.com>
2026-08-26 00:52:09 -0700

    [AutofillAi] Pass UI context when the user edited the entity.
    The save/update prompt can be accepted from the editor on Android.
    The UI context was not passed to the callback before, although it
    should be passed.
    
    Fixed: 551725455
    Change-Id: Icbd6f36d7a5334438656774e9e700bd929159d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8279733
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686186}

--- chrome/browser/autofill/android/autofill_ai_save_update_entity_prompt_controller.cc
+++ chrome/browser/autofill/android/autofill_ai_save_update_entity_prompt_controller.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
+#include "components/autofill/core/browser/integrators/autofill_ai/autofill_ai_import_utils.h"
 #include "components/autofill/core/browser/ui/addresses/autofill_address_util.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/strings/grit/components_strings.h"
@@ -187,7 +188,7 @@ void AutofillAiSaveUpdateEntityPromptController::RunPromptClosedCallback(
   if (prompt_result_callback_) {
     std::move(prompt_result_callback_)
         .Run(result, std::move(edited_entity_instance),
-             result == AutofillClient::AutofillAiBubbleResult::kAccepted
+             DidUserExplicitlyAcceptedImportPrompt(result)
                  ? ui_context_
                  : AutofillClient::EntityImportUIContext{});
   }
