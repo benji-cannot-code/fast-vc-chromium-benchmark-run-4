2d18dc701f9c1934ee87546e3c2e506afe3e490c
Janek Zarzycki <janzarzycki@google.com>
2026-08-26 07:28:18 -0700

    [SaaS report] Disable kSaasUsageReporting by default on ChromeOS.
    Updates the kSaasUsageReporting feature flag to be disabled by default on ChromeOS until we fix root cause of crashes.
    
    Bug: b:552317858
    Change-Id: I5545f0654fe73dc1d7c69c990051b06ec0c2cbd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8289912
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Janek Zarzycki <janzarzycki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686380}

--- components/enterprise/browser/reporting/reporting_features.cc
+++ components/enterprise/browser/reporting/reporting_features.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_reporting {
 
 BASE_FEATURE(kSaasUsageReporting,
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_CHROMEOS)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
