2e1c183421048d8f58eda5e2b1b3ac8ad3c2420c
Xianzhu Wang <wangxianzhu@chromium.org>
2026-08-26 13:24:15 -0700

    Remove unused PropertyTreeManager::CurrentEffectState
    It was planed for PropertyTreeManager::current_ to avoid the cost of
    Member<> in stack, but has never been actually used. Performance have
    been good since we first used EffectState for current_.
    
    Change-Id: I0f32c0d5f1a94bd3e6737f1b182dd876cf259683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8298479
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686621}

--- third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.cc
+++ third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.cc
@@ -26,27 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PropertyTreeManager::EffectState::EffectState(const CurrentEffectState& other)
-    : effect(other.effect),
-      clip(other.clip),
-      transform(other.transform),
-      effect_id(other.effect_id),
-      may_be_2d_axis_misaligned_to_render_surface(
-          other.may_be_2d_axis_misaligned_to_render_surface),
-      contained_by_non_render_surface_synthetic_rounded_clip(
-          other.contained_by_non_render_surface_synthetic_rounded_clip) {}
-
-PropertyTreeManager::CurrentEffectState::CurrentEffectState(
-    const EffectState& other)
-    : effect(other.effect),
-      clip(other.clip),
-      transform(other.transform),
-      effect_id(other.effect_id),
-      may_be_2d_axis_misaligned_to_render_surface(
-          other.may_be_2d_axis_misaligned_to_render_surface),
-      contained_by_non_render_surface_synthetic_rounded_clip(
-          other.contained_by_non_render_surface_synthetic_rounded_clip) {}
-
 PropertyTreeManager::PropertyTreeManager(PropertyTreeManagerClient& client,
                                          cc::PropertyTrees& property_trees,
                                          cc::Layer& root_layer,
