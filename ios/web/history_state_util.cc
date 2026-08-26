4a3d371d6bfec874b8adf166a5a9c78d766efbc1
Mike Dougherty <michaeldo@chromium.org>
2026-08-26 14:37:41 -0700

    Ensure blob URLs are not considered equivalent
    `IsHistoryStateChangeValid` was previously incorrectly associating all
    "non-standard" URLs as equivalent due to the usage of
    `DeprecatedGetOriginAsURL`.
    
    Fixed: 543471693
    Change-Id: I23304ae4f8cdf0238be58404d18f0dc72cea7897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8291494
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686710}

--- ios/web/history_state_util.cc
+++ ios/web/history_state_util.cc
@@ -6,16 +6,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/history_state_util.h"
 
 #include "base/check.h"
+#include "base/feature.h"
+#include "base/feature_list.h"
 #include "url/gurl.h"
+#include "url/origin.h"
 
 namespace web {
 namespace history_state_util {
+
+// Default enabled feature flag to control the non-standard URL fix. This is
+// intended for use as a kill switch.
+// TODO(crbug.com/553000452): Remove this flag once it's confirmed that the
+// non-standard URL fix is working as intended.
+BASE_FEATURE(kHistoryStateChangeNonStandardURLFix,
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsHistoryStateChangeValid(const GURL& current_url, const GURL& to_url) {
   // These two checks are very important to the security of the page. We cannot
   // allow the page to change the state to an invalid URL.
   CHECK(current_url.is_valid());
   CHECK(to_url.is_valid());
+
+  if (base::FeatureList::IsEnabled(kHistoryStateChangeNonStandardURLFix)) {
+    return url::IsSameOriginWith(current_url, to_url);
+  }
 
   return to_url.DeprecatedGetOriginAsURL() ==
          current_url.DeprecatedGetOriginAsURL();
