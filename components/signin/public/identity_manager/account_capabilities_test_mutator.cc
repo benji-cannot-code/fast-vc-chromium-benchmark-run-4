d2bd44d3f81ba46227a82da7575a87682ccd027a
James Lee <ljjlee@google.com>
2026-08-26 02:57:48 -0700

    Expose CanOverrideAccountInfo Account Capability
    Bug: 526865387
    Change-Id: Id87a1f8f5b197529277e59bcded45b206678f1ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287705
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686213}

--- components/signin/public/identity_manager/account_capabilities_test_mutator.cc
+++ components/signin/public/identity_manager/account_capabilities_test_mutator.cc
@@ -53,6 +53,12 @@ void AccountCapabilitiesTestMutator::
 }
 #endif
 
+void AccountCapabilitiesTestMutator::set_can_override_account_info(
+    bool value) {
+  capabilities_->capabilities_map_[kCanOverrideAccountInfoCapabilityName] =
+      value;
+}
+
 #if !BUILDFLAG(IS_IOS)
 void AccountCapabilitiesTestMutator::set_can_run_chrome_privacy_sandbox_trials(
     bool value) {
@@ -220,8 +226,8 @@ void AccountCapabilitiesTestMutator::set_is_subject_to_parental_controls(
 
 void AccountCapabilitiesTestMutator::set_is_subject_to_universal_opt_out(
     bool value) {
-  capabilities_
-      ->capabilities_map_[kIsSubjectToUniversalOptOutCapabilityName] = value;
+  capabilities_->capabilities_map_[kIsSubjectToUniversalOptOutCapabilityName] =
+      value;
 }
 
 #if BUILDFLAG(IS_IOS)
