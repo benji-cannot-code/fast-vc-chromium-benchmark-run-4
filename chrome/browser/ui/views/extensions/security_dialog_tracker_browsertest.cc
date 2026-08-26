2bca2e5132a0312ff7f24e1bda91a9a4fff38a44
Wenbin Zhang <wenbinzhang@google.com>
2026-08-26 11:34:30 -0700

    [bedrock] Remove obsolete TestWithBrowserView and references
    All BrowserView unit tests have been migrated to browser_tests or
    converted to ViewsTestBase mocks under Project Bedrock, leaving
    TestWithBrowserView unused. This removes the class, its build target,
    unused includes, and stale dependencies across the repository.
    
    TAG=agy
    CONV=0e2f8f65-9d3c-4886-8bb3-c908911c4a8c
    
    Bug: 543043111
    Change-Id: Ib14a53928a0a7f8723094fcf7f30b5db09565b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8291531
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686567}

--- chrome/browser/ui/views/extensions/security_dialog_tracker_browsertest.cc
+++ chrome/browser/ui/views/extensions/security_dialog_tracker_browsertest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "ui/views/test/widget_test.h"
--- chrome/browser/ui/views/user_education/impl/browser_feature_promo_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/user_education/impl/browser_feature_promo_controller_interactive_uitest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
-#include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/browser/ui/views/interaction/browser_elements_views.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_controller.h"
