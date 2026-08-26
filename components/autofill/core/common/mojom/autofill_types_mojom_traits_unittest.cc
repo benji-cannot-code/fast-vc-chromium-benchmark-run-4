ed37d34d70878a467142674ce4770c13b48c6fe1
Christoph Schwering <schwering@google.com>
2026-08-26 09:53:24 -0700

    [Autofill] Remove FormFieldData::check_status() and CheckStatus enum
    The CheckStatus is not used anymore.
    
    Since the launch of kAutofillIgnoreCheckableElements, Autofill does not
    extract <input type=checkbox> or <input type=radio> anymore. This CL is
    part of the cleanup.
    
    Bug: 402071086
    Change-Id: Iae962ca61dfae3de975598e708a7f5e3b22d6f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8251287
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686506}

--- components/autofill/core/common/mojom/autofill_types_mojom_traits_unittest.cc
+++ components/autofill/core/common/mojom/autofill_types_mojom_traits_unittest.cc
@@ -314,7 +314,6 @@ TEST_F(AutofillTypeTraitsTestImpl, PassFormFieldData) {
   input.set_host_form_id(FormRendererId(123));
   input.set_max_length(12345);
   input.set_is_autofilled_according_to_renderer(true);
-  input.set_check_status(FormFieldData::CheckStatus::kChecked);
   input.set_should_autocomplete(true);
   input.set_role(FormFieldData::RoleAttribute::kPresentation);
   input.set_text_direction(base::i18n::RIGHT_TO_LEFT);
@@ -348,7 +347,6 @@ TEST_F(AutofillTypeTraitsTestImpl, PassDataListFormFieldData) {
   input.set_aria_description(u"aria description");
   input.set_max_length(12345);
   input.set_is_autofilled_according_to_renderer(true);
-  input.set_check_status(FormFieldData::CheckStatus::kChecked);
   input.set_should_autocomplete(true);
   input.set_role(FormFieldData::RoleAttribute::kPresentation);
   input.set_text_direction(base::i18n::RIGHT_TO_LEFT);
