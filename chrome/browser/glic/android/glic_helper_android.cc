13a710fe941d8be8e1bcc1d19ccb84f623449423
Hailey Wang <haileywang@google.com>
2026-08-26 09:23:39 -0700

    [Glic] Add snackbar for mic permission denied
    Display a snackbar when the permission is denied or unable to send OS
    prompt. The snackbar redirect the user to app setting.
    
    https://screenshot-v2.corp.google.com/53ajne32hdcv0
    
    Bug: 545727652
    Change-Id: Ib5f96fdfaa8cf8d387bce6a1f1068c517e6dc8d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8265716
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686472}

---
+++ chrome/browser/glic/android/glic_helper_android.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/android/glic_helper_android.h"
+
+#include "base/android/jni_android.h"
+#include "ui/android/window_android.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
+#include "chrome/browser/glic/android/jni_headers/GlicHelper_jni.h"
+
+namespace glic {
+
+void ShowMicDisabledSnackbar(ui::WindowAndroid* window_android) {
+  if (window_android && window_android->GetJavaObject()) {
+    Java_GlicHelper_showMicDisabledSnackbar(
+        base::android::AttachCurrentThread(), window_android->GetJavaObject());
+  }
+}
+
+}  // namespace glic
