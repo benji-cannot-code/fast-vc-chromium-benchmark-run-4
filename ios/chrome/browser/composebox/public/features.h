35eb4e02943483197c3bc7d91be39a58b337af1d
Radu Nitescu <radunitescu@google.com>
2026-08-26 08:13:07 -0700

    [iOS] Add flag controlling verbatim suggestions display in AIM
    Fixed: 495524958
    Change-Id: I7aa639f3a1697f842105e74292bab13012fb1077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297098
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686411}

--- ios/chrome/browser/composebox/public/features.h
+++ ios/chrome/browser/composebox/public/features.h
@@ -44,6 +44,9 @@ bool ShowDeepSearchTool();
 // Whether to enable the server side state.
 bool EnableComposeboxServerSideState();
 
+// Weather verbatim suggestion is enabled in AIM in composebox.
+bool IsComposeboxVerbatimSuggestionEnabled();
+
 // Whether to enable compact mode.
 bool IsComposeboxCompactModeEnabled();
 
@@ -61,6 +64,9 @@ BASE_DECLARE_FEATURE(kComposeboxDeepSearch);
 
 // Used to enable server side state in the composebox.
 BASE_DECLARE_FEATURE(kComposeboxServerSideState);
+
+// Used to enable verbatim suggestions in AIM.
+BASE_DECLARE_FEATURE(kComposeboxVerbatimSuggestionInAIM);
 
 // Used to force top input plate in the composebox.
 BASE_DECLARE_FEATURE(kComposeboxForceTop);
