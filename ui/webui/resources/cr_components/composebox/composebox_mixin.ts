6f454f3eeb9a2788b71c42af55ca2b14cc8999bc
Ananya Seelam <ananyaseelam@google.com>
2026-08-25 18:27:59 -0700

    [context-input] Gate STS tab clearing
    Only reset restored tabs in `observeSmartTabSharingActive` if
    `smartTabSharingVisible` (STS enabled) is true.
    
    Auto suggested tabs were being reset prematurely in the side panel in
    this code path due to STS being default disabled.
    
    Bug: b:552035304
    Change-Id: I8a1f20fdb4db0f758379afee4d8a88250a32aeb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8291850
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Auto-Submit: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Wenhu Cheng <wenhuc@google.com>
    Reviewed-by: Juliet Knighton <julietlevesque@google.com>
    Commit-Queue: Wenhu Cheng <wenhuc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686086}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -402,7 +402,7 @@ export const ComposeboxEmbedderMixin =
               ComposeboxProxyImpl.getInstance().observeSmartTabSharingActive(
                   (active: boolean) => {
                     this.smartTabSharingActive = active;
-                    if (!active) {
+                    if (this.smartTabSharingVisible && !active) {
                       this.addedTabsIds = new Map();
                       this.resetRestoredTabs();
                     }
