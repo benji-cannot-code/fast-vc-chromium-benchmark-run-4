ad28253ef76cf3c985afd81a084a55a1c9952dec
Eric Leese <leese@chromium.org>
2026-08-26 07:45:56 -0700

    Remove monitorEvents and unmonitorEvents toString side-effect tests
    Removes the side-effect evaluation tests for the getters for monitorEvents and unmonitorEvents from the inspector protocol side-effects test suite.
    
    Bug: 528112664
    Change-Id: I8732ed5e30a5e280067aca0ee4b54255a22f40b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297418
    Auto-Submit: Eric Leese <leese@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686385}

--- third_party/blink/web_tests/http/tests/inspector-protocol/side-effects/evaluate-embedder-side-effect-free-methods.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/side-effects/evaluate-embedder-side-effect-free-methods.js
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Unsafe Command Line API
   await checkHasSideEffect(`monitorEvents()`);
   await checkHasSideEffect(`unmonitorEvents()`);
-  await checkHasNoSideEffect(`monitorEvents.toString()`);
-  await checkHasNoSideEffect(`unmonitorEvents.toString()`);
 
   // Document
   await checkHasNoSideEffect(`document.getElementsByTagName('div')`);
