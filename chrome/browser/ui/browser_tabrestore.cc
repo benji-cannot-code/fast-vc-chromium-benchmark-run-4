465ddb08e8240c4aa41acd114bfb1af32b62ed59
Keren Zhu <kerenzhu@chromium.org>
2026-08-25 22:09:24 -0700

    Defer SiteInstance creation for restored WebUI tabs
    Previously, CreateRestoredTab() queried tab_util::GetSiteInstanceFor-
    NewTab() and passed a non-null SiteInstance during CreateParams
    initialization if the restored tab was a WebUI URL. This eagerly
    allocated a RenderWidgetHostViewAura with full-window bounds (0, 0, W,
    H) and LAYER_SURFACE before the layout pass had run.
    
    When WindowReorderer stacked the background contents window above the
    top container, the stale bounds occluded the WebUI toolbar upon tab
    activation (crbug.com/547718312).
    
    This CL removes the early SiteInstance creation from
    CreateRestoredTab(). This aligns WebUI restored tabs with HTTP/HTTPS
    tabs by deferring SiteInstance, which as a result defers the view
    creation to LoadIfNecessary().
    
    Note that this upfront SiteInstance creation was to prevent an
    unnecessary render process swap from an about:blank process to a new
    trusted WebUI render process. This is not longer necessary after
    https://crrev.com/c/1080091, which specifies kNoRendererProcess for
    restored WebUIs.
    
    TAG=agy
    CONV=c39e6608-c77e-4d37-849d-c8f33c0f2fd1
    
    Fixed: 547718312
    Change-Id: Iacbce7d0efa60af2e81ba913ba4d7b324f762d20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287626
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686151}

--- chrome/browser/ui/browser_tabrestore.cc
+++ chrome/browser/ui/browser_tabrestore.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sessions/session_restore.h"
 #include "chrome/browser/sessions/session_service_base.h"
 #include "chrome/browser/sessions/session_service_lookup.h"
-#include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
@@ -64,7 +63,6 @@ std::unique_ptr<WebContents> CreateRestoredTab(
     const std::map<std::string, std::string>& extra_data,
     bool initially_hidden,
     bool from_session_restore) {
-  GURL restore_url = navigations[selected_navigation].virtual_url();
   // TODO(ajwong): Remove the temporary session_storage_namespace_map when
   // we teach session restore to understand that one tab can have multiple
   // SessionStorageNamespace objects. Also remove the
@@ -73,9 +71,7 @@ std::unique_ptr<WebContents> CreateRestoredTab(
   content::SessionStorageNamespaceMap session_storage_namespace_map =
       content::CreateMapWithDefaultSessionStorageNamespace(
           browser->GetProfile(), session_storage_namespace);
-  WebContents::CreateParams create_params(
-      browser->GetProfile(),
-      tab_util::GetSiteInstanceForNewTab(browser->GetProfile(), restore_url));
+  WebContents::CreateParams create_params(browser->GetProfile());
   create_params.initially_hidden = initially_hidden;
   create_params.desired_renderer_state =
       WebContents::CreateParams::kNoRendererProcess;
