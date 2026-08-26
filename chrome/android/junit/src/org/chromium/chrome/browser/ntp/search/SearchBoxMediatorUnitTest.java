b5c9efd185951a3b9c3e4479334eadb64a0af526
Yanling <yanlinghu@google.com>
2026-08-26 10:29:01 -0700

    Reland "[NTP Aurora] Fix fake search box hint text color"
    This reverts commit 45f84c2ddb52ffbe7f1193ad92f8878d6b10c22e.
    
    Reason for revert:  this CL fixed the test failures.
    
    Original change's description:
    > Revert "[NTP Aurora] Fix fake search box hint text color"
    >
    > This reverts commit 44d516765df7cecc5b07517093c3b07403984508.
    >
    > Reason for revert: Suspected cause of SearchBoxMediatorUnitTest#testApplyWhiteBackgroundWithShadow failures
    >
    > Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/android-10-x86-nofieldtrial-rel/5203/
    >
    > Original change's description:
    > > [NTP Aurora] Fix fake search box hint text color
    > >
    > > My previous CL
    > > (https://chromium-review.git.corp.google.com/c/chromium/src/+/8161482)
    > > missed properly styling the fake search box hint text when NTP Aurora is
    > > enabled. Additionally, we decided to retain the original font (as if no
    > > white background is applied) and only update the text color to
    > > colorOutline.
    > >
    > > Screenshots: https://docs.google.com/document/d/1bMRVyyqOd0-cAmyTbL9IhKNU9uSvDdpdCngSzSXIHnk/edit?tab=t.0#bookmark=id.crfjs6gr63um
    > >
    > > Bug: 552005771
    > > Change-Id: Iaf49439c91617933e81b7768261d3a64f6e1a06c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277323
    > > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > > Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    > > Commit-Queue: Yanling Hu <yanlinghu@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1685657}
    >
    > Bug: 552005771, 548708734
    > Bug: 552005771
    > Change-Id: Ibcd8b78dbc38005a1badbc4add3a0731ae9000f3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8289488
    > Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Richard Townsend <ritownsend@google.com>
    > Commit-Queue: Richard Townsend <ritownsend@google.com>
    > Reviewed-by: Richard Townsend <ritownsend@google.com>
    > Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1685909}
    
    Bug: 552005771, 548708734
    Bug: 552005771
    Change-Id: I702ee00f426bcf5bd435deae3e8d1aae1c9a8c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287568
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686534}

--- chrome/android/junit/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediatorUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ntp/search/SearchBoxMediatorUnitTest.java
@@ -53,6 +53,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.composeplate.ComposeplateUtils;
 import org.chromium.chrome.browser.composeplate.ComposeplateUtilsJni;
 import org.chromium.chrome.browser.feed.FeedSurfaceScrollDelegate;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.lens.LensController;
 import org.chromium.chrome.browser.lens.LensIntentParams;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
@@ -317,6 +318,7 @@ public class SearchBoxMediatorUnitTest {
     }
 
     @Test
+    @DisableFeatures(ChromeFeatureList.NTP_AURORA)
     public void testApplyWhiteBackgroundWithShadow() {
         Drawable defaultBackground =
                 mContext.getDrawable(R.drawable.home_surface_search_box_background);
@@ -694,5 +696,37 @@ public class SearchBoxMediatorUnitTest {
                             .getColor()
                             .getDefaultColor());
         }
+    }
+
+    @Test
+    @EnableFeatures(ChromeFeatureList.NTP_AURORA)
+    public void testGetFakeSearchBoxTextStyle_auroraEnabled_applyTrue() {
+        assertEquals(
+                R.style.TextAppearance_FakeSearchBoxTextNewStyle,
+                SearchBoxMediator.getFakeSearchBoxTextStyle(/* apply= */ true));
+    }
+
+    @Test
+    @EnableFeatures(ChromeFeatureList.NTP_AURORA)
+    public void testGetFakeSearchBoxTextStyle_auroraEnabled_applyFalse() {
+        assertEquals(
+                R.style.TextAppearance_FakeSearchBoxTextNewStyle,
+                SearchBoxMediator.getFakeSearchBoxTextStyle(/* apply= */ false));
+    }
+
+    @Test
+    @DisableFeatures(ChromeFeatureList.NTP_AURORA)
+    public void testGetFakeSearchBoxTextStyle_auroraDisabled_applyTrue() {
+        assertEquals(
+                R.style.TextAppearance_FakeSearchBoxTextMediumDark,
+                SearchBoxMediator.getFakeSearchBoxTextStyle(/* apply= */ true));
+    }
+
+    @Test
+    @DisableFeatures(ChromeFeatureList.NTP_AURORA)
+    public void testGetFakeSearchBoxTextStyle_auroraDisabled_applyFalse() {
+        assertEquals(
+                R.style.TextAppearance_FakeSearchBoxTextMedium,
+                SearchBoxMediator.getFakeSearchBoxTextStyle(/* apply= */ false));
     }
 }
