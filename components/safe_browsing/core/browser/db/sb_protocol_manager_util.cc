2d7d9be8de9dc0e8989c5ecb3c159cf83cb9628b
thefrog <thefrog@chromium.org>
2026-08-26 10:18:42 -0700

    Use emplace_back in SBProtocolManagerUtil
    This addresses feedback from https://crrev.com/c/8156339 (and applies it
    in another place in the file too).
    
    Bug: 362791941
    Change-Id: Idb8b713d6292658db11cbb010a7ff47e49ff72da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8270812
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686525}

--- components/safe_browsing/core/browser/db/sb_protocol_manager_util.cc
+++ components/safe_browsing/core/browser/db/sb_protocol_manager_util.cc
@@ -157,7 +157,7 @@ void GenerateHostVariantsToCheckV4(const std::string& host,
        i != host.rend() && hosts->size() < kMaxHostsToCheck; ++i) {
     if (*i == '.') {
       if (skipped_last_component) {
-        hosts->push_back(std::string(i.base(), host.end()));
+        hosts->emplace_back(i.base(), host.end());
       } else {
         skipped_last_component = true;
       }
@@ -848,7 +848,7 @@ void SBProtocolManagerUtil::GeneratePathVariantsToCheck(
   for (std::string::const_iterator i(path.begin());
        i != path.end() && paths->size() < kMaxPathsToCheck; ++i) {
     if (*i == '/') {
-      paths->push_back(std::string(path.begin(), i + 1));
+      paths->emplace_back(path.begin(), i + 1);
     }
   }
 
