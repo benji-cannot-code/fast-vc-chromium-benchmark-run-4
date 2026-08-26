3f4cd466b7b6f128e1d54143d0981f6d5481fbbc
Nafis Abedin <nafisabedin@google.com>
2026-08-25 18:58:36 -0700

    [network-service] Bind URLLoaderFactory to high priority runner
    In a recent experiment, it was seen that converting Browser ->
    NetworkService communication to be an IPC drastically improved
    LoaderStartToFetchStart duration for main/subframe navigations on
    Android at the tail-end. NetworkService thread runs in the Browser
    process in Android.
    
    This is because MessagePumpEpoll will pick from SequenceManager as well
    as check for IPC every run loop. Without NetworkContextDirectReceiver,
    these "IPCs" would be a PostTask meaning they could be serviced much
    later if the NetworkService thread was busy when, for the most part, we
    will probably want to handle these as soon as possible since critical
    parts of navigation can be held back at least on Android with lack of
    prioritization on PostTasks.
    
    So let's try experimenting with binding these receivers to a higher
    priority task runner to see if we can get the same improvements to
    LoaderStartToFetchStart without including IPC overhead.
    
    Bug: 468329446
    Change-Id: Ifb6ddfe1055d0ecffd2d5306588b2539605645f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8206349
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686100}

--- services/network/prefetch_matching_url_loader_factory.cc
+++ services/network/prefetch_matching_url_loader_factory.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
 #include "base/task/common/task_annotator.h"
+#include "base/task/single_thread_task_runner.h"
+#include "net/base/task/task_runner.h"
 #include "services/network/cors/cors_url_loader_factory.h"
 #include "services/network/prefetch_cache.h"
 #include "services/network/prefetch_url_loader_client.h"
@@ -22,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace network {
 
 namespace {
+
 void LogCreateLoaderAndStartQueueTimeHistogram(bool is_outermost_main_frame) {
   auto* task = base::TaskAnnotator::CurrentTaskForThread();
   // Only log non-delayed tasks with a valid queue_time.
@@ -36,6 +39,15 @@ void LogCreateLoaderAndStartQueueTimeHistogram(bool is_outermost_main_frame) {
                     is_outermost_main_frame ? ".MainFrame" : ".Subframe"}),
       base::TimeTicks::Now() - task->queue_time);
 }
+
+const scoped_refptr<base::SingleThreadTaskRunner>& GetFactoryTaskRunner() {
+  if (base::FeatureList::IsEnabled(
+          features::kBindURLLoaderFactoryToHighPriorityTaskRunner)) {
+    return net::GetTaskRunner(net::RequestPriority::HIGHEST);
+  }
+  return base::SingleThreadTaskRunner::GetCurrentDefault();
+}
+
 }  // namespace
 
 PrefetchMatchingURLLoaderFactory::PrefetchMatchingURLLoaderFactory(
@@ -56,7 +68,7 @@ PrefetchMatchingURLLoaderFactory::PrefetchMatchingURLLoaderFactory(
       cache_(cache),
       use_matches_(base::FeatureList::IsEnabled(
           features::kNetworkContextPrefetchUseMatches)) {
-  receivers_.Add(this, std::move(receiver));
+  receivers_.Add(this, std::move(receiver), GetFactoryTaskRunner());
   // This use of base::Unretained() is safe because `receivers_` won't call the
   // disconnect handler after it has been destroyed.
   receivers_.set_disconnect_handler(base::BindRepeating(
@@ -130,7 +142,7 @@ void PrefetchMatchingURLLoaderFactory::CreateLoaderAndStart(
 
 void PrefetchMatchingURLLoaderFactory::Clone(
     mojo::PendingReceiver<URLLoaderFactory> receiver) {
-  receivers_.Add(this, std::move(receiver));
+  receivers_.Add(this, std::move(receiver), GetFactoryTaskRunner());
 }
 
 void PrefetchMatchingURLLoaderFactory::ClearBindings() {
