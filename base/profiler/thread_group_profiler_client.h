4a9ed31aaa95a17e1e5400c6a946f1a488a40b55
Etienne Pierre-doray <etiennep@chromium.org>
2026-08-27 06:50:32 -0700

    [ThreadPool] Synchronize ThreadGroupProfiler using ThreadGroupImpl lock
    Previously, ThreadGroupProfiler used a sequence-bound pattern where
    worker thread lifecycle events (started, active, idle, exiting) were
    posted to a dedicated task runner to synchronize profiler state. This
    introduced asynchronous thread hops, complex shutdown sequencing, and
    potential races with thread exits
    (549390601,549465784,549311554,549363784,
    549630717,549372357,549586806,549636742).
    
    This CL refactors ThreadGroupProfiler to leverage ThreadGroupImpl's
    existing lock:
    - Replaces the dedicated sequence and thread hopping with synchronous
      state updates under ThreadGroupImpl's lock_.
    - Eliminates duplicate/shadowed worker tracking in ThreadGroupProfiler:
      ThreadGroupImpl now directly inspects its own `workers_` and
      `idle_workers_set_` when initiating or managing profiling.
    - Removes ThreadGroupProfiler::Shutdown() and associated teardown
      complexity.
    
    Bug: 549390601
    Change-Id: Ieb282bd73c3bdbdbb571a50d8bb07d5727ed3ade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8271150
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687168}

--- base/profiler/thread_group_profiler_client.h
+++ base/profiler/thread_group_profiler_client.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/functional/callback.h"
+#include "base/profiler/sampling_profiler_thread_token.h"
 #include "base/profiler/stack_sampling_profiler.h"
 
 namespace base {
--- base/task/thread_pool/thread_group_impl.h
+++ base/task/thread_pool/thread_group_impl.h
@@ -38,7 +38,8 @@ class TaskTracker;
 // posted at any time but will not run until after Start() is called.
 //
 // This class is thread-safe.
-class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
+class BASE_EXPORT ThreadGroupImpl : public ThreadGroup,
+                                    public ThreadGroupProfiler::Delegate {
  public:
   // Constructs a group without workers.
   //
@@ -55,7 +56,7 @@ class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
       ThreadType thread_type_hint,
       int64_t thread_group_type,
       TrackedRef<TaskTracker> task_tracker,
-      TrackedRef<Delegate> delegate,
+      TrackedRef<ThreadGroup::Delegate> delegate,
       bool monitor_worker_thread_priorities = false,
       ThreadPoolInstance::RecordLockContention record_lock_contention =
           ThreadPoolInstance::RecordLockContention::kDisabled);
@@ -104,6 +105,7 @@ class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
   // may_block_threshold(), both in ThreadGroup.
   friend class ThreadGroupImplBlockingTest;
   friend class ThreadGroupImplMayBlockTest;
+  friend class ThreadGroupImplProfilingTest;
   FRIEND_TEST_ALL_PREFIXES(ThreadGroupImplBlockingTest,
                            ThreadBlockUnblockPremature);
   FRIEND_TEST_ALL_PREFIXES(ThreadGroupImplBlockingTest,
@@ -131,6 +133,11 @@ class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
   // Returns the number of workers that are awake (i.e. not on the idle set).
   size_t GetNumAwakeWorkersLockRequired() const EXCLUSIVE_LOCKS_REQUIRED(lock_);
 
+  // ThreadGroupProfiler::Delegate:
+  void OnStartProfilingSession(
+      ThreadGroupProfiler::ActiveCollection active_collection) override;
+  void OnEndProfilingSession() override;
+
   bool IsOnIdleSetLockRequired(WorkerThread* worker) const
       EXCLUSIVE_LOCKS_REQUIRED(lock_);
 
@@ -150,9 +157,13 @@ class BASE_EXPORT ThreadGroupImpl : public ThreadGroup {
 
   // This is set in Start() if profiling is enabled, before any worker thread is
   // created. If profiling is not enabled, this will remain std::nullopt. If
-  // created the ThreadGroupProfiler instance will exist until ThreadGroupImpl
-  // destruction.
+  // created, the ThreadGroupProfiler instance will exist until ThreadGroupImpl
+  // destruction or JoinForTesting().
   std::optional<ThreadGroupProfiler> thread_group_profiler_;
+
+  // Active collection session when profiling is running.
+  std::optional<ThreadGroupProfiler::ActiveCollection> active_collection_
+      GUARDED_BY(lock_);
 
   // Ensures recently cleaned up workers (ref.
   // WorkerDelegate::CleanupLockRequired()) had time to exit as
