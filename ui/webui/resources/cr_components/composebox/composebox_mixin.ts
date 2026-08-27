393851e399463428177b7b597517ec90aec5782a
James Leung <jamesleung@google.com>
2026-08-27 13:07:16 -0700

    [contextual_tasks] Ensure text is not cleared when link is clicked
    * Due to postMessages sent from Google to Chrome when a link is
      clicked, `clearAutocomplete` is run, which clears the text input
      incorrectly.
    * Fix:
    Ensure that `clearAutocomplete` does not reset `lastQueriedInput`. In
    `composebox_mixin.ts`, this allows line 591 in `updated()` function
    (which runs after `clearAutocomplete` due to `selectedMatchIndex`
    changing) to not clear the input since the `lastQueriedInput` is not
    cleared.
    * This will not cause regressions/ghost text since `lastQueriedInput`
      is still cleared after autocomplete is cleared / a suggestion
      is clicked. This CL essentially ensures that `lastQueriedInput`
      is not cleared in `clearAutocomplete`, but the step after
      (navigation caused by suggestion click) (so it is cleared
      after `updated()` runs, which is where the bug would have occurred).
    * Add tests.
    
    Before: screencast/cast/NTQ4MDI1OTc3MTE3MDgxNnw5N2E1MzE1Yi02Mw
    
    After, with 'side panel silk api when link clicked' mendel flags
    enabled: Side panel -
    screencast/cast/NDU1MjA3MTk0Nzc0NzMyOHw0ZmEzZjU3NC1mZg Full tab -
    screencast/cast/NTc0MjA3MTI4ODY5MjczNnwyMDU0NGYwZS0wNw
    
    No regressions in ntp, omnibox composeboxes -
    screencast/cast/NTI1MDE1NzE0MTU1NzI0OHxmNGM5OTRmYi1hNg
    screencast/cast/NTY4OTI5NDczMDU1OTQ4OHw5NzgyYWU2Yy0xMw
    
    Bug: b:539685837
    Change-Id: Id937138b76e0006b15d28432ec2d205f21c09242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8301254
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: James Leung <jamesleung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687502}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -2229,7 +2229,6 @@ export const ComposeboxEmbedderMixin =
           // Autocomplete sends updates once it is stopped. Invalidate those
           // results by setting `activeQueryId` to -1.
           this.activeQueryId = -1;
-          this.lastQueriedInput = '';
         }
 
         /**
