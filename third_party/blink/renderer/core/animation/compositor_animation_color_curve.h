85a1c5146243b403490cf6e965a502f0042cf850
Kevin Ellis <kevers@google.com>
2026-08-27 06:16:29 -0700

    NPW: Support currentcolor system colors, and color-mix.
    This patch enables background-color animations to run on the
    compositor when a keyframe uses a style dependent color.
    
    Bug: 40795239
    Change-Id: I35114fe3cd85c3bcc5b8f7192308d9e62da53588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8196904
    Reviewed-by: Free Debreuil <freedebreuil@google.com>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687152}

--- third_party/blink/renderer/core/animation/compositor_animation_color_curve.h
+++ third_party/blink/renderer/core/animation/compositor_animation_color_curve.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_ANIMATION_COMPOSITOR_ANIMATION_COLOR_CURVE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_ANIMATION_COMPOSITOR_ANIMATION_COLOR_CURVE_H_
 
+#include "third_party/blink/public/mojom/css/preferred_color_scheme.mojom-shared.h"
 #include "third_party/blink/renderer/core/animation/compositor_animation_curve.h"
 
 namespace blink {
@@ -34,6 +35,11 @@ class CORE_EXPORT CompositorAnimationColorCurve
 
   bool IsOpaque() { return is_opaque_; }
 
+  bool NeedsKeyframeSnapshotUpdate(const Document& document,
+                                   const ComputedStyle& style) const override;
+
+  void UpdateStyleDependencies(const Element& element) override;
+
  protected:
   scoped_refptr<CompositorAnimationCurve> Clone() override;
 
@@ -41,6 +47,8 @@ class CORE_EXPORT CompositorAnimationColorCurve
   Color ConvertTypedInterpolationValue(
       const TypedInterpolationValue* value) override;
   Color InterpolateKeyframes(wtf_size_t index, double progress) override;
+
+  void UpdateIsOpaque();
 
  private:
   explicit CompositorAnimationColorCurve(CSSPropertyName property_name)
@@ -50,6 +58,8 @@ class CORE_EXPORT CompositorAnimationColorCurve
         is_opaque_(other.is_opaque_) {}  // NOLINT(modernize-use-equals-default)
 
   bool is_opaque_ = true;
+  Color current_color_ = Color::kBlack;
+  mojom::blink::ColorScheme color_scheme_ = mojom::blink::ColorScheme::kDark;
 };
 
 }  // namespace blink
