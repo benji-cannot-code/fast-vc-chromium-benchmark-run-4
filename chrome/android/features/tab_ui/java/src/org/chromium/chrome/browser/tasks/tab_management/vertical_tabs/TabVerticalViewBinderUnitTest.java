80952f6f8c85d047a72e0e6b9ccdc4fea2bca08c
Hao Dong <spdonghao@google.com>
2026-08-26 17:56:10 -0700

    [Vertical Tabs] Fix pinned tab item decoration offsets during moves.
    1. In calculatePinnedTabItemOffsets(), remove stale layout params span
       index override so offsets are recalculated.
    2. Invalidate item decorations on item moved.
    3. Remove vertical_tab_pinned_item_width.
    
    Before: http://screencast/cast/NDY5NzkyODkyODA2NzU4NHxlZTIyZWFmMS1lNA
    After: http://screencast/cast/NjM1NDQ1Nzc5MjA4NjAxNnwxZWZkYmU3Zi1iZA
    
    Bug: 553004273
    Change-Id: I138f45b8e73afc45f1b3dabc7de67c04fba71720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8298401
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686815}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/TabVerticalViewBinderUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/vertical_tabs/TabVerticalViewBinderUnitTest.java
@@ -1976,7 +1976,7 @@ public class TabVerticalViewBinderUnitTest {
         int defaultXmlWidth =
                 pinnedView
                         .getResources()
-                        .getDimensionPixelSize(R.dimen.vertical_tab_pinned_item_width);
+                        .getDimensionPixelSize(R.dimen.vertical_tab_pinned_item_min_width);
         int expectedHeight =
                 pinnedView
                         .getResources()
