13fdbd25a9a67f5c22183da54103e9fb0364eb4a
Timofey Chudakov <tchudakov@google.com>
2026-08-27 07:52:59 -0700

    [Android] Remove unused removeTab method.
    Bug: 40269514
    Change-Id: I40cc4ae199465b1679c204eb7d865efd46958c08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8305753
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687233}

--- chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/button_group_component/KeyboardAccessoryButtonGroupMediator.java
+++ chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/button_group_component/KeyboardAccessoryButtonGroupMediator.java
@@ -63,11 +63,6 @@ class KeyboardAccessoryButtonGroupMediator
     }
 
     @Override
-    public void removeTab(KeyboardAccessoryData.Tab tab) {
-        mModel.get(TABS).remove(tab);
-    }
-
-    @Override
     public void setTabs(KeyboardAccessoryData.Tab[] tabs) {
         mModel.get(TABS).set(tabs);
     }
