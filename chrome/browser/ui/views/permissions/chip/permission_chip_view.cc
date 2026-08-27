ffb3f82ce3f5636a63c012086201e7d582cb71e4
Maks Orlovich <morlovich@chromium.org>
2026-08-27 09:50:39 -0700

    Reland "Update permission chip/dashboard tests for WebUILocationBar compat."
    This reverts commit 11374aacebb8a0c11d549c257521608ff07d4248.
    
    Reason for revert: Dropped changes to the test that became flaky
    
    Original change's description:
    > Revert "Update permission chip/dashboard tests for WebUILocationBar compat."
    >
    > This reverts commit d342ee68f64b8a7a9646b9285b2ed80b35517817.
    >
    > [gardener]
    > Reason for revert:
    > Causes failures in `interactive_ui_tests` test suite on win11-arm64-rel-tests bot:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/win11-arm64-rel-tests
    >
    > Notably, the test DashboardKombuchaInteractiveUITest.SuppressPageInfoReopen fails.
    >
    > Example failed run:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/win11-arm64-rel-tests/8450/overview
    >
    > Failure message:
    > ```
    > interactive_test_internal.cc(498): Failed
    > Interactive test failed on step 22 (WaitForHide()) with reason kSequenceDestroyed; step type kHidden; id ElementIdentifier: PageInfoMainView::kPermissionsElementId
    > UI Elements
    >   ╰─[CURRENT CONTEXT] Tabbed browser window, 1 tab(s) (active: 0) profile Default at x:10-810 y:10-610 (800x600)
    >      ├─ViewSubregionAnchor - kTabStripFrameDialogAnchorId at x:467-467 y:38-38 (0x0)
    >      ├─ViewSubregionAnchor - kFallbackPopupAnchorElementId at x:409-411 y:92-93 (2x1)
    >      ├─ViewSubregionAnchor - kBrowserDialogAnchorElementId at x:409-411 y:89-90 (2x1)
    >      ├─TrackedElementWebUI - kReloadButtonElementId at x:92-126 y:56-90 (34x34)
    >      ├─Widget "BrowserWidget" at x:10-810 y:10-610 (800x600)
    >      │  ╰─BrowserFrameViewWin - kBrowserFrameElementId at x:18-802 y:10-602 (784x592)
    >      │     ╰─BrowserView - kBrowserViewElementId at x:18-802 y:10-602 (784x592)
    >      │        ├─HorizontalTabStripRegionViewOld - kTabStripRegionElementId at x:18-6...
    > ```
    >
    > Original change's description:
    > > Update permission chip/dashboard tests for WebUILocationBar compat.
    > >
    > > (So they pass with --enable-features=WebUILocationBar)
    > >
    > > This is largely based on Russ Hamilton's
    > > https://chromium-review.googlesource.com/c/chromium/src/+/8279769
    > > with additional work on top to make all of the tests in these files run.
    > >
    > > Bug: 545160323
    > > Change-Id: Id164c80af19af14cb770dc2a33950a3eaae8136a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287865
    > > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > > Reviewed-by: Elias Klim <elklm@chromium.org>
    > > Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1686304}
    >
    > Bug: 545160323
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I9e089f34ad8d55c9a9c9821f8b07fc7057638e2b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8301672
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Mark Pearson <mpearson@chromium.org>
    > Commit-Queue: Mark Pearson <mpearson@chromium.org>
    > Auto-Submit: Mark Pearson <mpearson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1686791}
    
    Bug: 545160323
    Change-Id: Id9b4b239340bc3179a91a76c635c885b6859440e
    Cq-Include-Trybots: luci.chromium.try:win-arm64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8301992
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687330}

--- chrome/browser/ui/views/permissions/chip/permission_chip_view.cc
+++ chrome/browser/ui/views/permissions/chip/permission_chip_view.cc
@@ -537,6 +537,10 @@ void PermissionChipView::ExecuteForTesting() {
   NotifyClick(event);
 }
 
+void PermissionChipView::EndAnimationForTesting() {
+  animation_->End();
+}
+
 BEGIN_METADATA(PermissionChipView)
 ADD_READONLY_PROPERTY_METADATA(int, IconSize)
 END_METADATA
