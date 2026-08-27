ea7833130d2fd9ff5729234b4ca92648c0aef346
Hyowon Kim <hyowon@igalia.com>
2026-08-26 19:56:02 -0700

    Reset font orientation when resolving canvas 2D fonts
    Canvas 2D fonts must use horizontal orientation regardless of the canvas
    element's writing mode. writing-mode/text-orientation aren't set by the
    text preparation algorithm, so they default to horizontal:
    https://html.spec.whatwg.org/multipage/canvas.html#text-preparation-algorithm
    
    Bug: 433324167
    Change-Id: I894eba74a61f8eb55221c6a8d9facaa6c402e89a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8238611
    Commit-Queue: Hyowon Kim <hyowon@igalia.com>
    Reviewed-by: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686908}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -98,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/canvas/canvas2d/path_2d.h"
 #include "third_party/blink/renderer/modules/canvas/htmlcanvas/canvas_context_creation_attributes_helpers.h"
 #include "third_party/blink/renderer/platform/fonts/font.h"
+#include "third_party/blink/renderer/platform/fonts/font_orientation.h"
 #include "third_party/blink/renderer/platform/geometry/layout_unit.h"
 #include "third_party/blink/renderer/platform/geometry/path.h"
 #include "third_party/blink/renderer/platform/geometry/path_builder.h"
@@ -694,6 +695,9 @@ bool CanvasRenderingContext2D::ResolveFont(const String& new_font) {
           element_font_description.SpecifiedSize());
       element_font_description.SetAdjustedSize(
           element_font_description.SpecifiedSize());
+      // Reset the orientation to avoid inheriting the vertical
+      // writing-mode/text-orientation from the <canvas> element.
+      element_font_description.SetOrientation(FontOrientation::kHorizontal);
 
       font_style_builder.SetFontDescription(element_font_description);
       const ComputedStyle* font_style = font_style_builder.TakeStyle();
