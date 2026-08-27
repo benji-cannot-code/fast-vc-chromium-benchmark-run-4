a7cf7d622f544441f8b7ff6bec5cc4cd92501da0
Maria del Pilar Dominguez Molina <mardelpi@google.com>
2026-08-27 08:14:26 -0700

    [ChroMenu GlowUp] Implement block-style menu items in ActionAppMenu
    This CL integrates the block-style menu items with their corresponding
    style into the ActionAppMenu. To achieve this it creates the
    BlockMenuEntryButton view. When creating the menu, it adds the top row
    with its corresponding actions (New Tab, New Window, New Incognito
    Window).
    
    Bug: 526711462
    Change-Id: I37e307c54688e76198244b653c3b5908d87dd340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269966
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: María del Pilar Domínguez Molina <mardelpi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687247}

--- chrome/browser/ui/views/app_menu/action_app_menu_manager.cc
+++ chrome/browser/ui/views/app_menu/action_app_menu_manager.cc
@@ -271,7 +271,10 @@ void ActionAppMenuManager::CreateMenuHierarchy() {
 }
 
 void ActionAppMenuManager::AddBlockHeaderActions(actions::ActionItem* root) {
-  auto builder = AppMenuBuilder(root);
+  auto block_section = CreateSectionActionItem(u"", DisplayType::kBlock);
+  auto* block_section_ptr = root->AddChild(std::move(block_section));
+
+  auto builder = AppMenuBuilder(block_section_ptr);
   builder
       .AddAction(kActionNewTab, DisplayType::kBlock,
                  /*text_override=*/std::nullopt,
@@ -337,7 +340,6 @@ void ActionAppMenuManager::AddYourChromeActions(actions::ActionItem* root) {
 void ActionAppMenuManager::AddToolsAndActionsActions(
     actions::ActionItem* root) {
   AppMenuBuilder builder(root, kColorAppMenuToolsAndActionsBackground);
-
   builder.AddSectionHeader(IDS_APP_MENU_TOOLS_AND_ACTIONS_HEADER)
       .AddSubmenu(
           kActionZoomSubmenu,
