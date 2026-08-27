fde8e6a5a3b73d97b7f63c251826e603e9d4341a
Jayden Nikifork <nikifork@google.com>
2026-08-27 07:38:04 -0700

    [SPC] Fix locale outcome histogram double counting
    Before, the PaymentRequest.SecurePaymentConfirmation.LocaleOutcome was
    emitted from the IsValidSecurePaymentConfirmationRequest function,
    resulting in a double count since this function is called twice per
    PaymentRequest lifecycle.
    
    This CL, moves the histogram emission to it's own function, ensuring it's called
    once per PaymentRequest lifecycle. It also moves the tests related to
    this histogram from unit tests to browser tests.
    
    Bug: 545148961
    Change-Id: I67ebd6b0d3f18f3a9b2b64843e7b3933ccbb5ef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8292030
    Commit-Queue: Jayden Nikifork <nikifork@google.com>
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687216}

--- chrome/browser/payments/secure_payment_confirmation_browsertest.cc
+++ chrome/browser/payments/secure_payment_confirmation_browsertest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/payments/core/features.h"
 #include "components/payments/core/native_error_strings.h"
 #include "components/payments/core/secure_payment_confirmation_credential.h"
+#include "components/payments/core/secure_payment_confirmation_metrics.h"
 #include "components/webdata/common/web_data_results.h"
 #include "components/webdata_services/web_data_service_wrapper_factory.h"
 #include "content/public/common/content_features.h"
@@ -606,6 +607,7 @@ class SecurePaymentConfirmationLocaleFeatureEnabledTest
 
 IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationLocaleFeatureEnabledTest,
                        Show_LocaleMatches) {
+  base::HistogramTester histogram_tester;
   ResetEventWaiterForSingleEvent(TestEvent::kUIDisplayed);
   ExecuteScriptAsync(GetActiveWebContents(),
                      "getSecurePaymentConfirmationStatusWithLocale(['en-US'])");
@@ -616,14 +618,42 @@ IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationLocaleFeatureEnabledTest,
   EXPECT_EQ(
       GetCancelErrorMessage(),
       content::EvalJs(GetActiveWebContents(), "getOutstandingStatusPromise()"));
+
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.SecurePaymentConfirmation.LocaleOutcome",
+      SecurePaymentConfirmationLocaleOutcome::kMatch, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationLocaleFeatureEnabledTest,
                        Show_LocaleMismatch) {
+  base::HistogramTester histogram_tester;
   EXPECT_EQ(
       GetLocaleMismatchErrorMessage(),
       content::EvalJs(GetActiveWebContents(),
                       "getSecurePaymentConfirmationStatusWithLocale(['fr'])"));
+
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.SecurePaymentConfirmation.LocaleOutcome",
+      SecurePaymentConfirmationLocaleOutcome::kNoMatch, 1);
+}
+
+IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationLocaleFeatureEnabledTest,
+                       Show_LocaleNotProvided) {
+  base::HistogramTester histogram_tester;
+  ResetEventWaiterForSingleEvent(TestEvent::kUIDisplayed);
+  ExecuteScriptAsync(GetActiveWebContents(),
+                     "getSecurePaymentConfirmationStatusWithLocale()");
+  WaitForObservedEvent();
+
+  EXPECT_TRUE(database_write_responded_);
+  test_controller()->CloseDialog();
+  EXPECT_EQ(
+      GetCancelErrorMessage(),
+      content::EvalJs(GetActiveWebContents(), "getOutstandingStatusPromise()"));
+
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.SecurePaymentConfirmation.LocaleOutcome",
+      SecurePaymentConfirmationLocaleOutcome::kLocaleNotProvided, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(SecurePaymentConfirmationLocaleFeatureEnabledTest,
