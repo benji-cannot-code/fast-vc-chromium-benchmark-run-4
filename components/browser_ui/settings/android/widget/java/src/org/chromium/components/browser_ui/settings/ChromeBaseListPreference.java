23b9ed8412f9fa1c18776d89b3be18ca3bd5dc7c
James Cook <jamescook@chromium.org>
2026-08-27 16:52:31 -0700

    settings: Introduce ChromeBaseListPreference to fix indentation
    AndroidX ListPreference extends DialogPreference (which extends
    Preference) and defaults iconSpaceReserved to true. In SettingsInTab,
    this causes ListPreference items without an icon (such as the Tracing
    mode setting in TracingSettings) to reserve empty icon space on the
    left, misaligning them with other preferences.
    
    This change:
    1. Introduces ChromeBaseListPreference, which defaults
       iconSpaceReserved to false.
    2. Replaces ListPreference with ChromeBaseListPreference in
       tracing_preferences.xml.
    3. Adds unit test coverage in ChromeBasePreferenceTest.
    
    Bug: 553503998, 521895796
    Test: ChromeBasePreferenceTest
    Test: TracingSettingsTest
    Change-Id: I1803d37fa9041dc0ba20134f2db12efc820f50cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8308632
    Reviewed-by: Moe Adel <adelm@google.com>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687667}

---
+++ components/browser_ui/settings/android/widget/java/src/org/chromium/components/browser_ui/settings/ChromeBaseListPreference.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.settings;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.ListPreference;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+
+/** A {@link ListPreference} that collapses unused icon space by default. */
+@NullMarked
+public class ChromeBaseListPreference extends ListPreference {
+    /** Constructor for use in Java. */
+    public ChromeBaseListPreference(Context context) {
+        this(context, null);
+    }
+
+    /** Constructor for inflating from XML. */
+    public ChromeBaseListPreference(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        SettingsUtils.initializePreferenceDefaults(context, attrs, this);
+    }
+}
