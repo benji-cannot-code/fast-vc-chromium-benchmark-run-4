1b0644dc7de2ed3812ddbf1a529ca409f12a9a7a
Alison Gale <agale@chromium.org>
2026-08-27 08:30:12 -0700

    Clean up unlaunched everything menu experiment
    We explored this feature to remove the tab groups button from the
    vertical tab strip in case we were going to replace it with another
    feature but ended up deciding we could keep it because it was used a
    lot.
    
    Change-Id: I5c5fe761ce7f8f0551952f6a5190b876ed2ca8d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8291502
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687258}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1005,6 +1005,12 @@ constexpr char kTrackingProtection3pcdEnabled[] =
 constexpr char kBlockAll3pcToggleEnabled[] =
     "tracking_protection.block_all_3pc_toggle_enabled";
 
+#if !BUILDFLAG(IS_ANDROID)
+// Deprecated 08/2026.
+constexpr char kEverythingMenuPinnedToTabstripMigrationComplete[] =
+    "everything_menu.pinned_to_tabstrip_migration_complete";
+#endif  // !BUILDFLAG(IS_ANDROID)
+
 #if BUILDFLAG(IS_CHROMEOS)
 // Deprecated 07/2026.
 inline constexpr char kPluginVmAllowed[] = "plugin_vm.allowed";
@@ -1425,6 +1431,12 @@ void RegisterProfilePrefsForMigration(
   // Deprecated 08/2026.
   registry->RegisterStringPref(kUkmLoggingUserSecret, std::string());
   registry->RegisterTimePref(kUkmLoggingUserSecretCreationTime, base::Time());
+
+#if !BUILDFLAG(IS_ANDROID)
+  // Deprecated 08/2026.
+  registry->RegisterBooleanPref(
+      kEverythingMenuPinnedToTabstripMigrationComplete, false);
+#endif  // !BUILDFLAG(IS_ANDROID)
 }
 
 }  // namespace
@@ -2702,11 +2714,6 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
   profile_prefs->ClearPref(kMV2DeprecationDisabledAcknowledgedGlobally);
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 
-#if !BUILDFLAG(IS_ANDROID)
-  // Added 07/2026.
-  tabs::MigrateEverythingMenuPinnedToTabstripPref(profile_prefs);
-#endif
-
   // Added 07/2026.
   profile_prefs->ClearPref(kObsoleteManagementProfileLastLogTime);
 
@@ -2772,6 +2779,11 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
   // Added 08/2026.
   profile_prefs->ClearPref(kUkmLoggingUserSecret);
   profile_prefs->ClearPref(kUkmLoggingUserSecretCreationTime);
+
+#if !BUILDFLAG(IS_ANDROID)
+  // Added 08/2026.
+  profile_prefs->ClearPref(kEverythingMenuPinnedToTabstripMigrationComplete);
+#endif  // !BUILDFLAG(IS_ANDROID)
 
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_PROFILE_PREFS
--- chrome/browser/ui/tabs/features.h
+++ chrome/browser/ui/tabs/features.h
@@ -90,8 +90,6 @@ BASE_DECLARE_FEATURE(kBackToOpener);
 
 BASE_DECLARE_FEATURE(kTabStripUnification);
 
-BASE_DECLARE_FEATURE(kMigrateEverythingMenuPinnedToTabstrip);
-
 bool IsSplitViewHorizontalIndirectAccessEnabled();
 
 bool IsVerticalTabsFeatureEnabled();
