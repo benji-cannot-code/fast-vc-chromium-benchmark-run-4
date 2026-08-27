88275de1c9ec705e24800c8081ff9f311709892c
Charles Meng <charlesmeng@chromium.org>
2026-08-27 12:08:06 -0700

    [TopBottomSplits] Add feature param for tab direct access
    Adds the feature param kSplitViewHorizontalDirectTabAccess which when
    enabled, shows the submenu for picking split orientation for the "Add
    Tab to New Split View" tab context menu item, but not for the "Open Link
    in Split View" link context menu item.
    
    Fixed: 552492548
    Change-Id: I62e548753c462b84a749438f2df3c407f65e8e98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8304434
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687458}

--- chrome/browser/ui/tabs/features.h
+++ chrome/browser/ui/tabs/features.h
@@ -22,9 +22,12 @@ BASE_DECLARE_FEATURE(kSessionRestoreShowThrobberOnVisible);
 
 // Allows split tabs to be arranged top/bottom.
 BASE_DECLARE_FEATURE(kSplitViewHorizontal);
-// When enabled, creating a new split tab through the context menu will open a
-// submenu to select the split's orientation.
+// When enabled, creating a new split tab through the tab and link context menus
+// will open a submenu to select the split's orientation.
 BASE_DECLARE_FEATURE_PARAM(bool, kSplitViewHorizontalDirectAccess);
+// When enabled, creating a new split tab through the tab context menu will open
+// a submenu to select the split's orientation.
+BASE_DECLARE_FEATURE_PARAM(bool, kSplitViewHorizontalDirectTabAccess);
 
 // Enables CJK word boundary detection in Tab Search.
 BASE_DECLARE_FEATURE(kTabSearchCjkWordBoundary);
@@ -80,6 +83,8 @@ BASE_DECLARE_FEATURE(kBackToOpener);
 BASE_DECLARE_FEATURE(kTabStripUnification);
 
 bool IsSplitViewHorizontalIndirectAccessEnabled();
+
+bool IsSplitViewHorizontalDirectAccessEnabledForTab();
 
 bool IsVerticalTabsExpandOnHoverFeatureEnabled();
 
