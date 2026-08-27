d86582e1857360fec394676fd1cd72004dbfb415
David Bienvenu <davidbienvenu@chromium.org>
2026-08-27 09:53:38 -0700

    Always allow writing to special dirs if under temp dir
    BlockTestsWritingToSpecialDirs is used to block tests from writing
    files to special directories and changing the local machine state.
    But, it inadvertently blocked writing to temp dirs. E.g., writes
    to C:\src are blocked, but DIR_TEMP might be C:\src\temp. This CL
    makes it so writing to temp dir and its descendents is always allowed
    because by definition, that won't change the local machine state.
    
    Bug: 40066315
    Change-Id: If98ab9a02bbd4cae60f2dde7a6b2a0718129d607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8299932
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687335}

--- base/files/block_tests_writing_to_special_dirs.cc
+++ base/files/block_tests_writing_to_special_dirs.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/base_paths.h"
 #include "base/files/file_path.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
@@ -25,6 +26,14 @@ BlockTestsWritingToSpecialDirs::Get() {
 bool BlockTestsWritingToSpecialDirs::CanWriteToPath(const FilePath& path) {
   auto& dir_blocker = Get();
   if (!dir_blocker.has_value()) {
+    return true;
+  }
+  // Allow writing to temporary directories (or sub-directories of DIR_TEMP),
+  // even if DIR_TEMP resides under a blocked directory (e.g.
+  // DIR_SRC_TEST_DATA_ROOT).
+  FilePath temp_path;
+  if (PathService::Get(DIR_TEMP, &temp_path) &&
+      (temp_path == path || temp_path.IsParent(path))) {
     return true;
   }
   if (!dir_blocker->blocked_dirs_.empty()) {
