87e60d7955baf21c699e0bc8d63f11ff3b808345
Amya Singhal <amyasinghal@google.com>
2026-08-27 06:54:41 -0700

    [Dictation] add new discovery badge
    Screenshot: https://drive.google.com/file/d/1aoX4Ef6JTzyRdWXRP88Dvh3jN6vuyD_d/view?usp=sharing
    Bug: b:539999694 b:548754395
    Change-Id: If07f7f005a4549b7617c9f28f1598931015d110d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8298118
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Amya Singhal <amyasinghal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687172}

--- chrome/browser/renderer_context_menu/dictation_menu_observer.cc
+++ chrome/browser/renderer_context_menu/dictation_menu_observer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/dictation/dictation_keyed_service.h"
 #include "chrome/browser/dictation/features.h"
+#include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/renderer_context_menu/render_view_context_menu_proxy.h"
@@ -58,6 +59,9 @@ void DictationMenuObserver::ExecuteCommand(int command_id) {
   if (!rfh) {
     return;
   }
+
+  UserEducationService::MaybeNotifyNewBadgeFeatureUsed(
+      proxy_->GetBrowserContext(), kDictation);
 
   DictationKeyedService* service = GetDictationService();
   if (service) {
--- chrome/browser/ui/views/user_education/browser_user_education_service.cc
+++ chrome/browser/ui/views/user_education/browser_user_education_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/contextual_tasks/contextual_tasks_ui_service.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_ui_service_factory.h"
 #include "chrome/browser/devtools/features.h"
+#include "chrome/browser/dictation/features.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/public/features.h"
@@ -2608,6 +2609,11 @@ void MaybeRegisterChromeNewBadges(user_education::NewBadgeRegistry& registry) {
       user_education::Metadata(
           153, "kristislee@google.com",
           "Shown on the Line Focus menu item in Reading Mode settings menu.")));
+
+  registry.RegisterFeature(user_education::NewBadgeSpecification(
+      dictation::kDictation,
+      user_education::Metadata(153, "amyasinghal@google.com",
+                               "Shown on the Dictation context menu item.")));
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   registry.RegisterFeature(user_education::NewBadgeSpecification(
