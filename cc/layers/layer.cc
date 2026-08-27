174e01ae9d1f787aca6e916b6aa72193446e757d
Stefan Zager <szager@chromium.org>
2026-08-26 19:15:26 -0700

    Prevent overflow of cc::Layer ID's.
    Overflowing a 32-bit integer is pathological so just crash.
    
    Fixed: 523554372
    Change-Id: Iadc3ce18060cce112f161679f9491d1cc4b2f64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8301512
    Auto-Submit: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686872}

--- cc/layers/layer.cc
+++ cc/layers/layer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cmath>
+#include <limits>
 #include <string>
 #include <unordered_set>
 #include <utility>
@@ -97,7 +98,9 @@ Layer::LayerTreeInputs::~LayerTreeInputs() = default;
 
 int Layer::GetNextLayerId() {
   // Layer IDs start from 1.
-  return g_next_layer_id.GetNext() + 1;
+  int prev = g_next_layer_id.GetNext();
+  CHECK_LT(prev, std::numeric_limits<int>::max());
+  return prev + 1;
 }
 
 scoped_refptr<Layer> Layer::Create() {
