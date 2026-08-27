732f361a3c0dc8591c395a3465d9aa068af8560a
Miguel Ruiz <migueloruiz@google.com>
2026-08-27 15:13:34 -0700

    Add flag to disable navigation context in AI Overlay
    Add kAiOverlayDisableNavigationContext feature flag to prevent
    the AI Overlay from sharing navigation history as conversation context.
    Plumb the flag state to the WebUI data source.
    
    Bug: none
    Change-Id: I5e35f59080dec7dd4debbe20c17d55d334a44a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297544
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Miguel Ruiz <migueloruiz@google.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687593}

--- chrome/browser/ui/webui/ai_overlay_dialog/ai_overlay_dialog_untrusted_ui.cc
+++ chrome/browser/ui/webui/ai_overlay_dialog/ai_overlay_dialog_untrusted_ui.cc
@@ -121,6 +121,9 @@ AiOverlayDialogUntrustedUI::AiOverlayDialogUntrustedUI(content::WebUI* web_ui)
   html_source->AddBoolean("enableDebugLogs", enable_debug_logs);
 
   html_source->AddString("apiKey", features::kAiOverlayDialogApiKey.Get());
+  html_source->AddBoolean("aiOverlayDisableNavigationContext",
+                          base::FeatureList::IsEnabled(
+                              features::kAiOverlayDisableNavigationContext));
 }
 
 AiOverlayDialogUntrustedUI::~AiOverlayDialogUntrustedUI() = default;
