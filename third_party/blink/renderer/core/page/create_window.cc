783f275ea88b3135a1a39baec069166c34fc6b6a
Nan Lin <linnan@chromium.org>
2026-08-27 16:00:07 -0700

    Restore attributionsrc parsing in window.open to stub API
    Bug: 553155942
    Change-Id: I5c71cb368eb99135f871aecaa4ee49587321e758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8302975
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687635}

--- third_party/blink/renderer/core/page/create_window.cc
+++ third_party/blink/renderer/core/page/create_window.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/loader/frame_load_request.h"
@@ -55,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/scheme_registry.h"
@@ -164,9 +166,14 @@ WebWindowFeatures GetWindowFeaturesFromString(const String& feature_string,
           StringToInt(value_string, NumberParsingOptions::Loose()).value_or(0);
     }
 
+    const bool attribution_reporting_enabled =
+        dom_window &&
+        RuntimeEnabledFeatures::AttributionReportingEnabled(dom_window);
+
     if (!ui_features_were_disabled && key_string != "noopener" &&
         (!explicit_opener_enabled || key_string != "opener") &&
-        key_string != "noreferrer") {
+        key_string != "noreferrer" &&
+        (!attribution_reporting_enabled || key_string != "attributionsrc")) {
       ui_features_were_disabled = true;
       menu_bar = false;
       status_bar = false;
@@ -209,6 +216,10 @@ WebWindowFeatures GetWindowFeaturesFromString(const String& feature_string,
       window_features.background = true;
     } else if (key_string == "persistent") {
       window_features.persistent = true;
+    } else if (attribution_reporting_enabled &&
+               key_string == "attributionsrc") {
+      UseCounter::Count(dom_window,
+                        mojom::blink::WebFeature::kAttributionReportingAPIAll);
     }
   }
 
