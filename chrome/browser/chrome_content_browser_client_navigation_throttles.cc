400b400c655473e70b3568addfc9f86975f47a39
HuanPo Lin <robertlin@chromium.org>
2026-08-27 01:35:07 -0700

    Support DSEv1 Prefetch for PreloadServingMetrics.{Initiator}.{All/Srp}
    Currently, DSEv1 Prefetch is classified as "Other" in
    PreloadServingMetrics.{Initiator}.{All/Srp} metrics. To properly record
    the metrics, this CL introduces a field
    `is_served_by_legacy_search_prefetch_` in NavigationHandleUserData for
    PreloadServingMetricsPageLoadMetricsObserver. Design Doc
    https://docs.google.com/document/d/1m5Icw3une4T5jb3FetzdODYKBjIWpMaz-IfLlJlN9_g/edit?usp=sharing
    
    Unresolved-Comment-Reason:
    HTTPCacheSearchPreloadUnifiedBrowserTest.BackwardHitHttpCache
    prerender metrics needs more investigation.
    
    Bug: 517725655
    Change-Id: I8191a9e7e56e8fbb88ef040f59f84a1939fa55fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254478
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687041}

--- chrome/browser/chrome_content_browser_client_navigation_throttles.cc
+++ chrome/browser/chrome_content_browser_client_navigation_throttles.cc
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/chrome_policy_blocklist_service_factory.h"
 #include "chrome/browser/policy/policy_util.h"
 #include "chrome/browser/preloading/prefetch/no_state_prefetch/chrome_no_state_prefetch_contents_delegate.h"
+#include "chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_navigation_throttle.h"
 #include "chrome/browser/preloading/prerender/dse_prewarm_navigation_throttle.h"
+#include "chrome/browser/preloading/search_preload/search_preload_features.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/pwc/pwc_navigation_throttle.h"
@@ -307,6 +309,9 @@ void CreateAndAddChromeThrottlesForNavigation(
   }
 
   DSEPrewarmNavigationThrottle::MaybeCreateAndAdd(registry);
+  if (!features::IsDsePreload2Enabled()) {
+    SearchPrefetchNavigationThrottle::MaybeCreateAndAdd(registry);
+  }
 
 #if BUILDFLAG(IS_ANDROID)
   // TODO(davidben): This is insufficient to integrate with prerender properly.
