87ed9181f0e3d059607c2a5dcafb8a56794db80f
Eriko Kurimoto <elkurin@chromium.org>
2026-08-27 02:45:34 -0700

    Revert "Add GlicBrowserInteractiveTest, and migrate GlicSelectionObserverInteractiveUiTest"
    This reverts commit 2815e3433d73135fd9bf8fd801b56dd5f1291085.
    
    Reason for revert: Culprit of GlicSelectionObserverInteractiveUiTest.SelectionUpdatesContext
    breakage
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/mac-rel-ready/46832/overview
    
    Original change's description:
    > Add GlicBrowserInteractiveTest, and migrate GlicSelectionObserverInteractiveUiTest
    >
    > Added GlicBrowserInteractiveTest as a start fixture to more easily
    > migrate tests from InteractiveGlicTestMixin. Arguably,
    > GlicSelectionObserverInteractiveUiTest should just be migrated to
    > GlicApiBrowserTest, but this requires more changes. I'm hoping we can
    > migrate all the GlicBrowserInteractiveTest tests more easily with a
    > fixture that supports RunTestSequence, even if it's not strictly
    > required.
    >
    > Change-Id: Ib6a647efa8a45e85d6c64e5b09f522d16a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8296842
    > Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    > Commit-Queue: Dan H <harringtond@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1686914}
    
    Change-Id: I3c16707e93b3c6d69abd043dd82240351224aa94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8304352
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1687072}

--- chrome/browser/glic/glic_selection_observer_interactive_uitest.cc
+++ chrome/browser/glic/glic_selection_observer_interactive_uitest.cc
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/glic/glic_selection_observer.h"
+
 #include "chrome/browser/glic/public/features.h"
-#include "chrome/browser/glic/test_support/glic_browser_interactive_test.h"
+#include "chrome/browser/glic/test_support/interactive_glic_test.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
@@ -17,15 +19,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace glic {
 namespace {
 
-// TODO(b/537847327): This test should be migrated to use GlicApiBrowserTest,
-// and not the test client.
+// TODO(crbug.com/537847327): Migrate this test suite to GlicBrowserTest.
 class GlicSelectionObserverInteractiveUiTest
-    : public GlicBrowserInteractiveTest {
+    : public test::InteractiveGlicTest {
  public:
   GlicSelectionObserverInteractiveUiTest() {
-    glic_test_environment().SetGlicPagePath("/glic/test_client/index.html");
     scoped_feature_list_.InitWithFeaturesAndParameters(
-        {{features::kGlicSelectionPrompt, {{"updates_only", "true"}}}}, {});
+        {{features::kGlic, {}},
+         {features::kGlicSelectionPrompt, {{"updates_only", "true"}}}},
+        {});
   }
 
  protected:
@@ -42,7 +44,7 @@ class GlicSelectionObserverInteractiveUiTest
   auto SelectAll() {
     return Do([this] {
       content::WebContents* web_contents =
-          GetTabListInterface()->GetActiveTab()->GetContents();
+          browser()->tab_strip_model()->GetActiveWebContents();
       web_contents->SelectAll();
     });
   }
@@ -60,23 +62,12 @@ IN_PROC_BROWSER_TEST_F(GlicSelectionObserverInteractiveUiTest,
       "body",
   };
 
-  RunTestSequence(InstrumentTab(kActiveTab),
-                  NavigateWebContents(kActiveTab, url));
-
-  ASSERT_OK_AND_ASSIGN(auto* instance, OpenGlicForActiveTab());
-
-  RunTestSequence(WaitForWebContentsPainted(kActiveTab),
+  RunTestSequence(InstrumentTab(kActiveTab, std::nullopt, browser(), true),
+                  NavigateWebContents(kActiveTab, url), OpenGlic(),
+                  WaitForWebContentsPainted(kActiveTab),
                   MoveMouseTo(kActiveTab, kPathToBody), ClickMouse(),
-                  ClickMouse(), SelectAll());
-
-  EXPECT_TRUE(RunUntil(
-      [&]() {
-        auto* guest_frame = instance->host().GetGuestMainFrame();
-        return guest_frame &&
-               content::EvalJs(guest_frame, base::StrCat({"(", kCheckContextJs,
-                                                          ")()"})) == true;
-      },
-      "Timeout waiting for selection context in Glic"));
+                  ClickMouse(), SelectAll(),
+                  WaitForJsResult(kGlicContentsElementId, kCheckContextJs));
 }
 
 }  // namespace
