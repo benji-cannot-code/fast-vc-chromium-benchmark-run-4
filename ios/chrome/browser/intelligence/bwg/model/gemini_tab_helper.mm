366075be5198e6ac2a420ecfa65fbff1f1f6c16a
Robbie Gibson <rkgibson@google.com>
2026-08-27 08:21:26 -0700

    [iOS] Hook Gemini IPH and completion to Level Up task
    This CL connects the "Use Gemini in Chrome" Level Up task to the Gemini
    / Page Action Menu In-Product Help (IPH) walkthrough and wires up task
    completion.
    
    There are also a few changes made here to support iPad/fix bugs.
    
    1. crrev.com/c/7997316 had a bug where when NextIA was disabled, no
    Location Bar layout guides were registered, as the `_active` flag is
    only used when NextIA is enabled.
    
    2. The kAppBarAssistantButtonGuide is not in a useful place on iPad, as
    the app bar is created, but isn't on screen in a useful place. This CL
    fixes this by assigning kAppBarAssistantButtonGuide to the toolbar
    assistant button on iPad.
    
    Bug: 538180585
    Change-Id: I114fa4855e880295637bc1825994437d1ae8180d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178144
    Auto-Submit: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687252}

--- ios/chrome/browser/intelligence/bwg/model/gemini_tab_helper.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_tab_helper.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/functional/callback_helpers.h"
 #import "base/memory/weak_ptr.h"
 #import "base/metrics/histogram_functions.h"
+#import "base/metrics/user_metrics.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/string_util.h"
 #import "base/strings/sys_string_conversions.h"
@@ -491,6 +492,9 @@ void GeminiTabHelper::DidFinishNavigation(
   }
 
   const GURL& current_url = navigation_context->GetUrl().GetWithoutRef();
+  if (IsAimURL(current_url)) {
+    base::RecordAction(base::UserMetricsAction("MobileGeminiPromptSent"));
+  }
   if (previous_main_frame_url_ == current_url) {
     return;
   }
