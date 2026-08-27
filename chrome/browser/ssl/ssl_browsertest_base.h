b17d00639d7d4918d18e3105c8eb20ba2896e1ae
Matt Mueller <mattm@chromium.org>
2026-08-27 11:00:37 -0700

    Remove unused SSLConfigClient in SSLUITestBase
    Change-Id: I1c32e912485e00cdd599d23e664d16c783a2c013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8292068
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687396}

--- chrome/browser/ssl/ssl_browsertest_base.h
+++ chrome/browser/ssl/ssl_browsertest_base.h
@@ -12,10 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/security_interstitials/core/controller_client.h"
-#include "mojo/public/cpp/bindings/receiver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/network/public/mojom/network_context.mojom.h"
-#include "services/network/public/mojom/ssl_config.mojom.h"
 
 namespace content {
 class WebContents;
@@ -28,8 +26,7 @@ class SecurityInterstitialControllerClient;
 
 class SSLBlockingPage;
 
-class SSLUITestBase : public InProcessBrowserTest,
-                      public network::mojom::SSLConfigClient {
+class SSLUITestBase : public InProcessBrowserTest {
  public:
   SSLUITestBase();
   SSLUITestBase(const SSLUITestBase&) = delete;
@@ -41,10 +38,6 @@ class SSLUITestBase : public InProcessBrowserTest,
   void TearDown() override;
   void SetUpCommandLine(base::CommandLine* command_line) override;
   void SetUpOnMainThread() override;
-  void TearDownOnMainThread() override;
-
-  // network::mojom::SSLConfigClient:
-  void OnSSLConfigUpdated(network::mojom::SSLConfigPtr config) override;
 
   static std::string GetFilePathWithHostAndPortReplacement(
       const std::string& original_file_path,
@@ -93,10 +86,6 @@ class SSLUITestBase : public InProcessBrowserTest,
   net::EmbeddedTestServer wss_server_mismatched_;
 
   testing::NiceMock<policy::MockConfigurationPolicyProvider> policy_provider_;
-
-  network::mojom::SSLConfig last_ssl_config_;
-  mojo::Receiver<network::mojom::SSLConfigClient> receiver_{this};
-
  private:
   network::mojom::NetworkContextParamsPtr CreateDefaultNetworkContextParams();
 };
