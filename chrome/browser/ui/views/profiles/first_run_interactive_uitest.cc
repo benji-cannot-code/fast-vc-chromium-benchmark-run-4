505ddbb8bf7cb4cc1c7a19b77e974c1fd1ab7740
Ernest Nguyen Hung <ernn@google.com>
2026-08-27 06:00:52 -0700

    [FRE] Fix interactive tests when `PreFirstRunDesktopRefresh` enabled
    This fixes tests `FirstRunInteractiveUiBaseTest` used by all tests in
    `first_run_interactive_uitest.cc` to disable `PreFirstRunDesktopRefresh`
    unless a corresponding variant `FirstRunVersion::PreFirstRunRefreshed`
    is enabled.
    
    It's needed to ensure proper tests isolation, and to enable the feature
    in the field trial testing config. Discovered in crrev.com/c/8304672.
    
    Bug: 469391064
    Change-Id: I30e87e6d4fcd403f1305ebc40fab595b243ebf2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8305932
    Reviewed-by: Sasha Kunitskaya <saskekun@google.com>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687135}

--- chrome/browser/ui/views/profiles/first_run_interactive_uitest.cc
+++ chrome/browser/ui/views/profiles/first_run_interactive_uitest.cc
@@ -361,6 +361,7 @@ class FirstRunInteractiveUiBaseTest
             [&](FirstRunVersion::Legacy) {
               disabled_features.push_back(switches::kFirstRunDesktopRefresh);
               disabled_features.push_back(switches::kFirstRunDesktopRevamp);
+              disabled_features.push_back(switches::kPreFirstRunDesktopRefresh);
             },
             [&](FirstRunVersion::Refreshed refreshed) {
               enabled_features.push_back(
@@ -370,7 +371,9 @@ class FirstRunInteractiveUiBaseTest
                          refreshed.variant)}}});
               enabled_features.push_back(
                   {switches::kFirstRunDesktopChoiceScreenRefresh, {}});
+
               disabled_features.push_back(switches::kFirstRunDesktopRevamp);
+              disabled_features.push_back(switches::kPreFirstRunDesktopRefresh);
             },
             [&](FirstRunVersion::Revamped revamped) {
               enabled_features.push_back(
@@ -390,6 +393,8 @@ class FirstRunInteractiveUiBaseTest
                 disabled_features.push_back(
                     switches::kFirstRunDesktopRevampSound);
               }
+
+              disabled_features.push_back(switches::kPreFirstRunDesktopRefresh);
             },
             [&](FirstRunVersion::PreFirstRunRefreshed) {
               enabled_features.push_back(
