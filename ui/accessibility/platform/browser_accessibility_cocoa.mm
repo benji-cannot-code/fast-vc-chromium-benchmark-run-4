3226cd8e2373907cdf83e53ffff811b113d1f9a2
Jack Thiesen <jthiesen@chromium.org>
2026-08-26 17:01:52 -0700

    Revert "[views-ax] Return success after performing AXPress"
    This reverts commit e409b46bfe90240527d3cbdf82554554f9a6b727.
    
    Reason for revert: Cannot upload entirely unrelated changes after this change.
    
    Failure Link: ** Presubmit ERRORS: 1 **
    [IfThisThenAnalyzer/IfThisThenAnalyzer] ui/accessibility/platform/browser_accessibility_cocoa.mm:3344: FAIL: Changes in the preceding block may need to be reflected in these files: /ui/accessibility/platform/browser_accessibility_cocoa.mm If this does not apply, add 'NO_IFTTT=some reason...' to your commit message
    
    Original change's description:
    > [views-ax] Return success after performing AXPress
    >
    > When the user performs an accessible action AXPress on a non-checkable
    > element (not a checkbox), it returned a failure despite having worked.
    > This is because we're not truly returning the state of whether it worked
    > or not (we can't since the action is serialized asynchronously and the
    > AX API is synchronous). Instead, we do the second best thing and align
    > the return condition to the condition used in the action handler.
    >
    > However, the shortcut here was wrong. If not checkable, we should return
    > a success because it should successfully have been handled.
    >
    > Bug: 40672441
    > Change-Id: I4ad0e1fd2f1cd45a5a7ff8c3a3d38d34d08e1913
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8290029
    > Reviewed-by: Elly <ellyjones@chromium.org>
    > Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1686772}
    
    Bug: 40672441
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I59ca17df708b73ae3c611ba73269e63bc3678b16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297724
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Owners-Override: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686787}

--- ui/accessibility/platform/browser_accessibility_cocoa.mm
+++ ui/accessibility/platform/browser_accessibility_cocoa.mm
@@ -3286,7 +3286,7 @@ bool IsAXCustomActionNamesForTestingProjectionEnabled() {
       data.SetCheckedState(newCheckedState);
     }
     node->SetData(data);  // Set the data back in the node.
-    // LINT.ThenChange(:accessibilityPerformPress)
+    // LINT.ThenChange(accessibilityPerformPress)
   } else if ([action isEqualToString:NSAccessibilityShowMenuAction]) {
     manager->ShowContextMenu(*actionTarget);
   } else if ([action isEqualToString:NSAccessibilityScrollToVisibleAction]) {
@@ -3316,11 +3316,9 @@ bool IsAXCustomActionNamesForTestingProjectionEnabled() {
   BrowserAccessibilityManager* manager = actionTarget->manager();
   manager->DoDefaultAction(*actionTarget);
   if (actionTarget->GetData().GetRestriction() !=
-      ax::mojom::Restriction::kNone) {
+          ax::mojom::Restriction::kNone ||
+      ![self isCheckable]) {
     return NO;
-  }
-  if (![self isCheckable]) {
-    return YES;
   }
 
   // Hack: preemptively set the checked state to what it should become,
@@ -3342,7 +3340,6 @@ bool IsAXCustomActionNamesForTestingProjectionEnabled() {
   node->SetData(data);  // Set the data back in the node.
   return YES;
 }
-// LINT.ThenChange(:NSAccessibilityPressAction)
 
 - (BOOL)accessibilityPerformShowMenu {
   if (![self instanceActive]) {
@@ -3365,6 +3362,7 @@ bool IsAXCustomActionNamesForTestingProjectionEnabled() {
 
   return NSAccessibilityActionDescription(action);
 }
+// LINT.ThenChange(NSAccessibilityPressAction)
 
 - (NSArray<NSAccessibilityCustomAction*>*)accessibilityCustomActions {
   if (![self instanceActive]) {
