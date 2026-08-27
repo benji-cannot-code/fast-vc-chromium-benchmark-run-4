0e8c50bb088115dff8ccff1aefffd71e2017c2e2
Fiaz Muhammad <mfiaz@google.com>
2026-08-26 17:05:49 -0700

    [Actor] Replace getLiveTabs with getAllTabIds
    - Replaces getLiveTabIds with getAllTabIds in BackgroundTabPool using
            ArraySet.
    - Adds getAllTabIds to TabCache using ArraySet.
    
    Bug: 546110665
    Change-Id: Ifb6ec6a6a6274fe5df3aa8a782e810fd9fd02a19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282302
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686788}

--- chrome/android/java/src/org/chromium/chrome/browser/actor/BackgroundTabPool.java
+++ chrome/android/java/src/org/chromium/chrome/browser/actor/BackgroundTabPool.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.actor;
 import static org.chromium.base.ThreadUtils.assertOnUiThread;
 
 import android.util.ArrayMap;
+import android.util.ArraySet;
 
 import org.chromium.base.Log;
 import org.chromium.base.lifetime.Destroyable;
@@ -30,7 +31,9 @@ import org.chromium.chrome.browser.tab.TabStateAttributesRegistry;
 import org.chromium.chrome.browser.tab.TabStateExtractor;
 import org.chromium.chrome.browser.tabmodel.TabPersistentStoreImpl;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Profile-scoped pool managing background actor tabs.
@@ -122,13 +125,17 @@ public class BackgroundTabPool
     }
 
     /**
-     * Returns an unmodifiable list of all active live tab IDs in memory.
+     * Returns a set of all tab IDs managed by this pool (both in-memory live tabs and cached tabs).
      *
-     * @return A list of live {@link TabId} integers.
+     * @return A {@link Set} of all {@link TabId} integers in the pool.
      */
-    public List<@TabId Integer> getLiveTabIds() {
+    public Set<@TabId Integer> getAllTabIds() {
         checkNotDestroyed();
-        return List.copyOf(mLiveEntries.keySet());
+        Set<@TabId Integer> cachedIds = mTabCache.getAllTabIds();
+        ArraySet<@TabId Integer> allIds = new ArraySet<>(cachedIds.size() + mLiveEntries.size());
+        allIds.addAll(cachedIds);
+        allIds.addAll(mLiveEntries.keySet());
+        return Collections.unmodifiableSet(allIds);
     }
 
     /**
