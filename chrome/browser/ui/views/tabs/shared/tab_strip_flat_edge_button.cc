2265338ab9c9fdd88a7a291ae4656d9424b45147
David Pennington <dpenning@google.com>
2026-08-26 23:51:39 -0700

    Revert back to original design for Glass color calculations for NTB and other Tabstrip UI
    The color mixer based calculations for glass cuased issues with
    the vertical tabstrip, and so to fix this we are going to revert a
    majority of that CL. Only the changes to the selected color and the
    glass opacity calculations will be left.
    
    Bug: 549790171
    Change-Id: Ia9ccb5d6f16ff929ea908952e77df8427c193b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8300074
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686986}

--- chrome/browser/ui/views/tabs/shared/tab_strip_flat_edge_button.cc
+++ chrome/browser/ui/views/tabs/shared/tab_strip_flat_edge_button.cc
@@ -175,6 +175,9 @@ void TabStripFlatEdgeButton::NotifyWillInvokeAction() {
 }
 
 void TabStripFlatEdgeButton::OnPaintBackground(gfx::Canvas* canvas) {
+  if (paint_transparent_for_glass_ && features::IsGlassFrameEnabled()) {
+    return;
+  }
   const SkColor color = GetColorProvider()->GetColor(GetBackgroundColor());
 
   cc::PaintFlags flags;
@@ -230,6 +233,15 @@ void TabStripFlatEdgeButton::UpdateHighlightPathAndInkDrop() {
     views::InkDrop::Get(this)->GetInkDrop()->HostSizeChanged(size());
   }
 
+  SchedulePaint();
+}
+
+void TabStripFlatEdgeButton::SetPaintTransparentForGlass(
+    bool paint_transparent) {
+  if (paint_transparent_for_glass_ == paint_transparent) {
+    return;
+  }
+  paint_transparent_for_glass_ = paint_transparent;
   SchedulePaint();
 }
 
