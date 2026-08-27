4c57c63b24a4197f89b801847a5aa7ae3fad72ee
Emmanuele Bassi <ebassi@igalia.com>
2026-08-27 02:15:53 -0700

    Unregister libxslt extra extensions
    During initialization, libxslt registers various additional non-standard
    extension modules. These modules are bound to xsltDocumentElem at
    stylesheet compile time, before the per-transform configuration that
    XSLTProcessor applies.
    
    Instead of relying on libxslt implicitly calling xsltInit() for us, we
    hoist it up inside our own initialization function, and undo the
    registration of the extensions. Any additional call to xsltInit() is
    idempotent to the first one.
    
    Bug: 513049042
    Change-Id: I15bbe5042ca49d1d7612776a6300b05a15e8aeb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8034081
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687063}

--- third_party/blink/renderer/core/xml/xsl_style_sheet_libxslt.cc
+++ third_party/blink/renderer/core/xml/xsl_style_sheet_libxslt.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/xml/parser/xml_document_parser_scope.h"
 #include "third_party/blink/renderer/core/xml/parser/xml_parser_input.h"
 #include "third_party/blink/renderer/core/xml/xsl_style_sheet.h"
+#include "third_party/blink/renderer/core/xml/xslt_extensions.h"
 #include "third_party/blink/renderer/core/xml/xslt_processor.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_initiator_type_names.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
@@ -272,6 +273,8 @@ void XSLStyleSheet::LoadChildSheet(const String& href) {
 }
 
 xsltStylesheetPtr XSLStyleSheet::CompileStyleSheet() {
+  InitializeLibXSLT();
+
   // FIXME: Hook up error reporting for the stylesheet compilation process.
   if (embedded_)
     return xsltLoadStylesheetPI(GetDocument());
