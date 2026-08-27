f45969d7810a980dfb0b644427738f69f3b7c81c
Sky Malice <skym@chromium.org>
2026-08-27 06:17:18 -0700

    [Clean] Remove unused errorMsg in PlatformAPIWrapperTest
    Remove unused local variable errorMsg in
    createMockedContentCaptureSession.
    
    TAG=agy
    CONV=ee1e69f6-c2bf-407f-8a60-2113354612cd
    
    Bug: 546549170
    Change-Id: Ibc9edd1b2a6a78b7a4e14f49e298aafac5fd886b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8293884
    Reviewed-by: Sayed Elabady <elabadysayed@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687153}

--- components/content_capture/android/junit/src/org/chromium/components/content_capture/PlatformAPIWrapperTest.java
+++ components/content_capture/android/junit/src/org/chromium/components/content_capture/PlatformAPIWrapperTest.java
@@ -106,7 +106,6 @@ public class PlatformAPIWrapperTest {
             ContentCaptureSession mockedContentCaptureSession =
                     Mockito.mock(ContentCaptureSession.class);
             // Prevent the below methods being called from the class other than PlatformAPIWrapper.
-            final String errorMsg = "Shall be called from PlatformAPIWrapper.";
             doThrow(new RuntimeException("Shall be called from PlatformAPIWrapper."))
                     .when(mockedContentCaptureSession)
                     .createContentCaptureSession(ArgumentMatchers.any());
