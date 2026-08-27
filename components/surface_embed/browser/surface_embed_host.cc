bdc0ea8e0dd9d9132faa17a5b76019921ad6e3a6
Keren Zhu <kerenzhu@chromium.org>
2026-08-27 14:19:29 -0700

    surface-embed: Focus child WebContents on attach if embed is focused
    When attaching an inner WebContents to a previously focused <embed>
    element, the WebContents currently won't get page focus. This is because
    the focus only propagates when an attached <embed> gets focused.
    
    This CL fixes the issue by:
    
    1. When a child WebContents is attached, Blink passes a boolean in
       SurfaceEmbedHost::AttachConnector parameter to indicate whether the
       <embed> element is currently focused.
    2. If <embed> is focused, then focus the child WebContents by calling
       WebContents::Focus(). This gives page focus.
    
    TEST=SurfaceEmbedBrowserTest.AttachToFocusedEmbedHasPageFocus
    
    TAG=agy
    CONV=118a318e-a783-46da-b597-c6dd53edf0a8
    
    Bug: 508638062
    Change-Id: I089fb3f7f2c4cb6b74b0e0e0cec197c90cdb4531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8283559
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687548}

--- components/surface_embed/browser/surface_embed_host.cc
+++ components/surface_embed/browser/surface_embed_host.cc
@@ -112,8 +112,8 @@ void SurfaceEmbedHost::SetSurfaceEmbed(
       &SurfaceEmbedHost::OnMojoDisconnect, base::Unretained(this)));
 }
 
-void SurfaceEmbedHost::AttachConnector(
-    const base::UnguessableToken& content_id) {
+void SurfaceEmbedHost::AttachConnector(const base::UnguessableToken& content_id,
+                                       bool is_embed_element_focused) {
   // Should never call attach without having a valid SurfaceEmbed remote already
   // bound.
   CHECK(surface_embed_);
@@ -156,6 +156,16 @@ void SurfaceEmbedHost::AttachConnector(
   // pass it to the connector now.
   if (container_accessibility_node_id_ != ui::kInvalidAXNodeID) {
     ForwardParentAccessibilityInfo();
+  }
+
+  if (is_embed_element_focused) {
+    FocusChildWebContents();
+  }
+}
+
+void SurfaceEmbedHost::FocusChildWebContents() {
+  if (child_contents_ && !child_contents_->ContainsOrIsFocusedWebContents()) {
+    child_contents_->Focus();
   }
 }
 
@@ -216,9 +226,7 @@ void SurfaceEmbedHost::OnEmbedElementFocused(
     child_contents_->FocusThroughTabTraversal(/*reverse=*/true);
   }
 
-  if (!child_contents_->ContainsOrIsFocusedWebContents()) {
-    child_contents_->Focus();
-  }
+  FocusChildWebContents();
 }
 
 void SurfaceEmbedHost::OnEmbedElementThrottlingStatusChanged(
