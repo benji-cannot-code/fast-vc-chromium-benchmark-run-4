8312b1d0a93c2508276c0bea3adbd40770dfd924
Ruoxi He <ruoxih@google.com>
2026-08-26 20:09:06 -0700

    [WebUI NTP on AL] Align Most Visited Tile and button hover states with Material 3
    This CL unifies the hover state styling across all elements in the Most
    Visited Tiles (MVT) area on the New Tab Page:
    - Sets --cr-hover-background-color: transparent on #addShortcut,
      #showMore, and #showLess to prevent double-layer hover stacking from
      <cr-button>'s internal shadow DOM.
    - Aligns all MVT tile and button hover states to use the unified 8%
      On-Surface token (kColorNewTabPageAddShortcutBackgroundHovered).
    
    Test:
    Light mode: http://screenshot/3ro00etsoopv8
    Dark mode: http://screenshot/46746u5rh5gs8
    Unified MVT and Add shortcut button: http://screenshot/4f94otp8rb7bg
    
    Fixed: b:535337013
    Change-Id: I0d4ed4853e328653f95cf59a72cf9ab8733e9750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281671
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Ruoxi He <ruoxih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686921}

--- chrome/browser/ui/color/new_tab_page_color_mixer.cc
+++ chrome/browser/ui/color/new_tab_page_color_mixer.cc
@@ -453,6 +453,8 @@ void AddNewTabPageColorMixer(ui::ColorProvider* provider,
   mixer[kColorNewTabPageAddShortcutForeground] =
       ui::SelectBasedOnDarkInput(kColorNewTabPageAddShortcutBackground,
                                  SK_ColorWHITE, gfx::kGoogleGrey900);
+  mixer[kColorNewTabPageAddShortcutBackgroundHovered] = {
+      kColorNewTabPageControlBackgroundHovered};
 
   AddSearchboxColors(mixer, key);
 
