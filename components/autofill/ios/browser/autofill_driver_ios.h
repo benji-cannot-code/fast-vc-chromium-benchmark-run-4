9fd6b757475ade6f2b37b5d710735483ccacf8dd
ginnnnnnny <ginnyhuang@chromium.org>
2026-08-28 11:47:17 -0700

    [iOS][actor] Fix gaps for `AutofillManager::ReparseKnownForms`
    Implemented the two methods used by `ReparseKnownForms` on iOS.
    `TriggerFormExtractionInAllFrames` follows the Desktop implementation
    and calls the same helper method as `TriggerFormExtractionInDriverFrame`
    for each driver, but with a callback.
    
    Fixed: 543096289
    Change-Id: I4682a3f39ca84e42c9a0d224328d7e1452442eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297419
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688196}

--- components/autofill/ios/browser/autofill_driver_ios.h
+++ components/autofill/ios/browser/autofill_driver_ios.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/containers/flat_set.h"
 #import "base/containers/span.h"
+#import "base/functional/callback_helpers.h"
 #import "base/memory/raw_ptr.h"
 #import "base/memory/raw_ref.h"
 #import "base/memory/weak_ptr.h"
@@ -221,8 +222,10 @@ class AutofillDriverIOS final : public AutofillDriver,
 
   // Scans to find all eligible forms in the frame's document. If batching is
   // enabled and `immediately` is true, runs this scan and the batch
-  // immediately altogether.
-  void ScanForms(bool immediately = false);
+  // immediately altogether. `callback` is called with true if forms were
+  // extracted successfully or false otherwise upon completion.
+  void ScanForms(bool immediately = false,
+                 base::OnceCallback<void(bool)> callback = base::DoNothing());
 
   // Fetches forms filtered by `form_name` and calls `caller_completion` with
   // the form fetch results upon completion of the fetch.
