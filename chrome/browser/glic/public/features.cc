a5eabc8b50b15f88e9054d8e6860cdedd6bb2cfd
Bryant Chandler <bryantchandler@google.com>
2026-08-28 08:57:38 -0700

    [glic] Trigger prewarming on glic IPH
    Preloads the Glic WebContents when the Glic IPH promo is shown, gated
    behind the kGlicWarmOnIph feature flag.
    
    Bug: b:552085G
    Change-Id: I3941e336d57b0cd0edcc08b609ec66986a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8288085
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688062}

--- chrome/browser/glic/public/features.cc
+++ chrome/browser/glic/public/features.cc
@@ -290,4 +290,6 @@ BASE_FEATURE(kGlicActionFirstFRE,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGlicWarmOnNudge, base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kGlicWarmOnIph, base::FEATURE_DISABLED_BY_DEFAULT);
 }  // namespace features
