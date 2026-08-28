09ede064b9b5c67dc4610a65bcc3e3ca79d9e414
Torne (Richard Coles) <torne@google.com>
2026-08-28 10:40:46 -0700

    webview: move Android C compat changes to CompatQuirks.
    Move the compat changes introduced in Android C to the new central
    location in CompatQuirks now that they've been upstreamed.
    
    Change-Id: Iba22eb53c606fc106468bfd62d17c2e16a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8307277
    Commit-Queue: Richard Coles <torne@chromium.org>
    Reviewed-by: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688138}

--- android_webview/java/src/org/chromium/android_webview/CompatQuirks.java
+++ android_webview/java/src/org/chromium/android_webview/CompatQuirks.java
@@ -33,6 +33,8 @@ public abstract class CompatQuirks {
         Quirk.FIXUP_OCTOTHORPES_IN_LOAD_DATA,
         Quirk.ALLOW_FILE_URL_ACCESS_BY_DEFAULT,
         Quirk.LEGACY_DARK_MODE,
+        Quirk.FULL_USERAGENT,
+        Quirk.DISABLE_FILESYSTEM_ACCESS_API,
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface Quirk {
@@ -59,6 +61,18 @@ public abstract class CompatQuirks {
          * apps targeting < T.
          */
         int LEGACY_DARK_MODE = 4;
+
+        /**
+         * Default to the legacy full useragent instead of the reduced UA. Normally enabled for apps
+         * targeting < C.
+         */
+        int FULL_USERAGENT = 5;
+
+        /**
+         * Disable the Filesystem Access API for compatibility with old implementations of
+         * WebChromeClient.onShowFileChooser(). Normally enabled for apps targeting < C.
+         */
+        int DISABLE_FILESYSTEM_ACCESS_API = 6;
     }
 
     /**
