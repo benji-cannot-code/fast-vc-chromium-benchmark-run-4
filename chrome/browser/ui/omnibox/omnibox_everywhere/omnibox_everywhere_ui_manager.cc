dcb040f0178ffc1de3c9fb853fea3bae26a3f316
Nihar Majmudar <niharm@google.com>
2026-08-27 19:38:04 -0700

    [loomnibox] Update settings strings.
    Update settings strings and remove learn more link from first setting.
    
    Change-Id: I02e22bd65ba892e0e974ef2abdf4f35b5ac7140d
    Bug: 553614515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282648
    Auto-Submit: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Hamzah Behery <behery@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687735}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.cc
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_ui_manager.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/input/native_web_keyboard_event.h"
 #include "components/keep_alive_registry/keep_alive_registry.h"
