6a43e0ea6b6aab6c80b051f03cfa54dbc17ba961
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-08-28 10:52:40 -0700

    Add orphan module_event_sink_impl_unittest.cc into it BUILD.gn
    Bug: 552910737
    Change-Id: I06e8c166d2ffb54dcebaaeffef003d5c3688dd47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8295944
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688147}

--- chrome/browser/win/conflicts/module_event_sink_impl_unittest.cc
+++ chrome/browser/win/conflicts/module_event_sink_impl_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/files/file_path.h"
+#include "base/functional/bind.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
