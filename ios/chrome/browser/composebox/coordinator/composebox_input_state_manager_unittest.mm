358979f123b4063c2c57745ecff2693640bfe631
Louis Romero <lpromero@google.com>
2026-08-28 03:11:51 -0700

    [iOS][Composebox] Add unit test verifying Drive is disabled in Incognito
    Add test coverage in ComposeboxInputStateManagerTest ensuring that
    ComposeboxAttachmentOption::kDrive is not allowed in Incognito mode.
    
    TAG=agy
    BUG=553840211
    CONV=f8823862-a7fc-45d3-a9b5-b3e1a95ca053
    
    Change-Id: Iae56fa139f5895e29edf403a46ba8b6d28967c8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8314167
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687887}

--- ios/chrome/browser/composebox/coordinator/composebox_input_state_manager_unittest.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_state_manager_unittest.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (ComposeboxModelOption)defaultModel;
 - (void)onDriveDisclaimerChecked:
     (drive_picker::DriveDisclaimerController::DisclaimerStatus)status;
+- (BOOL)isAttachmentAllowed:(ComposeboxAttachmentOption)attachmentOption;
 @end
 
 @interface FakeComposeboxInputStateManagerDelegate
@@ -1409,4 +1410,28 @@ TEST_F(ComposeboxInputStateManagerTest,
   identity_test_env_.ClearPrimaryAccount();
   EXPECT_EQ(pref_service_.GetInteger(contextual_search::kDriveConsentState),
             static_cast<int>(contextual_search::DriveConsentState::kNotReady));
+}
+
+// Tests that Drive attachment option is not allowed in Incognito mode.
+TEST_F(ComposeboxInputStateManagerTest, TestDriveNotAllowedInIncognito) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitWithFeatures(
+      {omnibox::kComposeboxDriveContextMenuOption}, {});
+
+  ComposeboxInputStateManager* incognitoManager =
+      [[ComposeboxInputStateManager alloc]
+           initWithWebStateList:&web_state_list_
+                     modeHolder:mode_holder_
+                    prefService:&pref_service_
+          aimEligibilityService:mock_aim_service_.get()
+                identityManager:nullptr
+             templateURLService:nullptr
+                  sessionHandle:session_handle_.get()
+                     entrypoint:ComposeboxEntrypoint::kOther
+                    isIncognito:YES
+               urlLoaderFactory:shared_url_loader_factory_];
+
+  EXPECT_FALSE([incognitoManager
+      isAttachmentAllowed:ComposeboxAttachmentOption::kDrive]);
+  [incognitoManager disconnect];
 }
