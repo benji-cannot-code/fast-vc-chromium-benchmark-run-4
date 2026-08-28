346e9da76c80fe662a22b2cfad236ada22f5fc64
HuanPo Lin <robertlin@chromium.org>
2026-08-27 19:50:50 -0700

    Reland "Support DSEv1 Prefetch for PreloadServingMetrics.{Initiator}.{All/Srp}"
    This is a reland of commit 50bdb78f3423a137369ff48bd326c90e068c2648
    
    Difference from the original CL:
    Instead of dynamically getting navigation id from a navigation request,
    SearchPrefetchNavigationThrottle cache the navigation id in the
    constructor and use the cached value.
    
    Original change's description:
    > Support DSEv1 Prefetch for PreloadServingMetrics.{Initiator}.{All/Srp}
    >
    > Currently, DSEv1 Prefetch is classified as "Other" in
    > PreloadServingMetrics.{Initiator}.{All/Srp} metrics. To properly record
    > the metrics, this CL introduces a field
    > `is_served_by_legacy_search_prefetch_` in NavigationHandleUserData for
    > PreloadServingMetricsPageLoadMetricsObserver. Design Doc
    > https://docs.google.com/document/d/1m5Icw3une4T5jb3FetzdODYKBjIWpMaz-IfLlJlN9_g/edit?usp=sharing
    >
    > Unresolved-Comment-Reason:
    > HTTPCacheSearchPreloadUnifiedBrowserTest.BackwardHitHttpCache
    > prerender metrics needs more investigation.
    >
    > Bug: 517725655
    > Change-Id: I8191a9e7e56e8fbb88ef040f59f84a1939fa55fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254478
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Ken Okada <kenoss@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1687041}
    
    Bug: 517725655
    Change-Id: I9b01ae206ff870eee2d04e3afc8190cbf948acbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8311347
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687740}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
+#include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
@@ -174,6 +175,11 @@ class SearchPrefetchService : public KeyedService,
   SearchPrefetchURLLoader::RequestHandler MaybeCreateResponseReaderForPrerender(
       const network::ResourceRequest& tentative_resource_request);
 
+  // Tracks navigation IDs currently being served by a search prefetch.
+  void AddServingNavigationId(int64_t navigation_id);
+  bool IsServingNavigation(int64_t navigation_id) const;
+  void RemoveServingNavigationId(int64_t navigation_id);
+
   // Reports the status of a prefetch for a given search suggestion URL.
   std::optional<SearchPrefetchStatus> GetSearchPrefetchStatusForTesting(
       const GURL& canonical_search_url);
@@ -297,6 +303,8 @@ class SearchPrefetchService : public KeyedService,
   // serving time of the response.
   std::map<GURL, std::pair<GURL, base::Time>> prefetch_cache_;
 
+  // Navigation IDs currently being served by a search prefetch.
+  base::flat_set<int64_t> serving_navigation_ids_;
 
   base::WeakPtrFactory<SearchPrefetchService> weak_factory_{this};
 };
