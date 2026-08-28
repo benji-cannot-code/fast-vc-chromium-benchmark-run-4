956614d88b433d9953c4d36dd63edf34e46b4b46
Andrew Paseltiner <apaseltiner@chromium.org>
2026-08-28 07:03:37 -0700

    Retain input case when parsing Avail-Language header
    The proposal for this header does not mention anything about
    lower-casing the input before parsing, and doing so can actually change
    the header's validity from a structured-headers syntactic perspective,
    as parameter keys cannot contain uppercase letters.
    
    This change is safe because the only non-test caller of
    ParseAvailLanguage is feature-disabled by default, and because all
    readers of its return value properly compare the values
    case-insensitively.
    
    Note that the ParseContentLanguages function also lower-cases its input,
    but the behavior of that function is ambiguous as its specification
    doesn't even mention structured headers, so it is left unchanged in this
    CL.
    
    Fixed: 552435230
    Change-Id: I46b50783fbd195f088577fd0b7ad7b9c92bba2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8288027
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687971}

--- services/network/public/cpp/avail_language_header_parser.cc
+++ services/network/public/cpp/avail_language_header_parser.cc
@@ -12,17 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/strings/string_util.h"
 #include "net/http/structured_headers.h"
 
 namespace network {
 
 std::optional<std::vector<std::string>> ParseAvailLanguage(
     const std::string& header) {
-  // Avail-Language is a sh-list of tokens to header; see:
-  // https://mnot.github.io/I-D/draft-nottingham-http-availability-hints.html#section-5.3
+  // https://projects.mnot.net/I-D/draft-nottingham-http-availability-hints.html#name-content-language
   std::optional<net::structured_headers::List> maybe_list =
-      net::structured_headers::ParseList(base::ToLowerASCII(header));
+      net::structured_headers::ParseList(header);
 
   if (!maybe_list.has_value()) {
     return std::nullopt;
