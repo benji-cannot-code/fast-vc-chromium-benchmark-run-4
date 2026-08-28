a4b6f78689fdd81f94c25b7a8bb490a30396ba0d
Lei Zhang <thestig@chromium.org>
2026-08-27 20:14:16 -0700

    Move BigInt support out of to_v8_traits.h and native_value_traits_impl.h
    Move the ToV8Traits and NativeValueTraits specializations for IDLBigint
    from to_v8_traits.h and native_value_traits_impl.h to dedicated headers.
    This removes bigint.h from 5K+ translation units. Update the bindings
    generator to include to_v8_traits_bigint.h and
    native_value_traits_impl_bigint.h whenever an IDL type contains bigint.
    
    TAG=agy
    CONV=60f13344-2159-4976-ab16-33b5392830f1
    
    Change-Id: I0c30977fe3ecc0a588aa595476182c3770014664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8303292
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687746}

--- third_party/blink/renderer/bindings/core/v8/native_value_traits_impl_test.cc
+++ third_party/blink/renderer/bindings/core/v8/native_value_traits_impl_test.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/numeric/int128.h"
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
+#include "third_party/blink/renderer/bindings/core/v8/native_value_traits_impl_bigint.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_internals.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_test_sequence_callback.h"
