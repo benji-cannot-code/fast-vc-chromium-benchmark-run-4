3ff1a98a600a52263fb3e186820c13a312923a10
Koji Ishii <kojii@chromium.org>
2026-08-28 02:05:25 -0700

    [text-box-trim] Apply to inline boxes
    The CSS `text-box-trim` property should apply to block
    containers and inline boxes, but it applied only to block
    containers.
    
    This patch supports applying it to inline boxes.
    
    Bug: 536956106, 514953511
    Change-Id: If3b90c232f4cd3704306605b7df66cb0e19bf139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8313707
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687862}

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/object_painter.h"
 #include "third_party/blink/renderer/core/paint/outline_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/size_assertions.h"
 #include "ui/gfx/geometry/quad_f.h"
 
@@ -194,8 +195,11 @@ bool LayoutInline::ComputeInitialShouldCreateBoxFragment(
     return true;
 
   if (style.HasBoxDecorationBackground() || style.MayHavePadding() ||
-      style.MayHaveMargin())
+      style.MayHaveMargin() ||
+      (style.TextBoxTrim() != ETextBoxTrim::kNone &&
+       RuntimeEnabledFeatures::TextBoxTrimOnInlineBoxEnabled())) {
     return true;
+  }
 
   if (style.AnchorName())
     return true;
