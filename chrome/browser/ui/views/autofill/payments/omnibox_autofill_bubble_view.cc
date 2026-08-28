2ab8e36c0fa08d408e5ca571e104412a34e37d73
Fernando Ramirez <ferny@google.com>
2026-08-27 21:10:27 -0700

    [AF] Do not collapse chip on omnibox autofill suggestion acceptance
    Design: go/autofill-omnibox-dd
    Fixed: 553640628
    Change-Id: Id73eef1164847a24ff5a38232f9caa7c9a3c4139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8309592
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687756}

--- chrome/browser/ui/views/autofill/payments/omnibox_autofill_bubble_view.cc
+++ chrome/browser/ui/views/autofill/payments/omnibox_autofill_bubble_view.cc
@@ -197,7 +197,10 @@ void OmniboxAutofillBubbleView::OnSuggestionAccepted(
     size_t row_index) {
   if (controller_) {
     controller_->OnSuggestionAccepted(suggestion, row_index);
-    CloseBubble();
+    if (views::Widget* widget = GetWidget()) {
+      widget->CloseWithReason(
+          views::Widget::ClosedReason::kAcceptButtonClicked);
+    }
   }
 }
 
