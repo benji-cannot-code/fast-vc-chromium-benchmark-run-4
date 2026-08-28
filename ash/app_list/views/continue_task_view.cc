c5ece3042d3ed0bc418c4ac414d74a08cce1558d
Alexei Svitkine <asvitkine@chromium.org>
2026-08-28 12:13:51 -0700

    Fix off-by-one errors in public Chromium TS and C++ metrics.
    - TS:
      - sea_pen_metrics_logger.ts:
        - Use SeaPenSamplePromptId.MAX_VALUE + 1 as boundary.
      - os_search_result_row.ts:
        - Use SearchResultType.MAX_VALUE + 1 as boundary.
      - wallpaper_search.ts:
        - Use WallpaperSearchStatus.MAX_VALUE + 1 as boundary.
    - C++:
      - continue_task_view.cc:
        - Use 2-arg version of UmaHistogramEnumeration to fix off-by-one.
      - video_capture_device_win.cc:
        - Use media::VideoPixelFormat::PIXEL_FORMAT_MAX + 1 as boundary.
      - video_capture_device_mf_win.cc:
        - Use media::VideoPixelFormat::PIXEL_FORMAT_MAX + 1 as boundary.
      - video_capture_metrics.cc:
        - Use media::VideoPixelFormat::PIXEL_FORMAT_MAX + 1 as boundary.
    
    TAG=agy
    CONV=032e824b-0738-44bd-a920-c2dcb18aa066
    
    Bug: 545187388
    Change-Id: Ifb9c85ad7f3d2efa4fcabcb2d792a32161004ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8275137
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688229}

--- ash/app_list/views/continue_task_view.cc
+++ ash/app_list/views/continue_task_view.cc
@@ -406,7 +406,7 @@ void ContinueTaskView::LogMetricsOnResultRemoved() {
   RecordCumulativeContinueSectionResultRemovedNumber();
 
   base::UmaHistogramEnumeration("Apps.AppList.Search.ContinueResultRemoved",
-                                GetTaskResultType(), TaskResultType::kMaxValue);
+                                GetTaskResultType());
 }
 
 BEGIN_METADATA(ContinueTaskView)
