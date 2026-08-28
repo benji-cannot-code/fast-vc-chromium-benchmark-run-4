5358243ac2606bd85f28fc57c5906433008d8462
behery <behery@chromium.org>
2026-08-28 09:22:06 -0700

    [Bookmarks Side Panel] Fix cr-lazy-list height estimation and view toggle clipping
    1. Bind .itemSize (36px in compact view, 68px in expanded view) to
       cr-lazy-list instances (#listA and #listB). This prevents
       cr-lazy-list from falling back to its default 100px unrendered item
       size estimate on initial load, eliminating the empty gap at the
       bottom of the card container.
    2. Trigger updateDisplayList_() on compact_ property changes in
       willUpdate() so toggling from compact to expanded view correctly
       re-measures and resizes the container to expanded card height,
       preventing cards from getting clipped.
    
    TAG=agy
    CONV=6ca99ea1-367a-4495-b486-3dbe5fc3a505
    
    Demo: https://screencast.googleplex.com/cast/NTU2NjgxNjQ5MDM1NjczNnwzYzgwZDA4Ny1iYQ
    Bug: 40271894, 553523726
    Change-Id: I39a9c258ca6b00ab3b86f57bc82c13efdf6bef2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8307553
    Commit-Queue: Hamzah Behery <behery@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688084}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.html.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.html.ts
@@ -49,6 +49,7 @@ export function getHtml(this: PowerBookmarksListElement) {
           <cr-lazy-list id="listA"
               .items="${this.displayListA_}"
               .scrollTarget="${this.scrollTarget_}"
+              .itemSize="${this.getItemSize_()}"
               role="tree"
               .template="${(item: unknown, index: number) => html`
                 <power-bookmark-row
@@ -101,6 +102,7 @@ export function getHtml(this: PowerBookmarksListElement) {
           <cr-lazy-list id="listB"
               .items="${this.displayListB_}"
               .scrollTarget="${this.scrollTarget_}"
+              .itemSize="${this.getItemSize_()}"
               role="tree"
               .template="${(item: unknown, index: number) => html`
                 <power-bookmark-row
