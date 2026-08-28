e10ddd66d2b7a81090a3608fc092efb935651c80
Salma Elmahallawy <esalma@google.com>
2026-08-28 06:44:08 -0700

    [Enterprise Skills] Set kError redirect_mode
    Skills that require sso are not supported. Therefore, this case often
    resulted in network success (code 200) and hash mismatch as it tried
    hashing the redirection response returned and compared it to expected
    hash.
    
    This CL fixes this by erroring out on redirection instead of considering
    it a successful fetch with hash mismatch.
    
    Fixed:553597322
    
    skills: Set kError redirect_mode for enterprise skills fetching
    
    Change-Id: Id380a3054c06db72a8842ef1a18871c32a76afa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8307874
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Reviewed-by: Howard Chang <howardcha@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687958}

--- components/skills/internal/enterprise_skills_provider.cc
+++ components/skills/internal/enterprise_skills_provider.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/skills/internal/skill_parser.rs.h"
 #include "components/skills/public/skills_prefs.h"
 #include "crypto/hash.h"
+#include "net/base/net_errors.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -215,6 +216,7 @@ void EnterpriseSkillsProvider::FetchSkillsFromUrls() {
     auto resource_request = std::make_unique<network::ResourceRequest>();
     resource_request->url = url;
     resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
+    resource_request->redirect_mode = network::mojom::RedirectMode::kError;
 
     auto url_loader = network::SimpleURLLoader::Create(
         std::move(resource_request), traffic_annotation);
@@ -304,7 +306,8 @@ void EnterpriseSkillsProvider::OnURLLoadComplete(
     // ParseAndValidateSkill.
   } else {
     LOG_POLICY(ERROR, POLICY_PROCESSING)
-        << kLogPrefix << kDownloadFailedError << expected_hash;
+        << kLogPrefix << kDownloadFailedError << expected_hash
+        << " (Error: " << net::ErrorToShortString(source->NetError()) << ").";
   }
 
   std::erase_if(url_loaders_, [source](const auto& loader) {
