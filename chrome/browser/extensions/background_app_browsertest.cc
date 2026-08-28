a0f7f0eb6de1659e40c9e8789c479a4745ed8c84
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-08-28 13:48:10 -0700

    Add orphan background_app_browsertest.cc to its BUILD.gn
    This file was accidentally orphaned in crrev.com/c/6282731. This CL is
    adding it back with a minor update for how the extension description is
    now accessed.
    
    Bug: 552910737
    Change-Id: I5c50f25e0f7b6130713b4fb0ce87a0f2b51416da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8308515
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688291}

--- chrome/browser/extensions/background_app_browsertest.cc
+++ chrome/browser/extensions/background_app_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/test_extension_registry_observer.h"
+#include "extensions/common/manifest_handlers/description_info.h"
 
 namespace extensions {
 
@@ -50,7 +51,9 @@ IN_PROC_BROWSER_TEST_F(BackgroundAppBrowserTest, InstallBackgroundApp) {
   const ExtensionSet& extensions = extension_registry()->enabled_extensions();
   EXPECT_TRUE(std::ranges::contains(
       extensions, "A simple app with background permission set.",
-      &Extension::description));
+      [](const auto& extension) -> const std::string& {
+        return DescriptionInfo::GetDescription(*extension);
+      }));
   // Verify the installed extension did not pop up a background mode
   // notification.
   EXPECT_EQ(0, g_browser_process->background_mode_manager()
