d169b1ecc4d550707a4979ac95bcf35f1be96672
Bryant Chandler <bryantchandler@google.com>
2026-08-28 08:56:18 -0700

    [glic] Trigger prewarming when showing contextual nudge
    Connects GlicNudgeController to GlicKeyedService to trigger prewarming
    with GlicWarmingTrigger::kNudge when a contextual nudge is shown and
    kGlicWarmOnNudge is enabled.
    
    Bug: b:552086591
    Change-Id: Ic6a30608d678bad23124122cc62733636a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281260
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688060}

--- chrome/browser/glic/browser_ui/glic_nudge_controller_impl.cc
+++ chrome/browser/glic/browser_ui/glic_nudge_controller_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/browser_ui/glic_split_button_controller.h"
 #include "chrome/browser/glic/browser_ui/glic_split_button_delegate.h"
 #include "chrome/browser/glic/glic_pref_names.h"
+#include "chrome/browser/glic/public/features.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
 #include "chrome/browser/glic/suggestions/contextual_cueing_features.h"
@@ -133,6 +134,12 @@ void GlicNudgeControllerImpl::OnNudgeActivity(GlicNudgeActivity activity) {
   switch (activity) {
     case GlicNudgeActivity::kNudgeShown: {
       nudge_activity_callback_.Run(GlicNudgeActivity::kNudgeShown);
+      if (base::FeatureList::IsEnabled(features::kGlicWarmOnNudge)) {
+        if (auto* glic_service = GlicKeyedService::Get(
+                browser_window_interface_->GetProfile())) {
+          glic_service->TryPreload(GlicWarmingTrigger::kNudge);
+        }
+      }
       if (!scoped_call_to_action_lock_) {
         // TODO(crbug.com/484037810): Once Android has BrowserWindowFeatures,
         // this shouldn't be nullable.
