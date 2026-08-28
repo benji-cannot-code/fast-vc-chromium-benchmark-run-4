a973d8865429f0fb9dee073b50875e730adfddf2
Sky Malice <skym@chromium.org>
2026-08-28 14:36:47 -0700

    [Clean] Remove unused variables in HomeModulesCoordinatorUnitTest
    Remove unused mock SegmentationPlatformService and unused profile
    observer captor in HomeModulesCoordinatorUnitTest.
    
    TAG=agy
    CONV=28e38366-e371-490c-9aa4-8186221b34be
    
    Bug: 546549170
    Change-Id: I5e659a47ba190fbcab5f7606ff69be4f76caf304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8299528
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688346}

--- chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinatorUnitTest.java
+++ chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinatorUnitTest.java
@@ -71,7 +71,6 @@ import org.chromium.components.browser_ui.widget.displaystyle.UiConfig;
 import org.chromium.components.browser_ui.widget.displaystyle.UiConfig.DisplayStyle;
 import org.chromium.components.browser_ui.widget.displaystyle.VerticalDisplayStyle;
 import org.chromium.components.segmentation_platform.ClassificationResult;
-import org.chromium.components.segmentation_platform.SegmentationPlatformService;
 import org.chromium.components.segmentation_platform.prediction_status.PredictionStatus;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
@@ -100,7 +99,6 @@ public class HomeModulesCoordinatorUnitTest {
     @Mock private DisplayMetrics mDisplayMetrics;
     @Mock private HomeModulesConfigManager mHomeModulesConfigManager;
     @Mock private Profile mProfile;
-    @Mock SegmentationPlatformService mSegmentationPlatformService;
     @Mock private ModuleRegistry mModuleRegistry;
     @Mock private HomeModulesMediator mMediator;
     @Mock private ModelList mModel;
@@ -108,7 +106,6 @@ public class HomeModulesCoordinatorUnitTest {
     @Mock private HomeModulesRankingHelper.Natives mHomeModulesRankingHelperJniMock;
 
     @Captor private ArgumentCaptor<DisplayStyleObserver> mDisplayStyleObserver;
-    @Captor private ArgumentCaptor<Callback<Profile>> mProfileObserver;
     @Captor private ArgumentCaptor<RecyclerView.OnScrollListener> mOnScrollListener;
     @Captor private ArgumentCaptor<Callback<ClassificationResult>> mClassificationResultCaptor;
     @Captor private ArgumentCaptor<OnLongClickListener> mLongClickListenerCaptor;
