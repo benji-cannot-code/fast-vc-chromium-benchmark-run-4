ccf0dd83ec7f19d534acf20ff47636df4e8e7fb6
Jingyun Liu <jingyun@google.com>
2026-08-28 12:34:41 -0700

    Add a chrome://flag entry for Prompt API tool use.
    Change-Id: I10272bae45f432b0601ef8ee3918644d8d165468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8300678
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688248}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4756,6 +4756,11 @@ const FeatureEntry::Choice kAISemanticEmbedderChoices[] = {
     {flags_ui::kGenericExperimentChoiceEnabled, switches::kEnableFeatures,
      "AIEmbeddingsAPI,AIEmbeddingsAPIForWorkers"}};
 
+const FeatureEntry::Choice kPromptAPIToolUseChoices[] = {
+    {flags_ui::kGenericExperimentChoiceDefault, "", ""},
+    {flags_ui::kGenericExperimentChoiceEnabled, switches::kEnableFeatures,
+     "AIPromptAPIToolUse,OnDeviceModelConversationBackend,"
+     "AIApiFoundationalModel:model_version/v4"}};
 
 const FeatureEntry::Choice kGemma4Choices[] = {
     {flags_ui::kGenericExperimentChoiceDefault, "", ""},
@@ -10560,6 +10565,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kPromptAPIMultimodalInputName,
      flag_descriptions::kPromptAPIMultimodalInputDescription, kOsDesktop,
      FEATURE_VALUE_TYPE(blink::features::kAIPromptAPIMultimodalInput),
+     flag_descriptions::kAIAPIsLinks},
+
+    {"prompt-api-tool-use", flag_descriptions::kPromptAPIToolUseName,
+     flag_descriptions::kPromptAPIToolUseDescription, kOsDesktop,
+     MULTI_VALUE_TYPE(kPromptAPIToolUseChoices),
      flag_descriptions::kAIAPIsLinks},
 
     {"prompt-api-sampling-mode", flag_descriptions::kPromptAPISamplingModeName,
