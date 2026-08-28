ec69eaea5ff96fbeba82e57e4a1068212a302a70
Alex Moshchuk <alexmos@chromium.org>
2026-08-28 14:00:17 -0700

    CPSP: Refactor isolated origin C++ helpers
    This CL prepares for the Rust migration of legacy isolated origins by
    refactoring several C++ methods:
    
    1. Changes GetMatchingProcessIsolatedOriginFromLegacyOriginList() to
    return an optional<url::Origin>, instead of returning a bool and
    populating the origin in a mutable param.
    
    2. Introduces RemoveTrailingDotFromUrlIfNecessary() and
    CreateOriginWithDefaultPortIfNecessary() helpers and uses them to
    simplify corner cases in origin matching code.
    
    3. Extracts RemoveIsolatedOriginsForBrowserContext() and
    RemoveIsolatedOriginsForBrowsingInstance() as separate methods for
    cleaner migration. Also unifies naming for all BrowsingInstance-related
    cleanup functions to use Remove* rather than Erase*.
    
    4. Fixes an existing bug in per-BrowsingInstance isolated origin cleanup
    that was discovered during the subsequent Rust migration, where map
    entries without any IsolatedOriginEntries could be kept around
    indefinitely.
    
    Bug: 482216433, 553804653
    Change-Id: I608421b9cc17421b6ad5940c0c0f5739843f94ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282857
    Reviewed-by: Liam Brady <lbrady@google.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688308}

--- content/browser/security/cpsp/child_process_security_policy_unittest.cc
+++ content/browser/security/cpsp/child_process_security_policy_unittest.cc
@@ -3574,7 +3574,7 @@ TEST_P(ChildProcessSecurityPolicyTest,
   ASSERT_TRUE(result.has_value());
   EXPECT_EQ(oac_opt_in, result.value());
 
-  p->EraseOriginAgentClusterState(browsing_instance_id);
+  p->RemoveOriginAgentClusterState(browsing_instance_id);
 }
 
 TEST_P(ChildProcessSecurityPolicyTest,
@@ -3617,7 +3617,7 @@ TEST_P(ChildProcessSecurityPolicyTest,
   ASSERT_TRUE(result.has_value());
   EXPECT_EQ(oac_default, result.value());
 
-  p->EraseOriginAgentClusterState(browsing_instance_id);
+  p->RemoveOriginAgentClusterState(browsing_instance_id);
 }
 
 TEST_P(ChildProcessSecurityPolicyTest,
