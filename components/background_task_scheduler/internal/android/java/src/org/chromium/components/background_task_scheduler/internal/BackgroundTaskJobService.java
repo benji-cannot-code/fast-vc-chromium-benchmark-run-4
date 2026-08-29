d054bec56f6bd613cb3ff06f564f3a9a72e62396
Mark Schillaci <mschillaci@google.com>
2026-08-28 17:37:44 -0700

    [histogram-cleanup] Remove Android.BackgroundTaskScheduler.TaskExpired
    Remove expired histogram Android.BackgroundTaskScheduler.TaskExpired which expired on 2025-06-08. This metric is no longer needed as the evaluation period has concluded. This removes the histogram metadata entry, the UMA recording call and helper method, and the corresponding unit test.
    
    OBSOLETE_HISTOGRAMS[Android.BackgroundTaskScheduler.TaskExpired]=expired
    
    This change was generated using the skill histogram-cleanup.
    
    Bug: 499059525, 414387624
    Change-Id: I9f371e4c100273b69cbc95b1e29b225eefe318d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8305522
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1688433}

--- components/background_task_scheduler/internal/android/java/src/org/chromium/components/background_task_scheduler/internal/BackgroundTaskJobService.java
+++ components/background_task_scheduler/internal/android/java/src/org/chromium/components/background_task_scheduler/internal/BackgroundTaskJobService.java
@@ -125,7 +125,6 @@ public class BackgroundTaskJobService extends JobService {
         }
 
         if (BackgroundTaskSchedulerJobService.didTaskExpire(params, mClock.currentTimeMillis())) {
-            BackgroundTaskSchedulerUma.getInstance().reportTaskExpired(params.getJobId());
             return false;
         }
 
