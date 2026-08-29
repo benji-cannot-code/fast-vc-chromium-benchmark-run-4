1280ca2d8aaea1b242e2d1ab76155084e4fe07ed
Lei Zhang <thestig@chromium.org>
2026-08-28 17:17:38 -0700

    Move blink::V8MicrotasksScope::EffectiveMode() impl out of the header
    Move the implementation of V8MicrotasksScope::EffectiveMode() from
    v8_microtasks_scope.h into a new v8_microtasks_scope.cc so that
    v8_microtasks_scope.h no longer needs to include event_loop.h. This
    helps reduce the build cost since v8_microtasks_scope.h is transitively
    included in 4K+ translation units. Then do IWYU to fix the build.
    
    TAG=agy
    CONV=54d59e7c-d76b-456f-b76e-416caed1e853
    
    Change-Id: I02df13ebcb9e468199a621fd84b5ae986d63ba78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8309158
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688404}

--- third_party/blink/renderer/bindings/core/v8/promise_all.h
+++ third_party/blink/renderer/bindings/core/v8/promise_all.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/heap_bind.h"
+#include "third_party/blink/renderer/platform/scheduler/public/event_loop.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 
 namespace blink {
