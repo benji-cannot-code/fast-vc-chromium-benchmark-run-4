9fa28ce91bc2e98439cdd466b9cb09b66dfbd95b
Sky Malice <skym@chromium.org>
2026-08-28 17:58:48 -0700

    [Clean] Remove unused mock in CustomTabToolbarUnitTest
    Remove unused mock BooleanSupplier in CustomTabToolbarUnitTest.
    
    TAG=agy
    CONV=1f31a1c2-0c8b-4b52-a53f-df123451cd4e
    
    Bug: 546549170
    Change-Id: I4c102a357501eaa7fa2943757b1ed4ed03466068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8310601
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1688464}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbarUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/features/toolbar/CustomTabToolbarUnitTest.java
@@ -85,7 +85,6 @@ import org.chromium.url.JUnitTestGURLs;
 
 import java.lang.ref.WeakReference;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BooleanSupplier;
 
 /** Tests AMP url handling in the CustomTab Toolbar. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -109,7 +108,6 @@ public class CustomTabToolbarUnitTest {
     @Mock MenuButtonCoordinator mMenuButtonCoordinator;
     @Mock private ToggleTabStackButtonCoordinator mTabSwitcherButtonCoordinator;
     @Mock HistoryDelegate mHistoryDelegate;
-    @Mock BooleanSupplier mPartnerHomepageEnabledSupplier;
     @Mock UserEducationHelper mUserEducationHelper;
     @Mock Tracker mTracker;
     @Mock Tab mTab;
