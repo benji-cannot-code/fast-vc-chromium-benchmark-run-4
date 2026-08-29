420012488a0d3c3b93c056e8c279d91ed414edf3
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-08-28 22:38:34 -0700

    [layout] Remove LayoutObject::Style usage from layout_box.cc
    The style is null on a LayoutObject for a very small period of time.
    Most callers shouldn't have to check if the style is null.
    
    Remove the null check.
    
    There should be no behaviour change.
    
    Change-Id: Ib5e0d506383923549239e10393e824ed6078e097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8290488
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688535}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -620,7 +620,7 @@ void LayoutBox::WillBeRemovedFromTree() {
   ClearCustomLayoutChild();
 
   // Notify the display-locks that anchors within a sub-tree may disappear.
-  if (Style() && StyleRef().HasOutOfFlowPosition()) {
+  if (StyleRef().HasOutOfFlowPosition()) {
     NotifyContainingDisplayLocksForAnchorPositioning(
         DisplayLocksAffectedByAnchors(), nullptr);
   }
