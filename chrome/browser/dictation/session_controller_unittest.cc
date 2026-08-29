83ae1e017299bcb9c7a82b016341528df0633760
Kevin McNee <mcnee@chromium.org>
2026-08-28 17:29:23 -0700

    Dictation: Default to automatic session end after stream end
    Bug: b:552527466, b:548754395, 553199051
    Change-Id: I859808fa3aafc7c28459ac4ed7fdda63f3a15fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8311247
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688420}

--- chrome/browser/dictation/session_controller_unittest.cc
+++ chrome/browser/dictation/session_controller_unittest.cc
@@ -79,6 +79,11 @@ class DictationSessionControllerTest : public ChromeRenderViewHostTestHarness {
     run_loop.Run();
   }
 
+  void FastForwardPastAutoSessionEnd() {
+    task_environment()->FastForwardBy(kAutoSessionEndDelay.Get() +
+                                      base::Milliseconds(1));
+  }
+
  protected:
   base::test::ScopedFeatureList scoped_feature_list_;
   testing::NiceMock<MockSessionControllerDelegate> mock_delegate_;
@@ -526,6 +531,11 @@ TEST_F(DictationSessionControllerTest, EndStreamOnFocusNonEditableNode) {
 }
 
 TEST_F(DictationSessionControllerTest, StartNewStreamOnFocusOtherEditableNode) {
+  if (kSessionEndsOnStreamEnd.Get()) {
+    GTEST_SKIP()
+        << "Multiple streams per session are not possible in this config.";
+  }
+
   Target target_1(EmptyTarget());
   auto mock_stream_provider_1 =
       std::make_unique<testing::NiceMock<MockStreamProvider>>();
@@ -724,6 +734,7 @@ TEST_F(DictationSessionControllerTest,
 
   controller_->DidUpdateStreamProviderState(
       *stream_provider, StreamProvider::StreamState::kTranscribing);
+  FastForwardPastAutoSessionEnd();
   WaitForPostedTasks();
 
   EXPECT_EQ(controller_, nullptr);
@@ -912,7 +923,7 @@ TEST_F(DictationSessionControllerTest,
   ASSERT_NE(controller_, nullptr);
   EXPECT_EQ(controller_->GetState(), SessionState::kInactive);
   EXPECT_TRUE(controller_->is_auto_session_end_timer_running_for_testing());
-  task_environment()->FastForwardBy(base::Milliseconds(751));
+  FastForwardPastAutoSessionEnd();
   EXPECT_EQ(controller_, nullptr);
 }
 
@@ -969,7 +980,7 @@ TEST_F(DictationSessionControllerTest,
 
   // Delayed shutdown should be cancelled.
   EXPECT_FALSE(controller_->is_auto_session_end_timer_running_for_testing());
-  task_environment()->FastForwardBy(base::Milliseconds(751));
+  FastForwardPastAutoSessionEnd();
   EXPECT_NE(controller_, nullptr);
   EXPECT_EQ(controller_->attached_stream_provider(), stream_provider2);
 }
