6d09f0839dd03c404bcbd9ce711971cc80f0d398
Zekun Jiang <zekunjiang@google.com>
2026-08-28 22:32:34 -0700

    Move ToExecuteRequest to a public helper file.
    The streaming RPC to MES uses the same request proto so this will be
    shared between both pipelines.
    
    Bug: 553134125
    Change-Id: I769c64abd022162a79fdd71bf28b4b48c7e38e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8309526
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688526}

--- components/optimization_guide/core/model_execution/model_execution_fetcher.cc
+++ components/optimization_guide/core/model_execution/model_execution_fetcher.cc
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "components/optimization_guide/core/model_execution/feature_keys.h"
 #include "components/optimization_guide/core/optimization_guide_util.h"
-#include "components/optimization_guide/proto/model_execution.pb.h"
-
-constexpr char kGoogleAPITypeName[] = "type.googleapis.com/";
 
 namespace optimization_guide {
 
@@ -23,19 +20,6 @@ void ModelExecutionFetcher::RecordRequestStatusHistogram(
       base::StrCat({"OptimizationGuide.ModelExecutionFetcher.RequestStatus.",
                     GetStringNameForModelExecutionFeature(feature)}),
       status);
-}
-
-// static
-proto::ExecuteRequest ModelExecutionFetcher::ToExecuteRequest(
-    ModelBasedCapabilityKey feature,
-    const google::protobuf::MessageLite& request_metadata) {
-  proto::ExecuteRequest execute_request;
-  execute_request.set_feature(ToModelExecutionFeatureProto(feature));
-  proto::Any* any_metadata = execute_request.mutable_request_metadata();
-  any_metadata->set_type_url(
-      base::StrCat({kGoogleAPITypeName, request_metadata.GetTypeName()}));
-  request_metadata.SerializeToString(any_metadata->mutable_value());
-  return execute_request;
 }
 
 }  // namespace optimization_guide
--- components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
+++ components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "components/optimization_guide/core/access_token_helper.h"
 #include "components/optimization_guide/core/model_execution/feature_keys.h"
+#include "components/optimization_guide/core/model_execution/model_execution_proto_util.h"
 #include "components/optimization_guide/core/model_execution/optimization_guide_model_execution_error.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/core/optimization_guide_logger.h"
@@ -679,7 +680,7 @@ void ModelExecutionFetcherImpl::ExecuteModel(
   model_execution_callback_ = std::move(callback);
 
   proto::ExecuteRequest execute_request =
-      ToExecuteRequest(feature, request_metadata);
+      CreateExecuteRequest(feature, request_metadata);
   std::string serialized_request;
   execute_request.SerializeToString(&serialized_request);
 
