fd566ad65c9686522c5a86e9e720a0337b9fb170
Mohamed Adel <adelm@google.com>
2026-08-28 18:23:32 -0700

    [LFF Dialogs] Default enable dialog enhancements on Large Form Factors
    This CL enables the DialogsOnLargeFormFactors feature flag by default in
    both C++ and Java.
    
    Key updates:
    - Updated kDialogsOnLargeFormFactors to base::FEATURE_ENABLED_BY_DEFAULT.
    - Updated ModalDialogFeatureMap.sDialogsOnLargeFormFactors CachedFlag default to true.
    - Removed obsolete pre-LFF tablet unit tests and redundant @EnableFeatures annotations in ModalDialogViewUnitTest and AppModalPresenterUnitTest.
    
    TAG=agy
    
    Bug: 553630184
    Change-Id: I63e12de15657a96920e75ad635005f2105020380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8311011
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Commit-Queue: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1688498}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/AppModalPresenterUnitTest.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/AppModalPresenterUnitTest.java
@@ -644,7 +644,6 @@ public class AppModalPresenterUnitTest {
 
     @Test
     @Config(qualifiers = "sw600dp")
-    @EnableFeatures(ModalDialogFeatureList.DIALOGS_ON_LARGE_FORM_FACTORS)
     public void addDialogView_LargeFormFactorUi_Tablet_VerticalMargin24dpAndDimAmount() {
         mAppModalPresenter.setInsetObserver(null);
 
@@ -668,7 +667,6 @@ public class AppModalPresenterUnitTest {
 
     @Test
     @Config(qualifiers = "sw600dp")
-    @EnableFeatures(ModalDialogFeatureList.DIALOGS_ON_LARGE_FORM_FACTORS)
     public void addDialogView_LargeFormFactorUi_Desktop_VerticalMargin24dpAndDimAmount() {
         DeviceInfo.setIsDesktopForTesting(true);
         mAppModalPresenter.setInsetObserver(null);
