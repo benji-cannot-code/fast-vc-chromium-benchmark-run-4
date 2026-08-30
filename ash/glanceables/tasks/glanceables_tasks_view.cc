aed326fa69a3cf6b06b9193a60df5bf1800f438b
Achuith Bhandarkar <achuith@chromium.org>
2026-08-29 23:53:11 -0700

    ash: Remove expired glanceables bubble load latency metrics
    Removes the expired Ash.Glanceables.TimeManagement.* metrics tracking
    initial load time and list change load time for Glanceables bubbles
    (Tasks and Classroom), as their milestone expiration dates have passed.
    
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Classroom.ChangeListToLoadTime.Fail]=Expired as of 2025-12-31.
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Classroom.ChangeListToLoadTime.Success]=Expired as of 2025-12-31.
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Classroom.OpenToInitialLoadTime.FirstOccurence]=Expired as of 2025-12-31.
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Classroom.OpenToInitialLoadTime.SubsequentOccurence]=Expired as of 2025-12-31.
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Tasks.ChangeListToLoadTime]=Expired as of 2025-12-31.
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Tasks.OpenToInitialLoadTime.FirstOccurence]=Expired as of 2025-12-31.
    OBSOLETE_HISTOGRAM[Ash.Glanceables.TimeManagement.Tasks.OpenToInitialLoadTime.SubsequentOccurence]=Expired as of 2025-12-31.
    
    Bug: 505648959
    Change-Id: Ib3d64a1194dc06f797eb141f7e6608f1ec521b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277597
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688694}

--- ash/glanceables/tasks/glanceables_tasks_view.cc
+++ ash/glanceables/tasks/glanceables_tasks_view.cc
@@ -302,7 +302,6 @@ void GlanceablesTasksView::SelectedListChanged() {
   UpdateComboboxReplacementLabelText();
 
   weak_ptr_factory_.InvalidateWeakPtrs();
-  tasks_requested_time_ = base::TimeTicks::Now();
   tasks_list_change_count_++;
   ScheduleUpdateTasks(ListShownContext::kUserSelectedList);
 }
@@ -566,17 +565,11 @@ void GlanceablesTasksView::UpdateTasksInTaskList(
   switch (context) {
     case ListShownContext::kCachedList:
       break;
-    case ListShownContext::kInitialList: {
-      auto* controller = Shell::Get()->glanceables_controller();
-      RecordTasksInitialLoadTime(
-          /*first_occurrence=*/controller->bubble_shown_count() == 1,
-          base::TimeTicks::Now() - controller->last_bubble_show_time());
+    case ListShownContext::kInitialList:
       first_task_list_shown_ = true;
       break;
-    }
     case ListShownContext::kUserSelectedList:
       RecordActiveTaskListChanged();
-      RecordTasksChangeLoadTime(base::TimeTicks::Now() - tasks_requested_time_);
       first_task_list_shown_ = true;
       break;
   }
