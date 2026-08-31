e117e5d1b9e3770da1471b2104b82b0c7b000903
Muhammad Salmaan <musalmaan@chromium.org>
2026-08-31 13:13:17 -0700

    [InfoBar] Fix include-what-you-use in known interception disclosure infobar
    TAG=agy
    
    Bug: 512837934
    Change-Id: I3a9a662c7e03bee76ea37c2e7dbcdd820b9e334e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8308565
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689191}

--- chrome/browser/ssl/known_interception_disclosure_infobar_delegate.h
+++ chrome/browser/ssl/known_interception_disclosure_infobar_delegate.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SSL_KNOWN_INTERCEPTION_DISCLOSURE_INFOBAR_DELEGATE_H_
 
 #include <algorithm>
+#include <memory>
+
 #include "base/memory/raw_ptr.h"
 #include "base/memory/singleton.h"
 #include "base/time/default_clock.h"
