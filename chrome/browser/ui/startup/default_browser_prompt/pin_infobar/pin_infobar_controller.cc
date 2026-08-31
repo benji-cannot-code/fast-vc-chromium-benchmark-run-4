f7f728137e62a6340307fc30910e6cd7bfa42e21
Muhammad Salmaan <musalmaan@chromium.org>
2026-08-31 07:38:50 -0700

    [InfoBar] Fix include-what-you-use in pin infobar
    TAG=agy
    
    Bug: 512837801
    Change-Id: Ibb0625d4d34e15f16369fd69307791abc1301cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8310103
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689067}

--- chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller.cc
+++ chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller.h"
 
+#include <utility>
+
 #include "base/feature_list.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
--- chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller_unittest.cc
+++ chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/default_browser_prompt/pin_infobar/pin_infobar_controller.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/test/bind.h"
 #include "chrome/browser/shell_integration.h"
