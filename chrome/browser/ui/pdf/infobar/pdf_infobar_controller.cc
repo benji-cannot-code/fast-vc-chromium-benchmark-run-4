7bb33bce79b5d142be6b93c76bceb374fbef59d8
Muhammad Salmaan <musalmaan@chromium.org>
2026-08-31 07:48:36 -0700

    [InfoBar] Fix include-what-you-use in PDF infobar
    TAG=agy
    
    Bug: 523213455
    Change-Id: I169af09511f63e5663246396d4e9e46d918864cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8315292
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689078}

--- chrome/browser/ui/pdf/infobar/pdf_infobar_controller.cc
+++ chrome/browser/ui/pdf/infobar/pdf_infobar_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/pdf/infobar/pdf_infobar_controller.h"
 
 #include <optional>
+#include <utility>
 
 #include "base/feature_list.h"
 #include "base/functional/callback.h"
--- chrome/browser/ui/pdf/infobar/pdf_infobar_controller.h
+++ chrome/browser/ui/pdf/infobar/pdf_infobar_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_PDF_INFOBAR_PDF_INFOBAR_CONTROLLER_H_
 
 #include <optional>
+#include <vector>
 
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
