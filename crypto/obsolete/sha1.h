e639f1c10dac4e4410ac4ff358f59d558a77e439
Sorin Jianu <sorin@chromium.org>
2026-08-31 14:31:56 -0700

    component_updater: fix iwyu in metadata_table_chromeos.cc
    Bug: 430344933
    Change-Id: Ieb90ce8499b432d8bf377ba69ee1f4eff4f23a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8298978
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689278}

--- crypto/obsolete/sha1.h
+++ crypto/obsolete/sha1.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CRYPTO_OBSOLETE_SHA1_H_
 #define CRYPTO_OBSOLETE_SHA1_H_
 
+#include <stdint.h>
+
 #include <array>
+#include <string>
 #include <string_view>
 #include <vector>
 
