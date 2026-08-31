e4d3eaec7464ccbf1cba09687c6ff831c3a0b510
Georg Neis <neis@chromium.org>
2026-08-30 22:47:12 -0700

    ash: Use BrowserController::TabObserver in BrowserStatusMonitor
    This change eliminates the dependency on `TabStripModelObserver` by
    migrating `BrowserStatusMonitor` to the new `TabObserver` from
    `BrowserController`. Because of the latter's ordering guarantees we can
    also delete the debug code that was tracking browser creations (via
    `std::set` and `DCHECK` bookkeeping).
    
    Bug: b:369688254
    Change-Id: I2dfbe95f29796ed6a692c0a0e47b5b18da80f1ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8293644
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688840}

--- chrome/browser/ash/browser_delegate/browser_controller_impl.cc
+++ chrome/browser/ash/browser_delegate/browser_controller_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lifetime/application_lifetime_desktop.h"
 #include "chrome/browser/lifetime/browser_shutdown.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/tab_list/tab_removed_reason.h"
 #include "chrome/browser/ui/autofill/chrome_autofill_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_init_state.h"
@@ -354,7 +355,7 @@ void BrowserControllerImpl::OnBrowserClosed(BrowserWindowInterface* browser) {
   for (size_t i = browser_delegate->GetWebContentsCount(); i-- > 0;) {
     content::WebContents* contents = browser_delegate->GetWebContentsAt(i);
     for (auto& observer : tab_observers_) {
-      observer.OnTabRemoved(browser_delegate, contents);
+      observer.OnTabRemoved(browser_delegate, contents, /*will_delete=*/true);
     }
   }
   browser->GetTabStripModel()->RemoveObserver(this);
@@ -387,8 +388,10 @@ void BrowserControllerImpl::OnTabStripModelChanged(
       break;
     case TabStripModelChange::kRemoved:
       for (const auto& item : change.GetRemove()->contents) {
+        bool will_delete =
+            TabRemoveReasonUtils::WillDeleteTab(item.remove_reason);
         for (auto& observer : tab_observers_) {
-          observer.OnTabRemoved(browser, item.contents);
+          observer.OnTabRemoved(browser, item.contents, will_delete);
         }
       }
       break;
