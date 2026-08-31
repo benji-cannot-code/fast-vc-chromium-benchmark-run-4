8cec0812413a4977ed1585f07df26859a2114248
Muhammad Salmaan <musalmaan@chromium.org>
2026-08-31 07:49:31 -0700

    [InfoBar] Fix include-what-you-use in collected cookies infobar
    TAG=agy
    
    Bug: 523208305
    Change-Id: I0d1f3a9f6b7c62229cc92698925230b6e55cfbb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8310104
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689079}

--- chrome/browser/ui/collected_cookies_infobar_delegate.cc
+++ chrome/browser/ui/collected_cookies_infobar_delegate.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/collected_cookies_infobar_delegate.h"
 
+#include <memory>
+
 #include "base/check_op.h"
 #include "build/build_config.h"
 #include "chrome/browser/infobars/confirm_infobar_creator.h"
