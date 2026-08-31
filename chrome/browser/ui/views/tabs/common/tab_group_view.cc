5c333ac237b182c7c32e79e91ebc3e2257137d4b
Caroline Rising <corising@chromium.org>
2026-08-31 07:52:27 -0700

    [Unified tabstrip] Fix layout issues.
    Fixes bug where sizes briefly change when dragging a tab into or out of
    a group due to the group trying to allocate its available space between
    its tabs during the animation for the tab being dragged in/out. Fixed by
    making sure the animating layout manager respects the available main
    axis space override so the group can calculate using target width.
    
    Fixes a layout space distribution bug caused by active and inactive tabs
    having different min widths which caused active tabs to always be
    proportionally given extra space. Now we make sure all tabs stay the
    same width down to the crossover point (the active tab's min width).
    
    Fixes bug where tabs briefly became larger when switching tabs in an
    overflowing tab strip. Fixed by making sure the unpinned container's
    horizontal layout always respects the available space override so
    unbounded layout queries do not calculate full preferred tab widths
    during interim layout calculations.
    
    Bug: 551936483, 552510187
    Change-Id: I33bed09f754c21d794ba9c934d3a7dea1a6cff30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8295962
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689083}

--- chrome/browser/ui/views/tabs/common/tab_group_view.cc
+++ chrome/browser/ui/views/tabs/common/tab_group_view.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/common/tab_strip_utils.h"
 #include "chrome/browser/ui/views/tabs/common/tab_strip_view.h"
 #include "chrome/browser/ui/views/tabs/common/tab_view.h"
+#include "chrome/browser/ui/views/tabs/common/unpinned_tab_container_view.h"
 #include "chrome/browser/ui/views/tabs/groups/tab_group_accessibility.h"
 #include "chrome/browser/ui/views/tabs/horizontal/horizontal_tab_closing_helper.h"
 #include "chrome/browser/ui/views/tabs/hovercard/tab_hover_card_controller.h"
@@ -235,6 +236,31 @@ bool TabGroupView::ShouldSnapToTarget(const views::View& child_view) const {
   return views::IsViewClass<SplitTabView>(&child_view);
 }
 
+void TabGroupView::SetAvailableSpace(views::SizeBound space) {
+  available_space_ = space;
+}
+
+int TabGroupView::GetCrossoverWidth() const {
+  if (collection_node_ &&
+      collection_node_->orientation() == TabStripOrientation::kHorizontal) {
+    return static_cast<TabGroupViewLayout*>(
+               layout_manager_->target_layout_manager())
+        ->CalculateHorizontalCrossoverWidth(this);
+  }
+  return GetMinimumSize().width();
+}
+
+std::optional<views::SizeBound>
+TabGroupView::GetAvailableMainAxisSpaceOverride() const {
+  if (collection_node_ &&
+      collection_node_->orientation() == TabStripOrientation::kHorizontal) {
+    if (available_space_.is_bounded()) {
+      return available_space_;
+    }
+  }
+  return std::nullopt;
+}
+
 void TabGroupView::OnAnimationEnded() {
   // For collapsed tab groups update child visibility only once animations have
   // completed. This allows tabs to remain visible as the group animates closed.
@@ -316,6 +342,9 @@ void TabGroupView::SetIsCollapsed(bool is_collapsed) {
     return;
   }
   is_collapsed_ = is_collapsed;
+  // Reset the cached available space so stale collapsed bounds (which only
+  // accommodated the header chip) do not constrain group tabs when expanding.
+  available_space_ = views::SizeBound();
   InvalidateLayout();
 }
 
