a3ed3031340ff876c665cff16e54c3a6717094ef
Thomas Nguyen <tungnh@chromium.org>
2026-08-31 03:07:20 -0700

    [Gardener] Disable OwnerConsentAndRegularSecondaryConsent
    ChromeOSPerUserOobeConsentTest.OwnerConsentAndRegularSecondaryConsent
    is failing on linux-chromeos-chrome builder.
    
    Bug: 555075193
    Change-Id: I9be409f6afbcfea9210afb6a2b2ea0c626c8b995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8319987
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688938}

--- chrome/browser/metrics/per_user_state_manager_chromeos_browsertest.cc
+++ chrome/browser/metrics/per_user_state_manager_chromeos_browsertest.cc
@@ -305,7 +305,7 @@ class ChromeOSPerUserOobeConsentTest : public ash::OobeBaseTest {
 };
 
 IN_PROC_BROWSER_TEST_F(ChromeOSPerUserOobeConsentTest,
-                       PRE_OwnerConsentAndRegularSecondaryConsent) {
+                       DISABLED_PRE_OwnerConsentAndRegularSecondaryConsent) {
   // Create and login as device owner.
   auto context = ash::LoginManagerMixin::CreateDefaultUserContext(
       ash::LoginManagerMixin::TestUserInfo(
@@ -335,7 +335,7 @@ IN_PROC_BROWSER_TEST_F(ChromeOSPerUserOobeConsentTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ChromeOSPerUserOobeConsentTest,
-                       OwnerConsentAndRegularSecondaryConsent) {
+                       DISABLED_OwnerConsentAndRegularSecondaryConsent) {
   auto context = ash::LoginManagerMixin::CreateDefaultUserContext(
       ash::LoginManagerMixin::TestUserInfo(
           AccountId::FromUserEmailGaiaId(kTestUser2, kTestUser2GaiaId)));
