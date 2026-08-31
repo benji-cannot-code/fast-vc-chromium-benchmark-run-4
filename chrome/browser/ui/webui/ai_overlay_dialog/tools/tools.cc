34eee5ddb62f07d777395b78c65f0c5f612d5e3d
Adithya Srinivasan <adithyas@chromium.org>
2026-08-31 13:48:07 -0700

    ttc: Use ToolController in AiOverlayTools behind a flag
    Allows us to manually test ToolController (and using ActorKeyedService
    for running tools) in the short term.
    
    Bug: b:544821996
    Change-Id: I94365a78864f0bd212364357285785818fb51275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8286568
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689226}

--- chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.cc
+++ chrome/browser/ui/webui/ai_overlay_dialog/tools/tools.cc
@@ -34,7 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
 #include "chrome/browser/ttc/resources/generated_tool_definitions.h"
+#include "chrome/browser/ttc/tool_controller.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/actor.mojom.h"
 #include "chrome/common/chrome_render_frame.mojom.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
@@ -128,7 +130,11 @@ AiOverlayTools::AiOverlayTools(
     PageContextMonitor* page_context_monitor)
     : receiver_(this, std::move(receiver)),
       browser_(browser),
-      page_context_monitor_(page_context_monitor) {}
+      page_context_monitor_(page_context_monitor) {
+  if (features::kAiOverlayDialogUsesActor.Get()) {
+    tool_controller_ = std::make_unique<ToolController>(browser_->GetProfile());
+  }
+}
 
 AiOverlayTools::~AiOverlayTools() = default;
 
@@ -142,6 +148,12 @@ void AiOverlayTools::OpenUrl(const std::string& url_string,
                              bool new_tab,
                              OpenUrlCallback callback) {
   RecordToolCallInvoked("OpenUrl");
+  if (tool_controller_) {
+    tool_controller_->OpenUrl(browser_, url_string, new_tab,
+                              std::move(callback));
+    return;
+  }
+
   GURL url(url_string);
   if (!url.is_valid()) {
     std::move(callback).Run(base::unexpected("Invalid URL"));
