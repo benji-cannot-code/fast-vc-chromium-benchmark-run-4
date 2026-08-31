876f47c7da8eee0a73e1a792929510a04700a4e1
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-08-31 14:04:00 -0700

    [layout] Remove LayoutObject::Style usage from style_engine.cc
    The style is null on a LayoutObject for a very small period of time.
    Most callers shouldn't have to check if the style is null.
    
    Switch to StyleRef().
    
    There should be no behaviour change.
    
    Change-Id: I7891d5a8e96ee194eb3506cd824ff7566599cbd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8325727
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689245}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -3737,7 +3737,7 @@ bool ContainerStyleChangesAllowed(Element& container,
   // the highlight styles hangs off the originating element's ComputedStyle.
   const ComputedStyle* new_element_style = container.GetComputedStyle();
   const ComputedStyle* new_layout_style =
-      container.GetLayoutObject() ? container.GetLayoutObject()->Style()
+      container.GetLayoutObject() ? &container.GetLayoutObject()->StyleRef()
                                   : nullptr;
 
   if (!new_element_style || !old_element_style) {
@@ -3781,7 +3781,7 @@ void StyleEngine::RecalcStyleForSizeContainer(Element& container,
 #if DCHECK_IS_ON()
   const ComputedStyle* old_element_style = container.GetComputedStyle();
   const ComputedStyle* old_layout_style =
-      container.GetLayoutObject() ? container.GetLayoutObject()->Style()
+      container.GetLayoutObject() ? &container.GetLayoutObject()->StyleRef()
                                   : nullptr;
 #endif  // DCHECK_IS_ON()
 
@@ -4737,7 +4737,7 @@ void StyleEngine::UpdateViewportStyle() {
 
   const ComputedStyle* viewport_style = resolver_->StyleForViewport();
   if (ComputedStyle::ComputeDifference(
-          viewport_style, GetDocument().GetLayoutView()->Style()) !=
+          viewport_style, &GetDocument().GetLayoutView()->StyleRef()) !=
       ComputedStyle::Difference::kEqual) {
     GetDocument().GetLayoutView()->SetStyle(viewport_style);
   }
