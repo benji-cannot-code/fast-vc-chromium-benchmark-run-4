e026d8526d7200044ae80b85931aa845b38b551e
Dominic Austria <dominicaustria@google.com>
2026-08-31 16:24:45 -0700

    [TabStripUnification] Put user action for settings appearance tab scroll buttons toggle
    Record user action when toggling the tab scroll buttons auto show /
    pinned pref in the appearance settings page.
    
    Bug: 537782348
    Change-Id: I324115566841d068309dd4d939d9bf8da7bd7c7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281844
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689389}

--- chrome/browser/ui/views/frame/system_menu_model_delegate.cc
+++ chrome/browser/ui/views/frame/system_menu_model_delegate.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/features.h"
+#include "chrome/browser/ui/tabs/tab_strip_prefs.h"
 #include "chrome/browser/ui/tabs/vertical_tab_strip_metrics.h"
 #include "chrome/browser/ui/tabs/vertical_tab_strip_state_controller.h"
 #include "chrome/browser/ui/ui_features.h"
