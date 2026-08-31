b30636261b4e4a36c557b6d55d6b5b74c080f372
Kristi Saney <kristislee@google.com>
2026-08-31 16:02:08 -0700

    [Reading mode] Remove clientHeight from isPointVisible calculation
    This was causing flakiness in tests and it is not needed.
    window.innerHeight is sufficient for this calculation.
    
    Bug: 502069860
    Change-Id: Iab1bf731d660cc006653c54d3be650fc7e8a9baa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8316011
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689363}

--- chrome/test/data/webui/side_panel/read_anything/rect_calculations_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/rect_calculations_test.ts
@@ -26,10 +26,9 @@ suite('RectCalculations', () => {
     let halfHeight: number;
 
     setup(() => {
-      windowHeight = document.documentElement.clientHeight || 600;
+      windowHeight = 600;
       halfHeight = windowHeight / 2;
-      setWindowSize(
-          windowHeight, document.documentElement.clientWidth || 1000);
+      setWindowSize(windowHeight, 1000);
     });
 
     test('fully inside window returns true', () => {
@@ -78,11 +77,10 @@ suite('RectCalculations', () => {
     let minorityHeight: number;
 
     setup(() => {
-      windowHeight = document.documentElement.clientHeight || 600;
+      windowHeight = 600;
       majorityHeight = windowHeight * MOSTLY_VISIBLE_PERCENT;
       minorityHeight = windowHeight - majorityHeight;
-      setWindowSize(
-          windowHeight, document.documentElement.clientWidth || 1000);
+      setWindowSize(windowHeight, 1000);
     });
 
     test('fully inside window returns true', () => {
