0231969416406008877ce57e56085e4f7b3cb516
Jason Hu <hujasonx@google.com>
2026-08-31 13:19:03 -0700

    [context-input] Bypass Android image downscaling for C2PA metadata
    When an image attachment is processed on Android with downscaling
    enabled, it is decoded into a Bitmap and recompressed, which strips
    ancillary metadata chunks including C2PA headers.
    
    This adds a check for the C2PA marker in the first 256KB of the image
    stream and skips the Java-side downscaling if present, allowing the raw
    file bytes to reach C++ intact.
    
    Bug: b:554089347
    Change-Id: Id76352d51169379371da18587ff4b6b1e557ca37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8316407
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689196}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -537,6 +537,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &language::kCctAutoTranslate,
     &language::kDetailedLanguageSettings,
     &language::kGmsCoreUlp,
+    &lens::features::kLensBypassCompressionForC2pa,
     &lens::features::kLensOverlayAndroid,
     &lens::features::kLensSendRawFileMediaTypes,
     &media::kAutoDocPiPPermissionPromptAndroid,
