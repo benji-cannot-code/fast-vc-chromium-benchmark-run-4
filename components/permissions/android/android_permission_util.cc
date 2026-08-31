15922473085d10fcbfb78aad605c19c75726fe92
Calder Kitagawa <ckitagawa@chromium.org>
2026-08-31 13:57:37 -0700

    Fix crashes in modal dialog/permissions
    Add null checks for when WindowAndroid is null during destruction or on
    reparenting.
    
    Bug: 553909996
    Change-Id: If932f90d1e62c2b9f288322c4f7056ddb1466487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8324307
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689236}

--- components/permissions/android/android_permission_util.cc
+++ components/permissions/android/android_permission_util.cc
@@ -73,6 +73,9 @@ void AppendOptionalAndroidPermissionsForContentSetting(
 bool HasRequiredAndroidPermissionsForContentSetting(
     ui::WindowAndroid* window_android,
     ContentSettingsType content_settings_type) {
+  if (!window_android) {
+    return false;
+  }
   JNIEnv* env = base::android::AttachCurrentThread();
   return Java_AndroidPermissionRequester_hasRequiredAndroidPermissionsForContentSetting(
       env, window_android, static_cast<int>(content_settings_type));
@@ -185,7 +188,9 @@ bool HasSystemPermission(ContentSettingsType type,
     return false;
   }
   auto* window_android = web_contents->GetNativeView()->GetWindowAndroid();
-  DCHECK(window_android);
+  if (!window_android) {
+    return false;
+  }
 
   return HasRequiredAndroidPermissionsForContentSetting(window_android, type);
 }
@@ -199,9 +204,11 @@ bool CanRequestSystemPermission(ContentSettingsType type,
       !IsSystemLocationSettingEnabled()) {
     return false;
   }
-  JNIEnv* env = base::android::AttachCurrentThread();
   auto* window_android = web_contents->GetNativeView()->GetWindowAndroid();
-  DCHECK(window_android);
+  if (!window_android) {
+    return false;
+  }
+  JNIEnv* env = base::android::AttachCurrentThread();
   return Java_PermissionUtil_canRequestSystemPermission(
       env, static_cast<int>(type), window_android);
 }
