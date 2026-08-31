a2d588cb4a95c1376b8efae0e7bc0e52561d9737
Bryan Kim <bryankim@google.com>
2026-08-31 13:46:54 -0700

    [Bedrock] Modularize views dialogs into dedicated targets
    Modularize the following leaf modules from //chrome/browser/ui into
    //chrome/browser/ui/views:
    - :global_error_bubble_view
    - :upgrade_notification_controller
    - :incognito_clear_browsing_data_dialog (including coordinator)
    - :task_manager_view
    
    AGENT_GENERATED_CL
    TAG=agy
    CONV=a30875d2-9c4c-481f-a36b-b0f928bd01e5
    
    Bug: 353332589
    Change-Id: I4030903a3b1a4a6df4554d5beee74079c16c0fb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8319290
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689224}

--- chrome/browser/ui/views/incognito_clear_browsing_data_dialog.cc
+++ chrome/browser/ui/views/incognito_clear_browsing_data_dialog.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lifetime/application_lifetime_desktop.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/views/accessibility/theme_tracking_non_accessible_image_view.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
