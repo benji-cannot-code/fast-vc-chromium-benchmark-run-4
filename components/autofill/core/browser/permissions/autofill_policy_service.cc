d0d055b1e19f977cec433df933737b509b778984
Luchen Peng <luchenpeng@google.com>
2026-08-31 08:12:28 -0700

    [EnterprisePolicy] Disable FormsAI types if legacy AutofillAddressEnabled is disabled by admin.
    Legacy AutofillAddressEnabled also disable forms ai types
    (identity_docs, travel), we need to keep the legacy behavior if
    enterprise admin set AutofillAddressEnabled to `false`.
    
    Bug: b:466345561
    Change-Id: Ib9c07d177474484277401b78bc8ab41ddf2d7156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8321031
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Luchen Peng <luchenpeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689096}

--- components/autofill/core/browser/permissions/autofill_policy_service.cc
+++ components/autofill/core/browser/permissions/autofill_policy_service.cc
@@ -41,11 +41,25 @@ bool IsCategoryGloballyBlocked(
       }
       break;
     case AutofillClient::AutofillPolicyDataCategory::kIdentityDocs:
+      // Due to the legacy AutofillAddressEnabled policy, if
+      // AutofillAddressEnabled is disabled by an enterprise admin, identity
+      // docs are also blocked by policy.
+      if (prefs.IsManagedPreference(prefs::kAutofillProfileEnabled) &&
+          !prefs.GetBoolean(prefs::kAutofillProfileEnabled)) {
+        return true;
+      }
       if (!prefs.GetBoolean(prefs::kAutofillAiIdentityEntitiesEnabled)) {
         return true;
       }
       break;
     case AutofillClient::AutofillPolicyDataCategory::kTravel:
+      // Due to the legacy AutofillAddressEnabled policy, if
+      // AutofillAddressEnabled is disabled by an enterprise admin, travel
+      // information is also blocked by policy.
+      if (prefs.IsManagedPreference(prefs::kAutofillProfileEnabled) &&
+          !prefs.GetBoolean(prefs::kAutofillProfileEnabled)) {
+        return true;
+      }
       if (!prefs.GetBoolean(prefs::kAutofillAiTravelEntitiesEnabled)) {
         return true;
       }
@@ -187,6 +201,15 @@ bool AutofillPolicyService::IsAutofillTypeDisabledByEnterprisePolicy(
   if (!base::FeatureList::IsEnabled(
           features::kAutofillEnableAutofillSettingsEnterprisePolicy)) {
     return false;
+  }
+
+  // If the legacy AutofillAddressEnabled policy is disabled by the enterprise
+  // admin, identity docs and travel are disabled as well.
+  if ((category == AutofillClient::AutofillPolicyDataCategory::kIdentityDocs ||
+       category == AutofillClient::AutofillPolicyDataCategory::kTravel) &&
+      prefs.IsManagedPreference(prefs::kAutofillProfileEnabled) &&
+      !prefs.GetBoolean(prefs::kAutofillProfileEnabled)) {
+    return true;
   }
 
   const base::ListValue& policy_list =
