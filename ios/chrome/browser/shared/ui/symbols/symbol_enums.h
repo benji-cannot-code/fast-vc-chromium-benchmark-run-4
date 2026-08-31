923c48deca377dc8b39e58c11a1ee7d68a41826a
Elmehdi Rahmaoui <erahmaoui@google.com>
2026-08-31 08:15:25 -0700

    Revert "[iOS] Add field trial testing config for Gemini Live"
    This reverts commit 0a679956372a8ec3817d9355334037c3598e6e44.
    
    Reason for revert: Enabling the HeliosLive field trial testing config
    (enabling features GeminiLive and GeminiLiveDormantReasons on iOS) in
    testing/variations/fieldtrial_testing_config.json caused 36 EarlGrey
    tests in ios_internal_chrome_external_url_eg2tests_module to
    consistently fail across all iOS simulator and physical device bots
    (e.g. iphone-simulator, ipad-simulator, iphone-device, ipad-device).The
    tests time out waiting for the standard floaty text send button: "Failed
    waiting for element with matcher
    (respondsToSelector(accessibilityIdentifier) &&
    accessibilityID('kGMNFloatyInputSendButtonAXID')) to become visible"
    because enabling GeminiLive replaces the standard Floaty text input /
    send button with the Gemini Live voice interface. First failing build:
    https://ci.chromium.org/b/8672191649117897793 (iphone-simulator build
    #83095)
    
    Original change's description:
    > [iOS] Add field trial testing config for Gemini Live
    >
    > And change the non branded live symbol
    >
    > Add fieldtrial testing configuration for kGeminiLive and
    > kGeminiLiveDormantReasons on iOS.
    >
    > Had to rename the existing symbol and add a new one as a non branded
    > icon for gemini live, since the existing one is only available post ios
    > 26 (was causing test failures) instead of adding the existing logo
    > conditionally.
    >
    > Bug: 552529404
    > Change-Id: I41b0075e3c858df19a35182861565170604b7bf9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8289711
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Yasaman Sedaghat <yasamans@google.com>
    > Reviewed-by: Adam Arcaro <adamta@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1688217}
    
    Bug: 552529404
    Change-Id: I2977afe271480acc5042242a6e5d5d6d4e9e8251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8322168
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Owners-Override: Elmehdi Rahmaoui <erahmaoui@google.com>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689101}

--- ios/chrome/browser/shared/ui/symbols/symbol_enums.h
+++ ios/chrome/browser/shared/ui/symbols/symbol_enums.h
@@ -345,7 +345,6 @@ typedef NS_ENUM(NSInteger, Symbol) {
   SymbolWarningShield,
   SymbolWandAndSparkles,
   SymbolWaveform,
-  SymbolWaveformMid,
   SymbolWifi,
   SymbolWork,
   SymbolWrenchAndScrewdriver,
