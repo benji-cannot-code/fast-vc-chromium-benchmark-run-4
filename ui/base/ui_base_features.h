5c3cd5a5525ba6b376fda8575c29d72807a4f4bd
Thomas Lukaszewicz <tluk@chromium.org>
2026-08-30 21:02:34 -0700

    [color] Add ColorIdCssStyleSheetOptimization feature flag
    Declare and define `kColorIdCssStyleSheetOptimization` and
    `IsColorIdCssStyleSheetOptimizationEnabled()` in ui_base_features
    to gate Phase 1 WebUI colors.css optimizations.
    
    TAG=agy
    CONV=55ba0da9-6321-48f2-bb41-2cf6fd5ead85
    
    Bug: 554934009
    Change-Id: Ic68807fac055a6103599c951c43729057ee78e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8319107
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688818}

--- ui/base/ui_base_features.h
+++ ui/base/ui_base_features.h
@@ -356,6 +356,9 @@ BASE_DECLARE_FEATURE(kUsePortalAccentColor);
 COMPONENT_EXPORT(UI_BASE_FEATURES)
 BASE_DECLARE_FEATURE(kThemeChangeOptimization);
 
+COMPONENT_EXPORT(UI_BASE_FEATURES)
+BASE_DECLARE_FEATURE(kColorIdCssStyleSheetOptimization);
+
 // Kill switch for removing
 // RenderFrameHostManager::EnsureRenderFrameHostVisibilityConsistent.
 //
@@ -370,6 +373,8 @@ COMPONENT_EXPORT(UI_BASE_FEATURES)
 bool IsRoundedIconsEnabled();
 COMPONENT_EXPORT(UI_BASE_FEATURES)
 bool IsWebUIRoundedIconsEnabled();
+COMPONENT_EXPORT(UI_BASE_FEATURES)
+bool IsColorIdCssStyleSheetOptimizationEnabled();
 
 }  // namespace features
 
