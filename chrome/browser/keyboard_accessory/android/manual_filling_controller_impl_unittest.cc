909406b896dc04c9fd56a0472d91bd853f7fa6a0
Piotr Kotynia <piotrkotynia@google.com>
2026-08-31 05:40:45 -0700

    [AndroidDesktop] Suppress empty Keyboard Accessory on all fields
    Extend AutofillAndroidDesktopSuppressAccessoryOnEmpty to suppress the
    keyboard accessory for all field types when there are no autofill
    suggestions, including username, password, and contenteditable fields.
    
    Previously, ShouldShowOnLargeFormFactor explicitly exempted credential
    fields (kFillableUsernameField, kFillablePasswordField) and
    contenteditable fields (kContenteditableField), showing the accessory
    even when no autofill suggestions were available. This change removes
    those exceptions so that the accessory is only shown on large form
    factors / desktop when active suggestions are present
    (available_sources_.contains(FillingSource::AUTOFILL)).
    
    Bug: 430304112
    Change-Id: I4e63b2038f492306c28342ade95d6f808039decc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8314808
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Luchen Peng <luchenpeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1688994}

--- chrome/browser/keyboard_accessory/android/manual_filling_controller_impl_unittest.cc
+++ chrome/browser/keyboard_accessory/android/manual_filling_controller_impl_unittest.cc
@@ -198,7 +198,7 @@ TEST_F(ManualFillingControllerTest,
   EXPECT_CALL(*view(), OnItemsAvailable(filled_passwords_sheet()))
       .Times(AnyNumber());
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(false)));
 
   NotifyPasswordSourceObserver(IsFillingSourceAvailable(true));
@@ -349,10 +349,21 @@ TEST_F(ManualFillingControllerTest,
   FocusFieldAndClearExpectations(FocusedFieldType::kFillablePasswordField);
 
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(false)));
 
   controller()->UpdateSourceAvailability(FillingSource::PASSWORD_FALLBACKS,
+                                         /*has_suggestions=*/true);
+}
+
+TEST_F(ManualFillingControllerTest,
+       ShowsAccessoryWhenAutofillSourceAvailableOnCredentialFields) {
+  FocusFieldAndClearExpectations(FocusedFieldType::kFillablePasswordField);
+
+  EXPECT_CALL(*view(),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+                   IsContentEditable(false)));
+  controller()->UpdateSourceAvailability(FillingSource::AUTOFILL,
                                          /*has_suggestions=*/true);
 }
 
@@ -499,7 +510,7 @@ TEST_F(ManualFillingControllerTest, LogsHistogramOnOptionSelected) {
 // Tests that focusing a contenteditable element shows the keyboard accessory.
 TEST_F(ManualFillingControllerTest, ShowsAccessoryForContentEditableField) {
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(true)));
 
   controller()->NotifyFocusedInputChanged(
@@ -520,7 +531,7 @@ TEST_F(ManualFillingControllerTest,
       .WillRepeatedly(Return(kTestSheet));
   EXPECT_CALL(*view(), OnItemsAvailable(kTestSheet)).Times(AtLeast(1));
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(true)));
 
   NotifyAtMemorySourceObserver(IsFillingSourceAvailable(true));
@@ -531,7 +542,7 @@ TEST_F(ManualFillingControllerTest,
 TEST_F(ManualFillingControllerTest,
        HidesAccessoryWhenFocusLeavesContentEditable) {
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(true)));
 
   controller()->NotifyFocusedInputChanged(
@@ -571,7 +582,7 @@ TEST_F(ManualFillingControllerTest,
   EXPECT_CALL(*view(), OnItemsAvailable(kTestCreditCardSheet)).Times(0);
   EXPECT_CALL(*view(), OnItemsAvailable(kTestAtMemorySheet)).Times(AtLeast(1));
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(true)));
 
   controller()->UpdateSourceAvailability(FillingSource::PASSWORD_FALLBACKS,
@@ -602,7 +613,7 @@ TEST_F(ManualFillingControllerTest,
   // Focus contenteditable: address sheet is suppressed.
   EXPECT_CALL(*view(), OnItemsAvailable(kTestAddressSheet)).Times(0);
   EXPECT_CALL(*view(),
-              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(true),
+              Show(WaitForKeyboard(true), ShouldShowOnLargeFormFactor(false),
                    IsContentEditable(true)));
   controller()->NotifyFocusedInputChanged(
       kFocusedFieldId, FocusedFieldType::kContenteditableField);
