60df3d263dcb1af6895b5e4ac7a24f51c9643959
Emily Shack <emshack@chromium.org>
2026-09-01 11:14:16 -0700

    [ChroMenu GlowUp] Add bookmarks static menu items
    Bug: 526712073
    Change-Id: I734619592569909bcba9caaaa30f8523e7e89774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8313109
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689969}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -3428,6 +3428,11 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
               },
               bwi))
           .SetActionId(kActionReadingListMenuAddTab)
+          .SetText(l10n_util::GetStringUTF16(IDS_READING_LIST_MENU_ADD_TAB))
+          .SetImage(ui::ImageModel::FromVectorIcon(
+              features::IsRoundedIconsEnabled() ? kListAltAddIcon
+                                                : kReadLaterAddOldIcon,
+              ui::kColorIcon))
           .Build());
 
   root_action_item_->AddChild(
@@ -3934,6 +3939,11 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
               },
               bwi))
           .SetActionId(kActionImportSettings)
+          .SetText(l10n_util::GetStringUTF16(IDS_IMPORT_SETTINGS_MENU_LABEL))
+          .SetImage(ui::ImageModel::FromVectorIcon(
+              features::IsRoundedIconsEnabled() ? kMenuBookIcon
+                                                : kMenuBookChromeRefreshOldIcon,
+              ui::kColorIcon))
           .Build());
 
   root_action_item_->AddChild(
@@ -4185,6 +4195,11 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
               },
               bwi))
           .SetActionId(kActionShowBookmarkBar)
+          .SetText(l10n_util::GetStringUTF16(IDS_SHOW_BOOKMARK_BAR))
+          .SetImage(ui::ImageModel::FromVectorIcon(
+              features::IsRoundedIconsEnabled() ? kToolbarIcon
+                                                : kToolbarChromeRefreshOldIcon,
+              ui::kColorIcon))
           .Build());
 
   root_action_item_->AddChild(
@@ -4242,6 +4257,13 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
               },
               bwi))
           .SetActionId(kActionShowBookmarkManager)
+          .SetText(l10n_util::GetStringUTF16(
+              features::IsMenuSimplificationEnabled() ? IDS_BOOKMARK_MANAGER_V2
+                                                      : IDS_BOOKMARK_MANAGER))
+          .SetImage(ui::ImageModel::FromVectorIcon(
+              features::IsRoundedIconsEnabled() ? kBookmarkManagerIcon
+                                                : kBookmarksManagerOldIcon,
+              ui::kColorIcon))
           .Build());
 
   root_action_item_->AddChild(
@@ -4925,8 +4947,8 @@ void BrowserActions::InitializeSubmenuActions() {
               bwi),
           kActionBookmarkBarSubmenu, IDS_BOOKMARK_BAR_SUBMENU_LABEL,
           IDS_BOOKMARK_BAR_SUBMENU_LABEL,
-          features::IsRoundedIconsEnabled() ? kStarIcon
-                                            : kBookmarksListsMenuOldIcon,
+          features::IsRoundedIconsEnabled() ? kToolbarIcon
+                                            : kToolbarChromeRefreshOldIcon,
           /*is_pinnable=*/false)
           .Build());
 
