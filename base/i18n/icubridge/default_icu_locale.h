6dd21811a4e0a3b45e2e5e576ed9de0b819246fd
Evan Luo <evanluo@google.com>
2026-09-01 12:01:59 -0700

    Revert "Call SetDefaultIcuLocale from the deprecated SetICUDefaultLocale"
    This reverts commit 65820e4ae4598ad1a801c82b89373f4867b2e4d7.
    
    Reason for revert: Broke tree
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/Win%20Builder%20%28dbg%29/235944
    
    Original change's description:
    > Call SetDefaultIcuLocale from the deprecated SetICUDefaultLocale
    >
    > This is being done so it is possible to call the new
    > GetDefaultIcuLocale() safely while we do not migrate everyone to use the
    > new setter and remove SetICUDefaultLocale from the codebase.
    >
    > This is also being done to solve a performance regression.
    >
    > Change-Id: Iad8bd0e277a6d8f2ec1a379750256bd2c101a09d
    > Bug: 542711615
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8337852
    > Commit-Queue: Danilo Tedeschi <danft@google.com>
    > Reviewed-by: Paula Gómez <paugomez@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1689985}
    
    Bug: 542711615
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ibd8b1a7759c2c9ec6d3036feeab85e5b19e31143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8338430
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Evan Luo <evanluo@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689997}

--- base/i18n/icubridge/default_icu_locale.h
+++ base/i18n/icubridge/default_icu_locale.h
@@ -51,7 +51,6 @@ std::string GetApplicationLocale(std::string_view, bool);
 namespace base::i18n {
 
 class ScopedDefaultIcuLocale;
-void SetICUDefaultLocale(std::string_view);
 
 // A capability token enforcing the C++ pass-key pattern for setting the
 // mutable default ICU locale.
@@ -73,7 +72,6 @@ class BASE_I18N_EXPORT DefaultIcuLocaleSetterKey {
   friend class ::WebEngineMainDelegate;
   friend class ::WebEngineBrowserMainParts;
   friend std::string(::l10n_util::GetApplicationLocale)(std::string_view, bool);
-  friend void SetICUDefaultLocale(std::string_view);
 
   DefaultIcuLocaleSetterKey() = default;
 };
