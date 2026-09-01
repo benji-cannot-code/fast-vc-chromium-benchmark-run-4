31dfcba05df0e4739925fe746e9a9be781f0c9d1
Evan Luo <evanluo@google.com>
2026-09-01 14:32:06 -0700

    Revert "[WebUI NTP on AL] Enable WebUI NTP by default on AL"
    This reverts commit e0d32564af2098a45215427491265c53e33bec39.
    
    Reason for revert: Many tests failing on android desktop bots in CI (crbug.com/555833714)
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-17-x64-rel-emu-tests/3350
    
    Original change's description:
    > [WebUI NTP on AL] Enable WebUI NTP by default on AL
    >
    > This change enables both UseWebUiNtpAndroid and WebUiAndroidTheming
    > features by default in preparation for the WebUI NTP launch.
    >
    > Bug: 520067577
    > Co-authored-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Change-Id: I531bf4bad3a5efb00d26652d43ed482b604ebc07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8328510
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Commit-Queue: Gianni Chen <giannich@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1689925}
    
    Bug: 520067577
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If4253ba077efd45b5a0b7adcdee238824bf3a9ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8338694
    Commit-Queue: Evan Luo <evanluo@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690151}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/ToolbarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/ToolbarTest.java
@@ -653,8 +653,6 @@ public class ToolbarTest {
     @EnableFeatures(
             ChromeFeatureList.HOME_BUTTON_REMOVAL
                     + ":set_default_to_false_on_homepage_on_desktop/false")
-    // TODO(b/555414915): Update Android tests with WebUI NTP enabled on AL.
-    @DisableFeatures(ChromeFeatureList.USE_WEB_UI_NTP_ANDROID)
     @ImportantFormFactors(DeviceFormFactor.TABLET_OR_DESKTOP)
     public void testHomeButton_loadsNtpOnSameTab() {
         WebPageStation webPage = mPage;
@@ -682,11 +680,7 @@ public class ToolbarTest {
 
     @Test
     @LargeTest
-    @DisableFeatures({
-        ChromeFeatureList.ANDROID_BOTTOM_BAR,
-        // TODO(b/555414915): Update Android tests with WebUI NTP enabled on AL.
-        ChromeFeatureList.USE_WEB_UI_NTP_ANDROID
-    })
+    @DisableFeatures(ChromeFeatureList.ANDROID_BOTTOM_BAR)
     @EnableFeatures({ChromeFeatureList.HOME_BUTTON_REMOVAL + ":keep_home_button_on_ntp/true"})
     public void testHomeButtonVisibility_KeepOnNtp() {
         ThreadUtils.runOnUiThreadBlocking(
@@ -709,11 +703,7 @@ public class ToolbarTest {
 
     @Test
     @LargeTest
-    @DisableFeatures({
-        ChromeFeatureList.ANDROID_BOTTOM_BAR,
-        // TODO(giannich): Update Android tests with WebUI NTP enabled on AL.
-        ChromeFeatureList.USE_WEB_UI_NTP_ANDROID
-    })
+    @DisableFeatures(ChromeFeatureList.ANDROID_BOTTOM_BAR)
     @EnableFeatures({
         ChromeFeatureList.HOME_BUTTON_REMOVAL
                 + ":keep_home_button_on_ntp/true/set_default_to_false_on_homepage_on_desktop/false"
