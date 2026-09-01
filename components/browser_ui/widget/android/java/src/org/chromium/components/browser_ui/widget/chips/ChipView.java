9df71d90bdf3d4c9c9de4f9eaa009d22f96e9783
Peter Kang <peterkang@google.com>
2026-09-01 13:11:38 -0700

    Clean up ChipView layout direction handling and update render tests
    This CL removes the custom updateLayoutDirection method from ChipView,
    which was forcing layout direction based on LocalizationUtils. Instead,
    ChipViewRenderTest and KeyboardAccessoryChipViewRenderTest are updated
    to set the layout direction on their test container views. This allows
    removing the layout direction overrides in SearchBoxContainerView and
    LocationBarLayout.
    
    Bug: 544731730
    Change-Id: I588309f4a09d9bd02c1523c341287bbf84076433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8301552
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Peter Kang <peterkang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690046}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipView.java
@@ -39,7 +39,6 @@ import org.chromium.build.annotations.MonotonicNonNull;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.widget.R;
-import org.chromium.ui.base.LocalizationUtils;
 import org.chromium.ui.widget.LoadingView;
 import org.chromium.ui.widget.RectProvider;
 import org.chromium.ui.widget.RippleBackgroundHelper;
@@ -333,7 +332,6 @@ public class ChipView extends LinearLayout {
             setIconWithTint(INVALID_ICON_ID, /* tintWithTextColor= */ false);
         }
 
-        updateLayoutDirection();
         a.recycle();
     }
 
@@ -506,7 +504,6 @@ public class ChipView extends LinearLayout {
         // Remove the end padding from the chip to make X icon touch target extend till the end of
         // the chip.
         this.setPaddingRelative(getPaddingStart(), getPaddingTop(), 0, getPaddingBottom());
-        updateLayoutDirection();
     }
 
     /** Adds a dropdown icon at the trailing end of the chip next to the primary text. */
@@ -534,7 +531,6 @@ public class ChipView extends LinearLayout {
         // Remove the end padding from the chip to make X icon touch target extend till the end of
         // the chip.
         this.setPaddingRelative(getPaddingStart(), getPaddingTop(), 0, getPaddingBottom());
-        updateLayoutDirection();
     }
 
     /**
@@ -636,7 +632,6 @@ public class ChipView extends LinearLayout {
             } else {
                 addView(mSecondaryText);
             }
-            updateLayoutDirection();
         }
         return mSecondaryText;
     }
@@ -793,15 +788,6 @@ public class ChipView extends LinearLayout {
         return mTextViewsWrapper == null;
     }
 
-    private void updateLayoutDirection() {
-        // Apply RTL layout changes, this is mostly relevant for render tests.
-        int layoutDirection =
-                LocalizationUtils.isLayoutRtl()
-                        ? View.LAYOUT_DIRECTION_RTL
-                        : View.LAYOUT_DIRECTION_LTR;
-
-        setLayoutDirection(layoutDirection);
-    }
 
     /**
      * Set a handler to be invoked when the selection state of the chip changes.
--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipViewRenderTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/chips/ChipViewRenderTest.java
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.browser_ui.widget.chips;
 
 import static org.chromium.base.ThreadUtils.runOnUiThreadBlocking;
-import static org.chromium.ui.base.LocalizationUtils.setRtlForTesting;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
@@ -67,13 +67,14 @@ public class ChipViewRenderTest {
     public final RenderTestRule mRenderTestRule =
             RenderTestRule.Builder.withPublicCorpus()
                     .setBugComponent(Component.UI_BROWSER_MOBILE)
-                    .setRevision(3)
+                    .setRevision(4)
                     .build();
 
+    private final boolean mUseRtlLayout;
     private ViewGroup mContentView;
 
     public ChipViewRenderTest(boolean nightModeEnabled, boolean useRtlLayout) {
-        setRtlForTesting(useRtlLayout);
+        mUseRtlLayout = useRtlLayout;
         NightModeTestUtils.setUpNightModeForBlankUiTestActivity(nightModeEnabled);
         mRenderTestRule.setNightModeEnabled(nightModeEnabled);
         mRenderTestRule.setVariantPrefix(useRtlLayout ? "RTL" : "LTR");
@@ -89,6 +90,9 @@ public class ChipViewRenderTest {
                             LinearLayout contentView = new LinearLayout(activity);
                             contentView.setOrientation(LinearLayout.VERTICAL);
                             contentView.setBackgroundColor(Color.WHITE);
+                            if (mUseRtlLayout) {
+                                contentView.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+                            }
 
                             activity.setContentView(
                                     contentView,
