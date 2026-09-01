39de4d0751589b8414fbd2eb3aee504e5a17e355
Colin Blundell <blundell@chromium.org>
2026-09-01 14:03:03 -0700

    [Blink] Remove line drawing as paths check from CanvasNon2DRP
    Disabling line drawing as paths for Graphite in CanvasNon2DRP
    was carried over from CanvasResourceProvider. However, the
    line-drawing-as-path batching heuristic in RecordPaintCanvas was an
    optimization specifically for 2D canvas vector lines in Ganesh for
    MotionMark. CanvasNon2DResourceProvider is used for non-2D contexts
    (WebGL, WebGPU, video frames, etc.) which never draw 2D vector
    lines/paths.
    
    Bug: 352263194
    Change-Id: If5e6bd8dc2a307e79a48734c84a948e21374207f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8318587
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690109}

--- third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_non_2d_resource_provider.cc
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/common/capabilities.h"
 #include "gpu/command_buffer/common/shared_image_capabilities.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
-#include "gpu/config/gpu_feature_info.h"
-#include "gpu/config/gpu_feature_type.h"
 #include "skia/ext/legacy_display_globals.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/web_graphics_shared_image_interface_provider.h"
@@ -251,13 +249,6 @@ CanvasNon2DResourceProvider::CanvasNon2DResourceProvider(
     context_provider_wrapper_->AddObserver(this);
     raster_context_provider_ = base::WrapRefCounted(
         context_provider_wrapper_->ContextProvider().RasterContextProvider());
-    // Graphite can handle a large buffer size.
-    if (context_provider_wrapper_->ContextProvider()
-            .GetGpuFeatureInfo()
-            .status_values[gpu::GPU_FEATURE_TYPE_SKIA_GRAPHITE] ==
-        gpu::kGpuFeatureStatusEnabled) {
-      recorder_for_external_draws_->DisableLineDrawingAsPaths();
-    }
   }
 
   if (raster_context_provider_) {
