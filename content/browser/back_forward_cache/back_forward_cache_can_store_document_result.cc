d4d99416c2775478d2246f4443cb7c6d01b70477
Yao Xiao <yaoxia@chromium.org>
2026-08-31 22:23:02 -0700

    [AdTagging] Enforce one-way tagging and introduce FrameAdStatus
    Explicitly enforce one-way ad tagging in related APIs. The original
    code included support for untagging frames, but this functionality was
    never enabled, and we do not plan to support untagging any time soon.
    This CL removes the dead code to prevent confusion.
    
    To lay the groundwork for tracking additional ad sub-types (e.g.,
    video ads), this CL also introduces the `FrameAdStatus` enum (`kNotAd`,
    `kAd`) and updates the internal frame replication state and
    `RemoteFrame` tagging API to use it.
    
    Note: `RenderFrameHostImpl::UpdateToAdFrame` is deliberately not
    updated to use this enum. It remains orthogonal to the upcoming video ad
    tagging code path: `UpdateToAdFrame()` is invoked from the component
    layer during the "ReadyToCommit" stage, whereas the upcoming
    `UpdateToVideoAdFrame()` will be invoked directly via
    `mojom.LocalFrameHost`.
    
    Bug: 552371621
    Change-Id: If37556a4ee775e1098222ea1f951594726199098
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8315747
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689628}

--- content/browser/back_forward_cache/back_forward_cache_can_store_document_result.cc
+++ content/browser/back_forward_cache/back_forward_cache_can_store_document_result.cc
@@ -218,8 +218,8 @@ ProtoEnum::BackForwardCacheNotRestoredReason NotRestoredReasonToTraceEnum(
     case Reason::kRfhHadStickyUserActivationBeforeNavigationChanged:
       return ProtoEnum::
           RFH_HAD_STICKY_USER_ACTIVATION_BEFORE_NAVIGATION_CHANGED;
-    case Reason::kRfhUpdateIsAdFrame:
-      return ProtoEnum::RFH_UPDATE_IS_AD_FRAME;
+    case Reason::kRfhUpdateAdFrameStatus:
+      return ProtoEnum::RFH_UPDATE_AD_FRAME_STATUS;
   }
   NOTREACHED();
 }
@@ -492,8 +492,8 @@ std::string BackForwardCacheCanStoreDocumentResult::NotRestoredReasonToString(
       return "Rfh: EnforceInsecureRequestPolicy";
     case Reason::kRfhHadStickyUserActivationBeforeNavigationChanged:
       return "Rfh: HadStickyUserActivationBeforeNavigationChanged";
-    case Reason::kRfhUpdateIsAdFrame:
-      return "Rfh: UpdateIsAdFrame";
+    case Reason::kRfhUpdateAdFrameStatus:
+      return "Rfh: UpdateAdFrameStatus";
   }
 }
 
@@ -624,7 +624,7 @@ BackForwardCacheCanStoreDocumentResult::NotRestoredReasonToReportString(
     case Reason::kRfhEnforceInsecureNavigationsSet:
     case Reason::kRfhEnforceInsecureRequestPolicy:
     case Reason::kRfhHadStickyUserActivationBeforeNavigationChanged:
-    case Reason::kRfhUpdateIsAdFrame:
+    case Reason::kRfhUpdateAdFrameStatus:
     case Reason::kUnknown:
       return "masked";
   }
--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -2179,7 +2179,7 @@ Page::BackForwardCacheNotRestoredReason NotRestoredReasonToProtocol(
     case Reason::kRfhEnforceInsecureNavigationsSet:
     case Reason::kRfhEnforceInsecureRequestPolicy:
     case Reason::kRfhHadStickyUserActivationBeforeNavigationChanged:
-    case Reason::kRfhUpdateIsAdFrame:
+    case Reason::kRfhUpdateAdFrameStatus:
       return Page::BackForwardCacheNotRestoredReasonEnum::Unknown;
     case Reason::kCacheControlNoStoreDeviceBoundSessionTerminated:
       return Page::BackForwardCacheNotRestoredReasonEnum::
@@ -2509,7 +2509,7 @@ Page::BackForwardCacheNotRestoredReasonType MapNotRestoredReasonToType(
     case Reason::kRfhEnforceInsecureNavigationsSet:
     case Reason::kRfhEnforceInsecureRequestPolicy:
     case Reason::kRfhHadStickyUserActivationBeforeNavigationChanged:
-    case Reason::kRfhUpdateIsAdFrame:
+    case Reason::kRfhUpdateAdFrameStatus:
     case Reason::kUnknown:
       return Page::BackForwardCacheNotRestoredReasonTypeEnum::SupportPending;
     case Reason::kBlocklistedFeatures:
