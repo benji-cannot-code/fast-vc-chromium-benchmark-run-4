b9346287ad458c7e3b0b7f779064236bd6af4c62
Daniel Angulo <angdaniel@google.com>
2026-08-31 17:04:37 -0700

    Spanification of partition_alloc_base/posix/safe_strerror.cc
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli and at last manually refined
    
    Gemini-CLI auto-fixes for compilation
    
    Bug: 505042664
    Change-Id: I98ca0d14df25278bc980aa31ad546392c4ee2488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7834548
    Commit-Queue: Daniel Angulo <angdaniel@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689424}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/posix/safe_strerror.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/posix/safe_strerror.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "partition_alloc/partition_alloc_base/component_export.h"
+#include "partition_alloc/partition_alloc_base/containers/span.h"
 
 namespace partition_alloc::internal::base {
 
@@ -30,7 +31,7 @@ namespace partition_alloc::internal::base {
 //
 // Use this instead of strerror_r().
 PA_COMPONENT_EXPORT(PARTITION_ALLOC_BASE)
-void safe_strerror_r(int err, char* buf, size_t len);
+void safe_strerror_r(int err, span<char, 256> buf);
 
 // Calls safe_strerror_r with a buffer of suitable size and returns the result
 // in a C++ string.
