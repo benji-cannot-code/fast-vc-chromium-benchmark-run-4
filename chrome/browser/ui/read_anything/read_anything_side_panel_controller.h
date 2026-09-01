e10cfda8f2c58ebaa83e7849422dfff863a5c86e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-09-01 16:19:30 -0700

    Revert "[RM Omnibox] Remove observers from side panel controller"
    This reverts commit d78fafa982002b38cb26326c90d4b311f2802d63.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8671821679222886785
    
    Sample failed build: https://ci.chromium.org/b/8671821679222886785
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8339327&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8671821679222886785&type=BUG
    
    Original change's description:
    > [RM Omnibox] Remove observers from side panel controller
    >
    > Now that the immersive flag is removed, we can consolidate the observer
    > list into the ReadAnythingController. Doing this migration also fixes
    > the issue where the omnibox chip wasn't showing up after closing RM in
    > side panel mode.
    >
    > Fixed: 555791046
    > Change-Id: Ib6a639dbda04aee18ee1fd95f681c50809f19140
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339327
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Auto-Submit: Kristi Saney <kristislee@google.com>
    > Commit-Queue: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1690227}
    >
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I24bcb3121ad2519dcbcd400a59369f60175e168c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8342347
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1690278}

--- chrome/browser/ui/read_anything/read_anything_side_panel_controller.h
+++ chrome/browser/ui/read_anything/read_anything_side_panel_controller.h
@@ -11,8 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/ui/page_action/page_action_observer.h"
 #include "chrome/browser/ui/read_anything/read_anything_enums.h"
+#include "chrome/browser/ui/read_anything/read_anything_lifecycle_observer.h"
+#include "chrome/browser/ui/read_anything/read_anything_omnibox_controller.h"
 #include "chrome/browser/ui/side_panel/side_panel_entry_observer.h"
 #include "chrome/browser/ui/tabs/contents_observing_tab_feature.h"
 #include "components/tabs/public/tab_interface.h"
@@ -35,6 +39,7 @@ class View;
 
 class ReadAnythingSidePanelController;
 class ReadAnythingSidePanelWebView;
+class ReadAnythingUntrustedPageHandler;
 
 // Conceptually, if the side panel is open, then ReadAnythingSidePanelController
 // owns the WebContents (even though this is not the case in practice). The
@@ -66,6 +71,7 @@ class ReadAnythingSidePanelController
     : public SidePanelEntryObserver,
       public tabs::ContentsObservingTabFeature {
  public:
+  using Observer = ReadAnythingLifecycleObserver;
   ReadAnythingSidePanelController(tabs::TabInterface* tab,
                                   SidePanelRegistry* side_panel_registry);
   ReadAnythingSidePanelController(const ReadAnythingSidePanelController&) =
@@ -77,11 +83,22 @@ class ReadAnythingSidePanelController
   // Removes the ReadAnythingControllerGlue from the web contents.
   void RemoveReadAnythingControllerGlue();
 
+  void AddPageHandlerAsObserver(
+      base::WeakPtr<ReadAnythingUntrustedPageHandler> page_handler);
+  void RemovePageHandlerAsObserver(
+      base::WeakPtr<ReadAnythingUntrustedPageHandler> page_handler);
+
   // SidePanelEntryObserver:
   void OnEntryShown(SidePanelEntry* entry) override;
   void OnEntryHidden(SidePanelEntry* entry) override;
   void OnEntryWillHide(SidePanelEntry* entry,
                        SidePanelEntryHideReason reason) override;
+
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
+
+  void SetDwellTimeForTesting(base::TimeTicks test_time);
 
   tabs::TabInterface* tab() { return tab_.get(); }
 
@@ -113,6 +130,15 @@ class ReadAnythingSidePanelController
   void UpdateIphVisibility();
 
   std::string default_language_code_;
+
+  std::unique_ptr<ReadAnythingOmniboxController> omnibox_controller_;
+
+  // TODO(crbug.com/484371187): Investigate if reentrancy can be removed.
+  base::ObserverList<
+      Observer,
+      /*check_empty=*/false,
+      base::ObserverListReentrancyPolicy::kAllowReentrancyUntriaged>
+      observers_;
 
   const raw_ptr<tabs::TabInterface> tab_;
   raw_ptr<SidePanelRegistry> side_panel_registry_;
