d72f8a59d79706c3fa054813a4d1b991ad156532
Louis Romero <lpromero@google.com>
2026-09-01 13:27:56 -0700

    [iOS] Hardcode ConsentKit product ID for Drive
    Replace the dynamic feature param with the hardcoded Chrome on iOS
    product ID for ConsentKit in the Composebox Drive file picker. In
    Chromium, it is standard and safe practice to hardcode taxonomy IDs
    rather than configuring them dynamically via feature parameters.
    
    TAG=agy
    CONV=1be36c17-dabf-45ac-be71-fe3760d5370f
    
    Bug: 555689721
    Change-Id: Ia02512a2e4ca1dfdd33218bd42d0080620fb0081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334211
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690058}

--- ios/chrome/browser/composebox/shared/coordinator/composebox_picker_presenter.mm
+++ ios/chrome/browser/composebox/shared/coordinator/composebox_picker_presenter.mm
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/privacy_primitive/privacy_primitive_api.h"
 #import "ios/public/provider/chrome/browser/privacy_primitive/privacy_primitive_configuration.h"
 
+namespace {
+// The ConsentKit product ID for Chrome on iOS.
+constexpr int kChromeIOSProductId = 71720513;
+}  // namespace
+
 @interface ComposeboxPickerPresenter () <PHPickerViewControllerDelegate,
                                          UIDocumentPickerDelegate,
                                          UIImagePickerControllerDelegate,
@@ -190,7 +195,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         [[PrivacyPrimitiveConfiguration alloc] init];
     config.identity = identity;
     config.flowID = omnibox::kComposeboxDriveConsentFlowId.Get();
-    config.productID = omnibox::kComposeboxDriveConsentProductId.Get();
+    config.productID = kChromeIOSProductId;
     config.productSurface =
         omnibox::kComposeboxDriveConsentProductSurface.Get();
 
