672cff521a65d31e19a69f95e2d68a445920ceb4
Samira Arabgol <samiraarabgol@google.com>
2026-09-01 15:23:41 -0700

    [WebUI Searchbox] Replace favicon with search loupe in keyword mode
    In keyword mode, if a keyword does not have a custom favicon, the
    searchbox leading icon should display the generic search loupe vector icon
    instead of the active match favicon or the prior page favicon.
    
    
    Screencast: https://screencast.googleplex.com/cast/NDczMDk2NDMxNDk0NzU4NHxmZjdhNzMzZC02Mw
    Bug: b:546811781
    Change-Id: Idfa6f518a4398f31c45ec82b6c3c556add9f0572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8272440
    Reviewed-by: Juliet Knighton <julietlevesque@google.com>
    Commit-Queue: Samira Arabgol <samiraarabgol@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690204}

--- ui/webui/resources/cr_components/searchbox/searchbox_input.html.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_input.html.ts
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
-import {KeywordType} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 
 import type {SearchboxInputElement} from './searchbox_input.js';
 
@@ -15,12 +14,14 @@ export function getHtml(this: SearchboxInputElement) {
     <slot name="contextual-entrypoint"></slot>
     <cr-searchbox-icon id="icon" .match="${this.selectedMatch}"
         page-url="${this.pageUrl}"
-        default-icon="${this.searchboxIcon}" in-searchbox part="icon">
+        default-icon="${this.searchboxIcon}"
+        ?in-keyword-mode="${this.inKeywordMode_()}"
+        in-searchbox part="icon">
     </cr-searchbox-icon>
     <slot name="thumbnail"></slot>
-    ${this.inputKeywordModel?.type === KeywordType.kInKeyword ?
+    ${this.inKeywordMode_() ?
       html`<span id="keyword">${
-          this.inputKeywordModel.displayText}</span>` : ''}
+          this.inputKeywordModel!.displayText}</span>` : ''}
     ${this.multiLineEnabled ? html`
       <textarea id="input" autocomplete="off"
           part="searchbox-input"
