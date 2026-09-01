bf725cd0bb323022b4e6af40a0bda1c87f577cfc
Charles Meng <charlesmeng@chromium.org>
2026-09-01 16:59:46 -0700

    [Unified Tabstrip] Clip tab view children based on path
    This prevents the favicon from escaping the hover background/separators
    during the tab close animation. We can't do SetClipPath(GetPath()) like
    for vertical tabs because that clips out the tab separators, instead we
    need to clip tab view's children.
    
    Bug: 543144048
    Change-Id: I48a327aebc5d3cd0ba8aa4e08c146f92ba06d70d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8282647
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690322}

--- chrome/browser/ui/views/tabs/common/tab_view.cc
+++ chrome/browser/ui/views/tabs/common/tab_view.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/list_selection_model.h"
 #include "ui/base/theme_provider.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/compositor/clip_recorder.h"
 #include "ui/compositor/layer.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/events/types/event_type.h"
@@ -741,6 +742,19 @@ void TabView::OnGestureEvent(ui::GestureEvent* event) {
     default:
       break;
   }
+}
+
+void TabView::PaintChildren(const views::PaintInfo& info) {
+  ui::ClipRecorder clip_recorder(info.context());
+  // The paint recording scale for tabs is consistent along the x and y axis.
+  const float paint_recording_scale = info.paint_recording_scale_x();
+
+  if (const std::optional<SkPath> clip_path =
+          tab_styling()->GetChildClipPath(paint_recording_scale)) {
+    clip_recorder.ClipPathWithAntiAliasing(clip_path.value());
+  }
+
+  View::PaintChildren(info);
 }
 
 void TabView::OnPaint(gfx::Canvas* canvas) {
