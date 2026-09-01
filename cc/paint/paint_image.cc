91ee8b5a60268b989ecbeef0a1b24ce4c4cc778c
Stefan Zager <szager@chromium.org>
2026-08-31 19:49:13 -0700

    Convert atomic sequences for cc::PaintImage to 64-bit integers
    Fixed: 553454734
    Change-Id: I7b1ed2117113df21db369ab7b7142f6955fa67c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8311137
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689557}

--- cc/paint/paint_image.cc
+++ cc/paint/paint_image.cc
@@ -37,9 +37,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 namespace {
-base::AtomicSequenceNumber g_next_image_id;
-base::AtomicSequenceNumber g_next_image_content_id;
-base::AtomicSequenceNumber g_next_generator_client_id;
+base::AtomicSequenceNumberT<int64_t> g_next_image_id;
+base::AtomicSequenceNumberT<int64_t> g_next_image_content_id;
+base::AtomicSequenceNumberT<int64_t> g_next_generator_client_id;
 }  // namespace
 
 const PaintImage::Id PaintImage::kNonLazyStableId = -1;
--- third_party/blink/renderer/platform/graphics/bitmap_image.cc
+++ third_party/blink/renderer/platform/graphics/bitmap_image.cc
@@ -366,7 +366,7 @@ void BitmapImage::Draw(cc::PaintCanvas* canvas,
     }
   }
 
-  uint32_t stable_id = image.stable_id();
+  PaintImage::Id stable_id = image.stable_id();
   bool is_lazy_generated = image.IsLazyGenerated();
 
   const cc::PaintFlags* image_flags = &flags;
