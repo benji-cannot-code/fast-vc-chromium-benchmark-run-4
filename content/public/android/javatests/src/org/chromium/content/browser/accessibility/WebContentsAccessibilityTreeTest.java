a0b6c4d8a131fd37e8e3e3f7d9baa740dba248d9
Aditya Batra <adibatra@google.com>
2026-09-01 13:40:58 -0700

    [A11Y] Consider (some) headings with aria-label as leaf nodes
    Let headings with aria-label also evaluate IsLeafConsideringChildren().
    
    Historically, headings evaluated IsLeafConsideringChildren() only if
    they had non-empty text content. However, `aria-label` is now mapped to
    `contentDescription` instead of text content (crrev.com/c/6829145).
    Also, crrev.com/c/7917010 restricted leaf evaluation for headings to
    `NameFrom::kContents`.
    
    Consequently, headings with `aria-label` (`NameFrom::kAttribute`) and
    child elements are not pruned as leaf nodes. When not pruned,
    accessibility focus lands on the children instead, preventing the
    heading readout in TalkBack.
    
    This CL updates `BrowserAccessibilityAndroid::ComputeIsLeaf()` so
    headings with `GetNameFrom() == NameFrom::kAttribute` also evaluate
    IsLeafConsideringChildren().
    
    Bug: 447967836
    Change-Id: I032c230d848b74777465279290d2f41fa99aa6a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280590
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Aditya Batra <adibatra@google.com>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690078}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
@@ -1249,6 +1249,12 @@ public class WebContentsAccessibilityTreeTest {
 
     @Test
     @SmallTest
+    public void test_headingWithAriaLabel() {
+        performAriaTest("heading-with-aria-label.html");
+    }
+
+    @Test
+    @SmallTest
     public void test_hiddenDescribedBy() {
         performAriaTest("hidden-described-by.html");
     }
