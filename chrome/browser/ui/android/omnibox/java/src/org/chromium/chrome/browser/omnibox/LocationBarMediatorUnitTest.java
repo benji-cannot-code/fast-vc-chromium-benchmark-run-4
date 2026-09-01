2ee4cc89337b8ee848d2617aca25c294e15fec9e
Sky Malice <skym@chromium.org>
2026-09-01 10:56:41 -0700

    [AIM] Adjust popup escape transition in incognito mode
    When in SUGGESTIONS display state, pressing Escape should transition
    back to DRAFTING display state to cleanly dismiss popups.
    
    TAG=agy
    CONV=6af6b5a2-2912-469b-8267-990753d3cc6b
    
    Bug: 552189623
    Change-Id: I62f7229f2b75b6ae8403d5c46a816079d7c1a681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8298423
    Reviewed-by: Jakob Koblinsky <jkoblinsky@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689957}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorUnitTest.java
@@ -715,6 +715,7 @@ public class LocationBarMediatorUnitTest {
 
         assertEquals(input.getUserText(), input.getInitialUserText());
         assertEquals(mUrlBarDataCaptor.getValue().displayText, input.getInitialUserText());
+        assertEquals(DisplayState.DRAFTING, input.getDisplayState());
     }
 
     @Test
@@ -1561,6 +1562,7 @@ public class LocationBarMediatorUnitTest {
         assertTrue(mMediator.handleEscPress());
         assertEquals(AutocompleteRequestType.SEARCH, input.getRequestType());
         assertEquals(input.getInitialUserText(), input.getUserText());
+        assertEquals(DisplayState.DRAFTING, input.getDisplayState());
     }
 
     @Test
@@ -5444,5 +5446,16 @@ public class LocationBarMediatorUnitTest {
                 .onLayoutChange(mLocationBarLayout, 0, 0, 200, 50, 0, 0, 300, 50);
 
         verify(mLocationBarLayout).setActivationChipCompact(true);
+    }
+
+    @Test
+    public void testHandleEscPress_suggestionsDisplayState_transitionsToDrafting() {
+        mSessionState.getAutocompleteInput().setDisplayState(DisplayState.SUGGESTIONS);
+        mSessionState.getAutocompleteInput().setRequestType(AutocompleteRequestType.SEARCH);
+        mMediator.beginInput(mSessionState.getAutocompleteInput());
+
+        assertTrue(mMediator.handleEscPress());
+        assertEquals(DisplayState.DRAFTING, mSessionState.getAutocompleteInput().getDisplayState());
+        assertAutocompleteState(AutocompleteState.STANDBY);
     }
 }
