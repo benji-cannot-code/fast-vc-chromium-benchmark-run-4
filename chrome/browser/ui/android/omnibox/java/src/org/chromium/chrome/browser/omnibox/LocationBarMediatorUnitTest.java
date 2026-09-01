7a1db110dfd0e2027d5b706f89ed6f7877113da3
Patrick Noland <pnoland@google.com>
2026-09-01 09:45:57 -0700

    Don't show any scrim for desktop omnibox
    Bug: 552121604
    Change-Id: I72492a84f30fffc2e149666bf83d5bfb58afe41b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8309362
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689898}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorUnitTest.java
@@ -4565,7 +4565,7 @@ public class LocationBarMediatorUnitTest {
     }
 
     @Test
-    public void testBeginInput_fromUnanimatedFocus_transitionsToEnabledAndShowsScrim() {
+    public void testBeginInput_fromUnanimatedFocus_transitionsToEnabledAndDoesNotShowScrim() {
         DeviceInput.setSupportsAlphabeticKeyboardForTesting(true);
         OmniboxCapabilities.setHasDesktopExperienceForTesting(true);
         OmniboxCapabilities.setIsDesktopPlatformForTesting(true);
@@ -4583,7 +4583,8 @@ public class LocationBarMediatorUnitTest {
 
         assertAutocompleteState(AutocompleteState.ENABLED);
         assertFalse(mMediator.isUrlBarFocusedWithoutAnimation());
-        verify(mScrimHandler).setVisibility(true);
+        // We don't show the scrim on desktop
+        verify(mScrimHandler, never()).setVisibility(true);
     }
 
     @Test
