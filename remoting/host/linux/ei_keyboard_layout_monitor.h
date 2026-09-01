e61ebba207872c9239b1018e5d60f512b23f9186
Jamie Walch <jamiewalch@chromium.org>
2026-08-31 17:44:25 -0700

    Reinstate the warning about multiple keyboard layouts.
    This was removed in
    but I don't think it was deliberate. Although the layout is only
    relevant for text injection, which is rare on Wayland, logging that
    there might be keyboard layout issues is generally useful for debugging
    keyboard issues.
    
    https: //chromium-review.git.corp.google.com/c/chromium/src/+/6876369,
    Change-Id: Ic865c02d9d3ca83c5910fba821b396a390b64756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8329988
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689459}

--- remoting/host/linux/ei_keyboard_layout_monitor.h
+++ remoting/host/linux/ei_keyboard_layout_monitor.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_HOST_LINUX_EI_KEYBOARD_LAYOUT_MONITOR_H_
 #define REMOTING_HOST_LINUX_EI_KEYBOARD_LAYOUT_MONITOR_H_
 
-#include <xkbcommon/xkbcommon.h>
-
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "remoting/host/keyboard_layout_monitor.h"
