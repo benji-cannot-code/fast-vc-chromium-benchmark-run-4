62adf4a31e7b847a3bf237ff759e025b1e43a3b0
Bryant Chandler <bryantchandler@google.com>
2026-09-01 15:19:52 -0700

    [metrics] Update consumer scripts to use ElementTree
    Migrate consumer scripts from xml.dom.minidom to xml.etree.ElementTree.
    This eliminates the performance overhead of DOM-to-ElementTree
    conversions and allows these tools to parse XML files directly using the
    faster ElementTree parser.
    
    Bug: 526579495
    Change-Id: Icd6d0885ab8d0ec8b9b2a1849519cc566a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8055688
    Reviewed-by: Andrzej Fiedukowicz <afie@google.com>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690199}

--- tools/metrics/histograms/generate_allowlist_from_histograms_file.py
+++ tools/metrics/histograms/generate_allowlist_from_histograms_file.py
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import pathlib
 import sys
-import xml.dom.minidom
+import xml.etree.ElementTree as ET
+from pathlib import Path
 
 import setup_modules  # pylint: disable=unused-import
 
@@ -110,7 +111,7 @@ def _GenerateFile(arguments):
       arguments.output_dir: A directory to put the generated file.
       arguments.allow_list_name: A name of the variant or enum list.
   """
-  histograms = xml.dom.minidom.parse(arguments.input)
+  histograms = ET.parse(arguments.input).getroot()
   values = _GenerateValueList(
     histograms, arguments.tag, arguments.allow_list_name
   )
