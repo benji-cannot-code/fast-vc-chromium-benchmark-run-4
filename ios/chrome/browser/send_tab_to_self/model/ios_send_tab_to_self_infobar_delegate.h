ce265c586b3e663c2a553287c4987d9d5b30cb5f
Michael Tatarski <mtatarski@google.com>
2026-09-01 06:18:37 -0700

    [STTS] Modernize device iteration and C++ header includes in infobar
    This CL modernizes target device iteration in
    SendTabToSelfTableViewController and standardizes C++ header includes in
    the iOS Send Tab to Self infobar delegate header.
    
    Previously, device population in the table view controller used verbose
    iterator-based loops with manual pointer dereferencing, while the
    infobar delegate header mixed #import for base C++ headers. This change
    converts the device loop to a range-based for loop and standardizes C++
    headers to use #include.
    
    Bug: None
    Change-Id: If516ecb4be7646fb1d346ce9b3ca68e9f15c8639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8330370
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689795}

--- ios/chrome/browser/send_tab_to_self/model/ios_send_tab_to_self_infobar_delegate.h
+++ ios/chrome/browser/send_tab_to_self/model/ios_send_tab_to_self_infobar_delegate.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#import "base/memory/raw_ptr.h"
-#import "base/memory/weak_ptr.h"
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "components/infobars/core/confirm_infobar_delegate.h"
 
 @protocol SceneCommands;
