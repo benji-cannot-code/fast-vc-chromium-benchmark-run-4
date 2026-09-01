ed8d800312b82bde934bf96ec38e61edd98530e2
behery <behery@chromium.org>
2026-09-01 16:01:05 -0700

    [InfoBar Centralization] Migrate Keystone Promotion infobar
    Migrates the Mac Keystone Promotion infobar to the modern centralized
    InfoBar framework.
    
    - Adds kMigratedKeystonePromotion feature param under
      kCentralizedInfoBarFramework.
    - Registers the KEYSTONE_PROMOTION_INFOBAR_DELEGATE_MAC spec in
      BrowserInfoBarRegistry.
    - Dispatches via BrowserInfoBarManager::ShowGlobally when migrated.
    - Updates InfoBarUiTest to cover the migrated Keystone promotion
      infobar.
    
    TAG=agy
    CONV=95d8ecc6-0241-4192-b632-f9fec8dd2359
    
    Bug: 530928970
    Change-Id: Ib472061a5ff71a1d940149d30cedf9a7c0ff9da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280485
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Hamzah Behery <behery@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690257}

--- chrome/browser/infobars/infobar_features.cc
+++ chrome/browser/infobars/infobar_features.cc
@@ -55,6 +55,11 @@ BASE_FEATURE_PARAM(bool,
                    false);
 
 BASE_FEATURE_PARAM(bool,
+                   kMigratedKeystonePromotion,
+                   &kCentralizedInfoBarFramework,
+                   false);
+
+BASE_FEATURE_PARAM(bool,
                    kMigratedKnownInterceptionDisclosure,
                    &kCentralizedInfoBarFramework,
                    false);
@@ -134,6 +139,8 @@ const base::FeatureParam<bool>* GetInfoBarMigrationParam(
       return &kMigratedGoogleApiKeys;
     case InfoBarDelegate::INSTALLER_DOWNLOADER_INFOBAR_DELEGATE:
       return &kMigratedInstallerDownloader;
+    case InfoBarDelegate::KEYSTONE_PROMOTION_INFOBAR_DELEGATE_MAC:
+      return &kMigratedKeystonePromotion;
     case InfoBarDelegate::KNOWN_INTERCEPTION_DISCLOSURE_INFOBAR_DELEGATE:
       return &kMigratedKnownInterceptionDisclosure;
     case InfoBarDelegate::ENABLE_LINK_CAPTURING_INFOBAR_DELEGATE:
