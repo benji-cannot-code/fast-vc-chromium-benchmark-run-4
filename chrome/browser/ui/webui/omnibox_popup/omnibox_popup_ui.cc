744ab0ebe8faf76f1f07e32aa55051a8683cf828
Kaan Alsan <alsan@chromium.org>
2026-09-01 14:51:57 -0700

    [OmniboxEverywhere] Refactor screensharing logic into dedicated controller
    Extracts screensharing logic from ContextualSearchboxHandler into a
    dedicated ContextualSearchboxScreenshareController.
    ContextualSearchboxHandler implements the controller's Host interface to
    receive processed captures and attach them to the active searchbox
    session.
    
    Beyond moving code, this changes:
    - Cleans up an unused transitive header
    - Protects StartScreenshare with mojo::WrapCallbackWithDefaultInvokeIfNotRun (needed to guarantee execution)
    - Forwards OnScreenshotMenuClosed in OmniboxEverywhereUI::ExecuteCommand to make WebUI state reset when executing menu actions
    
    BYPASS_LARGE_CHANGE_WARNING=Moving code
    
    Fixed: 549716561
    Change-Id: Idd21d2c25a96867d5c46e2a0c56edbbb8ee15bc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8323088
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Hamzah Behery <behery@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690159}

--- chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.cc
+++ chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/location_bar/location_bar.h"
 #include "chrome/browser/ui/omnibox/chrome_omnibox_client.h"
+#include "chrome/browser/ui/omnibox/omnibox_controller.h"
 #include "chrome/browser/ui/omnibox/omnibox_next_features.h"
 #include "chrome/browser/ui/omnibox/omnibox_view.h"
 #include "chrome/browser/ui/ui_features.h"
