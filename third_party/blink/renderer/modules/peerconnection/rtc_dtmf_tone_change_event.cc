302f2a8f476a2d74ee4b142a0affff0429694524
Michael Reeves <michael.reeves077@gmail.com>
2026-09-01 04:29:50 -0700

    [Interop 2026] Make DTMF tone event initializer optional
    Match the WebRTC IDL by making the RTCDTMFToneChangeEvent initializer optional with an empty dictionary default. This gives the constructor a length of one and allows construction without a second argument.
    
    Remove the expectation for the now-passing WPT.
    
    Bug: 501209160
    Change-Id: I2274ff8b0df3517d5dbca480490d85694c2cf065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8311029
    Commit-Queue: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689750}

--- third_party/blink/renderer/modules/peerconnection/rtc_dtmf_tone_change_event.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_dtmf_tone_change_event.cc
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-RTCDTMFToneChangeEvent* RTCDTMFToneChangeEvent::Create(const String& tone) {
-  return MakeGarbageCollected<RTCDTMFToneChangeEvent>(tone);
+RTCDTMFToneChangeEvent* RTCDTMFToneChangeEvent::Create(const String& type) {
+  return Create(AtomicString(type), RTCDTMFToneChangeEventInit::Create());
 }
 
 RTCDTMFToneChangeEvent* RTCDTMFToneChangeEvent::Create(
