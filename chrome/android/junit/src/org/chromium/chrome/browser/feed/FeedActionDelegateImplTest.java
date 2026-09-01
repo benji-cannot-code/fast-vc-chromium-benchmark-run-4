cee29d7756e2d658e4e553918e6b5b5d6c01a9e3
Sky Malice <skym@chromium.org>
2026-09-01 13:18:18 -0700

    [Clean] Remove unused mIntentCaptor in FeedActionDelegateImplTest
    Remove unused intent argument captor in FeedActionDelegateImplTest.
    
    TAG=agy
    CONV=8e52c20c-f36f-4732-907e-5a81312b5aa6
    
    Bug: 546549170
    Change-Id: I1a99f044c0f3ed00f6e361f3fed9b897d4f68351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8331788
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690051}

--- chrome/android/junit/src/org/chromium/chrome/browser/feed/FeedActionDelegateImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/feed/FeedActionDelegateImplTest.java
@@ -21,7 +21,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -80,8 +79,6 @@ public final class FeedActionDelegateImplTest {
     @Mock private Intent mSigninIntent;
 
     @Mock private BottomSheetSigninAndHistorySyncCoordinator mSigninCoordinator;
-
-    @Captor ArgumentCaptor<Intent> mIntentCaptor;
 
     private FeedActionDelegateImpl mFeedActionDelegateImpl;
 
