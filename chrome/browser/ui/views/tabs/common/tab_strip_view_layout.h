7c1fd061427861d80dfa582379b629f29bc2884f
Caroline Rising <corising@chromium.org>
2026-09-01 16:14:08 -0700

    [Unified tabstrip] fix tab closing mode bug
    Makes sure the new tab button stays at the end of the tabstrip as it
    shrinks during tab closing mode. Also fixes a bug in the closing mode
    transition between scrolling to shrinking the container.
    
    Bug: 551936343
    Change-Id: I562e6b0abea547c1aabbb4f7e2a9bd59d050a8a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8337610
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690271}

--- chrome/browser/ui/views/tabs/common/tab_strip_view_layout.h
+++ chrome/browser/ui/views/tabs/common/tab_strip_view_layout.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_COMMON_TAB_STRIP_VIEW_LAYOUT_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_COMMON_TAB_STRIP_VIEW_LAYOUT_H_
 
+#include <optional>
+
 #include "chrome/browser/ui/views/tabs/shared/tab_strip_types.h"
 #include "ui/views/layout/layout_manager_base.h"
 
@@ -36,6 +38,9 @@ class TabStripViewLayout : public views::LayoutManagerBase {
   views::ProposedLayout CalculateVerticalLayout(
       const TabStripView* tab_strip_view,
       const views::SizeBounds& size_bounds) const;
+
+  std::optional<int> GetClosingModeUnpinnedContainerOverrideWidth(
+      const TabStripView* tab_strip_view) const;
 
   const TabStripOrientation orientation_;
 };
