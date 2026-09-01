1b2c28a90e095ad7adf3f825cf335f81288a0e89
Bryan Kim <bryankim@google.com>
2026-08-31 18:43:40 -0700

    [Bedrock] Modularize views autofill payments and popup targets
    Modularize the following leaf modules from //chrome/browser/ui into
    dedicated targets under //chrome/browser/ui/views/autofill:
    - //chrome/browser/ui/views/autofill/payments:bnpl_issuer_linked_pill
    - //chrome/browser/ui/views/autofill/payments:select_bnpl_issuer_dialog (including bnpl_issuer_view)
    - //chrome/browser/ui/views/autofill/payments:omnibox_autofill_bubble_view
    - //chrome/browser/ui/views/autofill/popup:popup_loading_view
    - //chrome/browser/ui/views/autofill/popup:popup_row_factory_utils
    
    AGENT_GENERATED_CL
    TAG=agy
    CONV=a30875d2-9c4c-481f-a36b-b0f928bd01e5
    
    Bug: 353332589
    Change-Id: Iaba44d15af41fdeb41cf3cd15653dafe235c1547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8318393
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689508}

--- chrome/browser/ui/views/autofill/payments/bnpl_issuer_linked_pill.cc
+++ chrome/browser/ui/views/autofill/payments/bnpl_issuer_linked_pill.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autofill/payments/bnpl_issuer_linked_pill.h"
 
 #include "chrome/browser/ui/color/chrome_color_id.h"
-#include "chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.h"
+#include "chrome/browser/ui/views/autofill/popup/popup_badge_constants.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/interaction/element_identifier.h"
