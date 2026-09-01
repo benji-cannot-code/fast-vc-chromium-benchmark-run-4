bc6f34bc24d4c45433f4a178a31f2f712f3a217b
Paul Jensen <pauljensen@chromium.org>
2026-09-01 04:54:20 -0700

    Adjust MemorySaverChipInteractiveTest to work with WebUI toolbar
    This required a few changes:
    1. Turn off click suppression logic as it was inadvertently triggering
       on the clicks used by the test to close the bubble.
    2. Make WebUIPageActionControl ignore clicks during shutdown.
    3. Make the test use PageActionTestAccessor instead of checking Views
       properties to determine if chip is expanded.
    4. Wait for chips and bubbles to disappear as this happens
       asynchronously in WebUI toolbar.
    5. Send key presses to instrumented tab rather than omnibox so that it
       uses InteractionTestUtilSimulatorBrowser::SendKeyPress which works
       with TrackedElementWebContents.  Previously it tried to use the
       unimplemented InteractionTestUtilSimulatorWebUI::SendKeyPress.
    6. Wait for tab reload to complete so key presses don't hit discarded
       tab.
    
    Change-Id: Ibb9116318aef3a8726ff58cec07dbac9e76dfd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8299176
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689760}

--- chrome/browser/ui/performance_controls/test_support/memory_saver_interactive_test_mixin.h
+++ chrome/browser/ui/performance_controls/test_support/memory_saver_interactive_test_mixin.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/interaction/interaction_test_util_browser.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
+#include "content/public/test/browser_test_utils.h"
 #include "ui/base/interaction/interactive_test.h"
 
 // Template to be used as a mixin class for memory saver tests extending
@@ -63,7 +64,13 @@ class MemorySaverInteractiveTestMixin : public MemorySaverBrowserTestMixin<T> {
                       // a tab being discarded while it is notifying its
                       // observers
         TryDiscardTab(tab_index), T::SelectTab(kTabStripElementId, tab_index),
-        T::WaitForShow(contents_id));
+        T::WaitForShow(contents_id),
+        // Wait for reload to commit, otherwise later steps (e.g. SendKeyPress)
+        // may hit old tab in the process of being discarded.
+        T::Do([=, this]() {
+          content::WaitForLoadStop(
+              MemorySaverBrowserTestMixin<T>::GetWebContentsAt(tab_index));
+        }));
   }
 };
 
--- chrome/browser/ui/views/location_bar/webui_location_bar.cc
+++ chrome/browser/ui/views/location_bar/webui_location_bar.cc
@@ -845,6 +845,7 @@ void WebUILocationBar::SetSuppressionThresholdForTesting(
     base::TimeDelta threshold) {
   page_info_reopen_suppressor_.SetSuppressionThresholdForTesting(  // IN-TEST
       threshold);
+  page_action_control_.SetSuppressionThresholdForTesting(threshold);  // IN-TEST
 }
 
 void WebUILocationBar::OnLhsChipPointerEntered(
