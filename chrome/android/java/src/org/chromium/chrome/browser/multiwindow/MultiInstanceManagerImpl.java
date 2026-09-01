4a6d7a7dc6044b12cf1dcfdf81d5f5655de7044d
Fiaz Muhammad <mfiaz@google.com>
2026-08-31 20:06:21 -0700

    [MultiWindow] Migrate @Nullable Boolean in MultiInstanceManagerImpl
    Migrates @Nullable Boolean mMergeTabsOnResume in
    MultiInstanceManagerImpl to use @TriState int to eliminate boxed
    Boolean overhead and modernize tri-state state representation.
    
    Bug: 545992990
    Change-Id: I7544a8d7ab8c5e66d783b6dc7b26a2555e1f1316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8315551
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689573}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManagerImpl.java
@@ -23,6 +23,7 @@ import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ApplicationStatus.ActivityStateListener;
 import org.chromium.base.ResettersForTesting;
+import org.chromium.base.TriState;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.MonotonicObservableSupplier;
 import org.chromium.build.BuildConfig;
@@ -82,7 +83,7 @@ public class MultiInstanceManagerImpl extends MultiInstanceManager
     protected @Nullable TabModelSelectorTabModelObserver mTabModelObserver;
     protected MultiInstanceOrchestrator mMultiInstanceOrchestrator;
 
-    private @Nullable Boolean mMergeTabsOnResume;
+    private @TriState int mMergeTabsOnResume;
 
     /**
      * Used to observe state changes to a different ChromeTabbedActivity instances to determine when
@@ -256,15 +257,16 @@ public class MultiInstanceManagerImpl extends MultiInstanceManager
             boolean inMultiWindowMode =
                     mMultiWindowModeStateDispatcher.isInMultiWindowMode()
                             || mMultiWindowModeStateDispatcher.isInMultiDisplayMode();
-            // Don't need to merge tabs when mMergeTabsOnResume is null (cold start) since they get
-            // merged when TabPersistentStore.loadState(boolean) is called from initializeState().
-            if (!inMultiWindowMode && (mMergeTabsOnResume != null && mMergeTabsOnResume)) {
+            // Don't need to merge tabs when mMergeTabsOnResume is NOT_SET (cold start) since they
+            // get merged when TabPersistentStore.loadState(boolean) is called from
+            // initializeState().
+            if (!inMultiWindowMode && mMergeTabsOnResume == TriState.TRUE) {
                 maybeMergeTabs();
-            } else if (!inMultiWindowMode && mMergeTabsOnResume == null) {
+            } else if (!inMultiWindowMode && mMergeTabsOnResume == TriState.NOT_SET) {
                 // This happens on cold start to kill any second activity that might exist.
                 killOtherTask();
             }
-            mMergeTabsOnResume = false;
+            mMergeTabsOnResume = TriState.FALSE;
         }
     }
 
@@ -316,7 +318,7 @@ public class MultiInstanceManagerImpl extends MultiInstanceManager
                             mOtherCTAStateObserver, otherResumedCTA);
                 }
             } else {
-                mMergeTabsOnResume = true;
+                mMergeTabsOnResume = TriState.TRUE;
             }
         }
     }
