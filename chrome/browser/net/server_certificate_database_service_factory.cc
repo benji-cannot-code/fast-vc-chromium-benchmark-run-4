73fe13c711e7b10325c530049955ea030eb7f4ef
Matt Mueller <mattm@chromium.org>
2026-09-01 16:57:23 -0700

    Clean up chromeos NSS cert migration code
    The migration code has been disabled since https://crrev.com/c/7769909,
    this removes it completely.
    
    OBSOLETE_HISTOGRAMS=the migration code has been removed
    
    Bug: 390333881
    Fixed: 555232376
    Change-Id: If1353837f0e1edbc5519c702173ebd462976a04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8313895
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690319}

--- chrome/browser/net/server_certificate_database_service_factory.cc
+++ chrome/browser/net/server_certificate_database_service_factory.cc
@@ -5,48 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/net/server_certificate_database_service_factory.h"
 
-#include "base/functional/callback_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/server_certificate_database/server_certificate_database_service.h"
-#include "content/public/browser/browser_thread.h"
-
-#if BUILDFLAG(IS_CHROMEOS)
-#include "base/task/bind_post_task.h"
-#include "chrome/browser/net/nss_service.h"
-#include "chrome/browser/net/nss_service_factory.h"
-#include "net/cert/nss_cert_database.h"
-#endif
 
 namespace net {
-
-namespace {
-
-#if BUILDFLAG(IS_CHROMEOS)
-void GotNSSCertDatabaseOnIOThread(
-    base::OnceCallback<void(crypto::ScopedPK11Slot)> callback,
-    NSSCertDatabase* db) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-  std::move(callback).Run(db->GetPublicSlot());
-}
-
-void NssSlotGetter(NssCertDatabaseGetter nss_cert_db_getter,
-                   base::OnceCallback<void(crypto::ScopedPK11Slot)> callback) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-
-  auto split_callback = base::SplitOnceCallback(
-      base::BindOnce(&GotNSSCertDatabaseOnIOThread, std::move(callback)));
-
-  net::NSSCertDatabase* cert_db =
-      std::move(nss_cert_db_getter).Run(std::move(split_callback.first));
-  // If the NSS database was already available, |cert_db| is non-null and
-  // |did_get_cert_db_callback| has not been called. Call it explicitly.
-  if (cert_db) {
-    std::move(split_callback.second).Run(cert_db);
-  }
-}
-#endif  // BUILDFLAG(IS_CHROMEOS)
-
-}  // namespace
 
 ServerCertificateDatabaseService*
 ServerCertificateDatabaseServiceFactory::GetForBrowserContext(
@@ -73,16 +35,10 @@ ServerCertificateDatabaseServiceFactory::
               .WithGuest(ProfileSelection::kRedirectedToOriginal)
               // Not needed for Ash internals as it's not a real user
               // profile and so there isn't a user to use the database.
-              // This also matches the practical behavior of
-              // NssServiceFactory which will end up crashing the browser
-              // if attempted to use on an AshInternals profile.
               .WithAshInternals(ProfileSelection::kNone)
               .Build()
 
       ) {
-#if BUILDFLAG(IS_CHROMEOS)
-  DependsOn(NssServiceFactory::GetInstance());
-#endif
 }
 
 ServerCertificateDatabaseServiceFactory::
@@ -92,17 +48,7 @@ std::unique_ptr<KeyedService>
 ServerCertificateDatabaseServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* browser_context) const {
   Profile* profile = Profile::FromBrowserContext(browser_context);
-#if BUILDFLAG(IS_CHROMEOS)
-  return std::make_unique<ServerCertificateDatabaseService>(
-      profile->GetPath(), profile->GetPrefs(),
-      base::BindPostTask(
-          content::GetIOThreadTaskRunner({}),
-          base::BindOnce(&NssSlotGetter,
-                         NssServiceFactory::GetForContext(profile)
-                             ->CreateNSSCertDatabaseGetterForIOThread())));
-#else
   return std::make_unique<ServerCertificateDatabaseService>(profile->GetPath());
-#endif
 }
 
 }  // namespace net
