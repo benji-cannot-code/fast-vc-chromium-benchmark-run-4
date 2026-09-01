268e560af9e708e1f1115dc7ffbc08d4f95270bd
Volodymyr Riazantsev <riazantsevv@google.com>
2026-08-31 18:05:03 -0700

    renderer: media: Ignore remoting check while selecting Cast streaming renderer
    Cast may not support remoting (cast_streaming_enable_remoting=false)
    while we still need to select a proper streaming renderer.
    
    Bug: 539697279
    Tested: Reference Cast Platform
    Change-Id: Ib2bb8d90de7b533c91a6b93ab4771bb52d259a81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160325
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Auto-Submit: Volodymyr Riazantsev <riazantsevv@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1689477}

--- content/renderer/media/media_factory.cc
+++ content/renderer/media/media_factory.cc
@@ -84,7 +84,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENABLE_CAST_RECEIVER)
 #include "components/cast_streaming/common/public/cast_streaming_url.h"  // nogncheck
-#include "components/cast_streaming/common/public/features.h"  // nogncheck
 #include "components/cast_streaming/renderer/public/resource_provider.h"  // nogncheck
 #include "components/cast_streaming/renderer/public/wrapping_renderer_factory_selector.h"  // nogncheck
 #endif
@@ -531,8 +530,7 @@ MediaFactory::CreateRendererFactorySelector(
   bool is_base_renderer_factory_set = false;
 
 #if BUILDFLAG(ENABLE_CAST_RECEIVER)
-  if (cast_streaming::IsCastRemotingEnabled() &&
-      cast_streaming::IsCastStreamingMediaSourceUrl(url) &&
+  if (cast_streaming::IsCastStreamingMediaSourceUrl(url) &&
       cast_streaming_resource_provider_) {
     factory_selector =
         std::make_unique<cast_streaming::WrappingRendererFactorySelector>(
