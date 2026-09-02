d4a7ec4b49a4e2d0f82d01af45f0618b3c707baa
Zonghan Xu <xzonghan@google.com>
2026-09-02 13:56:24 -0700

    [SG Android] Add error page HTML/CSS/JS resource templates
    Create the base files for Enterprise error pages, this will allow UI implementation and browser process implement to proceed in parallel.
    
    At its current stage, the page will only include a few text that reflects its state, allowing manual testing to be done for the browser process.
    
    Bug: 543015665
    Change-Id: I62847d30b314e226efa86222571f7f0e1e51a6d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334518
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691077}

---
+++ components/enterprise/net/resources/enterprise_proxy_error_page.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function reloadButtonClicked() {
+  // Skeleton handler for reload button.
+}
+
+function signinButtonClicked() {
+  // Skeleton handler for sign-in button.
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const reloadButton = document.getElementById('reload-button');
+  if (reloadButton) {
+    reloadButton.addEventListener('click', reloadButtonClicked);
+  }
+  const signinButton = document.getElementById('signin-button');
+  if (signinButton) {
+    signinButton.addEventListener('click', signinButtonClicked);
+  }
+});
