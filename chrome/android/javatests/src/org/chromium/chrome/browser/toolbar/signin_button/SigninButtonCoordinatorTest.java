39ac107f155cdf656695e9ed999a0d5cba7a5b5e
Liza Bipin <mlbipin@google.com>
2026-09-02 01:30:30 -0700

    [Signin][AL] Open a profile menu when the profile disc is clicked
    On android desktop, when the profile disc (SigninButton) is clicked a
    profile menu should show up which has various options like sign in, open
    an incognito window, go to settings, etc. This CL creates the skeleton
    for this profile menu and links it to the SigninButton. The styles and
    structures are chosen to (1) match with the extensions popup menu [see:
    https://screenshot-v2.corp.google.com/042vcvqdd77j8] and (2) mirror the
    desktop version of this menu as much as possible [see:
    go/ALID-refinements-uir].
    
    The 'passwords and autofill' profile menu option is included in this
    skeleton CL as it will be included in all future profile menu variants
    (+ functions as a POC). Follow-up CLs will add the other menu options
    and conditional UI elements.
    
    Example UI screenshot in https://crbug.com/551756560#comment4
    
    Exception approved for use of DeviceInfo.isDesktop():
    crbug.com/545967927
    
    Bug: 551756560
    Change-Id: I6f92a5f2b80a923f3fc0235300d7efef73090d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8276734
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Sophie Crowley <crowleso@google.com>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690522}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonCoordinatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonCoordinatorTest.java
@@ -34,6 +34,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.DeviceInfo;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
@@ -568,6 +569,23 @@ public class SigninButtonCoordinatorTest {
         ColorStateList unfocusedTint = avatarButton.getImageTintList();
         assertNotNull(unfocusedTint);
         assertNotEquals("Tint should change when window is inactive", focusedTint, unfocusedTint);
+    }
+
+    @Test
+    @MediumTest
+    @Restriction(DeviceFormFactor.DESKTOP)
+    @EnableFeatures(SigninFeatures.SIGNIN_BUTTON_PROFILE_MENU)
+    public void testClickSigninButton_DesktopOpensAccountMenu() {
+        DeviceInfo.setIsDesktopForTesting(true);
+        startActivityOnNtp();
+
+        AppHeaderUtils.setAppInDesktopWindowForTesting(true);
+        ViewUtils.waitForVisibleView(withId(R.id.signin_button));
+
+        onView(withId(R.id.signin_button)).perform(click());
+
+        // Verify that the account menu popup is displayed.
+        ViewUtils.waitForVisibleView(withId(R.id.account_menu_container));
     }
 
     private void startActivityOnNtp() {
