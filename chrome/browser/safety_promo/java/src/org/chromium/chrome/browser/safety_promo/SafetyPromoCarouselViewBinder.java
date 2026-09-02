c7d652339836606fca1af62d802064cb15eb0350
Yanting Yu <yyanting@google.com>
2026-09-02 09:13:44 -0700

    [SafetyFRE] Implement MVC infrastructure for the carousel screen
    Implement the Model-View-Coordinator (MVC) pattern for the Carousel
    screen.
    
    Key changes:
    - Add carousel-specific fields (carouselTitleResId,
      carouselSubtitleResId, carouselIllustrationResId) to SafetyPromoItem.
    - Introduce SafetyPromoCarouselCoordinator, SafetyPromoCarouselAdapter,
      SafetyPromoCarouselProperties, SafetyPromoCarouselView, and
      SafetyPromoCarouselViewBinder under //chrome/browser/safety_promo.
    - Create SafetyPromoCarouselFirstRunFragment to host the carousel UI in
      the First Run Experience.
    - Integrate SafetyPromoCarouselFirstRunFragment into FirstRunActivity's
      page sequence when shouldShowSafetyFrePromoCarousel() is enabled.
    - Add Robolectric unit tests for the Adapter, Coordinator, ViewBinder,
      and FirstRunUtils helpers.
    
    Currently, the carousel screen is placed after the safety screen and
    entered by clicking the continue button. In follow-up CLs, we will
    introduce a circle page indicator and enable clicking an overview card
    to jump to the corresponding carousel position.
    
    Demo: https://docs.google.com/document/d/1L3W8rpcffRPkm-qpfYuPhmRq51qKCIoeZcGhmT9M3ls/edit?resourcekey=0-VzvGkvFTekGKV2JmcxEqzg&tab=t.0#bookmark=id.a5hknuhv4yg8
    
    Binary-Size: Size increase due to new Carousel illustrations and MVC classes.
    Bug: 534388538, 543028748
    Change-Id: I441dcfd714af431d2ace45eb4b7545878cc8cd04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8298421
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Yanting Yu <yyanting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690808}

---
+++ chrome/browser/safety_promo/java/src/org/chromium/chrome/browser/safety_promo/SafetyPromoCarouselViewBinder.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.safety_promo;
+
+import static org.chromium.chrome.browser.safety_promo.SafetyPromoCarouselProperties.ON_CONTINUE_CLICKED;
+import static org.chromium.chrome.browser.safety_promo.SafetyPromoCarouselProperties.SUBTITLE_RES_ID;
+import static org.chromium.chrome.browser.safety_promo.SafetyPromoCarouselProperties.TITLE_RES_ID;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Binder for Safety Promo Carousel. */
+@NullMarked
+class SafetyPromoCarouselViewBinder {
+    public static void bind(
+            PropertyModel model, SafetyPromoCarouselView view, PropertyKey propertyKey) {
+        if (propertyKey == TITLE_RES_ID) {
+            view.setTitleText(model.get(TITLE_RES_ID));
+        } else if (propertyKey == SUBTITLE_RES_ID) {
+            view.setSubtitleText(model.get(SUBTITLE_RES_ID));
+        } else if (propertyKey == ON_CONTINUE_CLICKED) {
+            view.setContinueButtonOnClickListener(model.get(ON_CONTINUE_CLICKED));
+        }
+    }
+}
