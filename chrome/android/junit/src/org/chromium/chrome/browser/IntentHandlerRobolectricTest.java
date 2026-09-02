26933a043409467d212c1427c82b00404728101b
Sky Malice <skym@chromium.org>
2026-09-02 04:49:27 -0700

    [Clean] Remove unused captor in IntentHandlerRobolectricTest
    Remove unused LoadUrlParams argument captor in
    IntentHandlerRobolectricTest.
    
    TAG=agy
    CONV=88b1b2ab-4292-4949-9bfd-61a68d9ba004
    
    Bug: 546549170
    Change-Id: I66c49945b996c82135a052ca7840998c238a1f98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8338287
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690604}

--- chrome/android/junit/src/org/chromium/chrome/browser/IntentHandlerRobolectricTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/IntentHandlerRobolectricTest.java
@@ -34,8 +34,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
@@ -202,8 +200,6 @@ public class IntentHandlerRobolectricTest {
 
     @Mock IntentHandler.Natives mNativeMock;
     @Mock ExternalIntentUrlChecker.Natives mExternalIntentUrlCheckerNativeMock;
-
-    @Captor ArgumentCaptor<LoadUrlParams> mLoadUrlParamsCaptor;
 
     private ShadowPowerManager mShadowPowerManager;
     private ShadowKeyguardManager mShadowKeyguardManager;
