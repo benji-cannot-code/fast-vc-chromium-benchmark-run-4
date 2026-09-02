36627caff70e5d3e9822b693a89a10ebab0f30a0
Tomasz Wiszkowski <ender@google.com>
2026-09-02 07:12:29 -0700

    [Clean] Add parameter comments for booleans in Fusebox classes
    Add parameter comments for boolean literals at call sites across
    FuseboxCoordinator and FuseboxMediator per Omnibox parameter
    annotation guidelines.
    
    Bug: None
    Change-Id: I2464815d5d98373159c836a38f2a5869bbbd9239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8342613
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Charles Cai <charlesyc@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690691}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediator.java
@@ -126,7 +126,7 @@ import java.util.function.Supplier;
     private final SettableNonNullObservableSupplier<@PopupState Integer> mPopupStateSupplier;
     private final BackPressManager mBackPressManager;
     private final SettableNonNullObservableSupplier<Boolean> mBackPressStateSupplier =
-            ObservableSuppliers.createNonNull(false);
+            ObservableSuppliers.createNonNull(/* initialValue= */ false);
     private final boolean mIsDesktopPlatform;
     private final SettableNonNullObservableSupplier<Boolean> mHasAttachmentsSupplier;
     private final OmniboxResourceProvider mResourceProvider;
@@ -1146,7 +1146,7 @@ import java.util.function.Supplier;
                             .setDataAndType(
                                     MediaStore.Images.Media.INTERNAL_CONTENT_URI,
                                     MimeTypeUtils.IMAGE_ANY_MIME_TYPE)
-                            .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                            .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, /* value= */ true);
         }
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
@@ -1191,7 +1191,7 @@ import java.util.function.Supplier;
                 new Intent(Intent.ACTION_OPEN_DOCUMENT)
                         .addCategory(Intent.CATEGORY_OPENABLE)
                         .setType(mimeType)
-                        .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                        .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, /* value= */ true)
                         .addFlags(
                                 Intent.FLAG_GRANT_READ_URI_PERMISSION
                                         | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
