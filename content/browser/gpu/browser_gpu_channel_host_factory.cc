f6c5a72095e45923461acded72efdf83c5e054c0
Bo Liu <boliu@chromium.org>
2026-09-02 10:21:56 -0700

    android: Default disable gpu launch timeout
    Default is_visible_ to false. If chrome is launched without creating any
    browser compositor (say launched in the background), timeout is still
    started because is_visible_ defaults to true.
    
    Just make it default false instead, but use a kill switch.
    
    Bug: 542394742
    Change-Id: Ie549f257fd6f1372c5127a92cf91e7101334420e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8347247
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690881}

--- content/browser/gpu/browser_gpu_channel_host_factory.cc
+++ content/browser/gpu/browser_gpu_channel_host_factory.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/orderfile/orderfile_buildflags.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/feature.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/process/process_handle.h"
@@ -41,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 namespace {
+
+BASE_FEATURE(kDefaultDisableGpuLaunchTimeout, base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 
@@ -302,7 +306,9 @@ void BrowserGpuChannelHostFactory::CloseChannel() {
 BrowserGpuChannelHostFactory::BrowserGpuChannelHostFactory()
     : gpu_client_id_(ChildProcessHostImpl::GenerateChildProcessUniqueId()),
       gpu_client_tracing_id_(
-          memory_instrumentation::mojom::kServiceTracingProcessId) {}
+          memory_instrumentation::mojom::kServiceTracingProcessId),
+      is_visible_(
+          !base::FeatureList::IsEnabled(kDefaultDisableGpuLaunchTimeout)) {}
 
 BrowserGpuChannelHostFactory::~BrowserGpuChannelHostFactory() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
