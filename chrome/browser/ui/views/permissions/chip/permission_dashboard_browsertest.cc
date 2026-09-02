9398750899babf95a3a5c95953c8b0f5a8befdaa
Camillia Smith Barnes <cammie@chromium.org>
2026-09-02 08:20:00 -0700

    WebUILocationBar: Fix PermissionDashboardBrowserTest
    Fix PermissionDashboardBrowserTest location_bar accessor.
    
    PermissionDashboardBrowserTest previously accessed LocationBarView
    directly via BrowserView::GetBrowserViewForBrowser(). To decouple the
    test from concrete Views implementation details and rely on the public
    interface, retrieve LocationBar through BrowserWindowFeatures instead.
    
    TAG=agy
    CONV=4a2021c7-892e-4b89-b20f-596a24e94ba2
    
    Bug: 555781385
    Change-Id: Ibe065e394b8864d4d8c815022930be63c800d12a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8335556
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690753}

--- chrome/browser/ui/views/permissions/chip/permission_dashboard_browsertest.cc
+++ chrome/browser/ui/views/permissions/chip/permission_dashboard_browsertest.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/location_bar/location_bar.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/permissions/chip/permission_chip_interface.h"
 #include "chrome/browser/ui/views/permissions/chip/permission_chip_view.h"
 #include "chrome/browser/ui/views/permissions/chip/permission_dashboard_controller.h"
@@ -93,8 +93,7 @@ class PermissionDashboardBrowserTest : public InProcessBrowserTest {
   }
 
   LocationBar* location_bar() {
-    return BrowserView::GetBrowserViewForBrowser(browser())
-        ->GetLocationBarView();
+    return browser()->GetFeatures().location_bar();
   }
 
   PermissionDashboardController* dashboard_controller() {
