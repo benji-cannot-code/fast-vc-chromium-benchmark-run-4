5965172aae00e2035d4a0b41d4e2c47c691eabc9
Lingqi Chi <lingqi@chromium.org>
2026-09-02 00:16:13 -0700

    [prerender] allow prerender-until-script to use initiator's process by default
    It will be working behind a feature flag of
    kPrerenderUntilScriptProcessReuse, which will work as a kill switch for
    performance testing.
    
    Bug: 524800804
    Change-Id: I36899d29cf629ac7242db46697d9606497c8b8b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287844
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690502}

--- content/browser/preloading/prerender/prerender_features.cc
+++ content/browser/preloading/prerender/prerender_features.cc
@@ -63,6 +63,9 @@ BASE_FEATURE(kPrerender2WarmUpCompositorForNonImmediate,
 
 BASE_FEATURE(kPrerenderUntilScriptUpgrade, base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kPrerenderUntilScriptProcessReuse,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kPrerender2ReuseInitiatorProcess,
              base::FEATURE_DISABLED_BY_DEFAULT);
 const base::FeatureParam<std::string>
