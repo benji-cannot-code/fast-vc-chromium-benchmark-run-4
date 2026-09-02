82d4687f15b91cde7a5e3129f7a38d2716a4a8c9
Bryan Kim <bryankim@google.com>
2026-09-02 10:49:10 -0700

    [Bedrock] Modularize 5 Views Extensions leaf UI modules
    Extract the following 5 Views Extensions leaf UI modules from
    static_library("ui") in chrome/browser/ui/BUILD.gn into dedicated
    modular targets in chrome/browser/ui/views/extensions/BUILD.gn:
    - :extension_install_dialog_view
    - :extensions_request_access_button
    - :extensions_toolbar_button
    - :extension_view_views
    - :extension_popup
    
    TAG=agy
    CONV=cbf2589a-3230-4cd2-879c-a58589a52642
    
    Bug: 406646549
    Change-Id: I8bc1faf7e43272509bf41196bc8700d762b6155a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8329727
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690914}

--- chrome/browser/extensions/extension_view_host_web_modal_handler.cc
+++ chrome/browser/extensions/extension_view_host_web_modal_handler.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "chrome/browser/platform_util.h"
-#include "chrome/browser/ui/views/extensions/extension_popup.h"
+#include "chrome/browser/ui/extensions/extension_popup_types.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/web_contents.h"
 
@@ -60,7 +60,7 @@ gfx::Size ExtensionViewHostWebModalHandler::GetMaximumDialogSize() {
   // up to the maximum popup dimensions instead of being constrained to the
   // current popup size. This gives dialogs room to use their preferred size
   // while keeping them appropriately scoped.
-  return ExtensionPopup::kMaxSize;
+  return kExtensionPopupMaxSize;
 }
 
 bool ExtensionViewHostWebModalHandler::ShouldConstrainDialogBoundsByHost() {
