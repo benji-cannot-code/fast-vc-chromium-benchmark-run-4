838cd246705acecb6f5de31f3fc5c2b7c48252a5
Federica Germinario <fedegermi@google.com>
2026-09-02 06:29:44 -0700

    [ios] Fix flakyness for all tests in PasswordSuggestionEGTest
    This CL moves the 1-second synchronization delay
    (base::test::ios::SpinRunLoopWithMinDelay) introduced in
    testShowKeyboardFromButtonOnProactiveBottomSheet directly into the
    shared `loadSignupPage` helper. This ensures that all test cases loading
    the signup page wait for these asynchronous tasks to complete before
    field interaction, while eliminating redundant delays at individual call
    sites.
    
    Bug: 508127830
    Change-Id: I05b46079789709b5e4a9beb7b0c3f0c20694f709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8341052
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690654}

--- ios/chrome/browser/passwords/password_suggestion/test/password_suggestion_egtest.mm
+++ ios/chrome/browser/passwords/password_suggestion/test/password_suggestion_egtest.mm
@@ -128,6 +128,10 @@ id<GREYMatcher> ProactivePasswordGenerationUseKeyboardButton() {
   [ChromeEarlGrey loadURL:self.testServer->GetURL(
                               "/new_password_and_confirmation_form.html")];
   [ChromeEarlGrey waitForWebStateContainingText:"Signup form."];
+
+  // TODO(crbug.com/508127830): Find a better solution to fix the flakiness.
+  // Wait for asynchronous tasks to finish before interacting with the field.
+  base::test::ios::SpinRunLoopWithMinDelay(base::Seconds(1));
 }
 
 - (void)loadSignupAutofocusPage {
@@ -216,10 +220,6 @@ id<GREYMatcher> ProactivePasswordGenerationUseKeyboardButton() {
 // tapped.
 - (void)testShowKeyboardFromButtonOnProactiveBottomSheet {
   [self loadSignupPage];
-
-  // TODO(crbug.com/508127830): Find a better solution to fix the flakiness.
-  // Wait for asynchronous tasks to finish before interacting with the field.
-  base::test::ios::SpinRunLoopWithMinDelay(base::Seconds(1));
 
   [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
       performAction:chrome_test_util::TapWebElementWithId(kNewPasswordFieldID)];
