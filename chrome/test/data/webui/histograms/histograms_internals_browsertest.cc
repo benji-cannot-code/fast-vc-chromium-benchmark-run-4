4c063e4c775270f1ff7d2548c2470cc4055d2b48
Ahmed Raslan <rslan@google.com>
2026-09-02 06:37:55 -0700

    Gardener: Disable HistogramsInternalsUIBrowserTest.RefreshHistograms on Linux
    This test is flaking and bringing down gardened Linux builders.
    Disabling on Linux only to preserve coverage on other platforms.
    
    SKIP_DISABLING_SUITES_CHECK: Gardener disabling flaky test to fix the tree.
    NOTREECHECKS=true
    NOTRY=true
    
    Bug: 543826418
    Change-Id: I0a21daeaa11782e41c7a8e4275a75c2214b70b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346768
    Reviewed-by: Ahmed Raslan <rslan@google.com>
    Owners-Override: Ahmed Raslan <rslan@google.com>
    Commit-Queue: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Reviewed-by: Anatoli Hancharou <antoli@google.com>
    Reviewed-by: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Auto-Submit: Ahmed Raslan <rslan@google.com>
    Reviewed-by: Aliaksei Bahdzevich <thelex@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690661}

--- chrome/test/data/webui/histograms/histograms_internals_browsertest.cc
+++ chrome/test/data/webui/histograms/histograms_internals_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test.h"
@@ -35,7 +36,14 @@ class HistogramsInternalsUIBrowserTest : public WebUIMochaBrowserTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(HistogramsInternalsUIBrowserTest, RefreshHistograms) {
+// TODO(b/543826418): Flaky on Linux.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_RefreshHistograms DISABLED_RefreshHistograms
+#else
+#define MAYBE_RefreshHistograms RefreshHistograms
+#endif
+IN_PROC_BROWSER_TEST_F(HistogramsInternalsUIBrowserTest,
+                       MAYBE_RefreshHistograms) {
   PopulateHistograms();
   RunTestCase("RefreshHistograms");
 }
