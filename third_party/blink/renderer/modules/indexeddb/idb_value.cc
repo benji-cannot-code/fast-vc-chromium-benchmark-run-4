a95ed5033dc6441846b569a2387f98a7c3fd4086
Evan Stade <evanstade@microsoft.com>
2026-09-01 22:42:41 -0700

    IDB: also avoid FD exhaustion when *reading* many large values
    (on platforms where shared memory handles are FDs)
    
    This doesn't make FD exhaustion completely impossible, but much less
    probable.
    
    Bug: 342779913
    Change-Id: I82639607133ced4e235ab139d69c47f8818f68a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258915
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1690471}

--- third_party/blink/renderer/modules/indexeddb/idb_value.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_value.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
+#include "build/build_config.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom-blink.h"
 #include "third_party/blink/public/platform/web_blob_info.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
@@ -99,6 +100,17 @@ void IDBValue::SetData(mojo_base::BigBuffer new_data) {
   }
 
   data_from_mojo_ = std::move(new_data);
+
+#if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_APPLE)
+  // Remove the bytes from shared memory to free up the FD and avoid FD
+  // exhaustion. See crbug.com/342779913. Apple uses Mach send rights for shared
+  // memory handles rather than FDs; see `PlatformSharedMemoryHandle`.
+  //
+  // This extra copy is inefficient and unnecessary in the case where FD
+  // pressure is not a concern. We might consider doing this more heuristically
+  // (e.g. only after N BigBuffers are in use concurrently).
+  data_from_mojo_.MakePrivateBytes();
+#endif
 }
 
 scoped_refptr<BlobDataHandle> IDBValue::TakeLastBlob() {
