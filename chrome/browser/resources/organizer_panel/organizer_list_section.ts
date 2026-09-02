f5838a352ff58feba772da311b5451a737c06fcc
Alison Gale <agale@chromium.org>
2026-09-01 17:53:08 -0700

    [Organizer] Pipe search query into the organizer section
    Step 1 of implementing search. This just hides all the entries if any
    search query is entered.
    
    Bug: 555343269
    Change-Id: I3dd346cccc8dc24f4de34557c4ba1dc8b6c00ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8337279
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690367}

--- chrome/browser/resources/organizer_panel/organizer_list_section.ts
+++ chrome/browser/resources/organizer_panel/organizer_list_section.ts
@@ -47,12 +47,14 @@ export class OrganizerListSectionElement extends CrLitElement implements
       delegate: {type: Object},
       items: {type: Array},
       expanded_: {type: Boolean},
+      searchQuery: {type: String},
     };
   }
 
   accessor delegate: OrganizerListSectionDelegate<unknown>|null = null;
   accessor items: Array<OrganizerListSectionItem<unknown>> = [];
   protected accessor expanded_: boolean = false;
+  accessor searchQuery: string = '';
 
   // The panel WebUI will remain loaded but invisible when the panel is closed.
   // While invisible, the WebUI will not receive update events from the browser,
@@ -95,18 +97,18 @@ export class OrganizerListSectionElement extends CrLitElement implements
   }
 
   protected getInitialItems_(): Array<OrganizerListSectionItem<unknown>> {
-    return this.items.slice(0, INITIAL_ITEM_COUNT);
+    return this.getFilteredItems_().slice(0, INITIAL_ITEM_COUNT);
   }
 
   protected getRemainingItems_(): Array<OrganizerListSectionItem<unknown>> {
     if (!this.expanded_) {
       return [];
     }
-    return this.items.slice(INITIAL_ITEM_COUNT);
+    return this.getFilteredItems_().slice(INITIAL_ITEM_COUNT);
   }
 
   protected hasMoreItems_(): boolean {
-    return this.items.length > INITIAL_ITEM_COUNT;
+    return this.getFilteredItems_().length > INITIAL_ITEM_COUNT;
   }
 
   protected onExpandedChanged_(e: CustomEvent<{value: boolean}>) {
@@ -117,6 +119,10 @@ export class OrganizerListSectionElement extends CrLitElement implements
     const target = e.currentTarget as OrganizerListSectionItemElement;
     assert(target.item);
     this.delegate?.onItemClick(target.item);
+  }
+
+  protected getFilteredItems_(): Array<OrganizerListSectionItem<unknown>> {
+    return this.searchQuery ? [] : this.items;
   }
 }
 
