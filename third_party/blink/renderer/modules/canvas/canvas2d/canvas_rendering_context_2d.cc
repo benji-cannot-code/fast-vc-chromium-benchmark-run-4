f2840522db27635316eda5c88b9827d19f1ad799
Colin Blundell <blundell@chromium.org>
2026-09-02 06:39:53 -0700

    [Blink] Align CRC2D manual teardown sites with ResetResourceProvider
    In preparation for unifying provider teardown in CanvasRenderingContext2D, this CL updates the manual provider destruction sites in SizeChanged(), Dispose(),
    and LoseContext() to perform the exact same operations as
    ResetResourceProvider() (specifically, disconnecting provider delegates
    prior to destruction and invoking
    HTMLCanvasElement::UpdateMemoryUsage()).
    
    Bug: 352263194
    Change-Id: I5d0779cb32a79c5859cbbf72a99114cff1f09b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8330368
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690665}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -304,9 +304,16 @@ void CanvasRenderingContext2D::LoseContext(LostContextMode lost_mode) {
   ResetInternal();
   HTMLCanvasElement* const element = canvas();
   if (element != nullptr) [[likely]] {
-    shared_image_provider_ = nullptr;
-    bitmap_provider_ = nullptr;
+    auto old_shared = std::move(shared_image_provider_);
+    auto old_bitmap = std::move(bitmap_provider_);
     last_recording_ = std::nullopt;
+    element->UpdateMemoryUsage();
+    if (old_shared) {
+      old_shared->SetDelegate(nullptr);
+    }
+    if (old_bitmap) {
+      old_bitmap->SetDelegate(nullptr);
+    }
     element->DiscardResources();
     element->DiscardResourceDispatcher();
 
@@ -1166,9 +1173,18 @@ UniqueFontSelector* CanvasRenderingContext2D::GetFontSelector() const {
 }
 
 void CanvasRenderingContext2D::SizeChanged() {
-  shared_image_provider_ = nullptr;
-  bitmap_provider_ = nullptr;
+  auto old_shared = std::move(shared_image_provider_);
+  auto old_bitmap = std::move(bitmap_provider_);
   last_recording_ = std::nullopt;
+  if (canvas()) {
+    canvas()->UpdateMemoryUsage();
+  }
+  if (old_shared) {
+    old_shared->SetDelegate(nullptr);
+  }
+  if (old_bitmap) {
+    old_bitmap->SetDelegate(nullptr);
+  }
   did_fail_to_create_resource_provider_ = false;
 }
 
@@ -1180,9 +1196,18 @@ CanvasHibernationHandler* CanvasRenderingContext2D::GetHibernationHandler()
 void CanvasRenderingContext2D::Dispose() {
   FlushForImageListener::Get()->RemoveObserver(this);
   hibernation_handler_ = nullptr;
-  shared_image_provider_ = nullptr;
-  bitmap_provider_ = nullptr;
+  auto old_shared = std::move(shared_image_provider_);
+  auto old_bitmap = std::move(bitmap_provider_);
   last_recording_ = std::nullopt;
+  if (canvas()) {
+    canvas()->UpdateMemoryUsage();
+  }
+  if (old_shared) {
+    old_shared->SetDelegate(nullptr);
+  }
+  if (old_bitmap) {
+    old_bitmap->SetDelegate(nullptr);
+  }
   CanvasRenderingContext::Dispose();
 }
 
