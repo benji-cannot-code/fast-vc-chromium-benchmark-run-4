8483bc6041bc1b2ccc4fff45be999a1ebf00ba91
Saifuddin Hitawala <hitawala@chromium.org>
2026-09-02 13:17:10 -0700

    [media] Reject different VF/SI color space in VideoFrame mojo traits
    Now that Wrap(Mappable)SharedImage sets color space from SharedImage,
    update VideoFrame mojom traits to reject VideoFrame creation if the
    incoming VideoFrame color space differs from SharedImage color space.
    This was detected by a fuzzer.
    
    Bug: 551047531
    Change-Id: Iecb4159dea7787645ddf0a76b463a7c84d88650e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8322707
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691025}

--- media/mojo/mojom/video_frame_mojom_traits.cc
+++ media/mojo/mojom/video_frame_mojom_traits.cc
@@ -278,6 +278,12 @@ bool StructTraits<media::mojom::VideoFrameDataView,
     return false;
   }
 
+  gfx::ColorSpace color_space;
+  if (!input.ReadColorSpace(&color_space)) {
+    DLOG(ERROR) << "Failed to read color_space";
+    return false;
+  }
+
   scoped_refptr<media::VideoFrame> frame;
   if (data.is_shared_memory_data()) {
     media::mojom::SharedMemoryVideoFrameDataDataView shared_memory_data;
@@ -434,6 +440,13 @@ bool StructTraits<media::mojom::VideoFrameDataView,
       DLOG(ERROR) << "coded_size (" << coded_size.ToString()
                   << ") does not match shared_image size ("
                   << shared_image->size().ToString() << ")";
+      return false;
+    }
+
+    if (color_space != shared_image->color_space()) {
+      DLOG(ERROR) << "color_space (" << color_space.ToString()
+                  << ") does not match shared_image color_space ("
+                  << shared_image->color_space().ToString() << ")";
       return false;
     }
 
@@ -681,12 +694,6 @@ bool StructTraits<media::mojom::VideoFrameDataView,
   }
 
   frame->set_metadata(metadata);
-
-  gfx::ColorSpace color_space;
-  if (!input.ReadColorSpace(&color_space)) {
-    DLOG(ERROR) << "Failed to read color_space";
-    return false;
-  }
   frame->set_color_space(color_space);
 
   gfx::HDRMetadata hdr_metadata;
