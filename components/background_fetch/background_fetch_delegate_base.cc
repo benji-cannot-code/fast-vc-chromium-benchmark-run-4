52bc5c04ad0b441ec9ed88696ff401be9a432840
Wonik Choi <choiwonik91@gmail.com>
2026-09-02 10:02:46 -0700

    Remove stale Background Fetch cancellation TODOs
    Background Fetch tracks active downloads and ignores callbacks for GUIDs
    it no longer tracks. The existing TODOs suggest that DownloadService
    should suppress callbacks for cancelled downloads so these guards can
    become DCHECKs.
    
    DownloadService clients are responsible for deciding whether callbacks
    are still relevant. Keep the existing client-side guards and remove the
    stale TODOs. There is no behavior change.
    
    Previous discussion:
    https://chromium-review.googlesource.com/c/chromium/src/+/8277215
    
    Bug: 40546930
    Change-Id: Ie64ae007ae81a76013cea352bfb02aae77478c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8316987
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690861}

--- components/background_fetch/background_fetch_delegate_base.cc
+++ components/background_fetch/background_fetch_delegate_base.cc
@@ -287,8 +287,6 @@ void BackgroundFetchDelegateBase::OnDownloadStarted(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
-  // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
-  // OnDownload* methods, then this can be a DCHECK.
   if (download_job_id_iter == download_job_id_map_.end()) {
     return;
   }
@@ -312,8 +310,6 @@ void BackgroundFetchDelegateBase::OnDownloadUpdated(
     uint64_t bytes_downloaded) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
-  // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
-  // OnDownload* methods, then this can be a DCHECK.
   if (download_job_id_iter == download_job_id_map_.end()) {
     return;
   }
@@ -347,9 +343,6 @@ void BackgroundFetchDelegateBase::OnDownloadFailed(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
-  // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs
-  // potentially calling OnDownloadFailed with a reason other than
-  // CANCELLED/ABORTED, we should add a DCHECK here.
   if (download_job_id_iter == download_job_id_map_.end()) {
     return;
   }
@@ -379,8 +372,6 @@ void BackgroundFetchDelegateBase::OnDownloadSucceeded(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
-  // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
-  // OnDownload* methods, then this can be a DCHECK.
   if (download_job_id_iter == download_job_id_map_.end()) {
     return;
   }
@@ -488,8 +479,6 @@ void BackgroundFetchDelegateBase::GetUploadData(
     const std::string& download_guid,
     download::GetUploadDataCallback callback) {
   auto job_it = download_job_id_map_.find(download_guid);
-  // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
-  // client methods, then this can be a DCHECK.
   if (job_it == download_job_id_map_.end()) {
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE, base::BindOnce(std::move(callback),
