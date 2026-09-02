62bb2713396f5291e2d9b7af7f40f3f25862c71b
Nico Weber <thakis@chromium.org>
2026-09-02 07:59:23 -0700

    Update Android RISC-V 64 minimum API level to 37
    The Android NDK sysroot only provides RISC-V 64 libraries starting from
    API level 37. This updates the minimum API level for riscv64 targets
    from 36 to 37 in both the GN build configuration and the Clang build
    script.
    
    Bug: 555690120,555796821
    Cq-Include-Trybots: luci.chromium.try:android-cronet-riscv64-rel,android-cronet-riscv64-dbg
    Change-Id: I7fc2c7c3c7ec3dca530812d900339a5793a2c2be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8310575
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690733}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -1727,7 +1727,7 @@ def main():
         target_triple = 'armv7'
       api_level = '21'
       if target_arch == 'riscv64':
-        api_level = '36'
+        api_level = '37'
       target_triple += '-linux-android' + api_level
       android_cflags = [
         '--sysroot=%s/sysroot' % toolchain_dir,
