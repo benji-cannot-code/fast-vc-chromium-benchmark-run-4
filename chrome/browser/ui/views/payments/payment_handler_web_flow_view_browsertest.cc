d8381aa20a1efc6c7080f77059311a1a11044f2f
Tigran Bantikyan <bantikyan@chromium.org>
2026-09-02 08:33:11 -0700

    [Gardener] Disable flaky ErrorMessageShownOnErrorAfterUserInteraction on Windows
    PaymentHandlerWebFlowViewMandatoryUiDisabledTest.ErrorMessageShownOnErrorAfterUserInteraction
    is flaky on Windows builders with a ~27% failure rate. It seems that
    after a fix checked in in July, the failure rate dropped from ~70%, but
    it is still high.
    
    Bug: 531590249
    Change-Id: I01ac30f538fead71d2367396100dd502eb942d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8340090
    Owners-Override: Tigran Bantikyan <bantikyan@chromium.org>
    Commit-Queue: Tigran Bantikyan <bantikyan@chromium.org>
    Reviewed-by: Ahmed Raslan <rslan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690768}

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/permissions/one_time_permissions_tracker_helper.h"
@@ -521,8 +522,16 @@ IN_PROC_BROWSER_TEST_F(PaymentHandlerWebFlowViewMandatoryUiDisabledTest,
   ASSERT_TRUE(WaitForObservedEvent());
 }
 
+// TODO(crbug.com/531590249): Re-enable this test
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_ErrorMessageShownOnErrorAfterUserInteraction \
+  DISABLED_ErrorMessageShownOnErrorAfterUserInteraction
+#else
+#define MAYBE_ErrorMessageShownOnErrorAfterUserInteraction \
+  ErrorMessageShownOnErrorAfterUserInteraction
+#endif
 IN_PROC_BROWSER_TEST_F(PaymentHandlerWebFlowViewMandatoryUiDisabledTest,
-                       ErrorMessageShownOnErrorAfterUserInteraction) {
+                       MAYBE_ErrorMessageShownOnErrorAfterUserInteraction) {
   NavigateTo("/payment_handler.html");
   std::string method_name;
   InstallPaymentApp("a.com",
