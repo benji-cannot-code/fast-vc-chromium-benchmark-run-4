c9dd465e5958e044ed588ed885b94b7538b0c102
Hidehiko Abe <hidehiko@chromium.org>
2026-09-01 21:32:21 -0700

    Fixup Profile for SystemWebApp launch.
    In ChromeOS, there are more variation of Profiles than typical
    user Profiles.
    This CL introduces the checking function to be shared with
    other places so that we can fix the check in one place
    in the future, too.
    
    Bug: 40146603
    Test: Tryjob
    Change-Id: I3e12f2df3fe574c9fda75a1630e9e7770afd3799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8284660
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690454}

--- chrome/browser/ash/app_list/search/help_app_zero_state_provider_unittest.cc
+++ chrome/browser/ash/app_list/search/help_app_zero_state_provider_unittest.cc
@@ -17,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_list/search/test/test_search_controller.h"
 #include "chrome/browser/ash/app_list/test/test_app_list_controller.h"
 #include "chrome/test/base/testing_profile.h"
+#include "chromeos/ash/components/browser_context_helper/annotated_account_id.h"
 #include "chromeos/strings/grit/chromeos_strings.h"
 #include "components/prefs/pref_service.h"
+#include "components/user_manager/test_helper.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace app_list::test {
@@ -45,7 +47,13 @@ class HelpAppZeroStateProviderTest : public AppListTestBase {
   ~HelpAppZeroStateProviderTest() override = default;
 
   void SetUp() override {
+    // TODO(crbug.com/477191550): Move to the base class.
+    const AccountId account_id = AccountId::FromUserEmailGaiaId(
+        TestingProfile::kDefaultProfileUserName, GaiaId("123456789"));
+    ASSERT_TRUE(user_manager::TestHelper(user_manager::UserManager::Get())
+                    .AddRegularUser(account_id));
     AppListTestBase::SetUp();
+    ash::AnnotatedAccountId::Set(profile(), account_id);
 
     app_list_notifier_ =
         std::make_unique<AppListNotifierImpl>(&app_list_controller_);
