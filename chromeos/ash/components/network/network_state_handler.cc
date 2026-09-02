f826ed1b04d4572493cf261c4466587e524e6530
Pavol Marko <pmarko@google.com>
2026-09-02 02:12:39 -0700

    Don't send notifications on no-op SetNetworkConnectRequested
    If SetNetworkConnectRequested is called but does not change effective
    state, avoid sending out notifications.
    
    Bug: 458838218
    Bug: 448640826
    Test: chromeos_unittests
    Change-Id: I27367ea7d06697b2412b68cb202c50277a8d89c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256408
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Chad Duffin <chadduffin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690548}

--- chromeos/ash/components/network/network_state_handler.cc
+++ chromeos/ash/components/network/network_state_handler.cc
@@ -622,6 +622,10 @@ void NetworkStateHandler::SetNetworkConnectRequested(
   if (!network) {
     return;
   }
+  if (network->connect_requested_ == connect_requested &&
+      network->shill_connect_error_.empty()) {
+    return;
+  }
   network->connect_requested_ = connect_requested;
   network->shill_connect_error_.clear();
   network_list_sorted_ = false;
