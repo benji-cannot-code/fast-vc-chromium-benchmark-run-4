c286b9d0f3ab6d1225b3b0239be6eb7f04121018
Dibyajyoti Pal <dibyapal@google.com>
2026-09-01 18:06:18 -0700

    [PWA] Remove unused WebAppUninstallCommand ctor overloads
    This CL removes some unused WebAppUninstallCommand ctor overloads that
    were being used for the RemoveWebAppJob. This completes the cleanup that
    started in crrev.com/c/5282614.
    
    Change-Id: I75dd49abe4ef53ed581be3eeb7583349d337beee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8333761
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Kunjan Patel <ksukh@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690387}

--- chrome/browser/web_applications/commands/web_app_uninstall_command.cc
+++ chrome/browser/web_applications/commands/web_app_uninstall_command.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/commands/web_app_command.h"
 #include "chrome/browser/web_applications/jobs/uninstall/remove_install_source_job.h"
 #include "chrome/browser/web_applications/jobs/uninstall/remove_install_url_job.h"
-#include "chrome/browser/web_applications/jobs/uninstall/remove_web_app_job.h"
 #include "chrome/browser/web_applications/jobs/uninstall/uninstall_job.h"
 #include "chrome/browser/web_applications/locks/all_apps_lock.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
@@ -50,17 +49,6 @@ WebAppUninstallCommand::CreateForRemoveInstallManagements(
     UninstallJob::Callback callback) {
   return base::WrapUnique(new WebAppUninstallCommand(
       uninstall_source, profile, app_id, install_sources, std::move(callback)));
-}
-
-// static
-std::unique_ptr<WebAppUninstallCommand>
-WebAppUninstallCommand::CreateForRemoveUserUninstallableManagement(
-    webapps::WebappUninstallSource uninstall_source,
-    Profile& profile,
-    webapps::AppId app_id,
-    UninstallJob::Callback callback) {
-  return base::WrapUnique(new WebAppUninstallCommand(
-      uninstall_source, profile, app_id, std::move(callback)));
 }
 
 WebAppUninstallCommand::WebAppUninstallCommand(
@@ -103,30 +91,6 @@ WebAppUninstallCommand::WebAppUninstallCommand(
           *GetMutableDebugValue().EnsureDict("remove_install_source_job"),
           app_id,
           install_managements)) {
-  // Track an uninstallation event, regardless of whether it succeeded or not.
-  webapps::InstallableMetrics::TrackUninstallEvent(uninstall_source);
-}
-
-WebAppUninstallCommand::WebAppUninstallCommand(
-    webapps::WebappUninstallSource uninstall_source,
-    Profile& profile,
-    webapps::AppId app_id,
-    UninstallJob::Callback callback)
-    : WebAppCommand<AllAppsLock, webapps::UninstallResultCode>(
-          "WebAppUninstallCommand",
-          AllAppsLockDescription(),
-          std::move(callback),
-          /*args_for_shutdown=*/webapps::UninstallResultCode::kShutdown),
-      job_(std::make_unique<RemoveInstallSourceJob>(
-          uninstall_source,
-          profile,
-          *GetMutableDebugValue().EnsureDict("remove_web_app_job"),
-          app_id,
-          kUserUninstallableSources)) {
-  CHECK(webapps::IsUserUninstall(uninstall_source))
-      << "The uninstall source for removing all user-installable install "
-         "management types must be a user uninstall source. Source:"
-      << uninstall_source;
   // Track an uninstallation event, regardless of whether it succeeded or not.
   webapps::InstallableMetrics::TrackUninstallEvent(uninstall_source);
 }
