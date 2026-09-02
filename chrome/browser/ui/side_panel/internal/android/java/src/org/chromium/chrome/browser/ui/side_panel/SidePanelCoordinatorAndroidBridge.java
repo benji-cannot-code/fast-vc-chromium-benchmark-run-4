d5004fb48c68f5ee65fcf88f1031d3d2a37f0007
Charles Hager <clhager@google.com>
2026-09-02 11:43:24 -0700

    Add native logic for getting SP state for Tab
    This change adds a `TabAndroid` input to `::HasContentToShow()` to check
    whether content should be showed for a specific tab, rather than just
    the "active" tab.
    
    Bug: 541376517
    Change-Id: I2bf77c2b38291fded64de45b09e1c8c3f20994f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264305
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690961}

--- chrome/browser/ui/side_panel/internal/android/java/src/org/chromium/chrome/browser/ui/side_panel/SidePanelCoordinatorAndroidBridge.java
+++ chrome/browser/ui/side_panel/internal/android/java/src/org/chromium/chrome/browser/ui/side_panel/SidePanelCoordinatorAndroidBridge.java
@@ -54,14 +54,13 @@ final class SidePanelCoordinatorAndroidBridge implements ChromeAndroidTaskFeatur
      * @see org.chromium.chrome.browser.ui.side_ui.SideUiContainer#hasContentToShow(Tab)
      */
     boolean hasContentToShow(Tab tab) {
-        // TODO(crbug.com/541376517): Update this method to actually pass the tab to native.
         boolean hasContentToShow =
                 mNativeSidePanelCoordinatorAndroid != 0
                         ? SidePanelCoordinatorAndroidBridgeJni.get()
-                                .hasContentToShow(mNativeSidePanelCoordinatorAndroid)
+                                .hasContentToShow(mNativeSidePanelCoordinatorAndroid, tab)
                         : false;
 
-        log(TAG, "hasContentToShow", hasContentToShow);
+        log(TAG, "hasContentToShow", hasContentToShow, "Tab#" + tab.getId());
         return hasContentToShow;
     }
 
@@ -285,7 +284,8 @@ final class SidePanelCoordinatorAndroidBridge implements ChromeAndroidTaskFeatur
         void init(long nativeSidePanelCoordinatorAndroid);
 
         /** See {@link SidePanelCoordinatorAndroidBridge#hasContentToShow}. */
-        boolean hasContentToShow(long nativeSidePanelCoordinatorAndroid);
+        boolean hasContentToShow(
+                long nativeSidePanelCoordinatorAndroid, @JniType("TabAndroid*") Tab tab);
 
         /** See {@link SidePanelCoordinatorAndroidBridge#onPanelContainerUpdated}. */
         void onPanelContainerUpdated(
--- chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.h
+++ chrome/browser/ui/side_panel/internal/android/side_panel_coordinator_android.h
@@ -55,7 +55,7 @@ class SidePanelCoordinatorAndroid : public SidePanelUIBase {
   void Init();
   void Destroy();
   void ClosePanel(bool suppress_animations);
-  bool HasContentToShow();
+  bool HasContentToShow(TabAndroid* tab);
   void OnPanelContainerUpdated(int old_width, int new_width);
   void OnPanelContentReplaced();
   void OnActiveChanged(bool active);
