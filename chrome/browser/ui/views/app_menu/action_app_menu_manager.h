bb1bdff59aa738a65654ddd1238e82f5645d8f87
Jason Jiang <jasonnjiang@google.com>
2026-09-02 15:12:07 -0700

    [ChroMenu Glowup] Tab Group Dynamic Menu
    - New class to create the Tab Group Dynamic Menu
    
    Change-Id: Icd12ad0637e9a7206693721adf17dfc075a82973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269600
    Commit-Queue: Jason Jiang <jasonnjiang@google.com>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691130}

--- chrome/browser/ui/views/app_menu/action_app_menu_manager.h
+++ chrome/browser/ui/views/app_menu/action_app_menu_manager.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class RecentTabsDynamicMenu;
 class BookmarksDynamicMenu;
+class TabGroupDynamicMenu;
 
 // Manages the ActionItem hierarchy for the Action App Menu, including
 // constructing the menu tree and managing dynamic submenus.
@@ -46,8 +47,7 @@ class ActionAppMenuManager {
       DisplayType display_type,
       std::optional<ui::ColorId> container_color = std::nullopt,
       std::optional<std::u16string> text_override = std::nullopt,
-      std::optional<ui::ImageModel> icon_override = std::nullopt,
-      std::optional<base::Uuid> saved_tab_group_guid = std::nullopt);
+      std::optional<ui::ImageModel> icon_override = std::nullopt);
 
   static std::unique_ptr<actions::ActionItem> CreateSectionActionItem(
       DisplayType display_type,
@@ -82,6 +82,7 @@ class ActionAppMenuManager {
   raw_ptr<BrowserWindowInterface> browser_window_interface_;
   std::unique_ptr<RecentTabsDynamicMenu> recent_tabs_menu_;
   std::unique_ptr<BookmarksDynamicMenu> bookmarks_menu_;
+  std::unique_ptr<TabGroupDynamicMenu> tab_groups_menu_;
 };
 
 DECLARE_UI_CLASS_PROPERTY_TYPE(ActionAppMenuManager::DisplayType)
