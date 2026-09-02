0757051560b6068d0c3bf911bec5fd32bcd6c52e
Vasili Svirski <vsvirski@google.com>
2026-09-02 05:15:58 -0700

    [Device Trust] Move and decouple attester unittests to components
    Move device_attester_unittest.cc and profile_attester_unittest.cc from
    chrome/browser/ down into
    components/enterprise/device_trust/core/attestation/.
    
    -Move MockDeviceTrustKeyManager into
    components/enterprise/device_trust/core:test_support so it can be reused
    by components and mobile tests.
    - Decouple ProfileAttesterTest from TestingProfile, TestingProfileManager, and content::BrowserTaskEnvironment by using the standalone ProfileIdService test constructor and SingleThreadTaskEnvironment.
    - Decouple DeviceAttesterTest from ScopedKeyPersistenceDelegateFactory
     by returning dummy key/signature data directly via
     MockDeviceTrustKeyManager.
     - Update BUILD.gn targets to register the moved tests in
    components_unittests.
    
    One-pager: go/cbe-cep-dtc-ios-refactoring-one-pager
    Bug: 540811203
    Bypass-Check-License: Moved files
    Change-Id: If75828cc9f1df8d5c5e161b0941a0b658fd0f0e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8304714
    Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Mikołaj Wałachowski <mwalachowski@google.com>
    Reviewed-by: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Commit-Queue: Vasili Svirski <vsvirski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690617}

--- components/enterprise/device_trust/core/mock_device_trust_key_manager.h
+++ components/enterprise/device_trust/core/mock_device_trust_key_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_BROWSER_MOCK_DEVICE_TRUST_KEY_MANAGER_H_
-#define CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_BROWSER_MOCK_DEVICE_TRUST_KEY_MANAGER_H_
+#ifndef COMPONENTS_ENTERPRISE_DEVICE_TRUST_CORE_MOCK_DEVICE_TRUST_KEY_MANAGER_H_
+#define COMPONENTS_ENTERPRISE_DEVICE_TRUST_CORE_MOCK_DEVICE_TRUST_KEY_MANAGER_H_
 
 #include "components/enterprise/device_trust/core/device_trust_key_manager.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -46,4 +46,4 @@ class MockDeviceTrustKeyManager : public DeviceTrustKeyManager {
 }  // namespace test
 }  // namespace enterprise_connectors
 
-#endif  // CHROME_BROWSER_ENTERPRISE_CONNECTORS_DEVICE_TRUST_KEY_MANAGEMENT_BROWSER_MOCK_DEVICE_TRUST_KEY_MANAGER_H_
+#endif  // COMPONENTS_ENTERPRISE_DEVICE_TRUST_CORE_MOCK_DEVICE_TRUST_KEY_MANAGER_H_
