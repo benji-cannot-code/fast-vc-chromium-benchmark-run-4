a30777c950a0f5ea19efa2ea8919c44b8d8d81bf
Julia Hansbrough <flowerhack@google.com>
2026-09-02 13:02:09 -0700

    Roll smallvec: 1.15.2 => 1.16.0 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * smallvec: 1.15.2 => 1.16.0; https://docs.rs/crate/smallvec/1.16.0
    
    Bug: None
    Change-Id: Id336965f43b386bcf97d89bbef2e6b3436e08647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339728
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691016}

--- third_party/rust/chromium_crates_io/vendor/smallvec-v1/src/arbitrary.rs
+++ third_party/rust/chromium_crates_io/vendor/smallvec-v1/src/arbitrary.rs
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-use crate::{Array, SmallVec};
-use arbitrary::{Arbitrary, Unstructured};
+use {
+    crate::{Array, SmallVec},
+    arbitrary::{Arbitrary, Unstructured},
+};
 
 impl<'a, A: Array> Arbitrary<'a> for SmallVec<A>
 where
