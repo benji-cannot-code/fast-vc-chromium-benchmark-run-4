e1d0d437c26b269ad93c73d7a83d2cd659f6a05c
Yuwei Huang <yuweih@chromium.org>
2026-09-01 17:47:16 -0700

    remoting: Detect headless systemd session dynamically in desktop process
    This is part of supporting multi-process custom sessions, as describe in
    go/crd-custom-multi-process#heading=h.66lomao58skd.
    
    Instead of unconditionally assuming multi-process desktop sessions are
    headless, query systemd via sd_pid_get_session() and
    sd_session_is_remote() to detect whether the session has Remote=yes.
    
    Add remoting::IsRunningInHeadlessSystemdSession() helper in
    remoting/host/linux/systemd_utils.h, with unit tests in
    systemd_utils_unittest.cc.
    
    Update GnomeRemoteDesktopSession::OnConnectionCreated() to invoke
    IsRunningInHeadlessSystemdSession() when process type is desktop.
    
    TAG=agy
    CONV=fc59dcab-6f02-49dc-bfc5-6daa5cb8b0fb
    
    Bug: 493980875, 475611769
    Change-Id: If17549bfb6981f881319abd73039e472c7e4381d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8342668
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690360}

--- remoting/host/linux/gnome_remote_desktop_session.cc
+++ remoting/host/linux/gnome_remote_desktop_session.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/linux/ei_keyboard_layout_monitor.h"
 #include "remoting/host/linux/gnome_desktop_display_info_monitor.h"
 #include "remoting/host/linux/screen_saver_inhibitor.h"
+#include "remoting/host/linux/systemd_utils.h"
 #include "remoting/proto/control.pb.h"
 
 namespace remoting {
@@ -159,12 +160,7 @@ void GnomeRemoteDesktopSession::OnConnectionCreated(
   const auto* command_line = base::CommandLine::ForCurrentProcess();
   if (command_line->GetSwitchValueASCII(kProcessTypeSwitchName) ==
       kProcessTypeDesktop) {
-    // For the multi-process Linux host, the desktop process is always run under
-    // a GDM remote display, so it is guaranteed to be headless.
-    // TODO: yuweih - This needs to be changed if we want to support custom
-    // sessions, or remoting the local session (if it becomes possible) in
-    // multi-process mode.
-    OnHeadlessDetection(/*is_headless=*/true);
+    OnHeadlessDetection(IsRunningInHeadlessSystemdSession());
     return;
   }
 
