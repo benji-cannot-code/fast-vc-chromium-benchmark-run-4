b1374db6257d5232c9de36e76fb34f2f026935a4
Morten Stenshorne <mstensho@chromium.org>
2026-09-02 10:16:41 -0700

    Add BoxFragmentBuilder::ShouldBreak().
    Some callers simply want to know if there's going to be a break or not.
    It was somewhat low-level having to both check if there's a child that
    needs a break OR if the node itself was too tall to fit.
    
    Rename ShouldBreakInside() to ShouldBreakInsideForContent(). Remove one
    such check inside FinishFragmentation(), since this code was already
    inside such a check.
    
    No behavior changes intended.
    
    Change-Id: Ifcb5c69539b86b31bdac58f34ae5df0ae43c25dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8331528
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1690878}

--- third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid_lanes/grid_lanes_layout_algorithm.cc
@@ -1788,11 +1788,9 @@ void GridLanesLayoutAlgorithm::PlaceOutOfFlowItems(
   HeapVector<Member<LayoutBox>> oofs;
   std::swap(oofs, oof_children);
 
-  bool should_process_block_end = true;
-  if (InvolvedInBlockFragmentation(container_builder_)) {
-    should_process_block_end = !container_builder_.DidBreakSelf() &&
-                               !container_builder_.ShouldBreakInside();
-  }
+  const bool should_process_block_end =
+      !InvolvedInBlockFragmentation(container_builder_) ||
+      !container_builder_.ShouldBreak();
 
   const LayoutUnit previously_consumed_block_size =
       GetBreakToken() ? GetBreakToken()->ConsumedBlockSize() : LayoutUnit();
