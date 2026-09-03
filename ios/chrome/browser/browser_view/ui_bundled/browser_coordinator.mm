ccedc32a8ffb85fe42ec00280e68f6541b3004c6
Gauthier Ambard <gambard@chromium.org>
2026-09-03 09:53:12 -0700

    [iOS] Add ShowPasswordsSettings to SettingsCommands
    This is necessary to move it out of the BrowserCoordinator.
    
    Bug: 542879805
    Change-Id: I8caea4079ec1c2bf790f652691ee1a34bdcf4733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8330410
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691693}

--- ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_coordinator.mm
@@ -173,8 +173,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/autofill/payments/coordinator/autofill_add_credit_card_coordinator.h"
 #import "ios/chrome/browser/settings/autofill/payments/coordinator/autofill_add_credit_card_coordinator_delegate.h"
 #import "ios/chrome/browser/settings/clear_browsing_data/coordinator/quick_delete_coordinator.h"
-#import "ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_coordinator.h"
-#import "ios/chrome/browser/settings/ui_bundled/password/password_settings/password_settings_coordinator_delegate.h"
 #import "ios/chrome/browser/shared/coordinator/alert/repost_form_coordinator.h"
 #import "ios/chrome/browser/shared/coordinator/alert/repost_form_coordinator_delegate.h"
 #import "ios/chrome/browser/shared/coordinator/default_browser_promo/non_modal_default_browser_promo_scheduler_scene_agent.h"
@@ -318,7 +316,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NotificationsOptInCoordinatorDelegate,
     OverscrollActionsControllerDelegate,
     PasswordControllerDelegate,
-    PasswordSettingsCoordinatorDelegate,
     PrerenderBrowserAgentDelegate,
     PromosManagerCommands,
     QuickDeleteCommands,
@@ -409,10 +406,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Coordinator for new tab pages.
 @property(nonatomic, strong) NewTabPageCoordinator* NTPCoordinator;
-
-// Coordinator for the password settings UI presentation.
-@property(nonatomic, strong)
-    PasswordSettingsCoordinator* passwordSettingsCoordinator;
 
 // Coordinator for the popup menu.
 @property(nonatomic, strong) PopupMenuCoordinator* popupMenuCoordinator;
@@ -870,13 +863,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.recentTabsCoordinator = nil;
 }
 
-// Stops the coordinator for password manager settings.
-- (void)stopPasswordSettingsCoordinator {
-  [self.passwordSettingsCoordinator stop];
-  self.passwordSettingsCoordinator.delegate = nil;
-  self.passwordSettingsCoordinator = nil;
-}
-
 - (void)setWebUsageEnabled:(BOOL)webUsageEnabled {
   if (!self.profile || !self.started) {
     return;
@@ -1319,8 +1305,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   /* NetExportCoordinator is created and started by a delegate method */
 
-  /* passwordSettingsCoordinator is created and started by a delegate method */
-
   /* paymentsScanCoordinator is created and started by a BrowserCommand */
 
   /* paymentsSuggestionBottomSheetCoordinator is created and started by a
@@ -1430,10 +1414,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self.netExportCoordinator stop];
   self.netExportCoordinator = nil;
-
-  [self.passwordSettingsCoordinator stop];
-  self.passwordSettingsCoordinator.delegate = nil;
-  self.passwordSettingsCoordinator = nil;
 
   [_credentialProviderPromoCoordinator stop];
   _credentialProviderPromoCoordinator = nil;
@@ -2085,10 +2065,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self hideReaderModeBlurOverlay];
 
-  [self.passwordSettingsCoordinator stop];
-  self.passwordSettingsCoordinator.delegate = nil;
-  self.passwordSettingsCoordinator = nil;
-
   [self stopRepostFormCoordinator];
 
   [_formInputAccessoryCoordinator clearPresentedState];
@@ -2442,21 +2418,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [HandlerForProtocol(self.dispatcher, SettingsCommands)
           showSavedPasswordsSettingsFromViewController:self.viewController];
       break;
-    case AutofillSettingsPage::kPasswordSettings: {
-      // Not an invariant due to possible race conditions. DCHECKing for
-      // debugging purposes. See crbug.com/40067451.
-      DCHECK(!self.passwordSettingsCoordinator);
-
-      // Use main browser to open the password settings.
-      SceneState* sceneState = self.sceneState;
-      self.passwordSettingsCoordinator = [[PasswordSettingsCoordinator alloc]
-          initWithBaseViewController:self.viewController
-                             browser:sceneState.browserProviderInterface
-                                         .mainBrowserProvider.browser];
-      self.passwordSettingsCoordinator.delegate = self;
-      [self.passwordSettingsCoordinator start];
+    case AutofillSettingsPage::kPasswordSettings:
+      [HandlerForProtocol(self.dispatcher, SettingsCommands)
+          showPasswordSettingsFromViewController:self.viewController];
       break;
-    }
     case AutofillSettingsPage::kAddresses:
       [HandlerForProtocol(self.dispatcher, SettingsCommands)
           showProfileSettingsFromViewController:self.viewController];
@@ -3144,21 +3109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)reloadNTPForWebState:(web::WebState*)webState {
   [_NTPCoordinator reload];
-}
-
-#pragma mark - PasswordSettingsCoordinatorDelegate
-
-- (void)passwordSettingsCoordinatorDidRemove:
-    (PasswordSettingsCoordinator*)coordinator {
-  DCHECK_EQ(self.passwordSettingsCoordinator, coordinator);
-
-  [self stopPasswordSettingsCoordinator];
-}
-
-#pragma mark - PasswordManagerReauthenticationDelegate
-
-- (void)dismissPasswordManagerAfterFailedReauthentication {
-  [self stopPasswordSettingsCoordinator];
 }
 
 #pragma mark - ReadingListCoordinatorDelegate
