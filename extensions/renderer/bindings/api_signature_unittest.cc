99617b247f635ef13bfe75fdb0f9d1489c578436
Tim <tjudkins@chromium.org>
2026-09-03 16:15:58 -0700

    [Extensions] Refactor trailing callback handling in APISignature
    Previously, APISignature synthesized a dummy "callback" ArgumentSpec
    onto the end of its positional signature list for APIs with asynchronous
    returns. This required popping and re-adding the callback spec during
    schema creation and argument parsing.
    
    This CL refactors APISignature and ArgumentParser to handle asynchronous
    returns separately from positional input arguments by plumbing the const
    APISignature& directly through the ArgumentParser subclasses and
    resolving any trailing callback argument (function, null, undefined)
    against the async return specification, before matching the remaining
    arguments.
    
    It also adds unit tests to explicitly document how we handle null and
    undefined values in place of the trailing callback.
    
    Note: Technically there is a slight behavior change on how the new logic would handle an API defined with an asynchronous return and a final trailing Function argument (distinct from the async response callback). Previously this would have been greedily taken from the front, taking the Function argument for the first Function, but now it would extract it for the async response callback at the end. However, no extension APIs currently have this form, so in reality no existing behavior change is expected.
    
    Bug: 552951908
    Change-Id: Iedb01f54adbd86043fb830e4240baf886bcc83a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258952
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692011}

--- extensions/renderer/bindings/api_signature_unittest.cc
+++ extensions/renderer/bindings/api_signature_unittest.cc
@@ -374,6 +374,8 @@ TEST_F(APISignatureTest, BasicSignatureParsing) {
                binding::AsyncResponseType::kCallback);
     ExpectFailure(*signature, "[function() {}]", NoMatchingSignature());
     ExpectFailure(*signature, "[1]", NoMatchingSignature());
+    ExpectFailure(*signature, "[1, null]", NoMatchingSignature());
+    ExpectFailure(*signature, "[1, undefined]", NoMatchingSignature());
   }
 
   {
@@ -383,7 +385,15 @@ TEST_F(APISignatureTest, BasicSignatureParsing) {
                binding::AsyncResponseType::kCallback);
     ExpectPass(*signature, "[function() {}]", "[null]",
                binding::AsyncResponseType::kCallback);
+    ExpectPass(*signature, "[null, function() {}]", "[null]",
+               binding::AsyncResponseType::kCallback);
+    ExpectPass(*signature, "[undefined, function() {}]", "[null]",
+               binding::AsyncResponseType::kCallback);
     ExpectFailure(*signature, "[1]", NoMatchingSignature());
+    ExpectFailure(*signature, "[1, null]", NoMatchingSignature());
+    ExpectFailure(*signature, "[1, undefined]", NoMatchingSignature());
+    ExpectFailure(*signature, "[null]", NoMatchingSignature());
+    ExpectFailure(*signature, "[undefined]", NoMatchingSignature());
   }
 
   {
@@ -392,6 +402,7 @@ TEST_F(APISignatureTest, BasicSignatureParsing) {
     ExpectPass(*signature, "[function() {}]", "[]",
                binding::AsyncResponseType::kCallback);
     ExpectPass(*signature, "[]", "[]", binding::AsyncResponseType::kNone);
+    ExpectPass(*signature, "[null]", "[]", binding::AsyncResponseType::kNone);
     ExpectPass(*signature, "[undefined]", "[]",
                binding::AsyncResponseType::kNone);
     ExpectFailure(*signature, "[0]", NoMatchingSignature());
@@ -757,6 +768,10 @@ TEST_F(APISignatureTest, PromisesSupport) {
         SpecVector(), ReturnsAsyncBuilder().Build());
     ExpectPass(*default_signature, "[]", "[]",
                binding::AsyncResponseType::kPromise);
+    ExpectPass(*default_signature, "[null]", "[]",
+               binding::AsyncResponseType::kPromise);
+    ExpectPass(*default_signature, "[undefined]", "[]",
+               binding::AsyncResponseType::kPromise);
 
     // Ensure that the promise support allowing the final argument to be
     // optional doesn't mean we can ignore it entirely if it doesn't match the
@@ -771,6 +786,10 @@ TEST_F(APISignatureTest, PromisesSupport) {
         SpecVector(),
         ReturnsAsyncBuilder().DoesNotSupportPromises().MakeRequired().Build());
     ExpectFailure(*required_callback_signature, "[]", NoMatchingSignature());
+    ExpectFailure(*required_callback_signature, "[null]",
+                  NoMatchingSignature());
+    ExpectFailure(*required_callback_signature, "[undefined]",
+                  NoMatchingSignature());
   }
 
   {
@@ -779,6 +798,10 @@ TEST_F(APISignatureTest, PromisesSupport) {
     auto optional_callback_signature = std::make_unique<APISignature>(
         SpecVector(), ReturnsAsyncBuilder().DoesNotSupportPromises().Build());
     ExpectPass(*optional_callback_signature, "[]", "[]",
+               binding::AsyncResponseType::kNone);
+    ExpectPass(*optional_callback_signature, "[null]", "[]",
+               binding::AsyncResponseType::kNone);
+    ExpectPass(*optional_callback_signature, "[undefined]", "[]",
                binding::AsyncResponseType::kNone);
   }
 
