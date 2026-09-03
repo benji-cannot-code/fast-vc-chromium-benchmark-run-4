ea9a7fd296a26d719ecf0ddbfbc7ce2b313c3d57
Amy Hwang <amyhwang@chromium.org>
2026-09-03 09:27:48 -0700

    [Page Actions] Reset callbacks to base::DoNothing in OnNewActiveController
    When the active PageActionController is set to null in
    PageActionView::OnNewActiveController (during tab switch or close),
    callbacks were previously reset using base::NullCallback() and 3 of the
    6 delegate callbacks were omitted.
    
    If a slide animation ends/resets around the time of detachment,
    NotifyIsChipShowingChange() posts is_chip_showing_changed_callback_ to
    the task runner. Because this callback was reset to NullCallback(), the
    posted task crashes.
    
    This CL resets all six delegate callbacks to base::DoNothing() to match
    their default initialized state in PageActionView and safely release any
    bound WeakPtrs without risk of null callback.
    
    A regression test is added to verify the behaviour.
    
    Bug: 555011257
    Change-Id: I81eddb092ee052e22b8068a3bf0fb868100d4234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8344247
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Commit-Queue: Amy Hwang <amyhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691672}

--- chrome/browser/ui/views/page_action/page_action_view.cc
+++ chrome/browser/ui/views/page_action/page_action_view.cc
@@ -177,9 +177,12 @@ void PageActionView::OnNewActiveController(PageActionController* controller) {
         controller->CreateActionItemSubscription(action_item_.get());
     OnPageActionModelChanged(*observation_.GetSource());
   } else {
-    SetIsChipShowingChangedCallback(base::NullCallback());
-    SetAnchoredMessageCloseCallback(base::NullCallback());
-    SetClickCallback(base::NullCallback());
+    SetIsChipShowingChangedCallback(base::DoNothing());
+    SetImageAnimationStartedCallback(base::DoNothing());
+    SetAnchoredMessageCloseCallback(base::DoNothing());
+    SetClickCallback(base::DoNothing());
+    SetAnchoredMessageExpandCallback(base::DoNothing());
+    SetAnchoredMessageCollapseCallback(base::DoNothing());
     SetVisible(false);
   }
 }
