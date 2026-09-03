ae8238956abab09ce50e08de865435d102338d2f
Sahir Vellani <sahir.vellani@microsoft.com>
2026-09-03 15:48:10 -0700

    Rename D3D11DecoderConfigurator to D3DDecoderConfigurator
    Rename D3D11DecoderConfigurator and its source files to
    D3DDecoderConfigurator. Also rename functions that perform operations
    specific to D3D11 within D3DDecoderConfigurator. This change prepares
    the class to be used in a D3D12 context along with D3D11. It also allows
    for the addition of D3D12 code in the future, if necessary.
    
    Bypass-Check-License: Renamed files.
    Bug: 542565485
    Change-Id: Id316e4eb5f926e73684a2a5c8d77bda0eec084e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8340147
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1691991}

--- media/gpu/windows/d3d11_video_decoder_wrapper.h
+++ media/gpu/windows/d3d11_video_decoder_wrapper.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "media/base/video_decoder_config.h"
-#include "media/gpu/windows/d3d11_decoder_configurator.h"
 #include "media/gpu/windows/d3d_com_defs.h"
+#include "media/gpu/windows/d3d_decoder_configurator.h"
 #include "media/gpu/windows/d3d_video_decoder_wrapper.h"
 
 namespace media {
@@ -22,7 +22,7 @@ class D3D11VideoDecoderWrapper : public D3DVideoDecoderWrapper {
       MediaLog* media_log,
       ComD3D11VideoDevice1 video_device,
       ComD3D11VideoContext1 video_context,
-      const D3D11DecoderConfigurator* decoder_configurator,
+      const D3DDecoderConfigurator* decoder_configurator,
       VideoDecoderConfig config);
   ~D3D11VideoDecoderWrapper() override;
 
