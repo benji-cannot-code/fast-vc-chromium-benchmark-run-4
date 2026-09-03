676e30df318af75d113fc3349790749c6ab1be7b
Zekun Jiang <zekunjiang@google.com>
2026-09-03 10:49:49 -0700

    Add implementation for RemoteModelExecutionSession.
    Bug: 553134125
    Change-Id: I2acf5df570456ff6cde9889d0e9340ce66313094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8309380
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691755}

--- components/streaming_client/streaming_websocket_client.cc
+++ components/streaming_client/streaming_websocket_client.cc
@@ -35,6 +35,8 @@ StreamingWebSocketClient::Delegate::GetAdditionalHeaders() {
   return {};
 }
 
+void StreamingWebSocketClient::Delegate::OnConnected() {}
+
 StreamingWebSocketClient::StreamingWebSocketClient(
     const GURL& service_url,
     network::mojom::NetworkContext* network_context,
@@ -44,10 +46,7 @@ StreamingWebSocketClient::StreamingWebSocketClient(
       network_context_(network_context),
       traffic_annotation_(traffic_annotation),
       delegate_(delegate),
-      readable_watcher_(FROM_HERE, mojo::SimpleWatcher::ArmingPolicy::MANUAL) {
-  CHECK(network_context_);
-  CHECK(delegate_);
-}
+      readable_watcher_(FROM_HERE, mojo::SimpleWatcher::ArmingPolicy::MANUAL) {}
 
 StreamingWebSocketClient::~StreamingWebSocketClient() = default;
 
@@ -78,6 +77,14 @@ void StreamingWebSocketClient::Close() {
 }
 
 void StreamingWebSocketClient::Connect() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  CHECK(network_context_);
+  CHECK(delegate_);
+
+  if (state_ == State::kConnecting || state_ == State::kOpen) {
+    return;
+  }
+
   // A disconnect handler is used so that the request can be completed in the
   // event of an unexpected disconnection from the network service.
   auto handshake_remote = handshake_receiver_.BindNewPipeAndPassRemote();
@@ -185,6 +192,10 @@ void StreamingWebSocketClient::OnConnectionEstablished(
     }
     pending_write_data_.pop();
   }
+
+  if (state_ == State::kOpen) {
+    delegate_->OnConnected();
+  }
 }
 
 void StreamingWebSocketClient::OnDataFrame(
@@ -279,6 +290,10 @@ void StreamingWebSocketClient::ClosePipe() {
     return;
   }
   state_ = State::kDisconnected;
+  websocket_.reset();
+  readable_watcher_.Cancel();
+  readable_.reset();
+  writable_.reset();
   client_receiver_.reset();
   handshake_receiver_.reset();
   pending_write_data_ = {};
