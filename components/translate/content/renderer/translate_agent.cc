474eeccd1d85fa6e2b311a7d42e1a40f7ff853e7
Andrea Gomez <andreaxg@google.com>
2026-09-03 10:47:54 -0700

    Only ignore PDF documents if kEnableTranslatePdf is enabled
    https://crrev.com/c/8021612 modified TranslateAgent::PageCaptured to
    return early for application/pdf, assuming that PdfPageCaptured would be
    called instead to detect language and register the page.
    
    PdfPageCaptured is only triggered in
    ChromePDFDocumentHelperClient::OnDocumentLoadComplete when the feature
    flag translate::kEnableTranslatePdf is enabled, which is disabled by
    default. In default/stable builds, this caused PDFs to have no language
    detection, resulting in an empty LanguageState::source_language_ and
    preventing the right-click context menu "Translate selection to
    [language]" option from appearing.
    
    This change ensures that TranslateAgent::PageCaptured only returns early
    for PDFs when kEnableTranslatePdf is enabled.
    
    TAG=agy
    
    Bug: 555282656
    Change-Id: I0d4efb73da07c90d3c77f94043f97f079ad1ad4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8344374
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Katie Lu <ktlu@google.com>
    Reviewed-by: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691753}

--- components/translate/content/renderer/translate_agent.cc
+++ components/translate/content/renderer/translate_agent.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/json/string_escape.h"
 #include "base/location.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/language_detection/core/constants.h"
 #include "components/language_detection/core/language_detection_provider.h"
 #include "components/translate/content/renderer/isolated_world_util.h"
+#include "components/translate/core/common/translate_features.h"
 #include "components/translate/core/common/translate_metrics.h"
 #include "components/translate/core/common/translate_util.h"
 #include "components/translate/core/language_detection/language_detection_model.h"
@@ -157,10 +159,11 @@ void TranslateAgent::PageCaptured(
     return;
 
   blink::WebDocumentLoader* doc_loader = main_frame->GetDocumentLoader();
-  if (doc_loader &&
+  if (base::FeatureList::IsEnabled(translate::kEnableTranslatePdf) &&
+      doc_loader &&
       doc_loader->GetWebResponse().MimeType() == "application/pdf") {
-    // If the page is a PDF, we should only register it when
-    // PdfPageCaptured is called.
+    // If the page is a PDF and PDF translation is enabled, we should only
+    // register it when PdfPageCaptured is called.
     return;
   }
 
