65d656103143383554a9224427e4ade11463b034
utzcoz <utzcoz@gmail.com>
2026-09-02 23:56:59 -0700

    [Android] Re-enable TextFieldViewUnitTest accessibility tests
    Both required-field label tests pass deterministically on the current
    Robolectric version: verified 10x with the full affected-class mix and
    3x in isolation, on both SDK variants.
    
    Bug: 533068290
    Change-Id: Ib06d2857ae8a9785a9ad94436decbd2e4b2d9731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346889
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691362}

--- chrome/browser/autofill/android/junit/src/org/chromium/chrome/browser/autofill/editors/common/text_field/TextFieldViewUnitTest.java
+++ chrome/browser/autofill/android/junit/src/org/chromium/chrome/browser/autofill/editors/common/text_field/TextFieldViewUnitTest.java
@@ -45,7 +45,6 @@ import org.robolectric.Robolectric;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.autofill.R;
 import org.chromium.chrome.browser.autofill.editors.common.field.EditorFieldValidator;
 import org.chromium.chrome.browser.autofill.editors.common.field.FieldView;
@@ -382,7 +381,6 @@ public final class TextFieldViewUnitTest {
      * required.
      */
     @Test
-    @DisabledTest(message = "crbug.com/533068290")
     public void testRequiredFieldHasCorrectLabelAndAccessibilityScreenReaderOff() {
         PropertyModel model = buildDefaultPropertyModel();
         model.set(IS_REQUIRED, true);
@@ -401,7 +399,6 @@ public final class TextFieldViewUnitTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/533068290")
     public void testRequiredFieldHasCorrectLabelAndAccessibilityScreenReaderOn() {
         PropertyModel model = buildDefaultPropertyModel();
         model.set(IS_REQUIRED, true);
