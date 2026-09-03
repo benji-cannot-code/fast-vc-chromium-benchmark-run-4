235be714f8af01edf893c4bce842f81511a61393
Tomasz Wiszkowski <ender@google.com>
2026-09-03 15:00:56 -0700

    [Clean] Enable Strictness.STRICT_STUBS in LocationBar tests
    Enable MockitoRule with Strictness.STRICT_STUBS across LocationBar,
    UrlBar, and AutocompleteEditText unit tests to enforce strict stubs
    and eliminate dead stubbings. Per guidelines, lenient() is kept
    exclusively in setUp().
    
    Bug: None
    Change-Id: If79cc625e9ba3f4a43e50b5c56f377b5fc9cd42b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349029
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Peter Kang <peterkang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691966}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBarUnitTest.java
@@ -17,7 +17,6 @@ import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyFloat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
@@ -64,6 +63,7 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
 import org.robolectric.Robolectric;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
@@ -133,7 +133,9 @@ public class UrlBarUnitTest {
             "www.a.com/"
                     + TextUtils.join("", Collections.nCopies(MAX_DISPLAYABLE_LENGTH + 100, "a"));
 
-    @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
+    @Rule
+    public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+
     @Rule public final TestName mTestName = new TestName();
 
     @Mock private UrlBarDelegate mUrlBarDelegate;
@@ -186,10 +188,12 @@ public class UrlBarUnitTest {
         mLastTextDirection = -1;
         mLastTextAlignment = -1;
 
-        doAnswer(i -> mLastTextDirection = i.getArgument(0))
+        lenient()
+                .doAnswer(i -> mLastTextDirection = i.getArgument(0))
                 .when(mUrlBar)
                 .setTextDirection(anyInt());
-        doAnswer(i -> mLastTextAlignment = i.getArgument(0))
+        lenient()
+                .doAnswer(i -> mLastTextAlignment = i.getArgument(0))
                 .when(mUrlBar)
                 .setTextAlignment(anyInt());
 
@@ -1020,7 +1024,6 @@ public class UrlBarUnitTest {
     @EnableFeatures(ChromeFeatureList.ANDROID_NO_VISIBLE_HINT_FOR_DIFFERENT_TLD)
     public void scrollToTLD_differentTLD_noVisibleHintCalculation() {
         doReturn(mLayout).when(mUrlBar).getLayout();
-        doReturn(mPaint).when(mLayout).getPaint();
 
         measureAndLayoutUrlBar();
         // Url needs to be long enough to fill the entire url bar.
