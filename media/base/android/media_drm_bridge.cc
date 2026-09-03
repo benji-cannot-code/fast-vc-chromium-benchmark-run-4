0bf272fa618776b75a84618d36c1ee45c6f7d740
Vikram Pasupathy <vpasupathy@chromium.org>
2026-09-03 13:29:24 -0700

    media: GetCurrentHdcpLevel should not check security level
    Bug: 40406119
    Change-Id: Ia9a4b30da8c066b0f6d6383143e7b01d403fb5f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349304
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Vikram Pasupathy <vpasupathy@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691888}

--- media/base/android/media_drm_bridge.cc
+++ media/base/android/media_drm_bridge.cc
@@ -1065,11 +1065,6 @@ std::string MediaDrmBridge::GetVersionInternal() {
 }
 
 HdcpVersion MediaDrmBridge::GetCurrentHdcpLevel() {
-  if (security_level_ == SECURITY_LEVEL_SW_SECURE_CRYPTO ||
-      security_level_ == SECURITY_LEVEL_SW_SECURE_DECODE) {
-    return HdcpVersion::kHdcpVersionNone;
-  }
-
   JNIEnv* env = AttachCurrentThread();
   ScopedJavaLocalRef<jstring> j_current_hdcp_level =
       Java_MediaDrmBridge_getCurrentHdcpLevel(env, j_media_drm_);
