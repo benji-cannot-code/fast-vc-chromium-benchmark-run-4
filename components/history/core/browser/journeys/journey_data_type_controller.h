01d6445deb7c1e518e5c73c0e07a0593264903c2
Judith Hemp <hempjudith@google.com>
2026-09-03 08:48:56 -0700

    [Journeys] Introduce JourneyDataTypeController and wire into builder
    Introduce JourneyDataTypeController for the syncer::JOURNEY data type
    and wire it into CommonControllerBuilder.
    
    Oriented on HistoryDataTypecontroller:
    https://source.chromium.org/chromium/chromium/src/+/main:components/history/core/browser/sync/history_data_type_controller.cc
    
    However, History is among the kLegacyTypes, while this is a New Sync
    data type which "must generally support kTransportOnly mode".
    https://source.chromium.org/chromium/chromium/src/+/main:components/sync/service/data_type_controller.cc;drc=37a2d3f3e369a8777ddba89f67838845b6afba70;l=84
    
    We implement stuff differently for this reason but JOURNEYS is still
    aligned to history. The gating is handled upstream by SyncService, since
    syncer::JOURNEY is part of UserSelectableType::kHistory:
    https://source.chromium.org/chromium/chromium/src/+/main:components/sync/base/user_selectable_type.cc;drc=3e3a0595a4dbd5042ee0b4356bcce0ab037623e6;l=93
    
    Following:
    * https://www.chromium.org/developers/design-documents/sync/integration-checklist/
    
    Bug: 526686844, 462706323
    Change-Id: I75cbca2f58dc61d4898b619d9f0fdba3baf45277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8295798
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691636}

---
+++ components/history/core/browser/journeys/journey_data_type_controller.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_HISTORY_CORE_BROWSER_JOURNEYS_JOURNEY_DATA_TYPE_CONTROLLER_H_
+#define COMPONENTS_HISTORY_CORE_BROWSER_JOURNEYS_JOURNEY_DATA_TYPE_CONTROLLER_H_
+
+#include "components/history/core/browser/sync/history_data_type_controller_helper.h"
+#include "components/sync/service/data_type_controller.h"
+
+class PrefService;
+
+namespace syncer {
+class SyncService;
+}  // namespace syncer
+
+namespace history {
+
+class HistoryService;
+
+// DataTypeController for syncer::JOURNEY.
+class JourneyDataTypeController : public syncer::DataTypeController {
+ public:
+  JourneyDataTypeController(syncer::SyncService* sync_service,
+                            HistoryService* history_service,
+                            PrefService* pref_service);
+
+  JourneyDataTypeController(const JourneyDataTypeController&) = delete;
+  JourneyDataTypeController& operator=(const JourneyDataTypeController&) =
+      delete;
+
+  ~JourneyDataTypeController() override;
+
+  // syncer::DataTypeController implementation.
+  PreconditionState GetPreconditionState(
+      const PreconditionContext& context) const override;
+
+ private:
+  HistoryDataTypeControllerHelper helper_;
+};
+
+}  // namespace history
+
+#endif  // COMPONENTS_HISTORY_CORE_BROWSER_JOURNEYS_JOURNEY_DATA_TYPE_CONTROLLER_H_
