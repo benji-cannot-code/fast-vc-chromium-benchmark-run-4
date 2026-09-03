117b3c381455c849de68a82b6e76f31c53753cce
Jilin Yang <jilinyang@google.com>
2026-09-03 16:04:24 -0700

    [NTP]: Handle BrowserWindowInterface changes in ActionChipsHandler
    1. Before:
    https://screencast.googleplex.com/cast/NTk0NjgyNjgwODA5ODgxNnw2MTI1YjRmNC1iYg
    2. After:
    https://screencast.googleplex.com/cast/NTAyMDM3NjYxNzcxMzY2NHwzZDE0MWEzNC1mNw
    
    When an NTP tab moves to a new window, ActionChipsHandler retains stale
    chips because it does not update its TabStripModel observation. This CL
    subscribes to BrowserWindowInterface changes to rebind the
    TabStripModelObserver to the current window. It also refreshes the
    action chips upon window migration to reflect the new window context.
    
    Fixed: b:552185137
    Change-Id: If3c9406364fef590633d4932e7e299d8ca5dd3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353048
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Jilin Yang <jilinyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692001}

--- chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.cc
+++ chrome/browser/ui/webui/new_tab_page/action_chips/action_chips_handler.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
+#include "chrome/browser/ui/webui/webui_embedding_context.h"
 #endif
 
 namespace {
@@ -137,14 +138,13 @@ ActionChipsHandler::ActionChipsHandler(
       action_chips_generator_(std::move(action_chips_generator)),
       get_session_handle_callback_(std::move(get_session_handle_callback)) {
 #if !BUILDFLAG(IS_ANDROID)
-  content::WebContents* web_contents = web_ui_->GetWebContents();
-  auto* browser_window_interface =
-      webui::GetBrowserWindowInterface(web_contents);
-  if (browser_window_interface) {
-    // No need to call RemoveObserver later since TabStripModelObserver takes
-    // care of it in its destructor.
-    browser_window_interface->GetTabStripModel()->AddObserver(this);
-  }
+  browser_window_interface_subscription_ =
+      webui::RegisterBrowserWindowInterfaceChanged(
+          web_ui_->GetWebContents(),
+          base::BindRepeating(
+              &ActionChipsHandler::OnBrowserWindowInterfaceChanged,
+              weak_factory_.GetWeakPtr()));
+  UpdateTabStripModelObservation();
 #endif  // !BUILDFLAG(IS_ANDROID)
   pref_change_registrar_.Init(profile_->GetPrefs());
   pref_change_registrar_.Add(
@@ -230,6 +230,25 @@ void ActionChipsHandler::OnTabStripModelChanged(
   if (!IsTabReadyForActionChipsRetrieval(web_ui_->GetWebContents(), change)) {
     return;
   }
+  StartActionChipsRetrieval();
+}
+
+bool ActionChipsHandler::UpdateTabStripModelObservation() {
+  TabStripModelObserver::StopObservingAll(this);
+  content::WebContents* web_contents = web_ui_->GetWebContents();
+  auto* browser_window_interface =
+      webui::GetBrowserWindowInterface(web_contents);
+  if (browser_window_interface) {
+    browser_window_interface->GetTabStripModel()->AddObserver(this);
+  }
+  return browser_window_interface != nullptr;
+}
+
+void ActionChipsHandler::OnBrowserWindowInterfaceChanged() {
+  if (!UpdateTabStripModelObservation()) {
+    return;
+  }
+  last_processed_url_.reset();
   StartActionChipsRetrieval();
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
