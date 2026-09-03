7b86cf2bf748e05110229b5715c25161b26b4847
Arthur Milchior <arthurmilchior@google.com>
2026-09-03 07:22:36 -0700

    [iOS][RecentTabs]Move distant tab opening logic to the Coordinator
    Delegate opening distant tabs to RecentTabsCoordinator via
    RecentTabsPresentationDelegate. This will allow to remove the browser
    from the VC in a next CL.
    
    Done by antigravity.
    
    Bug: 546330038
    Change-Id: Iff93e996df4ba8b7990441ca009ea4983ab14970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8286906
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691561}

--- ios/chrome/browser/recent_tabs/ui/recent_tabs_presentation_delegate.h
+++ ios/chrome/browser/recent_tabs/ui/recent_tabs_presentation_delegate.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace synced_sessions {
 struct DistantSession;
+struct DistantTab;
 }
 
 // Presentation commands that depend on the context from which they are
@@ -34,6 +35,8 @@ struct DistantSession;
 - (void)openAllTabsFromSession:(const synced_sessions::DistantSession*)session;
 // Tells the receiver to restore the tab with the given `sessionId`.
 - (void)openTabWithTabRestoreEntryId:(SessionID)sessionId;
+// Tells the receiver to open the given `distantTab`.
+- (void)openDistantTab:(const synced_sessions::DistantTab*)distantTab;
 // Asks the presenter to display the reauthenticate the primary account.
 // The primary should be available.
 - (void)showPrimaryAccountReauth;
