6f2b5d695facf2be09bc6c2fdb8262696f8164a3
Evan Stade <evanstade@microsoft.com>
2026-09-03 14:19:30 -0700

    IDB: SQLite store - add migration coverage to fuzzer
    The fuzzer gets some new actions to cause a migration on backing store
    shutdown. The verification code is also run to compare the source and
    target stores.
    
    This induces code coverage for about 60% of lines in
    backing_store_util.cc (based on migration.textproto, without actual
    fuzzing), and most of the missing ones are in the snapshotting code
    rather than migration. Follow up work will extend this coverage.
    
    Bug: 40253999
    Change-Id: I7d6ec5fc03348c77d226a0f2bb39449f94395a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8333347
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1691933}

--- content/browser/indexed_db/idb_factory_mojolpm_fuzzer.cc
+++ content/browser/indexed_db/idb_factory_mojolpm_fuzzer.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/no_destructor.h"
 #include "base/task/bind_post_task.h"
+#include "base/time/time.h"
 #include "components/services/storage/privileged/cpp/bucket_client_info.h"
 #include "components/services/storage/privileged/mojom/indexed_db_client_state_checker.mojom.h"
 #include "components/services/storage/privileged/mojom/indexed_db_control.mojom.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "storage/browser/quota/quota_manager.h"
 #include "storage/browser/quota/quota_manager_proxy.h"
+#include "storage/browser/quota/quota_settings.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom-mojolpm.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
@@ -59,6 +61,8 @@ class IdbFuzzerEnvironment : public content::mojolpm::FuzzerEnvironment {
             base::test::TaskEnvironment::ThreadPoolExecutionMode::ASYNC,
             base::test::TaskEnvironment::ThreadingMode::MULTIPLE_THREADS,
             content::BrowserTaskEnvironment::REAL_IO_THREAD) {}
+
+  void AdvanceTime() { task_environment_.FastForwardBy(base::Seconds(5)); }
 
  private:
   content::BrowserTaskEnvironment task_environment_;
@@ -129,6 +133,8 @@ class IdbFactoryTestcase
 
   // These are called from the UI thread.
   void FlushBucketSequence(base::OnceClosure done_closure);
+  void EnableSqliteMigration(base::OnceClosure done_closure);
+  void AdvanceTime(base::OnceClosure done_closure);
   void BindIndexedDB(
       storage::BucketClientInfo client_info,
       mojo::PendingRemote<storage::mojom::IndexedDBClientStateChecker>
@@ -180,6 +186,10 @@ void IdbFactoryTestcase::SetUp(base::OnceClosure done_closure) {
 }
 
 void IdbFactoryTestcase::SetUpOnUIThread(base::OnceClosure done_closure) {
+  static storage::QuotaSettings quota_settings =
+      storage::GetHardCodedSettings(100 * 1024 * 1024);
+  content::StoragePartition::SetDefaultQuotaSettingsForTesting(&quota_settings);
+
   browser_context_ = std::make_unique<content::TestBrowserContext>();
   browser_context_->set_is_off_the_record(in_memory_);
   browser_context_->GetDefaultStoragePartition()
@@ -294,6 +304,24 @@ void IdbFactoryTestcase::RunAction(const ProtoAction& action,
                              std::move(done_closure));
       return;
 
+    case ProtoAction::kEnableSqliteMigrationAction:
+      content::GetUIThreadTaskRunner({})->PostTask(
+          FROM_HERE,
+          base::BindOnce(&IdbFactoryTestcase::EnableSqliteMigration,
+                         base::Unretained(this),
+                         base::BindPostTask(GetFuzzerTaskRunner(),
+                                            std::move(done_closure))));
+      return;
+
+    case ProtoAction::kAdvanceTime:
+      content::GetUIThreadTaskRunner({})->PostTask(
+          FROM_HERE,
+          base::BindOnce(&IdbFactoryTestcase::AdvanceTime,
+                         base::Unretained(this),
+                         base::BindPostTask(GetFuzzerTaskRunner(),
+                                            std::move(done_closure))));
+      return;
+
     case ProtoAction::kNewBlob:
       CreateAndAddBlob(action.new_blob().id(), action.new_blob().content(),
                        std::move(done_closure));
@@ -332,6 +360,16 @@ void IdbFactoryTestcase::RunAction(const ProtoAction& action,
 void IdbFactoryTestcase::FlushBucketSequence(base::OnceClosure done_closure) {
   indexed_db_control_test_->FlushBucketSequenceForTesting(
       bucket_locator_, std::move(done_closure));
+}
+
+void IdbFactoryTestcase::EnableSqliteMigration(base::OnceClosure done_closure) {
+  indexed_db_control_test_->PerformAndVerifySqliteMigrationForTesting(
+      bucket_locator_, std::move(done_closure));
+}
+
+void IdbFactoryTestcase::AdvanceTime(base::OnceClosure done_closure) {
+  GetEnvironment().AdvanceTime();
+  std::move(done_closure).Run();
 }
 
 void IdbFactoryTestcase::BindIndexedDB(
