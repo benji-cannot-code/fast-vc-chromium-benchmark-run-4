3f9d68f1d247e2ff5df99ee9ecc4505e08d9445a
Ivan Kabadzhov <ikabadzhov@google.com>
2026-09-03 13:35:25 -0700

    Update CWS review URL and attribution for easier adoption
    Updates GetCWSWritingReviewUrl() to facilitate easier adoption and
    metrics alignment on the Chrome Web Store side. The URL now navigates
    directly to the dedicated review route (/reviews/my-review) instead of
    /reviews, and omits the ?action=write query parameter. Attribution is
    also switched to the standard utm_source parameter with aligned values
    (chrome_extensions_menu, chrome_extensions_page, and
    chrome_context_menu), with corresponding updates to unit tests.
    
    Bug: b:535481653
    Change-Id: Ie355457a1fe87ca211a98f8317b42f9156605789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334492
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Ivan Kabadzhov <ikabadzhov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691891}

--- chrome/browser/extensions/extension_util.cc
+++ chrome/browser/extensions/extension_util.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest_handlers/incognito_info.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
-#include "net/base/url_util.h"
 #include "url/gurl.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -427,20 +426,19 @@ GURL GetCWSWritingReviewUrl(const ExtensionId& extension_id,
   const char* source_str = nullptr;
   switch (source) {
     case CWSReviewSource::kExtensionsMenu:
-      source_str = "extensions_menu";
+      source_str = extension_urls::kReviewExtensionsMenuUtmSource;
       break;
     case CWSReviewSource::kExtensionsPage:
-      source_str = "extensions_page";
+      source_str = extension_urls::kReviewExtensionsPageUtmSource;
       break;
     case CWSReviewSource::kContextMenu:
-      source_str = "context_menu";
+      source_str = extension_urls::kReviewContextMenuUtmSource;
       break;
   }
 
   GURL review_url = extension_urls::GetNewWebstoreLaunchURL().Resolve(
-      base::StrCat({"detail/", extension_id, "/reviews"}));
-  review_url = net::AppendQueryParameter(review_url, "action", "write");
-  return net::AppendQueryParameter(review_url, "source", source_str);
+      base::StrCat({"detail/", extension_id, "/reviews/my-review"}));
+  return extension_urls::AppendUtmSource(review_url, source_str);
 }
 
 }  // namespace extensions::util
