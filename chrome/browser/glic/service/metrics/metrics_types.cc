9bd7c970be4f6051b0188b14bad22475f66c3a68
Chirag Arora <heychirag@google.com>
2026-09-03 02:07:59 -0700

    [Glic] Add HistoryPageChatLinkout invocation source and pin_on_bind option
    This CL adds support for invoking Glic from the History page chat
    linkout and adds an option to skip tab pinning on bind.
    
    Specifically:
    - Adds kHistoryPageChatLinkout to mojom::InvocationSource and updates
      the generated TypeScript API bindings.
    - Adds pin_on_bind option to GlicInvokeOptions and ShowOptions to allow
      invoking Glic without forcing tab pinning.
    - Adds GlicResponseSegmentation enum entries and histograms metrics
      for the new invocation source.
    - Adds browser test coverage for invoke with pin_on_bind set to false.
    
    Bug: 555611662
    Change-Id: I2a99d678f1fc884ff07fe7c61785b06278100d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8321587
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691413}

--- chrome/browser/glic/service/metrics/metrics_types.cc
+++ chrome/browser/glic/service/metrics/metrics_types.cc
@@ -147,6 +147,8 @@ std::string GetInvocationSourceString(mojom::InvocationSource source) {
       return "TabContextMenu";
     case mojom::InvocationSource::kWebContinuity:
       return "WebContinuity";
+    case mojom::InvocationSource::kHistoryPageChatLinkout:
+      return "HistoryPageChatLinkout";
   }
 }
 
--- chrome/browser/glic/service/metrics/metrics_types.h
+++ chrome/browser/glic/service/metrics/metrics_types.h
@@ -196,7 +196,11 @@ enum class ResponseSegmentation {
   kWebContinuityAttachedAudio = 170,
   kWebContinuityDetachedText = 171,
   kWebContinuityDetachedAudio = 172,
-  kMaxValue = kWebContinuityDetachedAudio,
+  kHistoryPageChatLinkoutAttachedText = 173,
+  kHistoryPageChatLinkoutAttachedAudio = 174,
+  kHistoryPageChatLinkoutDetachedText = 175,
+  kHistoryPageChatLinkoutDetachedAudio = 176,
+  kMaxValue = kHistoryPageChatLinkoutDetachedAudio,
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/glic/enums.xml:GlicResponseSegmentation)
 
