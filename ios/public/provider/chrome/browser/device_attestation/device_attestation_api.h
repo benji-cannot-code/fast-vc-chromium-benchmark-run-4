482ee49df44ab8ce75477d48d50b1b3473c32e36
Michał Kaczmarczyk <mickaczmarczyk@google.com>
2026-09-03 02:36:11 -0700

    Add device attestation provider API for iOS.
    This CL introduces the device attestation provider API, which allows the
    creation of an enterprise::AttestationServiceIOS instance. It registers
    this provider during startup and adds stub implementations for Chromium
    and test providers.
    
    Bug: 421927804
    Change-Id: I4a13a1da45012b91675e7d923c6b6decb086de2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8336387
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Vasili Svirski <vsvirski@google.com>
    Commit-Queue: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691431}

---
+++ ios/public/provider/chrome/browser/device_attestation/device_attestation_api.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_PUBLIC_PROVIDER_CHROME_BROWSER_DEVICE_ATTESTATION_DEVICE_ATTESTATION_API_H_
+#define IOS_PUBLIC_PROVIDER_CHROME_BROWSER_DEVICE_ATTESTATION_DEVICE_ATTESTATION_API_H_
+
+#import <memory>
+
+#import "components/enterprise/device_attestation/ios/attestation_service_ios.h"
+
+namespace ios::provider {
+
+// Creates an instance of `AttestationServiceIOS`.
+std::unique_ptr<enterprise::AttestationServiceIOS>
+CreateAttestationServiceIOS();
+
+}  // namespace ios::provider
+
+#endif  // IOS_PUBLIC_PROVIDER_CHROME_BROWSER_DEVICE_ATTESTATION_DEVICE_ATTESTATION_API_H_
