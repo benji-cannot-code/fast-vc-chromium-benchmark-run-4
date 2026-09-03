51e4abe4866273edd95d44993013f163a594a2f1
Joemer Ramos <joemerramos@google.com>
2026-09-03 09:29:58 -0700

    [iOS] Stop ContentSettingsCoordinator before replacing it in settings
    In SettingsTableViewController showContentSettings,
    _contentSettingsCoordinator was being re-instantiated and assigned
    without stopping the existing coordinator first. This caused the
    previous coordinator to be deallocated while _stopped remained NO,
    triggering CHECK(_stopped, base::NotFatalUntil::M154) in
    -[ContentSettingsCoordinator dealloc].
    
    Stop the existing coordinator before creating a replacement, matching
    the pattern used by sibling coordinators in SettingsTableViewController.
    Also add a defensive stopContentSettingsCoordinator call in
    SettingsNavigationController showContentSettings.
    
    Fixed: 556132286
    Bug: 427791214
    Change-Id: Ic36baed8b7866255b5e6b74516523866dd6eb6ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339971
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691675}

--- ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.mm
@@ -928,6 +928,7 @@ NSString* const kSettingsDoneButtonId = @"kSettingsDoneButtonId";
     // No need to open it.
     return;
   }
+  [self stopContentSettingsCoordinator];
   self.contentSettingsCoordinator = [[ContentSettingsCoordinator alloc]
       initWithBaseNavigationController:self
                                browser:self.browser];
