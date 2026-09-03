72133eb4151fae736b0dac971b99c44b305194df
Chase Hartsell <chazzy@google.com>
2026-09-03 11:05:28 -0700

    [organizer] Add close button to tabs in open tabs section
    Also cleans up some existing code:
    - Use `assert` instead of if-statement in callbacks where `tab` should
      be defined
    - Reuses existing strings from the tab search UI
    
    Demo: http://go/scrcast/NTA1MTQ1NzE5MDQ5NDIwOHwyMzRkODQ3Yy0yMQ
    
    Bug: 549784710
    Change-Id: Ib890843d7d2715b2e84968d8789859926c62b359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8343204
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Auto-Submit: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691767}

--- chrome/test/data/webui/organizer_panel/open_tabs_delegate_test.ts
+++ chrome/test/data/webui/organizer_panel/open_tabs_delegate_test.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {browserProxyFactory, OpenTabsDelegate, PageHandlerRemote} from 'chrome://organizer-panel.top-chrome/organizer_panel.js';
 import type {OrganizerListSectionClient, OrganizerListSectionItem, PageRemote, ProfileData, Tab} from 'chrome://organizer-panel.top-chrome/organizer_panel.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {TestMock} from 'chrome://webui-test/test_mock.js';
 import {microtasksFinished} from 'chrome://webui-test/test_util.js';
@@ -98,6 +99,10 @@ suite('OpenTabsDelegateTest', () => {
   };
 
   setup(() => {
+    loadTimeData.resetForTesting({
+      openTabs: 'Open Tabs',
+      closeTab: 'Close tab',
+    });
     mockPageHandler = TestMock.fromClass(PageHandlerRemote);
     const {instance, remote} =
         browserProxyFactory.createForTest(mockPageHandler);
@@ -124,6 +129,8 @@ suite('OpenTabsDelegateTest', () => {
     assertEquals('www.youtube.com', items[0]!.description?.[0]);
     assertEquals(youtubeTab.lastActiveElapsedText, items[0]!.description?.[1]);
     assertEquals(youtubeTab.url, items[0]!.prefixIcon?.urls?.[0]);
+    assertEquals('cr:close', items[0]!.hoveredActionButton?.icon);
+    assertEquals('Close tab', items[0]!.hoveredActionButton?.ariaLabel);
     assertEquals(youtubeTab, items[0]!.data);
 
     // Second most recent tab.
@@ -230,5 +237,19 @@ suite('OpenTabsDelegateTest', () => {
     assertEquals(1, mockPageHandler.getCallCount('switchToTab'));
     const args = mockPageHandler.getArgs('switchToTab')[0];
     assertEquals(chromiumTab.tabId, args.tabId);
+  });
+
+  test('closes tab when action button is clicked', async () => {
+    const client = new TestClient();
+    delegate.init(client);
+
+    const items = await delegate.getItems();
+    assertEquals(3, items.length);
+
+    delegate.onItemActionButtonClicked(items[1]!);
+
+    assertEquals(1, mockPageHandler.getCallCount('closeTab'));
+    const args = mockPageHandler.getArgs('closeTab')[0];
+    assertEquals(chromiumTab.tabId, args);
   });
 });
