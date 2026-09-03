3cc96a8aba6002b0e6c6e40fedd394bea3a2685b
Michael Wilson <mjwilson@chromium.org>
2026-09-02 17:16:32 -0700

    Replace empty destructors with default in media/audio and services/audio
    This should cause no functional change.
    
    Reference styleguide/c++/c++-dos-and-donts.md (prefer to use =default)
    
    Change-Id: Icb99a7a6a57592cd19fa2ede215003a9cd1d165e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8340912
    Auto-Submit: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691215}

--- media/audio/audio_io.h
+++ media/audio/audio_io.h
@@ -61,7 +61,7 @@ class MEDIA_EXPORT AudioOutputStream {
   // itself such as creating Windows or initializing COM.
   class MEDIA_EXPORT AudioSourceCallback {
    public:
-    virtual ~AudioSourceCallback() {}
+    virtual ~AudioSourceCallback() = default;
 
     // Provide more data by fully filling |dest|. The source will return the
     // number of frames it filled. |delay| is the duration of audio written to
@@ -100,7 +100,7 @@ class MEDIA_EXPORT AudioOutputStream {
     virtual void OnError(ErrorType type) = 0;
   };
 
-  virtual ~AudioOutputStream() {}
+  virtual ~AudioOutputStream() = default;
 
   // Opens the stream. Returns `false` if the stream cannot be opened. This
   // method should not be called more than once, and must always be eventually
@@ -180,10 +180,10 @@ class MEDIA_EXPORT AudioInputStream {
     virtual void OnError(Error error_code) = 0;
 
    protected:
-    virtual ~AudioInputCallback() {}
+    virtual ~AudioInputCallback() = default;
   };
 
-  virtual ~AudioInputStream() {}
+  virtual ~AudioInputStream() = default;
 
   enum class OpenOutcome {
     kSuccess,
