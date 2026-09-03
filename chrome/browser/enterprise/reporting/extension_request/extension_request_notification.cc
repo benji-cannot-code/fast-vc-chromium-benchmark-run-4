5c282be72d885abf87aa8b12a11e6f516bffa930
Danilo Tedeschi <danft@google.com>
2026-09-03 08:08:46 -0700

    Fix no member named SplitString in //c/b/enterprise/...
    This is causing another CL of mine fail somehow
    https://chromium-review.git.corp.google.com/c/chromium/src/+/8343167.
    
    Change-Id: I6d2fac7d1ab9d23d112cc885c0a25422ba8e1b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8344871
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691585}

--- chrome/browser/enterprise/reporting/extension_request/extension_request_notification.cc
+++ chrome/browser/enterprise/reporting/extension_request/extension_request_notification.cc
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/public/cpp/notification_delegate.h"
 #include "ui/native_theme/native_theme.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/strings/string_split.h"
+#endif
+
 namespace enterprise_reporting {
 namespace {
 
