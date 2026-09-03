de0c5ca421155c03e6ea0cb3f06f05297b106432
Wilson Low <wilsonlow@google.com>
2026-09-03 14:18:31 -0700

    [Direct Offers] Add flag for wallet direct offers notification bubble
    Add kAutofillEnableWalletDirectOffersNotificationBubble feature flag to
    enable offer notification bubble for wallet direct offers.
    
    Design: go/direct-offers-autofill-chrome-design
    
    Bug: 546252995
    Change-Id: I40aaec0df1b15999ffe6d80625e65bd1f62371d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264584
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691932}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8365,7 +8365,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAccessibilityReducedAnimationsDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(features::kAccessibilityReducedAnimations)},
 
-
     {"enable-accessibility-magnifier-follows-chromevox",
      flag_descriptions::kAccessibilityMagnifierFollowsChromeVoxName,
      flag_descriptions::kAccessibilityMagnifierFollowsChromeVoxDescription,
@@ -14059,6 +14058,15 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAndroidUnknownGamepadExtraAxesDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(features::kAndroidUnknownGamepadExtraAxes)},
 #endif  // BUILDFLAG(IS_ANDROID)
+
+    {"autofill-enable-wallet-direct-offers-notification-bubble",
+     flag_descriptions::kAutofillEnableWalletDirectOffersNotificationBubbleName,
+     flag_descriptions::
+         kAutofillEnableWalletDirectOffersNotificationBubbleDescription,
+     kOsDesktop,
+     FEATURE_VALUE_TYPE(
+         autofill::features::
+             kAutofillEnableWalletDirectOffersNotificationBubble)},
 
     // Add new entries above this line.
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
