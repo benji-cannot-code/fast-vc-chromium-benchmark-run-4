234c009e2b71b43deab19cadca6768361538f541
Amando Jimenez <amandoj@google.com>
2026-09-03 20:31:53 -0700

    Add styling and fix positioning for the action menu
    Bug: b:556805370
    Change-Id: I8e63c2cccc13724655b7df53d2fe72bd57ff6e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354787
    Reviewed-by: Isabelle Goralsky <igoralsky@google.com>
    Commit-Queue: Amando Jimenez <amandoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692154}

--- chrome/browser/resources/context_hub/memory_banks/memory_banks_entry.html.ts
+++ chrome/browser/resources/context_hub/memory_banks/memory_banks_entry.html.ts
@@ -51,10 +51,6 @@ export function getHtml(this: MemoryBanksElement, entry: MemoryBankEntry) {
             })}
             </span>
           </div>
-          <cr-icon-button class="card-more-btn" iron-icon="cr:more-vert"
-              title="More actions"
-              @click="${(e: MouseEvent) => this.onMoreActionsClick_(entry, e)}">
-          </cr-icon-button>
         </div>
         ${
         entry.tags && entry.tags.length > 0 ? html`
@@ -65,6 +61,10 @@ export function getHtml(this: MemoryBanksElement, entry: MemoryBankEntry) {
           </div>
         ` :
                                               ''}
+        <cr-icon-button class="card-more-btn" iron-icon="cr:more-vert"
+            title="More actions"
+            @click="${(e: MouseEvent) => this.onMoreActionsClick_(entry, e)}">
+        </cr-icon-button>
       </div>
     </a>
   `;
