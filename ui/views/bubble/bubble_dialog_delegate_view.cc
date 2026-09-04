b1d2a04f4d04fea4e372cb4778708df559a3f7e6
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-09-03 20:36:34 -0700

    Use base::ScopedObservation for the bubble anchor view observation
    Bug: 41446264
    Change-Id: I93b35de4055f98484e8958adee3516f3b4ea18e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8350484
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692157}

--- ui/views/bubble/bubble_dialog_delegate_view.cc
+++ ui/views/bubble/bubble_dialog_delegate_view.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/scoped_observation.h"
 #include "base/strings/strcat.h"
 #include "build/build_config.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -216,7 +217,7 @@ class BubbleDialogDelegate::AnchorViewObserver : public ViewObserver {
  public:
   AnchorViewObserver(BubbleDialogDelegate* parent, View* anchor_view)
       : parent_(parent), anchor_view_(anchor_view) {
-    anchor_view_->AddObserver(this);
+    view_observation_.Observe(anchor_view);
     scoped_notify_ = std::make_unique<
         views::View::ScopedNotifyObserversOnVisibleBoundsChanged>(*anchor_view);
     AddToAnchorVector();
@@ -227,7 +228,6 @@ class BubbleDialogDelegate::AnchorViewObserver : public ViewObserver {
 
   ~AnchorViewObserver() override {
     RemoveFromAnchorVector();
-    anchor_view_->RemoveObserver(this);
   }
 
   View* anchor_view() const { return anchor_view_; }
@@ -292,6 +292,9 @@ class BubbleDialogDelegate::AnchorViewObserver : public ViewObserver {
   const raw_ptr<View> anchor_view_;
   bool bubble_widget_visible_ = true;
   bool update_bounds_when_visible_ = false;
+
+  base::ScopedObservation<views::View, views::ViewObserver> view_observation_{
+      this};
 };
 
 // This class is responsible for observing events on a BubbleDialogDelegate's
