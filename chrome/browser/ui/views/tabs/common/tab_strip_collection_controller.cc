58fad111f8422be5c67c840cc800e9eba942c7ac
Vince Lugli <lugli@google.com>
2026-09-04 12:51:59 -0700

    [Unified Tabstrip] Add leading separator when tab group is collapsed.
    Since only the trailing separator is now being drawn, we must draw the
    leading separator in specific cases where the adjacent tab does not draw
    a separator. In this case, the tab group header does not have logic to
    draw their own separator.
    
    Bug: 556216987
    Change-Id: I39bcea39caa3cb968c25aa4e50e43df83e1c11a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8348184
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Vince Lugli <lugli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692681}

--- chrome/browser/ui/views/tabs/common/tab_strip_collection_controller.cc
+++ chrome/browser/ui/views/tabs/common/tab_strip_collection_controller.cc
@@ -131,6 +131,11 @@ TabStripCollectionController::GetFocusedGroup() const {
   return model_->GetFocusedGroup();
 }
 
+bool TabStripCollectionController::IsGroupCollapsed(
+    const tab_groups::TabGroupId& group_id) const {
+  return model_->SupportsTabGroups() && model_->IsGroupCollapsed(group_id);
+}
+
 std::optional<SkColor> TabStripCollectionController::GetGroupColor(
     const tabs::TabInterface* tab_interface) const {
   std::optional<tab_groups::TabGroupId> group_id = tab_interface->GetGroup();
