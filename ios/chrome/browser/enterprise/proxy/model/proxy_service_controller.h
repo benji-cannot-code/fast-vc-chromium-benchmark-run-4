0e2be01889027d68b5035a3534d6b3c8e9a4f1ff
Dmytro Dmytriiev <dmytriievd@google.com>
2026-09-04 08:45:28 -0700

    [iOS] Add ProxyServiceController skeleton
    Add skeleton for profile-keyed ProxyServiceController and its factory.
    
    ProxyServiceController is a ProfileIOS-scoped KeyedService responsible
    for managing web-layer proxy configurations, observing dynamic
    enterprise proxy route updates, and resolving proxy authentication
    challenges.
    
    Design doc: go/secure-gateway-on-ios
    (https://docs.google.com/document/d/1XQGXQotpWlymh5bIrEkGa53L4rgdAj45jOlZE6Y6X6M/edit?pli=1&tab=t.0)
    
    Bug: 543367420
    Change-Id: I1e4c1ecb7f8e3cda0b228606ef77359081f95f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8343089
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692478}

---
+++ ios/chrome/browser/enterprise/proxy/model/proxy_service_controller.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_ENTERPRISE_PROXY_MODEL_PROXY_SERVICE_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_ENTERPRISE_PROXY_MODEL_PROXY_SERVICE_CONTROLLER_H_
+
+#import "components/keyed_service/core/keyed_service.h"
+
+// Profile-scoped `KeyedService` responsible for managing web-layer proxy
+// configuration and observing enterprise proxy route updates.
+class ProxyServiceController : public KeyedService {
+ public:
+  ProxyServiceController();
+  ~ProxyServiceController() override;
+};
+
+#endif  // IOS_CHROME_BROWSER_ENTERPRISE_PROXY_MODEL_PROXY_SERVICE_CONTROLLER_H_
---
+++ ios/chrome/browser/enterprise/proxy/model/proxy_service_controller_factory.mm
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/proxy/model/proxy_service_controller_factory.h"
+
+#import "base/no_destructor.h"
+#import "ios/chrome/browser/enterprise/proxy/model/proxy_service_controller.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+
+// static
+ProxyServiceController* ProxyServiceControllerFactory::GetForProfile(
+    ProfileIOS* profile) {
+  return GetInstance()->GetServiceForProfileAs<ProxyServiceController>(
+      profile, /*create=*/true);
+}
+
+// static
+ProxyServiceControllerFactory* ProxyServiceControllerFactory::GetInstance() {
+  static base::NoDestructor<ProxyServiceControllerFactory> instance;
+  return instance.get();
+}
+
+ProxyServiceControllerFactory::ProxyServiceControllerFactory()
+    : ProfileKeyedServiceFactoryIOS("ProxyServiceController",
+                                    ProfileSelection::kNoInstanceInIncognito) {}
+
+ProxyServiceControllerFactory::~ProxyServiceControllerFactory() = default;
+
+std::unique_ptr<KeyedService>
+ProxyServiceControllerFactory::BuildServiceInstanceFor(
+    ProfileIOS* profile) const {
+  return std::make_unique<ProxyServiceController>();
+}
