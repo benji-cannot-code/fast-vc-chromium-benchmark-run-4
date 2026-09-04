ebcdbfddb01512ed38db19bbeb50e4775235e162
Julia Hansbrough <flowerhack@google.com>
2026-09-04 13:11:30 -0700

    [window.rust] Add ergonomic Rust API for FeatureParam<i32,bool>.
    Now that base::Feature is fully implemented, we'd like to add support
    for FeatureParams.
    
    FeatureParams are an extension for Features that allow for finer-grained
    trials.
    
    To start with, we are only implementing support for i32 and bool, though
    in follow-on CLs we'll implement support for all FeatureParams.
    
    Unlike base Features, FeatureParams doesn't particularly need access to
    raw pointers across the language boundary.  So, we simply create a shim
    that wraps the equivalent C++ functions, similar to how, e.g.,
    CommandLine support was provided.
    
    Bug: 548762794
    Change-Id: I2605460a2e34f7aad1c2a794e86ab40d0a92df0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8266588
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Commit-Queue: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692699}

---
+++ base/feature_rust_shim.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_FEATURE_RUST_SHIM_H_
+#define BASE_FEATURE_RUST_SHIM_H_
+
+#include <stdint.h>
+
+#include "base/feature.h"
+#include "third_party/rust/cxx/v1/cxx.h"
+
+namespace base {
+
+bool GetFieldTrialParamByFeatureAsBoolShim(const Feature& feature,
+                                           ::rust::Str param_name,
+                                           bool default_value);
+
+int32_t GetFieldTrialParamByFeatureAsIntShim(const Feature& feature,
+                                             ::rust::Str param_name,
+                                             int32_t default_value);
+
+}  // namespace base
+
+#endif  // BASE_FEATURE_RUST_SHIM_H_
