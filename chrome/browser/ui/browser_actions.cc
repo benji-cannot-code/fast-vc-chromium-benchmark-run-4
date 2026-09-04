90b16e311ccdf0aa24a834e7a40cf24a447d8526
Agata Szpotek <szpotekagata@google.com>
2026-09-04 01:15:53 -0700

    [Isolated mode] Support enterprise isolated mode profiles in kActionCloseProfile
    Update kActionCloseProfile to use
    Profile::IsPrimaryOTRProfileWithRegularParent() instead of checking only
    for Incognito profiles. This ensures that when closing an enterprise
    isolated mode profile, only the browser windows belonging to that
    isolated profile are closed (via CloseAllBrowsersWithIncognitoProfile)
    rather than closing all windows associated with the parent regular
    profile.
    
    Fixing: 556593733
    Change-Id: I2d5fe9ed9247c1b5d282879c4f29ab1a4bbbc905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353523
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Agata Szpotek <szpotekagata@google.com>
    Commit-Queue: Viacheslav Krakhotin <krakhotin@google.com>
    Reviewed-by: Viacheslav Krakhotin <krakhotin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692259}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -4119,7 +4119,7 @@ void BrowserActions::InitializeToolbarAndMiscActions() {
               [](BrowserWindowInterface* bwi, actions::ActionItem* item,
                  actions::ActionInvocationContext context) {
                 Profile* profile = bwi->GetProfile();
-                if (profile->IsIncognitoProfile()) {
+                if (profile->IsPrimaryOTRProfileWithRegularParent()) {
                   chrome::CloseAllBrowsersWithIncognitoProfile(profile);
                 } else {
                   profiles::CloseProfileWindows(profile);
