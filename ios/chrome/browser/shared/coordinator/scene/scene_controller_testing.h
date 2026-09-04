dd48ddf3cb6296a21e812b4edfce104b869f4d7d
Joemer Ramos <joemerramos@google.com>
2026-09-04 07:26:32 -0700

    [ios] Preserve Gemini floaty on external Gemini launch intents
    When opening a tab via an external intent, SceneController dismisses all
    modal dialogs before presenting the new tab. Gemini floaty was included
    as part of this dismissal in https://crrev.com/c/8232238.
    
    This CL updates SceneCommands and SceneCoordinator to support a
    dismissGemini parameter, allowing SceneController to bypass Gemini
    dismissal when handling START_GEMINI_AI_SUMMARIZATION or
    TRIGGER_GEMINI_PROMO actions outside of incognito mode.
    
    Bug: 546065819, 555233874, 555235097
    Change-Id: Ice7886329c0c4161e07d761428921cbc4dcb7ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8350166
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692431}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller_testing.h
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller_testing.h
@@ -25,6 +25,10 @@ struct UrlLoadParams;
 - (void)dismissModalDialogsWithCompletion:(ProceduralBlock)completion
                            dismissOmnibox:(BOOL)dismissOmnibox;
 
+- (void)dismissModalDialogsWithCompletion:(ProceduralBlock)completion
+                           dismissOmnibox:(BOOL)dismissOmnibox
+                            dismissGemini:(BOOL)dismissGemini;
+
 - (WrangledBrowser*)currentInterface;
 
 - (SceneCoordinator*)mainCoordinator;
