c63ada7dffb0f01d72e6ce4ab4fab868ea2e97aa
Longsheng Yin <longsheng@google.com>
2026-09-03 19:18:36 -0700

    Unify Pix account linking prompt user action metrics
    Unify Pix account linking prompt user action and interaction duration
    logging with the generic OnAccountLinkingPromptAction flow across Java
    and native C++ controller components.
    
    Bug: 505507305
    Change-Id: I49bb9dc2c6da944b38310e04d31bc3ad6a5c1f0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8272334
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Reviewed-by: Siddharth Shah <siashah@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692126}

--- chrome/browser/facilitated_payments/ui/android/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsComponent.java
+++ chrome/browser/facilitated_payments/ui/android/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsComponent.java
@@ -61,12 +61,6 @@ interface FacilitatedPaymentsPaymentMethodsComponent {
          */
         void onPaymentAppSelected(String packageName, String activityName);
 
-        /** Called whenever the Pix account linking prompt is accepted. */
-        void onPixAccountLinkingPromptAccepted();
-
-        /** Called whenever the Pix account linking prompt is declined. */
-        void onPixAccountLinkingPromptDeclined();
-
         /** Called whenever an account linking prompt is shown. */
         void onAccountLinkingPromptShown(@FacilitatedPaymentsType int type);
 
