12dc582fab882f4c190d315b7125ea34a5e553e5
Camillia Smith Barnes <cammie@chromium.org>
2026-09-04 09:24:13 -0700

    WebUILocationBar: Fix DiscountsIconViewBrowserTest
    When WebUILocationBar is enabled, chip rendering occurs asynchronously
    over Mojo IPC between the browser and WebUI renderer, updating
    PageActionModel::IsChipShowing() only upon receiving the response.
    
    Update
    DiscountsIconViewBrowserTest.InvokeUi_show_discounts_icon_with_label to
    use base::test::RunUntil() to wait for chip visibility before asserting
    on the chip text.
    
    TAG=agy
    CONV=b265148d-daf7-4f65-87ba-3181187a74e5
    
    Bug: 556729654
    Change-Id: I4c47335c37fb9b69f4d56d06cf45b34781361a42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8355328
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692512}

--- chrome/browser/ui/views/commerce/discounts_icon_view_browsertest.cc
+++ chrome/browser/ui/views/commerce/discounts_icon_view_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/run_until.h"
 #include "base/time/default_clock.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
@@ -93,7 +94,8 @@ class DiscountsIconViewBrowserTest : public UiBrowserTest {
     std::string test_name =
         testing::UnitTest::GetInstance()->current_test_info()->name();
     if (test_name == "InvokeUi_show_discounts_icon_with_label") {
-      EXPECT_TRUE(accessor.IsChipVisible());
+      EXPECT_TRUE(base::test::RunUntil(
+          [&accessor]() { return accessor.IsChipVisible(); }));
       EXPECT_EQ(accessor.GetText(),
                 l10n_util::GetStringUTF16(IDS_DISCOUNT_ICON_EXPANDED_TEXT));
     } else if (test_name == "InvokeUi_show_discounts_icon_only") {
