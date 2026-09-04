8e5bd23f51bef59faec923439b1b8402d3eb190d
Kristi Saney <kristislee@google.com>
2026-09-04 11:56:20 -0700

    [Reading mode] Fix flakiness in SettingsMenuTest
    Fixed: 535346329
    Change-Id: I2f301a190de01394754be395eaa614e7b63a1031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8355033
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692643}

--- chrome/test/data/webui/side_panel/read_anything/settings_menu_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/settings_menu_test.ts
@@ -12,7 +12,7 @@ import {assertDeepEquals, assertEquals, assertFalse, assertNotEquals, assertTrue
 import {keyDownOn} from 'chrome-untrusted://webui-test/keyboard_mock_interactions.js';
 import {MockTimer} from 'chrome-untrusted://webui-test/mock_timer.js';
 import {TestUserEducationMixedTrustHandler} from 'chrome-untrusted://webui-test/test_user_education_mixed_trust_handler.js';
-import {eventToPromise, microtasksFinished} from 'chrome-untrusted://webui-test/test_util.js';
+import {eventToPromise, microtasksFinished, whenAttributeIs} from 'chrome-untrusted://webui-test/test_util.js';
 
 import {setupTestEnvironment} from './common.js';
 import type {TestMetricsBrowserProxy} from './test_metrics_browser_proxy.js';
@@ -48,6 +48,10 @@ suite('SettingsMenuElement', () => {
 
     settingsMenu.$.lazyMenu.get();
     await microtasksFinished();
+  });
+
+  teardown(() => {
+    settingsMenu.close();
   });
 
   test('click outside fires close-all-menus event', async () => {
@@ -260,12 +264,14 @@ suite('SettingsMenuElement', () => {
         assertTrue(!!targetItem);
         targetItem.click();
 
-        const whenFired =
+        let whenFired =
             eventToPromise(ToolbarEvent.CLOSE_SUBMENU_REQUESTED, settingsMenu);
         keyDownOn(settingsMenu, 0, undefined, 'Escape');
         await whenFired;
 
         targetItem.click();
+        whenFired =
+            eventToPromise(ToolbarEvent.CLOSE_SUBMENU_REQUESTED, settingsMenu);
         keyDownOn(settingsMenu, 0, undefined, 'ArrowLeft');
         await whenFired;
       });
@@ -279,16 +285,17 @@ suite('SettingsMenuElement', () => {
     assertTrue(!!targetItem);
 
     let openSubmenuWasFiredAfterClose = false;
-    actionMenu.addEventListener(ToolbarEvent.OPEN_SETTINGS_SUBMENU, () => {
+    settingsMenu.addEventListener(ToolbarEvent.OPEN_SETTINGS_SUBMENU, () => {
       openSubmenuWasFiredAfterClose = true;
     });
 
     const timer = new MockTimer();
+    timer.install();
     targetItem.dispatchEvent(new PointerEvent(
         'pointerenter', {bubbles: true, cancelable: true, view: window}));
-    timer.tick(SUBMENU_SHOW_DELAY_MS - 1);
-    actionMenu.close();
-    timer.tick(1);
+    timer.tick(MENU_SHOW_DELAY_MS - 1);
+    settingsMenu.close();
+    timer.tick(MENU_SHOW_DELAY_MS + 1);
 
     assertFalse(openSubmenuWasFiredAfterClose);
     timer.uninstall();
@@ -407,6 +414,7 @@ suite('SettingsMenuElement', () => {
         eventToPromise(ToolbarEvent.OPEN_SETTINGS_SUBMENU, settingsMenu);
     fontItem.click();
     await whenFired;
+    await whenAttributeIs(fontItem, 'aria-expanded', 'true');
     await microtasksFinished();
 
     menuItems =
@@ -525,7 +533,10 @@ suite('SettingsMenuElement', () => {
     assertTrue(!!translateItem);
 
     // Click fontItem to open its submenu.
+    const whenSubmenuOpened =
+        eventToPromise(ToolbarEvent.OPEN_SETTINGS_SUBMENU, settingsMenu);
     fontItem.click();
+    await whenSubmenuOpened;
     await microtasksFinished();
 
     const whenFired = eventToPromise<CustomEvent<{previousId: SettingsOption}>>(
