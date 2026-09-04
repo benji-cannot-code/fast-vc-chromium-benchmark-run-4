4ed1213044c184f806a6d6034c1299e0793e6125
kylechar <kylechar@chromium.org>
2026-09-04 09:33:00 -0700

    Remove //ui/gl dep from //cc
    It's not used anymore.
    
    Bug: 553341301
    Change-Id: Ie5a0e9f77c4f69beed99dda1fab9968a2e7d3ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8304276
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692524}

--- cc/layers/heads_up_display_layer_impl.cc
+++ cc/layers/heads_up_display_layer_impl.cc
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/skia_conversions.h"
-#include "ui/gl/trace_util.h"
 
 namespace cc {
 
--- cc/raster/one_copy_raster_buffer_provider.cc
+++ cc/raster/one_copy_raster_buffer_provider.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkAlphaType.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColorType.h"
-#include "ui/gl/trace_util.h"
 
 namespace cc {
 
