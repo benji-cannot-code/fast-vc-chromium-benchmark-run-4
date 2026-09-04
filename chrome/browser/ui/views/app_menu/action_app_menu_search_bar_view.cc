8a34c432b53172b44d34fa0455c054a8f171023f
Eshwar Stalin <estalin@chromium.org>
2026-09-04 14:36:13 -0700

    [ChroMenu GlowUp] Setup focus behavior for block style button
    Setup focus behavior for the block style button to allow keyboard
    navigation.
    
    Change-Id: I73921a128e4d0cb67c107ae558aac504402d1f56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8357036
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692771}

--- chrome/browser/ui/views/app_menu/action_app_menu_search_bar_view.cc
+++ chrome/browser/ui/views/app_menu/action_app_menu_search_bar_view.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/event_monitor.h"
 #include "ui/views/widget/widget.h"
+
 ActionAppMenuSearchBarView::ActionAppMenuSearchBarView() {
   const auto* provider = ChromeLayoutProvider::Get();
   int icon_size =
