2f82551056666a1f27f14535507277ed423b5a77
Eva Su <evasu@chromium.org>
2026-09-04 09:12:26 -0700

    [SidePanel] Introduce Native Android UI for Extension Side Panel
    This CL introduces the native Android UI components and C++ bridge
    necessary to render extension side panels on Android.
    
    New files added:
    - ExtensionSidePanelContents.java: Wraps the extension's WebContents
      within a ThinWebView to attach it to the Android view hierarchy and
      manages the view delegates lifecycle.
    - ExtensionSidePanelContentsAndroid (C++): Acts as the native bridge,
      implementing the ExtensionView interface and connecting the
      ExtensionViewHost to the Java UI representation.
    - ExtensionSidePanelCoordinatorAndroid: Implements the Android delegate
      for ExtensionSidePanelCoordinator to handle the creation of the
      ExtensionViewHost and its corresponding SidePanelNativeView.
    
    This CL also updates CreateSidePanelHost in ExtensionViewHostFactory to
    support Android by utilizing ExtensionViewHostDelegateAndroid. This CL
    doesn’t cause any functional changes and helps set up the other parts of
    enabling Side Panel on Desktop Android. The code paths executed on
    Desktop remain unchanged. On Desktop Android, this CL adds all the
    necessary plumbing without breaking existing flows or exposing untested
    entry points.
    
    Tested this change with crrev.com/c/8309259 and crrev.com/c/8351248, and
    extensions with side panel work on Desktop Android:
    https://screenshot-v2.corp.google.com/3788dtbrptit8
    
    Fixed: 555358470
    Change-Id: I3d41ef57b6a45571f6548c2dad9cc1bb0e9a9d41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8309987
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692505}

--- chrome/browser/extensions/extension_view_host_factory.h
+++ chrome/browser/extensions/extension_view_host_factory.h
@@ -40,7 +40,6 @@ class ExtensionViewHostFactory {
       const GURL& url,
       BrowserWindowInterface* browser);
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
   // Creates a new ExtensionHost with its associated view, grouping it in the
   // appropriate SiteInstance (and therefore process) based on the URL and
   // profile.
@@ -49,7 +48,6 @@ class ExtensionViewHostFactory {
       const GURL& url,
       BrowserWindowInterface* browser,
       tabs::TabInterface* tab_interface);
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 };
 
 }  // namespace extensions
