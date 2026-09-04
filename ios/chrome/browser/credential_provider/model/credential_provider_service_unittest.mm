f54db208a9594e6e082aa4122ada9e97883cacc4
Gauthier Ambard <gambard@chromium.org>
2026-09-04 02:29:23 -0700

    [iOS] Remove unused DeleteFaviconsFolder utility function
    DeleteFaviconsFolder is dead code that was only invoked as redundant
    cleanup in unit tests using ScopedTempDir.
    
    Low-Coverage-Reason: TRIVIALLY_NOT_EVALUATED removing dead code
    Change-Id: Ibd3f615fe597185d0a5b4f2087bbf4d07e5bc2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8320390
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692292}

--- ios/chrome/browser/credential_provider/model/credential_provider_service_unittest.mm
+++ ios/chrome/browser/credential_provider/model/credential_provider_service_unittest.mm
@@ -114,9 +114,6 @@ class CredentialProviderServiceTest : public PlatformTest {
 
   void SetUp() override {
     PlatformTest::SetUp();
-    // Make sure there are no favicons left from some other tests.
-    ASSERT_TRUE(DeleteFaviconsFolder());
-
     ASSERT_TRUE(scoped_temp_dir_.CreateUniqueTempDir());
     NSURL* folder_url =
         base::apple::FilePathToNSURL(scoped_temp_dir_.GetPath());
@@ -133,8 +130,6 @@ class CredentialProviderServiceTest : public PlatformTest {
   }
 
   void TearDown() override {
-    // Delete all favicon files that were created during the test.
-    EXPECT_TRUE(DeleteFaviconsFolder());
     SetFaviconsFolderURLForTesting(nil);
     ResetMaxNumberOfFaviconsForTesting();
 
