5e359007fef3a4198f8eda625e8ad11163f7dad9
Jérôme Lebel <jlebel@chromium.org>
2026-09-04 05:10:14 -0700

    [iOS] Use TestProfileManagerIOS in unit tests
    Update unit tests to create and manage test profiles using
    TestProfileManagerIOS instead of building TestProfileIOS directly.
    
    When profiles are built standalone via TestProfileIOS::Builder, they are
    not registered with the ProfileManagerIOS in ApplicationContext. This
    causes issues with features that query profiles from the manager, such
    as identity mapping and filtering (preparing for crrev.com/c/8250990).
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    R=alionadangla@chromium.org, rkgibson@google.com
    
    - Replace std::unique_ptr<TestProfileIOS> with TestProfileManagerIOS
      and a raw_ptr<TestProfileIOS>.
    - Use TestProfileManagerIOS::AddProfileWithBuilder() to register the
      test profile.
    - Clean up raw pointers and dependent services/mediators in TearDown()
      to avoid dangling references upon profile destruction.
    - Simplify profile_.get() calls to profile_.
    
    Specifically: 
    Change-Id: I0f7f9532dfa0d850b748de06192d5cd5b8fbdf39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353385
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692356}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/grid_mediator_test.h
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/grid_mediator_test.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "base/test/metrics/user_action_tester.h"
 #import "base/test/scoped_feature_list.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
 #import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "ios/web/public/web_state_id.h"
@@ -30,7 +31,6 @@ class IOSChromeScopedTestingLocalState;
 class PlatformTest;
 @class SceneState;
 class TabGroupService;
-class TestProfileIOS;
 class TestSceneUrlLoadingService;
 class UrlLoadingBrowserAgent;
 
@@ -64,7 +64,8 @@ class GridMediatorTestClass : public PlatformTest {
   web::WebTaskEnvironment task_environment_;
   base::test::ScopedFeatureList scoped_feature_list_;
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
-  std::unique_ptr<TestProfileIOS> profile_;
+  TestProfileManagerIOS profile_manager_;
+  raw_ptr<TestProfileIOS> profile_;
   FakeTabCollectionConsumer* consumer_;
   std::vector<web::WebStateID> original_identifiers_;
   web::WebStateID original_selected_identifier_;
