599ce71c58483bdb2a008df7b68cd7641683dcf3
Adem Derinel <derinel@google.com>
2026-09-04 07:35:30 -0700

    [Autofill] Remove kAndroidAutofillFieldsUpdatedOnSelect feature flag
    kAndroidAutofillFieldsUpdatedOnSelect has been enabled by default.
    This CL cleans up the flag and associated code, calling UpdateCurrentField
    unconditionally when a select control changes, and cleans up the
    ProductionSupportedFlagList entry and test coverage.
    
    TAG=agy
    CONV=78083bba-06f2-4d9e-b724-abaa4ff6d61e
    
    Bug: 502346855
    Change-Id: Ia1164579514d3dff9c5802166237252ca7486bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8352724
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692439}

--- components/android_autofill/browser/android_autofill_provider_unittest.cc
+++ components/android_autofill/browser/android_autofill_provider_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/android_autofill/browser/android_autofill_bridge_factory.h"
-#include "components/android_autofill/browser/android_autofill_features.h"
 #include "components/android_autofill/browser/android_autofill_manager.h"
 #include "components/android_autofill/browser/android_autofill_provider_bridge.h"
 #include "components/android_autofill/browser/android_autofill_provider_test_api.h"
@@ -791,13 +790,10 @@ TEST_F(AndroidAutofillProviderTest, CancelSessionOnNavigation) {
   Reset(autofill_driver());
 }
 
-// Tests that OnSelectControlSelectionChanged updates the current field's origin
-// when the feature flag is enabled.
+// Tests that OnSelectControlSelectionChanged updates the current field's
+// origin.
 TEST_F(AndroidAutofillProviderTest,
        OnSelectControlSelectionChangedUpdatesOrigin) {
-  base::test::ScopedFeatureList scoped_feature_list{
-      features::kAndroidAutofillFieldsUpdatedOnSelect};
-
   FormData form = CreateFormDataForFrame(
       CreateTestPersonalInformationFormData(), main_frame_token());
   android_autofill_manager().OnFormsSeen({form}, /*removed_forms=*/{},
@@ -1317,9 +1313,6 @@ TEST_F(AndroidAutofillProviderWithCredManMultiFrameTest,
 // active (see crbug.com/518115316).
 TEST_F(AndroidAutofillProviderWithCredManMultiFrameTest,
        CredManActiveBlocksSpoofedSelectControlSelectionChanged) {
-  base::test::ScopedFeatureList scoped_feature_list{
-      features::kAndroidAutofillFieldsUpdatedOnSelect};
-
   // 1. Start session on main frame (origin https://foo.com) and trigger
   // CredMan.
   android_autofill_manager().OnFormsSeen({test_form()}, {},
