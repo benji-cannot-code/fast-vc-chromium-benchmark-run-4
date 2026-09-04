3a6da84a926acf555f3aa30eea1c1434a4b1dc74
David Awogbemila <awogbemila@chromium.org>
2026-09-04 07:48:33 -0700

    Reland Event.UserActivation.TriggerFor* histograms
    We are re-landing these histograms (removed in crrev.com/c/8183469) as
    part of our effort to revive browser-verified user activation over an
    alternate plan (widget-level tracking in browser). Re-measuring these
    metrics will help us understand how relanding browser-verified user
    activation affects user-activation-gated API calls in the wild and
    identify which synthetic activation triggers are actively relied upon by
    websites.
    
    Bug: 40091540
    Change-Id: Ic55acf26b296b0d0512b57c22bf7fbd72342b69d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8337469
    Commit-Queue: David A <awogbemila@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692442}

--- third_party/blink/common/frame/user_activation_state.cc
+++ third_party/blink/common/frame/user_activation_state.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/frame/user_activation_state.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/mojom/frame/user_activation_notification_type.mojom-shared.h"
 
 using blink::mojom::UserActivationNotificationType;
@@ -28,12 +29,25 @@ bool IsRestricted(UserActivationNotificationType notification_type) {
 
 }  // namespace
 
+UserActivationState::UserActivationState()
+    : first_notification_type_(UserActivationNotificationType::kNone),
+      last_notification_type_(UserActivationNotificationType::kNone) {}
 
 void UserActivationState::Activate(
     UserActivationNotificationType notification_type) {
   has_been_active_ = true;
   last_activation_was_restricted_ = IsRestricted(notification_type);
   ActivateTransientState();
+
+  // Update states for UMA.
+  CHECK_NE(notification_type, UserActivationNotificationType::kNone);
+  if (first_notification_type_ == UserActivationNotificationType::kNone) {
+    first_notification_type_ = notification_type;
+  }
+  last_notification_type_ = notification_type;
+  if (notification_type == UserActivationNotificationType::kInteraction) {
+    transient_state_expiry_time_for_interaction_ = transient_state_expiry_time_;
+  }
 }
 
 void UserActivationState::SetHasBeenActive() {
@@ -43,15 +57,27 @@ void UserActivationState::SetHasBeenActive() {
 void UserActivationState::Clear() {
   has_been_active_ = false;
   last_activation_was_restricted_ = false;
+  first_notification_type_ = UserActivationNotificationType::kNone;
+  last_notification_type_ = UserActivationNotificationType::kNone;
   DeactivateTransientState();
 }
 
 bool UserActivationState::HasBeenActive() const {
-  return has_been_active_;
+  if (has_been_active_) {
+    base::UmaHistogramEnumeration("Event.UserActivation.TriggerForSticky",
+                                  first_notification_type_);
+    return true;
+  }
+  return false;
 }
 
 bool UserActivationState::IsActive() const {
-  return IsActiveInternal();
+  if (IsActiveInternal()) {
+    base::UmaHistogramEnumeration("Event.UserActivation.TriggerForTransient",
+                                  EffectiveNotificationType());
+    return true;
+  }
+  return false;
 }
 
 bool UserActivationState::IsActiveInternal() const {
@@ -69,12 +95,32 @@ bool UserActivationState::LastActivationWasRestricted() const {
   return last_activation_was_restricted_;
 }
 
+void UserActivationState::RecordPreconsumptionUma() const {
+  if (!IsActiveInternal()) {
+    return;
+  }
+  base::UmaHistogramEnumeration("Event.UserActivation.TriggerForConsuming",
+                                EffectiveNotificationType());
+}
+
 void UserActivationState::ActivateTransientState() {
   transient_state_expiry_time_ = base::TimeTicks::Now() + kActivationLifespan;
 }
 
 void UserActivationState::DeactivateTransientState() {
   transient_state_expiry_time_ = base::TimeTicks();
+  transient_state_expiry_time_for_interaction_ = transient_state_expiry_time_;
+}
+
+UserActivationNotificationType UserActivationState::EffectiveNotificationType()
+    const {
+  // We treat a synthetic activation within the expiry time of a real
+  // interaction (of type kInteraction) as a real interaction because any user
+  // of transient activation state should work within that expiry time even if
+  // we drop all synthetic activations.
+  return base::TimeTicks::Now() <= transient_state_expiry_time_for_interaction_
+             ? UserActivationNotificationType::kInteraction
+             : last_notification_type_;
 }
 
 }  // namespace blink
