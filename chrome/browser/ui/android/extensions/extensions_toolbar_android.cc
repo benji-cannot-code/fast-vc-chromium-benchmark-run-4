2b5f04822f9bd119aa0851c856c522cdf91b9fd1
Nate Chapin <japhet@chromium.org>
2026-09-04 16:01:49 -0700

    Hide installed extensions in toolbar for TWAs
    Following https://crrev.com/cc9850bba285 which added the extensions menu
    to TWAs, pinned/installed extensions should not display action icons in
    the toolbar for web app / TWA windows.
    
    This change:
    1. Updates ToolbarActionsModel::CanShowActionsInToolbar() on Android to
       check that the browser type is not TYPE_APP or TYPE_APP_POPUP.
    2. Guards ExtensionsToolbarAndroid::GetPinnedActionIds() and
       GetRequestAccessButtonParams() with CanShowActionsInToolbar().
    3. In ExtensionsMenuViewModel::GetContextMenuButtonState(), guards
       is_action_pinned with CanShowActionsInToolbar().
    4. Passes isWebApp down to ExtensionActionListCoordinator and
       ExtensionActionListMediator, treating pinnedActionIds as empty for
       web apps while still allowing popped-out actions.
    5. Ensures the request access button is not visible in TWAs.
    
    TAG=agy
    CONV=c6002800-48ca-4478-a6c0-66bce1a2036d
    
    Bug: 557278759
    Change-Id: I4e3fcf2b793364794a9e2a171db44da62f666303
    Fixed: 557278759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8359308
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692819}

--- chrome/browser/ui/android/extensions/extensions_toolbar_android.cc
+++ chrome/browser/ui/android/extensions/extensions_toolbar_android.cc
@@ -112,8 +112,10 @@ base::android::ScopedJavaLocalRef<jobject>
 ExtensionsToolbarAndroid::GetRequestAccessButtonParams(
     JNIEnv* env,
     content::WebContents* web_contents) {
-  ExtensionsToolbarViewModel::RequestAccessButtonParams params =
-      toolbar_view_model_->GetRequestAccessButtonParams(web_contents);
+  ExtensionsToolbarViewModel::RequestAccessButtonParams params;
+  if (ToolbarActionsModel::CanShowActionsInToolbar(*browser_)) {
+    params = toolbar_view_model_->GetRequestAccessButtonParams(web_contents);
+  }
   return Java_RequestAccessButtonParams_Constructor(env, params.extension_ids,
                                                     params.tooltip_text);
 }
@@ -291,6 +293,9 @@ ExtensionsToolbarAndroid::GetAllActionIds(JNIEnv* env) {
 
 std::vector<ToolbarActionsModel::ActionId>
 ExtensionsToolbarAndroid::GetPinnedActionIds(JNIEnv* env) {
+  if (!ToolbarActionsModel::CanShowActionsInToolbar(*browser_)) {
+    return {};
+  }
   const auto& ids = toolbar_view_model_->GetPinnedActionIds();
   return std::vector(ids.begin(), ids.end());
 }
