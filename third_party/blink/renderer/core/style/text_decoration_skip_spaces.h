ec953b261c54a13d4a7d01a059e5341a366be12f
Perry <perryuwang@gmail.com>
2026-09-04 05:13:34 -0700

    text-decoration-skip-spaces: default to start end
    Change the initial value from `none` to `start end` per CSS Text
    Decoration 4. Absorb the new default in unrelated tests.
    
    Bug: 40862777
    Change-Id: I9fe2a65dc62f417c85c19f14154d8b7ae1d489a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8316575
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Perry <perryuwang@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1692359}

---
+++ third_party/blink/renderer/core/style/text_decoration_skip_spaces.h
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_TEXT_DECORATION_SKIP_SPACES_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_TEXT_DECORATION_SKIP_SPACES_H_
+
+namespace blink {
+
+enum class TextDecorationSkipSpaces : unsigned {
+  kNone = 0,
+  kStart = 1,
+  kEnd = 2,
+  kAll = 4,
+  kStartEnd = kStart | kEnd,
+};
+
+inline TextDecorationSkipSpaces operator|(TextDecorationSkipSpaces a,
+                                          TextDecorationSkipSpaces b) {
+  return static_cast<TextDecorationSkipSpaces>(static_cast<unsigned>(a) |
+                                               static_cast<unsigned>(b));
+}
+inline TextDecorationSkipSpaces& operator|=(TextDecorationSkipSpaces& a,
+                                            TextDecorationSkipSpaces b) {
+  return a = a | b;
+}
+inline TextDecorationSkipSpaces operator^(TextDecorationSkipSpaces a,
+                                          TextDecorationSkipSpaces b) {
+  return static_cast<TextDecorationSkipSpaces>(static_cast<unsigned>(a) ^
+                                               static_cast<unsigned>(b));
+}
+inline TextDecorationSkipSpaces& operator^=(TextDecorationSkipSpaces& a,
+                                            TextDecorationSkipSpaces b) {
+  return a = a ^ b;
+}
+inline TextDecorationSkipSpaces operator&(TextDecorationSkipSpaces a,
+                                          TextDecorationSkipSpaces b) {
+  return static_cast<TextDecorationSkipSpaces>(static_cast<unsigned>(a) &
+                                               static_cast<unsigned>(b));
+}
+inline TextDecorationSkipSpaces& operator&=(TextDecorationSkipSpaces& a,
+                                            TextDecorationSkipSpaces b) {
+  return a = a & b;
+}
+inline TextDecorationSkipSpaces operator~(TextDecorationSkipSpaces x) {
+  return static_cast<TextDecorationSkipSpaces>(~static_cast<unsigned>(x));
+}
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_STYLE_TEXT_DECORATION_SKIP_SPACES_H_
