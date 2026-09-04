f28444ccc3b58e9350350912ee2c8686e87e5f2a
Camillia Smith Barnes <cammie@chromium.org>
2026-09-04 09:13:11 -0700

    WebUILocationBar: Fix ContentSettingBubbleContentsInteractiveTest
    Fix ContentSettingBubbleContentsInteractiveTest with WebUILocationBar.
    
    Extend LocationBarTesting with GetContentSettingBubbleWidget method and
    implement it across LocationBarView and WebUILocationBar (via
    WebUIContentSettingImageControl). Update
    ContentSettingBubbleContentsInteractiveTest to interact with
    LocationBarTesting rather than directly accessing LocationBarView. In
    addition, refactor WebUIContentSettingImageControl::TestPressed() to
    reuse IsContentSettingImageVisible(). Also add a test for
    IsContentSettingImageVisible().
    
    TAG=agy
    CONV=cdaa0b19-b505-4501-ba50-4aa9425668ef
    
    Bug: 556227613
    Change-Id: Ibf85aee2726ee0f886063096831bfbd7716c6ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346129
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692506}

--- chrome/browser/ui/views/location_bar/webui_location_bar.cc
+++ chrome/browser/ui/views/location_bar/webui_location_bar.cc
@@ -705,6 +705,10 @@ bool WebUILocationBar::IsContentSettingImageVisible(size_t index) {
   return content_setting_image_control_.IsContentSettingImageVisible(index);
 }
 
+views::Widget* WebUILocationBar::GetContentSettingBubbleWidget(size_t index) {
+  return content_setting_image_control_.GetBubbleWidget(index);
+}
+
 void WebUILocationBar::OnLhsChipMousePressed(
     toolbar_ui_api::mojom::LhsChipIdentifier identifier,
     bool is_middle_click) {
