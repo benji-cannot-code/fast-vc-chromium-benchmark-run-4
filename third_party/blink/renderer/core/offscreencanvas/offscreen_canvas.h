f4ea88bb6e03541df10a7ad59d0df92a5b7e701a
Stefan Zager <szager@chromium.org>
2026-09-03 18:00:30 -0700

    [HiC] Implement geometry update events for offscreen canvas
    See: https://github.com/WICG/html-in-canvas/issues/135
    
    Bug: 532229486
    Change-Id: Ic3d0b4be42105dd50e34b26c6f018c99ddfc7b89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339821
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692090}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gfx/geometry/transform.h"
 
 namespace blink {
 
@@ -157,6 +158,19 @@ class CORE_EXPORT OffscreenCanvas final
   void ClearDrawnElementGeometry(Element&) override;
   void ClearDrawnElementGeometry(ElementImage&) override;
 
+  // A pending update created by a call to either updateElementGeometry or
+  // clearElementGeometry.
+  struct ElementGeometryUpdate {
+    DOMNodeId element_id;
+
+    // Fields populated by updateElementGeometry.
+    std::optional<gfx::Transform> transform;
+    bool update_hit_test_order = false;
+
+    // Set to `true` by clearElementGeometry.
+    bool clear_element_geometry = false;
+  };
+
   bool PushFrameIfNeeded();
   bool PushFrame(scoped_refptr<CanvasResource>&& frame);
   void DidDraw(const gfx::Rect&) override;
@@ -276,6 +290,13 @@ class CORE_EXPORT OffscreenCanvas final
   static ContextFactoryVector& RenderingContextFactories();
   static CanvasRenderingContextFactory* GetRenderingContextFactory(int);
 
+  void QueueUpdate(ElementGeometryUpdate update);
+  void QueueElementGeometryUpdate(DOMNodeId element_id,
+                                  const gfx::Transform* transform,
+                                  bool update_hit_test_order);
+  void QueueClearElementGeometry(DOMNodeId element_id);
+  void ProcessPendingElementGeometryUpdates();
+
   Member<CanvasRenderingContext> context_;
   WeakMember<ExecutionContext> execution_context_;
 
@@ -314,6 +335,8 @@ class CORE_EXPORT OffscreenCanvas final
   // then the following members would remain as initialized zero values.
   uint32_t client_id_ = 0;
   uint32_t sink_id_ = 0;
+
+  Vector<ElementGeometryUpdate> pending_element_geometry_updates_;
 };
 
 }  // namespace blink
--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_draw_element_image_options.h"
 #include "third_party/blink/renderer/core/css/properties/computed_style_utils.h"
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
+#include "third_party/blink/renderer/core/dom/dom_node_ids.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/node.h"
