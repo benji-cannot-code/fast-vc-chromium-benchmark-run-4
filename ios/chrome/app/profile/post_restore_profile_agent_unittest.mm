bea82c6ac4a62ea6668bb7ced8e7f1df5bc82e35
Jérôme Lebel <jlebel@chromium.org>
2026-09-04 01:53:08 -0700

    [iOS] Use TestProfileManagerIOS in unit tests
    Update unit tests to create and manage test profiles using
    TestProfileManagerIOS instead of building TestProfileIOS directly.
    
    When profiles are built standalone via TestProfileIOS::Builder, they are
    not registered with the ProfileManagerIOS in ApplicationContext. This
    causes issues with features that query profiles from the manager, such
    as identity mapping and filtering (preparing for crrev.com/c/8250990).
    
    This CL was uploaded by an experimental version of git cl split
    (https://crbug.com/389069356).
    
    R=marq@chromium.org
    
    - Replace std::unique_ptr<TestProfileIOS> with TestProfileManagerIOS
      and a raw_ptr<TestProfileIOS>.
    - Use TestProfileManagerIOS::AddProfileWithBuilder() to register the
      test profile.
    - Clean up raw pointers and dependent services/mediators in TearDown()
      to avoid dangling references upon profile destruction.
    - Simplify profile_.get() calls to profile_.
    
    Specifically: 
    Change-Id: Idb6fe8cf0e1f3ba07337054b4df288e3e868ae72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353384
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692278}

--- ios/chrome/app/profile/post_restore_profile_agent_unittest.mm
+++ ios/chrome/app/profile/post_restore_profile_agent_unittest.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/fake_authentication_service_delegate.h"
@@ -58,20 +59,28 @@ class PostRestoreProfileAgentTest : public PlatformTest {
                               base::BindRepeating(&CreateTestSyncService));
     builder.AddTestingFactory(PromosManagerFactory::GetInstance(),
                               base::BindOnce(&CreateMockPromosManager));
-    profile_ = std::move(builder).Build();
+    profile_ = profile_manager_.AddProfileWithBuilder(std::move(builder));
 
     promos_manager_ = static_cast<NiceMock<MockPromosManager>*>(
-        PromosManagerFactory::GetForProfile(profile_.get()));
-    auth_service_ = AuthenticationServiceFactory::GetForProfile(profile_.get());
+        PromosManagerFactory::GetForProfile(profile_));
+    auth_service_ = AuthenticationServiceFactory::GetForProfile(profile_);
 
     profile_state_ = [[ProfileState alloc] initWithAppState:nil];
-    profile_state_.profile = profile_.get();
+    profile_state_.profile = profile_;
 
     profile_agent_ = [[PostRestoreProfileAgent alloc] init];
     [profile_state_ addAgent:profile_agent_];
   }
 
-  ~PostRestoreProfileAgentTest() override { profile_state_.profile = nullptr; }
+  void TearDown() override {
+    profile_state_.profile = nullptr;
+    profile_agent_ = nil;
+    profile_state_ = nil;
+    auth_service_ = nullptr;
+    promos_manager_ = nullptr;
+    profile_ = nullptr;
+    PlatformTest::TearDown();
+  }
 
   void TriggerProfileStateChange() {
     [profile_agent_ profileState:profile_state_
@@ -102,12 +111,13 @@ class PostRestoreProfileAgentTest : public PlatformTest {
                           signin_metrics::AccessPoint::kStartPage);
   }
 
-  PrefService* pref_service() { return profile_.get()->GetPrefs(); }
+  PrefService* pref_service() { return profile_->GetPrefs(); }
 
  protected:
   IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   web::WebTaskEnvironment task_environment_;
-  std::unique_ptr<TestProfileIOS> profile_;
+  TestProfileManagerIOS profile_manager_;
+  raw_ptr<TestProfileIOS> profile_ = nullptr;
   raw_ptr<NiceMock<MockPromosManager>> promos_manager_;
   raw_ptr<AuthenticationService> auth_service_;
   PostRestoreProfileAgent* profile_agent_;
