1bed65b4ac27a5f4273f8297eb43014ece49d704
Ryo Hashimoto <hashimoto@google.com>
2026-09-03 22:20:41 -0700

    devtools: Detach devtools sessions on allowlist/blocklist update
    DevToolsWindow observes allowlist/blocklist policies to close windows
    when the policy is updated. However, this doesn't cover remote debugging
    sessions which don't have any DevToolsWindow on the debugged device.
    
    Observe these policies in ChromeDevToolsSessionBase to detach devtools
    sessions on policy update.
    
    BUG=467706550
    
    Change-Id: Ifbc20b3bae4d6206459570f62162bd534f4a1208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260256
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692197}

--- chrome/browser/devtools/chrome_devtools_session_base.cc
+++ chrome/browser/devtools/chrome_devtools_session_base.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/metrics_hashes.h"
 #include "chrome/browser/devtools/devtools_availability_checker.h"
+#include "chrome/browser/policy/developer_tools_policy_checker.h"
+#include "chrome/browser/policy/developer_tools_policy_checker_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_context.h"
@@ -43,6 +45,14 @@ ChromeDevToolsSessionBase::ChromeDevToolsSessionBase(
         prefs::kDevToolsAvailability,
         base::BindRepeating(&ChromeDevToolsSessionBase::OnDevToolsPolicyChanged,
                             base::Unretained(this)));
+    if (policy::DeveloperToolsPolicyChecker* checker =
+            policy::DeveloperToolsPolicyCheckerFactory::GetForBrowserContext(
+                profile)) {
+      policy_checker_callback_subscription_ =
+          checker->AddObserver(base::BindRepeating(
+              &ChromeDevToolsSessionBase::OnDevToolsPolicyChanged,
+              base::Unretained(this)));
+    }
   }
 }
 
--- chrome/browser/devtools/chrome_devtools_session_base.h
+++ chrome/browser/devtools/chrome_devtools_session_base.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback_list.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/devtools/protocol/protocol.h"
@@ -50,6 +51,7 @@ class ChromeDevToolsSessionBase : public protocol::FrontendChannel {
   protocol::UberDispatcher dispatcher_;
   raw_ptr<content::DevToolsAgentHostClientChannel> client_channel_;
   PrefChangeRegistrar pref_change_registrar_;
+  base::CallbackListSubscription policy_checker_callback_subscription_;
 };
 
 #endif  // CHROME_BROWSER_DEVTOOLS_CHROME_DEVTOOLS_SESSION_BASE_H_
