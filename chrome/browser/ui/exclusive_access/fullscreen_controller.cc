00671ddc4b254ee141e47e51ca0754a4111f2305
Mike Wasserman <msw@chromium.org>
2026-09-03 17:38:56 -0700

    Fullscreen: Ignore display ids without Window Management permission
    Adds an extra layer of protection for compromised renderers
    
    Fixed: 501896592
    Change-Id: Ic9986c3c1883f28308f6dbac9bc99141821eac25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8355032
    Reviewed-by: Vikram Pasupathy <vpasupathy@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692074}

--- chrome/browser/ui/exclusive_access/fullscreen_controller.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller.cc
@@ -605,6 +605,7 @@ void FullscreenController::EnterFullscreenModeInternal(
                       requesting_frame) !=
               blink::mojom::PermissionStatus::GRANTED) {
         display_id = display::kInvalidDisplayId;
+        fullscreen_tab_params.display_id = display::kInvalidDisplayId;
       } else if (entering_tab_fullscreen) {
         display_id_prior_to_tab_fullscreen_ = current_display;
       }
