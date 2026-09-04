70c35fea555a43e85ad116460425b9e00af5e6fa
Bryan Kim <bryankim@google.com>
2026-09-03 22:14:04 -0700

    [Bedrock] Modularize intent_picker_bubble_view
    Extract intent_picker_bubble_view into a separate source_set under
    //chrome/browser/ui/views.
    
    TAG=agy
    CONV=11ce5b45-b2b9-41b2-9bb7-cb275454bfa8
    
    Bug: 353332589
    Change-Id: I2cc657f8904299992dd90bf0a7f5a9b2ed5628c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8275791
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692193}

--- chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/global_media_controls/media_toolbar_button.h"
 #include "chrome/browser/ui/views/global_media_controls/media_toolbar_button_contextual_menu.h"
 #include "chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h"
+#include "chrome/browser/ui/views/intent_picker_bubble_view.h"
 #include "chrome/browser/ui/views/location_bar/webui_location_bar.h"
 #include "chrome/browser/ui/views/page_action/page_action_container_view.h"
 #include "chrome/browser/ui/views/page_action/page_action_view.h"
@@ -1245,10 +1246,10 @@ bool ToolbarView::GetAppMenuFocused() const {
 }
 
 void ToolbarView::ShowIntentPickerBubble(
-    std::vector<IntentPickerBubbleView::AppInfo> app_info,
+    std::vector<apps::IntentPickerAppInfo> app_info,
     bool show_stay_in_chrome,
     bool show_remember_selection,
-    IntentPickerBubbleView::BubbleType bubble_type,
+    apps::IntentPickerBubbleType bubble_type,
     const std::optional<url::Origin>& initiating_origin,
     IntentPickerResponse callback) {
   std::optional<ui::ElementIdentifier> higlighted_element;
