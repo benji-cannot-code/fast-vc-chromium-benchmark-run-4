ff3ebc9683b5356ea93b71d375c740ec2b81bfed
Boris Sazonov <bsazonov@chromium.org>
2026-09-04 06:41:35 -0700

    [Signin][Android] Use AccountInfo in SigninChecker
    Changes the type used in SigninChecker for the primary account info
    to AccountInfo.
    
    TAG=agy
    CONV=c9f6c8ce-2a1b-459f-a795-d5ad20e5c664
    
    Bug: 471185380
    Change-Id: I67e414701521f1f413ca8be402731693c49b2970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8345549
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692394}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/SigninChecker.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/SigninChecker.java
@@ -79,7 +79,7 @@ public class SigninChecker implements AccountsChangeObserver, Destroyable {
         assert childInfo != null;
         mSigninManager.runAfterOperationInProgress(
                 () -> {
-                    CoreAccountInfo accountInfo =
+                    AccountInfo accountInfo =
                             mSigninManager.getIdentityManager().getPrimaryAccountInfo();
 
                     if (accountInfo == null || childInfo.getId().equals(accountInfo.getId())) {
