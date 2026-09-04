b6bc7d200990bef0c090c1370ea36f86fd857105
Kalvin Lee <kdlee@chromium.org>
2026-09-04 01:05:42 -0700

    PA: Plumb bool showing if slot can store raw size
    In dealing with Checked Span slack space, for the cases where the
    `InSlotMetadata` would be stored out-of-line, we also don't want to
    smuggle the requested size in at the end-of-slot: we should prefer to
    (subtract extras size and) read the raw size directly.
    
    To set this bit properly, this CL adds plumbing to surface this
    information in the allocation path.
    
    Fuchsia-Binary-Size: Size increase is unavoidable.
    Bug: 534387022
    Change-Id: I83194f32dd13298c880b31971a6eb5010a525684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256815
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692251}

--- base/allocator/partition_allocator/src/partition_alloc/internal/partition_root_internal.h
+++ base/allocator/partition_allocator/src/partition_alloc/internal/partition_root_internal.h
@@ -328,7 +328,8 @@ PartitionRoot::AllocFromBucket(Bucket* bucket,
                                size_t slot_span_alignment,
                                size_t* usable_size,
                                size_t* slot_size,
-                               bool* is_already_zeroed) {
+                               bool* is_already_zeroed,
+                               bool* stored_raw_size) {
   PA_DCHECK((slot_span_alignment >= internal::PartitionPageSize()) &&
             std::has_single_bit(slot_span_alignment));
   SlotSpanMetadata* slot_span = bucket->active_slot_spans_head;
@@ -371,7 +372,7 @@ PartitionRoot::AllocFromBucket(Bucket* bucket,
     // Should check validity, but later in this function.
     slot_start = UntaggedSlotStart::Unchecked(
         bucket->SlowPathAlloc(this, flags, raw_size, slot_span_alignment,
-                              &slot_span, is_already_zeroed));
+                              &slot_span, is_already_zeroed, stored_raw_size));
     if (!slot_start) [[unlikely]] {
       return UntaggedSlotStart();
     }
@@ -1389,6 +1390,7 @@ PA_ALWAYS_INLINE void* PartitionRoot::AllocInternalNoHooks(
   bool is_already_zeroed = false;
   UntaggedSlotStart slot_start;
   size_t slot_size = 0;
+  bool stored_raw_size = false;
 
   auto* thread_cache = GetOrCreateThreadCache();
 
@@ -1425,14 +1427,15 @@ PA_ALWAYS_INLINE void* PartitionRoot::AllocInternalNoHooks(
       PA_DCHECK(!slot_span->bucket->is_direct_mapped());
 #endif
     } else {
-      slot_start = RawAlloc<flags>(PA_UNSAFE_TODO(buckets_ + bucket_index),
-                                   raw_size, slot_span_alignment, &usable_size,
-                                   &slot_size, &is_already_zeroed);
+      slot_start =
+          RawAlloc<flags>(PA_UNSAFE_TODO(buckets_ + bucket_index), raw_size,
+                          slot_span_alignment, &usable_size, &slot_size,
+                          &is_already_zeroed, &stored_raw_size);
     }
   } else {
-    slot_start = RawAlloc<flags>(PA_UNSAFE_TODO(buckets_ + bucket_index),
-                                 raw_size, slot_span_alignment, &usable_size,
-                                 &slot_size, &is_already_zeroed);
+    slot_start = RawAlloc<flags>(
+        PA_UNSAFE_TODO(buckets_ + bucket_index), raw_size, slot_span_alignment,
+        &usable_size, &slot_size, &is_already_zeroed, &stored_raw_size);
   }
 
   if (!slot_start.value()) [[unlikely]] {
@@ -1528,14 +1531,15 @@ PartitionRoot::RawAlloc(Bucket* bucket,
                         size_t slot_span_alignment,
                         size_t* usable_size,
                         size_t* slot_size,
-                        bool* is_already_zeroed) {
+                        bool* is_already_zeroed,
+                        bool* stored_raw_size) {
   UntaggedSlotStart slot_start;
   {
     ::partition_alloc::internal::ScopedGuard guard{
         internal::PartitionRootLock(this)};
-    slot_start =
-        AllocFromBucket<flags>(bucket, raw_size, slot_span_alignment,
-                               usable_size, slot_size, is_already_zeroed);
+    slot_start = AllocFromBucket<flags>(bucket, raw_size, slot_span_alignment,
+                                        usable_size, slot_size,
+                                        is_already_zeroed, stored_raw_size);
   }
 
   if (slot_start.value()) [[likely]] {
