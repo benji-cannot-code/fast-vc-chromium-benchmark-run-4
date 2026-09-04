ffaf851abba89b39be43ec2acde70cb449541110
Adithya Srinivasan <adithyas@chromium.org>
2026-09-04 14:26:30 -0700

    actor: Use ActorTask's UiEventDispatcher in ExecutionEngine
    Doing so allows us to:
    
    1) remove any knowledge of ActorUiStateManagerInterface from ActorTask
       and ExecutionEngine
    2) simplify some unit tests that had to previously create two separate
       mock UiEventDispatchers
    
    Bug: b:552950922
    Change-Id: I888e8eca1b61fec10334250115c3d52ec469c18b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354564
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692759}

--- chrome/browser/actor/execution_engine.cc
+++ chrome/browser/actor/execution_engine.cc
@@ -690,16 +690,10 @@ ExecutionEngine::GetFactoryFunctionForTesting() {
 
 // Protected constructor without pass key to allow subclassing.
 ExecutionEngine::ExecutionEngine(ActorTask& owner_task)
-    : ExecutionEngine(
-          base::PassKey<ExecutionEngine>(),
-          owner_task,
-          ui::NewUiEventDispatcher(
-              owner_task.actor_keyed_service().GetActorUiStateManager())) {}
+    : ExecutionEngine(base::PassKey<ExecutionEngine>(), owner_task) {}
 
-ExecutionEngine::ExecutionEngine(
-    base::PassKey<ExecutionEngine>,
-    ActorTask& owner_task,
-    std::unique_ptr<ui::UiEventDispatcher> ui_event_dispatcher)
+ExecutionEngine::ExecutionEngine(base::PassKey<ExecutionEngine>,
+                                 ActorTask& owner_task)
     : task_(owner_task),
       journal_(task_->actor_keyed_service().GetJournal().GetSafeRef()),
       tool_controller_(std::make_unique<ToolController>(*task_, *this)),
@@ -713,7 +707,6 @@ ExecutionEngine::ExecutionEngine(
               task_->GetProfile(),
               journal_,
               task_->id())),
-      ui_event_dispatcher_(std::move(ui_event_dispatcher)),
       origin_gating_checker_(
           *this,
           origin_gating::OriginGatingConfiguration(
@@ -789,24 +782,13 @@ ExecutionEngine::ExecutionEngine(
 
 // static
 std::unique_ptr<ExecutionEngine> ExecutionEngine::Create(
-    ActorTask& owner_task,
-    std::unique_ptr<ui::UiEventDispatcher> ui_event_dispatcher) {
+    ActorTask& owner_task) {
   if (!GetFactoryFunctionForTesting().is_null()) {
     return GetFactoryFunctionForTesting().Run(owner_task);
   }
 
   return std::make_unique<ExecutionEngine>(base::PassKey<ExecutionEngine>(),
-                                           owner_task,
-                                           std::move(ui_event_dispatcher));
-}
-
-// static
-std::unique_ptr<ExecutionEngine> ExecutionEngine::CreateForTesting(
-    ActorTask& owner_task,
-    std::unique_ptr<ui::UiEventDispatcher> ui_event_dispatcher) {
-  return std::make_unique<ExecutionEngine>(base::PassKey<ExecutionEngine>(),
-                                           owner_task,
-                                           std::move(ui_event_dispatcher));
+                                           owner_task);
 }
 
 ExecutionEngine::~ExecutionEngine() {
@@ -1511,7 +1493,7 @@ void ExecutionEngine::PostToolCreate(mojom::ActionResultPtr result) {
     return;
   }
   SetState(State::kUiPreInvoke);
-  ui_event_dispatcher_->OnPreTool(
+  GetUiEventDispatcher().OnPreTool(
       GetInProgressAction(),
       base::BindOnce(&ExecutionEngine::FinishedUiPreInvoke,
                      GetActionSequenceWeakPtr()));
@@ -1613,7 +1595,7 @@ void ExecutionEngine::FinishedToolInvoke(mojom::ActionResultPtr result) {
   }
 
   SetState(State::kUiPostInvoke);
-  ui_event_dispatcher_->OnPostTool(
+  GetUiEventDispatcher().OnPostTool(
       GetInProgressAction(),
       base::BindOnce(&ExecutionEngine::FinishedUiPostInvoke,
                      GetActionSequenceWeakPtr()));
@@ -1980,6 +1962,10 @@ size_t ExecutionEngine::GetResultIndexForAction(size_t action_index) const {
   CHECK_GT(original_count, 0ul);
 
   return original_count - 1;
+}
+
+ui::UiEventDispatcher& ExecutionEngine::GetUiEventDispatcher() {
+  return task_->ui_event_dispatcher();
 }
 
 std::ostream& operator<<(std::ostream& o, const ExecutionEngine::State& s) {
