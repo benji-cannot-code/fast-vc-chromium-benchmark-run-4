b70ce079c28992382fc11fe4b865b90fb0c9a11e
Eric Lok <lokeric@google.com>
2026-09-03 18:09:12 -0700

    [TabSharingToolbar] Rename TabSharingUI{Manager,Bridge} to Ui casing
    Rename TabSharingUIManager -> TabSharingUiManager and
    TabSharingUIBridge -> TabSharingUiBridge (and their unit tests) to follow
    Chromium's Java acronym-casing convention (Ui, not UI) and match the
    sibling TabSharingToolbarUiCoordinator.
    
    This is a purely mechanical rename with no behavior change:
    - Renames the Java source/test files and their classes.
    - Updates all Java references.
    - Updates the JNI glue in tab_sharing_ui_android.cc/.h (DEFINE_JNI, the
      Java_TabSharingUiBridge_* calls, and the generated *_jni.h include).
    - Updates the GN source lists.
    - Removes the TODO added in the (2/4) CL that tracked this rename.
    
    Bug: 546411766
    Test: tools/autotest.py -C out/android_x64 TabSharingUiBridgeTest TabSharingUiManagerTest TabSharingToolbarUiCoordinatorTest
    TAG=agy
    CONV=036c6b1c-7501-4fa4-b6f6-3718e96486a2
    
    Change-Id: I2e0d53fd7821860c85f93015d8205d9bae4b8e75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256790
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Eric Lok <lokeric@google.com>
    Auto-Submit: Eric Lok <lokeric@google.com>
    Reviewed-by: Phil Yan <philyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692092}

--- chrome/browser/media/android/tab_sharing_ui_android.cc
+++ chrome/browser/media/android/tab_sharing_ui_android.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/mediastream/media_stream.mojom.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/android/chrome_jni_headers/TabSharingUIBridge_jni.h"
+#include "chrome/android/chrome_jni_headers/TabSharingUiBridge_jni.h"
 
 namespace {
 // The MediaStreamUI interface specifies returning 0 if no window ID is
@@ -45,7 +45,7 @@ TabSharingUIAndroid::~TabSharingUIAndroid() {
   StopSharing();
   if (java_bridge_) {
     JNIEnv* env = base::android::AttachCurrentThread();
-    Java_TabSharingUIBridge_destroy(env, java_bridge_);
+    Java_TabSharingUiBridge_destroy(env, java_bridge_);
     java_bridge_.Reset();
   }
 }
@@ -125,7 +125,7 @@ gfx::NativeViewId TabSharingUIAndroid::OnStarted(
         base::FeatureList::IsEnabled(
             chrome::android::kTabSharingToolbarAndroid)) {
       JNIEnv* env = base::android::AttachCurrentThread();
-      java_bridge_ = Java_TabSharingUIBridge_create(
+      java_bridge_ = Java_TabSharingUiBridge_create(
           env, reinterpret_cast<intptr_t>(this), capturer_web_contents_.get(),
           web_contents, !source_callback_.is_null(),
           app_preferred_current_tab_);
@@ -137,4 +137,4 @@ gfx::NativeViewId TabSharingUIAndroid::OnStarted(
   return kNoWindowId;
 }
 
-DEFINE_JNI(TabSharingUIBridge)
+DEFINE_JNI(TabSharingUiBridge)
