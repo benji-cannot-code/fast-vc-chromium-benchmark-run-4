29208e7b073962be1405b8a204289002bace07a4
Friedrich Hauser <friedrichh@chromium.org>
2026-09-04 08:53:25 -0700

    Revert "[Autofill] Remove kAndroidAutofillFieldsUpdatedOnSelect feature flag"
    This reverts commit 1d89abd29b790739a2a522b42c0607c6a38a16a9.
    
    Reason for revert: Needed to prevent https://crbug.com/552440317
    
    Original change's description:
    > [Autofill] Remove kAndroidAutofillFieldsUpdatedOnSelect feature flag
    >
    > kAndroidAutofillFieldsUpdatedOnSelect has been enabled by default.
    > This CL cleans up the flag and associated code, calling UpdateCurrentField
    > unconditionally when a select control changes, and cleans up the
    > ProductionSupportedFlagList entry and test coverage.
    >
    > TAG=agy
    > CONV=78083bba-06f2-4d9e-b724-abaa4ff6d61e
    >
    > Bug: 502346855
    > Change-Id: Ia1164579514d3dff9c5802166237252ca7486bd9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8352724
    > Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1692439}
    
    Bug: 502346855, 552440317
    Bug: 502346855
    Change-Id: I14d105f98f6d73d26f5f7c7f1efdbbaa33fedc51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8350228
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Friedrich Hauser <friedrichh@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1692485}

--- components/android_autofill/browser/android_autofill_provider.cc
+++ components/android_autofill/browser/android_autofill_provider.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "components/android_autofill/browser/android_autofill_bridge_factory.h"
+#include "components/android_autofill/browser/android_autofill_features.h"
 #include "components/android_autofill/browser/android_autofill_manager.h"
 #include "components/android_autofill/browser/android_autofill_provider_bridge.h"
 #include "components/android_autofill/browser/form_data_android.h"
@@ -529,7 +530,10 @@ void AndroidAutofillProvider::OnSelectControlSelectionChanged(
     // crbug.com/518115316).
     return;
   }
-  UpdateCurrentField(manager, form, field);
+  if (base::FeatureList::IsEnabled(
+          features::kAndroidAutofillFieldsUpdatedOnSelect)) {
+    UpdateCurrentField(manager, form, field);
+  }
   if (!IsLinkedForm(form)) {
     StartNewSession(manager, form, field);
     // TODO(crbug.com/40929724): Return early at this point?
