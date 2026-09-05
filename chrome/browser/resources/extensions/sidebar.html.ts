94e4bf70cc541be1528095ab6ebe6f8385a15a1f
rbpotter <rbpotter@chromium.org>
2026-09-04 17:56:38 -0700

    Lit template formatter: Improve line length wrapping
    (1) Updating line wrapping to force multiline expressions to dedicated
        lines. This is done to improve consistency when formatting complex
        expressions, where indenting relative to the start of the
        attribute is not always as readable (see updated files). To avoid
        excessive newlines, keep the start of such expressions on the same
        line with the attribute name unless the attribute name is on the
        first opening tag line.
    (2) Fixing some line wrapping edge cases, where single line tags would
        exceed the line limit by 1 since the formatter didn't account for
        the closing ">" fitting on the line.
    (3) Fixing text node cases by checking for newlines and non-empty
        text, so that we can have e.g.
        <div id="something">
          Some text
        </div>
        and also respect cases where developers deliberately prevent
        whitespace, e.g.
        <span id="foo"
            class="hello">Hello</span>
    (4) Correctly re-escape "<" ">" characters in text nodes.
    
    Bug: 512911634
    Change-Id: I2b0d6c5b9cf55d2ede6dc1b3b6dd30815efb8188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8341648
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692869}

--- chrome/browser/resources/extensions/sidebar.html.ts
+++ chrome/browser/resources/extensions/sidebar.html.ts
@@ -16,9 +16,10 @@ export function getHtml(this: ExtensionsSidebarElement) {
   <a role="menuitem" class="cr-nav-menu-item" id="sectionsExtensions" href="/"
       @click="${this.onLinkClick_}" data-path="itemsList">
     <cr-icon
-        icon="${this.webuiRoundedIconsEnabled_ ?
-            'extensions-icons:chrome-extension' :
-            'extensions-icons:my_extensions-old'}">
+        icon="${
+            this.webuiRoundedIconsEnabled_ ?
+                'extensions-icons:chrome-extension' :
+                'extensions-icons:my_extensions-old'}">
     </cr-icon>
     $i18n{sidebarExtensions}
     <cr-ripple></cr-ripple>
@@ -27,9 +28,10 @@ export function getHtml(this: ExtensionsSidebarElement) {
       ?hidden="${!this.enableEnhancedSiteControls}" href="/sitePermissions"
       @click="${this.onLinkClick_}" data-path="site-permissions">
     <cr-icon
-        icon="${this.webuiRoundedIconsEnabled_ ?
-            'extensions-icons:tune' :
-            'extensions-icons:site_permissions-old'}">
+        icon="${
+            this.webuiRoundedIconsEnabled_ ?
+                'extensions-icons:tune' :
+                'extensions-icons:site_permissions-old'}">
     </cr-icon>
     $i18n{sitePermissions}
     <cr-ripple></cr-ripple>
@@ -38,9 +40,10 @@ export function getHtml(this: ExtensionsSidebarElement) {
       href="/shortcuts" @click="${this.onLinkClick_}"
       data-path="keyboard-shortcuts">
     <cr-icon
-        icon="${this.webuiRoundedIconsEnabled_ ?
-            'extensions-icons:keyboard' :
-            'extensions-icons:keyboard_shortcuts-old'}">
+        icon="${
+            this.webuiRoundedIconsEnabled_ ?
+                'extensions-icons:keyboard' :
+                'extensions-icons:keyboard_shortcuts-old'}">
     </cr-icon>
     $i18n{keyboardShortcuts}
     <cr-ripple></cr-ripple>
@@ -53,7 +56,7 @@ ${this.inDevMode ? html`
         .innerHTML="${this.computeModernWebGuidancePromoText_()}">
     </span>
   </div>
-`: ''}
+` : ''}
 <div class="separator"></div>
 <div class="cr-nav-menu-item" id="moreExtensions">
   <cr-icon id="web-store-icon" icon="extensions-icons:web_store-custom">
--- ui/webui/resources/tools/tests/lit_template_formatter/test_ternary_column_limit.html.ts
+++ ui/webui/resources/tools/tests/lit_template_formatter/test_ternary_column_limit.html.ts
@@ -6,8 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
 export function getHtml(this: DummyTestElement) {
-  // clang-format off
-  return html`
-<dummy-button id="${this.buttonId}" class="${this.buttonClass}" aria-label="${this.buttonLabel}" @click="${this.onClick}">${this.buttonText}</dummy-button>`;
-  // clang-format on
+  return html`<!--_html_template_start_-->
+<div>
+  <div>
+    ${this.isFirstVeryLongConditionActive() && this.isSecondVeryLongConditionActive() ? html`
+      <div>Sample content</div>
+    ` : ''}
+  </div>
+</div>
+<!--_html_template_end_-->`;
 }
