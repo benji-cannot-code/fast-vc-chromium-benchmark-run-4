e8c75f503b95fc547a0ed7de534b68b7108d4d15
Kelsen Liu <kelsenliu@chromium.org>
2026-09-07 04:35:42 -0700

    Spanify CommonDecoder::Bucket::SetData
    Replace the raw data pointer and size with a byte span and migrate
    callers.
    
    Bug: 40285824
    Change-Id: I18f6508e55ce4eb97825abf16e35478dc79c83af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346907
    Commit-Queue: Kelsen Liu <kelsenliu@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693269}

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough_handlers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough_handlers.cc
@@ -1452,7 +1452,7 @@ error::Error GLES2DecoderPassthroughImpl::HandleGetProgramInfoCHROMIUM(
   }
 
   bucket->SetSize(data.size());
-  bucket->SetData(data.data(), 0, data.size());
+  bucket->SetData(data, 0);
 
   return error::kNoError;
 }
@@ -1479,7 +1479,7 @@ error::Error GLES2DecoderPassthroughImpl::HandleGetUniformBlocksCHROMIUM(
   }
 
   bucket->SetSize(data.size());
-  bucket->SetData(data.data(), 0, data.size());
+  bucket->SetData(data, 0);
 
   return error::kNoError;
 }
@@ -1508,7 +1508,7 @@ GLES2DecoderPassthroughImpl::HandleGetTransformFeedbackVaryingsCHROMIUM(
   }
 
   bucket->SetSize(data.size());
-  bucket->SetData(data.data(), 0, data.size());
+  bucket->SetData(data, 0);
 
   return error::kNoError;
 }
@@ -1535,7 +1535,7 @@ error::Error GLES2DecoderPassthroughImpl::HandleGetUniformsES3CHROMIUM(
   }
 
   bucket->SetSize(data.size());
-  bucket->SetData(data.data(), 0, data.size());
+  bucket->SetData(data, 0);
 
   return error::kNoError;
 }
