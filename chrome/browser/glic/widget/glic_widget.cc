5321fe6a19afed97b7851c60be34a33601d691e5
Andrea Gomez <andreaxg@google.com>
2026-09-07 11:19:25 -0700

    [glic] Remove the glic-z-order-changes flag
    Removes the glic-z-order-changes feature flag, expired at M144, along
    with the code guarded by it in GlicWidget.
    
    TAG=agy
    CONV=977a464f-dd94-462b-bb7c-c863f82d0f4c
    
    Bug: 455810285
    Change-Id: I4aa6cebb43257e57a8e3eb6fe83076a2037459df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346311
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Auto-Submit: Andrea Gomez <andreaxg@google.com>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693418}

--- chrome/browser/glic/widget/glic_widget.cc
+++ chrome/browser/glic/widget/glic_widget.cc
@@ -44,8 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_WIN)
-#include "chrome/installer/util/install_util.h"
-#include "chrome/installer/util/shell_util.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/base/win/hwnd_metrics.h"
@@ -431,11 +429,7 @@ std::unique_ptr<GlicWidget> GlicWidget::Create(views::WidgetDelegate* delegate,
   }
 #endif  // BUILDFLAG(IS_OZONE)
 #if BUILDFLAG(IS_WIN)
-  // If floaty won't be always on top, it should appear in the taskbar and
-  // alt tab list.
-  if (!base::FeatureList::IsEnabled(features::kGlicZOrderChanges)) {
-    params.dont_show_in_taskbar = true;
-  }
+  params.dont_show_in_taskbar = true;
 #endif  // BUILDFLAG(IS_WIN)
 #if BUILDFLAG(IS_MAC)
   params.animation_enabled = true;
@@ -469,10 +463,6 @@ std::unique_ptr<GlicWidget> GlicWidget::Create(views::WidgetDelegate* delegate,
   HWND hwnd = widget->GetNativeWindow()->GetHost()->GetAcceleratedWidget();
   if (hwnd != nullptr) {
     ui::win::PreventWindowFromPinning(hwnd);
-    if (base::FeatureList::IsEnabled(features::kGlicZOrderChanges)) {
-      ui::win::SetAppIdForWindow(
-          ShellUtil::GetBrowserModelId(InstallUtil::IsPerUserInstall()), hwnd);
-    }
   }
 #endif  // BUILDFLAG(IS_WIN)
   return widget;
