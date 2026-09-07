e40844a546060abd8b1fc841b1edd7cdd9b16473
Julia Przybytniowska <przybytniowska@google.com>
2026-09-07 00:09:53 -0700

    [Isolated Mode] Fallback to standard NTP in Enterprise Mode
    Ensures that the Split View NTP is not used when in Enterprise Isolated
    Mode, matching the behavior of Incognito mode.
    
    BUG=b:556978954
    
    DD: go/chrome-enterprise-isolated-mode-client-dd
    Change-Id: Ia617b50cc3a80473a0aa479c19984984290ecc30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8351824
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Reviewed-by: Viacheslav Krakhotin <krakhotin@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693162}

--- chrome/browser/ui/browser_commands_browsertest.cc
+++ chrome/browser/ui/browser_commands_browsertest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tab_search_bubble_host.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/commerce/core/pref_names.h"
@@ -990,6 +991,22 @@ IN_PROC_BROWSER_TEST_F(BrowserCommandsIsolatedModeTest,
       isolated_browser->GetTabStripModel()->GetActiveTab();
   ASSERT_NE(isolated_tab, nullptr);
   EXPECT_EQ(isolated_tab->GetTabFeatures()->commerce_ui_tab_helper(), nullptr);
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserCommandsIsolatedModeTest,
+                       NewSplitTab_URLFallback) {
+  BrowserWindowInterface* isolated_browser = CreateIncognitoBrowser();
+  ASSERT_TRUE(isolated_browser);
+  EXPECT_TRUE(
+      isolated_browser->GetProfile()->IsEnterpriseIsolatedModeProfile());
+
+  AddBlankTabAndShow(isolated_browser);
+  EXPECT_EQ(2, isolated_browser->GetTabStripModel()->count());
+  NewSplitTab(isolated_browser, split_tabs::SplitTabLayout::kSideBySide,
+              split_tabs::SplitTabCreatedSource::kToolbarButton);
+  EXPECT_EQ(ChromeUINewTabURLAsGURL(), isolated_browser->GetTabStripModel()
+                                           ->GetActiveWebContents()
+                                           ->GetVisibleURL());
 }
 
 }  // namespace chrome
