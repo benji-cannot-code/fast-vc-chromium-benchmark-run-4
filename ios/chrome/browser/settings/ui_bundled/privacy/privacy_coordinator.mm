1fe9fbcf44e3971fc9532d35a01e46fa762b8c4a
Azamat Myrzabekov <azamatmz@google.com>
2026-09-07 06:36:48 -0700

    Add Universal Opt Out settings to IOS
    This change introduces a new setting on IOS that allows to toggle
    the universal opt-out preference.
    Screenshot: http://b/522291279#comment2
    
    Bug: b:522291279
    Change-Id: Ic0ac97cff40558f9fcd3d627c95bda9f9708eff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8344729
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Azamat Myrzabekov <azamatmz@google.com>
    Auto-Submit: Azamat Myrzabekov <azamatmz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693319}

--- ios/chrome/browser/settings/ui_bundled/privacy/privacy_coordinator.mm
+++ ios/chrome/browser/settings/ui_bundled/privacy/privacy_coordinator.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/privacy/privacy_navigation_commands.h"
 #import "ios/chrome/browser/settings/ui_bundled/privacy/privacy_safe_browsing_coordinator.h"
 #import "ios/chrome/browser/settings/ui_bundled/privacy/privacy_table_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/privacy/universal_opt_out_table_view_controller.h"
 #import "ios/chrome/browser/settings/ui_bundled/settings_navigation_controller.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
@@ -137,6 +138,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 #pragma mark - PrivacyNavigationCommands
+
+- (void)showUniversalOptOut {
+  UniversalOptOutTableViewController* viewController =
+      [[UniversalOptOutTableViewController alloc] initWithProfile:self.profile];
+  [self.viewController configureHandlersForRootViewController:viewController];
+  [self.baseNavigationController pushViewController:viewController
+                                           animated:YES];
+}
 
 - (void)showHandoff {
   HandoffTableViewController* viewController =
--- ios/chrome/browser/settings/ui_bundled/privacy/privacy_navigation_commands.h
+++ ios/chrome/browser/settings/ui_bundled/privacy/privacy_navigation_commands.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // controller.
 @protocol PrivacyNavigationCommands
 
+// Shows Universal Opt Out screen.
+- (void)showUniversalOptOut;
+
 // Shows Handoff screen.
 - (void)showHandoff;
 
