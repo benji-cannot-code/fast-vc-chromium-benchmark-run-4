8a6258590ab4ff43d845bcd22f3beecf8e5d4b72
Sylvain Defresne <sdefresne@chromium.org>
2026-09-07 06:27:40 -0700

    [ios] Remove kOmahaResyncTimerOnForeground feature
    The feature has been launched since https://crrev.com/c/6164047
    (from January 2025) so remove the feature flag.
    
    Bug: 555698908
    Change-Id: Ic006c4382b4da22022e156b88e392dc2e591d04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354063
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693316}

--- ios/chrome/browser/omaha/model/omaha_service.mm
+++ ios/chrome/browser/omaha/model/omaha_service.mm
@@ -548,8 +548,7 @@ void OmahaService::SendOrScheduleNextPing() {
         base::BindOnce(&OmahaService::SendPing, base::Unretained(this)));
     // Once the timer is started, register for
     // applicationWillEnterForeground notifications.
-    if (!foreground_notification_registration_handle_ &&
-        base::FeatureList::IsEnabled(kOmahaResyncTimerOnForeground)) {
+    if (!foreground_notification_registration_handle_) {
       foreground_notification_registration_handle_ =
           [[NSNotificationCenter defaultCenter]
               addObserverForName:@"UIApplicationWillEnterForegroundNotification"
@@ -570,8 +569,6 @@ void OmahaService::SendOrScheduleNextPing() {
 // the expected deadline.
 void OmahaService::ResyncTimerIfNeeded() {
   DCHECK_CURRENTLY_ON(web::WebThread::IO);
-  CHECK(base::FeatureList::IsEnabled(kOmahaResyncTimerOnForeground));
-
   // If the timer isn't already running, nothing needs to be done.
   if (!timer_.IsRunning()) {
     return;
--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -340,9 +340,6 @@ bool IsBlueDotOnToolsMenuButtoneEnabled();
 // Killswitch for the reauth-first step in AuthenticationFlowInProfile.
 BASE_DECLARE_FEATURE(kAuthenticationFlowReauthFirstKillswitch);
 
-// Feature to control resyncing the omaha ping timer on foregrounding.
-BASE_DECLARE_FEATURE(kOmahaResyncTimerOnForeground);
-
 // Feature flag to enable Reactivation Notifications.
 BASE_DECLARE_FEATURE(kIOSReactivationNotifications);
 
