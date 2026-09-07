7ca5d1588e31190068b0b07981f0b56edd2dd98c
Filipa Senra <fsenra@google.com>
2026-09-07 05:37:27 -0700

    [iOS[QD] Remove pref migration and set default time period to 15 minutes
    This CL removes the temporary MaybeMigrateToQuickDeletePrefValues
    migration function and its associated unit tests. Most users have been
    migrated 97% and only saw a change of 2% over the last year. The default
    value for the kDeleteTimePeriod preference is now directly set to 15
    minutes (LAST_15_MINUTES) in the preference registry.
    
    Fixed: 453663079, 471197613
    Change-Id: I6479663f99f8465796c9a0d13db4c4cf192e7169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8351584
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693290}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -252,6 +252,10 @@ constexpr char kInvalidationPerSenderRegisteredForInvalidation[] =
 constexpr char kInvalidationPerSenderActiveRegistrationTokens[] =
     "invalidation.per_sender_active_registration_tokens";
 
+// Deprecated 09/2026.
+inline constexpr char kMigratedToQuickDeletePrefValues[] =
+    "browser.migrated_to_quick_delete_pref_values";
+
 // Renames a boolean pref within a PrefService.
 void RenameBooleanPref(std::string_view target_pref_name,
                        std::string_view source_pref_name,
@@ -1019,6 +1023,9 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
       kInvalidationPerSenderRegisteredForInvalidation);
   registry->RegisterDictionaryPref(
       kInvalidationPerSenderActiveRegistrationTokens);
+
+  // Deprecated 09/2026.
+  registry->RegisterBooleanPref(kMigratedToQuickDeletePrefValues, false);
 }
 
 // This method should be periodically pruned of year+ old migrations.
@@ -1059,9 +1066,6 @@ void MigrateObsoleteProfilePrefs(PrefService* prefs) {
 
   // Check MigrateDeprecatedAutofillPrefs() to see if this is safe to remove.
   autofill::prefs::MigrateDeprecatedAutofillPrefs(prefs);
-
-  // Added 09/2024.
-  browsing_data::prefs::MaybeMigrateToQuickDeletePrefValues(prefs);
 
   // Added 10/2025.
   prefs->ClearPref(kSessionStorageFormatPref);
@@ -1128,6 +1132,9 @@ void MigrateObsoleteProfilePrefs(PrefService* prefs) {
   // Added 09/2026.
   prefs->ClearPref(kInvalidationPerSenderRegisteredForInvalidation);
   prefs->ClearPref(kInvalidationPerSenderActiveRegistrationTokens);
+
+  // Deprecated 09/2026.
+  prefs->ClearPref(kMigratedToQuickDeletePrefValues);
 }
 
 void MigrateObsoleteUserDefault() {
