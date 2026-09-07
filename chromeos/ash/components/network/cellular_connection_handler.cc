ee4a9fc09838141a6ab248d3c3d31ccc4b896145
rajeshhs <rajeshhs@google.com>
2026-09-06 23:47:21 -0700

    [CrOS Cellular] Increase of kWaitingForConnectableTimeout from 30s to 60s
    CellularConnectionHandler will wait for a cellular network to become
    "connectable" after an operation (like enabling an eSIM profile or
    checking a pSIM network's status).
    If the network does not become connectable within this duration,
    a timeout error is triggered.
    
    Bug: 467917050
    TEST=cellularui.ESimInstall,
    cellularui.ESimInstallBadActivationCode and
    cellularui.ESimInstallWithConfirmationCode
    
    Change-Id: I1e013a39c510c5d14b2bfb79b280a56d751d1b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8138699
    Reviewed-by: Chad Duffin <chadduffin@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Rajesh S (xWF) <rajeshhs@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693152}

--- chromeos/ash/components/network/cellular_connection_handler.cc
+++ chromeos/ash/components/network/cellular_connection_handler.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-constexpr base::TimeDelta kWaitingForConnectableTimeout = base::Seconds(30);
+constexpr base::TimeDelta kWaitingForConnectableTimeout = base::Seconds(60);
 
 bool CanInitiateShillConnection(const NetworkState* network) {
   // The network must be part of a Shill profile (i.e., it cannot be a "stub"
