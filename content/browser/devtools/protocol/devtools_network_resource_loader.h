ca6b001b69016f057a78e92eff5ba30cf9963ed0
Samiya Caur <samiyac@chromium.org>
2026-09-08 04:54:03 -0700

    Enforce CSP on redirects in loadNetworkResource
    Fixed: 514456975
    Change-Id: I5c880abb89bedd2d3190de7afab42482dd242a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8322187
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Samiya Caur <samiyac@chromium.org>
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693703}

--- content/browser/devtools/protocol/devtools_network_resource_loader.h
+++ content/browser/devtools/protocol/devtools_network_resource_loader.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string_view>
 
+#include "base/functional/callback.h"
 #include "content/common/content_export.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "net/url_request/redirect_info.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/cpp/simple_url_loader_stream_consumer.h"
@@ -34,6 +36,8 @@ class CONTENT_EXPORT DevToolsNetworkResourceLoader
                               bool success,
                               int net_error,
                               std::string content)>;
+  using RedirectCheckCallback =
+      base::RepeatingCallback<bool(const net::RedirectInfo& redirect_info)>;
   enum class Caching { kBypass, kDefault };
   enum class Credentials { kInclude, kSameSite };
 
@@ -49,7 +53,8 @@ class CONTENT_EXPORT DevToolsNetworkResourceLoader
       Caching caching,
       Credentials include_credentials,
       CompletionCallback complete_callback,
-      bool is_outermost_main_frame = false);
+      bool is_outermost_main_frame = false,
+      RedirectCheckCallback redirect_check_callback = {});
 
   ~DevToolsNetworkResourceLoader() override;
 
@@ -63,7 +68,8 @@ class CONTENT_EXPORT DevToolsNetworkResourceLoader
       network::ResourceRequest resource_request,
       const net::NetworkTrafficAnnotationTag& traffic_annotation,
       mojo::Remote<network::mojom::URLLoaderFactory> url_loader_factory,
-      CompletionCallback complete_callback);
+      CompletionCallback complete_callback,
+      RedirectCheckCallback redirect_check_callback);
   void DownloadAsStream();
 
   void OnDataReceived(std::string_view chunk,
@@ -73,11 +79,17 @@ class CONTENT_EXPORT DevToolsNetworkResourceLoader
 
   void OnRetry(base::OnceClosure start_retry) override;
 
+  void OnRedirect(const GURL& url_before_redirect,
+                  const net::RedirectInfo& redirect_info,
+                  const network::mojom::URLResponseHead& response_head,
+                  std::vector<std::string>* removed_headers);
+
   const network::ResourceRequest resource_request_;
   const net::NetworkTrafficAnnotationTag traffic_annotation_;
   std::unique_ptr<network::SimpleURLLoader> loader_;
   mojo::Remote<network::mojom::URLLoaderFactory> url_loader_factory_;
   CompletionCallback completion_callback_;
+  RedirectCheckCallback redirect_check_callback_;
   std::string content_;
 };
 
--- third_party/blink/web_tests/http/tests/inspector-protocol/network/load-network-resource-csp.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/load-network-resource-csp.js
@@ -28,5 +28,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       sameOriginUrl,
       `Response for fetch with same-origin resource (should be allowed by CSP):`);
 
+  const redirectSameOriginUrl =
+      `http://127.0.0.1:8000/resources/redirect.php?url=http://127.0.0.1:8000/inspector-protocol/network/resources/source.map`;
+  await loadResource(
+      redirectSameOriginUrl,
+      `Response for fetch with redirect to same-origin resource (should be allowed by CSP):`);
+
+  const redirectCrossOriginUrl =
+      `http://127.0.0.1:8000/resources/redirect.php?url=https://localhost:8443/inspector-protocol/network/resources/source.map`;
+  await loadResource(
+      redirectCrossOriginUrl,
+      `Response for fetch with redirect to cross-origin resource (should be blocked by CSP):`);
+
   testRunner.completeTest();
 })
