b7be3c80027c56e7d0b9dd9a9cbe6aa1b260e5d6
Yi Gu <yigu@chromium.org>
2026-09-08 10:34:03 -0700

    [DC] Fix UAF in in DigitalIdentityProviderDesktop::OnCableEvent
    Fixed: 557206809
    Change-Id: If7528c8202aa2b65d83ea08ad55d84b9b7ec9448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8370383
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693894}

--- chrome/browser/digital_credentials/digital_identity_provider_desktop.h
+++ chrome/browser/digital_credentials/digital_identity_provider_desktop.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DIGITAL_CREDENTIALS_DIGITAL_IDENTITY_PROVIDER_DESKTOP_H_
 #define CHROME_BROWSER_DIGITAL_CREDENTIALS_DIGITAL_IDENTITY_PROVIDER_DESKTOP_H_
 
+#include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "content/public/browser/cross_device_request_info.h"
@@ -83,6 +84,9 @@ class DigitalIdentityProviderDesktop : public content::DigitalIdentityProvider {
       content::DigitalIdentityProvider::RequestStatusForMetrics);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(DigitalIdentityMultiStepDialogBrowserTest,
+                           OnCableEventOwnerDestroyedDuringDialogUpdate);
+
   // Called whenever some significant event occurs during the transaction.
   void OnEvent(const std::string& qr_url,
                RequestInfo::RequestType request_type,
