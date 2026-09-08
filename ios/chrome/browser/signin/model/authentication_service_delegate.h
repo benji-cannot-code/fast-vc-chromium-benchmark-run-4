f7f37b4b726b49efd1dd0d5b38dc26c0dc80b711
Marc Treib <treib@chromium.org>
2026-09-08 01:43:52 -0700

    [iOS] Clean up AuthenticationServiceDelegate methods and implementation
    After previous cleanups, both its methods (ClearBrowsingData() and
    ClearBrowsingDataForSignedinPeriod()) were never called. They are
    removed in this CL. AuthenticationServiceDelegateImpl is also removed.
    
    The AuthenticationServiceDelegate interface itself is now empty and thus
    pointless, but it still exists for now because it is referenced in many
    tests. Those will be cleaned up in separate followup CLs.
    
    Bug: 407498240
    Change-Id: I535df91526c3783b4a8f0ebb79f2bc9c6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8366966
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693628}

--- ios/chrome/browser/signin/model/authentication_service_delegate.h
+++ ios/chrome/browser/signin/model/authentication_service_delegate.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SIGNIN_MODEL_AUTHENTICATION_SERVICE_DELEGATE_H_
 #define IOS_CHROME_BROWSER_SIGNIN_MODEL_AUTHENTICATION_SERVICE_DELEGATE_H_
 
-#import "base/functional/callback.h"
-
 // Delegate for AuthenticationService.
 class AuthenticationServiceDelegate {
  public:
@@ -18,20 +16,6 @@ class AuthenticationServiceDelegate {
       const AuthenticationServiceDelegate&) = delete;
 
   virtual ~AuthenticationServiceDelegate() = default;
-
-  // Invoked by AuthenticationService after the user has signed out. All the
-  // local browsing data must be cleared out. `completion` is then called
-  // asynchronously.
-  virtual void ClearBrowsingData(base::OnceClosure completion) = 0;
-  virtual void ClearBrowsingDataForSignedinPeriod(
-      base::OnceClosure completion) = 0;
-
- private:
-  friend class AuthenticationServiceTest;
-  friend class FakeAuthenticationServiceDelegate;
-
-  int clear_browsing_data_counter_ = 0;
-  int clear_browsing_data_from_signin_counter_ = 0;
 };
 
 #endif  // IOS_CHROME_BROWSER_SIGNIN_MODEL_AUTHENTICATION_SERVICE_DELEGATE_H_
