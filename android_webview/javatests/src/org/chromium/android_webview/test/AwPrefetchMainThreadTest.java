f17f7d30ca397c9e794b6c83e2b4e6ab0ebf1bc7
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-09-08 15:28:38 -0700

    Structurize AwPrefetchTest parameterization by trigger/feature
    Originally, the common tests applicable for multiple scenarios/feature
    flag configurations are manually copy-pasted in `AwPrefetchTest` with
    different test name suffixes (listed below).
    
    After this CL, `AwPrefetchTest` is split:
    
    - Abstract parent classes `AwPrefetchTestBase` and
      `AwPrefetchOMTTestBase` are introduced to place the test body of the
      common tests (for all scenarios or only for OMT scenarios,
      respectively).
    - Concrete subclasses are introduced to run the tests with
      corresponding suffixes. They are mostly empty (just to set different
      feature flags and `runOnWorkerThread` param).
      - `AwPrefetchMainThreadTest` for `_UIThread()`
      - `AwPrefetchOmtDisabledTest` for `_WorkerThread_OMTDisabled()`
      - `AwPrefetchOmtTest` for `_WorkerThread_OMTEnabled()`
      - `AwPrefetchOmtCheckWculfTest` for
        `_WorkerThread_OMTEnabled_WCULF()`
    
    Preparation CLs e.g. https://crrev.com/c/8252560 are just to make
    git diff history and codereview simpler by marking the migration from
    `AwPrefetchTest` to `AwPrefetchTestBase` as file move (instead of
    a large file deletion and addition).
    
    Drive-by: This CL also addresses:
    https://crrev.com/c/8252560/5/android_webview/javatests/src/org/chromium/android_webview/test/AwPrefetchMTTest.java#24
    
    Bypass-Check-License: Files are moved.
    Bug: 544544669, 452406598, 452389538
    Change-Id: I83283510d7b586250056b2ad855aec16c45e060b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252962
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694120}

---
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwPrefetchMainThreadTest.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
+
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.content_public.common.ContentSwitches;
+
+/**
+ * Tests for WebView Prefetch API on the main thread (MT) trigger. See the class comment of
+ * `AwPrefetchTestBase`.
+ */
+@RunWith(Parameterized.class)
+@UseParametersRunnerFactory(AwJUnit4ClassRunnerWithParameters.Factory.class)
+@DoNotBatch(reason = "Tests that need browser start are incompatible with @Batch")
+@CommandLineFlags.Add({
+    ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1",
+    "disable-features=PrefetchOffTheMainThread,WebViewPrefetchOffTheMainThread"
+})
+public class AwPrefetchMainThreadTest extends AwPrefetchTestBase {
+    public AwPrefetchMainThreadTest(AwSettingsMutation param) {
+        super(param, /* runOnWorkerThread= */ false);
+    }
+}
---
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwPrefetchOmtDisabledTest.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.UseParametersRunnerFactory;
+
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.content_public.common.ContentSwitches;
+
+/**
+ * Tests for WebView Prefetch API using the off the main thread (OMT) trigger, with OMT Prefetch
+ * *disabled*, i.e. `AwPrefetchManager.startPrefetchRequestAsync()` is called but rerouted to the
+ * main-thread prefetch path. See the class comment of `AwPrefetchTestBase`.
+ */
+@RunWith(Parameterized.class)
+@UseParametersRunnerFactory(AwJUnit4ClassRunnerWithParameters.Factory.class)
+@DoNotBatch(reason = "Tests that need browser start are incompatible with @Batch")
+@CommandLineFlags.Add({
+    ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1",
+    "disable-features=PrefetchOffTheMainThread,WebViewPrefetchOffTheMainThread"
+})
+public class AwPrefetchOmtDisabledTest extends AwPrefetchOmtTestBase {
+    public AwPrefetchOmtDisabledTest(AwSettingsMutation param) {
+        super(param);
+    }
+}
