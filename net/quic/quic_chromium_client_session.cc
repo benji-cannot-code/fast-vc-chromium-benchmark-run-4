380551a0c721e848e672b5aad31d58f396e837dc
Motomu Utsumi <motomuman@google.com>
2026-09-08 05:12:35 -0700

    Clean up kQuicRegisterConnectionClosePayload feature flag
    The feature has reached 100% rollout in production.
    This CL removes the kQuicRegisterConnectionClosePayload feature flag and
    enables the registration and unregistration of QUIC connection close
    payloads unconditionally.
    
    Bug: 550199861
    Change-Id: I77c26ab54c561f8dc1b4e46c17da21eabc394f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8365850
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693708}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -89,13 +89,6 @@ BASE_FEATURE(kQuicMigrationIgnoreDisconnectSignalDuringProbing,
              "kQuicMigrationIgnoreDisconnectSignalDuringProbing",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-// This feature caused an issue that was fixed by https://crrev.com/c/7071963.
-// It's suffixed with V2 to ensure that code without the fix cannot enable
-// the feature.
-BASE_FEATURE(kQuicRegisterConnectionClosePayload,
-             "kQuicRegisterConnectionClosePayloadV2",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 }  // namespace features
 
 namespace {
@@ -1834,10 +1827,6 @@ void QuicChromiumClientSession::OnTlsHandshakeComplete() {
 }
 
 void QuicChromiumClientSession::RegisterQuicConnectionClosePayload() {
-  if (!base::FeatureList::IsEnabled(
-          features::kQuicRegisterConnectionClosePayload)) {
-    return;
-  }
   // Cannot serialize ConnectionClosePacket before handshake is confirmed.
   if (!connection()->IsHandshakeConfirmed()) {
     return;
@@ -1854,10 +1843,6 @@ void QuicChromiumClientSession::RegisterQuicConnectionClosePayload() {
 }
 
 void QuicChromiumClientSession::UnregisterQuicConnectionClosePayload() {
-  if (!base::FeatureList::IsEnabled(
-          features::kQuicRegisterConnectionClosePayload)) {
-    return;
-  }
   static_cast<QuicChromiumPacketWriter*>(connection()->writer())
       ->UnregisterQuicConnectionClosePayload();
 }
