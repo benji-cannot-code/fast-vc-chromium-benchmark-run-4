e23d3e85d17242f80ec9fdb6766995057936c53c
Wan-Teh Chang <wtc@google.com>
2026-09-08 11:19:48 -0700

    Roll src/third_party/dav1d/libdav1d/ 52b9d3d3e..aa09a630e (1 commit)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/52b9d3d3ec52..aa09a630ef57
    
    $ git log 52b9d3d3e..aa09a630e --date=short --no-merges --format='%ad %ae %s'
    2026-06-15 GermanAizek lf_apply: move out of loop constant-calculated expressions
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Id17b5593383ea8ddbd18b2b1b7336ac2829e3192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349680
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693934}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.4-1-g52b9d3d3"
+#define DAV1D_VERSION "1.5.4-2-gaa09a630"
