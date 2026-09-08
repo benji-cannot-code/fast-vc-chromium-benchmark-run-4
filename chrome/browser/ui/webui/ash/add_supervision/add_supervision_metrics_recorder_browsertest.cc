2ae381023f4e87783f03824fd033fc9d75eae9c9
Jun Ishiguro <junis@google.com>
2026-09-07 22:29:18 -0700

    webui: Pass app_locale to AddSupervisionUI::SetUpResources
    Remove direct usage of g_browser_process in AddSupervisionUI by
    injecting ApplicationLocaleStorage into AddSupervisionUIConfig and
    passing the application locale to AddSupervisionUI::SetUpResources.
    
    Update BUILD.gn and DEPS to remove the browser_process dependency from
    the add_supervision production library, and update browser tests to
    look up ApplicationLocaleStorage via GlobalFeatures.
    
    TAG=agy
    
    Bug: b:489929293
    Change-Id: Ida342e4dbc6df251c1312d6ce40de0ff6a652dee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8365185
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693552}

--- chrome/browser/ui/webui/ash/add_supervision/add_supervision_metrics_recorder_browsertest.cc
+++ chrome/browser/ui/webui/ash/add_supervision/add_supervision_metrics_recorder_browsertest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/test_mock_time_task_runner.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/global_features.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision.mojom.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision_handler_utils.h"
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision_ui.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "components/application_locale_storage/application_locale_storage.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_web_ui.h"
@@ -57,7 +60,9 @@ class AddSupervisionMetricsRecorderTest : public InProcessBrowserTest {
   void NotifySupervisionEnabled() {
     mojo::PendingReceiver<add_supervision::mojom::AddSupervisionHandler>
         receiver;
-    AddSupervisionUI add_supervision_ui(&test_web_ui_);
+    AddSupervisionUI add_supervision_ui(
+        &test_web_ui_,
+        g_browser_process->GetFeatures()->application_locale_storage()->Get());
     AddSupervisionHandler add_supervision_handler(
         std::move(receiver), &test_web_ui_,
         identity_test_env_->identity_manager(), &add_supervision_ui);
