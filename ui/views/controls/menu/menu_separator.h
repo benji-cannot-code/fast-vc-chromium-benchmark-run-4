df351dd2d276cb478b36b7acc82ef88c7518747d
Maria del Pilar Dominguez Molina <mardelpi@google.com>
2026-09-08 10:51:02 -0700

    [ChroMenu GlowUp] Add optional divider for MenuItemView.
    This CL enables the support for a bottom separator for menu items. This
    is needed to add the proper style to the Profile menu entry for the
    Block-style ChroMenu. See the following mock
    https://drive.google.com/file/d/12pjtVDH1Kmo5kCsvVUgrEfPWDpzVDSXa/view?usp=sharing.
    
    Bug: 555371374
    Change-Id: I61e742c1ea8ab211e2bd050c1442f5d0f153dc4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8331647
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: María del Pilar Domínguez Molina <mardelpi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693914}

--- ui/views/controls/menu/menu_separator.h
+++ ui/views/controls/menu/menu_separator.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_CONTROLS_MENU_MENU_SEPARATOR_H_
 #define UI_VIEWS_CONTROLS_MENU_MENU_SEPARATOR_H_
 
+#include <optional>
+
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/models/menu_separator_types.h"
+#include "ui/color/color_id.h"
 #include "ui/views/view.h"
 #include "ui/views/views_export.h"
 
@@ -18,7 +21,8 @@ class VIEWS_EXPORT MenuSeparator : public View {
 
  public:
   explicit MenuSeparator(
-      ui::MenuSeparatorType type = ui::MenuSeparatorType::NORMAL_SEPARATOR);
+      ui::MenuSeparatorType type = ui::MenuSeparatorType::NORMAL_SEPARATOR,
+      std::optional<ui::ColorId> color_id = std::nullopt);
   MenuSeparator(const MenuSeparator&) = delete;
   MenuSeparator& operator=(const MenuSeparator&) = delete;
 
@@ -30,9 +34,15 @@ class VIEWS_EXPORT MenuSeparator : public View {
   ui::MenuSeparatorType GetType() const;
   void SetType(ui::MenuSeparatorType type);
 
+  std::optional<ui::ColorId> GetColorId() const;
+  void SetColorId(std::optional<ui::ColorId> color_id);
+
  private:
   // The type of the separator.
   ui::MenuSeparatorType type_;
+
+  // Optional custom color ID for the separator.
+  std::optional<ui::ColorId> color_id_;
 };
 
 }  // namespace views
