6c88754e34051fc80dc98135c10e9acfdaf563d7
Thomas Guilbert <tguilbert@chromium.org>
2026-09-08 14:16:55 -0700

    media: Return true for kSampleFormatDtse in IsBitstream()
    When kSampleFormatDtse was added in crrev.com/c/4228514, it was
    configured as a bitstream in SampleFormatToBytesPerChannel,
    SampleFormatToString ("Compressed DTS Express bitstream"), IsPlanar
    (false), and IsInterleaved (true). However, IsBitstream() returns
    false for kSampleFormatDtse.
    
    Returning false causes an AudioBuffer with kSampleFormatDtse to be
    treated as uncompressed PCM rather than a bitstream, which:
    1. Overwrites the bitstream payload size with a frame-based PCM
       calculation in AudioBuffer constructors.
    2. Triggers NOTREACHED() crashes in AudioBuffer::ReadFrames() and
       AudioBuffer::TrimRange() since kSampleFormatDtse is not a valid
       PCM format.
    3. Fails Mojo IPC deserialization in TypeConverter<AudioBuffer> due
       to PCM frame size and channel alignment checks.
    
    This CL updates IsBitstream(SampleFormat) to return true for
    kSampleFormatDtse, aligning it with the enum definition and sister
    DTS formats (kSampleFormatDts, kSampleFormatDtsxP2, kSampleFormatIECDts).
    
    R=dalecurtis@chromium.org
    
    TAG=agy
    CONV=1713d7f2-9657-444a-9a56-4b69d629fb98
    
    Change-Id: I7f933cac42b63f433ef5ecde2045ba60246a4777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8351970
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694061}

--- media/base/sample_format.cc
+++ media/base/sample_format.cc
@@ -109,6 +109,7 @@ bool IsBitstream(SampleFormat sample_format) {
     case kSampleFormatDts:
     case kSampleFormatDtsxP2:
     case kSampleFormatIECDts:
+    case kSampleFormatDtse:
       // If on-device decoding is required, the sample format will be
       // kSampleFormatS16, so it will return false. If bit-stream passthrough
       // is required, the sample format would already be
@@ -124,7 +125,6 @@ bool IsBitstream(SampleFormat sample_format) {
     case kSampleFormatPlanarS16:
     case kSampleFormatPlanarF32:
     case kSampleFormatPlanarS32:
-    case kSampleFormatDtse:
       return false;
   }
 
