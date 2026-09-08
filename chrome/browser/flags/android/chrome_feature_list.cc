dbdd28e8315ed0c6d7e156613b9bb9f213d3d034
Ritika Gupta <ritikagup@google.com>
2026-09-08 14:54:49 -0700

    [Actor] Pause and update TabGroupSync during background tab transitions
    Pauses TabGroupSync local observation mode across background tab
    transitions (both detaching to background pool and restoring to
    foreground window), and updates local tab ID mappings to prevent
    transient tab swaps from propagating spurious deletions or duplicate
    tabs to synced remote devices.
    
    Also adds a guard in restoreSessionTabToForeground to prevent re-adding
    tabs that were already restored into the TabModel.
    
    Bug: 543113015, 554122721
    Change-Id: I60cf8e2851dcd898985c7fce59b1890c155c7545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8364443
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694093}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -404,6 +404,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kFullscreenInsetsApiMigration,
     &kFullscreenInsetsApiMigrationOnAutomotive,
     &kGestureUserEducationBackSwipe,
+    &kGlicBackgroundActuationTabGroupSync,
     &kGmsCoreBindServiceOptimization,
     &kGridTabSwitcherSurfaceColorUpdate,
     &kHighPrioritySiteNotifications,
@@ -827,6 +828,7 @@ BASE_FEATURE(kForceTranslucentNotificationTrampoline, base::FEATURE_DISABLED_BY_
 BASE_FEATURE(kFullscreenInsetsApiMigration, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kFullscreenInsetsApiMigrationOnAutomotive, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kGestureUserEducationBackSwipe, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kGlicBackgroundActuationTabGroupSync, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kGmsCoreBindServiceOptimization, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kGridTabSwitcherSurfaceColorUpdate, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kHighPrioritySiteNotifications, base::FEATURE_DISABLED_BY_DEFAULT);
