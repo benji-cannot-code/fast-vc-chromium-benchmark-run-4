58eccfa6f406acae449a7ccf60e2982b950949aa
Thomas Lukaszewicz <tluk@chromium.org>
2026-09-08 15:07:02 -0700

    Remove unnecessary browser.h include from //components/live_caption
    Bug: 496674143
    Change-Id: I7f51636a0b8e6cc37d8e91c714ac012c9d11fa61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8373343
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694100}

--- components/live_caption/views/caption_bubble_browsertest.cc
+++ components/live_caption/views/caption_bubble_browsertest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/expected.h"
 #include "build/build_config.h"
 #include "caption_bubble_model.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/confirm_bubble.h"
 #include "chrome/browser/ui/test/test_browser_ui.h"
