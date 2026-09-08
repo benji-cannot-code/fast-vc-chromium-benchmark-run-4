0ee0301eb684403b5fe25b37621097bb89471f00
Zoraiz Naeem <zoraiznaeem@google.com>
2026-09-07 17:21:49 -0700

    Remove kSystemFeaturesDisableListHidden feature flag
    Permanently enable hiding apps disabled by SystemFeaturesDisableList
    policy in user sessions, and clean up the feature flag and associated
    test setups.
    
    TAG=agy
    CONV=991af677-2e1b-41f8-8050-141e3e537558
    
    Bug: b:558334457
    Change-Id: I8f03fbaee2d9c0ae63442e6b9b6f9dbc6f74c4bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8360267
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693474}

--- chrome/browser/policy/test/system_features_policy_browsertest.cc
+++ chrome/browser/policy/test/system_features_policy_browsertest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/policy_pref_names.h"
 #include "components/policy/core/common/system_features_disable_list_constants.h"
@@ -66,9 +65,7 @@ class SystemFeaturesPolicyTestBase : public MixinBasedInProcessBrowserTest {
  public:
   SystemFeaturesPolicyTestBase() {
     scoped_feature_list_.InitWithFeatures(
-        /*enabled_features=*/{ash::features::kEcheSWA, ash::features::kConch,
-                              chromeos::features::
-                                  kSystemFeaturesDisableListHidden},
+        /*enabled_features=*/{ash::features::kEcheSWA, ash::features::kConch},
         /*disabled_features=*/{});
 
     fake_crostini_features_.set_is_allowed_now(true);
