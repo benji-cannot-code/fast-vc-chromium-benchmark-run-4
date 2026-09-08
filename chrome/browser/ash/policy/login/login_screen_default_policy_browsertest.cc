7cba640912fa3b78c55cef672c3f442ef7e36883
Di Wu <diwux@google.com>
2026-09-08 06:12:46 -0700

    Ash: Migrate login screen policy teardown off RunUntilIdle
    This changelist is part of an ongoing series to modernize Ash tests by
    migrating them away from RunUntilIdle() and toward targeted
    synchronization with APIs such as RunUntil(), TestFuture, RunLoop
    callbacks, observer waiters, or exact mock-time advancement.
    
    LoginScreenDefaultPolicyLoginScreenBrowsertest now waits for the browser
    process to quit after teardown requests sign-out. A sign-out request
    only schedules shutdown. Browser-process exit is the teardown barrier.
    
    Bug: b:506794349
    Test: CQ
    Change-Id: I0bdbf0dfbd2c270823f0346a91ddf058b6e7e381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8231518
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693732}

--- chrome/browser/ash/policy/login/login_screen_default_policy_browsertest.cc
+++ chrome/browser/ash/policy/login/login_screen_default_policy_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
+#include "components/session_manager/core/session_manager.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -162,7 +163,7 @@ void LoginScreenDefaultPolicyLoginScreenBrowsertest::TearDownOnMainThread() {
       FROM_HERE, base::BindOnce([]() {
         session_manager::SessionManager::Get()->RequestSignOut();
       }));
-  base::RunLoop().RunUntilIdle();
+  RunUntilBrowserProcessQuits();
   LoginScreenDefaultPolicyBrowsertestBase::TearDownOnMainThread();
 }
 
