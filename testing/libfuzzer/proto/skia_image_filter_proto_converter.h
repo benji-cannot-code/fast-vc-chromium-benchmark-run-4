1a5e127fa5a171d7ef32887e1bc86df4d7df936c
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-09-08 10:12:59 -0700

    [unsafe-buffers] Resolve trivial UNSAFE_TODO in skia proto converter
    Replace trivial unsafe buffer usages with base::span or container
    methods in skia_image_filter_proto_converter.
    
    Bug: 351564777, 501112808
    Doc: http://bit.ly/complete_unsafe_todo_migration
    Change-Id: I31028296c285c6637c0bba94a9aba5c25ff834f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158380
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693880}

--- testing/libfuzzer/proto/skia_image_filter_proto_converter.h
+++ testing/libfuzzer/proto/skia_image_filter_proto_converter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TESTING_LIBFUZZER_PROTO_SKIA_IMAGE_FILTER_PROTO_CONVERTER_H_
 #define TESTING_LIBFUZZER_PROTO_SKIA_IMAGE_FILTER_PROTO_CONVERTER_H_
 
+#include <array>
 #include <random>
 #include <set>
 #include <string>
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/fixed_flat_map.h"
 #include "base/containers/fixed_flat_set.h"
+#include "base/containers/span.h"
 #include "testing/libfuzzer/proto/skia_image_filter.pb.h"
 #include "third_party/skia/include/core/SkPoint.h"
 
@@ -57,17 +59,15 @@ class Converter {
   static const char kSkPictReaderTag[];
   static const uint8_t kCountNibBits[];
 
-  // The size of kColorTableBuffer.
-  static const int kColorTableBufferLength;
-
   // Used to bound flattenable_depth_.
   static const int kFlattenableDepthLimit;
 
   // Used to bound numeric fields.
   static const int kNumBound;
 
-  // Used by ColorTableToArray to store a ColorTable Message as an array.
-  static uint8_t kColorTableBuffer[];
+  static constexpr size_t kColorTableEntries = 64;
+  static constexpr size_t kColorTableByteSize =
+      kColorTableEntries * sizeof(float);
 
   // There will be a 1/kMutateEnumDenominator chance that WriteEnum
   // writes an invalid enum value instead of the one given to us by LPM.
@@ -362,14 +362,12 @@ class Converter {
   // write_size + number of padding bytes is divisible by four.
   void Pad(const size_t write_size);
 
-  // Write size elements of RepeatedField of uint32_ts repeated_field as an
-  // array.
+  // Write repeated_field as an array.
   void WriteArray(
-      const google::protobuf::RepeatedField<uint32_t>& repeated_field,
-      const size_t size);
+      const google::protobuf::RepeatedField<uint32_t>& repeated_field);
 
-  // Write size bytes of arr as an array and pad if necessary.
-  void WriteArray(const char* arr, const size_t size);
+  // Write arr as an array and pad if necessary.
+  void WriteArray(base::span<const char> arr);
 
   void WriteBool(const bool bool_val);
 
@@ -471,9 +469,9 @@ class Converter {
   bool IsFinite(float num) const;
   bool IsBlacklisted(const std::string& field_name) const;
 
-  // Converts color_table from our proto Message format to a 256-byte array.
-  // Note that this function modifies kColorTableBuffer.
-  const uint8_t* ColorTableToArray(const ColorTable& color_table);
+  // Converts color_table from our proto Message format to an array of floats.
+  std::array<float, kColorTableEntries> ColorTableToArray(
+      const ColorTable& color_table);
 
 #ifdef DEVELOPMENT
   // ICC related functions
