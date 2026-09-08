922f0918cc9e613565a26669d5ebfa666c553042
Yurii Lahodiuk <ylahodiuk@google.com>
2026-09-08 03:39:55 -0700

    Refactor and simplify OnDeviceEncryptionMetricsReporter
    Before introducing this change OnDeviceEncryptionMetricsReporter
    contained the same logic for tracking and publishing passkey- and
    password- on-device encryption state metrics:
    
    ```
    class OnDeviceEncryptionMetricsReporter {
      ...
    
      void MaybeRecordPasskeyReadiness(...);
      void MaybeRecordPasswordReadiness(...);
    
      OnDeviceEncryptionStateTracker* passkey_tracker_;
      OnDeviceEncryptionStateTracker* password_tracker_;
    
      base::ScopedObservation passkey_observation_;
      base::ScopedObservation password_observation_;
    
      OnDeviceEncryptionStateHistogramBucket
        last_published_passkey_bucket_;
      OnDeviceEncryptionStateHistogramBucket
        last_published_password_bucket_;
    }
    ```
    
    In the scope of this change we are eliminating the duplication by
    introducing `OnDeviceEncryptionDataTypeSpecificMetricsReporter` (which
    will be instantiated twice: for passkeys and for passwords):
    
    ```
    class OnDeviceEncryptionMetricsReporter {
      ...
    
      OnDeviceEncryptionDataTypeSpecificMetricsReporter passkey_reporter_;
      OnDeviceEncryptionDataTypeSpecificMetricsReporter password_reporter_;
    }
    ```
    
    This change also allowed to simplify the observation interface of
    OnDeviceEncryptionStateTracker (we don't need anymore to pass a pointer
    to the observable object).
    
    The test
    `on_device_encryption_data_type_specific_metrics_reporter_unittest.cc`
    just contains the test cases carried over from
    `on_device_encryption_metrics_reporter_unittest.cc`.
    
    Bug: 540854648
    Change-Id: I5efefbe35431a5f0f2c81ddc5b0b5ea88a10d668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353188
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Yurii Lahodiuk <ylahodiuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693685}

--- components/password_manager/core/browser/ode/on_device_encryption_state_tracker.cc
+++ components/password_manager/core/browser/ode/on_device_encryption_state_tracker.cc
@@ -14,7 +14,7 @@ OnDeviceEncryptionStateTracker::OnDeviceEncryptionStateTracker() = default;
 OnDeviceEncryptionStateTracker::~OnDeviceEncryptionStateTracker() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   for (Observer& observer : observer_list_) {
-    observer.OnDeviceEncryptionStateTrackerShuttingDown(this);
+    observer.OnDeviceEncryptionStateTrackerShuttingDown();
   }
 }
 
@@ -42,7 +42,7 @@ void OnDeviceEncryptionStateTracker::SetState(
   }
   OnDeviceEncryptionState previous_state = std::exchange(state_, new_state);
   for (Observer& observer : observer_list_) {
-    observer.OnDeviceEncryptionStateChanged(this, previous_state, state_);
+    observer.OnDeviceEncryptionStateChanged(previous_state, state_);
   }
 }
 
