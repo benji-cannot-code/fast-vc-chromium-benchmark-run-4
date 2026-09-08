c5850155cd9427ed14233f0d838f5b24ff5e00b7
Khalid Peer <khalidpeer@chromium.org>
2026-09-08 13:04:22 -0700

    [gardener] Disable flaky AccountSettingsPagePixelBrowserTest test.
    This CL disables the OpenAccountSettingsPageWithPassphraseRequired test
    from the AccountSettingsPagePixelBrowserTest test suite due to flakiness
    on Windows builders.
    
    Bug: b:550857602
    Change-Id: I0f30c9245514cbd3e214f7e394a6c09dc1e7b1ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8373581
    Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Owners-Override: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693986}

--- chrome/browser/ui/signin/account_settings_page_pixel_browsertest.cc
+++ chrome/browser/ui/signin/account_settings_page_pixel_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/batch_upload/batch_upload_service_test_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
@@ -176,8 +177,16 @@ IN_PROC_BROWSER_TEST_F(AccountSettingsPagePixelBrowserTest,
       "account_settings_page_sync_datatypes_off"));
 }
 
+// TODO(crbug.com/550857602): Flaky on Windows builders.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_OpenAccountSettingsPageWithPassphraseRequired \
+  DISABLED_OpenAccountSettingsPageWithPassphraseRequired
+#else
+#define MAYBE_OpenAccountSettingsPageWithPassphraseRequired \
+  OpenAccountSettingsPageWithPassphraseRequired
+#endif
 IN_PROC_BROWSER_TEST_F(AccountSettingsPagePixelBrowserTest,
-                       OpenAccountSettingsPageWithPassphraseRequired) {
+                       MAYBE_OpenAccountSettingsPageWithPassphraseRequired) {
   SigninWithFullInfo();
   SimulatePassphraseError();
 
