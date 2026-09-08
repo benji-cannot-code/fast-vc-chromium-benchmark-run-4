14e173573bf5042f0a050960edf4fbf536d3a399
Brandon Fong <bjfong@google.com>
2026-09-08 14:32:24 -0700

    [tips] Remove code logic for migration and pref clear for tips
    Remove code logic that cleared deprecated prefs and migrated users from
    a deprecated notification channel to a newer channel, all done in M150
    which is a 5 milestone separation.
    
    NO_IFTTT=deprecation update
    
    Bug: 509668849, 509972652
    Change-Id: Ia796b71cdf821b14ec4f915e7dfe5f0879895ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354528
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694075}

--- chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/channels/ChromeChannelDefinitions.java
+++ chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/channels/ChromeChannelDefinitions.java
@@ -5,14 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.notifications.channels;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.os.Build;
 import android.text.TextUtils;
 
 import androidx.annotation.StringDef;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.base.DeviceInfo;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -113,7 +110,7 @@ public class ChromeChannelDefinitions extends ChannelDefinitions {
         ChannelId.BLUETOOTH,
         ChannelId.USB,
         ChannelId.SERIAL,
-        ChannelId.TIPS,
+        ChannelId.TIPS, // Deprecated, use TIPS_V2.
         ChannelId.TIPS_V2,
         ChannelId.CHROME_FINDS,
     })
@@ -402,31 +399,6 @@ public class ChromeChannelDefinitions extends ChannelDefinitions {
                             NotificationManager.IMPORTANCE_LOW,
                             ChannelGroupId.GENERAL));
 
-            // Temporary migration logic from the old tips notification channel to the new tips
-            // notification channel. To be removed in a follow up task.
-            // TODO(crbug.com/509668849): Fully deprecate the ChannelId.TIPS channel after a few
-            // milestones from M150 when this migration code is landed to consolidate the tips
-            // notifications channel to be solely using ChannelId.TIPS_V2.
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                NotificationManager notificationManager =
-                        ContextUtils.getApplicationContext()
-                                .getSystemService(NotificationManager.class);
-                if (notificationManager != null) {
-                    NotificationChannel tipsChannel =
-                            notificationManager.getNotificationChannel(ChannelId.TIPS);
-                    if (tipsChannel != null) {
-                        NotificationChannel tipsV2Channel =
-                                new NotificationChannel(
-                                        ChannelId.TIPS_V2,
-                                        ContextUtils.getApplicationContext()
-                                                .getString(R.string.notification_category_tips),
-                                        tipsChannel.getImportance());
-                        tipsV2Channel.setGroup(ChannelGroupId.GENERAL);
-                        notificationManager.createNotificationChannel(tipsV2Channel);
-                        notificationManager.deleteNotificationChannel(ChannelId.TIPS);
-                    }
-                }
-            }
             // The tips notification channel will appear for users who opted in.
             // For new users, it will be created when they explicitly opt in.
             map.put(
