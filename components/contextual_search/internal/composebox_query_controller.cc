7d46a3b9582109fc5070839e0e04f5285f9d3e04
Jason Hu <hujasonx@google.com>
2026-09-08 10:41:03 -0700

    [context-input] Use composebox endpoint for chunk uploads
    Updates ComposeboxQueryController::UploadChunk to query
    GetLensComposeboxUploadChunkEndpointUrl() instead of
    GetLensOverlayUploadChunkEndpointURL(). When identity delegation is
    enabled, this routes chunk uploads to the correct composebox endpoint.
    
    Bug: b:558673217
    Change-Id: Ic12c0d49e6bf9b11995a970b4286e254263ea351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8366988
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693902}

--- components/contextual_search/internal/composebox_query_controller.cc
+++ components/contextual_search/internal/composebox_query_controller.cc
@@ -2977,7 +2977,7 @@ void ComposeboxQueryController::UploadChunk(
           },
           std::move(completion_callback)),
       progress_callback,
-      GURL(lens::features::GetLensOverlayUploadChunkEndpointURL()));
+      GURL(lens::features::GetLensComposeboxUploadChunkEndpointUrl()));
 }
 
 void ComposeboxQueryController::OnPageContentPayloadForChunkUploadReady(
