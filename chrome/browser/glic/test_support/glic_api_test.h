df4d14f9bc7cb78ea2c2ac016e0b885b3307e5cc
Dan Harrington <harringtond@chromium.org>
2026-09-08 17:09:51 -0700

    Use https instead of http in glic api tests
    Bug: b:557294354
    Change-Id: Ifcf75ca4b6d2fa81b3d959e40c284d806a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8357884
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694201}

--- chrome/browser/glic/test_support/glic_api_test.h
+++ chrome/browser/glic/test_support/glic_api_test.h
@@ -200,6 +200,7 @@ class GlicApiBrowserTestMixin : public T {
   explicit GlicApiBrowserTestMixin(GlicTestJsPath js_source_path,
                                    Args&&... args)
       : Base(std::forward<Args>(args)...) {
+    Base::SetUseHttpsForGlicUrl(true);
     Base::AddMockGlicQueryParam(
         "test",
         ::testing::UnitTest::GetInstance()->current_test_info()->name());
@@ -219,6 +220,18 @@ class GlicApiBrowserTestMixin : public T {
     Base::embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
         &GlicApiBrowserTestMixin::OnEmbeddedTestServerHttpRequest,
         base::Unretained(this)));
+
+    Base::embedded_https_test_server().RegisterRequestHandler(
+        base::BindRepeating(&GlicApiBrowserTestMixin::SorryHtmlRequestHandler,
+                            base::Unretained(this)));
+    Base::embedded_https_test_server().RegisterRequestHandler(
+        base::BindRepeating(&GlicApiBrowserTestMixin::FakeRpcRequestHandler,
+                            base::Unretained(this)));
+
+    Base::embedded_https_test_server().RegisterRequestMonitor(
+        base::BindRepeating(
+            &GlicApiBrowserTestMixin::OnEmbeddedTestServerHttpRequest,
+            base::Unretained(this)));
 
     features_.InitWithFeaturesAndParameters(
         /*enabled_features=*/
