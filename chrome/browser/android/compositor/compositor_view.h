80a23cc14d2b219d7f02eac54ebc478f264c7c85
Patrick Monette <pmonette@chromium.org>
2026-09-08 18:33:57 -0700

    Log ChildProcessId in CompositorView and log PID on process launch
    In CompositorView::BrowserChildProcessKilled, avoid calling
    data.GetProcess().Pid() because the process handle may not be valid upon
    termination notification. Instead, log the session-unique
    ChildProcessId.
    
    To allow correlating the ChildProcessId with the system PID when reading
    logs, add a LOG(WARNING) when the child process is launched and
    connected, where the process handle is guaranteed to be valid.
    
    Bug: 40716800
    Change-Id: I6e5d45c2eee468b485801efe8b60e376b49b4f1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8356227
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694272}

--- chrome/browser/android/compositor/compositor_view.h
+++ chrome/browser/android/compositor/compositor_view.h
@@ -117,6 +117,8 @@ class CompositorView : public content::CompositorClient,
   ~CompositorView() override;
 
   // content::BrowserChildProcessObserver implementation:
+  void BrowserChildProcessLaunchedAndConnected(
+      const content::ChildProcessData& data) override;
   void BrowserChildProcessKilled(
       const content::ChildProcessData& data,
       const content::ChildProcessTerminationInfo& info) override;
