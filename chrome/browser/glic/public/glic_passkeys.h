db1d40503c4eb7904f9873b1a765afbd08188d28
Dave Tapuska <dtapuska@chromium.org>
2026-09-08 17:16:24 -0700

    Add a floating prompt UI to the Glic selection overlay.
    This CL introduces a floating prompt container that appears near the
    active selection in the Glic selection overlay. The UI includes a text
    input field ("Ask Gemini") and action chips for quick tasks like
    describing or creating images.
    
    Key changes:
    - Added HTML, CSS, and positioning logic in the WebUI to display and position the prompt container relative to the selection bounds.
    - Added a SubmitPrompt method to the selection overlay Mojo interface.
    - Implemented SubmitPrompt in SelectionOverlayController to invoke the Glic service with the user's prompt and auto-submit it.
    - Added SelectionOverlayController as a friend class in glic_passkeys.h to allow auto-submit invocation.
    
    Change-Id: Ifb3b1b433b56c3c84268a97af435b868bf746dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8329768
    Reviewed-by: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694207}

--- chrome/browser/glic/public/glic_passkeys.h
+++ chrome/browser/glic/public/glic_passkeys.h
@@ -38,6 +38,7 @@ namespace glic {
 
 class GlicExperimentalTriggeringCoordinator;
 class GlicInternalsPageHandler;
+class SelectionOverlayController;
 
 template <mojom::InvocationSource Source>
 class AndroidAutoSubmitPasskeyHelper;
@@ -68,6 +69,7 @@ class InvokeWithAutoSubmitPasskeyProvider {
   friend class GlicExperimentalTriggeringCoordinator;
   friend class GlicCueTarget;
   friend class GlicSelectionObserver;
+  friend class SelectionOverlayController;
   friend class ::indigo::IndigoPageActionController;
   friend class ::ttc::AiOverlayTools;
   friend class ::skills::SkillsUiTabController;
