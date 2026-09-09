043b404a7ad71cd81e80a0f812186ba3178c896a
Bryan Kim <bryankim@google.com>
2026-09-09 13:03:40 -0700

    [Bedrock] Modularize 5 Views frame leaf UI modules (Chunk 4)
    Extract 5 leaf modules from chrome/browser/ui/BUILD.gn
    static_library("ui") into fine-grained targets under
    //chrome/browser/ui/views/frame:
    - immersive_mode_controller_factory_views.cc
    - main_background_region_view.{h,cc}
    - browser_window_factory.cc
    - layout/browser_view_layout_delegate_impl.{h,cc}
    - layout/browser_view_layout_impl.{h,cc}
    
    TAG=agy
    CONV=9fde6f8b-8d5e-4451-a32d-384a25dbc59d
    
    Bug: 353332589
    Change-Id: I37ac637fcf0b7346c7ad10346cd5440439a2c099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8373188
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694827}

--- chrome/browser/ui/views/frame/layout/browser_view_layout_impl.cc
+++ chrome/browser/ui/views/frame/layout/browser_view_layout_impl.cc
@@ -15,9 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/views/view.h"
 
-#if BUILDFLAG(IS_MAC)
-#include "chrome/browser/ui/views/frame/glass_frame_service.h"
-#endif
 
 // Proposed layout implementation.
 
