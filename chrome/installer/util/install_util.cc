f888194add2ea1ee2a79df23eaa6a6484ec51dab
Marwan Tammam <quarz@chromium.org>
2026-09-09 14:39:07 -0700

    Install Platform Runtime component to Program Files on Windows
    Use the `install-component` app command to install the Chrome Platform
    Runtime component to system-level dir (Program Files) on Windows. On
    user-level Chrome installs, launch setup.exe in a subprocess to invoke
    InstallComponent directly skipping the App Command.
    
    Bug: b:548000843
    Change-Id: Ica669e37d507678d1e13e832ef120df876eaa56d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8307914
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Charles Harrison <csharrison@chromium.org>
    Commit-Queue: Marwan Tammam <quarz@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694888}

--- chrome/installer/util/install_util.cc
+++ chrome/installer/util/install_util.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
+#include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
@@ -618,4 +619,40 @@ std::wstring InstallUtil::GuidToSquid(std::wstring_view guid) {
   std::reverse_copy(input + 32, input + 34, output);
   std::reverse_copy(input + 34, input + 36, output);
   return squid;
+}
+
+// static
+base::FilePath InstallUtil::GetLatestInstalledComponentDir(
+    const base::FilePath& application_dir,
+    std::string_view crx_id,
+    base::Version* version) {
+  if (application_dir.empty() || crx_id.empty()) {
+    return base::FilePath();
+  }
+  base::FilePath component_root = application_dir.AppendASCII(crx_id);
+  base::Version highest_version;
+  base::FilePath latest_dir;
+
+  base::FileEnumerator file_enumerator(component_root, /*recursive=*/false,
+                                       base::FileEnumerator::DIRECTORIES);
+  file_enumerator.ForEach([&highest_version,
+                           &latest_dir](const base::FilePath& path) {
+    base::Version dir_version(path.BaseName().MaybeAsASCII());
+    if (!dir_version.IsValid()) {
+      return;
+    }
+    // Manifest.json must be present to be considered valid.
+    if (!base::PathExists(path.Append(FILE_PATH_LITERAL("manifest.json")))) {
+      return;
+    }
+    if (!highest_version.IsValid() || dir_version > highest_version) {
+      highest_version = std::move(dir_version);
+      latest_dir = path;
+    }
+  });
+
+  if (version && highest_version.IsValid()) {
+    *version = std::move(highest_version);
+  }
+  return latest_dir;
 }
