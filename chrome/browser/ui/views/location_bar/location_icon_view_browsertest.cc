66236626031ccb7d92e348d37f07fa0cda481aaa
Camillia Smith Barnes <cammie@chromium.org>
2026-09-09 11:18:44 -0700

    WebUILocationBar: Disable WebUILocationBar in LocationIconView tests
    LocationIconView is part of the native Views LocationBarView hierarchy.
    When WebUILocationBar is enabled, LocationBarView is not constructed in
    BrowserView, returning nullptr and causing these Views-specific tests to
    fail when dereferencing the location bar or its child views. Disable
    features::kWebUILocationBar in LocationIconViewBrowserTest and
    LocationIconViewTest fixtures.
    
    TAG=agy
    CONV=521599b3-d27a-4228-b2e2-93a09018322e
    
    Bug: 557274441
    Change-Id: I9cb0e4c64acb0ee6130830d2cde666b3e3478eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8356189
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694747}

--- chrome/browser/ui/views/location_bar/location_icon_view_browsertest.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view_browsertest.cc
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_icon_view.h"
 
 #include "base/memory/raw_ptr.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/omnibox/omnibox_controller.h"
 #include "chrome/browser/ui/omnibox/omnibox_edit_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "ui/views/animation/ink_drop.h"
@@ -18,7 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO (spqchan): Refine tests. See crbug.com/40542814.
 class LocationIconViewBrowserTest : public InProcessBrowserTest {
  public:
-  LocationIconViewBrowserTest() = default;
+  LocationIconViewBrowserTest() {
+    scoped_feature_list_.InitAndDisableFeature(features::kWebUILocationBar);
+  }
 
   LocationIconViewBrowserTest(const LocationIconViewBrowserTest&) = delete;
   LocationIconViewBrowserTest& operator=(const LocationIconViewBrowserTest&) =
@@ -46,6 +50,7 @@ class LocationIconViewBrowserTest : public InProcessBrowserTest {
   LocationIconView* icon_view() const { return icon_view_; }
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
   raw_ptr<LocationBarView> location_bar_;
   raw_ptr<LocationIconView> icon_view_;
 };
--- chrome/browser/ui/views/location_bar/location_icon_view_interactive_uitest.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view_interactive_uitest.cc
@@ -3,16 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/views/location_bar/location_icon_view.h"
+
 #include "base/test/run_until.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
-#include "chrome/browser/ui/views/location_bar/location_icon_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_bubble_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/browser/ui/views/translate/translate_bubble_controller.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -23,7 +26,9 @@ namespace {
 
 class LocationIconViewTest : public InProcessBrowserTest {
  public:
-  LocationIconViewTest() = default;
+  LocationIconViewTest() {
+    scoped_feature_list_.InitAndDisableFeature(features::kWebUILocationBar);
+  }
 
   LocationIconViewTest(const LocationIconViewTest&) = delete;
   LocationIconViewTest& operator=(const LocationIconViewTest&) = delete;
@@ -85,6 +90,9 @@ class LocationIconViewTest : public InProcessBrowserTest {
     })) << "Failed to exit fullscreen mode";
   }
 #endif  // BUILDFLAG(IS_MAC)
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 // Verify that clicking the location icon a second time hides the bubble.
