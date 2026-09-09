db7b1bced94af5c05ffa9c99600823cb395a945f
Wilson Low <wilsonlow@google.com>
2026-09-08 21:09:49 -0700

    [Autofill] Update payments_autofill_table.h comments to fit standards
    - Replace legacy pipe-style symbols (|symbol|) with markdown backticks
      (`symbol`) for code references in comments.
    - Update `offer_details_url` description to clarify that it links to
      the offer terms and conditions page.
    
    Bug: 546252995, 548660097
    Change-Id: Ia961089f40f941d29fc2d25ee7fc097f7e803243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8274192
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694327}

--- components/autofill/core/browser/webdata/payments/payments_autofill_table.h
+++ components/autofill/core/browser/webdata/payments/payments_autofill_table.h
@@ -95,7 +95,7 @@ class VirtualCardUsageData;
 //                      CardIssuer.Issuer enum from the Chrome Sync response.
 //                      For example, GOOGLE or ISSUER_UNKNOWN.
 //   instrument_id      Credit card id assigned by the server to identify this
-//                      card. This is opaque to the client, and |id| is the
+//                      card. This is opaque to the client, and `id` is the
 //                      legacy version of this.
 //   virtual_card_enrollment_state
 //                      An enum indicating the virtual card enrollment state of
@@ -220,7 +220,7 @@ class VirtualCardUsageData;
 //                      amount cashback (XXX$).
 //   expiry             The timestamp when the offer will go expired. Expired
 //                      offers will not be shown in the frontend.
-//   offer_details_url  The link leading to the offer details page on Gpay app.
+//   offer_details_url  The link leading to the offer terms and conditions page.
 //   promo_code         The promo code to be autofilled for a promo code offer.
 //   value_prop_text    Server-driven UI string to explain the value of the
 //                      offer.
@@ -235,7 +235,7 @@ class VirtualCardUsageData;
 //                      offers.
 //
 //   offer_id           Int 64 to identify the relevant offer. Matches the
-//                      offer_id in the offer_data table.
+//                      `offer_id` in the `offer_data` table.
 //   instrument_id      The new form of instrument id of the card. Will not be
 //                      used for now.
 // -----------------------------------------------------------------------------
@@ -244,7 +244,7 @@ class VirtualCardUsageData;
 //                      offers.
 //
 //   offer_id           Int 64 to identify the relevant offer. Matches the
-//                      offer_id in the offer_data table.
+//                      `offer_id` in the `offer_data` table.
 //   merchant_domain    List of full origins for merchant websites on which
 //                      this offer would apply.
 // -----------------------------------------------------------------------------
@@ -372,7 +372,7 @@ class PaymentsAutofillTable : public WebDatabaseTable {
 
   ~PaymentsAutofillTable() override;
 
-  // Retrieves the PaymentsAutofillTable* owned by |db|.
+  // Retrieves the PaymentsAutofillTable* owned by `db`.
   static PaymentsAutofillTable* FromWebDatabase(WebDatabase* db);
 
   // WebDatabaseTable:
@@ -412,7 +412,7 @@ class PaymentsAutofillTable : public WebDatabaseTable {
   // value indicates if `kLocalStoredCvcTable` got updated or not.
   bool UpdateLocalCvc(const std::string& guid, const std::u16string& cvc);
 
-  // Removes a row from the credit_cards table.  |guid| is the identifier of the
+  // Removes a row from the credit_cards table.  `guid` is the identifier of the
   // credit card to remove.
   bool RemoveCreditCard(const std::string& guid);
 
@@ -420,7 +420,7 @@ class PaymentsAutofillTable : public WebDatabaseTable {
   // production server cards are set directly via `SetServerCreditCards`.
   bool AddServerCreditCardForTesting(const CreditCard& credit_card);
 
-  // Retrieves a credit card with guid |guid|.
+  // Retrieves a credit card with guid `guid`.
   std::unique_ptr<CreditCard> GetCreditCard(const std::string& guid);
 
   // Retrieves the local/server credit cards in the database.
@@ -509,8 +509,8 @@ class PaymentsAutofillTable : public WebDatabaseTable {
   bool GetPaymentsCustomerData(
       std::unique_ptr<PaymentsCustomerData>& customer_data) const;
 
-  // |autofill_offer_data| must include all existing offers, since table will
-  // be completely overwritten.
+  // `autofill_offer_data` must include all existing offers, since the table
+  // will be completely overwritten.
   void SetAutofillOffers(
       const std::vector<AutofillOfferData>& autofill_offer_data);
   bool GetAutofillOffers(
@@ -605,14 +605,14 @@ class PaymentsAutofillTable : public WebDatabaseTable {
   bool MigrateToVersion153ReplaceOriginWithIsUserConfirmed();
 
  private:
-  // Adds to |masked_credit_cards| and updates |server_card_metadata|.
+  // Adds to `masked_credit_cards` and updates `server_card_metadata`.
   // Must already be in a transaction.
   void AddMaskedCreditCards(const std::vector<CreditCard>& credit_cards);
 
-  // Deletes server credit cards by |id|. Returns true if a row was deleted.
+  // Deletes server credit cards by `id`. Returns true if a row was deleted.
   bool DeleteFromMaskedCreditCards(const std::string& id);
 
-  // Get the list of eligible merchant domains for the specific 'benefit_id`.
+  // Get the list of eligible merchant domains for the specific `benefit_id`.
   base::flat_set<url::Origin> GetMerchantDomainsForBenefitId(
       const CreditCardBenefitBase::BenefitId& benefit_id);
 
