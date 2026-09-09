3b9d33a9942a373575e482c01afe6d26efefbdba
Hitarth Kothari <hitarthkothari@google.com>
2026-09-09 16:02:37 -0700

    [BottomSheetType] Migrate SimpleNoticeSheetView to BottomSheetType
    Migrate SimpleNoticeSheetView to declare its BottomSheetType as
    PASSIVE_PROMO.
    
    Bug: 505050661
    Change-Id: I1247e2e94f204adeb50f7628e4b21a172de29be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8310734
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694964}

--- chrome/browser/password_manager/android/bottom_sheet/java/src/org/chromium/chrome/browser/bottom_sheet/SimpleNoticeSheetView.java
+++ chrome/browser/password_manager/android/bottom_sheet/java/src/org/chromium/chrome/browser/bottom_sheet/SimpleNoticeSheetView.java
@@ -23,11 +23,15 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.password_manager.PasswordManagerResourceProviderFactory;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetType;
 import org.chromium.ui.widget.TextViewWithClickableSpans;
 
 /** This class is responsible for rendering the simple notice sheet. */
 @NullMarked
 class SimpleNoticeSheetView implements BottomSheetContent {
+    private static final BottomSheetType BOTTOM_SHEET_TYPE =
+            new BottomSheetType.Builder().build();
+
     private final RelativeLayout mContentView;
 
     SimpleNoticeSheetView(Context context) {
@@ -83,6 +87,11 @@ class SimpleNoticeSheetView implements BottomSheetContent {
 
     @Override
     public void destroy() {}
+
+    @Override
+    public BottomSheetType getSheetType() {
+        return BOTTOM_SHEET_TYPE;
+    }
 
     @Override
     public int getPriority() {
