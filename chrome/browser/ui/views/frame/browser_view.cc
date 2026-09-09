b97ceb0b4ac77ff1e7aa7f6ded195f426d083e74
Agata Szpotek <szpotekagata@google.com>
2026-09-08 23:57:07 -0700

    [Isolated mode] Disable custom frame backgrounds in isolated mode
    This change introduces BrowserView::GetEnterpriseIsolatedMode to check
    if the profile is in enterprise isolated mode. It uses this helper to
    disable custom background images for the browser frame in isolated mode,
    and refactors an existing check in ShouldShowAvatarToolbarIPH.
    
    Fixed: 558545343
    Change-Id: I7c1155e841a2557b977ee81e29192785b32e5504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8361778
    Commit-Queue: Agata Szpotek <szpotekagata@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Viacheslav Krakhotin <krakhotin@google.com>
    Reviewed-by: Elly <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694381}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1465,6 +1465,10 @@ bool BrowserView::GetIncognito() const {
   return browser_->GetProfile()->IsIncognitoProfile();
 }
 
+bool BrowserView::GetEnterpriseIsolatedMode() const {
+  return browser_->GetProfile()->IsEnterpriseIsolatedModeProfile();
+}
+
 bool BrowserView::GetGuestSession() const {
   return browser_->GetProfile()->IsGuestSession();
 }
@@ -5280,8 +5284,7 @@ void BrowserView::CreateJumpList() {
 #endif
 
 bool BrowserView::ShouldShowAvatarToolbarIPH() {
-  if (GetGuestSession() || GetIncognito() ||
-      GetProfile()->IsEnterpriseIsolatedModeProfile()) {
+  if (GetGuestSession() || GetIncognito() || GetEnterpriseIsolatedMode()) {
     return false;
   }
   AvatarToolbarButtonInterface* avatar_button =
