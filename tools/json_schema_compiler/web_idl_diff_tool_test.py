f72c9698c1fdef23b7dff4ae28b0da58686f7298
Tim <tjudkins@chromium.org>
2026-09-09 16:41:59 -0700

    [Extensions] Convert sockets.udp to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Jetski CLI. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    Fixed: 452427273
    Change-Id: Ia77da9ded05a6f1ee6f7e26b530408b311480c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8348772
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695008}

--- tools/json_schema_compiler/web_idl_diff_tool_test.py
+++ tools/json_schema_compiler/web_idl_diff_tool_test.py
@@ -108,6 +108,7 @@ _CONVERTED_SCHEMAS = [
   ('file_system.idl', 'file_system.webidl'),
   ('sockets_tcp_server.idl', 'sockets_tcp_server.webidl'),
   ('sockets_tcp.idl', 'sockets_tcp.webidl'),
+  ('sockets_udp.idl', 'sockets_udp.webidl'),
 ]
 
 
