ece2e685fb3c20fb7e9a13cdab94b10783ba7c4e
Muhammad Salmaan <musalmaan@chromium.org>
2026-09-09 07:25:07 -0700

    [InfoBar] Delete deprecated ReloadPluginInfoBarDelegate
    The ReloadPluginInfoBarDelegate is completely unused in production code
    and has been deprecated. This CL removes the delegate implementation,
    the associated build files, WebUI internals entries, and browser test
    cases, and comments out the delegate's ID enum value.
    
    Bug: 523212823
    Change-Id: If95ef1081f21b190496f4fc4af1d1a5dc76e0cbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354723
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694574}

--- chrome/browser/infobars/infobars_browsertest.cc
+++ chrome/browser/infobars/infobars_browsertest.cc
@@ -81,10 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/identity/web_auth_flow_info_bar_delegate.h"
 #endif
 
-#if BUILDFLAG(ENABLE_PLUGINS)
-#include "chrome/browser/plugins/reload_plugin_infobar_delegate.h"
-#endif
-
 #if !BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/ui/startup/default_browser_prompt/default_browser_infobar_delegate.h"
 #endif
@@ -253,9 +249,6 @@ void InfoBarUiTest::ShowUi(const std::string& name) {
           {"session_restore", IBD::SESSION_RESTORE_INFOBAR_DELEGATE},
 #endif
 
-#if BUILDFLAG(ENABLE_PLUGINS)
-          {"reload_plugin", IBD::RELOAD_PLUGIN_INFOBAR_DELEGATE},
-#endif  // BUILDFLAG(ENABLE_PLUGINS)
       });
   const auto id_entry = kIdentifiers.find(name);
   if (id_entry == kIdentifiers.end()) {
@@ -314,15 +307,6 @@ void InfoBarUiTest::ShowUi(const std::string& name) {
                 browser()->GetProfile(), base::OnceClosure()));
       }
       break;
-
-#if BUILDFLAG(ENABLE_PLUGINS)
-    case IBD::RELOAD_PLUGIN_INFOBAR_DELEGATE:
-      ReloadPluginInfoBarDelegate::Create(
-          GetInfoBarManager(), nullptr,
-          l10n_util::GetStringFUTF16(IDS_PLUGIN_CRASHED_PROMPT,
-                                     u"Test Plugin"));
-      break;
-#endif  // BUILDFLAG(ENABLE_PLUGINS)
 
     case IBD::FILE_ACCESS_DISABLED_INFOBAR_DELEGATE:
       ChromeSelectFilePolicy(GetWebContents()).SelectFileDenied();
@@ -546,12 +530,6 @@ IN_PROC_BROWSER_TEST_P(InfoBarUiTest, InvokeUi_incognito_connectability) {
 IN_PROC_BROWSER_TEST_P(InfoBarUiTest, InvokeUi_theme_installed) {
   ShowAndVerifyUi();
 }
-
-#if BUILDFLAG(ENABLE_PLUGINS)
-IN_PROC_BROWSER_TEST_P(InfoBarUiTest, InvokeUi_reload_plugin) {
-  ShowAndVerifyUi();
-}
-#endif  // BUILDFLAG(ENABLE_PLUGINS)
 
 IN_PROC_BROWSER_TEST_P(InfoBarUiTest, InvokeUi_file_access_disabled) {
   ShowAndVerifyUi();
