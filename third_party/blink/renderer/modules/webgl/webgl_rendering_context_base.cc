dc083314bbcf4c8ab6856b25e6143c165cfe1aad
Benoit <lizeb@google.com>
2026-09-09 15:12:43 -0700

    [webgl] Add a feature to discard MSAA and depth/stencil buffers in bg
    When preserveDrawingBuffer is false, the content of MSAA and
    Depth/Stencil buffers doesn't have to be preserved after a
    resolve. Currently, these buffers are never deallocated, including when
    the buffer is in background.
    
    This CL adds a feature (disabled by default) which deallocates this
    buffers when in background. Locally, on a macBook Pro 16 inch, this
    saves >300MiB of memory for a simple spinning cube
    (https://benoitlben.github.io/test-pages/webgl-cube-simple.html), as the
    MSAA buffer is not memoryless on this platform, and the Depth/Stencil
    format is 32/8, for a total cost of (1 + 5/4) * screen resolution * 4.
    
    However, this CL is not sufficien in isolation: due to laziness in
    ANGLE, the memory is actually not released even with this commit, it
    needs another change in ANGLE to eagerly apply the changes.
    
    Bug: 514593915
    Change-Id: I37606ff4939d3c3965e817b31f8063386dbe769f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8031101
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694914}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1777,9 +1777,9 @@ WebGLRenderingContextBase::ClearIfComposited(
   if (!framebuffer_binding_ ||
       (caller != kClearCallerDrawOrClear &&
        base::FeatureList::IsEnabled(features::kWebGLDiscardBackBuffer))) {
-    // EnsureBackColorBuffer() must be called before checking
-    // BufferClearNeeded() and returning early below. If the back buffer was
-    // discarded when the page was hidden, it must be recreated even if no
+    // EnsureBuffers() must be called before checking
+    // BufferClearNeeded() and returning early below. If buffers were
+    // discarded when the page was hidden, they must be recreated even if no
     // implicit clear is needed (BufferClearNeeded() is false).
     //
     // If a custom framebuffer is currently bound for a draw/clear operation,
@@ -1788,7 +1788,7 @@ WebGLRenderingContextBase::ClearIfComposited(
     //
     // Note: we don't call MarkContentsChanged(), because it hasn't (there is
     // no new content to present yet).
-    GetDrawingBuffer()->EnsureBackColorBuffer();
+    GetDrawingBuffer()->EnsureBuffers();
   }
 
   if (!GetDrawingBuffer()->BufferClearNeeded() ||
@@ -1800,7 +1800,6 @@ WebGLRenderingContextBase::ClearIfComposited(
     // Unlikely, but context was lost.
     return kSkipped;
   }
-
   ScopedPixelLocalStorageInterrupt scoped_pls_interrupt(this);
 
   // Determine if it's possible to combine the clear the user asked for and this
@@ -9374,7 +9373,15 @@ void WebGLRenderingContextBase::SetFramebuffer(GLenum target,
 }
 
 void WebGLRenderingContextBase::RestoreCurrentFramebuffer() {
-  bindFramebuffer(GL_FRAMEBUFFER, framebuffer_binding_.Get());
+  if (!framebuffer_binding_) {
+    if (GetDrawingBuffer()) {
+      // Not calling bindFrameBuffer() to avoid eager buffer reallocation.
+      GetDrawingBuffer()->RestoreDefaultFramebufferBinding(GL_FRAMEBUFFER);
+    }
+  } else {
+    ContextGL()->BindFramebuffer(GL_FRAMEBUFFER,
+                                 framebuffer_binding_->Object());
+  }
 }
 
 void WebGLRenderingContextBase::RestoreCurrentTexture2D() {
