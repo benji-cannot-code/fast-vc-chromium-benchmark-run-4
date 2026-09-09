125c7a49ca340dfc1a6556acb71f7b3f5e7af5d6
Hitarth Kothari <hitarthkothari@google.com>
2026-09-09 16:31:41 -0700

    [BottomSheetType] Migrate NoPasskeysBottomSheetContent to BottomSheetType
    Migrate NoPasskeysBottomSheetContent to declare its BottomSheetType.
    Registers the dedicated TOUCH_TO_FILL_NO_PASSKEYS constant in
    UserCriticalFeature and configures the sheet as
    USER_CRITICAL_PASSIVE_MODAL to ensure this security/passkey fallback
    dialog cannot be interrupted or suppressed.
    
    Bug: 505050661
    Change-Id: I618de529efe7c450f50fcc4d403f05c2318b8831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8321792
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694991}

--- components/browser_ui/bottomsheet/android/java/src/org/chromium/components/browser_ui/bottomsheet/UserCriticalFeature.java
+++ components/browser_ui/bottomsheet/android/java/src/org/chromium/components/browser_ui/bottomsheet/UserCriticalFeature.java
@@ -32,6 +32,7 @@ import java.lang.annotation.RetentionPolicy;
     UserCriticalFeature.TOUCH_TO_FILL_PASSWORD_MANAGER,
     UserCriticalFeature.ALL_PASSWORDS,
     UserCriticalFeature.TOUCH_TO_FILL_PASSWORD_GENERATION,
+    UserCriticalFeature.TOUCH_TO_FILL_NO_PASSKEYS,
 })
 @Retention(RetentionPolicy.SOURCE)
 public @interface UserCriticalFeature {
@@ -40,4 +41,5 @@ public @interface UserCriticalFeature {
     int TOUCH_TO_FILL_PASSWORD_MANAGER = 2;
     int ALL_PASSWORDS = 3;
     int TOUCH_TO_FILL_PASSWORD_GENERATION = 4;
+    int TOUCH_TO_FILL_NO_PASSKEYS = 5;
 }
