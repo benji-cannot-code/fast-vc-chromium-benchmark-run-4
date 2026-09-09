cf6a6853b3f42f83bde85acefaac9f0b4a02379a
Qijiang Fan <fqj@chromium.org>
2026-09-08 18:37:10 -0700

    Revert "Remove AppServiceProxyFactory use from QuickOffice look up."
    This reverts commit bb3f0c8084a42633b9ef42125ad70c640e0726f3.
    
    Reason for revert: Causes deterministic crash in All/FileTasksBrowserTest.FallbackSucceedsWithQuickOffice/Guest and All/FileTasksBrowserTest.FallbackFailsNoQuickOffice/Guest on linux-chromeos-chrome due to CHECK_DEREF on null AnnotatedAccountId in Guest profile mode.
    
    Failure Link: https://ci.chromium.org/b/8671205439245526369
    
    Original change's description:
    > Remove AppServiceProxyFactory use from QuickOffice look up.
    >
    > This CL removes the use of the KeyedServiceFactory from
    > IsQuickOfficeInstalled.
    > Instead, it starts to look up the service by using AccountId.
    > There are many exceptional Profiles that is not dedicated for the
    > user, and historically IsQuickOfficeInstalled didn't take care of it.
    >
    > With this CL, it starts look up AccountId, so, that part ensures
    > the profile is somehow corresponding to Profile.
    >
    > Bug: 40146603, 477191550
    > Test: Tryjob
    > Change-Id: If5f70e5c891f6a62a8f57f4415d09b908ee18b4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8277233
    > Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    > Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1693854}
    
    Bug: 40146603
    Bug: 477191550
    Bug: 558889486
    Bug: 40146603, 477191550
    Change-Id: I6dc8cb8d865b726b9eba9a28da979f8fb1ea8dc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8373416
    Reviewed-by: Grey Wang <greywang@google.com>
    Owners-Override: Grey Wang <greywang@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Qijiang Fan <fqj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694275}

--- chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog.cc
+++ chrome/browser/ui/webui/ash/cloud_upload/cloud_upload_dialog.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/cloud_upload/one_drive_upload_handler.h"
 #include "chrome/browser/ui/webui/ash/office_fallback/office_fallback_ui.h"
 #include "chrome/grit/generated_resources.h"
-#include "chromeos/ash/components/browser_context_helper/annotated_account_id.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/components/browser_delegate/browser_controller.h"
 #include "chromeos/ash/components/browser_delegate/browser_delegate.h"
@@ -696,13 +695,10 @@ void CloudOpenTask::OnGoogleDriveGetMetadata(
         metadata->item_id.value_or("").starts_with("local-")) {
       LOG(ERROR) << "Local item id, the file hasn't been uploaded";
       open_result = OfficeDriveOpenErrors::kWaitingForUpload;
-      if (const auto* account_id =
-              ash::AnnotatedAccountId::Get(profile_->GetOriginalProfile())) {
-        GetUserFallbackChoice(
-            *account_id, task_, file_urls_,
-            ash::office_fallback::FallbackReason::kWaitingForUpload,
-            base::DoNothing());
-      }
+      GetUserFallbackChoice(
+          profile_, task_, file_urls_,
+          ash::office_fallback::FallbackReason::kWaitingForUpload,
+          base::DoNothing());
     } else if (hosted_url.is_empty()) {
       LOG(ERROR) << "Empty URL";
       open_result = OfficeDriveOpenErrors::kEmptyAlternateUrl;
@@ -917,22 +913,12 @@ void CloudOpenTask::OpenAndroidOneDriveUrl(
         office_fallback::FallbackReason::kAndroidOneDriveUnsupportedLocation;
     // `cloud_open_metrics_` can be safely moved since CloudUploadTask is
     // expected to be destructed straight after.
-    const auto* account_id =
-        ash::AnnotatedAccountId::Get(profile_->GetOriginalProfile());
-    if (!account_id) {
-      // TODO(crbug.com/477191550): audit `profile_`'s characteristic,
-      // specifically whether this is always a user-representative-profile.
-      OnWaitingForAndroidUnsupportedPathFallbackChoiceReceived(
-          profile_, file_urls_, fallback_reason, std::move(cloud_open_metrics_),
-          std::nullopt);
-      return;
-    }
-
     GetUserFallbackChoice(
-        *account_id, task_, file_urls_, fallback_reason,
+        profile_, task_, file_urls_, fallback_reason,
         base::BindOnce(
             &OnWaitingForAndroidUnsupportedPathFallbackChoiceReceived, profile_,
             file_urls_, fallback_reason, std::move(cloud_open_metrics_)));
+
     return;
   }
   // Append relative path from Android OneDrive Url.
