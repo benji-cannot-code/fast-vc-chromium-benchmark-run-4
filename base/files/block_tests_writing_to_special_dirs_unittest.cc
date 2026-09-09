87115a17b20527a4796ac461674c1d8c5cc630c0
Travis Li <songhaoli@chromium.org>
2026-09-09 14:54:00 -0700

    Allow writing to DIR_SYSTEM_TEMP on Windows when blocking test writes.
    Update BlockTestsWritingToSpecialDirs to allow writing to
    DIR_SYSTEM_TEMP and its subdirectories on Windows, even if they reside
    under a blocked directory. This mirrors the existing behavior for
    DIR_TEMP. A unit test has been added to verify this behavior.
    
    This is a follow-up of crrev.com/c/8299932
    
    Bug: 40066315
    Change-Id: I30d8a8f39b32acc1a0a9d152d0c92a0d3dc26901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8382157
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Travis Li <songhaoli@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694899}

--- base/files/block_tests_writing_to_special_dirs_unittest.cc
+++ base/files/block_tests_writing_to_special_dirs_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/test/scoped_path_override.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
@@ -72,7 +73,7 @@ TEST_F(BlockTestsWritingToSpecialDirsTest, AllowTempDirSubdirectories) {
   }
   Get().emplace(std::move(dirs_to_block), ([](const FilePath& path) {}));
 
-  // Writing to `src_dir` outside `temp_sub_dir' should be blocked.
+  // Writing to `src_dir` outside `temp_sub_dir` should be blocked.
   EXPECT_FALSE(BlockTestsWritingToSpecialDirs::CanWriteToPath(
       src_dir.AppendASCII("other_file")));
 
@@ -82,6 +83,17 @@ TEST_F(BlockTestsWritingToSpecialDirsTest, AllowTempDirSubdirectories) {
       temp_sub_dir.AppendASCII("temp_file")));
   EXPECT_TRUE(BlockTestsWritingToSpecialDirs::CanWriteToPath(
       temp_sub_dir.AppendASCII("subdir").AppendASCII("temp_file")));
+
+#if BUILDFLAG(IS_WIN)
+  FilePath system_temp_sub_dir = src_dir.AppendASCII("system_temp_test_dir");
+  base::ScopedPathOverride system_temp_override(DIR_SYSTEM_TEMP,
+                                                system_temp_sub_dir,
+                                                /*should_skip_check=*/true);
+  EXPECT_TRUE(BlockTestsWritingToSpecialDirs::CanWriteToPath(
+      system_temp_sub_dir.AppendASCII("temp_file")));
+  EXPECT_TRUE(BlockTestsWritingToSpecialDirs::CanWriteToPath(
+      system_temp_sub_dir.AppendASCII("subdir").AppendASCII("temp_file")));
+#endif  // BUILDFLAG(IS_WIN)
 }
 
 }  // namespace base
