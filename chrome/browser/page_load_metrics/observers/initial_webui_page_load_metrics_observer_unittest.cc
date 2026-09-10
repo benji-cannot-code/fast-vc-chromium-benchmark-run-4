248f7fc76e8ab23b50720515e51e1c4052c7c8a1
Ming-Ying Chung <mych@chromium.org>
2026-09-09 21:04:06 -0700

    [webui] Add TestLayoutProvider to InitialWebUI metrics observer test
    When kWebUIInProcessResourceLoadingV2 is enabled, WebUI navigation to
    `chrome://webui-toolbar.top-chrome/` triggers
    WebUIToolbarUI::PopulateLocalResourceLoaderConfig(), which generates
    layout constants CSS via WebUIToolbarLayoutCssHelper. This helper
    queries views::TypographyProvider::Get(), which assumes a valid
    views::LayoutProvider singleton.
    
    In unit tests running under ChromeRenderViewHostTestHarness, no layout
    provider is initialized by default, causing a null pointer dereference
    and segfault in views::TypographyProvider::Get().
    
    This CL adds such missing views::test::TestLayoutProvider to the
    fixture and adds
    
    Bug: 459528908
    Change-Id: I5f70a7bdd747623ac55e9d67bc8c8bc571b7d482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8377141
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695158}

--- chrome/browser/page_load_metrics/observers/initial_webui_page_load_metrics_observer_unittest.cc
+++ chrome/browser/page_load_metrics/observers/initial_webui_page_load_metrics_observer_unittest.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/base/unowned_user_data/unowned_user_data_host.h"
+#include "ui/views/test/test_layout_provider.h"
 
 using content::NavigationSimulator;
 using page_load_metrics::PageLoadMetricsObserverTester;
@@ -105,6 +106,7 @@ class InitialWebUIPageLoadMetricsObserverTest
   ukm::TestAutoSetUkmRecorder test_ukm_recorder_;
 
  private:
+  views::test::TestLayoutProvider layout_provider_;
   base::test::ScopedFeatureList scoped_feature_list_;
   variations::test::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
       variations::VariationsIdsProvider::Mode::kUseSignedInState};
