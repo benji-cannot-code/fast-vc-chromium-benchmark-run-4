c211eb359b99e76ab8954686b1b3aaa7281ef647
Juliet Levesque <julietlevesque@google.com>
2026-09-09 20:42:10 -0700

    [loomnibox] Add disk file size and error scrim in Composebox
    - Pre-checks file size on disk with base::GetFileSize before reading
      file bytes into memory.
    - Guards against sending oversized Base64 strings across Mojo IPC when
      validation fails or max file size is exceeded.
    - Imports and renders <ntp-error-scrim> in Omnibox Everywhere Composebox
      to display validation errors and allow dismissal (also handles
      too many files)
    
    Bug: b:546605465, b:556718970
    Change-Id: I579b087a478724ebb3523f0f32da6b9e23a85146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8283473
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: James Leung <jamesleung@google.com>
    Commit-Queue: Juliet Knighton <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695141}

--- chrome/browser/ui/views/location_bar/omnibox_popup_file_selector.h
+++ chrome/browser/ui/views/location_bar/omnibox_popup_file_selector.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_OMNIBOX_POPUP_FILE_SELECTOR_H_
 #define CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_OMNIBOX_POPUP_FILE_SELECTOR_H_
 
+#include <optional>
+
+#include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/types/expected.h"
@@ -26,18 +29,18 @@ class UnguessableToken;
 }
 
 class OmniboxEditModel;
-
-// Struct to store file data and mime type.
-struct FileData {
-  std::string bytes;
-  std::string mime_type;
-  std::string name;
-};
-
 class OmniboxPopupDeactivationBlocker;
 
 class OmniboxPopupFileSelector : public ui::SelectFileDialog::Listener {
  public:
+  // Struct to store file data and mime type.
+  struct FileData {
+    std::string bytes;
+    std::string mime_type;
+    base::FilePath path;
+    std::optional<contextual_search::ContextUploadErrorType> error;
+  };
+
   // `owning_window` is the window that will be used to show the file selector
   // dialog.
   explicit OmniboxPopupFileSelector(gfx::NativeWindow owning_window);
