810bd0aabf01e38425a04a602da224f6da13e7ef
Sam Fortiner <samfort@microsoft.com>
2026-09-10 11:27:17 -0700

    Move overlap state into HitTestDataBuilder
    Overlap bookkeeping describes the state of a single hit test data build.
    Keeping it as local function state makes the traversal's invariants
    harder to follow and complicates extracting individual decisions into
    helpers.
    
    Store this bookkeeping in the single-use builder class. This preserves
    existing overlap behavior while simplifying the traversal ahead of the
    geometry changes needed for rounded corners.
    
    Bug: 40572334
    Change-Id: Ie2444d105277839d6ce06309ba2bc6e98d205439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8287926
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1695524}

--- cc/trees/hit_test_data_builder.h
+++ cc/trees/hit_test_data_builder.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/memory/raw_ref.h"
+#include "cc/base/region.h"
 
 namespace viz {
 struct HitTestRegionList;
@@ -16,7 +17,9 @@ struct HitTestRegionList;
 
 namespace cc {
 
+class LayerImpl;
 class LayerTreeImpl;
+class SurfaceLayerImpl;
 
 class HitTestDataBuilder {
  public:
@@ -25,7 +28,17 @@ class HitTestDataBuilder {
   std::optional<viz::HitTestRegionList> Build() &&;
 
  private:
+  void TrackHitTestableNonSurfaceLayer(const LayerImpl* layer);
+  void TrackNonEmittedSurface(const SurfaceLayerImpl* surface_layer);
+  bool IsSurfaceOverlapped(const SurfaceLayerImpl* surface_layer) const;
+  bool ShouldAssumeOverlap() const;
+
   const raw_ref<const LayerTreeImpl> active_tree_;
+
+  // TODO(sunxd): Submit all overlapping layer bounds as hit test regions. Also,
+  // investigate if we can use visible layer rect as overlapping regions.
+  Region overlapping_region_;
+  size_t num_hit_testable_non_surface_layers_ = 0;
 };
 
 }  // namespace cc
