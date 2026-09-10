8ed874914c9ce6e9876f8826d1b27418605ef95c
Charles Cai <charlesyc@google.com>
2026-09-10 15:17:49 -0700

    [Drive Integration] Add Drive picker client abstraction and models
    Add upstream abstractions, data models, and an icon utility to support
    Google Drive file attachments in Fusebox on Android.
    
    Bug: 548629516
    Change-Id: I851d071489f17301497302e599a4f6a49eedb9fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8390619
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Charles Cai <charlesyc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695708}

---
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/DriveAttachmentMetadata.java
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.omnibox.fusebox;
+
+import org.chromium.build.annotations.NullMarked;
+
+/** Holds metadata for a Drive file attached to a contextual search query. */
+@NullMarked
+public class DriveAttachmentMetadata {
+    public final String driveId;
+    public final String title;
+    public final String mimeType;
+
+    public DriveAttachmentMetadata(String driveId, String title, String mimeType) {
+        this.driveId = driveId;
+        this.title = title;
+        this.mimeType = mimeType;
+    }
+}
