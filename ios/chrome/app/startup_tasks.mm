756919412d14aa7975aa8a7f20cf120a719ba622
Sylvain Defresne <sdefresne@chromium.org>
2026-09-10 05:45:17 -0700

    [ios] Uniformize OmahaService initialisation
    Introduce a new callback used to create the SharedURLLoaderFactory
    to uniformize its initialization between production code and unit
    tests.
    
    Using network::PendingSharedURLLoaderFactory is not possible in the
    tests as even the network::WeakWrapperSharedURLLoaderFactory class
    will create a new SharedURLLoaderFactory and capturing the requests
    is not possible.
    
    In production a PendingSharedURLLoaderFactory will be owned by the
    callback and used via network::SharedURLLoaderFactory::Create() but
    in tests it will be possible to return an existing instance.
    
    Pass both the new callback and the UpgradeRecommendedCallback to the
    StartInternal() method and remove the helper methods used to set the
    corresponding properties.
    
    Bug: 555698908
    Change-Id: Ib0074e3b2485152f2deda02077e1432497d1dfaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380776
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695376}

--- ios/chrome/app/startup_tasks.mm
+++ ios/chrome/app/startup_tasks.mm
@@ -32,9 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Public methods.
 
 - (void)initializeOmaha {
-  OmahaService::Start(
-      GetApplicationContext()->GetSharedURLLoaderFactory()->Clone(),
-      base::DoNothing());
+  OmahaService::Start(GetApplicationContext()->GetSharedURLLoaderFactory(),
+                      base::DoNothing());
 }
 
 - (void)registerForApplicationWillResignActiveNotification {
