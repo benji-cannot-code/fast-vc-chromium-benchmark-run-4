f39e3ac670139e437f90552f29072739d1c051ba
Di Wu <diwux@google.com>
2026-09-10 01:52:14 -0700

    Ash: Migrate SyncAppsyncServiceTest off RunUntilIdle
    This changelist is part of an ongoing series to modernize Ash tests by
    migrating them away from RunUntilIdle() and toward targeted
    synchronization with APIs such as RunUntil(), TestFuture, RunLoop
    callbacks, observer waiters, or exact mock-time advancement.
    
    SyncAppsyncServiceTest shuts down the service synchronously. Its mock
    has no signed-in account, so initialization does not schedule an opt-in
    file write either. Remove the idle drain and the unused task
    environment; there is no asynchronous work for this shutdown smoke test
    to wait for.
    
    Bug: b:506794349
    Test: CQ
    Change-Id: Ib903a6388f081f59a595433c32acae3d4065c93e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8366328
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695280}

--- chrome/browser/ash/sync/sync_appsync_service_unittest.cc
+++ chrome/browser/ash/sync/sync_appsync_service_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/sync/sync_appsync_service.h"
 #include <memory>
 
-#include "base/test/task_environment.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "components/sync/test/mock_sync_service.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -31,11 +30,7 @@ class SyncAppsyncServiceTest : public testing::Test {
     return sync_appsync_service_.get();
   }
 
-  void RunAllPendingTasks() { task_environment_.RunUntilIdle(); }
-
  private:
-  base::test::SingleThreadTaskEnvironment task_environment_;
-
   testing::NiceMock<syncer::MockSyncService> sync_service_;
   FakeChromeUserManager user_manager_;
   std::unique_ptr<SyncAppsyncService> sync_appsync_service_;
@@ -43,7 +38,6 @@ class SyncAppsyncServiceTest : public testing::Test {
 
 TEST_F(SyncAppsyncServiceTest, DoesNotCrash) {
   sync_appsync_service()->Shutdown();
-  RunAllPendingTasks();
 }
 
 }  // namespace
