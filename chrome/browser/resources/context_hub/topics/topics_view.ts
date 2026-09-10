88c4d0a9b5c319083e0e265f1bddd6386c4d91bf
Saeid Eid <saeideid@google.com>
2026-09-10 01:12:56 -0700

    [ContextHub] Add card and view presentation components for topics
    Add presentation components and styles for topic cards and list view
    within Context Hub.
    
    TAG=agy
    CONV=d10ae520-3c28-4ccf-9cfa-19c168eafeb4
    
    Bug: 558574032
    Change-Id: I688b60b13fdca4971776b7161246c82e595d4883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8378143
    Reviewed-by: Isabelle Goralsky <igoralsky@google.com>
    Commit-Queue: Saeid Eid <saeideid@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695249}

--- chrome/browser/resources/context_hub/topics/topics_view.ts
+++ chrome/browser/resources/context_hub/topics/topics_view.ts
@@ -3,11 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './topic_card.js';
+
+import {OpenWindowProxyImpl} from '//resources/js/open_window_proxy.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
+import type {TopicItem} from './topic_card.js';
 import {getCss} from './topics_view.css.js';
 import {getHtml} from './topics_view.html.js';
 
+export type {TopicItem} from './topic_card.js';
+
+// TODO(crbug.com/558572977): Use internationalized strings once GRD strings are added.
 export class TopicsViewElement extends CrLitElement {
   static get is() {
     return 'topics-view';
@@ -19,6 +26,26 @@ export class TopicsViewElement extends CrLitElement {
 
   override render() {
     return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      topics: {type: Array},
+    };
+  }
+
+  accessor topics: TopicItem[] = [];
+
+  override connectedCallback() {
+    super.connectedCallback();
+    // TODO(crbug.com/558572977): Query topics from HistoryService KeyedService.
+  }
+
+  protected onJumpBackIn_(e: CustomEvent<{topic: TopicItem}>) {
+    const destinationUrl = e.detail.topic.destinationUrl;
+    if (destinationUrl) {
+      OpenWindowProxyImpl.getInstance().openUrl(destinationUrl);
+    }
   }
 }
 
