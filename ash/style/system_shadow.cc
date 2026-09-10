7f7af58a79c0b73ff2f92053069ad9078072fa7c
Zoraiz Naeem <zoraiznaeem@google.com>
2026-09-10 16:56:34 -0700

    ui::Shadow: Encapsulate internal shadow layers to prevent misuse
    Make ui::Shadow::shadow_layer() and ui::Shadow::fading_layer() private.
    This encapsulation prevents unintended misuse of the internal nine-patch
    layers and clarifies that clients should instead set layer properties,
    such as opacity or visibility, on the root container layer
    (ui::Shadow::layer()).
    
    TAG=agy
    CONV=00aef9d1-7d98-4d4b-b859-4baf6c41bf3e
    
    Bug: b:526669762
    Change-Id: I0db7083170c1f7a5fa94ffae31cfdcdd9b9fbc5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8361193
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695801}

--- ash/style/system_shadow.cc
+++ ash/style/system_shadow.cc
@@ -213,10 +213,6 @@ ui::Layer* SystemShadow::GetLayer() {
   return shadow()->layer();
 }
 
-ui::LayerNinePatch* SystemShadow::GetNinePatchLayer() {
-  return shadow()->shadow_layer();
-}
-
 void SystemShadow::ObserveColorProviderSource(
     ui::ColorProviderSource* color_provider_source) {
   Observe(color_provider_source);
