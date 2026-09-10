b6a790b5cfa692ec62acb0b70b6ce592b81aff3d
Kenneth Russell <kbr@chromium.org>
2026-09-10 16:46:43 -0700

    Implicitly disable PLS during generateMipmap.
    Mipmap generation could involve many operations such as CPU readbacks
    or implicit draw calls which could interfere with PLS's operation. To
    avoid potential implementation-dependent side effects, implicitly
    disable PLS upon calls to generateMipmap.
    
    Bug: angleproject:40096838
    Change-Id: I41eb55eb99b02120504e128c5407e2cd2ce5f1e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392717
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695793}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -3654,6 +3654,7 @@ void WebGLRenderingContextBase::frontFace(GLenum mode) {
 void WebGLRenderingContextBase::generateMipmap(GLenum target) {
   if (isContextLost())
     return;
+  MaybeEndPixelLocalStorageImplicit();
   if (!ValidateTextureBinding("generateMipmap", target))
     return;
   ContextGL()->GenerateMipmap(target);
