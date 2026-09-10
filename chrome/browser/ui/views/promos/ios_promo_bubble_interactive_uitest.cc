76f6e95c2bb91e3cf770ad5b4ae903c7b2b24843
Olivia Saul <osaul@google.com>
2026-09-09 17:52:18 -0700

    [AF] Clean up AutofillEnableWalletBranding (v1) flag, 3/?
    Cleanup initially existed as crrev.com/c/8156467, but stalled out due to
    being 77 files and nearly 2000 LoC.  This CL is one of many that's
    cleaning it up piece by piece.
    
    Bug: 468139053, 509845707
    Change-Id: I3028f67984cae42613fa4556ccbadaddf43b9c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349527
    Commit-Queue: Olivia Saul <osaul@google.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695055}

--- chrome/browser/ui/views/promos/ios_promo_bubble_interactive_uitest.cc
+++ chrome/browser/ui/views/promos/ios_promo_bubble_interactive_uitest.cc
@@ -52,8 +52,6 @@ class IOSPromoBubbleBrowserTest
 
     if (IsWalletBrandingV2Enabled()) {
       enabled_features.push_back(
-          {autofill::features::kAutofillEnableWalletBranding, {}});
-      enabled_features.push_back(
           {autofill::features::kAutofillEnableWalletBrandingV2, {}});
     } else {
       disabled_features.emplace_back(
