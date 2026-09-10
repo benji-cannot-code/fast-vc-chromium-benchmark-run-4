ade025956c1e19269c6590d27faa2fba0b853edc
Brandon Fong <bjfong@google.com>
2026-09-10 13:57:29 -0700

    [tips] Update branching logic for linking up DetermineBestTip
    Update branching logic in tips_agent_android.cc to use the self service
    logic and factory call to DetermineBestTip.
    
    Bug: 523360691
    Change-Id: I7cccbd9983e5df47f5ff59f2dbd30ebf75277de1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8378569
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695645}

--- chrome/browser/notifications/scheduler/tips_agent_android.h
+++ chrome/browser/notifications/scheduler/tips_agent_android.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_TIPS_AGENT_ANDROID_H_
 #define CHROME_BROWSER_NOTIFICATIONS_SCHEDULER_TIPS_AGENT_ANDROID_H_
 
+#include <optional>
+
 #include "base/android/jni_android.h"
 #include "chrome/browser/notifications/scheduler/public/notification_scheduler_types.h"
 #include "chrome/browser/notifications/scheduler/public/tips_agent.h"
@@ -39,6 +41,10 @@ class TipsAgentAndroid : public notifications::TipsAgent {
       bool is_bottom_omnibox,
       notifications::NotificationScheduleService* service,
       notifications::ClientOverview overview);
+
+  static void OnBestTipChosen(
+      notifications::NotificationScheduleService* service,
+      std::optional<tips::TipsNotificationsFeatureType> best_tip);
 
   TipsAgentAndroid();
   ~TipsAgentAndroid() override;
--- chrome/browser/tips/core/tips_service.h
+++ chrome/browser/tips/core/tips_service.h
@@ -49,7 +49,7 @@ class TipsService : public KeyedService, public base::SupportsUserData {
 
   // Queries database signals for all registered features via the segmentation
   // platform and determines the best eligible tip to display.
-  void DetermineBestTip(OnBestTipChosen callback);
+  virtual void DetermineBestTip(OnBestTipChosen callback);
 
  private:
   void OnFeaturesProcessed(
