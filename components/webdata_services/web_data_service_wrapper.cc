9a6394d3fea64f4ca67603f22385d0728b040ce1
Wilson Low <wilsonlow@google.com>
2026-09-09 18:09:08 -0700

    [Direct Offers] Deprecate Autofill Wallet Offer sync bridge
    Removes the creation of the Autofill Wallet Offer sync bridge and its
    data type controller. GPay promo code offers and card-linked offers are
    deprecated, and Wallet direct offers will be ingested via
    ValuableSyncBridge instead.
    
    This CL:
    - Removes CreateAutofillWalletOfferDataTypeController from
      CommonControllerBuilder and removes AUTOFILL_WALLET_OFFER registration.
    - Removes InitWalletOfferSyncBridgeOnDBSequence and its initialization
      calls in WebDataServiceWrapper.
    - Removes CreateForWebDataServiceAndBackend and FromWebDataService
      factory methods from AutofillWalletOfferSyncBridge.
    - Updates SyncServiceFactory tests on Desktop and iOS to not expect
      AUTOFILL_WALLET_OFFER.
    
    Design: go/direct-offers-autofill-chrome-design
    
    Bug: 546252995, 548660097
    Change-Id: Ia61511d77f12652f1938cecfe5d700c6dffe4203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349589
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Wilson Low <wilsonlow@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695071}

--- components/webdata_services/web_data_service_wrapper.cc
+++ components/webdata_services/web_data_service_wrapper.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/browser/webdata/payments/autofill_wallet_credential_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/payments/autofill_wallet_metadata_sync_bridge.h"
-#include "components/autofill/core/browser/webdata/payments/autofill_wallet_offer_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/payments/autofill_wallet_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/payments/autofill_wallet_usage_data_sync_bridge.h"
 #include "components/autofill/core/browser/webdata/payments/payments_autofill_table.h"
@@ -79,15 +78,6 @@ void InitWalletSyncBridgesOnDBSequence(
       app_locale, autofill_backend, autofill_web_data.get());
   autofill::AutofillWalletMetadataSyncBridge::CreateForWebDataServiceAndBackend(
       app_locale, autofill_backend, autofill_web_data.get());
-}
-
-void InitWalletOfferSyncBridgeOnDBSequence(
-    scoped_refptr<base::SequencedTaskRunner> db_task_runner,
-    const scoped_refptr<autofill::AutofillWebDataService>& autofill_web_data,
-    autofill::AutofillWebDataBackend* autofill_backend) {
-  DCHECK(db_task_runner->RunsTasksInCurrentSequence());
-  autofill::AutofillWalletOfferSyncBridge::CreateForWebDataServiceAndBackend(
-      autofill_backend, autofill_web_data.get());
 }
 
 void InitValuableSyncBridgeOnDBSequence(
@@ -202,9 +192,6 @@ WebDataServiceWrapper::WebDataServiceWrapper(
   profile_autofill_web_data_->GetAutofillBackend(
       base::BindOnce(&InitWalletSyncBridgesOnDBSequence, db_task_runner,
                      profile_autofill_web_data_, application_locale));
-  profile_autofill_web_data_->GetAutofillBackend(
-      base::BindOnce(&InitWalletOfferSyncBridgeOnDBSequence, db_task_runner,
-                     profile_autofill_web_data_));
 #if !BUILDFLAG(IS_IOS)
   profile_autofill_web_data_->GetAutofillBackend(
       base::BindOnce(&InitWalletUsageDataSyncBridgeOnDBSequence, db_task_runner,
@@ -250,9 +237,6 @@ WebDataServiceWrapper::WebDataServiceWrapper(
   account_autofill_web_data_->GetAutofillBackend(
       base::BindOnce(&InitWalletSyncBridgesOnDBSequence, db_task_runner,
                      account_autofill_web_data_, application_locale));
-  account_autofill_web_data_->GetAutofillBackend(
-      base::BindOnce(&InitWalletOfferSyncBridgeOnDBSequence, db_task_runner,
-                     account_autofill_web_data_));
 #if !BUILDFLAG(IS_IOS)
   account_autofill_web_data_->GetAutofillBackend(
       base::BindOnce(&InitWalletUsageDataSyncBridgeOnDBSequence, db_task_runner,
