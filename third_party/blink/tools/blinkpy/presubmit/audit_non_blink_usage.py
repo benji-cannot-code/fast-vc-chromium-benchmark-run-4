bdcc63439ef4b6788766f3bc2c540b0a3b9b7b07
David Awogbemila <awogbemila@chromium.org>
2026-09-10 12:51:45 -0700

    Sync rails_mode between mojom.blink.ScrollData and WebGestureEvent
    Background: We're moving[1] scroll railing application from the browser
    to the renderer.
    
    This CL completes the rails_mode plumbing by adding rails_mode to
    blink.mojom.ScrollData along with methods to convert it to and from
    GestureScrollBegin and GestureScrollUpdate WebGestureEvents.
    
    [1] https://chromium-review.git.corp.google.com/c/chromium/src/+/8281804
    
    Bug: 535432422, 545145490
    Change-Id: I343e3ec32e324408b7a8acc92e347fc99795a8a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239932
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: David A <awogbemila@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695586}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -436,6 +436,7 @@ _CONFIG = [
     },
     {
         'paths': [
+            'third_party/blink/common/input/',
             'third_party/blink/public/common/input/',
         ],
         'allowed': [
