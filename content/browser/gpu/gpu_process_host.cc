50c323d684ca662a295358904de61f11a0104471
Bryan Oltman <bryanoltman@google.com>
2026-09-10 16:39:08 -0700

    [macOS] Disclaim TCC responsibility for GPU process
    When Chromium has a parent process other than launchd (i.e., the Mach-O
    executable was launched directly instead of via Finder or the `open`
    command), it inherits the TCC permissions of that parent process. These
    permissions extend to child processes launched by Chromium, which is not
    desirable for the GPU process.
    
    This change is gated by the kMacDisclaimGpuTccResponsibility killswitch.
    
    Bug: 507596239
    Change-Id: I8b1e1aecfdb677a0f7958edaed68d01f222561d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8373722
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695786}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -130,6 +130,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_MAC)
+#include "base/process/process_info.h"
 #include "content/browser/gpu/browser_child_process_backgrounded_bridge.h"
 #include "content/browser/gpu/ca_transaction_gpu_coordinator.h"
 #endif
@@ -157,6 +158,13 @@ static_assert(RESULT_CODE_HUNG == static_cast<int>(gpu::RESULT_CODE_HUNG),
               "Please use the same enum value in both header files.");
 
 namespace {
+
+#if BUILDFLAG(IS_MAC)
+// If enabled, the GPU process will not inherit the browser process's TCC
+// responsibilities (https://crbug.com/507596239).
+BASE_FEATURE(kMacDisclaimGpuTccResponsibility,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 // UMA histogram names.
 constexpr char kFallbackEventCause[] = "GPU.FallbackEventCause";
@@ -471,6 +479,15 @@ class GpuSandboxedProcessLauncherDelegate
     return GetUnsandboxedZygote();
   }
 #endif  // BUILDFLAG(USE_ZYGOTE)
+
+#if BUILDFLAG(IS_MAC)
+  bool DisclaimResponsibility() override {
+    // Disclaim the GPU process if we've inherited TCC responsibilities from a
+    // different (possibly privileged) process.
+    return base::FeatureList::IsEnabled(kMacDisclaimGpuTccResponsibility) &&
+           !base::IsCurrentProcessSelfResponsible();
+  }
+#endif
 
   sandbox::mojom::Sandbox GetSandboxType() override {
     if (cmd_line_.HasSwitch(sandbox::policy::switches::kDisableGpuSandbox)) {
