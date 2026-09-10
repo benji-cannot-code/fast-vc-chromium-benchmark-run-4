fe7f3cfe527b8182e589fbaabd873ef79a4c7bb6
Chase Hartsell <chazzy@google.com>
2026-09-10 12:55:34 -0700

    [organizer] Create stacked favicons element
    Adds an element for rendering two favicons in either a horizontal or
    vertical stack. This will be used by tab-related sections for
    displaying split view entries.
    
    Horizontal: http://screen/3r6sdouirknpg
    Vertical: http://screen/4ov5dh2ekoueo
    
    Bug: 556227329
    Change-Id: Ibbae5b0af60038c2ce09f19187b1328c581a1d8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8376753
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695590}

---
+++ chrome/browser/resources/organizer_panel/stacked_favicons.html.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {StackedFaviconsElement} from './stacked_favicons.js';
+
+export function getHtml(this: StackedFaviconsElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<div id="favicons" class="favicons">
+  <div id="firstFavicon" class="favicon"
+      .style="background-image: ${this.getFavicon_(this.url)};">
+  </div>
+  <div id="secondFavicon" class="favicon"
+      .style="background-image: ${this.getFavicon_(this.secondaryUrl)};">
+  </div>
+</div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
