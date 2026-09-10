a5b8dc265731384c15cf4ce6de0682d8903b06cb
Josh Karlin <jkarlin@chromium.org>
2026-09-10 12:35:17 -0700

    Plumb `script_injector_id` from Blink to NavigationHandle
    This CL plumbs the script injector id that initiated the client-side
    navigation (empty string if none) to NavigationHandle.
    
    This id will is used in a follow-up CL by a navigation throttle that
    will monitor unwanted client-side redirects from injected script.
    
    Bug: 522344717
    Change-Id: I44748f51b77bbe0ddf54136f2e2c82d7e932f374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8315868
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695574}

--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-blink.h"
 #include "third_party/blink/public/mojom/frame/frame.mojom-blink.h"
 #include "third_party/blink/public/mojom/loader/request_context_frame_type.mojom-blink.h"
+#include "third_party/blink/public/mojom/navigation/navigation_params.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_network_provider.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_content_settings_client.h"
@@ -69,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_navigation_type.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_controller.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
+#include "third_party/blink/renderer/core/ad_tracker/extension_script_tracker.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_init.h"
 #include "third_party/blink/renderer/core/dom/element.h"
@@ -707,6 +709,14 @@ void FrameLoader::StartNavigation(FrameLoadRequest& request,
     element->CancelPendingLazyLoad();
 
   ResourceRequest& resource_request = request.GetResourceRequest();
+  String script_injector_host;
+  if (GetDocumentLoader() &&
+      GetDocumentLoader()->GetScriptInjectionPolicy() ==
+          mojom::blink::ScriptInjectionPolicy::kNavigationProtection) {
+    if (auto* extension_script_tracker = frame_->GetExtensionScriptTracker()) {
+      script_injector_host = extension_script_tracker->ExtensionScriptInStack();
+    }
+  }
   const KURL& url = resource_request.Url();
   LocalDOMWindow* origin_window = request.GetOriginWindow();
 
@@ -1021,7 +1031,7 @@ void FrameLoader::StartNavigation(FrameLoadRequest& request,
       request.IsContainerInitiated(),
       request.GetWindowFeatures().explicit_opener,
       request.TakeResumeDeferredCommitListener(),
-      request.GetScriptToolInvocationId());
+      request.GetScriptToolInvocationId(), script_injector_host);
 }
 
 static void FillStaticResponseIfNeeded(WebNavigationParams* params,
