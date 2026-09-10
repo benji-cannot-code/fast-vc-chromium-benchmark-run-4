61601dce9cf8d06826b38ceef06d5bbc01e691fd
Travis Li <songhaoli@chromium.org>
2026-09-09 18:06:54 -0700

    [Pinning Metric] Add Windows.IsPinnedToTaskbar3 metric
    Implements the Windows.IsPinnedToTaskbar3 metric using IPinnedList3 to
    query the taskbar pinning state of Chrome (chrome.exe). Key changes:
    
    - Uses IPinnedList3::IsPinned to directly query official OS taskbar
    status.
    - Evaluates on a COM STA background runner (base::ThreadPool) with
    early-exit optimization once a pinned Chrome shortcut is found.
    - Added an enabled-by-default feature (kWindowsIsPinnedToTaskbar3) as
    a Finch kill-switch.
    - Unit tests added in shell_integration_win_unittest.cc.
    
    Bug: b:549076078
    Change-Id: I3825494736b1bb6abd3945a71c5472944d7c8606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8275781
    Commit-Queue: Travis Li <songhaoli@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695068}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
+#include "base/feature_list.h"
 #include "base/scoped_observation.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
@@ -46,6 +47,12 @@ class SamplingMetricsProvider;
 namespace chrome {
 void AddMetricsExtraParts(ChromeBrowserMainParts* main_parts);
 }
+
+#if BUILDFLAG(IS_WIN)
+namespace features {
+BASE_DECLARE_FEATURE(kWindowsIsPinnedToTaskbar3);
+}  // namespace features
+#endif  // BUILDFLAG(IS_WIN)
 
 class ChromeBrowserMainExtraPartsMetrics : public ChromeBrowserMainExtraParts,
                                            public display::DisplayObserver {
