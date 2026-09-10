8696f978e49bd809fabdb069264cb6d7437807cd
Satyaveer Singh <satyaveer.s@samsung.com>
2026-09-10 12:23:04 -0700

    Fix IWYU issue
    This CL correctly includes/removes header to satisfy IWYU
    guidenline. It is a follow-up to address IWYU issues
    introduced in 4c6fe6abeb12a431882b502e3
    
    Bug: 395677330
    Change-Id: I15b1af30daf96a4d82974a6cee11b21dd55ce25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8388356
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695561}

--- chrome/browser/devtools/devtools_http_service_handler_unittest.cc
+++ chrome/browser/devtools/devtools_http_service_handler_unittest.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/devtools/devtools_http_service_handler.h"
 
-#include "base/memory/ptr_util.h"
+#include <memory>
+
 #include "base/test/bind.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/devtools/devtools_dispatch_http_request_params.h"
--- chrome/browser/policy/default_geolocation_policy_handler_unittest.cc
+++ chrome/browser/policy/default_geolocation_policy_handler_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/policy/default_geolocation_policy_handler.h"
 
+#include <memory>
+
 #include "ash/constants/ash_features.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/values.h"
