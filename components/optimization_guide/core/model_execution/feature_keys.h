c0400aec82eab5b5b45bb59bfe54ab73ca6df416
Gary Klassen <gklassen@chromium.org>
2026-09-09 18:01:04 -0700

    Register TTC model execution capability and add ttc.proto
    This CL registers ModelBasedCapabilityKey::kTtc and MODEL_EXECUTION_FEATURE_TTC
    in the Optimization Guide feature registry, adds features/ttc.proto to
    optimization_guide_proto, and configures OAuth token authorization.
    
    Bug: none
    Change-Id: I88a8d0f19cfafe8feec5ee9df73950ef550f28e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8312237
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Zekun Jiang <zekunjiang@google.com>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695063}

--- components/optimization_guide/core/model_execution/feature_keys.h
+++ components/optimization_guide/core/model_execution/feature_keys.h
@@ -64,6 +64,7 @@ enum class ModelBasedCapabilityKey {
       MODEL_EXECUTION_FEATURE_READ_ALOUD_GENERATE_TEXT,
   kReadAloudSynthesize = proto::ModelExecutionFeature::
       MODEL_EXECUTION_FEATURE_READ_ALOUD_SYNTHESIZE,
+  kTtc = proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_TTC,
 };
 
 inline std::ostream& operator<<(std::ostream& out,
@@ -119,6 +120,8 @@ inline std::ostream& operator<<(std::ostream& out,
       return out << "ReadAloudGenerateText";
     case ModelBasedCapabilityKey::kReadAloudSynthesize:
       return out << "ReadAloudSynthesize";
+    case ModelBasedCapabilityKey::kTtc:
+      return out << "Ttc";
   }
   return out;
 }
@@ -237,6 +240,8 @@ inline proto::ModelExecutionFeature ToModelExecutionFeatureProto(
     case ModelBasedCapabilityKey::kReadAloudSynthesize:
       return proto::ModelExecutionFeature::
           MODEL_EXECUTION_FEATURE_READ_ALOUD_SYNTHESIZE;
+    case ModelBasedCapabilityKey::kTtc:
+      return proto::ModelExecutionFeature::MODEL_EXECUTION_FEATURE_TTC;
   }
 }
 
