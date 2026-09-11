95b92d1db38825b7e18613c097ff32a36d163631
Kent Tamura <tkent@chromium.org>
2026-09-11 01:44:29 -0700

    WTF: Rename non-type template parameters to kCamelCase
    https://google.github.io/styleguide/cppguide.html#Template_Parameter_Names
    > non-type template parameters should follow the rules for naming
    > variables or constants.
    
    This CL updates non-type template parameter names in platform/fonts/
    and platform/text/ to kCamelCase.  It's better than snake_case because
    these parameters are compile-time constants.
    
    This CL has no behavior changes.
    
    Bug: 492927412
    Change-Id: I0db8e7d97cc731db2342c7a804d26fcfff72bccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394846
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696038}

--- third_party/blink/renderer/platform/fonts/shaping/glyph_bounds_accumulator.h
+++ third_party/blink/renderer/platform/fonts/shaping/glyph_bounds_accumulator.h
@@ -51,7 +51,7 @@ namespace blink {
 // coordinate, while ShapeResult::glyph_bounding_box_ is in logical coordinate.
 // To minimize the number of conversions, this class accumulates the bounding
 // boxes in physical coordinate, and convert the accumulated box to logical.
-template <bool is_horizontal_run>
+template <bool kIsHorizontalRun>
 struct GlyphBoundsAccumulator {
   // The accumulated glyph bounding box in physical coordinate, until
   // ConvertVerticalRunToLogicalIfNeeded().
@@ -75,7 +75,7 @@ struct GlyphBoundsAccumulator {
     // Glyphs are drawn at |origin + offset|. Move glyph_bounds to that point.
     // All positions in hb_glyph_position_t are relative to the current point.
     // https://behdad.github.io/harfbuzz/harfbuzz-Buffers.html#hb-glyph-position-t-struct
-    if constexpr (is_horizontal_run) {
+    if constexpr (kIsHorizontalRun) {
       bounds_for_glyph.set_x(bounds_for_glyph.x() + origin);
     } else {
       bounds_for_glyph.set_y(bounds_for_glyph.y() + origin);
@@ -104,7 +104,7 @@ struct GlyphBoundsAccumulator {
   // Convert vertical run glyph bounding box to logical. Horizontal runs do not
   // need conversions because physical and logical are the same.
   void ConvertVerticalRunToLogicalIfNeeded(const FontMetrics& font_metrics) {
-    if constexpr (is_horizontal_run) {
+    if constexpr (kIsHorizontalRun) {
       return;
     }
     // Convert physical glyph_bounding_box to logical.
@@ -130,7 +130,7 @@ struct GlyphBoundsAccumulator {
 
 #if defined(USE_SIMD_FOR_COMPUTING_GLYPH_BOUNDS)
 
-template <bool is_horizontal_run>
+template <bool kIsHorizontalRun>
 class VectorizedGlyphBoundsAccumulator final {
  public:
   static constexpr size_t kStride = 4;
@@ -191,7 +191,7 @@ class VectorizedGlyphBoundsAccumulator final {
     auto x_offsets_v = hw::Load(tag, x_offsets.data());
     auto y_offsets_v = hw::Load(tag, y_offsets.data());
 
-    if constexpr (is_horizontal_run) {
+    if constexpr (kIsHorizontalRun) {
       x_mins_v = x_mins_v + origins_v + x_offsets_v;
       y_mins_v = y_mins_v + y_offsets_v;
     } else {
@@ -238,7 +238,7 @@ class VectorizedGlyphBoundsAccumulator final {
     // Glyphs are drawn at |origin + offset|. Move glyph_bounds to that point.
     // All positions in hb_glyph_position_t are relative to the current point.
     // https://behdad.github.io/harfbuzz/harfbuzz-Buffers.html#hb-glyph-position-t-struct
-    if constexpr (is_horizontal_run) {
+    if constexpr (kIsHorizontalRun) {
       bounds_for_glyph.set_x(bounds_for_glyph.x() + origin);
     } else {
       bounds_for_glyph.set_y(bounds_for_glyph.y() + origin);
@@ -312,7 +312,7 @@ class VectorizedGlyphBoundsAccumulator final {
       float& max_x,
       float& max_y,
       const FontMetrics& font_metrics) {
-    if constexpr (is_horizontal_run) {
+    if constexpr (kIsHorizontalRun) {
       return;
     }
     // Convert physical glyph_bounding_box to logical.
