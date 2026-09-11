aacceb85b271eb182c27f66ed43e0906012561a4
Florian Leimgruber <fleimgruber@google.com>
2026-09-11 08:29:49 -0700

    Restrict subtitles of AutofillAi entities in settings to one line
    autofill_ai_entries_list.html intends to limit the subtitle to one line
    by specifying the "ellipses" class. However, this css class is defined
    in passwords-shared, which is not included.
    
    This bug was recently noticed during order & shipments manual testing,
    since these entities might have a long list of product names.
    
    In this CL, the problem is fixed by:
    - Migrating the css rule to autofill-shared.
    - Including it in autofill_ai_entries_list.html.
    
    Before: https://screenshot-v2.corp.google.com/6hfpql0pvujf0
    After https://screenshot-v2.corp.google.com/2pu1megj9pm10
    Note that the after screenshot is from a non-branded build and thus
    looks slightly different.
    
    Bug: 559140810
    Change-Id: Ia974104a481d7ec51f5a200b9be617c508d4a712
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395907
    Reviewed-by: Ireneusz Szulc <ireneuszszulc@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696277}

--- chrome/browser/resources/settings/autofill_page/payments/credit_card_list_entry.ts
+++ chrome/browser/resources/settings/autofill_page/payments/credit_card_list_entry.ts
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import '../../settings_shared.css.js';
-import '../passwords/passwords_shared.css.js';
+import '../autofill_shared.css.js';
 import './screen_reader_only.css.js';
 
 import {I18nMixin} from '//resources/cr_elements/i18n_mixin.js';
