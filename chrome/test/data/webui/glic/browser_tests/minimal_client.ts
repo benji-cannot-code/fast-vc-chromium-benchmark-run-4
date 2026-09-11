20347ea2f5a8c3af41a21a0f0eab9b73804d6bf2
Dan Harrington <harringtond@chromium.org>
2026-09-11 14:16:23 -0700

    Add glic_start_benchmark_browsertest.cc
    And a script to run it. This tracks startup time for glic, and runs with
    and without GlicNoWebview.
    
    Bug: b:534807813
    Change-Id: If7366c6fafb12c7e915337b1e84126f36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392757
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696597}

--- chrome/test/data/webui/glic/browser_tests/minimal_client.ts
+++ chrome/test/data/webui/glic/browser_tests/minimal_client.ts
@@ -3,12 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type {GlicBrowserHost, GlicWebClient} from '/glic/glic_api/glic_api.js';
+import type {GlicBrowserHost, GlicWebClient, OpenPanelInfo, PanelOpeningData, PanelState} from '/glic/glic_api/glic_api.js';
+import {WebClientMode} from '/glic/glic_api/glic_api.js';
 
 import {createGlicHostRegistryOnLoad} from '../api_boot.js';
 
+performance.mark('glic-client-main-script-start');
+
 class WebClient implements GlicWebClient {
-  async initialize(_browser: GlicBrowserHost): Promise<void> {}
+  async initialize(_browser: GlicBrowserHost): Promise<void> {
+    performance.mark('glic-client-initialized');
+  }
+
+  async notifyPanelWillOpen(_panelOpeningData: PanelOpeningData&PanelState):
+      Promise<OpenPanelInfo> {
+    performance.mark('glic-client-panel-opened');
+    return {startingMode: WebClientMode.TEXT};
+  }
 }
 
 const client = new WebClient();
