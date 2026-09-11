603004a3050128d989a141b243db859ebf49be95
Wan-Teh Chang <wtc@google.com>
2026-09-10 17:53:29 -0700

    Roll src/third_party/dav1d/libdav1d/ aa09a630e..a34e0685b (2 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/aa09a630ef57..a34e0685b911
    
    $ git log aa09a630e..a34e0685b --date=short --no-merges --format='%ad %ae %s'
    2026-09-09 martin compat: Fix building with newer versions of MSVC
    2026-08-28 GermanAizek x86: optimize cdef SIMD routines
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Iecf6f7e482410fa75a4e37aa1284332868576b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8391720
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695857}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.4-2-gaa09a630"
+#define DAV1D_VERSION "1.5.4-4-ga34e0685"
