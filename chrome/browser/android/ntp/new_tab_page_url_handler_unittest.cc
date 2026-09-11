71bafcc903353850fd74b9af88fba2e2f412cd18
Ankush Singh <ankushkush@google.com>
2026-09-11 10:08:02 -0700

    [ntp] Use HTTPS Google search URL in new_tab_page_url_handler unit test
    Update mock Google TemplateURL to use HTTPS.
    
    This is a preparatory change ahead of crrev.com/c/8362993, which
    enforces cryptographic scheme validation in
    TemplateURLAndSuggestionsAreGoogle() and causes
    DefaultSearchProviderIsGoogle() checks to fail for plaintext HTTP mock
    Google URLs.
    
    Bug: 501729675
    Change-Id: I1bf161d55e554ae45ebefe40b6cae92501c6e217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395468
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696372}

--- chrome/browser/android/ntp/new_tab_page_url_handler_unittest.cc
+++ chrome/browser/android/ntp/new_tab_page_url_handler_unittest.cc
@@ -66,7 +66,7 @@ TEST_F(NewTabPageUrlHandlerTest, TestWebUiNtpRedirection_Enabled_DseGoogle) {
 
   // Mock Google DSE setup
   std::unique_ptr<TemplateURL> google_turl = CreateTestTemplateURL(
-      u"google", "http://www.google.com/search?q={searchTerms}");
+      u"google", "https://www.google.com/search?q={searchTerms}");
   TemplateURL* added_turl = model()->Add(std::move(google_turl));
   model()->SetUserSelectedDefaultSearchProvider(added_turl);
   test_util_->ChangeModelToLoadState();
@@ -108,7 +108,7 @@ TEST_F(NewTabPageUrlHandlerTest, TestWebUiNtpRedirection_Disabled) {
 
   // Set DSE to Google.
   std::unique_ptr<TemplateURL> google_turl = CreateTestTemplateURL(
-      u"google", "http://www.google.com/search?q={searchTerms}");
+      u"google", "https://www.google.com/search?q={searchTerms}");
   TemplateURL* added_turl = model()->Add(std::move(google_turl));
   model()->SetUserSelectedDefaultSearchProvider(added_turl);
   test_util_->ChangeModelToLoadState();
@@ -129,7 +129,7 @@ TEST_F(NewTabPageUrlHandlerTest, TestWebUiNtpRedirection_Enabled_DseGoogle_Mobil
 
   // Set DSE to Google.
   std::unique_ptr<TemplateURL> google_turl = CreateTestTemplateURL(
-      u"google", "http://www.google.com/search?q={searchTerms}");
+      u"google", "https://www.google.com/search?q={searchTerms}");
   TemplateURL* added_turl = model()->Add(std::move(google_turl));
   model()->SetUserSelectedDefaultSearchProvider(added_turl);
   test_util_->ChangeModelToLoadState();
