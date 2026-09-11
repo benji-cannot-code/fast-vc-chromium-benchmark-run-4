7fe76d1a59180a145a5104500003351461089247
Allen Bauer <kylixrd@chromium.org>
2026-09-11 12:34:45 -0700

    Enable input protection on IntentPickerBubbleView to block keyjacking.
    Change-Id: Ib80d949aa9aaff1cea75b590e1fd858e81aa74ce
    Bug: 553264917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8383718
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696512}

--- chrome/browser/ui/views/intent_picker_bubble_view.cc
+++ chrome/browser/ui/views/intent_picker_bubble_view.cc
@@ -689,6 +689,10 @@ void IntentPickerBubbleView::OnWidgetDestroying(views::Widget* widget) {
                             false);
 }
 
+bool IntentPickerBubbleView::ShouldAllowKeyEventsDuringInputProtection() const {
+  return false;
+}
+
 void IntentPickerBubbleView::OnAppSelected(std::optional<size_t> index,
                                            bool accepted,
                                            const ui::Event* event) {
