8754db618f31293157b8c5827b627490bed2cf77
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-09-11 01:14:26 -0700

    [composebox] Restrict composebox video thumbnails to supported formats
    This change introduces a whitelist of supported video extensions (mp4,
    webm, ogg, mov, m4v) for rendering video thumbnails in the composebox
    file thumbnail component. Unsupported video formats will now fall back
    to displaying a document chip instead of attempting to render a video
    element. Unit tests are added to verify the rendering behavior for both
    supported and unsupported video formats.
    Screencast:
    http://screencast/cast/NDg3NTY2NjU1MzA0NDk5MnwwOTJlNTFhYi05OQ
    
    Fixed: b:530883803
    Change-Id: I720f6c2dca81593fd540a1f21f26053675d39e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392417
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696022}

--- chrome/test/data/webui/cr_components/composebox/file_thumbnail_test.ts
+++ chrome/test/data/webui/cr_components/composebox/file_thumbnail_test.ts
@@ -471,6 +471,7 @@ function createThumbnailElement(isAndroid: boolean):
     test('clicking video delete button sends event', async () => {
       // Arrange.
       fileThumbnailElement.file = createFile(1, {
+        name: 'video.mp4',
         type: 'video/mp4',
         objectUrl: 'data:foo',
       });
@@ -490,6 +491,7 @@ function createThumbnailElement(isAndroid: boolean):
     test('hides video delete button when not deletable', async () => {
       // Arrange.
       fileThumbnailElement.file = createFile(1, {
+        name: 'video.mp4',
         type: 'video/mp4',
         objectUrl: 'data:foo',
         isDeletable: false,
@@ -500,6 +502,43 @@ function createThumbnailElement(isAndroid: boolean):
       const removeButton =
           fileThumbnailElement.shadowRoot.querySelector('#removeImgButton');
       assertEquals(null, removeButton);
+    });
+
+    ['mp4', 'webm', 'ogg', 'mov', 'm4v'].forEach(ext => {
+      test(`display supported video format .${ext}`, async () => {
+        fileThumbnailElement.file = createFile(1, {
+          name: `video.${ext}`,
+          type: `video/${ext}`,
+          objectUrl: 'data:foo',
+        });
+        await microtasksFinished();
+
+        const thumbnail =
+            fileThumbnailElement.shadowRoot.querySelector<HTMLVideoElement>(
+                '.img-thumbnail');
+        assertTrue(!!thumbnail);
+        assertEquals(thumbnail.tagName, 'VIDEO');
+      });
+    });
+
+    ['wmv', 'flv', 'avi', 'mkv'].forEach(ext => {
+      test(
+          `fallback to document chip for unsupported video format .${ext}`,
+          async () => {
+            fileThumbnailElement.file = createFile(1, {
+              name: `video.${ext}`,
+              type: `video/${ext}`,
+              objectUrl: 'data:foo',
+            });
+            await microtasksFinished();
+
+            const video =
+                fileThumbnailElement.shadowRoot.querySelector('.img-thumbnail');
+            assertEquals(null, video);
+            const documentChip =
+                fileThumbnailElement.shadowRoot.querySelector('#documentChip');
+            assertTrue(!!documentChip);
+          });
     });
 
     test('clicking document delete button sends event', async () => {
--- ui/webui/resources/cr_components/composebox/file_thumbnail.html.ts
+++ ui/webui/resources/cr_components/composebox/file_thumbnail.html.ts
@@ -156,8 +156,7 @@ export function getHtml(this: ComposeboxFileThumbnailElement) {
         </div>
         `}
       ` : html`
-        ${(this.file.type.startsWith('image/') || this.file.objectUrl
-            || this.file.dataUrl || this.file.thumbnailUrl) ?
+        ${this.hasVisualThumbnail_() ?
           html`
             ${this.isAndroid_ ? html`
         <div id="imgChip">
@@ -172,7 +171,7 @@ export function getHtml(this: ComposeboxFileThumbnailElement) {
                   auto-src="${this.file.thumbnailUrl}"
                   aria-label="${this.file.name}">
               ` : html`
-                ${this.isVideo_() && this.file.objectUrl ? html`
+                ${this.isSupportedVideo_() && this.file.objectUrl ? html`
                   <video class="img-thumbnail"
                     src="${this.file.objectUrl}#t=0.001"
                     preload="metadata"
@@ -212,7 +211,7 @@ export function getHtml(this: ComposeboxFileThumbnailElement) {
                 auto-src="${this.file.thumbnailUrl}"
                 aria-label="${this.file.name}">
             ` : html`
-              ${this.isVideo_() && this.file.objectUrl ? html`
+              ${this.isSupportedVideo_() && this.file.objectUrl ? html`
                 <video class="img-thumbnail"
                   src="${this.file.objectUrl}#t=0.001"
                   preload="metadata"
