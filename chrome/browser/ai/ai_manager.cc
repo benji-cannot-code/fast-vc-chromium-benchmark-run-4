243f0bea599ab08577e48d7dd5bf12ce2380343f
Lei Zhang <thestig@chromium.org>
2026-09-11 09:11:47 -0700

    Clean up ai_manager.h
    - No need to expose `kModelVersionParam`.
    - Declare kAIApiFoundationalModel after forward declarations.
    - Move using-statement that pollutes the global namespace into
      ai_manager.cc.
    
    Change-Id: I88eef5aaa7d922cc105e2187a28544738de8383c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8393340
    Reviewed-by: Jingyun Liu <jingyun@google.com>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696324}

--- chrome/browser/ai/ai_manager.cc
+++ chrome/browser/ai/ai_manager.cc
@@ -81,6 +81,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/ai/model_streaming_responder.mojom.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom-shared.h"
 
+using blink::mojom::AILanguageCodePtr;
+
 namespace {
 
 constexpr float kDefaultMaxTemperature = 2.0f;
@@ -123,6 +125,8 @@ const char kExperimentalLanguageWarning[] =
 const char kSpeedPreferenceMarkdownWarning[] =
     "The 'speed' performance preference utilizes a model with limited support "
     "for 'markdown' format.";
+
+const char kModelVersionParam[] = "model_version";
 
 // Eagerly initializes other downloadable APIs when any session type is created.
 BASE_FEATURE(kBuiltInAIEagerInit, base::FEATURE_ENABLED_BY_DEFAULT);
@@ -717,7 +721,6 @@ std::string_view AILanguageModelSamplingModeToString(
 // field param kModelVersionParam to specify the model version. Example:
 // --enable-features=AIApiFoundationalModel:model_version/v4
 BASE_FEATURE(kAIApiFoundationalModel, base::FEATURE_DISABLED_BY_DEFAULT);
-const char kModelVersionParam[] = "model_version";
 
 AIManager::AIManager(content::BrowserContext* browser_context,
                      content::RenderFrameHost* rfh)
