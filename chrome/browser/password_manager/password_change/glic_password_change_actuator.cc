204de16ab941ad0a8238e68fd52dc161fb1b85a8
Talita Halboth <talitahalboth@google.com>
2026-09-11 08:16:28 -0700

    [APC] Integrate Origin Gating
    Bug: b:510306271
    Change-Id: I6cbd3e4acb78356c8eb20b96bbc6b512d6135b43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353949
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Talita Halboth <talitahalboth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696261}

--- chrome/browser/password_manager/password_change/glic_password_change_actuator.cc
+++ chrome/browser/password_manager/password_change/glic_password_change_actuator.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/actor_webui.mojom.h"
 #include "chrome/grit/browser_resources.h"
 #include "components/autofill/core/browser/logging/log_manager.h"
+#include "components/origin_gating/core/actor_container_config.h"
 #include "components/password_manager/content/browser/content_password_manager_driver.h"
 #include "components/password_manager/core/browser/actor_login/password_change_from_checkup_actor_login_service.h"
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents.h"
+#include "net/base/schemeful_site.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/window_open_disposition.h"
@@ -176,6 +178,24 @@ std::optional<GlicPasswordChangeActuator::TaskResult> ParseTaskResult(
   }
 
   return std::nullopt;
+}
+
+origin_gating::ActorContainerConfig BuildPasswordChangeContainerConfig(
+    const std::set<net::SchemefulSite>& allowed_origins) {
+  origin_gating::ActorContainerConfig::LocationRules rules;
+
+  origin_gating::ActorContainerConfig::Rule rule(
+      /*navigation_sources=*/{},
+      /*resources=*/
+      {origin_gating::ActorContainerConfig::Rule::Resource::kSession},
+      /*capabilities=*/
+      {origin_gating::ActorContainerConfig::Rule::Capability::kAll});
+
+  for (const auto& origin : allowed_origins) {
+    rules.emplace(origin_gating::ActorContainerConfig::Location(origin), rule);
+  }
+
+  return origin_gating::ActorContainerConfig(std::move(rules));
 }
 
 }  // namespace
@@ -395,6 +415,23 @@ void GlicPasswordChangeActuator::OnActorTaskStateChanged(
   task.GetExecutionEngine().SetActorLoginService(
       std::make_unique<actor_login::PasswordChangeFromCheckupActorLoginService>(
           password_manager::CloneStoredCredential(credential_)));
+
+  // TODO(crbug.com/559497033): Inject ActorContainerConfig through API when
+  // it's ready
+  CHECK(!task.GetExecutionEngine()
+             .origin_gating_checker()
+             .actor_container_config_slot()
+             .has_value());
+  std::set<net::SchemefulSite> allowed_origins;
+  allowed_origins.emplace(credential_.url);
+  if (change_password_url_.is_valid()) {
+    allowed_origins.emplace(change_password_url_);
+  }
+
+  task.GetExecutionEngine()
+      .origin_gating_checker()
+      .actor_container_config_slot()
+      .Assign(BuildPasswordChangeContainerConfig(allowed_origins));
 
   if (auto logger = GetLoggerIfAvailable(originator_.get())) {
     logger->LogMessage(
