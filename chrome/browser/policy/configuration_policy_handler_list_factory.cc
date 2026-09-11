b3ec59c5da5058b2d4d7c75930f3d456b188ceb4
Baran Erfani <baranerf@google.com>
2026-09-11 10:47:58 -0700

    [Indigo] Deobfuscate Try on You policy
    Renames the enterprise policy for the "Try on You" feature from its
    internal codename (Indigo) to its public name (TryOnYouSettings).
    Changes include:
    - Renaming the policy definition YAML and pref mapping JSON test.
    - Updating policy ID 1469 in policies.yaml and enums.xml.
    - Updating C++ policy handler mappings to use key::kTryOnYouSettings.
    - Promoting the policy from future_on to supported_on in M154.
    
    Bug: b:537397379
    Change-Id: Ic5eef449388e4412375e432a58db657e1fdef4fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8383879
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696420}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -498,7 +498,7 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kVoiceTypingSettings,
     prefs::kVoiceTypingSettings,
     base::Value::Type::INTEGER },
-  { key::kIndigo,
+  { key::kTryOnYouSettings,
     indigo::prefs::kIndigoPolicy,
     base::Value::Type::INTEGER },
 #endif
@@ -3682,7 +3682,7 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
 #if !BUILDFLAG(IS_ANDROID)
   gen_ai_default_policies.emplace_back(key::kVoiceTypingSettings,
                                        prefs::kVoiceTypingSettings);
-  gen_ai_default_policies.emplace_back(key::kIndigo,
+  gen_ai_default_policies.emplace_back(key::kTryOnYouSettings,
                                        indigo::prefs::kIndigoPolicy);
 #endif
   // Default value for SearchContentSharingSettings is 0 if
