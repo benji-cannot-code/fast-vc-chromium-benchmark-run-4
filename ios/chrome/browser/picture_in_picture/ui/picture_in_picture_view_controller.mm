a376d239585969653b790b6d4314f2e374722ada
Cheick Cisse <cheickcisse@google.com>
2026-09-11 15:33:14 -0700

    [iOS] Add accessibility description for Default Browser PiP text video
    When kDefaultBrowserPipTextVideo is enabled, the default browser PiP
    displays a video with on-screen text instructions. This CL adds audio
    description of the text.
    
    Fixed: 559795434
    Change-Id: I489469dac4e212802ea0e3e2aa557ff4f8b84875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400808
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Auto-Submit: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696665}

--- ios/chrome/browser/picture_in_picture/ui/picture_in_picture_view_controller.mm
+++ ios/chrome/browser/picture_in_picture/ui/picture_in_picture_view_controller.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/strcat.h"
 #import "base/task/sequenced_task_runner.h"
 #import "base/time/time.h"
+#import "ios/chrome/browser/default_browser/model/features.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
 #import "ios/chrome/browser/picture_in_picture/public/picture_in_picture_configuration.h"
 #import "ios/chrome/browser/picture_in_picture/ui/picture_in_picture_mutator.h"
@@ -44,6 +45,10 @@ constexpr CGFloat kPlayPauseButtonPointSize = 25.0;
 NSString* accessibilityLabel(PictureInPictureFeature feature) {
   switch (feature) {
     case PictureInPictureFeature::kDefaultBrowser:
+      if (IsDefaultBrowserPipTextVideoEnabled()) {
+        return l10n_util::GetNSString(
+            IDS_IOS_DEFAULT_BROWSER_PIP_TEXT_VIDEO_ACCESSIBILITY_DESCRIPTION);
+      }
       return l10n_util::GetNSString(
           IDS_IOS_DEFAULT_BROWSER_PIP_ACCESSIBILITY_ANNOUNCEMENT);
   }
