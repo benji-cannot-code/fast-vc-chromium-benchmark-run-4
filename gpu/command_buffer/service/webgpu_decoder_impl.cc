f5ce75977db9ee37d269d6554e89bbac7aa1b315
James Price <jrprice@google.com>
2026-09-10 17:24:04 -0700

    [webgpu] Add AtomicVec2uMinMax as an unsafe feature
    This will expose the feature to our CTS runners to get more device test
    coverage ahead of standardization.
    
    Bug: 559163248
    Change-Id: Ib00e384b0a382a1332099a36fe228879c52a450b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8389377
    Auto-Submit: James Price <jrprice@google.com>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695832}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1268,6 +1268,7 @@ bool WebGPUDecoderImpl::IsFeatureExposed(wgpu::FeatureName feature) const {
     case wgpu::FeatureName::AdapterPropertiesVk:
     case wgpu::FeatureName::AdapterPropertiesMemoryHeaps:
     case wgpu::FeatureName::ShaderModuleCompilationOptions:
+    case wgpu::FeatureName::AtomicVec2uMinMax:
       return safety_level_ == webgpu::SafetyLevel::kUnsafe ||
              safety_level_ == webgpu::SafetyLevel::kSafeExperimental;
     case wgpu::FeatureName::CoreFeaturesAndLimits:
