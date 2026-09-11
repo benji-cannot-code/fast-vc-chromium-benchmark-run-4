1ab10eceb05a8a011ee0bd8a2dc99fc4bbbaeb27
Sky Malice <skym@chromium.org>
2026-09-11 12:55:55 -0700

    [AIM] Rename Omnibox multimodal Ask Hint flag variation
    Rename the kOmniboxAimHint variation in about_flags.cc from "Hint"
    to "Ask Hint" to improve clarity in chrome://flags.
    
    TAG=agy
    CONV=7b4f425d-6d8f-4ecb-b860-1d4eaa071d92
    
    Bug: 476434460
    Change-Id: I2804ff79467671af32f96761fe47d5df6ebd43c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395057
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696520}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2108,7 +2108,7 @@ const FeatureEntry::FeatureVariation kOmniboxMultimodalInputVariants[] = {
      nullptr},
     {"Bottom Sheet: Carousel, No Accordion",
      kOmniboxAimBottomSheetCarouselNoAccordion, nullptr},
-    {"Hint", kOmniboxAimHint, nullptr},
+    {"Ask Hint", kOmniboxAimHint, nullptr},
 };
 
 #endif  // BUILDFLAG(IS_ANDROID)
