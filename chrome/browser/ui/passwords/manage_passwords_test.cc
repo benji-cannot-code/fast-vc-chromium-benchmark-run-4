5e3f88823be233e1fe424433501683085abd160f
Adithya Srinivasan <adithyas@chromium.org>
2026-09-11 07:45:00 -0700

    actor: Remove ActorKeyedService::SetActorUiStateManagerForTesting
    Part of an effort to clean up usage of actor_ui_state_manager_ in
    ActorKeyedService.
    
    Callers of AKS can provide their own AUSMI through the CreateTask API,
    and should be using that instead.
    
    Bug: b:552950922
    Change-Id: I7094430f3586aec4e2d53d7931c9fc9d5b2a8dd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8388196
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696229}

--- chrome/browser/ui/passwords/manage_passwords_test.cc
+++ chrome/browser/ui/passwords/manage_passwords_test.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/actor/actor_keyed_service_factory.h"
 #include "chrome/browser/actor/actor_keyed_service_fake.h"
-#include "chrome/browser/actor/ui/test_support/mock_actor_ui_state_manager.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/password_manager/factories/account_password_store_factory.h"
@@ -132,16 +131,8 @@ void ManagePasswordsTest::SetUpInProcessBrowserTestFixture() {
                                                     KeyedService> {
                           Profile* profile =
                               Profile::FromBrowserContext(context);
-                          auto actor_keyed_service =
-                              std::make_unique<actor::ActorKeyedServiceFake>(
-                                  profile);
-                          std::unique_ptr<actor::ui::MockActorUiStateManager>
-                              ausm = std::make_unique<
-                                  actor::ui::MockActorUiStateManager>();
-                          actor_keyed_service->SetActorUiStateManagerForTesting(
-                              std::move(ausm));
-
-                          return std::move(actor_keyed_service);
+                          return std::make_unique<actor::ActorKeyedServiceFake>(
+                              profile);
                         }));
               }));
 }
