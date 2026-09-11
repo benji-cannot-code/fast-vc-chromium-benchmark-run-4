12d35cff2269e27cfcaf60062fef65f05c0bd7cf
anthie@google.com <anthie@google.com>
2026-09-11 07:31:19 -0700

    Update gradient ring size for small avatars
    Avatars sized <= 24px should use a 2 px wide ring.
    Avatar sized more than that use the standard 3px thickness.
    
    Bug: 556167332
    Change-Id: I9dd9e781272635191ec58d5e348cac0c4409f11a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8355308
    Auto-Submit: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696216}

--- chrome/browser/ui/profiles/profile_view_utils.cc
+++ chrome/browser/ui/profiles/profile_view_utils.cc
@@ -51,6 +51,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/profiles/profile_view_avatar_decoration_specs.h"
 #endif
 
+int GetAvatarRingThickness(int avatar_size) {
+  return avatar_size <= kSmallAvatarThresholdDip ? kAvatarRingThicknessSmallDip
+                                                 : kAvatarRingThicknessDip;
+}
+
 void NavigateToGoogleAccountPage(Profile* profile, const std::string& email) {
   // Create a URL so that the account chooser is shown if the account with
   // |email| is not signed into the web. Include a UTM parameter to signal the
@@ -204,11 +209,13 @@ gfx::ImageSkia AddLinearGradientRingToAvatar(
     const ui::ColorProvider& color_provider,
     int avatar_size,
     int gap_width,
-    int ring_thickness) {
+    std::optional<int> ring_thickness) {
+  const int thickness =
+      ring_thickness.value_or(GetAvatarRingThickness(avatar_size));
   return profiles::AddLinearGradientRingToAvatar(
       avatar_image, color_provider,
       color_provider.GetColor(kAvatarRingGradientStartColorId),
       color_provider.GetColor(kAvatarRingGradientEndColorId),
       kAvatarRingGradientPositions, kAvatarRingGradientP1Normalized,
-      kAvatarRingGradientP2Normalized, avatar_size, gap_width, ring_thickness);
+      kAvatarRingGradientP2Normalized, avatar_size, gap_width, thickness);
 }
