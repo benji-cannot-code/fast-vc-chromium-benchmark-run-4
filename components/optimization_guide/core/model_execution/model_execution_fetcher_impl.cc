ccc7c4a3c58a71237ba2c031dbda3c5746438ad9
Zekun Jiang <zekunjiang@google.com>
2026-09-11 14:51:45 -0700

    Allow HTTP when MES base URL is overridden.
    Change-Id: I04381b70845682893ab234ee7ccb7e19d505ac8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8391717
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696638}

--- components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
+++ components/optimization_guide/core/model_execution/model_execution_fetcher_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
+#include "base/command_line.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/types/expected.h"
 #include "components/optimization_guide/core/access_token_helper.h"
@@ -49,9 +50,10 @@ ModelExecutionFetcherImpl::ModelExecutionFetcherImpl(
     : optimization_guide_service_url_(GetModelExecutionServiceURL()),
       url_loader_factory_(url_loader_factory),
       optimization_guide_logger_(optimization_guide_logger) {
-  if (!net::IsLocalhost(optimization_guide_service_url_)) {
-    CHECK(optimization_guide_service_url_.SchemeIs(url::kHttpsScheme));
-  }
+  CHECK(optimization_guide_service_url_.SchemeIs(url::kHttpsScheme) ||
+        (base::CommandLine::ForCurrentProcess()->HasSwitch(
+             kOptimizationGuideServiceModelExecutionURLSwitch) &&
+         optimization_guide_service_url_.SchemeIsHTTPOrHTTPS()));
 }
 
 ModelExecutionFetcherImpl::~ModelExecutionFetcherImpl() {
--- components/optimization_guide/core/model_execution/model_execution_fetcher_impl_unittest.cc
+++ components/optimization_guide/core/model_execution/model_execution_fetcher_impl_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/strings/strcat.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_command_line.h"
 #include "base/test/task_environment.h"
 #include "base/test/test.pb.h"
 #include "components/optimization_guide/core/model_execution/feature_keys.h"
@@ -439,6 +440,16 @@ TEST_F(ModelExecutionFetcherImplTest,
                 last_execute_response_->value().response_metadata())
                 ->test());
   EXPECT_TRUE(last_server_timeout_header_.empty());
+}
+
+TEST_F(ModelExecutionFetcherImplTest, DevHttpUrlAllowedWithSwitch) {
+  base::test::ScopedCommandLine scoped_command_line;
+  scoped_command_line.GetProcessCommandLine()->AppendSwitchASCII(
+      kOptimizationGuideServiceModelExecutionURLSwitch, "http://1.2.3.4:8080/");
+
+  auto fetcher = std::make_unique<ModelExecutionFetcherImpl>(
+      shared_url_loader_factory_, /*optimization_guide_logger=*/nullptr);
+  EXPECT_TRUE(fetcher);
 }
 
 }  // namespace optimization_guide
