91c70827547c5fd32afe5302561189719ba4b31e
Philip Rogers <pdr@chromium.org>
2026-09-11 15:51:27 -0700

    Prevent merging sticky layers across non-2d-translation transforms
    Sticky layers cannot be merged across non-2D-translation transforms
    because the compositor sticky offset applied to the merged layer would
    not correctly account for intermediate transforms. This patch adds a
    check to ensure both nodes share the same RootOf2dTranslation.
    
    Fixed: 514078734
    Change-Id: I1eac45e1539c975b74ff255c7a0ad1007e53ec8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8393871
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696682}

--- third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.cc
+++ third_party/blink/renderer/platform/graphics/paint/transform_paint_property_node.cc
@@ -185,7 +185,8 @@ TransformPaintPropertyNode::CanMergeForStickyPosition(
       UnaliasedParent()->NearestDirectlyCompositedAncestor() !=
           other.UnaliasedParent()->NearestDirectlyCompositedAncestor() ||
       &NearestScrollTranslationNode() !=
-          &other.NearestScrollTranslationNode()) {
+          &other.NearestScrollTranslationNode() ||
+      RootOf2dTranslation() != other.RootOf2dTranslation()) {
     return cc::StickyPositionConstraint::CanMergeResult::kCannotMerge;
   }
 
