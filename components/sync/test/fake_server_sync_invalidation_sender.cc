485b232b7743670e590daf5173f4e3c67dab76f4
Michael Tatarski <mtatarski@google.com>
2026-09-11 06:20:44 -0700

    [Sync] Move FakeServerSyncInvalidationSender to components/sync/test
    Move FakeServerSyncInvalidationSender from
    chrome/browser/sync/test/integration/invalidations to
    components/sync/test to allow sharing across platforms, including iOS
    integration test suites.
    
    Update build rules and includes accordingly.
    
    Bypass-Check-License: Moved existing files
    Bug: 556562996
    Change-Id: I55c6e4b4ae4b1f37f194871e80d6f1a650e259ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8397850
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696167}

--- components/sync/test/fake_server_sync_invalidation_sender.cc
+++ components/sync/test/fake_server_sync_invalidation_sender.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/sync/test/integration/invalidations/fake_server_sync_invalidation_sender.h"
+#include "components/sync/test/fake_server_sync_invalidation_sender.h"
 
 #include <algorithm>
 #include <vector>
