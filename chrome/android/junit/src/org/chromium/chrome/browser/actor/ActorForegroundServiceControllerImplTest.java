d0dd736cc1c29e7b7b7a6f84b205e7c0c9d75228
Satyaveer Singh <satyaveer.s@samsung.com>
2026-09-11 10:47:23 -0700

    [Clean] Remove redundant @Config(manifest = Config.NONE) in browser
    Robolectric @Config(manifest = Config.NONE) is a legacy attribute that
    has no effect in modern Robolectric test suites in Chromium.
    
    Remove all instances in browser to simplify test annotations.
    
    Bug: 554089323
    Change-Id: I4f2252cdd4d8114effc364cbe87c41eefb3f43c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400186
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696419}

--- chrome/android/junit/src/org/chromium/chrome/browser/actor/ActorForegroundServiceControllerImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/actor/ActorForegroundServiceControllerImplTest.java
@@ -32,7 +32,6 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
 
 import org.chromium.base.ActivityState;
@@ -56,7 +55,6 @@ import java.util.Collections;
 
 /** Unit tests for {@link ActorForegroundServiceControllerImpl}. */
 @RunWith(BaseRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
 public class ActorForegroundServiceControllerImplTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
