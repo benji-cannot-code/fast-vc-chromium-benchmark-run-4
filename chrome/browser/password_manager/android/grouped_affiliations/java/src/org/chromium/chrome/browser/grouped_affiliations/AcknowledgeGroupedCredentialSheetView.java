2e3c31a1e28c9074b79fa034227b6f45f3659785
Hitarth Kothari <hitarthkothari@google.com>
2026-09-11 08:08:49 -0700

    [BottomSheetHandles] Migrate Acknowledge Grouped Credential sheet to showHandlebar()
    Migrates AcknowledgeGroupedCredentialSheetView to use the unified
    BottomSheetContent#showHandlebar() API instead of inflating and managing
    a custom drag handle view inside its layout.
    
    Key changes:
    - Removed custom drag_handlebar ImageView and layout constraints from
      acknowledge_grouped_credential_sheet_content.xml.
    - Overrode showHandlebar() to return true in
      AcknowledgeGroupedCredentialSheetView.
    
    Bug: 543136317
    Change-Id: I8d91881e6cbf06a883b0a892ac4f515164310ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8321968
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696255}

--- chrome/browser/password_manager/android/grouped_affiliations/java/src/org/chromium/chrome/browser/grouped_affiliations/AcknowledgeGroupedCredentialSheetView.java
+++ chrome/browser/password_manager/android/grouped_affiliations/java/src/org/chromium/chrome/browser/grouped_affiliations/AcknowledgeGroupedCredentialSheetView.java
@@ -178,4 +178,9 @@ class AcknowledgeGroupedCredentialSheetView implements BottomSheetContent {
     public float getFullHeightRatio() {
         return HeightMode.WRAP_CONTENT;
     }
+
+    @Override
+    public boolean showHandlebar() {
+        return true;
+    }
 }
