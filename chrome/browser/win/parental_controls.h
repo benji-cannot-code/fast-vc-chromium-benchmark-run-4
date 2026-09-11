214aabded3f75299a086ec83c1c80a6a048081e5
James Lee <ljjlee@google.com>
2026-09-11 01:37:01 -0700

    Add UMA metric for Windows web filtering status
    Bug: 558275698
    Change-Id: I85530fecc4be987a0bc9fbf3c740b0b692b7d368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8356772
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696033}

--- chrome/browser/win/parental_controls.h
+++ chrome/browser/win/parental_controls.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WIN_PARENTAL_CONTROLS_H_
 #define CHROME_BROWSER_WIN_PARENTAL_CONTROLS_H_
 
+#include "base/feature_list.h"
+
+BASE_DECLARE_FEATURE(kEmitWindowsParentalControlsHistograms);
+
 struct WinParentalControls {
   bool any_restrictions = false;
   bool logging_required = false;
