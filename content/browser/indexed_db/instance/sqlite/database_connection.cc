c34b09b9f0cb6aadd1c0c06ccc01e25900103a84
Evan Stade <evanstade@microsoft.com>
2026-09-11 09:49:37 -0700

    IDB: SQLite store - include legacy blob files in DB size calc
    Bug: 419208485,419264073
    Change-Id: I13434ba4643f93ff85379f428925d504e43e6456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8383338
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1696356}

--- content/browser/indexed_db/instance/sqlite/database_connection.cc
+++ content/browser/indexed_db/instance/sqlite/database_connection.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/functional/callback.h"
+#include "base/functional/callback_helpers.h"
 #include "base/functional/function_ref.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_functions.h"
@@ -1224,6 +1225,12 @@ base::OnceCallback<void(bool)> DatabaseConnection::GetCleanupTask() && {
 
   // The error callback is no longer needed since `this` will be deleted soon.
   db_->reset_error_callback();
+
+  if (in_memory()) {
+    db_.reset();
+    return base::DoNothing();
+  }
+
   db_->DetachFromSequence();
   return base::BindOnce(&DatabaseConnection::CloseDatabase, std::move(db_),
                         path_, GetLegacyBlobDirectory(), should_delete_db,
@@ -1455,7 +1462,11 @@ uint64_t DatabaseConnection::GetSize() const {
     // Can only happen if one of the pragmas failed. Log under a common bucket.
     LogEvent(SpecificEvent::kPragmaPageCountFailed);
   }
-  return used_size.InBytes();
+  if (in_memory()) {
+    return used_size.InBytes();
+  }
+  return used_size.InBytes() +
+         base::ComputeDirectorySize(GetLegacyBlobDirectory());
 }
 
 bool DatabaseConnection::ReportMemoryUsage(
@@ -2880,9 +2891,16 @@ std::set<int64_t> DatabaseConnection::SnapshotLegacyBlobFiles() {
 }
 
 base::FilePath DatabaseConnection::GetLegacyBlobDirectory() const {
+  return GetLegacyBlobDirectory(path_);
+}
+
+// static
+base::FilePath DatabaseConnection::GetLegacyBlobDirectory(
+    const base::FilePath& db_path) {
+  CHECK(!db_path.empty());
   // For the sake of avoiding path length limits, the directory is given a short
   // name instead of a descriptive name.
-  return path_.InsertBeforeExtensionASCII("_");
+  return db_path.InsertBeforeExtensionASCII("_");
 }
 
 base::FilePath DatabaseConnection::GetBlobFilePath(int64_t blob_id) const {
