1800abadaf4a1b77e4b269728fa8be25e6fab1c5
dpapad <dpapad@chromium.org>
2026-09-11 17:35:58 -0700

    Settings Lit: Migrate relaunch_confirmation_dialog.ts.
    Bug: 393471368
    Change-Id: I44a614244a0b8f3bf96c89391a689bd61959d6b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8401066
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696742}

--- chrome/test/data/webui/settings/safety_hub_page_test.ts
+++ chrome/test/data/webui/settings/safety_hub_page_test.ts
@@ -374,8 +374,8 @@ suite('SafetyHubPage', function() {
     assertTrue(relaunchConfirmationDialogElement.$.dialog.open);
 
     // Ensure the confirmation dialog shows a correct description.
-    const dialog = relaunchConfirmationDialogElement.shadowRoot!.querySelector(
-        'cr-dialog');
+    const dialog =
+        relaunchConfirmationDialogElement.shadowRoot.querySelector('cr-dialog');
     assertTrue(!!dialog);
     const description =
         dialog.shadowRoot.querySelector<HTMLSlotElement>('slot[name=body]');
