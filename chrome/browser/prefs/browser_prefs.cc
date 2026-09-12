3d1ff188cc70b9392d6e7c51695d297a11517220
Gary Klassen <gklassen@chromium.org>
2026-09-11 17:02:31 -0700

    [TTC] Persist remembered notes across resets in Profile preferences
    - Replaces the transient in-memory map in AiOverlayDialogController with
      persistent Profile dictionary preferences (prefs::kAiOverlayRememberedNotes).
    - Registers the profile preference in chrome/browser/prefs/browser_prefs.cc.
    - Updates AiOverlayDialogPageHandler to read from controller Profile prefs.
    - Adds test coverage in AiOverlayDialogPageHandlerTest verifying that notes
      persist across controller resets.
    
    Bug: none
    Change-Id: Id5cc7d641f9df295c46ef62fc27a338785e27387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263727
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696727}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -342,6 +342,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/live_caption/live_translate_controller.h"
 #endif  // !BUILDFLAG(IS_ANDROID)
 
+#if !BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_DESKTOP_ANDROID)  // nocheck
+#include "chrome/browser/ui/ai_overlay_dialog/ai_overlay_dialog_controller.h"
+#endif
+
 #if BUILDFLAG(ENABLE_DEVTOOLS_FRONTEND)
 #include "chrome/browser/devtools/devtools_window.h"
 #endif  // BUILDFLAG(ENABLE_DEVTOOLS_FRONTEND)
@@ -1747,6 +1751,9 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   AccessibilityLabelsService::RegisterProfilePrefs(registry);
   registry->RegisterBooleanPref(prefs::kRendererAccessibilityEnabled, true);
   AccessibilityUIMessageHandler::RegisterProfilePrefs(registry);
+#if !BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_DESKTOP_ANDROID)  // nocheck
+  ttc::AiOverlayDialogController::RegisterProfilePrefs(registry);
+#endif
   AimEligibilityService::RegisterProfilePrefs(registry);
   AnnouncementNotificationService::RegisterProfilePrefs(registry);
   autofill::prefs::RegisterProfilePrefs(registry);
