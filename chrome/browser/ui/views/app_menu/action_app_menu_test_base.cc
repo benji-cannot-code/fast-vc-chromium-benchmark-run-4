05c8d4162be29f97ee0d108cc8bc5882f815ba67
Emily Shack <emshack@chromium.org>
2026-09-11 18:36:12 -0700

    [ChroMenu GlowUp] Flesh out profile submenu (3/3)
    Adds sync and signin items
    
    Bug: 553556615
    Change-Id: Ibcd7506aad9ef5652aadeebd84ad61a0ea2399b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8393903
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696772}

--- chrome/browser/ui/views/app_menu/action_app_menu_test_base.cc
+++ chrome/browser/ui/views/app_menu/action_app_menu_test_base.cc
@@ -11,11 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "chrome/browser/sessions/tab_restore_service_factory.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser_actions.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/saved_tab_groups/test_support/fake_tab_group_sync_service.h"
+#include "components/sync/test/test_sync_service.h"
 #include "ui/actions/actions.h"
 
 ActionAppMenuTestBase::ActionAppMenuTestBase() = default;
@@ -24,6 +26,12 @@ ActionAppMenuTestBase::~ActionAppMenuTestBase() = default;
 void ActionAppMenuTestBase::SetUp() {
   ChromeViewsTestBase::SetUp();
   profile_ = std::make_unique<TestingProfile>();
+  SyncServiceFactory::GetInstance()->SetTestingFactory(
+      profile_.get(),
+      base::BindRepeating(
+          [](content::BrowserContext*) -> std::unique_ptr<KeyedService> {
+            return std::make_unique<syncer::TestSyncService>();
+          }));
   TabRestoreServiceFactory::GetInstance()->SetTestingFactory(
       profile_.get(), TabRestoreServiceFactory::GetDefaultFactory());
   tab_groups::TabGroupSyncServiceFactory::GetInstance()->SetTestingFactory(
@@ -79,6 +87,12 @@ void ActionAppMenuTestBase::SetUp() {
   add_action(kActionAddNewProfile, u"Add new profile");
   add_action(kActionOpenGuestProfile, u"Open Guest profile");
   add_action(kActionManageChromeProfiles, u"Manage Chrome profiles");
+  add_action(kActionShowSignin, u"Sign in to Chrome");
+  add_action(kActionTurnOnSync, u"Turn on sync");
+  add_action(kActionShowSyncSettings, u"Sync settings");
+  add_action(kActionShowSyncPassphraseDialog, u"Enter passphrase");
+  add_action(kActionShowSigninWhenPaused, u"Sign in again");
+  add_action(kActionUpgradeDialog, u"Update Chrome");
   add_action(kActionPasswordsAndAutofillSubmenu, u"Passwords and autofill");
   add_action(kActionShowPasswordManager, u"Password Manager");
   add_action(kActionShowPaymentMethods, u"Payment methods");
