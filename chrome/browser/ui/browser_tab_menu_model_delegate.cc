958de5896e9f21533869803782406bad1f250c29
Bryan Kim <bryankim@google.com>
2026-09-11 18:01:00 -0700

    [Bedrock] Modularize 3 leaf UI modules (Chunk 4)
    Extract 3 leaf modules out of //chrome/browser/ui:ui into modular GN
    targets: 1. chrome/browser/ui/browser_tab_menu_model_delegate.{h,cc} ->
       //chrome/browser/ui:browser_tab_menu_model_delegate
    2. chrome/browser/ui/browser_window_deleter.{h,cc} ->
       //chrome/browser/ui:browser_window_deleter and
       //chrome/browser/ui:browser_window_deleter_impl
    3. chrome/browser/ui/browser_commands_mac.{h,mm} ->
       //chrome/browser/ui:browser_commands_mac and
       //chrome/browser/ui:browser_commands_mac_impl
    
    TAG=agy
    CONV=cd67019c-bf4c-4b0b-a30c-8bc1a634931f
    
    Bug: 353332589
    Change-Id: Id1b35d8b9639959fba1e2d701fe4947849996076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8389197
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696755}

--- chrome/browser/ui/browser_tab_menu_model_delegate.cc
+++ chrome/browser/ui/browser_tab_menu_model_delegate.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_tab_menu_model_delegate.h"
 
-#include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
-#include "chrome/browser/ui/browser_tab_strip_model_delegate.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
