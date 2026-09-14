4c170524d4431ed6b8a9af7ee4bb6c197e71d9d9
Jingyun Liu <jingyun@google.com>
2026-09-14 12:26:31 -0700

    Exempt origin masking for internal WebUI and dictation extensions
    Change-Id: I8d9691c319a1f08591b614b8078296aac69b6e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392977
    Commit-Queue: Jingyun Liu <jingyun@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697431}

--- chrome/browser/speech/on_device_speech_recognition_impl.cc
+++ chrome/browser/speech/on_device_speech_recognition_impl.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/security_principal.h"
 #include "content/public/browser/site_instance.h"
+#include "content/public/common/url_constants.h"
 #include "media/base/media_switches.h"
 #include "media/mojo/mojom/speech_recognizer.mojom.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -44,6 +45,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_execution/model_broker_client.h"
 #include "components/optimization_guide/public/mojom/model_broker.mojom-shared.h"
 #include "components/soda/soda_util.h"
+#include "extensions/buildflags/buildflags.h"
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "chrome/common/extensions/extension_constants.h"
+#endif
 
 namespace {
 const char kOnDeviceLanguagesDownloadedKey[] = "ondevice-languages-downloaded";
@@ -567,6 +573,19 @@ bool OnDeviceSpeechRecognitionImpl::IsLanguageAvailabilityMaskedForOrigin(
   }
 
   const GURL url = render_frame_host().GetLastCommittedOrigin().GetURL();
+  // Exempt masking from internal WebUI.
+  if (url.SchemeIs(content::kChromeUIScheme)) {
+    return false;
+  }
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  // Exempt masking from internal component extensions.
+  if (url.SchemeIs(extensions::kExtensionScheme) &&
+      url.host() == extension_misc::kDictationConnectorExtensionId) {
+    return false;
+  }
+#endif
+
   if (!url.is_valid() || url.SchemeIsFile()) {
     return !transient_on_device_languages_downloaded_.contains(
         std::string(language));
