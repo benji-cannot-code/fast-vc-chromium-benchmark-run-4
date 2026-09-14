e2f702f88144c59287a8046e602ece20146d3bba
Duong Dac <ddac@chromium.org>
2026-09-14 02:42:10 -0700

    Cleanup components/supervised_user/core/common/features
    - Remove kLocalWebApprovalsWidgetSupportsUrlPayload
    - Use 2-arg definiton for kSupervisedUserVerificationPageOnAndroid
    
    Change-Id: I7768d73f9ae485fbc3461cf372b6cb5a2e3cca9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394381
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697136}

--- components/supervised_user/core/common/features.cc
+++ components/supervised_user/core/common/features.cc
@@ -40,10 +40,6 @@ const base::FeatureParam<int> kLocalWebApprovalBottomSheetLoadTimeoutMs{
 #endif  // BUILDFLAG(IS_IOS) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_WIN)
 
-BASE_FEATURE(kLocalWebApprovalsWidgetSupportsUrlPayload,
-             "PacpWidgetSupportsUrlPayload",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 bool IsGoogleBrandedBuild() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   return true;
@@ -71,7 +67,6 @@ bool IsLocalWebApprovalsEnabledForSubframes() {
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kSupervisedUserVerificationPageOnAndroid,
-             "SupervisedUserVerificationPageOnAndroid",
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
