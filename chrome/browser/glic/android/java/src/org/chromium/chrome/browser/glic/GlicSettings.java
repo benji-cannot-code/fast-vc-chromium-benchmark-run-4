2b77c71c1e1a478246696ec542cf52d1f969c18e
Hailey Wang <haileywang@google.com>
2026-09-14 13:38:46 -0700

    [Glic] Add microphone toggle to Gemini in Chrome settings on Android
    Adds a "Microphone" switch to the Permissions section, backed by the
    same glic.microphone_enabled pref as the desktop setting. The pref is
    already sent to the web client by PopulateGlobalClientInitialState().
    
    https://screenshot-v2.corp.google.com/4ripoitsl2qi0
    
    Bug: 559795132
    Change-Id: I9031b47f6ac84ec14c8b3b66f50b9ce769c74e0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8402399
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697513}

--- chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicSettings.java
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicSettings.java
@@ -74,6 +74,7 @@ public class GlicSettings extends ChromeBaseSettingsFragment {
     static final String PREFERENCE_BOTTOM_BAR_BUTTON_TOGGLE = "glic_bottom_bar_button_toggle";
 
     @VisibleForTesting static final String PERMISSION_LOCATION = "permissions_location";
+    @VisibleForTesting static final String PERMISSION_MICROPHONE = "permissions_microphone";
     private static final String PERMISSION_DEFAULT_TAB_ACCESS =
             "glic_permissions_default_tab_access";
     private static final String PERMISSION_AUTO_BROWSE = "glic_permissions_auto_browse";
@@ -284,6 +285,20 @@ public class GlicSettings extends ChromeBaseSettingsFragment {
             ensureFineLocationPermissionGranted();
         }
 
+        setupSwitchPreference(
+                PERMISSION_MICROPHONE,
+                ChromePreferenceKeys.GLIC_MICROPHONE_SETTING_ENABLED,
+                GlicPrefNames.GLIC_MICROPHONE_ENABLED,
+                (preference, newValue) -> {
+                    boolean enabled = (boolean) newValue;
+                    if (enabled) {
+                        RecordUserAction.record("Glic.Settings.Microphone.Enabled");
+                    } else {
+                        RecordUserAction.record("Glic.Settings.Microphone.Disabled");
+                    }
+                    return true;
+                });
+
         ChromeExpandableSwitchPreference tabAccessPref =
                 setupSwitchPreference(
                         PERMISSION_DEFAULT_TAB_ACCESS,
@@ -410,6 +425,7 @@ public class GlicSettings extends ChromeBaseSettingsFragment {
                 PREFERENCE_BUTTON_TOGGLE,
                 PREFERENCE_BOTTOM_BAR_BUTTON_TOGGLE,
                 PERMISSION_LOCATION,
+                PERMISSION_MICROPHONE,
                 PREF_KEY_GLIC_PERMISSIONS_ACTIVITY,
                 PREF_KEY_GLIC_EXTENSIONS,
                 PREF_LAUNCHER_ENABLED,
@@ -560,6 +576,7 @@ public class GlicSettings extends ChromeBaseSettingsFragment {
         if (mPrefChangeRegistrar != null) {
             mPrefChangeRegistrar.removeObserver(GlicPrefNames.GLIC_PINNED_TO_TABSTRIP);
             mPrefChangeRegistrar.removeObserver(GlicPrefNames.GLIC_GEOLOCATION_ENABLED);
+            mPrefChangeRegistrar.removeObserver(GlicPrefNames.GLIC_MICROPHONE_ENABLED);
             mPrefChangeRegistrar.removeObserver(GlicPrefNames.GLIC_DEFAULT_TAB_CONTEXT_ENABLED);
             mPrefChangeRegistrar.destroy();
             mPrefChangeRegistrar = null;
