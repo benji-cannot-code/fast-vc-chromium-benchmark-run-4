b6945da7d21cf96c6497d1e54d110730fa83c9a8
Timofey Chudakov <tchudakov@google.com>
2026-09-14 02:14:03 -0700

    [PI] Do not show PI notice if CanShowAutofillUi returns false.
    PI - Private Inference
    
    CanShowAutofillUi checks if the current frame is still active.
    Autofill UI should not be shown if for inactive frames.
    
    Fixed: 553317583
    Change-Id: I6abf723cf28657da0af94720028226becdde1157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8397809
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697124}

--- components/autofill/core/browser/foundations/browser_autofill_manager.cc
+++ components/autofill/core/browser/foundations/browser_autofill_manager.cc
@@ -1315,6 +1315,10 @@ bool BrowserAutofillManager::TryToShowTouchToFillSuggestions(
 
 bool BrowserAutofillManager::MaybeShowPrivateInferenceNotice(
     base::span<const Suggestion> autofill_ai_suggestions) {
+  if (!driver().CanShowAutofillUi()) {
+    return false;
+  }
+
   if (std::ranges::contains(autofill_ai_suggestions,
                             SuggestionType::kAutofillAiPrivateInferenceNotice,
                             &Suggestion::type)) {
