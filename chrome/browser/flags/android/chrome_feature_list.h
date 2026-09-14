e872e2cda0315913e604e879003c3b816163ef18
Alvin Ji <alvinji@chromium.org>
2026-09-14 10:33:20 -0700

    Clean up stale feature flag kCastDeviceFilter
    The downstream code has remove the feature through https://chrome-internal-review.git.corp.google.com/c/clank/internal/apps/+/825575
    
    Bug: 356235149
    Change-Id: I39e437d6e78d73c4341d95e80ba5fb118c99081b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8350398
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Martin Kong <martinkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697328}

--- chrome/browser/flags/android/chrome_feature_list.h
+++ chrome/browser/flags/android/chrome_feature_list.h
@@ -146,7 +146,6 @@ BASE_DECLARE_FEATURE(kCCTTabSwitcherEnabledForEmbedderExperiment);
 BASE_DECLARE_FEATURE(kCacheDeprecatedSystemLocationSetting);
 BASE_DECLARE_FEATURE(kCacheIsGoogleSigned);
 BASE_DECLARE_FEATURE(kCacheIsMultiInstanceApi31Enabled);
-BASE_DECLARE_FEATURE(kCastDeviceFilter);
 BASE_DECLARE_FEATURE(kCctTabResumption);
 BASE_DECLARE_FEATURE(kChangeUnfocusedPriority);
 BASE_DECLARE_FEATURE(kChromeNativeUrlOverriding);
