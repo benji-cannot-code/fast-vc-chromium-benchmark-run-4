f04b77faeb0172deea4cfda2c0fe95b6108a9d57
Florian Leimgruber <fleimgruber@google.com>
2026-09-14 05:05:05 -0700

    AutofillAi iOS settings: Limit subtitle to 1 line
    The disambiguation label for order & shipment entries is based on the
    product names, which may not fit in one line. This CL limits the summary
    to a single line. See bug for details.
    
    Bug: 559140810
    Change-Id: I5097e2ebeb6212c3b3364e5c615f178673813c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8399227
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697189}

--- ios/chrome/browser/settings/autofill/autofill_ai/ui/autofill_ai_entity_item.mm
+++ ios/chrome/browser/settings/autofill/autofill_ai/ui/autofill_ai_entity_item.mm
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[TableViewCellContentConfiguration alloc] init];
 
   contentConfiguration.title = self.name;
+  contentConfiguration.titleNumberOfLines = 1;
+  contentConfiguration.titleLineBreakMode = NSLineBreakByTruncatingTail;
   contentConfiguration.subtitle = self.typeDescription;
   contentConfiguration.trailingText = self.trailingText;
 
