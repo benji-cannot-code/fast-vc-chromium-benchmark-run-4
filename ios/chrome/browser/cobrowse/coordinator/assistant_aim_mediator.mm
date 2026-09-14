bffcd99eb4a7aaa1aa8c81e8a856075e1ecbbd9b
Louis Romero <lpromero@google.com>
2026-09-14 03:21:45 -0700

    Remove openerWebState in AssistantAIMMediator
    In AssistantAIMMediator, createNewWebStateForURL assigns the
    standalone Cobrowse WebState as openerWebState on the
    OpenNewTabCommand.
    
    WebStateList enforces that any WebStateOpener must belong to the
    same WebStateList instance. Setting an external opener violates
    WebStateList invariants. Furthermore, the property is unused by the
    URL loading pipeline.
    
    Remove the assignment so the command does not claim an invalid
    cross-WebStateList opener relationship.
    
    Bug: None
    Change-Id: Ia412fda4d65e1c6399f4bb9dcb18efd796bd3d9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8398408
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697159}

--- ios/chrome/browser/cobrowse/coordinator/assistant_aim_mediator.mm
+++ ios/chrome/browser/cobrowse/coordinator/assistant_aim_mediator.mm
@@ -270,7 +270,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // regular mode.
   OpenNewTabCommand* command = [OpenNewTabCommand commandWithURLFromChrome:URL
                                                                inIncognito:NO];
-  command.openerWebState = webState->GetWeakPtr();
 
   [self.sceneHandler openURLInNewTab:command];
 
