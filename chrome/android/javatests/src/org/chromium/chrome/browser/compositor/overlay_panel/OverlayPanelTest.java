756579584796bf2cb516fd8608270363e2a50bdc
Sky Malice <skym@chromium.org>
2026-09-14 09:48:14 -0700

    [Clean] Remove @NullMarked and NullAway suppressions from tests
    Remove @NullMarked and @SuppressWarnings("NullAway.Init") from
    OverlayPanelTest.
    
    Remove redundant @SuppressWarnings("NullAway.Init") from
    RecentSearchQueueTest where fields are already initialized in setUp.
    
    Tests are exempt from NullAway enforcement by presubmit and the Java
    style guide. Removing these annotations eliminates suppressions and
    keeps tests consistent with the rest of the codebase.
    
    TAG=agy
    CONV=12386494-4cd6-4c81-8dbc-65eb4aaa2b5d
    
    Bug: None
    Change-Id: Ib507b175be053b3fcf914f22292cf0db44d5ce13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8406147
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697298}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlay_panel/OverlayPanelTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlay_panel/OverlayPanelTest.java
@@ -33,7 +33,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
-import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.browser_controls.BottomControlsStacker;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
@@ -53,7 +52,6 @@ import org.chromium.ui.test.util.BlankUiTestActivity;
 /** Tests logic in the OverlayPanel. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @Batch(Batch.PER_CLASS)
-@NullMarked
 public class OverlayPanelTest {
     private static final int MOCK_VIEWPORT_WIDTH = 400;
     private static final int MOCK_VIEWPORT_HEIGHT = 1000;
@@ -80,13 +78,8 @@ public class OverlayPanelTest {
     @Mock private BottomControlsStacker mBottomControlsStacker;
     @Mock private OverlayPanelContent mMockContent;
 
-    @SuppressWarnings("NullAway.Init")
     Activity mActivity;
-
-    @SuppressWarnings("NullAway.Init")
     ActivityWindowAndroid mWindowAndroid;
-
-    @SuppressWarnings("NullAway.Init")
     TestOverlayPanel mPanel;
 
     private static class TestOverlayPanel extends OverlayPanel {
