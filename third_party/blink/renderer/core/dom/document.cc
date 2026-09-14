fb0efbd9896998f10673649820f31d7ac6c7d829
Camille Lamy <clamy@chromium.org>
2026-09-14 06:43:25 -0700

    Add use counters for BeforeUnload dialog
    This CL adds use counters tracking when a BeforeUnload dialog is shown
    and when it leads to the unload of the document being cancelled.
    
    Bug: 559573618
    Change-Id: Id7277e8efe12e01f3c1742627875622813d0e816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380484
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697226}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -4634,6 +4634,7 @@ bool Document::DispatchBeforeUnloadEvent(
   }
 
   String text = before_unload_event.returnValue();
+  UseCounter::Count(*this, WebFeature::kBeforeUnloadShowedDialog);
   RecordBeforeUnloadUse(BeforeUnloadUse::kShowDialog);
   out_before_unload_dialog_opened_time = base::TimeTicks::Now();
   did_allow_navigation =
@@ -4648,6 +4649,8 @@ bool Document::DispatchBeforeUnloadEvent(
             out_before_unload_dialog_opened_time);
     return true;
   }
+
+  UseCounter::Count(*this, WebFeature::kBeforeUnloadDialogBlockedUnload);
 
   return false;
 }
