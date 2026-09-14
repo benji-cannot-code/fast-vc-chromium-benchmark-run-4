33b8b86f36d789b1135ddb45e9f5e4bb44329b38
Georg Neis <neis@chromium.org>
2026-09-14 01:16:21 -0700

    ash: Use BrowserController in KioskBrowserLogsCollector
    Replace direct usage of TabStripModel, TabStripModelObserver, and
    GlobalBrowserCollection in KioskBrowserLogsCollector with
    ash::BrowserController::TabObserver. This decouples
    KioskBrowserLogsCollector from the tab strip model, allowing removal of
    the tab_strip include rules in app_mode/DEPS and the
    //chrome/browser/ui/tabs:tab_strip dependency in app_mode/BUILD.gn.
    
    Bug: b:369688254
    Change-Id: I368167d7c5158400ebd96b6e69b7001560a9d307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8401979
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697095}

--- chrome/browser/chromeos/app_mode/kiosk_app_level_logs_manager_wrapper_unittest.cc
+++ chrome/browser/chromeos/app_mode/kiosk_app_level_logs_manager_wrapper_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_pref_names.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_types.h"
+#include "chrome/browser/ash/browser_delegate/browser_controller_impl.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chromeos/ash/components/policy/device_local_account/device_local_account_type.h"
@@ -65,11 +66,9 @@ class KioskAppLevelLogsManagerWrapperTest
 
  private:
   content::BrowserTaskEnvironment task_environment_;
-
+  ash::BrowserControllerImpl browser_controller_;
   TestingProfile profile_;
-
   ash::KioskAppId kiosk_app_id_;
-
   std::unique_ptr<KioskAppLevelLogsManagerWrapper> wrapper_;
 };
 
--- chrome/browser/chromeos/app_mode/kiosk_browser_logs_collector_unittest.cc
+++ chrome/browser/chromeos/app_mode/kiosk_browser_logs_collector_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/test/repeating_test_future.h"
+#include "chrome/browser/ash/browser_delegate/browser_controller_impl.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_level_logs_saver.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_web_contents_observer.h"
 #include "chrome/browser/profiles/profile.h"
@@ -87,6 +88,7 @@ class KioskBrowserLogsCollectorTest : public BrowserWithTestWindowTest {
   }
 
  private:
+  ash::BrowserControllerImpl browser_controller_;
   std::vector<std::unique_ptr<BrowserWindowInterface>> browsers_;
   std::unique_ptr<KioskBrowserLogsCollector> logs_collector_;
 };
