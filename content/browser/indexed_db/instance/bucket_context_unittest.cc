5f4d07024941f3f900167e8241a9dff87731c550
Evan Stade <evanstade@microsoft.com>
2026-09-14 11:18:26 -0700

    IDB: log size of backing store
    To avoid ever doing extra work just for metrics, we log this whenever
    it's calculated, which is perhaps not quite as intuitive as "when the
    backing store is opened".
    
    Bug: none
    Change-Id: Ia5b34c034005238005fe371e2f7dbd7b294a6627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400267
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1697364}

--- content/browser/indexed_db/instance/bucket_context_unittest.cc
+++ content/browser/indexed_db/instance/bucket_context_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/files/file_util.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -70,6 +71,23 @@ TEST_F(BucketContextTest, CanUseDiskSpaceQueuing) {
   EXPECT_FALSE(success_future3.Get());
 
   tester.ExpectTotalCount("IndexedDB.QuotaCheckTime2.Success", 1);
+}
+
+TEST_F(BucketContextTest, ReadUsageFromDiskRecordsSizeHistograms) {
+  constexpr uint64_t kFileSize = base::MiB(2).InBytes();
+  const storage::BucketLocator& bucket_locator =
+      bucket_context_->bucket_locator();
+  const base::FilePath database_path = GetFilePathForTesting(bucket_locator);
+  ASSERT_TRUE(base::CreateDirectory(database_path));
+  ASSERT_TRUE(base::WriteFile(database_path.AppendASCII("data"),
+                              std::string(kFileSize, 'a')));
+
+  base::HistogramTester histogram_tester;
+  EXPECT_EQ(
+      BucketContext::ReadUsageFromDisk(bucket_locator, database_path.DirName()),
+      kFileSize);
+  histogram_tester.ExpectUniqueSample("IndexedDB.BackingStore.SizeOnDisk",
+                                      kFileSize / 1024, 1);
 }
 
 TEST_F(BucketContextTest, CanUseDiskSpaceCaching) {
