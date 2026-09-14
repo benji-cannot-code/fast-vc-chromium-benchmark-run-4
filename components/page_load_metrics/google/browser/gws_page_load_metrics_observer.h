8abf2c647ecde0f0d68d59fbf4886a51cb55280c
Jiacheng Guo <gjc@google.com>
2026-09-14 03:26:43 -0700

    Add SearchPrewarmPrerenderCoverageStatus metric and test suite
    Introduce the SearchPrewarmPrerenderCoverageStatus enum and UMA metric
    to track the full funnel of prewarm and prerender coverage on Google
    Search navigations, distinguishing preload process reuse, current tab
    process reuse, blank process reuse, other process reuse, and cold
    process creation.
    
    Bug: 473593133
    Change-Id: I4a775b2c82ab87fb851382cfc60865f9f49d2b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281891
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697161}

--- components/page_load_metrics/google/browser/gws_page_load_metrics_observer.h
+++ components/page_load_metrics/google/browser/gws_page_load_metrics_observer.h
@@ -107,6 +107,7 @@ extern const char kHistogramPrerenderHostReused[];
 extern const char kHistogramGWSPrerenderNavigationToActivation[];
 extern const char kHistogramGWSActivationToFirstContentfulPaint[];
 extern const char kHistogramGWSActivationToLargestContentfulPaint[];
+extern const char kHistogramGWSPrewarmPrerenderCoverageStatus[];
 
 extern const char kHistogramPrerenderSuffix[];
 extern const char kHistogramNonPrerenderSuffix[];
