cd526c9e3c1cc517b66a9772d8443e0482a4278e
Ernesto Izquierdo Clua <eic@google.com>
2026-09-14 11:49:41 -0700

    [ios] Add Enterprise Proxy related factories
    Adds factories for services necessary for implementing policy-controlled
    proxying in Bling. Subsequent CL's will use these services from
    ProxyServiceController [1].
    
    [1] -
    https://chromium-review.git.corp.google.com/c/chromium/src/+/8343089
    
    dd: go/secure-gateway-on-ios
    Bug: 543367420
    Change-Id: I5d4f6f8a408235fa24e9862b34577573bc711fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392577
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697392}

--- ios/chrome/browser/profile/model/keyed_service_factories.mm
+++ ios/chrome/browser/profile/model/keyed_service_factories.mm
@@ -82,6 +82,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/enterprise/groups/model/enterprise_groups_profile_handler_factory.h"
 #import "ios/chrome/browser/enterprise/identifiers/profile_id_service_factory_ios.h"
 #import "ios/chrome/browser/enterprise/model/idle/idle_service_factory.h"
+#import "ios/chrome/browser/enterprise/proxy/model/enterprise_network_auth_service_factory_ios.h"
+#import "ios/chrome/browser/enterprise/proxy/model/enterprise_proxy_error_service_factory_ios.h"
+#import "ios/chrome/browser/enterprise/proxy/model/enterprise_proxy_service_factory_ios.h"
 #import "ios/chrome/browser/enterprise/proxy/model/proxy_service_controller_factory.h"
 #import "ios/chrome/browser/enterprise/signals/model/ios_signals_aggregator_factory.h"
 #import "ios/chrome/browser/external_files/model/external_file_remover_factory.h"
@@ -351,6 +354,9 @@ void EnsureProfileKeyedServiceFactoriesBuilt() {
   DomainDiversityReporterFactory::GetInstance();
   DownloadFileServiceFactory::GetInstance();
   DownloadRecordServiceFactory::GetInstance();
+  EnterpriseNetworkAuthServiceFactoryIOS::GetInstance();
+  EnterpriseProxyErrorServiceFactoryIOS::GetInstance();
+  EnterpriseProxyServiceFactoryIOS::GetInstance();
   ExternalFileRemoverFactory::GetInstance();
   GeminiCapabilitiesManagerFactory::GetInstance();
   GeminiServiceFactory::GetInstance();
