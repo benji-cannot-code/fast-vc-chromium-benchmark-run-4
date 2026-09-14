3d63c7abbc7d9b344c639d063b958e290add2fc8
dpapad <dpapad@chromium.org>
2026-09-14 12:29:03 -0700

    Settings Lit: Migrate controls/controlled_button.ts.
    Bug: 393471368
    Change-Id: I7fb74c311caafea0eccd5b3e36d10a7358fdf9e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8396950
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697433}

--- chrome/test/data/webui/settings/downloads_page_test.ts
+++ chrome/test/data/webui/settings/downloads_page_test.ts
@@ -117,11 +117,8 @@ suite('DownloadsHandler', function() {
         downloadsPage.shadowRoot.querySelector<ControlledButtonElement>(
             '#changeDownloadsPath');
     assertTrue(!!button);
-    const crButton = button.shadowRoot!.querySelector('cr-button');
-    assertTrue(!!crButton);
-
-    assertFalse(crButton.disabled);
-    assertFalse(!!button.shadowRoot!.querySelector('cr-policy-pref-indicator'));
+    assertFalse(button.$.button.disabled);
+    assertFalse(!!button.shadowRoot.querySelector('cr-policy-pref-indicator'));
 
     prefsBrowserProxy.fakeApi.sendPrefChanges([{
       key: 'download.default_directory',
@@ -132,8 +129,8 @@ suite('DownloadsHandler', function() {
     }]);
     await microtasksFinished();
 
-    assertTrue(crButton.disabled);
-    assertTrue(!!button.shadowRoot!.querySelector('cr-policy-pref-indicator'));
+    assertTrue(button.$.button.disabled);
+    assertTrue(!!button.shadowRoot.querySelector('cr-policy-pref-indicator'));
   });
 
   test('openAdvancedDownloadsettings', async function() {
