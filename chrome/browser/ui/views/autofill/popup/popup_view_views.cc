e2725df1be6dcae296ce692aa20834f1510e31ff
Maryia Mankevich <mmaryia@google.com>
2026-09-14 04:25:43 -0700

    [Memory] Announce search results and loading for screen readers
    - Introduce a11y_announcement field in Suggestion.
    - Announce the first non-empty a11y_announcement when suggestions
      are shown or updated.
    - Set a11y_announcement only for the first loading suggestion.
    - Set a11y_announcement only for the first result suggestion.
    - Add tests.
    
    Bug: 553052730, 553054925
    Change-Id: Ib896931b90d9a6ecc85b91273d408cbf73d844c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8356105
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Maryia Mankevich <mmaryia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697179}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.cc
@@ -474,6 +474,7 @@ bool PopupViewViews::Show(
   MaybeAnnounceCurrentTabAndFootnote();
   MaybeAnnouncePasswordRecoveryPopup();
   MaybeAnnounceLoadingState();
+  MaybeAnnounceA11yOverride();
   if (!MaybeA11yFocusInformationalSuggestion()) {
     return false;
   }
@@ -961,6 +962,7 @@ void PopupViewViews::OnSuggestionsChanged(bool prefer_prev_arrow_side) {
 
   MaybeAnnouncePasswordRecoveryPopup();
   MaybeAnnounceLoadingState();
+  MaybeAnnounceA11yOverride();
   if (!MaybeA11yFocusInformationalSuggestion()) {
     return;
   }
@@ -1205,6 +1207,18 @@ void PopupViewViews::MaybeAnnounceLoadingState() {
     a11y_announcer_.Run(l10n_util::GetStringUTF16(
                             IDS_AUTOFILL_BNPL_PROGRESS_DIALOG_LOADING_MESSAGE),
                         /*polite=*/true);
+  }
+}
+
+void PopupViewViews::MaybeAnnounceA11yOverride() {
+  if (!controller_) {
+    return;
+  }
+  for (const Suggestion& suggestion : controller_->GetSuggestions()) {
+    if (suggestion.a11y_announcement) {
+      a11y_announcer_.Run(*suggestion.a11y_announcement, /*polite=*/true);
+      break;
+    }
   }
 }
 
