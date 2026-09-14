dbe83b2d46882699aa6c8bf088d3fc83be7b41e2
Yao Xiao <yaoxia@chromium.org>
2026-09-14 16:59:22 -0700

    [AdTracker] Move DisplayAdElementMonitor to ad_tracker
    DisplayAdElementMonitor handles ad element tracking and visibility
    reporting. Move it from core/html to core/ad_tracker for better
    grouping and owner coverage.
    
    Bug: 435683890
    Change-Id: I199acd917f95ea42eff992a49b5d4f8a9e796952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8401267
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697641}

--- third_party/blink/renderer/core/ad_tracker/display_ad_element_monitor_test.cc
+++ third_party/blink/renderer/core/ad_tracker/display_ad_element_monitor_test.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/core/html/display_ad_element_monitor.h"
+#include "third_party/blink/renderer/core/ad_tracker/display_ad_element_monitor.h"
 
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
