1a795a95eae9793db89b99a74dea6c3a0886cd34
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-09-14 02:18:03 -0700

    [rlz] Remove SendFinancialPingDuringShutdown test
    RlzLibTest.SendFinancialPingDuringShutdown is fundamentally flawed:
    
    - Inherent race: Concurrently resetting the factory from an IO thread
      while SendFinancialPing() runs on the main thread is
      non-deterministic.
    - Mojo sequence violation: Simulating responses on the main thread for
      a request bound to ThreadPool crashes MultiplexRouter DCHECKs.
    - Test baggage: Requires maintaining test-only global state in
      FinancialPing.
    
    Remove the test and the associated test hooks.
    
    Bug: chromium:351564777
    Change-Id: I28333a5f06f4666b09c1218afb928984feb0808a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380680
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697125}

--- rlz/lib/financial_ping.h
+++ rlz/lib/financial_ping.h
@@ -69,12 +69,6 @@ class FinancialPing {
   FinancialPing() {}
   ~FinancialPing() {}
 };
-
-namespace test {
-void ResetSendFinancialPingInterrupted();
-bool WasSendFinancialPingInterrupted();
-}  // namespace test
-
 }  // namespace rlz_lib
 
 
