fa31d48ef1390369db85bca041683178949812ea
Rui Chen <chesterchen@chromium.org>
2026-09-14 10:55:17 -0700

    [AW] Record webview low entropy source in system profile metrics
    Record the device wide WebView Low Entropy Source (WVLES) received from
    the variations seed in AndroidMetricsProvider and populate the
    webview_low_entropy_source field in SystemProfileProto.
    
    This enables downstream analysis pipelines for handling Device Synched
    Finch experiment.
    
    Bug: 542173689, 542574005
    Change-Id: If6f7f12681d34d2696e5a6b4585bb7779c7b0035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8348131
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Commit-Queue: Rui Chen <chesterchen@chromium.org>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697343}

---
+++ android_webview/browser/metrics/aw_entropy_state_provider.cc
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/metrics/aw_entropy_state_provider.h"
+
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
+#include "third_party/metrics_proto/system_profile.pb.h"
+
+namespace android_webview {
+
+namespace {
+
+// Default value indicating the low entropy source has not been set.
+constexpr int kLowEntropySourceNotSet = -1;
+
+}  // namespace
+
+AwEntropyStateProvider::AwEntropyStateProvider(PrefService* local_state)
+    : metrics::EntropyStateProvider(local_state), pref_service_(local_state) {}
+
+AwEntropyStateProvider::~AwEntropyStateProvider() = default;
+
+// static
+void AwEntropyStateProvider::RegisterPrefs(PrefRegistrySimple* registry) {
+  registry->RegisterIntegerPref(prefs::kWebViewLowEntropySource,
+                                kLowEntropySourceNotSet);
+}
+
+void AwEntropyStateProvider::ProvideSystemProfileMetrics(
+    metrics::SystemProfileProto* system_profile) {
+  metrics::EntropyStateProvider::ProvideSystemProfileMetrics(system_profile);
+  if (pref_service_) {
+    int value = pref_service_->GetInteger(prefs::kWebViewLowEntropySource);
+    if (value >= 0) {
+      system_profile->set_webview_low_entropy_source(value);
+    }
+  }
+}
+
+}  // namespace android_webview
--- android_webview/browser/metrics/aw_metrics_service_client.cc
+++ android_webview/browser/metrics/aw_metrics_service_client.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/metrics/android_metrics_log_uploader.h"
 #include "android_webview/browser/metrics/android_metrics_provider.h"
+#include "android_webview/browser/metrics/aw_entropy_state_provider.h"
 #include "android_webview/browser/metrics/aw_metrics_service_accessor.h"
 #include "android_webview/common/aw_features.h"
 #include "base/android/callback_android.h"
@@ -46,7 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/content/subprocess_metrics_provider.h"
 #include "components/metrics/cpu_metrics_provider.h"
 #include "components/metrics/drive_metrics_provider.h"
-#include "components/metrics/entropy_state_provider.h"
 #include "components/metrics/file_metrics_provider.h"
 #include "components/metrics/metrics_features.h"
 #include "components/metrics/metrics_pref_names.h"
@@ -417,7 +417,7 @@ void AwMetricsServiceClient::RegisterMetricsProvidersAndInitState() {
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<metrics::CPUMetricsProvider>());
   metrics_service_->RegisterMetricsProvider(
-      std::make_unique<metrics::EntropyStateProvider>(local_state_));
+      std::make_unique<AwEntropyStateProvider>(local_state_));
   metrics_service_->RegisterMetricsProvider(
       std::make_unique<metrics::ScreenInfoMetricsProvider>());
   metrics_service_->RegisterMetricsProvider(
@@ -892,6 +892,7 @@ void AwMetricsServiceClient::RegisterMetricsPrefs(
   metrics::FileMetricsProvider::RegisterPrefs(registry);
   metrics::StabilityMetricsHelper::RegisterPrefs(registry);
   AndroidMetricsProvider::RegisterPrefs(registry);
+  AwEntropyStateProvider::RegisterPrefs(registry);
 }
 
 // static
