57be9817dc5efd6bee4e8deb3904c162aedd79f0
Bofeng Chen <bofengchen@google.com>
2026-09-14 14:04:17 -0700

    [composebox] Expose cr-composebox-submit via $ in TestComposeboxMixin
    Adds an id="submit" attribute to the composebox submit element in
    TestComposeboxMixin and types it on the $ property. This simplifies test
    code by allowing direct access to the submit element and its icon,
    eliminating the need for manual shadowRoot query helpers in tests.
    Updates autocomplete and voice search tests to access the submit element
    directly through $ instead of using querySelector or custom helper
    functions.
    
    Bug: 561529713
    Change-Id: Ibd8217347117b0a836a9ad649b54947d9e7ddca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8406206
    Commit-Queue: Bofeng Chen <bofengchen@google.com>
    Reviewed-by: Wenhu Cheng <wenhuc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697540}

--- chrome/test/data/webui/cr_components/composebox/composebox_voice_search_test.ts
+++ chrome/test/data/webui/cr_components/composebox/composebox_voice_search_test.ts
@@ -1167,11 +1167,7 @@ suite('ComposeboxVoiceSearch', () => {
     await microtasksFinished();
 
     searchboxHandler.resetResolver('submitQuery');
-    const mainSubmitButton =
-        composeboxElement.shadowRoot.querySelector<HTMLElement>(
-            'cr-composebox-submit');
-    assertTrue(!!mainSubmitButton);
-
+    const mainSubmitButton = composeboxElement.$.submit;
     mainSubmitButton.dispatchEvent(
         new CustomEvent('submit-focusin', {bubbles: true, composed: true}));
     await microtasksFinished();
