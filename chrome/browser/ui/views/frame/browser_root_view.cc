d0ec9e03e008f16588d81aaf3f9b8fc922f58851
Bryan Kim <bryankim@google.com>
2026-09-14 23:05:02 -0700

    [Bedrock] Modularize 4 frame layout & root view leaves (Chunk 5)
    Extract 4 leaf modules from chrome/browser/ui/BUILD.gn
    static_library("ui") into fine-grained targets in
    chrome/browser/ui/views/frame/BUILD.gn:
    - views/frame/layout/browser_view_app_layout_impl.{h,cc}
    - views/frame/layout/browser_view_popup_layout_impl.{h,cc}
    - views/frame/layout/browser_view_tabbed_layout_impl.{h,cc}
    - views/frame/browser_root_view.{h,cc}
    
    TAG=agy
    CONV=f3dbd129-50e8-4a6c-8ad9-53ad9822cabe
    
    Bug: 353332589
    Change-Id: Ib8dd7e4d578f49d7f666b5569bb0707472662546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8402351
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697843}

--- chrome/browser/ui/views/frame/browser_root_view.cc
+++ chrome/browser/ui/views/frame/browser_root_view.cc
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_user_gesture_details.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/browser_widget.h"
-#include "chrome/browser/ui/views/frame/horizontal_tab_strip_region_view.h"
-#include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "components/omnibox/browser/autocomplete_classifier.h"
 #include "components/omnibox/browser/autocomplete_match.h"
