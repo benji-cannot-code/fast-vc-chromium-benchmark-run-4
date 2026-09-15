97a64b04e4a04fbfdad8df69c85bc0b22a50cb49
Tomek Jurkiewicz <tju@google.com>
2026-09-15 05:54:28 -0700

    Clean up ChildAccountService constructor and method order
    Move the ChildAccountService destructor and Init() implementation below
    the constructor, and remove the redundant UpdateForceGoogleSafeSearch()
    call on startup.
    
    This move is safe because whatever happened in Init() had to happen
    after the pref service was ready (which is still true because the
    service depends on the Pref Service and is created eagerly). The
    extension part is also safe but note that the old comment on the Init()
    call is obsolete: extension-related initialization for supervised users
    is performed in SupervisedUserExtensionsDelegate, which now happens
    safely after the constructor of Child Account Service runs.
    
    Skip child user / account consistency check for ChromeOS TestingProfile
    tests explicitly: previously the check was not executed neither because
    Init() was never called in these tests.
    
    TAG=agy
    CONV=24a3a9e5-632c-4b76-a4a2-5326d194ae33
    
    Bug: 469335277
    Change-Id: I0fde27e93e560674df51b61379b16083552d9da4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8403025
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698012}

--- components/supervised_user/core/browser/child_account_service.cc
+++ components/supervised_user/core/browser/child_account_service.cc
@@ -56,11 +56,7 @@ ChildAccountService::ChildAccountService(
   OnSupervisionStatusChanged();
 
   identity_manager_observer_.Observe(identity_manager);
-}
 
-ChildAccountService::~ChildAccountService() = default;
-
-void ChildAccountService::Init() {
   std::move(check_user_child_status_callback_)
       .Run(supervised_user::IsSubjectToParentalControls(user_prefs_.get()));
 
@@ -72,8 +68,12 @@ void ChildAccountService::Init() {
 
   if (!primary_account_info.IsEmpty()) {
     OnExtendedAccountInfoUpdated(primary_account_info);
-    UpdateForceGoogleSafeSearch();
   }
+}
+
+ChildAccountService::~ChildAccountService() = default;
+
+void ChildAccountService::Init() {
 }
 
 void ChildAccountService::Shutdown() {
