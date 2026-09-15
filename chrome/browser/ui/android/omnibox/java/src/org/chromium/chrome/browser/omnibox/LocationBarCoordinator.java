6e81f364e57dca2abba1d0732a0fbd3737b43c59
Jakob Koblinsky <jkoblinsky@google.com>
2026-09-15 09:36:54 -0700

    [Omnibox] Extract LocationBarNavigator domain controller
    Extract URL loading and navigation responsibilities from
    LocationBarMediator into a dedicated domain controller,
    LocationBarNavigator.
    
    Advantages: 1. The new class hides complexity and exposes a single
    public method. 2. The new class can be tested directly without all the
    mediator setup.
    
    Methods moved from LocationBarMediator to LocationBarNavigator are 1:1,
    with only loadUrl adapting to return a boolean to signal whether the
    load was consumed.
    
    Diff analysis of unit test transfer (1:1 minus LBM specific) stuff ->
    https://paste.googleplex.com/5906049845035008
    
    TAG=agy
    CONV=8b3afc31-96e2-4a9e-b6f0-4ecc464bab22
    
    Bug: 543837915
    Change-Id: I76b783477f41c322708fabb7f95a2f738615a299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8381225
    Commit-Queue: Jakob Koblinsky <jkoblinsky@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698174}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarCoordinator.java
@@ -382,6 +382,15 @@ public class LocationBarCoordinator
         }
         // TODO(crbug.com/40733049): Inject LocaleManager instance to LocationBarCoordinator instead
         // of using the singleton.
+        LocationBarNavigator locationBarNavigator =
+                new LocationBarNavigator(
+                        locationBarDataProvider,
+                        profileObservableSupplier,
+                        templateUrlServiceSupplier,
+                        tabModelSelectorSupplier,
+                        overrideUrlLoadingDelegate,
+                        LocaleManager.getInstance(),
+                        omniboxUma);
         mLocationBarMediator =
                 new LocationBarMediator(
                         context,
@@ -390,17 +399,14 @@ public class LocationBarCoordinator
                         mResourceProvider,
                         uiOverrides,
                         profileObservableSupplier,
-                        overrideUrlLoadingDelegate,
-                        LocaleManager.getInstance(),
+                        locationBarNavigator,
                         templateUrlServiceSupplier,
                         backKeyBehavior,
                         windowAndroid,
                         isTabletWindow() && isTabletLayout(),
                         LensController.getInstance(),
-                        omniboxUma,
                         isToolbarMicEnabledSupplier,
                         mOmniboxDropdownEmbedderImpl,
-                        tabModelSelectorSupplier,
                         browserControlsStateProvider,
                         modalDialogManagerSupplier,
                         mPageZoomIndicatorCoordinator,
