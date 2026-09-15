974e0ed8b6b9946e8247cbbb008bf7ef003b3256
Sylvain Defresne <sdefresne@chromium.org>
2026-09-15 11:14:45 -0700

    [ios] Improve OmahaService callbacks
    Change once callbacks to receive the details by const reference
    (for consistency). Update Start(...) to accept a null callback
    (this avoid posting a task to execute base::DoNothing()).
    
    Use base::BindPostTask(...) to simplify calling the callback from
    the background thread and special case null callbacks.
    
    Update the unit tests to avoid using callback wrapping blocks when
    a regular callback using base::Unretained(this) can work.
    
    Bug: 555698908
    Change-Id: I20691c8ae41b3f9750794d7cf5ae6ab008f47a39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380660
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698249}

--- ios/chrome/app/startup_tasks.mm
+++ ios/chrome/app/startup_tasks.mm
@@ -32,8 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Public methods.
 
 - (void)initializeOmaha {
-  OmahaService::Start(GetApplicationContext()->GetSharedURLLoaderFactory(),
-                      base::DoNothing());
+  OmahaService::Start(GetApplicationContext()->GetSharedURLLoaderFactory());
 }
 
 - (void)registerForApplicationWillResignActiveNotification {
--- ios/chrome/browser/omaha/model/omaha_service_unittest.mm
+++ ios/chrome/browser/omaha/model/omaha_service_unittest.mm
@@ -392,9 +392,7 @@ TEST_F(OmahaServiceTest, OneOffSuccess) {
                                             base::Unretained(this)));
 
   service.one_off_check_callback_ =
-      base::BindOnce(^(UpgradeRecommendedDetails details) {
-        OmahaServiceTest::OneOffCheck(details);
-      });
+      base::BindOnce(&OmahaServiceTest::OneOffCheck, base::Unretained(this));
   CleanService(&service, std::string(version_info::GetVersionNumber()));
 
   service.SendPing();
@@ -436,9 +434,7 @@ TEST_F(OmahaServiceTest, OngoingPingOneOffCallbackUsed) {
   // One off callback set during ongoing ping, it should now be used for
   // response.
   service.one_off_check_callback_ =
-      base::BindOnce(^(UpgradeRecommendedDetails details) {
-        OmahaServiceTest::OneOffCheck(details);
-      });
+      base::BindOnce(&OmahaServiceTest::OneOffCheck, base::Unretained(this));
 
   auto* pending_request = test_url_loader_factory_.GetPendingRequest(0);
   test_url_loader_factory_.SimulateResponseForPendingRequest(
@@ -461,9 +457,7 @@ TEST_F(OmahaServiceTest, OneOffCallbackUsedOnlyOnce) {
                                             base::Unretained(this)));
 
   service.one_off_check_callback_ =
-      base::BindOnce(^(UpgradeRecommendedDetails details) {
-        OmahaServiceTest::OneOffCheck(details);
-      });
+      base::BindOnce(&OmahaServiceTest::OneOffCheck, base::Unretained(this));
   CleanService(&service, std::string(version_info::GetVersionNumber()));
 
   service.SendPing();
@@ -503,9 +497,7 @@ TEST_F(OmahaServiceTest, ScheduledPingDuringOneOffDropped) {
                                             base::Unretained(this)));
 
   service.one_off_check_callback_ =
-      base::BindOnce(^(UpgradeRecommendedDetails details) {
-        OmahaServiceTest::OneOffCheck(details);
-      });
+      base::BindOnce(&OmahaServiceTest::OneOffCheck, base::Unretained(this));
   CleanService(&service, std::string(version_info::GetVersionNumber()));
 
   service.SendPing();
