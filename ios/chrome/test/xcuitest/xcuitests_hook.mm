03c73ca658d67bbb05fc6847795d11f07ca6f079
Michael Tatarski <mtatarski@google.com>
2026-09-14 22:30:31 -0700

    [iOS][Test] Add GCMProfileService test hook for EarlGrey tests
    Add a hook in tests_hook to allow overriding the GCMProfileService
    factory in test runs. In EarlGrey tests, instantiate a test profile
    service backed by FakeGCMDriverForInstanceID.
    
    This allows the client to register for instance ID tokens and FCM
    messages in EarlGrey test runs, which is necessary for sync
    invalidations.
    
    Bug: 556562996
    Change-Id: Ic454768ff24bd57fe47fb17a98004707f2fe221c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8398586
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697823}

--- ios/chrome/test/xcuitest/xcuitests_hook.mm
+++ ios/chrome/test/xcuitest/xcuitests_hook.mm
@@ -110,6 +110,11 @@ std::unique_ptr<commerce::ShoppingService> CreateShoppingService(
   return nullptr;
 }
 
+std::unique_ptr<gcm::GCMProfileService> CreateGCMProfileService(
+    ProfileIOS* profile) {
+  return nullptr;
+}
+
 void DataSharingServiceHooks(
     data_sharing::DataSharingService* data_sharing_service) {}
 
