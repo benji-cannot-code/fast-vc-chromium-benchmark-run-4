628f453d3ba8745e12597cc9e0920a8fea271903
Stefano Duo <stefanoduo@google.com>
2026-09-15 05:45:05 -0700

    Support target_network in net::BidirectionalStream
    Add a new target_network parameter to BidirectionalStreamRequestInfo.
    This is simply passed down to HttpRequestInfo::target_network when a
    BidirectionalStream is started. From here, the pre-existing work done to
    support target_network for UrlRequest takes over and no further changes
    are necessary. This is confirmed via a new multi-network test suite
    specific to BidirectionalStream.
    
    Bug: 549645425
    Change-Id: I3f8fd29fe1ecaaf9d5ddef7f09a15f802a741ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259898
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698010}

--- net/http/bidirectional_stream_request_info.h
+++ net/http/bidirectional_stream_request_info.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "net/base/net_export.h"
+#include "net/base/network_handle.h"
 #include "net/base/request_priority.h"
 #include "net/http/http_request_headers.h"
 #include "net/socket/socket_tag.h"
@@ -51,6 +52,10 @@ struct NET_EXPORT BidirectionalStreamRequestInfo {
   // Suggests the period the broken connection detector should use to check
   // the status of the connection.
   base::TimeDelta heartbeat_interval;
+
+  // The target network to bind this stream to, or kInvalidNetworkHandle to
+  // use the default network.
+  handles::NetworkHandle target_network = handles::kInvalidNetworkHandle;
 };
 
 }  // namespace net
