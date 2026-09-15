e5d79a65bbe51ff881ebe83e89a6f59338cb832d
Harsh Shukla <hvs@google.com>
2026-09-14 17:07:01 -0700

    Remove expired flag autofill-enable-prefetching-risk-data-for-retrieval
    The flag kAutofillEnablePrefetchingRiskDataForRetrieval has expired.
    The feature was never ramped up or launched. Remove the feature flag,
    associated chrome://flags entries, flag metadata, and flag descriptions
    on desktop and iOS, and clean up the unused risk data prefetching and
    caching logic across CreditCardAccessManager and ChromePaymentsAutofillClient.
    
    TAG=agy
    CONV=376634aa-7eee-42a4-9903-9091695c8b30
    
    Bug: 465360232
    Change-Id: I10845493cbf13dff030e796aeb30d7f43c5c318e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7907224
    Reviewed-by: Olivia Saul <osaul@google.com>
    Commit-Queue: Harsh Shukla <hvs@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697651}

--- chrome/browser/ui/autofill/payments/chrome_payments_autofill_client_unittest.cc
+++ chrome/browser/ui/autofill/payments/chrome_payments_autofill_client_unittest.cc
@@ -210,10 +210,6 @@ class MockPaymentsChurnedUsersBubbleController
 class ChromePaymentsAutofillClientTest
     : public ChromeRenderViewHostTestHarness {
  public:
-  ChromePaymentsAutofillClientTest() {
-    feature_list_.InitAndEnableFeature(
-        features::kAutofillEnablePrefetchingRiskDataForRetrieval);
-  }
 
   void SetUp() override {
     ChromeRenderViewHostTestHarness::SetUp();
@@ -324,7 +320,6 @@ class ChromePaymentsAutofillClientTest
 #endif
 
  private:
-  base::test::ScopedFeatureList feature_list_;
 #if BUILDFLAG(IS_ANDROID)
   base::MockCallback<AccessoryController::FillingSourceObserver>
       mock_filling_source_observer_;
@@ -860,19 +855,6 @@ TEST_F(ChromePaymentsAutofillClientTest, GetPaymentsWindowManager) {
     EXPECT_NE(chrome_payments_client()->GetPaymentsWindowManager(), nullptr);
 }
 
-TEST_F(ChromePaymentsAutofillClientTest, RiskDataCaching_DataCached) {
-  base::MockCallback<base::OnceCallback<void(const std::string&)>> callback1;
-  base::MockCallback<base::OnceCallback<void(const std::string&)>> callback2;
-  chrome_payments_client()->SetCachedRiskDataLoadedCallbackForTesting(
-      callback1.Get());
-  chrome_payments_client()->SetRiskDataForTesting("risk_data");
-
-  EXPECT_CALL(callback1, Run("risk_data")).Times(1);
-  EXPECT_CALL(callback2, Run).Times(0);
-
-  chrome_payments_client()->LoadRiskData(callback2.Get());
-}
-
 // Test that BNPL strategy is created and returned correctly.
 TEST_F(ChromePaymentsAutofillClientTest, GetBnplStrategy) {
   payments::BnplStrategy* strategy =
@@ -1038,16 +1020,7 @@ TEST_P(ChromePaymentsAutofillClientWalletBrandingTest,
 
 #if !BUILDFLAG(IS_ANDROID)
 class ChromePaymentsAutofillIOSPromoClientTest
-    : public ChromePaymentsAutofillClientTest {
- public:
-  ChromePaymentsAutofillIOSPromoClientTest() {
-    feature_list_.InitAndEnableFeature(
-        features::kAutofillEnablePrefetchingRiskDataForRetrieval);
-  }
-
- private:
-  base::test::ScopedFeatureList feature_list_;
-};
+    : public ChromePaymentsAutofillClientTest {};
 
 // Test that calling `CreditCardUploadCompleted` still calls
 // SaveCardBubbleControllerImpl::ShowConfirmationBubbleView on card upload
