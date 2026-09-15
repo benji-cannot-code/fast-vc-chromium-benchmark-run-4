9cff6c55f218babeec8a1f8f691e7338c5dd4fa1
Angela Novakovic <novakovica@google.com>
2026-09-15 09:44:56 -0700

    [iOS][AtMemory] Make search result info button accessible to VoiceOver
    Previously, AtMemory search result cells prevented VoiceOver users from
    focusing on the info button, which also lacked an accessible label, thus
    not allowing VoiceOver users to access Granular fill.
    
    Bug: 555813713
    Change-Id: Ieb04c9b58054d1b2f6c83d397e8186a0692d95c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395114
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698183}

--- ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/check.h"
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
+#import "base/strings/sys_string_conversions.h"
 #import "build/buildflag.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/autofill/atmemory/public/at_memory_commands.h"
@@ -619,7 +620,16 @@ enum class ItemIdentifier {
   cell.accessibilityIdentifier =
       GetAtMemorySearchResultCellAccessibilityIdentifier(itemIdentifier.title);
 
-  cell.accessoryView = [self infoButtonForSearchItem:itemIdentifier];
+  UIButton* infoButton = [self infoButtonForSearchItem:itemIdentifier];
+  cell.accessoryView = infoButton;
+
+  // Make the cell a container so both the suggestion row and the info button
+  // are accessible to VoiceOver.
+  cell.isAccessibilityElement = NO;
+  cell.contentView.isAccessibilityElement = YES;
+  cell.contentView.accessibilityLabel = configuration.accessibilityLabel;
+  cell.contentView.accessibilityTraits = UIAccessibilityTraitButton;
+  cell.accessibilityElements = @[ cell.contentView, infoButton ];
 
   return cell;
 }
@@ -633,6 +643,9 @@ enum class ItemIdentifier {
   infoButton.tag = item.index;
   infoButton.accessibilityIdentifier =
       GetAtMemorySearchResultInfoButtonAccessibilityIdentifier(item.title);
+  infoButton.accessibilityLabel = l10n_util::GetNSStringF(
+      IDS_IOS_MANUAL_FALLBACK_THREE_DOT_MENU_BUTTON_ACCESSIBILITY_LABEL,
+      base::SysNSStringToUTF16(item.title));
   [infoButton addTarget:self
                  action:@selector(handleInfoButtonTap:)
        forControlEvents:UIControlEventTouchUpInside];
