8de3b99fda6debceca27bb05d75f7d6e4acd1ba4
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-09-15 02:09:41 -0700

    Revert "[AESD] Force bottom sheet version of the disclaimer on foldables"
    This reverts commit b313fe55cd338e72573671a556aac814e0f1b7bb.
    
    Reason for revert: Turns out we don't want to force bottom sheet on foldables.
    
    Original change's description:
    > [AESD] Force bottom sheet version of the disclaimer on foldables
    >
    > Foldable devices, despite the bigger screen should be displaying the
    > BottomSheet version of the EnterpriseSignalsDisclaimer instead of the
    > modal dialog version.
    >
    > This CL adds a check for `DeviceInfo.isFoldable()` when deciding which
    > form to choose.
    >
    > Bug: 556166676
    > Change-Id: I913f1b97242185d46cfddf60bf2a7fc065c0a91f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8386896
    > Reviewed-by: Janek Zarzycki <janzarzycki@google.com>
    > Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1697081}
    
    Bug: 556166676
    Change-Id: Ib022ac7a094e739f2e4fe13c18219909df672266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8406709
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Janek Zarzycki <janzarzycki@google.com>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697930}

--- chrome/browser/ui/android/enterprise_signals_disclaimer/java/src/org/chromium/chrome/browser/ui/enterprise_signals_disclaimer/EnterpriseSignalsDisclaimerCoordinator.java
+++ chrome/browser/ui/android/enterprise_signals_disclaimer/java/src/org/chromium/chrome/browser/ui/enterprise_signals_disclaimer/EnterpriseSignalsDisclaimerCoordinator.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.ui.enterprise_signals_disclaimer;
 
 import android.content.Context;
 
-import org.chromium.base.DeviceInfo;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.signin.services.SigninManager;
@@ -68,7 +67,9 @@ public class EnterpriseSignalsDisclaimerCoordinator
         assert identityManager.hasPrimaryAccount();
 
         EnterpriseSignalsDisclaimerView view;
-        if (shouldUseModalDialogInsteadOfBottomSheet(context)) {
+        // For the large form factors a modal dialog will be displayed, while smaller screens will
+        // get a bottom sheet.
+        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(context)) {
             view = EnterpriseSignalsDisclaimerView.createForModalDialog(context);
             mDisclaimerHost =
                     new ModalDialogDisclaimerHost(
@@ -149,15 +150,5 @@ public class EnterpriseSignalsDisclaimerCoordinator
                 || dismissalCause == DismissalCause.DISMISSED_BY_SWIPE_DOWN
                 || dismissalCause == DismissalCause.DISMISSED_BY_TAP_OUTSIDE
                 || dismissalCause == DismissalCause.DISMISSED_BY_CLOSE_BUTTON;
-    }
-
-    /**
-     * For large form factor devices, a modal dialog will be used instead of a bottom sheet, unless
-     * the device is a foldable. On folds the bottom sheet dialog should be used to be consistent
-     * between folded and unfolded states.
-     */
-    static boolean shouldUseModalDialogInsteadOfBottomSheet(Context context) {
-        return DeviceFormFactor.isNonMultiDisplayContextOnTablet(context)
-                && !DeviceInfo.isFoldable();
     }
 }
