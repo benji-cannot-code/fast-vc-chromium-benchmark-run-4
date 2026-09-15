7c0f5aea5d50df99a865a23003d768044ef988b6
Xi Han <hanxi@google.com>
2026-09-15 11:35:37 -0700

    [Composeplate] Clean up ctor of ComposeplateCoordinator.
    Bug: 561995440
    Change-Id: Id9e089a61d244efdc24b1dfae95ff756d64641ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8414346
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698262}

--- chrome/browser/composeplate/android/java/src/org/chromium/chrome/browser/composeplate/ComposeplateCoordinator.java
+++ chrome/browser/composeplate/android/java/src/org/chromium/chrome/browser/composeplate/ComposeplateCoordinator.java
@@ -14,7 +14,6 @@ import androidx.annotation.StyleRes;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.ntp.NewTabPageUtils;
-import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.util.BrowserUiUtils.ModuleTypeOnStartAndNtp;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
@@ -29,9 +28,8 @@ public class ComposeplateCoordinator {
      * Constructs a new ComposeplateCoordinator.
      *
      * @param parentView The parent {@link ViewGroup} for the composeplate.
-     * @param profile The current user profile.
      */
-    public ComposeplateCoordinator(ViewGroup parentView, Profile profile) {
+    public ComposeplateCoordinator(ViewGroup parentView) {
         mModel = new PropertyModel(ComposeplateProperties.ALL_KEYS);
         mView = parentView.findViewById(R.id.composeplate_view);
         PropertyModelChangeProcessor.create(mModel, mView, ComposeplateViewBinder::bind);
--- chrome/browser/composeplate/android/junit/src/org/chromium/chrome/browser/composeplate/ComposeplateCoordinatorUnitTest.java
+++ chrome/browser/composeplate/android/junit/src/org/chromium/chrome/browser/composeplate/ComposeplateCoordinatorUnitTest.java
@@ -38,7 +38,6 @@ import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
-import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.util.BrowserUiUtils.ModuleTypeOnStartAndNtp;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -52,7 +51,6 @@ public class ComposeplateCoordinatorUnitTest {
     @Mock private View mIncognitoButton;
     @Mock private View mComposeplateButton;
     @Mock private View.OnClickListener mOriginalOnClickListener;
-    @Mock private Profile mProfile;
 
     private Context mContext;
     private ComposeplateCoordinator mCoordinator;
@@ -65,7 +63,6 @@ public class ComposeplateCoordinatorUnitTest {
                         ApplicationProvider.getApplicationContext(),
                         R.style.Theme_BrowserUI_DayNight);
         IncognitoUtils.setEnabledForTesting(true);
-        assertTrue(IncognitoUtils.isIncognitoModeEnabled(mProfile));
 
         when(mParentView.findViewById(R.id.composeplate_view)).thenReturn(mComposeplateView);
         when(mParentView.getResources()).thenReturn(mContext.getResources());
@@ -75,7 +72,7 @@ public class ComposeplateCoordinatorUnitTest {
         when(mComposeplateView.findViewById(R.id.composeplate_button))
                 .thenReturn(mComposeplateButton);
 
-        mCoordinator = new ComposeplateCoordinator(mParentView, mProfile);
+        mCoordinator = new ComposeplateCoordinator(mParentView);
         mPropertyModel = mCoordinator.getModelForTesting();
     }
 
