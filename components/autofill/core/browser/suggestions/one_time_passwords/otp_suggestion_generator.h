d81a947a2341a9e818af009417893bc8482578ca
Matthias Körber <koerber@google.com>
2026-09-15 04:20:26 -0700

    [OTP] Format OTP suggestions in BuildOtpSuggestions
    Format OTP suggestions using SuggestionType::kGmailOneTimePasswordEntry
    with Suggestion::Icon::kGmail, and append a separator along with a
    kOpenGmailForOtps footer suggestion.
    
    TAG=agy
    CONV=fac17043-fea1-4f97-a8e1-859827bb390c
    
    Fixed: 556170227
    Change-Id: Ifb52305f5ff3469982f3df512d55aab408f479ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394612
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697982}

--- components/autofill/core/browser/suggestions/one_time_passwords/otp_suggestion_generator.h
+++ components/autofill/core/browser/suggestions/one_time_passwords/otp_suggestion_generator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/foundations/autofill_client.h"
 #include "components/autofill/core/browser/suggestions/suggestion.h"
 #include "components/autofill/core/browser/suggestions/suggestion_generator.h"
+#include "components/autofill/core/browser/suggestions/suggestion_type.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/form_field_data.h"
 
@@ -23,11 +25,12 @@ namespace autofill {
 
 class OtpManager;
 
-// Generates OTP suggestions from the provided vector of retrieved OTP values.
+// Generates OTP suggestions from the provided span of retrieved OTP values.
 // TODO(crbug.com/409962888): Cleanup once AutofillNewSuggestionGeneration is
 // launched.
 std::vector<Suggestion> BuildOtpSuggestions(
-    std::vector<std::string> one_time_passwords);
+    base::span<const std::string> one_time_passwords,
+    SuggestionType type = SuggestionType::kOneTimePasswordEntry);
 
 // A `SuggestionGenerator` for `FillingProduct::kOneTimePassword`.
 class OtpSuggestionGenerator : public SuggestionGenerator {
