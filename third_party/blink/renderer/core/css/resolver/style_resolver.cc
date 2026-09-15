637ec5c81715e39a32c42f6a9ade76393067937d
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-09-15 14:02:58 -0700

    [style] Make StyleForInitialLetterText return a reference.
    Most consumers of ComputedStyle expect a reference now.
    
    There should be no behaviour change.
    
    Change-Id: I7f6d23a252cc0caebcf0b8aea7055e054a022f9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8186538
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698367}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -3755,7 +3755,7 @@ static Font* ComputeInitialLetterFont(const ComputedStyle& style,
 //  * inline-sizing.
 // Additionally, all of the sizing properties and box-sizing also apply to
 // initial letters (see [css-sizing-3]).
-const ComputedStyle* StyleResolver::StyleForInitialLetterText(
+const ComputedStyle& StyleResolver::StyleForInitialLetterText(
     const ComputedStyle& initial_letter_box_style,
     const ComputedStyle& paragraph_style) {
   DCHECK(paragraph_style.InitialLetter().IsNormal());
@@ -3768,7 +3768,7 @@ const ComputedStyle* StyleResolver::StyleForInitialLetterText(
   builder.SetVerticalAlign(EVerticalAlign::kBaseline);
   builder.SetBaseTextDecorationData(
       initial_letter_box_style.AppliedTextDecorationData());
-  return builder.TakeStyle();
+  return *builder.TakeStyle();
 }
 
 StyleRulePositionTry* StyleResolver::ResolvePositionTryRule(
