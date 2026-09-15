1ca6582e330387d59c1dd4aee1f17d587feaa150
Agata Szpotek <szpotekagata@google.com>
2026-09-15 10:36:13 -0700

    [Isolated mode] Support Isolated Mode in SystemSignalsServiceHostFactory
    Redirects isolated mode profiles to the original profile in
    ProfileSelections for SystemSignalsServiceHostFactory.
    
    Recently, DeviceTrustConnectorService was enabled for Enterprise
    Isolated Mode profiles (crrev.com/c/8397329). As a result,
    SignalsAggregatorFactory now initializes collectors for Isolated Mode
    profiles. However, SystemSignalsServiceHostFactory was not configured
    for Isolated Mode, defaulting to nullptr and causing a CHECK failure in
    BaseSignalsCollector due to a null SystemSignalsServiceHost.
    
    Change-Id: I25a8451268facbd213872198a4759deb0338fa33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8411406
    Reviewed-by: Hamda Mare <hmare@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698231}

--- chrome/browser/enterprise/signals/system_signals_service_host_factory.cc
+++ chrome/browser/enterprise/signals/system_signals_service_host_factory.cc
@@ -34,6 +34,7 @@ SystemSignalsServiceHostFactory::SystemSignalsServiceHostFactory()
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kOriginalOnly)
               .WithGuest(ProfileSelection::kOffTheRecordOnly)
+              .WithIsolatedMode(ProfileSelection::kRedirectedToOriginal)
               .Build()) {}
 
 SystemSignalsServiceHostFactory::~SystemSignalsServiceHostFactory() = default;
