d31dbb2ac521e4a58b3534877670b553217711e2
Danilo Tedeschi <danft@google.com>
2026-09-15 08:06:51 -0700

    Remove kSodaLanguageToBcp47Map from //components/soda
    We're also adding //base/i18n/chinese_helpers to help checking if a tag
    is chinese (traditional or simplified).
    
    Change-Id: I596e7c86d16c9fd4ec0c9a60b80a2362f9452d99
    Bug: 526738932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392699
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698090}

---
+++ base/i18n/chinese_helpers.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_I18N_CHINESE_HELPERS_H_
+#define BASE_I18N_CHINESE_HELPERS_H_
+
+#include "base/component_export.h"
+#include "base/i18n/language_tag.h"
+
+namespace base::i18n {
+
+// Returns whether `tag` represents a Chinese (zh) language.
+COMPONENT_EXPORT(LANGUAGE_TAG_WITH_ICU) bool IsChinese(const LanguageTag& tag);
+// Returns whether `tag` represents a Chinese Simplified (zh-Hans) language.
+COMPONENT_EXPORT(LANGUAGE_TAG_WITH_ICU)
+bool IsSimplifiedChinese(const LanguageTag& tag);
+// Returns whether `tag` represents a Chinese Traditional (zh-Hant) language.
+COMPONENT_EXPORT(LANGUAGE_TAG_WITH_ICU)
+bool IsTraditionalChinese(const LanguageTag& tag);
+
+}  // namespace base::i18n
+
+#endif  // BASE_I18N_CHINESE_HELPERS_H_
--- components/soda/constants.h
+++ components/soda/constants.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/files/file_path.h"
+#include "base/i18n/language_tag.h"
 #include "components/soda/pref_names.h"
 #include "components/strings/grit/components_strings.h"
 
@@ -365,9 +366,9 @@ std::string_view GetDefaultLiveCaptionLanguage(
 // Otherwise, return `language_name`.
 const std::string MaybeMapToChineseLocale(std::string_view language_name);
 
-// Return a BCP47 language code for the given SODA language code. If the given
+// Return a BCP47 language tag for the given SODA language code. If the given
 // input is not a string corresponding a SODA language, then return nullopt.
-std::optional<std::string> GetBCP47LanguageCodeFromSodaLanguage(
+std::optional<base::i18n::LanguageTag> GetLanguageTagFromSodaLanguage(
     std::string_view soda_language);
 
 }  // namespace speech
