26d51f0ede8cf87cdbae91d0cc3ee6c9f570870a
Charles Cai <charlesyc@google.com>
2026-09-15 15:36:17 -0700

    [Drive Integration] Add DriveDisclaimerBridge to check Drive consent status
    Add DriveDisclaimerBridge on Android to asynchronously query Google
    Drive disclaimer/consent status against the Footprints FACS backend via
    FpopService and DriveDisclaimerController.
    
    Bug: 557197221
    Change-Id: I4c414d6314c78e18d2a542e60e82e0577b3ce6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8413685
    Commit-Queue: Charles Cai <charlesyc@google.com>
    Reviewed-by: Adam Yao <adamyao@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698432}

---
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/DriveDisclaimerBridgeUnitTest.java
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.omnibox.fusebox;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
+
+import org.chromium.base.Callback;
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.contextual_search.DisclaimerStatus;
+
+/** Unit tests for {@link DriveDisclaimerBridge}. */
+@RunWith(BaseRobolectricTestRunner.class)
+public class DriveDisclaimerBridgeUnitTest {
+    @Rule
+    public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
+
+    @Mock private DriveDisclaimerBridge.Natives mBridgeJni;
+    @Mock private Profile mProfile;
+    @Mock private Callback<@DisclaimerStatus Integer> mCallback;
+
+    @Before
+    public void setUp() {
+        DriveDisclaimerBridgeJni.setInstanceForTesting(mBridgeJni);
+    }
+
+    @Test
+    public void checkConsentStatus_delegatesToNative() {
+        DriveDisclaimerBridge.checkConsentStatus(mProfile, mCallback);
+        verify(mBridgeJni).checkConsentStatus(mProfile, mCallback);
+    }
+}
