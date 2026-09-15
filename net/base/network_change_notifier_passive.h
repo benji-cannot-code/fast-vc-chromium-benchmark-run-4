de47754237647de237160baf86aa1adeae428bda
Benoît Lizé <lizeb@google.com>
2026-09-14 23:50:48 -0700

    net: Add GUARDED_BY annotations to NetworkChangeNotifierPassive and NetLog
    Add GUARDED_BY(lock_) to connection_type_ and max_bandwidth_mbps_
    in NetworkChangeNotifierPassive.
    
    Add GUARDED_BY(lock_) to observers_ and capture_mode_observers_ in
    NetLog, and annotate observer lookup helper methods with
    EXCLUSIVE_LOCKS_REQUIRED(lock_).
    
    TAG=agy
    CONV=0f79d58f-a4bb-4d31-b2a0-5432cf151a7f
    
    Bug: 561352941
    Change-Id: Ic5c4912be67e86294c1050d257df9d619a883ac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394022
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697858}

--- net/base/network_change_notifier_passive.h
+++ net/base/network_change_notifier_passive.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/sequence_checker.h"
 #include "base/synchronization/lock.h"
+#include "base/thread_annotations.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
 #include "build/build_config.h"
@@ -80,9 +81,8 @@ class NET_EXPORT NetworkChangeNotifierPassive : public NetworkChangeNotifier {
 #endif
 
   mutable base::Lock lock_;
-  NetworkChangeNotifier::ConnectionType
-      connection_type_;        // Guarded by |lock_|.
-  double max_bandwidth_mbps_;  // Guarded by |lock_|.
+  NetworkChangeNotifier::ConnectionType connection_type_ GUARDED_BY(lock_);
+  double max_bandwidth_mbps_ GUARDED_BY(lock_);
 };
 
 }  // namespace net
