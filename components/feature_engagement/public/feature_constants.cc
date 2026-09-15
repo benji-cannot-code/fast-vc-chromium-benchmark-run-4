5bb1b17e51e8da7c171f4fa36f3dc9fe7d7e91b9
Tarek Makkouk <tarekmakkouk@google.com>
2026-09-14 17:31:00 -0700

    [iOS] Register UMA metrics and user actions for Gemini Live IPH
    Register the Gemini Live tooltip IPH and "New" badge features in histograms.xml and actions.xml to track impressions, dismissal durations, and failure reasons via FET. Update the feature name strings to adhere to FET naming conventions (pre-requisite to access those metrics).
    
    Bug: 514326402
    Change-Id: I3c6bb616088bc59c09366a3cb7de15f65cb57acd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8399408
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Tarek Makkouk <tarekmakkouk@google.com>
    Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697680}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -962,9 +962,12 @@ BASE_FEATURE(kIPHiOSAIHubNewBadge,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIPHiOSGeminiLiveNewBadgeFeature,
+             "IPH_iOSGeminiLiveNewBadgeFeature",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kIPHiOSGeminiLiveIPHFeature, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kIPHiOSGeminiLiveIPHFeature,
+             "IPH_iOSGeminiLiveIPHFeature",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIPHiOSGeminiFullscreenPromoFeature,
              "IPH_iOSGeminiFullscreenPromoFeature",
