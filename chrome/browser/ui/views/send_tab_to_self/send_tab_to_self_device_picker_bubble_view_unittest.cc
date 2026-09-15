af981644235da795f400ae27f2842fe080a476c5
Satyaveer Singh <satyaveer.s@samsung.com>
2026-09-15 05:36:36 -0700

    Modernize SendTabToSelf unit tests
    This CL replaces bare new, base::WrapUnique(new T), and
    std::unique_ptr<T>(new T) patterns with std::make_unique plus
    std::move for ownership transfer to align with Chromium C++
    style guidelines.
    
    Bug: 519062941
    Change-Id: Ifc66fe909e86f77030e2735ae574c2477ac9a7df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8387196
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698006}

--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_device_picker_bubble_view_unittest.cc
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_device_picker_bubble_view_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_device_picker_bubble_view.h"
 
+#include <memory>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -96,9 +97,11 @@ class SendTabToSelfDevicePickerBubbleViewTest : public ChromeViewsTestBase {
     web_contents_ =
         content::WebContentsTester::CreateTestWebContents(&profile_, nullptr);
     // Owned by WebContents.
-    controller_ = new SendTabToSelfBubbleControllerMock(web_contents_.get());
+    auto controller = std::make_unique<SendTabToSelfBubbleControllerMock>(
+        web_contents_.get());
+    controller_ = controller.get();
     web_contents_->SetUserData(SendTabToSelfBubbleControllerMock::UserDataKey(),
-                               base::WrapUnique(controller_.get()));
+                               std::move(controller));
 
     bubble_ = new SendTabToSelfDevicePickerBubbleView(
         views::BubbleAnchor(anchor_widget_->GetContentsView()),
