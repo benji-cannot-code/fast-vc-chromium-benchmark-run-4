01f48ee7d8d1f1f1ce5a0d34c92398e4d8ef14c3
Orko Garai <orko@igalia.com>
2026-09-15 07:05:20 -0700

    Move sb_embedded_test_server_browsertest.cc to browser_tests
    This file already lived under //chrome/browser/safe_browsing but was
    only ever compiled as part of the monolithic chrome/test:browser_tests
    target.
    
    Move it into the existing per-directory browser_tests target instead.
    
    The file also had an unused include of net/dns/mapped_host_resolver.h,
    which would have required a new dependency on net/dns:host_resolver.
    Drop the unused include instead.
    
    Bug: 353332589
    Change-Id: Id406a75cda26c13e997e0adbad16a9dee20d5e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8406306
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1698052}

--- chrome/browser/safe_browsing/sb_embedded_test_server_browsertest.cc
+++ chrome/browser/safe_browsing/sb_embedded_test_server_browsertest.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_utils.h"
 #include "net/cookies/canonical_cookie.h"
-#include "net/dns/mapped_host_resolver.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/network/public/mojom/network_context.mojom-forward.h"
