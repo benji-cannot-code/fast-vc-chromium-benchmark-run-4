3f74fe28a708993ca8cdba9038237d373d0d0983
Hira Mahmood <hiramahmood@google.com>
2026-09-15 10:36:42 -0700

    [iOS] Enable kUpdatedFirstRunSequence by default
    This CL enables the `kUpdatedFirstRunSequence` feature by default with
    param value 2, which removes the sign-in/sync screens for users who do
    not already have an account on the device.
    
    It also updates EG tests to account for this new default behavior. More
    specifically:
    - Tests that expect the sign-in screen to appear during FRE
      (e.g., in `bring_android_tabs_test_utils.mm`) now explicitly add fake
      identities at startup using `-AddFakeIdentitiesAtStartup`.
    - `FirstRunWithUpdatedFRESequenceTestCase` is updated to ensure no
      identities are added at startup, allowing it to verify the new
      expected sequence.
    
    Fixed: 556845953
    Change-Id: I9488d8e7df06f2e6f4c008239cd7d4f4d97263bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334796
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698232}

--- ios/chrome/browser/first_run/public/features.mm
+++ ios/chrome/browser/first_run/public/features.mm
@@ -20,7 +20,7 @@ BASE_FEATURE(kBestFeaturesScreenInFirstRun,
 BASE_FEATURE(kSkipDefaultBrowserPromoInFirstRun,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kUpdatedFirstRunSequence, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUpdatedFirstRunSequence, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPostFREIphInProfileAgent, base::FEATURE_ENABLED_BY_DEFAULT);
 
@@ -42,7 +42,7 @@ BASE_FEATURE_PARAM(int,
                    kUpdatedFirstRunSequenceParamFeature,
                    &kUpdatedFirstRunSequence,
                    kUpdatedFirstRunSequenceParam,
-                   1);
+                   2);
 
 BASE_FEATURE_PARAM(
     int,
