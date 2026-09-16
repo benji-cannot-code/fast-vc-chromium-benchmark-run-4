301f456582af3b98dd8e1f80301609112773156a
Christoph Schwering <schwering@google.com>
2026-09-16 03:05:11 -0700

    [Autofill] Fix bounds transformation in ContentAutofillDriver
    When CSS scales the content of an iframe as in
      <iframe style="transform: scale(2);">
    then the gfx::RectF::size() in the child frame is not the same as in
    the main frame.
    
    Or more precisely, the gfx::RectF::size() in the *local* root
    coordinate system is not the same as in the root coordinate system.
    
    This CL fixes that transformation.
    
    Bug: 562177779
    Change-Id: I0a57d12aa76ee41eda40e44ef036acb76940c85b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8414526
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698792}

--- components/autofill/content/browser/content_autofill_driver.cc
+++ components/autofill/content/browser/content_autofill_driver.cc
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/functional/overload.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
+#include "ui/gfx/geometry/quad_f.h"
+#include "ui/gfx/geometry/rect_conversions.h"
 #include "url/origin.h"
 
 namespace autofill {
@@ -79,20 +81,23 @@ T&& Lift(ContentAutofillDriver& source, T&& x) {
   return std::forward<T>(x);
 }
 
-gfx::Rect Lift(ContentAutofillDriver& source, gfx::Rect r) {
-  if (content::RenderWidgetHostView* view =
-          source.render_frame_host()->GetView()) {
-    r.set_origin(view->TransformPointToRootCoordSpace(r.origin()));
+gfx::RectF Lift(ContentAutofillDriver& source, gfx::RectF r) {
+  content::RenderWidgetHostView* view = source.render_frame_host()->GetView();
+  if (!view) {
+    return r;
   }
-  return r;
+
+  // We transform all corners to handle CSS `transform: scale(...)` correctly
+  // (crbug.com/562177779).
+  return gfx::QuadF(view->TransformPointToRootCoordSpaceF(r.origin()),
+                    view->TransformPointToRootCoordSpaceF(r.top_right()),
+                    view->TransformPointToRootCoordSpaceF(r.bottom_right()),
+                    view->TransformPointToRootCoordSpaceF(r.bottom_left()))
+      .BoundingBox();
 }
 
-gfx::RectF Lift(ContentAutofillDriver& source, gfx::RectF r) {
-  if (content::RenderWidgetHostView* view =
-          source.render_frame_host()->GetView()) {
-    r.set_origin(view->TransformPointToRootCoordSpaceF(r.origin()));
-  }
-  return r;
+gfx::Rect Lift(ContentAutofillDriver& source, gfx::Rect r) {
+  return gfx::ToRoundedRect(Lift(source, gfx::RectF(r)));
 }
 
 FormData Lift(ContentAutofillDriver& source, FormData form) {
