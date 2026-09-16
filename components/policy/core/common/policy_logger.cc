92447a449e8808bbacbe8c8da5581ba9cd17a360
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2026-09-16 08:10:41 -0700

    PolicyLogger: improve time format performance
    TimeFormatHTTP() is surprisingly slow: ~350us in benchmarks. While this
    may not sound too bad, running it 200x blocks the main thread for 60ms.
    This will be much worse if we increase the log limit in PolicyLogger.
    
    - Replace TimeFormatHTTP() with a custom implementation that's almost
      1000x faster in benchmarks (~450ns).
    - Avoid parsing then re-formatting the date on the JS side. Simply
      display the string passed by C++.
    
    Change-Id: Icef8231d1b91dde8e0d00652bf056b986a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8408592
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698948}

--- components/policy/core/common/policy_logger.cc
+++ components/policy/core/common/policy_logger.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check_is_test.h"
-#include "base/i18n/time_formatting.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/features.h"
 #include "components/policy/resources/webui/mojom/policy.mojom.h"
 #include "components/version_info/version_info.h"
+#include "third_party/abseil-cpp/absl/strings/str_format.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
 #include "chromeos/ash/components/channel/channel_info.h"
@@ -140,6 +140,18 @@ std::string GetFileAndLine(std::string_view file, int line) {
   return base::StrCat({GetFileBasename(file), ":", base::NumberToString(line)});
 }
 
+std::string GetTimestampString(base::Time timestamp) {
+  base::Time::Exploded exploded;
+  timestamp.LocalExplode(&exploded);
+  int hour12 = exploded.hour % 12;
+  if (hour12 == 0) {
+    hour12 = 12;
+  }
+  const char* ampm = exploded.hour >= 12 ? "PM" : "AM";
+  return absl::StrFormat("%d-%02d-%02d %02d:%02d:%02d %s", exploded.year,
+                         exploded.month, exploded.day_of_month, hour12,
+                         exploded.minute, exploded.second, ampm);
+}
 
 }  // namespace
 
@@ -237,14 +249,14 @@ base::DictValue PolicyLogger::Log::GetAsDict() const {
       .Set("logSource", GetLogSourceValue(log_source_))
       .Set("fileAndLine", GetFileAndLine(file_, line_))
       .Set("location", GetLineURL(file_, line_))
-      .Set("timestamp", base::TimeFormatHTTP(timestamp_));
+      .Set("timestamp", GetTimestampString(timestamp_));
 }
 
 policy::mojom::LogPtr PolicyLogger::Log::GetAsMojoLog() const {
   return policy::mojom::Log::New(
       message_, GetLogSeverity(log_severity_), GetLogSourceValue(log_source_),
       GetFileAndLine(file_, line_), GetLineURL(file_, line_),
-      base::TimeFormatHTTP(timestamp_));
+      GetTimestampString(timestamp_));
 }
 
 PolicyLogger::PolicyLogger() = default;
