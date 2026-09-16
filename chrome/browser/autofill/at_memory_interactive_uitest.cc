c0d08528b227050a3b54bc261751de8a13c168c1
Ireneusz Szulc <ireneuszszulc@google.com>
2026-09-16 04:21:06 -0700

    AtMemory: Propagate double Ctrl trigger preference to renderer
    This CL plumbs the `autofill.at_memory.double_ctrl_trigger_enabled`
    preference to `blink::RendererPreferences`.
    
    It also decouples the double Ctrl trigger in `AtMemoryHandler` from a
    configured custom keyboard shortcut, allowing both triggers to function
    independently.
    
    Bug: 553979691
    Change-Id: Ia6390b572c64eeb45e595c9b52acfde46a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8353363
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698828}

--- chrome/browser/autofill/at_memory_interactive_uitest.cc
+++ chrome/browser/autofill/at_memory_interactive_uitest.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/integrators/at_memory/mock_at_memory_query_service.h"
 #include "components/autofill/core/common/autofill_debug_features.h"
 #include "components/autofill/core/common/autofill_features.h"
+#include "components/autofill/core/common/autofill_prefs.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
@@ -105,6 +107,9 @@ class AtMemoryInteractiveUiTest : public AutofillUiTest,
         base::BindRepeating(
             &AtMemoryInteractiveUiTest::CreateMockAtMemoryQueryService,
             base::Unretained(this)));
+
+    browser()->GetProfile()->GetPrefs()->SetBoolean(
+        prefs::kAutofillAtMemoryDoubleCtrlTriggerEnabled, true);
   }
 
   std::unique_ptr<net::test_server::HttpResponse> HandleTestURL(
