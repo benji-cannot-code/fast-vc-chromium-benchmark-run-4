2d2d3a1063447f732c7e362c2244c2b1e8f033c8
Rafał Godlewski <rgod@google.com>
2026-09-16 03:22:35 -0700

    [iOS] Instantiate DeviceIntegrityService in ApplicationContext
    This CL is part of a multi-CL change:
      - https://crrev.com/c/8380517
      - https://crrev.com/i/9795936
     => https://crrev.com/c/8397827
    
    Bug: 405036154
    Change-Id: Icbb46d07e9671bcf15b7d40e188c5f22c4dda511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8397827
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698801}

--- ios/chrome/browser/application_context/model/application_context_impl.mm
+++ ios/chrome/browser/application_context/model/application_context_impl.mm
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/additional_features/additional_features_api.h"
 #import "ios/public/provider/chrome/browser/additional_features/additional_features_controller.h"
 #import "ios/public/provider/chrome/browser/app_distribution/app_distribution_api.h"
+#import "ios/public/provider/chrome/browser/device_attestation/device_attestation_api.h"
 #import "ios/public/provider/chrome/browser/device_attestation/device_integrity_service.h"
 #import "ios/public/provider/chrome/browser/push_notification/push_notification_api.h"
 #import "ios/public/provider/chrome/browser/signin/signin_identity_api.h"
@@ -625,7 +626,9 @@ ApplicationContextImpl::GetAdditionalFeaturesController() {
 
 DeviceIntegrityService* ApplicationContextImpl::GetDeviceIntegrityService() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  // TODO(crbug.com/405036154): Lazily instantiate once internal provider lands.
+  if (!device_integrity_service_) {
+    device_integrity_service_ = ios::provider::CreateDeviceIntegrityService();
+  }
   return device_integrity_service_.get();
 }
 
