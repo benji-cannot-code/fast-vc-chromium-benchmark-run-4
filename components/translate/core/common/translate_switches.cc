3449c0f0e4483f2a55b5edd38604a9a666ebc0d3
Joel Crumley <joelcrumley@google.com>
2026-09-16 11:17:23 -0700

    [Translate] Add --disable-translate-trigger command line flag
    --disable-translate-trigger flag prevents both the auto-translate
    feature and the "Translate page" popup from appearing upon page load.
    Manual translation via the context menu remains unaffected.
    
    Bug: 561502855
    Change-Id: I48a237d2a721e5ca57585c075b94ae42c0ac174a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8398769
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Joel Crumley <joelcrumley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699098}

--- components/translate/core/common/translate_switches.cc
+++ components/translate/core/common/translate_switches.cc
@@ -21,5 +21,9 @@ const char kTranslateRankerModelURL[] = "translate-ranker-model-url";
 // pages to the new target language.
 const char kForcedTranslateLanguage[] = "forced-translate-language";
 
+// Disables translation triggering for pages, including translate page pop up
+// and auto translate feature. Manual translation unaffected.
+const char kDisableTranslateTrigger[] = "disable-translate-trigger";
+
 }  // namespace switches
 }  // namespace translate
