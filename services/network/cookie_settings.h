aa7e29346f90cca36b64823271418d6513779878
Maks Orlovich <morlovich@chromium.org>
2026-09-16 10:47:10 -0700

    CookieSettings: specify what embedded hosts to accept in secure_origin_cookies_allowed_origins
    And narrow the lens exception with it.
    
    Bug: 554556700
    Change-Id: Ic4103755d51be3e74c774b52b84a4cf95331046c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8407287
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699052}

--- services/network/cookie_settings.h
+++ services/network/cookie_settings.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_util.h"
 #include "net/first_party_sets/first_party_set_metadata.h"
 #include "services/network/public/cpp/session_cookie_delete_predicate.h"
-#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 class GURL;
 
@@ -66,7 +66,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CookieSettings
 
   // Cannot be an opaque origin. Opaque origins will be ignored.
   void set_secure_origin_cookies_allowed_origins(
-      const std::vector<url::Origin>& secure_origin_cookies_allowed_origins);
+      std::vector<std::pair<url::Origin, std::vector<net::SchemefulSite>>>
+          secure_origin_cookies_allowed_origins);
 
   void set_matching_scheme_cookies_allowed_schemes(
       const std::vector<std::string>& matching_scheme_cookies_allowed_schemes) {
@@ -218,12 +219,16 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CookieSettings
   // Returns true if at least one content settings is session only.
   bool HasSessionOnlyOrigins() const;
 
+  bool HasSecureOriginException(const url::Origin& first_party_origin,
+                                const GURL& url) const;
+
   // Returns true if user blocks 3PC or 3PCD is on.
   bool block_third_party_cookies_ =
       net::cookie_util::IsForceThirdPartyCookieBlockingEnabled();
 
   std::set<std::string, std::less<>> secure_origin_cookies_allowed_schemes_;
-  absl::flat_hash_set<url::Origin> secure_origin_cookies_allowed_origins_;
+  absl::flat_hash_map<url::Origin, std::vector<net::SchemefulSite>>
+      secure_origin_cookies_allowed_origins_;
   std::set<std::string, std::less<>> matching_scheme_cookies_allowed_schemes_;
   std::set<std::string, std::less<>> third_party_cookies_allowed_schemes_;
 
