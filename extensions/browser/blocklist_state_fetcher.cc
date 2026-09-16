0b7cf1874ab714a076a6cc070dd65dc35e35cf4b
thefrog <thefrog@chromium.org>
2026-09-16 13:10:19 -0700

    Update extensions references to sb_protocol_manager_util
    Files + targets for v4_protocol_manager_util are being renamed to
    sb_protocol_manager_util as part of the Safe Browsing v4 migration to
    v5, since the util applies both to v4 and v5.
    
    Bug: 362791941
    Change-Id: I0e0678ffbb1b00bfbc59456e245cea4f05af16c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8383761
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699206}

--- extensions/browser/blocklist_state_fetcher.cc
+++ extensions/browser/blocklist_state_fetcher.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/strings/escape.h"
 #include "base/task/single_thread_task_runner.h"
-#include "components/safe_browsing/core/browser/db/v4_protocol_manager_util.h"
+#include "components/safe_browsing/core/browser/db/sb_protocol_manager_util.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "components/safe_browsing/core/common/proto/crx_info.pb.h"
 #include "content/public/browser/browser_thread.h"
--- extensions/browser/fake_safe_browsing_database_manager.cc
+++ extensions/browser/fake_safe_browsing_database_manager.cc
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
+#include "components/safe_browsing/core/browser/db/sb_protocol_manager_util.h"
 #include "components/safe_browsing/core/browser/db/util.h"
-#include "components/safe_browsing/core/browser/db/v4_protocol_manager_util.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 
