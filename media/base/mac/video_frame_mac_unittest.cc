996c02854a2f0ea58020b04c9701423352aa28f4
Markus Handell <handellm@google.com>
2026-09-16 02:06:34 -0700

    Mac: Introduce feature for avoiding pre-encode hardware readback
    Previous zero-copy cleanup destroyed RTCVideoEncoder's ability
    to directly hardware encode incoming video frames with IOSurface
    backing.
    
    Feature kWebRtcMacSharedImageEncode is introduced to re-instate
    direct encoding. Additionally, feature
    kVTVideoEncodeAcceleratorOpaqueRgbSharedImageEncode is introduced
    and adds support for RGBA formats for avoiding RGBA to NV12 copies
    before encoding since video toolbox supports that format.
    
    TESTED=manual tests on Mac M4, results (internal): http://shortn/_n8vPXjLL7f. 265 <-> 265 encode https://ui.perfetto.dev/#!/?s=734c995c94c028d1bab3e42c3bc2d6e893215e3f. 265 with vp <-> 265 https://ui.perfetto.dev/#!/?s=a472c4d7a35e114b63ffd14dd932477789f9ea09
    
    Bug: 555719593
    Change-Id: Ic9284f9f5cc143d2b4994a059c90009fd7837672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8348887
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698764}

--- media/base/mac/video_frame_mac_unittest.cc
+++ media/base/mac/video_frame_mac_unittest.cc
@@ -41,15 +41,20 @@ void ExpectWrappedPixelBuffer(CVPixelBufferRef pb,
             CVPixelBufferGetWidth(pb));
   EXPECT_EQ(static_cast<size_t>(frame.coded_size().height()),
             CVPixelBufferGetHeight(pb));
-  EXPECT_EQ(VideoFrame::NumPlanes(frame.format()),
-            CVPixelBufferGetPlaneCount(pb));
-  for (size_t i = 0; i < VideoFrame::NumPlanes(frame.format()); ++i) {
-    EXPECT_EQ(static_cast<size_t>(frame.columns(i)),
-              CVPixelBufferGetWidthOfPlane(pb, i))
-        << frame.format() << " plane " << i;
-    EXPECT_EQ(static_cast<size_t>(frame.rows(i)),
-              CVPixelBufferGetHeightOfPlane(pb, i))
-        << frame.format() << " plane " << i;
+  if (CVPixelBufferIsPlanar(pb)) {
+    EXPECT_EQ(VideoFrame::NumPlanes(frame.format()),
+              CVPixelBufferGetPlaneCount(pb));
+    for (size_t i = 0; i < VideoFrame::NumPlanes(frame.format()); ++i) {
+      EXPECT_EQ(static_cast<size_t>(frame.columns(i)),
+                CVPixelBufferGetWidthOfPlane(pb, i))
+          << frame.format() << " plane " << i;
+      EXPECT_EQ(static_cast<size_t>(frame.rows(i)),
+                CVPixelBufferGetHeightOfPlane(pb, i))
+          << frame.format() << " plane " << i;
+    }
+  } else {
+    EXPECT_EQ(1u, VideoFrame::NumPlanes(frame.format()));
+    EXPECT_EQ(0u, CVPixelBufferGetPlaneCount(pb));
   }
 }
 
@@ -153,6 +158,8 @@ TEST(VideoFrameMac, CheckFormats) {
     OSType video_range;
     OSType full_range;
   } kSupportedCases[] = {
+      {PIXEL_FORMAT_ARGB, kCVPixelFormatType_32BGRA, kCVPixelFormatType_32BGRA},
+      {PIXEL_FORMAT_XRGB, kCVPixelFormatType_32BGRA, kCVPixelFormatType_32BGRA},
       {PIXEL_FORMAT_I420, kCVPixelFormatType_420YpCbCr8Planar,
        kCVPixelFormatType_420YpCbCr8PlanarFullRange},
       {PIXEL_FORMAT_NV12, kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange,
@@ -170,7 +177,8 @@ TEST(VideoFrameMac, CheckFormats) {
   };
   constexpr VideoPixelFormat kUnsupportedCases[] = {
       PIXEL_FORMAT_YV12, PIXEL_FORMAT_I422, PIXEL_FORMAT_I420A,
-      PIXEL_FORMAT_I444};
+      PIXEL_FORMAT_I444, PIXEL_FORMAT_ABGR, PIXEL_FORMAT_XBGR,
+      PIXEL_FORMAT_BGRA};
 
   gfx::Size size(kWidth, kHeight);
   for (const auto& format : kUnsupportedCases) {
