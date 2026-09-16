a0d85b1e454edfa2af56b530b9d2a7b5a1157e61
April Kallmeyer <ask@chromium.org>
2026-09-15 17:23:32 -0700

    [PDF Ink Signatures] Allow multiple InkTextInfo to join one /ActualText
    This is necessary because sometimes 2D offsets split the text smaller
    than harfbuzz glyph clusters but at that point it's not possible to take
    substrings which map directly to glyphs. So those InkTextInfo objects
    must be joined together with a single /ActualText span.
    
    Bug: 525093248
    Change-Id: I55613cc1ce1b7e1be3508903c684304d4d73f839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8350292
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: April Kallmeyer <ask@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698539}

--- pdf/test/pdf_ink_test_helpers.cc
+++ pdf/test/pdf_ink_test_helpers.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/ranges.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/to_string.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "pdf/mojom/pdf.mojom.h"
 #include "pdf/pdf_ink_conversions.h"
@@ -274,7 +275,9 @@ void PrintTo(const InkTextInfo& info, std::ostream* os) {
       << ",\n  location=" << info.location.ToString()
       << ",\n  glyphs=" << testing::PrintToString(info.glyphs)
       << ",\n  glyph_positions=" << testing::PrintToString(info.glyph_positions)
-      << "\n}";
+      << ",\n  text=\"" << base::UTF16ToUTF8(info.text) << "\""
+      << ",\n  join_prev_actualtext="
+      << base::ToString(info.join_prev_actualtext) << "\n}";
 }
 
 void PrintTo(const InkTextBoxAttributes& info, std::ostream* os) {
@@ -330,7 +333,8 @@ bool InkTextInfoEquals(const InkTextInfo& lhs, const InkTextInfo& rhs) {
       });
   return glyph_positions_eq && lhs.font_id == rhs.font_id &&
          lhs.glyphs == rhs.glyphs && lhs.location == rhs.location &&
-         lhs.is_horizontal == rhs.is_horizontal && lhs.text == rhs.text;
+         lhs.is_horizontal == rhs.is_horizontal && lhs.text == rhs.text &&
+         lhs.join_prev_actualtext == rhs.join_prev_actualtext;
 }
 
 }  // namespace chrome_pdf
