58a3870f46078ec20dbc7983c6e93c4c68b15020
Michael Thiessen <mthiesse@chromium.org>
2026-09-16 14:40:37 -0700

    Reland "Reland: Remove EnsureRenderFrameHostVisibilityConsistent"
    Last step of making WebContents the sole manager of whether content
    would like (non-child) RenderWidgetHostViews to be visible.
    
    Per the comment in the code, EnsureRenderFrameHostVisibilityConsistent
    should no longer be necessary as we unconditionally hide and show when
    committing a navigation.
    
    However, EnsureRenderFrameHostVisibilityConsistent was incorrectly
    causing RWHV to become visible before the page is actually rendering
    (we should wait for commit). Fixing this caused browser inputs to break
    on aura platforms as the Window (visibility tied to the RWHV) is no
    longer visible pre-initial commit
    (https://issues.chromium.org/issues/521200679). To work around this,
    we now make the RFH visible either upon commit, or when the
    WebContents is Focused. Longer term we should decouple the notions of
    whether the page is rendering, vs whether the window is visible to
    the user, but for now we should allow the browser to handle input
    even when the page isn't rendering.
    
    Also, some test infrastructure was initializing the visibility for
    the TestRenderWidgetHostView incorrectly, and some unit tests that
    use it were relying on EnsureRenderFrameHostVisibilityConsistent to
    fix this incorrect initialization.
    
    This reland expands the scope of is_swapping_render_frame_hosts_
    in WebContentsImpl, as when the RenderFrameHostManager sets focus on
    the new view during commit, that focus change can propagate to the
    WebContents and we don't want that focus change updating visibility.
    
    Bug: 505623930
    Change-Id: Iedcf6c37cb115873f86f00d130ba90a63ca33683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415207
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699273}

--- content/browser/web_contents/web_contents_impl_unittest.cc
+++ content/browser/web_contents/web_contents_impl_unittest.cc
@@ -2047,12 +2047,13 @@ TEST_F(WebContentsImplTest, UpdateWebContentsVisibility) {
       main_test_rfh()->GetRenderViewHost()->GetWidget()->GetView());
   TestWebContentsObserver observer(contents());
 
-  EXPECT_FALSE(view->is_showing());
+  // Test WebContents always start visible.
+  EXPECT_TRUE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
 
-  // WebContents must be made visible once before it can be hidden.
+  // WebContents must be made visible once before its visibility can be changed.
   contents()->UpdateWebContentsVisibility(Visibility::HIDDEN);
-  EXPECT_FALSE(view->is_showing());
+  EXPECT_TRUE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
   EXPECT_EQ(Visibility::VISIBLE, contents()->GetVisibility());
 
@@ -2209,9 +2210,10 @@ void HideOrOccludeWithCapturerTest(WebContentsImpl* contents,
   TestRenderWidgetHostView* view = static_cast<TestRenderWidgetHostView*>(
       contents->GetRenderWidgetHostView());
 
-  EXPECT_FALSE(view->is_showing());
+  // Test WebContents always start visible.
+  EXPECT_TRUE(view->is_showing());
 
-  // WebContents must be made visible once before it can be hidden.
+  // WebContents must be made visible once before its visibility can be changed.
   contents->UpdateWebContentsVisibility(Visibility::VISIBLE);
   EXPECT_TRUE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
@@ -2279,7 +2281,8 @@ TEST_F(WebContentsImplTest, KeepVisibleUntilFirstVisuallyNonEmptyPaint) {
   TestRenderWidgetHostView* view = static_cast<TestRenderWidgetHostView*>(
       contents()->GetRenderWidgetHostView());
 
-  EXPECT_FALSE(view->is_showing());
+  // Test WebContents always start visible.
+  EXPECT_TRUE(view->is_showing());
 
   WebUIConfigMap::GetInstance().AddWebUIConfig(
       std::make_unique<KeepVisibleWebUIConfig>());
@@ -2287,6 +2290,7 @@ TEST_F(WebContentsImplTest, KeepVisibleUntilFirstVisuallyNonEmptyPaint) {
   const GURL kGURL("chrome://keep-visible/");
   NavigationSimulator::NavigateAndCommitFromBrowser(contents(), kGURL);
 
+  // WebContents must be made visible once before its visibility can be changed.
   contents()->UpdateWebContentsVisibility(Visibility::VISIBLE);
   EXPECT_TRUE(view->is_showing());
   EXPECT_FALSE(view->is_occluded());
