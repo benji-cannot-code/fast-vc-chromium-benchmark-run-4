297029f894e7c6b8bc9cfb3eea45e4a109eeef40
Hitarth Kothari <hitarthkothari@google.com>
2026-09-16 03:33:10 -0700

    [BottomSheetHandles] Migrate Mandatory Reauth Opt-In Bottom Sheet to core handlebar
    Migrates MandatoryReauthOptInBottomSheet to use the core framework
    handlebar by overriding showHandlebar() to return true, and removes the
    embedded custom drag handlebar from
    mandatory_reauth_opt_in_bottom_sheet.xml.
    
    Bug: 553514361
    Change-Id: Ibb980e3e3921c640c60bb486546e33d7c4628909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354791
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698806}

--- chrome/browser/mandatory_reauth/android/internal/java/src/org/chromium/chrome/browser/mandatory_reauth/MandatoryReauthOptInBottomSheet.java
+++ chrome/browser/mandatory_reauth/android/internal/java/src/org/chromium/chrome/browser/mandatory_reauth/MandatoryReauthOptInBottomSheet.java
@@ -65,6 +65,11 @@ class MandatoryReauthOptInBottomSheet implements BottomSheetContent {
     }
 
     @Override
+    public boolean showHandlebar() {
+        return true;
+    }
+
+    @Override
     public void destroy() {}
 
     @Override
