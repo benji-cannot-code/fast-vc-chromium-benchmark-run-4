60fbfc70a92f0d210fb6848d37f7dcbdfb978d4d
Michael Wu <mklw@google.com>
2026-09-15 19:30:15 -0700

    [Android] Add PrintFallbackToPrimaryMainFrame feature flag
    Add a default-enabled killswitch flag PrintFallbackToPrimaryMainFrame
    to guard falling back to the primary main frame (instead of
    GetFrameToPrint()) during whole-tab printing on Android.
    
    Bug: 551755476
    Change-Id: I3e5068972885c9641477c943b49a6657715b2504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415488
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698602}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -464,6 +464,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kPowerSavingModeBroadcastReceiverInBackground,
     &kPreconnectOnTabCreation,
     &kPriceChangeModule,
+    &kPrintFallbackToPrimaryMainFrame,
     &kPrintSelectionMenu,
     &kProtectRecentlyVisibleTab,
     &kPwaRestoreUi,
@@ -894,6 +895,7 @@ BASE_FEATURE(kPictureInPictureMovesToolbarAndroid, base::FEATURE_DISABLED_BY_DEF
 BASE_FEATURE(kPowerSavingModeBroadcastReceiverInBackground, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kPreconnectOnTabCreation, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kPriceChangeModule, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kPrintFallbackToPrimaryMainFrame, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kPrintSelectionMenu, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kProtectRecentlyVisibleTab, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kPwaRestoreUi, base::FEATURE_DISABLED_BY_DEFAULT);
