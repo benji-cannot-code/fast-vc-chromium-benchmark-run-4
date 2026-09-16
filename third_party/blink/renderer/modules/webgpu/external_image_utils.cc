e5929024e05eee9a5e4a3a282c440fe13dc15029
Kai Ninomiya <kainino@chromium.org>
2026-09-16 14:54:04 -0700

    Fix typo in CopyStaticImageBitmapToWGPUTexture
    Change-Id: If579713688c7576ddb3f5d0a67389b2ae6c3770c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8417044
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699280}

--- third_party/blink/renderer/modules/webgpu/external_image_utils.cc
+++ third_party/blink/renderer/modules/webgpu/external_image_utils.cc
@@ -402,7 +402,7 @@ std::optional<ExternalImageSource> GetExternalImageSourceFrom(
                                                      exception_state);
 }
 
-bool CopyStaticImagBitmapToWGPUTexture(
+bool CopyStaticImageBitmapToWGPUTexture(
     const scoped_refptr<DawnControlClientHolder>& dawn_control_client,
     const wgpu::Device& device,
     StaticBitmapImage* image,
