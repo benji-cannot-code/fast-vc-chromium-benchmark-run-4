3cea914c86c850ba3347e5beb5857c0cbc47bd0a
Zgroza Klimek <zgroza@chromium.org>
2026-09-16 03:34:26 -0700

    [WebView] Add UseRustCborParser to flag UI list
    Exposes the UseRustCborParser base::Feature in WebView DevTools UI
    and adds corresponding LoginCustomFlags entries to enums.xml so the
    feature can be toggled and evaluated via Finch trials.
    
    Bug: 454485901
    Change-Id: I003859c531b56129b453476e090f69796a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8412150
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Zgroza (Luke) Klimek <zgroza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698808}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1488,6 +1488,7 @@ public final class ProductionSupportedFlagList {
                 BlinkFeatures.XSLT,
                 "Controls whether XSLT is supported by the browser. Disabling this flag disables"
                         + " all XSLT processing."),
+        Flag.baseFeature("UseRustCborParser", "Enables the Rust-based CBOR parser instead of C++."),
         // Add new commandline switches and features above. The final entry should have a
         // trailing comma for cleaner diffs.
     };
