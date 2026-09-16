5450b82e5138f188bf7d466a7c45e80b02d28af3
Mark Schillaci <mschillaci@google.com>
2026-09-16 07:12:05 -0700

    [histogram-cleanup] Remove expired Android.Intent histograms
    Remove expired histograms Android.Intent.IdentityShared and
    Android.Intent.HasNonSpoofablePackageName and associated dead code
    in LaunchIntentDispatcher.java.
    
    OBSOLETE_HISTOGRAMS[Android.Intent.IdentityShared]=expired
    OBSOLETE_HISTOGRAMS[Android.Intent.HasNonSpoofablePackageName]=expired
    
    This change was generated using the skill histogram-cleanup.
    
    Bug: 499059525, 354713061, 330166628
    Change-Id: I2314c3c299b3b710e9f913afcc3279a6d102b9d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8413786
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698901}

--- chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
@@ -14,7 +14,6 @@ import android.app.ActivityManager.RecentTaskInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.text.TextUtils;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.VisibleForTesting;
@@ -443,10 +442,7 @@ public class LaunchIntentDispatcher {
                 // the flag to take effect only once.
                 newIntent.setFlags(newIntent.getFlags() & ~Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);
             }
-            RecordHistogram.recordBooleanHistogram(
-                    "Android.Intent.HasNonSpoofablePackageName", hasNonSpoofablePackageName());
-            boolean identityShared = maybePutCallingAppPackage(newIntent);
-            RecordHistogram.recordBooleanHistogram("Android.Intent.IdentityShared", identityShared);
+            maybePutCallingAppPackage(newIntent);
         }
 
         if (mActivity instanceof ChromeLauncherActivity) {
@@ -530,11 +526,6 @@ public class LaunchIntentDispatcher {
             // mistakenly lead to a Chrome task being removed.
             return true;
         }
-    }
-
-    private boolean hasNonSpoofablePackageName() {
-        return !TextUtils.isEmpty(mActivity.getCallingPackage())
-                || !TextUtils.isEmpty(getCallingPackageIdentitySharing());
     }
 
     private static boolean clearTopIntentsForCustomTabsEnabled(Intent intent) {
