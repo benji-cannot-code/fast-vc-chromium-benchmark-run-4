cc49e27ed0e80b1ae07011465bf44b95358bf00e
Christoph Schwering <schwering@google.com>
2026-09-17 09:05:01 -0700

    [Passwords] Fix bounds transformation in ContentPasswordManagerClient
    This is the PWM counterpart to crrev.com/c/8414526.
    
    When CSS scales the content of an iframe as in
      <iframe style="transform: scale(2);">
    then the gfx::RectF::size() in the child frame is not the same as in
    the main frame.
    
    Or more precisely, the gfx::RectF::size() in the *local* root
    coordinate system is not the same as in the root coordinate system.
    
    This CL fixes that transformation.
    
    Bug: 562177779
    Change-Id: Ib5070a52699112c0106d37e759ee152aeb7d6713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8412230
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699862}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -147,6 +147,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/geometry/quad_f.h"
 
 #if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager.h"
@@ -238,6 +239,23 @@ namespace {
 constexpr char kPasswordBreachEntryTrigger[] = "PASSWORD_ENTRY";
 #endif
 
+// Transforms `rect` from the `rfh`'s local root's coordinate system to the
+// main frame's coordinate system.
+gfx::RectF TransformToRootCoordinates(content::RenderFrameHost& rfh,
+                                      const gfx::RectF& rect) {
+  content::RenderWidgetHostView* view = rfh.GetView();
+  if (!view) {
+    return rect;
+  }
+
+  // Transform all corners to handle CSS `transform: scale(...)` correctly
+  // (crbug.com/562177779).
+  return gfx::QuadF(view->TransformPointToRootCoordSpaceF(rect.origin()),
+                    view->TransformPointToRootCoordSpaceF(rect.top_right()),
+                    view->TransformPointToRootCoordSpaceF(rect.bottom_right()),
+                    view->TransformPointToRootCoordSpaceF(rect.bottom_left()))
+      .BoundingBox();
+}
 
 }  // namespace
 
@@ -1588,9 +1606,9 @@ void ChromePasswordManagerClient::AutomaticGenerationAvailable(
     return;
   }
 
-  autofill::FieldGlobalId field_id = autofill::FieldGlobalId(
-      autofill::LocalFrameToken(*driver->render_frame_host()->GetFrameToken()),
-      ui_data.generation_element_id);
+  autofill::FieldGlobalId field_id =
+      autofill::FieldGlobalId(autofill::LocalFrameToken(*rfh.GetFrameToken()),
+                              ui_data.generation_element_id);
 
 #if BUILDFLAG(IS_ANDROID)
   if (!ShouldAcceptFocusEvent(web_contents(), driver,
@@ -1602,7 +1620,7 @@ void ChromePasswordManagerClient::AutomaticGenerationAvailable(
       PasswordGenerationController::GetOrCreate(web_contents());
 
   gfx::RectF element_bounds_in_screen_space =
-      TransformToRootCoordinates(&rfh, ui_data.bounds);
+      TransformToRootCoordinates(rfh, ui_data.bounds);
 
   auto has_saved_credentials =
       !credential_cache_.GetCredentialStore(rfh.GetLastCommittedOrigin())
@@ -1618,7 +1636,7 @@ void ChromePasswordManagerClient::AutomaticGenerationAvailable(
 #else
   // Attempt to show the autofill dropdown UI first.
   gfx::RectF element_bounds_in_top_frame_space =
-      TransformToRootCoordinates(driver->render_frame_host(), ui_data.bounds);
+      TransformToRootCoordinates(rfh, ui_data.bounds);
   if (driver->GetPasswordAutofillManager()
           ->MaybeShowPasswordSuggestionsWithGeneration(
               field_id, element_bounds_in_top_frame_space,
@@ -1731,7 +1749,7 @@ void ChromePasswordManagerClient::ShowPasswordEditingPopup(
   CHECK(driver);
 
   gfx::RectF element_bounds_in_screen_space =
-      GetBoundsInScreenSpace(TransformToRootCoordinates(&rfh, bounds));
+      GetBoundsInScreenSpace(TransformToRootCoordinates(rfh, bounds));
   autofill::password_generation::PasswordGenerationUIData ui_data(
       bounds, /*max_length=*/0, /*generation_element=*/std::u16string(),
       field_renderer_id,
@@ -2234,7 +2252,7 @@ void ChromePasswordManagerClient::ShowPasswordGenerationPopup(
     password_manager::ContentPasswordManagerDriver* driver,
     const autofill::password_generation::PasswordGenerationUIData& ui_data) {
   gfx::RectF element_bounds_in_top_frame_space =
-      TransformToRootCoordinates(driver->render_frame_host(), ui_data.bounds);
+      TransformToRootCoordinates(*driver->render_frame_host(), ui_data.bounds);
 
   gfx::RectF element_bounds_in_screen_space =
       GetBoundsInScreenSpace(element_bounds_in_top_frame_space);
@@ -2290,18 +2308,6 @@ void ChromePasswordManagerClient::MaybeShowSavePasswordPrimingPromo(
 }
 
 #endif  // !BUILDFLAG(IS_ANDROID)
-
-gfx::RectF ChromePasswordManagerClient::TransformToRootCoordinates(
-    content::RenderFrameHost* frame_host,
-    const gfx::RectF& bounds_in_frame_coordinates) {
-  content::RenderWidgetHostView* rwhv = frame_host->GetView();
-  if (!rwhv) {
-    return bounds_in_frame_coordinates;
-  }
-  return gfx::RectF(rwhv->TransformPointToRootCoordSpaceF(
-                        bounds_in_frame_coordinates.origin()),
-                    bounds_in_frame_coordinates.size());
-}
 
 #if BUILDFLAG(IS_ANDROID)
 void ChromePasswordManagerClient::ResetErrorMessageDelegate() {
