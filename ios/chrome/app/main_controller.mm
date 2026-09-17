125006f770981450c165464985d8e1dfa2ce953f
Marc Treib <treib@chromium.org>
2026-09-17 07:38:11 -0700

    MIM cleanup: Remove kMultiprofileKey
    Multiprofile is now always enabled, so this isn't needed anymore.
    
    Bug: 407498240
    Change-Id: I6a70b7d74c979511576876ae76f629f26a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425082
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699791}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -235,9 +235,6 @@ NSString* const kLogInstallAttribution = @"LogInstallAttribution";
 // Constant for deferred MetricKit registration.
 NSString* const kRegisterMetricKit = @"RegisterMetricKit";
 
-// Constant for enabling  multi-profile.
-NSString* const kMultiprofileKey = @"MultiprofileKey";
-
 // Adapted from chrome/browser/ui/browser_init.cc.
 void RegisterComponentsForUpdate() {
   component_updater::ComponentUpdateService* cus =
@@ -1390,12 +1387,6 @@ std::string GetProfileNameForChoice(ProfileChoice choice,
     kWidgetKitRefreshFiveMinutes : @{
       kFieldTrialValueKey : @([[NSUserDefaults standardUserDefaults]
           boolForKey:kWidgetKitRefreshFiveMinutes]),
-      kFieldTrialVersionKey : @1,
-    },
-    // TODO(crbug.com/407498240): Remove this key and its usages, since
-    // multi-profile is now always enabled.
-    kMultiprofileKey : @{
-      kFieldTrialValueKey : @YES,
       kFieldTrialVersionKey : @1,
     },
   };
