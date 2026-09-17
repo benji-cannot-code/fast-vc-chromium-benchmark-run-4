0ed2a895051f50641ca95d022d3eacfcfc4b0eca
Niko Tsirakis <ntsirakis@google.com>
2026-09-16 17:11:25 -0700

    reporting: Use FAST_BATCH properties for SECURITY events
    Prior to this commit, events with the SECURITY priority
    had no retry backoff and no batching. Under conditions
    where the client could not send records off the disk,
    this caused repeated and heavy logging spam to the syslog,
    which ultimately culminated in disk space exhaustion.
    
    Bug: 549212778
    Change-Id: Ib02532431313825be24d1d098ba0bd8358002c5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8358713
    Reviewed-by: Shanthanu Bhardwaj <xanth@google.com>
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699362}

--- chrome/browser/policy/messaging_layer/upload/encrypted_reporting_client.h
+++ chrome/browser/policy/messaging_layer/upload/encrypted_reporting_client.h
@@ -138,7 +138,7 @@ class EncryptedReportingClient {
   FRIEND_TEST_ALL_PREFIXES(EncryptedReportingClientTest,
                            UploadsSequenceThrottled);
   FRIEND_TEST_ALL_PREFIXES(EncryptedReportingClientTest,
-                           SecurityUploadsSequenceNotThrottled);
+                           SecurityUploadsSequenceThrottled);
   FRIEND_TEST_ALL_PREFIXES(EncryptedReportingClientTest,
                            FailedUploadsSequenceThrottled);
 
--- chrome/browser/policy/messaging_layer/upload/record_handler_upload_unittest.cc
+++ chrome/browser/policy/messaging_layer/upload/record_handler_upload_unittest.cc
@@ -661,6 +661,9 @@ TEST_F(RecordHandlerUploadTest, RepeatedInitiationAttempts) {
         init_encrypted_record.sequence_information().sequencing_id() + 1);
     expected_response.sequence_information.set_sequencing_id(
         init_encrypted_record.sequence_information().sequencing_id());
+
+    // Avoid rate limiting by time.
+    task_environment_.FastForwardBy(base::Minutes(1));
   }
 }
 
@@ -795,6 +798,9 @@ TEST_F(RecordHandlerUploadTest, RepeatedNextStepAttempts) {
             1);
     expected_response.sequence_information.set_sequencing_id(
         next_step_encrypted_record.sequence_information().sequencing_id());
+
+    // Avoid rate limiting by time.
+    task_environment_.FastForwardBy(base::Minutes(1));
   }
 }
 
@@ -924,6 +930,9 @@ TEST_F(RecordHandlerUploadTest, RepeatedFinalizeAttempts) {
         fin_encrypted_record.sequence_information().sequencing_id() + 1);
     expected_response.sequence_information.set_sequencing_id(
         fin_encrypted_record.sequence_information().sequencing_id());
+
+    // Avoid rate limiting by time.
+    task_environment_.FastForwardBy(base::Minutes(1));
   }
 }
 
