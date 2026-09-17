3235179ef3b531d861e48a64fe71625f3b2ee29f
Mingjing Zhang <mjzhang@chromium.org>
2026-09-17 13:53:41 -0700

    [gpu] Add release count comparison in SignalLatestSyncToken()
    This CL adds the necessary release count comparison for selecting the
    latest SyncToken in SignalLatestSyncToken(). This change is a
    prerequisite before enabling UseAutomaticSyncTokenManagement.
    
    Bug: 40286368
    Change-Id: I765351ad34db9c8dccc3f9e9232ea0e55df8899f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8417323
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700100}

--- gpu/command_buffer/client/client_shared_image.cc
+++ gpu/command_buffer/client/client_shared_image.cc
@@ -566,7 +566,8 @@ uint64_t ClientSharedImage::SignalLatestSyncToken(
       }
       base::AutoLock auto_lock(shared_image->lock_);
       auto it = shared_image->sync_token_map_.find(client_id);
-      if (it != shared_image->sync_token_map_.end() && it->second.HasData()) {
+      if (it != shared_image->sync_token_map_.end() &&
+          it->second.release_count() > latest_sync_token.release_count()) {
         latest_sync_token = it->second;
       }
     }
