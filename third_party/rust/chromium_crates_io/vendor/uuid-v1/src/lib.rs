0a7158bc1bdf01cc790b762b0d1e296fbe96039f
jingyingqiu <jingyingqiu@google.com>
2026-09-16 23:23:08 -0700

    Roll uuid: 1.26.0 => 1.26.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * uuid: 1.26.0 => 1.26.1; https://docs.rs/crate/uuid/1.26.1
    
    Bug: None
    Change-Id: I740df0d4fd4e6ac98fe71fccef6795b87fb0d6f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8403370
    Commit-Queue: Jing Ying Qiu <jingyingqiu@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699549}

--- third_party/rust/chromium_crates_io/vendor/uuid-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/uuid-v1/src/lib.rs
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.26.0"
+//! version = "1.26.1"
 //! # Lets you generate random UUIDs
 //! features = [
 //!     "v4",
@@ -139,7 +139,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.26.0"
+//! version = "1.26.1"
 //! features = [
 //!     "v4",
 //!     "v7",
@@ -154,7 +154,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //!
 //! ```toml
 //! [dependencies.uuid]
-//! version = "1.26.0"
+//! version = "1.26.1"
 //! default-features = false
 //! ```
 //!
@@ -207,14 +207,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! [`wasm-bindgen`]: https://crates.io/crates/wasm-bindgen
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
-
 #![no_std]
 #![deny(missing_debug_implementations, missing_docs)]
 #![allow(clippy::mixed_attributes_style)]
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/uuid/1.26.0"
+    html_root_url = "https://docs.rs/uuid/1.26.1"
 )]
 
 #[cfg(any(feature = "std", test))]
