1590f4c64765134dc30c0e9adeade62f1865d9a8
Alison Gale <agale@chromium.org>
2026-09-17 12:49:29 -0700

    [Organizer] Show all search results or no results
    Once the user types a query, show all results instead of the top N. If
    the search results in a section having no entries, then show placeholder
    text.
    
    Change-Id: I1cf7f8264370daf4ab840a7ef3d24a437dc9be90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420762
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700054}

--- chrome/test/data/webui/organizer_panel/app_test.ts
+++ chrome/test/data/webui/organizer_panel/app_test.ts
@@ -22,6 +22,7 @@ suite('OrganizerPanelAppTest', () => {
       cjkWordBoundaryEnabled: false,
       clearSearch: 'Clear search',
       closeTab: 'Close tab',
+      noResults: 'No results',
       openTabs: 'Open Tabs',
       recentlyClosed: 'Recently Closed',
       searchTabs: 'Search Tabs',
