ac040d2c75620deb71c20a9c478c3ca857eba87a
Nihar Majmudar <niharm@google.com>
2026-09-16 17:01:13 -0700

    [loomnibox] Create small variation of loomnibox.
    Creates a small variation of the Loomnibox where it is 480px wide (on
    by default).
    
    Updates most visited tiles to only allow for 5 tiles in this case.
    
    https://screencast.googleplex.com/cast/NTc5MTIzNzgxNzc2MTc5MnxlMGYxZGNlMy1kMw
    
    Change-Id: I3438c3296b7f9683416078ca4bc72beb03e1eb72
    Bug: 562172273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415768
    Reviewed-by: Juliet Knighton <julietlevesque@google.com>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699351}

--- chrome/browser/resources/omnibox_everywhere/app.ts
+++ chrome/browser/resources/omnibox_everywhere/app.ts
@@ -108,6 +108,11 @@ export class OmniboxEverywhereAppElement extends CrLitElement {
       callbackRouter_: {type: Object},
       hasMostVisitedTiles_: {type: Boolean},
       mostVisitedEnabled_: {type: Boolean},
+      smallLoomnibox_: {
+        type: Boolean,
+        reflect: true,
+        attribute: 'small-loomnibox',
+      },
       freStage_: {type: Number},
       hotkeyTokens_: {type: Array},
       isActive_: {
@@ -146,6 +151,8 @@ export class OmniboxEverywhereAppElement extends CrLitElement {
       SearchboxBrowserProxy.getInstance().callbackRouter;
   protected accessor mostVisitedEnabled_: boolean =
       loadTimeData.getBoolean('omniboxEverywhereMostVisitedEnabled');
+  protected accessor smallLoomnibox_: boolean =
+      loadTimeData.getBoolean('smallLoomnibox');
   protected accessor hasMostVisitedTiles_: boolean = false;
   protected accessor freStage_: FreStage =
       (loadTimeData.valueExists('initialFreStage') ?
