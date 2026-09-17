0eb5c28776fe44c7dba051d783818e5aed55fc43
Chris Fredrickson <cfredric@chromium.org>
2026-09-17 15:12:18 -0700

    [SAA] Check CanAccessDataForOrigin in IsStorageAccessRestricted
    This means that all callers (which includes permission requests,
    StorageAccessHandle binding, DOM storage binding, and permission
    renewals) will verify that the frame is not prevented from accessing
    that data by ChildProcessSecurityPolicyImpl.
    
    Fixed: 561099700
    Change-Id: Icd70880c2fdb965941d7f4dfdbbe8e4ae30e1386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420925
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700162}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -13806,7 +13806,9 @@ bool RenderFrameHostImpl::IsStorageAccessRestricted() {
          IsNestedWithinFencedFrame() ||
          IsSandboxed(
              network::mojom::WebSandboxFlags::kStorageAccessByUserActivation) ||
-         GetStorageKey().ForbidsUnpartitionedStorageAccess();
+         GetStorageKey().ForbidsUnpartitionedStorageAccess() ||
+         !ChildProcessSecurityPolicyImpl::GetInstance()->CanAccessDataForOrigin(
+             GetProcess()->GetID().GetUnsafeValue(), GetLastCommittedOrigin());
 }
 
 void RenderFrameHostImpl::BindBlobUrlStoreAssociatedReceiver(
