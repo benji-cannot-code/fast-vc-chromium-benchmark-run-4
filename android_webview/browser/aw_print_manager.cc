62da47d61ac116ee4508cdd98d42e7a2eadea818
Lei Zhang <thestig@chromium.org>
2026-09-16 18:12:38 -0700

    Simplify SaveDataToFd() inside aw_print_manager.cc
    In crrev.com/882924, SaveDataToFd() stopped doing size_t to int
    conversions in order to call base::WriteFileDescriptor(), so delete the
    base::IsValueInRangeForNumericType() check that makes sure the data's
    size fits in an int.
    
    Change-Id: I0a3ebc09cdd727da91bccca4c510edc0dc269cf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8398977
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699395}

--- android_webview/browser/aw_print_manager.cc
+++ android_webview/browser/aw_print_manager.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted_memory.h"
-#include "base/notreached.h"
-#include "base/numerics/safe_conversions.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "components/printing/browser/print_manager_utils.h"
@@ -35,8 +33,7 @@ namespace {
 uint32_t SaveDataToFd(base::ScopedFD fd,
                       uint32_t page_count,
                       scoped_refptr<base::RefCountedSharedMemoryMapping> data) {
-  bool did_write_successfully =
-      fd.is_valid() && base::IsValueInRangeForNumericType<int>(data->size());
+  bool did_write_successfully = fd.is_valid();
   if (did_write_successfully) {
     did_write_successfully = base::WriteFileDescriptor(fd.get(), *data);
   }
