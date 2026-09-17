7012b67903a9210c58ed2c2fdcabf806cb10f756
christianxu <christianxu@chromium.org>
2026-09-17 01:52:55 -0700

    [iOS][TTC] Add microphone input debug UI and mediator
    Add microphone input debug UI to TTC prototype UI.
    
    Bug: None
    Change-Id: I8c6564aba1c9bb396da8f29be0fd3056d1fda02a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8406615
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699618}

--- ios/chrome/browser/ai_prototyping/ttc/coordinator/ttc_coordinator.mm
+++ ios/chrome/browser/ai_prototyping/ttc/coordinator/ttc_coordinator.mm
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ai_prototyping/ttc/coordinator/ttc_coordinator.h"
 
+#import "ios/chrome/browser/ai_prototyping/ttc/coordinator/ttc_mediator.h"
 #import "ios/chrome/browser/ai_prototyping/ttc/ui/ttc_view_controller.h"
 #import "ios/chrome/browser/ai_prototyping/utils/ai_prototyping_constants.h"
 
 @implementation TTCCoordinator {
   // View controller for TalkToChrome.
   TTCViewController* _viewController;
+
+  // Mediator for TalkToChrome.
+  TTCMediator* _mediator;
 }
 
 #pragma mark - ChromeCoordinator
@@ -18,9 +22,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)start {
   _viewController = [[TTCViewController alloc]
       initForFeature:AIPrototypingFeature::kTalkToChrome];
+  _mediator = [[TTCMediator alloc] init];
+
+  _mediator.consumer = _viewController;
+  _viewController.ttcMutator = _mediator;
 }
 
 - (void)stop {
+  [_mediator disconnect];
+  _mediator = nil;
   _viewController = nil;
 }
 
---
+++ ios/chrome/browser/ai_prototyping/ttc/ui/ttc_mutator.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_AI_PROTOTYPING_TTC_UI_TTC_MUTATOR_H_
+#define IOS_CHROME_BROWSER_AI_PROTOTYPING_TTC_UI_TTC_MUTATOR_H_
+
+#import <Foundation/Foundation.h>
+
+// Protocol for mutating model and hardware state based on user interactions
+// in the TalkToChrome prototype UI.
+@protocol TTCMutator <NSObject>
+
+// Starts an end-to-end voice session.
+- (void)startSession;
+
+// Gracefully terminates the active voice session.
+- (void)stopSession;
+
+// Notifies the mutator that the UI has appeared, triggering state hydration.
+- (void)viewWillAppear;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_AI_PROTOTYPING_TTC_UI_TTC_MUTATOR_H_
