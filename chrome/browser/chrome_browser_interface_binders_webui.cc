3b98ceb5e38d8cc07315acc9e1296866b0f7bee9
Duncan Mercer <mercerd@google.com>
2026-09-16 18:27:07 -0700

    [ContextualTasks] Register toolbar PageHandlerFactory and add toolbar browser proxy
    Register contextual_tasks_toolbar::mojom::PageHandlerFactory in
    chrome_browser_interface_binders_webui.cc to allow binding the toolbar
    WebUI interface.
    
    Add contextual_tasks_toolbar_browser_proxy.ts and test proxy utilities
    for the new contextual_tasks_toolbar.mojom interface.
    
    Bug: b:561759282
    Change-Id: I5bb2cb16b494940f156a23f63bbf7e3925848b04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8396500
    Reviewed-by: James Leung <jamesleung@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699405}

--- chrome/browser/chrome_browser_interface_binders_webui.cc
+++ chrome/browser/chrome_browser_interface_binders_webui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_actuator/internals/browser_actuator_internals_ui.h"
 #include "chrome/browser/chrome_browser_interface_binders.h"
 #include "chrome/browser/chrome_browser_interface_binders_webui_parts.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_toolbar.mojom.h"
 #include "chrome/browser/contextual_tasks/contextual_tasks_ui.h"
 #include "chrome/browser/media/media_engagement_score_details.mojom.h"
 #include "chrome/browser/optimization_guide/optimization_guide_internals_ui.h"
@@ -268,6 +269,9 @@ void PopulateChromeWebUIFrameBindersPartsAllPlatforms(
   if (contextual_tasks::IsContextualTasksUIEnabled()) {
     RegisterWebUIControllerInterfaceBinder<
         contextual_tasks::mojom::PageHandlerFactory, ContextualTasksUI>(map);
+    RegisterWebUIControllerInterfaceBinder<
+        contextual_tasks_toolbar::mojom::PageHandlerFactory,
+        ::ContextualTasksUI>(map);
     RegisterWebUIControllerInterfaceBinder<
         contextual_tasks_internals::mojom::
             ContextualTasksInternalsPageHandlerFactory,
