4233b0de82f06daac2549c52be5231813858ba4c
Philip Pfaffe <pfaffe@chromium.org>
2026-09-17 05:03:48 -0700

    [inspector] Draw an IMCB overlay
    This will allow devtools to show an overlay highlighting the IMCB for
    oof-position elements, plus a few anchor positioning properties such as
    the position area grid and the anchor itself.
    
    Bug: 413061413
    Change-Id: I344e4525a0ce7b43bd108f8ca1ea27ce30fabf86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8391739
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699700}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -1118,6 +1118,9 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
   // https://drafts.csswg.org/css-anchor-position-1/#ref-for-valdef-anchor-implicit
   const LayoutObject* AcceptableImplicitAnchor() const;
 
+  // Finds the default anchor element based on StylePositionAnchor type.
+  const LayoutObject* FindDefaultAnchor() const;
+
   const GCedHeapVector<NonOverflowingScrollRange>* NonOverflowingScrollRanges()
       const;
 
@@ -1132,6 +1135,11 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
       const;
   bool NeedsAnchorPositionScrollAdjustmentInX() const;
   bool NeedsAnchorPositionScrollAdjustmentInY() const;
+
+  // The inset-modified containing block of this out-of-flow box, relative to
+  // the containing block's border-box.
+  // https://www.w3.org/TR/css-position-3/#inset-modified-containing-block
+  std::optional<PhysicalRect> InsetModifiedContainingBlockRect() const;
 
   using LayoutObject::GetBackgroundPaintLocation;
 
