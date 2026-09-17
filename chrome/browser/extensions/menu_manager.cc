42a55e3e957155277fd74c61cb8b5200790555bb
Tim <tjudkins@chromium.org>
2026-09-17 15:46:58 -0700

    [Extensions] Apply tab scrubbing in context menu command execution
    When dispatching `chrome.contextMenus.onClicked` in
    `MenuManager::ExecuteCommand`, tab objects were constructed using a
    hardcoded `kDontScrubTab` policy rather than evaluating extension
    permissions. Consequently, tab details were not scrubbed for extensions
    lacking corresponding permissions.
    
    Update `MenuManager::ExecuteCommand` to determine tab scrubbing behavior
    dynamically via `ExtensionTabUtil::GetScrubTabBehavior`. In addition,
    invoke `ActiveTabPermissionGranter::GrantIfRequested` prior to tab
    object construction so extensions with the `activeTab` permission
    receive the appropriate tab data upon invocation.
    
    Also add tests covering tab scrubbing behavior for context menus.
    
    Bug: 513521516
    Change-Id: I170a73d86dc77555ce1a1af85f14b99feaaa9764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8382844
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Oliver Dunk <oliverdunk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700202}

--- chrome/browser/extensions/menu_manager.cc
+++ chrome/browser/extensions/menu_manager.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_handlers/background_info.h"
+#include "extensions/common/mojom/context_type.mojom.h"
 #include "extensions/common/mojom/event_dispatcher.mojom.h"
 #include "ipc/constants.mojom.h"
 #include "third_party/blink/public/mojom/context_menu/context_menu.mojom.h"
@@ -738,6 +739,14 @@ void MenuManager::ExecuteCommand(content::BrowserContext* context,
       WebViewGuest::FromRenderFrameHost(render_frame_host);
 #endif  // BUILDFLAG(ENABLE_GUEST_VIEW)
 
+  // Note: web_contents are null in unit tests :(
+  if (extension && web_contents) {
+    if (auto* granter =
+            ActiveTabPermissionGranter::FromWebContents(web_contents)) {
+      granter->GrantIfRequested(extension);
+    }
+  }
+
   base::ListValue args;
   args.Append(std::move(properties));
 
@@ -748,15 +757,17 @@ void MenuManager::ExecuteCommand(content::BrowserContext* context,
     // Note: web_contents are null in unit tests :(
     if (web_contents) {
       int frame_id = ExtensionApiFrameIdMap::GetFrameId(render_frame_host);
-      if (frame_id != ExtensionApiFrameIdMap::kInvalidFrameId)
+      if (frame_id != ExtensionApiFrameIdMap::kInvalidFrameId) {
         args[0].GetDict().Set("frameId", frame_id);
+      }
 
-      // We intentionally don't scrub the tab data here, since the user chose to
-      // invoke the extension on the page.
-      // TODO(tjudkins) Potentially use GetScrubTabBehavior here to gate based
-      // on permissions.
-      ExtensionTabUtil::ScrubTabBehavior scrub_tab_behavior = {
-          ExtensionTabUtil::kDontScrubTab, ExtensionTabUtil::kDontScrubTab};
+      // The contextMenus API is only available to privileged extension
+      // contexts.
+      constexpr mojom::ContextType context_type =
+          mojom::ContextType::kPrivilegedExtension;
+      ExtensionTabUtil::ScrubTabBehavior scrub_tab_behavior =
+          ExtensionTabUtil::GetScrubTabBehavior(extension, context_type,
+                                                web_contents);
       args.Append(ExtensionTabUtil::CreateTabObject(
                       web_contents, scrub_tab_behavior, extension)
                       .ToValue());
@@ -777,15 +788,9 @@ void MenuManager::ExecuteCommand(content::BrowserContext* context,
     item->SetChecked(checked);
     args[0].GetDict().Set("checked", item->checked());
 
-    if (extension)
+    if (extension) {
       WriteToStorage(extension, item->id().extension_key);
-  }
-
-  // Note: web_contents are null in unit tests :(
-  if (web_contents &&
-      ActiveTabPermissionGranter::FromWebContents(web_contents)) {
-    ActiveTabPermissionGranter::FromWebContents(web_contents)
-        ->GrantIfRequested(extension);
+    }
   }
   {
     // Dispatch to menu item's .onclick handler (this is the legacy API, from
