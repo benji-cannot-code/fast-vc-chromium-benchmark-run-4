1795269d43229a5b288682785790f85a4ad58a58
viplavkadam <viplavkadam@google.com>
2026-09-17 16:41:39 -0700

    [Churned-User][Clank] Add UI skeleton for Churned Users bottom sheet
    Adds the Android UI components and layout resource for the Payments
    Churned Users bottom sheet.
    
    This CL introduces:
    - XML layout resource for the notice bottom sheet with a centered
      title.
    - Model-View-Controller (MVC) Java classes for the bottom sheet
      (Coordinator, Mediator, Properties, View, ViewBinder, Content).
    - Unit tests for the Content, Mediator, Module, and View.
    
    Bug: 558879825, 558874126
    Change-Id: Icc28949c13987d7df12dd2c078916d8b047195b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394452
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Longsheng Yin <longsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700266}

---
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/payments_churned_users/AutofillPaymentsChurnedUsersBottomSheetViewBinder.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.payments_churned_users;
+
+import static org.chromium.chrome.browser.autofill.payments_churned_users.AutofillPaymentsChurnedUsersBottomSheetProperties.TITLE;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** ViewBinder for the Payments Churned Users bottom sheet. */
+@NullMarked
+/*package*/ class AutofillPaymentsChurnedUsersBottomSheetViewBinder {
+    static void bind(
+            PropertyModel model,
+            AutofillPaymentsChurnedUsersBottomSheetView view,
+            PropertyKey propertyKey) {
+        if (propertyKey == TITLE) {
+            view.getTitleText().setText(model.get(TITLE));
+        } else {
+            assert false : "Unhandled update to property: " + propertyKey;
+        }
+    }
+
+    private AutofillPaymentsChurnedUsersBottomSheetViewBinder() {}
+}
