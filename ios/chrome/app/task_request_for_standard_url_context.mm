fd11c10e7d9af6c33397097e2f2297780bb644b4
Federica Germinario <fedegermi@google.com>
2026-09-17 06:07:34 -0700

    [iOS] Handle appswitchertesting external action in TaskRequest
    This change adds implementation in new startup flow to handle
    appswitchertesting external action.
    
    Metrics in this case are recorded at execution time because
    IsAppSwitcherAISummarizationEnabled() it's causing a startup creah if
    executed too early.
    
    Change-Id: Ie4bbd62609fcee963e1bb4f5d443b5428b110683
    Bug: 493816082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394232
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699724}

--- ios/chrome/app/task_request_for_standard_url_context.mm
+++ ios/chrome/app/task_request_for_standard_url_context.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "net/base/apple/url_conversions.h"
+#import "net/base/url_util.h"
 #import "url/gurl.h"
 
 namespace {
@@ -164,9 +165,18 @@ void RecordExternalActionMetrics(NSURL* url) {
       ProfileIOS* profile = sceneState.profileState.profile;
       CHECK_DEREF(profile).GetPrefs()->SetBoolean(
           prefs::kAppStoreGeminiPromoTriggered, true);
-    } else if (IsAppSwitcherAISummarizationEnabled() &&
-               [path isEqualToString:kExternalActionAppSwitcherTesting]) {
-      // TODO(crbug.com/493816082): Add implementation.
+    } else if ([path isEqualToString:kExternalActionAppSwitcherTesting]) {
+      // TODO(crbug.com/527016607): Remove this entire testing path when the
+      // feature is enabled by default.
+      std::string queryUrlString;
+      net::GetValueForKeyInQuery(externalGURL, "url", &queryUrlString);
+      GURL queryUrl(queryUrlString);
+
+      externalGURL = queryUrl.SchemeIsHTTPOrHTTPS()
+                         ? queryUrl
+                         : GURL(kGeminiAppStorePromoURL);
+      postOpeningAction =
+          TabOpeningPostOpeningAction::START_GEMINI_AI_SUMMARIZATION;
     } else {
       // An unrecognized or invalid external action is discarded without opening
       // a tab.
