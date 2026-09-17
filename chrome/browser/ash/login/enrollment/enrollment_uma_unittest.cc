34bee0ba0d0d2aa685a04b45fc8a8540011bba45
Achuith Bhandarkar <achuith@chromium.org>
2026-09-17 13:26:32 -0700

    ash: Clean up exit-time destructors in login/enrollment
    Give TokenBasedEnrollmentOOBEConfigUMATestCase a std::string_view
    member so the test case table can be constexpr, and name it
    kTestCases. This removes no_exit_time_destructors from unit_tests.
    
    TAG=agy
    CONV=11e4ce9d-d986-4c57-af7b-22dcb639d393
    
    Bug: 40031409, 522379416
    Change-Id: I376c74e82e9756a0654cd8590426d09d109cc17c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8416735
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700081}

--- chrome/browser/ash/login/enrollment/enrollment_uma_unittest.cc
+++ chrome/browser/ash/login/enrollment/enrollment_uma_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/enrollment/enrollment_uma.h"
 
+#include <string_view>
+
 #include "base/strings/strcat.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "chrome/browser/ash/policy/enrollment/enrollment_config.h"
@@ -15,10 +17,10 @@ namespace ash {
 
 struct TokenBasedEnrollmentOOBEConfigUMATestCase {
   policy::OOBEConfigSource oobe_config_source;
-  std::string expected_oobe_config_source_variant;
+  std::string_view expected_oobe_config_source_variant;
 };
 
-TokenBasedEnrollmentOOBEConfigUMATestCase test_cases[] = {
+constexpr TokenBasedEnrollmentOOBEConfigUMATestCase kTestCases[] = {
     {policy::OOBEConfigSource::kNone, "None"},
     {policy::OOBEConfigSource::kUnknown, "Unknown"},
     {policy::OOBEConfigSource::kRemoteDeployment, "RemoteDeployment"},
@@ -64,10 +66,10 @@ TEST_P(TokenBasedEnrollmentUMAFailureTest, Failure) {
 
 INSTANTIATE_TEST_SUITE_P(Success,
                          TokenBasedEnrollmentUMASuccessTest,
-                         testing::ValuesIn(test_cases));
+                         testing::ValuesIn(kTestCases));
 
 INSTANTIATE_TEST_SUITE_P(Failure,
                          TokenBasedEnrollmentUMAFailureTest,
-                         testing::ValuesIn(test_cases));
+                         testing::ValuesIn(kTestCases));
 
 }  //  namespace ash
