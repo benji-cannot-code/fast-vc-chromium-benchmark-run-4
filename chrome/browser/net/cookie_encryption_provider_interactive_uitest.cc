38a910ff0af14da0d5ac171c67b68e704b08711d
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-09-16 18:01:00 -0700

    Revert "[Encrypt Sessions] Launch Phase 1 & Test Phase 2"
    This reverts commit 41957424d2d82a7266ebb5cf69ec07fa69eec505.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5358730464985088
    
    Sample build with failed test: https://ci.chromium.org/b/8670472355633481905
    Affected test(s):
    [://chrome/test\:interactive_ui_tests!gtest::CookieEncryptionProviderBrowserTest#CookieStorage/secret_portal](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:interactive_ui_tests%21gtest::CookieEncryptionProviderBrowserTest%23CookieStorage%2Fsecret_portal?q=VHash%3Af48eb13aa23f21fe)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5358730464985088&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8027331&type=BUG
    
    Original change's description:
    > [Encrypt Sessions] Launch Phase 1 & Test Phase 2
    >
    > Phase 1 is approved for M155, and we can start testing Phase 2 :-)
    >
    > Bug: 479420496
    > Change-Id: I728831ad255910d282bb24b5affc84aea542aaaa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8027331
    > Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1699105}
    >
    
    Bug: 479420496
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifbe6bfc74fc451e9cb4b54a5ce0f29cec7ce9de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8421759
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699384}

--- chrome/browser/net/cookie_encryption_provider_interactive_uitest.cc
+++ chrome/browser/net/cookie_encryption_provider_interactive_uitest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/process/process_info.h"
-#include "base/sanitizer_buildflags.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
@@ -22,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
-#include "components/sessions/core/command_storage_features.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_launcher.h"
@@ -188,10 +186,6 @@ class CookieEncryptionProviderBrowserTest
 #if BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
         disabled_features.push_back(features::kDbusSecretPortal);
 #endif  // BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
-#if BUILDFLAG(IS_ASAN)
-        // TODO(crbug.com/479420496): Resolve encryptor issues in ASAN.
-        disabled_features.push_back(sessions::kEncryptSessionStorage);
-#endif  // BUILDFLAG(IS_ASAN)
         break;
 #if BUILDFLAG(IS_WIN)
       case kOSCryptAsyncNoService:
@@ -224,10 +218,6 @@ class CookieEncryptionProviderBrowserTest
         enabled_features.push_back(features::kDbusSecretPortal);
         enabled_features.push_back(
             features::kSecretPortalKeyProviderUseForEncryption);
-#if BUILDFLAG(IS_ASAN)
-        // TODO(crbug.com/479420496): Resolve encryptor issues in ASAN.
-        disabled_features.push_back(sessions::kEncryptSessionStorage);
-#endif  // BUILDFLAG(IS_ASAN)
         break;
 #endif  // BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
     }
