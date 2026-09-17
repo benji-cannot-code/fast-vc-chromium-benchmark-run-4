ef000964be1c9f11cf4eab6904c92cf404c2a76f
Arnaud <wendlassida@google.com>
2026-09-17 03:50:38 -0700

    [Signin][Android] Add kHandleMdmErrorsForDasherAccountsOnAndroid flag
    This CL adds a flag for the MDM error handling project on Android.
    
    Fixed: 561512291
    Change-Id: I380387a0e38a40915871817a6bac824063dc4ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394024
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Arnaud Yameogo <wendlassida@google.com>
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699677}

--- components/signin/public/base/signin_feature_map.cc
+++ components/signin/public/base/signin_feature_map.cc
@@ -38,6 +38,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &switches::kMakeIdentityManagerSourceOfAccountsPart2,
     &switches::kMigrateAccountManagerDelegate,
     &switches::kFullscreenSignInPromoUseDate,
+    &switches::kHandleMdmErrorsForDasherAccountsOnAndroid,
     &switches::kSmartEmailLineBreaking,
     &switches::kSupportWebSigninAddSession,
     &switches::kSignOutDeletesBrowsingData,
