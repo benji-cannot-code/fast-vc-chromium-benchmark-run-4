62e48f99dda095c1381b834fc3a35d3ed26f35ac
Sorin Jianu <sorin@chromium.org>
2026-09-16 21:11:28 -0700

    updater: remove non-portable _countof call
    Change-Id: Ie1f371c1ccbe7ebe03e774f885fc323722246c8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8421819
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699508}

--- chrome/updater/win/installer/string_unittest.cc
+++ chrome/updater/win/installer/string_unittest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include <stddef.h>
-#include <stdlib.h>
 
+#include <iterator>
 #include <string>
 
 #include "testing/gtest/include/gtest/gtest.h"
@@ -38,7 +38,7 @@ TEST_F(InstallerStringTest, StackStringOverflow) {
 
   size_t max_chars = str.capacity() - 1;
 
-  while ((str.length() + (_countof(kTestString) - 1)) <= max_chars) {
+  while ((str.length() + (std::size(kTestString) - 1)) <= max_chars) {
     EXPECT_TRUE(str.append(kTestString));
     compare_str.append(kTestString);
     EXPECT_EQ(str.length(), compare_str.length());
