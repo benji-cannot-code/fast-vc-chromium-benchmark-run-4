5d49bf9d2ed37bb73246cb41956898c1147c590c
Calder Kitagawa <ckitagawa@chromium.org>
2026-09-17 15:30:17 -0700

    [Android] Expose BottomControlsJankImprovement in about_flags
    Exposes the kBottomControlsJankImprovement feature flag to
    chrome://flags under "bottom-controls-jank-improvement" on Android so
    developers and testers can easily toggle scroll jank and visual
    transition improvements for bottom browser controls.
    
    * Adds kBottomControlsJankImprovementName and
      kBottomControlsJankImprovementDescription in flag_descriptions.h.
    * Registers the feature entry in about_flags.cc for kOsAndroid.
    * Adds flag metadata in flag-metadata.json with expiry milestone 165.
    * Adds corresponding LoginCustomFlags histogram enum entries in
      enums.xml.
    
    Bug: 552502622
    Change-Id: I6b590fe3ef8ee4f13f4a680fe1c12d346a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8426120
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700183}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11682,6 +11682,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAndroidWindowManagementWebApiDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(permissions::features::kAndroidWindowManagementWebApi)},
 
+    {"bottom-controls-jank-improvement",
+     flag_descriptions::kBottomControlsJankImprovementName,
+     flag_descriptions::kBottomControlsJankImprovementDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kBottomControlsJankImprovement)},
+
     {"browser-controls-debugging",
      flag_descriptions::kBrowserControlsDebuggingName,
      flag_descriptions::kBrowserControlsDebuggingDescription, kOsAndroid,
