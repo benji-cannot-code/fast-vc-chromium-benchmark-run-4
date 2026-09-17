af51a5c1e18064cea0a1e297cc0f34a40111d954
Russ Hamilton <behamilton@google.com>
2026-09-17 11:19:40 -0700

    [WebUI Toolbar] Add expanded margin to avatar button with label
    In native Views ToolbarView, expanded buttons with labels use an expanded
    margin of 5px (kBrowserAppMenuRefreshExpandedMargin) instead of the
    collapsed margin of 2px.
    
    In WebUI, the flex container has gap: 2px. This CL adds a 3px
    (5px - 2px) margin to <avatar-button> when has-label is set,
    matching the native Views button position and spacing.
    
    Bug: 548571813
    Change-Id: I1724f38c68f10861b4b26f52a7d77deef8a7881c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415232
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699973}

--- chrome/browser/ui/webui/webui_toolbar/webui_toolbar_layout_css_helper.cc
+++ chrome/browser/ui/webui/webui_toolbar/webui_toolbar_layout_css_helper.cc
@@ -141,6 +141,8 @@ std::string_view LayoutConstantToCssVarName(LayoutConstant layout_constant) {
       return "--toolbar-element-padding";
     case kToolbarIconDefaultMargin:
       return "--toolbar-icon-default-margin";
+    case kToolbarButtonRefreshExpandedMargin:
+      return "--toolbar-button-refresh-expanded-margin";
     case kToolbarCornerRadius:
       return "--toolbar-corner-radius";
     case kSidePanelInset:
