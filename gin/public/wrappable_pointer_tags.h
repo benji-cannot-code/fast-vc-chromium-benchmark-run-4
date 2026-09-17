308e1ff0839f046bd94425ae25e0c7593c6d20c6
Andreas Haas <ahaas@chromium.org>
2026-09-17 09:49:28 -0700

    Remove kScriptStateTag from kScriptWrappableTagRange
    kScriptStateTag is part of CppHeapPointerTag to avoid accidental tag
    sharing. However, ScriptState is not a ScriptWrappable, and therefore
    the tag should not be within kScriptWrappableTagRange. Otherwise, the
    function `ToAnyScriptWrappable` casts a ScriptState object to a
    Scriptwrappable and causes type confusion.
    
    As a drive-by change, gin::WrappablePointerTag gets cleaned up, and
    kGinPerContextData also gets extracted from kGinWrappableTagRange, as
    GinPerContextData not a subclass of gin::Wrappable.
    
    Bug: 562249006
    Change-Id: Idc34dfc52f24916ea9c3f5af46a6cd4c3972966e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419709
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699904}

--- gin/public/wrappable_pointer_tags.h
+++ gin/public/wrappable_pointer_tags.h
@@ -41,7 +41,6 @@ enum WrappablePointerTag : uint16_t {
   kGamepadControllerBindings,        // content::GameControllerBindings
   kGCController,                     // content::GCController
   kGinJavaBridgeObject,              // content::GinJavaBridgeObject
-  kGinPerContextData,                // gin::PerContextData
   kGinPort,                          // extensions::GinPort
   kGpuBenchmarking,                  // content::GpuBenchmarking
   kIndigoContext,                    // indigo::IndigoContext
@@ -54,9 +53,6 @@ enum WrappablePointerTag : uint16_t {
   kLoadTimesBindings,                // LoadTimesBindings
   kLocalStorageArea,                 // extensions::LocalStorageArea
   kManagedStorageArea,               // extensions::ManagedStorageArea
-  kMojo,                             // ax::Mojo
-  kMojoHandle,                       // ax::MojoHandle
-  kMojoWatcher,                      // ax::MojoWatcher
   kMyInterceptor,                    // gin::MyInterceptor
   kNetErrorPageController,           // NetErrorPageController
   kNewTabPageBindings,               // NewTabPageBindings
@@ -78,12 +74,15 @@ enum WrappablePointerTag : uint16_t {
   kTestObject2,                         // gin::MyObject2
   kTestPluginScriptableObject,   // content::(anonymous)::ScriptableObject
   kTestRunnerBindings,           // content::TestRunnerBindings
-  kTextDecoder,                  // ax::TextDecoder
-  kTextEncoder,                  // ax::TextEncoder
   kTextInputControllerBindings,  // content::TextInputControllerBindings
   kWebAXObjectProxy,             // content::WebAXObjectProxy
   kWrappedExceptionHandler,      // extensions::WrappedExceptionHandler
   kWrappedHandlerFunction,       // extensions::WrappedHandlerFunction
+  // keep-sorted end
+  kLastWrappablePointerTag,
+  // Pointer tags for non-gin::Wrappable classes.
+  // keep-sorted start case=no
+  kGinPerContextData,  // gin::PerContextData
   // keep-sorted end
   kLastPointerTag,
 };
