311727eb8414ec2c5f0c37ec038ab3a26528fc6d
Andrey Kosyakov <caseq@chromium.org>
2026-09-18 14:02:10 -0700

    Copy ArrayBuffer contents if we're trying to transfer with a non-1 use count
    Transferring ArrayBuffer contents while someone else still retains it doesn't look safe, so just defer to copying it.
    
    Fixed: 499250086
    Change-Id: I584edb73c85c9f62736f24bc4b782721a7d73078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423381
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701073}

--- third_party/blink/renderer/core/layout/shapes/shape.cc
+++ third_party/blink/renderer/core/layout/shapes/shape.cc
@@ -282,7 +282,7 @@ bool ExtractPathData(const Path& path,
     if (result.DataLength() != dst_size_bytes) {
       return false;
     }
-    result.Transfer(contents);
+    contents = std::move(result);
   }
 
   const SkSurfaceProps disable_lcd_props;
@@ -326,7 +326,7 @@ static bool ExtractImageData(Image* image,
                                ArrayBufferContents::kZeroInitialize);
     if (result.DataLength() != dst_size_bytes)
       return false;
-    result.Transfer(contents);
+    contents = std::move(result);
   }
 
   // Set |surface| to draw directly to |contents|.
--- third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer_contents.h
+++ third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer_contents.h
@@ -157,7 +157,7 @@ class CORE_EXPORT ArrayBufferContents {
     return backing_store_;
   }
 
-  void Transfer(ArrayBufferContents& other);
+  void TransferOrCopy(ArrayBufferContents& other);
   void ShareWith(ArrayBufferContents& other);
   void ShareNonSharedForInternalUse(ArrayBufferContents& other);
   void CopyTo(ArrayBufferContents& other);
