606ea097553a52aa32b736689a599626adbcab6b
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2026-09-18 03:47:45 -0700

    Revert "grvk: Implement BackendFormat format utils"
    This reverts commit e9ca6f72c87348d3876f56a90705118d24763859.
    
    Reason for revert: many tests hitting NOTREACHED in GetGraphiteContext
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/android-14-arm64-rel/19509/test-results?q=ExactID%3A%3A%2F%2Fandroid_webview%2Ftest%5C%3Awebview_64_cts_tests%21junit%3Aandroid.text.cts%3AEmojiTest%23testEmojiGlyphWebView+VHash%3A366b52a8fd946fa0
    
    Original change's description:
    > grvk: Implement BackendFormat format utils
    >
    > Add utility functions to create appropriate BackendFormat descriptors
    > for Graphite native Vulkan.
    >
    > Bug: 55295190
    > Change-Id: I4f77c4c781fb748fc70dd107750947fd5b49af32
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8414786
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1700166}
    
    Bug: 55295190, 563256152
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 55295190
    Change-Id: I202dde2e6090e3b3cc008a22d48230dc1cfeaccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423725
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Owners-Override: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700632}

--- gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h
+++ gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <dxgi.h>
 #endif  // BUILDFLAG(IS_WIN)
 
-namespace gfx {
-class ColorSpace;
-}
-
 namespace skgpu::graphite {
 class TextureInfo;
 class DawnTextureInfo;
@@ -134,10 +130,6 @@ GPU_GLES2_EXPORT VkFormat ToVkFormatSinglePlanar(viz::SharedImageFormat format);
 // multiplanar with per-plane sampling.
 GPU_GLES2_EXPORT VkFormat ToVkFormat(viz::SharedImageFormat format,
                                      int plane_index);
-// Returns supported vulkan usage flags based on `format`. `is_yuv_plane`
-// indicates if the texture corresponds to a plane of a multi-planar image
-GPU_GLES2_EXPORT VkImageUsageFlags
-SupportedVkImageUsage(viz::SharedImageFormat format, bool is_yuv_plane);
 #endif
 
 #if BUILDFLAG(IS_WIN)
@@ -203,7 +195,6 @@ GPU_GLES2_EXPORT skgpu::graphite::TextureInfo GraphiteBackendTextureInfo(
 GPU_GLES2_EXPORT skgpu::graphite::TextureInfo GraphitePromiseTextureInfo(
     GrContextType gr_context_type,
     viz::SharedImageFormat format,
-    const gfx::ColorSpace& color_space,
     std::optional<VulkanYCbCrInfo> ycbcr_info,
     int plane_index = 0,
     bool mipmapped = false);
