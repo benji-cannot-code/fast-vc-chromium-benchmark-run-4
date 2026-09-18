3fb83fcb7de50c1762ff59eec31de13dc2df1d2c
Russ Hamilton <behamilton@google.com>
2026-09-18 11:06:40 -0700

    [WebUI Toolbar] Add expanded margins to App Menu button with label
    In native Views ToolbarView, expanded buttons with labels use an
    expanded margin of 5px
    (LayoutConstant::kToolbarButtonRefreshExpandedMargin) instead of the
    default collapsed margin of 2px. Furthermore, when the App Menu button
    has a label, ToolbarView::UpdateRefreshMarginsWithContextualTasks
    increases the trailing interior margin by +1px (from 6px to 7px).
    
    In WebUI toolbar:
    
    1. When <app-menu-button> has a label, add an expanded leading margin of
       3px (calc(var(--toolbar-button-refresh-expanded-margin) -
       var(--toolbar-icon-default-margin))), expanding the gap to 5px.
    2. If the preceding <avatar-button> is visible and already has a label,
       its trailing margin already expands the inter-button gap to 5px.
       Collapse the app menu button's leading margin to 0px so the margins
       do not additively widen to 8px.
    3. In <app-menu-button>, increase the trailing margin by +1px (from 6px
       to 7px, or 0px with --toolbar-chip-trailing-margin set to 7px when
       maximized/fullscreen).
    
    Bug: 548571813
    Change-Id: Ib174d6f70abc766366f369b8cd71bb51769d681e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8424483
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700960}

--- chrome/browser/resources/webui_toolbar/avatar_button.ts
+++ chrome/browser/resources/webui_toolbar/avatar_button.ts
@@ -72,6 +72,9 @@ export class AvatarButtonElement extends AvatarButtonElementBase {
       BrowserProxyImpl.getInstance()
           .toolbarUIHandler.setAvatarButtonIphPromoShowing(this.hasHelpBubble);
     }
+    if (changedProperties.has('state')) {
+      this.toggleAttribute('has-label', !!this.state.text);
+    }
   }
 
   override accessor state: AvatarControlState = {
