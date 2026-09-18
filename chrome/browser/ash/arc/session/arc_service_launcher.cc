5b214b48b86c2af79a4e310c14c2ee96164e98ac
Satoru Takabayashi <satorux@google.com>
2026-09-17 21:36:04 -0700

    arc: Remove ArcSurveyService and chrome/browser/ash/arc/survey
    Deletes ArcSurveyService and its unit tests in
    chrome/browser/ash/arc/survey/, and removes service instantiation and
    build dependencies from ArcServiceLauncher and BUILD.gn files.
    
    The feature flag kHappinessTrackingSystemArcGames is disabled by default
    with 0 Finch study configurations and no entry in
    fieldtrial_testing_config.json.
    
    Bug: 562297559
    Change-Id: Ifbae7f89c0179ca31fe32eb0ff2786f6fede2950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425059
    Commit-Queue: Satoru Takabayashi <satorux@google.com>
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700466}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/session/arc_play_store_enabled_preference_handler.h"
 #include "chrome/browser/ash/arc/session/arc_session_manager.h"
 #include "chrome/browser/ash/arc/sharesheet/arc_sharesheet_bridge.h"
-#include "chrome/browser/ash/arc/survey/arc_survey_service.h"
 #include "chrome/browser/ash/arc/tracing/arc_app_performance_tracing.h"
 #include "chrome/browser/ash/arc/tracing/arc_tracing_bridge.h"
 #include "chrome/browser/ash/arc/tts/arc_tts_service.h"
@@ -368,7 +367,6 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcScreenCaptureBridge::GetForBrowserContext(profile);
   ArcSettingsService::GetForBrowserContext(profile);
   ArcSharesheetBridge::GetForBrowserContext(profile);
-  ArcSurveyService::GetForBrowserContext(profile);
   ArcSystemUIBridge::GetForBrowserContext(profile);
   if (base::FeatureList::IsEnabled(kArcTracingDataSource)) {
     ArcTracingBridge::GetForBrowserContext(profile);
@@ -557,7 +555,6 @@ void ArcServiceLauncher::EnsureFactoriesBuilt() {
   ArcScreenCaptureBridge::EnsureFactoryBuilt();
   ArcSettingsService::EnsureFactoryBuilt();
   ArcSharesheetBridge::EnsureFactoryBuilt();
-  ArcSurveyService::EnsureFactoryBuilt();
   ArcSystemUIBridge::EnsureFactoryBuilt();
   ArcSystemStateBridge::EnsureFactoryBuilt();
   ArcTracingBridge::EnsureFactoryBuilt();
