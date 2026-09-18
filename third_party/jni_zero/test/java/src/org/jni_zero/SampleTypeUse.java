f197fa6948562667f1bd5e64a033f5f2f411184b
Keigo Oka <oka@google.com>
2026-09-17 22:34:32 -0700

    JNI Zero: Pass JniUniquePtr by rvalue reference in CalledByNative
    JniUniquePtr is move-only. Accepting it by rvalue reference (&&) in
    CalledByNative dispatchers matches JNI Zero's convention for move-only
    @JniType parameters and avoids an extra move construction between the
    mirrored wrapper and the Java_... entry point. JniPtr (T*) and JniRawPtr
    are trivially copyable and continue to be passed by value.
    
    TAG=agy
    CONV=9d8066d1-de31-41b4-9093-054426041c40
    
    Bug: 40794873
    Change-Id: I83093ffe40c57c887719ae9b49e5978d75d95ac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264587
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700494}

--- third_party/jni_zero/test/java/src/org/jni_zero/SampleTypeUse.java
+++ third_party/jni_zero/test/java/src/org/jni_zero/SampleTypeUse.java
@@ -16,6 +16,9 @@ public class SampleTypeUse {
     public static void baz(JniPtr<SampleTypeDefine.Nested> ptr) {}
 
     @CalledByNative
+    public static void qux(JniUniquePtr<SampleTypeDefine> ptr) {}
+
+    @CalledByNative
     public static JniPtr<SampleTypeDefine> lendIt() {
         return null;
     }
