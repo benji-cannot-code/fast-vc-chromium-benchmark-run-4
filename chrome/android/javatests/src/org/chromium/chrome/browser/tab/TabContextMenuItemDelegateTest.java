b5dcd2a7d39149b49c556cf6968c78e0be6cf3cc
Sky Malice <skym@chromium.org>
2026-09-18 10:46:44 -0700

    Revert "[Tests] Fully remove mocks in Tab & TabModel tests"
    This reverts commit 7e009b61172f6f3975630ab561a83f5536ea8e91.
    
    Reason for revert: IncognitoTabModelTest (testTabCountSupplierAddedAfter, testCurrentTabSupplierAddedBefore, testCurrentTabSupplierAddedAfter, testTabCountSupplierAddedBefore) is flaking across android-x64-rel and android-internal-desktop-x64-rel
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/try/android-x64-rel/1145842/overview
    
    Original change's description:
    > [Tests] Fully remove mocks in Tab & TabModel tests
    >
    > Replaces test mocks in TabContextMenuItemDelegateTest,
    > IncognitoTabModelTest, and TabTest with real Profile and
    > CallbackHelper instances for reliable verification.
    >
    > See
    > https://docs.google.com/document/d/1h_ZpYiNikfBFuEkzqwHd21IhfrMPumT-_6Uz4RHtUF8
    > for more context.
    >
    > Bug: 551864627
    > Change-Id: Ic5aea043cfd22636fed9f9da413cd45277cec02f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8404452
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1700250}
    
    Bug: 551864627
    TAG=agy
    CONV=ed552894-39e8-41a6-9099-0d2065d0dbd7
    
    Change-Id: Iab4e48fb165996d6cf22c787749e329b6dab60e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8429666
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700940}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabContextMenuItemDelegateTest.java
@@ -33,8 +33,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-import org.chromium.base.CallbackUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
@@ -70,9 +72,12 @@ import java.util.function.Supplier;
 @DoNotBatch(reason = "This class runs tests that create new activities.")
 public class TabContextMenuItemDelegateTest {
     @Rule
-    public final FreshCtaTransitTestRule mActivityTestRule =
+    public FreshCtaTransitTestRule mActivityTestRule =
             ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Mock private Runnable mContextMenuCopyLinkObserver;
     private ModalDialogManager mModalDialogManager;
     private TabContextMenuItemDelegate mContextMenuDelegate;
     private List<ChromeTabbedActivity> mExtraTabbedActivities;
@@ -255,7 +260,7 @@ public class TabContextMenuItemDelegateTest {
                                     tab,
                                     tabModelSelector,
                                     ephemeralTabCoordinatorSupplier,
-                                    CallbackUtils.emptyRunnable(),
+                                    mContextMenuCopyLinkObserver,
                                     snackbarManagerSupplier,
                                     bottomSheetControllerSupplier);
                 });
