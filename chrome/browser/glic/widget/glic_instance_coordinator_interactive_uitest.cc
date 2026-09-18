9dc9bdc475e4900741d56a647991881598205bbc
Dan Harrington <harringtond@chromium.org>
2026-09-18 16:20:33 -0700

    Skip AccountInvalidatedWhileGlicOpen when kGlicNoWebview
    Error:
    https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:interactive_ui_tests%21gtest::GlicInstanceCoordinatorUiTest%23AccountInvalidatedWhileGlicOpen?q=VHash%3Ab40013e758d65ca1
    
    Bug: b:563462692
    Change-Id: I9f5d289e458f029e5fc8353e6d7456a36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423273
    Auto-Submit: Dan H <harringtond@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701192}

--- chrome/browser/glic/widget/glic_instance_coordinator_interactive_uitest.cc
+++ chrome/browser/glic/widget/glic_instance_coordinator_interactive_uitest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/glic/glic_warming_checks.h"
 #include "chrome/browser/glic/host/glic.mojom.h"
 #include "chrome/browser/glic/host/glic_web_contents_warming_pool.h"
+#include "chrome/browser/glic/public/features.h"
 #include "chrome/browser/glic/public/glic_invoke_options.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
@@ -420,6 +421,10 @@ IN_PROC_BROWSER_TEST_F(GlicInstanceCoordinatorUiTest,
 #endif
 IN_PROC_BROWSER_TEST_F(GlicInstanceCoordinatorUiTest,
                        MAYBE_AccountInvalidatedWhileGlicOpen) {
+  if (features::IsGlicNoWebviewEnabled()) {
+    // TODO(b/563462692): Fix this
+    GTEST_SKIP() << "Fails on some bots";
+  }
   TrackGlicInstanceWithTabIndex(0);
   RunTestSequence(
       SimulateGlicHotkey(), WaitForWebUIState(mojom::WebUiState::kReady),
