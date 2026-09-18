18c2ee8f55105d8f4c7d35c3f9c8a4d67c5ca586
Danilo Tedeschi <danft@google.com>
2026-09-18 09:53:23 -0700

    Use ScopedDefaultIcuLocale in L10nUtilTest
    On platforms that do not take the locale from the environment,
    SetDefaultLocaleForTest() changed the default ICU locale with the
    deprecated base::i18n::SetICUDefaultLocale and never restored it. It now
    delegates to the fixture's SetIcuLocaleForTest(), which owns a
    base::i18n::ScopedDefaultIcuLocale and therefore goes through the
    pass-key protected setter in
    //base/i18n/icubridge/default_icu_locale.h and restores the locale when
    the fixture is torn down.
    
    Bug: 542711615
    TAG=agy
    CONV=80ad1161-eaf8-4cb6-9fe5-d55481ed1bed
    
    Change-Id: I67ec4d0068ff83b94247dab5e03ae97b13dee5e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425428
    Reviewed-by: Paula Gómez <paugomez@google.com>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700880}

--- ui/base/l10n/l10n_util_unittest.cc
+++ ui/base/l10n/l10n_util_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/icu_test_util.h"
 #include "base/test/scoped_path_override.h"
 #include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -145,7 +144,7 @@ class L10nUtilTest : public PlatformTest {
     if (kUseLocaleFromEnvironment) {
       env->SetVar("LANGUAGE", tag.ToLegacyICUFormat());
     } else {
-      base::i18n::SetICUDefaultLocale(tag.ToLegacyICUFormat());
+      SetIcuLocaleForTest(tag);
     }
   }
 
