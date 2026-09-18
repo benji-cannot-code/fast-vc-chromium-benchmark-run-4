951343856450f1e7a166c3ba59fca7a4bbcb21a0
Stefan Zager <szager@chromium.org>
2026-09-18 08:44:43 -0700

    [HiC] Correctly apply canvas scaling to ElementImage.(width|height)
    This makes the width and height values equal to the internal default
    values for the dwidth and dheight parameters to drawElementImage, which
    obey the invariant: drawElementImage with default dwidth/dheight and no
    transform applied to the canvas context should draw the element with the
    same on-screen proportions as it would appear were the element placed
    outside the canvas with identical computed style.
    
    Bug: 456097302
    Change-Id: I3b268ea39a94e8b8236f685f8fd5916404863c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423113
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700809}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -2007,6 +2007,7 @@ static void PopulateCanvasChildPaintState(HTMLCanvasElement* canvas,
   const LayoutReplaced* replaced = To<LayoutReplaced>(canvas->GetLayoutBox());
   const ComputedStyle& style = replaced->StyleRef();
 
+  paint_state.canvas_size = canvas->Size();
   paint_state.canvas_content_size =
       gfx::SizeF(replaced->ReplacedContentRect().size);
   paint_state.canvas_device_pixel_content_box =
@@ -4823,6 +4824,13 @@ void PaintPropertyTreeBuilder::UpdateForChildren() {
           PaintPropertyChangeType::kUnchanged) {
     object_.GetFrameView()->SetIntersectionObservationState(
         LocalFrameView::kDesired);
+  }
+
+  if (const auto* canvas = DynamicTo<HTMLCanvasElement>(object_.GetNode())) {
+    if (canvas->IsContentDrawable() && object_.NeedsPaintPropertyUpdate()) {
+      context_.force_subtree_update_reasons |=
+          PaintPropertyTreeBuilderContext::kSubtreeUpdateIsolationPiercing;
+    }
   }
 
   if (is_isolated) {
