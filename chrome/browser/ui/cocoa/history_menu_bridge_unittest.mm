d1c7b034eed28fc298faae630c77f6c203bddcc9
thomas lukaszewicz <tluk@chromium.org>
2026-09-17 18:36:38 -0700

    Reland "[bedrock] Migrate Mac Cocoa tests from BrowserWithTestWindowTest"
    This is a reland of commit 27bdf05ca880cf4e61d5ac53f6983a253d56fde0
    
    See diff against PS1 for changes to address CI-only failures.
    
    Original change's description:
    > [bedrock] Migrate Mac Cocoa tests from BrowserWithTestWindowTest
    >
    > Migrate all 8 Mac Cocoa test suites from BrowserWithTestWindowTest:
    >
    > - Migrate window_size_autosaver_unittest, history_menu_bridge_unittest,
    >   history_menu_cocoa_controller_unittest, and
    >   bookmark_menu_bridge_unittest to CocoaTest with TestingProfile.
    >
    > - In profile_menu_controller_unittest, replace
    >   CreateBrowserWithTestWindowForParams with a mock
    >   BrowserWindowInterface.
    >
    > - Convert bookmark_menu_cocoa_controller_unittest,
    >   tab_group_menu_bridge_unittest, and
    >   browser_window_default_touch_bar_unittest to InProcessBrowserTest.
    >
    > - Remove BrowserWithTestWindowTest and TestBrowserWindow dependencies
    >   from chrome/browser/ui/cocoa/BUILD.gn.
    >
    > TAG=agy
    > CONV=8421e4d4-5e2d-43b1-8819-ba2e0028d0cb
    >
    > Bug: 417766643
    > Change-Id: Ibcb8ac7ced6496d2e203c6683efc76b112567392
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8416086
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1699044}
    
    TAG=agy
    CONV=7fe915a3-a0e5-42da-8668-8290e6231c7f
    
    Include-Ci-Only-Tests: true
    Bug: 417766643
    Change-Id: I876bb71898c689069cdab5ac35896581faf91b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8410433
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700379}

--- chrome/browser/ui/cocoa/history_menu_bridge_unittest.mm
+++ chrome/browser/ui/cocoa/history_menu_bridge_unittest.mm
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/cocoa/test/cocoa_test_helper.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/chrome_features.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/favicon_base/favicon_types.h"
 #include "components/os_crypt/async/browser/test_utils.h"
@@ -34,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/core/session_id.h"
 #include "components/sessions/core/tab_restore_service_impl.h"
 #include "components/tab_groups/tab_group_visual_data.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
@@ -134,33 +134,36 @@ class MockBridge : public HistoryMenuBridge {
   NSMenu* __strong menu_;
 };
 
-class HistoryMenuBridgeTest : public BrowserWithTestWindowTest {
+class HistoryMenuBridgeTest : public CocoaTest {
  public:
   bool ShouldMenuItemBeVisible(NSMenuItem* item) {
     return bridge_->ShouldMenuItemBeVisible(item);
   }
 
+  TestingProfile* profile() { return profile_.get(); }
+
  protected:
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
-    [AppController.sharedController setLastProfileForTesting:profile()];
+    CocoaTest::SetUp();
+    TestingProfile::Builder profile_builder;
+    profile_builder.AddTestingFactory(
+        FaviconServiceFactory::GetInstance(),
+        FaviconServiceFactory::GetDefaultFactory());
+    profile_builder.AddTestingFactory(
+        HistoryServiceFactory::GetInstance(),
+        HistoryServiceFactory::GetDefaultFactory());
+    profile_ = profile_builder.Build();
+    [AppController.sharedController setLastProfileForTesting:profile_.get()];
 
-    bridge_ = std::make_unique<MockBridge>(profile());
+    bridge_ = std::make_unique<MockBridge>(profile_.get());
     os_crypt_async_ = os_crypt_async::GetTestOSCryptAsyncForTesting(true);
   }
 
   void TearDown() override {
     bridge_.reset();
-    BrowserWithTestWindowTest::TearDown();
-  }
-
-  TestingProfile::TestingFactories GetTestingFactories() override {
-    return {TestingProfile::TestingFactory{
-                FaviconServiceFactory::GetInstance(),
-                FaviconServiceFactory::GetDefaultFactory()},
-            TestingProfile::TestingFactory{
-                HistoryServiceFactory::GetInstance(),
-                HistoryServiceFactory::GetDefaultFactory()}};
+    [AppController.sharedController setLastProfileForTesting:nullptr];
+    profile_.reset();
+    CocoaTest::TearDown();
   }
 
   // We are a friend of HistoryMenuBridge (and have access to
@@ -211,12 +214,13 @@ class HistoryMenuBridgeTest : public BrowserWithTestWindowTest {
     return bridge_->menu_item_map_;
   }
 
- private:
-  CocoaTestHelper cocoa_test_helper_;
-
  protected:
   std::unique_ptr<MockBridge> bridge_;
   std::unique_ptr<os_crypt_async::OSCryptAsync> os_crypt_async_;
+
+ private:
+  content::BrowserTaskEnvironment task_environment_;
+  std::unique_ptr<TestingProfile> profile_;
 };
 
 class HistoryMenuBridgeLifetimeTest : public testing::Test {
