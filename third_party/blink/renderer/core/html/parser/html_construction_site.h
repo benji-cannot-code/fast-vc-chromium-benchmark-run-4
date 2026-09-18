3f3397e0d1e83f129143957443bd06f8a1a79b32
Kurt Catti-Schmidt <kschmi@microsoft.com>
2026-09-17 17:54:27 -0700

    [large-text] Remove SplitLargeTextNodes flag and old code
    Remove the legacy 64k text node splitting path and make the unsplit
    parser behavior unconditional.
    
    This shipped in version 151 and no issues were reported.
    
    Bug: 523940241, 525278782
    Change-Id: Ie103118905c866b4b89139b274c920670c8cabdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8421865
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700339}

--- third_party/blink/renderer/core/html/parser/html_construction_site.h
+++ third_party/blink/renderer/core/html/parser/html_construction_site.h
@@ -73,7 +73,6 @@ class HTMLConstructionSite final {
   };
 
   static constexpr unsigned kMaximumHTMLParserDOMTreeDepth = 512;
-  static constexpr unsigned kObsoleteTextNodeLengthLimit = 1 << 16;
 
   HTMLConstructionSite(HTMLParserReentryPermit*,
                        Document&,
