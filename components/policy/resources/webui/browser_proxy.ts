561bf61e2173f701b9810df53cf3733f89c90614
Stanisław Solarewicz <ssolarewicz@google.com>
2026-09-18 03:36:22 -0700

    [chrome://policy] Listen for Status update from the mojo handler
    This CL is part of policy page mojo migration.
    
    This CL updates chrome://policy frontend to listen for the
    `StatusUpdated` event for the mojo version of the handler.
    
    To this end addition of the observers has been moved to the common
    constructor in the `PolicyUiHandler`.
    
    To match Desktop, iOS version of the handler now also sends status on
    policy update.
    
    Lastly, `GetStatusMojo` default implementation in the
    `PolicyStatusProvider` is updated to return a nullptr instead of
    silently returning an empty `policy::mojom::Status`.
    
    Bug: 40897784
    Change-Id: I87cfd11268ec18b4b6153d5f815ec86d811118d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8303619
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700625}

--- components/policy/resources/webui/browser_proxy.ts
+++ components/policy/resources/webui/browser_proxy.ts
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {sendWithPromise} from 'chrome://resources/js/cr.js';
+import {addWebUiListener, sendWithPromise} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {PolicyPageClientCallbackRouter, PolicyPageHandlerFactory, PolicyPageHandlerRemote} from './policy.mojom-webui.js';
-import type {GetPoliciesReason} from './policy.mojom-webui.js';
+import type {GetPoliciesReason, Status} from './policy.mojom-webui.js';
 
 const policyPageMojoMigrationEnabled =
     loadTimeData.getBoolean('policyPageMojoMigrationEnabled');
@@ -72,6 +72,15 @@ export class BrowserProxy {
           response => response.policiesJson);
     } else {
       return sendWithPromise('getPoliciesJson', reason as number);
+    }
+  }
+
+  static listenForStatusUpdated(
+      callback: (status: Record<string, Status>) => void) {
+    if (policyPageMojoMigrationEnabled) {
+      this.getInstance().callbackRouter.statusUpdated.addListener(callback);
+    } else {
+      addWebUiListener('status-updated', callback);
     }
   }
 }
