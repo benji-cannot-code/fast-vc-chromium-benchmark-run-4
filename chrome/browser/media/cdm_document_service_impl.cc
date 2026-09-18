51654cee83d88d1524916efeb5415184d76c4e36
Feras Aldahlawi <frs@chromium.org>
2026-09-18 15:31:45 -0700

    cdm: Compile ChallengePlatform for ChromeOS only at the mojo level
    Bug: 40499115
    Change-Id: If51fd775fd12d76476a681917d0496cf6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415320
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Feras Aldahlawi <frs@chromium.org>
    Reviewed-by: Sangbaek Park <sangbaekpark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701148}

--- chrome/browser/media/cdm_document_service_impl.cc
+++ chrome/browser/media/cdm_document_service_impl.cc
@@ -261,6 +261,7 @@ CdmDocumentServiceImpl::~CdmDocumentServiceImpl() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 }
 
+#if BUILDFLAG(IS_CHROMEOS)
 void CdmDocumentServiceImpl::ChallengePlatform(
     const std::string& service_id,
     const std::string& challenge,
@@ -268,10 +269,6 @@ void CdmDocumentServiceImpl::ChallengePlatform(
   DVLOG(2) << __func__;
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  // TODO(crbug.com/40499115). This should be commented out at the mojom
-  // level so that it's only available for ChromeOS.
-
-#if BUILDFLAG(IS_CHROMEOS)
   bool success = platform_verification::PerformBrowserChecks(
       render_frame_host().GetMainFrame());
   if (!success) {
@@ -288,13 +285,8 @@ void CdmDocumentServiceImpl::ChallengePlatform(
       service_id, challenge,
       base::BindOnce(&CdmDocumentServiceImpl::OnPlatformChallenged,
                      weak_factory_.GetWeakPtr(), std::move(callback)));
-#else
-  // Not supported, so return failure.
-  std::move(callback).Run(false, std::string(), std::string(), std::string());
-#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
-#if BUILDFLAG(IS_CHROMEOS)
 void CdmDocumentServiceImpl::OnPlatformChallenged(
     ChallengePlatformCallback callback,
     PlatformVerificationResult result,
