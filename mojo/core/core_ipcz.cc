b31f4c7a29441e70f4196486e5cfa7aa569669bc
Daniel Cheng <dcheng@chromium.org>
2026-09-18 08:44:12 -0700

    Ensure the handle is closed when unwrapping a platform handle fails
    The documentation for both MojoUnwrapPlatformHandle and
    MojoUnwrapPlatformSharedMemoryRegion states that "The Mojo handle is
    always closed regardless of success or failure". Update the ipcz
    implementations to match the documentation when trying to unwrap a
    handle of the incorrect type and add tests to catch future regressions.
    
    The legacy ChromeOS Mojo core implementation used by mojo_proxy is
    intentionally left unchanged, as it is used solely by the proxy
    implementation, which relies on MojoUnwrapPlatformHandle failing
    non-destructively to probe handle types when translating legacy Mojo
    handles to ipcz handles.
    
    Finally, fix an incidental typo in the PlatformHandle documentation.
    
    Bug: 41351792
    Change-Id: I6f87197fa8d276270deffe074052f9868584fb55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8421768
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700808}

--- mojo/core/core_ipcz.cc
+++ mojo/core/core_ipcz.cc
@@ -628,12 +628,16 @@ MojoResult MojoUnwrapPlatformHandleIpcz(
     MojoHandle mojo_handle,
     const MojoUnwrapPlatformHandleOptions* options,
     MojoPlatformHandle* platform_handle) {
-  if (!mojo_handle || !platform_handle ||
-      platform_handle->struct_size < sizeof(*platform_handle)) {
+  if (!mojo_handle) {
     return MOJO_RESULT_INVALID_ARGUMENT;
   }
   auto wrapper = ipcz_driver::WrappedPlatformHandle::Unbox(mojo_handle);
   if (!wrapper) {
+    MojoCloseIpcz(mojo_handle);
+    return MOJO_RESULT_INVALID_ARGUMENT;
+  }
+  if (!platform_handle ||
+      platform_handle->struct_size < sizeof(*platform_handle)) {
     return MOJO_RESULT_INVALID_ARGUMENT;
   }
   PlatformHandle::ToMojoPlatformHandle(std::move(wrapper->handle()),
@@ -674,13 +678,18 @@ MojoResult MojoUnwrapPlatformSharedMemoryRegionIpcz(
     uint64_t* num_bytes,
     MojoSharedBufferGuid* mojo_guid,
     MojoPlatformSharedMemoryRegionAccessMode* access_mode) {
-  if (!mojo_handle || !platform_handles || !num_platform_handles ||
-      !mojo_guid) {
+  if (!mojo_handle) {
     return MOJO_RESULT_INVALID_ARGUMENT;
   }
 
-  auto* buffer = ipcz_driver::SharedBuffer::FromBox(mojo_handle);
+  auto buffer = ipcz_driver::SharedBuffer::Unbox(mojo_handle);
   if (!buffer) {
+    MojoCloseIpcz(mojo_handle);
+    return MOJO_RESULT_INVALID_ARGUMENT;
+  }
+
+  if (!platform_handles || !num_platform_handles || !num_bytes || !mojo_guid ||
+      !access_mode) {
     return MOJO_RESULT_INVALID_ARGUMENT;
   }
 
@@ -740,7 +749,6 @@ MojoResult MojoUnwrapPlatformSharedMemoryRegionIpcz(
       NOTREACHED();
   }
 
-  std::ignore = ipcz_driver::SharedBuffer::Unbox(mojo_handle);
   return MOJO_RESULT_OK;
 }
 
