1151ac85cbc37a7d436b1e99a35e89e7344913df
Achuith Bhandarkar <achuith@chromium.org>
2026-09-17 18:43:17 -0700

    ash: Clean up exit-time destructors in ash/wm
    Fix exit-time destructors across ash/wm window restore, gestures,
    and overview code and unit tests. Modernize layout test parameters to
    constexpr arrays, move test save callback to a member variable on
    WindowRestoreController, and use a trivially destructible pointer
    for test restore image paths.
    
    TAG=agy
    CONV=e630c7ac-06eb-4a92-a894-f7de234b5aeb
    
    Bug: 40031409, 522379416
    Change-Id: Ic609928f2d1c74710aa43c81c20fde88c3189f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8405545
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700384}

--- ash/wm/overview/overview_metrics.h
+++ ash/wm/overview/overview_metrics.h
@@ -76,7 +76,7 @@ inline constexpr char kExitOverviewPresentationHistogram[] =
 inline constexpr char kOverviewDelayedDeskBarPresentationHistogram[] =
     "Ash.Overview.DelayedDeskBar.PresentationTime";
 
-const ui::PresentationTimeRecorder::BucketParams&
+ui::PresentationTimeRecorder::BucketParams
 GetOverviewPresentationTimeBucketParams();
 
 // Returns metric name with format:
