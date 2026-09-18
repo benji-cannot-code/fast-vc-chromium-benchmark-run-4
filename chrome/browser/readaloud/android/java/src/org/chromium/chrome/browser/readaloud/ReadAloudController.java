1c902b766997013f8a2217cfe15a5f5c9b10306d
Christian Jaime <christianjaime@google.com>
2026-09-18 13:52:30 -0700

    [ReadAloud] Fix use-after-free of ReadAloudBridge in CheckReadability
    Call OnNativeDestroyed() in ~ReadAloudBridge() so Java resets
    mNativeReadAloudBridge = 0 whenever the C++ bridge is destroyed (such as
    when replaced via SetDelegate()), preventing JNI calls on a dangling
    pointer. Also guard ReadAloudController#maybeCheckReadability() with
    mNativeBridge.isInitialized().
    
    Based on the Chrome Crash autofix draft: https://crrev.com/c/8427661
    
    Bug: 563030887
    Change-Id: Ibf596ad319aa015a5f514e930ef9390ebec8d00c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420167
    Commit-Queue: Christian Jaime <christianjaime@google.com>
    Reviewed-by: Roberto Rodriguez <rrodriguezh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1701066}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudController.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudController.java
@@ -924,7 +924,7 @@ public class ReadAloudController
             return;
         }
         if (ReadAloudFeatures.isNativeEnabled()) {
-            if (mNativeBridge != null) {
+            if (mNativeBridge.isInitialized()) {
                 mPendingRequests.add(urlSpecHash);
                 mNativeBridge.checkReadability(url);
             }
