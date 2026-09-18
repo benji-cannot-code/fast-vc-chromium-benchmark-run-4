052251415bf6caaeeb328061e39c7c62ef98116f
Mikel Astiz <mastiz@chromium.org>
2026-09-18 09:12:23 -0700

    [sync] Fix crash in TabContextItemSyncBridge::UploadItem()
    Starting with https://crrev.com/c/7956758, InMemoryMetadataChangeList
    enforces that all queued changes should either be honored/transferred or
    explicitly dropped.
    
    Before this patch, TabContextItemSyncBridge didn't consistently drop all
    changes, leading to CHECK failures in
    TabContextItemSyncBridge::UploadItem().
    
    To fix this, EmptyMetadataChangeList is adopted, which is precisely
    meant for the scenario at hands where a datatype doesn't actually need
    to support storage.
    
    Change-Id: I6b2651b51b0a9faf7f4e7222316f64b005525629
    Bug: 527991322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425107
    Reviewed-by: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700835}

--- components/sync_tab_context/tab_context_item_sync_bridge_unittest.cc
+++ components/sync_tab_context/tab_context_item_sync_bridge_unittest.cc
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/uuid.h"
 #include "components/sync/model/data_batch.h"
+#include "components/sync/model/metadata_change_list.h"
 #include "components/sync/protocol/encrypted_tab_context_item_specifics.pb.h"
 #include "components/sync/protocol/encryption.pb.h"
 #include "components/sync/protocol/entity_data.h"
+#include "components/sync/protocol/entity_metadata.pb.h"
 #include "components/sync/test/mock_data_type_local_change_processor.h"
 #include "components/sync_tab_context/container_id.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -47,7 +49,14 @@ TEST_F(TabContextItemSyncBridgeTest, ShouldUploadItemWhenTrackingMetadata) {
   encrypted_data.set_blob("encrypted_blob");
 
   ON_CALL(mock_processor_, IsTrackingMetadata).WillByDefault(Return(true));
-  EXPECT_CALL(mock_processor_, Put);
+  EXPECT_CALL(mock_processor_, Put)
+      .WillOnce([](const std::string& storage_key,
+                   std::unique_ptr<syncer::EntityData> entity_data,
+                   syncer::MetadataChangeList* metadata_change_list) {
+        ASSERT_THAT(metadata_change_list, NotNull());
+        metadata_change_list->UpdateMetadata(storage_key,
+                                             sync_pb::EntityMetadata());
+      });
 
   EXPECT_TRUE(
       bridge_->UploadItem(container_id, item_id, std::move(encrypted_data)));
