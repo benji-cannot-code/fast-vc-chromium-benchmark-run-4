34b063a0da7fe734db5f8ef4c0ab734f11e119f7
Joey Arhar <jarhar@chromium.org>
2026-09-18 08:45:50 -0700

    Exclude select with open autofill preview popover from FindBuffer
    Re-assigning select.selectedIndex can reset autofill_state_ to
    kNotFilled while leaving the autofill preview popover open, allowing
    window.find() to search the text in the popover.
    
    A followup change will be made to prevent the desync between
    autofill_state_ and the autofill popover being left open.
    
    Fixed: 562224383
    Change-Id: Ie0e495b577e4535a67aa2bb66a353f9661e2215a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423049
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700810}

--- third_party/blink/renderer/core/editing/finder/find_buffer_test.cc
+++ third_party/blink/renderer/core/editing/finder/find_buffer_test.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/finder/find_results.h"
 #include "third_party/blink/renderer/core/editing/selection_template.h"
 #include "third_party/blink/renderer/core/editing/testing/editing_test_base.h"
+#include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/forms/text_control_element.h"
 
 namespace blink {
@@ -834,6 +835,45 @@ TEST_P(FindBufferParamTest, DoNotSearchInSuggestedValues) {
 
     // This time, there should be a match.
     EXPECT_EQ(1U, results.CountForTesting());
+  }
+}
+
+TEST_P(FindBufferParamTest, DoNotSearchInSelectSuggestedOption) {
+  SetBodyContent(R"HTML(
+    <style>
+      #s option { visibility: hidden; }
+    </style>
+    <select id="s" size="2">
+      <option value="val1">secret</option>
+      <option value="val2">other</option>
+    </select>
+  )HTML");
+
+  auto* select = To<HTMLSelectElement>(GetElementById("s"));
+  select->SetSuggestedValue("val1");
+  ASSERT_TRUE(select->IsPreviewed());
+  ASSERT_TRUE(select->GetAutofillPreviewElement());
+  ASSERT_TRUE(select->GetAutofillPreviewElement()->popoverOpen());
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+
+  {
+    FindBuffer buffer(WholeDocumentRange(), GetParam());
+    const auto results = buffer.FindMatches("secret", FindOptions());
+    EXPECT_EQ(0U, results.CountForTesting());
+  }
+
+  // Self-assigning selectedIndex resets autofill state potentially without
+  // hiding the preview popover. Make sure that this does not affect
+  // findability.
+  select->setSelectedIndex(select->selectedIndex());
+  ASSERT_FALSE(select->IsPreviewed());
+  ASSERT_TRUE(select->GetAutofillPreviewElement()->popoverOpen());
+  GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kTest);
+
+  {
+    FindBuffer buffer(WholeDocumentRange(), GetParam());
+    const auto results = buffer.FindMatches("secret", FindOptions());
+    EXPECT_EQ(0U, results.CountForTesting());
   }
 }
 
