fa2bb0431de313a568393e9010d27236d07d0a43
Danilo Tedeschi <danft@google.com>
2026-09-17 17:48:52 -0700

    Use LanguageTag in //components/application_locale
    The LocaleFormat behavior is being removed since it is not properly
    working in the first place. If the user set the locale already in the
    bcp47 format, the different LocaleFormat has not effect, all the outputs
    from Get() will use "-" as separators. If the user call Set using a
    legacy format "_", we do get different outputs (e.g. "en_US" for
    kChromeNormalized and "en-US" for kBCP47). Since all the Setters in the
    codebase get their locales from resource bundle initialization, which
    uses bcp47 format, there is not reason to have LocaleFormat.
    
    Lastly, I'm also adding myself as an owner there since I am already an
    owner of //base/i18n and //ui/base/l10n.
    
    Bug: 537371481
    Change-Id: Id53123d1144212ea608f4c5f19fc1cfed695ab8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8416586
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700331}

--- components/application_locale_storage/application_locale_storage.h
+++ components/application_locale_storage/application_locale_storage.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback_list.h"
+#include "base/i18n/language_tag.h"
 #include "base/sequence_checker.h"
 
 // Manages the locale used by the application. Must be used from the same
@@ -41,8 +42,8 @@ class ApplicationLocaleStorage {
       OnLocaleChangedCallbackList::CallbackType cb);
 
  private:
-  std::string chrome_normalized_locale_;
-  std::string bcp47_locale_;
+  base::i18n::LanguageTag locale_;
+  std::string tag_string_;
   OnLocaleChangedCallbackList on_locale_changed_callback_list_;
 
   SEQUENCE_CHECKER(sequence_checker_);
