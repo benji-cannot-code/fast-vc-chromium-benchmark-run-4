b6e2107a4a902ac86328810b31cdf3c4a11c7017
dpapad <dpapad@chromium.org>
2026-09-18 16:45:16 -0700

    WebUI: Remove incorrect import URL from WebUI ESLint test files.
    Also removed the workaround that was previously used to mitigate the
    incorrect paths.
    
    Bug: 563435063
    Change-Id: I1121f00d92bf54346717be818dfe8e1f057771ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8428151
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701218}

--- ui/webui/resources/tools/tests/eslint_ts/with_webui_plugin_lit_reactive_properties_violations.html.ts
+++ ui/webui/resources/tools/tests/eslint_ts/with_webui_plugin_lit_reactive_properties_violations.html.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {html} from '/resources/lit/v3_0/lit.rollup.js';
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {SomeFooElement} from './with_webui_plugin_lit_reactive_properties_violations.js';
 
