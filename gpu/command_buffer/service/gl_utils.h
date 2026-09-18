4359cd8154d184b15530a400bc8d1afcd79fe5ef
Ken Russell <kbr@chromium.org>
2026-09-17 18:12:42 -0700

    gpu: revise size checks for compressed texture workarounds.
    For the previous upload_oversized_mip_levels_via_unpack_buffer
    workaround, compute compressed texture sizes in blocks rather than
    pixels to be more correct. Add a regression test for oversized ASTC
    mipmap uploads which ensures they don't crash.
    
    Bug: 556435507
    Change-Id: I9b0ad52ab332bbc18d9425f05d5a5df0a4304c6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8417361
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700359}

--- gpu/command_buffer/service/gl_utils.h
+++ gpu/command_buffer/service/gl_utils.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rrect_f.h"
 #include "ui/gfx/geometry/transform.h"
+#include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/overlay_transform.h"
 #include "ui/gl/gl_bindings.h"
 
@@ -117,6 +118,8 @@ GPU_GLES2_EXPORT void InitializeGLDebugLogging(bool log_non_errors,
 bool ValidContextLostReason(GLenum reason);
 error::ContextLostReason GetContextLostReasonFromResetStatus(
     GLenum reset_status);
+
+GPU_GLES2_EXPORT gfx::Vector2d GetCompressedTexBlockDimensions(GLenum format);
 
 bool GetCompressedTexSizeInBytes(const char* function_name,
                                  GLsizei width,
