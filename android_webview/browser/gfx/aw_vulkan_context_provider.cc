180f8a01495c7de0b5e999a447a6b32cb079ab07
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2026-09-18 03:56:40 -0700

    Revert "grvk: Add graphite context implementation"
    This reverts commit 54c5c5685528e29b1a087870885448b52b045e8f.
    
    Reason for revert: many tests hitting NOTREACHED in GetGraphiteContext
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/android-14-arm64-rel/19509/test-results?q=ExactID%3A%3A%2F%2Fandroid_webview%2Ftest%5C%3Awebview_64_cts_tests%21junit%3Aandroid.text.cts%3AEmojiTest%23testEmojiGlyphWebView+VHash%3A366b52a8fd946fa0
    
    Original change's description:
    > grvk: Add graphite context implementation
    >
    > Add code to VulkanContextProvider to create GraphiteSharedContext
    > instead of GrContext. Also add the code to trigger creation from
    > SharedContextState.
    >
    > This increases the binary size on Android considerably as it
    > includes the Skia Graphite native Vulkan code for the first time. The
    > size increase is unavoidable if Chrome uses the library.
    >
    > Binary-Size: See commit.
    > Bug: 552951905
    > Change-Id: I37b9c6e30529852bad8206c696048c87e711f166
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400809
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1699825}
    
    Bug: 552951905
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia61cf528b4cf8f95a546d6fb22dbba08afe6a17c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419545
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700634}

--- android_webview/browser/gfx/aw_vulkan_context_provider.cc
+++ android_webview/browser/gfx/aw_vulkan_context_provider.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/native_library.h"
-#include "base/notreached.h"
 #include "gpu/config/skia_limits.h"
 #include "gpu/vulkan/init/vulkan_factory.h"
 #include "gpu/vulkan/skia_vk_memory_allocator_impl.h"
@@ -267,10 +266,6 @@ GrDirectContext* AwVulkanContextProvider::GetGrContext() {
   return globals_->gr_context.get();
 }
 
-gpu::GraphiteSharedContext* AwVulkanContextProvider::GetGraphiteContext() {
-  NOTREACHED();
-}
-
 GrVkSecondaryCBDrawContext*
 AwVulkanContextProvider::GetGrSecondaryCBDrawContext() {
   CHECK(active_draw_state_);
@@ -308,11 +303,6 @@ bool AwVulkanContextProvider::InitializeGrContext(
   // GrContext is created in Globals, so nothing to do here besides DCHECK.
   DCHECK(globals_);
   return globals_->gr_context.get() != nullptr;
-}
-
-bool AwVulkanContextProvider::InitializeGraphiteContext(
-    const skgpu::graphite::ContextOptions& options) {
-  NOTREACHED();
 }
 
 void AwVulkanContextProvider::SecondaryCBDrawBegin(
