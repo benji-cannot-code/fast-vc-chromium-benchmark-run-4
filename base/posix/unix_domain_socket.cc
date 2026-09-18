b6041f1be6dda8124bbd9bf2aa553fb80207274c
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-09-18 09:29:12 -0700

    Spanification: Use std::array in sandbox/linux.
    Migrate C-style arrays with unsafe accesses to std::array and
    base::span across sandbox/linux to modernize storage and eliminate
    unsafe indexing.
    
    This is split from a large CL removing ~800 UNSAFE_TODOs. Using data
    from http://x20web/~arthursonzogni/state-of-spanification, a swarm of
    Gemini agents resolved the reported C-style array unsafe accesses.
    
    Bug: 559576258
    Change-Id: Ia95e41a45f77d5004a2efae91829f77fb83f2435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419114
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700847}

--- base/posix/unix_domain_socket.cc
+++ base/posix/unix_domain_socket.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-const size_t UnixDomainSocket::kMaxFileDescriptors = 16;
 
 bool CreateSocketPair(ScopedFD* one, ScopedFD* two) {
   int raw_socks[2];
