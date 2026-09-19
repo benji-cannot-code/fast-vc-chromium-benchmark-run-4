a9f0ee48edc45a72d5db23458a416ebf649a5803
dpapad <dpapad@chromium.org>
2026-09-18 18:31:34 -0700

    Settings Lit: Fix race condition in SettingsPrivacyGuideTest.FlowLength.
    This fixes the test failures on linux-chromeos-chrome.
    
    Bug: 393471368
    Fixed: b:563524000
    Change-Id: Icecceb74321b658f72d2a2832bd22b03070553c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8424432
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701292}

--- chrome/test/data/webui/settings/privacy_guide_test_util.ts
+++ chrome/test/data/webui/settings/privacy_guide_test_util.ts
@@ -174,6 +174,7 @@ export async function createPrivacyGuidePageForTest(
   document.body.appendChild(page);
 
   setupPrivacyRouteForTest();
+  await microtasksFinished();
 
   setupSync({
     syncBrowserProxy: syncBrowserProxy,
