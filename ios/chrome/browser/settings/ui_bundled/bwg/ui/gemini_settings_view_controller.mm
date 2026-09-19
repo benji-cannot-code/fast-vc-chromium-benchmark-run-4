e499d8296ae1d9bec48f333a68f99d8525c101a3
Ibrahim Kanouche <kanouche@google.com>
2026-09-19 16:22:06 -0700

    [Gemini] Add Gemini suggestions subpage scaffold and switch setting
    Create the Gemini suggestions settings subpage with the 'Show
    suggestions' toggle switch and the page content sharing footer link.
    Make the Gemini suggestions row clickable in
    GeminiSettingsViewController to navigate to this subpage.
    
    Bug: 559229402
    Change-Id: Ie74f98eb8f9890744b6d2862484524d558dfdd7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415146
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1701431}

--- ios/chrome/browser/settings/ui_bundled/bwg/ui/gemini_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/ui/gemini_settings_view_controller.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/settings/ui_bundled/bwg/model/gemini_settings_metadata.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/ui/gemini_camera_view_controller.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/ui/gemini_location_view_controller.h"
+#import "ios/chrome/browser/settings/ui_bundled/bwg/ui/gemini_suggestions_view_controller.h"
 #import "ios/chrome/browser/settings/ui_bundled/bwg/utils/gemini_settings_metrics.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_text_item.h"
@@ -101,6 +102,8 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   GeminiLocationViewController* _locationViewController;
   // Camera view controller shown when camera row is tapped.
   GeminiCameraViewController* _cameraViewController;
+  // Suggestions view controller shown when suggestions row is tapped.
+  GeminiSuggestionsViewController* _suggestionsViewController;
   // Closed captioning preference value.
   BOOL _closedCaptioningEnabled;
   // Precise location preference value.
@@ -203,8 +206,6 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
                                     IDS_IOS_GEMINI_SETTINGS_SUGGESTIONS_TITLE)
              trailingDetailText:[self suggestionsTrailingDetailText]
         accessibilityIdentifier:kSuggestionsCellId];
-    _suggestionsItem.accessoryType = UITableViewCellAccessoryNone;
-    _suggestionsItem.accessibilityTraits &= ~UIAccessibilityTraitButton;
 
     NSString* suggestionsFooterText =
         l10n_util::GetNSString(IDS_IOS_GEMINI_SETTINGS_SUGGESTIONS_FOOTER_TEXT);
@@ -485,6 +486,16 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   }
 
   if ([self.tableViewModel itemTypeForIndexPath:indexPath] ==
+      ItemTypeSuggestions) {
+    _suggestionsViewController = [[GeminiSuggestionsViewController alloc]
+        initWithStyle:ChromeTableViewStyle()];
+    _suggestionsViewController.suggestionsEnabled = _suggestionsEnabled;
+    _suggestionsViewController.mutator = self.mutator;
+    [self.navigationController pushViewController:_suggestionsViewController
+                                         animated:YES];
+  }
+
+  if ([self.tableViewModel itemTypeForIndexPath:indexPath] ==
       ItemTypeAppActivity) {
     RecordGeminiSettingsItemUsed(IOSGeminiSettingsItem::kGeminiAppsActivity);
     RecordGeminiSettingsAppsActivity();
@@ -591,6 +602,10 @@ NSString* const kPageContentSharingAction = @"PageContentSharingAction";
   }
 
   _suggestionsEnabled = enabled;
+
+  if (_suggestionsViewController) {
+    _suggestionsViewController.suggestionsEnabled = enabled;
+  }
 
   if ([self isViewLoaded]) {
     _suggestionsItem.trailingDetailText = [self suggestionsTrailingDetailText];
