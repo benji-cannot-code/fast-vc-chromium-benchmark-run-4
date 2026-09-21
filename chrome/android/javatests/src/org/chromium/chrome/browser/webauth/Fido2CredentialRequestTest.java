56d66d6ee1e97237eb52a154eeaa67f7356a9342
Adem Derinel <derinel@google.com>
2026-09-21 00:03:32 -0700

    [WebAuthn] Extract and complete MockFido2ApiCallHelper
    Previously, MockFido2ApiCallHelper only overrode
    invokeFido2GetCredentials and invokePasskeyCacheGetCredentials, leaving
    invokeFido2MakeCredential, invokeFido2GetAssertion, and
    invokeFido2HybridGetAssertion unmocked. As a result,
    Fido2CredentialRequestTest and AuthenticatorImplTest fell through to the
    real Fido2ApiCallHelper implementation, making live Binder IPC calls to
    IFido2PrivilegedService in GMSCore only for MockIntentSender to ignore
    the returned PendingIntent. Because local test APKs are signed with
    debug keys, GMSCore rejected these Binder calls unless the APK signing
    fingerprint was injected into the Fido2ApiKnownBrowsers__fingerprints
    Phenotype flag via a root shell command (applyFidoOverride).
    
    TAG=agy
    CONV=aabdcfff-de8f-43bd-9aec-8370b89df62e
    
    Change-Id: I46241e6ee5dd8bb928a86c0baa2d382f4a8b5a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420414
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701567}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
@@ -143,7 +143,7 @@ public class Fido2CredentialRequestTest {
     private WebauthnTestUtils.MockAuthenticatorRenderFrameHost mFrameHost;
     private MockWebContents mWebContents;
     private WebauthnTestUtils.MockBrowserBridge mMockBrowserBridge;
-    private WebauthnTestUtils.MockFido2ApiCallHelper mFido2ApiCallHelper;
+    private MockFido2ApiCallHelper mFido2ApiCallHelper;
     private AuthenticationContextProvider mAuthenticationContextProvider;
     private Origin mOrigin;
     private Bundle mBrowserOptions;
@@ -247,7 +247,6 @@ public class Fido2CredentialRequestTest {
     public void setUp() throws Exception {
         mPage = mActivityTestRule.startOnBlankPage();
         mContext = ContextUtils.getApplicationContext();
-        WebauthnTestUtils.applyFidoOverride(mContext);
         mIntentSender = new WebauthnTestUtils.MockIntentSender();
         mTestServer = mActivityTestRule.getTestServer();
         mCallback = Fido2ApiTestHelper.getAuthenticatorCallback();
@@ -309,7 +308,7 @@ public class Fido2CredentialRequestTest {
         mRequest = new Fido2CredentialRequest(mAuthenticationContextProvider);
         AuthenticatorImpl.overrideFido2CredentialRequestForTesting(mRequest);
 
-        mFido2ApiCallHelper = new WebauthnTestUtils.MockFido2ApiCallHelper();
+        mFido2ApiCallHelper = new MockFido2ApiCallHelper();
         mFido2ApiCallHelper.setReturnedCredentialDetails(
                 Arrays.asList(Fido2ApiTestHelper.getCredentialDetails()));
         Fido2ApiCallHelper.overrideInstanceForTesting(mFido2ApiCallHelper);
