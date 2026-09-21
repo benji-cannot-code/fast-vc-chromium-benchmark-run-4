e95c451ef19eba90d76a1f5ecee74ec57946ae55
Nihar Majmudar <niharm@google.com>
2026-09-21 12:15:50 -0700

    Allow secondary text wrapping on mobile for prompt suggest.
    For prompt suggest, mobile secondary text needs to wrap to a max of 2
    lines since the descriptions are generally longer. Since prompt suggest
    gets sent via SuggestTemplateInfo, a new field was added for mobile
    in order to know whether a suggestions secondary text/annotation was
    able to wrap to the 2nd line.
    
    Example of bug: https://screenshot-v2.corp.google.com/ABQwVyTHahuEZBf
    
    Bug: 543986835
    Change-Id: Ibda29fd9bef5f6bb7bcc1c2f19963a4726a5231c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8416130
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1701954}

--- ios/chrome/browser/omnibox/model/suggestions/pedal_suggestion_wrapper.mm
+++ ios/chrome/browser/omnibox/model/suggestions/pedal_suggestion_wrapper.mm
@@ -62,6 +62,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return 1;
 }
 
+- (BOOL)wrapSecondaryText {
+  return NO;
+}
+
 - (NSAttributedString*)text {
   return [[NSAttributedString alloc]
       initWithString:self.pedal.title
