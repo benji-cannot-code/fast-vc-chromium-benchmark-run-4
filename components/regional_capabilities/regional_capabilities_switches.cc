8a03c4bfe45ef10a31da9abc65017d8c2f2c7f05
James Lee <ljjlee@google.com>
2026-09-21 07:32:03 -0700

    Enable TaiyakiAllSurfaces by default on iOS
    Bug: 484887280
    Change-Id: I66b28de512cb581d42d8af662bae9eb04cd23dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8434485
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701745}

--- components/regional_capabilities/regional_capabilities_switches.cc
+++ components/regional_capabilities/regional_capabilities_switches.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 #if BUILDFLAG(IS_IOS)
-BASE_FEATURE(kTaiyakiAllSurfaces, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kTaiyakiAllSurfaces, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSearchEngineChoiceScreenSnackbar,
              base::FEATURE_DISABLED_BY_DEFAULT);
