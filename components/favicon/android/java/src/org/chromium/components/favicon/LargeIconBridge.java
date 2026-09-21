14cc23cbafd47cc3c21d3f57145e5df2713880da
Patrick Noland <pnoland@google.com>
2026-09-21 11:19:15 -0700

    Don't cache null icons in LargeIconBridge
    This makes it unlikely that a newly visited site will have an available
    favicon for the lifetime of Chrome, since we ask for and cache the
    unavailability of the icon very early and have no way of invalidating
    this short of evicting from the cache due to the memory limit.
    
    A check to always return false for some common URLs that will never have
    favicons is added to guard against going to native too often for these
    URLs.
    
    Bug: 553135962
    Change-Id: I796ad0c29747fadda7311ed1262b38713b0f8662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394344
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701895}

--- components/favicon/android/java/src/org/chromium/components/favicon/LargeIconBridge.java
+++ components/favicon/android/java/src/org/chromium/components/favicon/LargeIconBridge.java
@@ -8,6 +8,7 @@ package org.chromium.components.favicon;
 import static org.chromium.build.NullUtil.assumeNonNull;
 
 import android.graphics.Bitmap;
+import android.text.TextUtils;
 import android.util.LruCache;
 
 import androidx.annotation.VisibleForTesting;
@@ -19,7 +20,9 @@ import org.jni_zero.NativeMethods;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.util.ConversionUtils;
+import org.chromium.components.embedder_support.util.UrlUtilities;
 import org.chromium.content_public.browser.BrowserContextHandle;
+import org.chromium.content_public.common.ContentUrlConstants;
 import org.chromium.net.NetworkTrafficAnnotationTag;
 import org.chromium.url.GURL;
 
@@ -177,6 +180,11 @@ public class LargeIconBridge {
         assert mNativeLargeIconBridge != 0;
         assert callback != null;
 
+        if ((pageUrl.isEmpty() || !pageUrl.isValid() || isNtpOrAboutBlank(pageUrl))
+                && isDontCacheNullFaviconsEnabled()) {
+            return false;
+        }
+
         if (mFaviconCache == null) {
             return LargeIconBridgeJni.get()
                     .getLargeIconForURL(
@@ -206,10 +214,15 @@ public class LargeIconBridge {
                                 boolean isFallbackColorDefault,
                                 @IconType int iconType) {
                             assumeNonNull(mFaviconCache);
-                            mFaviconCache.put(
-                                    pageUrl,
-                                    new CachedFavicon(
-                                            icon, fallbackColor, isFallbackColorDefault, iconType));
+                            if (icon != null || !isDontCacheNullFaviconsEnabled()) {
+                                mFaviconCache.put(
+                                        pageUrl,
+                                        new CachedFavicon(
+                                                icon,
+                                                fallbackColor,
+                                                isFallbackColorDefault,
+                                                iconType));
+                            }
                             callback.onLargeIconAvailable(
                                     icon, fallbackColor, isFallbackColorDefault, iconType);
                         }
@@ -223,6 +236,13 @@ public class LargeIconBridge {
                             desiredSizePx,
                             callbackWrapper);
         }
+    }
+
+    private boolean isNtpOrAboutBlank(GURL pageUrl) {
+        String urlString = pageUrl.getValidSpecOrEmpty();
+        return UrlUtilities.isNtpUrl(pageUrl)
+                || TextUtils.equals(urlString, ContentUrlConstants.ABOUT_BLANK_DISPLAY_URL)
+                || TextUtils.equals(urlString, ContentUrlConstants.ABOUT_BLANK_URL);
     }
 
     /**
@@ -271,11 +291,18 @@ public class LargeIconBridge {
         mFaviconCache.remove(url);
     }
 
+    /** Returns whether LargeIconBridge should skip caching null favicons. */
+    public static boolean isDontCacheNullFaviconsEnabled() {
+        return LargeIconBridgeJni.get().isDontCacheNullFaviconsEnabled();
+    }
+
     @NativeMethods
     public interface Natives {
         long init();
 
         void destroy(long nativeLargeIconBridge);
+
+        boolean isDontCacheNullFaviconsEnabled();
 
         boolean getLargeIconForURL(
                 long nativeLargeIconBridge,
--- components/favicon/android/large_icon_bridge.h
+++ components/favicon/android/large_icon_bridge.h
@@ -9,11 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 
 #include "base/android/scoped_java_ref.h"
+#include "base/feature_list.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "components/favicon_base/favicon_types.h"
 
 namespace favicon {
+
+BASE_DECLARE_FEATURE(kDontCacheNullFavicons);
 
 // The C++ counterpart to Java's LargeIconBridge. Together these classes expose
 // LargeIconService to Java.
