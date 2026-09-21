2c97b4cfd32151530896328fb2bc72ce25b43835
Andrew Grieve <agrieve@chromium.org>
2026-09-21 12:00:55 -0700

    [Android] Inline A11y text change types API calls
    Inline setTextChangeTypes, isTextSuggestionSelected, and
    setTextFlagEnableTextSuggestionSelected from AconfigFlaggedApiDelegate
    into their callers guarded by SDK_INT >= CINNAMON_BUN (37), as
    android.view.accessibility.ExportedFlags.a11yTextChangeTypesApi()
    unconditionally returns true on SDK 37+.
    
    Bug: 563000637
    Change-Id: I5f611997cc53ff6926e0e1ed04bb7f117f8d959d
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423346
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701942}

--- content/public/android/java/src/org/chromium/content/browser/input/ImeUtils.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeUtils.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser.input;
 
+import android.os.Build;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.Selection;
@@ -16,7 +17,6 @@ import android.view.inputmethod.EditorInfo;
 
 import androidx.core.view.inputmethod.EditorInfoCompat;
 
-import org.chromium.base.AconfigFlaggedApiDelegate;
 import org.chromium.base.FileUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
@@ -61,9 +61,8 @@ public class ImeUtils {
         outAttrs.inputType =
                 EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT;
 
-        AconfigFlaggedApiDelegate delegate = AconfigFlaggedApiDelegate.getInstance();
-        if (delegate != null) {
-            delegate.setTextFlagEnableTextSuggestionSelected(outAttrs);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CINNAMON_BUN) {
+            outAttrs.inputType |= EditorInfo.TYPE_TEXT_FLAG_ENABLE_TEXT_SUGGESTION_SELECTED;
         }
 
         if ((inputFlags & WebTextInputFlags.AUTOCOMPLETE_OFF) != 0) {
