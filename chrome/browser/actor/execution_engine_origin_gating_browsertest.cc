ce0c158616574db5fc5f94cee9727f2013a0df0a
Rainhard Findling <rainhard@chromium.org>
2026-09-21 03:45:46 -0700

    [Gardener] Disable InterleavedAction_OutOfTurnNavigation
    Disable InterleaveAction_OutOfTurnNavigation on Mac, as it's flaking
    on Mac13 Tests.
    
    Bug: 563615151
    Change-Id: Ieeab81a676a4fa67767e93cf848a831ecf3aa160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8426729
    Reviewed-by: Mariam Ali <alimariam@google.com>
    Owners-Override: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701651}

--- chrome/browser/actor/execution_engine_origin_gating_browsertest.cc
+++ chrome/browser/actor/execution_engine_origin_gating_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "base/test/values_test_util.h"
+#include "build/build_config.h"
 #include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/actor/actor_task.h"
 #include "chrome/browser/actor/actor_task_metadata.h"
@@ -2814,7 +2815,8 @@ IN_PROC_BROWSER_TEST_P(OutOfTurnNavigationBrowserTest,
 }
 
 // TODO(crbug.com/482434165): Flaky test on win-asan builder.
-#if BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)
+// TODO(crbug.com/563615151): Flaky test on Mac builders.
+#if (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)) || BUILDFLAG(IS_MAC)
 #define MAYBE_InterleavedAction_OutOfTurnNavigation \
   DISABLED_InterleavedAction_OutOfTurnNavigation
 #else
