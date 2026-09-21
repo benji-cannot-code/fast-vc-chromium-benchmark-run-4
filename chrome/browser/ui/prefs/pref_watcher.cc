a92fe77c93e6d16bad8d87da3d757d8d2dbe0c95
Ireneusz Szulc <ireneuszszulc@google.com>
2026-09-21 03:30:35 -0700

    Autofill: Clean up obsolete kAutofillAtMemoryTriggerInfo preference
    Now that kAutofillAtMemoryShortcut and
    kAutofillAtMemoryDoubleCtrlTriggerEnabled have been introduced and
    wired, the old kAutofillAtMemoryTriggerInfo dictionary preference as
    well as mojo/renderer fields (autofill_trigger_string) are unused.
    
    Fixed: b:556518829
    Change-Id: I55d09ec2548e1367d01a883b1200f44d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8421081
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701643}

--- chrome/browser/ui/prefs/pref_watcher.cc
+++ chrome/browser/ui/prefs/pref_watcher.cc
@@ -161,8 +161,6 @@ PrefWatcher::PrefWatcher(Profile* profile) : profile_(profile) {
   profile_pref_change_registrar_.Add(autofill::prefs::kAutofillAtMemoryShortcut,
                                      renderer_callback);
   profile_pref_change_registrar_.Add(
-      autofill::prefs::kAutofillAtMemoryTriggerInfo, renderer_callback);
-  profile_pref_change_registrar_.Add(
       autofill::prefs::kAutofillAtMemoryDoubleCtrlTriggerEnabled,
       renderer_callback);
 
--- components/sync_preferences/common_syncable_prefs_database.cc
+++ components/sync_preferences/common_syncable_prefs_database.cc
@@ -166,7 +166,7 @@ enum {
   kPrefTranslateRecentTargets = 115,
   kBookmarkBarVisibilityState = 116,
   kSyncableAccountScopedPrefForTesting = 117,  // For tests.
-  kAutofillAtMemoryTriggerInfo = 118,
+  // kAutofillAtMemoryTriggerInfo = 118, (deprecated)
   kAutofillGmailOtpFillingEnabled = 119,
   kAutofillGmailOtpFillingActivationDismissalTimestamp = 120,
   kDriveConsentState = 121,
@@ -549,10 +549,6 @@ constexpr auto kCommonSyncablePrefsAllowlist =
           syncer::PRIORITY_PREFERENCES,
           PrefSensitivity::kExemptFromUserControlWhileSignedIn,
           MergeBehavior::kNone, WriteBehavior::kWriteToBoth}},
-        {autofill::prefs::kAutofillAtMemoryTriggerInfo,
-         {syncable_prefs_ids::kAutofillAtMemoryTriggerInfo, syncer::PREFERENCES,
-          PrefSensitivity::kNone, MergeBehavior::kNone,
-          WriteBehavior::kWriteToBoth}},
         {autofill::prefs::kAutofillGmailOtpFillingEnabled,
          {syncable_prefs_ids::kAutofillGmailOtpFillingEnabled,
           syncer::PREFERENCES, PrefSensitivity::kNone, MergeBehavior::kNone,
