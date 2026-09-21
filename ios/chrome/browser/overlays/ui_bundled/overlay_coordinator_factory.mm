407d4bbd038434876fae1f3f0138233f0a974fea
Yixi Wu <yixiwu@google.com>
2026-09-21 08:16:18 -0700

    [iOS] Create SpinningOverlayCoordinator and request config
    Introduce SpinningOverlayCoordinator, an OverlayRequestCoordinator
    subclass supporting OverlayModality::kWebContentArea.
    
    The SpinningOverlayRequestConfig specify whether the overlay has label
    and if it is cancellable. It displays a spinning overlay with an
    optional message banner over web content, dismissing on tap when
    configured as cancellable.
    
    This should be used the DataControlsTabHelper in the following CL. And
    eventually the SharingCoordinator will refactor the download overlay and
    use this as well.
    
    Bug: 537765041
    Change-Id: I8d0f07edce328a5ae34ef5e906d562b3f027fb0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8379628
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1701770}

--- ios/chrome/browser/overlays/ui_bundled/overlay_coordinator_factory.mm
+++ ios/chrome/browser/overlays/ui_bundled/overlay_coordinator_factory.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/overlays/ui_bundled/infobar_modal/translate/translate_infobar_modal_overlay_coordinator.h"
 #import "ios/chrome/browser/overlays/ui_bundled/overlay_request_coordinator.h"
 #import "ios/chrome/browser/overlays/ui_bundled/web_content_area/alerts/alert_overlay_coordinator.h"
+#import "ios/chrome/browser/overlays/ui_bundled/web_content_area/spinners/spinning_overlay_coordinator.h"
 
 @implementation OverlayRequestCoordinatorFactory {
   raw_ptr<Browser> _browser;
@@ -81,6 +82,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // converted to no longer use enums.
       NOTREACHED() << "Received unsupported modality.";
     case OverlayModality::kWebContentArea:
+      if ([SpinningOverlayCoordinator requestSupport]->IsRequestSupported(
+              request)) {
+        return [SpinningOverlayCoordinator class];
+      }
       return [AlertOverlayCoordinator class];
     case OverlayModality::kWatermark:
       NOTIMPLEMENTED();
---
+++ ios/chrome/browser/overlays/ui_bundled/web_content_area/spinners/spinning_overlay_coordinator.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OVERLAYS_UI_BUNDLED_WEB_CONTENT_AREA_SPINNERS_SPINNING_OVERLAY_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_OVERLAYS_UI_BUNDLED_WEB_CONTENT_AREA_SPINNERS_SPINNING_OVERLAY_COORDINATOR_H_
+
+#import "ios/chrome/browser/overlays/ui_bundled/overlay_request_coordinator.h"
+
+// Coordinator to present a spinning overlay over the web content area.
+@interface SpinningOverlayCoordinator : OverlayRequestCoordinator
+@end
+
+#endif  // IOS_CHROME_BROWSER_OVERLAYS_UI_BUNDLED_WEB_CONTENT_AREA_SPINNERS_SPINNING_OVERLAY_COORDINATOR_H_
