b8193f64b5f8081a44a31d98b7cd61bfd213d919
Liang Zhao <lzhao@microsoft.com>
2026-09-21 13:49:42 -0700

    [Surface Embed] Ignore stale attach requests
    Creating a Surface Embed plugin and attaching its child WebContents
    happen asynchronously. If a popup closes during that interval, its
    GuestContentsHandle is destroyed before the renderer sends
    AttachConnector. The browser previously treated the resulting stale
    content ID as an invariant violation and crashed.
    
    Ignore attachment requests whose GuestContentsHandle no longer exists.
    This treats destruction of the child during asynchronous plugin creation
    as a normal lifetime race while preserving validation of malformed empty
    IDs.
    
    Note that even though in the added ChildDestroyedBeforeEmbedAttachment
    test we assert that the <embed> element still exists, in a real browser
    scenario, the <embed> element will be removed later when tab removed
    notification is processed.
    
    Bug: 558819229
    Test: SurfaceEmbedBrowserTest.ChildDestroyedBeforeEmbedAttachment
    Test: image-upscaling-empty-url.html WPT
    Change-Id: I8adf70d92f969cc22db2e14a706a8a1c5fba95a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8430403
    Reviewed-by: Dave Risney <david.risney@microsoft.com>
    Reviewed-by: Liang Zhao <lzhao@microsoft.com>
    Commit-Queue: Liang Zhao <lzhao@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1702022}

--- components/surface_embed/browser/surface_embed_host.h
+++ components/surface_embed/browser/surface_embed_host.h
@@ -84,6 +84,9 @@ class SurfaceEmbedHost : public mojom::SurfaceEmbedHost,
   bool HasReceivedParentAccessibilityInfoForTesting() const {
     return container_accessibility_node_id_ != ui::kInvalidAXNodeID;
   }
+  bool HasReceivedAttachConnectorForTesting() const {
+    return has_received_attach_connector_for_testing_;
+  }
 
  private:
   friend class SurfaceEmbedHostCollection;
@@ -110,6 +113,7 @@ class SurfaceEmbedHost : public mojom::SurfaceEmbedHost,
   base::WeakPtr<content::WebContents> child_contents_ = nullptr;
 
   bool pending_request_focus_on_embed_element_ = false;
+  bool has_received_attach_connector_for_testing_ = false;
 
   ui::AXNodeID container_accessibility_node_id_ = ui::kInvalidAXNodeID;
 
