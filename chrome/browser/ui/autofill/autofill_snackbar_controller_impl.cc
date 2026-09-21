bfec1a968d6e0996f6e06ed92830427b5e728ba5
Friedrich Horschig <friedrichh@chromium.org>
2026-09-21 06:27:26 -0700

    [Android][Autofill AI] Add undo snackbar strings, enums & JNI
    Part 1/4.
    Add string resources and enum kAutofillAiSuppressionUndo for the
    Autofill AI suppression undo snackbar on Android. Register
    UMA_AUTOFILL_AI_SUPPRESSION_UNDO.
    
    Screenshot: 
    http: //go/strpic/0cfcec406da33bbe6583e3c9a02c087f623e559a
    Video: http://go/scrcast/NDY0MTg2OTI3MDA4OTcyOHwyYTE4MzExNC02Mg
    Feature: AutofillAmbientAutofillSuppressionUI
    Design: go/ambaf-md-android-suppression
    Bug: 556066104
    Change-Id: Ieb9e6b4c102821d5eb94c606cd3de00e31b7126a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8410348
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1701714}

--- chrome/browser/ui/autofill/autofill_snackbar_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_snackbar_controller_impl.cc
@@ -124,6 +124,9 @@ std::u16string AutofillSnackbarControllerImpl::GetMessageText() const {
     case AutofillSnackbarType::kAutofillAiFetchEntityFailure:
       return l10n_util::GetStringUTF16(
           IDS_AUTOFILL_AI_FETCH_ENTITY_FAILURE_NOTIFICATION);
+    case AutofillSnackbarType::kAutofillAiSuppressionUndo:
+      return l10n_util::GetStringUTF16(
+          IDS_AUTOFILL_AI_SUPPRESSION_UNDO_SNACKBAR_MESSAGE);
     case AutofillSnackbarType::kUnspecified:
       NOTREACHED();
   }
@@ -152,6 +155,9 @@ std::u16string AutofillSnackbarControllerImpl::GetActionButtonText() const {
     case AutofillSnackbarType::kAutofillAiFetchEntityFailure:
       return l10n_util::GetStringUTF16(
           IDS_AUTOFILL_AI_SNACK_BAR_CONFIRMATION_BUTTON_LABEL);
+    case AutofillSnackbarType::kAutofillAiSuppressionUndo:
+      return l10n_util::GetStringUTF16(
+          IDS_AUTOFILL_AI_SUPPRESSION_UNDO_SNACKBAR_ACTION);
     case AutofillSnackbarType::kUnspecified:
       NOTREACHED();
   }
@@ -197,6 +203,8 @@ std::string AutofillSnackbarControllerImpl::GetSnackbarTypeForLogging() const {
       return "AutofillAiSaveToWalletFailure";
     case AutofillSnackbarType::kAutofillAiFetchEntityFailure:
       return "AutofillAiFetchFromWalletFailure";
+    case AutofillSnackbarType::kAutofillAiSuppressionUndo:
+      return "AutofillAiSuppressionUndo";
     case AutofillSnackbarType::kUnspecified:
       return "Unspecified";
   }
