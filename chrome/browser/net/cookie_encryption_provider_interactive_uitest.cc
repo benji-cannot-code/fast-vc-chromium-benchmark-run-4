1c0539ccdf0738173716b5a644ea803f0e26f6ec
Ari Chivukula <arichiv@chromium.org>
2026-09-21 09:30:32 -0700

    Remove SecretPortal Session Encryption Test Exemptions
    The encryptor race condition was resolved in crrev.com/c/8428082.
    
    Bug: 562992858, 479420496
    Change-Id: I3803bd2fc190d0e18423eb9a67201c7061cc4da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8432389
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701817}

--- chrome/browser/net/cookie_encryption_provider_interactive_uitest.cc
+++ chrome/browser/net/cookie_encryption_provider_interactive_uitest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
-#include "components/sessions/core/command_storage_features.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/test_launcher.h"
@@ -187,8 +186,6 @@ class CookieEncryptionProviderBrowserTest
 #endif  // BUILDFLAG(IS_WIN)
 #if BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
         disabled_features.push_back(features::kDbusSecretPortal);
-        // TODO(crbug.com/479420496): Resolve session encryptor issues.
-        disabled_features.push_back(sessions::kEncryptSessionStorage);
 #endif  // BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
         break;
 #if BUILDFLAG(IS_WIN)
@@ -222,8 +219,6 @@ class CookieEncryptionProviderBrowserTest
         enabled_features.push_back(features::kDbusSecretPortal);
         enabled_features.push_back(
             features::kSecretPortalKeyProviderUseForEncryption);
-        // TODO(crbug.com/479420496): Resolve session encryptor issues.
-        disabled_features.push_back(sessions::kEncryptSessionStorage);
         break;
 #endif  // BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
     }
