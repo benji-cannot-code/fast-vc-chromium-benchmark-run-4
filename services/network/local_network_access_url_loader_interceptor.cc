03efe30269964cfbe5af4036e3a28cec77fa15a6
Hubert Chao <hchao@chromium.org>
2026-09-21 14:45:28 -0700

    [LNA] add additional netlogging for IP address space override info
    Bug: 540020538
    Change-Id: Ibdbe7825c9e316ebdba5fbdb2eab5603e7f2babd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8407793
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702067}

--- services/network/local_network_access_url_loader_interceptor.cc
+++ services/network/local_network_access_url_loader_interceptor.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/local_network_access_url_loader_interceptor.h"
 
+#include "base/strings/string_util.h"
 #include "net/base/transport_info.h"
 #include "net/log/net_log_event_type.h"
 #include "net/log/net_log_with_source.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/local_network_access_check_result.h"
 #include "services/network/public/mojom/devtools_observer.mojom.h"
 #include "services/network/public/mojom/url_loader_network_service_observer.mojom.h"
+#include "url/gurl.h"
 
 namespace network {
 
@@ -194,6 +196,11 @@ LocalNetworkAccessCheckResult LocalNetworkAccessUrlLoaderInterceptor::DoCheck(
 
   net_log.AddEvent(net::NetLogEventType::LOCAL_NETWORK_ACCESS_CHECK, [&] {
     return base::DictValue()
+        .Set("transport_info", transport_info.ToString())
+        .Set("address_space_overrides",
+             base::JoinString(
+                 IPAddressSpaceOverrides::GetInstance().GetCurrentOverrides(),
+                 ","))
         .Set("client_address_space",
              IPAddressSpaceToStringPiece(client_address_space))
         .Set("resource_address_space",
