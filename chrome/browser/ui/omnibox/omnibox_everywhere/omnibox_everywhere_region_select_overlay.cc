4d8b2d563de2f91c15dfb619f3e5e35d3ff070a1
Kaan Alsan <alsan@chromium.org>
2026-09-21 13:36:09 -0700

    [OmniboxEverywhere] Fix region selection overlay child mirroring in RTL
    TAG=agy
    CONV=a03d0617-2bd1-488a-b553-3c97e31d1386
    
    Bug: 563454003
    Change-Id: I70fee43ee10ff7d8d983e8abfc0a53a47a18c145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8432984
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: Juliet Knighton <julietlevesque@google.com>
    Commit-Queue: Juliet Knighton <julietlevesque@google.com>
    Auto-Submit: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702012}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_region_select_overlay.cc
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_region_select_overlay.cc
@@ -347,6 +347,7 @@ class TeardropCursorChipView : public views::View {
   static constexpr int kTotalSize = kChipSize + (kPadding * 2);
 
   TeardropCursorChipView() {
+    SetMirrored(false);
     SetCanProcessEventsWithinSubtree(false);
     GetViewAccessibility().SetIsIgnored(true);
 
@@ -427,6 +428,7 @@ class RegionSelectOverlayView : public views::View {
         bitmap_(screenshot),
         image_(!bitmap_.empty() ? gfx::ImageSkia::CreateFromBitmap(bitmap_, 1.f)
                                 : gfx::ImageSkia()) {
+    SetMirrored(false);
     SetFocusBehavior(FocusBehavior::ALWAYS);
     GetViewAccessibility().SetRole(ax::mojom::Role::kImage);
     GetViewAccessibility().SetName(l10n_util::GetStringUTF16(
@@ -505,6 +507,9 @@ class RegionSelectOverlayView : public views::View {
 
   void AddedToWidget() override {
     views::View::AddedToWidget();
+    if (views::Widget* widget = GetWidget(); widget && widget->GetRootView()) {
+      widget->GetRootView()->SetMirrored(false);
+    }
     UpdateToastPosition();
   }
 
