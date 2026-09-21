395c9d229ad32f357c9539f57cbd33c2218be54c
Andrew Grieve <agrieve@chromium.org>
2026-09-21 12:22:17 -0700

    [Android] Inline WebAppManager API calls on CINNAMON_BUN
    The WebAppManager API (isWebAppServiceEnabled, installTwa, isInstalled)
    is part of the public Android 37 (CINNAMON_BUN) SDK, and its aconfig
    flag returns true on SDK >= 37.
    
    Inline these methods directly into WebappsUtils and TwaInstaller with
    an SDK_INT >= CINNAMON_BUN check, and remove the
    AconfigFlaggedApiDelegate indirection and //base:flagged_apis_java
    dependency.
    
    Bug: 563000637
    Change-Id: I8b34f6f055a49f5af952f0343cdda68b744cf03c
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8427841
    Reviewed-by: Richard Coles <torne@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701958}

--- components/webapps/browser/android/java/src/org/chromium/components/webapps/TwaInstaller.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/TwaInstaller.java
@@ -5,13 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.webapps;
 
+import android.content.Context;
+import android.content.pm.webapp.WebAppInstallRequest;
+import android.content.pm.webapp.WebAppManager;
+import android.content.pm.webapp.WebAppQueryRequest;
+import android.os.Build;
+
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
-import org.chromium.base.AconfigFlaggedApiDelegate;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.Promise;
+import org.chromium.base.task.PostTask;
 import org.chromium.build.annotations.NullMarked;
 
 /**
@@ -44,16 +52,9 @@ class TwaInstaller {
             return false;
         }
 
-        var aconfigFlaggedApiDelegate = AconfigFlaggedApiDelegate.getInstance();
-        if (aconfigFlaggedApiDelegate == null) {
-            Log.e(TAG, "Failed to get AconfigFlaggedApiDelegate to call installTwa()");
-            onFlowCompleted(AddToHomescreenEvent.INSTALL_FAILED);
-            return false;
-        }
-
         // TODO(crbug.com/468477882): Consider if we should also report INSTALL_STARTED and UI_SHOWN
         // events at some appropriate timing.
-        if (!aconfigFlaggedApiDelegate.installTwa(
+        if (!installTwa(
                 title,
                 manifestUrl,
                 /* installSucceededCallback= */ () -> {
@@ -66,10 +67,67 @@ class TwaInstaller {
                     onFlowCompleted(AddToHomescreenEvent.UI_CANCELLED);
                 })) {
             Log.e(TAG, "Failed to call installTwa()");
+            onFlowCompleted(AddToHomescreenEvent.INSTALL_FAILED);
             return false;
         }
 
         return true;
+    }
+
+    private static boolean installTwa(
+            String title,
+            String manifestUrl,
+            Runnable installSucceededCallback,
+            Runnable installFailedCallback,
+            Runnable installCancelledCallback) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.CINNAMON_BUN) {
+            return false;
+        }
+        Context context = ContextUtils.getApplicationContext();
+        WebAppManager webAppManager = context.getSystemService(WebAppManager.class);
+        if (webAppManager == null) {
+            Log.e(TAG, "Failed to get WebAppManager");
+            return false;
+        }
+
+        var request = new WebAppInstallRequest.Builder(title, manifestUrl).build();
+        webAppManager.install(
+                request,
+                PostTask.getUiUserVisibleExecutor(),
+                (packageName, resultCode) -> {
+                    switch (resultCode) {
+                        case WebAppInstallRequest.RESULT_SUCCESS:
+                            installSucceededCallback.run();
+                            break;
+                        case WebAppInstallRequest.RESULT_CANCELLED_BY_USER:
+                            installCancelledCallback.run();
+                            break;
+                        default:
+                            installFailedCallback.run();
+                            break;
+                    }
+                });
+        return true;
+    }
+
+    static Promise<Boolean> isInstalled(String title) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.CINNAMON_BUN) {
+            return Promise.fulfilled(false);
+        }
+        Context context = ContextUtils.getApplicationContext();
+        WebAppManager webAppManager = context.getSystemService(WebAppManager.class);
+        if (webAppManager == null) {
+            return Promise.fulfilled(false);
+        }
+        Promise<Boolean> promise = new Promise<>();
+        var request = new WebAppQueryRequest(title);
+        webAppManager.query(
+                request,
+                PostTask.getUiUserVisibleExecutor(),
+                (resultCode) -> {
+                    promise.fulfill(resultCode == WebAppQueryRequest.RESULT_INSTALLED);
+                });
+        return promise;
     }
 
     private void onFlowCompleted(@AddToHomescreenEvent int event) {
--- components/webapps/browser/android/java/src/org/chromium/components/webapps/WebappsUtils.java
+++ components/webapps/browser/android/java/src/org/chromium/components/webapps/WebappsUtils.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
+import android.content.pm.webapp.WebAppManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Icon;
 import android.os.Build;
@@ -21,7 +22,6 @@ import androidx.annotation.WorkerThread;
 
 import org.jni_zero.CalledByNative;
 
-import org.chromium.base.AconfigFlaggedApiDelegate;
 import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
@@ -274,12 +274,12 @@ public class WebappsUtils {
 
     @CalledByNative
     private static boolean isWebAppServiceEnabled() {
-        var aconfigFlaggedApiDelegate = AconfigFlaggedApiDelegate.getInstance();
-        if (aconfigFlaggedApiDelegate == null) {
-            Log.e(TAG, "Failed to get AconfigFlaggedApiDelegate in isWebAppServiceEnabled()");
-            return false;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CINNAMON_BUN) {
+            Context context = ContextUtils.getApplicationContext();
+            WebAppManager webAppManager = context.getSystemService(WebAppManager.class);
+            return webAppManager != null;
         }
-        return aconfigFlaggedApiDelegate.isWebAppServiceEnabled();
+        return false;
     }
 
     public static void isTwaInstallerPackage(String title, Callback<Boolean> callback) {
@@ -287,14 +287,7 @@ public class WebappsUtils {
             callback.onResult(sIsTwaInstallerPackage == TriState.TRUE);
             return;
         }
-        var aconfigFlaggedApiDelegate = AconfigFlaggedApiDelegate.getInstance();
-        if (aconfigFlaggedApiDelegate == null) {
-            Log.e(TAG, "Failed to get AconfigFlaggedApiDelegate in isWebAppServiceEnabled()");
-            callback.onResult(false);
-            return;
-        }
-
-        aconfigFlaggedApiDelegate.isInstalled(title).then(callback);
+        TwaInstaller.isInstalled(title).then(callback);
     }
 
     /**
