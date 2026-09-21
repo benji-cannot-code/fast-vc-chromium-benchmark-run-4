5c19dc6f71c50e4d61cdc01bf9c8db835a5aaf02
Huan <huanwu@google.com>
2026-09-21 14:14:01 -0700

    [Glic] Show Android experimental opt-in on its own tab
    screencast: https://drive.google.com/file/d/1DHMGxkJ1Fqc-dnhqIkKlJmRBfVEPgeJa/view?usp=sharing
    
    Previously the Android experimental opt-in dialog was shown on top of
    whatever tab happened to be active. Instead, host the dialog on a
    dedicated new tab.
    
    Bug: 559232460
    Change-Id: Ia1345f42fe2335a3c9b4aea4e7d1651925bce7d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420759
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Huan Wu <huanwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702047}

--- chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_ui_host_android.h
+++ chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_ui_host_android.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_ui_host.h"
+#include "components/tabs/public/tab_interface.h"
 
 class Profile;
 
@@ -45,6 +46,10 @@ class GlicExperimentalOptInUIHostAndroid : public GlicExperimentalOptInUIHost {
   raw_ptr<Delegate> delegate_;
   std::unique_ptr<content::WebContents> opt_in_web_contents_;
   base::android::ScopedJavaGlobalRef<jobject> java_dialog_;
+  // The tab created by GetOrCreateSuitableWebContents() for the currently
+  // showing dialog, if any. Held as a handle rather than a pointer so it
+  // resolves to null if the tab goes away underneath us.
+  tabs::TabHandle dialog_tab_;
   bool is_accepted_ = false;
   base::WeakPtrFactory<GlicExperimentalOptInUIHostAndroid> weak_ptr_factory_{
       this};
