05cc4e0a7cd5a16b8c277ee2f32b4893fe811337
Fiaz Muhammad <mfiaz@google.com>
2026-09-21 15:21:16 -0700

    [🌷] Remove stale @RequiresRestart in ContextMenuTest
    Cross-test batch failures in ContextMenuTest were caused by an unremoved TabModelSelectorObserver that has since been fixed in tearDown(). Remove the redundant @RequiresRestart annotation on testLongPressOnImage so it runs cleanly within the shared class batch.
    
    Detailed breakdown: http://go/paste/4835610490175488
    
    Bug: 563356750
    Change-Id: I1005ab16a2ee7a2f64c3baffbbf4cdcee5218cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8432397
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702107}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -323,7 +323,6 @@ public class ContextMenuTest {
     @Test
     @MediumTest
     @Feature({"Browser"})
-    @RequiresRestart
     public void testLongPressOnImage() throws TimeoutException {
         doAnswer(sCopyIsAllowedByPolicy)
                 .when(mDataProtectionBridgeMock)
