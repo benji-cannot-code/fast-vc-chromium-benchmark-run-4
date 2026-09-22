b6703ff899c12106f917a42adb54014b43557248
William Liu <liuwilliam@chromium.org>
2026-09-21 17:03:20 -0700

    [selection] Fix a reentrance DCHECK caused by closing a widget
    See the inline comments for the explanation.
    
    Tested: https://chromium-swarm.appspot.com/task?id=7aca014a436f9910 50 iterations on the bot
    
    R=dtapuska@chromium.org
    R=wusirui@google.com
    
    Fixed: b:562498387
    Change-Id: Id25a0376569df6038ff20162c8ad7089547719f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8427259
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702227}

--- chrome/browser/glic/selection/selection_overlay_controller.cc
+++ chrome/browser/glic/selection/selection_overlay_controller.cc
@@ -248,7 +248,6 @@ void SelectionOverlayController::TabDeactivated(tabs::TabInterface* tab) {
   if (state() == State::kBackground) {
     return;
   }
-  ClosePreselectionBubbleImpl();
   TabWillEnterBackground(tab);
 }
 
@@ -597,7 +596,13 @@ void SelectionOverlayController::NotifyPageNavigated() {
 
 void SelectionOverlayController::NotifyTabForegrounded() {}
 
-void SelectionOverlayController::NotifyTabWillEnterBackground() {}
+void SelectionOverlayController::NotifyTabWillEnterBackground() {
+  // Closing the preselection bubble hides its widget synchronously, which can
+  // hand activation back to the browser window and re-enter `TabDeactivated()`.
+  // Closing the bubble at the end of the callstack so the reentrance to
+  // `TabDeactivated()` with kBackground is an early-out.
+  ClosePreselectionBubbleImpl();
+}
 
 OverlayBaseController::PreselectionUIConfig
 SelectionOverlayController::GetPreselectionBubbleConfig() {
