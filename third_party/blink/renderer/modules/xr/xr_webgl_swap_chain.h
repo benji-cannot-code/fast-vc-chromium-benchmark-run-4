2409e7a4e03a58f770ae35ffc6a7eefa4a52cf83
Yong Li (xWF) <yyonglii@google.com>
2026-09-22 13:02:38 -0700

    Revert "WebXR: Support texture-array in Blink and Mojo"
    This reverts commit 110e26df547e63e1733c55ecc37507d39623bae7.
    
    Reason for revert: breaking a test
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6457455610429440
    
    Original change's description:
    > WebXR: Support texture-array in Blink and Mojo
    >
    > Add XRTextureType enum to vr_service.mojom and plumb texture_type
    > through Blink WebXR composition layers and WebGL bindings. Support
    > texture-array shared images in XRWebGLSharedImageSwapChain.
    >
    > Bug: 462131288
    > Change-Id: I0a1e94ed3c42e0d8642bcd830cefc44a936d8776
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8399727
    > Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    > Commit-Queue: Yong Li (xWF) <yyonglii@google.com>
    > Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1702527}
    
    Bug: 462131288
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia1c40c294db8277421c302e7af36e597800cbfca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8447231
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Yong Li (xWF) <yyonglii@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702894}

--- third_party/blink/renderer/modules/xr/xr_webgl_swap_chain.h
+++ third_party/blink/renderer/modules/xr/xr_webgl_swap_chain.h
@@ -79,9 +79,6 @@ class XRWebGLSwapChain : public XRSwapChain<WebGLUnownedTexture> {
   virtual std::unique_ptr<SharedImageHolder> TransferToSharedImageHolder() {
     return nullptr;
   }
-  virtual std::unique_ptr<SharedImageHolder> DoneWithSharedBuffer() {
-    return nullptr;
-  }
   virtual bool IsCube() const { return false; }
   virtual gpu::SyncToken GetSyncToken() const { return gpu::SyncToken(); }
 
@@ -129,9 +126,6 @@ class XRWebGLSharedImageSwapChain final : public XRWebGLSwapChain {
   WebGLUnownedTexture* ProduceTexture() override;
 
   void OnFrameEnd() override;
-
-  std::unique_ptr<SharedImageHolder> TransferToSharedImageHolder() override;
-  std::unique_ptr<SharedImageHolder> DoneWithSharedBuffer() override;
 
   gpu::SyncToken GetSyncToken() const override { return sync_token_; }
 
