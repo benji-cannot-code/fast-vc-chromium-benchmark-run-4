40a6ee6973b0bfb840ec68a17b7cdf079c0baede
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-09-22 15:07:03 -0700

    Introduce GpuFenceHandle functions in GLFence
    Most of the places just want handles, packing/unpacking GpuFence isn't
    useful.
    
    This CL adds GpuFenceHandle functionality and converts straight-forward
    places.
    
    Change-Id: Id472abd45f1a00dd0d04a4ca1c3955e8a1ed9ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8429210
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702984}

--- ui/gl/gl_fence.h
+++ ui/gl/gl_fence.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ui/gfx/gpu_fence.h"
+#include "ui/gfx/gpu_fence_handle.h"
 #include "ui/gl/gl_export.h"
 
 namespace gl {
@@ -29,6 +30,11 @@ class GL_EXPORT GLFence {
   static bool IsGpuFenceSupported();
 
   // Consumes the GpuFenceHandle to create a paired local GL fence.
+  static std::unique_ptr<GLFence> CreateFromGpuFenceHandle(
+      gfx::GpuFenceHandle gpu_fence);
+
+  // Clones the GpuFenceHandle from `gpu_fence` to create a paired local GL
+  // fence.
   static std::unique_ptr<GLFence> CreateFromGpuFence(
       const gfx::GpuFence& gpu_fence);
 
@@ -51,9 +57,11 @@ class GL_EXPORT GLFence {
   // Loses the reference to the fence. Useful if the context is lost.
   virtual void Invalidate();
 
-  // Returns a GpuFence. Only valid on a GLFence created by
+  // Returns a GpuFenceHandle. Only valid on a GLFence created by
   // CreateForGpuFence.
-  virtual std::unique_ptr<gfx::GpuFence> GetGpuFence();
+  virtual gfx::GpuFenceHandle GetGpuFenceHandle();
+
+  std::unique_ptr<gfx::GpuFence> GetGpuFence();
 };
 
 }  // namespace gl
