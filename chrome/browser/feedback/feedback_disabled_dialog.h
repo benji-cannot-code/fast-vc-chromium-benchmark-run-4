e977962287d588a0403bfb69f425dc70fcd0be22
Jérôme <jlebel@chromium.org>
2026-09-22 10:38:01 -0700

    Implement ShowFeedbackDisabledDialog for Desktop
    Implements the modal dialog shown on Desktop (Windows, Mac, Linux) when
    user feedback cannot be submitted for the current account.
    
    Bug: 542047102
    Change-Id: Icbe6dc9b4e679cd4078a3e1837843366601d689a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8367047
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702763}

---
+++ chrome/browser/feedback/feedback_disabled_dialog.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FEEDBACK_FEEDBACK_DISABLED_DIALOG_H_
+#define CHROME_BROWSER_FEEDBACK_FEEDBACK_DISABLED_DIALOG_H_
+
+#include "ui/gfx/native_ui_types.h"
+
+class Profile;
+
+namespace chrome {
+
+// Shows a browser-modal dialog explaining that feedback cannot be submitted
+// for `profile`. `parent` and `profile` must not be null.
+void ShowFeedbackDisabledDialog(gfx::NativeWindow parent,
+                                const Profile* profile);
+
+}  // namespace chrome
+
+#endif  // CHROME_BROWSER_FEEDBACK_FEEDBACK_DISABLED_DIALOG_H_
