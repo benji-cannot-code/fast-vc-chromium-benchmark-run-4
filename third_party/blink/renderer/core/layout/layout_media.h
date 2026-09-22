41734d1b564c5d3e5d2cc4974d9b2ca8c716ccd7
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-09-22 08:55:57 -0700

    [layout] Remove LayoutObject::RecalcScrollableOverflow.
    All scrollable-overflow calculations now to through the
    ScrollableOverflowCalculator.
    
    Have RecalcScrollableOverflow on LayoutObject was a relic from a
    pre-LayoutNG engine.
    
    Remove, and simplify all the scrollable-overflow recalc methods on
    LayoutBox.
    
    Additionally there are some small fixes ensuring the
    ScrollableOverflowCalculator never gets called with a LayoutReplaced.
    
    There should be no behaviour change.
    
    Change-Id: Idb25e47eaeec9c7cef1f6a55cb559680d5d32fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8436835
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702672}

--- third_party/blink/renderer/core/layout/layout_media.h
+++ third_party/blink/renderer/core/layout/layout_media.h
@@ -106,8 +106,6 @@ class LayoutMedia : public LayoutImage {
     return false;
   }
 
-  RecalcScrollableOverflowResult RecalcScrollableOverflow() override;
-
   LayoutObjectChildList children_;
 };
 
