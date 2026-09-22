5ae391394a35763b91bcb1692415bd52bd27ef7e
Stanisław Solarewicz <ssolarewicz@chromium.org>
2026-09-22 02:13:26 -0700

    [AESD] Remove a TODO about checking all forms of management
    Eventually it was established that the disclaimer should be only shown
    for the managed profiles, thus this TODO is not up to date anymore.
    
    Bug: 527872237
    Change-Id: I5002a51c1b22e3d4dc3157341d6582da5b20d0f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8438717
    Reviewed-by: Janek Zarzycki <janzarzycki@google.com>
    Commit-Queue: Stanisław Solarewicz <ssolarewicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702443}

--- chrome/browser/ui/android/enterprise_signals_disclaimer/java/src/org/chromium/chrome/browser/ui/enterprise_signals_disclaimer/EnterpriseSignalsDisclaimerController.java
+++ chrome/browser/ui/android/enterprise_signals_disclaimer/java/src/org/chromium/chrome/browser/ui/enterprise_signals_disclaimer/EnterpriseSignalsDisclaimerController.java
@@ -168,7 +168,6 @@ public class EnterpriseSignalsDisclaimerController implements SigninManager.Sign
             return false;
         }
 
-        // TODO(b/512836948): Expand this check to include all forms of management.
         if (!ManagedBrowserUtils.isProfileManaged(mProfile)) {
             return false;
         }
