718d1d9e544387abfeb6e2bf823d0d2b9c0ec4ef
Jakub Lewandowski <lkuba@google.com>
2026-09-22 00:14:08 -0700

    [Autofill] Handle physical keyboard input in the keyboard accessory
    Implements HandleKeyPressEvent() in the keyboard accessory controller,
    bringing physical keyboard handling on par with the Desktop popup.
    Arrow Up/Down toggle the suggestion navigation mode, in which arrow
    Left/Right move the selection instead of the text caret.
    
    Bug: 542535472
    Change-Id: I3499852e8a59a3a3b99266916fc8630780531cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8424804
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Kuba Lewandowski <lkuba@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702399}

--- chrome/browser/ui/autofill/autofill_keyboard_accessory_controller_impl_test_api.h
+++ chrome/browser/ui/autofill/autofill_keyboard_accessory_controller_impl_test_api.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_keyboard_accessory_view.h"
 #include "chrome/browser/ui/autofill/autofill_suggestion_controller.h"
 #include "chrome/browser/ui/autofill/next_idle_barrier.h"
+#include "components/input/native_web_keyboard_event.h"
 
 namespace autofill {
 
@@ -38,6 +39,14 @@ class AutofillKeyboardAccessoryControllerImplTestApi {
 
   AutofillKeyboardAccessoryView* view() const {
     return controller_->view_.get();
+  }
+
+  bool HandleKeyPressEvent(const input::NativeWebKeyboardEvent& event) {
+    return controller_->HandleKeyPressEvent(event);
+  }
+
+  std::optional<int> selected_suggestion_index() const {
+    return controller_->selected_suggestion_index_;
   }
 
  private:
