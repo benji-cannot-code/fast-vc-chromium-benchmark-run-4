4fd35f9e4f548b1cff9155453f8d7330b9e77f5e
Alison Gale <agale@chromium.org>
2026-09-22 13:12:00 -0700

    [Organizer] Hook up keyboard shortcut to organizer panel
    The shortcut toggles the panel in and out, similar to the existing tab
    search bubble
    
    Bug: 564600713
    Change-Id: I284bfd2d10d32e6ce79eaf49442d1153302e05f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8444398
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702907}

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -110,6 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/back_to_opener/back_to_opener_controller.h"
 #include "chrome/browser/ui/tabs/features.h"
 #include "chrome/browser/ui/tabs/new_tab_grouping_user_data.h"
+#include "chrome/browser/ui/tabs/organizer/organizer_panel_controller.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
 #include "chrome/browser/ui/tabs/split_tab_metrics.h"
 #include "chrome/browser/ui/tabs/split_tab_util.h"
@@ -122,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tabs/organizer/organizer_panel_utils.h"
 #include "chrome/browser/ui/waap/initial_webui_window_metrics_manager.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/ui/web_applications/web_app_launch_utils.h"
@@ -2530,6 +2532,12 @@ void FindInPage(BrowserWindowInterface* browser,
 }
 
 void ShowTabSearch(BrowserWindowInterface* browser) {
+  if (organizer_panel::IsOrganizerPanelFeatureEnabled()) {
+    auto* controller = OrganizerPanelController::From(browser);
+    CHECK(controller);
+    controller->SetOrganizerVisible(!controller->IsOrganizerPanelVisible());
+    return;
+  }
   BrowserWindow::FromBrowser(browser)->CreateTabSearchBubble();
 }
 
