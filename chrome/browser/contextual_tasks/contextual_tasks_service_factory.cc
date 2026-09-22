8619c5b2f11495f734546f50c641f2a1db417360
Justin Mew <jmew@google.com>
2026-09-21 17:45:51 -0700

    Remove spurious DependsOn(GlicKeyedServiceFactory) from ContextualTasksServiceFactory
    ContextualTasksServiceFactory does not use GlicKeyedService; it only
    calls glic::GlicEnabling::IsEnabledForProfile(profile). Declaring
    DependsOn(GlicKeyedServiceFactory) introduces an unnecessary dependency
    edge that causes a static dependency cycle with
    PageContentAnnotationsServiceFactory and SyncServiceFactory.
    
    Bug: 556442614
    Change-Id: I021756d234b55b5d1102d0f133292d2e0acb677e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8421384
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Justin Mew <jmew@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702258}

--- chrome/browser/contextual_tasks/contextual_tasks_service_factory.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_service_factory.cc
@@ -11,11 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "build/buildflag.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
-#include "chrome/browser/glic/public/glic_keyed_service.h"
-#include "chrome/browser/glic/public/glic_keyed_service_factory.h"
+#include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/subscription_eligibility/subscription_eligibility_service_factory.h"
 #include "chrome/browser/sync/data_type_store_service_factory.h"
 #include "chrome/common/channel_info.h"
 #include "chrome/common/webui_url_constants.h"
@@ -105,7 +105,11 @@ ContextualTasksServiceFactory::ContextualTasksServiceFactory()
               .Build()) {
   DependsOn(DataTypeStoreServiceFactory::GetInstance());
   DependsOn(FaviconServiceFactory::GetInstance());
-  DependsOn(glic::GlicKeyedServiceFactory::GetInstance());
+  // Add a dependency on SubscriptionEligibilityServiceFactory since
+  // GlicEnabling::IsEnabledForProfile() can potentially call
+  // SubscriptionEligibilityServiceFactory::GetForProfile.
+  DependsOn(subscription_eligibility::SubscriptionEligibilityServiceFactory::
+            GetInstance());
   DependsOn(HistoryServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
 }
