28771f2aa6d7bad279721df750fff25ea20a89de
Javier Contreras Tenorio <javiercon@microsoft.com>
2026-09-22 14:09:49 -0700

    [gap-decorations] Don't copy vectors unnecessarily
    In multiple places of the gap decorations code we were making
    unnecessary copies to entire vectors when simple references sufficed.
    
    This CL fixes that.
    
    Change-Id: I337e04b43c4b119939b01670972420ac8513a7a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8444939
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1702952}

--- third_party/blink/renderer/core/layout/physical_box_fragment.cc
+++ third_party/blink/renderer/core/layout/physical_box_fragment.cc
@@ -118,7 +118,7 @@ void ApplyOverflowClip(OverflowClipAxes overflow_clip_axes,
 }
 
 int MaxGapDecorationsWidth(const GapDataList<int>& width_value) {
-  const auto widths = width_value.GetGapDataList();
+  const auto& widths = width_value.GetGapDataList();
   CHECK(!widths.empty());
 
   const auto& first_width = widths[0];
