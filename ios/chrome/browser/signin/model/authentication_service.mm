bd41e5c45be5241c5d094a72fc4569cb3fc80b2f
Marc Treib <treib@chromium.org>
2026-09-22 04:27:58 -0700

    Add AuthenticationService::SignOut() overload without completion param
    AuthenticationService::SignOut() always completes synchronously, so the
    completion callback is not needed. This CL introduces an overload
    without the completion callback as the first step to deprecating and
    removing the callback parameter.
    
    TAG=agy
    CONV=03737e6d-c3c8-447f-8827-468bad8c03d1
    
    Change-Id: I9b440b91468bc501a8103856f37c6969b7dab397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8440256
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702490}

--- ios/chrome/browser/signin/model/authentication_service.mm
+++ ios/chrome/browser/signin/model/authentication_service.mm
@@ -453,6 +453,11 @@ void AuthenticationService::SignOut(
   }
 }
 
+void AuthenticationService::SignOut(
+    signin_metrics::ProfileSignout signout_source) {
+  SignOut(signout_source, nil);
+}
+
 AuthenticationService::ProfileInitializationOutcome
 AuthenticationService::PerformProfileInitializationIfNecessary() {
   ProfileManagerIOS* profile_manager =
