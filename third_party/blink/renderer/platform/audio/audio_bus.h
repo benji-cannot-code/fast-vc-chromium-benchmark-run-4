019fdb144247b95a137b4a87bfed9025e30a2300
Jordan Bayles <jophba@chromium.org>
2026-09-21 23:23:48 -0700

    Revert "Remove class WebAudioBus"
    This reverts commit 768c89d2c0f1a484a2c4d874512c4f260925a9c6 and commit 9e6d178b3492c058a2c76fcf784d627cee6283a3.
    
    Reason for revert:
    Causes a high-volume P0/S2 crash spike on 32-bit Android (b/562782946).
    Switching from WebAudioBus to media::AudioBus changed memory allocation
    in DecodeAudioFileData from Blink's fallible allocator
    (Partitions::BufferTryAlignedZeroedMalloc) to media::AudioBus's
    infallible base::AlignedAlloc(), leading PartitionAlloc to terminate the
    renderer on OOM instead of safely returning nullptr / rejecting the
    decode promise.
    
    Reverting commit 768c89d2c0f1a484a2c4d874512c4f260925a9c6 is also
    included as it was a follow-up that removed ResizeSmaller/MaxAbsValue
    needed by WebAudioBus.
    
    Original change's description:
    > Remove class WebAudioBus
    >
    > This should cause no functional change.
    >
    > The functionality of this class can be replaced directly with AudioBus,
    > so remove it to ease maintainability and reduce complexity.
    >
    > Change-Id: Ib2c8db1ed65adee4abb1d75da49d58f6ce03bfaa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8308032
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    > Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1696056}
    
    Bug: 562782946
    Change-Id: Iedadeb6f95ef64cc5fc18d984a90df5655bda971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8433454
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702385}

--- third_party/blink/renderer/platform/audio/audio_bus.h
+++ third_party/blink/renderer/platform/audio/audio_bus.h
@@ -36,10 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
-namespace media {
-class AudioBus;
-}
-
 namespace blink {
 
 // An AudioBus represents a collection of one or more AudioChannels.
@@ -73,12 +69,6 @@ class PLATFORM_EXPORT AudioBus final : public ThreadSafeRefCounted<AudioBus> {
   static scoped_refptr<AudioBus> TryCreate(unsigned number_of_channels,
                                            uint32_t length);
 
-  // Creates an AudioBus that takes ownership of an existing media::AudioBus.
-  // The channels in AudioBus wrap the channel memory of `media_bus`.
-  static scoped_refptr<AudioBus> CreateFromMediaAudioBus(
-      std::unique_ptr<media::AudioBus> media_bus,
-      float sample_rate = 0.0f);
-
   // Pass in 0.0 for sampleRate to use the file's sample-rate, otherwise a
   // sample-rate conversion to the requested sampleRate will be made (if it
   // doesn't already match the file's sample-rate).  The created buffer will
@@ -90,7 +80,6 @@ class PLATFORM_EXPORT AudioBus final : public ThreadSafeRefCounted<AudioBus> {
 
   AudioBus(const AudioBus&) = delete;
   AudioBus& operator=(const AudioBus&) = delete;
-  ~AudioBus();
 
   // Tells the given channel to use externally allocated storage. The channel
   // length is `storage.size()`. Pass an empty span to clear the channel's
@@ -111,6 +100,10 @@ class PLATFORM_EXPORT AudioBus final : public ThreadSafeRefCounted<AudioBus> {
 
   // Number of sample-frames
   uint32_t length() const { return length_; }
+
+  // resizeSmaller() can only be called with a new length <= the current length.
+  // The data stored in the bus will remain undisturbed.
+  void ResizeSmaller(uint32_t new_length);
 
   // Sample-rate : 0.0 if unknown or "don't care"
   float SampleRate() const { return sample_rate_; }
@@ -179,12 +172,18 @@ class PLATFORM_EXPORT AudioBus final : public ThreadSafeRefCounted<AudioBus> {
       const AudioBus& source_bus,
       base::span<const float> gain_values);
 
+  // Returns maximum absolute value across all channels (useful for
+  // normalization).
+  float MaxAbsValue() const;
+
+  // Makes maximum absolute value == 1.0 (if possible).
+  void Normalize();
+
   static scoped_refptr<AudioBus> GetDataResource(int resource_id,
                                                  float sample_rate);
 
  private:
   AudioBus(unsigned number_of_channels, uint32_t length, bool allocate);
-  explicit AudioBus(std::unique_ptr<media::AudioBus> media_bus);
 
   void DiscreteSumFrom(const AudioBus&);
 
@@ -193,8 +192,7 @@ class PLATFORM_EXPORT AudioBus final : public ThreadSafeRefCounted<AudioBus> {
   void SumFromByUpMixing(const AudioBus&);
   void SumFromByDownMixing(const AudioBus&);
 
-  std::unique_ptr<media::AudioBus> underlying_media_bus_;
-  uint32_t length_ = 0;
+  uint32_t length_;
   Vector<AudioChannel, 2> channels_;
   float sample_rate_ = 0.0f;  // 0.0 if unknown or N/A
 };
--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -1668,7 +1668,6 @@ _CONFIG = [
             'third_party/blink/public/platform/platform.h',
         ],
         'allowed': [
-            'media::AudioBus',
             'viz::RasterContextProvider',
         ],
     },
