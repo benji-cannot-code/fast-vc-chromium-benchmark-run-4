46bf72ac5b6bef3087fad70e18a1bae3429e7775
Dibyajyoti Pal <dibyapal@google.com>
2026-09-22 09:09:49 -0700

    [Gardener] Deflake FindBarViewsUiTest.PasteWithoutTextChange
    When WebUIReloadButton is enabled, clicking kReloadButtonElementId
    directly with MoveMouseTo() and ClickMouse() races against
    WebUIToolbarWebView surface hit-testing (:hover) and reload-button state
    readiness (such as hover-protection disable timers after initial page
    load), causing WaitForWebContentsNavigation() to time out.
    
    This CL attempts to deflake the test above in FindBarViewsUiTest to
    inherit from WebUIAndViewsToolbarInteractiveUiTestBase, instrument the
    toolbar in Init(), and use ClickReloadButton() that is already available
    in order to fix the flakiness.
    
    Fixed: 540863131
    Change-Id: I34d611cb8e6eb2322806f9618c0d6633554c3975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8436202
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702689}

--- chrome/browser/ui/views/find_bar_views_interactive_uitest.cc
+++ chrome/browser/ui/views/find_bar_views_interactive_uitest.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/find_bar_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
+#include "chrome/browser/ui/views/toolbar/webui_and_views_toolbar_interactive_uitest_base.h"
 #include "chrome/test/base/find_result_waiter.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
@@ -88,7 +89,8 @@ std::unique_ptr<net::test_server::HttpResponse> HandleHttpRequest(
   return response;
 }
 
-DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kTabId);
+const ui::ElementIdentifier kTabId =
+    WebUIAndViewsToolbarInteractiveUiTestBase::TabId();
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kTabBId);
 DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<bool>,
                                     kTextCopiedState);
@@ -219,7 +221,7 @@ class LegacyFindInPageTest : public InProcessBrowserTest {
   }
 };
 
-class FindBarViewsUiTest : public InteractiveBrowserTest,
+class FindBarViewsUiTest : public WebUIAndViewsToolbarInteractiveUiTestBase,
                            public ::testing::WithParamInterface<bool> {
  public:
   FindBarViewsUiTest() {
@@ -228,7 +230,7 @@ class FindBarViewsUiTest : public InteractiveBrowserTest,
 
   void SetUp() override {
     ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
-    InteractiveBrowserTest::SetUp();
+    WebUIAndViewsToolbarInteractiveUiTestBase::SetUp();
   }
 
   void SetUpOnMainThread() override {
@@ -237,7 +239,7 @@ class FindBarViewsUiTest : public InteractiveBrowserTest,
         base::BindRepeating(&HandleHttpRequest));
     embedded_test_server()->StartAcceptingConnections();
 
-    InteractiveBrowserTest::SetUpOnMainThread();
+    WebUIAndViewsToolbarInteractiveUiTestBase::SetUpOnMainThread();
   }
 
   ui::InteractionSequence::StepBuilder BringBrowserWindowToFront() {
@@ -282,7 +284,7 @@ class FindBarViewsUiTest : public InteractiveBrowserTest,
         ObserveState(
             views::test::kCurrentFocusedViewId,
             BrowserView::GetBrowserViewForBrowser(browser())->GetWidget()),
-        ObserveState(views::test::kCurrentWidgetFocus), InstrumentTab(kTabId),
+        ObserveState(views::test::kCurrentWidgetFocus), InstrumentToolbar(),
         NavigateWebContents(kTabId, url));
   }
 
@@ -810,13 +812,7 @@ IN_PROC_BROWSER_TEST_F(LegacyFindInPageTest, PrepopulateRespectBlank) {
 }
 #endif
 
-// TODO(crbug.com/540863131): Flaky on Linux ARM64.
-#if BUILDFLAG(IS_LINUX) && defined(ARCH_CPU_ARM64)
-#define MAYBE_PasteWithoutTextChange DISABLED_PasteWithoutTextChange
-#else
-#define MAYBE_PasteWithoutTextChange PasteWithoutTextChange
-#endif
-IN_PROC_BROWSER_TEST_F(FindBarViewsUiTest, MAYBE_PasteWithoutTextChange) {
+IN_PROC_BROWSER_TEST_F(FindBarViewsUiTest, PasteWithoutTextChange) {
   constexpr char16_t kSearchA[] = u"a";
   const GURL page_a = embedded_test_server()->GetURL("/a.html");
 
@@ -835,21 +831,7 @@ IN_PROC_BROWSER_TEST_F(FindBarViewsUiTest, MAYBE_PasteWithoutTextChange) {
       CheckViewProperty(FindBarView::kElementId, &FindBarView::GetFindText,
                         kSearchA),
       // Reload the page to clear the matching result.
-      // TODO(crbug.com/479732140): improve the test method to simplify the
-      // call.
-      MoveMouseTo(kReloadButtonElementId,
-#if !BUILDFLAG(IS_ANDROID)
-                  features::IsWebUIReloadButtonEnabled()
-                      ? RelativePositionSpecifier(
-                            base::BindOnce([](ui::TrackedElement* el) {
-                              return el->GetScreenBounds().CenterPoint();
-                            }))
-                      : CenterPoint()
-#else
-                  CenterPoint()
-#endif  // !BUILDFLAG(IS_ANDROID)
-                      ),
-      ClickMouse(), WaitForWebContentsNavigation(kTabId),
+      ClickReloadButton(), WaitForWebContentsNavigation(kTabId),
       WaitForState(views::test::kCurrentFocusedViewId,
                    ContentsWebView::kContentsWebViewElementId),
       // Focus the Find bar again to make sure the text is selected.
