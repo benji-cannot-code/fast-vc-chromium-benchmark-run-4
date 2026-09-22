12d3fb527cd0cedbe59bfe4fefdd2bbf17fe24bc
Maria del Pilar Dominguez Molina <mardelpi@google.com>
2026-09-21 20:42:34 -0700

    [ChroMenu GlowUp] Share GetSigninStatusChipString in profile_view_utils
    This CL moves GetSigninStatusChipString() into profile_view_utils so it
    can be shared between the legacy AppMenu and the new
    ActionAppMenuManager. This avoids code duplication.
    
    Change-Id: I1789c5c5087b4cb2fdc54bfa868372039703f1c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8381363
    Commit-Queue: María del Pilar Domínguez Molina <mardelpi@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702351}

--- chrome/browser/ui/views/app_menu/app_menu_chip_view_unittest.cc
+++ chrome/browser/ui/views/app_menu/app_menu_chip_view_unittest.cc
@@ -8,15 +8,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/actions/chrome_action_id.h"
 #include "chrome/browser/ui/browser_actions.h"
+#include "chrome/browser/ui/profiles/profile_view_utils.h"
 #include "chrome/browser/ui/views/app_menu/action_app_menu.h"
 #include "chrome/browser/ui/views/app_menu/action_app_menu_test_base.h"
 #include "chrome/browser/ui/views/app_menu/app_menu_action_item.h"
+#include "chrome/grit/generated_resources.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/actions/actions.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_item_view.h"
@@ -30,6 +37,21 @@ class AppMenuChipViewTest : public ActionAppMenuTestBase {
   AppMenuChipViewTest() = default;
   ~AppMenuChipViewTest() override = default;
 };
+
+TEST_F(AppMenuChipViewTest, GetSigninStatusChipString_NotSignedIn) {
+  EXPECT_EQ(GetSigninStatusChipString(profile_.get()),
+            l10n_util::GetStringUTF16(IDS_PROFILES_LOCAL_PROFILE_STATE));
+}
+
+TEST_F(AppMenuChipViewTest, GetSigninStatusChipString_SignedIn) {
+  signin::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForProfile(profile_.get());
+  signin::MakePrimaryAccountAvailable(identity_manager, "test@example.com",
+                                      signin::ConsentLevel::kSignin);
+
+  EXPECT_EQ(GetSigninStatusChipString(profile_.get()),
+            l10n_util::GetStringUTF16(IDS_PROFILE_ROW_SIGNED_IN_MESSAGE));
+}
 
 TEST_F(AppMenuChipViewTest, AttachTo_AddsChipAndUpdatesAccessibleName) {
   views::TestMenuItemView root_item;
