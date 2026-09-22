7661fc58b070bee2028e53ec85c2d2fa94b6f0e4
Pier-Alexandre Bouchard <pabouchard@google.com>
2026-09-22 14:01:54 -0700

    [iOS] Enable kLevelUpUIEnabled pref by default
    Changes the default registration value of `prefs::kLevelUpUIEnabled`
    from `false` to `true` in `LevelUpService` so that Level Up progress
    updates are enabled by default, and updates unit tests accordingly.
    
    Fixed: 562960579
    Change-Id: Ie5c7d3d56f8f4509b10c836fc8d25e975ac0d0c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8447890
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702947}

--- ios/chrome/browser/level_up/model/level_up_scene_agent_unittest.mm
+++ ios/chrome/browser/level_up/model/level_up_scene_agent_unittest.mm
@@ -90,7 +90,7 @@ class LevelUpSceneAgentTest : public PlatformTest {
 // snackbar when progress updates are disabled.
 TEST_F(LevelUpSceneAgentTest,
        TestActionTriggersCompletionWithoutSnackbarWhenProgressUpdatesDisabled) {
-  // Progress updates are disabled by default.
+  service_->SetUIEnabled(false);
   EXPECT_FALSE(service_->IsUIEnabled());
   EXPECT_FALSE(service_->IsTaskCompleted(TaskType::kTabGroups));
 
@@ -113,7 +113,7 @@ TEST_F(LevelUpSceneAgentTest,
 // progress updates are enabled.
 TEST_F(LevelUpSceneAgentTest,
        TestActionTriggersCompletionWithSnackbarWhenProgressUpdatesEnabled) {
-  service_->SetUIEnabled(true);
+  // Progress updates are enabled by default.
   EXPECT_TRUE(service_->IsUIEnabled());
   EXPECT_FALSE(service_->IsTaskCompleted(TaskType::kTabGroups));
 
--- ios/chrome/browser/level_up/model/level_up_service.mm
+++ ios/chrome/browser/level_up/model/level_up_service.mm
@@ -571,7 +571,7 @@ void LevelUpService::RegisterProfilePrefs(
       prefs::kLevelUpHighestLevel, 1,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(
-      prefs::kLevelUpUIEnabled, false,
+      prefs::kLevelUpUIEnabled, true,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(
       prefs::kLevelUpOptIn, false,
