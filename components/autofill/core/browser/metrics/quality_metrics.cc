c6ae4b11b91110db560d7a1741315a1f3f9e07a7
Tamino Bauknecht <taminob@chromium.org>
2026-09-22 05:11:53 -0700

    autofill: Add Autofill.SubmittedCountryCode metrics
    This change introduces a new set of metrics to decide if it is safe to
    remove the capability to fill country codes from Autofill using the
    feature `AutofillDisallowCountryCodeFilling`.
    
    Bug: 40264633
    Change-Id: Ib157328a7b3233250d5a22ce2ee25d6a6205aab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425600
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Tamino Bauknecht <taminob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702506}

--- components/autofill/core/browser/metrics/quality_metrics.cc
+++ components/autofill/core/browser/metrics/quality_metrics.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/i18n/char_iterator.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/data_quality/autofill_data_util.h"
@@ -153,6 +154,42 @@ GetAlternativeNameFieldValueCharacterSet(
   return AutofillAlternativeNameFieldValueCharacterSet::kOther;
 }
 
+// TODO(crbug.com/40264633): Remove this metric after feature
+// `AutofillDisallowCountryCodeFilling` is launched.
+void LogSubmittedCountryCodeMetric(const FormStructure& form) {
+  for (const std::unique_ptr<AutofillField>& field : form) {
+    if (field->IsSelectElement() ||
+        field->Type().GetAddressType() != ADDRESS_HOME_COUNTRY) {
+      continue;
+    }
+    std::u16string_view value =
+        base::TrimWhitespace(field->value(), base::TRIM_ALL);
+    if (value.empty()) {
+      continue;
+    }
+
+    const bool is_valid_country_code =
+        data_util::IsValidCountryCode(base::ToUpperASCII(value));
+
+    base::UmaHistogramBoolean("Autofill.SubmittedCountryCode",
+                              is_valid_country_code);
+    if (!field->last_modifier()) {
+      continue;
+    }
+    switch (*field->last_modifier()) {
+      case FieldModifier::kAutofill:
+        base::UmaHistogramBoolean("Autofill.SubmittedCountryCode.Autofilled",
+                                  is_valid_country_code);
+        break;
+      case FieldModifier::kUser:
+        base::UmaHistogramBoolean(
+            "Autofill.SubmittedCountryCode.ManuallyFilled",
+            is_valid_country_code);
+        break;
+    }
+  }
+}
+
 // Records the character set of the submitted value for each alternative name
 // field in the form.
 void LogSubmittedAlternativeNameCharacterSetValues(const FormStructure& form) {
@@ -243,6 +280,7 @@ void LogQualityMetrics(
   LogFillingMetrics(form_structure, form_interactions_ukm_logger, source_id,
                     observed_submission, now, ac_unrecognized_behavior);
   if (observed_submission) {
+    LogSubmittedCountryCodeMetric(form_structure);
     // TODO(crbug.com/359768803): Remove this metric once the feature is
     // launched.
     LogSubmittedAlternativeNameCharacterSetValues(form_structure);
