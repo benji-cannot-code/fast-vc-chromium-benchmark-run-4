e3bae278930ca45e8a924de7300124e79518568a
Gustavo Martin <gusmartin@google.com>
2026-09-22 15:01:46 -0700

    [Read Anything] Add distillation result to readability request IPC.
    Adding a result enum to RequestReadabilityDistillation() allows the
    renderer to tell the distillation outcome directly from the reply status
    so it can ignore cancelled requests while falling back to Screen2x on
    empty or ineligible pages.
    
    This CL adds ReadabilityDistillationResult (kSuccess, kEmpty,
    kIneligible, kCancelled) to the RequestReadabilityDistillation()
    callback and updates ReadAnythingUntrustedPageHandler and browser tests
    to return and verify each result state.
    
    Bug: 543987370
    Change-Id: I937f16d252bc885d9ae0bf0292ad853df1896896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8428310
    Commit-Queue: Gustavo Martin <gusmartin@google.com>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702980}

--- chrome/renderer/accessibility/read_anything/read_anything_app_controller_browsertest.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_controller_browsertest.cc
@@ -65,7 +65,9 @@ class MockReadAnythingUntrustedPageHandler
   MockReadAnythingUntrustedPageHandler() {
     ON_CALL(*this, RequestReadabilityDistillation(testing::_))
         .WillByDefault([](RequestReadabilityDistillationCallback callback) {
-          std::move(callback).Run("", "");
+          std::move(callback).Run(
+              read_anything::mojom::ReadabilityDistillationResult::kEmpty, "",
+              "");
         });
   }
 
