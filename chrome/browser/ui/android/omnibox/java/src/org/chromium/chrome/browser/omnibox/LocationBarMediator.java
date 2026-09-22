e06301aa977c21d8c67f4305d8f0416bd09925e8
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2026-09-22 09:56:53 -0700

    Fix NullPointerException in LocationBarMediator.updateUrlBarHintTextColor
    Add a null check for mUrlCoordinator in updateUrlBarHintTextColor. When
    LocationBarMediator.destroy() is called, mUrlCoordinator is set to null.
    Asynchronous callbacks such as NTP scroll updates can still run after
    destroy(), causing a NullPointerException when referencing
    mUrlCoordinator. Returning early if mUrlCoordinator is null prevents the
    crash.
    
    This fix was generated by the Crash Autofix pipeline for the crash
    details.
    
    http: //crash/50aa437a70400000. Please see go/crash-autofix for more
    Bug: 560575139
    Change-Id: I2b136fd3567bffb15b17a5a431c07ff894c350f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400347
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Peter Kang <peterkang@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702734}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -3534,6 +3534,7 @@ class LocationBarMediator
      *     the search box. If not we will use specific color for NTP's un-focus state.
      */
     public void updateUrlBarHintTextColor(boolean useDefaultUrlBarHintTextColor) {
+        if (mUrlCoordinator == null) return;
         if (useDefaultUrlBarHintTextColor) {
             mUrlCoordinator.setUrlBarHintTextColorForDefault(mBrandedColorScheme);
         } else {
