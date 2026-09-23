cda606158c86ca2116c903a43d9c3f5f960274a4
Maria del Pilar Dominguez Molina <mardelpi@google.com>
2026-09-23 16:57:22 -0700

    [ChroMenu GlowUp] Add Profile menu item to ActionAppMenu
    This CL adds the ProfileDynamicMenu item to the ActionAppMenu with its
    respective style, including the avatar image of the profile, the name
    bold, and the divider.
    Screenshot: https://drive.google.com/file/d/15lN0PRLTZXu8vqIOE0Vs-VmlndUZyvgd/view?usp=sharing
    
    Bug: 564614303
    Change-Id: I918b70ec17577e60dcff33abb9f99eb74a54c166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8435804
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: María del Pilar Domínguez Molina <mardelpi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703929}

--- chrome/browser/ui/views/app_menu/profile_dynamic_menu.h
+++ chrome/browser/ui/views/app_menu/profile_dynamic_menu.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_APP_MENU_PROFILE_DYNAMIC_MENU_H_
 #define CHROME_BROWSER_UI_VIEWS_APP_MENU_PROFILE_DYNAMIC_MENU_H_
 
+#include <optional>
+
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "ui/actions/actions.h"
+#include "ui/base/models/image_model.h"
 
 namespace ui {
 class ColorProvider;
@@ -27,6 +30,8 @@ class ProfileDynamicMenu {
   base::WeakPtr<ProfileDynamicMenu> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
+
+  std::optional<ui::ImageModel> GetProfileAvatarIcon() const;
 
   void BuildSyncSection(actions::BaseAction* parent_item);
   void BuildOtherProfiles(actions::BaseAction* parent_item);
