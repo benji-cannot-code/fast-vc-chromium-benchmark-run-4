5a5b69ff8c4cf0451b9e5bc5e43110d5b6727cc2
Mustafa Emre Acer <meacer@chromium.org>
2026-09-22 20:35:50 -0700

    [MTCs] Rename MTC index fields to serial in root_store.proto
    Bug: 520071497
    Change-Id: Ib5f49f4602be20ab3b06d612829bf23974a228d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8426328
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703166}

--- chrome/browser/component_updater/pki_metadata_component_installer_browsertest.cc
+++ chrome/browser/component_updater/pki_metadata_component_installer_browsertest.cc
@@ -1433,8 +1433,8 @@ IN_PROC_BROWSER_TEST_P(PKIMetadataComponentChromeRootStoreMtcMetadataTest,
           mtc_metadata_proto.add_mtc_anchor_data();
       mtc_anchor_metadata->set_ca_id(
           base::as_string_view(kMtcCaInMetadataWithNoLandmarksId));
-      chrome_root_store::MtcIndexRange* revoked_range =
-          mtc_anchor_metadata->add_revoked_indices();
+      chrome_root_store::MtcSerialRange* revoked_range =
+          mtc_anchor_metadata->add_revoked_serials();
       revoked_range->set_start_inclusive(5);
       revoked_range->set_end_exclusive(10);
     }
@@ -1686,8 +1686,8 @@ IN_PROC_BROWSER_TEST_P(PKIMetadataComponentChromeRootStoreMtcMetadataTest,
           mtc_metadata_proto.add_mtc_anchor_data();
       mtc_anchor_metadata->set_ca_id(
           base::as_string_view(kMtcCaInMetadataWithNoLandmarksId));
-      chrome_root_store::MtcIndexRange* revoked_range =
-          mtc_anchor_metadata->add_revoked_indices();
+      chrome_root_store::MtcSerialRange* revoked_range =
+          mtc_anchor_metadata->add_revoked_serials();
       revoked_range->set_start_inclusive(5);
       revoked_range->set_end_exclusive(10);
     }
@@ -2419,7 +2419,7 @@ IN_PROC_BROWSER_TEST_P(PKIMetadataComponentChromeRootStoreMtcMetadataTest,
 
     // Add revoked range that contains 1 certificate.
     {
-      auto* revoked_range = mtc_anchor_data->add_revoked_indices();
+      auto* revoked_range = mtc_anchor_data->add_revoked_serials();
       revoked_range->set_start_inclusive(test_cert_data[1].mtc_serial);
       revoked_range->set_end_exclusive(test_cert_data[2].mtc_serial);
       test_cert_data[1].expect_is_revoked = true;
@@ -2427,7 +2427,7 @@ IN_PROC_BROWSER_TEST_P(PKIMetadataComponentChromeRootStoreMtcMetadataTest,
 
     // Add revoked range that contains multiple certificates.
     {
-      auto* revoked_range = mtc_anchor_data->add_revoked_indices();
+      auto* revoked_range = mtc_anchor_data->add_revoked_serials();
       revoked_range->set_start_inclusive(test_cert_data[3].mtc_serial);
       revoked_range->set_end_exclusive(test_cert_data[5].mtc_serial);
       test_cert_data[3].expect_is_revoked = true;
@@ -2808,7 +2808,7 @@ IN_PROC_BROWSER_TEST_P(PKIMetadataComponentChromeRootStoreMtcMetadataTest,
     issuer->set_realm(realm());
     auto* constraint = issuer->add_constraints();
     constraint->add_permitted_dns_names("example.org");
-    constraint->set_index_not_after((kLogNumber << 48) + mtc_log_index2);
+    constraint->set_serial_not_after((kLogNumber << 48) + mtc_log_index2);
 
     InstallCRSUpdate(root_store_proto, mtc_config);
   }
