70a911446fad1b6f013e28f4ce4ada456dd6d9c7
David Roger <droger@chromium.org>
2026-09-23 08:56:54 -0700

    [Mac] Disable test TutorialFlowCompletesOnTabSend
    Bug: 565062574
    Change-Id: If61daf7e54025060bd4425fb550b4f86b560be09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8450454
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703486}

--- chrome/browser/ui/send_tab_to_self/send_tab_to_self_iph_interactive_uitest.cc
+++ chrome/browser/ui/send_tab_to_self/send_tab_to_self_iph_interactive_uitest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
+#include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/send_tab_to_self_sync_service_factory.h"
@@ -463,8 +464,15 @@ IN_PROC_BROWSER_TEST_F(SendTabToSelfIphInteractiveUiTest,
       WaitForPromo(feature_engagement::kIPHSendTabToSelfTutorialFeature));
 }
 
+// TODO(crbug.com/565062574): The test crashes on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_TutorialFlowCompletesOnTabSend \
+  DISABLED_TutorialFlowCompletesOnTabSend
+#else
+#define MAYBE_TutorialFlowCompletesOnTabSend TutorialFlowCompletesOnTabSend
+#endif
 IN_PROC_BROWSER_TEST_F(SendTabToSelfIphInteractiveUiTest,
-                       TutorialFlowCompletesOnTabSend) {
+                       MAYBE_TutorialFlowCompletesOnTabSend) {
   const GURL eligible_url =
       embedded_https_test_server().GetURL("example.com", "/title1.html");
 
