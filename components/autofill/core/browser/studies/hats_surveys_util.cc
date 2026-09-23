aae7fd1f53f89f976aa6d18c79545b80309fed2a
Tamino Bauknecht <taminob@chromium.org>
2026-09-23 02:32:24 -0700

    autofill: Remove AutofillAi Journey HaTS survey
    This survey is intended to allow users to provide feedback when they
    were shown Ambient Autofill suggestions.
    
    Since the same can be achieved by the new personalization and trust
    surveys, this change removes them to avoid multiple surveys triggering
    on the same preconditions.
    
    Bug: 543741063
    Change-Id: I35d63f54efd57600be800ea108b6722a9bc7bf41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8279809
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Commit-Queue: Tamino Bauknecht <taminob@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703284}

--- components/autofill/core/browser/studies/hats_surveys_util.cc
+++ components/autofill/core/browser/studies/hats_surveys_util.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "components/autofill/core/browser/autofill_field.h"
-#include "components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h"
 #include "components/autofill/core/browser/filling/filling_product.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
@@ -283,34 +282,6 @@ RecentUserAutofillAiInteractionsForHats::GetRecentUserInteraction(
     return it->second;
   }
   return std::nullopt;
-}
-
-void MaybeTriggerAutofillAiSubmissionHatsSurveys(
-    AutofillClient& client,
-    const FormStructure& submitted_form,
-    const RecentUserAutofillAiInteractionsForHats& suggestion_interactions) {
-  std::optional<RecentUserAutofillAiInteractionsForHats::InteractionDetails>
-      interaction_details = suggestion_interactions.GetRecentUserInteraction(
-          submitted_form.global_id());
-  if (!interaction_details) {
-    return;
-  }
-  const EntityDataManager* entity_manager = client.GetEntityDataManager();
-  if (!entity_manager) {
-    return;
-  }
-  if (!interaction_details->entity_type_accepted.empty() &&
-      std::ranges::contains(interaction_details->accepted_entity_record_type,
-                            EntityInstance::RecordType::kPersonalContext)) {
-    auto saved_entity_type_names = base::MakeFlatSet<EntityTypeName>(
-        entity_manager->GetEntityInstances(), std::less(),
-        [](const EntityInstance& entity) { return entity.type().name(); });
-
-    client.TriggerAutofillAiFillingJourneySurvey(
-        /*suggestion_accepted=*/true,
-        interaction_details->entity_type_accepted.front(),
-        saved_entity_type_names, interaction_details->autofill_ai_field_types);
-  }
 }
 
 }  // namespace autofill
