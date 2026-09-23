2c8cf99cf0413d843c1214b00b4f816d3697387e
Rob Pitkin <robpitkin@google.com>
2026-09-23 16:26:47 -0700

    Spanify RgbVideoFrame with base::HeapArray
    Replace std::unique_ptr<RgbColor[]> with base::HeapArray<RgbColor> in
    recording::RgbVideoFrame as part of the unsafe buffers cleanup.
    
    - Use base::HeapArray::Uninit() in the frame constructor to avoid
      redundant zero-initialization overhead before SkBitmap pixels are
      copied.
    - Use base::HeapArray::CopiedFrom() in the copy constructor, removing
      manual std::memcpy and associated UNSAFE_TODO annotations.
    - Leverage HeapArray's bounds-checked operator[] in pixel_color().
    
    Bug: 40285824
    Change-Id: I0b8f85dd9ce5d13a356f28ef273bbdcdb9e62127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8445214
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Rob Pitkin <robpitkin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703900}

--- chromeos/ash/services/recording/rgb_video_frame.h
+++ chromeos/ash/services/recording/rgb_video_frame.h
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_SERVICES_RECORDING_RGB_VIDEO_FRAME_H_
 
 #include <cstdint>
-#include <memory>
 
 #include "base/compiler_specific.h"
+#include "base/containers/heap_array.h"
 #include "base/time/time.h"
 #include "media/base/video_frame.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -84,7 +84,7 @@ class RgbVideoFrame {
   // Returns the color of the pixel at `row` and `column`. The non-const version
   // can be used to change the color of the pixel.
   RgbColor& pixel_color(int row, int column) {
-    return UNSAFE_TODO(data_[row * width_ + column]);
+    return data_[row * width_ + column];
   }
   const RgbColor& pixel_color(int row, int column) const {
     return const_cast<RgbVideoFrame*>(this)->pixel_color(row, column);
@@ -104,7 +104,7 @@ class RgbVideoFrame {
   const base::TimeTicks frame_time_;
 
   // The pixel color data.
-  std::unique_ptr<RgbColor[]> data_;
+  base::HeapArray<RgbColor> data_;
 };
 
 }  // namespace recording
