4e29ae7a7f9e6f22b90b07f9603ce41bb46d54ee
Kevin Ellis <kevers@google.com>
2026-09-23 06:52:41 -0700

    SDA: Use globally scoped names.
    Global scoping was previously implemented but never turned on and
    using the flat tree. In this patch:
    
    * Set status to experimental
    * Use tree scoped names
    
    Change-Id: I37e12ab75ad1fc43fd91a16c614deca8127aeb06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8410590
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703342}

--- third_party/blink/renderer/core/animation/document_animations.h
+++ third_party/blink/renderer/core/animation/document_animations.h
@@ -149,8 +149,13 @@ class CORE_EXPORT DocumentAnimations final
   void UpdateCompositorAnimationTriggers(
       const PaintArtifactCompositor* paint_artifact_compositor);
 
+  DeferredTimeline& GetGlobalDeferredTimeline(const TreeScope&,
+                                              const AtomicString&);
+  DeferredTimeline* FindExistingGlobalDeferredTimeline(
+      const TreeScope&,
+      const AtomicString&) const;
   DeferredTimeline& GetGlobalDeferredTimeline(const AtomicString& name) {
-    return *global_deferred_timelines_.Find(*document_, name);
+    return GetGlobalDeferredTimeline(*document_, name);
   }
 
   uint64_t current_transition_generation_;
@@ -162,6 +167,22 @@ class CORE_EXPORT DocumentAnimations final
   void RemoveReplacedAnimations(ReplaceableAnimationsMap*);
 
  private:
+  class TreeScopeDeferredTimelineMap
+      : public GarbageCollected<TreeScopeDeferredTimelineMap> {
+   public:
+    TreeScopeDeferredTimelineMap()
+        : map_(StyleTimelineScope{StyleTimelineScope::Type::kAll,
+                                  Vector<AtomicString>()}) {}
+
+    CSSDeferredTimelineMap& Map() { return map_; }
+    const CSSDeferredTimelineMap& Map() const { return map_; }
+
+    void Trace(Visitor* visitor) const { visitor->Trace(map_); }
+
+   private:
+    CSSDeferredTimelineMap map_;
+  };
+
   void MarkPendingIfCompositorPropertyAnimationChanges(
       const PaintArtifactCompositor*);
 
@@ -178,8 +199,8 @@ class CORE_EXPORT DocumentAnimations final
   // as a last resort.
   //
   // Only used when the CSSTimelineScopeGlobal flag is enabled.
-  CSSDeferredTimelineMap global_deferred_timelines_{StyleTimelineScope{
-      StyleTimelineScope::Type::kAll, /*names=*/Vector<AtomicString>()}};
+  HeapHashMap<WeakMember<const TreeScope>, Member<TreeScopeDeferredTimelineMap>>
+      global_deferred_timelines_by_scope_;
 };
 
 }  // namespace blink
