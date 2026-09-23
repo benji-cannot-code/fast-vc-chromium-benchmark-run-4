4e53297557cbc34bc56fc6946f1b0b4d9f1c4f07
Guido Urdaneta <guidou@chromium.org>
2026-09-23 07:35:11 -0700

    [RTCLogging] Implement diagnostic logging API in RTCPeerConnection
    The latest spec version specifies that the diagnostic logging methods
    should be static methods in RTCPeerConnection.
    
    This CL moves their implementation to comply with this, but keeps
    the old navigator.rtc methods for compatibility.
    
    Bug: 481412281
    Change-Id: I1c63d31b58465b1319b21f6ff69b36861b0c2def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8164001
    Reviewed-by: Palak Agarwal <agpalak@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703403}

---
+++ third_party/blink/renderer/modules/peerconnection/diagnostic_logging/rtc_diagnostic_logging.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_DIAGNOSTIC_LOGGING_RTC_DIAGNOSTIC_LOGGING_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_DIAGNOSTIC_LOGGING_RTC_DIAGNOSTIC_LOGGING_H_
+
+#include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+
+namespace blink {
+
+class ExceptionState;
+class RTCDiagnosticLoggingOptions;
+class ScriptState;
+
+class MODULES_EXPORT RTCDiagnosticLogging final {
+  STATIC_ONLY(RTCDiagnosticLogging);
+
+ public:
+  static String startDiagnosticLogging(ScriptState*,
+                                       const RTCDiagnosticLoggingOptions*,
+                                       ExceptionState&);
+  static void stopDiagnosticLogging(ScriptState*, ExceptionState&);
+  static void cancelDiagnosticLogging(ScriptState*, ExceptionState&);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_PEERCONNECTION_DIAGNOSTIC_LOGGING_RTC_DIAGNOSTIC_LOGGING_H_
