5ed7852cc119193636bc6b5cbf9638ef4c18ced2
Foromo Daniel Soromou <koretadaniel@chromium.org>
2026-09-23 08:36:46 -0700

    Use base::ScopedMultiSourceObservation in AnimationAbortHandle
    Bug: 41446264
    Change-Id: I42132a879f0daf735a030c69417ec37759f680df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8356546
    Commit-Queue: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703465}

--- ui/views/animation/animation_abort_handle.h
+++ ui/views/animation/animation_abort_handle.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_ANIMATION_ANIMATION_ABORT_HANDLE_H_
 #define UI_VIEWS_ANIMATION_ANIMATION_ABORT_HANDLE_H_
 
-#include <set>
-
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_multi_source_observation.h"
 #include "ui/compositor/layer_observer.h"
 #include "ui/views/animation/animation_builder.h"
 #include "ui/views/views_export.h"
@@ -51,11 +50,11 @@ class VIEWS_EXPORT AnimationAbortHandle : public ui::LayerObserver {
   raw_ptr<AnimationBuilder::Observer, DanglingUntriaged> observer_;
   AnimationState animation_state_ = AnimationState::kNotStarted;
 
-  // Stores the layers tracked by the animation abort handle.
-  std::set<raw_ptr<ui::Layer, SetExperimental>> tracked_layers_;
-
-  // Stores the layers that are deleted during tracking.
-  std::set<raw_ptr<ui::Layer, SetExperimental>> deleted_layers_;
+  // The layers whose animations this handle aborts. A layer is removed from
+  // this set as soon as it is destroyed (see LayerDestroyed()), so every
+  // source in it is guaranteed to be alive.
+  base::ScopedMultiSourceObservation<ui::Layer, ui::LayerObserver>
+      layer_observations_{this};
 };
 
 }  // namespace views
