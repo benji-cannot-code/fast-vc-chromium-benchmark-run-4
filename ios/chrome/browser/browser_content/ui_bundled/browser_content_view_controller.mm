e23cd6ee0e6fa1826ff6236c7b163265e14eaa9f
Xu Li <mxlg@google.com>
2026-09-23 07:50:23 -0700

    [iOS] Remove the redundant code.
    Change-Id: I8b8e4ac10e2e3d01c4a54a208d086a80a8d90024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8442676
    Commit-Queue: Xu Li <mxlg@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703426}

--- ios/chrome/browser/browser_content/ui_bundled/browser_content_view_controller.mm
+++ ios/chrome/browser/browser_content/ui_bundled/browser_content_view_controller.mm
@@ -93,7 +93,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (contentViewController) {
     [contentViewController willMoveToParentViewController:self];
     [self addChildViewController:contentViewController];
-    [self.view insertSubview:contentViewController.view atIndex:0];
     if (_contentView) {
       [self.view insertSubview:contentViewController.view
                   aboveSubview:self.contentView];
