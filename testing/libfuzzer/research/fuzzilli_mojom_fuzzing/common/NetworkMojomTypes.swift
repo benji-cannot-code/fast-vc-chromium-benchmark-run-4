9f908ce40cdbf03de6c4b6f56b7e46712c2bef6f
Tigran Bantikyan <bantikyan@chromium.org>
2026-09-23 07:29:18 -0700

    fuzzilli: Split up MojoCommonProfile.swift by Mojo module
    Split MojoCommonProfile.swift into separate files for each module to
    improve readability. MojoCommonProfile.swift consolidates all the
    definitions in these files. It also contains definitions for
    JS types.
    
    Bug: 479225939
    Change-Id: I3a1fde016e92629b379e6f5995d5602d625f18cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8444596
    Commit-Queue: Tigran Bantikyan <bantikyan@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703395}

---
+++ testing/libfuzzer/research/fuzzilli_mojom_fuzzing/common/NetworkMojomTypes.swift
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Definitions for `network.mojom` types.
+///
+/// See `MojoCommonProfile.swift` for the naming conventions these definitions
+/// follow and for how the lists below are registered.
+
+extension CommonMojoStrings {
+    static let networkMojomConnectionInfo = "network.mojom.ConnectionInfo"
+    static let networkMojomEffectiveConnectionType = "network.mojom.EffectiveConnectionType"
+}
+
+public let networkMojomEnumerations: [ILType] = [
+    .jsNetworkMojomConnectionInfo,
+    .jsNetworkMojomEffectiveConnectionType,
+]
+
+extension ILType {
+    public static let jsNetworkMojomConnectionInfo: ILType = .intEnumeration(
+        ofName: CommonMojoStrings.networkMojomConnectionInfo, withValues: Array(0...42)
+    )
+    public static let jsNetworkMojomEffectiveConnectionType: ILType = .intEnumeration(
+        ofName: CommonMojoStrings.networkMojomEffectiveConnectionType, withValues: Array(0...5)
+    )
+}
