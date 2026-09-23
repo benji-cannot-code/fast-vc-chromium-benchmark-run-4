7afcb7eba203a2118b894f064b31f66e21a56a09
Feras Aldahlawi <frs@chromium.org>
2026-09-23 12:02:49 -0700

    cdm: Compile cdm storage id functions only if needed in mojo
    CdmDocumentService.GetStorageId is only supported when
    enable_cdm_storage_id is enabled. Previously, it was declared
    unconditionally at the Mojom level, requiring unsupported platforms to
    compile and maintain dummy stubs returning empty storage IDs.
    
    This change uses [EnableIf=enable_cdm_storage_id] on GetStorageId in
    cdm_document_service.mojom and conditionally compiles its implementation
    in CdmDocumentServiceImpl and MojoCdmHelper. This reduces the browser's
    exposed IPC attack surface on unsupported platforms and resolves the
    remaining TODO for crbug.com/40499115 on this interface.
    
    Bug: 40499115
    Change-Id: I4086c999206d3a1ce62035c8f5114a076a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419383
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Feras Aldahlawi <frs@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703645}

--- chrome/browser/media/cdm_document_service_impl.h
+++ chrome/browser/media/cdm_document_service_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/document_service.h"
+#include "media/media_buildflags.h"
 #include "media/mojo/mojom/cdm_document_service.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 
@@ -43,11 +44,11 @@ class CdmDocumentServiceImpl final
   void ChallengePlatform(const std::string& service_id,
                          const std::string& challenge,
                          ChallengePlatformCallback callback) final;
-#endif  // BUILDFLAG(IS_CHROMEOS)
-  void GetStorageId(uint32_t version, GetStorageIdCallback callback) final;
-#if BUILDFLAG(IS_CHROMEOS)
   void IsVerifiedAccessEnabled(IsVerifiedAccessEnabledCallback callback) final;
 #endif  // BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(ENABLE_CDM_STORAGE_ID)
+  void GetStorageId(uint32_t version, GetStorageIdCallback callback) final;
+#endif  // BUILDFLAG(ENABLE_CDM_STORAGE_ID)
 #if BUILDFLAG(IS_WIN)
   void GetMediaFoundationCdmData(
       GetMediaFoundationCdmDataCallback callback) final;
@@ -87,8 +88,10 @@ class CdmDocumentServiceImpl final
                             const std::string& platform_key_certificate);
 #endif
 
+#if BUILDFLAG(ENABLE_CDM_STORAGE_ID)
   void OnStorageIdResponse(GetStorageIdCallback callback,
                            const std::vector<uint8_t>& storage_id);
+#endif  // BUILDFLAG(ENABLE_CDM_STORAGE_ID)
 
 #if BUILDFLAG(IS_CHROMEOS)
   scoped_refptr<ash::attestation::PlatformVerificationFlow>
