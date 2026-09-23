c2e5772cb3eb2db06425561fbd6f4ebcf5c0b61c
Gayane Petrosyan <gayane@google.com>
2026-09-23 10:01:13 -0700

    [iOS] Remove duplicate [_mediator connect] in GeminiContainerCoordinator
    Remove the early [_mediator connect] call in
    -[GeminiContainerCoordinator start]. The mediator is connected at the
    end of -start after the consumer and container handler are configured
    and the assistant container is presented.
    
    Change-Id: I00124c4f042437403fe5d1beddcec6b68087eebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8446141
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Auto-Submit: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703537}

--- ios/chrome/browser/intelligence/bwg/coordinator/gemini_container_coordinator.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/gemini_container_coordinator.mm
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       initWithBrowser:self.browser
          actorService:actorService
          eventHandler:GeminiBrowserAgent::FromBrowser(self.browser)];
-  [_mediator connect];
   _containerHandler = HandlerForProtocol(self.browser->GetCommandDispatcher(),
                                          AssistantContainerCommands);
   _mediator.containerHandler = _containerHandler;
