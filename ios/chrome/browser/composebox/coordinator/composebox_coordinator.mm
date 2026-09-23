3e770d731ed7cf3f7c7c646c56ca1a871a368ae4
Christian Xu <christianxu@chromium.org>
2026-09-23 04:14:46 -0700

    [iOS] Force composebox to top position on iPhone
    https://crrev.com/c/8345550 cleaned up kComposeboxForceTop (which was
    enabled by default), but removed the IsComposeboxForceTopEnabled() early
    return in -[ComposeboxCoordinator inputPlatePositionPreference] instead
    of removing the bottom omnibox preference branch.
    
    Remove the bottom omnibox preference check so the composebox is always
    forced to the top on non-iPad layouts.
    
    Fixed: 565229955
    Change-Id: Iabe3db93a62bdf86b0e1e577f414c047dc96c56b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8446501
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703320}

--- ios/chrome/browser/composebox/coordinator/composebox_coordinator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_coordinator.mm
@@ -6,9 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/composebox/coordinator/composebox_coordinator.h"
 
 #import "base/ios/ios_util.h"
-#import "components/omnibox/browser/omnibox_pref_names.h"
 #import "components/open_from_clipboard/clipboard_recent_content.h"
-#import "components/prefs/pref_service.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_input_plate_coordinator.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_mode_holder.h"
 #import "ios/chrome/browser/composebox/coordinator/composebox_navigation_mediator.h"
@@ -29,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/model/new_tab_page_util.h"
 #import "ios/chrome/browser/omnibox/ui/popup/omnibox_popup_util.h"
 #import "ios/chrome/browser/shared/coordinator/layout_guide/layout_guide_util.h"
-#import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
@@ -329,12 +326,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (ComposeboxInputPlatePosition)inputPlatePositionPreference {
   if (ShouldApplyOmniboxPopoutLayout(self.baseViewController)) {
     return ComposeboxInputPlatePosition::kiPad;
-  }
-
-  if (IsBottomOmniboxAvailable() &&
-      GetApplicationContext()->GetLocalState()->GetBoolean(
-          omnibox::kIsOmniboxInBottomPosition)) {
-    return ComposeboxInputPlatePosition::kBottom;
   }
 
   return ComposeboxInputPlatePosition::kTop;
