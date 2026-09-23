63b0dc3b29a0ba9def568f573b1b6aec0390dbb9
Gayane Petrosyan <gayane@google.com>
2026-09-23 08:38:49 -0700

    [iOS] Add didTapResponseReadyViewButton to GeminiSessionDelegate
    Adds didTapResponseReadyViewButton to GeminiSessionDelegate and
    GeminiViewStateDelegate, and animates the assistant container to the
    medium detent when tapped in GeminiContainerMediator.
    
    Bug: 542592938
    Change-Id: Ia41a79f26401ab0faf87bfaa3f60a463b1e0e834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8447790
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703468}

--- ios/chrome/browser/intelligence/bwg/model/gemini_session_handler.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_session_handler.mm
@@ -462,6 +462,10 @@ IOSGeminiSessionCancellationReason HistogramEnumFromGeminiCancelType(
   [self dismissAndRecordMetrics];
 }
 
+- (void)didTapResponseReadyViewButton {
+  [self.geminiViewStateDelegate didTapResponseReadyViewButton];
+}
+
 #pragma mark - Private
 
 // Finds the web state with the given client ID as unique identifier.
