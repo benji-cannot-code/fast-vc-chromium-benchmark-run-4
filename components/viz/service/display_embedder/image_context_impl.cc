70793708650f8a9105bba6e3528dda5c305ba5f8
Seonggon Cho <jmsmg1@me.com>
2026-09-23 11:19:30 -0700

    [viz] Remove the kValidatePromiseImageFormat kill switch
    ImageContextImpl refuses to fulfill a promise texture when the shared
    image format does not match the TransferableResource format, behind a
    kill switch to be removed once M152 was stable. The validation has
    been enabled by default since it landed and the tree is at M156, so
    make it unconditional and delete the flag along with the include it
    was the last user of.
    
    R=kylechar@chromium.org
    
    Bug: 524822746
    Change-Id: I2d864ed21210756e7fc134d83aaed80530a86966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8450690
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703601}

--- components/viz/service/display_embedder/image_context_impl.cc
+++ components/viz/service/display_embedder/image_context_impl.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
-#include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/trace_event/trace_event.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
@@ -34,10 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/private/chromium/GrPromiseImageTexture.h"
 
 namespace {
-
-// TODO(crbug.com/524822746): Killswitch for extra validation, remove after M152
-// stable.
-BASE_FEATURE(kValidatePromiseImageFormat, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
@@ -418,10 +413,9 @@ bool ImageContextImpl::BeginAccessIfNecessaryInternal(
       return false;
     }
 
-    if ((representation->format() != format() ||
-         representation->format().PrefersExternalSampler() !=
-             format().PrefersExternalSampler()) &&
-        base::FeatureList::IsEnabled(kValidatePromiseImageFormat)) {
+    if (representation->format() != format() ||
+        representation->format().PrefersExternalSampler() !=
+            format().PrefersExternalSampler()) {
       DLOG(ERROR) << "Failed to fulfill the promise texture - SharedImage "
                      "format does not match TransferableResource format: "
                   << representation->format().ToString() << " vs "
