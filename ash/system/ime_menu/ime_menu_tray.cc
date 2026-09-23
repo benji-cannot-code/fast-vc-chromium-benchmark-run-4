346eb0e530885120a64b986fccf18a57c413cd09
Jack Geng <jackgeng@google.com>
2026-09-23 16:28:02 -0700

    ash: Migrate ImeMenuTray label color to ui::ColorId
    This CL simplifies `ImeMenuTray` by applying its tray label color once
    at construction. Previously, this view was required to manually handle
    theme updates by overriding `OnThemeChanged()`.
    
    Following crrev.com/c/6267139, we can pass the `ui::ColorId` directly to
    `views::Label::SetEnabledColor`. The label now automatically handles
    theme changes, eliminating the need for manual color management.
    
    Bug: 394420459
    Change-Id: I456800e8f2b0f8583635d20321b26936fe06de2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8432434
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jack Geng <jackgeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703902}

--- ash/system/ime_menu/ime_menu_tray.cc
+++ ash/system/ime_menu/ime_menu_tray.cc
@@ -390,7 +390,11 @@ ImeMenuTray::ImeMenuTray(Shelf* shelf)
   DCHECK(ime_controller_);
   SetCallback(base::BindRepeating(&ImeMenuTray::OnTrayButtonPressed,
                                   weak_ptr_factory_.GetWeakPtr()));
+
+  // Only the label exists at construction; the globe icon is created later.
   CreateLabel();
+  UpdateTrayImageOrLabelColor(/*is_image=*/false);
+
   SystemTrayNotifier* tray_notifier = Shell::Get()->system_tray_notifier();
   tray_notifier->AddIMEObserver(this);
   tray_notifier->AddVirtualKeyboardObserver(this);
@@ -486,11 +490,6 @@ bool ImeMenuTray::ShouldShowKeyboardToggle() const {
                                       ->accessibility_controller()
                                       ->virtual_keyboard()
                                       .enabled();
-}
-
-void ImeMenuTray::OnThemeChanged() {
-  TrayBackgroundView::OnThemeChanged();
-  UpdateTrayLabel();
 }
 
 void ImeMenuTray::HandleLocaleChange() {
