7ae75d63aa135558544846ad76d8f1b4df896558
Rafał Godlewski <rgod@google.com>
2026-09-23 10:48:51 -0700

    [iOS][CPE] Re-enable automatic passkey upgrade
    Re-enable Automatic Passkey Upgrade in Credential Provider Extension by
    removing the temporary force-disable in
    performPasskeyRegistrationWithoutUserInteractionIfPossible:.
    
    The underlying crash in MemoryCredentialStore (crbug.com/515318495) was
    resolved in crrev.com/c/7886850 by making MemoryCredentialStore
    thread-safe.
    
    Fixed: 515318495
    Change-Id: I1af32a646faab373ad310b775ec07c147f41af29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8450951
    Auto-Submit: Rafał Godlewski <rgod@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703580}

--- ios/chrome/credential_provider_extension/credential_provider_view_controller.mm
+++ ios/chrome/credential_provider_extension/credential_provider_view_controller.mm
@@ -318,14 +318,6 @@ enum class PasskeyCreationEligibility {
 
 - (void)performPasskeyRegistrationWithoutUserInteractionIfPossible:
     (ASPasskeyCredentialRequest*)registrationRequest {
-  // TODO(crbug.com/515318495): Force disable registration to prevent the CPE
-  // startup crash in M149
-  [self exitWithErrorCode:ASExtensionErrorCodeFailed];
-  return;
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunreachable-code"
-
   PasskeyRequestDetails* passkeyRequestDetails =
       [self passkeyDetailsFromConditionalCreateRequest:registrationRequest];
   if (![passkeyRequestDetails
@@ -348,8 +340,6 @@ enum class PasskeyCreationEligibility {
 
   // Try to create a passkey while user interaction is disallowed.
   [self createPasskeyWithDetails:passkeyRequestDetails gaia:gaia];
-
-#pragma clang diagnostic pop
 }
 
 - (void)prepareInterfaceForPasskeyRegistration:
