64b38b0f1a7681beaf71d1d759ad45e2eefe0f91
Timofey Chudakov <tchudakov@google.com>
2026-09-23 01:26:53 -0700

    [Android] Create helper class for security sensitive views.
    SecurityTouchEventInterceptionHelper is a helper class to monitor
    motion events to check if the application window is obscured or not.
    
    It's possible to receive motion events by setting a
    OnTouchEventListener on a view (the OnClickListener does not receive
    granular motion events). However, this has the following downsides:
    
    * The ViewGroup won't receive a MotionEvent if some child view has
      consumed it.
    * Motion events are granular, a view receives several motion events
      for a single click (a click event is an aggregated event).
    * Autofill surfaces render a lot of different view types, which can
      trigger filling. Setting OnTouchEventListeners on all of them will
      require a lot of engineering effort.
    
    That's why the optimal way to track click events when the application
    window is obscured is by implementing the onInterceptTouchEvent() for
    the parent Autofill surface: the onInterceptTouchEvent() method is
    invoked before the MotionEvent is propagated to the receiving view.
    
    The Autofill UI surfaces like Keyboard Accessory, Touch-to-fill
    bottomsheets, etc. will override the onInterceptTouchEvent
    method and provide information to the OnClickListeners whether
    the application is obscured or not.
    
    Bug: 561395976
    Change-Id: I84f8f8c4b324a43da249ff768a2a0c0a8e66201d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8412885
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703248}

--- chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryView.java
+++ chrome/browser/keyboard_accessory/android/internal/java/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryView.java
@@ -37,6 +37,7 @@ import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.keyboard_accessory.R;
 import org.chromium.chrome.browser.keyboard_accessory.bar_component.KeyboardAccessoryStyle.NotchPosition;
+import org.chromium.components.browser_ui.widget.security.SecurityTouchEventInterceptionHelper;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.ui.base.ViewUtils;
 import org.chromium.ui.widget.ViewRectProvider;
@@ -53,6 +54,7 @@ class KeyboardAccessoryView extends LinearLayout {
     private static final int FADE_ANIMATION_DURATION_MS = 150; // Total duration of show/hide.
     private static final int HIDING_ANIMATION_DELAY_MS = 50; // Shortens animation duration.
 
+    private final SecurityTouchEventInterceptionHelper mSecurityHelper;
     private @Nullable Tracker mFeatureEngagementTracker;
     private @Nullable Callback<Integer> mObfuscatedLastChildAt;
     private @Nullable Callback<Boolean> mOnTouchEvent;
@@ -180,6 +182,7 @@ class KeyboardAccessoryView extends LinearLayout {
     /** Constructor for inflating from XML which is why it must be public. */
     public KeyboardAccessoryView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mSecurityHelper = new SecurityTouchEventInterceptionHelper();
     }
 
     @Override
@@ -187,16 +190,16 @@ class KeyboardAccessoryView extends LinearLayout {
         return true; // Accessory view is a sink for all events. Touch/Click is handled earlier.
     }
 
+    public boolean wasClickedWhenObscured() {
+        return mSecurityHelper.isWindowObscured();
+    }
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        final boolean isViewObscured =
-                (event.getFlags()
-                                & (MotionEvent.FLAG_WINDOW_IS_PARTIALLY_OBSCURED
-                                        | MotionEvent.FLAG_WINDOW_IS_OBSCURED))
-                        != 0;
+        mSecurityHelper.registerMotionEvent(event);
         // The event is filtered out when the keyboard accessory view is fully or partially obscured
         // given that no user education bubbles are shown to the user.
-        final boolean shouldFilterEvent = isViewObscured && !mAllowClicksWhileObscured;
+        final boolean shouldFilterEvent = wasClickedWhenObscured() && !mAllowClicksWhileObscured;
         if (mOnTouchEvent != null) {
             mOnTouchEvent.onResult(shouldFilterEvent);
         }
