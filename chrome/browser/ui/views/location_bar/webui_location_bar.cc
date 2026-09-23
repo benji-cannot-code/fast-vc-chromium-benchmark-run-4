2297b587433eaa2351aa6088c9b05574dcbea70f
Lei Zhang <thestig@chromium.org>
2026-09-23 11:45:43 -0700

    Remove superfluous includes from extension_action_view_model.h
    Remove unused include and do IWYU to fix the build.
    
    Bug: 40318405
    Change-Id: I067f2ae9e01620041fba3229b7e216225e9f8007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8445576
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703629}

--- chrome/browser/ui/views/location_bar/webui_location_bar.cc
+++ chrome/browser/ui/views/location_bar/webui_location_bar.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/notimplemented.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/task/single_thread_task_runner.h"
 #include "build/branding_buildflags.h"
 #include "build/buildflag.h"
 #include "chrome/browser/actor/ui/actor_ui_window_controller.h"
