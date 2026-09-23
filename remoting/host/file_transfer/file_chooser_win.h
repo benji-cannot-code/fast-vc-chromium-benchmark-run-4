88e38012dfc33a763ba88b7f45fd5861342adfff
Joe Downing <joedow@google.com>
2026-09-23 10:44:19 -0700

    [remoting] Launch Windows file chooser process off of the UI thread
    LaunchChooserProcess performs several blocking operations including
    WTSQueryUserToken, GetExePath, named pipe creation
    (mojo::PlatformChannel), and base::LaunchProcess (CreateProcessAsUserW).
    
    This CL offloads the blocking process launch to base::ThreadPool using
    PostTaskAndReplyWithResult, and binds the returned Mojo PendingRemote
    and base::Process back on the FileChooser sequence.
    
    Change-Id: I2bca15c73f9902e885be422a3621c1afc6cd8a74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8402915
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703576}

--- remoting/host/file_transfer/file_chooser_win.h
+++ remoting/host/file_transfer/file_chooser_win.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/process/process.h"
 #include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
@@ -60,6 +61,7 @@ class FileChooserWindows : public FileChooser {
   base::Process process_;
   mojo::Remote<mojom::FileChooser> file_chooser_;
   LaunchProcessCallback launcher_for_testing_;
+  base::WeakPtrFactory<FileChooserWindows> weak_factory_{this};
 };
 
 }  // namespace remoting
