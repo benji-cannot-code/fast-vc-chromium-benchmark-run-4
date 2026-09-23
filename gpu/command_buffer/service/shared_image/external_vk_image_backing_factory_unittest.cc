ff0611a1a24c00302497f1024a319d65a8d7dc55
Saifuddin Hitawala <hitawala@chromium.org>
2026-09-23 12:24:10 -0700

    [gpu] Add CHECK for premul alpha with multiplanar formats
    Add a CHECK if SharedImageInfo is created with Premultiply alpha
    for multiplanar format without external sampler. This is because
    creating such a SharedImage is unsupported by Skia.
    
    Bug: 390568190
    Change-Id: Ibbaab3c04d8b09ac8111a24692ddf11253d6e364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8356445
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703660}

--- gpu/command_buffer/service/shared_image/external_vk_image_backing_factory_unittest.cc
+++ gpu/command_buffer/service/shared_image/external_vk_image_backing_factory_unittest.cc
@@ -674,7 +674,7 @@ TEST_P(ExternalVkImageBackingFactoryWithFormatTest,
                                 size,
                                 color_space,
                                 kTopLeft_GrSurfaceOrigin,
-                                kPremul_SkAlphaType,
+                                kUnpremul_SkAlphaType,
                                 usage,
                                 "TestLabel"};
 
