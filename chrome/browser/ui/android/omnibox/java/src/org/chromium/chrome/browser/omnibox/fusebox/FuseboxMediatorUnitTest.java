dc07ccf2c8885f6a785fa446216602940f8ecde9
Peter Kang <peterkang@google.com>
2026-09-24 11:21:31 -0700

    Reland "Modify fusebox popup variations params to preserve existing behavior"
    This is a reland of commit ef4008478e96c747352529a2a375bd8d3027c083
    
    Original change's description:
    > Modify fusebox popup variations params to preserve existing behavior
    >
    > Update sFuseboxPopupShowBottomSheet and sFuseboxPopupCarousel to default
    > to true so that pre-variations behavior is retained when the study is
    > disabled or params are unset. Also update the corresponding parameter
    > names and values in fieldtrial_testing_config.json.
    >
    > Bug: None
    > Change-Id: I9a202eccbda711db147c567ae5662ac54eee08de
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8446841
    > Commit-Queue: Peter Kang <peterkang@google.com>
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1703557}
    
    Bug: None
    Change-Id: Ic93791e441d72c7da8e0b05faaf239adb33af29f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8448284
    Commit-Queue: Peter Kang <peterkang@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704439}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/fusebox/FuseboxMediatorUnitTest.java
@@ -926,6 +926,7 @@ public class FuseboxMediatorUnitTest {
 
     @Test
     public void onPlusButtonClicked_togglePopup() {
+        OmniboxFeatures.setShowBottomSheetPopupForTesting(/* value= */ false);
         Runnable runnable = mModel.get(FuseboxProperties.PLUS_BUTTON_CLICKED);
         assertNotNull(runnable);
 
@@ -952,6 +953,7 @@ public class FuseboxMediatorUnitTest {
 
     @Test
     public void onPlusButtonClicked_updatesPopupStateSupplier() {
+        OmniboxFeatures.setShowBottomSheetPopupForTesting(/* value= */ false);
         assertEquals(PopupState.HIDDEN, mPopupStateSupplier.get().intValue());
         mModel.get(FuseboxProperties.PLUS_BUTTON_CLICKED).run();
         assertEquals(PopupState.FLOATING, mPopupStateSupplier.get().intValue());
@@ -1021,6 +1023,7 @@ public class FuseboxMediatorUnitTest {
 
     @Test
     public void testDoubleBeginInput_doesNotReapplyPopupState() {
+        OmniboxFeatures.setShowBottomSheetPopupForTesting(/* value= */ false);
         mInput.setFocusReason(OmniboxFocusReason.FAKE_BOX_PLUS_BUTTON_TAP);
         recreateMediator();
 
@@ -1081,6 +1084,7 @@ public class FuseboxMediatorUnitTest {
 
     @Test
     public void testEndInput_DismissesPopup() {
+        OmniboxFeatures.setShowBottomSheetPopupForTesting(/* value= */ false);
         mModel.get(FuseboxProperties.PLUS_BUTTON_CLICKED).run();
         assertEquals(PopupState.FLOATING, (int) mModel.get(FuseboxProperties.POPUP_STATE));
 
