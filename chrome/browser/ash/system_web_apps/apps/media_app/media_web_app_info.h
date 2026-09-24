4f296cf7ba9cc9208b27516fb81a3fb9a8d2fb2d
Satoru Takabayashi <satorux@google.com>
2026-09-24 01:19:58 -0700

    media_app: Remove Google Photos Experience HaTS survey trigger
    The HappinessTrackingPhotosExperience feature flag is disabled by default
    and obsolete. This CL removes the PhotosExperienceSurveyTrigger observer,
    HatsProductSpecificDataForMediaApp(), and associated user action tracking
    and browser tests in MediaApp.
    
    This is part 1 of removing the obsolete MediaApp PDF and Photos HaTS
    surveys.
    
    Bug: 562297500
    Change-Id: Ic65e4751eb2fa8f975186bb1d6397e9ae0195c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425259
    Reviewed-by: Patricia Lor <patricialor@chromium.org>
    Commit-Queue: Satoru Takabayashi <satorux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704121}

--- chrome/browser/ash/system_web_apps/apps/media_app/media_web_app_info.h
+++ chrome/browser/ash/system_web_apps/apps/media_app/media_web_app_info.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/containers/flat_map.h"
 #include "chromeos/ash/experiences/system_web_apps/types/system_web_app_delegate.h"
 
 namespace web_app {
@@ -36,11 +35,5 @@ class MediaSystemAppDelegate : public ash::SystemWebAppDelegate {
       const GURL& url,
       const apps::AppLaunchParams& params) const override;
 };
-
-// Returns a snapshot of the product-specific data that is attached to HaTS for
-// the MediaApp.
-base::flat_map<std::string, std::string> HatsProductSpecificDataForMediaApp();
-
-void SetPhotosExperienceSurveyTriggerAppIdForTesting(const char* app_id);
 
 #endif  // CHROME_BROWSER_ASH_SYSTEM_WEB_APPS_APPS_MEDIA_APP_MEDIA_WEB_APP_INFO_H_
