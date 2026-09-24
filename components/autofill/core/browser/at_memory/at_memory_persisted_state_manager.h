1254b458ed57dd1f0f9e1ee93b4a69493124d395
Ireneusz Szulc <ireneuszszulc@google.com>
2026-09-24 10:17:32 -0700

    AtMemory: Dismiss active popup on TTL expiry
    This change makes the AtMemory popup to be dismissed when
    `search_state_timer_` expires with non-empty suggestions.  Also, it
    removes `kSpiiTimeToLive` and always applies `kDefaultTimeToLive` (30
    minutes) to all persisted suggestions, as 1-minute SPII eviction is no
    longer required.
    
    Bug: b:556060660
    Change-Id: I7c933bc2897c517b63900806e3ae22806a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8462083
    Reviewed-by: Piotr Domaradzki <doland@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704379}

--- components/autofill/core/browser/at_memory/at_memory_persisted_state_manager.h
+++ components/autofill/core/browser/at_memory/at_memory_persisted_state_manager.h
@@ -64,7 +64,6 @@ class AtMemoryPersistedStateManager
 
   static constexpr size_t kMaxPreviouslyFilledSuggestions = 20;
   static constexpr base::TimeDelta kDefaultTimeToLive = base::Minutes(30);
-  static constexpr base::TimeDelta kSpiiTimeToLive = base::Minutes(1);
 
   // `client` must be non-null and outlive `this`. The pref service, identity
   // manager and eligibility service are obtained from `client`.
@@ -132,6 +131,7 @@ class AtMemoryPersistedStateManager
   // executes `on_reset_callback_`.
   void Reset();
   void ResetSearchState();
+  void OnSearchStateTimerExpired();
   void RestartSearchStateTimer();
   void RestartPreviouslyFilledSuggestionsTimer();
 
