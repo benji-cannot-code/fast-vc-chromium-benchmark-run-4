ad12f2b2b7edbe8cae9895e5f4525358b84b5d7a
Alex Rudenko <alexrudenko@chromium.org>
2026-09-24 09:41:24 -0700

    [devtools] Migrate elements/highlight web tests to inspector-protocol
    Migrates the following 36 legacy Blink web tests to inspector-protocol/overlay/:
    - http/tests/devtools/elements/highlight/highlight-css-flex-alignment.js
    - http/tests/devtools/elements/highlight/highlight-css-flex-gap.js
    - http/tests/devtools/elements/highlight/highlight-css-flex-item.js
    - http/tests/devtools/elements/highlight/highlight-css-flex-multiline.js
    - http/tests/devtools/elements/highlight/highlight-css-flex-reverse.js
    - http/tests/devtools/elements/highlight/highlight-css-flex.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-area-direction.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-area.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-direction.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-huge.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-lanes-area.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-lanes-direction.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-lanes.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-line-names.js
    - http/tests/devtools/elements/highlight/highlight-css-grid-writing-mode.js
    - http/tests/devtools/elements/highlight/highlight-css-grid.js
    - http/tests/devtools/elements/highlight/highlight-css-persistent-grid-huge.js
    - http/tests/devtools/elements/highlight/highlight-css-persistent-grid-iframe.js
    - http/tests/devtools/elements/highlight/highlight-css-shapes-outside-scroll.js
    - http/tests/devtools/elements/highlight/highlight-css-shapes-outside.js
    - http/tests/devtools/elements/highlight/highlight-detached.js
    - http/tests/devtools/elements/highlight/highlight-display-locked-grid.js
    - http/tests/devtools/elements/highlight/highlight-display-locked-in-frame.js
    - http/tests/devtools/elements/highlight/highlight-display-locked.js
    - http/tests/devtools/elements/highlight/highlight-multiple-css-grid.js
    - http/tests/devtools/elements/highlight/highlight-node-a11y.js
    - http/tests/devtools/elements/highlight/highlight-node-scaled-and-scrolled.js
    - http/tests/devtools/elements/highlight/highlight-node-scaled.js
    - http/tests/devtools/elements/highlight/highlight-node-scroll.js
    - http/tests/devtools/elements/highlight/highlight-node-styles.js
    - http/tests/devtools/elements/highlight/highlight-node-transformed.js
    - http/tests/devtools/elements/highlight/highlight-node-vertical-rl.js
    - http/tests/devtools/elements/highlight/highlight-node.js
    - http/tests/devtools/elements/highlight/highlight-svg-content-inside-iframe.js
    - http/tests/devtools/elements/highlight/highlight-svg-root-zoomed.js
    - http/tests/devtools/elements/highlight/highlight-svg-root.js
    
    Bug: 407751692
    Change-Id: Ib861235f574fe0f76e134f771d824472db15bce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8460442
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704357}

---
+++ third_party/blink/web_tests/inspector-protocol/overlay/highlight-css-shapes-outside-scroll.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {session, dp} =
+      await testRunner.startHTML(`
+      <!DOCTYPE html>
+      <style>
+
+      .float {
+          width: 8em;
+          height: 8em;
+          float: left;
+          shape-margin: 2em;
+          margin: 1em;
+      }
+
+      .circle {
+          background-color:blue;
+          shape-outside: circle(closest-side at center);
+          -webkit-clip-path: circle(closest-side at center);
+      }
+
+      </style>
+      <div class="float circle" id="circle"> </div>
+    `,
+                                 `Highlight CSS shapes outside scroll.\n`);
+
+  const OverlayHelper =
+      await testRunner.loadScript('./resources/highlight-test-helper.js');
+  const helper = new OverlayHelper(testRunner, dp, session);
+  await helper.init();
+
+
+  await helper.dumpHighlight('circle');
+  // Scroll the inspected page and dump again (matching the two dumps in the
+  // original layout test: initial dump at i=0 and post-scroll dump at i=1).
+  await session.evaluate('window.scrollTo(0, 100)');
+  await helper.dumpHighlight('circle');
+  testRunner.completeTest();
+});
