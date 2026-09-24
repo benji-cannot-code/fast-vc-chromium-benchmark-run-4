92e86eca8ab2a168805047a566975b16903e9973
Nico Weber <thakis@chromium.org>
2026-09-24 12:28:14 -0700

    Download linux runtimes on non-linux hosts when cross-building to linux
    Bug: 40176116
    Change-Id: I02cb0f883f290b93e2e46adf0f42f2cbe5a35380
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8461481
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704513}

--- tools/clang/scripts/sync_deps.py
+++ tools/clang/scripts/sync_deps.py
@@ -25,6 +25,7 @@ CLANG_PLATFORM_TO_PACKAGE_FILES = {
   'Linux_x64': [
     'clang',
     'clang-android-runtime-library',
+    'clang-linux-runtime-library',
     'clang-tidy',
     'clangd',
     'llvm-code-coverage',
