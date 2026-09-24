d4d413d078ae3d580739eaa5a3a2c3c07f06925f
Linyu He <linyuh@google.com>
2026-09-24 11:25:42 -0700

    SidePanel: Add integration test for undoing single tab closure
    Also updates UndoSnackbarFacility:
    * Relaxes the host station type bound from TabSwitcherStation to
      Station<?> so it can be used on page stations. In the new test in
      this CL, the UndoSnackbar is triggered on a page station (main browser
      UI instead of Grid Tab Switcher).
    
    * Adds pressUndoTo() to support transitions triggered by pressing the
      undo button (such as arriving at a restored PageStation).
    
    Include-Ci-Only-Tests: true
    Bug: 556366592
    Change-Id: Ib3a36bb3d850ee5f945b52c32965ff7e4ee12c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8455282
    Commit-Queue: Linyu He <linyuh@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704446}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/UndoSnackbarFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/UndoSnackbarFacility.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.test.transit.hub;
 
 import org.chromium.base.test.transit.Station;
+import org.chromium.base.test.transit.TripBuilder;
 import org.chromium.chrome.test.transit.ui.SnackbarFacility;
 
 /**
@@ -13,7 +14,7 @@ import org.chromium.chrome.test.transit.ui.SnackbarFacility;
  *
  * @param <HostStationT> the type of host {@link Station} this is scoped to.
  */
-public class UndoSnackbarFacility<HostStationT extends TabSwitcherStation>
+public class UndoSnackbarFacility<HostStationT extends Station<?>>
         extends SnackbarFacility<HostStationT> {
 
     public UndoSnackbarFacility(String message) {
@@ -22,6 +23,11 @@ public class UndoSnackbarFacility<HostStationT extends TabSwitcherStation>
 
     /** Press undo to revert the operation. */
     public void pressUndo() {
-        buttonElement.clickTo().exitFacility();
+        pressUndoTo().complete();
+    }
+
+    /** Start a transition by pressing undo to revert the operation. */
+    public TripBuilder pressUndoTo() {
+        return buttonElement.clickTo().exitFacilityAnd();
     }
 }
