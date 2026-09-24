49c94feaac0bcdf6ca9dec83b3b146f5074bfcba
Andreas Haas <ahaas@chromium.org>
2026-09-23 21:56:42 -0700

    Allow larger JS ArrayBuffer allocations in PartitionAlloc
    Allow allocating larger JS ArrayBuffers directly without needing the
    workaround of allocating a WebAssembly memory.
    
    To support allocations up to ~16 GiB (16 GiB - kSuperPageSize) for
    ArrayBuffers without adding runtime overhead for normal allocations, we
    retain the normal constexpr bounds check against MaxAllocationSize() on
    the fast path. Only if that check fails do we check whether the
    allocation has AllocFlags::kAllowGigaAllocations and occurs in a
    partition configured with PartitionOptions::allow_giga_allocations,
    checking against the higher MaxGigaAllocationSize() limit only then.
    
    TAG=agy
    CONV=de69a7fe-0685-4882-85dc-5f4d8c93a314
    
    Bug: 40055619
    Change-Id: I05a4343748fecd72c37a50f4d281041c1e4d5590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8402423
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704039}

--- third_party/blink/renderer/core/html/canvas/image_data_test.cc
+++ third_party/blink/renderer/core/html/canvas/image_data_test.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/skia/modules/skcms/skcms.h"
 #include "ui/gfx/geometry/size.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
+#include "third_party/blink/renderer/platform/testing/task_environment.h"
 
 namespace blink {
 namespace {
@@ -19,6 +21,8 @@ class ImageDataTest : public testing::Test {};
 // This test passes if it does not crash. If the required memory is not
 // allocated to the ImageData, then an exception must raise.
 TEST_F(ImageDataTest, CreateImageDataTooBig) {
+  test::TaskEnvironment task_environment;
+  V8TestingScope scope;
   DummyExceptionStateForTesting exception_state;
   ImageData* too_big_image_data = ImageData::Create(
       32767, 32767, ImageDataSettings::Create(), exception_state);
