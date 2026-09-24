19f52c5d07c5723a25c12b4d778161d677778215
Hidehiko Abe <hidehiko@chromium.org>
2026-09-24 10:23:21 -0700

    Use base::TimeTicks for elapsed time in SimpleGeolocationRequest
    SimpleGeolocationRequest used base::Time for tracking request_started_at_
    and computing elapsed request time. Because base::Time is not monotonic,
    wall-clock adjustments could skew elapsed time measurements. Use
    base::TimeTicks instead.
    
    TAG=agy
    CONV=04fa7772-ef71-470e-ad43-d9d117f5c2c5
    
    Bug: 565697515
    Test: chromeos_unittests --gtest_filter=*Location*:*Geolocation*
    Change-Id: I02f9e162bacbe6b572bae4e05b94d85538c15385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8462502
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Zauri Meshveliani‎ <zauri@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704382}

--- chromeos/ash/components/geolocation/simple_geolocation_request.cc
+++ chromeos/ash/components/geolocation/simple_geolocation_request.cc
@@ -382,7 +382,7 @@ SimpleGeolocationRequest::~SimpleGeolocationRequest() {
 
   // If callback is not empty, request is cancelled.
   if (callback_) {
-    RecordUmaResponseTime(base::Time::Now() - request_started_at_, false);
+    RecordUmaResponseTime(base::TimeTicks::Now() - request_started_at_, false);
     RecordUmaResult(SIMPLE_GEOLOCATION_REQUEST_RESULT_CANCELLED, retries_);
   }
 
@@ -475,7 +475,7 @@ void SimpleGeolocationRequest::MakeRequest(
   request_url_ = GeolocationRequestURL(service_url_);
   timeout_timer_.Start(FROM_HERE, timeout_, this,
                        &SimpleGeolocationRequest::OnTimeout);
-  request_started_at_ = base::Time::Now();
+  request_started_at_ = base::TimeTicks::Now();
   StartRequest();
 }
 
@@ -534,7 +534,7 @@ void SimpleGeolocationRequest::OnSimpleURLLoaderComplete(
     return;
   }
 
-  const base::TimeDelta elapsed = base::Time::Now() - request_started_at_;
+  const base::TimeDelta elapsed = base::TimeTicks::Now() - request_started_at_;
   RecordUmaResponseTime(elapsed, success);
 
   RecordUmaResult(SIMPLE_GEOLOCATION_REQUEST_RESULT_SUCCESS, retries_);
@@ -568,7 +568,7 @@ void SimpleGeolocationRequest::OnTimeout() {
            : SIMPLE_GEOLOCATION_REQUEST_RESULT_FAILURE);
   RecordUmaResult(result, retries_);
   position_.status = Geoposition::STATUS_TIMEOUT;
-  const base::TimeDelta elapsed = base::Time::Now() - request_started_at_;
+  const base::TimeDelta elapsed = base::TimeTicks::Now() - request_started_at_;
   ReplyAndDestroySelf(elapsed, true /* server_error */);
   // "this" is already destroyed here.
 }
