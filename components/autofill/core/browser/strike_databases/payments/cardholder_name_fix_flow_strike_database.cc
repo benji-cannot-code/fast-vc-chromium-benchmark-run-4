6bf638ca81ae33f40334dd5964a2bad676b225cc
Siyu <siyua@chromium.org>
2026-09-23 20:11:03 -0700

    [Cardholder Name] Add CardholderNameFixFlowStrikeDatabase
    Add CardholderNameFixFlowStrikeDatabase to manage strike counts and
    cooldown enforcement for the Cardholder Name Fix Flow. The database is
    keyed by card instrument ID, enforces a maximum limit of 3 strikes with
    no expiration, and applies a 7-day cooldown delay since the last strike.
    
    TAG=agy
    CONV=6c84931d-a4fa-48ca-b00d-e497dc09c51d
    
    Bug: 564567572
    Change-Id: I0be50942d9f43a7f2a8130441606e82b4b0f41db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8435541
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704021}

---
+++ components/autofill/core/browser/strike_databases/payments/cardholder_name_fix_flow_strike_database.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/strike_databases/payments/cardholder_name_fix_flow_strike_database.h"
+
+#include <optional>
+
+#include "base/time/time.h"
+
+namespace autofill {
+
+std::optional<base::TimeDelta>
+CardholderNameFixFlowStrikeDatabase::GetRequiredDelaySinceLastStrike() const {
+  return base::Days(7);
+}
+
+}  // namespace autofill
