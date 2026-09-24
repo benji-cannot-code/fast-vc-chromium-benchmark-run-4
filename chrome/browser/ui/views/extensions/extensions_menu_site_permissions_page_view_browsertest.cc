c71c8dd489bd55ddc7b869728d299e211b005092
Mahmoud Rashad <mmrashad@google.com>
2026-09-24 10:43:52 -0700

    Disable AddAndRemoveExtensionWhenSitePermissionsPageIsOpen test
    Bug: 565744038
    Change-Id: Ida8176bf0a4fedd40e6e549355cd8df01819e55f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8460223
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Owners-Override: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704409}

--- chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/scoped_refptr.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/chrome_test_extension_loader.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/extensions/extensions_menu_coordinator.h"
@@ -147,8 +148,17 @@ ExtensionsSitePermissionsPageViewBrowserTest::site_permissions_page() {
                        : nullptr;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionsSitePermissionsPageViewBrowserTest,
-                       AddAndRemoveExtensionWhenSitePermissionsPageIsOpen) {
+// TODO(crbug.com/565744038): Re-enable this test on Windows.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_AddAndRemoveExtensionWhenSitePermissionsPageIsOpen \
+  DISABLED_AddAndRemoveExtensionWhenSitePermissionsPageIsOpen
+#else
+#define MAYBE_AddAndRemoveExtensionWhenSitePermissionsPageIsOpen \
+  AddAndRemoveExtensionWhenSitePermissionsPageIsOpen
+#endif
+IN_PROC_BROWSER_TEST_F(
+    ExtensionsSitePermissionsPageViewBrowserTest,
+    MAYBE_AddAndRemoveExtensionWhenSitePermissionsPageIsOpen) {
   auto extensionA =
       InstallExtensionWithHostPermissions("A Extension", {"<all_urls>"});
 
