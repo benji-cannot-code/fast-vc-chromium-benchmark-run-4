f9637abe5f19a8292f4c643b0baf3837eca0a07e
Jordan Bayles <jophba@chromium.org>
2026-09-24 10:34:14 -0700

    [Remoting] Fix potential UaF in RpcMessenger unregistration
    media::remoting::Receiver is constructed on the main thread, where it
    registers its RPC callback with openscreen::cast::RpcMessenger, but is
    destroyed on the media thread. Its destructor invokes
    UnregisterMessageReceiverCallback on the media thread, causing a
    cross-thread data race and potential UaF.
    
    This patch resolves this with the following changes:
    
    1. Fix Receiver::~Receiver() to post UnregisterMessageReceiverCallback
       to the main_task_runner_ (matching SendRpcMessageOnMainThread and
       CourierRenderer::DeregisterFromRpcMessaging).
    
    2. Replace sequence checks with
       SEQUENCE_CHECKER(media_sequence_checker_), modernizing this area of
       the code and cleaning it up.
    
    3. Adds some unit tests with a test media thread to ensure the threading
       for this class behaves properly.
    
    Bug: 499869964
    Change-Id: Ia22a1a6001a514aefbdc89802531acea0ec5e5aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400832
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704397}

--- media/remoting/receiver.h
+++ media/remoting/receiver.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/timer/timer.h"
@@ -152,6 +153,8 @@ class Receiver final : public Renderer,
 
   // The timer to periodically update the media time.
   base::RepeatingTimer time_update_timer_;
+
+  SEQUENCE_CHECKER(media_sequence_checker_);
 
   base::WeakPtrFactory<Receiver> weak_factory_{this};
 };
