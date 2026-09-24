ff243f468db19e2a8729a5fa8fa5e7e8913e5a94
Kunal Daftari <kunaldaftari@google.com>
2026-09-23 20:25:31 -0700

    [Focus] Styling Color of Ephemeral Group Header
    Text on header shows the current tab count. Applies to both legacy and
    unified tab strips.
    
    Screencast: https://screencast.googleplex.com/cast/NTUzMDkwNjI5Mzk2MDcwNHwyM2U2ODY3OC0wZA
    Bug: 557267190
    Change-Id: I0ad34d46d4fd38bb93340ba612e8f0218770ea5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8443697
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704024}

--- chrome/browser/ui/views/tabs/common/tab_group_header_view.h
+++ chrome/browser/ui/views/tabs/common/tab_group_header_view.h
@@ -114,6 +114,7 @@ class TabGroupHeaderView : public views::FlexLayoutView,
   views::BubbleBorder::Arrow GetAnchorPosition() const override;
 
   void OnDataChanged(const tabs::TabGroupData& tab_group_data);
+  void OnTabCountChanged();
 
   tab_groups::TabGroupId group() const;
 
@@ -139,7 +140,9 @@ class TabGroupHeaderView : public views::FlexLayoutView,
   int GetCornerRadius() const;
   int GetHorizontalInset() const;
 
+  SkColor GetBackgroundColor() const;
   SkColor GetForegroundColor() const;
+  void UpdateColors();
 
   tab_groups::TabGroupVisualData tab_group_visual_data_;
   bool needs_attention_ = false;
