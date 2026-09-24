ad1c57f37397780bbaa3832d5d7421de0ce52e93
Kristi Saney <kristislee@google.com>
2026-09-23 17:15:49 -0700

    Add Gustavo as an OWNER for Reading mode code
    This includes renaming the screen2x files to make the glob pattern
    simpler.
    
    Change-Id: Iaab6f8131e46b29e38852e323ec9555a529b2bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8447559
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Reviewed-by: Gustavo Martin <gusmartin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703940}

--- chrome/renderer/accessibility/read_anything/read_anything_distiller_factory.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_distiller_factory.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "chrome/renderer/accessibility/read_anything/read_anything_app_model.h"
 #include "chrome/renderer/accessibility/read_anything/read_anything_distiller.h"
-#include "chrome/renderer/accessibility/read_anything/screen2x_distiller.h"
+#include "chrome/renderer/accessibility/read_anything/read_anything_screen2x_distiller.h"
 
 ReadAnythingDistillerFactory::ReadAnythingDistillerFactory(
     content::RenderFrame* render_frame,
--- chrome/renderer/accessibility/read_anything/read_anything_screen2x_distiller.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_screen2x_distiller.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/renderer/accessibility/read_anything/screen2x_distiller.h"
+#include "chrome/renderer/accessibility/read_anything/read_anything_screen2x_distiller.h"
 
 #include <utility>
 
