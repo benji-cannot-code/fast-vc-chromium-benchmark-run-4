c60b440e3e205e31d954838456ebe4a0317547b0
Markus Handell <handellm@google.com>
2026-09-24 00:35:42 -0700

    Default-enable media thread priorities
    Enable WebRtcUseMediaThreadTypes, AomVpxUsePresentationThreadType,
    UseHighPriorityThreadGroup, and WebRTCBoostMediaIOThreads by default
    following the launch evaluation in http://shortn/_Ws8YvA2wqM.
    
    These features elevate media and WebRTC processing and I/O threads to
    media thread types (kPresentation and kAudioProcessing) and enable the
    corresponding high-priority ThreadPool groups to reduce media jitter and
    scheduling latency under load. The base::Feature flags are retained so
    each feature remains disable-able as a kill switch.
    
    Also remove the now-redundant WebRTC-MediaTaskQueuePriorities entry from
    fieldtrial_testing_config.json and update
    ThreadPoolImplTestBase::SetupFeatures() to explicitly disable
    kUseHighPriorityThreadGroup when testing the disabled state.
    
    Bug: 470337728
    Test: base_unittests, blink_platform_unittests
    Change-Id: Ib2c3fd3f0fbf04882d7cbfb0ec73c0f28988d44d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8453502
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704092}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2415,7 +2415,7 @@ BASE_FEATURE(kWebRtcPqcForDtls, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kWebRtcSuppressDtlsStateChangeOnClose,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kWebRtcUseMediaThreadTypes, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kWebRtcUseMediaThreadTypes, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRendererMainIsDefaultThreadTypeForWebRTC,
              "RendererMainIsNormalThreadTypeForWebRTC",
