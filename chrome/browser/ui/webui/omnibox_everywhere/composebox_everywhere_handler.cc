1d66bcf3aa93d11bff999a6c4d4c666f0ad2c01b
Diana OuYang <dianaou@google.com>
2026-09-23 18:23:45 -0700

    Adjust navigation parameters and context handling for Composebox Everywhere.
    - Update the page transition type to PAGE_TRANSITION_GENERATED when opening URLs from Composebox Everywhere.
    - Set user_gesture to false in OmniboxEverywhereService navigation params.
    - Return nullptr for BrowserWindowInterface in ContextualSearchboxHandler when the page classification is COMPOSEBOX_EVERYWHERE.
    
    Bug: b:564664956
    Change-Id: I0119cda8b098e35af76c33d535b376c6fe9af5b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8444017
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Reviewed-by: Juliet Knighton <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1703984}

--- chrome/browser/ui/webui/omnibox_everywhere/composebox_everywhere_handler.cc
+++ chrome/browser/ui/webui/omnibox_everywhere/composebox_everywhere_handler.cc
@@ -125,7 +125,7 @@ void ComposeboxEverywhereHandler::OpenUrl(
     base::OnceCallback<void(content::NavigationHandle&)>
         navigation_handle_callback) {
   if (service_) {
-    service_->OpenUrl(url, disposition, ui::PAGE_TRANSITION_LINK,
+    service_->OpenUrl(url, disposition, ui::PAGE_TRANSITION_GENERATED,
                       std::move(navigation_handle_callback));
   }
 }
