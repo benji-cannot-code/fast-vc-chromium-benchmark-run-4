d5405c750c26eee555cde1b43d064e034062bfd9
David Bienvenu <davidbienvenu@chromium.org>
2026-09-24 21:14:20 -0700

    Fix ProtocolHandlersManagerServiceTest in Chrome branded builds
    The protocol_handlers manifest feature requires channel 'dev' in
    _manifest_features.json. In Chrome branded unit_tests builds,
    run_all_unittests.cc overrides the default channel to Channel::kStable,
    causing the protocol_handlers key to be ignored during extension
    loading.
    
    Add ScopedCurrentChannel set to version_info::Channel::DEV in
    ProtocolHandlersManagerServiceTest.
    
    bug: 563435066
    Change-Id: I2118a0ac015f8bf4e0808445165c237716386e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8429047
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704630}

--- chrome/browser/extensions/api/protocol_handlers/protocol_handlers_manager_unittest.cc
+++ chrome/browser/extensions/api/protocol_handlers/protocol_handlers_manager_unittest.cc
@@ -11,12 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/custom_handlers/protocol_handler.h"
 #include "components/custom_handlers/protocol_handler_registry.h"
 #include "components/custom_handlers/simple_protocol_handler_registry_factory.h"
+#include "components/version_info/channel.h"
 #include "extensions/browser/disable_reason.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registrar.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/extension_features.h"
+#include "extensions/common/features/feature_channel.h"
 #include "extensions/common/switches.h"
 
 namespace {
@@ -56,6 +58,10 @@ class ProtocolHandlersManagerServiceTest : public ExtensionServiceTestBase {
   }
 
  private:
+  // TODO(https://crbug.com/563435066): Document that this can be removed if
+  // the feature goes to stable - see
+  // extensions/common/api/_manifest_features.json.
+  ScopedCurrentChannel current_channel_{version_info::Channel::DEV};
   base::test::ScopedFeatureList feature_list_;
 };
 
