6a577155e227117c22365933c6e914ef37f2d594
Fernando Ramirez <ferny@google.com>
2026-09-24 21:13:06 -0700

    [AF] Create OmniboxGPayButtonPageActionController skeleton class
    This CL creates the class that will be used to show and control the GPay
    button page action (see chrome/browser/ui/page_action/README.md).
    
    Design: go/chrome-omnibox-gpay-button-design
    Fixed: 565902966
    Change-Id: Iba6de54b513bcb0af2f88597d05cc855bb570ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8465130
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Olivia Saul <osaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704628}

---
+++ chrome/browser/ui/autofill/payments/omnibox_gpay_button_page_action_controller.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_OMNIBOX_GPAY_BUTTON_PAGE_ACTION_CONTROLLER_H_
+#define CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_OMNIBOX_GPAY_BUTTON_PAGE_ACTION_CONTROLLER_H_
+
+namespace autofill {
+
+class OmniboxGPayButtonPageActionController {
+ public:
+  OmniboxGPayButtonPageActionController();
+  ~OmniboxGPayButtonPageActionController();
+  OmniboxGPayButtonPageActionController(
+      const OmniboxGPayButtonPageActionController&) = delete;
+  OmniboxGPayButtonPageActionController& operator=(
+      const OmniboxGPayButtonPageActionController&) = delete;
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_OMNIBOX_GPAY_BUTTON_PAGE_ACTION_CONTROLLER_H_
